e8dae886541d4ae4988aa89bb24a70b32730ebfe
Shandem <unknown@kiln.example.com>
2009-08-03 11:51:03 +0000

    DO NOT D,OWNLOAD. DOWNLOAD LATEST STABLE FROM RELEASE TAB File cleanup, removed resx files, moved all files tagged for deletion to umbraco.Legacy project.
    [TFS Changeset #57568]

---
+++ components/editorControls/wysiwyg/editorButton.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace umbraco.editorControls.wysiwyg
+{
+    public class editorButton
+    {
+        public string imageUrl;
+        public string onClickCommand;
+        public string alttag;
+        public string id;
+
+        public editorButton(string Id, string AltTag, string ImageUrl, string OnClickCommand)
+        {
+            id = Id;
+            alttag = AltTag;
+            imageUrl = ImageUrl;
+            onClickCommand = OnClickCommand;
+        }
+    }
+}
