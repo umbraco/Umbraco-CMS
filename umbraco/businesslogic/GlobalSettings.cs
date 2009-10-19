a24fdc8c78afca66166dc9403692876337168d72
hartvig <unknown@kiln.example.com>
2009-10-19 10:17:52 +0000

    DO NOT DOWNLOAD, STABLE RELEASE AVAILABLE ON THE DOWNLOADS TAB Setting internal version number to 4.1.0beta :-)
    [TFS Changeset #60253]

--- umbraco/businesslogic/GlobalSettings.cs
+++ umbraco/businesslogic/GlobalSettings.cs
@@ -15,7 +15,7 @@ namespace umbraco
     {
         #region Private static fields
         // CURRENT UMBRACO VERSION ID
-        private static string _currentVersion = "4.1.0.alpha";
+        private static string _currentVersion = "4.1.0.beta";
 
         private static string _reservedUrlsCache;
         private static string _reservedPathsCache;
