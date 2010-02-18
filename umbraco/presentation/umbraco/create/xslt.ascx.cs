5806330ebbf1fcb346c00005d00e05bda7992fe4
hartvig <unknown@kiln.example.com>
2010-02-18 21:12:37 +0000

    WORK IN PROGRESS, GET STABLE SOURCE FROM DOWNLOAD TAB Implements 26252: Get new xslt templates if new schema is used
    [TFS Changeset #64069]

--- umbraco/presentation/umbraco/create/xslt.ascx.cs
+++ umbraco/presentation/umbraco/create/xslt.ascx.cs
@@ -13,68 +13,81 @@ namespace umbraco.presentation.create
 {
 
 
-	/// <summary>
-	///		Summary description for xslt.
-	/// </summary>
-	public partial class xslt : System.Web.UI.UserControl
-	{
-		protected System.Web.UI.WebControls.ListBox nodeType;
+    /// <summary>
+    ///		Summary description for xslt.
+    /// </summary>
+    public partial class xslt : System.Web.UI.UserControl
+    {
+        protected System.Web.UI.WebControls.ListBox nodeType;
 
-		protected void Page_Load(object sender, System.EventArgs e)
-		{
+        protected void Page_Load(object sender, System.EventArgs e)
+        {
             sbmt.Text = ui.Text("create");
-            foreach (string fileName in Directory.GetFiles( IOHelper.MapPath(SystemDirectories.Umbraco + "/xslt/templates"), "*.xslt") )
-			{
-				FileInfo fi = new FileInfo(fileName);
-				if (fi.Name != "Clean.xslt")
-					xsltTemplate.Items.Add(new ListItem(helper.SpaceCamelCasing(fi.Name.Replace(".xslt", "")), fi.Name));
+            foreach (string fileName in Directory.GetFiles(IOHelper.MapPath(SystemDirectories.Umbraco + getXsltTemplatePath()), "*.xslt"))
+            {
+                FileInfo fi = new FileInfo(fileName);
+                if (fi.Name != "Clean.xslt")
+                    xsltTemplate.Items.Add(new ListItem(helper.SpaceCamelCasing(fi.Name.Replace(".xslt", "")), fi.Name));
 
-			}
+            }
 
-		}
+        }
 
-		#region Web Form Designer generated code
-		override protected void OnInit(EventArgs e)
-		{
-			//
-			// CODEGEN: This call is required by the ASP.NET Web Form Designer.
-			//
-			InitializeComponent();
-			base.OnInit(e);
-		}
+        #region Web Form Designer generated code
+        override protected void OnInit(EventArgs e)
+        {
+            //
+            // CODEGEN: This call is required by the ASP.NET Web Form Designer.
+            //
+            InitializeComponent();
+            base.OnInit(e);
+        }
 
-		/// <summary>
-		///		Required method for Designer support - do not modify
-		///		the contents of this method with the code editor.
-		/// </summary>
-		private void InitializeComponent()
-		{
+        /// <summary>
+        ///		Required method for Designer support - do not modify
+        ///		the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
 
-		}
-		#endregion
+        }
+        #endregion
 
-		protected void sbmt_Click(object sender, System.EventArgs e)
-		{
-			if (Page.IsValid)
-			{
-				int createMacroVal = 0;
-				if (createMacro.Checked)
-					createMacroVal = 1;
-				string returnUrl = dialogHandler_temp.Create(
-					helper.Request("nodeType"),
-					createMacroVal,
-					xsltTemplate.SelectedValue + "|||" + rename.Text);
+        private string getXsltTemplatePath()
+        {
+            if (UmbracoSettings.UseLegacyXmlSchema) {
+                return "/xslt/templates";
+            } else {
+                return "/xslt/templates/schema2";
+            }
+        }
 
-				BasePage.Current.ClientTools
-					.ChangeContentFrameUrl(returnUrl)
-					.ChildNodeCreated()
-					.CloseModalWindow();
+        protected void sbmt_Click(object sender, System.EventArgs e)
+        {
+            if (Page.IsValid)
+            {
+                int createMacroVal = 0;
+                if (createMacro.Checked)
+                    createMacroVal = 1;
+
+                string xsltName = UmbracoSettings.UseLegacyXmlSchema ? xsltTemplate.SelectedValue :
+                    Path.Combine("schema2", xsltTemplate.SelectedValue);
+
+                string returnUrl = dialogHandler_temp.Create(
+                    helper.Request("nodeType"),
+                    createMacroVal,
+                    xsltName + "|||" + rename.Text);
+
+                BasePage.Current.ClientTools
+                    .ChangeContentFrameUrl(returnUrl)
+                    .ChildNodeCreated()
+                    .CloseModalWindow();
 
 
 
 
-			}
+            }
 
-		}
-	}
+        }
+    }
 }
