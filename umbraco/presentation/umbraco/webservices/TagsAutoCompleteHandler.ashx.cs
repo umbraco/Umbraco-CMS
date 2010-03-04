570c792341882502d25ec88d9624e1daae5121b2
starfighter83 <unknown@kiln.example.com>
2010-03-04 11:33:56 +0000

    DO NOT DOWNLOAD - GET THE STABLE SOURCE FROM THE DOWNLOADS TAB Fixes 26386 (Tag datatype, use jquery autocomplete instead of ajaxcontroltoolkit)
    [TFS Changeset #64431]

---
+++ umbraco/presentation/umbraco/webservices/TagsAutoCompleteHandler.ashx.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Services;
+using umbraco.DataLayer;
+using umbraco.BusinessLogic;
+
+namespace umbraco.presentation.umbraco.webservices
+{
+    /// <summary>
+    /// Summary description for $codebehindclassname$
+    /// </summary>
+    [WebService(Namespace = "http://tempuri.org/")]
+    [WebServiceBinding(ConformsTo = WsiProfiles.BasicProfile1_1)]
+    public class TagsAutoCompleteHandler : IHttpHandler
+    {
+
+        public void ProcessRequest(HttpContext context)
+        {
+            Authorize();
+            context.Response.ContentType = "text/plain";
+
+            int count = 2;
+            string prefixText = context.Request.QueryString["q"];
+            string group = context.Request.QueryString["group"];
+            string id = context.Request.QueryString["id"];
+           
+            //fallback...
+            string sql;
+
+            IRecordsReader rr;
+
+            try
+            {
+                //if all is correct
+                if (!String.IsNullOrEmpty(group) && !String.IsNullOrEmpty(id))
+                {
+                    sql = @"SELECT TOP 20 tag FROM cmsTags WHERE tag LIKE @prefix AND cmsTags.id not in 
+                        (SELECT tagID FROM cmsTagRelationShip WHERE NodeId = @nodeId) AND cmstags.[group] = @group;";
+
+                    rr = SqlHelper.ExecuteReader(sql,
+                        SqlHelper.CreateParameter("@count", count),
+                        SqlHelper.CreateParameter("@prefix", prefixText + "%"),
+                        SqlHelper.CreateParameter("@nodeId", id),
+                        SqlHelper.CreateParameter("@group", group)
+                        );
+
+
+
+                }
+                else
+                {
+                    //fallback...
+                    sql = "SELECT TOP 20 tag FROM cmsTags WHERE tag LIKE @prefix";
+
+                    rr = SqlHelper.ExecuteReader(sql,
+                       SqlHelper.CreateParameter("@count", count),
+                       SqlHelper.CreateParameter("@prefix", prefixText + "%")
+                       );
+                }
+
+
+                while (rr.Read())
+                {
+                    context.Response.Write(rr.GetString("tag") + Environment.NewLine);
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                Log.Add(LogTypes.Debug, -1, ex.ToString());
+            }
+
+        }
+
+        private static ISqlHelper _sqlHelper;
+        private static string _ConnString = GlobalSettings.DbDSN;
+
+        public static ISqlHelper SqlHelper
+        {
+            get
+            {
+                if (_sqlHelper == null)
+                {
+                    try
+                    {
+                        _sqlHelper = DataLayerHelper.CreateSqlHelper(_ConnString);
+                    }
+                    catch { }
+                }
+                return _sqlHelper;
+            }
+        }
+
+        public static void Authorize()
+        {
+            if (!BasePages.BasePage.ValidateUserContextID(BasePages.BasePage.umbracoUserContextID))
+                throw new Exception("Client authorization failed. User is not logged in");
+
+        }
+
+        public bool IsReusable
+        {
+            get
+            {
+                return false;
+            }
+        }
+    }
+}
