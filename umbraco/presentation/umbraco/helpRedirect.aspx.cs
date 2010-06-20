05846327f6827b0b7f878a82fb4fcbd2682d4cc5
kenny <unknown@kiln.example.com>
2010-06-20 09:38:09 +0000

    Added helpRedirect.aspx to umbraco.presentation project. Work in progress...
    [TFS Changeset #70185]

---
+++ umbraco/presentation/umbraco/helpRedirect.aspx.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace umbraco.presentation.umbraco
+{
+    public partial class helpRedirect : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
