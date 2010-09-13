c3044f41fce5d24938557918a5178342a58a4ee9
starfighter83 <unknown@kiln.example.com>
2010-09-13 09:41:10 +0000

    WIP installer refactor, adds ImageUploader page to canvas skin module
    [TFS Changeset #77126]

---
+++ umbraco/presentation/umbraco/LiveEditing/Modules/SkinModule/ImageUploader.aspx
@@ -0,0 +1,48 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="ImageUploader.aspx.cs" Inherits="umbraco.presentation.umbraco.LiveEditing.Modules.SkinModule.ImageUploader" %>
+
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <title></title>
+
+    <script type="text/javascript" src="../../../../umbraco_client/ui/jquery.js"></script>
+
+    <script type="text/javascript">
+
+        function setImage() {
+
+            var val = $('#<%= Image.ClientID %>').val();
+            top.jQuery('#<%= Request["ctrl"] %>').val(val);
+            top.jQuery('#<%= Request["ctrl"] %>').trigger('change');
+            closeModal();
+        }
+
+        function closeModal() {
+
+            return false;
+        }
+    
+    </script>
+</head>
+<body>
+    <form id="form1" runat="server">
+
+
+    <asp:HiddenField ID="Image" runat="server" />
+
+    <asp:FileUpload ID="FileUpload1" runat="server" /> 
+    <asp:Button ID="bt_upload" runat="server" Text="Upload" 
+        onclick="bt_upload_Click" /><br />
+
+    <asp:Image ID="Image1" runat="server" />
+
+
+    <br />
+
+    <button type="button" id="bt_select" onclick=" setImage();">Select</button> or
+    <button type="button" onclick="closeModal();">Cancel</button>
+
+    </form>
+</body>
+</html>
---
+++ umbraco/presentation/umbraco/LiveEditing/Modules/SkinModule/ImageUploader.aspx.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.IO;
+
+namespace umbraco.presentation.umbraco.LiveEditing.Modules.SkinModule
+{
+    public partial class ImageUploader : BasePages.UmbracoEnsuredPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void bt_upload_Click(object sender, EventArgs e)
+        {
+            if (FileUpload1.HasFile)
+            {
+                Guid g = Guid.NewGuid();
+                DirectoryInfo updir = new DirectoryInfo(Server.MapPath("~/media/upload/" + g));
+               
+                if (!updir.Exists)
+                    updir.Create();
+
+                FileUpload1.SaveAs(updir.FullName + "/" + FileUpload1.FileName);
+
+                if (IsValidImage(updir.FullName + "/" + FileUpload1.FileName))
+                {
+                    Image1.ImageUrl = this.ResolveUrl("~/media/upload/" + g) + "/" + FileUpload1.FileName;
+                    Image.Value = Image1.ImageUrl;
+                }
+                else
+                    throw new Exception("Not a valid image");
+            }
+        }
+
+        private bool IsValidImage(string filename)
+        {
+            try
+            {
+                System.Drawing.Image newImage = System.Drawing.Image.FromFile(filename);
+            }
+            catch (OutOfMemoryException ex)
+            {
+                // Image.FromFile will throw this if file is invalid.
+
+                return false;
+            }
+            return true;
+        }
+    }
+}
