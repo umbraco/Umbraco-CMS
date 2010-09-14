437538b23f43b9ae85e3bd033368ee0c93dd3d8e
starfighter83 <unknown@kiln.example.com>
2010-09-14 07:01:20 +0000

    WIP installer refactor, color picker changes
    [TFS Changeset #77159]

--- umbraco/cms/businesslogic/skinning/controls/ColorPicker.cs
+++ umbraco/cms/businesslogic/skinning/controls/ColorPicker.cs
@@ -5,38 +5,15 @@ using System.Text;
 using System.Web.UI.WebControls;
 using ClientDependency.Core;
 using System.Web.UI;
+using ClientDependency.Core.Controls;
 
 namespace umbraco.cms.businesslogic.skinning.controls
 {
-    //[ClientDependency(ClientDependencyType.Javascript, "colorpicker/js/colorpicker.js", "UmbracoClient")]
+    [ClientDependency(450,ClientDependencyType.Javascript, "colorpicker/js/colorpicker.js", "UmbracoClient")]
+    [ClientDependency(460, ClientDependencyType.Javascript, "colorpicker/js/initcolorpicker.js", "UmbracoClient")]
     [ClientDependency(ClientDependencyType.Css, "colorpicker/css/colorpicker.css", "UmbracoClient")]
     public class ColorPicker: TextBox
     {
-
-
-        protected override void OnLoad(EventArgs e)
-        {
-            base.OnLoad(e);
-
-            ScriptManager.RegisterClientScriptInclude(this, this.GetType(), "colorpicker.js", 
-                this.ResolveUrl(umbraco.IO.SystemDirectories.Umbraco_client + "/colorpicker/js/colorpicker.js"));
-
-
-            ScriptManager.RegisterClientScriptBlock(this, this.GetType(), "cp" + this.ClientID,
-               string.Format(@"$(document).ready(function() {{
-                            $('#{0}').ColorPicker({{
-            	                onSubmit: function(hsb, hex, rgb, el) {{
-            		                $(el).val('#' + hex);
-            		                $(el).ColorPickerHide();
-                                    jQuery(el).trigger('change');
-            	                }},
-            	                onBeforeShow: function () {{
-            		                $(this).ColorPickerSetColor(this.value);
-            	                }}
-                            }})
-                            .bind('keyup', function(){{
-            	                $(this).ColorPickerSetColor(this.value);
-                            }});}});", this.ClientID), true);
-        }
+        
     }
 }
---
+++ umbraco/presentation/umbraco_client/colorpicker/js/initcolorpicker.js
@@ -0,0 +1,13 @@
+﻿jQuery('input.color').ColorPicker({
+onSubmit: function(hsb, hex, rgb, el) {
+    jQuery(el).val('#' + hex);
+    jQuery(el).ColorPickerHide();
+    jQuery(el).trigger('change');
+},
+onBeforeShow: function () {
+    jQuery(this).ColorPickerSetColor(this.value);
+}
+})
+.bind('keyup', function(){
+    jQuery(this).ColorPickerSetColor(this.value);
+});
