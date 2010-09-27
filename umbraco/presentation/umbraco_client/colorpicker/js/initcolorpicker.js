78f96b255684ce49aad821bc3d9b165272a2138c
starfighter83 <unknown@kiln.example.com>
2010-09-27 12:13:29 +0000

    WIP installer refactor fixes 28978: Color picker, remove need for button
    [TFS Changeset #77527]

--- umbraco/presentation/umbraco_client/colorpicker/js/initcolorpicker.js
+++ umbraco/presentation/umbraco_client/colorpicker/js/initcolorpicker.js
@@ -1,13 +1,22 @@
-﻿jQuery('input.color').ColorPicker({
-onSubmit: function(hsb, hex, rgb, el) {
-    jQuery(el).val('#' + hex);
-    jQuery(el).ColorPickerHide();
-    jQuery(el).trigger('change');
-},
-onBeforeShow: function () {
-    jQuery(this).ColorPickerSetColor(this.value);
-}
+﻿var activecolorpicker;
+
+jQuery('input.color').ColorPicker({
+    onSubmit: function (hsb, hex, rgb, el) {
+        jQuery(el).val('#' + hex);
+        jQuery(el).ColorPickerHide();
+        jQuery(el).trigger('change');
+    },
+    onBeforeShow: function () {
+        activecolorpicker = this;
+        jQuery(this).ColorPickerSetColor(this.value);
+    },
+    onChange: function (hsb, hex, rgb) {
+       
+        jQuery(activecolorpicker).val('#' + hex);
+        jQuery(activecolorpicker).trigger('change');
+    }
 })
-.bind('keyup', function(){
+.bind('keyup', function () {
     jQuery(this).ColorPickerSetColor(this.value);
 });
+
