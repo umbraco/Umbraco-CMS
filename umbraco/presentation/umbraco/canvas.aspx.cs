d0ba1575016fe59e530c6facf1b7c4369e577ce7
starfighter83 <unknown@kiln.example.com>
2010-10-01 11:55:14 +0000

    WIP installer refactor, open skin customizer on startup if umbSkinningConfigurator is set
    [TFS Changeset #77706]

--- umbraco/presentation/umbraco/canvas.aspx.cs
+++ umbraco/presentation/umbraco/canvas.aspx.cs
@@ -17,8 +17,8 @@ namespace umbraco.presentation
                 else if (Request.UrlReferrer != null && !Request.UrlReferrer.AbsolutePath.Contains("login.aspx"))
                     redirUrl = Request.UrlReferrer.AbsolutePath;
 
-                Response.Redirect(redirUrl + 
-                    (string.IsNullOrEmpty(UmbracoContext.Current.Request["umbSkinning"]) ? "" : "?umbSkinning=true") , true);
+                Response.Redirect(redirUrl +
+                    (string.IsNullOrEmpty(UmbracoContext.Current.Request["umbSkinning"]) ? "" : "?umbSkinning=true") + (string.IsNullOrEmpty(UmbracoContext.Current.Request["umbSkinningConfigurator"]) ? "" : "&umbSkinningConfigurator=true"), true);
             }
             else
             {
