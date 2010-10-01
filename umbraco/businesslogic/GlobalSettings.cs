bc6c01f44c4d117c3ed63aa47055257af8a712eb
hartvig <unknown@kiln.example.com>
2010-10-01 10:37:50 +0000

    Updating version number
    [TFS Changeset #77698]

--- umbraco/businesslogic/GlobalSettings.cs
+++ umbraco/businesslogic/GlobalSettings.cs
@@ -18,7 +18,7 @@ namespace umbraco
     {
         #region Private static fields
         // CURRENT UMBRACO VERSION ID
-        private static string _currentVersion = "4.6 Alpha";
+        private static string _currentVersion = "4.6.0.Alpha";
 
         private static string _reservedUrlsCache;
         private static string _reservedPathsCache;
