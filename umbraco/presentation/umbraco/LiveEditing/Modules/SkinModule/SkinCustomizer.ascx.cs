1ea5bc5d7c22911ddc81d0e12b7da0290f00376e
starfighter83 <unknown@kiln.example.com>
2010-10-20 07:26:44 +0000

    WIP installer refactor, adds new skinning task type, ModifyPageProperty
    [TFS Changeset #78647]

--- umbraco/presentation/umbraco/LiveEditing/Modules/SkinModule/SkinCustomizer.ascx.cs
+++ umbraco/presentation/umbraco/LiveEditing/Modules/SkinModule/SkinCustomizer.ascx.cs
@@ -53,6 +53,8 @@ namespace umbraco.presentation.LiveEditing.Modules.SkinModule
                     }
                 }
             }
+
+            library.RefreshContent();
         }
 
         protected void LoadDependencies()
