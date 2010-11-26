aafbb3bfb0bed7ab615dd92a4ac533d537d74a86
starfighter83 <unknown@kiln.example.com>
2010-11-26 13:46:30 +0000

    WIP skinning, adds CssVariable and CssVariableProperty to skinning bl
    [TFS Changeset #80669]

---
+++ umbraco/cms/businesslogic/skinning/CssVariable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace umbraco.cms.businesslogic.skinning
+{
+    public class CssVariable
+    {
+        public string Name { get; set; }
+        public List<CssVariableProperty> Properties { get; set; }
+
+        public CssVariable(string Name)
+        {
+            this.Name = Name;
+            Properties = new List<CssVariableProperty>();
+        }
+    }
+}
---
+++ umbraco/cms/businesslogic/skinning/CssVariableProperty.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace umbraco.cms.businesslogic.skinning
+{
+    public class CssVariableProperty
+    {
+        public string Name { get; set; }
+        public List<string> Selectors { get; set; }
+
+        public CssVariableProperty(string Name)
+        {
+            this.Name = Name;
+            Selectors = new List<string>();
+        }
+    }
+}
