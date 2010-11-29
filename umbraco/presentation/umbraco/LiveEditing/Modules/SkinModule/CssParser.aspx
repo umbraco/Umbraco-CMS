ce3851828c89944ca57e54fcfb17764ac7190542
starfighter83 <unknown@kiln.example.com>
2010-11-29 08:58:35 +0000

    WIP skinning, add css parser page to skinning module
    [TFS Changeset #80822]

---
+++ umbraco/presentation/umbraco/LiveEditing/Modules/SkinModule/CssParser.aspx
@@ -0,0 +1,1 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="CssParser.aspx.cs" Inherits="umbraco.presentation.umbraco.LiveEditing.Modules.SkinModule.CssParser" %>
---
+++ umbraco/presentation/umbraco/LiveEditing/Modules/SkinModule/CssParser.aspx.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace umbraco.presentation.umbraco.LiveEditing.Modules.SkinModule
+{
+    public partial class CssParser : System.Web.UI.Page
+    {
+        //will be used to parse the global variables in the embedded css of a skin manifest
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
