5859cc9867531b0cf4502872c26815fd44da3393
Elijah <unknown@kiln.example.com>
2011-01-21 05:22:49 -1000

    Added BaseContext Future Stub

---
+++ umbraco.MacroEngines.Juno/BaseContext.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Web.WebPages;
+using umbraco.cms.businesslogic.macro;
+using umbraco.interfaces;
+
+namespace umbraco.MacroEngines
+{
+    public abstract class BaseContext<T> : WebPage, IMacroContext
+    {
+        private MacroModel _macro;
+        protected T CurrentModel;
+        protected DynamicLambdaDictionary<string> ParameterDictionary;
+        protected DynamicLambdaDictionary<string> CultureDictionary;
+
+        public dynamic Parameter { get { return ParameterDictionary; } }
+        public dynamic Dictionary { get { return CultureDictionary; } }
+
+        public MacroModel Macro { get { return _macro; } }
+        public T Current { get { return CurrentModel; } }
+        public new dynamic Model { get { return CurrentModel; } }
+
+        public virtual void SetMembers(MacroModel macro, INode node) {
+            if (macro == null)
+                throw new ArgumentNullException("macro");
+            if (node == null)
+                throw new ArgumentNullException("node");
+            _macro = macro;
+            ParameterDictionary = new ParameterDictionary(macro.Properties);
+            CultureDictionary = new CultureDictionary();
+        }
+    }
+}
--- umbraco.MacroEngines.Juno/DynamicNodeContext.cs
+++ umbraco.MacroEngines.Juno/DynamicNodeContext.cs
@@ -1,33 +1,18 @@
 ﻿using System;
-using System.Web.WebPages;
 using umbraco.cms.businesslogic.macro;
 using umbraco.interfaces;
 
 namespace umbraco.MacroEngines {
 
-    public abstract class DynamicNodeContext : WebPage, IMacroContext {
+    public abstract class DynamicNodeContext : BaseContext<DynamicNode> {
 
-        private MacroModel _macro;
-        private DynamicNode _dynamicNode;
-        private DynamicLambdaDictionary<string> _parameter;
-        private DynamicLambdaDictionary<string> _culture;
-
-        public dynamic Parameter { get { return _parameter; } }
-        public dynamic Dictionary { get { return _culture; } }
-
-        public MacroModel Macro { get { return _macro; } }
-        public DynamicNode Current { get { return _dynamicNode; } }
-        public new dynamic Model { get { return _dynamicNode; } }
-
-        public void SetMembers(MacroModel macro, INode node) {
+        public override void SetMembers(MacroModel macro, INode node) {
             if (macro == null)
                 throw new ArgumentNullException("macro");
             if (node == null)
                 throw new ArgumentNullException("node");
-            _macro = macro;
-            _dynamicNode = new DynamicNode(node);
-            _parameter = new ParameterDictionary(macro.Properties);
-            _culture = new CultureDictionary();
+            CurrentModel = new DynamicNode(node);
+            base.SetMembers(macro, node);
         }
 
     }
