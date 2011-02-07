649a98daf4386c777fa73f823f35859acab28eaa
agrath@gmail.com <unknown@kiln.example.com>
2011-02-06 19:53:07 -1300

    Added missing DynamicQueryable and DynamicQueryableBinders class files for previous commit

---
+++ umbraco.MacroEngines.Juno/DynamicQueryableBinders.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Dynamic;
+
+namespace umbraco.MacroEngines
+{
+    public class DynamicQueryableGetMemberBinder : GetMemberBinder
+    {
+        public DynamicQueryableGetMemberBinder(string name, bool ignoreCase) : base(name, ignoreCase) { }
+
+        public override DynamicMetaObject FallbackGetMember(DynamicMetaObject target, DynamicMetaObject errorSuggestion)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
