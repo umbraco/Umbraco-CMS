60c18bdff95ef6c00508880256b19b0a55f02428
agrath@gmail.com <unknown@kiln.example.com>
2011-02-27 21:53:45 -1300

    More graceful handling of missing properties in .Where - TryGetMember now returns DynamicNull (IEnumerable with fake .Where and .OrderBy) when the property/child type doesn't exist. Expression Tree in DynamicQueryable explictly checks for this type and forces that portion of the predicate/expression tree to return true, bypassing the missing property Handy when you are checking e.g. a boolean property in a .Where and that boolean doesn't exist on all nodes.

---
+++ umbraco.MacroEngines.Juno/RazorDynamicNode/DynamicNull.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections;
+using System.Dynamic;
+
+namespace umbraco.MacroEngines
+{
+    //This type is used as a return type when TryGetMember fails on a DynamicNode
+    //.Where explicitly checks for this type, to indicate that nothing was returned
+    //Because it's IEnumerable, if the user is actually trying @Model.TextPages or similar
+    //it will still return an enumerable object (assuming the call actually failed because there were no children of that type)
+    //but in .Where, if they use a property that doesn't exist, the lambda will bypass this and return false
+    public class DynamicNull : DynamicObject, IEnumerable
+    {
+        public IEnumerator GetEnumerator()
+        {
+            return (new List<DynamicNull>()).GetEnumerator();
+        }
+        public DynamicNull Where(string predicate, params object[] values)
+        {
+            return this;
+        }
+        public DynamicNull OrderBy(string orderBy)
+        {
+            return this;
+        }
+    }
+}
