cd38cb1412232b23d8e3c4e6e6bd6ba3e7c24c9a
starfighter83 <unknown@kiln.example.com>
2011-03-14 09:44:03 -0100

    Updates tinymce to 3.4

--- umbraco/presentation/umbraco_client/tinymce3/plugins/advhr/langs/de_dlg.js
+++
@@ -1,5 +0,0 @@
-tinyMCE.addI18n('de.advhr_dlg',{
-width:"Breite",
-size:"H\u00F6he",
-noshade:"Kein Schatten"
-});
--- umbraco/presentation/umbraco_client/tinymce3/plugins/advhr/langs/sv_dlg.js
+++
@@ -1,5 +0,0 @@
-tinyMCE.addI18n('sv.advhr_dlg',{
-width:"Bredd",
-size:"H\u00F6jd",
-noshade:"Ingen skugga"
-});
