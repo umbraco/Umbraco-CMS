4221d8a53f03e4edd5f085796e840300550ab9db
agrath <unknown@kiln.example.com>
2011-09-21 21:29:52 -1200

    Added missing constructor overloads back onto DynamicMedia

--- umbraco.MacroEngines.Juno/RazorDynamicNode/DynamicMedia.cs
+++ umbraco.MacroEngines.Juno/RazorDynamicNode/DynamicMedia.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using umbraco.cms.businesslogic.media;
 
 namespace umbraco.MacroEngines
 {
@@ -9,5 +10,12 @@ namespace umbraco.MacroEngines
     public class DynamicMedia : DynamicNode
     {
         public DynamicMedia(DynamicBackingItem item) : base(item) { }
+
+        public DynamicMedia(int mediaId) : base(mediaId, DynamicBackingItemType.Media) { }
+        public DynamicMedia(Media media) : base(media.Id, DynamicBackingItemType.Media) { }
+        public DynamicMedia(string mediaId) : base(mediaId) { }
+        public DynamicMedia(object mediaId) : base(mediaId) { }
+        public DynamicMedia() : base() { }
+
     }
 }
