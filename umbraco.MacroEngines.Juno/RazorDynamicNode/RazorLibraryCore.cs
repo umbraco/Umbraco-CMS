79f4925faf933984a4c137722ccdbd5517248ce6
agrath <unknown@kiln.example.com>
2011-10-16 09:24:14 -1300

    Fixed a null in @Library.Wrap check that wasn't short circuiting as intended

--- umbraco.MacroEngines.Juno/RazorDynamicNode/RazorLibraryCore.cs
+++ umbraco.MacroEngines.Juno/RazorDynamicNode/RazorLibraryCore.cs
@@ -231,7 +231,7 @@ namespace umbraco.MacroEngines.Library
         public HtmlTagWrapper Wrap(string tag, object inner, object anonymousAttributes, params HtmlTagWrapperBase[] Children)
         {
             string innerText = null;
-            if (inner.GetType() != typeof(DynamicNull) && inner != null)
+            if (inner != null && inner.GetType() != typeof(DynamicNull))
             {
                 innerText = string.Format("{0}", inner);
             }
@@ -245,7 +245,7 @@ namespace umbraco.MacroEngines.Library
         public HtmlTagWrapper Wrap(string tag, object inner)
         {
             string innerText = null;
-            if (inner.GetType() != typeof(DynamicNull) && inner != null)
+            if (inner != null && inner.GetType() != typeof(DynamicNull))
             {
                 innerText = string.Format("{0}", inner);
             }
