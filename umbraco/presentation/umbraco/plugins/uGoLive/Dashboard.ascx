19eebba8ccf4fa63668dbf3d308cf1d092fe40e2
mattbrailsford <unknown@kiln.example.com>
2011-11-01 16:55:06 +0000

    Added uGoLive dashboard

---
+++ umbraco/presentation/umbraco/plugins/uGoLive/Dashboard.ascx
@@ -0,0 +1,49 @@
+﻿<%@ Control Language="C#" AutoEventWireup="true" CodeBehind="Dashboard.ascx.cs" Inherits="Our.Umbraco.uGoLive.Web.Umbraco.Plugins.uGoLive.Dashboard" %>
+<%@ Import Namespace="umbraco.IO" %>
+<style type="text/css"> 
+    .uGoLive .propertyItemContent img { vertical-align: middle; margin-right: 5px; } 
+    .uGoLive .propertypane .propertypane { padding: 10px 10px 0px; } 
+    .uGoLive .dashboardWrapper { padding: 5px 5px 5px 5px; overflow: hidden; color: #333; }
+    .uGoLive .dashboardWrapper h2 { margin-top: 0; padding-bottom: 10px; border-bottom: 1px solid #CCC; padding-left: 37px; line-height: 32px; }
+    .uGoLive .dashboardWrapper .dashboardIcon { position: absolute; top: 10px; left: 10px; }
+    .uGoLive .dashboardWrapper h3 { margin-bottom: 10px; }
+    .uGoLive .dashboardWrapper p { line-height: 1.4em; font-size: 1.1em; margin-top: 0; } 
+    .uGoLive #btnRunChecks { color: #fff; background: #f26e20; padding: 10px; border: 0; font-weight: bold; cursor: pointer; }
+    .uGoLive #btnRunChecks.disabled { background: #f9b790; }
+    .uGoLive a.disabled { cursor: default; opacity:0.3; filter:alpha(opacity=30); }
+</style>
+<script type="text/javascript" src="/umbraco/plugins/uGoLive/Dashboard.js"></script>
+<script type="text/javascript">
+
+    (function ($) {
+
+    	$(function () {
+
+    		Our.Umbraco.uGoLive.Dashboard.init({
+    			basePath: '<%= IOHelper.ResolveUrl(IOHelper.returnPath("umbracoBaseDirectory", "~/base")) %>',
+    			umbracoPath: '<%= IOHelper.ResolveUrl(SystemDirectories.Umbraco) %>'
+    		});
+
+    	});
+
+    })(jQuery)  
+
+</script>
+
+<div class="uGoLive">
+    <div class="propertypane">
+        <div>
+            <div class="propertyItem">
+                <div class="dashboardWrapper">
+                    <h2>uGoLive Checklist</h2>
+                    <img src="/umbraco/plugins/uGoLive/icon.png" alt="uGoLive" class="dashboardIcon" />
+                    <p>The uGoLive checklist is a checklist of the most widely accredited best practises when deploying an Umbraco website. uGoLive performs a complete system check against these practises, and highlights any areas that need attention.</p>
+                    <button id="btnRunChecks">Run All Checks</button>
+                    <div class="checks">
+                        <asp:PlaceHolder ID="phChecks" runat="server" />
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ umbraco/presentation/umbraco/plugins/uGoLive/Dashboard.js
@@ -0,0 +1,170 @@
+﻿var Our = Our || {};
+Our.Umbraco = Our.Umbraco || {};
+Our.Umbraco.uGoLive = Our.Umbraco.uGoLive || {};
+
+(function ($) {
+
+    Our.Umbraco.uGoLive.Dashboard = (function() {
+
+        var checks = [];
+        var currentCheckIndex = -1;
+		var basePath = "/base";
+		var umbracoPath = "/umbraco";
+        
+        function performNextCheck() {
+            var checkId = checks[currentCheckIndex];
+            performCheck(checkId, function(data) {
+                if(currentCheckIndex + 1 == checks.length) {
+                    // Re-enable the "Run All Checks" button
+                    var $btn = $("#btnRunChecks");
+                    $btn.text("Re-Run All Checks");
+                    $btn.removeClass("disabled");
+                    $("a.check").removeAttr("disabled");
+                } else {
+                    // Run the next check
+                    currentCheckIndex++;
+                    performNextCheck();
+                }
+            });
+        }
+        
+        function performCheck(checkId, callBack) {
+            $("span.status[data-check-id=" + checkId + "]").html("<img src='" + umbracoPath + "/plugins/uGoLive/throbber.gif' alt='Checking...' /> Checking...");
+            $.getJSON(basePath + '/uGoLive/Check/'+ checkId +'.aspx', function(data) {
+
+                var $checkEl = $("span.status[data-check-id=" + checkId + "]");
+
+                $checkEl.empty();
+                
+                switch(data.Status.Value) {
+                    case "Passed":
+                        $checkEl.append("<img src='" + umbracoPath + "/plugins/uGoLive/tick.png' alt='Passed' />");
+                        $("a.rectify[data-check-id=" + checkId + "][data-check-can-rectify='true']").addClass("disabled");
+                        break;
+                    case "Indeterminate":
+                        $checkEl.append("<img src='" + umbracoPath + "/plugins/uGoLive/error.png' alt='Indeterminate' />");
+                        $("a.rectify[data-check-id=" + checkId + "][data-check-can-rectify='true']").removeClass("disabled");
+                        break;
+                    case "Failed":
+                        $checkEl.append("<img src='" + umbracoPath + "/plugins/uGoLive/cross.png' alt='Failed' />");
+                        $("a.rectify[data-check-id=" + checkId + "][data-check-can-rectify='true']").removeClass("disabled");
+                        break;
+                }
+                
+                if($.trim(data.Message) != "")
+                    $checkEl.append(data.Message);
+
+                if(callBack != undefined)
+                    callBack(data);
+            });
+        }
+        
+        function performRectify(checkId, callBack) {
+            $("span.status[data-check-id=" + checkId + "]").html("<img src='" + umbracoPath + "/plugins/uGoLive/throbber.gif' alt='Rectifying...' /> Rectifying...");
+            $.getJSON(basePath + '/uGoLive/Rectify/'+ checkId +'.aspx', function(data) {
+
+                var $checkEl = $("span.status[data-check-id=" + checkId + "]");
+
+                $checkEl.empty();
+                
+                switch(data.Status.Value) {
+                    case "Success":
+                        $checkEl.append("<img src='" + umbracoPath + "/plugins/uGoLive/tick.png' alt='Passed' />");
+                        $("a.rectify[data-check-id=" + checkId + "][data-check-can-rectify='true']").addClass("disabled");
+                        break;
+                    case "Failed":
+                        $checkEl.append("<img src='" + umbracoPath + "/plugins/uGoLive/cross.png' alt='Failed' />");
+                        $("a.rectify[data-check-id=" + checkId + "][data-check-can-rectify='true']").removeClass("disabled");
+                        break;
+                }
+                
+                if($.trim(data.Message) != "")
+                    $checkEl.append(data.Message);
+
+                if(callBack != undefined)
+                    callBack(data);
+            });
+        }
+        
+        return {
+            
+            init: function (o) {
+				
+				// Set the paths
+				if (typeof(o.umbracoPath) != 'undefined')
+					umbracoPath = o.umbracoPath;
+				if (typeof(o.basePath) != 'undefined')
+					basePath = o.basePath;
+
+                // Parse all checks
+                $("span.status").each(function (idx, el)
+                {
+                    checks.push($(el).attr("data-check-id"));
+                });
+                
+                // Hookup run all check button
+                $("#btnRunChecks").click(function(e) {
+
+                    e.preventDefault();
+
+                    var $this = $(this);
+                    
+                    if(!$this.hasClass("disabled")) {
+                        
+                        // Clear out previous checks
+                        $("span.status").empty();
+
+                        // Update button
+                        $this.text("Running checks...");
+                        $this.addClass("disabled");
+                        $("a.check").attr("disabled", "disabled");
+
+                        // Start checks
+                        currentCheckIndex = 0;
+                        performNextCheck();
+                        
+                    }
+                });
+                
+                // Hookup individual check buttons
+                $("a.check").click(function(e) {
+
+                    e.preventDefault();
+
+                    var $this = $(this);
+                    
+                    if(!$this.hasClass("disabled")) {
+                        
+                        $this.addClass("disabled");
+                        var checkId = $this.attr("data-check-id");
+                        performCheck(checkId, function(data) {
+                            $this.removeClass("disabled");
+                        });
+                        
+                    }
+                });
+                
+                // Hookup individual rectify buttons
+                $("a.rectify").click(function(e) {
+
+                    e.preventDefault();
+
+                    var $this = $(this);
+                    
+                    if(!$this.hasClass("disabled")) {
+                        
+                        $this.addClass("disabled");
+                        var checkId = $this.attr("data-check-id");
+                        performRectify(checkId, function(data) {
+                            //$this.removeAttr("disabled");
+                        });
+                        
+                    }
+                });
+            }
+            
+        };
+        
+    })();
+    
+})(jQuery)
