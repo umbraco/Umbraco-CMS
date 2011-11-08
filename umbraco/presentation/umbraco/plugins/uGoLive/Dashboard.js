a2b3f62aba2d7b3d4d07d1d7166c61849ed6797a
mattbrailsford <unknown@kiln.example.com>
2011-11-08 21:43:23 +0000

    Updated uGoLive to v1.1

--- umbraco/presentation/umbraco/plugins/uGoLive/Dashboard.js
+++ umbraco/presentation/umbraco/plugins/uGoLive/Dashboard.js
@@ -4,134 +4,140 @@ Our.Umbraco.uGoLive = Our.Umbraco.uGoLive || {};
 
 (function ($) {
 
+    // Class representing a check group
+    Our.Umbraco.uGoLive.CheckGroup = function(name, checks, opts) {
+        var me = {
+            name: name,
+            checks: ko.observableArray([])
+        };
+
+        for(var i = 0; i < checks.length; i++) {
+            me.checks.push(new Our.Umbraco.uGoLive.Check(checks[i], opts));
+        }
+        
+        return me;
+    };
+    
+    // Class representing a check
+    Our.Umbraco.uGoLive.Check = function(check, opts) {
+        var me = {
+            id: check.Id,
+            name: check.Name,
+            description: check.Description,
+            canRectify: check.CanRectify,
+            status: ko.observable("Unchecked"),
+            message: ko.observable("Unchecked"),
+            check: function (e, doneCallback) {
+                var _this = this;
+                
+                // Set status / message
+                _this.status("Checking");    
+                _this.message("Checking...");
+                
+                // Perform check
+                $.getJSON(opts.basePath + '/uGoLive/Check/' + this.id + '.aspx', function(data) {
+                    
+                    // Set status / message
+                    _this.status(data.Status.Value);
+                    _this.message(data.Message);
+                    
+                    // Trigger done callback
+                    if(doneCallback != undefined)
+                        doneCallback(data);
+                });
+            },
+            rectify: function (e, doneCallback) {
+                var _this = this;
+                
+                // Set status / message
+                _this.status("Checking");
+                _this.message("Rectifying...");
+                
+                // Perform check
+                $.getJSON(opts.basePath + '/uGoLive/Rectify/' + this.id + '.aspx', function(data) {
+                    
+                    // Set status / message
+                    _this.status(data.Status.Value);
+                    _this.message(data.Message);
+                    
+                    // Trigger done callback
+                    if(doneCallback != undefined)
+                        doneCallback(data);
+                });
+            }
+        };
+
+        return me;
+    };
+
     Our.Umbraco.uGoLive.Dashboard = (function() {
 
-        var checks = [];
-        var currentCheckIndex = -1;
-		var opts = {
-			basePath: "/base",
-			umbracoPath: "/umbraco"
-		};
-        
-        function performNextCheck() {
-			
-			// Get the current check id
-            var checkId = checks[currentCheckIndex];
-			
-			// Trigger check
-            performCheck(checkId, function(data) {
-				
-				// Trigger next check, or finish
-                if(currentCheckIndex + 1 == checks.length) {
-					
-                    // Re-enable the "Run All Checks" button
-                    var $btn = $("#btnRunChecks");
-                    $btn.text("Re-Run All Checks");
-                    $btn.removeClass("disabled");
-					
+        var opts = {
+            checkDefs: [],
+            basePath: "/base",
+            umbracoPath: "/umbraco"
+        };
+
+        var viewModel = {
+            checkGroups: ko.observableArray([]),
+            checkAllText: ko.observable("Run All Checks"),
+            checkAll: function() {
+                var _this = this;
+                
+                // Set button text
+                _this.checkAllText("Running...");
+                
+                // Queue checks
+                ko.utils.arrayForEach(_this.allChecks(), function(check) {
+                    check.status("Queued");
+                    check.message("Queued...");
+                });
+                
+                // Trigger checks
+                _this.checkNext(function () {
+                    
+                    // Reset button text
+                    _this.checkAllText("Re-Run All Checks");
+                });
+            },
+            checkNext: function (doneCallback) {
+                var _this = this;
+                
+                // Check for any queued checks
+                if(_this.queuedChecks().length > 0) {
+                    
+                    // Trigger first queued check
+                    _this.queuedChecks()[0].check(null, function() {
+                        
+                        // Trigger the next check
+                        _this.checkNext(doneCallback);
+                    });
                 } else {
-					
-                    // Run the next check
-                    currentCheckIndex++;
-                    performNextCheck();
-					
+                    
+                    // Call done callback
+                    if(doneCallback != undefined)
+                        doneCallback();
                 }
-            });
-        }
+            }
+        };
         
-        function performCheck(checkId, callBack) {
-			
-			// Get references
-			var $checkEl = $("span.status[data-check-id=" + checkId + "]");
-			var $checkButton = $("a.check[data-check-id=" + checkId + "]");
-			var $rectifyButton = $("a.rectify[data-check-id=" + checkId + "][data-check-can-rectify='true']");
-			
-			// Disable buttons
-			$checkButton.addClass("disabled");
-			$rectifyButton.addClass("disabled");
-			
-			// Display throbber
-            $checkEl.html("<img src='" + opts.umbracoPath + "/plugins/uGoLive/throbber.gif' alt='Checking...' /> Checking...");
-            
-			// Perform check
-			$.getJSON(opts.basePath + '/uGoLive/Check/'+ checkId +'.aspx', function(data) {
-
-				// Remove throbber
-                $checkEl.empty();
-                
-				// Display icon & enable / disable rectify button
-                switch(data.Status.Value) {
-                    case "Passed":
-                        $checkEl.append("<img src='" + opts.umbracoPath + "/plugins/uGoLive/tick.png' alt='Passed' />");
-                        $rectifyButton.addClass("disabled");
-                        break;
-                    case "Indeterminate":
-                        $checkEl.append("<img src='" + opts.umbracoPath + "/plugins/uGoLive/error.png' alt='Indeterminate' />");
-                        $rectifyButton.removeClass("disabled");
-                        break;
-                    case "Failed":
-                        $checkEl.append("<img src='" + opts.umbracoPath + "/plugins/uGoLive/cross.png' alt='Failed' />");
-                        $rectifyButton.removeClass("disabled");
-                        break;
-                }
-                
-				// Display message
-                if($.trim(data.Message) != "")
-                    $checkEl.append(data.Message);
-
-				// Re-enable check button
-				$checkButton.removeClass("disabled");
-
-				// Execute callback
-                if(callBack != undefined)
-                    callBack(data);
+        // Helper list of all checks
+        viewModel.allChecks = ko.dependentObservable(function() {
+            var all = [];
+            ko.utils.arrayForEach(this.checkGroups(), function(group) {
+                ko.utils.arrayForEach(group.checks(), function(check) {
+                    all.push(check);
+                });
             });
-        }
-        
-        function performRectify(checkId, callBack) {
-			
-			// Get references
-			var $checkEl = $("span.status[data-check-id=" + checkId + "]");
-			var $checkButton = $("a.check[data-check-id=" + checkId + "]");
-			var $rectifyButton = $("a.rectify[data-check-id=" + checkId + "][data-check-can-rectify='true']");
+            return all;
+        }, viewModel);
 
-			// Disable buttons
-			$checkButton.addClass("disabled");
-			$rectifyButton.addClass("disabled");
-			
-			// Display throbber
-            $checkEl.html("<img src='" + opts.umbracoPath + "/plugins/uGoLive/throbber.gif' alt='Rectifying...' /> Rectifying...");
-            
-			// Perform rectify
-			$.getJSON(opts.basePath + '/uGoLive/Rectify/'+ checkId +'.aspx', function(data) {
-
-				// Remove throbber
-                $checkEl.empty();
-                
-				// Display icon & enable / disable rectify button
-                switch(data.Status.Value) {
-                    case "Success":
-                        $checkEl.append("<img src='" + opts.umbracoPath + "/plugins/uGoLive/tick.png' alt='Passed' />");
-                        $rectifyButton.addClass("disabled");
-                        break;
-                    case "Failed":
-                        $checkEl.append("<img src='" + opts.umbracoPath + "/plugins/uGoLive/cross.png' alt='Failed' />");
-                        $rectifyButton.removeClass("disabled");
-                        break;
-                }
-                
-				// Display message
-                if($.trim(data.Message) != "")
-                    $checkEl.append(data.Message);
-
-				// Re-enable check button
-				$checkButton.removeClass("disabled");
-
-				// Execute callback
-                if(callBack != undefined)
-                    callBack(data);
+        // Helper list of queued checks
+        viewModel.queuedChecks = ko.dependentObservable(function() {
+            return ko.utils.arrayFilter(this.allChecks(), function(check) {
+                return check.status() == "Queued";
             });
-        }
+        }, viewModel);
         
         return {
             
@@ -139,62 +145,17 @@ Our.Umbraco.uGoLive = Our.Umbraco.uGoLive || {};
 				
 				// Merge options
 				opts = $.extend(opts, o);
-
+                
                 // Parse all checks
-                $("span.status").each(function (idx, el) {
-                    checks.push($(el).attr("data-check-id"));
-                });
-                
-                // Hookup run all check button
-                $("#btnRunChecks").click(function(e) {
-
-                    e.preventDefault();
-
-                    var $this = $(this);
-                    
-                    if(!$this.hasClass("disabled")) {
-                        
-                        // Clear out previous checks
-                        $("span.status").empty();
-						
-						// Disable check/rectify buttons
-                        $("a.check").addClass("disabled");
-                        $("a.rectify").addClass("disabled");
-
-                        // Update run checks button
-                        $this.text("Running checks...");
-                        $this.addClass("disabled");
-
-                        // Start checks
-                        currentCheckIndex = 0;
-                        performNextCheck();
-                        
+                for(var i = 0; i < opts.checkDefs.length; i++) {
+                    var groupChecks = opts.checkDefs[i];
+                    if(groupChecks.length > 0) {
+                        viewModel.checkGroups.push(new Our.Umbraco.uGoLive.CheckGroup(groupChecks[0].Group, groupChecks,  opts));
                     }
-                });
-                
-                // Hookup individual check buttons
-                $("a.check").click(function(e) {
+                }
 
-                    e.preventDefault();
-
-                    var $this = $(this);
-                    
-                    if(!$this.hasClass("disabled")) {
-                        performCheck($this.attr("data-check-id"));
-                    }
-                });
-                
-                // Hookup individual rectify buttons
-                $("a.rectify").click(function(e) {
-
-                    e.preventDefault();
-
-                    var $this = $(this);
-                    
-                    if(!$this.hasClass("disabled")) {
-                        performRectify($this.attr("data-check-id")); 
-                    }
-                });
+                // Bind view model
+                ko.applyBindings(viewModel, $("#uGoLive").get(0));
             }
             
         };
