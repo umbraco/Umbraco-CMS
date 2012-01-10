e09c531404318fe32cae8197d99a2b40c1da2771
agrath <unknown@kiln.example.com>
2012-01-09 20:56:38 -1300

    Add two more guids to IntegerDataTypeModel identified from the Cultiv Razor Examples project

--- umbraco.MacroEngines.Juno/RazorDataTypeModels/IntegerDataTypeModel.cs
+++ umbraco.MacroEngines.Juno/RazorDataTypeModels/IntegerDataTypeModel.cs
@@ -6,6 +6,9 @@ using System.Text;
 namespace umbraco.MacroEngines.RazorDataTypeModels
 {
     [RazorDataTypeModel("1413afcb-d19a-4173-8e9a-68288d2a73b8", 90)]
+    [RazorDataTypeModel("f231cd8a-e447-424a-94a4-bc73b11736bb", 90)]
+    [RazorDataTypeModel("a52c7c1c-c330-476e-8605-d63d3b84b6a6", 90)]
+
     public class IntegerDataTypeModel : IRazorDataTypeModel
     {
         public bool Init(int CurrentNodeId, string PropertyData, out object instance)
