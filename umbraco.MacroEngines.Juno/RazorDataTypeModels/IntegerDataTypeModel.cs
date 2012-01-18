6a3e1819aac6cf8c6693c3fe4194180ad62b59c5
agrath <unknown@kiln.example.com>
2012-01-17 11:43:16 -1300

    Added guid for enhanced content picker for integer upcast

--- umbraco.MacroEngines.Juno/RazorDataTypeModels/IntegerDataTypeModel.cs
+++ umbraco.MacroEngines.Juno/RazorDataTypeModels/IntegerDataTypeModel.cs
@@ -8,8 +8,8 @@ namespace umbraco.MacroEngines.RazorDataTypeModels
     [RazorDataTypeModel("1413afcb-d19a-4173-8e9a-68288d2a73b8", 90)]
     [RazorDataTypeModel("f231cd8a-e447-424a-94a4-bc73b11736bb", 90)]
     [RazorDataTypeModel("a52c7c1c-c330-476e-8605-d63d3b84b6a6", 90)]
-    //[RazorDataTypeModel("22c6f52c-5d41-45b8-845e-c16354cfdd7b", 90)]
     [RazorDataTypeModel("39f533e4-0551-4505-a64b-e0425c5ce775", 90)]
+    [RazorDataTypeModel("f6524852-2fb0-11e0-a9fa-6f1cdfd72085", 90)] //Extended Content Picker http://our.umbraco.org/projects/backoffice-extensions/extended-content-picker
     public class IntegerDataTypeModel : IRazorDataTypeModel
     {
         public bool Init(int CurrentNodeId, string PropertyData, out object instance)
