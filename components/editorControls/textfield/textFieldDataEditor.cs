686e0116d8f7b72e53457873a67342e1a56ed4fb
agrath <unknown@kiln.example.com>
2012-01-17 14:26:48 -1300

    Allow CData tags to be embedded in a textboxmultiple controls contents

--- components/editorControls/textfield/textFieldDataEditor.cs
+++ components/editorControls/textfield/textFieldDataEditor.cs
@@ -6,41 +6,46 @@ using System.Collections;
 
 namespace umbraco.editorControls.textfield
 {
-	public class TextFieldEditor : TextBox, interfaces.IDataEditor
-	{
-		private interfaces.IData _data;
+    public class TextFieldEditor : TextBox, interfaces.IDataEditor
+    {
+        private interfaces.IData _data;
 
 
-		public TextFieldEditor(interfaces.IData Data) {
-			_data = Data;
-		}
+        public TextFieldEditor(interfaces.IData Data)
+        {
+            _data = Data;
+        }
 
-		public virtual bool TreatAsRichTextEditor 
-		{
-			get {return false;}
-		}
+        public virtual bool TreatAsRichTextEditor
+        {
+            get { return false; }
+        }
 
-		public bool ShowLabel 
-		{
-			get {return true;}
-		}
+        public bool ShowLabel
+        {
+            get { return true; }
+        }
 
-		public Control Editor {get{return this;}}
+        public Control Editor { get { return this; } }
 
-		public void Save() 
-		{
-			_data.Value = this.Text;
-		}
+        public void Save()
+        {
+            //GE 2012-01-18 
+            //allow the textboxmultiple datatype to contain CDATA tags
+            _data.Value = this.Text.Replace("<![CDATA[", "<!--CDATAOPENTAG-->").Replace("]]>", "<!--CDATACLOSETAG-->");
+        }
 
-		protected override void OnInit(EventArgs e)
-		{
-			if (this.CssClass == "")
-				this.CssClass = "umbEditorTextField";
-	
-			if (_data != null && _data.Value != null) 
-				Text = _data.Value.ToString();
-	
-			base.OnInit(e);
-		}
-	}
+        protected override void OnInit(EventArgs e)
+        {
+            if (this.CssClass == "")
+                this.CssClass = "umbEditorTextField";
+
+            //GE 2012-01-18 
+            //allow the textboxmultiple datatype to contain CDATA tags
+            if (_data != null && _data.Value != null)
+                Text = _data.Value.ToString().Replace("<!--CDATAOPENTAG-->", "<![CDATA[").Replace("<!--CDATACLOSETAG-->", "]]>");
+
+            base.OnInit(e);
+        }
+    }
 }
--- umbraco/businesslogic/xmlHelper.cs
+++ umbraco/businesslogic/xmlHelper.cs
@@ -4,39 +4,40 @@ using umbraco.IO;
 
 namespace umbraco
 {
-	/// <summary>
-	/// The xmlHelper class contains general helper methods for working with xml in umbraco.
-	/// </summary>
-	public class xmlHelper
-	{
+    /// <summary>
+    /// The xmlHelper class contains general helper methods for working with xml in umbraco.
+    /// </summary>
+    public class xmlHelper
+    {
         /// <summary>
         /// Imports a XML node from text.
         /// </summary>
         /// <param name="text">The text.</param>
         /// <param name="xmlDoc">The XML doc.</param>
         /// <returns></returns>
-		public static XmlNode ImportXmlNodeFromText (string text, ref XmlDocument xmlDoc) 
-		{
-			xmlDoc.LoadXml(text);
-			return xmlDoc.FirstChild;
-		}
+        public static XmlNode ImportXmlNodeFromText(string text, ref XmlDocument xmlDoc)
+        {
+            xmlDoc.LoadXml(text);
+            return xmlDoc.FirstChild;
+        }
 
         /// <summary>
         /// Opens a file as a XmlDocument.
         /// </summary>
         /// <param name="filePath">The relative file path. ei. /config/umbraco.config</param>
         /// <returns>Returns a XmlDocument class</returns>
-        public static XmlDocument OpenAsXmlDocument(string filePath) {
+        public static XmlDocument OpenAsXmlDocument(string filePath)
+        {
 
             XmlTextReader reader = new XmlTextReader(IOHelper.MapPath(filePath));
-            
+
             reader.WhitespaceHandling = WhitespaceHandling.All;
             XmlDocument xmlDoc = new XmlDocument();
             //Load the file into the XmlDocument
             xmlDoc.Load(reader);
             //Close off the connection to the file.
             reader.Close();
-            
+
             return xmlDoc;
         }
 
@@ -47,12 +48,12 @@ namespace umbraco
         /// <param name="Name">The name of the attribute.</param>
         /// <param name="Value">The value of the attribute.</param>
         /// <returns>a XmlAttribute</returns>
-		public static XmlAttribute addAttribute(XmlDocument Xd, string Name, string Value) 
-		{
-			XmlAttribute temp = Xd.CreateAttribute(Name);
-			temp.Value = Value;
-			return temp;
-		}
+        public static XmlAttribute addAttribute(XmlDocument Xd, string Name, string Value)
+        {
+            XmlAttribute temp = Xd.CreateAttribute(Name);
+            temp.Value = Value;
+            return temp;
+        }
 
         /// <summary>
         /// Creates a text XmlNode with the specified name and value
@@ -61,12 +62,12 @@ namespace umbraco
         /// <param name="Name">The node name.</param>
         /// <param name="Value">The node value.</param>
         /// <returns>a XmlNode</returns>
-		public static XmlNode addTextNode(XmlDocument Xd, string Name, string Value) 
-		{
-			XmlNode temp = Xd.CreateNode(XmlNodeType.Element, Name, "");
-			temp.AppendChild(Xd.CreateTextNode(Value));
-			return temp;
-		}
+        public static XmlNode addTextNode(XmlDocument Xd, string Name, string Value)
+        {
+            XmlNode temp = Xd.CreateNode(XmlNodeType.Element, Name, "");
+            temp.AppendChild(Xd.CreateTextNode(Value));
+            return temp;
+        }
 
         /// <summary>
         /// Creates a cdata XmlNode with the specified name and value
@@ -75,25 +76,32 @@ namespace umbraco
         /// <param name="Name">The node name.</param>
         /// <param name="Value">The node value.</param>
         /// <returns>A XmlNode</returns>
-		public static XmlNode addCDataNode(XmlDocument Xd, string Name, string Value) 
-		{
-			XmlNode temp = Xd.CreateNode(XmlNodeType.Element, Name, "");
-			temp.AppendChild(Xd.CreateCDataSection(Value));
-			return temp;
-		}
+        public static XmlNode addCDataNode(XmlDocument Xd, string Name, string Value)
+        {
+            XmlNode temp = Xd.CreateNode(XmlNodeType.Element, Name, "");
+            temp.AppendChild(Xd.CreateCDataSection(Value));
+            return temp;
+        }
 
         /// <summary>
         /// Gets the value of a XmlNode
         /// </summary>
         /// <param name="n">The XmlNode.</param>
         /// <returns>the value as a string</returns>
-		public static string GetNodeValue(XmlNode n) 
-		{
-			if (n == null || n.FirstChild == null)
-				return string.Empty;
-            if(n.FirstChild.Value != null)
-			    return n.FirstChild.Value;
-            return n.InnerXml;
-		}
-	}
+        public static string GetNodeValue(XmlNode n)
+        {
+            string value = string.Empty;
+            if (n == null || n.FirstChild == null)
+                return value;
+            if (n.FirstChild.Value != null)
+            {
+                value = n.FirstChild.Value;
+            }
+            else
+            {
+                value = n.InnerXml;
+            }
+            return value.Replace("<!--CDATAOPENTAG-->", "<![CDATA[").Replace("<!--CDATACLOSETAG-->", "]]>");
+        }
+    }
 }
