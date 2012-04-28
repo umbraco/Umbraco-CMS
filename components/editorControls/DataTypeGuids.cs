0db8dc8da51c5bae8e31e0ca07e1e95ee48264dc
leekelleher <unknown@kiln.example.com>
2012-04-28 13:53:36 -0100

    uComponents: Class to define the data-type GUIDs (for 3rd party reference)

---
+++ components/editorControls/DataTypeGuids.cs
@@ -0,0 +1,33 @@
+﻿namespace umbraco.editorControls
+{
+	/// <summary>
+	/// Defines the Guids for all the DataTypes; e.g. quick access to 3rd-party developers.
+	/// </summary>
+	public static class DataTypeGuids
+	{
+		/// <summary>
+		/// Guid for the MultiNodeTreePicker data-type.
+		/// </summary>
+		public const string MultiNodeTreePickerId = "7E062C13-7C41-4AD9-B389-41D88AEEF87C";
+
+		/// <summary>
+		/// Guid for the MultipleTextstring data-type.
+		/// </summary>
+		public const string MultipleTextstringId = "5359AD0B-06CC-4182-92BD-0A9117448D3F";
+
+		/// <summary>
+		/// Guid for the Slider data-type.
+		/// </summary>
+		public const string SliderId = "29E790E6-26B3-438A-B21F-908663A0B19E";
+
+		/// <summary>
+		/// Guid for the XPathCheckBoxList data-type.
+		/// </summary>
+		public const string XPathCheckBoxListId = "34451D92-D270-49BA-8C7F-EE55BFEEE1CB";
+
+		/// <summary>
+		/// Guid for the XPathDropDownList data-type.
+		/// </summary>
+		public const string XPathDropDownListId = "173A96AE-00ED-4A7C-9F76-4B53D4A0A1B9";
+	}
+}
