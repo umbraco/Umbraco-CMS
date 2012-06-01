e8d649b72a7b5e023abd6f3ca0f8a83db6071cf2
leekelleher <unknown@kiln.example.com>
2012-06-01 09:59:45 -0100

    Client Dependency Framework updated to 1.5 (beta). Shandem fixed backwards compatibility in CDF 1.5. Sync'd the assembly and config. Updated 'CanvasClientDependencyProvider' class (from CDF 1.4 source).

--- umbraco/presentation/umbraco/LiveEditing/CanvasClientDependencyProvider.cs
+++ umbraco/presentation/umbraco/LiveEditing/CanvasClientDependencyProvider.cs
@@ -2,79 +2,73 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using ClientDependency.Core;
 using ClientDependency.Core.FileRegistration.Providers;
 using umbraco.presentation;
-using ClientDependency.Core;
 
 namespace umbraco.presentation.LiveEditing
 {
     public class CanvasClientDependencyProvider : LazyLoadProvider
     {
-        public CanvasClientDependencyProvider()
-            : base()
-        {}
-
         public new const string DefaultName = "CanvasProvider";
 
-        public override void Initialize(string name, System.Collections.Specialized.NameValueCollection config)
+        /// <summary>
+        /// override to never render out the dependency handler address (no compression, combination, etc...)
+        /// </summary>
+        /// <param name="jsDependencies"></param>
+        /// <param name="http"></param>
+        /// <param name="htmlAttributes"></param>
+        /// <returns></returns>
+        protected override string RenderJsDependencies(IEnumerable<IClientDependencyFile> jsDependencies, System.Web.HttpContextBase http, IDictionary<string, string> htmlAttributes)
         {
-            base.Initialize(name, config);            
+            if (!jsDependencies.Any())
+                return string.Empty;
+
+            var sb = new StringBuilder();
+
+            foreach (var dependency in jsDependencies)
+            {
+                sb.Append(RenderSingleJsFile(string.Format("'{0}','{1}'", dependency.FilePath, string.Empty), new Dictionary<string, string>()));
+            }
+
+            return sb.ToString();
         }
 
         /// <summary>
         /// override to never render out the dependency handler address (no compression, combination, etc...)
         /// </summary>
         /// <param name="cssDependencies"></param>
+        /// <param name="http"></param>
+        /// <param name="htmlAttributes"></param>
         /// <returns></returns>
-        protected override string RenderCssDependencies(List<ClientDependency.Core.IClientDependencyFile> cssDependencies)
+        protected override string RenderCssDependencies(IEnumerable<IClientDependencyFile> cssDependencies, System.Web.HttpContextBase http, IDictionary<string, string> htmlAttributes)
         {
-            if (cssDependencies.Count == 0)
+            if (!cssDependencies.Any())
                 return string.Empty;
 
-            StringBuilder sb = new StringBuilder();
+            var sb = new StringBuilder();
 
-            foreach (IClientDependencyFile dependency in cssDependencies)
+            foreach (var dependency in cssDependencies)
             {
-                sb.Append(RenderSingleCssFile(dependency.FilePath));
+                sb.Append(RenderSingleCssFile(dependency.FilePath, new Dictionary<string, string>()));
             }
 
             return sb.ToString();
         }
 
-        /// <summary>
-        /// override to never render out the dependency handler address (no compression, combination, etc...)
-        /// </summary>
-        /// <param name="jsDependencies"></param>
-        /// <returns></returns>
-        protected override string RenderJsDependencies(List<ClientDependency.Core.IClientDependencyFile> jsDependencies)
-        {
-            if (jsDependencies.Count == 0)
-                return string.Empty;
-
-            StringBuilder sb = new StringBuilder();
-
-            foreach (IClientDependencyFile dependency in jsDependencies)
-            {
-                sb.Append(RenderSingleJsFile(string.Format("'{0}','{1}'", dependency.FilePath, string.Empty)));
-            }
-
-            return sb.ToString();
-        }
-
-        protected override string RenderSingleCssFile(string css)
+        protected override string RenderSingleCssFile(string css, IDictionary<string, string> htmlAttributes)
         {
             if (UmbracoContext.Current.LiveEditingContext.Enabled)
-                return base.RenderSingleCssFile(css);
+                return base.RenderSingleCssFile(css, new Dictionary<string, string>());
             return string.Empty;
         }
 
-        protected override string RenderSingleJsFile(string js)
+        protected override string RenderSingleJsFile(string js, IDictionary<string, string> htmlAttributes)
         {
             if (UmbracoContext.Current.LiveEditingContext.Enabled)
-                return base.RenderSingleJsFile(js);
+                return base.RenderSingleJsFile(js, new Dictionary<string, string>());
             return string.Empty;
         }
-
 
     }
 }
