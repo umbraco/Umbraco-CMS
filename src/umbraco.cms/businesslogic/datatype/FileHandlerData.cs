9a95c08271e83bea36df8304563d4982beed8aa5
hartvig <unknown@kiln.example.com>
2012-07-03 20:58:50 -0200

    Adds better support for simpler file attachment to document/media properties, by just assigning an HttpPostedFile as the value of an upload property (then it'll handle storing the file, generating thumbnails, etc): myImage.getProperty("mainImage").Value = Request.Files[0]; Adds helper class "UmbracoFile" for easier file upload / file manipulation in general (to be provider based so we can support manipulation via ImageGen or ImageResizing.NET as well as support for abstracting file storage via providers (through an IFile but currently in an implementation that needs refactoring (used in a wrong way atm)). Will fix that part tomorrow!

---
+++ src/umbraco.cms/businesslogic/datatype/FileHandlerData.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.IO;
+using System.Web;
+using umbraco.cms.businesslogic.Files;
+using umbraco.IO;
+
+namespace umbraco.cms.businesslogic.datatype
+{
+    public class FileHandlerData : DefaultData
+    {
+        private readonly string _thumbnailSizes;
+
+        public FileHandlerData(BaseDataType DataType, string thumbnailSizes)
+            : base(DataType)
+        {
+            _thumbnailSizes = thumbnailSizes;
+        }
+
+        public override object Value
+        {
+            get { return base.Value; }
+            set
+            {
+                UmbracoFile um = null;
+                if (value is HttpPostedFile)
+                {
+                    // handle upload
+                    var file = value as HttpPostedFile;
+                    if (file.FileName != String.Empty)
+                    {
+                        var fileName = UmbracoSettings.UploadAllowDirectories ?
+                            Path.Combine(PropertyId.ToString(), file.FileName) :
+                            PropertyId + "-" + file.FileName;
+
+                        fileName = Path.Combine(SystemDirectories.Media, fileName);
+                        um = UmbracoFile.Save(file, fileName);
+
+                        if (um.SupportsResizing)
+                        {
+                            // make default thumbnail
+                            um.Resize(100, "thumb");
+
+                            // additional thumbnails configured as prevalues on the DataType
+                            if (_thumbnailSizes != "")
+                            {
+                                char sep = (!_thumbnailSizes.Contains("") && _thumbnailSizes.Contains(",")) ? ',' : ';';
+
+                                foreach (string thumb in _thumbnailSizes.Split(sep))
+                                {
+                                    int thumbSize;
+                                    if (thumb != "" && int.TryParse(thumb, out thumbSize))
+                                    {
+                                        um.Resize(thumbSize, string.Format("thumb_{0}", thumbSize));
+                                    }
+                                }
+                            }
+                        }
+
+                        base.Value = um.LocalName;
+                    }
+                    else
+                    {
+                        // if no file is uploaded, we reset the value
+                        base.Value = String.Empty;
+                    }
+                }
+                else
+                {
+                    base.Value = value;
+                }
+            }
+        }
+    }
+}
