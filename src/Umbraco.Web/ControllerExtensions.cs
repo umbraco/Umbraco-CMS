23b289910124dbfeb19276cc9b494e5ce132cb37
shannon@ShandemVaio <unknown@kiln.example.com>
2012-07-17 03:51:34 +0600

    Imports many of the useful extension methods from v5 that are also used in umbraMVCo to begin the MVC integration. Imported the unit tests associated with each of these imported classes.

---
+++ src/Umbraco.Web/ControllerExtensions.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Umbraco.Web
+{
+    public static class ControllerExtensions
+    {
+        /// <summary>
+        /// Return the controller name from the controller type
+        /// </summary>
+        /// <param name="controllerType"></param>
+        /// <returns></returns>
+        public static string GetControllerName(Type controllerType)
+        {
+            return controllerType.Name.Substring(0, controllerType.Name.LastIndexOf("Controller"));
+        }
+
+        /// <summary>
+        /// Return the controller name from the controller type
+        /// </summary>
+        /// <typeparam name="T"></typeparam>
+        /// <returns></returns>
+        /// <remarks></remarks>
+        public static string GetControllerName<T>()
+        {
+            return GetControllerName(typeof(T));
+        }
+    }
+}
