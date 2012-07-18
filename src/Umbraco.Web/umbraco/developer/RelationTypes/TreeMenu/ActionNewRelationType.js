7ca29b87f3cfab58427267a233a747a6403f2d5f
shannon@ShandemVaio <unknown@kiln.example.com>
2012-07-19 00:52:58 +0600

    Removed all client side files as they now exist in Umbraco.Web.UI

--- src/Umbraco.Web/umbraco/developer/RelationTypes/TreeMenu/ActionNewRelationType.js
+++
@@ -1,3 +0,0 @@
-﻿function actionNewRelationType() {
-    UmbClientMgr.openModalWindow('developer/RelationTypes/NewRelationType.aspx', 'Create New RelationType', true, 400, 300, 0, 0);
-}
--- src/Umbraco.Web/umbraco_client/Application/JQuery/VerticalAlign.js
+++
@@ -1,8 +0,0 @@
-﻿(function($) {
-    $.fn.VerticalAlign = function(opts) {
-        return this.each(function() {
-            var top = (($(this).parent().height() - $(this).height()) / 2);
-            $(this).css('margin-top', top);
-        });
-    };
-})(jQuery);
