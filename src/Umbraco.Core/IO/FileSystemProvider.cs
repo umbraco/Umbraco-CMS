524244d1ec118b76c66b579633c012c3befd7a2b
Matt@MBP13-PC <unknown@kiln.example.com>
2012-08-13 13:26:06 -0100

    Added FileSystemProviders config section Added FileSystemProviderManager class for fetching a file system provider by alias (this should probably be moved into the Umbraco context in 4.10)

---
+++ src/Umbraco.Core/IO/FileSystemProvider.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Umbraco.Core.IO
+{
+    internal class FileSystemProvider
+    {
+        public const string Media = "media";
+    }
+}
