fbd20fd4b19db9dbd200734fe7348b07549c41eb
Shannon Deminick <sdeminick@gmail.com>
2012-08-24 22:41:00 +0700

    Removes 'OLD' root folder

--- OLD/umbraco.Legacy/umbraco_client/tinymce/plugins/advhr/langs/el.js
+++
@@ -1,8 +0,0 @@
-// Initial translation by Dimitris Giannitsaros / MediSign SA
-
-tinyMCE.addToLang('',{
-insert_advhr_desc : 'Ορίζόντιος κανόνας',
-insert_advhr_width : 'Πλάτος',
-insert_advhr_size : 'Ύψος',
-insert_advhr_noshade : 'Χωρίς σκια'
-});
