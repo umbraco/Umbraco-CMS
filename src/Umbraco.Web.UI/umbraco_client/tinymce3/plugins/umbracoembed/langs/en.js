f32f559ab78a57f2b7ad596f0b99bd2520672f52
tg@umbraco.com <unknown@kiln.example.com>
2012-08-31 09:37:35 -0200

    Adds tinyMCE umbraco embed plugin

---
+++ src/Umbraco.Web.UI/umbraco_client/tinymce3/plugins/umbracoembed/langs/en.js
@@ -0,0 +1,10 @@
+tinyMCE.addI18n('en.embed_dlg', {
+    title: 'Insert rich media',
+    general: 'General',
+    url: 'Url:',
+    size: 'Size:',
+    constrain_proportions: 'Constrain',
+    preview: 'Preview',
+    source: 'Source'
+
+});
