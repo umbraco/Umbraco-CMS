20899907691a3b670b88acfd07895e676f848ebe
sebastiaan <unknown@kiln.example.com>
2012-09-04 12:59:05 -0200

    Changed folderBrowser datatype to use the new MediaDashboardFolderBrowser control

--- src/umbraco.editorControls/folderbrowser/DataTypeFolderbrowser.cs
+++ src/umbraco.editorControls/folderbrowser/DataTypeFolderbrowser.cs
@@ -2,62 +2,35 @@ using System;
 
 namespace umbraco.editorControls.folderbrowser
 {
-	/// <summary>
-	/// Summary description for DataTypeFolderbrowser.
-	/// </summary>
-	/// <summary>
-	/// Summary description for DataTypeUploadField.
-	/// </summary>
-	public class DataTypeFolderBrowser: cms.businesslogic.datatype.BaseDataType,interfaces.IDataType
-	{
-		private interfaces.IDataEditor _Editor;
-		private cms.businesslogic.datatype.DefaultData _baseData;
-		private interfaces.IDataPrevalue _prevalueeditor;
+    /// <summary>
+    /// Summary description for DataTypeFolderbrowser.
+    /// </summary>
+    /// <summary>
+    /// Summary description for DataTypeUploadField.
+    /// </summary>
+    public class DataTypeFolderBrowser : cms.businesslogic.datatype.BaseDataType, interfaces.IDataType
+    {
+        private interfaces.IDataEditor _editor;
+        private cms.businesslogic.datatype.DefaultData _baseData;
+        private interfaces.IDataPrevalue _prevalueeditor;
 
-		public override interfaces.IDataEditor DataEditor 
-		{
-			get
-			{
-				if (_Editor == null)
-                    _Editor = new folderBrowser((umbraco.cms.businesslogic.datatype.DefaultData)Data);
-				return _Editor;
-			}
-		}
+        public override interfaces.IDataEditor DataEditor
+        {
+            get { return _editor ?? (_editor = new folderBrowser()); }
+        }
 
-		public override interfaces.IData Data 
-		{
-			get 
-			{
-				if (_baseData == null)
-					_baseData = new cms.businesslogic.datatype.DefaultData(this);
-				return _baseData;
-			}
-		}
-		
-		public override Guid Id
-		{
-			get
-			{
-				return new Guid ("CCCD4AE9-F399-4ED2-8038-2E88D19E810C");
-			}
-		}
+        public override interfaces.IData Data
+        {
+            get { return _baseData ?? (_baseData = new cms.businesslogic.datatype.DefaultData(this)); }
+        }
 
-		public override string DataTypeName
-		{
-			get
-			{
-				return "Folder browser";
-			}
-		}
+        public override Guid Id { get { return new Guid("CCCD4AE9-F399-4ED2-8038-2E88D19E810C"); } }
 
-		public override interfaces.IDataPrevalue PrevalueEditor 
-		{
-			get 
-			{
-				if (_prevalueeditor == null)
-					_prevalueeditor = new DefaultPrevalueEditor(this,false);
-				return _prevalueeditor;
-			}
-		}
-	}
+        public override string DataTypeName { get { return "Folder browser"; } }
+
+        public override interfaces.IDataPrevalue PrevalueEditor
+        {
+            get { return _prevalueeditor ?? (_prevalueeditor = new DefaultPrevalueEditor(this, false)); }
+        }
+    }
 }
