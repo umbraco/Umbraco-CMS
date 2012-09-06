03865ba97823cf9516e0f9e17076454bbe3b33ec
sebastiaan <unknown@kiln.example.com>
2012-09-06 08:10:48 -0200

    Re-included ItemEditing*.js for canvas, was removed after a merge with 4.10

---
+++ src/Umbraco.Web.UI/umbraco/LiveEditing/Modules/ItemEditing/ItemEditingInvoke.js
@@ -0,0 +1,3 @@
+﻿//this is simply used for live editing in order to invoke a method from a previously lazy loaded script;
+//alert("ItemEditingInvoke: " + initializeGlobalItemEditing);
+initializeGlobalItemEditing();
