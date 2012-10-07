248af2eaa21524bc5ea823072af5bd731a7cb768
Michiel van Oosterhout <michielvoo@gmail.com>
2012-10-07 20:54:34 -0200

    Correct code that validates the user name when the session times out by serializing user name and password.
    Adds a minified 4KB JSON library to umbraco.aspx to support older browsers, namely
    https://github.com/douglascrockford/JSON-js (public domain)

--- src/Umbraco.Web.UI/umbraco/Umbraco.aspx.designer.cs
+++ src/Umbraco.Web.UI/umbraco/Umbraco.aspx.designer.cs
@@ -11,5 +11,14 @@ namespace Umbraco.Web.UI.Umbraco {
     
     
     public partial class Umbraco {
+        
+        /// <summary>
+        /// JSON2 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::ClientDependency.Core.Controls.JsInclude JSON2;
     }
 }
