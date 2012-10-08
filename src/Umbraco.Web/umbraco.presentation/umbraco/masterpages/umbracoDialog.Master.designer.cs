76a521dd8bcdfe2e9a47a9a6240e3c46da3e2fc4
Shannon Deminick <sdeminick@gmail.com>
2012-10-08 23:36:25 +0500

    fixes script manager reference

--- src/Umbraco.Web/umbraco.presentation/umbraco/masterpages/umbracoDialog.Master.designer.cs
+++ src/Umbraco.Web/umbraco.presentation/umbraco/masterpages/umbracoDialog.Master.designer.cs
@@ -101,15 +101,15 @@ namespace umbraco.presentation.masterpages {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.HtmlControls.HtmlForm form1;
-        
-        /// <summary>
-        /// ScriptManager1 control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.ScriptManager ScriptManager1;
+
+		/// <summary>
+		/// ScriptManager1 control.
+		/// </summary>
+		/// <remarks>
+		/// Auto-generated field.
+		/// To modify move field declaration from designer file to code-behind file.
+		/// </remarks>
+		protected global::System.Web.UI.ScriptManager ScriptManager1;
         
         /// <summary>
         /// body control.
