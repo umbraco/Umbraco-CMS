747675816d815e86b7482b5a0acb9110eeeec590
Shannon Deminick <sdeminick@gmail.com>
2012-10-08 23:57:27 +0500

    Changed editMacro over to use the correct codebehind structure.

---
+++ src/Umbraco.Web.UI/umbraco/developer/Macros/EditMacro.aspx.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Umbraco.Web.UI.Umbraco.Developer.Macros
+{
+	public partial class EditMacro : global::umbraco.cms.presentation.developer.editMacro
+	{
+	}
+}
