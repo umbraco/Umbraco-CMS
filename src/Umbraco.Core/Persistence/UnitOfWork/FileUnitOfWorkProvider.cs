e42865ef9d31dc1234df9b2c024972c834d247c5
Morten@Thinkpad-X220 <unknown@kiln.example.com>
2012-10-08 10:16:59 -0200

    Adds Unit of Work for working with files U4-975

--- src/Umbraco.Core/Persistence/UnitOfWork/FileUnitOfWorkProvider.cs
+++ src/Umbraco.Core/Persistence/UnitOfWork/FileUnitOfWorkProvider.cs
@@ -1,15 +1,15 @@
 ﻿namespace Umbraco.Core.Persistence.UnitOfWork
 {
     /// <summary>
-    /// Represents a Unit of Work Provider for creating a <see cref="PetaPocoUnitOfWork"/>
+    /// Represents a Unit of Work Provider for creating a <see cref="FileUnitOfWork"/>
     /// </summary>
-    internal class PetaPocoUnitOfWorkProvider : IUnitOfWorkProvider
+    internal class FileUnitOfWorkProvider : IUnitOfWorkProvider
     {
         #region Implementation of IUnitOfWorkProvider
 
         public IUnitOfWork GetUnitOfWork()
         {
-            return new PetaPocoUnitOfWork();
+            return new FileUnitOfWork();
         }
 
         #endregion
