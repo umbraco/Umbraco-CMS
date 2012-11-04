a4a9b26d9399eba9938ab64c3c323cc9b935396a
Shannon Deminick <sdeminick@gmail.com>
2012-11-04 09:24:24 +0600

    Committed first tests of strongly typed querying with strongly typed document types using what would be generated code and extension methods.

--- src/Umbraco.Web/IPublishedStore.cs
+++ src/Umbraco.Web/IPublishedStore.cs
@@ -10,5 +10,13 @@ namespace Umbraco.Web
 	{
 		IPublishedContent GetDocumentById(UmbracoContext umbracoContext, int nodeId);
 		IEnumerable<IPublishedContent> GetRootDocuments(UmbracoContext umbracoContext);
+		
+		//TODO: SD: We should make this happen! This will allow us to natively do a GetByDocumentType query
+		// on the UmbracoHelper (or an internal DataContext that it uses, etc...)
+		// One issue is that we need to make media work as fast as we can and need to create a ConvertFromMediaObject
+		// method in the DefaultPublishedMediaStore, there's already a TODO noting this but in order to do that we'll 
+		// have to also use Examine as much as we can so we don't have to make db calls for looking up things like the 
+		// node type alias, etc... in order to populate the created IPublishedContent object.
+		//IEnumerable<IPublishedContent> GetDocumentsByType(string docTypeAlias);
 	}
 }
