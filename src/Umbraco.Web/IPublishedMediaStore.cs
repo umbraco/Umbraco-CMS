fba31a0a6a07cbe963d7ea6463b9bff6bbab62dd
Shannon Deminick <sdeminick@gmail.com>
2012-11-07 10:16:36 +0500

    Added Code annotations for IPublishedContentStore, etc... as experimental features with links to the tracker.

--- src/Umbraco.Web/IPublishedMediaStore.cs
+++ src/Umbraco.Web/IPublishedMediaStore.cs
@@ -1,8 +1,11 @@
+using Umbraco.Core.CodeAnnotations;
+
 namespace Umbraco.Web
 {
 	/// <summary>
 	/// Defines the methods to access published media
 	/// </summary>
+	[UmbracoExperimentalFeature("http://issues.umbraco.org/issue/U4-1153", "This should become public one day but we need to create something like the IPublishListener interface to have proper published content storage. We'll also need to publicize the resolveres so people can set a resolver at app startup.")]
 	internal interface IPublishedMediaStore : IPublishedStore
 	{
 	}
