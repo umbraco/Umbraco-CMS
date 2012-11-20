d3927a1265e7a55d06b32d80e5e9f269d23948a2
Stephan <sgay@pilotine.com>
2012-11-20 08:49:06 -0100

    code annotation attributes TLC

--- src/Umbraco.Core/CodeAnnotations/UmbracoExperimentalFeatureAttribute.cs
+++ src/Umbraco.Core/CodeAnnotations/UmbracoExperimentalFeatureAttribute.cs
@@ -3,16 +3,30 @@ using System;
 namespace Umbraco.Core.CodeAnnotations
 {
 	/// <summary>
-	/// An attribute used to decorate classes or methods that have been marked to become public but require more testing and review 
-	/// before this is possible.
+	/// Marks the program elements that Umbraco is experimenting with and could become public.
 	/// </summary>
-	internal class UmbracoExperimentalFeatureAttribute : Attribute
+	/// <remarks>
+	/// <para>Indicates that Umbraco  is experimenting with code that potentially could become
+	/// public, but we're not sure, and the code is not stable and can be refactored at any time.</para>
+	/// <para>The issue tracker should contain more details, discussion, and planning.</para>
+	/// </remarks>
+	[AttributeUsage(AttributeTargets.All, AllowMultiple = false, Inherited = false)]
+	internal sealed class UmbracoExperimentalFeatureAttribute : Attribute
 	{
 		/// <summary>
-		/// constructor requires a tracker url and a description
+		/// Initializes a new instance of the <see cref="UmbracoExperimentalFeatureAttribute"/> class with a description.
 		/// </summary>
-		/// <param name="trackerUrl"></param>
-		/// <param name="description"></param>
+		/// <param name="description">The text string that describes what is intended.</param>
+		public UmbracoExperimentalFeatureAttribute(string description)
+		{
+
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="UmbracoExperimentalFeatureAttribute"/> class with a tracker url and a description.
+		/// </summary>
+		/// <param name="trackerUrl">The url of a tracker issue containing more details, discussion, and planning.</param>
+		/// <param name="description">The text string that describes what is intended.</param>
 		public UmbracoExperimentalFeatureAttribute(string trackerUrl, string description)
 		{
 			
--- src/Umbraco.Core/CodeAnnotations/UmbracoProposedPublicAttribute.cs
+++ src/Umbraco.Core/CodeAnnotations/UmbracoProposedPublicAttribute.cs
@@ -3,20 +3,31 @@ using System;
 namespace Umbraco.Core.CodeAnnotations
 {
 	/// <summary>
-	/// An attribute used to decorate classes or methods that have potential to become part of the public API
+	/// Marks the program elements that Umbraco is considering making public.
 	/// </summary>
 	/// <remarks>
-	/// Classes, properties or methods marked with this attribute will have a tracker URL assigned where discussions may take place
-	/// regarding whether or not this should become part of the public APIs and if/when. Please note that any objects marked with this
-	/// attribute may not ever become public, this attribute exists to tag code that has potential to become public pending reviews.
+	/// <para>Indicates that Umbraco considers making the (currently internal) program element public
+	/// at some point in the future, but the decision is not fully made yet and is still pending reviews.</para>
+	/// <para>Note that it is not a commitment to make the program element public. It may not ever become public.</para>
+	/// <para>The issue tracker should contain more details, discussion, and planning.</para>
 	/// </remarks>
-	internal class UmbracoProposedPublicAttribute : Attribute
+	[AttributeUsage(AttributeTargets.All, AllowMultiple=false, Inherited=false)]
+	internal sealed class UmbracoProposedPublicAttribute : Attribute
 	{
 		/// <summary>
-		/// constructor requires a tracker url and a description
+		/// Initializes a new instance of the <see cref="UmbracoProposedPublicAttribute"/> class with a description.
 		/// </summary>
-		/// <param name="trackerUrl"></param>
-		/// <param name="description"></param>
+		/// <param name="description">The text string that describes what is intended.</param>
+		public UmbracoProposedPublicAttribute(string description)
+		{
+
+		}
+
+		/// <summary>
+		/// Initializes a new instance of the <see cref="UmbracoProposedPublicAttribute"/> class with a tracker url and a description.
+		/// </summary>
+		/// <param name="trackerUrl">The url of a tracker issue containing more details, discussion, and planning.</param>
+		/// <param name="description">The text string that describes what is intended.</param>
 		public UmbracoProposedPublicAttribute(string trackerUrl, string description)
 		{
 			
