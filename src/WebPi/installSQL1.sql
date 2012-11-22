96a6f17bd7610a4ba730a588dae4a86b1b69ec1b
Sebastiaan Janssen <sebastiaan@umbraco.com>
2012-11-22 16:06:06 -0100

    Update for the WebPI build, should now automatically be good Fixes U4-132

---
+++ src/WebPi/installSQL1.sql
@@ -0,0 +1,9 @@
+/**********************************************************************/
+/* Install.SQL                                                        */
+/* Creates a login and makes the user a member of db roles            */
+/*                                                                    */
+/*           Modifications for SQL AZURE  - ON MASTER                 */
+/**********************************************************************/
+
+
+CREATE LOGIN PlaceHolderForUser WITH PASSWORD = 'PlaceHolderForPassword'
---
+++ src/WebPi/installSQL2.sql
@@ -0,0 +1,18 @@
+/**********************************************************************/
+/* CreateUser.SQL */
+/* Creates a user and makes the user a member of db roles */
+/* This script runs against the User database and requires connection string */
+/* Supports SQL Server and SQL AZURE */
+/**********************************************************************/
+
+-- Create database user and map to login
+-- and add user to the datareader, datawriter, ddladmin and securityadmin roles
+--
+
+CREATE USER PlaceHolderForUser FOR LOGIN PlaceHolderForUser;
+GO
+EXEC sp_addrolemember 'db_ddladmin', PlaceHolderForUser;
+EXEC sp_addrolemember 'db_securityadmin', PlaceHolderForUser;
+EXEC sp_addrolemember 'db_datareader', PlaceHolderForUser;
+EXEC sp_addrolemember 'db_datawriter', PlaceHolderForUser;
+GO
