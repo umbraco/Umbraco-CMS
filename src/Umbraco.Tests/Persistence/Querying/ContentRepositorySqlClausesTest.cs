8ee0fffb84bce5239beebc5197c76a87d903e941
Morten Christensen <unknown@kiln.example.com>
2012-11-26 11:29:20 -0100

    Content repo sql tests

---
+++ src/Umbraco.Tests/Persistence/Querying/ContentRepositorySqlClausesTest.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using NUnit.Framework;
+using Umbraco.Core.Persistence;
+
+namespace Umbraco.Tests.Persistence.Querying
+{
+    [TestFixture]
+    public class ContentRepositorySqlClausesTest
+    {
+         public void Can_Verify_Base_Clause()
+         {
+             var NodeObjectType = new Guid("");
+
+             var sql = new Sql();
+             sql.Select("*")
+                 .From("cmsDocument")
+                 .InnerJoin("cmsContentVersion").On("[cmsDocument].[versionId] = [cmsContentVersion].[VersionId]")
+                 .InnerJoin("cmsContent").On("[cmsContentVersion].[ContentId] = [cmsContent].[nodeId]")
+                 .InnerJoin("umbracoNode").On("[cmsContent].[nodeId] = [umbracoNode].[id]")
+                 .Where("[umbracoNode].[nodeObjectType] = @NodeObjectType", new { NodeObjectType });
+         }
+    }
+}
