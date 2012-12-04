2c2177fcc75d8bebfca61165fea5e0aa3b99b046
Morten Christensen <unknown@kiln.example.com>
2012-12-04 07:36:51 -0100

    Creating a new DefinitionFactory for refactoring the database definitions used by the syntax provider.

---
+++ src/Umbraco.Core/Persistence/DatabaseModelDefinitions/DefinitionFactory.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using Umbraco.Core.Persistence.Migrations.Model;
+
+namespace Umbraco.Core.Persistence.DatabaseModelDefinitions
+{
+    internal static class DefinitionFactory
+    {
+         public static TableDefinition GetTableDefinition(Type modelType)
+         {
+             var tableDefinition = new TableDefinition();
+             return tableDefinition;
+         }
+    }
+}
