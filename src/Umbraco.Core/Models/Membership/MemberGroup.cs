d9881a5530c04179c3769460ac86b2fe463d0c3e
Morten Christensen <unknown@kiln.example.com>
2012-12-17 14:01:29 -0100

    Adding stubs for membership classes - keeping it internal for now as implementation will come at a later stage.

--- src/Umbraco.Core/Models/Membership/MemberGroup.cs
+++ src/Umbraco.Core/Models/Membership/MemberGroup.cs
@@ -5,7 +5,7 @@ using Umbraco.Core.Models.EntityBase;
 namespace Umbraco.Core.Models.Membership
 {
     /// <summary>
-    /// Represents the Type for a Backoffice User
+    /// Represents a Group for a Backoffice User
     /// </summary>
     /// <remarks>
     /// Should be internal until a proper user/membership implementation
@@ -13,15 +13,8 @@ namespace Umbraco.Core.Models.Membership
     /// </remarks>
     [Serializable]
     [DataContract(IsReference = true)]
-    internal class UserType : Entity, IUserType
+    internal class MemberGroup : Entity
     {
-        [DataMember]
-        public string Alias { get; set; }
-
-        [DataMember]
-        public string Name { get; set; }
-
-        [DataMember]
-        public string Permissions { get; set; }
+        //Add MemberCollection ?
     }
 }
