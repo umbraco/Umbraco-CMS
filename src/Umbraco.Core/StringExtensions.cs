c98d906f36abade5b1a20d941f04de525f58f82a
Morten Christensen <unknown@kiln.example.com>
2012-12-28 19:08:01 -0100

    Correcting a few formatting issues after importing patch.

--- src/Umbraco.Core/StringExtensions.cs
+++ src/Umbraco.Core/StringExtensions.cs
@@ -744,21 +744,21 @@ namespace Umbraco.Core
 
         public static string FormatUrl(this string url)
         {
-            string _newUrl = url;
+            string newUrl = url;
             XmlNode replaceChars = UmbracoSettings.UrlReplaceCharacters;
             foreach (XmlNode n in replaceChars.SelectNodes("char"))
             {
                 if (n.Attributes.GetNamedItem("org") != null && n.Attributes.GetNamedItem("org").Value != "")
-                    _newUrl = _newUrl.Replace(n.Attributes.GetNamedItem("org").Value, Umbraco.Core.XmlHelper.GetNodeValue(n));
+                    newUrl = newUrl.Replace(n.Attributes.GetNamedItem("org").Value, XmlHelper.GetNodeValue(n));
             }
 
             // check for double dashes
             if (UmbracoSettings.RemoveDoubleDashesFromUrlReplacing)
             {
-                _newUrl = Regex.Replace(_newUrl, @"[-]{2,}", "-");
+                newUrl = Regex.Replace(newUrl, @"[-]{2,}", "-");
             }
 
-            return _newUrl;
+            return newUrl;
         }
     }
 }
