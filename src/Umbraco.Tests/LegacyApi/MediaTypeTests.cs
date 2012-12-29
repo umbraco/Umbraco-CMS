56e2a89199816aca4a96f0c18b2e4d260fc2b07c
Morten Christensen <unknown@kiln.example.com>
2012-12-29 11:11:09 -0100

    Adding a few new test cases to verify that AllowedChildContentTypes are saved correctly on ContentTypes and MediaTypes.

---
+++ src/Umbraco.Tests/LegacyApi/MediaTypeTests.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Linq;
+using NUnit.Framework;
+using Umbraco.Tests.TestHelpers;
+using Umbraco.Tests.TestHelpers.Entities;
+using umbraco.cms.businesslogic.media;
+
+namespace Umbraco.Tests.LegacyApi
+{
+    [TestFixture]
+    public class MediaTypeTests : BaseDatabaseFactoryTest
+    {
+        [SetUp]
+        public override void Initialize()
+        {
+            base.Initialize();
+
+            CreateTestData();
+        }
+
+        [Test]
+        public void Can_Verify_AllowedChildContentTypes_On_MediaType()
+        {
+            // Arrange
+            var folder = MediaType.GetByAlias("Folder");
+            var folderStructure = folder.AllowedChildContentTypeIDs.ToList();
+            folderStructure.Add(1045);
+
+            // Act
+            folder.AllowedChildContentTypeIDs = folderStructure.ToArray();
+            folder.Save();
+
+            // Assert
+            var updated = MediaType.GetByAlias("Folder");
+
+            Assert.That(updated.AllowedChildContentTypeIDs.Any(), Is.True);
+            Assert.That(updated.AllowedChildContentTypeIDs.Any(x => x == 1045), Is.True);
+        }
+
+        public void CreateTestData()
+        {
+            //Create and Save ContentType "video" -> 1045
+            var videoMediaType = MockedContentTypes.CreateVideoMediaType();
+            ServiceContext.ContentTypeService.Save(videoMediaType);
+        }
+
+
+        [TearDown]
+        public override void TearDown()
+        {
+            base.TearDown();
+        }
+    }
+}
