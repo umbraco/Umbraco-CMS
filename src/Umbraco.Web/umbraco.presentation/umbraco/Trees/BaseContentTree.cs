6187af76c05b82c3409426e964206d0b3862b687
Sebastiaan Janssen <sebastiaan@umbraco.com>
2013-01-10 13:55:42 -0100

    Revert my c1722c82aa28 changes to published version checking (which were wrong!)

--- src/Umbraco.Web/umbraco.presentation/umbraco/Trees/BaseContentTree.cs
+++ src/Umbraco.Web/umbraco.presentation/umbraco/Trees/BaseContentTree.cs
@@ -113,7 +113,7 @@ function openContent(id) {
                 node.OpenIcon = dd.ContentTypeIcon;
             }
 
-            if (dd.Published == false)
+            if (dd.HasPublishedVersion() == false)
                 node.Style.DimNode();
 
             if (dd.HasPendingChanges())
