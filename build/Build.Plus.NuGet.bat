930e6a0989be820d0d77a54676e8c09b345b72a9
Sebastiaan Janssen <sebastiaan@umbraco.com>
2013-01-12 17:29:48 -0100

    Forgot some parenthesis ..

--- build/Build.Plus.NuGet.bat
+++ build/Build.Plus.NuGet.bat
@@ -1,5 +1,5 @@
 @ECHO OFF
-set version=4.11.3
+set version=4.11.3.1
 %windir%\Microsoft.NET\Framework\v4.0.30319\msbuild.exe "Build.proj" /p:BUILD_NUMBER=%version%
 
 echo This file is only here so that the containing folder will be included in the NuGet package, it is safe to delete. > .\_BuildOutput\WebApp\App_Code\dummy.txt
