1568e5f19c0ed29a20a61e0c3b7afced56f15651
Shannon Deminick <sdeminick@gmail.com>
2013-01-23 01:44:01 +0300

    updated comments.

--- src/Umbraco.Core/StringExtensions.cs
+++ src/Umbraco.Core/StringExtensions.cs
@@ -23,7 +23,6 @@ namespace Umbraco.Core
 		/// Encrypt the string using the MachineKey in medium trust
 		/// </summary>
 		/// <param name="value">The string value to be encrypted.</param>
-		/// <param name="value">The string value to be encrypted.</param>
 		/// <returns></returns>
 		public static string EncryptWithMachineKey(this string value)
 		{
