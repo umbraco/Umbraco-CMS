82455136bb77a07402065b1842589a7b17d87b57
Morten Christensen <unknown@kiln.example.com>
2013-01-24 14:59:03 -0100

    Missing files

---
+++ src/Umbraco.Core/Persistence/Migrations/Initial/DatabaseSchemaResult.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Umbraco.Core.Persistence.Migrations.Initial
+{
+    public class DatabaseSchemaResult
+    {
+        public DatabaseSchemaResult()
+        {
+            Errors = new Dictionary<string, string>();
+            Successes = new Dictionary<string, string>();
+        }
+
+        public IDictionary<string, string> Errors { get; set; }
+
+        public IDictionary<string, string> Successes { get; set; }
+    }
+}
