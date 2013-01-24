c8807c379868ec0fbd391dffa46baca49e0321ed
Shannon Deminick <sdeminick@gmail.com>
2013-01-24 06:16:44 +0300

    Fixes: #U4-1530. Backports ContentExtension changes from 6.1 required for Examine and media. Added unit tests to support and enhanced old unit test for ToXml for content to test for more values to ensure nothing is broken. Updates ExamineEvents to ensure that no event binding occurs until the application is configured and the database is ready. Updates ExamineEvents to ensure that data is indexed using the new 6.0 APIs, though this is a temporary fix and a better one will be implemented in 6.1 since UmbracoExamine is in the source.

--- src/Umbraco.Tests/Models/ContentXmlTest.cs
+++ src/Umbraco.Tests/Models/ContentXmlTest.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Linq;
 using System.Xml.Linq;
 using NUnit.Framework;
 using Umbraco.Core;
@@ -61,6 +62,27 @@ namespace Umbraco.Tests.Models
             // Assert
             Assert.That(element, Is.Not.Null);
             Assert.That(element.Name.LocalName, Is.EqualTo(nodeName));
+            Assert.AreEqual(content.Id.ToString(), (string)element.Attribute("id"));
+            Assert.AreEqual(content.ParentId.ToString(), (string)element.Attribute("parentID"));
+            Assert.AreEqual(content.Level.ToString(), (string)element.Attribute("level"));
+            Assert.AreEqual(content.CreatorId.ToString(), (string)element.Attribute("creatorID"));
+            Assert.AreEqual(content.SortOrder.ToString(), (string)element.Attribute("sortOrder"));
+            Assert.AreEqual(content.CreateDate.ToString("s"), (string)element.Attribute("createDate"));
+            Assert.AreEqual(content.UpdateDate.ToString("s"), (string)element.Attribute("updateDate"));
+            Assert.AreEqual(content.Name, (string)element.Attribute("nodeName"));
+            Assert.AreEqual(content.Name.FormatUrl().ToLower(), (string)element.Attribute("urlName"));
+            Assert.AreEqual(content.Path, (string)element.Attribute("path"));
+            Assert.AreEqual("", (string)element.Attribute("isDoc"));
+            Assert.AreEqual(content.ContentType.Id.ToString(), (string)element.Attribute("nodeType"));
+            Assert.AreEqual(content.GetCreatorProfile().Name, (string)element.Attribute("creatorName"));
+            Assert.AreEqual(content.GetWriterProfile().Name, (string)element.Attribute("writerName"));
+            Assert.AreEqual(content.WriterId.ToString(), (string)element.Attribute("writerID"));
+            Assert.AreEqual(content.Template == null ? "0" : content.Template.Id.ToString(), (string)element.Attribute("template"));
+
+            Assert.AreEqual(content.Properties["title"].Value.ToString(), element.Elements("title").Single().Value);
+            Assert.AreEqual(content.Properties["bodyText"].Value.ToString(), element.Elements("bodyText").Single().Value);
+            Assert.AreEqual(content.Properties["keywords"].Value.ToString(), element.Elements("keywords").Single().Value);
+            Assert.AreEqual(content.Properties["metaDescription"].Value.ToString(), element.Elements("metaDescription").Single().Value);
         } 
     }
 }
