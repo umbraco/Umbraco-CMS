1c08921f559f610178989c56b3d95d950b1327ef
Morten Christensen <unknown@kiln.example.com>
2013-01-29 12:45:42 -0100

    Implements U4-1568 so raising events for save methods is optional.

--- src/Umbraco.Core/Events/NewEventArgs.cs
+++ src/Umbraco.Core/Events/NewEventArgs.cs
@@ -1,3 +1,5 @@
+using Umbraco.Core.Models;
+
 namespace Umbraco.Core.Events
 {
 	public class NewEventArgs<TEntity> : CancellableObjectEventArgs<TEntity>
@@ -8,11 +10,25 @@ namespace Umbraco.Core.Events
 			ParentId = parentId;
 		}
 
+        public NewEventArgs(TEntity eventObject, bool canCancel, string @alias, IContent parent)
+            : base(eventObject, canCancel)
+        {
+            Alias = alias;
+            Parent = parent;
+        }
+
 		public NewEventArgs(TEntity eventObject, string @alias, int parentId) : base(eventObject)
 		{
 			Alias = alias;
 			ParentId = parentId;
 		}
+
+        public NewEventArgs(TEntity eventObject, string @alias, IContent parent)
+            : base(eventObject)
+        {
+            Alias = alias;
+            Parent = parent;
+        }
 
 		/// <summary>
 		/// The entity being created
@@ -31,5 +47,10 @@ namespace Umbraco.Core.Events
 		/// Gets or Sets the Id of the parent.
 		/// </summary>
 		public int ParentId { get; private set; }
+
+        /// <summary>
+        /// Gets or Sets the parent IContent object.
+        /// </summary>
+        public IContent Parent { get; private set; }
 	}
 }
