ca7fe6854b73713e0457db11e8d6483ce54e141f
Shannon Deminick <sdeminick@gmail.com>
2013-02-03 05:16:42 +0600

    Moved all old installer legacy classes into single files called LegacyFiles... much less clutter in the old legacy folders.

--- src/Umbraco.Web/umbraco.presentation/install/LegacyClasses.cs
+++ src/Umbraco.Web/umbraco.presentation/install/LegacyClasses.cs
@@ -15,6 +15,16 @@ using System.Collections.Generic;
 
 namespace umbraco.presentation.install
 {
+
+    [Obsolete("This file is no longer used and will be removed in future version. The UserControl Umbraco.Web.UI.Install.Title has superceded this.")]
+    public partial class Title : System.Web.UI.UserControl
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+
 	/// <summary>
 	/// Summary description for _default.
 	/// </summary>
