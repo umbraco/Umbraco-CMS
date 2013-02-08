def651d98ea50a5bde222cfce155564dd0a0baac
abutland <unknown@kiln.example.com>
2013-02-08 15:01:00 -0100

    Implemented an attribute for surface controllers to provide the restrictions to certain members as available in base.

---
+++ src/Umbraco.Web/Mvc/SurfaceAuthorizeAttribute.cs
@@ -0,0 +1,83 @@
+﻿using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Security;
+using umbraco.cms.businesslogic.member;
+
+namespace Umbraco.Web.Mvc
+{
+    /// <summary>
+    /// "Base-like" attribute for attributing surface controller actions to restrict them
+    /// to just authenticated members, and optionally of a particular type and/or group
+    /// </summary>
+    public class SurfaceAuthorizeAttribute : ActionFilterAttribute
+    {
+        /// <summary>
+        /// Flag for whether to allow all site visitors or just authenticated members
+        /// </summary>
+        public bool AllowAll { get; set; }
+
+        /// <summary>
+        /// Comma delimited list of allowed member types
+        /// </summary>
+        public string AllowType { get; set; }
+
+        /// <summary>
+        /// Comma delimited list of allowed member groups
+        /// </summary>
+        public string AllowGroup { get; set; }
+
+        /// <summary>
+        /// Comma delimited list of allowed members
+        /// </summary>
+        public string AllowMembers { get; set; }
+
+        public override void OnActionExecuting(ActionExecutingContext filterContext)
+        {
+            // Allow by default
+            bool allowAction = true;
+
+            // If not set to allow all, need to check current loggined in member
+            if (!AllowAll)
+            {
+                // Get member details
+                var member = Member.GetCurrentMember();
+                if (member == null)
+                {
+                    // If not logged on, not allowed
+                    allowAction = false;
+                }
+                else
+                {
+                    // If types defined, check member is of one of those types
+                    if (!string.IsNullOrEmpty(AllowType))
+                    {
+                        // Allow only if member's type is in list
+                        allowAction = AllowType.ToLower().Split(',').Contains(member.ContentType.Alias.ToLower());
+                    }
+
+                    // If groups defined, check member is of one of those groups
+                    if (allowAction && !string.IsNullOrEmpty(AllowGroup))
+                    {
+                        // Allow only if member's type is in list
+                        var groups = Roles.GetRolesForUser(member.LoginName);
+                        allowAction = groups.Select(s => s.ToLower()).Intersect(AllowGroup.ToLower().Split(',')).Any();
+                    }
+
+                    // If specific members defined, check member is of one of those
+                    if (allowAction && !string.IsNullOrEmpty(AllowMembers))
+                    {
+                        // Allow only if member's type is in list
+                        allowAction = AllowMembers.ToLower().Split(',').Contains(member.Id.ToString());
+                    }
+                }
+            }
+
+            // If not allowed, throw 403 exception
+            if (!allowAction)
+            {
+                throw new HttpException(403, "Resource restricted: either member is not logged on or is not of a permitted type or group.");
+            }
+        }
+    }
+}
