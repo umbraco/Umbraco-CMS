ef86d2f1f1f3c7c88674267649b675d4de33de0f
Shannon Deminick <sdeminick@gmail.com>
2013-02-14 23:26:29 +0600

    Manually applies patch for #U4-1718

--- src/Umbraco.Web/ApplicationEventsResolver.cs
+++ src/Umbraco.Web/ApplicationEventsResolver.cs
@@ -1,49 +1,86 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using Umbraco.Core;
 using Umbraco.Core.ObjectResolution;
 using umbraco.interfaces;
 
 namespace Umbraco.Web
 {
-	/// <summary>
-	/// A resolver to return all IApplicationEvents objects
-	/// </summary>
-	internal sealed class ApplicationEventsResolver : ManyObjectsResolverBase<ApplicationEventsResolver, IApplicationStartupHandler>
-	{
+    /// <summary>
+    /// A resolver to return all IApplicationEvents objects
+    /// </summary>
+    internal sealed class ApplicationEventsResolver : ManyObjectsResolverBase<ApplicationEventsResolver, IApplicationEventHandler>
+    {
 
-		/// <summary>
-		/// Constructor
-		/// </summary>
-		/// <param name="applicationEventHandlers"></param>		
-		internal ApplicationEventsResolver(IEnumerable<Type> applicationEventHandlers)
-			: base(applicationEventHandlers)
-		{
+        private readonly LegacyStartupHandlerResolver _legacyResolver;
 
-		}
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        /// <param name="applicationEventHandlers"></param>		
+        internal ApplicationEventsResolver(IEnumerable<Type> applicationEventHandlers)
+            : base(applicationEventHandlers)
+        {
+            //create the legacy resolver and only include the legacy types
+            _legacyResolver = new LegacyStartupHandlerResolver(
+                applicationEventHandlers.Where(x => !TypeHelper.IsTypeAssignableFrom<IApplicationEventHandler>(x)));
+        }
 
-		/// <summary>
-		/// Gets the <see cref="IApplicationEventHandler"/> implementations.
-		/// </summary>
-		public IEnumerable<IApplicationEventHandler> ApplicationEventHandlers
-		{
-			get { return Values.OfType<IApplicationEventHandler>(); }
-		}
+        /// <summary>
+        /// Override in order to only return types of IApplicationEventHandler and above, 
+        /// do not include the legacy types of IApplicationStartupHandler
+        /// </summary>
+        protected override IEnumerable<Type> InstanceTypes
+        {
+            get { return base.InstanceTypes.Where(TypeHelper.IsTypeAssignableFrom<IApplicationEventHandler>); }
+        }
 
-		protected override bool SupportsClear
-		{
-			get { return false; }
-		}		
+        /// <summary>
+        /// Gets the <see cref="IApplicationEventHandler"/> implementations.
+        /// </summary>
+        public IEnumerable<IApplicationEventHandler> ApplicationEventHandlers
+        {
+            get { return Values; }
+        }
 
-		protected override bool SupportsInsert
-		{
-			get { return false; }			
-		}
+        /// <summary>
+        /// Create instances of all of the legacy startup handlers
+        /// </summary>
+        public void InstantiateLegacyStartupHanlders()
+        {
+            //this will instantiate them all
+            var handlers = _legacyResolver.LegacyStartupHandlers;
+        }
 
-		protected override bool SupportsRemove
-		{
-			get { return false; }
-		}
+        protected override bool SupportsClear
+        {
+            get { return false; }
+        }
 
-	}
+        protected override bool SupportsInsert
+        {
+            get { return false; }
+        }
+
+        protected override bool SupportsRemove
+        {
+            get { return false; }
+        }
+
+        private class LegacyStartupHandlerResolver : ManyObjectsResolverBase<ApplicationEventsResolver, IApplicationStartupHandler>
+        {
+            internal LegacyStartupHandlerResolver(IEnumerable<Type> legacyStartupHandlers)
+                : base(legacyStartupHandlers)
+            {
+
+            }
+
+            public IEnumerable<IApplicationStartupHandler> LegacyStartupHandlers
+            {
+                get { return Values; }
+            }
+        }
+
+    }
 }
