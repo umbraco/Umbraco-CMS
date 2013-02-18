e56738481b353eb9d7b784eeb10f70ba7f589eec
Sebastiaan Janssen <sebastiaan@umbraco.com>
2013-02-18 11:38:07 -0100

    Adding trailing slash to failing routing tests. The issue with these tests might again be because of the UmbracoSettings file, which has the trailing slash setting. But with the settings being reset after teardown the tests were still failing, so made these minor corrections which shoudl correspond to the default setting.

--- src/Umbraco.Tests/Routing/NiceUrlProviderTests.cs
+++ src/Umbraco.Tests/Routing/NiceUrlProviderTests.cs
@@ -40,14 +40,14 @@ namespace Umbraco.Tests.Routing
 			ConfigurationManager.AppSettings.Set("umbracoUseDirectoryUrls", "true");
 
 			var samples = new Dictionary<int, string> {
-				{ 1046, "/home" },
-				{ 1173, "/home/sub1" },
-				{ 1174, "/home/sub1/sub2" },
-				{ 1176, "/home/sub1/sub-3" },
-				{ 1177, "/home/sub1/custom-sub-1" },
-				{ 1178, "/home/sub1/custom-sub-2" },
-				{ 1175, "/home/sub-2" },
-				{ 1172, "/test-page" }
+				{ 1046, "/home/" },
+				{ 1173, "/home/sub1/" },
+				{ 1174, "/home/sub1/sub2/" },
+				{ 1176, "/home/sub1/sub-3/" },
+				{ 1177, "/home/sub1/custom-sub-1/" },
+				{ 1178, "/home/sub1/custom-sub-2/" },
+				{ 1175, "/home/sub-2/" },
+				{ 1172, "/test-page/" }
 			};
 
 			foreach (var sample in samples)
@@ -84,14 +84,14 @@ namespace Umbraco.Tests.Routing
 		}
 
 		// test hideTopLevelNodeFromPath false
-		[TestCase(1046, "/home")]
-		[TestCase(1173, "/home/sub1")]
-		[TestCase(1174, "/home/sub1/sub2")]
-		[TestCase(1176, "/home/sub1/sub-3")]
-		[TestCase(1177, "/home/sub1/custom-sub-1")]
-		[TestCase(1178, "/home/sub1/custom-sub-2")]
-		[TestCase(1175, "/home/sub-2")]
-		[TestCase(1172, "/test-page")]
+		[TestCase(1046, "/home/")]
+		[TestCase(1173, "/home/sub1/")]
+		[TestCase(1174, "/home/sub1/sub2/")]
+		[TestCase(1176, "/home/sub1/sub-3/")]
+		[TestCase(1177, "/home/sub1/custom-sub-1/")]
+		[TestCase(1178, "/home/sub1/custom-sub-2/")]
+		[TestCase(1175, "/home/sub-2/")]
+		[TestCase(1172, "/test-page/")]
 
 		public void Get_Nice_Url_Not_Hiding_Top_Level(int nodeId, string niceUrlMatch)
 		{
@@ -109,13 +109,13 @@ namespace Umbraco.Tests.Routing
 
 		// test hideTopLevelNodeFromPath true
 		[TestCase(1046, "/")]
-		[TestCase(1173, "/sub1")]
-		[TestCase(1174, "/sub1/sub2")]
-		[TestCase(1176, "/sub1/sub-3")]
-		[TestCase(1177, "/sub1/custom-sub-1")]
-		[TestCase(1178, "/sub1/custom-sub-2")]
-		[TestCase(1175, "/sub-2")]
-		[TestCase(1172, "/test-page")] // not hidden because not first root
+		[TestCase(1173, "/sub1/")]
+		[TestCase(1174, "/sub1/sub2/")]
+		[TestCase(1176, "/sub1/sub-3/")]
+		[TestCase(1177, "/sub1/custom-sub-1/")]
+		[TestCase(1178, "/sub1/custom-sub-2/")]
+		[TestCase(1175, "/sub-2/")]
+		[TestCase(1172, "/test-page/")] // not hidden because not first root
 
 		public void Get_Nice_Url_Hiding_Top_Level(int nodeId, string niceUrlMatch)
 		{
@@ -138,14 +138,14 @@ namespace Umbraco.Tests.Routing
 			ConfigurationManager.AppSettings.Set("umbracoHideTopLevelNodeFromPath", "false");
 
 			Umbraco.Core.Configuration.UmbracoSettings.UseDomainPrefixes = false;
-			Assert.AreEqual("/home/sub1/custom-sub-1", routingContext.NiceUrlProvider.GetNiceUrl(1177));
+			Assert.AreEqual("/home/sub1/custom-sub-1/", routingContext.NiceUrlProvider.GetNiceUrl(1177));
 
 			Umbraco.Core.Configuration.UmbracoSettings.UseDomainPrefixes = true;
-			Assert.AreEqual("http://example.com/home/sub1/custom-sub-1", routingContext.NiceUrlProvider.GetNiceUrl(1177));
+			Assert.AreEqual("http://example.com/home/sub1/custom-sub-1/", routingContext.NiceUrlProvider.GetNiceUrl(1177));
 
 			Umbraco.Core.Configuration.UmbracoSettings.UseDomainPrefixes = false;
 			routingContext.NiceUrlProvider.EnforceAbsoluteUrls = true;
-			Assert.AreEqual("http://example.com/home/sub1/custom-sub-1", routingContext.NiceUrlProvider.GetNiceUrl(1177));
+			Assert.AreEqual("http://example.com/home/sub1/custom-sub-1/", routingContext.NiceUrlProvider.GetNiceUrl(1177));
 		}
 
 		[Test]
