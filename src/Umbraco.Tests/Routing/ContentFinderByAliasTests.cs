11560a5451476ac1016d855250a6bfdf4a7ff761
Stephan <sgay@pilotine.com>
2013-02-20 06:47:07 -0100

    Test.Routing - more umbracoUrlAlias test

--- src/Umbraco.Tests/Routing/ContentFinderByAliasTests.cs
+++ src/Umbraco.Tests/Routing/ContentFinderByAliasTests.cs
@@ -1,26 +1,147 @@
+using System.Linq;
 using NUnit.Framework;
 using Umbraco.Tests.TestHelpers;
 using Umbraco.Web.Routing;
+using umbraco.cms.businesslogic.web;
+using umbraco.cms.businesslogic.language;
 
 namespace Umbraco.Tests.Routing
 {
 	[TestFixture]
 	public class ContentFinderByAliasTests : BaseRoutingTest
 	{
-        /// <summary>
-		/// We don't need a db for this test, will run faster without one
-		/// </summary>
-		protected override bool RequiresDbSetup
-		{
-			get { return false; }
-		}
+        public override void Initialize()
+        {
+            base.Initialize();
 
-		[TestCase("/this/is/my/alias", 1046)]
-		[TestCase("/anotheralias", 1046)]
-		[TestCase("/page2/alias", 1173)]
-		[TestCase("/2ndpagealias", 1173)]
-		[TestCase("/only/one/alias", 1174)]
-		[TestCase("/ONLY/one/Alias", 1174)]
+            // ensure we can create them although the content is not in the database
+            TestHelper.DropForeignKeys("umbracoDomains");
+
+            InitializeLanguagesAndDomains();
+        }
+
+        void InitializeLanguagesAndDomains()
+        {
+            var domains = Domain.GetDomains();
+            foreach (var d in domains)
+                d.Delete();
+
+            var langs = Language.GetAllAsList();
+            foreach (var l in langs.Skip(1))
+                l.Delete();
+
+            // en-US is there by default
+            Language.MakeNew("fr-FR");
+            Language.MakeNew("de-DE");
+        }
+
+        void SetDomains1()
+        {
+            var langEn = Language.GetByCultureCode("en-US");
+            var langFr = Language.GetByCultureCode("fr-FR");
+            var langDe = Language.GetByCultureCode("de-DE");
+
+            Domain.MakeNew("domain1.com/", 1001, langDe.id);
+            Domain.MakeNew("domain1.com/en", 10011, langEn.id);
+            Domain.MakeNew("domain1.com/fr", 10012, langFr.id);
+        }
+
+        protected override string GetXmlContent(int templateId)
+        {
+            return @"<?xml version=""1.0"" encoding=""utf-8""?>
+<!DOCTYPE root[ 
+<!ELEMENT Doc ANY>
+<!ATTLIST Doc id ID #REQUIRED>
+]>
+<root id=""-1"">
+	<Doc id=""1001"" parentID=""-1"" level=""1"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" + templateId + @""" sortOrder=""0"" createDate=""2012-06-12T14:13:17"" updateDate=""2012-07-20T18:50:43"" nodeName=""Home"" urlName=""1001"" writerName=""admin"" creatorName=""admin"" path=""-1,1001"" isDoc="""">
+		<content><![CDATA[]]></content>
+        <umbracoUrlAlias><![CDATA[this/is/my/alias, anotheralias]]></umbracoUrlAlias>
+		<Doc id=""10011"" parentID=""1001"" level=""2"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" + templateId + @""" sortOrder=""0"" createDate=""2012-07-20T18:06:45"" updateDate=""2012-07-20T19:07:31"" nodeName=""Sub1"" urlName=""1001-1"" writerName=""admin"" creatorName=""admin"" path=""-1,1001,10011"" isDoc="""">
+			<content><![CDATA[<div>This is some content</div>]]></content>
+            <umbracoUrlAlias><![CDATA[page2/alias, 2ndpagealias, en/flux, endanger]]></umbracoUrlAlias>
+			<Doc id=""100111"" parentID=""10011"" level=""3"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" + templateId + @""" sortOrder=""0"" createDate=""2012-07-20T18:07:54"" updateDate=""2012-07-20T19:10:27"" nodeName=""Sub2"" urlName=""1001-1-1"" writerName=""admin"" creatorName=""admin"" path=""-1,1001,10011,100111"" isDoc="""">
+				<content><![CDATA[]]></content>
+                <umbracoUrlAlias><![CDATA[only/one/alias, entropy, bar/foo, en/bar/nil]]></umbracoUrlAlias>
+			</Doc>
+			<Doc id=""100112"" parentID=""10011"" level=""3"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" + templateId + @""" sortOrder=""1"" createDate=""2012-07-20T18:08:08"" updateDate=""2012-07-20T19:10:52"" nodeName=""Sub 3"" urlName=""1001-1-2"" writerName=""admin"" creatorName=""admin"" path=""-1,1001,10011,100112"" isDoc="""">
+				<content><![CDATA[]]></content>
+				<Doc id=""1001121"" parentID=""100112"" level=""4"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" + templateId + @""" sortOrder=""1"" createDate=""2012-07-20T18:08:08"" updateDate=""2012-07-20T19:10:52"" nodeName=""Sub 3"" urlName=""1001-1-2-1"" writerName=""admin"" creatorName=""admin"" path=""-1,1001,10011,100112,1001121"" isDoc="""">
+					<content><![CDATA[]]></content>
+				</Doc>
+				<Doc id=""1001122"" parentID=""100112"" level=""4"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" + templateId + @""" sortOrder=""1"" createDate=""2012-07-20T18:08:08"" updateDate=""2012-07-20T19:10:52"" nodeName=""Sub 3"" urlName=""1001-1-2-2"" writerName=""admin"" creatorName=""admin"" path=""-1,1001,10011,100112,1001122"" isDoc="""">
+					<content><![CDATA[]]></content>
+				</Doc>
+			</Doc>
+		</Doc>
+		<Doc id=""10012"" parentID=""1001"" level=""2"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" + templateId + @""" sortOrder=""1"" createDate=""2012-07-20T18:08:01"" updateDate=""2012-07-20T18:49:32"" nodeName=""Sub 2"" urlName=""1001-2"" writerName=""admin"" creatorName=""admin"" path=""-1,1001,10012"" isDoc="""">
+			<content><![CDATA[<div>This is some content</div>]]></content>
+            <umbracoUrlAlias><![CDATA[alias42]]></umbracoUrlAlias>
+			<Doc id=""100121"" parentID=""10012"" level=""3"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" + templateId + @""" sortOrder=""0"" createDate=""2012-07-20T18:07:54"" updateDate=""2012-07-20T19:10:27"" nodeName=""Sub2"" urlName=""1001-2-1"" writerName=""admin"" creatorName=""admin"" path=""-1,1001,10012,100121"" isDoc="""">
+				<content><![CDATA[]]></content>
+                <umbracoUrlAlias><![CDATA[alias43]]></umbracoUrlAlias>
+			</Doc>
+			<Doc id=""100122"" parentID=""10012"" level=""3"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" + templateId + @""" sortOrder=""1"" createDate=""2012-07-20T18:08:08"" updateDate=""2012-07-20T19:10:52"" nodeName=""Sub 3"" urlName=""1001-2-2"" writerName=""admin"" creatorName=""admin"" path=""-1,1001,10012,100122"" isDoc="""">
+				<content><![CDATA[]]></content>
+				<Doc id=""1001221"" parentID=""100122"" level=""4"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" + templateId + @""" sortOrder=""1"" createDate=""2012-07-20T18:08:08"" updateDate=""2012-07-20T19:10:52"" nodeName=""Sub 3"" urlName=""1001-2-2-1"" writerName=""admin"" creatorName=""admin"" path=""-1,1001,10012,100122,1001221"" isDoc="""">
+					<content><![CDATA[]]></content>
+				</Doc>
+				<Doc id=""1001222"" parentID=""100122"" level=""4"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" + templateId + @""" sortOrder=""1"" createDate=""2012-07-20T18:08:08"" updateDate=""2012-07-20T19:10:52"" nodeName=""Sub 3"" urlName=""1001-2-2-2"" writerName=""admin"" creatorName=""admin"" path=""-1,1001,10012,100122,1001222"" isDoc="""">
+					<content><![CDATA[]]></content>
+				</Doc>
+			</Doc>
+		</Doc>
+		<Doc id=""10013"" parentID=""1001"" level=""2"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" + templateId + @""" sortOrder=""2"" createDate=""2012-07-20T18:08:01"" updateDate=""2012-07-20T18:49:32"" nodeName=""Sub 2"" urlName=""1001-3"" writerName=""admin"" creatorName=""admin"" path=""-1,1001,10013"" isDoc="""">
+            <umbracoUrlAlias><![CDATA[alias42]]></umbracoUrlAlias>
+		</Doc>
+	</Doc>
+	<Doc id=""1002"" parentID=""-1"" level=""1"" writerID=""0"" creatorID=""0"" nodeType=""1234"" template=""" + templateId + @""" sortOrder=""2"" createDate=""2012-07-16T15:26:59"" updateDate=""2012-07-18T14:23:35"" nodeName=""Test"" urlName=""1002"" writerName=""admin"" creatorName=""admin"" path=""-1,1002"" isDoc="""">
+	</Doc>
+	<Doc id=""1003"" parentID=""-1"" level=""1"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" + templateId + @""" sortOrder=""0"" createDate=""2012-06-12T14:13:17"" updateDate=""2012-07-20T18:50:43"" nodeName=""Home"" urlName=""1003"" writerName=""admin"" creatorName=""admin"" path=""-1,1003"" isDoc="""">
+		<content><![CDATA[]]></content>
+		<Doc id=""10031"" parentID=""1003"" level=""2"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" + templateId + @""" sortOrder=""0"" createDate=""2012-07-20T18:06:45"" updateDate=""2012-07-20T19:07:31"" nodeName=""Sub1"" urlName=""1003-1"" writerName=""admin"" creatorName=""admin"" path=""-1,1003,10031"" isDoc="""">
+			<content><![CDATA[<div>This is some content</div>]]></content>
+			<Doc id=""100311"" parentID=""10031"" level=""3"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" + templateId + @""" sortOrder=""0"" createDate=""2012-07-20T18:07:54"" updateDate=""2012-07-20T19:10:27"" nodeName=""Sub2"" urlName=""1003-1-1"" writerName=""admin"" creatorName=""admin"" path=""-1,1003,10031,100311"" isDoc="""">
+				<content><![CDATA[]]></content>
+			</Doc>
+			<Doc id=""100312"" parentID=""10031"" level=""3"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" + templateId + @""" sortOrder=""1"" createDate=""2012-07-20T18:08:08"" updateDate=""2012-07-20T19:10:52"" nodeName=""Sub 3"" urlName=""1003-1-2"" writerName=""admin"" creatorName=""admin"" path=""-1,1003,10031,100312"" isDoc="""">
+				<content><![CDATA[]]></content>
+				<Doc id=""1003121"" parentID=""100312"" level=""4"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" + templateId + @""" sortOrder=""1"" createDate=""2012-07-20T18:08:08"" updateDate=""2012-07-20T19:10:52"" nodeName=""Sub 3"" urlName=""1003-1-2-1"" writerName=""admin"" creatorName=""admin"" path=""-1,1003,10031,100312,1003121"" isDoc="""">
+					<content><![CDATA[]]></content>
+				</Doc>
+				<Doc id=""1003122"" parentID=""100312"" level=""4"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" + templateId + @""" sortOrder=""1"" createDate=""2012-07-20T18:08:08"" updateDate=""2012-07-20T19:10:52"" nodeName=""Sub 3"" urlName=""1003-1-2-2"" writerName=""admin"" creatorName=""admin"" path=""-1,1003,10031,100312,1003122"" isDoc="""">
+					<content><![CDATA[]]></content>
+				</Doc>
+			</Doc>
+		</Doc>
+		<Doc id=""10032"" parentID=""1003"" level=""2"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" + templateId + @""" sortOrder=""1"" createDate=""2012-07-20T18:08:01"" updateDate=""2012-07-20T18:49:32"" nodeName=""Sub 2"" urlName=""1003-2"" writerName=""admin"" creatorName=""admin"" path=""-1,1003,10032"" isDoc="""">
+			<content><![CDATA[<div>This is some content</div>]]></content>
+			<Doc id=""100321"" parentID=""10032"" level=""3"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" + templateId + @""" sortOrder=""0"" createDate=""2012-07-20T18:07:54"" updateDate=""2012-07-20T19:10:27"" nodeName=""Sub2"" urlName=""1003-2-1"" writerName=""admin"" creatorName=""admin"" path=""-1,1003,10032,100321"" isDoc="""">
+				<content><![CDATA[]]></content>
+			</Doc>
+			<Doc id=""100322"" parentID=""10032"" level=""3"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" + templateId + @""" sortOrder=""1"" createDate=""2012-07-20T18:08:08"" updateDate=""2012-07-20T19:10:52"" nodeName=""Sub 3"" urlName=""1003-2-2"" writerName=""admin"" creatorName=""admin"" path=""-1,1003,10032,100322"" isDoc="""">
+				<content><![CDATA[]]></content>
+				<Doc id=""1003221"" parentID=""100322"" level=""4"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" + templateId + @""" sortOrder=""0"" createDate=""2012-07-20T18:08:08"" updateDate=""2012-07-20T19:10:52"" nodeName=""Sub 3"" urlName=""1003-2-2-1"" writerName=""admin"" creatorName=""admin"" path=""-1,1003,10032,100322,1003221"" isDoc="""">
+					<content><![CDATA[]]></content>
+				</Doc>
+				<Doc id=""1003222"" parentID=""100322"" level=""4"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" + templateId + @""" sortOrder=""1"" createDate=""2012-07-20T18:08:08"" updateDate=""2012-07-20T19:10:52"" nodeName=""Sub 3"" urlName=""1003-2-2-2"" writerName=""admin"" creatorName=""admin"" path=""-1,1003,10032,100322,1003222"" isDoc="""">
+					<content><![CDATA[]]></content>
+				</Doc>
+			</Doc>
+		</Doc>
+		<Doc id=""10033"" parentID=""1003"" level=""2"" writerID=""0"" creatorID=""0"" nodeType=""1044"" template=""" + templateId + @""" sortOrder=""2"" createDate=""2012-07-20T18:08:01"" updateDate=""2012-07-20T18:49:32"" nodeName=""Sub 2"" urlName=""1003-3"" writerName=""admin"" creatorName=""admin"" path=""-1,1003,10033"" isDoc="""">
+		</Doc>
+	</Doc>
+</root>";
+        }
+
+		[TestCase("/this/is/my/alias", 1001)]
+		[TestCase("/anotheralias", 1001)]
+		[TestCase("/page2/alias", 10011)]
+		[TestCase("/2ndpagealias", 10011)]
+		[TestCase("/only/one/alias", 100111)]
+		[TestCase("/ONLY/one/Alias", 100111)]
+        [TestCase("/alias43", 100121)]
 		public void Lookup_By_Url_Alias(string urlAsString, int nodeMatch)
 		{
 			var routingContext = GetRoutingContext(urlAsString);
@@ -33,5 +154,42 @@ namespace Umbraco.Tests.Routing
 			Assert.IsTrue(result);
 			Assert.AreEqual(docRequest.PublishedContent.Id, nodeMatch);
 		}
+
+        [TestCase("http://domain1.com/this/is/my/alias", "de-DE", -1001)] // alias to domain's page fails FIXME wanted?
+        [TestCase("http://domain1.com/page2/alias", "de-DE", 10011)] // alias to sub-page works
+        [TestCase("http://domain1.com/en/flux", "en-US", -10011)] // alias to domain's page fails FIXME wanted?
+        [TestCase("http://domain1.com/endanger", "de-DE", 10011)] // alias to sub-page works, even with "en..."
+        [TestCase("http://domain1.com/en/endanger", "en-US", -10011)] // no
+        [TestCase("http://domain1.com/only/one/alias", "de-DE", 100111)] // ok
+        [TestCase("http://domain1.com/entropy", "de-DE", 100111)] // ok
+        [TestCase("http://domain1.com/bar/foo", "de-DE", 100111)] // ok
+        [TestCase("http://domain1.com/en/bar/foo", "en-US", -100111)] // no, alias must include "en/"
+        [TestCase("http://domain1.com/en/bar/nil", "en-US", 100111)] // ok, alias includes "en/"
+        public void Lookup_By_Url_Alias_And_Domain(string inputUrl, string expectedCulture, int expectedNode)
+        {
+            SetDomains1();
+
+            var routingContext = GetRoutingContext(inputUrl);
+            var url = routingContext.UmbracoContext.CleanedUmbracoUrl; //very important to use the cleaned up umbraco url
+            var pcr = new PublishedContentRequest(url, routingContext);
+            // must lookup domain
+            pcr.Engine.FindDomain();
+
+            if (expectedNode > 0)
+                Assert.AreEqual(expectedCulture, pcr.Culture.Name);
+
+            var finder = new ContentFinderByUrlAlias();
+            var result = finder.TryFindDocument(pcr);
+
+            if (expectedNode > 0)
+            {
+                Assert.IsTrue(result);
+                Assert.AreEqual(pcr.PublishedContent.Id, expectedNode);
+            }
+            else
+            {
+                Assert.IsFalse(result);
+            }
+        }
 	}
 }
