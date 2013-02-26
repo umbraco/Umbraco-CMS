d9540bcfc52574b84ec1e6f19a93bc793611aec0
Stephan <sgay@pilotine.com>
2013-02-26 16:52:27 -0100

    Core - cleanup UriExtensions, create unit tests, make it public

--- src/Umbraco.Core/UriExtensions.cs
+++ src/Umbraco.Core/UriExtensions.cs
@@ -3,75 +3,99 @@ using System.Text;
 
 namespace Umbraco.Core
 {
-    internal static class UriExtensions
+    /// <summary>
+    /// Provides extension methods to <see cref="Uri"/>.
+    /// </summary>
+    public static class UriExtensions
     {
-		// Creates a new Uri with a rewritten path
-		// Everything else is unchanged but for the fragment which is lost
-		public static Uri Rewrite(this Uri uri, string path)
+        /// <summary>
+        /// Rewrites the path of uri.
+        /// </summary>
+        /// <param name="uri">The uri.</param>
+        /// <param name="path">The new path, which must begin with a slash.</param>
+        /// <returns>The rewritten uri.</returns>
+        /// <remarks>Everything else remains unchanged, except for the fragment which is removed.</remarks>
+        public static Uri Rewrite(this Uri uri, string path)
 		{
-			if (uri.IsAbsoluteUri)
-			{
-				return new Uri(uri.GetLeftPart(UriPartial.Authority) + path + uri.Query);
-			}
-			else
-			{
-				// cannot get .Query on relative uri (InvalidOperation)
-				var s = uri.OriginalString;
-				var posq = s.IndexOf("?");
-				var posf = s.IndexOf("#");
-				var query = posq < 0 ? null : (posf < 0 ? s.Substring(posq) : s.Substring(posq, posf - posq));
+            if (!path.StartsWith("/"))
+                throw new ArgumentException("Path must start with a slash.", "path");
 
-				return new Uri(path + query, UriKind.Relative);
-			}
+			return uri.IsAbsoluteUri 
+                ? new Uri(uri.GetLeftPart(UriPartial.Authority) + path + uri.Query) 
+                : new Uri(path + uri.GetSafeQuery(), UriKind.Relative);
 		}
 
-		// Creates a new Uri with a rewritten path and query
-		// Everything else is unchanged but for the fragment which is lost
-		public static Uri Rewrite(this Uri uri, string path, string query)
+        /// <summary>
+        /// Rewrites the path and query of a uri.
+        /// </summary>
+        /// <param name="uri">The uri.</param>
+        /// <param name="path">The new path, which must begin with a slash.</param>
+        /// <param name="query">The new query, which must be empty or begin with a question mark.</param>
+        /// <returns>The rewritten uri.</returns>
+        /// <remarks>Everything else remains unchanged, except for the fragment which is removed.</remarks>
+        public static Uri Rewrite(this Uri uri, string path, string query)
 		{
-			if (uri.IsAbsoluteUri)
-			{
-				return new Uri(uri.GetLeftPart(UriPartial.Authority) + path + query);
-			}
-			else
-			{
-				return new Uri(path + query, UriKind.Relative);
-			}
+            if (!path.StartsWith("/"))
+                throw new ArgumentException("Path must start with a slash.", "path");
+            if (query.Length > 0 && !query.StartsWith("?"))
+                throw new ArgumentException("Query must start with a question mark.", "query");
+            if (query == "?")
+                query = "";
+            
+            return uri.IsAbsoluteUri 
+                ? new Uri(uri.GetLeftPart(UriPartial.Authority) + path + query) 
+                : new Uri(path + query, UriKind.Relative);
 		}
 
-		// Gets the absolute path of the Uri
-		// Works both for Absolute and Relative Uris
+        /// <summary>
+        /// Gets the absolute path of the uri, even if the uri is relative.
+        /// </summary>
+        /// <param name="uri">The uri.</param>
+        /// <returns>The absolute path of the uri.</returns>
+        /// <remarks>Default uri.AbsolutePath does not support relative uris.</remarks>
 		public static string GetSafeAbsolutePath(this Uri uri)
 		{
 			if (uri.IsAbsoluteUri)
-			{
 				return uri.AbsolutePath;
-			}
-			else
-			{
-				// cannot get .AbsolutePath on relative uri (InvalidOperation)
-				var s = uri.OriginalString;
-				var posq = s.IndexOf("?");
-				var posf = s.IndexOf("#");
-				var pos = posq > 0 ? posq : (posf > 0 ? posf : 0);
-				var path = pos > 0 ? s.Substring(0, pos) : s;
-				return path;
-			}
+
+            // cannot get .AbsolutePath on relative uri (InvalidOperation)
+            var s = uri.OriginalString;
+            var posq = s.IndexOf("?", StringComparison.Ordinal);
+            var posf = s.IndexOf("#", StringComparison.Ordinal);
+            var pos = posq > 0 ? posq : (posf > 0 ? posf : 0);
+            var path = pos > 0 ? s.Substring(0, pos) : s;
+            return path;
 		}
 
-		public static string GetAbsolutePathDecoded(this Uri uri)
+        /// <summary>
+        /// Gets the decoded, absolute path of the uri.
+        /// </summary>
+        /// <param name="uri">The uri.</param>
+        /// <returns>The absolute path of the uri.</returns>
+        /// <remarks>Only for absolute uris.</remarks>
+        public static string GetAbsolutePathDecoded(this Uri uri)
 		{
 			return System.Web.HttpUtility.UrlDecode(uri.AbsolutePath);
 		}
 
-		public static string GetSafeAbsolutePathDecoded(this Uri uri)
+        /// <summary>
+        /// Gets the decoded, absolute path of the uri, even if the uri is relative.
+        /// </summary>
+        /// <param name="uri">The uri.</param>
+        /// <returns>The absolute path of the uri.</returns>
+        /// <remarks>Default uri.AbsolutePath does not support relative uris.</remarks>
+        public static string GetSafeAbsolutePathDecoded(this Uri uri)
 		{
 			return System.Web.HttpUtility.UrlDecode(uri.GetSafeAbsolutePath());
 		}
 
-		// Creates a new Uri with path ending with a slash
-		// Everything else is unchanged but for the fragment which is lost
-		public static Uri EndPathWithSlash(this Uri uri)
+		/// <summary>
+		/// Rewrites the path of the uri so it ends with a slash.
+		/// </summary>
+		/// <param name="uri">The uri.</param>
+		/// <returns>The rewritten uri.</returns>
+		/// <remarks>Everything else remains unchanged.</remarks>
+        public static Uri EndPathWithSlash(this Uri uri)
 		{
 			var path = uri.GetSafeAbsolutePath();
 			if (uri.IsAbsoluteUri)
@@ -88,10 +112,13 @@ namespace Umbraco.Core
 			return uri;
 		}
 
-		// Creates a new Uri with path trimmed of trailing slash
-		// Everything else is unchanged but for the fragment which is lost
-		// If path is "/" it remains "/".
-		public static Uri TrimPathEndSlash(this Uri uri)
+        /// <summary>
+        /// Rewrites the path of the uri so it does not end with a slash.
+        /// </summary>
+        /// <param name="uri">The uri.</param>
+        /// <returns>The rewritten uri.</returns>
+        /// <remarks>Everything else remains unchanged.</remarks>
+        public static Uri TrimPathEndSlash(this Uri uri)
 		{
 			var path = uri.GetSafeAbsolutePath();
 			if (uri.IsAbsoluteUri)
@@ -107,11 +134,32 @@ namespace Umbraco.Core
 			return uri;
 		}
 
+        /// <summary>
+        /// Transforms a relative uri into an absolute uri.
+        /// </summary>
+        /// <param name="uri">The relative uri.</param>
+        /// <param name="baseUri">The base absolute uri.</param>
+        /// <returns>The absolute uri.</returns>
 		public static Uri MakeAbsolute(this Uri uri, Uri baseUri)
 		{
 			if (uri.IsAbsoluteUri)
 				throw new ArgumentException("Uri is already absolute.", "uri");
-			return new Uri(baseUri.GetLeftPart(UriPartial.Authority) + uri.GetSafeAbsolutePath());
+
+			return new Uri(baseUri.GetLeftPart(UriPartial.Authority) + uri.GetSafeAbsolutePath() + uri.GetSafeQuery());
 		}
+
+        static string GetSafeQuery(this Uri uri)
+        {
+            if (uri.IsAbsoluteUri)
+                return uri.Query;
+
+            // cannot get .Query on relative uri (InvalidOperation)
+            var s = uri.OriginalString;
+            var posq = s.IndexOf("?", StringComparison.Ordinal);
+            var posf = s.IndexOf("#", StringComparison.Ordinal);
+            var query = posq < 0 ? null : (posf < 0 ? s.Substring(posq) : s.Substring(posq, posf - posq));
+
+            return query;
+        }
 	}
 }
