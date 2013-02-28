b017e8690acf5025a2182fd05302e8d04edfcf67
Sebastiaan Janssen <sebastiaan@umbraco.com>
2013-02-28 09:53:57 -0100

    Fix errors in log caused by no prevalues beind selected.

--- src/umbraco.editorControls/DefaultDataKeyValue.cs
+++ src/umbraco.editorControls/DefaultDataKeyValue.cs
@@ -22,15 +22,20 @@ namespace umbraco.editorControls
 			string v = "";
 			try 
 			{
-                IRecordsReader dr = SqlHelper.ExecuteReader("Select [value] from cmsDataTypeprevalues where id in (" + SqlHelper.EscapeString(Value.ToString()) + ")");
+                // Don't query if there's nothing to query for..
+                if (string.IsNullOrWhiteSpace(Value.ToString()) == false)
+                {
+                    IRecordsReader dr = SqlHelper.ExecuteReader("Select [value] from cmsDataTypeprevalues where id in (@id)", SqlHelper.CreateParameter("id", Value.ToString()));
 
-				while (dr.Read()) {
+                    while (dr.Read())
+                    {
 					if (v.Length == 0)
 						v += dr.GetString("value");
 					else
 						v += "," + dr.GetString("value");
 				}
 				dr.Close();
+			} 
 			} 
 			catch {}
 			return d.CreateCDataSection(v);
