c635ec67d02e7787737a37147629caf0030850b9
Stephan <sgay@pilotine.com>
2013-03-01 13:56:11 -0100

    Web.Configuration - import WebRouting infrastructure

---
+++ src/Umbraco.Web/Configuration/WebRouting.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+using System.Configuration;
+using Umbraco.Core.Configuration;
+
+namespace Umbraco.Web.Configuration
+{
+    /// <summary>
+    /// The Web.Routing settings section.
+    /// </summary>
+    [ConfigurationKey("web.routing", ConfigurationKeyType.Umbraco)]
+    internal class WebRouting : UmbracoConfigurationSection
+    {
+    }
+}
