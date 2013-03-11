cb0dd70434e0d35dae69fac45603b20ef2b858a1
Morten Christensen <unknown@kiln.example.com>
2013-03-11 12:44:12 -0100

    Minor change to the SqlSyntaxProvidersResolver in the CoreBootManager as per Shannons review.

--- src/Umbraco.Core/CoreBootManager.cs
+++ src/Umbraco.Core/CoreBootManager.cs
@@ -168,7 +168,7 @@ namespace Umbraco.Core
 				new RepositoryFactory());
 
 		    SqlSyntaxProvidersResolver.Current = new SqlSyntaxProvidersResolver(
-		        PluginManager.Current.ResolveSqlSyntaxProviders())
+                new[] { typeof(MySqlSyntaxProvider), typeof(SqlCeSyntaxProvider), typeof(SqlServerSyntaxProvider) })
 		        {
 		            CanResolveBeforeFrozen = true
 		        };
