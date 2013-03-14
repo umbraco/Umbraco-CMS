aa5588bbd398562c7620363702ec3714171c0545
leekelleher <leekelleher@gmail.com>
2013-03-14 21:55:46 +0000

    Added Constants (static class). Included all GUIDs for PropertyEditors and ObjectTypes.

---
+++ src/Umbraco.Core/Constants-PropertyEditors.cs
@@ -0,0 +1,186 @@
+﻿namespace Umbraco.Core
+{
+	public static partial class Constants
+	{
+		/// <summary>
+		/// Defines the identifiers for Umbraco Property Editors as constants for easy centralized access/management.
+		/// </summary>
+		public static class PropertyEditors
+		{
+			/// <summary>
+			/// Guid for the Checkbox list datatype.
+			/// </summary>
+			public const string CheckBoxList = "B4471851-82B6-4C75-AFA4-39FA9C6A75E9";
+
+			/// <summary>
+			/// Guid for the Color Picker datatype.
+			/// </summary>
+			public const string ColorPicker = "F8D60F68-EC59-4974-B43B-C46EB5677985";
+
+			/// <summary>
+			/// Guid for the Content Picker datatype.
+			/// </summary>
+			public const string ContentPicker = "158AA029-24ED-4948-939E-C3DA209E5FBA";
+
+			/// <summary>
+			/// Guid for the Date datatype.
+			/// </summary>
+			public const string Date = "23E93522-3200-44E2-9F29-E61A6FCBB79A";
+
+			/// <summary>
+			/// Guid for the Date/Time datatype.
+			/// </summary>
+			public const string DateTime = "B6FB1622-AFA5-4BBF-A3CC-D9672A442222";
+
+			/// <summary>
+			/// Guid for the Dictionary Picker datatype.
+			/// </summary>
+			public const string DictionaryPicker = "17B70066-F764-407D-AB05-3717F1E1C513";
+
+			/// <summary>
+			/// Guid for the Dropdown list datatype.
+			/// </summary>
+			public const string DropDownList = "A74EA9C9-8E18-4D2A-8CF6-73C6206C5DA6";
+
+			/// <summary>
+			/// Guid for the Dropdown list multiple datatype.
+			/// </summary>
+			public const string DropDownListMultiple = "928639ED-9C73-4028-920C-1E55DBB68783";
+
+			/// <summary>
+			/// Guid for the Dropdown list multiple, publish keys datatype.
+			/// </summary>
+			public const string DropdownlistMultiplePublishKeys = "928639AA-9C73-4028-920C-1E55DBB68783";
+
+			/// <summary>
+			/// Guid for the Dropdown list, publishing keys datatype.
+			/// </summary>
+			public const string DropdownlistPublishingKeys = "A74EA9E1-8E18-4D2A-8CF6-73C6206C5DA6";
+
+			/// <summary>
+			/// Guid for the Folder browser datatype.
+			/// </summary>
+			public const string FolderBrowser = "CCCD4AE9-F399-4ED2-8038-2E88D19E810C";
+
+			/// <summary>
+			/// Guid for the Image Cropper datatype.
+			/// </summary>
+			public const string ImageCropper = "7A2D436C-34C2-410F-898F-4A23B3D79F54";
+
+			/// <summary>
+			/// Guid for the Integer datatype.
+			/// </summary>
+			public const string Integer = "1413AFCB-D19A-4173-8E9A-68288D2A73B8";
+
+			/// <summary>
+			/// Guid for the Macro Container datatype.
+			/// </summary>
+			public const string MacroContainer = "474FCFF8-9D2D-11DE-ABC6-AD7A56D89593";
+
+			/// <summary>
+			/// Guid for the Media Picker datatype.
+			/// </summary>
+			public const string MediaPicker = "EAD69342-F06D-4253-83AC-28000225583B";
+
+			/// <summary>
+			/// Guid for the Member Picker datatype.
+			/// </summary>
+			public const string MemberPicker = "39F533E4-0551-4505-A64B-E0425C5CE775";
+
+			/// <summary>
+			/// Guid for the Multi-Node Tree Picker datatype
+			/// </summary>
+			public const string MultiNodeTreePicker = "7E062C13-7C41-4AD9-B389-41D88AEEF87C";
+
+			/// <summary>
+			/// Guid for the Multiple Textstring datatype.
+			/// </summary>
+			public const string MultipleTextstring = "5359AD0B-06CC-4182-92BD-0A9117448D3F";
+
+			/// <summary>
+			/// Guid for the No edit datatype.
+			/// </summary>
+			public const string NoEdit = "6C738306-4C17-4D88-B9BD-6546F3771597";
+
+			/// <summary>
+			/// Guid for the Picker Relations datatype.
+			/// </summary>
+			public const string PickerRelations = "83396FF2-2E39-4A90-9066-17F5F3989374";
+
+			/// <summary>
+			/// Guid for the Radiobutton list datatype.
+			/// </summary>
+			public const string RadioButtonList = "A52C7C1C-C330-476E-8605-D63D3B84B6A6";
+
+			/// <summary>
+			/// Guid for the Related Links datatype.
+			/// </summary>
+			public const string RelatedLinks = "71B8AD1A-8DC2-425C-B6B8-FAA158075E63";
+
+			/// <summary>
+			/// Guid for the Slider datatype.
+			/// </summary>
+			public const string Slider = "29E790E6-26B3-438A-B21F-908663A0B19E";
+
+			/// <summary>
+			/// Guid for the Tags datatype.
+			/// </summary>
+			public const string Tags = "4023E540-92F5-11DD-AD8B-0800200C9A66";
+
+			/// <summary>
+			/// Guid for the Textbox datatype.
+			/// </summary>
+			public const string Textbox = "EC15C1E5-9D90-422A-AA52-4F7622C63BEA";
+
+			/// <summary>
+			/// Guid for the Textbox multiple datatype.
+			/// </summary>
+			public const string TextboxMultiple = "67DB8357-EF57-493E-91AC-936D305E0F2A";
+
+			/// <summary>
+			/// Guid for the TinyMCE v3 wysiwyg datatype.
+			/// </summary>
+			public const string TinyMCEv3 = "5E9B75AE-FACE-41C8-B47E-5F4B0FD82F83";
+
+			/// <summary>
+			/// Guid for the TinyMCE wysiwyg (deprecated, upgrade to tinymce v3!) datatype.
+			/// </summary>
+			public const string TinyMCE = "83722133-F80C-4273-BDB6-1BEFAA04A612";
+
+			/// <summary>
+			/// Guid for the True/False (Ja/Nej) datatype.
+			/// </summary>
+			public const string TrueFalse = "38B352C1-E9F8-4FD8-9324-9A2EAB06D97A";
+
+			/// <summary>
+			/// Guid for the Ultimate Picker datatype.
+			/// </summary>
+			public const string UltimatePicker = "CDBF0B5D-5CB2-445F-BC12-FCAAEC07CF2C";
+
+			/// <summary>
+			/// Guid for the UltraSimpleEditor datatype.
+			/// </summary>
+			public const string UltraSimpleEditor = "60B7DABF-99CD-41EB-B8E9-4D2E669BBDE9";
+
+			/// <summary>
+			/// Guid for the Umbraco Usercontrol Wrapper datatype.
+			/// </summary>
+			public const string UmbracoUserControlWrapper = "D15E1281-E456-4B24-AA86-1DDA3E4299D5";
+
+			/// <summary>
+			/// Guid for the Upload field datatype.
+			/// </summary>
+			public const string UploadField = "5032A6E6-69E3-491D-BB28-CD31CD11086C";
+
+			/// <summary>
+			/// Guid for the XPath CheckBoxList datatype.
+			/// </summary>
+			public const string XPathCheckBoxList = "34451D92-D270-49BA-8C7F-EE55BFEEE1CB";
+
+			/// <summary>
+			/// Guid for the XPath DropDownList datatype.
+			/// </summary>
+			public const string XPathDropDownList = "173A96AE-00ED-4A7C-9F76-4B53D4A0A1B9";
+		}
+	}
+}
---
+++ src/Umbraco.Core/Constants.cs
@@ -0,0 +1,7 @@
+﻿namespace Umbraco.Core
+{
+	public static partial class Constants
+	{
+		// generic constants can go here
+	}
+}
