db3f21429eae1cc8f39fba0fd4cd65148e060f54
Shannon Deminick <sdeminick@gmail.com>
2013-03-15 11:31:11 +0600

    Fixes infinite loop bug with string trimming.

--- src/Umbraco.Core/StringExtensions.cs
+++ src/Umbraco.Core/StringExtensions.cs
@@ -179,6 +179,8 @@ namespace Umbraco.Core
         public static string TrimEnd(this string value, string forRemoving)
         {
             if (string.IsNullOrEmpty(value)) return value;
+            if (string.IsNullOrEmpty(forRemoving)) return value;
+
             while (value.EndsWith(forRemoving, StringComparison.InvariantCultureIgnoreCase))
             {
                 value = value.Remove(value.LastIndexOf(forRemoving, StringComparison.InvariantCultureIgnoreCase));
@@ -189,6 +191,8 @@ namespace Umbraco.Core
         public static string TrimStart(this string value, string forRemoving)
         {
             if (string.IsNullOrEmpty(value)) return value;
+            if (string.IsNullOrEmpty(forRemoving)) return value;
+
             while (value.StartsWith(forRemoving, StringComparison.InvariantCultureIgnoreCase))
             {
                 value = value.Substring(forRemoving.Length);
