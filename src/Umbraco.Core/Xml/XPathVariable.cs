c94eb4328fb4505625f99d647ed76d34df57617f
Stephan <sgay@pilotine.com>
2013-03-19 17:34:28 -0100

    Core.Xml - document + public XPathVariable

--- src/Umbraco.Core/Xml/XPathVariable.cs
+++ src/Umbraco.Core/Xml/XPathVariable.cs
@@ -2,11 +2,27 @@
 
 namespace Umbraco.Core.Xml
 {
-	internal class XPathVariable
+    /// <summary>
+    /// Represents a variable in an XPath query.
+    /// </summary>
+    /// <remarks>The name must be <c>foo</c> in the constructor and <c>$foo</c> in the XPath query.</remarks>
+	public class XPathVariable
 	{
-		public string Name { get; set; }
-		public string Value { get; set; }
+        /// <summary>
+        /// Gets or sets the name of the variable.
+        /// </summary>
+		public string Name { get; private set; }
 
+        /// <summary>
+        /// Gets or sets the value of the variable.
+        /// </summary>
+		public string Value { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="XPathVariable"/> class with a name and a value.
+        /// </summary>
+        /// <param name="name"></param>
+        /// <param name="value"></param>
 		public XPathVariable(string name, string value)
 		{
 			Name = name;
