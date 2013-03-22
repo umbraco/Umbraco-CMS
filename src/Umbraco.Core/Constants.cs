6b07ee83eb5648e554678349956d48c29ccb15db
leekelleher <leekelleher@gmail.com>
2013-03-22 13:11:27 +0000

    Added constants for Application (section aliases), System (node IDs) and MediaType aliases.

--- src/Umbraco.Core/Constants.cs
+++ src/Umbraco.Core/Constants.cs
@@ -1,5 +1,8 @@
 ﻿namespace Umbraco.Core
 {
+	/// <summary>
+	/// Constants all the identifiers within the Umbraco core.
+	/// </summary>
 	public static partial class Constants
 	{
 		// generic constants can go here
