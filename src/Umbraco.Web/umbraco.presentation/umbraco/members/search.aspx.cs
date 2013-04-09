4ccb655f7332a527276d581c894c496f09d7f5fe
Shannon Deminick <sdeminick@gmail.com>
2013-04-10 02:43:03 +0600

    updates #U4-2070 for members/search.aspx

--- src/Umbraco.Web/umbraco.presentation/umbraco/members/search.aspx.cs
+++ src/Umbraco.Web/umbraco.presentation/umbraco/members/search.aspx.cs
@@ -16,6 +16,9 @@ namespace umbraco.presentation.members
 
     public partial class search : BasePages.UmbracoEnsuredPage
     {
-
+        public search()
+        {
+            CurrentApp = DefaultApps.member.ToString();
+        }
     }
 }
