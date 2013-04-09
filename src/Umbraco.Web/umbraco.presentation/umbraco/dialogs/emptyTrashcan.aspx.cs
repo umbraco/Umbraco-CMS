bad284515c8094c62d68f5a5cfb89111659ae2d0
Shannon Deminick <sdeminick@gmail.com>
2013-04-09 07:32:13 +0600

    Fixes: #U4-2076

--- src/Umbraco.Web/umbraco.presentation/umbraco/dialogs/emptyTrashcan.aspx.cs
+++ src/Umbraco.Web/umbraco.presentation/umbraco/dialogs/emptyTrashcan.aspx.cs
@@ -6,11 +6,24 @@ namespace umbraco.presentation.dialogs
 {
     public partial class emptyTrashcan : UmbracoEnsuredPage
     {
+
+        protected override void OnInit(EventArgs e)
+        {
+            base.OnInit(e);
+
+            var recycleBinType = helper.Request("type");
+            if (!ValidateUserApp(recycleBinType))
+            {
+                throw new InvalidOperationException("The user does not have access to the requested app");
+            }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
         }
 
-        protected override void OnPreRender(EventArgs e) {
+        protected override void OnPreRender(EventArgs e)
+        {
             base.OnPreRender(e);
 
             ScriptManager.GetCurrent(Page).Services.Add(new ServiceReference("../webservices/trashcan.asmx"));
