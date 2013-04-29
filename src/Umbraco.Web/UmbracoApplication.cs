aba4a9b07cffbce456667bccf8c234cd23a740cd
Shannon Deminick <sdeminick@gmail.com>
2013-04-28 16:15:19 -1000

    oops, removed test code

--- src/Umbraco.Web/UmbracoApplication.cs
+++ src/Umbraco.Web/UmbracoApplication.cs
@@ -8,30 +8,11 @@ using System.Web.Hosting;
 using System.Web.Mvc;
 using Umbraco.Core;
 using Umbraco.Core.Logging;
-using Umbraco.Web.Mvc;
 using Umbraco.Web.Routing;
 using umbraco.businesslogic;
 
 namespace Umbraco.Web
 {
-    public class CustomRenderController : RenderMvcController
-    {
-        public override ActionResult Index(Models.RenderModel model)
-        {
-            HttpContext.Response.ContentType = "text/plain";
-            return base.Index(model);
-        }
-    }
-
-    public class CustomApplicationEventHandler : ApplicationEventHandler
-    {
-        protected override void ApplicationStarting(UmbracoApplicationBase umbracoApplication, ApplicationContext applicationContext)
-        {
-            DefaultRenderMvcControllerResolver.Current.SetDefaultControllerType(typeof(CustomRenderController));
-            base.ApplicationStarting(umbracoApplication, applicationContext);
-        }
-    }
-
 	/// <summary>
 	/// The Umbraco global.asax class
 	/// </summary>
