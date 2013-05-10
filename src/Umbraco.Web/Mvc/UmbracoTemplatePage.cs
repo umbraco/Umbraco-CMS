191d744ec6098b269bdf85c660e572b62067cb95
NielsHartvig@UMBRACORATI.localdomain <unknown@kiln.example.com>
2013-05-10 10:38:59 -0200

    Adds profiling support to MVC views

--- src/Umbraco.Web/Mvc/UmbracoTemplatePage.cs
+++ src/Umbraco.Web/Mvc/UmbracoTemplatePage.cs
@@ -10,60 +10,60 @@ using Umbraco.Web.Models;
 
 namespace Umbraco.Web.Mvc
 {
-	/// <summary>
-	/// The View that front-end templates inherit from
-	/// </summary>
-	public abstract class UmbracoTemplatePage : UmbracoViewPage<RenderModel>
-	{
-		protected UmbracoTemplatePage()
-		{
+    /// <summary>
+    /// The View that front-end templates inherit from
+    /// </summary>
+    public abstract class UmbracoTemplatePage : UmbracoViewPage<RenderModel>
+    {
+        protected UmbracoTemplatePage()
+        {
 
-		}
+        }
 
-		protected override void InitializePage()
-		{
-			base.InitializePage();
-			//set the model to the current node if it is not set, this is generally not the case
-			if (Model != null)
-			{
-				////this.ViewData.Model = Model;
-				//var backingItem = new DynamicBackingItem(Model.CurrentNode);
-				var dynamicNode = new DynamicPublishedContent(Model.Content);
-				CurrentPage = dynamicNode.AsDynamic();
-			}
-		}
+        protected override void InitializePage()
+        {
+            base.InitializePage();
+            //set the model to the current node if it is not set, this is generally not the case
+            if (Model != null)
+            {
+                ////this.ViewData.Model = Model;
+                //var backingItem = new DynamicBackingItem(Model.CurrentNode);
+                var dynamicNode = new DynamicPublishedContent(Model.Content);
+                CurrentPage = dynamicNode.AsDynamic();
+            }
+        }
 
-		protected override void SetViewData(System.Web.Mvc.ViewDataDictionary viewData)
-		{
-			//Here we're going to check if the viewData's model is of IPublishedContent, this is basically just a helper for
-			//syntax on the front-end so we can just pass in an IPublishedContent object to partial views that inherit from
-			//UmbracoTemplatePage. Then we're going to manually contruct a RenderViewModel to pass back in to SetViewData			
-			if (viewData.Model is IPublishedContent)
-			{
-				//change the model to a RenderModel and auto set the culture
-				viewData.Model = new RenderModel((IPublishedContent)viewData.Model, UmbracoContext.PublishedContentRequest.Culture);
-			}
+        protected override void SetViewData(System.Web.Mvc.ViewDataDictionary viewData)
+        {
+            //Here we're going to check if the viewData's model is of IPublishedContent, this is basically just a helper for
+            //syntax on the front-end so we can just pass in an IPublishedContent object to partial views that inherit from
+            //UmbracoTemplatePage. Then we're going to manually contruct a RenderViewModel to pass back in to SetViewData			
+            if (viewData.Model is IPublishedContent)
+            {
+                //change the model to a RenderModel and auto set the culture
+                viewData.Model = new RenderModel((IPublishedContent)viewData.Model, UmbracoContext.PublishedContentRequest.Culture);
+            }
 
-			base.SetViewData(viewData);
-		}
+            base.SetViewData(viewData);
+        }
 
-		/// <summary>
-		/// Returns the a DynamicPublishedContent object
-		/// </summary>
-		public dynamic CurrentPage { get; private set; }
+        /// <summary>
+        /// Returns the a DynamicPublishedContent object
+        /// </summary>
+        public dynamic CurrentPage { get; private set; }
 
-		private UmbracoHelper _helper;
+        private UmbracoHelper _helper;
 
-		/// <summary>
-		/// Gets an UmbracoHelper
-		/// </summary>
-		/// <remarks>
-		/// This ensures that the UmbracoHelper is constructed with the content model of this view
-		/// </remarks>
-		public override UmbracoHelper Umbraco
-		{
-			get { return _helper ?? (_helper = new UmbracoHelper(UmbracoContext, Model.Content)); }
-		}
+        /// <summary>
+        /// Gets an UmbracoHelper
+        /// </summary>
+        /// <remarks>
+        /// This ensures that the UmbracoHelper is constructed with the content model of this view
+        /// </remarks>
+        public override UmbracoHelper Umbraco
+        {
+            get { return _helper ?? (_helper = new UmbracoHelper(UmbracoContext, Model.Content)); }
+        }
 
         /// <summary>
         /// This will detect the end /body tag and insert the preview badge if in preview mode
@@ -72,26 +72,38 @@ namespace Umbraco.Web.Mvc
         public override void WriteLiteral(object value)
         {
             // filter / add preview banner
-            if (Response.ContentType.InvariantEquals("text/html") && UmbracoContext.Current.InPreviewMode) // ASP.NET default value
+            if (Response.ContentType.InvariantEquals("text/html")) // ASP.NET default value
             {
-                var text = value.ToString().ToLowerInvariant();
-                int pos = text.IndexOf("</body>");
-                if (pos > -1)
-                {
-                    var htmlBadge =
-                        String.Format(UmbracoSettings.PreviewBadge,
-                            IOHelper.ResolveUrl(SystemDirectories.Umbraco),
-                            IOHelper.ResolveUrl(SystemDirectories.UmbracoClient),
-                            Server.UrlEncode(UmbracoContext.Current.HttpContext.Request.Path));
 
-                    text = text.Substring(0, pos) + htmlBadge + text.Substring(pos, text.Length - pos);
-                    base.WriteLiteral(text);
-                    return;
+                if (UmbracoContext.Current.IsDebug || UmbracoContext.Current.InPreviewMode)
+                {
+                    var text = value.ToString().ToLowerInvariant();
+                    int pos = text.IndexOf("</body>");
+                    if (pos > -1)
+                    {
+                        if (UmbracoContext.Current.InPreviewMode)
+                        {
+                            var htmlBadge =
+                                String.Format(UmbracoSettings.PreviewBadge,
+                                    IOHelper.ResolveUrl(SystemDirectories.Umbraco),
+                                    IOHelper.ResolveUrl(SystemDirectories.UmbracoClient),
+                                    Server.UrlEncode(UmbracoContext.Current.HttpContext.Request.Path));
+
+                            text = text.Substring(0, pos) + htmlBadge + text.Substring(pos, text.Length - pos);
+                        }
+                        else
+                        {
+                            var profilerMarkup = Core.Profiling.Profiler.Instance.Render();
+                            text = text.Substring(0, pos) + profilerMarkup + text.Substring(pos, text.Length - pos);
+                        }
+                        base.WriteLiteral(text);
+                        return;
+                    }
                 }
             }
 
             base.WriteLiteral(value);
         }
 
-	}
+    }
 }
