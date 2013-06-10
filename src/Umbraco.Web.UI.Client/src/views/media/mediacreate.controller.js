fa9c259a8424fe1ebbfeaf690b344d698f00a107
Shannon <unknown@kiln.example.com>
2013-06-10 13:19:45 -0200

    Adds media views and controllers

---
+++ src/Umbraco.Web.UI.Client/src/views/media/mediacreate.controller.js
@@ -0,0 +1,6 @@
+function mediaCreateController ($scope, $routeParams,contentTypeResource) {
+    $scope.allowedTypes  = contentTypeResource.getAllowedTypes($scope.currentNode.id);
+}
+
+angular.module('umbraco')
+    .controller("Umbraco.Editors.ContentCreateController", mediaCreateController);
