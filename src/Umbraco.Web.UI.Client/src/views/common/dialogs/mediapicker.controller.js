77c8b22fe1af3cc737a362418d05027759c2ee18
Shannon <unknown@kiln.example.com>
2013-06-10 13:01:17 -0200

    Merges in custom controller changes.

--- src/Umbraco.Web.UI.Client/src/views/common/dialogs/mediapicker.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/common/dialogs/mediapicker.controller.js
@@ -1,6 +1,22 @@
 //used for the media picker dialog
 angular.module("umbraco")
-.controller("Umbraco.Dialogs.MediaPickerController",
-	function ($scope, mediaResource) {	
-	$scope.images = mediaResource.rootMedia();
-});
+    .controller("Umbraco.Dialogs.MediaPickerController",
+        function($scope, mediaResource) {
+
+            mediaResource.rootMedia()
+                .then(function(data) {
+                    $scope.images = data;
+                });
+
+            $scope.selectMediaItem = function(image) {
+                if (image.contentTypeAlias.toLowerCase() == 'folder') {
+                    mediaResource.getChildren(image.id)
+                        .then(function(data) {
+                            $scope.images = data;
+                        });
+                } else if (image.contentTypeAlias.toLowerCase() == 'image') {
+                    $scope.select(image);
+                }
+            };
+
+        });
