15ea17e6d6c70612a31c8cd8eed993483897e77a
Shannon <sdeminick@gmail.com>
2013-06-18 12:02:34 +1000

    started on avatar directive

---
+++ src/Umbraco.Web.UI.Client/src/common/directives/umbavatar.directive.js
@@ -0,0 +1,14 @@
+﻿/**
+* @ngdoc directive 
+* @name umbraco.directives:login 
+* @restrict E
+**/
+function avatarDirective() {
+    return {
+        restrict: "E",    // restrict to an element
+        replace: true,   // replace the html element with the template
+        templateUrl: 'views/directives/umb-avatar.html'        
+    };
+}
+
+angular.module('umbraco.directives').directive("umbAvatar", avatarDirective);
