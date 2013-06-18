1ca883631e10fe065e5599d597ea579a7902b0ae
Per Ploug <pph@umbraco.dk>
2013-06-18 11:14:16 +0200

    Media Resource Mock returns promise

--- src/Umbraco.Web.UI.Client/src/common/mocks/resources/media.resource.js
+++ src/Umbraco.Web.UI.Client/src/common/mocks/resources/media.resource.js
@@ -5,14 +5,13 @@ angular.module('umbraco.mocks.resources')
         rootMedia: function(){
 
           var deferred = $q.defer();
-
           deferred.resolve(
             [
               {id: 1234, src: "/Media/boston.jpg", thumbnail: "/Media/boston.jpg" },
               {src: "/Media/bird.jpg", thumbnail: "/Media/bird.jpg" },
               {src: "/Media/frog.jpg", thumbnail: "/Media/frog.jpg" }
             ]);
-          
+
           return deferred.promise;
       }
   };
