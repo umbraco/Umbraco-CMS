e501e7fb987f8d6a6e77a66f3fab3c3d9b06e34c
Per Ploug <pph@umbraco.dk>
2013-06-18 10:58:04 +0200

    chore Clean up the build/belle directory

--- src/Umbraco.Web.UI.Client/build/belle/views/content/contentcreate.controller.js
+++
@@ -1,5 +0,0 @@
-angular.module('umbraco')
-.controller("Umbraco.Editors.ContentCreateController",
-	function ($scope, $routeParams,contentTypeResource) {
-	$scope.allowedTypes  = contentTypeResource.getAllowedTypes($scope.currentNode.id);
-});
