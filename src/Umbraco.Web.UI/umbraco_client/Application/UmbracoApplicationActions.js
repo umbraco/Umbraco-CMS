7f22809ebfe96ff9dc06fb4e20fca2041d1b643c
Phil Dye <codeplex@user.history>
2013-06-19 17:55:00 +0200

    U4-1478 Create button still visible when not active

--- src/Umbraco.Web.UI/umbraco_client/Application/UmbracoApplicationActions.js
+++ src/Umbraco.Web.UI/umbraco_client/Application/UmbracoApplicationActions.js
@@ -111,16 +111,16 @@ Umbraco.Application.Actions = function () {
             this._currApp = whichApp.toLowerCase();
 
             if (this._currApp != 'media' && this._currApp != 'content' && this._currApp != 'member') {
-                jQuery("#buttonCreate").attr("disabled", "true");
+                jQuery("#buttonCreate").attr("disabled", "true").fadeOut(400);
                 jQuery("#FindDocuments .umbracoSearchHolder").fadeOut(400);
             }
             else {
                 // create button should still remain disabled for the memebers section
                 if (this._currApp == 'member') {
-                    jQuery("#buttonCreate").attr("disabled", "true");
+                    jQuery("#buttonCreate").attr("disabled", "true").css("display", "inline-block").css("visibility", "hidden");
                 }
                 else {
-                    jQuery("#buttonCreate").removeAttr("disabled");
+                    jQuery("#buttonCreate").removeAttr("disabled").fadeIn(500).css("visibility", "visible");
                 }
                 jQuery("#FindDocuments .umbracoSearchHolder").fadeIn(500);
                 //need to set the recycle bin node id based on app
