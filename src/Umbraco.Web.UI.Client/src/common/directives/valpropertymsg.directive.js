43972a66c22dfe7f927054b7ca0a9d30a0a20458
Shannon <sdeminick@gmail.com>
2013-06-25 22:53:59 +1000

    another tweak to property level validation

--- src/Umbraco.Web.UI.Client/src/common/directives/valpropertymsg.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/valpropertymsg.directive.js
@@ -62,7 +62,7 @@ function valPropertyMsg(angularHelper, serverValidationService) {
                 if (hasError && scope.errorMsg === "") {
                     scope.errorMsg = serverValidationService.getError(currentProperty, "");
                 }
-                else {
+                else if (!hasError) {
                     scope.errorMsg = "";
                 }
             });
