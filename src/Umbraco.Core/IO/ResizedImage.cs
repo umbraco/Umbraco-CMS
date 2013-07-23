739a6c47a45e7f8a1cdeac117190cf06d1d3c2b3
Shannon <sdeminick@gmail.com>
2013-07-23 17:49:54 +1000

    Obsoletes UmbracoFile with new UmbracoMediaFile in the Core assembly and wraps the legacy class to use the new one. Improves performance of UmbracoMediaFile by ensuring any IO is lazy loaded and then saved to a local property so it doesn't need to re-read the file for things like length/size.

---
+++ src/Umbraco.Core/IO/ResizedImage.cs
@@ -0,0 +1,20 @@
+﻿namespace Umbraco.Core.IO
+{
+    internal class ResizedImage
+    {
+        public ResizedImage()
+        {
+        }
+
+        public ResizedImage(int width, int height, string fileName)
+        {
+            Width = width;
+            Height = height;
+            FileName = fileName;
+        }
+
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public string FileName { get; set; }
+    }
+}
