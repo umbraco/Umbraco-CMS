c49becea53079e696df9a0e16a3c953142d84659
Morten Christensen <mc@sitereactor.dk>
2013-07-26 12:35:40 +0200

    Cleanup of unused using statements in a few files

--- src/umbraco.MacroEngines/RazorDynamicNode/RazorLibraryCore.cs
+++ src/umbraco.MacroEngines/RazorDynamicNode/RazorLibraryCore.cs
@@ -1,18 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 using System.Web.Mvc;
-using Umbraco.Core.Dynamics;
-using Umbraco.Core.Models;
 using Umbraco.Web;
-using Umbraco.Web.Models;
 using umbraco.interfaces;
 using System.Xml.Linq;
 using System.Xml.XPath;
 using System.Web;
-using System.IO;
-using HtmlAgilityPack;
 
 namespace umbraco.MacroEngines.Library
 {
