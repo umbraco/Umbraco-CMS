b9cb3de16777d1c08380147aa6c46164cc01bc06
Shannon <sdeminick@gmail.com>
2013-07-31 17:06:45 +1000

    updates uri extensions + tests

--- src/Umbraco.Core/UriExtensions.cs
+++ src/Umbraco.Core/UriExtensions.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Web;
 using Umbraco.Core.Configuration;
+using Umbraco.Core.IO;
 
 namespace Umbraco.Core
 {
@@ -12,6 +13,38 @@ namespace Umbraco.Core
     /// </summary>
     public static class UriExtensions
     {
+        /// <summary>
+        /// Checks if the current uri is a back office request
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        internal static bool IsBackOfficeRequest(this Uri url)
+        {
+            var authority = url.GetLeftPart(UriPartial.Authority);
+            var afterAuthority = url.GetLeftPart(UriPartial.Query)
+                                    .TrimStart(authority)
+                                    .TrimStart("/");
+
+            //check if this is in the umbraco back office
+            return afterAuthority.InvariantStartsWith(GlobalSettings.Path.TrimStart("/"));
+        }
+
+        /// <summary>
+        /// Checks if the current uri is an install request
+        /// </summary>
+        /// <param name="url"></param>
+        /// <returns></returns>
+        internal static bool IsInstallerRequest(this Uri url)
+        {
+            var authority = url.GetLeftPart(UriPartial.Authority);
+            var afterAuthority = url.GetLeftPart(UriPartial.Query)
+                                    .TrimStart(authority)
+                                    .TrimStart("/");
+
+            //check if this is in the umbraco back office
+            return afterAuthority.InvariantStartsWith(IOHelper.ResolveUrl("~/install").TrimStart("/"));
+        }
+
         /// <summary>
         /// Checks if the uri is a request for the default back office page
         /// </summary>
@@ -38,7 +71,7 @@ namespace Umbraco.Core
         /// <returns></returns>
         internal static bool IsClientSideRequest(this Uri url)
         {
-            var toIgnore = new[] { ".js", ".css", ".ico", ".png", ".jpg", ".jpeg", ".gif" };
+            var toIgnore = new[] { ".js", ".css", ".ico", ".png", ".jpg", ".jpeg", ".gif", ".html", ".svg" };
             return toIgnore.Any(x => Path.GetExtension(url.LocalPath).InvariantEquals(x));
         }
 
@@ -50,14 +83,14 @@ namespace Umbraco.Core
         /// <returns>The rewritten uri.</returns>
         /// <remarks>Everything else remains unchanged, except for the fragment which is removed.</remarks>
         public static Uri Rewrite(this Uri uri, string path)
-		{
+        {
             if (!path.StartsWith("/"))
                 throw new ArgumentException("Path must start with a slash.", "path");
 
-			return uri.IsAbsoluteUri 
-                ? new Uri(uri.GetLeftPart(UriPartial.Authority) + path + uri.Query) 
+            return uri.IsAbsoluteUri
+                ? new Uri(uri.GetLeftPart(UriPartial.Authority) + path + uri.Query)
                 : new Uri(path + uri.GetSafeQuery(), UriKind.Relative);
-		}
+        }
 
         /// <summary>
         /// Rewrites the path and query of a uri.
@@ -68,18 +101,18 @@ namespace Umbraco.Core
         /// <returns>The rewritten uri.</returns>
         /// <remarks>Everything else remains unchanged, except for the fragment which is removed.</remarks>
         public static Uri Rewrite(this Uri uri, string path, string query)
-		{
+        {
             if (!path.StartsWith("/"))
                 throw new ArgumentException("Path must start with a slash.", "path");
             if (query.Length > 0 && !query.StartsWith("?"))
                 throw new ArgumentException("Query must start with a question mark.", "query");
             if (query == "?")
                 query = "";
-            
-            return uri.IsAbsoluteUri 
-                ? new Uri(uri.GetLeftPart(UriPartial.Authority) + path + query) 
+
+            return uri.IsAbsoluteUri
+                ? new Uri(uri.GetLeftPart(UriPartial.Authority) + path + query)
                 : new Uri(path + query, UriKind.Relative);
-		}
+        }
 
         /// <summary>
         /// Gets the absolute path of the uri, even if the uri is relative.
@@ -87,10 +120,10 @@ namespace Umbraco.Core
         /// <param name="uri">The uri.</param>
         /// <returns>The absolute path of the uri.</returns>
         /// <remarks>Default uri.AbsolutePath does not support relative uris.</remarks>
-		public static string GetSafeAbsolutePath(this Uri uri)
-		{
-			if (uri.IsAbsoluteUri)
-				return uri.AbsolutePath;
+        public static string GetSafeAbsolutePath(this Uri uri)
+        {
+            if (uri.IsAbsoluteUri)
+                return uri.AbsolutePath;
 
             // cannot get .AbsolutePath on relative uri (InvalidOperation)
             var s = uri.OriginalString;
@@ -99,7 +132,7 @@ namespace Umbraco.Core
             var pos = posq > 0 ? posq : (posf > 0 ? posf : 0);
             var path = pos > 0 ? s.Substring(0, pos) : s;
             return path;
-		}
+        }
 
         /// <summary>
         /// Gets the decoded, absolute path of the uri.
@@ -108,9 +141,9 @@ namespace Umbraco.Core
         /// <returns>The absolute path of the uri.</returns>
         /// <remarks>Only for absolute uris.</remarks>
         public static string GetAbsolutePathDecoded(this Uri uri)
-		{
-			return System.Web.HttpUtility.UrlDecode(uri.AbsolutePath);
-		}
+        {
+            return System.Web.HttpUtility.UrlDecode(uri.AbsolutePath);
+        }
 
         /// <summary>
         /// Gets the decoded, absolute path of the uri, even if the uri is relative.
@@ -119,32 +152,32 @@ namespace Umbraco.Core
         /// <returns>The absolute path of the uri.</returns>
         /// <remarks>Default uri.AbsolutePath does not support relative uris.</remarks>
         public static string GetSafeAbsolutePathDecoded(this Uri uri)
-		{
-			return System.Web.HttpUtility.UrlDecode(uri.GetSafeAbsolutePath());
-		}
+        {
+            return System.Web.HttpUtility.UrlDecode(uri.GetSafeAbsolutePath());
+        }
 
-		/// <summary>
-		/// Rewrites the path of the uri so it ends with a slash.
-		/// </summary>
-		/// <param name="uri">The uri.</param>
-		/// <returns>The rewritten uri.</returns>
-		/// <remarks>Everything else remains unchanged.</remarks>
+        /// <summary>
+        /// Rewrites the path of the uri so it ends with a slash.
+        /// </summary>
+        /// <param name="uri">The uri.</param>
+        /// <returns>The rewritten uri.</returns>
+        /// <remarks>Everything else remains unchanged.</remarks>
         public static Uri EndPathWithSlash(this Uri uri)
-		{
-			var path = uri.GetSafeAbsolutePath();
-			if (uri.IsAbsoluteUri)
-			{
-				if (path != "/" && !path.EndsWith("/"))
-					uri = new Uri(uri.GetLeftPart(UriPartial.Authority) + path + "/" + uri.Query);
-				return uri;
-			}
-			else
-			{
-				if (path != "/" && !path.EndsWith("/"))
-					uri = new Uri(path + "/" + uri.Query, UriKind.Relative);
-			}
-			return uri;
-		}
+        {
+            var path = uri.GetSafeAbsolutePath();
+            if (uri.IsAbsoluteUri)
+            {
+                if (path != "/" && !path.EndsWith("/"))
+                    uri = new Uri(uri.GetLeftPart(UriPartial.Authority) + path + "/" + uri.Query);
+                return uri;
+            }
+            else
+            {
+                if (path != "/" && !path.EndsWith("/"))
+                    uri = new Uri(path + "/" + uri.Query, UriKind.Relative);
+            }
+            return uri;
+        }
 
         /// <summary>
         /// Rewrites the path of the uri so it does not end with a slash.
@@ -153,20 +186,20 @@ namespace Umbraco.Core
         /// <returns>The rewritten uri.</returns>
         /// <remarks>Everything else remains unchanged.</remarks>
         public static Uri TrimPathEndSlash(this Uri uri)
-		{
-			var path = uri.GetSafeAbsolutePath();
-			if (uri.IsAbsoluteUri)
-			{
-				if (path != "/")
-					uri = new Uri(uri.GetLeftPart(UriPartial.Authority) + path.TrimEnd('/') + uri.Query);
-			}
-			else
-			{
-				if (path != "/")
-					uri = new Uri(path.TrimEnd('/') + uri.Query, UriKind.Relative);
-			}
-			return uri;
-		}
+        {
+            var path = uri.GetSafeAbsolutePath();
+            if (uri.IsAbsoluteUri)
+            {
+                if (path != "/")
+                    uri = new Uri(uri.GetLeftPart(UriPartial.Authority) + path.TrimEnd('/') + uri.Query);
+            }
+            else
+            {
+                if (path != "/")
+                    uri = new Uri(path.TrimEnd('/') + uri.Query, UriKind.Relative);
+            }
+            return uri;
+        }
 
         /// <summary>
         /// Transforms a relative uri into an absolute uri.
@@ -174,13 +207,13 @@ namespace Umbraco.Core
         /// <param name="uri">The relative uri.</param>
         /// <param name="baseUri">The base absolute uri.</param>
         /// <returns>The absolute uri.</returns>
-		public static Uri MakeAbsolute(this Uri uri, Uri baseUri)
-		{
-			if (uri.IsAbsoluteUri)
-				throw new ArgumentException("Uri is already absolute.", "uri");
+        public static Uri MakeAbsolute(this Uri uri, Uri baseUri)
+        {
+            if (uri.IsAbsoluteUri)
+                throw new ArgumentException("Uri is already absolute.", "uri");
 
-			return new Uri(baseUri.GetLeftPart(UriPartial.Authority) + uri.GetSafeAbsolutePath() + uri.GetSafeQuery());
-		}
+            return new Uri(baseUri.GetLeftPart(UriPartial.Authority) + uri.GetSafeAbsolutePath() + uri.GetSafeQuery());
+        }
 
         static string GetSafeQuery(this Uri uri)
         {
@@ -195,5 +228,5 @@ namespace Umbraco.Core
 
             return query;
         }
-	}
+    }
 }
