2f80aee165d7c49b43a6333e53c322f597208253
Shannon <sdeminick@gmail.com>
2013-07-31 11:09:30 +1000

    Fixes an issue with WebSecurity logout

--- src/Umbraco.Web/Security/WebSecurity.cs
+++ src/Umbraco.Web/Security/WebSecurity.cs
@@ -135,7 +135,7 @@ namespace Umbraco.Web.Security
                                       SqlHelper.CreateParameter("@contextId", retVal));
             UmbracoUserContextId = retVal.ToString();
 
-            LogHelper.Info(typeof(WebSecurity), "User Id: {0} logged in", () => userId);
+            LogHelper.Info<WebSecurity>("User Id: {0} logged in", () => userId);
 
         }
 
@@ -154,8 +154,11 @@ namespace Umbraco.Web.Security
             }
             catch (Exception ex)
             {
-                LogHelper.Error(typeof(WebSecurity), string.Format("Login with contextId {0} didn't exist in the database", UmbracoUserContextId), ex);
+                LogHelper.Error<WebSecurity>(string.Format("Login with contextId {0} didn't exist in the database", UmbracoUserContextId), ex);
             }
+
+            //this clears the cookie
+            UmbracoUserContextId = "";
         }
 
         public void RenewLoginTimeout()
