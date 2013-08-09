f777fdc8434d34992165fef65b1dc3daf3c7358d
Per Ploug Krogslund <pph@umbraco.dk>
2013-08-09 15:36:36 +0200

    Added mntp changes

--- src/Umbraco.Web.UI/App_Plugins/property editors/mntp/mntp.js
+++ src/Umbraco.Web.UI/App_Plugins/property editors/mntp/mntp.js
@@ -2,14 +2,41 @@
 //with a specified callback, this callback will receive an object with a selection on it
 angular.module('umbraco')
 .controller("uComponents.Editors.MNTPController",
-	function($scope, dialogService){
+	
+	function($scope, dialogService, entityResource){
+		$scope.ids = $scope.model.value.split(',');
+		$scope.renderModel = [];
 		
-    $scope.openContentPicker =function(value){
-            var d = dialogService.contentPicker({scope: $scope, callback: populate});
-            var s = d;
-    };
+		entityResource.getByIds($scope.ids).then(function(data){
+			$(data).each(function(i, item){
+				$scope.renderModel.push({name: item.name, id: item.id, icon: item.icon});
+			});
+		});
 
-    function populate(data){
-        $scope.model.value = data.selection;
-    }
+		$scope.openContentPicker =function(){
+			var d = dialogService.contentPicker({scope: $scope, callback: populate});
+		};
+
+		$scope.remove =function(index){
+			$scope.renderModel.splice(index, 1);
+			$scope.ids.splice(index, 1);
+			$scope.model.value = $scope.ids.join();
+		};
+
+		$scope.add =function(item){
+
+			if($scope.ids.indexOf(item.id) < 0){
+				$scope.renderModel.push({name: item.name, id: item.id, icon: item.icon})
+				$scope.ids.push(item.id);	
+
+				$scope.model.value = $scope.ids.join();	
+			}	
+		};
+
+
+		function populate(data){
+			$(data.selection).each(function(i, item){
+				$scope.add(item);
+			});
+		}
 });
