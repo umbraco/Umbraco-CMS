4bb71273e20a8752b64da0299d1f67ad80877e62
Shannon <sdeminick@gmail.com>
2013-08-09 16:00:09 +1000

    doh, re-publicized EntityPermissions... caused test errors.

---
+++ src/Umbraco.Core/Models/Membership/EntityPermission.cs
@@ -0,0 +1,23 @@
+﻿namespace Umbraco.Core.Models.Membership
+{
+    /// <summary>
+    /// Represents a user -> entity permission
+    /// </summary>
+    public class EntityPermission
+    {
+        public EntityPermission(object userId, int entityId, string[] assignedPermissions)
+        {
+            UserId = userId;
+            EntityId = entityId;
+            AssignedPermissions = assignedPermissions;
+        }
+
+        public object UserId { get; set; }
+        public int EntityId { get; set; }
+
+        /// <summary>
+        /// The assigned permissions for the user/entity combo
+        /// </summary>
+        public string[] AssignedPermissions { get; set; }
+    }
+}
--- src/Umbraco.Core/Models/Membership/User.cs
+++ src/Umbraco.Core/Models/Membership/User.cs
@@ -8,27 +8,6 @@ using Umbraco.Core.Persistence.Mappers;
 namespace Umbraco.Core.Models.Membership
 {
     /// <summary>
-    /// Represents a user -> entity permission
-    /// </summary>
-    internal class EntityPermission
-    {
-        public EntityPermission(object userId, int entityId, string[] assignedPermissions)
-        {
-            UserId = userId;
-            EntityId = entityId;
-            AssignedPermissions = assignedPermissions;
-        }
-
-        public object UserId { get; set; }
-        public int EntityId { get; set; }
-
-        /// <summary>
-        /// The assigned permissions for the user/entity combo
-        /// </summary>
-        public string[] AssignedPermissions { get; set; }
-    }
-
-    /// <summary>
     /// Represents a backoffice user
     /// </summary>
     /// <remarks>
