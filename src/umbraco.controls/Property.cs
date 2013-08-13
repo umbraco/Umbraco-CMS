2f0ff5837e8fb76c08dc057da0d08174e2c9a16b
garyhuntddn <gary.hunt@datadeliverynetwork.com>
2013-08-13 18:12:18 +0100

    Use WebControl.ToolTip property as it already exists

--- src/umbraco.controls/Property.cs
+++ src/umbraco.controls/Property.cs
@@ -1,12 +1,11 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Web.UI;
 
 namespace umbraco.uicontrols {
     public class PropertyPanel  : System.Web.UI.WebControls.Panel {
         public string Text { get; set; }
-
-        public string Tooltip { get; set; }
 
         protected override void Render(System.Web.UI.HtmlTextWriter writer) {
             this.CreateChildControls();
@@ -22,8 +21,8 @@ namespace umbraco.uicontrols {
                 writer.AddAttribute( HtmlTextWriterAttribute.Class, "propertyItemheader" );
 
                 // write the alias into the title (tooltip) if one has been supplied
-                if ( !String.IsNullOrEmpty(Alias) )
-                  writer.AddAttribute( HtmlTextWriterAttribute.Title, Tooltip );
+            if ( !String.IsNullOrEmpty( ToolTip ) )
+                writer.AddAttribute( HtmlTextWriterAttribute.Title, ToolTip );
 
                 writer.RenderBeginTag( HtmlTextWriterTag.Div );
                 if ( !String.IsNullOrEmpty(Text))
