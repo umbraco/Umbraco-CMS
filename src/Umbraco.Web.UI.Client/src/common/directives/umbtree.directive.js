2b0e32cdcf0ec1903b4a60b784db1b9c9feb8883
perploug <pph@umbraco.dk>
2013-08-16 15:41:27 +0200

    Linking tree header to dashboard

--- src/Umbraco.Web.UI.Client/src/common/directives/umbtree.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/umbtree.directive.js
@@ -29,7 +29,7 @@ angular.module("umbraco.directives")
 
          if(!hideheader){ 
            template +='<div>' + 
-           '<h5><a class="root-link">{{tree.name}}</a></h5>' +
+           '<h5><a href="#{{section}}" class="root-link">{{tree.name}}</a></h5>' +
                '<a href class="umb-options" ng-hide="tree.root.isContainer || !tree.root.menuUrl" ng-click="options(this, tree.root, $event)"><i></i><i></i><i></i></a>' +
            '</div>';
          }
