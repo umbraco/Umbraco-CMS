ec4f6de6317805d60903c877b95a5b4e8057027f
Shannon <sdeminick@gmail.com>
2013-08-23 13:41:16 +1000

    Fixes:  U4-2701 The media creation process needs to be optimized, there's far too much IO occuring

--- src/Umbraco.Core/IO/FileSystemExtensions.cs
+++ src/Umbraco.Core/IO/FileSystemExtensions.cs
@@ -2,12 +2,10 @@
 using Umbraco.Core.CodeAnnotations;
 
 namespace Umbraco.Core.IO
-{
-	[UmbracoExperimentalFeature("http://issues.umbraco.org/issue/U4-1156", "Will be declared public after 4.10")]
+{	
     public static class FileSystemExtensions
     {
-		[UmbracoExperimentalFeature("http://issues.umbraco.org/issue/U4-1156", "Will be declared public after 4.10")]
-		internal static long GetSize(this IFileSystem fs, string path)
+		public static long GetSize(this IFileSystem fs, string path)
         {
             using (var s = fs.OpenFile(path))
             {
@@ -18,20 +16,17 @@ namespace Umbraco.Core.IO
             }
         }
 
-		[UmbracoExperimentalFeature("http://issues.umbraco.org/issue/U4-1156", "Will be declared public after 4.10")]
-		internal static void CopyFile(this IFileSystem fs, string path, string newPath)
+        public static void CopyFile(this IFileSystem fs, string path, string newPath)
         {
             fs.AddFile(newPath, fs.OpenFile(path));
         }
 
-		[UmbracoExperimentalFeature("http://issues.umbraco.org/issue/U4-1156", "Will be declared public after 4.10")]
-		internal static string GetExtension(this IFileSystem fs, string path)
+        public static string GetExtension(this IFileSystem fs, string path)
 		{
 			return Path.GetExtension(fs.GetFullPath(path));
 		}
 
-		[UmbracoExperimentalFeature("http://issues.umbraco.org/issue/U4-1156", "Will be declared public after 4.10")]
-		internal static string GetFileName(this IFileSystem fs, string path)
+        public static string GetFileName(this IFileSystem fs, string path)
 		{
 			return Path.GetFileName(fs.GetFullPath(path));
 		}
