53b6fb7b5625b2ed56179b5d7f20091648772435
Sebastiaan Janssen <sebastiaan@umbraco.com>
2013-08-27 11:37:20 +0200

    Add login template to partial view macros

---
+++ src/Umbraco.Web/UmbracoControllers/LoginController.cs
@@ -0,0 +1,27 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using umbraco.cms.businesslogic.member;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.UmbracoModels;
+
+namespace Umbraco.Web.UmbracoControllers
+{
+    public class LoginController : SurfaceController
+    {
+        [HttpPost]
+        public ActionResult HandleLogin([Bind(Prefix="loginModel")]LoginModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                var m = Member.GetMemberFromLoginNameAndPassword(model.Username, model.Password);
+                if (m != null)
+                {
+                    Member.AddMemberToCache(m);
+                    return Redirect("/");
+                }
+            }
+
+            return CurrentUmbracoPage();
+        }
+    }
+}
---
+++ src/Umbraco.Web/UmbracoModels/LoginModel.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Linq;
+
+namespace Umbraco.Web.UmbracoModels
+{
+    public class LoginModel
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}
