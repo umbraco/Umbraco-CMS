342232f21e2988245538f30529a8dcb495e673ad
Morten Christensen <mc@sitereactor.dk>
2013-08-27 15:49:46 +0200

    Updating the Member class and adding an UmbracoMember wrapper as a bridge between MembershipUser and Member

--- src/Umbraco.Core/Models/Membership/Member.cs
+++ src/Umbraco.Core/Models/Membership/Member.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Diagnostics;
 using System.Reflection;
 using System.Runtime.Serialization;
@@ -23,12 +24,19 @@ namespace Umbraco.Core.Models.Membership
         private Guid _key;
         private DateTime _createDate;
         private DateTime _updateDate;
+        private PropertyCollection _properties;
 
         private static readonly PropertyInfo IdSelector = ExpressionHelper.GetPropertyInfo<Member, int>(x => x.Id);
         private static readonly PropertyInfo KeySelector = ExpressionHelper.GetPropertyInfo<Member, Guid>(x => x.Key);
         private static readonly PropertyInfo CreateDateSelector = ExpressionHelper.GetPropertyInfo<Member, DateTime>(x => x.CreateDate);
         private static readonly PropertyInfo UpdateDateSelector = ExpressionHelper.GetPropertyInfo<Member, DateTime>(x => x.UpdateDate);
         private static readonly PropertyInfo HasIdentitySelector = ExpressionHelper.GetPropertyInfo<Member, bool>(x => x.HasIdentity);
+        private readonly static PropertyInfo PropertyCollectionSelector = ExpressionHelper.GetPropertyInfo<Member, PropertyCollection>(x => x.Properties);
+
+        protected void PropertiesChanged(object sender, NotifyCollectionChangedEventArgs e)
+        {
+            OnPropertyChanged(PropertyCollectionSelector);
+        }
 
         /// <summary>
         /// Integer Id
@@ -113,6 +121,7 @@ namespace Umbraco.Core.Models.Membership
         /// <summary>
         /// Indicates whether the current entity has an identity, eg. Id.
         /// </summary>
+        [IgnoreDataMember]
         public virtual bool HasIdentity
         {
             get
@@ -129,22 +138,243 @@ namespace Umbraco.Core.Models.Membership
             }
         }
 
+        /// <summary>
+        /// Gets or sets the Username
+        /// </summary>
+        [DataMember]
         public string Username { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Email
+        /// </summary>
+        [DataMember]
         public string Email { get; set; }
 
+        /// <summary>
+        /// Gets or sets the Password
+        /// </summary>
+        [DataMember]
         public string Password { get; set; }
-        public string PasswordQuestion { get; set; }
-        public string PasswordAnswer { get; set; }
-        public string Comments { get; set; }
-        public bool IsApproved { get; set; }
-        public bool IsOnline { get; set; }
-        public bool IsLockedOut { get; set; }
-        public DateTime LastLoginDate { get; set; }
-        public DateTime LastPasswordChangeDate { get; set; }
-        public DateTime LastLockoutDate { get; set; }
+
+        /// <summary>
+        /// Gets or sets the Password Question
+        /// </summary>
+        /// <remarks>
+        /// Alias: umbracoPasswordRetrievalQuestionPropertyTypeAlias
+        /// Part of the standard properties collection.
+        /// </remarks>
+        [IgnoreDataMember]
+        public string PasswordQuestion
+        {
+            get
+            {
+                return Properties[Constants.Conventions.Member.PasswordQuestion].Value.ToString();
+            }
+            set
+            {
+                Properties[Constants.Conventions.Member.PasswordQuestion].Value = value;
+            }
+        }
         
+        /// <summary>
+        /// Gets or sets the Password Answer
+        /// </summary>
+        /// <remarks>
+        /// Alias: umbracoPasswordRetrievalAnswerPropertyTypeAlias
+        /// Part of the standard properties collection.
+        /// </remarks>
+        [IgnoreDataMember]
+        public string PasswordAnswer
+        {
+            get
+            {
+                return Properties[Constants.Conventions.Member.PasswordAnswer].Value.ToString();
+            }
+            set
+            {
+                Properties[Constants.Conventions.Member.PasswordAnswer].Value = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or set the comments for the member
+        /// </summary>
+        /// <remarks>
+        /// Alias: umbracoCommentPropertyTypeAlias
+        /// Part of the standard properties collection.
+        /// </remarks>
+        [IgnoreDataMember]
+        public string Comments
+        {
+            get
+            {
+                return Properties[Constants.Conventions.Member.Comments].Value.ToString();
+            }
+            set
+            {
+                Properties[Constants.Conventions.Member.Comments].Value = value;
+            }
+        }
+        
+        /// <summary>
+        /// Gets or sets a boolean indicating whether the Member is approved
+        /// </summary>
+        /// <remarks>
+        /// Alias: umbracoApprovePropertyTypeAlias
+        /// Part of the standard properties collection.
+        /// </remarks>
+        [IgnoreDataMember]
+        public bool IsApproved
+        {
+            get
+            {
+                if (Properties[Constants.Conventions.Member.IsApproved].Value is bool)
+                    return (bool)Properties[Constants.Conventions.Member.IsApproved].Value;
+
+                return (bool)Convert.ChangeType(Properties[Constants.Conventions.Member.IsApproved].Value, typeof(bool));
+            }
+            set
+            {
+                Properties[Constants.Conventions.Member.IsApproved].Value = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a boolean indicating whether the Member is locked out
+        /// </summary>
+        /// <remarks>
+        /// Alias: umbracoLockPropertyTypeAlias
+        /// Part of the standard properties collection.
+        /// </remarks>
+        [IgnoreDataMember]
+        public bool IsLockedOut
+        {
+            get
+            {
+                if (Properties[Constants.Conventions.Member.IsLockedOut].Value is bool)
+                    return (bool)Properties[Constants.Conventions.Member.IsLockedOut].Value;
+
+                return (bool)Convert.ChangeType(Properties[Constants.Conventions.Member.IsLockedOut].Value, typeof(bool));
+            }
+            set
+            {
+                Properties[Constants.Conventions.Member.IsLockedOut].Value = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the date for last login
+        /// </summary>
+        /// <remarks>
+        /// Alias: umbracoLastLoginPropertyTypeAlias
+        /// Part of the standard properties collection.
+        /// </remarks>
+        [IgnoreDataMember]
+        public DateTime LastLoginDate
+        {
+            get
+            {
+                if (Properties[Constants.Conventions.Member.LastLoginDate].Value is DateTime)
+                    return (DateTime)Properties[Constants.Conventions.Member.LastLoginDate].Value;
+
+                return (DateTime)Convert.ChangeType(Properties[Constants.Conventions.Member.LastLoginDate].Value, typeof(DateTime));
+            }
+            set
+            {
+                Properties[Constants.Conventions.Member.LastLoginDate].Value = value;
+            }
+        }
+
+        /// <summary>
+        /// Gest or sets the date for last password change
+        /// </summary>
+        /// <remarks>
+        /// Alias: umbracoMemberLastPasswordChange
+        /// Part of the standard properties collection.
+        /// </remarks>
+        [IgnoreDataMember]
+        public DateTime LastPasswordChangeDate
+        {
+            get
+            {
+                if (Properties[Constants.Conventions.Member.LastPasswordChangeDate].Value is DateTime)
+                    return (DateTime)Properties[Constants.Conventions.Member.LastPasswordChangeDate].Value;
+
+                return (DateTime)Convert.ChangeType(Properties[Constants.Conventions.Member.LastPasswordChangeDate].Value, typeof(DateTime));
+            }
+            set
+            {
+                Properties[Constants.Conventions.Member.LastPasswordChangeDate].Value = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the date for when Member was locked out
+        /// </summary>
+        /// <remarks>
+        /// Alias: umbracoMemberLastLockout
+        /// Part of the standard properties collection.
+        /// </remarks>
+        [IgnoreDataMember]
+        public DateTime LastLockoutDate
+        {
+            get
+            {
+                if (Properties[Constants.Conventions.Member.LastLockoutDate].Value is DateTime)
+                    return (DateTime)Properties[Constants.Conventions.Member.LastLockoutDate].Value;
+
+                return (DateTime)Convert.ChangeType(Properties[Constants.Conventions.Member.LastLockoutDate].Value, typeof(DateTime));
+            }
+            set
+            {
+                Properties[Constants.Conventions.Member.LastLockoutDate].Value = value;
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the number of failed password attempts.
+        /// This is the number of times the password was entered incorrectly upon login.
+        /// </summary>
+        /// <remarks>
+        /// Alias: umbracoFailedPasswordAttemptsPropertyTypeAlias
+        /// Part of the standard properties collection.
+        /// </remarks>
+        [IgnoreDataMember]
+        public int FailedPasswordAttempts
+        {
+            get
+            {
+                if (Properties[Constants.Conventions.Member.FailedPasswordAttempts].Value is int)
+                    return (int)Properties[Constants.Conventions.Member.FailedPasswordAttempts].Value;
+
+                return (int)Convert.ChangeType(Properties[Constants.Conventions.Member.FailedPasswordAttempts].Value, typeof(int));
+            }
+            set
+            {
+                Properties[Constants.Conventions.Member.LastLockoutDate].Value = value;
+            }
+        }
+
+        /// <summary>
+        /// Boolean indicating whether the user is online
+        /// </summary>
+        /// <remarks>Context dependent property</remarks>
+        [IgnoreDataMember]
+        public bool IsOnline { get; set; }
+
         public object ProfileId { get; set; }
         public IEnumerable<object> Groups { get; set; }
+
+        [DataMember]
+        public PropertyCollection Properties
+        {
+            get { return _properties; }
+            set
+            {
+                _properties = value;
+                _properties.CollectionChanged += PropertiesChanged;
+            }
+        }
 
         #region Internal methods
 
---
+++ src/Umbraco.Core/Models/Membership/UmbracoMember.cs
@@ -0,0 +1,20 @@
+﻿using System.Web.Security;
+
+namespace Umbraco.Core.Models.Membership
+{
+    internal class UmbracoMember : MembershipUser
+    {
+        private readonly IMembershipUser _member;
+
+        public UmbracoMember(IMembershipUser member)
+        {
+            _member = member;
+        }
+
+        public override string Email
+        {
+            get { return _member.Email; }
+            set { _member.Email = value; }
+        }
+    }
+}
