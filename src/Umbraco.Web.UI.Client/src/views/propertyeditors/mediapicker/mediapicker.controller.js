d6493a742dee8946112969a4183ca9756449f3f8
perploug <pph@umbraco.dk>
2013-08-28 23:13:32 +0200

    wip on the media picker cropper

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/mediapicker/mediapicker.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/mediapicker/mediapicker.controller.js
@@ -9,33 +9,38 @@ angular.module('umbraco').controller("Umbraco.Editors.MediaPickerController",
 	//saved value contains a list of images with their coordinates a Dot coordinates
 	//this will be $scope.model.value...
 	var sampleData = [
-		{id: 1234, coordinates: {x:123,y:345}, center: {x:123,y:12}},
-		{id: 2234, coordinates: {x:123,y:345}, center: {x:123,y:12}},
-		{id: 2344, coordinates: {x:123,y:345}, center: {x:123,y:12}},
+		{id: 1143, coordinates: {x:123,y:345}, center: {x:123,y:12}},
+		{id: 1144, coordinates: {x:123,y:345}, center: {x:123,y:12}},
+		{id: 1145, coordinates: {x:123,y:345}, center: {x:123,y:12}},
 	];
 
 	$scope.images = sampleData;
 	$($scope.images).each(function(i,img){
 		mediaResource.getById(img.id).then(function(media){
-			img.media = media;
+			//img.media = media;
 
 			//shortcuts
-			img.file = imageHelper.getImagePropertyVaue({imageModel: media});
-			img.thumbnail = imageHelper.getThumbnailFromPath(img.file}); 
+			//TODO, do something better then this for searching
+			img.src = imageHelper.getImagePropertyVaue({imageModel: media});
+			img.thumbnail = imageHelper.getThumbnailFromPath(img.src); 
 		});
 	});
 
  	//List of crops with name and size			
  	$scope.config = {
  		crops: [
- 		{name: "default", x:300,y:400},
- 		{name: "header", x:23,y:40},
- 		{name: "tiny", x:10,y:10}
+	 		{name: "default", x:300,y:400},
+	 		{name: "header", x:23,y:40},
+	 		{name: "tiny", x:10,y:10}
  		]};
 
 
  		$scope.openMediaPicker =function(value){
  			var d = dialogService.mediaPicker({scope: $scope, callback: populate});
+ 		};
+
+ 		$scope.crop = function(image){
+ 			$scope.currentImage = image;
  		};
 
  		function populate(data){
