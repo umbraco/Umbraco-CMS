5713226297eefc9e5b6042c0c81e4a143e26d7bb
perploug <pph@umbraco.dk>
2013-09-02 15:32:55 +0200

    add umbraco.package module

--- src/Umbraco.Web.UI.Client/src/app.js
+++ src/Umbraco.Web.UI.Client/src/app.js
@@ -3,12 +3,16 @@ var app = angular.module('umbraco', [
 	'umbraco.directives',
 	'umbraco.resources',
 	'umbraco.services',
+	'umbraco.packages',
     'ngCookies',
     'ngMobile',
     
     'ui.sortable',
     'blueimp.fileupload'
 ]);
+
+//seperate core and packages? 
+var packages = angular.module("umbraco.packages", []);
 
 
 /* For Angular 1.2: we need to load in Route, animate and touch seperately
