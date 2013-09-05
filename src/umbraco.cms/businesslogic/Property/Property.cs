baa22301e9a95d213e6aafb41a5abe6985d23d4c
Stefan Kip <mail@kipusoep.nl>
2013-09-05 11:31:06 +0200

    Added Exception which tells you which datatype is unable to load

--- src/umbraco.cms/businesslogic/Property/Property.cs
+++ src/umbraco.cms/businesslogic/Property/Property.cs
@@ -37,6 +37,8 @@ namespace umbraco.cms.businesslogic.property
 
             _pt = pt;
             _id = Id;
+            if (_pt.DataTypeDefinition.DataType == null)
+                throw new Exception(string.Format("Could not load datatype '{0}'", _pt.DataTypeDefinition.Text));
             _data = _pt.DataTypeDefinition.DataType.Data;
             _data.PropertyId = Id;
         }
@@ -47,6 +49,8 @@ namespace umbraco.cms.businesslogic.property
             _pt = PropertyType.GetPropertyType(
                 SqlHelper.ExecuteScalar<int>("select propertytypeid from cmsPropertyData where id = @id",
                                              SqlHelper.CreateParameter("@id", Id)));
+            if (_pt.DataTypeDefinition.DataType == null)
+                throw new Exception(string.Format("Could not load datatype '{0}'", _pt.DataTypeDefinition.Text));
             _data = _pt.DataTypeDefinition.DataType.Data;
             _data.PropertyId = Id;
         }
@@ -59,6 +63,8 @@ namespace umbraco.cms.businesslogic.property
 
             //Just to ensure that there is a PropertyType available
             _pt = PropertyType.GetPropertyType(property.PropertyTypeId);
+            if (_pt.DataTypeDefinition.DataType == null)
+                throw new Exception(string.Format("Could not load datatype '{0}'", _pt.DataTypeDefinition.Text));
             _data = _pt.DataTypeDefinition.DataType.Data;
             _data.PropertyId = Id;
         }
