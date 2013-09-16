8400ad82ab1a416098d7887101328fa8fdcc939b
perploug <pph@umbraco.dk>
2013-09-16 09:16:47 +0200

    added examine constants to core

---
+++ src/Umbraco.Core/Constants-Examine.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umbraco.Core
+{
+    public static partial class Constants
+    {
+        public static class Examine
+        {
+            /// <summary>
+            /// The alias of the internal member searcher
+            /// </summary>
+            public const string InternalMemberSearcher = "InternalMemberSearcher";
+
+            /// <summary>
+            /// The alias of the internal content searcher
+            /// </summary>
+            public const string InternalSearcher = "InternalSearcher";
+        }
+    }
+}
