b469ed92ff68d549194583b8bb99fe3783f59062
Tim Geyssens <tg@umbraco.com>
2013-09-18 15:03:23 +0200

    Also make sure the embed rte plugin only shows resize options if provider supports it

--- src/Umbraco.Web.UI.Client/src/views/common/dialogs/rteembed.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/common/dialogs/rteembed.controller.js
@@ -5,23 +5,43 @@
     $scope.constrain = true;
     $scope.preview = "";
     $scope.success = false;
-
+    $scope.info = "";
+    $scope.supportsDimensions = false;
+    
     var origWidth = 500;
     var origHeight = 300;
+    
     $scope.showPreview = function(){
 
         if ($scope.url != "") {
             
             $scope.preview = "<div class=\"umb-loader\">";
+            $scope.info = "";
             $scope.success = false;
 
             $http({ method: 'GET', url: '/umbraco/UmbracoApi/RteEmbed/GetEmbed', params: { url: $scope.url, width: $scope.width, height: $scope.height } })
-                .success(function(data) {
-                    $scope.preview = data.Markup;
-                    $scope.success = true;
+                .success(function (data) {
+                    
+                    $scope.preview = "";
+                    
+                    switch (data.Status) {
+                        case 0:
+                            //not supported
+                            $scope.info = "Not Supported";
+                            break;
+                        case 1:
+                            //error
+                            $scope.info = "Computer says no";
+                            break;
+                        case 2:
+                            $scope.preview = data.Markup;
+                            $scope.supportsDimensions = data.SupportsDimensions;
+                            break;
+                    }
                 })
                 .error(function() {
                     $scope.preview = "";
+                    $scope.info = "Computer says no";
                 });
 
         }
