36133a3ee4bf4245cfca59e33338f0e5d0aa9f58
Tim Geyssens <tg@umbraco.com>
2013-09-19 14:47:08 +0200

    List view editor, make select all function and show bulk actions when at least 1 thing is selected

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/listview/listview.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/listview/listview.controller.js
@@ -2,7 +2,7 @@ angular.module("umbraco")
     .controller("Umbraco.Editors.ListViewController", 
         function ($rootScope, $scope, $routeParams, contentResource, contentTypeResource, editorContextService) {
 
-           
+        $scope.selected = [];
             
         $scope.options = {
             pageSize: 10,
@@ -68,6 +68,48 @@ angular.module("umbraco")
             });
         };
 
+        var updateSelected = function (action, id) {
+            if (action === 'add' && $scope.selected.indexOf(id) === -1) {
+                $scope.selected.push(id);
+            }
+            if (action === 'remove' && $scope.selected.indexOf(id) !== -1) {
+                $scope.selected.splice($scope.selected.indexOf(id), 1);
+            }
+        };
+
+        $scope.updateSelection = function ($event, id) {
+            var checkbox = $event.target;
+            var action = (checkbox.checked ? 'add' : 'remove');
+            updateSelected(action, id);
+        };
+
+        $scope.selectAll = function ($event) {
+            var checkbox = $event.target;
+            var action = (checkbox.checked ? 'add' : 'remove');
+            for (var i = 0; i < $scope.listViewResultSet.items.length; i++) {
+                var entity = $scope.listViewResultSet.items[i];
+                updateSelected(action, entity.id);
+            }
+        };
+
+        $scope.getSelectedClass = function (entity) {
+            return $scope.isSelected(entity.id) ? 'selected' : '';
+        };
+
+        $scope.isSelected = function (id) {
+            return $scope.selected.indexOf(id) >= 0;
+        };
+
+        $scope.isSelectedAll = function () {
+            if ($scope.listViewResultSet != null)
+                return $scope.selected.length === $scope.listViewResultSet.items.length;
+            else
+                return false;
+        };
+
+        $scope.isAnythingSelected = function() {
+            return $scope.selected.length > 0;
+        };
 
         if($routeParams.id){
             $scope.pagination = new Array(100);
