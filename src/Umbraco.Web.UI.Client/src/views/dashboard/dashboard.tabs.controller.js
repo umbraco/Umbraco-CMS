7d50d0fc6c1eadabaf06a87e65437723a272f4e4
perploug <pph@umbraco.dk>
2013-09-25 15:14:09 +0200

    Replaces all the old core dashboards

--- src/Umbraco.Web.UI.Client/src/views/dashboard/dashboard.tabs.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/dashboard/dashboard.tabs.controller.js
@@ -1,20 +1,37 @@
 function startUpVideosDashboardController($scope, xmlhelper, $log, $http) {
-	//xmlHelper.parseFeed("http://umbraco.org/feeds/videos/getting-started").then(function(feed){
-		
-	//});
-   $scope.videos = [];
-   $http.get("dashboard/feedproxy.aspx?url=http://umbraco.org/feeds/videos/getting-started").then(function(data){
-    	var feed = $(data.data);
-    	$('item', feed).each(function (i, item) {
-   				var video = {};
-   				video.thumbnail = $(item).find('thumbnail').attr('url');
-   				video.title = $("title", item).text();
-   				video.link = $("guid", item).text();
-   				$scope.videos.push(video);  		
-    	});
-   });
+    $scope.videos = [];
+    $scope.init = function(url){
+        var proxyUrl = "dashboard/feedproxy.aspx?url=" + url; 
+        $http.get(proxyUrl).then(function(data){
+              var feed = $(data.data);
+              $('item', feed).each(function (i, item) {
+                  var video = {};
+                  video.thumbnail = $(item).find('thumbnail').attr('url');
+                  video.title = $("title", item).text();
+                  video.link = $("guid", item).text();
+                  $scope.videos.push(video);      
+              });
+        });
+    };
 }
 angular.module("umbraco").controller("Umbraco.Dashboard.StartupVideosController", startUpVideosDashboardController);
+
+
+function MediaFolderBrowserDashboardController($scope, xmlhelper, $log, userResource) {
+    //this is the model we will pass to the service
+    $scope.profile = {};
+
+    $scope.changePassword = function (p) {   
+        userResource.changePassword(p.oldPassword, p.newPassword).then(function () {
+            $scope.passwordForm.$setValidity(true);
+        }, function () {
+            //this only happens if there is a wrong oldPassword sent along
+            $scope.passwordForm.oldpass.$setValidity("oldPassword", false);
+        });
+    }
+}
+angular.module("umbraco").controller("Umbraco.Dashboard.MediaFolderBrowserDashboardController", MediaFolderBrowserDashboardController);
+
 
 function ChangePasswordDashboardController($scope, xmlhelper, $log, userResource) {
     //this is the model we will pass to the service
@@ -22,14 +39,11 @@ function ChangePasswordDashboardController($scope, xmlhelper, $log, userResource
 
     $scope.changePassword = function (p) {   
         userResource.changePassword(p.oldPassword, p.newPassword).then(function () {
-            alert("changed");
             $scope.passwordForm.$setValidity(true);
         }, function () {
-          alert("not changed");
             //this only happens if there is a wrong oldPassword sent along
             $scope.passwordForm.oldpass.$setValidity("oldPassword", false);
         });
     }
 }
-
 angular.module("umbraco").controller("Umbraco.Dashboard.StartupChangePasswordController", ChangePasswordDashboardController);
