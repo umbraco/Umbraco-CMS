29d849754ce90698e1e01d9d34ba7fbe09ebcd5e
Shannon <sdeminick@gmail.com>
2013-09-26 12:53:33 +1000

    Fixes: U4-2703 Content editor save/publish should update modified attributes

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/urllist/urllist.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/urllist/urllist.controller.js
@@ -1,11 +1,23 @@
 angular.module('umbraco').controller("Umbraco.Editors.UrlListController",
 	function($rootScope, $scope, $filter) {
 
-	    $scope.renderModel = _.map($scope.model.value.split(","), function(item) {
-	        return {
-	            url: item,
-	            urlTarget : ($scope.config && $scope.config.target) ? $scope.config.target : "_blank" 
-	        };
+        function formatDisplayValue() {
+            $scope.renderModel = _.map($scope.model.value.split(","), function (item) {
+                return {
+                    url: item,
+                    urlTarget: ($scope.config && $scope.config.target) ? $scope.config.target : "_blank"
+                };
+            });
+        }
+
+	    formatDisplayValue();
+	    
+	    //we need to put a watch on the real model because when it changes we have to update our renderModel
+	    $scope.$watch("model.value", function (newVal, oldVal) {
+	        if (newVal !== null && newVal !== undefined && newVal !== oldVal) {
+	            //update the display val again
+	            formatDisplayValue();
+	        }
 	    });
 
 	});
