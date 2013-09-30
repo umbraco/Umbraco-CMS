a11e947f964e76f18aa6ffb2dbefc66ba8ef8d74
Shannon <sdeminick@gmail.com>
2013-09-30 17:12:31 +1000

    Fixes: U4-2780 UserPermissions editor just YSODs

--- src/Umbraco.Web/umbraco.presentation/umbraco/users/PermissionEditor.aspx.cs
+++ src/Umbraco.Web/umbraco.presentation/umbraco/users/PermissionEditor.aspx.cs
@@ -10,6 +10,7 @@ using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
 using System.Xml;
 using System.IO;
+using Umbraco.Core;
 using umbraco;
 using umbraco.BusinessLogic;
 using System.Collections.Generic;
@@ -37,7 +38,7 @@ namespace umbraco.cms.presentation.user
 
 			if (!IsPostBack)
 			{
-				JTree.App = TreeDefinitionCollection.Instance.FindTree<loadContent>().Tree.ApplicationAlias;
+			    JTree.App = Constants.Applications.Content;
 				JTree.ShowContextMenu = false;
 				JTree.IsDialog = true;
 			}
