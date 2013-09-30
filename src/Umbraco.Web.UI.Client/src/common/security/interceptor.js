1a9ced80f9afd1d7bcedea82f5830dd80389aeb0
Shannon <sdeminick@gmail.com>
2013-09-30 16:31:52 +1000

    Fixes: U4-2974 Authentication fail displays username and password

--- src/Umbraco.Web.UI.Client/src/common/security/interceptor.js
+++ src/Umbraco.Web.UI.Client/src/common/security/interceptor.js
@@ -22,9 +22,15 @@ angular.module('umbraco.security.interceptor', ['umbraco.security.retryQueue'])
                 //http://issues.umbraco.org/issue/U4-2749
                 
                 //It was decided to just put these messages into the normal status messages. 
+
+                var msg = "Unauthorized access to URL: <br/><i>" + originalResponse.config.url.split('?')[0] + "</i>";
+                if (originalResponse.config.data) {
+                    msg += "<br/> with data: <br/><i>" + angular.toJson(originalResponse.config.data) + "</i><br/>Contact your administrator for information.";
+                }
+
                 notifications.error(
                     "Authorization error", 
-                    "Unauthorized access to URL: <br/><i>" + originalResponse.config.url + "</i><br/> with data: <br/><i>" + angular.toJson(originalResponse.config.data) + "</i><br/>Contact your administrator for information.");
+                    msg);
             }
             return promise;
         });
