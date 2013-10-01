e27a0e2bd21e44f55cade6932badc88bda168a7e
Shannon <sdeminick@gmail.com>
2013-10-01 14:16:57 +1000

    updates boolean and check box list to use onValueChanged to detect any server value changes.

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/boolean/boolean.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/boolean/boolean.controller.js
@@ -1,14 +1,26 @@
 function booleanEditorController($scope, $rootScope, assetsService) {
-    $scope.renderModel = {
-        value: false
-    };
-    if ($scope.model && $scope.model.value && ($scope.model.value.toString() === "1" || angular.lowercase($scope.model.value) === "true")) {
-        $scope.renderModel.value = true;
+
+    function setupViewModel() {
+        $scope.renderModel = {
+            value: false
+        };
+        if ($scope.model && $scope.model.value && ($scope.model.value.toString() === "1" || angular.lowercase($scope.model.value) === "true")) {
+            $scope.renderModel.value = true;
+        }
     }
+
+    setupViewModel();
 
     $scope.$watch("renderModel.value", function (newVal) {
         $scope.model.value = newVal === true ? "1" : "0";
     });
+    
+    //here we declare a special method which will be called whenever the value has changed from the server
+    //this is instead of doing a watch on the model.value = faster
+    $scope.model.onValueChanged = function (newVal, oldVal) {
+        //update the display val again if it has changed from the server
+        setupViewModel();
+    };
 
 }
 angular.module("umbraco").controller("Umbraco.Editors.BooleanController", booleanEditorController);
