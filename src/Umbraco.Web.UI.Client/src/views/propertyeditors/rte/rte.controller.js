2bb279d8f7c347d89207446528e0772fb307d1c4
Shannon <sdeminick@gmail.com>
2013-10-01 14:08:14 +1000

    Ensures rte controller will re-load itself if server data has changed.

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/rte/rte.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/rte/rte.controller.js
@@ -32,65 +32,79 @@ angular.module("umbraco")
         var plugins = "code";
 
         assetsService.loadJs("lib/tinymce/tinymce.min.js", $scope).then(function () {
-            //we need to add a timeout here, to force a redraw so TinyMCE can find
-            //the elements needed
-            $timeout(function () {
-                tinymce.DOM.events.domLoaded = true;
-                tinymce.init({
-                    mode: "exact",
-                    elements: $scope.model.alias + "_rte",
-                    skin: "umbraco",                    
-                    plugins: plugins,
-                    valid_elements: validElements,
-                    menubar: false,
-                    statusbar: false,
-                    height: 340,
-                    toolbar: toolbar,
-                    setup: function (editor) {
-                        
-                        //We need to listen on multiple things here because of the nature of tinymce, it doesn't 
-                        //fire events when you think!
-                        //The change event doesn't fire when content changes, only when cursor points are changed and undo points
-                        //are created. the blur event doesn't fire if you insert content into the editor with a button and then 
-                        //press save. 
-                        //We have a couple of options, one is to do a set timeout and check for isDirty on the editor, or we can 
-                        //listen to both change and blur and also on our own 'saving' event. I think this will be best because a 
-                        //timer might end up using unwanted cpu and we'd still have to listen to our saving event in case they clicked
-                        //save before the timeout elapsed.
-                        editor.on('change', function (e) {
-                            angularHelper.safeApply($scope, function() {
-                                $scope.model.value = editor.getContent();
-                            });                            
-                        });
-                        editor.on('blur', function (e) {
-                            angularHelper.safeApply($scope, function () {
+            
+            /** Loads in the editor */
+            function loadTinyMce() {
+                
+                //we need to add a timeout here, to force a redraw so TinyMCE can find
+                //the elements needed
+                $timeout(function () {
+                    tinymce.DOM.events.domLoaded = true;
+                    tinymce.init({
+                        mode: "exact",
+                        elements: $scope.model.alias + "_rte",
+                        skin: "umbraco",
+                        plugins: plugins,
+                        valid_elements: validElements,
+                        menubar: false,
+                        statusbar: false,
+                        height: 340,
+                        toolbar: toolbar,
+                        setup: function (editor) {
+
+                            //We need to listen on multiple things here because of the nature of tinymce, it doesn't 
+                            //fire events when you think!
+                            //The change event doesn't fire when content changes, only when cursor points are changed and undo points
+                            //are created. the blur event doesn't fire if you insert content into the editor with a button and then 
+                            //press save. 
+                            //We have a couple of options, one is to do a set timeout and check for isDirty on the editor, or we can 
+                            //listen to both change and blur and also on our own 'saving' event. I think this will be best because a 
+                            //timer might end up using unwanted cpu and we'd still have to listen to our saving event in case they clicked
+                            //save before the timeout elapsed.
+                            editor.on('change', function (e) {
+                                angularHelper.safeApply($scope, function () {
+                                    $scope.model.value = editor.getContent();
+                                });
+                            });
+                            editor.on('blur', function (e) {
+                                angularHelper.safeApply($scope, function () {
+                                    $scope.model.value = editor.getContent();
+                                });
+                            });
+                            var unsubscribe = $scope.$on("saving", function () {
                                 $scope.model.value = editor.getContent();
                             });
-                        });
-                        var unsubscribe = $scope.$on("saving", function() {
-                            $scope.model.value = editor.getContent();
-                        });
 
-                        //when the element is disposed we need to unsubscribe!
-                        // NOTE: this is very important otherwise if this is part of a modal, the listener still exists because the dom 
-                        // element might still be there even after the modal has been hidden.
-                        $element.bind('$destroy', function () {
-                            unsubscribe();
-                        });
+                            //when the element is disposed we need to unsubscribe!
+                            // NOTE: this is very important otherwise if this is part of a modal, the listener still exists because the dom 
+                            // element might still be there even after the modal has been hidden.
+                            $element.bind('$destroy', function () {
+                                unsubscribe();
+                            });
 
-                        //Create the insert media plugin
-                        tinyMceService.createMediaPicker(editor, $scope);
+                            //Create the insert media plugin
+                            tinyMceService.createMediaPicker(editor, $scope);
 
-                        //Create the insert icon plugin
-                        tinyMceService.createInsertEmbeddedMedia(editor, $scope);
+                            //Create the insert icon plugin
+                            tinyMceService.createInsertEmbeddedMedia(editor, $scope);
 
-                        //Create the insert macro plugin
-                        tinyMceService.createInsertMacro(editor, $scope);
+                            //Create the insert macro plugin
+                            tinyMceService.createInsertMacro(editor, $scope);
 
 
-                    }
-                });
-            }, 1);
+                        }
+                    });
+                }, 1);
+            }
+            
+            loadTinyMce();
+
+            //here we declare a special method which will be called whenever the value has changed from the server
+            //this is instead of doing a watch on the model.value = faster
+            $scope.model.onValueChanged = function (newVal, oldVal) {
+                //update the display val again if it has changed from the server
+                loadTinyMce();
+            };
 
         });
     });
