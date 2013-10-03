cbe566302c673654a9052f5c938c143bd53132e0
Tim Geyssens <tg@umbraco.com>
2013-10-03 14:20:13 +0200

    Related links prop editor: first attempt at UI

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/relatedlinks/relatedlinks.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/relatedlinks/relatedlinks.controller.js
@@ -2,4 +2,32 @@
     .controller("Umbraco.Editors.RelatedLinksController", 
         function ($rootScope, $scope, $routeParams, contentResource, contentTypeResource, editorContextService, notificationsService) {
 
+            $scope.newCaption = '';
+            $scope.newLink = 'http://';
+            $scope.newNewWindow = false;
+            
+            $scope.relatedLinks = [
+                { caption: 'Google', link: "http://google.com", newWindow: false },
+                { caption: 'Umbraco', link: "http://umbraco.com", newWindow: false },
+                { caption: 'Nibble', link: "http://nibble.be", newWindow: false }
+            ];
+
+            $scope.delete = function (idx) {
+                $scope.relatedLinks.splice($scope.relatedLinks[idx], 1);
+                
+            };
+
+            $scope.add = function() {
+                var newLink = new function() {
+                    this.caption = $scope.newCaption;
+                    this.link = $scope.newLink;
+                    this.newWindow = $scope.newNewWindow;
+                };
+
+                $scope.newCaption = '';
+                $scope.newLink = '';
+                $scope.newNewWindow = false;
+                
+                $scope.relatedLinks.push(newLink);
+            };
         });
