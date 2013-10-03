4ace6e10db98da4ba9855e8fde3e0f5c4a943813
Tim Geyssens <tg@umbraco.com>
2013-10-03 13:27:13 +0200

    Related links prop editor: adds RelatedLinksPropertyEditor.cs

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/relatedlinks/relatedlinks.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/relatedlinks/relatedlinks.controller.js
@@ -1,1 +1,5 @@
-﻿
+﻿angular.module("umbraco")
+    .controller("Umbraco.Editors.RelatedLinksController", 
+        function ($rootScope, $scope, $routeParams, contentResource, contentTypeResource, editorContextService, notificationsService) {
+
+        });
