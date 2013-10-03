9ee1584ede8a948e9c2ed03ca6117759a9281733
perploug <pph@umbraco.dk>
2013-10-03 20:58:16 +0200

    Initial PublishedContent service for niceUrl access

---
+++ src/Umbraco.Web.UI.Client/src/common/resources/publishedcontent.resource.js
@@ -0,0 +1,44 @@
+/**
+    * @ngdoc service
+    * @name umbraco.resources.publishedContentResource
+    * @description service to retrieve published content from the umbraco cache
+    * 
+    *
+    **/
+function publishedContentResource($q, $http, umbRequestHelper) {
+
+    //the factory object returned
+    return {
+        
+        /**
+         * @ngdoc method
+         * @name umbraco.resources.publishedContentResource#getUrl
+         * @methodOf umbraco.resources.publishedContentResource
+         *
+         * @description
+         * Returns a url, given a node ID
+         *
+         * ##usage
+         * <pre>
+         * publishedContentResource.getUrl()
+         *    .then(function(stylesheets) {
+         *        alert('its here!');
+         *    });
+         * </pre> 
+         * 
+         * @param {Int} id Id of node to return the public url to
+         * @returns {Promise} resourcePromise object containing the url.
+         *
+         */
+        getUrl: function (id) {            
+            return umbRequestHelper.resourcePromise(
+               $http.get(
+                   umbRequestHelper.getApiUrl(
+                       "publishedContentApiBaseUrl",
+                       "GetUrl",[{id: id}])),
+               'Failed to retreive url for id:' + id);
+        }
+    };
+}
+
+angular.module('umbraco.resources').factory('publishedContentResource', publishedContentResource);
---
+++ src/Umbraco.Web/Editors/PublishedContentController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Web.Mvc;
+
+namespace Umbraco.Web.Editors
+{
+    [PluginController("UmbracoApi")]
+    public class PublishedContentController : UmbracoAuthorizedJsonController
+    {
+        UmbracoHelper helper = new UmbracoHelper(UmbracoContext.Current);
+        public string GetNiceUrl(int id)
+        {
+            return helper.NiceUrl(id);
+        }
+    }
+}
