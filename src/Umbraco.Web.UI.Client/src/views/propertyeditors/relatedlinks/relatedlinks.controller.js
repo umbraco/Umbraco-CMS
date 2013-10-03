fb1ea4b8800f5c64db07a6f3197f69b42ad56605
Tim Geyssens <tg@umbraco.com>
2013-10-03 14:58:12 +0200

    Related links prop editor: more work

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/relatedlinks/relatedlinks.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/relatedlinks/relatedlinks.controller.js
@@ -1,10 +1,13 @@
 ﻿angular.module("umbraco")
     .controller("Umbraco.Editors.RelatedLinksController", 
-        function ($rootScope, $scope, $routeParams, contentResource, contentTypeResource, editorContextService, notificationsService) {
+        function ($rootScope, $scope, dialogService, $routeParams, contentResource, contentTypeResource, editorContextService, notificationsService) {
 
             $scope.newCaption = '';
             $scope.newLink = 'http://';
             $scope.newNewWindow = false;
+            $scope.newInternal = null;
+            $scope.newInternalName = '';
+            $scope.addExternal = true;
             
             $scope.relatedLinks = [
                 { caption: 'Google', link: "http://google.com", newWindow: false },
@@ -12,22 +15,56 @@
                 { caption: 'Nibble', link: "http://nibble.be", newWindow: false }
             ];
 
+            $scope.internal = function ($event) {
+                var d = dialogService.contentPicker({ scope: $scope, multipicker: false, callback: select });
+
+                $event.preventDefault();
+            };
+            
+
+            
             $scope.delete = function (idx) {
                 $scope.relatedLinks.splice($scope.relatedLinks[idx], 1);
                 
             };
 
-            $scope.add = function() {
-                var newLink = new function() {
-                    this.caption = $scope.newCaption;
-                    this.link = $scope.newLink;
-                    this.newWindow = $scope.newNewWindow;
-                };
+            $scope.add = function () {
 
+                if ($scope.addExternal) {
+                    var newExtLink = new function() {
+                        this.caption = $scope.newCaption;
+                        this.link = $scope.newLink;
+                        this.newWindow = $scope.newNewWindow;
+                    };
+                    $scope.relatedLinks.push(newExtLink);
+                } else {
+                    var newIntLink = new function () {
+                        this.caption = $scope.newCaption;
+                        this.link = $scope.newLink;
+                        this.newWindow = $scope.newNewWindow;
+                        this.internal = $scope.newInternal;
+                    };
+                    $scope.relatedLinks.push(newIntLink);
+                }
                 $scope.newCaption = '';
                 $scope.newLink = '';
                 $scope.newNewWindow = false;
+                $scope.newInternal = null;
+                $scope.newInternalName = '';
                 
-                $scope.relatedLinks.push(newLink);
+               
             };
+
+            $scope.switch = function ($event) {
+                $scope.addExternal = !$scope.addExternal;
+                $event.preventDefault();
+            };
+            
+            function select(data) {
+                $scope.newInternal = data.id;
+                $scope.newInternalName = data.name;
+            }
+
+            
+
         });
