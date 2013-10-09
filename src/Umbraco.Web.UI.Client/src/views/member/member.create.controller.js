38761e141343b047eb820bc98405a0d140f9162f
Shannon <sdeminick@gmail.com>
2013-10-09 11:01:43 +1100

    started on member create dialog (wip!)

--- src/Umbraco.Web.UI.Client/src/views/member/member.create.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/member/member.create.controller.js
@@ -7,7 +7,23 @@
  * The controller for the member creation dialog
  */
 function memberCreateController($scope, $routeParams, memberTypeResource, iconHelper) {
+
+    $scope.currentStep = 1;
+    $scope.memberModel = null;
+    $scope.memberType = null;
+
+    $scope.submitMember = function () {
+        alert("SDFSDF");
+        //#media/media/edit/{{currentNode.id}}?doctype={{docType.alias}}&create=true
+    };
     
+    $scope.selectMemberType = function (alias) {
+        $scope.memberType = alias;
+        $scope.currentStep = 2;
+    };
+    
+    
+
     memberTypeResource.getTypes($scope.currentNode.id).then(function (data) {
         $scope.allowedTypes = iconHelper.formatContentTypeIcons(data);
     });
