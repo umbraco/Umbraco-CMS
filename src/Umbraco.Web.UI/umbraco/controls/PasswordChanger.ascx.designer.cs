5d5f7a4516d9b050673fc0733e077105537a231b
Shannon <sdeminick@gmail.com>
2013-10-18 08:52:46 +1100

    Updated edit member with correct usage of password changer.

--- src/Umbraco.Web.UI/umbraco/controls/PasswordChanger.ascx.designer.cs
+++ src/Umbraco.Web.UI/umbraco/controls/PasswordChanger.ascx.designer.cs
@@ -56,5 +56,14 @@ namespace Umbraco.Web.UI.Umbraco.Controls {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.RegularExpressionValidator NewPasswordLengthValidator;
+        
+        /// <summary>
+        /// Div1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Div1;
     }
 }
--- src/Umbraco.Web/umbraco.presentation/umbraco/members/EditMember.aspx.cs
+++ src/Umbraco.Web/umbraco.presentation/umbraco/members/EditMember.aspx.cs
@@ -1,10 +1,14 @@
 using System;
+using System.Configuration.Provider;
 using System.Web.UI;
 using System.Web.UI.Design.WebControls;
+using System.Web.UI.HtmlControls;
 using System.Web.UI.WebControls;
 using Umbraco.Core.IO;
+using Umbraco.Web;
 using umbraco.cms.businesslogic.member;
 using System.Web.Security;
+using umbraco.controls;
 
 namespace umbraco.cms.presentation.members
 {
@@ -32,12 +36,43 @@ namespace umbraco.cms.presentation.members
         protected CustomValidator MemberEmailExistCheck = new CustomValidator();
 		protected controls.DualSelectbox _memberGroups = new controls.DualSelectbox();
 
+        private MembershipProvider MemberProvider
+        {
+            get
+            {
+                var provider = Membership.Providers[Member.UmbracoMemberProviderName];
+                if (provider == null)
+                {
+                    throw new ProviderException("The membership provider " + UmbracoSettings.DefaultBackofficeProvider + " was not found");
+                }
+                return provider;
+            }
+        }
 
 		protected void Page_Load(object sender, EventArgs e)
 		{
 
             // Add password changer
-            MemberPasswordTxt.Controls.Add(new UserControl().LoadControl(SystemDirectories.Umbraco + "/controls/passwordChanger.ascx"));
+		    var passwordChanger = (passwordChanger) LoadControl(SystemDirectories.Umbraco + "/controls/passwordChanger.ascx");
+		    passwordChanger.MembershipProviderName = Membership.Provider.Name;
+            //Add a custom validation message for the password changer
+            var passwordValidation = new CustomValidator
+            {
+                ID = "PasswordChangerValidator"
+            };
+            var validatorContainer = new HtmlGenericControl("div")
+            {
+                Visible = false,
+                EnableViewState = false
+            };
+            validatorContainer.Attributes["class"] = "error";
+            validatorContainer.Style.Add(HtmlTextWriterStyle.MarginTop, "10px");
+            validatorContainer.Style.Add(HtmlTextWriterStyle.Width, "300px");
+            var validatorContainer2 = new HtmlGenericControl("p");
+            validatorContainer.Controls.Add(validatorContainer2);
+            validatorContainer2.Controls.Add(passwordValidation);
+            MemberPasswordTxt.Controls.Add(passwordChanger);
+            MemberPasswordTxt.Controls.Add(validatorContainer);
 
             if (Member.InUmbracoMemberMode())
             {
@@ -146,7 +181,7 @@ namespace umbraco.cms.presentation.members
             var oldEmail = _document.Email.ToLower();
             var newEmail = MemberEmail.Text.ToLower();
 
-            var requireUniqueEmail = Membership.Providers[Member.UmbracoMemberProviderName].RequiresUniqueEmail;
+            var requireUniqueEmail = MemberProvider.RequiresUniqueEmail;
 
             var howManyMembersWithEmail = 0;
             var membersWithEmail = Member.GetMembersFromEmail(newEmail);
@@ -192,40 +227,58 @@ namespace umbraco.cms.presentation.members
                     {
                         tp.ErrorControl.Visible = false;
                     }
-                }    
+                }
+
+                //Change the password
+                var passwordChangerControl = (passwordChanger)MemberPasswordTxt.Controls[0];
+                var passwordChangerValidator = (CustomValidator)MemberPasswordTxt.Controls[1].Controls[0].Controls[0];
+                if (passwordChangerControl.IsChangingPassword)
+                {                    
+                    var changePassResult = UmbracoContext.Current.Security.ChangePassword(
+                        _member.UserName, passwordChangerControl.ChangingPasswordModel, MemberProvider);
+
+                    if (changePassResult.Success)
+                    {
+                        //if it is successful, we need to show the generated password if there was one, so set
+                        //that back on the control
+                        passwordChangerControl.ChangingPasswordModel.GeneratedPassword = changePassResult.Result.ResetPassword;
+                    }
+                    else
+                    {
+                        passwordChangerValidator.IsValid = false;
+                        passwordChangerValidator.ErrorMessage = changePassResult.Result.ChangeError.ErrorMessage;
+                        MemberPasswordTxt.Controls[1].Visible = true;
+                    }
+                }
 
 	            if (Member.InUmbracoMemberMode())
 	            {
+                    //TODO: This should really be done with the member provider too...
 	                _document.LoginName = MemberLoginNameTxt.Text;
 	                _document.Email = MemberEmail.Text;
 
-	                // Check if password should be changed
-	                string tempPassword = ((controls.passwordChanger) MemberPasswordTxt.Controls[0]).Password;
-	                if (tempPassword.Trim() != "")
-	                    _document.Password = tempPassword;
-
 	                // Groups
 	                foreach (ListItem li in _memberGroups.Items)
-                    {
-	                    if (("," + _memberGroups.Value + ",").IndexOf("," + li.Value + ",") > -1)
+	                {
+	                    if (("," + _memberGroups.Value + ",").IndexOf("," + li.Value + ",", StringComparison.Ordinal) > -1)
 	                    {
-	                        if (!Roles.IsUserInRole(_document.LoginName, li.Value))
+	                        if (Roles.IsUserInRole(_document.LoginName, li.Value) == false)
 	                            Roles.AddUserToRole(_document.LoginName, li.Value);
 	                    }
 	                    else if (Roles.IsUserInRole(_document.LoginName, li.Value))
 	                    {
 	                        Roles.RemoveUserFromRole(_document.LoginName, li.Value);
 	                    }
-                    }
+	                }
 
-                    //The value of the properties has been set on IData through IDataEditor in the ContentControl
-                    //so we need to 'retrieve' that value and set it on the property of the new IContent object.
-                    //NOTE This is a workaround for the legacy approach to saving values through the DataType instead of the Property 
-                    //- (The DataType shouldn't be responsible for saving the value - especically directly to the db).
-                    foreach (var item in _contentControl.DataTypes)
-                    {
-                        _document.getProperty(item.Key).Value = item.Value.Data.Value;
-                    }
+	                //The value of the properties has been set on IData through IDataEditor in the ContentControl
+	                //so we need to 'retrieve' that value and set it on the property of the new IContent object.
+	                //NOTE This is a workaround for the legacy approach to saving values through the DataType instead of the Property 
+	                //- (The DataType shouldn't be responsible for saving the value - especically directly to the db).
+	                foreach (var item in _contentControl.DataTypes)
+	                {
+	                    _document.getProperty(item.Key).Value = item.Value.Data.Value;
+	                }
 
 	                // refresh cache
 	                _document.XmlGenerate(new System.Xml.XmlDocument());
@@ -234,28 +287,23 @@ namespace umbraco.cms.presentation.members
 	            else
 	            {
 	                _member.Email = MemberEmail.Text;
-	                if (Membership.Provider.EnablePasswordRetrieval)
-	                {
-	                    string tempPassword = ((controls.passwordChanger) MemberPasswordTxt.Controls[0]).Password;
-	                    if (tempPassword.Trim() != "")
-	                        _member.ChangePassword(_member.GetPassword(), tempPassword);
-	                }
 	                Membership.UpdateUser(_member);
 	                // Groups
 	                foreach (ListItem li in _memberGroups.Items)
-	                    if (("," + _memberGroups.Value + ",").IndexOf("," + li.Value + ",") > -1)
+                    {
+	                    if (("," + _memberGroups.Value + ",").IndexOf("," + li.Value + ",", StringComparison.Ordinal) > -1)
 	                    {
-	                        if (!Roles.IsUserInRole(_member.UserName, li.Value))
+	                        if (Roles.IsUserInRole(_member.UserName, li.Value) == false)
 	                            Roles.AddUserToRole(_member.UserName, li.Value);
 	                    }
 	                    else if (Roles.IsUserInRole(_member.UserName, li.Value))
 	                    {
 	                        Roles.RemoveUserFromRole(_member.UserName, li.Value);
 	                    }
-
+                    }
 	            }
 
-                ClientTools.ShowSpeechBubble(speechBubbleIcon.save, ui.Text("speechBubbles", "editMemberSaved", base.getUser()), "");
+	            ClientTools.ShowSpeechBubble(speechBubbleIcon.save, ui.Text("speechBubbles", "editMemberSaved", base.getUser()), "");
             }
 		}
 
