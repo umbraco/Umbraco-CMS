b94f2aba70da33b203a38c815c5c107ce8d2d4e9
perploug <pph@umbraco.dk>
2013-10-22 14:58:30 +0200

    Datatype delete controller

--- src/Umbraco.Web.UI.Client/src/views/datatype/datatype.delete.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/datatype/datatype.delete.controller.js
@@ -17,7 +17,7 @@ function DataTypeDeleteController($scope, dataTypeResource, treeService, navigat
 
             //get the root node before we remove it
             var rootNode = treeService.getTreeRoot($scope.currentNode);
-
+            
             //TODO: Need to sync tree, etc...
             treeService.removeNode($scope.currentNode);
             navigationService.hideMenu();
