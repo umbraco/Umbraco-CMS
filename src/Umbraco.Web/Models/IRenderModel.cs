128a08f169bb01a4c5831ecaabfd65686336bbed
Stephan <sgay@pilotine.com>
2013-10-23 10:13:54 +0200

    Web.Mvc - Refactor UmbracoViewPage, UmbracoTemplatePage, support strongly-typed content
    Conflicts:
    	src/Umbraco.Web/Umbraco.Web.csproj

---
+++ src/Umbraco.Web/Models/IRenderModel.cs
@@ -0,0 +1,9 @@
+﻿using Umbraco.Core.Models;
+
+namespace Umbraco.Web.Models
+{
+    public interface IRenderModel
+    {
+        IPublishedContent Content { get; }
+    }
+}
--- src/Umbraco.Web/Mvc/UmbracoTemplatePage.cs
+++ src/Umbraco.Web/Mvc/UmbracoTemplatePage.cs
@@ -1,13 +1,4 @@
-using System;
-using StackExchange.Profiling;
-using Umbraco.Core;
-using Umbraco.Core.Configuration;
-using Umbraco.Core.Dictionary;
 using Umbraco.Core.Dynamics;
-using Umbraco.Core.IO;
-using Umbraco.Core.Logging;
-using Umbraco.Core.Models;
-using Umbraco.Core.Profiling;
 using Umbraco.Web.Models;
 
 namespace Umbraco.Web.Mvc
@@ -17,58 +8,19 @@ namespace Umbraco.Web.Mvc
     /// </summary>
     public abstract class UmbracoTemplatePage : UmbracoViewPage<RenderModel>
     {
-        protected UmbracoTemplatePage()
-        {
-
-        }
-
-        protected override void InitializePage()
-        {
-            base.InitializePage();
-            //set the model to the current node if it is not set, this is generally not the case
-            if (Model != null)
-            {
-                ////this.ViewData.Model = Model;
-                //var backingItem = new DynamicBackingItem(Model.CurrentNode);
-                var dynamicNode = new DynamicPublishedContent(Model.Content);
-                CurrentPage = dynamicNode.AsDynamic();
-            }
-        }
-
-        protected override void SetViewData(System.Web.Mvc.ViewDataDictionary viewData)
-        {
-            //Here we're going to check if the viewData's model is of IPublishedContent, this is basically just a helper for
-            //syntax on the front-end so we can just pass in an IPublishedContent object to partial views that inherit from
-            //UmbracoTemplatePage. Then we're going to manually contruct a RenderViewModel to pass back in to SetViewData			
-            if (viewData.Model is IPublishedContent)
-            {
-                //change the model to a RenderModel and auto set the culture
-                viewData.Model = new RenderModel((IPublishedContent)viewData.Model, UmbracoContext.PublishedContentRequest.Culture);
-            }
-
-            base.SetViewData(viewData);
-        }
+        private object _currentPage;
 
         /// <summary>
-        /// Returns the a DynamicPublishedContent object
+        /// Returns the content as a dynamic object
         /// </summary>
-        public dynamic CurrentPage { get; private set; }
-
-        private UmbracoHelper _helper;
-
-        /// <summary>
-        /// Gets an UmbracoHelper
-        /// </summary>
-        /// <remarks>
-        /// This ensures that the UmbracoHelper is constructed with the content model of this view
-        /// </remarks>
-        public override UmbracoHelper Umbraco
+        public dynamic CurrentPage
         {
             get
             {
-                return _helper ?? (_helper = Model == null
-                                                 ? new UmbracoHelper(UmbracoContext)
-                                                 : new UmbracoHelper(UmbracoContext, Model.Content));
+                // it's invalid to create a DynamicPublishedContent around a null content anyway
+                // fixme - should we return null or DynamicNull.Null?
+                if (Model == null || Model.Content == null) return null;
+                return _currentPage ?? (_currentPage = Model.Content.AsDynamic());
             }
         }
     }
