94de454264dda5ad3b16012fe2e3c82059e0cd3c
Shannon <sdeminick@gmail.com>
2013-11-04 17:24:25 +1100

    Fixing unit tests

--- src/umbraco.interfaces/ITaskReturnUrl.cs
+++ src/umbraco.interfaces/ITaskReturnUrl.cs
@@ -1,5 +1,8 @@
+using System;
+
 namespace umbraco.interfaces
 {
+    [Obsolete("ITask is used for legacy webforms back office editors, change to using the v7 angular approach")]
 	public interface ITaskReturnUrl : ITask
 	{
 		string ReturnUrl {get;}
