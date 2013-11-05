fed8f51de8fbb03af6a17544169754cd8c62de01
Sebastiaan Janssen <sebastiaan@umbraco.com>
2013-11-05 12:00:56 +0100

    Prefixed built in controllers to better avoid conflicts with people's custom code

---
+++ src/Umbraco.Web/Controllers/UmbLoginController.cs
@@ -0,0 +1,28 @@
+﻿using System.Linq;
+using System.Web.Mvc;
+using umbraco.cms.businesslogic.member;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+
+namespace Umbraco.Web.Controllers
+{
+    public class UmbLoginController : SurfaceController
+    {
+        [HttpPost]
+        public ActionResult HandleLogin([Bind(Prefix="loginModel")]LoginModel model)
+        {
+            // TODO: Use new Member API
+            if (ModelState.IsValid)
+            {
+                var m = Member.GetMemberFromLoginNameAndPassword(model.Username, model.Password);
+                if (m != null)
+                {
+                    Member.AddMemberToCache(m);
+                    return Redirect("/");
+                }
+            }
+
+            return CurrentUmbracoPage();
+        }
+    }
+}
