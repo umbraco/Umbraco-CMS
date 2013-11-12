c28f800fe6ed48ea766f5752a3241df7ce103426
Shannon <sdeminick@gmail.com>
2013-11-12 17:38:09 +1100

    Removes the re-committed app plugins property editor that shouldn't be there

--- src/Umbraco.Web.UI/App_Plugins/SimpleEditor/PropertyEditors/js/simpleeditor.js
+++
@@ -1,6 +0,0 @@
-(function () {
-    angular.module("umbraco").controller("Umbraco.Editors.SimpleEditorController",
-        function ($rootScope, $scope, notificationsService, dialogService) {
-           
-        });
-})();
