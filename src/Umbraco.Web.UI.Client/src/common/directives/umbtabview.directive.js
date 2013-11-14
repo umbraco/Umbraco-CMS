378fc2c655d983af1587c21cf78a71deeced42d0
perploug <pph@umbraco.dk>
2013-11-14 09:16:35 +0100

    fixes datatype editor buttons not snapping to buttom

--- src/Umbraco.Web.UI.Client/src/common/directives/umbtabview.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/umbtabview.directive.js
@@ -9,50 +9,6 @@ angular.module("umbraco.directives")
 		restrict: 'E',
 		replace: true,
 		transclude: 'true',
-		templateUrl: 'views/directives/umb-tab-view.html',
-
-		link: function (scope, el, attrs) {
-			
-			function _setClass(resize){
-				var bar = $(".tab-content .active .umb-tab-buttons");
-
-				//no need to process
-				if(resize){
-					bar.removeClass("umb-bottom-bar");
-				}	
-
-				//already positioned
-				if(bar.hasClass("umb-bottom-bar")){
-					return;
-				}
-
-				var offset = bar.offset();
-
-				if(offset){
-					var bottom = bar.offset().top + bar.height();
-		            if(bottom > $(window).height()){
-						bar.addClass("umb-bottom-bar");
-						$(".tab-content .active").addClass("with-buttons");
-					}else{
-						bar.removeClass("umb-bottom-bar");
-						$(".tab-content .active").removeClass("with-buttons");
-					}	
-				}
-			}
-
-
-			//initial loading
-			$timeout(function(){
-				$('a[data-toggle="tab"]').on('shown', function (e) {
-					_setClass();	
-				});
-
-				_setClass();
-			}, 500, false);
-			
-			$(window).bind("resize", function () {
-			  _setClass(true);
-			});	
-		}
+		templateUrl: 'views/directives/umb-tab-view.html'
 	};
 });
