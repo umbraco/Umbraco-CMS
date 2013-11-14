9eb9b8df92d14d0996cbed135d71732b73f488cf
Shannon <sdeminick@gmail.com>
2013-11-14 20:32:11 +1100

    Fixes: U4-3501 Error when selecting multiple items with the media picker in v7 - ensures the standard media picker cannot multi-select, hides the add button when multi select is disabled and there's a value.

--- src/Umbraco.Web/PropertyEditors/MediaPickerPropertyEditor.cs
+++ src/Umbraco.Web/PropertyEditors/MediaPickerPropertyEditor.cs
@@ -12,12 +12,28 @@ namespace Umbraco.Web.PropertyEditors
     [PropertyEditor(Constants.PropertyEditors.MediaPickerAlias, "Media Picker", "INT", "mediapicker")]
     public class MediaPickerPropertyEditor : PropertyEditor
     {
+        public MediaPickerPropertyEditor()
+        {
+            _defaultPreValues = new Dictionary<string, object>
+                {
+                    {"multiPicker", false}
+                };
+        }
+
+        private IDictionary<string, object> _defaultPreValues;
+
         protected override PropertyValueEditor CreateValueEditor()
         {
             //TODO: Need to add some validation to the ValueEditor to ensure that any media chosen actually exists!
 
             return base.CreateValueEditor();
+            
         }
 
+        public override IDictionary<string, object> DefaultPreValues
+        {
+            get { return _defaultPreValues; }
+            set { _defaultPreValues = value; }
+        }
     }
 }
