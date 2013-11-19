6cf9312d1a85628c44c882fe8bebe3220b3e2165
perploug <pph@umbraco.dk>
2013-11-19 20:24:19 +0100

    editorState docs

--- src/Umbraco.Web.UI.Client/src/common/services/appstate.service.js
+++ src/Umbraco.Web.UI.Client/src/common/services/appstate.service.js
@@ -75,7 +75,7 @@ function appState($rootScope) {
          * @function
          *
          * @description
-         * Returns the current global state value by key - we do not return an object here - we do NOT want this
+         * Returns the current global state value by key - we do not return an object reference here - we do NOT want this
          * to be publicly mutable and allow setting arbitrary values
          *
          */
@@ -203,13 +203,52 @@ angular.module('umbraco.services').factory("editorState", function() {
 
     var current = null;
     var state = {
+
+        /**
+         * @ngdoc function
+         * @name umbraco.services.angularHelper#set
+         * @methodOf umbraco.services.editorState
+         * @function
+         *
+         * @description
+         * Sets the current entity object for the currently active editor
+         * This is only used when implementing an editor with a complex model
+         * like the content editor, where the model is modified by several
+         * child controllers. 
+         */
         set: function (entity) {
             current = entity;
         },
+
+        /**
+         * @ngdoc function
+         * @name umbraco.services.angularHelper#reset
+         * @methodOf umbraco.services.editorState
+         * @function
+         *
+         * @description
+         * Since the editorstate entity is read-only, you cannot set it to null
+         * only through the reset() method
+         */
         reset: function() {
             current = null;
         }
-        
+
+        /**
+         * @ngdoc function
+         * @name umbraco.services.angularHelper#current
+         * @methodOf umbraco.services.editorState
+         * @function
+         *
+         * @description
+         * Returns an object reference to the current editor entity.
+         * the entity is the root object of the editor.
+         * EditorState is used by property/parameter editors that need
+         * access to the entire entity being edited, not just the property/parameter 
+         *
+         * editorState.current can not be overwritten, you should only read values from it
+         * since modifying individual properties should be handled by the property editors
+         */
     };
 
     //create a get/set property but don't allow setting
