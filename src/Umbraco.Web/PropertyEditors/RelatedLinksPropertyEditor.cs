b624d87a59b028c8682523aeabfd03a0b82c011b
Tim Geyssens <tg@umbraco.com>
2013-11-21 11:17:28 +0100

    make sure related links datatype value type is set to JSON

--- src/Umbraco.Web/PropertyEditors/RelatedLinksPropertyEditor.cs
+++ src/Umbraco.Web/PropertyEditors/RelatedLinksPropertyEditor.cs
@@ -8,7 +8,7 @@ using Umbraco.Core.PropertyEditors;
 
 namespace Umbraco.Web.PropertyEditors
 {
-    [PropertyEditor(Constants.PropertyEditors.RelatedLinksAlias, "Related links", "relatedlinks")]
+    [PropertyEditor(Constants.PropertyEditors.RelatedLinksAlias, "Related links", "relatedlinks", ValueType ="JSON")]
     public class RelatedLinksPropertyEditor : PropertyEditor
     {
     }
