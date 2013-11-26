7535585b6321a61d7559abac269f3fa63fa38857
Shannon <sdeminick@gmail.com>
2013-11-27 10:22:45 +1100

    Fixes: U4-3720 Color picker prevalues isn't saved as an array and therefore "model.config.items.length === 0" doesn't work

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/colorpicker/colorpicker.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/colorpicker/colorpicker.controller.js
@@ -2,6 +2,7 @@ function ColorPickerController($scope) {
     $scope.selectItem = function (color) {
         $scope.model.value = color;
     };
+    $scope.isConfigured = $scope.model.config && $scope.model.config.items && _.keys($scope.model.config.items).length > 0;
 }
 
 angular.module("umbraco").controller("Umbraco.PropertyEditors.ColorPickerController", ColorPickerController);
