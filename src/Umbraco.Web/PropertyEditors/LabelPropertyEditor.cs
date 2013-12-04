176ec0edc05f16a8e846194daebe4b89cf31ac08
Shannon <sdeminick@gmail.com>
2013-12-04 17:13:26 +1100

    Fixes: U4-3651 What is the "Pick multiple items" prevalue on MNTP for? and removes the remaining uGoLive references.

--- src/Umbraco.Web/PropertyEditors/LabelPropertyEditor.cs
+++ src/Umbraco.Web/PropertyEditors/LabelPropertyEditor.cs
@@ -21,6 +21,25 @@ namespace Umbraco.Web.PropertyEditors
         {
             return new LabelPreValueEditor();
         }
+        
+        /// <summary>
+        /// Custom value editor to mark it as readonly
+        /// </summary>
+        internal class LabelPropertyValueEditor : PropertyValueEditorWrapper
+        {
+            public LabelPropertyValueEditor(PropertyValueEditor wrapped)
+                : base(wrapped)
+            {
+            }
+
+            /// <summary>
+            /// This editor is for display purposes only, any values bound to it will not be saved back to the database
+            /// </summary>
+            public override bool IsReadOnly
+            {
+                get { return true; }
+            }
+        }
 
         internal class LabelPreValueEditor : PreValueEditor
         {
--- src/Umbraco.Web/PropertyEditors/MultiNodeTreePickerPropertyEditor.cs
+++ src/Umbraco.Web/PropertyEditors/MultiNodeTreePickerPropertyEditor.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Umbraco.Core;
+using Umbraco.Core.Models;
 using Umbraco.Core.PropertyEditors;
 
 namespace Umbraco.Web.PropertyEditors
@@ -15,10 +16,10 @@ namespace Umbraco.Web.PropertyEditors
         {
             _internalPreValues = new Dictionary<string, object>
                 {
-                    {"multiPicker", true}
+                    {"multiPicker", "1"}
                 };
         }
-  
+        
         protected override PreValueEditor CreatePreValueEditor()
         {
             return new MultiNodePickerPreValueEditor();
@@ -35,9 +36,6 @@ namespace Umbraco.Web.PropertyEditors
         {
             [PreValueField("startNode", "Node type", "treesource")]
             public string StartNode { get; set; }
-
-            [PreValueField("multiPicker", "Pick multiple items", "boolean")]
-            public bool MultiPicker { get; set; }
             
             [PreValueField("filter", "Filter out items with type", "textstring", Description = "Seperate with comma")]
             public string Filter { get; set; }
@@ -47,6 +45,43 @@ namespace Umbraco.Web.PropertyEditors
 
             [PreValueField("maxNumber", "Maximum number of items", "number")]
             public string MaxNumber { get; set; }
+
+            /// <summary>
+            /// This ensures the multiPicker pre-val is set based on the maxNumber of nodes set
+            /// </summary>
+            /// <param name="defaultPreVals"></param>
+            /// <param name="persistedPreVals"></param>
+            /// <returns></returns>
+            /// <remarks>
+            /// Due to compatibility with 7.0.0 the multiPicker pre-val might already exist in the db, but we've removed that setting in 7.0.1 so we need to detect it and if it is
+            /// there, then we'll set the maxNumber to '1'
+            /// </remarks>
+            public override IDictionary<string, object> ConvertDbToEditor(IDictionary<string, object> defaultPreVals, PreValueCollection persistedPreVals)
+            {
+                var result = base.ConvertDbToEditor(defaultPreVals, persistedPreVals);
+
+                //backwards compatibility check
+                if (result.ContainsKey("multiPicker") && result["multiPicker"].ToString() == "0")
+                {
+                    result["maxNumber"] = "1";
+                }
+
+                //set the multiPicker val correctly depending on the maxNumber
+                var asNumber = result["maxNumber"].TryConvertTo<int>();
+                if (asNumber.Success)
+                {
+                    if (asNumber.Result <= 1)
+                    {
+                        result["multiPicker"] = "0";
+                    }
+                    else
+                    {
+                        result["multiPicker"] = "1";
+                    }
+                }
+
+                return result;
+            }
 
         }
     }
