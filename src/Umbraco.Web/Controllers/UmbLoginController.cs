a64dbb172edbf1c0e8b28c5af4cb7f4b2c701fc7
Shannon <sdeminick@gmail.com>
2013-12-05 16:20:02 +1100

    undo my custom test change to login controller

--- src/Umbraco.Web/Controllers/UmbLoginController.cs
+++ src/Umbraco.Web/Controllers/UmbLoginController.cs
@@ -3,7 +3,6 @@ using System.Web.Mvc;
 using umbraco.cms.businesslogic.member;
 using Umbraco.Web.Models;
 using Umbraco.Web.Mvc;
-using umbraco.cms.businesslogic.web;
 
 namespace Umbraco.Web.Controllers
 {
@@ -12,9 +11,6 @@ namespace Umbraco.Web.Controllers
         [HttpPost]
         public ActionResult HandleLogin([Bind(Prefix="loginModel")]LoginModel model)
         {
-            var doc = new Document(1089);
-            var prop = doc.getProperty("cardinal");
-
             // TODO: Use new Member API
             if (ModelState.IsValid)
             {
