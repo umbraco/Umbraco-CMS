d0a5892f9f79c120cf637341967fd2de46a1f955
Shannon <sdeminick@gmail.com>
2013-12-10 14:13:56 +1100

    Fixes: U4-3796 No media macro parameter in v7

---
+++ src/Umbraco.Web/PropertyEditors/ParameterEditors/MultipleMediaPickerParameterEditor.cs
@@ -0,0 +1,14 @@
+﻿using Umbraco.Core;
+using Umbraco.Core.PropertyEditors;
+
+namespace Umbraco.Web.PropertyEditors.ParameterEditors
+{
+    [ParameterEditor(Constants.PropertyEditors.MultipleMediaPickerAlias, "Multiple Media Picker", "mediapicker")]
+    public class MultipleMediaPickerParameterEditor : ParameterEditor
+    {
+        public MultipleMediaPickerParameterEditor()
+        {
+            Configuration.Add("multiPicker", "1");
+        }
+    }
+}
