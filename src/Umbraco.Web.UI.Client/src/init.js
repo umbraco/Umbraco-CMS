68aa3f7b1b283c7f5d3fe22f6fe1a992f09d7b32
perploug <pph@umbraco.dk>
2013-12-10 12:15:40 +0100

    broke touch detection, reverting

--- src/Umbraco.Web.UI.Client/src/init.js
+++ src/Umbraco.Web.UI.Client/src/init.js
@@ -64,7 +64,6 @@ app.run(['userService', '$log', '$rootScope', '$location', 'navigationService', 
 
         //check for touch device, add to global appState
         //var touchDevice = ("ontouchstart" in window || window.touch || window.navigator.msMaxTouchPoints === 5 || window.DocumentTouch && document instanceof DocumentTouch);
-        var touchDevice = true; //  /android|webos|iphone|ipad|ipod|blackberry|iemobile|touch/i.test(navigator.userAgent.toLowerCase());
-
+        var touchDevice =  /android|webos|iphone|ipad|ipod|blackberry|iemobile|touch/i.test(navigator.userAgent.toLowerCase());
         appState.setGlobalState("touchDevice", touchDevice);
     }]);
