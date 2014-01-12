f674fdf014d2e6ffbc9481130a59e978a53f1646
Robert Foster <rfoster@refoster.com.au>
2014-01-13 03:26:55 +1100

    Implemented spectrum for picking colors for the color picker pre-value editor and added broader bgColor directive

---
+++ src/Umbraco.Web.UI.Client/src/common/directives/backgroundcolor.directive.js
@@ -0,0 +1,39 @@
+﻿
+/**
+* @ngdoc directive
+* @name umbraco.directives.directive:bgColor
+* @restrict A
+* @description Used to set a background color on an element without any validation.
+**/
+function bgColor(assetsService) {
+    return {
+        restrict: "A",
+        link: function (scope, element, attr, formCtrl) {
+
+            var origColor = null;
+            if (attr.bgOrig) {
+                //set the orig based on the attribute if there is one
+                origColor = attr.bgOrig;
+            }
+
+            attr.$observe("bgColor", function (newVal) {
+                if (newVal) {
+                    if (!origColor) {
+                        //get the orig color before changing it
+                        origColor = element.css("background-color");
+                    }
+
+                    assetsService.loadJs("lib/tinycolor/tinycolor.js").then(function () {
+                        var t = tinycolor(newVal);
+                        if (t.ok) {
+                            element.css("background-color", t.toString());
+                            return;
+                        }
+                    });
+                }
+                element.css("background-color", origColor);
+            });
+        }
+    };
+}
+angular.module('umbraco.directives').directive("bgColor", bgColor);
---
+++ src/Umbraco.Web.UI.Client/src/views/prevalueeditors/multicolorpicker.controller.js
@@ -0,0 +1,67 @@
+﻿function NewColorPicker($scope, $timeout, assetsService, angularHelper) {
+    assetsService.loadJs("lib/spectrum/spectrum.js").then(function () {
+        var elem = $("#newColor");
+        elem.spectrum({
+            color: '#000',
+            showInitial: true,
+            chooseText: "choose", // TODO: These can be localised
+            cancelText: "cancel", // TODO: These can be localised
+            preferredFormat: "hex",
+            showInput: true,
+            clickoutFiresChange: true
+        });
+    });
+}
+
+angular.module("umbraco").controller("Umbraco.Editors.MultiColorPickerController",
+    function ($scope, $timeout, assetsService, angularHelper) {
+        //NOTE: We need to make each color an object, not just a string because you cannot 2-way bind to a primitive.
+
+        $scope.newColor = "";
+        $scope.hasError = false;
+
+        NewColorPicker($scope, $timeout, assetsService, angularHelper);
+
+        if (!angular.isArray($scope.model.value)) {
+            //make an array from the dictionary
+            var items = [];
+            for (var i in $scope.model.value) {
+                items.push({
+                    value: $scope.model.value[i],
+                    id: i
+                });
+            }
+            //now make the editor model the array
+            $scope.model.value = items;
+        }
+
+        $scope.remove = function (item, evt) {
+
+            evt.preventDefault();
+
+            $scope.model.value = _.reject($scope.model.value, function (x) {
+                return x.value === item.value;
+            });
+
+        };
+
+        $scope.add = function (evt) {
+
+            evt.preventDefault();
+
+            if ($scope.newColor) {
+                if (!_.contains($scope.model.value, $scope.newColor)) {
+                    $scope.model.value.push({ value: $scope.newColor });
+                    $scope.newColor = "";
+                    $scope.hasError = false;
+                    return;
+                }
+            }
+
+            //there was an error, do the highlight (will be set back by the directive)
+            $scope.hasError = true;
+        };
+
+        //load the separate css for the editor to avoid it blocking our js loading
+        assetsService.loadCss("lib/spectrum/spectrum.css");
+    });
