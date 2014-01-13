23b0c898485195d3673eaf092ccdda73cc201fae
Shannon <sdeminick@gmail.com>
2014-01-13 17:28:49 +1100

    ensures the mail usage is disposed.

--- src/Umbraco.Core/Services/NotificationService.cs
+++ src/Umbraco.Core/Services/NotificationService.cs
@@ -323,8 +323,14 @@ namespace Umbraco.Core.Services
                 {
                     try
                     {
-                        var sender = new SmtpClient();
-                        sender.Send(mail);
+                        using (mail)
+                        {
+                            using (var sender = new SmtpClient())
+                            {
+                                sender.Send(mail);        
+                            }
+                        }
+                        
                     }
                     catch (Exception ex)
                     {
