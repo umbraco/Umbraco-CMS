334a56ce5c740816fa9668461497250f15161489
Shannon <sdeminick@gmail.com>
2014-01-17 12:25:34 +1100

    Fixes: U4-3765 v7 MultiNode Treepicker Sort not working

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/contentpicker/contentpicker.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/contentpicker/contentpicker.controller.js
@@ -54,39 +54,36 @@ angular.module('umbraco')
 
 		$scope.remove =function(index){
 			$scope.renderModel.splice(index, 1);
-			$scope.ids.splice(index, 1);
-			$scope.model.value = trim($scope.ids.join(), ",");
 		};
 
 
 		$scope.add =function(item){
 			if($scope.ids.indexOf(item.id) < 0){
 				item.icon = iconHelper.convertFromLegacyIcon(item.icon);
-
-				$scope.ids.push(item.id);
 				$scope.renderModel.push({name: item.name, id: item.id, icon: item.icon});
-				$scope.model.value = trim($scope.ids.join(), ",");
 			}	
 		};
 
 	    $scope.clear = function() {
-	        $scope.model.value = "";
 	        $scope.renderModel = [];
-	        $scope.ids = [];
-	    };
-	   
-	    $scope.sortableOptions = {
-	        update: function(e, ui) {
-	        	var r = [];
-	        	angular.forEach($scope.renderModel, function(value, key){
-	        		r.push(value.id);
-	        	});
-
-	        	$scope.ids = r;
-	        	$scope.model.value = trim($scope.ids.join(), ",");
-	        }
 	    };
 
+	    //We need to watch our renderModel so that we can update the underlying $scope.model.value properly, this is required
+	    // because the ui-sortable doesn't dispatch an event after the digest of the sort operation. Any of the events for UI sortable
+	    // occur after the DOM has updated but BEFORE the digest has occured so the model has NOT changed yet - it even states so in the docs.
+	    // In their source code there is no event so we need to just subscribe to our model changes here.
+        //This also makes it easier to manage models, we update one and the rest will just work.
+	    $scope.$watch(function() {
+	        //return the joined Ids as a string to watch
+	        return _.map($scope.renderModel, function(i) {
+	            return i.id;
+	        }).join();
+	    }, function (newVal) {
+	        $scope.ids = _.map($scope.renderModel, function (i) {
+	            return i.id;
+	        });
+	        $scope.model.value = trim($scope.ids.join(), ",");
+	    });
 
 	    $scope.$on("formSubmitting", function (ev, args) {
 			$scope.model.value = trim($scope.ids.join(), ",");
