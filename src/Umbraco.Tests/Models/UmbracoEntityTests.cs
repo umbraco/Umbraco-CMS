a38e0dcf863251d5fb6971e8bbaa5d1c99a40075
Morten Christensen <mc@sitereactor.dk>
2014-02-12 12:47:47 +0100

    Adding integer overload for UmbracoEntity so it can be used with a dynamic object containing either a boolean or integer retrieved from the database to initialize the UmbracoEntity object.

---
+++ src/Umbraco.Tests/Models/UmbracoEntityTests.cs
@@ -0,0 +1,22 @@
+﻿using NUnit.Framework;
+using Umbraco.Core.Models;
+
+namespace Umbraco.Tests.Models
+{
+    [TestFixture]
+    public class UmbracoEntityTests
+    {
+        [Test]
+        public void UmbracoEntity_Can_Be_Initialized_From_Dynamic()
+        {
+            var boolIsTrue = true;
+            var intIsTrue = 1;
+
+            var trashedWithBool = new UmbracoEntity((dynamic)boolIsTrue);
+            var trashedWithInt = new UmbracoEntity((dynamic)intIsTrue);
+
+            Assert.IsTrue(trashedWithBool.Trashed);
+            Assert.IsTrue(trashedWithInt.Trashed);
+        }
+    }
+}
