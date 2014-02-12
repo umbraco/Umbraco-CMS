e5bab346d4da8d266eaaf823c7213b446796fa39
jakobdyrby <jad@netmester.dk>
2014-02-12 14:59:14 +0100

    more updates

--- src/Umbraco.Core/Services/PackagingService.cs
+++ src/Umbraco.Core/Services/PackagingService.cs
@@ -1023,26 +1023,36 @@ namespace Umbraco.Core.Services
             throw new NotImplementedException();
 
 
-            foreach (XmlNode n in xmlNodeList.OfType<XmlNode>())
-            {
-                StyleSheet s = StyleSheet.MakeNew(
-                    currentUser,
-                    XmlHelper.GetNodeValue(n.SelectSingleNode("Name")),
-                    XmlHelper.GetNodeValue(n.SelectSingleNode("FileName")),
-                    XmlHelper.GetNodeValue(n.SelectSingleNode("Content")));
+            //foreach (XmlNode n in xmlNodeList.OfType<XmlNode>())
+            //{
+            //    StyleSheet s = StyleSheet.MakeNew(
+            //        currentUser,
+            //        XmlHelper.GetNodeValue(n.SelectSingleNode("Name")),
+            //        XmlHelper.GetNodeValue(n.SelectSingleNode("FileName")),
+            //        XmlHelper.GetNodeValue(n.SelectSingleNode("Content")));
 
-                foreach (XmlNode prop in n.SelectNodes("Properties/Property"))
-                {
-                    StylesheetProperty sp = StylesheetProperty.MakeNew(
-                        xmlHelper.GetNodeValue(prop.SelectSingleNode("Name")),
-                        s,
-                        currentUser);
-                    sp.Alias = XmlHelper.GetNodeValue(prop.SelectSingleNode("Alias"));
-                    sp.value = XmlHelper.GetNodeValue(prop.SelectSingleNode("Value"));
-                }
-                s.saveCssToFile();
-                s.Save();
-            }
+            //    foreach (XmlNode prop in n.SelectNodes("Properties/Property"))
+            //    {
+            //        StylesheetProperty sp = StylesheetProperty.MakeNew(
+            //            xmlHelper.GetNodeValue(prop.SelectSingleNode("Name")),
+            //            s,
+            //            currentUser);
+            //        sp.Alias = XmlHelper.GetNodeValue(prop.SelectSingleNode("Alias"));
+            //        sp.value = XmlHelper.GetNodeValue(prop.SelectSingleNode("Value"));
+            //    }
+            //    s.saveCssToFile();
+            //    s.Save();
+            //}
+        }
+
+        public IEnumerable<IMacro> ImportMacros(XElement xElement, int userId = 0)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IEnumerable<IDictionaryItem> ImportDictionaryItems(XElement xElement, int userId = 0)
+        {
+            throw new NotImplementedException();
         }
 
         private bool IsMasterPageSyntax(string code)
