acc6d46b4b4089381aa50fe96a19b7ee5b15f701
jakobdyrby <jad@netmester.dk>
2014-02-13 16:18:08 +0100

    splittet into more single resposebility classes

---
+++ src/Umbraco.Core/Services/PackageValidationHelper.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+using Umbraco.Core.Models;
+
+namespace Umbraco.Core.Services
+{
+    public class PackageValidationHelper : IPackageValidationHelper
+    {
+        private readonly IMacroService _macroService;
+        private readonly IFileService _fileService;
+
+        public PackageValidationHelper(IMacroService macroService,
+            IFileService fileService)
+        {
+            if (fileService != null) _fileService = fileService;
+            else throw new ArgumentNullException("fileService");
+            if (macroService != null) _macroService = macroService;
+            else throw new ArgumentNullException("macroService");
+        }
+
+        public bool StylesheetExists(string styleSheetName, out Stylesheet existingStyleSheet)
+        {
+            existingStyleSheet = _fileService.GetStylesheets(styleSheetName).SingleOrDefault();
+            return existingStyleSheet != null;
+        }
+
+        public bool TemplateExists(string templateAlias, out ITemplate existingTemplate)
+        {
+            existingTemplate = _fileService.GetTemplate(templateAlias);
+            return existingTemplate != null;
+        }
+
+        public bool MacroExists(string macroAlias, out IMacro existingMacro)
+        {
+            existingMacro = _macroService.GetByAlias(macroAlias);
+            return macroAlias != null;
+        }
+    }
+}
