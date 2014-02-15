f822400036b2730c9e09b6e2c15f577259d3e641
Sebastiaan Janssen <sebastiaan@umbraco.com>
2014-02-15 14:24:40 +0100

    Fix broken build

--- src/Umbraco.Core/Models/Template.cs
+++ src/Umbraco.Core/Models/Template.cs
@@ -187,7 +187,7 @@ namespace Umbraco.Core.Models
 
         public void SetMasterTemplate(ITemplate masterTemplate)
         {
-            MasterTemplateId = new Lazy<int>(() => {return masterTemplate.Id});
+            MasterTemplateId = new Lazy<int>(() => { return masterTemplate.Id; });
         }
     }
 }
