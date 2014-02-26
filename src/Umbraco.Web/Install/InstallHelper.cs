429b5f7c0168770e1543f2c05c1052846d89b362
Shannon <sdeminick@gmail.com>
2014-02-26 16:49:35 +0100

    Updates the completion stage of the installer

--- src/Umbraco.Web/Install/InstallHelper.cs
+++ src/Umbraco.Web/Install/InstallHelper.cs
@@ -17,6 +17,8 @@ namespace Umbraco.Web.Install
             UmbracoContext umbracoContext,
             InstallStatus status)
         {
+            //TODO: Add UserToken step to save our user token with Mother
+
             var steps = new List<InstallSetupStep>();
             if (status == InstallStatus.NewInstall)
             {
@@ -54,6 +56,9 @@ namespace Umbraco.Web.Install
                     new StarterKitCleanupStep()
                     {
                         ServerOrder = 7,
+                    },
+                    new SetUmbracoVersionStep(umbracoContext.Application, umbracoContext.HttpContext) {
+                        ServerOrder = 8
                     }
                 });
                 return steps;
@@ -81,71 +86,5 @@ namespace Umbraco.Web.Install
                 return false;
             }
         }
-
-        //private static readonly InstallerStepCollection Steps = new InstallerStepCollection
-        //    {
-        //        new Steps.Welcome(),
-        //        new Steps.License(),
-        //        new Steps.FilePermissions(),
-        //        new Steps.MajorUpgradeReport(),
-        //        new Steps.Database(),
-        //        new Steps.DefaultUser(),
-        //        //new Steps.RenderingEngine(),
-        //        new Steps.Skinning(),
-        //        new Steps.WebPi(),
-        //        new Steps.TheEnd()
-        //    };
-
-        //internal static InstallerStepCollection InstallerSteps
-        //{
-        //    get { return Steps; }
-        //}
-
-        //public static void RedirectToNextStep(Page page, string currentStep)
-        //{
-        //    var s = InstallerSteps.GotoNextStep(currentStep);
-        //    page.Response.Redirect("?installStep=" + s.Alias);
-        //}
-
-        //public static void RedirectToLastStep(Page page)
-        //{
-        //    var s = InstallerSteps.Get("theend");
-        //    page.Response.Redirect("?installStep=" + s.Alias);
-        //}
-
-
-        //private static int _percentage = -1;
-        //public static int Percentage 
-        //{ 
-        //    get { return _percentage; } 
-        //    set { _percentage = value; } 
-        //}
-
-        //public static string Description { get; set; }
-        //public static string Error { get; set; }
-
-
-        //public static void ClearProgress()
-        //{
-        //    Percentage = -1;
-        //    Description = string.Empty;
-        //    Error = string.Empty;
-        //}
-
-        //public static void SetProgress(int percent, string description, string error)
-        //{
-        //    if (percent > 0)
-        //        Percentage = percent;
-
-        //    Description = description;
-        //    Error = error;
-        //}
-
-        //public static string GetProgress()
-        //{
-        //    var pr = new ProgressResult(Percentage, Description, Error);
-        //    var js = new JavaScriptSerializer();
-        //    return js.Serialize(pr);
-        //}
     }
 }
