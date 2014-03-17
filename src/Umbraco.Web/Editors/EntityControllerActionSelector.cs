c669a2702b5b3a0485b7798c699167c290c89b00
Shannon <sdeminick@gmail.com>
2014-03-17 18:34:41 +1100

    Fixes: U4-4210 MNTP not working with Member and ensures the content picker has real validation built into it. Fixes up the EntityController switching out the global action selector.

--- src/Umbraco.Web/Editors/EntityControllerActionSelector.cs
+++ src/Umbraco.Web/Editors/EntityControllerActionSelector.cs
@@ -1,18 +1,19 @@
 using System;
 using System.Web;
 using System.Web.Http.Controllers;
+using Umbraco.Core;
 
 namespace Umbraco.Web.Editors
 {
     /// <summary>
-    /// This allows for calling GetById with a GUID... so it will automatically route to GetByKey
+    /// This allows for calling GetById/GetByIds with a GUID... so it will automatically route to GetByKey/GetByKeys
     /// </summary>
     internal class EntityControllerActionSelector : ApiControllerActionSelector 
     {
 
         public override HttpActionDescriptor SelectAction(HttpControllerContext controllerContext)
         {
-            if (controllerContext.Request.RequestUri.GetLeftPart(UriPartial.Path).ToUpper().EndsWith("GETBYID"))
+            if (controllerContext.Request.RequestUri.GetLeftPart(UriPartial.Path).InvariantEndsWith("GetById"))
             {
                 var id = HttpUtility.ParseQueryString(controllerContext.Request.RequestUri.Query).Get("id");
 
@@ -27,6 +28,33 @@ namespace Umbraco.Web.Editors
                         {
                             return new ReflectedHttpActionDescriptor(controllerContext.ControllerDescriptor, method);    
                         }                        
+                    }
+                }
+            }
+
+            if (controllerContext.Request.RequestUri.GetLeftPart(UriPartial.Path).InvariantEndsWith("GetByIds"))
+            {
+                var ids = HttpUtility.ParseQueryString(controllerContext.Request.RequestUri.Query).GetValues("ids");
+
+                if (ids != null)
+                {
+                    var allmatched = true;
+                    foreach (var id in ids)
+                    {
+                        Guid parsed;
+                        if (Guid.TryParse(id, out parsed) == false)
+                        {
+                            allmatched = false;                            
+                        }
+                    }
+                    if (allmatched)
+                    {
+                        var controllerType = controllerContext.Controller.GetType();
+                        var method = controllerType.GetMethod("GetByKeys");
+                        if (method != null)
+                        {
+                            return new ReflectedHttpActionDescriptor(controllerContext.ControllerDescriptor, method);
+                        }
                     }
                 }
             }
