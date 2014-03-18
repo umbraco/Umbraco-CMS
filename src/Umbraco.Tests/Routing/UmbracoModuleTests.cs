9e466971af74d1d95e8ca1e4c9675f1baa20143d
Shannon <sdeminick@gmail.com>
2014-03-18 14:18:34 +1100

    fixes some test logic

--- src/Umbraco.Tests/Routing/UmbracoModuleTests.cs
+++ src/Umbraco.Tests/Routing/UmbracoModuleTests.cs
@@ -32,8 +32,10 @@ namespace Umbraco.Tests.Routing
             //SettingsForTests.ReservedPaths = "~/umbraco,~/install/";
             //SettingsForTests.ReservedUrls = "~/config/splashes/booting.aspx,~/install/default.aspx,~/config/splashes/noNodes.aspx,~/VSEnterpriseHelper.axd";
 
+		    Directory.CreateDirectory(Path.GetDirectoryName(IOHelper.MapPath(SystemFiles.NotFoundhandlersConfig, false)));
+
 			//create the not found handlers config
-			using (var sw = File.CreateText(Umbraco.Core.IO.IOHelper.MapPath(Umbraco.Core.IO.SystemFiles.NotFoundhandlersConfig, false)))
+			using (var sw = File.CreateText(IOHelper.MapPath(SystemFiles.NotFoundhandlersConfig, false)))
 			{
 				sw.Write(@"<NotFoundHandlers>
 	<notFound assembly='umbraco' type='SearchForAlias' />
