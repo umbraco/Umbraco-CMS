2b6b8e9e8c9f0b30b4c0866d92a3fee794593d3c
Shannon <sdeminick@gmail.com>
2014-03-19 13:28:23 +1100

    Fixes: U4-4470 New Installer doesn't deal with 500 errors, also fixes a tree init problem causing this particular ysod

--- src/Umbraco.Web.UI.Client/src/installer/installer.controller.js
+++ src/Umbraco.Web.UI.Client/src/installer/installer.controller.js
@@ -1,37 +1,43 @@
 angular.module("umbraco.install").controller("Umbraco.InstallerController",
-	function($scope, installerService){
+	function ($scope, installerService) {
 
-	$scope.stepIndex = 0;
-	//comment this out if you just want to see tips
-	installerService.init();
+	    //TODO: Decouple the service from the controller - the controller should be responsible
+	    // for the model (state) and the service should be responsible for helping the controller,
+	    // the controller should be passing the model into it's methods for manipulation and not hold
+	    // state. We should not be assigning properties from a service to a controller's scope.
+	    // see: https://github.com/umbraco/Umbraco-CMS/commit/b86ef0d7ac83f699aee35d807f7f7ebb6dd0ed2c#commitcomment-5721204
 
-	//uncomment this to see tips
-	//installerService.switchToFeedback();
+	    $scope.stepIndex = 0;
+	    //comment this out if you just want to see tips
+	    installerService.init();
 
-	$scope.installer = installerService.status;
+	    //uncomment this to see tips
+	    //installerService.switchToFeedback();
 
-	$scope.forward = function(){
-		installerService.forward();
-	};
+	    $scope.installer = installerService.status;
 
-	$scope.backward = function(){
-		installerService.backward();
-	};
+	    $scope.forward = function () {
+	        installerService.forward();
+	    };
 
-	$scope.install = function(){
-		installerService.install();
-	};
+	    $scope.backward = function () {
+	        installerService.backward();
+	    };
 
-	$scope.gotoStep = function(step){
-		installerService.gotoNamedStep(step);
-	};
+	    $scope.install = function () {
+	        installerService.install();
+	    };
 
-	$scope.restart = function () {
-	    installerService.gotoStep(0);
-	};
-});
+	    $scope.gotoStep = function (step) {
+	        installerService.gotoNamedStep(step);
+	    };
+
+	    $scope.restart = function () {
+	        installerService.gotoStep(0);
+	    };
+	});
 
 //this ensure that we start with a clean slate on every install and upgrade
-angular.module("umbraco.install").run(function($templateCache){
-	$templateCache.removeAll();
+angular.module("umbraco.install").run(function ($templateCache) {
+    $templateCache.removeAll();
 });
--- src/Umbraco.Web.UI.Client/src/installer/installer.service.js
+++ src/Umbraco.Web.UI.Client/src/installer/installer.service.js
@@ -229,41 +229,58 @@ angular.module("umbraco.install").factory('installerService', function($rootScop
 			service.status.feedback = getDescriptionForStepAtIndex(service.status.steps, 0);
 			service.status.progress = 0;
 
-			function processInstallStep(){
-				$http.post(Umbraco.Sys.ServerVariables.installApiBaseUrl + "PostPerformInstall",
-					_installerModel).then(function(response){
-						if(!response.data.complete){
-							
-							//progress feedback
-							service.status.progress = calculateProgress(service.status.steps, response.data.nextStep);
+			function processInstallStep() {
 
-							if(response.data.view){
-								//set the current view and model to whatever the process returns, the view is responsible for retriggering install();
-								var v = resolveView(response.data.view);
-								service.status.current = {view: v, model: response.data.model};
+			    $http.post(Umbraco.Sys.ServerVariables.installApiBaseUrl + "PostPerformInstall", _installerModel)
+			        .success(function(data, status, headers, config) {
+			            if (!data.complete) {
 
-								//turn off loading bar and feedback
-								service.switchToConfiguration();
-							}
-							else {
-							    var desc = getDescriptionForStepName(service.status.steps, response.data.nextStep);
-								if (desc) {
-									service.status.feedback = desc;
-								}
-								processInstallStep();
-							}
-						}
-						else {
-							service.complete();
-						}
-					}, function(err){
-							//this is where we handle installer error
-							var v = err.data.view ? resolveView(err.data.view) : resolveView("error");
-							var model = err.data.model ? err.data.model : err.data;
+			                //progress feedback
+			                service.status.progress = calculateProgress(service.status.steps, data.nextStep);
 
-							service.status.current = {view: v, model: model};
-							service.switchToConfiguration();
-					});
+			                if (data.view) {
+			                    //set the current view and model to whatever the process returns, the view is responsible for retriggering install();
+			                    var v = resolveView(data.view);
+			                    service.status.current = { view: v, model: data.model };
+
+			                    //turn off loading bar and feedback
+			                    service.switchToConfiguration();
+			                }
+			                else {
+			                    var desc = getDescriptionForStepName(service.status.steps, data.nextStep);
+			                    if (desc) {
+			                        service.status.feedback = desc;
+			                    }
+			                    processInstallStep();
+			                }
+			            }
+			            else {
+			                service.complete();
+			            }
+			        }).error(function(data, status, headers, config) {
+			            //need to handle 500's separately, this will happen if something goes wrong outside
+			            // of the installer (like app startup events or something) and these will get returned as text/html
+			            // not as json. If this happens we can't actually load in external views since they will YSOD as well!
+                        // so we need to display this in our own internal way
+			            
+			            if (status >= 500 && status < 600) {			                
+			                service.status.current = { view: "ysod", model: null };
+			                var ysod = data;
+                            //we need to manually write the html to the iframe - the html contains full html markup
+			                $timeout(function () {
+			                    document.getElementById('ysod').contentDocument.write(ysod);
+			                }, 500);
+			            }
+			            else {
+			                //this is where we handle installer error
+			                var v = data.view ? resolveView(data.view) : resolveView("error");
+			                var model = data.model ? data.model : data;
+			                service.status.current = { view: v, model: model };			                
+			            }
+
+			            service.switchToConfiguration();
+			            
+			        });
 			}
 			processInstallStep();
 		},
