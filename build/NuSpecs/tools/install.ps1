bfaace373cb1ebf4e39f5ee8c6bb215e9bf49fd6
Morten Christensen <mc@sitereactor.dk>
2014-03-20 14:00:07 +0100

    Updating the install ps1 to only copy over the web.config on initial install to ensure that the web.config is correct.

--- build/NuSpecs/tools/install.ps1
+++ build/NuSpecs/tools/install.ps1
@@ -3,7 +3,7 @@ param($rootPath, $toolsPath, $package, $project)
 if ($project) {	
 	# Copy umbraco files from package to project folder
 	$projectDestinationPath = Split-Path $project.FullName -Parent
-	$umbracoFilesPath = Join-Path $rootPath "UmbracoFiles\*"
+	$umbracoFilesPath = Join-Path $rootPath "UmbracoFiles\Web.config"
 	Copy-Item $umbracoFilesPath $projectDestinationPath -recurse -force
 	
 	# Open readme.txt file
