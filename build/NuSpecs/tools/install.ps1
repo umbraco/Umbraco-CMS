ea7d4765592add92419259537e4f8dfa69b1470d
Sebastiaan Janssen <sebastiaan@umbraco.com>
2014-03-20 14:24:09 +0100

    Backup config files properly

--- build/NuSpecs/tools/install.ps1
+++ build/NuSpecs/tools/install.ps1
@@ -1,10 +1,30 @@
 param($rootPath, $toolsPath, $package, $project)
 
 if ($project) {	
-	# Copy umbraco files from package to project folder
+	# Create a backup of extisting umbraco config files
+	$configPath = Join-Path (Split-Path $project.FullName -Parent) "\config"
+    
+    $ticks = (Get-Date).ToFileTime()
+    $backupPath = Join-Path (Split-Path $project.FullName -Parent) "\App_Data\ConfigBackup\$ticks"
+    $configBackupPath = Join-Path $backupPath "config"
+
+    New-Item -ItemType Directory -Force -Path $configBackupPath
+
+	Get-ChildItem -Path $configPath |
+	   Where -filterscript {($_.Name.EndsWith("config"))} | Foreach-Object {
+	    $newFileName = Join-Path $configBackupPath $_.Name
+        New-Item -ItemType File -Path $newFileName -Force
+	    Copy-Item $_.FullName $newFileName -Force
+	   }
+		
+	# Create a backup of original web.config
 	$projectDestinationPath = Split-Path $project.FullName -Parent
-	$umbracoFilesPath = Join-Path $rootPath "UmbracoFiles\Web.config"
-	Copy-Item $umbracoFilesPath $projectDestinationPath -recurse -force
+	$webConfigSource = Join-Path $projectDestinationPath "web.config"
+	Copy-Item $webConfigSource $backupPath -Force
+	
+	# Copy umbraco files from package to project folder
+	$umbracoFilesPath = Join-Path $rootPath "UmbracoFiles\*"
+	Copy-Item $umbracoFilesPath $projectDestinationPath -Recurse -Force
 	
 	# Open readme.txt file
 	$DTE.ItemOperations.OpenFile($toolsPath + '\Readme.txt')
