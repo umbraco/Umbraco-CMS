0bfbacfd66f3862ecb331f5492722c09d8cc8df9
Sebastiaan Janssen <sebastiaan@umbraco.com>
2014-03-20 15:33:41 +0100

    No need to back up the config files, they don't get overwritten if you answer "No"
    Updated the readme.

--- build/NuSpecs/tools/install.ps1
+++ build/NuSpecs/tools/install.ps1
@@ -1,30 +1,16 @@
 param($rootPath, $toolsPath, $package, $project)
 
 if ($project) {	
-	# Create a backup of extisting umbraco config files
-	$configPath = Join-Path (Split-Path $project.FullName -Parent) "\config"
-    
-    $ticks = (Get-Date).ToFileTime()
-    $backupPath = Join-Path (Split-Path $project.FullName -Parent) "\App_Data\ConfigBackup\$ticks"
-    $configBackupPath = Join-Path $backupPath "config"
-
-    New-Item -ItemType Directory -Force -Path $configBackupPath
-
-	Get-ChildItem -Path $configPath |
-	   Where -filterscript {($_.Name.EndsWith("config"))} | Foreach-Object {
-	    $newFileName = Join-Path $configBackupPath $_.Name
-        New-Item -ItemType File -Path $newFileName -Force
-	    Copy-Item $_.FullName $newFileName -Force
-	   }
-		
+    $dateTime = Get-Date -Format yyyyMMdd-HHmmss
+    $backupPath = Join-Path (Split-Path $project.FullName -Parent) "\App_Data\ConfigBackup\$dateTime"
+	
+	# Create backup folder if it doesn't exist yet
+	New-Item -ItemType Directory -Force -Path $backupPath
+	
 	# Create a backup of original web.config
 	$projectDestinationPath = Split-Path $project.FullName -Parent
 	$webConfigSource = Join-Path $projectDestinationPath "Web.config"
 	Copy-Item $webConfigSource $backupPath -Force
-	
-	# Copy Config files from package to project folder
-	$configFilesPath = Join-Path $rootPath "Content\Config\*.config"
-	Copy-Item $umbracoFilesPath $configPath -Force
 	
 	# Copy Web.config from package to project folder
 	$umbracoFilesPath = Join-Path $rootPath "UmbracoFiles\Web.config"
