f13ddddc16112d33af5060dc21e99dbdbebf07e9
Sebastiaan Janssen <sebastiaan@umbraco.com>
2014-03-25 20:39:46 +0100

    U4-4510 nuget targets file creates an invalid path in the property group
    That should fix this one properly now

--- build/NuSpecs/tools/install.ps1
+++ build/NuSpecs/tools/install.ps1
@@ -2,7 +2,7 @@ param($rootPath, $toolsPath, $package, $project)
 
 if ($project) {	
     $dateTime = Get-Date -Format yyyyMMdd-HHmmss
-    $backupPath = Join-Path (Split-Path $project.FullName -Parent) "\App_Data\ConfigBackup\$dateTime"
+	$backupPath = Join-Path (Split-Path $project.FullName -Parent) "\App_Data\NuGetBackup\$dateTime"
 	
 	# Create backup folder if it doesn't exist yet
 	New-Item -ItemType Directory -Force -Path $backupPath
@@ -15,7 +15,23 @@ if ($project) {
 	# Copy Web.config from package to project folder
 	$umbracoFilesPath = Join-Path $rootPath "UmbracoFiles\Web.config"
 	Copy-Item $umbracoFilesPath $projectDestinationPath -Force
-	
+
+	# Copy umbraco and umbraco_files from package to project folder
+	# This is only done when these folders already exist because we 
+	# only want to do this for upgrades
+	$umbracoFolder = Join-Path $projectDestinationPath "Umbraco\"
+	if(Test-Path $umbracoFolder) {
+		$umbracoFolderSource = Join-Path $rootPath "UmbracoFiles\Umbraco"
+		Copy-Item $umbracoFolder $backupPath -Force
+		robocopy $umbracoFolderSource $umbracoFolder /e /xf UI.xml
+	}
+
+	$umbracoClientFolder = Join-Path $projectDestinationPath "Umbraco_Client"	
+	if(Test-Path $umbracoClientFolder) {
+		$umbracoClientFolderSource = Join-Path $rootPath "UmbracoFiles\Umbraco_Client"
+		Copy-Item $umbracoClientFolder $backupPath -Force
+		robocopy $umbracoFolderSource $umbracoClientFolder /e
+	}
 	# Open readme.txt file
 	$DTE.ItemOperations.OpenFile($toolsPath + '\Readme.txt')
 }
