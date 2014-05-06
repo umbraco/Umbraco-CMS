a846ba64eb36e2901927b69287d2a6f660a694f5
Shannon <sdeminick@gmail.com>
2014-05-06 15:27:53 +1000

    Fixes: U4-4799 Ensure image logic found in the ContentService is duplicated for the new cropper

--- src/Umbraco.Web/PropertyEditors/ImageCropperPropertyEditor.cs
+++ src/Umbraco.Web/PropertyEditors/ImageCropperPropertyEditor.cs
@@ -18,10 +18,20 @@ namespace Umbraco.Web.PropertyEditors
     [PropertyEditor(Constants.PropertyEditors.ImageCropperAlias, "Image Cropper", "imagecropper", ValueType = "JSON", HideLabel = false)]
     public class ImageCropperPropertyEditor : PropertyEditor
     {
+
+        /// <summary>
+        /// We're going to bind to the MediaService Saving event so that we can populate the umbracoFile size, type, etc... label fields
+        /// if we find any attached to the current media item.
+        /// </summary>
+        /// <remarks>
+        /// I think this kind of logic belongs on this property editor, I guess it could exist elsewhere but it all has to do with the cropper.
+        /// </remarks>
         static ImageCropperPropertyEditor()
         {
             MediaService.Saving += MediaServiceSaving;
             MediaService.Created += MediaServiceCreated;
+
+            ContentService.Copied += ContentServiceCopied;
         }
 
         /// <summary>
@@ -47,6 +57,67 @@ namespace Umbraco.Web.PropertyEditors
                     {"focalPoint", "{left: 0.5, top: 0.5}"},
                     {"src", ""}
                 };
+        }
+
+        /// <summary>
+        /// After the content is copied we need to check if there are files that also need to be copied
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        static void ContentServiceCopied(IContentService sender, Core.Events.CopyEventArgs<IContent> e)
+        {
+            if (e.Original.Properties.Any(x => x.PropertyType.PropertyEditorAlias == Constants.PropertyEditors.ImageCropperAlias))
+            {
+                bool isUpdated = false;
+                var fs = FileSystemProviderManager.Current.GetFileSystemProvider<MediaFileSystem>();
+
+                //Loop through properties to check if the content contains media that should be deleted
+                foreach (var property in e.Original.Properties.Where(x => x.PropertyType.PropertyEditorAlias == Constants.PropertyEditors.ImageCropperAlias
+                    && x.Value != null                                                   
+                    && string.IsNullOrEmpty(x.Value.ToString()) == false))
+                {
+                    JObject json;
+                    try
+                    {
+                        json = JsonConvert.DeserializeObject<JObject>(property.Value.ToString());
+                    }
+                    catch (Exception ex)
+                    {
+                        LogHelper.Error<ImageCropperPropertyEditor>("An error occurred parsing the value stored in the image cropper value: " + property.Value.ToString(), ex);
+                        continue;
+                    }
+
+                    if (json["src"] != null && json["src"].ToString().IsNullOrWhiteSpace() == false)
+                    {
+                        if (fs.FileExists(IOHelper.MapPath(json["src"].ToString())))
+                        {
+                            var currentPath = fs.GetRelativePath(json["src"].ToString());
+                            var propertyId = e.Copy.Properties.First(x => x.Alias == property.Alias).Id;
+                            var newPath = fs.GetRelativePath(propertyId, System.IO.Path.GetFileName(currentPath));
+
+                            fs.CopyFile(currentPath, newPath);
+                            json["src"] = fs.GetUrl(newPath);
+                            e.Copy.SetValue(property.Alias, json.ToString());
+
+                            //Copy thumbnails
+                            foreach (var thumbPath in fs.GetThumbnails(currentPath))
+                            {
+                                var newThumbPath = fs.GetRelativePath(propertyId, System.IO.Path.GetFileName(thumbPath));
+                                fs.CopyFile(thumbPath, newThumbPath);
+                            }
+                            isUpdated = true;
+                        }
+                    }
+
+                    
+                }
+
+                if (isUpdated)
+                {
+                    //need to re-save the copy with the updated path value
+                    sender.Save(e.Copy);
+                }
+            }
         }
 
         static void MediaServiceCreated(IMediaService sender, Core.Events.NewEventArgs<IMedia> e)
