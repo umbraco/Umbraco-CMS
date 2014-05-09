00880fcbfa983541f13b40fb8ef642a691d361f6
Sebastiaan Janssen <sebastiaan@umbraco.com>
2014-05-09 13:07:03 +0200

    I was confused, these files exist just fine :)

--- build/Build.bat
+++ build/Build.bat
@@ -30,10 +30,10 @@ echo This file is only here so that the containing folder will be included in th
 echo This file is only here so that the containing folder will be included in the NuGet package, it is safe to delete. > .\_BuildOutput\WebApp\Views\Partials\dummy.txt
 echo This file is only here so that the containing folder will be included in the NuGet package, it is safe to delete. > .\_BuildOutput\WebApp\Views\MacroPartials\dummy.txt
 
-REM ECHO Adding Web.config transform files to the NuGet package
-REM ren .\_BuildOutput\WebApp\MacroScripts\Web.config Web.config.transform
-REM ren .\_BuildOutput\WebApp\Views\Web.config Web.config.transform
-REM ren .\_BuildOutput\WebApp\Xslt\Web.config Web.config.transform
+ECHO Adding Web.config transform files to the NuGet package
+ren .\_BuildOutput\WebApp\MacroScripts\Web.config Web.config.transform
+ren .\_BuildOutput\WebApp\Views\Web.config Web.config.transform
+ren .\_BuildOutput\WebApp\Xslt\Web.config Web.config.transform
 
 ECHO Packing the NuGet release files
 ..\src\.nuget\NuGet.exe Pack NuSpecs\UmbracoCms.Core.nuspec -Version %version%
