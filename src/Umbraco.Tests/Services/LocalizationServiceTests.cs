54b202dd1ac55a33f2dcd975c454243bc3c3681f
Henrik Rossen Jakobsen <henrik@rossenjakobsen.dk>
2014-06-04 22:08:09 +0200

    Added tests for LocalizationService

--- src/Umbraco.Tests/Services/LocalizationServiceTests.cs
+++ src/Umbraco.Tests/Services/LocalizationServiceTests.cs
@@ -1,4 +1,5 @@
-﻿using NUnit.Framework;
+﻿using System.Linq;
+using NUnit.Framework;
 using Umbraco.Tests.TestHelpers;
 
 namespace Umbraco.Tests.Services
@@ -8,7 +9,7 @@ namespace Umbraco.Tests.Services
     /// This is more of an integration test as it involves multiple layers
     /// as well as configuration.
     /// </summary>
-    [DatabaseTestBehavior(DatabaseBehavior.NewDbFileAndSchemaPerFixture)]
+    [DatabaseTestBehavior(DatabaseBehavior.NewDbFileAndSchemaPerTest)]
     [TestFixture, RequiresSTA]
     public class LocalizationServiceTests : BaseServiceTest
     {
@@ -22,6 +23,66 @@ namespace Umbraco.Tests.Services
         public override void TearDown()
         {
             base.TearDown();
+        }
+
+        [Test]
+        public void Find_BaseData_Language()
+        {
+            // Arrange
+            var localizationService = ServiceContext.LocalizationService;
+            
+            // Act
+            var languages = localizationService.GetAllLanguages();
+
+            // Assert 
+            Assert.That(1, Is.EqualTo(languages.Count()));
+        }
+
+        [Test]
+        public void Save_Language_And_GetLanguageByIsoCode()
+        {
+            // Arrange
+            var localizationService = ServiceContext.LocalizationService;
+            var isoCode = "en-GB";
+            var language = new Core.Models.Language(isoCode);
+
+            // Act
+            localizationService.Save(language);
+            var result = localizationService.GetLanguageByIsoCode(isoCode);
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Test]
+        public void Save_Language_And_GetLanguageById()
+        {
+            var localizationService = ServiceContext.LocalizationService;
+            var isoCode = "en-GB";
+            var language = new Core.Models.Language(isoCode);
+
+            // Act
+            localizationService.Save(language);
+            var result = localizationService.GetLanguageById(language.Id);
+
+            // Assert
+            Assert.NotNull(result);
+        }
+
+        [Test]
+        public void Deleted_Language_Should_Not_Exist()
+        {
+            var localizationService = ServiceContext.LocalizationService;
+            var isoCode = "en-GB";
+            var language = new Core.Models.Language(isoCode);
+            localizationService.Save(language);
+
+            // Act
+            localizationService.Delete(language);
+            var result = localizationService.GetLanguageByIsoCode(isoCode);
+
+            // Assert
+            Assert.Null(result);
         }
     }
 }
