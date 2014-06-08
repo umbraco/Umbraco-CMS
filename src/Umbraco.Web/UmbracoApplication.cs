34dcde645c9d1f071ba33f505a0c5a5856f4d73c
Shannon <sdeminick@gmail.com>
2014-06-08 19:05:48 +0200

    Half completes: U4-2462 Does the app_plugins manager monitor file changes?

--- src/Umbraco.Web/UmbracoApplication.cs
+++ src/Umbraco.Web/UmbracoApplication.cs
@@ -1,13 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Web;
 using System.Web.Hosting;
 using System.Web.Mvc;
 using Umbraco.Core;
+using Umbraco.Core.Configuration;
+using Umbraco.Core.IO;
 using Umbraco.Core.Logging;
+using Umbraco.Core.Manifest;
 using Umbraco.Web.Routing;
 using umbraco.businesslogic;
 
@@ -18,7 +22,25 @@ namespace Umbraco.Web
 	/// </summary>
     public class UmbracoApplication : UmbracoApplicationBase
 	{
-           
+        private readonly ManifestWatcher _mw = new ManifestWatcher();
+
+	    protected override void OnApplicationStarted(object sender, EventArgs e)
+	    {
+	        base.OnApplicationStarted(sender, e);
+
+	        if (ApplicationContext.Current.IsConfigured && GlobalSettings.DebugMode)
+	        {   
+	            _mw.Start(Directory.GetDirectories(IOHelper.MapPath("~/App_Plugins/")));
+	        }
+	    }
+
+	    protected override void OnApplicationEnd(object sender, EventArgs e)
+	    {
+	        base.OnApplicationEnd(sender, e);
+
+            _mw.Dispose();
+	    }
+
 	    protected override IBootManager GetBootManager()
 	    {
             return new WebBootManager(this);	        
