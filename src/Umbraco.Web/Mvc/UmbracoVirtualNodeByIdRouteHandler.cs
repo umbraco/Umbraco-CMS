b5282244f0dc400ebefdfa195a47e83ca14dce42
Shannon <sdeminick@gmail.com>
2014-06-08 17:16:28 +0200

    Umbraco MVC style routes now implemented.

---
+++ src/Umbraco.Web/Mvc/UmbracoVirtualNodeByIdRouteHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Routing;
+using Umbraco.Core.Models;
+
+namespace Umbraco.Web.Mvc
+{
+    public abstract class UmbracoVirtualNodeByIdRouteHandler : UmbracoVirtualNodeRouteHandler
+    {
+        private readonly int _realNodeId;
+
+        protected UmbracoVirtualNodeByIdRouteHandler(int realNodeId)
+        {
+            _realNodeId = realNodeId;
+        }
+
+        protected sealed override IPublishedContent FindContent(RequestContext requestContext, UmbracoContext umbracoContext)
+        {
+            var byId = umbracoContext.ContentCache.GetById(_realNodeId);
+            if (byId == null) return null;
+
+            return FindContent(requestContext, umbracoContext, byId);
+        }
+
+        protected abstract IPublishedContent FindContent(RequestContext requestContext, UmbracoContext umbracoContext, IPublishedContent baseContent);
+    }
+}
