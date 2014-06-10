ee6e1a9d8bfe505fc9c73f09cf20f1b31f7e1e2c
Sebastiaan Janssen <sebastiaan@umbraco.com>
2014-06-10 17:13:58 +0200

    Disable extra templates tree

--- src/Umbraco.Web/Trees/TemplateTreeController.cs
+++ src/Umbraco.Web/Trees/TemplateTreeController.cs
@@ -10,43 +10,43 @@ using Umbraco.Web.Mvc;
 using umbraco.cms.businesslogic.template;
 using Umbraco.Web.Models.Trees;
 
-namespace Umbraco.Web.Trees
-{
-    [UmbracoApplicationAuthorize(Constants.Applications.Settings)]
-    [Tree(Constants.Applications.Settings, Constants.Trees.Templates, "Templates")]
-    [PluginController("UmbracoTrees")]
-    [CoreTree]
-    public class TemplateTreeController : TreeController
-    {
-        protected override Models.Trees.MenuItemCollection GetMenuForNode(string id, System.Net.Http.Formatting.FormDataCollection queryStrings)
-        {
-            return new Models.Trees.MenuItemCollection();
-        }
+//namespace Umbraco.Web.Trees
+//{
+//    [UmbracoApplicationAuthorize(Constants.Applications.Settings)]
+//    [Tree(Constants.Applications.Settings, Constants.Trees.Templates, "Templates")]
+//    [PluginController("UmbracoTrees")]
+//    [CoreTree]
+//    public class TemplateTreeController : TreeController
+//    {
+//        protected override Models.Trees.MenuItemCollection GetMenuForNode(string id, System.Net.Http.Formatting.FormDataCollection queryStrings)
+//        {
+//            return new Models.Trees.MenuItemCollection();
+//        }
 
-        protected override Models.Trees.TreeNodeCollection GetTreeNodes(string id, System.Net.Http.Formatting.FormDataCollection queryStrings)
-        {
-            IEnumerable<Umbraco.Core.Models.EntityBase.IUmbracoEntity> templates;
-            var nodes = new TreeNodeCollection();
+//        protected override Models.Trees.TreeNodeCollection GetTreeNodes(string id, System.Net.Http.Formatting.FormDataCollection queryStrings)
+//        {
+//            IEnumerable<Umbraco.Core.Models.EntityBase.IUmbracoEntity> templates;
+//            var nodes = new TreeNodeCollection();
 
 
-            if (id == "-1")
-                templates = Services.EntityService.GetRootEntities(Core.Models.UmbracoObjectTypes.Template);
-            else
-                templates = Services.EntityService.GetChildren(int.Parse(id), Core.Models.UmbracoObjectTypes.Template);
+//            if (id == "-1")
+//                templates = Services.EntityService.GetRootEntities(Core.Models.UmbracoObjectTypes.Template);
+//            else
+//                templates = Services.EntityService.GetChildren(int.Parse(id), Core.Models.UmbracoObjectTypes.Template);
 
-            foreach (var t in templates)
-            {
-                var node = CreateTreeNode(t.Id.ToString(), t.ParentId.ToString(), queryStrings, t.Name);
-                node.Icon = "icon-newspaper-alt";
-                node.HasChildren = Services.EntityService.GetChildren(t.Id, Core.Models.UmbracoObjectTypes.Template).Any();
+//            foreach (var t in templates)
+//            {
+//                var node = CreateTreeNode(t.Id.ToString(), t.ParentId.ToString(), queryStrings, t.Name);
+//                node.Icon = "icon-newspaper-alt";
+//                node.HasChildren = Services.EntityService.GetChildren(t.Id, Core.Models.UmbracoObjectTypes.Template).Any();
 
-                if (node.HasChildren)
-                    node.Icon = "icon-newspaper";
+//                if (node.HasChildren)
+//                    node.Icon = "icon-newspaper";
 
-                nodes.Add(node);
-            }
+//                nodes.Add(node);
+//            }
 
-            return nodes;
-        }
-    }
-}
+//            return nodes;
+//        }
+//    }
+//}
