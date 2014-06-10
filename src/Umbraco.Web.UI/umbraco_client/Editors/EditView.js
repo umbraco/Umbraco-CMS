07ec9aa813ac3286fa2786cdd1df566a2f063704
Sebastiaan Janssen <sebastiaan@umbraco.com>
2014-06-10 18:11:14 +0200

    Undo formatting of query for now

--- src/Umbraco.Web.UI/umbraco_client/Editors/EditView.js
+++ src/Umbraco.Web.UI/umbraco_client/Editors/EditView.js
@@ -79,9 +79,9 @@
                 template: "views/common/dialogs/template/queryBuilder.html",
                 callback: function (data) {
 
-                    var dataFormatted = data.replace(new RegExp('[' + "." + ']', 'g'), "\n\t\t\t\t\t.");
+                    //var dataFormatted = data.replace(new RegExp('[' + "." + ']', 'g'), "\n\t\t\t\t\t.");
 
-                    var code = "\n@{\n" + "\tvar selection = " + dataFormatted + ";\n}\n";
+                    var code = "\n@{\n" + "\tvar selection = " + data + ";\n}\n";
                     code += "<ul>\n" +
                                 "\t@foreach(var item in selection){\n" +
                                     "\t\t<li>\n" +
