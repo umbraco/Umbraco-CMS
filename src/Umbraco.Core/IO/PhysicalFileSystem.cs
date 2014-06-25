12cdd3326aa18932e4ce043f0385b284ebc43c91
Morten Christensen <mc@sitereactor.dk>
2014-06-25 16:00:21 +0200

    The CopyFile extension method was not disposing the file stream, so any attempt to delete the source file afterwards would fail.
    Removing unused using statements.

--- src/Umbraco.Core/IO/PhysicalFileSystem.cs
+++ src/Umbraco.Core/IO/PhysicalFileSystem.cs
@@ -3,10 +3,7 @@ using System.Collections.Generic;
 using System.Globalization;
 using System.IO; 
 using System.Linq;
-using System.Web;
-using Umbraco.Core.CodeAnnotations;
 using Umbraco.Core.Logging;
-using Umbraco.Core.Publishing;
 
 namespace Umbraco.Core.IO
 {
