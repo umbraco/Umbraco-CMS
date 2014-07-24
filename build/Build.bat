69c7ecad597e448493cc218bbb22083e8e591a51
Sebastiaan Janssen <sebastiaan@umbraco.com>
2014-07-24 14:43:40 +0200

    Got bitten by stray files that were left behing in _BuildOutput folder, clean before build

--- build/Build.bat
+++ build/Build.bat
@@ -21,6 +21,7 @@ ECHO Removing the belle build folder to make sure everything is clean as a whist
 RD ..\src\Umbraco.Web.UI.Client\build /Q /S
 
 ECHO Removing existing built files to make sure everything is clean as a whistle
+RMDIR /Q /S _BuildOutput
 DEL /F /Q UmbracoCms.*.zip
 DEL /F /Q UmbracoCms.*.nupkg
 DEL /F /Q webpihash.txt
