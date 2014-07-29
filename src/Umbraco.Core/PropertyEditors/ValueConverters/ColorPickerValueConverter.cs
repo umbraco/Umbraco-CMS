53ea491bd6ee2c183722bcbdf351e2eaba7f375f
Stephan <stephane@umbraco.dk>
2014-07-29 09:52:00 +0200

    U4-5286 - add property converter for color picker

---
+++ src/Umbraco.Core/PropertyEditors/ValueConverters/ColorPickerValueConverter.cs
@@ -0,0 +1,18 @@
+﻿using Umbraco.Core.Models.PublishedContent;
+
+namespace Umbraco.Core.PropertyEditors.ValueConverters
+{
+    public class ColorPickerValueConverter : PropertyValueConverterBase
+    {
+        public override bool IsConverter(PublishedPropertyType propertyType)
+        {
+            return propertyType.PropertyEditorAlias.InvariantEquals(Constants.PropertyEditors.ColorPickerAlias);
+        }
+
+        public override object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            // make sure it's a string
+            return source.ToString();
+        }
+    }
+}
