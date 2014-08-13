f6ec4798a60267b32ae645e1fbf721bb649cf015
AndyButland <abutland73@gmail.com>
2014-08-13 22:11:41 +0100

    Prevented list view filter box from setting dirty flag on form.  Without this you get the save changes warning after using the filter, but there's nothing to save.

--- src/Umbraco.Web.UI.Client/src/common/directives/validation/valformmanager.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/validation/valformmanager.directive.js
@@ -64,6 +64,7 @@ function valFormManager(serverValidationManager, $rootScope, $log, $timeout, not
                     return;
                 }
 
+                console.log("I am dirty");
                 var path = nextLocation.split("#")[1];
                 if (path) {
                     if (path.indexOf("%253") || path.indexOf("%252")) {
