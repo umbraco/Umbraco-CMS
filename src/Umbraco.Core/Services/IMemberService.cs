9236225bdf09bdf995abde5684178812fd73f80e
Morten Christensen <mc@sitereactor.dk>
2014-08-15 14:42:05 +0200

    Adding and updating code comments for the Membership service interfaces and implementations

--- src/Umbraco.Core/Services/IMemberService.cs
+++ src/Umbraco.Core/Services/IMemberService.cs
@@ -5,128 +5,180 @@ using Umbraco.Core.Persistence.Querying;
 
 namespace Umbraco.Core.Services
 {
-
     /// <summary>
     /// Defines the MemberService, which is an easy access to operations involving (umbraco) members.
     /// </summary>
     public interface IMemberService : IMembershipMemberService
     {
-        int Count(string contentTypeAlias = null);     
-
+        /// <summary>
+        /// Creates an <see cref="IMember"/> object without persisting it
+        /// </summary>
+        /// <remarks>This method is convenient for when you need to add properties to a new Member
+        /// before persisting it in order to limit the amount of times its saved.
+        /// Also note that the returned <see cref="IMember"/> will not have an Id until its saved.</remarks>
+        /// <param name="username">Username of the Member to create</param>
+        /// <param name="email">Email of the Member to create</param>
+        /// <param name="name">Name of the Member to create</param>
+        /// <param name="memberTypeAlias">Alias of the MemberType the Member should be based on</param>
+        /// <returns><see cref="IMember"/></returns>
         IMember CreateMember(string username, string email, string name, string memberTypeAlias);
+
+        /// <summary>
+        /// Creates an <see cref="IMember"/> object without persisting it
+        /// </summary>
+        /// <remarks>This method is convenient for when you need to add properties to a new Member
+        /// before persisting it in order to limit the amount of times its saved.
+        /// Also note that the returned <see cref="IMember"/> will not have an Id until its saved.</remarks>
+        /// <param name="username">Username of the Member to create</param>
+        /// <param name="email">Email of the Member to create</param>
+        /// <param name="name">Name of the Member to create</param>
+        /// <param name="memberType">MemberType the Member should be based on</param>
+        /// <returns><see cref="IMember"/></returns>
         IMember CreateMember(string username, string email, string name, IMemberType memberType);
+
+        /// <summary>
+        /// Creates and persists a Member
+        /// </summary>
+        /// <remarks>Using this method will persist the Member object before its returned 
+        /// meaning that it will have an Id available (unlike the CreateMember method)</remarks>
+        /// <param name="username">Username of the Member to create</param>
+        /// <param name="email">Email of the Member to create</param>
+        /// <param name="name">Name of the Member to create</param>
+        /// <param name="memberTypeAlias">Alias of the MemberType the Member should be based on</param>
+        /// <returns><see cref="IMember"/></returns>
         IMember CreateMemberWithIdentity(string username, string email, string name, string memberTypeAlias);
+
+        /// <summary>
+        /// Creates and persists a Member
+        /// </summary>
+        /// <remarks>Using this method will persist the Member object before its returned 
+        /// meaning that it will have an Id available (unlike the CreateMember method)</remarks>
+        /// <param name="username">Username of the Member to create</param>
+        /// <param name="email">Email of the Member to create</param>
+        /// <param name="name">Name of the Member to create</param>
+        /// <param name="memberType">MemberType the Member should be based on</param>
+        /// <returns><see cref="IMember"/></returns>
         IMember CreateMemberWithIdentity(string username, string email, string name, IMemberType memberType);
         
         /// <summary>
         /// This is simply a helper method which essentially just wraps the MembershipProvider's ChangePassword method
         /// </summary>
-        /// <param name="member">The member to save the password for</param>
-        /// <param name="password"></param>
-        /// <remarks>
-        /// This method exists so that Umbraco developers can use one entry point to create/update members if they choose to.
-        /// </remarks>
+        /// <remarks>This method exists so that Umbraco developers can use one entry point to create/update 
+        /// Members if they choose to. </remarks>
+        /// <param name="member">The Member to save the password for</param>
+        /// <param name="password">The password to encrypt and save</param>
         void SavePassword(IMember member, string password);
 
         /// <summary>
-        /// Checks if a member with the id exists
+        /// Gets the count of Members by an optional MemberType alias
         /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
+        /// <remarks>If no alias is supplied then the count for all Member will be returned</remarks>
+        /// <param name="memberTypeAlias">Optional alias for the MemberType when counting number of Members</param>
+        /// <returns><see cref="System.int"/> with number of Members</returns>
+        int Count(string memberTypeAlias = null);
+
+        /// <summary>
+        /// Checks if a Member with the id exists
+        /// </summary>
+        /// <param name="id">Id of the Member</param>
+        /// <returns><c>True</c> if the Member exists otherwise <c>False</c></returns>
         bool Exists(int id);
         
         /// <summary>
-        /// Get a member by the unique key
+        /// Gets a Member by the unique key
         /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
+        /// <remarks>The guid key corresponds to the unique id in the database
+        /// and the user id in the membership provider.</remarks>
+        /// <param name="id"><see cref="Guid"/> Id</param>
+        /// <returns><see cref="IMember"/></returns>
         IMember GetByKey(Guid id);
 
         /// <summary>
-        /// Gets a member by it's id
+        /// Gets a Member by its integer id
         /// </summary>
-        /// <param name="id"></param>
-        /// <returns></returns>
+        /// <param name="id"><see cref="System.int"/> Id</param>
+        /// <returns><see cref="IMember"/></returns>
         IMember GetById(int id);
 
         /// <summary>
-        /// Get all members for the member type alias
+        /// Gets all Members for the specified MemberType alias
         /// </summary>
-        /// <param name="memberTypeAlias"></param>
-        /// <returns></returns>
+        /// <param name="memberTypeAlias">Alias of the MemberType</param>
+        /// <returns><see cref="IEnumerable{IMember}"/></returns>
         IEnumerable<IMember> GetMembersByMemberType(string memberTypeAlias);
 
         /// <summary>
-        /// Get all members for the member type id
+        /// Gets all Members for the MemberType id
         /// </summary>
-        /// <param name="memberTypeId"></param>
-        /// <returns></returns>
+        /// <param name="memberTypeId">Id of the MemberType</param>
+        /// <returns><see cref="IEnumerable{IMember}"/></returns>
         IEnumerable<IMember> GetMembersByMemberType(int memberTypeId);
 
         /// <summary>
-        /// Get all members in the member group name specified
+        /// Gets all Members within the specified MemberGroup name
         /// </summary>
-        /// <param name="memberGroupName"></param>
-        /// <returns></returns>
+        /// <param name="memberGroupName">Name of the MemberGroup</param>
+        /// <returns><see cref="IEnumerable{IMember}"/></returns>
         IEnumerable<IMember> GetMembersByGroup(string memberGroupName);
 
         /// <summary>
-        /// Get all members with the ids specified
+        /// Gets all Members with the ids specified
         /// </summary>
-        /// <param name="ids"></param>
-        /// <returns></returns>
+        /// <remarks>If no Ids are specified all Members will be retrieved</remarks>
+        /// <param name="ids">Optional list of Member Ids</param>
+        /// <returns><see cref="IEnumerable{IMember}"/></returns>
         IEnumerable<IMember> GetAllMembers(params int[] ids);
         
         /// <summary>
-        /// Delete members of the specified member type id
+        /// Delete Members of the specified MemberType id
         /// </summary>
-        /// <param name="memberTypeId"></param>
+        /// <param name="memberTypeId">Id of the MemberType</param>
         void DeleteMembersOfType(int memberTypeId);
 
         /// <summary>
-        /// Find members based on their display name
+        /// Finds Members based on their display name
         /// </summary>
-        /// <param name="displayNameToMatch"></param>
-        /// <param name="pageIndex"></param>
-        /// <param name="pageSize"></param>
-        /// <param name="totalRecords"></param>
-        /// <param name="matchType"></param>
-        /// <returns></returns>
+        /// <param name="displayNameToMatch">Display name to match</param>
+        /// <param name="pageIndex">Current page index</param>
+        /// <param name="pageSize">Size of the page</param>
+        /// <param name="totalRecords">Total number of records found (out)</param>
+        /// <param name="matchType">The type of match to make as <see cref="StringPropertyMatchType"/>. Default is <see cref="StringPropertyMatchType.StartsWith"/></param>
+        /// <returns><see cref="IEnumerable{IMember}"/></returns>
         IEnumerable<IMember> FindMembersByDisplayName(string displayNameToMatch, int pageIndex, int pageSize, out int totalRecords, StringPropertyMatchType matchType = StringPropertyMatchType.StartsWith);
 
         /// <summary>
-        /// Get members based on a property search
+        /// Gets a list of Members based on a property search
         /// </summary>
-        /// <param name="propertyTypeAlias"></param>
-        /// <param name="value"></param>
-        /// <param name="matchType"></param>
-        /// <returns></returns>
+        /// <param name="propertyTypeAlias">Alias of the PropertyType to search for</param>
+        /// <param name="value"><see cref="System.string"/> Value to match</param>
+        /// <param name="matchType">The type of match to make as <see cref="StringPropertyMatchType"/>. Default is <see cref="StringPropertyMatchType.Exact"/></param>
+        /// <returns><see cref="IEnumerable{IMember}"/></returns>
         IEnumerable<IMember> GetMembersByPropertyValue(string propertyTypeAlias, string value, StringPropertyMatchType matchType = StringPropertyMatchType.Exact);
 
         /// <summary>
-        /// Get members based on a property search
+        /// Gets a list of Members based on a property search
         /// </summary>
-        /// <param name="propertyTypeAlias"></param>
-        /// <param name="value"></param>
-        /// <param name="matchType"></param>
-        /// <returns></returns>
+        /// <param name="propertyTypeAlias">Alias of the PropertyType to search for</param>
+        /// <param name="value"><see cref="System.int"/> Value to match</param>
+        /// <param name="matchType">The type of match to make as <see cref="StringPropertyMatchType"/>. Default is <see cref="StringPropertyMatchType.Exact"/></param>
+        /// <returns><see cref="IEnumerable{IMember}"/></returns>
         IEnumerable<IMember> GetMembersByPropertyValue(string propertyTypeAlias, int value, ValuePropertyMatchType matchType = ValuePropertyMatchType.Exact);
 
         /// <summary>
-        /// Get members based on a property search
+        /// Gets a list of Members based on a property search
         /// </summary>
-        /// <param name="propertyTypeAlias"></param>
-        /// <param name="value"></param>
-        /// <returns></returns>
+        /// <param name="propertyTypeAlias">Alias of the PropertyType to search for</param>
+        /// <param name="value"><see cref="System.bool"/> Value to match</param>
+        /// <returns><see cref="IEnumerable{IMember}"/></returns>
         IEnumerable<IMember> GetMembersByPropertyValue(string propertyTypeAlias, bool value);
 
         /// <summary>
-        /// Get members based on a property search
+        /// Gets a list of Members based on a property search
         /// </summary>
-        /// <param name="propertyTypeAlias"></param>
-        /// <param name="value"></param>
-        /// <param name="matchType"></param>
-        /// <returns></returns>
+        /// <param name="propertyTypeAlias">Alias of the PropertyType to search for</param>
+        /// <param name="value"><see cref="System.DateTime"/> Value to match</param>
+        /// <param name="matchType">The type of match to make as <see cref="StringPropertyMatchType"/>. Default is <see cref="StringPropertyMatchType.Exact"/></param>
+        /// <returns><see cref="IEnumerable{IMember}"/></returns>
         IEnumerable<IMember> GetMembersByPropertyValue(string propertyTypeAlias, DateTime value, ValuePropertyMatchType matchType = ValuePropertyMatchType.Exact);
     }
 }
