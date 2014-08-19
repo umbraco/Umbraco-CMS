055f9a48483c699a42fad114894c08dd8d9070da
Rolf Johansson <rojo@nocrew.org>
2014-08-19 18:06:38 +0200

    Initial commit
    To get something to sync and create a pull request of.

---
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/mediacontentrelation/mediacontentusage.controller.js
@@ -0,0 +1,15 @@
+﻿angular.module("umbraco").controller("Chalmers.MediaContentUsageController", function ($scope, editorState, mediaContentUsageResource, notificationsService) {
+
+    // Check the pre-values for showing the path
+    $scope.showPath = $scope.model.config.showPath && $scope.model.config.showPath !== '0' ? true : false;
+
+    // Indicate that we are loading
+    $scope.loading = true;
+
+    // Get data about Content usage from our api resource
+    mediaContentUsageResource.getMediaContentUsage(editorState.current.id)
+        .then(function (response) {
+            $scope.media = response.data;
+            $scope.loading = false;
+        });
+});
---
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/mediacontentrelation/mediacontentusage.resource.js
@@ -0,0 +1,14 @@
+﻿//adds the resource to umbraco.resources module:
+angular.module('umbraco.resources').factory('mediaContentUsageResource',
+    function ($q, $http) {
+        return {
+            getMediaContentUsage: function (id) {
+                return $http({
+                    url: "backoffice/Chalmers/MediaContentUsageApi/GetMediaContentUsage",
+                    method: "GET",
+                    params: { id: id }
+                });
+            }
+        };
+    }
+);
