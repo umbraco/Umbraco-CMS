72dc5fc477e21ea10f9c970aa5ea9c64b254bc5f
Shannon <sdeminick@gmail.com>
2014-10-03 00:23:28 +1000

    Removes unused method

--- src/Umbraco.Core/Persistence/Querying/BaseExpressionHelper.cs
+++ src/Umbraco.Core/Persistence/Querying/BaseExpressionHelper.cs
@@ -626,13 +626,6 @@ namespace Umbraco.Core.Persistence.Querying
         //               ? "'" + escapeCallback(value) + "'"
         //               : value.ToString();
         //}
-
-        public virtual string EscapeParam(object paramValue)
-        {
-            return paramValue == null 
-                ? string.Empty 
-                : SqlSyntaxContext.SqlSyntaxProvider.EscapeString(paramValue.ToString());
-        }
         
         public virtual bool ShouldQuoteValue(Type fieldType)
         {
