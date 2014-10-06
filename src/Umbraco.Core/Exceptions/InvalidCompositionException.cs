e6820ad52ab272d27af6ab6882ae8bdfe28ff08d
Morten Christensen <mc@sitereactor.dk>
2014-10-06 15:02:47 +0200

    Fixes U4-5077 Umbraco 7.2 Alpha - doc types with compositions get moved in doc type tree
    Return 0 if parent is the root

---
+++ src/Umbraco.Core/Exceptions/InvalidCompositionException.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Umbraco.Core.Exceptions
+{
+    public class InvalidCompositionException : Exception
+    {  
+        public string ContentTypeAlias { get; set; }
+
+        public string AddedCompositionAlias { get; set; }
+
+        public string PropertyTypeAlias { get; set; }
+
+        public override string Message
+        {
+            get
+            {
+                return string.Format(
+                        "InvalidCompositionException - ContentType with alias '{0}' was added as a Compsition to ContentType with alias '{1}', " +
+                        "but there was a conflict on the PropertyType alias '{2}'. " +
+                        "PropertyTypes must have a unique alias across all Compositions in order to compose a valid ContentType Composition.",
+                        AddedCompositionAlias, ContentTypeAlias, PropertyTypeAlias);
+            }
+        }
+    }
+}
