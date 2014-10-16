7df39efa7bc37687bffa2c2e92c097323774902d
Shannon <sdeminick@gmail.com>
2014-10-16 12:15:39 +1000

    Ensures missing indexes are added that are required for the other constraints, even though these are now added for 6.0, if the version is detected as greater than 6.2 they still need to be added. (U4-4133)

--- src/Umbraco.Core/Persistence/Migrations/Upgrades/TargetVersionSeven/AlterTagRelationsTable.cs
+++ src/Umbraco.Core/Persistence/Migrations/Upgrades/TargetVersionSeven/AlterTagRelationsTable.cs
@@ -137,6 +137,22 @@ namespace Umbraco.Core.Persistence.Migrations.Upgrades.TargetVersionSeven
                   .OnDelete(Rule.None)
                   .OnUpdate(Rule.None);
 
+            //Some very old schemas don't have an index on the cmsContent.nodeId column, I'm not actually sure when it was added but 
+            // it is absolutely required to exist in order to add this foreign key, so we'll need to check it's existence
+            // this came to light from this issue: http://issues.umbraco.org/issue/U4-4133
+            var dbIndexes = SqlSyntaxContext.SqlSyntaxProvider.GetDefinedIndexes(Context.Database)
+                .Select(x => new DbIndexDefinition()
+                {
+                    TableName = x.Item1,
+                    IndexName = x.Item2,
+                    ColumnName = x.Item3,
+                    IsUnique = x.Item4
+                }).ToArray();
+            if (dbIndexes.Any(x => x.IndexName.InvariantEquals("IX_cmsContent")) == false)
+            {
+                Create.Index("IX_cmsContent").OnTable("cmsContent").OnColumn("nodeId").Ascending().WithOptions().Unique();
+            }
+
             //now we need to add a foreign key to the nodeId column to cmsContent (intead of the original umbracoNode)
             Create.ForeignKey("FK_cmsTagRelationship_cmsContent")
                   .FromTable("cmsTagRelationship")
--- src/Umbraco.Core/Persistence/Migrations/Upgrades/TargetVersionSevenTwoZero/AddMissingForeignKeyForContentType.cs
+++ src/Umbraco.Core/Persistence/Migrations/Upgrades/TargetVersionSevenTwoZero/AddMissingForeignKeyForContentType.cs
@@ -40,6 +40,26 @@ namespace Umbraco.Core.Persistence.Migrations.Upgrades.TargetVersionSevenTwoZero
                     Delete.FromTable("umbracoNode").Row(new { id = orphanedId });
                 }
 
+                //Some very old schemas don't have an index on the cmsContentType.nodeId column, I'm not actually sure when it was added but 
+                // it is absolutely required to exist in order to add other foreign keys and much better for perf, so we'll need to check it's existence
+                // this came to light from this issue: http://issues.umbraco.org/issue/U4-4133
+                var dbIndexes = SqlSyntaxContext.SqlSyntaxProvider.GetDefinedIndexes(Context.Database)
+                    .Select(x => new DbIndexDefinition()
+                    {
+                        TableName = x.Item1,
+                        IndexName = x.Item2,
+                        ColumnName = x.Item3,
+                        IsUnique = x.Item4
+                    }).ToArray();
+                if (dbIndexes.Any(x => x.IndexName.InvariantEquals("IX_cmsContentType")) == false)
+                {
+                    Create.Index("IX_cmsContentType").OnTable("cmsContentType").OnColumn("nodeId").Ascending().WithOptions().Unique();
+                }
+                if (dbIndexes.Any(x => x.TableName.InvariantEquals("cmsContentType") && x.ColumnName.InvariantEquals("icon")) == false)
+                {
+                    Create.Index("IX_cmsContentType_icon").OnTable("cmsContentType").OnColumn("icon").Ascending().WithOptions().NonClustered();
+                }
+
                 Create.ForeignKey("FK_cmsContent_cmsContentType_nodeId")
                     .FromTable("cmsContent")
                     .ForeignColumn("contentType")
