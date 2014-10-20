54cd0662a5f1ab737246c984ed0afd2b351136b7
leekelleher <leekelleher@gmail.com>
2014-10-20 11:23:39 +0100

    Added stub template + JS controller for the Dictionary "list" view.

---
+++ src/Umbraco.Web.UI.Client/src/views/dictionary/dictionary.list.controller.js
@@ -0,0 +1,15 @@
+/**
+ * @ngdoc controller
+ * @name Umbraco.Editors.Dictionary.ListController
+ * @function
+ * 
+ * @description
+ * The controller for listing dictionary items
+ */
+function DictionaryListController($scope, localizationService) {
+
+	// do something
+
+}
+
+angular.module("umbraco").controller("Umbraco.Editors.Dictionary.ListController", DictionaryListController);
