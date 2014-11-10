462e57a49f184da4d990c17274493018e2441bf7
Jeremy Pyne <jpyne@proteusb2b.com>
2014-11-10 15:00:22 -0500

    Added code to resort items on user sorting.

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/nestedpropertieslist/fields.prevalues.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/nestedpropertieslist/fields.prevalues.controller.js
@@ -1,34 +1,66 @@
 ﻿angular.module("umbraco").controller("Umbraco.PrevalueEditors.NestedPropertyController",
-    function ($scope, entityResource) {
+function ($scope, entityResource) {
 
-        if (!$scope.model.value) {
-            $scope.model.value = [];
-        }
+    if (!$scope.model.value) {
+        $scope.model.value = [];
+    }
 
-        $scope.add = function () {
-            $scope.model.value.push({ alias: '', title: '', descriptoin: '', datatype: 'textstring', required: false });
-        };
+    $scope.add = function () {
+        $scope.model.value.push({ alias: '', title: '', descriptoin: '', datatype: 'textstring', required: false });
+    };
 
-        $scope.remove = function (index) {
-            var remainder = [];
-            for (var x = 0; x < $scope.model.value.length; x++) {
-                if (x !== index) {
-                    remainder.push($scope.model.value[x]);
-                }
+    $scope.remove = function (index) {
+        var remainder = [];
+        for (var x = 0; x < $scope.model.value.length; x++) {
+            if (x !== index) {
+                remainder.push($scope.model.value[x]);
             }
-            $scope.model.value = remainder;
-        };
+        }
+        $scope.model.value = remainder;
+    };
 
-        var setAliasDebounce = _.debounce(function (index) {
-            $scope.model.value[index].alias = $scope.model.value[index].label.substring(0, 1).toLowerCase() + $scope.model.value[index].label.replace(/\s+/, '').slice(1);
-        }, 500);
+    var setAliasDebounce = _.debounce(function (index) {
+        $scope.model.value[index].alias = $scope.model.value[index].label.substring(0, 1).toLowerCase() + $scope.model.value[index].label.replace(/\s+/, '').slice(1);
+    }, 500);
 
-        $scope.setAlias = function (index) {
-            setAliasDebounce(index);
+    $scope.setAlias = function (index) {
+        setAliasDebounce(index);
+    }
+
+    entityResource.getAll("Datatype").then(function (data) {
+        $scope.datatypes = data;
+    });
+
+    $scope.sortableOptions = {
+        axis: 'y',
+        containment: 'parent',
+        cursor: 'move',
+        items: '> div.control-group',
+        tolerance: 'pointer',
+        update: function (e, ui) {
+            // Get the new and old index for the moved element (using the text as the identifier, so 
+            // we'd have a problem if two prevalues were the same, but that would be unlikely)
+            var newIndex = ui.item.index();
+            var movedPrevalueText = $('input[type="text"][ng-model="item.alias"]', ui.item).val();
+            var originalIndex = getElementIndexByPrevalueText(movedPrevalueText);
+
+            // Move the element in the model
+            if (originalIndex > -1) {
+                var movedElement = $scope.model.value[originalIndex];
+                $scope.model.value.splice(originalIndex, 1);
+                $scope.model.value.splice(newIndex, 0, movedElement);
+            }
+        }
+    };
+
+    
+    function getElementIndexByPrevalueText(value) {
+        for (var i = 0; i < $scope.model.value.length; i++) {
+            if ($scope.model.value[i].alias === value) {
+                return i;
+            }
         }
 
-        entityResource.getAll("Datatype").then(function (data) {
-            $scope.datatypes = data;
-        });
-
-    });
+        return -1;
+    }
+});
--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/nestedpropertieslist/nestedpropertieslist.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/nestedpropertieslist/nestedpropertieslist.controller.js
@@ -4,24 +4,25 @@
         $scope.model.value = [];
     }
 
-    $scope.model.value = [{ fieldName: "test", optionalField: 1070 }];
-
     //add any fields that there isn't values for
     if ($scope.model.config.min > 0) {
         for (var i = 0; i < $scope.model.config.min; i++) {
             if ((i + 1) > $scope.model.value.length) {
-                $scope.model.value.push({ });
+                $scope.model.value.push({});
             }
         }
     }
 
     $scope.add = function () {
         if ($scope.model.config.max <= 0 || $scope.model.value.length < $scope.model.config.max) {
-            $scope.model.value.push({ });
+            writeValue();
+            $scope.model.value.push({});
+            syncValue();
         }
     };
 
     $scope.remove = function (index) {
+        writeValue();
         var remainder = [];
         for (var x = 0; x < $scope.model.value.length; x++) {
             if (x !== index) {
@@ -29,21 +30,59 @@
             }
         }
         $scope.model.value = remainder;
+        syncValue();
     };
 
-    
-    var await = [];
 
+    var await = [];
     var dataTypes = [];
+    var propertyEditors;
     $scope.properties = [];
 
-    dataTypeResource.getAllPropertyEditors().then(function (propertyEditors) {
+    function syncValue() {
+        $scope.value = _.map($scope.model.value, function (value) {
+            return _.map($scope.model.config.items, function (property) {
+                var datatype = dataTypes[property.datatype];
+                var propertyeditor = _.filter(propertyEditors, function (dt) {
+                    if (dt.alias == datatype.selectedEditor)
+                        return this;
+                }).pop();
+
+                var fakeItem = _.clone(property);
+
+                fakeItem.view = propertyeditor.view;
+                fakeItem.hideLabel = propertyeditor.hideLabel;
+
+                fakeItem.config = _.reduce(datatype.preValues, function (o, v, i) {
+                    o[v.key] = v.value;
+                    return o;
+                }, {});
+
+                fakeItem.validation = { mandatory: property.required, pattern: property.pattern };
+                fakeItem.value = value[property.alias];
+                return fakeItem;
+            });
+        });
+    }
+
+    function writeValue() {
+        console.log($scope.value);
+        console.log($scope.model.value);
+        $scope.model.value = _.map($scope.value, function (value) {
+            return _.reduce(value, function (o, v, i) {
+                o[v.alias] = v.value;
+                return o;
+            }, {});
+        });
+    }
+
+    dataTypeResource.getAllPropertyEditors().then(function (data) {
         //await.push(dataTypeResource.getAll().then(function (data) {
         //      _.each(dataTypes, function(data) {
         //          dataTypes[data.id] = data;
         //      });
         //}));
-        
+
         _.each(_.unique(_.map($scope.model.config.items, function (i) { return i.datatype })), function (id) {
             await.push(dataTypeResource.getById(id)
                 .then(function (data) {
@@ -51,40 +90,52 @@
                 }));
         });
 
+        propertyEditors = data;
+
         // Wait for all property types to be read.  This should be quicker then using the sommented out getAll above.
         $q.all(await).then(function () {
-            $scope.value = _.map($scope.model.value, function (value) {
-                return _.map($scope.model.config.items, function (property) {
-                    var datatype = dataTypes[property.datatype];
-                    var propertyeditor = _.filter(propertyEditors, function (dt) {
-                        if (dt.alias == datatype.selectedEditor)
-                            return this;
-                    }).pop();
-
-                    var fakeItem = _.clone(property);
-
-                    fakeItem.view = propertyeditor.view;
-                    fakeItem.hideLabel = propertyeditor.hideLabel;
-
-                    fakeItem.config = _.reduce(datatype.preValues, function (o, v, i) {
-                        o[v.key] = v.value;
-                        return o;
-                    }, {});
-
-                    fakeItem.validation = { mandatory: property.required, pattern: property.pattern };
-                    fakeItem.value = value[property.alias];
-                    return fakeItem;
-                });
-            });
+            syncValue();
 
         });
     });
 
 
     $scope.$on("formSubmitting", function (ev, args) {
-        //$scope.model.value; $scope.value
+        writeValue();
     });
-    
+
+    /*$scope.sortableOptions = {
+        axis: 'y',
+        containment: 'parent',
+        cursor: 'move',
+        items: '> div.control-group',
+        tolerance: 'pointer',
+        update: function (e, ui) {
+            // Get the new and old index for the moved element (using the text as the identifier, so 
+            // we'd have a problem if two prevalues were the same, but that would be unlikely)
+            var newIndex = ui.item.index();
+            var movedPrevalueText = $('input[type="text"][ng-model="item.alias"]', ui.item).val();
+            var originalIndex = getElementIndexByPrevalueText(movedPrevalueText);
+
+            // Move the element in the model
+            if (originalIndex > -1) {
+                var movedElement = $scope.model.value[originalIndex];
+                $scope.model.value.splice(originalIndex, 1);
+                $scope.model.value.splice(newIndex, 0, movedElement);
+            }
+        }
+    };
+
+
+    function getElementIndexByPrevalueText(value) {
+        for (var i = 0; i < $scope.model.value.length; i++) {
+            if ($scope.model.value[i].alias === value) {
+                return i;
+            }
+        }
+
+        return -1;
+    }*/
 }
 
 angular.module("umbraco").controller("Umbraco.PropertyEditors.NestedPropertiesListController", NestedPropertiesListController);
