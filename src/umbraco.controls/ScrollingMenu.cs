1e7197d28c3b2df07c89b8a75712b9772d30a61d
Shannon <sdeminick@gmail.com>
2014-11-14 18:45:29 +1100

    removes obsolete io classes

--- src/umbraco.controls/ScrollingMenu.cs
+++ src/umbraco.controls/ScrollingMenu.cs
@@ -1,15 +1,8 @@
 using System;
 using System.Collections;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Web;
-using System.Web.SessionState;
 using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.HtmlControls;
-using ClientDependency.Core;
-using umbraco.IO;
 
 
 namespace umbraco.uicontrols
