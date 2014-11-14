ed518feb4c684f0c0c2d54ad9bb5043ba5e2b74a
Shannon <sdeminick@gmail.com>
2014-11-14 14:24:35 +1100

    working on U4-5648 Remove legacy razor macro's entirely

--- src/umbraco.MacroEngines/RazorCore/RazorUmbracoFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using System.Web.WebPages.Razor;
-
-namespace umbraco.MacroEngines {
-    public class RazorUmbracoFactory : WebRazorHostFactory {
-        public override WebPageRazorHost CreateHost(string virtualPath, string physicalPath) {
-            return new RazorUmbracoHost(virtualPath, physicalPath);
-        }
-    }
-}
