c839264ad8427bde7cb27dab3f95ad673fd7eef5
Shannon <sdeminick@gmail.com>
2014-11-14 18:39:13 +1100

    Removes more old tinymce files, removes unused js files, removes unused search controls

--- src/Umbraco.Web.UI/umbraco/js/UmbracoSpeechBubbleInit.js
+++
@@ -1,3 +0,0 @@
-﻿//used by live editing to ensure the speech bubble is initialized after the main js file has been lazy loaded.
-//alert("Speech Bubble init: " + InitUmbracoSpeechBubble);
-InitUmbracoSpeechBubble();
