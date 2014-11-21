79990b6a6ccbee0bdcb3cac63635d994b6990e92
Anthony Dang <adang@thecogworks.co.uk>
2014-11-21 12:38:27 +0000

    Cleaned up interface as per Shannons comments https://github.com/umbraco/Umbraco-CMS/pull/559/files#r20683448

--- src/Umbraco.Core/Services/ILoggingService.cs
+++ src/Umbraco.Core/Services/ILoggingService.cs
@@ -7,34 +7,17 @@ namespace Umbraco.Core.Services
     /// </summary>
     public interface ILoggingService
     {
-        void Error<T>(string message, Exception exception);
         void Error(Type callingType, string message, Exception exception);
         void Warn(Type callingType, string message, params Func<object>[] formatItems);
-        void Warn(Type callingType, string message, bool showHttpTrace, params Func<object>[] formatItems);
+       
         void WarnWithException(Type callingType, string message, Exception e, params Func<object>[] formatItems);
-        void WarnWithException(Type callingType, string message, bool showHttpTrace, Exception e, params Func<object>[] formatItems);
-
-        void Warn<T>(string message, params Func<object>[] formatItems);
-        void Warn<T>(string message, bool showHttpTrace, params Func<object>[] formatItems);
-        void WarnWithException<T>(string message, Exception e, params Func<object>[] formatItems);
-        void WarnWithException<T>(string message, bool showHttpTrace, Exception e, params Func<object>[] formatItems);
-
-        void Info<T>(Func<string> generateMessage);
 
         void Info(Type callingType, Func<string> generateMessage);
 
         void Info(Type type, string generateMessageFormat, params Func<object>[] formatItems);
 
-        void Info<T>(string generateMessageFormat, params Func<object>[] formatItems);
-
-        void Debug<T>(Func<string> generateMessage);
-
         void Debug(Type callingType, Func<string> generateMessage);
 
         void Debug(Type type, string generateMessageFormat, params Func<object>[] formatItems);
-
-        void Debug<T>(string generateMessageFormat, params Func<object>[] formatItems);
-
-        void Debug<T>(string generateMessageFormat, bool showHttpTrace, params Func<object>[] formatItems);
     }
 }
