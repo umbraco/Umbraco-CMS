dc273683972c857b01b572142ba7e4a56bebe2fa
Shannon <sdeminick@gmail.com>
2014-11-27 17:57:33 +1100

    Fixes: U4-5891

---
+++ src/Umbraco.Web/WebApi/Filters/LegacyTreeAuthorizeAttribute.cs
@@ -0,0 +1,28 @@
+﻿using System.Web.Http;
+using System.Web.Http.Controllers;
+using Umbraco.Core;
+
+namespace Umbraco.Web.WebApi.Filters
+{
+    internal class LegacyTreeAuthorizeAttribute : AuthorizeAttribute
+    {
+        protected override bool IsAuthorized(HttpActionContext actionContext)
+        {
+            var httpContext = actionContext.Request.TryGetHttpContext();
+            if (httpContext)
+            {
+                var treeRequest = httpContext.Result.Request.QueryString["treeType"];
+                if (treeRequest.IsNullOrWhiteSpace()) return false;
+
+                var tree = ApplicationContext.Current.Services.ApplicationTreeService.GetByAlias(treeRequest);
+                if (tree == null) return false;
+
+                return UmbracoContext.Current.Security.CurrentUser != null
+                       && UmbracoContext.Current.Security.UserHasAppAccess(tree.ApplicationAlias, UmbracoContext.Current.Security.CurrentUser);
+            }
+            return false;
+
+            
+        }
+    }
+}
