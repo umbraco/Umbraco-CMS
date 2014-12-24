2f74dfe5cb0877667759741d36b13fc74e0b6c40
Shannon <sdeminick@gmail.com>
2014-12-24 17:01:41 +1100

    fixes missing using statement

--- src/Umbraco.Core/Services/MediaService.cs
+++ src/Umbraco.Core/Services/MediaService.cs
@@ -2,6 +2,7 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.Linq;
+using System.Text.RegularExpressions;
 using System.Threading;
 using System.Xml.Linq;
 using Umbraco.Core.Auditing;
