7c59c712612a288cfe16acab14ffe13b5f919729
Shannon <sdeminick@gmail.com>
2015-01-02 15:58:31 +1100

    updates PR for U4-6054 to just read directly from the file stream, no need to copy it first.

--- src/Umbraco.Core/IO/FileSystemExtensions.cs
+++ src/Umbraco.Core/IO/FileSystemExtensions.cs
@@ -2,27 +2,19 @@
 using System.IO;
 
 namespace Umbraco.Core.IO
-{	
+{
     public static class FileSystemExtensions
     {
-		public static long GetSize(this IFileSystem fs, string path)
+        public static long GetSize(this IFileSystem fs, string path)
         {
-			 using (var ms = new MemoryStream())
-			 {
-				 using (var sr = new StreamReader(ms))
-				 {
-					 using (Stream file = fs.OpenFile(path))
-					 {
-						 var bytes = new byte[file.Length];
-						 file.Read(bytes, 0, (int) file.Length);
-						 ms.Write(bytes, 0, (int) file.Length);
-						 file.Close();
-						 ms.Position = 0;
-						 string str = sr.ReadToEnd();
-						 return str.Length;
-					 }
-				 }
-			 }
+            using (var file = fs.OpenFile(path))
+            {
+                using (var sr = new StreamReader(file))
+                {
+                    var str = sr.ReadToEnd();
+                    return str.Length;
+                }
+            }
         }
 
         public static void CopyFile(this IFileSystem fs, string path, string newPath)
@@ -34,14 +26,14 @@ namespace Umbraco.Core.IO
         }
 
         public static string GetExtension(this IFileSystem fs, string path)
-		{
-			return Path.GetExtension(fs.GetFullPath(path));
-		}
+        {
+            return Path.GetExtension(fs.GetFullPath(path));
+        }
 
         public static string GetFileName(this IFileSystem fs, string path)
-		{
-			return Path.GetFileName(fs.GetFullPath(path));
-		}
+        {
+            return Path.GetFileName(fs.GetFullPath(path));
+        }
 
         //TODO: Currently this is the only way to do this
         internal static void CreateFolder(this IFileSystem fs, string folderPath)
