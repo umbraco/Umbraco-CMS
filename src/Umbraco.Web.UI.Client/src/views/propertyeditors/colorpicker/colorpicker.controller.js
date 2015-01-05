a4f7c49f50392d3f30ee97edeb4b821bf9acb261
Shannon <sdeminick@gmail.com>
2015-01-06 10:55:38 +1100

    Merge branch 'Alain-es-Fix_for_U4-5875' into dev-v7 - And updates quite a lot: changes directive name to valPropertyValidator and changes how it works to be ultra flexible for custom validation of property data. Updates umbProperty directive to expose an API for other directives to utilize (like the new set setPropertyError method).  Updates valServer to use the better way to access the current property object from the umbProperty directive API. Updates both valServer and valPropertyMsg to only perform a watch when necessary when there has been server side errors, this will speed up client side performance quite a bit.  Now the tags, color picker and upload property editors performs correct client side and server side validation for the required validation flag.

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/colorpicker/colorpicker.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/colorpicker/colorpicker.controller.js
@@ -2,11 +2,23 @@ function ColorPickerController($scope) {
     $scope.toggleItem = function (color) {
         if ($scope.model.value == color) {
             $scope.model.value = "";
+            //this is required to re-validate
+            $scope.propertyForm.modelValue.$setViewValue($scope.model.value);
         }
         else {
             $scope.model.value = color;
+            //this is required to re-validate
+            $scope.propertyForm.modelValue.$setViewValue($scope.model.value);
         }
     };
+    // Method required by the valPropertyValidator directive (returns true if the property editor has at least one color selected)
+    $scope.validateMandatory = function () {
+        return {
+            isValid: !$scope.model.validation.mandatory || ($scope.model.value != null && $scope.model.value != ""),
+            errorMsg: "Value cannot be empty",
+            errorKey: "required"
+        };
+    }
     $scope.isConfigured = $scope.model.config && $scope.model.config.items && _.keys($scope.model.config.items).length > 0;
 }
 
