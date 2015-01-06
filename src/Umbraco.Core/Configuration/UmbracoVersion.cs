ec5ca9087e11b7b079ef2e98837389be5affa96f
Shannon <sdeminick@gmail.com>
2015-01-06 18:24:27 +1100

    Removes all references of Microsoft.Web.Helpers - we don't need to ship this, we don't reference it and it has compatibility problems. Also updates the version to 7.3.0

--- src/Umbraco.Core/Configuration/UmbracoVersion.cs
+++ src/Umbraco.Core/Configuration/UmbracoVersion.cs
@@ -5,7 +5,7 @@ namespace Umbraco.Core.Configuration
 {
     public class UmbracoVersion
     {
-        private static readonly Version Version = new Version("7.2.1");
+        private static readonly Version Version = new Version("7.3.0");
 
         /// <summary>
         /// Gets the current version of Umbraco.
