10990e50e68898fcc4dac6355d388ba742d2f3b6
Shannon <sdeminick@gmail.com>
2015-01-30 15:48:55 +1100

    Fixes: U4-6209 GetGridHtml needs to be an extension on HtmlHelper not IPublishedContent so that ViewData and ModelState are inherited

--- src/Umbraco.Web/GridTemplateExtensions.cs
+++ src/Umbraco.Web/GridTemplateExtensions.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.Mvc.Html;
 using Umbraco.Core;
 using Umbraco.Core.Models;
 using System.Web.Mvc;
@@ -13,20 +14,63 @@ using Umbraco.Web.Mvc;
 
 namespace Umbraco.Web
 {
-    [Obsolete("This should not be used, GetGridHtml extensions on HtmlHelper should be used instead")]
+    
     public static class GridTemplateExtensions
     {
+        public static MvcHtmlString GetGridHtml(this HtmlHelper html, IPublishedProperty property, string framework = "bootstrap3")
+        {
+            var asString = property.Value as string;
+            if (asString.IsNullOrWhiteSpace()) return new MvcHtmlString(string.Empty);
+
+            var view = "Grid/" + framework;
+            return html.Partial(view, property.Value);
+        }
+
+        public static MvcHtmlString GetGridHtml(this HtmlHelper html, IPublishedContent contentItem)
+        {
+            return html.GetGridHtml(contentItem, "bodyText", "bootstrap3");
+        }
+
+        public static MvcHtmlString GetGridHtml(this HtmlHelper html, IPublishedContent contentItem, string propertyAlias)
+        {
+            Mandate.ParameterNotNullOrEmpty(propertyAlias, "propertyAlias");
+
+            return html.GetGridHtml(contentItem, propertyAlias, "bootstrap3");
+        }
+
+        public static MvcHtmlString GetGridHtml(this HtmlHelper html, IPublishedContent contentItem, string propertyAlias, string framework)
+        {
+            Mandate.ParameterNotNullOrEmpty(propertyAlias, "propertyAlias");
+
+            var view = "Grid/" + framework;
+            var prop = contentItem.GetProperty(propertyAlias);
+            if (prop == null) throw new NullReferenceException("No property type found with alias " + propertyAlias);
+            var model = prop.Value;
+
+            var asString = model as string;
+            if (asString.IsNullOrWhiteSpace()) return new MvcHtmlString(string.Empty);
+
+            return html.Partial(view, model);
+        }
+
+
+        [Obsolete("This should not be used, GetGridHtml extensions on HtmlHelper should be used instead")]
         public static MvcHtmlString GetGridHtml(this IPublishedProperty property, string framework = "bootstrap3")
         {
+            var asString = property.Value as string;
+            if (asString.IsNullOrWhiteSpace()) return new MvcHtmlString(string.Empty);
+            
             var view = "Grid/" + framework;
             return new MvcHtmlString(RenderPartialViewToString(view, property.Value));
         }
 
+        [Obsolete("This should not be used, GetGridHtml extensions on HtmlHelper should be used instead")]
         public static MvcHtmlString GetGridHtml(this IPublishedContent contentItem)
         {
             return GetGridHtml(contentItem, "bodyText", "bootstrap3");
         }
 
+        [Obsolete("This should not be used, GetGridHtml extensions on HtmlHelper should be used instead")]
         public static MvcHtmlString GetGridHtml(this IPublishedContent contentItem, string propertyAlias)
         {
             Mandate.ParameterNotNullOrEmpty(propertyAlias, "propertyAlias");
@@ -34,6 +78,7 @@ namespace Umbraco.Web
             return GetGridHtml(contentItem, propertyAlias, "bootstrap3");    
         }
 
+        [Obsolete("This should not be used, GetGridHtml extensions on HtmlHelper should be used instead")]
         public static MvcHtmlString GetGridHtml(this IPublishedContent contentItem, string propertyAlias, string framework)
         {
             Mandate.ParameterNotNullOrEmpty(propertyAlias, "propertyAlias");
@@ -49,6 +94,7 @@ namespace Umbraco.Web
             return new MvcHtmlString(RenderPartialViewToString(view, model));
         }
 
+        [Obsolete("This should not be used, GetGridHtml extensions on HtmlHelper should be used instead")]
         private static string RenderPartialViewToString(string viewName, object model)
         {
 
