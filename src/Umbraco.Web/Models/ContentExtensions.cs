20b21c86eab1cd743725b1b5131e674d37f6e19b
Stephan <stephane@umbraco.dk>
2015-03-03 13:03:11 +0100

    U4-3753 - add a way to get the rendering culture of a content

---
+++ src/Umbraco.Web/Models/ContentExtensions.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using Umbraco.Core;
+using Umbraco.Core.Models;
+using Umbraco.Web.Routing;
+
+namespace Umbraco.Web.Models
+{
+    public static class ContentExtensions
+    {
+        /// <summary>
+        /// Gets the culture that would be selected to render a specified content,
+        /// within the context of a specified current request.
+        /// </summary>
+        /// <param name="content">The content.</param>
+        /// <param name="current">The request Uri.</param>
+        /// <returns>The culture that would be selected to render the content.</returns>
+        public static CultureInfo GetCulture(this IContent content, Uri current = null)
+        {
+            var route = UmbracoContext.Current.ContentCache.GetRouteById(content.Id); // cached
+            var pos = route.IndexOf('/');
+
+            var domainHelper = new DomainHelper(ApplicationContext.Current.Services.DomainService);
+
+            var domain = pos == 0
+                ? null
+                : domainHelper.DomainForNode(int.Parse(route.Substring(0, pos)), current).UmbracoDomain;
+
+            if (domain == null)
+            {
+                var defaultLanguage = ApplicationContext.Current.Services.LocalizationService.GetAllLanguages().FirstOrDefault();
+                return defaultLanguage == null ? CultureInfo.CurrentUICulture : new CultureInfo(defaultLanguage.IsoCode);
+            }
+
+            var wcDomain = DomainHelper.FindWildcardDomainInPath(ApplicationContext.Current.Services.DomainService.GetAll(true), content.Path, domain.RootContent.Id);
+            return wcDomain == null
+                ? new CultureInfo(domain.Language.IsoCode)
+                : new CultureInfo(wcDomain.Language.IsoCode);
+        }
+    }
+}
--- src/Umbraco.Web/PublishedContentExtensions.cs
+++ src/Umbraco.Web/PublishedContentExtensions.cs
@@ -1,6 +1,7 @@
 using System;
 using System.Collections.Generic;
 using System.Data;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using Examine.LuceneEngine.SearchCriteria;
@@ -8,6 +9,7 @@ using Umbraco.Core.Models;
 using Umbraco.Core.Models.PublishedContent;
 using Umbraco.Web.Models;
 using Umbraco.Core;
+using Umbraco.Web.Routing;
 using ContentType = umbraco.cms.businesslogic.ContentType;
 
 namespace Umbraco.Web
@@ -1881,6 +1883,40 @@ namespace Umbraco.Web
 			set { _getPropertyAliasesAndNames = value; }
         }
 
+        #endregion
+
+        #region Culture
+
+        /// <summary>
+        /// Gets the culture that would be selected to render a specified content,
+        /// within the context of a specified current request.
+        /// </summary>
+        /// <param name="content">The content.</param>
+        /// <param name="current">The request Uri.</param>
+        /// <returns>The culture that would be selected to render the content.</returns>
+        public static CultureInfo GetCulture(this IPublishedContent content, Uri current = null)
+        {
+            var route = UmbracoContext.Current.ContentCache.GetRouteById(content.Id); // cached
+            var pos = route.IndexOf('/');
+
+            var domainHelper = new DomainHelper(ApplicationContext.Current.Services.DomainService);
+
+            var domain = pos == 0
+                ? null
+                : domainHelper.DomainForNode(int.Parse(route.Substring(0, pos)), current).UmbracoDomain;
+
+            if (domain == null)
+            {
+                var defaultLanguage = ApplicationContext.Current.Services.LocalizationService.GetAllLanguages().FirstOrDefault();
+                return defaultLanguage == null ? CultureInfo.CurrentUICulture : new CultureInfo(defaultLanguage.IsoCode);
+            }
+
+            var wcDomain = DomainHelper.FindWildcardDomainInPath(ApplicationContext.Current.Services.DomainService.GetAll(true), content.Path, domain.RootContent.Id);
+            return wcDomain == null
+                ? new CultureInfo(domain.Language.IsoCode)
+                : new CultureInfo(wcDomain.Language.IsoCode);
+        }
+        
         #endregion
     }
 }
