907e5e7ca4cb87f45b375268ddabe29aabac5cb6
Sebastiaan Janssen <sebastiaan@umbraco.com>
2015-03-20 11:25:59 +0100

    Bump version

--- src/Umbraco.Core/Configuration/UmbracoVersion.cs
+++ src/Umbraco.Core/Configuration/UmbracoVersion.cs
@@ -5,7 +5,7 @@ namespace Umbraco.Core.Configuration
 {
     public class UmbracoVersion
     {
-        private static readonly Version Version = new Version("7.2.4");
+        private static readonly Version Version = new Version("7.2.5");
 
         /// <summary>
         /// Gets the current version of Umbraco.
