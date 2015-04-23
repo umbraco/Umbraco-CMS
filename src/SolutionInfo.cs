5106ffab9714b094143c69ce97a925768d57ceea
Sebastiaan Janssen <sebastiaan@umbraco.com>
2015-04-23 15:00:12 +0200

    Mark as Release Candidate

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -12,4 +12,4 @@ using System.Resources;
 [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyFileVersion("7.2.5")]
-[assembly: AssemblyInformationalVersion("7.2.5")]
+[assembly: AssemblyInformationalVersion("7.2.5-RC")]
