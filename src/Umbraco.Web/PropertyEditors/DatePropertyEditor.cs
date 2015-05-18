2abb3177afb405925e61c5158fd01fce43c5a15e
Alain-es <alain.m@outlook.es>
2015-05-18 15:57:27 +0100

    Datepicker custom format

--- src/Umbraco.Web/PropertyEditors/DatePropertyEditor.cs
+++ src/Umbraco.Web/PropertyEditors/DatePropertyEditor.cs
@@ -65,6 +65,7 @@ namespace Umbraco.Web.PropertyEditors
 
         internal class DatePreValueEditor : PreValueEditor
         {
+            [PreValueField("format", "Date format", "textstring", Description = "If left empty then the format is YYYY-MM-DD. (see momentjs.com for supported formats)")]
             public string DefaultValue { get; set; }
         }
     }
