fe28bcafca6e7821612fc9fbf44917938d0db8c1
Mads Rasmussen <mads@umbraco.com>
2015-06-02 15:00:37 +0200

    Added custom directive to add sub views to a page

---
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/subviews/umbsubviews.directive.js
@@ -0,0 +1,44 @@
+angular.module("umbraco.directives")
+    .directive('umbSubViews', function () {
+        return {
+            restrict: 'E',
+            replace: true,
+            templateUrl: 'views/components/subviews/umb-sub-views.html',
+            link: function (scope, element, attrs, ctrl) {
+
+                scope.tools = [];
+
+                scope.toggleView = function(selectedView) {
+
+                    // set all other views to inactive
+                    for (var index = 0; index < scope.page.subViews.length; index++) {
+                        var view = scope.page.subViews[index];
+                        view.isActive = false;
+                    }
+
+                    // activate view
+                    selectedView.isActive = true;
+
+                    // set tool bar
+                    setToolBar(selectedView);
+                };
+
+                function setToolBar(selectedView) {
+                    scope.tools = selectedView.tools;
+                }
+
+                function init() {
+                    // set first element to active
+                    scope.page.subViews[0].isActive = true;
+                }
+
+                scope.$watch('page.subViews', function(newValue, oldValue) {
+                    if (newValue) {
+                        // wait for sub views to be loaded - then init
+                        init();
+                    }
+                });
+
+            }
+        };
+    });
