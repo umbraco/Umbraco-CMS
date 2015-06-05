50f3a4bd612d6306a13c85e42c7129de8e105bdf
Mads Rasmussen <mads@umbraco.com>
2015-06-05 22:34:11 +0200

    Change from select element to target overlay

---
+++ src/Umbraco.Web.UI.Client/src/views/documenttype/dialogs/contenttypes/contenttypes.controller.js
@@ -0,0 +1,15 @@
+/**
+ * @ngdoc controller
+ * @name Umbraco.Editors.DocumentType.PropertyController
+ * @function
+ *
+ * @description
+ * The controller for the content type editor property dialog
+ */
+function CompositionsController($scope, contentTypeResource) {
+
+
+
+}
+
+angular.module("umbraco").controller("Umbraco.Editors.DocumentType.CompositionsController", CompositionsController);
