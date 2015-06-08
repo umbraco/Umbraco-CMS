12702a12b4cde898e73341433de189f69badea3b
Mads Rasmussen <mads@umbraco.com>
2015-06-08 19:37:15 +0200

    Add validation to property

--- src/Umbraco.Web.UI.Client/src/views/documenttype/dialogs/editpropertysettings/editpropertysettings.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/documenttype/dialogs/editpropertysettings/editpropertysettings.controller.js
@@ -8,6 +8,47 @@
  */
 function EditPropertySettingsController($scope, contentTypeResource) {
 
+    $scope.propertySettings = {};
+    $scope.propertySettings.validationTypes = [];
+    $scope.propertySettings.showValidationPattern = false;
+
+
+    $scope.validationTypes = [
+        {
+            "name": "Validate as email",
+            "pattern": "[a-zA-Z0-9_\.\+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-\.]+",
+            "enableEditing": true
+        },
+        {
+            "name": "Validate as a number",
+            "pattern": "^[1-9]\d*$",
+            "enableEditing": true
+        },
+        {
+            "name": "Validate as a Url",
+            "pattern": "https?\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,}",
+            "enableEditing": true
+        },
+        {
+            "name": "...or enter a custom validation",
+            "pattern": "",
+            "enableEditing": true
+        }
+    ];
+
+
+    $scope.changeValidationType = function(selectedValidationType) {
+
+        if($scope.model.property.validation === undefined) {
+            $scope.model.property.validation = {};
+        }
+
+        $scope.model.property.validation.pattern = selectedValidationType.pattern;
+
+        $scope.propertySettings.showValidationPattern = true;
+
+    }
+
 
 }
 
