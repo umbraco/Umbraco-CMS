efd231f6a99020df29ff0325576d1a4955f767af
Per Ploug <ploug@umbraco.dk>
2015-06-23 09:56:19 +0200

    Core Constants Icons class

---
+++ src/Umbraco.Core/Constants-Icons.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umbraco.Core
+{
+    public static partial class Constants
+    {
+        public static class Icons
+        {
+            
+
+            /// <summary>
+            /// System contenttype icon
+            /// </summary>
+            public const string ContentType = "icon-arrangement";
+
+            /// <summary>
+            /// System datatype icon
+            /// </summary>
+            public const string DataType = "icon-autofill";
+
+            /// <summary>
+            /// System property editor icon
+            /// </summary>
+            public const string PropertyEditor = "icon-autofill";
+
+            /// <summary>
+            /// System macro icon
+            /// </summary>
+            public const string Macro = "icon-settings-alt";
+
+            /// <summary>
+            /// System member icon
+            /// </summary>
+            public const string Member = "icon-user";
+
+            /// <summary>
+            /// System member icon
+            /// </summary>
+            public const string MemberType = "icon-users";
+
+
+            /// <summary>
+            /// System member icon
+            /// </summary>
+            public const string Template = "icon-layout";
+
+        }
+    }
+}
