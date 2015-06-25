5487ec1d5f3949313705c0bca3ae9dee590695b3
Shannon <sdeminick@gmail.com>
2015-06-25 16:57:52 +0200

    Merge branch 'dev-v7-oembed' of https://github.com/tcmorris/Umbraco-CMS into tcmorris-dev-v7-oembed
    Conflicts:
    	src/Umbraco.Web/Umbraco.Web.csproj

---
+++ src/Umbraco.Web/Media/EmbedProviders/OEmbedPhoto.cs
@@ -0,0 +1,22 @@
+﻿using System.Web;
+using System.Xml;
+
+namespace Umbraco.Web.Media.EmbedProviders
+{
+    public class OEmbedPhoto : AbstractOEmbedProvider
+    {
+        public override string GetMarkup(string url, int maxWidth, int maxHeight)
+        {
+            string requestUrl = BuildFullUrl(url, maxWidth, maxHeight);
+
+            XmlDocument doc = GetXmlResponse(requestUrl);
+            string imageUrl = GetXmlProperty(doc, "/oembed/url");
+            string imageWidth = GetXmlProperty(doc, "/oembed/width");
+            string imageHeight = GetXmlProperty(doc, "/oembed/height");
+            string imageTitle = GetXmlProperty(doc, "/oembed/title");
+
+            return string.Format("<img src=\"{0}\" width\"{1}\" height=\"{2}\" alt=\"{3}\" />",
+                imageUrl, imageWidth, imageHeight, HttpUtility.HtmlEncode(imageTitle));
+        }
+    }
+}
