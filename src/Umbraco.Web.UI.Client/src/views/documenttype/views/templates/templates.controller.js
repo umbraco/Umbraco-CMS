05a411d54a7f56b6a759a2468127743a82bfab47
Mads Rasmussen <mads@umbraco.com>
2015-07-07 14:24:30 +0200

    add "fake" template as default for new content types

--- src/Umbraco.Web.UI.Client/src/views/documenttype/views/templates/templates.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/documenttype/views/templates/templates.controller.js
@@ -9,7 +9,7 @@
 (function() {
     'use strict';
 
-    function TemplatesController(entityResource) {
+    function TemplatesController($scope, entityResource) {
 
         /* ----------- SCOPE VARIABLES ----------- */
 
@@ -26,7 +26,51 @@
 
             entityResource.getAll("Template").then(function(templates){
                 vm.availableTemplates = templates;
+
+                // if new content type make default template based on type
+                if($scope.model.id === 0 && $scope.model.defaultTemplate === null) {
+
+                  var template = {
+                    "name": $scope.model.name,
+                    "id": -1,
+                    "icon": "icon-layout",
+                    "alias": $scope.model.alias
+                  };
+
+                  // set fake content type template as default
+                  $scope.model.defaultTemplate = template;
+
+                  // push fake content type template to allowed templates
+                  if(checkExistense($scope.model.allowedTemplates, template) === false ) {
+                    $scope.model.allowedTemplates.push(template);
+                  }
+
+                  // push fake content type template to available templates
+                  if(checkExistense(vm.availableTemplates, template) === false); {
+                    vm.availableTemplates.push(template);
+                  }
+
+                }
+
             });
+
+        }
+
+        function checkExistense(array, item) {
+
+          var found = false;
+
+          angular.forEach(array, function(arrayItem){
+            if(parseFloat(arrayItem.id) === parseFloat(item.id)) {
+              found = true;
+            }
+          });
+
+          if(found){
+            return true;
+          } else {
+            return false;
+          }
 
         }
 
