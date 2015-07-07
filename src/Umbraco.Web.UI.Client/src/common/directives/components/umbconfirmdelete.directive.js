2846c0c6b17123651013cd7a078e01bad6a15d09
Mads Rasmussen <mads@umbraco.com>
2015-07-07 09:46:26 +0200

    add confirm delete directive + add it to removing properties and tabs

---
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/umbconfirmdelete.directive.js
@@ -0,0 +1,31 @@
+(function() {
+  'use strict';
+
+  function ConfirmDelete() {
+
+    function link(scope, el, attr, ctrl) {
+
+      scope.confirmOverlayOpen = false;
+
+      scope.toggleOverlay = function() {
+        scope.confirmOverlayOpen = !scope.confirmOverlayOpen;
+      };
+
+    }
+
+    var directive = {
+      restrict: 'E',
+      replace: true,
+      templateUrl: 'views/components/umb-confirm-delete.html',
+      scope: {
+        confirmAction: "&"
+      },
+      link: link
+    };
+
+    return directive;
+  }
+
+  angular.module('umbraco.directives').directive('umbConfirmDelete', ConfirmDelete);
+
+})();
