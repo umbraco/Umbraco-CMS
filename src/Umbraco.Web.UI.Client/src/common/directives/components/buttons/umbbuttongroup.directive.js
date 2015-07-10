09941be0899d6eb0d3b8146e2e7e6c6ed111863c
Mads Rasmussen <mads@umbraco.com>
2015-07-10 11:57:11 +0200

    umb button group: set default button based on property value to make it easier to switch default button from outside the directive.

--- src/Umbraco.Web.UI.Client/src/common/directives/components/buttons/umbbuttongroup.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/buttons/umbbuttongroup.directive.js
@@ -7,33 +7,6 @@ angular.module("umbraco.directives")
             },
             restrict: 'E',
             replace: true,
-            templateUrl: 'views/components/buttons/umb-button-group.html',
-            link: function (scope, element, attrs, ctrl) {
-
-                scope.primaryAction = {};
-                scope.secondaryActions = [];
-
-                function generateActions() {
-
-                    for (var index = 0; index < scope.actions.length; index++) {
-
-                        var action = scope.actions[index];
-
-                        if( index === 0 ) {
-                            scope.primaryAction = action;
-                        } else {
-                            scope.secondaryActions.push(action);
-                        }
-                    }
-
-                }
-
-                scope.$watch('actions', function(newValue, oldValue) {
-                    if (newValue) {
-                        generateActions();
-                    }
-                });
-
-            }
+            templateUrl: 'views/components/buttons/umb-button-group.html'
         };
     });
