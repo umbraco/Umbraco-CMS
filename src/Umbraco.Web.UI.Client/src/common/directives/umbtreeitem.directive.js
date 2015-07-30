517464505955a5b47e53abd7d6660545b13ada7b
bjarnef <bjarne_fyrstenborg@hotmail.com>
2015-07-30 16:04:54 +0200

    Make tree nodes consistent
    Make tree nodes have same position from the left regardless of the arrow
    to expand the children.

--- src/Umbraco.Web.UI.Client/src/common/directives/umbtreeitem.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/umbtreeitem.directive.js
@@ -37,8 +37,7 @@ angular.module("umbraco.directives")
         template: '<li ng-class="{\'current\': (node == currentNode)}" on-right-click="altSelect(node, $event)">' +
             '<div ng-class="getNodeCssClass(node)" ng-swipe-right="options(node, $event)" >' +
             //NOTE: This ins element is used to display the search icon if the node is a container/listview and the tree is currently in dialog
-            //'<ins ng-if="tree.enablelistviewsearch && node.metaData.isContainer" class="umb-tree-node-search icon-search" ng-click="searchNode(node, $event)" alt="searchAltText"></ins>' +            
-            '<ins style="width:18px;"></ins>' +
+            //'<ins ng-if="tree.enablelistviewsearch && node.metaData.isContainer" class="umb-tree-node-search icon-search" ng-click="searchNode(node, $event)" alt="searchAltText"></ins>' + 
             '<ins ng-class="{\'icon-navigation-right\': !node.expanded, \'icon-navigation-down\': node.expanded}" ng-click="load(node)"></ins>' +
             '<i class="icon umb-tree-icon sprTree"></i>' +
             '<a ng-click="select(node, $event)"></a>' +
