ee1055339c91e0cde47b7117e53114d55dc4d1de
Mads Rasmussen <mads@umbraco.com>
2015-08-27 11:12:12 +0200

    refactor property settings dialog to use controller as syntax

--- src/Umbraco.Web.UI.Client/src/views/documenttype/dialogs/editpropertysettings/editpropertysettings.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/documenttype/dialogs/editpropertysettings/editpropertysettings.controller.js
@@ -8,92 +8,92 @@
  */
 function EditPropertySettingsController($scope, contentTypeResource) {
 
-    $scope.propertySettings = {};
-    $scope.propertySettings.validationTypes = [];
-    $scope.propertySettings.showValidationPattern = false;
-    $scope.propertySettings.focusOnPatternField = false;
+   var vm = this;
 
-    $scope.selectedValidationType = {};
+   vm.showValidationPattern = false;
+   vm.focusOnPatternField = false;
+   vm.selectedValidationType = {};
+   vm.validationTypes = [
+      {
+         "name": "Validate as email",
+         "key": "email",
+         "pattern": "[a-zA-Z0-9_\.\+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-\.]+",
+         "enableEditing": true
+      },
+      {
+         "name": "Validate as a number",
+         "key": "number",
+         "pattern": "^[0-9]*$",
+         "enableEditing": true
+      },
+      {
+         "name": "Validate as a Url",
+         "key": "url",
+         "pattern": "https?\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,}",
+         "enableEditing": true
+      },
+      {
+         "name": "...or enter a custom validation",
+         "key": "custom",
+         "pattern": "",
+         "enableEditing": true
+      }
+   ];
 
-    $scope.validationTypes = [
-        {
-            "name": "Validate as email",
-            "key": "email",
-            "pattern": "[a-zA-Z0-9_\.\+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-\.]+",
-            "enableEditing": true
-        },
-        {
-            "name": "Validate as a number",
-            "key": "number",
-            "pattern": "^[0-9]*$",
-            "enableEditing": true
-        },
-        {
-            "name": "Validate as a Url",
-            "key": "url",
-            "pattern": "https?\:\/\/[a-zA-Z0-9\-\.]+\.[a-zA-Z]{2,}",
-            "enableEditing": true
-        },
-        {
-            "name": "...or enter a custom validation",
-            "key": "custom",
-            "pattern": "",
-            "enableEditing": true
-        }
-    ];
+   vm.changeValidationType = changeValidationType;
 
-    function activate() {
+   function activate() {
 
       matchValidationType();
 
-    }
+   }
 
-    function matchValidationType() {
+   function matchValidationType() {
 
       if($scope.model.property.validation.pattern !== null && $scope.model.property.validation.pattern !== "" && $scope.model.property.validation.pattern !== undefined) {
 
-        var match = false;
+         var match = false;
 
-        // find and show if a match from the list has been chosen
-        angular.forEach($scope.validationTypes, function(validationType, index){
-          if($scope.model.property.validation.pattern === validationType.pattern) {
-            $scope.selectedValidationType = $scope.validationTypes[index];
-            $scope.propertySettings.showValidationPattern = true;
-            match = true;
-          }
-        });
-
-        // if there is no match - choose the custom validation option.
-        if(!match) {
-          angular.forEach($scope.validationTypes, function(validationType){
-            if(validationType.key === "custom") {
-              $scope.selectedValidationType = validationType;
-              $scope.propertySettings.showValidationPattern = true;
+         // find and show if a match from the list has been chosen
+         angular.forEach(vm.validationTypes, function(validationType, index){
+            if($scope.model.property.validation.pattern === validationType.pattern) {
+               vm.selectedValidationType = vm.validationTypes[index];
+               vm.showValidationPattern = true;
+               match = true;
             }
-          });
-        }
+         });
 
+         // if there is no match - choose the custom validation option.
+         if(!match) {
+            angular.forEach(vm.validationTypes, function(validationType){
+               if(validationType.key === "custom") {
+                  vm.selectedValidationType = validationType;
+                  vm.showValidationPattern = true;
+               }
+            });
+         }
       }
 
-    }
+   }
 
-    $scope.changeValidationType = function(selectedValidationType) {
+   function changeValidationType(selectedValidationType) {
+
       if(selectedValidationType) {
          $scope.model.property.validation.pattern = selectedValidationType.pattern;
-         $scope.propertySettings.showValidationPattern = true;
+         vm.showValidationPattern = true;
 
          // set focus on textarea
          if(selectedValidationType.key === "custom") {
-            $scope.propertySettings.focusOnPatternField = true;
+            vm.focusOnPatternField = true;
          }
 
       } else {
-         $scope.propertySettings.showValidationPattern = false;
+         vm.showValidationPattern = false;
       }
-    }
 
-    activate();
+   }
 
+   activate();
 
 }
 
