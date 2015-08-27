9104b99b3026843943bfecedfc589f1a69d5abfe
Mads Rasmussen <mads@umbraco.com>
2015-08-27 11:50:05 +0200

    Refactor property controller to follow angular style guide

--- src/Umbraco.Web.UI.Client/src/views/documenttype/dialogs/property.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/documenttype/dialogs/property.controller.js
@@ -6,78 +6,88 @@
  * @description
  * The controller for the content type editor property dialog
  */
-function DocumentTypePropertyController($scope, dataTypeResource) {
 
-	$scope.showTabs = false;
+ (function() {
+	"use strict";
 
-	var tabsLoaded = 0;
+	function DocumentTypePropertyController($scope, dataTypeResource) {
 
-	$scope.tabs = [
-		{
-			active: true,
-			id: 1,
-			label: "Default",
-			alias: "Default",
-			typesAndEditors: []
-		},
-		{
-			active: false,
-			id: 2,
-			label: "Reuse",
-			alias: "Reuse",
-			userConfigured: []
+		var vm = this;
+
+		vm.searchTerm = "";
+		vm.showTabs = false;
+		vm.tabsLoaded = 0;
+		vm.tabs = [
+			{
+				active: true,
+				id: 1,
+				label: "Default",
+				alias: "Default",
+				typesAndEditors: []
+			},
+			{
+				active: false,
+				id: 2,
+				label: "Reuse",
+				alias: "Reuse",
+				userConfigured: []
+			}
+		];
+
+		vm.showDetailsOverlay = showDetailsOverlay;
+		vm.hideDetailsOverlay = hideDetailsOverlay;
+
+		function activate() {
+
+			getAllUserConfiguredDataTypes();
+			getAllTypesAndEditors();
+
 		}
-	];
 
-	function activate() {
+		function getAllTypesAndEditors() {
 
-		getAllUserConfiguredDataTypes();
-		getAllTypesAndEditors();
+			dataTypeResource.getAllTypesAndEditors().then(function(data){
+				vm.tabs[0].typesAndEditors = data;
+				vm.tabsLoaded = vm.tabsLoaded + 1;
+				checkIfTabContentIsLoaded();
+			});
 
-	}
-
-	function getAllTypesAndEditors() {
-
-		dataTypeResource.getAllTypesAndEditors().then(function(data){
-			$scope.tabs[0].typesAndEditors = data;
-			tabsLoaded = tabsLoaded + 1;
-			checkIfTabContentIsLoaded();
-		});
-
-	}
-
-	function getAllUserConfiguredDataTypes() {
-
-		dataTypeResource.getAllUserConfigured().then(function(data){
-			$scope.tabs[1].userConfigured = data;
-			tabsLoaded = tabsLoaded + 1;
-			checkIfTabContentIsLoaded();
-		});
-
-	}
-
-	function checkIfTabContentIsLoaded() {
-		if(tabsLoaded === 2) {
-			$scope.showTabs = true;
 		}
+
+		function getAllUserConfiguredDataTypes() {
+
+			dataTypeResource.getAllUserConfigured().then(function(data){
+				vm.tabs[1].userConfigured = data;
+				vm.tabsLoaded = vm.tabsLoaded + 1;
+				checkIfTabContentIsLoaded();
+			});
+
+		}
+
+		function checkIfTabContentIsLoaded() {
+			if(vm.tabsLoaded === 2) {
+				vm.showTabs = true;
+			}
+		}
+
+		function showDetailsOverlay(property) {
+
+			var propertyDetails = {};
+			propertyDetails.icon = property.icon;
+			propertyDetails.title = property.name;
+
+			$scope.model.itemDetails = propertyDetails;
+
+		};
+
+		function hideDetailsOverlay() {
+			$scope.model.itemDetails = null;
+		}
+
+		activate();
+
 	}
 
-	$scope.showDetailsOverlay = function(property) {
+	angular.module("umbraco").controller("Umbraco.Editors.DocumentType.PropertyController", DocumentTypePropertyController);
 
-		var propertyDetails = {};
-		propertyDetails.icon = property.icon;
-		propertyDetails.title = property.name;
-
-		$scope.model.itemDetails = propertyDetails;
-
-	};
-
-	$scope.hideDetailsOverlay = function() {
-		$scope.model.itemDetails = null;
-	}
-
-	activate();
-
-}
-
-angular.module("umbraco").controller("Umbraco.Editors.DocumentType.PropertyController", DocumentTypePropertyController);
+})();
