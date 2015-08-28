1b7f204bb3ee6375bf708aff1af4317d82073e07
Mads Rasmussen <mads@umbraco.com>
2015-08-28 12:57:00 +0200

    refactor umb editor container directive to follow angular style guide

--- src/Umbraco.Web.UI.Client/src/common/directives/components/editor/umbeditorcontainer.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/editor/umbeditorcontainer.directive.js
@@ -1,9 +1,18 @@
-angular.module("umbraco.directives.html")
-    .directive('umbEditorContainer', function () {
-        return {
-            transclude: true,
-            restrict: 'E',
-            replace: true,        
-            templateUrl: 'views/components/editor/umb-editor-container.html'
-        };
-    });
+(function() {
+   'use strict';
+
+   function EditorContainerDirective() {
+
+      var directive = {
+         transclude: true,
+         restrict: 'E',
+         replace: true,
+         templateUrl: 'views/components/editor/umb-editor-container.html'
+      };
+
+      return directive;
+   }
+
+   angular.module('umbraco.directives').directive('umbEditorContainer', EditorContainerDirective);
+
+})();
