9ad888371227657fe0c4c7b345775bf343d254a6
Mads Rasmussen <mads@umbraco.com>
2015-08-28 12:58:50 +0200

    refactor umb editor footer directive to follow angular style guide

--- src/Umbraco.Web.UI.Client/src/common/directives/components/editor/umbeditorfooter.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/editor/umbeditorfooter.directive.js
@@ -1,9 +1,18 @@
-angular.module("umbraco.directives.html")
-    .directive('umbEditorFooter', function () {
-        return {
-            transclude: true,
-            restrict: 'E',
-            replace: true,        
-            templateUrl: 'views/components/editor/umb-editor-footer.html'
-        };
-    });
+(function() {
+   'use strict';
+
+   function EditorFooterDirective() {
+
+      var directive = {
+         transclude: true,
+         restrict: 'E',
+         replace: true,
+         templateUrl: 'views/components/editor/umb-editor-footer.html'
+      };
+
+      return directive;
+   }
+
+   angular.module('umbraco.directives').directive('umbEditorFooter', EditorFooterDirective);
+
+})();
