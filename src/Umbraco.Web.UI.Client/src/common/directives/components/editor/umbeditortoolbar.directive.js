58c0127b6e18f448d95650a4e0d89b6fc43ca56b
Mads Rasmussen <mads@umbraco.com>
2015-08-28 13:05:01 +0200

    refactor umb editor toolbar directive to follow angular style guide

--- src/Umbraco.Web.UI.Client/src/common/directives/components/editor/umbeditortoolbar.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/editor/umbeditortoolbar.directive.js
@@ -1,11 +1,20 @@
-angular.module("umbraco.directives")
-    .directive('umbEditorToolbar', function () {
-        return {
-            restrict: 'E',
-            replace: true,
-            templateUrl: 'views/components/editor/umb-editor-toolbar.html',
-            scope: {
-              tools: "="
-            }
-        };
-    });
+(function() {
+   'use strict';
+
+   function EditorToolbarDirective() {
+
+      var directive = {
+         restrict: 'E',
+         replace: true,
+         templateUrl: 'views/components/editor/umb-editor-toolbar.html',
+         scope: {
+            tools: "="
+         }
+      };
+
+      return directive;
+   }
+
+   angular.module('umbraco.directives').directive('umbEditorToolbar', EditorToolbarDirective);
+
+})();
