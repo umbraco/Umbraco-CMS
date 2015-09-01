9f5d5af7134f6cf6c1ec86288bcd10ee14054bd0
Mads Rasmussen <mads@umbraco.com>
2015-09-01 13:36:43 +0200

    move logic from umb-options-menu to umb-editor-menu

--- src/Umbraco.Web.UI.Client/src/common/directives/components/editor/umbeditormenu.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/editor/umbeditormenu.directive.js
@@ -1,16 +1,44 @@
 (function() {
    'use strict';
 
-   function EditorMenuDirective() {
+   function EditorMenuDirective($injector, treeService, navigationService, umbModelMapper, appState) {
+
+      function link(scope, el, attr, ctrl) {
+
+         //adds a handler to the context menu item click, we need to handle this differently
+         //depending on what the menu item is supposed to do.
+         scope.executeMenuItem = function (action) {
+             navigationService.executeMenuAction(action, scope.currentNode, scope.currentSection);
+         };
+
+         //callback method to go and get the options async
+         scope.getOptions = function () {
+
+             if (!scope.currentNode) {
+                 return;
+             }
+
+             //when the options item is selected, we need to set the current menu item in appState (since this is synonymous with a menu)
+             appState.setMenuState("currentNode", scope.currentNode);
+
+             if (!scope.actions) {
+                 treeService.getMenu({ treeNode: scope.currentNode })
+                     .then(function (data) {
+                         scope.actions = data.menuItems;
+                     });
+             }
+         };
+
+      }
 
       var directive = {
-         transclude: true,
          restrict: 'E',
          replace: true,
          templateUrl: 'views/components/navigation/umb-editor-menu.html',
+         link: link,
          scope: {
-            menu: "=",
-            dimmed: "="
+            currentNode: "=",
+            currentSection: "@"
          }
       };
 
