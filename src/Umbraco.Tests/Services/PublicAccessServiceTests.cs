c514ce8da3086698af4c2dc04a5430a3eeba66b0
Shannon <sdeminick@gmail.com>
2015-10-15 17:06:00 +0200

    Fixes: U4-7248 Can't update roles on public access, obsolete AddOrUpdateRule

--- src/Umbraco.Tests/Services/PublicAccessServiceTests.cs
+++ src/Umbraco.Tests/Services/PublicAccessServiceTests.cs
@@ -80,7 +80,7 @@ namespace Umbraco.Tests.Services
             publicAccessService.Save(entry);
 
             // Act
-            var updated = publicAccessService.AddOrUpdateRule(c, "TestType2", "AnotherVal");
+            var updated = publicAccessService.AddRule(c, "TestType2", "AnotherVal");
             //re-get
             entry = publicAccessService.GetEntryForContent(c);
 
@@ -88,6 +88,42 @@ namespace Umbraco.Tests.Services
             Assert.IsTrue(updated.Success);
             Assert.AreEqual(OperationStatusType.Success, updated.Result.StatusType);
             Assert.AreEqual(2, entry.Rules.Count());
+        }
+
+        [Test]
+        public void Can_Add_Multiple_Value_For_Same_Rule_Type()
+        {
+            // Arrange
+            var contentService = ServiceContext.ContentService;
+            var contentTypeService = ServiceContext.ContentTypeService;
+            var ct = MockedContentTypes.CreateSimpleContentType("blah", "Blah");
+            contentTypeService.Save(ct);
+            var c = MockedContent.CreateSimpleContent(ct, "Test", -1);
+            contentService.Save(c);
+            var publicAccessService = ServiceContext.PublicAccessService;
+            var entry = new PublicAccessEntry(c, c, c, new[]
+           {
+                new PublicAccessRule()
+                {
+                    RuleType = "TestType",
+                    RuleValue = "TestVal"
+                },
+            });
+            publicAccessService.Save(entry);
+
+            // Act
+            var updated1 = publicAccessService.AddRule(c, "TestType", "AnotherVal1");
+            var updated2 = publicAccessService.AddRule(c, "TestType", "AnotherVal2");
+
+            //re-get
+            entry = publicAccessService.GetEntryForContent(c);
+
+            // Assert           
+            Assert.IsTrue(updated1.Success);
+            Assert.IsTrue(updated2.Success);
+            Assert.AreEqual(OperationStatusType.Success, updated1.Result.StatusType);
+            Assert.AreEqual(OperationStatusType.Success, updated2.Result.StatusType);
+            Assert.AreEqual(3, entry.Rules.Count());
         }
 
         [Test]
@@ -106,14 +142,18 @@ namespace Umbraco.Tests.Services
                 new PublicAccessRule()
                 {
                     RuleType = "TestType",
-                    RuleValue = "TestValue"
+                    RuleValue = "TestValue1"
+                },
+                new PublicAccessRule()
+                {
+                    RuleType = "TestType",
+                    RuleValue = "TestValue2"
                 },
             });
-            publicAccessService.Save(entry);
-            publicAccessService.AddOrUpdateRule(c, "TestType", "AnotherVal");
+            publicAccessService.Save(entry);            
 
             // Act
-            var removed = publicAccessService.RemoveRule(c, "TestType", "TestValue");
+            var removed = publicAccessService.RemoveRule(c, "TestType", "TestValue1");
             //re-get
             entry = publicAccessService.GetEntryForContent(c);
 
@@ -121,7 +161,8 @@ namespace Umbraco.Tests.Services
             Assert.IsTrue(removed.Success);
             Assert.AreEqual(OperationStatusType.Success, removed.Result.StatusType);
             Assert.AreEqual(1, entry.Rules.Count());
-            Assert.AreEqual("AnotherVal", entry.Rules.ElementAt(0).RuleValue);
+            Assert.AreEqual("TestValue2", entry.Rules.ElementAt(0).RuleValue);
         }
+
     }
 }
