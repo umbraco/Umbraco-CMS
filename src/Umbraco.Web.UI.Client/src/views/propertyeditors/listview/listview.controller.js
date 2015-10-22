9449a778e915895f2b519c585e69b8ffc20c6685
Mads Rasmussen <mads@umbraco.com>
2015-10-22 09:42:00 +0200

    move list view pagination to directive

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/listview/listview.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/listview/listview.controller.js
@@ -142,29 +142,20 @@ function listViewController($rootScope, $scope, $routeParams, $injector, notific
         }
     }
 
-    $scope.next = function() {
-        if ($scope.options.pageNumber < $scope.listViewResultSet.totalPages) {
-            $scope.options.pageNumber++;
-            $scope.reloadView($scope.contentId);
-            //TODO: this would be nice but causes the whole view to reload
-            //$location.search("page", $scope.options.pageNumber);
-        }
+    $scope.next = function(pageNumber) {
+      $scope.options.pageNumber = pageNumber;
+      $scope.reloadView($scope.contentId);
     };
 
     $scope.goToPage = function(pageNumber) {
-        $scope.options.pageNumber = pageNumber + 1;
-        $scope.reloadView($scope.contentId);
-        //TODO: this would be nice but causes the whole view to reload
-        //$location.search("page", $scope.options.pageNumber);
+      $scope.options.pageNumber = pageNumber;
+      $scope.reloadView($scope.contentId);
     };
 
-    $scope.prev = function() {
-        if ($scope.options.pageNumber > 1) {
-            $scope.options.pageNumber--;
-            $scope.reloadView($scope.contentId);
-            //TODO: this would be nice but causes the whole view to reload
-            //$location.search("page", $scope.options.pageNumber);
-        }
+    $scope.prev = function(pageNumber) {
+      console.log(pageNumber);
+      $scope.options.pageNumber = pageNumber;
+      $scope.reloadView($scope.contentId);
     };
 
 
@@ -206,45 +197,6 @@ function listViewController($rootScope, $scope, $routeParams, $injector, notific
 
                 //reload!
                 $scope.reloadView(id);
-            }
-
-            $scope.pagination = [];
-
-            //list 10 pages as per normal
-            if ($scope.listViewResultSet.totalPages <= 10) {
-                for (var i = 0; i < $scope.listViewResultSet.totalPages; i++) {
-                    $scope.pagination.push({
-                        val: (i + 1),
-                        isActive: $scope.options.pageNumber == (i + 1)
-                    });
-                }
-            }
-            else {
-                //if there is more than 10 pages, we need to do some fancy bits
-
-                //get the max index to start
-                var maxIndex = $scope.listViewResultSet.totalPages - 10;
-                //set the start, but it can't be below zero
-                var start = Math.max($scope.options.pageNumber - 5, 0);
-                //ensure that it's not too far either
-                start = Math.min(maxIndex, start);
-
-                for (var i = start; i < (10 + start) ; i++) {
-                    $scope.pagination.push({
-                        val: (i + 1),
-                        isActive: $scope.options.pageNumber == (i + 1)
-                    });
-                }
-
-                //now, if the start is greater than 0 then '1' will not be displayed, so do the elipses thing
-                if (start > 0) {
-                    $scope.pagination.unshift({ name: "First", val: 1, isActive: false }, {val: "...",isActive: false});
-                }
-
-                //same for the end
-                if (start < maxIndex) {
-                    $scope.pagination.push({ val: "...", isActive: false }, { name: "Last", val: $scope.listViewResultSet.totalPages, isActive: false });
-                }
             }
 
         });
