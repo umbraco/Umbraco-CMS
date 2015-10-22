f66c07ec81a66df68b32c7b2d44ac84a4a5d4ef8
Shannon <sdeminick@gmail.com>
2015-10-22 17:34:01 +0200

    updates base data creation to not install the folder browser and instead to use the default media list view for the media folder property. Updates the FolderBrowserPropertyEditor to use the default list view property editor with no pre-values.

--- src/Umbraco.Web/PropertyEditors/FolderBrowserPropertyEditor.cs
+++ src/Umbraco.Web/PropertyEditors/FolderBrowserPropertyEditor.cs
@@ -1,15 +1,20 @@
-﻿using System.ComponentModel;
+﻿using System;
+using System.ComponentModel;
 using System.Web.Mvc;
 using Umbraco.Core;
 using Umbraco.Core.PropertyEditors;
 
 namespace Umbraco.Web.PropertyEditors
 {
-    [PropertyEditor(Constants.PropertyEditors.FolderBrowserAlias, "Folder Browser", "folderbrowser", HideLabel=true, Icon="icon-folder", Group="media")]
-    public class FolderBrowserPropertyEditor : PropertyEditor
+    [Obsolete("This is no longer used by default, use the ListViewPropertyEditor instead")]
+    [PropertyEditor(Constants.PropertyEditors.FolderBrowserAlias, "(Obsolete) Folder Browser", "listview", HideLabel=true, Icon="icon-folder", Group="media")]
+    public class FolderBrowserPropertyEditor : ListViewPropertyEditor
     {
-
-        
+        protected override PreValueEditor CreatePreValueEditor()
+        {
+            //return an empty one
+            return new PreValueEditor();
+        }
 
     }
 }
