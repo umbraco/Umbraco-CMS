5749f80a7fbfb574735f20b3b43df9be8794f2c8
andydale1982 <adale1982@hotmail.com>
2015-10-29 17:10:39 +0000

    Added instructions to manually install node_modules.

--- src/Umbraco.Web.UI/Umbraco/js/install.loader.js
+++ src/Umbraco.Web.UI/Umbraco/js/install.loader.js
@@ -1,4 +1,5 @@
-LazyLoad.js( [
+try {
+    LazyLoad.js([
       'lib/jquery/jquery.min.js',
       /* 1.1.5 */
       'lib/angular/1.1.5/angular.min.js',
@@ -13,5 +14,11 @@ LazyLoad.js( [
         jQuery(document).ready(function () {
             angular.bootstrap(document, ['ngSanitize', 'umbraco.install', 'umbraco.directives.validation']);
         });
+    });
+}
+catch (err) {
+    if (err.message == 'LazyLoad is not defined') {
+        document.getElementById("feedback").style.display = "none";
+        document.getElementById("missinglazyload").style.display = "block";
     }
-);
+}
