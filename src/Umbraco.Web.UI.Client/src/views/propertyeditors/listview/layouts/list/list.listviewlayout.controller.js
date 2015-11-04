608aa269f96a2dd1268a81b3373d5ca90e2aa4db
Simon Busborg <simon@busb.org>
2015-11-04 13:24:02 +0100

    Add umb-table directive, restyling, use directive in listview

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/listview/layouts/list/list.listviewlayout.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/listview/layouts/list/list.listviewlayout.controller.js
@@ -1,99 +1,46 @@
 (function() {
-   "use strict";
+    "use strict";
 
-   function ListViewListLayoutController($scope) {
+    function ListViewListLayoutController($scope, listViewHelper, $location) {
 
-      var vm = this;
+        var vm = this;
 
-      vm.selectItem = selectItem;
-      vm.selectAll = selectAll;
-      vm.isSelectedAll = isSelectedAll;
-      vm.isSortDirection = isSortDirection;
-      vm.sort = sort;
+        vm.selectItem = selectItem;
+        vm.clickItem = clickItem;
+        vm.selectAll = selectAll;
+        vm.isSelectedAll = isSelectedAll;
+        vm.isSortDirection = isSortDirection;
+        vm.sort = sort;
 
-      function selectAll($event) {
-         var checkbox = $event.target;
-         var clearSelection = false;
+        function selectAll($event) {
+            listViewHelper.selectAllItems($scope.items, $scope.selection, $event);
+        }
 
-         if (!angular.isArray($scope.items)) {
-            return;
-         }
+        function isSelectedAll() {
+            return listViewHelper.isSelectedAll($scope.items, $scope.selection);
+        }
 
-         $scope.selection.length = 0;
+        function selectItem(selectedItem, $index, $event) {
+            listViewHelper.selectHandler(selectedItem, $index, $scope.items, $scope.selection, $event);
+        }
 
-         for (var i = 0; i < $scope.items.length; i++) {
+        function clickItem(item) {
+            $location.path($scope.entityType + '/' + $scope.entityType + '/edit/' + item.id);
+        }
 
-            var entity = $scope.items[i];
+        function isSortDirection(col, direction) {
+            return listViewHelper.setSortingDirection(col, direction, $scope.options);
+        }
 
-            if (checkbox.checked) {
-               $scope.selection.push({id: entity.id});
-            } else {
-               clearSelection = true;
+        function sort(field, allow) {
+            if (allow) {
+                listViewHelper.setSorting(field, allow, $scope.options);
+                $scope.getContent($scope.contentId);
             }
+        }
 
-            entity.selected = checkbox.checked;
+    }
 
-         }
-
-         if (clearSelection) {
-            $scope.selection.length = 0;
-         }
-
-      }
-
-      function selectItem(item) {
-
-         var selection = $scope.selection;
-         var isSelected = false;
-
-         for (var i = 0; selection.length > i; i++) {
-            var selectedItem = selection[i];
-
-            if (item.id === selectedItem.id) {
-               isSelected = true;
-               selection.splice(i, 1);
-               item.selected = false;
-            }
-         }
-
-         if (!isSelected) {
-            selection.push({id: item.id});
-            item.selected = true;
-         }
-
-      }
-
-      function isSelectedAll() {
-         if (!angular.isArray($scope.items)) {
-            return false;
-         }
-         return _.every($scope.items, function(item) {
-            return item.selected;
-         });
-      }
-
-      function isSortDirection(col, direction) {
-          return $scope.options.orderBy.toUpperCase() == col.toUpperCase() && $scope.options.orderDirection == direction;
-      }
-
-      function sort(field, allow) {
-          if (allow) {
-             $scope.options.orderBy = field;
-
-             if ($scope.options.orderDirection === "desc") {
-                  $scope.options.orderDirection = "asc";
-             }
-             else {
-                  $scope.options.orderDirection = "desc";
-             }
-
-             $scope.getContent($scope.contentId);
-
-          }
-      }
-
-   }
-
-   angular.module("umbraco").controller("Umbraco.PropertyEditors.ListView.ListLayoutController", ListViewListLayoutController);
+    angular.module("umbraco").controller("Umbraco.PropertyEditors.ListView.ListLayoutController", ListViewListLayoutController);
 
 })();
