2664a75c84d11711ccb3efaf11638b771c65289c
Wincent <david.wincent@gmail.com>
2015-11-10 21:09:34 +0100

    Removed generation of old thumbnails

--- src/Umbraco.Web/PropertyEditors/FileUploadPropertyValueEditor.cs
+++ src/Umbraco.Web/PropertyEditors/FileUploadPropertyValueEditor.cs
@@ -128,29 +128,6 @@ namespace Umbraco.Web.PropertyEditors
                         {
                             var umbracoFile = UmbracoMediaFile.Save(fileStream, fileName);
 
-                            if (umbracoFile.SupportsResizing)
-                            {
-                                var additionalSizes = new List<int>();
-                                //get the pre-vals value
-                                var thumbs = editorValue.PreValues.FormatAsDictionary();
-                                if (thumbs.Any())
-                                {
-                                    var thumbnailSizes = thumbs.First().Value.Value;
-                                    // additional thumbnails configured as prevalues on the DataType
-									foreach (var thumb in thumbnailSizes.Split(new[] { ";", "," }, StringSplitOptions.RemoveEmptyEntries))
-                                    {
-                                        int thumbSize;
-                                        if (thumb == "" || int.TryParse(thumb, out thumbSize) == false) continue;
-                                        additionalSizes.Add(thumbSize);
-                                    }
-                                }
-
-                                using (var image = Image.FromStream(fileStream))
-                                {
-                                    ImageHelper.GenerateMediaThumbnails(fs, fileName, umbracoFile.Extension, image, additionalSizes);
-                                }
-
-                            }
                             newValue.Add(umbracoFile.Url);
                             //add to the saved paths
                             savedFilePaths.Add(umbracoFile.Url);
