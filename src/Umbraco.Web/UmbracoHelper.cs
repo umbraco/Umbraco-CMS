d6779619ea48d547bd03b36eb200c70646817b39
Shannon <sdeminick@gmail.com>
2015-12-22 15:45:15 +0100

    U4-6899 Change UmbracoHelper.TagQuery to return ITagQuery instead of the current hack to return a non interface

--- src/Umbraco.Web/UmbracoHelper.cs
+++ src/Umbraco.Web/UmbracoHelper.cs
@@ -35,7 +35,7 @@ namespace Umbraco.Web
         private IUmbracoComponentRenderer _componentRenderer;        
         private PublishedContentQuery _query;
         private MembershipHelper _membershipHelper;
-        private TagQuery _tag;
+        private ITagQuery _tag;
         private IDataTypeService _dataTypeService;
         private UrlProvider _urlProvider;
         private ICultureDictionary _cultureDictionary;
@@ -43,12 +43,8 @@ namespace Umbraco.Web
         /// <summary>
         /// Lazy instantiates the tag context
         /// </summary>
-        public TagQuery TagQuery
+        public ITagQuery TagQuery
         {
-            //TODO: Unfortunately we cannot change this return value to be ITagQuery
-            // since it's a breaking change, need to fix it for v8
-            // http://issues.umbraco.org/issue/U4-6899
-
             get
             {
                 return _tag ??
