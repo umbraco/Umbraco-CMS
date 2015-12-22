0d7363af23d93f8561d54d1ad115923ef4ef52f6
Jeremy Pyne <jpyne@proteusb2b.com>
2015-12-22 10:47:04 -0500

    Fix for MarkdownEditor no seeing changes from button clicks.

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/markdowneditor/markdowneditor.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/markdowneditor/markdowneditor.controller.js
@@ -1,5 +1,5 @@
 //inject umbracos assetsServce and dialog service
-function MarkdownEditorController($scope, assetsService, dialogService, $timeout) {
+function MarkdownEditorController($scope, assetsService, dialogService, angularHelper, $timeout) {
 
     //tell the assets service to load the markdown.editor libs from the markdown editors
     //plugin folder
@@ -17,29 +17,31 @@ function MarkdownEditorController($scope, assetsService, dialogService, $timeout
         .then(function () {
 
             // we need a short delay to wait for the textbox to appear.
-            setTimeout(function () {
-                //this function will execute when all dependencies have loaded
-                // but in the case that they've been previously loaded, we can only 
-                // init the md editor after this digest because the DOM needs to be ready first
-                // so run the init on a timeout
-                $timeout(function () {
-                    var converter2 = new Markdown.Converter();
-                    var editor2 = new Markdown.Editor(converter2, "-" + $scope.model.alias);
-                    editor2.run();
+            //this function will execute when all dependencies have loaded
+            // but in the case that they've been previously loaded, we can only 
+            // init the md editor after this digest because the DOM needs to be ready first
+            // so run the init on a timeout
+            $timeout(function () {
+                var converter2 = new Markdown.Converter();
+                var editor2 = new Markdown.Editor(converter2, "-" + $scope.model.alias);
+                editor2.run();
 
-                    //subscribe to the image dialog clicks
-                    editor2.hooks.set("insertImageDialog", function (callback) {
+                //subscribe to the image dialog clicks
+                editor2.hooks.set("insertImageDialog", function (callback) {
 
-                        dialogService.mediaPicker({
-                            callback: function (data) {
-                                callback(data.image);
-                            }
-                        });
-
-                        return true; // tell the editor that we'll take care of getting the image url
+                    dialogService.mediaPicker({
+                        callback: function (data) {
+                            callback(data.image);
+                        }
                     });
-                }, 200);
-            });
+
+                    return true; // tell the editor that we'll take care of getting the image url
+                });
+
+                editor2.hooks.set("onPreviewRefresh", function () {
+                    angularHelper.getCurrentForm($scope).$setDirty();
+                });
+            }, 200);
 
             //load the seperat css for the editor to avoid it blocking our js loading TEMP HACK
             assetsService.loadCss("lib/markdown/markdown.css");
