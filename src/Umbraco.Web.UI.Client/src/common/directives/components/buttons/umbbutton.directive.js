6ebb1d1b4b78b8b39579065bd031d8645d924f7a
Mads Rasmussen <mads@umbraco.com>
2015-12-29 13:54:46 +0100

    added documentation for umbButton

--- src/Umbraco.Web.UI.Client/src/common/directives/components/buttons/umbbutton.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/buttons/umbbutton.directive.js
@@ -1,3 +1,71 @@
+/**
+@ngdoc directive
+@name umbraco.directives.directive:umbButton
+@restrict E
+@scope
+
+@description
+Use this directive to render an umbraco button. The directive can be used to generate all types of buttons, set type, style, translation, shortcut and much more.
+
+<h3>Markup example</h3>
+<pre>
+    <div ng-controller="Umbraco.Controller as vm">
+
+        <umb-button
+            action="vm.clickButton"
+            type="button"
+            button-style="success"
+            state="vm.buttonState"
+            shortcut="ctrl+c"
+            labelKey="general_buttonText"
+            disabled="vm.buttonState === 'busy'">
+        </umb-button>
+
+    </div>
+</pre>
+
+<h3>Controller example</h3>
+<pre>
+    (function () {
+        "use strict";
+
+        function Controller(myService) {
+
+            var vm = this;
+            vm.buttonState = "init";
+
+            vm.clickButton = clickButton;
+
+            function clickButton() {
+
+                vm.buttonState = "busy";
+
+                myService.clickButton().then(function() {
+                    vm.buttonState = "success";
+                }, function() {
+                    vm.buttonState = "error";
+                });
+
+            }
+        }
+
+        angular.module("umbraco").controller("Umbraco.Controller", Controller);
+
+    })();
+</pre>
+
+@param {callback} action The button action which should be performed when the button is clicked.
+@param {string=} href Url/Path to navigato to. (The "href" attribute can not be used in combination with the "action" attribute).
+@param {string=} type Set the button type ("button" or "submit").
+@param {string=} buttonStyle Set the style of the button. The directive uses the default bootstrap styles ("primary", "info", "success", "warning", "danger", "inverse", "link").
+@param {string=} state Set a progress state on the button (init, busy, success, error).
+@param {string=} shortcut Set a keyboard shortcut for the button ("ctrl+c").
+@param {string=} label Set the button label.
+@param {string=} labelKey Set a localization key to make a multi lingual button ("general_buttonText").
+@param {string=} icon Set a button icon. Can only be used when buttonStyle is "link".
+@param {boolean=} disabled Set to <code>true</code> to disable the button.
+**/
+
 (function() {
    'use strict';
 
