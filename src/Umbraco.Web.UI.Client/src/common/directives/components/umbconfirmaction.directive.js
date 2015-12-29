adceb246518a644e78d86a00f77e8c7fcc5cddcd
Mads Rasmussen <mads@umbraco.com>
2015-12-29 14:06:05 +0100

    added documentation for umbConfirmAction

--- src/Umbraco.Web.UI.Client/src/common/directives/components/umbconfirmaction.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/umbconfirmaction.directive.js
@@ -1,3 +1,69 @@
+/**
+@ngdoc directive
+@name umbraco.directives.directive:umbConfirmAction
+@restrict E
+@scope
+
+@description
+<p>Use this directive to toggle a confirmation prompt for an action.
+The prompt consists of a checkmark and a cross to confirm or cancel the action.
+The prompt can be opened in four direction up, down, left or right.</p>
+
+<h3>Markup example</h3>
+<pre>
+    <div ng-controller="Umbraco.Controller as vm">
+
+        <div class="my-action">
+            <i class="icon-trash" ng-click="vm.showPrompt()"></i>
+            <umb-confirm-action
+                ng-if="vm.promptIsVisible"
+                direction="left"
+                on-confirm="confirmAction()"
+                on-cancel="hidePrompt()">
+            </umb-confirm-action>
+        </div>
+
+    </div>
+</pre>
+
+<h3>Controller example</h3>
+<pre>
+    (function () {
+
+        "use strict";
+
+        function Controller() {
+
+            var vm = this;
+            vm.promptIsVisible = false;
+
+            vm.confirmAction = confirmAction;
+            vm.showPrompt = showPrompt;
+            vm.hidePrompt = hidePrompt;
+
+            function confirmAction() {
+                // confirm logic here
+            }
+
+            function showPrompt() {
+                vm.promptIsVisible = true;
+            }
+
+            function hidePrompt() {
+                vm.promptIsVisible = false;
+            }
+
+        }
+
+        angular.module("umbraco").controller("Umbraco.Controller", Controller);
+    })();
+</pre>
+
+@param {string} direction The direction the prompt opens ("up", "down", "left", "right").
+@param {callback} onConfirm Callback when the checkmark is clicked.
+@param {callback} onCancel Callback when the cross is clicked.
+**/
+
 (function() {
   'use strict';
 
