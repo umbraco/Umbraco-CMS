86c6b01e737cc4de785ae9674d1d23a64659de41
Mads Rasmussen <mads@umbraco.com>
2015-12-29 09:45:59 +0100

    added documentation for editor directives

--- src/Umbraco.Web.UI.Client/src/common/directives/components/editor/umbeditorheader.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/editor/umbeditorheader.directive.js
@@ -1,3 +1,184 @@
+/**
+@ngdoc directive
+@name umbraco.directives.directive:umbEditorHeader
+@restrict E
+@scope
+
+@description
+Use this directive to construct a header inside the main editor window.
+
+<h3>Markup example</h3>
+<pre>
+    <div ng-controller="Umbraco.Controller as vm">
+
+        <umb-editor-view>
+
+            <umb-editor-header
+                name="vm.content.name"
+                alias="vm.content.alias"
+                description="vm.content.description"
+                icon="vm.content.icon">
+            </umb-editor-header>
+
+            <umb-editor-container>
+                // main content here
+            </umb-editor-container>
+
+            <umb-editor-footer>
+                // footer content here
+            </umb-editor-footer>
+
+        </umb-editor-view>
+
+    </div>
+</pre>
+
+<h3>Markup example - with tabs</h3>
+<pre>
+    <div ng-controller="Umbraco.Controller as vm">
+
+        <umb-editor-view umb-tabs>
+
+            <umb-editor-header
+               name="content.name"
+               tabs="content.tabs"
+               hide-icon="true"
+               hide-description="true"
+               hide-alias="true">
+            </umb-editor-header>
+
+            <umb-editor-container>
+               <umb-tabs-content class="form-horizontal" view="true">
+                  <umb-tab id="tab{{tab.id}}" ng-repeat="tab in vm.content.tabs" rel="{{tab.id}}">
+                    // tab content here
+                  </umb-tab>
+               </umb-tabs-content>
+            </umb-editor-container>
+
+            <umb-editor-footer>
+                // footer content here
+            </umb-editor-footer>
+
+        </umb-editor-view>
+
+    </div>
+</pre>
+
+<h3>Controller example - with tabs</h3>
+<pre>
+    (function () {
+        "use strict";
+
+        function Controller() {
+
+            var vm = this;
+            vm.content = {
+                name: "My editor",
+                tabs: [
+                    {
+                        id: 1,
+                        label: "Tab 1",
+                        alias: "tab1",
+                        active: true
+                    },
+                    {
+                        id: 2,
+                        label: "Tab 2",
+                        alias: "tab2",
+                        active: false
+                    }
+                ]
+            };
+
+        }
+
+        angular.module("umbraco").controller("Umbraco.Controller", Controller);
+    })();
+</pre>
+
+<h3>Markup example - with sub views</h3>
+<pre>
+    <div ng-controller="Umbraco.Controller as vm">
+
+        <umb-editor-view>
+
+            <umb-editor-header
+                name="vm.content.name"
+                navigation="vm.content.navigation"
+                hide-icon="true"
+                hide-description="true"
+                hide-alias="true">
+            </umb-editor-header>
+
+            <umb-editor-container>
+                <umb-editor-sub-views
+                    sub-views="vm.content.navigation"
+                    model="vm.content">
+                </umb-editor-sub-views>
+            </umb-editor-container>
+
+            <umb-editor-footer>
+                // footer content here
+            </umb-editor-footer>
+
+        </umb-editor-view>
+
+    </div>
+</pre>
+
+<h3>Controller example - with sub views</h3>
+<pre>
+    (function () {
+
+        "use strict";
+
+        function Controller() {
+
+            var vm = this;
+            vm.content = {
+                name: "My editor",
+                navigation: [
+                    {
+                        "name": "Section 1",
+                        "icon": "icon-document-dashed-line",
+                        "view": "path/to/html/file.html",
+                        "active": true
+                    },
+                    {
+                        "name": "Section 2",
+                        "icon": "icon-list",
+                        "view": "path/to/html/file.html"
+                    }
+                ]
+            };
+
+        }
+
+        angular.module("umbraco").controller("Umbraco.Controller", Controller);
+    })();
+</pre>
+
+<h3>Use in combination with</h3>
+<ul>
+    <li>{@link umbraco.directives.directive:umbEditorView umbEditorView}</li>
+    <li>{@link umbraco.directives.directive:umbEditorContainer umbEditorContainer}</li>
+    <li>{@link umbraco.directives.directive:umbEditorFooter umbEditorFooter}</li>
+</ul>
+
+@param {string} name The content name.
+@param {array=} tabs Array of tabs. See example above.
+@param {boolean=} nameLocked Set to <code>true</code> to lock the name.
+@param {object=} menu Add a context menu to the editor.
+@param {string=} icon Show and edit the content icon. Opens an overlay to change the icon.
+@param {boolean=} hideIcon Set to <code>true</code> to hide icon.
+@param {string=} alias show and edit the content alias.
+@param {boolean=} hideAlias Set to <code>true</code> to hide alias.
+@param {string=} description Add a description to the content.
+@param {boolean=} hideDescription Set to <code>true</code> to hide description.
+@param {array=} navigation Array of sub views. See example above.
+
+**/
+
 (function() {
     'use strict';
 
--- src/Umbraco.Web.UI.Client/src/common/directives/components/editor/umbeditorview.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/editor/umbeditorview.directive.js
@@ -1,3 +1,43 @@
+/**
+@ngdoc directive
+@name umbraco.directives.directive:umbEditorView
+@restrict E
+@scope
+
+@description
+Use this directive to construct the main editor window.
+
+<h3>Markup example</h3>
+<pre>
+    <div ng-controller="Umbraco.Controller as vm">
+
+        <umb-editor-view>
+
+            <umb-editor-header
+                // header configuration>
+            </umb-editor-header>
+
+            <umb-editor-container>
+                // main content here
+            </umb-editor-container>
+
+            <umb-editor-footer>
+                // footer content here
+            </umb-editor-footer>
+
+        </umb-editor-view>
+
+    </div>
+</pre>
+
+<h3>Use in combination with</h3>
+<ul>
+    <li>{@link umbraco.directives.directive:umbEditorHeader umbEditorHeader}</li>
+    <li>{@link umbraco.directives.directive:umbEditorContainer umbEditorContainer}</li>
+    <li>{@link umbraco.directives.directive:umbEditorFooter umbEditorFooter}</li>
+</ul>
+**/
+
 (function() {
    'use strict';
 
