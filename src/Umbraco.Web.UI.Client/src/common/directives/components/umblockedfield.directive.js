8fc84f9b093d8b497432417ca40f1717e539f6c2
Mads Rasmussen <mads@umbraco.com>
2015-12-30 13:13:38 +0100

    added documentation to umbLockedField

--- src/Umbraco.Web.UI.Client/src/common/directives/components/umblockedfield.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/umblockedfield.directive.js
@@ -1,11 +1,48 @@
 /**
-* @ngdoc directive
-* @name umbraco.directives.directive:umbLockedField 
-* @restrict E
-* @function
-* @description
-* Used by editors that require naming an entity. Shows a textbox/headline with a required validator within it's own form.
+@ngdoc directive
+@name umbraco.directives.directive:umbLockedField
+@restrict E
+@scope
+
+@description
+Use this directive to render a value with a lock next to it. When the lock is clicked the value gets unlocked and can be edited.
+
+<h3>Markup example</h3>
+<pre>
+	<div ng-controller="Umbraco.Controller as vm">
+
+		<umb-locked-field
+			ng-model="vm.value"
+			placeholder-text="'Click to unlock...'">
+		</umb-locked-field>
+
+	</div>
+</pre>
+
+<h3>Controller example</h3>
+<pre>
+	(function () {
+		"use strict";
+
+		function Controller() {
+
+			var vm = this;
+			vm.value = "My locked text";
+
+        }
+
+		angular.module("umbraco").controller("Umbraco.Controller", Controller);
+
+	})();
+</pre>
+
+@param {string} ngModel (<code>binding</code>): The locked text.
+@param {boolean} locked (<code>binding</code>: Set to <code>true</code> to unlock the text.
+@param {string=} placeholderText (<code>binding</code>): If ngModel is empty this text will be shown.
+@param {string=} regexValidation (<code>binding</code>): Set a regex expression for validation of the field.
+@param {string=} serverValidationField (<code>attribute</code>): Set a server validation field.
 **/
+
 (function() {
 	'use strict';
 
