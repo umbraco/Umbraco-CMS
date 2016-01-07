02b6f94a1b01a408f3b941de3fb7d107cff28b34
Shannon <sdeminick@gmail.com>
2016-01-07 19:46:22 +0100

    moves the current logic into a re-usable ext method

--- src/Umbraco.Core/Services/ContentTypeService.cs
+++ src/Umbraco.Core/Services/ContentTypeService.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.Xml.Linq;
 using System.Threading;
+using AutoMapper;
 using Umbraco.Core.Auditing;
 using Umbraco.Core.Configuration;
 using Umbraco.Core.Events;
@@ -20,6 +21,95 @@ using Umbraco.Core.Persistence.UnitOfWork;
 
 namespace Umbraco.Core.Services
 {
+    public static class ContentTypeServiceExtensions
+    {
+        /// <summary>
+        /// Returns the available composite content types for a given content type
+        /// </summary>
+        /// <returns></returns>
+        public static IEnumerable<IContentTypeComposition> GetAvailableCompositeContentTypes(this IContentTypeService ctService, 
+            int contentTypeId,
+            IContentTypeComposition[] allContentTypes)
+        {
+            IContentTypeComposition source = null;
+
+            //below is all ported from the old doc type editor and comes with the same weaknesses /insanity / magic
+
+            // note: there are many sanity checks missing here and there ;-((
+            // make sure once and for all
+            //if (allContentTypes.Any(x => x.ParentId > 0 && x.ContentTypeComposition.Any(y => y.Id == x.ParentId) == false))
+            //    throw new Exception("A parent does not belong to a composition.");
+
+            // find out if any content type uses this content type
+            var isUsing = allContentTypes.Where(x => x.ContentTypeComposition.Any(y => y.Id == contentTypeId)).ToArray();
+            if (isUsing.Length > 0)
+            {
+                //if already in use a composition, do not allow any composited types
+                return new List<IContentTypeComposition>();
+            }
+
+            // if it is not used then composition is possible
+            // hashset guarantees unicity on Id
+            var list = new HashSet<IContentTypeComposition>(new DelegateEqualityComparer<IContentTypeComposition>(
+                (x, y) => x.Id == y.Id,
+                x => x.Id));
+
+            // usable types are those that are top-level
+            var usableContentTypes = allContentTypes
+                .Where(x => x.ContentTypeComposition.Any() == false).ToArray();
+            foreach (var x in usableContentTypes)
+                list.Add(x);
+
+            // indirect types are those that we use, directly or indirectly
+            var indirectContentTypes = GetIndirect(source).ToArray();
+            foreach (var x in indirectContentTypes)
+                list.Add(x);
+
+            //// directContentTypes are those we use directly
+            //// they are already in indirectContentTypes, no need to add to the list
+            //var directContentTypes = source.ContentTypeComposition.ToArray();
+
+            //var enabled = usableContentTypes.Select(x => x.Id) // those we can use
+            //    .Except(indirectContentTypes.Select(x => x.Id)) // except those that are indirectly used
+            //    .Union(directContentTypes.Select(x => x.Id)) // but those that are directly used
+            //    .Where(x => x != source.ParentId) // but not the parent
+            //    .Distinct()
+            //    .ToArray();
+
+            return list
+                .Where(x => x.Id != contentTypeId)
+                .OrderBy(x => x.Name)                
+                .ToList();
+        }
+
+        private static IEnumerable<IContentTypeComposition> GetIndirect(IContentTypeComposition ctype)
+        {
+            // hashset guarantees unicity on Id
+            var all = new HashSet<IContentTypeComposition>(new DelegateEqualityComparer<IContentTypeComposition>(
+                (x, y) => x.Id == y.Id,
+                x => x.Id));
+
+            var stack = new Stack<IContentTypeComposition>();
+
+            if (ctype != null)
+            {
+                foreach (var x in ctype.ContentTypeComposition)
+                    stack.Push(x);
+            }
+
+            while (stack.Count > 0)
+            {
+                var x = stack.Pop();
+                all.Add(x);
+                foreach (var y in x.ContentTypeComposition)
+                    stack.Push(y);
+            }
+
+            return all;
+        }
+
+    }
+
     /// <summary>
     /// Represents the ContentType Service, which is an easy access to operations involving <see cref="IContentType"/>
     /// </summary>
