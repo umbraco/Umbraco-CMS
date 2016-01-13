88b8cfb2bdefde7660b0b2068f2b963e68249a21
Shannon <sdeminick@gmail.com>
2016-01-13 16:59:14 +0100

    streamlines how the disabled check boxes work - will perform better too - fixes re-allowing the checkbox on refilter for ancestors.

--- src/Umbraco.Web.UI.Client/src/views/common/overlays/contenttypeeditor/compositions/compositions.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/common/overlays/contenttypeeditor/compositions/compositions.controller.js
@@ -5,17 +5,10 @@
 
         var vm = this;
 
-        vm.isDisabled = isDisabled;
         vm.isSelected = isSelected;
 
         function isSelected(alias) {
             if($scope.model.contentType.compositeContentTypes.indexOf(alias) !== -1) {
-                return true;
-            }
-        }
-
-        function isDisabled(alias) {
-            if($scope.model.contentType.lockedCompositeContentTypes.indexOf(alias) !== -1) {
                 return true;
             }
         }
