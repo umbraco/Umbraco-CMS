1362df8e64cc0daafec578e9686b1da154c27117
AndyButland <abutland73@gmail.com>
2016-01-30 20:48:21 +0100

    Provided means of setting which bulk actions are allowed on list views; only enable selection of multiple items in list view if at least one bulk action is available

---
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/listview/bulkActionPermissions.prevalues.controller.js
@@ -0,0 +1,17 @@
+/**
+ * @ngdoc controller
+ * @name Umbraco.PrevalueEditors.ListViewBulkActionPermissionsPreValsController
+ * @function
+ *
+ * @description
+ * The controller for configuring the allowed bulk actions for list views
+ */
+(function () {
+    "use strict";
+
+    function ListViewBulkActionPermissionsPreValsController($scope) {
+    }
+
+    angular.module("umbraco").controller("Umbraco.PrevalueEditors.ListViewBulkActionPermissionsPreValsController", ListViewBulkActionPermissionsPreValsController);
+
+})();
