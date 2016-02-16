89df81d3dfcf2a256b99ee2ee884b7482701ba9f
Mads Rasmussen <mads@umbraco.com>
2016-02-16 15:12:15 +0100

    update umbButtonGroup example to match newest code

--- src/Umbraco.Web.UI.Client/src/common/directives/components/buttons/umbbuttongroup.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/buttons/umbbuttongroup.directive.js
@@ -9,13 +9,13 @@ Use this directive to render a button with a dropdown of alternative actions.
 
 <h3>Markup example</h3>
 <pre>
-    <div ng-controller="Umbraco.Controller as vm">
+    <div ng-controller="My.Controller as vm">
 
         <umb-button-group
            ng-if="vm.buttonGroup"
            default-button="vm.buttonGroup.defaultButton"
            sub-buttons="vm.buttonGroup.subButtons"
-           direction="up"
+           direction="down"
            float="right">
         </umb-button-group>
 
@@ -30,10 +30,12 @@ Use this directive to render a button with a dropdown of alternative actions.
         function Controller() {
 
             var vm = this;
+
             vm.buttonGroup = {
                 defaultButton: {
                     labelKey: "general_defaultButton",
                     hotKey: "ctrl+d",
+                    hotKeyWhenHidden: true,
                     handler: function() {
                         // do magic here
                     }
@@ -42,15 +44,16 @@ Use this directive to render a button with a dropdown of alternative actions.
                     {
                         labelKey: "general_subButton",
                         hotKey: "ctrl+b",
+                        hotKeyWhenHidden: true,
                         handler: function() {
                             // do magic here
                         }
                     }
                 ]
-            }
+            };
         }
 
-        angular.module("umbraco").controller("Umbraco.Controller", Controller);
+        angular.module("umbraco").controller("My.Controller", Controller);
 
     })();
 </pre>
@@ -68,6 +71,11 @@ Use this directive to render a button with a dropdown of alternative actions.
         Set a keyboard shortcut for the button ("ctrl+c").
     </li>
     <li>
+        <strong>hotKeyWhenHidden</strong>
+        <small>(boolean)</small> -
+        As a default the hotkeys only works on elements visible in the UI. Set to <code>true</code> to set a hotkey on the hidden sub buttons.
+    </li>
+    <li>
         <strong>handler</strong>
         <small>(callback)</small> -
         Set a callback to handle button click events.
@@ -76,9 +84,9 @@ Use this directive to render a button with a dropdown of alternative actions.
 
 @param {object} defaultButton The model of the default button.
 @param {array} subButtons Array of sub buttons.
-@param {string=} state Set a progress state on the button (init, busy, success, error).
-@param {string=} direction Set the direction of the dropdown (up, down).
-@param {string=} float Set the float of the dropdown. (left, right).
+@param {string=} state Set a progress state on the button ("init", "busy", "success", "error").
+@param {string=} direction Set the direction of the dropdown ("up", "down").
+@param {string=} float Set the float of the dropdown. ("left", "right").
 **/
 
 (function() {
