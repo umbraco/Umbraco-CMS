dbb9386d22b88daf2e5e375c6bb989d7edfa2122
Mads Rasmussen <mads@umbraco.com>
2016-02-18 11:20:04 +0100

    Fixes: U4-7660 [7.4 Fix] Added a default value to the Umbaco.TrueFalse datatype. Thanks Jeremy Pyne

--- src/Umbraco.Web/PropertyEditors/TrueFalsePropertyEditor.cs
+++ src/Umbraco.Web/PropertyEditors/TrueFalsePropertyEditor.cs
@@ -6,5 +6,15 @@ namespace Umbraco.Web.PropertyEditors
     [PropertyEditor(Constants.PropertyEditors.TrueFalseAlias, "True/False", "INT", "boolean", IsParameterEditor = true, Group = "Common", Icon="icon-checkbox")]
     public class TrueFalsePropertyEditor : PropertyEditor
     {
+        protected override PreValueEditor CreatePreValueEditor()
+        {
+            return new TrueFalsePreValueEditor();
+        }
+
+        internal class TrueFalsePreValueEditor : PreValueEditor
+        {
+            [PreValueField("default", "Default Value", "boolean")]
+            public string Default { get; set; }
+        }
     }
 }
