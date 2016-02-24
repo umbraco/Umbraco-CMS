9f7b82e20faad47d6c51e8f1717a5cd0c5dd36b1
Stephan <stephane@umbraco.dk>
2016-02-24 13:59:15 +0100

    U4-8077 - ensure product name in migration event handlers

--- src/Umbraco.Web/Strategies/Migrations/ClearMediaXmlCacheForDeletedItemsAfterUpgrade.cs
+++ src/Umbraco.Web/Strategies/Migrations/ClearMediaXmlCacheForDeletedItemsAfterUpgrade.cs
@@ -5,21 +5,24 @@ using Umbraco.Core.Logging;
 using Umbraco.Core.Persistence.Migrations;
 using Umbraco.Core.Persistence.SqlSyntax;
 using umbraco.interfaces;
+using Umbraco.Core.Configuration;
 
 namespace Umbraco.Web.Strategies.Migrations
 {
     /// <summary>
     /// This will execute after upgrading to remove any xml cache for media that are currently in the bin
     /// </summary>
-    /// <remarks>    
-    /// This will execute for specific versions - 
-    /// 
+    /// <remarks>
+    /// This will execute for specific versions -
+    ///
     /// * If current is less than or equal to 7.0.0
     /// </remarks>
     public class ClearMediaXmlCacheForDeletedItemsAfterUpgrade : MigrationStartupHander
-    {       
+    {
         protected override void AfterMigration(MigrationRunner sender, MigrationEventArgs e)
         {
+            if (e.ProductName != GlobalSettings.UmbracoMigrationName) return;
+
             var target70 = new Version(7, 0, 0);
 
             if (e.ConfiguredVersion <= target70)
@@ -28,7 +31,7 @@ namespace Umbraco.Web.Strategies.Migrations
                 // http://issues.umbraco.org/issue/U4-3876
 
                 var sql = @"DELETE FROM cmsContentXml WHERE nodeId IN
-    (SELECT nodeId FROM (SELECT DISTINCT cmsContentXml.nodeId FROM cmsContentXml 
+    (SELECT nodeId FROM (SELECT DISTINCT cmsContentXml.nodeId FROM cmsContentXml
     INNER JOIN umbracoNode ON cmsContentXml.nodeId = umbracoNode.id
     WHERE nodeObjectType = '" + Constants.ObjectTypes.Media + "' AND " + SqlSyntaxContext.SqlSyntaxProvider.GetQuotedColumnName("path") + " LIKE '%-21%') x)";
 
