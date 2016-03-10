479aba8e1d4f50e530cde418999c158d09ac2ebc
Sebastiaan Janssen <sebastiaan@umbraco.com>
2016-03-10 11:47:51 +0100

    Fixes unit test.. in the correct branch this time

--- src/Umbraco.Tests/Plugins/TypeFinderTests.cs
+++ src/Umbraco.Tests/Plugins/TypeFinderTests.cs
@@ -79,8 +79,8 @@ namespace Umbraco.Tests.Plugins
             var originalTypesFound = TypeFinderOriginal.FindClassesOfType<IApplicationStartupHandler>(_assemblies);
 
             Assert.AreEqual(originalTypesFound.Count(), typesFound.Count());
-            Assert.AreEqual(8, typesFound.Count());
-            Assert.AreEqual(8, originalTypesFound.Count());
+            Assert.AreEqual(9, typesFound.Count());
+            Assert.AreEqual(9, originalTypesFound.Count());
         }
 
         [Test]
