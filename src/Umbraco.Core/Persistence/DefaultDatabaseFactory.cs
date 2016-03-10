061e04e7d2bee6f7313fd17e462fc224f81e2307
Stephan <stephane@umbraco.dk>
2016-03-10 19:27:51 +0100

    NPoco - configure database factory (tests fail)

--- src/Umbraco.Core/Persistence/DefaultDatabaseFactory.cs
+++ src/Umbraco.Core/Persistence/DefaultDatabaseFactory.cs
@@ -1,7 +1,10 @@
 using System;
 using System.Web;
+using NPoco;
+using NPoco.FluentMappings;
 using Umbraco.Core.Configuration;
 using Umbraco.Core.Logging;
+using Umbraco.Core.Persistence.Mappers;
 
 namespace Umbraco.Core.Persistence
 {
@@ -17,9 +20,11 @@ namespace Umbraco.Core.Persistence
 	{
 	    private readonly string _connectionStringName;
 	    private readonly ILogger _logger;
+	    private readonly DatabaseFactory _databaseFactory;
+
 	    public string ConnectionString { get; private set; }
         public string ProviderName { get; private set; }
-        
+
         //very important to have ThreadStatic:
         // see: http://issues.umbraco.org/issue/U4-2172
         [ThreadStatic]
@@ -33,11 +38,10 @@ namespace Umbraco.Core.Persistence
 	    /// <param name="connectionStringName">Name of the connection string in web.config</param>
 	    /// <param name="logger"></param>
 	    public DefaultDatabaseFactory(string connectionStringName, ILogger logger)
+            : this(logger)
 		{
-	        if (logger == null) throw new ArgumentNullException("logger");
 	        Mandate.ParameterNotNullOrEmpty(connectionStringName, "connectionStringName");
 			_connectionStringName = connectionStringName;
-	        _logger = logger;
 		}
 
 	    /// <summary>
@@ -47,45 +51,61 @@ namespace Umbraco.Core.Persistence
 	    /// <param name="providerName">Database Provider for the Connection String</param>
 	    /// <param name="logger"></param>
 	    public DefaultDatabaseFactory(string connectionString, string providerName, ILogger logger)
+            : this(logger)
 		{
-	        if (logger == null) throw new ArgumentNullException("logger");
 	        Mandate.ParameterNotNullOrEmpty(connectionString, "connectionString");
 			Mandate.ParameterNotNullOrEmpty(providerName, "providerName");
 			ConnectionString = connectionString;
 			ProviderName = providerName;
-            _logger = logger;
 		}
+
+	    private DefaultDatabaseFactory(ILogger logger)
+	    {
+            if (logger == null) throw new ArgumentNullException("logger");
+            _logger = logger;
+
+            // fixme - is this enough to properly configure NPoco?
+
+            // ensure we have only 1 set of mappers, and 1 PocoDataFactory, for all database
+            // so that everything NPoco is properly cached for the lifetime of the application
+	        var mappers = new MapperCollection { new PocoMapper() };
+	        var pocoDataFactory = new FluentPocoDataFactory((type, iPocoDataFactory) => new PocoDataBuilder(type, mappers).Init());
+            var config = new FluentConfig(xmappers => pocoDataFactory);
+
+            // create the database factory
+            _databaseFactory = DatabaseFactory.Config(x => x
+                .UsingDatabase(CreateDatabaseInstance) // creating UmbracoDatabase instances
+                .WithFluentConfig(config)); // with proper configuration
+        }
+
+	    private UmbracoDatabase CreateDatabaseInstance()
+	    {
+	        return string.IsNullOrEmpty(ConnectionString) == false && string.IsNullOrEmpty(ProviderName) == false
+                ? new UmbracoDatabase(ConnectionString, ProviderName, _logger)
+                : new UmbracoDatabase(_connectionStringName, _logger);
+        }
 
 		public UmbracoDatabase CreateDatabase()
 		{
-			//no http context, create the singleton global object
+			// no http context, create the singleton global object
 			if (HttpContext.Current == null)
 			{
-                if (_nonHttpInstance == null)
-				{
-					lock (Locker)
-					{
-						//double check
-                        if (_nonHttpInstance == null)
-						{
-                            _nonHttpInstance = string.IsNullOrEmpty(ConnectionString) == false && string.IsNullOrEmpty(ProviderName) == false
-                                                  ? new UmbracoDatabase(ConnectionString, ProviderName, _logger)
-                                                  : new UmbracoDatabase(_connectionStringName, _logger);
-						}
-					}
-				}
-                return _nonHttpInstance;
+			    if (_nonHttpInstance != null) // double-check... bah.
+                    return _nonHttpInstance;
+
+                lock (Locker)
+                {
+                    if (_nonHttpInstance == null) // double-check... bah.
+                        _nonHttpInstance = (UmbracoDatabase) _databaseFactory.GetDatabase();
+                }
+
+			    return _nonHttpInstance;
 			}
 
-			//we have an http context, so only create one per request
-			if (HttpContext.Current.Items.Contains(typeof(DefaultDatabaseFactory)) == false)
-			{
-			    HttpContext.Current.Items.Add(typeof (DefaultDatabaseFactory),
-			                                  string.IsNullOrEmpty(ConnectionString) == false && string.IsNullOrEmpty(ProviderName) == false
-                                                  ? new UmbracoDatabase(ConnectionString, ProviderName, _logger)
-                                                  : new UmbracoDatabase(_connectionStringName, _logger));
-			}
-			return (UmbracoDatabase)HttpContext.Current.Items[typeof(DefaultDatabaseFactory)];
+			// we have an http context, so only create one per request
+		    var db = HttpContext.Current.Items[typeof (DefaultDatabaseFactory)] as UmbracoDatabase;
+		    if (db == null) HttpContext.Current.Items[typeof (DefaultDatabaseFactory)] = db = (UmbracoDatabase) _databaseFactory.GetDatabase();
+		    return db;
 		}
 
 		protected override void DisposeResources()
