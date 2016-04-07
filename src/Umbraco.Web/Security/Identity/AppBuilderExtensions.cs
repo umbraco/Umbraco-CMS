506ed9f866deeca3d0c74fdb0adb91e97daea40e
Shannon <sdeminick@gmail.com>
2016-04-07 17:51:09 +0200

    U4-8286 Add OWIN startup events to the UmbracoDefaultOwinStartup class

--- src/Umbraco.Web/Security/Identity/AppBuilderExtensions.cs
+++ src/Umbraco.Web/Security/Identity/AppBuilderExtensions.cs
@@ -25,9 +25,30 @@ namespace Umbraco.Web.Security.Identity
     public static class AppBuilderExtensions
     {
         /// <summary>
+        /// Called at the end of configuring middleware
+        /// </summary>
+        /// <param name="app"></param>
+        /// <remarks>
+        /// This could be used for something else in the future - maybe to inform Umbraco that middleware is done/ready, but for
+        /// now this is used to raise the custom event
+        /// 
+        /// This is an extension method in case developer entirely replace the UmbracoDefaultOwinStartup class, in which case they will
+        /// need to ensure they call this extension method in their startup class.
+        /// 
+        /// TODO: Move this method in v8, it doesn't belong in this namespace/extension class
+        /// </remarks>
+        public static void FinalizeMiddlewareConfiguration(this IAppBuilder app)
+        {
+            UmbracoDefaultOwinStartup.OnMiddlewareConfigured(new OwinMiddlewareConfiguredEventArgs(app));
+        }
+
+        /// <summary>
         /// Sets the OWIN logger to use Umbraco's logging system
         /// </summary>
         /// <param name="app"></param>
+        /// <remarks>
+        /// TODO: Move this method in v8, it doesn't belong in this namespace/extension class
+        /// </remarks>
         public static void SetUmbracoLoggerFactory(this IAppBuilder app)
         {
             app.SetLoggerFactory(new OwinLoggerFactory());
