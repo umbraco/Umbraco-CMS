f87bec398d1fa3d58d200d9b866458bd49badf7b
Shannon <sdeminick@gmail.com>
2016-04-07 16:44:45 +0200

    U4-8239 DisableBrowserCacheAttribute causing YSOD when a previous exceptions is thrown before the filter kicks in

--- src/Umbraco.Web/WebApi/Filters/DisableBrowserCacheAttribute.cs
+++ src/Umbraco.Web/WebApi/Filters/DisableBrowserCacheAttribute.cs
@@ -16,7 +16,11 @@ namespace Umbraco.Web.WebApi.Filters
             base.OnActionExecuted(actionExecutedContext);
 
             // happens if exception
-            if (actionExecutedContext.Response == null) return;
+            if (actionExecutedContext == null || actionExecutedContext.Response == null ||
+                    actionExecutedContext.Response.Headers == null)
+            {
+                return;
+            }
 
             //NOTE: Until we upgraded to WebApi 2, this didn't work correctly and we had to revert to using
             // HttpContext.Current responses. I've changed this back to what it should be now since it works
