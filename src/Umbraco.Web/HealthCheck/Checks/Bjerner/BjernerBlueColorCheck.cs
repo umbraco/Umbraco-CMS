b71ed833c1320bdc3ab9a3c7e908c52be030a921
Anders Bjerner <abjerner@skybrud.dk>
2016-04-22 00:37:15 +0200

    HealthCheck: Two custom checks added for testing purposes
    One check wants *THE* color to be green, while the second check wants *THE* color to be blue. Rectify one, and the a new check for the other will fail :D
    
    The two checks are just for testing purposes, so they should obviously be removed before release. But for now they serve at an example that rectifying (executing an action) actually works.

---
+++ src/Umbraco.Web/HealthCheck/Checks/Bjerner/BjernerBlueColorCheck.cs
@@ -0,0 +1,50 @@
+﻿using Umbraco.Web.HealthCheck.Checks.Config;
+
+namespace Umbraco.Web.HealthCheck.Checks.Bjerner {
+    
+    [HealthCheck("BC850B85-7AD2-4280-98DB-FE5C3D724598", "Blue",
+        Description = "The color should obviously be blue.",
+        Group = "Bjerner")]
+    public class BjernerBlueColorCheck : AbstractConfigCheck {
+
+        #region Properties
+
+        public override string FilePath {
+            get { return "~/Bjerner.config"; }
+        }
+
+        public override string XPath {
+            get { return "/root/hest/color/@value"; }
+        }
+
+        public override ValueComparisonType ValueComparisonType {
+            get { return ValueComparisonType.ShouldEqual; }
+        }
+
+        public override string Value {
+            get { return "Blue"; }
+        }
+
+        public override string CheckSuccessMessage {
+            get { return "Yay! The color is {2}."; }
+        }
+
+        public override string CheckErrorMessage {
+            get { return "Nay. The color is {3}. It should totally be {2}."; }
+        }
+
+        public override string RectifySuccessMessage {
+            get { return "Yay! The color is now {2}."; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public BjernerBlueColorCheck(HealthCheckContext healthCheckContext) : base(healthCheckContext) { }
+
+        #endregion
+
+    }
+
+}
---
+++ src/Umbraco.Web/HealthCheck/Checks/Bjerner/BjernerGreenColorCheck.cs
@@ -0,0 +1,50 @@
+﻿using Umbraco.Web.HealthCheck.Checks.Config;
+
+namespace Umbraco.Web.HealthCheck.Checks.Bjerner {
+    
+    [HealthCheck("2168F7C0-7CB5-4334-B398-086B41F27607", "Green",
+        Description = "The color should obviously be green.",
+        Group = "Bjerner")]
+    public class BjernerGreenColorCheck : AbstractConfigCheck {
+
+        #region Properties
+
+        public override string FilePath {
+            get { return "~/Bjerner.config"; }
+        }
+
+        public override string XPath {
+            get { return "/root/hest/color/@value"; }
+        }
+
+        public override ValueComparisonType ValueComparisonType {
+            get { return ValueComparisonType.ShouldEqual; }
+        }
+
+        public override string Value {
+            get { return "Green"; }
+        }
+
+        public override string CheckSuccessMessage {
+            get { return "Yay! The color is {2}."; }
+        }
+
+        public override string CheckErrorMessage {
+            get { return "Nay. The color is {3}. It should totally be {2}."; }
+        }
+
+        public override string RectifySuccessMessage {
+            get { return "Yay! The color is now {2}."; }
+        }
+
+        #endregion
+
+        #region Constructors
+
+        public BjernerGreenColorCheck(HealthCheckContext healthCheckContext) : base(healthCheckContext) { }
+
+        #endregion
+
+    }
+
+}
