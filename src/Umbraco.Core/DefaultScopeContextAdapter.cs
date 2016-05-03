a2bc99fabd11f62ecf96ff153958eb58d28853fc
Shannon <sdeminick@gmail.com>
2016-05-03 22:33:29 +0200

    Removes AmbientContext

--- src/Umbraco.Core/DefaultScopeContextAdapter.cs
+++ src/Umbraco.Core/DefaultScopeContextAdapter.cs
@@ -5,6 +5,20 @@ namespace Umbraco.Core
 {
     internal class DefaultScopeContextAdapter : IScopeContextAdapter
     {
+        
+        // some reading:
+        // fixme: do we need to keep these links?
+        // https://github.com/seesharper/LightInject/issues/170
+        // http://stackoverflow.com/questions/22924048/is-it-possible-to-detect-if-you-are-on-the-synchronous-side-of-an-async-method
+        // http://stackoverflow.com/questions/29917671/lightinject-with-web-api-how-can-i-get-the-httprequestmessage
+        // http://stackoverflow.com/questions/18459916/httpcontext-current-items-after-an-async-operation
+        // http://stackoverflow.com/questions/12029091/httpcontext-is-null-after-await-task-factory-fromasyncbeginxxx-endxxx
+        //
+        // also note
+        // CallContext stuff will flow downwards in async but not upwards ie an async call will receive the values
+        // but any changes it makes will *not* modify the caller's CallContext, so we should be careful when using
+        // this for caches and stuff
+
         // fixme - should we use the LogicalCallContext here?
 
         public object Get(string key)
