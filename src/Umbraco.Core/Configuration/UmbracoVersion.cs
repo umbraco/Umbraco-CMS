4891bbf9a431c2fce8a765b52e8435c723142ca9
Stephan <stephane@umbraco.dk>
2016-05-12 14:18:34 +0200

    Update UmbracoVersion

--- src/Umbraco.Core/Configuration/UmbracoVersion.cs
+++ src/Umbraco.Core/Configuration/UmbracoVersion.cs
@@ -6,7 +6,9 @@ namespace Umbraco.Core.Configuration
 {
     public class UmbracoVersion
     {
-        private static readonly Version Version = new Version("8.0.0");
+        // BEWARE!
+        // This class is parsed and updated by the build scripts.
+        // Do NOT modify it unless you understand what you are doing.
 
         /// <summary>
         /// Gets the current version of Umbraco.
@@ -15,27 +17,24 @@ namespace Umbraco.Core.Configuration
         /// <remarks>
         /// CURRENT UMBRACO VERSION ID.
         /// </remarks>
-        public static Version Current
-        {
-            get { return Version; }
-        }
+        public static Version Current { get; } = new Version("8.0.0");
 
         /// <summary>
         /// Gets the version comment (like beta or RC).
         /// </summary>
         /// <value>The version comment.</value>
-        public static string CurrentComment { get { return "beta"; } }
+        public static string CurrentComment => "alpha0001";
 
         // Get the version of the Umbraco.Core.dll by looking at a class in that dll
         // Had to do it like this due to medium trust issues, see: http://haacked.com/archive/2010/11/04/assembly-location-and-medium-trust.aspx
-        public static string AssemblyVersion { get { return new AssemblyName(typeof(UmbracoVersion).Assembly.FullName).Version.ToString(); } }
+        public static string AssemblyVersion => new AssemblyName(typeof(UmbracoVersion).Assembly.FullName).Version.ToString();
 
         public static SemVersion GetSemanticVersion()
         {
             return new SemVersion(
-                Current.Major, 
+                Current.Major,
                 Current.Minor,
-                Current.Build, 
+                Current.Build,
                 CurrentComment.IsNullOrWhiteSpace() ? null : CurrentComment,
                 Current.Revision > 0 ? Current.Revision.ToInvariantString() : null);
         }
