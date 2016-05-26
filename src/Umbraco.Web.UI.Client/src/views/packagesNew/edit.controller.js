f876b6b7280cdc41507d54111fac8253264752dd
Simon Busborg <simon@umbraco.com>
2016-05-26 13:39:01 +0200

    added umb-packages.less, added repeater for packages + minimal styling

--- src/Umbraco.Web.UI.Client/src/views/packagesNew/edit.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/packagesNew/edit.controller.js
@@ -7,7 +7,38 @@
 
         vm.page = {};
         vm.page.name = "Packages";
-        
+
+        vm.packages = [
+            {
+                "name": "Test App",
+                "description": "lorem flaxis slk asjasd ks",
+                "karma": "1",
+                "downloads": "2",
+                "icon": "flax"
+            },
+            {
+                "name": "Tessti flaxi",
+                "description": "loremlaksjd lkajs dasjasd ks",
+                "karma": "10",
+                "downloads": "22",
+                "icon": "flaxo"
+            },
+            {
+                "name": "Walla",
+                "description": "lorem flaxis slk asjasd ks",
+                "karma": "1",
+                "downloads": "2",
+                "icon": "flax"
+            },
+            {
+                "name": "Walla",
+                "description": "lorem flaxis slk asjasd ks",
+                "karma": "1",
+                "downloads": "2",
+                "icon": "flax"
+            }
+        ];
+
 
     }
 
