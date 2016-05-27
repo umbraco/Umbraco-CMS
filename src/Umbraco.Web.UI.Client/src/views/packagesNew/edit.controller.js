db21aef15091f7b7a98a2ec609e95d1e8d88ac9b
Simon Busborg <simon@umbraco.com>
2016-05-27 11:59:19 +0200

    added icons and versions

--- src/Umbraco.Web.UI.Client/src/views/packagesNew/edit.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/packagesNew/edit.controller.js
@@ -41,42 +41,48 @@
                 "description": "An HTML5 audio player based on jPlayer",
                 "karma": "1",
                 "downloads": "1672",
-                "icon":"https://our.umbraco.org/media/wiki/150283/635768313097111400_usightlylogopng.png?bgcolor=fff&height=154&width=281&format=png"
+                "icon":"https://our.umbraco.org/media/wiki/150283/635768313097111400_usightlylogopng.png?bgcolor=fff&height=154&width=281&format=png",
+                "version":"No good"
             },
             {
                 "name": "Kill IE6",
                 "description": "A simple port of the IE6 warning script (http://code.google.com/p/ie6-upgrade-warning/) to use in your Umbraco websites.",
                 "karma": "11",
                 "downloads": "688",
-                "icon":"https://our.umbraco.org/media/wiki/9138/634697622367666000_offroadcode-100x100.png?bgcolor=fff&height=154&width=281&format=png"
+                "icon":"https://our.umbraco.org/media/wiki/9138/634697622367666000_offroadcode-100x100.png?bgcolor=fff&height=154&width=281&format=png",
+                "version":"Fits perfectly"
             },
             {
                 "name": "Examine Media Indexer",
                 "description": "CogUmbracoExamineMediaIndexer",
                 "karma": "3",
                 "downloads": "1329",
-                "icon":"https://our.umbraco.org/media/wiki/50703/634782902373558000_cogworks.jpg?bgcolor=fff&height=154&width=281&format=png"
+                "icon":"https://our.umbraco.org/media/wiki/50703/634782902373558000_cogworks.jpg?bgcolor=fff&height=154&width=281&format=png",
+                "version":"Fits perfectly"
             },
             {
                 "name": "SVG Icon Picker",
                 "description": "A picker, for picking icons from an SVG spritesheet.",
                 "karma": "5",
                 "downloads": "8",
-                "icon":"https://our.umbraco.org/media/wiki/154472/635997115126742822_logopng.png?bgcolor=fff&height=154&width=281&format=png"
+                "icon":"https://our.umbraco.org/media/wiki/154472/635997115126742822_logopng.png?bgcolor=fff&height=154&width=281&format=png",
+                "version":"Fits perfectly"
             },
             {
                 "name": "Pipeline CRM",
                 "description": "Pipeline is a social CRM that lives in Umbraco back-office. It tracks opportunities and helps teams collaborate with timelines and tasks. It stores information about your customers and your interactions with them. It integrates with your website, capturing opportunities from forms and powering personal portals.",
                 "karma": "3",
                 "downloads": "105",
-                "icon":"https://our.umbraco.org/media/wiki/152476/635917291068518788_pipeline-crm-logopng.png?bgcolor=fff&height=154&width=281&format=png"
+                "icon":"https://our.umbraco.org/media/wiki/152476/635917291068518788_pipeline-crm-logopng.png?bgcolor=fff&height=154&width=281&format=png",
+                "version":"Fits perfectly"
             },
             {
                 "name": "CodeMirror",
                 "description": "CodeMirror Editor for Umbraco",
                 "karma": "1",
                 "downloads": "70",
-                "icon":"https://our.umbraco.org/media/wiki/151028/635810233171153461_logopng.png?bgcolor=fff&height=154&width=281&format=png"
+                "icon":"https://our.umbraco.org/media/wiki/151028/635810233171153461_logopng.png?bgcolor=fff&height=154&width=281&format=png",
+                "version":"No good"
             }
         ];
 
