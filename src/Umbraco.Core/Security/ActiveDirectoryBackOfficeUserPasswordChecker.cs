b168923d5f8880c64e0b08663ccd64fcc28b6a98
Darren Ferguson <df@darren-ferguson.com>
2016-05-29 15:46:42 +0100

    http://issues.umbraco.org/issue/U4-7032 : Include a password checker for Active Directory - as ability to authenticate via AD was removed in 7.3

---
+++ src/Umbraco.Core/Security/ActiveDirectoryBackOfficeUserPasswordChecker.cs
@@ -0,0 +1,25 @@
+﻿using System.Configuration;
+using System.DirectoryServices.AccountManagement;
+using System.Threading.Tasks;
+using Umbraco.Core.Models.Identity;
+
+namespace Umbraco.Core.Security
+{
+    public class ActiveDirectoryBackOfficeUserPasswordChecker : IBackOfficeUserPasswordChecker
+    {
+        public Task<BackOfficeUserPasswordCheckerResult> CheckPasswordAsync(BackOfficeIdentityUser user, string password)
+        {
+            bool isValid;
+            using (var pc = new PrincipalContext(ContextType.Domain, ConfigurationManager.AppSettings["ActiveDirectoryDomain"]))
+            {
+                isValid = pc.ValidateCredentials(user.UserName, password);
+            }
+
+            var result = isValid
+                ? BackOfficeUserPasswordCheckerResult.ValidCredentials
+                : BackOfficeUserPasswordCheckerResult.InvalidCredentials;
+
+            return Task.FromResult(result);
+        }
+    }
+}
