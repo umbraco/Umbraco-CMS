c66de2298b9177712876f82cff4d4ea20fb58314
Simon Busborg <simon@umbraco.com>
2016-05-31 16:18:41 +0200

    Added local installer

--- src/Umbraco.Web.UI.Client/src/views/packagesNew/views/install-local.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/packagesNew/views/install-local.controller.js
@@ -1,7 +1,31 @@
 (function () {
     "use strict";
 
+    function PackagesInstallLocalController($scope, $route, $location) {
 
-    angular.module("umbraco").controller("Umbraco.Editors.Packages.InstallLocalController");
+        var vm = this;
+
+        vm.state = "upload";
+        vm.localPackage = {
+            "packageName": "SvgIconPicker Version: 0.1.0",
+            "packageAuthor": "Søren Kottal",
+            "packageAuthorLink": "https://github.com/skttl/",
+            "packageInfo": "https://github.com/skttl/Umbraco.SvgIconPicker",
+            "packageLicens": "GPLv3",
+            "packageLicensLink": "http://www.gnu.org/licenses/quick-guide-gplv3.en.html",
+            "packageLicensAccept": false,
+            "packageReadme": false,
+            "filePath": "",
+            "riskAccept": false
+        };
+
+        vm.loadPackage = loadPackage;
+
+        function loadPackage(){
+            vm.state = "packageDetails";
+        }
+    }
+
+    angular.module("umbraco").controller("Umbraco.Editors.Packages.InstallLocalController", PackagesInstallLocalController);
 
 })();
