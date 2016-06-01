2ce2a03072e49607739800c2963999a6f8a2fd12
Simon Busborg <simon@umbraco.com>
2016-06-01 13:03:14 +0200

    Local installer

--- src/Umbraco.Web.UI.Client/src/views/packagesNew/views/install-local.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/packagesNew/views/install-local.controller.js
@@ -4,17 +4,18 @@
     function PackagesInstallLocalController($scope, $route, $location) {
 
         var vm = this;
-
         vm.state = "upload";
+
         vm.localPackage = {
-            "packageName": "SvgIconPicker Version: 0.1.0",
-            "packageAuthor": "Søren Kottal",
-            "packageAuthorLink": "https://github.com/skttl/",
-            "packageInfo": "https://github.com/skttl/Umbraco.SvgIconPicker",
-            "packageLicens": "GPLv3",
-            "packageLicensLink": "http://www.gnu.org/licenses/quick-guide-gplv3.en.html",
-            "packageLicensAccept": false,
-            "packageReadme": false,
+            "icon":"https://our.umbraco.org/media/wiki/154472/635997115126742822_logopng.png?bgcolor=fff&height=154&width=281&format=png",
+            "name": "SvgIconPicker Version: 0.1.0",
+            "author": "Søren Kottal",
+            "authorLink": "https://github.com/skttl/",
+            "info": "https://github.com/skttl/Umbraco.SvgIconPicker",
+            "licens": "GPLv3",
+            "licensLink": "http://www.gnu.org/licenses/quick-guide-gplv3.en.html",
+            "licensAccept": false,
+            "readme": "Color Palettes is a simple property editor that let you define different color palettes (or get them from Adobe Kuler or COLOURlovers) and present them to the editor as a list of radio buttons.",
             "filePath": "",
             "riskAccept": false
         };
