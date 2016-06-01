3e05c62ee3aa12bbb2dc7d2a924dc9a7ccfd70a5
Mads Rasmussen <mads@umbraco.com>
2016-06-01 15:38:15 +0200

    rewrite controller to follow angular style guide

--- src/Umbraco.Web.UI.Client/src/views/dashboard/developer/healthcheck.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/dashboard/developer/healthcheck.controller.js
@@ -1,40 +1,51 @@
-function healthCheckController($scope, healthCheckService) {
-	
-	$scope.groups = [];
-	
-	// Get a (grouped) list of all health checks
-	healthCheckService.getAllChecks().then(
-		function(response) {
-			$scope.groups = response;	
+(function () {
+    "use strict";
+
+    function HealthCheckController($scope, healthCheckService) {
+
+        var vm = this;
+
+		vm.groups = [];
+
+		vm.getStatus = getStatus;
+		vm.executeAction = executeAction;
+		vm.checkAllInGroup = checkAllInGroup;
+
+		// Get a (grouped) list of all health checks
+		healthCheckService.getAllChecks().then(
+			function(response) {
+				vm.groups = response;
+			}
+		);
+
+		// Get the status of an individual check
+		function getStatus(check) {
+			check.loading = true;
+			check.status = null;
+			healthCheckService.getStatus(check.id).then(function(response) {
+				check.loading = false;
+				check.status = response;
+			});
 		}
-	);
 
-	// Get the status of an individual check
-	$scope.getStatus = function(check) {
-		check.loading = true;
-		check.status = null;
-		healthCheckService.getStatus(check.id).then(function(response) {
-			check.loading = false;
-			check.status = response;
-		});
-	};
+		function executeAction(check, status, action) {
+			check.status = null;
+			check.rectify = "Loading result of '" + action.name + "'...";
+			healthCheckService.executeAction(action).then(function (response) {
+				check.rectify = response.message;
+			});
+		}
 
-	$scope.executeAction = function (check, status, action) {
-	    check.status = null;
-	    check.rectify = "Loading result of '" + action.name + "'...";
-	    healthCheckService.executeAction(action).then(function (response) {
-	        check.rectify = response.message;
-	    });
-	};
+		function checkAllInGroup(checks) {
+			angular.forEach(checks, function(check) {
+				healthCheckService.getStatus(check.id).then(function(response) {
+					check.loading = false;
+					check.status = response;
+				});
+			});
+		}
 
-	$scope.checkAllInGroup = function(checks) {
-	    angular.forEach(checks, function(check) {
-	        healthCheckService.getStatus(check.id).then(function(response) {
-	            check.loading = false;
-	            check.status = response;
-	        });
-	    });
-	};
+    }
 
-}
-angular.module("umbraco").controller("Umbraco.Dashboard.HealthCheckController", healthCheckController);
+    angular.module("umbraco").controller("Umbraco.Dashboard.HealthCheckController", HealthCheckController);
+})();
