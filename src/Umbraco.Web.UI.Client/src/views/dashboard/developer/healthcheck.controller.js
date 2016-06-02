87ae6261f09da696f52f3feafa8fd1c15a514090
Mads Rasmussen <mads@umbraco.com>
2016-06-02 11:37:20 +0200

    set global check result on groups

--- src/Umbraco.Web.UI.Client/src/views/dashboard/developer/healthcheck.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/dashboard/developer/healthcheck.controller.js
@@ -14,9 +14,68 @@
 		// Get a (grouped) list of all health checks
 		healthCheckService.getAllChecks().then(
 			function(response) {
+
+                // set number of checks which has been executed
+                for(var i = 0; i < response.length; i++) {
+                    var group = response[i];
+                    group.checkCounter = 0;
+                }
+
 				vm.groups = response;
 			}
 		);
+
+        function setGroupGlobalResultType(group) {
+
+            var totalSucces = 0;
+            var totalError = 0;
+            var totalWarning = 0;
+            var totalInfo = 0;
+
+            // count total number of statusses
+            angular.forEach(group.checks, function(check){
+                angular.forEach(check.status, function(status){
+                    switch(status.resultType) {
+                        case 0:
+                            totalSucces = totalSucces + 1;
+                            break;
+                        case 1:
+                            totalWarning = totalWarning + 1;
+                            break;
+                        case 2:
+                            totalError = totalError + 1;
+                            break;
+                        case 3:
+                            totalInfo = totalInfo + 1;
+                            break;
+                    }
+                });
+            });
+
+            // set global group result
+            if(totalError > 0) {
+
+                // set group to error
+                group.resultType = 2;
+
+            } else if ( totalWarning > 0 ) {
+
+                // set group to warning
+                group.resultType = 1;
+
+            } else if ( totalInfo > 0 ) {
+
+                // set group to info
+                group.resultType = 3;
+
+            } else if (totalSucces > 0) {
+
+                // set group to success
+                group.resultType = 0;
+
+            }
+
+        }
 
 		// Get the status of an individual check
 		function getStatus(check) {
@@ -36,13 +95,24 @@
 			});
 		}
 
-		function checkAllInGroup(checks) {
+		function checkAllInGroup(group, checks) {
+
+            group.checkCounter = 0;
+
 			angular.forEach(checks, function(check) {
 				healthCheckService.getStatus(check.id).then(function(response) {
-					check.loading = false;
 					check.status = response;
+                    group.checkCounter = group.checkCounter + 1;
+                    check.loading = false;
+
+                    // when all checks are done, set global group result
+                    if(group.checkCounter === checks.length) {
+                        setGroupGlobalResultType(group);
+                    }
+
 				});
 			});
+
 		}
 
     }
