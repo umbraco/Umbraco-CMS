b1a4bee7e925fdfae8b58a3c7f530acd429574bb
Kris Janssen <kris.janssen@gmail.com>
2016-07-06 02:18:12 +0200

    U4-8552 Cannot apply style to an a tag in RTE
    In the grid RTE (and normal RTE for that matter), I expect to be able to use a CSS style like so:
    
    'p.alert.alert-border.aler-info'
    
    to produce:
    
    '<p class="alert alert-border alert-info"> </p>'
    
    Instead of having <span class="alert alert-border alert-info"> ... </span> tags inserted.
    
    With these changes, this becomes possible.
    
    Note that:
    
    'r.classes = rule.selector.substring(rule.selector.indexOf(".") + 1).replace(".", " ");'
    
    would not work since 'replace()' only replaces the first instance of "."
    
    These changes are related to this [Umbraco Issue Tracker entry](http://issues.umbraco.org/issue/U4-8552).

--- src/Umbraco.Web.UI.Client/src/common/directives/components/grid/grid.rte.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/grid/grid.rte.directive.js
@@ -64,20 +64,30 @@ angular.module("umbraco.directives")
                                     await.push(stylesheetResource.getRulesByName(stylesheet).then(function (rules) {
                                         angular.forEach(rules, function (rule) {
                                           var r = {};
-                                          r.title = rule.name;
-                                          if (rule.selector[0] === ".") {
-                                              r.inline = "span";
-                                              r.classes = rule.selector.substring(1);
-                                          }else if (rule.selector[0] === "#") {
-                                              //Even though this will render in the style drop down, it will not actually be applied
-                                              // to the elements, don't think TinyMCE even supports this and it doesn't really make much sense
-                                              // since only one element can have one id.
-                                              r.inline = "span";
-                                              r.attributes = { id: rule.selector.substring(1) };
-                                          }else {
-                                              r.block = rule.selector;
-                                          }
-                                          styleFormats.push(r);
+                                            r.title = rule.name;
+                                            if (rule.selector[0] == ".") {
+                                                r.inline = "span";
+                                                r.classes = rule.selector.substring(1);
+                                            }
+                                            else if (rule.selector[0] == "#") {
+                                                r.inline = "span";
+                                                r.attributes = { id: rule.selector.substring(1) };
+                                            }
+                                            else if (rule.selector[0] != "." && rule.selector.indexOf(".") > -1) {
+                                                var split = rule.selector.split(".");
+                                                r.block = split[0];
+                                                r.classes = rule.selector.substring(rule.selector.indexOf(".") + 1).split(".").join(" ");
+                                            }
+                                            else if (rule.selector[0] != "#" && rule.selector.indexOf("#") > -1) {
+                                                var split = rule.selector.split("#");
+                                                r.block = split[0];
+                                                r.classes = rule.selector.substring(rule.selector.indexOf("#") + 1).split(".").join(" ");
+                                            }
+                                            else {
+                                                r.block = rule.selector;
+                                            }
+                                            
+                                            styleFormats.push(r);
                                         });
                                     }));
                             });
