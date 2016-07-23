331d042a072279f98c41582609309508c1f7588c
bjarnef <bjarne_fyrstenborg@hotmail.com>
2016-07-23 02:34:57 +0200

    Remove output to console

--- src/Umbraco.Web.UI.Client/src/common/filters/stripStyles.filter.js
+++ src/Umbraco.Web.UI.Client/src/common/filters/stripStyles.filter.js
@@ -1,6 +1,5 @@
 angular.module("umbraco.filters").filter('stripStyles', function () {
     return function (str) {
-        console.log("str", str);
         return str ? str.replace(/style=['"]([^"]*)["']/gi, '') : '';
     };
   });
