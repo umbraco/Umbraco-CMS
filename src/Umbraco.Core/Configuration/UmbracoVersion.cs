3bee1d3bceaf1eb3d6cff5c23609bf2fb396be8f
Stephan <stephane@umbraco.dk>
2016-09-19 12:25:14 +0200

    Version 8.0.0-alpha0020

--- src/Umbraco.Core/Configuration/UmbracoVersion.cs
+++ src/Umbraco.Core/Configuration/UmbracoVersion.cs
@@ -21,7 +21,7 @@ namespace Umbraco.Core.Configuration
         /// <summary>
         /// Gets the version comment of the executing code (eg "beta").
         /// </summary>
-        public static string CurrentComment => "alpha0017";
+        public static string CurrentComment => "alpha0020";
 
         /// <summary>
         /// Gets the assembly version of Umbraco.Code.dll.
