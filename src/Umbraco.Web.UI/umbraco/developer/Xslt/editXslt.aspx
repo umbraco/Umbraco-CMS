umbraco/Umbraco-CMS

commit 2eb050e6632146bfb84a2e3ec66a9eec20432372

removing a checkbox that didnt do anything at all.

src/Umbraco.Web.UI/umbraco/developer/Xslt/editXslt.aspx
@@ -61,9 +61,6 @@
                  <cc1:PropertyPanel ID="pp_filename" runat="server" Text="Filename">
                     <asp:TextBox ID="xsltFileName" runat="server" CssClass="guiInputText"></asp:TextBox>
                 </cc1:PropertyPanel>
-                <cc1:PropertyPanel ID="pp_testing" runat="server" Text="Skip testing (ignore errors)">
-                    <asp:CheckBox ID="SkipTesting" runat="server"></asp:CheckBox>
-                </cc1:PropertyPanel>
             </cc1:Pane>
     </cc1:TabView>
 </asp:Content>

src/Umbraco.Web/umbraco.presentation/umbraco/developer/Xslt/editXslt.aspx.cs
@@ -178,24 +178,6 @@ namespace umbraco.cms.presentation.developer
 		protected global::System.Web.UI.WebControls.TextBox xsltFileName;
 
 		/// <summary>
-		/// pp_testing control.
-		/// </summary>
-		/// <remarks>
-		/// Auto-generated field.
-		/// To modify move field declaration from designer file to code-behind file.
-		/// </remarks>
-		protected global::umbraco.uicontrols.PropertyPanel pp_testing;
-
-		/// <summary>
-		/// SkipTesting control.
-		/// </summary>
-		/// <remarks>
-		/// Auto-generated field.
-		/// To modify move field declaration from designer file to code-behind file.
-		/// </remarks>
-		protected global::System.Web.UI.WebControls.CheckBox SkipTesting;
-
-		/// <summary>
 		/// pp_errorMsg control.
 		/// </summary>
 		/// <remarks>

commit ac42c8ae26162a46fcd5a6727bd356a4669fe966

Fix 2eb050e, broke Xslt edit

src/Umbraco.Web.UI/umbraco/developer/Xslt/editXslt.aspx
@@ -9,7 +9,7 @@
 </asp:Content>
 
 <asp:Content ContentPlaceHolderID="head" runat="server" ID="cp2">
-    
+
     <umb:JsInclude ID="JsInclude2" runat="server" FilePath="Editors/EditXslt.js" PathNameAlias="UmbracoClient" />
     <umb:CssInclude ID="CssInclude1" runat="server" FilePath="Editors/EditXslt.css" PathNameAlias="UmbracoClient" />
 
@@ -21,8 +21,7 @@
                     nameTxtBox: $('#<%= xsltFileName.ClientID %>'),
                     originalFileName: '<%= xsltFileName.Text %>',
                     saveButton: $("#<%= ((Control)SaveButton).ClientID %>"),
-                    editorSourceElement: $('#<%= editorSource.ClientID %>'),
-                    skipTestingCheckBox: $("#<%= SkipTesting.ClientID %>"),
+                    editorSourceElement: $('#<%= editorSource.ClientID %>')
                 });
                 editor.init();
 
@@ -32,7 +31,7 @@
         })(jQuery);
 
         //TODO: Move these to EditXslt.js one day
-        var xsltSnippet = "";        
+        var xsltSnippet = "";
         function xsltVisualize() {
 
             xsltSnippet = UmbEditor.IsSimpleEditor
@@ -46,7 +45,7 @@
             }
 
             UmbClientMgr.openModalWindow('<%= Umbraco.Core.IO.IOHelper.ResolveUrl(Umbraco.Core.IO.SystemDirectories.Umbraco) %>/developer/xslt/xsltVisualize.aspx', 'Visualize XSLT', true, 550, 650);
-        } 
+        }
     </script>
     <umb:JsInclude ID="JsInclude1" runat="server" FilePath="Application/jQuery/jquery-fieldselection.js"
         PathNameAlias="UmbracoClient" />
@@ -56,7 +55,7 @@
             <cc1:Pane runat="server" ID="pane1">
                 <cc1:CodeArea ID="editorSource" CodeBase="XML" runat="server" AutoResize="false"  />
             </cc1:Pane>
-            
+
             <cc1:Pane runat="server" ID="pane2">
                  <cc1:PropertyPanel ID="pp_filename" runat="server" Text="Filename">
                     <asp:TextBox ID="xsltFileName" runat="server" CssClass="guiInputText"></asp:TextBox>