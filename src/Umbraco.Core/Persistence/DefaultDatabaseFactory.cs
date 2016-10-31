59ace3d8814e74ece37de3d207b039edd608ee02
Stephan <stephane@umbraco.dk>
2016-10-31 11:28:28 +0100

    Backport SafeCallContext, DefaultDatabaseFactory fixes from 7.6

--- src/Umbraco.Core/Persistence/DefaultDatabaseFactory.cs
+++ src/Umbraco.Core/Persistence/DefaultDatabaseFactory.cs
@@ -1,6 +1,6 @@
 using System;
+using System.Runtime.Remoting.Messaging;
 using System.Web;
-using Umbraco.Core.Configuration;
 using Umbraco.Core.Logging;
 
 namespace Umbraco.Core.Persistence
@@ -19,13 +19,24 @@ namespace Umbraco.Core.Persistence
 	    private readonly ILogger _logger;
 	    public string ConnectionString { get; private set; }
         public string ProviderName { get; private set; }
-        
-        //very important to have ThreadStatic:
-        // see: http://issues.umbraco.org/issue/U4-2172
-        [ThreadStatic]
-        private static volatile UmbracoDatabase _nonHttpInstance;
 
-		private static readonly object Locker = new object();
+        // NO! see notes in v8 HybridAccessorBase
+        //[ThreadStatic]
+        //private static volatile UmbracoDatabase _nonHttpInstance;
+
+        private const string ItemKey = "Umbraco.Core.Persistence.DefaultDatabaseFactory";
+
+        private static UmbracoDatabase NonContextValue
+        {
+            get { return (UmbracoDatabase) CallContext.LogicalGetData(ItemKey); }
+            set
+            {
+                if (value == null) CallContext.FreeNamedDataSlot(ItemKey);
+                else CallContext.LogicalSetData(ItemKey, value);
+            }
+        }
+
+        private static readonly object Locker = new object();
 
 	    /// <summary>
 	    /// Constructor accepting custom connection string
@@ -36,7 +47,10 @@ namespace Umbraco.Core.Persistence
 		{
 	        if (logger == null) throw new ArgumentNullException("logger");
 	        Mandate.ParameterNotNullOrEmpty(connectionStringName, "connectionStringName");
-			_connectionStringName = connectionStringName;
+
+            //if (NonContextValue != null) throw new Exception("NonContextValue is not null.");
+
+            _connectionStringName = connectionStringName;
 	        _logger = logger;
 		}
 
@@ -51,65 +65,133 @@ namespace Umbraco.Core.Persistence
 	        if (logger == null) throw new ArgumentNullException("logger");
 	        Mandate.ParameterNotNullOrEmpty(connectionString, "connectionString");
 			Mandate.ParameterNotNullOrEmpty(providerName, "providerName");
-			ConnectionString = connectionString;
+
+            //if (NonContextValue != null) throw new Exception("NonContextValue is not null.");
+
+            ConnectionString = connectionString;
 			ProviderName = providerName;
             _logger = logger;
 		}
 
 		public UmbracoDatabase CreateDatabase()
 		{
-			//no http context, create the singleton global object
-			if (HttpContext.Current == null)
-			{
-                if (_nonHttpInstance == null)
-				{
-					lock (Locker)
-					{
-						//double check
-                        if (_nonHttpInstance == null)
-						{
-                            _nonHttpInstance = string.IsNullOrEmpty(ConnectionString) == false && string.IsNullOrEmpty(ProviderName) == false
-                                                  ? new UmbracoDatabase(ConnectionString, ProviderName, _logger)
-                                                  : new UmbracoDatabase(_connectionStringName, _logger);
-						}
-					}
-				}
-                return _nonHttpInstance;
-			}
+            // no http context, create the call context object
+            // NOTHING is going to track the object and it is the responsibility of the caller to release it!
+            // using the ReleaseDatabase method.
+            if (HttpContext.Current == null)
+            {
+                LogHelper.Debug<DefaultDatabaseFactory>("Get NON http [T" + Environment.CurrentManagedThreadId + "]");
+                var value = NonContextValue;
+                if (value != null) return value;
+                lock (Locker)
+                {
+                    value = NonContextValue;
+                    if (value != null) return value;
 
-			//we have an http context, so only create one per request
-			if (HttpContext.Current.Items.Contains(typeof(DefaultDatabaseFactory)) == false)
-			{
-			    HttpContext.Current.Items.Add(typeof (DefaultDatabaseFactory),
-			                                  string.IsNullOrEmpty(ConnectionString) == false && string.IsNullOrEmpty(ProviderName) == false
+                    LogHelper.Debug<DefaultDatabaseFactory>("Create NON http [T" + Environment.CurrentManagedThreadId + "]");
+                    NonContextValue = value = string.IsNullOrEmpty(ConnectionString) == false && string.IsNullOrEmpty(ProviderName) == false
+                                            ? new UmbracoDatabase(ConnectionString, ProviderName, _logger)
+                                            : new UmbracoDatabase(_connectionStringName, _logger);
+
+                    return value;
+                }
+            }
+
+            // we have an http context, so only create one per request.
+            // UmbracoDatabase is marked IDisposeOnRequestEnd and therefore will be disposed when
+            // UmbracoModule attempts to dispose the relevant HttpContext items. so we DO dispose
+            // connections at the end of each request. no need to call ReleaseDatabase.
+            LogHelper.Debug<DefaultDatabaseFactory>("Get http [T" + Environment.CurrentManagedThreadId + "]");
+            if (HttpContext.Current.Items.Contains(typeof(DefaultDatabaseFactory)) == false)
+            {
+                LogHelper.Debug<DefaultDatabaseFactory>("Create http [T" + Environment.CurrentManagedThreadId + "]");
+                HttpContext.Current.Items.Add(typeof(DefaultDatabaseFactory),
+                                              string.IsNullOrEmpty(ConnectionString) == false && string.IsNullOrEmpty(ProviderName) == false
                                                   ? new UmbracoDatabase(ConnectionString, ProviderName, _logger)
                                                   : new UmbracoDatabase(_connectionStringName, _logger));
-			}
-			return (UmbracoDatabase)HttpContext.Current.Items[typeof(DefaultDatabaseFactory)];
-		}
+            }
+            return (UmbracoDatabase)HttpContext.Current.Items[typeof(DefaultDatabaseFactory)];
+        }
 
-		protected override void DisposeResources()
+        // releases the "context" database
+        public void ReleaseDatabase()
+        {
+            if (HttpContext.Current == null)
+            {
+                var value = NonContextValue;
+                if (value != null) value.Dispose();
+                NonContextValue = null;
+            }
+            else
+            {
+                var db = (UmbracoDatabase)HttpContext.Current.Items[typeof(DefaultDatabaseFactory)];
+                if (db != null) db.Dispose();
+                HttpContext.Current.Items.Remove(typeof(DefaultDatabaseFactory));
+            }
+        }
+
+        protected override void DisposeResources()
 		{
-			if (HttpContext.Current == null)
-			{
-                _nonHttpInstance.Dispose();
-			}
-			else
-			{
-				if (HttpContext.Current.Items.Contains(typeof(DefaultDatabaseFactory)))
-				{
-					((UmbracoDatabase)HttpContext.Current.Items[typeof(DefaultDatabaseFactory)]).Dispose();
-				}
-			}
+            ReleaseDatabase();
 		}
 
         // during tests, the thread static var can leak between tests
         // this method provides a way to force-reset the variable
 	    internal void ResetForTests()
 	    {
-	        if (_nonHttpInstance == null) return;
-	        _nonHttpInstance.Dispose();
-	        _nonHttpInstance = null;
-	    }
-	}
+            var value = NonContextValue;
+            if (value != null) value.Dispose();
+            NonContextValue = null;
+        }
+
+        #region SafeCallContext
+
+        // see notes in SafeCallContext - need to do this since we are using
+        // the logical call context...
+
+        static DefaultDatabaseFactory()
+        {
+            SafeCallContext.Register(DetachDatabase, AttachDatabase);
+        }
+
+        // detaches the current database
+        // ie returns the database and remove it from whatever is "context"
+        private static UmbracoDatabase DetachDatabase()
+        {
+            if (HttpContext.Current == null)
+            {
+                var db = NonContextValue;
+                NonContextValue = null;
+                return db;
+            }
+            else
+            {
+                var db = (UmbracoDatabase)HttpContext.Current.Items[typeof(DefaultDatabaseFactory)];
+                HttpContext.Current.Items.Remove(typeof(DefaultDatabaseFactory));
+                return db;
+            }
+        }
+
+        // attach a current database
+        // ie assign it to whatever is "context"
+        // throws if there already is a database
+        private static void AttachDatabase(object o)
+        {
+            var database = o as UmbracoDatabase;
+            if (o != null && database == null) throw new ArgumentException("Not an UmbracoDatabase.", "o");
+
+            if (HttpContext.Current == null)
+            {
+                if (NonContextValue != null) throw new InvalidOperationException();
+                if (database != null) NonContextValue = database;
+            }
+            else
+            {
+                if (HttpContext.Current.Items[typeof(DefaultDatabaseFactory)] != null) throw new InvalidOperationException();
+                if (database != null) HttpContext.Current.Items[typeof(DefaultDatabaseFactory)] = database;
+            }
+        }
+
+        #endregion
+    }
 }
