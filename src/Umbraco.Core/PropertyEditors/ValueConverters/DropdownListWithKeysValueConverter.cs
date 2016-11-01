a10214d82b6b0e41c7c6d37025f68995c568feeb
James Coxhead <me@jamescoxhead.co.uk>
2016-11-01 23:53:55 +0000

    Added dropdown with keys value converter

---
+++ src/Umbraco.Core/PropertyEditors/ValueConverters/DropdownListWithKeysValueConverter.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace Umbraco.Core.PropertyEditors.ValueConverters
+{
+    public class DropdownListWithKeysValueConverter : PropertyValueConverterBase, IPropertyValueConverterMeta
+    {
+        public override bool IsConverter(PublishedPropertyType propertyType)
+        {
+            return
+                propertyType.PropertyEditorAlias.InvariantEquals(Constants.PropertyEditors
+                                                                          .DropdownlistPublishingKeysAlias);
+        }
+
+        public override object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview)
+        {
+            var intAttempt = source.TryConvertTo<int>();
+            if (intAttempt.Success)
+                return intAttempt.Result;
+
+            return null;
+        }
+
+        public Type GetPropertyValueType(PublishedPropertyType propertyType)
+        {
+            return typeof(int);
+        }
+
+        public PropertyCacheLevel GetPropertyCacheLevel(PublishedPropertyType propertyType,
+                                                        PropertyCacheValue cacheValue)
+        {
+            return PropertyCacheLevel.Content;
+        }
+    }
+}
