84fa2f5a4e26b09e70cf27843a37e33405f2f2f5
Hanro Viljoen <Hanro.Viljoen@gmail.com>
2016-11-03 13:25:13 +0000

    Fix for U4-8359. Changed the MNTP & MMP's dbType to NText Added Migration script which can run multiple times.

---
+++ src/Umbraco.Core/Persistence/Migrations/Upgrades/TargetVersionSevenSixZero/ModifyMultiNodeTreePickerDataType.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Configuration;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Models.Rdbms;
+using Umbraco.Core.Persistence.SqlSyntax;
+
+namespace Umbraco.Core.Persistence.Migrations.Upgrades.TargetVersionSevenSixZero
+{
+    [Migration("7.6.0", 1, GlobalSettings.UmbracoMigrationName)]
+    public class ModifyMultiNodeTreePickerDataType : MigrationBase
+    {
+        public ModifyMultiNodeTreePickerDataType(ISqlSyntaxProvider sqlSyntax, ILogger logger) : base(sqlSyntax, logger)
+        {
+
+        }
+
+        public override void Down()
+        {
+            // dont be silly
+        }
+
+        public override void Up()
+        {
+            // get all data
+            var sql = $@"
+             SELECT    pd.id, pd.dataNvarchar from cmsPropertyData pd
+             JOIN      cmsPropertyType pt on pd.propertyTypeId = pt.id
+             JOIN      cmsDataType dt on dt.nodeid = pt.dataTypeId
+             WHERE     dt.propertyEditorAlias in ('{Constants.PropertyEditors.MultiNodeTreePickerAlias}','{Constants.PropertyEditors.MultipleMediaPickerAlias}')
+             AND       dt.dbType = 'Nvarchar'";
+
+            var multiNodeRelations = Context.Database.Fetch<PropertyDataDto>(sql);
+
+            // I'm not deleting the old value on purpose to prevent deleting data when running the upgrade multiple times.
+
+            foreach (var propertyData in multiNodeRelations)
+            {
+                Update.Table("cmsPropertyData")
+                    .Set(new
+                    {
+                        dataNtext = propertyData.VarChar
+                    })
+                    .Where(new {Id = propertyData.Id});
+            }
+
+            Update.Table("cmsDataType")
+                .Set(new
+                {
+                    dbType = "Ntext"
+                })
+                .Where(new
+                {
+                    propertyEditorAlias = Constants.PropertyEditors.MultiNodeTreePickerAlias,
+                    dbTYpe = "Nvarchar"
+                });
+
+            Update.Table("cmsDataType")
+                .Set(new
+                {
+                    dbType = "Ntext"
+                })
+                .Where(new
+                {
+                    propertyEditorAlias = Constants.PropertyEditors.MultipleMediaPickerAlias,
+                    dbTYpe = "Nvarchar"
+                });
+        }
+    }
+}
