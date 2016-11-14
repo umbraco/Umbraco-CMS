9d2fb0dd03317332c7282f74762ee9da11b29d5b
Stephan <stephane@umbraco.dk>
2016-11-14 19:01:44 +0100

    Fix after merges

--- src/Umbraco.Core/Services/MediaService.cs
+++ src/Umbraco.Core/Services/MediaService.cs
@@ -6,6 +6,7 @@ using System.IO;
 using System.Linq;
 using System.Text.RegularExpressions;
 using System.Threading;
+using System.Xml.Linq;
 using Umbraco.Core.Configuration;
 using Umbraco.Core.Events;
 using Umbraco.Core.IO;
