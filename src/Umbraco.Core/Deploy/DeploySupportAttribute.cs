f51a086137fc34a619ed45ac19ea220f01a5626e
Stephan <stephane@umbraco.dk>
2016-11-30 18:18:41 +0100

    deploy-11 - implement DeployVersionAttribute

---
+++ src/Umbraco.Core/Deploy/DeploySupportAttribute.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Semver;
+
+namespace Umbraco.Core.Deploy
+{
+    [AttributeUsage(AttributeTargets.Assembly)]
+    public class DeploySupportAttribute : Attribute
+    {
+        public DeploySupportAttribute(string version)
+        {
+            Version = version;
+        }
+
+        public SemVersion Version { get; private set; }
+    }
+}
