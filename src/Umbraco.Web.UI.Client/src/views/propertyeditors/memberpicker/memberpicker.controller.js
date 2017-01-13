eba8181d0c5025addd6cbc6ee6b508b3a4cbaa63
Mads Rasmussen <mads@umbraco.com>
2017-01-13 11:17:28 +0100

    use new umb node preview component in member picker

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/memberpicker/memberpicker.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/memberpicker/memberpicker.controller.js
@@ -8,6 +8,7 @@ function memberPickerController($scope, dialogService, entityResource, $log, ico
     }
 
     $scope.renderModel = [];
+    $scope.allowRemove = true;
 
     var dialogOptions = {
         multiPicker: false,
