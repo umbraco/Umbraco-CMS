c857d822d82d84e95a181d347e28ebe29121ffcc
Sebastiaan Janssen <sebastiaan@umbraco.com>
2017-01-26 10:22:54 +0100

    This new method is for internal use only, made private

--- src/Umbraco.Web/Editors/MediaController.cs
+++ src/Umbraco.Web/Editors/MediaController.cs
@@ -240,7 +240,7 @@ namespace Umbraco.Web.Editors
         /// Returns the child media objects
         /// </summary>
         [FilterAllowedOutgoingMedia(typeof(IEnumerable<ContentItemBasic<ContentPropertyBasic, IMedia>>), "Items")]
-        public PagedResult<ContentItemBasic<ContentPropertyBasic, IMedia>> GetChildrenByInt(int id,
+        private PagedResult<ContentItemBasic<ContentPropertyBasic, IMedia>> GetChildrenByInt(int id,
             int pageNumber = 0,
             int pageSize = 0,
             string orderBy = "SortOrder",
