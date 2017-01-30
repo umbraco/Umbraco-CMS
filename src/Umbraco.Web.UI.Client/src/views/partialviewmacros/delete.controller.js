67da15330ee0863a3c375ac0b929ad7c654ac690
Claus <claus@umbraco.com>
2017-01-30 11:56:30 +0100

    U4-9410 Hook up delete dialog for Code Files
    delete dialogs for partialviews, partialviewmacros and scripts.
    + fixed codefileResource.
    + fixed comments in MemberTypesDeleteController.

---
+++ src/Umbraco.Web.UI.Client/src/views/partialviewmacros/delete.controller.js
@@ -0,0 +1,34 @@
+/**
+ * @ngdoc controller
+ * @name Umbraco.Editors.PartialViewMacros.DeleteController
+ * @function
+ *
+ * @description
+ * The controller for deleting partial view macros
+ */
+function PartialViewMacrosDeleteController($scope, codefileResource, treeService, navigationService) {
+
+    $scope.performDelete = function() {
+
+        //mark it for deletion (used in the UI)
+        $scope.currentNode.loading = true;
+
+        var virtualPath = $scope.currentNode.parentId + $scope.currentNode.name;
+
+        codefileResource.deleteByPath('partialViewMacros', virtualPath)
+            .then(function() {
+                $scope.currentNode.loading = false;
+                //get the root node before we remove it
+                var rootNode = treeService.getTreeRoot($scope.currentNode);
+                //TODO: Need to sync tree, etc...
+                treeService.removeNode($scope.currentNode);
+                navigationService.hideMenu();
+            });
+    };
+
+    $scope.cancel = function() {
+        navigationService.hideDialog();
+    };
+}
+
+angular.module("umbraco").controller("Umbraco.Editors.PartialViewMacros.DeleteController", PartialViewMacrosDeleteController);
