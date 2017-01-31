d4fab66411f2d12edc24f41cbf7229f5ad66ce6c
Mads Rasmussen <mads@umbraco.com>
2017-01-31 15:00:37 +0100

    add UI for partial views create dialog

--- src/Umbraco.Web.UI.Client/src/views/partialviews/create.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/partialviews/create.controller.js
@@ -1,7 +1,56 @@
 (function () {
     "use strict";
 
-    function PartialViewsCreateController() {
+    function PartialViewsCreateController($scope, codefileResource, $location, navigationService) {
+
+        var vm = this;
+        var node = $scope.dialogOptions.currentNode;
+
+        vm.snippets = [];
+        vm.showSnippets = false;
+        vm.creatingFolder = false;
+
+        vm.createPartialView = createPartialView;
+        vm.showCreateFolder = showCreateFolder;
+        vm.createFolder = createFolder;
+        vm.showCreateFromSnippet = showCreateFromSnippet;
+
+        function onInit() {
+            codefileResource.getSnippets('partialViews')
+                .then(function(snippets) {
+                    vm.snippets = snippets;
+                });
+        }
+
+        function createPartialView(selectedSnippet) {
+
+            var snippet = null;
+
+            if(selectedSnippet && selectedSnippet.fileName) {
+                snippet = selectedSnippet.fileName;
+            }
+
+            $location.search('create', null);
+            $location.search('snippet', null);
+            $location.path("/settings/partialviews/edit/" + node.id).search("create", "true").search("snippet", snippet);
+            navigationService.hideMenu();
+
+        }
+
+        function showCreateFolder() {
+            vm.creatingFolder = true;
+        }
+
+        function createFolder() {
+            alert("create folder");
+        }
+        
+        function showCreateFromSnippet() {
+            vm.showSnippets = true;
+        }
+        
+        onInit();
+
     }
 
     angular.module("umbraco").controller("Umbraco.Editors.PartialViews.CreateController", PartialViewsCreateController);
