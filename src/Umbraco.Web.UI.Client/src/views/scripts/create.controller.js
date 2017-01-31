d22afe07db387db5b857f263ad25be9e8e2c0bae
Mads Rasmussen <mads@umbraco.com>
2017-01-31 12:40:56 +0100

    add UI for scripts create dialog

--- src/Umbraco.Web.UI.Client/src/views/scripts/create.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/scripts/create.controller.js
@@ -1,7 +1,34 @@
 (function () {
     "use strict";
 
-    function ScriptsCreateController() {
+    function ScriptsCreateController($scope, $location, navigationService) {
+
+        var vm = this;
+        var node = $scope.dialogOptions.currentNode;
+
+        vm.creatingFolder = false;
+        vm.folderName = "";
+        vm.fileExtension = "";
+
+        vm.createFile = createFile;
+        vm.showCreateFolder = showCreateFolder;
+        vm.createFolder = createFolder;
+
+        function createFile(fileExtension) {
+            $location.search('create', null);
+            $location.search('fileExtension', null);
+            $location.path("/settings/scripts/edit/" + node.id).search("create", "true").search("fileExtension", fileExtension);
+            navigationService.hideMenu();
+        }
+
+        function showCreateFolder() {
+            vm.creatingFolder = true;
+        }
+
+        function createFolder() {
+            alert("create folder");
+        }
+
     }
 
     angular.module("umbraco").controller("Umbraco.Editors.Scripts.CreateController", ScriptsCreateController);
