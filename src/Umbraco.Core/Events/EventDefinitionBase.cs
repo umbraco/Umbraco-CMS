a65ad3c72a517ca31bc64ffed7ae3997cf7a29ea
Shannon <sdeminick@gmail.com>
2017-02-06 18:46:23 +1100

    Adds equality members for all event args, updates equality check for EventDefinitionBase to include the args (since that is quite important!), creates an OrderedHashSet class which can deduplicate lists for us based on member equality and supports first in or last in + tests.

--- src/Umbraco.Core/Events/EventDefinitionBase.cs
+++ src/Umbraco.Core/Events/EventDefinitionBase.cs
@@ -35,7 +35,7 @@ namespace Umbraco.Core.Events
         {
             if (ReferenceEquals(null, other)) return false;
             if (ReferenceEquals(this, other)) return true;
-            return Sender.Equals(other.Sender) && string.Equals(EventName, other.EventName);
+            return Args.Equals(other.Args) && string.Equals(EventName, other.EventName) && Sender.Equals(other.Sender);
         }
 
         public override bool Equals(object obj)
@@ -50,7 +50,10 @@ namespace Umbraco.Core.Events
         {
             unchecked
             {
-                return (Sender.GetHashCode() * 397) ^ EventName.GetHashCode();
+                var hashCode = Args.GetHashCode();
+                hashCode = (hashCode * 397) ^ EventName.GetHashCode();
+                hashCode = (hashCode * 397) ^ Sender.GetHashCode();
+                return hashCode;
             }
         }
 
@@ -61,7 +64,7 @@ namespace Umbraco.Core.Events
 
         public static bool operator !=(EventDefinitionBase left, EventDefinitionBase right)
         {
-            return !Equals(left, right);
+            return Equals(left, right) == false;
         }
     }
 }
