999de5ae1ddab5e429d26804f4b3e738d201ee1b
Mads Rasmussen <mads@umbraco.com>
2017-02-06 15:05:13 +0100

    add client side logic for partial view macros folder creation

--- src/Umbraco.Web.UI.Client/src/views/partialviewmacros/create.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/partialviewmacros/create.controller.js
@@ -1,14 +1,17 @@
 (function () {
     "use strict";
 
-    function PartialViewMacrosCreateController($scope, codefileResource, $location, navigationService) {
+    function PartialViewMacrosCreateController($scope, codefileResource, $location, navigationService, formHelper, localizationService) {
 
         var vm = this;
         var node = $scope.dialogOptions.currentNode;
+        var localizeCreateFolder = localizationService.localize("defaultdialog_createFolder");
 
         vm.snippets = [];
         vm.showSnippets = false;
         vm.creatingFolder = false;
+        vm.createFolderError = "";
+        vm.folderName = "";
 
         vm.createPartialViewMacro = createPartialViewMacro;
         vm.showCreateFolder = showCreateFolder;
@@ -39,8 +42,38 @@
             vm.creatingFolder = true;
         }
 
-        function createFolder() {
+        function createFolder(form) {
+            if (formHelper.submitForm({scope: $scope, formCtrl: form, statusMessage: localizeCreateFolder})) {
 
+                codefileResource.createContainer("partialViewMacros", node.id, vm.folderName).then(function(path) {
+
+                    navigationService.hideMenu();
+
+                    navigationService.syncTree({
+                        tree: "partialViewMacros",
+                        path: path,
+                        forceReload: true,
+                        activate: true
+                    });
+
+                    formHelper.resetForm({
+                        scope: $scope
+                    });
+
+                    var section = appState.getSectionState("currentSection");
+
+                }, function(err) {
+
+                    vm.createFolderError = err;
+
+                    //show any notifications
+                    if (angular.isArray(err.data.notifications)) {
+                        for (var i = 0; i < err.data.notifications.length; i++) {
+                            notificationsService.showNotification(err.data.notifications[i]);
+                        }
+                    }
+                });
+            }
         }
         
         function showCreateFromSnippet() {
