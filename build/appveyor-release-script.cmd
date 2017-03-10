fa62363b65944887e580e661391ea1b032eff3ed
Stephan <stephane@umbraco.dk>
2017-03-10 09:57:41 +0100

    Refactor Core build process

--- build/appveyor-release-script.cmd
+++ build/appveyor-release-script.cmd
@@ -12,25 +12,6 @@ CD build
 SET PATH=C:\Program Files (x86)\MSBuild\14.0\Bin;%PATH%
 
 %SRC%\.nuget\NuGet.exe sources Add -Name MyGetUmbracoCore -Source https://www.myget.org/F/umbracocore/api/v2/ >NUL
-build.bat -integration -nugetfolder:%PACKAGES%
-
-COPY UmbracoCms.* ..\
-
-
-REM -- below is original script
-
-cd build
-
-SET nuGetFolder=%CD%\..\src\packages\
-..\src\.nuget\NuGet.exe sources Add -Name MyGetUmbracoCore -Source https://www.myget.org/F/umbracocore/api/v2/ >NUL
-..\src\.nuget\NuGet.exe install ..\src\Umbraco.Web.UI\packages.config -OutputDirectory %nuGetFolder% -Verbosity quiet
-
-IF EXIST ..\src\umbraco.businesslogic\packages.config ..\src\.nuget\NuGet.exe install ..\src\umbraco.businesslogic\packages.config -OutputDirectory %nuGetFolder% -Verbosity quiet
-..\src\.nuget\NuGet.exe install ..\src\Umbraco.Core\packages.config -OutputDirectory %nuGetFolder% -Verbosity quiet
-
-SET MSBUILD="C:\Program Files (x86)\MSBuild\14.0\Bin\MsBuild.exe"
-%MSBUILD% "../src/Umbraco.Tests/Umbraco.Tests.csproj" /consoleloggerparameters:Summary;ErrorsOnly;WarningsOnly
-
-build.bat
+build.bat -integration -nugetfolder:%PACKAGES% -nugetpkg
 
 COPY UmbracoCms.* ..\
