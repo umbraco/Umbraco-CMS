3571c238b4815a859421c52d41129f54ea964a16
Shannon <sdeminick@gmail.com>
2017-03-13 12:59:36 +1100

    Renames the new UDI pickers to still be suffixed with "PropertyEditor" to make things more consistent

--- src/Umbraco.Web/PropertyEditors/ContentPickerPropertyEditor.cs
+++ src/Umbraco.Web/PropertyEditors/ContentPickerPropertyEditor.cs
@@ -1,5 +1,4 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using Umbraco.Core;
 using Umbraco.Core.PropertyEditors;
@@ -12,7 +11,7 @@ namespace Umbraco.Web.PropertyEditors
     /// </summary>
     [Obsolete("This editor is obsolete, use ContentPickerPropertyEditor2 instead which stores UDI")]
     [PropertyEditor(Constants.PropertyEditors.ContentPickerAlias, "(Obsolete) Content Picker", PropertyEditorValueTypes.Integer, "contentpicker", IsParameterEditor = true, Group = "Pickers", IsDeprecated = true)]
-    public class ContentPickerPropertyEditor : ContentPickerPropertyEditor2
+    public class ContentPickerPropertyEditor : ContentPicker2PropertyEditor
     {
         public ContentPickerPropertyEditor()
         {
@@ -28,63 +27,6 @@ namespace Umbraco.Web.PropertyEditors
             var preValEditor = base.CreatePreValueEditor();
             preValEditor.Fields.Single(x => x.Key == "startNodeId").Config["idType"] = "int";
             return preValEditor;
-        }
-    }
-
-    /// <summary>
-    /// Content property editor that stores UDI
-    /// </summary>
-    [PropertyEditor(Constants.PropertyEditors.ContentPicker2Alias, "Content Picker", PropertyEditorValueTypes.String, "contentpicker", IsParameterEditor = true, Group = "Pickers")]
-    public class ContentPickerPropertyEditor2 : PropertyEditor
-    {
-
-        public ContentPickerPropertyEditor2()
-        {
-            InternalPreValues = new Dictionary<string, object>
-            {
-                {"startNodeId", "-1"},
-                {"showOpenButton", "0"},
-                {"showEditButton", "0"},
-                {"showPathOnHover", "0"},
-                {"idType", "udi"}
-            };
-        }
-
-        internal IDictionary<string, object> InternalPreValues;
-        public override IDictionary<string, object> DefaultPreValues
-        {
-            get { return InternalPreValues; }
-            set { InternalPreValues = value; }
-        }
-
-        protected override PreValueEditor CreatePreValueEditor()
-        {
-            return new ContentPickerPreValueEditor();
-        }
-
-        internal class ContentPickerPreValueEditor : PreValueEditor
-        {
-            public ContentPickerPreValueEditor()
-            {
-                //create the fields
-                Fields.Add(new PreValueField()
-                {                    
-                    Key = "showOpenButton",
-                    View = "boolean",
-                    Name = "Show open button (this feature is in preview!)",
-                    Description = "Opens the node in a dialog"
-                });
-                Fields.Add(new PreValueField()
-                {
-                    Key = "startNodeId",
-                    View = "treepicker",
-                    Name = "Start node",
-                    Config = new Dictionary<string, object>
-                    {
-                        {"idType", "udi"}
-                    }
-                });
-            }
         }
     }
 }
--- src/Umbraco.Web/PropertyEditors/MultiNodeTreePickerPropertyEditor.cs
+++ src/Umbraco.Web/PropertyEditors/MultiNodeTreePickerPropertyEditor.cs
@@ -1,15 +1,13 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Linq;
 using Umbraco.Core;
-using Umbraco.Core.Models;
 using Umbraco.Core.PropertyEditors;
 
 namespace Umbraco.Web.PropertyEditors
 {
     [Obsolete("This editor is obsolete, use MultiNodeTreePickerPropertyEditor2 instead which stores UDI")]
     [PropertyEditor(Constants.PropertyEditors.MultiNodeTreePickerAlias, "(Obsolete) Multinode Treepicker", "contentpicker", Group = "pickers", Icon = "icon-page-add", IsDeprecated = true)]
-    public class MultiNodeTreePickerPropertyEditor : MultiNodeTreePickerPropertyEditor2
+    public class MultiNodeTreePickerPropertyEditor : MultiNodeTreePicker2PropertyEditor
     {
         public MultiNodeTreePickerPropertyEditor()
         {
@@ -25,120 +23,6 @@ namespace Umbraco.Web.PropertyEditors
             var preValEditor = base.CreatePreValueEditor();
             preValEditor.Fields.Single(x => x.Key == "startNode").Config["idType"] = "int";
             return preValEditor;
-        }
-    }
-
-    [PropertyEditor(Constants.PropertyEditors.MultiNodeTreePicker2Alias, "Multinode Treepicker", PropertyEditorValueTypes.Text, "contentpicker", Group="pickers", Icon="icon-page-add")]
-    public class MultiNodeTreePickerPropertyEditor2 : PropertyEditor
-    {
-        public MultiNodeTreePickerPropertyEditor2()
-        {
-            InternalPreValues = new Dictionary<string, object>
-            {
-                {"multiPicker", "1"},
-                {"showOpenButton", "0"},
-                {"showEditButton", "0"},
-                {"showPathOnHover", "0"},
-                {"idType", "udi"}
-            };
-        }
-        
-        protected override PreValueEditor CreatePreValueEditor()
-        {
-            return new MultiNodePickerPreValueEditor();
-        }
-
-        internal IDictionary<string, object> InternalPreValues;
-        public override IDictionary<string, object> DefaultPreValues
-        {
-            get { return InternalPreValues; }
-            set { InternalPreValues = value; }
-        }
-
-        internal class MultiNodePickerPreValueEditor : PreValueEditor
-        {
-            public MultiNodePickerPreValueEditor()
-            {
-                //create the fields
-                Fields.Add(new PreValueField()
-                {
-                    Key = "startNode",
-                    View = "treesource",
-                    Name = "Node type",
-                    Config = new Dictionary<string, object>
-                    {
-                        {"idType", "udi"}
-                    }
-                });
-                Fields.Add(new PreValueField()
-                {
-                    Key = "filter",
-                    View = "textstring",
-                    Name = "Allow items of type",
-                    Description = "Separate with comma"
-                });
-                Fields.Add(new PreValueField()
-                {
-                    Key = "minNumber",
-                    View = "number",
-                    Name = "Minimum number of items"
-                });
-                Fields.Add(new PreValueField()
-                {
-                    Key = "maxNumber",
-                    View = "number",
-                    Name = "Maximum number of items"
-                });
-                Fields.Add(new PreValueField()
-                {
-                    Key = "showOpenButton",
-                    View = "boolean",
-                    Name = "Show open button (this feature is in preview!)",
-                    Description = "Opens the node in a dialog"
-                });
-            }            
-
-            /// <summary>
-            /// This ensures the multiPicker pre-val is set based on the maxNumber of nodes set
-            /// </summary>
-            /// <param name="defaultPreVals"></param>
-            /// <param name="persistedPreVals"></param>
-            /// <returns></returns>
-            /// <remarks>
-            /// Due to compatibility with 7.0.0 the multiPicker pre-val might already exist in the db, but we've removed that setting in 7.0.1 so we need to detect it and if it is
-            /// there, then we'll set the maxNumber to '1'
-            /// </remarks>
-            public override IDictionary<string, object> ConvertDbToEditor(IDictionary<string, object> defaultPreVals, PreValueCollection persistedPreVals)
-            {
-                var result = base.ConvertDbToEditor(defaultPreVals, persistedPreVals);
-
-                //backwards compatibility check
-                if (result.ContainsKey("multiPicker") && result["multiPicker"].ToString() == "0")
-                {
-                    result["maxNumber"] = "1";
-                }
-
-                //set the multiPicker val correctly depending on the maxNumber
-                if (result.ContainsKey("maxNumber"))
-                {
-                    var asNumber = result["maxNumber"].TryConvertTo<int>();
-                    if (asNumber.Success)
-                    {
-                        if (asNumber.Result <= 1)
-                        {
-                            result["multiPicker"] = "0";
-                        }
-                        else
-                        {
-                            result["multiPicker"] = "1";
-                        }
-                    }    
-                }
-                
-
-                return result;
-            }
-
         }
     }
 }
