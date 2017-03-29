db809f30fe865873c4249b2b0d8651e516fe7911
Emil Wangaa <emilwangaa@gmail.com>
2017-03-29 14:00:36 +0200

    Reverted hideHeader value to true in treepicker.controller.js - should not have been changed

--- src/Umbraco.Web.UI.Client/src/views/common/overlays/treepicker/treepicker.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/common/overlays/treepicker/treepicker.controller.js
@@ -9,7 +9,7 @@ angular.module("umbraco").controller("Umbraco.Overlays.TreePickerController",
         $scope.section = dialogOptions.section;
         $scope.treeAlias = dialogOptions.treeAlias;
         $scope.multiPicker = dialogOptions.multiPicker;
-        $scope.hideHeader = false;
+        $scope.hideHeader = true;
         // if you need to load a not initialized tree set this value to false - default is true
         $scope.onlyInitialized = dialogOptions.onlyInitialized;
         $scope.searchInfo = {
