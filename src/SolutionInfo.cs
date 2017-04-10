f2c7c1e2c6a3e4dfa73ad1d1d5a1c01dd1779a56
Shannon <sdeminick@gmail.com>
2017-04-10 16:42:31 +1000

    bumps version

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -12,4 +12,4 @@ using System.Resources;
 [assembly: AssemblyVersion("1.0.*")]
 
 [assembly: AssemblyFileVersion("7.5.13")]
-[assembly: AssemblyInformationalVersion("7.5.13-beta1")]
+[assembly: AssemblyInformationalVersion("7.5.13")]
