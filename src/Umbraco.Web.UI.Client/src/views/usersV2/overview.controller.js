e7137e9cce56f6256d4eb1b6165cc8ad3816c9f3
Mads Rasmussen <mads@umbraco.com>
2017-05-04 11:34:09 +0200

    first steps on user dashboard

---
+++ src/Umbraco.Web.UI.Client/src/views/usersV2/overview.controller.js
@@ -0,0 +1,64 @@
+(function () {
+    "use strict";
+
+    function UsersOverviewController($scope, $timeout, usersResource) {
+
+        var vm = this;
+
+        vm.page = {};
+        vm.page.name = "Users";
+        vm.users = [];
+        vm.userGroups = [];
+        vm.usersViewState = 'overview';
+
+        vm.save = save;
+        vm.setUsersViewState = setUsersViewState;
+        vm.getUserStateType = getUserStateType;
+
+        function init() {
+
+            vm.loading = true;
+
+            // Get users
+            usersResource.getUsers().then(function (users) {
+                vm.users = users;
+            });
+
+            // Get user groups
+            usersResource.getUserGroups().then(function (userGroups) {
+                vm.userGroups = userGroups;
+            });
+
+            // fake loading
+            $timeout(function () {
+                vm.loading = false;
+            }, 500);
+
+        }
+
+        function save() {
+            alert("save");
+        }
+
+        function setUsersViewState(state) {
+            vm.usersViewState = state;
+        }
+
+        function getUserStateType(state) {
+            switch (state) {
+                case "disabled" || "umbracoDisabled":
+                    return "danger";
+                case "pending":
+                    return "warning";
+                default:
+                    return "success";
+            }
+        }
+
+        init();
+
+    }
+
+    angular.module("umbraco").controller("Umbraco.Editors.Users.OverviewController", UsersOverviewController);
+
+})();
