350bd75310e394c221e4025869821363cc995ba1
Mads Rasmussen <mads@umbraco.com>
2017-05-08 10:56:21 +0200

    add filters, layout selector and invite/create user buttons

--- src/Umbraco.Web.UI.Client/src/views/usersV2/overview.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/usersV2/overview.controller.js
@@ -9,15 +9,52 @@
         vm.page.name = "Users";
         vm.users = [];
         vm.userGroups = [];
+        vm.userStates = [];
         vm.usersViewState = 'overview';
         vm.usersPagination = {
             "pageNumber": 1,
             "totalPages": 5
         }
+        
+        vm.layouts = [
+            {
+                "icon": "icon-thumbnails-small",
+                "path": "1",
+                "selected": true
+            },
+            {
+                "icon": "icon-list",
+                "path": "2",
+                "selected": true
+            }
+        ];
+
+        vm.activeLayout = {
+            "icon": "icon-thumbnails-small",
+            "path": "1",
+            "selected": true
+        };
+
+        vm.defaultButton = {
+            labelKey:"users_inviteUser",
+            handler: function() {
+                vm.setUsersViewState('inviteUser');
+            }
+        };
+
+        vm.subButtons = [
+            {
+                labelKey: "users_createUser",
+                handler: function () {
+                    vm.setUsersViewState('createUser');
+                }
+            }
+        ];
 
         vm.save = save;
         vm.setUsersViewState = setUsersViewState;
         vm.getUserStateType = getUserStateType;
+        vm.selectLayout = selectLayout;
 
         function init() {
 
@@ -26,6 +63,7 @@
             // Get users
             usersResource.getUsers().then(function (users) {
                 vm.users = users;
+                vm.userStates = getUserStates(users);
             });
 
             // Get user groups
@@ -57,6 +95,42 @@
                 default:
                     return "success";
             }
+        }
+
+        function selectLayout(selectedLayout) {
+
+            angular.forEach(vm.layouts, function(layout){
+                layout.active = false;
+            });
+
+            selectedLayout.active = true;
+            vm.activeLayout = selectedLayout;
+        }
+
+
+        // helpers
+        function getUserStates(users) {
+            var userStates = [];
+            
+            angular.forEach(users, function(user) {
+
+                var newUserState = {"name": user.state, "count": 1};
+                var userStateExists = false;
+
+                angular.forEach(userStates, function(userState){
+                    if(newUserState.name === userState.name) {
+                        userState.count = userState.count + 1;
+                        userStateExists = true;
+                    }
+                });
+
+                if(userStateExists === false) {
+                    userStates.push(newUserState);
+                }
+
+            });
+
+            return userStates;
         }
 
         init();
