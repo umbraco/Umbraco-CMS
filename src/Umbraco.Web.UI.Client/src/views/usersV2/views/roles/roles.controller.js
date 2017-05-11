e5240575bd8454ffee141febf86d0d2af12836a3
Mads Rasmussen <mads@umbraco.com>
2017-05-11 09:31:58 +0200

    add bulk selection to user roles

---
+++ src/Umbraco.Web.UI.Client/src/views/usersV2/views/roles/roles.controller.js
@@ -0,0 +1,94 @@
+(function () {
+    "use strict";
+
+    function UserRolesController($scope, $timeout, $location, usersResource) {
+
+        var vm = this;
+
+        vm.userRoles = [];
+        vm.selection = [];
+        vm.viewState = 'overview';
+        
+        vm.pagination = {
+            "pageNumber": 1,
+            "totalPages": 5
+        };
+
+        vm.setViewState = setViewState;
+        vm.goToUserRole = goToUserRole;
+        vm.clearSelection = clearSelection;
+        vm.selectUserRole = selectUserRole;
+        vm.selectAll = selectAll;
+        vm.areAllSelected = areAllSelected;
+
+        function init() {
+
+            vm.loading = true;
+
+            // Get users
+            usersResource.getUserGroups().then(function (userRoles) {
+                vm.userRoles = userRoles;
+            });
+
+            // fake loading
+            $timeout(function () {
+                vm.loading = false;
+            }, 500);
+
+        }
+
+        function setViewState(state) {
+            vm.viewState = state;
+        }
+
+        function selectUserRole(userRole, selection) {
+            if(userRole.selected) {
+                var index = selection.indexOf(userRole.id);
+                selection.splice(index, 1);
+                userRole.selected = false;
+            } else {
+                userRole.selected = true;
+                vm.selection.push(userRole.id);
+            }
+        }
+
+        function clearSelection() {
+            angular.forEach(vm.userRoles, function(userRole){
+                userRole.selected = false;
+            });
+            vm.selection = [];
+        }
+
+        function goToUserRole(userRole) {
+            $location.path('users/usersV2/role/' + userRole.id);
+        }
+        function selectAll() {
+            if(areAllSelected()) {
+                vm.selection = [];
+                angular.forEach(vm.userRoles, function(userRole){
+                    userRole.selected = false;
+                });
+            } else {
+                // clear selection so we don't add the same user twice
+                vm.selection = [];
+                // select all users
+                angular.forEach(vm.userRoles, function(userRole){
+                    userRole.selected = true;
+                    vm.selection.push(userRole.id);
+                });
+            }
+        }
+
+        function areAllSelected() {
+            if(vm.selection.length === vm.userRoles.length) {
+                return true;
+            }
+        }
+
+        init();
+
+    }
+
+    angular.module("umbraco").controller("Umbraco.Editors.Users.RolesController", UserRolesController);
+
+})();
