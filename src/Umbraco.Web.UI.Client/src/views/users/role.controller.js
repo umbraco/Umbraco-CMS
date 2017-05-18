aeabca87d70acd5f0ed1c2785b7a440a18513234
Mads Rasmussen <mads@umbraco.com>
2017-05-18 15:25:05 +0200

    init edit role editor

--- src/Umbraco.Web.UI.Client/src/views/users/role.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/users/role.controller.js
@@ -10,6 +10,11 @@
         vm.userRole = {};
 
         vm.goBack = goBack;
+        vm.openSectionPicker = openSectionPicker;
+        vm.openContentPicker = openContentPicker;
+        vm.openMediaPicker = openMediaPicker;
+        vm.removeSelectedItem = removeSelectedItem;
+        vm.getUserStateType = getUserStateType;
 
         function init() {
 
@@ -29,6 +34,68 @@
 
         function goBack() {
             $location.path("/users/users/overview").search("subview", "roles");
+        }
+
+        function openSectionPicker() {
+            alert("open section picker");
+        }
+
+        function openContentPicker() {
+            vm.contentPicker = {
+                title: "Select content start node",
+                view: "contentpicker",
+                multiPicker: true,
+                show: true,
+                submit: function(model) {
+                    if(model.selection) {
+                        vm.userRole.startNodesContent = model.selection;
+                    }
+                    vm.contentPicker.show = false;
+                    vm.contentPicker = null;
+                },
+                close: function(oldModel) {
+                    vm.contentPicker.show = false;
+                    vm.contentPicker = null;
+                }
+            };
+        }
+
+        function openMediaPicker() {
+            vm.contentPicker = {
+                title: "Select media start node",
+                view: "treepicker",
+                section: "media",
+                treeAlias: "media",
+                entityType: "media",
+                multiPicker: true,
+                show: true,
+                submit: function(model) {
+                    if(model.selection) {
+                        vm.userRole.startNodesMedia = model.selection;
+                    }
+                    vm.contentPicker.show = false;
+                    vm.contentPicker = null;
+                },
+                close: function(oldModel) {
+                    vm.contentPicker.show = false;
+                    vm.contentPicker = null;
+                }
+            };
+        }
+
+        function removeSelectedItem(index, selection) {
+            selection.splice(index, 1);
+        }
+
+        function getUserStateType(state) {
+            switch (state) {
+                case "disabled" || "umbracoDisabled":
+                    return "danger";
+                case "pending":
+                    return "warning";
+                default:
+                    return "success";
+            }
         }
  
         init();
