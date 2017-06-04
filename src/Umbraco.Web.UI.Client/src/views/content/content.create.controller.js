10761a2211121cc946f457f5d1f1e0e06c23aeb4
Lars-Erik Aabech <lea@markedspartner.no>
2017-06-05 00:39:16 +0200

    Selecting blueprints made configurable

--- src/Umbraco.Web.UI.Client/src/views/content/content.create.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/content/content.create.controller.js
@@ -6,41 +6,58 @@
  * @description
  * The controller for the content creation dialog
  */
-function contentCreateController($scope, $routeParams, contentTypeResource, iconHelper, $location, navigationService) {
-    contentTypeResource.getAllowedTypes($scope.currentNode.id).then(function (data) {
-        $scope.allowedTypes = iconHelper.formatContentTypeIcons(data);
-    });
+function contentCreateController($scope,
+  $routeParams,
+  contentTypeResource,
+  iconHelper,
+  $location,
+  navigationService,
+  blueprintConfig) {
+  contentTypeResource.getAllowedTypes($scope.currentNode.id).then(function(data) {
+    $scope.allowedTypes = iconHelper.formatContentTypeIcons(data);
+  });
 
-    $scope.selectContentType = true;
-    $scope.selectBlueprint = false;
+  $scope.selectContentType = true;
+  $scope.selectBlueprint = false;
 
-    $scope.createBlank = function (docType) {
-        $location
-            .path("/content/content/edit/" + $scope.currentNode.id)
-            .search("doctype=" + docType.alias + "&create=true");
-        navigationService.hideMenu();
+  $scope.createBlank = function(docType) {
+    $location
+      .path("/content/content/edit/" + $scope.currentNode.id)
+      .search("doctype=" + docType.alias + "&create=true");
+    navigationService.hideMenu();
+  };
+
+  $scope.createOrSelectBlueprintIfAny = function(docType) {
+    var blueprintIds = _.keys(docType.blueprints || {});
+    $scope.docType = docType;
+    if (blueprintIds.length) {
+      if (blueprintConfig.skipSelect) {
+        $scope.createFromBlueprint(blueprintIds[0]);
+      } else {
+        $scope.selectContentType = false;
+        $scope.selectBlueprint = true;
+      }
+    } else {
+      $scope.createBlank(docType);
     }
+  };
 
-    $scope.createOrSelectBlueprintIfAny = function (docType) {
-        if (docType.blueprints && _.keys(docType.blueprints).length) {
-            $scope.docType = docType;
-            $scope.selectContentType = false;
-            $scope.selectBlueprint = true;
-        } else {
-            $scope.createBlank(docType);
-        }
-    }
-
-    $scope.createFromBlueprint = function (blueprintId) {
-        $location
-            .path("/content/content/edit/" + $scope.currentNode.id)
-            .search(
-            "doctype=" + $scope.docType.alias +
-            "&create=true" +
-            "&blueprintId=" + blueprintId
-            );
-        navigationService.hideMenu();
-    }
+  $scope.createFromBlueprint = function(blueprintId) {
+    $location
+      .path("/content/content/edit/" + $scope.currentNode.id)
+      .search(
+        "doctype=" +
+        $scope.docType.alias +
+        "&create=true" +
+        "&blueprintId=" +
+        blueprintId
+      );
+    navigationService.hideMenu();
+  };
 }
 
-angular.module('umbraco').controller("Umbraco.Editors.Content.CreateController", contentCreateController);
+angular.module("umbraco").controller("Umbraco.Editors.Content.CreateController", contentCreateController);
+
+angular.module("umbraco").value("blueprintConfig", {
+  skipSelect: false
+});
