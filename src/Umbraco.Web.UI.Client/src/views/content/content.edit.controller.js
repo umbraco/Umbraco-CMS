584c3e4c199e1da44cffb8fe7f4c1c67c38e1991
Shannon <sdeminick@gmail.com>
2017-06-04 16:27:59 +0200

    adds parameters to the content editor directive

---
+++ src/Umbraco.Web.UI.Client/src/views/content/content.edit.controller.js
@@ -0,0 +1,20 @@
+/**
+ * @ngdoc controller
+ * @name Umbraco.Editors.Content.EditController
+ * @function
+ * 
+ * @description
+ * The controller for the content editor
+ */
+function ContentEditController($scope, $routeParams) {
+
+  $scope.contentId = $routeParams.id;
+  $scope.createOptions = null;
+  if ($routeParams.create && $routeParams.doctype) {
+    $scope.createOptions = {
+      docType: $routeParams.doctype
+    }
+  }
+}
+
+angular.module("umbraco").controller("Umbraco.Editors.Content.EditController", ContentEditController);
