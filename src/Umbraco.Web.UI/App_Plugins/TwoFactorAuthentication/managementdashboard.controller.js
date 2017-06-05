6387beb37d343c9adf72431915da66bea4ada227
Sebastiaan Janssen <sebastiaan@umbraco.com>
2017-06-05 15:45:03 +0200

    Adds the plugin files

---
+++ src/Umbraco.Web.UI/App_Plugins/TwoFactorAuthentication/managementdashboard.controller.js
@@ -0,0 +1,71 @@
+﻿angular.module("umbraco").controller("TwoFactorAuthentication.DashboardController",
+    function($scope, twoFactorService, userService) {
+
+        $scope.error2FA = "";
+        $scope.code = "";
+        $scope.enabledText = "disabled";
+        $scope.enabled = false;
+        $scope.qrCodeImageUrl = "";
+        $scope.secret = "";
+        $scope.email = "";
+        $scope.applicationName = "";
+        $scope.googleAuthEnabled = false;
+
+        twoFactorService.getGoogleAuthenticatorSetupCode().then(function (response) {
+            $scope.qrCodeImageUrl = response.data;
+            $scope.secret = response.data.Secret;
+            $scope.email = response.data.Email;
+            $scope.applicationName = response.data.ApplicationName;
+        });
+
+        userService.getCurrentUser().then(function (userData) {
+            twoFactorService.getEnabled(userData.id).then(function (response) {
+                
+                if (response.data.length !== 0) {
+                    $scope.enabledText = "enabled";
+                    $scope.enabled = true;
+                    for (var i = 0; i < response.data.length; i++) {
+                        var currentProvider = response.data[i];
+                        if (currentProvider.ApplicationName === "GoogleAuthenticator") {
+                            $scope.googleAuthEnabled = true;
+                            console.log($scope.googleAuthEnabled);
+                        }
+                    }
+                }
+            });
+        });
+
+        $scope.validateAndSaveGoogleAuth = function (code) {
+            $scope.error2FA = "";
+            twoFactorService.validateAndSaveGoogleAuth(code)
+                .then(function (response) {
+                    if (response.data === "true") {
+                        $scope.enabledText = "enabled";
+                        $scope.enabled = true;
+                        $scope.googleAuthEnabled = true;
+                    } else {
+                        $scope.error2FA = "Invalid code entered.";
+                    }
+                }, function () {
+                    $scope.error2FA = "Error validating code.";
+                });
+        };
+
+        $scope.disable = function () {
+            twoFactorService.disable()
+                .then(function (response) {
+                    if (response.data === "true") {
+                        $scope.enabledText = "disabled";
+                        $scope.enabled = false;
+                        $scope.googleAuthEnabled = false;
+
+                        twoFactorService.getGoogleAuthenticatorSetupCode().then(function (response) {
+                            $scope.qrCodeImageUrl = response.data;
+                            $scope.secret = response.data.Secret;
+                            $scope.email = response.data.Email;
+                            $scope.applicationName = response.data.ApplicationName;
+                        });
+                    }
+                });
+        };
+    });
---
+++ src/Umbraco.Web.UI/App_Plugins/TwoFactorAuthentication/managementdashboard.service.js
@@ -0,0 +1,19 @@
+﻿angular.module("umbraco.services").factory("twoFactorService", function ($http) {
+    return {
+        getEnabled: function (userId) {
+            return $http.get("/umbraco/backoffice/UmbracoApi/TwoFactorAuthentication/TwoFactorEnabled/?userId=" + userId);
+        },
+        getGoogleAuthenticatorSetupCode: function () {
+            return $http.get("/umbraco/backoffice/UmbracoApi/TwoFactorAuthentication/GoogleAuthenticatorSetupCode/");
+        },
+        validateAndSave: function (code) {
+            return $http.post("/umbraco/backoffice/UmbracoApi/TwoFactorAuthentication/ValidateAndSave/?code=" + code);
+        },
+        validateAndSaveGoogleAuth: function (code) {
+            return $http.post("/umbraco/backoffice/UmbracoApi/TwoFactorAuthentication/ValidateAndSaveGoogleAuth/?code=" + code);
+        },
+        disable: function () {
+            return $http.post("/umbraco/backoffice/UmbracoApi/TwoFactorAuthentication/Disable/");
+        }
+    };
+});
