57b037fda0a176907b50fe4cd18ae7f14b19a489
leekelleher <leekelleher@gmail.com>
2017-06-05 15:26:02 +0200

    Switched to a better GUID generation method
    Fixed issue with keys being generated on every publish rather than being remembered

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/nestedcontent/nestedcontent.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/nestedcontent/nestedcontent.controller.js
@@ -356,7 +356,7 @@ angular.module("umbraco").controller("Umbraco.PropertyEditors.NestedContent.Prop
         var initNode = function (scaffold, item) {
             var node = angular.copy(scaffold);
 
-            node.key = guid();
+            node.key = item && item.key ? item.key : UUID.generate();
             node.ncContentTypeAlias = scaffold.contentTypeAlias;
 
             for (var t = 0; t < node.tabs.length; t++) {
@@ -426,13 +426,22 @@ angular.module("umbraco").controller("Umbraco.PropertyEditors.NestedContent.Prop
             unsubscribe();
         });
 
-        var guid = function () {
-            function _p8(s) {
-                var p = (Math.random().toString(16) + "000000000").substr(2, 8);
-                return s ? "-" + p.substr(0, 4) + "-" + p.substr(4, 4) : p;
+        //TODO: Move this into a shared location?
+        var UUID = (function () {
+            var self = {};
+            var lut = []; for (var i = 0; i < 256; i++) { lut[i] = (i < 16 ? '0' : '') + (i).toString(16); }
+            self.generate = function () {
+                var d0 = Math.random() * 0xffffffff | 0;
+                var d1 = Math.random() * 0xffffffff | 0;
+                var d2 = Math.random() * 0xffffffff | 0;
+                var d3 = Math.random() * 0xffffffff | 0;
+                return lut[d0 & 0xff] + lut[d0 >> 8 & 0xff] + lut[d0 >> 16 & 0xff] + lut[d0 >> 24 & 0xff] + '-' +
+                  lut[d1 & 0xff] + lut[d1 >> 8 & 0xff] + '-' + lut[d1 >> 16 & 0x0f | 0x40] + lut[d1 >> 24 & 0xff] + '-' +
+                  lut[d2 & 0x3f | 0x80] + lut[d2 >> 8 & 0xff] + '-' + lut[d2 >> 16 & 0xff] + lut[d2 >> 24 & 0xff] +
+                  lut[d3 & 0xff] + lut[d3 >> 8 & 0xff] + lut[d3 >> 16 & 0xff] + lut[d3 >> 24 & 0xff];
             }
-            return _p8() + _p8(true) + _p8(true) + _p8();
-        };
+            return self;
+        })();
     }
 
 ]);
