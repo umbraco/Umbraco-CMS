8aed60a25d3654dc3c3a18c129ad30a05badbd2d
Per Ploug <per@umbraco.dk>
2017-06-05 23:10:45 +0200

    Core help view for the help drawer

---
+++ src/Umbraco.Web.UI.Client/src/views/dashboard/user-help/corehelp.controller.js
@@ -0,0 +1,54 @@
+﻿angular.module("umbraco").controller("Umbraco.Dashboards.Help.CoreHelpController", function ($scope, $routeParams, $timeout, eventsService, userService, helpService, localizationService) {
+
+    $scope.section = $routeParams.section;
+    $scope.tree = $routeParams.tree;
+
+    var stateKey = $scope.section + $scope.tree;
+
+    if (!$scope.section) {
+        $scope.section = "content";
+    }
+
+    userService.getCurrentUser().then(function (user) {
+
+        $scope.usertype = user.userType;
+        $scope.lang = user.locale;
+
+        evts.push(eventsService.on("appState.treeState.changed", function (e, args) {
+
+            $timeout(function () {
+                if ($scope.section !== $routeParams.section || $scope.tree !== $routeParams.tree) {
+                    $scope.section = $routeParams.section;
+                    $scope.tree = $routeParams.tree;
+
+                    findHelp($scope.section, $scope.tree, $scope.usertype, $scope.lang);
+                }
+            }, 500);
+            
+        }));
+
+        findHelp($scope.section, $scope.tree, $scope.usertype, $scope.lang);
+
+    });
+
+    var findHelp = function (section, tree, usertype, lang) {
+
+        helpService.getContextHelpForPage(section, tree, "http://localhost:24292").then(function (topics) {
+            $scope.topics = topics;
+        });
+
+    };
+
+    var evts = [];
+
+   
+    $scope.$on('$destroy', function () {
+        for (var e in evts) {
+            eventsService.unsubscribe(evts[e]);
+        }
+    });
+
+
+
+
+});
