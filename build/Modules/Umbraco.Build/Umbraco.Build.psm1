6e905b5693b8245227b948f9ff53f8cfcb4bf3c1
Stephan <stephane@umbraco.dk>
2017-06-12 09:41:49 +0200

    build - nuspec and cleanup

--- build/Modules/Umbraco.Build/Umbraco.Build.psm1
+++ build/Modules/Umbraco.Build/Umbraco.Build.psm1
@@ -1,9 +1,16 @@
 
-# build-module.psm1
+# Umbraco.Build.psm1
 #
-# import-module .\build-module.psm1
-# get-module
-# remove-module build-module
+# $env:PSModulePath = "$pwd\build\Modules\;$env:PSModulePath"
+# Import-Module Umbraco.Build -Force -DisableNameChecking
+#
+# PowerShell Modules:
+# https://msdn.microsoft.com/en-us/library/dd878324%28v=vs.85%29.aspx?f=255&MSPPError=-2147217396
+#
+# PowerShell Module Manifest:
+# https://msdn.microsoft.com/en-us/library/dd878337%28v=vs.85%29.aspx?f=255&MSPPError=-2147217396
+#
+
 
 # returns a string containing the hash of $file
 function genHash($file) 
@@ -41,13 +48,13 @@ function fullPath($file)
 # removes a directory, doesn't complain if it does not exist
 function rmrf($dir)
 {
-  remove-item $dir -force -recurse -errorAction SilentlyContinue
+  remove-item $dir -force -recurse -errorAction SilentlyContinue > $null
 }
 
 # removes a file, doesn't complain if it does not exist
 function rmf($file)
 {
-  remove-item $file -force -errorAction SilentlyContinue
+  remove-item $file -force -errorAction SilentlyContinue > $null
 }
 
 # copies a file, creates target dir if needed
@@ -129,7 +136,7 @@ function Get-UmbracoBuildEnv
   
   if (-not (test-path $path))
   {
-    mkdir $path
+    mkdir $path > $null
   }
 
   # ensure we have NuGet
@@ -156,8 +163,8 @@ function Get-UmbracoBuildEnv
   {
     write-host "Download 7-Zip..."
     &$nuget install 7-Zip.CommandLine -OutputDirectory $path -Verbosity quiet
-    $dir = ls $path\7-Zip.CommandLine.* | sort -property Name -descending | select -first 1
-    $file = ls -path $dir -name 7za.exe -recurse
+    $dir = ls "$path\7-Zip.CommandLine.*" | sort -property Name -descending | select -first 1
+    $file = ls -path "$dir" -name 7za.exe -recurse
     mv "$dir\$file" $sevenZip
     rmrf $dir
   }
@@ -172,8 +179,8 @@ function Get-UmbracoBuildEnv
   {
     write-host "Download VsWhere..."
     &$nuget install vswhere -OutputDirectory $path -Verbosity quiet
-    $dir = ls $path\vswhere.* | sort -property Name -descending | select -first 1
-    $file = ls -path $dir -name vswhere.exe -recurse
+    $dir = ls "$path\vswhere.*" | sort -property Name -descending | select -first 1
+    $file = ls -path "$dir" -name vswhere.exe -recurse
     mv "$dir\$file" $vswhere
     rmrf $dir
   }
@@ -188,7 +195,7 @@ function Get-UmbracoBuildEnv
   {
     write-host "Download Semver..."
     &$nuget install semver -OutputDirectory $path -Verbosity quiet
-    $dir = ls $path\semver.* | sort -property Name -descending | select -first 1
+    $dir = ls "$path\semver.*" | sort -property Name -descending | select -first 1
     $file = "$dir\lib\net452\Semver.dll"
     if (-not (test-path $file))
     {
@@ -197,6 +204,43 @@ function Get-UmbracoBuildEnv
     }
     mv "$file" $semver
     rmrf $dir
+  }
+  
+  # ensure we have node
+  $node = "$path\node-v6.9.1-win-x86"
+  $source = "http://nodejs.org/dist/v6.9.1/node-v6.9.1-win-x86.7z"
+  if (-not (test-path $node))
+  {
+  write-host $node
+    write-host "Download Node..."
+    $client = new-object Net.WebClient
+    $client.DownloadFile($source, "$path\node-v6.9.1-win-x86.7z")
+    &$sevenZip x "$path\node-v6.9.1-win-x86.7z" -o"$path" -aos > $nul
+    rmf "$path\node-v6.9.1-win-x86.7z"    
+  }
+  
+  # ensure we have npm
+  $npm = "$path\npm.*"
+  $getNpm = $true
+  if (test-path $npm)
+  {
+    $getNpm = $false
+    $tmpNpm = ls "$path\npm.*" | sort -property Name -descending | select -first 1
+    if ($tmpNpm.CreationTime -lt [DateTime]::Now.AddDays(-$cache))
+    {
+      $getNpm = $true
+    }
+    else
+    {
+      $npm = $tmpNpm.ToString()
+    }
+  }
+  if ($getNpm)
+  {
+    write-host "Download Npm..."
+    &$nuget install npm -OutputDirectory $path -Verbosity quiet
+    $npm = ls "$path\npm.*" | sort -property Name -descending | select -first 1
+    $npm = $npm.ToString()
   }
   
   # find visual studio
@@ -236,11 +280,13 @@ function Get-UmbracoBuildEnv
   
   $uenv = new-object -typeName PsObject
   $uenv | add-member -memberType NoteProperty -name SolutionRoot -value $solutionRoot
+  $uenv | add-member -memberType NoteProperty -name VisualStudio -value $vs
   $uenv | add-member -memberType NoteProperty -name NuGet -value $nuget
   $uenv | add-member -memberType NoteProperty -name Zip -value $sevenZip
   $uenv | add-member -memberType NoteProperty -name VsWhere -value $vswhere
   $uenv | add-member -memberType NoteProperty -name Semver -value $semver
-  $uenv | add-member -memberType NoteProperty -name VisualStudio -value $vs
+  $uenv | add-member -memberType NoteProperty -name NodePath -value $node
+  $uenv | add-member -memberType NoteProperty -name NpmPath -value $npm
   
   return $uenv
 }
@@ -381,15 +427,271 @@ function Get-UmbracoVersion()
   return $versions
 }
 
-function Build-Umbraco()
+function Build-Pre($uenv)
 {
-  write-host "so what?"
+  write-host "Pre-Compile"
+  
+  $src = "$($uenv.SolutionRoot)\src"
+  $tmp = "$($uenv.SolutionRoot)\build.tmp"
+  $out = "$($uenv.SolutionRoot)\build.out"
+
+  # clear
+  write-host "Clear folders and files"
+
+  rmrf "$src\Umbraco.Web.UI.Client\build"
+  rmrf "$src\Umbraco.Web.UI.Client\bower_components"
+
+  rmrf "$tmp"
+  mkdir "$tmp" > $null
+  
+  rmrf "$out"
+  mkdir "$out" > $null
+
+  # prepare
+  write-host "Making sure we have a web.config"
+
+  $webUi = "$src\Umbraco.Web.UI"
+  if (-not (test-path "$webUi\web.config"))
+  {
+    cpf "$webUi\web.Template.config" "$webUi\web.config"
+  }
 }
 
-function Test-UmbracoModule()
+function Build-Belle($uenv, $version)
 {
-  write-host "bam"
-  return 123
+  $tmp = "$($uenv.SolutionRoot)\build.tmp"
+
+  write-host "Build Belle (logging to $tmp\belle.log)"
+
+  push-location "$($uenv.SolutionRoot)\src\Umbraco.Web.UI.Client"
+  $p = $env:path
+  $env:path = $uenv.NpmPath + ";" + $uenv.NodePath + ";" + $env:path
+  
+  write "cache clean" > $tmp\belle.log
+  &npm cache clean --quiet >> $tmp\belle.log 2>&1
+  &npm install --quiet >> $tmp\belle.log 2>&1
+  &npm install -g grunt-cli --quiet >> $tmp\belle.log 2>&1
+  &npm install -g bower --quiet >> $tmp\belle.log 2>&1
+  &grunt build --buildversion=$version.Release >> $tmp\belle.log 2>&1
+  
+  # need to filter the log?
+  #get-content .\build.tmp\belle.log | %{ if ($_ -match "build") { write $_}}
+  
+  pop-location
+  $env:path = $p
+}
+
+function Build-Compile($uenv)
+{
+  $src = "$($uenv.SolutionRoot)\src"
+  $tmp = "$($uenv.SolutionRoot)\build.tmp"
+  $out = "$($uenv.SolutionRoot)\build.out"
+
+  $buildConfiguration = "Release"
+  
+  $toolsVersion = "4.0"
+  if ($uenv.VisualStudio.Major -eq 15)
+  {
+    $toolsVersion = "15.0"
+  }
+    
+  write-host "Compile (logging to $tmp\msbuild.log)"
+
+  # beware of the weird double \\ at the end of paths
+  # see http://edgylogic.com/blog/powershell-and-external-commands-done-right/
+  &$uenv.VisualStudio.MsBuild "$src\Umbraco.Web.UI\Umbraco.Web.UI.csproj" `
+    /p:WarningLevel=0 `
+    /p:Configuration=$buildConfiguration `
+    /p:UseWPP_CopyWebApplication=True `
+    /p:PipelineDependsOnBuild=False `
+    /p:OutDir=$tmp\bin\\ `
+    /p:WebProjectOutputDir=$tmp\WebApp\\ `
+    /p:Verbosity=minimal `
+    /t:Clean`;Rebuild `
+    /tv:$toolsVersion `
+    /p:UmbracoBuild=True `
+    > $tmp\msbuild.log
+    
+  # /p:UmbracoBuild tells the csproj that we are building from PS
+}
+
+function Build-Post($uenv)
+{
+  write-host "Post-Compile" 
+  
+  $src = "$($uenv.SolutionRoot)\src"
+  $tmp = "$($uenv.SolutionRoot)\build.tmp"
+  $out = "$($uenv.SolutionRoot)\build.out"
+
+  $buildConfiguration = "Release"
+
+  # fixme
+  #  at that point build.bat triggers build.proj
+  #  vso should build and run tests
+
+  # cleanup build
+  write "Clean build"
+
+  rmf "$tmp\bin\*.dll.config"
+  rmf "$tmp\WebApp\bin\*.dll.config"
+
+  # cleanup presentation
+  write "Cleanup presentation"
+
+  rmrf "$tmp\WebApp\umbraco.presentation"
+
+  # create directories
+  write "Create directories"
+
+  mkdir "$tmp\Configs" > $null
+  mkdir "$tmp\Configs\Lang" > $null
+  mkdir "$tmp\WebApp\App_Data" > $null
+  #mkdir "$tmp\WebApp\Media" > $null
+  #mkdir "$tmp\WebApp\Views" > $null
+
+  # copy various files
+  write "Copy xml documentation"
+
+  cp -force "$tmp\bin\*.xml" "$tmp\WebApp\bin"
+
+  write "Copy transformed configs and langs"
+
+  cprf "$tmp\WebApp\config" "*.config" "$tmp\Configs"
+  cprf "$tmp\WebApp\config" "*.js" "$tmp\Configs"
+  cprf "$tmp\WebApp\config\lang" "*.xml" "$tmp\Configs\Lang"
+  cpf "$tmp\WebApp\web.config" "$tmp\Configs\web.config.transform"
+
+  write "Copy transformed web.config"
+
+  cpf "$src\Umbraco.Web.UI\web.$buildConfiguration.Config.transformed" "$tmp\WebApp\web.config"
+
+  # offset the modified timestamps on all umbraco dlls, as WebResources
+  # break if date is in the future, which, due to timezone offsets can happen.
+  write "Offset dlls timestamps"
+  ls -r "$tmp\*.dll" | foreach {
+    $_.CreationTime = $_.CreationTime.AddHours(-11)
+    $_.LastWriteTime = $_.LastWriteTime.AddHours(-11)
+  }
+
+  # copy libs
+  write "Copy SqlCE libraries"
+
+  cprf "$src\packages\SqlServerCE.4.0.0.1" "*.*" "$tmp\bin" `
+    { -not $_.Extension.StartsWith(".nu") -and -not $_.RelativeName.StartsWith("lib\") }
+  cprf "$src\packages\SqlServerCE.4.0.0.1" "*.*" "$tmp\WebApp\bin" `
+    { -not $_.Extension.StartsWith(".nu") -and -not $_.RelativeName.StartsWith("lib\") }
+          
+  # copy Belle
+  write "Copy Belle"
+
+  cprf "$src\Umbraco.Web.UI.Client\build\belle" "*" "$tmp\WebApp\umbraco" `
+    { -not ($_.RelativeName -eq "index.html") }
+
+  # zip webapp
+  write "Zip WebApp"
+
+  &$uenv.Zip a -r "$out\UmbracoCms.AllBinaries.$version.zip" `
+    "$tmp\bin\*" `
+    -x!dotless.Core.dll `
+    > $null
+    
+  &$uenv.Zip a -r "$out\UmbracoCms.$version.zip" `
+    "$tmp\WebApp\*" `
+    -x!dotless.Core.dll -x!Content_Types.xml `
+    > $null
+
+  # prepare and zip WebPI
+  write "Zip WebPI"
+
+  rmrf "$tmp\WebPi"
+  mkdir "$tmp\WebPi" > $null
+  mkdir "$tmp\WebPi\umbraco" > $null
+
+  cprf "$tmp\WebApp" "*" "$tmp\WebPi\umbraco"
+  cprf "$src\WebPi" "*" "$tmp\WebPi"
+      
+  &$uenv.Zip a -r "$out\UmbracoCms.WebPI.$($version.Semver).zip" `
+    "$tmp\WebPi\*" `
+    -x!dotless.Core.dll `
+    > $null
+    
+  # clear
+  write "Delete build folder"
+  #rmrf "$tmp"
+
+  # hash the webpi file
+  write "Hash the WebPI file"
+
+  $hash = genHash "$out\UmbracoCms.WebPI.$($version.Semver).zip"
+  write $hash | out-file "$out\webpihash.txt" -encoding ascii
+
+  # setting node_modules folder to hidden to prevent VS13 from
+  # crashing on it while loading the websites project (still needed?)
+  # use get-item -force 'cos it might be hidden already
+  write "Set hidden attribute on node_modules"
+
+  $dir = get-item -force "$src\Umbraco.Web.UI.Client\node_modules"
+  $dir.Attributes = $dir.Attributes -bor ([System.IO.FileAttributes]::Hidden)
+
+  # add Web.config transform files to the NuGet package
+  write "Add web.config transforms to NuGet package"
+
+  mv "$tmp\WebApp\Views\Web.config" "$tmp\WebApp\Views\Web.config.transform"
+  # that one does not exist in .bat build either?
+  #mv "$tmp\WebApp\Xslt\Web.config" "$tmp\WebApp\Xslt\Web.config.transform"
+}
+
+function Build-NuGet($uenv)
+{
+  $src = "$($uenv.SolutionRoot)\src"
+  $tmp = "$($uenv.SolutionRoot)\build.tmp"
+  $out = "$($uenv.SolutionRoot)\build.out"
+  $nuspecs = "$($uenv.SolutionRoot)\build\NuSpecs"
+  
+  $version = Get-UmbracoVersion
+
+  write-host "Create NuGet packages"
+
+  # fixme - ugly references to ..\_BuildOutput in there?!!?
+  #-Properties tmp="$tmp" AND THEN use $tmp in nuspecs?
+  # see https://docs.microsoft.com/en-us/nuget/schema/nuspec
+  &$uenv.NuGet Pack "$nuspecs\UmbracoCms.Core.nuspec" `
+    -Properties BuildTmp="$tmp" `
+    -Version $version.Semver.ToString() `
+    -Symbols -Verbosity quiet -outputDirectory $out
+  &$uenv.NuGet Pack "$nuspecs\UmbracoCms.nuspec" `
+    -Properties BuildTmp="$tmp" `
+    -Version $version.Semver.ToString() `
+    -Verbosity quiet -outputDirectory $out
+}
+
+function Build-Umbraco($target)
+{
+  $uenv = Get-UmbracoBuildEnv
+  $version = Get-UmbracoVersion
+  
+  # set environment variables
+  #$env:UMBRACO_VERSION=$version.Semver.ToString()
+  #$env:UMBRACO_RELEASE=$version.Release
+  #$env:UMBRACO_COMMENT=$version.Comment
+  #$env:UMBRACO_BUILD=$version.Build
+
+  # set environment variable for VSO
+  # https://github.com/Microsoft/vsts-tasks/issues/375
+  # https://github.com/Microsoft/vsts-tasks/blob/master/docs/authoring/commands.md
+  if ($vso)
+  {
+    write-host ("##vso[task.setvariable variable=UMBRACO_VERSION;]$($version.Semver.ToString())")
+    write-host ("##vso[task.setvariable variable=UMBRACO_RELEASE;]$($version.Release)")
+    write-host ("##vso[task.setvariable variable=UMBRACO_COMMENT;]$($version.Comment)")
+    write-host ("##vso[task.setvariable variable=UMBRACO_BUILD;]$($version.Build)")
+  }
+  
+  Build-Pre $uenv
+  Build-Belle $uenv $version
+  Build-Compile $uenv
+  Build-Post $uenv
+  Build-NuGet $uenv
 }
 
 Export-ModuleMember -function Get-UmbracoBuildEnv
@@ -399,3 +701,9 @@ Export-ModuleMember -function Build-Umbraco
 
 # fixme
 # stop using relative paths, need $uenv.SolutionRoot
+# build-pre ok (test?)
+# build-belle ok (needs better error detection?)
+# build-compile
+#  imports .nuget targets wtf <<<<<<<<<<<<<<<
+# build-post ?
+# build-nuget ?
