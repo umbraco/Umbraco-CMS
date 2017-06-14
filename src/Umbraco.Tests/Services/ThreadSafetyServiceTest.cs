1ccceb6b4050991c47d2fc588ef23bbf059e8870
Stephan <stephane@umbraco.dk>
2017-06-14 21:05:01 +0200

    Build - deal with SqlCE timeouts

--- src/Umbraco.Tests/Services/ThreadSafetyServiceTest.cs
+++ src/Umbraco.Tests/Services/ThreadSafetyServiceTest.cs
@@ -5,7 +5,6 @@ using System.Linq;
 using System.Threading;
 using NUnit.Framework;
 using Umbraco.Core;
-using Umbraco.Core.Models;
 using Umbraco.Core.Services;
 using Umbraco.Tests.TestHelpers;
 using Umbraco.Tests.TestHelpers.Entities;
@@ -32,17 +31,23 @@ namespace Umbraco.Tests.Services
 			base.TearDown();
 		}
 
-		/// <summary>
+	    protected override string GetDbConnectionString()
+	    {
+            // need a longer timeout for tests?
+	        return base.GetDbConnectionString() + "default lock timeout=10000;";
+	    }
+
+	    /// <summary>
 		/// Used to track exceptions during multi-threaded tests, volatile so that it is not locked in CPU registers.
 		/// </summary>
-		private volatile Exception _error = null;
+		private volatile Exception _error;
 
 		private const int MaxThreadCount = 20;
 
 		[Test]
 		public void Ensure_All_Threads_Execute_Successfully_Content_Service()
 		{
-			//we will mimick the ServiceContext in that each repository in a service (i.e. ContentService) is a singleton
+			// the ServiceContext in that each repository in a service (i.e. ContentService) is a singleton
 			var contentService = (ContentService)ServiceContext.ContentService;
 
 			var threads = new List<Thread>();
@@ -57,24 +62,15 @@ namespace Umbraco.Tests.Services
 						{
                             Debug.WriteLine("[{0}] Running...", Thread.CurrentThread.ManagedThreadId);
 
-                            using (var scope = ApplicationContext.Current.ScopeProvider.CreateScope())
-						    {
-						        var database = scope.Database;
-                                Debug.WriteLine("[{0}] Database {1}.", Thread.CurrentThread.ManagedThreadId, database.InstanceSid);
-                            }
-
-                            //create 2 content items
-
-                            string name1 = "test" + Guid.NewGuid();
-							var content1 = contentService.CreateContent(name1, -1, "umbTextpage", 0);
-
+                            var name1 = "test-" + Guid.NewGuid();
+							var content1 = contentService.CreateContent(name1, -1, "umbTextpage");
 						    Debug.WriteLine("[{0}] Saving content #1.", Thread.CurrentThread.ManagedThreadId);
 							contentService.Save(content1);
 
-							Thread.Sleep(100); //quick pause for maximum overlap!
+							Thread.Sleep(100); // quick pause for maximum overlap
 
-                            string name2 = "test" + Guid.NewGuid();
-							var content2 = contentService.CreateContent(name2, -1, "umbTextpage", 0);
+                            var name2 = "test-" + Guid.NewGuid();
+							var content2 = contentService.CreateContent(name2, -1, "umbTextpage");
                             Debug.WriteLine("[{0}] Saving content #2.", Thread.CurrentThread.ManagedThreadId);
                             contentService.Save(content2);
 						}
@@ -86,33 +82,29 @@ namespace Umbraco.Tests.Services
 				threads.Add(t);
 			}
 
-			//start all threads
+			// start all threads
 			threads.ForEach(x => x.Start());
 
-			//wait for all to complete
+			// wait for all to complete
 			threads.ForEach(x => x.Join());
-
-			//kill them all
-			threads.ForEach(x => x.Abort());
 
 			if (_error == null)
 			{
-				//now look up all items, there should be 40!
+				// now look up all items, there should be 40!
 				var items = contentService.GetRootContent();
-				Assert.AreEqual(40, items.Count());
+				Assert.AreEqual(2 * MaxThreadCount, items.Count());
 			}
 			else
 			{
-			    throw new Exception("Error!", _error);
-			}
-
+			    Assert.Fail("ERROR! " + _error);
+            }
 		}
 
 		[Test]
 		public void Ensure_All_Threads_Execute_Successfully_Media_Service()
 		{
-			//we will mimick the ServiceContext in that each repository in a service (i.e. ContentService) is a singleton
-			var mediaService = (MediaService)ServiceContext.MediaService;
+			// mimick the ServiceContext in that each repository in a service (i.e. ContentService) is a singleton
+			var mediaService = (MediaService) ServiceContext.MediaService;
 
 			var threads = new List<Thread>();
 
@@ -126,25 +118,17 @@ namespace Umbraco.Tests.Services
 					{
                         Debug.WriteLine("[{0}] Running...", Thread.CurrentThread.ManagedThreadId);
 
-                        using (var scope = ApplicationContext.Current.ScopeProvider.CreateScope())
-                        {
-                            var database = scope.Database;
-                            Debug.WriteLine("[{0}] Database {1}.", Thread.CurrentThread.ManagedThreadId, database.InstanceSid);
-                        }
-
-                        //create 2 content items
-
-                        string name1 = "test" + Guid.NewGuid();
-					    var folder1 = mediaService.CreateMedia(name1, -1, Constants.Conventions.MediaTypes.Folder, 0);
+                        var name1 = "test-" + Guid.NewGuid();
+					    var media1 = mediaService.CreateMedia(name1, -1, Constants.Conventions.MediaTypes.Folder);
                         Debug.WriteLine("[{0}] Saving content #1.", Thread.CurrentThread.ManagedThreadId);
-                        mediaService.Save(folder1, 0);
+                        mediaService.Save(media1);
 
-						Thread.Sleep(100); //quick pause for maximum overlap!
+						Thread.Sleep(100); // quick pause for maximum overlap
 
-                        string name = "test" + Guid.NewGuid();
-                        var folder2 = mediaService.CreateMedia(name, -1, Constants.Conventions.MediaTypes.Folder, 0);
+                        var name2 = "test-" + Guid.NewGuid();
+                        var media2 = mediaService.CreateMedia(name2, -1, Constants.Conventions.MediaTypes.Folder);
                         Debug.WriteLine("[{0}] Saving content #2.", Thread.CurrentThread.ManagedThreadId);
-                        mediaService.Save(folder2, 0);
+                        mediaService.Save(media2);
 					}
 					catch (Exception e)
 					{
@@ -154,20 +138,17 @@ namespace Umbraco.Tests.Services
 				threads.Add(t);
 			}
 
-			//start all threads
+			// start all threads
 			threads.ForEach(x => x.Start());
 
-			//wait for all to complete
+			// wait for all to complete
 			threads.ForEach(x => x.Join());
-
-			//kill them all
-			threads.ForEach(x => x.Abort());
 
 			if (_error == null)
 			{
-				//now look up all items, there should be 40!
+				// now look up all items, there should be 40
 				var items = mediaService.GetRootMedia();
-				Assert.AreEqual(40, items.Count());
+				Assert.AreEqual(2 * MaxThreadCount, items.Count());
 			}
 			else
 			{
@@ -178,8 +159,8 @@ namespace Umbraco.Tests.Services
 
 		public void CreateTestData()
 		{
-			//Create and Save ContentType "umbTextpage" -> 1045
-			ContentType contentType = MockedContentTypes.CreateSimpleContentType("umbTextpage", "Textpage");
+			// Create and Save ContentType "umbTextpage" -> 1045
+			var contentType = MockedContentTypes.CreateSimpleContentType("umbTextpage", "Textpage");
 			contentType.Key = new Guid("1D3A8E6E-2EA9-4CC1-B229-1AEE19821522");
 			ServiceContext.ContentTypeService.Save(contentType);
 		}
