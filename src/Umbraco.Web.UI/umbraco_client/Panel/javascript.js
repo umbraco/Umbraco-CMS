8ee05063a2cd9adffbc0ade691254e681f0abb41
Niels Hartvig <nh@umbraco.dk>
2017-06-19 15:05:02 +0200

    More client files clean-up (including a duplicate tinymce)

--- src/Umbraco.Web.UI/umbraco_client/Panel/javascript.js
+++
@@ -1,9 +0,0 @@
-//OBSOLETE! THIS IS LEGACY CODE AND WILL BE REMOVED IN FUTURE VERSIONS!
-
-function resizePanel(PanelName, hasMenu, redoOnResize) {
-    //DO NOTHING
-}
-
-function resizePanelTo(PanelName, hasMenu, pWidth, pHeight) {
-    //DO NOTHING
-}
