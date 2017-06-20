cc41447ff79e7d9958a00384ad927daf9ce434b2
Mads Rasmussen <mads@umbraco.com>
2017-06-20 15:19:06 +0200

    prototype default permissions on edit user group

--- src/Umbraco.Web.UI.Client/src/views/users/group.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/users/group.controller.js
@@ -19,6 +19,7 @@
     vm.clearStartNode = clearStartNode;
     vm.getUserStateType = getUserStateType;
     vm.save = save;
+    vm.togglePermission = togglePermission;
     
     function init() {
 
@@ -42,6 +43,7 @@
           vm.userGroup = userGroup;
           setSectionIcon(vm.userGroup.sections);
           makeBreadcrumbs();
+                    fakePermissions();
           vm.loading = false;
         });
       }
@@ -190,6 +192,10 @@
       }
     }
 
+        function togglePermission(permission) {
+            permission.checked = !permission.checked;
+        }
+
     function makeBreadcrumbs() {
       vm.breadcrumbs = [
         {
@@ -199,6 +205,101 @@
         },
         {
           "name": vm.userGroup.name
+                }
+            ];
+        }
+
+        function fakePermissions() {
+            vm.userGroup.permissions = [
+                {
+                    "groupName": "Group 1",
+                    "permissions": [
+                        {
+                            "name": "Edit content (save)",
+                            "description": "Lorem ipsum dolor sit amet",
+                            "checked": false
+                        },
+                        {
+                            "name": "Browse content",
+                            "description": "Nullam egestas porta mi, quis finibus nisl commodo a",
+                            "checked": true
+                        },
+                        {
+                            "name": "Publish",
+                            "description": "Aliquam molestie consequat felis",
+                            "checked": true
+                        },
+                        {
+                            "name": "Send to publish",
+                            "description": "Sed pharetra sodales enim quis molestie",
+                            "checked": true
+                        },
+                        {
+                            "name": "Delete",
+                            "description": "Vitae porta mauris turpis sit amet ligula",
+                            "checked": true
+                        },
+                        {
+                            "name": "Create",
+                            "description": "Vestibulum pretium sapien id turpis elementum viverra",
+                            "checked": true
+                        },
+                    ]
+                },
+                {
+                    "groupName": "Group 2",
+                    "permissions": [
+                        {
+                            "name": "Move",
+                            "description": "Vestibulum pretium sapien id turpis elementum viverra",
+                            "checked": true
+                        },
+                        {
+                            "name": "Copy",
+                            "description": "Phasellus sagittis, dolor vel accumsan porttitor",
+                            "checked": false
+                        },
+                        {
+                            "name": "Sort",
+                            "description": "Aliquam erat volutpat",
+                            "checked": false
+                        }
+                    ]
+                },
+                {
+                    "groupName": "Group 3",
+                    "permissions": [
+                        {
+                            "name": "Culture and Hostnames",
+                            "description": "Lorem ipsum dolor sit amet",
+                            "checked": true
+                        },
+                        {
+                            "name": "Audit Trail",
+                            "description": "Lorem ipsum dolor sit amet",
+                            "checked": true
+                        },
+                        {
+                            "name": "Translate",
+                            "description": "Lorem ipsum dolor sit amet",
+                            "checked": true
+                        },
+                        {
+                            "name": "Change document type",
+                            "description": "Lorem ipsum dolor sit amet",
+                            "checked": true
+                        },
+                        {
+                            "name": "Public access",
+                            "description": "Lorem ipsum dolor sit amet",
+                            "checked": true
+                        },
+                        {
+                            "name": "Rollback",
+                            "description": "Lorem ipsum dolor sit amet",
+                            "checked": true
+                        }
+                    ]
         }
       ];
     }
