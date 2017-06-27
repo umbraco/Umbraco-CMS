687ccee1005b2f1e40ed2847b33f91c749ed69e5
Stephan <stephane@umbraco.dk>
2017-06-27 16:35:06 +0200

    Fix assembly version

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -2,13 +2,12 @@
 using System.Resources;
 
 [assembly: AssemblyCompany("Umbraco")]
-[assembly: AssemblyCopyright("Copyright © Umbraco 2016")]
+[assembly: AssemblyCopyright("Copyright © Umbraco 2017")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 [assembly: NeutralResourcesLanguage("en-US")]
 
-[assembly: AssemblyVersion("1.0.*")]
-
-[assembly: AssemblyFileVersion("8.0.0")]
+[assembly: AssemblyVersion("8.0.0")]
+[assembly: AssemblyFileVersion("8.0.0.20")]
 [assembly: AssemblyInformationalVersion("8.0.0-alpha0020")]
