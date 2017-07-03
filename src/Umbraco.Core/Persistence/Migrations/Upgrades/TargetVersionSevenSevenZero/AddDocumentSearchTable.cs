25e0f86f8bc8c0e06b48b66e828680be77ee6258
AndyButland <abutland73@gmail.com>
2017-07-03 08:46:15 +0200

    Added migrations for support of list view filter across properties

---
+++ src/Umbraco.Core/Persistence/Migrations/Upgrades/TargetVersionSevenSevenZero/AddDocumentSearchTable.cs
@@ -0,0 +1,35 @@
+﻿using System.Data;
+using System.Linq;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Persistence.SqlSyntax;
+
+namespace Umbraco.Core.Persistence.Migrations.Upgrades.TargetVersionSevenSevenZero
+{
+    [Migration("7.7.0", 100, Constants.System.UmbracoMigrationName)]
+    public class AddDocumentSearchTable : MigrationBase
+    {
+        public AddDocumentSearchTable(ISqlSyntaxProvider sqlSyntax, ILogger logger)
+            : base(sqlSyntax, logger)
+        { }
+
+        public override void Up()
+        {
+            var tables = SqlSyntax.GetTablesInSchema(Context.Database).ToArray();
+            if (tables.InvariantContains("cmsDocumentSearch") == false)
+            {
+                Create.Table("cmsDocumentSearch")
+                    .WithColumn("NodeId").AsInt32().PrimaryKey("PK_cmsDocumentSearch")
+                    .WithColumn("SearchText").AsString().NotNullable();
+
+                Create.ForeignKey("FK_cmsDocumentSearch_umbracoNode_id")
+                    .FromTable("cmsDocumentSearch").ForeignColumn("NodeId")
+                    .ToTable("umbracoNode").PrimaryColumn("id").OnDeleteOrUpdate(Rule.Cascade);
+            }
+        }
+
+        public override void Down()
+        {
+            // not implemented
+        }
+    }
+}
---
+++ src/Umbraco.Core/Persistence/Migrations/Upgrades/TargetVersionSevenSevenZero/AddSearchablePropertyTypeColumn.cs
@@ -0,0 +1,31 @@
+﻿using System.Data;
+using System.Linq;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Persistence.SqlSyntax;
+
+namespace Umbraco.Core.Persistence.Migrations.Upgrades.TargetVersionSevenSevenZero
+{
+    [Migration("7.7.0", 110, Constants.System.UmbracoMigrationName)]
+    public class AddSearchablePropertyTypeColumn : MigrationBase
+    {
+        public AddSearchablePropertyTypeColumn(ISqlSyntaxProvider sqlSyntax, ILogger logger)
+            : base(sqlSyntax, logger)
+        { }
+
+        public override void Up()
+        {
+            //Don't exeucte if the column is already there
+            var columns = SqlSyntax.GetColumnsInSchema(Context.Database).ToArray();
+
+            if (columns.Any(x => x.TableName.InvariantEquals("cmsPropertyType") && x.ColumnName.InvariantEquals("Searchable")) == false)
+            {
+                Create.Column("Searchable").OnTable("cmsPropertyType").AsBoolean().NotNullable().WithDefault(0);
+            }
+        }
+
+        public override void Down()
+        {
+            // not implemented
+        }
+    }
+}
