a3f4275b3097681ec3277ef2a97ed1fceb74e3be
Stephan <stephane@umbraco.dk>
2017-07-09 18:45:29 +0200

    U4-10006 - build docs

--- build/Modules/Umbraco.Build/Utilities.ps1
+++ build/Modules/Umbraco.Build/Utilities.ps1
@@ -81,3 +81,40 @@ function Replace-FileText($filename, $source, $replacement)
   $utf8bom = New-Object System.Text.UTF8Encoding $true
   [System.IO.File]::WriteAllText($filepath, $text, $utf8bom)
 }
+
+# store web.config
+function Store-WebConfig($webUi)
+{
+  if (test-path "$webUi\web.config")
+  {
+    if (test-path "$webUi\web.config.temp-build")
+    {
+      Write-Host "Found existing web.config.temp-build"
+      $i = 0
+      while (test-path "$webUi\web.config.temp-build.$i")
+      {
+        $i = $i + 1
+      }
+      Write-Host "Save existing web.config as web.config.temp-build.$i"
+      Write-Host "(WARN: the original web.config.temp-build will be restored during post-build)"
+      mv "$webUi\web.config" "$webUi\web.config.temp-build.$i"
+    }
+    else
+    {
+      Write-Host "Save existing web.config as web.config.temp-build"
+      Write-Host "(will be restored during post-build)"
+      mv "$webUi\web.config" "$webUi\web.config.temp-build"
+    }
+  }
+}
+
+# restore web.config
+function Restore-WebConfig($webUi)
+{
+  if (test-path "$webUi\web.config.temp-build")
+  {
+    Write-Host "Restoring existing web.config"
+    Remove-File "$webUi\web.config"
+    mv "$webUi\web.config.temp-build" "$webUi\web.config"
+  }
+}
