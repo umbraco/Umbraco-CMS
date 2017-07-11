cb4e1d324d8bb681691e2879e64df885b2e6d334
Shannon <sdeminick@gmail.com>
2017-07-11 12:18:12 +1000

    Removes the NoCredentials enum (we don't use it or need it)

--- src/Umbraco.Core/Models/Membership/UserState.cs
+++ src/Umbraco.Core/Models/Membership/UserState.cs
@@ -9,14 +9,6 @@
         Active = 0,
         Disabled = 1,
         LockedOut = 2,
-        Invited = 3,
-
-        /// <summary>
-        /// Occurs when the user has been created (not invited) and has no credentials assigned
-        /// </summary>
-        /// <remarks>
-        /// This state shouldn't really exist or occur
-        /// </remarks>
-        NoCredentials = 100
+        Invited = 3
     }
 }
