40a62c677916bcb8745e038c69a268b920c73a6d
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-07-21 08:52:48 +0200

    Added angular dictionary resource

---
+++ src/Umbraco.Web.UI.Client/src/common/resources/dictionary.resource.js
@@ -0,0 +1,11 @@
+﻿/**
+    * @ngdoc service
+    * @name umbraco.resources.dictionaryResource
+    * @description Loads in data for dictionary items
+**/
+function dictionaryResource($q, $http, umbDataFormatter, umbRequestHelper) {
+  return {
+  };
+}
+
+angular.module("umbraco.resources").factory("dictionaryResource", dictionaryResource);
