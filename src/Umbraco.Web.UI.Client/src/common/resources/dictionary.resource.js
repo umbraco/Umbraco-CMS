64b301f2118cb634c9ae7147928f27da28d6c23c
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-08-04 08:23:06 +0200

    Added delete by id method to dictionary resource

--- src/Umbraco.Web.UI.Client/src/common/resources/dictionary.resource.js
+++ src/Umbraco.Web.UI.Client/src/common/resources/dictionary.resource.js
@@ -3,9 +3,45 @@
     * @name umbraco.resources.dictionaryResource
     * @description Loads in data for dictionary items
 **/
-function dictionaryResource($q, $http, umbDataFormatter, umbRequestHelper) {
-  return {
+function dictionaryResource($q, $http, umbRequestHelper) {
+
+  /**
+         * @ngdoc method
+         * @name umbraco.resources.dictionaryResource#deleteById
+         * @methodOf umbraco.resources.dictionaryResource
+         *
+         * @description
+         * Deletes a dictionary item with a given id
+         *
+         * ##usage
+         * <pre>
+         * dictionaryResource.deleteById(1234)
+         *    .then(function() {
+         *        alert('its gone!');
+         *    });
+         * </pre>
+         *
+         * @param {Int} id id of dictionary item to delete
+         * @returns {Promise} resourcePromise object.
+         *
+  **/
+  function deleteById(id) {
+    return umbRequestHelper.resourcePromise(
+      $http.post(
+        umbRequestHelper.getApiUrl(
+          "dictionaryApiBaseUrl",
+          "DeleteById",
+          [{ id: id }])),
+      "Failed to delete item " + id);
+  }
+
+  var resource = {
+    deleteById: deleteById
   };
+
+  return resource;
+
+  
 }
 
 angular.module("umbraco.resources").factory("dictionaryResource", dictionaryResource);
