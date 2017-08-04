7f058d2c495cff73300dfeb1dd1359c73abaa85f
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-08-04 08:36:52 +0200

    Added dictionary delelte controller and added cancel method

---
+++ src/Umbraco.Web.UI.Client/src/views/dictionary/dictionary.delete.controller.js
@@ -0,0 +1,19 @@
+﻿/**
+ * @ngdoc controller
+ * @name Umbraco.Editors.Dictionary.DeleteController
+ * @function
+ * 
+ * @description
+ * The controller for deleting dictionary items
+ */
+function DictionaryDeleteController($scope, dictionaryResource, treeService, navigationService) {
+  vm = this;
+
+  function cancel() {
+    navigationService.hideDialog();
+  }
+
+  vm.cancel = cancel;
+}
+
+angular.module("umbraco").controller("Umbraco.Editors.Dictionary.DeleteController", DictionaryDeleteController);
