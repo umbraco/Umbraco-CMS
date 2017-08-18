9ac95e43280fbc5772a5415588680b000665052a
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-08-18 13:15:41 +0200

    Added detele function to angular controller

--- src/Umbraco.Web.UI.Client/src/views/dictionary/dictionary.delete.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/dictionary/dictionary.delete.controller.js
@@ -13,7 +13,36 @@ function DictionaryDeleteController($scope, dictionaryResource, treeService, nav
     navigationService.hideDialog();
   }
 
+  function performDelete() {
+    // stop from firing again on double-click
+    if ($scope.busy) { return false; }
+
+    //mark it for deletion (used in the UI)
+    $scope.currentNode.loading = true;
+    $scope.busy = true;
+
+    dictionaryResource.deleteById($scope.currentNode.id).then(function () {
+      $scope.currentNode.loading = false;
+      
+      treeService.removeNode($scope.currentNode);
+
+      //if the current edited item is the same one as we're deleting, we need to navigate elsewhere
+      if (editorState.current && editorState.current.id === $scope.currentNode.id) {
+
+        //If the deleted item lived at the root then just redirect back to the root, otherwise redirect to the item's parent
+        var location = "/settings";
+        if ($scope.currentNode.parentId.toString() !== "-1")
+          location = "/settings/current/edit/" + $scope.currentNode.parentId;
+
+        $location.path(location);
+      }
+
+      navigationService.hideMenu();
+    });
+  }
+
   vm.cancel = cancel;
+  vm.performDelete = performDelete;
 }
 
 angular.module("umbraco").controller("Umbraco.Editors.Dictionary.DeleteController", DictionaryDeleteController);
