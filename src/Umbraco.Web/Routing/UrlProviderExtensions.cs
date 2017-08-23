d980ffe7837f3f9779d43fd46f290fa1c4cafd7a
Lennard Fonteijn <lennard@lennardf1989.com>
2017-08-23 23:14:49 +0200

    Added possibilty to ignore collisions from an IContentFinder

--- src/Umbraco.Web/Routing/UrlProviderExtensions.cs
+++ src/Umbraco.Web/Routing/UrlProviderExtensions.cs
@@ -78,7 +78,7 @@ namespace Umbraco.Web.Routing
                 {
                     urls.Add(ui.Text("content", "routeError", "(error)", umbracoContext.Security.CurrentUser));
                 }
-                else if (pcr.PublishedContent.Id != content.Id)
+                else if (pcr.IgnorePublishedContentCollisions == false && pcr.PublishedContent.Id != content.Id)
                 {
                     var o = pcr.PublishedContent;
                     string s;
