45eeddd9bbfdea9c2432246fc0d681208528801e
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-08-25 08:35:08 +0200

    Added emtpy edit view and angular controller

---
+++ src/Umbraco.Web.UI.Client/src/views/dictionary/dictionairy.edit.controller.js
@@ -0,0 +1,15 @@
+﻿/**
+ * @ngdoc controller
+ * @name Umbraco.Editors.Dictionary.EditController
+ * @function
+ * 
+ * @description
+ * The controller for editing dictionary items
+ */
+function DictionaryEditController($scope, dictionaryResource, treeService, navigationService) {
+  vm = this;
+
+  
+}
+
+angular.module("umbraco").controller("Umbraco.Editors.Dictionary.EditController", DictionaryEditController);
