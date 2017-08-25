8c9588f680bbd580ddf1932a675bb2cec1feeccb
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-08-25 08:44:47 +0200

    Added dictoinairy angular create controller and view

---
+++ src/Umbraco.Web.UI.Client/src/views/dictionary/dictionary.create.controller.js
@@ -0,0 +1,15 @@
+﻿/**
+ * @ngdoc controller
+ * @name Umbraco.Editors.Dictionary.CreateController
+ * @function
+ * 
+ * @description
+ * The controller for creating dictionary items
+ */
+function DictionaryCreateController($scope, dictionaryResource, treeService, navigationService) {
+  vm = this;
+
+
+}
+
+angular.module("umbraco").controller("Umbraco.Editors.Dictionary.CreateController", DictionaryCreateController);
