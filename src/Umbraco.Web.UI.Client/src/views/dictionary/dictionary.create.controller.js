35a547cc4eaa50b8d3a29c93f3041ef2cf486081
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-08-30 08:24:27 +0200

    Handle success state of creating dictionary item

--- src/Umbraco.Web.UI.Client/src/views/dictionary/dictionary.create.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/dictionary/dictionary.create.controller.js
@@ -6,16 +6,29 @@
  * @description
  * The controller for creating dictionary items
  */
-function DictionaryCreateController($scope, dictionaryResource, treeService, navigationService, notificationsService) {
+function DictionaryCreateController($scope, $location, dictionaryResource, navigationService, notificationsService, formHelper) {
   vm = this;
 
   vm.itemKey = '';
 
-  function createItem() {    
+  function createItem() {
 
-    dictionaryResource.create($scope.dialogOptions.currentNode.id, vm.itemKey).then(function(data) {
-      console.log(data);
+    var node = $scope.dialogOptions.currentNode;
+
+    dictionaryResource.create(node.id, vm.itemKey).then(function(data) {      
       navigationService.hideMenu();
+
+      // set new item as active in tree
+      var currPath = node.path ? node.path : "-1";
+      navigationService.syncTree({ tree: "dictionary", path: currPath + "," + data, forceReload: true, activate: true });
+
+      // reset form state
+      formHelper.resetForm({ scope: $scope });
+
+      // navigate to edit view
+      $location.path("/settings/dictionary/edit/" + data);
+
+      
     },function(err) {
       if (err.data && err.data.message) {
         notificationsService.error(err.data.message);
