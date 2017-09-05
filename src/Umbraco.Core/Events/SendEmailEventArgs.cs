2590ee51ec7541866fd0c67d95684d4519a3e729
Shannon <sdeminick@gmail.com>
2017-09-05 19:20:33 +1000

    cleans up event args and makes internal

--- src/Umbraco.Core/Events/SendEmailEventArgs.cs
+++ src/Umbraco.Core/Events/SendEmailEventArgs.cs
@@ -3,25 +3,13 @@ using System.Net.Mail;
 
 namespace Umbraco.Core.Events
 {
-    public class SendEmailEventArgs : EventArgs
+    internal class SendEmailEventArgs : EventArgs
     {
-        public SmtpClient Smtp { get; private set; }
         public MailMessage Message { get; private set; }
-
-        ///// <summary>
-        ///// A flag indicating that the mail sending was handled by an event handler
-        ///// </summary>
-        //public bool SendingHandledExternally { get; set; }
 
         public SendEmailEventArgs(MailMessage message)
         {
             Message = message;
-        }
-
-        public SendEmailEventArgs(SmtpClient smtp, MailMessage message)
-        {
-            Smtp = smtp;
-            Message = message;
-        }
+        }        
     }
 }
