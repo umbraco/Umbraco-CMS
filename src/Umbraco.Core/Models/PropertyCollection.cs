598944d3d83753d2e2051a0bdbe3edf50ddb39e2
Claus <claus@umbraco.com>
2017-09-07 13:13:03 +0200

    ensuring id is only set if unset.

--- src/Umbraco.Core/Models/PropertyCollection.cs
+++ src/Umbraco.Core/Models/PropertyCollection.cs
@@ -93,9 +93,12 @@ namespace Umbraco.Core.Models
                         //NOTE: Consider checking type before value is set: item.PropertyType.DataTypeId == property.PropertyType.DataTypeId
                         //Transfer the existing value to the new property
                         var property = this[key];
-                        if (item.Value == null && property.Value != null)
+                        if (item.Id == 0 && property.Id != 0)
                         {
                             item.Id = property.Id;
+                        }
+                        if (item.Value == null && property.Value != null)
+                        {
                             item.Value = property.Value;
                         }
 
