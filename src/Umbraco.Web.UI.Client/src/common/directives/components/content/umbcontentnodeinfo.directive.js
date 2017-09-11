c6937618c20241d93735b938a4150c4458f8d5f4
Mads Rasmussen <mads@umbraco.com>
2017-09-11 20:02:46 +0200

    fixes: U4-10405 When scheduled publishing it set, change the default button to "Save and schedule"

--- src/Umbraco.Web.UI.Client/src/common/directives/components/content/umbcontentnodeinfo.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/content/umbcontentnodeinfo.directive.js
@@ -1,7 +1,7 @@
 (function () {
     'use strict';
 
-    function ContentNodeInfoDirective($timeout, $location, logResource) {
+    function ContentNodeInfoDirective($timeout, $location, logResource, eventsService) {
 
         function link(scope, element, attrs, ctrl) {
             
@@ -167,6 +167,10 @@
                     });
                 });
 
+                // emit event
+                var args = { node: scope.node, date: date };
+                eventsService.emit("editors.content.changePublishDate", args);
+
             }
 
             function clearPublishDate() {
@@ -182,6 +186,10 @@
                         }
                     });
                 });
+
+                // emit event
+                var args = { node: scope.node, date: null };
+                eventsService.emit("editors.content.changePublishDate", args);
 
             }
 
@@ -199,6 +207,10 @@
                     });
                 });
 
+                // emit event
+                var args = { node: scope.node, date: date };
+                eventsService.emit("editors.content.changeUnpublishDate", args);
+
             }
 
             function clearUnpublishDate() {
@@ -214,6 +226,10 @@
                         }
                     });
                 });
+
+                // emit event
+                var args = { node: scope.node, date: null };
+                eventsService.emit("editors.content.changeUnpublishDate", args);
 
             }
 
