c5d874464a9e2ae9d3b423ed172b764d37a46576
Shannon <sdeminick@gmail.com>
2017-10-12 17:11:45 +1100

    Fixes PostAddFolder

---
+++ src/Umbraco.Web/Models/ContentEditing/PostedFolder.cs
@@ -0,0 +1,17 @@
+using System.Runtime.Serialization;
+
+namespace Umbraco.Web.Models.ContentEditing
+{
+    /// <summary>
+    /// Used to create a folder with the MediaController
+    /// </summary>
+    [DataContract]
+    public class PostedFolder
+    {
+        [DataMember(Name = "parentId")]
+        public string ParentId { get; set; }
+
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+    }
+}
