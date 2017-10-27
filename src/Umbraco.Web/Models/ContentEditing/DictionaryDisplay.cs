e4127a9ff81484d52b0eea0c127190d905e000bc
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-10-27 08:49:21 +0200

    U4-5863 added basic mapping for dictionary display model

--- src/Umbraco.Web/Models/ContentEditing/DictionaryDisplay.cs
+++ src/Umbraco.Web/Models/ContentEditing/DictionaryDisplay.cs
@@ -1,9 +1,25 @@
 ﻿namespace Umbraco.Web.Models.ContentEditing
 {
+    using System.Collections.Generic;
     using System.Runtime.Serialization;
 
+    /// <summary>
+    /// The dictionary display model
+    /// </summary>
     [DataContract(Name = "dictionary", Namespace = "")]
-    public class DictionaryDisplay : EntityBasic
+    public class DictionaryDisplay : EntityBasic, INotificationModel
     {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DictionaryDisplay"/> class.
+        /// </summary>
+        public DictionaryDisplay()
+        {
+            this.Notifications = new List<Notification>();
+        }
+
+        /// <summary>
+        /// This is used to add custom localized messages/strings to the response for the app to use for localized UI purposes.
+        /// </summary>
+        public List<Notification> Notifications { get; private set; }
     }
 }
---
+++ src/Umbraco.Web/Models/Mapping/DictionaryModelMapper.cs
@@ -0,0 +1,26 @@
+﻿namespace Umbraco.Web.Models.Mapping
+{
+    using AutoMapper;
+
+    using Umbraco.Core;
+    using Umbraco.Core.Models;
+    using Umbraco.Core.Models.Mapping;
+    using Umbraco.Web.Models.ContentEditing;
+
+    /// <summary>
+    /// The dictionary model mapper.
+    /// </summary>
+    internal class DictionaryModelMapper : MapperConfiguration
+    {
+        public override void ConfigureMappings(IConfiguration config, ApplicationContext applicationContext)
+        {
+            config.CreateMap<IDictionaryItem, DictionaryDisplay>()
+                .ForMember(
+                    x => x.Udi,
+                    expression => expression.MapFrom(
+                        content => Udi.Create(Constants.UdiEntityType.DictionaryItem, content.Key))).ForMember(
+                    x => x.Name,
+                    expression => expression.MapFrom(content => content.ItemKey));
+        }
+    }
+}
