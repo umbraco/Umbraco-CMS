5d20a8e07fef73ea89f7f291f0dee34fe2d37d7c
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-10-27 08:42:08 +0200

    U4-5863 added dictionary display model

---
+++ src/Umbraco.Web/Models/ContentEditing/DictionaryDisplay.cs
@@ -0,0 +1,9 @@
+﻿namespace Umbraco.Web.Models.ContentEditing
+{
+    using System.Runtime.Serialization;
+
+    [DataContract(Name = "dictionary", Namespace = "")]
+    public class DictionaryDisplay : EntityBasic
+    {
+    }
+}
