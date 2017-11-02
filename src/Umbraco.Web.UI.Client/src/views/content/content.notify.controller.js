eacb168682e62461c5344c602b5cd37e10bf539a
Mike <mike.masey@yoyodesign.com>
2017-11-02 17:23:16 +0000

    Update notification setting to use angular.

---
+++ src/Umbraco.Web.UI.Client/src/views/content/content.notify.controller.js
@@ -0,0 +1,65 @@
+﻿(function () {
+
+  function CreateNotifyController(
+    $scope,
+    contentResource,
+    notificationsService,
+    navigationService,
+    localizationService,
+	angularHelper) {
+
+  	var vm = this;
+  	var currentForm;
+
+  	vm.notifyOptions = [];
+  	vm.save = save;
+
+    var successText = {};
+
+  	function onInit() {
+  		vm.loading = true;
+  		contentResource.getNotifySettingsById($scope.currentNode.id).then(function (options) {
+  			currentForm = angularHelper.getCurrentForm($scope);
+  			vm.loading = false;
+  			vm.notifyOptions = options;
+  		});
+  	}
+
+    $scope.message = {
+      name: $scope.currentNode.name
+    };
+
+
+    $scope.cancel = function () {
+      navigationService.hideMenu();
+    };
+
+
+    function save(notifyOptions) {
+    	vm.saveState = "busy";
+    	vm.saveError = false;
+    	vm.saveSuccces = false;
+
+    	var selectedSting = "";
+
+    	angular.forEach(notifyOptions, function (option) {
+    		if (option.checked === true) {
+    			selectedSting += option.permissionCode;
+    		}
+    	})
+
+    	contentResource.setNotifySettingsById($scope.currentNode.id, selectedSting).then(function () {
+    		vm.saveState = "success";
+    		vm.saveSuccces = true;
+    	}, function(error){
+    		vm.saveState = "error";
+    		vm.saveError = error;
+    	});
+    }
+
+    onInit();
+  }
+
+  angular.module("umbraco").controller("Umbraco.Editors.Content.CreateNotifyController", CreateNotifyController);
+
+}());
--- src/Umbraco.Web/Editors/ContentController.cs
+++ src/Umbraco.Web/Editors/ContentController.cs
@@ -23,10 +23,13 @@ using Umbraco.Web.Mvc;
 using Umbraco.Web.WebApi;
 using Umbraco.Web.WebApi.Binders;
 using Umbraco.Web.WebApi.Filters;
+using umbraco.cms.businesslogic;
 using umbraco.cms.businesslogic.web;
 using umbraco.presentation.preview;
 using Umbraco.Core.Events;
 using Constants = Umbraco.Core.Constants;
+using System.Collections;
+using umbraco;
 
 namespace Umbraco.Web.Editors
 {
@@ -1092,5 +1095,47 @@ namespace Umbraco.Web.Editors
             return allowed;
         }
 
-    }
+		[EnsureUserPermissionForContent("contentId", 'R')]
+		public List<Permission> GetNotificationOptions(int contentId)
+		{
+			List<Permission> notifications = new List<Permission>();
+
+			if (contentId <= 0) throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+			var content = Services.ContentService.GetById(contentId);
+			if (content == null) throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+
+			var node = new CMSNode(contentId);
+
+			ArrayList actionList = global::umbraco.BusinessLogic.Actions.Action.GetAll();
+
+			foreach (global::umbraco.interfaces.IAction a in actionList)
+			{
+				if (a.ShowInNotifier)
+				{
+
+					Permission n = new Permission
+					{
+						Name = ui.Text("actions", a.Alias),
+						Checked = (UmbracoUser.GetNotifications(node.Path).IndexOf(a.Letter) > -1),
+						PermissionCode = a.Letter.ToString()
+					};
+
+					notifications.Add(n);
+				}
+			}
+
+			return notifications;
+		}
+
+		public void SetNotificationOptions(int contentId, string notifyOptions = "")
+		{
+			if (contentId <= 0) throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+			var content = Services.ContentService.GetById(contentId);
+			if (content == null) throw new HttpResponseException(Request.CreateResponse(HttpStatusCode.NotFound));
+
+			var node = new CMSNode(contentId);
+
+			global::umbraco.cms.businesslogic.workflow.Notification.UpdateNotifications(UmbracoUser, node, notifyOptions ?? "");
+		}
+	}
 }
