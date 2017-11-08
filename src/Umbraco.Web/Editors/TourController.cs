b9b1fc3a70e0e941fb9e64b32ad99c3e2d1a88d9
Shannon <sdeminick@gmail.com>
2017-11-09 00:57:05 +1100

    Adds a global config switch to enable/disable tours entirely

--- src/Umbraco.Web/Editors/TourController.cs
+++ src/Umbraco.Web/Editors/TourController.cs
@@ -2,6 +2,7 @@
 using System.IO;
 using System.Linq;
 using Newtonsoft.Json.Linq;
+using Umbraco.Core.Configuration;
 using Umbraco.Core.IO;
 using Umbraco.Web.Mvc;
 using Umbraco.Web.WebApi.Filters;
@@ -19,6 +20,10 @@ namespace Umbraco.Web.Editors
             //TODO: Add error checking to this for final release!
 
             var result = new JArray();
+
+            if (UmbracoConfig.For.UmbracoSettings().BackOffice.Tours.EnableTours == false)
+                return result;
+
             var tourFiles = Directory.GetFiles(
                     Path.Combine(IOHelper.MapPath(SystemDirectories.Config), "BackOfficeTours"), "*.json")
                 .OrderBy(x => x, StringComparer.InvariantCultureIgnoreCase);
