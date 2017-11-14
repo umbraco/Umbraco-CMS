5982a868ea6bd8f870d2d69a21d22ad30180dab1
Robert <robe2006@edu.eal.dk>
2017-11-14 12:01:11 +0100

    Added custom RollingFileAppender and changed rollingStyle to "size"
    OpenFile method has been overiden, this means that everytime a log will be opened, it will check the last access time of all existing logs, and delete the ones that haven't been accessed in more than 1 month.
    
    Changing the rollingStyle to "size" will make sure that logs will roll once the maximumFileSize reaches its set limit.

---
+++ src/Umbraco.Core/Logging/CustomRollingFileAppender.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.IO;
+using log4net.Appender;
+
+namespace Umbraco.Core.Logging
+{
+    public class CustomRollingFileAppender: RollingFileAppender
+    {
+        /// <summary>
+        /// This ovveride will delete logs older than 30 days
+        /// </summary>
+        /// <param name="fileName"></param>
+        /// <param name="append"></param>
+        protected override void OpenFile(string fileName, bool append)
+        {
+            var logFolder = Path.GetDirectoryName(fileName);
+
+            if (logFolder != null)
+            {
+                string[] logFiles = Directory.GetFiles(logFolder);
+
+                foreach (var logFile in logFiles)
+                {
+                    DateTime lastAccessTime = System.IO.File.GetLastAccessTime(logFile);
+
+                    if (lastAccessTime < DateTime.Now.AddMonths(-1))
+                    {
+                        System.IO.File.Delete(logFile);
+                    }
+                }
+            }
+
+            base.OpenFile(fileName, append);
+        }
+
+    }
+}
