41b857d89c786cfe453a204e1ff2f2a4139fe8e7
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-12-05 14:17:54 +0100

    U4-5863 added view and controller for overview

---
+++ src/Umbraco.Web.UI.Client/src/views/dictionary/dictionary.overview.controller.js
@@ -0,0 +1,16 @@
+﻿/**
+ * @ngdoc controller
+ * @name Umbraco.Editors.Dictionary.DictionaryOverviewController
+ * @function
+ * 
+ * @description
+ * The controller for listting dictionary items
+ */
+function DictionaryOverviewController($scope, $location, dictionaryResource) {
+    vm = this;
+
+    vm.loading = true;
+}
+
+
+angular.module("umbraco").controller("Umbraco.Editors.Dictionary.OverviewController", DictionaryOverviewController);
