c6c3e14641208b1ef4cc54f8b93eef7443c890d7
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2017-12-12 13:05:12 +0100

    U4-5683 added models for dictionary overview

---
+++ src/Umbraco.Web/Models/ContentEditing/DictionaryOverviewDisplay.cs
@@ -0,0 +1,38 @@
+﻿namespace Umbraco.Web.Models.ContentEditing
+{
+    using System.Collections.Generic;
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// The dictionary overview display.
+    /// </summary>
+    [DataContract(Name = "dictionary", Namespace = "")]
+    public class DictionaryOverviewDisplay
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="DictionaryOverviewDisplay"/> class.
+        /// </summary>
+        public DictionaryOverviewDisplay()
+        {
+            this.Translations = new List<DictionaryOverviewTranslationDisplay>();
+        }
+
+        /// <summary>
+        /// Gets or sets the key.
+        /// </summary>
+        [DataMember(Name = "key")]
+        public string Key { get; set; }
+
+        /// <summary>
+        /// Gets or sets the id.
+        /// </summary>
+        [DataMember(Name = "id")]
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Gets or sets the translations.
+        /// </summary>
+        [DataMember(Name = "translations")]
+        public IEnumerable<DictionaryOverviewTranslationDisplay> Translations { get; set; }
+    }
+}
---
+++ src/Umbraco.Web/Models/ContentEditing/DictionaryOverviewTranslationDisplay.cs
@@ -0,0 +1,23 @@
+﻿namespace Umbraco.Web.Models.ContentEditing
+{
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// The dictionary translation overview display.
+    /// </summary>
+    [DataContract(Name = "dictionaryTranslation", Namespace = "")]
+    public class DictionaryOverviewTranslationDisplay
+    {
+        /// <summary>
+        /// Gets or sets the display name.
+        /// </summary>
+        [DataMember(Name = "displayName")]
+        public string DisplayName { get; set; }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether has translation.
+        /// </summary>
+        [DataMember(Name = "hasTranslation")]
+        public bool HasTranslation { get; set; }
+    }
+}
