c3be9004f6086e7f10e346630e3d544be166774e
Shannon <sdeminick@gmail.com>
2018-01-10 16:01:46 +1100

    U4-10811 Store custom tours as json and load from App_Plugins/{MyPlugin}/backoffice/tours

--- src/Umbraco.Web/Editors/TourController.cs
+++ src/Umbraco.Web/Editors/TourController.cs
@@ -1,9 +1,13 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text.RegularExpressions;
+using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Umbraco.Core.Configuration;
 using Umbraco.Core.IO;
+using Umbraco.Web.Models;
 using Umbraco.Web.Mvc;
 using Umbraco.Web.WebApi.Filters;
 using Constants = Umbraco.Core.Constants;
@@ -11,29 +15,54 @@ using Constants = Umbraco.Core.Constants;
 namespace Umbraco.Web.Editors
 {
     [PluginController("UmbracoApi")]
-    [UmbracoApplicationAuthorize(Constants.Applications.Content)]   
+    [UmbracoApplicationAuthorize(Constants.Applications.Content)]
     public class TourController : UmbracoAuthorizedJsonController
     {
-        //TODO: Strongly type this for final release!
-        public JArray GetTours()
+        public IEnumerable<BackOfficeTourFile> GetTours()
         {
-            //TODO: Add error checking to this for final release!
-
-            var result = new JArray();
+            var result = new List<BackOfficeTourFile>();
 
             if (UmbracoConfig.For.UmbracoSettings().BackOffice.Tours.EnableTours == false)
                 return result;
 
-            var tourFiles = Directory.GetFiles(
-                    Path.Combine(IOHelper.MapPath(SystemDirectories.Config), "BackOfficeTours"), "*.json")
-                .OrderBy(x => x, StringComparer.InvariantCultureIgnoreCase);
-            foreach (var tourFile in tourFiles)
+            var coreTourFiles = Directory.GetFiles(
+                Path.Combine(IOHelper.MapPath(SystemDirectories.Config), "BackOfficeTours"), "*.json");
+
+            foreach (var tourFile in coreTourFiles)
             {
                 var contents = File.ReadAllText(tourFile);
-                result.Add(JArray.Parse(contents));
+
+                result.Add(new BackOfficeTourFile
+                {
+                    FileName = Path.GetFileNameWithoutExtension(tourFile),
+                    Tours = JsonConvert.DeserializeObject<BackOfficeTour[]>(contents)
+                });
             }
 
-            return result;
+            //collect all tour files in packges
+            foreach (var plugin in Directory.EnumerateDirectories(IOHelper.MapPath(SystemDirectories.AppPlugins)))
+            {
+                var pluginName = Path.GetFileName(plugin.TrimEnd('\\'));
+
+                foreach (var backofficeDir in Directory.EnumerateDirectories(plugin, "backoffice"))
+                {
+                    foreach (var tourDir in Directory.EnumerateDirectories(backofficeDir, "tours"))
+                    {
+                        foreach (var tourFile in Directory.EnumerateFiles(tourDir, "*.json"))
+                        {
+                            var contents = File.ReadAllText(tourFile);
+                            result.Add(new BackOfficeTourFile
+                            {
+                                FileName = Path.GetFileNameWithoutExtension(tourFile),
+                                PluginName = pluginName,
+                                Tours = JsonConvert.DeserializeObject<BackOfficeTour[]>(contents)
+                            });
+                        }
+                    }
+                }
+            }
+
+            return result.OrderBy(x => x.FileName, StringComparer.InvariantCultureIgnoreCase);
         }
     }
 }
