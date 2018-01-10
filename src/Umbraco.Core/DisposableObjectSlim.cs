d0d4885812c2bc77798e7f689a03dc9a59f54d12
Robert <robertcopilau@gmail.com>
2018-01-10 13:36:53 +0100

    Created slim version of DisposableObject and fixed some code indentation
    DisposableObjectSlim has no finalizer

---
+++ src/Umbraco.Core/DisposableObjectSlim.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Umbraco.Core
+{
+    public abstract class DisposableObjectSlim : IDisposable
+    {
+        private bool _disposed;
+        private readonly object _locko = new object();
+
+        // gets a value indicating whether this instance is disposed.
+        // for internal tests only (not thread safe)
+        //TODO make this internal + rename "Disposed" when we can break compatibility
+        public bool IsDisposed { get { return _disposed; } }
+
+        // implements IDisposable
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        //TODO make this private, non-virtual when we can break compatibility
+        protected virtual void Dispose(bool disposing)
+        {
+            lock (_locko)
+            {
+                if (_disposed) return;
+                _disposed = true;
+            }
+
+            DisposeUnmanagedResources();
+
+            if (disposing)
+                DisposeResources();
+        }
+
+        protected abstract void DisposeResources();
+
+        protected virtual void DisposeUnmanagedResources()
+        { }
+    }
+}
