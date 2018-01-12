59d75277c29fbdc35483a29c523166f4e969d4a4
Claus <claus@umbraco.com>
2018-01-12 09:52:52 +0100

    fixing up helpers to match exact strings as this is what people will be using. If they need more advanced regex matches they should register by a regex instead.

--- src/Umbraco.Web/TourFilterResolver.cs
+++ src/Umbraco.Web/TourFilterResolver.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text.RegularExpressions;
+using Umbraco.Core;
 using Umbraco.Core.Logging;
 using Umbraco.Core.ObjectResolution;
 using Umbraco.Web.Models;
@@ -35,31 +36,34 @@ namespace Umbraco.Web
         }
 
         /// <summary>
-        /// Helper method for adding a filter by plugin name
+        /// Helper method for adding a filter by exact plugin name
         /// </summary>
-        /// <param name="pluginName"></param>
+        /// <param name="pluginName">Regex string used for matching</param>
         public void AddFilterByPlugin(string pluginName)
         {
+            pluginName = pluginName.EnsureStartsWith("^").EnsureEndsWith("$");
             using (Resolution.Configuration)
                 _instances.Add(BackOfficeTourFilter.FilterPlugin(new Regex(pluginName, RegexOptions.IgnoreCase)));
         }
 
         /// <summary>
-        /// Helper method for adding a filter by file name
+        /// Helper method for adding a filter by exact file name
         /// </summary>
         /// <param name="file"></param>
         public void AddFilterByFile(string file)
         {
+            file = file.EnsureStartsWith("^").EnsureEndsWith("$");
             using (Resolution.Configuration)
                 _instances.Add(BackOfficeTourFilter.FilterFile(new Regex(file, RegexOptions.IgnoreCase)));
         }
 
         /// <summary>
-        /// Helper method for adding a filter by tour alias
+        /// Helper method for adding a filter by exact tour alias
         /// </summary>
         /// <param name="alias"></param>
         public void AddFilterByAlias(string alias)
         {
+            alias = alias.EnsureStartsWith("^").EnsureEndsWith("$");
             using (Resolution.Configuration)
                 _instances.Add(BackOfficeTourFilter.FilterAlias(new Regex(alias, RegexOptions.IgnoreCase)));
         }
