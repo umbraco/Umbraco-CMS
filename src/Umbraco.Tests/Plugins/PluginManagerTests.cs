4a0cf81d7dadff559f9f7b17c6dd10857b608e0a
Shannon <sdeminick@gmail.com>
2018-01-15 18:19:23 +1100

    fixes test

--- src/Umbraco.Tests/Plugins/PluginManagerTests.cs
+++ src/Umbraco.Tests/Plugins/PluginManagerTests.cs
@@ -286,7 +286,7 @@ AnotherContentFinder
         {
             var trees = _manager.ResolveAttributedTrees();
             // commit 6c5e35ec2cbfa31be6790d1228e0c2faf5f55bc8 brings the count down to 14
-            Assert.AreEqual(8, trees.Count());
+            Assert.AreEqual(6, trees.Count());
         }
 
         [Test]
