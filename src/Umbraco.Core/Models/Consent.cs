1a289be99901089ee420b14f3cddc693efe18f55
Sebastiaan Jansssen <sebastiaan@umbraco.com>
2018-02-01 13:09:06 +0100

    Whoops, I see what you did there, Consent can be internal indeed, cool!

--- src/Umbraco.Core/Models/Consent.cs
+++ src/Umbraco.Core/Models/Consent.cs
@@ -11,7 +11,7 @@ namespace Umbraco.Core.Models
     /// </summary>
     [Serializable]
     [DataContract(IsReference = true)]
-    public class Consent : Entity, IConsent
+    internal class Consent : Entity, IConsent
     {
         private static PropertySelectors _selector;
 
