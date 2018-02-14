4d5557fb782fb5ac4471d6338d8f196fb0430417
jamiepollock <jamie.does.umbraco@gmail.com>
2018-02-13 20:30:29 -0500

    Added in CheckTable expression & builder

---
+++ src/Umbraco.Core/Persistence/Migrations/Syntax/Check/Expressions/CheckTableExpression.cs
@@ -0,0 +1,14 @@
+﻿using Umbraco.Core.Persistence.SqlSyntax;
+
+namespace Umbraco.Core.Persistence.Migrations.Syntax.Check.Expressions
+{
+    public class CheckTableExpression : MigrationExpressionBase
+    {
+        public CheckTableExpression(DatabaseProviders current, DatabaseProviders[] databaseProviders, ISqlSyntaxProvider sqlSyntax) : base(current, databaseProviders, sqlSyntax)
+        {
+        }
+
+        public virtual string SchemaName { get; set; }
+        public virtual string TableName { get; set; }
+    }
+}
---
+++ src/Umbraco.Core/Persistence/Migrations/Syntax/Check/Table/CheckTableBuilder.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using Umbraco.Core.Persistence.Migrations.Syntax.Check.Column;
+using Umbraco.Core.Persistence.Migrations.Syntax.Check.Expressions;
+using Umbraco.Core.Persistence.SqlSyntax;
+
+namespace Umbraco.Core.Persistence.Migrations.Syntax.Check.Table
+{
+    public class CheckTableBuilder : ExpressionBuilderBase<CheckTableExpression>, ICheckTableOptionSyntax
+    {
+        private readonly IMigrationContext _context;
+        private readonly ISqlSyntaxProvider _sqlSyntax;
+
+        public CheckTableBuilder(IMigrationContext context, ISqlSyntaxProvider sqlSyntax, CheckTableExpression expression)
+            : base(expression)
+        {
+            _context = context;
+            _sqlSyntax = sqlSyntax;
+        }
+
+        public ICheckColumnOptionSyntax Column(string columnName)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Exists()
+        {
+            return _sqlSyntax.DoesTableExist(_context.Database, Expression.TableName);
+        }
+    }
+}
