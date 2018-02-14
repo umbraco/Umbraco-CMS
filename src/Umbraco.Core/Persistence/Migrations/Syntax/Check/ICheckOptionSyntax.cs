002227574680d0169861cbd2e7fd1f3ebd341e62
jamiepollock <jamie.does.umbraco@gmail.com>
2018-02-13 22:45:11 -0500

    Adding a ICheckOptionSyntax that all Check Exists() interfaces inherit

---
+++ src/Umbraco.Core/Persistence/Migrations/Syntax/Check/ICheckOptionSyntax.cs
@@ -0,0 +1,14 @@
+﻿namespace Umbraco.Core.Persistence.Migrations.Syntax.Check
+{
+    /// <summary>
+    /// The underlying interface that ensures an entity can evaluate it exists.
+    /// </summary>
+    public interface ICheckOptionSyntax : IFluentSyntax
+    {
+        /// <summary>
+        /// Evalutes the current chain to see if it exists.
+        /// </summary>
+        /// <returns>Whether it exists or not.</returns>
+        bool Exists();
+    }
+}
