a2799d3bb4f9456513ccbff7bff49223c362b9af
jamiepollock <jamie.does.umbraco@gmail.com>
2018-02-13 20:53:43 -0500

    Added CheckColumn Expression & Builder

---
+++ src/Umbraco.Core/Persistence/Migrations/Syntax/Check/Column/CheckColumnBuilder.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Umbraco.Core.Persistence.Migrations.Syntax.Check.Expressions;
+using Umbraco.Core.Persistence.SqlSyntax;
+
+namespace Umbraco.Core.Persistence.Migrations.Syntax.Check.Column
+{
+    public class CheckColumnBuilder : ExpressionBuilderBase<CheckColumnExpression>, ICheckColumnOptionSyntax
+    {
+        private readonly IMigrationContext _context;
+        private readonly ISqlSyntaxProvider _sqlSyntax;
+
+        public CheckColumnBuilder(IMigrationContext context, ISqlSyntaxProvider sqlSyntax, CheckColumnExpression expression) : base(expression)
+        {
+            _context = context;
+            _sqlSyntax = sqlSyntax;
+        }
+
+        public bool Exists()
+        {
+            return _sqlSyntax.GetColumnsInSchema(_context.Database).Any(x => x.TableName.InvariantEquals(Expression.TableName)
+                                                                          && x.ColumnName.InvariantEquals(Expression.ColumnName));
+        }
+    }
+}
---
+++ src/Umbraco.Core/Persistence/Migrations/Syntax/Check/Expressions/CheckColumnExpression.cs
@@ -0,0 +1,14 @@
+﻿using Umbraco.Core.Persistence.SqlSyntax;
+
+namespace Umbraco.Core.Persistence.Migrations.Syntax.Check.Expressions
+{
+    public class CheckColumnExpression : MigrationExpressionBase
+    {
+        public CheckColumnExpression(DatabaseProviders current, DatabaseProviders[] databaseProviders, ISqlSyntaxProvider sqlSyntax) : base(current, databaseProviders, sqlSyntax)
+        {
+        }
+
+        public string TableName { get; set; }
+        public string ColumnName { get; set; }
+    }
+}
