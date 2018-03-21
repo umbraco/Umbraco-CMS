49a9a249d017cb71e6e1dfdf2daada407eaaff9e
Claus <claus@umbraco.com>
2018-03-21 14:35:43 +0100

    removes using statements.

--- src/Umbraco.Web/Trees/LanguageTreeController.cs
+++ src/Umbraco.Web/Trees/LanguageTreeController.cs
@@ -1,14 +1,7 @@
-using System;
-using System.Globalization;
 using System.Net.Http.Formatting;
-using Umbraco.Core;
-using Umbraco.Core.Services;
-using Umbraco.Core.Models;
-using Umbraco.Core.Models.Entities;
 using Umbraco.Web.Models.Trees;
 using Umbraco.Web.Mvc;
 using Umbraco.Web.WebApi.Filters;
-using Umbraco.Web._Legacy.Actions;
 using Constants = Umbraco.Core.Constants;
 
 namespace Umbraco.Web.Trees
