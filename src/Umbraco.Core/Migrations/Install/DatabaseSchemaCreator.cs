df312309a306bc5cc4bb5ff34ee9b8fdc31d9c9b
Shannon <sdeminick@gmail.com>
2018-03-21 16:53:21 +1100

    ensure the key/value table is created

--- src/Umbraco.Core/Migrations/Install/DatabaseSchemaCreator.cs
+++ src/Umbraco.Core/Migrations/Install/DatabaseSchemaCreator.cs
@@ -91,7 +91,8 @@ namespace Umbraco.Core.Migrations.Install
             {53, typeof (UserGroup2AppDto) },
             {54, typeof (UserStartNodeDto) },
             {55, typeof (ContentNuDto) },
-            {56, typeof (DocumentVersionDto) }
+            {56, typeof (DocumentVersionDto) },
+            {57, typeof (KeyValueDto) }
         };
 
         /// <summary>
