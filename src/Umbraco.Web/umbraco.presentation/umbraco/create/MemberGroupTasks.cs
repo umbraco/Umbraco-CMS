0d2df4de811fbbd5760e0ef70adf0abcf8da326c
Robert <robertcopilau@gmail.com>
2018-03-21 10:22:08 +0100

    Some cleanup

--- src/Umbraco.Web/umbraco.presentation/umbraco/create/MemberGroupTasks.cs
+++ src/Umbraco.Web/umbraco.presentation/umbraco/create/MemberGroupTasks.cs
@@ -1,24 +1,17 @@
-using System;
-using System.Data;
 using System.Linq;
 using System.Web.Security;
-using Umbraco.Web.UI;
 using umbraco.BusinessLogic;
-using umbraco.DataLayer;
-using umbraco.BasePages;
-using Umbraco.Core.IO;
 using umbraco.cms.businesslogic.member;
-using Umbraco.Core;
-using Umbraco.Web;
+using Umbraco.Web.UI;
 
-namespace umbraco
+namespace Umbraco.Web.umbraco.presentation.umbraco.create
 {
     public class MemberGroupTasks : LegacyDialogTask
     {
         public override bool PerformSave()
         {
             Roles.CreateRole(Alias);
-            _returnUrl = string.Format("members/EditMemberGroup.aspx?id={0}", System.Web.HttpContext.Current.Server.UrlEncode(Alias));
+            _returnUrl = $"members/EditMemberGroup.aspx?id={System.Web.HttpContext.Current.Server.UrlEncode(Alias)}";
             return true;
         }
 
@@ -29,7 +22,7 @@ namespace umbraco
             // only built-in roles can be deleted
             if (Member.IsUsingUmbracoRoles())
             {
-                MemberGroup.GetByName(Alias).delete();
+                Roles.DeleteRole(Alias);
                 roleDeleted = true;
             }
 
@@ -54,14 +47,8 @@ namespace umbraco
 
         private string _returnUrl = "";
 
-        public override string ReturnUrl
-        {
-            get { return _returnUrl; }
-        }
+        public override string ReturnUrl => _returnUrl;
 
-        public override string AssignedApp
-        {
-            get { return DefaultApps.member.ToString(); }
-        }
+        public override string AssignedApp => DefaultApps.member.ToString();
     }
 }
