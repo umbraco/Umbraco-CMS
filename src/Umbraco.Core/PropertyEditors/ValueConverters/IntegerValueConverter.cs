601726993096e1bc808dd6d5f44cfc427f2e5804
Sebastiaan Janssen <sebastiaan@umbraco.com>
2018-03-21 19:06:43 +0100

    Simplify: use TryConvertTo

--- src/Umbraco.Core/PropertyEditors/ValueConverters/IntegerValueConverter.cs
+++ src/Umbraco.Core/PropertyEditors/ValueConverters/IntegerValueConverter.cs
@@ -15,23 +15,7 @@ namespace Umbraco.Core.PropertyEditors.ValueConverters
 
         public override object ConvertDataToSource(PublishedPropertyType propertyType, object source, bool preview)
         {
-            if (source == null)
-                return default(int);
-
-            // in the database an integer is an integer
-            if (source is int)
-                return source;
-
-            if (source is long)
-                return Convert.ToInt32(source);
-
-            // in XML an integer is a string
-            int i;
-            if (source is string && int.TryParse((string)source, out i))
-                return i;
-
-            // default value is zero
-            return default(int);
+            return source.TryConvertTo<int>().Result;
         }
     }
 }
