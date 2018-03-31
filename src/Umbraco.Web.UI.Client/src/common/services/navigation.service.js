f6263aad5bb41f61c534b1e385abdc87e59b4115
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-03-31 15:09:30 +0200

    U4-11167 wrap syncTree method to handle rare occasions where it is not defined yet

--- src/Umbraco.Web.UI.Client/src/common/services/navigation.service.js
+++ src/Umbraco.Web.UI.Client/src/common/services/navigation.service.js
@@ -288,9 +288,12 @@ function navigationService($rootScope, $routeParams, $log, $location, $q, $timeo
                 throw "args.tree cannot be null";
             }
 
-            if (mainTreeEventHandler) {
-                //returns a promise
-                return mainTreeEventHandler.syncTree(args);
+            if (mainTreeEventHandler) {                
+                //returns a promise, but wrap it in a time out,
+                // on some occasions the syncTree method is not defined yet in the umb - tree directive(U4 - 11167)
+                $timeout(function () {                   
+                    return mainTreeEventHandler.syncTree(args);
+                }, 50, false);                
             }
 
             //couldn't sync
