21a8395142446e8b0f0ca7c90e99fc16177e869a
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-04-18 10:21:12 +0200

    U4-11216 Multiple Content Picker allows only one item when used as macro parameter  (#2593)
    U4-11216 set min and max number when used as parameter editor
    
    (cherry picked from commit f25233ac2b97603d633d88a4fd01dafb57f22f62)

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/contentpicker/contentpicker.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/contentpicker/contentpicker.controller.js
@@ -58,7 +58,7 @@ function contentPickerController($scope, entityResource, editorState, iconHelper
 
     $scope.renderModel = [];
 	    
-    $scope.dialogEditor = editorState && editorState.current && editorState.current.isDialogEditor === true;
+    $scope.dialogEditor = editorState && editorState.current && editorState.current.isDialogEditor === true;    
 
     //the default pre-values
     var defaultConfig = {
@@ -93,7 +93,7 @@ function contentPickerController($scope, entityResource, editorState, iconHelper
     $scope.model.config.showOpenButton = ($scope.model.config.showOpenButton === "1" ? true : false);
     $scope.model.config.showEditButton = ($scope.model.config.showEditButton === "1" ? true : false);
     $scope.model.config.showPathOnHover = ($scope.model.config.showPathOnHover === "1" ? true : false);
- 
+    
     var entityType = $scope.model.config.startNode.type === "member"
         ? "Member"
         : $scope.model.config.startNode.type === "media"
