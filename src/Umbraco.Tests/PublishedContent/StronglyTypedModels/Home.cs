90499db47b95ce3d9f0eed5b6f97abdc23549e0c
Dan Booth <dan@ph-creative.com>
2018-05-11 10:20:23 +0100

    Added unit tests to test that the FirstChild<T> method correctly returns a strongly typed model of type T. This necessitated creating a new strongly typed model called Home in tests that inherits from TypedModelBase

---
+++ src/Umbraco.Tests/PublishedContent/StronglyTypedModels/Home.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Web;
+using Umbraco.Core.Models;
+
+namespace Umbraco.Tests.PublishedContent.StronglyTypedModels
+{
+    /// <summary>
+    /// Used for testing strongly-typed published content extensions that work against the <see cref="PublishedContentTests"/>
+    /// </summary>
+    public class Home : TypedModelBase
+    {
+        public Home(IPublishedContent publishedContent) : base(publishedContent)
+        {
+        }
+    }
+}
