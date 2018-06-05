aa582658acfc40d125178d7b3b78836d4c70a2d2
Stephan <stephane@umbraco.dk>
2018-06-05 11:44:35 +0200

    Fix tags

--- src/Umbraco.Core/PropertyEditors/TagConfiguration.cs
+++ src/Umbraco.Core/PropertyEditors/TagConfiguration.cs
@@ -7,12 +7,15 @@ namespace Umbraco.Core.PropertyEditors
     /// </summary>
     public class TagConfiguration
     {
-        // no field attribute, all defined in the editor, due to validators
-
+        [ConfigurationField("group", "Tag group", "requiredfield",
+            Description = "Define a tag group")]
         public string Group { get; set; } = "default";
 
+        [ConfigurationField("storageType", "Storage Type", "views/propertyeditors/tags/tags.prevalues.html",
+            Description = "Select whether to store the tags in cache as CSV (default) or as JSON. The only benefits of storage as JSON is that you are able to have commas in a tag value but this will require parsing the json in your views or using a property value converter")]
         public TagsStorageType StorageType { get; set; } = TagsStorageType.Csv;
 
+        // not a field
         public char Delimiter { get; set; }
     }
 }
