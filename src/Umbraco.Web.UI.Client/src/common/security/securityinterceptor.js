02c02a3abee406302dd841e36b91d881c5e7574c
Mads Rasmussen <mads@umbraco.com>
2018-06-12 14:17:03 +0200

    update responseError in security interceptor

--- src/Umbraco.Web.UI.Client/src/common/security/securityinterceptor.js
+++ src/Umbraco.Web.UI.Client/src/common/security/securityinterceptor.js
@@ -45,19 +45,16 @@ angular.module('umbraco.security.interceptor')
             },
         
             'responseError': function(rejection) {
-               
-                console.log('response error rejection', rejection);
 
-                /*
                 // Intercept failed requests
                 // Make sure we have the configuration of the request (don't we always?)
-                var config = originalResponse.config ? originalResponse.config : {};
+                var config = rejection.config ? rejection.config : {};
 
                 // Make sure we have an object for the headers of the request
                 var headers = config.headers ? config.headers : {};
 
                 //Here we'll check if we should ignore the error (either based on the original header set or the request configuration)
-                if (headers["x-umb-ignore-error"] === "ignore" || config.umbIgnoreErrors === true || (angular.isArray(config.umbIgnoreStatus) && config.umbIgnoreStatus.indexOf(originalResponse.status) !== -1)) {
+                if (headers["x-umb-ignore-error"] === "ignore" || config.umbIgnoreErrors === true || (angular.isArray(config.umbIgnoreStatus) && config.umbIgnoreStatus.indexOf(rejection.status) !== -1)) {
                     //exit/ignore
                     return promise;
                 }
@@ -69,7 +66,7 @@ angular.module('umbraco.security.interceptor')
                 }
 
                 //A 401 means that the user is not logged in
-                if (originalResponse.status === 401 && !originalResponse.config.url.endsWith("umbraco/backoffice/UmbracoApi/Authentication/GetCurrentUser")) {
+                if (rejection.status === 401 && !rejection.config.url.endsWith("umbraco/backoffice/UmbracoApi/Authentication/GetCurrentUser")) {
 
                     var userService = $injector.get('userService'); // see above
 
@@ -80,21 +77,21 @@ angular.module('umbraco.security.interceptor')
                             //The request bounced because it was not authorized - add a new request to the retry queue
                             return queue.pushRetryFn('unauthorized-server', userName, function retryRequest() {
                                 // We must use $injector to get the $http service to prevent circular dependency
-                                return $injector.get('$http')(originalResponse.config);
+                                return $injector.get('$http')(rejection.config);
                             });
                         });
                 }
-                else if (originalResponse.status === 404) {
+                else if (rejection.status === 404) {
 
                     //a 404 indicates that the request was not found - this could be due to a non existing url, or it could
                     //be due to accessing a url with a parameter that doesn't exist, either way we should notifiy the user about it
 
-                    var errMsg = "The URL returned a 404 (not found): <br/><i>" + originalResponse.config.url.split('?')[0] + "</i>";
-                    if (originalResponse.data && originalResponse.data.ExceptionMessage) {
-                        errMsg += "<br/> with error: <br/><i>" + originalResponse.data.ExceptionMessage + "</i>";
+                    var errMsg = "The URL returned a 404 (not found): <br/><i>" + rejection.config.url.split('?')[0] + "</i>";
+                    if (rejection.data && rejection.data.ExceptionMessage) {
+                        errMsg += "<br/> with error: <br/><i>" + rejection.data.ExceptionMessage + "</i>";
                     }
-                    if (originalResponse.config.data) {
-                        errMsg += "<br/> with data: <br/><i>" + angular.toJson(originalResponse.config.data) + "</i><br/>Contact your administrator for information.";
+                    if (rejection.config.data) {
+                        errMsg += "<br/> with data: <br/><i>" + angular.toJson(rejection.config.data) + "</i><br/>Contact your administrator for information.";
                     }
 
                     notifications.error(
@@ -102,7 +99,7 @@ angular.module('umbraco.security.interceptor')
                         errMsg);
 
                 }
-                else if (originalResponse.status === 403) {
+                else if (rejection.status === 403) {
                     //if the status was a 403 it means the user didn't have permission to do what the request was trying to do.
                     //How do we deal with this now, need to tell the user somehow that they don't have permission to do the thing that was
                     //requested. We can either deal with this globally here, or we can deal with it globally for individual requests on the umbRequestHelper,
@@ -112,16 +109,13 @@ angular.module('umbraco.security.interceptor')
 
                     //It was decided to just put these messages into the normal status messages.
 
-                    var msg = "Unauthorized access to URL: <br/><i>" + originalResponse.config.url.split('?')[0] + "</i>";
-                    if (originalResponse.config.data) {
-                        msg += "<br/> with data: <br/><i>" + angular.toJson(originalResponse.config.data) + "</i><br/>Contact your administrator for information.";
+                    var msg = "Unauthorized access to URL: <br/><i>" + rejection.config.url.split('?')[0] + "</i>";
+                    if (rejection.config.data) {
+                        msg += "<br/> with data: <br/><i>" + angular.toJson(rejection.config.data) + "</i><br/>Contact your administrator for information.";
                     }
 
-                    notifications.error(
-                        "Authorization error",
-                        msg);
+                    notifications.error("Authorization error", msg);
                 }
-                */
 
                 return $q.reject(rejection);
             }
