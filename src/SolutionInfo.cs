9875c37cf25b163d9a5ae92f066e69db06518de6
Sebastiaan Janssen <sebastiaan@umbraco.com>
2018-06-21 10:48:25 +0200

    Bumps version to 7.11.1

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -11,5 +11,5 @@ using System.Resources;
 
 [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyFileVersion("7.11.0")]
-[assembly: AssemblyInformationalVersion("7.11.0")]
+[assembly: AssemblyFileVersion("7.11.1")]
+[assembly: AssemblyInformationalVersion("7.11.1")]
