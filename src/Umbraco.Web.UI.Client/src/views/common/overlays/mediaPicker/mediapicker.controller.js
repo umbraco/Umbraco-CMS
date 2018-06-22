a9e9eb9bdd1a01d8148e43b529a6bb138838afb5
Anders Bjerner <abjerner@skybrud.dk>
2018-06-22 19:09:20 +0200

    Fixes U4-11426 for files as well
    The original fix from #2679 only fixes the bug for images, as it checks whether the `umbracoWidth` and `umbracoHeight` properties are present. With this PR, the bug is now fixed for both images and regular files.

--- src/Umbraco.Web.UI.Client/src/views/common/overlays/mediaPicker/mediapicker.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/common/overlays/mediaPicker/mediapicker.controller.js
@@ -318,25 +318,32 @@ angular.module("umbraco")
                                 mediaItem.thumbnail = mediaHelper.resolveFileFromEntity(mediaItem, true);
                                 mediaItem.image = mediaHelper.resolveFileFromEntity(mediaItem, false);
                                 // set properties to match a media object
-                                if (mediaItem.metaData &&
-                                    mediaItem.metaData.umbracoWidth &&
-                                    mediaItem.metaData.umbracoHeight) {
-
-                                    mediaItem.properties = [
-                                        {
-                                            alias: "umbracoWidth",
-                                            value: mediaItem.metaData.umbracoWidth.Value
-                                        },
-                                        {
-                                            alias: "umbracoHeight",
-                                            value: mediaItem.metaData.umbracoHeight.Value
-                                        },
-                                        {
-                                            alias: 'umbracoFile',
-                                            editor: mediaItem.metaData.umbracoFile.PropertyEditorAlias,
-                                            value: mediaItem.metaData.umbracoFile.Value
-                                        }
-                                    ];
+                                if (mediaItem.metaData) {
+                                    if (mediaItem.metaData.umbracoWidth && mediaItem.metaData.umbracoHeight) {
+                                        mediaItem.properties = [
+                                            {
+                                                alias: "umbracoWidth",
+                                                value: mediaItem.metaData.umbracoWidth.Value
+                                            },
+                                            {
+                                                alias: "umbracoHeight",
+                                                value: mediaItem.metaData.umbracoHeight.Value
+                                            },
+                                            {
+                                                alias: "umbracoFile",
+                                                editor: mediaItem.metaData.umbracoFile.PropertyEditorAlias,
+                                                value: mediaItem.metaData.umbracoFile.Value
+                                            }
+                                        ];
+                                    } else {
+                                        mediaItem.properties = [
+                                            {
+                                                alias: "umbracoFile",
+                                                editor: mediaItem.metaData.umbracoFile.PropertyEditorAlias,
+                                                value: mediaItem.metaData.umbracoFile.Value
+                                            }
+                                        ];
+                                    }
                                 }
                             });
                         // update images
