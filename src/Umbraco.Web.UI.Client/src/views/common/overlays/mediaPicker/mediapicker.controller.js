ad99d164c9477c098b9c1e6b9106dc7140cf57d5
Anders Bjerner <abjerner@skybrud.dk>
2018-06-25 23:51:19 +0200

    U4-11477 Here we go again
    https://github.com/umbraco/Umbraco-CMS/pull/2720#issuecomment-400106496

--- src/Umbraco.Web.UI.Client/src/views/common/overlays/mediaPicker/mediapicker.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/common/overlays/mediaPicker/mediapicker.controller.js
@@ -318,31 +318,24 @@ angular.module("umbraco")
                                 mediaItem.thumbnail = mediaHelper.resolveFileFromEntity(mediaItem, true);
                                 mediaItem.image = mediaHelper.resolveFileFromEntity(mediaItem, false);
                                 // set properties to match a media object
+                                mediaItem.properties = [];
                                 if (mediaItem.metaData) {
                                     if (mediaItem.metaData.umbracoWidth && mediaItem.metaData.umbracoHeight) {
-                                        mediaItem.properties = [
-                                            {
-                                                alias: "umbracoWidth",
-                                                value: mediaItem.metaData.umbracoWidth.Value
-                                            },
-                                            {
-                                                alias: "umbracoHeight",
-                                                value: mediaItem.metaData.umbracoHeight.Value
-                                            },
-                                            {
-                                                alias: "umbracoFile",
-                                                editor: mediaItem.metaData.umbracoFile.PropertyEditorAlias,
-                                                value: mediaItem.metaData.umbracoFile.Value
-                                            }
-                                        ];
-                                    } else {
-                                        mediaItem.properties = [
-                                            {
-                                                alias: "umbracoFile",
-                                                editor: mediaItem.metaData.umbracoFile.PropertyEditorAlias,
-                                                value: mediaItem.metaData.umbracoFile.Value
-                                            }
-                                        ];
+                                        mediaItem.properties.push({
+                                            alias: "umbracoWidth",
+                                            value: mediaItem.metaData.umbracoWidth.Value
+                                        });
+                                        mediaItem.properties.push({
+                                            alias: "umbracoHeight",
+                                            value: mediaItem.metaData.umbracoHeight.Value
+                                        });
+                                    }
+                                    if (mediaItem.metaData.umbracoFile) {
+                                        mediaItem.properties.push({
+                                            alias: "umbracoFile",
+                                            editor: mediaItem.metaData.umbracoFile.PropertyEditorAlias,
+                                            value: mediaItem.metaData.umbracoFile.Value
+                                        });
                                     }
                                 }
                             });
