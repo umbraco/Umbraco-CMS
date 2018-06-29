69d1f4e0cd9d9cfba482cdb6500eda0a14d79ba2
Anthony <anthony.dang@thecogworks.com>
2018-06-29 13:41:47 +0100

    Moved methods which wrapped UmbracoHelper

--- src/Umbraco.Web/umbraco.presentation/library.cs
+++ src/Umbraco.Web/umbraco.presentation/library.cs
@@ -5,7 +5,6 @@ using Umbraco.Core.Cache;
 using Umbraco.Core.Configuration;
 using Umbraco.Core.Logging;
 using Umbraco.Core.Services;
-using Umbraco.Web;
 using Umbraco.Web.Composing;
 
 namespace umbraco
@@ -20,33 +19,6 @@ namespace umbraco
     [Obsolete("v8.kill.kill")]
     public class library
     {
-        /// <summary>
-        /// Returns a new UmbracoHelper so that we can start moving the logic from some of these methods to it
-        /// </summary>
-        /// <returns></returns>
-        private static UmbracoHelper GetUmbracoHelper()
-        {
-            return new UmbracoHelper(Current.UmbracoContext, Current.Services, Current.ApplicationCache);
-        }
-
-        private page _page;
-
-
-
-        /// <summary>
-        /// Returns a string with a friendly url from a node.
-        /// IE.: Instead of having /482 (id) as an url, you can have
-        /// /screenshots/developer/macros (spoken url)
-        /// </summary>
-        /// <param name="nodeID">Identifier for the node that should be returned</param>
-        /// <returns>String with a friendly url from a node</returns>
-        public static string NiceUrl(int nodeID)
-        {
-            return GetUmbracoHelper().Url(nodeID);
-        }
-
-
-
         /// <summary>
         /// Get a media object as an xml object
         /// </summary>
@@ -106,28 +78,5 @@ namespace umbraco
                 deep);
             return Tuple.Create(serialized, media.Path);
         }
-
-
-        /// <summary>
-        /// Replaces text line breaks with html line breaks
-        /// </summary>
-        /// <param name="text">The text.</param>
-        /// <returns>The text with text line breaks replaced with html linebreaks (<br/>)</returns>
-        public static string ReplaceLineBreaks(string text)
-        {
-            return GetUmbracoHelper().ReplaceLineBreaksForHtml(text);
-        }
-
-
-        /// <summary>
-        /// Gets the dictionary item with the specified key.
-        /// </summary>
-        /// <param name="Key">The key.</param>
-        /// <returns>A dictionary items value as a string.</returns>
-        public static string GetDictionaryItem(string Key)
-        {
-            return GetUmbracoHelper().GetDictionaryValue(Key);
-        }
-
     }
 }
