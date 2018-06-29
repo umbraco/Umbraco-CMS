e6842a10206721e9be7bd35e6474d4b34683212f
Nathan Woulfe <nathan@nathanw.com.au>
2018-06-29 20:34:04 +1000

    clean up usings

--- src/Umbraco.Web/Editors/TemplateController.cs
+++ src/Umbraco.Web/Editors/TemplateController.cs
@@ -1,18 +1,13 @@
-﻿using System.Collections.Generic;
-using System.IO;
+﻿using AutoMapper;
+using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Net.Http;
-using System.Web;
 using System.Web.Http;
-using AutoMapper;
-using Umbraco.Core;
 using Umbraco.Core.IO;
 using Umbraco.Core.Models;
-using Umbraco.Core.Services;
 using Umbraco.Web.Models.ContentEditing;
 using Umbraco.Web.Mvc;
-using Umbraco.Web.WebApi;
 using Umbraco.Web.WebApi.Filters;
 using Constants = Umbraco.Core.Constants;
 
