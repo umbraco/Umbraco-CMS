52b7be0f1555e3ccd63ac58c16fe034290909fd7
Ed Parry <me@edparry.com>
2018-06-29 15:01:24 +0100

    Removed obsolete constructors from actions

--- src/Umbraco.Web/_Legacy/Actions/ActionCopy.cs
+++ src/Umbraco.Web/_Legacy/Actions/ActionCopy.cs
@@ -16,14 +16,6 @@ namespace Umbraco.Web._Legacy.Actions
         private static readonly ActionCopy m_instance = new ActionCopy();
 #pragma warning restore 612,618
 
-        /// <summary>
-        /// A public constructor exists ONLY for backwards compatibility in regards to 3rd party add-ons.
-        /// All Umbraco assemblies should use the singleton instantiation (this.Instance)
-        /// When this applicatio is refactored, this constuctor should be made private.
-        /// </summary>
-        [Obsolete("Use the singleton instantiation instead of a constructor")]
-        public ActionCopy() { }
-
         public static ActionCopy Instance
         {
             get { return m_instance; }
