8521a405f7ac847ff778ff535157182dddea1fe7
Shannon <sdeminick@gmail.com>
2018-07-11 16:43:09 +1000

    manually merging other files

---
+++ src/Umbraco.Web/PropertyEditors/ValueConverters/FlexibleDropdownPropertyValueConverter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Core.Models.PublishedContent;
+using Umbraco.Core.PropertyEditors;
+using Umbraco.Core.Services;
+
+namespace Umbraco.Web.PropertyEditors.ValueConverters
+{
+    [DefaultPropertyValueConverter]
+    public class FlexibleDropdownPropertyValueConverter : PropertyValueConverterBase
+    {
+        public FlexibleDropdownPropertyValueConverter()
+        {
+        }
+
+        public override bool IsConverter(PublishedPropertyType propertyType)
+        {
+            return propertyType.EditorAlias.Equals(Umbraco.Core.Constants.PropertyEditors.Aliases.DropDownListFlexible);
+        }
+
+        public override object ConvertSourceToIntermediate(IPublishedElement owner, PublishedPropertyType propertyType, object source, bool preview)
+        {
+            return source?.ToString().Split(new[] { ',' }, StringSplitOptions.RemoveEmptyEntries);
+        }
+
+        public override object ConvertIntermediateToObject(IPublishedElement owner, PublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object inter, bool preview)
+        {
+            if (inter == null)
+            {
+                return null;
+            }
+
+            var selectedValues = (string[])inter;
+            if (selectedValues.Any())
+            {
+                if (propertyType.DataType.ConfigurationAs<DropDownFlexibleConfiguration>().Multiple)
+                {
+                    return selectedValues;
+                }
+
+                return selectedValues.First();
+            }
+
+            return inter;
+        }
+
+        public override Type GetPropertyValueType(PublishedPropertyType propertyType)
+        {
+            return propertyType.DataType.ConfigurationAs<DropDownFlexibleConfiguration>().Multiple
+                       ? typeof(IEnumerable<string>)
+                       : typeof(string);
+        }
+        
+    }
+}
