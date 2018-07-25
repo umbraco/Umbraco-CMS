01ef407c0d7742744d2e2051f2743323fac19375
Jan Skovgaard <jaskov@gmail.com>
2018-07-25 09:34:47 +0200

    Set the default storageType for the Umbraco.Tags datatype to "Json", which is now going to be the default

--- src/Umbraco.Core/Persistence/Migrations/Initial/BaseDataCreation.cs
+++ src/Umbraco.Core/Persistence/Migrations/Initial/BaseDataCreation.cs
@@ -289,6 +289,7 @@ namespace Umbraco.Core.Persistence.Migrations.Initial
         {
             _database.Insert("cmsDataTypePreValues", "id", false, new DataTypePreValueDto { Id = 3, Alias = "", SortOrder = 0, DataTypeNodeId = -87, Value = ",code,undo,redo,cut,copy,mcepasteword,stylepicker,bold,italic,bullist,numlist,outdent,indent,mcelink,unlink,mceinsertanchor,mceimage,umbracomacro,mceinserttable,umbracoembed,mcecharmap,|1|1,2,3,|0|500,400|1049,|true|" });
             _database.Insert("cmsDataTypePreValues", "id", false, new DataTypePreValueDto { Id = 4, Alias = "group", SortOrder = 0, DataTypeNodeId = 1041, Value = "default" });
+            _database.Insert("cmsDataTypePreValues", "id", false, new DataTypePreValueDto { Id = 5, Alias = "storageType", SortOrder = 0, DataTypeNodeId = 1041, Value = "Json" });
             
             //defaults for the member list
             _database.Insert("cmsDataTypePreValues", "id", false, new DataTypePreValueDto { Id = -1, Alias = "pageSize", SortOrder = 1, DataTypeNodeId = Constants.System.DefaultMembersListViewDataTypeId, Value = "10" });
