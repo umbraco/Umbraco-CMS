5f38a9ca623d1732b99858d3a4f94ad5ad2e1515
Owain Williams <owaingdwilliams@gmail.com>
2018-07-25 12:51:20 +0100

    Implementing new colour picker for document types
    Changing the dropdown menu for a colour swatch picker

--- src/Umbraco.Web.UI.Client/src/views/common/overlays/iconpicker/iconpicker.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/common/overlays/iconpicker/iconpicker.controller.js
@@ -11,8 +11,46 @@ function IconPickerOverlay($scope, iconHelper, localizationService) {
    $scope.loading = true;
    $scope.model.hideSubmitButton = false;
 
+    $scope.colors = [
+        {name:'Black',value:'color-black'},
+        {name:'Blue Grey',value:'color-blue-grey'},
+        {name:'Grey',value:'color-grey'},
+        {name:'Brown',value:'color-brown'},
+        {name:'Blue',value:'color-blue'},
+        {name:'Light Blue',value:'color-light-blue'},
+        {name: 'Indigo',value:'color-indigo'},
+        {name: 'Purple',value:'color-purple' },
+        {name: 'Deep Purple',value:'color-deep-purple' },
+        {name: 'Cyan',value:'color-cyan' },
+        {name: 'Green',value:'color-green' },
+        {name: 'Light Green',value:'color-light-green' },
+        {name: 'Lime',value:'color-lime' },
+        {name: 'Yellow',value:'color-yellow' },
+        {name: 'Amber',value:'color-amber' },
+        {name: 'Orange',value:'color-orange' },
+        {name: 'Deep Orange',value:'color-deep-orange' },
+        {name: 'Red',value:'color-red' },
+        {name: 'Pink',value:'color-pink' }       
+    ]
+  
+
+    if (!$scope.color) {
+        // Set default selected color to black
+        $scope.color = $scope.colors[0].value;
+    };
+
+
+
     if (!$scope.model.title) {
+        
         $scope.model.title = localizationService.localize("defaultdialogs_selectIcon");
+        
+    };
+
+
+
+    $scope.setColor = function (color) {
+        $scope.color = color;
     };
 
     if ($scope.model.color) {
