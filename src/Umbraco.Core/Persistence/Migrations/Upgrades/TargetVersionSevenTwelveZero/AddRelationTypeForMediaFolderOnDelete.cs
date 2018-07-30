10db2834aa0abccf36e5a4f63532534043c4f23d
Shannon <sdeminick@gmail.com>
2018-07-30 21:36:15 +1000

    committing new migrations from v7 so we can properly rename them on next commit

---
+++ src/Umbraco.Core/Persistence/Migrations/Upgrades/TargetVersionSevenTwelveZero/AddRelationTypeForMediaFolderOnDelete.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Models.Rdbms;
+using Umbraco.Core.Persistence.DatabaseAnnotations;
+using Umbraco.Core.Persistence.SqlSyntax;
+
+namespace Umbraco.Core.Persistence.Migrations.Upgrades.TargetVersionSevenTwelveZero
+{
+    [Migration("7.12.0", 0, Constants.System.UmbracoMigrationName)]
+    public class AddRelationTypeForMediaFolderOnDelete : MigrationBase
+    {
+        public AddRelationTypeForMediaFolderOnDelete(ISqlSyntaxProvider sqlSyntax, ILogger logger)
+            : base(sqlSyntax, logger)
+        {
+        }
+
+        public override void Up()
+        {
+            var exists = Context.Database.FirstOrDefault<RelationTypeDto>("WHERE alias=@alias", new { alias = Constants.Conventions.RelationTypes.RelateParentMediaFolderOnDeleteAlias });
+            if (exists == null)
+            {
+                var relationTypeDto = new RelationTypeDto
+                {
+                    Alias = Constants.Conventions.RelationTypes.RelateParentMediaFolderOnDeleteAlias,
+                    Name = Constants.Conventions.RelationTypes.RelateParentMediaFolderOnDeleteName,
+                    ChildObjectType = Guid.Parse(Constants.ObjectTypes.MediaType),
+                    ParentObjectType = Guid.Parse(Constants.ObjectTypes.MediaType),
+                    Dual = false
+                };
+
+                Context.Database.Insert(relationTypeDto);
+            }
+        }
+
+        public override void Down()
+        { }
+    }
+}
---
+++ src/Umbraco.Core/Persistence/Migrations/Upgrades/TargetVersionSevenTwelveZero/IncreaseLanguageIsoCodeColumnLength.cs
@@ -0,0 +1,37 @@
+﻿using Umbraco.Core.Logging;
+using Umbraco.Core.Persistence.SqlSyntax;
+
+namespace Umbraco.Core.Persistence.Migrations.Upgrades.TargetVersionSevenTwelveZero
+{
+    [Migration("7.12.0", 2, Constants.System.UmbracoMigrationName)]
+    public class IncreaseLanguageIsoCodeColumnLength : MigrationBase
+    {
+        public IncreaseLanguageIsoCodeColumnLength(ISqlSyntaxProvider sqlSyntax, ILogger logger)
+            : base(sqlSyntax, logger)
+        {
+        }
+
+        public override void Up()
+        {
+            Execute.Code(database =>
+            {
+                database.Execute("DROP INDEX [umbracoLanguage].[IX_umbracoLanguage_languageISOCode]");
+                return null;
+            });
+
+            Alter.Table("umbracoLanguage")
+                .AlterColumn("languageISOCode")
+                .AsString(14)
+                .Nullable();
+
+            Create.Index("IX_umbracoLanguage_languageISOCode")
+                .OnTable("umbracoLanguage")
+                .OnColumn("languageISOCode")
+                .Unique();
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
