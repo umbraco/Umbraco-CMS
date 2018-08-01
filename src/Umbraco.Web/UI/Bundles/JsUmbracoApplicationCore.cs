a2839876c6b292524ffe94d94b8bbd8046c22f77
Warren <warren@umbraco.com>
2018-08-01 15:30:16 +0100

    Extnesions.js removed from umbraco_client as it has a new home in umbraco/lib/umbraco

--- src/Umbraco.Web/UI/Bundles/JsUmbracoApplicationCore.cs
+++ src/Umbraco.Web/UI/Bundles/JsUmbracoApplicationCore.cs
@@ -6,7 +6,6 @@ namespace Umbraco.Web.UI.Bundles
     /// <summary>
     /// The core libs that require JQuery to be loaded
     /// </summary>
-    [ClientDependency(ClientDependencyType.Javascript, "Application/Extensions.js", "UmbracoClient", Priority = 0, Group = 2)]
     [ClientDependency(ClientDependencyType.Javascript, "Application/UmbracoApplicationActions.js", "UmbracoClient", Priority = 1, Group = 2)]
     public class JsUmbracoApplicationCore : Control
     {
