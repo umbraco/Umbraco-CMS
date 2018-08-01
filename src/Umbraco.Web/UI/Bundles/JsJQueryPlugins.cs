3bc6bf6da5369459bf2828ac8f70963e1bf5a773
Warren <warren@umbraco.com>
2018-08-01 15:34:32 +0100

    jQuery.cookie lib has ran off into the woods

--- src/Umbraco.Web/UI/Bundles/JsJQueryPlugins.cs
+++ src/Umbraco.Web/UI/Bundles/JsJQueryPlugins.cs
@@ -7,7 +7,6 @@ namespace Umbraco.Web.UI.Bundles
     /// The core Jquery libs
     /// </summary>
     [ClientDependency(ClientDependencyType.Javascript, "modal/jquery.simplemodal.1.4.1.custom.js", "UmbracoClient", Priority = 2, Group = 4)]
-    [ClientDependency(ClientDependencyType.Javascript, "Application/jQuery/jquery.cookie.js", "UmbracoClient", Priority = 3, Group = 4)]
     [ClientDependency(ClientDependencyType.Javascript, "Application/JQuery/jquery.metadata.min.js", "UmbracoClient", Priority = 4, Group = 4)]
     [ClientDependency(ClientDependencyType.Javascript, "Application/jQuery/jquery.hotkeys.js", "UmbracoClient", Priority = 5, Group = 4)]
     public class JsJQueryPlugins : Control
