0e6465ee14835c89ff702de7fb3a0b331577758e
Warren <warren@umbraco.com>
2018-08-01 15:48:19 +0100

    See ya later alligator jQuery.hotkeys.js - CTRL+S still wortks in DocType editor so all good

--- src/Umbraco.Web/UI/Bundles/JsJQueryPlugins.cs
+++ src/Umbraco.Web/UI/Bundles/JsJQueryPlugins.cs
@@ -8,7 +8,6 @@ namespace Umbraco.Web.UI.Bundles
     /// </summary>
     [ClientDependency(ClientDependencyType.Javascript, "modal/jquery.simplemodal.1.4.1.custom.js", "UmbracoClient", Priority = 2, Group = 4)]
     [ClientDependency(ClientDependencyType.Javascript, "Application/JQuery/jquery.metadata.min.js", "UmbracoClient", Priority = 4, Group = 4)]
-    [ClientDependency(ClientDependencyType.Javascript, "Application/jQuery/jquery.hotkeys.js", "UmbracoClient", Priority = 5, Group = 4)]
     public class JsJQueryPlugins : Control
     {
     }
