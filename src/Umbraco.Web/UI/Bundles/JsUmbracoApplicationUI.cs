08991dc98c662dd3f76424f715c07424a80747a9
Warren <warren@umbraco.com>
2018-08-02 11:58:10 +0100

    Removes old modal js lib & references

--- src/Umbraco.Web/UI/Bundles/JsUmbracoApplicationUI.cs
+++ src/Umbraco.Web/UI/Bundles/JsUmbracoApplicationUI.cs
@@ -7,9 +7,7 @@ namespace Umbraco.Web.UI.Bundles
     /// The UI Umbraco libs
     /// </summary>
     [ClientDependency(ClientDependencyType.Javascript, "js/guiFunctions.js", "UmbracoRoot", Priority = 1, Group = 5)]
-    [ClientDependency(ClientDependencyType.Javascript, "modal/modal.js", "UmbracoClient", Priority = 2, Group = 5)]
     [ClientDependency(ClientDependencyType.Javascript, "js/UmbracoSpeechBubbleBackend.js", "UmbracoRoot", Priority = 3, Group = 5)]
-    [ClientDependency(ClientDependencyType.Javascript, "modal/modal.js", "UmbracoClient", Priority = 2, Group = 5)]
     public class JsUmbracoApplicationUI : Control
     {
     }
