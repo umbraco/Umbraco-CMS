789a9434c12925ed72914713f99aad90952c2dfd
Shannon <sdeminick@gmail.com>
2018-08-03 10:22:29 +1000

    fixes property rebinding with variants and fixes more validation flow

--- src/Umbraco.Web.UI.Client/src/common/directives/components/content/edit.controller.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/content/edit.controller.js
@@ -182,9 +182,8 @@
 
                     //in one particular special case, after we've created a new item we redirect back to the edit
                     // route but there might be server validation errors in the collection which we need to display
-                    // after the redirect, so we will bind all subscriptions which will show the server validation errors
-                    // if there are any and then clear them so the collection no longer persists them.
-                    serverValidationManager.notifyAndClearAllSubscriptions();
+                    // after the redirect, so we will bind all subscriptions which will show the server validation errors.
+                    serverValidationManager.notify();
 
                     if (!infiniteMode) {
                         syncTreeNode($scope.content, data.path, true);
@@ -666,6 +665,9 @@
             for (var e in evts) {
                 eventsService.unsubscribe(evts[e]);
             }
+            //since we are not notifying and clearing server validation messages when they are received due to how the variant
+            //switching works, we need to ensure they are cleared when this editor is destroyed
+            serverValidationManager.clear();
         });
 
     }
