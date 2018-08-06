4730ee317bfdbf38ddb0f265b65990645494af83
Warren <warren@umbraco.com>
2018-08-06 11:52:12 +0100

    Removes some unused .aspx files for dictionary editing - no longer needed as we now have this in Angular

--- src/Umbraco.Web/UI/Bundles/JsJQueryCore.cs
+++ src/Umbraco.Web/UI/Bundles/JsJQueryCore.cs
@@ -6,6 +6,7 @@ namespace Umbraco.Web.UI.Bundles
     /// <summary>
     /// The core Jquery libs
     /// </summary>
+    [ClientDependency(ClientDependencyType.Javascript, "lib/jquery/jquery.min.js", "UmbracoRoot", Priority = 1, Group = 1)]
     [ClientDependency(ClientDependencyType.Javascript, "lib/jquery-migrate/jquery-migrate.min.js", "UmbracoRoot", Priority = 2, Group = 1)]
     public class JsJQueryCore : Control
     {
