3777ea9a7df30fb6c7953dab25f3a5209414bc3a
Shannon <sdeminick@gmail.com>
2018-08-06 21:19:36 +1000

    Moves the 'editor' variant editor to a separate directive so that we can separate out a bit of logic and have a form to watch for dirty tracking on the header value

--- src/Umbraco.Web.UI.Client/src/common/directives/components/content/edit.controller.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/content/edit.controller.js
@@ -556,76 +556,7 @@
             });
         };
 
-        $scope.selectVariant = function (editorIndex, variantDropDownItem) {
-
-            //if the editor index is zero, then update the query string to track the lang selection, otherwise if it's part
-            //of a 2nd split view editor then update the model directly.
-            if (editorIndex === 0) {
-                //if we've made it this far, then update the query string
-                $location.search("cculture", variantDropDownItem.language.culture);
-            }
-            else {
-                //set all variant drop down items as inactive for this editor and then set the selected on as active
-                var editor = $scope.editors[editorIndex];
-                for (var i = 0; i < editor.content.variants.length; i++) {
-                    editor.content.variants[i].active = false;
-                }
-                variantDropDownItem.active = true;
-
-                //get the variant content model and initialize the editor with that
-                var variant = _.find($scope.content.variants, function (v) {
-                    return v.language.culture === variantDropDownItem.language.culture;
-                });
-                editor.content = initVariant(variant);
-            }
-        };
-
-        $scope.closeSplitView = function (index, editor) {
-            //TODO: hacking animation states - these should hopefully be easier to do when we upgrade angular
-            editor.loading = true;
-            editor.collapsed = true;
-            $timeout(function () {
-                $scope.editors.splice(index, 1);
-            }, 400);
-        };
-
-        $scope.openInSplitView = function (selectedVariant) {
-
-            var selectedCulture = selectedVariant.language.culture;
-
-            //only the content app can be selected since no other apps are shown, and because we copy all of these apps
-            //to the "editors" we need to update this across all editors
-            for (var e = 0; e < $scope.editors.length; e++) {
-                var editor = $scope.editors[e];
-                for (var i = 0; i < editor.content.apps.length; i++) {
-                    var app = editor.content.apps[i];
-                    if (app.alias === "content") {
-                        app.active = true;
-                    }
-                    else {
-                        app.active = false;
-                    }
-                }
-            }
-
-            //Find the whole variant model based on the culture that was chosen
-            var variant = _.find($scope.content.variants, function (v) {
-                return v.language.culture === selectedCulture;
-            });
-
-            var editor = {
-                content: initVariant(variant)
-            };
-            $scope.editors.push(editor);
-
-            //TODO: hacking animation states - these should hopefully be easier to do when we upgrade angular
-            editor.collapsed = true;
-            editor.loading = true;
-            $timeout(function () {
-                editor.collapsed = false;
-                editor.loading = false;
-            }, 100);
-        };
+        $scope.initVariant = initVariant;
 
         /* publish method used in infinite editing */
         $scope.publishAndClose = function (content) {
@@ -650,7 +581,7 @@
                 $scope.saveAndCloseButtonState = "success";
             }).catch(angular.noop);;
         };
-
+        
         function moveNode(node, target) {
 
             contentResource.move({ "parentId": target.id, "id": node.id })
