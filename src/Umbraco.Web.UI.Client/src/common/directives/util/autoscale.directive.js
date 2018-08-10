23e81a67fa64e408888dc1b1ec4e195be8791d8a
Mads Rasmussen <mads@umbraco.com>
2018-08-10 14:05:15 +0200

    fix indention

--- src/Umbraco.Web.UI.Client/src/common/directives/util/autoscale.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/util/autoscale.directive.js
@@ -16,23 +16,23 @@
 **/
 
 angular.module("umbraco.directives")
-  .directive('autoScale', function ($window) {
-    return function (scope, el, attrs) {
-      
-      var totalOffset = 0;
-      var offsety = parseInt(attrs.autoScale, 10);
-      var window = angular.element($window);
-      if (offsety !== undefined){
-        totalOffset += offsety;
-      }
+    .directive('autoScale', function ($window) {
+        return function (scope, el, attrs) {
 
-      setTimeout(function () {
-        el.height(window.height() - (el.offset().top + totalOffset));
-      }, 500);
+            var totalOffset = 0;
+            var offsety = parseInt(attrs.autoScale, 10);
+            var window = angular.element($window);
+            if (offsety !== undefined) {
+                totalOffset += offsety;
+            }
 
-      window.bind("resize", function () {
-        el.height(window.height() - (el.offset().top + totalOffset));
-      });
+            setTimeout(function () {
+                el.height(window.height() - (el.offset().top + totalOffset));
+            }, 500);
 
-    };
-  });
+            window.bind("resize", function () {
+                el.height(window.height() - (el.offset().top + totalOffset));
+            });
+
+        };
+    });
