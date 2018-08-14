1c54c4c87d38ad136dd12c4e91d2ab62cfb2b3d8
Warren <warren@umbraco.com>
2018-08-14 15:05:58 +0100

    Update Umbraco ILogger extension methods to have some that support message templates for Serilog * NOTE: Added obsolete with error bool set - to help me find uses to update easier

--- src/Umbraco.Core/Logging/LoggerExtensions.cs
+++ src/Umbraco.Core/Logging/LoggerExtensions.cs
@@ -15,9 +15,15 @@ namespace Umbraco.Core.Logging
         /// <param name="logger">The logger.</param>
         /// <param name="message">A message.</param>
         /// <param name="exception">An exception.</param>
+        //[Obsolete("Use the Error<T> with message template", true)]
         public static void Error<T>(this ILogger logger, string message, Exception exception = null)
         {
             logger.Error(typeof(T), message, exception);
+        }
+
+        public static void Error<T>(this ILogger logger, string messageTemplate, Exception exception = null, params object[] propertyValues)
+        {
+            logger.Error(typeof(T), messageTemplate, exception, propertyValues);
         }
 
         /// <summary>
@@ -31,12 +37,18 @@ namespace Umbraco.Core.Logging
             logger.Warn(typeof(T), message);
         }
 
+        public static void Warn<T>(this ILogger logger, string messageTemplate, params object[] propertyValues)
+        {
+            logger.Warn(typeof(T), messageTemplate, propertyValues);
+        }
+
         /// <summary>
         /// Logs a warning message.
         /// </summary>
         /// <typeparam name="T">The reporting type.</typeparam>
         /// <param name="logger">The logger.</param>
         /// <param name="messageBuilder">A message builder.</param>
+        [Obsolete("Use the Warn<T> with message template", true)]
         public static void Warn<T>(this ILogger logger, Func<string> messageBuilder)
         {
             logger.Warn(typeof(T), messageBuilder);
@@ -49,6 +61,7 @@ namespace Umbraco.Core.Logging
         /// <param name="logger">The logger.</param>
         /// <param name="exception">An exception.</param>
         /// <param name="messageBuilder">A message builder.</param>
+        [Obsolete("Use the Warn<T> with message template", true)]
         public static void Warn<T>(this ILogger logger, Exception exception, Func<string> messageBuilder)
         {
             logger.Warn(typeof(T), exception, messageBuilder);
@@ -66,6 +79,11 @@ namespace Umbraco.Core.Logging
             logger.Warn(typeof(T), exception, message);
         }
 
+        public static void Warn<T>(this ILogger logger, Exception exception, string messageTemplate, params object[] propertyValues)
+        {
+            logger.Warn(typeof(T), exception, messageTemplate, propertyValues);
+        }
+
         /// <summary>
         /// Logs an information message.
         /// </summary>
@@ -77,12 +95,18 @@ namespace Umbraco.Core.Logging
             logger.Info(typeof(T), message);
         }
 
+        public static void Info<T>(this ILogger logger, string messageTemplate, params object[] propertyValues)
+        {
+            logger.Info(typeof(T), messageTemplate, propertyValues);
+        }
+
         /// <summary>
         /// Logs an information message.
         /// </summary>
         /// <typeparam name="T">The reporting type.</typeparam>
         /// <param name="logger">The logger.</param>
         /// <param name="messageBuilder">A message builder.</param>
+        [Obsolete("Use the Info<T> with message template", true)]
         public static void Info<T>(this ILogger logger, Func<string> messageBuilder)
         {
             logger.Info(typeof(T), messageBuilder);
@@ -99,12 +123,18 @@ namespace Umbraco.Core.Logging
             logger.Debug(typeof(T), message);
         }
 
+        public static void Debug<T>(this ILogger logger, string messageTemplate, params object[] propertyValues)
+        {
+            logger.Debug(typeof(T), messageTemplate, propertyValues);
+        }
+
         /// <summary>
         /// Logs a debugging message.
         /// </summary>
         /// <typeparam name="T">The reporting type.</typeparam>
         /// <param name="logger">The logger.</param>
         /// <param name="messageBuilder">A message builder.</param>
+        [Obsolete("Use the Debug<T> with message template", true)]
         public static void Debug<T>(this ILogger logger, Func<string> messageBuilder)
         {
             logger.Debug(typeof(T), messageBuilder);
