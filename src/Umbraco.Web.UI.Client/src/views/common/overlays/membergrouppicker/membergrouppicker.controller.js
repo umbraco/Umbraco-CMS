7d439566cdace458c030e57c78cf27e0e7931e53
BatJan <jaskov@gmail.com>
2018-08-23 23:56:32 +0200

    U4-11526 - Member Group Picker doesn't remove group after unselecting it during selection. (#2835)

--- src/Umbraco.Web.UI.Client/src/views/common/overlays/membergrouppicker/membergrouppicker.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/common/overlays/membergrouppicker/membergrouppicker.controller.js
@@ -24,7 +24,16 @@ angular.module("umbraco").controller("Umbraco.Overlays.MemberGroupPickerControll
         }
 
         function selectMemberGroups(id) {
-           $scope.model.selectedMemberGroups.push(id);
+            var index = $scope.model.selectedMemberGroups.indexOf(id);
+
+            if(index === -1){
+                // If the id does not exists in the array then add it
+                $scope.model.selectedMemberGroups.push(id);
+            }
+            else{
+                // Otherwise we will remove it from the array instead
+                $scope.model.selectedMemberGroups.splice(index, 1);
+            }
         }
 
         /** Method used for selecting a node */
