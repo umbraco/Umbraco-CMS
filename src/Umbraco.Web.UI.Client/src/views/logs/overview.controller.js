fbba60a56bf246b19fcef5c95f425391754b30fd
Warren <warren@umbraco.com>
2018-08-24 12:58:50 +0100

    Super rough - WIP UI/layout with a working chart with fake data

---
+++ src/Umbraco.Web.UI.Client/src/views/logs/overview.controller.js
@@ -0,0 +1,59 @@
+(function () {
+    "use strict";
+
+    function LogsOverviewController($scope, $location, localizationService) {
+
+        var vm = this;
+
+        vm.page = {};
+        vm.labels = {};
+
+
+        vm.logTypeLabels = ["Info", "Debug", "Warning", "Error", "Critical"];
+        vm.logTypeData = [500, 75, 10, 45, 2];
+
+        vm.chartOptions = {
+            legend: {
+                display: true,
+                position: 'left',
+                labels: {
+                    /*fontColor: 'rgb(255, 99, 132)'*/
+                }
+            }
+        };
+
+        vm.editLanguage = editLanguage;
+
+
+        function init() {
+
+            vm.loading = true;
+
+            // localize labels
+            var labelKeys = [
+                "treeHeaders_languages",
+                "general_mandatory",
+                "general_default"
+            ];
+
+            localizationService.localizeMany(labelKeys).then(function (values) {
+                vm.labels.languages = values[0];
+                vm.labels.mandatory = values[1];
+                vm.labels.general = values[2];
+                // set page name
+                vm.page.name = vm.labels.languages;
+            });            
+        }
+        
+        function editLanguage(language) {
+            $location.search('create', null);
+            $location.path("/settings/languages/edit/" + language.id);
+        }
+
+        init();
+
+    }
+
+    angular.module("umbraco").controller("Umbraco.Editors.Logs.OverviewController", LogsOverviewController);
+
+})();
