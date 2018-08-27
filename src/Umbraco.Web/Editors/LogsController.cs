df13af9dc223aee10192e9295e925c913ea200f3
Warren <warren@umbraco.com>
2018-08-27 20:05:59 +0100

    Attempt to get a WebAPI Controller setup

---
+++ src/Umbraco.Web/Editors/LogsController.cs
@@ -0,0 +1,32 @@
+﻿using System.Web.Mvc;
+using Umbraco.Core.Logging.Viewer;
+using Umbraco.Web.Mvc;
+
+namespace Umbraco.Web.Editors
+{
+    /// <summary>
+    /// Backoffice controller supporting the dashboard for language administration.
+    /// </summary>
+    [PluginController("UmbracoApi")]
+    public class LogsController : UmbracoAuthorizedJsonController
+    {
+        private LogViewer _logViewer;
+
+        public LogsController()
+        {
+            _logViewer = new LogViewer();
+        }
+
+        [HttpGet]
+        public int GetNumberOfErrors()
+        {
+            return _logViewer.GetNumberOfErrors();
+        }
+
+        [HttpGet]
+        public LogLevelCounts GetLogLevelCounts()
+        {
+            return _logViewer.GetLogLevelCounts();
+        }
+    }
+}
