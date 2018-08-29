cc915d6ef1245223e7f10fb823442e4ffda5ae85
Warren <warren@umbraco.com>
2018-08-29 19:01:26 +0100

    Adds in URL to WebAPI/Editor to Umbraco.Sys global JS object

--- src/Umbraco.Web/Editors/BackOfficeServerVariables.cs
+++ src/Umbraco.Web/Editors/BackOfficeServerVariables.cs
@@ -296,6 +296,10 @@ namespace Umbraco.Web.Editors
                         {
                             "languageApiBaseUrl", _urlHelper.GetUmbracoApiServiceBaseUrl<LanguageController>(
                                 controller => controller.GetAllLanguages())
+                        },
+                        {
+                            "logViewerApiBaseUrl", _urlHelper.GetUmbracoApiServiceBaseUrl<LogViewerController>(
+                                controller => controller.GetNumberOfErrors())
                         }
                     }
                 },
