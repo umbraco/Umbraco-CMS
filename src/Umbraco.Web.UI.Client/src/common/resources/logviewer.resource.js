08618b653cd7a7ed1602ab87a09ba28a1d94f54d
Warren <warren@umbraco.com>
2018-08-30 12:42:43 +0100

    Gets the rough wrieframe to use real API data now * List number of total errors * Pie Chart of log level counts * Message counts * Log items with paging & searching

---
+++ src/Umbraco.Web.UI.Client/src/common/resources/logviewer.resource.js
@@ -0,0 +1,72 @@
+/**
+    * @ngdoc service
+    * @name umbraco.resources.logViewerResource
+    * @description Retrives Umbraco log items (by default from JSON files on disk)
+    * 
+    *
+    **/
+   function logViewerResource($q, $http, umbRequestHelper) {
+
+    //the factory object returned
+    return {
+
+        getNumberOfErrors: function(){
+            return umbRequestHelper.resourcePromise(
+                $http.get(
+                    umbRequestHelper.getApiUrl(
+                        "logViewerApiBaseUrl",
+                        "GetNumberOfErrors")),
+                'Failed to retrieve number of errors in logs')
+        },
+
+        getLogLevelCounts: function(){
+            return umbRequestHelper.resourcePromise(
+                $http.get(
+                    umbRequestHelper.getApiUrl(
+                        "logViewerApiBaseUrl",
+                        "GetLogLevelCounts")),
+                'Failed to retrieve log level counts');
+        },
+
+        getCommonLogMessages: function(){
+            return umbRequestHelper.resourcePromise(
+                $http.get(
+                    umbRequestHelper.getApiUrl(
+                        "logViewerApiBaseUrl",
+                        "GetCommonLogMessages")),
+                'Failed to retrieve common log messages');
+        },
+
+        getLogs: function(options){
+
+            var defaults = {
+                pageSize: 100,
+                pageNumber: 1,
+                orderDirection: "Descending",
+                filterExpression: ''
+            };
+
+            if (options === undefined) {
+                options = {};
+            }
+
+            //overwrite the defaults if there are any specified
+            angular.extend(defaults, options);
+
+            //now copy back to the options we will use
+            options = defaults;
+
+
+            return umbRequestHelper.resourcePromise(
+                $http.get(
+                    umbRequestHelper.getApiUrl(
+                        "logViewerApiBaseUrl",
+                        "GetLogs",
+                        options)),
+                'Failed to retrieve common log messages');
+        }
+        
+    };
+}
+
+angular.module('umbraco.resources').factory('logViewerResource', logViewerResource);
--- src/Umbraco.Web.UI.Client/src/views/logviewer/overview.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/logviewer/overview.controller.js
@@ -1,12 +1,18 @@
 (function () {
     "use strict";
 
-    function LogViewerOverviewController($scope, $location, localizationService) {
+    function LogViewerOverviewController($scope, $location, localizationService, logViewerResource) {
 
         var vm = this;
 
+        vm.loading = false;
         vm.page = {};
         vm.labels = {};
+
+        vm.logItems = {};
+        vm.numberOfErrors = 0;
+        vm.commonLogMessages = [];
+        vm.logOptions = {};
 
         // ChartJS Options - for count/overview of log distribution
         vm.logTypeLabels = ["Info", "Debug", "Warning", "Error", "Critical"];
@@ -19,12 +25,33 @@
             }
         };
 
-        vm.editLanguage = editLanguage;
+        //Functions
+        vm.getLogs = getLogs;
+        vm.changePageNumber = changePageNumber;
+        vm.search = search;
 
 
         function init() {
 
-            vm.loading = true;
+            logViewerResource.getNumberOfErrors().then(function (data) {
+                vm.numberOfErrors = data;
+            });
+
+            logViewerResource.getLogLevelCounts().then(function (data) {
+                vm.logTypeData = [];
+                vm.logTypeData.push(data.Information);
+                vm.logTypeData.push(data.Debug);
+                vm.logTypeData.push(data.Warning);
+                vm.logTypeData.push(data.Error);
+                vm.logTypeData.push(data.Fatal);
+            });
+
+            logViewerResource.getCommonLogMessages().then(function(data){
+                vm.commonLogMessages = data;
+            });
+
+            //Get all logs on init load
+            getLogs();
 
             // localize labels
             var labelKeys = [
@@ -40,11 +67,24 @@
                 // set page name
                 vm.page.name = vm.labels.languages;
             });            
+        }        
+       
+
+        function search(){
+            //Reset pagenumber back to 1
+            vm.logOptions.pageNumber = 1;
+            getLogs();
         }
-        
-        function editLanguage(language) {
-            $location.search('create', null);
-            $location.path("/settings/languages/edit/" + language.id);
+
+        function changePageNumber(pageNumber) {
+            vm.logOptions.pageNumber = pageNumber;
+            getLogs();
+        }
+
+        function getLogs(){
+            logViewerResource.getLogs(vm.logOptions).then(function (data) {
+                vm.logItems = data;
+            });
         }
 
         init();
