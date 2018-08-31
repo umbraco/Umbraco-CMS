20cf8a7637f67c5520857537bc915dd6c5dc5587
Warren <warren@umbraco.com>
2018-08-31 21:57:51 +0100

    Create a base class - so we can have the GetLogs implemented by 3rd party devs. Say from Azure Table Storage for example

---
+++ src/Umbraco.Core/Logging/Viewer/LogViewerSourceBase.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Serilog.Events;
+using Serilog.Filters.Expressions;
+using Umbraco.Core.Models;
+using Umbraco.Core.Persistence.DatabaseModelDefinitions;
+
+namespace Umbraco.Core.Logging.Viewer
+{
+    public abstract class LogViewerSourceBase : ILogViewer
+    {
+        public abstract IEnumerable<LogEvent> GetAllLogs(DateTimeOffset startDate, DateTimeOffset endDate);
+        
+        public int GetNumberOfErrors(DateTimeOffset startDate, DateTimeOffset endDate)
+        {
+            var logs = GetAllLogs(startDate, endDate);
+            return logs.Count(x => x.Level == LogEventLevel.Fatal || x.Level == LogEventLevel.Error || x.Exception != null);
+        }
+
+        public LogLevelCounts GetLogLevelCounts(DateTimeOffset startDate, DateTimeOffset endDate)
+        {
+            var logs = GetAllLogs(startDate, endDate);
+            return new LogLevelCounts
+            {
+                Information = logs.Count(x => x.Level == LogEventLevel.Information),
+                Debug = logs.Count(x => x.Level == LogEventLevel.Debug),
+                Warning = logs.Count(x => x.Level == LogEventLevel.Warning),
+                Error = logs.Count(x => x.Level == LogEventLevel.Error),
+                Fatal = logs.Count(x => x.Level == LogEventLevel.Fatal)
+            };
+        }
+
+        public IEnumerable<CommonLogMessage> GetCommonLogMessages(DateTimeOffset startDate, DateTimeOffset endDate, int numberOfResults)
+        {
+            var logs = GetAllLogs(startDate, endDate);
+            var templates = logs.GroupBy(x => x.MessageTemplate.Text)
+                .Select(g => new CommonLogMessage { MessageTemplate = g.Key, Count = g.Count() })
+                .OrderByDescending(x => x.Count)
+                .Take(numberOfResults);
+
+            return templates;
+        }
+
+        public PagedResult<LogMessage> GetLogs(DateTimeOffset startDate, DateTimeOffset endDate,
+            int pageNumber = 1, int pageSize = 100,
+            Direction orderDirection = Direction.Descending,
+            string filterExpression = null)
+        {
+            //Get all logs into memory (Not sure this good or not)
+            var logs = GetAllLogs(startDate, endDate);
+
+            //If we have a filter expression, apply it
+            if (string.IsNullOrEmpty(filterExpression) == false)
+            {
+                Func<LogEvent, bool> filter = null;
+                var eval = FilterLanguage.CreateFilter(filterExpression);
+                filter = evt => true.Equals(eval(evt));
+
+                logs = logs.Where(filter);
+            }
+
+            long totalRecords = logs.Count();
+            long pageIndex = pageNumber - 1;
+
+            //Order By, Skip, Take & Select
+            IEnumerable<LogMessage> logMessages = logs
+                .OrderBy(l => l.Timestamp, orderDirection)
+                .Skip(pageSize * (pageNumber - 1))
+                .Take(pageSize)
+                .Select(x => new LogMessage
+                {
+                    Timestamp = x.Timestamp,
+                    Level = x.Level,
+                    MessageTemplateText = x.MessageTemplate.Text,
+                    Exception = x.Exception?.ToString(),
+                    Properties = x.Properties,
+                    RenderedMessage = x.RenderMessage()
+                });
+
+            return new PagedResult<LogMessage>(totalRecords, pageNumber, pageSize)
+            {
+                Items = logMessages
+            };
+        }
+    }
+}
