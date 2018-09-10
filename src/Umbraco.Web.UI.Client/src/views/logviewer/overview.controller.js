ce83e25390b668dfac9c17b34829e70e52b61a99
Warren Buckley <warren@umbraco.com>
2018-09-10 10:30:23 +0100

    Adds in a little better CSS styling for the error ex message block than a massive RED background Starts work on getting the dropdown for some example queries setup

--- src/Umbraco.Web.UI.Client/src/views/logviewer/overview.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/logviewer/overview.controller.js
@@ -32,6 +32,33 @@
             }
         ];
 
+        vm.searches = [
+            {
+                name: "Find all logs where the Level is NOT Verbose and NOT Debug",
+                query: "Not(@Level='Verbose') and Not(@Level='Debug')"
+            },
+            {
+                name: "Find all logs that has an exception property (Warning, Error & Critical with Exceptions)",
+                query: "Has(@Exception)"
+            },
+            {
+                name: "Find all logs that have the property 'TimingDuration'",
+                query: "Has(TimingDuration)"
+            },
+            {
+                name: "Find all logs that have the property 'TimingDuration' and the duration is greater than 1000ms",
+                query: "Has(TimingDuration) and TimingDuration > 1000"
+            },
+            {
+                name: "Find all logs that are from the namespace 'Umbraco.Core'",
+                query: "StartsWith(SourceContext, 'Umbraco.Core')"
+            },
+            {
+                name: "Find all logs that use a specific log message template",
+                query: "@MessageTemplate = '[Timing {TimingId}] {EndMessage} ({TimingDuration}ms)'"
+            }
+        ]
+
         vm.logItems = {};
         vm.numberOfErrors = 0;
         vm.commonLogMessages = [];
