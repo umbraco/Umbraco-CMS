ca5e9940d5931d1e522eb210459d4dc42279adfc
Mads Rasmussen <mads@umbraco.com>
2018-09-19 14:24:21 +0200

    add styling for search results + basic wire up of search service

--- src/Umbraco.Web.UI.Client/src/common/directives/components/application/umbsearch.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/application/umbsearch.directive.js
@@ -13,17 +13,19 @@
         }
     };
     
-    function umbSearchController($scope, backdropService) {
+    function umbSearchController($scope, backdropService, searchService) {
 
         var vm = this;
-
-        vm.searchResults = [];
 
         vm.$onInit = onInit;
         vm.search = search;
         vm.closeSearch = closeSearch;
 
         function onInit() {
+
+            vm.searchResults = [];
+            vm.hasResults = false;
+
             console.log("init search thingy");
             backdropService.open();
         }
@@ -43,6 +45,29 @@
          */
         function search(searchQuery) {
             console.log(searchQuery);
+            if(searchQuery.length > 0) {
+                var search = {"term": searchQuery};
+                searchService.searchAll(search).then(function(result){
+                
+                    //result is a dictionary of group Title and it's results
+                    var filtered = {};
+                    _.each(result, function (value, key) {
+                        if (value.results.length > 0) {
+                            filtered[key] = value;
+                        }
+                    });
+
+                    vm.searchResults = filtered;
+                    // check if search has results
+                    vm.hasResults = Object.keys(vm.searchResults).length > 0;
+                    console.log("results", vm.searchResults);
+                    console.log("has results", vm.hasResults);
+                });
+
+            } else {
+                vm.searchResults = [];
+                vm.hasResults = false;
+            }
         }
 
     }
