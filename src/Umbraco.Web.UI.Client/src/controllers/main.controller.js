bf3234c8b2bb7353c774aa8f6cc16bc77e8c41e2
Mads Rasmussen <mads@umbraco.com>
2018-09-19 15:13:55 +0200

    register search in appState + launch when click search icon

--- src/Umbraco.Web.UI.Client/src/controllers/main.controller.js
+++ src/Umbraco.Web.UI.Client/src/controllers/main.controller.js
@@ -15,6 +15,8 @@ function MainController($scope, $location, appState, treeService, notificationsS
     $scope.touchDevice = appState.getGlobalState("touchDevice");
     $scope.editors = [];
     $scope.overlay = {};
+    $scope.drawer = {};
+    $scope.search = {};
     
     $scope.removeNotification = function (index) {
         notificationsService.remove(index);
@@ -114,9 +116,15 @@ function MainController($scope, $location, appState, treeService, notificationsS
         };
     }));
 
+    // events for search
+    evts.push(eventsService.on("appState.searchState.changed", function (e, args) {
+        if (args.key === "show") {
+            $scope.search.show = args.value;
+        }
+    }));
+
     // events for drawer
     // manage the help dialog by subscribing to the showHelp appState
-    $scope.drawer = {};
     evts.push(eventsService.on("appState.drawerState.changed", function (e, args) {
         // set view
         if (args.key === "view") {
@@ -156,6 +164,7 @@ function MainController($scope, $location, appState, treeService, notificationsS
         $scope.backdrop = args;
     }));
 
+    // event for infinite editors
     evts.push(eventsService.on("appState.editors.add", function (name, args) {
         $scope.editors = args.editors;
     }));
