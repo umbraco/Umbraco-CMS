a8328eeb2c80681c57e446986cefdc855e40cff0
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2018-10-01 16:17:38 +0200

    Documented umb-tour-step-header directive

--- src/Umbraco.Web.UI.Client/src/common/directives/components/application/umbtour/umbtourstep.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/application/umbtour/umbtourstep.directive.js
@@ -7,17 +7,6 @@
 @description
 <b>Added in Umbraco 7.8</b>. The tour step component is a component that can be used in custom views for tour steps.
 
-<h3>Markup example - custom step view</h3>
-<pre>
-    <div ng-controller="My.TourStep as vm">
-
-        <umb-tour-step on-close="model.endTour()" hide-close="false">           
-
-        </umb-tour-step>
-
-    </div>
-</pre>
-
 @param {callback} onClose The callback which should be performened when the close button of the tour step is clicked
 @param {boolean=} hideClose A boolean indicating if the close button needs to be shown
 
