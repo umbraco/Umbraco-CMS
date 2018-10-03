362f9db6f81c67424b528b75665a8d6dd1169b34
Kim Holzmann <kim.holzmann@gmail.com>
2018-10-03 03:52:34 +0200

    #3001 Styles cannot be saved - Not sure if oldName is legacy
    Works now -
    But is not sure, is oldName used anymore, It fails when used, in both create new stylefile and save filestyle.
    If oldName is legacy, I think we should clean up metode and the calls to it.

--- src/Umbraco.Web/WebServices/SaveFileController.cs
+++ src/Umbraco.Web/WebServices/SaveFileController.cs
@@ -251,7 +251,7 @@ namespace Umbraco.Web.WebServices
                 .EnsureEndsWith(".css");
 
             var svce = (FileService) Services.FileService;
-            var stylesheet = svce.GetStylesheetByName(oldName);
+            var stylesheet = svce.GetStylesheetByName(filename);
             if (stylesheet == null)
                 stylesheet = new Stylesheet(filename);
             else
