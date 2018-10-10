c16d484ebda16e60e6bfb68214c0f7ed6273868f
Gareth Evans <gareth.evans@snipersystems.co.nz>
2018-10-10 15:10:32 +1300

    #3231 Add event to valTab directive that subscribes to the bootstrap tab shown event and broadcasts it on the rootScope. Add listener to grid rte directive that listens for tab shown and triggers autoresize command.

--- src/Umbraco.Web.UI.Client/src/common/directives/validation/valtab.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/validation/valtab.directive.js
@@ -6,15 +6,15 @@
 * @description Used to show validation warnings for a tab to indicate that the tab content has validations errors in its data.
 * In order for this directive to work, the valFormManager directive must be placed on the containing form.
 **/
-function valTab() {
+function valTab($rootScope) {
     return {
         require: ['^form', '^valFormManager'],
         restrict: "A",
         link: function (scope, element, attr, ctrs) {
 
             var valFormManager = ctrs[1];
-            var tabId = "tab" + scope.tab.id;                        
-            scope.tabHasError = false;
+            var tabId = "tab" + scope.tab.id;
+            scope.tabHasError = false;            
 
             //listen for form validation changes
             valFormManager.onValidationStatusChanged(function (evt, args) {
@@ -31,7 +31,15 @@ function valTab() {
                     scope.tabHasError = false;
                 }
             });
-
+            var tabShownFunc = function (e) {
+                var tabContent = element.closest(".umb-panel").find("#" + tabId);
+                $rootScope.$emit('valTab.tabShown', e, scope.tab, tabContent);
+            };
+            var anchorElement = element.find("a[data-toggle='tab']");
+            anchorElement.on('shown.bs.tab', tabShownFunc);
+            scope.$on('$destroy', function () {
+                anchorElement.off('shown.bs.tab', tabShownFunc);
+            });
         }
     };
 }
