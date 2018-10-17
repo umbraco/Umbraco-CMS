cbd36301680b2a4ead4e85ad4d61b0baabf2f0ee
Jan Skovgaard <jaskov@gmail.com>
2018-10-17 22:06:17 +0200

    Add editmode property to the $scope and check which controls should be visible when it's set to true

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/imagecropper/imagecropper.prevalues.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/imagecropper/imagecropper.prevalues.controller.js
@@ -5,6 +5,8 @@ angular.module("umbraco").controller("Umbraco.PrevalueEditors.CropSizesControlle
 	        $scope.model.value = [];
 	    }
 
+        $scope.editMode = false;
+
 	    $scope.remove = function (item, evt) {
 	        evt.preventDefault();
 	        $scope.model.value = _.reject($scope.model.value, function (x) {
@@ -13,29 +15,39 @@ angular.module("umbraco").controller("Umbraco.PrevalueEditors.CropSizesControlle
 	    };
 
 	    $scope.edit = function (item, evt) {
-	        evt.preventDefault();
+            evt.preventDefault();
+            $scope.editMode = true;
+
 	        $scope.newItem = item;
 	    };
 
 	    $scope.cancel = function (evt) {
-	        evt.preventDefault();
+            evt.preventDefault();
+            $scope.editMode = false;
+
 	        $scope.newItem = null;
 	    };
 
 	    $scope.add = function (evt) {
-	        evt.preventDefault();
+            evt.preventDefault();
+
+            $scope.editMode = false;
 
 	        if ($scope.newItem && $scope.newItem.alias &&
                 angular.isNumber($scope.newItem.width) && angular.isNumber($scope.newItem.height) &&
                 $scope.newItem.width > 0 && $scope.newItem.height > 0) {
 
-	            var exists = _.find($scope.model.value, function (item) { return $scope.newItem.alias === item.alias; });
+                var exists = _.find($scope.model.value, function (item) { return $scope.newItem.alias === item.alias; });
+
 	            if (!exists) {
 	                $scope.model.value.push($scope.newItem);
 	                $scope.newItem = {};
 	                $scope.hasError = false;
 	                return;
-	            }
+                }
+                else{
+                    $scope.newItem = null;
+                }
 	        }
 
 	        //there was an error, do the highlight (will be set back by the directive)
