26536db1c605be368b63c781fc95b7d4c33d9801
prjseal <prjseal@gmail.com>
2018-10-18 13:35:15 +0100

    - added helper for Umbraco.EditLink()

--- src/Umbraco.Web/UmbracoHelper.cs
+++ src/Umbraco.Web/UmbracoHelper.cs
@@ -1,7 +1,9 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Web;
+using System.Web.Mvc;
 using System.Xml.XPath;
 using Umbraco.Core;
 using Umbraco.Core.Dictionary;
@@ -1041,6 +1043,85 @@ namespace Umbraco.Web
         public HtmlString If(bool test, string valueIfTrue)
         {
             return test ? new HtmlString(valueIfTrue) : new HtmlString(string.Empty);
+        }
+
+        #endregion
+
+        #region EditLink
+
+        public enum EditLinkPosition
+        {
+            TopLeft = 0,
+            TopRight = 1,
+            BottomRight = 2,
+            BottomLeft = 3
+        }
+
+        public IHtmlString EditLink(IPublishedContent thisPage,
+            EditLinkPosition position = EditLinkPosition.TopLeft, string linkColour = "#00aea2", string editMessage = "Edit",
+            int margin = 10, int zindex = 999, string umbracoPath = "/umbraco",
+            int fontSize = 16, string outerPosition = "fixed", string linkPosition = "absolute",
+            string outerClassName = "edit-link-outer", string linkClassName = "edit-link-inner")
+        {
+            StringBuilder editLinkCode = new StringBuilder();
+            var userTicket = new HttpContextWrapper(HttpContext.Current).GetUmbracoAuthTicket();
+
+            if (userTicket != null)
+            {
+                string outerStyles = "display:block;";
+                if (outerPosition == "fixed")
+                {
+                    switch (position)
+                    {
+                        case EditLinkPosition.TopLeft:
+                            outerStyles += $"top:{margin}px;";
+                            outerStyles += $"left:{margin}px;";
+                            break;
+                        case EditLinkPosition.TopRight:
+                            outerStyles += $"top:{margin}px;";
+                            outerStyles += $"right:{margin}px;";
+                            break;
+                        case EditLinkPosition.BottomRight:
+                            outerStyles += $"bottom:{margin}px;";
+                            outerStyles += $"right:{margin}px;";
+                            break;
+                        case EditLinkPosition.BottomLeft:
+                            outerStyles += $"bottom:{margin}px;";
+                            outerStyles += $"left:{margin}px;";
+                            break;
+                    }
+                }
+
+                outerStyles += $"z-index:{zindex};";
+                outerStyles += $"position:{outerPosition};";
+
+                string umbracoEditContentUrl = $"{umbracoPath}#/content/content/edit/";
+
+                editLinkCode.Append($"<div");
+                editLinkCode.Append($" class=\"{outerClassName}\"");
+                if (!string.IsNullOrEmpty(outerStyles))
+                {
+                    editLinkCode.Append($" style=\"{outerStyles}\"");
+                }
+                editLinkCode.Append($">");
+
+                string linkStyles = $"color:{linkColour};";
+                linkStyles += $"font-size:{fontSize}px;";
+
+                editLinkCode.Append($"<a ");
+                editLinkCode.Append($" class=\"{linkClassName}\"");
+                if (!string.IsNullOrEmpty(linkStyles))
+                {
+                    editLinkCode.Append($"style={linkStyles}");
+                }
+
+                editLinkCode.Append($" target=\"_blank\"");
+                editLinkCode.Append($" href =\"{umbracoEditContentUrl}{thisPage.Id}\"");
+                editLinkCode.Append($">{editMessage}</a>");
+                editLinkCode.Append($"</div>");
+            }
+
+            return MvcHtmlString.Create(editLinkCode.ToString());
         }
 
         #endregion
