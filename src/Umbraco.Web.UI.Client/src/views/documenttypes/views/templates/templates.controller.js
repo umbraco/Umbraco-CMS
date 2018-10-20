d759c64706fb13fbe30da741d10aa1d94b0215c0
Søren Kottal <snielsen43@gmail.com>
2018-10-20 11:35:25 +0200

    adds ability to create template from doctype editor

--- src/Umbraco.Web.UI.Client/src/views/documenttypes/views/templates/templates.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/documenttypes/views/templates/templates.controller.js
@@ -9,7 +9,7 @@
 (function() {
     'use strict';
 
-    function TemplatesController($scope, entityResource, contentTypeHelper, $routeParams) {
+    function TemplatesController($scope, entityResource, contentTypeHelper, templateResource, $routeParams) {
 
         /* ----------- SCOPE VARIABLES ----------- */
 
@@ -21,22 +21,51 @@
 
         /* ---------- INIT ---------- */
 
-        init();
+        init(function () {
 
-        function init() {
+            // update placeholder template information on new doc types
+            if (!$routeParams.notemplate && $scope.model.id === 0) {
+                vm.updateTemplatePlaceholder = true;
+                vm.availableTemplates = contentTypeHelper.insertTemplatePlaceholder(vm.availableTemplates);
+            }
+        });
+
+        function init(callback) {
 
             entityResource.getAll("Template").then(function(templates){
 
                 vm.availableTemplates = templates;
 
-                // update placeholder template information on new doc types
-                if (!$routeParams.notemplate && $scope.model.id === 0) {
-                  vm.updateTemplatePlaceholder = true;
-                  vm.availableTemplates = contentTypeHelper.insertTemplatePlaceholder(vm.availableTemplates);
-                }
+                callback();
 
             });
 
+        }
+
+        vm.createTemplate = function () {
+            templateResource.getScaffold(-1).then(function (template) {
+
+                template.alias = $scope.model.alias;
+                template.name = $scope.model.name;
+
+                templateResource.save(template).then(function (savedTemplate) {
+
+                    init(function () {
+
+                        var newTemplate = vm.availableTemplates.filter(function (t) { return t.id === savedTemplate.id });
+                        if (newTemplate.length > 0) {
+                            $scope.model.allowedTemplates.push(newTemplate[0]);
+
+                            if ($scope.model.defaultTemplate === null) {
+                                $scope.model.defaultTemplate = newTemplate[0];
+                            }
+                        }
+
+                    });
+
+                });
+
+            });
         }
 
     }
