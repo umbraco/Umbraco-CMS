2bd3e961069272e60dcb2f178ebce0d2b6ab1837
Emma Burstow <emma.burstow@poweredbypie.co.uk>
2018-10-22 10:13:26 +0100

    Found the right place to change save message

--- src/Umbraco.Web/umbraco.presentation/umbraco/members/EditMemberGroup.aspx.cs
+++ src/Umbraco.Web/umbraco.presentation/umbraco/members/EditMemberGroup.aspx.cs
@@ -67,7 +67,7 @@ namespace umbraco.presentation.members
 			_memberGroup.Text = NameTxt.Text;
             memberGroupName.Value = NameTxt.Text;
             _memberGroup.Save();
-            this.ClientTools.ShowSpeechBubble(speechBubbleIcon.save, ui.Text("speechBubbles", "editMemberGroupSaved", base.getUser()),"");
+            this.ClientTools.ShowSpeechBubble(speechBubbleIcon.save, ui.Text("speechBubbles", "Member group saved", base.getUser()),"");
 
             ClientTools
                 .RefreshTree(TreeDefinitionCollection.Instance.FindTree<loadMemberGroups>().Tree.Alias);
