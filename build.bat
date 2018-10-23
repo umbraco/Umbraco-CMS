5f765967a6f9adf3b735c32fa8bac9f79558951d
skoomasteve <43890114+skoomasteve@users.noreply.github.com>
2018-10-23 15:47:14 -0500

    Just a few spelling fixes.  (#3405)

--- build.bat
+++ build.bat
@@ -9,6 +9,6 @@ IF ERRORLEVEL 1 (
 
 :error
 ECHO.
-ECHO Can not run build\build.ps1.
+ECHO Cannot run build\build.ps1.
 ECHO If this is due to a SecurityError then please refer to BUILD.md for help!
 ECHO.
