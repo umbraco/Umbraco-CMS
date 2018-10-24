62716fb417217ad86c1c21086c95976d165b5a52
Warren Buckley <warren@umbraco.com>
2018-10-24 11:03:41 +0100

    WIP disable tab index directive

--- src/Umbraco.Web.UI.Client/src/common/directives/util/disabletabindex.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/util/disabletabindex.directive.js
@@ -3,55 +3,78 @@ angular.module("umbraco.directives")
 
     return {
         restrict: 'A', //Can only be used as an attribute
+        scope: { disableTabindex: "<"},
         link: function (scope, element, attrs) {
 
-            //Use DOM Mutation Observer
-            //Select the node that will be observed for mutations (native DOM element not jQLite version)
-            var targetNode = element[0];
+            function enableTest(){
+                //Add in observer code
 
-            // Options for the observer (which mutations to observe)
-            var config = { attributes: false, childList: true, subtree: false };
+                //Use DOM Mutation Observer
+                //Select the node that will be observed for mutations (native DOM element not jQLite version)
+                var targetNode = element[0];
 
-            // Callback function to execute when mutations are observed
-            var callback = function(mutationsList, observer) {
-                for(var mutation of mutationsList) {
+                // Options for the observer (which mutations to observe)
+                var config = { attributes: false, childList: true, subtree: false };
 
-                    console.log('mutation', mutation);
+                // Callback function to execute when mutations are observed
+                var callback = function(mutationsList, observer) {
+                    for(var mutation of mutationsList) {
 
-                    //DOM items have been added or removed
-                    if (mutation.type == 'childList') {
+                        console.log('mutation', mutation);
 
-                        //Check if any child items in mutation.target contain an input
-                        var jqLiteEl = angular.element(mutation.target);
-                        var childInputs = jqLiteEl.find('input');
+                        //DOM items have been added or removed
+                        if (mutation.type == 'childList') {
 
-                        //For each item in childInputs - override or set HTML attribute tabindex="-1"
-                        angular.forEach(childInputs, function(element){
-                            console.log('item in loop', element);
+                            //Check if any child items in mutation.target contain an input
+                            var jqLiteEl = angular.element(mutation.target);
+                            var childInputs = jqLiteEl.find('input');
 
-                            //TODO: Note we updating way too many times from the DOMSubtreeModified event - is this expensive?
-                            angular.element(element).attr('tabindex', '-1');
-                        });
+                            //For each item in childInputs - override or set HTML attribute tabindex="-1"
+                            angular.forEach(childInputs, function(element){
+                                console.log('item in loop', element);
+
+                                //TODO: Get existing element & it's tabindex (if any set)
+
+
+                                //TODO: Note we updating way too many times from the DOMSubtreeModified event - is this expensive?
+                                angular.element(element).attr('tabindex', '-1');
+                            });
+                        }
                     }
-                }
+                };
+
+                // Create an observer instance linked to the callback function
+                var observer = new MutationObserver(callback);
+
+                // Start observing the target node for configured mutations
+                //GO GO GO
+                observer.observe(targetNode, config);
             };
 
-            // Create an observer instance linked to the callback function
-            var observer = new MutationObserver(callback);
 
-            // Start observing the target node for configured mutations
-            //GO GO GO
-            observer.observe(targetNode, config);
+            scope.$watch('disableTabindex',(newVal, oldVal) =>{
+                console.log('new val', newVal);
+
+                if(newVal === true){
+                    enableTest();
+                }else{
+
+                }
+            });
+
+
+
+
 
 
             //TODO: Unsure if we need to do this - to ensure the browser not trying to notify us still
             //When we browse away from the page
-            element.on('$destroy', function(e){
-                console.log('element with disable-tabindex attribute is destoryed');
+            // element.on('$destroy', function(e){
+            //     console.log('element with disable-tabindex attribute is destoryed');
 
-                //Remove/stop the observer
-                observer.disconnect();
-            });
+            //     //Remove/stop the observer
+            //     observer.disconnect();
+            // });
 
         }
     };
