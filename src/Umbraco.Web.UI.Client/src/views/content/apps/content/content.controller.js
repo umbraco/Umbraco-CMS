02221f6dd0c36505b66a8fc3bbfc2fdea2f51bb4
Shannon <sdeminick@gmail.com>
2018-10-26 15:07:21 +1100

    Fixes how the content app view model works so dirty tracking continues to work properly, previously we tried to omit the apps so we didn' thave a circualar model reference but that doesn't work because that clones the object, so now the view model is just the variant index which works much better.Fixes other js probs

--- src/Umbraco.Web.UI.Client/src/views/content/apps/content/content.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/content/apps/content/content.controller.js
@@ -3,33 +3,39 @@
 
     function ContentAppContentController($scope, $timeout, serverValidationManager) {
 
+        //the contentApp's viewModel is actually the index of the variant being edited, not the variant itself.
+        //if we make the viewModel the variant itself, we end up with a circular reference in the models which isn't ideal
+        // (i.e. variant.apps[contentApp].viewModel = variant)
+        //so instead since we already have access to the content, we can just get the variant directly by the index.
+
         var vm = this;
         vm.loading = true;
 
         function onInit() {
-            vm.content = $scope.model.viewModel;
+            //get the variant by index (see notes above)
+            vm.content = $scope.content.variants[$scope.model.viewModel];
             serverValidationManager.notify();
             vm.loading = false;
+
+            //if this variant has a culture/language assigned, then we need to watch it since it will change
+            //if the language drop down changes and we need to re-init
+            if (vm.content.language) {
+                $scope.$watch(function () {
+                    return vm.content.language.culture;
+                }, function (newVal, oldVal) {
+                    if (newVal !== oldVal) {
+                        vm.loading = true;
+
+                        //TODO: Can we minimize the flicker?
+                        $timeout(function () {
+                            onInit();
+                        }, 100);
+                    }
+                });
+            }
         }
 
         onInit();
-
-        //if this variant has a culture/language assigned, then we need to watch it since it will change
-        //if the language drop down changes and we need to re-init
-        if ($scope.model.viewModel.language) {
-            $scope.$watch(function () {
-                return $scope.model.viewModel.language.culture;
-            }, function (newVal, oldVal) {
-                if (newVal !== oldVal) {
-                    vm.loading = true;
-
-                    //TODO: Can we minimize the flicker?
-                    $timeout(function () {
-                        onInit();
-                    }, 100);
-                }
-            });
-        }
     }
 
     angular.module("umbraco").controller("Umbraco.Editors.Content.Apps.ContentController", ContentAppContentController);
