083de953113482ace9f9c8d9b386b593ba71f120
James Coxhead <jamescoxhead@users.noreply.github.com>
2018-10-27 23:37:08 +0100

    Stubbed out relation type controller and resource

---
+++ src/Umbraco.Web.UI.Client/src/common/resources/relationtype.resource.js
@@ -0,0 +1,40 @@
+/**
+ * @ngdoc service
+ * @name umbraco.resources.relationTypeResource
+ * @description Loads in data for relation types.
+ */
+function relationTypeResource($q, $http, umbRequestHelper) {
+    return {
+
+        /**
+         * @ngdoc method
+         * @name umbraco.resources.relationTypeResource#getById
+         * @methodOf umbraco.resources.relationTypeResource
+         * @param {Int} id of the dictionary item to get.
+         * @returns {Promise} resourcePromise containing relation type data.
+         */
+        getById: function (id) {
+            return umbRequestHelper.resourcePromise(
+                $http.get(
+                    umbRequestHelper.getApiUrl(
+                        "relationTypeBaseUrl",
+                        "GetById",
+                        [{ id: id }]
+                    )
+                ),
+                "Failed to get item " + id
+            );
+        },
+
+        save: function () {
+
+        },
+
+        deleteById: function (id) {
+
+        }
+
+    };
+}
+
+angular.module("umbraco.resources").factory("relationTypeResource", relationTypeResource);
---
+++ src/Umbraco.Web/Editors/RelationTypeController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Http;
+using AutoMapper;
+using Umbraco.Core.Models;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.WebApi.Filters;
+using Constants = Umbraco.Core.Constants;
+using RelationType = Umbraco.Web.Models.ContentEditing.RelationType;
+
+namespace Umbraco.Web.Editors
+{
+    /// <summary>
+    /// The API controller for editing relation types.
+    /// </summary>
+    [PluginController("UmbracoApi")]
+    [UmbracoTreeAuthorize(Constants.Trees.RelationTypes)]
+    [EnableOverrideAuthorization]
+    public class RelationTypeController : BackOfficeNotificationsController
+    {
+        /// <summary>
+        /// Gets a relation type by ID.
+        /// </summary>
+        /// <param name="id">The relation type ID.</param>
+        /// <returns>Returns the <see cref="RelationType"/>.</returns>
+        public RelationType GetById(int id)
+        {
+            var relationType = Services.RelationService.GetRelationTypeById(id);
+
+            if (relationType == null)
+            {
+                throw new HttpResponseException(HttpStatusCode.NotFound);
+            }
+
+            return Mapper.Map<IRelationType, RelationType>(relationType);
+        }
+
+        public void PostSave()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void DeleteById()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
