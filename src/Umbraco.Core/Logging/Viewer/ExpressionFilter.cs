436a6df917da0b3bea796da3c0471b12889db148
Warren Buckley <warren@umbraco.com>
2018-10-30 12:33:28 +0000

    Fix up log viewer - if viewing all log items & filterExpression is null or emtpy string return all log items

--- src/Umbraco.Core/Logging/Viewer/ExpressionFilter.cs
+++ src/Umbraco.Core/Logging/Viewer/ExpressionFilter.cs
@@ -15,30 +15,39 @@ namespace Umbraco.Core.Logging.Viewer
         {
             Func<LogEvent, bool> filter = null;
 
-            // If the expression is one word and doesn't contain a serilog operator then we can perform a like search
-            if (!filterExpression.Contains(" ") && !filterExpression.ContainsAny(expressionOperators.Select(c => c)))
+            if (string.IsNullOrEmpty(filterExpression) == false)
             {
-                filter = PerformMessageLikeFilter(filterExpression);
-            }
-            else // check if it's a valid expression
-            {
-                // If the expression evaluates then make it into a filter
-                if (FilterLanguage.TryCreateFilter(filterExpression, out Func<LogEvent, object> eval, out string error))
+                // If the expression is one word and doesn't contain a serilog operator then we can perform a like search
+                if (!filterExpression.Contains(" ") && !filterExpression.ContainsAny(expressionOperators.Select(c => c)))
                 {
-                    filter = evt => true.Equals(eval(evt));
-                }
-                else
-                {
-                    //Assume the expression was a search string and make a Like filter from that
                     filter = PerformMessageLikeFilter(filterExpression);
                 }
-            }
+                else // check if it's a valid expression
+                {
+                    // If the expression evaluates then make it into a filter
+                    if (FilterLanguage.TryCreateFilter(filterExpression, out Func<LogEvent, object> eval, out string error))
+                    {
+                        filter = evt => true.Equals(eval(evt));
+                    }
+                    else
+                    {
+                        //Assume the expression was a search string and make a Like filter from that
+                        filter = PerformMessageLikeFilter(filterExpression);
+                    }
+                }
 
-            _filter = filter;
+                _filter = filter;
+            }
         }
     
         public bool TakeLogEvent(LogEvent e)
         {
+            if(_filter == null)
+            {
+                //If no filter has been setup - take all log items
+                return true;
+            }
+
             return _filter(e);
         }
 
