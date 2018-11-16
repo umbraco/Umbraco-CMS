3598da8bd17a4c4117cece0af158bb24af76e070
Marcin Zajkowski <m.zajkowski@sicc.pl>
2018-11-16 22:56:35 +0100

    Added new dictionary item getter

--- src/Umbraco.Web.UI.Client/src/common/resources/dictionary.resource.js
+++ src/Umbraco.Web.UI.Client/src/common/resources/dictionary.resource.js
@@ -1,91 +1,91 @@
 ﻿/**
-    * @ngdoc service
-    * @name umbraco.resources.dictionaryResource
-    * @description Loads in data for dictionary items
-**/
+ * @ngdoc service
+ * @name umbraco.resources.dictionaryResource
+ * @description Loads in data for dictionary items
+ */
 function dictionaryResource($q, $http, $location, umbRequestHelper, umbDataFormatter) {
 
-  /**
-         * @ngdoc method
-         * @name umbraco.resources.dictionaryResource#deleteById
-         * @methodOf umbraco.resources.dictionaryResource
-         *
-         * @description
-         * Deletes a dictionary item with a given id
-         *
-         * ##usage
-         * <pre>
-         * dictionaryResource.deleteById(1234)
-         *    .then(function() {
-         *        alert('its gone!');
-         *    });
-         * </pre>
-         *
-         * @param {Int} id id of dictionary item to delete
-         * @returns {Promise} resourcePromise object.
-         *
-  **/
-  function deleteById(id) {
-    return umbRequestHelper.resourcePromise(
-      $http.post(
-        umbRequestHelper.getApiUrl(
-          "dictionaryApiBaseUrl",
-          "DeleteById",
-          [{ id: id }])),
-      "Failed to delete item " + id);
-  }
-
-  /**
-         * @ngdoc method
-         * @name umbraco.resources.dictionaryResource#create
-         * @methodOf umbraco.resources.dictionaryResource
-         *
-         * @description
-         * Creates a dictionary item with the gieven key and parent id
-         *
-         * ##usage
-         * <pre>
-         * dictionaryResource.create(1234,"Item key")
-         *    .then(function() {
-         *        alert('its created!');
-         *    });
-         * </pre>
-         *
-         * @param {Int} parentid the parentid of the new dictionary item
-         * @param {String} key the key of the new dictionary item
-         * @returns {Promise} resourcePromise object.
-         *
-  **/
-  function create(parentid, key) {
-    return umbRequestHelper.resourcePromise(
-      $http.post(
-        umbRequestHelper.getApiUrl(
-          "dictionaryApiBaseUrl",
-          "Create",
-          { parentId: parentid, key : key })),
-      "Failed to create item ");
-  }
+    /**
+     * @ngdoc method
+     * @name umbraco.resources.dictionaryResource#deleteById
+     * @methodOf umbraco.resources.dictionaryResource
+     *
+     * @description
+     * Deletes a dictionary item with a given id
+     *
+     * ##usage
+     * <pre>
+     * dictionaryResource.deleteById(1234)
+     *    .then(function() {
+     *        alert('its gone!');
+     *    });
+     * </pre>
+     *
+     * @param {Int} id id of dictionary item to delete
+     * @returns {Promise} resourcePromise object.
+     *
+     */
+    function deleteById(id) {
+        return umbRequestHelper.resourcePromise(
+            $http.post(
+                umbRequestHelper.getApiUrl(
+                    "dictionaryApiBaseUrl",
+                    "DeleteById",
+                    [{ id: id }])),
+            "Failed to delete item " + id);
+    }
 
     /**
-         * @ngdoc method
-         * @name umbraco.resources.dictionaryResource#deleteById
-         * @methodOf umbraco.resources.dictionaryResource
-         *
-         * @description
-         * Gets a dictionary item with a given id
-         *
-         * ##usage
-         * <pre>
-         * dictionaryResource.getById(1234)
-         *    .then(function() {
-         *        alert('Found it!');
-         *    });
-         * </pre>
-         *
-         * @param {Int} id id of dictionary item to get
-         * @returns {Promise} resourcePromise object.
-         *
-  **/
+     * @ngdoc method
+     * @name umbraco.resources.dictionaryResource#create
+     * @methodOf umbraco.resources.dictionaryResource
+     *
+     * @description
+     * Creates a dictionary item with the gieven key and parent id
+     *
+     * ##usage
+     * <pre>
+     * dictionaryResource.create(1234,"Item key")
+     *    .then(function() {
+     *        alert('its created!');
+     *    });
+     * </pre>
+     *
+     * @param {Int} parentid the parentid of the new dictionary item
+     * @param {String} key the key of the new dictionary item
+     * @returns {Promise} resourcePromise object.
+     *
+     */
+    function create(parentid, key) {
+        return umbRequestHelper.resourcePromise(
+            $http.post(
+                umbRequestHelper.getApiUrl(
+                    "dictionaryApiBaseUrl",
+                    "Create",
+                    { parentId: parentid, key: key })),
+            "Failed to create item ");
+    }
+
+    /**
+     * @ngdoc method
+     * @name umbraco.resources.dictionaryResource#getById
+     * @methodOf umbraco.resources.dictionaryResource
+     *
+     * @description
+     * Gets a dictionary item with a given id
+     *
+     * ##usage
+     * <pre>
+     * dictionaryResource.getById(1234)
+     *    .then(function() {
+     *        alert('Found it!');
+     *    });
+     * </pre>
+     *
+     * @param {Int} id id of dictionary item to get
+     * @returns {Promise} resourcePromise object.
+     *
+     */
     function getById(id) {
         return umbRequestHelper.resourcePromise(
             $http.get(
@@ -97,46 +97,76 @@ function dictionaryResource($q, $http, $location, umbRequestHelper, umbDataForma
     }
 
     /**
-        * @ngdoc method
-        * @name umbraco.resources.dictionaryResource#save
-        * @methodOf umbraco.resources.dictionaryResource
-        *
-        * @description
-        * Updates a dictionary
-        *
-        * @param {Object} dictionary  dictionary object to update     
-        * @param {Bool} nameIsDirty set to true if the name has been changed
-        * @returns {Promise} resourcePromise object.
-        *
-        */
+     * @ngdoc method
+     * @name umbraco.resources.dictionaryResource#getByKey
+     * @methodOf umbraco.resources.dictionaryResource
+     *
+     * @description
+     * Gets a dictionary item with a given key
+     *
+     * ##usage
+     * <pre>
+     * dictionaryResource.getByKey('general_add')
+     *    .then(function() {
+     *        alert('Found it!');
+     *    });
+     * </pre>
+     *
+     * @param {String} key key of dictionary item to get
+     * @returns {Promise} resourcePromise object.
+     *
+     */
+    function getByKey(key) {
+        return umbRequestHelper.resourcePromise(
+            $http.get(
+                umbRequestHelper.getApiUrl(
+                    "dictionaryApiBaseUrl",
+                    "GetByKey",
+                    [{ key: key }])),
+            "Failed to get item " + key);
+    }
+
+    /**
+     * @ngdoc method
+     * @name umbraco.resources.dictionaryResource#save
+     * @methodOf umbraco.resources.dictionaryResource
+     *
+     * @description
+     * Updates a dictionary
+     *
+     * @param {Object} dictionary  dictionary object to update     
+     * @param {Bool} nameIsDirty set to true if the name has been changed
+     * @returns {Promise} resourcePromise object.
+     *
+     */
     function save(dictionary, nameIsDirty) {
 
         var saveModel = umbDataFormatter.formatDictionaryPostData(dictionary, nameIsDirty);
 
-       return umbRequestHelper.resourcePromise(
+        return umbRequestHelper.resourcePromise(
             $http.post(umbRequestHelper.getApiUrl("dictionaryApiBaseUrl", "PostSave"), saveModel),
             "Failed to save data for dictionary id " + dictionary.id);
     }
 
     /**
-         * @ngdoc method
-         * @name umbraco.resources.dictionaryResource#getList
-         * @methodOf umbraco.resources.dictionaryResource
-         *
-         * @description
-         * Gets a list of all dictionary items
-         *
-         * ##usage
-         * <pre>
-         * dictionaryResource.getList()
-         *    .then(function() {
-         *        alert('Found it!');
-         *    });
-         * </pre>
-         *         
-         * @returns {Promise} resourcePromise object.
-         *
-  **/
+     * @ngdoc method
+     * @name umbraco.resources.dictionaryResource#getList
+     * @methodOf umbraco.resources.dictionaryResource
+     *
+     * @description
+     * Gets a list of all dictionary items
+     *
+     * ##usage
+     * <pre>
+     * dictionaryResource.getList()
+     *    .then(function() {
+     *        alert('Found it!');
+     *    });
+     * </pre>
+     *         
+     * @returns {Promise} resourcePromise object.
+     *
+     */
     function getList() {
         return umbRequestHelper.resourcePromise(
             $http.get(
@@ -145,18 +175,18 @@ function dictionaryResource($q, $http, $location, umbRequestHelper, umbDataForma
                     "getList")),
             "Failed to get list");
     }
-    
-  var resource = {
-    deleteById: deleteById,
-    create: create,
-    getById: getById,
-    save: save,
-    getList : getList
-  };
 
-  return resource;
+    var resource = {
+        deleteById: deleteById,
+        create: create,
+        getById: getById,
+        getByKey: getByKey,
+        save: save,
+        getList: getList
+    };
 
-  
+    return resource;
+
 }
 
 angular.module("umbraco.resources").factory("dictionaryResource", dictionaryResource);
