0aa6bd555293104250041192a7f033cee0cde521
Bjarke Berg <mail@bergmania.dk>
2018-11-22 11:57:45 +0100

    #3545 - nullable instead of 0 when serialized

--- src/Umbraco.Tests/Models/ContentXmlTest.cs
+++ src/Umbraco.Tests/Models/ContentXmlTest.cs
@@ -49,7 +49,7 @@ namespace Umbraco.Tests.Models
             Assert.AreEqual(content.GetCreatorProfile(ServiceContext.UserService).Name, (string)element.Attribute("creatorName"));
             Assert.AreEqual(content.GetWriterProfile(ServiceContext.UserService).Name, (string)element.Attribute("writerName"));
             Assert.AreEqual(content.WriterId.ToString(), (string)element.Attribute("writerID"));
-            Assert.AreEqual((content.TemplateId ?? 0).ToString(), (string)element.Attribute("template"));
+            Assert.AreEqual(content.TemplateId?.ToString() ?? string.Empty, (string)element.Attribute("template"));
 
             Assert.AreEqual(content.Properties["title"].GetValue().ToString(), element.Elements("title").Single().Value);
             Assert.AreEqual(content.Properties["bodyText"].GetValue().ToString(), element.Elements("bodyText").Single().Value);
