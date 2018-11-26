27c2b3ba5e4a39d6a16acc89ed419eb5db47948e
Stephan <stephane@umbraco.dk>
2018-11-26 14:43:19 +0100

    Cleanup

--- src/Umbraco.Web/Actions/ActionCollectionBuilder.cs
+++ src/Umbraco.Web/Actions/ActionCollectionBuilder.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Reflection;
 using LightInject;
 using Umbraco.Core.Composing;
-
 
 namespace Umbraco.Web.Actions
 {
@@ -19,13 +17,13 @@ namespace Umbraco.Web.Actions
         protected override IEnumerable<IAction> CreateItems(params object[] args)
         {
             var items = base.CreateItems(args).ToList();
+
             //validate the items, no actions should exist that do not either expose notifications or permissions
-            var invalid = items.Where(x => !x.CanBePermissionAssigned && !x.ShowInNotifier).ToList();
-            if (invalid.Count > 0)
-            {
-                throw new InvalidOperationException($"Invalid actions '{string.Join(", ", invalid.Select(x => x.Alias))}'. All {typeof(IAction)} implementations must be true for either {nameof(IAction.CanBePermissionAssigned)} or {nameof(IAction.ShowInNotifier)}");
-            }
-            return items;
+            var invalidItems = items.Where(x => !x.CanBePermissionAssigned && !x.ShowInNotifier).ToList();
+            if (invalidItems.Count == 0) return items;
+
+            var invalidActions = string.Join(", ", invalidItems.Select(x => "'" + x.Alias + "'"));
+            throw new InvalidOperationException($"Invalid actions {invalidActions}'. All {typeof(IAction)} implementations must be true for either {nameof(IAction.CanBePermissionAssigned)} or {nameof(IAction.ShowInNotifier)}.");
         }
     }
 }
