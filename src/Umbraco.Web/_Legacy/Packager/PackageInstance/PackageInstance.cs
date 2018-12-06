c7e6cf44ed019e4e99c493c786e02e7b4c079301
Warren Buckley <warren@umbraco.com>
2018-12-06 11:21:01 +0000

    Anotate model so its camelCase friendly for API responses for Mads

--- src/Umbraco.Web/_Legacy/Packager/PackageInstance/PackageInstance.cs
+++ src/Umbraco.Web/_Legacy/Packager/PackageInstance/PackageInstance.cs
@@ -1,70 +1,97 @@
 using System;
-
-using System.Xml;
-using System.Xml.XPath;
 using System.Collections.Generic;
+using System.Runtime.Serialization;
 
 namespace umbraco.cms.businesslogic.packager
 {
+    [DataContract(Name = "packageInstance")]
     public class PackageInstance
     {
+        [DataMember(Name = "id")]
         public int Id { get; set; }
 
+        [DataMember(Name = "repositoryGuid")]
         public string RepositoryGuid { get; set; }
 
+        [DataMember(Name = "packageGuid")]
         public string PackageGuid { get; set; }
 
+        [DataMember(Name = "hasUpdate")]
         public bool HasUpdate { get; set; }
 
+        [DataMember(Name = "name")]
         public string Name { get; set; }
 
+        [DataMember(Name = "url")]
         public string Url { get; set; }
 
+        [DataMember(Name = "folder")]
         public string Folder { get; set; }
 
+        [DataMember(Name = "packagePath")]
         public string PackagePath { get; set; }
 
+        [DataMember(Name = "version")]
         public string Version { get; set; }
 
         /// <summary>
         /// The minimum umbraco version that this package requires
         /// </summary>
+        [DataMember(Name = "umbracoVersion")]
         public Version UmbracoVersion { get; set; }
 
+        [DataMember(Name = "author")]
         public string Author { get; set; }
 
+        [DataMember(Name = "authorUrl")]
         public string AuthorUrl { get; set; }
 
+        [DataMember(Name = "license")]
         public string License { get; set; }
 
+        [DataMember(Name = "licenseUrl")]
         public string LicenseUrl { get; set; }
 
+        [DataMember(Name = "readme")]
         public string Readme { get; set; }
 
+        [DataMember(Name = "contentLoadChildNodes")]
         public bool ContentLoadChildNodes { get; set; }
 
+        [DataMember(Name = "contentNodeId")]
         public string ContentNodeId { get; set; }
 
+        [DataMember(Name = "macros")]
         public List<string> Macros { get; set; }
 
+        [DataMember(Name = "languages")]
         public List<string> Languages { get; set; }
 
+        [DataMember(Name = "dictionaryItems")]
         public List<string> DictionaryItems { get; set; }
 
+        [DataMember(Name = "templates")]
         public List<string> Templates { get; set; }
 
+        [DataMember(Name = "documenttypes")]
         public List<string> Documenttypes { get; set; }
 
+        [DataMember(Name = "stylesheets")]
         public List<string> Stylesheets { get; set; }
 
+        [DataMember(Name = "files")]
         public List<string> Files { get; set; }
 
+        [DataMember(Name = "loadControl")]
         public string LoadControl { get; set; }
 
+        [DataMember(Name = "actions")]
         public string Actions { get; set; }
 
+        [DataMember(Name = "dataTypes")]
         public List<string> DataTypes { get; set; }
 
+        [DataMember(Name = "iconUrl")]
         public string IconUrl { get; set; }
 
         public PackageInstance()
