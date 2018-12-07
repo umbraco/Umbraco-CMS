04b2765d72f4200da79869414442e949f1b30898
Stephan <stephane@umbraco.dk>
2018-12-07 09:03:36 +0100

    Document

--- src/Umbraco.Core/PropertyEditors/IPropertyIndexValueFactory.cs
+++ src/Umbraco.Core/PropertyEditors/IPropertyIndexValueFactory.cs
@@ -11,6 +11,14 @@ namespace Umbraco.Core.PropertyEditors
         /// <summary>
         /// Gets the index values for a property.
         /// </summary>
+        /// <remarks>
+        /// <para>Returns key-value pairs, where keys are indexed field names. By default, that would be the property alias,
+        /// and there would be only one pair, but some implementations (see for instance the grid one) may return more than
+        /// one pair, with different indexed field names.</para>
+        /// <para>And then, values are an enumerable of objects, because each indexed field can in turn have multiple
+        /// values. By default, there would be only one object: the property value. But some implementations may return
+        /// more than one value for a given field.</para>
+        /// </remarks>
         IEnumerable<KeyValuePair<string, IEnumerable<object>>> GetIndexValues(Property property, string culture, string segment, bool published);
     }
 }
