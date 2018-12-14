2a61d0d98814b47450435f20bb29745f9018e3a3
Tristan Thompson <tristankoffee@gmail.com>
2018-12-14 11:28:00 +0000

    Remove/Replace ping.aspx
    Remove ping.aspx
    Add KeepAliveController to receive ping requests
    Update KeepAlive.cs to call new MVC route rather than ping.aspx

---
+++ src/Umbraco.Web/Editors/KeepAliveController.cs
@@ -0,0 +1,28 @@
+﻿using System.Runtime.Serialization;
+using System.Web.Http;
+using Umbraco.Web.WebApi;
+
+namespace Umbraco.Web.Editors
+{
+    public class KeepAliveController : UmbracoApiController
+    {
+        [HttpGet]
+        public KeepAlivePingResult Ping()
+        {
+            return new KeepAlivePingResult
+            {
+                Success = true,
+                Message = "I'm alive!"
+            };
+        }   
+    }
+
+    public class KeepAlivePingResult
+    {
+        [DataMember(Name = "success")]
+        public bool Success { get; set; }
+
+        [DataMember(Name = "message")]
+        public string Message { get; set; }
+    }
+}
