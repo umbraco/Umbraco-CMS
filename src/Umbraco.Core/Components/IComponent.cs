0481c2c7366ccfa779da58af6ccc69be3b5cabc0
Stephan <stephane@umbraco.dk>
2019-01-03 21:10:13 +0100

    Fix components

---
+++ src/Umbraco.Core/Components/IComponent.cs
@@ -0,0 +1,14 @@
+﻿namespace Umbraco.Core.Components
+{
+    /// <summary>
+    /// Represents a component.
+    /// </summary>
+    /// <remarks>
+    /// <para>All components which are also disposable, will be disposed in reverse
+    /// order, when Umbraco terminates.</para>
+    /// <para>The Dispose method may be invoked more than once, and components
+    /// should ensure they support this.</para>
+    /// </remarks>
+    public interface IComponent
+    { }
+}
---
+++ src/Umbraco.Core/Components/IUserComposer.cs
@@ -0,0 +1,12 @@
+﻿namespace Umbraco.Core.Components
+{
+    /// <summary>
+    /// Represents a user <see cref="IComposer"/>.
+    /// </summary>
+    /// <remarks>
+    /// <para>All user composers require (compose after) all core composers.</para>
+    /// </remarks>
+    [Require(typeof(ICoreComposer))]
+    public interface IUserComposer : IComposer
+    { }
+}
