003d54cacd06d1796c537fa87f62b7b3bbddd446
Claus <claus@umbraco.com>
2019-01-07 12:59:38 +0100

    cleanup usings

--- src/Umbraco.Web/Trees/LegacyTreeDataConverter.cs
+++ src/Umbraco.Web/Trees/LegacyTreeDataConverter.cs
@@ -1,15 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net.Http.Formatting;
-using System.Web.Http.Routing;
 using Umbraco.Core;
-using Umbraco.Core.Configuration;
 using Umbraco.Core.Services;
 using Umbraco.Web.Actions;
 
 using Umbraco.Web.Composing;
-using Umbraco.Web.Models.Trees;
 
 namespace Umbraco.Web.Trees
 {
@@ -78,6 +72,5 @@ namespace Umbraco.Web.Trees
             public ActionUrlMethod ActionMethod { get; private set; }
             public string DialogTitle { get; private set; }
         }
-
     }
 }
