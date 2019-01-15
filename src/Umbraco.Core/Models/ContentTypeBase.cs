11ffafacdf42a461b793df8a3e32fda1c88b6a4f
Bjarke Berg <mail@bergmania.dk>
2019-01-15 15:12:59 +0100

    Revert "Revert "Identify element types""
    This reverts commit ff6a9ba9

--- src/Umbraco.Core/Models/ContentTypeBase.cs
+++ src/Umbraco.Core/Models/ContentTypeBase.cs
@@ -26,6 +26,7 @@ namespace Umbraco.Core.Models
         private string _thumbnail = "folder.png";
         private bool _allowedAsRoot; // note: only one that's not 'pure element type'
         private bool _isContainer;
+        private bool _isElement;
         private PropertyGroupCollection _propertyGroups;
         private PropertyTypeCollection _noGroupPropertyTypes;
         private IEnumerable<ContentTypeSort> _allowedContentTypes;
@@ -90,6 +91,7 @@ namespace Umbraco.Core.Models
             public readonly PropertyInfo IconSelector = ExpressionHelper.GetPropertyInfo<ContentTypeBase, string>(x => x.Icon);
             public readonly PropertyInfo ThumbnailSelector = ExpressionHelper.GetPropertyInfo<ContentTypeBase, string>(x => x.Thumbnail);
             public readonly PropertyInfo AllowedAsRootSelector = ExpressionHelper.GetPropertyInfo<ContentTypeBase, bool>(x => x.AllowedAsRoot);
+            public readonly PropertyInfo IsElementSelector = ExpressionHelper.GetPropertyInfo<ContentTypeBase, bool>(x => x.IsElement);
             public readonly PropertyInfo IsContainerSelector = ExpressionHelper.GetPropertyInfo<ContentTypeBase, bool>(x => x.IsContainer);
             public readonly PropertyInfo AllowedContentTypesSelector = ExpressionHelper.GetPropertyInfo<ContentTypeBase, IEnumerable<ContentTypeSort>>(x => x.AllowedContentTypes);
             public readonly PropertyInfo PropertyGroupsSelector = ExpressionHelper.GetPropertyInfo<ContentTypeBase, PropertyGroupCollection>(x => x.PropertyGroups);
@@ -178,6 +180,14 @@ namespace Umbraco.Core.Models
         {
             get => _isContainer;
             set => SetPropertyValueAndDetectChanges(value, ref _isContainer, Ps.Value.IsContainerSelector);
+        }
+
+        /// <inheritdoc />
+        [DataMember]
+        public bool IsElement
+        {
+            get => _isElement;
+            set => SetPropertyValueAndDetectChanges(value, ref _isElement, Ps.Value.IsElementSelector);
         }
 
         /// <summary>
