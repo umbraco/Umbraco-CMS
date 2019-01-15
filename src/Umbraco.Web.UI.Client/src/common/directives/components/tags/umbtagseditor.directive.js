9bc047c03f50e4795fc0d13b71af8218889d1e5a
Robert <copilaurobert@gmail.com>
2019-01-15 10:17:26 +0100

    Adding code to re-validate on value change

--- src/Umbraco.Web.UI.Client/src/common/directives/components/tags/umbtagseditor.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/tags/umbtagseditor.directive.js
@@ -182,6 +182,9 @@
             else {
                 vm.onValueChanged({ value: [] });
             }
+            
+            //this is required to re-validate
+            $scope.propertyForm.tagCount.$setViewValue($scope.model.value.length);
         }
 
         /**
@@ -189,7 +192,7 @@
          */
         function validateMandatory() {
             return {
-                isValid: !vm.validation.mandatory || (vm.viewModel != null && vm.viewModel.length > 0) || (vm.value != null && vm.value.length > 0),
+                isValid: !vm.validation.mandatory || (vm.viewModel != null && vm.viewModel.length > 0),
                 errorMsg: "Value cannot be empty",
                 errorKey: "required"
             };
