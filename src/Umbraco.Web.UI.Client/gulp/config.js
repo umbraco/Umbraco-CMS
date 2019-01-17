591d100726842828be9edc3b88eb6fed0a60eec1
Niels Lyngsø <nsl@umbraco.dk>
2019-01-17 15:07:18 +0100

    Corrected paths

--- src/Umbraco.Web.UI.Client/gulp/config.js
+++ src/Umbraco.Web.UI.Client/gulp/config.js
@@ -6,30 +6,30 @@ module.exports = {
         //less files used by backoffice and preview
         //processed in the less task
         less: {
-            installer: { files: ["src/less/installer.less"], out: "installer.css" },
-            nonodes: { files: ["src/less/pages/nonodes.less"], out: "nonodes.style.min.css"},
-            preview: { files: ["src/less/canvas-designer.less"], out: "canvasdesigner.css" },
-            umbraco: { files: ["src/less/belle.less"], out: "umbraco.css" }
+            installer: { files: ["./src/less/installer.less"], out: "installer.css" },
+            nonodes: { files: ["./src/less/pages/nonodes.less"], out: "nonodes.style.min.css"},
+            preview: { files: ["./src/less/canvas-designer.less"], out: "canvasdesigner.css" },
+            umbraco: { files: ["./src/less/belle.less"], out: "umbraco.css" }
         },
 
         //js files for backoffie
         //processed in the js task
         js: {
-            preview: { files: ["src/preview/**/*.js"], out: "umbraco.preview.js" },
-            installer: { files: ["src/installer/**/*.js"], out: "umbraco.installer.js" },
-            controllers: { files: ["src/{views,controllers}/**/*.controller.js"], out: "umbraco.controllers.js" },
-            directives: { files: ["src/common/directives/**/*.js"], out: "umbraco.directives.js" },
-            filters: { files: ["src/common/filters/**/*.js"], out: "umbraco.filters.js" },
-            resources: { files: ["src/common/resources/**/*.js"], out: "umbraco.resources.js" },
-            services: { files: ["src/common/services/**/*.js"], out: "umbraco.services.js" },
-            security: { files: ["src/common/interceptors/**/*.js"], out: "umbraco.interceptors.js" }
+            preview: { files: ["./src/preview/**/*.js"], out: "umbraco.preview.js" },
+            installer: { files: ["./src/installer/**/*.js"], out: "umbraco.installer.js" },
+            controllers: { files: ["./src/{views,controllers}/**/*.controller.js"], out: "umbraco.controllers.js" },
+            directives: { files: ["./src/common/directives/**/*.js"], out: "umbraco.directives.js" },
+            filters: { files: ["./src/common/filters/**/*.js"], out: "umbraco.filters.js" },
+            resources: { files: ["./src/common/resources/**/*.js"], out: "umbraco.resources.js" },
+            services: { files: ["./src/common/services/**/*.js"], out: "umbraco.services.js" },
+            security: { files: ["./src/common/interceptors/**/*.js"], out: "umbraco.interceptors.js" }
         },
 
         //selectors for copying all views into the build
         //processed in the views task
         views:{
-            umbraco: {files: ["src/views/**/*.html"], folder: ""},
-            installer: {files: ["src/installer/steps/*.html"], folder: "install"}
+            umbraco: {files: ["./src/views/**/*.html"], folder: ""},
+            installer: {files: ["./src/installer/steps/*.html"], folder: "install/"}
         },
 
         //globs for file-watching
