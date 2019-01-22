e99fe5ab0f5b13e96cbc56cf64061445ee09035b
Sebastiaan Janssen <sebastiaan@umbraco.com>
2019-01-22 23:24:01 +0100

    Update MediaExtensions.cs

--- src/Umbraco.Core/Models/MediaExtensions.cs
+++ src/Umbraco.Core/Models/MediaExtensions.cs
@@ -21,7 +21,7 @@ namespace Umbraco.Core.Models
             var val = media.Properties[propertyType];
             if (val == null) return string.Empty;
 
-            //fixme doesn't take into account variants
+            //todo would need to be adjusted to variations, when media become variants
             var jsonString = val.GetValue() as string;
             if (jsonString == null) return string.Empty;
 
