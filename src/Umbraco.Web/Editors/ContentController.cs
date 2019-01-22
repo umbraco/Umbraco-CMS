a8d5bd6d1fa30cc7fa678ceb274db5cec35ed2c0
Kenn Jacobsen <post@kennjacobsen.dk>
2019-01-22 22:05:12 +0100

    Don't let the users empty the recycle bin without delete permissions (#3699)

--- src/Umbraco.Web/Editors/ContentController.cs
+++ src/Umbraco.Web/Editors/ContentController.cs
@@ -847,7 +847,7 @@ namespace Umbraco.Web.Editors
         /// </remarks>
         [HttpDelete]
         [HttpPost]
-        [EnsureUserPermissionForContent(Constants.System.RecycleBinContent)]
+        [EnsureUserPermissionForContent(Constants.System.RecycleBinContent, 'D')]
         public HttpResponseMessage EmptyRecycleBin()
         {
             Services.ContentService.EmptyRecycleBin();
