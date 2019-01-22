62617537fa55def7e9d631e636c4e396c129665b
Warren Buckley <warren@umbraco.com>
2019-01-22 10:05:14 +0000

    Change icon from scary bug to box-alt

--- src/Umbraco.Web/Trees/LogViewerTreeController.cs
+++ src/Umbraco.Web/Trees/LogViewerTreeController.cs
@@ -34,7 +34,7 @@ namespace Umbraco.Web.Trees
 
             //this will load in a custom UI instead of the dashboard for the root node
             root.RoutePath = string.Format("{0}/{1}/{2}", Constants.Applications.Settings, Constants.Trees.LogViewer, "overview");
-            root.Icon = "icon-bug";
+            root.Icon = "icon-box-alt";
             root.HasChildren = false;
             root.MenuUrl = null;
 
