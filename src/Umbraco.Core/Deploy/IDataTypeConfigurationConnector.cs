3475f536df6d75a857f7e03136f4e2190bae86ba
Chris Houston <chouston@vizioz.com>
2019-01-23 08:39:03 -0500

    Fixed a few typos I spotted in the last commit :)

--- src/Umbraco.Core/Deploy/IDataTypeConfigurationConnector.cs
+++ src/Umbraco.Core/Deploy/IDataTypeConfigurationConnector.cs
@@ -4,11 +4,11 @@ using System.Diagnostics.CodeAnalysis;
 namespace Umbraco.Core.Deploy
 {
     /// <summary>
-    /// Defines methods that can convert a data type configurations to / from an environment-agnostic string.
+    /// Defines methods that can convert data type configuration to / from an environment-agnostic string.
     /// </summary>
-    /// <remarks>Configurations may contain values such as content identifiers, that would be local
+    /// <remarks>Configuration may contain values such as content identifiers, that would be local
     /// to one environment, and need to be converted in order to be deployed.</remarks>
-    [SuppressMessage("ReSharper", "UnusedMember.Global", Justification = "This is actual only used by Deploy, but we dont want third parties to have references on deploy, thats why this interface is part of core.")]
+    [SuppressMessage("ReSharper", "UnusedMember.Global", Justification = "This is actual only used by Deploy, but we don't want third parties to have references on deploy, that's why this interface is part of core.")]
     public interface IDataTypeConfigurationConnector
     {
         /// <summary>
@@ -17,7 +17,7 @@ namespace Umbraco.Core.Deploy
         IEnumerable<string> PropertyEditorAliases { get; }
 
         /// <summary>
-        /// Gets the environment-agnostic data type configurations corresponding to environment-specific configurations.
+        /// Gets the environment-agnostic data type configuration corresponding to environment-specific configuration.
         /// </summary>
         /// <param name="configuration">The environment-specific configuration.</param>
         /// <param name="dependencies">The dependencies.</param>
@@ -25,7 +25,7 @@ namespace Umbraco.Core.Deploy
         IDictionary<string, string> ConvertToDeploy(IDictionary<string, string> configuration, ICollection<ArtifactDependency> dependencies);
 
         /// <summary>
-        /// Gets the environment-specific data type configurations corresponding to environment-agnostic configurations.
+        /// Gets the environment-specific data type configuration corresponding to environment-agnostic configuration.
         /// </summary>
         /// <param name="configuration">The environment-agnostic configuration.</param>
         /// <returns></returns>
