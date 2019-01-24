85da2a94b92a8f527843f30b5191e959cb75bebe
Warren Buckley <warren@umbraco.com>
2019-01-24 14:09:28 +0000

    Bye 404Handlers XML config file - add/remove IContentFinders in your own composer/component

--- src/Umbraco.Core/IO/SystemFiles.cs
+++ src/Umbraco.Core/IO/SystemFiles.cs
@@ -10,8 +10,6 @@ namespace Umbraco.Core.IO
         public static string CreateUiXml => SystemDirectories.Umbraco + "/config/create/UI.xml";
 
         public static string TinyMceConfig => SystemDirectories.Config + "/tinyMceConfig.config";
-
-        public static string NotFoundhandlersConfig => SystemDirectories.Config + "/404handlers.config";
         
         public static string GetContentCacheXml(IGlobalSettings globalSettings)
         {
