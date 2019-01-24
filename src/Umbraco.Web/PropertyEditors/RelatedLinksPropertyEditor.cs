9e0d2e58732c4ac00baa369451da5e855c39cc08
Sebastiaan Janssen <sebastiaan@umbraco.com>
2019-01-24 11:37:28 +0100

    Revert "Remove obsoleted RelatedLinks picker (replaced by Multi URL Picker)"
    This reverts commit fedf0c78de6d2ad365db1c112c22af13abd7b9fb.

---
+++ src/Umbraco.Web/PropertyEditors/RelatedLinksPropertyEditor.cs
@@ -0,0 +1,16 @@
+﻿using Umbraco.Core;
+using Umbraco.Core.Logging;
+using Umbraco.Core.PropertyEditors;
+
+namespace Umbraco.Web.PropertyEditors
+{
+    [DataEditor(Constants.PropertyEditors.Aliases.RelatedLinks, "Related links", "relatedlinks", ValueType = ValueTypes.Json, Icon = "icon-thumbnail-list", Group = "pickers")]
+    public class RelatedLinksPropertyEditor : DataEditor
+    {
+        public RelatedLinksPropertyEditor(ILogger logger)
+            : base(logger)
+        { }
+
+        protected override IConfigurationEditor CreateConfigurationEditor() => new RelatedLinksConfigurationEditor();
+    }
+}
