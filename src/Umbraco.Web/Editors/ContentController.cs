36cc06d4860944700217e09314f4de331e8900ec
Jan Skovgaard <jaskov@gmail.com>
2019-01-27 17:14:01 +0100

    Add change for dc item "contentPublishedFailedAwaitingRelease"

--- src/Umbraco.Web/Editors/ContentController.cs
+++ src/Umbraco.Web/Editors/ContentController.cs
@@ -1074,7 +1074,7 @@ namespace Umbraco.Web.Editors
                     display.AddWarningNotification(
                             Services.TextService.Localize("publish"),
                             Services.TextService.Localize("publish/contentPublishedFailedAwaitingRelease",
-                                    new[] { string.Format("{0} ({1})", status.ContentItem.Name, status.ContentItem.Id) }).Trim());
+                            new[] { status.ContentItem.Name, status.ContentItem.Id.ToString() }));
                     break;
                 case PublishStatusType.FailedHasExpired:
                     display.AddWarningNotification(
