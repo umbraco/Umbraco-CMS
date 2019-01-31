4a480ab3918be8f0bce3c57027bd2ce750a361ba
Bjarke Berg <mail@bergmania.dk>
2019-01-31 15:10:19 +0100

    #4068 - Added interceptor to remove `$` variables from post data

---
+++ src/Umbraco.Web.UI.Client/src/common/interceptors/donotpostdollarvariablesrequest.interceptor.js
@@ -0,0 +1,38 @@
+﻿(function() {
+   'use strict';
+   
+    function removeProperty(obj, propertyPrefix) {
+        for (var property in obj) {
+            if (obj.hasOwnProperty(property)) {
+                
+                if (property.startsWith(propertyPrefix) && obj[property]) {
+                    obj[property] = undefined;
+                }
+                
+                if (typeof obj[property] == "object") {
+                    removeProperty(obj[property], propertyPrefix);
+                }
+            }
+        }
+        
+    }
+    
+    function transform(data){
+        removeProperty(data, "$");
+    }
+    
+    function doNotPostDollarVariablesRequestInterceptor($q, urlHelper) {
+        return {
+            //dealing with requests:
+            'request': function(config) {
+                if(config.method === "POST"){
+                    config.transformRequest.push(transform);
+                }
+                
+                return config;
+            }
+        };
+   }
+
+    angular.module('umbraco.interceptors').factory('doNotPostDollarVariablesOnPostRequestInterceptor', doNotPostDollarVariablesRequestInterceptor);
+})();
