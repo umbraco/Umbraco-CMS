4b69a7859d426583c9d6e50a2695061ee0178709
Kenn Jacobsen <post@kennjacobsen.dk>
2019-02-03 13:56:00 +0100

    Make sure the app menu state is set

--- src/Umbraco.Web.UI.Client/src/common/directives/components/editor/umbeditormenu.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/editor/umbeditormenu.directive.js
@@ -30,8 +30,8 @@
                 }
 
                 //when the options item is selected, we need to set the current menu item in appState (since this is synonymous with a menu)
-                // Niels: No i think we are wrong, we should not set the currentNode, cause it represents the currentNode of interaction.
-                //appState.setMenuState("currentNode", scope.currentNode);
+                //- otherwise we break any tree dialog that works with the current node (and that's pretty much all of them)
+                appState.setMenuState("currentNode", scope.currentNode);
                 
                 if (!scope.actions) {
                     treeService.getMenu({ treeNode: scope.currentNode })
