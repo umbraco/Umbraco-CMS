81c7267ff528a193fdc48d429a6e93103184373b
Bjarne Fyrstenborg <bf@oerskov.dk>
2019-02-06 19:58:55 +0100

    Update onLayoutSelect

--- src/Umbraco.Web.UI.Client/src/common/directives/components/umblayoutselector.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/umblayoutselector.directive.js
@@ -10,7 +10,7 @@
             bindings: {
                 layouts: '<',
                 activeLayout: '<',
-                onLayoutSelect: "&"
+                onLayoutSelect: '&'
             }
         });
 
