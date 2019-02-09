0c860e999bc3c5ffa44b5db2daf47c0edbfbb678
Kenn Jacobsen <post@kennjacobsen.dk>
2019-02-09 18:26:03 +0100

    Reimplement after merge

--- src/Umbraco.Web/WebApi/Filters/HttpQueryStringModelBinder.cs
+++ src/Umbraco.Web/WebApi/Filters/HttpQueryStringModelBinder.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Net.Http.Formatting;
 using System.Web.Http.Controllers;
 using System.Web.Http.ModelBinding;
@@ -21,7 +22,13 @@ namespace Umbraco.Web.WebApi.Filters
             {
                 if (actionContext.Request.Properties["MS_QueryNameValuePairs"] is IEnumerable<KeyValuePair<string, string>> queryStrings)
                 {
-                    var formData = new FormDataCollection(queryStrings);
+                    var queryStringKeys = queryStrings.Select(kvp => kvp.Key).ToArray();
+                    var additionalParameters = new Dictionary<string, string>();
+                    if(queryStringKeys.Contains("culture") == false) {
+                        additionalParameters["culture"] = actionContext.Request.ClientCulture();
+                    }
+
+                    var formData = new FormDataCollection(queryStrings.Union(additionalParameters));
                     bindingContext.Model = formData;
                     return true;
                 }
