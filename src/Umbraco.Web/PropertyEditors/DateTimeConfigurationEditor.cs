ab4d2748bc61cd31f186a1237b8fcfce6591a5ec
Sebastiaan Janssen <sebastiaan@umbraco.com>
2019-02-18 09:52:42 +0100

    Support really crazy date-time formats too + fix unit test

--- src/Umbraco.Web/PropertyEditors/DateTimeConfigurationEditor.cs
+++ src/Umbraco.Web/PropertyEditors/DateTimeConfigurationEditor.cs
@@ -1,5 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using Umbraco.Core;
 using Umbraco.Core.PropertyEditors;
 
 namespace Umbraco.Web.PropertyEditors
@@ -9,36 +9,15 @@ namespace Umbraco.Web.PropertyEditors
     /// </summary>
     public class DateTimeConfigurationEditor : ConfigurationEditor<DateTimeConfiguration>
     {
-        private static readonly HashSet<string> TimeFormats = new HashSet<string>( new [] {"HH", "H", "mm", "m"});
-
         public override IDictionary<string, object> ToValueEditor(object configuration)
         {
             var d = base.ToValueEditor(configuration);
 
             var format = d["format"].ToString();
 
-            d["pickTime"] = ContainsTimeElement(format);
+            d["pickTime"] = format.ContainsAny(new string[] { "H", "m", "s" });
+
             return d;
-        }
-
-        private bool ContainsTimeElement(string format)
-        {
-            if (string.IsNullOrWhiteSpace(format))
-            {
-                return false;
-            }
-
-            var elements = format.Split(new[] {' ', '-', '.', ':', '/', '\\'}, StringSplitOptions.RemoveEmptyEntries);
-
-            foreach (var element in elements)
-            {
-                if (TimeFormats.Contains(element))
-                {
-                    return true;
-                }
-            }
-
-            return false;
         }
     }
 }
