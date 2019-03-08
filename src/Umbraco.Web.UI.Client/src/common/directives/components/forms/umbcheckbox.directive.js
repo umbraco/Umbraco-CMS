0d58b4c42be414b192274528a8bba19095c89e08
Niels Lyngsø <nsl@umbraco.dk>
2019-03-08 14:37:23 +0100

    renamed umb-checkbox id attribute to input-id to avoid colliding Ids

--- src/Umbraco.Web.UI.Client/src/common/directives/components/forms/umbcheckbox.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/forms/umbcheckbox.directive.js
@@ -22,14 +22,14 @@
 </pre>
 
 @param {boolean} model Set to <code>true</code> or <code>false</code> to set the checkbox to checked or unchecked.
-@param {string} id Set the id of the checkbox.
+@param {string} input-id Set the <code>id</code> of the checkbox.
 @param {string} value Set the value of the checkbox.
 @param {string} name Set the name of the checkbox.
 @param {string} text Set the text for the checkbox label.
-@param {string} serverValidationField Set the val-server-field of the checkbox.
+@param {string} server-validation-field Set the <code>val-server-field</code> of the checkbox.
 @param {boolean} disabled Set the checkbox to be disabled.
 @param {boolean} required Set the checkbox to be required.
-@param {string} onChange Callback when the value of the input element changes.
+@param {string} on-change Callback when the value of the checkbox changed by interaction.
 
 **/
 
@@ -56,7 +56,7 @@
         controllerAs: 'vm',
         bindings: {
             model: "=",
-            id: "@",
+            inputId: "@",
             value: "@",
             name: "@",
             text: "@",
