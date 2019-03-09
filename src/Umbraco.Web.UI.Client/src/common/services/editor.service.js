966bfafe22d1ba85abb28dbdfad59be663dcfaf5
Andy Butland <abutland73@gmail.com>
2019-03-09 14:06:58 +0100

    Fixed casing of editorService open method inline documentation for the size parameter

--- src/Umbraco.Web.UI.Client/src/common/services/editor.service.js
+++ src/Umbraco.Web.UI.Client/src/common/services/editor.service.js
@@ -202,7 +202,7 @@ When building a custom infinite editor view you can use the same components as a
          *
          * @param {Object} editor rendering options
          * @param {String} editor.view Path to view
-         * @param {String} editor.size Sets the size of the editor ("Small"). If nothing is set it will use full width.
+         * @param {String} editor.size Sets the size of the editor ("small"). If nothing is set it will use full width.
          */
         function open(editor) {
 
