20c3002cbbc85bc69387524713f13f274c274def
Bjarne Fyrstenborg <bf@oerskov.dk>
2019-03-13 19:07:43 +0100

    Use vm in grid config for settings and styles

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/grid/dialogs/editconfig.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/grid/dialogs/editconfig.controller.js
@@ -1,16 +1,22 @@
 function EditConfigController($scope) {
 
-    $scope.close = function() {
-        if($scope.model.close) {
-           $scope.model.close();
+    var vm = this;
+
+    vm.submit = submit;
+    vm.close = close;
+
+    function submit() {
+        if ($scope.model && $scope.model.submit) {
+            $scope.model.submit($scope.model);
         }
     }
-    
-    $scope.submit = function() {
-        if($scope.model && $scope.model.submit) {
-           $scope.model.submit($scope.model);
+
+    function close() {
+        if ($scope.model.close) {
+            $scope.model.close();
         }
     }
+
 }
 
 angular.module("umbraco").controller("Umbraco.PropertyEditors.GridPrevalueEditor.EditConfigController", EditConfigController);
