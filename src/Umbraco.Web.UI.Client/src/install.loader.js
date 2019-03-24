4e43fa414ea6f78a64b6b3388f0135cca87a48c4
BatJan <jaskov@gmail.com>
2019-03-24 20:51:21 +0100

    Oops - The polyfill is of course not needed during installation :-)

--- src/Umbraco.Web.UI.Client/src/install.loader.js
+++ src/Umbraco.Web.UI.Client/src/install.loader.js
@@ -5,7 +5,6 @@ LazyLoad.js([
     'lib/angular-touch/angular-touch.js',
     'lib/angular-sanitize/angular-sanitize.js',
     'lib/angular-messages/angular-messages.js',
-    'lib/wicg-inert/dist/inert.min.js',
     'lib/underscore/underscore-min.js',
     'lib/angular-ui-sortable/sortable.js', 
     'js/installer.app.js',
