91cdfb8cd3e28f0a9a4ca9a9a538ad4e5dd41061
Pavel Budik <budik@ics.muni.cz>
2019-04-01 17:22:48 +0200

    Fix typo

--- src/Umbraco.Web.UI.Client/src/common/directives/components/grid/grid.rte.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/grid/grid.rte.directive.js
@@ -387,7 +387,7 @@ angular.module("umbraco.directives")
                                 //TODO: Here we should parse out the macro rendered content so we can save on a lot of bytes in data xfer
                                 // we do parse it out on the server side but would be nice to do that on the client side before as well.
                                 if (tinyMceEditor !== undefined && tinyMceEditor != null && !scope.isLoading) {
-                                    $scope.model.value = tinyMceEditor.getContent();
+                                    scope.model.value = tinyMceEditor.getContent();
                                 }
                             });
 
