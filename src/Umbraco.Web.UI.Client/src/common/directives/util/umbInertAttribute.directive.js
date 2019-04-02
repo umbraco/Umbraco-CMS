08d3e1f776c58a725ef3d649c82ff6b754b8f512
BatJan <jaskov@gmail.com>
2019-04-02 20:50:49 +0200

    Remove console logs and refactor the infinite overlay into a classname variable

--- src/Umbraco.Web.UI.Client/src/common/directives/util/umbInertAttribute.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/util/umbInertAttribute.directive.js
@@ -8,11 +8,13 @@
             link: function (scope, element, attrs) {
                 // If the value passed to the "umbInertAttribute" is "overlay" we will add/remove the inert attribute depending on what is emitted
                 if (scope.umbInertAttribute === 'infinite-overlay') {
+                    var infiniteEditorClassName = 'umb-editor--infinityMode';
+
                     eventsService.on('appState.editors.open', function (name, args) {
                         console.log('an infinite editor just opened');
 
                         // The umb-editor needs a special touch :)
-                        if (element.hasClass('umb-editor--infinityMode')) {
+                        if (element.hasClass(infiniteEditorClassName)) {
                             console.log(element,'what element are we dealing with?');
                             console.log(args,'what are the passed args?');
                         }
@@ -27,21 +29,18 @@
                     });
     
                     eventsService.on('appState.editors.close', function (name, args) {
-                        console.log('an infinite editor just closed');
-                        console.log(element,'what element are we dealing with?');
-                        console.log(args,'what are the passed args?');
-
-                        if (element.hasClass('umb-editor--infinityMode')) {
+                        if (element.hasClass(infiniteEditorClassName)) {
                             console.log('do something special');
+
+                            // TODO: Figure out which one should NOT be inerted
+                            // TODO: Make sure that the active overlay gets focused!
                         }
-                        // Figure out if any editors are still en view before removing the inert attribute
+                        // If there are no more open editor remove the inert attribute
                         else {
                             if (args.editors.length === 0) {
                                 element.removeAttr('inert');
                             }
                         }
-
-                        // element.removeAttr('inert',''); // TODO: do something special if it's the "editors" element we're on AND make sure to check the args and see if the inert attribute needs to be removed
                     });
                 }
 
