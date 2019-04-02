d0cbee463432ec640ee3819861db2c3c6b756e85
BatJan <jaskov@gmail.com>
2019-04-02 22:20:56 +0200

    Finishing up the directive

--- src/Umbraco.Web.UI.Client/src/common/directives/util/umbInertAttribute.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/util/umbInertAttribute.directive.js
@@ -8,15 +8,19 @@
             link: function (scope, element, attrs) {
                 // If the value passed to the "umbInertAttribute" is "overlay" we will add/remove the inert attribute depending on what is emitted
                 if (scope.umbInertAttribute === 'infinite-overlay') {
-                    var infiniteEditorClassName = 'umb-editor--infinityMode';
+                    var infiniteEditorClassName = 'umb-editors';
 
                     eventsService.on('appState.editors.open', function (name, args) {
-                        console.log('an infinite editor just opened');
-
                         // The umb-editor needs a special touch :)
                         if (element.hasClass(infiniteEditorClassName)) {
-                            console.log(element,'what element are we dealing with?');
-                            console.log(args,'what are the passed args?');
+                            setTimeout(function(){
+                                var children = element.children();
+                                var lastChildIndex = children.length - 1;
+                                var lastChild = children[lastChildIndex];
+
+                                children.attr('inert','');
+                                lastChild.removeAttribute('inert');
+                            }, 100);
                         }
                         // Set the inert attribute if it's missing on the element
                         else {
@@ -24,16 +28,24 @@
                                 element.attr('inert','');
                             }
                         }
-
-                        // TODO: Perhaps a mutation observer is needed for the "umb-editor" stuff...
                     });
     
                     eventsService.on('appState.editors.close', function (name, args) {
                         if (element.hasClass(infiniteEditorClassName)) {
-                            console.log('do something special');
 
-                            // TODO: Figure out which one should NOT be inerted
-                            // TODO: Make sure that the active overlay gets focused!
+                            setTimeout(function(){
+                                var children = element.children();
+                                var lastChildIndex = args.editors.length - 1;
+                                var lastChild = children[lastChildIndex];
+
+                                // Remove the inert atribute for the last child
+                                if (lastChild) {
+                                    lastChild.removeAttribute('inert');
+                                    
+                                    // Find the first focusable element and put focus on it
+                                    setFocus(lastChild);
+                                }
+                            }, 500);
                         }
                         // If there are no more open editor remove the inert attribute
                         else {
@@ -59,6 +71,20 @@
                 }
             }
         };
+
+        function setFocus(element) {
+            var $element = $(element);
+            var focusableElementsString = 'a[href], area[href], input:not([disabled]):not(.ng-hide), select:not([disabled]), textarea:not([disabled]), button:not([disabled]):not([tabindex="-1"]), iframe, object, embed, [tabindex="0"], [contenteditable]';
+            var focusableElements = $element.find(focusableElementsString);
+        
+            if(focusableElements.length){
+                var $firstFocusableElement = $(focusableElements[0]);
+
+                setTimeout(function() {
+                    $firstFocusableElement.removeAttr('auto-umb-focus').removeAttr('focus-on-filled').focus();
+                }, 100);
+            }
+        }
 
         return directive;
 
