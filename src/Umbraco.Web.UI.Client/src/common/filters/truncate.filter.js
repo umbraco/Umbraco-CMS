906c7a4ccd4a44c4b18d510a01d8fa9cb34c159b
Niels Lyngsø <nsl@umbraco.dk>
2019-04-30 15:52:00 +0200

    added angularJS truncate filter

---
+++ src/Umbraco.Web.UI.Client/src/common/filters/truncate.filter.js
@@ -0,0 +1,19 @@
+angular.module("umbraco.filters").filter('truncate', 
+    function () {
+        return function (value, wordwise, max, tail) {
+            if (!value) return '';
+            max = parseInt(max, 10);
+            if (!max) return value;
+            if (value.length <= max) return value;
+
+            value = value.substr(0, max);
+            if (wordwise) {
+                var lastspace = value.lastIndexOf(' ');
+                if (lastspace != -1) {
+                    value = value.substr(0, lastspace);
+                }
+            }
+            return value + (tail || ' …');
+        };
+    }
+);
