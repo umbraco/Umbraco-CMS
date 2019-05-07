e906808a9be56f8034d0d5332135de78e50305a8
Niels Lyngsø <nsl@umbraco.dk>
2019-05-07 10:59:09 +0200

    unbind keyboard shortcuts for editors when backdrop is active

--- src/Umbraco.Web.UI.Client/src/common/directives/components/forms/hotkey.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/forms/hotkey.directive.js
@@ -49,8 +49,6 @@ angular.module("umbraco.directives")
                                 } else {
                                     element.trigger("click");
                                 }
-                                
-                                keyboardService.stopPropagation()
 
                             }
 
