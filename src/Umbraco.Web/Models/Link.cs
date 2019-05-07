6bbb0120c7e668bedb2f58dab63e15eafec68fdb
Ronald Barendse <ronald@panoramastudios.nl>
2019-05-08 00:02:09 +0200

    Added documentation to Link and LinkType models

--- src/Umbraco.Web/Models/Link.cs
+++ src/Umbraco.Web/Models/Link.cs
@@ -2,12 +2,49 @@
 
 namespace Umbraco.Web.Models
 {
+    /// <summary>
+    /// Represents a link (e.g. to content, media or an external URL).
+    /// </summary>
     public class Link
     {
+        /// <summary>
+        /// Gets or sets the name.
+        /// </summary>
+        /// <value>
+        /// The name.
+        /// </value>
         public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the target.
+        /// </summary>
+        /// <value>
+        /// The target.
+        /// </value>
         public string Target { get; set; }
+
+        /// <summary>
+        /// Gets or sets the type of link.
+        /// </summary>
+        /// <value>
+        /// The type of link.
+        /// </value>
         public LinkType Type { get; set; }
+
+        /// <summary>
+        /// Gets or sets the UDI.
+        /// </summary>
+        /// <value>
+        /// The UDI.
+        /// </value>
         public Udi Udi { get; set; }
+
+        /// <summary>
+        /// Gets or sets the URL.
+        /// </summary>
+        /// <value>
+        /// The URL.
+        /// </value>
         public string Url { get; set; }
     }
 }
