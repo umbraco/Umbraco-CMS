f952877d04b4f88486b83e8a9709c96426e8791e
Shannon <sdeminick@gmail.com>
2019-05-10 01:48:11 +1000

    ensures that the reset flag is passed to the server when an admin is changing a members password

--- src/Umbraco.Web.UI.Client/src/views/member/member.edit.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/member/member.edit.controller.js
@@ -131,6 +131,10 @@ function MemberEditController($scope, $routeParams, $location, $q, $window, appS
             $scope.busy = true;
             $scope.page.saveButtonState = "busy";
 
+            //anytime a user is changing a member's password, we are in effect resetting it so we need to set that flag here
+            var passwordProp = _.find(contentEditingHelper.getAllProps($scope.content), function (e) { return e.alias === '_umb_password' });
+            passwordProp.value.reset = true;
+
             memberResource.save($scope.content, $routeParams.create, fileManager.getFiles())
                 .then(function(data) {
 
