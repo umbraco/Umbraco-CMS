174af027e92e1d985e98f7e4644b4b3fd1f8e448
Kenn Jacobsen <post@kennjacobsen.dk>
2019-05-19 11:59:24 +0200

    Block editor config first stab

--- src/Umbraco.Web/PropertyEditors/BlockEditorConfiguration.cs
+++ src/Umbraco.Web/PropertyEditors/BlockEditorConfiguration.cs
@@ -1,7 +1,27 @@
-﻿namespace Umbraco.Web.PropertyEditors
+﻿using Umbraco.Core;
+using Umbraco.Core.PropertyEditors;
+
+namespace Umbraco.Web.PropertyEditors
 {
     public class BlockEditorConfiguration
     {
-        // TODO: implement
+        [ConfigurationField("blocks", "Blocks", "views/propertyeditors/textarea/textarea.html")]
+        public Block[] Blocks { get; set; }
+
+        public class Block
+        {
+            public Udi ElementType { get; set; }
+
+            public BlockSetting Settings { get; set; }
+        }
+
+        public class BlockSetting
+        {
+            public string Label { get; set; }
+
+            public string Alias { get; set; }
+
+            public Udi DataType { get; set; }
+        }
     }
 }
