2455e2bed99aa5e540560c9d59d2e979ad6446a5
Callum Whyte <hey@callumwhyte.com>
2019-05-19 10:17:32 +0100

    Adding initial block editor property editor

---
+++ src/Umbraco.Web/PropertyEditors/BlockEditorPropertyEditor.cs
@@ -0,0 +1,18 @@
+﻿using Umbraco.Core;
+using Umbraco.Core.Logging;
+using Umbraco.Core.PropertyEditors;
+
+namespace Umbraco.Web.PropertyEditors
+{
+    [DataEditor(Constants.PropertyEditors.Aliases.BlockEditor, "Block Editor", ValueType = ValueTypes.Json, Group="rich content")]
+    public class BlockEditorPropertyEditor : DataEditor
+    {
+        public BlockEditorPropertyEditor(ILogger logger)
+            : base(logger)
+        { }
+
+        protected override IConfigurationEditor CreateConfigurationEditor() => base.CreateConfigurationEditor();
+
+        protected override IDataValueEditor CreateValueEditor() => base.CreateValueEditor();
+    }
+}
