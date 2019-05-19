5cb847930dbf88de44066718dad3dd6ef8536549
Callum Whyte <hey@callumwhyte.com>
2019-05-19 14:05:14 +0100

    Ensuring block editor settings JSON properties are lowercased

--- src/Umbraco.Web/PropertyEditors/BlockEditorConfiguration.cs
+++ src/Umbraco.Web/PropertyEditors/BlockEditorConfiguration.cs
@@ -1,4 +1,5 @@
-﻿using Umbraco.Core;
+﻿using Newtonsoft.Json;
+using Umbraco.Core;
 using Umbraco.Core.PropertyEditors;
 
 namespace Umbraco.Web.PropertyEditors
@@ -10,17 +11,22 @@ namespace Umbraco.Web.PropertyEditors
 
         public class Block
         {
+            [JsonProperty("elementType")]
             public Udi ElementType { get; set; }
 
+            [JsonProperty("settings")]
             public BlockSetting Settings { get; set; }
         }
 
         public class BlockSetting
         {
+            [JsonProperty("label")]
             public string Label { get; set; }
 
+            [JsonProperty("alias")]
             public string Alias { get; set; }
 
+            [JsonProperty("dataType")]
             public Udi DataType { get; set; }
         }
     }
