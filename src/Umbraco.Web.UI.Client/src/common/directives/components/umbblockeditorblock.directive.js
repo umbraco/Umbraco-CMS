62e19279a469d3a44aa31a051f5985128c1ae276
Kenn Jacobsen <post@kennjacobsen.dk>
2019-05-20 11:06:38 +0200

    Split things into directives

---
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/umbblockeditorblock.directive.js
@@ -0,0 +1,13 @@
+﻿angular.module("umbraco.directives").directive('umbBlockEditorBlock', [
+    function () {
+        var link = function (scope, el, attr, ctrl) {
+        };
+
+        return {
+            restrict: "E",
+            replace: true,
+            templateUrl: "views/propertyeditors/blockeditor/blockeditor.block.html",
+            link: link
+        };
+    }
+]);
---
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/umbblockeditorblocks.directive.js
@@ -0,0 +1,13 @@
+﻿angular.module("umbraco.directives").directive('umbBlockEditorBlocks', [
+    function () {
+        var link = function (scope, el, attr, ctrl) {
+        };
+
+        return {
+            restrict: "E",
+            replace: true,
+            templateUrl: "views/propertyeditors/blockeditor/blockeditor.blocks.html",
+            link: link
+        };
+    }
+]);
