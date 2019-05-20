1eb1d4dc63092769f775a20f8685e25239d793f8
Nathan Woulfe <nathan@nathanw.com.au>
2019-05-21 00:31:55 +1000

    path to thumbnail folder

--- src/Umbraco.Core/IO/SystemDirectories.cs
+++ src/Umbraco.Core/IO/SystemDirectories.cs
@@ -39,6 +39,8 @@ namespace Umbraco.Core.IO
 
         public static string Preview => Data + "/preview";
 
+        public static string Thumbnails => Config + "/thumbnails";
+
         private static string _root;
 
         /// <summary>
