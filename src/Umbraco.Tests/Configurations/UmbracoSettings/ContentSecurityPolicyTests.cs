34adda8254505a487d11a1abe362ef60d4af37ee
Maciej Sadowski <masa@scalepoint.com>
2019-06-19 14:54:27 +0200

    unit tests

---
+++ src/Umbraco.Tests/Configurations/UmbracoSettings/ContentSecurityPolicyTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+
+namespace Umbraco.Tests.Configurations.UmbracoSettings
+{
+    [TestFixture]
+    public class ContentSecurityPolicyTests : UmbracoSettingsTests
+    {
+        [Test]
+        public void DefaultSrcSelfIsTrue()
+        {
+            Assert.IsTrue(SettingsSection.Security.ContentSecurityPolicy.DefaultSrc.Self == true);
+        }
+
+        [Test]
+        public void NonceIsEnabled()
+        {
+            Assert.IsTrue(SettingsSection.Security.HideDisabledUsersInBackoffice == false);
+        }
+
+        [Test]
+        public void ScriptUnsafeInlineIsDisabled()
+        {
+            Assert.IsTrue(SettingsSection.Security.ContentSecurityPolicy.ScriptSrc?.UnsafeInline == false);
+        }
+        [Test]
+        public void ScriptUnsafeEvalIsDisabled()
+        {
+            Assert.IsTrue(SettingsSection.Security.ContentSecurityPolicy.ScriptSrc?.UnsafeEval == false);
+        }
+    }
+}
---
+++ src/Umbraco.Tests/Security/ContentSecurityPolicyTests.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using Umbraco.Core.Security;
+
+namespace Umbraco.Tests.Security
+{
+    [TestFixture]
+    public class NonceProviderTests
+    {
+        INonceProvider _service;
+        [SetUp]
+        public void Setup()
+        {
+            _service = new NonceProvider();
+        }
+
+        [Test]
+        public void ScriptNonceRequestedMultipleTimesIsTheSame()
+        {
+            var nonce1 = _service.ScriptNonce;
+            var nonce2 = _service.ScriptNonce;
+
+            Assert.That(nonce1, Is.EqualTo(nonce2));
+        }
+
+        [Test]
+        public void StyleNonceRequestedMultipleTimesIsTheSame()
+        {
+            var nonce1 = _service.StyleNonce;
+            var nonce2 = _service.StyleNonce;
+
+            Assert.That(nonce1, Is.EqualTo(nonce2));
+        }
+
+        [Test]
+        public void StyleandScriptNonceAreDifferent()
+        {
+            var scriptNonce = _service.ScriptNonce;
+            var styleNonce = _service.StyleNonce;
+
+            Assert.That(styleNonce, Is.Not.EqualTo(scriptNonce));
+        }
+    }
+}
