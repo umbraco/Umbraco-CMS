fcfb10765321b2297f3f61fe4dad511c602765b9
Shannon <sdeminick@gmail.com>
2019-06-20 15:40:57 +1000

    Fixes up UmbracoContextFactory to ensure it's not returning a disposed Umbraco context

--- src/Umbraco.Web/UmbracoContextFactory.cs
+++ src/Umbraco.Web/UmbracoContextFactory.cs
@@ -64,7 +64,7 @@ namespace Umbraco.Web
         public UmbracoContextReference EnsureUmbracoContext(HttpContextBase httpContext = null)
         {
             var currentUmbracoContext = _umbracoContextAccessor.UmbracoContext;
-            if (currentUmbracoContext != null)
+            if (currentUmbracoContext != null && !currentUmbracoContext.Disposed)
                 return new UmbracoContextReference(currentUmbracoContext, false, _umbracoContextAccessor);
 
 
