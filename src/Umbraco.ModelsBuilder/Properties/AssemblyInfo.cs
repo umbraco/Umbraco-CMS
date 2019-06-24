355a5e0eb490173a10c72681bbbbe2cf565bdaa2
Stephan <stephane@umbraco.dk>
2019-06-24 10:32:52 +0200

    Remove ModelsBuilder NuGet dependency, add project

--- src/Umbraco.ModelsBuilder/Properties/AssemblyInfo.cs
+++ src/Umbraco.ModelsBuilder/Properties/AssemblyInfo.cs
@@ -1,14 +1,36 @@
 ﻿using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
 [assembly: AssemblyTitle("Umbraco.ModelsBuilder")]
-[assembly: AssemblyDescription("Umbraco ModelsBuilder")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyProduct("Umbraco CMS")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Umbraco.ModelsBuilder")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
-[assembly: Guid("7020a059-c0d1-43a0-8efd-23591a0c9af6")]
 
-// code analysis
-// IDE1006 is broken, wants _value syntax for consts, etc - and it's even confusing ppl at MS, kill it
-[assembly: System.Diagnostics.CodeAnalysis.SuppressMessage("Style", "IDE1006:Naming Styles", Justification = "~_~")]
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("52ac0ba8-a60e-4e36-897b-e8b97a54ed1c")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
