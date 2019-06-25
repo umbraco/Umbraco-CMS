568429dc8467feccb6afb488b9cf2af5389a812a
Rasmus Olsen <rol@vertica.dk>
2019-06-25 12:04:00 +0200

    Refactored redundant code

--- src/Umbraco.Web.UI.Client/src/common/directives/components/buttons/umbbutton.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/buttons/umbbutton.directive.js
@@ -112,11 +112,9 @@ Use this directive to render an umbraco button. The directive can be used to gen
 
         function onInit() {
 
-            vm.generalActions = false;
             vm.blockElement = false;
             vm.style = null;
             vm.innerState = "init";
-
             vm.generalActions = vm.labelKey === "general_actions";
 
             vm.buttonLabel = vm.label;
