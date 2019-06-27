85fe7e4f6780fc964800e029d40c1bcf992d7d49
Nathan Woulfe <nathan@nathanw.com.au>
2019-06-27 18:58:39 +1000

    fix behaviour on page load - don't add active class if sticky-bar is in viewport

--- src/Umbraco.Web.UI.Client/src/common/directives/components/umbstickybar.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/umbstickybar.directive.js
@@ -24,14 +24,24 @@ Use this directive make an element sticky and follow the page when scrolling. `u
     'use strict';
 
     function StickyBarDirective() {
-        
+
+        /**
+        On initial load, the intersector fires if the grid editor is in the viewport
+        This flag is used to suppress the setClass behaviour on the initial load
+        **/
+        var initial = true;
+
         /**
         Toggle `umb-sticky-bar--active` class on the sticky-bar element
-        **/        
+        **/
         function setClass(addClass, current) {
-            current.classList.toggle('umb-sticky-bar--active', addClass);
+            if (!initial) {
+                current.classList.toggle('umb-sticky-bar--active', addClass);
+            } else {
+                initial = false;
+            }
         }
-        
+
         /**
         Inserts two elements in the umbStickyBar parent element
         These are used by the IntersectionObserve to calculate scroll position
@@ -40,9 +50,9 @@ Use this directive make an element sticky and follow the page when scrolling. `u
             ['-top', '-bottom'].forEach(s => {
                 const sentinel = document.createElement('div');
                 sentinel.classList.add('umb-sticky-sentinel', s);
-                current.parentElement.appendChild(sentinel);                
+                current.parentElement.appendChild(sentinel);
             });
-        }        
+        }
 
         /**
         Calls into setClass when the footer sentinel enters/exits the bottom of the container
@@ -50,15 +60,14 @@ Use this directive make an element sticky and follow the page when scrolling. `u
         **/
         function observeFooter(current, container) {
             const observer = new IntersectionObserver((records, observer) => {
-                let [target, rootBounds, intersected] 
-                    = [records[0].boundingClientRect, records[0].rootBounds, records[0].intersectionRatio === 1];                
-                
+                let [target, rootBounds, intersected] = [records[0].boundingClientRect, records[0].rootBounds, records[0].intersectionRatio === 1];
+
                 if (target.bottom > rootBounds.top && intersected) {
                     setClass(true, current);
                 }
                 if (target.top < rootBounds.top && target.bottom < rootBounds.bottom) {
                     setClass(false, current);
-                }                
+                }
             }, {
                 threshold: [1],
                 root: container
@@ -66,14 +75,14 @@ Use this directive make an element sticky and follow the page when scrolling. `u
 
             observer.observe(current.parentElement.querySelector('.umb-sticky-sentinel.-bottom'));
         }
-        
+
         /**
         Calls into setClass when the header sentinel enters/exits the top of the container
         Container is the parent element of the umbStickyBar element
-        **/        
+        **/
         function observeHeader(current, container) {
             const observer = new IntersectionObserver((records, observer) => {
-                let [target, rootBounds] = [records[0].boundingClientRect, records[0].rootBounds];                
+                let [target, rootBounds] = [records[0].boundingClientRect, records[0].rootBounds];
 
                 if (target.bottom < rootBounds.top) {
                     setClass(true, current);
@@ -86,19 +95,19 @@ Use this directive make an element sticky and follow the page when scrolling. `u
                 threshold: [0],
                 root: container
             });
-            
+
             observer.observe(current.parentElement.querySelector('.umb-sticky-sentinel.-top'));
         }
 
         function link(scope, el, attr, ctrl) {
-            
-            let current = el[0];           
-            let container = _el.closest('[data-element="editor-container"]');
-            
+
+            let current = el[0];
+            let container = current.closest('[data-element="editor-container"]');
+
             addSentinels(current);
 
             observeHeader(current, container);
-            observeFooter(current, container);            
+            observeFooter(current, container);
         }
 
         var directive = {
