37bf6fe9385f026f6921ea03330e2ded57914fa6
Shannon <sdeminick@gmail.com>
2019-07-03 19:15:54 +1000

    Automatically refresh the page if we encounter a ModelBindingException

---
+++ src/Umbraco.Web/Mvc/ModelBindingExceptionFilter.cs
@@ -0,0 +1,23 @@
+﻿using System.Web.Mvc;
+
+namespace Umbraco.Web.Mvc
+{
+    /// <summary>
+    /// An exception filter checking if we get a <see cref="ModelBindingException"/> in which case it returns the html to auto refresh the page
+    /// </summary>
+    internal class ModelBindingExceptionFilter : FilterAttribute, IExceptionFilter
+    {
+        public void OnException(ExceptionContext filterContext)
+        {
+            if (!filterContext.ExceptionHandled && filterContext.Exception is ModelBindingException)
+            {
+                filterContext.Result = new ContentResult
+                {
+                    Content = "<html><head><meta http-equiv=\"refresh\" content=\"3\" /></head><body><p>Loading page...</p></body></html>",
+                    ContentType = "text/html"
+                };
+                filterContext.ExceptionHandled = true;
+            }
+        }
+    }
+}
