1f7d5c88684de9af230b2c2e85a1fa27bf2dd8d3
David Sheiles <david@digitalmomentum.com.au>
2019-07-11 14:26:49 +1000

    Issue #5840 - Need to check for null Value before .ToSting() method in case the relatedLinks picker is null

--- src/Umbraco.Core/Migrations/Upgrade/V_8_0_0/ConvertRelatedLinksToMultiUrlPicker.cs
+++ src/Umbraco.Core/Migrations/Upgrade/V_8_0_0/ConvertRelatedLinksToMultiUrlPicker.cs
@@ -53,7 +53,7 @@ namespace Umbraco.Core.Migrations.Upgrade.V_8_0_0
             
             foreach (var property in properties)
             {
-                var value = property.Value.ToString();
+                var value = property.Value?.ToString();
                 if (string.IsNullOrWhiteSpace(value))
                     continue;
 
