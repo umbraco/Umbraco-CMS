da71a94c39fbd5403c114344a5d5627adacc88b0
Kenn Jacobsen <post@kennjacobsen.dk>
2019-07-16 12:45:27 +0200

    Use IsCollectionEmpty() as per review

--- src/Umbraco.Web/PublishedCache/NuCache/PublishedSnapshotService.cs
+++ src/Umbraco.Web/PublishedCache/NuCache/PublishedSnapshotService.cs
@@ -1057,7 +1057,7 @@ namespace Umbraco.Web.PublishedCache.NuCache
 
                 _mediaStore.UpdateContentTypes(removedIds, typesA, kits);
                 _mediaStore.UpdateContentTypes(CreateContentTypes(PublishedItemType.Media, otherIds.ToArray()).ToArray());
-                if(newIds != null && newIds.Any())
+                if(newIds.IsCollectionEmpty() == false)
                 {
                     _mediaStore.NewContentTypes(CreateContentTypes(PublishedItemType.Media, newIds.ToArray()).ToArray());
                 }
