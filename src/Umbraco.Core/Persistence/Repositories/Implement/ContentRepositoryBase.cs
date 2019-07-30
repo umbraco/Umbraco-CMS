20d28ea2159f90b833ea14f8317f644763fbb3ea
Shannon <sdeminick@gmail.com>
2019-07-30 19:08:37 +1000

    removes commented out code

--- src/Umbraco.Core/Persistence/Repositories/Implement/ContentRepositoryBase.cs
+++ src/Umbraco.Core/Persistence/Repositories/Implement/ContentRepositoryBase.cs
@@ -512,34 +512,16 @@ namespace Umbraco.Core.Persistence.Repositories.Implement
             foreach (var a in allPropertyDataDtos)
                 a.PropertyTypeDto = indexedPropertyTypeDtos[a.PropertyTypeId];
 
-            //TODO: This logic was here but then we never used the data being passed to GetPropertyCollections, this will save a bit of CPU without it!
-            //// prefetch configuration for tag properties
-            //var tagEditors = new Dictionary<string, TagConfiguration>();
-            //foreach (var propertyTypeDto in indexedPropertyTypeDtos.Values)
-            //{
-            //    var editorAlias = propertyTypeDto.DataTypeDto.EditorAlias;
-            //    var editorAttribute = PropertyEditors[editorAlias].GetTagAttribute();
-            //    if (editorAttribute == null) continue;
-            //    var tagConfigurationSource = propertyTypeDto.DataTypeDto.Configuration;
-            //    var tagConfiguration = string.IsNullOrWhiteSpace(tagConfigurationSource)
-            //        ? new TagConfiguration()
-            //        : JsonConvert.DeserializeObject<TagConfiguration>(tagConfigurationSource);
-            //    if (tagConfiguration.Delimiter == default) tagConfiguration.Delimiter = editorAttribute.Delimiter;
-            //    tagEditors[editorAlias] = tagConfiguration;
-            //}
-
             // now we have
             // - the definitions
             // - all property data dtos
-            // - tag editors
+            // - tag editors (Actually ... no we don't since i removed that code, but we don't need them anyways it seems)
             // and we need to build the proper property collections
 
-            return GetPropertyCollections(temps, allPropertyDataDtos
-                /*, tagEditors*/);
+            return GetPropertyCollections(temps, allPropertyDataDtos);
         }
 
-        private IDictionary<int, PropertyCollection> GetPropertyCollections<T>(List<TempContent<T>> temps, IEnumerable<PropertyDataDto> allPropertyDataDtos
-            /*, Dictionary<string, TagConfiguration> tagConfigurations*/)
+        private IDictionary<int, PropertyCollection> GetPropertyCollections<T>(List<TempContent<T>> temps, IEnumerable<PropertyDataDto> allPropertyDataDtos)
             where T : class, IContentBase
         {
             var result = new Dictionary<int, PropertyCollection>();
