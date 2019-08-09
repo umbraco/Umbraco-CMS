47a47292e822dc7fdd14cf5bf2d0a90951de6d69
Shannon <sdeminick@gmail.com>
2019-08-09 12:08:41 +1000

    adds the constant back

--- src/Umbraco.Web/PropertyEditors/BlockEditorPropertyEditor.cs
+++ src/Umbraco.Web/PropertyEditors/BlockEditorPropertyEditor.cs
@@ -1,9 +1,10 @@
-﻿using Umbraco.Core.Logging;
+﻿using Umbraco.Core;
+using Umbraco.Core.Logging;
 using Umbraco.Core.PropertyEditors;
 
 namespace Umbraco.Web.PropertyEditors
 {
-    [DataEditor("Umbraco.BlockEditor", EditorType.PropertyValue , "Block Editor", "blockeditor", ValueType = ValueTypes.Json, Group="rich content", Icon="icon-application-window-alt")]
+    [DataEditor(Constants.PropertyEditors.Aliases.BlockEditor, EditorType.PropertyValue , "Block Editor", "blockeditor", ValueType = ValueTypes.Json, Group="rich content", Icon="icon-application-window-alt")]
     public class BlockEditorPropertyEditor : DataEditor
     {
         public BlockEditorPropertyEditor(ILogger logger)
