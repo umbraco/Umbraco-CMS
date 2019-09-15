8b91030607cd330457d7a64ff2e964ee41fa1456
Steve Megson <steve@megson.org>
2019-09-15 21:13:44 +0100

    Legacy pickers migration

--- src/Umbraco.Core/Migrations/Upgrade/UmbracoPlan.cs
+++ src/Umbraco.Core/Migrations/Upgrade/UmbracoPlan.cs
@@ -129,6 +129,7 @@ namespace Umbraco.Core.Migrations.Upgrade
             To<DataTypeMigration>("{8640C9E4-A1C0-4C59-99BB-609B4E604981}");
             To<TagsMigration>("{DD1B99AF-8106-4E00-BAC7-A43003EA07F8}");
             To<SuperZero>("{9DF05B77-11D1-475C-A00A-B656AF7E0908}");
+            To<LegacyPickersPropertyEditorsMigration>("{258CE811-4201-4004-95EF-402086B38B77}");
             To<PropertyEditorsMigration>("{6FE3EF34-44A0-4992-B379-B40BC4EF1C4D}");
             To<LanguageColumns>("{7F59355A-0EC9-4438-8157-EB517E6D2727}");
             ToWithReplace<AddVariationTables2, AddVariationTables1A>("{941B2ABA-2D06-4E04-81F5-74224F1DB037}", "{76DF5CD7-A884-41A5-8DC6-7860D95B1DF5}"); // kill AddVariationTable1
---
+++ src/Umbraco.Core/Migrations/Upgrade/V_8_0_0/LegacyPickersPropertyEditorsMigration.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Umbraco.Core.Persistence;
+using Umbraco.Core.Persistence.Dtos;
+using Umbraco.Core.PropertyEditors;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Migrations.PostMigrations;
+using Umbraco.Core.Models;
+using Newtonsoft.Json;
+
+namespace Umbraco.Core.Migrations.Upgrade.V_8_0_0
+{
+    public class LegacyPickersPropertyEditorsMigration : PropertyEditorsMigrationBase
+    {
+        private Lazy<Dictionary<int, NodeDto>> _nodeIdToKey;
+
+        public LegacyPickersPropertyEditorsMigration(IMigrationContext context)
+            : base(context)
+        { }
+
+        public override void Migrate()
+        {
+            _nodeIdToKey = new Lazy<Dictionary<int, NodeDto>>(
+                () => Context.Database.Fetch<NodeDto>(
+                    Context.SqlContext.Sql()
+                        .Select<NodeDto>(x => x.NodeId, x => x.NodeObjectType, x => x.UniqueId)
+                        .From<NodeDto>()
+                    ).ToDictionary(n => n.NodeId)
+                );
+
+            var refreshCache = Migrate(GetDataTypes("Umbraco.ContentPickerAlias"));
+            refreshCache |= Migrate(GetDataTypes(Constants.PropertyEditors.Aliases.MediaPicker));
+            refreshCache |= Migrate(GetDataTypes(Constants.PropertyEditors.Aliases.MemberPicker));
+            refreshCache |= Migrate(GetDataTypes(Constants.PropertyEditors.Aliases.MultiNodeTreePicker));
+            
+            // if some data types have been updated directly in the database (editing DataTypeDto and/or PropertyDataDto),
+            // bypassing the services, then we need to rebuild the cache entirely, including the umbracoContentNu table
+            if (refreshCache)
+                Context.AddPostMigration<RebuildPublishedSnapshot>();
+        }
+
+        private bool Migrate(IEnumerable<DataTypeDto> dataTypes)
+        {
+            var refreshCache = false;
+
+            foreach (var dataType in dataTypes)
+            {
+                Context.Logger.Info<LegacyPickersPropertyEditorsMigration>("Migrating " + dataType.EditorAlias + ", " + dataType.NodeId);
+
+                dataType.DbType = ValueStorageType.Ntext.ToString();
+                Database.Update(dataType);
+
+                // get property data dtos
+                var propertyDataDtos = Database.Fetch<PropertyDataDto>(Sql()
+                    .Select<PropertyDataDto>()
+                    .From<PropertyDataDto>()
+                    .InnerJoin<PropertyTypeDto>().On<PropertyTypeDto, PropertyDataDto>((pt, pd) => pt.Id == pd.PropertyTypeId)
+                    .InnerJoin<DataTypeDto>().On<DataTypeDto, PropertyTypeDto>((dt, pt) => dt.NodeId == pt.DataTypeId)
+                    .Where<PropertyTypeDto>(x => x.DataTypeId == dataType.NodeId));
+
+                // update dtos
+                var updatedDtos = propertyDataDtos.Where(x => UpdatePropertyDataDto(x, true));
+
+                // persist changes
+                foreach (var propertyDataDto in updatedDtos)
+                    Database.Update(propertyDataDto);
+
+                refreshCache = true;
+            }
+
+            return refreshCache;
+        }
+
+        private bool UpdatePropertyDataDto(PropertyDataDto propData, bool isMultiple)
+        {
+            //Get the INT ids stored for this property/drop down
+            int[] ids = null;
+            if (!propData.VarcharValue.IsNullOrWhiteSpace())
+            {
+                ids = ConvertStringValues(propData.VarcharValue);
+            }
+            else if (!propData.TextValue.IsNullOrWhiteSpace())
+            {
+                ids = ConvertStringValues(propData.TextValue);
+            }
+            else if (propData.IntegerValue.HasValue)
+            {
+                ids = new[] { propData.IntegerValue.Value };
+            }
+
+            if (ids == null || ids.Length <= 0) return false;
+
+            // map ids to values
+            var values = new List<Udi>();
+            var canConvert = true;
+
+            foreach (var id in ids)
+            {
+                if (_nodeIdToKey.Value.TryGetValue(id, out var node))
+                {
+                    values.Add(Udi.Create(ObjectTypes.GetUdiType(node.NodeObjectType.Value), node.UniqueId));
+                    continue;
+                }
+                canConvert = false;
+            }
+
+            if (!canConvert) return false;
+
+            propData.TextValue = String.Join(",", values);
+            propData.VarcharValue = null;
+            propData.IntegerValue = null;
+            return true;
+        }
+
+
+
+    }
+}
