98e39ed3ef3277216df9c423494ecbc1d259476c
stevemegson <steve@megson.org>
2019-09-20 10:38:44 +0100

    V7: Error upgrading due to SecurityStamp being null (#6343)
    * Allow login with a null SecurityStamp when upgrading
    
    * Added migration to fill in missing SecurityStamp
    
    * Make sure we initialize UmbracoContext as it is needed in some tests that use UmbracoBackOfficeIdentity.AddUserDataClaims
    
    * Moved check for null SecurityStamp
    
    * Generate new SecurityStamp in C#
    
    * Bump version to 7.15.4

---
+++ src/Umbraco.Core/Persistence/Migrations/Upgrades/TargetVersionSevenFifteenFour/PopulateMissingSecurityStamps.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Umbraco.Core.Logging;
+using Umbraco.Core.Persistence.SqlSyntax;
+
+namespace Umbraco.Core.Persistence.Migrations.Upgrades.TargetVersionSevenFifteenFour
+{
+    [Migration("7.15.4", 1, Constants.System.UmbracoMigrationName)]
+    public class PopulateMissingSecurityStamps : MigrationBase
+    {
+        public PopulateMissingSecurityStamps(ISqlSyntaxProvider sqlSyntax, ILogger logger) : base(sqlSyntax, logger)
+        {
+        }
+
+        public override void Up()
+        {
+            // A user with a NULL securityStampToken can't log in after v7.8.0
+            Execute.Sql($@"UPDATE umbracoUser SET securityStampToken = '{Guid.NewGuid().ToString()}' WHERE securityStampToken IS NULL");
+        }
+
+        public override void Down()
+        {
+        }
+    }
+}
