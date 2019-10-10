90d1eaf8f1d3a8af44d1aee1fabc59bd20cebffe
Ronald Barendse <ronald@panoramastudios.nl>
2019-10-10 23:37:43 +0200

    Add IUserComponent and UserComponentBase

---
+++ src/Umbraco.Core/Composing/UserComponentBase.cs
@@ -0,0 +1,22 @@
+﻿namespace Umbraco.Core.Composing
+{
+    /// <summary>
+    /// Base class for user components that only requires implmenting the <see cref="Initialize" /> method.
+    /// </summary>
+    /// <seealso cref="Umbraco.Core.Composing.IUserComponent" />
+    public abstract class UserComponentBase : IUserComponent
+    {
+        /// <summary>
+        /// Initializes this component.
+        /// </summary>
+        public abstract void Initialize();
+
+        /// <summary>
+        /// Terminates this component.
+        /// </summary>
+        public virtual void Terminate()
+        {
+            // Nothing to terminate
+        }
+    }
+}
