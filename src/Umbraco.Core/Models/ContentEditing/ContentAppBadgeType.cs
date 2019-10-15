5f12794561d67bc1c58038df5dbf746c7835d203
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2019-10-15 09:06:14 +0200

    Added models for content app badge

---
+++ src/Umbraco.Core/Models/ContentEditing/ContentAppBadgeType.cs
@@ -0,0 +1,20 @@
+﻿namespace Umbraco.Core.Models.ContentEditing
+{
+    using System.Runtime.Serialization;
+
+    /// <summary>
+    /// Represent the content app badge types
+    /// </summary>
+    [DataContract(Name = "contentAppBadgeType")]
+    public enum ContentAppBadgeType
+    {
+        [EnumMember(Value = "default")]
+        Default = 0,
+
+        [EnumMember(Value = "warning")]
+        Warning = 1,
+
+        [EnumMember(Value = "alert")]
+        Alert = 2
+    }
+}
