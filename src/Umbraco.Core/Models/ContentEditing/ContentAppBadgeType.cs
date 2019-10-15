c6f17d1ec256786ffa35db55dbbd7b4a3b4b5240
Dave Woestenborghs <dave.woestenborghs@gmail.com>
2019-10-15 16:59:45 +0200

    Fix the serialization of the type enum

--- src/Umbraco.Core/Models/ContentEditing/ContentAppBadgeType.cs
+++ src/Umbraco.Core/Models/ContentEditing/ContentAppBadgeType.cs
@@ -2,10 +2,14 @@
 {
     using System.Runtime.Serialization;
 
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Converters;
+
     /// <summary>
     /// Represent the content app badge types
     /// </summary>
     [DataContract(Name = "contentAppBadgeType")]
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum ContentAppBadgeType
     {
         [EnumMember(Value = "default")]
