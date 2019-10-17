7af15b3a28a01b3141280b8a9dde545bdf188edf
Jeavon <jeavon@crumpled-dog.com>
2019-10-17 17:26:32 +0100

    Add additional null conditionals as this method is sometimes called for front end rendering where this is no current user (DTGE)

--- src/Umbraco.Web/PropertyEditors/RichTextPropertyEditor.cs
+++ src/Umbraco.Web/PropertyEditors/RichTextPropertyEditor.cs
@@ -114,7 +114,7 @@ namespace Umbraco.Web.PropertyEditors
                 if (editorValue.Value == null)
                     return null;
 
-                var userId = _umbracoContextAccessor.UmbracoContext?.Security.CurrentUser.Id ?? Constants.Security.SuperUserId;
+                var userId = _umbracoContextAccessor.UmbracoContext?.Security?.CurrentUser?.Id ?? Constants.Security.SuperUserId;
 
                 var config = editorValue.DataTypeConfiguration as RichTextConfiguration;
                 var mediaParent = config?.MediaParentId;
