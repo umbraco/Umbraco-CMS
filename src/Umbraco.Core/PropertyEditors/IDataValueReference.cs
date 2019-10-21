13f7ee8a5fff4c3e584cb8d4405e8ac12060ed8d
Shannon <sdeminick@gmail.com>
2019-10-21 19:40:06 +1100

    Adds IDataValueReference

---
+++ src/Umbraco.Core/PropertyEditors/IDataValueReference.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+
+namespace Umbraco.Core.PropertyEditors
+{
+    /// <summary>
+    /// Used to resolve references from <see cref="IDataValueEditor"/> values
+    /// </summary>
+    public interface IDataValueReference
+    {
+        /// <summary>
+        /// Returns any references contained in the value
+        /// </summary>
+        /// <param name="value"></param>
+        /// <returns></returns>
+        IEnumerable<Udi> GetReferences(object value);
+    }
+}
