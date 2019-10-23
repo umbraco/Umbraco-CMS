dcc0d9c7206ded8fd73c40da5923dcb2d28c9435
Niels Lyngsø <nsl@umbraco.dk>
2019-10-23 15:50:15 +0200

    correct mistake in WatchTask of our Gulp Script

--- src/Umbraco.Web.UI.Client/gulp/tasks/watchTask.js
+++ src/Umbraco.Web.UI.Client/gulp/tasks/watchTask.js
@@ -20,21 +20,26 @@ function watchTask(cb) {
     //Setup a watcher for all groups of JS files
     _.forEach(config.sources.js, function (group) {
         if(group.watch !== false) {
-            watch(group.files, { ignoreInitial: true, interval: watchInterval }, function JS_Group_Compile() { return processJs(group.files, group.out) })
+            watch(group.files, { ignoreInitial: true, interval: watchInterval }, function JS_Group_Compile() { return processJs(group.files, group.out) });
         }
     });
 
     //Setup a watcher for all groups of LESS files
     _.forEach(config.sources.less, function (group) {
         if(group.watch !== false) {
-            watch(group.watch, { ignoreInitial: true, interval: watchInterval }, function Less_Group_Compile() { processLess(group.files, group.out) })
+            watch(group.watch, { ignoreInitial: true, interval: watchInterval }, function Less_Group_Compile() { processLess(group.files, group.out) });
         }
     });
     
     //Setup a watcher for all groups of view files
+    var viewWatcher;
     _.forEach(config.sources.views, function (group) {
         if(group.watch !== false) {
-            watch(group.files, { ignoreInitial: true, interval: watchInterval }, function View_Group_Compile() { processLess(group.files, group.out) })
+            viewWatcher = watch(group.files, { ignoreInitial: true, interval: watchInterval });
+            viewWatcher.on('change', function(path, stats) {
+                console.log("copying " + group.files + " to " + config.root + config.targets.views + group.folder);
+                src(group.files).pipe( dest(config.root + config.targets.views + group.folder) )
+            });
         }
     });
     
