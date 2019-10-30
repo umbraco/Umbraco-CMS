58271a32be85ecddef25c718953bd0493eed6441
Rasmus Østergård <r@sser.dk>
2019-10-30 17:49:02 +0100

    Auto select Content-Group when configuring Nested Content if only one is present

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/nestedcontent/nestedcontent.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/nestedcontent/nestedcontent.controller.js
@@ -138,6 +138,9 @@
                 event: $event,
                 submit: function (model) {
                     config.ncAlias = model.selectedItem.alias;
+                    if (model.selectedItem.tabs.length === 1) {
+                        config.ncTabAlias = model.selectedItem.tabs[0];
+                    }
                     overlayService.close();
                 },
                 close: function () {
