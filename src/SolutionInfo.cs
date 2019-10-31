ef38fc1a6d63fea945ca0b1c564e2373b751141c
Bjarke Berg <mail@bergmania.dk>
2019-10-31 10:02:08 +0100

    Bump version to 8.2.2

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -18,5 +18,5 @@ using System.Resources;
 [assembly: AssemblyVersion("8.0.0")]
 
 // these are FYI and changed automatically
-[assembly: AssemblyFileVersion("8.2.1")]
-[assembly: AssemblyInformationalVersion("8.2.1")]
+[assembly: AssemblyFileVersion("8.2.2")]
+[assembly: AssemblyInformationalVersion("8.2.2")]
