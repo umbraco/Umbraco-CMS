69c2b732ce7082d177ea25a4c7d601feff2ae96d
Shannon <sdeminick@gmail.com>
2019-11-07 21:28:56 +1100

    new IJsonSerializer, moves other base cache refresher classes

---
+++ src/Umbraco.Core/Serialization/JsonNetSerializer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+
+namespace Umbraco.Core.Serialization
+{
+    public class JsonNetSerializer : IJsonSerializer
+    {
+        public string Serialize(object input)
+        {
+            return JsonConvert.SerializeObject(input);
+        }
+
+        public T Deserialize<T>(string input)
+        {
+            return JsonConvert.DeserializeObject<T>(input);
+        }
+    }
+}
