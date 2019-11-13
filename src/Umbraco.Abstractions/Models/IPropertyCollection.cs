319376d157bcbc0945e1e8f447f7d070cdaa11bc
Shannon <sdeminick@gmail.com>
2019-11-13 12:04:50 +1100

    Cleans up IPropertyCollection

--- src/Umbraco.Abstractions/Models/IPropertyCollection.cs
+++ src/Umbraco.Abstractions/Models/IPropertyCollection.cs
@@ -3,19 +3,34 @@ using System.Collections.Specialized;
 
 namespace Umbraco.Core.Models
 {
-    public interface IPropertyCollection : IEnumerable<IProperty>
+    public interface IPropertyCollection : IEnumerable<IProperty>, IDeepCloneable, INotifyCollectionChanged
     {
         bool TryGetValue(string propertyTypeAlias, out IProperty property);
         bool Contains(string key);
 
-        event NotifyCollectionChangedEventHandler CollectionChanged;
-
+        /// <summary>
+        /// Ensures that the collection contains properties for the specified property types.
+        /// </summary>
         void EnsurePropertyTypes(IEnumerable<IPropertyType> propertyTypes);
-        void EnsureCleanPropertyTypes(IEnumerable<IPropertyType> propertyTypes);
-        object DeepClone();
 
+        /// <summary>
+        /// Ensures that the collection does not contain properties not in the specified property types.
+        /// </summary>
+        void EnsureCleanPropertyTypes(IEnumerable<IPropertyType> propertyTypes);
+
+        /// <summary>
+        /// Gets the property with the specified alias.
+        /// </summary>
         IProperty this[string name] { get; }
+
+        /// <summary>
+        /// Gets the property at the specified index.
+        /// </summary>
         IProperty this[int index] { get; }
+
+        /// <summary>
+        /// Adds or updates a property.
+        /// </summary>
         void Add(IProperty property);
 
         int Count { get; }
