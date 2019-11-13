995b31b86c2dd203ae8cd3d5ba5dd910a3820f55
Shannon <sdeminick@gmail.com>
2019-11-13 12:17:09 +1100

    Cleans up IProperty

--- src/Umbraco.Abstractions/Models/IProperty.cs
+++ src/Umbraco.Abstractions/Models/IProperty.cs
@@ -4,7 +4,7 @@ using Umbraco.Core.Models.Entities;
 
 namespace Umbraco.Core.Models
 {
-    public interface IProperty
+    public interface IProperty : IEntity, IRememberBeingDirty
     {
 
         ValueStorageType ValueStorageType { get; }
@@ -23,24 +23,6 @@ namespace Umbraco.Core.Models
         /// </summary>
         string Alias { get; }
 
-        /// <inheritdoc />
-        int Id { get; set; }
-
-        /// <inheritdoc />
-        Guid Key { get; set; }
-
-        /// <inheritdoc />
-        DateTime CreateDate { get; set; }
-
-        /// <inheritdoc />
-        DateTime UpdateDate { get; set; }
-
-        /// <inheritdoc />
-        DateTime? DeleteDate { get; set; } // no change tracking - not persisted
-
-        /// <inheritdoc />
-        bool HasIdentity { get; }
-
         /// <summary>
         /// Gets the value.
         /// </summary>
@@ -55,49 +37,6 @@ namespace Umbraco.Core.Models
         /// Resets the entity identity.
         /// </summary>
         void ResetIdentity();
-
-        bool Equals(EntityBase other);
-        bool Equals(object obj);
-        int GetHashCode();
-        object DeepClone();
-
-        /// <inheritdoc />
-        bool IsDirty();
-
-        /// <inheritdoc />
-        bool IsPropertyDirty(string propertyName);
-
-        /// <inheritdoc />
-        IEnumerable<string> GetDirtyProperties();
-
-        /// <inheritdoc />
-        /// <remarks>Saves dirty properties so they can be checked with WasDirty.</remarks>
-        void ResetDirtyProperties();
-
-        /// <inheritdoc />
-        bool WasDirty();
-
-        /// <inheritdoc />
-        bool WasPropertyDirty(string propertyName);
-
-        /// <inheritdoc />
-        void ResetWereDirtyProperties();
-
-        /// <inheritdoc />
-        void ResetDirtyProperties(bool rememberDirty);
-
-        /// <inheritdoc />
-        IEnumerable<string> GetWereDirtyProperties();
-
-        /// <summary>
-        /// Disables change tracking.
-        /// </summary>
-        void DisableChangeTracking();
-
-        /// <summary>
-        /// Enables change tracking.
-        /// </summary>
-        void EnableChangeTracking();
 
         int PropertyTypeId { get; }
         void PublishValues(string culture = "*", string segment = "*");
