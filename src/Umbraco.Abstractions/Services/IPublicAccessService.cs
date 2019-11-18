c62088aa85849444e9641be4274db47f0d030c92
Bjarke Berg <mail@bergmania.dk>
2019-11-18 09:27:06 +0100

    Moved IMember, IPublicAccessService

--- src/Umbraco.Abstractions/Services/IPublicAccessService.cs
+++ src/Umbraco.Abstractions/Services/IPublicAccessService.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
+﻿using System.Collections.Generic;
 using Umbraco.Core.Models;
-using Umbraco.Core.Security;
 
 namespace Umbraco.Core.Services
 {
