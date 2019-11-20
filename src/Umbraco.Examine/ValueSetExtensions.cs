cd87babfb74ff5c6dd4800f5c59b10a82c9ae98b
Shannon <sdeminick@gmail.com>
2019-11-20 17:20:51 +1100

    Fixes issue with IValueSetValidator filtering

---
+++ src/Umbraco.Examine/ValueSetExtensions.cs
@@ -0,0 +1,17 @@
+﻿using Examine;
+using System.Collections.Generic;
+
+namespace Umbraco.Examine
+{
+    public static class ValueSetExtensions
+    {
+        public static ValueSet Clone(this ValueSet valueSet)
+        {
+            var values = new Dictionary<string, IEnumerable<object>>();
+            foreach (var keyVal in valueSet.Values)
+                values[keyVal.Key] = keyVal.Value;
+            return new ValueSet(valueSet.Id, valueSet.Category, valueSet.ItemType, values);
+        }
+    }
+
+}
