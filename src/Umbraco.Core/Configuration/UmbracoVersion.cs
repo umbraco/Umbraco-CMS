2ef925f9dd3c6201d3267651be2432b070ddd200
Claus <claus@umbraco.com>
2019-11-22 11:04:21 +0100

    Removing the MiniProfiler routes if solution is not in debug mode.

--- src/Umbraco.Core/Configuration/UmbracoVersion.cs
+++ src/Umbraco.Core/Configuration/UmbracoVersion.cs
@@ -5,7 +5,7 @@ namespace Umbraco.Core.Configuration
 {
     public class UmbracoVersion
     {
-        private static readonly Version Version = new Version("7.2.8");
+        private static readonly Version Version = new Version("7.2.9");
 
         /// <summary>
         /// Gets the current version of Umbraco.
