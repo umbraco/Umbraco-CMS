1fd8ff8f5e2a59519a740d8e3e28743ff60608db
Matthew Wise <6782865+Matthew-Wise@users.noreply.github.com>
2019-11-26 17:14:54 +0000

    Service worker configuration

--- src/Umbraco.Web.UI/Umbraco/service-worker.js
+++ src/Umbraco.Web.UI/Umbraco/service-worker.js
@@ -1,1 +1,58 @@
-﻿
+﻿ var currentCache;
+ const cachePrefix = 'umbraco-';
+ 
+ function IsCacheable(event) {
+    const cacheableURLRegexes = [
+        /\/umbraco\/assets\/*/
+        ,/\/umbraco\/lib\/*/
+        ,/\/umbraco\/views\/*/
+        ,/\/umbraco\/js\/*/
+        ,/\/DependencyHandler\.axd/
+        ,/\/umbraco\/Application/
+        ,/\/umbraco$/
+        /* ,/\/umbraco\/ServerVariables$/
+        This shouldnt really change and has the same cache versioning as we use for the sw cache.
+        However changes to the response can be done without the hash changing*/
+    ];
+    var result = cacheableURLRegexes.some(reg => { return reg.test(new URL(event.request.url).pathname); });
+    return result;
+}
+
+self.addEventListener('install', event => {
+    event.waitUntil(async function() {
+        let version = new URL(location).searchParams.get('version');
+        currentCache = cachePrefix + version;
+        await caches.open(currentCache)
+    }());
+});
+
+self.addEventListener('activate', event => {
+    event.waitUntil(
+        caches.keys().then(cacheNames => {
+        return Promise.all(
+            cacheNames.filter(cacheName => {
+            if(cacheName.indexOf(cachePrefix) == 0) {
+                return cacheName !== currentCache;
+            }
+            return false;
+            }).map(cacheName => { return caches.delete(cacheName); })
+        );
+        })
+    );
+});
+
+self.addEventListener('fetch', event => {
+    if(IsCacheable(event)) 
+    {
+        event.respondWith(
+            caches.open(currentCache).then(function(cache) {
+              return cache.match(event.request).then(function (cachedResponse) {
+                return cachedResponse || fetch(event.request).then(function(response) {
+                  cache.put(event.request, response.clone());
+                  return response;
+                });
+              });
+            })
+          );
+    }
+});
