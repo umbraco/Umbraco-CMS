1e68c7584638d9dabf41f9e3fb3c2ea8ccb38841
Niels Lyngsø <nsl@umbraco.dk>
2019-11-26 13:32:28 +0100

    minor correction

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/nestedcontent/nestedcontent.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/nestedcontent/nestedcontent.controller.js
@@ -562,7 +562,7 @@
 
         function updatePropertyActionStates() {
             copyAllEntriesAction.isDisabled = !model.value || model.value.length === 0;
-            removeAllEntriesAction.isDisabled = $scope.model.value.length === 0;
+            removeAllEntriesAction.isDisabled = model.value.length === 0;
         }
 
 
