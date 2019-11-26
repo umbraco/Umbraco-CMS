3dc00827d9d2f6d21ba8968c56566821dacec98a
Matthew Wise <6782865+Matthew-Wise@users.noreply.github.com>
2019-11-26 12:29:18 +0000

    Creatuib of backoffice service worker configuration section

---
+++ src/Umbraco.Core/Configuration/UmbracoSettings/ServiceWorkerConfigElement.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace Umbraco.Core.Configuration.UmbracoSettings
+{
+    internal class ServiceWorkerConfigElement : RawXmlConfigurationElement, IServiceWorkerSection
+    {
+        public IEnumerable<string> Domains
+        {
+            get
+            {
+                if (RawXml != null)
+                {
+                    var domains = RawXml.Elements("domain");
+                    if (domains != null)
+                    {
+                        var result = domains.Select(a => a.Value).Where(d => string.IsNullOrWhiteSpace(d) == false);
+                        return result;
+                    }
+                }
+                return Enumerable.Empty<string>();
+
+            }
+        }
+    }
+}
