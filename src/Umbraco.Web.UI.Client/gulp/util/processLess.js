2948d506eacfc30ba83757b81fa82a831e7a9773
Warren Buckley <warren@umbraco.com>
2019-12-05 12:38:57 +0000

    V8: Bye hardcoded colors lets use Variables (#7246)
    * Bye bye #fff let's use the variable @white instead
    
    * Update to use @black
    
    * Update canvas-designer file with variables where possible - ALOT was not found (need to see if we have similar matches already)
    
    * Update all hex colour variables or add a TODO comment
    
    * Empty LESS files so lets get rid of them
    
    * Moves the color CSS classes out into own less file to ensure variables contains variables only
    
    * Adds in gulp-sourcemaps to allow us to have LESS sourcemaps - https://www.npmjs.com/package/gulp-less#source-maps
    
    * A VERY BAD first attempt at doing DARK mode, but plumbing done. Now a game of tweaking variables
    
    * Revert "A VERY BAD first attempt at doing DARK mode, but plumbing done. Now a game of tweaking variables"
    
    This reverts commit 5ac8080c8b314cd65b1bd735fea88569991736b2.
    
    * Update hard-coded hex values to color variables
    
    * Fix couple typos with LESS variables gray not grey
    
    * Amend build script so we remove the /umbraco/assets/css/maps folder from the build ouput of the ZIP & Nuget Packages
    
    * Removing more hard coded colour values

--- src/Umbraco.Web.UI.Client/gulp/util/processLess.js
+++ src/Umbraco.Web.UI.Client/gulp/util/processLess.js
@@ -6,25 +6,28 @@ var postcss = require('gulp-postcss');
 var less = require('gulp-less');
 var autoprefixer = require('autoprefixer');
 var cssnano = require('cssnano');
-var cleanCss = require("gulp-clean-css");
+var cleanCss = require('gulp-clean-css');
 var rename = require('gulp-rename');
+var sourcemaps = require('gulp-sourcemaps');
 
 module.exports = function(files, out) {
-    
+
     var processors = [
         autoprefixer,
         cssnano({zindex: false})
     ];
-    
+
     console.log("LESS: ", files, " -> ", config.root + config.targets.css + out)
-    
+
     var task = gulp.src(files)
+        .pipe(sourcemaps.init())
         .pipe(less())
         .pipe(cleanCss())
         .pipe(postcss(processors))
         .pipe(rename(out))
+        .pipe(sourcemaps.write('./maps'))
         .pipe(gulp.dest(config.root + config.targets.css));
-    
+
     return task;
-    
+
 };
