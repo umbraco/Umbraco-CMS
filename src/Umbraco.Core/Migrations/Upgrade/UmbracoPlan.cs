6b570cd5e3ea71cd5f55864cd6815e7506973255
LiamL <liam.laverty@intelligentmobile.com>
2020-01-15 14:56:06 +0000

    adds fk index to cmsDictionary -> parentId

--- src/Umbraco.Core/Migrations/Upgrade/UmbracoPlan.cs
+++ src/Umbraco.Core/Migrations/Upgrade/UmbracoPlan.cs
@@ -187,6 +187,9 @@ namespace Umbraco.Core.Migrations.Upgrade
             To<AddPropertyTypeValidationMessageColumns>("{3D67D2C8-5E65-47D0-A9E1-DC2EE0779D6B}");
             To<MissingContentVersionsIndexes>("{EE288A91-531B-4995-8179-1D62D9AA3E2E}");
 
+
+            To<AddDatabaseIndexesMissingOnForeignKeys>("{a78e3369-8ea3-40ec-ad3f-5f76929d2b20}");
+
             //FINAL
         }
     }
---
+++ src/Umbraco.Core/Migrations/Upgrade/V_8_6_0/AddDatabaseIndexesMissingOnForeignKeys.cs
@@ -0,0 +1,53 @@
+﻿using NPoco;
+using System;
+using System.Linq;
+using Umbraco.Core.Persistence.DatabaseAnnotations;
+using Umbraco.Core.Persistence.Dtos;
+
+namespace Umbraco.Core.Migrations.Upgrade.V_8_6_0
+{
+    public class AddDatabaseIndexesMissingOnForeignKeys : MigrationBase
+    {
+        public AddDatabaseIndexesMissingOnForeignKeys(IMigrationContext context)
+            : base(context)
+        {
+
+        }
+
+        public override void Migrate()
+        {
+            var newIndexes = new[]
+            {
+                (typeof(DictionaryDto), nameof(DictionaryDto.Parent))
+            };
+
+            foreach (var (type, propertyName) in newIndexes)
+            {
+                CreateIndexIfNotExists(type, propertyName);
+            }
+        }
+
+        private void CreateIndexIfNotExists(Type dto, string propertyName)
+        {
+            var property = dto.GetProperty(propertyName);
+            var indexName = property.GetCustomAttributes(false).OfType<IndexAttribute>().Single().Name;
+
+            if (IndexExists(indexName))
+            {
+                return;
+            }
+
+            var tableName = dto.GetCustomAttributes<TableNameAttribute>(false).Single().Value;
+            var columnName = property.GetCustomAttributes(false).OfType<ColumnAttribute>().Single().Name;
+
+
+            Create
+                .Index(indexName)
+                .OnTable(tableName)
+                .OnColumn(columnName)
+                .Ascending()
+                .WithOptions().NonClustered() // All newly defined indexes are non-clustered
+                .Do();
+        }
+    }
+}
