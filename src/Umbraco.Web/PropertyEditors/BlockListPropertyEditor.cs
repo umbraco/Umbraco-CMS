678e31834d966adc59b6f423d403e1dfc88a3744
Niels Lyngsø <nsl@umbraco.dk>
2020-01-20 09:06:27 +0100

    registrer Block List Editor

---
+++ src/Umbraco.Web/PropertyEditors/BlockListPropertyEditor.cs
@@ -0,0 +1,30 @@
+﻿using Umbraco.Core;
+using Umbraco.Core.Logging;
+using Umbraco.Core.PropertyEditors;
+using Umbraco.Core.Services;
+
+namespace Umbraco.Web.PropertyEditors
+{
+    /// <summary>
+    /// Represents a block list property editor.
+    /// </summary>
+    [DataEditor(
+        Constants.PropertyEditors.Aliases.BlockList,
+        "Block List",
+        "blocklist",
+        ValueType = ValueTypes.Json,
+        Group = Constants.PropertyEditors.Groups.Lists,
+        Icon = "icon-thumbnail-list")]
+    public class BlockListPropertyEditor : DataEditor
+    {
+        public BlockListPropertyEditor(ILogger logger)
+            : base(logger)
+        { }
+
+        #region Pre Value Editor
+        //protected override IConfigurationEditor CreateConfigurationEditor() => new BlockEditorListConfigurationEditor();
+
+        #endregion
+
+    }
+}
