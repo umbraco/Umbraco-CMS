cf70877f2e427b0ab225c7e8170cbf3e9e79f1d7
Sebastiaan Janssen <sebastiaan@umbraco.com>
2020-01-24 11:07:30 +0100

    Mistake in the manual fix for 5831

--- src/Umbraco.Core/Migrations/Upgrade/V_8_0_0/VariantsMigration.cs
+++ src/Umbraco.Core/Migrations/Upgrade/V_8_0_0/VariantsMigration.cs
@@ -191,7 +191,7 @@ FROM {PreTables.ContentVersion} v INNER JOIN {PreTables.Document} d ON d.version
                 var temp2 = Database.Fetch<dynamic>($@"SELECT cver.versionId, n.text
 FROM {PreTables.ContentVersion} cver
 JOIN {SqlSyntax.GetQuotedTableName(Constants.DatabaseSchema.Tables.Node)} n ON cver.nodeId=n.id
-WHERE cver.versionId NOT IN (SELECT nodeId FROM {SqlSyntax.GetQuotedTableName(PreTables.Document)})");
+WHERE cver.nodeId NOT IN (SELECT nodeId FROM {SqlSyntax.GetQuotedTableName(PreTables.Document)})");
 
                 foreach (var t in temp2)
                     Database.Execute($@"UPDATE {PreTables.ContentVersion} SET text=@text, {SqlSyntax.GetQuotedColumnName("current")}=1, userId=0 WHERE versionId=@versionId",
@@ -202,7 +202,7 @@ WHERE cver.versionId NOT IN (SELECT nodeId FROM {SqlSyntax.GetQuotedTableName(Pr
                 Database.Execute($@"UPDATE {PreTables.ContentVersion} SET text=n.text, {SqlSyntax.GetQuotedColumnName("current")}=1, userId=0
 FROM {PreTables.ContentVersion} cver
 JOIN {SqlSyntax.GetQuotedTableName(Constants.DatabaseSchema.Tables.Node)} n ON cver.nodeId=n.id
-WHERE cver.versionId NOT IN (SELECT nodeId FROM {SqlSyntax.GetQuotedTableName(PreTables.Document)})");
+WHERE cver.nodeId NOT IN (SELECT nodeId FROM {SqlSyntax.GetQuotedTableName(PreTables.Document)})");
             }
 
             // create table
