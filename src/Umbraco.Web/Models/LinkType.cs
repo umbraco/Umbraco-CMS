90cd3a2a3101c7a928db8fc091ddfdad62fe143b
Ronald Barendse <ronald@panoramastudios.nl>
2020-01-28 23:22:05 +0100

    Added documentation to Link and LinkType models

--- src/Umbraco.Web/Models/LinkType.cs
+++ src/Umbraco.Web/Models/LinkType.cs
@@ -1,9 +1,21 @@
 ﻿namespace Umbraco.Web.Models
 {
+    /// <summary>
+    /// Represents the type of link.
+    /// </summary>
     public enum LinkType
     {
+        /// <summary>
+        /// Represents a link to content.
+        /// </summary>
         Content,
+        /// <summary>
+        /// Represents a link to media.
+        /// </summary>
         Media,
+        /// <summary>
+        /// Represents a link to an external URL.
+        /// </summary>
         External
     }
 }
