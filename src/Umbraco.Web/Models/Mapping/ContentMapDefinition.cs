12bb2a42d6286b3c2b6d43e1de231dc590727590
Daniël Knippers <daniel.knippers@perplex.nl>
2020-02-18 11:47:58 +0100

    ContentVariantDisplay: add DisplayName property and use in Umbr… (#7677)

--- src/Umbraco.Web/Models/Mapping/ContentMapDefinition.cs
+++ src/Umbraco.Web/Models/Mapping/ContentMapDefinition.cs
@@ -50,7 +50,7 @@ namespace Umbraco.Web.Models.Mapping
             _tabsAndPropertiesMapper = new TabsAndPropertiesMapper<IContent>(localizedTextService);
             _stateMapper = new ContentSavedStateMapper<ContentPropertyDisplay>();
             _basicStateMapper = new ContentBasicSavedStateMapper<ContentPropertyBasic>();
-            _contentVariantMapper = new ContentVariantMapper(_localizationService);
+            _contentVariantMapper = new ContentVariantMapper(_localizationService, localizedTextService);
         }
 
         public void DefineMaps(UmbracoMapper mapper)
@@ -102,7 +102,7 @@ namespace Umbraco.Web.Models.Mapping
             target.ContentDto.Properties = context.MapEnumerable<Property, ContentPropertyDto>(source.Properties);
         }
 
-        // Umbraco.Code.MapAll -Segment -Language
+        // Umbraco.Code.MapAll -Segment -Language -DisplayName
         private void Map(IContent source, ContentVariantDisplay target, MapperContext context)
         {
             target.CreateDate = source.CreateDate;
