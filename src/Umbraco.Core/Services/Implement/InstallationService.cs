b2ddc4a2b060dfa4d55d5e10b7684dddeb906895
elitsa <elm@umbraco.dk>
2020-02-18 14:48:36 +0100

    Creating the service interfaces and their implementations. Registering them in CoreInitialComposer. These services delegate the work to repositories

---
+++ src/Umbraco.Core/Services/Implement/InstallationService.cs
@@ -0,0 +1,21 @@
+﻿using System.Threading.Tasks;
+using Umbraco.Core.Models;
+using Umbraco.Core.Persistence.Repositories;
+
+namespace Umbraco.Core.Services.Implement
+{
+    public class InstallationService : IInstallationService
+    {
+        private readonly IInstallationRepository _installationRepository;
+
+        public InstallationService(IInstallationRepository installationRepository)
+        {
+            _installationRepository = installationRepository;
+        }
+
+        public async Task Install(InstallLog installLog)
+        {
+            await _installationRepository.SaveInstall(installLog);
+        }
+    }
+}
