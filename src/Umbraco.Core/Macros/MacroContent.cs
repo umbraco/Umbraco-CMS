e0b921952848e87cabe2bdd55b05b7a8bb709891
Elitsa Marinovska <21998037+elit0451@users.noreply.github.com>
2020-02-25 14:27:58 +0100

    Update MacroContent.cs to use `is` operator for comparison to null
    Co-Authored-By: Bjarke Berg <mail@bergmania.dk>

--- src/Umbraco.Core/Macros/MacroContent.cs
+++ src/Umbraco.Core/Macros/MacroContent.cs
@@ -12,7 +12,7 @@ namespace Umbraco.Web.Macros
         public DateTime Date { get; set; } = DateTime.Now;
 
         // a value indicating whether the content is empty
-        public bool IsEmpty => Text == null;
+        public bool IsEmpty => Text is null;
 
         // gets an empty macro content
         public static MacroContent Empty { get; } = new MacroContent();
