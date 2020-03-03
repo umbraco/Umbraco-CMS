a4003ad7992599624ec410486ce2caa8831d0ad1
Bjarke Berg <mail@bergmania.dk>
2020-03-03 12:09:15 +0100

    Clean up

--- src/Umbraco.Core/Routing/PublishedRouter.cs
+++ src/Umbraco.Core/Routing/PublishedRouter.cs
@@ -595,37 +595,6 @@ namespace Umbraco.Web.Routing
                         _logger.Debug<PublishedRouter>("Current member has access");
                         break;
                 }
-
-                //
-                // else
-                // {
-                //     // grab the current member
-                //     var member = _membershipHelper.GetCurrentMember();
-                //     // if the member has the "approved" and/or "locked out" properties, make sure they're correctly set before allowing access
-                //     var memberIsActive = true;
-                //     if (member != null)
-                //     {
-                //         if (member.HasProperty(Constants.Conventions.Member.IsApproved) == false)
-                //             memberIsActive = member.Value<bool>(Constants.Conventions.Member.IsApproved);
-                //
-                //         if (member.HasProperty(Constants.Conventions.Member.IsLockedOut) == false)
-                //             memberIsActive = member.Value<bool>(Constants.Conventions.Member.IsLockedOut) == false;
-                //     }
-                //
-                //     if (memberIsActive == false)
-                //     {
-                //         _logger.Debug<PublishedRouter>(
-                //             "Current member is either unapproved or locked out, redirect to error page");
-                //         var errorPageId = publicAccessAttempt.Result.NoAccessNodeId;
-                //         if (errorPageId != request.PublishedContent.Id)
-                //             request.PublishedContent =
-                //                 request.UmbracoContext.PublishedSnapshot.Content.GetById(errorPageId);
-                //     }
-                //     else
-                //     {
-                //         _logger.Debug<PublishedRouter>("Current member has access");
-                //     }
-                // }
             }
             else
             {
