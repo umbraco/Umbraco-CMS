6d8a23504e493b48dca62446e66165e90bd4103d
Bjarke Berg <mail@bergmania.dk>
2020-03-17 11:39:55 +0100

    Remove resize parameters if larger than 5000

--- src/Umbraco.Web.Website/AspNetCore/UmbracoWebsiteServiceCollectionExtensions.cs
+++ src/Umbraco.Web.Website/AspNetCore/UmbracoWebsiteServiceCollectionExtensions.cs
@@ -1,5 +1,15 @@
+using System.Collections.Generic;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using SixLabors.ImageSharp.Processing;
+using SixLabors.ImageSharp.Web;
+using SixLabors.ImageSharp.Web.Caching;
+using SixLabors.ImageSharp.Web.Commands;
 using SixLabors.ImageSharp.Web.DependencyInjection;
+using SixLabors.ImageSharp.Web.Middleware;
+using SixLabors.ImageSharp.Web.Processors;
+using SixLabors.ImageSharp.Web.Providers;
+using SixLabors.Memory;
 
 namespace Umbraco.Web.Website.AspNetCore
 {
@@ -7,11 +17,72 @@ namespace Umbraco.Web.Website.AspNetCore
     {
         public static IServiceCollection AddUmbracoWebsite(this IServiceCollection services)
         {
-            services.AddImageSharp();
+            services.AddImageSharpCore(
+                    options =>
+                    {
+                        options.Configuration = SixLabors.ImageSharp.Configuration.Default;
+                        options.MaxBrowserCacheDays = 7;
+                        options.MaxCacheDays = 365;
+                        options.CachedNameLength = 8;
+                        options.OnParseCommands = context =>
+                        {
+                            RemoveIntParamenterIfValueGreatherThen(context.Commands, ResizeWebProcessor.Width, 5000);
+                            RemoveIntParamenterIfValueGreatherThen(context.Commands, ResizeWebProcessor.Height, 5000);
+                        };
+                        options.OnBeforeSave = _ => { };
+                        options.OnProcessed = _ => { };
+                        options.OnPrepareResponse = _ => { };
+                    })
+                .SetRequestParser<QueryCollectionRequestParser>()
+                .SetMemoryAllocator(provider => ArrayPoolMemoryAllocator.CreateWithMinimalPooling())
+                .Configure<PhysicalFileSystemCacheOptions>(options =>
+                {
+                    options.CacheFolder = "is-cache";
+
+                })
+                .SetCache<PhysicalFileSystemCache>()
+                .SetCacheHash<CacheHash>()
+                .AddProvider<PhysicalFileSystemProvider>()
+                .AddProcessor<ResizeWebProcessor>()
+                .AddProcessor<FormatWebProcessor>()
+                .AddProcessor<BackgroundColorWebProcessor>();
 
 
             return services;
         }
 
+        private static void RemoveIntParamenterIfValueGreatherThen(IDictionary<string, string> commands, string parameter, int maxValue)
+        {
+            if (commands.TryGetValue(parameter, out var command))
+            {
+                if (int.TryParse(command, out var i))
+                {
+                    if (i > maxValue)
+                    {
+                        commands.Remove(parameter);
+                    }
+                }
+            }
+        }
+    }
+
+    public class UmbracoResizeWebProcessor : ResizeWebProcessor
+    {
+
+        public new FormattedImage Process(FormattedImage image, ILogger logger, IDictionary<string, string> commands)
+        {
+            if(commands.TryGetValue("width", out var command))
+            {
+                if (int.TryParse(command, out var width))
+                {
+                    if (width > 5000)
+                    {
+                        commands.Remove("width");
+                    }
+                }
+            }
+
+            return Process(image, logger, commands);
+        }
     }
 }
