8986783cbc4a78cad9a3cbd1abe6a31c38252027
Bjarke Berg <mail@bergmania.dk>
2020-03-23 14:31:21 +0100

    Parse connection string provider

--- src/Umbraco.Configuration/Models/ConnectionStrings.cs
+++ src/Umbraco.Configuration/Models/ConnectionStrings.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Linq;
-using System.Text.Json.Serialization;
+using System.Data.Common;
 using Microsoft.Extensions.Configuration;
+using Umbraco.Core;
 using Umbraco.Core.Configuration;
 
 namespace Umbraco.Configuration.Models
 {
-    internal class ConnectionStrings : IConnectionStrings
+    public class ConnectionStrings : IConnectionStrings
     {
         private readonly IConfiguration _configuration;
 
@@ -17,8 +17,43 @@ namespace Umbraco.Configuration.Models
 
         public ConfigConnectionString this[string key]
         {
-            get => new ConfigConnectionString(_configuration.GetConnectionString(key), "System.Data.SqlClient", key);
+            get
+            {
+                var connectionString = _configuration.GetConnectionString(key);
+                var provider = ParseProvider(connectionString);
+                return new ConfigConnectionString(connectionString, provider, key);
+            }
             set => throw new NotImplementedException();
+        }
+
+        private string ParseProvider(string connectionString)
+        {
+            if (string.IsNullOrEmpty(connectionString))
+            {
+                return null;
+            }
+
+            var builder = new DbConnectionStringBuilder();
+
+            builder.ConnectionString = connectionString;
+
+            if (builder.TryGetValue("Data Source", out var ds) && ds is string dataSource)
+            {
+                if (dataSource.EndsWith(".sdf"))
+                {
+                    return Constants.DbProviderNames.SqlCe;
+                }
+            }
+
+            if (builder.TryGetValue("Server", out var s) && s is string server && builder.TryGetValue("Database", out var db) && db is string database)
+            {
+                if (!string.IsNullOrEmpty(server) && !string.IsNullOrEmpty(database))
+                {
+                    return Constants.DbProviderNames.SqlServer;
+                }
+            }
+
+            throw new ArgumentException("Cannot determine provider name from connection string", nameof(connectionString));
         }
     }
 }
---
+++ src/Umbraco.Tests.UnitTests/Umbraco.Configuration/Models/ConnectionStringsTests.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.Extensions.Configuration;
+using Moq;
+using NUnit.Framework;
+using Umbraco.Configuration.Models;
+using Umbraco.Core;
+
+namespace Umbraco.Tests.UnitTests.Umbraco.Configuration.Models
+{
+    public class ConnectionStringsTests
+    {
+        [Test]
+        [TestCase("", ExpectedResult = null)]
+        [TestCase(null, ExpectedResult = null)]
+        [TestCase(@"Data Source=|DataDirectory|\Umbraco.sdf;Flush Interval=1;", ExpectedResult = Constants.DbProviderNames.SqlCe)]
+        [TestCase(@"Server=(LocalDb)\Umbraco;Database=NetCore;Integrated Security=true", ExpectedResult = Constants.DbProviderNames.SqlServer)]
+        public string ParseProviderName(string connectionString)
+        {
+            var key = Constants.System.UmbracoConnectionName;
+            var configuration = new Mock<IConfiguration>();
+
+
+            //This is the underlying method that is called by Configuration.GetConnectionString(string)
+            if (connectionString != null)
+            {
+                configuration.Setup(x => x.GetSection("ConnectionStrings")[key]).Returns(connectionString);
+            }
+
+
+            var connectionStrings = new ConnectionStrings(configuration.Object);
+
+            var actual = connectionStrings[key];
+
+            Assert.AreEqual(connectionString, actual.ConnectionString);
+            Assert.AreEqual(key, actual.Name);
+
+            return connectionStrings[key].ProviderName;
+        }
+    }
+}
