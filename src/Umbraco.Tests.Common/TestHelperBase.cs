139cd3935f968b377ac1fac4a7afbfe4e485d476
Shannon <sdeminick@gmail.com>
2020-03-24 12:00:48 +1100

    oops

--- src/Umbraco.Tests.Common/TestHelperBase.cs
+++ src/Umbraco.Tests.Common/TestHelperBase.cs
@@ -1,9 +1,6 @@
 ﻿using System;
 using System.IO;
 using System.Reflection;
-using LightInject;
-using LightInject.Microsoft.DependencyInjection;
-using Microsoft.Extensions.Hosting;
 using Moq;
 using Umbraco.Core;
 using Umbraco.Core.Cache;
