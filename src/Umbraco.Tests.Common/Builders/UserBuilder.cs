9e38ed045fc708b1cf51ca0875f730111152c9dc
Bjarke Berg <mail@bergmania.dk>
2020-04-01 08:18:56 +0200

    Aligned builders

--- src/Umbraco.Tests.Common/Builders/UserBuilder.cs
+++ src/Umbraco.Tests.Common/Builders/UserBuilder.cs
@@ -1,16 +1,21 @@
-using System;
-using System.Linq;
-using Moq;
-using Umbraco.Core;
-using Umbraco.Core.Configuration;
 using Umbraco.Core.Models.Membership;
 using Umbraco.Tests.Common.Builders.Interfaces;
 
 namespace Umbraco.Tests.Common.Builders
 {
-    public class UserBuilder
-        : BuilderBase<User>,
-            IWithIdBuilder
+
+    public class UserBuilder : UserBuilder<object>
+    {
+        public UserBuilder() : base(null)
+        {
+        }
+    }
+
+    public class UserBuilder<TParent>
+        : ChildBuilderBase<TParent, User>,
+            IWithIdBuilder,
+            IWithNameBuilder,
+            IWithApprovedBuilder
     {
         private int? _id;
         private string _language;
@@ -22,52 +27,48 @@ namespace Umbraco.Tests.Common.Builders
         private string _username;
         private string _defaultLang;
         private string _suffix = string.Empty;
+        private GlobalSettingsBuilder<UserBuilder<TParent>> _globalSettingsBuilder;
 
-        public UserBuilder WithDefaultUILanguage(string defaultLang)
+
+        public UserBuilder(TParent parentBuilder) : base(parentBuilder)
+        {
+            _globalSettingsBuilder = new GlobalSettingsBuilder<UserBuilder<TParent>>(this);
+        }
+
+        public GlobalSettingsBuilder<UserBuilder<TParent>> AddGlobalSettings() => _globalSettingsBuilder;
+        public UserBuilder<TParent> WithDefaultUILanguage(string defaultLang)
         {
             _defaultLang = defaultLang;
             return this;
         }
 
-        public UserBuilder WithLanguage(string language)
+        public UserBuilder<TParent> WithLanguage(string language)
         {
             _language = language;
             return this;
         }
 
-        public UserBuilder WithApproved(bool approved)
-        {
-            _approved = approved;
-            return this;
-        }
-
-        public UserBuilder WithRawPassword(string rawPassword)
+        public UserBuilder<TParent> WithRawPassword(string rawPassword)
         {
             _rawPassword = rawPassword;
             return this;
         }
 
-        public UserBuilder WithEmail(string email)
+        public UserBuilder<TParent> WithEmail(string email)
         {
             _email = email;
             return this;
         }
 
-        public UserBuilder WithUsername(string username)
+        public UserBuilder<TParent> WithUsername(string username)
         {
             _username = username;
             return this;
         }
 
-        public UserBuilder WithLockedOut(bool isLockedOut)
+        public UserBuilder<TParent> WithLockedOut(bool isLockedOut)
         {
             _isLockedOut = isLockedOut;
-            return this;
-        }
-
-        public UserBuilder WithName(string name)
-        {
-            _name = name;
             return this;
         }
 
@@ -76,7 +77,7 @@ namespace Umbraco.Tests.Common.Builders
         /// </summary>
         /// <param name="suffix"></param>
         /// <returns></returns>
-        public UserBuilder WithSuffix(string suffix)
+        public UserBuilder<TParent> WithSuffix(string suffix)
         {
             _suffix = suffix;
             return this;
@@ -84,16 +85,25 @@ namespace Umbraco.Tests.Common.Builders
 
         public override User Build()
         {
-            var globalSettings = Mock.Of<IGlobalSettings>(x => x.DefaultUILanguage == (_defaultLang ?? "en-US"));
-            return new User(globalSettings,
-                _name ?? "TestUser" + _suffix,
-                _email ?? "test" + _suffix + "@test.com",
-                _username ?? "TestUser" + _suffix,
-                _rawPassword ?? "abcdefghijklmnopqrstuvwxyz")
+            var globalSettings = _globalSettingsBuilder.Build();
+            var name = _name ?? "TestUser" + _suffix;
+            var email = _email ?? "test" + _suffix + "@test.com";
+            var username = _username ?? "TestUser" + _suffix;
+            var rawPassword = _rawPassword ?? "abcdefghijklmnopqrstuvwxyz";
+            var language = _language ?? globalSettings.DefaultUILanguage;
+            var isLockedOut = _isLockedOut ?? false;
+            var approved = _approved ?? true;
+
+            return new User(
+                globalSettings,
+                name,
+                email,
+                username,
+                rawPassword)
             {
-                Language = _language ?? _defaultLang ?? "en-US",
-                IsLockedOut = _isLockedOut ?? false,
-                IsApproved = _approved ?? true
+                Language = language,
+                IsLockedOut = isLockedOut,
+                IsApproved = approved
             };
         }
 
@@ -101,6 +111,18 @@ namespace Umbraco.Tests.Common.Builders
         {
             get => _id;
             set => _id = value;
+        }
+
+        string IWithNameBuilder.Name
+        {
+            get => _name;
+            set => _name = value;
+        }
+
+        bool? IWithApprovedBuilder.Approved
+        {
+            get => _approved;
+            set => _approved = value;
         }
     }
 }
