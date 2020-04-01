d6cad317509ced1a9b65d21f973c4518274f2a3e
Niels Lyngsø <nsl@umbraco.dk>
2020-04-01 14:31:56 +0200

    remove unused controller

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/notsupported/notsupported.controller.js
+++
@@ -1,9 +0,0 @@
-angular.module('umbraco').controller("Umbraco.PropertyEditors.NotSupportedController",
-    function ($scope) {
-
-        var vm = this;
-
-        console.log($scope.umbProperty);
-
-    }
-);
