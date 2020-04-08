b4387e757a3b70c8e7433aa303132688c657abd6
Niels Lyngsø <nsl@umbraco.dk>
2020-04-08 15:46:37 +0200

    Block Picker

---
+++ src/Umbraco.Web.UI.Client/src/views/common/infiniteeditors/blockpicker/blockpicker.controller.js
@@ -0,0 +1,21 @@
+//used for the media picker dialog
+angular.module("umbraco")
+.controller("Umbraco.Editors.BlockPickerController",
+    function ($scope) {
+        var vm = this;
+
+        vm.model = $scope.model;
+
+        vm.selectItem = function() {
+            vm.model.selectedItem = item;
+            vm.model.submit($scope.model);
+        }
+
+        vm.close = function() {
+            if ($scope.model && $scope.model.close) {
+                $scope.model.close($scope.model);
+            }
+        }
+
+    }
+);
