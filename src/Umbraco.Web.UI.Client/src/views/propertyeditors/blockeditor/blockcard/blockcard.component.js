33f507a5757135f9ca5b3ee36edd86dfd8126fca
Niels Lyngsø <nsl@umbraco.dk>
2020-04-08 15:33:04 +0200

    blockcard corrections

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/blockeditor/blockcard/blockcard.component.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/blockeditor/blockcard/blockcard.component.js
@@ -5,8 +5,6 @@
         .module("umbraco")
         .component("umbBlockCard", {
             templateUrl: "views/propertyeditors/blockeditor/blockcard/blockcard.component.html",
-            controller: BlockCardController,
-            controllerAs: "vm",
             transclude: true,
             bindings: {
                 blockConfigModel: "<",
@@ -14,15 +12,5 @@
             }
         });
 
-    function BlockCardController() {
-        
-        var vm = this;
-
-        vm.$onInit = function() {
-            console.log(vm.blockConfigModel);
-            console.log(vm.elementTypeModel);
-        }
-
-    }
 
 })();
