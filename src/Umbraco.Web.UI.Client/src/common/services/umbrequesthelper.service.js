76e4b6b3012753b9388963e636380ac3c227ec47
Matthew-Wise <6782865+Matthew-Wise@users.noreply.github.com>
2020-04-10 17:07:37 +0100

    Replaced angular.toJson with Utilities.toJson [#CanConHackathon] (#7924)

--- src/Umbraco.Web.UI.Client/src/common/services/umbrequesthelper.service.js
+++ src/Umbraco.Web.UI.Client/src/common/services/umbrequesthelper.service.js
@@ -342,11 +342,11 @@ function umbRequestHelper($http, $q, notificationsService, eventsService, formHe
                     //add the json data
                     if (angular.isArray(data)) {
                         _.each(data, function(item) {
-                            formData.append(item.key, !angular.isString(item.value) ? angular.toJson(item.value) : item.value);
+                            formData.append(item.key, !angular.isString(item.value) ? Utilities.toJson(item.value) : item.value);
                         });
                     }
                     else {
-                        formData.append(data.key, !angular.isString(data.value) ? angular.toJson(data.value) : data.value);
+                        formData.append(data.key, !angular.isString(data.value) ? Utilities.toJson(data.value) : data.value);
                     }
 
                     //call the callback
