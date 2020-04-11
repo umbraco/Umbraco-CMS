c3101e5e1e2cbbce75a87b60b3ecb85627462c65
BatJan <jaskov@gmail.com>
2020-04-12 00:16:20 +0200

    Replace angular.element with jQuery

--- src/Umbraco.Web.UI.Client/src/common/directives/components/umbpasswordtoggle.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/umbpasswordtoggle.directive.js
@@ -20,9 +20,9 @@
         var directive = {
             restrict: 'A',
             scope: {},
-            link: function(scope, elem, attrs) {
+            link: function (scope, elem, attrs) {
                 scope.tgl = function () { elem.attr("type", (elem.attr("type") === "text" ? "password" : "text")); }
-                var lnk = angular.element("<a data-ng-click=\"tgl()\">Toggle</a>");
+                var lnk = $("<a data-ng-click=\"tgl()\">Toggle</a>");
                 $compile(lnk)(scope);
                 elem.wrap("<div class=\"password-toggle\"/>").after(lnk);
             }
