edd5adbb034c08737bce6ee06902cd76d5c00a15
Marc Goodson <marc@snowmoon.co.uk>
2020-04-12 20:46:04 +0100

    Make the 'GetBackOfficeFields' methods virtual
    When implementing your own IUmbracoTreeSearcherFields, it would be good to be able to inherit from this default UmbracoTreeSearcherFields implementation, and then override whichever method you'd like to manipulate to add additional fields to, instead, 'I think' you'd have to implement all four methods in your custom implementation?

--- src/Umbraco.Web/Search/UmbracoTreeSearcherFields.cs
+++ src/Umbraco.Web/Search/UmbracoTreeSearcherFields.cs
@@ -7,23 +7,23 @@ namespace Umbraco.Web.Search
     public class UmbracoTreeSearcherFields : IUmbracoTreeSearcherFields
     {
         private IReadOnlyList<string> _backOfficeFields = new List<string> {"id", "__NodeId", "__Key"};
-        public IEnumerable<string> GetBackOfficeFields()
+        public virtual IEnumerable<string> GetBackOfficeFields()
         {
             return _backOfficeFields;
         }
 
 
         private IReadOnlyList<string> _backOfficeMembersFields = new List<string> {"email", "loginName"};
-        public IEnumerable<string> GetBackOfficeMembersFields()
+        public virtual IEnumerable<string> GetBackOfficeMembersFields()
         {
             return _backOfficeMembersFields;
         }
         private IReadOnlyList<string> _backOfficeMediaFields = new List<string> {UmbracoExamineIndex.UmbracoFileFieldName };
-        public IEnumerable<string> GetBackOfficeMediaFields()
+        public virtual IEnumerable<string> GetBackOfficeMediaFields()
         {
             return _backOfficeMediaFields;
         }
-        public IEnumerable<string> GetBackOfficeDocumentFields()
+        public virtual IEnumerable<string> GetBackOfficeDocumentFields()
         {
             return Enumerable.Empty<string>();
         }
