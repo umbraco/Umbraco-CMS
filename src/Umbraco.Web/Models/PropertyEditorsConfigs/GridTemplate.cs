99a582c6fbb90e9cc90b52be03eea0d4d0d4ed9c
Arkadiusz Biel <abi@novicell.co.uk>
2020-04-16 15:13:23 +0100

    Update src/Umbraco.Web/Models/PropertyEditorsConfigs/GridTemplate.cs
    Co-Authored-By: Ronald Barendse <ronald@barend.se>

--- src/Umbraco.Web/Models/PropertyEditorsConfigs/GridTemplate.cs
+++ src/Umbraco.Web/Models/PropertyEditorsConfigs/GridTemplate.cs
@@ -5,6 +5,6 @@ namespace Umbraco.Web.Models.PropertyEditorsConfigs
     public class GridTemplate
     {
         public string Name { get; set; }
-        public List<GridSection> Sections{ get; set; }
+        public IList<GridSection> Sections { get; set; }
     }
 }
