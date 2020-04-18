73012ab4af6796bb561caa0d3ade7011b0435252
Liam Laverty <liamlaverty@gmail.com>
2020-04-18 14:19:20 +0100

    Utilities.js now has an implementation of angular.equals

--- src/Umbraco.Web.UI.Client/src/utilities.js
+++ src/Umbraco.Web.UI.Client/src/utilities.js
@@ -10,106 +10,179 @@
  */
 (function (window) {
 
-    /**
-     * Equivalent to angular.noop
-     */
-    const noop = () => { };
+  /**
+   * Equivalent to angular.noop
+   */
+  const noop = () => { };
 
-    /**
-     * Facade to angular.copy
-     */
-    const copy = val => angular.copy(val);
+  /**
+   * Facade to angular.copy
+   */
+  const copy = val => angular.copy(val);
 
-    /**
-     * Equivalent to angular.isArray
-     */
-    const isArray = val => Array.isArray(val) || val instanceof Array;
 
-    /**
-     * Facade to angular.equals
-     */
-    const equals = (a, b) => angular.equals(a, b);
-    
-    /**
-     * Facade to angular.extend
-     * Use this with Angular objects, for vanilla JS objects, use Object.assign()
-     */
-    const extend = (dst, src) => angular.extend(dst, src);
 
-    /**
-     * Equivalent to angular.isFunction
-     */
-    const isFunction = val => typeof val === 'function';
+  /**
+   * Equivalent to angular.isArray
+   */
+  const isArray = val => Array.isArray(val) || val instanceof Array;
 
-    /**
-     * Equivalent to angular.isUndefined
-     */
-    const isUndefined = val => typeof val === 'undefined';
 
-    /**
-     * Equivalent to angular.isDefined. Inverts result of const isUndefined
-     */
-    const isDefined = val => !isUndefined(val);
 
-    /**
-     * Equivalent to angular.isString
-     */
-    const isString = val => typeof val === 'string';
+  /**
+   * Facade to angular.equals
+   */
+  const equals = (a, b) => {
+    // angular.equals(a, b);
 
-    /**
-     * Equivalent to angular.isNumber
-     */
-    const isNumber = val => typeof val === 'number';
-
-    /**
-     * Equivalent to angular.isObject
-     */
-    const isObject = val => val !== null && typeof val === 'object';
-
-    const isWindow = obj => obj && obj.window === obj;
-
-    const isScope = obj => obj && obj.$evalAsync && obj.$watch;
-
-    const toJsonReplacer = (key, value) => {
-        var val = value;      
-        if (typeof key === 'string' && key.charAt(0) === '$' && key.charAt(1) === '$') {
-          val = undefined;
-        } else if (isWindow(value)) {
-          val = '$WINDOW';
-        } else if (value &&  window.document === value) {
-          val = '$DOCUMENT';
-        } else if (isScope(value)) {
-          val = '$SCOPE';
-        }      
-        return val;
-      }
-    /**
-     * Equivalent to angular.toJson
-     */
-    const toJson = (obj, pretty) => {
-        if (isUndefined(obj)) return undefined;
-        if (!isNumber(pretty)) {
-          pretty = pretty ? 2 : null;
+    if (o1 === o2) return true;
+    if (o1 === null || o2 === null) return false;
+    // eslint-disable-next-line no-self-compare
+    if (o1 !== o1 && o2 !== o2) return true; // NaN === NaN
+    var t1 = typeof o1, t2 = typeof o2, length, key, keySet;
+    if (t1 === t2 && t1 === 'object') {
+      if (isArray(o1)) {
+        if (!isArray(o2)) return false;
+        if ((length = o1.length) === o2.length) {
+          for (key = 0; key < length; key++) {
+            if (!equals(o1[key], o2[key])) return false;
+          }
+          return true;
         }
-        return JSON.stringify(obj, toJsonReplacer, pretty);
+      } else if (isDate(o1)) {
+        if (!isDate(o2)) return false;
+        return simpleCompare(o1.getTime(), o2.getTime());
+      } else if (isRegExp(o1)) {
+        if (!isRegExp(o2)) return false;
+        return o1.toString() === o2.toString();
+      } else {
+        if (isScope(o1) || isScope(o2) || isWindow(o1) || isWindow(o2) ||
+          isArray(o2) || isDate(o2) || isRegExp(o2)) return false;
+        keySet = createMap();
+        for (key in o1) {
+          if (key.charAt(0) === '$' || isFunction(o1[key])) continue;
+          if (!equals(o1[key], o2[key])) return false;
+          keySet[key] = true;
+        }
+        for (key in o2) {
+          if (!(key in keySet) &&
+            key.charAt(0) !== '$' &&
+            isDefined(o2[key]) &&
+            !isFunction(o2[key])) return false;
+        }
+        return true;
+      }
     }
+    return false;
 
-    let _utilities = {
-        noop: noop,
-        copy: copy,
-        isArray: isArray,
-        equals: equals,
-        extend: extend,
-        isFunction: isFunction,
-        isUndefined: isUndefined,
-        isDefined: isDefined,
-        isString: isString,
-        isNumber: isNumber,
-        isObject: isObject,
-        toJson: toJson
-    };
+  }
 
-    if (typeof (window.Utilities) === 'undefined') {
-        window.Utilities = _utilities;
+
+  /** 
+   * equivalent to angular.isDate
+   */
+  const isDate = val => {
+    return toString.call(value) === '[object Date]';
+  }
+
+  /**
+   * equivalent to angular.isRegExp
+   */
+  const isRegExp = val => {
+    return toString.call(val) === '[object RegExp]';
+  }
+
+
+  /** 
+   * equivalent to angular.simpleCompare
+   */
+  const simpleCompare = (a, b) => {
+      return a === b || (a !== a && b !== b); 
+  }
+
+  /**
+   * Facade to angular.extend
+   * Use this with Angular objects, for vanilla JS objects, use Object.assign()
+   */
+  const extend = (dst, src) => angular.extend(dst, src);
+
+  /**
+   * Equivalent to angular.isFunction
+   */
+  const isFunction = val => typeof val === 'function';
+
+  /**
+   * Equivalent to angular.isUndefined
+   */
+  const isUndefined = val => typeof val === 'undefined';
+
+  /**
+   * Equivalent to angular.isDefined. Inverts result of const isUndefined
+   */
+  const isDefined = val => !isUndefined(val);
+
+  /**
+   * Equivalent to angular.isString
+   */
+  const isString = val => typeof val === 'string';
+
+  /**
+   * Equivalent to angular.isNumber
+   */
+  const isNumber = val => typeof val === 'number';
+
+  /**
+   * Equivalent to angular.isObject
+   */
+  const isObject = val => val !== null && typeof val === 'object';
+
+  const isWindow = obj => obj && obj.window === obj;
+
+  const isScope = obj => obj && obj.$evalAsync && obj.$watch;
+
+  const toJsonReplacer = (key, value) => {
+    var val = value;
+    if (typeof key === 'string' && key.charAt(0) === '$' && key.charAt(1) === '$') {
+      val = undefined;
+    } else if (isWindow(value)) {
+      val = '$WINDOW';
+    } else if (value && window.document === value) {
+      val = '$DOCUMENT';
+    } else if (isScope(value)) {
+      val = '$SCOPE';
     }
+    return val;
+  }
+  /**
+   * Equivalent to angular.toJson
+   */
+  const toJson = (obj, pretty) => {
+    if (isUndefined(obj)) return undefined;
+    if (!isNumber(pretty)) {
+      pretty = pretty ? 2 : null;
+    }
+    return JSON.stringify(obj, toJsonReplacer, pretty);
+  }
+
+  let _utilities = {
+    noop: noop,
+    copy: copy,
+    isDate: isDate,
+    isRegExp: isRegExp,
+    simpleCompare: simpleCompare,
+    isArray: isArray,
+    equals: equals,
+    extend: extend,
+    isFunction: isFunction,
+    isUndefined: isUndefined,
+    isDefined: isDefined,
+    isString: isString,
+    isNumber: isNumber,
+    isObject: isObject,
+    toJson: toJson
+  };
+
+  if (typeof (window.Utilities) === 'undefined') {
+    window.Utilities = _utilities;
+  }
 })(window);
