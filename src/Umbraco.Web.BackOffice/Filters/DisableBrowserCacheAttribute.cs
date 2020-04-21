1caee481ceef04048d29c9b45825aae9024f329d
Andy Butland <abutland@zonedigital.com>
2020-04-21 09:58:21 +0200

    Implemented DisableBrowserCacheAttribute in .Net core.

---
+++ src/Umbraco.Web.BackOffice/Filters/DisableBrowserCacheAttribute.cs
@@ -0,0 +1,30 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace Umbraco.Web.BackOffice.Filters
+{
+    /// <summary>
+    /// Ensures that the request is not cached by the browser
+    /// </summary>
+    public class DisableBrowserCacheAttribute : ActionFilterAttribute
+    {
+        public override async Task OnResultExecutionAsync(ResultExecutingContext context, ResultExecutionDelegate next)
+        {
+            if (context.HttpContext == null || context.HttpContext.Response == null)
+            {
+                await next();
+            }
+
+            if (context.HttpContext.Response.StatusCode != 200)
+            {
+                await next();
+            }
+
+            context.HttpContext.Response.Headers["Cache-Control"] = "no-cache, no-store, must-revalidate";
+            context.HttpContext.Response.Headers["Expires"] = "-1";
+            context.HttpContext.Response.Headers["Pragma"] = "no-cache";
+
+            await next();
+        }
+    }
+}
