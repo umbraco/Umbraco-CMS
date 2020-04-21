138b8099fc119408241b5d1ba5f6470affae2981
Elitsa Marinovska <elm@umbraco.dk>
2020-04-21 09:28:22 +0200

    Reimplementing EnsurePartialViewMacroViewContextFilterAttribute and other required classes

---
+++ src/Umbraco.Web.Common/Constants/Constants.cs
@@ -0,0 +1,12 @@
+﻿namespace Umbraco.Web.Common.Constants
+{
+    /// <summary>
+    /// constants
+    /// </summary>
+    internal static class Constants
+    {
+        internal const string ViewLocation = "~/Views";
+
+        internal const string DataTokenCurrentViewContext = "umbraco-current-view-context";
+    }
+}
