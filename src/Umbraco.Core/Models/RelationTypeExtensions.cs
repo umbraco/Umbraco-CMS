c82fe14c4d01e354cb60b6d41a6ac271149a7b25
Bjarne Fyrstenborg <bf@oerskov-gruppen.lan>
2020-04-27 15:56:17 +0200

    Add property if relation type is system relation type

---
+++ src/Umbraco.Core/Models/RelationTypeExtensions.cs
@@ -0,0 +1,9 @@
+﻿namespace Umbraco.Core.Models
+{
+    internal static class RelationTypeExtensions
+    {
+        internal static bool IsSystemRelationType(this IRelationType relationType) =>
+            relationType.Alias == Constants.Conventions.RelationTypes.RelatedDocumentAlias
+            || relationType.Alias == Constants.Conventions.RelationTypes.RelatedMediaAlias;
+    }
+}
