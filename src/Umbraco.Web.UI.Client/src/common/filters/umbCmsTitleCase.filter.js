45ba811a96885010ffcc8441c537d6f6873a5cf0
Niels Lyngsø <nsl@umbraco.dk>
2020-05-06 15:11:16 +0200

    simplified umbCmsTitleCase

--- src/Umbraco.Web.UI.Client/src/common/filters/umbCmsTitleCase.filter.js
+++ src/Umbraco.Web.UI.Client/src/common/filters/umbCmsTitleCase.filter.js
@@ -10,23 +10,10 @@
  * 
  */
 angular.module("umbraco.filters").filter('umbCmsTitleCase', function() {
-    return function (input) {
-        var smallWords = /^(a|an|and|as|at|but|by|en|for|if|in|nor|of|on|or|per|the|to|vs?\.?|via)$/i;
-
-        input = input.toLowerCase();
-        return input.replace(/[A-Za-z0-9\u00C0-\u00FF]+[^\s-]*/g, function(match, index, title) {
-            if (index > 0 && index + match.length !== title.length &&
-                match.search(smallWords) > -1 && title.charAt(index - 2) !== ":" &&
-                (title.charAt(index + match.length) !== '-' || title.charAt(index - 1) === '-') &&
-                title.charAt(index - 1).search(/[^\s-]/) < 0) {
-                return match.toLowerCase();
-            }
-
-            if (match.substr(1).search(/[A-Z]|\../) > -1) {
-                return match;
-            }
-
-            return match.charAt(0).toUpperCase() + match.substr(1);
-        });
+    return function (str) {
+        return str.replace(
+            \w\S*/g,
+            txt => txt.charAt(0).toUpperCase() + txt.substr(1).toLowerCase()
+        );
     }
 });
