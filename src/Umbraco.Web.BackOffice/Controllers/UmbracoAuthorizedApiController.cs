1f51d667e9837982a92985c46983646d2697ed14
Bjarke Berg <mail@bergmania.dk>
2020-05-18 06:43:33 +0200

    https://dev.azure.com/umbraco/D-Team%20Tracker/_workitems/edit/6586 - Migrated UmbracoWebApiRequireHttpsFilter

---
+++ src/Umbraco.Web.BackOffice/Controllers/UmbracoAuthorizedApiController.cs
@@ -0,0 +1,28 @@
+﻿using Umbraco.Web.BackOffice.Filters;
+using Umbraco.Web.Common.Attributes;
+using Umbraco.Web.Common.Controllers;
+using Umbraco.Web.Common.Filters;
+
+namespace Umbraco.Web.BackOffice.Controllers
+{
+    /// <summary>
+    /// Provides a base class for authorized auto-routed Umbraco API controllers.
+    /// </summary>
+    /// <remarks>
+    /// This controller will also append a custom header to the response if the user
+    /// is logged in using forms authentication which indicates the seconds remaining
+    /// before their timeout expires.
+    /// </remarks>
+    [IsBackOffice]
+    [UmbracoUserTimeoutFilter]
+    [UmbracoAuthorize]
+    [DisableBrowserCache]
+    [UmbracoWebApiRequireHttps]
+    [CheckIfUserTicketDataIsStale]
+    [UnhandedExceptionLoggerConfiguration]
+    [EnableDetailedErrors]
+    public abstract class UmbracoAuthorizedApiController : UmbracoApiController
+    {
+
+    }
+}
