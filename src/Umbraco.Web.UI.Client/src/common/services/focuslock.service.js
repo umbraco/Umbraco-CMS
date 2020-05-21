b56d6d0e7e786be75524dc95846d405ebbd83b22
BatJan <jaskov@gmail.com>
2020-05-22 00:52:23 +0200

    Refactor focuslock since it's not tied to the backdrop now

--- src/Umbraco.Web.UI.Client/src/common/services/focuslock.service.js
+++ src/Umbraco.Web.UI.Client/src/common/services/focuslock.service.js
@@ -10,29 +10,8 @@
 (function () {
     "use strict";
 
-    function focusLockService($timeout, eventsService) {
+    function focusLockService() {
         var elementToInert = document.querySelector('#mainwrapper');
-        var setInertAttribute = false;
-
-        eventsService.on("appState.overlay", (name, args) => {
-            if(args){
-                setInertAttribute = true;
-                return;
-            }
-        });
-
-        // TODO; at a later stage hook into these events as well and determine whether the inert should be set or not
-        // At some point it will also be neccessary to check if the attribute has already been set since the backdrop can
-        // potentially already be open in another context and then that context can trigger an infinte editor for instance, which
-        // will trigger the backdrop again
-
-        // eventsService.on("appState.editors.open", (name, args) => {
-        //     console.log('editor open:', args);
-        // });
-
-        // eventsService.on("appState.editors.close", (name, args) => {
-        //     console.log('editor close:', args);
-        // });
 
         var service = {
             
@@ -48,12 +27,7 @@
              * 
              */
             addInertAttribute: function() {
-                // If the backdrop was triggered by an overlay we add the inert attribute
-                $timeout(function(){
-                    if(setInertAttribute) {
-                        elementToInert.setAttribute('inert', true);
-                    }
-                });
+                elementToInert.setAttribute('inert', true);
             },
 
             /**
@@ -68,7 +42,6 @@
              */
             removeInertAttribute: function() {
                 elementToInert.removeAttribute('inert');
-                setInertAttribute = false;
             }
         };
 
