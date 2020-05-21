11b5a8cea551a0a1429e6b3cdb650fa86cdb382f
BatJan <jaskov@gmail.com>
2020-05-22 00:51:59 +0200

    Refactor code so variables are reset on init

--- src/Umbraco.Web.UI.Client/src/common/directives/components/forms/umbfocuslock.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/forms/umbfocuslock.directive.js
@@ -3,12 +3,9 @@
 
     function FocusLock($timeout) {
 
-        // List of elements that can be focusable within the focus lock
-        var focusableElementsSelector = 'a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input:not([disabled]), select:not([disabled])';
-        var bodyElement = document.querySelector('body');
-        var elmentWithAutoFocus = null;
-
         function getAutoFocusElement (elements) {
+            var elmentWithAutoFocus = null;
+
             elements.forEach((element) => {
                 if(element.getAttribute('umb-auto-focus') === 'true') {
                     elmentWithAutoFocus = element;
@@ -18,55 +15,58 @@
             return elmentWithAutoFocus;
         }
 
-        function link(scope, el) {
-            $timeout(function() {
-                var target = el[0];
+        function link(scope, element) {
 
-                var focusableElements = target.querySelectorAll(focusableElementsSelector);
-                var defaultFocusedElement = getAutoFocusElement(focusableElements);
-                var firstFocusableElement = focusableElements[0];
-                var lastFocusableElement = focusableElements[focusableElements.length -1];
-                var focusedElement = defaultFocusedElement !== null ? defaultFocusedElement : document.querySelector(':focus');
-
-                // TODO: figure out a clever solution when some of the focusable elements are wrapped in hidden containers...
-                console.log(focusableElements);
-                console.log(firstFocusableElement);
-                console.log(lastFocusableElement);
-
-                // We need to add the tabbing-active class in order to highlight the focused button since the default style is
-                // outline: none; set in the stylesheet specifically
-                bodyElement.classList.add('tabbing-active');
-
-                // If there is no default focused element put focus on the first focusable element in the nodelist
-                if(focusedElement === null){
-                    firstFocusableElement.focus();
-                }
-
-                target.addEventListener('keydown', function(event){
-                    var isTabPressed = (event.key === 'Tab' || event.keyCode === 9);
-        
-                    if (!isTabPressed){
-                        return;
+            function onInit() {
+                // List of elements that can be focusable within the focus lock
+                var focusableElementsSelector = 'a[href]:not([disabled]), button:not([disabled]), textarea:not([disabled]), input:not([disabled]), select:not([disabled])';
+                var bodyElement = document.querySelector('body');
+                $timeout(function() {
+                    var target = element[0];
+    
+                    var focusableElements = target.querySelectorAll(focusableElementsSelector);
+                    var defaultFocusedElement = getAutoFocusElement(focusableElements);
+                    var firstFocusableElement = focusableElements[0];
+                    var lastFocusableElement = focusableElements[focusableElements.length -1];
+                    var focusedElement = defaultFocusedElement !== null ? defaultFocusedElement : document.querySelector(':focus');
+    
+                    // We need to add the tabbing-active class in order to highlight the focused button since the default style is
+                    // outline: none; set in the stylesheet specifically
+                    bodyElement.classList.add('tabbing-active');
+    
+                    // If there is no default focused element put focus on the first focusable element in the nodelist
+                    if(focusedElement === null){
+                        firstFocusableElement.focus();
                     }
-        
-                    // If shift + tab key
-                    if(event.shiftKey){
-                        // Set focus on the last focusable element if shift+tab are pressed meaning we go backwards
-                        if(document.activeElement === firstFocusableElement){
-                            lastFocusableElement.focus();
-                            event.preventDefault();
+    
+                    target.addEventListener('keydown', function(event){
+                        var isTabPressed = (event.key === 'Tab' || event.keyCode === 9);
+            
+                        if (!isTabPressed){
+                            return;
                         }
-                    }
-                    // Else only the tab key is pressed
-                    else{
-                        // Using only the tab key we set focus on the first focusable element mening we go forward
-                        if (document.activeElement === lastFocusableElement) {
-                            firstFocusableElement.focus();
-                            event.preventDefault();
+            
+                        // If shift + tab key
+                        if(event.shiftKey){
+                            // Set focus on the last focusable element if shift+tab are pressed meaning we go backwards
+                            if(document.activeElement === firstFocusableElement){
+                                lastFocusableElement.focus();
+                                event.preventDefault();
+                            }
                         }
-                    }
-                });
-            }, false);
+                        // Else only the tab key is pressed
+                        else{
+                            // Using only the tab key we set focus on the first focusable element mening we go forward
+                            if (document.activeElement === lastFocusableElement) {
+                                firstFocusableElement.focus();
+                                event.preventDefault();
+                            }
+                        }
+                    });
+                }, false);
+            }
+
+            onInit();
         }
 
         var directive = {
