47ee78f52bcb44196d14c816623361abcf327fe9
BatJan <jaskov@gmail.com>
2020-05-23 11:53:12 +0200

    Align coding style and remove ngdoc stuff - This is meant as an internal thing

--- src/Umbraco.Web.UI.Client/src/common/services/focuslock.service.js
+++ src/Umbraco.Web.UI.Client/src/common/services/focuslock.service.js
@@ -1,49 +1,21 @@
-/**
- @ngdoc service
- * @name umbraco.services.focusLockService
- *
- * @description
- * <b>Added in Umbraco 8.7</b>. Application-wide service for locking focus within overlays or sections that are triggered by the backdrop service.
- * 
- */
-
 (function () {
     "use strict";
 
     function focusLockService() {
         var elementToInert = document.querySelector('#mainwrapper');
 
-        var service = {
-            
-            /**
-            * @ngdoc function
-            * @name umbraco.services.focusLockService#addInertAttribute
-            * @methodOf umbraco.services.focusLockService
-            * @function
-             *
-             * @description
-             * Adds the "inert" attribute, which will set <code>aria-hidden="true"</code> and <code>tabindex="-1"</code> attributes on all
-             * interactive elements within the passed container to support the "focus-lock" directive so focus can be locked within the overlay that is open
-             * 
-             */
-            addInertAttribute: function() {
-                elementToInert.setAttribute('inert', true);
-            },
+        function addInertAttribute() {
+            elementToInert.setAttribute('inert', true);
+        }
 
-            /**
-            * @ngdoc function
-            * @name umbraco.services.focusLockService#removeInertAttribute
-            * @methodOf umbraco.services.focusLockService
-            * @function
-             *
-             * @description
-             * Removes the "inert" attribute, which will also remove the  <code>aria-hidden="true"</code> and <code>tabindex="-1"</code> attributes and remove the fockus lock
-             * 
-             */
-            removeInertAttribute: function() {
-                elementToInert.removeAttribute('inert');
-            }
-        };
+        function removeInertAttribute() {
+            elementToInert.removeAttribute('inert');
+        }
+
+        var service = {
+            addInertAttribute: addInertAttribute,
+            removeInertAttribute: removeInertAttribute
+        }
 
         return service;
 
