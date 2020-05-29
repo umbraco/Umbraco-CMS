cb9f6144465031a59d27356f622305bcfbb9cc51
Niels Lyngsø <nsl@umbraco.com>
2020-05-29 10:59:33 +0200

    fixed inline views gulp watcher

--- src/Umbraco.Web.UI.Client/gulp/tasks/watchTask.js
+++ src/Umbraco.Web.UI.Client/gulp/tasks/watchTask.js
@@ -1,7 +1,7 @@
 'use strict';
 
 const config = require('../config');
-const {watch, parallel, dest, src} = require('gulp');
+const {watch, series, parallel, dest, src} = require('gulp');
 
 var _ = require('lodash');
 var MergeStream = require('merge-stream');
@@ -33,16 +33,16 @@ function watchTask(cb) {
     var viewWatcher;
     _.forEach(config.sources.views, function (group) {
         if(group.watch !== false) {
-            viewWatcher = watch(group.files, { ignoreInitial: true, interval: watchInterval });
-            viewWatcher.on('change', function(path, stats) {
+            viewWatcher = watch(group.files, { ignoreInitial: true, interval: watchInterval }, function() {
 
                 console.log("copying " + group.files + " to " + config.root + config.targets.views + group.folder);
                 
-                return MergeStream(
-                    src(group.files)
-                    .pipe( dest(config.root + config.targets.views + group.folder) )
-                    , js()
-                );
+                return parallel(
+                    function MoveViewsAndRegenerateJS() {
+                        return src(group.files).pipe( dest(config.root + config.targets.views + group.folder) );
+                    }, 
+                    js
+                )();
 
             });
         }
