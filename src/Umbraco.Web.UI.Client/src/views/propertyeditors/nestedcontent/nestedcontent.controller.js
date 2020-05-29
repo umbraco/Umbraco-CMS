1fe7de0b5f78d3d2d7843a02f520fe65b21a2952
Warren Buckley <warren@umbraco.com>
2020-05-29 15:18:46 +0000

    Merge pull request #7166 from umbraco/v8/feature/7133-do-not-paste-keys-in-nested-content
    Cherry picked from SHA 49c438b55f002b02e4555bb2622c73bf1ca51239
    
    NC keys: Do not copy keys for Nested Content (Fix for #7133)
    # Conflicts:
    #	src/Umbraco.Web.UI.Client/src/views/propertyeditors/nestedcontent/nestedcontent.controller.js

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/nestedcontent/nestedcontent.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/nestedcontent/nestedcontent.controller.js
@@ -1,6 +1,58 @@
 ﻿(function () {
     'use strict';
 
+    /**
+     * When performing a copy, we do copy the ElementType Data Model, but each inner Nested Content property is still stored as the Nested Content Model, aka. each property is just storing its value. To handle this we need to ensure we handle both scenarios.
+     */
+
+
+    angular.module('umbraco').run(['clipboardService', function (clipboardService) {
+
+        function clearNestedContentPropertiesForStorage(prop, propClearingMethod) {
+
+            // if prop.editor is "Umbraco.NestedContent"
+            if ((typeof prop === 'object' && prop.editor === "Umbraco.NestedContent")) {
+
+                var value = prop.value;
+                for (var i = 0; i < value.length; i++) {
+                    var obj = value[i];
+
+                    // remove the key
+                    delete obj.key;
+
+                    // Loop through all inner properties:
+                    for (var k in obj) {
+                        propClearingMethod(obj[k]);
+                    }
+                }
+            }
+        }
+        
+        clipboardService.registrerClearPropertyResolver(clearNestedContentPropertiesForStorage)
+
+
+        function clearInnerNestedContentPropertiesForStorage(prop, propClearingMethod) {
+
+            // if we got an array, and it has a entry with ncContentTypeAlias this meants that we are dealing with a NestedContent property inside a NestedContent property.
+            if ((Array.isArray(prop) && prop.length > 0 && prop[0].ncContentTypeAlias !== undefined)) {
+
+                for (var i = 0; i < prop.length; i++) {
+                    var obj = prop[i];
+
+                    // remove the key
+                    delete obj.key;
+
+                    // Loop through all inner properties:
+                    for (var k in obj) {
+                        propClearingMethod(obj[k]);
+                    }
+                }
+            }
+        }
+        
+        clipboardService.registrerClearPropertyResolver(clearInnerNestedContentPropertiesForStorage)
+    }]);
+
     angular
         .module('umbraco')
         .component('nestedContentPropertyEditor', {
@@ -13,7 +65,7 @@
             }
         });
 
-    function NestedContentController($scope, $interpolate, $filter, $timeout, contentResource, localizationService, iconHelper, clipboardService, eventsService, overlayService, $routeParams, editorState) {
+    function NestedContentController($scope, $interpolate, $filter, $timeout, contentResource, localizationService, iconHelper, clipboardService, eventsService, overlayService) {
         
         var vm = this;
         var model = $scope.$parent.$parent.model;
@@ -76,7 +128,7 @@
             }
 
             localizationService.localize("clipboard_labelForArrayOfItemsFrom", [model.label, nodeName]).then(function(data) {
-                clipboardService.copyArray("elementTypeArray", aliases, vm.nodes, data, "icon-thumbnail-list", model.id);
+                clipboardService.copyArray("elementTypeArray", aliases, vm.nodes, data, "icon-thumbnail-list", model.id, clearNodeForCopy);
             });
         }
 
@@ -208,7 +260,8 @@
                 });
             });
 
-            vm.overlayMenu.title = vm.overlayMenu.pasteItems.length > 0 ? labels.grid_addElement : labels.content_createEmpty;
+            vm.overlayMenu.title = labels.grid_addElement;
+            vm.overlayMenu.hideHeader = vm.overlayMenu.pasteItems.length > 0;
 
             vm.overlayMenu.clickClearPaste = function ($event) {
                 $event.stopPropagation();
@@ -216,6 +269,7 @@
                 clipboardService.clearEntriesOfType("elementType", contentTypeAliases);
                 clipboardService.clearEntriesOfType("elementTypeArray", contentTypeAliases);
                 vm.overlayMenu.pasteItems = [];// This dialog is not connected via the clipboardService events, so we need to update manually.
+                vm.overlayMenu.hideHeader = false;
             };
 
             if (vm.overlayMenu.availableItems.length === 1 && vm.overlayMenu.pasteItems.length === 0) {
@@ -383,6 +437,11 @@
             });
         }
 
+        function clearNodeForCopy(clonedData) {
+            delete clonedData.key;
+            delete clonedData.$$hashKey;
+        }
+
         vm.showCopy = clipboardService.isSupported();
         vm.showPaste = false;
 
@@ -390,7 +449,7 @@
 
             syncCurrentNode();
 
-            clipboardService.copy("elementType", node.contentTypeAlias, node);
+            clipboardService.copy("elementType", node.contentTypeAlias, node, null, null, null, clearNodeForCopy);
             $event.stopPropagation();
         }
         
@@ -488,10 +547,12 @@
                 }
 
                 // Enforce min items if we only have one scaffold type
+                var modelWasChanged = false;
                 if (vm.nodes.length < vm.minItems && vm.scaffolds.length === 1) {
                     for (var i = vm.nodes.length; i < model.config.minItems; i++) {
                         addNode(vm.scaffolds[0].contentTypeAlias);
                     }
+                    modelWasChanged = true;
                 }
 
                 // If there is only one item, set it as current node
@@ -503,6 +564,9 @@
 
                 vm.inited = true;
 
+                if (modelWasChanged) {
+                    updateModel();
+                }
                 updatePropertyActionStates();
                 checkAbilityToPasteContent();
             }
@@ -585,8 +649,8 @@
         }
 
         function updatePropertyActionStates() {
-            copyAllEntriesAction.isDisabled = !model.value || model.value.length === 0;
-            removeAllEntriesAction.isDisabled = !model.value || model.value.length === 0;
+            copyAllEntriesAction.isDisabled = !model.value || !model.value.length;
+            removeAllEntriesAction.isDisabled = copyAllEntriesAction.isDisabled;
         }
 
 
