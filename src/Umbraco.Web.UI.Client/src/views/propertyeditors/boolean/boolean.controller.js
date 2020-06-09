eb50209f2cd7a81a33c80697ebfc03c720bbb028
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2020-06-09 10:51:59 +0200

    Update comments

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/boolean/boolean.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/boolean/boolean.controller.js
@@ -1,15 +1,19 @@
 function booleanEditorController($scope, angularHelper) {
 
-    // setup the default config
+    // Setup the default config
+    // This allow to overwrite the configuration when property editor is re-used 
+    // in e.g. third party packages, dashboard or content app. For example when using umb-property-editor.
+    // At the moment this use "1/0" as default for "truevalue" and "falsevalue", but allow "True/False" as well.
+    // Maybe sometime later we can make it support "Yes/No" or "On/Off" as well similar to ng-true-value and ng-false-value in Angular.
     var config = {
         truevalue: "1",
         falsevalue: "0"
     };
 
-    // map the user config
+    // Map the user config
     angular.extend(config, $scope.model.config);
 
-    // map back to the model
+    // Map back to the model
     $scope.model.config = config;
 
     function setupViewModel() {
@@ -33,8 +37,8 @@ function booleanEditorController($scope, angularHelper) {
         $scope.model.value = ($scope.renderModel.value === true) ? $scope.model.config.truevalue : $scope.model.config.falsevalue;
     }
 
-    //here we declare a special method which will be called whenever the value has changed from the server
-    //this is instead of doing a watch on the model.value = faster
+    // Here we declare a special method which will be called whenever the value has changed from the server
+    // this is instead of doing a watch on the model.value = faster
     $scope.model.onValueChanged = function (newVal, oldVal) {
         //update the display val again if it has changed from the server
         setupViewModel();
@@ -43,7 +47,7 @@ function booleanEditorController($scope, angularHelper) {
     // Update the value when the toggle is clicked
     $scope.toggle = function(){
         angularHelper.getCurrentForm($scope).$setDirty();
-        if($scope.renderModel.value){
+        if ($scope.renderModel.value){
             $scope.model.value = $scope.model.config.falsevalue;
             setupViewModel();
             return;
