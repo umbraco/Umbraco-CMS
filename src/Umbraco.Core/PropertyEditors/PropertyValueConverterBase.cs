caecedba2f67205e173e1c3ef59f3101df79fc4d
Ronald Barendse <ronald@panoramastudios.nl>
2020-06-09 15:13:29 +0200

    Add generic PropertyValueConverterBase classes

--- src/Umbraco.Core/PropertyEditors/PropertyValueConverterBase.cs
+++ src/Umbraco.Core/PropertyEditors/PropertyValueConverterBase.cs
@@ -45,4 +45,61 @@ namespace Umbraco.Core.PropertyEditors
         public virtual object ConvertIntermediateToXPath(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object inter, bool preview)
             => inter?.ToString() ?? string.Empty;
     }
+
+    public abstract class PropertyValueConverterBase<TIntermediate, TObject> : PropertyValueConverterBase
+    {
+        public sealed override bool? IsValue(object value, PropertyValueLevel level)
+        {
+            switch (level)
+            {
+                case PropertyValueLevel.Source:
+                    return IsSourceValue(value);
+                case PropertyValueLevel.Inter:
+                    return IsIntermediateValue((TIntermediate)value);
+                case PropertyValueLevel.Object:
+                    return IsObjectValue((TObject)value);
+                default:
+                    return base.IsValue(value, level);
+            }
+        }
+
+        protected virtual bool? IsSourceValue(object value)
+            => base.IsValue(value, PropertyValueLevel.Source);
+
+        protected virtual bool? IsIntermediateValue(TIntermediate value)
+            => base.IsValue(value, PropertyValueLevel.Inter);
+
+        protected virtual bool? IsObjectValue(TObject value)
+            => base.IsValue(value, PropertyValueLevel.Object);
+
+        public override Type GetPropertyValueType(IPublishedPropertyType propertyType)
+            => typeof(TObject);
+
+        public sealed override object ConvertSourceToIntermediate(IPublishedElement owner, IPublishedPropertyType propertyType, object source, bool preview)
+            => this.ConvertSourceToIntermediate<object>(owner, propertyType, source, preview);
+
+        protected abstract TIntermediate ConvertSourceToIntermediate<T>(IPublishedElement owner, IPublishedPropertyType propertyType, object source, bool preview);
+
+        public sealed override object ConvertIntermediateToObject(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object inter, bool preview)
+            => this.ConvertIntermediateToObject<TIntermediate>(owner, propertyType, referenceCacheLevel, (TIntermediate)inter, preview);
+
+        protected abstract TObject ConvertIntermediateToObject<T>(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, TIntermediate inter, bool preview)
+            where T : TIntermediate;
+
+        public sealed override object ConvertIntermediateToXPath(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, object inter, bool preview)
+            => this.ConvertIntermediateToXPath<TIntermediate>(owner, propertyType, referenceCacheLevel, (TIntermediate)inter, preview);
+
+        protected virtual object ConvertIntermediateToXPath<T>(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, TIntermediate inter, bool preview)
+            where T : TIntermediate
+            => base.ConvertIntermediateToXPath(owner, propertyType, referenceCacheLevel, inter, preview);
+    }
+
+    public abstract class PropertyValueConverterBase<TValue> : PropertyValueConverterBase<TValue, TValue>
+    {
+        protected override TValue ConvertIntermediateToObject<T>(IPublishedElement owner, IPublishedPropertyType propertyType, PropertyCacheLevel referenceCacheLevel, TValue inter, bool preview)
+            => inter;
+
+        protected override bool? IsObjectValue(TValue value)
+            => IsIntermediateValue(value);
+    }
 }
