98e81614d0057d466d9b9ba1521849b118e9e269
Bjarke Berg <mail@bergmania.dk>
2020-06-26 08:21:17 +0200

    Changed check from null only to IsNullOrEmpty to make it safer
    Signed-off-by: Bjarke Berg <mail@bergmania.dk>

--- src/Umbraco.Infrastructure/Media/UploadAutoFillProperties.cs
+++ src/Umbraco.Infrastructure/Media/UploadAutoFillProperties.cs
@@ -112,16 +112,16 @@ namespace Umbraco.Web.Media
             if (content == null) throw new ArgumentNullException(nameof(content));
             if (autoFillConfig == null) throw new ArgumentNullException(nameof(autoFillConfig));
 
-            if (!(autoFillConfig.WidthFieldAlias is null) && content.Properties.Contains(autoFillConfig.WidthFieldAlias))
+            if (!string.IsNullOrEmpty(autoFillConfig.WidthFieldAlias) && content.Properties.Contains(autoFillConfig.WidthFieldAlias))
                 content.Properties[autoFillConfig.WidthFieldAlias].SetValue(size.HasValue ? size.Value.Width.ToInvariantString() : string.Empty, culture, segment);
 
-            if (!(autoFillConfig.HeightFieldAlias is null) && content.Properties.Contains(autoFillConfig.HeightFieldAlias))
+            if (!string.IsNullOrEmpty(autoFillConfig.HeightFieldAlias) && content.Properties.Contains(autoFillConfig.HeightFieldAlias))
                 content.Properties[autoFillConfig.HeightFieldAlias].SetValue(size.HasValue ? size.Value.Height.ToInvariantString() : string.Empty, culture, segment);
 
-            if (!(autoFillConfig.LengthFieldAlias is null) && content.Properties.Contains(autoFillConfig.LengthFieldAlias))
+            if (!string.IsNullOrEmpty(autoFillConfig.LengthFieldAlias) && content.Properties.Contains(autoFillConfig.LengthFieldAlias))
                 content.Properties[autoFillConfig.LengthFieldAlias].SetValue(length, culture, segment);
 
-            if (!(autoFillConfig.ExtensionFieldAlias is null) && content.Properties.Contains(autoFillConfig.ExtensionFieldAlias))
+            if (!string.IsNullOrEmpty(autoFillConfig.ExtensionFieldAlias) && content.Properties.Contains(autoFillConfig.ExtensionFieldAlias))
                 content.Properties[autoFillConfig.ExtensionFieldAlias].SetValue(extension, culture, segment);
         }
 
