3522843a0d8079efecf7745d852252ffcfca1faf
Bjarke Berg <mail@bergmania.dk>
2020-06-28 14:31:21 +0200

    Fix acceptance tests
    Signed-off-by: Bjarke Berg <mail@bergmania.dk>

--- src/Umbraco.Tests.AcceptanceTest/cypress/integration/Settings/documentTypes.ts
+++ src/Umbraco.Tests.AcceptanceTest/cypress/integration/Settings/documentTypes.ts
@@ -34,7 +34,7 @@ context('Document Types', () => {
     cy.get('.umb-search-field').type('Textstring');
 
     // Choose first item
-    cy.get('ul.umb-card-grid li a[title="Textstring"]').closest("li").click();
+    cy.get('ul.umb-card-grid [title="Textstring"]').closest("li").click();
 
     // Save property
     cy.get('.btn-success').last().click();
