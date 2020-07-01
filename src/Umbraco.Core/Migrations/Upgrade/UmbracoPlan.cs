a8a1bb2a5515e544f9b5d688dff82066c829c154
Shannon <sdeminick@gmail.com>
2020-07-01 15:01:55 +1000

    manually merge in benj PR

--- src/Umbraco.Core/Migrations/Upgrade/UmbracoPlan.cs
+++ src/Umbraco.Core/Migrations/Upgrade/UmbracoPlan.cs
@@ -7,7 +7,6 @@ using Umbraco.Core.Migrations.Upgrade.V_8_0_0;
 using Umbraco.Core.Migrations.Upgrade.V_8_0_1;
 using Umbraco.Core.Migrations.Upgrade.V_8_1_0;
 using Umbraco.Core.Migrations.Upgrade.V_8_6_0;
-using Umbraco.Core.Migrations.Upgrade.V_8_7_0;
 
 namespace Umbraco.Core.Migrations.Upgrade
 {
@@ -194,10 +193,7 @@ namespace Umbraco.Core.Migrations.Upgrade
 
             // to 8.7.0...
             To<MissingDictionaryIndex>("{a78e3369-8ea3-40ec-ad3f-5f76929d2b20}");
-
-            // to 8.7.0...
-            To<StackedContentToBlockList>("{DFA35FA2-BFBB-433F-84E5-BD75940CDDF6}");
-            To<ConvertToElements>("{DA434576-3DEF-46D7-942A-CE34D7F7FB8A}");
+            
             //FINAL
         }
     }
