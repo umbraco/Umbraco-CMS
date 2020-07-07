872731f72685a8284940117b29be2b4a24cd1c81
Ronald Barendse <ronald@barend.se>
2020-07-07 23:30:34 +0200

    Update HasValue obsolete message

--- src/Umbraco.Core/PropertyEditors/PropertyValueConverterBase.cs
+++ src/Umbraco.Core/PropertyEditors/PropertyValueConverterBase.cs
@@ -31,7 +31,7 @@ namespace Umbraco.Core.PropertyEditors
             }
         }
 
-        [Obsolete("This method is not part of the IPropertyValueConverter contract and therefore not used, use IsValue instead.")]
+        [Obsolete("This method is not part of the IPropertyValueConverter contract, therefore not used and will be removed in future versions; use IsValue instead.")]
         public virtual bool HasValue(IPublishedProperty property, string culture, string segment)
         {
             var value = property.GetSourceValue(culture, segment);
