204d6e126cee7e51300bc52c2e19c1a2ace0aa8f
Marc Goodson <marc@snowmoon.co.uk>
2020-07-11 12:01:56 +0100

    Update 'default value' title + description of CheckBox Configuration Editor
    Change 'Default Value' To 'Initial State' to try to avoid misconceptions that this option is some kind of 'fall back default value' in code.

--- src/Umbraco.Web/PropertyEditors/TrueFalseConfiguration.cs
+++ src/Umbraco.Web/PropertyEditors/TrueFalseConfiguration.cs
@@ -7,7 +7,7 @@ namespace Umbraco.Web.PropertyEditors
     /// </summary>
     public class TrueFalseConfiguration
     {
-        [ConfigurationField("default", "Default Value", "boolean")]
+        [ConfigurationField("default","Initial State", "boolean",Description = "The initial state for the checkbox, when this is displayed in the backoffice for the first time. NB changing this initial value does not alter the value on existing published content items.")]
         public string Default { get; set; } // TODO: well, true or false?!
 
         [ConfigurationField("labelOn", "Write a label text", "textstring")]
