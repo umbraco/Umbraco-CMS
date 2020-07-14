690d9f8f2c8bf45493b5d9ab7cd1d46f9315688c
Sebastiaan Janssen <sebastiaan@umbraco.com>
2020-07-14 17:15:42 +0200

    Update src/Umbraco.Web.UI.Client/src/views/member/apps/content/content.controller.js

--- src/Umbraco.Web.UI.Client/src/views/member/apps/content/content.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/member/apps/content/content.controller.js
@@ -11,7 +11,7 @@
                 return false;
             }
             // hide all label properties with the alias prefix "umbracoMember" (e.g. "umbracoMemberFailedPasswordAttempts")
-            return property.view !== "readonlyvalue" || property.alias.indexOf("umbracoMember") !== 0;
+            return property.view !== "readonlyvalue" || property.alias.startsWith('umbracoMember') === false;
         }
     }
 
