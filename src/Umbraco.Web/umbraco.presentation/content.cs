0ce34b1b41bf82e04e2e8b0e1934821304f2bbc2
Arkadiusz Biel <abi@novicell.co.uk>
2020-07-22 13:43:23 +0100

    Revert previous changes, force writer in update of cache

--- src/Umbraco.Web/umbraco.presentation/content.cs
+++ src/Umbraco.Web/umbraco.presentation/content.cs
@@ -306,6 +306,10 @@ namespace umbraco
             // note that some threads could read from it while we hold the lock, though
             using (var safeXml = GetSafeXmlWriter())
             {
+                if (!safeXml.IsWriter)
+                {
+                    safeXml.UpgradeToWriter();
+                }
                 safeXml.Xml = PublishNodeDo(d, safeXml.Xml, true);
                 safeXml.AcceptChanges();
             }
