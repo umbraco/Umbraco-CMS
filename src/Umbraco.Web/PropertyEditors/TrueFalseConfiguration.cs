8af0a9951930d3746084e6ef6be26b94d1d00121
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2020-07-25 18:06:55 +0200

    Add descriptions

--- src/Umbraco.Web/PropertyEditors/TrueFalseConfiguration.cs
+++ src/Umbraco.Web/PropertyEditors/TrueFalseConfiguration.cs
@@ -10,10 +10,10 @@ namespace Umbraco.Web.PropertyEditors
         [ConfigurationField("default", "Initial State", "boolean", Description = "The initial state for the toggle, when it is displayed for the first time in the backoffice, eg. for a new content item.")]
         public bool Default { get; set; }
 
-        [ConfigurationField("labelOn", "Label text when enabled", "textstring")]
+        [ConfigurationField("labelOn", "Label On", "textstring", Description = "Label text when checked.")]
         public string LabelOn { get; set; }
 
-        [ConfigurationField("labelOff", "Label text when disabled", "textstring")]
+        [ConfigurationField("labelOff", "Label Off", "textstring", Description = "Label text unchecked.")]
         public string LabelOff { get; set; }
     }
 }
