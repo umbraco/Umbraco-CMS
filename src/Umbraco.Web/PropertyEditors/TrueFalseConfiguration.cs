6759d2737f57122a93f215d422c4be929f4d3787
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2020-07-25 17:36:37 +0200

    Add label on and label off configuration prevalues

--- src/Umbraco.Web/PropertyEditors/TrueFalseConfiguration.cs
+++ src/Umbraco.Web/PropertyEditors/TrueFalseConfiguration.cs
@@ -10,7 +10,10 @@ namespace Umbraco.Web.PropertyEditors
         [ConfigurationField("default","Initial State", "boolean",Description = "The initial state for this checkbox, when it is displayed for the first time in the backoffice, eg. for a new content item.")]
         public string Default { get; set; } // TODO: well, true or false?!
 
-        [ConfigurationField("labelOn", "Write a label text", "textstring")]
-        public string Label { get; set; }
+        [ConfigurationField("labelOn", "Label text when enabled", "textstring")]
+        public string LabelOn { get; set; }
+
+        [ConfigurationField("labelOff", "Label text when disabled", "textstring")]
+        public string LabelOff { get; set; }
     }
 }
