15d4cbb2a67d75479ff35f59cc492d8071a35a2a
rickliveshere <rick.read@corp.sigmauk>
2020-07-27 22:31:17 +0100

    Removes checking parent scope from directive, reads whether number should be enforced as only digits based on boolean parameter

---
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/forms/onlydigits.directive.js
@@ -0,0 +1,31 @@
+﻿
+/**
+* @ngdoc directive
+* @name umbraco.directives.directive:onlyDigits
+* @restrict A
+* @description Used in conjunction with type='number' input fields to check if a field should accept digits only. 
+**/
+function onlyDigits($parse) {
+    return {
+        restrict: "A",
+        require: "ngModel",
+        link: function (scope, elem, attrs, ctrl) {
+
+            var allowOnlyDigits = scope.$eval(attrs.onlyDigits);
+
+            if (allowOnlyDigits) {
+                ctrl.$parsers.push(function (input) {
+                    if (input === undefined || input === null) return '';
+                    var inputAsStr = input.toString();
+                    var transformedInput = inputAsStr.replace(/\D/g, '');
+                    if (transformedInput !== inputAsStr) {
+                        ctrl.$setViewValue(transformedInput);
+                        ctrl.$render();
+                    }
+                    return transformedInput;
+                });
+            }
+        }
+    };
+}
+angular.module('umbraco.directives').directive("onlyDigits", onlyDigits);
