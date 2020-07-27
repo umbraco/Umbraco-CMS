eea55c6c2909fb3bf8d15c09d6a10226d0c4ce2f
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2020-07-27 19:00:58 +0200

    Remove unused configuration

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/contentpicker/contentpicker.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/contentpicker/contentpicker.controller.js
@@ -90,8 +90,6 @@ function contentPickerController($scope, $q, $routeParams, $location, entityReso
     var defaultConfig = {
         multiPicker: false,
         showOpenButton: false,
-        showEditButton: false,
-        showPathOnHover: false,
         dataTypeKey: null,
         maxNumber: 1,
         minNumber: 0,
@@ -144,7 +142,6 @@ function contentPickerController($scope, $q, $routeParams, $location, entityReso
             : "Document";
     
     $scope.allowOpenButton = false;
-    $scope.allowEditButton = entityType === "Document";
     $scope.allowRemoveButton = true;
 
     //the dialog options for the picker
