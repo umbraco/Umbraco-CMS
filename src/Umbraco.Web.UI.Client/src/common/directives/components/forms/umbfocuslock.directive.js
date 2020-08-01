bdfc3e317e132b7b528f86625befade4158536b5
BatJan <1932158+BatJan@users.noreply.github.com>
2020-08-01 16:16:15 +0200

    Refactoring code to make things a bit more clean

--- src/Umbraco.Web.UI.Client/src/common/directives/components/forms/umbfocuslock.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/forms/umbfocuslock.directive.js
@@ -17,19 +17,53 @@
 
         function link(scope, element) {
             var target = element[0];
+            var focusableElements;
+            var firstFocusableElement;
+            var lastFocusableElement;
             
             // List of elements that can be focusable within the focus lock
             var focusableElementsSelector = 'a[href]:not([disabled]):not(.ng-hide), button:not([disabled]):not(.ng-hide), textarea:not([disabled]):not(.ng-hide), input:not([disabled]):not(.ng-hide), select:not([disabled]):not(.ng-hide)';
             var bodyElement = document.querySelector('body');
+
+            function getFocusableElements() {
+                focusableElements = target.querySelectorAll(focusableElementsSelector);
+                firstFocusableElement = focusableElements[0];
+                lastFocusableElement = focusableElements[focusableElements.length -1];
+                
+                return focusableElements;
+            }
+
+            function handleKeydown() {
+                var isTabPressed = (event.key === 'Tab' || event.keyCode === 9);
+                
+                if (!isTabPressed){
+                    return;
+                }
+    
+                // If shift + tab key
+                if(event.shiftKey){
+                    // Set focus on the last focusable element if shift+tab are pressed meaning we go backwards
+                    if(document.activeElement === firstFocusableElement){
+                        lastFocusableElement.focus();
+                        event.preventDefault();
+                    }
+                }
+                // Else only the tab key is pressed
+                else{
+                    // Using only the tab key we set focus on the first focusable element mening we go forward
+                    if (document.activeElement === lastFocusableElement) {
+                        firstFocusableElement.focus();
+                        event.preventDefault();
+                    }
+                }
+            }
             
             function onInit() {
                 $timeout(function() {
-                    var focusableElements = target.querySelectorAll(focusableElementsSelector);
+                    getFocusableElements();
 
                     if(focusableElements.length > 0) {
                         var defaultFocusedElement = getAutoFocusElement(focusableElements);
-                        var firstFocusableElement = focusableElements[0];
-                        var lastFocusableElement = focusableElements[focusableElements.length -1];
     
                         // We need to add the tabbing-active class in order to highlight the focused button since the default style is
                         // outline: none; set in the stylesheet specifically
@@ -40,30 +74,8 @@
                             firstFocusableElement.focus();
                         }
         
-                        target.addEventListener('keydown', function(event){
-                            var isTabPressed = (event.key === 'Tab' || event.keyCode === 9);
-                
-                            if (!isTabPressed){
-                                return;
-                            }
-                
-                            // If shift + tab key
-                            if(event.shiftKey){
-                                // Set focus on the last focusable element if shift+tab are pressed meaning we go backwards
-                                if(document.activeElement === firstFocusableElement){
-                                    lastFocusableElement.focus();
-                                    event.preventDefault();
-                                }
-                            }
-                            // Else only the tab key is pressed
-                            else{
-                                // Using only the tab key we set focus on the first focusable element mening we go forward
-                                if (document.activeElement === lastFocusableElement) {
-                                    firstFocusableElement.focus();
-                                    event.preventDefault();
-                                }
-                            }
-                        });
+                        //  Handle keydown
+                        target.addEventListener('keydown', handleKeydown);
                     }
 
                 }, 500);
