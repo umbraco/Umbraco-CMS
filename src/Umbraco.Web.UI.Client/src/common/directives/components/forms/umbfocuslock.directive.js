1749cf82a3ad6e27eb43b9c11a3d2d9c5db275fe
BatJan <1932158+BatJan@users.noreply.github.com>
2020-08-01 14:14:19 +0200

    Don't try to add focus to something that does not exist

--- src/Umbraco.Web.UI.Client/src/common/directives/components/forms/umbfocuslock.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/forms/umbfocuslock.directive.js
@@ -16,7 +16,6 @@
         }
 
         function link(scope, element) {
-
             function onInit() {
                 // List of elements that can be focusable within the focus lock
                 var focusableElementsSelector = 'a[href]:not([disabled]):not(.ng-hide), button:not([disabled]):not(.ng-hide), textarea:not([disabled]):not(.ng-hide), input:not([disabled]):not(.ng-hide), select:not([disabled]):not(.ng-hide)';
@@ -26,43 +25,47 @@
                     var target = element[0];
     
                     var focusableElements = target.querySelectorAll(focusableElementsSelector);
-                    var defaultFocusedElement = getAutoFocusElement(focusableElements);
-                    var firstFocusableElement = focusableElements[0];
-                    var lastFocusableElement = focusableElements[focusableElements.length -1];
 
-                    // We need to add the tabbing-active class in order to highlight the focused button since the default style is
-                    // outline: none; set in the stylesheet specifically
-                    bodyElement.classList.add('tabbing-active');
+                    if(focusableElements.length > 0) {
+                        var defaultFocusedElement = getAutoFocusElement(focusableElements);
+                        var firstFocusableElement = focusableElements[0];
+                        var lastFocusableElement = focusableElements[focusableElements.length -1];
     
-                    // If there is no default focused element put focus on the first focusable element in the nodelist
-                    if(defaultFocusedElement === null ){
-                        firstFocusableElement.focus();
+                        // We need to add the tabbing-active class in order to highlight the focused button since the default style is
+                        // outline: none; set in the stylesheet specifically
+                        bodyElement.classList.add('tabbing-active');
+        
+                        // If there is no default focused element put focus on the first focusable element in the nodelist
+                        if(defaultFocusedElement === null ){
+                            firstFocusableElement.focus();
+                        }
+        
+                        target.addEventListener('keydown', function(event){
+                            var isTabPressed = (event.key === 'Tab' || event.keyCode === 9);
+                
+                            if (!isTabPressed){
+                                return;
+                            }
+                
+                            // If shift + tab key
+                            if(event.shiftKey){
+                                // Set focus on the last focusable element if shift+tab are pressed meaning we go backwards
+                                if(document.activeElement === firstFocusableElement){
+                                    lastFocusableElement.focus();
+                                    event.preventDefault();
+                                }
+                            }
+                            // Else only the tab key is pressed
+                            else{
+                                // Using only the tab key we set focus on the first focusable element mening we go forward
+                                if (document.activeElement === lastFocusableElement) {
+                                    firstFocusableElement.focus();
+                                    event.preventDefault();
+                                }
+                            }
+                        });
                     }
-    
-                    target.addEventListener('keydown', function(event){
-                        var isTabPressed = (event.key === 'Tab' || event.keyCode === 9);
-            
-                        if (!isTabPressed){
-                            return;
-                        }
-            
-                        // If shift + tab key
-                        if(event.shiftKey){
-                            // Set focus on the last focusable element if shift+tab are pressed meaning we go backwards
-                            if(document.activeElement === firstFocusableElement){
-                                lastFocusableElement.focus();
-                                event.preventDefault();
-                            }
-                        }
-                        // Else only the tab key is pressed
-                        else{
-                            // Using only the tab key we set focus on the first focusable element mening we go forward
-                            if (document.activeElement === lastFocusableElement) {
-                                firstFocusableElement.focus();
-                                event.preventDefault();
-                            }
-                        }
-                    });
+
                 }, 500);
             }
 
