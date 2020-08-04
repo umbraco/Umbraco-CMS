82b3416d429cb2003ed06b6d5187ccf16f2ecc54
Nikolaj <nel@umbraco.dk>
2020-08-04 11:20:06 +0200

    Test partial view macro from snippet

--- src/Umbraco.Tests.AcceptanceTest/cypress/integration/Settings/partialsViewMacroFiles.ts
+++ src/Umbraco.Tests.AcceptanceTest/cypress/integration/Settings/partialsViewMacroFiles.ts
@@ -63,4 +63,36 @@ context('Partial View Macro Files', () => {
     cy.umbracoEnsureMacroNameNotExists(name);
   });
 
+  it('Create new partial view macro from snippet', () => {
+      const name = "TestPartialFromSnippet";
+      const fileName = name + ".cshtml";
+
+      cy.umbracoEnsureMacroNameNotExists(fileName);
+      cy.umbracoEnsureMacroNameNotExists(fileName);
+
+      cy.umbracoSection('settings');
+      cy.get('li .umb-tree-root:contains("Settings")').should("be.visible");
+
+      cy.umbracoTreeItem("settings", ["Partial View Macro Files"]).rightclick();
+
+      cy.umbracoContextMenuAction("action-create").click();
+      cy.get('.menu-label').eq(2).click();
+
+      // Select snippet
+      cy.get('.menu-label').eq(1).click();
+
+      // Type name
+      cy.umbracoEditorHeaderName(name);
+
+      // Save
+      cy.get('.btn-success').click();
+
+      // Assert
+      cy.umbracoSuccessNotification().should('be.visible');
+
+      // Clean
+      cy.umbracoEnsurePartialViewMacroFileNameNotExists(fileName);
+      cy.umbracoEnsureMacroNameNotExists(name);
+  });
+
 });
