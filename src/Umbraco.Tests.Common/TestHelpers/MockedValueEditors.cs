5e78bbd2f314384b75445bd40b2682007b94b639
Nikolaj <nel@umbraco.dk>
2020-08-12 11:35:12 +0200

    Move CoreThings

---
+++ src/Umbraco.Tests.Common/TestHelpers/MockedValueEditors.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Moq;
+using Umbraco.Core.PropertyEditors;
+using Umbraco.Core.Services;
+using Umbraco.Core.Strings;
+
+namespace Umbraco.Tests.Common.TestHelpers
+{
+    public static class MockedValueEditors
+    {
+        public static DataValueEditor CreateDataValueEditor(string name)
+        {
+            var valueType = (ValueTypes.IsValue(name)) ? name : ValueTypes.String;
+
+            return new DataValueEditor(
+                Mock.Of<IDataTypeService>(),
+                Mock.Of<ILocalizationService>(),
+                Mock.Of<ILocalizedTextService>(),
+                Mock.Of<IShortStringHelper>(),
+                new DataEditorAttribute(name, name, name)
+                {
+                    ValueType = valueType
+                }
+
+            );
+        }
+    }
+}
