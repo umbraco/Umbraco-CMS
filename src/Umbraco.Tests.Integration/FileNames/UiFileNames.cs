16db36eecd10cb339ba469b5f6086b3848b5e404
Mole <nikolajlauridsen@protonmail.ch>
2020-08-21 13:53:27 +0200

    Start working on tests

---
+++ src/Umbraco.Tests.Integration/FileNames/UiFileNames.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Text;
+using NUnit.Framework;
+
+namespace Umbraco.Tests.Integration.FileNames
+{
+    [TestFixture]
+    public class UiFileNames
+    {
+        [Test]
+        public void MacroTemplates()
+        {
+            var files = Directory.GetFiles(@"..\\..\\..\\..\\Umbraco.Web.UI.NetCore\\umbraco\\PartialViewMacros\\Templates");
+            foreach(var file in files)
+            {
+                var fileName = file.Split("\\").Last();
+                Assert.AreEqual(char.ToUpper(fileName[0]), fileName[0], $"{fileName} does not start with an uppercase letter.");
+                var titleCase = CultureInfo.InvariantCulture.TextInfo.ToTitleCase(fileName.ToLower());
+            }
+        }
+
+        [Test]
+        public void LanguageFilesAreLowercase()
+        {
+            var files = Directory.GetFiles(@"..\\..\\..\\..\\Umbraco.Web.UI.NetCore\\umbraco\\config\\lang");
+            foreach(var file in files)
+            {
+                var fileName = file.Split("\\").Last();
+                Assert.AreEqual(fileName.ToLower(), fileName);
+            }
+
+        }
+    }
+}
