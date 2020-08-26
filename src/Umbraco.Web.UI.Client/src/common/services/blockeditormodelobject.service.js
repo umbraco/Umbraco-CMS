fa07cf0ac9b810b68eed1fa76cd77e90d19abc8b
Niels Lyngsø <niels.lyngso@gmail.com>
2020-08-26 16:30:48 +0200

    use ClipboardService on Paste

--- src/Umbraco.Web.UI.Client/src/common/services/blockeditormodelobject.service.js
+++ src/Umbraco.Web.UI.Client/src/common/services/blockeditormodelobject.service.js
@@ -13,8 +13,7 @@
 (function () {
     'use strict';
 
-
-    function blockEditorModelObjectFactory($interpolate, $q, udiService, contentResource, localizationService, umbRequestHelper) {
+    function blockEditorModelObjectFactory($interpolate, $q, udiService, contentResource, localizationService, umbRequestHelper, clipboardService) {
 
         /**
          * Simple mapping from property model content entry to editing model,
@@ -748,7 +747,9 @@
              */
             createFromElementType: function (elementTypeDataModel) {
 
-                elementTypeDataModel = Utilities.copy(elementTypeDataModel);
+
+                elementTypeDataModel = clipboardService.parseContentForPaste(elementTypeDataModel);
+
 
                 var contentElementTypeKey = elementTypeDataModel.contentTypeKey;
 
