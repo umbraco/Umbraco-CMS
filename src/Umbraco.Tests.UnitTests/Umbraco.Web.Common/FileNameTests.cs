df4677e97000b3f8ace4974160e71c464689f03f
Mole <nikolajlauridsen@protonmail.ch>
2020-08-27 11:31:46 +0200

    Update src/Umbraco.Tests.UnitTests/Umbraco.Web.Common/FileNameTests.cs
    Co-authored-by: Bjarke Berg <mail@bergmania.dk>

--- src/Umbraco.Tests.UnitTests/Umbraco.Web.Common/FileNameTests.cs
+++ src/Umbraco.Tests.UnitTests/Umbraco.Web.Common/FileNameTests.cs
@@ -1,40 +1,34 @@
-﻿using System;
 using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using System.Linq;
 using System.Threading.Tasks;
+using AutoFixture.NUnit3;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
 using Moq;
 using NUnit.Framework;
 using Umbraco.Core.Configuration;
-using Umbraco.Tests.UnitTests.AutoFixture;
-using Umbraco.Web.Common.Install;
-using Umbraco.Core;
-using AutoFixture.NUnit3;
 using Umbraco.Core.Hosting;
-using System.IO;
-using System.Reflection;
-using System.Linq;
+using Umbraco.Tests.UnitTests.AutoFixture;
 using Umbraco.Web.BackOffice.Controllers;
-using Microsoft.AspNetCore.Mvc.ViewFeatures;
-using AutoFixture;
-using Microsoft.AspNetCore.Http;
+using Umbraco.Web.Common.Install;
 
 namespace Umbraco.Tests.UnitTests.Umbraco.Web.Common
 {
     [TestFixture]
-    class FileNameTests 
+    internal class FileNameTests
     {
-        private string getViewName(ViewResult viewResult, string seperator = "/")
+        private string GetViewName(ViewResult viewResult, string separator = "/")
         {
-            var sections = viewResult.ViewName.Split(seperator);
-            return sections[sections.Length - 1];
+            var sections = viewResult.ViewName.Split(separator);
+            return sections[^1];
         }
 
-        private string[] getUiFiles(IEnumerable<string> pathFromNetCore)
+        private IEnumerable<string> GetUiFiles(IEnumerable<string> pathFromNetCore)
         {
             var sourceRoot = TestContext.CurrentContext.TestDirectory.Split("Umbraco.Tests.UnitTests")[0];
             var pathToFiles = Path.Combine(sourceRoot, "Umbraco.Web.UI.NetCore");
-            foreach(var pathSection in pathFromNetCore)
+            foreach (var pathSection in pathFromNetCore)
             {
                 pathToFiles = Path.Combine(pathToFiles, pathSection);
             }
@@ -42,35 +36,38 @@ namespace Umbraco.Tests.UnitTests.Umbraco.Web.Common
             return new DirectoryInfo(pathToFiles).GetFiles().Select(f => f.Name).ToArray();
         }
 
-        [Test, AutoMoqData]
+        [Test]
+        [AutoMoqData]
         public async Task InstallViewExists(
             [Frozen] IHostingEnvironment hostingEnvironment,
             InstallController sut)
         {
             Mock.Get(hostingEnvironment).Setup(x => x.ToAbsolute(It.IsAny<string>())).Returns("http://localhost/");
             var viewResult = await sut.Index() as ViewResult;
-            var fileName = getViewName(viewResult, Path.DirectorySeparatorChar.ToString());
+            var fileName = GetViewName(viewResult, Path.DirectorySeparatorChar.ToString());
 
-            var views = getUiFiles(new string[] { "Umbraco", "UmbracoInstall" });
+            var views = GetUiFiles(new[] { "Umbraco", "UmbracoInstall" });
             Assert.True(views.Contains(fileName), $"Expected {fileName} to exist, but it didn't");
         }
 
-        [Test, AutoMoqData]
-        public void PrviewViewExists(
+        [Test]
+        [AutoMoqData]
+        public void PreviewViewExists(
             [Frozen] IGlobalSettings globalSettings,
             PreviewController sut)
         {
             Mock.Get(globalSettings).Setup(x => x.UmbracoPath).Returns("/");
 
             var viewResult = sut.Index() as ViewResult;
-            var fileName = getViewName(viewResult);
+            var fileName = GetViewName(viewResult);
 
-            var views = getUiFiles(new string[] {"Umbraco", "UmbracoBackOffice" });
+            var views = GetUiFiles(new[] { "Umbraco", "UmbracoBackOffice" });
 
             Assert.True(views.Contains(fileName), $"Expected {fileName} to exist, but it didn't");
         }
 
-        [Test, AutoMoqData]
+        [Test]
+        [AutoMoqData]
         public async Task BackOfficeDefaultExists(
             [Frozen] IGlobalSettings globalSettings,
             [Frozen] IHostingEnvironment hostingEnvironment,
@@ -85,8 +82,8 @@ namespace Umbraco.Tests.UnitTests.Umbraco.Web.Common
             sut.TempData = tempDataDictionary;
 
             var viewResult = await sut.Default() as ViewResult;
-            var fileName = getViewName(viewResult);
-            var views = getUiFiles(new string[] { "Umbraco", "UmbracoBackOffice" });
+            var fileName = GetViewName(viewResult);
+            var views = GetUiFiles(new[] { "Umbraco", "UmbracoBackOffice" });
 
             Assert.True(views.Contains(fileName), $"Expected {fileName} to exist, but it didn't");
         }
@@ -95,13 +92,12 @@ namespace Umbraco.Tests.UnitTests.Umbraco.Web.Common
         [Test]
         public void LanguageFilesAreLowercase()
         {
-            
-            var files = getUiFiles(new string[] { "Umbraco", "config", "lang" });
+            var files = GetUiFiles(new[] { "Umbraco", "config", "lang" });
             foreach (var fileName in files)
             {
-                Assert.AreEqual(fileName.ToLower(), fileName, $"Language files must be all lowercase but {fileName} is not lowercase.");
+                Assert.AreEqual(fileName.ToLower(), fileName,
+                    $"Language files must be all lowercase but {fileName} is not lowercase.");
             }
-
         }
     }
 }
