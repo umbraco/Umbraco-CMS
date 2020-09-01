262e69ae2a6f98749bed4e20c1e8e82acf6b6fbf
ChadC <chad.d.currie@gmail.com>
2020-09-01 19:50:55 +1200

    Implement remaining bplustree interfaces

--- src/Umbraco.Web/PublishedCache/NuCache/BPlusTreeTransactableDictionary.cs
+++ src/Umbraco.Web/PublishedCache/NuCache/BPlusTreeTransactableDictionary.cs
@@ -19,81 +19,90 @@ namespace Umbraco.Web.PublishedCache.NuCache
             _bplusTree = bplusTree;
         }
 
+        #region IDictionary
         public TValue this[TKey key] { get => _bplusTree[key]; set => _bplusTree[key] = value; }
 
         public ICollection<TKey> Keys => _bplusTree.Keys;
 
         public ICollection<TValue> Values => _bplusTree.Values;
-
-        public int Count => _bplusTree.Count;
-
-        public bool IsReadOnly => _bplusTree.IsReadOnly;
-
         public void Add(TKey key, TValue value)
         {
             _bplusTree.Add(key, value);
         }
-
-        public void Add(KeyValuePair<TKey, TValue> item)
+        public bool ContainsKey(TKey key)
         {
-            _bplusTree.Add(item.Key, item.Value);
+            return _bplusTree.ContainsKey(key);
         }
+        public bool Remove(TKey key)
+        {
+            return _bplusTree.Remove(key);
+        }
+        public bool TryGetValue(TKey key, out TValue value)
+        {
+            return _bplusTree.TryGetValue(key, out value);
+        }
+        #endregion
+
+        #region ICollection<T>
+        public int Count => _bplusTree.Count;
+
+        public bool IsReadOnly => _bplusTree.IsReadOnly;
 
         public void Clear()
         {
             _bplusTree.Clear();
         }
+        #endregion
 
-        public void Commit()
-        {
-            _bplusTree.Commit();
-        }
-
-        public bool Contains(KeyValuePair<TKey, TValue> item)
-        {
-            return _bplusTree.Contains(item);
-        }
-
-        public bool ContainsKey(TKey key)
-        {
-            return _bplusTree.ContainsKey(key);
-        }
-
-        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
-        {
-            _bplusTree.CopyTo(array, arrayIndex);
-        }
-
+        #region IEnumerable<T>
         public IEnumerator<KeyValuePair<TKey, TValue>> GetEnumerator()
         {
             return _bplusTree.GetEnumerator();
         }
+        #endregion
 
-        public bool Remove(TKey key)
+        #region ICollection<KeyValuePair<TKey, TValue>>
+
+        public void Add(KeyValuePair<TKey, TValue> item)
         {
-            return _bplusTree.Remove(key);
+            _bplusTree.Add(item.Key, item.Value);
+        }
+        public bool Contains(KeyValuePair<TKey, TValue> item)
+        {
+            return _bplusTree.Contains(item);
+        }
+       
+        public void CopyTo(KeyValuePair<TKey, TValue>[] array, int arrayIndex)
+        {
+            _bplusTree.CopyTo(array, arrayIndex);
         }
 
         public bool Remove(KeyValuePair<TKey, TValue> item)
         {
             return Remove(item);
         }
+        #endregion
+
+        #region ITransactable
+        public void Commit()
+        {
+            _bplusTree.Commit();
+        }
 
         public void Rollback()
         {
             _bplusTree.Rollback();
         }
+        #endregion
 
-        public bool TryGetValue(TKey key, out TValue value)
-        {
-            return _bplusTree.TryGetValue(key, out value);
-        }
-
+        #region IEnumerable
         IEnumerator IEnumerable.GetEnumerator()
         {
             return _bplusTree.GetEnumerator();
         }
+        #endregion
 
+        #region IDisposable
         protected virtual void Dispose(bool disposing)
         {
             if (!_disposedValue)
@@ -113,9 +122,78 @@ namespace Umbraco.Web.PublishedCache.NuCache
             GC.SuppressFinalize(this);
         }
 
-        public void TryRemove(TKey key, out TValue unused)
+        #endregion
+
+        #region IConcurrentDictionary<TKey,TValue>
+
+        public TValue GetOrAdd(TKey key, Converter<TKey, TValue> fnCreate)
         {
-            _bplusTree.TryRemove(key, out unused);
+            return _bplusTree.GetOrAdd(key, fnCreate);
         }
+
+        public TValue AddOrUpdate(TKey key, TValue addValue, KeyValueUpdate<TKey, TValue> fnUpdate)
+        {
+            return _bplusTree.AddOrUpdate(key, addValue, fnUpdate);
+        }
+
+        public TValue AddOrUpdate(TKey key, Converter<TKey, TValue> fnCreate, KeyValueUpdate<TKey, TValue> fnUpdate)
+        {
+            return _bplusTree.AddOrUpdate(key, fnCreate, fnUpdate);
+        }
+
+        public bool AddOrUpdate<T>(TKey key, ref T createOrUpdateValue) where T : ICreateOrUpdateValue<TKey, TValue>
+        {
+            return _bplusTree.AddOrUpdate(key, ref createOrUpdateValue);
+        }
+
+        public bool TryAdd(TKey key, Converter<TKey, TValue> fnCreate)
+        {
+            return _bplusTree.TryAdd(key, fnCreate);
+        }
+
+        public bool TryUpdate(TKey key, KeyValueUpdate<TKey, TValue> fnUpdate)
+        {
+            return _bplusTree.TryUpdate(key, fnUpdate);
+        }
+
+        public bool TryRemove(TKey key, KeyValuePredicate<TKey, TValue> fnCondition)
+        {
+            return _bplusTree.TryRemove(key, fnCondition);
+        }
+
+        public bool TryRemove<T>(TKey key, ref T removeValue) where T : IRemoveValue<TKey, TValue>
+        {
+            return _bplusTree.TryRemove(key, ref removeValue);
+        }
+
+        public TValue GetOrAdd(TKey key, TValue value)
+        {
+            return _bplusTree.GetOrAdd(key, value);
+        }
+
+        public bool TryAdd(TKey key, TValue value)
+        {
+            return _bplusTree.TryAdd(key, value);
+        }
+
+        public bool TryUpdate(TKey key, TValue value)
+        {
+            return _bplusTree.TryUpdate(key, value);
+        }
+
+        public bool TryUpdate(TKey key, TValue value, TValue comparisonValue)
+        {
+            return _bplusTree.TryUpdate(key, value, comparisonValue);
+        }
+
+        public bool TryRemove(TKey key, out TValue value)
+        {
+            return _bplusTree.TryRemove(key, out value);
+        }
+        #endregion
+
+        #region DictionaryEX
+
+        #endregion
     }
 }
--- src/Umbraco.Web/PublishedCache/NuCache/ITransactableDictionary.cs
+++ src/Umbraco.Web/PublishedCache/NuCache/ITransactableDictionary.cs
@@ -6,16 +6,13 @@ namespace Umbraco.Web.PublishedCache.NuCache
 {
     public interface ITransactableDictionary<TKey, TValue> :
         ITransactable,
-        //IConcurrentDictionary<TKey, TValue>,
-        //IDictionaryEx<TKey, TValue>,
+        CSharpTest.Net.Collections.IConcurrentDictionary<TKey, TValue>,
         IDictionary<TKey, TValue>,
         ICollection<KeyValuePair<TKey, TValue>>,
         IEnumerable<KeyValuePair<TKey, TValue>>,
         IEnumerable,
         IDisposable
     {
-        //IDictionaryEx<TKey, TValue>
-        void TryRemove(TKey key, out TValue unused);
     }
     public interface ITransactable : IDisposable
     {
