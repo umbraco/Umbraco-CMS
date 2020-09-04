87486ffa4bb3c8cce5fff37c48fcf6388cbfec4b
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2020-09-04 17:53:36 +0200

    Add more logic to checkboxlist prevalue editor

--- src/Umbraco.Web.UI.Client/src/views/prevalueeditors/checkboxlist.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/prevalueeditors/checkboxlist.controller.js
@@ -3,9 +3,59 @@
         
         var vm = this;
 
+        vm.configItems = [];
+        vm.viewItems = [];
         vm.change = change;
 
         function init() {
+
+            var prevalues = [];
+            if ($scope.model.config) {
+                prevalues = $scope.model.config.prevalues || [];
+            }
+            else {
+                prevalues = $scope.model.prevalues || [];
+            }
+
+            console.log("prevalues", prevalues);
+
+            var items = [];
+            var vals = _.values(prevalues);
+            var keys = _.keys(prevalues);
+            console.log("vals", vals);
+            console.log("keys", keys);
+
+            for (var i = 0; i < vals.length; i++) {
+                items.push({ key: keys[i], value: vals[i].value });
+            }
+
+            console.log("items", items);
+
+            vm.configItems = items;
+
+            if ($scope.model.value === null || $scope.model.value === undefined) {
+                $scope.model.value = [];
+            }
+
+            // update view model.
+            generateViewModel($scope.model.value);
+
+        }
+
+        function generateViewModel(newVal) {
+
+            vm.viewItems = [];
+
+            var iConfigItem;
+            for (var i = 0; i < vm.configItems.length; i++) {
+                iConfigItem = vm.configItems[i];
+                var isChecked = _.contains(newVal, iConfigItem.value);
+                vm.viewItems.push({
+                    checked: isChecked,
+                    key: iConfigItem.key,
+                    value: iConfigItem.value
+                });
+            }
 
         }
 
