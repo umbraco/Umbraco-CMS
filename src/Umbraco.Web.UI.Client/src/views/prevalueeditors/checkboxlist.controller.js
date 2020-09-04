9cb8b10039cdf38c34c7f47d698f6edf7fea8b38
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2020-09-04 17:29:23 +0200

    Prepare checkboxlist prevalue editor

---
+++ src/Umbraco.Web.UI.Client/src/views/prevalueeditors/checkboxlist.controller.js
@@ -0,0 +1,34 @@
+﻿angular.module("umbraco").controller("Umbraco.PrevalueEditors.CheckboxListController",
+    function ($scope) {
+        
+        var vm = this;
+
+        vm.change = change;
+
+        function init() {
+
+        }
+
+        function change(model, value) {
+
+            console.log("checkboxlist prevalues", model, value);
+
+            var index = $scope.model.value.indexOf(value);
+
+            if (model === true) {
+                //if it doesn't exist in the model, then add it
+                if (index < 0) {
+                    $scope.model.value.push(value);
+                }
+            } else {
+                //if it exists in the model, then remove it
+                if (index >= 0) {
+                    $scope.model.value.splice(index, 1);
+                }
+            }
+
+        }
+
+        init();
+
+    });
