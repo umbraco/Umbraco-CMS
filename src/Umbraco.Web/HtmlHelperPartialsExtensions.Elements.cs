5c273250aef42d1cb02edb72d8336a2659754d52
Ronald Barendse <ronald@panoramastudios.nl>
2020-09-07 13:22:22 +0200

    Add Partials and RenderPartials extension methods

---
+++ src/Umbraco.Web/HtmlHelperPartialsExtensions.Elements.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.Mvc;
+using Umbraco.Core;
+using Umbraco.Core.Models.PublishedContent;
+
+namespace Umbraco.Web
+{
+    public static partial class HtmlHelperPartialsExtensions
+    {
+        /// <summary>
+        /// Gets the partial view name for <see cref="IPublishedElement" /> items.
+        /// </summary>
+        private static readonly Func<IPublishedElement, string, string> getElementPartialViewName = (c, prefix) => (string.IsNullOrEmpty(prefix) ? null : prefix.EnsureEndsWith('/')) + c.ContentType.Alias;
+
+        /// <summary>
+        /// Renders the partial views for every <see cref="IPublishedElement" /> item as an HTML-encoded string, using the content type alias as partial view name.
+        /// </summary>
+        /// <param name="htmlHelper">The HTML helper.</param>
+        /// <param name="elements">The elements used as model for the partial views.</param>
+        /// <param name="partialViewPath">The partial view path, prefixed to the content type alias.</param>
+        /// <param name="viewData">The view data for the partial views.</param>
+        /// <returns>
+        /// The partial views that are rendered as an HTML-encoded string.
+        /// </returns>
+        public static IHtmlString Partials(this HtmlHelper htmlHelper, IEnumerable<IPublishedElement> elements, string partialViewPath = null, ViewDataDictionary viewData = null)
+        {
+            return htmlHelper.Partials(elements, c => getElementPartialViewName(c, partialViewPath), viewData);
+        }
+
+        /// <summary>
+        /// Renders the partial views for every <see cref="IPublishedElement" /> item as an HTML-encoded string, using the content type alias as partial view name and <paramref name="getViewData" /> to get the view data.
+        /// </summary>
+        /// <param name="htmlHelper">The HTML helper.</param>
+        /// <param name="elements">The elements used as model for the partial views.</param>
+        /// <param name="partialViewPath">The partial view path, prefixed to the content type alias.</param>
+        /// <param name="getViewData">A function to get the view data for an item.</param>
+        /// <returns>
+        /// The partial views that are rendered as an HTML-encoded string.
+        /// </returns>
+        public static IHtmlString Partials(this HtmlHelper htmlHelper, IEnumerable<IPublishedElement> elements, string partialViewPath, Func<IPublishedElement, ViewDataDictionary> getViewData)
+        {
+            return htmlHelper.Partials(elements, c => getElementPartialViewName(c, partialViewPath), getViewData);
+        }
+
+        /// <summary>
+        /// Renders the partial views for every <see cref="IPublishedElement" /> item as an HTML-encoded string, using the content type alias as partial view name and <paramref name="getViewData" /> to get the view data.
+        /// </summary>
+        /// <param name="htmlHelper">The HTML helper.</param>
+        /// <param name="elements">The elements used as model for the partial views.</param>
+        /// <param name="partialViewPath">The partial view path, prefixed to the content type alias.</param>
+        /// <param name="getViewData">A function to get the view data for an item.</param>
+        /// <returns>
+        /// The partial views that are rendered as an HTML-encoded string.
+        /// </returns>
+        public static IHtmlString Partials(this HtmlHelper htmlHelper, IEnumerable<IPublishedElement> elements, string partialViewPath, Func<IPublishedElement, int, ViewDataDictionary> getViewData)
+        {
+            return htmlHelper.Partials(elements, (c, i) => getElementPartialViewName(c, partialViewPath), getViewData);
+        }
+
+        /// <summary>
+        /// Renders the partial views for every <see cref="IPublishedElement" /> item as an HTML-encoded string, using <paramref name="getModel" /> to get the model and the content type alias as partial view name.
+        /// </summary>
+        /// <typeparam name="T">The type of the model.</typeparam>
+        /// <param name="htmlHelper">The HTML helper.</param>
+        /// <param name="elements">The elements used as model for the partial views.</param>
+        /// <param name="getModel">A function to get the model for an item.</param>
+        /// <param name="partialViewPath">The partial view path, prefixed to the content type alias.</param>
+        /// <param name="viewData">The view data.</param>
+        /// <returns>
+        /// The partial views that are rendered as an HTML-encoded string.
+        /// </returns>
+        public static IHtmlString Partials<T>(this HtmlHelper htmlHelper, IEnumerable<IPublishedElement> elements, Func<IPublishedElement, T> getModel, string partialViewPath, ViewDataDictionary viewData = null)
+        {
+            return htmlHelper.Partials(elements, getModel, (c, m, i) => getElementPartialViewName(c, partialViewPath), (c, m, i) => viewData);
+        }
+
+        /// <summary>
+        /// Renders the partial views for every <see cref="IPublishedElement" /> item as an HTML-encoded string, using <paramref name="getModel" /> to get the model, the content type alias as partial view name and <paramref name="getViewData" /> to get the view data.
+        /// </summary>
+        /// <typeparam name="T">The type of the model.</typeparam>
+        /// <param name="htmlHelper">The HTML helper.</param>
+        /// <param name="elements">The elements used as model for the partial views.</param>
+        /// <param name="getModel">A function to get the model for an item.</param>
+        /// <param name="partialViewPath">The partial view path, prefixed to the content type alias.</param>
+        /// <param name="getViewData">A function to get the view data for an item.</param>
+        /// <returns>
+        /// The partial views that are rendered as an HTML-encoded string.
+        /// </returns>
+        public static IHtmlString Partials<T>(this HtmlHelper htmlHelper, IEnumerable<IPublishedElement> elements, Func<IPublishedElement, T> getModel, string partialViewPath, Func<IPublishedElement, T, int, ViewDataDictionary> getViewData)
+        {
+            return htmlHelper.Partials(elements, getModel, (c, m, i) => getElementPartialViewName(c, partialViewPath), getViewData);
+        }
+
+        /// <summary>
+        /// Renders the partial views for every <see cref="IPublishedElement" /> item, using the content type alias as partial view name.
+        /// </summary>
+        /// <param name="htmlHelper">The HTML helper.</param>
+        /// <param name="elements">The elements to render partial views for.</param>
+        /// <param name="partialViewPath">The partial view path, prefixed to the content type alias.</param>
+        /// <param name="viewData">The view data for the partial views.</param>
+        public static void RenderPartials(this HtmlHelper htmlHelper, IEnumerable<IPublishedElement> elements, string partialViewPath = null, ViewDataDictionary viewData = null)
+        {
+            htmlHelper.RenderPartials(elements, c => getElementPartialViewName(c, partialViewPath), viewData);
+        }
+
+        /// <summary>
+        /// Renders the partial views for every <see cref="IPublishedElement" /> item, using the content type alias as partial view name and <paramref name="getViewData" /> to get the view data.
+        /// </summary>
+        /// <param name="htmlHelper">The HTML helper.</param>
+        /// <param name="elements">The elements to render partial views for.</param>
+        /// <param name="partialViewPath">The partial view path, prefixed to the content type alias.</param>
+        /// <param name="getViewData">A function to get the view data for an item.</param>
+        public static void RenderPartials(this HtmlHelper htmlHelper, IEnumerable<IPublishedElement> elements, string partialViewPath, Func<IPublishedElement, ViewDataDictionary> getViewData)
+        {
+            htmlHelper.RenderPartials(elements, c => getElementPartialViewName(c, partialViewPath), getViewData);
+        }
+
+        /// <summary>
+        /// Renders the partial views for every <see cref="IPublishedElement" /> item, using the content type alias as partial view name and <paramref name="getViewData" /> to get the view data.
+        /// </summary>
+        /// <param name="htmlHelper">The HTML helper.</param>
+        /// <param name="elements">The elements to render partial views for.</param>
+        /// <param name="partialViewPath">The partial view path, prefixed to the content type alias.</param>
+        /// <param name="getViewData">A function to get the view data for an item.</param>
+        public static void RenderPartials(this HtmlHelper htmlHelper, IEnumerable<IPublishedElement> elements, string partialViewPath, Func<IPublishedElement, int, ViewDataDictionary> getViewData)
+        {
+            htmlHelper.RenderPartials(elements, (c, i) => getElementPartialViewName(c, partialViewPath), getViewData);
+        }
+
+        /// <summary>
+        /// Renders the partial views for every <see cref="IPublishedElement" /> item, using <paramref name="getModel" /> to get the model, the content type alias as partial view name and <paramref name="getViewData" /> to get the view data.
+        /// </summary>
+        /// <typeparam name="T">The type of the model.</typeparam>
+        /// <param name="htmlHelper">The HTML helper.</param>
+        /// <param name="elements">The elements to render partial views for.</param>
+        /// <param name="getModel">A function to get the model for an item.</param>
+        /// <param name="partialViewPath">The partial view path, prefixed to the content type alias.</param>
+        /// <param name="getViewData">A function to get the view data for an item.</param>
+        public static void RenderPartials<T>(this HtmlHelper htmlHelper, IEnumerable<IPublishedElement> elements, Func<IPublishedElement, T> getModel, string partialViewPath, Func<IPublishedElement, T, int, ViewDataDictionary> getViewData)
+        {
+            htmlHelper.RenderPartials(elements, getModel, (c, m, i) => getElementPartialViewName(c, partialViewPath), getViewData);
+        }
+    }
+}
---
+++ src/Umbraco.Web/HtmlHelperPartialsExtensions.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Mvc.Html;
+
+namespace Umbraco.Web
+{
+    public static partial class HtmlHelperPartialsExtensions
+    {
+        /// <summary>
+        /// Renders the partial views for every <paramref name="items" /> item as an HTML-encoded string, using <paramref name="getPartialViewName" /> to get the partial view name.
+        /// </summary>
+        /// <typeparam name="T">The type of the items.</typeparam>
+        /// <param name="htmlHelper">The HTML helper.</param>
+        /// <param name="items">The items to render partial views for.</param>
+        /// <param name="getPartialViewName">A function to get the partial view name for an item.</param>
+        /// <param name="viewData">The view data for the partial views.</param>
+        /// <returns>
+        /// The partial views that are rendered as an HTML-encoded string.
+        /// </returns>
+        public static IHtmlString Partials<T>(this HtmlHelper htmlHelper, IEnumerable<T> items, Func<T, string> getPartialViewName, ViewDataDictionary viewData = null)
+        {
+            return htmlHelper.Partials(items, c => c, (c, m, i) => getPartialViewName(c), (c, m, i) => viewData);
+        }
+
+        /// <summary>
+        /// Renders the partial views for every <paramref name="items" /> item as an HTML-encoded string, using <paramref name="getPartialViewName" /> to get the partial view name and <paramref name="getViewData" /> to get the view data.
+        /// </summary>
+        /// <typeparam name="T">The type of the items.</typeparam>
+        /// <param name="htmlHelper">The HTML helper.</param>
+        /// <param name="items">The items to render partial views for.</param>
+        /// <param name="getPartialViewName">A function to get the partial view name for an item.</param>
+        /// <param name="getViewData">A function to get the view data for an item.</param>
+        /// <returns>
+        /// The partial views that are rendered as an HTML-encoded string.
+        /// </returns>
+        public static IHtmlString Partials<T>(this HtmlHelper htmlHelper, IEnumerable<T> items, Func<T, string> getPartialViewName, Func<T, ViewDataDictionary> getViewData)
+        {
+            return htmlHelper.Partials(items, c => c, (c, m, i) => getPartialViewName(c), (c, m, i) => getViewData(c));
+        }
+
+        /// <summary>
+        /// Renders the partial views for every <paramref name="items" /> item as an HTML-encoded string, using <paramref name="getPartialViewName" /> to get the partial view name and <paramref name="getViewData" /> to get the view data.
+        /// </summary>
+        /// <typeparam name="T">The type of the items.</typeparam>
+        /// <param name="htmlHelper">The HTML helper.</param>
+        /// <param name="items">The items to render partial views for.</param>
+        /// <param name="getPartialViewName">A function to get the partial view name for an item.</param>
+        /// <param name="getViewData">A function to get the view data for an item.</param>
+        /// <returns>
+        /// The partial views that are rendered as an HTML-encoded string.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">getPartialViewName
+        /// or
+        /// getViewData</exception>
+        public static IHtmlString Partials<T>(this HtmlHelper htmlHelper, IEnumerable<T> items, Func<T, int, string> getPartialViewName, Func<T, int, ViewDataDictionary> getViewData)
+        {
+            return htmlHelper.Partials(items, c => c, (c, m, i) => getPartialViewName(c, i), (c, m, i) => getViewData(c, i));
+        }
+
+        /// <summary>
+        /// Renders the partial views for every <paramref name="items" /> item as an HTML-encoded string, using <paramref name="getPartialViewName" /> to get the partial view name and <paramref name="getViewData" /> to get the view data.
+        /// </summary>
+        /// <typeparam name="T1">The type of the items.</typeparam>
+        /// <typeparam name="T2">The type of the model.</typeparam>
+        /// <param name="htmlHelper">The HTML helper.</param>
+        /// <param name="items">The items to render partial views for.</param>
+        /// <param name="getModel">A function to get the model for an item.</param>
+        /// <param name="getPartialViewName">A function to get the partial view name for an item.</param>
+        /// <param name="getViewData">A function to get the view data for an item.</param>
+        /// <returns>
+        /// The partial views that are rendered as an HTML-encoded string.
+        /// </returns>
+        /// <exception cref="ArgumentNullException">getModel
+        /// or
+        /// getPartialViewName
+        /// or
+        /// getViewData</exception>
+        public static IHtmlString Partials<T1, T2>(this HtmlHelper htmlHelper, IEnumerable<T1> items, Func<T1, T2> getModel, Func<T1, T2, int, string> getPartialViewName, Func<T1, T2, int, ViewDataDictionary> getViewData)
+        {
+            if (getModel == null)
+            {
+                throw new ArgumentNullException(nameof(getModel));
+            }
+            if (getPartialViewName == null)
+            {
+                throw new ArgumentNullException(nameof(getPartialViewName));
+            }
+            if (getViewData == null)
+            {
+                throw new ArgumentNullException(nameof(getViewData));
+            }
+
+            var sb = new StringBuilder();
+
+            if (items != null)
+            {
+                var index = 0;
+                foreach (var item in items)
+                {
+                    var model = getModel(item);
+                    var partialViewName = getPartialViewName(item, model, index);
+                    if (!string.IsNullOrEmpty(partialViewName))
+                    {
+                        sb.Append(htmlHelper.Partial(partialViewName, model, getViewData(item, model, index) ?? htmlHelper.ViewData));
+                    }
+
+                    index++;
+                }
+            }
+
+            return new HtmlString(sb.ToString());
+        }
+
+        /// <summary>
+        /// Renders the partial views for every <typeparamref name="T" /> item, using <paramref name="getPartialViewName" /> to get the partial view name.
+        /// </summary>
+        /// <typeparam name="T">The type of the items.</typeparam>
+        /// <param name="htmlHelper">The HTML helper.</param>
+        /// <param name="items">The items to render partial views for.</param>
+        /// <param name="getPartialViewName">A function to get the partial view name for an item.</param>
+        /// <param name="viewData">The view data for the partial views.</param>
+        public static void RenderPartials<T>(this HtmlHelper htmlHelper, IEnumerable<T> items, Func<T, string> getPartialViewName, ViewDataDictionary viewData = null)
+        {
+            htmlHelper.RenderPartials(items, c => c, (c, m, i) => getPartialViewName(c), (c, m, i) => viewData);
+        }
+
+        /// <summary>
+        /// Renders the partial views for every <typeparamref name="T" /> item, using <paramref name="getPartialViewName" /> to get the partial view name and <paramref name="getViewData" /> to get the view data.
+        /// </summary>
+        /// <typeparam name="T">The type of the items.</typeparam>
+        /// <param name="htmlHelper">The HTML helper.</param>
+        /// <param name="items">The items to render partial views for.</param>
+        /// <param name="getPartialViewName">A function to get the partial view name for an item.</param>
+        /// <param name="getViewData">A function to get the view data for an item.</param>
+        public static void RenderPartials<T>(this HtmlHelper htmlHelper, IEnumerable<T> items, Func<T, string> getPartialViewName, Func<T, ViewDataDictionary> getViewData)
+        {
+            htmlHelper.RenderPartials(items, c => c, (c, m, i) => getPartialViewName(c), (c, m, i) => getViewData(c));
+        }
+
+        /// <summary>
+        /// Renders the partial views for every <typeparamref name="T" /> item, using <paramref name="getPartialViewName" /> to get the partial view name and <paramref name="getViewData" /> to get the view data.
+        /// </summary>
+        /// <typeparam name="T">The type of the items.</typeparam>
+        /// <param name="htmlHelper">The HTML helper.</param>
+        /// <param name="items">The items to render partial views for.</param>
+        /// <param name="getPartialViewName">A function to get the partial view name for an item.</param>
+        /// <param name="getViewData">A function to get the view data for an item.</param>
+        public static void RenderPartials<T>(this HtmlHelper htmlHelper, IEnumerable<T> items, Func<T, int, string> getPartialViewName, Func<T, int, ViewDataDictionary> getViewData)
+        {
+            htmlHelper.RenderPartials(items, c => c, (c, m, i) => getPartialViewName(c, i), (c, m, i) => getViewData(c, i));
+        }
+
+        /// <summary>
+        /// Renders the partial views for every <typeparamref name="T1" /> item, using <paramref name="getModel" /> to get the model, <paramref name="getPartialViewName" /> to get the partial view name and <paramref name="getViewData" /> to get the view data.
+        /// </summary>
+        /// <typeparam name="T1">The type of the items.</typeparam>
+        /// <typeparam name="T2">The type of the model.</typeparam>
+        /// <param name="htmlHelper">The HTML helper.</param>
+        /// <param name="items">The items to render partial views for.</param>
+        /// <param name="getModel">A function to get the model for an item.</param>
+        /// <param name="getPartialViewName">A function to get the partial view name for an item.</param>
+        /// <param name="getViewData">A function to get the view data for an item.</param>
+        /// <exception cref="ArgumentNullException">getModel
+        /// or
+        /// getPartialViewName
+        /// or
+        /// getViewData</exception>
+        public static void RenderPartials<T1, T2>(this HtmlHelper htmlHelper, IEnumerable<T1> items, Func<T1, T2> getModel, Func<T1, T2, int, string> getPartialViewName, Func<T1, T2, int, ViewDataDictionary> getViewData)
+        {
+            if (getModel == null)
+            {
+                throw new ArgumentNullException(nameof(getModel));
+            }
+            if (getPartialViewName == null)
+            {
+                throw new ArgumentNullException(nameof(getPartialViewName));
+            }
+            if (getViewData == null)
+            {
+                throw new ArgumentNullException(nameof(getViewData));
+            }
+
+            if (items != null)
+            {
+                var index = 0;
+                foreach (var item in items)
+                {
+                    var model = getModel(item);
+                    var partialViewName = getPartialViewName(item, model, index);
+                    if (!string.IsNullOrEmpty(partialViewName))
+                    {
+                        htmlHelper.RenderPartial(partialViewName, model, getViewData(item, model, index) ?? htmlHelper.ViewData);
+                    }
+
+                    index++;
+                }
+            }
+        }
+    }
+}
