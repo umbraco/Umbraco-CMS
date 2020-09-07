a2c24bcc7682a1c7ca01fbd4aabe9b3f39c60ec0
Shannon <sdeminick@gmail.com>
2020-09-08 02:07:02 +1000

    Gets copying logic working for the block editor, needs more tests though

---
+++ src/Umbraco.Tests/PropertyEditors/BlockEditorComponentTests.cs
@@ -0,0 +1,158 @@
+﻿using Newtonsoft.Json;
+using NUnit.Framework;
+using System;
+using Umbraco.Web.Compose;
+
+namespace Umbraco.Tests.PropertyEditors
+{
+    [TestFixture]
+    public class BlockEditorComponentTests
+    {
+        private readonly JsonSerializerSettings _serializerSettings = new JsonSerializerSettings
+        {
+            Formatting = Formatting.None,
+            NullValueHandling = NullValueHandling.Ignore,
+            
+        };
+
+        [Test]
+        public void No_Nesting()
+        {
+            var guids = new[] { Guid.NewGuid(), Guid.NewGuid() };
+            var guidCounter = 0;
+            Func<Guid> guidFactory = () => guids[guidCounter++];
+
+            var json = @"{
+    ""layout"":
+    {
+        ""Umbraco.BlockList"": [
+            {
+                ""contentUdi"": ""umb://element/036ce82586a64dfba2d523a99ed80f58""
+            },
+            {
+                ""contentUdi"": ""umb://element/48288c21a38a40ef82deb3eda90a58f6""
+            }
+        ]
+    },
+    ""contentData"": [
+        {
+            ""contentTypeKey"": ""d6ce4a86-91a2-45b3-a99c-8691fc1fb020"",
+            ""udi"": ""umb://element/036ce82586a64dfba2d523a99ed80f58"",
+            ""featureName"": ""Hello"",
+            ""featureDetails"": ""World""
+        },
+        {
+            ""contentTypeKey"": ""d6ce4a86-91a2-45b3-a99c-8691fc1fb020"",
+            ""udi"": ""umb://element/48288c21a38a40ef82deb3eda90a58f6"",
+            ""featureName"": ""Another"",
+            ""featureDetails"": ""Feature""
+        }
+    ],
+    ""settingsData"": []
+}";
+
+
+            var expected = json
+                .Replace("036ce82586a64dfba2d523a99ed80f58", guids[0].ToString("N"))
+                .Replace("48288c21a38a40ef82deb3eda90a58f6", guids[1].ToString("N"));
+
+            var component = new BlockEditorComponent();
+            var result = component.CreateNestedContentKeys(json, false, guidFactory);
+
+            var expectedJson = JsonConvert.DeserializeObject(expected, _serializerSettings).ToString();
+            var resultJson = JsonConvert.DeserializeObject(result, _serializerSettings).ToString();
+            Console.WriteLine(expectedJson);
+            Console.WriteLine(resultJson);
+            Assert.AreEqual(expectedJson, resultJson);
+        }
+
+        [Test]
+        public void One_Level_Nesting_Escaped()
+        {
+            var guids = new[] { Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid(), Guid.NewGuid() };
+            var guidCounter = 0;
+            Func<Guid> guidFactory = () => guids[guidCounter++];
+
+            // we need to ensure the escaped json is consistent with how it will be re-escaped after parsing
+            // and this is how to do that, the result will also include quotes around it.
+            var innerJson = JsonConvert.DeserializeObject(@"{
+    ""layout"":
+    {
+        ""Umbraco.BlockList"": [
+            {
+                ""contentUdi"": ""umb://element/4C44CE6B3A5C4F5F8F15E3DC24819A9E""
+            },
+            {
+                ""contentUdi"": ""umb://element/A062C06D6B0B44AC892B35D90309C7F8""
+            }
+        ]
+    },
+    ""contentData"": [
+        {
+            ""contentTypeKey"": ""d6ce4a86-91a2-45b3-a99c-8691fc1fb020"",
+            ""udi"": ""umb://element/4C44CE6B3A5C4F5F8F15E3DC24819A9E"",
+            ""featureName"": ""Hello"",
+            ""featureDetails"": ""World""
+        },
+        {
+            ""contentTypeKey"": ""d6ce4a86-91a2-45b3-a99c-8691fc1fb020"",
+            ""udi"": ""umb://element/A062C06D6B0B44AC892B35D90309C7F8"",
+            ""featureName"": ""Another"",
+            ""featureDetails"": ""Feature""
+        }
+    ],
+    ""settingsData"": []
+}", _serializerSettings);
+
+            var serializedInnerJson = JsonConvert.SerializeObject(innerJson, _serializerSettings);
+
+            var subJsonEscaped = JsonConvert.ToString(serializedInnerJson);
+
+            var json = @"{
+    ""layout"":
+    {
+        ""Umbraco.BlockList"": [
+            {
+                ""contentUdi"": ""umb://element/036ce82586a64dfba2d523a99ed80f58""
+            },
+            {
+                ""contentUdi"": ""umb://element/48288c21a38a40ef82deb3eda90a58f6""
+            }
+        ]
+    },
+    ""contentData"": [
+        {
+            ""contentTypeKey"": ""d6ce4a86-91a2-45b3-a99c-8691fc1fb020"",
+            ""udi"": ""umb://element/036ce82586a64dfba2d523a99ed80f58"",
+            ""featureName"": ""Hello"",
+            ""featureDetails"": ""World"",
+            ""subFeatures"": " + subJsonEscaped + @"
+        },
+        {
+            ""contentTypeKey"": ""d6ce4a86-91a2-45b3-a99c-8691fc1fb020"",
+            ""udi"": ""umb://element/48288c21a38a40ef82deb3eda90a58f6"",
+            ""featureName"": ""Another"",
+            ""featureDetails"": ""Feature""
+        }
+    ],
+    ""settingsData"": []
+}";
+
+            var expected = json
+                .Replace("036ce82586a64dfba2d523a99ed80f58", guids[0].ToString("N"))
+                .Replace("48288c21a38a40ef82deb3eda90a58f6", guids[1].ToString("N"))
+                .Replace("4C44CE6B3A5C4F5F8F15E3DC24819A9E", guids[2].ToString("N"))
+                .Replace("A062C06D6B0B44AC892B35D90309C7F8", guids[3].ToString("N"));
+
+            var component = new BlockEditorComponent();
+            var result = component.CreateNestedContentKeys(json, false, guidFactory);
+
+            var expectedJson = JsonConvert.DeserializeObject(expected, _serializerSettings).ToString();
+            var resultJson = JsonConvert.DeserializeObject(result, _serializerSettings).ToString();
+            Console.WriteLine(expectedJson);
+            Console.WriteLine(resultJson);
+            Assert.AreEqual(expectedJson, resultJson);
+        }
+
+    }
+}
---
+++ src/Umbraco.Web/Compose/BlockEditorComponent.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+using Umbraco.Core.Events;
+using Umbraco.Core.Models;
+using Umbraco.Core.Models.Blocks;
+using Umbraco.Core.PropertyEditors;
+using Umbraco.Core.Services;
+using Umbraco.Core.Services.Implement;
+using Umbraco.Web.PropertyEditors;
+
+namespace Umbraco.Web.Compose
+{
+
+
+    /// <summary>
+    /// A component for Block editors used to bind to events
+    /// </summary>
+    public class BlockEditorComponent : IComponent
+    {
+        private ComplexPropertyEditorContentEventHandler _handler;
+        private readonly BlockListEditorDataConverter _converter = new BlockListEditorDataConverter();
+
+        public void Initialize()
+        {
+            _handler = new ComplexPropertyEditorContentEventHandler(
+                Constants.PropertyEditors.Aliases.BlockList,
+                CreateNestedContentKeys);
+        }
+
+        public void Terminate() => _handler?.Dispose();
+
+        private string CreateNestedContentKeys(string rawJson, bool onlyMissingKeys) => CreateNestedContentKeys(rawJson, onlyMissingKeys, null);
+
+        // internal for tests
+        internal string CreateNestedContentKeys(string rawJson, bool onlyMissingKeys, Func<Guid> createGuid = null, JsonSerializerSettings serializerSettings = null)
+        {
+            // used so we can test nicely
+            if (createGuid == null)
+                createGuid = () => Guid.NewGuid();
+
+            if (string.IsNullOrWhiteSpace(rawJson) || !rawJson.DetectIsJson())
+                return rawJson;
+
+            // Parse JSON
+            var blockListValue = _converter.Deserialize(rawJson);
+
+            UpdateBlockListRecursively(blockListValue, onlyMissingKeys, createGuid, serializerSettings);
+
+            return JsonConvert.SerializeObject(blockListValue.BlockValue, serializerSettings);
+        }
+
+        private void UpdateBlockListRecursively(BlockEditorData blockListData, bool onlyMissingKeys, Func<Guid> createGuid, JsonSerializerSettings serializerSettings)
+        {
+            var oldToNew = new Dictionary<Udi, Udi>();
+            MapOldToNewUdis(oldToNew, blockListData.BlockValue.ContentData, onlyMissingKeys, createGuid);
+            MapOldToNewUdis(oldToNew, blockListData.BlockValue.SettingsData, onlyMissingKeys, createGuid);
+
+            for (var i = 0; i < blockListData.References.Count; i++)
+            {
+                var reference = blockListData.References[i];
+                var hasContentMap = oldToNew.TryGetValue(reference.ContentUdi, out var contentMap);
+                Udi settingsMap = null;
+                var hasSettingsMap = reference.SettingsUdi != null && oldToNew.TryGetValue(reference.SettingsUdi, out settingsMap);
+
+                if (hasContentMap)
+                {
+                    // replace the reference
+                    blockListData.References.RemoveAt(i);
+                    blockListData.References.Insert(i, new ContentAndSettingsReference(contentMap, hasSettingsMap ? settingsMap : null));
+                }
+            }
+
+            // build the layout with the new UDIs
+            var layout = (JArray)blockListData.Layout;
+            layout.Clear();
+            foreach (var reference in blockListData.References)
+            {
+                layout.Add(JObject.FromObject(new BlockListLayoutItem
+                {
+                    ContentUdi = reference.ContentUdi,
+                    SettingsUdi = reference.SettingsUdi
+                }));
+            }
+
+
+            RecursePropertyValues(blockListData.BlockValue.ContentData, onlyMissingKeys, createGuid, serializerSettings);
+            RecursePropertyValues(blockListData.BlockValue.SettingsData, onlyMissingKeys, createGuid, serializerSettings);
+        }
+
+        private void RecursePropertyValues(IEnumerable<BlockItemData> blockData, bool onlyMissingKeys, Func<Guid> createGuid, JsonSerializerSettings serializerSettings)
+        {
+            foreach (var data in blockData)
+            {
+                // check if we need to recurse (make a copy of the dictionary since it will be modified)
+                foreach (var propertyAliasToBlockItemData in new Dictionary<string, object>(data.RawPropertyValues))
+                {
+                    var asString = propertyAliasToBlockItemData.Value?.ToString();
+
+                    //// if this is a nested block list
+                    //if (propertyAliasToBlockItemData.Value.PropertyType.PropertyEditorAlias == Constants.PropertyEditors.Aliases.BlockList)
+                    //{
+                    //    // recurse
+                    //    var blockListValue = _converter.Deserialize(asString);
+                    //    UpdateBlockListRecursively(blockListValue, onlyMissingKeys, createGuid);
+                    //    // set new value
+                    //    data.RawPropertyValues[propertyAliasToBlockItemData.Key] = JsonConvert.SerializeObject(blockListValue.BlockValue);
+                    //}
+
+                    if (asString != null && asString.DetectIsJson())
+                    {
+                        // this gets a little ugly because there could be some other complex editor that contains another block editor
+                        // and since we would have no idea how to parse that, all we can do is try JSON Path to find another block editor
+                        // of our type
+                        var json = JToken.Parse(asString);
+
+                        // select all tokens (flatten)
+                        var allProperties = json.SelectTokens("$..*").Select(x => x.Parent as JProperty).WhereNotNull().ToList();
+                        foreach (var prop in allProperties)
+                        {
+                            if (prop.Name == Constants.PropertyEditors.Aliases.BlockList)
+                            {
+                                // get it's parent 'layout' and it's parent's container
+                                var layout = prop.Parent?.Parent as JProperty;
+                                if (layout != null && layout.Parent is JObject layoutJson)
+                                {
+                                    // recurse
+                                    var blockListValue = _converter.ConvertFrom(layoutJson);
+                                    UpdateBlockListRecursively(blockListValue, onlyMissingKeys, createGuid, serializerSettings);
+
+                                    // set new value
+                                    if (layoutJson.Parent != null)
+                                    {
+                                        // we can replace the sub string
+                                        layoutJson.Replace(JsonConvert.SerializeObject(blockListValue.BlockValue, serializerSettings));
+                                    }
+                                    else
+                                    {
+                                        // this was the root string
+                                        data.RawPropertyValues[propertyAliasToBlockItemData.Key] = JsonConvert.SerializeObject(blockListValue.BlockValue, serializerSettings);
+                                    }
+                                }
+                            }
+                            else if (prop.Name != "layout" && prop.Name != "contentData" && prop.Name != "settingsData" && prop.Name != "contentTypeKey")
+                            {
+                                // this is an arbitrary property that could contain a nested complex editor
+                                var propVal = prop.Value?.ToString();
+                                // check if this might contain a nested Block Editor
+                                if (!propVal.IsNullOrWhiteSpace() && propVal.DetectIsJson() && propVal.InvariantContains(Constants.PropertyEditors.Aliases.BlockList))
+                                {
+                                    if (_converter.TryDeserialize(propVal, out var nestedBlockData))
+                                    {
+                                        // recurse
+                                        UpdateBlockListRecursively(nestedBlockData, onlyMissingKeys, createGuid, serializerSettings);
+                                        // set the value to the updated one
+                                        prop.Value = JsonConvert.SerializeObject(nestedBlockData.BlockValue, serializerSettings);
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+        private void MapOldToNewUdis(Dictionary<Udi, Udi> oldToNew, IEnumerable<BlockItemData> blockData, bool onlyMissingKeys, Func<Guid> createGuid)
+        {
+            foreach (var data in blockData)
+            {
+                if (data.Udi == null)
+                    throw new InvalidOperationException("Block data cannot contain a null UDI");
+
+                // replace the UDIs
+                if (!onlyMissingKeys)
+                {
+                    var newUdi = GuidUdi.Create(Constants.UdiEntityType.Element, createGuid());
+                    oldToNew[data.Udi] = newUdi;
+                    data.Udi = newUdi;
+                }
+            }
+        }
+    }
+}
