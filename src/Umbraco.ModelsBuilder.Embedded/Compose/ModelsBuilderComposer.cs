8a68ba95471cf0331a2fa7a96bf8c3f324cee69d
Bjarke Berg <mail@bergmania.dk>
2020-09-09 11:15:14 +0200

    Removed the init of legacy MB. Future versions of the full MB will have to disable the embedded MB
    Signed-off-by: Bjarke Berg <mail@bergmania.dk>

--- src/Umbraco.ModelsBuilder.Embedded/Compose/ModelsBuilderComposer.cs
+++ src/Umbraco.ModelsBuilder.Embedded/Compose/ModelsBuilderComposer.cs
@@ -15,47 +15,17 @@ namespace Umbraco.ModelsBuilder.Embedded.Compose
     {
         public void Compose(Composition composition)
         {
-            var isLegacyModelsBuilderInstalled = IsLegacyModelsBuilderInstalled();
-
-            if (isLegacyModelsBuilderInstalled)
-            {
-                ComposeForLegacyModelsBuilder(composition);
-                return;
-            }
-
             composition.Components().Append<ModelsBuilderComponent>();
             composition.Register<UmbracoServices>(Lifetime.Singleton);
             composition.RegisterUnique<ModelsGenerator>();
             composition.RegisterUnique<LiveModelsProvider>();
             composition.RegisterUnique<OutOfDateModelsStatus>();
             composition.RegisterUnique<ModelsGenerationError>();
-            
+
             if (composition.Configs.ModelsBuilder().ModelsMode == ModelsMode.PureLive)
                 ComposeForLiveModels(composition);
             else if (composition.Configs.ModelsBuilder().EnableFactory)
                 ComposeForDefaultModelsFactory(composition);
-        }
-
-        private static bool IsLegacyModelsBuilderInstalled()
-        {
-            Assembly legacyMbAssembly = null;
-            try
-            {
-                legacyMbAssembly = Assembly.Load("Umbraco.ModelsBuilder");
-            }
-            catch (System.Exception)
-            {
-                //swallow exception, DLL must not be there
-            }
-
-            return legacyMbAssembly != null;
-        }
-
-        private void ComposeForLegacyModelsBuilder(Composition composition)
-        {
-            composition.Logger.Info<ModelsBuilderComposer>("ModelsBuilder.Embedded is disabled, the external ModelsBuilder was detected.");
-            composition.Components().Append<DisabledModelsBuilderComponent>();
-            composition.Dashboards().Remove<ModelsBuilderDashboard>();
         }
 
         private void ComposeForDefaultModelsFactory(Composition composition)
