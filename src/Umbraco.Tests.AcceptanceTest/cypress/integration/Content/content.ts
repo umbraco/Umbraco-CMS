a4abfd4962814004d47360123828256693949459
Nikolaj <nel@umbraco.dk>
2020-09-10 08:52:18 +0200

    Add Content with contentpicker test and bump helpers version

--- src/Umbraco.Tests.AcceptanceTest/cypress/integration/Content/content.ts
+++ src/Umbraco.Tests.AcceptanceTest/cypress/integration/Content/content.ts
@@ -1,5 +1,5 @@
 /// <reference types="Cypress" />
-import { DocumentTypeBuilder, ContentBuilder } from 'umbraco-cypress-testhelpers';
+import { DocumentTypeBuilder, ContentBuilder, AliasHelper } from 'umbraco-cypress-testhelpers';
 context('Content', () => {
 
     beforeEach(() => {
@@ -498,5 +498,102 @@ context('Content', () => {
 
         // Clean up (content is automatically deleted when document types are gone)
         cy.umbracoEnsureDocumentTypeNameNotExists(rootDocTypeName);
+    });
+
+    it('Content with contentpicker', () => {
+        const pickerDocTypeName = 'Content Picker Type';
+        const groupName = 'ContentPickerGroup';
+        const alias = AliasHelper.toAlias(pickerDocTypeName);
+        const pickedDocTypeName = 'Picked Content Type';
+        const pickedNodeName = 'Content to pick';
+
+        cy.deleteAllContent();
+        cy.umbracoEnsureDocumentTypeNameNotExists(pickerDocTypeName);
+        cy.umbracoEnsureTemplateNameNotExists(pickerDocTypeName);
+        cy.umbracoEnsureDocumentTypeNameNotExists(pickedDocTypeName);
+
+        // Create the content type and content we'll be picking from.
+        const pickedDocType = new DocumentTypeBuilder()
+            .withName(pickedDocTypeName)
+            .withAllowAsRoot(true)
+            .addGroup()
+            .addTextBoxProperty()
+            .withAlias('text')
+            .done()
+            .done()
+            .build();
+            
+        cy.saveDocumentType(pickedDocType).then((generatedType) => {
+            const pickedContentNode = new ContentBuilder()
+                .withContentTypeAlias(generatedType["alias"])
+                .withAction("publishNew")
+                .addVariant()
+                .withName(pickedNodeName)
+                .withSave(true)
+                .withPublish(true)
+                .addProperty()
+                .withAlias('text')
+                .withValue('Acceptance test')
+                .done()
+                .withSave(true)
+                .withPublish(true)
+                .done()
+                .build();
+            cy.saveContent(pickedContentNode);
+        });
+
+        // Create the doctype with a the picker
+        const pickerDocType = new DocumentTypeBuilder()
+            .withName(pickerDocTypeName)
+            .withAlias(alias)
+            .withAllowAsRoot(true)
+            .withDefaultTemplate(alias)
+            .addGroup()
+            .withName(groupName)
+            .addContentPickerProperty()
+            .withAlias('picker')
+            .done()
+            .done()
+            .build();
+
+        cy.saveDocumentType(pickerDocType);
+
+        // Edit it the template to allow us to verify the rendered view.
+        cy.editTemplate(pickerDocTypeName, `@inherits Umbraco.Web.Mvc.UmbracoViewPage<ContentModels.ContentPickerType>
+        @using ContentModels = Umbraco.Web.PublishedModels;
+        @{
+            Layout = null;
+        }
+        
+        @{
+            IPublishedContent typedContentPicker = Model.Value<IPublishedContent>("picker");
+            if (typedContentPicker != null)
+            {
+                <p>@typedContentPicker.Value("text")</p>
+            }
+        }`);
+
+        // Create content with content picker
+        cy.get('.umb-tree-root-link').rightclick();
+        cy.get('.-opens-dialog > .umb-action-link').click();
+        cy.get('[data-element="action-create-contentPickerType"] > .umb-action-link').click();
+        // Fill out content
+        cy.umbracoEditorHeaderName('ContentPickerContent');
+        cy.get('.umb-node-preview-add').click();
+        // Should really try and find a better way to do this, but umbracoTreeItem tries to click the content pane in the background
+        cy.get('[ng-if="vm.treeReady"] > .umb-tree > [ng-if="!tree.root.containsGroups"] > .umb-animated > .umb-tree-item__inner').click();
+        // We have to wait for the picked content to show up or it wont be added.
+        cy.get('.umb-node-preview__description').should('be.visible');
+        //save and publish
+        cy.umbracoButtonByLabelKey('buttons_saveAndPublish').click();
+        cy.umbracoSuccessNotification().should('be.visible');
+
+        // Assert
+        const expectedContent = '<p>Acceptance test</p>'
+        cy.umbracoVerifyRenderedViewContent('contentpickercontent', expectedContent, true).should('be.true');
+        // clean
+        cy.umbracoEnsureDocumentTypeNameNotExists(pickerDocTypeName);
+        cy.umbracoEnsureTemplateNameNotExists(pickerDocTypeName);
+        cy.umbracoEnsureDocumentTypeNameNotExists(pickedDocTypeName);
     });
 });
