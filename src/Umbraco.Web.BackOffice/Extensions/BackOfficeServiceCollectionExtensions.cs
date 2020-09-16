d2b20d8e7f697aabf017266e50d5a240e64b9737
Bjarke Berg <mail@bergmania.dk>
2020-09-16 09:33:20 +0200

    Moved class into own file

--- src/Umbraco.Web.BackOffice/Extensions/BackOfficeServiceCollectionExtensions.cs
+++ src/Umbraco.Web.BackOffice/Extensions/BackOfficeServiceCollectionExtensions.cs
@@ -93,23 +93,4 @@ namespace Umbraco.Extensions
             return new IdentityBuilder(typeof(BackOfficeIdentityUser), services);
         }
     }
-
-
-    public static class IdentityBuilderExtensions
-    {
-        /// <summary>
-        /// Adds a <see cref="UserManager{TUser}"/> for the <seealso cref="UserType"/>.
-        /// </summary>
-        /// <typeparam name="TUserManager">The type of the user manager to add.</typeparam>
-        /// <typeparam name="TInterface"></typeparam>
-        /// <returns>The current <see cref="IdentityBuilder"/> instance.</returns>
-        public static IdentityBuilder AddUserManager<TInterface, TUserManager>(this IdentityBuilder identityBuilder) where TUserManager : UserManager<BackOfficeIdentityUser>, TInterface
-        {
-            identityBuilder.AddUserManager<TUserManager>();
-            identityBuilder.Services.AddScoped(typeof(TInterface), typeof(TUserManager));
-            return identityBuilder;
-        }
-    }
-
-
 }
