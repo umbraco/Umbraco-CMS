b327399cd2c637a715792dc1d40ac1e6253ae3eb
Claus <claus@claus.nu>
2020-09-22 08:11:58 +0200

    adding icon svg data from IconController.

--- src/Umbraco.Web/Editors/BackOfficeModel.cs
+++ src/Umbraco.Web/Editors/BackOfficeModel.cs
@@ -6,13 +6,19 @@ namespace Umbraco.Web.Editors
 
     public class BackOfficeModel
     {
+        private IconController IconController { get; }
         public BackOfficeModel(UmbracoFeatures features, IGlobalSettings globalSettings)
         {
             Features = features;
             GlobalSettings = globalSettings;
+            IconController = new IconController();
+            IconCheckData = IconController.GetIcon("icon-check")?.SvgString;
+            IconDeleteData = IconController.GetIcon("icon-delete")?.SvgString;
         }
         
         public UmbracoFeatures Features { get; }
         public IGlobalSettings GlobalSettings { get; }
+        public string IconCheckData { get; }
+        public string IconDeleteData { get; }
     }
 }
