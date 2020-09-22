a2cb264f270eb790d43f56105480ed1381edab3e
Claus <claus@claus.nu>
2020-09-22 13:14:03 +0200

    Merge remote-tracking branch 'origin/v8/bugfix/8433-member-login-sql-locks' into v8/8.6
    (cherry picked from commit 6122bffa24a6ee5e554c285ee01629552abc0118)

--- src/Umbraco.Core/Persistence/Repositories/IMemberRepository.cs
+++ src/Umbraco.Core/Persistence/Repositories/IMemberRepository.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Umbraco.Core.Models;
 using Umbraco.Core.Persistence.Querying;
 
@@ -6,6 +7,8 @@ namespace Umbraco.Core.Persistence.Repositories
 {
     public interface IMemberRepository : IContentRepository<int, IMember>
     {
+        IMember GetByUsername(string username);
+
         /// <summary>
         /// Finds members in a given role
         /// </summary>
@@ -35,5 +38,17 @@ namespace Umbraco.Core.Persistence.Repositories
         /// <param name="query"></param>
         /// <returns></returns>
         int GetCountByQuery(IQuery<IMember> query);
+
+        /// <summary>
+        /// Sets a members last login date based on their username
+        /// </summary>
+        /// <param name="username"></param>
+        /// <param name="date"></param>
+        /// <remarks>
+        /// This is a specialized method because whenever a member logs in, the membership provider requires us to set the 'online' which requires
+        /// updating their login date. This operation must be fast and cannot use database locks which is fine if we are only executing a single query
+        /// for this data since there won't be any other data contention issues.
+        /// </remarks>
+        void SetLastLogin(string username, DateTime date);
     }
 }
