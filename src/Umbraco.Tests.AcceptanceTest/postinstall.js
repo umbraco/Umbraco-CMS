408546812597989894f5f4597400a7b0fc32e2cb
Niels Lyngsø <niels.lyngso@gmail.com>
2020-09-25 12:01:40 +0200

    update dependency name

--- src/Umbraco.Tests.AcceptanceTest/postinstall.js
+++ src/Umbraco.Tests.AcceptanceTest/postinstall.js
@@ -7,7 +7,7 @@ try {
     //file exists
     console.log("Skips configuration as file already exists, run 'npm run config' to change your configuration.");
   } else {
-    require('./createEnviromentConfiguration.js');
+    require('./config.js');
   }
 } catch(err) {
   console.error(err)
