4e718f6c2712f8be8691d921a65be6b79f2c1a42
Ronald Barendse <ronald@panoramastudios.nl>
2020-09-29 22:52:49 +0200

    Update Range documentation comments and use expression-bodied methods

--- src/Umbraco.Core/Models/Range.cs
+++ src/Umbraco.Core/Models/Range.cs
@@ -1,52 +1,71 @@
 ﻿using System;
+
 namespace Umbraco.Core.Models
 {
-    // <summary>The Range class. Adapted from http://stackoverflow.com/questions/5343006/is-there-a-c-sharp-type-for-representing-an-integer-range</summary>
-    /// <typeparam name="T">Generic parameter.</typeparam>
-    public class Range<T> where T : IComparable<T>
+    /// <summary>
+    /// Represents a range with a minimum and maximum value.
+    /// </summary>
+    /// <typeparam name="T">The type of the minimum and maximum values.</typeparam>
+    public class Range<T>
+        where T : IComparable<T>
     {
-        /// <summary>Minimum value of the range.</summary>
+        /// <summary>
+        /// Gets or sets the minimum value.
+        /// </summary>
+        /// <value>
+        /// The minimum value.
+        /// </value>
         public T Minimum { get; set; }
 
-        /// <summary>Maximum value of the range.</summary>
+        /// <summary>
+        /// Gets or sets the maximum value.
+        /// </summary>
+        /// <value>
+        /// The maximum value.
+        /// </value>
         public T Maximum { get; set; }
 
-        /// <summary>Presents the Range in readable format.</summary>
-        /// <returns>String representation of the Range</returns>
-        public override string ToString()
-        {
-            return string.Format("{0},{1}", this.Minimum, this.Maximum);
-        }
+        /// <summary>
+        /// Returns a <see cref="System.String" /> that represents this instance.
+        /// </summary>
+        /// <returns>
+        /// A <see cref="System.String" /> that represents this instance.
+        /// </returns>
+        public override string ToString() => string.Format("{0},{1}", this.Minimum, this.Maximum);
 
-        /// <summary>Determines if the range is valid.</summary>
-        /// <returns>True if range is valid, else false</returns>
-        public bool IsValid()
-        {
-            return this.Minimum.CompareTo(this.Maximum) <= 0;
-        }
+        /// <summary>
+        /// Determines whether this range is valid (the minimum value is lower than or equal to the maximum value).
+        /// </summary>
+        /// <returns>
+        ///   <c>true</c> if this range is valid; otherwise, <c>false</c>.
+        /// </returns>
+        public bool IsValid() => this.Minimum.CompareTo(this.Maximum) <= 0;
 
-        /// <summary>Determines if the provided value is inside the range.</summary>
-        /// <param name="value">The value to test</param>
-        /// <returns>True if the value is inside Range, else false</returns>
-        public bool ContainsValue(T value)
-        {
-            return (this.Minimum.CompareTo(value) <= 0) && (value.CompareTo(this.Maximum) <= 0);
-        }
+        /// <summary>
+        /// Determines whether this range contains the specified value.
+        /// </summary>
+        /// <param name="value">The value.</param>
+        /// <returns>
+        ///   <c>true</c> if this range contains the specified value; otherwise, <c>false</c>.
+        /// </returns>
+        public bool ContainsValue(T value) => this.Minimum.CompareTo(value) <= 0 && value.CompareTo(this.Maximum) <= 0;
 
-        /// <summary>Determines if this Range is inside the bounds of another range.</summary>
-        /// <param name="Range">The parent range to test on</param>
-        /// <returns>True if range is inclusive, else false</returns>
-        public bool IsInsideRange(Range<T> range)
-        {
-            return this.IsValid() && range.IsValid() && range.ContainsValue(this.Minimum) && range.ContainsValue(this.Maximum);
-        }
+        /// <summary>
+        /// Determines whether this range is inside the specified range.
+        /// </summary>
+        /// <param name="range">The range.</param>
+        /// <returns>
+        ///   <c>true</c> if this range is inside the specified range; otherwise, <c>false</c>.
+        /// </returns>
+        public bool IsInsideRange(Range<T> range) => this.IsValid() && range.IsValid() && range.ContainsValue(this.Minimum) && range.ContainsValue(this.Maximum);
 
-        /// <summary>Determines if another range is inside the bounds of this range.</summary>
-        /// <param name="Range">The child range to test</param>
-        /// <returns>True if range is inside, else false</returns>
-        public bool ContainsRange(Range<T> range)
-        {
-            return this.IsValid() && range.IsValid() && this.ContainsValue(range.Minimum) && this.ContainsValue(range.Maximum);
-        }
+        /// <summary>
+        /// Determines whether this range contains the specified range.
+        /// </summary>
+        /// <param name="range">The range.</param>
+        /// <returns>
+        ///   <c>true</c> if this range contains the specified range; otherwise, <c>false</c>.
+        /// </returns>
+        public bool ContainsRange(Range<T> range) => this.IsValid() && range.IsValid() && this.ContainsValue(range.Minimum) && this.ContainsValue(range.Maximum);
     }
 }
