c94e96aad7e235ba38b4aa9c015e9b1c127ca255
nzdev <834725+nzdev@users.noreply.github.com>
2020-09-29 19:10:13 +1300

    Fix ScopedNuCacheTests

--- src/Umbraco.Web/PublishedCache/NuCache/DataSource/JsonContentNestedDataSerializer.cs
+++ src/Umbraco.Web/PublishedCache/NuCache/DataSource/JsonContentNestedDataSerializer.cs
@@ -10,7 +10,7 @@ namespace Umbraco.Web.PublishedCache.NuCache.DataSource
     {
         public ContentCacheDataModel Deserialize(int contentTypeId, string stringData, byte[] byteData)
         {
-            if (byteData != null)
+            if (byteData != null && byteData.Length >0)
                 throw new NotSupportedException($"{typeof(JsonContentNestedDataSerializer)} does not support byte[] serialization");
 
             // by default JsonConvert will deserialize our numeric values as Int64
