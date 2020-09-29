05295cfef9b1c1c494a782075912419a2e030cca
Mole <nikolajlauridsen@protonmail.ch>
2020-09-29 12:49:02 +0200

    Add hub files to Backoffice

---
+++ src/Umbraco.Web.BackOffice/SignalR/IPreviewHub.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.SignalR;
+
+namespace Umbraco.Web.BackOffice.SignalR
+{
+    public interface IPreviewHub
+    {
+        // define methods implemented by client
+        // ReSharper disable InconsistentNaming
+
+        void refreshed(int id);
+
+        // ReSharper restore InconsistentNaming
+    }
+}
---
+++ src/Umbraco.Web.BackOffice/SignalR/PreviewHub.cs
@@ -0,0 +1,8 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+
+namespace Umbraco.Web.BackOffice.SignalR
+{
+    public class PreviewHub : Hub<IPreviewHub>
+    { }
+}
