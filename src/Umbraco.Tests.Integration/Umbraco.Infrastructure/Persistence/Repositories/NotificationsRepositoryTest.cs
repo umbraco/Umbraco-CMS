eb575826687fc1c41d9ffe83bf245108857e5232
Andy Butland <abutland@zonedigital.com>
2020-10-18 09:42:51 +0200

    Reorganised integration tests into folders and classes grouping by project.

---
+++ src/Umbraco.Tests.Integration/Umbraco.Infrastructure/Persistence/Repositories/NotificationsRepositoryTest.cs
@@ -0,0 +1,169 @@
+﻿using System;
+using System.Globalization;
+using System.Linq;
+using Moq;
+using NUnit.Framework;
+using Umbraco.Core;
+using Umbraco.Core.Models.Entities;
+using Umbraco.Core.Models.Membership;
+using Umbraco.Core.Persistence.Dtos;
+using Umbraco.Core.Persistence.Repositories.Implement;
+using Umbraco.Core.Scoping;
+using Umbraco.Tests.Integration.Testing;
+using Umbraco.Tests.Testing;
+
+namespace Umbraco.Tests.Umbraco.Infrastructure.Persistence.Repositories
+{
+    [TestFixture]
+    [UmbracoTest(Database = UmbracoTestOptions.Database.NewSchemaPerTest)]
+    public class NotificationsRepositoryTest : UmbracoIntegrationTest
+    {
+        [Test]
+        public void CreateNotification()
+        {
+            var provider = ScopeProvider;
+            using (var scope = provider.CreateScope())
+            {
+                var repo = new NotificationsRepository((IScopeAccessor) provider);
+
+                var node = new NodeDto // create bogus item so we can add a notification
+                {
+                    CreateDate = DateTime.Now,
+                    Level = 1,
+                    NodeObjectType = Constants.ObjectTypes.ContentItem,
+                    ParentId = -1,
+                    Path = "-1,123",
+                    SortOrder = 1,
+                    Text = "hello",
+                    Trashed = false,
+                    UniqueId = Guid.NewGuid(),
+                    UserId = Constants.Security.SuperUserId
+                };
+                var result = scope.Database.Insert(node);
+                var entity = Mock.Of<IEntity>(e => e.Id == node.NodeId);
+                var user = Mock.Of<IUser>(e => e.Id == node.UserId);
+
+                var notification = repo.CreateNotification(user, entity, "A");
+
+                Assert.AreEqual("A", notification.Action);
+                Assert.AreEqual(node.NodeId, notification.EntityId);
+                Assert.AreEqual(node.NodeObjectType, notification.EntityType);
+                Assert.AreEqual(node.UserId, notification.UserId);
+            }
+        }
+
+        [Test]
+        public void GetUserNotifications()
+        {
+            var provider = ScopeProvider;
+            using (var scope = provider.CreateScope())
+            {
+                var repo = new NotificationsRepository((IScopeAccessor) provider);
+
+                var userDto = new UserDto { Email = "test", Login = "test", Password = "test", UserName = "test", UserLanguage = "en", CreateDate = DateTime.Now, UpdateDate = DateTime.Now };
+                scope.Database.Insert(userDto);
+
+                var userNew = Mock.Of<IUser>(e => e.Id == userDto.Id);
+                var userAdmin = Mock.Of<IUser>(e => e.Id == Constants.Security.SuperUserId);
+
+                for (var i = 0; i < 10; i++)
+                {
+                    var node = new NodeDto { CreateDate = DateTime.Now, Level = 1, NodeObjectType = Constants.ObjectTypes.ContentItem, ParentId = -1, Path = "-1," + i, SortOrder = 1, Text = "hello" + i, Trashed = false, UniqueId = Guid.NewGuid(), UserId = -1 };
+                    var result = scope.Database.Insert(node);
+                    var entity = Mock.Of<IEntity>(e => e.Id == node.NodeId);
+                    var notification = repo.CreateNotification((i%2 == 0) ? userAdmin : userNew, entity, i.ToString(CultureInfo.InvariantCulture));
+                }
+
+                var notifications = repo.GetUserNotifications(userAdmin);
+
+                Assert.AreEqual(5, notifications.Count());
+            }
+        }
+
+        [Test]
+        public void GetEntityNotifications()
+        {
+            var provider = ScopeProvider;
+            using (var scope = provider.CreateScope())
+            {
+                var repo = new NotificationsRepository((IScopeAccessor) provider);
+
+                var node1 = new NodeDto { CreateDate = DateTime.Now, Level = 1, NodeObjectType = Constants.ObjectTypes.ContentItem, ParentId = -1, Path = "-1,1", SortOrder = 1, Text = "hello1", Trashed = false, UniqueId = Guid.NewGuid(), UserId = -1 };
+                scope.Database.Insert(node1);
+                var entity1 = Mock.Of<IEntity>(e => e.Id == node1.NodeId);
+                var node2 = new NodeDto { CreateDate = DateTime.Now, Level = 1, NodeObjectType = Constants.ObjectTypes.ContentItem, ParentId = -1, Path = "-1,2", SortOrder = 1, Text = "hello2", Trashed = false, UniqueId = Guid.NewGuid(), UserId = -1 };
+                scope.Database.Insert(node2);
+                var entity2 = Mock.Of<IEntity>(e => e.Id == node2.NodeId);
+
+                for (var i = 0; i < 10; i++)
+                {
+                    var userDto = new UserDto { Email = "test" + i, Login = "test" + i, Password = "test", UserName = "test" + i, UserLanguage = "en", CreateDate = DateTime.Now, UpdateDate = DateTime.Now };
+                    scope.Database.Insert(userDto);
+                    var userNew = Mock.Of<IUser>(e => e.Id == userDto.Id);
+                    var notification = repo.CreateNotification(userNew, (i%2 == 0) ? entity1 : entity2, i.ToString(CultureInfo.InvariantCulture));
+                }
+
+                var notifications = repo.GetEntityNotifications(entity1);
+
+                Assert.AreEqual(5, notifications.Count());
+            }
+        }
+
+        [Test]
+        public void Delete_By_Entity()
+        {
+            var provider = ScopeProvider;
+            using (var scope = provider.CreateScope())
+            {
+                var repo = new NotificationsRepository((IScopeAccessor) provider);
+
+                var node1 = new NodeDto { CreateDate = DateTime.Now, Level = 1, NodeObjectType = Constants.ObjectTypes.ContentItem, ParentId = -1, Path = "-1,1", SortOrder = 1, Text = "hello1", Trashed = false, UniqueId = Guid.NewGuid(), UserId = -1 };
+                scope.Database.Insert(node1);
+                var entity1 = Mock.Of<IEntity>(e => e.Id == node1.NodeId);
+                var node2 = new NodeDto { CreateDate = DateTime.Now, Level = 1, NodeObjectType = Constants.ObjectTypes.ContentItem, ParentId = -1, Path = "-1,2", SortOrder = 1, Text = "hello2", Trashed = false, UniqueId = Guid.NewGuid(), UserId = -1 };
+                scope.Database.Insert(node2);
+                var entity2 = Mock.Of<IEntity>(e => e.Id == node2.NodeId);
+
+                for (var i = 0; i < 10; i++)
+                {
+                    var userDto = new UserDto { Email = "test" + i, Login = "test" + i, Password = "test", UserName = "test" + i, UserLanguage = "en", CreateDate = DateTime.Now, UpdateDate = DateTime.Now };
+                    scope.Database.Insert(userDto);
+                    var userNew = Mock.Of<IUser>(e => e.Id == userDto.Id);
+                    var notification = repo.CreateNotification(userNew, (i%2 == 0) ? entity1 : entity2, i.ToString(CultureInfo.InvariantCulture));
+                }
+
+                var delCount = repo.DeleteNotifications(entity1);
+
+                Assert.AreEqual(5, delCount);
+            }
+        }
+
+        [Test]
+        public void Delete_By_User()
+        {
+            var provider = ScopeProvider;
+            using (var scope = provider.CreateScope())
+            {
+                var repo = new NotificationsRepository((IScopeAccessor) provider);
+
+                var userDto = new UserDto { Email = "test", Login = "test", Password = "test", UserName = "test", UserLanguage = "en", CreateDate = DateTime.Now, UpdateDate = DateTime.Now };
+                scope.Database.Insert(userDto);
+
+                var userNew = Mock.Of<IUser>(e => e.Id == userDto.Id);
+                var userAdmin = Mock.Of<IUser>(e => e.Id == Constants.Security.SuperUserId);
+
+                for (var i = 0; i < 10; i++)
+                {
+                    var node = new NodeDto { CreateDate = DateTime.Now, Level = 1, NodeObjectType = Constants.ObjectTypes.ContentItem, ParentId = -1, Path = "-1," + i, SortOrder = 1, Text = "hello" + i, Trashed = false, UniqueId = Guid.NewGuid(), UserId = -1 };
+                    var result = scope.Database.Insert(node);
+                    var entity = Mock.Of<IEntity>(e => e.Id == node.NodeId);
+                    var notification = repo.CreateNotification((i%2 == 0) ? userAdmin : userNew, entity, i.ToString(CultureInfo.InvariantCulture));
+                }
+
+                var delCount = repo.DeleteNotifications(userAdmin);
+
+                Assert.AreEqual(5, delCount);
+            }
+        }
+    }
+}
---
+++ src/Umbraco.Tests.Integration/Umbraco.Infrastructure/Services/SectionServiceTests.cs
@@ -0,0 +1,70 @@
+﻿using System.Linq;
+using System.Threading;
+using NUnit.Framework;
+using Umbraco.Core.Configuration.Models;
+using Umbraco.Core.Models.Membership;
+using Umbraco.Core.Services;
+using Umbraco.Tests.Integration.Testing;
+using Umbraco.Tests.Testing;
+using Umbraco.Web.Services;
+
+namespace Umbraco.Tests.Integration.Umbraco.Infrastructure.Services
+{
+    /// <summary>
+    /// Tests covering the SectionService
+    /// </summary>
+    [TestFixture]
+    [Apartment(ApartmentState.STA)]
+    [UmbracoTest(Database = UmbracoTestOptions.Database.NewSchemaPerTest)]
+    public class SectionServiceTests : UmbracoIntegrationTest
+    {
+        private ISectionService SectionService => GetRequiredService<ISectionService>();
+        private IUserService UserService => GetRequiredService<IUserService>();
+
+        [Test]
+        public void SectionService_Can_Get_Allowed_Sections_For_User()
+        {
+            // Arrange
+            var user = CreateTestUser();
+
+            // Act
+            var result = SectionService.GetAllowedSections(user.Id).ToList();
+
+            // Assert
+            Assert.AreEqual(3, result.Count);
+        }
+
+        private IUser CreateTestUser()
+        {
+            var globalSettings = new GlobalSettings();
+            var user = new User(globalSettings)
+            {
+                Name = "Test user",
+                Username = "testUser",
+                Email = "testuser@test.com",
+            };
+            UserService.Save(user, false);
+
+            var userGroupA = new UserGroup(ShortStringHelper)
+            {
+                Alias = "GroupA",
+                Name = "Group A"
+            };
+            userGroupA.AddAllowedSection("media");
+            userGroupA.AddAllowedSection("settings");
+            // TODO: This is failing the test
+            UserService.Save(userGroupA, new[] { user.Id }, false);
+
+            var userGroupB = new UserGroup(ShortStringHelper)
+            {
+                Alias = "GroupB",
+                Name = "Group B"
+            };
+            userGroupB.AddAllowedSection("settings");
+            userGroupB.AddAllowedSection("member");
+            UserService.Save(userGroupB, new[] { user.Id }, false);
+
+            return UserService.GetUserById(user.Id);
+        }
+    }
+}
