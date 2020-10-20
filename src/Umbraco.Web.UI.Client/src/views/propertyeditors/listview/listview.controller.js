b2f5b4d7afb6acb426f0d103cd65c18b31a3b20c
Evan Moore <evan.moore@cityofnorthbay.ca>
2020-10-20 16:31:23 -0400

    fix ncNodeName filter broken on refresh

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/listview/listview.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/listview/listview.controller.js
@@ -686,15 +686,28 @@ function listViewController($scope, $interpolate, $routeParams, $injector, $time
             }
 
             if (e.nameExp) {
-                var newValue = e.nameExp({ value });
-                if (newValue && (newValue = $.trim(newValue))) {
-                    value = newValue;
-                }
+                value = resolveExpression(e, result, value, alias);
             }
 
             // set what we've got on the result
             result[alias] = value;
         });
+    }
+
+    // an ugly hack to resolve $stateful filters such as ncNodeName
+    // e.nameExp doesn't allow an expression to return a promise so the best thing
+    // i can think of doing is to hold my nose and poll e.nameExp until we get a value
+    function resolveExpression(e, result, expression, alias) {
+        var newValue = e.nameExp({ value: expression });
+        if (newValue === 'Loading...') {
+            $timeout(function () {
+                resolveExpression(e, result, expression, alias);
+            }, 1000);
+        }
+        if (newValue && (newValue = $.trim(newValue))) {
+            result[alias] = newValue;
+        }
+        return newValue;
     }
 
     function isDate(val) {
