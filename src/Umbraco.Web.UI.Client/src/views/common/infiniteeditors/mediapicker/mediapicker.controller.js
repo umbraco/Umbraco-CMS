8c6139dafe5bdb3d67c829b33b86eb543366580d
Matt Darby <hello@matt-darby.co.uk>
2020-10-24 15:20:42 +0100

    Add node parameter to media picker goToStartNode

--- src/Umbraco.Web.UI.Client/src/views/common/infiniteeditors/mediapicker/mediapicker.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/common/infiniteeditors/mediapicker/mediapicker.controller.js
@@ -133,7 +133,8 @@ angular.module("umbraco")
                         entityResource.getById($scope.lastOpenedNode, "media")
                             .then(ensureWithinStartNode, gotoStartNode);
                     } else {
-                        gotoStartNode();
+                        entityResource.getById($scope.startNodeId, "media")
+                            .then(gotoStartNode);
                     }
                 } else {
                     // if a target is specified, go look it up - generally this target will just contain ids not the actual full
@@ -350,7 +351,7 @@ angular.module("umbraco")
                     gotoFolder({ id: $scope.lastOpenedNode || $scope.startNodeId, name: "Media", icon: "icon-folder", path: node.path });
                     return true;
                 } else {
-                    gotoFolder({ id: $scope.startNodeId, name: "Media", icon: "icon-folder" });
+                    gotoFolder({ id: $scope.startNodeId, name: "Media", icon: "icon-folder", path: node.path });
                     return false;
                 }
             }
@@ -366,8 +367,8 @@ angular.module("umbraco")
                 return false;
             }
 
-            function gotoStartNode() {
-                gotoFolder({ id: $scope.startNodeId, name: "Media", icon: "icon-folder" });
+            function gotoStartNode(node) {
+                gotoFolder({ id: $scope.startNodeId, name: "Media", icon: "icon-folder", path: node.path });
             }
 
             function openDetailsDialog() {
