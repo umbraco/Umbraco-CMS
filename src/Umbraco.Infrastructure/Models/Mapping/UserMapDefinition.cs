141d3e750034836c123e508f80dea474a8204a72
Nikolaj <nel@umbraco.dk>
2020-10-28 09:03:58 +0100

    Fix errors from Umbraco.Code.MapAll in UserMapDefinition

--- src/Umbraco.Infrastructure/Models/Mapping/UserMapDefinition.cs
+++ src/Umbraco.Infrastructure/Models/Mapping/UserMapDefinition.cs
@@ -108,6 +108,7 @@ namespace Umbraco.Web.Models.Mapping
         // Umbraco.Code.MapAll -SecurityStamp -Avatar -ProviderUserKey -RawPasswordValue
         // Umbraco.Code.MapAll -RawPasswordAnswerValue -Comments -IsApproved -IsLockedOut -LastLoginDate
         // Umbraco.Code.MapAll -LastPasswordChangeDate -LastLockoutDate -FailedPasswordAttempts
+        // Umbraco.Code.MapAll -PasswordConfiguration
         private void Map(UserInvite source, IUser target, MapperContext context)
         {
             target.Email = source.Email;
@@ -126,6 +127,7 @@ namespace Umbraco.Web.Models.Mapping
         // Umbraco.Code.MapAll -ProviderUserKey -RawPasswordValue -RawPasswordAnswerValue -PasswordQuestion -Comments
         // Umbraco.Code.MapAll -IsApproved -IsLockedOut -LastLoginDate -LastPasswordChangeDate -LastLockoutDate
         // Umbraco.Code.MapAll -FailedPasswordAttempts
+        // Umbraco.Code.MapAll -PasswordConfiguration
         private void Map(UserSave source, IUser target, MapperContext context)
         {
             target.Name = source.Name;
@@ -447,6 +449,6 @@ namespace Umbraco.Web.Models.Mapping
                 Trashed = false,
                 ParentId = -1
             };
-        }       
+        }
     }
 }
