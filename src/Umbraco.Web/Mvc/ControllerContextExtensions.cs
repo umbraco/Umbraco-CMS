1a4a72ee21267f489044b912761ef6e41e2874b5
Bjarke Berg <mail@bergmania.dk>
2020-11-03 14:34:27 +0100

    Cleanup
    Signed-off-by: Bjarke Berg <mail@bergmania.dk>

--- src/Umbraco.Web/Mvc/ControllerContextExtensions.cs
+++ src/Umbraco.Web/Mvc/ControllerContextExtensions.cs
@@ -22,7 +22,6 @@ namespace Umbraco.Web.Mvc
         /// <param name="controllerContext">The controller context.</param>
         /// <param name="dataTokenName">The name of the data token.</param>
         /// <returns>The data token, or null.</returns>
-        /// MIGRATED TO NETCORE AS EXTENSION ON ActionContext
         internal static object GetDataTokenInViewContextHierarchy(this ControllerContext controllerContext, string dataTokenName)
         {
             var context = controllerContext;
