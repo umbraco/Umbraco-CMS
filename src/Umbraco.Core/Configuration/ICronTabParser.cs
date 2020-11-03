5321b761a5d0a8887bac375ddfc4a964462b118e
James Jackson-South <james_south@hotmail.com>
2020-11-03 17:31:05 +0000

    Add stylecop linting and example fix.

--- src/Umbraco.Core/Configuration/ICronTabParser.cs
+++ src/Umbraco.Core/Configuration/ICronTabParser.cs
@@ -1,10 +1,28 @@
-﻿using System;
+﻿// Copyright (c) Umbraco and contributors.
+// See LICENSE for more details.
+
+using System;
 
 namespace Umbraco.Core.Configuration
 {
+    /// <summary>
+    /// Defines the contract for that allows the parsing of chrontab expressions.
+    /// </summary>
     public interface ICronTabParser
     {
+        /// <summary>
+        /// Returns a value indicating whether a given chrontab expression is valid.
+        /// </summary>
+        /// <param name="cronTab">The chrontab expression to parse.</param>
+        /// <returns>The <see cref="bool"/> result.</returns>
         bool IsValidCronTab(string cronTab);
+
+        /// <summary>
+        /// Returns the next occurence for the given chrontab expression from the given time.
+        /// </summary>
+        /// <param name="cronTab">The chrontab expression to parse.</param>
+        /// <param name="time">The date and time to start from.</param>
+        /// <returns>The <see cref="DateTime"/> representing the next occurence.</returns>
         DateTime GetNextOccurrence(string cronTab, DateTime time);
     }
 }
