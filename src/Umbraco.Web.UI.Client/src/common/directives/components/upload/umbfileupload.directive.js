b83dfca5bbd40e741b8b865eff4b92d35c2570c1
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2020-11-10 22:50:51 +0100

    Emit filesSelected on drop

--- src/Umbraco.Web.UI.Client/src/common/directives/components/upload/umbfileupload.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/upload/umbfileupload.directive.js
@@ -10,25 +10,29 @@
 function umbFileUpload() {
     return {
         restrict: "A",
-        scope: true,        //create a new scope
+        scope: true, // create a new scope
         link: function (scope, el, attrs) {
             el.on('change', function (event) {
                 var files = event.target.files;
-                //emit event upward
+                // emit event upward
                 scope.$emit("filesSelected", { files: files });
-                //clear the element value - this allows us to pick the same file again and again
+                // clear the element value - this allows us to pick the same file again and again
                 el.val('');
-            });
-
-            el.on('drag dragstart dragend dragover dragenter dragleave drop', function (e) {
-                e.preventDefault();
-                e.stopPropagation();
+            })
+            .on('drag dragstart dragend dragover dragenter dragleave drop', function (event) {
+                event.preventDefault();
+                event.stopPropagation();
             })
             .on('dragover dragenter', function () {
                 scope.$emit("isDragover", { value: true });
             })
             .on('dragleave dragend drop', function () {
                 scope.$emit("isDragover", { value: false });
+            })
+            .on('drop', function (event) {
+                var files = event.originalEvent.dataTransfer.files;
+
+                scope.$emit("filesSelected", { files: files });
             });
         }
     };
