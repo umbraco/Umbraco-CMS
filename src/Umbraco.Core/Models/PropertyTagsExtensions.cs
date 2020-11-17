cb0734f1c55185a9e06c1d1f6bada77a36598c4d
Elitsa Marinovska <elm@umbraco.dk>
2020-11-17 11:22:56 +0100

    VS couldn't find Composing, so build was failing. Removing the using solves the problem

--- src/Umbraco.Core/Models/PropertyTagsExtensions.cs
+++ src/Umbraco.Core/Models/PropertyTagsExtensions.cs
@@ -2,7 +2,6 @@
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Extensions.Logging;
-using Umbraco.Composing;
 using Umbraco.Core.PropertyEditors;
 using Umbraco.Core.Serialization;
 using Umbraco.Core.Services;
