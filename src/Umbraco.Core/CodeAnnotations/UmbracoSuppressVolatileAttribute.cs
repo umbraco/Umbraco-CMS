b3d8eca53cfa1642472a5d6a5d6eaaf5e5f8a0e1
Mole <nikolajlauridsen@protonmail.ch>
2020-11-25 15:03:00 +0100

    Add comments to VolatileAttributes

--- src/Umbraco.Core/CodeAnnotations/UmbracoSuppressVolatileAttribute.cs
+++ src/Umbraco.Core/CodeAnnotations/UmbracoSuppressVolatileAttribute.cs
@@ -2,6 +2,12 @@
 
 namespace Umbraco.Core.CodeAnnotations
 {
+    /// This attribute is used by the Umbraco.Code package to control the behaviour of the Volatile Analyzer.
+    /// If a user wishes to use a resource marked with the UmbracoVolatileAttribute for testing purposes
+    /// they can mark their assembly with this attribute, and the analyzer will diagnose a warning instead of an error,
+    /// allowing their build to succeed. We don't need to use this in the Umbraco projects because the analyzer has an
+    /// allow list specifically for the Umbraco CMS projects.
+
     /// <summary>
     /// Assembly level attribute used to suppress <see cref="UmbracoVolatileAttribute"/> errors to warnings.
     /// </summary>
--- src/Umbraco.Core/CodeAnnotations/UmbracoVolatileAttribute.cs
+++ src/Umbraco.Core/CodeAnnotations/UmbracoVolatileAttribute.cs
@@ -3,12 +3,13 @@
 namespace Umbraco.Core.CodeAnnotations
 {
     /// <summary>
-    /// An empty attribute which is used as a marker for Roslyn Analyzers.
+    /// An empty attribute which is used as a marker for the Volatile Analyzer contained in the Umbraco.Code package.
     /// </summary>
     /// <remarks>
     /// Any class or method marked with this attribute will throw a Volatile error which can be suppressed
     /// with <see cref="UmbracoSuppressVolatileAttribute"/>, this is intended for classes which are public
-    /// for testing purposes, but should not be accessed otherwise
+    /// for testing purposes, but should not be accessed otherwise.
+    /// The analyzer resides in the Umbraco.Code package, so if that package isn't installed this attribute has no effect.
     /// </remarks>
     public class UmbracoVolatileAttribute : Attribute
     {
