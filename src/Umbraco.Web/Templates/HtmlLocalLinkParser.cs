175f2c9630e4eeedd5138cb29c833f0fc0747564
Joe Glombek <joe@rockdove.uk>
2020-11-29 21:56:21 +0000

    Allow local links and query strings in Markdown editor links

--- src/Umbraco.Web/Templates/HtmlLocalLinkParser.cs
+++ src/Umbraco.Web/Templates/HtmlLocalLinkParser.cs
@@ -13,8 +13,13 @@ namespace Umbraco.Web.Templates
     /// </summary>
     public sealed class HtmlLocalLinkParser
     {
+        private static readonly Regex LocalUdiPattern = new Regex(@"[/]?(?:\{|\%7B)localLink:(?<udi>[a-zA-Z0-9-://]+)(?:\}|\%7D)",
+            RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
 
-        private static readonly Regex LocalLinkPattern = new Regex(@"href=""[/]?(?:\{|\%7B)localLink:([a-zA-Z0-9-://]+)(?:\}|\%7D)",
+        private static readonly Regex LocalLinkPattern = new Regex(@"(?<prefix>href="")" + LocalUdiPattern,
+            RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
+
+        private static readonly Regex LocalLinkMarkdownPattern = new Regex(@"(?:" + LocalLinkPattern + @")|(?:(?<prefix>\[[0-9]+\]:\s*)" + LocalUdiPattern + @")|(?:(?<prefix>\[[^\]]+\]\()" + LocalUdiPattern + @")",
             RegexOptions.IgnoreCase | RegexOptions.IgnorePatternWhitespace);
 
         private readonly IUmbracoContextAccessor _umbracoContextAccessor;
@@ -24,9 +29,9 @@ namespace Umbraco.Web.Templates
             _umbracoContextAccessor = umbracoContextAccessor;
         }
 
-        internal IEnumerable<Udi> FindUdisFromLocalLinks(string text)
+        internal IEnumerable<Udi> FindUdisFromLocalLinks(string text, bool markdown = false)
         {
-            foreach ((int? intId, GuidUdi udi, string tagValue) in FindLocalLinkIds(text))
+            foreach ((int? intId, GuidUdi udi, string tagValue, string format) in FindLocalLinkIds(text, markdown))
             {
                 if (udi != null)
                     yield return udi; // In v8, we only care abuot UDIs
@@ -39,17 +44,17 @@ namespace Umbraco.Web.Templates
         /// <param name="text"></param>
         /// <param name="preview"></param>
         /// <returns></returns>
-        public string EnsureInternalLinks(string text, bool preview)
+        public string EnsureInternalLinks(string text, bool preview, bool markdown = false)
         {
             if (_umbracoContextAccessor.UmbracoContext == null)
                 throw new InvalidOperationException("Could not parse internal links, there is no current UmbracoContext");
 
             if (!preview)
-                return EnsureInternalLinks(text);
+                return EnsureInternalLinksInternal(text, markdown);
 
             using (_umbracoContextAccessor.UmbracoContext.ForcedPreview(preview)) // force for URL provider
             {
-                return EnsureInternalLinks(text);
+                return EnsureInternalLinksInternal(text, markdown);
             }
         }
 
@@ -61,12 +66,23 @@ namespace Umbraco.Web.Templates
         /// <returns></returns>
         public string EnsureInternalLinks(string text)
         {
+            return EnsureInternalLinksInternal(text, false);
+        }
+
+        /// <summary>
+        /// Parses the string looking for the {localLink} syntax and updates them to their correct links.
+        /// </summary>
+        /// <param name="text"></param>
+        /// <param name="urlProvider"></param>
+        /// <returns></returns>
+        private string EnsureInternalLinksInternal(string text, bool markdown)
+        {
             if (_umbracoContextAccessor.UmbracoContext == null)
                 throw new InvalidOperationException("Could not parse internal links, there is no current UmbracoContext");
 
             var urlProvider = _umbracoContextAccessor.UmbracoContext.UrlProvider;
 
-            foreach((int? intId, GuidUdi udi, string tagValue) in FindLocalLinkIds(text))
+            foreach((int? intId, GuidUdi udi, string tagValue, string format) in FindLocalLinkIds(text, markdown))
             {
                 if (udi != null)
                 {
@@ -79,39 +95,40 @@ namespace Umbraco.Web.Templates
                     if (newLink == null)
                         newLink = "#";
 
-                    text = text.Replace(tagValue, "href=\"" + newLink);
+                    text = text.Replace(tagValue, string.Format(format, newLink));
                 }
                 else if (intId.HasValue)
                 {
                     var newLink = urlProvider.GetUrl(intId.Value);
-                    text = text.Replace(tagValue, "href=\"" + newLink);
+                    text = text.Replace(tagValue, string.Format(format, newLink));
                 }
             }
 
             return text;
         }
 
-        private IEnumerable<(int? intId, GuidUdi udi, string tagValue)> FindLocalLinkIds(string text)
+        private IEnumerable<(int? intId, GuidUdi udi, string tagValue, string format)> FindLocalLinkIds(string text, bool markdown)
         {
             // Parse internal links
-            var tags = LocalLinkPattern.Matches(text);
+            var tags = (markdown ? LocalLinkMarkdownPattern : LocalLinkPattern).Matches(text);
             foreach (Match tag in tags)
             {
                 if (tag.Groups.Count > 0)
                 {
-                    var id = tag.Groups[1].Value; //.Remove(tag.Groups[1].Value.Length - 1, 1);
-
+                    var id = tag.Groups["udi"].Value; //.Remove(tag.Groups[1].Value.Length - 1, 1);
+                    var prefix = tag.Groups["prefix"]?.Value;
+                    var format = $"{prefix}{{0}}";
                     //The id could be an int or a UDI
                     if (Udi.TryParse(id, out var udi))
                     {
                         var guidUdi = udi as GuidUdi;
                         if (guidUdi != null)
-                            yield return (null, guidUdi, tag.Value);
+                            yield return (null, guidUdi, tag.Value, format);
                     }
 
                     if (int.TryParse(id, out var intId))
                     {
-                        yield return (intId, null, tag.Value);
+                        yield return (intId, null, tag.Value, format);
                     }
                 }
             }
