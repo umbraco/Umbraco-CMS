22d4712596b6299fa0a47616cda7311986b869d5
Noémi VDP <74990646+noemivdp@users.noreply.github.com>
2020-12-23 10:33:50 +0000

    Add AllowedChildren EventManager - EventArgs

---
+++ src/Umbraco.Web/Editors/AllowedChildrenEventArgs.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Umbraco.Web.Editors
+{
+    public sealed class AllowedChildrenEventArgs<T> : AllowedChildrenEventArgs
+    {
+        private readonly AllowedChildrenEventArgs _baseArgs;
+        private T _model;
+
+        public AllowedChildrenEventArgs(AllowedChildrenEventArgs baseArgs)
+            : base(baseArgs.Model, baseArgs.UmbracoContext, baseArgs.ContentId)
+        {
+            _baseArgs = baseArgs;
+            Model = (T)baseArgs.Model;
+            ContentId = baseArgs.ContentId;
+        }
+
+        public AllowedChildrenEventArgs(T model, UmbracoContext umbracoContext, int? contentId)
+            : base(model, umbracoContext,contentId)
+        {
+            Model = model;
+            ContentId = contentId;
+        }
+
+        public new T Model
+        {
+            get => _model;
+            set
+            {
+                _model = value;
+                if (_baseArgs != null)
+                    _baseArgs.Model = _model;
+            }
+        }
+    }
+
+    public class AllowedChildrenEventArgs : EventArgs
+    {
+        public AllowedChildrenEventArgs(object model, UmbracoContext umbracoContext, int? contentId)
+        {
+            Model = model;
+            ContentId = contentId;
+            UmbracoContext = umbracoContext;
+        }
+
+        public object Model { get; set; }
+        public int? ContentId { get; set; }
+        public UmbracoContext UmbracoContext { get; }
+    }
+}
---
+++ src/Umbraco.Web/Editors/AllowedChildrenEventManager.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Web.Http.Filters;
+using Umbraco.Core.Dashboards;
+using Umbraco.Core.Events;
+using Umbraco.Web.Models.ContentEditing;
+namespace Umbraco.Web.Editors
+{
+    public sealed class AllowedChildrenEventManager
+    {
+        public static event TypedEventHandler<HttpActionExecutedContext, AllowedChildrenEventArgs<System.Collections.Generic.IEnumerable<Umbraco.Web.Models.ContentEditing.ContentTypeBasic>>> SendingAllowedChildrenModel;
+
+        private static void OnSendingAllowedChildrenModel(HttpActionExecutedContext sender, AllowedChildrenEventArgs<System.Collections.Generic.IEnumerable<Umbraco.Web.Models.ContentEditing.ContentTypeBasic>> e)
+        {
+            var handler = SendingAllowedChildrenModel;
+            handler?.Invoke(sender, e);
+        }
+        /// <summary>
+        /// Emit the Event
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        internal static void EmitEvent(HttpActionExecutedContext sender, AllowedChildrenEventArgs e)
+        {
+               OnSendingAllowedChildrenModel(sender, new AllowedChildrenEventArgs<System.Collections.Generic.IEnumerable<Umbraco.Web.Models.ContentEditing.ContentTypeBasic>>(e));
+        }
+    }
+}
