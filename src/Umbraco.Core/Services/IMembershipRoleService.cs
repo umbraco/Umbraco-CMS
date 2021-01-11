5b4f9a5100262a46109c18cdabb4e4b82f35c954
Emma Garland <emmagarland77@gmail.com>
2021-01-11 17:03:34 +0000

    Separated properties by a line

--- src/Umbraco.Core/Services/IMembershipRoleService.cs
+++ src/Umbraco.Core/Services/IMembershipRoleService.cs
@@ -5,26 +5,43 @@ using Umbraco.Core.Persistence.Querying;
 
 namespace Umbraco.Core.Services
 {
-    public interface IMembershipRoleService<out T>
+    public interface IMembershipRoleService<out T> 
         where T : class, IMembershipUser
     {
         void AddRole(string roleName);
+
         IEnumerable<IMemberGroup> GetAllRoles();
+
         IEnumerable<string> GetAllRoles(int memberId);
+
         IEnumerable<string> GetAllRoles(string username);
+
         IEnumerable<int> GetAllRolesIds();
+
         IEnumerable<int> GetAllRolesIds(int memberId);
+
         IEnumerable<int> GetAllRolesIds(string username);
+
         IEnumerable<T> GetMembersInRole(string roleName);
+
         IEnumerable<T> FindMembersInRole(string roleName, string usernameToMatch, StringPropertyMatchType matchType = StringPropertyMatchType.StartsWith);
+
         bool DeleteRole(string roleName, bool throwIfBeingUsed);
+
         void AssignRole(string username, string roleName);
+
         void AssignRoles(string[] usernames, string[] roleNames);
+
         void DissociateRole(string username, string roleName);
+
         void DissociateRoles(string[] usernames, string[] roleNames);
+
         void AssignRole(int memberId, string roleName);
+
         void AssignRoles(int[] memberIds, string[] roleNames);
+
         void DissociateRole(int memberId, string roleName);
+
         void DissociateRoles(int[] memberIds, string[] roleNames);
     }
 }
