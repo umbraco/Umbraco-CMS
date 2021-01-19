acdaba5d479e9619850cb18b57418eb1c84e257f
Shannon <sdeminick@gmail.com>
2021-01-19 19:16:42 +1100

    more cleanup

--- src/Umbraco.ModelsBuilder.Embedded/UmbracoAssemblyLoadContext.cs
+++ src/Umbraco.ModelsBuilder.Embedded/UmbracoAssemblyLoadContext.cs
@@ -1,33 +1,23 @@
-using System;
-using System.Collections.Generic;
 using System.Reflection;
 using System.Runtime.Loader;
-using System.Text;
 
 namespace Umbraco.ModelsBuilder.Embedded
 {
     internal class UmbracoAssemblyLoadContext : AssemblyLoadContext
     {
-        private AssemblyDependencyResolver _resolver;
-
         /// <summary>
+        /// Initializes a new instance of the <see cref="UmbracoAssemblyLoadContext"/> class.
+        /// </summary>
+        /// <remarks>
         /// Collectible AssemblyLoadContext used to load in the compiled generated models.
         /// Must be a collectible assembly in order to be able to be unloaded.
-        /// </summary>
-        public UmbracoAssemblyLoadContext() : base(isCollectible: true)
+        /// </remarks>
+        public UmbracoAssemblyLoadContext()
+            : base(isCollectible: true)
         {
-            //_resolver = new AssemblyDependencyResolver(pluginPath);
         }
 
-        protected override Assembly Load(AssemblyName assemblyName)
-        {
-            //string assemblyPath = _resolver.ResolveAssemblyToPath(assemblyName);
-            //if (assemblyPath != null)
-            //{
-            //    return LoadFromAssemblyPath(assemblyPath);
-            //}
-
-            return null;
-        }
+        // we never load anything directly by assembly name. This method will never be called
+        protected override Assembly Load(AssemblyName assemblyName) => null;
     }
 }
