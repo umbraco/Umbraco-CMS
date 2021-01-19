040fe18282510902b04ba377e2a48380b4fa1735
Nathan Woulfe <nathan@nathanw.com.au>
2021-01-19 12:10:50 +1000

    update UI to add buttons for changing image / editing existing

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/grid/editors/media.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/grid/editors/media.controller.js
@@ -1,10 +1,8 @@
 angular.module("umbraco")
     .controller("Umbraco.PropertyEditors.Grid.MediaController",
-    function ($scope, $timeout, userService, editorService) {
+    function ($scope, userService, editorService, localizationService) {        
         
-        
-        $scope.thumbnailUrl = getThumbnailUrl();
-        
+        $scope.thumbnailUrl = getThumbnailUrl();        
         
         if (!$scope.model.config.startNodeId) {
             if ($scope.model.config.ignoreUserStartNodes === true) {
@@ -12,101 +10,120 @@ angular.module("umbraco")
                 $scope.model.config.startNodeIsVirtual = true;
 
             } else {
-                userService.getCurrentUser().then(function (userData) {
+                userService.getCurrentUser().then(userData => {
                     $scope.model.config.startNodeId = userData.startMediaIds.length !== 1 ? -1 : userData.startMediaIds[0];
                     $scope.model.config.startNodeIsVirtual = userData.startMediaIds.length !== 1;
                 });
             }
         }
-        
-        $scope.setImage = function(){
+
+        $scope.setImage = function() {
             var startNodeId = $scope.model.config && $scope.model.config.startNodeId ? $scope.model.config.startNodeId : undefined;
-            var startNodeIsVirtual = startNodeId ? $scope.model.config.startNodeIsVirtual : undefined;
-            var value = $scope.control.value;
-            var target = value
-                ? {
-                    udi: value.udi,
-                    url: value.image,
-                    image: value.image,
-                    focalPoint: value.focalPoint,
-                    coordinates: value.coordinates
-                }
-                : null;
+
             var mediaPicker = {
                 startNodeId: startNodeId,
-                startNodeIsVirtual: startNodeIsVirtual,
+                startNodeIsVirtual: startNodeId ? $scope.model.config.startNodeIsVirtual : undefined,
                 cropSize: $scope.control.editor.config && $scope.control.editor.config.size ? $scope.control.editor.config.size : undefined,
                 showDetails: true,
                 disableFolderSelect: true,
                 onlyImages: true,
                 dataTypeKey: $scope.model.dataTypeKey,
-                currentTarget: target,
-                submit: function(model) {
-                    var selectedImage = model.selection[0];
-                   
-                    $scope.control.value = {
-                        focalPoint: selectedImage.focalPoint,
-                        coordinates: selectedImage.coordinates,
-                        id: selectedImage.id,
-                        udi: selectedImage.udi,
-                        image: selectedImage.image,
-                        caption: selectedImage.altText
-                    };
-                    
+                submit: model => {
+                    updateControlValue(model.selection[0]);                    
                     editorService.close();
                 },
-                close: function() {
-                    editorService.close();
-                }
-            }
-            
+                close: () => editorService.close()                
+            };
+
             editorService.mediaPicker(mediaPicker);
         };
+
+        $scope.editImage = function() {               
+
+            const mediaCropDetailsConfig = {
+                size: 'small',
+                target: $scope.control.value,
+                submit: model => {
+                    updateControlValue(model.target);
+                    editorService.close();
+                },
+                close: function () {
+                    editorService.close();
+                }
+            };
+
+            localizationService.localize('defaultdialogs_editSelectedMedia').then(value => {
+                mediaCropDetailsConfig.title = value;
+                editorService.mediaCropDetails(mediaCropDetailsConfig);
+            });        
+        }
         
-        $scope.$watch('control.value', function(newValue, oldValue) {
-            if(angular.equals(newValue, oldValue)){
-                return; // simply skip that
-            }
-            
-            $scope.thumbnailUrl = getThumbnailUrl();
-        }, true);
-        
+        /**
+         * 
+         */
         function getThumbnailUrl() {
 
-            if($scope.control.value && $scope.control.value.image) {
+            if ($scope.control.value && $scope.control.value.image) {
                 var url = $scope.control.value.image;
 
-                if($scope.control.editor.config && $scope.control.editor.config.size){
+                if ($scope.control.editor.config && $scope.control.editor.config.size){
                     if ($scope.control.value.coordinates) {
                         // New way, crop by percent must come before width/height.
                         var coords = $scope.control.value.coordinates;
-                        url += "?crop=" + coords.x1 + "," + coords.y1 + "," + coords.x2 + "," + coords.y2 + "&cropmode=percentage";
+                        url += `?crop=${coords.x1},${coords.y1},${coords.x2},${coords.y2}&cropmode=percentage`;
                     } else {
                         // Here in order not to break existing content where focalPoint were used.
                         // For some reason width/height have to come first when mode=crop.
                         if ($scope.control.value.focalPoint) {
-                            url += "?center=" + $scope.control.value.focalPoint.top + "," + $scope.control.value.focalPoint.left;
-                            url += "&mode=crop";
+                            url += `?center=${$scope.control.value.focalPoint.top},${$scope.control.value.focalPoint.left}`;
+                            url += '&mode=crop';
                         } else {
                             // Prevent black padding and no crop when focal point not set / changed from default
-                            url += "?center=0.5,0.5&mode=crop";
+                            url += '?center=0.5,0.5&mode=crop';
                         }
                     }
-                    url += "&width=" + $scope.control.editor.config.size.width;
-                    url += "&height=" + $scope.control.editor.config.size.height;
-                    url += "&animationprocessmode=first";
 
+                    url += '&width=' + $scope.control.editor.config.size.width;
+                    url += '&height=' + $scope.control.editor.config.size.height;
+                    url += '&animationprocessmode=first';
                 }
 
                 // set default size if no crop present (moved from the view)
-                if (url.indexOf('?') == -1)
+                if (url.includes('?') === false)
                 {
-                    url += "?width=800&upscale=false&animationprocessmode=false"
+                    url += '?width=800&upscale=false&animationprocessmode=false'
                 }
+
                 return url;
             }
             
             return null;
-        };
+        }
 
-});
+        /**
+         * 
+         * @param {object} selectedImage 
+         */
+        function updateControlValue(selectedImage) {
+
+            const doGetThumbnail = $scope.control.value.focalPoint !== selectedImage.focalPoint 
+                || $scope.control.value.image !== selectedImage.image;
+
+            // we could apply selectedImage directly to $scope.control.value,
+            // but this allows excluding fields in future if needed
+            $scope.control.value = {
+                focalPoint: selectedImage.focalPoint,
+                coordinates: selectedImage.coordinates,
+                id: selectedImage.id,
+                udi: selectedImage.udi,
+                image: selectedImage.image,
+                caption: selectedImage.caption,
+                altText: selectedImage.altText
+            };
+
+
+            if (doGetThumbnail) {
+                $scope.thumbnailUrl = getThumbnailUrl();
+            }
+        }       
+    });
