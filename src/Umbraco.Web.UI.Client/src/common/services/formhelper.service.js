86752aab9901e8f6196d7e9cb3058fdb3687d661
Niels Lyngsø <nsl@umbraco.com>
2021-01-25 16:53:39 +0100

    stop using .length !== 0 as its causing errors.

--- src/Umbraco.Web.UI.Client/src/common/services/formhelper.service.js
+++ src/Umbraco.Web.UI.Client/src/common/services/formhelper.service.js
@@ -46,7 +46,7 @@ function formHelper(angularHelper, serverValidationManager, notificationsService
             args.scope.$broadcast("formSubmitting", { scope: args.scope, action: args.action });
 
             this.focusOnFirstError(currentForm);
-            
+
             // Some property editors need to perform an action after all property editors have reacted to the formSubmitting.
             args.scope.$broadcast("formSubmittingFinalPhase", { scope: args.scope, action: args.action });
 
@@ -80,18 +80,17 @@ function formHelper(angularHelper, serverValidationManager, notificationsService
          *
          * @description
          * Called by submitForm when a form has been submitted, it will fire a focus on the first found invalid umb-property it finds in the form..
-         * 
+         *
          * @param {object} form Pass in a form object.
          */
         focusOnFirstError: function(form) {
             var invalidNgForms = form.$$element.find(`.umb-property ng-form.ng-invalid, .umb-property-editor ng-form.ng-invalid-required`);
-            var firstInvalidNgForm = invalidNgForms.first();
-
-            if(firstInvalidNgForm.length !== 0) {
-                var focusableFields = [...firstInvalidNgForm.find("umb-range-slider .noUi-handle,input,textarea,select,button")];
-                if(focusableFields.length !== 0) { 
+            if(invalidNgForms !== undefined && invalidNgForms.length > 0) {
+                var firstInvalidNgForm = invalidNgForms.first();
+                var focusableFields = firstInvalidNgForm.find("umb-range-slider .noUi-handle,input,textarea,select,button");
+                if(focusableFields !== undefined) {
                     var firstErrorEl = focusableFields.find(el => el.type !== "hidden" && el.hasAttribute("readonly") === false);
-                    if(firstErrorEl.length !== 0) {
+                    if(firstErrorEl !== undefined) {
                         firstErrorEl.focus();
                     }
                 }
