caa0c25454de18e27758fac95cf3dd0a9ed06958
Shannon <sdeminick@gmail.com>
2021-02-01 17:38:11 +1100

    missing files

--- tests/loadtests/run.ps1
+++ tests/loadtests/run.ps1
@@ -1,8 +1,9 @@
 # Set environment variables for artillery to use:
 
 $Env:U_BASE_URL = "http://localhost:8111/"
-$Env:U_USERNAME = "test@example.com"
+$Env:U_USERNAME = "sdeminick2@gmail.com"
 $Env:U_PASS = "testtesttest"
 
 # Execute artillery
-& artillery run .\general-coverage.yml
+& artillery run "$PSScriptRoot\general-coverage.yml" --output "$PSScriptRoot\report.json"
+& artillery report --output "$PSScriptRoot\report.html" "$PSScriptRoot\report.json"
--- tests/loadtests/umbraco-flow.js
+++ tests/loadtests/umbraco-flow.js
@@ -1,33 +1,55 @@
 // Export methods for Artillery to be able to use
 module.exports = {
     captureXsrf: captureXsrf,
-    sendXsrf: sendXsrf
+    sendXsrf: sendXsrf,
+    beforeScenario: beforeScenario,
+    afterScenario: afterScenario
 }
 
 // TODO: This is purely testing perf counters, you can list all perf counters names
 // by doing this at the cmd prompt: typeperf -qx > all-counters.txt
+// Sometimes this works, sometimes it doesn't, not sure why yet
 
 // This uses this package: https://github.com/markitondemand/node-perfmon
 var perfmon = require('perfmon');
 
+function logPerf(err, data) {
+    console.log("Logging...");
+    if (err) {
+        console.error(err);
+    }
+    else {
+        console.log(data);
+    }
+}
+
 perfmon('\\processor(_total)\\% processor time', function(err, data) {
-	console.log(data);
+    logPerf(err, data);
 });
 
-// This can bet the working set from a process (like iisexpress)
-perfmon('\\Process(iisexpress)\\Private Bytes', function(err, data) {
-	console.log(data);
-});
+function collectPerfStats() {
+    console.log("Collecting...");
 
-// TODO: This should get the managed memory for (all) aspnet apps but I cannot get this to work
-// with iisexpress. Even by configuring AppDomain.MonitoringIsEnabled = true; in c# code
-// and <appDomainResourceMonitoring enabled="true"/> in the applicationhost.config along with
-// C:\Windows\Microsoft.NET\Framework\v4.0.30319\Aspnet.config and
-// C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Aspnet.config
-// This might work in IIS though, I need to check.
-perfmon('\\ASP.NET Applications(__Total__)\\Managed Memory Used (estimated)', function(err, data) {
-	console.log(data);
-});
+    // Can get the process time from a process (like iisexpress)
+    perfmon('\\Process(iisexpress)\\% Processor Time', function (err, data) {
+        logPerf(err, data);
+    });
+
+    // This can bet the working set from a process (like iisexpress)
+    perfmon('\\Process(iisexpress)\\Private Bytes', function (err, data) {
+        logPerf(err, data);
+    });
+
+    // TODO: This should get the managed memory for (all) aspnet apps but I cannot get this to work
+    // with iisexpress. Even by configuring AppDomain.MonitoringIsEnabled = true; in c# code
+    // and <appDomainResourceMonitoring enabled="true"/> in the applicationhost.config along with
+    // C:\Windows\Microsoft.NET\Framework\v4.0.30319\Aspnet.config and
+    // C:\Windows\Microsoft.NET\Framework64\v4.0.30319\Aspnet.config
+    // This might work in IIS though, I need to check.
+    perfmon('\\ASP.NET Applications(__Total__)\\Managed Memory Used (estimated)', function (err, data) {
+        logPerf(err, data);
+    });
+}
 
 /** Called when artillery sends a request */
 function sendXsrf(requestParams, context, ee, next) {
@@ -53,5 +75,15 @@ function captureXsrf(requestParams, response, context, ee, next) {
             }
         }
     }
+    return next();
+}
+
+function beforeScenario(context, ee, next) {
+    collectPerfStats();
+    return next();
+}
+
+function afterScenario(context, ee, next) {
+    collectPerfStats();
     return next();
 }
