3bf36ef7057c3bdbf09ed1cc69ff5c67e145a268
Bjarke Berg <mail@bergmania.dk>
2021-02-03 08:05:16 +0100

    Fixed links. They are not allowed to contain "/"

--- src/Umbraco.Core/Constants-HealthChecks.cs
+++ src/Umbraco.Core/Constants-HealthChecks.cs
@@ -13,43 +13,43 @@
 
             public static class DocumentationLinks
             {
-                public const string SmtpCheck = "https://umbra.co/healthchecks/smtp";
+                public const string SmtpCheck = "https://umbra.co/healthchecks-smtp";
 
                 public static class LiveEnvironment
                 {
 
-                    public const string CompilationDebugCheck = "https://umbra.co/healthchecks/compilation-debug";
+                    public const string CompilationDebugCheck = "https://umbra.co/healthchecks-compilation-debug";
                 }
                 public static class Configuration
                 {
-                    public const string MacroErrorsCheck = "https://umbra.co/healthchecks/macro-errors";
-                    public const string TrySkipIisCustomErrorsCheck = "https://umbra.co/healthchecks/skip-iis-custom-errors";
-                    public const string NotificationEmailCheck = "https://umbra.co/healthchecks/notification-email";
+                    public const string MacroErrorsCheck = "https://umbra.co/healthchecks-macro-errors";
+                    public const string TrySkipIisCustomErrorsCheck = "https://umbra.co/healthchecks-skip-iis-custom-errors";
+                    public const string NotificationEmailCheck = "https://umbra.co/healthchecks-notification-email";
                 }
                 public static class FolderAndFilePermissionsCheck
                 {
 
-                    public const string FileWriting = "https://umbra.co/healthchecks/file-writing";
-                    public const string FolderCreation = "https://umbra.co/healthchecks/folder-creation";
-                    public const string FileWritingForPackages = "https://umbra.co/healthchecks/file-writing-for-packages";
-                    public const string MediaFolderCreation = "https://umbra.co/healthchecks/media-folder-creation";
+                    public const string FileWriting = "https://umbra.co/healthchecks-file-writing";
+                    public const string FolderCreation = "https://umbra.co/healthchecks-folder-creation";
+                    public const string FileWritingForPackages = "https://umbra.co/healthchecks-file-writing-for-packages";
+                    public const string MediaFolderCreation = "https://umbra.co/healthchecks-media-folder-creation";
                 }
 
                 public static class Security
                 {
 
-                    public const string ClickJackingCheck = "https://umbra.co/healthchecks/click-jacking";
-                    public const string HstsCheck = "https://umbra.co/healthchecks/hsts";
-                    public const string NoSniffCheck = "https://umbra.co/healthchecks/no-sniff";
-                    public const string XssProtectionCheck = "https://umbra.co/healthchecks/xss-protection";
-                    public const string ExcessiveHeadersCheck = "https://umbra.co/healthchecks/excessive-headers";
+                    public const string ClickJackingCheck = "https://umbra.co/healthchecks-click-jacking";
+                    public const string HstsCheck = "https://umbra.co/healthchecks-hsts";
+                    public const string NoSniffCheck = "https://umbra.co/healthchecks-no-sniff";
+                    public const string XssProtectionCheck = "https://umbra.co/healthchecks-xss-protection";
+                    public const string ExcessiveHeadersCheck = "https://umbra.co/healthchecks-excessive-headers";
 
                     public static class HttpsCheck
                     {
 
-                        public const string CheckIfCurrentSchemeIsHttps = "https://umbra.co/healthchecks/https-request";
-                        public const string CheckHttpsConfigurationSetting = "https://umbra.co/healthchecks/https-config";
-                        public const string CheckForValidCertificate = "https://umbra.co/healthchecks/valid-certificate";
+                        public const string CheckIfCurrentSchemeIsHttps = "https://umbra.co/healthchecks-https-request";
+                        public const string CheckHttpsConfigurationSetting = "https://umbra.co/healthchecks-https-config";
+                        public const string CheckForValidCertificate = "https://umbra.co/healthchecks-valid-certificate";
                     }
                 }
             }
