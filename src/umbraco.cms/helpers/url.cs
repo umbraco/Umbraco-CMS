c4ddf40fe8da682160a953893d6a0b165097792e
Sebastiaan Janssen <sebastiaan@umbraco.com>
2021-02-04 13:18:49 +0100

    Fix for issue trying to circumvent the url validator

--- src/umbraco.cms/helpers/url.cs
+++ src/umbraco.cms/helpers/url.cs
@@ -38,6 +38,9 @@ namespace umbraco.cms.helpers
                 return false;
             }
 
+            if (url.StartsWith("'"))
+                return false;
+
             if (url.StartsWith("//"))
                 return false;
 
