1ec181d32866ea7864dd43edf8d95b0ee4c5265b
Shannon <sdeminick@gmail.com>
2021-02-08 15:43:06 +1100

    adds notes

--- tests/loadtests/run.ps1
+++ tests/loadtests/run.ps1
@@ -1,4 +1,5 @@
 # Set environment variables for artillery to use:
+# TODO: Cypress generates this information so either use what cypress generates or use the code that it uses
 
 $Env:U_BASE_URL = "http://localhost:8111/"
 $Env:U_USERNAME = "sdeminick2@gmail.com"
