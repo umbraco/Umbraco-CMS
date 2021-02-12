d425fbe05e97db92f9ea33a2e800f95e4bfa2d27
nzdev <834725+nzdev@users.noreply.github.com>
2021-02-12 15:41:46 +1300

    rebuild nucache if serializer changes

---
+++ src/Umbraco.Web/PublishedCache/NuCache/NuCacheSerializerComposer.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core;
+using Umbraco.Core.Composing;
+
+namespace Umbraco.Web.PublishedCache.NuCache
+{
+
+    [ComposeAfter(typeof(NuCacheComposer))]
+    [RuntimeLevel(MinLevel = RuntimeLevel.Run)]
+    public class NuCacheSerializerComposer : ICoreComposer
+    {
+        public void Compose(Composition composition)
+        {
+            composition.Components().Append<NuCacheSerializerComponent>();
+        }
+    }
+}
