4fd5085032bdd68215bf4d2ad310e2d2bf992375
nzdev <834725+nzdev@users.noreply.github.com>
2021-02-20 22:44:53 +1300

    Revert "Remove HtmlSanitizer"
    This reverts commit 80b11616197c16fee11a20468ebde20c66698dd0.

--- src/Umbraco.Web/Services/IconService.cs
+++ src/Umbraco.Web/Services/IconService.cs
@@ -1,6 +1,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using Ganss.XSS;
 using Umbraco.Core;
 using Umbraco.Core.Configuration;
 using Umbraco.Core.IO;
@@ -67,13 +68,20 @@ namespace Umbraco.Web.Services
         /// <returns></returns>
         private IconModel CreateIconModel(string iconName, string iconPath)
         {
+            var sanitizer = new HtmlSanitizer();
+            sanitizer.AllowedAttributes.UnionWith(Constants.SvgSanitizer.Attributes);
+            sanitizer.AllowedCssProperties.UnionWith(Constants.SvgSanitizer.Attributes);
+            sanitizer.AllowedTags.UnionWith(Constants.SvgSanitizer.Tags);
+
             try
             {
                 var svgContent = System.IO.File.ReadAllText(iconPath);
+                var sanitizedString = sanitizer.Sanitize(svgContent);
+
                 var svg = new IconModel
                 {
                     Name = iconName,
-                    SvgString = svgContent
+                    SvgString = sanitizedString
                 };
 
                 return svg;
