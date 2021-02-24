0a2efc252fbd90653ea22ca58206d801b4c050e2
Mole <nikolajlauridsen@protonmail.ch>
2021-02-24 13:20:53 +0100

    Re introduce cookie check, but adding return statement

--- src/Umbraco.Web.BackOffice/Filters/ValidateAngularAntiForgeryTokenAttribute.cs
+++ src/Umbraco.Web.BackOffice/Filters/ValidateAngularAntiForgeryTokenAttribute.cs
@@ -44,6 +44,16 @@ namespace Umbraco.Cms.Web.BackOffice.Filters
 
             public async Task OnActionExecutionAsync(ActionExecutingContext context, ActionExecutionDelegate next)
             {
+                if (context.Controller is ControllerBase controller && controller.User.Identity is ClaimsIdentity userIdentity)
+                {
+                    // if there is not CookiePath claim, then exit
+                    if (userIdentity.HasClaim(x => x.Type == ClaimTypes.CookiePath) == false)
+                    {
+                        await next();
+                        return;
+                    }
+                }
+
                 var cookieToken = _cookieManager.GetCookieValue(Constants.Web.CsrfValidationCookieName);
                 var httpContext = context.HttpContext;
 
