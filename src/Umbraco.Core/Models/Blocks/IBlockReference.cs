163cd10c307c71d02a05d0140479181a57cb7fd9
Callum Whyte <hey@callumwhyte.com>
2021-02-28 22:21:57 +0000

    Adds overload to IBlockReference for content

--- src/Umbraco.Core/Models/Blocks/IBlockReference.cs
+++ src/Umbraco.Core/Models/Blocks/IBlockReference.cs
@@ -34,4 +34,23 @@
         /// </value>
         TSettings Settings { get; }
     }
+
+    /// <summary>
+    /// Represents a data item reference with content and settings for a Block editor implementation.
+    /// </summary>
+    /// <typeparam name="TContent">The type of the content.</typeparam>
+    /// <typeparam name="TSettings">The type of the settings.</typeparam>
+    /// <remarks>
+    /// See: https://github.com/umbraco/rfcs/blob/907f3758cf59a7b6781296a60d57d537b3b60b8c/cms/0011-block-data-structure.md#strongly-typed
+    /// </remarks>
+    public interface IBlockReference<TContent, TSettings> : IBlockReference<TSettings>
+    {
+        /// <summary>
+        /// Gets the content.
+        /// </summary>
+        /// <value>
+        /// The content.
+        /// </value>
+        TContent Content { get; }
+    }
 }
