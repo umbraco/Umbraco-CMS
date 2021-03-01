c1f14de943bf0c6bfc250872152b41bf2cf11a21
Shannon <sdeminick@gmail.com>
2021-03-01 14:20:15 +1100

    fix build

--- src/Umbraco.Tests.UnitTests/Umbraco.Web.Website/Security/UmbracoWebsiteSecurityTests.cs
+++ src/Umbraco.Tests.UnitTests/Umbraco.Web.Website/Security/UmbracoWebsiteSecurityTests.cs
@@ -13,6 +13,7 @@ using Umbraco.Cms.Core.Security;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Core.Strings;
 using Umbraco.Cms.Tests.Common.Builders;
+using Umbraco.Cms.Web.Common.Security;
 using CoreConstants = Umbraco.Cms.Core.Constants;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Web.Website.Security
