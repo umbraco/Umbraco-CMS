88ccb6e8718b0312a6b4b280ce17a76b32f41beb
Bjarke Berg <mail@bergmania.dk>
2021-03-01 20:21:14 +0100

    https://github.com/umbraco/Umbraco-CMS/issues/9861 Fixed test that shows the error. Caches needs to be active, otherwise it do not show the error

--- src/Umbraco.Tests/Membership/MembersMembershipProviderTests.cs
+++ src/Umbraco.Tests/Membership/MembersMembershipProviderTests.cs
@@ -1,5 +1,8 @@
+using System.Collections.Specialized;
+using System.Web.Security;
 using NUnit.Framework;
 using Umbraco.Core;
+using Umbraco.Core.Cache;
 using Umbraco.Core.Composing;
 using Umbraco.Core.Models;
 using Umbraco.Core.Services;
@@ -14,34 +17,69 @@ namespace Umbraco.Tests.Membership
     [UmbracoTest(Database = UmbracoTestOptions.Database.NewSchemaPerFixture)]
     public class MembersMembershipProviderTests : TestWithDatabaseBase
     {
-        private MembersMembershipProvider MembersMembershipProvider => new MembersMembershipProvider(MemberService, MemberTypeService);
+        private MembersMembershipProvider MembersMembershipProvider {get;set; }
 
         public IMemberService MemberService => Current.Factory.GetInstance<IMemberService>();
         public IMemberTypeService MemberTypeService => Current.Factory.GetInstance<IMemberTypeService>();
 
+        public override void SetUp()
+        {
+            base.SetUp();
+
+            MembersMembershipProvider = new MembersMembershipProvider(MemberService, MemberTypeService);
+
+            MembersMembershipProvider.Initialize("test", new NameValueCollection { { "passwordFormat", MembershipPasswordFormat.Clear.ToString() } });
+        }
+
+        protected override AppCaches GetAppCaches()
+        {
+            // this is what's created core web runtime
+            return new AppCaches(
+                new DeepCloneAppCache(new ObjectCacheAppCache()),
+                NoAppCache.Instance,
+                new IsolatedCaches(type => new DeepCloneAppCache(new ObjectCacheAppCache())));
+        }
+
+        /// <summary>
+        /// MembersMembershipProvider.ValidateUser is expected to increase the number of failed attempts and also read that same number.
+        /// </summary>
+        /// <remarks>
+        /// This test requires the caching to be enabled, as it already is correct in the database.
+        /// Shows the error described here: https://github.com/umbraco/Umbraco-CMS/issues/9861
+        /// </remarks>
         [Test]
-        public void ValidateUser_must_increase_failed_attempts()
+        public void ValidateUser__must_lock_out_users_after_max_attempts_of_wrong_password()
         {
             // Arrange
             IMemberType memberType = MockedContentTypes.CreateSimpleMemberType();
             ServiceContext.MemberTypeService.Save(memberType);
-            var member = MockedMember.CreateSimpleMember(memberType, "test", "test@test.com",  "test");
+            var member = MockedMember.CreateSimpleMember(memberType, "test", "test@test.com",  "password","test");
             ServiceContext.MemberService.Save(member);
 
             var wrongPassword = "wrongPassword";
-            var numberOfFailedAttempts = 6;
+            var numberOfFailedAttempts = MembersMembershipProvider.MaxInvalidPasswordAttempts+2;
 
             // Act
-            var attemptsBefore = ServiceContext.MemberService.GetById(member.Id).FailedPasswordAttempts;
+            var memberBefore = ServiceContext.MemberService.GetById(member.Id);
             for (int i = 0; i < numberOfFailedAttempts; i++)
             {
                 MembersMembershipProvider.ValidateUser(member.Username, wrongPassword);
             }
-            var attemptsAfter = ServiceContext.MemberService.GetById(member.Id).FailedPasswordAttempts;
+            var memberAfter = ServiceContext.MemberService.GetById(member.Id);
 
             // Assert
-            Assert.AreEqual(0, attemptsBefore);
-            Assert.AreEqual(numberOfFailedAttempts, attemptsAfter);
+            Assert.Multiple(() =>
+            {
+                Assert.AreEqual(0 , memberBefore.FailedPasswordAttempts, "Expected 0 failed password attempts before");
+                Assert.IsFalse(memberBefore.IsLockedOut, "Expected the member NOT to be locked out before");
+
+                Assert.AreEqual(MembersMembershipProvider.MaxInvalidPasswordAttempts, memberAfter.FailedPasswordAttempts, "Expected exactly the max possible failed password attempts after");
+                Assert.IsTrue(memberAfter.IsLockedOut, "Expected the member to be locked out after");
+
+
+
+            });
+
         }
     }
 }
