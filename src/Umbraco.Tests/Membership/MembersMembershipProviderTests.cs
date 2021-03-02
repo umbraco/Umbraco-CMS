70e4d639dcd3426a1058e5638892354cf9b56a34
Bjarke Berg <mail@bergmania.dk>
2021-03-02 08:23:50 +0100

    https://github.com/umbraco/Umbraco-CMS/issues/9861 More fix of test that shows the error. Caches needs to be active, otherwise it do not show the error

--- src/Umbraco.Tests/Membership/MembersMembershipProviderTests.cs
+++ src/Umbraco.Tests/Membership/MembersMembershipProviderTests.cs
@@ -1,14 +1,20 @@
 using System.Collections.Specialized;
 using System.Web.Security;
+using Moq;
 using NUnit.Framework;
 using Umbraco.Core;
 using Umbraco.Core.Cache;
 using Umbraco.Core.Composing;
+using Umbraco.Core.Logging;
 using Umbraco.Core.Models;
 using Umbraco.Core.Services;
+using Umbraco.Core.Sync;
+using Umbraco.Tests.Integration;
 using Umbraco.Tests.TestHelpers;
 using Umbraco.Tests.TestHelpers.Entities;
 using Umbraco.Tests.Testing;
+using Umbraco.Web;
+using Umbraco.Web.Cache;
 using Umbraco.Web.Security.Providers;
 
 namespace Umbraco.Tests.Membership
@@ -17,10 +23,12 @@ namespace Umbraco.Tests.Membership
     [UmbracoTest(Database = UmbracoTestOptions.Database.NewSchemaPerFixture)]
     public class MembersMembershipProviderTests : TestWithDatabaseBase
     {
-        private MembersMembershipProvider MembersMembershipProvider {get;set; }
+        private MembersMembershipProvider MembersMembershipProvider { get; set; }
+        private IDistributedCacheBinder DistributedCacheBinder { get; set; }
 
         public IMemberService MemberService => Current.Factory.GetInstance<IMemberService>();
         public IMemberTypeService MemberTypeService => Current.Factory.GetInstance<IMemberTypeService>();
+        public ILogger Logger => Current.Factory.GetInstance<ILogger>();
 
         public override void SetUp()
         {
@@ -29,6 +37,30 @@ namespace Umbraco.Tests.Membership
             MembersMembershipProvider = new MembersMembershipProvider(MemberService, MemberTypeService);
 
             MembersMembershipProvider.Initialize("test", new NameValueCollection { { "passwordFormat", MembershipPasswordFormat.Clear.ToString() } });
+
+            DistributedCacheBinder = new DistributedCacheBinder(new DistributedCache(), Mock.Of<IUmbracoContextFactory>(), Logger);
+            DistributedCacheBinder.BindEvents(true);
+        }
+
+        [TearDown]
+        public void Teardown()
+        {
+            DistributedCacheBinder?.UnbindEvents();
+            DistributedCacheBinder = null;
+        }
+
+        protected override void Compose()
+        {
+            base.Compose();
+
+            // the cache refresher component needs to trigger to refresh caches
+            // but then, it requires a lot of plumbing ;(
+            // FIXME: and we cannot inject a DistributedCache yet
+            // so doing all this mess
+            Composition.RegisterUnique<IServerMessenger, ContentEventsTests.LocalServerMessenger>();
+            Composition.RegisterUnique(f => Mock.Of<IServerRegistrar>());
+            Composition.WithCollectionBuilder<CacheRefresherCollectionBuilder>()
+                .Add(() => Composition.TypeLoader.GetCacheRefreshers());
         }
 
         protected override AppCaches GetAppCaches()
@@ -75,9 +107,6 @@ namespace Umbraco.Tests.Membership
 
                 Assert.AreEqual(MembersMembershipProvider.MaxInvalidPasswordAttempts, memberAfter.FailedPasswordAttempts, "Expected exactly the max possible failed password attempts after");
                 Assert.IsTrue(memberAfter.IsLockedOut, "Expected the member to be locked out after");
-
-
-
             });
 
         }
