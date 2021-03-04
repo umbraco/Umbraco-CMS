e0a224e0b0a87d1c29dadd8ecbada0ada2a18fb0
Shannon <sdeminick@gmail.com>
2021-03-04 15:54:51 +1100

    Fixing tests

--- src/Umbraco.Tests.UnitTests/Umbraco.Core/Cache/HttpContextRequestAppCacheTests.cs
+++ src/Umbraco.Tests.UnitTests/Umbraco.Core/Cache/HttpContextRequestAppCacheTests.cs
@@ -1,10 +1,13 @@
 // Copyright (c) Umbraco.
 // See LICENSE for more details.
 
+using System;
 using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
 using Moq;
 using NUnit.Framework;
 using Umbraco.Cms.Core.Cache;
+using static Umbraco.Cms.Core.Cache.HttpContextRequestAppCache;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Cache
 {
@@ -18,6 +21,14 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Cache
         {
             base.Setup();
             var httpContext = new DefaultHttpContext();
+
+            var services = new ServiceCollection();
+            services.AddScoped<RequestLock>();
+            var serviceProviderFactory = new DefaultServiceProviderFactory();            
+            IServiceCollection builder = serviceProviderFactory.CreateBuilder(services);
+            IServiceProvider serviceProvider = serviceProviderFactory.CreateServiceProvider(builder);
+            httpContext.RequestServices = serviceProvider;
+
             _httpContextAccessor = Mock.Of<IHttpContextAccessor>(x => x.HttpContext == httpContext);
             _appCache = new HttpContextRequestAppCache(_httpContextAccessor);
         }
