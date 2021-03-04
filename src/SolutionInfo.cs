475f7e6c3b8a71234151db2265a53885b710f616
Sebastiaan Janssen <sebastiaan@umbraco.com>
2021-03-04 09:40:23 +0100

    Bump version to 8.12.0

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -19,4 +19,4 @@ using System.Resources;
 
 // these are FYI and changed automatically
 [assembly: AssemblyFileVersion("8.12.0")]
-[assembly: AssemblyInformationalVersion("8.12.0-rc")]
+[assembly: AssemblyInformationalVersion("8.12.0")]
