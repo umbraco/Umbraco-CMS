d869e8551065e1d90c9a8699ca040d9457b60f9e
Warren Buckley <warren@umbraco.com>
2021-03-11 15:08:56 +0000

    Remove commented out code - just need to figure out why event is not hit/triggered

--- src/Umbraco.Web/Install/CreateUnattendedUserComponent.cs
+++ src/Umbraco.Web/Install/CreateUnattendedUserComponent.cs
@@ -52,25 +52,25 @@ namespace Umbraco.Web.Install
             if (admin.Email == unattendedEmail) return;
 
             // Everything looks good, create the user
-            //var membershipProvider = Security.MembershipProviderExtensions.GetUsersMembershipProvider();
-            //var superUser = membershipProvider.GetUser(Constants.Security.SuperUserId, true);
-            //if (superUser == null)
-            //{
-            //    throw new InvalidOperationException($"No user found in membership provider with id of {Constants.Security.SuperUserId}.");
-            //}
+            var membershipProvider = Core.Security.MembershipProviderExtensions.GetUsersMembershipProvider();
+            var superUser = membershipProvider.GetUser(Constants.Security.SuperUserId, true);
+            if (superUser == null)
+            {
+                throw new InvalidOperationException($"No user found in membership provider with id of {Constants.Security.SuperUserId}.");
+            }
 
-            //try
-            //{
-            //    var success = superUser.ChangePassword("default", unattendedPassword);
-            //    if (success == false)
-            //    {
-            //        throw new FormatException("Password must be at least " + membershipProvider.MinRequiredPasswordLength + " characters long and contain at least " + membershipProvider.MinRequiredNonAlphanumericCharacters + " symbols");
-            //    }
-            //}
-            //catch (Exception)
-            //{
-            //    throw new FormatException("Password must be at least " + membershipProvider.MinRequiredPasswordLength + " characters long and contain at least " + membershipProvider.MinRequiredNonAlphanumericCharacters + " symbols");
-            //}
+            try
+            {
+                var success = superUser.ChangePassword("default", unattendedPassword);
+                if (success == false)
+                {
+                    throw new FormatException("Password must be at least " + membershipProvider.MinRequiredPasswordLength + " characters long and contain at least " + membershipProvider.MinRequiredNonAlphanumericCharacters + " symbols");
+                }
+            }
+            catch (Exception)
+            {
+                throw new FormatException("Password must be at least " + membershipProvider.MinRequiredPasswordLength + " characters long and contain at least " + membershipProvider.MinRequiredNonAlphanumericCharacters + " symbols");
+            }
 
             // Set name, email & login
             admin.Name = unattendedName.Trim();
