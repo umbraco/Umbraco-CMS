ef27a2c27349143aa3b6516440b7523a49f4c715
Mads Rasmussen <madsr@hey.com>
2021-03-16 21:01:46 +0100

    refactor content type tab to component

---
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/contenttype/umbcontenttypetab.component.js
@@ -0,0 +1,73 @@
+(function () {
+  'use strict';
+
+  /**
+   * A component to render the content type tab
+   */
+  
+  function umbContentTypeTabController() {
+
+    const vm = this;
+
+    vm.removePromptIsVisible = false;
+
+    vm.click = click;
+    vm.removeTab = removeTab;
+    vm.togglePrompt = togglePrompt;
+    vm.hidePrompt = hidePrompt;
+    vm.whenFocusName = whenFocusName;
+    vm.whenFocus = whenFocus;
+
+    function togglePrompt () {
+      vm.removePromptIsVisible = !vm.removePromptIsVisible;
+    }
+
+    function hidePrompt () {
+      vm.removePromptIsVisible = false;
+    }
+
+    function click () {
+      if (vm.onClick) {
+        vm.onClick({ tab: vm.tab });
+      }
+    }
+
+    function removeTab () {
+      if (vm.onRemove) {
+        vm.onRemove({ tab: vm.tab });
+        vm.removePromptIsVisible = false;
+      }
+    }
+
+    function whenFocusName () {
+      if (vm.onFocusName) {
+        vm.onFocusName();
+      }
+    }
+
+    function whenFocus () {
+      if (vm.onFocus) {
+        vm.onFocus();
+      }
+    }
+  }
+
+  const umbContentTypeTabComponent = {
+    templateUrl: 'views/components/contenttype/umb-content-type-tab.html',
+    controllerAs: 'vm',
+    transclude: true,
+    bindings: {
+      tab: '<',
+      onClick: '&',
+      isOpen: '<',
+      allowRemove: '<',
+      onRemove: '&',
+      sorting: '<',
+      onFocusName: '&',
+      onFocus: '&'
+    },
+    controller: umbContentTypeTabController
+  };
+
+  angular.module('umbraco.directives').component('umbContentTypeTab', umbContentTypeTabComponent);
+})();
