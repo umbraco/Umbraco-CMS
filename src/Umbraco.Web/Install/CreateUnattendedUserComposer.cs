fa125cdc33f5761630cf561521091afefa69d2dd
Warren Buckley <warren@umbraco.com>
2021-03-16 12:59:43 +0000

    Remove component based event - Component were only initialized after DB creation

--- src/Umbraco.Web/Install/CreateUnattendedUserComposer.cs
+++
@@ -1,7 +0,0 @@
-﻿using Umbraco.Core.Composing;
-
-namespace Umbraco.Web.Install
-{
-    public class CreateUnattendUserComposer : ComponentComposer<CreateUnattendUserComponent>, ICoreComposer
-    { }
-}
