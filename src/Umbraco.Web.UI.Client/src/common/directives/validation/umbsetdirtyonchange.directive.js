a23e205ac413be7d73a0e6894300cd6825d4d545
Shannon <sdeminick@gmail.com>
2021-03-19 18:56:30 +1100

    Fixes media picker to ensure that it has an ngModel that can be monitored for changes

--- src/Umbraco.Web.UI.Client/src/common/directives/validation/umbsetdirtyonchange.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/validation/umbsetdirtyonchange.directive.js
@@ -1,21 +1,40 @@
 (function() {
     'use strict';
 
+    /**
+    * @ngdoc directive
+    * @name umbraco.directives.directive:umbSetDirtyOnChange
+    * @restrict A
+    * @description Useful for editors that do not have an html input field that tracks the model with ngModel
+    * which responds to user input. This allows having a hidden html input field (which prohibits user interaction)
+    * for being flagged as dirty if it's value dynamically changes (i.e. with 2 way binding). The other use for this
+    * is when no ngModel exists at all but you need to force the editor's current ngForm to be set as dirty based
+    * on dynamic user interaction.
+    **/
     function SetDirtyOnChange() {
 
         function link(scope, el, attr, ctrls) {
 
             var formCtrl = ctrls[0];
+            var ngModel = ctrls.length > 1 ? ctrls[1] : null;
             
-            if (ctrls.length > 1 && ctrls[1]) {
+            if (ngModel) {
                 //if an ngModel is supplied, assign a render function which is called when the model is changed
-                var modelCtrl = ctrls[1];
-                var oldRender = modelCtrl.$render;
-                modelCtrl.$render = function () {
-                    formCtrl.$setDirty();
+                let origRender = ngModel.$render;                
+                let bindCount = 0;
+
+                ngModel.$render = function () {
+
+                    // set dirty only after init bind
+                    if (bindCount > 0) {
+                        ngModel.$setDirty();
+                    }
+
+                    bindCount++;
+
                     //call any previously set render method
-                    if (oldRender) {
-                        oldRender();
+                    if (origRender) {
+                        origRender.apply(ngModel);
                     }
                 }
             }
