24fbfbe91577788e137beaec9211bf8eebbf59bf
Shannon <sdeminick@gmail.com>
2021-03-25 17:24:51 +1100

    remove debug output, revert changes to umbsetdirtyonchange.directive since we don't need it

--- src/Umbraco.Web.UI.Client/src/common/directives/validation/umbsetdirtyonchange.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/validation/umbsetdirtyonchange.directive.js
@@ -1,44 +1,22 @@
 (function() {
     'use strict';
 
-    /**
-    * @ngdoc directive
-    * @name umbraco.directives.directive:umbSetDirtyOnChange
-    * @restrict A
-    * @description Useful for editors that do not have an html input field that tracks the model with ngModel
-    * which responds to user input. This allows having a hidden html input field (which prohibits user interaction)
-    * for being flagged as dirty if it's value dynamically changes (i.e. with 2 way binding). The other use for this
-    * is when no ngModel exists at all but you need to force the editor's current ngForm to be set as dirty based
-    * on dynamic user interaction.
-    **/
     function SetDirtyOnChange() {
 
         function link(scope, el, attr, ctrls) {
 
             var formCtrl = ctrls[0];
-            var ngModel = ctrls.length > 1 ? ctrls[1] : null;
             
-            if (ngModel) {
-                // TODO: Should we validate that this is a hidden input? It should only be used with that else it doesn't make sense.
-
+            if (ctrls.length > 1 && ctrls[1]) {
                 //if an ngModel is supplied, assign a render function which is called when the model is changed
-                let origRender = ngModel.$render;                
-                let bindCount = 0;
-                let viewValue = null;
-
-                ngModel.$render = function () {
-
-                    // set dirty only after init bind
-                    if (bindCount > 0 && viewValue != ngModel.$viewValue) {
-                        ngModel.$setDirty();
+                var modelCtrl = ctrls[1];
+                var oldRender = modelCtrl.$render;
+                modelCtrl.$render = function () {
+                    formCtrl.$setDirty();
+                    //call any previously set render method
+                    if (oldRender) {
+                        oldRender();
                     }
-                    else {
-                        viewValue = ngModel.$viewValue;
-                        bindCount++;
-                    }
-
-                    //call the previously set render method
-                    origRender.apply(ngModel);
                 }
             }
             else {
