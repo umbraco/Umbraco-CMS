03782bca866460ea71f029007a30546e88c22cd6
Warren Buckley <warren@umbraco.com>
2021-03-26 10:43:07 +0000

    New generic Deleting Notification to match the DeletedNotification and Saving/Saved ones

---
+++ src/Umbraco.Core/Events/DeletingNotification.cs
@@ -0,0 +1,17 @@
+using System.Collections.Generic;
+
+namespace Umbraco.Cms.Core.Events
+{
+    public abstract class DeletingNotification<T> : CancelableEnumerableObjectNotification<T>
+    {
+        protected DeletingNotification(T target, EventMessages messages) : base(target, messages)
+        {
+        }
+
+        protected DeletingNotification(IEnumerable<T> target, EventMessages messages) : base(target, messages)
+        {
+        }
+
+        public IEnumerable<T> DeletedEntities => Target;
+    }
+}
