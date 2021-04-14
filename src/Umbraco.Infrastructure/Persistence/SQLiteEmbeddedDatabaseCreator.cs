d389e5a000fe4dd6434ee9627277c322faba0d60
Warren Buckley <warren@umbraco.com>
2021-04-14 14:15:58 +0100

    Not 100% sure we will need to create the DB as SQLite typically creates the file when opening the connection

--- src/Umbraco.Infrastructure/Persistence/SQLiteEmbeddedDatabaseCreator.cs
+++ src/Umbraco.Infrastructure/Persistence/SQLiteEmbeddedDatabaseCreator.cs
@@ -1,38 +1,32 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+using System.IO;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Hosting;
 
 namespace Umbraco.Cms.Infrastructure.Persistence
 {
     public class SQLiteEmbeddedDatabaseCreator : IEmbeddedDatabaseCreator
     {
+        private readonly IHostingEnvironment _hostingEnvironment;
+
+        public SQLiteEmbeddedDatabaseCreator(IHostingEnvironment hostingEnvironment)
+        {
+            _hostingEnvironment = hostingEnvironment;
+        }
+
         public string ProviderName => Cms.Core.Constants.DatabaseProviders.SQLite;
 
-        public string ConnectionString { get; set; } = "Data Source=C:/Application.db;";
+        public string ConnectionString { get; set; }
 
         public void Create()
         {
-            // Do we need to do anything as SQLite will create file if not exist it seems
+            // NOTE: Not sure we need to do anything as SQLite will create file if it does not exist when opening connection
 
-            // Parse Connection String
-            var connection = new Microsoft.Data.Sqlite.SqliteConnectionStringBuilder(ConnectionString);
-            var dataSource = connection.DataSource; // Could be c:/something.db OR ./Something.db
-
-
-
-            // Do we just create a file on disk in the connection string location?
-
-            // Use DB in project directory.  If it does not exist, create it:
-            //var connectionStringBuilder = new Microsoft.Data.Sqlite.SqliteConnectionStringBuilder
+            // Create file on disk from standard path....
+            //var path = _hostingEnvironment.MapPathContentRoot(Path.Combine(Constants.SystemDirectories.Data, "Umbraco.db"));
+            //if (File.Exists(path) == false)
             //{
-            //    DataSource = "./SqliteDB.db"
-            //};
-
-
-
-            // SQL CE example...
-            //var engine = new System.Data.SqlServerCe.SqlCeEngine(ConnectionString);
-            //engine.CreateDatabase();
+            //    File.Create(path);
+            //}
         }
     }
 }
