4f0d4d1fcd067dead721dc7109a680f88a14aae9
Kenn Jacobsen <kja@vertica.dk>
2021-04-27 10:36:28 +0200

    Remove a bunch of script related, unreferenced methods from file service and script repository (#9878)
    Co-authored-by: Bjarke Berg <mail@bergmania.dk>

--- src/Umbraco.Core/Persistence/Repositories/IScriptRepository.cs
+++ src/Umbraco.Core/Persistence/Repositories/IScriptRepository.cs
@@ -1,15 +1,9 @@
-﻿using System.IO;
 using Umbraco.Cms.Core.Models;
 
 namespace Umbraco.Cms.Core.Persistence.Repositories
 {
     public interface IScriptRepository : IReadRepository<string, IScript>, IWriteRepository<IScript>
     {
-        bool ValidateScript(IScript script);
-        Stream GetFileContentStream(string filepath);
-        void SetFileContent(string filepath, Stream content);
-        long GetFileSize(string filepath);
-
         void AddFolder(string folderPath);
         void DeleteFolder(string folderPath);
     }
