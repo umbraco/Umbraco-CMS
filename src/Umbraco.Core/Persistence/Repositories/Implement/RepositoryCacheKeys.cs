6a1d6be018a13227f9e82ad5993a7c43e99a297c
Nikolaj <nel@umbraco.dk>
2021-04-29 08:07:14 +0200

    Add GetKey<T, TId>

--- src/Umbraco.Core/Persistence/Repositories/Implement/RepositoryCacheKeys.cs
+++ src/Umbraco.Core/Persistence/Repositories/Implement/RepositoryCacheKeys.cs
@@ -8,14 +8,30 @@ namespace Umbraco.Core.Persistence.Repositories.Implement
     /// </summary>
     internal static class RepositoryCacheKeys
     {
-        private static readonly Dictionary<Type, string> Keys = new Dictionary<Type, string>();
+        private static readonly Dictionary<Type, string> s_keys = new Dictionary<Type, string>();
 
         public static string GetKey<T>()
         {
             var type = typeof(T);
-            return Keys.TryGetValue(type, out var key) ? key : (Keys[type] = "uRepo_" + type.Name + "_");
+            return s_keys.TryGetValue(type, out var key) ? key : (s_keys[type] = "uRepo_" + type.Name + "_");
         }
 
-        public static string GetKey<T>(object id) => GetKey<T>() + id;
+        [Obsolete("Use GetKey<T, TId> instead")]
+        public static string GetKey<T>(object id) => GetKey<T>() + id.ToString().ToUpperInvariant();
+
+        public static string GetKey<T, TId>(TId id)
+        {
+            if (EqualityComparer<TId>.Default.Equals(id, default))
+            {
+                return string.Empty;
+            }
+
+            if (typeof(TId).IsValueType)
+            {
+                return GetKey<T>() + id;
+            }
+
+            return GetKey<T>() + id.ToString().ToUpperInvariant();
+        }
     }
 }
