7ab09cb4045b92b4832450cb39c7c9e2af686fa4
Niels Lyngsø <nsl@umbraco.dk>
2021-05-04 16:58:31 +0200

    Ensure BlockObjects have references to a working current property editor. (#10195)
    Co-authored-by: Niels Lyngsø <nsl@umbraco.com>

--- src/Umbraco.Web.UI.Client/src/common/directives/components/editor/umbeditors.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/editor/umbeditors.directive.js
@@ -135,11 +135,11 @@
     }
 
     // This directive allows for us to run a custom $compile for the view within the repeater which allows
-    // us to maintain a $scope hierarchy with the rendered view based on the $scope that initiated the 
+    // us to maintain a $scope hierarchy with the rendered view based on the $scope that initiated the
     // infinite editing. The retain the $scope hiearchy a special $parentScope property is passed in to the model.
     function EditorRepeaterDirective($http, $templateCache, $compile, angularHelper) {
-        function link(scope, el, attr, ctrl) {
-            
+        function link(scope, el) {
+
             var editor = scope && scope.$parent ? scope.$parent.model : null;
             if (!editor) {
                 return;
