130be8b303619e8c3f46ef0de1f91a9a31b62f4a
Andy Butland <abutland73@gmail.com>
2021-05-17 12:49:08 +0200

    Added comment explaining use of ShouldSerialize... method in ArtifactBase.

--- src/Umbraco.Core/Deploy/ArtifactBase.cs
+++ src/Umbraco.Core/Deploy/ArtifactBase.cs
@@ -33,6 +33,14 @@ namespace Umbraco.Cms.Core.Deploy
 
         public string Checksum => _checksum.Value;
 
+        /// <summary>
+        /// Prevents the <see cref="Checksum" /> property from being serialized.
+        /// </summary>
+        /// <remarks>
+        /// Note that we can't use <see cref="NonSerializedAttribute"/> here as that works only on fields, not properties.  And we want to avoid using [JsonIgnore]
+        /// as that would require an external dependency in Umbraco.Cms.Core.
+        /// So using this method of excluding properties from serialized data, documented here: https://www.newtonsoft.com/json/help/html/ConditionalProperties.htm
+        /// </remarks>
         public bool ShouldSerializeChecksum() => false;
 
         public IEnumerable<ArtifactDependency> Dependencies
