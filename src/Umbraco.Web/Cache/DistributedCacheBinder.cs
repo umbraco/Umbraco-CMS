348d1676cd7b82a70f82496619ad34722c0bdadf
Andy Butland <abutland73@gmail.com>
2021-05-29 07:29:00 +0200

    Handle only single content type event to avoid reloading the content and media cache multiple times after a deployment containing more than one changed document type.

--- src/Umbraco.Web/Cache/DistributedCacheBinder.cs
+++ src/Umbraco.Web/Cache/DistributedCacheBinder.cs
@@ -61,11 +61,15 @@ namespace Umbraco.Web.Cache
         /// <inheritdoc />
         public void HandleEvents(IEnumerable<IEventDefinition> events)
         {
-            // ensure we run with an UmbracoContext, because this may run in a background task,
-            // yet developers may be using the 'current' UmbracoContext in the event handlers
+            // Ensure we run with an UmbracoContext, because this may run in a background task,
+            // yet developers may be using the 'current' UmbracoContext in the event handlers.
             using (_umbracoContextFactory.EnsureUmbracoContext())
             {
-                foreach (var e in events)
+                // When it comes to content types types, a change to any single one will trigger a reload of the content and media caches.
+                // As far as I (AB) can tell, there's no type specific logic here, they all clear caches for all content types, and trigger a reload of all content and media.
+                // We also have events registered for Changed and Saved, which do the same thing, so really only need one of these.
+                // Hence if we have more than one document or media types, we can and should only handle one of the events for one, to avoid repeated cache reloads.
+                foreach (var e in GetReducedEventList(events))
                 {
                     var handler = FindHandler(e);
                     if (handler == null)
@@ -79,6 +83,50 @@ namespace Umbraco.Web.Cache
                     handler.Invoke(this, new[] { e.Sender, e.Args });
                 }
             }
+        }
+
+        // Internal for tests
+        internal static IEnumerable<IEventDefinition> GetReducedEventList(IEnumerable<IEventDefinition> events)
+        {
+            var reducedEvents = new List<IEventDefinition>();
+
+            var gotDoumentType = false;
+            var gotMediaType = false;
+            var gotMemberType = false;
+
+            foreach (var evt in events)
+            {
+                if (evt.Sender.ToString().Contains(nameof(Core.Services.Implement.ContentTypeService)))
+                {
+                    if (gotDoumentType == false)
+                    {
+                        reducedEvents.Add(evt);
+                        gotDoumentType = true;
+                    }
+                }
+                else if (evt.Sender.ToString().Contains(nameof(Core.Services.Implement.MediaTypeService)))
+                {
+                    if (gotMediaType == false)
+                    {
+                        reducedEvents.Add(evt);
+                        gotMediaType = true;
+                    }
+                }
+                else if (evt.Sender.ToString().Contains(nameof(Core.Services.Implement.MemberTypeService)))
+                {
+                    if (gotMemberType == false)
+                    {
+                        reducedEvents.Add(evt);
+                        gotMemberType = true;
+                    }
+                }
+                else
+                {
+                    reducedEvents.Add(evt);
+                }
+            }
+
+            return reducedEvents;
         }
     }
 }
