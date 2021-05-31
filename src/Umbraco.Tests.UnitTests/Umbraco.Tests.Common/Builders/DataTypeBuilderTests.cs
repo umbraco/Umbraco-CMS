165e5de097928c9ec27f43802d243a08c01d1372
Andy Butland <abutland73@gmail.com>
2021-05-31 17:41:45 +0200

    Adds fields to unit test model builder for data editors.

--- src/Umbraco.Tests.UnitTests/Umbraco.Tests.Common/Builders/DataTypeBuilderTests.cs
+++ src/Umbraco.Tests.UnitTests/Umbraco.Tests.Common/Builders/DataTypeBuilderTests.cs
@@ -1,6 +1,7 @@
 // Copyright (c) Umbraco.
 // See LICENSE for more details.
 
+using System.Collections.Generic;
 using NUnit.Framework;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Tests.Common.Builders;
@@ -16,16 +17,31 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Tests.Common.Builders
         {
             // Arrange
             const int testId = 3123;
+            const string testEditorAlias = "testAlias";
+            const string testEditorName = "Test Name";
 
             var builder = new DataTypeBuilder();
 
             // Act
             DataType dataType = builder
                 .WithId(testId)
+                .AddEditor()
+                    .WithAlias(testEditorAlias)
+                    .WithName(testEditorName)
+                    .WithDefaultConfiguration(new Dictionary<string, object>
+                        {
+                            { "value1", "value1" },
+                            { "value2", "value2" },
+                        })
+                    .Done()
                 .Build();
 
             // Assert
             Assert.AreEqual(testId, dataType.Id);
+            Assert.AreEqual(testEditorAlias, dataType.Editor.Alias);
+            Assert.AreEqual(testEditorName, dataType.Editor.Name);
+            Assert.AreEqual(2, dataType.Editor.DefaultConfiguration.Count);
+            Assert.AreEqual("value1", dataType.Editor.DefaultConfiguration["value1"]);
         }
     }
 }
