e379a8fd44f14e8a3f2270b92c9fc16b014cb83a
Bjarke Berg <mail@bergmania.dk>
2021-07-06 21:20:31 +0200

    Update src/Umbraco.Infrastructure/Persistence/UmbracoDatabaseFactory.cs

--- src/Umbraco.Infrastructure/Persistence/UmbracoDatabaseFactory.cs
+++ src/Umbraco.Infrastructure/Persistence/UmbracoDatabaseFactory.cs
@@ -280,7 +280,7 @@ namespace Umbraco.Cms.Infrastructure.Persistence
             // add all registered mappers for NPoco
             _pocoMappers.AddRange(_npocoMappers);
 
-            _pocoMappers.AddRange(_npocoMappers);
+            _pocoMappers.AddRange(_dbProviderFactoryCreator.ProviderSpecificMappers(_providerName));
 
             var factory = new FluentPocoDataFactory(GetPocoDataFactoryResolver);
             _pocoDataFactory = factory;
