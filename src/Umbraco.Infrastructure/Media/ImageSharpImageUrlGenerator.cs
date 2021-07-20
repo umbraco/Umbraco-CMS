238ac54d506bf10ac6767cc74c3e7c016e3c56dd
Jeavon Leopold <jeavon@crumpled-dog.com>
2021-07-20 10:31:51 +0100

    Replace image processor commands with image sharp web commands

--- src/Umbraco.Infrastructure/Media/ImageSharpImageUrlGenerator.cs
+++ src/Umbraco.Infrastructure/Media/ImageSharpImageUrlGenerator.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections.Generic;
+using System.Collections.Generic;
 using System.Globalization;
 using System.Text;
 using Umbraco.Cms.Core.Media;
@@ -15,48 +15,53 @@ namespace Umbraco.Cms.Infrastructure.Media
         {
             if (options == null) return null;
 
-            var imageProcessorUrl = new StringBuilder(options.ImageUrl ?? string.Empty);
+            var imageSharpWebUrl = new StringBuilder(options.ImageUrl ?? string.Empty);
 
-            if (options.FocalPoint != null) AppendFocalPoint(imageProcessorUrl, options);
-            else if (options.Crop != null) AppendCrop(imageProcessorUrl, options);
-            else if (options.DefaultCrop) imageProcessorUrl.Append("?anchor=center&mode=crop");
+            if (options.FocalPoint != null) AppendFocalPoint(imageSharpWebUrl, options);
+            else if (options.Crop != null) AppendCrop(imageSharpWebUrl, options);
+            else if (options.DefaultCrop) imageSharpWebUrl.Append("?ranchor=center&rmode=crop");
             else
             {
-                imageProcessorUrl.Append("?mode=").Append((options.ImageCropMode ?? ImageCropMode.Crop).ToString().ToLower());
+                imageSharpWebUrl.Append("?rmode=").Append((options.ImageCropMode ?? ImageCropMode.Crop).ToString().ToLower());
 
-                if (options.ImageCropAnchor != null) imageProcessorUrl.Append("&anchor=").Append(options.ImageCropAnchor.ToString().ToLower());
+                if (options.ImageCropAnchor != null) imageSharpWebUrl.Append("&ranchor=").Append(options.ImageCropAnchor.ToString().ToLower());
             }
 
             var hasFormat = options.FurtherOptions != null && options.FurtherOptions.InvariantContains("&format=");
 
             //Only put quality here, if we don't have a format specified.
             //Otherwise we need to put quality at the end to avoid it being overridden by the format.
-            if (options.Quality.HasValue && hasFormat == false) imageProcessorUrl.Append("&quality=").Append(options.Quality);
-            if (options.HeightRatio.HasValue) imageProcessorUrl.Append("&heightratio=").Append(options.HeightRatio.Value.ToString(CultureInfo.InvariantCulture));
-            if (options.WidthRatio.HasValue) imageProcessorUrl.Append("&widthratio=").Append(options.WidthRatio.Value.ToString(CultureInfo.InvariantCulture));
-            if (options.Width.HasValue) imageProcessorUrl.Append("&width=").Append(options.Width);
-            if (options.Height.HasValue) imageProcessorUrl.Append("&height=").Append(options.Height);
-            if (options.UpScale == false) imageProcessorUrl.Append("&upscale=false");
-            if (!string.IsNullOrWhiteSpace(options.AnimationProcessMode)) imageProcessorUrl.Append("&animationprocessmode=").Append(options.AnimationProcessMode);
-            if (!string.IsNullOrWhiteSpace(options.FurtherOptions)) imageProcessorUrl.Append(options.FurtherOptions);
+            if (options.Quality.HasValue && hasFormat == false) imageSharpWebUrl.Append("&quality=").Append(options.Quality);
+
+            // ** TO DO **
+            if (options.HeightRatio.HasValue) imageSharpWebUrl.Append("&heightratio=").Append(options.HeightRatio.Value.ToString(CultureInfo.InvariantCulture));
+            // ** TO DO **
+            if (options.WidthRatio.HasValue) imageSharpWebUrl.Append("&widthratio=").Append(options.WidthRatio.Value.ToString(CultureInfo.InvariantCulture));
+
+            if (options.Width.HasValue) imageSharpWebUrl.Append("&width=").Append(options.Width);
+            if (options.Height.HasValue) imageSharpWebUrl.Append("&height=").Append(options.Height);
+            if (options.UpScale == false) imageSharpWebUrl.Append("&upscale=false");
+            if (!string.IsNullOrWhiteSpace(options.AnimationProcessMode)) imageSharpWebUrl.Append("&animationprocessmode=").Append(options.AnimationProcessMode);
+            if (!string.IsNullOrWhiteSpace(options.FurtherOptions)) imageSharpWebUrl.Append(options.FurtherOptions);
 
             //If furtherOptions contains a format, we need to put the quality after the format.
-            if (options.Quality.HasValue && hasFormat) imageProcessorUrl.Append("&quality=").Append(options.Quality);
-            if (!string.IsNullOrWhiteSpace(options.CacheBusterValue)) imageProcessorUrl.Append("&rnd=").Append(options.CacheBusterValue);
+            if (options.Quality.HasValue && hasFormat) imageSharpWebUrl.Append("&quality=").Append(options.Quality);
+            if (!string.IsNullOrWhiteSpace(options.CacheBusterValue)) imageSharpWebUrl.Append("&rnd=").Append(options.CacheBusterValue);
 
-            return imageProcessorUrl.ToString();
+            return imageSharpWebUrl.ToString();
         }
 
         private void AppendFocalPoint(StringBuilder imageProcessorUrl, ImageUrlGenerationOptions options)
         {
-            imageProcessorUrl.Append("?center=");
+            imageProcessorUrl.Append("?rxy=");
             imageProcessorUrl.Append(options.FocalPoint.Top.ToString(CultureInfo.InvariantCulture)).Append(",");
             imageProcessorUrl.Append(options.FocalPoint.Left.ToString(CultureInfo.InvariantCulture));
-            imageProcessorUrl.Append("&mode=crop");
+            imageProcessorUrl.Append("&rmode=crop");
         }
 
         private void AppendCrop(StringBuilder imageProcessorUrl, ImageUrlGenerationOptions options)
         {
+            // ** TO DO **
             imageProcessorUrl.Append("?crop=");
             imageProcessorUrl.Append(options.Crop.X1.ToString(CultureInfo.InvariantCulture)).Append(",");
             imageProcessorUrl.Append(options.Crop.Y1.ToString(CultureInfo.InvariantCulture)).Append(",");
