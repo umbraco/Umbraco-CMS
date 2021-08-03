733e6c04105723b386fbc7f791eaf7779fb0e6f3
Ronald Barendse <ronald@barend.se>
2021-08-03 22:59:27 +0200

    Update migration to use AddColumn

--- src/Umbraco.Core/Migrations/Upgrade/V_8_16_0/AddDomainSortOrder.cs
+++ src/Umbraco.Core/Migrations/Upgrade/V_8_16_0/AddDomainSortOrder.cs
@@ -13,21 +13,7 @@ namespace Umbraco.Core.Migrations.Upgrade.V_8_16_0
         {
             if (!ColumnExists(Constants.DatabaseSchema.Tables.Domain, "sortOrder"))
             {
-                // Create sortOrder with default value as it's not nullable
-                Create
-                    .Column("sortOrder")
-                    .OnTable(Constants.DatabaseSchema.Tables.Domain)
-                    .AsInt32()
-                    .NotNullable()
-                    .WithDefaultValue(0)
-                    .Do();
-
-                // Delete the default value constraint
-                Delete
-                    .DefaultConstraint()
-                    .OnTable(Constants.DatabaseSchema.Tables.Domain)
-                    .OnColumn("sortOrder")
-                    .Do();
+                AddColumn<DomainDto>("sortOrder", out var sqls);
 
                 // Keep exising sort order by setting it to the id
                 var updateSortOrder = Sql().Update<DomainDto>().Append("SET sortOrder = id");
@@ -36,6 +22,9 @@ namespace Umbraco.Core.Migrations.Upgrade.V_8_16_0
                 // Set sort order of wildcard domains to -1
                 var updateWildcardSortOrder = Sql().Update<DomainDto>(d => d.Set(f => f.SortOrder, -1)).Where("LEN(domainName) = 0 OR CHARINDEX('*', domainName) = 1");
                 Execute.Sql(updateWildcardSortOrder).Do();
+
+                // Alter the column (make it non-nullable)
+                foreach (var sql in sqls) Database.Execute(sql);
             }
         }
     }
