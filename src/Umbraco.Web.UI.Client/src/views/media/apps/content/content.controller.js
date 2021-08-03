a5eeee038aa85a689c361091fafe70cfdfbfb692
Mads Rasmussen <madsr@hey.com>
2021-08-03 13:49:09 +0200

    render tabs in media section

--- src/Umbraco.Web.UI.Client/src/views/media/apps/content/content.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/media/apps/content/content.controller.js
@@ -1,9 +1,39 @@
 (function () {
     "use strict";
 
-    function MediaAppContentController($scope) {
+    function MediaAppContentController($scope, $filter, contentEditingHelper) {
 
         var vm = this;
+
+        vm.tabs = [];
+        vm.activeTabAlias = null;
+
+        vm.setActiveTab = setActiveTab;
+
+        $scope.$watchCollection('content.tabs', () => {
+
+            // Add parentAlias property to all groups aka. tabs.
+            $scope.content.tabs.forEach((group) => {
+                group.parentAlias = contentEditingHelper.getParentAlias(group.alias);
+            });
+
+            vm.tabs = $filter("filter")($scope.content.tabs, (tab) => {
+                return tab.type === 1;
+            });
+
+            if (vm.tabs.length > 0) {
+                // if we have tabs and some groups that doesn't belong to a tab we need to render those on an "Other" tab.
+                contentEditingHelper.registerGenericTab($scope.content.tabs);
+
+                setActiveTab($scope.content.tabs[0]);
+            }
+        });
+
+        function setActiveTab (tab) {
+            vm.activeTabAlias = tab.alias;
+            vm.tabs.forEach(tab => tab.active = false);
+            tab.active = true;
+        }
 
     }
 
