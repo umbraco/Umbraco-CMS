294620f9b16ae2eaa22e7596ed2471a46f28c516
Mole <nikolajlauridsen@protonmail.ch>
2021-08-18 09:36:46 +0200

    Remove old nested content load code

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/nestedcontent/nestedcontent.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/nestedcontent/nestedcontent.controller.js
@@ -564,46 +564,6 @@
             initNestedContent();
         });
 
-        // _.each(model.config.contentTypes, function (contentType) {
-        //     // Get the scaffold for the contentType
-        //     contentResource.getScaffold(-20, contentType.ncAlias).then(function (scaffold) {
-        //         // make sure it's an element type before allowing the user to create new ones
-        //         if (scaffold.isElement) {
-        //             // remove all tabs except the specified tab
-        //             var tabs = scaffold.variants[0].tabs;
-        //             var tab = _.find(tabs, function (tab) {
-        //                 return tab.id !== 0 && (tab.alias.toLowerCase() === contentType.ncTabAlias.toLowerCase() || contentType.ncTabAlias === "");
-        //             });
-        //             scaffold.variants[0].tabs = [];
-        //             if (tab) {
-        //                 scaffold.variants[0].tabs.push(tab);
-        //
-        //                 tab.properties.forEach(
-        //                     function (property) {
-        //                         if (_.find(notSupported, function (x) { return x === property.editor; })) {
-        //                             property.notSupported = true;
-        //                             // TODO: Not supported message to be replaced with 'content_nestedContentEditorNotSupported' dictionary key. Currently not possible due to async/timing quirk.
-        //                             property.notSupportedMessage = "Property " + property.label + " uses editor " + property.editor + " which is not supported by Nested Content.";
-        //                         }
-        //                     }
-        //                 );
-        //             }
-        //
-        //             // Ensure Culture Data for Complex Validation.
-        //             ensureCultureData(scaffold);
-        //
-        //             // Store the scaffold object
-        //             vm.scaffolds.push(scaffold);
-        //         }
-        //
-        //         scaffoldsLoaded++;
-        //         initIfAllScaffoldsHaveLoaded();
-        //     }, function (error) {
-        //         scaffoldsLoaded++;
-        //         initIfAllScaffoldsHaveLoaded();
-        //     });
-        // });
-
         /**
          * Ensure that the containing content variant language and current property culture is transferred along
          * to the scaffolded content object representing this block.
