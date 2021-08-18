460df9bfd231ae3ddd3a5d125af0e86e0552fff2
Elitsa Marinovska <elm@umbraco.dk>
2021-08-18 11:26:27 +0200

    Changing gulp config and karma.conf.js

--- src/Umbraco.Web.UI.Client/test/config/karma.conf.js
+++ src/Umbraco.Web.UI.Client/test/config/karma.conf.js
@@ -34,12 +34,12 @@ module.exports = function (config) {
             'test/config/app.unit.js',
 
             //application files
-            '../Umbraco.Web.UI.NetCore/wwwroot/umbraco/js/*.controllers.min.js',
-            '../Umbraco.Web.UI.NetCore/wwwroot/umbraco/js/*.directives.min.js',
-            '../Umbraco.Web.UI.NetCore/wwwroot/umbraco/js/*.filters.min.js',
-            '../Umbraco.Web.UI.NetCore/wwwroot/umbraco/js/*.services.min.js',
-            '../Umbraco.Web.UI.NetCore/wwwroot/umbraco/js/*.interceptors.min.js',
-            '../Umbraco.Web.UI.NetCore/wwwroot/umbraco/js/*.resources.min.js',
+            '../Umbraco.Web.UI/wwwroot/umbraco/js/*.controllers.min.js',
+            '../Umbraco.Web.UI/wwwroot/umbraco/js/*.directives.min.js',
+            '../Umbraco.Web.UI/wwwroot/umbraco/js/*.filters.min.js',
+            '../Umbraco.Web.UI/wwwroot/umbraco/js/*.services.min.js',
+            '../Umbraco.Web.UI/wwwroot/umbraco/js/*.interceptors.min.js',
+            '../Umbraco.Web.UI/wwwroot/umbraco/js/*.resources.min.js',
 
             //mocked data and routing
             'src/common/mocks/umbraco.servervariables.js',
