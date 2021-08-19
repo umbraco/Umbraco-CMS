6b18f93ae70616c61dc101b860ae06032c0e4ce8
Paul Johnson <pmj@umbraco.com>
2021-08-19 14:11:40 +0100

    Fix issue where MSDI confused which ctor to use for EmailSender

--- src/Umbraco.Infrastructure/EmailSender.cs
+++ src/Umbraco.Infrastructure/EmailSender.cs
@@ -30,11 +30,15 @@ namespace Umbraco.Cms.Infrastructure
             ILogger<EmailSender> logger,
             IOptions<GlobalSettings> globalSettings,
             IEventAggregator eventAggregator)
-            : this(globalSettings, eventAggregator, null)
-            => _logger = logger;
+            : this(logger, globalSettings, eventAggregator, null) { }
 
-        public EmailSender(IOptions<GlobalSettings> globalSettings, IEventAggregator eventAggregator, INotificationHandler<SendEmailNotification> handler)
+        public EmailSender(
+            ILogger<EmailSender> logger,
+            IOptions<GlobalSettings> globalSettings,
+            IEventAggregator eventAggregator,
+            INotificationHandler<SendEmailNotification> handler)
         {
+            _logger = logger;
             _eventAggregator = eventAggregator;
             _globalSettings = globalSettings.Value;
             _notificationHandlerRegistered = handler is not null;
