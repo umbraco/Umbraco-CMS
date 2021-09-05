899df37a8d8ccb375225c22cc9d496be6a6e3bf2
TBVanderley4545 <TBVanderley4545@users.noreply.github.com>
2021-09-05 00:51:40 -0400

    * Revert.

--- src/Umbraco.Web.UI.Client/src/common/directives/util/getDomElement.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/util/getDomElement.directive.js
@@ -1,16 +1,17 @@
-var directiveDefinitionObject = {
+angular.module("umbraco.directives").directive("retriveDomElement", function () {
+    var directiveDefinitionObject = {
 
-    restrict: "A",
-    selector: '[retriveDomElement]',
-    scope: {
-        "retriveDomElement": "&"
-    },
-    link: {
-        post: function (scope, iElement, iAttrs, controller) {
-            scope.retriveDomElement({ element: iElement, attributes: iAttrs });
+        restrict: "A",
+        selector: '[retriveDomElement]',
+        scope: {
+            "retriveDomElement": "&"
+        },
+        link: {
+            post: function (scope, iElement, iAttrs, controller) {
+                scope.retriveDomElement({ element: iElement, attributes: iAttrs });
+            }
         }
-    }
-};
+    };
 
-return directiveDefinitionObject;
+    return directiveDefinitionObject;
 });
