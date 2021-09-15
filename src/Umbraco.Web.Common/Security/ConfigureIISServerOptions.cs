00bd5bd6eee2619d666e675589f8985ec03b5a8b
Elitsa Marinovska <elm@umbraco.dk>
2021-09-15 09:57:56 +0200

    Fixing the IIS Express maxAllowedContentLength

--- src/Umbraco.Web.Common/Security/ConfigureIISServerOptions.cs
+++ src/Umbraco.Web.Common/Security/ConfigureIISServerOptions.cs
@@ -12,7 +12,7 @@ namespace Umbraco.Cms.Web.Common.Security
         public void Configure(IISServerOptions options)
         {
             // convert from KB to bytes
-            options.MaxRequestBodySize = _runtimeSettings.Value.MaxRequestLength * 1024;
+            options.MaxRequestBodySize = _runtimeSettings.Value.MaxRequestLength.HasValue ? _runtimeSettings.Value.MaxRequestLength.Value * 1024 : int.MaxValue;
         }
     }
 }
