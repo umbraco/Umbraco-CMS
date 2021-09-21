8b853b17ac6b63a8e55b93f865224eae6877702d
Ronald Barendse <ronald@barend.se>
2021-09-21 11:11:40 +0200

    Include installed media to package definition on install

--- src/Umbraco.Infrastructure/Packaging/PackageInstallation.cs
+++ src/Umbraco.Infrastructure/Packaging/PackageInstallation.cs
@@ -44,20 +44,29 @@ namespace Umbraco.Cms.Infrastructure.Packaging
             //make sure the definition is up to date with everything
             foreach (var x in installationSummary.DataTypesInstalled)
                 packageDefinition.DataTypes.Add(x.Id.ToInvariantString());
+
             foreach (var x in installationSummary.LanguagesInstalled)
                 packageDefinition.Languages.Add(x.Id.ToInvariantString());
+
             foreach (var x in installationSummary.DictionaryItemsInstalled)
                 packageDefinition.DictionaryItems.Add(x.Id.ToInvariantString());
+
             foreach (var x in installationSummary.MacrosInstalled)
                 packageDefinition.Macros.Add(x.Id.ToInvariantString());
+
             foreach (var x in installationSummary.TemplatesInstalled)
                 packageDefinition.Templates.Add(x.Id.ToInvariantString());
+
             foreach (var x in installationSummary.DocumentTypesInstalled)
                 packageDefinition.DocumentTypes.Add(x.Id.ToInvariantString());
-            foreach (var x in installationSummary.StylesheetsInstalled)
-                packageDefinition.Stylesheets.Add(x.Id.ToInvariantString());
-            var contentInstalled = installationSummary.ContentInstalled.ToList();
-            packageDefinition.ContentNodeId = contentInstalled.Count > 0 ? contentInstalled[0].Id.ToInvariantString() : null;
+
+            foreach (var x in installationSummary.MediaTypesInstalled)
+                packageDefinition.MediaTypes.Add(x.Id.ToInvariantString());
+
+            packageDefinition.ContentNodeId = installationSummary.ContentInstalled.FirstOrDefault()?.Id.ToInvariantString();
+            
+            foreach (var x in installationSummary.MediaInstalled)
+                packageDefinition.MediaUdis.Add(x.GetUdi());
 
             return installationSummary;
         }
