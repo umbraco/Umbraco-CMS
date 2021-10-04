f3f674437d101ebdd647dd21aa1bbba4b51b6229
Andy Butland <abutland73@gmail.com>
2021-10-04 18:44:14 +0200

    Ensured order of Deploy artifact dependencies irrespective of ICU or NLS globalization settings. (#11265)

--- src/Umbraco.Tests.UnitTests/Umbraco.Core/Deploy/ArtifactBaseTests.cs
+++ src/Umbraco.Tests.UnitTests/Umbraco.Core/Deploy/ArtifactBaseTests.cs
@@ -3,6 +3,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using Newtonsoft.Json;
 using NUnit.Framework;
 using Umbraco.Cms.Core;
@@ -27,6 +28,33 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Deploy
 
             var expected = "{\"Udi\":\"umb://test/3382d5433b5749d08919bc9961422a1f\",\"Dependencies\":[],\"Name\":\"Test Name\",\"Alias\":\"testAlias\"}";
             Assert.AreEqual(expected, serialized);
+        }
+
+        [Test]
+        public void Dependencies_Are_Correctly_Ordered()
+        {
+            // This test was introduced following: https://github.com/umbraco/Umbraco.Deploy.Issues/issues/72 to verify
+            // that consistent ordering rules are used across platforms.
+            var udi = new GuidUdi("test", Guid.Parse("3382d5433b5749d08919bc9961422a1f"));
+            var artifact = new TestArtifact(udi, new List<ArtifactDependency>())
+            {
+                Name = "Test Name",
+                Alias = "testAlias",
+            };
+
+            var dependencies = new ArtifactDependencyCollection();
+
+            var dependencyUdi1 = new GuidUdi("template", Guid.Parse("d4651496fad24c1290a53ea4d55d945b"));
+            dependencies.Add(new ArtifactDependency(dependencyUdi1, true, ArtifactDependencyMode.Exist));
+
+            var dependencyUdi2 = new StringUdi(Constants.UdiEntityType.TemplateFile, "TestPage.cshtml");
+            dependencies.Add(new ArtifactDependency(dependencyUdi2, true, ArtifactDependencyMode.Exist));
+
+            artifact.Dependencies = dependencies;
+
+            Assert.AreEqual(
+                "umb://template-file/TestPage.cshtml,umb://template/d4651496fad24c1290a53ea4d55d945b",
+                string.Join(",", artifact.Dependencies.Select(x => x.Udi.ToString())));
         }
 
         private class TestArtifact : ArtifactBase<GuidUdi>
