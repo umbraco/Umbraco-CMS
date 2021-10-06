5e68aed053efc020b3364f6dc88f209a2e5ad607
Warren Buckley <warren@umbraco.com>
2021-10-06 12:17:12 +0100

    A dictionary tag helper to work on any HTML element

--- src/Umbraco.Web.Common/TagHelpers/DictionaryAttrTagHelper.cs
+++ src/Umbraco.Web.Common/TagHelpers/DictionaryAttrTagHelper.cs
@@ -3,13 +3,13 @@ using Umbraco.Cms.Core.Dictionary;
 
 namespace Umbraco.Cms.Web.Common.TagHelpers
 {
-    [HtmlTargetElement("umb-dictionary", TagStructure = TagStructure.NormalOrSelfClosing)]
-    public class DictionaryTagHelper : TagHelper
+    [HtmlTargetElement("*", Attributes = "umb-dictionary-key")]
+    public class DictionaryAttrTagHelper : TagHelper
     {
         private readonly ICultureDictionaryFactory _cultureDictionaryFactory;
         private ICultureDictionary _cultureDictionary;
 
-        public DictionaryTagHelper(ICultureDictionaryFactory cultureDictionaryFactory)
+        public DictionaryAttrTagHelper(ICultureDictionaryFactory cultureDictionaryFactory)
         {
             _cultureDictionaryFactory = cultureDictionaryFactory;
         }
@@ -17,17 +17,15 @@ namespace Umbraco.Cms.Web.Common.TagHelpers
         [HtmlAttributeNotBound]
         public ICultureDictionary CultureDictionary => _cultureDictionary ??= _cultureDictionaryFactory.CreateDictionary();
 
-        [HtmlAttributeName("key")]
+        [HtmlAttributeName("umb-dictionary-key")]
         public string Key { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
             base.Process(context, output);
 
-            output.TagName = ""; // Remove the outer tag of <umb-dictionary>
-
             // Ensure we have a dictionary key to lookup
-            if(string.IsNullOrEmpty(Key) == false)
+            if (string.IsNullOrEmpty(Key) == false)
             {
                 var localizedValue = CultureDictionary[Key];
                 if (string.IsNullOrEmpty(localizedValue) == false)
