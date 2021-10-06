c9e773c14637e6b61d3376356d258d6a6c5dd82c
Matthew Care <matthew.care@fatmedia.co.uk>
2021-10-06 16:59:05 +0100

    Sort usings
    Remove unused using

--- src/Umbraco.Web.UI/Startup.cs
+++ src/Umbraco.Web.UI/Startup.cs
@@ -5,7 +5,6 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Umbraco.Cms.Core.DependencyInjection;
-using Umbraco.Cms.Core.Notifications;
 using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Web.UI
