be66e2ea618a359c0369a93cf11520bce62d4578
Nikolaj Geisle <niko737@edu.ucl.dk>
2021-10-06 13:53:33 +0200

    Updated cypress test to use newly created helper method

--- src/Umbraco.Tests.AcceptanceTest/cypress/integration/HelpPanel/systemInformation.ts
+++ src/Umbraco.Tests.AcceptanceTest/cypress/integration/HelpPanel/systemInformation.ts
@@ -12,6 +12,7 @@ context('System Information', () => {
     beforeEach(() => {
         //arrange
         cy.umbracoLogin(Cypress.env('username'), Cypress.env('password'));
+        cy.umbracoSetCurrentUserLanguage('en-US');
     });
 
     it('Check System Info Displays', () => {
@@ -20,33 +21,20 @@ context('System Information', () => {
         cy.contains('Current Culture').parent().should('contain', 'en-US');
         cy.contains('Current UI Culture').parent().should('contain', 'en-US');
     });
+    
+    it('Checks language displays correctly after switching', () => {
 
-    context('Language switching', () => {
-
-        afterEach(() => {
-            cy.reload()
-            cy.get('[data-element="global-user"]', {timeout: 10000}).should('be.visible', {timeout: 10000}).click();
-            cy.get('[alias="editUser"]').click();
-            cy.get('.input-block-level', {timeout: 10000}).last().select('string:en-US', {timeout: 10000, force: true});
-            cy.umbracoButtonByLabelKey('buttons_save').click({force: true});
-            cy.reload();
-        });
-
-        it('Checks language displays correctly after switching', () => {
-
-            //Navigate to edit user and change language
-            cy.get('[data-element="global-user"]').click();
-            cy.get('[alias="editUser"]').click();
-            cy.get('[name="culture"]').select('string:da-DK', {timeout: 10000, force: true});
-            cy.umbracoButtonByLabelKey('buttons_save').click({force: true});
-            //Refresh site to display new language
-            cy.reload();
-            openSystemInformation();
-            //Assert
-            cy.contains('Current Culture').parent().should('contain', 'da-DK');
-            cy.contains('Current UI Culture').parent().should('contain', 'da-DK');
-            cy.get('.umb-button__content').last().click();
-            cy.reload();
-        });
+        //Navigate to edit user and change language
+        cy.get('[data-element="global-user"]').click();
+        cy.get('[alias="editUser"]').click();
+        cy.get('[name="culture"]').select('string:da-DK', {timeout: 10000, force: true});
+        cy.umbracoButtonByLabelKey('buttons_save').click({force: true});
+        //Refresh site to display new language
+        cy.reload();
+        openSystemInformation();
+        //Assert
+        cy.contains('Current Culture').parent().should('contain', 'da-DK');
+        cy.contains('Current UI Culture').parent().should('contain', 'da-DK');
+        cy.get('.umb-button__content').last().click();
     });
 });
