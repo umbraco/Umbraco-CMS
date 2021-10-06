c25f16706f029b2e78f91449b299b7ff9b72a54f
Patrick de Mooij <patrickdemooij98@hotmail.com>
2021-10-06 21:17:14 +0200

    New interface for custom backoffice assets in C#

--- src/Umbraco.Core/WebAssets/CustomBackOfficeAssetsCollectionBuilder.cs
+++ src/Umbraco.Core/WebAssets/CustomBackOfficeAssetsCollectionBuilder.cs
@@ -1,9 +1,22 @@
+using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.DependencyInjection;
 using Umbraco.Cms.Core.Composing;
 
 namespace Umbraco.Cms.Core.WebAssets
 {
-    public class CustomBackOfficeAssetsCollectionBuilder : OrderedCollectionBuilderBase<CustomBackOfficeAssetsCollectionBuilder, CustomBackOfficeAssetsCollection, IAssetFile>
+    public class CustomBackOfficeAssetsCollectionBuilder : ICollectionBuilder<CustomBackOfficeAssetsCollection, IAssetFile>
     {
-        protected override CustomBackOfficeAssetsCollectionBuilder This => this;
+        private readonly List<IAssetFile> _files = new List<IAssetFile>();
+
+        public CustomBackOfficeAssetsCollection CreateCollection(IServiceProvider factory) => new CustomBackOfficeAssetsCollection(() => _files);
+
+        public void RegisterWith(IServiceCollection services) => services.Add(new ServiceDescriptor(typeof(CustomBackOfficeAssetsCollection), CreateCollection, ServiceLifetime.Singleton));
+
+        public void AddAssetFile(IAssetFile file)
+        {
+            if (file is null) throw new ArgumentNullException(nameof(file));
+            _files.Add(file);
+        }
     }
 }
