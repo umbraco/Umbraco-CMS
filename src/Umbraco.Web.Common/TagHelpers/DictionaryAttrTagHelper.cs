67fb3ee4fedb23831f43b75cbc62bd742bd81de1
Warren Buckley <warren@umbraco.com>
2021-10-07 13:15:48 +0100

    Dictionary tag & attribute now supports optional fallback language ISO code to use

--- src/Umbraco.Web.Common/TagHelpers/DictionaryAttrTagHelper.cs
+++ src/Umbraco.Web.Common/TagHelpers/DictionaryAttrTagHelper.cs
@@ -1,24 +1,25 @@
+using System.Globalization;
+using System.Linq;
 using Microsoft.AspNetCore.Razor.TagHelpers;
-using Umbraco.Cms.Core.Dictionary;
+using Umbraco.Cms.Core.Services;
 
 namespace Umbraco.Cms.Web.Common.TagHelpers
 {
     [HtmlTargetElement("*", Attributes = "umb-dictionary-key")]
     public class DictionaryAttrTagHelper : TagHelper
     {
-        private readonly ICultureDictionaryFactory _cultureDictionaryFactory;
-        private ICultureDictionary _cultureDictionary;
+        private ILocalizationService _localizationService;
 
-        public DictionaryAttrTagHelper(ICultureDictionaryFactory cultureDictionaryFactory)
+        public DictionaryAttrTagHelper(ILocalizationService localizationService)
         {
-            _cultureDictionaryFactory = cultureDictionaryFactory;
+            _localizationService = localizationService;
         }
-
-        [HtmlAttributeNotBound]
-        public ICultureDictionary CultureDictionary => _cultureDictionary ??= _cultureDictionaryFactory.CreateDictionary();
 
         [HtmlAttributeName("umb-dictionary-key")]
         public string Key { get; set; }
+
+        [HtmlAttributeName("umb-dictionary-fallback-lang")]
+        public string FallbackLang { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
@@ -27,11 +28,32 @@ namespace Umbraco.Cms.Web.Common.TagHelpers
             // Ensure we have a dictionary key to lookup
             if (string.IsNullOrEmpty(Key) == false)
             {
-                var localizedValue = CultureDictionary[Key];
-                if (string.IsNullOrEmpty(localizedValue) == false)
+                // Get current culture
+                var currentCulture = CultureInfo.CurrentCulture;
+
+                // Ensure the Dictionary item/key even exist
+                var translation = _localizationService.GetDictionaryItemByKey(Key);
+                if (translation != null)
                 {
-                    // Only replace the HTML inside the <umb-dictionary> tag if we have a value
-                    output.Content.SetHtmlContent(localizedValue);
+                    // Try to see if we have a value set for the current culture/language
+                    var langTranslation = translation.Translations.FirstOrDefault(x => x.Language.CultureInfo.Name.Equals(currentCulture.Name, comparisonType: System.StringComparison.InvariantCultureIgnoreCase));
+                    if (string.IsNullOrEmpty(langTranslation?.Value) == false)
+                    {
+                        // Only replace the HTML inside the <umb-dictionary> tag if we have a value
+                        output.Content.SetHtmlContent(langTranslation.Value);
+                    }
+
+                    // If we can't find the current lang value - check if we have set an attribute to check for a fallback
+                    else if (string.IsNullOrEmpty(FallbackLang) == false)
+                    {
+                        // Try & see if we have a value set for fallback lang
+                        var fallbackLangTranslation = translation.Translations.FirstOrDefault(x => x.Language.CultureInfo.Name.Equals(FallbackLang, comparisonType: System.StringComparison.InvariantCultureIgnoreCase));
+                        if (string.IsNullOrEmpty(fallbackLangTranslation?.Value) == false)
+                        {
+                            // Only replace the HTML inside the <umb-dictionary> tag if we have a value for the fallback lang
+                            output.Content.SetHtmlContent(fallbackLangTranslation.Value);
+                        }
+                    }
                 }
             }
         }
--- src/Umbraco.Web.Common/TagHelpers/DictionaryTagHelper.cs
+++ src/Umbraco.Web.Common/TagHelpers/DictionaryTagHelper.cs
@@ -1,24 +1,25 @@
+using System.Globalization;
+using System.Linq;
 using Microsoft.AspNetCore.Razor.TagHelpers;
-using Umbraco.Cms.Core.Dictionary;
+using Umbraco.Cms.Core.Services;
 
 namespace Umbraco.Cms.Web.Common.TagHelpers
 {
     [HtmlTargetElement("umb-dictionary", TagStructure = TagStructure.NormalOrSelfClosing)]
     public class DictionaryTagHelper : TagHelper
     {
-        private readonly ICultureDictionaryFactory _cultureDictionaryFactory;
-        private ICultureDictionary _cultureDictionary;
+        private readonly ILocalizationService _localizationService;
 
-        public DictionaryTagHelper(ICultureDictionaryFactory cultureDictionaryFactory)
+        public DictionaryTagHelper(ILocalizationService localizationService)
         {
-            _cultureDictionaryFactory = cultureDictionaryFactory;
+            _localizationService = localizationService;
         }
-
-        [HtmlAttributeNotBound]
-        public ICultureDictionary CultureDictionary => _cultureDictionary ??= _cultureDictionaryFactory.CreateDictionary();
 
         [HtmlAttributeName("key")]
         public string Key { get; set; }
+
+        [HtmlAttributeName("fallback-lang")]
+        public string FallbackLang { get; set; }
 
         public override void Process(TagHelperContext context, TagHelperOutput output)
         {
@@ -29,11 +30,32 @@ namespace Umbraco.Cms.Web.Common.TagHelpers
             // Ensure we have a dictionary key to lookup
             if(string.IsNullOrEmpty(Key) == false)
             {
-                var localizedValue = CultureDictionary[Key];
-                if (string.IsNullOrEmpty(localizedValue) == false)
+                // Get current culture
+                var currentCulture = CultureInfo.CurrentCulture;
+
+                // Ensure the Dictionary item/key even exist
+                var translation = _localizationService.GetDictionaryItemByKey(Key);
+                if(translation != null)
                 {
-                    // Only replace the HTML inside the <umb-dictionary> tag if we have a value
-                    output.Content.SetHtmlContent(localizedValue);
+                    // Try to see if we have a value set for the current culture/language
+                    var langTranslation = translation.Translations.FirstOrDefault(x => x.Language.CultureInfo.Name.Equals(currentCulture.Name, comparisonType: System.StringComparison.InvariantCultureIgnoreCase));
+                    if(string.IsNullOrEmpty(langTranslation?.Value) == false)
+                    {
+                        // Only replace the HTML inside the <umb-dictionary> tag if we have a value
+                        output.Content.SetHtmlContent(langTranslation.Value);
+                    }
+
+                    // If we can't find the current lang value - check if we have set an attribute to check for a fallback
+                    else if(string.IsNullOrEmpty(FallbackLang) == false)
+                    {
+                        // Try & see if we have a value set for fallback lang
+                        var fallbackLangTranslation = translation.Translations.FirstOrDefault(x => x.Language.CultureInfo.Name.Equals(FallbackLang, comparisonType: System.StringComparison.InvariantCultureIgnoreCase));
+                        if (string.IsNullOrEmpty(fallbackLangTranslation?.Value) == false)
+                        {
+                            // Only replace the HTML inside the <umb-dictionary> tag if we have a value for the fallback lang
+                            output.Content.SetHtmlContent(fallbackLangTranslation.Value);
+                        }
+                    }
                 }
             }
         }
