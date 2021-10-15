d865448177f7cc2f70860afe58cad98a974f3904
Paul Johnson <pmj@umbraco.com>
2021-10-15 15:45:21 +0100

    Added database migration to support version cleanup feature

---
+++ src/Umbraco.Core/Migrations/Upgrade/V_8_18_0/AddContentVersionCleanupFeature.cs
@@ -0,0 +1,20 @@
+﻿using System.Linq;
+using Umbraco.Core.Persistence.Dtos;
+
+namespace Umbraco.Core.Migrations.Upgrade.V_8_18_0
+{
+    class AddContentVersionCleanupFeature : MigrationBase
+    {
+        public AddContentVersionCleanupFeature(IMigrationContext context)
+            : base(context) { }
+
+        public override void Migrate()
+        {
+            Create.Table<ContentVersionCleanupPolicyDto>().Do();
+
+            // What's this about, we worry someone else edited table with same change?
+            var columns = SqlSyntax.GetColumnsInSchema(Context.Database).ToList();
+            AddColumnIfNotExists<ContentVersionDto>(columns, "preventCleanup");
+        }
+    }
+}
