4dd663d6651cef04560dbd3df1bb2604af269b6d
BatJan <1932158+BatJan@users.noreply.github.com>
2021-10-20 16:25:12 +0200

    Add remove warning

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/grid/grid.prevalues.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/grid/grid.prevalues.controller.js
@@ -188,7 +188,6 @@ angular.module("umbraco")
             }
 
             function deleteLayout(layout, index, event) {
-
                 const dialog = {
                     view: "views/propertyEditors/grid/overlays/rowdeleteconfirm.html",
                     layout: layout,
