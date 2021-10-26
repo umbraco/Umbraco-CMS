06060f55c6215744b74244dbc820ca9dfd85bcea
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2021-10-26 15:06:56 +0200

    changed template

--- tests/Umbraco.Tests.AcceptanceTest/cypress/integration/Settings/templates.ts
+++ tests/Umbraco.Tests.AcceptanceTest/cypress/integration/Settings/templates.ts
@@ -136,9 +136,7 @@ context('Templates', () => {
         // Insert macro
         cy.umbracoButtonByLabelKey('general_insert').click();
         cy.get('.umb-insert-code-box__title').contains('Macro').click();
-        cy.get('.umb-card-grid-item').contains(name).then(button => {
-          button.click();
-        });
+        cy.get(`.umb-card-grid-item[title='${name}']`).click('bottom');
 
         // Assert
         cy.get('.ace_content').contains('@await Umbraco.RenderMacroAsync("' + name + '")').should('exist');
