0f7e134000a7c5adf6094113701b19f8f0dcdda4
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2021-10-27 19:56:48 +0200

    Create new cypress test- delete stylesheet

--- tests/Umbraco.Tests.AcceptanceTest/cypress/integration/Settings/stylesheets.ts
+++ tests/Umbraco.Tests.AcceptanceTest/cypress/integration/Settings/stylesheets.ts
@@ -5,9 +5,17 @@ context('Stylesheets', () => {
     cy.umbracoLogin(Cypress.env('username'), Cypress.env('password'));
   });
 
+  afterEach(() => {
+    //Clean up, this ensures even if tests break we clean up
+    cy.umbracoEnsureStylesheetNameNotExists(fileName);
+  });
+
+  let fileName = "";
+
+
   it('Create new style sheet file', () => {
     const name = "TestStylesheet";
-    const fileName = name + ".css";
+    fileName = name + ".css";
 
    cy.umbracoEnsureStylesheetNameNotExists(fileName);
 
@@ -28,8 +36,44 @@ context('Stylesheets', () => {
     //Assert
     cy.umbracoSuccessNotification().should('be.visible');
 
-    //Clean up
+   });
+
+   it('Deletes a stylesheet', () => {
+    const name = "TestStylesheet";
+    fileName = name + ".css";
+
+    //Ensure stylesheet doesn't already exists
     cy.umbracoEnsureStylesheetNameNotExists(fileName);
+
+    var stylesheetData =
+    {
+      "virtualPath": "/css/",
+      "path": null,
+      "name": name,
+      "content": "",
+      "fileType": "stylesheets",
+      "snippet": null,
+      "id": "0",
+      "notifications": []
+    }
+
+
+    let url = '/umbraco/backoffice/umbracoapi/codefile/PostSave'
+    cy.umbracoApiRequest(url, 'POST', stylesheetData);
+
+    //Navigate to settings ection
+    cy.umbracoSection('settings');
+    cy.get('li .umb-tree-root:contains("Settings")').should("be.visible");
+
+    //Open stylesheet tree
+    cy.get('[data-element="tree-item-stylesheets"] > .umb-tree-item__inner > .umb-tree-item__arrow').click();
+    //Delete stylesheet
+    cy.get('.umb-tree-item__inner > .umb-tree-item__label').contains(name).rightclick();
+    cy.get('.umb-action-link').click();
+    cy.get('[ng-if="showConfirm"]').click();
+    //Assert
+    cy.get('.umb-tree-item__inner > .umb-tree-item__label').contains(name).should('not.exist');
+
    });
 
 });
