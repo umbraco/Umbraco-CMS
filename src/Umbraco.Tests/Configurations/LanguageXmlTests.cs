2a4efa4a180809cc4fd51b619cdb1aa9168ba3ad
Andy Butland <abutland73@gmail.com>
2021-10-27 13:44:58 +0200

    Updated method of detecting language folder for XML tests, that should work correctly on the build server.

--- src/Umbraco.Tests/Configurations/LanguageXmlTests.cs
+++ src/Umbraco.Tests/Configurations/LanguageXmlTests.cs
@@ -1,38 +1,41 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
+using System.Linq;
 using System.Xml;
 using NUnit.Framework;
 using Umbraco.Tests.TestHelpers;
 
 namespace Umbraco.Tests.Configurations
 {
-
     [TestFixture]
     public class LanguageXmlTests
     {
         [Test]
         public void Can_Load_Language_Xml_Files()
         {
-            var dir = new DirectoryInfo(TestHelper.MapPathForTest("~/"));
-            if (dir == null)
-                throw new NullReferenceException("dir is null. | " + dir.FullName);
-            while (dir.Name != "src")
-            {
-                if (dir.Parent == null)
-                    throw new NullReferenceException("dir is null - 2 | " + dir.FullName);
-                dir = dir.Parent;
-            }
-            var languageDirectory = new DirectoryInfo(dir.FullName + "/Umbraco.Web.UI/Umbraco/config/lang/");
+            var languageDirectory = GetLanguageDirectory();
             var readFilesCount = 0;
             var xmlDocument = new XmlDocument();
             foreach (var languageFile in languageDirectory.EnumerateFiles("*.xml"))
             {
-                // load will throw an exception if the xml isn't valid.
+                // Load will throw an exception if the XML isn't valid.
                 xmlDocument.Load(languageFile.FullName);
                 readFilesCount++;
             }
-            // ensure that at least one file was read.
+
+            // Ensure that at least one file was read.
             Assert.AreNotEqual(0, readFilesCount);
+        }
+
+        private static DirectoryInfo GetLanguageDirectory()
+        {
+            var testDirectoryPathParts = Path.GetDirectoryName(Path.GetDirectoryName(TestContext.CurrentContext.TestDirectory)).Split(Path.DirectorySeparatorChar);
+            var solutionDirectoryPathParts = testDirectoryPathParts
+                .Take(Array.IndexOf(testDirectoryPathParts, "src") + 1);
+            var languageFolderPathParts = new List<string>(solutionDirectoryPathParts);
+            languageFolderPathParts.AddRange(new[] { "Umbraco.Web.UI", "Umbraco", "config", "lang" });
+            return new DirectoryInfo(string.Join(Path.DirectorySeparatorChar.ToString(), languageFolderPathParts));
         }
     }
 }
