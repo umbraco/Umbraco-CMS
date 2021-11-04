94d73539471f4d2cd4c2e81f4991a15da47e3387
Elitsa Marinovska <elm@umbraco.dk>
2021-11-04 15:20:47 +0100

    Cleanup

--- tests/Umbraco.Tests.AcceptanceTest/cypress/integration/Settings/stylesheets.ts
+++ tests/Umbraco.Tests.AcceptanceTest/cypress/integration/Settings/stylesheets.ts
@@ -1,23 +1,21 @@
 /// <reference types="Cypress" />
 context('Stylesheets', () => {
 
+  const name = "TestStylesheet";
+  const fileName = name + ".css";
+
   beforeEach(() => {
     cy.umbracoLogin(Cypress.env('username'), Cypress.env('password'));
-  });
 
-  afterEach(() => {
-    //Clean up, this ensures even if tests break we clean up
     cy.umbracoEnsureStylesheetNameNotExists(fileName);
   });
 
-  let fileName = "";
-
+  afterEach(() => {
+    // Clean up, this ensures that even if tests break we clean up
+    cy.umbracoEnsureStylesheetNameNotExists(fileName);
+  });
 
   it('Create new style sheet file', () => {
-    const name = "TestStylesheet";
-    fileName = name + ".css";
-
-   cy.umbracoEnsureStylesheetNameNotExists(fileName);
 
     cy.umbracoSection('settings');
     cy.get('li .umb-tree-root:contains("Settings")').should("be.visible");
@@ -25,28 +23,22 @@ context('Stylesheets', () => {
     cy.umbracoTreeItem("settings", ["Stylesheets"]).rightclick();
 
     cy.umbracoContextMenuAction("action-create").click();
-    cy.get('.menu-label').first().click(); // TODO: Fucked we cant use something like cy.umbracoContextMenuAction("action-mediaType").click();
+    cy.get('.menu-label').first().click(); // TODO: Would be better to use something like cy.umbracoContextMenuAction("action-mediaType").click();
 
-    //Type name
+    // Type name
     cy.umbracoEditorHeaderName(name);
 
-    //Save
+    // Save
     cy.get('.btn-success').click();
 
-    //Assert
+    // Assert
     cy.umbracoSuccessNotification().should('be.visible');
 
    });
 
    it('Deletes a stylesheet', () => {
-    const name = "TestStylesheet";
-    fileName = name + ".css";
 
-    //Ensure stylesheet doesn't already exists
-    cy.umbracoEnsureStylesheetNameNotExists(fileName);
-
-    var stylesheetData =
-    {
+    var stylesheetData = {
       "virtualPath": "/css/",
       "path": null,
       "name": name,
@@ -57,23 +49,23 @@ context('Stylesheets', () => {
       "notifications": []
     }
 
-
     let url = '/umbraco/backoffice/umbracoapi/codefile/PostSave'
     cy.umbracoApiRequest(url, 'POST', stylesheetData);
 
-    //Navigate to settings ection
+    // Navigate to Settings section
     cy.umbracoSection('settings');
     cy.get('li .umb-tree-root:contains("Settings")').should("be.visible");
 
-    //Open stylesheet tree
+    // Open stylesheet tree
     cy.get('[data-element="tree-item-stylesheets"] > .umb-tree-item__inner > .umb-tree-item__arrow').click();
-    //Delete stylesheet
+
+    // Delete stylesheet
     cy.get('.umb-tree-item__inner > .umb-tree-item__label').contains(name).rightclick();
     cy.get('.umb-action-link').click();
     cy.get('[ng-if="showConfirm"]').click();
-    //Assert
+
+    // Assert
     cy.get('.umb-tree-item__inner > .umb-tree-item__label').contains(name).should('not.exist');
 
    });
-
 });
