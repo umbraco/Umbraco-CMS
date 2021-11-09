d273d1834605a45eb5d489f5bb309240a8f59f36
Paul Johnson <pmj@umbraco.com>
2021-11-09 16:16:53 +0000

    Make xmldocs for IPublishedContentQueryAccessor more helpful.

--- src/Umbraco.Infrastructure/IPublishedContentQueryAccessor.cs
+++ src/Umbraco.Infrastructure/IPublishedContentQueryAccessor.cs
@@ -1,8 +1,17 @@
 namespace Umbraco.Cms.Core
 {
     /// <remarks>
-    /// Not intended for use in background threads where you should instead resolve IPublishedContentQuery directly
-    /// and make use of <see cref="Umbraco.Cms.Core.Web.IUmbracoContextFactory.EnsureUmbracoContext"/>
+    /// Not intended for use in background threads where you should make use of <see cref="Umbraco.Cms.Core.Web.IUmbracoContextFactory.EnsureUmbracoContext"/>
+    /// and instead resolve IPublishedContentQuery from a <see cref="Microsoft.Extensions.DependencyInjection.IServiceScope"/>
+    /// e.g. using <see cref="Microsoft.Extensions.DependencyInjection.ServiceProviderServiceExtensions.CreateScope"/>
+    /// <example>
+    /// <code>
+    /// // Background thread example
+    /// using UmbracoContextReference _ = _umbracoContextFactory.EnsureUmbracoContext();
+    /// using IServiceScope serviceScope = _serviceProvider.CreateScope();
+    /// IPublishedContentQuery query = serviceScope.ServiceProvider.GetRequiredService&lt;IPublishedContentQuery&gt;();
+    /// </code>
+    /// </example>
     /// </remarks>
     public interface IPublishedContentQueryAccessor
     {
