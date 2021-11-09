a2106735c551f4911ec5e0969df6bf503a6127e6
Paul Johnson <pmj@umbraco.com>
2021-11-09 15:49:43 +0000

    Make PublishedContentQueryAccessor usable
    Closes #11319

--- src/Umbraco.Infrastructure/IPublishedContentQueryAccessor.cs
+++ src/Umbraco.Infrastructure/IPublishedContentQueryAccessor.cs
@@ -1,7 +1,9 @@
-using Umbraco.Cms.Infrastructure;
-
 namespace Umbraco.Cms.Core
 {
+    /// <remarks>
+    /// Not intended for use in background threads where you should instead resolve IPublishedContentQuery directly
+    /// and make use of <see cref="Umbraco.Cms.Core.Web.IUmbracoContextFactory.EnsureUmbracoContext"/>
+    /// </remarks>
     public interface IPublishedContentQueryAccessor
     {
         bool TryGetValue(out IPublishedContentQuery publishedContentQuery);
