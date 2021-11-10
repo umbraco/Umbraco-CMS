7b07ec6ccaabe7cfb435cdcec20278a2259c5722
Nikolaj Geisle <niko737@edu.ucl.dk>
2021-11-10 08:48:18 +0100

    Fixed rollback by not doing cy.reload()

--- tests/Umbraco.Tests.AcceptanceTest/cypress/integration/Languages/languages.ts
+++ tests/Umbraco.Tests.AcceptanceTest/cypress/integration/Languages/languages.ts
@@ -13,11 +13,12 @@ context('Languages', () => {
         cy.umbracoEnsureLanguageNotExists(language2);
         cy.umbracoCreateLanguage(language1, true, '1');
         cy.umbracoCreateLanguage(language2, true, '1');
+        //Enter settings section and wait for everything to load
         cy.umbracoSection('settings');
+        cy.get('.umb-box-content').should('be.visible');
+        cy.get('.umb-tree-root').contains("Settings").should('be.visible');
 
         // Enter language tree and select the language we just created
-        cy.get('.umb-box-content').should('be.visible');
-        cy.get('.umb-tree').should('be.visible');
         cy.umbracoTreeItem('settings', ['Languages']).click();
 
         // Assert there are 3 languages
