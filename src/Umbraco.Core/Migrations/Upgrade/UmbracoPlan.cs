e0fb21b939149496e69c8f4a6f6b172437d60e62
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2021-11-27 18:20:19 +0100

    Added migration

--- src/Umbraco.Core/Migrations/Upgrade/UmbracoPlan.cs
+++ src/Umbraco.Core/Migrations/Upgrade/UmbracoPlan.cs
@@ -9,6 +9,7 @@ using Umbraco.Core.Migrations.Upgrade.V_8_1_0;
 using Umbraco.Core.Migrations.Upgrade.V_8_6_0;
 using Umbraco.Core.Migrations.Upgrade.V_8_9_0;
 using Umbraco.Core.Migrations.Upgrade.V_8_10_0;
+using Umbraco.Core.Migrations.Upgrade.V_8_18_0;
 
 namespace Umbraco.Core.Migrations.Upgrade
 {
@@ -201,6 +202,9 @@ namespace Umbraco.Core.Migrations.Upgrade
 			
             // to 8.10.0
             To<AddPropertyTypeLabelOnTopColumn>("{D6A8D863-38EC-44FB-91EC-ACD6A668BD18}");
+
+            // to 8.18.0
+            To<AddDefaultForNotificationsToggle>("{AD3D3B7F-8E74-45A4-85DB-7FFAD57F9243}");
 
             //FINAL
         }
---
+++ src/Umbraco.Core/Migrations/Upgrade/V_8_18_0/AddDefaultForNotificationsToggle.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Umbraco.Core.Persistence.Dtos;
+
+namespace Umbraco.Core.Migrations.Upgrade.V_8_18_0
+{
+    public class AddDefaultForNotificationsToggle : MigrationBase
+    {
+        public AddDefaultForNotificationsToggle(IMigrationContext context) : base(context)
+        {
+        }
+
+        public override void Migrate()
+        {
+            Update.Table(Constants.DatabaseSchema.Tables.UserGroup).Set("userGroupDefaultPermissions = userGroupDefaultPermissions + 'N'").Where("id = '1' OR id = '2' OR id = '3'").Do();
+        }
+    }
+}
