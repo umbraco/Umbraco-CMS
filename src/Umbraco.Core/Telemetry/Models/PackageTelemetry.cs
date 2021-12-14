af44b70b35141568749f6284f5df7073641fb869
Mole <nikolajlauridsen@protonmail.ch>
2021-12-14 16:09:31 +0100

    Merge Telemetry classes from V9

---
+++ src/Umbraco.Core/Telemetry/Models/PackageTelemetry.cs
@@ -0,0 +1,28 @@
+using System;
+using System.Runtime.Serialization;
+
+namespace Umbraco.Core.Telemetry.Models
+{
+    /// <summary>
+    /// Serializable class containing information about an installed package.
+    /// </summary>
+    [Serializable]
+    [DataContract(Name = "packageTelemetry")]
+    public class PackageTelemetry
+    {
+        /// <summary>
+        /// Gets or sets the name of the installed package.
+        /// </summary>
+        [DataMember(Name = "name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Gets or sets the version of the installed package.
+        /// </summary>
+        /// <remarks>
+        /// This may be an empty string if no version is specified, or if package telemetry has been restricted.
+        /// </remarks>
+        [DataMember(Name = "version")]
+        public string Version { get; set; }
+    }
+}
