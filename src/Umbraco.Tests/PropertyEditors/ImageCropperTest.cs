75bb8051bff84e3cc2e9da2379d696f02d7a8845
Ronald Barendse <ronald@barend.se>
2022-01-05 11:11:27 +0100

    Prune Image Cropper and Media Picker (v3) values (#11805)
    * Clean up redundant/default Umbraco.ImageCropper data
    
    * Fix ToString() and add HasCrops() method
    
    * Re-use crop/focal point pruning for Umbraco.MediaPicker3
    
    * Fix ImageCropperTest
    
    Co-authored-by: Elitsa Marinovska <elm@umbraco.dk>

--- src/Umbraco.Tests/PropertyEditors/ImageCropperTest.cs
+++ src/Umbraco.Tests/PropertyEditors/ImageCropperTest.cs
@@ -29,13 +29,13 @@ namespace Umbraco.Tests.PropertyEditors
     {
         private const string CropperJson1 = "{\"focalPoint\": {\"left\": 0.96,\"top\": 0.80827067669172936},\"src\": \"/media/1005/img_0671.jpg\",\"crops\": [{\"alias\":\"thumb\",\"width\": 100,\"height\": 100,\"coordinates\": {\"x1\": 0.58729977382575338,\"y1\": 0.055768992440203169,\"x2\": 0,\"y2\": 0.32457553600198386}}]}";
         private const string CropperJson2 = "{\"focalPoint\": {\"left\": 0.98,\"top\": 0.80827067669172936},\"src\": \"/media/1005/img_0672.jpg\",\"crops\": [{\"alias\":\"thumb\",\"width\": 100,\"height\": 100,\"coordinates\": {\"x1\": 0.58729977382575338,\"y1\": 0.055768992440203169,\"x2\": 0,\"y2\": 0.32457553600198386}}]}";
-        private const string CropperJson3 = "{\"focalPoint\": {\"left\": 0.98,\"top\": 0.80827067669172936},\"src\": \"/media/1005/img_0672.jpg\",\"crops\": []}";
+        private const string CropperJson3 = "{\"focalPoint\": {\"left\": 0.5,\"top\": 0.5},\"src\": \"/media/1005/img_0672.jpg\",\"crops\": []}";
         private const string MediaPath = "/media/1005/img_0671.jpg";
 
         [Test]
         public void CanConvertImageCropperDataSetSrcToString()
         {
-            //cropperJson3 - has not crops
+            //cropperJson3 - has no crops
             var cropperValue = CropperJson3.DeserializeImageCropperValue();
             var serialized = cropperValue.TryConvertTo<string>();
             Assert.IsTrue(serialized.Success);
@@ -45,7 +45,7 @@ namespace Umbraco.Tests.PropertyEditors
         [Test]
         public void CanConvertImageCropperDataSetJObject()
         {
-            //cropperJson3 - has not crops
+            //cropperJson3 - has no crops
             var cropperValue = CropperJson3.DeserializeImageCropperValue();
             var serialized = cropperValue.TryConvertTo<JObject>();
             Assert.IsTrue(serialized.Success);
