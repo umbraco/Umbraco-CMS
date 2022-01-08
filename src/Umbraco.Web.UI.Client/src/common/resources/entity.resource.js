aae86946180a9998254abebebcaf16be0e49396f
Lantzify <lithvia@gmail.com>
2022-01-08 15:44:01 +0100

    Added description to entityResource.getSafeAlias

--- src/Umbraco.Web.UI.Client/src/common/resources/entity.resource.js
+++ src/Umbraco.Web.UI.Client/src/common/resources/entity.resource.js
@@ -36,6 +36,27 @@ function entityResource($q, $http, umbRequestHelper) {
     //the factory object returned
     return {
 
+        /**
+         * @ngdoc method
+         * @name umbraco.resources.entityResource#getSafeAlias
+         * @methodOf umbraco.resources.entityResource
+         *
+         * @description
+         * Converts the given string to a safe alias
+         *
+         * ##usage
+         * <pre>
+         * entityResource.getSafeAlias(value, camelCase)
+         *    .then(function(safeAlias) {
+         *        Do stuff...
+         *    });
+         * </pre>
+         *
+         * @param {string} value the value to convert to a safe alias
+         * @param {boolean} camelCase if camel casing should be used
+         * @returns {Promise} resourcePromise object containing the safe alias.
+         *
+         */
         getSafeAlias: function (value, camelCase) {
 
             if (!value) {
