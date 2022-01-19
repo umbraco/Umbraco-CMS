0392f3d60bf72dca15a04638f126227984fd8284
Elitsa Marinovska <elm@umbraco.dk>
2022-01-19 16:09:52 +0100

    Adding/Editing js files

--- src/Umbraco.Web.UI.Client/src/common/directives/components/media/umbmedianodeinfo.directive.js
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/media/umbmedianodeinfo.directive.js
@@ -8,22 +8,6 @@
             var evts = [];
 
             scope.allowChangeMediaType = false;
-            scope.loading = true;
-
-            scope.changeContentPageNumber = changeContentPageNumber;
-            scope.contentOptions = {};
-            scope.contentOptions.entityType = "DOCUMENT";
-            scope.hasContentReferences = false;
-
-            scope.changeMediaPageNumber = changeMediaPageNumber;
-            scope.mediaOptions = {};
-            scope.mediaOptions.entityType = "MEDIA";
-            scope.hasMediaReferences = false;
-
-            scope.changeMemberPageNumber = changeMemberPageNumber;
-            scope.memberOptions = {};
-            scope.memberOptions.entityType = "MEMBER";
-            scope.hasMemberReferences = false;
 
             function onInit() {
 
@@ -110,45 +94,6 @@
                 setMediaExtension();
             });
 
-            function changeContentPageNumber(pageNumber) {
-                scope.contentOptions.pageNumber = pageNumber;
-                loadContentRelations();
-            }
-
-            function changeMediaPageNumber(pageNumber) {
-                scope.mediaOptions.pageNumber = pageNumber;
-                loadMediaRelations();
-            }
-
-            function changeMemberPageNumber(pageNumber) {
-                scope.memberOptions.pageNumber = pageNumber;
-                loadMemberRelations();
-            }
-
-            function loadContentRelations() {
-                return mediaResource.getPagedReferences(scope.node.id, scope.contentOptions)
-                    .then(function (data) {
-                        scope.contentReferences = data;
-                        scope.hasContentReferences = data.items.length > 0;
-                    });
-            }
-
-            function loadMediaRelations() {
-                return mediaResource.getPagedReferences(scope.node.id, scope.mediaOptions)
-                    .then(data => {
-                        scope.mediaReferences = data;
-                        scope.hasMediaReferences = data.items.length > 0;
-                    });
-            }
-
-            function loadMemberRelations() {
-                return mediaResource.getPagedReferences(scope.node.id, scope.memberOptions)
-                    .then(data => {
-                        scope.memberReferences = data;
-                        scope.hasMemberReferences = data.items.length > 0;
-                    });
-            }
-
             //ensure to unregister from all events!
             scope.$on('$destroy', function () {
                 for (var e in evts) {
@@ -157,18 +102,6 @@
             });
 
             onInit();
-
-            // load media type references when the 'info' tab is first activated/switched to
-            evts.push(eventsService.on("app.tabChange", function (event, args) {
-                $timeout(function () {
-                    if (args.alias === "umbInfo") {
-
-                        $q.all([loadContentRelations(), loadMediaRelations(), loadMemberRelations()]).then(function () {
-                            scope.loading = false;
-                        });
-                    }
-                });
-            }));
         }
 
         var directive = {
---
+++ src/Umbraco.Web.UI.Client/src/common/directives/components/references/umbtrackedreferences.component.js
@@ -0,0 +1,135 @@
+(function () {
+    'use strict';
+
+    /**
+     * A component to render the tracked references of an item
+     */
+
+    function umbTrackedReferencesController($q, trackedReferencesResource) {
+
+        var vm = this;
+
+        vm.loading = true;
+      
+        vm.changeContentPageNumber = changeContentPageNumber;
+        vm.contentOptions = {};
+        vm.contentOptions.entityType = "DOCUMENT";
+        vm.hasContentReferences = false;
+        vm.hasContentReferencesInDescendants = false;
+
+        vm.changeMediaPageNumber = changeMediaPageNumber;
+        vm.mediaOptions = {};
+        vm.mediaOptions.entityType = "MEDIA";
+        vm.hasMediaReferences = false;
+        vm.hasMediaReferencesInDescendants = false;
+
+        vm.changeMemberPageNumber = changeMemberPageNumber;
+        vm.memberOptions = {};
+        vm.memberOptions.entityType = "MEMBER";
+        vm.hasMemberReferences = false;
+        vm.hasMemberReferencesInDescendants = false;
+
+        vm.$onInit = onInit;
+
+        function onInit() {
+
+            $q.all([loadContentRelations(), loadMediaRelations(), loadMemberRelations()]).then(function () {
+                
+                if (vm.hasContentReferences && vm.hasMediaReferences && vm.hasMemberReferences) {
+                    vm.loading = false;
+                } else {
+                    var descendantsPromises = [];
+
+                    if (!vm.hasContentReferences) {
+                        descendantsPromises.push(checkContentDescendantsUsage());
+                    }
+
+                    if (!vm.hasMediaReferences) {
+                        descendantsPromises.push(checkMediaDescendantsUsage());
+                    }
+
+                    if (!vm.hasMemberReferences) {
+                        descendantsPromises.push(checkMemberDescendantsUsage())
+                    }
+
+                    $q.all(descendantsPromises).then(function() {
+                        vm.loading = false;
+                    });
+                }
+            });
+        }
+
+        function changeContentPageNumber(pageNumber) {
+            vm.contentOptions.pageNumber = pageNumber;
+            loadContentRelations();
+        }
+
+        function changeMediaPageNumber(pageNumber) {
+            vm.mediaOptions.pageNumber = pageNumber;
+            loadMediaRelations();
+        }
+
+        function changeMemberPageNumber(pageNumber) {
+            vm.memberOptions.pageNumber = pageNumber;
+            loadMemberRelations();
+        }
+
+        function loadContentRelations() {
+            return trackedReferencesResource.getPagedReferences(vm.id, vm.contentOptions)
+                .then(function (data) {
+                    vm.contentReferences = data;
+                    vm.hasContentReferences = data.items.length > 0;
+                });
+        }
+
+        function loadMediaRelations() {
+            return trackedReferencesResource.getPagedReferences(vm.id, vm.mediaOptions)
+                .then(function (data) {
+                    vm.mediaReferences = data;
+                    vm.hasMediaReferences = data.items.length > 0;
+                });
+        }
+
+        function loadMemberRelations() {
+            return trackedReferencesResource.getPagedReferences(vm.id, vm.memberOptions)
+                .then(function (data) {
+                    vm.memberReferences = data;
+                    vm.hasMemberReferences = data.items.length > 0;
+                });
+        }
+
+        function checkContentDescendantsUsage() {
+           return trackedReferencesResource.hasReferencesInDescendants(vm.id, vm.contentOptions.entityType)
+                .then(function (data) {
+                    vm.hasContentReferencesInDescendants = data;
+                });
+        }
+
+        function checkMediaDescendantsUsage() {
+            return trackedReferencesResource.hasReferencesInDescendants(vm.id, vm.mediaOptions.entityType)
+                .then(function (data) {
+                    vm.hasMediaReferencesInDescendants = data;
+                });
+        }
+
+        function checkMemberDescendantsUsage() {
+            return trackedReferencesResource.hasReferencesInDescendants(vm.id, vm.memberOptions.entityType)
+                .then(function (data) {
+                    vm.hasMemberReferencesInDescendants = data;
+                });
+        }
+    }
+
+    var umbTrackedReferencesComponent = {
+        templateUrl: 'views/components/references/umb-tracked-references.html',
+        transclude: true,
+        bindings: {
+            id: "<"
+        },
+        controllerAs: 'vm',
+        controller: umbTrackedReferencesController
+    };
+
+    angular.module('umbraco.directives').component('umbTrackedReferences', umbTrackedReferencesComponent);
+
+})();
