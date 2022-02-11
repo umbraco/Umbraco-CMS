55088be0ef95895a6dc3da712cf819f594f23283
Matt Brailsford <me@mattbrailsford.com>
2022-02-11 19:31:19 +0000

    Added HostingEnvironmentExtensions.cs

---
+++ src/Umbraco.Core/Hosting/HostingEnvironmentExtensions.cs
@@ -0,0 +1,31 @@
+using System.IO;
+
+namespace Umbraco.Cms.Core.Hosting
+{
+    public static class HostingEnvironmentExtensions
+    {
+        private static bool? s_isCaseSensitiveFileSystem;
+
+        /// <summary>
+        /// Check to see if the system is a case sensitive filesytem
+        /// </summary>
+        public static bool IsCaseSensitiveFileSystem(this IHostingEnvironment hostingEnv)
+        {
+            if (!s_isCaseSensitiveFileSystem.HasValue)
+            {
+                var tempDir = hostingEnv.MapPathContentRoot(Constants.SystemDirectories.TempData);
+
+                // Ensure the temp dir exists
+                if (!Directory.Exists(tempDir))
+                {
+                    Directory.CreateDirectory(tempDir);
+                }
+
+                // Check whether it can be found if the casing is changed
+                s_isCaseSensitiveFileSystem = !Directory.Exists(tempDir.ToUpper()) || !Directory.Exists(tempDir.ToLower());
+            }
+
+            return s_isCaseSensitiveFileSystem.Value;
+        }
+    }
+}
