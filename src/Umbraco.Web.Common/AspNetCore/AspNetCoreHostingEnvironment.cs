7637ee8f0050306eee5cfdfd002612314806194c
Matt Brailsford <me@mattbrailsford.com>
2022-02-11 19:08:30 +0000

    Undo last changes

--- src/Umbraco.Web.Common/AspNetCore/AspNetCoreHostingEnvironment.cs
+++ src/Umbraco.Web.Common/AspNetCore/AspNetCoreHostingEnvironment.cs
@@ -25,8 +25,6 @@ namespace Umbraco.Cms.Web.Common.AspNetCore
         private string _localTempPath;
         private UrlMode _urlProviderMode;
 
-        private static bool? s_isCaseSensitiveFileSystem;
-
         public AspNetCoreHostingEnvironment(
             IServiceProvider serviceProvider,
             IOptionsMonitor<HostingSettings> hostingSettings,
@@ -200,18 +198,5 @@ namespace Umbraco.Cms.Web.Common.AspNetCore
                 }
             }
         }
-    }
-
-    /// <inheritdoc/>
-    public bool IsCaseSensitiveFileSystem()
-    {
-        if (!s_isCaseSensitiveFileSystem.HasValue)
-        {
-            var tmp = Path.GetTempPath();
-
-            s_isCaseSensitiveFileSystem = !Directory.Exists(tmp.ToUpper()) || !Directory.Exists(tmp.ToLower());
-        }
-
-        return s_isCaseSensitiveFileSystem.Value;
     }
 }
