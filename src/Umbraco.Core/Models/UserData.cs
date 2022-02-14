21b7d70c137e83b3ee48d4302950d417033394f9
Ronald Barendse <ronald@barend.se>
2022-02-14 15:55:49 +0100

    Re-use server telemetry data (and fix disposing of process)

--- src/Umbraco.Core/Models/UserData.cs
+++ src/Umbraco.Core/Models/UserData.cs
@@ -1,4 +1,4 @@
-﻿using System.Runtime.Serialization;
+using System.Runtime.Serialization;
 
 namespace Umbraco.Cms.Core.Models
 {
@@ -7,6 +7,7 @@ namespace Umbraco.Cms.Core.Models
     {
         [DataMember(Name = "name")]
         public string Name { get; }
+
         [DataMember(Name = "data")]
         public string Data { get; }
 
--- src/Umbraco.Core/Services/UserDataService.cs
+++ src/Umbraco.Core/Services/UserDataService.cs
@@ -1,10 +1,9 @@
-﻿using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
+using System.Collections.Generic;
 using System.Runtime.InteropServices;
 using System.Threading;
 using Umbraco.Cms.Core.Configuration;
 using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Telemetry.DataCollectors;
 using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Core.Services
@@ -23,26 +22,13 @@ namespace Umbraco.Cms.Core.Services
         public IEnumerable<UserData> GetUserData() =>
             new List<UserData>
             {
-                new("Server OS", RuntimeInformation.OSDescription),
-                new("Server Framework", RuntimeInformation.FrameworkDescription),
-                new("Default Language", _localizationService.GetDefaultLanguageIsoCode()),
-                new("Umbraco Version", _version.SemanticVersion.ToSemanticStringWithoutBuild()),
-                new("Current Culture", Thread.CurrentThread.CurrentCulture.ToString()),
-                new("Current UI Culture", Thread.CurrentThread.CurrentUICulture.ToString()),
-                new("Current Webserver", GetCurrentWebServer())
+                new ("Server OS", RuntimeInformation.OSDescription),
+                new ("Server Framework", RuntimeInformation.FrameworkDescription),
+                new ("Default Language", _localizationService.GetDefaultLanguageIsoCode()),
+                new ("Umbraco Version", _version.SemanticVersion.ToSemanticStringWithoutBuild()),
+                new ("Current Culture", Thread.CurrentThread.CurrentCulture.ToString()),
+                new ("Current UI Culture", Thread.CurrentThread.CurrentUICulture.ToString()),
+                new ("Current Webserver", SystemInformationTelemetryDataCollector.GetServer())
             };
-
-        private string GetCurrentWebServer() => IsRunningInProcessIIS() ? "IIS" : "Kestrel";
-
-        public bool IsRunningInProcessIIS()
-        {
-            if (!RuntimeInformation.IsOSPlatform(OSPlatform.Windows))
-            {
-                return false;
-            }
-
-            string processName = Path.GetFileNameWithoutExtension(Process.GetCurrentProcess().ProcessName);
-            return (processName.Contains("w3wp") || processName.Contains("iisexpress"));
-        }
     }
 }
