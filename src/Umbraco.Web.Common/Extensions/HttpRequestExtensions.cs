36b6e3251a401e476115bae442a9195cce9c97e0
Matt Brailsford <me@mattbrailsford.com>
2022-02-18 12:19:04 +0000

    Added missing using statement

--- src/Umbraco.Web.Common/Extensions/HttpRequestExtensions.cs
+++ src/Umbraco.Web.Common/Extensions/HttpRequestExtensions.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Http.Extensions;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Primitives;
 using Umbraco.Cms.Core.Configuration.Models;
 using Umbraco.Cms.Core.Routing;
 
