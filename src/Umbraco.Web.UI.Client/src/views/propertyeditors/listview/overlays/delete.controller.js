22ad1f0f155bb349e7cdbd3779a59201cbb4b124
Elitsa Marinovska <elm@umbraco.dk>
2022-02-24 11:32:19 +0100

    Disable deletion of list view items

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/listview/overlays/delete.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/listview/overlays/delete.controller.js
@@ -5,6 +5,7 @@
 
         var vm = this;
         vm.loading = true;
+        vm.disableDelete = false;
 
         function onInit() {
             $scope.model.hideSubmitButton = true;
@@ -16,6 +17,13 @@
 
         vm.onReferencesWarning = () => {
             $scope.model.submitButtonStyle = "danger";
+
+            // check if the deletion of items that have references has been disabled
+            if (Umbraco.Sys.ServerVariables.umbracoSettings.disableDeleteWhenReferenced) {
+                // this will only be set to true if we have a warning, indicating that this item or its descendants have reference
+                vm.disableDelete = true;
+                $scope.model.disableSubmitButton = true;
+            }
 
             localizationService.localize("general_delete").then(function (action) {
               localizationService.localize("references_listViewDialogWarning", [action.toLowerCase()]).then((value) => {
