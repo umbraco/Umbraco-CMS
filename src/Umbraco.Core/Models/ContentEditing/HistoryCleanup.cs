4f89cb1a341a169c4f1155d092024a33725c9704
nikolajlauridsen <nel@umbraco.dk>
2022-02-24 13:12:02 +0100

    Make HistoryCleanup inherit from BeingDirtyBase and implement dirty properties

--- src/Umbraco.Core/Models/ContentEditing/HistoryCleanup.cs
+++ src/Umbraco.Core/Models/ContentEditing/HistoryCleanup.cs
@@ -1,17 +1,37 @@
 using System.Runtime.Serialization;
+using Umbraco.Core.Models.Entities;
 
 namespace Umbraco.Core.Models.ContentEditing
 {
     [DataContract(Name = "historyCleanup", Namespace = "")]
-    public class HistoryCleanup
+    public class HistoryCleanup : BeingDirtyBase
     {
+        private bool _preventCleanup;
+
         [DataMember(Name = "preventCleanup")]
-        public bool PreventCleanup { get; set; }
+        public bool PreventCleanup
+        {
+            get => _preventCleanup;
+            set => SetPropertyValueAndDetectChanges(value, ref _preventCleanup, nameof(PreventCleanup));
+        }
+
+        private int? _keepAllVersionsNewerThanDays;
 
         [DataMember(Name = "keepAllVersionsNewerThanDays")]
-        public int? KeepAllVersionsNewerThanDays { get; set; }
+        public int? KeepAllVersionsNewerThanDays
+        {
+            get => _keepAllVersionsNewerThanDays;
+            set => SetPropertyValueAndDetectChanges(value, ref _keepAllVersionsNewerThanDays, nameof(KeepAllVersionsNewerThanDays));
+        }
+
+        private int? _keepLatestVersionPerDayForDays;
 
         [DataMember(Name = "keepLatestVersionPerDayForDays")]
-        public int? KeepLatestVersionPerDayForDays { get; set; }
+        public int? KeepLatestVersionPerDayForDays
+        {
+            get => _keepLatestVersionPerDayForDays;
+            set => SetPropertyValueAndDetectChanges(value, ref _keepLatestVersionPerDayForDays,
+                nameof(_keepAllVersionsNewerThanDays));
+        }
     }
 }
