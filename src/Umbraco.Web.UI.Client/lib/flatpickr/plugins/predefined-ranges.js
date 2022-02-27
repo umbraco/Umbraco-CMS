66c7a537a802e1a42ae577cf12027d172823129f
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2022-02-27 15:38:36 +0100

    Rewrite for vanilla JS

--- src/Umbraco.Web.UI.Client/lib/flatpickr/plugins/predefined-ranges.js
+++ src/Umbraco.Web.UI.Client/lib/flatpickr/plugins/predefined-ranges.js
@@ -1,33 +1,48 @@
-(function ($) {
+(function () {
+  'use strict';
 
-  if (typeof window.myFlatpickrPlugins === 'undefined') {
+  if (typeof window.umbFlatpickrPlugins === 'undefined') {
     window.umbFlatpickrPlugins = [];
   }
 
   var predefinedRanges = function () {
     return function (fp) {
+
+      let rangesNav = document.createElement('ul');
+      rangesNav.className = "nav flex-column flatpickr-predefined-ranges";
+
       const pluginData = {
         ranges: typeof fp.config.ranges !== 'undefined' ? fp.config.ranges : {},
         rangesOnly: typeof fp.config.rangesOnly === 'undefined' || fp.config.rangesOnly,
         rangesAllowCustom: typeof fp.config.rangesAllowCustom === 'undefined' || fp.config.rangesAllowCustom,
         rangesCustomLabel: typeof fp.config.rangesCustomLabel !== 'undefined' ? fp.config.rangesCustomLabel : 'Custom Range',
-        rangesNav: $('<ul>').addClass('nav flex-column flatpickr-predefined-ranges'),
+        rangesNav: rangesNav,
         rangesButtons: {}
       };
 
       /**
        * @param {string} label
-       * @returns {jQuery}
+       * @returns HTML Element
        */
       const addRangeButton = function (label) {
-        pluginData.rangesButtons[label] = $('<button>')
-          .addClass('nav-link btn btn-link')
-          .attr('type', 'button')
-          .text(label);
 
-        pluginData.rangesNav.append(
-          $('<li>').addClass('nav-item d-grid').append(pluginData.rangesButtons[label])
-        );
+        let button = document.createElement('button');
+        button.type = "button";
+        button.className = "nav-link btn btn-link";
+        button.innerText = label;
+
+        pluginData.rangesButtons[label] = button;
+
+        let item = document.createElement('li');
+        item.className = "nav-item d-grid";
+
+        item.appendChild(pluginData.rangesButtons[label]);
+
+        pluginData.rangesNav.appendChild(item);
+
+        console.log("pluginData.rangesNav", pluginData.rangesNav);
+        console.log("rangeButton", pluginData.rangesButtons[label]);
+
         return pluginData.rangesButtons[label];
       };
 
@@ -45,14 +60,18 @@
        */
       const selectActiveRangeButton = function (selectedDates) {
         let isPredefinedRange = false;
-        pluginData.rangesNav.find('.active').removeClass('active');
+        let current = pluginData.rangesNav.querySelector('.active');
+
+        if (current) {
+          current.classList.remove('active');
+        }
 
         if (selectedDates.length > 0) {
           let startDate = moment(selectedDates[0]);
           let endDate = selectedDates.length > 1 ? moment(selectedDates[1]) : startDate;
           for (const [label, range] of Object.entries(pluginData.ranges)) {
             if (startDate.isSame(moment(range[0]), 'day') && endDate.isSame(moment(range[1]), 'day')) {
-              pluginData.rangesButtons[label].addClass('active');
+              pluginData.rangesButtons[label].classList.add('active');
               isPredefinedRange = true;
               break;
             }
@@ -63,10 +82,10 @@
           !isPredefinedRange &&
           pluginData.rangesButtons.hasOwnProperty(pluginData.rangesCustomLabel)
         ) {
-          pluginData.rangesButtons[pluginData.rangesCustomLabel].addClass('active');
-          $(fp.calendarContainer).removeClass('flatpickr-predefined-ranges-only');
+          pluginData.rangesButtons[pluginData.rangesCustomLabel].classList.add('active');
+          fp.calendarContainer.classList.remove('flatpickr-predefined-ranges-only');
         } else if (pluginData.rangesOnly) {
-          $(fp.calendarContainer).addClass('flatpickr-predefined-ranges-only');
+          fp.calendarContainer.classList.add('flatpickr-predefined-ranges-only');
         }
       };
 
@@ -77,27 +96,25 @@
          */
         onReady(selectedDates) {
           for (const [label, range] of Object.entries(pluginData.ranges)) {
-            addRangeButton(label)
-              .on('click', function () {
-                $(this).blur();
-                fp.setDate([moment(range[0]).toDate(), moment(range[1]).toDate()], true, );
+            addRangeButton(label).addEventListener('click', function() {
+                this.blur();
+                fp.setDate([moment(range[0]).toDate(), moment(range[1]).toDate()], true,);
                 fp.close();
               });
           }
 
-          if (pluginData.rangesNav.children().length > 0) {
+          if (pluginData.rangesNav.children.length > 0) {
             if (pluginData.rangesOnly && pluginData.rangesAllowCustom) {
-              addRangeButton(pluginData.rangesCustomLabel)
-                .on('click', function () {
-                  $(this).blur();
-                  pluginData.rangesNav.find('.active').removeClass('active');
-                  $(this).addClass('active');
-                  $(fp.calendarContainer).removeClass('flatpickr-predefined-ranges-only');
+              addRangeButton(pluginData.rangesCustomLabel).addEventListener('click', function () {
+                  this.blur();
+                  pluginData.rangesNav.querySelector('.active').classList.remove('active');
+                  this.classList.add('active');
+                  fp.calendarContainer.classList.remove('flatpickr-predefined-ranges-only');
                 });
             }
 
-            $(fp.calendarContainer).prepend(pluginData.rangesNav);
-            $(fp.calendarContainer).addClass('flatpickr-has-predefined-ranges');
+            fp.calendarContainer.prepend(pluginData.rangesNav);
+            fp.calendarContainer.classList.add('flatpickr-has-predefined-ranges');
             // make sure the right range button is active for the default value
             selectActiveRangeButton(selectedDates);
           }
@@ -117,4 +134,4 @@
 
   window.umbFlatpickrPlugins.push(new predefinedRanges());
 
-})(jQuery);
+})();
