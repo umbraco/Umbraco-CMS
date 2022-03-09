f7cd029564dd978d533dd7ee372e688499b57c34
Nikolaj <nikolajlauridsen@protonmail.ch>
2022-03-09 10:59:33 +0100

    Add new columns to the member table

---
+++ src/Umbraco.Infrastructure/Migrations/Upgrade/V_10_0_0/AddMemberPropertiesAsColumns.cs
@@ -0,0 +1,24 @@
+﻿using System.Linq;
+using Umbraco.Cms.Infrastructure.Persistence.Dtos;
+
+namespace Umbraco.Cms.Infrastructure.Migrations.Upgrade.V_10_0_0;
+
+public class AddMemberPropertiesAsColumns : MigrationBase
+{
+    public AddMemberPropertiesAsColumns(IMigrationContext context) : base(context)
+    {
+    }
+
+    protected override void Migrate()
+    {
+        var columns = SqlSyntax.GetColumnsInSchema(Context.Database).ToList();
+
+        AddColumnIfNotExists<MemberDto>(columns, "failedPasswordAttempts");
+        AddColumnIfNotExists<MemberDto>(columns, "isLockedOut");
+        AddColumnIfNotExists<MemberDto>(columns, "lastLoginDate");
+        AddColumnIfNotExists<MemberDto>(columns, "lastLockoutDate");
+        AddColumnIfNotExists<MemberDto>(columns, "lastPasswordChangeDate");
+
+        // TODO: Migrate existing data.
+    }
+}
