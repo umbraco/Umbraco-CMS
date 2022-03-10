92480b8446e498ffefe36fbaf83a42a65ed2d180
Elitsa Marinovska <elm@umbraco.dk>
2022-03-10 16:29:56 +0100

    Changes in the editors so we can track the items

--- src/Umbraco.Core/PropertyEditors/ParameterEditors/MultipleMediaPickerParameterEditor.cs
+++ src/Umbraco.Core/PropertyEditors/ParameterEditors/MultipleMediaPickerParameterEditor.cs
@@ -1,5 +1,8 @@
-﻿using Microsoft.Extensions.Logging;
-using Umbraco.Cms.Core.Hosting;
+using System;
+using System.Collections.Generic;
+using Umbraco.Cms.Core.IO;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Models.Editors;
 using Umbraco.Cms.Core.Serialization;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Core.Strings;
@@ -25,6 +28,56 @@ namespace Umbraco.Cms.Core.PropertyEditors.ParameterEditors
             : base(dataValueEditorFactory)
         {
             DefaultConfiguration.Add("multiPicker", "1");
+        }
+
+        protected override IDataValueEditor CreateValueEditor() => DataValueEditorFactory.Create<MultipleMediaPickerPropertyValueEditor>(Attribute);
+
+        internal class MultipleMediaPickerPropertyValueEditor : DataValueEditor, IDataValueReference
+        {
+            private readonly IEntityService _entityService;
+
+            public MultipleMediaPickerPropertyValueEditor(
+                ILocalizedTextService localizedTextService,
+                IShortStringHelper shortStringHelper,
+                IJsonSerializer jsonSerializer,
+                IIOHelper ioHelper,
+                DataEditorAttribute attribute,
+                IEntityService entityService)
+                : base(localizedTextService, shortStringHelper, jsonSerializer, ioHelper, attribute)
+            {
+                _entityService = entityService;
+            }
+
+            public IEnumerable<UmbracoEntityReference> GetReferences(object value)
+            {
+                var asString = value is string str ? str : value?.ToString();
+
+                if (string.IsNullOrEmpty(asString))
+                {
+                    yield break;
+                }
+
+                foreach (var udiStr in asString.Split(','))
+                {
+                    if (UdiParser.TryParse(udiStr, out Udi udi))
+                    {
+                        yield return new UmbracoEntityReference(udi);
+                    }
+
+                    // this is needed to support the legacy case when the multiple media picker parameter editor stores ints not udis
+                    if (int.TryParse(udiStr, out var id))
+                    {
+                        Attempt<Guid> guidAttempt = _entityService.GetKey(id, UmbracoObjectTypes.Media);
+                        Guid guid = guidAttempt.Success ? guidAttempt.Result : Guid.Empty;
+
+                        if (guid != Guid.Empty)
+                        {
+                            yield return new UmbracoEntityReference(new GuidUdi(Constants.UdiEntityType.Media, guid));
+                        }
+
+                    }
+                }
+            }
         }
     }
 }
