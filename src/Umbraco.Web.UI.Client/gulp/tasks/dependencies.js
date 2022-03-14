fb842dc21362be363a3b443b0de7aafb060121d9
Warren Buckley <warren@umbraco.com>
2022-03-14 14:03:10 +0000

    Include the font asset files used in UUI-css

--- src/Umbraco.Web.UI.Client/gulp/tasks/dependencies.js
+++ src/Umbraco.Web.UI.Client/gulp/tasks/dependencies.js
@@ -285,7 +285,8 @@ function dependencies() {
             "src": [
                 "./node_modules/@umbraco-ui/uui/dist/uui.min.js",
                 "./node_modules/@umbraco-ui/uui/dist/uui.min.js.map",
-                "./node_modules/@umbraco-ui/uui-css/dist/uui-css.css"
+                "./node_modules/@umbraco-ui/uui-css/dist/uui-css.css",
+                "./node_modules/@umbraco-ui/uui-css/assets/fonts/**"
             ],
             "base": "./node_modules/@umbraco-ui"
         }
