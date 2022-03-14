f9be9be75d072943e74d82c6879657476966eb02
Warren Buckley <warren@umbraco.com>
2022-03-14 13:49:38 +0000

    Include the UUI-CSS into the Smidge Bundle

--- src/Umbraco.Infrastructure/WebAssets/BackOfficeWebAssets.cs
+++ src/Umbraco.Infrastructure/WebAssets/BackOfficeWebAssets.cs
@@ -59,6 +59,7 @@ namespace Umbraco.Cms.Infrastructure.WebAssets
                 BundlingOptions.NotOptimizedAndComposite,
                 FormatPaths(
                     "assets/css/umbraco.min.css",
+                    "lib/umbraco-ui/uui-css/dist/uui-css.css",
                     "lib/bootstrap-social/bootstrap-social.css",
                     "lib/font-awesome/css/font-awesome.min.css"));
 
