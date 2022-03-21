30bb416ff8e59bddec59e22c5dbb3dea06c3b210
Sebastiaan Janssen <sebastiaan@umbraco.com>
2022-03-21 14:48:55 +0100

    Add missing null checks for redirect URLs (#12154)
    * Fixes missing null checks as reported in #10066
    
    * Fixed check for null or empty in NPoco where clause.
    
    Co-authored-by: Andy Butland <abutland73@gmail.com>

--- src/Umbraco.Core/Persistence/Repositories/Implement/RedirectUrlRepository.cs
+++ src/Umbraco.Core/Persistence/Repositories/Implement/RedirectUrlRepository.cs
@@ -174,13 +174,13 @@ JOIN umbracoNode ON umbracoRedirectUrl.contentKey=umbracoNode.uniqueID");
             var urlHash = url.GenerateHash<SHA1>();
             var sql = GetBaseQuery(false)
                 .Where<RedirectUrlDto>(x => x.Url == url && x.UrlHash == urlHash &&
-                    (x.Culture == culture.ToLower() || x.Culture == string.Empty))
+                    (x.Culture == culture.ToLower() || x.Culture == null || x.Culture == string.Empty))
                 .OrderByDescending<RedirectUrlDto>(x => x.CreateDateUtc);
             var dtos = Database.Fetch<RedirectUrlDto>(sql);
             var dto = dtos.FirstOrDefault(f => f.Culture == culture.ToLower());
 
             if (dto == null)
-                dto = dtos.FirstOrDefault(f => f.Culture == string.Empty);
+                dto = dtos.FirstOrDefault(f => string.IsNullOrWhiteSpace(f.Culture));
 
             return dto == null ? null : Map(dto);
         }
