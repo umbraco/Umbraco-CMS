f9b3d65b39b79a9a710caef2bed7c89b0f4742ca
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2022-03-24 15:04:02 +0100

    add styling and a11y to configureTwoFactor view
    - also ensure that the view reloads when changes happen in the custom user view to enable 2fa
    - ensure that view updates when disabling 2fa
    - add extra button to show options (disable) for each 2fa provider

--- src/Umbraco.Web.UI.Client/src/views/common/infiniteeditors/twofactor/configuretwofactor.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/common/infiniteeditors/twofactor/configuretwofactor.controller.js
@@ -1,71 +1,91 @@
-//used for the media picker dialog
+//used for the user editor overlay
 angular.module("umbraco").controller("Umbraco.Editors.ConfigureTwoFactorController",
-    function ($scope,
-              localizationService,
-              twoFactorLoginResource,
-              editorService) {
+  function ($scope,
+    localizationService,
+    notificationsService,
+    overlayService,
+    twoFactorLoginResource,
+    editorService) {
 
 
-        let vm = this;
-        vm.close = close;
-        vm.enable = enable;
-        vm.disable = disable;
-        vm.disableWithCode = disableWithCode;
-        vm.code = "";
+    let vm = this;
+    vm.close = close;
+    vm.enable = enable;
+    vm.disable = disable;
+    vm.disableWithCode = disableWithCode;
+    vm.code = "";
+    vm.buttonState = "init";
 
-        function onInit() {
-
-          localizationService.localize("user_configureTwoFactor").then(function (value) {
-            vm.title = value;
-          });
-
-          twoFactorLoginResource.get2FAProvidersForUser($scope.model.user.id)
-            .then(function(providers) {
-              vm.providers = providers;
-            });
-        }
-
-        function close() {
-            if ($scope.model.close) {
-                $scope.model.close();
-            }
-        }
-
-        function enable(providerName) {
-          twoFactorLoginResource.viewPathForProviderName(providerName)
-            .then(function(viewPath) {
-              var providerSettings = {
-                user: $scope.model.user,
-                providerName: providerName,
-                size: "small",
-                view: viewPath,
-                close: function() {
-                  editorService.close();
-                }
-              };
-
-              editorService.open(providerSettings);
-            });
-
-
-        }
-
-        function disable(providerName) {
-          twoFactorLoginResource.disable(providerName, $scope.model.user.key)
-            .then(function(response) {
-              //TODO change the button to an enable one instead?
-              close();
-            });
-        }
-
-      function disableWithCode(providerName) {
-        twoFactorLoginResource.disableWithCode(providerName, vm.code)
-          .then(function(response) {
-            //TODO change the button to an enable one instead?
-            close();
-          });
-      }
-        //initialize
-        onInit();
-
+    localizationService.localize("user_configureTwoFactor").then(function (value) {
+      vm.title = value;
     });
+
+    function onInit() {
+      vm.code = "";
+
+      twoFactorLoginResource.get2FAProvidersForUser($scope.model.user.id)
+        .then(function (providers) {
+          vm.providers = providers;
+        });
+    }
+
+    function close() {
+      if ($scope.model.close) {
+        $scope.model.close();
+      }
+    }
+
+    function enable(providerName) {
+      twoFactorLoginResource.viewPathForProviderName(providerName)
+        .then(function (viewPath) {
+          var providerSettings = {
+            user: $scope.model.user,
+            providerName: providerName,
+            size: "small",
+            view: viewPath,
+            close: function () {
+              notificationsService.removeAll();
+              editorService.close();
+              onInit();
+            }
+          };
+
+          editorService.open(providerSettings);
+        }).catch(onError);
+    }
+
+    function disable(providerName) {
+      vm.buttonState = "busy";
+      twoFactorLoginResource.disable(providerName, $scope.model.user.key)
+        .then(onResponse)
+        .catch(onError);
+    }
+
+    function disableWithCode(providerName) {
+      vm.buttonState = "busy";
+      twoFactorLoginResource.disableWithCode(providerName, vm.code)
+        .then(onResponse)
+        .catch(onError);
+    }
+
+    function onResponse(response) {
+      if (response) {
+        vm.buttonState = "success";
+        onInit();
+      } else {
+        vm.buttonState = "error";
+        vm.error = "invalid";
+        localizationService.localize("login_2faInvalidCode").then(function (value) {
+          notificationsService.error(value);
+        });
+      }
+    }
+
+    function onError(error) {
+      vm.buttonState = "error";
+      overlayService.ysod(error);
+    }
+
+    //initialize
+    onInit();
+  });
