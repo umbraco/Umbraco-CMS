fa34f117bad5988d9999ea843a27e6ed27e452d3
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-04-06 14:33:37 +0200

    Add MediaTelemetryProvider

---
+++ src/Umbraco.Core/Telemetry/Providers/MediaTelemetryProvider.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Services;
+
+namespace Umbraco.Cms.Core.Telemetry.Providers
+{
+    public class MediaTelemetryProvider : IDetailedTelemetryProvider
+    {
+        private readonly INodeCountService _nodeCountService;
+
+        public MediaTelemetryProvider(INodeCountService nodeCountService)
+        {
+            _nodeCountService = nodeCountService;
+        }
+
+        public IEnumerable<UsageInformation> GetInformation()
+        {
+            var result = new List<UsageInformation>();
+
+            result.Add(new UsageInformation("MediaCount", _nodeCountService.GetMediaCount()));
+
+            return result;
+        }
+    }
+}
