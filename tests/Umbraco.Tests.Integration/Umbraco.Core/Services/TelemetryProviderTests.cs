1d0c8c2ab21a46c976bffd2c35d283c7aa4d2bf4
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-04-06 21:48:57 +0200

    Add more tests and todos for tests

--- tests/Umbraco.Tests.Integration/Umbraco.Core/Services/TelemetryProviderTests.cs
+++ tests/Umbraco.Tests.Integration/Umbraco.Core/Services/TelemetryProviderTests.cs
@@ -1,7 +1,6 @@
 ﻿// Copyright (c) Umbraco.
 // See LICENSE for more details.
 
-using System;
 using System.Collections.Generic;
 using System.Linq;
 using Microsoft.Extensions.DependencyInjection;
@@ -9,13 +8,13 @@ using NUnit.Framework;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Models.Membership;
 using Umbraco.Cms.Core.Services;
-using Umbraco.Cms.Core.Services.Implement;
 using Umbraco.Cms.Core.Telemetry.Providers;
 using Umbraco.Cms.Tests.Common.Builders;
+using Umbraco.Cms.Tests.Common.Builders.Extensions;
 using Umbraco.Cms.Tests.Common.Testing;
 using Umbraco.Cms.Tests.Integration.Testing;
-using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Tests.Integration.Umbraco.Core.Services
 {
@@ -27,16 +26,37 @@ namespace Umbraco.Cms.Tests.Integration.Umbraco.Core.Services
     public class TelemetryProviderTests : UmbracoIntegrationTest
     {
         private IContentTypeService ContentTypeService => GetRequiredService<IContentTypeService>();
+
         private IFileService FileService => GetRequiredService<IFileService>();
+
         private IDomainService DomainService => GetRequiredService<IDomainService>();
+
         private IContentService ContentService => GetRequiredService<IContentService>();
+
         private DomainTelemetryProvider DetailedTelemetryProviders => GetRequiredService<DomainTelemetryProvider>();
+
         private ContentTelemetryProvider ContentTelemetryProvider => GetRequiredService<ContentTelemetryProvider>();
+
+        private LanguagesTelemetryProvider LanguagesTelemetryProvider => GetRequiredService<LanguagesTelemetryProvider>();
+
+        private UserTelemetryProvider UserTelemetryProvider => GetRequiredService<UserTelemetryProvider>();
+
+        private ILocalizationService LocalizationService => GetRequiredService<ILocalizationService>();
+
+        private IUserService UserService => GetRequiredService<IUserService>();
+
+        private LanguageBuilder _languageBuilder = new();
+        
+        private UserBuilder _userBuilder = new();
+        
+        private UserGroupBuilder _userGroupBuilder = new();
 
         protected override void CustomTestSetup(IUmbracoBuilder builder)
         {
             builder.Services.AddTransient<DomainTelemetryProvider>();
             builder.Services.AddTransient<ContentTelemetryProvider>();
+            builder.Services.AddTransient<LanguagesTelemetryProvider>();
+            builder.Services.AddTransient<UserTelemetryProvider>();
             base.CustomTestSetup(builder);
         }
 
@@ -52,7 +72,7 @@ namespace Umbraco.Cms.Tests.Integration.Umbraco.Core.Services
 
 
             // Assert
-            Assert.AreEqual(1, result.Count());
+            Assert.AreEqual(1, result.First().Data);
         }
 
         [Test]
@@ -82,7 +102,122 @@ namespace Umbraco.Cms.Tests.Integration.Umbraco.Core.Services
 
 
             // Assert
-            Assert.AreEqual(1, result.Count());
+            // TODO : Test multiple roots, with children + grandchildren
+            Assert.AreEqual(1, result.First().Data);
+        }
+
+        [Test]
+        public void Language_Telemetry_Can_Get_Languages()
+        {
+            // Arrange
+            var langTwo = _languageBuilder.WithCultureInfo("da-DK").Build();
+            var langThree = _languageBuilder.WithCultureInfo("se-SV").Build();
+
+            LocalizationService.Save(langTwo);
+            LocalizationService.Save(langThree);
+
+            IEnumerable<UsageInformation> result = null;
+
+            // Act
+            result = LanguagesTelemetryProvider.GetInformation();
+
+            // Assert
+            Assert.AreEqual(3, result.First().Data);
+        }
+
+        [Test]
+        public void MacroTelemetry_Can_Get_Macros()
+        {
+            // TODO: Test MacroTelemetryProvider by creating macros & asserting telemetry reports the correct number
+        }
+
+        [Test]
+        public void MediaTelemetry_Can_Get_Media()
+        {
+            // TODO: Test MediaTelemetryProvider by creating media & asserting telemetry reports the correct number
+        }
+
+        [Test]
+        public void PropertyEditorTelemetry_Can_Get_PropertyTypes()
+        {
+            // TODO: Test PropertyEditorTelemetryProvider by creating documentTypes and using propertyTypes, and creating custom PropertyTypes
+            // and assert that the telemetry reports the correct numbers/properties
+        }
+
+        [Test]
+        public void UserTelemetry_Can_GetUser()
+        {
+            var result = UserTelemetryProvider.GetInformation().FirstOrDefault(x => x.Name == "UserCount");
+
+            Assert.AreEqual(1, result.Data);
+        }
+
+        [Test]
+        public void UserTelemetry_Can_Get_More_Users()
+        {
+            var user = BuildUser();
+
+            UserService.Save(user);
+
+            var result = UserTelemetryProvider.GetInformation().FirstOrDefault(x => x.Name == "UserCount");
+
+            Assert.AreEqual(2, result.Data);
+        }
+
+        [Test]
+        public void UserTelemetry_Can_Get_Default_UserGroups()
+        {
+            var result = UserTelemetryProvider.GetInformation().FirstOrDefault(x => x.Name == "UserGroupCount");
+
+            Assert.AreEqual(5, result.Data);
+        }
+
+        [Test]
+        public void UserTelemetry_Can_Get_Saved_UserGroups()
+        {
+            var userGroup = BuildUserGroup("testGroup");
+
+            UserService.Save(userGroup);
+            var result = UserTelemetryProvider.GetInformation().FirstOrDefault(x => x.Name == "UserGroupCount");
+
+            Assert.AreEqual(6, result.Data);
+        }
+        
+        [Test]
+        public void UserTelemetry_Can_Get_More_UserGroups()
+        {
+            var userGroups = BuildUserGroups(100);
+
+
+            foreach (var userGroup in userGroups)
+            {
+                UserService.Save(userGroup);
+            }
+
+            var result = UserTelemetryProvider.GetInformation().FirstOrDefault(x => x.Name == "UserGroupCount");
+
+            Assert.AreEqual(105, result.Data);
+        }
+
+        private User BuildUser() =>
+            _userBuilder
+                .WithLogin("username", "test pass")
+                .WithName("Test")
+                .WithEmail("test@test.com")
+                .Build();
+        private IUserGroup BuildUserGroup(string alias) =>
+            _userGroupBuilder
+                .WithAlias(alias)
+                .WithName(alias)
+                .WithAllowedSections(new List<string>(){"A", "B"})
+                .Build();
+
+        private IEnumerable<IUserGroup> BuildUserGroups(int count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                yield return BuildUserGroup(i.ToString());
+            }
         }
     }
 }
