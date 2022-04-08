fb7f323dcccc556c47a15383b1da060e40bb8152
Nikolaj <nikolajlauridsen@protonmail.ch>
2022-04-08 10:41:11 +0200

    Implement MetricsConsentService using KeyValueService

--- src/Umbraco.Core/Models/MetricsConsentService.cs
+++ src/Umbraco.Core/Models/MetricsConsentService.cs
@@ -1,11 +1,36 @@
-﻿using Umbraco.Cms.Core.Services;
+﻿using System;
+using Umbraco.Cms.Core.Services;
 
 namespace Umbraco.Cms.Core.Models
 {
     public class MetricsConsentService : IMetricsConsentService
     {
-        private ConsentLevel _consentLevel = ConsentLevel.Detailed;
-        public ConsentLevel GetConsentLevel() => _consentLevel;
-        public ConsentLevel SetConsentLevel(ConsentLevel consentLevel) => _consentLevel = consentLevel;
+
+        private const string Key = "UmbracoAnalyticsLevel";
+
+        private readonly IKeyValueService _keyValueService;
+
+        public MetricsConsentService(IKeyValueService keyValueService)
+        {
+            _keyValueService = keyValueService;
+        }
+
+        public ConsentLevel GetConsentLevel()
+        {
+            var analyticsLevelString = _keyValueService.GetValue(Key);
+
+            if (analyticsLevelString is null || Enum.TryParse(analyticsLevelString, out ConsentLevel analyticsLevel) is false)
+            {
+                return ConsentLevel.Basic;
+            }
+
+            return analyticsLevel;
+        }
+
+        public ConsentLevel SetConsentLevel(ConsentLevel consentLevel)
+        {
+            _keyValueService.SetValue(Key, consentLevel.ToString());
+            return consentLevel;
+        }
     }
 }
