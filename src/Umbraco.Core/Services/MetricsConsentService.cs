79fbcaa3789fdb65116e9714c2844358a68d63eb
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-04-25 14:42:46 +0200

    Add logging to SetConsentLevel

--- src/Umbraco.Core/Services/MetricsConsentService.cs
+++ src/Umbraco.Core/Services/MetricsConsentService.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using Microsoft.Extensions.Logging;
 using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Security;
 
 namespace Umbraco.Cms.Core.Services
 {
@@ -8,10 +10,22 @@ namespace Umbraco.Cms.Core.Services
         internal const string Key = "UmbracoAnalyticsLevel";
 
         private readonly IKeyValueService _keyValueService;
+        private readonly ILogger<MetricsConsentService> _logger;
+        private readonly IBackOfficeSecurity _backOfficeSecurity;
 
         public MetricsConsentService(IKeyValueService keyValueService)
         {
             _keyValueService = keyValueService;
+        }
+
+        public MetricsConsentService(
+            IKeyValueService keyValueService,
+            ILogger<MetricsConsentService> logger,
+            IBackOfficeSecurity backOfficeSecurity)
+        {
+            _keyValueService = keyValueService;
+            _logger = logger;
+            _backOfficeSecurity = backOfficeSecurity;
         }
 
         public TelemetryLevel GetConsentLevel()
@@ -28,6 +42,8 @@ namespace Umbraco.Cms.Core.Services
 
         public void SetConsentLevel(TelemetryLevel telemetryLevel)
         {
+            var currentUser = _backOfficeSecurity.CurrentUser;
+            _logger.LogInformation($"Telemetry level changed by {currentUser.Name}");
             _keyValueService.SetValue(Key, telemetryLevel.ToString());
         }
     }
