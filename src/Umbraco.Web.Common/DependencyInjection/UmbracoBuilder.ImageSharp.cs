3cae534f264a301ded64982aa3e3544829a92c09
Ronald Barendse <ronald@barend.se>
2022-04-28 11:11:29 +0200

    Use IConfigureOptions to configure ImageSharp options

--- src/Umbraco.Web.Common/DependencyInjection/UmbracoBuilder.ImageSharp.cs
+++ src/Umbraco.Web.Common/DependencyInjection/UmbracoBuilder.ImageSharp.cs
@@ -1,16 +1,9 @@
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Http;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
-using Microsoft.Net.Http.Headers;
 using SixLabors.ImageSharp.Web.Caching;
-using SixLabors.ImageSharp.Web.Commands;
 using SixLabors.ImageSharp.Web.DependencyInjection;
 using SixLabors.ImageSharp.Web.Middleware;
-using SixLabors.ImageSharp.Web.Processors;
 using SixLabors.ImageSharp.Web.Providers;
-using Umbraco.Cms.Core.Configuration.Models;
 using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Cms.Core.Media;
 using Umbraco.Cms.Web.Common.DependencyInjection;
@@ -28,73 +21,18 @@ namespace Umbraco.Extensions
         {
             builder.Services.AddSingleton<IImageUrlGenerator, ImageSharpImageUrlGenerator>();
 
-            ImagingSettings imagingSettings = builder.Config.GetSection(Cms.Core.Constants.Configuration.ConfigImaging)
-                .Get<ImagingSettings>() ?? new ImagingSettings();
-
-            builder.Services.AddImageSharp(options =>
-            {
-                // options.Configuration is set using ImageSharpConfigurationOptions below
-                options.BrowserMaxAge = imagingSettings.Cache.BrowserMaxAge;
-                options.CacheMaxAge = imagingSettings.Cache.CacheMaxAge;
-                options.CacheHashLength = imagingSettings.Cache.CacheHashLength;
-
-                // Use configurable maximum width and height (overwrite ImageSharps default)
-                options.OnParseCommandsAsync = context =>
-                {
-                    if (context.Commands.Count == 0)
-                    {
-                        return Task.CompletedTask;
-                    }
-
-                    int width = context.Parser.ParseValue<int>(context.Commands.GetValueOrDefault(ResizeWebProcessor.Width), context.Culture);
-                    if (width <= 0 || width > imagingSettings.Resize.MaxWidth)
-                    {
-                        context.Commands.Remove(ResizeWebProcessor.Width);
-                    }
-
-                    int height = context.Parser.ParseValue<int>(context.Commands.GetValueOrDefault(ResizeWebProcessor.Height), context.Culture);
-                    if (height <= 0 || height > imagingSettings.Resize.MaxHeight)
-                    {
-                        context.Commands.Remove(ResizeWebProcessor.Height);
-                    }
-
-                    return Task.CompletedTask;
-                };
-                options.OnBeforeSaveAsync = _ => Task.CompletedTask;
-                options.OnProcessedAsync = _ => Task.CompletedTask;
-                options.OnPrepareResponseAsync = context =>
-                {
-                    // Change Cache-Control header when cache buster value is present
-                    if (context.Request.Query.ContainsKey("rnd") || context.Request.Query.ContainsKey("v"))
-                    {
-                        var headers = context.Response.GetTypedHeaders();
-
-                        var cacheControl = headers.CacheControl;
-                        if (cacheControl is not null)
-                        {
-                            cacheControl.MustRevalidate = false;
-                            cacheControl.Extensions.Add(new NameValueHeaderValue("immutable"));
-                        }
-
-                        headers.CacheControl = cacheControl;
-                    }
-
-                    return Task.CompletedTask;
-                };
-            })
+            builder.Services.AddImageSharp()
                 // Replace default image provider
-                .ClearProviders().AddProvider<WebRootImageProvider>()
-                // Configure cache options
-                .Configure<PhysicalFileSystemCacheOptions>(options =>
-                {
-                    options.CacheFolder = builder.BuilderHostingEnvironment.MapPathContentRoot(imagingSettings.Cache.CacheFolder);
-                    options.CacheFolderDepth = imagingSettings.Cache.CacheFolderDepth;
-                })
+                .ClearProviders()
+                .AddProvider<WebRootImageProvider>()
                 // Add custom processors
                 .AddProcessor<CropWebProcessor>();
 
-            // Configure middleware to use the registered/shared ImageSharp configuration
-            builder.Services.AddTransient<IConfigureOptions<ImageSharpMiddlewareOptions>, ImageSharpConfigurationOptions>();
+            // Configure middleware
+            builder.Services.AddTransient<IConfigureOptions<ImageSharpMiddlewareOptions>, ConfigureImageSharpMiddlewareOptions>();
+
+            // Configure cache options
+            builder.Services.AddTransient<IConfigureOptions<PhysicalFileSystemCacheOptions>, ConfigurePhysicalFileSystemCacheOptions>();
 
             return builder.Services;
         }
