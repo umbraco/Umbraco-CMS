fd78faa1a9bf56f12ac6d1972cb5b072fbc6f8b2
Andy Butland <abutland73@gmail.com>
2022-05-05 17:21:01 +0200

    IMediaService.GetRootMedia to be non-nullable.

--- src/Umbraco.Core/Services/IMediaService.cs
+++ src/Umbraco.Core/Services/IMediaService.cs
@@ -130,7 +130,7 @@ namespace Umbraco.Cms.Core.Services
         /// Gets a collection of <see cref="IMedia"/> objects, which reside at the first level / root
         /// </summary>
         /// <returns>An Enumerable list of <see cref="IMedia"/> objects</returns>
-        IEnumerable<IMedia>? GetRootMedia();
+        IEnumerable<IMedia> GetRootMedia();
 
         /// <summary>
         /// Gets a collection of an <see cref="IMedia"/> objects, which resides in the Recycle Bin
