0c8f57c505911fdd2139ed97962a3b9cf373c766
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2022-05-05 16:36:53 +0200

    Add telemetry levels
    Add raw nouislider to select a telemetry level + set chosen level onChange to the user model + show detailed description of each level as a tooltip

--- src/Umbraco.Web.UI.Client/src/installer/steps/user.controller.js
+++ src/Umbraco.Web.UI.Client/src/installer/steps/user.controller.js
@@ -1,8 +1,9 @@
-angular.module("umbraco.install").controller("Umbraco.Install.UserController", function ($scope, installerService) {
+angular.module("umbraco.install").controller("Umbraco.Install.UserController", function ($scope, $sce, installerService) {
 
   $scope.majorVersion = Umbraco.Sys.ServerVariables.application.version;
   $scope.passwordPattern = /.*/;
   $scope.installer.current.model.subscribeToNewsLetter = false;
+  $scope.installer.current.model.telemetryLevel = $scope.installer.current.model.consentLevels[1].level;
 
   if ($scope.installer.current.model.minNonAlphaNumericLength > 0) {
     var exp = "";
@@ -14,36 +15,73 @@ angular.module("umbraco.install").controller("Umbraco.Install.UserController", f
     $scope.passwordPattern = new RegExp(exp);
   }
 
-  $scope.loading = true;
-  let sliderRef = null;
-  $scope.consentLevel = 'Basic';
-  $scope.sliderValue = 2;
-  $scope.sliderVal = $scope.installer.current.model.consentLevels[1];
-  $scope.sliderOptions =
-    {
-      "start": 2,
-      "step": 1,
-      "tooltips": [false],
-      "range": {
-        "min": 1,
-        "max": 3
-      },
-      pips: {
-        mode: 'values',
-        density: 50,
-        values: [1, 2, 3],
-        "format": {
-          to: function (value) {
-            return $scope.consentLevels[value - 1];
-          },
-          from: function (value) {
-            return Number(value);
-          }
+  $scope.consentTooltip = {
+    show: false,
+    event: null
+  };
+
+  const sliderOptions =
+  {
+    "start": 2,
+    "step": 1,
+    "tooltips": [false],
+    "range": {
+      "min": 1,
+      "max": 3
+    },
+    pips: {
+      mode: 'values',
+      density: 50,
+      values: [1, 2, 3],
+      "format": {
+        to: function (value) {
+          return $scope.installer.current.model.consentLevels[value - 1].level;
+        },
+        from: function (value) {
+          return Number(value);
         }
       }
-    };
+    }
+  };
 
-  $scope.loading = false;
+  if ('noUiSlider' in window) {
+    const consentSlider = document.getElementById("consentSlider");
+    if (consentSlider) {
+      window.noUiSlider.create(consentSlider, sliderOptions);
+      consentSlider.noUiSlider.on('change', onChangeConsent);
+
+      const pips = consentSlider.querySelectorAll('.noUi-value');
+
+      $(consentSlider).on('$destroy', function () {
+        consentSlider.noUiSlider.off();
+      });
+
+      pips.forEach(function (pip) {
+        pip.addEventListener('mouseenter', function (e) {
+          $scope.$apply(function () {
+            const value = pip.getAttribute('data-value');
+            $scope.consentTooltip.show = true;
+            $scope.consentTooltip.event = e;
+            $scope.consentTooltip.description = $sce.trustAsHtml($scope.installer.current.model.consentLevels[value - 1].description);
+          });
+        });
+
+        pip.addEventListener('mouseleave', function () {
+          $scope.$apply(function () {
+            $scope.consentTooltip.show = false;
+            $scope.consentTooltip.event = null;
+            $scope.consentTooltip.description = '';
+          });
+        });
+
+        pip.addEventListener('click', function () {
+          const value = pip.getAttribute('data-value');
+          consentSlider.noUiSlider.set(value);
+        });
+      });
+    }
+  }
+
   $scope.validateAndInstall = function () {
     installerService.install();
   };
@@ -54,13 +92,9 @@ angular.module("umbraco.install").controller("Umbraco.Install.UserController", f
     }
   };
 
-  $scope.setup = function (slider) {
-    sliderRef = slider;
-  }
-
-  $scope.sliderChange = function (values) {
+  function onChangeConsent(values) {
     const result = Number(values[0]);
-    $scope.sliderVal = $scope.consentLevels[result - 1];
+    $scope.installer.current.model.telemetryLevel = $scope.installer.current.model.consentLevels[result - 1].level;
   };
 
 });
