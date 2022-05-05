4b1e161effa714f58ed897766b5552569087a6ce
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-05-05 18:28:12 +0200

    Fix MetricsConsentService to log correctly when we set analyticsLevel in installer

--- src/Umbraco.Core/Services/MetricsConsentService.cs
+++ src/Umbraco.Core/Services/MetricsConsentService.cs
@@ -14,6 +14,7 @@ namespace Umbraco.Cms.Core.Services
         private readonly IKeyValueService _keyValueService;
         private readonly ILogger<MetricsConsentService> _logger;
         private readonly IBackOfficeSecurityAccessor _backOfficeSecurityAccessor;
+        private readonly IUserService _userService;
 
         // Scheduled for removal in V12
         [Obsolete("Please use the constructor that takes and ILogger and IBackOfficeSecurity instead")]
@@ -21,7 +22,8 @@ namespace Umbraco.Cms.Core.Services
         : this(
             keyValueService,
             StaticServiceProvider.Instance.GetRequiredService<ILogger<MetricsConsentService>>(),
-            StaticServiceProvider.Instance.GetRequiredService<IBackOfficeSecurityAccessor>())
+            StaticServiceProvider.Instance.GetRequiredService<IBackOfficeSecurityAccessor>(),
+            StaticServiceProvider.Instance.GetRequiredService<IUserService>())
         {
         }
 
@@ -29,10 +31,24 @@ namespace Umbraco.Cms.Core.Services
             IKeyValueService keyValueService,
             ILogger<MetricsConsentService> logger,
             IBackOfficeSecurityAccessor backOfficeSecurityAccessor)
+        : this(keyValueService,
+            logger,
+            backOfficeSecurityAccessor,
+            StaticServiceProvider.Instance.GetRequiredService<IUserService>())
+        {
+        }
+
+        public MetricsConsentService(
+            IKeyValueService keyValueService,
+            ILogger<MetricsConsentService> logger,
+            IBackOfficeSecurityAccessor backOfficeSecurityAccessor,
+            IUserService userService
+            )
         {
             _keyValueService = keyValueService;
             _logger = logger;
             _backOfficeSecurityAccessor = backOfficeSecurityAccessor;
+            _userService = userService;
         }
 
         public TelemetryLevel GetConsentLevel()
@@ -50,6 +66,11 @@ namespace Umbraco.Cms.Core.Services
         public void SetConsentLevel(TelemetryLevel telemetryLevel)
         {
             var currentUser = _backOfficeSecurityAccessor.BackOfficeSecurity?.CurrentUser;
+            if (currentUser is null)
+            {
+                currentUser = _userService.GetUserById(Constants.Security.SuperUserId);
+            }
+
             _logger.LogInformation("Telemetry level set to {telemetryLevel} by {username}", telemetryLevel, currentUser?.Username);
             _keyValueService.SetValue(Key, telemetryLevel.ToString());
         }
