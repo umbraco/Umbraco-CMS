bc2572fac5a8c2b445dc002dd56ac7f1b1fca894
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2022-05-07 15:28:39 +0200

    Cleanup usings

--- src/Umbraco.Web.Common/Media/ImageSharpImageUrlGenerator.cs
+++ src/Umbraco.Web.Common/Media/ImageSharpImageUrlGenerator.cs
@@ -1,7 +1,4 @@
-using System;
-using System.Collections.Generic;
 using System.Globalization;
-using System.Linq;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Primitives;
 using SixLabors.ImageSharp;
