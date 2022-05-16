7508f4a3b09706868daf104fe0258e3577249f8d
Bjarke Berg <mail@bergmania.dk>
2022-05-16 12:31:49 +0200

    Update src/Umbraco.Core/Configuration/Models/ConnectionStrings.cs
    Co-authored-by: Ronald Barendse <ronald@barend.se>

--- src/Umbraco.Core/Configuration/Models/ConnectionStrings.cs
+++ src/Umbraco.Core/Configuration/Models/ConnectionStrings.cs
@@ -28,7 +28,8 @@ public class ConnectionStrings // TODO: Rename to [Umbraco]ConnectionString (sin
     /// <value>
     /// The name.
     /// </value>
-    public string? Name { get; set; } // TODO: Remove this property (since v10 this is already using named options)
+    [Obsolete("This property will be removed in a future version, because this class is now using named options.")]
+    public string? Name { get; set; }
 
     /// <summary>
     /// Gets or sets the connection string.
