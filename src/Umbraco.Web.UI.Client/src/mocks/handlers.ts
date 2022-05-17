6a082fdbf15405dfff5e3f66636e1ddd829e453e
Mads Rasmussen <madsr@hey.com>
2022-05-17 10:38:51 +0200

    initial setup of Mock Service Worker

---
+++ src/Umbraco.Web.UI.Client/src/mocks/handlers.ts
@@ -0,0 +1,42 @@
+import { rest } from 'msw';
+
+export const handlers = [
+  rest.post('/login', (req, res, ctx) => {
+    // Persist user's authentication in the session
+    sessionStorage.setItem('is-authenticated', 'true');
+    return res(
+      // Respond with a 200 status code
+      ctx.status(200),
+    )
+  }),
+
+  rest.post('/logout', (req, res, ctx) => {
+    // Persist user's authentication in the session
+    sessionStorage.removeItem('is-authenticated');
+    return res(
+      // Respond with a 200 status code
+      ctx.status(200),
+    )
+  }),
+
+  rest.get('/user', (req, res, ctx) => {
+    // Check if the user is authenticated in this session
+    const isAuthenticated = sessionStorage.getItem('is-authenticated');
+    if (!isAuthenticated) {
+      // If not authenticated, respond with a 403 error
+      return res(
+        ctx.status(403),
+        ctx.json({
+          errorMessage: 'Not authorized',
+        }),
+      )
+    }
+    // If authenticated, return a mocked user details
+    return res(
+      ctx.status(200),
+      ctx.json({
+        username: 'admin',
+      }),
+    )
+  }),
+];
