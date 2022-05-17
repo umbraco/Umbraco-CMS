5dd65aaeacce011b22ddea9a1f54fb375552deb5
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2022-05-17 08:57:12 +0200

    Cleanup usings

--- src/Umbraco.Core/Media/EmbedProviders/OEmbedProviderBase.cs
+++ src/Umbraco.Core/Media/EmbedProviders/OEmbedProviderBase.cs
@@ -1,8 +1,4 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
 using System.Net;
-using System.Net.Http;
 using System.Text;
 using System.Xml;
 using Umbraco.Cms.Core.Serialization;
