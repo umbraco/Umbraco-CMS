c6637d8bce14b45deff78e526d1ef03efa56fb46
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2022-05-18 12:58:40 +0200

    create fetchers for known methods

---
+++ src/Umbraco.Web.UI.Client/src/api/fetcher.ts
@@ -0,0 +1,14 @@
+import { Fetcher } from 'openapi-typescript-fetch';
+
+import { paths } from '../../schemas/generated-schema';
+
+const fetcher = Fetcher.for<paths>();
+
+fetcher.configure({
+    baseUrl: '/umbraco/backoffice'
+});
+
+export const getInitStatus = fetcher.path('/init').method('get').create();
+export const postUserLogin = fetcher.path('/user/login').method('post').create();
+export const postUserLogout = fetcher.path('/user/logout').method('post').create();
+export const postInstall = fetcher.path('/install').method('post').create();
