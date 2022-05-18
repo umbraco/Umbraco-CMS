21266965597e3a60acd05edd295afe42f5364b3d
Mole <nikolajlauridsen@protonmail.ch>
2022-05-18 13:52:29 +0200

    Fix domain for invariant content nodes (#12404)

--- src/Umbraco.PublishedCache.NuCache/DomainCacheExtensions.cs
+++ src/Umbraco.PublishedCache.NuCache/DomainCacheExtensions.cs
@@ -11,7 +11,8 @@ namespace Umbraco.Cms.Infrastructure.PublishedCache
             var assigned = domainCache.GetAssigned(documentId, includeWildcards);
 
             // It's super important that we always compare cultures with ignore case, since we can't be sure of the casing!
-            return culture is null ? assigned.Any() : assigned.Any(x => x.Culture.Equals(culture, StringComparison.InvariantCultureIgnoreCase));
+            // Comparing with string.IsNullOrEmpty since both empty string and null signifies invariant.
+            return string.IsNullOrEmpty(culture) ? assigned.Any() : assigned.Any(x => x.Culture.Equals(culture, StringComparison.InvariantCultureIgnoreCase));
         }
     }
 }
