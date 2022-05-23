18ac7bd55a57a1d98bbf140a257d82455c7d69d7
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-05-23 10:46:48 +0200

    Remove obsolete methods

--- src/Umbraco.Infrastructure/DependencyInjection/UmbracoBuilder.Repositories.cs
+++ src/Umbraco.Infrastructure/DependencyInjection/UmbracoBuilder.Repositories.cs
@@ -33,7 +33,6 @@ namespace Umbraco.Cms.Infrastructure.DependencyInjection
             builder.Services.AddUnique<IDomainRepository, DomainRepository>();
             builder.Services.AddMultipleUnique<IEntityRepository, IEntityRepositoryExtended, EntityRepository>();
             builder.Services.AddUnique<ITwoFactorLoginRepository, TwoFactorLoginRepository>();
-            builder.Services.AddUnique<ExternalLoginRepository>();
             builder.Services.AddUnique<IExternalLoginRepository>(factory => factory.GetRequiredService<ExternalLoginRepository>());
             builder.Services.AddUnique<IExternalLoginWithKeyRepository>(factory => factory.GetRequiredService<ExternalLoginRepository>());
             builder.Services.AddUnique<ILanguageRepository, LanguageRepository>();
