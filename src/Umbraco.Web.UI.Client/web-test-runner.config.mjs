5655c97f6ea1a79cd8187b806ceb098350b87ff6
JesmoDev <jjensenmoeller@gmail.com>
2022-05-24 16:14:15 +0200

    added packages and basic setup

---
+++ src/Umbraco.Web.UI.Client/web-test-runner.config.mjs
@@ -0,0 +1,19 @@
+import { esbuildPlugin } from '@web/dev-server-esbuild';
+import { playwrightLauncher } from '@web/test-runner-playwright';
+
+export default {
+  nodeResolve: true,
+  files: 'src/**/*.test.ts',
+  plugins: [esbuildPlugin({ ts: true, target: 'auto' })],
+  browsers: [
+    playwrightLauncher({ product: 'chromium' }),
+    playwrightLauncher({ product: 'firefox' }),
+    playwrightLauncher({ product: 'webkit' }),
+  ],
+  testRunnerHtml: (testFramework) =>
+    `<html>
+      <body>
+        <script type="module" src="${testFramework}"></script>
+      </body>
+    </html>`,
+};
