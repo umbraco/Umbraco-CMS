85b7ef411451d068661127a052504718a5f73809
Mads Rasmussen <madsr@hey.com>
2022-05-24 09:59:36 +0200

    filter unhandled requests

--- src/Umbraco.Web.UI.Client/src/index.ts
+++ src/Umbraco.Web.UI.Client/src/index.ts
@@ -66,5 +66,18 @@ const setup = async () => {
   await import('./app');
 }
 
-worker.start();
+worker.start({
+  onUnhandledRequest: (req) => {
+    if (req.url.pathname.startsWith('/node_modules/')) return;
+    if (req.url.pathname.startsWith('/src/')) return;
+    if (req.destination === 'image') return;
+
+    console.warn(
+      'Found an unhandled %s request to %s',
+      req.method,
+      req.url.href,
+    )
+  }
+});
+
 setup();
