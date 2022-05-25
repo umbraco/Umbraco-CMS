e1d39f7b02d07d06b278fee29ddc41168160a791
Niels Lyngsø <niels.lyngso@gmail.com>
2022-05-25 16:27:12 +0200

    fix test for WebKit

--- src/Umbraco.Web.UI.Client/src/core/context/context-provider.mixin.test.ts
+++ src/Umbraco.Web.UI.Client/src/core/context/context-provider.mixin.test.ts
@@ -1,11 +1,12 @@
 import { expect, fixture, html } from '@open-wc/testing';
+import { UmbContextProvider } from './context-provider';
 import { UmbContextProviderMixin } from './context-provider.mixin';
 
 class MyClass {
   prop: string;
 
   constructor(text: string) {
-    this.prop = text
+    this.prop = text;
   }
 }
 
@@ -20,8 +21,14 @@ class MyTestProviderElement extends UmbContextProviderMixin(HTMLElement) {
 customElements.define('my-test-provider-element', MyTestProviderElement);
 
 describe('UmbContextProviderMixin', async () => {
-  const element: MyTestProviderElement = await fixture(html`<my-test-provider-element></my-test-provider-element>`);
-  const _providers = (element as any)['_providers'];
+
+  let element: MyTestProviderElement;
+  let _providers: Map<string, UmbContextProvider>;
+
+  beforeEach(async () => {
+    element = await fixture(html`<my-test-provider-element></my-test-provider-element>`);
+    _providers = (element as any)['_providers'];
+  });
 
   it('sets all providers to element', () => {
     expect(_providers.has('my-test-context-1')).to.be.true;
@@ -32,8 +39,8 @@ describe('UmbContextProviderMixin', async () => {
     const provider = _providers.get('my-test-context-1');
     expect(provider).to.not.be.undefined;
     if (!provider) return;
-    expect(provider['_instance'].prop).to.eq('context value 1');
+    expect((provider['_instance'] as MyClass).prop).to.eq('context value 1');
     element.provideContext('my-test-context-1', new MyClass('new context value 1'));
-    expect(provider['_instance'].prop).to.eq('context value 1');
+    expect((provider['_instance'] as MyClass).prop).to.eq('context value 1');
   });
 });
