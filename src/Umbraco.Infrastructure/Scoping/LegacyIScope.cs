11d9bee901e6954e2dc7c04bf8d4401e3c7448f4
mattbrailsford <me@mattbrailsford.com>
2022-05-25 18:59:26 +0100

    Make LegacyIScope have all props/methods defined upon it as otherwise it causes method missing errors

--- src/Umbraco.Infrastructure/Scoping/LegacyIScope.cs
+++ src/Umbraco.Infrastructure/Scoping/LegacyIScope.cs
@@ -1,5 +1,7 @@
 using System;
+using Umbraco.Cms.Core.Cache;
 using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Infrastructure.Persistence;
 
 // ReSharper disable once CheckNamespace
 namespace Umbraco.Cms.Core.Scoping;
@@ -7,6 +9,16 @@ namespace Umbraco.Cms.Core.Scoping;
 [Obsolete("Please use Umbraco.Cms.Infrastructure.Scoping.IScope or Umbraco.Cms.Core.Scoping.ICoreScope instead.")]
 public interface IScope : Infrastructure.Scoping.IScope
 {
+    /// <summary>
+    /// Gets the scope database.
+    /// </summary>
+    IUmbracoDatabase Database { get; }
+
+    /// <summary>
+    /// Gets the Sql context.
+    /// </summary>
+    ISqlContext SqlContext { get; }
+
     /// <summary>
     /// Gets the scope event messages.
     /// </summary>
@@ -16,4 +28,59 @@ public interface IScope : Infrastructure.Scoping.IScope
     /// Gets the scope event dispatcher.
     /// </summary>
     IEventDispatcher Events { get; }
+
+    /// <summary>
+    /// Gets the scope notification publisher
+    /// </summary>
+    IScopedNotificationPublisher Notifications { get; }
+
+    /// <summary>
+    /// Gets the repositories cache mode.
+    /// </summary>
+    RepositoryCacheMode RepositoryCacheMode { get; }
+
+    /// <summary>
+    /// Gets the scope isolated cache.
+    /// </summary>
+    IsolatedCaches IsolatedCaches { get; }
+
+    /// <summary>
+    /// Completes the scope.
+    /// </summary>
+    /// <returns>A value indicating whether the scope has been successfully completed.</returns>
+    /// <remarks>Can return false if any child scope has not completed.</remarks>
+    bool Complete();
+
+    /// <summary>
+    /// Read-locks some lock objects.
+    /// </summary>
+    /// <param name="lockIds">Array of lock object identifiers.</param>
+    void ReadLock(params int[] lockIds);
+
+    /// <summary>
+    /// Write-locks some lock objects.
+    /// </summary>
+    /// <param name="lockIds">Array of object identifiers.</param>
+    void WriteLock(params int[] lockIds);
+
+    /// <summary>
+    /// Write-locks some lock objects.
+    /// </summary>
+    /// <param name="timeout">The database timeout in milliseconds</param>
+    /// <param name="lockId">The lock object identifier.</param>
+    void WriteLock(TimeSpan timeout, int lockId);
+
+    /// <summary>
+    /// Read-locks some lock objects.
+    /// </summary>
+    /// <param name="timeout">The database timeout in milliseconds</param>
+    /// <param name="lockId">The lock object identifier.</param>
+    void ReadLock(TimeSpan timeout, int lockId);
+
+    void EagerWriteLock(params int[] lockIds);
+    void EagerWriteLock(TimeSpan timeout, int lockId);
+
+    void EagerReadLock(TimeSpan timeout, int lockId);
+
+    void EagerReadLock(params int[] lockIds);
 }
