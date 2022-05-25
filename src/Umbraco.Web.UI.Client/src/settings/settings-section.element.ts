f9d48ca62f5a5887c6e0567a8a63f1b93789815c
Niels Lyngsø <niels.lyngso@gmail.com>
2022-05-25 19:02:46 +0200

    updated code idea/comments

--- src/Umbraco.Web.UI.Client/src/settings/settings-section.element.ts
+++ src/Umbraco.Web.UI.Client/src/settings/settings-section.element.ts
@@ -18,13 +18,16 @@ export class UmbSettingsSection extends UmbContextConsumerMixin(LitElement) {
     this.consumeContext('umbExtensionRegistry', (_instance: UmbExtensionRegistry) => {
       this._extensionRegistry = _instance;
 
-      // TODO: Could we make it easier to unsubscribe? If we invented a Pattern/Mixin/class ala Lit-Controllers we could make it auto unsubscribe.
-      // ContextConsumers could be turned into single classes which uses the 'Controller' ability to hook into connected and disconnected.
-      // Generally that means that a web component must have the ControllerMixin?? and then controllers can easily be attached, they would know about life cycle and thereby be able to unsubscribe on disconnected etc.
       this._extensionsSubscription?.unsubscribe();
 
+      // TODO: Niels: Could we make it easier to unsubscribe? If we invented a Pattern/Mixin/class ala Lit-Controllers we could make it auto unsubscribe.
+      // ContextConsumers could be turned into single classes which uses the 'Controller' ability to hook into connected and disconnected.
+      // Generally that means that a web component must have the ControllerMixin?? and then controllers can easily be attached, they would know about life cycle and thereby be able to unsubscribe on disconnected etc.
+      //
+      // All code regarding subscription could be boiled down to:
+      //OurUmbracoSubscribeMethod(this, this._extensionRegistry.extensions, (extensions) => {}); // uses `this` to append the subscription to the controller array.
       this._extensionsSubscription = this._extensionRegistry.extensions.subscribe(extensions => {
-        this._extensions = [...extensions];
+        this._extensions = [...extensions];// TODO: Though, this is a shallow clone, wouldn't we either do a deep clone or no clone at all?
       });
     });
   }
