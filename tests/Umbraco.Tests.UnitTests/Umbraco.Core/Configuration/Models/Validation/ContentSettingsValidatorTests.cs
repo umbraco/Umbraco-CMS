5df1f47fce4f3e54e78133a71b75e8bde416f078
Nikolaj Geisle <niko737@edu.ucl.dk>
2022-05-30 15:18:56 +0200

    Fix up errors

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/Configuration/Models/Validation/ContentSettingsValidatorTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Configuration/Models/Validation/ContentSettingsValidatorTests.cs
@@ -1,69 +1,66 @@
 // Copyright (c) Umbraco.
 // See LICENSE for more details.
 
+using Microsoft.Extensions.Options;
 using NUnit.Framework;
 using Umbraco.Cms.Core.Configuration.Models;
 using Umbraco.Cms.Core.Configuration.Models.Validation;
 
-namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Configuration.Models.Validation;
-
-[TestFixture]
-public class ContentSettingsValidatorTests
+namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Configuration.Models.Validation
 {
-    [Test]
-    public void Returns_Success_ForValid_Configuration()
+    [TestFixture]
+    public class ContentSettingsValidatorTests
     {
-        var validator = new ContentSettingsValidator();
-        var options = BuildContentSettings();
-        var result = validator.Validate("settings", options);
-        Assert.True(result.Succeeded);
-    }
-
-    [Test]
-    public void Returns_Fail_For_Configuration_With_Invalid_Error404Collection_Due_To_Duplicate_Id()
-    {
-        var validator = new ContentSettingsValidator();
-        var options = BuildContentSettings(contentXPath: "/aaa/bbb");
-        var result = validator.Validate("settings", options);
-        Assert.False(result.Succeeded);
-    }
-
-    [Test]
-    public void Returns_Fail_For_Configuration_With_Invalid_Error404Collection_Due_To_Empty_Culture()
-    {
-        var validator = new ContentSettingsValidator();
-        var options = BuildContentSettings(string.Empty);
-        var result = validator.Validate("settings", options);
-        Assert.False(result.Succeeded);
-    }
-
-    [Test]
-    public void Returns_Fail_For_Configuration_With_Invalid_AutoFillImageProperties_Collection()
-    {
-        var validator = new ContentSettingsValidator();
-        var options = BuildContentSettings(string.Empty);
-        var result = validator.Validate("settings", options);
-        Assert.False(result.Succeeded);
-    }
-
-    private static ContentSettings BuildContentSettings(string culture = "en-US", string contentXPath = "",
-        string autoFillImagePropertyAlias = "testAlias") =>
-        new()
+        [Test]
+        public void Returns_Success_ForValid_Configuration()
         {
-            Error404Collection = new[] {new() {Culture = culture, ContentId = 1, ContentXPath = contentXPath}},
-            Imaging = new ContentImagingSettings
+            var validator = new ContentSettingsValidator();
+            ContentSettings options = BuildContentSettings();
+            ValidateOptionsResult result = validator.Validate("settings", options);
+            Assert.True(result.Succeeded);
+        }
+
+        [Test]
+        public void Returns_Fail_For_Configuration_With_Invalid_Error404Collection_Due_To_Duplicate_Id()
+        {
+            var validator = new ContentSettingsValidator();
+            ContentSettings options = BuildContentSettings(contentXPath: "/aaa/bbb");
+            ValidateOptionsResult result = validator.Validate("settings", options);
+            Assert.False(result.Succeeded);
+        }
+
+        [Test]
+        public void Returns_Fail_For_Configuration_With_Invalid_Error404Collection_Due_To_Empty_Culture()
+        {
+            var validator = new ContentSettingsValidator();
+            ContentSettings options = BuildContentSettings(culture: string.Empty);
+            ValidateOptionsResult result = validator.Validate("settings", options);
+            Assert.False(result.Succeeded);
+        }
+
+        [Test]
+        public void Returns_Fail_For_Configuration_With_Invalid_AutoFillImageProperties_Collection()
+        {
+            var validator = new ContentSettingsValidator();
+            ContentSettings options = BuildContentSettings(culture: string.Empty);
+            ValidateOptionsResult result = validator.Validate("settings", options);
+            Assert.False(result.Succeeded);
+        }
+
+        private static ContentSettings BuildContentSettings(string culture = "en-US", string contentXPath = "", string autoFillImagePropertyAlias = "testAlias") =>
+            new ContentSettings
             {
-                AutoFillImageProperties = new[]
-                {
-                    new ImagingAutoFillUploadField
+                Error404Collection = new ContentErrorPage[]
                     {
-                        Alias = autoFillImagePropertyAlias,
-                        WidthFieldAlias = "w",
-                        HeightFieldAlias = "h",
-                        LengthFieldAlias = "l",
-                        ExtensionFieldAlias = "e"
-                    }
+                        new ContentErrorPage { Culture = culture, ContentId = 1, ContentXPath = contentXPath },
+                    },
+                Imaging = new ContentImagingSettings
+                {
+                    AutoFillImageProperties = new ImagingAutoFillUploadField[]
+                        {
+                            new ImagingAutoFillUploadField { Alias = autoFillImagePropertyAlias, WidthFieldAlias = "w", HeightFieldAlias = "h", LengthFieldAlias = "l", ExtensionFieldAlias = "e" }
+                        }
                 }
-            }
-        };
+            };
+    }
 }
