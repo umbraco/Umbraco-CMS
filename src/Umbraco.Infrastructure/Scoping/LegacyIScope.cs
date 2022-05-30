6fc0d3f003c19ed15d7a60bacb8c9317bcc10287
mattbrailsford <me@mattbrailsford.com>
2022-05-30 15:16:40 +0100

    Added new keywork to hidden properties / methods

--- src/Umbraco.Infrastructure/Scoping/LegacyIScope.cs
+++ src/Umbraco.Infrastructure/Scoping/LegacyIScope.cs
@@ -12,12 +12,12 @@ public interface IScope : Infrastructure.Scoping.IScope
     /// <summary>
     /// Gets the scope database.
     /// </summary>
-    IUmbracoDatabase Database { get; }
+    new IUmbracoDatabase Database { get; }
 
     /// <summary>
     /// Gets the Sql context.
     /// </summary>
-    ISqlContext SqlContext { get; }
+    new ISqlContext SqlContext { get; }
 
     /// <summary>
     /// Gets the scope event messages.
@@ -32,55 +32,55 @@ public interface IScope : Infrastructure.Scoping.IScope
     /// <summary>
     /// Gets the scope notification publisher
     /// </summary>
-    IScopedNotificationPublisher Notifications { get; }
+    new IScopedNotificationPublisher Notifications { get; }
 
     /// <summary>
     /// Gets the repositories cache mode.
     /// </summary>
-    RepositoryCacheMode RepositoryCacheMode { get; }
+    new RepositoryCacheMode RepositoryCacheMode { get; }
 
     /// <summary>
     /// Gets the scope isolated cache.
     /// </summary>
-    IsolatedCaches IsolatedCaches { get; }
+    new IsolatedCaches IsolatedCaches { get; }
 
     /// <summary>
     /// Completes the scope.
     /// </summary>
     /// <returns>A value indicating whether the scope has been successfully completed.</returns>
     /// <remarks>Can return false if any child scope has not completed.</remarks>
-    bool Complete();
+    new bool Complete();
 
     /// <summary>
     /// Read-locks some lock objects.
     /// </summary>
     /// <param name="lockIds">Array of lock object identifiers.</param>
-    void ReadLock(params int[] lockIds);
+    new void ReadLock(params int[] lockIds);
 
     /// <summary>
     /// Write-locks some lock objects.
     /// </summary>
     /// <param name="lockIds">Array of object identifiers.</param>
-    void WriteLock(params int[] lockIds);
+    new void WriteLock(params int[] lockIds);
 
     /// <summary>
     /// Write-locks some lock objects.
     /// </summary>
     /// <param name="timeout">The database timeout in milliseconds</param>
     /// <param name="lockId">The lock object identifier.</param>
-    void WriteLock(TimeSpan timeout, int lockId);
+    new void WriteLock(TimeSpan timeout, int lockId);
 
     /// <summary>
     /// Read-locks some lock objects.
     /// </summary>
     /// <param name="timeout">The database timeout in milliseconds</param>
     /// <param name="lockId">The lock object identifier.</param>
-    void ReadLock(TimeSpan timeout, int lockId);
+    new void ReadLock(TimeSpan timeout, int lockId);
 
-    void EagerWriteLock(params int[] lockIds);
-    void EagerWriteLock(TimeSpan timeout, int lockId);
+    new void EagerWriteLock(params int[] lockIds);
+    new void EagerWriteLock(TimeSpan timeout, int lockId);
 
-    void EagerReadLock(TimeSpan timeout, int lockId);
+    new void EagerReadLock(TimeSpan timeout, int lockId);
 
-    void EagerReadLock(params int[] lockIds);
+    new void EagerReadLock(params int[] lockIds);
 }
