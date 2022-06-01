b54cffbfdf3b908003a145ca6a259431f5cb2f86
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-06-01 13:41:30 +0200

    Update src/Umbraco.Infrastructure/Examine/PublishedContentIndexPopulator.cs
    Co-authored-by: Mole <nikolajlauridsen@protonmail.ch>

--- src/Umbraco.Infrastructure/Examine/PublishedContentIndexPopulator.cs
+++ src/Umbraco.Infrastructure/Examine/PublishedContentIndexPopulator.cs
@@ -9,9 +9,8 @@ namespace Umbraco.Cms.Infrastructure.Examine;
 /// </summary>
 /// <remarks>
 ///     The published (external) index will still rebuild just fine using the default <see cref="ContentIndexPopulator" />
-///     which is what
-///     is used when rebuilding all indexes, but this will be used when the single index is rebuilt and will go a little
-///     bit faster
+///     which is what is used when rebuilding all indexes,
+///     but this will be used when the single index is rebuilt and will go a little bit faster since the data query is more specific.
 ///     since the data query is more specific.
 /// </remarks>
 public class PublishedContentIndexPopulator : ContentIndexPopulator
