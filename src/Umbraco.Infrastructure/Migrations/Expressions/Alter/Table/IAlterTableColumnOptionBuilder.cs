cd71c03771c06d2023d0fb76b6dbb0c5a5841256
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-06-01 13:43:13 +0200

    Update src/Umbraco.Infrastructure/Migrations/Expressions/Alter/Table/IAlterTableColumnOptionBuilder.cs
    Co-authored-by: Mole <nikolajlauridsen@protonmail.ch>

--- src/Umbraco.Infrastructure/Migrations/Expressions/Alter/Table/IAlterTableColumnOptionBuilder.cs
+++ src/Umbraco.Infrastructure/Migrations/Expressions/Alter/Table/IAlterTableColumnOptionBuilder.cs
@@ -2,8 +2,9 @@ using Umbraco.Cms.Infrastructure.Migrations.Expressions.Common;
 
 namespace Umbraco.Cms.Infrastructure.Migrations.Expressions.Alter.Table;
 
-public interface IAlterTableColumnOptionBuilder : IColumnOptionBuilder<IAlterTableColumnOptionBuilder,
-        IAlterTableColumnOptionForeignKeyCascadeBuilder>,
-    IAlterTableBuilder, IExecutableBuilder
+public interface IAlterTableColumnOptionBuilder :
+    IColumnOptionBuilder<IAlterTableColumnOptionBuilder, IAlterTableColumnOptionForeignKeyCascadeBuilder>,
+    IAlterTableBuilder,
+    IExecutableBuilder
 {
 }
