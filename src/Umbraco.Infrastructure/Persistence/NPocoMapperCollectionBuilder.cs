3f2ee39f1245b7b985048bfc4bd8e0693dc0a709
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-06-01 13:51:11 +0200

    Update src/Umbraco.Infrastructure/Persistence/NPocoMapperCollectionBuilder.cs
    Co-authored-by: Mole <nikolajlauridsen@protonmail.ch>

--- src/Umbraco.Infrastructure/Persistence/NPocoMapperCollectionBuilder.cs
+++ src/Umbraco.Infrastructure/Persistence/NPocoMapperCollectionBuilder.cs
@@ -3,9 +3,7 @@ using Umbraco.Cms.Core.Composing;
 
 namespace Umbraco.Cms.Infrastructure.Persistence;
 
-public sealed class
-    NPocoMapperCollectionBuilder : SetCollectionBuilderBase<NPocoMapperCollectionBuilder, NPocoMapperCollection,
-        IMapper>
+public sealed class NPocoMapperCollectionBuilder : SetCollectionBuilderBase<NPocoMapperCollectionBuilder, NPocoMapperCollection, IMapper>
 {
     protected override NPocoMapperCollectionBuilder This => this;
 }
