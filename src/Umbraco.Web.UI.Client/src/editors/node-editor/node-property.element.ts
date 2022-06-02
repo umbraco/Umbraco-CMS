ec2314d85513e9c8dafaa09637d3b6c9668f4395
Niels Lyngsø <niels.lyngso@gmail.com>
2022-06-02 15:17:42 +0200

    concat node-property-data-type.element.ts to node-property

--- src/Umbraco.Web.UI.Client/src/editors/node-editor/node-property.element.ts
+++ src/Umbraco.Web.UI.Client/src/editors/node-editor/node-property.element.ts
@@ -1,35 +1,142 @@
-import { css, html, LitElement } from 'lit';
+import { css, html, LitElement, PropertyValueMap } from 'lit';
 import { UUITextStyles } from '@umbraco-ui/uui-css/lib';
-import { customElement, property } from 'lit/decorators.js';
+import { customElement, property, state } from 'lit/decorators.js';
+import { createExtensionElement, UmbExtensionManifest, UmbExtensionRegistry } from '../../core/extension';
+import { Subscription, map, switchMap } from 'rxjs';
+import { UmbContextConsumerMixin } from '../../core/context';
+import { DataTypeEntity } from '../../mocks/data/content.data';
+import { UmbDataTypeStore } from '../../core/stores/data-type.store';
 
 @customElement('umb-node-property')
-class UmbNodeProperty extends LitElement {
+class UmbNodeProperty extends UmbContextConsumerMixin(LitElement) {
   static styles = [
     UUITextStyles,
     css`
       :host {
         display: block;
       }
-
       p {
         color: var(--uui-color-text-alt);
       }
     `,
   ];
 
+  private _property: any; // TODO: property data model interface..
   @property()
-  property: any; // TODO: property data model interface..
+  public get property(): any {
+    return this._property;
+  }
+  public set property(value: any) {
+    this._property = value;
+    this._useDataType();
+  }
 
   @property()
   value?: string;
 
-  // TODO: maybe a bit messy with all the event listeners on the different levels:
-  private _onPropertyDataTypeChange = (e: CustomEvent) => {
+  // TODO: make interface for UMBPropertyEditorElement
+  @state()
+  private _element?: { value?: string } & HTMLElement; // TODO: invent interface for propertyEditorUI.
+
+  private _dataType?: DataTypeEntity;
+  private _extensionRegistry?: UmbExtensionRegistry;
+  private _dataTypeStore?: UmbDataTypeStore;
+  private _dataTypeSubscription?: Subscription;
+
+  constructor() {
+    super();
+
+    /** TODO: Use DI for these types of services. */
+    this.consumeContext('umbDataTypeStore', (_instance: UmbDataTypeStore) => {
+      this._dataTypeStore = _instance;
+      this._useDataType();
+    });
+    this.consumeContext('umbExtensionRegistry', (_instance: UmbExtensionRegistry) => {
+      this._extensionRegistry = _instance;
+      this._useDataType();
+    });
+
+    // TODO: solution to know when both contexts are available
+  }
+
+  // TODO: use subscribtion, rename to _useDataType:
+  private _useDataType() {
+    this._dataTypeSubscription?.unsubscribe();
+    if (this._property.dataTypeKey && this._extensionRegistry && this._dataTypeStore) {
+
+      this._dataTypeSubscription = this._dataTypeStore
+        .getByKey(this._property.dataTypeKey)
+        .pipe(
+          // eslint-disable-next-line @typescript-eslint/ban-ts-comment
+          // @ts-ignore
+          map((dataTypeEntity: DataTypeEntity) => {
+            this._dataType = dataTypeEntity;
+            return dataTypeEntity.propertyEditorUIAlias;
+          }),
+          switchMap((alias: string) => this._extensionRegistry?.getByAlias(alias) as any)
+        )
+        .subscribe((propertyEditorUI: any) => {
+          this._gotData(propertyEditorUI);
+        });
+    }
+  }
+
+  private _gotData(_propertyEditorUI?: UmbExtensionManifest) {
+
+    if (!this._dataType || !_propertyEditorUI) {
+      // TODO: if dataTypeKey didn't exist in store, we should do some nice UI.
+      return;
+    }
+
+    createExtensionElement(_propertyEditorUI)
+      .then((el) => {
+        const oldValue = this._element;
+        this._element = el;
+
+        // TODO: Set/Parse Data-Type-UI-configuration
+
+        if (oldValue) {
+          oldValue.removeEventListener('property-editor-change', this._onPropertyEditorChange as any as EventListener);
+        }
+
+        /* NYT lav callback: */
+        if (this._element) {
+          this._element.addEventListener(
+            'property-editor-change',
+            this._onPropertyEditorChange as any as EventListener
+          );
+          this._element.value = this.value; // Be aware its duplicated code
+        }
+        this.requestUpdate('element', oldValue);
+      })
+      .catch(() => {
+        // TODO: loading JS failed so we should do some nice UI. (This does only happen if extension has a js prop, otherwise we concluded that no source was needed resolved the load.)
+      });
+  }
+
+  private _onPropertyEditorChange = (e: CustomEvent) => {
     this.value = (e.target as any).value;
     this.dispatchEvent(new CustomEvent('property-value-change', { bubbles: true, composed: true }));
     // No need for this event to leave scope.
     e.stopPropagation();
   };
+
+  /** Lit does not currently handle dynamic tag names, therefor we are doing some manual rendering */
+  // TODO: Refactor into a base class for dynamic-tag element? we will be using this a lot for extensions.
+  // This could potentially hook into Lit and parse all properties defined in the specific class on to the dynamic-element. (see static elementProperties: PropertyDeclarationMap;)
+  willUpdate(changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>) {
+    super.willUpdate(changedProperties);
+
+    const hasChangedProps = changedProperties.has('value');
+    if (hasChangedProps && this._element) {
+      this._element.value = this.value; // Be aware its duplicated code
+    }
+  }
+
+  disconnectedCallback(): void {
+    super.disconnectedCallback();
+    this._dataTypeSubscription?.unsubscribe();
+  }
 
   render() {
     return html`
@@ -39,10 +146,7 @@ class UmbNodeProperty extends LitElement {
           <p>${this.property.description}</p>
         </div>
         <div slot="editor">
-          <umb-node-property-data-type
-            .dataTypeKey=${this.property.dataTypeKey}
-            .value=${this.value}
-            @property-data-type-value-change=${this._onPropertyDataTypeChange}></umb-node-property-data-type>
+          ${this._element}
         </div>
       </umb-editor-property-layout>
     `;
