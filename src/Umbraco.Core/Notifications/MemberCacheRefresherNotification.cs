b40ba1cbfc378351d60bffecf20262cc89f6907f
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-06-07 10:03:08 +0200

    Update src/Umbraco.Core/Notifications/MemberCacheRefresherNotification.cs
    Co-authored-by: Mole <nikolajlauridsen@protonmail.ch>

--- src/Umbraco.Core/Notifications/MemberCacheRefresherNotification.cs
+++ src/Umbraco.Core/Notifications/MemberCacheRefresherNotification.cs
@@ -5,9 +5,7 @@ namespace Umbraco.Cms.Core.Notifications;
 public class MemberCacheRefresherNotification : CacheRefresherNotification
 {
     public MemberCacheRefresherNotification(object messageObject, MessageType messageType)
-        : base(
-        messageObject,
-        messageType)
+        : base(messageObject, messageType)
     {
     }
 }
