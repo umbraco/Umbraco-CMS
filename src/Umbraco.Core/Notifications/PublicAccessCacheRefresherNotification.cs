8a02257516160bd63206fc5c9b67b705e74320e0
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-06-07 10:18:13 +0200

    Update src/Umbraco.Core/Notifications/PublicAccessCacheRefresherNotification.cs
    Co-authored-by: Mole <nikolajlauridsen@protonmail.ch>

--- src/Umbraco.Core/Notifications/PublicAccessCacheRefresherNotification.cs
+++ src/Umbraco.Core/Notifications/PublicAccessCacheRefresherNotification.cs
@@ -5,9 +5,7 @@ namespace Umbraco.Cms.Core.Notifications;
 public class PublicAccessCacheRefresherNotification : CacheRefresherNotification
 {
     public PublicAccessCacheRefresherNotification(object messageObject, MessageType messageType)
-        : base(
-        messageObject,
-        messageType)
+        : base(messageObject, messageType)
     {
     }
 }
