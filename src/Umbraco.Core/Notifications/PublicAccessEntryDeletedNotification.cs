8d23456a9580fb2ba6a52266e7ecc5a211fccdd4
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-06-07 10:18:33 +0200

    Update src/Umbraco.Core/Notifications/PublicAccessEntryDeletedNotification.cs
    Co-authored-by: Mole <nikolajlauridsen@protonmail.ch>

--- src/Umbraco.Core/Notifications/PublicAccessEntryDeletedNotification.cs
+++ src/Umbraco.Core/Notifications/PublicAccessEntryDeletedNotification.cs
@@ -9,9 +9,7 @@ namespace Umbraco.Cms.Core.Notifications;
 public sealed class PublicAccessEntryDeletedNotification : DeletedNotification<PublicAccessEntry>
 {
     public PublicAccessEntryDeletedNotification(PublicAccessEntry target, EventMessages messages)
-        : base(
-        target,
-        messages)
+        : base(messageObject, messageType)
     {
     }
 }
