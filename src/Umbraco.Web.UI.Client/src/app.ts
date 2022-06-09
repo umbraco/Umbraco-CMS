b5b339459541a7f92045a75cbd8da96d427dbcb3
Mads Rasmussen <madsr@hey.com>
2022-06-09 10:17:10 +0200

    remove extension registry from window

--- src/Umbraco.Web.UI.Client/src/app.ts
+++ src/Umbraco.Web.UI.Client/src/app.ts
@@ -3,78 +3,101 @@ import '@umbraco-ui/uui-css/dist/uui-css.css';
 
 import { UUIIconRegistryEssential } from '@umbraco-ui/uui';
 import { css, html, LitElement } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { customElement, state } from 'lit/decorators.js';
 
 import { getInitStatus } from './core/api/fetcher';
 import { UmbContextProviderMixin } from './core/context';
-
-const routes = [
-  {
-    path: 'login',
-    component: () => import('./auth/login/login.element'),
-  },
-  {
-    path: 'install',
-    component: () => import('./installer/installer.element'),
-  },
-  {
-    path: '**',
-    component: () => import('./backoffice/backoffice.element'),
-  },
-];
+import { UmbExtensionManifest, UmbExtensionManifestCore, UmbExtensionRegistry } from './core/extension';
+import { internalManifests } from './temp-internal-manifests';
 
 @customElement('umb-app')
 export class UmbApp extends UmbContextProviderMixin(LitElement) {
   static styles = css`
     :host,
-    #outlet {
+    #router-slot {
       display: block;
       width: 100%;
       height: 100vh;
     }
   `;
 
+  @state()
+  private _routes = [
+    {
+      path: 'login',
+      component: () => import('./auth/login/login.element'),
+    },
+    {
+      path: 'install',
+      component: () => import('./installer/installer.element'),
+    },
+    {
+      path: '**',
+      component: () => import('./backoffice/backoffice.element'),
+    },
+  ];
+
+  private _extensionRegistry: UmbExtensionRegistry = new UmbExtensionRegistry();
   private _iconRegistry: UUIIconRegistryEssential = new UUIIconRegistryEssential();
   private _isInstalled = false;
 
   constructor() {
     super();
-    this._iconRegistry.attach(this);
+    this._setup();
+  }
 
-    const { extensionRegistry } = window.Umbraco;
-    this.provideContext('umbExtensionRegistry', extensionRegistry);
+  private async _setup () {
+    this._iconRegistry.attach(this);
+    this.provideContext('umbExtensionRegistry', this._extensionRegistry);
+
+    await this._registerExtensionManifestsFromServer();
+    await this._registerInternalManifests();
+    await this._setInitStatus();
+    this._redirect();
+  }
+
+  private async _setInitStatus() {
+    try {
+      const { data } = await getInitStatus({});
+      this._isInstalled = data.installed;
+    } catch (error) {
+      console.log(error);
+    }
+  }
+
+  private _redirect () {
+    if (!this._isInstalled) {
+      history.pushState(null, '', '/install');
+      return;
+    }
+
+    if (this._isAuthorized()) {
+      history.pushState(null, '', window.location.pathname);
+    } else {
+      history.pushState(null, '', '/login');
+    }
   }
 
   private _isAuthorized(): boolean {
     return sessionStorage.getItem('is-authenticated') === 'true';
   }
 
-  protected async firstUpdated(): Promise<void> {
-    this.shadowRoot?.querySelector('router-slot')?.render();
+  private async _registerExtensionManifestsFromServer() {
+    // TODO: add schema and use fetcher
+    const res = await fetch('/umbraco/backoffice/manifests');
+    const { manifests } = await res.json();
+    manifests.forEach((manifest: UmbExtensionManifest) => this._extensionRegistry.register(manifest));
+  };
 
-    try {
-      const { data } = await getInitStatus({});
-
-      this._isInstalled = data.installed;
-
-      if (!this._isInstalled) {
-        history.pushState(null, '', '/install');
-        return;
-      }
-
-      if (!this._isAuthorized() || window.location.pathname === '/install') {
-        history.pushState(null, '', 'login');
-      } else {
-        const next = window.location.pathname === '/' ? '/section/content' : window.location.pathname;
-        history.pushState(null, '', next);
-      }
-    } catch (error) {
-      console.log(error);
-    }
+  private async _registerInternalManifests() {
+    // TODO: where do we get these from?  
+    internalManifests.forEach((manifest: UmbExtensionManifestCore) =>
+      this._extensionRegistry.register<UmbExtensionManifestCore>(manifest)
+    );
   }
 
   render() {
-    return html`<router-slot id="outlet" .routes=${routes}></router-slot>`;
+    return html`<router-slot id="router-slot" .routes=${this._routes}></router-slot>`;
   }
 }
 
