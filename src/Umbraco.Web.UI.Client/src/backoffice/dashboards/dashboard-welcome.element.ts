25d0011da1c2725af990f86540e60e87da28286b
Mads Rasmussen <madsr@hey.com>
2022-06-10 13:37:54 +0200

    update welcome dashboard

--- src/Umbraco.Web.UI.Client/src/backoffice/dashboards/dashboard-welcome.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/dashboards/dashboard-welcome.element.ts
@@ -9,18 +9,8 @@ export class UmbDashboardWelcome extends LitElement {
   render() {
     return html`
       <uui-box>
-        <h1>POC Introduction</h1>
-        <p>
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris lectus turpis, facilisis in quam non, mattis
-          varius massa. Nullam pretium, dui in facilisis sollicitudin, diam tortor scelerisque tellus, eget cursus ex orci
-          et mauris. Aliquam condimentum enim a erat ultricies blandit. Aliquam urna sem, lacinia ut eros id, placerat
-          commodo libero. Pellentesque iaculis lacus in neque pharetra dignissim. Duis gravida nibh in consequat lobortis.
-          Nullam sollicitudin ante orci, ac molestie lectus maximus non.
-        </p>
-        <ul>
-          <li>Nulla luctus, neque sed feugiat laoreet</li>
-          <li>Nam varius orci velit, quis interdum enim facilisis sed.</li>
-        </ul>
+        <h1>Welcome</h1>
+        <p>You can find details about the POC in the readme.md file.</p>
       </uui-box>
     `;
   }
