2bcf052d9b1d367871b0b5bc664b37afe4dde238
Nikolaj <nikolajlauridsen@protonmail.ch>
2022-06-13 10:02:05 +0200

    Fix missing import statements in FileSystemMainDomLock

--- src/Umbraco.Infrastructure/Runtime/FileSystemMainDomLock.cs
+++ src/Umbraco.Infrastructure/Runtime/FileSystemMainDomLock.cs
@@ -1,4 +1,8 @@
+using System;
 using System.Diagnostics;
+using System.IO;
+using System.Threading;
+using System.Threading.Tasks;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Umbraco.Cms.Core.Configuration.Models;
