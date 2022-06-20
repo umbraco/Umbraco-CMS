2ea30ca8de80555f9a1fe86262673346ff29f98e
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-06-20 08:20:11 +0200

    Update tests/Umbraco.Tests.Integration/Testing/TestDbMeta.cs
    Co-authored-by: Mole <nikolajlauridsen@protonmail.ch>

--- tests/Umbraco.Tests.Integration/Testing/TestDbMeta.cs
+++ tests/Umbraco.Tests.Integration/Testing/TestDbMeta.cs
@@ -33,9 +33,8 @@ public class TestDbMeta
         return connectionString.Replace(";;", ";");
     }
 
-    public static TestDbMeta
-        CreateWithMasterConnectionString(string name, bool isEmpty, string masterConnectionString) =>
-        new(name, isEmpty, ConstructConnectionString(masterConnectionString, name), Constants.ProviderName, null);
+        public static TestDbMeta CreateWithMasterConnectionString(string name, bool isEmpty, string masterConnectionString) =>
+            new TestDbMeta(name, isEmpty, ConstructConnectionString(masterConnectionString, name), Persistence.SqlServer.Constants.ProviderName, null);
 
     // LocalDb mdf funtimes
     public static TestDbMeta CreateWithoutConnectionString(string name, bool isEmpty) =>
