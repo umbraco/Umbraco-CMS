39b3e0a319498bb4923415510deb6c2218bce7e5
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2022-06-27 10:56:19 +0200

    add server handler

---
+++ src/Umbraco.Web.UI.Client/src/mocks/domains/server.handlers.ts
@@ -0,0 +1,24 @@
+import { rest } from 'msw';
+
+import { StatusResponse, VersionResponse } from '../../core/models';
+
+export const handlers = [
+  rest.get('/umbraco/backoffice/server/status', (_req, res, ctx) => {
+    return res(
+      // Respond with a 200 status code
+      ctx.status(200),
+      ctx.json<StatusResponse>({
+        installed: import.meta.env.VITE_UMBRACO_INSTALL_STATUS !== 'false',
+      })
+    );
+  }),
+  rest.get('/umbraco/backoffice/server/version', (_req, res, ctx) => {
+    return res(
+      // Respond with a 200 status code
+      ctx.status(200),
+      ctx.json<VersionResponse>({
+        version: '13.0.0',
+      })
+    );
+  }),
+];
--- src/Umbraco.Web.UI.Client/src/mocks/handlers.ts
+++ src/Umbraco.Web.UI.Client/src/mocks/handlers.ts
@@ -1,25 +1,13 @@
-import { rest } from 'msw';
-
-import { StatusResponse } from '../core/models';
 import { handlers as contentHandlers } from './domains/content.handlers';
 import { handlers as installHandlers } from './domains/install.handlers';
 import { handlers as manifestsHandlers } from './domains/manifests.handlers';
+import { handlers as serverHandlers } from './domains/server.handlers';
 import { handlers as userHandlers } from './domains/user.handlers';
-import { handlers as versionHandlers } from './domains/version.handlers';
 
 export const handlers = [
-  rest.get('/umbraco/backoffice/init', (_req, res, ctx) => {
-    return res(
-      // Respond with a 200 status code
-      ctx.status(200),
-      ctx.json<StatusResponse>({
-        installed: import.meta.env.VITE_UMBRACO_INSTALL_STATUS !== 'false',
-      })
-    );
-  }),
+  ...serverHandlers,
   ...contentHandlers,
   ...installHandlers,
   ...manifestsHandlers,
   ...userHandlers,
-  ...versionHandlers,
 ];
