074926811e54702bd24a351cb9dd5fc158aa7cde
Niels Lyngsø <niels.lyngso@gmail.com>
2022-06-28 10:31:49 +0200

    scale blocks

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/blockgrid/umbblockgridentry.component.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/blockgrid/umbblockgridentry.component.js
@@ -1,13 +1,47 @@
 (function () {
     "use strict";
 
+
+    /**
+     * Helper method that takes a weight map and finds the index of the value.
+     * @param {number} position - the value to find the index of.
+     * @param {number[]} weights - array of numbers each representing the weight/length.
+     * @returns {number} - the index of the weight that contains the accumulated value
+     */
+    function getIndexOfPositionInWeightMap(position, weights) {
+        let i = 0, len = weights.length, calc = 0;
+        while(i<len) {
+            if(position > calc && position <= calc+weights[i]) {
+                return i;
+            }
+
+            calc += weights[i];
+            i++;
+        }
+        return -1;
+    }
+
+    function closestColumnSpanOption(target, map) {
+        return map.reduce((a, b) => {
+            let aDiff = Math.abs(a.columnSpan - target);
+            let bDiff = Math.abs(b.columnSpan - target);
+    
+            if (aDiff == bDiff) {
+                return a.columnSpan < b.columnSpan ? a : b;
+            } else {
+                return bDiff < aDiff ? b : a;
+            }
+        });
+    }
+
+
+
     /**
      * @ngdoc directive
      * @name umbraco.directives.directive:umbBlockGridEntry
      * @description
      * renders each row for the block grid editor
      */
-    
     angular
         .module("umbraco")
         .component("umbBlockGridEntry", {
@@ -32,8 +66,25 @@
         let layoutContainer = null;
         let gridColumns = null;
         let gridRows = null;
-        let targetCol = null;
-        let targetRow = null;
+        //let targetCol = null;
+        //let targetRow = null;
+
+
+        function getNewSpans(startX, startY, endX, endY) {
+
+            // Get start col:
+            const blockStartCol = getIndexOfPositionInWeightMap(startX, gridColumns);
+            const blockStartRow = getIndexOfPositionInWeightMap(startY, gridRows);
+            const blockEndCol = getIndexOfPositionInWeightMap(endX, gridColumns);
+            const blockEndRow = getIndexOfPositionInWeightMap(endY, gridRows);
+
+            let newColumnSpan = Math.max(blockEndCol-blockStartCol, 1);
+            // Find nearest allowed Column:
+            newColumnSpan = closestColumnSpanOption(newColumnSpan, vm.layoutEntry.$block.config.columnSpanOptions).columnSpan;
+
+            const newRowSpan = Math.max(blockEndRow-blockStartRow, 1);
+            return {'columnSpan': newColumnSpan, 'rowSpan': newRowSpan};
+        }
 
         vm.scaleHandlerMouseDown = function($event) {
             $event.originalEvent.preventDefault();
@@ -42,14 +93,23 @@
             window.addEventListener('mouseup', vm.onMouseUp);
             window.addEventListener('mouseleave', vm.onMouseUp);
 
+
             layoutContainer = $element[0].closest('.umb-block-grid__layout-container');
             if(!layoutContainer) {
                 console.error($element[0], 'could not find parent layout-container');
             }
+
             const computedStyles = window.getComputedStyle(layoutContainer);
 
-            gridColumns = computedStyles.gridTemplateColumns.trim().split("px").map(x => Number(x))
-            gridRows = computedStyles.gridTemplateRows.trim().split("px").map(x => Number(x))
+            gridColumns = computedStyles.gridTemplateColumns.trim().split("px").map(x => Number(x));
+            gridRows = computedStyles.gridTemplateRows.trim().split("px").map(x => Number(x));
+
+            gridRows.push(100);
+            gridRows.push(100);
+            gridRows.push(100);
+
+            // ensure all columns are there.
+            // add a few extra rows, so there is something to extend too.
 
             // TODO: handle non-css-grid mode,
             // use container width divided by amount of columns( or the item width divided by its amount of columnSpan)
@@ -58,39 +118,35 @@
         }
         vm.onMouseMove = function(e) {
 
-            const localX = e.offsetX;
-            const localY = e.offsetY;
+            const layoutContainerRect = layoutContainer.getBoundingClientRect();
+            const layoutItemRect = $element[0].getBoundingClientRect();
 
-            targetCol = findIndexOfArray(localX, gridColumns);
-            targetRow = findIndexOfArray(localY, gridRows);
+            const startX = layoutItemRect.left - layoutContainerRect.left;
+            const startY = layoutItemRect.top - layoutContainerRect.top;
+            const endX = e.offsetX;
+            const endY = e.offsetY;
 
-        }
+            const newSpans = getNewSpans(startX, startY, endX, endY);
 
-        function findIndexOfArray(position, weights) {
-            let i = 0, len = weights.length, calc = 0;
-            while(i<len) {
-                if(position > calc && position <= calc+weights[i]) {
-                    return i;
-                }
-
-                calc += weights[i]
-                i++;
-            }
-            return -1;
+            console.log("newSpans", newSpans.columnSpan, newSpans.rowSpan)
         }
 
         vm.onMouseUp = function(e) {
-            console.log(targetCol, targetRow);
 
-            console.log(layoutContainer, $element[0]);
-            const targetRect = layoutContainer.getBoundingClientRect();
-            const elementRect = $element[0].getBoundingClientRect();
-            const elementX = elementRect.left - targetRect.left;
-            const elementY = elementRect.top - targetRect.top;
+            const layoutContainerRect = layoutContainer.getBoundingClientRect();
+            const layoutItemRect = $element[0].getBoundingClientRect();
 
-            // TODO: Numbers are good now, need to use them to find currentCol / Row and then figure out the new size.
-            console.log(elementX, elementY)
-            console.log(e.offsetX, e.offsetY)
+            const startX = layoutItemRect.left - layoutContainerRect.left;
+            const startY = layoutItemRect.top - layoutContainerRect.top;
+            const endX = e.offsetX;
+            const endY = e.offsetY;
+
+            const newSpans = getNewSpans(startX, startY, endX, endY);
+
+            console.log("newSpans", newSpans.columnSpan, newSpans.rowSpan)
+
+            vm.layoutEntry.columnSpan = newSpans.columnSpan;
+            vm.layoutEntry.rowSpan = newSpans.rowSpan;
 
             // Remove listeners:
             window.removeEventListener('mousemove', vm.onMouseMove);
@@ -101,8 +157,40 @@
             layoutContainer = null;
             gridColumns = null;
             gridRows = null;
-            targetCol = null;
-            targetRow = null;
+            //targetCol = null;
+            //targetRow = null;
+        }
+
+
+
+        vm.scaleHandlerKeyUp = function($event) {
+
+            let addCol = 0;
+            let addRow = 0;
+
+            console.log($event.originalEvent);
+
+            switch ($event.originalEvent.key) {
+                case 'ArrowUp':
+                    addRow = -1;
+                    break;
+                case 'ArrowDown':
+                    addRow = 1;
+                    break;
+                case 'ArrowLeft':
+                    addCol = -1;
+                    break;
+                case 'ArrowRight':
+                    addCol = 1;
+                    break;
+            }
+
+            const newColumnSpan = Math.max(vm.layoutEntry.columnSpan + addCol, 1);
+
+            vm.layoutEntry.columnSpan = closestColumnSpanOption(newColumnSpan, vm.layoutEntry.$block.config.columnSpanOptions).columnSpan;
+            vm.layoutEntry.rowSpan = Math.max(vm.layoutEntry.rowSpan + addRow, 1);
+
+            $event.originalEvent.stopPropagation();
         }
 
     }   
