558083ec78e7ed61eb0191198e243b08f382b5dd
Nikolaj <nikolajlauridsen@protonmail.ch>
2022-06-30 11:39:55 +0200

    Fix nullability of databasemodel

--- src/Umbraco.Core/Install/Models/DatabaseModel.cs
+++ src/Umbraco.Core/Install/Models/DatabaseModel.cs
@@ -12,16 +12,16 @@ public class DatabaseModel
     public string? ProviderName { get; set; }
 
     [DataMember(Name = "server")]
-    public string Server { get; set; } = null!;
+    public string? Server { get; set; }
 
     [DataMember(Name = "databaseName")]
     public string DatabaseName { get; set; } = null!;
 
     [DataMember(Name = "login")]
-    public string Login { get; set; } = null!;
+    public string? Login { get; set; }
 
     [DataMember(Name = "password")]
-    public string Password { get; set; } = null!;
+    public string? Password { get; set; }
 
     [DataMember(Name = "integratedAuth")]
     public bool IntegratedAuth { get; set; }
