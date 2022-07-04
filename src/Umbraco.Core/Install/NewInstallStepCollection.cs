9a8c167a085c0369b4e8c48f86a3dc21e1615dbf
Nikolaj <nikolajlauridsen@protonmail.ch>
2022-07-04 12:32:14 +0200

    Add installstep collection

---
+++ src/Umbraco.Core/Install/NewInstallStepCollection.cs
@@ -0,0 +1,19 @@
+﻿using Umbraco.Cms.Core.Composing;
+using Umbraco.Cms.Core.Install.Models;
+using Umbraco.Cms.Core.Install.NewModels;
+
+namespace Umbraco.Cms.Core.Install;
+
+public class NewInstallStepCollection : BuilderCollectionBase<NewInstallSetupStep>
+{
+    public NewInstallStepCollection(Func<IEnumerable<NewInstallSetupStep>> items)
+        : base(items)
+    {
+    }
+
+    public IEnumerable<NewInstallSetupStep> GetInstallSteps()
+        => this.Where(x => x.InstallationTypeTarget.HasFlag(InstallationType.NewInstall));
+
+    public IEnumerable<NewInstallSetupStep> GetUpgradeSteps()
+        => this.Where(x => x.InstallationTypeTarget.HasFlag(InstallationType.Upgrade));
+}
