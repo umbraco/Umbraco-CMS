cc0bcedfa8769ef285694be7f01e86a621df6c43
kjac <kjn@impact.dk>
2022-07-04 12:39:16 +0200

    Review changes

--- src/Umbraco.Core/Trees/ISearchableTreeWithCulture.cs
+++ src/Umbraco.Core/Trees/ISearchableTreeWithCulture.cs
@@ -1,28 +1,21 @@
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Umbraco.Cms.Core.Composing;
 using Umbraco.Cms.Core.Models.ContentEditing;
 
 namespace Umbraco.Cms.Core.Trees
 {
-    public interface ISearchableTree : IDiscoverable
+    [Obsolete("This interface will be merged into ISearchableTree in Umbraco 12")]
+    public interface ISearchableTreeWithCulture : ISearchableTree
     {
-        /// <summary>
-        /// The alias of the tree that the <see cref="ISearchableTree"/> belongs to
-        /// </summary>
-        string TreeAlias { get; }
-
         /// <summary>
         /// Searches for results based on the entity type
         /// </summary>
         /// <param name="query"></param>
         /// <param name="pageSize"></param>
         /// <param name="pageIndex"></param>
-        /// <param name="totalFound"></param>
         /// <param name="searchFrom">
         ///     A starting point for the search, generally a node id, but for members this is a member type alias
         /// </param>
+        /// <param name="culture"></param>
         /// <returns></returns>
-        Task<EntitySearchResults> SearchAsync(string query, int pageSize, long pageIndex, string? searchFrom = null);
+        Task<EntitySearchResults> SearchAsync(string query, int pageSize, long pageIndex, string? searchFrom = null, string? culture = null);
     }
 }
