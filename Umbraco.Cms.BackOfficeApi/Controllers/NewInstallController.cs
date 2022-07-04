1d7f0749c9a95972a32100cf430c08de6d9e5acb
Nikolaj <nikolajlauridsen@protonmail.ch>
2022-07-04 12:33:26 +0200

    Use service in controller

--- Umbraco.Cms.BackOfficeApi/Controllers/NewInstallController.cs
+++ Umbraco.Cms.BackOfficeApi/Controllers/NewInstallController.cs
@@ -3,15 +3,13 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
 using Umbraco.Cms.BackOfficeApi.Factories.Installer;
 using Umbraco.Cms.BackOfficeApi.Models.Installer;
+using Umbraco.Cms.BackOfficeApi.Services;
 using Umbraco.Cms.BackOfficeApi.ViewModels.Installer;
-using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Install.Models;
 using Umbraco.Cms.Core.Install.NewInstallSteps;
 using Umbraco.Cms.Core.Install.NewModels;
 using Umbraco.Cms.Core.Mapping;
-using Umbraco.Cms.Core.Security;
 using Umbraco.Cms.Core.Services;
-using Umbraco.Cms.Web.BackOffice.Security;
 
 namespace Umbraco.Cms.BackOfficeApi.Controllers;
 
@@ -25,27 +23,24 @@ public class NewInstallController : Controller
     private readonly IEnumerable<NewInstallSetupStep> _steps;
     private readonly ILogger<NewInstallController> _logger;
     private readonly IRuntime _runtime;
-    private readonly IBackOfficeUserManager _backOfficeUserManager;
-    private readonly IBackOfficeSignInManager _backOfficeSignInManager;
     private readonly IUmbracoMapper _mapper;
     private readonly IInstallSettingsFactory _installSettingsFactory;
+    private readonly IInstallService _installService;
 
     public NewInstallController(
         IEnumerable<NewInstallSetupStep> steps,
         ILogger<NewInstallController> logger,
         IRuntime runtime,
-        IBackOfficeUserManager backOfficeUserManager,
-        IBackOfficeSignInManager backOfficeSignInManager,
         IUmbracoMapper mapper,
-        IInstallSettingsFactory installSettingsFactory)
+        IInstallSettingsFactory installSettingsFactory,
+        IInstallService installService)
     {
         _steps = steps;
         _logger = logger;
         _runtime = runtime;
-        _backOfficeUserManager = backOfficeUserManager;
-        _backOfficeSignInManager = backOfficeSignInManager;
         _mapper = mapper;
         _installSettingsFactory = installSettingsFactory;
+        _installService = installService;
     }
 
     [HttpGet("settings")]
@@ -74,31 +69,7 @@ public class NewInstallController : Controller
 
         InstallData data = _mapper.Map<InstallData>(installData)!;
 
-        IOrderedEnumerable<NewInstallSetupStep> orderedSteps = _steps
-            .Where(x => x.InstallationTypeTarget.HasFlag(InstallationType.NewInstall))
-            .OrderBy(x => x.Order);
-
-        foreach (NewInstallSetupStep step in orderedSteps)
-        {
-            var stepName = step.Name;
-            _logger.LogInformation("Checking if {StepName} requires execution", stepName);
-            if (await step.RequiresExecutionAsync(data) is false)
-            {
-                _logger.LogInformation("Skipping {StepName}", stepName);
-                continue;
-            }
-
-            _logger.LogInformation("Running {StepName}", stepName);
-            await step.ExecuteAsync(data);
-        }
-
-        // Restart the runtime now that the installer has run
-        await _runtime.RestartAsync();
-
-        // Sign the newly created user in (Not sure if we want this separately in the future?
-        BackOfficeIdentityUser identityUser =
-            await _backOfficeUserManager.FindByIdAsync(Constants.Security.SuperUserIdAsString);
-        await _backOfficeSignInManager.SignInAsync(identityUser, false);
+        await _installService.Install(data);
 
         return Created("/", null);
     }
