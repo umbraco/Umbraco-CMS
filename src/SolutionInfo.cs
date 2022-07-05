613dbe27a954bd8fb444fa82b4745a1dbdf0212f
Sebastiaan Janssen <sebastiaan@umbraco.com>
2022-07-05 17:07:28 +0200

    Bump version to 7.15.8

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -2,7 +2,7 @@
 using System.Resources;
 
 [assembly: AssemblyCompany("Umbraco")]
-[assembly: AssemblyCopyright("Copyright © Umbraco 2018")]
+[assembly: AssemblyCopyright("Copyright © Umbraco 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -11,5 +11,5 @@ using System.Resources;
 
 [assembly: AssemblyVersion("1.0.*")]
 
-[assembly: AssemblyFileVersion("7.15.7")]
-[assembly: AssemblyInformationalVersion("7.15.7")]
+[assembly: AssemblyFileVersion("7.15.8")]
+[assembly: AssemblyInformationalVersion("7.15.8")]
--- src/Umbraco.Core/Configuration/UmbracoVersion.cs
+++ src/Umbraco.Core/Configuration/UmbracoVersion.cs
@@ -6,7 +6,7 @@ namespace Umbraco.Core.Configuration
 {
     public class UmbracoVersion
     {
-        private static readonly Version Version = new Version("7.15.7");
+        private static readonly Version Version = new Version("7.15.8");
 
         /// <summary>
         /// Gets the current version of Umbraco.
