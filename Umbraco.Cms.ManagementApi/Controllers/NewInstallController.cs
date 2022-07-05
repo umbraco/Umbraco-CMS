430482618f3fb23238f1da65db52430adadfe7ce
Nikolaj <nikolajlauridsen@protonmail.ch>
2022-07-05 12:48:44 +0200

    Restrict access to installcontroller based on runtime level

--- Umbraco.Cms.ManagementApi/Controllers/NewInstallController.cs
+++ Umbraco.Cms.ManagementApi/Controllers/NewInstallController.cs
@@ -1,14 +1,14 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Mapping;
+using Umbraco.Cms.ManagementApi.Filters;
 using Umbraco.Cms.ManagementApi.ViewModels.Installer;
 using Umbraco.New.Cms.Core.Factories;
 using Umbraco.New.Cms.Core.Models.Installer;
 using Umbraco.New.Cms.Core.Services.Installer;
 
 namespace Umbraco.Cms.ManagementApi.Controllers;
-
-// TODO: Create a filter to require install/upgrade for endpoints to be valid
 
 [ApiController]
 [ApiVersion("1.0")]
@@ -32,7 +32,9 @@ public class NewInstallController : Controller
     [HttpGet("settings")]
     [MapToApiVersion("1.0")]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status428PreconditionRequired)]
     [ProducesResponseType(typeof(InstallSettingsViewModel), StatusCodes.Status200OK)]
+    [RequireRuntimeLevel(RuntimeLevel.Install)]
     public async Task<IActionResult> Settings()
     {
         InstallSettingsModel installSettings = _installSettingsFactory.GetInstallSettings();
@@ -44,8 +46,10 @@ public class NewInstallController : Controller
 
     [HttpPost("setup")]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status428PreconditionRequired)]
     [ProducesResponseType(StatusCodes.Status200OK)]
     [MapToApiVersion("1.0")]
+    [RequireRuntimeLevel(RuntimeLevel.Install)]
     public async Task<IActionResult> Setup(InstallViewModel installData)
     {
         if (ModelState.IsValid is false)
@@ -63,7 +67,9 @@ public class NewInstallController : Controller
     [HttpPost("upgrade")]
     [MapToApiVersion("1.0")]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status428PreconditionRequired)]
     [ProducesResponseType(StatusCodes.Status200OK)]
+    [RequireRuntimeLevel(RuntimeLevel.Upgrade)]
     public async Task<IActionResult> Upgrade()
     {
         await _installService.Upgrade();
