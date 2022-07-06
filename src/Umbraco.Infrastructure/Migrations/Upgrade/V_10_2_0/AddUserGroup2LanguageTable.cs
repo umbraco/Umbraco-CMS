49dd2b44e3f1b51347c1918f6e7954bc463bd70f
Zeegaan <nge@umbraco.dk>
2022-07-06 14:11:19 +0200

    Add hasAccessToAllLanguages column to usergroup table

--- src/Umbraco.Infrastructure/Migrations/Upgrade/V_10_2_0/AddUserGroup2LanguageTable.cs
+++ src/Umbraco.Infrastructure/Migrations/Upgrade/V_10_2_0/AddUserGroup2LanguageTable.cs
@@ -1,4 +1,5 @@
-﻿using Umbraco.Cms.Infrastructure.Persistence.Dtos;
+﻿using Umbraco.Cms.Core;
+using Umbraco.Cms.Infrastructure.Persistence.Dtos;
 using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Infrastructure.Migrations.Upgrade.V_10_2_0;
@@ -12,6 +13,13 @@ public class AddUserGroup2LanguageTable : MigrationBase
     protected override void Migrate()
     {
         IEnumerable<string> tables = SqlSyntax.GetTablesInSchema(Context.Database);
+
+        Create.Column("hasAccessToAllLanguages")
+            .OnTable(Constants.DatabaseSchema.Tables.UserGroup)
+            .AsBoolean()
+            .WithDefaultValue(true)
+            .Do();
+
         if (tables.InvariantContains(UserGroup2LanguageDto.TableName))
         {
             return;
