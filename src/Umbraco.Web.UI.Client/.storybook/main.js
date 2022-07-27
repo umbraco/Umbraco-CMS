78558238202d7a9617591acd7755922dad2f1c10
JesmoDev <jjensenmoeller@gmail.com>
2022-07-27 17:49:32 +0200

    added storybook

---
+++ src/Umbraco.Web.UI.Client/.storybook/main.js
@@ -0,0 +1,8 @@
+module.exports = {
+  "stories": ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  "addons": ["@storybook/addon-links", "@storybook/addon-essentials"],
+  "framework": "@storybook/web-components",
+  "core": {
+    "builder": "@storybook/builder-vite"
+  }
+};
