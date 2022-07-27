f0cc0d114778c3e0406fdd59d69e88e63e7e065f
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2022-07-27 14:48:34 +0200

    add upgrade handlers

--- src/Umbraco.Web.UI.Client/src/mocks/handlers.ts
+++ src/Umbraco.Web.UI.Client/src/mocks/handlers.ts
@@ -2,12 +2,14 @@ import { handlers as contentHandlers } from './domains/content.handlers';
 import { handlers as installHandlers } from './domains/install.handlers';
 import { handlers as manifestsHandlers } from './domains/manifests.handlers';
 import { handlers as serverHandlers } from './domains/server.handlers';
+import { handlers as upgradeHandlers } from './domains/upgrade.handlers';
 import { handlers as userHandlers } from './domains/user.handlers';
 
 export const handlers = [
 	...serverHandlers,
 	...contentHandlers,
 	...installHandlers,
+	...upgradeHandlers,
 	...manifestsHandlers,
 	...userHandlers,
 ];
