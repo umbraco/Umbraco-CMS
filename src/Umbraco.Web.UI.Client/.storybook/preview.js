8ed68bd02d0b250123642a590519a6fcfbc8e9dc
JesmoDev <jjensenmoeller@gmail.com>
2022-07-28 14:38:42 +0200

    add msw to storybook

--- src/Umbraco.Web.UI.Client/.storybook/preview.js
+++ src/Umbraco.Web.UI.Client/.storybook/preview.js
@@ -1,5 +1,7 @@
 import '@umbraco-ui/uui-css/dist/uui-css.css';
 import '@umbraco-ui/uui';
+// This imports and runs the MSW
+import '../src/index.ts';
 
 export const parameters = {
 	actions: { argTypesRegex: '^on[A-Z].*' },
