bf8b19614b71d30536d815b6a7535ea0d60f60ae
JesmoDev <jjensenmoeller@gmail.com>
2022-07-28 14:15:51 +0200

    storybook setup and styling

--- src/Umbraco.Web.UI.Client/.storybook/preview.js
+++ src/Umbraco.Web.UI.Client/.storybook/preview.js
@@ -1,9 +1,12 @@
+import '@umbraco-ui/uui-css/dist/uui-css.css';
+import '@umbraco-ui/uui';
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
-  controls: {
-    matchers: {
-      color: /(background|color)$/i,
-      date: /Date$/,
-    },
-  },
-}
+	actions: { argTypesRegex: '^on[A-Z].*' },
+	controls: {
+		matchers: {
+			color: /(background|color)$/i,
+			date: /Date$/,
+		},
+	},
+};
--- src/Umbraco.Web.UI.Client/src/upgrader/upgrader.stories.ts
+++ src/Umbraco.Web.UI.Client/src/upgrader/upgrader.stories.ts
@@ -1,5 +1,5 @@
 import { Story, Meta } from '@storybook/web-components';
-import { html } from 'lit';
+import { html } from 'lit-html';
 import './upgrader.element';
 
 export default {
