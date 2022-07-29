d899279505257b9966a4cbc6b6e25f6ea42b8f81
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2022-07-29 11:04:25 +0200

    divide the upgrader into a logic and a view component

--- src/Umbraco.Web.UI.Client/src/upgrader/upgrader.element.ts
+++ src/Umbraco.Web.UI.Client/src/upgrader/upgrader.element.ts
@@ -1,23 +1,14 @@
-import '../installer/installer-layout.element';
+import './upgrader-view.element';
 
-import { css, CSSResultGroup, html, LitElement } from 'lit';
+import { html, LitElement } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
-import { ifDefined } from 'lit/directives/if-defined.js';
 
 import { getUpgradeSettings, PostUpgradeAuthorize } from '../core/api/fetcher';
-import { UmbContextProviderMixin } from '../core/context';
 import { UmbracoUpgrader } from '../core/models';
 
 @customElement('umb-upgrader')
-export class UmbUpgrader extends UmbContextProviderMixin(LitElement) {
-	static styles: CSSResultGroup = [
-		css`
-			.error {
-				color: var(--uui-color-danger);
-			}
-		`,
-	];
-
+export class UmbUpgrader extends LitElement {
+	@state()
 	private upgradeSettings?: UmbracoUpgrader;
 
 	@state()
@@ -34,55 +25,13 @@ export class UmbUpgrader extends UmbContextProviderMixin(LitElement) {
 		this._setup();
 	}
 
-	private _renderLayout() {
-		return html`
-				<h1>Upgrading Umbraco</h1>
-
-				<p>
-					Welcome to the Umbraco installer. You see this screen because your Umbraco installation needs a quick upgrade
-					of its database and files, which will ensure your website is kept as fast, secure and up to date as possible.
-				</p>
-
-				<p>
-					Detected current version <strong>${this.upgradeSettings?.oldVersion}</strong> (${this.upgradeSettings?.currentState}),
-					which needs to be upgraded to <strong>${this.upgradeSettings?.newVersion}</strong> (${this.upgradeSettings?.newState}).
-					To compare versions and read a report of changes between versions, use the View Report button below.
-				</p>
-
-				<p>
-					<uui-button
-						look="secondary"
-						href="${ifDefined(this.upgradeSettings?.reportUrl)}"
-						target="_blank"
-						label="View Report"></uui-button>
-				</p>
-
-				<p>Simply click <strong>continue</strong> below to be guided through the rest of the upgrade.</p>
-
-				<form @submit=${this._handleSubmit}>
-					<p>
-						<uui-button
-							type="submit"
-							look="primary"
-							color="positive"
-							label="Continue"
-							state=${ifDefined(this.upgrading ? 'waiting' : undefined)}></uui-button>
-					</p>
-				</form>
-
-				${this._renderError()}
-			</div>
-		`;
-	}
-
-	private _renderError() {
-		return html` ${this.errorMessage ? html`<p class="error">${this.errorMessage}</p>` : ''} `;
-	}
-
 	render() {
-		return html` <umb-installer-layout>
-			<div id="container" class="uui-text">${this.fetching ? html`<div>Loading...</div>` : this._renderLayout()}</div>
-		</umb-installer-layout>`;
+		return html`<umb-upgrader-view
+			.fetching=${this.fetching}
+			.upgrading=${this.upgrading}
+			.settings=${this.upgradeSettings}
+			.errorMessage=${this.errorMessage}
+			@onAuthorizeUpgrade=${this._handleSubmit}></umb-upgrader-view>`;
 	}
 
 	private async _setup() {
@@ -101,8 +50,7 @@ export class UmbUpgrader extends UmbContextProviderMixin(LitElement) {
 		this.fetching = false;
 	}
 
-	_handleSubmit = async (e: SubmitEvent) => {
-		e.preventDefault();
+	_handleSubmit = async () => {
 		this.errorMessage = '';
 		this.upgrading = true;
 
