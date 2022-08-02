320a7b66a28462c3f76843800af0b82dbf259c99
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2022-08-02 13:37:46 +0200

    replace mocked decorator with umb-context-provider element with the real decorator

--- src/Umbraco.Web.UI.Client/src/installer/installer.stories.ts
+++ src/Umbraco.Web.UI.Client/src/installer/installer.stories.ts
@@ -1,80 +1,28 @@
-import './installer.element';
+import '../core/context/context-provider.element';
+import './installer-consent.element';
+import './installer-database.element';
+import './installer-installing.element';
+import './installer-user.element';
 
 import { Meta, Story } from '@storybook/web-components';
-import { css, CSSResult, LitElement } from 'lit';
 import { html } from 'lit-html';
-import { customElement } from 'lit/decorators.js';
-import { BehaviorSubject } from 'rxjs';
 
 import { UmbInstallerUser } from '.';
-import { UmbContextProviderMixin } from '../core/context';
-import { PostInstallRequest, UmbracoInstaller } from '../core/models';
-
-@customElement('mock-installer-context')
-class UmbInstallerContext extends UmbContextProviderMixin(LitElement) {
-	static styles: CSSResult[] = [
-		css`
-			:host {
-				display: block;
-				margin: 2rem 25%;
-				padding: 1rem;
-				border: 1px solid #ddd;
-			}
-		`,
-	];
-
-	constructor() {
-		super();
-		const data = new BehaviorSubject<PostInstallRequest>({
-			telemetryLevel: 'Basic',
-			user: {
-				name: 'Umbraco',
-				email: 'test@umbraco.com',
-				password: 'test123456',
-				subscribeToNewsletter: false,
-			},
-		});
-
-		this.provideContext('umbInstallerContext', {
-			data,
-			getData: () => data.value,
-			settings: new BehaviorSubject<UmbracoInstaller>({
-				user: {
-					consentLevels: [{ description: 'This is a consent text', level: 'Basic' }],
-					minCharLength: 2,
-					minNonAlphaNumericLength: 2,
-				},
-				databases: [
-					{
-						id: '123',
-						defaultDatabaseName: 'umbraco',
-						displayName: 'SQLite',
-						isConfigured: false,
-						providerName: 'Umbraco',
-						requiresConnectionTest: false,
-						requiresCredentials: false,
-						requiresServer: false,
-						serverPlaceholder: 'umbraco',
-						sortOrder: 0,
-						supportsIntegratedAuthentication: false,
-					},
-				],
-			}),
-			appendData: () => true,
-			requestInstall: () => Promise.resolve(),
-		} as Partial<UmbInstallerContext>);
-	}
-
-	render() {
-		return html`<slot></slot>`;
-	}
-}
+import { UmbInstallerContext } from './installer-context';
 
 export default {
 	title: 'Components/Installer/Steps',
 	component: 'umb-installer',
 	id: 'installer',
-	decorators: [(story) => html`<mock-installer-context>${story()}</mock-installer-context>`],
+	decorators: [
+		(story) =>
+			html`<umb-context-provider
+				style="display: block;margin: 2rem 25%;padding: 1rem;border: 1px solid #ddd;"
+				key="umbInstallerContext"
+				.value=${new UmbInstallerContext()}>
+				${story()}
+			</umb-context-provider>`,
+	],
 } as Meta;
 
 export const Step1User: Story<UmbInstallerUser> = () => html`<umb-installer-user></umb-installer-user>`;
