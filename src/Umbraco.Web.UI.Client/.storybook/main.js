6c1fc5c5e63867e4dfd4f93a25bbdd80d6c5fd1e
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2022-08-02 12:59:59 +0200

    add @storybook/a11y to storybook

--- src/Umbraco.Web.UI.Client/.storybook/main.js
+++ src/Umbraco.Web.UI.Client/.storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
 	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials'],
+	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
 	framework: '@storybook/web-components',
 	core: {
 		builder: '@storybook/builder-vite',
