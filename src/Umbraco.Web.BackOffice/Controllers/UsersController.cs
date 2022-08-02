180f205195818909df4f87ef95573615bf97f0c0
Sebastiaan Janssen <sebastiaan@umbraco.com>
2022-08-02 11:35:53 +0200

    Force the allowed avatar image types

--- src/Umbraco.Web.BackOffice/Controllers/UsersController.cs
+++ src/Umbraco.Web.BackOffice/Controllers/UsersController.cs
@@ -226,8 +226,9 @@ namespace Umbraco.Cms.Web.BackOffice.Controllers
             var fileName = file.FileName.Trim(new[] { '\"' }).TrimEnd();
             var safeFileName = fileName.ToSafeFileName(shortStringHelper);
             var ext = safeFileName.Substring(safeFileName.LastIndexOf('.') + 1).ToLower();
+            const string allowedAvatarFileTypes = "jpeg,jpg,gif,bmp,png,tiff,tif,webp";
 
-            if (contentSettings.DisallowedUploadFiles.Contains(ext) == false)
+            if (allowedAvatarFileTypes.Contains(ext) == true && contentSettings.DisallowedUploadFiles.Contains(ext) == false)
             {
                 //generate a path of known data, we don't want this path to be guessable
                 user.Avatar = "UserAvatars/" + (user.Id + safeFileName).GenerateHash<SHA1>() + "." + ext;
