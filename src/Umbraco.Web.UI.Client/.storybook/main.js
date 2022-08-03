34571d45122da853d7ae3b02cdc668e8bf2e416b
Mads Rasmussen <madsr@hey.com>
2022-08-03 16:24:09 +0200

    add docs for notifications

--- src/Umbraco.Web.UI.Client/.storybook/main.js
+++ src/Umbraco.Web.UI.Client/.storybook/main.js
@@ -1,7 +1,10 @@
 module.exports = {
 	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
+	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y', '@storybook/addon-docs'],
 	framework: '@storybook/web-components',
+	features: {
+		previewMdx2: true
+	},
 	core: {
 		builder: '@storybook/builder-vite',
 	},
