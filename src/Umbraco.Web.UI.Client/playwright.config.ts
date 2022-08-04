50f45920a0843b6d0e06455cde509a2fc945c606
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2022-08-04 14:47:58 +0200

    set webserver for playwright

--- src/Umbraco.Web.UI.Client/playwright.config.ts
+++ src/Umbraco.Web.UI.Client/playwright.config.ts
@@ -5,7 +5,7 @@ import type { PlaywrightTestConfig } from '@playwright/test';
  * Read environment variables from file.
  * https://github.com/motdotla/dotenv
  */
-// require('dotenv').config();
+// dotenv.config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -36,7 +36,7 @@ const config: PlaywrightTestConfig = {
 		/* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
 		actionTimeout: 0,
 		/* Base URL to use in actions like `await page.goto('/')`. */
-		baseURL: 'http://localhost:5173', // Vitejs dev server
+		// baseURL: 'http://localhost:5173',
 
 		/* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
 		trace: 'on-first-retry',
@@ -98,10 +98,11 @@ const config: PlaywrightTestConfig = {
 	// outputDir: 'test-results/',
 
 	/* Run your local dev server before starting the tests */
-	// webServer: {
-	//   command: 'npm run start',
-	//   port: 3000,
-	// },
+	webServer: {
+		command: 'npm run dev',
+		port: 5173,
+		reuseExistingServer: !process.env.CI,
+	},
 };
 
 export default config;
