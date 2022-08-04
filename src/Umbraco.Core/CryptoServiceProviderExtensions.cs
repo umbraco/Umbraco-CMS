21d5f1b754f66afb5f53099fa0e2c80741c715c9
nikolajlauridsen <nikolajlauridsen@protonmail.ch>
2022-08-04 10:53:00 +0200

    Replace usage of Random().Next with RNGCryptoServiceProvider

---
+++ src/Umbraco.Core/CryptoServiceProviderExtensions.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Security.Cryptography;
+
+namespace Umbraco.Core
+{
+    public static class CryptoServiceProviderExtensions
+    {
+        /// <summary>
+        /// Generates a random int withing a specified range.
+        /// </summary>
+        /// <param name="provider">Random bytes provider.</param>
+        /// <param name="minValue">The minimum value of the resulting int.</param>
+        /// <param name="maxValue">The maximum value of the resulting int.</param>
+        /// <returns>A random integer that falls withing the specified range.</returns>
+        public static int GetInt32(this RNGCryptoServiceProvider provider, int minValue, int maxValue)
+        {
+            var randomBytes = new byte[4];
+            provider.GetBytes(randomBytes);
+
+            var randomInt = Math.Abs(BitConverter.ToInt32(randomBytes, 0));
+            // We call do mod to ensure that the value is within the specified range.
+            return randomInt % (maxValue - minValue + 1) + minValue;
+        }
+    }
+}
+
+
