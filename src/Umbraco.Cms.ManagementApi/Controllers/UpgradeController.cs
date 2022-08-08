c65afc09a63ec4596ab656acc022e85a946396bc
Nikolaj <nikolajlauridsen@protonmail.ch>
2022-08-08 09:25:11 +0200

    Add upgrade/settings endpoint

---
+++ src/Umbraco.Cms.ManagementApi/Controllers/UpgradeController.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Mapping;
+using Umbraco.Cms.ManagementApi.Filters;
+using Umbraco.Cms.ManagementApi.ViewModels.Installer;
+using Umbraco.New.Cms.Core.Factories;
+using Umbraco.New.Cms.Core.Models.Installer;
+using Umbraco.New.Cms.Web.Common.Routing;
+
+namespace Umbraco.Cms.ManagementApi.Controllers;
+
+[ApiController]
+[ApiVersion("1.0")]
+[BackofficeRoute("api/v{version:apiVersion}/upgrade")]
+public class UpgradeController : Controller
+{
+    private readonly IUpgradeSettingsFactory _upgradeSettingsFactory;
+    private readonly IUmbracoMapper _mapper;
+
+    public UpgradeController(
+        IUpgradeSettingsFactory upgradeSettingsFactory,
+        IUmbracoMapper mapper)
+    {
+        _upgradeSettingsFactory = upgradeSettingsFactory;
+        _mapper = mapper;
+    }
+
+    [HttpGet("settings")]
+    [MapToApiVersion("1.0")]
+    [RequireRuntimeLevel(RuntimeLevel.Upgrade)]
+    [ProducesResponseType(typeof(UpgradeSettingsViewModel), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status428PreconditionRequired)]
+    public async Task<ActionResult<UpgradeSettingsViewModel>> Settings()
+    {
+        UpgradeSettingsModel upgradeSettings = _upgradeSettingsFactory.GetUpgradeSettings();
+        UpgradeSettingsViewModel viewModel = _mapper.Map<UpgradeSettingsViewModel>(upgradeSettings)!;
+
+        return viewModel;
+    }
+}
