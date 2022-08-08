2abcd64808440704f47324dc04d31327723fee6a
Jesper Møller Jensen <jmj@umbraco.dk>
2022-08-08 15:24:02 +0200

    removed redundant @storybook/addon-docs package

--- src/Umbraco.Web.UI.Client/.storybook/main.js
+++ src/Umbraco.Web.UI.Client/.storybook/main.js
@@ -1,6 +1,6 @@
 module.exports = {
 	stories: ['../src/**/*.stories.mdx', '../src/**/*.stories.@(js|jsx|ts|tsx)'],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y', '@storybook/addon-docs'],
+	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
 	framework: '@storybook/web-components',
 	features: {
 		previewMdx2: true,
