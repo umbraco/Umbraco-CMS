0a16d9921e0cc797fe33fbec96f7d1d219900bf5
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2022-08-09 15:47:05 +0200

    add test for upgrader-view

---
+++ src/Umbraco.Web.UI.Client/src/upgrader/upgrader-view.test.ts
@@ -0,0 +1,19 @@
+import { expect, fixture, html } from '@open-wc/testing';
+
+import { UmbUpgraderView } from './upgrader-view.element';
+
+describe('UmbUpgrader', () => {
+	let element: UmbUpgraderView;
+
+	beforeEach(async () => {
+		element = await fixture(html`<umb-upgrader-view></umb-upgrader-view>`);
+	});
+
+	it('is defined with its own instance', () => {
+		expect(element).to.be.instanceOf(UmbUpgraderView);
+	});
+
+	it('passes the a11y audit', async () => {
+		await expect(element).shadowDom.to.be.accessible();
+	});
+});
