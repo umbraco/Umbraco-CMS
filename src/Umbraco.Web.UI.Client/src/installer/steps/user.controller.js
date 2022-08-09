10b96c5939077dd834d34b0302cdba34a6e22e1e
Tobias Mønster <tobias@klean.dk>
2022-08-09 12:17:22 +0200

    Renames Installer form from "myForm" to "installerForm"

--- src/Umbraco.Web.UI.Client/src/installer/steps/user.controller.js
+++ src/Umbraco.Web.UI.Client/src/installer/steps/user.controller.js
@@ -93,7 +93,7 @@ angular.module("umbraco.install").controller("Umbraco.Install.UserController", f
   };
 
   $scope.validateAndForward = function () {
-    if (this.myForm.$valid) {
+    if (this.installerForm.$valid) {
       installerService.forward();
     }
   };
