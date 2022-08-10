8e59eadc53f52dfbedab3eb20ad7704f3147d852
Mads Rasmussen <madsr@hey.com>
2022-08-10 15:14:56 +0200

    use open prop to close notification instead of internal method

--- src/Umbraco.Web.UI.Client/src/core/services/notification/notification-handler.ts
+++ src/Umbraco.Web.UI.Client/src/core/services/notification/notification-handler.ts
@@ -70,8 +70,7 @@ export class UmbNotificationHandler {
 	 */
 	public close(...args: any) {
 		this._closeResolver(...args);
-		// TODO: make public close method on uui-toast-notification
-		this.element._makeClose?.();
+		this.element.open = false;
 	}
 
 	/**
