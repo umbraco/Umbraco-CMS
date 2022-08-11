feed58ecb991d8f09c53e225085b5b855bcdddde
kjac <kja@umbraco.dk>
2022-08-11 14:18:39 +0200

    Prettify block grid editor data conveter a bit

--- src/Umbraco.Infrastructure/Models/Blocks/BlockGridEditorDataConverter.cs
+++ src/Umbraco.Infrastructure/Models/Blocks/BlockGridEditorDataConverter.cs
@@ -1,50 +1,43 @@
-﻿using System.Collections.Generic;
-using System.Linq;
 using Newtonsoft.Json.Linq;
 using Umbraco.Cms.Core.Serialization;
 
-namespace Umbraco.Cms.Core.Models.Blocks
+namespace Umbraco.Cms.Core.Models.Blocks;
+
+/// <summary>
+/// Data converter for the block grid property editor
+/// </summary>
+public class BlockGridEditorDataConverter : BlockEditorDataConverter
 {
-    /// <summary>
-    /// Data converter for the block grid property editor
-    /// </summary>
-    public class BlockGridEditorDataConverter : BlockEditorDataConverter
+    private readonly IJsonSerializer _jsonSerializer;
+
+    public BlockGridEditorDataConverter(IJsonSerializer jsonSerializer) : base(Cms.Core.Constants.PropertyEditors.Aliases.BlockGrid)
+        => _jsonSerializer = jsonSerializer;
+
+    protected override IEnumerable<ContentAndSettingsReference>? GetBlockReferences(JToken jsonLayout)
     {
-        private readonly IJsonSerializer _jsonSerializer;
-
-        public BlockGridEditorDataConverter(IJsonSerializer jsonSerializer) : base(Cms.Core.Constants.PropertyEditors.Aliases.BlockGrid)
+        IEnumerable<BlockGridLayoutItem>? blockListLayouts = jsonLayout.ToObject<IEnumerable<BlockGridLayoutItem>>();
+        if (blockListLayouts == null)
         {
-            _jsonSerializer = jsonSerializer;
+            return null;
         }
 
-        protected override IEnumerable<ContentAndSettingsReference> GetBlockReferences(JToken jsonLayout)
+        var result = new List<ContentAndSettingsReference>();
+
+        foreach (BlockGridLayoutItem blockGridLayoutItem in blockListLayouts)
         {
-
-            var blockListLayouts = _jsonSerializer.Deserialize<BlockGridLayoutItem[]>(jsonLayout.ToString())!;
-
-            var result = new List<ContentAndSettingsReference>();
-
-            foreach (BlockGridLayoutItem blockGridLayoutItem in blockListLayouts)
-            {
-                AddToResult(blockGridLayoutItem, result);
-            }
-
-            return result;
+            AddToResult(blockGridLayoutItem, result);
         }
 
-        private void AddToResult(BlockGridLayoutItem x, List<ContentAndSettingsReference> result)
+        return result;
+    }
+
+    private void AddToResult(BlockGridLayoutItem layoutItem, List<ContentAndSettingsReference> result)
+    {
+        result.Add(new ContentAndSettingsReference(layoutItem.ContentUdi, layoutItem.SettingsUdi));
+
+        foreach (BlockGridLayoutItem areaLayoutItem in layoutItem.Areas.SelectMany(x => x.Items))
         {
-            if (x is null)
-            {
-                return;
-            }
-
-           result.Add(new ContentAndSettingsReference(x.ContentUdi, x.SettingsUdi));
-
-           foreach (var child in x.Areas.SelectMany(x=>x.Items))
-           {
-               AddToResult(child, result);
-           }
+            AddToResult(areaLayoutItem, result);
         }
     }
 }
