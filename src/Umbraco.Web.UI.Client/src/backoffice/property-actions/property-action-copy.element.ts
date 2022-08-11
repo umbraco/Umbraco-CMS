fa159676376908383c983a4e70e00fd90490345a
Mads Rasmussen <madsr@hey.com>
2022-08-11 11:47:11 +0200

    format all files

--- src/Umbraco.Web.UI.Client/src/backoffice/property-actions/property-action-copy.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/property-actions/property-action-copy.element.ts
@@ -6,43 +6,44 @@ import type { UmbNotificationService } from '../../core/services/notification';
 import type { UmbPropertyAction } from './property-action/property-action.model';
 
 @customElement('umb-property-action-copy')
-export default class UmbPropertyActionCopyElement extends UmbContextConsumerMixin(LitElement) implements UmbPropertyAction {
-  
-  @property()
-  value = '';
+export default class UmbPropertyActionCopyElement
+	extends UmbContextConsumerMixin(LitElement)
+	implements UmbPropertyAction
+{
+	@property()
+	value = '';
 
-  private _notificationService?: UmbNotificationService;
-  
-  constructor () {
-    super();
+	private _notificationService?: UmbNotificationService;
 
-    // TODO implement a property context
-    this.consumeContext('umbProperty', (property) => {
-      console.log('PROPERTY', property);
-    });
+	constructor() {
+		super();
 
-    this.consumeContext('umbNotificationService', (notificationService: UmbNotificationService) => {
-      this._notificationService = notificationService;
-    });
-  }
+		// TODO implement a property context
+		this.consumeContext('umbProperty', (property) => {
+			console.log('PROPERTY', property);
+		});
 
-  private _handleLabelClick () {
-    const data: UmbNotificationDefaultData = { message: 'Copied to clipboard' };
-    this._notificationService?.peek('positive', { data });
-    // TODO: how do we want to close the menu? Testing an event based approach
-    this.dispatchEvent(new CustomEvent('close', { bubbles: true, composed: true }));
-  }
+		this.consumeContext('umbNotificationService', (notificationService: UmbNotificationService) => {
+			this._notificationService = notificationService;
+		});
+	}
 
-  render() {
-    return html`
-      <uui-menu-item label="Copy" @click-label="${this._handleLabelClick}">
-        <uui-icon slot="icon" name="copy"></uui-icon>
-      </uui-menu-item>`;
-  }
+	private _handleLabelClick() {
+		const data: UmbNotificationDefaultData = { message: 'Copied to clipboard' };
+		this._notificationService?.peek('positive', { data });
+		// TODO: how do we want to close the menu? Testing an event based approach
+		this.dispatchEvent(new CustomEvent('close', { bubbles: true, composed: true }));
+	}
+
+	render() {
+		return html` <uui-menu-item label="Copy" @click-label="${this._handleLabelClick}">
+			<uui-icon slot="icon" name="copy"></uui-icon>
+		</uui-menu-item>`;
+	}
 }
 
 declare global {
-  interface HTMLElementTagNameMap {
-    'umb-property-action-copy': UmbPropertyActionCopyElement;
-  }
+	interface HTMLElementTagNameMap {
+		'umb-property-action-copy': UmbPropertyActionCopyElement;
+	}
 }
--- src/Umbraco.Web.UI.Client/src/backoffice/property-actions/property-action-menu/property-action-menu.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/property-actions/property-action-menu/property-action-menu.element.ts
@@ -10,123 +10,126 @@ import '../property-action/property-action.element';
 
 @customElement('umb-property-action-menu')
 export class UmbPropertyActionMenuElement extends UmbContextProviderMixin(UmbContextConsumerMixin(LitElement)) {
-  static styles: CSSResultGroup = [
-    UUITextStyles,
-    css`
-      #popover {
-        width: auto;
-      }
+	static styles: CSSResultGroup = [
+		UUITextStyles,
+		css`
+			#popover {
+				width: auto;
+			}
 
-      #more-symbol {
-        font-size: 0.6em;
-      }
+			#more-symbol {
+				font-size: 0.6em;
+			}
 
-      #popover-trigger {
-        --uui-button-padding-top-factor: 0.5;
-        --uui-button-padding-bottom-factor: 0.1;
-      }
+			#popover-trigger {
+				--uui-button-padding-top-factor: 0.5;
+				--uui-button-padding-bottom-factor: 0.1;
+			}
 
-      #dropdown {
-        background-color: white;
-        border-radius: var(--uui-border-radius);
-        width: 100%;
-        height: 100%;
-        box-sizing: border-box;
-        box-shadow: var(--uui-shadow-depth-3);
-        min-width: 200px;
-        color: black; /* Change to variable */
-      }
-    `,
-  ];
+			#dropdown {
+				background-color: white;
+				border-radius: var(--uui-border-radius);
+				width: 100%;
+				height: 100%;
+				box-sizing: border-box;
+				box-shadow: var(--uui-shadow-depth-3);
+				min-width: 200px;
+				color: black; /* Change to variable */
+			}
+		`,
+	];
 
+	@property()
+	public propertyEditorUIAlias = '';
 
-  @property()
-  public propertyEditorUIAlias = '';
+	// TODO: we need to investigate context api vs values props and events
+	@property()
+	public value?: string;
 
-  // TODO: we need to investigate context api vs values props and events
-  @property()
-  public value?: string;
+	@state()
+	private _actions: Array<UmbExtensionManifestPropertyAction> = [];
 
-  @state()
-  private _actions: Array<UmbExtensionManifestPropertyAction> = [];
+	@state()
+	private _open = false;
 
-  @state()
-  private _open = false;
-  
-  private _extensionRegistry?: UmbExtensionRegistry;
-  private _propertyActionsSubscription?: Subscription;
-  private _propertyActionMenuContext = new UmbPropertyActionMenuContext();
-  private _propertyActionMenuOpenSubscription?: Subscription;
-  
-  constructor () {
-    super();
-    
-    this._propertyActionMenuOpenSubscription = this._propertyActionMenuContext.isOpen.subscribe((value: boolean) => {
-      this._open = value;
-    });
+	private _extensionRegistry?: UmbExtensionRegistry;
+	private _propertyActionsSubscription?: Subscription;
+	private _propertyActionMenuContext = new UmbPropertyActionMenuContext();
+	private _propertyActionMenuOpenSubscription?: Subscription;
 
-    this.consumeContext('umbExtensionRegistry', (extensionRegistry: UmbExtensionRegistry) => {
-      this._extensionRegistry = extensionRegistry;
-      this._usePropertyActions();
-    });
+	constructor() {
+		super();
 
-    this.provideContext('umbPropertyActionMenu', this._propertyActionMenuContext);
-  }
+		this._propertyActionMenuOpenSubscription = this._propertyActionMenuContext.isOpen.subscribe((value: boolean) => {
+			this._open = value;
+		});
 
-  private _usePropertyActions () {
-    this._propertyActionsSubscription?.unsubscribe();
+		this.consumeContext('umbExtensionRegistry', (extensionRegistry: UmbExtensionRegistry) => {
+			this._extensionRegistry = extensionRegistry;
+			this._usePropertyActions();
+		});
 
-    this._extensionRegistry?.extensionsOfType('propertyAction')
-    .pipe(
-      map(propertyActions => propertyActions.filter(propertyAction => propertyAction.meta.propertyEditors.includes(this.propertyEditorUIAlias))))
-    .subscribe(extensions => {
-      this._actions = extensions;
-    });
-  }
+		this.provideContext('umbPropertyActionMenu', this._propertyActionMenuContext);
+	}
 
-  private _toggleMenu () {
-    //this._open = !this._open;
-    this._open ? this._propertyActionMenuContext.close() : this._propertyActionMenuContext.open();
-  }
+	private _usePropertyActions() {
+		this._propertyActionsSubscription?.unsubscribe();
 
-  private _handleClose (event: CustomEvent) {
-    this._open = false;
-    event.stopPropagation();
-  };
+		this._extensionRegistry
+			?.extensionsOfType('propertyAction')
+			.pipe(
+				map((propertyActions) =>
+					propertyActions.filter((propertyAction) =>
+						propertyAction.meta.propertyEditors.includes(this.propertyEditorUIAlias)
+					)
+				)
+			)
+			.subscribe((extensions) => {
+				this._actions = extensions;
+			});
+	}
 
-  disconnectedCallback () {
-    super.disconnectedCallback();
-    this._propertyActionMenuOpenSubscription?.unsubscribe();
-    this._propertyActionsSubscription?.unsubscribe();
-  }
-  
-  render () {
-    return html`
-      ${ this._actions?.length > 0 ? html`
-        <uui-popover
-          id="popover"
-          placement="bottom-start"
-          .open=${this._open}
-          @close="${this._handleClose}">
-          <uui-button
-            id="popover-trigger"
-            slot="trigger"
-            look="secondary"
-            label="More"
-            @click="${this._toggleMenu}"
-            compact>
-            <uui-symbol-more id="more-symbol"></uui-symbol-more>
-          </uui-button>
+	private _toggleMenu() {
+		//this._open = !this._open;
+		this._open ? this._propertyActionMenuContext.close() : this._propertyActionMenuContext.open();
+	}
 
-          <div slot="popover" id="dropdown">
-            ${this._actions.map(
-              action => html`
-                <umb-property-action .propertyAction=${action} .value="${this.value}"></umb-property-action>
-              `
-            )}
-          </div>
-        </uui-popover>
-      ` : '' }
-    `;
-  }
+	private _handleClose(event: CustomEvent) {
+		this._open = false;
+		event.stopPropagation();
+	}
+
+	disconnectedCallback() {
+		super.disconnectedCallback();
+		this._propertyActionMenuOpenSubscription?.unsubscribe();
+		this._propertyActionsSubscription?.unsubscribe();
+	}
+
+	render() {
+		return html`
+			${this._actions?.length > 0
+				? html`
+						<uui-popover id="popover" placement="bottom-start" .open=${this._open} @close="${this._handleClose}">
+							<uui-button
+								id="popover-trigger"
+								slot="trigger"
+								look="secondary"
+								label="More"
+								@click="${this._toggleMenu}"
+								compact>
+								<uui-symbol-more id="more-symbol"></uui-symbol-more>
+							</uui-button>
+
+							<div slot="popover" id="dropdown">
+								${this._actions.map(
+									(action) => html`
+										<umb-property-action .propertyAction=${action} .value="${this.value}"></umb-property-action>
+									`
+								)}
+							</div>
+						</uui-popover>
+				  `
+				: ''}
+		`;
+	}
 }
