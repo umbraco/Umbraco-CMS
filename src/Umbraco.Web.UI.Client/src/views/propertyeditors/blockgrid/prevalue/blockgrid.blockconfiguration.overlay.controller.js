ccbb34468417af03d9b1eb9a60179fede76a588f
Niels Lyngsø <niels.lyngso@gmail.com>
2022-08-11 15:15:30 +0200

    custom views for blocks

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/blockgrid/prevalue/blockgrid.blockconfiguration.overlay.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/blockgrid/prevalue/blockgrid.blockconfiguration.overlay.controller.js
@@ -26,7 +26,7 @@
         vm.rowMinMaxModel = {
             hideLabel: true,
             view: "numberrange",
-            value: {min: vm.block.rowMinSpan || 1, max: vm.block.rowMaxSpan || 1}
+            value: {min: vm.block.rowMinSpan || 1, max: vm.block.rowMaxSpan || null}
         }
 
         loadElementTypes();
