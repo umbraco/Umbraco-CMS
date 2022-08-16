e2f5c93528ac63fd4fed1efd8d880df05506db7d
Nikolaj <nikolajlauridsen@protonmail.ch>
2022-08-16 15:47:01 +0200

    Ensure that LoginTokens gets marked as dirty
    Might want to experiment with only removing/updating if the value is different

--- src/Umbraco.Infrastructure/Security/BackOfficeUserStore.cs
+++ src/Umbraco.Infrastructure/Security/BackOfficeUserStore.cs
@@ -440,14 +440,15 @@ public class BackOfficeUserStore : UmbracoUserStore<BackOfficeIdentityUser, Iden
 
         IIdentityUserToken? token = user.LoginTokens.FirstOrDefault(x =>
             x.LoginProvider.InvariantEquals(loginProvider) && x.Name.InvariantEquals(name));
-        if (token == null)
+
+        // We have to remove token and then re-add to ensure that LoginTokens are dirty, which is required for them to save
+        // This is because we're using an observable collection, which only cares about added/removed items.
+        if (token is not null)
         {
-            user.LoginTokens.Add(new IdentityUserToken(loginProvider, name, value, user.Id));
+            user.LoginTokens.Remove(token);
         }
-        else
-        {
-            token.Value = value;
-        }
+
+        user.LoginTokens.Add(new IdentityUserToken(loginProvider, name, value, user.Id));
 
         return Task.CompletedTask;
     }
