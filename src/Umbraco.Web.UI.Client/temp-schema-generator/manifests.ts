47de2e25d3558cf271be95c4c49b8280cd181ffe
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2022-08-25 09:34:54 +0200

    add schema for manifests

--- src/Umbraco.Web.UI.Client/temp-schema-generator/manifests.ts
+++ src/Umbraco.Web.UI.Client/temp-schema-generator/manifests.ts
@@ -11,15 +11,28 @@ export class Manifests {
 	default(@body body: ProblemDetails) {}
 }
 
-export type Manifest = IManifestElement | IManifestEntrypoint;
-export type ManifestStandardTypes = 'section' | 'propertyEditorUI' | 'dashboard';
+export type Manifest =
+	| IManifestSection
+	| IManifestPropertyEditorUI
+	| IManifestDashboard
+	| IManifestEditorView
+	| IManifestPropertyAction
+	| IManifestEntrypoint;
+export type ManifestStandardTypes =
+	| 'section'
+	| 'propertyEditorUI'
+	| 'dashboard'
+	| 'editorView'
+	| 'propertyAction'
+	| 'entrypoint';
 
 export interface ManifestsResponse {
 	manifests: Manifest[];
 }
 
 export interface IManifest {
-	type: string;
+	type: ManifestStandardTypes;
+	alias: string;
 }
 
 export interface MetaSection {
@@ -36,15 +49,49 @@ export interface MetaDashboard {
 	sections: string[];
 	pathname: string;
 	weight: number;
+	label: string;
+}
+
+export interface MetaEditorView {
+	editors: string[];
+	pathname: string;
+	weight: number;
+	icon: string;
+}
+
+export interface MetaPropertyAction {
+	propertyEditors: string[];
 }
 
 export interface IManifestElement extends IManifest {
-	type: ManifestStandardTypes;
-	alias: string;
 	name: string;
 	js: string;
 	elementName: string;
-	meta: MetaSection | MetaPropertyEditorUI | MetaDashboard;
+}
+
+export interface IManifestSection extends IManifestElement {
+	type: 'section';
+	meta: MetaSection;
+}
+
+export interface IManifestPropertyEditorUI extends IManifestElement {
+	type: 'propertyEditorUI';
+	meta: MetaPropertyEditorUI;
+}
+
+export interface IManifestDashboard extends IManifestElement {
+	type: 'dashboard';
+	meta: MetaDashboard;
+}
+
+export interface IManifestEditorView extends IManifestElement {
+	type: 'editorView';
+	meta: MetaEditorView;
+}
+
+export interface IManifestPropertyAction extends IManifestElement {
+	type: 'propertyAction';
+	meta: MetaPropertyAction;
 }
 
 export interface IManifestEntrypoint extends IManifest {
