db3cfe42f89d3a9b3c8bcaec4756694fe4206b6a
Nikolaj <nikolajlauridsen@protonmail.ch>
2022-08-29 14:50:26 +0200

    Add OpenApiTag attribute
    Otherwise the endpoints won't be grouped correctly

--- src/Umbraco.Cms.ManagementApi/Controllers/Server/ServerController.cs
+++ src/Umbraco.Cms.ManagementApi/Controllers/Server/ServerController.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using NSwag.Annotations;
 using Umbraco.New.Cms.Web.Common.Routing;
 
 namespace Umbraco.Cms.ManagementApi.Controllers.Server;
@@ -6,6 +7,7 @@ namespace Umbraco.Cms.ManagementApi.Controllers.Server;
 [ApiController]
 [ApiVersion("1.0")]
 [BackOfficeRoute("api/v{version:apiVersion}/server")]
+[OpenApiTag("Server")]
 public class ServerController : Controller
 {
 
