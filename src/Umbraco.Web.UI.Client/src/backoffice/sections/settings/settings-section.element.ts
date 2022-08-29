b2a90d8ebc7b615ece65e8fe75e839a803be7cd4
Mads Rasmussen <madsr@hey.com>
2022-08-29 13:57:19 +0200

    remove unused settings tree element

--- src/Umbraco.Web.UI.Client/src/backoffice/sections/settings/settings-section.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/sections/settings/settings-section.element.ts
@@ -1,7 +1,6 @@
 import { html, LitElement } from 'lit';
 import { customElement } from 'lit/decorators.js';
 
-import './settings-section-tree.element';
 @customElement('umb-section-settings')
 export class UmbSectionSettingsElement extends LitElement {
 	render() {
