acc7daa4bcddc84cfc140b7564b7aef6eea9e25e
Nikolaj <nikolajlauridsen@protonmail.ch>
2022-09-01 14:22:08 +0200

    Refactor install controller

---
+++ src/Umbraco.Cms.ManagementApi/Controllers/Install/InstallControllerBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSwag.Annotations;
+using Umbraco.New.Cms.Web.Common.Routing;
+
+namespace Umbraco.Cms.ManagementApi.Controllers.Install;
+
+[ApiController]
+[BackOfficeRoute("api/v{version:apiVersion}/install")]
+[OpenApiTag("Install")]
+public abstract class InstallControllerBase : Controller
+{
+}
