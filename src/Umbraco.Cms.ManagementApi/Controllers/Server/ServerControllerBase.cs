9cf7e965a962665f88e669badd77bb4b75c08889
Mole <nikolajlauridsen@protonmail.ch>
2022-09-01 14:01:49 +0200

    Backoffice Api: Server Controller (#12932)
    * Add initial implementation using 1 controller pr. action
    
    * Add OpenApiTag attribute
    
    Otherwise the endpoints won't be grouped correctly
    
    * Use correct response type
    
    * Move ApiVersion to endpoint controllers
    
    * Add ServerController suffix to endpoint controllers

---
+++ src/Umbraco.Cms.ManagementApi/Controllers/Server/ServerControllerBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSwag.Annotations;
+using Umbraco.New.Cms.Web.Common.Routing;
+
+namespace Umbraco.Cms.ManagementApi.Controllers.Server;
+
+[ApiController]
+[BackOfficeRoute("api/v{version:apiVersion}/server")]
+[OpenApiTag("Server")]
+public abstract class ServerControllerBase : Controller
+{
+
+}
