669f8ff5ba25aa933915ce7df993d36b3b9665fd
Mole <nikolajlauridsen@protonmail.ch>
2022-09-01 14:01:49 +0200

    Backoffice Api: Server Controller (#12932)
    * Add initial implementation using 1 controller pr. action
    
    * Add OpenApiTag attribute
    
    Otherwise the endpoints won't be grouped correctly
    
    * Use correct response type
    
    * Move ApiVersion to endpoint controllers
    
    * Add ServerController suffix to endpoint controllers
    
    (cherry picked from commit 9cf7e965a962665f88e669badd77bb4b75c08889)

---
+++ src/Umbraco.Cms.ManagementApi/ViewModels/Server/ServerStatusViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Text.Json.Serialization;
+using Umbraco.Cms.Core;
+
+namespace Umbraco.Cms.ManagementApi.ViewModels.Server;
+
+public class ServerStatusViewModel
+{
+    [JsonConverter(typeof(JsonStringEnumConverter))]
+    public RuntimeLevel ServerStatus { get; set; }
+}
