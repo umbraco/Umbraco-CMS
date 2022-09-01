798c5baa1a707288070e5d0fa2b81f516f8838ba
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-09-01 08:40:04 +0200

    Add LanguageViewModelMapping

---
+++ src/Umbraco.Cms.ManagementApi/Mapping/Languages/LanguageViewModelsMapDefinition.cs
@@ -0,0 +1,26 @@
+﻿using Umbraco.Cms.Core.Mapping;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.ManagementApi.ViewModels.Languages;
+
+namespace Umbraco.Cms.ManagementApi.Mapping.Languages;
+
+public class LanguageViewModelsMapDefinition : IMapDefinition
+{
+    public void DefineMaps(IUmbracoMapper mapper) => mapper.Define<LanguageViewModel, Language>((source, context) => new Language(string.Empty, string.Empty), Map);
+
+
+    // Umbraco.Code.MapAll
+    private static void Map(LanguageViewModel source, Language target, MapperContext context)
+    {
+        target.CreateDate = default;
+        target.CultureName = source.Name!;
+        target.DeleteDate = null;
+        target.FallbackLanguageId = source.FallbackLanguageId;
+        target.Id = source.Id;
+        target.IsDefault = source.IsDefault;
+        target.IsMandatory = source.IsMandatory;
+        target.IsoCode = source.IsoCode;
+        target.Key = default;
+        target.UpdateDate = default;
+    }
+}
--- src/Umbraco.Core/Models/Mapping/LanguageMapDefinition.cs
+++ src/Umbraco.Core/Models/Mapping/LanguageMapDefinition.cs
@@ -9,7 +9,6 @@ public class LanguageMapDefinition : IMapDefinition
     {
         mapper.Define<ILanguage, EntityBasic>((source, context) => new EntityBasic(), Map);
         mapper.Define<ILanguage, ContentEditing.Language>((source, context) => new ContentEditing.Language(), Map);
-        mapper.Define<ContentEditing.Language, Language>((source, context) => new Language(string.Empty, string.Empty), Map);
         mapper.Define<IEnumerable<ILanguage>, IEnumerable<ContentEditing.Language>>((source, context) => new List<ContentEditing.Language>(), Map);
     }
 
@@ -32,21 +31,6 @@ public class LanguageMapDefinition : IMapDefinition
         target.IsDefault = source.IsDefault;
         target.IsMandatory = source.IsMandatory;
         target.FallbackLanguageId = source.FallbackLanguageId;
-    }
-
-    // Umbraco.Code.MapAll
-    private static void Map(ContentEditing.Language source, Language target, MapperContext context)
-    {
-        target.CreateDate = default;
-        target.CultureName = source.Name!;
-        target.DeleteDate = null;
-        target.FallbackLanguageId = source.FallbackLanguageId;
-        target.Id = source.Id;
-        target.IsDefault = source.IsDefault;
-        target.IsMandatory = source.IsMandatory;
-        target.IsoCode = source.IsoCode;
-        target.Key = default;
-        target.UpdateDate = default;
     }
 
     private static void Map(IEnumerable<ILanguage> source, IEnumerable<ContentEditing.Language> target, MapperContext context)
