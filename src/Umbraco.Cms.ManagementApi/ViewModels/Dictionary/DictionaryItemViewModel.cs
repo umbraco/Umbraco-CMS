f13007a36575e994f5a0024f32e32afb3593df9f
Zeegaan <nge@umbraco.dk>
2022-09-02 12:08:00 +0200

    Apply DataContract/Datamember to view model

--- src/Umbraco.Cms.ManagementApi/ViewModels/Dictionary/DictionaryItemViewModel.cs
+++ src/Umbraco.Cms.ManagementApi/ViewModels/Dictionary/DictionaryItemViewModel.cs
@@ -1,7 +1,13 @@
-﻿namespace Umbraco.Cms.ManagementApi.ViewModels.Dictionary;
+﻿using System.Runtime.Serialization;
 
+namespace Umbraco.Cms.ManagementApi.ViewModels.Dictionary;
+
+[DataContract]
 public class DictionaryItemViewModel
 {
+    [DataMember(Name = "parentId")]
     public int ParentId { get; set; }
+
+    [DataMember(Name = "key")]
     public string Key { get; set; } = null!;
 }
