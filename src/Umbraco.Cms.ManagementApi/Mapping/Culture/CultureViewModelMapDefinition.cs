c54b1ea966c6ac0491964717a810ecae1e0e0e30
Zeegaan <nge@umbraco.dk>
2022-09-02 09:47:25 +0200

    Map CultureInfos to paged CultureViewModel

---
+++ src/Umbraco.Cms.ManagementApi/Mapping/Culture/CultureViewModelMapDefinition.cs
@@ -0,0 +1,22 @@
+﻿using System.Globalization;
+using Umbraco.Cms.Core.Mapping;
+using Umbraco.Cms.ManagementApi.ViewModels.Culture;
+using Umbraco.Cms.ManagementApi.ViewModels.Pagination;
+
+namespace Umbraco.Cms.ManagementApi.Mapping.Culture;
+
+public class CultureViewModelMapDefinition : IMapDefinition
+{
+    /// <inheritdoc/>
+    public void DefineMaps(IUmbracoMapper mapper) =>
+        mapper.Define<IEnumerable<CultureInfo>, PagedViewModel<CultureViewModel>>(
+            (source, context) => new PagedViewModel<CultureViewModel>(), Map);
+
+    // Umbraco.Code.MapAll
+    private static void Map(IEnumerable<CultureInfo> source, PagedViewModel<CultureViewModel> target, MapperContext context)
+    {
+        CultureInfo[] cultureInfos = source.ToArray();
+        target.Items = cultureInfos.Select(culture => new CultureViewModel { EnglishName = culture.EnglishName, Name = culture.Name });
+        target.Total = cultureInfos.Length;
+    }
+}
