d6991a2f25ac90c12225084118d7447ca7eeaf61
Zeegaan <nge@umbraco.dk>
2022-09-02 10:18:59 +0200

    Add delete controller

---
+++ src/Umbraco.Cms.ManagementApi/Controllers/Dictionary/DeleteDictionaryController.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Security;
+using Umbraco.Cms.Core.Services;
+
+namespace Umbraco.Cms.ManagementApi.Controllers.Dictionary;
+
+public class DeleteDictionaryController : DictionaryControllerBase
+{
+    private readonly ILocalizationService _localizationService;
+    private readonly IBackOfficeSecurityAccessor _backOfficeSecurityAccessor;
+
+    public DeleteDictionaryController(ILocalizationService localizationService, IBackOfficeSecurityAccessor backOfficeSecurityAccessor)
+    {
+        _localizationService = localizationService;
+        _backOfficeSecurityAccessor = backOfficeSecurityAccessor;
+    }
+    /// <summary>
+    ///     Deletes a data type with a given ID
+    /// </summary>
+    /// <param name="id"></param>
+    /// <returns>
+    ///     <see cref="HttpResponseMessage" />
+    /// </returns>
+    [HttpDelete]
+    public IActionResult Delete(int id)
+    {
+        IDictionaryItem? foundDictionary = _localizationService.GetDictionaryItemById(id);
+
+        if (foundDictionary == null)
+        {
+            return NotFound();
+        }
+
+        IEnumerable<IDictionaryItem> foundDictionaryDescendants =
+            _localizationService.GetDictionaryItemDescendants(foundDictionary.Key);
+
+        foreach (IDictionaryItem dictionaryItem in foundDictionaryDescendants)
+        {
+            _localizationService.Delete(dictionaryItem, _backOfficeSecurityAccessor.BackOfficeSecurity?.CurrentUser?.Id ?? -1);
+        }
+
+        _localizationService.Delete(foundDictionary, _backOfficeSecurityAccessor.BackOfficeSecurity?.CurrentUser?.Id ?? -1);
+
+        return Ok();
+    }
+}
