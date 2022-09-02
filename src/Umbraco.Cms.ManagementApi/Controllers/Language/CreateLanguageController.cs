07f57b2978041541a581929f1eddc4fd16425ebe
Zeegaan <nge@umbraco.dk>
2022-09-02 10:33:20 +0200

    Split Save action into Create & Update actions

---
+++ src/Umbraco.Cms.ManagementApi/Controllers/Language/CreateLanguageController.cs
@@ -0,0 +1,62 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Mapping;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.ManagementApi.ViewModels.Languages;
+using Umbraco.New.Cms.Core.Services.Installer;
+
+namespace Umbraco.Cms.ManagementApi.Controllers.Language;
+
+[ApiVersion("1.0")]
+public class CreateLanguageController : LanguageControllerBase
+{
+    private readonly ILanguageService _languageService;
+    private readonly IUmbracoMapper _umbracoMapper;
+    private readonly ILocalizationService _localizationService;
+
+    public CreateLanguageController(ILanguageService languageService, IUmbracoMapper umbracoMapper, ILocalizationService localizationService)
+    {
+        _languageService = languageService;
+        _umbracoMapper = umbracoMapper;
+        _localizationService = localizationService;
+    }
+
+    /// <summary>
+    ///     Creates or saves a language
+    /// </summary>
+    [HttpPost("save")]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    // TODO: This needs to be an authorized endpoint.
+    public async Task<ActionResult<LanguageViewModel?>> Create(LanguageViewModel language)
+    {
+        if (_languageService.LanguageAlreadyExists(language.Id, language.IsoCode))
+        {
+            // Someone is trying to create a language that already exist
+            ModelState.AddModelError("IsoCode", "The language " + language.IsoCode + " already exists");
+            return ValidationProblem(ModelState);
+        }
+
+        // Creating a new lang...
+        CultureInfo culture;
+        try
+        {
+            culture = CultureInfo.GetCultureInfo(language.IsoCode!);
+        }
+        catch (CultureNotFoundException)
+        {
+            ModelState.AddModelError("IsoCode", "No Culture found with name " + language.IsoCode);
+            return ValidationProblem(ModelState);
+        }
+
+        language.Name ??= culture.EnglishName;
+
+        ILanguage? newLang = _umbracoMapper.Map<ILanguage>(language);
+
+        _localizationService.Save(newLang!);
+        return _umbracoMapper.Map<LanguageViewModel>(newLang);
+    }
+}
--- src/Umbraco.Cms.ManagementApi/Controllers/Language/UpdateLanguageController.cs
+++ src/Umbraco.Cms.ManagementApi/Controllers/Language/UpdateLanguageController.cs
@@ -10,13 +10,13 @@ using Umbraco.New.Cms.Core.Services.Installer;
 namespace Umbraco.Cms.ManagementApi.Controllers.Language;
 
 [ApiVersion("1.0")]
-public class SaveLanguageController : LanguageControllerBase
+public class UpdateLanguageController : LanguageControllerBase
 {
     private readonly ILanguageService _languageService;
     private readonly IUmbracoMapper _umbracoMapper;
     private readonly ILocalizationService _localizationService;
 
-    public SaveLanguageController(ILanguageService languageService, IUmbracoMapper umbracoMapper, ILocalizationService localizationService)
+    public UpdateLanguageController(ILanguageService languageService, IUmbracoMapper umbracoMapper, ILocalizationService localizationService)
     {
         _languageService = languageService;
         _umbracoMapper = umbracoMapper;
@@ -26,41 +26,19 @@ public class SaveLanguageController : LanguageControllerBase
     /// <summary>
     ///     Creates or saves a language
     /// </summary>
-    [HttpPost("save")]
+    [HttpPost("update")]
     [MapToApiVersion("1.0")]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status200OK)]
     // TODO: This needs to be an authorized endpoint.
-    public async Task<ActionResult<LanguageViewModel?>> Save(LanguageViewModel language)
+    public async Task<ActionResult<LanguageViewModel?>> Update(LanguageViewModel language)
     {
-        if (_languageService.LanguageAlreadyExists(language.Id, language.IsoCode))
-        {
-            // Someone is trying to create a language that already exist
-            ModelState.AddModelError("IsoCode", "The language " + language.IsoCode + " already exists");
-            return ValidationProblem(ModelState);
-        }
-
         ILanguage? existingById = language.Id != default ? _localizationService.GetLanguageById(language.Id) : null;
         if (existingById is null)
         {
-            // Creating a new lang...
-            CultureInfo culture;
-            try
-            {
-                culture = CultureInfo.GetCultureInfo(language.IsoCode!);
-            }
-            catch (CultureNotFoundException)
-            {
-                ModelState.AddModelError("IsoCode", "No Culture found with name " + language.IsoCode);
-                return ValidationProblem(ModelState);
-            }
-
-            language.Name ??= culture.EnglishName;
-
-            ILanguage? newLang = _umbracoMapper.Map<ILanguage>(language);
-
-            _localizationService.Save(newLang!);
-            return _umbracoMapper.Map<LanguageViewModel>(newLang);
+            // Someone is trying to update a language that doesn't exist
+            ModelState.AddModelError("IsoCode", "The language " + language.IsoCode + " does not exist");
+            return ValidationProblem(ModelState);
         }
 
         // note that the service will prevent the default language from being "un-defaulted"
