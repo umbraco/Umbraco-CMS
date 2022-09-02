434635be0b3c023e928dfeafcdd9d0483207f651
Zeegaan <nge@umbraco.dk>
2022-09-02 09:30:59 +0200

    Add culture controllers

---
+++ src/Umbraco.Cms.ManagementApi/Controllers/Culture/GetAllCultureController.cs
@@ -0,0 +1,19 @@
+﻿using System.Globalization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Umbraco.Cms.ManagementApi.Controllers.Culture;
+
+public class GetAllCultureController
+{
+    /// <summary>
+    ///     Returns all cultures available for creating languages.
+    /// </summary>
+    /// <returns></returns>
+    [HttpGet]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(typeof(IDictionary<string, string>), StatusCodes.Status200OK)]
+    public async Task<IDictionary<string, string>> GetAll()
+        => CultureInfo.GetCultures(CultureTypes.AllCultures).DistinctBy(x => x.Name).OrderBy(x => x.EnglishName)
+            .ToDictionary(x => x.Name, x => x.EnglishName);
+}
