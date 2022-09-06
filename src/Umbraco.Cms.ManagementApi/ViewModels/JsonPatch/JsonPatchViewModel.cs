109ef943a2b436bcf6a0b78e16c77ed5ac3170ac
Zeegaan <nge@umbraco.dk>
2022-09-06 11:08:09 +0200

    Make model non-nullable

--- src/Umbraco.Cms.ManagementApi/ViewModels/JsonPatch/JsonPatchViewModel.cs
+++ src/Umbraco.Cms.ManagementApi/ViewModels/JsonPatch/JsonPatchViewModel.cs
@@ -4,12 +4,11 @@ namespace Umbraco.Cms.ManagementApi.ViewModels.JsonPatch;
 
 public class JsonPatchViewModel
 {
-    [JsonPropertyName("op")]
-    public string? Op { get; set; }
+    [JsonPropertyName("op")] public string Op { get; set; } = null!;
 
     [JsonPropertyName("path")]
-    public string? Path { get; set; }
+    public string Path { get; set; } = null!;
 
     [JsonPropertyName("value")]
-    public object? Value { get; set; }
+    public object Value { get; set; } = null!;
 }
