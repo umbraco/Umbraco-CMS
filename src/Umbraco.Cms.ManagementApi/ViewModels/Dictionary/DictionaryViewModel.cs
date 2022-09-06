5d1b94f0e605866e786ed3b831a1ccb400824c77
Zeegaan <nge@umbraco.dk>
2022-09-06 10:30:35 +0200

    Create DictionaryViewModels and do not use IEntity

--- src/Umbraco.Cms.ManagementApi/ViewModels/Dictionary/DictionaryViewModel.cs
+++ src/Umbraco.Cms.ManagementApi/ViewModels/Dictionary/DictionaryViewModel.cs
@@ -1,13 +1,14 @@
-﻿using System.Runtime.Serialization;
+﻿using System.ComponentModel.DataAnnotations;
 using Umbraco.Cms.Core.Models.ContentEditing;
+using Umbraco.Cms.Core.Models.Validation;
+using System.Text.Json.Serialization;
 
 namespace Umbraco.Cms.ManagementApi.ViewModels.Dictionary;
 
 /// <summary>
 ///     The dictionary display model
 /// </summary>
-[DataContract(Name = "dictionary", Namespace = "")]
-public class DictionaryViewModel : EntityBasic, INotificationModel
+public class DictionaryViewModel : INotificationModel
 {
     /// <summary>
     ///     Initializes a new instance of the <see cref="DictionaryDisplay" /> class.
@@ -15,50 +16,67 @@ public class DictionaryViewModel : EntityBasic, INotificationModel
     public DictionaryViewModel()
     {
         Notifications = new List<BackOfficeNotification>();
-        Translations = new List<DictionaryTranslationDisplay>();
+        Translations = new List<DictionaryTranslationViewModel>();
         ContentApps = new List<ContentApp>();
     }
 
     /// <summary>
     ///     Gets or sets the parent id.
     /// </summary>
-    [DataMember(Name = "parentId")]
-    public new Guid? ParentId { get; set; }
+    [JsonPropertyName("parentId")]
+    public Guid? ParentId { get; set; }
 
     /// <summary>
-    ///     Gets the translations.
+    ///     Gets or sets the translations.
     /// </summary>
-    [DataMember(Name = "translations")]
-    public List<DictionaryTranslationDisplay> Translations { get; private set; }
+    [JsonPropertyName("translations")]
+    public IEnumerable<DictionaryTranslationViewModel> Translations { get; set; }
 
     /// <summary>
     ///     Apps for the dictionary item
     /// </summary>
-    [DataMember(Name = "apps")]
-    public List<ContentApp> ContentApps { get; private set; }
+    [JsonPropertyName("apps")]
+    public IEnumerable<ContentApp> ContentApps { get; set; }
 
     /// <inheritdoc />
     /// <summary>
     ///     This is used to add custom localized messages/strings to the response for the app to use for localized UI purposes.
     /// </summary>
-    [DataMember(Name = "notifications")]
+    [JsonPropertyName("notifications")]
     public List<BackOfficeNotification> Notifications { get; private set; }
 
     /// <summary>
     ///     Gets or sets a value indicating whether name is dirty.
     /// </summary>
-    [DataMember(Name = "nameIsDirty")]
+    [JsonPropertyName("nameIsDirty")]
     public bool NameIsDirty { get; set; }
 
     /// <summary>
     ///     Gets or sets a value indicating when the object was created.
     /// </summary>
-    [DataMember(Name = "createDate")]
+    [JsonPropertyName("createDate")]
     public DateTime CreateDate { get; set; }
 
     /// <summary>
     ///     Gets or sets a value indicating when the object was updated.
     /// </summary>
-    [DataMember(Name = "updateDate")]
+    [JsonPropertyName("updateDate")]
     public DateTime UpdateDate { get; set; }
+
+    [JsonPropertyName("name")]
+    [RequiredForPersistence(AllowEmptyStrings = false, ErrorMessage = "Required")]
+    [Required]
+    public string Name { get; set; } = null!;
+
+    /// <summary>
+    ///     Gets or sets the Key for the object
+    /// </summary>
+    [JsonPropertyName("key")]
+    public Guid Key { get; set; }
+
+    /// <summary>
+    ///     The path of the entity
+    /// </summary>
+    [JsonPropertyName("path")]
+    public string Path { get; set; } = string.Empty;
 }
