5b939b1aae8b7350cfb9f5e36e7d25a98645dd04
Zeegaan <nge@umbraco.dk>
2022-09-06 11:05:59 +0200

    Implement JsonPatchService.cs

--- src/Umbraco.Cms.ManagementApi/Controllers/Dictionary/UpdateDictionaryController.cs
+++ src/Umbraco.Cms.ManagementApi/Controllers/Dictionary/UpdateDictionaryController.cs
@@ -7,6 +7,7 @@ using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Core.Mapping;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.ManagementApi.Services;
 using Umbraco.Cms.ManagementApi.ViewModels.Dictionary;
 using Umbraco.Cms.ManagementApi.ViewModels.JsonPatch;
 using Umbraco.New.Cms.Core.Factories;
@@ -20,17 +21,20 @@ public class UpdateDictionaryController : DictionaryControllerBase
     private readonly IUmbracoMapper _umbracoMapper;
     private readonly IDictionaryService _dictionaryService;
     private readonly IDictionaryFactory _dictionaryFactory;
+    private readonly IJsonPatchService _jsonPatchService;
 
     public UpdateDictionaryController(
         ILocalizationService localizationService,
         IUmbracoMapper umbracoMapper,
         IDictionaryService dictionaryService,
-        IDictionaryFactory dictionaryFactory)
+        IDictionaryFactory dictionaryFactory,
+        IJsonPatchService jsonPatchService)
     {
         _localizationService = localizationService;
         _umbracoMapper = umbracoMapper;
         _dictionaryService = dictionaryService;
         _dictionaryFactory = dictionaryFactory;
+        _jsonPatchService = jsonPatchService;
     }
 
     [HttpPatch("update/{id:Guid}")]
@@ -38,12 +42,15 @@ public class UpdateDictionaryController : DictionaryControllerBase
     {
         IDictionaryItem? dictionaryItem = _localizationService.GetDictionaryItemById(id);
         DictionaryViewModel dictionaryToPatch = _umbracoMapper.Map<DictionaryViewModel>(dictionaryItem)!;
-        var patchString = JsonSerializer.Serialize(updateViewModel);
-        var docString = JsonSerializer.Serialize(dictionaryToPatch);
-        JsonPatch? patch = JsonSerializer.Deserialize<JsonPatch>(patchString);
-        var doc = JsonNode.Parse(docString);
-        PatchResult? result = patch?.Apply(doc);
+
+        PatchResult? result = _jsonPatchService.Patch(updateViewModel, dictionaryToPatch);
+
         DictionaryViewModel? updatedDictionaryItem = JsonSerializer.Deserialize<DictionaryViewModel>(result?.Result);
+        if (updatedDictionaryItem is null)
+        {
+            throw new JsonException("Could not serialize JsonNode to DictionaryViewModel");
+        }
+
         IDictionaryItem dictionaryToSave = _dictionaryFactory.CreateDictionary(updatedDictionaryItem!);
         _localizationService.Save(dictionaryToSave);
         return Content(_dictionaryService.CalculatePath(dictionaryToSave.ParentId, dictionaryToSave.Id), MediaTypeNames.Text.Plain, Encoding.UTF8);
