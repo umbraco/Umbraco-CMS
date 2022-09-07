faee4d488d7020543afabe668f261c96f7f808fa
Zeegaan <nge@umbraco.dk>
2022-09-07 12:38:15 +0200

    Use mapping to viewmodel

--- src/Umbraco.Cms.ManagementApi/Controllers/Relation/GetRelationController.cs
+++ src/Umbraco.Cms.ManagementApi/Controllers/Relation/GetRelationController.cs
@@ -1,8 +1,9 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Core.Mapping;
 using Umbraco.Cms.Core.Models;
-using Umbraco.Cms.Core.Models.ContentEditing;
 using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.ManagementApi.ViewModels.Relation;
 
 namespace Umbraco.Cms.ManagementApi.Controllers.Relation;
 
@@ -18,6 +19,9 @@ public class GetRelationController : RelationControllerBase
         _relationService = relationService;
     }
 
-    public RelationDisplay? GetById(int id) =>
-        _umbracoMapper.Map<IRelation, RelationDisplay>(_relationService.GetById(id));
+    [HttpGet("{id:int}")]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(typeof(RelationViewModel), StatusCodes.Status200OK)]
+    public RelationViewModel Get(int id) =>
+        _umbracoMapper.Map<IRelation, RelationViewModel>(_relationService.GetById(id))!;
 }
