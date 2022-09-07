75c46f8bc82eb263ea546925a76fedd002734755
Zeegaan <nge@umbraco.dk>
2022-09-07 12:49:53 +0200

    Add GetByChildRelationController

---
+++ src/Umbraco.Cms.ManagementApi/Controllers/Relation/GetByChildRelationController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Mapping;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Models.ContentEditing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.ManagementApi.ViewModels.Relation;
+using Umbraco.Extensions;
+
+namespace Umbraco.Cms.ManagementApi.Controllers.Relation;
+
+public class GetByChildRelationController : RelationControllerBase
+{
+    private readonly IRelationService _relationService;
+    private readonly IUmbracoMapper _umbracoMapper;
+
+    public GetByChildRelationController(IRelationService relationService, IUmbracoMapper umbracoMapper)
+    {
+        _relationService = relationService;
+        _umbracoMapper = umbracoMapper;
+    }
+
+    [HttpGet("/getByChild/{childId:int}")]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(typeof(RelationViewModel), StatusCodes.Status200OK)]
+    public IEnumerable<RelationViewModel> GetByChild(int childId, string relationTypeAlias = "")
+    {
+        IRelation[] relations = _relationService.GetByChildId(childId).ToArray();
+
+        if (relations.Any() == false)
+        {
+            return Enumerable.Empty<RelationViewModel>();
+        }
+
+        if (string.IsNullOrWhiteSpace(relationTypeAlias) == false)
+        {
+            return _umbracoMapper.MapEnumerable<IRelation, RelationViewModel>(relations.Where(x => x.RelationType.Alias.InvariantEquals(relationTypeAlias))).WhereNotNull();
+        }
+
+        return _umbracoMapper.MapEnumerable<IRelation, RelationViewModel>(relations).WhereNotNull();
+    }
+}
