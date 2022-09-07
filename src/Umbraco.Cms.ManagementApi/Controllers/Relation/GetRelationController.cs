537891e33a3c16332adb2326de20acf8782617b5
Zeegaan <nge@umbraco.dk>
2022-09-07 12:26:38 +0200

    Add GetRelationController

---
+++ src/Umbraco.Cms.ManagementApi/Controllers/Relation/GetRelationController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Mapping;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Models.ContentEditing;
+using Umbraco.Cms.Core.Services;
+
+namespace Umbraco.Cms.ManagementApi.Controllers.Relation;
+
+[ApiVersion("1.0")]
+public class GetRelationController : RelationControllerBase
+{
+    private readonly IUmbracoMapper _umbracoMapper;
+    private readonly IRelationService _relationService;
+
+    public GetRelationController(IUmbracoMapper umbracoMapper, IRelationService relationService)
+    {
+        _umbracoMapper = umbracoMapper;
+        _relationService = relationService;
+    }
+
+    public RelationDisplay? GetById(int id) =>
+        _umbracoMapper.Map<IRelation, RelationDisplay>(_relationService.GetById(id));
+}
