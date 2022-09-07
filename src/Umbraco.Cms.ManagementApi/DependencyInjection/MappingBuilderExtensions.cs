99a4d762a2553a12cb3f908af106fbf0bb201231
Zeegaan <nge@umbraco.dk>
2022-09-07 13:04:48 +0200

    Add relationViewModelsMapDefinition to DI

--- src/Umbraco.Cms.ManagementApi/DependencyInjection/MappingBuilderExtensions.cs
+++ src/Umbraco.Cms.ManagementApi/DependencyInjection/MappingBuilderExtensions.cs
@@ -1,6 +1,7 @@
 ﻿using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Cms.Core.Mapping;
 using Umbraco.Cms.ManagementApi.Mapping.Installer;
+using Umbraco.Cms.ManagementApi.Mapping.Relation;
 
 namespace Umbraco.Cms.ManagementApi.DependencyInjection;
 
@@ -9,6 +10,7 @@ public static class MappingBuilderExtensions
     internal static IUmbracoBuilder AddMappers(this IUmbracoBuilder builder)
     {
         builder.WithCollectionBuilder<MapDefinitionCollectionBuilder>()
+            .Add<RelationViewModelsMapDefinition>()
             .Add<InstallerViewModelsMapDefinition>();
 
         return builder;
