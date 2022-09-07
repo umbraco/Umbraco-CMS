1b1606f7a09787dabc98cdacef6f66ad68384ff8
Zeegaan <nge@umbraco.dk>
2022-09-07 08:41:39 +0200

    Add GetAnalyticsController

---
+++ src/Umbraco.Cms.ManagementApi/Controllers/Analytics/GetAnalyticsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.ManagementApi.ViewModels.Server;
+
+namespace Umbraco.Cms.ManagementApi.Controllers.Analytics;
+
+[ApiVersion("1.0")]
+public class GetAnalyticsController : AnalyticsControllerBase
+{
+    private readonly IMetricsConsentService _metricsConsentService;
+
+    public GetAnalyticsController(IMetricsConsentService metricsConsentService) => _metricsConsentService = metricsConsentService;
+
+    [HttpGet]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(typeof(ServerStatusViewModel), StatusCodes.Status200OK)]
+    public TelemetryLevel Get() => _metricsConsentService.GetConsentLevel();
+}
