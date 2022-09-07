143eda1f57b660dd33c35ee2fc84a6cfb04a806e
Zeegaan <nge@umbraco.dk>
2022-09-07 11:37:57 +0200

    Add relation controller base

---
+++ src/Umbraco.Cms.ManagementApi/Controllers/Relation/RelationControllerBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSwag.Annotations;
+using Umbraco.New.Cms.Web.Common.Routing;
+
+namespace Umbraco.Cms.ManagementApi.Controllers.Relation;
+
+[ApiController]
+[BackOfficeRoute("api/v{version:apiVersion}/relation")]
+[OpenApiTag("Relation")]
+public abstract class RelationControllerBase : Controller
+{
+
+}
