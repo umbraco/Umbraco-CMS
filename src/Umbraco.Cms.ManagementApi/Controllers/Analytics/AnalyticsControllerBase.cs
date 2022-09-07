779c59c211fc0370584fe2111380be4734d18e04
Zeegaan <nge@umbraco.dk>
2022-09-07 08:06:22 +0200

    Add AnalyticsControllerBase

---
+++ src/Umbraco.Cms.ManagementApi/Controllers/Analytics/AnalyticsControllerBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using NSwag.Annotations;
+using Umbraco.New.Cms.Web.Common.Routing;
+
+namespace Umbraco.Cms.ManagementApi.Controllers.Analytics;
+
+[ApiController]
+[BackOfficeRoute("api/v{version:apiVersion}/analytics")]
+[OpenApiTag("Analytics")]
+public class AnalyticsControllerBase : Controller
+{
+}
