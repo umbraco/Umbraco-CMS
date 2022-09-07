abb516f31bfab1862bd6a1a62643c5a5b899e851
Zeegaan <nge@umbraco.dk>
2022-09-07 09:09:27 +0200

    Add GetAllAnalyticsController

---
+++ src/Umbraco.Cms.ManagementApi/Controllers/Analytics/GetAllAnalyticsController.cs
@@ -0,0 +1,17 @@
+﻿using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.ManagementApi.ViewModels.Pagination;
+
+namespace Umbraco.Cms.ManagementApi.Controllers.Analytics;
+
+public class GetAllAnalyticsController : AnalyticsControllerBase
+{
+    public PagedViewModel<TelemetryLevel> GetAll(int skip, int take)
+    {
+        TelemetryLevel[] levels = { TelemetryLevel.Minimal, TelemetryLevel.Basic, TelemetryLevel.Detailed };
+        return new PagedViewModel<TelemetryLevel>
+        {
+            Total = levels.Length,
+            Items = levels.Skip(skip).Take(take),
+        };
+    }
+}
