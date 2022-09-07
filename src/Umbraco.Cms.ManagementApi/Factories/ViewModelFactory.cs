d39b0367206edd20c61cab55e10c20800bee7631
Zeegaan <nge@umbraco.dk>
2022-09-07 09:14:09 +0200

    Add viewmodel factory

---
+++ src/Umbraco.Cms.ManagementApi/Factories/ViewModelFactory.cs
@@ -0,0 +1,12 @@
+﻿using Umbraco.Cms.ManagementApi.ViewModels.Pagination;
+
+namespace Umbraco.Cms.ManagementApi.Factories;
+
+public class ViewModelFactory : IViewModelFactory
+{
+    public PagedViewModel<T> Create<T>(IEnumerable<T> items, int skip, int take)
+    {
+        T[] enumerable = items as T[] ?? items.ToArray();
+        return new PagedViewModel<T> { Total = enumerable.Length, Items = enumerable.Skip(skip).Take(take) };
+    }
+}
