39fe44c5fdecd2689925f9d43a26570c32771cc7
Zeegaan <nge@umbraco.dk>
2022-09-07 09:30:47 +0200

    Use factory for creation of ViewModel

--- src/Umbraco.Cms.ManagementApi/Controllers/Analytics/GetAllAnalyticsController.cs
+++ src/Umbraco.Cms.ManagementApi/Controllers/Analytics/GetAllAnalyticsController.cs
@@ -1,17 +1,18 @@
 ﻿using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.ManagementApi.Factories;
 using Umbraco.Cms.ManagementApi.ViewModels.Pagination;
 
 namespace Umbraco.Cms.ManagementApi.Controllers.Analytics;
 
 public class GetAllAnalyticsController : AnalyticsControllerBase
 {
+    private readonly IViewModelFactory _viewModelFactory;
+
+    public GetAllAnalyticsController(IViewModelFactory viewModelFactory) => _viewModelFactory = viewModelFactory;
+
     public PagedViewModel<TelemetryLevel> GetAll(int skip, int take)
     {
         TelemetryLevel[] levels = { TelemetryLevel.Minimal, TelemetryLevel.Basic, TelemetryLevel.Detailed };
-        return new PagedViewModel<TelemetryLevel>
-        {
-            Total = levels.Length,
-            Items = levels.Skip(skip).Take(take),
-        };
+        return _viewModelFactory.Create(levels, skip, take);
     }
 }
