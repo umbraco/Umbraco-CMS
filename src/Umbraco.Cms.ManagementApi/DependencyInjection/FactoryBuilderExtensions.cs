bb6956bb81115a797401136565a8ba9b79242ae8
Zeegaan <nge@umbraco.dk>
2022-09-07 09:28:03 +0200

    Register factory for DI

---
+++ src/Umbraco.Cms.ManagementApi/DependencyInjection/FactoryBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.ManagementApi.Factories;
+
+namespace Umbraco.Cms.ManagementApi.DependencyInjection;
+
+public static class FactoryBuilderExtensions
+{
+    internal static IUmbracoBuilder AddFactories(this IUmbracoBuilder builder)
+    {
+        builder.Services.AddTransient<IViewModelFactory, ViewModelFactory>();
+        return builder;
+    }
+
+}
