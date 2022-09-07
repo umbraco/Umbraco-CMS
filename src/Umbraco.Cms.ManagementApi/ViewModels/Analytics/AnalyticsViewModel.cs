053f6814026fd181c75c66da845577f066189c5a
Zeegaan <nge@umbraco.dk>
2022-09-07 08:48:25 +0200

    Update ViewModel to use System.Text.Json

--- src/Umbraco.Cms.ManagementApi/ViewModels/Analytics/AnalyticsViewModel.cs
+++ src/Umbraco.Cms.ManagementApi/ViewModels/Analytics/AnalyticsViewModel.cs
@@ -1,6 +1,10 @@
-﻿namespace Umbraco.Cms.ManagementApi.ViewModels.Analytics;
+﻿using System.Text.Json.Serialization;
+using Umbraco.Cms.Core.Models;
+
+namespace Umbraco.Cms.ManagementApi.ViewModels.Analytics;
 
 public class AnalyticsViewModel
 {
-
+    [JsonPropertyName("telemetryLevel")]
+    public TelemetryLevel TelemetryLevel { get; set; }
 }
