9cc97616ca9205166f62592f712e2475fc10826d
Zeegaan <nge@umbraco.dk>
2022-09-07 10:41:55 +0200

    Add GetHelpController

---
+++ src/Umbraco.Cms.ManagementApi/Controllers/Help/GetHelpController.cs
@@ -0,0 +1,76 @@
+﻿using System.Net;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Newtonsoft.Json;
+using Umbraco.Cms.Core.Configuration.Models;
+using Umbraco.Cms.ManagementApi.ViewModels.Help;
+using Umbraco.Cms.ManagementApi.ViewModels.Pagination;
+
+namespace Umbraco.Cms.ManagementApi.Controllers.Help;
+
+public class GetHelpController : HelpControllerBase
+{
+    private static HttpClient? _httpClient;
+    private readonly ILogger<GetHelpController> _logger;
+    private HelpPageSettings _helpPageSettings;
+
+    public GetHelpController(IOptionsMonitor<HelpPageSettings> helpPageSettings, ILogger<GetHelpController> logger)
+    {
+        _logger = logger;
+        _helpPageSettings = helpPageSettings.CurrentValue;
+        helpPageSettings.OnChange(UpdateHelpPageSettings);
+    }
+
+    private void UpdateHelpPageSettings(HelpPageSettings settings) => _helpPageSettings = settings;
+
+    [HttpGet]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(PagedViewModel<HelpPageViewModel>),StatusCodes.Status200OK)]
+    public async Task<IActionResult> Get(string section, string tree, string baseUrl = "https://our.umbraco.com")
+    {
+        if (IsAllowedUrl(baseUrl) is false)
+        {
+            _logger.LogError(
+                $"The following URL is not listed in the allowlist for HelpPage in HelpPageSettings: {baseUrl}");
+            HttpContext.Response.StatusCode = (int)HttpStatusCode.BadRequest;
+
+
+            var invalidModelProblem = new ProblemDetails
+            {
+                Title = "Invalid database configuration",
+                Detail = "The provided database configuration is invalid",
+                Status = StatusCodes.Status400BadRequest,
+                Type = "Error",
+            };
+
+            return BadRequest(invalidModelProblem);
+        }
+
+        var url = string.Format(baseUrl + "/Umbraco/Documentation/Lessons/GetContextHelpDocs?sectionAlias={0}&treeAlias={1}", section, tree);
+
+        try
+        {
+            _httpClient ??= new HttpClient();
+
+            // fetch dashboard json and parse to JObject
+            var json = await _httpClient.GetStringAsync(url);
+            List<HelpPageViewModel>? result = JsonConvert.DeserializeObject<List<HelpPageViewModel>>(json);
+            if (result != null)
+            {
+                return Ok(result);
+            }
+        }
+        catch (HttpRequestException rex)
+        {
+            _logger.LogInformation($"Check your network connection, exception: {rex.Message}");
+        }
+
+        return Ok(new List<HelpPageViewModel>());
+    }
+
+    private bool IsAllowedUrl(string url) =>
+        _helpPageSettings.HelpPageUrlAllowList is null || _helpPageSettings.HelpPageUrlAllowList.Contains(url);
+}
