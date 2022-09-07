4eb32f4285598a4b36adde334e75937ffed2baec
Zeegaan <nge@umbraco.dk>
2022-09-07 13:37:47 +0200

    Use PagedViewModel for child relations

--- src/Umbraco.Cms.ManagementApi/Controllers/Relation/GetByChildRelationController.cs
+++ src/Umbraco.Cms.ManagementApi/Controllers/Relation/GetByChildRelationController.cs
@@ -2,8 +2,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Core.Mapping;
 using Umbraco.Cms.Core.Models;
-using Umbraco.Cms.Core.Models.ContentEditing;
 using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.ManagementApi.Factories;
+using Umbraco.Cms.ManagementApi.ViewModels.Pagination;
 using Umbraco.Cms.ManagementApi.ViewModels.Relation;
 using Umbraco.Extensions;
 
@@ -13,30 +14,37 @@ public class GetByChildRelationController : RelationControllerBase
 {
     private readonly IRelationService _relationService;
     private readonly IUmbracoMapper _umbracoMapper;
+    private readonly IPagedViewModelFactory _pagedViewModelFactory;
 
-    public GetByChildRelationController(IRelationService relationService, IUmbracoMapper umbracoMapper)
+    public GetByChildRelationController(IRelationService relationService, IUmbracoMapper umbracoMapper, IPagedViewModelFactory pagedViewModelFactory)
     {
         _relationService = relationService;
         _umbracoMapper = umbracoMapper;
+        _pagedViewModelFactory = pagedViewModelFactory;
     }
 
-    [HttpGet("/getByChild/{childId:int}")]
+    [HttpGet("childRelations/{childId:int}")]
     [MapToApiVersion("1.0")]
     [ProducesResponseType(typeof(RelationViewModel), StatusCodes.Status200OK)]
-    public IEnumerable<RelationViewModel> GetByChild(int childId, string relationTypeAlias = "")
+    public PagedViewModel<RelationViewModel> GetByChild(int childId, int skip, int take, string? relationTypeAlias = "")
     {
         IRelation[] relations = _relationService.GetByChildId(childId).ToArray();
+        IEnumerable<RelationViewModel> result = Enumerable.Empty<RelationViewModel>();
 
-        if (relations.Any() == false)
+        if (relations.Any())
         {
-            return Enumerable.Empty<RelationViewModel>();
+            if (string.IsNullOrWhiteSpace(relationTypeAlias) == false)
+            {
+                result = _umbracoMapper
+                    .MapEnumerable<IRelation, RelationViewModel>(relations.Where(x => x.RelationType.Alias.InvariantEquals(relationTypeAlias)))
+                    .WhereNotNull();
+            }
+            else
+            {
+                result = _umbracoMapper.MapEnumerable<IRelation, RelationViewModel>(relations).WhereNotNull();
+            }
         }
 
-        if (string.IsNullOrWhiteSpace(relationTypeAlias) == false)
-        {
-            return _umbracoMapper.MapEnumerable<IRelation, RelationViewModel>(relations.Where(x => x.RelationType.Alias.InvariantEquals(relationTypeAlias))).WhereNotNull();
-        }
-
-        return _umbracoMapper.MapEnumerable<IRelation, RelationViewModel>(relations).WhereNotNull();
+        return _pagedViewModelFactory.Create(result, skip, take);
     }
 }
