9e991e6f2b15bd3bece62aab0a8a8180e541c21e
Zeegaan <nge@umbraco.dk>
2022-09-07 12:42:10 +0200

    Add RelationViewModelFactory

---
+++ src/Umbraco.Cms.ManagementApi/Factories/IRelationViewModelFactory.cs
@@ -0,0 +1,9 @@
+﻿using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.ManagementApi.ViewModels.Relation;
+
+namespace Umbraco.Cms.ManagementApi.Factories;
+
+public interface IRelationViewModelFactory
+{
+    RelationViewModel Create(IRelation relation);
+}
