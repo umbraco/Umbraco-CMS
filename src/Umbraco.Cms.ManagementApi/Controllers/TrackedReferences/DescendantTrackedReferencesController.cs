150a769e3858c99a774b2155e18be6d7f02a9cb8
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-09-08 10:52:00 +0200

    Add DescendantTrackedReferencesController

---
+++ src/Umbraco.Cms.ManagementApi/Controllers/TrackedReferences/DescendantTrackedReferencesController.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Mapping;
+using Umbraco.Cms.ManagementApi.Services;
+using Umbraco.Cms.ManagementApi.ViewModels.Pagination;
+using Umbraco.Cms.ManagementApi.ViewModels.Server;
+using Umbraco.Cms.ManagementApi.ViewModels.TrackedReferences;
+using Umbraco.New.Cms.Core.Models.TrackedReferences;
+
+namespace Umbraco.Cms.ManagementApi.Controllers.TrackedReferences;
+
+public class DescendantTrackedReferencesController : TrackedReferencesControllerBase
+{
+    private readonly ITrackedReferencesSkipTakeService _trackedReferencesSkipTakeService;
+    private readonly IUmbracoMapper _umbracoMapper;
+
+    public DescendantTrackedReferencesController(ITrackedReferencesSkipTakeService trackedReferencesSkipTakeService, IUmbracoMapper umbracoMapper)
+    {
+        _trackedReferencesSkipTakeService = trackedReferencesSkipTakeService;
+        _umbracoMapper = umbracoMapper;
+    }
+
+    /// <summary>
+    ///     Gets a page list of the child nodes of the current item used in any kind of relation.
+    /// </summary>
+    /// <remarks>
+    ///     Used when deleting and unpublishing a single item to check if this item has any descending items that are in any
+    ///     kind of relation.
+    ///     This is basically finding the descending items which are children in relations.
+    /// </remarks>
+    [HttpGet("descendants/{parentId:int}")]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(typeof(ServerStatusViewModel), StatusCodes.Status200OK)]
+    public async Task<ActionResult<PagedViewModel<RelationItemViewModel>>> Descendants(int parentId, long skip, long take, bool filterMustBeIsDependency = true)
+    {
+        PagedViewModel<RelationItemModel> relationItems = _trackedReferencesSkipTakeService.GetPagedDescendantsInReferences(parentId, skip, take, filterMustBeIsDependency);
+        return new PagedViewModel<RelationItemViewModel>
+        {
+            Items = _umbracoMapper.MapEnumerable<RelationItemModel, RelationItemViewModel>(relationItems.Items),
+            Total = relationItems.Total
+        };
+    }
+}
