3a82aa758e6242108e76e2a62d2e8784d9ac5c4a
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-09-08 08:01:07 +0200

    Add RelationMapDefinition.cs

---
+++ src/Umbraco.New.Cms.Infrastructure/Persistence/Mappers/RelationMapDefinition.cs
@@ -0,0 +1,27 @@
+﻿using Umbraco.Cms.Core.Mapping;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Infrastructure.Persistence.Repositories.Implement;
+using Umbraco.New.Cms.Core.Models.TrackedReferences;
+
+namespace Umbraco.New.Cms.Infrastructure.Persistence.Mappers;
+
+public class RelationMapDefinition : IMapDefinition
+{
+    public void DefineMaps(IUmbracoMapper mapper)
+    {
+        mapper.Define<RelationItemDto, RelationModel>((source, context) => new RelationModel(), Map);
+    }
+
+    private void Map(RelationItemDto source, RelationModel target, MapperContext context)
+    {
+        target.NodeKey = source.ChildNodeKey;
+        target.NodeType = ObjectTypes.GetUdiType(source.ChildNodeObjectType);
+        target.NodeName = source.ChildNodeName;
+        target.RelationTypeName = source.RelationTypeName;
+        target.RelationTypeIsBidirectional = source.RelationTypeIsBidirectional;
+        target.RelationTypeIsDependency = source.RelationTypeIsDependency;
+        target.ContentTypeAlias = source.ChildContentTypeAlias;
+        target.ContentTypeIcon = source.ChildContentTypeIcon;
+        target.ContentTypeName = source.ChildContentTypeName;
+    }
+}
