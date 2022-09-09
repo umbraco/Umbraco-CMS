3769faf2a07c2978ee3587164bef9e89cb382c86
Zeegaan <nge@umbraco.dk>
2022-09-09 11:31:21 +0200

    Add SearchResultViewModel

---
+++ src/Umbraco.Cms.ManagementApi/ViewModels/ExamineManagement/SearchResultViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System.Text.Json.Serialization;
+
+namespace Umbraco.Cms.ManagementApi.ViewModels.ExamineManagement;
+
+public class SearchResultViewModel
+{
+    [JsonPropertyName("id")]
+    public string? Id { get; set; }
+
+    [JsonPropertyName("score")]
+    public float Score { get; set; }
+
+    [JsonPropertyName("fieldCount")]
+    public int FieldCount => Values?.Count ?? 0;
+
+    [JsonPropertyName("values")]
+    public IReadOnlyDictionary<string, IReadOnlyList<string>>? Values { get; set; }
+}
