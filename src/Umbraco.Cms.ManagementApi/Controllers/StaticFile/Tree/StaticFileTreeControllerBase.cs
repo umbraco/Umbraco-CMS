6bebaaea22e939570bf56f8c13b7a67c0c2b3261
kjac <kja@umbraco.dk>
2022-09-09 11:54:39 +0200

    Static files tree

---
+++ src/Umbraco.Cms.ManagementApi/Controllers/StaticFile/Tree/StaticFileTreeControllerBase.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.IO;
+using Umbraco.New.Cms.Web.Common.Routing;
+
+namespace Umbraco.Cms.ManagementApi.Controllers.StaticFile.Tree;
+
+[ApiVersion("1.0")]
+[ApiController]
+[VersionedApiBackOfficeRoute("static-file/tree")]
+public class StaticFileTreeControllerBase : FileSystemTreeControllerBase
+{
+    private static readonly string[] _whiteListedRootFolders = { "App_Plugins", "wwwroot" };
+
+    public StaticFileTreeControllerBase(IPhysicalFileSystem physicalFileSystem)
+        => FileSystem = physicalFileSystem;
+
+    protected override IFileSystem FileSystem { get; }
+
+    protected override string FileIcon(string path) => Constants.Icons.DefaultIcon;
+
+    protected override string ItemType(string path) => "static-file";
+
+    protected override string[] GetDirectories(string path) =>
+        IsRootPath(path)
+            ? _whiteListedRootFolders
+            : IsWhiteListedPath(path)
+                ? base.GetDirectories(path)
+                : Array.Empty<string>();
+
+    protected override string[] GetFiles(string path)
+        => IsRootPath(path) || IsWhiteListedPath(path) == false
+            ? Array.Empty<string>()
+            : base.GetFiles(path);
+
+    private bool IsRootPath(string path) => string.IsNullOrWhiteSpace(path);
+
+    private bool IsWhiteListedPath(string path) => _whiteListedRootFolders.Contains(path) || _whiteListedRootFolders.Any(folder => path.StartsWith($"{folder}/"));
+}
