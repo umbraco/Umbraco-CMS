eacd2033e43a15d69e95831732ec65224218394f
kjac <kja@umbraco.dk>
2022-09-09 08:42:28 +0200

    Document blueprint tree

---
+++ src/Umbraco.Cms.ManagementApi/Controllers/DocumentBlueprint/Tree/DocumentBlueprintTreeItemsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.ManagementApi.ViewModels.Tree;
+
+namespace Umbraco.Cms.ManagementApi.Controllers.DocumentBlueprint.Tree;
+
+public class DocumentBlueprintTreeItemsController : DocumentBlueprintTreeControllerBase
+{
+    public DocumentBlueprintTreeItemsController(IEntityService entityService, IContentTypeService contentTypeService)
+        : base(entityService, contentTypeService)
+    {
+    }
+
+    [HttpGet("items")]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(typeof(PagedResult<DocumentBlueprintTreeItemViewModel>), StatusCodes.Status200OK)]
+    public async Task<ActionResult<PagedResult<DocumentBlueprintTreeItemViewModel>>> Items([FromQuery(Name = "key")] Guid[] keys)
+        => await GetItems(keys);
+}
