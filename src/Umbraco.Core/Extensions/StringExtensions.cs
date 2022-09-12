9c1f0b2b02288746d7058e4c2c9e411d474ec7cc
patrickdemooij9 <patrickdemooij98@hotmail.com>
2022-09-12 10:07:11 +0200

    Remove for str as that one could be an empty string

--- src/Umbraco.Core/Extensions/StringExtensions.cs
+++ src/Umbraco.Core/Extensions/StringExtensions.cs
@@ -419,8 +419,7 @@ public static class StringExtensions
     ///     returns <see langword="false" />.
     /// </returns>
     public static bool IsNullOrWhiteSpace(this string? value) => string.IsNullOrWhiteSpace(value);
-
-    [return: NotNullIfNotNull("str")]
+    
     [return: NotNullIfNotNull("defaultValue")]
     public static string? IfNullOrWhiteSpace(this string? str, string? defaultValue) =>
         str.IsNullOrWhiteSpace() ? defaultValue : str;
