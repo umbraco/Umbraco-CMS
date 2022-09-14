998af30a0f8f6626118fbd8e64aba4e5c2cf7a9d
Zeegaan <nge@umbraco.dk>
2022-09-14 14:20:22 +0200

    Add SystemTextJsonConfigurationAttribute

---
+++ src/Umbraco.Cms.ManagementApi/Controllers/MananagementApiBaseController.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.ManagementApi.Filters;
+
+namespace Umbraco.Cms.ManagementApi.Controllers;
+
+[SystemTextJsonConfiguration]
+public class MananagementApiBaseController : Controller
+{
+
+}
---
+++ src/Umbraco.Cms.ManagementApi/Filters/EnumOutputFormatter.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+using Microsoft.AspNetCore.Mvc.Formatters;
+
+namespace Umbraco.Cms.ManagementApi.Filters;
+
+public class EnumOutputFormatter : SystemTextJsonOutputFormatter
+{
+    public EnumOutputFormatter(JsonSerializerOptions jsonSerializerOptions) : base(RegisterJsonConverters(jsonSerializerOptions))
+    {
+    }
+
+    protected static JsonSerializerOptions RegisterJsonConverters(JsonSerializerOptions serializerOptions)
+    {
+        serializerOptions.Converters.Add(new JsonStringEnumConverter());
+
+        return serializerOptions;
+    }
+}
