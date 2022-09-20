a1751f4c5868b651431c4c27a82d65669f6c2f36
Jesper Møller Jensen <jmj@umbraco.dk>
2022-09-20 15:02:37 +0200

    added selection element

---
+++ src/Umbraco.Web.UI.Client/src/backoffice/editors/users/views/users/editor-view-users-selection.element.ts
@@ -0,0 +1,86 @@
+import { css, html, LitElement, nothing } from 'lit';
+import { UUITextStyles } from '@umbraco-ui/uui-css/lib';
+import { customElement, property, state } from 'lit/decorators.js';
+import { UmbContextConsumerMixin } from '../../../../../core/context';
+import { repeat } from 'lit/directives/repeat.js';
+import UmbEditorViewUsersElement, { UserItem } from './editor-view-users.element';
+import { Subscription } from 'rxjs';
+
+@customElement('umb-editor-view-users-selection')
+export class UmbEditorViewUsersSelectionElement extends UmbContextConsumerMixin(LitElement) {
+	static styles = [
+		UUITextStyles,
+		css`
+			:host {
+				display: flex;
+				gap: var(--uui-size-3);
+				width: 100%;
+				padding: var(--uui-size-space-4);
+				background-color: var(--uui-color-selected);
+				color: var(--uui-color-selected-contrast);
+				align-items: center;
+				border-radius: var(--uui-size-space-2);
+				box-sizing: border-box;
+			}
+		`,
+	];
+
+	@state()
+	private _users: Array<UserItem> = [];
+
+	@state()
+	private _selection: Array<string> = [];
+
+	protected _usersContext?: UmbEditorViewUsersElement;
+	protected _usersSubscription?: Subscription;
+	protected _selectionSubscription?: Subscription;
+
+	connectedCallback(): void {
+		super.connectedCallback();
+
+		this.consumeContext('umbUsersContext', (usersContext: UmbEditorViewUsersElement) => {
+			this._usersContext = usersContext;
+
+			this._usersSubscription?.unsubscribe();
+			this._selectionSubscription?.unsubscribe();
+			this._usersSubscription = this._usersContext?.users.subscribe((users: Array<UserItem>) => {
+				this._users = users;
+			});
+			this._selectionSubscription = this._usersContext?.selection.subscribe((selection: Array<string>) => {
+				this._selection = selection;
+			});
+		});
+	}
+
+	disconnectedCallback(): void {
+		super.disconnectedCallback();
+
+		this._usersSubscription?.unsubscribe();
+		this._selectionSubscription?.unsubscribe();
+	}
+
+	private _handleClearSelection() {
+		this._usersContext?.setSelection([]);
+	}
+
+	private _renderSelectionCount() {
+		return html`<div>${this._selection.length} of ${this._users.length} selected</div>`;
+	}
+
+	render() {
+		return html`<uui-button @click=${this._handleClearSelection} label="Clear selection" look="secondary"></uui-button>
+			${this._renderSelectionCount()}
+			<uui-button style="margin-left: auto" label="Set group" look="secondary"></uui-button>
+			<uui-button label="Enable" look="secondary"></uui-button>
+			<uui-button label="Unlock" disabled look="secondary"></uui-button>
+			<uui-button label="Disable" look="secondary"></uui-button> `;
+	}
+}
+
+export default UmbEditorViewUsersSelectionElement;
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'umb-editor-view-users-selection': UmbEditorViewUsersSelectionElement;
+	}
+}
