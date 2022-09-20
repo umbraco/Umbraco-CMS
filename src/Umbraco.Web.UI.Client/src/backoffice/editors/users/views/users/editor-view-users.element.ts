550f5265c739ff03bc4ae58e543cdd7d705b9cc4
Jesper Møller Jensen <jmj@umbraco.dk>
2022-09-20 15:02:49 +0200

    using selection element and more dummy data

--- src/Umbraco.Web.UI.Client/src/backoffice/editors/users/views/users/editor-view-users.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/editors/users/views/users/editor-view-users.element.ts
@@ -1,11 +1,12 @@
-import { css, html, LitElement } from 'lit';
+import { css, html, LitElement, nothing } from 'lit';
 import { UUITextStyles } from '@umbraco-ui/uui-css/lib';
 import { customElement, property, state } from 'lit/decorators.js';
 import { UmbContextConsumerMixin, UmbContextProviderMixin } from '../../../../../core/context';
-import './editor-view-users-list.element';
-import './editor-view-users-grid.element';
 import { BehaviorSubject, Observable } from 'rxjs';
 import { InterfaceColor, InterfaceLook } from '@umbraco-ui/uui-base/lib/types';
+import './editor-view-users-list.element';
+import './editor-view-users-grid.element';
+import './editor-view-users-selection.element';
 
 export type UsersViewType = 'list' | 'grid';
 
@@ -35,6 +36,10 @@ export class UmbEditorViewUsersElement extends UmbContextProviderMixin(LitElemen
 			#user-list {
 				margin-top: var(--uui-size-layout-2);
 				font-size: 1rem;
+			}
+
+			umb-editor-view-users-selection {
+				margin-bottom: var(--uui-size-layout-2);
 			}
 		`,
 	];
@@ -72,6 +77,110 @@ export class UmbEditorViewUsersElement extends UmbContextProviderMixin(LitElemen
 			userGroup: 'Translators',
 			lastLogin: 'Tue, 11 December 2021',
 		},
+		{
+			key: 'b75af81a-b994-4e65-9330-b66c336d0203',
+			name: 'Jane Doe',
+			userGroup: 'Editors',
+			lastLogin: 'Fri, 13 April 2022',
+		},
+		{
+			key: 'b75af81a-b994-4e65-9330-b66c336d0204',
+			name: 'John Smith',
+			userGroup: 'Administrators',
+			lastLogin: 'Mon, 15 November 2021',
+		},
+		{
+			key: 'b75af81a-b994-4e65-9330-b66c336d0205',
+			name: 'Jane Smith',
+			userGroup: 'Editors',
+			lastLogin: 'Fri, 13 April 2022',
+		},
+		{
+			key: 'b75af81a-b994-4e65-9330-b66c336d0206',
+			name: 'Oliver Twist',
+			userGroup: 'Translators',
+			lastLogin: 'Tue, 11 December 2021',
+		},
+		{
+			key: 'b75af81a-b994-4e65-2330-b66c336d0207',
+			name: 'Olivia Doe',
+			userGroup: 'Editors',
+			lastLogin: 'Fri, 13 April 2022',
+		},
+		{
+			key: 'b75af81a-b994-4e65-9330-b66c336d0208',
+			name: 'Hans Hansen',
+			userGroup: 'Administrators',
+			lastLogin: 'Mon, 15 November 2021',
+		},
+		{
+			key: 'a9b18a00-58f2-sjh2-bf60-48d33ab156db',
+			name: 'Brian Adams',
+			userGroup: 'Translators',
+			lastLogin: 'Fri, 23 April 2021',
+			status: 'Invited',
+		},
+		{
+			key: '3179d0b2-eec2-4432-b86a-149e13b93e14',
+			name: 'Smith John',
+			userGroup: 'Editors',
+			lastLogin: 'Tue, 6 June 2021',
+			status: 'Disabled',
+		},
+		{
+			key: '1b1c9723-b845-4d9a-9ed2-b2f46c05fd72',
+			name: 'Reese Witherspoon',
+			userGroup: 'Administrators',
+			lastLogin: 'Mon, 15 November 2021',
+		},
+		{
+			key: 'b75af81a-2f94-4e65-9330-b66c336d0207',
+			name: 'Denzel Washington',
+			userGroup: 'Editors',
+			lastLogin: 'Fri, 13 April 2022',
+		},
+		{
+			key: 'b75af81a-b994-4e23-9330-b66c336d0202',
+			name: 'Leonardo DiCaprio',
+			userGroup: 'Translators',
+			lastLogin: 'Tue, 11 December 2021',
+		},
+		{
+			key: 'b75af81a-2394-4e65-9330-b66c336d0203',
+			name: 'Idris Elba',
+			userGroup: 'Editors',
+			lastLogin: 'Fri, 13 April 2022',
+		},
+		{
+			key: 'b75af81a-b994-4e65-9330-b6u7336d0204',
+			name: 'Quentin Tarantino',
+			userGroup: 'Administrators',
+			lastLogin: 'Mon, 15 November 2021',
+		},
+		{
+			key: 'b75af81a-b994-4e65-2330-c66c336d0205',
+			name: 'Tom Hanks',
+			userGroup: 'Editors',
+			lastLogin: 'Fri, 13 April 2022',
+		},
+		{
+			key: 'b75af82a-b994-4b65-9330-b66c336d0206',
+			name: 'Oprah Winfrey',
+			userGroup: 'Translators',
+			lastLogin: 'Tue, 11 December 2021',
+		},
+		{
+			key: 'b75af81a-b994-4e65-2s30-b66b336d0207',
+			name: 'Pamela Anderson',
+			userGroup: 'Editors',
+			lastLogin: 'Fri, 13 April 2022',
+		},
+		{
+			key: 'b75af81a-b994-4e65-9930-b66c336d0l33t',
+			name: 'Keanu Reeves',
+			userGroup: 'Administrators',
+			lastLogin: 'Mon, 15 November 2021',
+		},
 	];
 
 	@state()
@@ -92,16 +201,19 @@ export class UmbEditorViewUsersElement extends UmbContextProviderMixin(LitElemen
 	public setSelection(value: Array<string>) {
 		if (!value) return;
 		this._selection.next(value);
+		this.requestUpdate('selection');
 	}
 
 	public select(key: string) {
 		const selection = this._selection.getValue();
 		this._selection.next([...selection, key]);
+		this.requestUpdate('selection');
 	}
 
 	public deselect(key: string) {
 		const selection = this._selection.getValue();
 		this._selection.next(selection.filter((k) => k !== key));
+		this.requestUpdate('selection');
 	}
 
 	public getTagLookAndColor(status: string): { color: InterfaceColor; look: InterfaceLook } {
@@ -118,6 +230,10 @@ export class UmbEditorViewUsersElement extends UmbContextProviderMixin(LitElemen
 		}
 	}
 
+	private _toggleViewType() {
+		this._viewType = this._viewType === 'list' ? 'grid' : 'list';
+	}
+
 	private _renderViewType() {
 		switch (this._viewType) {
 			case 'list':
@@ -129,12 +245,17 @@ export class UmbEditorViewUsersElement extends UmbContextProviderMixin(LitElemen
 		}
 	}
 
-	private _toggleViewType() {
-		this._viewType = this._viewType === 'list' ? 'grid' : 'list';
+	private _renderSelection() {
+		if (this._selection.getValue().length === 0) return nothing;
+
+		return html`<umb-editor-view-users-selection></umb-editor-view-users-selection>`;
 	}
 
 	render() {
-		return html`<div id="top-bar">
+		return html`
+			${this._renderSelection()}
+
+			<div id="top-bar">
 				<uui-button label="Invite user" look="outline"></uui-button>
 				<div>
 					<uui-button
@@ -160,7 +281,8 @@ export class UmbEditorViewUsersElement extends UmbContextProviderMixin(LitElemen
 				</div>
 
 				${this._renderViewType()}
-			</div> `;
+			</div>
+		`;
 	}
 }
 
