9c35cc0b63d3542dc5959e03ad0ba866168573ef
Zeegaan <nge@umbraco.dk>
2022-09-21 15:05:03 +0200

    Add SystemTextJsonSerializer

---
+++ src/Umbraco.Cms.ManagementApi/Serialization/ISystemTextJsonSerializer.cs
@@ -0,0 +1,7 @@
+﻿using Umbraco.Cms.Core.Serialization;
+
+namespace Umbraco.Cms.ManagementApi.Serialization;
+
+public interface ISystemTextJsonSerializer : IJsonSerializer
+{
+}
--- src/Umbraco.Cms.ManagementApi/Services/JsonPatchService.cs
+++ src/Umbraco.Cms.ManagementApi/Services/JsonPatchService.cs
@@ -1,18 +1,22 @@
-﻿using System.Text.Json;
-using System.Text.Json.Nodes;
+﻿using System.Text.Json.Nodes;
 using Json.Patch;
+using Umbraco.Cms.ManagementApi.Serialization;
 using Umbraco.Cms.ManagementApi.ViewModels.JsonPatch;
 
 namespace Umbraco.Cms.ManagementApi.Services;
 
 public class JsonPatchService : IJsonPatchService
 {
+    private readonly ISystemTextJsonSerializer _systemTextJsonSerializer;
+
+    public JsonPatchService(ISystemTextJsonSerializer systemTextJsonSerializer) => _systemTextJsonSerializer = systemTextJsonSerializer;
 
     public PatchResult? Patch(JsonPatchViewModel[] patchViewModel, object objectToPatch)
     {
-        var patchString = JsonSerializer.Serialize(patchViewModel);
-        var docString = JsonSerializer.Serialize(objectToPatch);
-        JsonPatch? patch = JsonSerializer.Deserialize<JsonPatch>(patchString);
+        var patchString = _systemTextJsonSerializer.Serialize(patchViewModel);
+
+        var docString = _systemTextJsonSerializer.Serialize(objectToPatch);
+        JsonPatch? patch = _systemTextJsonSerializer.Deserialize<JsonPatch>(patchString);
         var doc = JsonNode.Parse(docString);
         return patch?.Apply(doc);
     }
