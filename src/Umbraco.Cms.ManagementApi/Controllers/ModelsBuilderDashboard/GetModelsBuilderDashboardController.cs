0c152679323b88101628e5db73cda9247ff8ab07
Zeegaan <nge@umbraco.dk>
2022-09-22 12:50:01 +0200

    Add GetModelsBuilderDashboardController

---
+++ src/Umbraco.Cms.ManagementApi/Controllers/ModelsBuilderDashboard/GetModelsBuilderDashboardController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.ManagementApi.Factories;
+using Umbraco.Cms.ManagementApi.ViewModels.ModelsBuilderDashboard;
+
+namespace Umbraco.Cms.ManagementApi.Controllers.ModelsBuilderDashboard;
+
+public class GetModelsBuilderDashboardController : ModelsBuilderDashboardControllerBase
+{
+    private readonly IModelsBuilderDashboardViewModelFactory _modelsBuilderDashboardViewModelFactory;
+
+    public GetModelsBuilderDashboardController(IModelsBuilderDashboardViewModelFactory modelsBuilderDashboardViewModelFactory) => _modelsBuilderDashboardViewModelFactory = modelsBuilderDashboardViewModelFactory;
+
+    [HttpGet]
+    public ModelsBuilderDashboardViewModel GetDashboard() => _modelsBuilderDashboardViewModelFactory.Create();
+}
