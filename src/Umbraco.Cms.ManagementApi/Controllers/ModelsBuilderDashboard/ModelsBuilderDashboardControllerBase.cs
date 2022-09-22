8615c178a968ffd97c0385ae8797b6637fd6333e
Zeegaan <nge@umbraco.dk>
2022-09-22 12:33:50 +0200

    Add StatusModelsBuilderDashboardController

--- src/Umbraco.Cms.ManagementApi/Controllers/ModelsBuilderDashboard/ModelsBuilderDashboardControllerBase.cs
+++ src/Umbraco.Cms.ManagementApi/Controllers/ModelsBuilderDashboard/ModelsBuilderDashboardControllerBase.cs
@@ -5,8 +5,8 @@ using Umbraco.New.Cms.Web.Common.Routing;
 namespace Umbraco.Cms.ManagementApi.Controllers.ModelsBuilderDashboard;
 
 [ApiController]
-[BackOfficeRoute("api/v{version:apiVersion}/server")]
-[OpenApiTag("Server")]
+[BackOfficeRoute("api/v{version:apiVersion}/modelsBuilderDashboard")]
+[OpenApiTag("ModelsBuilderDashboard")]
 [ApiVersion("1.0")]
 public class ModelsBuilderDashboardControllerBase : ManagementApiControllerBase
 {
---
+++ src/Umbraco.Cms.ManagementApi/Controllers/ModelsBuilderDashboard/StatusModelsBuilderDashboardController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Infrastructure.ModelsBuilder;
+using Umbraco.Cms.ManagementApi.ViewModels.ModelsBuilderDashboard;
+using Umbraco.New.Cms.Core.Models;
+
+namespace Umbraco.Cms.ManagementApi.Controllers.ModelsBuilderDashboard;
+
+public class StatusModelsBuilderDashboardController : ModelsBuilderDashboardControllerBase
+{
+    private readonly OutOfDateModelsStatus _outOfDateModelsStatus;
+
+    public StatusModelsBuilderDashboardController(OutOfDateModelsStatus outOfDateModelsStatus) => _outOfDateModelsStatus = outOfDateModelsStatus;
+
+    [HttpGet("status")]
+    public ActionResult<OutOfDateStatusViewModel> GetModelsOutOfDateStatus()
+    {
+        OutOfDateStatusViewModel status = _outOfDateModelsStatus.IsEnabled
+            ? _outOfDateModelsStatus.IsOutOfDate
+                ? new OutOfDateStatusViewModel { Status = OutOfDateType.OutOfDate }
+                : new OutOfDateStatusViewModel { Status = OutOfDateType.Current }
+            : new OutOfDateStatusViewModel { Status = OutOfDateType.Unknown };
+
+        return status;
+    }
+}
