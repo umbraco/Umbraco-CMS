ceb539220105f6fd368800f31262a6d6eed016fe
Mads Rasmussen <madsr@hey.com>
2022-09-22 19:13:19 +0200

    remove unused import

--- src/Umbraco.Web.UI.Client/src/backoffice/property-editors/textarea/property-editor-textarea.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/property-editors/textarea/property-editor-textarea.element.ts
@@ -1,6 +1,6 @@
 import { css, html, LitElement } from 'lit';
 import { UUITextStyles } from '@umbraco-ui/uui-css/lib';
-import { customElement, property, state } from 'lit/decorators.js';
+import { customElement, property } from 'lit/decorators.js';
 
 @customElement('umb-property-editor-textarea')
 export class UmbPropertyEditorTextareaElement extends LitElement {
