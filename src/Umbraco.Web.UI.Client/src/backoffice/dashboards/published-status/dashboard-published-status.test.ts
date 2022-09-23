1c978736e4ca16a0e796070a431747c1fc0a6944
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2022-09-23 13:32:56 +0200

    add a11y test to dashboard

--- src/Umbraco.Web.UI.Client/src/backoffice/dashboards/published-status/dashboard-published-status.test.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/dashboards/published-status/dashboard-published-status.test.ts
@@ -1,5 +1,6 @@
 import { expect, fixture, html } from '@open-wc/testing';
 
+import { defaultA11yConfig } from '../../../core/helpers/chai';
 import { UmbDashboardPublishedStatusElement } from './dashboard-published-status.element';
 
 describe('UmbDashboardPublishedStatus', () => {
@@ -11,5 +12,9 @@ describe('UmbDashboardPublishedStatus', () => {
 
 	it('is defined with its own instance', () => {
 		expect(element).to.be.instanceOf(UmbDashboardPublishedStatusElement);
+	});
+
+	it('passes the a11y audit', async () => {
+		await expect(element).to.be.accessible(defaultA11yConfig);
 	});
 });
