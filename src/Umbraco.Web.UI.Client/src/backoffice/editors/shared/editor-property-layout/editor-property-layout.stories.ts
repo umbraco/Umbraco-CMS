fad6b7ee800ed491b1d78d3a673985355126b2c5
Mads Rasmussen <madsr@hey.com>
2022-09-27 10:44:46 +0200

    update story with new properties for editor-property-layout

--- src/Umbraco.Web.UI.Client/src/backoffice/editors/shared/editor-property-layout/editor-property-layout.stories.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/editors/shared/editor-property-layout/editor-property-layout.stories.ts
@@ -10,8 +10,11 @@ export default {
 	id: 'umb-editor-property-layout',
 } as Meta;
 
-export const AAAOverview: Story<UmbEditorPropertyLayoutElement> = () => html` <umb-editor-property-layout>
-	<div slot="header"><uui-button color="" look="placeholder">Header</uui-button></div>
+export const AAAOverview: Story<UmbEditorPropertyLayoutElement> = () => html` <umb-editor-property-layout
+	label="Label"
+	description="Description">
+	<div slot="property-action-menu"><uui-button color="" look="placeholder">Menu</uui-button></div>
+
 	<div slot="editor"><uui-button color="" look="placeholder">Editor</uui-button></div>
 </umb-editor-property-layout>`;
 AAAOverview.storyName = 'Overview';
