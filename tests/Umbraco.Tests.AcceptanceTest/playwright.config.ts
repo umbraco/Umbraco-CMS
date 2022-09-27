36829a40076c2298b063e2eb531f638f4c9eaad7
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-09-27 14:15:55 +0200

    create .env file when installing

--- tests/Umbraco.Tests.AcceptanceTest/playwright.config.ts
+++ tests/Umbraco.Tests.AcceptanceTest/playwright.config.ts
@@ -1,11 +1,8 @@
 import type { PlaywrightTestConfig } from '@playwright/test';
 import { devices } from '@playwright/test';
+import dotenv from 'dotenv';
 
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
+dotenv.config();
 
 /**
  * See https://playwright.dev/docs/test-configuration.
@@ -34,7 +31,7 @@ const config: PlaywrightTestConfig = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:44332',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
--- tests/Umbraco.Tests.AcceptanceTest/postinstall.js
+++ tests/Umbraco.Tests.AcceptanceTest/postinstall.js
@@ -1,6 +1,6 @@
 const fs = require('fs');
 
-const configPath = './playwright.env.json';
+const configPath = './.env';
 
 try {
   if (fs.existsSync(configPath)) {
