3b6b5d91489b4dc7796c62a9d3a11005c9792c27
Zeegaan <nge@umbraco.dk>
2022-09-28 13:00:45 +0200

    ignore https in config

--- tests/Umbraco.Tests.AcceptanceTest/playwright.config.ts
+++ tests/Umbraco.Tests.AcceptanceTest/playwright.config.ts
@@ -31,10 +31,11 @@ const config: PlaywrightTestConfig = {
     /* Maximum time each action such as `click()` can take. Defaults to 0 (no limit). */
     actionTimeout: 0,
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:44332',
-
+    // baseURL: 'http://localhost:44332',
+    
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
+    ignoreHTTPSErrors: true,    
   },
 
   /* Configure projects for major browsers */
