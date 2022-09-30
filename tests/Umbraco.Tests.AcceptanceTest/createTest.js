45e869b685aaaca4e6caa99ec656cf5b57ddad9c
Zeegaan <nge@umbraco.dk>
2022-09-30 10:01:51 +0200

    Implement create test script

--- tests/Umbraco.Tests.AcceptanceTest/createTest.js
+++ tests/Umbraco.Tests.AcceptanceTest/createTest.js
@@ -2,13 +2,7 @@ const fs = require('fs');
 
 const args = process.argv.slice(2);
 
-function makeDirs() {
-    // Here we want to make sure our directories exist.
-    fs.mkdirSync('./tests', { recursive: true });
-}
-
 function generate(fileName) {
-    makeDirs();
     fs.writeFileSync(
         `./tests/${fileName}.spec.ts`,
         `import {test} from '@umbraco/playwright-testhelpers';
