71abbdf9ffcf769e76682194e3af9c772a6b7160
kjac <kja@umbraco.dk>
2022-10-03 19:19:39 +0200

    Refactor models + remove unused properties

--- src/Umbraco.Infrastructure/Models/Blocks/BlockGridLayoutItem.cs
+++ src/Umbraco.Infrastructure/Models/Blocks/BlockGridLayoutItem.cs
@@ -4,45 +4,27 @@
 using Newtonsoft.Json;
 using Umbraco.Cms.Infrastructure.Serialization;
 
-namespace Umbraco.Cms.Core.Models.Blocks
+namespace Umbraco.Cms.Core.Models.Blocks;
+
+/// <summary>
+/// Used for deserializing the block grid layout
+/// </summary>
+public class BlockGridLayoutItem : IBlockLayoutItem
 {
-    /// <summary>
-    /// Used for deserializing the block grid layout
-    /// </summary>
-    public class BlockGridLayoutItem : IBlockLayoutItem
-    {
-        [JsonProperty("contentUdi", Required = Required.Always)]
-        [JsonConverter(typeof(UdiJsonConverter))]
-        public Udi? ContentUdi { get; set; }
+    [JsonProperty("contentUdi", Required = Required.Always)]
+    [JsonConverter(typeof(UdiJsonConverter))]
+    public Udi? ContentUdi { get; set; }
 
-        [JsonProperty("settingsUdi", NullValueHandling = NullValueHandling.Ignore)]
-        [JsonConverter(typeof(UdiJsonConverter))]
-        public Udi? SettingsUdi { get; set; }
+    [JsonProperty("settingsUdi", NullValueHandling = NullValueHandling.Ignore)]
+    [JsonConverter(typeof(UdiJsonConverter))]
+    public Udi? SettingsUdi { get; set; }
 
-        [JsonProperty("columnSpan", NullValueHandling = NullValueHandling.Ignore)]
-        public int? ColumnSpan { get; set; }
+    [JsonProperty("columnSpan", NullValueHandling = NullValueHandling.Ignore)]
+    public int? ColumnSpan { get; set; }
 
-        [JsonProperty("rowSpan", NullValueHandling = NullValueHandling.Ignore)]
-        public int? RowSpan { get; set; }
+    [JsonProperty("rowSpan", NullValueHandling = NullValueHandling.Ignore)]
+    public int? RowSpan { get; set; }
 
-        [JsonProperty("areaGridColumns", NullValueHandling = NullValueHandling.Ignore)]
-        public int? AreaGridColumns { get; set; }
-
-        [JsonProperty("areas", NullValueHandling = NullValueHandling.Ignore)]
-        public BlockGridLayoutAreaItem[] Areas { get; set; } = Array.Empty<BlockGridLayoutAreaItem>();
-
-        /*
-        [JsonProperty("children", NullValueHandling = NullValueHandling.Ignore)]
-        public JEnumerable<JToken> Children { get; set; }
-        */
-    }
-
-    public class BlockGridLayoutAreaItem
-    {
-        [JsonProperty("key", NullValueHandling = NullValueHandling.Ignore)]
-        public Guid Key { get; set; } = Guid.Empty;
-
-        [JsonProperty("items", NullValueHandling = NullValueHandling.Ignore)]
-        public BlockGridLayoutItem[] Items { get; set; } = Array.Empty<BlockGridLayoutItem>();
-    }
+    [JsonProperty("areas", NullValueHandling = NullValueHandling.Ignore)]
+    public BlockGridLayoutAreaItem[] Areas { get; set; } = Array.Empty<BlockGridLayoutAreaItem>();
 }
