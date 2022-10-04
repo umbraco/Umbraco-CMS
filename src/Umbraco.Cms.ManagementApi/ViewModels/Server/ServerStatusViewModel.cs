668aa0abd5ab39368c68ffd68978f65b494dd28b
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-10-04 09:43:23 +0200

    Fix open api doc

--- src/Umbraco.Cms.ManagementApi/ViewModels/Server/ServerStatusViewModel.cs
+++ src/Umbraco.Cms.ManagementApi/ViewModels/Server/ServerStatusViewModel.cs
@@ -1,8 +1,10 @@
-﻿using Umbraco.Cms.Core;
+﻿using System.Text.Json.Serialization;
+using Umbraco.Cms.Core;
 
 namespace Umbraco.Cms.ManagementApi.ViewModels.Server;
 
 public class ServerStatusViewModel
 {
+    [JsonConverter(typeof(JsonStringEnumConverter))]
     public RuntimeLevel ServerStatus { get; set; }
 }
