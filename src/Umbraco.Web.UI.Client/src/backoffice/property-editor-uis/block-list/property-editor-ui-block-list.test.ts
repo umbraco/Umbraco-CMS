936630652380dcf1531ec2b10cc187826de2fc11
Mads Rasmussen <madsr@hey.com>
2022-10-07 16:03:42 +0200

    fix imports

--- src/Umbraco.Web.UI.Client/src/backoffice/property-editor-uis/block-list/property-editor-ui-block-list.test.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/property-editor-uis/block-list/property-editor-ui-block-list.test.ts
@@ -1,21 +1,19 @@
 import { expect, fixture, html } from '@open-wc/testing';
 import { defaultA11yConfig } from '../../../core/helpers/chai';
-import { UmbPropertyEditorUIBlockListElement } from './umb-property-editor-ui-block-list.element';
+import { UmbPropertyEditorUIBlockListElement } from './property-editor-ui-block-list.element';
 
 describe('UmbPropertyEditorUIBlockListElement', () => {
-  let element: UmbPropertyEditorUIBlockListElement;
+	let element: UmbPropertyEditorUIBlockListElement;
 
-  beforeEach(async () => {
-    element = await fixture(
-      html` <umb-property-editor-ui-block-list></umb-property-editor-ui-block-list> `
-    );
-  });
+	beforeEach(async () => {
+		element = await fixture(html` <umb-property-editor-ui-block-list></umb-property-editor-ui-block-list> `);
+	});
 
-  it('is defined with its own instance', () => {
-    expect(element).to.be.instanceOf(UmbPropertyEditorUIBlockListElement);
-  });
+	it('is defined with its own instance', () => {
+		expect(element).to.be.instanceOf(UmbPropertyEditorUIBlockListElement);
+	});
 
-  it('passes the a11y audit', async () => {
-    await expect(element).shadowDom.to.be.accessible(defaultA11yConfig);
-  });
+	it('passes the a11y audit', async () => {
+		await expect(element).shadowDom.to.be.accessible(defaultA11yConfig);
+	});
 });
--- src/Umbraco.Web.UI.Client/src/backoffice/property-editor-uis/icon-picker/property-editor-ui-icon-picker.test.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/property-editor-uis/icon-picker/property-editor-ui-icon-picker.test.ts
@@ -1,21 +1,19 @@
 import { expect, fixture, html } from '@open-wc/testing';
 import { defaultA11yConfig } from '../../../core/helpers/chai';
-import { UmbPropertyEditorUIIconPickerElement } from './umb-property-editor-ui-icon-picker.element';
+import { UmbPropertyEditorUIIconPickerElement } from './property-editor-ui-icon-picker.element';
 
 describe('UmbPropertyEditorUIIconPickerElement', () => {
-  let element: UmbPropertyEditorUIIconPickerElement;
+	let element: UmbPropertyEditorUIIconPickerElement;
 
-  beforeEach(async () => {
-    element = await fixture(
-      html` <umb-property-editor-ui-icon-picker></umb-property-editor-ui-icon-picker> `
-    );
-  });
+	beforeEach(async () => {
+		element = await fixture(html` <umb-property-editor-ui-icon-picker></umb-property-editor-ui-icon-picker> `);
+	});
 
-  it('is defined with its own instance', () => {
-    expect(element).to.be.instanceOf(UmbPropertyEditorUIIconPickerElement);
-  });
+	it('is defined with its own instance', () => {
+		expect(element).to.be.instanceOf(UmbPropertyEditorUIIconPickerElement);
+	});
 
-  it('passes the a11y audit', async () => {
-    await expect(element).shadowDom.to.be.accessible(defaultA11yConfig);
-  });
+	it('passes the a11y audit', async () => {
+		await expect(element).shadowDom.to.be.accessible(defaultA11yConfig);
+	});
 });
