5c135404f14051a4e4a2f01b5099ea81e18f0939
Zeegaan <nge@umbraco.dk>
2022-10-10 10:45:54 +0200

    Add UmbracoManagementApiController tag

--- src/Umbraco.Cms.ManagementApi/Controllers/ManagementApiControllerBase.cs
+++ src/Umbraco.Cms.ManagementApi/Controllers/ManagementApiControllerBase.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.ManagementApi.Filters;
+using Umbraco.Cms.Web.Common.Attributes;
 
 namespace Umbraco.Cms.ManagementApi.Controllers;
 
+[UmbracoManagementApiController]
 [ManagementApiJsonConfiguration]
 public class ManagementApiControllerBase : Controller
 {
