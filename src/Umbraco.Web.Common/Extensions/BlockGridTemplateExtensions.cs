e40cb0c0bf62c02ec8a1aa97d3fdf59f0bfd52b2
kjac <kja@umbraco.dk>
2022-10-11 13:55:51 +0200

    Add sync rendering extensions for block grid and async ones for block list

--- src/Umbraco.Web.Common/Extensions/BlockGridTemplateExtensions.cs
+++ src/Umbraco.Web.Common/Extensions/BlockGridTemplateExtensions.cs
@@ -15,6 +15,8 @@ public static class BlockGridTemplateExtensions
     public const string DefaultItemsTemplate = "items";
     public const string DefaultItemAreasTemplate = "areas";
 
+    #region Async
+
     public static async Task<IHtmlContent> GetBlockGridHtmlAsync(this IHtmlHelper html, BlockGridModel? model, string template = DefaultTemplate)
     {
         if (model?.Count == 0)
@@ -22,8 +24,7 @@ public static class BlockGridTemplateExtensions
             return new HtmlString(string.Empty);
         }
 
-        var view = $"{DefaultFolder}{template}";
-        return await html.PartialAsync(view, model);
+        return await html.PartialAsync(DefaultFolderTemplate(template), model);
     }
 
     public static async Task<IHtmlContent> GetBlockGridHtmlAsync(this IHtmlHelper html, IPublishedProperty property, string template = DefaultTemplate)
@@ -34,6 +35,54 @@ public static class BlockGridTemplateExtensions
 
     public static async Task<IHtmlContent> GetBlockGridHtmlAsync(this IHtmlHelper html, IPublishedContent contentItem, string propertyAlias, string template)
     {
+        IPublishedProperty prop = GetRequiredProperty(contentItem, propertyAlias);
+        return await GetBlockGridHtmlAsync(html, prop.GetValue() as BlockGridModel, template);
+    }
+
+    public static async Task<IHtmlContent> GetBlockGridItemsHtmlAsync(this IHtmlHelper html, IEnumerable<BlockGridItem> items, string template = DefaultItemsTemplate)
+        => await html.PartialAsync(DefaultFolderTemplate(template), items);
+
+    public static async Task<IHtmlContent> GetBlockGridItemAreasHtmlAsync(this IHtmlHelper html, BlockGridItem item, string template = DefaultItemAreasTemplate)
+        => await html.PartialAsync(DefaultFolderTemplate(template), item);
+
+    #endregion
+
+    #region Sync
+
+    public static IHtmlContent GetBlockGridHtml(this IHtmlHelper html, BlockGridModel? model, string template = DefaultTemplate)
+    {
+        if (model?.Count == 0)
+        {
+            return new HtmlString(string.Empty);
+        }
+
+        return html.Partial(DefaultFolderTemplate(template), model);
+    }
+
+    public static IHtmlContent GetBlockGridHtml(this IHtmlHelper html, IPublishedProperty property, string template = DefaultTemplate)
+        => GetBlockGridHtml(html, property.GetValue() as BlockGridModel, template);
+
+    public static IHtmlContent GetBlockGridHtml(this IHtmlHelper html, IPublishedContent contentItem, string propertyAlias)
+        => GetBlockGridHtml(html, contentItem, propertyAlias, DefaultTemplate);
+
+    public static IHtmlContent GetBlockGridHtml(this IHtmlHelper html, IPublishedContent contentItem, string propertyAlias, string template)
+    {
+        IPublishedProperty prop = GetRequiredProperty(contentItem, propertyAlias);
+        return GetBlockGridHtml(html, prop.GetValue() as BlockGridModel, template);
+    }
+
+    public static IHtmlContent GetBlockGridItemsHtml(this IHtmlHelper html, IEnumerable<BlockGridItem> items, string template = DefaultItemsTemplate)
+        => html.Partial(DefaultFolderTemplate(template), items);
+
+    public static IHtmlContent GetBlockGridItemAreasHtml(this IHtmlHelper html, BlockGridItem item, string template = DefaultItemAreasTemplate)
+        => html.Partial(DefaultFolderTemplate(template), item);
+
+    #endregion
+
+    private static string DefaultFolderTemplate(string template) => $"{DefaultFolder}{template}";
+
+    private static IPublishedProperty GetRequiredProperty(IPublishedContent contentItem, string propertyAlias)
+    {
         ArgumentNullException.ThrowIfNull(propertyAlias);
 
         if (string.IsNullOrWhiteSpace(propertyAlias))
@@ -43,18 +92,12 @@ public static class BlockGridTemplateExtensions
                 nameof(propertyAlias));
         }
 
-        IPublishedProperty? prop = contentItem.GetProperty(propertyAlias);
-        if (prop == null)
+        IPublishedProperty? property = contentItem.GetProperty(propertyAlias);
+        if (property == null)
         {
             throw new InvalidOperationException("No property type found with alias " + propertyAlias);
         }
 
-        return await GetBlockGridHtmlAsync(html, prop.GetValue() as BlockGridModel, template);
+        return property;
     }
-
-    public static async Task<IHtmlContent> GetBlockGridItemsHtmlAsync(this IHtmlHelper html, IEnumerable<BlockGridItem> items, string template = DefaultItemsTemplate)
-        => await html.PartialAsync($"{DefaultFolder}{template}", items);
-
-    public static async Task<IHtmlContent> GetBlockGridItemAreasHtmlAsync(this IHtmlHelper html, BlockGridItem item, string template = DefaultItemAreasTemplate)
-        => await html.PartialAsync($"{DefaultFolder}{template}", item);
 }
