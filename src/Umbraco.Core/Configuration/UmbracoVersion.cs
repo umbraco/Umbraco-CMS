86aaf4ff038704b4def767fe5a3920b46b93a830
Giordano Polidoro <giordanop@users.noreply.github.com>
2022-10-13 12:46:15 +0200

    Bump version to 7.15.10 (#13076)

--- src/Umbraco.Core/Configuration/UmbracoVersion.cs
+++ src/Umbraco.Core/Configuration/UmbracoVersion.cs
@@ -6,7 +6,7 @@ namespace Umbraco.Core.Configuration
 {
     public class UmbracoVersion
     {
-        private static readonly Version Version = new Version("7.15.8");
+        private static readonly Version Version = new Version("7.15.10");
 
         /// <summary>
         /// Gets the current version of Umbraco.
