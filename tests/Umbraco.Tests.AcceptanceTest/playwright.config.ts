8f70276cf72c742fc42788a977fb6f6972872481
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-10-17 14:53:41 +0200

    Update maximum test time-out

--- tests/Umbraco.Tests.AcceptanceTest/playwright.config.ts
+++ tests/Umbraco.Tests.AcceptanceTest/playwright.config.ts
@@ -10,7 +10,7 @@ dotenv.config();
 const config: PlaywrightTestConfig = {
   testDir: './tests/',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 120 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
