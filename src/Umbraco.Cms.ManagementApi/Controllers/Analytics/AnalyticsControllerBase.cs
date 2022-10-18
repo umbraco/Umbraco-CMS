d3fd31426bb43a4eba96b64ef507c8dcd3b0a253
nikolajlauridsen <nikolajlauridsen@protonmail.ch>
2022-10-18 14:07:45 +0200

    Change management api base path to  /umbraco/management/api/

--- src/Umbraco.Cms.ManagementApi/Controllers/Analytics/AnalyticsControllerBase.cs
+++ src/Umbraco.Cms.ManagementApi/Controllers/Analytics/AnalyticsControllerBase.cs
@@ -5,7 +5,7 @@ using Umbraco.New.Cms.Web.Common.Routing;
 namespace Umbraco.Cms.ManagementApi.Controllers.Analytics;
 
 [ApiController]
-[BackOfficeRoute("api/v{version:apiVersion}/analytics")]
+[VersionedApiBackOfficeRoute("analytics")]
 [OpenApiTag("Analytics")]
 [ApiVersion("1.0")]
 public abstract class AnalyticsControllerBase : ManagementApiControllerBase
--- src/Umbraco.Cms.ManagementApi/Controllers/Culture/CultureControllerBase.cs
+++ src/Umbraco.Cms.ManagementApi/Controllers/Culture/CultureControllerBase.cs
@@ -5,7 +5,7 @@ using Umbraco.New.Cms.Web.Common.Routing;
 namespace Umbraco.Cms.ManagementApi.Controllers.Culture;
 
 [ApiController]
-[BackOfficeRoute("api/v{version:apiVersion}/culture")]
+[VersionedApiBackOfficeRoute("culture")]
 [OpenApiTag("Culture")]
 [ApiVersion("1.0")]
 public abstract class CultureControllerBase : ManagementApiControllerBase
