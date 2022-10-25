10d03f0b585866394db18c31dddb50ff3aa6e4da
Ronald Barendse <ronald@barend.se>
2022-10-25 18:40:11 +0200

    Add IContextCache to deploy connectors (#13287)
    * Add IContextCache and implementations
    
    * Update connector interfaces to use IContextCache
    
    * Minor cleanup
    
    * Move DeployContextCache prefix to constant
    
    * Move default implementations to obsolete methods
    
    * Remove DeployContextCache and DictionaryCache
    
    Co-authored-by: Andy Butland <abutland73@gmail.com>

--- src/Umbraco.Core/Deploy/IDataTypeConfigurationConnector.cs
+++ src/Umbraco.Core/Deploy/IDataTypeConfigurationConnector.cs
@@ -1,37 +1,67 @@
-using System.Diagnostics.CodeAnalysis;
 using Umbraco.Cms.Core.Models;
 
 namespace Umbraco.Cms.Core.Deploy;
 
 /// <summary>
-///     Defines methods that can convert data type configuration to / from an environment-agnostic string.
+/// Defines methods that can convert data type configuration to / from an environment-agnostic string.
 /// </summary>
 /// <remarks>
-///     Configuration may contain values such as content identifiers, that would be local
-///     to one environment, and need to be converted in order to be deployed.
+/// Configuration may contain values such as content identifiers, that would be local
+/// to one environment, and need to be converted in order to be deployed.
 /// </remarks>
-[SuppressMessage(
-    "ReSharper",
-    "UnusedMember.Global",
-    Justification = "This is actual only used by Deploy, but we don't want third parties to have references on deploy, that's why this interface is part of core.")]
 public interface IDataTypeConfigurationConnector
 {
     /// <summary>
-    ///     Gets the property editor aliases that the value converter supports by default.
+    /// Gets the property editor aliases that the value converter supports by default.
     /// </summary>
+    /// <value>
+    /// The property editor aliases.
+    /// </value>
     IEnumerable<string> PropertyEditorAliases { get; }
 
     /// <summary>
-    ///     Gets the artifact datatype configuration corresponding to the actual datatype configuration.
+    /// Gets the artifact configuration value corresponding to a data type configuration and gather dependencies.
     /// </summary>
-    /// <param name="dataType">The datatype.</param>
+    /// <param name="dataType">The data type.</param>
     /// <param name="dependencies">The dependencies.</param>
-    string? ToArtifact(IDataType dataType, ICollection<ArtifactDependency> dependencies);
+    /// <returns>
+    /// The artifact configuration value.
+    /// </returns>
+    [Obsolete("Use the overload accepting IContextCache instead. This overload will be removed in a future version.")]
+    string? ToArtifact(IDataType dataType, ICollection<ArtifactDependency> dependencies)
+        => ToArtifact(dataType, dependencies, PassThroughCache.Instance);
 
     /// <summary>
-    ///     Gets the actual datatype configuration corresponding to the artifact configuration.
+    /// Gets the artifact configuration value corresponding to a data type configuration and gather dependencies.
     /// </summary>
-    /// <param name="dataType">The datatype.</param>
-    /// <param name="configuration">The artifact configuration.</param>
-    object? FromArtifact(IDataType dataType, string? configuration);
+    /// <param name="dataType">The data type.</param>
+    /// <param name="dependencies">The dependencies.</param>
+    /// <param name="contextCache">The context cache.</param>
+    /// <returns>
+    /// The artifact configuration value.
+    /// </returns>
+    string? ToArtifact(IDataType dataType, ICollection<ArtifactDependency> dependencies, IContextCache contextCache);
+
+    /// <summary>
+    /// Gets the data type configuration corresponding to an artifact configuration value.
+    /// </summary>
+    /// <param name="dataType">The data type.</param>
+    /// <param name="configuration">The artifact configuration value.</param>
+    /// <returns>
+    /// The data type configuration.
+    /// </returns>
+    [Obsolete("Use the overload accepting IContextCache instead. This overload will be removed in a future version.")]
+    object? FromArtifact(IDataType dataType, string? configuration)
+        => FromArtifact(dataType, configuration, PassThroughCache.Instance);
+
+    /// <summary>
+    /// Gets the data type configuration corresponding to an artifact configuration value.
+    /// </summary>
+    /// <param name="dataType">The data type.</param>
+    /// <param name="configuration">The artifact configuration value.</param>
+    /// <param name="contextCache">The context cache.</param>
+    /// <returns>
+    /// The data type configuration.
+    /// </returns>
+    object? FromArtifact(IDataType dataType, string? configuration, IContextCache contextCache);
 }
