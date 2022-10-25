e4cbf9173b4fd854aa48aa0ce181f567d5dce16e
Elitsa Marinovska <elm@umbraco.dk>
2022-10-25 13:43:06 +0200

    Fix modelsbuilder paths and related naming

--- src/Umbraco.Cms.ManagementApi/Controllers/ModelsBuilder/GetModelsBuilderController.cs
+++ src/Umbraco.Cms.ManagementApi/Controllers/ModelsBuilder/GetModelsBuilderController.cs
@@ -4,7 +4,7 @@ using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.ManagementApi.Factories;
 using Umbraco.Cms.ManagementApi.ViewModels.ModelsBuilderDashboard;
 
-namespace Umbraco.Cms.ManagementApi.Controllers.ModelsBuilderDashboard;
+namespace Umbraco.Cms.ManagementApi.Controllers.ModelsBuilder;
 
 public class GetModelsBuilderController : ModelsBuilderControllerBase
 {
@@ -12,7 +12,7 @@ public class GetModelsBuilderController : ModelsBuilderControllerBase
 
     public GetModelsBuilderController(IModelsBuilderViewModelFactory modelsBuilderViewModelFactory) => _modelsBuilderViewModelFactory = modelsBuilderViewModelFactory;
 
-    [HttpGet]
+    [HttpGet("dashboard")]
     [ProducesResponseType(typeof(CreatedResult), StatusCodes.Status200OK)]
     [MapToApiVersion("1.0")]
     public async Task<ActionResult<ModelsBuilderViewModel>> GetDashboard() => await Task.FromResult(Ok(_modelsBuilderViewModelFactory.Create()));
