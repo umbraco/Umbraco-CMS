b7c3d0515006dec44274df8cbeb0e7b113106179
Mads Rasmussen <madsr@hey.com>
2022-10-26 15:14:35 +0200

    hardcode path until we have found a way to make umbracoPath work with msw

--- src/Umbraco.Web.UI.Client/src/core/mocks/domains/examine-management.handlers.ts
+++ src/Umbraco.Web.UI.Client/src/core/mocks/domains/examine-management.handlers.ts
@@ -3,6 +3,11 @@ import { rest } from 'msw';
 import { Indexer, Searcher, getIndexByName, getIndexers, SearchResult, searchResFromIndex } from '../data/examine.data';
 import { umbracoPath } from '@umbraco-cms/utils';
 
+const test = '/search/index/:indexName';
+
+// use a regex to replace :indexName with {indexName}
+const regex = new RegExp(test.replace(/:[^/]+/g, '([^/]+)'));
+
 export const handlers = [
 	rest.get(umbracoPath('/search/index'), (_req, res, ctx) => {
 		return res(
@@ -12,7 +17,8 @@ export const handlers = [
 		);
 	}),
 
-	rest.get(umbracoPath('/search/index/:indexName'), (_req, res, ctx) => {
+	// TODO: when using the umbracoPath helper you have to write parameters like {indexName}. MSW wants parameters as :indexName
+	rest.get('umbraco/backoffice/search/index/:indexName', (_req, res, ctx) => {
 		const query = _req.url.searchParams.get('query');
 		const indexName = _req.params.indexName as string;
 		if (!indexName) return;
@@ -25,7 +31,8 @@ export const handlers = [
 		}
 	}),
 
-	rest.post(umbracoPath('/search/index/:indexName/rebuild'), async (_req, res, ctx) => {
+	// TODO: when using the umbracoPath helper you have to write parameters like {indexName}. MSW wants parameters as :indexName
+	rest.post('/umbraco/backoffice/search/index/:indexName/rebuild', async (_req, res, ctx) => {
 		await new Promise((resolve) => setTimeout(resolve, (Math.random() + 1) * 1000)); // simulate a delay of 1-2 seconds
 
 		const indexName = _req.params.indexName as string;
@@ -68,7 +75,8 @@ export const handlers = [
 	}),
 	*/
 
-	rest.get(umbracoPath('/search/searcher/:searcherName'), (_req, res, ctx) => {
+	// TODO: when using the umbracoPath helper you have to write parameters like {indexName}. MSW wants parameters as :indexName
+	rest.get('/umbraco/backoffice/search/searcher/:searcherName', (_req, res, ctx) => {
 		const query = _req.url.searchParams.get('query');
 		const take = _req.url.searchParams.get('take');
 
