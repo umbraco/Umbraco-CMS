42e908dab06ec40dcebb20eb174a127bfda97657
Nikolaj <nikolajlauridsen@protonmail.ch>
2022-10-26 09:53:33 +0200

    Update AllTelemetryController

--- src/Umbraco.Cms.ManagementApi/Controllers/Telemetry/AllTelemetryController.cs
+++ src/Umbraco.Cms.ManagementApi/Controllers/Telemetry/AllTelemetryController.cs
@@ -1,8 +1,8 @@
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Core.Models;
-using Umbraco.Cms.ManagementApi.ViewModels.Analytics;
 using Umbraco.Cms.ManagementApi.ViewModels.Pagination;
+using Umbraco.Cms.ManagementApi.ViewModels.Telemetry;
 
 namespace Umbraco.Cms.ManagementApi.Controllers.Telemetry;
 
@@ -10,14 +10,14 @@ public class AllTelemetryController : TelemetryControllerBase
 {
     [HttpGet]
     [MapToApiVersion("1.0")]
-    [ProducesResponseType(typeof(PagedViewModel<AnalyticsLevelViewModel>), StatusCodes.Status200OK)]
-    public async Task<PagedViewModel<AnalyticsLevelViewModel>> GetAll(int skip, int take)
+    [ProducesResponseType(typeof(PagedViewModel<TelemetryLevelViewModel>), StatusCodes.Status200OK)]
+    public async Task<PagedViewModel<TelemetryLevelViewModel>> GetAll(int skip, int take)
     {
         TelemetryLevel[] levels = Enum.GetValues<TelemetryLevel>();
-        return await Task.FromResult(new PagedViewModel<AnalyticsLevelViewModel>
+        return await Task.FromResult(new PagedViewModel<TelemetryLevelViewModel>
         {
             Total = levels.Length,
-            Items = levels.Skip(skip).Take(take).Select(x => new AnalyticsLevelViewModel { AnalyticsLevel = x }),
+            Items = levels.Skip(skip).Take(take).Select(x => new TelemetryLevelViewModel { TelemetryLevel = x }),
         });
     }
 }
