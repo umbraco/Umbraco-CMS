62428ad83734dfe3f5a8934ad639d4518e799553
Jesper Møller Jensen <jmj@umbraco.dk>
2022-10-27 14:59:22 +0200

    fix user group handler

--- src/Umbraco.Web.UI.Client/src/core/mocks/domains/user-groups.handlers.ts
+++ src/Umbraco.Web.UI.Client/src/core/mocks/domains/user-groups.handlers.ts
@@ -4,7 +4,7 @@ import type { UserGroupDetails } from '@umbraco-cms/models';
 
 export const handlers = [
 	rest.get('/umbraco/backoffice/user-groups/list/items', (req, res, ctx) => {
-		const items = fakeData;
+		const items = umbUserGroupsData.getItems('userGroup');
 
 		const response = {
 			total: items.length,
