1f65e4a79c45322db3ceea7bfa0ef7dccbea0f45
Jesper Møller Jensen <jmj@umbraco.dk>
2022-10-27 14:38:33 +0200

    section picker

--- src/Umbraco.Web.UI.Client/src/backoffice/sections/users/picker-layout-section.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/sections/users/picker-layout-section.element.ts
@@ -1,90 +1,127 @@
+import { UmbContextConsumerMixin } from '@umbraco-cms/context-api';
 import { UUITextStyles } from '@umbraco-ui/uui-css';
 import { css, html } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
 import { UmbModalLayoutElement } from '../../../core/services/modal/layouts/modal-layout.element';
+import { UmbObserverMixin } from '@umbraco-cms/observable-api';
+import { UmbPickerData } from './picker.element';
+import { umbExtensionsRegistry } from '@umbraco-cms/extensions-registry';
+import { ManifestSection } from '@umbraco-cms/models';
 
 @customElement('umb-picker-layout-section')
-export class UmbPickerLayoutSectionElement extends UmbModalLayoutElement {
+export class UmbPickerLayoutSectionElement extends UmbContextConsumerMixin(
+	UmbObserverMixin(UmbModalLayoutElement<UmbPickerData>)
+) {
 	static styles = [
 		UUITextStyles,
 		css`
+			uui-input {
+				width: 100%;
+			}
+			hr {
+				border: none;
+				border-bottom: 1px solid var(--uui-color-divider);
+				margin: 16px 0;
+			}
+			#item-list {
+				display: flex;
+				flex-direction: column;
+				gap: var(--uui-size-1);
+			}
 			.item {
 				color: var(--uui-color-interactive);
-				display: flex;
+				display: grid;
+				grid-template-columns: var(--uui-size-8) 1fr;
+				padding: var(--uui-size-4) var(--uui-size-2);
+				gap: var(--uui-size-space-5);
 				align-items: center;
-				padding: var(--uui-size-2);
-				gap: var(--uui-size-space-4);
+				border-radius: var(--uui-size-2);
 				cursor: pointer;
 			}
-			.item:hover {
+			.item.selected {
+				background-color: var(--uui-color-selected);
+				color: var(--uui-color-selected-contrast);
+			}
+			.item:not(.selected):hover {
 				background-color: var(--uui-color-surface-emphasis);
 				color: var(--uui-color-interactive-emphasis);
 			}
-			uui-icon[name='check'] {
-				color: var(--uui-color-positive);
+			.item.selected:hover {
+				background-color: var(--uui-color-selected-emphasis);
+			}
+			.item uui-icon {
+				width: 100%;
+				box-sizing: border-box;
+				display: flex;
+				height: fit-content;
 			}
 		`,
 	];
+
 	@state()
 	private _selection: Array<string> = [];
+
+	@state()
+	private _sections: Array<ManifestSection> = [];
+
+	connectedCallback(): void {
+		super.connectedCallback();
+		umbExtensionsRegistry.extensionsOfType('section').subscribe((sections: Array<ManifestSection>) => {
+			this._sections = sections;
+		});
+	}
 
 	private _submit() {
 		this.modalHandler?.close({ selection: this._selection });
 	}
 
 	private _close() {
-		this.modalHandler?.close({ selection: this._selection });
+		this.modalHandler?.close();
 	}
 
-	private tempData = [
-		{
-			key: '1',
-			name: 'Content',
-		},
-		{
-			key: '2',
-			name: 'Media',
-		},
-		{
-			key: '3',
-			name: 'Settings',
-		},
-	];
-
-	private _handleKeydown(e: KeyboardEvent, key: string) {
+	private _handleKeydown(e: KeyboardEvent, alias: string) {
 		if (e.key === 'Enter') {
-			this._handleItemClick(key);
+			this._handleItemClick(alias);
 		}
 	}
 
-	private _handleItemClick(clickedKey: string) {
-		if (this._isSelected(clickedKey)) {
-			this._selection = this._selection.filter((key) => key !== clickedKey);
+	private _handleItemClick(clickedAlias: string) {
+		if (this.data?.multiple) {
+			if (this._isSelected(clickedAlias)) {
+				this._selection = this._selection.filter((alias) => alias !== clickedAlias);
+			} else {
+				this._selection.push(clickedAlias);
+			}
 		} else {
-			this._selection.push(clickedKey);
+			this._selection = [clickedAlias];
 		}
+
 		this.requestUpdate('_selection');
 	}
 
-	private _isSelected(key: string): boolean {
-		return this._selection.includes(key);
+	private _isSelected(alias: string): boolean {
+		return this._selection.includes(alias);
 	}
+	//todo: save section aliasess in array
 
 	render() {
 		return html`
 			<umb-editor-entity-layout headline="Select sections">
 				<uui-box>
-					${this.tempData.map(
-						(item) => html`
-							<div
-								@click=${() => this._handleItemClick(item.key)}
-								@keydown=${(e: KeyboardEvent) => this._handleKeydown(e, item.key)}
-								class="item">
-								<uui-icon name="${this._isSelected(item.key) ? 'check' : 'add'}"></uui-icon>
-								<span>${item.name}</span>
-							</div>
-						`
-					)}
+					<uui-input></uui-input>
+					<hr />
+					<div id="item-list">
+						${this._sections.map(
+							(item) => html`
+								<div
+									@click=${() => this._handleItemClick(item.alias)}
+									@keydown=${(e: KeyboardEvent) => this._handleKeydown(e, item.alias)}
+									class=${this._isSelected(item.alias) ? 'item selected' : 'item'}>
+									<span>${item.meta.label}</span>
+								</div>
+							`
+						)}
+					</div>
 				</uui-box>
 				<div slot="actions">
 					<uui-button label="Close" @click=${this._close}></uui-button>
