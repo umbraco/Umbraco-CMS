d432d165de02814d12d4e7736458b69ab60137d8
Nikolaj <nikolajlauridsen@protonmail.ch>
2022-10-31 09:17:05 +0100

    Update UnPublishedContentPropertyCacheCompressionOptions to also accept Nvarchar

--- src/Umbraco.Core/PropertyEditors/UnPublishedContentPropertyCacheCompressionOptions.cs
+++ src/Umbraco.Core/PropertyEditors/UnPublishedContentPropertyCacheCompressionOptions.cs
@@ -9,7 +9,7 @@ public class UnPublishedContentPropertyCacheCompressionOptions : IPropertyCacheC
 {
     public bool IsCompressed(IReadOnlyContentBase content, IPropertyType propertyType, IDataEditor dataEditor, bool published)
     {
-        if (!published && propertyType.SupportsPublishing && propertyType.ValueStorageType == ValueStorageType.Ntext)
+        if (!published && propertyType.SupportsPublishing && propertyType.ValueStorageType is ValueStorageType.Ntext or ValueStorageType.Nvarchar)
         {
             // Only compress non published content that supports publishing and the property is text
             return true;
