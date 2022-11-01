c12beb010d3461c2b91edbb3d7864aab7645aa65
nikolajlauridsen <nikolajlauridsen@protonmail.ch>
2022-11-01 09:48:05 +0100

    Refactor column migration for reuse

--- src/Umbraco.Infrastructure/Migrations/Upgrade/V_12_0_0/UseNvarcharInsteadOfNText.cs
+++ src/Umbraco.Infrastructure/Migrations/Upgrade/V_12_0_0/UseNvarcharInsteadOfNText.cs
@@ -1,4 +1,5 @@
-﻿using System.Text;
+﻿using System.Linq.Expressions;
+using System.Text;
 using NPoco;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Infrastructure.Persistence;
@@ -17,33 +18,41 @@ public class UseNvarcharInsteadOfNText : MigrationBase
 
     protected override void Migrate()
     {
-        // Rename old Column
+        MigrateNtextColumn<PropertyDataDto>("textValue", Constants.DatabaseSchema.Tables.PropertyData, x => x.TextValue);
+    }
+
+    private void MigrateNtextColumn<TDto>(string columnName, string tableName, Expression<Func<TDto, object?>> fieldSelector)
+    {
+        var oldColumnName = $"Old{columnName}";
+
+        // Rename the column so we can create the new one and copy over the data.
         Rename
-            .Column("textValue")
-            .OnTable(Constants.DatabaseSchema.Tables.PropertyData)
-            .To("oldTextValue")
+            .Column(columnName)
+            .OnTable(tableName)
+            .To(oldColumnName)
             .Do();
 
         // Create new column with the correct type
+        // This should change between every column migration, so we'll request it each time.
         var columns = SqlSyntax.GetColumnsInSchema(Context.Database).ToList();
-        AddColumnIfNotExists<PropertyDataDto>(columns, "textValue");
+        AddColumnIfNotExists<TDto>(columns, columnName);
 
-        // Copy over data NPOCO doesn't support this for some insane reason, so we'll have to do it like so
+        // Copy over data NPOCO doesn't support this for some reason, so we'll have to do it like so
         // While we're add it we'll also set all the old values to be NULL since it's recommended here:
         // https://learn.microsoft.com/en-us/sql/t-sql/data-types/ntext-text-and-image-transact-sql?view=sql-server-ver16#remarks
         StringBuilder queryBuilder = new StringBuilder()
-            .AppendLine($"UPDATE {Constants.DatabaseSchema.Tables.PropertyData}")
+            .AppendLine($"UPDATE {tableName}")
             .AppendLine("SET")
-            .AppendLine($"\t{Database.SqlContext.SqlSyntax.GetFieldNameForUpdate<PropertyDataDto>(x => x.TextValue)} = {SqlSyntax.GetQuotedTableName(Constants.DatabaseSchema.Tables.PropertyData)}.{SqlSyntax.GetQuotedColumnName("oldTextValue")},")
-            .AppendLine($"\t{SqlSyntax.GetQuotedColumnName("oldTextValue")} = NULL");
+            .AppendLine($"\t{SqlSyntax.GetFieldNameForUpdate(fieldSelector)} = {SqlSyntax.GetQuotedTableName(tableName)}.{SqlSyntax.GetQuotedColumnName(oldColumnName)},")
+            .AppendLine($"\t{SqlSyntax.GetQuotedColumnName(oldColumnName)} = NULL");
 
         Sql<ISqlContext> copyDataQuery = Database.SqlContext.Sql(queryBuilder.ToString());
         Database.Execute(copyDataQuery);
 
         // Delete old column
         Delete
-            .Column("oldTextValue")
-            .FromTable(Constants.DatabaseSchema.Tables.PropertyData)
+            .Column(oldColumnName)
+            .FromTable(tableName)
             .Do();
     }
 }
