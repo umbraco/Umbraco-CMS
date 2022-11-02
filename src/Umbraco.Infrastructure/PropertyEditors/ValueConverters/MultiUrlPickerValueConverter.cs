eed7c1b884730e87742d46b702f509ad78afffc7
Rasmus John Pedersen <mail@rjp.dk>
2022-11-02 14:47:59 +0100

    fix: mark MultiUrlPickerValueConverter with DefaultValueConverter

--- src/Umbraco.Infrastructure/PropertyEditors/ValueConverters/MultiUrlPickerValueConverter.cs
+++ src/Umbraco.Infrastructure/PropertyEditors/ValueConverters/MultiUrlPickerValueConverter.cs
@@ -12,6 +12,7 @@ using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Core.PropertyEditors.ValueConverters;
 
+[DefaultPropertyValueConverter(typeof(JsonValueConverter))]
 public class MultiUrlPickerValueConverter : PropertyValueConverterBase
 {
     private readonly IJsonSerializer _jsonSerializer;
