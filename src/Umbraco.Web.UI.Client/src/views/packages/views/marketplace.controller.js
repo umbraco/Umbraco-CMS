1c74485926d2dddcde24feca6e9f81245051c570
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2022-11-09 09:39:20 +0100

    add handling of dynamic querystring params

--- src/Umbraco.Web.UI.Client/src/views/packages/views/marketplace.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/packages/views/marketplace.controller.js
@@ -4,10 +4,15 @@
   function MarketplaceController($sce) {
 
     var vm = this;
-    var marketplaceUrl = 'https://dev.marketplace.umbraco.com';
+    var marketplaceUrl = new URL('https://dev.marketplace.umbraco.com');
+    marketplaceUrl.searchParams.set('umbversion', Umbraco.Sys.ServerVariables.application.version);
+    marketplaceUrl.searchParams.set('style', 'backoffice');
 
-    vm.onLoad = onLoad;
-    vm.marketplaceUrl = $sce.trustAsResourceUrl(marketplaceUrl);
+    function init() {
+      vm.marketplaceUrl = $sce.trustAsResourceUrl(marketplaceUrl.toString());
+    }
+
+    init();
   }
 
   angular.module("umbraco").controller("Umbraco.Editors.Packages.MarketplaceController", MarketplaceController);
