d8e9c28c4460811609349b4e32e83df966b98589
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2022-11-09 09:24:48 +0100

    add ng-controller with utitlities for future PostMessage API

---
+++ src/Umbraco.Web.UI.Client/src/views/packages/views/marketplace.controller.js
@@ -0,0 +1,37 @@
+(function () {
+  "use strict";
+
+  function MarketplaceController($sce) {
+
+    var vm = this;
+    var marketplaceUrl = 'https://dev.marketplace.umbraco.com';
+
+    vm.onLoad = onLoad;
+    vm.marketplaceUrl = $sce.trustAsResourceUrl(marketplaceUrl);
+
+    function onLoad(evt) {
+      // Listen for PostMessage from the iframe to know when it's loaded
+      evt.target.addEventListener("message", onMessage);
+    }
+
+    function onMessage(event) {
+      if (event.origin !== vm.marketplaceUrl) {
+        return;
+      }
+
+      if (event.data === "loaded") {
+        // Remove the listener once we know the iframe is loaded
+        window.removeEventListener("message", onMessage);
+
+        onLoaded();
+      }
+    }
+
+    function onLoaded() {
+      console.log('Marketplace is loaded - we need to do something here');
+    }
+  }
+
+  angular.module("umbraco").controller("Umbraco.Editors.Packages.MarketplaceController", MarketplaceController);
+
+})();
