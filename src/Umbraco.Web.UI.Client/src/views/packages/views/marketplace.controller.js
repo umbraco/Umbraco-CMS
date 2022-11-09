e68612dab97b39f6add02b22e8ba1475c47c67c4
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2022-11-09 09:35:35 +0100

    remove iframe postmessage logic for time being

--- src/Umbraco.Web.UI.Client/src/views/packages/views/marketplace.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/packages/views/marketplace.controller.js
@@ -8,28 +8,6 @@
 
     vm.onLoad = onLoad;
     vm.marketplaceUrl = $sce.trustAsResourceUrl(marketplaceUrl);
-
-    function onLoad(evt) {
-      // Listen for PostMessage from the iframe to know when it's loaded
-      evt.target.addEventListener("message", onMessage);
-    }
-
-    function onMessage(event) {
-      if (event.origin !== vm.marketplaceUrl) {
-        return;
-      }
-
-      if (event.data === "loaded") {
-        // Remove the listener once we know the iframe is loaded
-        window.removeEventListener("message", onMessage);
-
-        onMarketplaceLoaded();
-      }
-    }
-
-    function onMarketplaceLoaded() {
-      console.log('Marketplace is loaded - we need to do something here');
-    }
   }
 
   angular.module("umbraco").controller("Umbraco.Editors.Packages.MarketplaceController", MarketplaceController);
