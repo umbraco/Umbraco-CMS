7c99c6f1c335858e0d7fa81df583231edf4a14c4
Bjarke Berg <mail@bergmania.dk>
2022-11-10 14:53:41 +0100

    Obsoleted and deleted internal migrations + Enabled breaking changes checks agains v11.0.0-rc1

--- src/Umbraco.Infrastructure/Migrations/Upgrade/V_8_0_0/PropertyEditorsMigration.cs
+++ src/Umbraco.Infrastructure/Migrations/Upgrade/V_8_0_0/PropertyEditorsMigration.cs
@@ -4,6 +4,7 @@ using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Infrastructure.Migrations.Upgrade.V_8_0_0;
 
+[Obsolete("This is not used anymore and will be removed in Umbraco 13")]
 public class PropertyEditorsMigration : MigrationBase
 {
     public PropertyEditorsMigration(IMigrationContext context)
