40d5cd7815c9cc6c33843c37a3c7ea33a714fb66
Niels Lyngsø <niels.lyngso@gmail.com>
2022-11-10 21:18:29 +0100

    simpler media item filter

--- src/Umbraco.Web.UI.Client/src/common/filters/mediaPickerResolver.filter.js
+++ src/Umbraco.Web.UI.Client/src/common/filters/mediaPickerResolver.filter.js
@@ -1,17 +1,36 @@
 ﻿(function () {
   "use strict";
 
-  function mediaPickerResolverFilterService(eventsService) {
+  function mediaItemResolverFilterService(mediaResource, eventsService) {
     
+    var mediaKeysRequest = [];
+    var mediaItemCache = [];
+
     var service = {
-      mediaKeysRequest: [],
-      mediaEntryCache: []
+      
+      getByKey: function (key) {
+        // Is it cached, then get that:
+        const cachedMediaItem = mediaItemCache.find(cache => key === cache.key);
+        if(cachedMediaItem) {
+          return cachedMediaItem;
+        }
+
+        // check its not already being loaded, and then start loading:
+        if(mediaKeysRequest.indexOf(key) === -1) {
+          mediaKeysRequest.push(key);
+          mediaResource.getById(key).then(function (mediaItem) {
+            mediaItemCache.push(mediaItem);
+          });
+        }
+
+        return null;
+      }
     };
 
     eventsService.on("editors.media.saved", function (name, args) {
-      const index = service.mediaEntryCache.findIndex(cache => cache.key === args.media.key);
+      const index = mediaItemCache.findIndex(cache => cache.key === args.media.key);
       if(index !== -1) {
-        service.mediaEntryCache[index] = args.media;
+        mediaItemCache[index] = args.media;
       }
     });
 
@@ -19,42 +38,25 @@
 
   }
   
-  angular.module("umbraco.filters").factory("mediaPickerResolverFilterService", mediaPickerResolverFilterService);
+  angular.module("umbraco.filters").factory("mediaItemResolverFilterService", mediaItemResolverFilterService);
 
 
   // Filter to take a node id and grab it's name instead
   // Usage: {{ pickerAlias | ncNodeName }}
-  angular.module("umbraco.filters").filter("mediaPickerResolverFilter", function (mediaResource, mediaPickerResolverFilterService) {
+  angular.module("umbraco.filters").filter("mediaItemResolver", function (mediaItemResolverFilterService) {
 
-    mediaPickerResolverFilter.$stateful = true;
-    function mediaPickerResolverFilter(input) {
-
-
+    mediaItemResolverFilter.$stateful = true;
+    function mediaItemResolverFilter(input) {
 
       // Check we have a value at all
-      if (typeof input === 'object') {
-        if(input.length > 0) {
-          var keys = input.map(x => x.mediaKey);
-
-          const notLoadedKeys = keys.filter(key => mediaPickerResolverFilterService.mediaEntryCache.find(cache => key === cache.key) === undefined);
-          const notRequestedKeys = notLoadedKeys.filter(key => mediaPickerResolverFilterService.mediaKeysRequest.indexOf(key) === -1);
-          
-          mediaPickerResolverFilterService.mediaKeysRequest = [...mediaPickerResolverFilterService.mediaKeysRequest, ...notRequestedKeys];
-
-          if(notRequestedKeys.length > 0) {
-            mediaResource.getById(notRequestedKeys[0]).then(function (mediaEntities) {
-              mediaPickerResolverFilterService.mediaEntryCache.push(mediaEntities);
-            });
-          } else {
-            return mediaPickerResolverFilterService.mediaEntryCache.filter(cache => keys.indexOf(cache.key) !== -1);
-          }
-        }
+      if (typeof input === 'string' && input.length > 0) {
+        return mediaItemResolverFilterService.getByKey(input);
       }
 
-      return [];
+      return null;
     }
 
-    return mediaPickerResolverFilter;
+    return mediaItemResolverFilter;
 
   });
 
