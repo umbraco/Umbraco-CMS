31dcbb9e6ea9e9646d4ec9026455aaa1aa489484
Mads Rasmussen <madsr@hey.com>
2022-11-17 10:21:48 +0100

    add media temp data

---
+++ src/Umbraco.Web.UI.Client/src/core/mocks/data/media.data.ts
@@ -0,0 +1,77 @@
+import { UmbData } from './data';
+import { ContentTreeItem, PagedContentTreeItem } from '@umbraco-cms/backend-api';
+import type { MediaDetails } from '@umbraco-cms/models';
+
+export const data: Array<MediaDetails> = [
+	{
+		key: 'f2f81a40-c989-4b6b-84e2-057cecd3adc1',
+		name: 'Media 1',
+		type: 'media',
+		icon: 'picture',
+		parentKey: 'c0858d71-52be-4bb2-822f-42fa0c9a1ea5',
+		hasChildren: false,
+		properties: [
+			{
+				alias: 'myMediaHeadline',
+				label: 'Media Headline',
+				description: 'Text string property',
+				dataTypeKey: 'dt-1',
+			},
+		],
+		data: [
+			{
+				alias: 'myMediaHeadline',
+				value: 'The daily life at Umbraco HQ',
+			},
+		],
+		variants: [],
+	},
+	{
+		key: '69431027-8867-45bf-a93b-72bbdabfb177',
+		type: 'media',
+		name: 'Media 2',
+		icon: 'picture',
+		parentKey: 'c0858d71-52be-4bb2-822f-42fa0c9a1ea5',
+		hasChildren: false,
+		properties: [
+			{
+				alias: 'myMediaDescription',
+				label: 'Description',
+				description: 'Textarea property',
+				dataTypeKey: 'dt-2',
+			},
+		],
+		data: [
+			{
+				alias: 'myMediaDescription',
+				value: 'Every day, a rabbit in a military costume greets me at the front door',
+			},
+		],
+		variants: [],
+	},
+];
+
+// Temp mocked database
+class UmbDataTypeData extends UmbData<MediaDetails> {
+	constructor() {
+		super(data);
+	}
+
+	getTreeRoot(): PagedContentTreeItem {
+		const items = this.data.filter((item) => item.parentKey === null);
+		const total = items.length;
+		return { items, total };
+	}
+
+	getTreeItemChildren(key: string): PagedContentTreeItem {
+		const items = this.data.filter((item) => item.key === null);
+		const total = items.length;
+		return { items, total };
+	}
+
+	getTreeItem(keys: Array<string>): Array<ContentTreeItem> {
+		return this.data.filter((item) => keys.includes(item.key ?? ''));
+	}
+}
+
+export const umbDataTypeData = new UmbDataTypeData();
