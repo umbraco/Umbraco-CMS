8f915e16b1e50dd063522ede1b4c6ca333d22768
Lone Iversen <108085781+loivsen@users.noreply.github.com>
2022-11-21 11:02:44 +0100

    shortcut

---
+++ src/Umbraco.Web.UI.Client/src/core/services/index.ts
@@ -0,0 +1,7 @@
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+export { UmbModalService } from './modal';
+export { UmbNotificationService } from './notification';
+export type { UmbNotificationDefaultData } from './notification/layouts/default';
