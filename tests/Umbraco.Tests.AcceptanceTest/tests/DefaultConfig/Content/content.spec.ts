168abc7729a139e0698032ec174a6feeb549bcea
kjac <kja@umbraco.dk>
2022-11-22 08:00:28 +0100

    Fix rollback playwright text

--- tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Content/content.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Content/content.spec.ts
@@ -271,6 +271,7 @@ test.describe('Content tests', () => {
         .addVariant()
           .withName(initialNodeName)
           .withSave(true)
+          .withPublish(true)
         .done()
         .build();
     await umbracoApi.content.save(rootContentNode);
@@ -285,7 +286,7 @@ test.describe('Content tests', () => {
     await page.keyboard.press('Backspace');
 
     await umbracoUi.setEditorHeaderName(newNodeName);
-    await umbracoUi.clickElement(umbracoUi.getButtonByLabelKey(ConstantHelper.buttons.saveAndPublish));
+    await umbracoUi.clickElement(umbracoUi.getButtonByLabelKey(ConstantHelper.buttons.save));
     await umbracoUi.isSuccessNotificationVisible();
     await page.locator('span:has-text("×")').click();
 
@@ -298,7 +299,7 @@ test.describe('Content tests', () => {
     await page.locator('[action="vm.rollback()"]').click();
 
     await umbracoUi.refreshContentTree();
-    await expect(page.locator('.umb-badge >> text=Save')).toBeVisible();
+    await expect(page.locator('.umb-badge >> text=Save')).toHaveCount(2);
     await expect(page.locator('.umb-badge >> text=RollBack')).toBeVisible();
     const node = await umbracoUi.getTreeItem("content", [initialNodeName])
     await expect(node).toBeVisible();
