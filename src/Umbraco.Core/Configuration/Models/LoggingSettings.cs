906fc2dadfa2348619388bf39de4fb4533bd6885
Bjarke Berg <mail@bergmania.dk>
2022-11-28 08:47:54 +0100

    Allow changing logging directory from configuration

--- src/Umbraco.Core/Configuration/Models/LoggingSettings.cs
+++ src/Umbraco.Core/Configuration/Models/LoggingSettings.cs
@@ -2,6 +2,8 @@
 // See LICENSE for more details.
 
 using System.ComponentModel;
+using Microsoft.Extensions.Hosting;
+using Umbraco.Cms.Core.Extensions;
 
 namespace Umbraco.Cms.Core.Configuration.Models;
 
@@ -12,10 +14,28 @@ namespace Umbraco.Cms.Core.Configuration.Models;
 public class LoggingSettings
 {
     internal const string StaticMaxLogAge = "1.00:00:00"; // TimeSpan.FromHours(24);
+    internal const string StaticLoggingDirectory = Constants.SystemDirectories.LogFiles;
 
     /// <summary>
     ///     Gets or sets a value for the maximum age of a log file.
     /// </summary>
     [DefaultValue(StaticMaxLogAge)]
     public TimeSpan MaxLogAge { get; set; } = TimeSpan.Parse(StaticMaxLogAge);
+
+    /// <summary>
+    ///     Gets or sets the folder to use for log files
+    /// </summary>
+    [DefaultValue(StaticLoggingDirectory)]
+    public string LoggingDirectory { get; set; } = StaticLoggingDirectory;
+
+    public string GetAbsoluteLoggingPath(IHostEnvironment hostEnvironment)
+    {
+        var dir = LoggingDirectory;
+        if (dir.StartsWith("~/"))
+        {
+            hostEnvironment.MapPathContentRoot(dir);
+        }
+
+        return dir;
+    }
 }
