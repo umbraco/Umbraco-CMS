254f6a59be62c90a21438717d280c7aaa775398a
Lone Iversen <108085781+loivsen@users.noreply.github.com>
2022-11-28 10:04:05 +0100

    update

--- src/Umbraco.Web.UI.Client/src/backoffice/dashboards/performance-profiling/dashboard-performance-profiling.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/dashboards/performance-profiling/dashboard-performance-profiling.element.ts
@@ -26,14 +26,17 @@ export class UmbDashboardPerformanceProfilingElement extends UmbContextConsumerM
 	];
 
 	@state()
-	private _profilingStatus = false;
+	private _profilingStatus?: boolean;
+
+	@state()
+	private _profilingPerfomance = false;
 
 	private _notificationService?: UmbNotificationService;
 
 	private async _getProfilingStatus() {
 		try {
 			const status = await ProfilingResource.getProfilingStatus();
-			status.enabled === true ? (this._profilingStatus = status.enabled) : (this._profilingStatus = false);
+			this._profilingStatus = status.enabled;
 		} catch (e) {
 			if (e instanceof ApiError) {
 				const error = e as ProblemDetails;
@@ -45,41 +48,69 @@ export class UmbDashboardPerformanceProfilingElement extends UmbContextConsumerM
 
 	constructor() {
 		super();
-		this._getProfilingStatus();
 		this.consumeAllContexts(['umbNotificationService'], (instances) => {
 			this._notificationService = instances['umbNotificationService'];
 		});
+	}
+
+	connectedCallback(): void {
+		super.connectedCallback();
+		this._getProfilingStatus();
+		if (localStorage.getItem('profilingPerformance') === 'true') this._profilingPerfomance = true;
+		else this._profilingPerfomance = false;
+	}
+
+	private _changeProfilingPerformance() {
+		this._profilingPerfomance = !this._profilingPerfomance;
+		this._profilingPerfomance
+			? localStorage.setItem('profilingPerformance', 'true')
+			: localStorage.setItem('profilingPerformance', 'false');
 	}
 
 	render() {
 		return html`
 			<uui-box>
 				<h1>Performance Profiling</h1>
-				<p>
-					Umbraco currently runs in debug mode. This means you can use the built-in performance profiler to assess the
-					performance when rendering pages.
-				</p>
-				<p>
-					If you want to activate the profiler for a specific page rendering, simply add
-					<strong>umbDebug=true</strong> to the querystring when requesting the page.
-				</p>
+				${this._profilingStatus
+					? html`
+							<p>
+								Umbraco currently runs in debug mode. This means you can use the built-in performance profiler to assess
+								the performance when rendering pages.
+							</p>
+							<p>
+								If you want to activate the profiler for a specific page rendering, simply add
+								<strong>umbDebug=true</strong> to the querystring when requesting the page.
+							</p>
 
-				<p>
-					If you want the profiler to be activated by default for all page renderings, you can use the toggle below. It
-					will set a cookie in your browser, which then activates the profiler automatically. In other words, the
-					profiler will only be active by default in your browser - not everyone else's.
-				</p>
+							<p>
+								If you want the profiler to be activated by default for all page renderings, you can use the toggle
+								below. It will set a cookie in your browser, which then activates the profiler automatically. In other
+								words, the profiler will only be active by default in your browser - not everyone else's.
+							</p>
 
-				<uui-toggle
-					label="Activate the profiler by default"
-					label-position="left"
-					.checked="${this._profilingStatus}"></uui-toggle>
-				<h4>Friendly reminder</h4>
-				<p>
-					You should never let a production site run in debug mode. Debug mode is turned off by setting
-					Umbraco:CMS:Hosting:Debug to false in appsettings.json, appsettings.{Environment}.json or via an environment
-					variable.
-				</p>
+							<uui-toggle
+								label="Activate the profiler by default"
+								label-position="left"
+								.checked="${this._profilingPerfomance}"
+								@change="${this._changeProfilingPerformance}"></uui-toggle>
+
+							<h4>Friendly reminder</h4>
+							<p>
+								You should never let a production site run in debug mode. Debug mode is turned off by setting
+								Umbraco:CMS:Hosting:Debug to false in appsettings.json, appsettings.{Environment}.json or via an
+								environment variable.
+							</p>
+					  `
+					: html`
+							<p>
+								Umbraco currently does not run in debug mode, so you can't use the built-in profiler. This is how it
+								should be for a production site.
+							</p>
+							<p>
+								Debug mode is turned on by setting <b>debug="true"</b> on the <b>&lt;compilation /&gt;</b> element in
+								web.config.
+							</p>
+					  `}
 			</uui-box>
 		`;
 	}
