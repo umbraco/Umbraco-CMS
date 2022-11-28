7342fde17f04c60d1a5f3da897640bcc5eaa3eef
Nikolaj <nikolajlauridsen@protonmail.ch>
2022-11-28 14:42:22 +0100

    Revert "Add status endpoint"
    This reverts commit b594ad8ccf30936f579a239f05f3ca499e2a15eb.

--- src/Umbraco.Cms.ManagementApi/ViewModels/RedirectManagement/RedirectStatusViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Umbraco.Cms.ManagementApi.ViewModels.RedirectManagement;
-
-public class RedirectStatusViewModel
-{
-    public bool Enabled { get; set; }
-
-    public bool UserIsAdmin { get; set; }
-}
