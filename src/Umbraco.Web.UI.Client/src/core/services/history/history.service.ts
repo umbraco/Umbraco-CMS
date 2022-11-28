9b73c7a359d8f668c8b7c2ad77ba29925a050437
Jesper Møller Jensen <jmj@umbraco.dk>
2022-11-28 11:05:43 +0100

    prevent duplicates in history

--- src/Umbraco.Web.UI.Client/src/core/services/history/history.service.ts
+++ src/Umbraco.Web.UI.Client/src/core/services/history/history.service.ts
@@ -9,19 +9,7 @@ export type UmbHistoryItem = {
 };
 
 class UmbHistoryService {
-	private _history: BehaviorSubject<Array<UmbHistoryItem>> = new BehaviorSubject(<Array<UmbHistoryItem>>[
-		{ label: 'Users grid', path: 'section/users/view/users/overview/grid' },
-		{ label: ['User', 'Nat Linnane'], path: 'section/users/view/users/user/50f184d4-71f3-4a43-b8be-7a36340fbd0d' },
-		{
-			label: ['User Group', 'Administrator'],
-			path: 'section/users/view/users/userGroup/10000000-0000-0000-0000-000000000000',
-		},
-		{ label: 'About us', path: 'section/content/document/74e4008a-ea4f-4793-b924-15e02fd380d2/view/content' },
-		{
-			label: ['Blog', 'Look at this nice history page!'],
-			path: 'section/content/document/74e4008a-ea4f-4793-b924-15e02fd380d2/view/content',
-		},
-	]);
+	private _history: BehaviorSubject<Array<UmbHistoryItem>> = new BehaviorSubject(<Array<UmbHistoryItem>>[]);
 	public readonly history: Observable<Array<UmbHistoryItem>> = this._history.asObservable();
 
 	/**
@@ -31,7 +19,18 @@ class UmbHistoryService {
 	 * @memberof UmbHistoryService
 	 */
 	public push(historyItem: UmbHistoryItem): void {
-		this._history.next([...this._history.getValue(), historyItem]);
+		const history = this._history.getValue();
+		const lastItem = history[history.length - 1];
+
+		// This prevents duplicate entries in the history array.
+		if (!lastItem || lastItem.path !== historyItem.path) {
+			this._history.next([...this._history.getValue(), historyItem]);
+		} else {
+			//Update existing item
+			const newHistory = this._history.getValue();
+			newHistory[history.length - 1] = historyItem;
+			this._history.next(newHistory);
+		}
 	}
 
 	/**
