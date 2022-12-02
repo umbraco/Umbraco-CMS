bc94b2e16c8ee4adc036f65d1b7f00ad582bb573
Kenn Jacobsen <kja@umbraco.dk>
2022-12-02 11:33:02 +0100

    Rename management API from Umbraco.Cms.ManagementApi to Umbraco.Cms.Api.Management (#13512)

---
+++ src/Umbraco.Cms.Api.Management/Controllers/ManagementApiControllerBase.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Management.Filters;
+
+namespace Umbraco.Cms.Api.Management.Controllers;
+
+[ManagementApiJsonConfiguration]
+public class ManagementApiControllerBase : Controller
+{
+
+}
--- src/Umbraco.Cms.ManagementApi/Factories/IIndexViewModelFactory.cs
+++
@@ -1,9 +0,0 @@
-﻿using Examine;
-using Umbraco.Cms.ManagementApi.ViewModels.Search;
-
-namespace Umbraco.Cms.ManagementApi.Factories;
-
-public interface IIndexViewModelFactory
-{
-    IndexViewModel Create(IIndex index);
-}
