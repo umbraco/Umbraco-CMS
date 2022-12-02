03c864760cb56a2aa90561e50b8423e79cb74131
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2022-12-02 14:16:44 +0100

    update msw

--- src/Umbraco.Web.UI.Client/public/mockServiceWorker.js
+++ src/Umbraco.Web.UI.Client/public/mockServiceWorker.js
@@ -2,7 +2,7 @@
 /* tslint:disable */
 
 /**
- * Mock Service Worker (0.49.0).
+ * Mock Service Worker (0.49.1).
  * @see https://github.com/mswjs/msw
  * - Please do NOT modify this file.
  * - Please do NOT serve this file on production.
