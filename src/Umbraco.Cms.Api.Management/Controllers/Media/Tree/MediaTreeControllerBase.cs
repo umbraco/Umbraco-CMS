c8ca13646bdad0da44d6a364a155b645fc8c8747
kjac <kja@umbraco.dk>
2022-12-07 07:22:34 +0100

    Remove test auth attribute from Media

--- src/Umbraco.Cms.Api.Management/Controllers/Media/Tree/MediaTreeControllerBase.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/Media/Tree/MediaTreeControllerBase.cs
@@ -9,7 +9,6 @@ using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Api.Management.Controllers.Tree;
 using Umbraco.Cms.Api.Management.Services.Entities;
 using Umbraco.Cms.Api.Management.ViewModels.Tree;
-using Umbraco.Cms.Web.Common.Authorization;
 using Umbraco.Cms.Api.Management.Routing;
 
 namespace Umbraco.Cms.Api.Management.Controllers.Media.Tree;
@@ -18,7 +17,6 @@ namespace Umbraco.Cms.Api.Management.Controllers.Media.Tree;
 [ApiController]
 [VersionedApiBackOfficeRoute($"{Constants.Web.RoutePath.Tree}/{Constants.UdiEntityType.Media}")]
 [ApiExplorerSettings(GroupName = nameof(Constants.UdiEntityType.Media))]
-[Authorize(Policy = $"New{AuthorizationPolicies.SectionAccessForMediaTree}")]
 public class MediaTreeControllerBase : UserStartNodeTreeControllerBase<ContentTreeItemViewModel>
 {
     private readonly AppCaches _appCaches;
