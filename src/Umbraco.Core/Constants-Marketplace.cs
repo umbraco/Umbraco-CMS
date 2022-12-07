793272bda1132774498bf874245639a99e42e47d
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2022-12-07 10:44:34 +0100

    V10: Umbraco Marketplace (#13511)
    * v11: Umbraco Marketplace replaces packages repo (#13371)
    
    * add lang keys for marketplace
    
    * remove old 'repo' page and deprecate related services
    
    * add new view for Umbraco Marketplace
    
    * optimise margin/padding for other tabs
    
    * mark Our Repository constants as obsolete
    
    * improve css path to iframe slightly with more aliases and classnames
    
    * remove style qs
    
    * update URL of Marketplace
    
    * add ng-controller with utitlities for future PostMessage API
    
    * rename marketplace loaded function
    
    * remove iframe postmessage logic for time being
    
    * add handling of dynamic querystring params
    
    * assume url does not change
    
    * Added support for additional parameters for marketplace
    
    * Update src/JsonSchema/AppSettings.cs
    
    Fix styling issue
    
    Co-authored-by: Ronald Barendse <ronald@barend.se>
    
    * Update src/Umbraco.Core/Configuration/Models/MarketplaceSettings.cs
    
    Fix styling issue
    
    Co-authored-by: Ronald Barendse <ronald@barend.se>
    
    * Update src/Umbraco.Core/Configuration/Models/MarketplaceSettings.cs
    
    Make comment more descriptive
    
    Co-authored-by: Ronald Barendse <ronald@barend.se>
    
    * Update src/Umbraco.Core/Constants-Marketplace.cs
    
    Fix styling issue
    
    Co-authored-by: Ronald Barendse <ronald@barend.se>
    
    Co-authored-by: Bjarke Berg <mail@bergmania.dk>
    Co-authored-by: Ronald Barendse <ronald@barend.se>
    
    * Info panes not loading prerequisites (#13486)
    
    * load relations immediately on init and remove call to non-existing function `appTabChange` on destroy
    
    * load relations immediately on init and remove now not-used eventsService
    
    * update marketplace url to its final, live url
    
    * re-add files to prevent breaking change
    
    Co-authored-by: Bjarke Berg <mail@bergmania.dk>
    Co-authored-by: Ronald Barendse <ronald@barend.se>

---
+++ src/Umbraco.Core/Constants-Marketplace.cs
@@ -0,0 +1,12 @@
+namespace Umbraco.Cms.Core;
+
+public static partial class Constants
+{
+    /// <summary>
+    /// Defines the constants used for the Umbraco Marketplace.
+    /// </summary>
+    public static class Marketplace
+    {
+        public const string Url = "https://marketplace.umbraco.com";
+    }
+}
