aef663af75895c6fea209d1d81dc40229be59212
Jesper Møller Jensen <26099018+JesmoDev@users.noreply.github.com>
2022-12-08 13:44:05 +0100

    add files and folders

--- src/Umbraco.Web.UI.Client/src/backoffice/dashboards/media-management/media-management-grid.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/dashboards/media-management/media-management-grid.element.ts
@@ -1,5 +1,6 @@
 import { UUITextStyles } from '@umbraco-ui/uui-css';
-import { css, html, LitElement } from 'lit';
+import { css, html, LitElement, nothing } from 'lit';
+import { repeat } from 'lit-html/directives/repeat.js';
 import { customElement } from 'lit/decorators.js';
 
 export interface UmbMediaItem {
@@ -9,7 +10,24 @@ export interface UmbMediaItem {
 
 @customElement('umb-media-management-grid')
 export class UmbMediaManagementGridElement extends LitElement {
-	static styles = [UUITextStyles, css``];
+	static styles = [
+		UUITextStyles,
+		css`
+			#media-folders {
+				margin-bottom: var(--uui-size-space-5);
+			}
+			#media-folders,
+			#media-files {
+				display: grid;
+				grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
+				grid-template-rows: repeat(auto-fill, 200px);
+				gap: var(--uui-size-space-5);
+			}
+			.media-item img {
+				object-fit: contain;
+			}
+		`,
+	];
 
 	private _mediaItems: UmbMediaItem[] = [
 		{
@@ -40,22 +58,24 @@ export class UmbMediaManagementGridElement extends LitElement {
 
 	private _renderMediaItem(item: UmbMediaItem) {
 		switch (item.type) {
+			case 'folder':
+				return html`<uui-card-media class="media-item" name="${item.name}"></uui-card-media>`;
 			case 'file':
-				return html`<uui-card-media name="${item.name}" file-ext="${item.type}"></uui-card-media>`;
+				return html`<uui-card-media class="media-item" name="${item.name}" file-ext="${item.type}"></uui-card-media>`;
+			case 'image':
+				return html`<uui-card-media class="media-item" name="${item.name}">
+					<img src="https://picsum.photos/seed/picsum/200/300" alt="" />
+				</uui-card-media>`;
 
 			default:
-				break;
+				return nothing;
 		}
 	}
 
 	render() {
 		return html`
-			<div id="media-folders">
-				${this.mediaFolders.map((folder) => html`<uui-card-media name="${folder.name}"></uui-card-media>`)}
-			</div>
-			<div id="media-files">
-				${this.mediaFiles.map((file) => html` <uui-card-media name="File name" file-ext="txt"></uui-card-media>`)}
-			</div>
+			<div id="media-folders">${repeat(this.mediaFolders, (file) => this._renderMediaItem(file))}</div>
+			<div id="media-files">${repeat(this.mediaFiles, (file) => this._renderMediaItem(file))}</div>
 		`;
 	}
 }
