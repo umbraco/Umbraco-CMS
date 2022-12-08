3afbc37fcd656d95f0a19955d98ebc6be9175d0c
Elitsa Marinovska <elm@umbraco.dk>
2022-12-08 10:41:27 +0100

    Adding viewModels

---
+++ src/Umbraco.Cms.Api.Management/ViewModels/HealthCheck/HealthCheckResultViewModel.cs
@@ -0,0 +1,21 @@
+using Umbraco.Cms.Core.HealthChecks;
+
+namespace Umbraco.Cms.Api.Management.ViewModels.HealthCheck;
+
+public class HealthCheckResultViewModel
+{
+    /// <summary>
+    ///     Gets or sets the status message.
+    /// </summary>
+    public required string Message { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the status type.
+    /// </summary>
+    public StatusResultType ResultType { get; set; }
+
+    /// <summary>
+    ///     This is optional but would allow a developer to get or set a link that is shown as a "Read more" button.
+    /// </summary>
+    public string? ReadMoreLink { get; set; }
+}
