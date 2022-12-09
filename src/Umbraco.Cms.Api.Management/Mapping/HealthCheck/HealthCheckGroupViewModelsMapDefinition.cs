bfbfe50f70360652c160c8f243c2f8898f1bb65b
Elitsa Marinovska <elm@umbraco.dk>
2022-12-09 14:24:01 +0100

    Adding Umbraco.Code comments

--- src/Umbraco.Cms.Api.Management/Mapping/HealthCheck/HealthCheckGroupViewModelsMapDefinition.cs
+++ src/Umbraco.Cms.Api.Management/Mapping/HealthCheck/HealthCheckGroupViewModelsMapDefinition.cs
@@ -11,10 +11,11 @@ public class HealthCheckGroupViewModelsMapDefinition : IMapDefinition
     {
         mapper.Define<HealthCheckStatus, HealthCheckResultViewModel>((source, context) => new HealthCheckResultViewModel() { Message = string.Empty }, Map);
         mapper.Define<Core.HealthChecks.HealthCheck, HealthCheckViewModel>((source, context) => new HealthCheckViewModel() { Name = string.Empty }, Map);
-        mapper.Define<IGrouping<string?, Core.HealthChecks.HealthCheck>, HealthCheckGroupViewModel>((source, context) => new HealthCheckGroupViewModel(), Map);
+        mapper.Define<IGrouping<string?, Core.HealthChecks.HealthCheck>, HealthCheckGroupViewModel>((source, context) => new HealthCheckGroupViewModel() { Checks = new List<HealthCheckViewModel>() }, Map);
         mapper.Define<IEnumerable<IGrouping<string?, Core.HealthChecks.HealthCheck>>, PagedViewModel<HealthCheckGroupViewModel>>((source, context) => new PagedViewModel<HealthCheckGroupViewModel>(), Map);
     }
 
+    // Umbraco.Code.MapAll
     private static void Map(HealthCheckStatus source, HealthCheckResultViewModel target, MapperContext context)
     {
         target.Message = source.Message;
@@ -22,6 +23,7 @@ public class HealthCheckGroupViewModelsMapDefinition : IMapDefinition
         target.ReadMoreLink = source.ReadMoreLink;
     }
 
+    // Umbraco.Code.MapAll
     private static void Map(Core.HealthChecks.HealthCheck source, HealthCheckViewModel target, MapperContext context)
     {
         target.Key = source.Id;
@@ -29,12 +31,14 @@ public class HealthCheckGroupViewModelsMapDefinition : IMapDefinition
         target.Description = source.Description;
     }
 
+    // Umbraco.Code.MapAll
     private static void Map(IGrouping<string?, Core.HealthChecks.HealthCheck> source, HealthCheckGroupViewModel target, MapperContext context)
     {
         target.Name = source.Key;
         target.Checks = context.MapEnumerable<Core.HealthChecks.HealthCheck, HealthCheckViewModel>(source.OrderBy(x => x.Name));
     }
 
+    // Umbraco.Code.MapAll
     private static void Map(IEnumerable<IGrouping<string?, Core.HealthChecks.HealthCheck>> source, PagedViewModel<HealthCheckGroupViewModel> target, MapperContext context)
     {
         target.Items = context.MapEnumerable<IGrouping<string?, Core.HealthChecks.HealthCheck>, HealthCheckGroupViewModel>(source);
