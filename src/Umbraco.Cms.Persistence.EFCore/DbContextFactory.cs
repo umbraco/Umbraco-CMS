900efb5496689d7dbcfd4455ba090d62b396c54b
Zeegaan <nge@umbraco.dk>
2022-12-09 10:31:58 +0100

    Formatting of DbContextFactory
    Signed-off-by: Zeegaan <nge@umbraco.dk>

--- src/Umbraco.Cms.Persistence.EFCore/DbContextFactory.cs
+++ src/Umbraco.Cms.Persistence.EFCore/DbContextFactory.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.DependencyInjection;
 using Umbraco.Cms.Persistence.EFCore.Entities;
+using Umbraco.Cms.Web.Common.DependencyInjection;
 
 namespace Umbraco.Cms.Persistence.EFCore;
 
@@ -11,23 +12,19 @@ public class UmbracoDbContextFactory : DbContextFactory<UmbracoEFContext>
     {
     }
 }
+
 public class DbContextFactory<TDbContext> where TDbContext : DbContext
 {
     private readonly IServiceScopeFactory _scopeFactory;
 
-    public DbContextFactory(IServiceScopeFactory scopeFactory)
-    {
-        _scopeFactory = scopeFactory;
-    }
+    public DbContextFactory(IServiceScopeFactory scopeFactory) => _scopeFactory = scopeFactory;
 
     public async Task<T> ExecuteWithContextAsync<T>(Func<TDbContext, Task<T>> method)
     {
-        using(var scope = _scopeFactory.CreateScope())
-        {
-            var db = scope.ServiceProvider.GetRequiredService<TDbContext>();
+        using IServiceScope scope = _scopeFactory.CreateScope();
+        TDbContext db = StaticServiceProvider.Instance.GetRequiredService<TDbContext>();
 
-            return await method(db);
-        }
+        return await method(db);
     }
 
     public async Task ExecuteWithContextAsync<T>(Func<TDbContext, Task> method) =>
