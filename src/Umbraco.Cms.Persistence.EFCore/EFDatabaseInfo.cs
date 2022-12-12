fd85f75a138182da224e62b8eb7897dd6e698ba9
Zeegaan <nge@umbraco.dk>
2022-12-12 08:52:56 +0100

    Check for pending package migrations
    Signed-off-by: Zeegaan <nge@umbraco.dk>

--- src/Umbraco.Cms.Persistence.EFCore/EFDatabaseInfo.cs
+++ src/Umbraco.Cms.Persistence.EFCore/EFDatabaseInfo.cs
@@ -1,24 +1,27 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Configuration.Models;
+using Umbraco.Cms.Core.Packaging;
 using Umbraco.Cms.Core.Persistence;
+using Umbraco.Cms.Persistence.EFCore.Entities;
 
 namespace Umbraco.Cms.Persistence.EFCore;
 
 public class EFDatabaseInfo : DatabaseInfoBase
 {
     private readonly UmbracoDbContextFactory _umbracoDbContextFactory;
-    private readonly ILogger<EFDatabaseInfo> _logger;
+    private readonly PendingPackageMigrations _pendingPackageMigrations;
 
     public EFDatabaseInfo(
         IOptionsMonitor<ConnectionStrings> connectionStrings,
         UmbracoDbContextFactory umbracoDbContextFactory,
-        ILogger<EFDatabaseInfo> logger)
+        PendingPackageMigrations pendingPackageMigrations)
         : base(connectionStrings)
     {
         _umbracoDbContextFactory = umbracoDbContextFactory;
-        _logger = logger;
+        _pendingPackageMigrations = pendingPackageMigrations;
     }
 
     protected override async Task<DatabaseState> GetConfiguredStateAsync()
@@ -41,7 +44,13 @@ public class EFDatabaseInfo : DatabaseInfoBase
                         return DatabaseState.NeedsUpgrade;
                     }
 
-                    //TODO DatabaseState.NeedsPackageMigration
+                    // Get package migrations already run from database, then match to pending migrations to see if we need to run any
+                    List<UmbracoKeyValue> keyValues = await db.UmbracoKeyValues.Where(x => x.Key.StartsWith(Constants.Conventions.Migrations.KeyValuePrefix)).ToListAsync();
+                    IReadOnlyDictionary<string, string?> dic = keyValues.ToDictionary(x => x.Key, x => x.Value);
+                    if (_pendingPackageMigrations.GetPendingPackageMigrations(dic).Count > 0)
+                    {
+                        return DatabaseState.NeedsPackageMigration;
+                    }
 
                     return DatabaseState.Ok;
                 }
