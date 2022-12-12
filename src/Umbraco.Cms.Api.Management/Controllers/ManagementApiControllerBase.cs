1fd4ed1de763cd6005a2aea5863f45de3a7497f6
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2022-12-12 14:15:54 +0100

    V12: Named json options (#13537)
    * Introduce named JSON options for specific input/output JSON formatting
    
    * Handle empty objects
    
    * Remove obsolete attributes
    
    * Update src/Umbraco.Cms.Api.Management/DependencyInjection/MvcBuilderExtensions.cs
    
    Co-authored-by: Mole <nikolajlauridsen@protonmail.ch>
    
    * Add constant for Backoffice NamedJsonOptions
    
    Co-authored-by: kjac <kja@umbraco.dk>
    Co-authored-by: Mole <nikolajlauridsen@protonmail.ch>
    Co-authored-by: Zeegaan <nge@umbraco.dk>

--- src/Umbraco.Cms.Api.Management/Controllers/ManagementApiControllerBase.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/ManagementApiControllerBase.cs
@@ -1,9 +1,10 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Api.Management.Filters;
+using Umbraco.New.Cms.Core;
 
 namespace Umbraco.Cms.Api.Management.Controllers;
 
-[ManagementApiJsonConfiguration]
+[JsonOptionsName(Constants.JsonOptionsNames.BackOffice)]
 public class ManagementApiControllerBase : Controller
 {
 
