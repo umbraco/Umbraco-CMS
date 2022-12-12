d6b2b6861ce9bfa27b1dc28b11ccaa353a16113c
nikolajlauridsen <nikolajlauridsen@protonmail.ch>
2022-12-12 15:45:43 +0100

    Add status endpoint

---
+++ src/Umbraco.Cms.Api.Management/RedirectUrlManagement/GetEnabledController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
+using Umbraco.Cms.Api.Management.ViewModels.RedirectUrlManagement;
+using Umbraco.Cms.Core.Configuration.Models;
+using Umbraco.Cms.Core.Security;
+using Umbraco.Extensions;
+
+namespace Umbraco.Cms.Api.Management.RedirectUrlManagement;
+
+public class GetEnabledController : RedirectUrlManagementBaseController
+{
+    private readonly IOptionsMonitor<WebRoutingSettings> _webRoutingSettings;
+    private readonly IBackOfficeSecurityAccessor _backOfficeSecurityAccessor;
+
+    public GetEnabledController(
+        IOptionsMonitor<WebRoutingSettings> webRoutingSettings,
+        IBackOfficeSecurityAccessor backOfficeSecurityAccessor)
+    {
+        _webRoutingSettings = webRoutingSettings;
+        _backOfficeSecurityAccessor = backOfficeSecurityAccessor;
+    }
+
+    [HttpGet("status")]
+    public Task<ActionResult<RedirectUrlStatusViewModel>> GetStatus() =>
+        Task.FromResult<ActionResult<RedirectUrlStatusViewModel>>(new RedirectUrlStatusViewModel
+        {
+            Enabled = _webRoutingSettings.CurrentValue.DisableRedirectUrlTracking is false,
+            UserIsAdmin = _backOfficeSecurityAccessor.BackOfficeSecurity?.CurrentUser?.IsAdmin() ?? false,
+        });
+}
---
+++ src/Umbraco.Cms.Api.Management/ViewModels/RedirectUrlManagement/RedirectUrlStatusViewModel.cs
@@ -0,0 +1,8 @@
+﻿namespace Umbraco.Cms.Api.Management.ViewModels.RedirectUrlManagement;
+
+public class RedirectUrlStatusViewModel
+{
+    public bool Enabled { get; set; }
+
+    public bool UserIsAdmin { get; set; }
+}
