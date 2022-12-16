21c5a638d518476d2dc240385797a6d2425c2351
Niels Lyngsø <niels.lyngso@gmail.com>
2022-12-16 11:22:26 +0100

    outcommented corrections

--- src/Umbraco.Web.UI.Client/src/backoffice/workspaces/user/workspace-user.test.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/workspaces/user/workspace-user.test.ts
@@ -1,17 +1,17 @@
 import { expect, fixture, html } from '@open-wc/testing';
 //TODO: Test has been commented out while we figure out how to setup import maps for the test environment
 // import { defaultA11yConfig } from '@umbraco-cms/test-utils';
-// import UmbEditorUserElement from './editor-user.element';
+// import UmbWorkspaceUserElement from './editor-user.element';
 
-// describe('UmbEditorUserElement', () => {
-// 	let element: UmbEditorUserElement;
+// describe('UmbWorkspaceUserElement', () => {
+// 	let element: UmbWorkspaceUserElement;
 
 // 	beforeEach(async () => {
 // 		element = await fixture(html`<umb-workspace-user></umb-workspace-user>`);
 // 	});
 
 // 	it('is defined with its own instance', () => {
-// 		expect(element).to.be.instanceOf(UmbEditorUserElement);
+// 		expect(element).to.be.instanceOf(UmbWorkspaceUserElement);
 // 	});
 
 // 	it('passes the a11y audit', async () => {
