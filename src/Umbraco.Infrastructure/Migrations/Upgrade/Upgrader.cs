a69ae0a1440f13b3e2115bbcece8b31cb4b5f2bf
Nikolaj <nikolajlauridsen@protonmail.ch>
2022-12-19 11:28:13 +0100

    Remove outer scope from Upgrader

--- src/Umbraco.Infrastructure/Migrations/Upgrade/Upgrader.cs
+++ src/Umbraco.Infrastructure/Migrations/Upgrade/Upgrader.cs
@@ -31,20 +31,24 @@ public class Upgrader
     public virtual string StateValueKey => Constants.Conventions.Migrations.KeyValuePrefix + Name;
 
     /// <summary>
-        /// Executes.
-        /// </summary>
-        /// <param name="scopeProvider">A scope provider.</param>
-        /// <param name="keyValueService">A key-value service.</param>
-        [Obsolete("Please use the Execute method that accepts an Umbraco.Cms.Core.Scoping.ICoreScopeProvider instead.")]
-        public ExecutedMigrationPlan Execute(IMigrationPlanExecutor migrationPlanExecutor, IScopeProvider scopeProvider, IKeyValueService keyValueService)
-            => Execute(migrationPlanExecutor, (ICoreScopeProvider)scopeProvider, keyValueService);
-
-        /// <summary>
-    ///     Executes.
+    /// Executes.
     /// </summary>
     /// <param name="scopeProvider">A scope provider.</param>
     /// <param name="keyValueService">A key-value service.</param>
-    public ExecutedMigrationPlan Execute(IMigrationPlanExecutor migrationPlanExecutor, ICoreScopeProvider scopeProvider,
+    [Obsolete("Please use the Execute method that accepts an Umbraco.Cms.Core.Scoping.ICoreScopeProvider instead.")]
+    public ExecutedMigrationPlan Execute(IMigrationPlanExecutor migrationPlanExecutor, IScopeProvider scopeProvider,
+        IKeyValueService keyValueService)
+        => Execute(migrationPlanExecutor, (ICoreScopeProvider)scopeProvider, keyValueService);
+
+    /// <summary>
+    ///     Executes.
+    /// </summary>
+    /// <param name="migrationPlanExecutor"></param>
+    /// <param name="scopeProvider">A scope provider.</param>
+    /// <param name="keyValueService">A key-value service.</param>
+    public ExecutedMigrationPlan Execute(
+        IMigrationPlanExecutor migrationPlanExecutor,
+        ICoreScopeProvider scopeProvider,
         IKeyValueService keyValueService)
     {
         if (scopeProvider == null)
@@ -57,38 +61,40 @@ public class Upgrader
             throw new ArgumentNullException(nameof(keyValueService));
         }
 
-        using (ICoreScope scope = scopeProvider.CreateCoreScope())
+        // TODO: Ensure that state is always returned and saved, regardless of errors.
+
+        var initialState = GetInitialState(scopeProvider, keyValueService);
+
+        // execute plan
+        var state = migrationPlanExecutor.Execute(Plan, initialState);
+        if (string.IsNullOrWhiteSpace(state))
         {
-            // read current state
-            var currentState = keyValueService.GetValue(StateValueKey);
-            var forceState = false;
-
-            if (currentState == null || Plan.IgnoreCurrentState)
-            {
-                currentState = Plan.InitialState;
-                forceState = true;
-            }
-
-            // execute plan
-            var state = migrationPlanExecutor.Execute(Plan, currentState);
-            if (string.IsNullOrWhiteSpace(state))
-            {
-                throw new InvalidOperationException("Plan execution returned an invalid null or empty state.");
-            }
-
-            // save new state
-            if (forceState)
-            {
-                keyValueService.SetValue(StateValueKey, state);
-            }
-            else if (currentState != state)
-            {
-                keyValueService.SetValue(StateValueKey, currentState, state);
-            }
-
-            scope.Complete();
-
-            return new ExecutedMigrationPlan(Plan, currentState, state);
+            throw new InvalidOperationException("Plan execution returned an invalid null or empty state.");
         }
+
+        SetState(state, scopeProvider, keyValueService);
+
+        return new ExecutedMigrationPlan(Plan, initialState, state);
+    }
+
+    private string GetInitialState(ICoreScopeProvider scopeProvider, IKeyValueService keyValueService)
+    {
+        using ICoreScope scope = scopeProvider.CreateCoreScope();
+        var currentState = keyValueService.GetValue(StateValueKey);
+        scope.Complete();
+
+        if (currentState is null || Plan.IgnoreCurrentState)
+        {
+            return Plan.InitialState;
+        }
+
+        return currentState;
+    }
+
+    private void SetState(string state, ICoreScopeProvider scopeProvider, IKeyValueService keyValueService)
+    {
+        using ICoreScope scope = scopeProvider.CreateCoreScope();
+        keyValueService.SetValue(StateValueKey, state);
+        scope.Complete();
     }
 }
