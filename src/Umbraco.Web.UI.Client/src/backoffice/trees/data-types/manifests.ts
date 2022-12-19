94f4807142628af4ba4dd0b7f03c7f93010cfbd7
Mads Rasmussen <madsr@hey.com>
2022-12-19 19:47:24 +0100

    remove data type tree custom element

---
+++ src/Umbraco.Web.UI.Client/src/backoffice/trees/data-types/manifests.ts
@@ -0,0 +1,45 @@
+import type { ManifestTree, ManifestTreeItemAction } from '@umbraco-cms/models';
+
+const dataTypeTreeAlias = 'Umb.Tree.DataTypes';
+
+const tree: ManifestTree = {
+	type: 'tree',
+	alias: dataTypeTreeAlias,
+	name: 'Data Types Tree',
+	weight: 100,
+	meta: {
+		label: 'Data Types',
+		icon: 'umb:folder',
+		sections: ['Umb.Section.Settings'],
+		storeContextAlias: 'umbDataTypeStore',
+	},
+};
+
+const treeItemActions: Array<ManifestTreeItemAction> = [
+	{
+		type: 'treeItemAction',
+		alias: 'Umb.TreeItemAction.DataType.Create',
+		name: 'Tree Item Action Create',
+		loader: () => import('./actions/create/action-data-type-create.element'),
+		weight: 200,
+		meta: {
+			trees: [dataTypeTreeAlias],
+			label: 'Create',
+			icon: 'umb:add',
+		},
+	},
+	{
+		type: 'treeItemAction',
+		alias: 'Umb.TreeItemAction.DataType.Delete',
+		name: 'Tree Item Action Delete',
+		loader: () => import('./actions/delete/action-data-type-delete.element'),
+		weight: 100,
+		meta: {
+			trees: [dataTypeTreeAlias],
+			label: 'Delete',
+			icon: 'umb:delete',
+		},
+	},
+];
+
+export const manifests = [tree, ...treeItemActions];
