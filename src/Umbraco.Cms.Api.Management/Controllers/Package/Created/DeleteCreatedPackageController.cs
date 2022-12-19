61efc4533cbcfab75d511410664d4a24c9480c91
Elitsa Marinovska <elm@umbraco.dk>
2022-12-19 08:22:47 +0100

    Implement delete endpoint

---
+++ src/Umbraco.Cms.Api.Management/Controllers/Package/Created/DeleteCreatedPackageController.cs
@@ -0,0 +1,42 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core.Packaging;
+using Umbraco.Cms.Core.Security;
+using Umbraco.Cms.Core.Services;
+
+namespace Umbraco.Cms.Api.Management.Controllers.Package.Created;
+
+public class DeleteCreatedPackageController : CreatedPackageControllerBase
+{
+    private readonly IPackagingService _packagingService;
+    private readonly IBackOfficeSecurityAccessor _backofficeSecurityAccessor;
+
+    public DeleteCreatedPackageController(IPackagingService packagingService, IBackOfficeSecurityAccessor backofficeSecurityAccessor)
+    {
+        _packagingService = packagingService;
+        _backofficeSecurityAccessor = backofficeSecurityAccessor;
+    }
+
+    /// <summary>
+    ///     Deletes a package with a given key.
+    /// </summary>
+    /// <param name="key">The key of the package.</param>
+    /// <returns>The result of the deletion.</returns>
+    [HttpDelete("{key:guid}")]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<IActionResult> Delete(Guid key)
+    {
+        PackageDefinition? package = _packagingService.GetCreatedPackageByKey(key);
+
+        if (package is null)
+        {
+            return await Task.FromResult(NotFound());
+        }
+
+        _packagingService.DeleteCreatedPackage(package.Id, _backofficeSecurityAccessor.BackOfficeSecurity?.GetUserId().Result ?? -1);
+
+        return await Task.FromResult(Ok());
+    }
+}
