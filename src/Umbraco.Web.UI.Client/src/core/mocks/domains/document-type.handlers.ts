02d8acada56b3c7671cc740da75a0fae66f1889a
Mads Rasmussen <madsr@hey.com>
2022-12-20 21:00:38 +0100

    combine silo handlers in one file

--- src/Umbraco.Web.UI.Client/src/core/mocks/domains/document-type.handlers.ts
+++ src/Umbraco.Web.UI.Client/src/core/mocks/domains/document-type.handlers.ts
@@ -21,4 +21,36 @@ export const handlers = [
 
 		return res(ctx.status(200), ctx.json(saved));
 	}),
+
+	rest.get('/umbraco/management/api/v1/tree/document-type/root', (req, res, ctx) => {
+		const rootItems = umbDocumentTypeData.getTreeRoot();
+		const response = {
+			total: rootItems.length,
+			items: rootItems,
+		};
+		return res(ctx.status(200), ctx.json(response));
+	}),
+
+	rest.get('/umbraco/management/api/v1/tree/document-type/children', (req, res, ctx) => {
+		const key = req.params.key as string;
+		if (!key) return;
+
+		const children = umbDocumentTypeData.getTreeItemChildren(key);
+
+		const response = {
+			total: children.length,
+			items: children,
+		};
+
+		return res(ctx.status(200), ctx.json(response));
+	}),
+
+	rest.get('/umbraco/management/api/v1/tree/document-type/item', (req, res, ctx) => {
+		const keys = req.params.keys as string;
+		if (!keys) return;
+
+		const items = umbDocumentTypeData.getTreeItem(keys.split(','));
+
+		return res(ctx.status(200), ctx.json(items));
+	}),
 ];
--- src/Umbraco.Web.UI.Client/src/core/mocks/domains/media.handlers.ts
+++ src/Umbraco.Web.UI.Client/src/core/mocks/domains/media.handlers.ts
@@ -32,4 +32,25 @@ export const handlers = [
 
 		return res(ctx.status(200), ctx.json(trashed));
 	}),
+
+	rest.get('/umbraco/management/api/v1/tree/media/root', (req, res, ctx) => {
+		const response = umbMediaData.getTreeRoot();
+		return res(ctx.status(200), ctx.json(response));
+	}),
+
+	rest.get('/umbraco/management/api/v1/tree/media/children', (req, res, ctx) => {
+		const key = req.params.key as string;
+		if (!key) return;
+		const response = umbMediaData.getTreeItemChildren(key);
+		return res(ctx.status(200), ctx.json(response));
+	}),
+
+	rest.get('/umbraco/management/api/v1/tree/media/item', (req, res, ctx) => {
+		const keys = req.params.keys as string;
+		if (!keys) return;
+
+		const items = umbMediaData.getTreeItem(keys.split(','));
+
+		return res(ctx.status(200), ctx.json(items));
+	}),
 ];
