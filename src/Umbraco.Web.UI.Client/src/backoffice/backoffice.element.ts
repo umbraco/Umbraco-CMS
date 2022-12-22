4418f8757067bd4b4bd9eec86d8a984cf2fc1bb2
Niels Lyngsø <niels.lyngso@gmail.com>
2022-12-22 09:31:24 +0100

    folder layout -> views

--- src/Umbraco.Web.UI.Client/src/backoffice/backoffice.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/backoffice.element.ts
@@ -42,7 +42,7 @@ import { manifests as propertyActionManifests } from './property-actions/manifes
 import { manifests as externalLoginProviderManifests } from './external-login-providers/manifests';
 import { manifests as userDashboards } from './user-dashboards/manifests';
 import { manifests as collectionBulkActionManifests } from './components/collection/bulk-actions/manifests';
-import { manifests as collectionViewManifests } from './components/collection/layouts/manifests';
+import { manifests as collectionViewManifests } from './components/collection/views/manifests';
 import { UmbContextConsumerMixin, UmbContextProviderMixin } from '@umbraco-cms/context-api';
 import { umbExtensionsRegistry } from '@umbraco-cms/extensions-registry';
 import type { ManifestTypes } from '@umbraco-cms/models';
