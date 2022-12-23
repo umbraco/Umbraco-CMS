fc34aa83105b00693df63f6670a68d3c7548b12b
Niels Lyngsø <niels.lyngso@gmail.com>
2022-12-23 13:29:39 +0100

    remove comma

--- src/Umbraco.Web.UI.Client/src/backoffice/workspaces/data-type/workspace-data-type.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/workspaces/data-type/workspace-data-type.element.ts
@@ -111,7 +111,7 @@ export class UmbWorkspaceDataTypeElement extends UmbContextProviderMixin(
 				name: 'Save Data Type Workspace Action',
 				loader: () => import('../shared/actions/save/workspace-action-node-save.element'),
 				meta: {
-					workspaces: ['Umb.Workspace.DataType'],
+					workspaces: ['Umb.Workspace.DataType']
 				},
 			},
 		];
