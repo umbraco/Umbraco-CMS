e8e4e7207eea7c48f55d78216f5b7541698f7911
Niels Lyngsø <niels.lyngso@gmail.com>
2023-01-02 10:20:53 +0100

    remove consumer mixing from property

--- src/Umbraco.Web.UI.Client/src/backoffice/components/entity-property/entity-property.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/components/entity-property/entity-property.element.ts
@@ -5,7 +5,6 @@ import { UmbWorkspacePropertyContext } from './workspace-property.context';
 import { UmbObserverMixin } from '@umbraco-cms/observable-api';
 import { createExtensionElement } from '@umbraco-cms/extensions-api';
 import { umbExtensionsRegistry } from '@umbraco-cms/extensions-registry';
-import { UmbContextConsumerMixin } from '@umbraco-cms/context-api';
 import type { ManifestPropertyEditorUI, ManifestTypes } from '@umbraco-cms/models';
 
 import '../../property-actions/shared/property-action-menu/property-action-menu.element';
@@ -21,7 +20,7 @@ import { UmbControllerHostMixin } from 'src/core/controller/controller-host.mixi
 
 // TODO: get rid of the other mixins:
 @customElement('umb-entity-property')
-export class UmbEntityPropertyElement extends UmbControllerHostMixin(UmbContextConsumerMixin(UmbObserverMixin(LitElement))) {
+export class UmbEntityPropertyElement extends UmbControllerHostMixin(UmbObserverMixin(LitElement)) {
 	static styles = [
 		UUITextStyles,
 		css`
