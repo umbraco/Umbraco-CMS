c63863adca1fab6dcc0eeacdeb2311b986dbd205
Mads Rasmussen <madsr@hey.com>
2023-01-02 15:39:57 +0100

    remove redundant import

--- src/Umbraco.Web.UI.Client/src/backoffice/trees.manifest.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/trees.manifest.ts
@@ -1,12 +1,10 @@
 // TODO: temp file until we have a way to register from each extension
-import { manifests as extensionTreeManifests } from './core/extensions/tree/manifests';
 import { manifests as memberTypesTreeManifests } from './members/member-types/tree/manifests';
 import { manifests as mediaTypeTreeManifests } from './media/media-types/tree/manifests';
 
 import type { ManifestTree, ManifestTreeItemAction, ManifestWorkspace } from '@umbraco-cms/models';
 
 export const manifests: Array<ManifestTree | ManifestTreeItemAction | ManifestWorkspace> = [
-	...extensionTreeManifests,
 	...mediaTypeTreeManifests,
 	...memberTypesTreeManifests,
 ];
