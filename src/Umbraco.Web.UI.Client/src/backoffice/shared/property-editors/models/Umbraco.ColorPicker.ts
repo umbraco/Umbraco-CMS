b4c326f6ed87030cb63dab80d39db6e5b331e4fe
Mads Rasmussen <madsr@hey.com>
2023-01-03 13:09:56 +0100

    add config

--- src/Umbraco.Web.UI.Client/src/backoffice/shared/property-editors/models/Umbraco.ColorPicker.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/shared/property-editors/models/Umbraco.ColorPicker.ts
@@ -4,5 +4,23 @@ export const manifest: ManifestPropertyEditorModel = {
 	type: 'propertyEditorModel',
 	name: 'Color Picker',
 	alias: 'Umbraco.ColorPicker',
-	meta: {},
+	meta: {
+		config: {
+			properties: [
+				{
+					alias: 'useLabel',
+					label: 'Include labels?',
+					description:
+						'Stores colors as a Json object containing both the color hex string and label, rather than just the hex string.',
+					propertyEditorUI: 'Umb.PropertyEditorUI.Toggle',
+				},
+				{
+					alias: 'colors',
+					label: 'Colors',
+					description: 'Add, remove or sort colors',
+					propertyEditorUI: 'Umb.PropertyEditorUI.ColorPicker',
+				},
+			],
+		},
+	},
 };
