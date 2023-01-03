e35e4363894c2b5e827ef685a937f22fcd880e4a
Mads Rasmussen <madsr@hey.com>
2023-01-03 12:50:22 +0100

    generate ui files

---
+++ src/Umbraco.Web.UI.Client/src/backoffice/shared/property-editors/uis/color-picker/property-editor-ui-color-picker.test.ts
@@ -0,0 +1,19 @@
+import { expect, fixture, html } from '@open-wc/testing';
+import { UmbPropertyEditorUIColorPickerElement } from './property-editor-ui-color-picker.element';
+import { defaultA11yConfig } from '@umbraco-cms/test-utils';
+
+describe('UmbPropertyEditorUIColorPickerElement', () => {
+	let element: UmbPropertyEditorUIColorPickerElement;
+
+	beforeEach(async () => {
+		element = await fixture(html` <umb-property-editor-ui-color-picker></umb-property-editor-ui-color-picker> `);
+	});
+
+	it('is defined with its own instance', () => {
+		expect(element).to.be.instanceOf(UmbPropertyEditorUIColorPickerElement);
+	});
+
+	it('passes the a11y audit', async () => {
+		await expect(element).shadowDom.to.be.accessible(defaultA11yConfig);
+	});
+});
