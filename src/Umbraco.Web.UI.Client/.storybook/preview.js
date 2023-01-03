ce0287db0747a69694998c68a8384d9db7bd2dfd
Niels Lyngsø <niels.lyngso@gmail.com>
2023-01-03 10:06:42 +0100

    preview

--- src/Umbraco.Web.UI.Client/.storybook/preview.js
+++ src/Umbraco.Web.UI.Client/.storybook/preview.js
@@ -9,27 +9,22 @@ import { initialize, mswDecorator } from 'msw-storybook-addon';
 import { setCustomElements } from '@storybook/web-components';
 
 import customElementManifests from '../custom-elements.json';
-import { UmbDataTypeStore } from '../src/backoffice/core/data-types/data-type.store';
+import { UmbDataTypeStore } from '../src/backoffice/settings/data-types/data-type.store';
 import { UmbDocumentTypeStore } from '../src/backoffice/documents/document-types/document-type.store';
-import { UmbIconStore } from '../src/backoffice/core/stores/icon/icon.store';
+import { UmbIconStore } from '../src/core/stores/icon/icon.store';
 import { onUnhandledRequest } from '../src/core/mocks/browser';
 import { handlers } from '../src/core/mocks/browser-handlers';
 import { LitElement } from 'lit';
-import { UmbModalService } from '../src/backoffice/core/services/modal';
+import { UmbModalService } from '../src/core/modal';
 
-import { manifests as sectionManifests } from '../src/backoffice/sections.manifest';
-import { manifests as propertyEditorModelManifests } from '../src/backoffice/core/property-editors/models/manifests';
-import { manifests as propertyEditorUIManifests } from '../src/backoffice/core/property-editors/uis/manifests';
-import { manifests as treeManifests } from '../src/backoffice/trees.manifest';
-import { manifests as workspaceManifests } from '../src/backoffice/workspaces.manifest';
-import { manifests as propertyActionManifests } from '../src/backoffice/core/property-actions/manifests';
+// TODO: Fix storybook manifest registrations.
 
 import { umbExtensionsRegistry } from '../src/core/extensions-registry';
 
 import '../src/core/context-api/provide/context-provider.element';
 import '../src/core/css/custom-properties.css';
-import '../src/backoffice/core/components/backoffice-frame/backoffice-modal-container.element';
-import '../src/backoffice/core/components/code-block/code-block.element';
+import '../src/backoffice/shared/components/backoffice-frame/backoffice-modal-container.element';
+import '../src/backoffice/shared/components/code-block/code-block.element';
 
 class UmbStoryBookElement extends LitElement {
 	_umbIconStore = new UmbIconStore();
@@ -37,13 +32,6 @@ class UmbStoryBookElement extends LitElement {
 	constructor() {
 		super();
 		this._umbIconStore.attach(this);
-
-		this._registerExtensions(sectionManifests);
-		this._registerExtensions(treeManifests);
-		this._registerExtensions(workspaceManifests);
-		this._registerExtensions(propertyEditorModelManifests);
-		this._registerExtensions(propertyEditorUIManifests);
-		this._registerExtensions(propertyActionManifests);
 	}
 
 	_registerExtensions(manifests) {
