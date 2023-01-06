1870767cc3f663a4715b4a9230605a2367a9f328
Mads Rasmussen <madsr@hey.com>
2023-01-06 13:51:00 +0100

    register document menu item

---
+++ src/Umbraco.Web.UI.Client/src/backoffice/documents/documents/sidebar-menu-item/manifests.ts
@@ -0,0 +1,17 @@
+import type { ManifestSidebarMenuItem } from '@umbraco-cms/models';
+
+const sidebarMenuItem: ManifestSidebarMenuItem = {
+	type: 'sidebarMenuItem',
+	alias: 'Umb.SidebarMenuItem.Documents',
+	name: 'Documents Sidebar Menu Item',
+	weight: 100,
+	loader: () => import('./document-sidebar-menu-item.element'),
+	meta: {
+		label: 'Documents',
+		icon: 'umb:folder',
+		sections: ['Umb.Section.Content'],
+		entityType: 'document',
+	},
+};
+
+export const manifests = [sidebarMenuItem];
