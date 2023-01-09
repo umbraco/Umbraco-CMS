9b5fab187d564a4bb9f0eafeceb578549d6d1fcf
Niels Lyngsø <niels.lyngso@gmail.com>
2023-01-09 20:28:49 +0100

    action menu

--- src/Umbraco.Web.UI.Client/src/backoffice/shared/property-actions/shared/property-action-menu/property-action-menu.context.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/shared/property-actions/shared/property-action-menu/property-action-menu.context.ts
@@ -1,10 +1,10 @@
-import { BehaviorSubject } from 'rxjs';
 import { UmbContextProviderController } from 'src/core/context-api/provide/context-provider.controller';
 import type { UmbControllerHostInterface } from 'src/core/controller/controller-host.mixin';
+import { UniqueBehaviorSubject } from 'src/core/observable-api/unique-behavior-subject';
 
 export class UmbPropertyActionMenuContext {
 
-	#isOpen = new BehaviorSubject(false);
+	#isOpen = new UniqueBehaviorSubject(false);
 	public readonly isOpen = this.#isOpen.asObservable();
 
 	constructor(host: UmbControllerHostInterface) {
