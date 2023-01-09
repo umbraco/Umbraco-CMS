e6d616e590f16ed79ba75bd2e740a7294e02a9cc
Elitsa Marinovska <elm@umbraco.dk>
2023-01-09 12:55:33 +0100

    Migrating GetNumberOfErrors()

---
+++ src/Umbraco.Cms.Api.Management/Controllers/Log/ErrorCountLogController.cs
@@ -0,0 +1,37 @@
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Management.Controllers.LogViewer;
+using Umbraco.Cms.Core.Logging.Viewer;
+
+namespace Umbraco.Cms.Api.Management.Controllers.Log;
+
+public class ErrorCountLogController : LogControllerBase
+{
+    private readonly ILogViewer _logViewer;
+
+    public ErrorCountLogController(ILogViewer logViewer)
+        : base(logViewer) => _logViewer = logViewer;
+
+    /// <summary>
+    ///     Gets the number of errors from the logs for a specific date range.
+    /// </summary>
+    /// <param name="startDate">The start date for the date range (can be null).</param>
+    /// <param name="endDate">The end date for the date range (can be null).</param>
+    /// <returns>The error count from the (filtered) logs.</returns>
+    [HttpGet("error/count")]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    public async Task<ActionResult<int>> ErrorCount(DateTime? startDate = null, DateTime? endDate = null)
+    {
+        LogTimePeriod logTimePeriod = GetTimePeriod(startDate, endDate);
+
+        // We will need to stop the request if trying to do this on a 1GB file
+        if (CanViewLogs(logTimePeriod) == false)
+        {
+            return await Task.FromResult(ValidationProblem("Unable to view logs, due to their size"));
+        }
+
+        return await Task.FromResult(Ok(_logViewer.GetNumberOfErrors(logTimePeriod)));
+    }
+}
