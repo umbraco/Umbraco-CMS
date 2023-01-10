67f2f7d5df620b1e1383f8ba5a8386a584e2450f
Niels Lyngsø <niels.lyngso@gmail.com>
2023-01-10 21:52:42 +0100

    only deep freeze objects

--- src/Umbraco.Web.UI.Client/src/core/observable-api/unique-behavior-subject.ts
+++ src/Umbraco.Web.UI.Client/src/core/observable-api/unique-behavior-subject.ts
@@ -1,24 +1,27 @@
 import { BehaviorSubject, distinctUntilChanged, map, Observable, shareReplay } from "rxjs";
 
 
+// TODO: Should this handle array as well?
 function deepFreeze<T>(inObj: T): T {
-    Object.freeze(inObj);
+	if(typeof inObj === 'object') {
+		Object.freeze(inObj);
 
-    Object.getOwnPropertyNames(inObj).forEach(function (prop) {
-      // eslint-disable-next-line no-prototype-builtins
-      if ((inObj as any).hasOwnProperty(prop)
-        && (inObj as any)[prop] != null
-        && typeof (inObj as any)[prop] === 'object'
-        && !Object.isFrozen((inObj as any)[prop])) {
-          deepFreeze((inObj as any)[prop]);
-        }
-    });
-    return inObj;
+		Object.getOwnPropertyNames(inObj)?.forEach(function (prop) {
+			// eslint-disable-next-line no-prototype-builtins
+			if ((inObj as any).hasOwnProperty(prop)
+				&& (inObj as any)[prop] != null
+				&& typeof (inObj as any)[prop] === 'object'
+				&& !Object.isFrozen((inObj as any)[prop])) {
+					deepFreeze((inObj as any)[prop]);
+				}
+		});
+	}
+	return inObj;
 }
 
 
 export function naiveObjectComparison(objOne: any, objTwo: any): boolean {
-    return JSON.stringify(objOne) === JSON.stringify(objTwo);
+	return JSON.stringify(objOne) === JSON.stringify(objTwo);
 }
 
 
