c88aa523667d1679c042866429fb918c419ce98d
Zeegaan <nge@umbraco.dk>
2023-01-11 11:13:54 +0100

    Remove commented code
    Signed-off-by: Zeegaan <nge@umbraco.dk>

--- src/Umbraco.Infrastructure/Install/UnattendedInstaller.cs
+++ src/Umbraco.Infrastructure/Install/UnattendedInstaller.cs
@@ -65,88 +65,31 @@ public class UnattendedInstaller : INotificationAsyncHandler<RuntimeUnattendedIn
             _databaseBuilder.CreateDatabase();
         }
 
-        //
-        // bool connect;
-        // try
-        // {
-        //     for (var i = 0; ;)
-        //     {
-        //         connect = _databaseFactory.CanConnect;
-        //         if (connect || ++i == 5)
-        //         {
-        //             break;
-        //         }
-        //
-        //         _logger.LogDebug("Could not immediately connect to database, trying again.");
-        //
-        //         Thread.Sleep(1000);
-        //     }
-        // }
-        // catch (Exception ex)
-        // {
-        //     _logger.LogInformation(ex, "Error during unattended install.");
-        //
-        //     var innerException = new UnattendedInstallException("Unattended installation failed.", ex);
-        //     _runtimeState.Configure(RuntimeLevel.BootFailed, RuntimeLevelReason.BootFailedOnException, innerException);
-        //     return Task.CompletedTask;
-        // }
-        //
-        // // could not connect to the database
-        // if (connect == false)
-        // {
-        //     return Task.CompletedTask;
-        // }
+        try
+        {
+            if (_databaseBuilder.IsUmbracoInstalled())
+            {
+                return Task.CompletedTask;
+            }
 
-        if (_databaseBuilder.IsUmbracoInstalled())
-        {
-            return Task.CompletedTask;
-        }
-        else
-        {
+            _logger.LogInformation("Starting unattended install.");
             _databaseBuilder.CreateSchemaAndData();
+            _logger.LogInformation("Unattended install completed.");
+        }
+        catch (Exception ex)
+        {
+            _logger.LogInformation(ex, "Error during unattended install.");
+
+            var innerException = new UnattendedInstallException(
+                "The database configuration failed."
+                + "\n Please check log file for additional information (can be found in '/Umbraco/Data/Logs/')",
+                ex);
+
+            _runtimeState.Configure(RuntimeLevel.BootFailed, RuntimeLevelReason.BootFailedOnException, innerException);
         }
 
         _eventAggregator.Publish(new UnattendedInstallNotification());
 
-        // try
-        // {
-        //     using (database = _databaseFactory.CreateDatabase())
-        //     {
-        //         var hasUmbracoTables = database.IsUmbracoInstalled();
-        //
-        //         // database has umbraco tables, assume Umbraco is already installed
-        //         if (hasUmbracoTables)
-        //         {
-        //             return Task.CompletedTask;
-        //         }
-        //
-        //
-        //         // all conditions fulfilled, do the install
-        //         _logger.LogInformation("Starting unattended install.");
-        //
-        //         database.BeginTransaction();
-        //         IDatabaseSchemaCreator creator = _databaseSchemaCreatorFactory.Create(database);
-        //         creator.InitializeDatabaseSchema(true);
-        //         database.CompleteTransaction();
-        //         _logger.LogInformation("Unattended install completed.");
-        //
-        //         // Emit an event with EventAggregator that unattended install completed
-        //         // Then this event can be listened for and create an unattended user
-        //         _eventAggregator.Publish(new UnattendedInstallNotification());
-        //     }
-        // }
-        // catch (Exception ex)
-        // {
-        //     _logger.LogInformation(ex, "Error during unattended install.");
-        //     database?.AbortTransaction();
-        //
-        //     var innerException = new UnattendedInstallException(
-        //         "The database configuration failed."
-        //         + "\n Please check log file for additional information (can be found in '/Umbraco/Data/Logs/')",
-        //         ex);
-        //
-        //     _runtimeState.Configure(RuntimeLevel.BootFailed, RuntimeLevelReason.BootFailedOnException, innerException);
-        // }
 
         return Task.CompletedTask;
     }
