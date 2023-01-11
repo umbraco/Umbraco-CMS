93d34888864771524511ce9afdf7c525bc123733
Nikolaj <nikolajlauridsen@protonmail.ch>
2023-01-11 12:43:59 +0100

    Obsolete chings changed in v13

--- src/Umbraco.Infrastructure/Migrations/IMigrationPlanExecutor.cs
+++ src/Umbraco.Infrastructure/Migrations/IMigrationPlanExecutor.cs
@@ -4,5 +4,6 @@ namespace Umbraco.Cms.Core.Migrations;
 
 public interface IMigrationPlanExecutor
 {
+    [Obsolete("This will return an ExecutedMigrationPlan in V13")]
     string Execute(MigrationPlan plan, string fromState);
 }
