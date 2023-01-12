6e4fc6ba0cf6ef0884492fca9751f48f99dc48fb
nikolajlauridsen <nikolajlauridsen@protonmail.ch>
2023-01-12 15:04:28 +0100

    Only allow scoping the database from UnscopedMigrationBase

--- src/Umbraco.Infrastructure/Migrations/MigrationBase.cs
+++ src/Umbraco.Infrastructure/Migrations/MigrationBase.cs
@@ -108,23 +108,6 @@ public abstract partial class MigrationBase : IDiscoverable
     }
 
     /// <summary>
-    /// <para>Scope the database used by the migration builder.</para>
-    /// <para>This is used with <see cref="UnscopedMigrationBase"/> when you need to execute something before the scope is created
-    /// but later need to have your queries scoped in a transaction.</para>
-    /// </summary>
-    /// <param name="scope">The scope to get the database from.</param>
-    /// <exception cref="InvalidOperationException">If the migration is missing or has a malformed MigrationContext this exception is thrown.</exception>
-    protected void ScopeDatabase(IScope scope)
-    {
-        if (Context is not MigrationContext context)
-        {
-            throw new InvalidOperationException("Cannot scope database because context is not a MigrationContext");
-        }
-
-        context.Database = scope.Database;
-    }
-
-    /// <summary>
     ///     Creates a new Sql statement.
     /// </summary>
     protected Sql<ISqlContext> Sql() => Context.SqlContext.Sql();
