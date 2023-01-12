e52a91ce1011e562480fd912aa6469ee41c0acdf
Niels Lyngsø <niels.lyngso@gmail.com>
2023-01-12 08:48:35 +0100

    updated story

--- src/Umbraco.Web.UI.Client/src/backoffice/settings/data-types/workspace/data-type-workspace.stories.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/settings/data-types/workspace/data-type-workspace.stories.ts
@@ -5,7 +5,7 @@ import { html } from 'lit-html';
 
 import { data } from '../../../../core/mocks/data/data-type.data';
 
-import type { UmbWorkspaceDataTypeElement } from './data-type-workspace.element';
+import type { UmbDataTypeWorkspaceElement } from './data-type-workspace.element';
 
 export default {
 	title: 'Workspaces/Data Type',
@@ -13,6 +13,6 @@ export default {
 	id: 'umb-data-type-workspace',
 } as Meta;
 
-export const AAAOverview: Story<UmbWorkspaceDataTypeElement> = () =>
+export const AAAOverview: Story<UmbDataTypeWorkspaceElement> = () =>
 	html` <umb-data-type-workspace id="${data[0].key}"></umb-data-type-workspace>`;
 AAAOverview.storyName = 'Overview';
