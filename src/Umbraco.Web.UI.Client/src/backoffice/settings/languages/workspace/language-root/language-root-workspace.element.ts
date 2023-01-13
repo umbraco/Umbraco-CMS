00fb727b5fc07bed1966e9cd6b6ea19b1fb11188
Jesper Møller Jensen <26099018+JesmoDev@users.noreply.github.com>
2023-01-13 03:55:20 +0100

    add layout and table

--- src/Umbraco.Web.UI.Client/src/backoffice/settings/languages/workspace/language-root/language-root-workspace.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/settings/languages/workspace/language-root/language-root-workspace.element.ts
@@ -1,10 +1,37 @@
-import { html, LitElement } from 'lit';
+import { UUITextStyles } from '@umbraco-ui/uui-css';
+import { css, html, LitElement } from 'lit';
 import { customElement } from 'lit/decorators.js';
 
 @customElement('umb-language-root-workspace')
 export class UmbLanguageRootWorkspaceElement extends LitElement {
+	static styles = [
+		UUITextStyles,
+		css`
+			:host {
+				display: block;
+				width: 100%;
+				height: 100%;
+			}
+
+			umb-table {
+				padding: 0;
+				margin: var(--uui-size-space-3) var(--uui-size-space-6);
+			}
+			#add-language {
+				margin-left: var(--uui-size-space-6);
+			}
+		`,
+	];
+
 	render() {
-		return html` <div>Language Root Workspace</div> `;
+		return html`
+			<umb-body-layout no-header-background>
+				<uui-button id="add-language" slot="header" label="Add language" look="outline" color="default"></uui-button>
+				<!-- <div slot="header" id="toolbar">
+				</div> -->
+				<umb-table></umb-table>
+			</umb-body-layout>
+		`;
 	}
 }
 
