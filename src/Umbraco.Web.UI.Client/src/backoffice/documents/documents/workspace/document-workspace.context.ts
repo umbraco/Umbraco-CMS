a4cfaab4f5d21d6e76fc6b07a4c59cafebecfc18
Niels Lyngsø <niels.lyngso@gmail.com>
2023-01-13 09:42:38 +0100

    change import

--- src/Umbraco.Web.UI.Client/src/backoffice/documents/documents/workspace/document-workspace.context.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/documents/documents/workspace/document-workspace.context.ts
@@ -1,9 +1,9 @@
 import { UmbWorkspaceContentContext } from '../../../shared/components/workspace/workspace-content/workspace-content.context';
 import { isDocumentDetails, STORE_ALIAS as DOCUMENT_STORE_ALIAS } from 'src/backoffice/documents/documents/document.store';
 import type { UmbDocumentStore, UmbDocumentStoreItemType } from 'src/backoffice/documents/documents/document.store';
-import { UmbControllerHostInterface } from 'src/core/controller/controller-host.mixin';
+import type { UmbControllerHostInterface } from 'src/core/controller/controller-host.mixin';
 import type { DocumentDetails } from '@umbraco-cms/models';
-import { appendToFrozenArray } from 'src/core/observable-api/unique-behavior-subject';
+import { appendToFrozenArray } from '@umbraco-cms/observable-api';
 
 const DefaultDocumentData = {
 	key: '',
