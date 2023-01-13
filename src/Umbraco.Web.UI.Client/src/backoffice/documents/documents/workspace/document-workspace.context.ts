efa94a75a96281bb138fa80c153c3231f0524d4f
Niels Lyngsø <niels.lyngso@gmail.com>
2023-01-13 12:35:56 +0100

    remove import

--- src/Umbraco.Web.UI.Client/src/backoffice/documents/documents/workspace/document-workspace.context.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/documents/documents/workspace/document-workspace.context.ts
@@ -1,5 +1,5 @@
 import { UmbWorkspaceContentContext } from '../../../shared/components/workspace/workspace-content/workspace-content.context';
-import { isDocumentDetails, STORE_ALIAS as DOCUMENT_STORE_ALIAS } from '../../../documents/documents/document.store';
+import { STORE_ALIAS as DOCUMENT_STORE_ALIAS } from '../../../documents/documents/document.store';
 import type { UmbDocumentStore, UmbDocumentStoreItemType } from '../../../documents/documents/document.store';
 import type { UmbControllerHostInterface } from 'src/core/controller/controller-host.mixin';
 import type { DocumentDetails } from '@umbraco-cms/models';
