c5e7afc5d7e7998306925fd3c9e6733adbd16d25
Elitsa Marinovska <elm@umbraco.dk>
2023-01-14 18:25:18 +0100

    Obsoleting old methods in favor of the ILogViewerService

--- src/Umbraco.Infrastructure/Logging/Viewer/ILogLevelLoader.cs
+++ src/Umbraco.Infrastructure/Logging/Viewer/ILogLevelLoader.cs
@@ -8,10 +8,12 @@ public interface ILogLevelLoader
     /// <summary>
     ///     Get the Serilog level values of the global minimum and the UmbracoFile one from the config file.
     /// </summary>
+    [Obsolete("Use ILogViewerService.GetLogLevelsFromSinks instead. Scheduled for removal in Umbraco 15.")]
     ReadOnlyDictionary<string, LogEventLevel?> GetLogLevelsFromSinks();
 
     /// <summary>
     ///     Get the Serilog minimum-level value from the config file.
     /// </summary>
+    [Obsolete("Use ILogViewerService.GetGlobalMinLogLevel instead. Scheduled for removal in Umbraco 15.")]
     LogEventLevel? GetGlobalMinLogLevel();
 }
--- src/Umbraco.Infrastructure/Logging/Viewer/ILogViewerConfig.cs
+++ src/Umbraco.Infrastructure/Logging/Viewer/ILogViewerConfig.cs
@@ -2,14 +2,18 @@ namespace Umbraco.Cms.Core.Logging.Viewer;
 
 public interface ILogViewerConfig
 {
+    [Obsolete("Use ILogViewerService.GetSavedLogQueriesAsync instead. Scheduled for removal in Umbraco 15.")]
     IReadOnlyList<SavedLogSearch> GetSavedSearches();
 
+    [Obsolete("Use ILogViewerService.AddSavedLogQueryAsync instead. Scheduled for removal in Umbraco 15.")]
     IReadOnlyList<SavedLogSearch> AddSavedSearch(string name, string query);
 
     [Obsolete("Use the overload that only takes a 'name' parameter instead. This will be removed in Umbraco 14.")]
     IReadOnlyList<SavedLogSearch> DeleteSavedSearch(string name, string query);
 
+    [Obsolete("Use ILogViewerService.DeleteSavedLogQueryAsync instead. Scheduled for removal in Umbraco 15.")]
     IReadOnlyList<SavedLogSearch> DeleteSavedSearch(string name) => DeleteSavedSearch(name, string.Empty);
 
+    [Obsolete("Use ILogViewerService.GetSavedLogQueryByNameAsync instead. Scheduled for removal in Umbraco 15.")]
     SavedLogSearch? GetSavedSearchByName(string name);
 }
