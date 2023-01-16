3cf8a2999a53229b4aa6a01a3b2e97d68a49c74c
kjac <kja@umbraco.dk>
2023-01-16 13:12:41 +0100

    Refactored ManagementApiComposer into several extension classes

---
+++ src/Umbraco.Cms.Api.Management/DependencyInjection/ApiVersioningBuilderExtensions.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc.Versioning;
+using Microsoft.Extensions.DependencyInjection;
+using Umbraco.Cms.Core.DependencyInjection;
+
+namespace Umbraco.Cms.Api.Management.DependencyInjection;
+
+internal static class ApiVersioningBuilderExtensions
+{
+    internal static IUmbracoBuilder AddApiVersioning(this IUmbracoBuilder builder)
+    {
+        builder.Services.AddApiVersioning(options =>
+        {
+            options.DefaultApiVersion = ManagementApiConfiguration.DefaultApiVersion;
+            options.ReportApiVersions = true;
+            options.ApiVersionReader = new UrlSegmentApiVersionReader();
+            options.AssumeDefaultVersionWhenUnspecified = true;
+            options.UseApiBehavior = false;
+        });
+
+        builder.Services.AddVersionedApiExplorer(options =>
+        {
+            options.DefaultApiVersion = ManagementApiConfiguration.DefaultApiVersion;
+            options.GroupNameFormat = "'v'VVV";
+            options.SubstituteApiVersionInUrl = true;
+            options.AddApiVersionParametersWhenVersionNeutral = true;
+            options.AssumeDefaultVersionWhenUnspecified = true;
+        });
+
+        return builder;
+    }
+}
