57bd9b4e615c7cee36eb50377a73a58011161e0e
Nikolaj <nikolajlauridsen@protonmail.ch>
2023-01-16 15:45:32 +0100

    Add ByKey endpoint

---
+++ src/Umbraco.Cms.Api.Management/Controllers/UserGroups/ByKeyUserGroupController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Management.ViewModels.DataType;
+using Umbraco.Cms.Core.Services;
+
+namespace Umbraco.Cms.Api.Management.Controllers.UserGroups;
+
+
+public class ByKeyUserGroupController : UserGroupsControllerBase
+{
+    private readonly IUserService _userService;
+
+    public ByKeyUserGroupController(IUserService userService)
+    {
+        _userService = userService;
+    }
+
+    [HttpGet("{key:guid}")]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(typeof(DataTypeViewModel), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> ByKey(Guid key)
+    {
+        _userService.GetUserGroupByKey(key);
+        return Ok();
+    }
+}
---
+++ src/Umbraco.Cms.Api.Management/Controllers/UserGroups/UserGroupsControllerBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Management.Routing;
+
+namespace Umbraco.Cms.Api.Management.Controllers.UserGroups;
+
+// TODO: This needs to be an authorized controller.
+
+[ApiController]
+[VersionedApiBackOfficeRoute("user-groups")]
+[ApiExplorerSettings(GroupName = "User Groups")]
+[ApiVersion("1.0")]
+public class UserGroupsControllerBase : ManagementApiControllerBase
+{
+
+}
