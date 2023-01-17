450ca9b638948ef57b9e03092965d2ddcbca8b08
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-01-17 11:21:51 +0100

    clean imports

--- src/Umbraco.Web.UI.Client/src/backoffice/settings/languages/workspace/language/language-workspace.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/settings/languages/workspace/language/language-workspace.element.ts
@@ -1,17 +1,14 @@
-import { UmbLitElement } from '@umbraco-cms/element';
-import { html, LitElement } from 'lit';
+import { html } from 'lit';
 import { customElement } from 'lit/decorators.js';
 import { UMB_NOTIFICATION_SERVICE_CONTEXT_ALIAS } from 'src/core/notification';
+
+import { UmbLitElement } from '@umbraco-cms/element';
 
 @customElement('umb-language-workspace')
 export class UmbLanguageWorkspaceElement extends UmbLitElement {
 	constructor() {
 		super();
 		this.consumeContext(UMB_NOTIFICATION_SERVICE_CONTEXT_ALIAS, (service) => {
-			console.log(
-				'🚀 ~ file: language-workspace.element.ts:11 ~ UmbLanguageWorkspaceElement ~ this.consumeContext ~ service',
-				service
-			);
 			service.peek('positive', { data: { message: 'Language Workspace' } });
 		});
 	}
