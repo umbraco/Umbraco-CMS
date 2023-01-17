87f2881a8f4dd9c8db3366fb58b11bee2e8219b3
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-01-17 10:00:22 +0100

    add ContextToken

---
+++ src/Umbraco.Web.UI.Client/src/core/context-api/context-token.ts
@@ -0,0 +1,19 @@
+export class ContextToken<T> {
+	/**
+	 * @param _desc   Description for the token,
+	 *                used only for debugging purposes,
+	 *                it should but does not need to be unique
+	 */
+	constructor(protected _desc: string) {}
+
+	/**
+	 * @internal
+	 */
+	get multi(): ContextToken<Array<T>> {
+		return this as ContextToken<Array<T>>;
+	}
+
+	toString(): string {
+		return `${ContextToken.name} ${this._desc}`;
+	}
+}
--- src/Umbraco.Web.UI.Client/src/core/context-api/index.ts
+++ src/Umbraco.Web.UI.Client/src/core/context-api/index.ts
@@ -2,3 +2,4 @@ export * from './consume/context-consumer';
 export * from './consume/context-request.event';
 export * from './provide/context-provider';
 export * from './provide/context-provide.event';
+export * from './context-token';
