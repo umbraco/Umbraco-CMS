386931e1ea59fe9730543cef43e4b1bd2d1b61f7
nikolajlauridsen <nikolajlauridsen@protonmail.ch>
2023-01-17 13:40:40 +0100

    add POC displaying model

--- src/Umbraco.Cms.Api.Management/Controllers/UserGroups/ByKeyUserGroupController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/UserGroups/ByKeyUserGroupController.cs
@@ -1,6 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Umbraco.Cms.Api.Management.ViewModels.DataType;
+using Umbraco.Cms.Api.Management.ViewModels.UserGroups;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Models.Membership;
 using Umbraco.Cms.Core.Services;
 
 namespace Umbraco.Cms.Api.Management.Controllers.UserGroups;
@@ -9,19 +11,53 @@ namespace Umbraco.Cms.Api.Management.Controllers.UserGroups;
 public class ByKeyUserGroupController : UserGroupsControllerBase
 {
     private readonly IUserService _userService;
+    private readonly IContentService _contentService;
+    private readonly IMediaService _mediaService;
 
-    public ByKeyUserGroupController(IUserService userService)
+    public ByKeyUserGroupController(
+        IUserService userService,
+        IContentService contentService,
+        IMediaService mediaService)
     {
         _userService = userService;
+        _contentService = contentService;
+        _mediaService = mediaService;
     }
 
     [HttpGet("{key:guid}")]
     [MapToApiVersion("1.0")]
-    [ProducesResponseType(typeof(DataTypeViewModel), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(UserGroupViewModel), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> ByKey(Guid key)
+    public async Task<ActionResult<UserGroupViewModel>> ByKey(Guid key)
     {
-        _userService.GetUserGroupByKey(key);
-        return Ok();
+        IUserGroup? userGroup = _userService.GetUserGroupByKey(key);
+
+        if (userGroup is null)
+        {
+            return NotFound();
+        }
+
+        IContent? contentStartNode = userGroup.StartContentId is not null
+            ? _contentService.GetById(userGroup.StartContentId.Value)
+            : null;
+
+        IMedia? mediaStartNode = userGroup.StartMediaId is not null
+            ? _mediaService.GetById(userGroup.StartMediaId.Value)
+            : null;
+
+        var viewModel = new UserGroupViewModel
+        {
+            Name = userGroup.Name,
+            Key = userGroup.Key,
+            ContentStartNodeKey = contentStartNode?.Key,
+            MediaStartNodeKey = mediaStartNode?.Key,
+            Icon = userGroup.Icon,
+            Languages = userGroup.AllowedLanguages,
+            HasAccessToAllLanguages = userGroup.HasAccessToAllLanguages,
+            Permissions = new[] {"ad", "hoc", "strings", "from", "save"},
+            Sections = userGroup.AllowedSections
+        };
+
+        return viewModel;
     }
 }
---
+++ src/Umbraco.Cms.Api.Management/ViewModels/UserGroups/UserGroupViewModel.cs
@@ -0,0 +1,24 @@
+﻿namespace Umbraco.Cms.Api.Management.ViewModels.UserGroups;
+
+public class UserGroupViewModel
+{
+    public required Guid Key { get; init; }
+
+    public string? Name { get; init; }
+
+    public string? Icon { get; init; }
+
+    public string Type => "user-group";
+
+    public required IEnumerable<string> Sections { get; init; }
+
+    public required IEnumerable<int> Languages { get; init; }
+
+    public required bool HasAccessToAllLanguages { get; init; }
+
+    public Guid? ContentStartNodeKey { get; init; }
+
+    public Guid? MediaStartNodeKey { get; init; }
+
+    public required IEnumerable<string> Permissions { get; init; }
+}
