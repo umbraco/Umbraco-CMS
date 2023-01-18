7dd1886594f40a6f5d0d173d239992fa9c248144
Nikolaj <nikolajlauridsen@protonmail.ch>
2023-01-18 14:07:04 +0100

    Bootstrap create and update endpoints

---
+++ src/Umbraco.Cms.Api.Management/Controllers/UserGroups/CreateUserGroupController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Management.ViewModels.UserGroups;
+
+namespace Umbraco.Cms.Api.Management.Controllers.UserGroups;
+
+public class CreateUserGroupController : UserGroupsControllerBase
+{
+    [HttpPost]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status201Created)]
+    public async Task<ActionResult> Create(UserGroupSaveModel userGroupSaveModel)
+    {
+        return await Task.FromResult(CreatedAtAction<ByKeyUserGroupController>(controller => nameof(controller.ByKey), Guid.Empty));
+    }
+}
---
+++ src/Umbraco.Cms.Api.Management/Controllers/UserGroups/UpdateUserGroupController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Management.ViewModels.DataType;
+
+namespace Umbraco.Cms.Api.Management.Controllers.UserGroups;
+
+public class UpdateUserGroupController : UserGroupsControllerBase
+{
+    [HttpPut]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<ActionResult> Update(Guid key, DataTypeUpdateModel dataTypeUpdateModel)
+    {
+        return await Task.FromResult(Ok());
+    }
+}
