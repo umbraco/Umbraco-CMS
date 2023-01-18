c18e1edea20dca7164283866191a11889843e5be
Nikolaj <nikolajlauridsen@protonmail.ch>
2023-01-18 13:47:48 +0100

    Split UserGroupViewModel into multiple models
    This is to make it possible to make endpoints more rest-ish

--- src/Umbraco.Cms.Api.Management/ViewModels/UserGroups/UserGroupViewModel.cs
+++ src/Umbraco.Cms.Api.Management/ViewModels/UserGroups/UserGroupViewModel.cs
@@ -1,24 +1,14 @@
 ﻿namespace Umbraco.Cms.Api.Management.ViewModels.UserGroups;
 
-public class UserGroupViewModel
+public class UserGroupViewModel : UserGroupBase
 {
+    /// <summary>
+    /// The key identifier for the user group.
+    /// </summary>
     public required Guid Key { get; init; }
 
-    public string? Name { get; init; }
-
-    public string? Icon { get; init; }
-
+    /// <summary>
+    /// The type of entity this model represents.
+    /// </summary>
     public string Type => "user-group";
-
-    public required IEnumerable<string> Sections { get; init; }
-
-    public required IEnumerable<int> Languages { get; init; }
-
-    public required bool HasAccessToAllLanguages { get; init; }
-
-    public Guid? ContentStartNodeKey { get; init; }
-
-    public Guid? MediaStartNodeKey { get; init; }
-
-    public required IEnumerable<string> Permissions { get; init; }
 }
