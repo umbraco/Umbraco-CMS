2e9f15b4189907b69797681f156f9a0c59e837e0
Nikolaj <nikolajlauridsen@protonmail.ch>
2023-01-18 15:42:32 +0100

    Add sanity check version of endpoint

--- src/Umbraco.Cms.Api.Management/Controllers/UserGroups/CreateUserGroupController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/UserGroups/CreateUserGroupController.cs
@@ -1,17 +1,33 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Management.Factories;
 using Umbraco.Cms.Api.Management.ViewModels.UserGroups;
+using Umbraco.Cms.Core.Models.Membership;
+using Umbraco.Cms.Core.Services;
 
 namespace Umbraco.Cms.Api.Management.Controllers.UserGroups;
 
 public class CreateUserGroupController : UserGroupsControllerBase
 {
+    private readonly IUserService _userService;
+    private readonly IUserGroupViewModelFactory _userGroupViewModelFactory;
+
+    public CreateUserGroupController(IUserService userService, IUserGroupViewModelFactory userGroupViewModelFactory)
+    {
+        _userService = userService;
+        _userGroupViewModelFactory = userGroupViewModelFactory;
+    }
+
     [HttpPost]
     [MapToApiVersion("1.0")]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status201Created)]
     public async Task<ActionResult> Create(UserGroupSaveModel userGroupSaveModel)
     {
-        return await Task.FromResult(CreatedAtAction<ByKeyUserGroupController>(controller => nameof(controller.ByKey), Guid.Empty));
+        // TODO: Validation etc...
+
+        IUserGroup group = _userGroupViewModelFactory.Create(userGroupSaveModel);
+        _userService.Save(group);
+        return await Task.FromResult(CreatedAtAction<ByKeyUserGroupController>(controller => nameof(controller.ByKey), group.Key));
     }
 }
