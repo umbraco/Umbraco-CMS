e77f150d7d3621dcf9534a2442df6383418e88cf
Nikolaj <nikolajlauridsen@protonmail.ch>
2023-01-20 14:21:03 +0100

    Update create controller to use the create method

--- src/Umbraco.Cms.Api.Management/Controllers/UserGroups/CreateUserGroupController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/UserGroups/CreateUserGroupController.cs
@@ -2,8 +2,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Api.Management.Factories;
 using Umbraco.Cms.Api.Management.ViewModels.UserGroups;
+using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Models.Membership;
+using Umbraco.Cms.Core.Security;
 using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Services.OperationStatus;
 
 namespace Umbraco.Cms.Api.Management.Controllers.UserGroups;
 
@@ -11,23 +14,38 @@ public class CreateUserGroupController : UserGroupsControllerBase
 {
     private readonly IUserService _userService;
     private readonly IUserGroupViewModelFactory _userGroupViewModelFactory;
+    private readonly IBackOfficeSecurityAccessor _backOfficeSecurityAccessor;
 
-    public CreateUserGroupController(IUserService userService, IUserGroupViewModelFactory userGroupViewModelFactory)
+    public CreateUserGroupController(
+        IUserService userService,
+        IUserGroupViewModelFactory userGroupViewModelFactory,
+        IBackOfficeSecurityAccessor backOfficeSecurityAccessor)
     {
         _userService = userService;
         _userGroupViewModelFactory = userGroupViewModelFactory;
+        _backOfficeSecurityAccessor = backOfficeSecurityAccessor;
     }
 
     [HttpPost]
     [MapToApiVersion("1.0")]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status201Created)]
-    public async Task<ActionResult> Create(UserGroupSaveModel userGroupSaveModel)
+    public async Task<IActionResult> Create(UserGroupSaveModel userGroupSaveModel)
     {
-        // TODO: Validation etc...
+        IUser? currentUser = _backOfficeSecurityAccessor.BackOfficeSecurity?.CurrentUser;
+        if (currentUser is null)
+        {
+            return UserGroupOperationStatusResult(UserGroupOperationStatus.MissingUser);
+        }
 
         IUserGroup group = _userGroupViewModelFactory.Create(userGroupSaveModel);
-        _userService.Save(group);
-        return await Task.FromResult(CreatedAtAction<ByKeyUserGroupController>(controller => nameof(controller.ByKey), group.Key));
+
+        Attempt<IUserGroup, UserGroupOperationStatus> result = _userService.Create(group, currentUser.Id);
+        if (result.Success)
+        {
+            CreatedAtAction<ByKeyUserGroupController>(controller => nameof(controller.ByKey), group.Key);
+        }
+
+        return UserGroupOperationStatusResult(result.Status);
     }
 }
