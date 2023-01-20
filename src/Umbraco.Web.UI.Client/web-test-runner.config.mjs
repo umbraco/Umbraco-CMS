316bfcb83e1cec8c280add19dbf451803d9a8d56
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-01-20 11:35:23 +0100

    move backend-api to its own lib folder

--- src/Umbraco.Web.UI.Client/web-test-runner.config.mjs
+++ src/Umbraco.Web.UI.Client/web-test-runner.config.mjs
@@ -13,7 +13,7 @@ export default {
 					imports: {
 						'src/': './src/',
 						'@umbraco-cms/models': './src/core/models/index.ts',
-						'@umbraco-cms/backend-api': './src/core/backend-api/index.ts',
+						'@umbraco-cms/backend-api': './libs/backend-api/index.ts',
 						'@umbraco-cms/context-api': './src/core/context-api/index.ts',
 						'@umbraco-cms/controller': './src/core/controller/index.ts',
 						'@umbraco-cms/element': './src/core/element/index.ts',
