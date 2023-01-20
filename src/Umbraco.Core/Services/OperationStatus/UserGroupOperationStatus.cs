52437777cdc882d1eb3892a21b96476440dd3a1b
Nikolaj <nikolajlauridsen@protonmail.ch>
2023-01-20 13:36:16 +0100

    Add UserGroupOperationStatuses

---
+++ src/Umbraco.Core/Services/OperationStatus/UserGroupOperationStatus.cs
@@ -0,0 +1,13 @@
+﻿namespace Umbraco.Cms.Core.Services.OperationStatus;
+
+public enum UserGroupOperationStatus
+{
+    Success,
+    NotFound,
+    AlreadyExists,
+    DuplicateAlias,
+    MissingUser,
+    UnauthorizedMissingSections,
+    UnauthorizedStartNodes,
+    CancelledByNotification
+}
