b1e4f7dff84997fbf0f28511bd820ac5688af3e2
Elitsa Marinovska <elm@umbraco.dk>
2023-01-23 15:30:47 +0100

    Rename ViewPermission controller to ValidateLogFileSize

--- src/Umbraco.Cms.Api.Management/Controllers/LogViewer/ValidateLogFileSizeLogViewerController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/LogViewer/ValidateLogFileSizeLogViewerController.cs
@@ -4,11 +4,11 @@ using Umbraco.Cms.Core.Services;
 
 namespace Umbraco.Cms.Api.Management.Controllers.LogViewer;
 
-public class ViewPermissionLogViewerController : LogViewerControllerBase
+public class ValidateLogFileSizeLogViewerController : LogViewerControllerBase
 {
     private readonly ILogViewerService _logViewerService;
 
-    public ViewPermissionLogViewerController(ILogViewerService logViewerService) => _logViewerService = logViewerService;
+    public ValidateLogFileSizeLogViewerController(ILogViewerService logViewerService) => _logViewerService = logViewerService;
 
     /// <summary>
     ///     Gets a value indicating whether or not you are able to view logs for a specified date range.
@@ -16,7 +16,7 @@ public class ViewPermissionLogViewerController : LogViewerControllerBase
     /// <param name="startDate">The start date for the date range (can be null).</param>
     /// <param name="endDate">The end date for the date range (can be null).</param>
     /// <returns>The boolean result.</returns>
-    [HttpGet("view-permission")]
+    [HttpGet("validate-logs-size")]
     [MapToApiVersion("1.0")]
     [ProducesResponseType(StatusCodes.Status200OK)]
     public async Task<ActionResult<bool>> CanViewLogs(DateTime? startDate = null, DateTime? endDate = null)
