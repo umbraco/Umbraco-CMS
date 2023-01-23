30b1bb4abc295e52977039ca44aa04325e2cc64a
Niels Lyngsø <niels.lyngso@gmail.com>
2023-01-23 22:44:37 +0100

    correct type usages

--- src/Umbraco.Web.UI.Client/src/backoffice/shared/collection/dashboards/dashboard-collection.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/shared/collection/dashboards/dashboard-collection.element.ts
@@ -3,12 +3,13 @@ import { css, html } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
 import '../collection.element';
 import { ifDefined } from 'lit-html/directives/if-defined.js';
-import { UmbMediaDetailStore } from 'src/backoffice/media/media/media.detail.store';
+import { UmbMediaTreeStore } from '../../../media/media/media.tree.store';
 import {
 	UmbCollectionContext,
 	UMB_COLLECTION_CONTEXT_TOKEN,
-} from 'src/backoffice/shared/collection/collection.context';
-import type { ManifestDashboardCollection, MediaDetails } from '@umbraco-cms/models';
+} from '../../../shared/collection/collection.context';
+import type { ManifestDashboardCollection } from '@umbraco-cms/models';
+import type { FolderTreeItem } from '@umbraco-cms/backend-api';
 import { UmbLitElement } from '@umbraco-cms/element';
 
 @customElement('umb-dashboard-collection')
@@ -26,7 +27,8 @@ export class UmbDashboardCollectionElement extends UmbLitElement {
 		`,
 	];
 
-	private _collectionContext?: UmbCollectionContext<MediaDetails, UmbMediaDetailStore>;
+	// TODO: Use the right type here:
+	private _collectionContext?: UmbCollectionContext<FolderTreeItem, UmbMediaTreeStore>;
 
 	public manifest!: ManifestDashboardCollection;
 
