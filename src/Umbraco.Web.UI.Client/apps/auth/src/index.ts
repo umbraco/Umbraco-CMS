6c287e4b4bac6c1ebac764b258130e13303a1d4d
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-01-24 12:13:20 +0100

    add uui to auth

--- src/Umbraco.Web.UI.Client/apps/auth/src/index.ts
+++ src/Umbraco.Web.UI.Client/apps/auth/src/index.ts
@@ -2,6 +2,8 @@ import { ManifestTypes } from '@umbraco-cms/extensions-registry';
 import { umbExtensionsRegistry } from '@umbraco-cms/extensions-api';
 import { manifests as externalLoginProviders } from './external-login-providers/manifests';
 
+import '@umbraco-ui/uui';
+import '@umbraco-ui/uui-css';
 import './login.element';
 
 const registerExtensions = (manifests: Array<ManifestTypes>) => {
