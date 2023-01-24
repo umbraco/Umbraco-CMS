87baa320958ed2a06de8c7198c568c4ee211f052
Elitsa Marinovska <elm@umbraco.dk>
2023-01-24 11:24:51 +0100

    Remove methods obsoletion

--- src/Umbraco.Infrastructure/Logging/Viewer/ILogViewerConfig.cs
+++ src/Umbraco.Infrastructure/Logging/Viewer/ILogViewerConfig.cs
@@ -3,15 +3,12 @@ namespace Umbraco.Cms.Core.Logging.Viewer;
 [Obsolete("Use ILogViewerService instead. Scheduled for removal in Umbraco 15.")]
 public interface ILogViewerConfig
 {
-    [Obsolete("Use ILogViewerService.GetSavedLogQueriesAsync instead. Scheduled for removal in Umbraco 15.")]
     IReadOnlyList<SavedLogSearch> GetSavedSearches();
 
-    [Obsolete("Use ILogViewerService.AddSavedLogQueryAsync instead. Scheduled for removal in Umbraco 15.")]
     IReadOnlyList<SavedLogSearch> AddSavedSearch(string name, string query);
 
     [Obsolete("Use the overload that only takes a 'name' parameter instead. This will be removed in Umbraco 14.")]
     IReadOnlyList<SavedLogSearch> DeleteSavedSearch(string name, string query);
 
-    [Obsolete("Use ILogViewerService.DeleteSavedLogQueryAsync instead. Scheduled for removal in Umbraco 15.")]
     IReadOnlyList<SavedLogSearch> DeleteSavedSearch(string name) => DeleteSavedSearch(name, string.Empty);
 }
