16ea46ea804a1e4042754004feab20c0fbc2eb50
Jesper Møller Jensen <26099018+JesmoDev@users.noreply.github.com>
2023-01-26 02:35:24 +0100

    cleanup

--- src/Umbraco.Web.UI.Client/src/backoffice/settings/languages/workspace/language-root/language-root-table-name-column-layout.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/settings/languages/workspace/language-root/language-root-table-name-column-layout.element.ts
@@ -7,7 +7,7 @@ export class UmbLanguageRootTableNameColumnLayoutElement extends LitElement {
 	static styles = [UUITextStyles, css``];
 
 	@property({ attribute: false })
-  value!: { key: string, name: string };
+	value!: { key: string; name: string };
 
 	render() {
 		if (!this.value) return nothing;
--- src/Umbraco.Web.UI.Client/src/backoffice/settings/languages/workspace/language/language-workspace.context.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/settings/languages/workspace/language/language-workspace.context.ts
@@ -1,9 +1,9 @@
-import { UmbLanguageStore, UmbLanguageStoreItemType } from '../../language.store';
 import type { LanguageDetails } from '@umbraco-cms/models';
 import { UmbControllerHostInterface } from '@umbraco-cms/controller';
+import { UmbObserverController } from '@umbraco-cms/observable-api';
+import { UmbLanguageStore, UmbLanguageStoreItemType, UMB_LANGUAGE_STORE_CONTEXT_TOKEN } from '../../language.store';
 import { UniqueBehaviorSubject } from 'src/core/observable-api/unique-behavior-subject';
 import { UmbContextConsumerController } from 'src/core/context-api/consume/context-consumer.controller';
-import { UmbObserverController } from '@umbraco-cms/observable-api';
 
 const DefaultLanguageData: UmbLanguageStoreItemType = {
 	id: 0,
@@ -32,12 +32,8 @@ export class UmbWorkspaceLanguageContext {
 		this._data = new UniqueBehaviorSubject<LanguageDetails>(DefaultLanguageData);
 		this.data = this._data.asObservable();
 
-		new UmbContextConsumerController(host, 'umbLanguageStore', (_instance: UmbLanguageStore) => {
+		new UmbContextConsumerController(host, UMB_LANGUAGE_STORE_CONTEXT_TOKEN, (_instance: UmbLanguageStore) => {
 			this._store = _instance;
-			if (!this._store) {
-				// TODO: make sure to break the application in a good way.
-				return;
-			}
 
 			this._observeStore();
 		});
