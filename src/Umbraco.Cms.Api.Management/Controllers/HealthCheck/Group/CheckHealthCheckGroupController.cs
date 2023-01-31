7a01b21642940c139416e2ab76f284fbbc4937f0
Elitsa Marinovska <elm@umbraco.dk>
2023-01-31 09:28:56 +0100

    Create a "check" endpoint

--- src/Umbraco.Cms.Api.Management/Controllers/HealthCheck/Group/CheckHealthCheckGroupController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/HealthCheck/Group/CheckHealthCheckGroupController.cs
@@ -7,41 +7,41 @@ using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Api.Management.Controllers.HealthCheck.Group;
 
-public class ByNameWithResultHealthCheckGroupController : HealthCheckGroupControllerBase
+public class CheckHealthCheckGroupController : HealthCheckGroupControllerBase
 {
     private readonly HealthCheckCollection _healthChecks;
-    private readonly IHealthCheckGroupWithResultViewModelFactory _healthCheckGroupWithResultViewModelFactory;
+    private readonly IHealthCheckGroupViewModelFactory _healthCheckGroupViewModelFactory;
 
-    public ByNameWithResultHealthCheckGroupController(
+    public CheckHealthCheckGroupController(
         HealthCheckCollection healthChecks,
-        IHealthCheckGroupWithResultViewModelFactory healthCheckGroupWithResultViewModelFactory)
+        IHealthCheckGroupViewModelFactory healthCheckGroupViewModelFactory)
     {
         _healthChecks = healthChecks;
-        _healthCheckGroupWithResultViewModelFactory = healthCheckGroupWithResultViewModelFactory;
+        _healthCheckGroupViewModelFactory = healthCheckGroupViewModelFactory;
     }
 
     /// <summary>
-    ///     Gets a health check group with all its health checks by a group name.
+    ///     Check all health checks in the group with a given group name.
     /// </summary>
     /// <param name="name">The name of the group.</param>
     /// <remarks>The health check result(s) will be included as part of the health checks.</remarks>
     /// <returns>The health check group or not found result.</returns>
-    [HttpGet("{name}")]
+    [HttpPost("{name}/check")]
     [MapToApiVersion("1.0")]
-    [ProducesResponseType(typeof(NotFoundResult), StatusCodes.Status404NotFound)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
     [ProducesResponseType(typeof(HealthCheckGroupWithResultViewModel), StatusCodes.Status200OK)]
-    public async Task<ActionResult<HealthCheckGroupWithResultViewModel>> ByName(string name)
+    public async Task<ActionResult<HealthCheckGroupWithResultViewModel>> ByNameWithResult(string name)
     {
-        IEnumerable<IGrouping<string?, Core.HealthChecks.HealthCheck>> groups = _healthCheckGroupWithResultViewModelFactory
+        IEnumerable<IGrouping<string?, Core.HealthChecks.HealthCheck>> groups = _healthCheckGroupViewModelFactory
             .CreateGroupingFromHealthCheckCollection(_healthChecks);
 
         IGrouping<string?, Core.HealthChecks.HealthCheck>? group = groups.FirstOrDefault(x => x.Key.InvariantEquals(name.Trim()));
 
         if (group is null)
         {
-            return await Task.FromResult(NotFound());
+            return NotFound();
         }
 
-        return await Task.FromResult(Ok(_healthCheckGroupWithResultViewModelFactory.CreateHealthCheckGroupWithResultViewModel(group)));
+        return await Task.FromResult(Ok(_healthCheckGroupViewModelFactory.CreateHealthCheckGroupWithResultViewModel(group)));
     }
 }
