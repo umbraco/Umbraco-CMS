7cb874b3234578753ad89cdee4f762348af6194b
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-01-31 10:12:30 +0100

    only test firefox and webkit

--- src/Umbraco.Web.UI.Client/web-test-runner.config.mjs
+++ src/Umbraco.Web.UI.Client/web-test-runner.config.mjs
@@ -32,7 +32,6 @@ export default {
 		}),
 	],
 	browsers: [
-		playwrightLauncher({ product: 'chromium' }),
 		playwrightLauncher({ product: 'firefox' }),
 		playwrightLauncher({ product: 'webkit' }),
 	],
