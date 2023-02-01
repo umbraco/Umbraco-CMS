801cc24f5b21b445cfff736b5954f250d097b3f7
Niels Lyngsø <niels.lyngso@gmail.com>
2023-02-01 14:20:01 +0100

    loader should return string

--- src/Umbraco.Web.UI.Client/libs/extensions-registry/theme.models.ts
+++ src/Umbraco.Web.UI.Client/libs/extensions-registry/theme.models.ts
@@ -2,7 +2,7 @@ import type { ManifestWithLoader } from "./models";
 
 
 // TODO: make or find type for JS Module with default export: Would be nice to support css file directly.
-export interface ManifestTheme extends ManifestWithLoader<any> {
+export interface ManifestTheme extends ManifestWithLoader<string> {
 	type: 'theme';
 	css?: string;
 }
