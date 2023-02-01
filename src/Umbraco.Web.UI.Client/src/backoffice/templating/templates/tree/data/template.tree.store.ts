9409a2223fc88f4ca9904fdd7cb48c6b04b7b9fb
Mads Rasmussen <madsr@hey.com>
2023-02-01 21:21:17 +0100

    remove unused

--- src/Umbraco.Web.UI.Client/src/backoffice/templating/templates/tree/data/template.tree.store.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/templating/templates/tree/data/template.tree.store.ts
@@ -1,6 +1,6 @@
 import { EntityTreeItem } from '@umbraco-cms/backend-api';
 import { UmbContextToken } from '@umbraco-cms/context-api';
-import { createObservablePart, ArrayState } from '@umbraco-cms/observable-api';
+import { ArrayState } from '@umbraco-cms/observable-api';
 import { UmbStoreBase } from '@umbraco-cms/store';
 import { UmbControllerHostInterface } from '@umbraco-cms/controller';
 
