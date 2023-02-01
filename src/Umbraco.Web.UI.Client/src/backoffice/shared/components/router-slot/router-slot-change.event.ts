c80d86e5f17198050e32271ca7900aad17867dc9
Mads Rasmussen <madsr@hey.com>
2023-02-01 14:30:45 +0100

    fix spelling mistake in filename

--- src/Umbraco.Web.UI.Client/src/backoffice/shared/components/router-slot/router-slot-change.event.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/shared/components/router-slot/router-slot-change.event.ts
@@ -1,7 +1,7 @@
 import { UUIEvent } from '@umbraco-ui/uui-base/lib/events';
 import type { UmbRouterSlotElement } from './router-slot.element';
 export class UmbRouterSlotChangeEvent extends UUIEvent<never, UmbRouterSlotElement> {
-    constructor() {
-			super('change')
-		}
+	constructor() {
+		super('change');
+	}
 }
