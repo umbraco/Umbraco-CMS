270e646a0a78479edcf78d8b16579f3c7f873f6a
Elitsa Marinovska <21998037+elit0451@users.noreply.github.com>
2023-02-02 08:59:35 +0100

    Content API: Add start-node header value to deal with url collisions (#13746)
    * Use start-node header value to deal with url collisions
    
    * Cleanup
    
    * Rename "url" param to "path"
    
    * Adding a start node service to get the start-node header value
    
    * Trim '/' from both beginning and end

--- src/Umbraco.Cms.Api.Content/Controllers/ByRouteContentApiController.cs
+++ src/Umbraco.Cms.Api.Content/Controllers/ByRouteContentApiController.cs
@@ -1,7 +1,9 @@
 using System.Net;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Options;
 using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Configuration.Models;
 using Umbraco.Cms.Core.ContentApi;
 using Umbraco.Cms.Core.Models.ContentApi;
 using Umbraco.Cms.Core.Models.PublishedContent;
@@ -11,22 +13,35 @@ namespace Umbraco.Cms.Api.Content.Controllers;
 
 public class ByRouteContentApiController : ContentApiControllerBase
 {
-    public ByRouteContentApiController(IPublishedSnapshotAccessor publishedSnapshotAccessor, IApiContentBuilder apiContentBuilder)
+    private readonly GlobalSettings _globalSettings;
+    private readonly IStartNodeService _startNodeService;
+
+    public ByRouteContentApiController(
+        IPublishedSnapshotAccessor publishedSnapshotAccessor,
+        IApiContentBuilder apiContentBuilder,
+        IOptions<GlobalSettings> globalSettings,
+        IStartNodeService startNodeService)
         : base(publishedSnapshotAccessor, apiContentBuilder)
     {
+        _globalSettings = globalSettings.Value;
+        _startNodeService = startNodeService;
     }
 
     /// <summary>
     ///     Gets a content item by route.
     /// </summary>
-    /// <param name="url">The path to the content item.</param>
+    /// <param name="path">The path to the content item.</param>
+    /// <remarks>
+    ///     Optional path for the start node of the content item
+    ///     can be added through a "start-node" header.
+    /// </remarks>
     /// <returns>The content item or not found result.</returns>
-    [HttpGet("{url}")]
+    [HttpGet("{path}")]
     [MapToApiVersion("1.0")]
     [ProducesResponseType(typeof(IApiContent), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
     [ProducesResponseType(StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> ByRoute(string url)
+    public async Task<IActionResult> ByRoute(string path)
     {
         IPublishedContentCache? contentCache = GetContentCache();
 
@@ -35,7 +50,7 @@ public class ByRouteContentApiController : ContentApiControllerBase
             return BadRequest(ContentCacheNotFoundProblemDetails());
         }
 
-        var decodedPath = $"/{WebUtility.UrlDecode(url).TrimStart(Constants.CharArrays.ForwardSlash)}";
+        var decodedPath = ConstructRoute(path);
 
         IPublishedContent? contentItem = contentCache.GetByRoute(decodedPath);
 
@@ -45,5 +60,25 @@ public class ByRouteContentApiController : ContentApiControllerBase
         }
 
         return await Task.FromResult(Ok(ApiContentBuilder.Build(contentItem)));
+    }
+
+    // Decode the node path and check "start-node" header if the top level node is not hidden
+    private string ConstructRoute(string path)
+    {
+        var decodedPath = $"/{WebUtility.UrlDecode(path).TrimStart(Constants.CharArrays.ForwardSlash)}";
+
+        if (_globalSettings.HideTopLevelNodeFromPath == false)
+        {
+            // Construct the path, using the value from "start-node" header
+            string? startNodePath = _startNodeService.GetStartNode();
+
+            if (startNodePath is not null)
+            {
+                var combinedPath = $"/{startNodePath}{decodedPath}";
+                return combinedPath;
+            }
+        }
+
+        return decodedPath;
     }
 }
