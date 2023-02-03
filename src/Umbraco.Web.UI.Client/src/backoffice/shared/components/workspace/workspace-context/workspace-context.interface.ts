2e8f102566c2d124350dd2d99241699498393cfa
Niels Lyngsø <niels.lyngso@gmail.com>
2023-02-03 10:42:21 +0100

    use interface

--- src/Umbraco.Web.UI.Client/src/backoffice/shared/components/workspace/workspace-context/workspace-context.interface.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/shared/components/workspace/workspace-context/workspace-context.interface.ts
@@ -4,13 +4,10 @@ export interface UmbWorkspaceContextInterface<T = unknown> {
 
 	readonly data: Observable<T>;
 
-	getUnique(): string | undefined;
+
+	//getUnique(): string | undefined;
 
 	getData(): T;
-
-	load(unique: string): void;
-
-	create(parentUnique: string | null): void;
 
 	destroy(): void;
 }
