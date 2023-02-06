8bdd8d5b4c4773d691d9a891d33b7ae6c2906101
Zeegaan <nge@umbraco.dk>
2023-02-06 14:03:30 +0100

    Create EfCoreScope test

---
+++ tests/Umbraco.Tests.Integration/Umbraco.Persistence.EFCore/EfCoreScopeTest.cs
@@ -0,0 +1,37 @@
+﻿using NUnit.Framework;
+using Umbraco.Cms.Persistence.EFCore.Scoping;
+using Umbraco.Cms.Tests.Common.Testing;
+using Umbraco.Cms.Tests.Integration.Testing;
+
+namespace Umbraco.Cms.Tests.Integration.Umbraco.Persistence.EFCore;
+
+[TestFixture]
+[UmbracoTest(Database = UmbracoTestOptions.Database.NewEmptyPerTest)]
+public class EfCoreScopeTest : UmbracoIntegrationTest
+{
+    private IEfCoreScopeProvider EfCoreScopeProvider =>
+        GetRequiredService<IEfCoreScopeProvider>();
+
+    [Test]
+    public void CanCreateScope() =>
+        Assert.DoesNotThrow(() =>
+        {
+            using var scope = EfCoreScopeProvider.CreateScope();
+            scope.Complete();
+        });
+
+    [Test]
+    public void CanCreateScopeTwice() =>
+        Assert.DoesNotThrow(() =>
+        {
+            using (var scope = EfCoreScopeProvider.CreateScope())
+            {
+                scope.Complete();
+            }
+
+            using (var scopeTwo = EfCoreScopeProvider.CreateScope())
+            {
+                scopeTwo.Complete();
+            }
+        });
+}
