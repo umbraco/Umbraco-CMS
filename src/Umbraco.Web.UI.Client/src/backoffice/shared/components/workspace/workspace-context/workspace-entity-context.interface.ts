473ebd582e83b2edae790f29ab92a8f952c390fd
Niels Lyngsø <niels.lyngso@gmail.com>
2023-02-06 16:22:35 +0100

    remove getStore from workspace interface

--- src/Umbraco.Web.UI.Client/src/backoffice/shared/components/workspace/workspace-context/workspace-entity-context.interface.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/shared/components/workspace/workspace-context/workspace-entity-context.interface.ts
@@ -1,6 +1,5 @@
 import type { Observable } from 'rxjs';
 import { UmbWorkspaceContextInterface } from './workspace-context.interface';
-import { UmbEntityDetailStore } from '@umbraco-cms/store';
 
 export interface UmbWorkspaceEntityContextInterface<T = unknown> extends UmbWorkspaceContextInterface<T> {
 	readonly name: Observable<string | undefined>;
@@ -9,8 +8,6 @@ export interface UmbWorkspaceEntityContextInterface<T = unknown> extends UmbWork
 	getEntityType(): string;
 
 	getData(): T;
-
-	getStore(): UmbEntityDetailStore<T> | undefined;
 
 	setPropertyValue(alias: string, value: unknown): void;
 
