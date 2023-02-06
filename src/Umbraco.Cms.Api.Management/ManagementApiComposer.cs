73ff68e1a3196bbb2a8348c4d94dd3c3db78db4a
Elitsa Marinovska <elm@umbraco.dk>
2023-02-06 11:19:14 +0100

    Create PackageBuilderExtensions

--- src/Umbraco.Cms.Api.Management/ManagementApiComposer.cs
+++ src/Umbraco.Cms.Api.Management/ManagementApiComposer.cs
@@ -37,6 +37,7 @@ public class ManagementApiComposer : IComposer
             .AddDataTypes()
             .AddTemplates()
             .AddLogViewer()
+            .AddPackages()
             .AddBackOfficeAuthentication()
             .AddApiVersioning()
             .AddSwaggerGen();
