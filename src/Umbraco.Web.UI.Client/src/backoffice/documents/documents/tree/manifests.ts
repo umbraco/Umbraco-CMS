4de70d8a99c07a03e2c9043f1826a9587fb07e94
Mads Rasmussen <madsr@hey.com>
2023-02-07 20:24:40 +0100

    remove tree item actions

--- src/Umbraco.Web.UI.Client/src/backoffice/documents/documents/tree/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/documents/documents/tree/manifests.ts
@@ -1,5 +1,5 @@
 import { UmbDocumentRepository } from '../repository/document.repository';
-import type { ManifestTree, ManifestTreeItemAction } from '@umbraco-cms/models';
+import type { ManifestTree } from '@umbraco-cms/models';
 
 const treeAlias = 'Umb.Tree.Documents';
 
@@ -12,31 +12,4 @@ const tree: ManifestTree = {
 	},
 };
 
-const treeItemActions: Array<ManifestTreeItemAction> = [
-	{
-		type: 'treeItemAction',
-		alias: 'Umb.TreeItemAction.Document.Create',
-		name: 'Document Tree Item Action Create',
-		loader: () => import('./actions/action-document-create.element'),
-		weight: 100,
-		meta: {
-			entityType: 'document',
-			label: 'Create',
-			icon: 'add',
-		},
-	},
-	{
-		type: 'treeItemAction',
-		alias: 'Umb.TreeItemAction.Document.Paged',
-		name: 'Document Tree Item Action Paged',
-		loader: () => import('./actions/action-document-paged.element'),
-		weight: 100,
-		meta: {
-			entityType: 'document',
-			label: 'Paged',
-			icon: 'favorite',
-		},
-	},
-];
-
-export const manifests = [tree, ...treeItemActions];
+export const manifests = [tree];
