6618a9dead9212cb7585100b9e085a840c3083f2
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2023-02-07 20:59:32 +0100

    Implement EFCore version of DistributedLockingMechanism

--- src/Umbraco.Cms.Persistence.EFCore/Extensions/UmbracoEFCoreServiceCollectionExtensions.cs
+++ src/Umbraco.Cms.Persistence.EFCore/Extensions/UmbracoEFCoreServiceCollectionExtensions.cs
@@ -1,6 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Umbraco.Cms.Core.DistributedLocking;
 using Umbraco.Cms.Core.Persistence;
 using Umbraco.Cms.Infrastructure.Migrations.Install;
 using Umbraco.Cms.Persistence.EFCore;
@@ -33,6 +34,7 @@ public static class UmbracoEFCoreServiceCollectionExtensions
         services.AddUnique<IEFCoreScopeAccessor, EFCoreScopeAccessor>();
         services.AddUnique<IEfCoreScopeProvider, EfCoreScopeProvider>();
         services.AddUnique<IHttpEFCoreScopeReference, HttpEFCoreScopeReference>();
+        services.AddUnique<IDistributedLockingMechanism, SqliteEFCoreDistributedLockingMechanism>();
 
         return services;
     }
