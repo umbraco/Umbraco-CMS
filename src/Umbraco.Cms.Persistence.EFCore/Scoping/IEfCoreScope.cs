90e90a25dd04db61bf2050abcf76cb98184f87c8
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2023-02-07 11:38:50 +0100

    Formatting of interface

--- src/Umbraco.Cms.Persistence.EFCore/Scoping/IEfCoreScope.cs
+++ src/Umbraco.Cms.Persistence.EFCore/Scoping/IEfCoreScope.cs
@@ -4,8 +4,29 @@ namespace Umbraco.Cms.Persistence.EFCore.Scoping;
 
 public interface IEfCoreScope : IDisposable
 {
+    /// <summary>
+    /// Instance ID of the current scope.
+    /// </summary>
     public Guid InstanceId { get; }
+
+    /// <summary>
+    /// Executes the given function on the database.
+    /// </summary>
+    /// <param name="method">Function to execute.</param>
+    /// <typeparam name="T">Type to use and return.</typeparam>
+    /// <returns></returns>
     public Task<T> ExecuteWithContextAsync<T>(Func<UmbracoEFContext, Task<T>> method);
+
+    /// <summary>
+    /// Executes the given function on the database.
+    /// </summary>
+    /// <param name="method">Function to execute.</param>
+    /// <typeparam name="T">Type to use and return.</typeparam>
+    /// <returns></returns>
     public Task ExecuteWithContextAsync<T>(Func<UmbracoEFContext, Task> method);
+
+    /// <summary>
+    /// Completes the scope, if this is not call, the transaction will be rolled back.
+    /// </summary>
     public void Complete();
 }
