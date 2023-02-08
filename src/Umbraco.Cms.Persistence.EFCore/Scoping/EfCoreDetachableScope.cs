83932034a466738a82aab86db57e46a78ac20f0f
Zeegaan <nge@umbraco.dk>
2023-02-08 12:56:18 +0100

    Move properties in correct order

--- src/Umbraco.Cms.Persistence.EFCore/Scoping/EfCoreDetachableScope.cs
+++ src/Umbraco.Cms.Persistence.EFCore/Scoping/EfCoreDetachableScope.cs
@@ -7,39 +7,6 @@ internal class EfCoreDetachableScope : EfCoreScope
     private readonly IEFCoreScopeAccessor _efCoreScopeAccessor;
     private readonly EfCoreScopeProvider _efCoreScopeProvider;
 
-    // Properties needed for DetachedScope
-    public EfCoreScope? OriginalScope { get; set; }
-
-    public IScopeContext? OriginalContext { get; set; }
-
-    public bool Detachable { get; set; }
-
-    public bool Attached { get; set; }
-
-    private void HandleDetachedScopes()
-    {
-        if (Detachable)
-        {
-            // get out of the way, restore original
-
-            // TODO: Difficult to know if this is correct since this is all required
-            // by Deploy which I don't fully understand since there is limited tests on this in the CMS
-            if (OriginalScope != _efCoreScopeAccessor.AmbientScope)
-            {
-                _efCoreScopeProvider.PopAmbientScope();
-            }
-
-            if (OriginalContext != _efCoreScopeProvider.AmbientScopeContext)
-            {
-                _efCoreScopeProvider.PopAmbientScopeContext();
-            }
-
-            Attached = false;
-            OriginalScope = null;
-            OriginalContext = null;
-        }
-    }
-
     public EfCoreDetachableScope(
         IUmbracoEfCoreDatabaseFactory efCoreDatabaseFactory,
         IEFCoreScopeAccessor efCoreScopeAccessor,
@@ -77,4 +44,42 @@ internal class EfCoreDetachableScope : EfCoreScope
             parentScope,
             scopeContext) =>
         throw new NotImplementedException();
+
+    public EfCoreScope? OriginalScope { get; set; }
+
+    public IScopeContext? OriginalContext { get; set; }
+
+    public bool Detachable { get; }
+
+    public bool Attached { get; set; }
+
+    public new void Dispose()
+    {
+        HandleDetachedScopes();
+        base.Dispose();
+    }
+
+    private void HandleDetachedScopes()
+    {
+        if (Detachable)
+        {
+            // get out of the way, restore original
+
+            // TODO: Difficult to know if this is correct since this is all required
+            // by Deploy which I don't fully understand since there is limited tests on this in the CMS
+            if (OriginalScope != _efCoreScopeAccessor.AmbientScope)
+            {
+                _efCoreScopeProvider.PopAmbientScope();
+            }
+
+            if (OriginalContext != _efCoreScopeProvider.AmbientScopeContext)
+            {
+                _efCoreScopeProvider.PopAmbientScopeContext();
+            }
+
+            Attached = false;
+            OriginalScope = null;
+            OriginalContext = null;
+        }
+    }
 }
