27160dbc8439e8fdf3ca55b5677dd5d08a52e20a
Elitsa Marinovska <elm@umbraco.dk>
2023-02-08 15:40:13 +0100

    Implement RunMigrationPackageController

--- src/Umbraco.Cms.Api.Management/Controllers/Package/PackageControllerBase.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/Package/PackageControllerBase.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Common.Builders;
 using Umbraco.Cms.Api.Management.Routing;
+using Umbraco.Cms.Core.Services.OperationStatus;
 
 namespace Umbraco.Cms.Api.Management.Controllers.Package;
 
@@ -9,4 +12,15 @@ namespace Umbraco.Cms.Api.Management.Controllers.Package;
 [ApiVersion("1.0")]
 public abstract class PackageControllerBase : ManagementApiControllerBase
 {
+    protected IActionResult PackageMigrationOperationStatusResult(PackageMigrationOperationStatus status) =>
+        status switch
+        {
+            PackageMigrationOperationStatus.NotFound => NotFound("No migration plans were found for that package"),
+            PackageMigrationOperationStatus.CancelledByFailedMigration => Conflict(new ProblemDetailsBuilder()
+                .WithTitle("Package migration failed")
+                .WithDetail("Check log for full details about the failed migration.")
+                .WithStatus(StatusCodes.Status409Conflict)
+                .Build()),
+            _ => StatusCode(StatusCodes.Status500InternalServerError, "Unknown package migration operation status")
+        };
 }
