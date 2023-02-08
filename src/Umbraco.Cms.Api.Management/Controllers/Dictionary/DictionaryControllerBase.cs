9ce79120f70626d2c962bb1248415c217e94bbc2
Elitsa Marinovska <elm@umbraco.dk>
2023-02-08 15:35:06 +0100

    Add Status to DictionaryControllerBase ProblemDetails

--- src/Umbraco.Cms.Api.Management/Controllers/Dictionary/DictionaryControllerBase.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/Dictionary/DictionaryControllerBase.cs
@@ -1,4 +1,4 @@
-﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Api.Common.Builders;
 using Umbraco.Cms.Api.Management.Routing;
@@ -19,6 +19,7 @@ public abstract class DictionaryControllerBase : ManagementApiControllerBase
             DictionaryItemOperationStatus.DuplicateItemKey => Conflict(new ProblemDetailsBuilder()
                 .WithTitle("Duplicate dictionary item name detected")
                 .WithDetail("Another dictionary item exists with the same name. Dictionary item names must be unique.")
+                .WithStatus(StatusCodes.Status409Conflict)
                 .Build()),
             DictionaryItemOperationStatus.ItemNotFound => NotFound("The dictionary item could not be found"),
             DictionaryItemOperationStatus.ParentNotFound => NotFound("The dictionary item parent could not be found"),
