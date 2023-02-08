6645ecb41c68feaeec6bdd125a38e651aa201142
Mads Rasmussen <madsr@hey.com>
2023-02-08 11:13:43 +0100

    add create and delete entity actions

--- src/Umbraco.Web.UI.Client/src/backoffice/templating/templates/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/templating/templates/manifests.ts
@@ -1,6 +1,13 @@
 import { manifests as repositoryManifests } from './repository/manifests';
 import { manifests as sidebarMenuItemManifests } from './sidebar-menu-item/manifests';
 import { manifests as treeManifests } from './tree/manifests';
+import { manifests as entityActionsManifests } from './entity-actions/manifests';
 import { manifests as workspaceManifests } from './workspace/manifests';
 
-export const manifests = [...repositoryManifests, ...sidebarMenuItemManifests, ...treeManifests, ...workspaceManifests];
+export const manifests = [
+	...repositoryManifests,
+	...sidebarMenuItemManifests,
+	...treeManifests,
+	...entityActionsManifests,
+	...workspaceManifests,
+];
