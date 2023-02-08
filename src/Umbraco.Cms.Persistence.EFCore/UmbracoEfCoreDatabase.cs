ec01d3130ff0823c92fbcca426126e8899ef1ca0
Zeegaan <nge@umbraco.dk>
2023-02-08 12:57:45 +0100

    Refactor EFCoreDatabase

--- src/Umbraco.Cms.Persistence.EFCore/UmbracoEfCoreDatabase.cs
+++ src/Umbraco.Cms.Persistence.EFCore/UmbracoEfCoreDatabase.cs
@@ -1,25 +1,19 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Umbraco.Cms.Core.Persistence;
-using Umbraco.Cms.Persistence.EFCore;
+﻿using Umbraco.Cms.Core.Persistence;
 using Umbraco.Cms.Persistence.EFCore.Entities;
 
-namespace Umbraco.Cms.Infrastructure.Persistence.EfCore;
+namespace Umbraco.Cms.Persistence.EFCore;
 
 internal class UmbracoEfCoreDatabase : IUmbracoEfCoreDatabase
 {
     private readonly IDatabaseInfo _databaseInfo;
-    private readonly UmbracoDbContextFactory _umbracoDbContextFactory;
     private readonly Guid _instanceGuid = Guid.NewGuid();
     private string? _instanceId;
 
     public UmbracoEfCoreDatabase(IDatabaseInfo databaseInfo, UmbracoEFContext umbracoEfContext, UmbracoDbContextFactory umbracoDbContextFactory)
     {
         _databaseInfo = databaseInfo;
-        _umbracoDbContextFactory = umbracoDbContextFactory;
         UmbracoEFContext = umbracoEfContext;
     }
-
-    public Task<T> ExecuteWithContextAsync<T>(Func<UmbracoEFContext, Task<T>> method) => _umbracoDbContextFactory.ExecuteWithContextAsync<T>(method);
 
     public UmbracoEFContext UmbracoEFContext { get; }
     public string InstanceId => _instanceId ??= _instanceGuid.ToString("N").Substring(0, 8);
