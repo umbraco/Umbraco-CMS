1f9d591497e9770d07051910511fd5cf4ac7d078
Elitsa Marinovska <elm@umbraco.dk>
2023-02-08 08:57:18 +0100

    Use GetInstalledPackagesFromMigrationPlans to return all migration statuses

--- src/Umbraco.Cms.Api.Management/Mapping/Package/PackageViewModelMapDefinition.cs
+++ src/Umbraco.Cms.Api.Management/Mapping/Package/PackageViewModelMapDefinition.cs
@@ -16,7 +16,9 @@ public class PackageViewModelMapDefinition : IMapDefinition
                 PackagePath = string.Empty
             },
             Map);
+        mapper.Define<InstalledPackage, PackageMigrationStatusViewModel>((source, context) => new PackageMigrationStatusViewModel() { PackageName = string.Empty }, Map);
         mapper.Define<IEnumerable<PackageDefinition>, PagedViewModel<PackageDefinitionViewModel>>((source, context) => new PagedViewModel<PackageDefinitionViewModel>(), Map);
+        mapper.Define<IEnumerable<InstalledPackage>, PagedViewModel<PackageMigrationStatusViewModel>>((source, context) => new PagedViewModel<PackageMigrationStatusViewModel>(), Map);
     }
 
     // Umbraco.Code.MapAll
@@ -41,9 +43,27 @@ public class PackageViewModelMapDefinition : IMapDefinition
     }
 
     // Umbraco.Code.MapAll
+    private static void Map(InstalledPackage source, PackageMigrationStatusViewModel target, MapperContext context)
+    {
+        if (source.PackageName is not null)
+        {
+            target.PackageName = source.PackageName;
+        }
+
+        target.HasPendingMigrations = source.HasPendingMigrations;
+    }
+
+    // Umbraco.Code.MapAll
     private static void Map(IEnumerable<PackageDefinition> source, PagedViewModel<PackageDefinitionViewModel> target, MapperContext context)
     {
         target.Items = context.MapEnumerable<PackageDefinition, PackageDefinitionViewModel>(source);
+        target.Total = source.Count();
+    }
+
+    // Umbraco.Code.MapAll
+    private static void Map(IEnumerable<InstalledPackage> source, PagedViewModel<PackageMigrationStatusViewModel> target, MapperContext context)
+    {
+        target.Items = context.MapEnumerable<InstalledPackage, PackageMigrationStatusViewModel>(source);
         target.Total = source.Count();
     }
 }
