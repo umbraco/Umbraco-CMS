9a6e38458e3bdd2eeae3645c54a80c2dfe64ce27
Zeegaan <nge@umbraco.dk>
2023-02-09 15:06:35 +0100

    Implement SqlServerEFCoreDistributedLockingMechanism

---
+++ src/Umbraco.Cms.Persistence.EFCore/SqlServerEFCoreDistributedLockingMechanism.cs
@@ -0,0 +1,188 @@
+﻿using System.Data;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Umbraco.Cms.Core.Configuration.Models;
+using Umbraco.Cms.Core.DistributedLocking;
+using Umbraco.Cms.Core.DistributedLocking.Exceptions;
+using Umbraco.Cms.Core.Exceptions;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Infrastructure.Scoping;
+using Umbraco.Cms.Persistence.EFCore.Scoping;
+using Umbraco.Extensions;
+
+namespace Umbraco.Cms.Persistence.EFCore;
+
+public class SqlServerEFCoreDistributedLockingMechanism : IDistributedLockingMechanism
+{
+    private readonly IOptionsMonitor<ConnectionStrings> _connectionStrings;
+    private readonly IOptionsMonitor<GlobalSettings> _globalSettings;
+    private readonly ILogger<SqlServerEFCoreDistributedLockingMechanism> _logger;
+    private readonly Lazy<IEFCoreScopeAccessor> _scopeAccessor; // Hooray it's a circular dependency.
+
+    /// <summary>
+    ///     Initializes a new instance of the <see cref="SqlServerDistributedLockingMechanism" /> class.
+    /// </summary>
+    public SqlServerEFCoreDistributedLockingMechanism(
+        ILogger<SqlServerEFCoreDistributedLockingMechanism> logger,
+        Lazy<IEFCoreScopeAccessor> scopeAccessor,
+        IOptionsMonitor<GlobalSettings> globalSettings,
+        IOptionsMonitor<ConnectionStrings> connectionStrings)
+    {
+        _logger = logger;
+        _scopeAccessor = scopeAccessor;
+        _globalSettings = globalSettings;
+        _connectionStrings = connectionStrings;
+    }
+
+    /// <inheritdoc />
+    public bool Enabled => _connectionStrings.CurrentValue.IsConnectionStringConfigured() &&
+                           string.Equals(_connectionStrings.CurrentValue.ProviderName, "Microsoft.Data.SqlClient",
+                               StringComparison.InvariantCultureIgnoreCase);
+
+    /// <inheritdoc />
+    public IDistributedLock ReadLock(int lockId, TimeSpan? obtainLockTimeout = null)
+    {
+        obtainLockTimeout ??= _globalSettings.CurrentValue.DistributedLockingReadLockDefaultTimeout;
+        return new SqlServerDistributedLock(this, lockId, DistributedLockType.ReadLock, obtainLockTimeout.Value);
+    }
+
+    /// <inheritdoc />
+    public IDistributedLock WriteLock(int lockId, TimeSpan? obtainLockTimeout = null)
+    {
+        obtainLockTimeout ??= _globalSettings.CurrentValue.DistributedLockingWriteLockDefaultTimeout;
+        return new SqlServerDistributedLock(this, lockId, DistributedLockType.WriteLock, obtainLockTimeout.Value);
+    }
+
+    private class SqlServerDistributedLock : IDistributedLock
+    {
+        private readonly SqlServerEFCoreDistributedLockingMechanism _parent;
+        private readonly TimeSpan _timeout;
+
+        public SqlServerDistributedLock(
+            SqlServerEFCoreDistributedLockingMechanism parent,
+            int lockId,
+            DistributedLockType lockType,
+            TimeSpan timeout)
+        {
+            _parent = parent;
+            _timeout = timeout;
+            LockId = lockId;
+            LockType = lockType;
+
+            _parent._logger.LogDebug("Requesting {lockType} for id {id}", LockType, LockId);
+
+            try
+            {
+                switch (lockType)
+                {
+                    case DistributedLockType.ReadLock:
+                        ObtainReadLock();
+                        break;
+                    case DistributedLockType.WriteLock:
+                        ObtainWriteLock();
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException(nameof(lockType), lockType, @"Unsupported lockType");
+                }
+            }
+            catch (SqlException ex) when (ex.Number == 1222)
+            {
+                if (LockType == DistributedLockType.ReadLock)
+                {
+                    throw new DistributedReadLockTimeoutException(LockId);
+                }
+
+                throw new DistributedWriteLockTimeoutException(LockId);
+            }
+
+            _parent._logger.LogDebug("Acquired {lockType} for id {id}", LockType, LockId);
+        }
+
+        public int LockId { get; }
+
+        public DistributedLockType LockType { get; }
+
+        public void Dispose() =>
+            // Mostly no op, cleaned up by completing transaction in scope.
+            _parent._logger.LogDebug("Dropped {lockType} for id {id}", LockType, LockId);
+
+        public override string ToString()
+            => $"SqlServerDistributedLock({LockId}, {LockType}";
+
+        private void ObtainReadLock()
+        {
+            IEfCoreScope? scope = _parent._scopeAccessor.Value.AmbientScope;
+
+            if (scope is null)
+            {
+                throw new PanicException("No ambient scope");
+            }
+
+            scope.ExecuteWithContextAsync<Task>(async dbContext =>
+            {
+                if (dbContext.Database.CurrentTransaction is null)
+                {
+                    throw new InvalidOperationException(
+                        "SqlServerDistributedLockingMechanism requires a transaction to function.");
+                }
+
+                if (dbContext.Database.CurrentTransaction.GetDbTransaction().IsolationLevel <
+                    IsolationLevel.ReadCommitted)
+                {
+                    throw new InvalidOperationException(
+                        "A transaction with minimum ReadCommitted isolation level is required.");
+                }
+
+                // dbContext.Database.ExecuteSql($"SET LOCK_TIMEOUT {_timeout.TotalMilliseconds};");
+
+                var i = await dbContext.Database.ExecuteScalarAsync<int?>(
+                    $"SELECT value FROM umbracoLock WITH (REPEATABLEREAD)  WHERE id={LockId}");
+
+                if (i == null)
+                {
+                    // ensure we are actually locking!
+                    throw new ArgumentException(@$"LockObject with id={LockId} does not exist.", nameof(LockId));
+                }
+            });
+        }
+
+        private void ObtainWriteLock()
+        {
+            IEfCoreScope? scope = _parent._scopeAccessor.Value.AmbientScope;
+            if (scope is null)
+            {
+                throw new PanicException("No ambient scope");
+            }
+
+            scope.ExecuteWithContextAsync<Task>(async dbContext =>
+            {
+                if (dbContext.Database.CurrentTransaction is null)
+                {
+                    throw new InvalidOperationException(
+                        "SqlServerDistributedLockingMechanism requires a transaction to function.");
+                }
+
+                if (dbContext.Database.CurrentTransaction.GetDbTransaction().IsolationLevel < IsolationLevel.ReadCommitted)
+                {
+                    throw new InvalidOperationException(
+                        "A transaction with minimum ReadCommitted isolation level is required.");
+                }
+
+                string query = @$"UPDATE umbracoLock WITH (REPEATABLEREAD) SET value = (CASE WHEN (value=1) THEN -1 ELSE 1 END) WHERE id={LockId}";
+
+                // dbContext.Database.ExecuteSql($"SET LOCK_TIMEOUT {_timeout.TotalMilliseconds};");
+
+                var i = await dbContext.Database.ExecuteScalarAsync<int>(query);
+
+                if (i == 0)
+                {
+                    // ensure we are actually locking!
+                    throw new ArgumentException($"LockObject with id={LockId} does not exist.");
+                }
+            });
+        }
+    }
+}
--- tests/Umbraco.Tests.Integration/Umbraco.Persistence.EFCore/EfCoreLockTests.cs
+++ tests/Umbraco.Tests.Integration/Umbraco.Persistence.EFCore/EfCoreLockTests.cs
@@ -1,8 +1,10 @@
-﻿using System.Transactions;
+﻿using System.Text;
+using System.Transactions;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.DistributedLocking;
+using Umbraco.Cms.Core.DistributedLocking.Exceptions;
 using Umbraco.Cms.Infrastructure.Persistence;
 using Umbraco.Cms.Persistence.EFCore;
 using Umbraco.Cms.Persistence.EFCore.Entities;
@@ -18,16 +20,16 @@ namespace Umbraco.Cms.Tests.Integration.Umbraco.Persistence.EFCore;
 [UmbracoTest(Database = UmbracoTestOptions.Database.NewSchemaPerTest, Logger = UmbracoTestOptions.Logger.Console)]
 public class EfCoreLockTests : UmbracoIntegrationTest
 {
-    private IEfCoreScopeProvider EfCoreScopeProvider =>
+    private IEfCoreScopeProvider EFScopeProvider =>
         GetRequiredService<IEfCoreScopeProvider>();
 
-    private EFCoreScopeAccessor EfCoreScopeAccessor => (EFCoreScopeAccessor)GetRequiredService<IEFCoreScopeAccessor>();
+    private EFCoreScopeAccessor EFScopeAccessor => (EFCoreScopeAccessor)GetRequiredService<IEFCoreScopeAccessor>();
 
     [SetUp]
     protected void SetUp()
     {
         // create a few lock objects
-        using (var scope = EfCoreScopeProvider.CreateScope())
+        using (var scope = EFScopeProvider.CreateScope())
         {
             scope.ExecuteWithContextAsync<Task>(async database =>
             {
@@ -44,14 +46,14 @@ public class EfCoreLockTests : UmbracoIntegrationTest
     {
         // SQLite + retry policy makes tests fail, we retry before throwing distributed locking timeout.
         services.RemoveAll(x => x.ImplementationType == typeof(SqliteAddRetryPolicyInterceptor));
-        services.AddUnique<IDistributedLockingMechanism, SqliteEFCoreDistributedLockingMechanism>();
+        services.AddUnique<IDistributedLockingMechanism, SqlServerEFCoreDistributedLockingMechanism>();
     }
 
 
     [Test]
     public void SingleReadLockTest()
     {
-        using (var scope = EfCoreScopeProvider.CreateScope())
+        using (var scope = EFScopeProvider.CreateScope())
         {
             scope.EagerReadLock(Constants.Locks.Servers);
             scope.Complete();
@@ -81,7 +83,7 @@ public class EfCoreLockTests : UmbracoIntegrationTest
             var ic = i; // capture
             threads[i] = new Thread(() =>
             {
-                using (var scope = EfCoreScopeProvider.CreateScope())
+                using (var scope = EFScopeProvider.CreateScope())
                 {
                     try
                     {
@@ -144,7 +146,7 @@ public class EfCoreLockTests : UmbracoIntegrationTest
     {
         var sqlCount = 0;
 
-        using (var scope = EfCoreScopeProvider.CreateScope())
+        using (var scope = EFScopeProvider.CreateScope())
         {
             // Issue a lock request, but we are using non-eager
             // locks so this only queues the request.
@@ -159,5 +161,228 @@ public class EfCoreLockTests : UmbracoIntegrationTest
         }
 
         Assert.AreEqual(0, sqlCount);
+    }
+
+    [Test]
+    public void ConcurrentWritersTest()
+    {
+        if (BaseTestDatabase.DatabaseType.IsSqlite())
+        {
+            Assert.Ignore(
+                "This test doesn't work with Microsoft.Data.Sqlite in EFCore as we no longer use deferred transactions");
+            return;
+        }
+
+        const int threadCount = 8;
+        var threads = new Thread[threadCount];
+        var exceptions = new Exception[threadCount];
+        var locker = new object();
+        var acquired = 0;
+        var entered = 0;
+        var ms = new AutoResetEvent[threadCount];
+        for (var i = 0; i < threadCount; i++)
+        {
+            ms[i] = new AutoResetEvent(false);
+        }
+
+        var m1 = new ManualResetEventSlim(false);
+
+        for (var i = 0; i < threadCount; i++)
+        {
+            var ic = i; // capture
+            threads[i] = new Thread(() =>
+            {
+                using (var scope = EFScopeProvider.CreateScope())
+                {
+                    try
+                    {
+                        lock (locker)
+                        {
+                            entered++;
+                            if (entered == threadCount)
+                            {
+                                m1.Set();
+                            }
+                        }
+
+                        ms[ic].WaitOne();
+                        scope.EagerWriteLock(Constants.Locks.Servers);
+                        lock (locker)
+                        {
+                            acquired++;
+                        }
+
+                        ms[ic].WaitOne();
+                        lock (locker)
+                        {
+                            acquired--;
+                        }
+                    }
+                    catch (Exception e)
+                    {
+                        exceptions[ic] = e;
+                    }
+
+                    scope.Complete();
+                }
+            });
+        }
+
+        // ensure that current scope does not leak into starting threads
+        using (ExecutionContext.SuppressFlow())
+        {
+            foreach (var thread in threads)
+            {
+                thread.Start();
+            }
+        }
+
+        m1.Wait();
+        // all threads have entered
+        ms[0].Set(); // let 0 go
+        // TODO: This timing is flaky
+        Thread.Sleep(100);
+        for (var i = 1; i < threadCount; i++)
+        {
+            ms[i].Set(); // let others go
+        }
+
+        // TODO: This timing is flaky
+        Thread.Sleep(500);
+        // only 1 thread has locked
+        Assert.AreEqual(1, acquired);
+        for (var i = 0; i < threadCount; i++)
+        {
+            ms[i].Set(); // let all go
+        }
+
+        foreach (var thread in threads)
+        {
+            thread.Join();
+        }
+
+        Assert.AreEqual(0, acquired);
+
+        for (var i = 0; i < threadCount; i++)
+        {
+            Assert.IsNull(exceptions[i]);
+        }
+    }
+
+    [Retry(10)] // TODO make this test non-flaky.
+    [Test]
+    public void DeadLockTest()
+    {
+        if (BaseTestDatabase.DatabaseType.IsSqlite())
+        {
+            Assert.Ignore("This test doesn't work with Microsoft.Data.Sqlite - SELECT * FROM sys.dm_tran_locks;");
+            return;
+        }
+
+        Exception e1 = null, e2 = null;
+        AutoResetEvent ev1 = new(false), ev2 = new(false);
+
+        // testing:
+        // two threads will each obtain exclusive write locks over two
+        // identical lock objects deadlock each other
+
+        var thread1 = new Thread(() => DeadLockTestThread(1, 2, ev1, ev2, ref e1));
+        var thread2 = new Thread(() => DeadLockTestThread(2, 1, ev2, ev1, ref e2));
+
+        // ensure that current scope does not leak into starting threads
+        using (ExecutionContext.SuppressFlow())
+        {
+            thread1.Start();
+            thread2.Start();
+        }
+
+        ev2.Set();
+
+        thread1.Join();
+        thread2.Join();
+
+        //Assert.IsNotNull(e1);
+        if (e1 != null)
+        {
+            AssertIsDistributedLockingTimeoutException(e1);
+        }
+
+        // the assertion below depends on timing conditions - on a fast enough environment,
+        // thread1 dies (deadlock) and frees thread2, which succeeds - however on a slow
+        // environment (CI) both threads can end up dying due to deadlock - so, cannot test
+        // that e2 is null - but if it's not, can test that it's a timeout
+        //
+        //Assert.IsNull(e2);
+        if (e2 != null)
+        {
+            AssertIsDistributedLockingTimeoutException(e2);
+        }
+    }
+
+    private void AssertIsDistributedLockingTimeoutException(Exception e)
+    {
+        var sqlException = e as DistributedLockingTimeoutException;
+        Assert.IsNotNull(sqlException);
+    }
+
+    private void DeadLockTestThread(int id1, int id2, EventWaitHandle myEv, WaitHandle otherEv, ref Exception exception)
+    {
+        using (var scope = EFScopeProvider.CreateScope())
+        {
+            try
+            {
+                otherEv.WaitOne();
+                Console.WriteLine($"[{id1}] WAIT {id1}");
+                scope.EagerWriteLock(id1);
+                Console.WriteLine($"[{id1}] GRANT {id1}");
+                WriteLocks(scope);
+                myEv.Set();
+
+                if (id1 == 1)
+                {
+                    otherEv.WaitOne();
+                }
+                else
+                {
+                    Thread.Sleep(5200); // wait for deadlock...
+                }
+
+                Console.WriteLine($"[{id1}] WAIT {id2}");
+                scope.EagerWriteLock(id2);
+                Console.WriteLine($"[{id1}] GRANT {id2}");
+                WriteLocks(scope);
+            }
+            catch (Exception e)
+            {
+                exception = e;
+            }
+            finally
+            {
+                scope.Complete();
+            }
+        }
+    }
+
+    private void WriteLocks(IEfCoreScope scope)
+    {
+        Console.WriteLine("LOCKS:");
+        List<dynamic> info = new List<dynamic>();
+        scope.ExecuteWithContextAsync<Task>(async db =>
+        {
+            info = await db.Database.ExecuteScalarAsync<List<dynamic>>("SELECT * FROM sys.dm_tran_locks;");
+        });
+
+        var sb = new StringBuilder("> ");
+        foreach (var row in info)
+        {
+            if (row is IDictionary<string, object> values)
+            {
+                sb.AppendJoin(", ", values);
+            }
+
+            sb.AppendLine(string.Empty);
+        }
+
+        Console.WriteLine(sb.ToString());
     }
 }
