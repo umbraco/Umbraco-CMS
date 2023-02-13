967dc2b1f04e9bc10508c24440fb976646acfa15
Jesper Møller Jensen <26099018+JesmoDev@users.noreply.github.com>
2023-02-13 15:16:13 +1300

    add property creator

---
+++ src/Umbraco.Web.UI.Client/src/backoffice/shared/property-creator/property-creator.element.ts
@@ -0,0 +1,43 @@
+import { UmbLitElement } from '@umbraco-cms/element';
+import { UmbModalHandler, UmbModalService, UMB_MODAL_SERVICE_CONTEXT_TOKEN } from '@umbraco-cms/modal';
+import { UUITextStyles } from '@umbraco-ui/uui-css';
+import { css, html } from 'lit';
+import { customElement } from 'lit/decorators.js';
+
+@customElement('umb-property-creator')
+export class UmbPropertyCreatorElement extends UmbLitElement {
+	static styles = [UUITextStyles, css``];
+
+	#modalService?: UmbModalService;
+
+	constructor() {
+		super();
+
+		this.consumeContext(UMB_MODAL_SERVICE_CONTEXT_TOKEN, (instance) => (this.#modalService = instance));
+	}
+
+	#onAddProperty() {
+		const modalHandler = this.#modalService?.propertySettings();
+
+		if (modalHandler) {
+			modalHandler.onClose().then((result) => {
+				console.log('result', result);
+			});
+		}
+	}
+
+	render() {
+		return html`
+			<div>added properties goes here:</div>
+			<uui-button look="outline" @click=${this.#onAddProperty}> Add property </uui-button>
+		`;
+	}
+}
+
+export default UmbPropertyCreatorElement;
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'umb-property-creator': UmbPropertyCreatorElement;
+	}
+}
