22d38940167f31787412ee312a21deea17e6ee29
Nathan Woulfe <nathan@nathanw.com.au>
2023-02-15 19:20:36 +1000

    fix manifests (#522)

--- src/Umbraco.Web.UI.Client/src/backoffice/documents/document-types/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/documents/document-types/manifests.ts
@@ -1,5 +1,6 @@
 import { manifests as sidebarMenuItemManifests } from './sidebar-menu-item/manifests';
 import { manifests as treeManifests } from './tree/manifests';
 import { manifests as workspaceManifests } from './workspace/manifests';
+import { manifests as repositoryManifests } from './repository/manifests';
 
-export const manifests = [...sidebarMenuItemManifests, ...treeManifests, ...workspaceManifests];
+export const manifests = [...sidebarMenuItemManifests, ...treeManifests, ...repositoryManifests, ...workspaceManifests];
