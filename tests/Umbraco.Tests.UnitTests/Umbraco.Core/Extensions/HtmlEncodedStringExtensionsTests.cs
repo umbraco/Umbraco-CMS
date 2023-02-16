751c919c56dde1d40fb99ec910a7c09d855d5bfa
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2023-02-16 16:28:33 +0100

    Update unit test

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/Extensions/HtmlEncodedStringExtensionsTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Extensions/HtmlEncodedStringExtensionsTests.cs
@@ -8,16 +8,18 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Extensions;
 [TestFixture]
 public class HtmlEncodedStringExtensionsTests
 {
-    [TestCase(null, true)]
-    [TestCase("", true)]
-    [TestCase("   ", true)]
-    [TestCase("This is a non-empty string", false)]
-    [TestCase("<p>This is a non-empty string</p>", false)]
-    [TestCase("<p></p>", true)]
-    public void IsNullOrWhiteSpace(string? htmlString, bool expectedResult)
+    [TestCase(null, false, true)]
+    [TestCase("", false, true)]
+    [TestCase("   ", false, true)]
+    [TestCase("This is a non-empty string", false, false)]
+    [TestCase("<p>This is a non-empty string</p>", true,false)]
+    [TestCase("<p>This is a non-empty string</p>", false,false)]
+    [TestCase("<p></p>", true, true)]
+    [TestCase("<p></p>", false, false)]
+    public void IsNullOrWhiteSpace(string? htmlString, bool stripHtml, bool expectedResult)
     {
         var htmlEncodedString = htmlString == null ? null : Mock.Of<IHtmlEncodedString>(x => x.ToHtmlString() == htmlString);
-        var result = htmlEncodedString.IsNullOrWhiteSpace(stripHtml: true);
+        var result = htmlEncodedString.IsNullOrWhiteSpace(stripHtml: stripHtml);
 
         Assert.AreEqual(expectedResult, result);
     }
