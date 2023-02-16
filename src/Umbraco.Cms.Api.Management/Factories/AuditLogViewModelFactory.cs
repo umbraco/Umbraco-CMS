e865daf39c2ef096ee4d2047832352e51c5974fc
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2023-02-16 10:10:07 +0100

    Add CreateAuditLogWithUsernameViewModel method to factory

--- src/Umbraco.Cms.Api.Management/Factories/AuditLogViewModelFactory.cs
+++ src/Umbraco.Cms.Api.Management/Factories/AuditLogViewModelFactory.cs
@@ -1,4 +1,7 @@
 ﻿using Umbraco.Cms.Api.Management.ViewModels.AuditLogs;
+using Umbraco.Cms.Core.Cache;
+using Umbraco.Cms.Core.IO;
+using Umbraco.Cms.Core.Media;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Models.Membership;
 using Umbraco.Cms.Core.Services;
@@ -8,17 +11,46 @@ namespace Umbraco.Cms.Api.Management.Factories;
 public class AuditLogViewModelFactory : IAuditLogViewModelFactory
 {
     private readonly IUserService _userService;
+    private readonly AppCaches _appCaches;
+    private readonly MediaFileManager _mediaFileManager;
+    private readonly IImageUrlGenerator _imageUrlGenerator;
 
-    public AuditLogViewModelFactory(IUserService userService)
+    public AuditLogViewModelFactory(IUserService userService, AppCaches appCaches, MediaFileManager mediaFileManager, IImageUrlGenerator imageUrlGenerator)
     {
         _userService = userService;
+        _appCaches = appCaches;
+        _mediaFileManager = mediaFileManager;
+        _imageUrlGenerator = imageUrlGenerator;
     }
 
-    public IEnumerable<AuditLogByTypeViewModel> CreateAuditLogViewModel(IEnumerable<IAuditItem> auditItems) => auditItems.Select(CreateAuditLogViewModel);
+    public IEnumerable<AuditlogViewModel> CreateAuditLogViewModel(IEnumerable<IAuditItem> auditItems) => auditItems.Select(CreateAuditLogViewModel);
 
-    private AuditLogByTypeViewModel CreateAuditLogViewModel(IAuditItem auditItem)
+    public IEnumerable<AuditLogWithUsernameViewModel> createAuditLogWithUsernameViewModels(IEnumerable<IAuditItem> auditItems) => auditItems.Select(CreateAuditLogWithUsernameViewModel);
+
+    private AuditLogWithUsernameViewModel CreateAuditLogWithUsernameViewModel(IAuditItem auditItem)
     {
-        var target = new AuditLogByTypeViewModel
+        var target = new AuditLogWithUsernameViewModel
+        {
+            Comment = auditItem.Comment, EntityType = auditItem.EntityType, LogType = auditItem.AuditType,
+            Parameters = auditItem.Parameters,
+            Timestamp = auditItem.CreateDate,
+        };
+
+        IUser? user = _userService.GetUserById(auditItem.UserId);
+        if (user is null)
+        {
+            throw new ArgumentException($"Could not find user with id {auditItem.UserId}");
+        }
+
+        target.UserKey = user.Key;
+        target.UserAvatars = user.GetUserAvatarUrls(_appCaches.RuntimeCache, _mediaFileManager, _imageUrlGenerator);
+        target.UserName = user.Name;
+        return target;
+    }
+
+    private AuditlogViewModel CreateAuditLogViewModel(IAuditItem auditItem)
+    {
+        var target = new AuditlogViewModel
             {
                 Comment = auditItem.Comment, EntityType = auditItem.EntityType, LogType = auditItem.AuditType,
                 Parameters = auditItem.Parameters,
@@ -31,7 +63,7 @@ public class AuditLogViewModelFactory : IAuditLogViewModelFactory
             throw new ArgumentException($"Could not find user with id {auditItem.UserId}");
         }
 
-        target.UserKey = _userService.GetUserById(auditItem.UserId)!.Key;
+        target.UserKey = user.Key;
         return target;
     }
 }
--- src/Umbraco.Cms.Api.Management/Factories/IAuditLogViewModelFactory.cs
+++ src/Umbraco.Cms.Api.Management/Factories/IAuditLogViewModelFactory.cs
@@ -5,5 +5,7 @@ namespace Umbraco.Cms.Api.Management.Factories;
 
 public interface IAuditLogViewModelFactory
 {
-    IEnumerable<AuditLogByTypeViewModel> CreateAuditLogViewModel(IEnumerable<IAuditItem> auditItems);
+    IEnumerable<AuditlogViewModel> CreateAuditLogViewModel(IEnumerable<IAuditItem> auditItems);
+
+    IEnumerable<AuditLogWithUsernameViewModel> createAuditLogWithUsernameViewModels(IEnumerable<IAuditItem> auditItems);
 }
