57b7e907777a5a7ac361cf1e5e5437a8eb0770a4
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2023-02-16 13:22:33 +0100

    Create RelationTypeControllerBase.cs

---
+++ src/Umbraco.Cms.Api.Management/Controllers/RelationTypeController/RelationTypeControllerBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Management.Routing;
+
+namespace Umbraco.Cms.Api.Management.Controllers.RelationTypeController;
+
+[ApiController]
+[VersionedApiBackOfficeRoute("relationType")]
+[ApiExplorerSettings(GroupName = "RelationType")]
+[ApiVersion("1.0")]
+public class RelationTypeControllerBase : ManagementApiControllerBase
+{
+}
