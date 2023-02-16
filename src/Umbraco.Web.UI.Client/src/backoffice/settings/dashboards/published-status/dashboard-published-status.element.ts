a42cc1bdb256ea579781ea60a2dde511490bd696
Warren Buckley <warren@umbraco.com>
2023-02-16 20:13:52 +0000

    Update src/backoffice/settings/dashboards/published-status/dashboard-published-status.element.ts
    Co-authored-by: Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>

--- src/Umbraco.Web.UI.Client/src/backoffice/settings/dashboards/published-status/dashboard-published-status.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/settings/dashboards/published-status/dashboard-published-status.element.ts
@@ -133,7 +133,7 @@ export class UmbDashboardPublishedStatusElement extends UmbLitElement {
 
 	render() {
 		return html`
-			<umb-debug enabled useDialog></umb-debug>
+			<umb-debug enabled dialog></umb-debug>
 			<uui-box headline="Published Cache Status">
 				<p>${this._publishedStatusText}</p>
 				<uui-button
