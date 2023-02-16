bb747709be4f2a71cff5aa2c6dc622d29e6e0f5d
Mads Rasmussen <madsr@hey.com>
2023-02-16 14:13:52 +0100

    add isNew to workspace interface

--- src/Umbraco.Web.UI.Client/src/backoffice/shared/components/workspace/workspace-context/workspace-context.interface.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/shared/components/workspace/workspace-context/workspace-context.interface.ts
@@ -1,10 +1,8 @@
 export interface UmbWorkspaceContextInterface<T = unknown> {
 	//readonly data: Observable<T>;
 	//getUnique(): string | undefined;
-
+	isNew: boolean;
 	getEntityType(): string;
-
 	getData(): T;
-
 	destroy(): void;
 }
