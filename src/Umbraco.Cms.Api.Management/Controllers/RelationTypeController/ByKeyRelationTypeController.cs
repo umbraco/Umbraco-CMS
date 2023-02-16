325fee9bbf607c69d825704ef5d1fa6f8455aee0
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2023-02-16 13:38:03 +0100

    Implement ByKey endpoint

---
+++ src/Umbraco.Cms.Api.Management/Controllers/RelationTypeController/ByKeyRelationTypeController.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Management.ViewModels.RelationType;
+using Umbraco.Cms.Api.Management.ViewModels.Telemetry;
+using Umbraco.Cms.Core.Mapping;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Services;
+
+namespace Umbraco.Cms.Api.Management.Controllers.RelationTypeController;
+
+public class ByKeyRelationTypeController : RelationTypeControllerBase
+{
+    private readonly IRelationService _relationService;
+    private readonly IUmbracoMapper _mapper;
+
+    public ByKeyRelationTypeController(IRelationService relationService, IUmbracoMapper mapper)
+    {
+        _relationService = relationService;
+        _mapper = mapper;
+    }
+
+    [HttpGet("ByKey")]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(typeof(TelemetryViewModel), StatusCodes.Status200OK)]
+    public async Task<RelationTypeViewModel> ByKey(Guid key)
+    {
+        IRelationType? relationType = _relationService.GetRelationTypeById(key);
+        RelationTypeViewModel mappedRelationType = _mapper.Map<RelationTypeViewModel>(relationType)!;
+
+        return await Task.FromResult(mappedRelationType);
+    }
+}
---
+++ src/Umbraco.Cms.Api.Management/ViewModels/RelationType/RelationTypeViewModel.cs
@@ -0,0 +1,46 @@
+﻿namespace Umbraco.Cms.Api.Management.ViewModels.RelationType;
+
+public class RelationTypeViewModel
+{
+    public Guid Key { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Alias { get; set; }
+
+    public string Path { get; set; } = string.Empty;
+
+    public bool IsSystemRelationType { get; set; }
+
+    /// <summary>
+    ///     Gets or sets a boolean indicating whether the RelationType is Bidirectional (true) or Parent to Child (false)
+    /// </summary>
+    public bool IsBidirectional { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the Parents object type id
+    /// </summary>
+    /// <remarks>Corresponds to the NodeObjectType in the umbracoNode table</remarks>
+    public Guid? ParentObjectType { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the Parent's object type name.
+    /// </summary>
+    public string? ParentObjectTypeName { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the Child's object type id
+    /// </summary>
+    /// <remarks>Corresponds to the NodeObjectType in the umbracoNode table</remarks>
+    public Guid? ChildObjectType { get; set; }
+
+    /// <summary>
+    ///     Gets or sets the Child's object type name.
+    /// </summary>
+    public string? ChildObjectTypeName { get; set; }
+
+    /// <summary>
+    ///     Gets or sets a boolean indicating whether the RelationType should be returned in "Used by"-queries.
+    /// </summary>
+    public bool IsDependency { get; set; }
+}
