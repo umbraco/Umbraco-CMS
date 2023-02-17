50fef879e62714aeea41d7532bc4ae9bf8585950
Zeegaan <nge@umbraco.dk>
2023-02-17 14:14:01 +0100

    Implement UpdateRelationTypeController

---
+++ src/Umbraco.Cms.Api.Management/Controllers/RelationType/Query/UpdateRelationTypeController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Common.Builders;
+using Umbraco.Cms.Api.Management.ViewModels.RelationType;
+using Umbraco.Cms.Core.Mapping;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Services;
+
+namespace Umbraco.Cms.Api.Management.Controllers.RelationType.Query;
+
+public class UpdateRelationTypeController : RelationTypeControllerBase
+{
+    private readonly IRelationService _relationService;
+    private readonly UmbracoMapper _umbracoMapper;
+
+    public UpdateRelationTypeController(IRelationService relationService, UmbracoMapper umbracoMapper)
+    {
+        _relationService = relationService;
+        _umbracoMapper = umbracoMapper;
+    }
+
+    [HttpPut("update")]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(typeof(RelationTypeViewModel), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Update(RelationTypeUpdatingViewModel relationTypeSavingViewModel)
+    {
+        IRelationType? persistedRelationType = _relationService.GetRelationTypeById(relationTypeSavingViewModel.Key);
+
+        if (persistedRelationType is null)
+        {
+            ProblemDetails problemDetails = new ProblemDetailsBuilder()
+                .WithTitle("Could not find relation type")
+                .WithDetail($"Relation type with key {relationTypeSavingViewModel.Key} could not be found")
+                .Build();
+            return NotFound(problemDetails);
+        }
+
+        _relationService.Save(persistedRelationType);
+
+        RelationTypeViewModel relationTypeViewModel = _umbracoMapper.Map<RelationTypeViewModel>(persistedRelationType)!;
+
+        return await Task.FromResult(Ok(relationTypeViewModel));
+    }
+}
