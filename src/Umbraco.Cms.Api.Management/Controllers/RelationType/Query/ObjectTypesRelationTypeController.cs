2430498ebb4fca30e07494c681f0599367dc3aee
Zeegaan <nge@umbraco.dk>
2023-02-20 09:43:07 +0100

    Fix paging for object types

--- src/Umbraco.Cms.Api.Management/Controllers/RelationType/Query/ObjectTypesRelationTypeController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/RelationType/Query/ObjectTypesRelationTypeController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Common.ViewModels.Pagination;
 using Umbraco.Cms.Api.Management.Factories;
 using Umbraco.Cms.Api.Management.ViewModels.RelationType;
 using Umbraco.Cms.Api.Management.ViewModels.Telemetry;
@@ -14,11 +15,15 @@ public class ObjectTypesRelationTypeController : RelationTypeControllerBase
 
     [HttpGet("object-types")]
     [MapToApiVersion("1.0")]
-    [ProducesResponseType(typeof(TelemetryViewModel), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(PagedViewModel<ObjectTypeViewModel>), StatusCodes.Status200OK)]
     public async Task<IActionResult> ObjectTypes(int skip = 0, int take = 100)
     {
-        IEnumerable<ObjectTypeViewModel> objectTypes = _objectTypeViewModelFactory.Create();
+        IEnumerable<ObjectTypeViewModel> objectTypes = _objectTypeViewModelFactory.Create().ToArray();
 
-        return await Task.FromResult(Ok(objectTypes.Skip(skip).Take(take)));
+        return await Task.FromResult(Ok(new PagedViewModel<ObjectTypeViewModel>
+        {
+            Total = objectTypes.Count(),
+            Items = objectTypes.Skip(skip).Take(take),
+        }));
     }
 }
