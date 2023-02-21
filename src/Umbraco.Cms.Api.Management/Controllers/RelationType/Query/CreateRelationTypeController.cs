9461ff9562e33cd379a3ebaa6e4f5142436f302f
Zeegaan <nge@umbraco.dk>
2023-02-21 12:31:23 +0100

    Update create action to use CreateAsync

--- src/Umbraco.Cms.Api.Management/Controllers/RelationType/Query/CreateRelationTypeController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/RelationType/Query/CreateRelationTypeController.cs
@@ -2,8 +2,11 @@
 using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Api.Management.Factories;
 using Umbraco.Cms.Api.Management.ViewModels.RelationType;
+using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Security;
 using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Services.OperationStatus;
 
 
 namespace Umbraco.Cms.Api.Management.Controllers.RelationType.Query;
@@ -12,11 +15,15 @@ public class CreateRelationTypeController : RelationTypeControllerBase
 {
     private readonly IRelationService _relationService;
     private readonly IRelationTypeViewModelFactory _relationTypeViewModelFactory;
+    private readonly IBackOfficeSecurityAccessor _backOfficeSecurityAccessor;
 
-    public CreateRelationTypeController(IRelationService relationService, IRelationTypeViewModelFactory relationTypeViewModelFactory)
+    public CreateRelationTypeController(IRelationService relationService,
+        IRelationTypeViewModelFactory relationTypeViewModelFactory,
+        IBackOfficeSecurityAccessor backOfficeSecurityAccessor)
     {
         _relationService = relationService;
         _relationTypeViewModelFactory = relationTypeViewModelFactory;
+        _backOfficeSecurityAccessor = backOfficeSecurityAccessor;
     }
 
     [HttpPost]
@@ -26,8 +33,10 @@ public class CreateRelationTypeController : RelationTypeControllerBase
     {
         IRelationType relationTypePersisted = _relationTypeViewModelFactory.CreateRelationType(relationTypeSavingViewModel);
 
-        _relationService.Save(relationTypePersisted);
+        Attempt<IRelationType, RelationTypeOperationStatus> result = await _relationService.CreateAsync(relationTypePersisted, CurrentUserId(_backOfficeSecurityAccessor));
 
-        return await Task.FromResult(CreatedAtAction<ByKeyRelationTypeController>(controller => nameof(controller.ByKey), relationTypePersisted.Key));
+        return result.Success
+            ? await Task.FromResult(CreatedAtAction<ByKeyRelationTypeController>(controller => nameof(controller.ByKey), relationTypePersisted.Key))
+            : RelationTypeOperationStatusResult(result.Status);
     }
 }
--- src/Umbraco.Cms.Api.Management/Controllers/RelationType/Query/RelationTypeControllerBase.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/RelationType/Query/RelationTypeControllerBase.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Common.Builders;
 using Umbraco.Cms.Api.Management.Routing;
+using Umbraco.Cms.Core.Services.OperationStatus;
 
 namespace Umbraco.Cms.Api.Management.Controllers.RelationType.Query;
 
@@ -9,4 +11,20 @@ namespace Umbraco.Cms.Api.Management.Controllers.RelationType.Query;
 [ApiVersion("1.0")]
 public class RelationTypeControllerBase : ManagementApiControllerBase
 {
+        protected IActionResult RelationTypeOperationStatusResult(RelationTypeOperationStatus status) =>
+        status switch
+        {
+            RelationTypeOperationStatus.InvalidId => BadRequest(new ProblemDetailsBuilder()
+                .WithTitle("Invalid id")
+                .WithDetail("Can not assign an Id when creating a relation type")
+                .Build()),
+            RelationTypeOperationStatus.CancelledByNotification => BadRequest(new ProblemDetailsBuilder()
+                .WithTitle("Cancelled by notification")
+                .WithDetail("A notification handler prevented the relation type operation.")
+                .Build()),
+            RelationTypeOperationStatus.KeyAlreadyExists => BadRequest(new ProblemDetailsBuilder()
+                .WithTitle("Key already exists")
+                .WithDetail("An entity with the given key already exists")
+                .Build()),
+        };
 }
