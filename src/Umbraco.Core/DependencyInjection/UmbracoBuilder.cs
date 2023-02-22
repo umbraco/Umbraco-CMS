ccf06018633af1aded70798c0ed1c7fe3fc61b29
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2023-02-22 11:48:30 +0100

    Implement AllowedRelationObjectTypesService.cs

--- src/Umbraco.Core/DependencyInjection/UmbracoBuilder.cs
+++ src/Umbraco.Core/DependencyInjection/UmbracoBuilder.cs
@@ -333,6 +333,7 @@ namespace Umbraco.Cms.Core.DependencyInjection
             Services.AddUnique<ICultureImpactFactory>(provider => new CultureImpactFactory(provider.GetRequiredService<IOptionsMonitor<ContentSettings>>()));
             Services.AddUnique<IDictionaryService, DictionaryService>();
             Services.AddUnique<ITemporaryMediaService, TemporaryMediaService>();
+            Services.AddUnique<IAllowedRelationObjectTypesService, AllowedRelationObjectTypesService>();
         }
     }
 }
---
+++ src/Umbraco.Core/Services/IAllowedRelationObjectTypesService.cs
@@ -0,0 +1,8 @@
+﻿using Umbraco.Cms.Core.Models;
+
+namespace Umbraco.Cms.Core.Services;
+
+public interface IAllowedRelationObjectTypesService
+{
+    IEnumerable<UmbracoObjectTypes> Get();
+}
