60c42a60e01f1ed73e99754b2063292ac8ee4d56
Elitsa Marinovska <elm@umbraco.dk>
2023-02-22 14:25:04 +0100

    Fix Mapper - multiple enumeration and cleanup

--- src/Umbraco.Cms.Api.Management/Mapping/Package/PackageViewModelMapDefinition.cs
+++ src/Umbraco.Cms.Api.Management/Mapping/Package/PackageViewModelMapDefinition.cs
@@ -1,5 +1,6 @@
 using Umbraco.Cms.Api.Common.ViewModels.Pagination;
 using Umbraco.Cms.Api.Management.ViewModels.Package;
+using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Mapping;
 using Umbraco.Cms.Core.Packaging;
 
@@ -9,16 +10,36 @@ public class PackageViewModelMapDefinition : IMapDefinition
 {
     public void DefineMaps(IUmbracoMapper mapper)
     {
+        mapper.Define<PackageModelBase, PackageDefinition>((_, _) => new PackageDefinition(), Map);
         mapper.Define<PackageDefinition, PackageDefinitionViewModel>(
-            (source, context) => new PackageDefinitionViewModel()
+            (_, _) => new PackageDefinitionViewModel
             {
                 Name = string.Empty,
                 PackagePath = string.Empty
             },
             Map);
-        mapper.Define<InstalledPackage, PackageMigrationStatusViewModel>((source, context) => new PackageMigrationStatusViewModel() { PackageName = string.Empty }, Map);
-        mapper.Define<IEnumerable<PackageDefinition>, PagedViewModel<PackageDefinitionViewModel>>((source, context) => new PagedViewModel<PackageDefinitionViewModel>(), Map);
-        mapper.Define<IEnumerable<InstalledPackage>, PagedViewModel<PackageMigrationStatusViewModel>>((source, context) => new PagedViewModel<PackageMigrationStatusViewModel>(), Map);
+        mapper.Define<InstalledPackage, PackageMigrationStatusViewModel>((_, _) => new PackageMigrationStatusViewModel { PackageName = string.Empty }, Map);
+        mapper.Define<IEnumerable<PackageDefinition>, PagedViewModel<PackageDefinitionViewModel>>((_, _) => new PagedViewModel<PackageDefinitionViewModel>(), Map);
+        mapper.Define<IEnumerable<InstalledPackage>, PagedViewModel<PackageMigrationStatusViewModel>>((_, _) => new PagedViewModel<PackageMigrationStatusViewModel>(), Map);
+    }
+
+    // Umbraco.Code.MapAll -Id -PackageId -PackagePath -Macros
+    private static void Map(PackageModelBase source, PackageDefinition target, MapperContext context)
+    {
+        target.Name = source.Name;
+        target.ContentLoadChildNodes = source.ContentLoadChildNodes;
+        target.ContentNodeId = source.ContentNodeId;
+        target.Languages = source.Languages;
+        target.DictionaryItems = source.DictionaryItems;
+        target.Templates = source.Templates;
+        target.PartialViews = source.PartialViews;
+        target.DocumentTypes = source.DocumentTypes;
+        target.MediaTypes = source.MediaTypes;
+        target.Stylesheets = source.Stylesheets;
+        target.Scripts = source.Scripts;
+        target.DataTypes = source.DataTypes;
+        target.MediaUdis = source.MediaKeys.Select(x => new GuidUdi(Constants.UdiEntityType.Media, x)).ToList();
+        target.MediaLoadChildNodes = source.MediaLoadChildNodes;
     }
 
     // Umbraco.Code.MapAll
@@ -56,14 +77,16 @@ public class PackageViewModelMapDefinition : IMapDefinition
     // Umbraco.Code.MapAll
     private static void Map(IEnumerable<PackageDefinition> source, PagedViewModel<PackageDefinitionViewModel> target, MapperContext context)
     {
-        target.Items = context.MapEnumerable<PackageDefinition, PackageDefinitionViewModel>(source);
-        target.Total = source.Count();
+        PackageDefinition[] definitions = source.ToArray();
+        target.Items = context.MapEnumerable<PackageDefinition, PackageDefinitionViewModel>(definitions);
+        target.Total = definitions.Length;
     }
 
     // Umbraco.Code.MapAll
     private static void Map(IEnumerable<InstalledPackage> source, PagedViewModel<PackageMigrationStatusViewModel> target, MapperContext context)
     {
-        target.Items = context.MapEnumerable<InstalledPackage, PackageMigrationStatusViewModel>(source);
-        target.Total = source.Count();
+        InstalledPackage[] installedPackages = source.ToArray();
+        target.Items = context.MapEnumerable<InstalledPackage, PackageMigrationStatusViewModel>(installedPackages);
+        target.Total = installedPackages.Length;
     }
 }
