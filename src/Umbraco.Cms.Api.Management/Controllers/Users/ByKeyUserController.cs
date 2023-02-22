b822bab85eb4eb79cf127321725fc3a43aee2ad8
nikolajlauridsen <nikolajlauridsen@protonmail.ch>
2023-02-22 15:42:58 +0100

    Add GetByKey controller

---
+++ src/Umbraco.Cms.Api.Management/Controllers/Users/ByKeyUserController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Management.Factories;
+using Umbraco.Cms.Api.Management.ViewModels.Users;
+using Umbraco.Cms.Core.Models.Membership;
+using Umbraco.Cms.Core.Services;
+
+namespace Umbraco.Cms.Api.Management.Controllers.Users;
+
+public class ByKeyUserController : UsersControllerBase
+{
+    private readonly IUserService _userService;
+    private readonly IUserPresentationFactory _userPresentationFactory;
+
+    public ByKeyUserController(
+        IUserService userService,
+        IUserPresentationFactory userPresentationFactory)
+    {
+        _userService = userService;
+        _userPresentationFactory = userPresentationFactory;
+    }
+
+    [HttpGet("{key:guid}")]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(typeof(UserResponseModel), StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> ByKey(Guid key)
+    {
+        IUser? user = await _userService.GetAsync(key);
+
+        if (user is null)
+        {
+            return NotFound();
+        }
+
+        UserResponseModel responseModel = _userPresentationFactory.CreateResponseModel(user);
+        return Ok(responseModel);
+    }
+}
---
+++ src/Umbraco.Cms.Api.Management/Controllers/Users/UsersControllerBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Management.Routing;
+
+namespace Umbraco.Cms.Api.Management.Controllers.Users;
+
+[ApiController]
+[VersionedApiBackOfficeRoute("users")]
+[ApiExplorerSettings(GroupName = "Users")]
+[ApiVersion("1.0")]
+public class UsersControllerBase : ManagementApiControllerBase
+{
+
+}
