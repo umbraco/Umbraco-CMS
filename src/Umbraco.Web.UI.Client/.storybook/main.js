036e296ae69e9099307045ce2a228ac7904b107d
Warren Buckley <warren@umbraco.com>
2023-02-22 17:35:18 +0000

    sb@next upgrade --prerelease
    Run the storybook upgrade command to get 7.0.0 beta 53 - From the community Discord to try & get MDX code syntax working

--- src/Umbraco.Web.UI.Client/.storybook/main.js
+++ src/Umbraco.Web.UI.Client/.storybook/main.js
@@ -1,22 +1,28 @@
 const tsconfigPaths = require('vite-tsconfig-paths').default;
-const { mergeConfig } = require('vite');
-
+const {
+  mergeConfig
+} = require('vite');
 module.exports = {
-	stories: ['../@(src|libs|apps)/**/*.stories.@(js|jsx|ts|tsx|mdx)'],
-	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
-	framework: '@storybook/web-components',
-	features: {
-		previewMdx2: true,
-		storyStoreV7: true,
-	},
-	core: {
-		builder: '@storybook/builder-vite',
-	},
-	staticDirs: ['../public-assets'],
-	async viteFinal(config, { configType }) {
-		return mergeConfig(config, {
-			// customize the Vite config here
-			plugins: [tsconfigPaths()],
-		});
-	},
+  stories: ['../@(src|libs|apps)/**/*.@(mdx|stories.@(js|jsx|ts|tsx))'],
+  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
+  framework: {
+    name: '@storybook/web-components-vite',
+    options: {}
+  },
+  features: {
+    previewMdx2: true,
+    storyStoreV7: true
+  },
+  staticDirs: ['../public-assets'],
+  async viteFinal(config, {
+    configType
+  }) {
+    return mergeConfig(config, {
+      // customize the Vite config here
+      plugins: [tsconfigPaths()]
+    });
+  },
+  docs: {
+    autodocs: true
+  }
 };
