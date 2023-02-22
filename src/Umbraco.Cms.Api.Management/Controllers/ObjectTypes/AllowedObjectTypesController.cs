ce87c9bbbef72bacdc28ad65ec0fc0fc9179d634
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2023-02-22 12:38:07 +0100

    move object types endpoint to own silo

--- src/Umbraco.Cms.Api.Management/Controllers/ObjectTypes/AllowedObjectTypesController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/ObjectTypes/AllowedObjectTypesController.cs
@@ -3,20 +3,19 @@ using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Api.Common.ViewModels.Pagination;
 using Umbraco.Cms.Api.Management.Factories;
 using Umbraco.Cms.Api.Management.ViewModels.RelationType;
-using Umbraco.Cms.Api.Management.ViewModels.Telemetry;
 
-namespace Umbraco.Cms.Api.Management.Controllers.RelationType.Query;
+namespace Umbraco.Cms.Api.Management.Controllers.ObjectTypes;
 
-public class ObjectTypesRelationTypeController : RelationTypeControllerBase
+public class AllowedObjectTypesController : ObjectTypesControllerBase
 {
     private readonly IObjectTypeViewModelFactory _objectTypeViewModelFactory;
 
-    public ObjectTypesRelationTypeController(IObjectTypeViewModelFactory objectTypeViewModelFactory) => _objectTypeViewModelFactory = objectTypeViewModelFactory;
+    public AllowedObjectTypesController(IObjectTypeViewModelFactory objectTypeViewModelFactory) => _objectTypeViewModelFactory = objectTypeViewModelFactory;
 
-    [HttpGet("object-types")]
+    [HttpGet]
     [MapToApiVersion("1.0")]
     [ProducesResponseType(typeof(PagedViewModel<ObjectTypeResponseModel>), StatusCodes.Status200OK)]
-    public async Task<IActionResult> ObjectTypes(int skip = 0, int take = 100)
+    public async Task<IActionResult> Allowed(int skip = 0, int take = 100)
     {
         IEnumerable<ObjectTypeResponseModel> objectTypes = _objectTypeViewModelFactory.Create().ToArray();
 
