93149160a79758f36650024b813213830d44493e
kjac <kja@umbraco.dk>
2023-02-23 09:12:10 +0100

    Make various value editors handle their respective data as concrete types rather than strings or otherwise strange legacy data formats

--- src/Umbraco.Infrastructure/PropertyEditors/TrueFalsePropertyEditor.cs
+++ src/Umbraco.Infrastructure/PropertyEditors/TrueFalsePropertyEditor.cs
@@ -4,7 +4,11 @@
 using Microsoft.Extensions.DependencyInjection;
 using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Cms.Core.IO;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Models.Editors;
+using Umbraco.Cms.Core.Serialization;
 using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Strings;
 
 namespace Umbraco.Cms.Core.PropertyEditors;
 
@@ -49,6 +53,39 @@ public class TrueFalsePropertyEditor : DataEditor
     }
 
     /// <inheritdoc />
+    protected override IDataValueEditor CreateValueEditor()
+        => DataValueEditorFactory.Create<TrueFalsePropertyValueEditor>(Attribute!);
+
+    /// <inheritdoc />
     protected override IConfigurationEditor CreateConfigurationEditor() =>
         new TrueFalseConfigurationEditor(_ioHelper, _editorConfigurationParser);
+
+    internal class TrueFalsePropertyValueEditor : DataValueEditor
+    {
+        public TrueFalsePropertyValueEditor(
+            ILocalizedTextService localizedTextService,
+            IShortStringHelper shortStringHelper,
+            IJsonSerializer jsonSerializer,
+            IIOHelper ioHelper,
+            DataEditorAttribute attribute)
+            : base(localizedTextService, shortStringHelper, jsonSerializer, ioHelper, attribute)
+        {
+        }
+
+        public override object? ToEditor(IProperty property, string? culture = null, string? segment = null)
+            => ParsePropertyValue(property.GetValue(culture, segment));
+
+        // NOTE: property editor value type is Integer, which means we need to store the boolean representation as 0 or 1
+        public override object? FromEditor(ContentPropertyData editorValue, object? currentValue)
+            => ParsePropertyValue(editorValue.Value) ? 1 : 0;
+
+        private bool ParsePropertyValue(object? value)
+            => value switch
+            {
+                bool booleanValue => booleanValue,
+                int integerValue => integerValue == 1,
+                string stringValue => stringValue == "1" || stringValue.Equals("true", StringComparison.OrdinalIgnoreCase),
+                _ => false
+            };
+    }
 }
