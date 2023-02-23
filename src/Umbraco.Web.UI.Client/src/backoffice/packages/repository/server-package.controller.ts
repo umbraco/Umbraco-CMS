a30c0249ba0a6fd3b77641a9c624cf9cecab8bbf
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-02-23 17:13:08 +0100

    load extensions rather than packages in server controller

--- src/Umbraco.Web.UI.Client/src/backoffice/packages/repository/server-package.controller.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/packages/repository/server-package.controller.ts
@@ -1,4 +1,4 @@
-import { first, Subject, takeUntil } from 'rxjs';
+import { Subject, takeUntil } from 'rxjs';
 import { UmbPackageRepository } from './package.repository';
 import { UmbController, UmbControllerHostInterface } from '@umbraco-cms/controller';
 import { isManifestJSType, UmbExtensionRegistry } from '@umbraco-cms/extensions-api';
@@ -23,29 +23,21 @@ export class UmbServerPackageController extends UmbController {
 	}
 
 	async #loadPackages() {
-		const package$ = await this.#repository.rootItems();
+		const extensions$ = await this.#repository.extensions();
 
-		package$
+		extensions$
 			.pipe(
 				// If the app breaks then stop the request
-				takeUntil(this.#unobserve),
-				// Stop after the first time, we only need to load the packages once
-				first()
+				takeUntil(this.#unobserve)
 			)
-			.subscribe((packages) => {
-				// Go through packages and register their extensions
-				packages.forEach((p) => {
-					const { extensions } = p;
-					if (extensions?.length) {
-						extensions.forEach((extension) => {
-							/**
-							 * Crude check to see if extension is of type "js" since it is safe to assume we do not
-							 * need to load any other types of extensions in the backoffice (we need a js file to load)
-							 */
-							if (isManifestJSType(extension)) {
-								this.extensionRegistry.register(extension);
-							}
-						});
+			.subscribe((extensions) => {
+				extensions.forEach((extension) => {
+					/**
+					 * Crude check to see if extension is of type "js" since it is safe to assume we do not
+					 * need to load any other types of extensions in the backoffice (we need a js file to load)
+					 */
+					if (isManifestJSType(extension)) {
+						this.extensionRegistry.register(extension);
 					}
 				});
 			});
