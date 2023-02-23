feb2e06abc525123fee840fda53754dcfff869df
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-02-23 16:07:40 +0100

    add package controller to load server packages and register their extensions

---
+++ src/Umbraco.Web.UI.Client/src/backoffice/packages/repository/server-package.controller.ts
@@ -0,0 +1,40 @@
+import { Subject, takeUntil } from 'rxjs';
+import { UmbPackageRepository } from './package.repository';
+import { UmbController, UmbControllerHostInterface } from '@umbraco-cms/controller';
+import type { UmbExtensionRegistry } from '@umbraco-cms/extensions-api';
+
+export class UmbServerPackageController extends UmbController {
+	#unobserve = new Subject<void>();
+	#repository: UmbPackageRepository;
+
+	constructor(host: UmbControllerHostInterface, private readonly extensionRegistry: UmbExtensionRegistry) {
+		super(host, UmbServerPackageController.name);
+
+		this.#repository = new UmbPackageRepository(host);
+	}
+
+	hostConnected(): void {
+		this.#loadPackages();
+	}
+
+	hostDisconnected(): void {
+		this.#unobserve.next();
+		this.#unobserve.complete();
+	}
+
+	async #loadPackages() {
+		const package$ = await this.#repository.rootItems();
+
+		package$.pipe(takeUntil(this.#unobserve)).subscribe((packages) => {
+			// Go through packages and register their extensions
+			packages.forEach((p: any) => {
+				const { extensions } = p;
+				if (extensions?.length) {
+					extensions.forEach((extension: any) => {
+						this.extensionRegistry.register(extension);
+					});
+				}
+			});
+		});
+	}
+}
