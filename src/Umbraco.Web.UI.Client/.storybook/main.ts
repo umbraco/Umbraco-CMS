710f1be63b536aacb9435e8ab751b6660f2ec02e
Mads Rasmussen <madsr@hey.com>
2023-02-23 21:28:34 +0100

    remove .stories from mdx files. It can cause problems

--- src/Umbraco.Web.UI.Client/.storybook/main.ts
+++ src/Umbraco.Web.UI.Client/.storybook/main.ts
@@ -1,21 +1,21 @@
 import { StorybookConfig } from '@storybook/web-components-vite';
 
 const config: StorybookConfig = {
-  stories: ['../@(src|libs|apps)/**/*.stories.mdx', '../@(src|libs|apps)/**/*.stories.@(js|jsx|ts|tsx)'],
-  addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
-  framework: { 
-    name: '@storybook/web-components-vite',
-    options: {}
-   },
-   features: {
-    storyStoreV7: true
-  },
-  staticDirs: ['../public-assets'],
-  typescript: {
-    check: true,
-  },
-  docs: {
-    autodocs: true
-  }
+	stories: ['../@(src|libs|apps)/**/*.mdx', '../@(src|libs|apps)/**/*.stories.@(js|jsx|ts|tsx)'],
+	addons: ['@storybook/addon-links', '@storybook/addon-essentials', '@storybook/addon-a11y'],
+	framework: {
+		name: '@storybook/web-components-vite',
+		options: {},
+	},
+	features: {
+		storyStoreV7: true,
+	},
+	staticDirs: ['../public-assets'],
+	typescript: {
+		check: true,
+	},
+	docs: {
+		autodocs: true,
+	},
 };
 export default config;
