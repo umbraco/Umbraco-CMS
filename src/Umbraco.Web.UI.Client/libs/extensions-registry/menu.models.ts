f39037632ca2c9f18dbffdaab56cff702369a5a3
Mads Rasmussen <madsr@hey.com>
2023-02-28 20:15:21 +0100

    remove unused label from menu model

--- src/Umbraco.Web.UI.Client/libs/extensions-registry/menu.models.ts
+++ src/Umbraco.Web.UI.Client/libs/extensions-registry/menu.models.ts
@@ -2,9 +2,4 @@ import type { ManifestElement } from './models';
 
 export interface ManifestMenu extends ManifestElement {
 	type: 'menu';
-	meta: MetaMenu;
-}
-
-export interface MetaMenu {
-	label: string;
 }
