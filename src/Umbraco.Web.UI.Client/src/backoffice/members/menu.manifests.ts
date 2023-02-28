0edbe541fc5b3c1d4449e245f29745d3c2986cd6
Mads Rasmussen <madsr@hey.com>
2023-02-28 19:34:57 +0100

    register members menu

---
+++ src/Umbraco.Web.UI.Client/src/backoffice/members/menu.manifests.ts
@@ -0,0 +1,12 @@
+import { ManifestMenu } from 'libs/extensions-registry/menu.models';
+
+const menu: ManifestMenu = {
+	type: 'menu',
+	alias: 'Umb.Menu.Members',
+	name: 'Members Menu',
+	meta: {
+		label: 'Members',
+	},
+};
+
+export const manifests = [menu];
