7c418d84c8e21205adae6782f0bcb8317ea1f4b7
Mads Rasmussen <madsr@hey.com>
2023-02-28 19:34:45 +0100

    register media menu

---
+++ src/Umbraco.Web.UI.Client/src/backoffice/media/menu.manifests.ts
@@ -0,0 +1,12 @@
+import { ManifestMenu } from 'libs/extensions-registry/menu.models';
+
+const menu: ManifestMenu = {
+	type: 'menu',
+	alias: 'Umb.Menu.Media',
+	name: 'Media Menu',
+	meta: {
+		label: 'Media',
+	},
+};
+
+export const manifests = [menu];
