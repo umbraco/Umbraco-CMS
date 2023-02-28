323138bbf92a98fd88fb3740be87f14a77450612
Mads Rasmussen <madsr@hey.com>
2023-02-28 14:46:42 +0100

    register content menu

---
+++ src/Umbraco.Web.UI.Client/src/backoffice/documents/menu.manifests.ts
@@ -0,0 +1,12 @@
+import { ManifestMenu } from 'libs/extensions-registry/menu.models';
+
+const menu: ManifestMenu = {
+	type: 'menu',
+	alias: 'Umb.Menu.Content',
+	name: 'Content Menu',
+	meta: {
+		label: 'Content',
+	},
+};
+
+export const manifests = [menu];
