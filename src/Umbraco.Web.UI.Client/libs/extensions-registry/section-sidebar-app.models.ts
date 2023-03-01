5d3af0a7a6c99ec45de05a27a9b478387ec8a5e0
Mads Rasmussen <madsr@hey.com>
2023-03-01 13:35:15 +0100

    move sidebar apps and sidebar menu app to same file and extend meta

--- src/Umbraco.Web.UI.Client/libs/extensions-registry/section-sidebar-app.models.ts
+++ src/Umbraco.Web.UI.Client/libs/extensions-registry/section-sidebar-app.models.ts
@@ -8,3 +8,14 @@ export interface ManifestSectionSidebarApp extends ManifestElement {
 export interface MetaSectionSidebarApp {
 	sections: Array<string>;
 }
+
+// TODO: this is a temp solution until we implement kinds
+export interface ManifestMenuSectionSidebarApp extends ManifestElement {
+	type: 'menuSectionSidebarApp';
+	meta: MetaMenuSectionSidebarApp;
+}
+
+export interface MetaMenuSectionSidebarApp extends MetaSectionSidebarApp {
+	label: string;
+	menu: string;
+}
