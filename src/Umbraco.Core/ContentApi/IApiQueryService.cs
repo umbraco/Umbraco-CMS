18d62a2e8c19561a18fbfa69d93acb8b63864121
Elitsa Marinovska <elm@umbraco.dk>
2023-03-06 11:52:19 +0100

    Introduce new ExecuteQuery that uses the Core OptionHandlers

--- src/Umbraco.Core/ContentApi/IApiQueryService.cs
+++ src/Umbraco.Core/ContentApi/IApiQueryService.cs
@@ -8,23 +8,19 @@ public interface IApiQueryService
     ApiQueryType GetQueryType(string queryOption);
 
     /// <summary>
-    ///     Extracts Guid from query.
-    /// </summary>
-    Guid? GetGuidFromFetch(string fetchQuery);
-
-    /// <summary>
     ///     Gets a collection of Guid from querying the Content API using a specific <see cref="ApiQueryType" /> for a given id.
     /// </summary>
     IEnumerable<Guid> GetGuidsFromQuery(Guid id, ApiQueryType queryType);
+
+    IEnumerable<Guid> ExecuteQuery(string query, string fieldValue);
 }
 
 /// <summary>
+///     ToDo: Remove
 ///     Defines the possible query types for the Content API.
 /// </summary>
 public enum ApiQueryType
 {
     Ancestors,
-    Children,
-    Descendants,
     Unknown // Custom
 }
