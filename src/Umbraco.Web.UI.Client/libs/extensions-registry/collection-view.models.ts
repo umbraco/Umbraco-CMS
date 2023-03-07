91b65723ec6928ed5a77493645aa52a1bd317ed1
Warren Buckley <warren@umbraco.com>
2023-03-07 13:56:18 +0000

    Collection View JSDoc

--- src/Umbraco.Web.UI.Client/libs/extensions-registry/collection-view.models.ts
+++ src/Umbraco.Web.UI.Client/libs/extensions-registry/collection-view.models.ts
@@ -1,13 +1,35 @@
 import type { ManifestElement } from './models';
 
+/**
+ * A collection view is a view that can be used to display a collection of entities.
+ * For example you may wish to display a collection of nodes as a table or grid of cards
+ */
 export interface ManifestCollectionView extends ManifestElement {
 	type: 'collectionView';
 	meta: MetaCollectionView;
 }
 
 export interface MetaCollectionView {
+	/**
+	 * The friendly name of the collection view
+	 */
 	label: string;
+
+	/**
+	 * An icon to represent the collection view
+	 * @example 'umb:box'
+	 * @example 'umb:grid'
+	 */
 	icon: string;
+
+	/**
+	 * The entity type that this collection view is for
+	 * @example 'media'
+	 */
 	entityType: string;
+
+	/**
+	 * The URL pathname for this collection view that can be deep linked to by sharing the url
+	 */
 	pathName: string;
 }
