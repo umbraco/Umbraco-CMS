9fefde8ef7352aaeb84c9e3dddc0d84559d1d7fa
Warren Buckley <warren@umbraco.com>
2023-03-07 21:23:14 +0000

    JSDoc header app

--- src/Umbraco.Web.UI.Client/libs/extensions-registry/header-app.models.ts
+++ src/Umbraco.Web.UI.Client/libs/extensions-registry/header-app.models.ts
@@ -1,10 +1,15 @@
 import type { ManifestElement } from './models';
 
+/**
+ * Header apps are displayed in the top right corner of the backoffice
+ * The two provided header apps are the search and the user menu
+ */
 export interface ManifestHeaderApp extends ManifestElement {
 	type: 'headerApp';
 	meta: MetaHeaderApp;
 }
 
+// TODO: Warren these don't seem to be used anywhere
 export interface MetaHeaderApp {
 	pathname: string;
 	label: string;
