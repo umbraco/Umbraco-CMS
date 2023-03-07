76309b7732def762b84adb617738370134283b99
Warren Buckley <warren@umbraco.com>
2023-03-07 12:23:58 +0000

    User dashboard manifest
    However unsure the label & pathname are used at all?

--- src/Umbraco.Web.UI.Client/libs/extensions-registry/user-dashboard.models.ts
+++ src/Umbraco.Web.UI.Client/libs/extensions-registry/user-dashboard.models.ts
@@ -1,5 +1,8 @@
 import type { ManifestElement } from './models';
 
+/**
+ * A user dashboard extension is shown in the dialog that opens when the user clicks on the user avatar in the top right corner
+ */
 export interface ManifestUserDashboard extends ManifestElement {
 	type: 'userDashboard';
 	meta: MetaUserDashboard;
