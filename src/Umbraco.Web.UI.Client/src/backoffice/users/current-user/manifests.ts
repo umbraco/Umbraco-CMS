5625a8c30dc91b9792a63312eda1eecfe465d283
Warren Buckley <warren@umbraco.com>
2023-03-07 12:30:30 +0000

    Remove unused meta properties for UserDashboard manifest interface
    https://umbraco.slack.com/archives/C04RZVARL81/p1678191653191159

--- src/Umbraco.Web.UI.Client/src/backoffice/users/current-user/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/users/current-user/manifests.ts
@@ -7,10 +7,6 @@ export const userDashboards: Array<ManifestUserDashboard> = [
 		name: 'Themes User Dashboard',
 		loader: () => import('./user-dashboard-themes.element'),
 		weight: 1,
-		meta: {
-			label: 'Themes User Dashboard',
-			pathname: 'themes',
-		},
 	},
 ];
 
