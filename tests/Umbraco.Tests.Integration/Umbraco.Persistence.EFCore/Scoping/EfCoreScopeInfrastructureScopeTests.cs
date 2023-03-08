bcccbb63034ace09c52e6964474db8c7fde650ad
Zeegaan <nge@umbraco.dk>
2023-03-08 08:23:48 +0100

    Add EfCoreScopeInfrastructureScopeTests.cs
    Signed-off-by: Zeegaan <nge@umbraco.dk>

---
+++ tests/Umbraco.Tests.Integration/Umbraco.Persistence.EFCore/Scoping/EfCoreScopeInfrastructureScopeTests.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.EntityFrameworkCore;
+using NUnit.Framework;
+using Umbraco.Cms.Infrastructure.Scoping;
+using Umbraco.Cms.Persistence.EFCore.Scoping;
+using Umbraco.Cms.Tests.Common.Testing;
+using Umbraco.Cms.Tests.Integration.Testing;
+
+namespace Umbraco.Cms.Tests.Integration.Umbraco.Persistence.EFCore.Scoping;
+
+[TestFixture]
+[UmbracoTest(Database = UmbracoTestOptions.Database.NewEmptyPerTest)]
+public class EfCoreScopeInfrastructureScopeTests : UmbracoIntegrationTest
+{
+    private IEfCoreScopeProvider EfCoreScopeProvider =>
+        GetRequiredService<IEfCoreScopeProvider>();    
+    
+    private IScopeProvider InfrastructureScopeProvider =>
+        GetRequiredService<IScopeProvider>();
+
+    private EFCoreScopeAccessor EfCoreScopeAccessor => (EFCoreScopeAccessor)GetRequiredService<IEFCoreScopeAccessor>();
+
+    private IScopeAccessor InfrastructureScopeAccessor => GetRequiredService<IScopeAccessor>();
+
+    [Test]
+    public void CanCreateNestedInfrastructureScope()
+    {
+        Assert.IsNull(EfCoreScopeAccessor.AmbientScope);
+        using (IEfCoreScope scope = EfCoreScopeProvider.CreateScope())
+        {
+            Assert.IsInstanceOf<EfCoreScope>(scope);
+            Assert.IsNotNull(EfCoreScopeAccessor.AmbientScope);
+            Assert.IsNotNull(InfrastructureScopeAccessor.AmbientScope);
+            Assert.AreSame(scope, EfCoreScopeAccessor.AmbientScope);
+            using (var infrastructureScope = InfrastructureScopeProvider.CreateScope())
+            {
+                Assert.AreNotSame(infrastructureScope, InfrastructureScopeAccessor.AmbientScope);
+            }
+
+            Assert.IsNotNull(InfrastructureScopeAccessor.AmbientScope);
+        }
+
+        Assert.IsNull(EfCoreScopeAccessor.AmbientScope);
+        Assert.IsNull(InfrastructureScopeAccessor.AmbientScope);
+    }
+
+    [Test]
+    public async Task CompleteWhenChildInfrastructureScopeDoesComplete()
+    {
+        using (IEfCoreScope parentScope = EfCoreScopeProvider.CreateScope())
+        {
+            await parentScope.ExecuteWithContextAsync<Task>(async database =>
+            {
+                await database.Database.ExecuteSqlAsync($"CREATE TABLE tmp3 (id INT, name NVARCHAR(64))");
+            });
+
+            // This should be using same transaction, so insert data into table we're creating
+            using (IScope childScope = InfrastructureScopeProvider.CreateScope())
+            {
+                childScope.Database.Execute("INSERT INTO tmp3 (id, name) VALUES (1, 'a')");
+                string n = ScopeAccessor.AmbientScope.Database.ExecuteScalar<string>("SELECT name FROM tmp3 WHERE id=1");
+                Assert.AreEqual("a", n);
+                childScope.Complete();
+            }
+
+            await parentScope.ExecuteWithContextAsync<Task>(async database =>
+            {
+                // Should still be in transaction and not rolled back yet
+                string? result = await database.Database.ExecuteScalarAsync<string>("SELECT name FROM tmp3 WHERE id=1");
+                Assert.AreEqual("a", result);
+            });
+
+
+            parentScope.Complete();
+        }
+
+        using (IEfCoreScope parentScope = EfCoreScopeProvider.CreateScope())
+        {
+            using (IScope childScope = InfrastructureScopeProvider.CreateScope())
+            {
+                childScope.Database.Execute("INSERT INTO tmp3 (id, name) VALUES (1, 'a')");
+                string n = ScopeAccessor.AmbientScope.Database.ExecuteScalar<string>("SELECT name FROM tmp3 WHERE id=1");
+                Assert.AreEqual("a", n);
+            }
+
+            await parentScope.ExecuteWithContextAsync<Task>(async database =>
+            {
+                // Should still be in transaction and not rolled back yet
+                string? result = await database.Database.ExecuteScalarAsync<string>("SELECT name FROM tmp3 WHERE id=1");
+                Assert.AreEqual("a", result);
+            });
+
+            parentScope.Complete();
+        }
+
+        // Check that its rolled back
+        using (IEfCoreScope scope = EfCoreScopeProvider.CreateScope())
+        {
+            await scope.ExecuteWithContextAsync<Task>(async database =>
+            {
+                // Should still be in transaction and not rolled back yet
+                string? result = await database.Database.ExecuteScalarAsync<string>("SELECT name FROM tmp3 WHERE id=1");
+                Assert.IsNull(result);
+            });
+        }
+    }
+    
+    [Test]
+    public async Task DontCompleteWhenChildInfrastructureScopeDoesNotComplete()
+    {
+        using (IEfCoreScope scope = EfCoreScopeProvider.CreateScope())
+        {
+            await scope.ExecuteWithContextAsync<Task>(async database =>
+            {
+                await database.Database.ExecuteSqlAsync($"CREATE TABLE tmp3 (id INT, name NVARCHAR(64))");
+            });
+            scope.Complete();
+        }
+
+        using (IEfCoreScope parentScope = EfCoreScopeProvider.CreateScope())
+        {
+            using (IScope scope = InfrastructureScopeProvider.CreateScope())
+            {
+                scope.Database.Execute("INSERT INTO tmp3 (id, name) VALUES (1, 'a')");
+                string n = ScopeAccessor.AmbientScope.Database.ExecuteScalar<string>("SELECT name FROM tmp3 WHERE id=1");
+                Assert.AreEqual("a", n);
+            }
+
+            await parentScope.ExecuteWithContextAsync<Task>(async database =>
+            {
+                // Should still be in transaction and not rolled back yet
+                string? result = await database.Database.ExecuteScalarAsync<string>("SELECT name FROM tmp3 WHERE id=1");
+                Assert.AreEqual("a", result);
+            });
+
+            parentScope.Complete();
+        }
+
+        // Check that its rolled back
+        using (IEfCoreScope scope = EfCoreScopeProvider.CreateScope())
+        {
+            await scope.ExecuteWithContextAsync<Task>(async database =>
+            {
+                // Should still be in transaction and not rolled back yet
+                string? result = await database.Database.ExecuteScalarAsync<string>("SELECT name FROM tmp3 WHERE id=1");
+                Assert.IsNull(result);
+            });
+        }
+    }
+
+}
