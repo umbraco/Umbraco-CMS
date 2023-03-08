cceab47006a151dad78d41e54827504364fa3629
Mads Rasmussen <madsr@hey.com>
2023-03-08 14:04:23 +0100

    register modal

--- src/Umbraco.Web.UI.Client/src/backoffice/shared/modals/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/shared/modals/manifests.ts
@@ -7,6 +7,12 @@ const modals: Array<ManifestModal> = [
 		name: 'Icon Picker Modal',
 		loader: () => import('./icon-picker/icon-picker-modal.element'),
 	},
+	{
+		type: 'modal',
+		alias: 'Umb.Modal.LinkPicker',
+		name: 'Link Picker Modal',
+		loader: () => import('./link-picker/link-picker-modal.element'),
+	},
 ];
 
 export const manifests = [...modals];
