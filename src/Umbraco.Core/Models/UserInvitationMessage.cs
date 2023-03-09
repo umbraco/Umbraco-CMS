7d3b838c87900cf7d95486b92418c41f6ca11080
Nikolaj <nikolajlauridsen@protonmail.ch>
2023-03-09 10:41:01 +0100

    Add EmailUserInviteSender

---
+++ src/Umbraco.Core/Models/UserInvitationMessage.cs
@@ -0,0 +1,16 @@
+﻿using Umbraco.Cms.Core.Models.Membership;
+
+namespace Umbraco.Cms.Core.Models;
+
+public class UserInvitationMessage
+{
+    public required string Sender { get; set; }
+
+    public required string SenderEmail { get; set; }
+
+    public required string Message { get; set; }
+
+    public required Uri InviteUri { get; set; }
+
+    public required IUser Recipient { get; set; }
+}
