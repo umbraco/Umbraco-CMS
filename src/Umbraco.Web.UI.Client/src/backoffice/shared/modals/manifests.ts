e63058a049ee4335cebe27a89749ad964df8f171
Mads Rasmussen <madsr@hey.com>
2023-03-09 20:26:59 +0100

    remove unused modal

--- src/Umbraco.Web.UI.Client/src/backoffice/shared/modals/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/shared/modals/manifests.ts
@@ -3,12 +3,6 @@ import type { ManifestModal } from '@umbraco-cms/extensions-registry';
 const modals: Array<ManifestModal> = [
 	{
 		type: 'modal',
-		alias: 'Umb.Modal.Basic',
-		name: 'Basic Modal',
-		loader: () => import('./basic/basic-modal.element'),
-	},
-	{
-		type: 'modal',
 		alias: 'Umb.Modal.Confirm',
 		name: 'Confirm Modal',
 		loader: () => import('./confirm/confirm-modal.element'),
