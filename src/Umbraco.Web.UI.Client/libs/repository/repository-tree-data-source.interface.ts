e5744fe14ef9c8b79743926eb9fb62242eba9c62
Jesper Møller Jensen <26099018+JesmoDev@users.noreply.github.com>
2023-03-14 12:50:04 +1300

    bring back deleted function in interface

--- src/Umbraco.Web.UI.Client/libs/repository/repository-tree-data-source.interface.ts
+++ src/Umbraco.Web.UI.Client/libs/repository/repository-tree-data-source.interface.ts
@@ -3,5 +3,6 @@ import type { DataSourceResponse } from '@umbraco-cms/models';
 
 export interface RepositoryTreeDataSource {
 	getRootItems(): Promise<DataSourceResponse<PagedEntityTreeItemModel>>;
+	getChildrenOf(parentKey: string): Promise<DataSourceResponse<PagedEntityTreeItemModel>>;
 	getItems(key: Array<string>): Promise<DataSourceResponse<EntityTreeItemModel[]>>;
 }
