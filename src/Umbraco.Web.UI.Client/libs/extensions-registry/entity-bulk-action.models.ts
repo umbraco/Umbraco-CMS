00ce3b027504d5ed76cf66001943c1941d61d95f
Mads Rasmussen <madsr@hey.com>
2023-03-13 20:43:05 +0100

    add conditions to bulk action manifest

--- src/Umbraco.Web.UI.Client/libs/extensions-registry/entity-bulk-action.models.ts
+++ src/Umbraco.Web.UI.Client/libs/extensions-registry/entity-bulk-action.models.ts
@@ -3,11 +3,15 @@ import type { ManifestElement } from './models';
 export interface ManifestEntityBulkAction extends ManifestElement {
 	type: 'entityBulkAction';
 	meta: MetaEntityBulkAction;
+	conditions: ConditionsEntityBulkAction;
 }
 
 export interface MetaEntityBulkAction {
 	label: string;
-	entityType: string;
 	api: any; // create interface
 	repositoryAlias: string;
+}
+
+export interface ConditionsEntityBulkAction {
+	entityType: string;
 }
