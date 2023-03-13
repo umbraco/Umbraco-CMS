c94189ff2812045e03e89a9c9a8d6655e165be14
Mads Rasmussen <madsr@hey.com>
2023-03-13 20:22:05 +0100

    add conditions to collection view

--- src/Umbraco.Web.UI.Client/libs/extensions-registry/collection-view.models.ts
+++ src/Umbraco.Web.UI.Client/libs/extensions-registry/collection-view.models.ts
@@ -3,11 +3,15 @@ import type { ManifestElement } from './models';
 export interface ManifestCollectionView extends ManifestElement {
 	type: 'collectionView';
 	meta: MetaCollectionView;
+	conditions: ConditionsCollectionView;
 }
 
 export interface MetaCollectionView {
 	label: string;
 	icon: string;
-	entityType: string;
 	pathName: string;
+}
+
+export interface ConditionsCollectionView {
+	entityType: string;
 }
