af7cb18551b9288311c421bbcc9493deef8c6b19
Jesper Møller Jensen <26099018+JesmoDev@users.noreply.github.com>
2023-03-14 16:04:25 +1300

    fixed manifest

--- src/Umbraco.Web.UI.Client/src/backoffice/settings/relation-types/repository/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/settings/relation-types/repository/manifests.ts
@@ -1,13 +1,33 @@
 import { UmbRelationTypeRepository } from '../repository/relation-type.repository';
+import { UmbRelationTypeStore } from './relation-type.store';
+import { UmbRelationTypeTreeStore } from './relation-type.tree.store';
 import { ManifestRepository } from 'libs/extensions-registry/repository.models';
+import { ManifestStore, ManifestTreeStore } from '@umbraco-cms/extensions-registry';
 
-export const RELATION_TYPE_REPOSITORY_ALIAS = 'Umb.Repository.RelationTypes';
+export const RELATION_TYPE_REPOSITORY_ALIAS = 'Umb.Repository.RelationType';
 
 const repository: ManifestRepository = {
 	type: 'repository',
 	alias: RELATION_TYPE_REPOSITORY_ALIAS,
-	name: 'Relation Types Repository',
+	name: 'Relation Type Repository',
 	class: UmbRelationTypeRepository,
 };
 
-export const manifests = [repository];
+export const RELATION_TYPE_STORE_ALIAS = 'Umb.Store.RelationType';
+export const RELATION_TYPE_TREE_STORE_ALIAS = 'Umb.Store.RelationTypeTree';
+
+const store: ManifestStore = {
+	type: 'store',
+	alias: RELATION_TYPE_STORE_ALIAS,
+	name: 'Relation Type Store',
+	class: UmbRelationTypeStore,
+};
+
+const treeStore: ManifestTreeStore = {
+	type: 'treeStore',
+	alias: RELATION_TYPE_TREE_STORE_ALIAS,
+	name: 'Relation Type Tree Store',
+	class: UmbRelationTypeTreeStore,
+};
+
+export const manifests = [repository, store, treeStore];
