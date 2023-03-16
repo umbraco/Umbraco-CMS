d4c7f6288c11fa5fc4e3e1b6dd02c34e4e91d298
Niels Lyngsø <nsl@umbraco.dk>
2023-03-16 09:59:29 +0100

    fix import order

--- src/Umbraco.Web.UI.Client/src/backoffice/shared/components/workspace-property/workspace-property.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/shared/components/workspace-property/workspace-property.element.ts
@@ -2,9 +2,9 @@ import { UUITextStyles } from '@umbraco-ui/uui-css/lib';
 import { css, html } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
 import { ifDefined } from 'lit-html/directives/if-defined.js';
+import { UmbPropertyEditorElement } from '@umbraco-cms/property-editor';
 import { UmbVariantId } from '../../variants/variant-id.class';
 import { UmbWorkspacePropertyContext } from './workspace-property.context';
-import { UmbPropertyEditorElement } from '@umbraco-cms/property-editor';
 import { createExtensionElement, umbExtensionsRegistry } from '@umbraco-cms/extensions-api';
 import type { ManifestPropertyEditorUI } from '@umbraco-cms/models';
 
