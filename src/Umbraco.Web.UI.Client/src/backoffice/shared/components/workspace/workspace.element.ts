47465e870a8cf3683165f98570714815c82dd36a
Mads Rasmussen <madsr@hey.com>
2023-03-16 09:33:37 +0100

    don't render routes in umb-workspace element

--- src/Umbraco.Web.UI.Client/src/backoffice/shared/components/workspace/workspace.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/shared/components/workspace/workspace.element.ts
@@ -1,101 +1,21 @@
 import { UUITextStyles } from '@umbraco-ui/uui-css/lib';
 import { css, html, nothing } from 'lit';
-import { customElement, property, state } from 'lit/decorators.js';
-import { map } from 'rxjs';
-import { IRoute, IRoutingInfo } from 'router-slot';
-import { UmbRouteLocation } from '@umbraco-cms/router';
+import { customElement, property } from 'lit/decorators.js';
 import { UmbLitElement } from '@umbraco-cms/element';
-import { createExtensionElement, umbExtensionsRegistry } from '@umbraco-cms/extensions-api';
 import { ManifestWorkspace } from '@umbraco-cms/extensions-registry';
-
-export interface UmbWorkspaceEntityElement extends HTMLElement {
-	manifest: ManifestWorkspace;
-	location: UmbRouteLocation;
-}
 
 @customElement('umb-workspace')
 export class UmbWorkspaceElement extends UmbLitElement {
 	static styles = [UUITextStyles, css``];
 
-	private _entityType = '';
-	@property()
-	public get entityType() {
-		return this._entityType;
-	}
-	public set entityType(value) {
-		const oldValue = this._entityType;
-		this._entityType = value;
-		this.#observeWorkspace();
-		this.requestUpdate('entityType', oldValue);
-	}
-
-	@state()
-	_element?: UmbWorkspaceElement;
-
-	@state()
-	_routes: Array<IRoute> = [];
-
-	async #createRoutes(workspaceManifest: ManifestWorkspace) {
-		const workspaceContextModule = await workspaceManifest.meta.context?.();
-		const workspaceContext = workspaceContextModule ? new workspaceContextModule.default(this) : undefined;
-		const paths = (await workspaceContext?.getPaths?.()) || [];
-
-		const routes = paths.map((path: any) => {
-			return {
-				path: path.path,
-				component: () => createExtensionElement(workspaceManifest),
-				setup: (component: Promise<UmbWorkspaceEntityElement>, info: IRoutingInfo) =>
-					this.#onRouteSetup(component, info, path, workspaceManifest),
-			};
-		});
-
-		this._routes = [
-			...routes,
-			{
-				path: '**',
-				component: () => createExtensionElement(workspaceManifest),
-				setup: (component: Promise<UmbWorkspaceEntityElement>, info: IRoutingInfo) =>
-					this.#onRouteSetup(component, info, { name: 'catch-all', params: {} }, workspaceManifest),
-			},
-		];
-	}
-
-	#onRouteSetup(
-		component: Promise<UmbWorkspaceEntityElement>,
-		info: IRoutingInfo,
-		path: any,
-		workspaceManifest: ManifestWorkspace
-	) {
-		component.then((element) => {
-			element.manifest = workspaceManifest;
-			const location: UmbRouteLocation = {
-				name: path.name,
-				params: info.match.params,
-			};
-			element.location = location;
-		});
-	}
-
-	#observeWorkspace() {
-		this.observe(
-			umbExtensionsRegistry
-				.extensionsOfType('workspace')
-				.pipe(
-					map((workspaceManifests) =>
-						workspaceManifests.find((manifest) => manifest.meta.entityType === this.entityType)
-					)
-				),
-			async (workspaceManifest) => {
-				// TODO: add fallback element if we can't find the workspace
-				if (!workspaceManifest) return;
-				this.#createRoutes(workspaceManifest);
-			}
-		);
-	}
+	@property({ type: String, attribute: 'entity-type' })
+	entityType = '';
 
 	render() {
-		if (this._routes.length === 0) return nothing;
-		return html`<umb-router-slot .routes=${this._routes}></umb-router-slot>`;
+		if (!this.entityType) nothing;
+		return html`<umb-extension-slot
+			type="workspace"
+			.filter=${(manifest: ManifestWorkspace) => manifest.meta.entityType === this.entityType}></umb-extension-slot>`;
 	}
 }
 
