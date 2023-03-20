8a7724efa711f9c97d59673568017298b05ca46a
Mads Rasmussen <madsr@hey.com>
2023-03-20 13:10:15 +0100

    add todo

--- src/Umbraco.Web.UI.Client/src/backoffice/shared/components/workspace/workspace-context/workspace-entity-context.interface.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/shared/components/workspace/workspace-context/workspace-entity-context.interface.ts
@@ -2,7 +2,7 @@ import type { UmbWorkspaceContextInterface } from './workspace-context.interface
 
 export interface UmbEntityWorkspaceContextInterface<T = unknown> extends UmbWorkspaceContextInterface<T> {
 	getEntityKey(): string | undefined; // COnsider if this should go away now that we have getUnique()
-	getEntityType(): string;
+	getEntityType(): string; // TODO: consider of this should be on the repository because a repo is responsible for one entity type
 	getData(): T;
 	save(): Promise<void>;
 }
