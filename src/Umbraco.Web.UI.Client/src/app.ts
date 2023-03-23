f9cd211eb8e2a363390c5543da3fcad1ebca808a
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-03-23 15:17:22 +0100

    remove old uui-modal-* imports

--- src/Umbraco.Web.UI.Client/src/app.ts
+++ src/Umbraco.Web.UI.Client/src/app.ts
@@ -1,11 +1,6 @@
 import '@umbraco-ui/uui-css/dist/uui-css.css';
 import './core/css/custom-properties.css';
 
-// TODO: remove these imports when they are part of UUI
-import '@umbraco-ui/uui-modal';
-import '@umbraco-ui/uui-modal-container';
-import '@umbraco-ui/uui-modal-dialog';
-import '@umbraco-ui/uui-modal-sidebar';
 import 'element-internals-polyfill';
 
 import './core/router/router-slot.element';
@@ -16,11 +11,12 @@ import { UUIIconRegistryEssential } from '@umbraco-ui/uui';
 import { css, html } from 'lit';
 import { customElement, property, state } from 'lit/decorators.js';
 
+import { UmbIconStore } from './core/stores/icon/icon.store';
+
 import type { Guard, IRoute } from '@umbraco-cms/internal/router';
 import { UmbLitElement } from '@umbraco-cms/internal/lit-element';
 import { tryExecuteAndNotify } from '@umbraco-cms/backoffice/resources';
 import { OpenAPI, RuntimeLevelModel, ServerResource } from '@umbraco-cms/backoffice/backend-api';
-import { UmbIconStore } from './core/stores/icon/icon.store';
 import { umbDebugContextEventType } from '@umbraco-cms/backoffice/context-api';
 
 @customElement('umb-app')
