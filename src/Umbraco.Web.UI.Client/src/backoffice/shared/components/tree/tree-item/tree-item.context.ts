9dcc631ea3d35820dc9b858c3e7a283fe872df82
Mads Rasmussen <madsr@hey.com>
2023-03-23 09:19:42 +0100

    move most business logic from tree item element to a tree item context

---
+++ src/Umbraco.Web.UI.Client/src/backoffice/shared/components/tree/tree-item/tree-item.context.ts
@@ -0,0 +1,136 @@
+import { map } from 'rxjs';
+import {
+	UmbSectionSidebarContext,
+	UMB_SECTION_SIDEBAR_CONTEXT_TOKEN,
+} from '../../section/section-sidebar/section-sidebar.context';
+import { UmbSectionContext, UMB_SECTION_CONTEXT_TOKEN } from '../../section/section.context';
+import { UmbTreeContextBase } from '../tree.context';
+import { BooleanState, StringState, UmbObserverController } from '@umbraco-cms/backoffice/observable-api';
+import { UmbControllerHostInterface } from '@umbraco-cms/backoffice/controller';
+import { UmbContextConsumerController, UmbContextToken } from '@umbraco-cms/backoffice/context-api';
+import { umbExtensionsRegistry } from '@umbraco-cms/backoffice/extensions-api';
+import type { TreeItemPresentationModel } from '@umbraco-cms/backoffice/backend-api';
+
+export class UmbTreeItemContextBase<T extends TreeItemPresentationModel = TreeItemPresentationModel> {
+	public host: UmbControllerHostInterface;
+	public treeItem: T;
+	public unique: string;
+	public type: string;
+
+	#isLoading = new BooleanState(false);
+	isLoading = this.#isLoading.asObservable();
+
+	#isSelectable = new BooleanState(false);
+	isSelectable = this.#isSelectable.asObservable();
+
+	#isSelected = new BooleanState(false);
+	isSelected = this.#isSelected.asObservable();
+
+	#isActive = new BooleanState(false);
+	isActive = this.#isActive.asObservable();
+
+	#hasActions = new BooleanState(false);
+	hasActions = this.#hasActions.asObservable();
+
+	#path = new StringState('');
+	path = this.#path.asObservable();
+
+	treeContext?: UmbTreeContextBase;
+	#sectionContext?: UmbSectionContext;
+	#sectionSidebarContext?: UmbSectionSidebarContext;
+
+	constructor(host: UmbControllerHostInterface, treeItem: T, getUnique: (x: T) => string | null | undefined) {
+		this.host = host;
+		this.treeItem = treeItem;
+
+		const unique = getUnique(this.treeItem);
+		if (!unique) throw new Error('Could not create tree item context, unique key is missing');
+		this.unique = unique;
+
+		if (!this.treeItem.type) throw new Error('Could not create tree item context, tree item type is missing');
+
+		this.type = this.treeItem.type;
+
+		new UmbContextConsumerController(host, UMB_SECTION_CONTEXT_TOKEN, (instance) => {
+			this.#sectionContext = instance;
+			this.#observeSectionPath();
+		});
+
+		new UmbContextConsumerController(host, UMB_SECTION_SIDEBAR_CONTEXT_TOKEN, (instance) => {
+			this.#sectionSidebarContext = instance;
+		});
+
+		new UmbContextConsumerController(host, 'umbTreeContext', (treeContext: UmbTreeContextBase) => {
+			this.treeContext = treeContext;
+			this.#observeIsSelectable();
+			this.#observeIsSelected();
+		});
+
+		this.#observeTreeItemActions();
+	}
+
+	public async requestChildren() {
+		// TODO: wait for tree context to be ready
+		this.#isLoading.next(true);
+		const response = await this.treeContext!.repository.requestTreeItemsOf(this.unique);
+		this.#isLoading.next(false);
+		return response;
+	}
+
+	public toggleContextMenu() {
+		this.#sectionSidebarContext?.toggleContextMenu(this.type, this.unique, this.treeItem.name || '');
+	}
+
+	public select() {
+		this.treeContext?.select(this.unique);
+	}
+
+	public deselect() {
+		this.treeContext?.deselect(this.unique);
+	}
+
+	#observeIsSelectable() {
+		if (!this.treeContext) return;
+		new UmbObserverController(this.host, this.treeContext.selectable, (value) => this.#isSelectable.next(value));
+	}
+
+	#observeIsSelected() {
+		if (!this.treeContext) return;
+
+		new UmbObserverController(
+			this.host,
+			this.treeContext.selection.pipe(map((selection) => selection.includes(this.unique))),
+			(isSelected) => {
+				this.#isSelected.next(isSelected);
+			}
+		);
+	}
+
+	#observeSectionPath() {
+		if (!this.#sectionContext) return;
+
+		new UmbObserverController(this.host, this.#sectionContext.pathname, (pathname) => {
+			if (!pathname) return;
+			const path = this.#constructPath(pathname, this.type, this.unique);
+			this.#path.next(path);
+		});
+	}
+
+	#observeTreeItemActions() {
+		new UmbObserverController(
+			this.host,
+			umbExtensionsRegistry
+				.extensionsOfType('entityAction')
+				.pipe(map((actions) => actions.filter((action) => action.conditions.entityType === this.type))),
+			(actions) => {
+				this.#hasActions.next(actions.length > 0);
+			}
+		);
+	}
+
+	#constructPath(pathname: string, entityType: string, key: string) {
+		return `section/${pathname}/${entityType}/edit/${key}`;
+	}
+}
+
+export const UMB_TREE_ITEM_CONTEXT_TOKEN = new UmbContextToken<UmbTreeItemContextBase>('UmbTreeItemContext');
--- src/Umbraco.Web.UI.Client/src/backoffice/shared/components/tree/tree-item/tree-item.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/shared/components/tree/tree-item/tree-item.element.ts
@@ -2,256 +2,122 @@ import { css, html, nothing } from 'lit';
 import { UUITextStyles } from '@umbraco-ui/uui-css/lib';
 import { customElement, property, state } from 'lit/decorators.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
-import { map, Observable } from 'rxjs';
 import { repeat } from 'lit/directives/repeat.js';
-import { UmbSectionContext, UMB_SECTION_CONTEXT_TOKEN } from '../../section/section.context';
-import {
-	UmbSectionSidebarContext,
-	UMB_SECTION_SIDEBAR_CONTEXT_TOKEN,
-} from '../../section/section-sidebar/section-sidebar.context';
-import type { UmbTreeContextBase } from '../tree.context';
-import type { Entity } from '@umbraco-cms/backoffice/models';
-import type { UmbTreeStore } from '@umbraco-cms/backoffice/store';
+import { UmbTreeItemContextBase, UMB_TREE_ITEM_CONTEXT_TOKEN } from './tree-item.context';
 import { UmbLitElement } from '@umbraco-cms/internal/lit-element';
-import { umbExtensionsRegistry } from '@umbraco-cms/backoffice/extensions-api';
+import { EntityTreeItemResponseModel } from '@umbraco-cms/backoffice/backend-api';
 
-// TODO: align menu items and tree items
 @customElement('umb-tree-item')
 export class UmbTreeItem extends UmbLitElement {
 	static styles = [UUITextStyles, css``];
 
-	@property({ type: String })
-	key = '';
-
-	@property({ type: String })
-	parentKey: string | null = null;
-
-	@property({ type: String })
-	label = '';
-
-	@property({ type: String })
-	icon = '';
-
-	private _entityType = '';
-	@property({ type: String })
-	get entityType() {
-		return this._entityType;
+	private _item?: EntityTreeItemResponseModel;
+	@property({ type: Object, attribute: false })
+	get item() {
+		return this._item;
 	}
-	set entityType(newVal) {
-		const oldVal = this._entityType;
-		this._entityType = newVal;
-		this.requestUpdate('entityType', oldVal);
-		this._observeEntityActions();
+	set item(newVal) {
+		const oldVal = this._item;
+		this._item = newVal;
+		this.requestUpdate('item', oldVal);
 	}
 
 	@property({ type: Boolean, attribute: 'has-children' })
 	hasChildren = false;
 
 	@state()
-	private _childItems?: Entity[];
+	private _childItems?: EntityTreeItemResponseModel[];
 
 	@state()
 	private _href?: string;
 
 	@state()
-	private _loading = false;
+	private _isLoading = false;
 
 	@state()
-	private _selectable = false;
+	private _isSelectable = false;
 
 	@state()
-	private _selected = false;
-
-	@state()
-	private _isActive = false;
+	private _isSelected = false;
 
 	@state()
 	private _hasActions = false;
 
-	private _treeContext?: UmbTreeContextBase;
-	private _store?: UmbTreeStore<unknown>;
-	private _sectionContext?: UmbSectionContext;
-	private _sectionSidebarContext?: UmbSectionSidebarContext;
+	#treeItemContext?: UmbTreeItemContextBase;
 
 	constructor() {
 		super();
 
-		this.consumeContext('umbTreeContext', (treeContext: UmbTreeContextBase) => {
-			this._treeContext = treeContext;
-			this._observeSelectable();
-			this._observeIsSelected();
-		});
-
-		this.consumeContext('umbStore', (store: UmbTreeStore<unknown>) => {
-			this._store = store;
-		});
-
-		this.consumeContext(UMB_SECTION_CONTEXT_TOKEN, (sectionContext) => {
-			this._sectionContext = sectionContext;
-			this._observeSection();
-			this._observeActiveTreeItem();
-		});
-
-		this.consumeContext(UMB_SECTION_SIDEBAR_CONTEXT_TOKEN, (instance) => {
-			this._sectionSidebarContext = instance;
+		this.consumeContext(UMB_TREE_ITEM_CONTEXT_TOKEN, (instance) => {
+			this.#treeItemContext = instance;
+			if (!this.#treeItemContext) return;
+			// TODO: investigate if we can make an observe decorator
+			this.observe(this.#treeItemContext.isLoading, (value) => (this._isLoading = value));
+			this.observe(this.#treeItemContext.isSelectable, (value) => (this._isSelectable = value));
+			this.observe(this.#treeItemContext.isSelected, (value) => (this._isSelected = value));
+			this.observe(this.#treeItemContext.hasActions, (value) => (this._hasActions = value));
+			this.observe(this.#treeItemContext.path, (value) => (this._href = value));
 		});
 	}
 
 	connectedCallback(): void {
 		super.connectedCallback();
-
 		this.addEventListener('selected', this._handleSelectedItem);
 		this.addEventListener('unselected', this._handleDeselectedItem);
 	}
 
 	private _handleSelectedItem(event: Event) {
 		event.stopPropagation();
-		this._treeContext?.select(this.key);
+		this.#treeItemContext?.select();
 	}
 
 	private _handleDeselectedItem(event: Event) {
 		event.stopPropagation();
-		this._treeContext?.deselect(this.key);
-	}
-
-	private _observeSection() {
-		if (!this._sectionContext) return;
-
-		this.observe(this._sectionContext?.pathname, (pathname) => {
-			this._href = this._constructPath(pathname || '', this.entityType, this.key);
-		});
-	}
-
-	private _observeSelectable() {
-		if (!this._treeContext) return;
-
-		this.observe(this._treeContext.selectable, (value) => {
-			this._selectable = value || false;
-		});
-	}
-
-	private _observeIsSelected() {
-		if (!this._treeContext) return;
-
-		this.observe(this._treeContext.selection.pipe(map((keys) => keys?.includes(this.key))), (isSelected) => {
-			this._selected = isSelected || false;
-		});
-	}
-
-	private _observeActiveTreeItem() {
-		if (!this._sectionContext) return;
-
-		this.observe(this._sectionContext?.activeTreeItem, (treeItem) => {
-			this._isActive = this.key === treeItem?.key;
-		});
-	}
-
-	private _observeEntityActions() {
-		// TODO: Stop previous observation, currently we can do this from the UmbElementMixin as its a new subscription when Actions or entityType has changed.
-		// Solution: store the current observation controller and if it existing then destroy it.
-		// TODO: as long as a tree consist of one entity type we don't have to observe this every time a new tree item is created.
-		// Solution: move this to the tree context and observe it once.
-		this.observe(
-			umbExtensionsRegistry
-				.extensionsOfType('entityAction')
-				.pipe(map((actions) => actions.filter((action) => action.conditions.entityType === this._entityType))),
-			(actions) => {
-				this._hasActions = actions.length > 0;
-			}
-		);
-	}
-
-	// TODO: how do we handle this?
-	private _constructPath(sectionPathname: string, type: string, key: string) {
-		return type ? `section/${sectionPathname}/workspace/${type}/edit/${key}` : undefined;
+		this.#treeItemContext?.deselect();
 	}
 
 	// TODO: do we want to catch and emit a backoffice event here?
 	private _onShowChildren() {
 		if (this._childItems && this._childItems.length > 0) return;
-		this._observeChildren();
-		this._observeRepositoryChildren();
+		this.#observeChildren();
 	}
 
-	private async _observeRepositoryChildren() {
-		if (!this._treeContext?.requestChildrenOf) return;
+	async #observeChildren() {
+		if (!this.#treeItemContext?.requestChildren) return;
 
-		// TODO: add loading state
-		this._treeContext.requestChildrenOf(this.key);
+		const { asObservable } = await this.#treeItemContext.requestChildren();
+		if (!asObservable) return;
 
-		this.observe(await this._treeContext.childrenOf(this.key), (childItems) => {
-			this._childItems = childItems as Entity[];
-		});
-	}
-
-	// TODO: remove when repositories are in place
-	private _observeChildren() {
-		if (!this._store?.getTreeItemChildren) return;
-
-		this._loading = true;
-
-		// TODO: we should do something about these types, stop having our own version of Entity.
-		this.observe(this._store.getTreeItemChildren(this.key) as Observable<Entity[]>, (childItems) => {
+		this.observe(asObservable(), (childItems) => {
 			this._childItems = childItems;
-			this._loading = false;
 		});
 	}
 
 	private _openActions() {
-		if (!this._treeContext || !this._sectionContext) return;
-
-		// This is out-commented as it was not used. only kept if someone need this later:
-		//this._sectionContext?.setActiveTree(this._treeContext?.tree);
-
-		this._sectionContext?.setActiveTreeItem({
-			key: this.key,
-			name: this.label,
-			icon: this.icon,
-			type: this.entityType,
-			hasChildren: this.hasChildren,
-			parentKey: this.parentKey,
-		});
-		this._sectionSidebarContext?.toggleContextMenu(this.entityType, this.key, this.label);
+		this.#treeItemContext?.toggleContextMenu();
 	}
 
 	render() {
 		return html`
 			<uui-menu-item
 				@show-children=${this._onShowChildren}
-				?selectable=${this._selectable}
-				?selected=${this._selected}
-				.loading=${this._loading}
+				?selectable=${this._isSelectable}
+				?selected=${this._isSelected}
+				.loading=${this._isLoading}
 				.hasChildren=${this.hasChildren}
-				label="${this.label}"
-				href="${ifDefined(this._href)}"
-				?active=${this._isActive}>
-				${this._renderChildItems()}
-				<uui-icon slot="icon" name="${this.icon}"></uui-icon>
-				${this._renderActions()}
+				label="${ifDefined(this.item?.name)}"
+				href="${ifDefined(this._href)}">
+				${this.#renderIcon()} ${this.#renderActions()} ${this.#renderChildItems()}
 				<slot></slot>
 			</uui-menu-item>
 		`;
 	}
 
-	private _renderChildItems() {
-		return html`
-			${this._childItems
-				? repeat(
-						this._childItems,
-						(item) => item.key,
-						(item) =>
-							html`<umb-tree-item
-								.key=${item.key}
-								.label=${item.name}
-								.icon=${item.icon}
-								.entityType=${item.type}
-								.hasChildren=${item.hasChildren}></umb-tree-item>`
-				  )
-				: ''}
-		`;
+	#renderIcon() {
+		return html` ${this.item?.icon ? html` <uui-icon slot="icon" name="${this.item.icon}"></uui-icon> ` : nothing} `;
 	}
 
-	private _renderActions() {
+	#renderActions() {
 		return html`
 			${this._hasActions
 				? html`
@@ -262,6 +128,18 @@ export class UmbTreeItem extends UmbLitElement {
 						</uui-action-bar>
 				  `
 				: nothing}
+		`;
+	}
+
+	#renderChildItems() {
+		return html`
+			${this._childItems
+				? repeat(
+						this._childItems,
+						(item) => item.key,
+						(item) => html`<umb-tree-item .item=${item}></umb-tree-item>`
+				  )
+				: ''}
 		`;
 	}
 }
