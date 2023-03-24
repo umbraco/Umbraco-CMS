5b7e85423cb18a5677b4fb926e5c7c360faf5e5f
Mads Rasmussen <madsr@hey.com>
2023-03-24 13:12:48 +0100

    make url friendly unique for file system tree items

--- src/Umbraco.Web.UI.Client/src/backoffice/shared/components/tree/file-system-tree-item/file-system-tree-item.context.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/shared/components/tree/file-system-tree-item/file-system-tree-item.context.ts
@@ -1,10 +1,15 @@
 import { UmbTreeItemContextBase } from '../tree-item-base/tree-item-base.context';
 import { UmbControllerHostInterface } from '@umbraco-cms/backoffice/controller';
 import { FileSystemTreeItemPresentationModel } from '@umbraco-cms/backoffice/backend-api';
+import { urlFriendlyPathFromServerPath } from 'src/backoffice/templating/utils';
 
 // TODO get unique method from an entity repository static method
 export class UmbFileSystemTreeItemContext extends UmbTreeItemContextBase<FileSystemTreeItemPresentationModel> {
 	constructor(host: UmbControllerHostInterface, treeItem: FileSystemTreeItemPresentationModel) {
 		super(host, treeItem, (x: FileSystemTreeItemPresentationModel) => x.path);
+	}
+
+	constructPath(pathname: string, entityType: string, path: string) {
+		return `section/${pathname}/workspace/${entityType}/edit/${urlFriendlyPathFromServerPath(path)}`;
 	}
 }
