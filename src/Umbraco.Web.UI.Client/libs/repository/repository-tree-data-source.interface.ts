0dcbd7b5e72d7d79fdbfdec4e5afd337dcbf9e53
Mads Rasmussen <madsr@hey.com>
2023-03-24 09:37:11 +0100

    make tree data source a generic

--- src/Umbraco.Web.UI.Client/libs/repository/repository-tree-data-source.interface.ts
+++ src/Umbraco.Web.UI.Client/libs/repository/repository-tree-data-source.interface.ts
@@ -1,8 +1,8 @@
 import { EntityTreeItemResponseModel, PagedEntityTreeItemResponseModel } from '@umbraco-cms/backoffice/backend-api';
 import type { DataSourceResponse } from '@umbraco-cms/backoffice/repository';
 
-export interface RepositoryTreeDataSource {
-	getRootItems(): Promise<DataSourceResponse<PagedEntityTreeItemResponseModel>>;
-	getChildrenOf(parentKey: string): Promise<DataSourceResponse<PagedEntityTreeItemResponseModel>>;
+export interface RepositoryTreeDataSource<T = PagedEntityTreeItemResponseModel> {
+	getRootItems(): Promise<DataSourceResponse<T>>;
+	getChildrenOf(parentKey: string): Promise<DataSourceResponse<T>>;
 	getItems(key: Array<string>): Promise<DataSourceResponse<EntityTreeItemResponseModel[]>>;
 }
