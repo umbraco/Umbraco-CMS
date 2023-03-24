c008d71bb3bfd54611d2c53369fd41c6a2d99e50
Mads Rasmussen <madsr@hey.com>
2023-03-24 10:46:31 +0100

    use tree store interface in manifest

--- src/Umbraco.Web.UI.Client/libs/extensions-registry/store.models.ts
+++ src/Umbraco.Web.UI.Client/libs/extensions-registry/store.models.ts
@@ -1,10 +1,10 @@
 import type { ManifestClass } from './models';
-import { UmbStoreBase, UmbEntityTreeStore } from '@umbraco-cms/backoffice/store';
+import { UmbStoreBase, UmbTreeStore } from '@umbraco-cms/backoffice/store';
 
 export interface ManifestStore extends ManifestClass<UmbStoreBase> {
 	type: 'store';
 }
 
-export interface ManifestTreeStore extends ManifestClass<UmbEntityTreeStore> {
+export interface ManifestTreeStore extends ManifestClass<UmbTreeStore> {
 	type: 'treeStore';
 }
