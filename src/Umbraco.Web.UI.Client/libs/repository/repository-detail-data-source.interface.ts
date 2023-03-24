54465d2c4a59d310686b8ba1f06d7667f5bd7f7e
Mads Rasmussen <madsr@hey.com>
2023-03-24 14:41:17 +0100

    remove repository from data source + prefix with Umb

--- src/Umbraco.Web.UI.Client/libs/repository/repository-detail-data-source.interface.ts
+++ src/Umbraco.Web.UI.Client/libs/repository/repository-detail-data-source.interface.ts
@@ -1,9 +1,9 @@
 import type { DataSourceResponse } from '@umbraco-cms/backoffice/repository';
 
-export interface RepositoryDetailDataSource<DetailType> {
-	createScaffold(parentKey: string | null): Promise<DataSourceResponse<DetailType>>;
-	get(key: string): Promise<DataSourceResponse<DetailType>>;
-	insert(data: DetailType): Promise<DataSourceResponse<DetailType>>;
-	update(data: DetailType): Promise<DataSourceResponse<DetailType>>;
-	delete(key: string): Promise<DataSourceResponse<DetailType>>;
+export interface UmbDataSource<T> {
+	createScaffold(parentKey: string | null): Promise<DataSourceResponse<T>>;
+	get(key: string): Promise<DataSourceResponse<T>>;
+	insert(data: T): Promise<DataSourceResponse<T>>;
+	update(data: T): Promise<DataSourceResponse<T>>;
+	delete(key: string): Promise<DataSourceResponse<T>>;
 }
