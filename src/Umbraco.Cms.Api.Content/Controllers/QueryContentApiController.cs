a486c2af0f29765146bdd4331c783605bfc29e12
Elitsa <elm@umbraco.dk>
2023-03-24 11:16:54 +0100

    Refactored the QueryContentApiController.cs

--- src/Umbraco.Cms.Api.Content/Controllers/QueryContentApiController.cs
+++ src/Umbraco.Cms.Api.Content/Controllers/QueryContentApiController.cs
@@ -1,4 +1,3 @@
-using System.Web;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Core.ContentApi;
@@ -11,19 +10,16 @@ namespace Umbraco.Cms.Api.Content.Controllers;
 
 public class QueryContentApiController : ContentApiControllerBase
 {
-    private readonly IHttpContextAccessor _httpContextAccessor;
     private readonly IApiQueryService _apiQueryService;
     private readonly IRequestRoutingService _requestRoutingService;
 
     public QueryContentApiController(
         IPublishedSnapshotAccessor publishedSnapshotAccessor,
         IApiContentBuilder apiContentBuilder,
-        IHttpContextAccessor httpContextAccessor,
         IApiQueryService apiQueryService,
         IRequestRoutingService requestRoutingService)
         : base(publishedSnapshotAccessor, apiContentBuilder)
     {
-        _httpContextAccessor = httpContextAccessor;
         _apiQueryService = apiQueryService;
         _requestRoutingService = requestRoutingService;
     }
@@ -35,44 +31,27 @@ public class QueryContentApiController : ContentApiControllerBase
     [MapToApiVersion("1.0")]
     [ProducesResponseType(typeof(IApiContent), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
-    public async Task<IActionResult> Query(string? fetch, string? filter, string? sort)
+    public async Task<IActionResult> Query(string? fetch, string[]? filter, string[]? sort)
     {
-        HttpContext? context = _httpContextAccessor.HttpContext;
-        if (context is null || (fetch is null && filter is null && sort is null))
-        {
-            return BadRequest("Not implemented yet.");
-        }
-
         IPublishedContentCache? contentCache = GetContentCache();
         if (contentCache is null)
         {
             return BadRequest(ContentCacheNotFoundProblemDetails());
         }
 
-        Guid? id = GetGuidFromQuery(fetch!, contentCache);
-        if (id is null)
-        {
-            return BadRequest("Invalid query value format.");
-        }
-
         IEnumerable<Guid> ids = Enumerable.Empty<Guid>();
-
-        string query = HttpUtility.UrlDecode(context.Request.QueryString.Value!.Substring(1));
-        var queryParams = query.Split('&')
-            .Select(p => p.Split('='))
-            .ToDictionary(p => p[0], p => p[1]);
-        ids = _apiQueryService.ExecuteQuery(queryParams, ((Guid)id).ToString());
+        ids = _apiQueryService.ExecuteQuery(fetch, filter, sort);
 
         IEnumerable<IPublishedContent> contentItems = ids.Select(contentCache.GetById)
-                                                         .WhereNotNull()
-                                                         .OrderBy(x => x.Path)
-                                                         .ThenBy(c => c.SortOrder);
+            .WhereNotNull()
+            .OrderBy(x => x.Path)
+            .ThenBy(c => c.SortOrder);
 
         // Currently sorting is not supported through the ContentAPI index
-        // So we need to add the name to it;
-        if (sort is not null && sort.StartsWith("name"))
+        // So we need to add the name to it
+        if (sort is not null && sort[0].StartsWith("name")) // TODO: change
         {
-            string sortValue = sort.Substring(sort.IndexOf(':', StringComparison.Ordinal) + 1);
+            string sortValue = sort[0].Substring(sort[0].IndexOf(':', StringComparison.Ordinal) + 1);
             if (sortValue.StartsWith("asc"))
             {
                 contentItems = contentItems.OrderBy(x => x.Name);
@@ -86,27 +65,5 @@ public class QueryContentApiController : ContentApiControllerBase
         IEnumerable<IApiContent> results = contentItems.Select(ApiContentBuilder.Build);
 
         return Ok(results);
-    }
-
-    private Guid? GetGuidFromQuery(string fetchQuery, IPublishedContentCache contentCache)
-    {
-        var queryStringValue = fetchQuery.Substring(fetchQuery.IndexOf(':', StringComparison.Ordinal) + 1);
-
-        if (Guid.TryParse(queryStringValue, out Guid id))
-        {
-            return id;
-        }
-
-        // Check if the passed value is a path of a content item
-        var contentRoute = _requestRoutingService.GetContentRoute(queryStringValue);
-
-        IPublishedContent? contentItem = contentCache.GetByRoute(contentRoute);
-
-        if (contentItem is not null)
-        {
-            return contentItem.Key;
-        }
-
-        return null;
     }
 }
