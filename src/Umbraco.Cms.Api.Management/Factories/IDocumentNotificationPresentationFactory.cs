8202879ba75be28c6b1531a12fac74b13d3f7e33
kjac <kja@umbraco.dk>
2023-03-27 07:55:18 +0200

    Add content notifications API

---
+++ src/Umbraco.Cms.Api.Management/Factories/IDocumentNotificationPresentationFactory.cs
@@ -0,0 +1,9 @@
+﻿using Umbraco.Cms.Api.Management.ViewModels.Document;
+using Umbraco.Cms.Core.Models;
+
+namespace Umbraco.Cms.Api.Management.Factories;
+
+public interface IDocumentNotificationPresentationFactory
+{
+    Task<IEnumerable<DocumentNotificationResponseModel>> CreateNotificationModelsAsync(IContent content);
+}
