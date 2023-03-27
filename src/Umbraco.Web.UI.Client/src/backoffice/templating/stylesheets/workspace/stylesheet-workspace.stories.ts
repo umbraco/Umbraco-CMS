7ca21588c47f2c1c517dea8631a3b675bf88f72f
Mads Rasmussen <madsr@hey.com>
2023-03-27 12:05:29 +0200

    fix duplicate storybook ids

--- src/Umbraco.Web.UI.Client/src/backoffice/templating/stylesheets/workspace/stylesheet-workspace.stories.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/templating/stylesheets/workspace/stylesheet-workspace.stories.ts
@@ -3,16 +3,14 @@ import './stylesheet-workspace.element';
 import { Meta, Story } from '@storybook/web-components';
 import { html } from 'lit';
 
-import { data } from '../../../../core/mocks/data/member.data';
-
 import type { UmbStylesheetWorkspaceElement } from './stylesheet-workspace.element';
 
 export default {
-	title: 'Workspaces/Member',
-	component: 'umb-member-workspace',
-	id: 'umb-member-workspace',
+	title: 'Workspaces/Stylesheet',
+	component: 'umb-stylesheet-workspace',
+	id: 'umb-stylesheet-workspace',
 } as Meta;
 
 export const AAAOverview: Story<UmbStylesheetWorkspaceElement> = () =>
-	html` <umb-member-workspace id="${data[0].key}"></umb-member-workspace>`;
+	html` <umb-stylesheet-workspace></umb-stylesheet-workspace>`;
 AAAOverview.storyName = 'Overview';
