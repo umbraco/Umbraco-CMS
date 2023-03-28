a4bec805c95b9b4fccbe4faee8dd881f26430250
nikolajlauridsen <nikolajlauridsen@protonmail.ch>
2023-03-28 14:25:59 +0200

    Don't call to array

--- src/Umbraco.Cms.Api.Management/Controllers/Users/DisableUsersController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/Users/DisableUsersController.cs
@@ -20,7 +20,7 @@ public class DisableUsersController : UsersControllerBase
     public async Task<IActionResult> DisableUsers(DisableUserRequestModel model)
     {
         // FIXME: use the actual currently logged in user key
-        UserOperationStatus result = await _userService.DisableAsync(Constants.Security.SuperUserKey, model.UserIds.ToArray());
+        UserOperationStatus result = await _userService.DisableAsync(Constants.Security.SuperUserKey, model.UserIds);
 
         return result is UserOperationStatus.Success
             ? Ok()
