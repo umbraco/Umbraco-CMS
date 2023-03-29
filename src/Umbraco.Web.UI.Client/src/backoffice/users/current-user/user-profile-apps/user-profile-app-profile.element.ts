348d4cd445c2cbe40229d18dbe0502735bc8bda9
Jesper Møller Jensen <26099018+JesmoDev@users.noreply.github.com>
2023-03-30 12:31:47 +1300

    fix merge

--- src/Umbraco.Web.UI.Client/src/backoffice/users/current-user/user-profile-apps/user-profile-app-profile.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/users/current-user/user-profile-apps/user-profile-app-profile.element.ts
@@ -1,11 +1,10 @@
 import { css, html } from 'lit';
 import { UUITextStyles } from '@umbraco-ui/uui-css/lib';
 import { customElement, state } from 'lit/decorators.js';
+import { UmbCurrentUserStore, UMB_CURRENT_USER_STORE_CONTEXT_TOKEN } from '../current-user.store';
 import { UmbLitElement } from '@umbraco-cms/internal/lit-element';
 import type { UserDetails } from '@umbraco-cms/backoffice/models';
-import { UmbModalContext, UMB_MODAL_CONTEXT_TOKEN } from '@umbraco-cms/backoffice/modal';
-import { UmbCurrentUserStore, UMB_CURRENT_USER_STORE_CONTEXT_TOKEN } from '../current-user.store';
-import { UMB_CHANGE_PASSWORD_MODAL_TOKEN } from '../modals/change-password';
+import { UmbModalContext, UMB_CHANGE_PASSWORD_MODAL, UMB_MODAL_CONTEXT_TOKEN } from '@umbraco-cms/backoffice/modal';
 
 @customElement('umb-user-profile-app-profile')
 export class UmbUserProfileAppProfileElement extends UmbLitElement {
@@ -49,7 +48,7 @@ export class UmbUserProfileAppProfileElement extends UmbLitElement {
 	private _changePassword() {
 		if (!this._modalContext) return;
 
-		this._modalContext.open(UMB_CHANGE_PASSWORD_MODAL_TOKEN, {
+		this._modalContext.open(UMB_CHANGE_PASSWORD_MODAL, {
 			requireOldPassword: this._currentUserStore?.isAdmin || false,
 		});
 	}
