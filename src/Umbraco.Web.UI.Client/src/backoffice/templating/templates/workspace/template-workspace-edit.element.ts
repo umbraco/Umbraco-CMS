0394bfe177190c0ea3e49dd255390d4fd0e17c33
Julia Gru <56249914+julczka@users.noreply.github.com>
2023-03-29 11:42:57 +0200

    fix template section display

---
+++ src/Umbraco.Web.UI.Client/src/backoffice/templating/templates/workspace/template-workspace-edit.element.ts
@@ -0,0 +1,118 @@
+import { UUITextStyles } from '@umbraco-ui/uui-css/lib';
+import { css, html } from 'lit';
+import { customElement, query, state } from 'lit/decorators.js';
+import { UUIInputElement } from '@umbraco-ui/uui';
+import { UmbCodeEditorElement } from '../../../shared/components/code-editor/code-editor.element';
+import { UmbTemplateWorkspaceContext } from './template-workspace.context';
+import { UmbLitElement } from '@umbraco-cms/internal/lit-element';
+
+@customElement('umb-template-workspace-edit')
+export class UmbTemplateWorkspaceEditElement extends UmbLitElement {
+	static styles = [
+		UUITextStyles,
+		css`
+			:host {
+				display: block;
+				width: 100%;
+				height: 100%;
+			}
+
+			umb-code-editor {
+				--editor-height: calc(100vh - 300px);
+			}
+
+			uui-box {
+				margin: 1em;
+				--uui-box-default-padding: 0;
+			}
+
+			uui-input {
+				width: 100%;
+				margin: 1em;
+			}
+		`,
+	];
+
+	public load(entityKey: string) {
+		this.#templateWorkspaceContext?.load(entityKey);
+	}
+
+	public create(parentKey: string | null) {
+		this.#isNew = true;
+		this.#templateWorkspaceContext?.createScaffold(parentKey);
+	}
+
+	@state()
+	private _name?: string | null = '';
+
+	@state()
+	private _content?: string | null = '';
+
+	@query('umb-code-editor')
+	private _codeEditor?: UmbCodeEditorElement;
+
+	#templateWorkspaceContext?: UmbTemplateWorkspaceContext;
+	#isNew = false;
+
+	constructor() {
+		super();
+
+		this.consumeContext('umbWorkspaceContext', (workspaceContext: UmbTemplateWorkspaceContext) => {
+			this.#templateWorkspaceContext = workspaceContext;
+			this.observe(this.#templateWorkspaceContext.name, (name) => {
+				this._name = name;
+			});
+
+			this.observe(this.#templateWorkspaceContext.content, (content) => {
+				this._content = content;
+			});
+		});
+	}
+
+	// TODO: temp code for testing create and save
+	#onNameInput(event: Event) {
+		const target = event.target as UUIInputElement;
+		const value = target.value as string;
+		this.#templateWorkspaceContext?.setName(value);
+	}
+
+	//TODO - debounce that
+	#onCodeEditorInput(event: Event) {
+		const target = event.target as UmbCodeEditorElement;
+		const value = target.code as string;
+		this.#templateWorkspaceContext?.setContent(value);
+	}
+
+	#insertCode(event: Event) {
+		const target = event.target as UUIInputElement;
+		const value = target.value as string;
+
+		this._codeEditor?.insert(`My hovercraft is full of eels`);
+	}
+
+	render() {
+		// TODO: add correct UI elements
+		return html`<umb-workspace-layout alias="Umb.Workspace.Template">
+			<uui-input slot="header" .value=${this._name} @input=${this.#onNameInput}></uui-input>
+			<uui-box>
+				<uui-button color="danger" look="primary" slot="header" @click=${this.#insertCode}
+					>Insert "My hovercraft is full of eels"</uui-button
+				>
+
+				<umb-code-editor
+					language="razor"
+					id="content"
+					.code=${this._content ?? ''}
+					@input=${this.#onCodeEditorInput}></umb-code-editor>
+			</uui-box>
+		</umb-workspace-layout>`;
+	}
+}
+
+export default UmbTemplateWorkspaceEditElement;
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'umb-template-workspace-edit': UmbTemplateWorkspaceEditElement;
+	}
+}
--- src/Umbraco.Web.UI.Client/src/backoffice/templating/templates/workspace/template-workspace.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/templating/templates/workspace/template-workspace.element.ts
@@ -1,108 +1,49 @@
-import { UUITextStyles } from '@umbraco-ui/uui-css/lib';
+import { UUITextStyles } from '@umbraco-ui/uui-css';
 import { css, html } from 'lit';
-import { customElement, query, state } from 'lit/decorators.js';
-import { UUIInputElement } from '@umbraco-ui/uui';
-import { UmbCodeEditorElement } from '../../../shared/components/code-editor/code-editor.element';
-import { UmbTemplateWorkspaceContext } from './template-workspace.context';
+import { customElement, state } from 'lit/decorators.js';
+import { UmbRouterSlotInitEvent, IRoute, IRoutingInfo } from '@umbraco-cms/internal/router';
 import { UmbLitElement } from '@umbraco-cms/internal/lit-element';
+
+import './template-workspace-edit.element';
+import { UmbTemplateWorkspaceContext } from './template-workspace.context';
 
 @customElement('umb-template-workspace')
 export class UmbTemplateWorkspaceElement extends UmbLitElement {
-	static styles = [
-		UUITextStyles,
-		css`
-			:host {
-				display: block;
-				width: 100%;
-				height: 100%;
-			}
-
-			umb-code-editor {
-				--editor-height: calc(100vh - 300px);
-			}
-
-			uui-box {
-				margin: 1em;
-				--uui-box-default-padding: 0;
-			}
-
-			uui-input {
-				width: 100%;
-				margin: 1em;
-			}
-		`,
-	];
-
-	public load(entityKey: string) {
-		this.#templateWorkspaceContext.load(entityKey);
-	}
-
-	public create(parentKey: string | null) {
-		this.#isNew = true;
-		this.#templateWorkspaceContext.createScaffold(parentKey);
-	}
-
-	@state()
-	private _name?: string | null = '';
-
-	@state()
-	private _content?: string | null = '';
-
-	@query('umb-code-editor')
-	private _codeEditor?: UmbCodeEditorElement;
+	static styles = [UUITextStyles, css``];
 
 	#templateWorkspaceContext = new UmbTemplateWorkspaceContext(this);
-	#isNew = false;
 
-	async connectedCallback() {
-		super.connectedCallback();
+	#routerPath? = '';
 
-		this.observe(this.#templateWorkspaceContext.name, (name) => {
-			this._name = name;
-		});
+	#element = document.createElement('umb-template-workspace-edit');
+	#key = '';
 
-		this.observe(this.#templateWorkspaceContext.content, (content) => {
-			this._content = content;
-		});
-	}
-
-	// TODO: temp code for testing create and save
-	#onNameInput(event: Event) {
-		const target = event.target as UUIInputElement;
-		const value = target.value as string;
-		this.#templateWorkspaceContext.setName(value);
-	}
-
-	//TODO - debounce that
-	#onCodeEditorInput(event: Event) {
-		const target = event.target as UmbCodeEditorElement;
-		const value = target.code as string;
-		this.#templateWorkspaceContext.setContent(value);
-	}
-
-	#insertCode(event: Event) {
-		const target = event.target as UUIInputElement;
-		const value = target.value as string;
-
-		this._codeEditor?.insert(`My hovercraft is full of eels`);
-	}
+	@state()
+	_routes: IRoute[] = [
+		{
+			path: 'create/:parentKey',
+			component: () => this.#element,
+			setup: async (component: HTMLElement, info: IRoutingInfo) => {
+				const parentKey = info.match.params.parentKey;
+				this.#templateWorkspaceContext.createScaffold(parentKey);
+			},
+		},
+		{
+			path: 'edit/:key',
+			component: () => this.#element,
+			setup: (component: HTMLElement, info: IRoutingInfo) => {
+				const key = info.match.params.key;
+				this.#templateWorkspaceContext.load(key);
+			},
+		},
+	];
 
 	render() {
-		// TODO: add correct UI elements
-		return html`<umb-workspace-layout alias="Umb.Workspace.Template">
-			<uui-input slot="header" .value=${this._name} @input=${this.#onNameInput}></uui-input>
-			<uui-box>
-				<uui-button color="danger" look="primary" slot="header" @click=${this.#insertCode}
-					>Insert "My hovercraft is full of eels"</uui-button
-				>
-
-				<umb-code-editor
-					language="razor"
-					id="content"
-					.code=${this._content ?? ''}
-					@input=${this.#onCodeEditorInput}></umb-code-editor>
-			</uui-box>
-		</umb-workspace-layout>`;
+		return html`<umb-router-slot
+			.routes=${this._routes}
+			@init=${(event: UmbRouterSlotInitEvent) => {
+				this.#routerPath = event.target.absoluteRouterPath;
+			}}></umb-router-slot>`;
 	}
 }
 
