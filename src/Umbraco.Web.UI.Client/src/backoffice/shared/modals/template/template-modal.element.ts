dfbd83c464c255b4a33f4118df1aef3b77086334
Niels Lyngsø <nsl@umbraco.dk>
2023-03-29 16:05:00 +0200

    lint fixes

--- src/Umbraco.Web.UI.Client/src/backoffice/shared/modals/template/template-modal.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/shared/modals/template/template-modal.element.ts
@@ -3,11 +3,11 @@ import { UUITextStyles } from '@umbraco-ui/uui-css/lib';
 import { customElement, query, state } from 'lit/decorators.js';
 import { ifDefined } from 'lit/directives/if-defined.js';
 import { UUIInputEvent } from '@umbraco-ui/uui';
+import { UmbInputEvent } from '@umbraco-cms/backoffice/umb-events/input.event';
+import { TemplateResource, TemplateResponseModel } from '@umbraco-cms/backoffice/backend-api/src';
 import { UmbCodeEditor } from '../../components/code-editor';
 import { UmbTemplateModalData, UmbTemplateModalResult } from '.';
 import { UmbModalBaseElement } from '@umbraco-cms/internal/modal';
-import { UmbInputEvent } from 'libs/umb-events/input.event';
-import { TemplateResource, TemplateResponseModel } from 'libs/backend-api/src';
 import { tryExecuteAndNotify } from '@umbraco-cms/backoffice/resources';
 
 //TODO: make a default tree-picker that can be used across multiple pickers
