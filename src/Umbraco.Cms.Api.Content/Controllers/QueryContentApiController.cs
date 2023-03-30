bc6e65959c997cf6954f729f39d858c538e49f8f
Elitsa <elm@umbraco.dk>
2023-03-30 19:18:06 +0200

    Refactor the QueryContentApiController

--- src/Umbraco.Cms.Api.Content/Controllers/QueryContentApiController.cs
+++ src/Umbraco.Cms.Api.Content/Controllers/QueryContentApiController.cs
@@ -1,55 +1,39 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Core.ContentApi;
 using Umbraco.Cms.Core.Models.ContentApi;
 using Umbraco.Cms.Core.Models.PublishedContent;
-using Umbraco.Cms.Core.PublishedCache;
-using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Api.Content.Controllers;
 
 public class QueryContentApiController : ContentApiControllerBase
 {
     private readonly IApiQueryService _apiQueryService;
-    private readonly IRequestRoutingService _requestRoutingService;
 
     public QueryContentApiController(
-        IPublishedSnapshotAccessor publishedSnapshotAccessor,
-        IApiContentBuilder apiContentBuilder,
-        IApiQueryService apiQueryService,
-        IRequestRoutingService requestRoutingService)
-        : base(publishedSnapshotAccessor, apiContentBuilder)
-    {
-        _apiQueryService = apiQueryService;
-        _requestRoutingService = requestRoutingService;
-    }
+        IApiPublishedContentCache apiPublishedContentCache,
+        IApiContentResponseBuilder apiContentResponseBuilderBuilder,
+        IApiQueryService apiQueryService)
+        : base(apiPublishedContentCache, apiContentResponseBuilderBuilder)
+        => _apiQueryService = apiQueryService;
 
     /// <summary>
     ///     Gets content item(s) from query.
     /// </summary>
+    /// <param name="fetch">Optional fetch query parameter value.</param>
+    /// <param name="filter">Optional filter query parameters values.</param>
+    /// <param name="sort">Optional sort query parameters values.</param>
+    /// <returns>The content item(s) or empty collection.</returns>
     [HttpGet]
     [MapToApiVersion("1.0")]
-    [ProducesResponseType(typeof(IApiContent), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(IEnumerable<IApiContentResponse>), StatusCodes.Status200OK)]
     [ProducesResponseType(StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> Query(string? fetch, [FromQuery] string[] filter, [FromQuery] string[] sort)
     {
-        IPublishedContentCache? contentCache = GetContentCache();
-        if (contentCache is null)
-        {
-            return BadRequest(ContentCacheNotFoundProblemDetails());
-        }
+        IEnumerable<Guid> ids = _apiQueryService.ExecuteQuery(fetch, filter, sort);
+        IEnumerable<IPublishedContent> contentItems = ApiPublishedContentCache.GetByIds(ids);
+        IEnumerable<IApiContentResponse> results = contentItems.Select(ApiContentResponseBuilder.Build);
 
-        IEnumerable<Guid> ids = Enumerable.Empty<Guid>();
-        ids = _apiQueryService.ExecuteQuery(fetch, filter, sort);
-
-        IEnumerable<IPublishedContent> contentItems = ids.Select(contentCache.GetById).WhereNotNull();
-
-        IEnumerable<IApiContent> results = contentItems.Select(ApiContentBuilder.Build);
-
-        return Ok(results);
+        return await Task.FromResult(Ok(results));
     }
 }
