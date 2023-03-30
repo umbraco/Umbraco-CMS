4e95dbc6201dbab146324d0fd477a86ededecd2a
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2023-03-30 20:03:48 +0200

    Add DocumentItemControllerBase.cs

---
+++ src/Umbraco.Cms.Api.Management/Controllers/Document/Item/ItemDocumentItemController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Management.ViewModels.DataType.Item;
+
+namespace Umbraco.Cms.Api.Management.Controllers.Document.Item;
+
+public class ItemDocumentItemController : DocumentItemControllerBase
+{
+    [HttpGet]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(typeof(IEnumerable<DataTypeItemResponseModel>), StatusCodes.Status200OK)]
+    public async Task<ActionResult> Item([FromQuery(Name = "key")] Guid[] keys)
+    {
+        return Ok();
+    }
+}
