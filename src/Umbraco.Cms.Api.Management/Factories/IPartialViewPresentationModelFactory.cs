d0e259b282567229f61343d73803eda84a6a95d1
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2023-03-31 13:06:27 +0200

    Implement partial view viewport

---
+++ src/Umbraco.Cms.Api.Management/Factories/IPartialViewPresentationModelFactory.cs
@@ -0,0 +1,8 @@
+﻿using Umbraco.Cms.Api.Management.ViewModels.PartialView.Item;
+
+namespace Umbraco.Cms.Api.Management.Factories;
+
+public interface IPartialViewPresentationModelFactory
+{
+    IEnumerable<PartialViewItemResponseModel> Create(IEnumerable<string> path);
+}
---
+++ src/Umbraco.Cms.Api.Management/Factories/PartialViewPresentationModelFactory.cs
@@ -0,0 +1,30 @@
+﻿using Umbraco.Cms.Api.Management.ViewModels.PartialView.Item;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.IO;
+using Umbraco.Extensions;
+
+namespace Umbraco.Cms.Api.Management.Factories;
+
+public class PartialViewPresentationModelFactory : IPartialViewPresentationModelFactory
+{
+    private readonly IFileSystem _fileSystem;
+
+    public PartialViewPresentationModelFactory(FileSystems fileSystems) => _fileSystem = fileSystems.PartialViewsFileSystem ?? throw new ArgumentException("Missing partial views file system", nameof(fileSystems));
+
+    public IEnumerable<PartialViewItemResponseModel> Create(IEnumerable<string> paths) => paths.Select(path => CreateResponseModel(path)).WhereNotNull();
+
+    private PartialViewItemResponseModel? CreateResponseModel(string path)
+    {
+        if (!_fileSystem.FileExists(path))
+        {
+            return null;
+        }
+
+        return new PartialViewItemResponseModel
+        {
+            Path = path,
+            Name = _fileSystem.GetFileName(path),
+            Icon = Constants.Icons.PartialView,
+        };
+    }
+}
