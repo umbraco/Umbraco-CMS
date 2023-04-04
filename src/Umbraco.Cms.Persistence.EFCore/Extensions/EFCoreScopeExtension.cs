a3b9d9237e5173198fb6f4a0ac482d1bc525045a
Zeegaan <nge@umbraco.dk>
2023-04-04 14:52:45 +0200

    Create migrate database async method
    Signed-off-by: Zeegaan <nge@umbraco.dk>

---
+++ src/Umbraco.Cms.Persistence.EFCore/Extensions/EFCoreScopeExtension.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
+using Umbraco.Cms.Persistence.EFCore.Entities;
+using Umbraco.Cms.Persistence.EFCore.Scoping;
+
+namespace Umbraco.Extensions;
+
+public static class EFCoreScopeExtension
+{
+    public static async Task MigrateDatabaseAsync(this IEfCoreScope<UmbracoEFContext> scope, string targetMigration) =>
+        await scope.ExecuteWithContextAsync<Task>(async db =>
+        {
+            await db.GetService<IMigrator>().MigrateAsync(targetMigration);
+        });
+}
