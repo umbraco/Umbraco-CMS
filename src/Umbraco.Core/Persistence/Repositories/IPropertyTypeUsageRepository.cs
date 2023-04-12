7fd9909d71c4a6e841f05ba2a76991352bd10289
Bjarke Berg <mail@bergmania.dk>
2023-04-12 11:05:24 +0200

    Added is-used endpoints

--- src/Umbraco.Core/Persistence/Repositories/IPropertyTypeUsageRepository.cs
+++ src/Umbraco.Core/Persistence/Repositories/IPropertyTypeUsageRepository.cs
@@ -2,5 +2,7 @@ namespace Umbraco.Cms.Core.Persistence.Repositories;
 
 public interface IPropertyTypeUsageRepository
 {
+    [Obsolete("Please use HasSavedPropertyValuesAsync. Scheduled for removable in Umbraco 15.")]
     bool HasSavedPropertyValues(string propertyTypeAlias);
+    Task<bool> HasSavedPropertyValuesAsync(Guid contentTypeKey, string propertyAlias);
 }
--- src/Umbraco.Infrastructure/Persistence/Repositories/Implement/PropertyTypeUsageRepository.cs
+++ src/Umbraco.Infrastructure/Persistence/Repositories/Implement/PropertyTypeUsageRepository.cs
@@ -16,6 +16,7 @@ internal class PropertyTypeUsageRepository : IPropertyTypeUsageRepository
         _scopeAccessor = scopeAccessor;
     }
 
+    [Obsolete("Please use HasSavedPropertyValuesAsync. Scheduled for removable in Umbraco 15.")]
     public bool HasSavedPropertyValues(string propertyTypeAlias)
     {
         IUmbracoDatabase? database = _scopeAccessor.AmbientScope?.Database;
@@ -31,6 +32,31 @@ internal class PropertyTypeUsageRepository : IPropertyTypeUsageRepository
             .InnerJoin<PropertyDataDto>("p")
             .On<PropertyDataDto, PropertyTypeDto>((left, right) => left.PropertyTypeId == right.Id, "p", "m")
             .Where<PropertyTypeDto>(m => m.Alias == propertyTypeAlias, "m");
+
+        Sql<ISqlContext> hasValuesQuery = database.SqlContext.Sql()
+            .SelectAnyIfExists(selectQuery);
+
+        return database.ExecuteScalar<bool>(hasValuesQuery);
+    }
+
+    public async Task<bool> HasSavedPropertyValuesAsync(Guid contentTypeKey, string propertyAlias)
+    {
+        IUmbracoDatabase? database = _scopeAccessor.AmbientScope?.Database;
+
+        if (database is null)
+        {
+            throw new InvalidOperationException("A scope is required to query the database");
+        }
+
+        Sql<ISqlContext> selectQuery = database.SqlContext.Sql()
+            .SelectAll()
+            .From<PropertyTypeDto>("pt")
+            .InnerJoin<PropertyDataDto>("pd")
+            .On<PropertyDataDto, PropertyTypeDto>((pd, pt) => pd.PropertyTypeId == pt.Id, "pd", "pt")
+            .InnerJoin<NodeDto>("n")
+            .On<PropertyTypeDto, NodeDto>((pt, n) => pt.ContentTypeId == n.NodeId, "pt", "n")
+            .Where<PropertyTypeDto>(pt => pt.Alias == propertyAlias, "pt")
+            .Where<NodeDto>(n => n.UniqueId == contentTypeKey, "n");
 
         Sql<ISqlContext> hasValuesQuery = database.SqlContext.Sql()
             .SelectAnyIfExists(selectQuery);
