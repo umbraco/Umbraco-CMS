bef326ec6995698c2a46246c267af1327b82be7a
Mads Rasmussen <madsr@hey.com>
2023-04-12 17:05:28 +0200

    add data type picker token

---
+++ src/Umbraco.Web.UI.Client/libs/modal/token/data-type-picker-modal.token.ts
@@ -0,0 +1,17 @@
+import { UmbModalToken } from '@umbraco-cms/backoffice/modal';
+
+export interface UmbDataTypePickerModalData {
+	selection?: Array<string>;
+}
+
+export interface UmbDataTypePickerModalResult {
+	selection: Array<string>;
+}
+
+export const UMB_DATA_TYPE_PICKER_MODAL = new UmbModalToken<UmbDataTypePickerModalData, UmbDataTypePickerModalResult>(
+	'Umb.Modal.DataTypePicker',
+	{
+		type: 'sidebar',
+		size: 'small',
+	}
+);
