eab9676607dd2d28773d539e963cecb3467baa0d
Zeegaan <nge@umbraco.dk>
2023-04-12 10:10:42 +0200

    Add routing to static file item controller

--- src/Umbraco.Cms.Api.Management/Controllers/StaticFile/Item/StaticFileItemControllerBase.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/StaticFile/Item/StaticFileItemControllerBase.cs
@@ -1,5 +1,12 @@
-﻿namespace Umbraco.Cms.Api.Management.Controllers.StaticFile.Item;
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Management.Routing;
 
+namespace Umbraco.Cms.Api.Management.Controllers.StaticFile.Item;
+
+[ApiVersion("1.0")]
+[ApiController]
+[VersionedApiBackOfficeRoute("static-file")]
+[ApiExplorerSettings(GroupName = "Static File")]
 public class StaticFileItemControllerBase : ManagementApiControllerBase
 {
 }
