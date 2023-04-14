bc7174ad04a70083f0b1d76f259da5a6b92bb8ce
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2023-04-14 11:37:31 +0200

    Add test DbContext classes

---
+++ tests/Umbraco.Tests.Integration/Umbraco.Persistence.EFCore/DbContext/TestUmbracoDbContext.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Umbraco.Cms.Tests.Integration.Umbraco.Persistence.EFCore.DbContext;
+
+public class TestUmbracoDbContext : Microsoft.EntityFrameworkCore.DbContext
+{
+    public TestUmbracoDbContext(DbContextOptions<TestUmbracoDbContext> options)
+        : base(options)
+    {
+    }
+
+    internal virtual DbSet<UmbracoLock> UmbracoLocks { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<UmbracoLock>(entity =>
+        {
+            entity.ToTable("umbracoLock");
+
+            entity.Property(e => e.Id)
+                .ValueGeneratedNever()
+                .HasColumnName("id");
+
+            entity.Property(e => e.Name)
+                .HasMaxLength(64)
+                .HasColumnName("name");
+
+            entity.Property(e => e.Value).HasColumnName("value");
+        });
+    }
+}
