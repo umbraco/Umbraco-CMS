b315335432d4708a1ae8724a63475c09878d6420
Ambert van Unen <ambertvu@gmail.com>
2023-04-17 14:05:20 +0200

    Update IDictionaryTranslation.cs

--- src/Umbraco.Core/Models/IDictionaryTranslation.cs
+++ src/Umbraco.Core/Models/IDictionaryTranslation.cs
@@ -8,11 +8,11 @@ public interface IDictionaryTranslation : IEntity, IRememberBeingDirty
     /// <summary>
     ///     Gets or sets the <see cref="Language" /> for the translation
     /// </summary>
-    [Obsolete("This will be removed in V13. From V13 onwards you should get languages by ISO code from ILanguageService.")]
+    [Obsolete("This will be removed in V13. From V15 onwards you should get languages by ISO code from ILanguageService.")]
     [DataMember]
     ILanguage? Language { get; set; }
 
-    [Obsolete("This will be replaced by language ISO code in V13.")]
+    [Obsolete("This will be replaced by language ISO code in V15.")]
     int LanguageId { get; }
 
     /// <summary>
