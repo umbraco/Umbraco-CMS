431cef5c44c2afebb2d7b5c3f1495d0b898bd0f4
Mads Rasmussen <madsr@hey.com>
2023-04-18 13:31:07 +0200

    data and error are optional

--- src/Umbraco.Web.UI.Client/libs/repository/item-repository.interface.ts
+++ src/Umbraco.Web.UI.Client/libs/repository/item-repository.interface.ts
@@ -3,8 +3,8 @@ import { ItemResponseModelBaseModel, ProblemDetailsModel } from '@umbraco-cms/ba
 
 export interface UmbItemRepository<ItemType extends ItemResponseModelBaseModel> {
 	requestItems: (uniques: string[]) => Promise<{
-		data: Array<ItemType> | undefined;
-		error: ProblemDetailsModel | undefined;
+		data?: Array<ItemType> | undefined;
+		error?: ProblemDetailsModel | undefined;
 		asObservable?: () => Observable<Array<ItemType>>;
 	}>;
 	items: (uniques: string[]) => Promise<Observable<Array<ItemType>>>;
