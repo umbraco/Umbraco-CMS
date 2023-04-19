6fd11c050574b3d47c22f71802b0a3ad51e0d20e
Zeegaan <nge@umbraco.dk>
2023-04-19 14:28:10 +0200

    Reset cache when migrating

---
+++ src/Umbraco.Infrastructure/Migrations/Upgrade/V_12_0_0/ResetCache.cs
@@ -0,0 +1,10 @@
+﻿namespace Umbraco.Cms.Infrastructure.Migrations.Upgrade.V_12_0_0;
+
+public class ResetCache : MigrationBase
+{
+    public ResetCache(IMigrationContext context) : base(context)
+    {
+    }
+
+    protected override void Migrate() => RebuildCache = true;
+}
