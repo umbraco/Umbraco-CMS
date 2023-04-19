49028e76d35f724e2c927f5ddcceddb961976c04
Zeegaan <nge@umbraco.dk>
2023-04-19 10:13:09 +0200

    Fix namespace after merge

--- src/Umbraco.Cms.Api.Management/ViewModels/User/Item/UserItemResponseModel.cs
+++ src/Umbraco.Cms.Api.Management/ViewModels/User/Item/UserItemResponseModel.cs
@@ -1,6 +1,6 @@
 ﻿using Umbraco.Cms.Api.Management.ViewModels.Item;
 
-namespace Umbraco.Cms.Api.Management.ViewModels.Users.Item;
+namespace Umbraco.Cms.Api.Management.ViewModels.User.Item;
 
 public class UserItemResponseModel : ItemResponseModelBase
 {
