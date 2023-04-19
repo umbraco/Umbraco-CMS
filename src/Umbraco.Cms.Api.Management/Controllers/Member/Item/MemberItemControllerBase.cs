cb682e2f3eee435e2653f49316e75c0fd9d91add
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2023-04-19 09:43:01 +0200

    New backoffice: User and member item endpoints (#14079)
    * Implement ByKeys in MemberService
    
    * Implement member item endpoint
    
    * Add routing to static file item controller
    
    * Implement user item endpoint
    
    * Implement user group item endpoint
    
    * return proper response model
    
    * update OpenApi.json
    
    * Add CompatibilitySuppressions
    
    * Cleanup
    
    * Update OpenApi
    
    ---------
    
    Co-authored-by: Zeegaan <nge@umbraco.dk>
    Co-authored-by: Elitsa <elm@umbraco.dk>

---
+++ src/Umbraco.Cms.Api.Management/Controllers/Member/Item/MemberItemControllerBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Management.Routing;
+using Umbraco.Cms.Core;
+
+namespace Umbraco.Cms.Api.Management.Controllers.Member.Item;
+
+[ApiVersion("1.0")]
+[ApiController]
+[VersionedApiBackOfficeRoute($"{Constants.UdiEntityType.Member}")]
+[ApiExplorerSettings(GroupName = "Member")]
+public class MemberItemControllerBase : ManagementApiControllerBase
+{
+}
