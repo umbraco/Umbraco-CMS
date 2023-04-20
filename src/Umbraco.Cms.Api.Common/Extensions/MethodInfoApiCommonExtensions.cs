24862f9d41d421b483fd8d2336362a2692281897
Zeegaan <nge@umbraco.dk>
2023-04-20 10:30:54 +0200

    use strong types instead of var

--- src/Umbraco.Cms.Api.Common/Extensions/MethodInfoApiCommonExtensions.cs
+++ src/Umbraco.Cms.Api.Common/Extensions/MethodInfoApiCommonExtensions.cs
@@ -10,16 +10,18 @@ public static class MethodInfoApiCommonExtensions
 
     public static string? GetMapToApiVersionAttributeValue(this MethodInfo methodInfo)
     {
-        var mapToApis = methodInfo.GetCustomAttributes(typeof(MapToApiVersionAttribute), inherit: true).Cast<MapToApiVersionAttribute>().ToArray();
+        MapToApiVersionAttribute[] mapToApis = methodInfo.GetCustomAttributes(typeof(MapToApiVersionAttribute), inherit: true).Cast<MapToApiVersionAttribute>().ToArray();
 
         return string.Join("|", mapToApis.SelectMany(x=>x.Versions));
     }
+
     public static string? GetMapToApiAttributeValue(this MethodInfo methodInfo)
     {
-        var mapToApis = (methodInfo.DeclaringType?.GetCustomAttributes(typeof(MapToApiAttribute), inherit: true) ?? Array.Empty<object>()).Cast<MapToApiAttribute>().ToArray();
+        MapToApiAttribute[] mapToApis = (methodInfo.DeclaringType?.GetCustomAttributes(typeof(MapToApiAttribute), inherit: true) ?? Array.Empty<object>()).Cast<MapToApiAttribute>().ToArray();
 
         return mapToApis.SingleOrDefault()?.ApiName;
     }
+
     public static bool HasMapToApiAttribute(this MethodInfo methodInfo, string apiName)
     {
         var value = methodInfo.GetMapToApiAttributeValue();
