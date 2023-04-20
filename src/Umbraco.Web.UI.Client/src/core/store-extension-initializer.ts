7243365e4452a846b745d32e303f468eebbf1002
Mads Rasmussen <madsr@hey.com>
2023-04-20 15:15:53 +0200

    manual update after merge

--- src/Umbraco.Web.UI.Client/src/core/store-extension-initializer.ts
+++ src/Umbraco.Web.UI.Client/src/core/store-extension-initializer.ts
@@ -9,15 +9,19 @@ export class UmbStoreExtensionInitializer {
 	constructor(host: UmbControllerHostElement) {
 		this.host = host;
 
-		new UmbObserverController(this.host, umbExtensionsRegistry.extensionsOfTypes(['store', 'treeStore']), (stores) => {
-			if (!stores) return;
+		new UmbObserverController(
+			this.host,
+			umbExtensionsRegistry.extensionsOfTypes(['store', 'treeStore', 'itemStore']),
+			(stores) => {
+				if (!stores) return;
 
-			stores.forEach((store) => {
-				if (this.#storeMap.has(store.alias)) return;
+				stores.forEach((store) => {
+					if (this.#storeMap.has(store.alias)) return;
 
-				// Instantiate and provide stores. Stores are self providing when the class is instantiated.
-				this.#storeMap.set(store.alias, createExtensionClass(store, [this.host]));
-			});
-		});
+					// Instantiate and provide stores. Stores are self providing when the class is instantiated.
+					this.#storeMap.set(store.alias, createExtensionClass(store, [this.host]));
+				});
+			}
+		);
 	}
 }
