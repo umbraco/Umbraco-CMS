b09b1fef9cb48c3840fb8a1a7804f560c1da057b
Zeegaan <nge@umbraco.dk>
2023-04-20 11:32:48 +0200

    Manually delete cache file

--- src/Umbraco.Infrastructure/Migrations/Upgrade/V_12_0_0/ResetCache.cs
+++ src/Umbraco.Infrastructure/Migrations/Upgrade/V_12_0_0/ResetCache.cs
@@ -1,10 +1,20 @@
-﻿namespace Umbraco.Cms.Infrastructure.Migrations.Upgrade.V_12_0_0;
+﻿using Microsoft.Extensions.Hosting;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Extensions;
+
+namespace Umbraco.Cms.Infrastructure.Migrations.Upgrade.V_12_0_0;
 
 public class ResetCache : MigrationBase
 {
-    public ResetCache(IMigrationContext context) : base(context)
-    {
-    }
+    private readonly IHostEnvironment _hostEnvironment;
 
-    protected override void Migrate() => RebuildCache = true;
+    public ResetCache(IMigrationContext context, IHostEnvironment hostEnvironment)
+        : base(context) => _hostEnvironment = hostEnvironment;
+
+    protected override void Migrate()
+    {
+        RebuildCache = true;
+        var distCacheFolderAbsolutePath = _hostEnvironment.MapPathContentRoot(Constants.SystemDirectories.TempFileUploads + "/DistCache");
+        File.Delete(distCacheFolderAbsolutePath);
+    }
 }
