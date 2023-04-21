c952a0546a0c9b6f238fcd00449acf6058153702
Niels Lyngsø <nsl@umbraco.dk>
2023-04-21 09:16:40 +0200

    no need to define type here

--- src/Umbraco.Web.UI.Client/src/backoffice/templating/index.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/templating/index.ts
@@ -2,11 +2,10 @@ import { manifests as menuManifests } from './menu.manifests';
 import { manifests as templateManifests } from './templates/manifests';
 import { manifests as stylesheetManifests } from './stylesheets/manifests';
 import type { UmbEntrypointOnInit } from '@umbraco-cms/backoffice/extensions-api';
-import type { ManifestTypes } from '@umbraco-cms/backoffice/extensions-registry';
 
 import './components';
 
-export const manifests: Array<ManifestTypes> = [...menuManifests, ...templateManifests, ...stylesheetManifests];
+export const manifests = [...menuManifests, ...templateManifests, ...stylesheetManifests];
 
 export const onInit: UmbEntrypointOnInit = (_host, extensionRegistry) => {
 	extensionRegistry.registerMany(manifests);
