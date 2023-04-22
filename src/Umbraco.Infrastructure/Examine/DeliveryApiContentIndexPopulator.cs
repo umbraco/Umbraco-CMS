40bcafede758a5af47119c92674d2f4e0744ee51
kjac <kja@umbraco.dk>
2023-04-22 08:37:38 +0200

    Fix edge case for published children of unpublished parents

--- src/Umbraco.Infrastructure/Examine/DeliveryApiContentIndexPopulator.cs
+++ src/Umbraco.Infrastructure/Examine/DeliveryApiContentIndexPopulator.cs
@@ -1,19 +1,23 @@
 using Examine;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Persistence.Querying;
 using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Infrastructure.Persistence;
 
 namespace Umbraco.Cms.Infrastructure.Examine;
 
 public class DeliveryApiContentIndexPopulator : IndexPopulator
 {
     private readonly IContentService _contentService;
+    private readonly IUmbracoDatabaseFactory _umbracoDatabaseFactory;
     private readonly IDeliveryApiContentIndexValueSetBuilder _deliveryContentIndexValueSetBuilder;
 
-    public DeliveryApiContentIndexPopulator(IContentService contentService, IDeliveryApiContentIndexValueSetBuilder deliveryContentIndexValueSetBuilder)
+    public DeliveryApiContentIndexPopulator(IContentService contentService, IDeliveryApiContentIndexValueSetBuilder deliveryContentIndexValueSetBuilder, IUmbracoDatabaseFactory umbracoDatabaseFactory)
     {
         _contentService = contentService;
         _deliveryContentIndexValueSetBuilder = deliveryContentIndexValueSetBuilder;
+        _umbracoDatabaseFactory = umbracoDatabaseFactory;
         RegisterIndex(Constants.UmbracoIndexes.DeliveryApiContentIndexName);
     }
 
@@ -26,13 +30,27 @@ public class DeliveryApiContentIndexPopulator : IndexPopulator
 
         const int pageSize = 10000;
         var pageIndex = 0;
+        var publishedContentIds = new HashSet<int>();
 
-        IContent[] content;
+        IContent[] descendants;
+        IQuery<IContent> publishedQuery = _umbracoDatabaseFactory.SqlContext.Query<IContent>().Where(x => x.Published);
         do
         {
-            content = _contentService.GetPagedDescendants(Constants.System.Root, pageIndex, pageSize, out _).ToArray();
+            descendants = _contentService.GetPagedDescendants(Constants.System.Root, pageIndex, pageSize, out _, publishedQuery, Ordering.By("Path")).ToArray();
 
-            ValueSet[] valueSets = _deliveryContentIndexValueSetBuilder.GetValueSets(content).ToArray();
+            // children of unpublished content can still be published; we need to filter them out, as they're not supposed to go into the index.
+            // as we're querying published content and ordering by path, we can construct a list of "allowed" published content IDs like this.
+            foreach (IContent content in descendants)
+            {
+                if (content.Level == 1 || publishedContentIds.Contains(content.ParentId))
+                {
+                    publishedContentIds.Add(content.Id);
+                }
+            }
+
+            // now we can utilize the list of "allowed" published content to filter out the ones we don't want in the index.
+            IContent[] allowedDescendants = descendants.Where(content => publishedContentIds.Contains(content.Id)).ToArray();
+            ValueSet[] valueSets = _deliveryContentIndexValueSetBuilder.GetValueSets(allowedDescendants).ToArray();
 
             // ReSharper disable once PossibleMultipleEnumeration
             foreach (IIndex index in indexes)
@@ -42,6 +60,6 @@ public class DeliveryApiContentIndexPopulator : IndexPopulator
 
             pageIndex++;
         }
-        while (content.Length == pageSize);
+        while (descendants.Length == pageSize);
     }
 }
