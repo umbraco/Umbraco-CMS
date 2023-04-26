160d7aeee4b25877e2869fab386644547302161f
Mads Rasmussen <madsr@hey.com>
2023-04-26 21:21:19 +0200

    remove version numbers

--- src/Umbraco.Web.UI.Client/src/backoffice/members/umbraco-package.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/members/umbraco-package.ts
@@ -1,5 +1,4 @@
 export const name = 'Umbraco.Core.MemberManagement';
-export const version = '0.0.1';
 export const extensions = [
 	{
 		name: 'Member Management Entry Point',
--- src/Umbraco.Web.UI.Client/src/backoffice/umbraco-news/umbraco-package.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/umbraco-news/umbraco-package.ts
@@ -1,5 +1,4 @@
 export const name = 'Umbraco.Core.UmbracoNews';
-export const version = '0.0.1';
 export const extensions = [
 	{
 		name: 'Umbraco News Entry Point',
