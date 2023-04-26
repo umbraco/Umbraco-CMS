20d73ac22b8702184f994dca5b1b25222dabe8d0
Niels Lyngsø <nsl@umbraco.dk>
2023-04-26 14:52:24 +0200

    split interfaces

--- src/Umbraco.Web.UI.Client/libs/extensions-registry/interfaces/index.ts
+++ src/Umbraco.Web.UI.Client/libs/extensions-registry/interfaces/index.ts
@@ -1,6 +1,2 @@
-import { DataTypePropertyPresentationModel } from '@umbraco-cms/backoffice/backend-api';
-
-export interface UmbPropertyEditorElement extends HTMLElement {
-	value: unknown;
-	config: DataTypePropertyPresentationModel[];
-}
+export * from './property-editor-ui-element.interface';
+export * from './tree-item-element.interface';
