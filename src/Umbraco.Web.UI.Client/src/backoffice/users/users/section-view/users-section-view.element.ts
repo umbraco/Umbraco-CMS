252696d2a778f01881c201dd021f16052c9eb34c
Mads Rasmussen <madsr@hey.com>
2023-05-02 11:39:10 +0200

    remove unused code

--- src/Umbraco.Web.UI.Client/src/backoffice/users/users/section-view/users-section-view.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/users/users/section-view/users-section-view.element.ts
@@ -25,40 +25,6 @@ export class UmbSectionViewUsersElement extends UmbLitElement {
 		},
 	];
 
-	// // TODO: This must be turned into context api: Maybe its a Collection View (SectionView Collection View)?
-	// private _userStore?: UmbUserStore;
-
-	// #users = new DeepState(<Array<UserDetails>>[]);
-	// public readonly users = this.#users.asObservable();
-
-	// #search = new DeepState('');
-	// public readonly search = this.#search.asObservable();
-
-	// constructor() {
-	// 	super();
-
-	// 	this.consumeContext<UmbUserStore>(UMB_USER_STORE_CONTEXT_TOKEN, (_instance) => {
-	// 		this._userStore = _instance;
-	// 		this._observeUsers();
-	// 	});
-	// }
-
-	// private _observeUsers() {
-	// 	if (!this._userStore) return;
-
-	// 	if (this.#search.getValue()) {
-	// 		this.observe(this._userStore.getByName(this.#search.getValue()), (users) => this.#users.next(users));
-	// 	} else {
-	// 		this.observe(this._userStore.getAll(), (users) => this.#users.next(users));
-	// 	}
-	// }
-
-	// public setSearch(value?: string) {
-	// 	this.#search.next(value || '');
-	// 	this._observeUsers();
-	// 	this.requestUpdate('search');
-	// }
-
 	render() {
 		return html`<umb-router-slot id="router-slot" .routes=${this.#routes}></umb-router-slot>`;
 	}
