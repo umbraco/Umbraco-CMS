7c84f4044ec10b3c5112148aba8194546647afb3
Elitsa <elm@umbraco.dk>
2023-05-03 09:57:12 +0200

    Handle unhappy paths for the multiple items endpoint

--- src/Umbraco.Core/DeliveryApi/NoopApiContentQueryService.cs
+++ src/Umbraco.Core/DeliveryApi/NoopApiContentQueryService.cs
@@ -1,3 +1,4 @@
+using Umbraco.Cms.Core.Services.OperationStatus;
 using Umbraco.New.Cms.Core.Models;
 
 namespace Umbraco.Cms.Core.DeliveryApi;
@@ -5,6 +6,6 @@ namespace Umbraco.Cms.Core.DeliveryApi;
 public sealed class NoopApiContentQueryService : IApiContentQueryService
 {
     /// <inheritdoc />
-    public PagedModel<Guid> ExecuteQuery(string? fetch, IEnumerable<string> filters, IEnumerable<string> sorts, int skip, int take)
-        => new();
+    public Attempt<PagedModel<Guid>, ApiContentQueryOperationStatus> ExecuteQuery(string? fetch, IEnumerable<string> filters, IEnumerable<string> sorts, int skip, int take)
+        => Attempt.SucceedWithStatus(ApiContentQueryOperationStatus.Success, new PagedModel<Guid>());
 }
