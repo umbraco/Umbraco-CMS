39e2b7cae448d8c57d8bef093a0893f27e9ecc1c
Bjarke Berg <mail@bergmania.dk>
2023-05-03 14:35:00 +0200

    Remove actual files

--- src/Umbraco.Cms.Api.Management/ViewModels/Dictionary/DictionaryItemsImportViewModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Umbraco.Cms.Api.Management.ViewModels.Dictionary;
-
-public class DictionaryItemsImportViewModel
-{
-    public string? Name { get; set; }
-
-    public int Level { get; set; }
-}
