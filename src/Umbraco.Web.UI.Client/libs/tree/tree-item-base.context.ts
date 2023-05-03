56fee66c961b24a1cdcdebbf6ca57d593c9092ea
Mads Rasmussen <madsr@hey.com>
2023-05-03 16:01:01 +0200

    update import

--- src/Umbraco.Web.UI.Client/libs/tree/tree-item-base.context.ts
+++ src/Umbraco.Web.UI.Client/libs/tree/tree-item-base.context.ts
@@ -1,5 +1,5 @@
 import { map } from 'rxjs';
-import { UmbTreeItemContext } from '../tree-item.context.interface';
+import { UmbTreeItemContext } from './tree-item.context.interface';
 import { UMB_SECTION_CONTEXT_TOKEN, UMB_SECTION_SIDEBAR_CONTEXT_TOKEN } from '@umbraco-cms/backoffice/section';
 import type { UmbSectionContext, UmbSectionSidebarContext } from '@umbraco-cms/backoffice/section';
 import { UmbTreeContextBase } from '@umbraco-cms/backoffice/tree';
