601a623396f2bffb2f045fb37ba837a4feffaf9f
Lone Iversen <108085781+loivsen@users.noreply.github.com>
2023-05-03 14:02:14 +0200

    remove type from label

--- src/Umbraco.Web.UI.Client/src/backoffice/core/property-editors/uis/label/property-editor-ui-label.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/core/property-editors/uis/label/property-editor-ui-label.element.ts
@@ -3,9 +3,6 @@ import { customElement, property, state } from 'lit/decorators.js';
 import { UUITextStyles } from '@umbraco-ui/uui-css/lib';
 import { UmbPropertyEditorExtensionElement } from '@umbraco-cms/backoffice/extensions-registry';
 import { UmbLitElement } from '@umbraco-cms/internal/lit-element';
-import type { DataTypePropertyPresentationModel } from '@umbraco-cms/backoffice/backend-api';
-
-type umbracoDataValueType = 'STRING' | 'DECIMAL' | 'DATE/TIME' | 'TIME' | 'INTEGER' | 'BIG INTEGER' | 'LONG STRING';
 
 /**
  * @element umb-property-editor-ui-label
@@ -18,14 +15,8 @@ export class UmbPropertyEditorUILabelElement extends UmbLitElement implements Um
 	@property()
 	description = '';
 
-	@state()
-	private _dvt?: umbracoDataValueType;
-
 	@property({ type: Array, attribute: false })
-	public set config(config: Array<DataTypePropertyPresentationModel>) {
-		const dvt = config.find((x) => x.alias === 'umbracoDataValueType');
-		if (dvt) this._dvt = dvt.value as umbracoDataValueType;
-	}
+	config = [];
 
 	render() {
 		return html`${this.value}`;
