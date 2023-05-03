5092b9fdb77f993278586ccab03b4cc6d4a95d61
Bjarke Berg <mail@bergmania.dk>
2023-05-03 08:41:07 +0200

    Use timespan for timeout

--- src/Umbraco.Cms.Persistence.EFCore/Extensions/DbContextExtensions.cs
+++ src/Umbraco.Cms.Persistence.EFCore/Extensions/DbContextExtensions.cs
@@ -9,18 +9,21 @@ namespace Umbraco.Extensions;
 public static class DbContextExtensions
 {
     /// <summary>
-    /// Executes a raw SQL query and returns the result
+    /// Executes a raw SQL query and returns the result.
     /// </summary>
-    /// <param name="database">The database</param>
-    /// <param name="sql">The sql query</param>
-    /// <param name="parameters">The list of db paramaters</param>
-    /// <param name="commandType">The command type</param>
-    /// <param name="commandTimeOutInSeconds">The amount of seconds to wait before the command times out</param>
-    /// <typeparam name="T">the type to return</typeparam>
-    /// <returns>Returns an object of the given type T</returns>
-    public static async Task<T?> ExecuteScalarAsync<T>(this DatabaseFacade database, string sql, List<DbParameter>? parameters = null, CommandType commandType = CommandType.Text, int? commandTimeOutInSeconds = null)
+    /// <param name="database">The database.</param>
+    /// <param name="sql">The sql query.</param>
+    /// <param name="parameters">The list of db parameters.</param>
+    /// <param name="commandType">The command type.</param>
+    /// <param name="commandTimeOut">The amount of time to wait before the command times out.</param>
+    /// <typeparam name="T">the type to return.</typeparam>
+    /// <returns>Returns an object of the given type T.</returns>
+    public static async Task<T?> ExecuteScalarAsync<T>(this DatabaseFacade database, string sql, List<DbParameter>? parameters = null, CommandType commandType = CommandType.Text, TimeSpan? commandTimeOut = null)
     {
-        using DbCommand dbCommand = database.GetDbConnection().CreateCommand();
+        ArgumentNullException.ThrowIfNull(database);
+        ArgumentNullException.ThrowIfNull(sql);
+
+        await using DbCommand dbCommand = database.GetDbConnection().CreateCommand();
 
         if (database.CurrentTransaction is not null)
         {
@@ -34,9 +37,9 @@ public static class DbContextExtensions
 
         dbCommand.CommandText = sql;
         dbCommand.CommandType = commandType;
-        if (commandTimeOutInSeconds != null)
+        if (commandTimeOut is not null)
         {
-            dbCommand.CommandTimeout = (int)commandTimeOutInSeconds;
+            dbCommand.CommandTimeout = (int)commandTimeOut.Value.TotalSeconds;
         }
 
         if (parameters != null)
