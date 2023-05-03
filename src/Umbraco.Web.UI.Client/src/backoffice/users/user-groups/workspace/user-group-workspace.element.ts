2fdb19362a1c41734936625ac6b6d6719d1e8f5e
Jesper Møller Jensen <26099018+JesmoDev@users.noreply.github.com>
2023-05-03 21:18:16 +1200

    moved workspace action to manifest file

--- src/Umbraco.Web.UI.Client/src/backoffice/users/user-groups/workspace/user-group-workspace.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/users/user-groups/workspace/user-group-workspace.element.ts
@@ -3,9 +3,6 @@ import { html } from 'lit';
 import { customElement, state } from 'lit/decorators.js';
 import { UmbUserGroupWorkspaceContext } from './user-group-workspace.context';
 import { UmbUserGroupWorkspaceEditElement } from './user-group-workspace-edit.element';
-import { UmbSaveWorkspaceAction } from '@umbraco-cms/backoffice/workspace';
-import type { ManifestWorkspaceAction } from '@umbraco-cms/backoffice/extensions-registry';
-import { umbExtensionsRegistry } from '@umbraco-cms/backoffice/extensions-api';
 import { UmbLitElement } from '@umbraco-cms/internal/lit-element';
 import type { UmbRoute } from '@umbraco-cms/backoffice/router';
 
@@ -13,36 +10,6 @@ import type { UmbRoute } from '@umbraco-cms/backoffice/router';
 export class UmbUserGroupWorkspaceElement extends UmbLitElement {
 	#workspaceContext = new UmbUserGroupWorkspaceContext(this);
 	#element = new UmbUserGroupWorkspaceEditElement();
-
-	constructor() {
-		super();
-		this._registerWorkspaceActions();
-	}
-
-	// TODO: move this to a manifest file
-	private _registerWorkspaceActions() {
-		const manifests: Array<ManifestWorkspaceAction> = [
-			{
-				type: 'workspaceAction',
-				alias: 'Umb.WorkspaceAction.UserGroup.Save',
-				name: 'Save User Group Workspace Action',
-				meta: {
-					label: 'Save',
-					look: 'primary',
-					color: 'positive',
-					api: UmbSaveWorkspaceAction,
-				},
-				conditions: {
-					workspaces: ['Umb.Workspace.UserGroup'],
-				},
-			},
-		];
-
-		manifests.forEach((manifest) => {
-			if (umbExtensionsRegistry.isRegistered(manifest.alias)) return;
-			umbExtensionsRegistry.register(manifest);
-		});
-	}
 
 	@state()
 	_routes: UmbRoute[] = [
