2752fd125e4a89d637ea9e1a67598885c5f5fccc
Lone Iversen <108085781+loivsen@users.noreply.github.com>
2023-05-04 13:34:20 +0200

    autoheight and min max height values for textarea

--- src/Umbraco.Web.UI.Client/src/backoffice/core/property-editors/uis/textarea/property-editor-ui-textarea.element.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/core/property-editors/uis/textarea/property-editor-ui-textarea.element.ts
@@ -1,8 +1,9 @@
-import { css, html, nothing } from 'lit';
+import { css, html } from 'lit';
 import { UUITextStyles } from '@umbraco-ui/uui-css/lib';
-import { customElement, property, query, state } from 'lit/decorators.js';
+import { customElement, property, state } from 'lit/decorators.js';
 import { UUITextareaElement } from '@umbraco-ui/uui';
 import { ifDefined } from 'lit/directives/if-defined.js';
+import { styleMap } from 'lit/directives/style-map.js';
 import { UmbPropertyEditorExtensionElement } from '@umbraco-cms/backoffice/extensions-registry';
 import { UmbLitElement } from '@umbraco-cms/internal/lit-element';
 import { DataTypePropertyPresentationModel } from '@umbraco-cms/backoffice/backend-api';
@@ -18,14 +19,33 @@ export class UmbPropertyEditorUITextareaElement extends UmbLitElement implements
 	@state()
 	private _rows?: number;
 
+	@state()
+	private _maxheight?: number;
+
+	@state()
+	private _minheight?: number;
+
+	@state()
+	private _css?: any;
+
 	@property({ type: Array, attribute: false })
 	public set config(config: Array<DataTypePropertyPresentationModel>) {
 		const maxChars = config.find((x) => x.alias === 'maxChars');
 		if (maxChars) this._maxChars = maxChars.value;
 
 		const rows = config.find((x) => x.alias === 'rows');
-		if (!rows) return;
-		this._rows = rows.value;
+		if (rows) this._rows = rows.value;
+
+		const minheight = config.find((x) => x.alias === 'minHeight');
+		if (minheight) this._minheight = minheight.value;
+
+		const maxheight = config.find((x) => x.alias === 'maxHeight');
+		if (maxheight) this._maxheight = maxheight.value;
+
+		this._css = {
+			'--uui-textarea-min-height': `${this._minheight ? `${this._minheight}px` : 'reset'}`,
+			'--uui-textarea-max-height': `${this._maxheight ? `${this._maxheight}px` : 'reset'}`,
+		};
 	}
 
 	private onInput(e: InputEvent) {
@@ -42,6 +62,7 @@ export class UmbPropertyEditorUITextareaElement extends UmbLitElement implements
 			maxlength="${ifDefined(this._maxChars)}"
 			rows="${ifDefined(this._rows)}"
 			@input=${this.onInput}
+			style="${styleMap(this._css)}"
 			autoheight="${this._rows ? false : true}"></uui-textarea>`;
 	}
 
