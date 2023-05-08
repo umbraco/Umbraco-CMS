845d2ff3c9e777cdee4dd9a41a52ffc8af5b1b34
Mads Rasmussen <madsr@hey.com>
2023-05-08 21:00:44 +0200

    use tree picker for document picker

--- src/Umbraco.Web.UI.Client/src/backoffice/documents/documents/modals/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/documents/documents/modals/manifests.ts
@@ -1,11 +1,15 @@
+import { DOCUMENT_TREE_ALIAS } from '../tree/manifests';
 import type { ManifestModal } from '@umbraco-cms/backoffice/extensions-registry';
 
 const modals: Array<ManifestModal> = [
 	{
 		type: 'modal',
+		kind: 'treePicker',
 		alias: 'Umb.Modal.DocumentPicker',
 		name: 'Document Picker Modal',
-		loader: () => import('./document-picker/document-picker-modal.element'),
+		meta: {
+			treeAlias: DOCUMENT_TREE_ALIAS,
+		},
 	},
 	{
 		type: 'modal',
