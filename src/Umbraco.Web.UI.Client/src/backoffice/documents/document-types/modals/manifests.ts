b639c4dbe3d54a9ded61229a916cfe9d32348120
Mads Rasmussen <madsr@hey.com>
2023-05-09 12:02:48 +0200

    use tree picker for document types

--- src/Umbraco.Web.UI.Client/src/backoffice/documents/document-types/modals/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/documents/document-types/modals/manifests.ts
@@ -1,16 +1,6 @@
-import { DOCUMENT_TYPE_TREE_ALIAS } from '../tree/manifests';
 import type { ManifestModal } from '@umbraco-cms/backoffice/extensions-registry';
 
 const modals: Array<ManifestModal> = [
-	{
-		type: 'modal',
-		kind: 'treePicker',
-		alias: 'Umb.Modal.DocumentTypePicker',
-		name: 'Document Type Picker Modal',
-		meta: {
-			treeAlias: DOCUMENT_TYPE_TREE_ALIAS,
-		},
-	},
 	{
 		type: 'modal',
 		alias: 'Umb.Modal.AllowedDocumentTypes',
