84e4aaec6b98fe967a6094698840f3a343425e77
Niels Lyngsø <nsl@umbraco.dk>
2023-05-10 14:44:52 +0200

    manifest on UmbModalExtensionElement

--- src/Umbraco.Web.UI.Client/src/core/modal/modal-element.element.ts
+++ src/Umbraco.Web.UI.Client/src/core/modal/modal-element.element.ts
@@ -9,7 +9,7 @@ export abstract class UmbModalBaseElement<
 		ModalManifestType extends ManifestModal = ManifestModal
 	>
 	extends UmbLitElement
-	implements UmbModalExtensionElement<ModalDataType, ModalResultType>
+	implements UmbModalExtensionElement<ModalDataType, ModalResultType, ModalManifestType>
 {
 	@property({ type: Array, attribute: false })
 	public manifest?: ModalManifestType;
