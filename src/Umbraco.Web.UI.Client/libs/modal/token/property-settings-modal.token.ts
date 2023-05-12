f97946e768e0cac9dfc1ae6b3852a9d337eee225
Niels Lyngsø <nsl@umbraco.dk>
2023-05-12 11:20:52 +0200

    clean up

--- src/Umbraco.Web.UI.Client/libs/modal/token/property-settings-modal.token.ts
+++ src/Umbraco.Web.UI.Client/libs/modal/token/property-settings-modal.token.ts
@@ -1,24 +1,8 @@
 import { PropertyTypeResponseModelBaseModel } from '@umbraco-cms/backoffice/backend-api';
 import { UmbModalToken } from '@umbraco-cms/backoffice/modal';
 
-// TODO: add interface for data
-// PropertyTypeViewModelBaseModel
-
 export type UmbPropertySettingsModalData = PropertyTypeResponseModelBaseModel;
 export type UmbPropertySettingsModalResult = PropertyTypeResponseModelBaseModel;
-/*{
-	label: string;
-	alias: string;
-	description: string;
-	propertyEditorUI?: string;
-	labelOnTop: boolean;
-	validation: {
-		mandatory: boolean;
-		mandatoryMessage: string;
-		pattern: string;
-		patternMessage: string;
-	};
-}*/
 
 export const UMB_PROPERTY_SETTINGS_MODAL = new UmbModalToken<
 	UmbPropertySettingsModalData,
