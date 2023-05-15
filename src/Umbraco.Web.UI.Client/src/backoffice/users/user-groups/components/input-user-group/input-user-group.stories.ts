10963eea19945db4b6e4db5bc188de0b68921fa7
Jesper Møller Jensen <26099018+JesmoDev@users.noreply.github.com>
2023-05-15 17:26:38 +1200

    rename

--- src/Umbraco.Web.UI.Client/src/backoffice/users/user-groups/components/input-user-group/input-user-group.stories.ts
+++ src/Umbraco.Web.UI.Client/src/backoffice/users/user-groups/components/input-user-group/input-user-group.stories.ts
@@ -3,28 +3,27 @@ import './input-user-group.element';
 import type { UmbInputPickerUserGroupElement } from './input-user-group.element';
 
 const meta: Meta<UmbInputPickerUserGroupElement> = {
-    title: 'Components/Inputs/User Group',
-    component: 'umb-input-user-group',
-    argTypes: {
-        modalType: {
-            control: 'inline-radio',
-            options: ['dialog', 'sidebar'],
-            defaultValue: 'sidebar',
-            description: 'The type of modal to use when selecting user groups',
-        },
-        modalSize:{
-            control: 'select',
-            options: ['small', 'medium', 'large', 'full'],
-            defaultValue: 'small',
-            description: 'The size of the modal to use when selecting user groups, only applicable to sidebar not dialog',
-        }
-    }
+	title: 'Components/Inputs/User Group',
+	component: 'umb-user-group-input',
+	argTypes: {
+		modalType: {
+			control: 'inline-radio',
+			options: ['dialog', 'sidebar'],
+			defaultValue: 'sidebar',
+			description: 'The type of modal to use when selecting user groups',
+		},
+		modalSize: {
+			control: 'select',
+			options: ['small', 'medium', 'large', 'full'],
+			defaultValue: 'small',
+			description: 'The size of the modal to use when selecting user groups, only applicable to sidebar not dialog',
+		},
+	},
 };
-  
+
 export default meta;
 type Story = StoryObj<UmbInputPickerUserGroupElement>;
-  
+
 export const Overview: Story = {
-    args: {
-    }
+	args: {},
 };
