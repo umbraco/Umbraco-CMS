0d0b0b46ef3499ba7ea7dd13635f9c46114a507d
Mads Rasmussen <madsr@hey.com>
2023-05-16 13:33:08 +0200

    remove import

--- src/Umbraco.Web.UI.Client/libs/extension-api/registry/extension.registry.test.ts
+++ src/Umbraco.Web.UI.Client/libs/extension-api/registry/extension.registry.test.ts
@@ -1,11 +1,10 @@
 import { expect } from '@open-wc/testing';
 import type { ManifestElementWithElementName, ManifestKind, ManifestWithMeta } from '../types';
 import { UmbExtensionRegistry } from './extension.registry';
-import type { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
 
 describe('UmbExtensionRegistry', () => {
 	let extensionRegistry: UmbExtensionRegistry<ManifestWithMeta>;
-	let manifests: Array<ManifestTypes>;
+	let manifests: Array<ManifestWithMeta>;
 
 	beforeEach(() => {
 		extensionRegistry = new UmbExtensionRegistry();
