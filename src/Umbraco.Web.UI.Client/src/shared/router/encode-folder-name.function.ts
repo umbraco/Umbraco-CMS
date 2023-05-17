b1db6e35620cca801b78823bc2ed9741ee287b1e
Niels Lyngsø <nsl@umbraco.dk>
2023-05-17 10:07:04 +0200

    remove comment

--- src/Umbraco.Web.UI.Client/src/shared/router/encode-folder-name.function.ts
+++ src/Umbraco.Web.UI.Client/src/shared/router/encode-folder-name.function.ts
@@ -1,6 +1,5 @@
 export const encodeFolderName = (path: string) =>
 	encodeURIComponent(path.toLowerCase().replace(/\s+/g, '-'))
-		//.replace(/-/g, '%2D')
 		.replace(/_/g, '-')
 		.replace(/\./g, '-')
 		.replace(/!/g, '-')
