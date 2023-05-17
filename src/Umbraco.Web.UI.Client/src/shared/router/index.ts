2a37c6f7e21358a480cf4114a20c23a5113dc4a9
Mads Rasmussen <madsr@hey.com>
2023-05-17 16:55:48 +0200

    move router into shared

--- src/Umbraco.Web.UI.Client/src/shared/router/index.ts
+++ src/Umbraco.Web.UI.Client/src/shared/router/index.ts
@@ -2,3 +2,17 @@ export * from './router-slot.element';
 export * from './router-slot-change.event';
 export * from './router-slot-init.event';
 export * from './variant-router-slot.element';
+export type {
+	PageComponent,
+	Guard,
+	IRoutingInfo,
+	IComponentRoute,
+	IRedirectRoute,
+	IResolverRoute,
+	Params,
+	Query,
+} from 'router-slot/model';
+export * from 'router-slot/util';
+export * from './route.context';
+export * from './route.interface';
+export * from './generate-route-path-builder.function';
