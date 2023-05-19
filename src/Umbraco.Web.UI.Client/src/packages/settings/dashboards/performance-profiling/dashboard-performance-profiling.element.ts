59e1c48b89c5ddced668ba652e650722d48c38b4
Nathan Woulfe <nathan@nathanw.com.au>
2023-05-19 12:02:05 +1000

    typo + grammar in text

--- src/Umbraco.Web.UI.Client/src/packages/settings/dashboards/performance-profiling/dashboard-performance-profiling.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/settings/dashboards/performance-profiling/dashboard-performance-profiling.element.ts
@@ -41,8 +41,8 @@ export class UmbDashboardPerformanceProfilingElement extends UmbLitElement {
 		return this._isDebugMode
 			? html`
 					<p>
-						Umbraco currently runs in debug mode. This means you can use the built-in performance profiler to assess the
-						performance when rendering pages.
+						Umbraco is running in debug mode. This means you can use the built-in performance profiler 
+						to assess performance when rendering pages.
 					</p>
 					<p>
 						If you want to activate the profiler for a specific page rendering, simply add
@@ -70,11 +70,11 @@ export class UmbDashboardPerformanceProfilingElement extends UmbLitElement {
 			  `
 			: html`
 					<p>
-						Umbraco currently does not run in debug mode, so you can't use the built-in profiler. This is how it should
+						Umbraco is not running in debug mode, so you can't use the built-in profiler. This is how it should
 						be for a production site.
 					</p>
 					<p>
-						Debug mode is turned on by by setting <strong>Umbraco:CMS:Hosting:Debug</strong> to <strong>true</strong> in
+						Debug mode is turned on by setting <strong>Umbraco:CMS:Hosting:Debug</strong> to <strong>true</strong> in
 						appsettings.json, appsettings.{Environment}.json or via an environment variable.
 					</p>
 			  `;
