683907aa73a24860bac86b419efa3e53c0062e4c
Mads Rasmussen <madsr@hey.com>
2023-05-19 11:23:28 +0200

    import relative when inside the same module

--- src/Umbraco.Web.UI.Client/src/packages/core/store/entity-tree-store.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/store/entity-tree-store.ts
@@ -1,6 +1,7 @@
+import { UmbStoreBase } from './store-base';
+import type { UmbTreeStore } from './tree-store.interface';
 import { EntityTreeItemResponseModel } from '@umbraco-cms/backoffice/backend-api';
 import { UmbArrayState } from '@umbraco-cms/backoffice/observable-api';
-import { UmbStoreBase, UmbTreeStore } from '@umbraco-cms/backoffice/store';
 import { UmbControllerHostElement } from '@umbraco-cms/backoffice/controller-api';
 
 /**
