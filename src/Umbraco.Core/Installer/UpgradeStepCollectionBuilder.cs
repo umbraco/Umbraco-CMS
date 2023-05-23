c936d425186818f446c5ffb060f092e6768333ef
Bjarke Berg <mail@bergmania.dk>
2023-05-23 10:35:35 +0200

    Added missing files

---
+++ src/Umbraco.Core/Installer/UpgradeStepCollectionBuilder.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using Umbraco.Cms.Core.Composing;
+
+namespace Umbraco.Cms.Core.Installer;
+
+public class UpgradeStepCollectionBuilder : OrderedCollectionBuilderBase<UpgradeStepCollectionBuilder, UpgradeStepCollection, IUpgradeStep>
+{
+    protected override UpgradeStepCollectionBuilder This => this;
+
+    protected override ServiceLifetime CollectionLifetime => ServiceLifetime.Scoped;
+}
