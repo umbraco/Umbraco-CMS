a35763174972450ae8b7538f28d7fc6b3769e0eb
Mads Rasmussen <madsr@hey.com>
2023-05-23 19:53:45 +0200

    export repository from data types

--- src/Umbraco.Web.UI.Client/src/packages/settings/data-types/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/settings/data-types/index.ts
@@ -1,6 +1,8 @@
 import './components';
-export type * from './models';
 
 export const DATA_TYPE_ROOT_ENTITY_TYPE = 'data-type-root';
 export const DATA_TYPE_ENTITY_TYPE = 'data-type';
 export const DATA_TYPE_FOLDER_ENTITY_TYPE = 'data-type-folder';
+
+export type * from './models';
+export * from './repository';
