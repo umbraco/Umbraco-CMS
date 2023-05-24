cd8000dece6537c053c783903f2795a0c21dc36e
Niels Lyngsø <nsl@umbraco.dk>
2023-05-24 11:17:59 +0200

    language-root create

---
+++ src/Umbraco.Web.UI.Client/src/packages/settings/languages/entity-actions/language-create-entity-action.ts
@@ -0,0 +1,14 @@
+import { UmbLanguageRepository } from '../repository/language.repository';
+import { UmbEntityActionBase } from '@umbraco-cms/backoffice/entity-action';
+import { UmbControllerHostElement } from '@umbraco-cms/backoffice/controller-api';
+
+export class UmbLanguageCreateEntityAction extends UmbEntityActionBase<UmbLanguageRepository> {
+	constructor(host: UmbControllerHostElement, repositoryAlias: string, unique: string) {
+		super(host, repositoryAlias, unique);
+	}
+
+	async execute() {
+		console.log(`execute for: ${this.unique}`);
+		alert('language create dialog');
+	}
+}
