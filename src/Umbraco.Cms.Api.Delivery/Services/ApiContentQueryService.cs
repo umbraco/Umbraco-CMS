53e0227c985b4514573ef7d12486c11c4c4af712
Kenn Jacobsen <kja@umbraco.dk>
2023-05-25 11:27:37 +0200

    Fix the namespace for PagedModel (#14296)
    * Fix the namespace for PagedModel
    
    * Add compat suppressions

--- src/Umbraco.Cms.Api.Delivery/Services/ApiContentQueryService.cs
+++ src/Umbraco.Cms.Api.Delivery/Services/ApiContentQueryService.cs
@@ -1,10 +1,10 @@
 using Umbraco.Cms.Api.Delivery.Indexing.Selectors;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.DeliveryApi;
+using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Models.PublishedContent;
 using Umbraco.Cms.Core.Services.OperationStatus;
 using Umbraco.Extensions;
-using Umbraco.New.Cms.Core.Models;
 
 namespace Umbraco.Cms.Api.Delivery.Services;
 
