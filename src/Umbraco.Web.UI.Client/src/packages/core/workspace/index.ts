5c027884cc27825a5c032578dede4d5de2f995ac
Niels Lyngsø <nsl@umbraco.dk>
2023-05-25 10:07:00 +0200

    import workspace-property-layout

--- src/Umbraco.Web.UI.Client/src/packages/core/workspace/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/workspace/index.ts
@@ -5,3 +5,4 @@ export * from './workspace-context/index.js';
 export * from './workspace-variant/index.js';
 export * from './workspace-split-view-manager.class.js';
 export * from './workspace-property/index.js';
+export * from './workspace-property-layout/workspace-property-layout.element.js';
