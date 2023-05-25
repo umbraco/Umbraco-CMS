59431facd20778bd2ff8111ed41e65b456eca8d0
Niels Lyngsø <nsl@umbraco.dk>
2023-05-25 10:36:47 +0200

    install UUI RC-1

--- src/Umbraco.Web.UI.Client/src/external/uui/index.ts
+++ src/Umbraco.Web.UI.Client/src/external/uui/index.ts
@@ -1,8 +1,1 @@
 export * from '@umbraco-ui/uui';
-// TODO: these export can be removed when we upgrade to 1.3.0. Then they will be part of the main export
-export * from '@umbraco-ui/uui-base/lib/animations';
-export * from '@umbraco-ui/uui-base/lib/events';
-export * from '@umbraco-ui/uui-base/lib/mixins';
-export * from '@umbraco-ui/uui-base/lib/registration';
-export * from '@umbraco-ui/uui-base/lib/types';
-export * from '@umbraco-ui/uui-base/lib/utils';
