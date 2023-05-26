3db04552e7a76a5c0e7d7594c15fd4711e166e36
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-05-26 16:01:56 +0200

    use named type exports

--- src/Umbraco.Web.UI.Client/src/external/router-slot/index.ts
+++ src/Umbraco.Web.UI.Client/src/external/router-slot/index.ts
@@ -1,3 +1,14 @@
 export * from 'router-slot';
-export * from 'router-slot/model';
+export type {
+	Guard,
+	IRoute,
+	IRoutingInfo,
+	IComponentRoute,
+	IRedirectRoute,
+	IResolverRoute,
+	IRouterSlot,
+	PageComponent,
+	Component,
+	Params,
+} from 'router-slot/model';
 export * from 'router-slot/util';
--- src/Umbraco.Web.UI.Client/src/packages/settings/data-types/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/settings/data-types/index.ts
@@ -4,5 +4,5 @@ export const DATA_TYPE_ROOT_ENTITY_TYPE = 'data-type-root';
 export const DATA_TYPE_ENTITY_TYPE = 'data-type';
 export const DATA_TYPE_FOLDER_ENTITY_TYPE = 'data-type-folder';
 
-export * from './models.js';
+export type { UmbDataTypeModel } from './models.js';
 export * from './repository/index.js';
