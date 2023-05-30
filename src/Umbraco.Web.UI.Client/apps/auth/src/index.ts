8020c51113add590deaaf865bf492516c4469f74
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-05-30 10:10:58 +0200

    import correct css file

--- src/Umbraco.Web.UI.Client/apps/auth/src/index.ts
+++ src/Umbraco.Web.UI.Client/apps/auth/src/index.ts
@@ -1,7 +1,7 @@
 import 'element-internals-polyfill';
 
 import '@umbraco-ui/uui-css/dist/uui-css.css';
-import '../../../src/shared/css/custom-properties.css';
+import '../../../src/css/umb-css.css';
 import '@umbraco-ui/uui';
 
 import './login.element.js';
