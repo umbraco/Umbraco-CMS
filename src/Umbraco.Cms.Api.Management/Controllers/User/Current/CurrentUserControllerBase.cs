12aa4312b75483e6927086c7cf14371da9db377c
Nikolaj <nikolajlauridsen@protonmail.ch>
2023-05-30 11:38:44 +0200

    Add current user data endpoint

---
+++ src/Umbraco.Cms.Api.Management/Controllers/User/Current/CurrentUserControllerBase.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Management.Routing;
+
+namespace Umbraco.Cms.Api.Management.Controllers.User.Current;
+
+[ApiController]
+[VersionedApiBackOfficeRoute("user/current")]
+[ApiExplorerSettings(GroupName = "User")]
+public class CurrentUserControllerBase : ManagementApiControllerBase
+{
+
+}
---
+++ src/Umbraco.Cms.Api.Management/Mapping/Users/CurrentUserViewModelsMapDefinition.cs
@@ -0,0 +1,19 @@
+﻿using Umbraco.Cms.Api.Management.ViewModels.User.Current;
+using Umbraco.Cms.Core.Mapping;
+using Umbraco.Cms.Core.Models;
+
+namespace Umbraco.Cms.Api.Management.Mapping.Users;
+
+public class CurrentUserViewModelsMapDefinition : IMapDefinition
+{
+    public void DefineMaps(IUmbracoMapper mapper)
+    {
+        mapper.Define<UserData, UserDataViewModel>((_, _) => new UserDataViewModel {Data = string.Empty, Name = string.Empty }, Map);
+    }
+
+    private void Map(UserData source, UserDataViewModel target, MapperContext context)
+    {
+        target.Name = source.Name;
+        target.Data = source.Data;
+    }
+}
