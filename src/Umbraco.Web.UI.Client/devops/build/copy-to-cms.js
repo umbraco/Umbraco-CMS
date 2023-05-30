9a073d64f59a09e171520f5c1f3c9e43746c9133
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-05-30 15:26:16 +0200

    let cms handle its output files manually

--- src/Umbraco.Web.UI.Client/devops/build/copy-to-cms.js
+++ src/Umbraco.Web.UI.Client/devops/build/copy-to-cms.js
@@ -2,7 +2,5 @@ import { cpSync } from 'fs';
 
 const srcDir = './dist-cms';
 const outputDir = '../Umbraco.Cms.StaticAssets/wwwroot/umbraco/backoffice';
-const outputWebDir = '../Umbraco.Web.UI.New';
 
 cpSync(srcDir, outputDir, { recursive: true });
-cpSync(`${srcDir}/umbraco-package-schema.json`, `${outputWebDir}/umbraco-package-schema.json`);
