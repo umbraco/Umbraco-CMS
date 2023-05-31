a20c502a7fe98724fda78126a61dddb034161826
nikolajlauridsen <nikolajlauridsen@protonmail.ch>
2023-05-31 14:53:11 +0200

    Add set tour endpoint

--- src/Umbraco.Cms.Api.Management/Mapping/Tour/TourViewModelsMapDefinition.cs
+++ src/Umbraco.Cms.Api.Management/Mapping/Tour/TourViewModelsMapDefinition.cs
@@ -9,6 +9,15 @@ public class TourViewModelsMapDefinition : IMapDefinition
     public void DefineMaps(IUmbracoMapper mapper)
     {
         mapper.Define<UserTourStatus, TourStatusViewModel>((_, _) => new TourStatusViewModel{ Alias = string.Empty}, Map);
+        mapper.Define<SetTourStatusRequestModel, UserTourStatus>((_, _) => new UserTourStatus(), Map);
+    }
+
+    // Umbraco.Code.MapAll
+    private void Map(SetTourStatusRequestModel source, UserTourStatus target, MapperContext context)
+    {
+        target.Alias = source.Alias;
+        target.Completed = source.Completed;
+        target.Disabled = source.Disabled;
     }
 
     // Umbraco.Code.MapAll
