186e884d92b420cec09406b096ac084453a6a71a
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-06-12 19:17:12 +0200

    cleanup imports

--- src/Umbraco.Web.UI.Client/src/shared/auth/auth-flow.ts
+++ src/Umbraco.Web.UI.Client/src/shared/auth/auth-flow.ts
@@ -13,7 +13,6 @@
  * License for the specific language governing permissions and limitations under
  * the License.
  */
-import { BehaviorSubject } from '@umbraco-cms/backoffice/external/rxjs';
 import {
 	BaseTokenRequestHandler,
 	BasicQueryStringUtils,
