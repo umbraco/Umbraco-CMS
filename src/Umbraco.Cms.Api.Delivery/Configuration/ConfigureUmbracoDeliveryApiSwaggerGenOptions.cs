cbf42bd91a59af7528a23ff859d77174286ffc6b
Kenn Jacobsen <kja@umbraco.dk>
2023-06-12 09:31:39 +0200

    Make the API versioning and Swagger config less intrusive (#14363)

--- src/Umbraco.Cms.Api.Delivery/Configuration/ConfigureUmbracoDeliveryApiSwaggerGenOptions.cs
+++ src/Umbraco.Cms.Api.Delivery/Configuration/ConfigureUmbracoDeliveryApiSwaggerGenOptions.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Options;
 using Microsoft.OpenApi.Models;
 using Swashbuckle.AspNetCore.SwaggerGen;
+using Umbraco.Cms.Api.Common.OpenApi;
 
 namespace Umbraco.Cms.Api.Delivery.Configuration;
 
@@ -16,5 +17,7 @@ public class ConfigureUmbracoDeliveryApiSwaggerGenOptions: IConfigureOptions<Swa
                 Title = DeliveryApiConfiguration.ApiTitle,
                 Version = "Latest",
             });
+
+        swaggerGenOptions.DocumentFilter<MimeTypeDocumentFilter>(DeliveryApiConfiguration.ApiName);
     }
 }
