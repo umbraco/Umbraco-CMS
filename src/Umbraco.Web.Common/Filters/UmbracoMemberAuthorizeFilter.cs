umbraco/Umbraco-CMS

commit 22328598dbbb22b36fb395e52507eeba9c3c3000

Adding dedicated Forbidden and Unauthorized handling for members (#14036)

src/Umbraco.Web.Common/Filters/UmbracoMemberAuthorizeFilter.cs
@@ -54,11 +54,20 @@ public class UmbracoMemberAuthorizeFilter : IAsyncAuthorizationFilter
 
         IMemberManager memberManager = context.HttpContext.RequestServices.GetRequiredService<IMemberManager>();
 
-        if (!await IsAuthorizedAsync(memberManager))
+        if (memberManager.IsLoggedIn())
+        {
+            if (!await IsAuthorizedAsync(memberManager))
+            {
+                context.HttpContext.SetReasonPhrase(
+                    "Resource restricted: the member is not of a permitted type or group.");
+                context.Result = new ForbidResult();
+            }
+        }
+        else
         {
             context.HttpContext.SetReasonPhrase(
-                "Resource restricted: either member is not logged on or is not of a permitted type or group.");
-            context.Result = new ForbidResult();
+                "Resource restricted: the member is not logged in.");
+            context.Result = new UnauthorizedResult();
         }
     }
 

src/Umbraco.Web.Common/Security/ConfigureMemberCookieOptions.cs
@@ -1,4 +1,5 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Options;
 using Umbraco.Cms.Core.Routing;
@@ -43,6 +44,12 @@ public sealed class ConfigureMemberCookieOptions : IConfigureNamedOptions<Cookie
 
                 // When we are signed in with the cookie, assign the principal to the current HttpContext
                 ctx.HttpContext.SetPrincipalForRequest(ctx.Principal);
+
+                return Task.CompletedTask;
+            },
+            OnRedirectToAccessDenied = ctx =>
+            {
+                ctx.Response.StatusCode = StatusCodes.Status403Forbidden;
 
                 return Task.CompletedTask;
             },

commit 052f53eb0ae6aa6f8aa18dae6fd9a2b25abb5c39

Fix broken CookieAuthenticationRedirect caused by PR #14036 when not in an API controller

src/Umbraco.Web.Common/Filters/UmbracoMemberAuthorizeFilter.cs
@@ -60,14 +60,14 @@ public class UmbracoMemberAuthorizeFilter : IAsyncAuthorizationFilter
             {
                 context.HttpContext.SetReasonPhrase(
                     "Resource restricted: the member is not of a permitted type or group.");
+                context.HttpContext.Response.StatusCode = 403;
                 context.Result = new ForbidResult();
             }
         }
         else
         {
-            context.HttpContext.SetReasonPhrase(
-                "Resource restricted: the member is not logged in.");
-            context.Result = new UnauthorizedResult();
+            context.HttpContext.Response.StatusCode = 401;
+            context.Result = new ForbidResult();
         }
     }
 

src/Umbraco.Web.Common/Security/ConfigureMemberCookieOptions.cs
@@ -1,9 +1,11 @@
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc.Controllers;
 using Microsoft.Extensions.Options;
 using Umbraco.Cms.Core.Routing;
 using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Web.Common.Controllers;
 using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Web.Common.Security;
@@ -49,8 +51,13 @@ public sealed class ConfigureMemberCookieOptions : IConfigureNamedOptions<Cookie
             },
             OnRedirectToAccessDenied = ctx =>
             {
-                ctx.Response.StatusCode = StatusCodes.Status403Forbidden;
-
+                //When the controller is an UmbracoAPIController, we want to return a statuscode instead of a redirect.
+                //All other cases should use the default Redirect of the CookieAuthenticationEvent.
+                var controllerDescriptor = ctx.HttpContext.GetEndpoint()?.Metadata?.OfType<ControllerActionDescriptor>().FirstOrDefault();
+                if (!controllerDescriptor?.ControllerTypeInfo.IsSubclassOf(typeof(UmbracoApiController)) ?? false)
+                {
+                    new CookieAuthenticationEvents().OnRedirectToAccessDenied(ctx);
+                }
                 return Task.CompletedTask;
             },
         };