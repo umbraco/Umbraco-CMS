3ebff43e0171828369283a1f4d34f024bd6f323d
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2023-06-18 13:42:26 +0200

    Load default icons to show them next to checkboxes in configuration of richtext editor

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/rte/rte.prevalues.controller.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/rte/rte.prevalues.controller.js
@@ -1,12 +1,12 @@
 angular.module("umbraco").controller("Umbraco.PrevalueEditors.RteController",
-    function ($scope, $timeout, $log, tinyMceService, stylesheetResource, assetsService) {
+    function ($scope, $sce, tinyMceService, stylesheetResource, assetsService) {
         var cfg = tinyMceService.defaultPrevalues();
 
-        if($scope.model.value){
-            if(Utilities.isString($scope.model.value)){
+        if($scope.model.value) {
+            if (Utilities.isString($scope.model.value)){
                 $scope.model.value = cfg;
             }
-        }else{
+        }else {
             $scope.model.value = cfg;
         }
 
@@ -27,14 +27,15 @@ angular.module("umbraco").controller("Umbraco.PrevalueEditors.RteController",
             $scope.model.value.mode = 'inline';
         }
 
-        tinyMceService.configuration().then(function(config){
-            $scope.tinyMceConfig = config;
-
+        tinyMceService.configuration().then(config => {
+          $scope.tinyMceConfig = config;
+            
             // extend commands with properties for font-icon and if it is a custom command
-            $scope.tinyMceConfig.commands = _.map($scope.tinyMceConfig.commands, function (obj) {
-                var icon = getFontIcon(obj.alias);
+            $scope.tinyMceConfig.commands = _.map($scope.tinyMceConfig.commands, obj => {
+                const icon = getIcon(obj.alias);
+                console.log("icon", icon);
 
-                var objCmd = Utilities.extend(obj, {
+                const objCmd = Utilities.extend(obj, {
                     fontIcon: icon.name,
                     isCustom: icon.isCustom,
                     selected: $scope.model.value.toolbar.indexOf(obj.alias) >= 0,
@@ -43,9 +44,79 @@ angular.module("umbraco").controller("Umbraco.PrevalueEditors.RteController",
 
                 return objCmd;
             });
+
+            assetsService.loadJs("lib/tinymce/icons/default/icons.js", $scope).then(() => {
+                const icons = tinymce.IconManager.get('default').icons;
+                const parser = new DOMParser();
+
+                Utilities.forEach($scope.tinyMceConfig.commands, cmd => {
+
+                  console.log("cmd", cmd);
+
+                  let icon = cmd.alias;
+
+                  switch (cmd.alias) {
+                    case "ace":
+                      icon = "sourcecode";
+                      break;
+                    case "anchor":
+                      icon = "bookmark";
+                      break;
+                    case "alignleft":
+                      icon = "align-left";
+                      break;
+                    case "aligncenter":
+                      icon = "align-center";
+                      break;
+                    case "alignright":
+                      icon = "align-right";
+                      break;
+                    case "alignjustify":
+                      icon = "align-justify";
+                      break;
+                    case "charmap":
+                      icon = "insert-character";
+                      break;
+                    case "hr":
+                      icon = "horizontal-rule";
+                      break;
+                    case "bullist":
+                      icon = "unordered-list";
+                      break;
+                    case "numlist":
+                      icon = "ordered-list";
+                      break;
+                    case "strikethrough":
+                      icon = "strike-through";
+                      break;
+                    case "removeformat":
+                      icon = "remove-formatting";
+                      break;
+                    case "forecolor":
+                      icon = "text-color";
+                      break;
+                    case "hilitecolor":
+                      icon = "highlight-bg-color";
+                      break;
+                  }
+
+                  if (!cmd.isCustom && icons.hasOwnProperty(icon)) {
+                    const svg = icons[icon];
+                    const frag = parser.parseFromString(svg, 'text/html').body.childNodes[0];
+                    const width = frag.getAttribute("width");
+                    const height = frag.getAttribute("height");
+
+                    frag.setAttribute("viewbox", `0 0 ${width} ${height}`);
+                    cmd.svgIcon = $sce.trustAsHtml(frag.outerHTML);
+                    cmd.icon = null;
+                  }
+
+                });
+            });
+            
         });
 
-        stylesheetResource.getAll().then(function(stylesheets){
+        stylesheetResource.getAll().then(stylesheets => {
             $scope.stylesheets = stylesheets;
 
             // if the CSS directory changes, previously assigned stylesheets are retained, but will not be visible
@@ -63,14 +134,14 @@ angular.module("umbraco").controller("Umbraco.PrevalueEditors.RteController",
         $scope.selectCommand = function(command){
             var index = $scope.model.value.toolbar.indexOf(command.alias);
 
-            if(command.selected && index === -1){
+            if (command.selected && index === -1){
                 $scope.model.value.toolbar.push(command.alias);
-            }else if(index >= 0){
+            }else if (index >= 0){
                 $scope.model.value.toolbar.splice(index, 1);
             }
         };
 
-        $scope.selectStylesheet = function (css) {
+        $scope.selectStylesheet = css => {
 
             // find out if the stylesheet is already selected; first look for the full stylesheet path (current format)
             var index = $scope.model.value.stylesheets.indexOf(css.path);
@@ -79,15 +150,15 @@ angular.module("umbraco").controller("Umbraco.PrevalueEditors.RteController",
                 index = $scope.model.value.stylesheets.indexOf(css.name);
             }
 
-            if(index === -1){
+            if (index === -1){
                 $scope.model.value.stylesheets.push(css.path);
-            }else{
+            }else {
                 $scope.model.value.stylesheets.splice(index, 1);
             }
         };
 
         // map properties for specific commands
-        function getFontIcon(alias) {
+        function getIcon(alias) {
             var icon = { name: alias, isCustom: false };
 
             switch (alias) {
