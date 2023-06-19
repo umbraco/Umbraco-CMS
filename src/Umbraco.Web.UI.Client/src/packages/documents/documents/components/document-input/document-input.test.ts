cdeb5a21a5ddeb71de1876c1a45aa935c8609db3
Lone Iversen <108085781+loivsen@users.noreply.github.com>
2023-06-19 14:20:08 +0200

    document input test file

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/components/document-input/document-input.test.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/components/document-input/document-input.test.ts
@@ -1,7 +1,7 @@
 import { expect, fixture, html } from '@open-wc/testing';
 import { UmbDocumentInputElement } from './document-input.element.js';
 import { defaultA11yConfig } from '@umbraco-cms/internal/test-utils';
-describe('UmbInputDocumentPickerElement', () => {
+describe('UmbDocumentInputElement', () => {
 	let element: UmbDocumentInputElement;
 
 	beforeEach(async () => {
