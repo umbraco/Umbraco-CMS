f53bc5d23d2d67784fd211d011d768834d58da63
Anders Reus <arh@umbraco.dk>
2023-06-21 22:58:52 +0200

    Add exclusion setting to typefinder settings

--- src/Umbraco.Core/Composing/TypeFinder.cs
+++ src/Umbraco.Core/Composing/TypeFinder.cs
@@ -21,7 +21,7 @@ public class TypeFinder : ITypeFinder
     ///     its an exact name match
     ///     NOTE this means that "foo." will NOT exclude "foo.dll" but only "foo.*.dll"
     /// </remarks>
-    internal static readonly string[] KnownAssemblyExclusionFilter =
+    internal static string[] KnownAssemblyExclusionFilter =
     {
         "mscorlib,", "netstandard,", "System,", "Antlr3.", "AutoMapper,", "AutoMapper.", "Autofac,", // DI
         "Autofac.", "AzureDirectory,", "Castle.", // DI, tests
@@ -49,11 +49,23 @@ public class TypeFinder : ITypeFinder
     private string[]? _assembliesAcceptingLoadExceptions;
     private volatile HashSet<Assembly>? _localFilteredAssemblyCache;
 
+    [Obsolete("Please use constructor that takes additionalExlusionAssemblies, scheduled for removal in v14")]
     public TypeFinder(ILogger<TypeFinder> logger, IAssemblyProvider assemblyProvider, ITypeFinderConfig? typeFinderConfig = null)
     {
         _logger = logger ?? throw new ArgumentNullException(nameof(logger));
         _assemblyProvider = assemblyProvider;
         _typeFinderConfig = typeFinderConfig;
+    }
+
+    public TypeFinder(ILogger<TypeFinder> logger, IAssemblyProvider assemblyProvider, string[]? additionalExlusionAssemblies, ITypeFinderConfig? typeFinderConfig = null)
+    {
+        _logger = logger ?? throw new ArgumentNullException(nameof(logger));
+        _assemblyProvider = assemblyProvider;
+        _typeFinderConfig = typeFinderConfig;
+        if (additionalExlusionAssemblies is not null)
+        {
+            KnownAssemblyExclusionFilter = KnownAssemblyExclusionFilter.Concat(additionalExlusionAssemblies).ToArray();
+        }
     }
 
     /// <inheritdoc />
