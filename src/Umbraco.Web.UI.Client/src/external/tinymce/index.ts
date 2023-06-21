e070e4532da12ec2e6515f1ff087c3d393ea7912
Nathan Woulfe <nathan@nathanw.com.au>
2023-06-21 12:43:08 +1000

    remove obsolete

--- src/Umbraco.Web.UI.Client/src/external/tinymce/index.ts
+++ src/Umbraco.Web.UI.Client/src/external/tinymce/index.ts
@@ -1,6 +1,1 @@
-// import tinymce from 'tinymce';
-// import type { EditorEvent, Editor, AstNode, TinyMCE } from 'tinymce';
-
-// export { tinymce, Editor, AstNode, EditorEvent, TinyMCE };
-
 export * as tinymce from 'tinymce';
