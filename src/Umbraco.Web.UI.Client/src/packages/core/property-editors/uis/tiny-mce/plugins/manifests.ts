a05d266d013c2b0613552d343875f65ce67795f8
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-06-22 14:50:24 +0200

    load manifests dynamically

--- src/Umbraco.Web.UI.Client/src/packages/core/property-editors/uis/tiny-mce/plugins/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property-editors/uis/tiny-mce/plugins/manifests.ts
@@ -1,36 +1,35 @@
 import type { ManifestTinyMcePlugin } from '@umbraco-cms/backoffice/extension-registry';
 
-const pluginBaseUrl = '/src/packages/core/property-editors/uis/tiny-mce/plugins/';
 const pluginManifests: Array<ManifestTinyMcePlugin> = [
 	{
 		type: 'tinyMcePlugin',
 		alias: 'Umb.TinyMcePlugin.CodeEditor',
 		name: 'Code Editor TinyMCE Plugin',
-		js: `${pluginBaseUrl}tiny-mce-code-editor.plugin`,
+		loader: () => import('./tiny-mce-code-editor.plugin.js'),
 	},
 	{
 		type: 'tinyMcePlugin',
 		alias: 'Umb.TinyMcePlugin.LinkPicker',
 		name: 'Link Picker TinyMCE Plugin',
-		js: `${pluginBaseUrl}tiny-mce-linkpicker.plugin`,
+		loader: () => import('./tiny-mce-linkpicker.plugin.js'),
 	},
 	{
 		type: 'tinyMcePlugin',
 		alias: 'Umb.TinyMcePlugin.MediaPicker',
 		name: 'Media Picker TinyMCE Plugin',
-		js: `${pluginBaseUrl}tiny-mce-mediapicker.plugin`,
+		loader: () => import('./tiny-mce-mediapicker.plugin.js'),
 	},
 	{
 		type: 'tinyMcePlugin',
 		alias: 'Umb.TinyMcePlugin.EmbeddedMedia',
 		name: 'Embedded Media TinyMCE Plugin',
-		js: `${pluginBaseUrl}tiny-mce-embeddedmedia.plugin`,
+		loader: () => import('./tiny-mce-embeddedmedia.plugin.js'),
 	},
 	{
 		type: 'tinyMcePlugin',
 		alias: 'Umb.TinyMcePlugin.MacroPicker',
 		name: 'Macro Picker TinyMCE Plugin',
-		js: `${pluginBaseUrl}tiny-mce-macropicker.plugin`,
+		loader: () => import('./tiny-mce-macropicker.plugin.js'),
 	},
 ];
 
