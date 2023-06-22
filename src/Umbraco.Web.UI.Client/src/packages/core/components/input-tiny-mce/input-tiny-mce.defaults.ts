8b08a5002b994f848c2b21fc62d2d22440425c92
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-06-22 14:29:53 +0200

    add types

--- src/Umbraco.Web.UI.Client/src/packages/core/components/input-tiny-mce/input-tiny-mce.defaults.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/components/input-tiny-mce/input-tiny-mce.defaults.ts
@@ -1,6 +1,8 @@
-export type TinyStyleSheet = { title: string; items: Array<{ title: string; block: string }> };
+import type { tinymce } from '@umbraco-cms/backoffice/external/tinymce';
 
-export const defaultStyleFormats: Array<TinyStyleSheet> = [
+export type TinyStyleSheet = tinymce.RawEditorOptions['style_formats'];
+
+export const defaultStyleFormats: TinyStyleSheet = [
 	{
 		title: 'Headers',
 		items: [
@@ -27,7 +29,7 @@ export const defaultStyleFormats: Array<TinyStyleSheet> = [
 export const defaultExtendedValidElements =
 	'@[id|class|style],-div[id|dir|class|align|style],ins[datetime|cite],-ul[class|style],-li[class|style],-h1[id|dir|class|align|style],-h2[id|dir|class|align|style],-h3[id|dir|class|align|style],-h4[id|dir|class|align|style],-h5[id|dir|class|align|style],-h6[id|style|dir|class|align],span[id|class|style|lang],figure,figcaption';
 
-export const defaultFallbackConfig = {
+export const defaultFallbackConfig: tinymce.RawEditorOptions = {
 	toolbar: [
 		'styles',
 		'bold',
