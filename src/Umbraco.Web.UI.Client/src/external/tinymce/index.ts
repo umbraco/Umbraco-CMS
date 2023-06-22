347e63089563280b1c50db5470e171c24df5f8a7
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-06-22 14:16:11 +0200

    import tinymce as const and create a function to render the editor, which can be used from outside, and re-export the entire namespace for consumption (not just the const)

--- src/Umbraco.Web.UI.Client/src/external/tinymce/index.ts
+++ src/Umbraco.Web.UI.Client/src/external/tinymce/index.ts
@@ -1,1 +1,14 @@
+/* Import TinyMCE */
+import tinymce, { type RawEditorOptions } from 'tinymce';
+
+const defaultConfig: RawEditorOptions = {
+	base_url: '/tinymce',
+};
+
+/* Initialize TinyMCE */
+export function renderEditor(userConfig?: RawEditorOptions) {
+	const config = { ...defaultConfig, ...userConfig };
+	return tinymce.init(config);
+}
+
 export * as tinymce from 'tinymce';
