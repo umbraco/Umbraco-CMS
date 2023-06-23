5de488914eb74a23da0334a4b9e2a5422515cd63
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-06-23 14:54:23 +0200

    add toolbar config to default plugins

--- src/Umbraco.Web.UI.Client/src/packages/core/property-editors/uis/tiny-mce/plugins/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property-editors/uis/tiny-mce/plugins/manifests.ts
@@ -6,30 +6,80 @@ const pluginManifests: Array<ManifestTinyMcePlugin> = [
 		alias: 'Umb.TinyMcePlugin.CodeEditor',
 		name: 'Code Editor TinyMCE Plugin',
 		loader: () => import('./tiny-mce-code-editor.plugin.js'),
+		meta: {
+			toolbar: [
+				{
+					alias: 'sourcecode',
+					label: 'Source code editor',
+					icon: 'umb:code',
+				},
+			],
+		},
 	},
 	{
 		type: 'tinyMcePlugin',
 		alias: 'Umb.TinyMcePlugin.LinkPicker',
 		name: 'Link Picker TinyMCE Plugin',
 		loader: () => import('./tiny-mce-linkpicker.plugin.js'),
+		meta: {
+			toolbar: [
+				{
+					alias: 'link',
+					label: 'Insert/Edit link',
+					icon: 'umb:link',
+				},
+				{
+					alias: 'unlink',
+					label: 'Remove link',
+					icon: 'umb:link',
+				},
+			],
+		},
 	},
 	{
 		type: 'tinyMcePlugin',
 		alias: 'Umb.TinyMcePlugin.MediaPicker',
 		name: 'Media Picker TinyMCE Plugin',
 		loader: () => import('./tiny-mce-mediapicker.plugin.js'),
+		meta: {
+			toolbar: [
+				{
+					alias: 'umbmediapicker',
+					label: 'Media picker',
+					icon: 'umb:image',
+				},
+			],
+		},
 	},
 	{
 		type: 'tinyMcePlugin',
 		alias: 'Umb.TinyMcePlugin.EmbeddedMedia',
 		name: 'Embedded Media TinyMCE Plugin',
 		loader: () => import('./tiny-mce-embeddedmedia.plugin.js'),
+		meta: {
+			toolbar: [
+				{
+					alias: 'umbembeddialog',
+					label: 'Embed',
+					icon: 'umb:embed',
+				},
+			],
+		},
 	},
 	{
 		type: 'tinyMcePlugin',
 		alias: 'Umb.TinyMcePlugin.MacroPicker',
 		name: 'Macro Picker TinyMCE Plugin',
 		loader: () => import('./tiny-mce-macropicker.plugin.js'),
+		meta: {
+			toolbar: [
+				{
+					alias: 'umbmacro',
+					label: 'Macro',
+					icon: 'umb:preferences',
+				},
+			],
+		},
 	},
 ];
 
