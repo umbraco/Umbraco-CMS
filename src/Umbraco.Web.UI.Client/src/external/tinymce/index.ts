a876de325a13f4b2ac1aec1c1bd0cde405cfebc9
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-06-23 16:24:49 +0200

    load tiny with its default icons so that we bundle it correctly and can reference the icons from outside through the IconManager

--- src/Umbraco.Web.UI.Client/src/external/tinymce/index.ts
+++ src/Umbraco.Web.UI.Client/src/external/tinymce/index.ts
@@ -1,7 +1,23 @@
-/* Import TinyMCE */
+/**
+ * TinyMce is a CommonJS module, but in order to make @web/test-runner happy
+ * we need to load it as a module and then manually register it in the browser
+ * as a global variable, so that we can find it and use it in our tests.
+ * We are also loading the default icons, so that we can use them outside of a TinyMce instance.
+ */
 import * as tiny from 'tinymce';
 
-declare const tinymce: tiny.TinyMCE;
+// Declare a global variable to hold the TinyMCE instance
+declare global {
+	interface Window {
+		tinymce: typeof tiny.default;
+	}
+}
+
+// Register it globally so we can load the icons immediately
+window.tinymce = tiny as unknown as typeof tiny.default;
+
+// Load default icons making them available to everyone
+import 'tinymce/icons/default/icons.js';
 
 const defaultConfig: tiny.RawEditorOptions = {
 	base_url: '/umbraco/backoffice/tinymce',
@@ -10,7 +26,7 @@ const defaultConfig: tiny.RawEditorOptions = {
 /* Initialize TinyMCE */
 export function renderEditor(userConfig?: tiny.RawEditorOptions) {
 	const config = { ...defaultConfig, ...userConfig };
-	return tinymce.init(config);
+	return window.tinymce.init(config);
 }
 
 export * as tinymce from 'tinymce';
