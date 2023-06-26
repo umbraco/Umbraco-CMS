72e96ef84d8db96dd0fa7e74bed032d30a7df3f4
Andreas Zerbst <73799582+andr317c@users.noreply.github.com>
2023-06-26 12:32:41 +0200

    Added acceptance tests for testing the API for the Template section (#14450)
    * Added acceptance tests for testing the API for the Template section
    
    * Added acceptance tests for testing the API for the TemporaryFiles section (#14451)

---
+++ tests/Umbraco.Tests.AcceptanceTest/tests/ApiTesting/TemporaryFile/TemporaryFile.spec.ts
@@ -0,0 +1,35 @@
+﻿import {test} from "@umbraco/playwright-testhelpers";
+import {expect} from "@playwright/test";
+
+test.describe('Temporary File tests', () => {
+  const temporaryFileId = crypto.randomUUID();
+  const fileName = 'Umbraco.png';
+  const mimeType = 'image/png';
+  const filePath = './fixtures/mediaLibrary/Umbraco.png';
+
+  test.beforeEach(async ({page, umbracoApi}) => {
+    await umbracoApi.temporaryFile.ensureTemporaryFileWithIdNotExists(temporaryFileId);
+  });
+
+  test.afterEach(async ({page, umbracoApi}) => {
+    await umbracoApi.temporaryFile.ensureTemporaryFileWithIdNotExists(temporaryFileId);
+  });
+
+  test('can create temporary file', async ({page, umbracoApi, umbracoUi}) => {
+    await umbracoApi.temporaryFile.createTemporaryFile(temporaryFileId, fileName, mimeType, filePath);
+
+    // Assert
+    await expect(await umbracoApi.temporaryFile.doesTemporaryFileWithIdExist(temporaryFileId)).toBeTruthy();
+  });
+
+  test('can delete temporary file', async ({page, umbracoApi, umbracoUi}) => {
+    await umbracoApi.temporaryFile.createTemporaryFile(temporaryFileId, fileName, mimeType, filePath);
+
+    await expect(await umbracoApi.temporaryFile.getTemporaryFileById(temporaryFileId)).toBeTruthy();
+
+    await umbracoApi.temporaryFile.deleteTemporaryFileById(temporaryFileId);
+
+    // Assert
+    await expect(await umbracoApi.temporaryFile.doesTemporaryFileWithIdExist(temporaryFileId)).toBeFalsy();
+  });
+});
