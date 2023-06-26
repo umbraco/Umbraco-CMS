b0b29f05af4172c0adffd76825f7355e997cd53f
Andreas Zerbst <73799582+andr317c@users.noreply.github.com>
2023-06-26 10:13:36 +0200

    Added acceptance tests for testing the API for the Users section. (#14445)
    * Added auth to gitignore
    
    * Added a setup for logging in which allows us to save the token for the user
    
    * Added simple acceptance test for testing our api for the DataTypes
    
    * Added simple acceptance tests for testing the API for the PartialView section
    
    * Added simple acceptance tests for testing the API for the Script section
    
    * Added acceptance tests for testing the API for the Stylesheet section.
    
    * Added acceptance tests for testing the API for the User section
    
    * Bumped the version of our testhelpers!

---
+++ tests/Umbraco.Tests.AcceptanceTest/tests/ApiTesting/User/UserAvatar.spec.ts
@@ -0,0 +1,68 @@
+﻿import {test} from '@umbraco/playwright-testhelpers';
+import {expect} from "@playwright/test";
+
+test.describe('User Avatar Tests', () => {
+  // User
+  const userEmail = "userAvatar@email.com";
+  const userName = "UserAvatarTests";
+  // Avatar
+  // Creates a random GUID
+  const avatarFileId = crypto.randomUUID();
+  const avatarName = 'Umbraco.png';
+  const mimeType = 'image/png';
+  const avatarFilePath = './fixtures/mediaLibrary/Umbraco.png';
+
+  test.beforeEach(async ({page, umbracoApi}) => {
+    await umbracoApi.user.ensureUserNameNotExists(userName);
+    await umbracoApi.temporaryFile.ensureTemporaryFileWithIdNotExists(avatarFileId);
+  });
+
+  test.afterEach(async ({page, umbracoApi}) => {
+    await umbracoApi.user.ensureUserNameNotExists(userName);
+    await umbracoApi.temporaryFile.ensureTemporaryFileWithIdNotExists(avatarFileId);
+  });
+
+  test('can add an avatar to a user', async ({page, umbracoApi, umbracoUi}) => {
+    const userGroup = await umbracoApi.userGroup.getUserGroupByName("Writers");
+
+    const userGroupData = [userGroup.id];
+
+    await umbracoApi.user.createUser(userEmail, userName, userGroupData);
+
+    const userData = await umbracoApi.user.getUserByName(userName);
+
+    await umbracoApi.temporaryFile.createTemporaryFile(avatarFileId, avatarName, mimeType, avatarFilePath);
+
+    await umbracoApi.user.addAvatarToUserWithId(userData.id, avatarFileId);
+
+    // Assert
+    // Checks if the avatar was added to the user
+    const userDataWithAvatar = await umbracoApi.user.getUserByName(userName);
+    await expect(userDataWithAvatar.avatarUrls.length !== 0).toBeTruthy();
+  });
+
+  test('can remove an avatar from a user', async ({page, umbracoApi, umbracoUi}) => {
+    const userGroup = await umbracoApi.userGroup.getUserGroupByName("Writers");
+
+    const userGroupData = [userGroup.id];
+
+    await umbracoApi.user.createUser(userEmail, userName, userGroupData);
+
+    const userData = await umbracoApi.user.getUserByName(userName);
+
+    await umbracoApi.temporaryFile.createTemporaryFile(avatarFileId, avatarName, mimeType, avatarFilePath);
+
+    await umbracoApi.user.addAvatarToUserWithId(userData.id, avatarFileId);
+
+    // Checks if the avatar was added to the user
+    const userDataWithAvatar = await umbracoApi.user.getUserByName(userName);
+    await expect(userDataWithAvatar.avatarUrls.length !== 0).toBeTruthy();
+
+    await umbracoApi.user.removeAvatarFromUserWithId(userData.id);
+
+    // Assert
+    // Checks if the avatar was removed from the user
+    const userDataWithoutAvatar = await umbracoApi.user.getUserByName(userName);
+    await expect(userDataWithoutAvatar.avatarUrls.length == 0).toBeTruthy();
+  });
+});
