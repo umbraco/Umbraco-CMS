151ae7573df7b03682a006287f3b4f21c5a43061
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-06-26 09:10:22 +0200

    import tinymce in test for toolbar to make sure the global is registered

--- src/Umbraco.Web.UI.Client/src/packages/core/property-editors/uis/tiny-mce/config/toolbar/property-editor-ui-tiny-mce-toolbar-configuration.test.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property-editors/uis/tiny-mce/config/toolbar/property-editor-ui-tiny-mce-toolbar-configuration.test.ts
@@ -1,21 +1,24 @@
 import { expect, fixture, html } from '@open-wc/testing';
 import { UmbPropertyEditorUITinyMceToolbarConfigurationElement } from './property-editor-ui-tiny-mce-toolbar-configuration.element.js';
 import { defaultA11yConfig } from '@umbraco-cms/internal/test-utils';
+import '@umbraco-cms/backoffice/external/tinymce';
 
 describe('UmbPropertyEditorUITinyMceToolbarConfigurationElement', () => {
-  let element: UmbPropertyEditorUITinyMceToolbarConfigurationElement;
+	let element: UmbPropertyEditorUITinyMceToolbarConfigurationElement;
 
-  beforeEach(async () => {
-    element = await fixture(
-      html` <umb-property-editor-ui-tiny-mce-toolbar-configuration></umb-property-editor-ui-tiny-mce-toolbar-configuration> `
-    );
-  });
+	beforeEach(async () => {
+		element = await fixture(
+			html`
+				<umb-property-editor-ui-tiny-mce-toolbar-configuration></umb-property-editor-ui-tiny-mce-toolbar-configuration>
+			`
+		);
+	});
 
-  it('is defined with its own instance', () => {
-    expect(element).to.be.instanceOf(UmbPropertyEditorUITinyMceToolbarConfigurationElement);
-  });
+	it('is defined with its own instance', () => {
+		expect(element).to.be.instanceOf(UmbPropertyEditorUITinyMceToolbarConfigurationElement);
+	});
 
-  it('passes the a11y audit', async () => {
-    await expect(element).shadowDom.to.be.accessible(defaultA11yConfig);
-  });
+	it('passes the a11y audit', async () => {
+		await expect(element).shadowDom.to.be.accessible(defaultA11yConfig);
+	});
 });
