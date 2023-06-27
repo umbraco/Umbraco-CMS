fd9c460076c7c0558101697e10a63066feb4e16d
Niels Lyngsø <niels.lyngso@gmail.com>
2023-06-27 11:52:32 +0200

    move responsibility of newing up language context

--- src/Umbraco.Web.UI.Client/src/packages/settings/package-entry-point.ts
+++ src/Umbraco.Web.UI.Client/src/packages/settings/package-entry-point.ts
@@ -1,14 +1,12 @@
-import {
-	UMB_APP_LANGUAGE_CONTEXT_TOKEN,
-	UmbAppLanguageContext,
-} from './languages/app-language-select/app-language.context.js';
+import { UmbAppLanguageContext } from './languages/app-language-select/app-language.context.js';
 import { UmbThemeContext } from './themes/theme.context.js';
 import { manifests } from './manifests.js';
 import type { UmbEntryPointOnInit } from '@umbraco-cms/backoffice/extension-api';
-import { UmbContextProviderController } from '@umbraco-cms/backoffice/context-api';
 
 export const onInit: UmbEntryPointOnInit = (host, extensionRegistry) => {
 	extensionRegistry.registerMany(manifests);
-	new UmbContextProviderController(host, UMB_APP_LANGUAGE_CONTEXT_TOKEN, new UmbAppLanguageContext(host));
+
+	// TODO: Move these to a extension type, that will new them up?
+	new UmbAppLanguageContext(host);
 	new UmbThemeContext(host);
 };
