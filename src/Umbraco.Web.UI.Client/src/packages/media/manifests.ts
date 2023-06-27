c0c27c0d1dc5254ff5203eb30f8ee3e794cce56a
Niels Lyngsø <niels.lyngso@gmail.com>
2023-06-27 11:35:38 +0200

    media bundle

--- src/Umbraco.Web.UI.Client/src/packages/media/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/manifests.ts
@@ -2,10 +2,5 @@ import { manifests as mediaSectionManifests } from './section.manifests.js';
 import { manifests as mediaMenuManifests } from './menu.manifests.js';
 import { manifests as mediaManifests } from './media/manifests.js';
 import { manifests as mediaTypesManifests } from './media-types/manifests.js';
-import type { UmbEntryPointOnInit } from '@umbraco-cms/backoffice/extension-api';
 
 export const manifests = [...mediaSectionManifests, ...mediaMenuManifests, ...mediaManifests, ...mediaTypesManifests];
-
-export const onInit: UmbEntryPointOnInit = (_host, extensionRegistry) => {
-	extensionRegistry.registerMany(manifests);
-};
