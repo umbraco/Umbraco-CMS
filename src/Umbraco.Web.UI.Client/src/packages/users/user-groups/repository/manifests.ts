e1824495a13cb3ee2e6a5ed1dad9bdac0a9ad275
Niels Lyngsø <niels.lyngso@gmail.com>
2023-06-27 13:30:49 +0200

    re introduce USER_GROUP_REPOSITORY_ALIAS

--- src/Umbraco.Web.UI.Client/src/packages/users/user-groups/repository/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/users/user-groups/repository/manifests.ts
@@ -3,9 +3,11 @@ import { UmbUserGroupItemStore } from './user-group-item.store.js';
 import { UmbUserGroupStore } from './user-group.store.js';
 import type { ManifestStore, ManifestRepository, ManifestItemStore } from '@umbraco-cms/backoffice/extension-registry';
 
+export const USER_GROUP_REPOSITORY_ALIAS = 'Umb.Repository.UserGroup';
+
 const repository: ManifestRepository = {
 	type: 'repository',
-	alias: 'Umb.Repository.UserGroup',
+	alias: USER_GROUP_REPOSITORY_ALIAS,
 	name: 'User Group Repository',
 	class: UmbUserGroupRepository,
 };
