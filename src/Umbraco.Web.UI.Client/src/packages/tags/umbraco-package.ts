3f955bb37b80b838d9d66609178d2ee5a1ad56ff
Niels Lyngsø <niels.lyngso@gmail.com>
2023-06-27 12:53:39 +0200

    tags bundle

--- src/Umbraco.Web.UI.Client/src/packages/tags/umbraco-package.ts
+++ src/Umbraco.Web.UI.Client/src/packages/tags/umbraco-package.ts
@@ -2,9 +2,9 @@ export const name = 'Umbraco.Core.UserManagement';
 export const version = '0.0.1';
 export const extensions = [
 	{
-		name: 'Tags Management Entry Point',
-		alias: 'Umb.EntryPoint.TagsManagement',
-		type: 'entryPoint',
-		loader: () => import('./package-entry-point.js'),
+		name: 'Tags Management Bundle',
+		alias: 'Umb.Bundle.TagsManagement',
+		type: 'bundle',
+		loader: () => import('./manifests.js'),
 	},
 ];
