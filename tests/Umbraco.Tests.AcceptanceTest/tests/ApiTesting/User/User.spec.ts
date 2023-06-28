7928cf433868ed7e89af9432e9e397a68e65a8ab
Andreas Zerbst <andr317c@live.dk>
2023-06-28 15:49:54 +0200

    Updated our user API tests to use our updated testHelpers

--- tests/Umbraco.Tests.AcceptanceTest/tests/ApiTesting/User/User.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/ApiTesting/User/User.spec.ts
@@ -2,38 +2,39 @@
 import {expect} from "@playwright/test";
 
 test.describe('User Tests', () => {
+  let userId = "";
   const userEmail = "user@email.com";
   const userName = "UserTests";
 
   test.beforeEach(async ({page, umbracoApi}) => {
-    await umbracoApi.user.ensureUserNameNotExists(userName);
+    await umbracoApi.user.ensureNameNotExists(userName);
   });
 
   test.afterEach(async ({page, umbracoApi}) => {
-    await umbracoApi.user.ensureUserNameNotExists(userName);
+    await umbracoApi.user.delete(userId);
   });
 
   test('can create a user', async ({page, umbracoApi, umbracoUi}) => {
     // Gets the id for the writers userGroup
-    const userGroup = await umbracoApi.userGroup.getUserGroupByName("Writers");
+    const userGroup = await umbracoApi.userGroup.getByName("Writers");
     const userGroupId = [userGroup.id];
 
-    await umbracoApi.user.createUser(userEmail, userName, userGroupId);
+    userId = await umbracoApi.user.create(userEmail, userName, userGroupId);
 
     // Assert
-    await expect(await umbracoApi.user.doesUserWithNameExist(userName)).toBeTruthy();
+    await expect(await umbracoApi.user.exists(userId)).toBeTruthy();
   });
 
   test('can update a user', async ({page, umbracoApi, umbracoUi}) => {
     // Gets userGroup data for Writers and Translators
-    const userGroup = await umbracoApi.userGroup.getUserGroupByName("Writers");
-    const anotherUserGroup = await umbracoApi.userGroup.getUserGroupByName("Translators");
+    const userGroup = await umbracoApi.userGroup.getByName("Writers");
+    const anotherUserGroup = await umbracoApi.userGroup.getByName("Translators");
 
     const userGroupId = [userGroup.id];
 
-    await umbracoApi.user.createUser(userEmail, userName, userGroupId);
+    userId = await umbracoApi.user.create(userEmail, userName, userGroupId);
 
-    const userData = await umbracoApi.user.getUserByName(userName);
+    const userData = await umbracoApi.user.get(userId);
 
     const newUserGroupData = [
       userGroup.id,
@@ -42,29 +43,29 @@ test.describe('User Tests', () => {
 
     userData.userGroupIds = newUserGroupData;
 
-    await umbracoApi.user.updateUserById(userData.id, userData);
+    await umbracoApi.user.update(userId, userData);
 
     // Assert
-    await umbracoApi.user.doesUserWithNameExist(userName);
+    await umbracoApi.user.exists(userId);
     // Checks if the user was updated with another userGroupID
-    const updatedUser = await umbracoApi.user.getUserByName(userName);
-    await expect(updatedUser.userGroupIds.toString() == newUserGroupData.toString()).toBeTruthy();
+    const updatedUser = await umbracoApi.user.get(userId);
+    await expect(updatedUser.userGroupIds.toString()).toEqual(newUserGroupData.toString());
   });
 
   test('can delete a user', async ({page, umbracoApi, umbracoUi}) => {
-    const userGroupData = await umbracoApi.userGroup.getUserGroupByName("Writers");
+    const userGroupData = await umbracoApi.userGroup.getByName("Writers");
 
     const userData = [
       userGroupData.id
     ];
 
-    await umbracoApi.user.createUser(userEmail, userName, userData);
+    userId = await umbracoApi.user.create(userEmail, userName, userData);
 
-    await expect(await umbracoApi.user.doesUserWithNameExist(userName)).toBeTruthy();
+    await expect(await umbracoApi.user.exists(userId)).toBeTruthy();
 
-    await umbracoApi.user.deleteUserByName(userName);
+    await umbracoApi.user.delete(userId);
 
     // Assert
-    await expect(await umbracoApi.user.doesUserWithNameExist(userName)).toBeFalsy();
+    await expect(await umbracoApi.user.exists(userId)).toBeFalsy();
   });
 });
