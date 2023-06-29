486b8935d0b23cde113a1f0757cc62d57f9f9052
Zeegaan <nge@umbraco.dk>
2023-06-29 14:20:08 +0200

    Implement PublicAccessResponseModel

---
+++ src/Umbraco.Cms.Api.Management/ViewModels/PublicAccess/PublicAccessResponseModel.cs
@@ -0,0 +1,16 @@
+﻿using Umbraco.Cms.Api.Management.ViewModels.Member.Item;
+using Umbraco.Cms.Api.Management.ViewModels.MemberGroup.Item;
+using Umbraco.Cms.Api.Management.ViewModels.Tree;
+
+namespace Umbraco.Cms.Api.Management.ViewModels.PublicAccess;
+
+public class PublicAccessResponseModel
+{
+    public MemberItemResponseModel[] Members { get; set; } = Array.Empty<MemberItemResponseModel>();
+
+    public MemberGroupItemReponseModel[] MemberGroups { get; set; } = Array.Empty<MemberGroupItemReponseModel>();
+
+    public ContentTreeItemResponseModel? LoginPage { get; set; }
+
+    public ContentTreeItemResponseModel? ErrorPage { get; set; }
+}
