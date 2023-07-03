eb407885943882cf5f0c1eafaa058b060bff774c
Zeegaan <nge@umbraco.dk>
2023-07-03 13:46:20 +0200

    Remove CreatePublicAccessEntry from presentation factory

---
+++ src/Umbraco.Cms.Api.Management/Controllers/Document/CreatePublicAccessDocumentController.cs
@@ -0,0 +1,34 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Management.Factories;
+using Umbraco.Cms.Api.Management.ViewModels.PublicAccess;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Mapping;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Services.OperationStatus;
+
+namespace Umbraco.Cms.Api.Management.Controllers.Document;
+
+public class CreatePublicAccessDocumentController : DocumentControllerBase
+{
+    private readonly IPublicAccessService _publicAccessService;
+    private readonly IUmbracoMapper _mapper;
+
+    public CreatePublicAccessDocumentController(IPublicAccessService publicAccessService, IUmbracoMapper mapper)
+    {
+        _publicAccessService = publicAccessService;
+        _mapper = mapper;
+    }
+
+    [MapToApiVersion("1.0")]
+    [HttpPost("public-access")]
+    public async Task<IActionResult> CreatePublicAccess(PublicAccessRequestModel publicAccessRequestModel)
+    {
+        PublicAccessEntrySlim publicAccessEntrySlim = _mapper.Map<PublicAccessEntrySlim>(publicAccessRequestModel)!;
+
+        Attempt<PublicAccessOperationStatus> saveAttempt = await _publicAccessService.SaveAsync(publicAccessEntrySlim);
+
+        return saveAttempt.Success ? Ok() : PublicAccessOperationStatusResult(saveAttempt.Result);
+    }
+}
--- src/Umbraco.Cms.Api.Management/Factories/IPublicAccessPresentationFactory.cs
+++ src/Umbraco.Cms.Api.Management/Factories/IPublicAccessPresentationFactory.cs
@@ -8,6 +8,5 @@ namespace Umbraco.Cms.Api.Management.Factories;
 public interface IPublicAccessPresentationFactory
 {
     Task<Attempt<PublicAccessResponseModel?, PublicAccessOperationStatus>> CreatePublicAccessResponseModel(PublicAccessEntry entry);
-    Task<Attempt<PublicAccessEntry?, PublicAccessOperationStatus>> CreatePublicAccessEntry(PublicAccessRequestModel requestModel);
 
 }
