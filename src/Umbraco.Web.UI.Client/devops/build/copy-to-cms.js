441dd93f5ca4a5956f3fadcf4f67a6e24ff1c242
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-07-03 14:17:29 +0200

    clean outputDir before copy

--- src/Umbraco.Web.UI.Client/devops/build/copy-to-cms.js
+++ src/Umbraco.Web.UI.Client/devops/build/copy-to-cms.js
@@ -1,6 +1,7 @@
-import { cpSync } from 'fs';
+import { cpSync, rmSync } from 'fs';
 
 const srcDir = './dist-cms';
 const outputDir = '../Umbraco.Cms.StaticAssets/wwwroot/umbraco/backoffice';
 
+rmSync(outputDir, { recursive: true });
 cpSync(srcDir, outputDir, { recursive: true });
