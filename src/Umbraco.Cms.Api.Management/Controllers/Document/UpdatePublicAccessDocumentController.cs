327aca805d7dc491308fb606afd739ee80847bca
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2023-07-04 09:48:47 +0200

    Use new service layer

--- src/Umbraco.Cms.Api.Management/Controllers/Document/UpdatePublicAccessDocumentController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/Document/UpdatePublicAccessDocumentController.cs
@@ -5,7 +5,6 @@ using Umbraco.Cms.Api.Management.Factories;
 using Umbraco.Cms.Api.Management.ViewModels.PublicAccess;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Models;
-using Umbraco.Cms.Core.Models.ContentEditing;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Core.Services.OperationStatus;
 
@@ -29,14 +28,10 @@ public class UpdatePublicAccessDocumentController : DocumentControllerBase
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Create(Guid id, PublicAccessRequestModel requestModel)
     {
-        Attempt<PublicAccessEntry?, PublicAccessOperationStatus> publicAccessEntryAttempt = await _publicAccessPresentationFactory.CreatePublicAccessEntry(requestModel, id);
-        if (publicAccessEntryAttempt.Success is false)
-        {
-            return PublicAccessOperationStatusResult(publicAccessEntryAttempt.Status);
-        }
+        PublicAccessEntrySlim publicAccessEntrySlim = _publicAccessPresentationFactory.CreatePublicAccessEntrySlim(requestModel, id);
 
-        await _publicAccessService.UpdateAsync(publicAccessEntryAttempt.Result!);
+        Attempt<PublicAccessEntry?, PublicAccessOperationStatus> updateAttempt = await _publicAccessService.UpdateAsync(publicAccessEntrySlim);
 
-        return Ok(publicAccessEntryAttempt.Result);
+        return updateAttempt.Success ? Ok() : PublicAccessOperationStatusResult(updateAttempt.Status);
     }
 }
