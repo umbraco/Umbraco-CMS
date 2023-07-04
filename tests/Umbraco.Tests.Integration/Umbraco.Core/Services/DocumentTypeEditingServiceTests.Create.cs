97f7a5148c23f5afd74b60be7fff597e78d4b3d3
nikolajlauridsen <nikolajlauridsen@protonmail.ch>
2023-07-04 11:12:39 +0200

    Add first test

---
+++ tests/Umbraco.Tests.Integration/Umbraco.Core/Services/DocumentTypeEditingServiceTests.Create.cs
@@ -0,0 +1,35 @@
+﻿using NUnit.Framework;
+using Umbraco.Cms.Api.Management.Controllers.Template;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Models.ContentTypeEditing.Document;
+
+namespace Umbraco.Cms.Tests.Integration.Umbraco.Core.Services;
+
+public partial class DocumentTypeEditingServiceTests
+{
+
+    [Test]
+    public async Task Can_Create_Basic_DocumentType()
+    {
+        var name = "Test";
+        var alias = "test";
+
+        var createModel = new DocumentTypeCreateModel { Alias = alias, Name = name };
+        createModel.IsElement = true;
+
+        var response = await DocumentTypeEditingService.CreateAsync(createModel, Constants.Security.SuperUserKey);
+
+        // Ensure it's actually persisted
+        var documentType = await ContentTypeService.GetAsync(response.Result!.Key);
+
+        Assert.Multiple(() =>
+        {
+            Assert.IsTrue(response.Success);
+            Assert.IsNotNull(documentType);
+            Assert.AreEqual(alias, documentType.Alias);
+            Assert.AreEqual(name, documentType.Name);
+            Assert.AreEqual(response.Result.Id, documentType.Id);
+            Assert.AreEqual(response.Result.Key, documentType.Key);
+        });
+    }
+}
---
+++ tests/Umbraco.Tests.Integration/Umbraco.Core/Services/DocumentTypeEditingServiceTests.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Services.ContentTypeEditing;
+using Umbraco.Cms.Tests.Common.Testing;
+using Umbraco.Cms.Tests.Integration.Testing;
+
+namespace Umbraco.Cms.Tests.Integration.Umbraco.Core.Services;
+
+[TestFixture]
+[UmbracoTest(
+    Database = UmbracoTestOptions.Database.NewSchemaPerTest,
+    PublishedRepositoryEvents = true,
+    WithApplication = true)]
+public partial class DocumentTypeEditingServiceTests : UmbracoIntegrationTest
+{
+    private IDocumentTypeEditingService DocumentTypeEditingService => GetRequiredService<IDocumentTypeEditingService>();
+
+    private IContentTypeService ContentTypeService => GetRequiredService<IContentTypeService>();
+}
