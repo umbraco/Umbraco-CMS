35f231384c70778a9717ae300e83f981c3fe28bf
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-07-07 15:49:23 +0200

    import from external package

--- src/Umbraco.Web.UI.Client/src/packages/umbraco-news/umbraco-news-dashboard.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/umbraco-news/umbraco-news-dashboard.element.ts
@@ -1,4 +1,4 @@
-import { UUITextStyles } from '@umbraco-ui/uui-css';
+import { UUITextStyles } from '@umbraco-cms/backoffice/external/uui';
 import { UMB_AUTH } from '@umbraco-cms/backoffice/auth';
 import { css, html, customElement, state } from '@umbraco-cms/backoffice/external/lit';
 import { UmbLitElement } from '@umbraco-cms/internal/lit-element';
