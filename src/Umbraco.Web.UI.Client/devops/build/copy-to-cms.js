f927e42ffad747e14ff71021917503aabe59340e
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-07-10 11:22:05 +0200

    force delete to avoid errors if directory does not exist

--- src/Umbraco.Web.UI.Client/devops/build/copy-to-cms.js
+++ src/Umbraco.Web.UI.Client/devops/build/copy-to-cms.js
@@ -3,5 +3,5 @@ import { cpSync, rmSync } from 'fs';
 const srcDir = './dist-cms';
 const outputDir = '../Umbraco.Cms.StaticAssets/wwwroot/umbraco/backoffice';
 
-rmSync(outputDir, { recursive: true });
+rmSync(outputDir, { recursive: true, force: true });
 cpSync(srcDir, outputDir, { recursive: true });
