89609b2f2d4c0e93b2859eefdf6e3b36daa19186
Zeegaan <nge@umbraco.dk>
2023-07-10 14:20:03 +0200

    Add missing namespace to migration in plan

--- src/Umbraco.Infrastructure/Migrations/Upgrade/UmbracoPlan.cs
+++ src/Umbraco.Infrastructure/Migrations/Upgrade/UmbracoPlan.cs
@@ -86,7 +86,7 @@ public class UmbracoPlan : MigrationPlan
         // To 14.0.0
         To<V_14_0_0.AddPropertyEditorUiAliasColumn>("{419827A0-4FCE-464B-A8F3-247C6092AF55}");
         To<V_14_0_0.MigrateDataTypeConfigurations>("{5F15A1CC-353D-4889-8C7E-F303B4766196}");
-        To<AddGuidsToUserGroups>("{69E12556-D9B3-493A-8E8A-65EC89FB658D}");
+        To<V_14_0_0.AddGuidsToUserGroups>("{69E12556-D9B3-493A-8E8A-65EC89FB658D}");
         To<V_14_0_0.AddUserGroup2PermisionTable>("{F2B16CD4-F181-4BEE-81C9-11CF384E6025}");
         To<V_14_0_0.AddGuidsToUsers>("{A8E01644-9F2E-4988-8341-587EF5B7EA69}");
         To<V_14_0_0.UpdateDefaultGuidsOfCreatedPackages>("{E073DBC0-9E8E-4C92-8210-9CB18364F46E}");
