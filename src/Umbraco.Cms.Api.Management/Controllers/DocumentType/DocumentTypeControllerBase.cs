fbe5e3bc513cf418ef2f903e3801e2b8a9f9e39f
Elitsa <elm@umbraco.dk>
2023-07-13 08:50:35 +0300

    DocumentType controllers policies

--- src/Umbraco.Cms.Api.Management/Controllers/DocumentType/DocumentTypeControllerBase.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/DocumentType/DocumentTypeControllerBase.cs
@@ -1,13 +1,15 @@
-﻿using Asp.Versioning;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Api.Management.Routing;
 using Umbraco.Cms.Core;
+using Umbraco.Cms.Web.Common.Authorization;
 
 namespace Umbraco.Cms.Api.Management.Controllers.DocumentType;
 
 [ApiController]
 [VersionedApiBackOfficeRoute(Constants.UdiEntityType.DocumentType)]
 [ApiExplorerSettings(GroupName = "Document Type")]
+[Authorize(Policy = "New" + AuthorizationPolicies.TreeAccessDocumentTypes)]
 public abstract class DocumentTypeControllerBase : ManagementApiControllerBase
 {
 }
