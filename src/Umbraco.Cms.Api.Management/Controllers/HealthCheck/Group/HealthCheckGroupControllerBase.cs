be68a40997d5b8ee382891f8e6f6e42ecf97fa4a
Elitsa <elm@umbraco.dk>
2023-07-13 08:51:17 +0300

    HealthCheck controllers policies

--- src/Umbraco.Cms.Api.Management/Controllers/HealthCheck/Group/HealthCheckGroupControllerBase.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/HealthCheck/Group/HealthCheckGroupControllerBase.cs
@@ -1,6 +1,7 @@
-using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Api.Management.Routing;
+using Umbraco.Cms.Web.Common.Authorization;
 using Constants = Umbraco.Cms.Core.Constants;
 
 namespace Umbraco.Cms.Api.Management.Controllers.HealthCheck.Group;
@@ -8,6 +9,7 @@ namespace Umbraco.Cms.Api.Management.Controllers.HealthCheck.Group;
 [ApiController]
 [VersionedApiBackOfficeRoute($"{Constants.HealthChecks.RoutePath.HealthCheck}-group")]
 [ApiExplorerSettings(GroupName = "Health Check")]
+[Authorize(Policy = "New" + AuthorizationPolicies.SectionAccessSettings)]
 public abstract class HealthCheckGroupControllerBase : ManagementApiControllerBase
 {
 }
