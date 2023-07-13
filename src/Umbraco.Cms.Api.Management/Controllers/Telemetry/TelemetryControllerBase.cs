6691c943085e71f26ccc5d7327223b64eeca2b67
Elitsa <elm@umbraco.dk>
2023-07-13 09:06:32 +0300

    Telemetry controllers policies

--- src/Umbraco.Cms.Api.Management/Controllers/Telemetry/TelemetryControllerBase.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/Telemetry/TelemetryControllerBase.cs
@@ -1,12 +1,14 @@
-﻿using Asp.Versioning;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Api.Management.Routing;
+using Umbraco.Cms.Web.Common.Authorization;
 
 namespace Umbraco.Cms.Api.Management.Controllers.Telemetry;
 
 [ApiController]
 [VersionedApiBackOfficeRoute("telemetry")]
 [ApiExplorerSettings(GroupName = "Telemetry")]
+[Authorize(Policy = "New" + AuthorizationPolicies.SectionAccessSettings)]
 public abstract class TelemetryControllerBase : ManagementApiControllerBase
 {
 }
