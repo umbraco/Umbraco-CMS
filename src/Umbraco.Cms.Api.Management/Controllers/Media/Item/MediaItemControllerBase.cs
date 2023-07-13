095f6b8b0250c603b38b9190d056b49f731e8b31
Elitsa <elm@umbraco.dk>
2023-07-13 08:54:49 +0300

    Media controllers policies

--- src/Umbraco.Cms.Api.Management/Controllers/Media/Item/MediaItemControllerBase.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/Media/Item/MediaItemControllerBase.cs
@@ -1,7 +1,9 @@
 ﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Api.Management.Routing;
 using Umbraco.Cms.Core;
+using Umbraco.Cms.Web.Common.Authorization;
 
 namespace Umbraco.Cms.Api.Management.Controllers.Media.Item;
 
@@ -9,6 +11,7 @@ namespace Umbraco.Cms.Api.Management.Controllers.Media.Item;
 [ApiController]
 [VersionedApiBackOfficeRoute($"{Constants.UdiEntityType.Media}")]
 [ApiExplorerSettings(GroupName = nameof(Constants.UdiEntityType.Media))]
+[Authorize(Policy = "New" + AuthorizationPolicies.SectionAccessForMediaTree)]
 public class MediaItemControllerBase : ManagementApiControllerBase
 {
 }
