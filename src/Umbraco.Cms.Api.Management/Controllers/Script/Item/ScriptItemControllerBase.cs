618614027da77d94761980d19d1e9d0537196417
Elitsa <elm@umbraco.dk>
2023-07-13 09:05:22 +0300

    Script controllers policies

--- src/Umbraco.Cms.Api.Management/Controllers/Script/Item/ScriptItemControllerBase.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/Script/Item/ScriptItemControllerBase.cs
@@ -1,13 +1,15 @@
-﻿using Asp.Versioning;
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Api.Management.Routing;
 using Umbraco.Cms.Core;
+using Umbraco.Cms.Web.Common.Authorization;
 
 namespace Umbraco.Cms.Api.Management.Controllers.Script.Item;
 
 [ApiController]
 [VersionedApiBackOfficeRoute($"{Constants.UdiEntityType.Script}")]
 [ApiExplorerSettings(GroupName = nameof(Constants.UdiEntityType.Script))]
+[Authorize(Policy = "New" + AuthorizationPolicies.TreeAccessScripts)]
 public class ScriptItemControllerBase : ManagementApiControllerBase
 {
 }
