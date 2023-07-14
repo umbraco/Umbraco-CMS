8cac749d6e79cb2ad3efe798daa689d30d424a79
Jesper Møller Jensen <26099018+JesmoDev@users.noreply.github.com>
2023-07-14 16:07:17 +1200

    ts-ignore

--- src/Umbraco.Web.UI.Login/mock.ts
+++ src/Umbraco.Web.UI.Login/mock.ts
@@ -1,6 +1,6 @@
 import { startMockServiceWorker } from "./mocks/index.js";
 
-//NOTE: Don't know why typescript is complaining about this, but it works.
+// @ts-ignore - NOTE: Don't know why typescript is complaining about this, but it works.
 const isMocking = import.meta.env.VITE_UMBRACO_AUTH_USE_MSW;
 
 if (isMocking) {
