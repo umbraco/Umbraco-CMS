4b88aa0f14eabe10b90b9f91890fb882e09c2a52
Julia Gru <56249914+julczka@users.noreply.github.com>
2023-07-18 09:29:02 +0200

    cleanup

--- src/Umbraco.Web.UI.Client/src/packages/core/tree/file-system-tree-item/file-system-tree-item.context.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/tree/file-system-tree-item/file-system-tree-item.context.ts
@@ -2,7 +2,6 @@ import { UmbTreeItemContextBase } from '../tree-item-base/tree-item-base.context
 import { UmbControllerHostElement } from '@umbraco-cms/backoffice/controller-api';
 import { FileSystemTreeItemPresentationModel } from '@umbraco-cms/backoffice/backend-api';
 
-// TODO get unique method from an entity repository static method
 export class UmbFileSystemTreeItemContext extends UmbTreeItemContextBase<FileSystemTreeItemPresentationModel> {
 	constructor(host: UmbControllerHostElement) {
 		super(host, (x: FileSystemTreeItemPresentationModel) => x.path);
