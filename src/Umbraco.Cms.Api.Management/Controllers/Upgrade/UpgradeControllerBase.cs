397ed5eb906e15340711d18378a9afaba0f8c11a
Elitsa <elm@umbraco.dk>
2023-07-18 11:30:57 +0300

    Revery UpgradeControllerBase attribute

--- src/Umbraco.Cms.Api.Management/Controllers/Upgrade/UpgradeControllerBase.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/Upgrade/UpgradeControllerBase.cs
@@ -1,12 +1,12 @@
-﻿using Microsoft.AspNetCore.Authorization;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Api.Management.Filters;
 using Umbraco.Cms.Api.Management.Routing;
 
 namespace Umbraco.Cms.Api.Management.Controllers.Upgrade;
 
-[AllowAnonymous]
+// TODO: This needs to be an authorized (at admin level) controller.
+
 [ApiController]
 [RequireRuntimeLevel(RuntimeLevel.Upgrade)]
 [VersionedApiBackOfficeRoute("upgrade")]
