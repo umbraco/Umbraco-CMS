9bb8d2030e6f4c117eb651763164c1f59d00ef6e
Jesper Møller Jensen <26099018+JesmoDev@users.noreply.github.com>
2023-07-19 16:24:45 +1200

    fix interface error

--- src/Umbraco.Web.UI.Login/src/context/auth.context.ts
+++ src/Umbraco.Web.UI.Login/src/context/auth.context.ts
@@ -1,6 +1,22 @@
-import { IUmbAuthContext, LoginRequestModel, LoginResponse } from '../types.ts';
+import {
+	IUmbAuthContext,
+	LoginRequestModel,
+	LoginResponse,
+	NewPasswordResponse,
+	ResetPasswordResponse,
+	ValidatePasswordResetCodeResponse,
+} from '../types.ts';
 
 export class UmbAuthContext implements IUmbAuthContext {
+	resetPassword(username: string): Promise<ResetPasswordResponse> {
+		throw new Error('Method not implemented.');
+	}
+	validatePasswordResetCode(code: string): Promise<ValidatePasswordResetCodeResponse> {
+		throw new Error('Method not implemented.');
+	}
+	newPassword(password: string, code: string): Promise<NewPasswordResponse> {
+		throw new Error('Method not implemented.');
+	}
 	supportsPersistLogin = false;
 
 	login(_data: LoginRequestModel): Promise<LoginResponse> {
