a2f24a46f3e24a620052329db3b16b77c6a2fac1
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2023-07-26 12:24:16 +0200

    Create publish controller

---
+++ src/Umbraco.Cms.Api.Management/Controllers/Document/PublishDocumentController.cs
@@ -0,0 +1,34 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Security;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Services.OperationStatus;
+
+namespace Umbraco.Cms.Api.Management.Controllers.Document;
+
+public class PublishDocumentController : DocumentControllerBase
+{
+    private readonly IContentEditingService _contentEditingService;
+    private readonly IBackOfficeSecurityAccessor _backOfficeSecurityAccessor;
+
+    public PublishDocumentController(IContentEditingService contentEditingService, IBackOfficeSecurityAccessor backOfficeSecurityAccessor)
+    {
+        _contentEditingService = contentEditingService;
+        _backOfficeSecurityAccessor = backOfficeSecurityAccessor;
+    }
+
+    [HttpPut("publish/{id:guid}")]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status404NotFound)]
+    public async Task<IActionResult> Publish(Guid id)
+    {
+        Attempt<ContentEditingOperationStatus> attempt = await _contentEditingService.PublishAsync(id, CurrentUserKey(_backOfficeSecurityAccessor));
+
+        return attempt.Success ? Ok() : ContentEditingOperationStatusResult(attempt.Result);
+    }
+}
