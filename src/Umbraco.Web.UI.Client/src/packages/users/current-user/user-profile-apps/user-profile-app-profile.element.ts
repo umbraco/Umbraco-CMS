95a182487131783fbd04fc56ed643445677930e9
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-07-26 10:29:01 +0200

    translation to your profile

--- src/Umbraco.Web.UI.Client/src/packages/users/current-user/user-profile-apps/user-profile-app-profile.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/users/current-user/user-profile-apps/user-profile-app-profile.element.ts
@@ -1,3 +1,4 @@
+import { UMB_LOCALIZATION_CONTEXT } from '@umbraco-cms/backoffice/localization-api';
 import { css, html, customElement, state } from '@umbraco-cms/backoffice/external/lit';
 import { UUITextStyles } from '@umbraco-cms/backoffice/external/uui';
 import { UmbLitElement } from '@umbraco-cms/internal/lit-element';
@@ -12,6 +13,15 @@ import { UMB_AUTH, type UmbLoggedInUser } from '@umbraco-cms/backoffice/auth';
 export class UmbUserProfileAppProfileElement extends UmbLitElement {
 	@state()
 	private _currentUser?: UmbLoggedInUser;
+
+	@state()
+	protected _labelYourProfile = 'Your profile';
+
+	@state()
+	protected _labelEditProfile = 'Edit your profile';
+
+	@state()
+	protected _labelChangePassword = 'Change your password';
 
 	private _modalContext?: UmbModalManagerContext;
 	private _auth?: typeof UMB_AUTH.TYPE;
@@ -28,7 +38,15 @@ export class UmbUserProfileAppProfileElement extends UmbLitElement {
 			this._observeCurrentUser();
 		});
 
-		this._observeCurrentUser();
+		this.consumeContext(UMB_LOCALIZATION_CONTEXT, (instance) => {
+			instance
+				.localizeMany(['user_yourProfile', 'user_editProfileLabel', 'user_changePasswordLabel'])
+				.subscribe((value) => {
+					this._labelYourProfile = value[0];
+					this._labelEditProfile = value[1];
+					this._labelChangePassword = value[2];
+				});
+		});
 	}
 
 	private async _observeCurrentUser() {
@@ -56,10 +74,13 @@ export class UmbUserProfileAppProfileElement extends UmbLitElement {
 
 	render() {
 		return html`
-			<uui-box>
-				<b slot="headline">Your profile</b>
-				<uui-button look="primary" @click=${this._edit}>Edit</uui-button>
-				<uui-button look="primary" @click=${this._changePassword}>Change password</uui-button>
+			<uui-box .headline=${this._labelYourProfile}>
+				<uui-button look="primary" label=${this._labelEditProfile} @click=${this._edit}>
+					<umb-localize key="user_editProfile">Edit profile</umb-localize>
+				</uui-button>
+				<uui-button look="primary" label=${this._labelChangePassword} @click=${this._changePassword}>
+					<umb-localize key="user_changePassword">Change password</umb-localize>
+				</uui-button>
 			</uui-box>
 		`;
 	}
