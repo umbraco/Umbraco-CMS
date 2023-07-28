65e47814eb4cdb62ec16c534864776d8b558529f
Niels Lyngsø <niels.lyngso@gmail.com>
2023-07-28 11:14:55 +0200

    remove question mark

--- src/Umbraco.Web.UI.Client/src/libs/extension-api/controller/base-extensions-controller.ts
+++ src/Umbraco.Web.UI.Client/src/libs/extension-api/controller/base-extensions-controller.ts
@@ -100,7 +100,7 @@ export abstract class UmbBaseExtensionsController<
 				// Check if it overwrites another extension:
 				// if so, look up the extension it overwrites, and remove it from the list. and check that for if it overwrites another extension and so on.
 				if (extCtrl.overwrites && extCtrl.overwrites.length > 0) {
-					extCtrl.overwrites?.forEach((overwrite) => {
+					extCtrl.overwrites.forEach((overwrite) => {
 						this.#removeOverwrittenExtensions(exposedPermittedExts, overwrite);
 					});
 				}
