72e277b4517e0e3a34e696c6be6a233a67fab1f3
Julia Gru <56249914+julczka@users.noreply.github.com>
2023-07-31 10:14:14 +0200

    add missing context methods

--- src/Umbraco.Web.UI.Client/src/packages/templating/stylesheets/workspace/stylesheet-workspace.context.ts
+++ src/Umbraco.Web.UI.Client/src/packages/templating/stylesheets/workspace/stylesheet-workspace.context.ts
@@ -2,26 +2,53 @@ import { UmbStylesheetRepository } from '../repository/stylesheet.repository.js'
 import { StylesheetDetails } from '../index.js';
 import { UmbWorkspaceContext } from '@umbraco-cms/backoffice/workspace';
 import { UmbControllerHostElement } from '@umbraco-cms/backoffice/controller-api';
-import { UmbObjectState } from '@umbraco-cms/backoffice/observable-api';
+import { UmbBooleanState, UmbObjectState, createObservablePart } from '@umbraco-cms/backoffice/observable-api';
+import { loadCodeEditor } from '@umbraco-cms/backoffice/code-editor';
 
 export class UmbStylesheetWorkspaceContext extends UmbWorkspaceContext<UmbStylesheetRepository, StylesheetDetails> {
 	#data = new UmbObjectState<StylesheetDetails | undefined>(undefined);
 	data = this.#data.asObservable();
+	name = createObservablePart(this.#data, (data) => data?.name);
+	content = createObservablePart(this.#data, (data) => data?.content);
+	path = createObservablePart(this.#data, (data) => data?.path);
 
+	#isCodeEditorReady = new UmbBooleanState(false);
+	isCodeEditorReady = this.#isCodeEditorReady.asObservable();
+	
 	constructor(host: UmbControllerHostElement) {
 		super(host, new UmbStylesheetRepository(host));
+		this.#loadCodeEditor();
+
 	}
+
+	async #loadCodeEditor() {
+		try {
+			await loadCodeEditor();
+			this.#isCodeEditorReady.next(true);
+		} catch (error) {
+			console.error(error);
+		}
+	}
+
 
 	getEntityType(): string {
 		return 'stylesheet';
+	}
+
+	getEntityId() {
+		return this.getData()?.path || '';
 	}
 
 	getData() {
 		return this.#data.getValue();
 	}
 
-	getEntityId() {
-		return this.getData()?.path || '';
+	setName(value: string) {
+		this.#data.next({ ...this.#data.value, name: value });
+	}
+
+	setContent(value: string) {
+		this.#data.next({ ...this.#data.value, content: value });
 	}
 
 	async load(path: string) {
@@ -34,6 +61,21 @@ export class UmbStylesheetWorkspaceContext extends UmbWorkspaceContext<UmbStyles
 
 	public async save() {
 		throw new Error('Save method not implemented.');
+	}
+
+	async create(parentKey: string | null) {
+		// const { data } = await this.repository.createScaffold(parentKey, name);
+		// const newStylesheet = {
+		// 	...data,
+		// 	name: '',
+		// 	path: parentKey ?? '',
+		// };
+		// if (!data) return;
+		// this.setIsNew(true);
+		// this.#data.next(newStylesheet);
+
+		throw new Error('Create method not implemented.');
+
 	}
 
 	public destroy(): void {
