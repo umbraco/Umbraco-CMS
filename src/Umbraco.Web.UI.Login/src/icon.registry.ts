d794c210a3dc7f0508e0c06db6ced486d9626fa1
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-07-31 11:54:49 +0200

    use getIcons() to request icons

--- src/Umbraco.Web.UI.Login/src/icon.registry.ts
+++ src/Umbraco.Web.UI.Login/src/icon.registry.ts
@@ -1,4 +1,5 @@
 import {UUIIconRegistry} from "@umbraco-ui/uui";
+import {UmbAuthMainContext} from "./context/auth-main.context.ts";
 
 /**
  * This is a custom icon registry that will load icons from the Umbraco assets folder.
@@ -9,23 +10,15 @@ export class UmbIconRegistry extends UUIIconRegistry {
     // Inform that we will be providing this.
     const icon = this.provideIcon(iconName);
 
-    // Try and load icon
-    const iconPath = `assets/icons/${iconName}.svg`;
-    fetch(iconPath)
-      .then(async response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-
-        return response.text();
-      })
-      .then(svg => {
-        icon.svg = svg;
-      })
-      .catch(() => {
+    UmbAuthMainContext.Instance.getIcons().subscribe(icons => {
+      if (icons[iconName]) {
+        icon.svg = icons[iconName];
+      } else {
         // If we can't load the icon, we will not provide it.
         console.warn(`Icon ${iconName} not found`);
-      });
+      }
+    });
+
     return true;
   }
 }
