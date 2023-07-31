ec5e62e4bbec6ef7bfb1881b9c4dfd5823de4373
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-07-31 12:52:53 +0200

    Revert "install remixicon to supplement backoffice icons (replacing font-awesome eventually)"
    This reverts commit 0c6659d8d9dfe0664314ec6a3acaa8e20cb606ed.

--- src/Umbraco.Web.UI.Client/gulp/tasks/dependencies.js
+++ src/Umbraco.Web.UI.Client/gulp/tasks/dependencies.js
@@ -2,7 +2,6 @@
 
 var config = require('../config');
 var gulp = require('gulp');
-var flatten = require('gulp-flatten');
 
 var MergeStream = require('merge-stream');
 
@@ -193,13 +192,6 @@ function dependencies() {
             "base": "./node_modules/font-awesome"
         },
         {
-            "name": "remixicon",
-            "src":  ["./node_modules/remixicon/icons/**/*.svg"],
-            "base": "./node_modules/remixicon/icons",
-            "dest": config.targets.assets + "/icons",
-            "flatten": true
-        },
-        {
             "name": "jquery",
             "src":  [
                 "./node_modules/jquery/dist/jquery.min.js",
@@ -307,11 +299,7 @@ function dependencies() {
         var task = gulp.src(module.src, { base: module.base, allowEmpty: true });
 
         _.forEach(config.roots, function(root){
-            // If flatten is provided
-            if (module.flatten) {
-                task = task.pipe(flatten());
-            }
-            task = task.pipe(gulp.dest(root + (module.dest || (config.targets.lib + "/" + module.name))))
+            task = task.pipe(gulp.dest(root + config.targets.lib + "/" + module.name))
         });
 
         stream.add(task);
