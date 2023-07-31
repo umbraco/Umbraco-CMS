7cddc45e89417fa4c5454f3ecefbe967b1634e2f
Julia Gru <56249914+julczka@users.noreply.github.com>
2023-07-31 10:02:24 +0200

    correct the type

--- src/Umbraco.Web.UI.Client/src/packages/templating/stylesheets/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/templating/stylesheets/index.ts
@@ -1,9 +1,7 @@
-import { FileSystemTreeItemPresentationModel } from '@umbraco-cms/backoffice/backend-api';
+import { StylesheetResponseModel } from '@umbraco-cms/backoffice/backend-api';
 
 // TODO: temp until we have a proper stylesheet model
-export interface StylesheetDetails extends FileSystemTreeItemPresentationModel {
-	content: string;
-}
+export type StylesheetDetails = StylesheetResponseModel;
 
 export const STYLESHEET_ENTITY_TYPE = 'stylesheet';
 export * from './repository/index.js';
