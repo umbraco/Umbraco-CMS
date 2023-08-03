ca04804886a94c92238e06c4730c1f4a5b53318b
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-08-03 14:01:04 +0200

    update eslint rule for '.js' imports to also include exports that reference a file

--- src/Umbraco.Web.UI.Client/eslint-local-rules.cjs
+++ src/Umbraco.Web.UI.Client/eslint-local-rules.cjs
@@ -200,7 +200,7 @@ module.exports = {
 		},
 	},
 
-	/** @type {import('eslint').RuleModule}*/
+	/** @type {import('eslint').Rule.RuleModule}*/
 	'prefer-static-styles-last': {
 		meta: {
 			type: 'suggestion',
@@ -247,7 +247,7 @@ module.exports = {
 		},
 	},
 
-	/** @type {import('eslint').RuleModule}*/
+	/** @type {import('eslint').Rule.RuleModule}*/
 	'ensure-relative-import-use-js-extension': {
 		meta: {
 			type: 'problem',
@@ -308,6 +308,33 @@ module.exports = {
 						});
 					}
 				},
+				ExportAllDeclaration: (node) => {
+					const { source } = node;
+					const { value } = source;
+
+					const fixedSource = correctImport(value);
+					if (fixedSource) {
+						context.report({
+							node: source,
+							message: 'Relative exports should use the ".js" file extension.',
+							fix: (fixer) => fixer.replaceText(source, `'${fixedSource}'`),
+						});
+					}
+				},
+				ExportNamedDeclaration: (node) => {
+					const { source } = node;
+					if (!source) return;
+					const { value } = source;
+
+					const fixedSource = correctImport(value);
+					if (fixedSource) {
+						context.report({
+							node: source,
+							message: 'Relative exports should use the ".js" file extension.',
+							fix: (fixer) => fixer.replaceText(source, `'${fixedSource}'`),
+						});
+					}
+				}
 			};
 		},
 	},
