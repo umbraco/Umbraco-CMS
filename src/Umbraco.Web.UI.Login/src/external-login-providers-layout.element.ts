b00ef1832adc8b9f012b01b17f02da9c16c81460
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-08-04 12:06:11 +0200

    only show external login layout if there are slotted children

--- src/Umbraco.Web.UI.Login/src/external-login-providers-layout.element.ts
+++ src/Umbraco.Web.UI.Login/src/external-login-providers-layout.element.ts
@@ -1,62 +1,75 @@
-import { css, CSSResultGroup, html, LitElement } from 'lit';
-import { customElement } from 'lit/decorators.js';
+import { css, CSSResultGroup, html, LitElement, nothing } from 'lit';
+import { customElement, queryAssignedElements, state } from 'lit/decorators.js';
 
 import './login-external.element.js';
 
 @customElement('umb-external-login-providers-layout')
 export class UmbExternalLoginProvidersLayoutElement extends LitElement {
-	render() {
-		return html`
-			<div id="divider"><span>or</span></div>
-			<div>
-				<slot></slot>
-			</div>
-		`;
-	}
 
-	static styles: CSSResultGroup = [
-		css`
-			:host {
-				margin-top: 16px;
-				display: flex;
-				flex-direction: column;
-			}
+  @queryAssignedElements({ flatten: true })
+  protected slottedElements?: HTMLElement[];
 
-			slot {
-				display: flex;
-				flex-direction: column;
-				gap: var(--uui-size-space-4);
-			}
+  @state()
+  protected hasSlottedElements: boolean = false;
 
-			#divider {
-				width: 100%;
-				text-align: center;
-				color: #868686; /* TODO: Change to uui color when uui gets a muted text variable */
-				position: relative;
-				z-index: 0;
-				margin-bottom: 16px;
-			}
-			#divider span {
-				background-color: var(--uui-color-surface-alt);
-				padding: 0 9px;
-			}
-			#divider::before {
-				content: '';
-				display: block;
-				width: 100%;
-				height: 1px;
-				background-color: var(--uui-color-border);
-				position: absolute;
-				top: calc(50% + 1px);
-				transform: translateY(-50%);
-				z-index: -1;
-			}
-		`,
-	];
+  firstUpdated() {
+    this.hasSlottedElements = !!this.slottedElements?.length;
+  }
+
+  render() {
+      return html`
+        ${this.hasSlottedElements ? html`<div id="divider"><span>or</span></div>` : nothing}
+        <div>
+          <slot></slot>
+        </div>
+      `;
+  }
+
+  static styles: CSSResultGroup = [
+    css`
+      :host {
+        margin-top: 16px;
+        display: flex;
+        flex-direction: column;
+      }
+
+      slot {
+        display: flex;
+        flex-direction: column;
+        gap: var(--uui-size-space-4);
+      }
+
+      #divider {
+        width: 100%;
+        text-align: center;
+        color: #868686; /* TODO: Change to uui color when uui gets a muted text variable */
+        position: relative;
+        z-index: 0;
+        margin-bottom: 16px;
+      }
+
+      #divider span {
+        background-color: var(--uui-color-surface-alt);
+        padding: 0 9px;
+      }
+
+      #divider::before {
+        content: '';
+        display: block;
+        width: 100%;
+        height: 1px;
+        background-color: var(--uui-color-border);
+        position: absolute;
+        top: calc(50% + 1px);
+        transform: translateY(-50%);
+        z-index: -1;
+      }
+    `,
+  ];
 }
 
 declare global {
-	interface HTMLElementTagNameMap {
-		'umb-external-login-providers-layout': UmbExternalLoginProvidersLayoutElement;
-	}
+  interface HTMLElementTagNameMap {
+    'umb-external-login-providers-layout': UmbExternalLoginProvidersLayoutElement;
+  }
 }
