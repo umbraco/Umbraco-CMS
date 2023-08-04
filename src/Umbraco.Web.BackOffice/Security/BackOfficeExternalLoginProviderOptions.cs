ebb143c3ec772148b5d8508e3fa31c86294e52c0
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-08-04 11:50:26 +0200

    add required xml comments

--- src/Umbraco.Web.BackOffice/Security/BackOfficeExternalLoginProviderOptions.cs
+++ src/Umbraco.Web.BackOffice/Security/BackOfficeExternalLoginProviderOptions.cs
@@ -90,17 +90,18 @@ public class BackOfficeExternalLoginProviderOptions
     public string Icon { get; set; } = "icon-user";
 
     /// <summary>
-    ///     Options used to control how users can be auto-linked/created/updated based on the external login provider
+    ///     Gets or sets options used to control how users can be auto-linked/created/updated based on the external login provider
     /// </summary>
     public ExternalSignInAutoLinkOptions AutoLinkOptions { get; set; } = new();
 
     /// <summary>
-    ///     When set to true will disable all local user login functionality
+    ///     Gets or sets a value indicating whether when set to true will disable all local user login functionality.
+    ///     This is useful if you want to force users to login with an external provider.
     /// </summary>
     public bool DenyLocalLogin { get; set; }
 
     /// <summary>
-    ///     When specified this will automatically redirect to the OAuth login provider instead of prompting the user to click
+    ///     Gets or sets a value indicating whether when specified this will automatically redirect to the OAuth login provider instead of prompting the user to click
     ///     on the OAuth button first.
     /// </summary>
     /// <remarks>
@@ -111,13 +112,13 @@ public class BackOfficeExternalLoginProviderOptions
     public bool AutoRedirectLoginToExternalProvider { get; set; }
 
     /// <summary>
-    ///     A virtual path to a custom angular view that is used to replace the entire UI that renders the external login
-    ///     button that the user interacts with
+    ///     Gets or sets a virtual path to a custom JavaScript module that will be rendered in place of the default OAuth login buttons.
+    ///     The view can optionally replace the entire login screen if the <see cref="DenyLocalLogin"/> option is set to true.
     /// </summary>
     /// <remarks>
     ///     If this view is specified it is 100% up to the user to render the html responsible for rendering the link/un-link
-    ///     buttons along with showing any errors
-    ///     that occur. This overrides what Umbraco normally does by default.
+    ///     buttons along with showing any errors that occur.
+    ///     This overrides what Umbraco normally does by default.
     /// </remarks>
     public string? CustomBackOfficeView { get; set; }
 }
