c401a4021ec132322a7e3c95e90edb21e5352199
Arkadiusz Biel - bielu <bielu@bielu.pl>
2023-08-06 23:00:25 +0100

    refactor: remove code duplication

--- Umbraco.Search.Lifti/DepedencyInjection/UmbracoBuilderExtensions.cs
+++ Umbraco.Search.Lifti/DepedencyInjection/UmbracoBuilderExtensions.cs
@@ -7,6 +7,7 @@ using Umbraco.Cms.Core.Models.Entities;
 using Umbraco.Search.Configuration;
 using Umbraco.Search.Lifti.SpecialisedSearchers;
 using Umbraco.Search.Services;
+using Umbraco.Search.ValueSet.ValueSetBuilders;
 using IBackOfficeExamineSearcher = Umbraco.Search.SpecialisedSearchers.IBackOfficeExamineSearcher;
 using IContentValueSetBuilder = Umbraco.Search.ValueSet.ValueSetBuilders.IContentValueSetBuilder;
 
@@ -16,19 +17,13 @@ public static class UmbracoBuilderExtensions
 {
     public static IServiceCollection RegisterIndexExternal(this IServiceCollection services)
     {
-        services.AddSingleton<ILiftiIndex>(serviceCollection => new UmbracoLiftiIndex( Constants
+        services.AddInMemoryIndex<IContent,IContentValueSetBuilder>(Constants.UmbracoIndexes
+            .ExternalIndexName);
+        services.AddSingleton<ILiftiIndex>(serviceCollection => new UmbracoLiftiIndex(Constants
                 .UmbracoIndexes
                 .ExternalIndexName,
             new FullTextIndexBuilder<string>()
                 .Build()));
-        // This is the long way to add IOptions but gives us access to the
-        // services collection which we need to get the dir factory
-        services.AddSingleton<IUmbracoIndex>(serviceCollection =>
-            new UmbracoMemoryIndex<IContent>(serviceCollection.GetRequiredService<ILiftiIndexManager>().GetIndex(
-                    Constants
-                        .UmbracoIndexes
-                        .ExternalIndexName),
-                serviceCollection.GetRequiredService<IContentValueSetBuilder>()));
         services.AddSingleton<IUmbracoSearcher>(serviceCollection =>
             new UmbracoMemorySearcher<IContent>(serviceCollection.GetRequiredService<ILiftiIndexManager>().GetIndex(
                 Constants
@@ -41,53 +36,34 @@ public static class UmbracoBuilderExtensions
 
     public static IServiceCollection RegisterIndexInternal(this IServiceCollection services)
     {
-        services.AddSingleton<ILiftiIndex>(serviceCollection => new UmbracoLiftiIndex( Constants
-                .UmbracoIndexes
-                .InternalIndexName,
-            new FullTextIndexBuilder<string>()
-                .Build()));
-        // This is the long way to add IOptions but gives us access to the
-        // services collection which we need to get the dir factory
-        services.AddSingleton<IUmbracoIndex>(serviceCollection =>
-            new UmbracoMemoryIndex<IContent>(serviceCollection.GetRequiredService<ILiftiIndexManager>().GetIndex(
-                    Constants
-                        .UmbracoIndexes
-                        .InternalIndexName),
-                serviceCollection.GetRequiredService<IContentValueSetBuilder>()));
-        services.AddSingleton<IUmbracoSearcher>(serviceCollection =>
-            new UmbracoMemorySearcher<IContent>(serviceCollection.GetRequiredService<ILiftiIndexManager>().GetIndex(
-                Constants
-                    .UmbracoIndexes
-                    .InternalIndexName), Constants
-                .UmbracoIndexes
-                .InternalIndexName));
+        services.AddInMemoryIndex<IContent, IContentValueSetBuilder>(Constants.UmbracoIndexes
+            .InternalIndexName);
         return services;
     }
 
     /// <summary>
     /// Registers an Examine index
     /// </summary>
-    public static IServiceCollection AddInMemoryIndex<T>(
+    public static IServiceCollection AddInMemoryIndex<TIndexedModelType,TValueSetBuilder>(
         this IServiceCollection serviceCollection,
-        string name) where T : IUmbracoEntity
+        string name) where TIndexedModelType : IUmbracoEntity where TValueSetBuilder : notnull, IValueSetBuilder<TIndexedModelType>
     {
         serviceCollection.AddSingleton<ILiftiIndex>(serviceCollection => new UmbracoLiftiIndex(name,
             new FullTextIndexBuilder<string>()
                 .Build()));
         serviceCollection.AddSingleton<IUmbracoIndex>(serviceCollection =>
-            new UmbracoMemoryIndex<T>(serviceCollection.GetRequiredService<ILiftiIndexManager>().GetIndex(name),
-                serviceCollection.GetRequiredService<ValueSet.ValueSetBuilders.IValueSetBuilder<T>>()));
+            new UmbracoMemoryIndex<TIndexedModelType>(serviceCollection.GetRequiredService<ILiftiIndexManager>().GetIndex(name),
+                serviceCollection.GetRequiredService<TValueSetBuilder>()));
         serviceCollection.AddSingleton<IUmbracoSearcher>(serviceCollection =>
-            new UmbracoMemorySearcher<T>(serviceCollection.GetRequiredService<ILiftiIndexManager>().GetIndex(name),
+            new UmbracoMemorySearcher<TIndexedModelType>(serviceCollection.GetRequiredService<ILiftiIndexManager>().GetIndex(name),
                 name));
         return serviceCollection;
     }
 
     public static IServiceCollection RegisterIndexMember(this IServiceCollection services)
     {
-        services.AddInMemoryIndex<IMember>(Constants.UmbracoIndexes
+        services.AddInMemoryIndex<IMember,IValueSetBuilder<IMember>>(Constants.UmbracoIndexes
             .MembersIndexName);
-
         return services;
     }
 
@@ -137,6 +113,7 @@ public static class UmbracoBuilderExtensions
             .RegisterContentApiIndex();
         return services;
     }
+
     /// <summary>
     ///     Adds the Examine indexes for Umbraco
     /// </summary>
