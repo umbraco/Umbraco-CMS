6ff98370cd894be093f2a3c1e27cbd383791587c
Arkadiusz Biel - bielu <bielu@bielu.pl>
2023-08-06 12:17:32 +0100

    refactor: simplied extension code

--- src/Umbraco.Search/Extensions/ObjectExtensions.cs
+++ src/Umbraco.Search/Extensions/ObjectExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.ComponentModel;
+using Umbraco.Extensions;
 
 namespace Umbraco.Search.Extensions;
 
@@ -8,28 +9,36 @@ public static class ObjectExtensions
     /// <summary>
     /// Turns object into dictionary
     /// </summary>
-    /// <param name="o"></param>
+    /// <param name="sourceObject"></param>
     /// <param name="ignoreProperties">Properties to ignore</param>
     /// <returns></returns>
-    public static IDictionary<string, object> ConvertObjectToDictionary(object o, params string[] ignoreProperties)
+    public static IDictionary<string, object> ConvertObjectToDictionary(
+        object? sourceObject,
+        params string[] ignoreProperties)
     {
-        if (o != null)
+        if (sourceObject == null)
         {
-            if (o is IDictionary)
-                throw new InvalidOperationException($"The input object is already of type {typeof(IDictionary)}");
-
-            var props = TypeDescriptor.GetProperties(o);
-            var d = new Dictionary<string, object>();
-            foreach (var prop in props.Cast<PropertyDescriptor>().Where(x => !ignoreProperties.Contains(x.Name)))
-            {
-                var val = prop.GetValue(o);
-                if (val != null)
-                {
-                    d.Add(prop.Name, val);
-                }
-            }
-            return d;
+            return new Dictionary<string, object>();
         }
-        return new Dictionary<string, object>();
+
+        if (sourceObject is IDictionary sourceDictionary)
+        {
+            return sourceDictionary.Keys.Cast<object>().ToDictionary(
+                key => key.ToString()!,
+                key => sourceDictionary[key]!);
+        }
+
+        var props = TypeDescriptor.GetProperties(sourceObject);
+        var d = new Dictionary<string, object>();
+        foreach (var prop in props.Cast<PropertyDescriptor>().Where(x => !ignoreProperties.Contains(x.Name)))
+        {
+            var val = prop.GetValue(sourceObject);
+            if (val != null)
+            {
+                d.Add(prop.Name, val);
+            }
+        }
+
+        return d;
     }
 }
