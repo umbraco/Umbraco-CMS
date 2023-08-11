84e9eda2947618baeb756393aceaa74614461f7f
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-08-11 10:49:21 +0200

    move lit properties to UmbLitElement
    we cannot have lit properties on the UmbElementMixin since we are not guaranteed it's a Lit element

--- src/Umbraco.Web.UI.Client/src/shared/lit-element/lit-element.element.ts
+++ src/Umbraco.Web.UI.Client/src/shared/lit-element/lit-element.element.ts
@@ -1,4 +1,8 @@
-import { LitElement } from '@umbraco-cms/backoffice/external/lit';
+import { LitElement, property } from '@umbraco-cms/backoffice/external/lit';
 import { UmbElementMixin } from '@umbraco-cms/backoffice/element-api';
 
-export class UmbLitElement extends UmbElementMixin(LitElement) {}
+export class UmbLitElement extends UmbElementMixin(LitElement) {
+	// Make `dir` and `lang` reactive properties so they react to language changes:
+	@property() dir = '';
+	@property() lang = '';
+}
