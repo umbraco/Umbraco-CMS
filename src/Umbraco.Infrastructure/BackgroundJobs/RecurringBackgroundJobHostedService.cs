2fc4e7fc9b035b93341b08223d0cdf44e2ec0c52
Andrew McKaskill <andrew.mckaskill@greatstate.co>
2023-08-11 14:59:01 +0100

    Add NotificationEvents

--- src/Umbraco.Infrastructure/BackgroundJobs/RecurringBackgroundJobHostedService.cs
+++ src/Umbraco.Infrastructure/BackgroundJobs/RecurringBackgroundJobHostedService.cs
@@ -2,8 +2,10 @@ using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
+using Serilog.Core;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Configuration.Models;
+using Umbraco.Cms.Core.Events;
 using Umbraco.Cms.Core.Runtime;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Core.Sync;
@@ -33,6 +35,7 @@ public class RecurringBackgroundJobHostedService<TJob> : RecurringHostedServiceB
     private readonly IMainDom _mainDom;
     private readonly IRuntimeState _runtimeState;
     private readonly IServerRoleAccessor _serverRoleAccessor;
+    private readonly IEventAggregator _eventAggregator;
     private readonly IRecurringBackgroundJob _job;
 
     public RecurringBackgroundJobHostedService(
@@ -40,6 +43,7 @@ public class RecurringBackgroundJobHostedService<TJob> : RecurringHostedServiceB
         ILogger<RecurringBackgroundJobHostedService<TJob>> logger,
         IMainDom mainDom,
         IServerRoleAccessor serverRoleAccessor,
+        IEventAggregator eventAggregator,
         TJob job)
         : base(logger, job.Period, job.Delay)
     {
@@ -47,6 +51,7 @@ public class RecurringBackgroundJobHostedService<TJob> : RecurringHostedServiceB
         _logger = logger;
         _mainDom = mainDom;
         _serverRoleAccessor = serverRoleAccessor;
+        _eventAggregator = eventAggregator;
         _job = job;
 
         _job.PeriodChanged += (sender, e) => ChangePeriod(_job.Period);
@@ -55,27 +60,61 @@ public class RecurringBackgroundJobHostedService<TJob> : RecurringHostedServiceB
     /// <inheritdoc />
     public override async Task PerformExecuteAsync(object? state)
     {
-
-        if (_runtimeState.Level != RuntimeLevel.Run)
+        try
         {
-            _logger.LogDebug("Job not running as runlevel not yet ready");
-            return;
+
+            if (_runtimeState.Level != RuntimeLevel.Run)
+            {
+                _logger.LogDebug("Job not running as runlevel not yet ready");
+                return;
+            }
+
+            // Don't run on replicas nor unknown role servers
+            if (!_job.ServerRoles.Contains(_serverRoleAccessor.CurrentServerRole))
+            {
+                _logger.LogDebug("Job not running on this server role");
+                return;
+            }
+
+            // Ensure we do not run if not main domain, but do NOT lock it
+            if (!_mainDom.IsMainDom)
+            {
+                _logger.LogDebug("Job not running as not MainDom");
+                return;
+            }
+
+
+            await _eventAggregator.PublishAsync(new Notifications.RecurringBackgroundJobExecutingNotification(_job, new EventMessages()));
+            await _job.RunJobAsync();
+            await _eventAggregator.PublishAsync(new Notifications.RecurringBackgroundJobExecutingNotification(_job, new EventMessages()));
+
+
+        }
+        catch (Exception ex)
+        {
+            await _eventAggregator.PublishAsync(new Notifications.RecurringBackgroundJobFailedNotification(_job, new EventMessages()));
+            _logger.LogError(ex, "Unhandled exception in recurring background job.");
         }
 
-        // Don't run on replicas nor unknown role servers
-        if (!_job.ServerRoles.Contains(_serverRoleAccessor.CurrentServerRole))
-        {
-            _logger.LogDebug("Job not running on this server role");
-            return;
-        }
+    }
 
-        // Ensure we do not run if not main domain, but do NOT lock it
-        if (!_mainDom.IsMainDom)
-        {
-            _logger.LogDebug("Job not running as not MainDom");
-            return;
-        }
+    public override async Task StartAsync(CancellationToken cancellationToken)
+    {
+        await _eventAggregator.PublishAsync(new Notifications.RecurringBackgroundJobStartingNotification(_job, new EventMessages()));
 
-        await _job.RunJobAsync();
+        await base.StartAsync(cancellationToken);
+
+        await _eventAggregator.PublishAsync(new Notifications.RecurringBackgroundJobStartedNotification(_job, new EventMessages()));
+
+    }
+
+    public override async Task StopAsync(CancellationToken cancellationToken)
+    {
+
+        await _eventAggregator.PublishAsync(new Notifications.RecurringBackgroundJobStoppingNotification(_job, new EventMessages()));
+
+        await base.StopAsync(cancellationToken);
+
+        await _eventAggregator.PublishAsync(new Notifications.RecurringBackgroundJobStoppedNotification(_job, new EventMessages()));
     }
 }
