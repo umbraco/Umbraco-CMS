30f0cfc15df9425329ed5f780034f82b1cc0ffe7
Anders Reus <88318565+andersreus@users.noreply.github.com>
2023-08-15 09:46:27 +0200

    Add exclusion filter setting to typefinder settings (#14426)
    * Obsolete constructor with deprecated IScopeProvider
    
    * Add exclusion setting to typefinder settings
    
    * The old TypeFinder constructor calls the new constructor now instead and excluded duplicates by using Union instead of Concat.
    
    * Revert "The old TypeFinder constructor calls the new constructor now instead and excluded duplicates by using Union instead of Concat."
    
    This reverts commit 87801c6c1cbaa6adab6f29dba1e876a586e05885.
    
    * Add changes to TypeFinder
    
    * Do not use null when type is not nullable
    
    ---------
    
    Co-authored-by: Bjarke Berg <mail@bergmania.dk>

--- src/Umbraco.Web.Common/Extensions/ServiceCollectionExtensions.cs
+++ src/Umbraco.Web.Common/Extensions/ServiceCollectionExtensions.cs
@@ -200,6 +200,7 @@ public static class ServiceCollectionExtensions
         var typeFinder = new TypeFinder(
             loggerFactory.CreateLogger<TypeFinder>(),
             assemblyProvider,
+            typeFinderSettings.AdditionalAssemblyExclusionEntries,
             typeFinderConfig);
 
         var typeLoader = new TypeLoader(typeFinder, loggerFactory.CreateLogger<TypeLoader>());
