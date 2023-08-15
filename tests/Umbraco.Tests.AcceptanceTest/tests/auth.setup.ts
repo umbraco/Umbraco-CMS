057071fc44196dea2f09df0f719c4a37f4ae78fe
Andreas Zerbst <andr317c@live.dk>
2023-08-15 17:48:05 +0200

    Updated auth to work

--- tests/Umbraco.Tests.AcceptanceTest/tests/auth.setup.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/auth.setup.ts
@@ -1,18 +1,18 @@
-﻿import {test as setup, expect, Page} from '@playwright/test';
+﻿import {test as setup, expect} from '@playwright/test';
 import {STORAGE_STATE} from '../playwright.config'
 
 // Update this setup with test ids in the future. Maybe also find better locators in general
 setup('authenticate', async ({page}) => {
   await page.goto(process.env.URL + '/umbraco');
 
-  await page.getByRole('textbox', { name: 'Email' }).fill(process.env.UMBRACO_USER_LOGIN);
-  await page.getByRole('textbox', { name: 'Password' }).fill(process.env.UMBRACO_USER_PASSWORD);
+  await page.getByRole('textbox', {name: 'Email'}).fill(process.env.UMBRACO_USER_LOGIN);
+  await page.getByRole('textbox', {name: 'Password'}).fill(process.env.UMBRACO_USER_PASSWORD);
   await page.getByRole('button', {name: 'Login'}).click();
 
-  await page.waitForURL(process.env.URL + '/umbraco');
-
   // Assert
-  await expect(page.getByRole('tab', { name: 'Settings' })).toBeVisible();
+  // CREATE A UI TEST HELPER FOR DATA ELEMENTS
+  await expect(page.locator('[data-element="section-settings"]')).toBeVisible();
+  await page.locator('[data-element="section-settings"]').click();
 
   await page.context().storageState({path: STORAGE_STATE});
 });
