8a528df976bc3a840fb558d096f21a55bef930c1
Ronald Barendse <ronald@barend.se>
2023-08-17 10:51:23 +0200

    Do not enable static web assets on production environments

--- src/Umbraco.Web.Common/Extensions/WebApplicationBuilderExtensions.cs
+++ src/Umbraco.Web.Common/Extensions/WebApplicationBuilderExtensions.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Umbraco.Cms.Core.Configuration.Models;
 using Umbraco.Cms.Core.DependencyInjection;
 
 namespace Umbraco.Extensions;
@@ -23,7 +24,12 @@ public static class WebApplicationBuilderExtensions
         // don't add runtime as hosted service (this is replaced by the explicit BootUmbracoAsync)
         builder.Host.ConfigureUmbracoDefaults(false);
 
-        builder.WebHost.UseStaticWebAssets();
+        // Do not enable static web assets on production environments,
+        // because the files are already copied to the publish output folder.
+        if (builder.Configuration.GetRuntimeMode() != RuntimeMode.Production)
+        {
+            builder.WebHost.UseStaticWebAssets();
+        }
 
         return builder.Services.AddUmbraco(builder.Environment, builder.Configuration);
     }
