66bbad33799b7e98d2235715f6dc2086a4ebfd1b
Kenn Jacobsen <kja@umbraco.dk>
2023-08-21 13:57:36 +0200

    Media Delivery API (#14692)
    * Introduce media API - controllers, services, tests, Swagger docs
    
    * Add path to media API response + add "by path" endpoint
    
    * Review comments
    
    * Implement filtering and sorting
    
    * Add explicit media access configuration
    
    * Cleanup
    
    * Adding default case as in the MediaApiControllerBase
    
    * Update src/Umbraco.Cms.Api.Delivery/Services/ApiMediaQueryService.cs
    
    Co-authored-by: Elitsa Marinovska <21998037+elit0451@users.noreply.github.com>
    
    * Swap sort order calculation to align with Content API
    
    * Add CreateDate and UpdateDate to media responses
    
    * Mirror Content Delivery API behavior for empty children selector
    
    ---------
    
    Co-authored-by: Elitsa <elm@umbraco.dk>
    Co-authored-by: Elitsa Marinovska <21998037+elit0451@users.noreply.github.com>

--- src/Umbraco.Cms.Api.Delivery/Controllers/ByRouteContentApiController.cs
+++ src/Umbraco.Cms.Api.Delivery/Controllers/ByRouteContentApiController.cs
@@ -48,15 +48,7 @@ public class ByRouteContentApiController : ContentApiItemControllerBase
     [ProducesResponseType(StatusCodes.Status404NotFound)]
     public async Task<IActionResult> ByRoute(string path = "")
     {
-        // OpenAPI does not allow reserved chars as "in:path" parameters, so clients based on the Swagger JSON will URL
-        // encode the path. Normally, ASP.NET Core handles that encoding with an automatic decoding - apparently just not
-        // for forward slashes, for whatever reason... so we need to deal with those. Hopefully this will be addressed in
-        // an upcoming version of ASP.NET Core.
-        // See also https://github.com/dotnet/aspnetcore/issues/11544
-        if (path.Contains("%2F", StringComparison.OrdinalIgnoreCase))
-        {
-            path = WebUtility.UrlDecode(path);
-        }
+        path = DecodePath(path);
 
         path = path.TrimStart("/");
         path = path.Length == 0 ? "/" : path;
