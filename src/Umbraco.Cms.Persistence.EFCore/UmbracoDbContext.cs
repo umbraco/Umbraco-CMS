a5a6fe4f76291cef93103f77e7c9a57e4d8ac9fc
Nikolaj Brask-Nielsen <nikolajgive@gmail.com>
2023-08-22 09:34:19 +0200

    docs: Improve DbContext XML docs (#14673)

--- src/Umbraco.Cms.Persistence.EFCore/UmbracoDbContext.cs
+++ src/Umbraco.Cms.Persistence.EFCore/UmbracoDbContext.cs
@@ -8,8 +8,15 @@ namespace Umbraco.Cms.Persistence.EFCore;
 /// To autogenerate migrations use the following commands
 /// and insure the 'src/Umbraco.Web.UI/appsettings.json' have a connection string set with the right provider.
 ///
-/// dotnet ef migrations add %Name% -s src/Umbraco.Web.UI -p src/Umbraco.Cms.Persistence.EFCore.SqlServer  -- --provider SqlServer
-/// dotnet ef migrations add %Name% -s src/Umbraco.Web.UI -p src/Umbraco.Cms.Persistence.EFCore.Sqlite  -- --provider Sqlite
+/// Create a migration for each provider.
+/// <code>dotnet ef migrations add %Name% -s src/Umbraco.Web.UI -p src/Umbraco.Cms.Persistence.EFCore.SqlServer -- --provider SqlServer</code>
+///
+/// <code>dotnet ef migrations add %Name% -s src/Umbraco.Web.UI -p src/Umbraco.Cms.Persistence.EFCore.Sqlite -- --provider Sqlite</code>
+///
+/// Remove the last migration for each provider.
+/// <code>dotnet ef migrations remove -s src/Umbraco.Web.UI -p src/Umbraco.Cms.Persistence.EFCore.SqlServer -- --provider SqlServer</code>
+///
+/// <code>dotnet ef migrations remove -s src/Umbraco.Web.UI -p src/Umbraco.Cms.Persistence.EFCore.Sqlite -- --provider Sqlite</code>
 ///
 /// To find documentation about this way of working with the context see
 /// https://learn.microsoft.com/en-us/ef/core/managing-schemas/migrations/providers?tabs=dotnet-core-cli#using-one-context-type
