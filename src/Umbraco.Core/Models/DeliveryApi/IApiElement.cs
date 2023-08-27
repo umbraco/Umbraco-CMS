efe00910f33f8052bb6f3c6c9301dd5920734e06
Vitor Rodrigues <vsilvar@users.noreply.github.com>
2023-08-27 11:11:02 +0200

    DeliveryApi: Ensure the ContentType property is serialized first
    This is needed so it can be used as a discriminator field by System.Text.Json
    Changed both ApiElement and IApiElement to ensure not only ApiContentResponse,
    but also ApiBlockItem, which uses the interface directly, are adjusted.

--- src/Umbraco.Core/Models/DeliveryApi/IApiElement.cs
+++ src/Umbraco.Core/Models/DeliveryApi/IApiElement.cs
@@ -1,9 +1,14 @@
+using System.Text.Json.Serialization;
+
 namespace Umbraco.Cms.Core.Models.DeliveryApi;
 
 public interface IApiElement
 {
     Guid Id { get; }
 
+    // Ensure the ContentType property is serialized first
+    // This is needed so it can be used as a discriminator field by System.Text.Json
+    [JsonPropertyOrder(-100)]
     string ContentType { get; }
 
     IDictionary<string, object?> Properties { get; }
