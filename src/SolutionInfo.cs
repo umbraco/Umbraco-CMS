dac9986ed28b798400ced60d1b055771f6b973e1
Nikolaj <nikolajlauridsen@protonmail.ch>
2023-08-28 12:32:59 +0200

    Bump version

--- src/SolutionInfo.cs
+++ src/SolutionInfo.cs
@@ -18,5 +18,5 @@ using System.Resources;
 [assembly: AssemblyVersion("8.0.0")]
 
 // these are FYI and changed automatically
-[assembly: AssemblyFileVersion("8.18.8")]
-[assembly: AssemblyInformationalVersion("8.18.8")]
+[assembly: AssemblyFileVersion("8.18.9")]
+[assembly: AssemblyInformationalVersion("8.18.9")]
