093e3bae20cd4a3733f90948b64f4314a4c063c1
Niels Lyngsø <niels.lyngso@gmail.com>
2023-08-28 20:16:41 +0200

    correction bundle section

--- src/Umbraco.Web.UI.Client/public-assets/App_Plugins/custom-bundle-package/index.js
+++ src/Umbraco.Web.UI.Client/public-assets/App_Plugins/custom-bundle-package/index.js
@@ -1,13 +1,13 @@
 export const manifests = [
 	{
 		type: 'section',
-		alias: 'My.Section.Custom',
+		alias: 'MyBundle.Section.Custom',
 		name: 'Custom Section',
 		js: '/App_Plugins/section.js',
 		weight: 1,
 		meta: {
-			label: 'My Custom',
-			pathname: 'my-custom',
+			label: 'My Bundle Section',
+			pathname: 'my-custom-bundle',
 		},
 	},
 ];
