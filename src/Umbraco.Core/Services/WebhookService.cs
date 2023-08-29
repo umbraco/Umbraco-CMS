54ac9c503e828e3a80e67e694f481e9e5a11a9f5
Zeegaan <nge@umbraco.dk>
2023-08-29 08:37:48 +0200

    Use scopes in service

--- src/Umbraco.Core/Services/WebhookService.cs
+++ src/Umbraco.Core/Services/WebhookService.cs
@@ -1,35 +1,50 @@
 ﻿using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Persistence.Repositories;
+using Umbraco.Cms.Core.Scoping;
 
 namespace Umbraco.Cms.Core.Services;
 
 public class WebhookService : IWebHookService
 {
     private readonly IWebhookRepository _webhookRepository;
+    private readonly ICoreScopeProvider _coreScopeProvider;
 
-    public WebhookService(IWebhookRepository webhookRepository) => _webhookRepository = webhookRepository;
+    public WebhookService(IWebhookRepository webhookRepository, ICoreScopeProvider coreScopeProvider)
+    {
+        _webhookRepository = webhookRepository;
+        _coreScopeProvider = coreScopeProvider;
+    }
 
     public Task CreateAsync(Webhook webhook)
     {
+        using ICoreScope scope = _coreScopeProvider.CreateCoreScope();
         _webhookRepository.Save(webhook);
+        scope.Complete();
+
         return Task.CompletedTask;
     }
 
     public Task DeleteAsync(Webhook webhook)
     {
+        using ICoreScope scope = _coreScopeProvider.CreateCoreScope();
         _webhookRepository.Delete(webhook);
+        scope.Complete();
         return Task.CompletedTask;
     }
 
     public Task<Webhook?> GetAsync(Guid key)
     {
+        using ICoreScope scope = _coreScopeProvider.CreateCoreScope();
         Webhook? webhook = _webhookRepository.Get(key);
+        scope.Complete();
         return Task.FromResult(webhook);
     }
 
     public Task<IEnumerable<Webhook>> GetMultipleAsync(IEnumerable<Guid> keys)
     {
+        using ICoreScope scope = _coreScopeProvider.CreateCoreScope();
         IEnumerable<Webhook> webhooks = _webhookRepository.GetMany(keys.ToArray());
+        scope.Complete();
         return Task.FromResult(webhooks);
     }
 }
