eaa4728a2768f7340a8e810c9c037a437a4683c2
Nikolaj <nikolajlauridsen@protonmail.ch>
2023-08-29 09:35:35 +0200

    Add extension point to add analyzers

---
+++ src/Umbraco.Core/Security/FileStreamSecurityValidatorFactory.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+namespace Umbraco.Core.Security
+{
+    public class FileStreamSecurityValidatorFactory
+    {
+        private static List<IFileStreamSecurityAnalyzer> _fileAnalyzers = new List<IFileStreamSecurityAnalyzer>();
+
+        public IFileStreamSecurityValidator CreateValidator()
+        {
+            return new FileStreamSecurityValidator(_fileAnalyzers);
+        }
+
+        public void AddAnalyzer(IFileStreamSecurityAnalyzer analyzer)
+        {
+            // We don't want duplicates
+            if (_fileAnalyzers.Contains(analyzer) == false)
+            {
+                _fileAnalyzers.Add(analyzer);
+            }
+        }
+
+        public void RemoveAnalyzer(IFileStreamSecurityAnalyzer analyzer)
+        {
+            _fileAnalyzers.Remove(analyzer);
+        }
+    }
+}
