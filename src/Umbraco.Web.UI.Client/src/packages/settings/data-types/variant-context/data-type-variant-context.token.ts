1ee13490bc66dbd5c49dac0e8ed3fa034ffd5212
Niels Lyngsø <niels.lyngso@gmail.com>
2023-09-06 10:09:19 +0200

    isDataTypeVariantContext

--- src/Umbraco.Web.UI.Client/src/packages/settings/data-types/variant-context/data-type-variant-context.token.ts
+++ src/Umbraco.Web.UI.Client/src/packages/settings/data-types/variant-context/data-type-variant-context.token.ts
@@ -2,7 +2,7 @@ import type { UmbDataTypeVariantContext } from "./data-type-variant-context.js";
 import { UmbVariantContext } from "@umbraco-cms/backoffice/workspace";
 import { UmbContextToken } from "@umbraco-cms/backoffice/context-api";
 
-export const IsDataTypePropertySetContext = (context: UmbVariantContext): context is UmbDataTypeVariantContext => ('properties' in context && context.getType() === 'data-type');
+export const isDataTypeVariantContext = (context: UmbVariantContext): context is UmbDataTypeVariantContext => ('properties' in context && context.getType() === 'data-type');
 
 export const UMB_DATA_TYPE_VARIANT_CONTEXT = new UmbContextToken<UmbVariantContext, UmbDataTypeVariantContext>(
-	"UmbVariantContext", IsDataTypePropertySetContext);
+	"UmbVariantContext", isDataTypeVariantContext);
