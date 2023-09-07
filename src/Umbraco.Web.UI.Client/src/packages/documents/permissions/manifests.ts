0ddba739b968be3ff93eb35cb3e87a9d684906fd
Mads Rasmussen <madsr@hey.com>
2023-09-07 14:01:57 +0200

    add more document permissions

--- src/Umbraco.Web.UI.Client/src/packages/documents/permissions/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/permissions/manifests.ts
@@ -3,7 +3,7 @@ import type { ManifestPermission } from '@umbraco-cms/backoffice/extension-regis
 const permissions: Array<ManifestPermission> = [
 	{
 		type: 'permission',
-		alias: 'Umb.Permission.BrowseDocument',
+		alias: 'Umb.Permission.Document.Browse',
 		name: 'Read Document Permission',
 		meta: {
 			label: 'Read',
@@ -12,7 +12,7 @@ const permissions: Array<ManifestPermission> = [
 	},
   {
 		type: 'permission',
-		alias: 'Umb.Permission.CreateDocumentBlueprint',
+		alias: 'Umb.Permission.Document.CreateBlueprint',
 		name: 'Create Document Blueprint Permission',
 		meta: {
 			label: 'Create Content Template',
@@ -21,11 +21,74 @@ const permissions: Array<ManifestPermission> = [
 	},
   {
 		type: 'permission',
-		alias: 'Umb.Permission.DeleteDocument',
+		alias: 'Umb.Permission.Document.Delete',
 		name: 'Delete Document Permission',
 		meta: {
 			label: 'Delete',
-      description: 'Allow access to create a Content Template'
+      description: 'Allow access to delete a document'
+		},
+	},
+	{
+		type: 'permission',
+		alias: 'Umb.Permission.Document.Create',
+		name: 'Create Document Permission',
+		meta: {
+			label: 'Create',
+      description: 'Allow access to create a document'
+		},
+	},
+	{
+		type: 'permission',
+		alias: 'Umb.Permission.Document.Notifications',
+		name: 'Document Notifications Permission',
+		meta: {
+			label: 'Notifications',
+      description: 'Allow access to setup notifications for documents'
+		},
+	},
+	{
+		type: 'permission',
+		alias: 'Umb.Permission.Document.Publish',
+		name: 'Publish Document Permission',
+		meta: {
+			label: 'Publish',
+      description: 'Allow access to publish a document'
+		},
+	},
+	{
+		type: 'permission',
+		alias: 'Umb.Permission.Document.Permissions',
+		name: 'Document Permissions Permission',
+		meta: {
+			label: 'Permissions',
+      description: 'Allow access to change permissions for a document'
+		},
+	},
+	{
+		type: 'permission',
+		alias: 'Umb.Permission.Document.SendForApproval',
+		name: 'Send Document For Approval Permission',
+		meta: {
+			label: 'Permissions',
+      description: 'Allow access to send a document for approval before publishing'
+		},
+	},
+	{
+		type: 'permission',
+		alias: 'Umb.Permission.Document.Unpublish',
+		name: 'Unpublish Document Permission',
+		meta: {
+			label: 'Unpublish',
+      description: 'Allow access to unpublish a document'
+		},
+	},
+	{
+		type: 'permission',
+		alias: 'Umb.Permission.Document.Update',
+		name: 'Update Document Permission',
+		meta: {
+			label: 'Update',
+      description: 'Allow access to save a document'
 		},
 	},
 ];
