63a29cd4c138e604d4f3ecd7645e8d8fa00b7ccf
Mads Rasmussen <madsr@hey.com>
2023-09-08 11:13:07 +0200

    add type to import

--- src/Umbraco.Web.UI.Client/src/packages/core/extension-registry/conditions/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/extension-registry/conditions/types.ts
@@ -1,7 +1,7 @@
 import type { SectionAliasConditionConfig } from './section-alias.condition.js';
 import type { SwitchConditionConfig } from './switch.condition.js';
 import type { WorkspaceAliasConditionConfig } from '@umbraco-cms/backoffice/workspace';
-import { UmbConditionConfigBase } from '@umbraco-cms/backoffice/extension-api';
+import type { UmbConditionConfigBase } from '@umbraco-cms/backoffice/extension-api';
 
 export type ConditionTypes =
 	| SectionAliasConditionConfig
