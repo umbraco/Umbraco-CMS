e04ec6b65ac6989a241845a7b44ab93a53890f53
Zeegaan <nge@umbraco.dk>
2023-09-11 13:19:06 +0200

    Add datacontract attirbutes to properties

--- src/Umbraco.Web.Common/Models/WebhookViewModel.cs
+++ src/Umbraco.Web.Common/Models/WebhookViewModel.cs
@@ -1,12 +1,20 @@
-﻿using Umbraco.Cms.Core.Models;
+﻿using System.Runtime.Serialization;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Umbraco.Cms.Core.Models;
 
 namespace Umbraco.Cms.Web.Common.Models;
 
+[DataContract]
 public class WebhookViewModel
 {
+    [DataMember(Name = "url")]
     public string Url { get; set; } = string.Empty;
 
+    [DataMember(Name = "event")]
+    [JsonConverter(typeof(StringEnumConverter))]
     public WebhookEvent Event { get; set; }
 
+    [DataMember(Name = "entityKeys")]
     public Guid[] EntityKeys { get; set; } = Array.Empty<Guid>();
 }
