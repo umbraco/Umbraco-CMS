cee8e643e1cba749a8c17ad2c427f4485fa22eff
Mads Rasmussen <madsr@hey.com>
2023-09-12 16:22:08 +0200

    remove unneeded menu item element

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/recycle-bin/menu-item/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/recycle-bin/menu-item/manifests.ts
@@ -1,12 +1,13 @@
-import type { ManifestMenuItem } from '@umbraco-cms/backoffice/extension-registry';
+import type { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
 
-const menuItem: ManifestMenuItem = {
+const menuItem: ManifestTypes = {
 	type: 'menuItem',
+	kind: 'tree',
 	alias: 'Umb.MenuItem.DocumentRecycleBin',
 	name: 'Document Recycle Bin Menu Item',
 	weight: 100,
-	loader: () => import('./document-recycle-bin-menu-item.element.js'),
 	meta: {
+		treeAlias: 'Umb.Tree.DocumentRecycleBin',
 		label: 'Recycle Bin',
 		icon: 'umb:trash',
 		menus: ['Umb.Menu.Content'],
