67a6b011fce0cb44ed96b727ce5a878fd0189ae8
Mads Rasmussen <madsr@hey.com>
2023-09-12 19:15:15 +0200

    add document item handlers

--- src/Umbraco.Web.UI.Client/src/mocks/handlers/document/index.ts
+++ src/Umbraco.Web.UI.Client/src/mocks/handlers/document/index.ts
@@ -1,5 +1,6 @@
 import { handlers as recycleBinHandlers } from './recycle-bin.handlers.js';
 import { handlers as treeHandlers } from './tree.handlers.js';
 import { handlers as documentHandlers } from './document.handlers.js';
+import { handlers as itemHandlers } from './item.handlers.js';
 
-export const handlers = [...recycleBinHandlers, ...treeHandlers, ...documentHandlers];
+export const handlers = [...recycleBinHandlers, ...treeHandlers, ...itemHandlers, ...documentHandlers];
