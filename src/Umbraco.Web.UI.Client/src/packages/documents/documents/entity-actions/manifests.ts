05f05e401ecc1dc9afdaadd674c44c0aa5cba259
Mads Rasmussen <madsr@hey.com>
2023-09-14 13:53:36 +0200

    move trash entity action to recycle bin folder

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/entity-actions/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/entity-actions/manifests.ts
@@ -11,26 +11,12 @@ import { manifests as createManifests } from './create/manifests.js';
 import {
 	UmbCopyEntityAction,
 	UmbMoveEntityAction,
-	UmbTrashEntityAction,
 	UmbSortChildrenOfEntityAction,
 } from '@umbraco-cms/backoffice/entity-action';
 import { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
 
 const entityActions: Array<ManifestTypes> = [
 	...createManifests,
-	{
-		type: 'entityAction',
-		alias: 'Umb.EntityAction.Document.Trash',
-		name: 'Trash Document Entity Action',
-		weight: 900,
-		meta: {
-			icon: 'umb:trash',
-			label: 'Trash',
-			repositoryAlias: DOCUMENT_REPOSITORY_ALIAS,
-			api: UmbTrashEntityAction,
-			entityTypes: [DOCUMENT_ENTITY_TYPE],
-		},
-	},
 	{
 		type: 'entityAction',
 		alias: 'Umb.EntityAction.Document.CreateBlueprint',
--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/recycle-bin/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/recycle-bin/manifests.ts
@@ -1,5 +1,6 @@
 import { manifests as repositoryManifests } from './repository/manifests.js';
 import { manifests as treeManifests } from './tree/manifests.js';
 import { manifests as menuItemManifests } from './menu-item/manifests.js';
+import { manifests as entityActionManifests } from './entity-action/manifests.js';
 
-export const manifests = [...repositoryManifests, ...treeManifests, ...menuItemManifests];
+export const manifests = [...repositoryManifests, ...treeManifests, ...menuItemManifests, ...entityActionManifests];
