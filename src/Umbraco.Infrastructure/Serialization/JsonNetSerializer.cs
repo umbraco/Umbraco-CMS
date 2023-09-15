201984aedf51f0a1602cac5153d7433c0caf4ae1
Ronald Barendse <ronald@barend.se>
2023-09-15 23:37:06 +0200

    Add new Settings property and obsolete static field

--- src/Umbraco.Infrastructure/Serialization/JsonNetSerializer.cs
+++ src/Umbraco.Infrastructure/Serialization/JsonNetSerializer.cs
@@ -7,23 +7,23 @@ namespace Umbraco.Cms.Infrastructure.Serialization;
 
 public class JsonNetSerializer : IJsonSerializer
 {
-    protected static readonly JsonSerializerSettings JsonSerializerSettings = new()
+    [Obsolete("This is a static field and shared between derived implementations. Use the Settings instance property instead. This field will be removed in v13.")]
+    protected static readonly JsonSerializerSettings JsonSerializerSettings = new();
+
+    protected JsonSerializerSettings Settings { get; } = new()
     {
         Converters = new List<JsonConverter> { new StringEnumConverter() },
         Formatting = Formatting.None,
         NullValueHandling = NullValueHandling.Ignore,
     };
 
-    public string Serialize(object? input) => JsonConvert.SerializeObject(input, JsonSerializerSettings);
+    public string Serialize(object? input) => JsonConvert.SerializeObject(input, Settings);
 
-    public T? Deserialize<T>(string input) => JsonConvert.DeserializeObject<T>(input, JsonSerializerSettings);
+    public T? Deserialize<T>(string input) => JsonConvert.DeserializeObject<T>(input, Settings);
 
     public T? DeserializeSubset<T>(string input, string key)
     {
-        if (key == null)
-        {
-            throw new ArgumentNullException(nameof(key));
-        }
+        ArgumentNullException.ThrowIfNull(key);
 
         JObject? root = Deserialize<JObject>(input);
         JToken? jToken = root?.SelectToken(key);
