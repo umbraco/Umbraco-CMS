94094a4eb9ee394cb17c5daabd523100c4dc60c0
Mads Rasmussen <madsr@hey.com>
2023-09-15 09:44:17 +0200

    add mock handler for move to recycle bin

--- src/Umbraco.Web.UI.Client/src/mocks/handlers/document/document.handlers.ts
+++ src/Umbraco.Web.UI.Client/src/mocks/handlers/document/document.handlers.ts
@@ -4,45 +4,35 @@ import { umbracoPath } from '@umbraco-cms/backoffice/utils';
 
 // TODO: add schema
 export const handlers = [
-	rest.post<string[]>(umbracoPath('/document/trash'), async (req, res, ctx) => {
-		console.warn('Please move to schema');
-		const ids = await req.json();
-
-		const trashed = umbDocumentData.trash(ids);
-
-		return res(ctx.status(200), ctx.json(trashed));
-	}),
-
 	rest.get(umbracoPath('/document/root/allowed-document-types'), (req, res, ctx) => {
 		const response = umbDocumentData.getAllowedDocumentTypesAtRoot();
-
 		return res(ctx.status(200), ctx.json(response));
+	}),
+
+	rest.put(umbracoPath('/document/:id/move-to-recycle-bin'), async (req, res, ctx) => {
+		const id = req.params.id as string;
+		umbDocumentData.trash([id]);
+		return res(ctx.status(200));
 	}),
 
 	rest.get(umbracoPath('/document/:id/allowed-document-types'), (req, res, ctx) => {
 		const id = req.params.id as string;
 		if (!id) return;
-
 		const response = umbDocumentData.getDocumentByIdAllowedDocumentTypes(id);
-
 		return res(ctx.status(200), ctx.json(response));
 	}),
 
 	rest.get(umbracoPath('/document/:id'), (req, res, ctx) => {
 		const id = req.params.id as string;
 		if (!id) return;
-
 		const document = umbDocumentData.getById(id);
-
 		return res(ctx.status(200), ctx.json(document));
 	}),
 
 	rest.delete(umbracoPath('/document/:id'), (req, res, ctx) => {
 		const id = req.params.id as string;
 		if (!id) return;
-
 		const document = umbDocumentData.getById(id);
-
 		return res(ctx.status(200), ctx.json(document));
 	}),
 
@@ -51,18 +41,14 @@ export const handlers = [
 		if (!id) return;
 		const data = await req.json();
 		if (!data) return;
-
 		const saved = umbDocumentData.save(id, data);
-
 		return res(ctx.status(200), ctx.json(saved));
 	}),
 
 	rest.post(umbracoPath(`/document`), async (req, res, ctx) => {
 		const data = await req.json();
 		if (!data) return;
-
 		umbDocumentData.insert(data);
-
 		return res(ctx.status(200));
 	}),
 ];
