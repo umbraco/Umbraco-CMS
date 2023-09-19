ec4a34279a8c41dacc7974cb3a4d9061375b4388
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2023-09-19 19:32:00 +0200

    Implement MemberTypeContainerRepository

--- src/Umbraco.Infrastructure/Persistence/Repositories/Implement/MemberTypeContainerRepository.cs
+++ src/Umbraco.Infrastructure/Persistence/Repositories/Implement/MemberTypeContainerRepository.cs
@@ -1,31 +1,16 @@
+using Microsoft.Extensions.Logging;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Cache;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Persistence.Repositories;
+using Umbraco.Cms.Infrastructure.Scoping;
 
 namespace Umbraco.Cms.Infrastructure.Persistence.Repositories.Implement
 {
-    /// <summary>
-    /// A no-op implementation of <see cref="IMemberTypeContainerRepository"/>, as containers aren't supported for members.
-    /// </summary>
-    /// <remarks>
-    /// Introduced to avoid inconsistencies with nullability of dependencies for type repositories for content, media and members.
-    /// </remarks>
-    internal class MemberTypeContainerRepository : IMemberTypeContainerRepository
+    internal class MemberTypeContainerRepository : EntityContainerRepository, IMemberTypeContainerRepository
     {
-        public void Delete(EntityContainer entity)
-        {
-        }
-
-        public bool Exists(int id) => false;
-
-        public EntityContainer? Get(Guid id) => null;
-
-        public IEnumerable<EntityContainer> Get(string name, int level) => Enumerable.Empty<EntityContainer>();
-
-        public EntityContainer? Get(int id) => null;
-
-        public IEnumerable<EntityContainer> GetMany(params int[]? ids) => Enumerable.Empty<EntityContainer>();
-
-        public void Save(EntityContainer entity)
+        public MemberTypeContainerRepository(IScopeAccessor scopeAccessor, AppCaches cache, ILogger<MemberTypeContainerRepository> logger)
+            : base(scopeAccessor, cache, logger, Constants.ObjectTypes.MemberTypeContainer)
         {
         }
     }
