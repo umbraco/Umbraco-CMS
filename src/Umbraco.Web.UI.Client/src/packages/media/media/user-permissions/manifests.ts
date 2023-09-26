b2e198ea09c4f5e84258d9c5f490e3e353027c30
Mads Rasmussen <madsr@hey.com>
2023-09-26 14:21:25 +0200

    add a couple of media user permissions

---
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/user-permissions/manifests.ts
@@ -0,0 +1,29 @@
+import type { ManifestUserPermission } from '@umbraco-cms/backoffice/extension-registry';
+
+export const UMB_USER_PERMISSION_MEDIA_MOVE = 'Umb.UserPermission.Media.Move';
+export const UMB_USER_PERMISSION_MEDIA_COPY = 'Umb.UserPermission.Media.Copy';
+
+const permissions: Array<ManifestUserPermission> = [
+	{
+		type: 'userPermission',
+		alias: UMB_USER_PERMISSION_MEDIA_MOVE,
+		name: 'Move Media Item User Permission',
+		meta: {
+			entityType: 'media',
+			label: 'Move',
+			description: 'Allow access to move media items',
+		},
+	},
+	{
+		type: 'userPermission',
+		alias: UMB_USER_PERMISSION_MEDIA_COPY,
+		name: 'Copy Media Item User Permission',
+		meta: {
+			entityType: 'media',
+			label: 'Copy',
+			description: 'Allow access to copy a media item',
+		},
+	},
+];
+
+export const manifests = [...permissions];
