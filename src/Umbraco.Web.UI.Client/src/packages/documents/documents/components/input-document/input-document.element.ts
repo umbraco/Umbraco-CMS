72979d35ca3284554388fc06014510104a6ab098
Mads Rasmussen <madsr@hey.com>
2023-09-27 09:52:08 +0200

    update document input to use document input context

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/components/input-document/input-document.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/components/input-document/input-document.element.ts
@@ -1,16 +1,8 @@
-import { UMB_DOCUMENT_TREE_STORE_CONTEXT_TOKEN } from '../../repository/document.tree.store.js';
-import type { UmbDocumentTreeStore } from '../../repository/document.tree.store.js';
-import { css, html, nothing, customElement, property, state, ifDefined } from '@umbraco-cms/backoffice/external/lit';
+import { UmbDocumentPickerContext } from './input-document.context.js';
+import { css, html, customElement, property, state, ifDefined } from '@umbraco-cms/backoffice/external/lit';
 import { FormControlMixin } from '@umbraco-cms/backoffice/external/uui';
-import {
-	UmbModalManagerContext,
-	UMB_MODAL_MANAGER_CONTEXT_TOKEN,
-	UMB_CONFIRM_MODAL,
-	UMB_DOCUMENT_PICKER_MODAL,
-} from '@umbraco-cms/backoffice/modal';
 import { UmbLitElement } from '@umbraco-cms/internal/lit-element';
-import type { DocumentTreeItemResponseModel, EntityTreeItemResponseModel } from '@umbraco-cms/backoffice/backend-api';
-import type { UmbObserverController } from '@umbraco-cms/backoffice/observable-api';
+import type { DocumentItemResponseModel } from '@umbraco-cms/backoffice/backend-api';
 
 @customElement('umb-input-document')
 export class UmbInputDocumentElement extends FormControlMixin(UmbLitElement) {
@@ -18,10 +10,15 @@ export class UmbInputDocumentElement extends FormControlMixin(UmbLitElement) {
 	 * This is a minimum amount of selected items in this input.
 	 * @type {number}
 	 * @attr
-	 * @default undefined
+	 * @default 0
 	 */
 	@property({ type: Number })
-	min?: number;
+	public get min(): number {
+		return this.#pickerContext.min;
+	}
+	public set min(value: number) {
+		this.#pickerContext.min = value;
+	}
 
 	/**
 	 * Min validation message.
@@ -36,10 +33,15 @@ export class UmbInputDocumentElement extends FormControlMixin(UmbLitElement) {
 	 * This is a maximum amount of selected items in this input.
 	 * @type {number}
 	 * @attr
-	 * @default undefined
+	 * @default Infinity
 	 */
 	@property({ type: Number })
-	max?: number;
+	public get max(): number {
+		return this.#pickerContext.max;
+	}
+	public set max(value: number) {
+		this.#pickerContext.max = value;
+	}
 
 	/**
 	 * Max validation message.
@@ -50,30 +52,23 @@ export class UmbInputDocumentElement extends FormControlMixin(UmbLitElement) {
 	@property({ type: String, attribute: 'min-message' })
 	maxMessage = 'This field exceeds the allowed amount of items';
 
-	// TODO: do we need both selectedIds and value? If we just use value we follow the same pattern as native form controls.
-	private _selectedIds: Array<string> = [];
 	public get selectedIds(): Array<string> {
-		return this._selectedIds;
+		return this.#pickerContext.getSelection();
 	}
 	public set selectedIds(ids: Array<string>) {
-		this._selectedIds = ids;
-		super.value = ids.join(',');
-		this._observePickedDocuments();
+		this.#pickerContext.setSelection(ids);
 	}
 
 	@property()
 	public set value(idsString: string) {
-		if (idsString !== this._value) {
-			this.selectedIds = idsString.split(/[ ,]+/);
-		}
+		// Its with full purpose we don't call super.value, as thats being handled by the observation of the context selection.
+		this.selectedIds = idsString.split(/[ ,]+/);
 	}
 
 	@state()
-	private _items?: Array<DocumentTreeItemResponseModel>;
+	private _items?: Array<DocumentItemResponseModel>;
 
-	private _modalContext?: UmbModalManagerContext;
-	private _documentStore?: UmbDocumentTreeStore;
-	private _pickedItemsObserver?: UmbObserverController<EntityTreeItemResponseModel[]>;
+	#pickerContext = new UmbDocumentPickerContext(this);
 
 	constructor() {
 		super();
@@ -81,84 +76,43 @@ export class UmbInputDocumentElement extends FormControlMixin(UmbLitElement) {
 		this.addValidator(
 			'rangeUnderflow',
 			() => this.minMessage,
-			() => !!this.min && this._selectedIds.length < this.min,
+			() => !!this.min && this.#pickerContext.getSelection().length < this.min,
 		);
+
 		this.addValidator(
 			'rangeOverflow',
 			() => this.maxMessage,
-			() => !!this.max && this._selectedIds.length > this.max,
+			() => !!this.max && this.#pickerContext.getSelection().length > this.max,
 		);
 
-		this.consumeContext(UMB_DOCUMENT_TREE_STORE_CONTEXT_TOKEN, (instance) => {
-			this._documentStore = instance;
-			this._observePickedDocuments();
-		});
-		this.consumeContext(UMB_MODAL_MANAGER_CONTEXT_TOKEN, (instance) => {
-			this._modalContext = instance;
-		});
+		this.observe(this.#pickerContext.selection, (selection) => (super.value = selection.join(',')));
+		this.observe(this.#pickerContext.selectedItems, (selectedItems) => (this._items = selectedItems));
 	}
 
 	protected getFormElement() {
 		return undefined;
 	}
 
-	private _observePickedDocuments() {
-		this._pickedItemsObserver?.destroy();
-
-		if (!this._documentStore) return;
-
-		// TODO: consider changing this to the list data endpoint when it is available
-		this._pickedItemsObserver = this.observe(this._documentStore.items(this._selectedIds), (items) => {
-			this._items = items;
-		});
-	}
-
-	private _openPicker() {
-		// We send a shallow copy(good enough as its just an array of ids) of our this._selectedIds, as we don't want the modal to manipulate our data:
-		const modalContext = this._modalContext?.open(UMB_DOCUMENT_PICKER_MODAL, {
-			multiple: this.max === 1 ? false : true,
-			selection: [...this._selectedIds],
-		});
-
-		modalContext?.onSubmit().then(({ selection }: any) => {
-			this._setSelection(selection);
-		});
-	}
-
-	private async _removeItem(item: EntityTreeItemResponseModel) {
-		const modalContext = this._modalContext?.open(UMB_CONFIRM_MODAL, {
-			color: 'danger',
-			headline: `Remove ${item.name}?`,
-			content: 'Are you sure you want to remove this item',
-			confirmLabel: 'Remove',
-		});
-
-		await modalContext?.onSubmit();
-		const newSelection = this._selectedIds.filter((value) => value !== item.id);
-		this._setSelection(newSelection);
-	}
-
-	private _setSelection(newSelection: Array<string>) {
-		this.selectedIds = newSelection;
-		this.dispatchEvent(new CustomEvent('change', { bubbles: true, composed: true }));
-	}
-
 	render() {
 		return html`
-			${this._items?.map((item) => this._renderItem(item))}
-			<uui-button id="add-button" look="placeholder" @click=${this._openPicker} label="open">Add</uui-button>
+			<uui-ref-list>${this._items?.map((item) => this._renderItem(item))}</uui-ref-list>
+			<uui-button id="add-button" look="placeholder" @click=${() => this.#pickerContext.openPicker()} label="open"
+				>Add</uui-button
+			>
 		`;
 	}
 
-	private _renderItem(item: EntityTreeItemResponseModel) {
-		// TODO: remove when we have a way to handle trashed items
-		const tempItem = item as EntityTreeItemResponseModel & { isTrashed: boolean };
-
+	private _renderItem(item: DocumentItemResponseModel) {
+		if (!item.id) return;
 		return html`
-			<uui-ref-node name=${ifDefined(item.name === null ? undefined : item.name)} detail=${ifDefined(item.id)}>
-				${tempItem.isTrashed ? html` <uui-tag size="s" slot="tag" color="danger">Trashed</uui-tag> ` : nothing}
+			<uui-ref-node name=${ifDefined(item.name)} detail=${ifDefined(item.id)}>
+				<!-- TODO: implement is trashed <uui-tag size="s" slot="tag" color="danger">Trashed</uui-tag> -->
 				<uui-action-bar slot="actions">
-					<uui-button @click=${() => this._removeItem(item)} label="Remove document ${item.name}">Remove</uui-button>
+					<uui-button
+						@click=${() => this.#pickerContext.requestRemoveItem(item.id!)}
+						label="Remove document ${item.name}"
+						>Remove</uui-button
+					>
 				</uui-action-bar>
 			</uui-ref-node>
 		`;
