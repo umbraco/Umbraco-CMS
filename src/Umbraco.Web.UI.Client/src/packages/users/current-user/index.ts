45fdc76a8dd154c7a412ac6595b7d10f5ede50be
Mads Rasmussen <madsr@hey.com>
2023-09-28 19:40:09 +0200

    move user permission condition

--- src/Umbraco.Web.UI.Client/src/packages/users/current-user/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/users/current-user/index.ts
@@ -1,3 +1,2 @@
 // TODO:Do not export store, but instead export future repository
 export * from './current-user-history.store.js';
-export * from './conditions/index.js';
---
+++ src/Umbraco.Web.UI.Client/src/packages/users/user-permission/manifests.ts
@@ -0,0 +1,3 @@
+import { manifest as userPermissionConditionManifest } from './conditions/user-permission.condition.js';
+
+export const manifests = [userPermissionConditionManifest];
