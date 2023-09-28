ce86abe8ac05315c1b7d93e4b40dc0aa5cd73dad
Kenn Jacobsen <kja@umbraco.dk>
2023-09-28 13:20:03 +0200

    Add default property value converters for all value types (#14869)
    * Add default property value converters for all value types
    
    * Clean up some left-over stuff

---
+++ src/Umbraco.Core/PropertyEditors/ValueConverters/BigintValueTypeConverter.cs
@@ -0,0 +1,24 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Extensions;
+
+namespace Umbraco.Cms.Core.PropertyEditors.ValueConverters;
+
+[DefaultValueTypePropertyValueConverter]
+public class BigintValueTypeConverter : ValueTypePropertyValueConverterBase
+{
+    protected override string[] SupportedValueTypes => new[] { ValueTypes.Bigint };
+
+    public BigintValueTypeConverter(PropertyEditorCollection propertyEditors)
+        : base(propertyEditors)
+    {
+    }
+
+    public override Type GetPropertyValueType(IPublishedPropertyType propertyType)
+        => typeof(long);
+
+    public override PropertyCacheLevel GetPropertyCacheLevel(IPublishedPropertyType propertyType)
+        => PropertyCacheLevel.Element;
+
+    public override object? ConvertSourceToIntermediate(IPublishedElement owner, IPublishedPropertyType propertyType, object? source, bool preview)
+        => source.TryConvertTo<long>().Result;
+}
