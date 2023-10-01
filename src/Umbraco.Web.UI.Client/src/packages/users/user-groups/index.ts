947b38d4abe624da7fada049267b391d9518d64f
Mads Rasmussen <madsr@hey.com>
2023-10-01 19:25:37 +0200

    export repos

--- src/Umbraco.Web.UI.Client/src/packages/users/user-groups/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/users/user-groups/index.ts
@@ -1,4 +1,5 @@
 export * from './types.js';
 export * from './components/index.js';
+export * from './repository/index.js';
 
 export const UMB_USER_GROUP_ENTITY_TYPE = 'user-group';
---
+++ src/Umbraco.Web.UI.Client/src/packages/users/user-groups/repository/index.ts
@@ -0,0 +1,2 @@
+export * from './user-group.repository.js';
+export * from './collection/user-group-collection.repository.js';
