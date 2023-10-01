75cac73742035a145a401540e84571e85772484d
Mads Rasmussen <madsr@hey.com>
2023-10-01 20:12:05 +0200

    export from collection module

--- src/Umbraco.Web.UI.Client/src/packages/users/user-groups/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/users/user-groups/index.ts
@@ -1,5 +1,6 @@
-export * from './types.js';
+export * from './collection/index.js';
 export * from './components/index.js';
 export * from './repository/index.js';
+export * from './types.js';
 
 export const UMB_USER_GROUP_ENTITY_TYPE = 'user-group';
