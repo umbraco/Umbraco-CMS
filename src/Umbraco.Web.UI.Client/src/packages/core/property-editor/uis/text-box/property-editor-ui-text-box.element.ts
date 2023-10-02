8c6b50085ec4403c730c273ca90f108de16b6a71
Lone Iversen <108085781+loivsen@users.noreply.github.com>
2023-10-02 11:38:02 +0200

    email type

--- src/Umbraco.Web.UI.Client/src/packages/core/property-editor/uis/text-box/property-editor-ui-text-box.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property-editor/uis/text-box/property-editor-ui-text-box.element.ts
@@ -1,29 +1,32 @@
 import { css, html, customElement, property, state, ifDefined } from '@umbraco-cms/backoffice/external/lit';
-import { UmbTextStyles } from "@umbraco-cms/backoffice/style";
+import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
 import { UmbPropertyEditorExtensionElement } from '@umbraco-cms/backoffice/extension-registry';
 import { UmbLitElement } from '@umbraco-cms/internal/lit-element';
 import type { UmbPropertyEditorConfigCollection } from '@umbraco-cms/backoffice/property-editor';
+import { UUIInputElement } from '@umbraco-cms/backoffice/external/uui';
+
+type UuiInputTypeType = typeof UUIInputElement.prototype.type;
 
 @customElement('umb-property-editor-ui-text-box')
 export class UmbPropertyEditorUITextBoxElement extends UmbLitElement implements UmbPropertyEditorExtensionElement {
-	#defaultType = 'text';
+	#defaultType: UuiInputTypeType = 'text';
 
 	@property()
 	value = '';
 
 	@state()
-	private _type?: string;
+	private _type: UuiInputTypeType = this.#defaultType;
 
 	@state()
 	private _maxChars?: number;
 
 	@property({ attribute: false })
 	public set config(config: UmbPropertyEditorConfigCollection | undefined) {
-		this._type = config?.getValueByAlias('inputType') ?? this.#defaultType;
+		this._type = config?.getValueByAlias<UuiInputTypeType>('inputType') ?? this.#defaultType;
 		this._maxChars = config?.getValueByAlias('maxChars');
 	}
 
-	private onInput(e: InputEvent) {
+	private onChange(e: Event) {
 		this.value = (e.target as HTMLInputElement).value;
 		this.dispatchEvent(new CustomEvent('property-value-change'));
 	}
@@ -31,9 +34,9 @@ export class UmbPropertyEditorUITextBoxElement extends UmbLitElement implements 
 	render() {
 		return html`<uui-input
 			.value=${this.value ?? ''}
-			type="${this._type}"
-			maxlength="${ifDefined(this._maxChars)}"
-			@input=${this.onInput}></uui-input>`;
+			.type=${this._type}
+			maxlength=${ifDefined(this._maxChars)}
+			@change=${this.onChange}></uui-input>`;
 	}
 
 	static styles = [
