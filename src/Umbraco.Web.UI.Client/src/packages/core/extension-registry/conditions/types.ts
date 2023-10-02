9dce42825a2ba5f7daabae352757124222c92b4b
Mads Rasmussen <madsr@hey.com>
2023-10-02 13:48:48 +0200

    fix import order

--- src/Umbraco.Web.UI.Client/src/packages/core/extension-registry/conditions/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/extension-registry/conditions/types.ts
@@ -1,8 +1,8 @@
+import type { UserPermissionConditionConfig } from '@umbraco-cms/backoffice/user-permission';
 import type { SectionAliasConditionConfig } from './section-alias.condition.js';
 import type { SwitchConditionConfig } from './switch.condition.js';
 import type { WorkspaceAliasConditionConfig } from '@umbraco-cms/backoffice/workspace';
 import type { UmbConditionConfigBase } from '@umbraco-cms/backoffice/extension-api';
-import type { UserPermissionConditionConfig } from '@umbraco-cms/backoffice/user-permission';
 
 /* TODO: in theory should't the core package import from other packages.
 Are there any other way we can do this? */
