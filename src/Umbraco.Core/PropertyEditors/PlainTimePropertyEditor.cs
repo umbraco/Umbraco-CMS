a59a1a94177f596c7d086f041d86a2ff9c276df8
kjac <kja@umbraco.dk>
2023-10-05 15:24:00 +0200

    Added "plain" property editors for all (meaningful) value types

---
+++ src/Umbraco.Core/PropertyEditors/PlainTimePropertyEditor.cs
@@ -0,0 +1,19 @@
+﻿namespace Umbraco.Cms.Core.PropertyEditors;
+
+/// <summary>
+///     Represents a property editor for configuration-less time properties.
+/// </summary>
+[DataEditor(
+    Constants.PropertyEditors.Aliases.PlainTime,
+    EditorType.PropertyValue,
+    "Configuration-less time",
+    "not-applicable",
+    Icon = "umb:edit",
+    ValueEditorIsReusable = true,
+    ValueType = ValueTypes.Time)]
+public class PlainTimePropertyEditor : DataEditor
+{
+    public PlainTimePropertyEditor(IDataValueEditorFactory dataValueEditorFactory, EditorType type = EditorType.PropertyValue)
+        : base(dataValueEditorFactory, type)
+        => SupportsReadOnly = true;
+}
