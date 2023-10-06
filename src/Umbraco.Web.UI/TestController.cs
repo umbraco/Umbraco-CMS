24a5fa711467eef58a12e4f1dbb357913707d093
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2023-10-06 13:57:06 +0200

    Add Status code to detailed view

--- src/Umbraco.Web.UI/TestController.cs
+++
@@ -1,9 +0,0 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using Umbraco.Cms.Web.Common.Controllers;
-
-namespace Umbraco.Cms.Web.UI;
-
-public class TestController : UmbracoApiController
-{
-    public IActionResult GetAllProducts() => Ok();
-}
