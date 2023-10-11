26c6c876c8a4f28bd744297d4412c1a42bc8c355
Lone Iversen <108085781+loivsen@users.noreply.github.com>
2023-10-11 11:19:37 +0200

    note

--- src/Umbraco.Web.UI.Client/src/external/dompurify/index.ts
+++ src/Umbraco.Web.UI.Client/src/external/dompurify/index.ts
@@ -1,4 +1,7 @@
 import DOMPurify from 'isomorphic-dompurify';
-//Note: isomorphic-dompurify is a project that sets up DOMPurify for us, and is recommended by DOMPurify
+/** The library makes it possible to seamlessly use DOMPurify on server and client in the same way.
+ * It does nothing by itself except providing an isomorphic/universal wrapper around DOMPurify, so all credits go to DOMPurify authors and contributors.
+ * https://github.com/kkomelin/isomorphic-dompurify
+ */
 
 export { DOMPurify };
