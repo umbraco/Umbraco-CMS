df0b8c9e12f925fdec5030e02f6f0a59d37ba386
Mads Rasmussen <madsr@hey.com>
2023-10-11 14:21:24 +0200

    register disable repo

--- src/Umbraco.Web.UI.Client/src/packages/user/user/repository/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/repository/manifests.ts
@@ -1,15 +1,24 @@
 import { UmbUserRepository } from './user.repository.js';
 import { UmbUserItemStore } from './user-item.store.js';
 import { UmbUserStore } from './user.store.js';
+import { UmbDisableUserRepository } from './disable-user.repository.js';
 import type { ManifestStore, ManifestRepository, ManifestItemStore } from '@umbraco-cms/backoffice/extension-registry';
 
 export const USER_REPOSITORY_ALIAS = 'Umb.Repository.User';
+export const DISABLE_USER_REPOSITORY_ALIAS = 'Umb.Repository.User.Disable';
 
 const repository: ManifestRepository = {
 	type: 'repository',
 	alias: USER_REPOSITORY_ALIAS,
 	name: 'User Repository',
 	api: UmbUserRepository,
+};
+
+const disableRepository: ManifestRepository = {
+	type: 'repository',
+	alias: DISABLE_USER_REPOSITORY_ALIAS,
+	name: 'Disable User Repository',
+	api: UmbDisableUserRepository,
 };
 
 const store: ManifestStore = {
@@ -26,4 +35,4 @@ const itemStore: ManifestItemStore = {
 	api: UmbUserItemStore,
 };
 
-export const manifests = [repository, store, itemStore];
+export const manifests = [repository, disableRepository, store, itemStore];
