9c75508dce90bc0f98960a634c3cefb5f6bdbdea
Mads Rasmussen <madsr@hey.com>
2023-10-12 11:35:26 +0200

    register change password modal

--- src/Umbraco.Web.UI.Client/src/packages/core/modal/token/change-password-modal.token.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/modal/token/change-password-modal.token.ts
@@ -4,6 +4,12 @@ export interface UmbChangePasswordModalData {
 	requireOldPassword: boolean;
 }
 
+export interface UmbChangePasswordModalValue {
+	newPassword: string;
+	confirmPassword: string;
+	oldPassword?: string;
+}
+
 export const UMB_CHANGE_PASSWORD_MODAL = new UmbModalToken<UmbChangePasswordModalData>('Umb.Modal.ChangePassword', {
 	type: 'dialog',
 });
--- src/Umbraco.Web.UI.Client/src/packages/user/modals/change-password/change-password-modal.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/modals/change-password/change-password-modal.element.ts
@@ -1,21 +1,15 @@
-import { UmbTextStyles } from "@umbraco-cms/backoffice/style";
-import { css, CSSResultGroup, html, nothing, customElement, property } from '@umbraco-cms/backoffice/external/lit';
-import { UmbModalContext, UmbChangePasswordModalData } from '@umbraco-cms/backoffice/modal';
-import { UmbLitElement } from '@umbraco-cms/internal/lit-element';
+import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
+import { css, CSSResultGroup, html, nothing, customElement } from '@umbraco-cms/backoffice/external/lit';
+import { UmbChangePasswordModalData } from '@umbraco-cms/backoffice/modal';
+import { UmbModalBaseElement } from '@umbraco-cms/internal/modal';
 
 @customElement('umb-change-password-modal')
-export class UmbChangePasswordModalElement extends UmbLitElement {
-	@property({ attribute: false })
-	modalContext?: UmbModalContext;
-
-	@property()
-	data?: UmbChangePasswordModalData;
-
-	private _close() {
-		this.modalContext?.submit();
+export class UmbChangePasswordModalElement extends UmbModalBaseElement<UmbChangePasswordModalData> {
+	#onClose() {
+		this.modalContext?.reject();
 	}
 
-	private _handleSubmit(e: SubmitEvent) {
+	#onSubmit(e: SubmitEvent) {
 		e.preventDefault();
 
 		const form = e.target as HTMLFormElement;
@@ -29,29 +23,16 @@ export class UmbChangePasswordModalElement extends UmbLitElement {
 		const oldPassword = formData.get('oldPassword') as string;
 		const newPassword = formData.get('newPassword') as string;
 		const confirmPassword = formData.get('confirmPassword') as string;
-		console.log('IMPLEMENT SUBMIT', { oldPassword, newPassword, confirmPassword });
-		this._close();
-	}
 
-	private _renderOldPasswordInput() {
-		return html`
-			<uui-form-layout-item style="margin-bottom: var(--uui-size-layout-2)">
-				<uui-label id="oldPasswordLabel" for="oldPassword" slot="label" required>Old password</uui-label>
-				<uui-input-password
-					id="oldPassword"
-					name="oldPassword"
-					required
-					required-message="Old password is required"></uui-input-password>
-			</uui-form-layout-item>
-		`;
+		this.modalContext?.submit({ oldPassword, newPassword, confirmPassword });
 	}
 
 	render() {
 		return html`
 			<uui-dialog-layout class="uui-text" headline="Change password">
 				<uui-form>
-					<form id="LoginForm" name="login" @submit="${this._handleSubmit}">
-						${this.data?.requireOldPassword ? this._renderOldPasswordInput() : nothing}
+					<form id="LoginForm" name="login" @submit="${this.#onSubmit}">
+						${this.data?.requireOldPassword ? this.#renderOldPasswordInput() : nothing}
 						<uui-form-layout-item>
 							<uui-label id="newPasswordLabel" for="newPassword" slot="label" required>New password</uui-label>
 							<uui-input-password
@@ -72,12 +53,25 @@ export class UmbChangePasswordModalElement extends UmbLitElement {
 						</uui-form-layout-item>
 
 						<div id="actions">
-							<uui-button @click=${this._close} label="Cancel"></uui-button>
+							<uui-button @click=${this.#onClose} label="Cancel"></uui-button>
 							<uui-button type="submit" label="Confirm" look="primary" color="positive"></uui-button>
 						</div>
 					</form>
 				</uui-form>
 			</uui-dialog-layout>
+		`;
+	}
+
+	#renderOldPasswordInput() {
+		return html`
+			<uui-form-layout-item style="margin-bottom: var(--uui-size-layout-2)">
+				<uui-label id="oldPasswordLabel" for="oldPassword" slot="label" required>Old password</uui-label>
+				<uui-input-password
+					id="oldPassword"
+					name="oldPassword"
+					required
+					required-message="Old password is required"></uui-input-password>
+			</uui-form-layout-item>
 		`;
 	}
 
