2ff4456a25c990e7d937e2023bac79b5a46a3bf9
Niels Lyngsø <niels.lyngso@gmail.com>
2023-10-12 09:38:07 +0200

    styling

--- src/Umbraco.Web.UI.Client/src/views/propertyeditors/rte/blocks/umb-rte-block.component.js
+++ src/Umbraco.Web.UI.Client/src/views/propertyeditors/rte/blocks/umb-rte-block.component.js
@@ -29,18 +29,39 @@
         //$scope.api = model.api;
         //$scope.index = model.index;
 
-        const stylesheet = $scope.block.config.stylesheet + `?umb__rnd=${Umbraco.Sys.ServerVariables.application.cacheBuster}`;
+        const stylesheet = $scope.block.config.stylesheet;
         console.log("$scope.block", $scope.block)
         console.log("$scope.block stylesheet", stylesheet)
 
         var shadowRoot = $element[0].attachShadow({ mode: 'open' });
-        shadowRoot.innerHTML =
-        (stylesheet ? `<style>
-          @import "${stylesheet}"
-        </style>` : '') +
-        `
-            <div class="umb-block-rte--view" ng-include="block.view"></div>
-        `;
+          shadowRoot.innerHTML =
+          `
+              <style>@import "assets/css/icons.css?umb__rnd=${Umbraco.Sys.ServerVariables.application.cacheBuster}"</style>
+              <style>
+                .umb-block-rte--view {
+                  position: relative;
+                }
+                .umb-block-rte--view::after {
+                  position:absolute;
+                  content: '';
+                  inset: 0;
+                  border-style: solid;
+                  border-color: #6ab4f0;
+                  border-color: -webkit-focus-ring-color;
+                  border-width: calc(var(--umb-rte-block--selected, 0) * 2px);
+                }
+              </style>
+
+              ${ stylesheet ? `
+                  <style>@import "${stylesheet}?umb__rnd=${Umbraco.Sys.ServerVariables.application.cacheBuster}"</style>`
+                  : ''
+              }
+
+              <div
+                  class="umb-block-rte--view"
+                  ng-class="{'show-validation': api.internal.showValidation}" ng-include="block.view">
+              </div>
+          `;
         $compile(shadowRoot)($scope);
 
       }
@@ -48,6 +69,7 @@
 
       function onChanges(simpleChanges) {
         console.log("block gets change", simpleChanges);
+        // TODO Make sure to fire something to update/reset $block on data-udi change.
       }
 
   }
