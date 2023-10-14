15114445180aea1c2719012a4d0c7b878afbd2f2
Georgina Bidder <georgina.bidder@truedigital.co.uk>
2023-10-14 14:45:48 +0100

    our umbraco link check

--- tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/HelpPanel/helpLinks.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/HelpPanel/helpLinks.spec.ts
@@ -14,28 +14,44 @@ test.describe("Help panel links", () => {
       await umbracoApi.users.setCurrentLanguage(enCulture);
     });
 
-test("Check the youtube link works as expected", async ({ page, umbracoUi }) => {
-    // Action
-    await umbracoUi.clickElement(umbracoUi.getGlobalHelp());
-    let watchVideoLink = await page.locator('[key="help_umbracoLearningBase"]');
-    await watchVideoLink.click();
+    test("Check the youtube link works as expected", async ({ page, umbracoUi }) => {
+        // Action
+        await umbracoUi.clickElement(umbracoUi.getGlobalHelp());
+        let watchVideoLink = await page.locator('[key="help_umbracoLearningBase"]');
+        await watchVideoLink.click();
 
-    // Wait for a popup event
-    const youtubeConsent = page.waitForEvent("popup");
-    const youtubePopup = await youtubeConsent;
+        // Wait for a popup event
+        const youtubeConsent = page.waitForEvent("popup");
+        const youtubePopup = await youtubeConsent;
 
-    if (youtubePopup) {
-      // If the YouTube consent popup is displayed, interact with it
-      const rejectAllButton = await youtubePopup.locator('[aria-label="Reject all"]').first();
-      await rejectAllButton.waitFor({ state: "visible" });
-      await rejectAllButton.click();
+        if (youtubePopup) {
+        // If the YouTube consent popup is displayed, interact with it
+        const rejectAllButton = await youtubePopup.locator('[aria-label="Reject all"]').first();
+        await rejectAllButton.waitFor({ state: "visible" });
+        await rejectAllButton.click();
 
-      // Assert the URL of the popup
-      await expect(youtubePopup).toHaveURL(/.*UmbracoLearningBase/);
-      await youtubePopup.close();
+        // Assert the URL of the popup
+        await expect(youtubePopup).toHaveURL(/.*UmbracoLearningBase/);
+        await youtubePopup.close();
 
-    } else {
-      await expect(page).toHaveURL(/.*UmbracoLearningBase/);
-    }
-  });
+        } else {
+        await expect(page).toHaveURL(/.*UmbracoLearningBase/);
+        await page.close();
+        }
+    });
+
+    test("Check the Our Umbraco link works as expected", async ({ page, umbracoUi }) => {
+
+         // Action
+         await umbracoUi.clickElement(umbracoUi.getGlobalHelp());
+         let ourUmbracoLink = await page.locator('[key="help_umbracoForum"]');
+         await ourUmbracoLink.click();
+         let ourUmbraco = page.waitForEvent('popup');
+         let ourUmbracoPopup = await ourUmbraco;
+         
+         //Assert
+        await expect(ourUmbracoPopup).toHaveURL(/.*our.umbraco.com/);
+        await ourUmbracoPopup.close();
+    });
+
 });
