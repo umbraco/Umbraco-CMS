11746f4846391978012ee5120407acf83098e290
Mads Rasmussen <madsr@hey.com>
2023-10-16 21:38:50 +0200

    add base condition for user action conditions

--- src/Umbraco.Web.UI.Client/src/packages/user/user/conditions/user-allow-delete-action.condition.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/conditions/user-allow-delete-action.condition.ts
@@ -1,41 +1,23 @@
-import { UmbUserDetail } from '../types.js';
-import { UmbUserWorkspaceContext } from '../workspace/user-workspace.context.js';
-import { UserStateModel } from '@umbraco-cms/backoffice/backend-api';
-import { UmbBaseController } from '@umbraco-cms/backoffice/controller-api';
+import { UmbUserActionConditionBase } from './user-allow-action-base.condition.js';
 import {
 	ManifestCondition,
 	UmbConditionConfigBase,
 	UmbConditionControllerArguments,
-	UmbExtensionCondition,
 } from '@umbraco-cms/backoffice/extension-api';
-import { UMB_WORKSPACE_CONTEXT } from '@umbraco-cms/backoffice/workspace';
-import { isCurrentUser } from '@umbraco-cms/backoffice/current-user';
 
-export class UmbUserAllowDeleteActionCondition extends UmbBaseController implements UmbExtensionCondition {
-	config: UmbConditionConfigBase;
-	permitted = false;
-	#onChange: () => void;
-
+export class UmbUserAllowDeleteActionCondition extends UmbUserActionConditionBase {
 	constructor(args: UmbConditionControllerArguments<UmbConditionConfigBase>) {
-		super(args.host);
-		this.config = args.config;
-		this.#onChange = args.onChange;
-
-		this.consumeContext(UMB_WORKSPACE_CONTEXT, (context) => {
-			const userContext = context as UmbUserWorkspaceContext;
-			this.observe(userContext.data, (data) => this.onUserDataChange(data));
-		});
+		super(args);
 	}
 
-	async onUserDataChange(user: UmbUserDetail | undefined) {
+	async onUserDataChange() {
 		// don't allow the current user to delete themselves
-		if (!user || !user.id || (await isCurrentUser(this._host, user.id))) {
+		if (!this.userData || !this.userData.id || (await this.isCurrentUser())) {
 			this.permitted = false;
 		} else {
 			this.permitted = true;
 		}
-
-		this.#onChange();
+		super.onUserDataChange();
 	}
 }
 
--- src/Umbraco.Web.UI.Client/src/packages/user/user/conditions/user-allow-resend-invite-action.condition.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/conditions/user-allow-resend-invite-action.condition.ts
@@ -1,41 +1,25 @@
-import { UmbUserDetail } from '../types.js';
-import { UmbUserWorkspaceContext } from '../workspace/user-workspace.context.js';
+import { UmbUserActionConditionBase } from './user-allow-action-base.condition.js';
 import { UserStateModel } from '@umbraco-cms/backoffice/backend-api';
-import { UmbBaseController } from '@umbraco-cms/backoffice/controller-api';
-import { isCurrentUser } from '@umbraco-cms/backoffice/current-user';
 import {
 	ManifestCondition,
 	UmbConditionConfigBase,
 	UmbConditionControllerArguments,
-	UmbExtensionCondition,
 } from '@umbraco-cms/backoffice/extension-api';
-import { UMB_WORKSPACE_CONTEXT } from '@umbraco-cms/backoffice/workspace';
 
-export class UmbUserAllowResendInviteActionCondition extends UmbBaseController implements UmbExtensionCondition {
-	config: UmbConditionConfigBase;
-	permitted = false;
-	#onChange: () => void;
-
+export class UmbUserAllowResendInviteActionCondition extends UmbUserActionConditionBase {
 	constructor(args: UmbConditionControllerArguments<UmbConditionConfigBase>) {
-		super(args.host);
-		this.config = args.config;
-		this.#onChange = args.onChange;
-
-		this.consumeContext(UMB_WORKSPACE_CONTEXT, (context) => {
-			const userContext = context as UmbUserWorkspaceContext;
-			this.observe(userContext.data, (data) => this.onUserDataChange(data));
-		});
+		super(args);
 	}
 
-	async onUserDataChange(user: UmbUserDetail | undefined) {
-		if (!user || !user.id) {
+	async onUserDataChange() {
+		if (!this.userData || !this.userData.id) {
 			this.permitted = false;
-			this.#onChange();
+			super.onUserDataChange();
 			return;
 		}
 
-		this.permitted = user?.state === UserStateModel.INVITED;
-		this.#onChange();
+		this.permitted = this.userData?.state === UserStateModel.INVITED;
+		super.onUserDataChange();
 	}
 }
 
