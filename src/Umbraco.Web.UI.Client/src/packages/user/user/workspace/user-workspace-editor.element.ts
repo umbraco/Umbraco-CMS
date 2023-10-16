bb08d375870c1f7a167dfb4b808a4a73a1d27a63
Mads Rasmussen <madsr@hey.com>
2023-10-16 08:27:13 +0200

    add component for workspace user info

--- src/Umbraco.Web.UI.Client/src/packages/user/user/workspace/user-workspace-editor.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/workspace/user-workspace-editor.element.ts
@@ -1,16 +1,15 @@
-import { getDisplayStateFromUserStatus } from '../../utils.js';
 import { UMB_USER_ENTITY_TYPE, type UmbUserDetail } from '../index.js';
 import { UmbUserWorkspaceContext } from './user-workspace.context.js';
 import { UUIInputElement, UUIInputEvent } from '@umbraco-cms/backoffice/external/uui';
-import { css, html, nothing, customElement, state, ifDefined } from '@umbraco-cms/backoffice/external/lit';
+import { css, html, nothing, customElement, state } from '@umbraco-cms/backoffice/external/lit';
 import { UmbLitElement } from '@umbraco-cms/internal/lit-element';
 import { UMB_WORKSPACE_CONTEXT } from '@umbraco-cms/backoffice/workspace';
-import { UserStateModel } from '@umbraco-cms/backoffice/backend-api';
 import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
 
 // Import of local components that should only be used here
 import './components/user-workspace-profile-settings/user-workspace-profile-settings.element.js';
 import './components/user-workspace-access-settings/user-workspace-access-settings.element.js';
+import './components/user-workspace-info/user-workspace-info.element.js';
 
 @customElement('umb-user-workspace-editor')
 export class UmbUserWorkspaceEditorElement extends UmbLitElement {
@@ -81,60 +80,12 @@ export class UmbUserWorkspaceEditorElement extends UmbLitElement {
 	#renderRightColumn() {
 		if (!this._user || !this.#workspaceContext) return nothing;
 
-		const displayState = getDisplayStateFromUserStatus(this._user.state);
-
 		return html`
-			<uui-box>
-				<div id="user-info">
-					<uui-avatar .name=${this._user?.name || ''}></uui-avatar>
-					<uui-button label=${this.localize.term('user_changePhoto')}></uui-button>
-					<hr />
-
-					<div>
-						<b><umb-localize key="general_status">Status</umb-localize>:</b>
-						<uui-tag look="${ifDefined(displayState?.look)}" color="${ifDefined(displayState?.color)}">
-							${this.localize.term('user_' + displayState.key)}
-						</uui-tag>
-					</div>
-
-					${this._user?.state === UserStateModel.INVITED
-						? html`
-								<uui-textarea placeholder=${this.localize.term('placeholders_enterMessage')}></uui-textarea>
-								<uui-button look="primary" label=${this.localize.term('actions_resendInvite')}></uui-button>
-						  `
-						: nothing}
-					${this.#renderInfoItem(
-						'user_lastLogin',
-						this.localize.date(this._user.lastLoginDate!) ||
-							`${this._user.name + ' ' + this.localize.term('user_noLogin')} `,
-					)}
-					${this.#renderInfoItem('user_failedPasswordAttempts', this._user.failedLoginAttempts)}
-					${this.#renderInfoItem(
-						'user_lastLockoutDate',
-						this._user.lastLockoutDate || `${this._user.name + ' ' + this.localize.term('user_noLockouts')}`,
-					)}
-					${this.#renderInfoItem(
-						'user_lastPasswordChangeDate',
-						this._user.lastLoginDate || `${this._user.name + ' ' + this.localize.term('user_noPasswordChange')}`,
-					)}
-					${this.#renderInfoItem('user_createDate', this.localize.date(this._user.createDate!))}
-					${this.#renderInfoItem('user_updateDate', this.localize.date(this._user.updateDate!))}
-					${this.#renderInfoItem('general_id', this._user.id)}
-				</div>
-			</uui-box>
+			<umb-user-workspace-info></umb-user-workspace-info>
 
 			<uui-box>
 				<umb-entity-action-list .entityType=${UMB_USER_ENTITY_TYPE} .unique=${this._user.id}></umb-entity-action-list>
 			</uui-box>
-		`;
-	}
-
-	#renderInfoItem(labelkey: string, value?: string | number) {
-		return html`
-			<div>
-				<b><umb-localize key=${labelkey}></umb-localize></b>
-				<span>${value}</span>
-			</div>
 		`;
 	}
 
@@ -163,27 +114,6 @@ export class UmbUserWorkspaceEditorElement extends UmbLitElement {
 				display: flex;
 				flex-direction: column;
 				gap: var(--uui-size-space-4);
-			}
-			#right-column > uui-box > div {
-				display: flex;
-				flex-direction: column;
-				gap: var(--uui-size-space-2);
-			}
-			uui-avatar {
-				font-size: var(--uui-size-16);
-				place-self: center;
-			}
-
-			uui-tag {
-				width: fit-content;
-			}
-			#user-info {
-				display: flex;
-				gap: var(--uui-size-space-6);
-			}
-			#user-info > div {
-				display: flex;
-				flex-direction: column;
 			}
 		`,
 	];
