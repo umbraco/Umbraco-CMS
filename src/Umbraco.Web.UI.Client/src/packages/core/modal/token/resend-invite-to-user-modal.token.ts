373138167dcfb8b2070ecaaf3f0edb0a80e15e32
Mads Rasmussen <madsr@hey.com>
2023-10-16 11:53:49 +0200

    add modal token to resend invite

---
+++ src/Umbraco.Web.UI.Client/src/packages/core/modal/token/resend-invite-to-user-modal.token.ts
@@ -0,0 +1,6 @@
+import { UmbModalToken } from '@umbraco-cms/backoffice/modal';
+
+export const UMB_RESEND_INVITE_TO_USER_MODAL = new UmbModalToken('Umb.Modal.User.ResendInvite', {
+	type: 'dialog',
+	size: 'small',
+});
