c28bdada16bcc506d679947840e7037f7d7dd071
Mads Rasmussen <madsr@hey.com>
2023-10-16 12:14:25 +0200

    remove unused repo

--- src/Umbraco.Web.UI.Client/src/packages/user/user/entity-actions/resend-invite/resend-invite-to-user.action.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/entity-actions/resend-invite/resend-invite-to-user.action.ts
@@ -1,5 +1,4 @@
 import { type UmbEnableUserRepository } from '../../repository/enable/enable-user.repository.js';
-import { UmbUserRepository } from '../../repository/user.repository.js';
 import { UmbEntityActionBase } from '@umbraco-cms/backoffice/entity-action';
 import { UmbContextConsumerController } from '@umbraco-cms/backoffice/context-api';
 import { UmbControllerHostElement } from '@umbraco-cms/backoffice/controller-api';
@@ -11,12 +10,9 @@ import {
 
 export class UmbResendInviteToUserEntityAction extends UmbEntityActionBase<UmbEnableUserRepository> {
 	#modalManager?: UmbModalManagerContext;
-	#userRepository: UmbUserRepository;
 
 	constructor(host: UmbControllerHostElement, repositoryAlias: string, unique: string) {
 		super(host, repositoryAlias, unique);
-
-		this.#userRepository = new UmbUserRepository(this.host);
 
 		new UmbContextConsumerController(this.host, UMB_MODAL_MANAGER_CONTEXT_TOKEN, (instance) => {
 			this.#modalManager = instance;
