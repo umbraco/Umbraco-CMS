14584f16d13697072d9a7c40ab8d6eb4e6d276b2
Mads Rasmussen <madsr@hey.com>
2023-10-16 12:03:02 +0200

    register resend invite modal + align aliases

--- src/Umbraco.Web.UI.Client/src/packages/user/user/modals/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/modals/manifests.ts
@@ -3,19 +3,25 @@ import type { ManifestModal } from '@umbraco-cms/backoffice/extension-registry';
 const modals: Array<ManifestModal> = [
 	{
 		type: 'modal',
-		alias: 'Umb.Modal.CreateUser',
+		alias: 'Umb.Modal.User.Create',
 		name: 'Create User Modal',
 		loader: () => import('./create/user-create-modal.element.js'),
 	},
 	{
 		type: 'modal',
-		alias: 'Umb.Modal.InviteUser',
+		alias: 'Umb.Modal.User.Invite',
 		name: 'Invite User Modal',
 		loader: () => import('./invite/user-invite-modal.element.js'),
 	},
 	{
 		type: 'modal',
-		alias: 'Umb.Modal.UserPicker',
+		alias: 'Umb.Modal.User.ResendInvite',
+		name: 'Resend Invite to User Modal',
+		loader: () => import('./resend-invite/resend-invite-to-user-modal.element.js'),
+	},
+	{
+		type: 'modal',
+		alias: 'Umb.Modal.User.Picker',
 		name: 'User Picker Modal',
 		loader: () => import('./user-picker/user-picker-modal.element.js'),
 	},
