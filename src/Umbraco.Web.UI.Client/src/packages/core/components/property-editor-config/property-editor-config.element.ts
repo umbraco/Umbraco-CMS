af69f7851385447358d27a10dc24a29e7627935e
Mads Rasmussen <madsr@hey.com>
2023-10-17 11:32:53 +0200

    remove unused props

--- src/Umbraco.Web.UI.Client/src/packages/core/components/property-editor-config/property-editor-config.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/components/property-editor-config/property-editor-config.element.ts
@@ -1,4 +1,4 @@
-import { html, customElement, property, state, ifDefined } from '@umbraco-cms/backoffice/external/lit';
+import { html, customElement, state, ifDefined, repeat } from '@umbraco-cms/backoffice/external/lit';
 import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
 import { PropertyEditorConfigProperty } from '@umbraco-cms/backoffice/extension-registry';
 
@@ -15,16 +15,6 @@ export class UmbPropertyEditorConfigElement extends UmbLitElement {
 	// TODO: Make this element generic, so its not bound to DATA-TYPEs. This will require moving some functionality of Data-Type-Context to this. and this might need to self provide a variant Context for its inner property editor UIs.
 	#variantContext?: typeof UMB_DATA_TYPE_VARIANT_CONTEXT.TYPE;
 
-	/**
-	 * Data. The element will render configuration editors with values from this data.
-	 * If a value is not found in this data, the element will use the default value from the configuration.
-	 * @type {Array<{ alias: string; value: unknown }>}
-	 * @attr
-	 * @default []
-	 */
-	@property({ type: Array })
-	public data: Array<{ alias: string; value: unknown }> = [];
-
 	@state()
 	private _properties: Array<PropertyEditorConfigProperty> = [];
 
@@ -33,34 +23,35 @@ export class UmbPropertyEditorConfigElement extends UmbLitElement {
 
 		this.consumeContext(UMB_DATA_TYPE_VARIANT_CONTEXT, (instance) => {
 			this.#variantContext = instance;
-			this.observe(
-				this.#variantContext.properties,
-				(properties) => {
-					console.log('properties', properties);
-					this._properties = properties as Array<PropertyEditorConfigProperty>;
-				},
-				'observeProperties',
-			);
+			this.#observeProperties();
 		});
 	}
 
+	#observeProperties() {
+		if (!this.#variantContext) return;
+		this.observe(
+			this.#variantContext.properties,
+			(properties) => {
+				this._properties = properties as Array<PropertyEditorConfigProperty>;
+			},
+			'observeProperties',
+		);
+	}
+
 	render() {
-		return html`
-			${this._properties.length > 0
-				? html`
-						${this._properties?.map(
-							(property) => html`
-								<umb-workspace-property
-									label="${property.label}"
-									description="${ifDefined(property.description)}"
-									alias="${property.alias}"
-									property-editor-ui-alias="${property.propertyEditorUiAlias}"
-									.config=${property.config}></umb-workspace-property>
-							`,
-						)}
-				  `
-				: html`<div>No configuration</div>`}
-		`;
+		return this._properties.length > 0
+			? repeat(
+					this._properties,
+					(property) => property.alias,
+					(property) =>
+						html`<umb-workspace-property
+							label="${property.label}"
+							description="${ifDefined(property.description)}"
+							alias="${property.alias}"
+							property-editor-ui-alias="${property.propertyEditorUiAlias}"
+							.config=${property.config}></umb-workspace-property>`,
+			  )
+			: html`<div>No configuration</div>`;
 	}
 
 	static styles = [UmbTextStyles];
