bde7d1f36f1fd5292e954c9c1c3ef947fd89b6b9
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-10-24 14:04:35 +0200

    add UseBackOfficeCors for debug mode in executable to allow connections from the Vite dev-server

--- src/Umbraco.Web.UI.New/Program.cs
+++ src/Umbraco.Web.UI.New/Program.cs
@@ -11,6 +11,14 @@ WebApplication app = builder.Build();
 
 await app.BootUmbracoAsync();
 
+#if DEBUG
+if (app.Environment.IsDevelopment())
+{
+    Console.WriteLine(@"Enabling CORS in development mode");
+    app.UseBackOfficeCors("http://127.0.0.1:5173", "http://localhost:5173", "https://127.0.0.1:5173", "https://localhost:5173");
+}
+#endif
+
 #if (UseHttpsRedirect)
 app.UseHttpsRedirection();
 #endif
