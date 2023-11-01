49f521e2dca6acb9f37c3db6274954725fb129ce
Jesper Møller Jensen <26099018+JesmoDev@users.noreply.github.com>
2023-11-01 20:57:54 +1300

    init workspace views

--- src/Umbraco.Web.UI.Client/src/packages/media/media-types/workspace/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media-types/workspace/manifests.ts
@@ -15,7 +15,62 @@ const workspace: ManifestWorkspace = {
 	},
 };
 
-const workspaceViews: Array<ManifestWorkspaceEditorView> = [];
+const workspaceViews: Array<ManifestWorkspaceEditorView> = [
+	{
+		type: 'workspaceEditorView',
+		alias: 'Umb.WorkspaceView.MediaType.Design',
+		name: 'Media Type Workspace Design View',
+		loader: () => import('./views/details/media-type-design-workspace-view.element.js'),
+		weight: 90,
+		meta: {
+			label: 'Details',
+			pathname: 'details',
+			icon: 'document',
+		},
+		conditions: [
+			{
+				alias: 'Umb.Condition.WorkspaceAlias',
+				match: workspace.alias,
+			},
+		],
+	},
+	{
+		type: 'workspaceEditorView',
+		alias: 'Umb.WorkspaceView.MediaType.ListView',
+		name: 'Media Type Workspace ListView View',
+		loader: () => import('./views/details/media-type-list-view-workspace-view.element.js'),
+		weight: 90,
+		meta: {
+			label: 'List View',
+			pathname: 'list-view',
+			icon: 'bug',
+		},
+		conditions: [
+			{
+				alias: 'Umb.Condition.WorkspaceAlias',
+				match: workspace.alias,
+			},
+		],
+	},
+	{
+		type: 'workspaceEditorView',
+		alias: 'Umb.WorkspaceView.MediaType.Permissions',
+		name: 'Media Type Workspace Permissions View',
+		loader: () => import('./views/details/media-type-permissions-workspace-view.element.js'),
+		weight: 90,
+		meta: {
+			label: 'Permissions',
+			pathname: 'permissions',
+			icon: 'bug',
+		},
+		conditions: [
+			{
+				alias: 'Umb.Condition.WorkspaceAlias',
+				match: workspace.alias,
+			},
+		],
+	},
+];
 const workspaceViewCollections: Array<ManifestWorkspaceViewCollection> = [];
 const workspaceActions: Array<ManifestWorkspaceAction> = [];
 
