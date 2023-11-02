07840ea99ed3dc20c8c3e212213e3804a60b712a
Mads Rasmussen <madsr@hey.com>
2023-11-02 12:22:20 +0100

    remove getCollection from interface

--- src/Umbraco.Web.UI.Client/src/packages/core/repository/data-source/collection-data-source.interface.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/repository/data-source/collection-data-source.interface.ts
@@ -2,6 +2,5 @@ import type { UmbPagedData } from '../tree-repository.interface.js';
 import type { DataSourceResponse } from '../index.js';
 
 export interface UmbCollectionDataSource<ItemType = any, PagedItemType = UmbPagedData<ItemType>> {
-	getCollection(): Promise<DataSourceResponse<PagedItemType>>;
 	filterCollection(filter: any): Promise<DataSourceResponse<PagedItemType>>;
 }
