181c5acd23ad9e4018d6de257cdf33ac96d23ed2
Mads Rasmussen <madsr@hey.com>
2023-11-02 10:26:00 +0100

    use umb state instead of behaviour subject

--- src/Umbraco.Web.UI.Client/src/shared/auth/auth.context.ts
+++ src/Umbraco.Web.UI.Client/src/shared/auth/auth.context.ts
@@ -3,14 +3,16 @@ import { UmbAuthFlow } from './auth-flow.js';
 import { UmbLoggedInUser } from './types.js';
 import { UserResource } from '@umbraco-cms/backoffice/backend-api';
 import { UmbControllerHostElement } from '@umbraco-cms/backoffice/controller-api';
-import { UmbObjectState } from '@umbraco-cms/backoffice/observable-api';
+import { UmbBooleanState, UmbObjectState } from '@umbraco-cms/backoffice/observable-api';
 import { tryExecuteAndNotify } from '@umbraco-cms/backoffice/resources';
-import { BehaviorSubject, firstValueFrom } from '@umbraco-cms/backoffice/external/rxjs';
+import { firstValueFrom } from '@umbraco-cms/backoffice/external/rxjs';
 
 export class UmbAuthContext implements IUmbAuth {
+
 	#currentUser = new UmbObjectState<UmbLoggedInUser | undefined>(undefined);
 	readonly currentUser = this.#currentUser.asObservable();
-	readonly isLoggedIn = new BehaviorSubject<boolean>(false);
+	
+	readonly isLoggedIn = new UmbBooleanState<boolean>(false);
 	readonly languageIsoCode = this.#currentUser.asObservablePart((user) => user?.languageIsoCode ?? 'en-us');
 
 	#host;
