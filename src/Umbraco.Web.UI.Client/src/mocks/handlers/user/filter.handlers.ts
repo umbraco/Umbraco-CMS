468763e74dfd9250bea4c1a7fa7c976c582a72ec
Mads Rasmussen <madsr@hey.com>
2023-11-02 15:11:04 +0100

    get all filter values from the mocked request

--- src/Umbraco.Web.UI.Client/src/mocks/handlers/user/filter.handlers.ts
+++ src/Umbraco.Web.UI.Client/src/mocks/handlers/user/filter.handlers.ts
@@ -5,7 +5,18 @@ import { umbracoPath } from '@umbraco-cms/backoffice/utils';
 
 export const handlers = [
   rest.get(umbracoPath(`${slug}/filter`), (req, res, ctx) => {
-    const response = umbUsersData.filter();
+
+    const filter = {
+      skip: Number(req.url.searchParams.get('skip')),
+      take: Number(req.url.searchParams.get('take')),
+      orderBy: req.url.searchParams.get('orderBy'),
+      orderDirection: req.url.searchParams.get('orderDirection'),
+      userGroupIds: req.url.searchParams.getAll('userGroupIds'),
+      userStates: req.url.searchParams.getAll('userStates'),
+      filter: req.url.searchParams.get('filter'),
+    };
+
+    const response = umbUsersData.filter(filter);
     return res(ctx.status(200), ctx.json(response));
   }),
 ];
