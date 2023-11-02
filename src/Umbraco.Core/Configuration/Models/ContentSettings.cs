2a87f1d723ced60595d5490d8eb732bcb5046fd5
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-11-02 09:53:35 +0100

    replace background image

--- src/Umbraco.Core/Configuration/Models/ContentSettings.cs
+++ src/Umbraco.Core/Configuration/Models/ContentSettings.cs
@@ -152,7 +152,7 @@ public class ContentSettings
     internal const string StaticMacroErrors = "Inline";
     internal const string StaticDisallowedUploadFiles = "ashx,aspx,ascx,config,cshtml,vbhtml,asmx,air,axd,xamlx";
     internal const bool StaticShowDeprecatedPropertyEditors = false;
-    internal const string StaticLoginBackgroundImage = "assets/img/login.svg";
+    internal const string StaticLoginBackgroundImage = "assets/img/login.jpg";
     internal const string StaticLoginLogoImage = "assets/img/application/umbraco_logo_white.svg";
     internal const string StaticLoginLogoImageDark = "assets/img/application/umbraco_logo_blue.svg";
     internal const bool StaticHideBackOfficeLogo = false;
