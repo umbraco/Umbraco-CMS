da3a2f12103a4ced7f724156c2d3906706fe2564
Elitsa <elm@umbraco.dk>
2023-11-03 13:37:44 +0100

    Add default implementation for a single overload

--- src/Umbraco.Cms.Api.Management/Security/Authorization/Media/IMediaPermissionAuthorizer.cs
+++ src/Umbraco.Cms.Api.Management/Security/Authorization/Media/IMediaPermissionAuthorizer.cs
@@ -8,6 +8,15 @@ namespace Umbraco.Cms.Api.Management.Security.Authorization.Media;
 public interface IMediaPermissionAuthorizer
 {
     /// <summary>
+    ///     Authorizes whether the current user has access to the specified media item.
+    /// </summary>
+    /// <param name="currentUser">The current user's principal.</param>
+    /// <param name="mediaKey">The key of the media item to check for.</param>
+    /// <returns>Returns <c>true</c> if authorization is successful, otherwise <c>false</c>.</returns>
+    Task<bool> IsAuthorizedAsync(IPrincipal currentUser, Guid mediaKey)
+        => IsAuthorizedAsync(currentUser, new[] { mediaKey });
+
+    /// <summary>
     ///     Authorizes whether the current user has access to the specified media item(s).
     /// </summary>
     /// <param name="currentUser">The current user's principal.</param>
--- src/Umbraco.Cms.Api.Management/Security/Authorization/UserGroup/IUserGroupPermissionAuthorizer.cs
+++ src/Umbraco.Cms.Api.Management/Security/Authorization/UserGroup/IUserGroupPermissionAuthorizer.cs
@@ -8,6 +8,15 @@ namespace Umbraco.Cms.Api.Management.Security.Authorization.UserGroup;
 public interface IUserGroupPermissionAuthorizer
 {
     /// <summary>
+    ///     Authorizes whether the current user has access to the specified user group.
+    /// </summary>
+    /// <param name="currentUser">The current user's principal.</param>
+    /// <param name="userGroupKey">The key of the user group to check against.</param>
+    /// <returns>Returns <c>true</c> if authorization is successful, otherwise <c>false</c>.</returns>
+    Task<bool> IsAuthorizedAsync(IPrincipal currentUser, Guid userGroupKey)
+        => IsAuthorizedAsync(currentUser, new[] { userGroupKey });
+
+    /// <summary>
     ///     Authorizes whether the current user has access to the specified user group(s).
     /// </summary>
     /// <param name="currentUser">The current user's principal.</param>
