8546c9796a12d6078ca299a1e92a041d021bfea5
Elitsa <elm@umbraco.dk>
2023-11-07 13:58:37 +0100

    Save policies

--- src/Umbraco.Cms.Api.Management/Controllers/Document/CreateDocumentController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/Document/CreateDocumentController.cs
@@ -1,28 +1,38 @@
 ﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Api.Management.Factories;
+using Umbraco.Cms.Api.Management.Security.Authorization.Content;
 using Umbraco.Cms.Api.Management.ViewModels.Document;
 using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Actions;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Models.ContentEditing;
 using Umbraco.Cms.Core.Security;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Core.Services.OperationStatus;
+using Umbraco.Cms.Web.Common.Authorization;
 
 namespace Umbraco.Cms.Api.Management.Controllers.Document;
 
 [ApiVersion("1.0")]
 public class CreateDocumentController : DocumentControllerBase
 {
-    private readonly IContentEditingService _contentEditingService;
+    private readonly IAuthorizationService _authorizationService;
     private readonly IDocumentEditingPresentationFactory _documentEditingPresentationFactory;
+    private readonly IContentEditingService _contentEditingService;
     private readonly IBackOfficeSecurityAccessor _backOfficeSecurityAccessor;
 
-    public CreateDocumentController(IContentEditingService contentEditingService, IDocumentEditingPresentationFactory documentEditingPresentationFactory, IBackOfficeSecurityAccessor backOfficeSecurityAccessor)
+    public CreateDocumentController(
+        IAuthorizationService authorizationService,
+        IDocumentEditingPresentationFactory documentEditingPresentationFactory,
+        IContentEditingService contentEditingService,
+        IBackOfficeSecurityAccessor backOfficeSecurityAccessor)
     {
-        _contentEditingService = contentEditingService;
+        _authorizationService = authorizationService;
         _documentEditingPresentationFactory = documentEditingPresentationFactory;
+        _contentEditingService = contentEditingService;
         _backOfficeSecurityAccessor = backOfficeSecurityAccessor;
     }
 
@@ -33,6 +43,25 @@ public class CreateDocumentController : DocumentControllerBase
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Create(CreateDocumentRequestModel requestModel)
     {
+        AuthorizationResult authorizationResult;
+
+        if (requestModel.ParentId is null)
+        {
+            authorizationResult = await _authorizationService.AuthorizeAsync(User, new[] { ActionNew.ActionLetter },
+                $"New{AuthorizationPolicies.ContentPermissionAtRoot}");
+        }
+        else
+        {
+            var resource = new ContentPermissionResource((Guid)requestModel.ParentId, ActionNew.ActionLetter);
+            authorizationResult = await _authorizationService.AuthorizeAsync(User, resource,
+                $"New{AuthorizationPolicies.ContentPermissionByResource}");
+        }
+
+        if (!authorizationResult.Succeeded)
+        {
+            return Forbidden();
+        }
+
         ContentCreateModel model = _documentEditingPresentationFactory.MapCreateModel(requestModel);
         Attempt<IContent?, ContentEditingOperationStatus> result = await _contentEditingService.CreateAsync(model, CurrentUserKey(_backOfficeSecurityAccessor));
 
--- src/Umbraco.Cms.Api.Management/Controllers/Document/UpdateDocumentController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/Document/UpdateDocumentController.cs
@@ -1,29 +1,36 @@
 ﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Api.Management.Factories;
+using Umbraco.Cms.Api.Management.Security.Authorization.Content;
 using Umbraco.Cms.Api.Management.ViewModels.Document;
 using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Actions;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Models.ContentEditing;
 using Umbraco.Cms.Core.Security;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Core.Services.OperationStatus;
+using Umbraco.Cms.Web.Common.Authorization;
 
 namespace Umbraco.Cms.Api.Management.Controllers.Document;
 
 [ApiVersion("1.0")]
 public class UpdateDocumentController : DocumentControllerBase
 {
+    private readonly IAuthorizationService _authorizationService;
     private readonly IContentEditingService _contentEditingService;
     private readonly IDocumentEditingPresentationFactory _documentEditingPresentationFactory;
     private readonly IBackOfficeSecurityAccessor _backOfficeSecurityAccessor;
 
     public UpdateDocumentController(
+        IAuthorizationService authorizationService,
         IContentEditingService contentEditingService,
         IDocumentEditingPresentationFactory documentEditingPresentationFactory,
         IBackOfficeSecurityAccessor backOfficeSecurityAccessor)
     {
+        _authorizationService = authorizationService;
         _contentEditingService = contentEditingService;
         _documentEditingPresentationFactory = documentEditingPresentationFactory;
         _backOfficeSecurityAccessor = backOfficeSecurityAccessor;
@@ -36,6 +43,15 @@ public class UpdateDocumentController : DocumentControllerBase
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
     public async Task<IActionResult> Update(Guid id, UpdateDocumentRequestModel requestModel)
     {
+        var resource = new ContentPermissionResource(id, ActionUpdate.ActionLetter);
+        AuthorizationResult authorizationResult = await _authorizationService.AuthorizeAsync(User, resource,
+            $"New{AuthorizationPolicies.ContentPermissionByResource}");
+
+        if (!authorizationResult.Succeeded)
+        {
+            return Forbidden();
+        }
+
         IContent? content = await _contentEditingService.GetAsync(id);
         if (content == null)
         {
