33b67cacab733fb2f4f47dde5114a9b4cee730aa
Mads Rasmussen <madsr@hey.com>
2023-11-08 14:34:57 +0100

    add repository to upload temporary files

--- src/Umbraco.Web.UI.Client/src/packages/core/temporary-file/temporary-file.repository.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/temporary-file/temporary-file.repository.ts
@@ -1,5 +1,54 @@
-export class UmbTemporaryFileRepository {
-	constructor() {
-		debugger;
+import { UmbTemporaryFileServerDataSource } from './temporary-file.server.data-source.js';
+import { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
+import { UmbRepositoryBase } from '@umbraco-cms/backoffice/repository';
+
+/**
+ * A repository for uploading temporary files
+ * @export
+ * @class UmbTemporaryFileRepository
+ * @extends {UmbRepositoryBase}
+ */
+export class UmbTemporaryFileRepository extends UmbRepositoryBase {
+	#source: UmbTemporaryFileServerDataSource;
+
+	/**
+	 * Creates an instance of UmbTemporaryFileRepository.
+	 * @param {UmbControllerHost} host
+	 * @memberof UmbTemporaryFileRepository
+	 */
+	constructor(host: UmbControllerHost) {
+		super(host);
+		this.#source = new UmbTemporaryFileServerDataSource(host);
+	}
+
+	/**
+	 * Uploads a temporary file
+	 * @param {string} id
+	 * @param {File} file
+	 * @return {*}
+	 * @memberof UmbTemporaryFileRepository
+	 */
+	upload(id: string, file: File) {
+		return this.#source.create('id', file);
+	}
+
+	/**
+	 * Deletes a temporary file
+	 * @param {string} id
+	 * @return {*}
+	 * @memberof UmbTemporaryFileRepository
+	 */
+	delete(id: string) {
+		return this.#source.delete(id);
+	}
+
+	/**
+	 * Gets a temporary file
+	 * @param {string} id
+	 * @return {*}
+	 * @memberof UmbTemporaryFileRepository
+	 */
+	requestById(id: string) {
+		return this.#source.read(id);
 	}
 }
