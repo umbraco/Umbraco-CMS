83cb2e2e2f8f2c015e30e24398adadfcff656487
Zeegaan <nge@umbraco.dk>
2023-11-08 09:28:42 +0100

    Start implementing repository

---
+++ src/Umbraco.Infrastructure/HostedServices/WebhookFiring.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Logging;
+
+namespace Umbraco.Cms.Infrastructure.HostedServices;
+
+public class WebhookFiring : RecurringHostedServiceBase
+{
+    public WebhookFiring(ILogger? logger)
+        : base(logger, TimeSpan.FromMinutes(1), DefaultDelay)
+    {
+    }
+
+    public override Task PerformExecuteAsync(object? state)
+    {
+        return Task.CompletedTask;
+    }
+}
---
+++ src/Umbraco.Infrastructure/Persistence/Repositories/Implement/WebhookRequestRepository.cs
@@ -0,0 +1,12 @@
+﻿using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Persistence.Repositories;
+
+namespace Umbraco.Cms.Infrastructure.Persistence.Repositories.Implement;
+
+public class WebhookRequestRepository : IWebhookRequestRepository
+{
+    public Task<WebhookRequest> CreateAsync(WebhookRequest webhookRequest)
+    {
+        return Task.CompletedTask;
+    }
+}
