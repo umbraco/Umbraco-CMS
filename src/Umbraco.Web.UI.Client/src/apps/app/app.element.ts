4a35848f19c396e1abdb1b4f206c6bbf30ad233d
Mads Rasmussen <madsr@hey.com>
2023-11-09 22:10:47 +0100

    clean up

--- src/Umbraco.Web.UI.Client/src/apps/app/app.element.ts
+++ src/Umbraco.Web.UI.Client/src/apps/app/app.element.ts
@@ -1,7 +1,7 @@
 import type { UmbAppErrorElement } from './app-error.element.js';
-import { UMB_APP, UmbAppContext } from './app.context.js';
+import { UmbAppContext } from './app.context.js';
 import { umbLocalizationRegistry } from '@umbraco-cms/backoffice/localization';
-import { UMB_AUTH_CONTEXT, UmbAuthContext } from '@umbraco-cms/backoffice/auth';
+import { UmbAuthContext } from '@umbraco-cms/backoffice/auth';
 import { css, html, customElement, property } from '@umbraco-cms/backoffice/external/lit';
 import { UUIIconRegistryEssential } from '@umbraco-cms/backoffice/external/uui';
 import { UmbIconRegistry } from '@umbraco-cms/backoffice/icon';
