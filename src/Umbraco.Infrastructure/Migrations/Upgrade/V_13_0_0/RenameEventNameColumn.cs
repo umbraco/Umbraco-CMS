7bc48e0da09daacaabbb9a76a73e4b828477e033
Zeegaan <nge@umbraco.dk>
2023-11-09 09:25:05 +0100

    Add renaming column migration

---
+++ src/Umbraco.Infrastructure/Migrations/Upgrade/V_13_0_0/RenameEventNameColumn.cs
@@ -0,0 +1,17 @@
+﻿using Umbraco.Cms.Core;
+
+namespace Umbraco.Cms.Infrastructure.Migrations.Upgrade.V_13_0_0;
+
+public class RenameEventNameColumn : MigrationBase
+{
+    public RenameEventNameColumn(IMigrationContext context) : base(context)
+    {
+    }
+
+    protected override void Migrate() =>
+        Rename
+            .Column("eventName")
+            .OnTable(Constants.DatabaseSchema.Tables.WebhookLog)
+            .To("eventAlias")
+            .Do();
+}
