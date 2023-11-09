f0a032167538ea6fe145c1894a6dc25c4e8efe50
Zeegaan <nge@umbraco.dk>
2023-11-09 13:29:22 +0100

    Update migration to check if old column is there

--- src/Umbraco.Infrastructure/Migrations/Upgrade/V_13_0_0/RenameEventNameColumn.cs
+++ src/Umbraco.Infrastructure/Migrations/Upgrade/V_13_0_0/RenameEventNameColumn.cs
@@ -8,10 +8,21 @@ public class RenameEventNameColumn : MigrationBase
     {
     }
 
-    protected override void Migrate() =>
+    protected override void Migrate()
+    {
+        // This check is here because we renamed a column from 13-rc1 to 13-rc2, the previous migration adds the table
+        // so if you are upgrading from 13-rc1 to 13-rc2 then this column will not exist.
+        // If you are however upgrading from 12, then this column will exist, and thus there is no need to rename it.
+        if (ColumnExists(Constants.DatabaseSchema.Tables.WebhookLog, "eventName") is false)
+        {
+            return;
+        }
+
         Rename
             .Column("eventName")
             .OnTable(Constants.DatabaseSchema.Tables.WebhookLog)
             .To("eventAlias")
             .Do();
+    }
+
 }
