7c877431912178a0938296679f638568a8cbbeef
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2023-11-10 14:14:32 +0100

    Add Auth attribute to controller (#15178)

--- src/Umbraco.Web.BackOffice/Controllers/WebhookController.cs
+++ src/Umbraco.Web.BackOffice/Controllers/WebhookController.cs
@@ -1,3 +1,4 @@
+﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Mapping;
@@ -6,11 +7,13 @@ using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Core.Webhooks;
 using Umbraco.Cms.Web.BackOffice.Services;
 using Umbraco.Cms.Web.Common.Attributes;
+using Umbraco.Cms.Web.Common.Authorization;
 using Umbraco.Cms.Web.Common.Models;
 
 namespace Umbraco.Cms.Web.BackOffice.Controllers;
 
 [PluginController(Constants.Web.Mvc.BackOfficeApiArea)]
+[Authorize(Policy = AuthorizationPolicies.TreeAccessWebhooks)]
 public class WebhookController : UmbracoAuthorizedJsonController
 {
     private readonly IWebhookService _webhookService;
