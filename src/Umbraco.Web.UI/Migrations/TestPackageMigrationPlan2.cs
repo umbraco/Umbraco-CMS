8d826361a8607ad5d3195d8127388ea1081e0721
Andy Butland <abutland73@gmail.com>
2023-11-10 07:49:07 +0100

    Fixed failing unit test.

---
+++ src/Umbraco.Web.UI/Migrations/TestPackageMigrationPlan2.cs
@@ -0,0 +1,10 @@
+//using Umbraco.Cms.Infrastructure.Packaging;
+
+//namespace Umbraco.Cms.Web.UI.Migrations
+//{
+//    public class TestPackageMigrationPlan2 : AutomaticPackageMigrationPlan
+//    {
+//        public TestPackageMigrationPlan2()
+//            : base("TestPackage2") { }
+//    }
+//}
