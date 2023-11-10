e60f08e11f8d5fe04362a57420b7e78710f65bbe
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-11-10 13:32:56 +0100

    do not include credentials (we do not rely on cookies)

--- src/Umbraco.Web.UI.Client/src/apps/app/app.element.ts
+++ src/Umbraco.Web.UI.Client/src/apps/app/app.element.ts
@@ -194,7 +194,6 @@ export class UmbAppElement extends UmbLitElement {
 
 			// Instruct all requests to use the auth flow to get and use the access_token for all subsequent requests
 			OpenAPI.TOKEN = () => this.#authContext!.getLatestToken();
-			OpenAPI.WITH_CREDENTIALS = true;
 		}
 
 		this.#listenForLanguageChange();
