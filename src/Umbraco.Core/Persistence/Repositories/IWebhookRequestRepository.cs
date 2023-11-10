5624f53eec7826c7a9b13cebc6bcf53e40a865d5
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2023-11-10 12:24:33 +0100

    Update docs

--- src/Umbraco.Core/Persistence/Repositories/IWebhookRequestRepository.cs
+++ src/Umbraco.Core/Persistence/Repositories/IWebhookRequestRepository.cs
@@ -5,27 +5,27 @@ namespace Umbraco.Cms.Core.Persistence.Repositories;
 public interface IWebhookRequestRepository
 {
     /// <summary>
-    ///     Creates a webhook request.
+    ///     Creates a webhook request in the current repository.
     /// </summary>
-    /// <param name="webhookRequest">The webhook request you want to create.</param>
-    /// <returns>The created <see cref="WebhookRequest" /> webhook</returns>
+    /// <param name="webhookRequest">The webhook you want to create.</param>
+    /// <returns>The created <see cref="Webhook" /> webhook</returns>
     Task<WebhookRequest> CreateAsync(WebhookRequest webhookRequest);
 
     /// <summary>
-    ///     Deletes a webhook request
+    ///     Deletes a webhook request in the current repository
     /// </summary>
     /// <param name="webhookRequest">The webhook request to be deleted.</param>
     /// <returns><placeholder>A <see cref="Task"/> representing the asynchronous operation.</placeholder></returns>
     Task DeleteAsync(WebhookRequest webhookRequest);
 
     /// <summary>
-    ///     Gets all of the webhook requests in the current database.
+    ///     Gets all of the webhook requests in the current repository.
     /// </summary>
-    /// <returns>An enumerable of <see cref="WebhookRequest" /> objects.</returns>
+    /// <returns>A paged model of <see cref="Webhook" /> objects.</returns>
     Task<IEnumerable<WebhookRequest>> GetAllAsync();
 
     /// <summary>
-    ///     Update a webhook request.
+    ///     Update a webhook request in the current repository.
     /// </summary>
     /// <param name="webhookRequest">The webhook request you want to update.</param>
     /// <returns>The updated <see cref="WebhookRequest" /> webhook</returns>
