995e84e8e2f5366450b3d01c329849b9a8414981
Niels Lyngsø <niels.lyngso@gmail.com>
2023-11-13 11:37:17 +0100

    document impl.

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/workspace/document-workspace.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/workspace/document-workspace.element.ts
@@ -1,4 +1,4 @@
-import { UmbDocumentWorkspaceContext } from './document-workspace.context.js';
+import type { UmbDocumentWorkspaceContext } from './document-workspace.context.js';
 import { UmbTextStyles } from "@umbraco-cms/backoffice/style";
 import { html, customElement, state } from '@umbraco-cms/backoffice/external/lit';
 import type { UmbRoute } from '@umbraco-cms/backoffice/router';
@@ -6,39 +6,63 @@ import { UmbLitElement } from '@umbraco-cms/internal/lit-element';
 
 import './document-workspace-editor.element.js';
 import { UmbWorkspaceIsNewRedirectController } from '@umbraco-cms/backoffice/workspace';
+import { UmbApi, UmbExtensionsApiInitializer, createExtensionApi } from '@umbraco-cms/backoffice/extension-api';
+import { ManifestWorkspace, umbExtensionsRegistry } from '@umbraco-cms/backoffice/extension-registry';
 
 @customElement('umb-document-workspace')
 export class UmbDocumentWorkspaceElement extends UmbLitElement {
-	#workspaceContext = new UmbDocumentWorkspaceContext(this);
+
+
+	#workspaceContext?: UmbDocumentWorkspaceContext;
+
 
 	@state()
-	_routes: UmbRoute[] = [
-		{
-			path: 'create/:parentId/:documentTypeKey',
-			component: import('./document-workspace-editor.element.js'),
-			setup: async (_component, info) => {
-				// TODO: use parent id:
-				// TODO: Notice the perspective of permissions here, we need to check if the user has access to create a document of this type under this parent?
-				const parentId = info.match.params.parentId === 'null' ? null : info.match.params.parentId;
-				const documentTypeKey = info.match.params.documentTypeKey;
-				this.#workspaceContext.create(documentTypeKey, parentId);
+	_routes: UmbRoute[] = [];
 
-				new UmbWorkspaceIsNewRedirectController(
-					this,
-					this.#workspaceContext,
-					this.shadowRoot!.querySelector('umb-router-slot')!
-				);
+	public set manifest(manifest: ManifestWorkspace) {
+
+		console.log("got manifest", manifest)
+
+		createExtensionApi(this.manifest, [this]).then( (context) => {
+			if(context) {
+				this.#gotWorkspaceContext(context);
+			}
+		})
+
+	};
+
+	#gotWorkspaceContext(context: UmbApi) {
+		this.#workspaceContext = context as UmbDocumentWorkspaceContext;
+
+		this._routes = [
+			{
+				path: 'create/:parentId/:documentTypeKey',
+				component: import('./document-workspace-editor.element.js'),
+				setup: async (_component, info) => {
+					// TODO: Remember the perspective of permissions here, we need to check if the user has access to create a document of this type under this parent?
+					const parentId = info.match.params.parentId === 'null' ? null : info.match.params.parentId;
+					const documentTypeKey = info.match.params.documentTypeKey;
+					this.#workspaceContext!.create(documentTypeKey, parentId);
+	
+					new UmbWorkspaceIsNewRedirectController(
+						this,
+						this.#workspaceContext!,
+						this.shadowRoot!.querySelector('umb-router-slot')!
+					);
+				},
 			},
-		},
-		{
-			path: 'edit/:id',
-			component: import('./document-workspace-editor.element.js'),
-			setup: (_component, info) => {
-				const id = info.match.params.id;
-				this.#workspaceContext.load(id);
+			{
+				path: 'edit/:id',
+				component: import('./document-workspace-editor.element.js'),
+				setup: (_component, info) => {
+					const id = info.match.params.id;
+					this.#workspaceContext!.load(id);
+				},
 			},
-		},
-	];
+		];
+
+		new UmbExtensionsApiInitializer(this, umbExtensionsRegistry, 'workspaceContext', [this, this.#workspaceContext]);
+	}
 
 	render() {
 		return html`<umb-router-slot .routes="${this._routes}"></umb-router-slot>`;
