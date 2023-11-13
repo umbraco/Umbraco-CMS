200a0305c76687f587c6d21bb27b5f8867dcc8b7
Mads Rasmussen <madsr@hey.com>
2023-11-13 20:57:45 +0100

    export all repositories

--- src/Umbraco.Web.UI.Client/src/packages/core/data-type/repository/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/data-type/repository/index.ts
@@ -1,1 +1,5 @@
+export { UmbCopyDataTypeRepository } from './copy/data-type-copy.repository.js';
 export { UmbDataTypeDetailRepository } from './detail/data-type-detail.repository.js';
+export { UmbDataTypeFolderRepository } from './folder/data-type-folder.repository.js';
+export { UmbDataTypeItemRepository } from './item/data-type-item.repository.js';
+export { UmbMoveDataTypeRepository } from './move/data-type-move.repository.js';
