3ba51bac0285c8f39a75fd65317c779f08ee9e49
Mads Rasmussen <madsr@hey.com>
2023-11-13 19:25:36 +0100

    split item into its own module

---
+++ src/Umbraco.Web.UI.Client/src/packages/settings/data-types/repository/data-type-repository-base.ts
@@ -0,0 +1,38 @@
+import { UMB_DATA_TYPE_ITEM_STORE_CONTEXT_TOKEN, UmbDataTypeItemStore } from './item/data-type-item.store.js';
+import { UMB_DATA_TYPE_TREE_STORE_CONTEXT_TOKEN, UmbDataTypeTreeStore } from './data-type.tree.store.js';
+import { UMB_DATA_TYPE_STORE_CONTEXT_TOKEN, UmbDataTypeStore } from './data-type.store.js';
+import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
+import { UMB_NOTIFICATION_CONTEXT_TOKEN, UmbNotificationContext } from '@umbraco-cms/backoffice/notification';
+import { UmbRepositoryBase } from '@umbraco-cms/backoffice/repository';
+
+export class UmbDataTypeRepositoryBase extends UmbRepositoryBase {
+	protected _init: Promise<unknown>;
+
+	protected _detailStore?: UmbDataTypeStore;
+	protected _treeStore?: UmbDataTypeTreeStore;
+	protected _itemStore?: UmbDataTypeItemStore;
+
+	protected _notificationContext?: UmbNotificationContext;
+
+	constructor(host: UmbControllerHost) {
+		super(host);
+
+		this._init = Promise.all([
+			this.consumeContext(UMB_DATA_TYPE_STORE_CONTEXT_TOKEN, (instance) => {
+				this._detailStore = instance;
+			}).asPromise(),
+
+			this.consumeContext(UMB_DATA_TYPE_TREE_STORE_CONTEXT_TOKEN, (instance) => {
+				this._treeStore = instance;
+			}).asPromise(),
+
+			this.consumeContext(UMB_DATA_TYPE_ITEM_STORE_CONTEXT_TOKEN, (instance) => {
+				this._itemStore = instance;
+			}).asPromise(),
+
+			this.consumeContext(UMB_NOTIFICATION_CONTEXT_TOKEN, (instance) => {
+				this._notificationContext = instance;
+			}).asPromise(),
+		]);
+	}
+}
--- src/Umbraco.Web.UI.Client/src/packages/settings/data-types/repository/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/settings/data-types/repository/manifests.ts
@@ -1,13 +1,8 @@
 import { UmbDataTypeRepository } from './data-type.repository.js';
-import { UmbDataTypeItemStore } from './data-type-item.store.js';
 import { UmbDataTypeStore } from './data-type.store.js';
 import { UmbDataTypeTreeStore } from './data-type.tree.store.js';
-import type {
-	ManifestStore,
-	ManifestTreeStore,
-	ManifestRepository,
-	ManifestItemStore,
-} from '@umbraco-cms/backoffice/extension-registry';
+import { manifests as itemManifests } from './item/manifests.js';
+import type { ManifestStore, ManifestTreeStore, ManifestRepository } from '@umbraco-cms/backoffice/extension-registry';
 
 export const DATA_TYPE_REPOSITORY_ALIAS = 'Umb.Repository.DataType';
 
@@ -20,7 +15,6 @@ const repository: ManifestRepository = {
 
 export const DATA_TYPE_STORE_ALIAS = 'Umb.Store.DataType';
 export const DATA_TYPE_TREE_STORE_ALIAS = 'Umb.Store.DataTypeTree';
-export const DATA_TYPE_ITEM_STORE_ALIAS = 'Umb.Store.DataTypeItem';
 
 const store: ManifestStore = {
 	type: 'store',
@@ -36,11 +30,4 @@ const treeStore: ManifestTreeStore = {
 	api: UmbDataTypeTreeStore,
 };
 
-const itemStore: ManifestItemStore = {
-	type: 'itemStore',
-	alias: DATA_TYPE_ITEM_STORE_ALIAS,
-	name: 'Data Type Item Store',
-	api: UmbDataTypeItemStore,
-};
-
-export const manifests = [repository, store, treeStore, itemStore];
+export const manifests = [repository, store, treeStore, ...itemManifests];
