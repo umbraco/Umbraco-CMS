f685c495cb02c8a65763c2490e6b74940ef2d8f2
Mads Rasmussen <madsr@hey.com>
2023-11-14 21:51:38 +0100

    expand array

--- src/Umbraco.Web.UI.Client/src/packages/documents/document-types/repository/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/document-types/repository/manifests.ts
@@ -21,4 +21,4 @@ const store: ManifestStore = {
 	api: UmbDocumentTypeStore,
 };
 
-export const manifests = [repository, store, itemManifests];
+export const manifests = [repository, store, ...itemManifests];
