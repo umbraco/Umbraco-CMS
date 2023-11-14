8080b68a9a6e17a8c9a5d0388224ce1b907d5e06
Elitsa <elm@umbraco.dk>
2023-11-14 15:20:38 +0100

    Add TODO comment

--- src/Umbraco.Cms.Api.Management/Security/Authorization/Content/ContentPermissionRequirement.cs
+++ src/Umbraco.Cms.Api.Management/Security/Authorization/Content/ContentPermissionRequirement.cs
@@ -7,6 +7,7 @@ namespace Umbraco.Cms.Api.Management.Security.Authorization.Content;
 ///     Authorization requirement for the <see cref="ContentPermissionHandler" />
 ///     and <see cref="ContentBranchPermissionHandler" />.
 /// </summary>
+// TODO: ContentBranchPermissionHandler might need its own requirement and policy, once we have publishing functionality in place.
 public class ContentPermissionRequirement : IAuthorizationRequirement
 {
 }
