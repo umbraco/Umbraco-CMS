5dfa76600a9bd673888ff7f2a6ac5c87297cb981
Bjarke Berg <mail@bergmania.dk>
2023-11-14 09:14:01 +0100

    Post fix

--- src/Umbraco.Infrastructure/Persistence/Repositories/Implement/CreatedPackageSchemaRepository.cs
+++ src/Umbraco.Infrastructure/Persistence/Repositories/Implement/CreatedPackageSchemaRepository.cs
@@ -264,14 +264,16 @@ public class CreatedPackageSchemaRepository : ICreatedPackagesRepository
                 _hostingEnvironment.MapPathContentRoot(Path.Combine(
                     _createdPackagesFolderPath,
                     definition.Name.Replace(' ', '_')));
-            Directory.CreateDirectory(directoryName);
 
-            var expectedRoot = _hostingEnvironment.MapPathContentRoot(_createdPackagesFolderPath);
-            var finalPackagePath = Path.Combine(directoryName, fileName);
+            var expectedRoot = Path.GetFullPath(_hostingEnvironment.MapPathContentRoot(_createdPackagesFolderPath));
+            var finalPackagePath = Path.GetFullPath(Path.Combine(directoryName, fileName));
             if (finalPackagePath.StartsWith(expectedRoot) == false)
             {
                 throw new IOException("Invalid path due to the package name");
             }
+
+            Directory.CreateDirectory(directoryName);
+
 
             // Clean existing files
             foreach (var packagePath in new[] { definition.PackagePath, finalPackagePath })
