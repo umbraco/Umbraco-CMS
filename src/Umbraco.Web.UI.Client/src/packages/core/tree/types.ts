3fb3ad1cf98917866b3e3aabdcf526b29193aea8
Mads Rasmussen <madsr@hey.com>
2023-11-14 10:15:41 +0100

    temp update of models to match server until we have our own types

--- src/Umbraco.Web.UI.Client/src/packages/core/tree/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/tree/types.ts
@@ -1,14 +1,15 @@
 export interface UmbTreeItemModel {
-	type: string;
-	name: string;
-	hasChildren: boolean;
-	icon?: string;
+	type?: string; // TODO: remove option to be undefined when we have our own types
+	name?: string;
+	hasChildren?: boolean;
+	icon?: string | null;
+	parentId?: string | null;
 }
 
 export interface UmbEntityTreeItemModel extends UmbTreeItemModel {
-	id: string;
+	id?: string; // TODO: remove option to be undefined when server returns the same
 }
 
 export interface UmbFileSystemTreeItemModel {
-	path: string;
+	path?: string; // TODO: remove option to be undefined when server returns the same
 }
