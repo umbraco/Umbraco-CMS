ad68fde98dab032f1777ef67cce1043575e1df65
Andy Butland <abutland73@gmail.com>
2023-11-15 18:32:54 +0100

    Updated rich text parser interfaces to support context cache.

--- src/Umbraco.Core/Deploy/IImageSourceParser.cs
+++ src/Umbraco.Core/Deploy/IImageSourceParser.cs
@@ -12,7 +12,20 @@ public interface IImageSourceParser
     /// <param name="dependencies">A list of dependencies.</param>
     /// <returns>The parsed value.</returns>
     /// <remarks>Turns src="/media/..." into src="umb://media/..." and adds the corresponding udi to the dependencies.</remarks>
+    [Obsolete("Please use the overload taking all parameters. This method will be removed in Umbraco 14.")]
     string ToArtifact(string value, ICollection<Udi> dependencies);
+
+    /// <summary>
+    ///     Parses an Umbraco property value and produces an artifact property value.
+    /// </summary>
+    /// <param name="value">The property value.</param>
+    /// <param name="dependencies">A list of dependencies.</param>
+    /// <param name="contextCache">The context cache.</param>
+    /// <returns>The parsed value.</returns>
+    /// <remarks>Turns src="/media/..." into src="umb://media/..." and adds the corresponding udi to the dependencies.</remarks>
+#pragma warning disable CS0618 // Type or member is obsolete
+    string ToArtifact(string value, ICollection<Udi> dependencies, IContextCache contextCache) => ToArtifact(value, dependencies);
+#pragma warning restore CS0618 // Type or member is obsolete
 
     /// <summary>
     ///     Parses an artifact property value and produces an Umbraco property value.
@@ -20,5 +33,17 @@ public interface IImageSourceParser
     /// <param name="value">The artifact property value.</param>
     /// <returns>The parsed value.</returns>
     /// <remarks>Turns umb://media/... into /media/....</remarks>
+    [Obsolete("Please use the overload taking all parameters. This method will be removed in Umbraco 14.")]
     string FromArtifact(string value);
+
+    /// <summary>
+    ///     Parses an artifact property value and produces an Umbraco property value.
+    /// </summary>
+    /// <param name="value">The artifact property value.</param>
+    /// <param name="contextCache">The context cache.</param>
+    /// <returns>The parsed value.</returns>
+    /// <remarks>Turns umb://media/... into /media/....</remarks>
+#pragma warning disable CS0618 // Type or member is obsolete
+    string FromArtifact(string value, IContextCache contextCache) => FromArtifact(value);
+#pragma warning restore CS0618 // Type or member is obsolete
 }
