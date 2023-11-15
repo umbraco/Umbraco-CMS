69fa9cf73930f7df694a5236937ee068236df050
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-11-15 14:40:49 +0100

    reformat code

--- src/Umbraco.Web.UI.Login/src/components/layouts/external-login-providers-layout.element.ts
+++ src/Umbraco.Web.UI.Login/src/components/layouts/external-login-providers-layout.element.ts
@@ -3,86 +3,88 @@ import { customElement, property, queryAssignedElements } from 'lit/decorators.j
 
 @customElement('umb-external-login-providers-layout')
 export class UmbExternalLoginProvidersLayoutElement extends LitElement {
-	@property({ type: Boolean, attribute: 'divider' })
-	showDivider = true;
+  @property({ type: Boolean, attribute: 'divider' })
+  showDivider = true;
 
-	@queryAssignedElements({ flatten: true })
-	protected slottedElements?: HTMLElement[];
+  @queryAssignedElements({ flatten: true })
+  protected slottedElements?: HTMLElement[];
 
-	firstUpdated() {
-		!!this.slottedElements?.length ? this.toggleAttribute('empty', false) : this.toggleAttribute('empty', true);
-	}
+  firstUpdated() {
+    !!this.slottedElements?.length ? this.toggleAttribute('empty', false) : this.toggleAttribute('empty', true);
+  }
 
-	render() {
-		return html`
-			${this.showDivider
-				? html`
-						<div id="divider" aria-hidden="true">
-							<span><umb-localize key="general_or">Or</umb-localize></span>
-						</div>
-				  `
-				: nothing}
-			<div>
-				<slot></slot>
-			</div>
-		`;
-	}
+  render() {
+    return html`
+      ${this.showDivider
+        ? html`
+          <div id="divider" aria-hidden="true">
+            <span><umb-localize key="general_or">Or</umb-localize></span>
+          </div>
+        `
+        : nothing}
+      <div>
+        <slot></slot>
+      </div>
+    `;
+  }
 
-	static styles: CSSResultGroup = [
-		css`
-			:host {
-				margin-top: 16px;
-				display: flex;
-				flex-direction: column;
-			}
+  static styles: CSSResultGroup = [
+    css`
+      :host {
+        margin-top: 16px;
+        display: flex;
+        flex-direction: column;
+      }
 
-			:host([empty]) {
-				display: none;
-			}
+      :host([empty]) {
+        display: none;
+      }
 
-			slot {
-				display: flex;
-				flex-direction: column;
-				gap: var(--uui-size-space-4);
-			}
+      slot {
+        display: flex;
+        flex-direction: column;
+        gap: var(--uui-size-space-4);
+      }
 
-			#divider {
-				width: calc(100% - 18px);
-				margin: 0 auto;
-				margin-bottom: 16px;
-				text-align: center;
-				z-index: 0;
-				overflow: hidden;
-			}
+      #divider {
+        width: calc(100% - 18px);
+        margin: 0 auto;
+        margin-bottom: 16px;
+        text-align: center;
+        z-index: 0;
+        overflow: hidden;
+      }
 
-			#divider span {
-				padding-inline: 10px;
-				position: relative;
-				color: var(--uui-color-border-emphasis);
-			}
+      #divider span {
+        padding-inline: 10px;
+        position: relative;
+        color: var(--uui-color-border-emphasis);
+      }
 
-			#divider span::before,
-			#divider span::after {
-				content: '';
-				display: block;
-				width: 500px; /* Arbitrary value, just be bigger than 50% of the max width of the container */
-				height: 1px;
-				background-color: var(--uui-color-border);
-				position: absolute;
-				top: calc(50% + 1px);
-			}
-			#divider span::before {
-				right: 100%;
-			}
-			#divider span::after {
-				left: 100%;
-			}
-		`,
-	];
+      #divider span::before,
+      #divider span::after {
+        content: '';
+        display: block;
+        width: 500px; /* Arbitrary value, just be bigger than 50% of the max width of the container */
+        height: 1px;
+        background-color: var(--uui-color-border);
+        position: absolute;
+        top: calc(50% + 1px);
+      }
+
+      #divider span::before {
+        right: 100%;
+      }
+
+      #divider span::after {
+        left: 100%;
+      }
+    `,
+  ];
 }
 
 declare global {
-	interface HTMLElementTagNameMap {
-		'umb-external-login-providers-layout': UmbExternalLoginProvidersLayoutElement;
-	}
+  interface HTMLElementTagNameMap {
+    'umb-external-login-providers-layout': UmbExternalLoginProvidersLayoutElement;
+  }
 }
