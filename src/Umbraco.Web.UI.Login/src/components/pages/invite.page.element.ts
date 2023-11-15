c9a5c3719d992b8b295357543f84f9aa9b8b3268
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2023-11-15 14:38:32 +0100

    reformat code

--- src/Umbraco.Web.UI.Login/src/components/pages/invite.page.element.ts
+++ src/Umbraco.Web.UI.Login/src/components/pages/invite.page.element.ts
@@ -8,71 +8,74 @@ import { umbLocalizationContext } from '../../external/localization/localization
 
 @customElement('umb-invite-page')
 export default class UmbInvitePageElement extends LitElement {
-	@state()
-	state: UUIButtonState = undefined;
+  @state()
+  state: UUIButtonState = undefined;
 
-	@state()
-	error = '';
+  @state()
+  error = '';
 
-	@state()
-	invitedUser?: any;
+  @state()
+  invitedUser?: any;
 
-	protected async firstUpdated(_changedProperties: any) {
-		super.firstUpdated(_changedProperties);
+  protected async firstUpdated(_changedProperties: any) {
+    super.firstUpdated(_changedProperties);
 
-		const response = await umbAuthContext.getInvitedUser();
+    const response = await umbAuthContext.getInvitedUser();
 
-		if (!response.user?.id) {
-			// The login page should already have redirected the user to an error page. They should never get here.
-			this.error = 'No invited user found';
-			return;
-		}
+    if (!response.user?.id) {
+      // The login page should already have redirected the user to an error page. They should never get here.
+      this.error = 'No invited user found';
+      return;
+    }
 
-		this.invitedUser = response.user;
-	}
+    this.invitedUser = response.user;
+  }
 
-	async #onSubmit(event: CustomEvent) {
-		event.preventDefault();
-		const password = event.detail.password;
+  async #onSubmit(event: CustomEvent) {
+    event.preventDefault();
+    const password = event.detail.password;
 
-		if (!password) return;
+    if (!password) return;
 
-		this.state = 'waiting';
-		const response = await umbAuthContext.newInvitedUserPassword(password);
+    this.state = 'waiting';
+    const response = await umbAuthContext.newInvitedUserPassword(password);
 
-		if (response.error) {
-			this.error = response.error;
-			this.state = 'failed';
-			return;
-		}
+    if (response.error) {
+      this.error = response.error;
+      this.state = 'failed';
+      return;
+    }
 
-		this.state = 'success';
-		window.location.href = umbAuthContext.returnPath;
-	}
+    this.state = 'success';
+    window.location.href = umbAuthContext.returnPath;
+  }
 
-	render() {
-		return this.invitedUser
-			? html`<umb-new-password-layout
-					@submit=${this.#onSubmit}
-					.userId=${this.invitedUser.id}
-					.userName=${this.invitedUser.name}
-					.state=${this.state}
-					.error=${this.error}></umb-new-password-layout>`
-			: this.error
-			? html`<umb-error-layout
-					.header=${until(umbLocalizationContext.localize('general_error', undefined, 'Error'))}
-					.message=${this.error}></umb-error-layout>`
-			: html`<umb-error-layout
-					header=${until(umbLocalizationContext.localize('general_error', undefined, 'Error'))}
-					message=${until(
-						umbLocalizationContext.localize('errors_defaultError', undefined, 'An unknown failure has occured')
-					)}>
-			  </umb-error-layout>`;
-	}
+  render() {
+    return this.invitedUser
+      ? html`
+        <umb-new-password-layout
+          @submit=${this.#onSubmit}
+          .userId=${this.invitedUser.id}
+          .userName=${this.invitedUser.name}
+          .state=${this.state}
+          .error=${this.error}></umb-new-password-layout>`
+      : this.error
+        ? html`
+          <umb-error-layout
+            .header=${until(umbLocalizationContext.localize('general_error', undefined, 'Error'))}
+            .message=${this.error}></umb-error-layout>`
+        : html`
+          <umb-error-layout
+            header=${until(umbLocalizationContext.localize('general_error', undefined, 'Error'))}
+            message=${until(
+              umbLocalizationContext.localize('errors_defaultError', undefined, 'An unknown failure has occured')
+            )}>
+          </umb-error-layout>`;
+  }
 }
 
 declare global {
-	interface HTMLElementTagNameMap {
-		'umb-invite-page': UmbInvitePageElement;
-	}
+  interface HTMLElementTagNameMap {
+    'umb-invite-page': UmbInvitePageElement;
+  }
 }
