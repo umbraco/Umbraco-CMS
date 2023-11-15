a62baf4242f3f08d03f26095f56cf893017413c0
Lone Iversen <108085781+loivsen@users.noreply.github.com>
2023-11-15 13:46:15 +0100

    fix node issue, show tree, remove old code

--- src/Umbraco.Web.UI.Client/src/packages/dictionary/dictionary/entity-actions/import/import-dictionary-modal.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/dictionary/dictionary/entity-actions/import/import-dictionary-modal.element.ts
@@ -1,6 +1,6 @@
 import { UmbDictionaryRepository } from '../../repository/dictionary.repository.js';
 import { UUIInputFileElement } from '@umbraco-cms/backoffice/external/uui';
-import { css, html, customElement, query, state, when, repeat } from '@umbraco-cms/backoffice/external/lit';
+import { css, html, customElement, query, state, when } from '@umbraco-cms/backoffice/external/lit';
 import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
 import {
 	UmbImportDictionaryModalData,
@@ -90,7 +90,11 @@ export class UmbImportDictionaryModalLayout extends UmbModalBaseElement<
 	#makeDictionaryItems(nodeList: NodeListOf<ChildNode>): Array<DictionaryItem> {
 		const items: Array<DictionaryItem> = [];
 		const list: Array<Element> = [];
-		nodeList.forEach((node) => list.push(node as Element));
+		nodeList.forEach((node) => {
+			if (node.nodeType === Node.ELEMENT_NODE) {
+				list.push(node as Element);
+			}
+		});
 
 		list.forEach((item) => {
 			items.push({
@@ -140,7 +144,7 @@ export class UmbImportDictionaryModalLayout extends UmbModalBaseElement<
 			<div>
 				<strong><umb-localize key="actions_chooseWhereToImport">Choose where to import</umb-localize>:</strong>
 
-				Render list here
+				<umb-tree alias="Umb.Tree.Dictionary"></umb-tree>
 			</div>
 
 			${this.#renderNavigate()}
@@ -178,155 +182,6 @@ export class UmbImportDictionaryModalLayout extends UmbModalBaseElement<
 				</form>
 			</uui-form>`;
 	}
-
-	/*
-	@query('#form')
-	private _form!: HTMLFormElement;
-
-	@state()
-	private _uploadedDictionaryTempId?: string;
-
-	@state()
-	private _showUploadView = true;
-
-	@state()
-	private _showImportView = false;
-
-	@state()
-	private _showErrorView = false;
-
-	@state()
-	private _selection: Array<string> = [];
-
-	#detailRepo = new UmbDictionaryRepository(this);
-
-	async #importDictionary() {
-		if (!this._uploadedDictionaryTempId) return;
-
-		this.modalContext?.submit({
-			temporaryFileId: this._uploadedDictionaryTempId,
-			parentId: this._selection[0],
-		});
-	}
-
-	#handleClose() {
-		this.modalContext?.reject();
-	}
-
-	#submitForm() {
-		this._form?.requestSubmit();
-	}
-
-	async #handleSubmit(e: SubmitEvent) {
-		e.preventDefault();
-
-		if (!this._form.checkValidity()) return;
-
-		const formData = new FormData(this._form);
-
-		const uploadData: ImportDictionaryRequestModel = {
-			temporaryFileId: formData.get('file')?.toString() ?? '',
-		};
-
-		// TODO: fix this upload experience. We need to update our form so it gets temporary file id from the server:
-		const { data } = await this.#detailRepo.upload(uploadData);
-
-		if (!data) return;
-
-		this._uploadedDictionaryTempId = data;
-		// TODO: We need to find another way to gather the data of the uploaded dictionary, to represent the dictionaryItems? See further below.
-		//this._uploadedDictionary = data;
-
-		if (!this._uploadedDictionaryTempId) {
-			this._showErrorView = true;
-			this._showImportView = false;
-			return;
-		}
-
-		this._showErrorView = false;
-		this._showUploadView = false;
-		this._showImportView = true;
-	}
-
-	/*
-	#handleSelectionChange(e: CustomEvent) {
-		e.stopPropagation();
-		const element = e.target as UmbTreeElement;
-		this._selection = element.selection;
-	}
-	*/
-	/*
-	#renderUploadView() {
-		return html`<p>
-				To import a dictionary item, find the ".udt" file on your computer by clicking the "Import" button (you'll be
-				asked for confirmation on the next screen)
-			</p>
-			<uui-form>
-				<form id="form" name="form" @submit=${this.#handleSubmit}>
-					<uui-form-layout-item>
-						<uui-label for="file" slot="label" required>File</uui-label>
-						<div>
-							<uui-input-file
-								accept=".udt"
-								name="file"
-								id="file"
-								required
-								required-message="File is required"></uui-input-file>
-						</div>
-					</uui-form-layout-item>
-				</form>
-			</uui-form>
-			<uui-button slot="actions" type="button" label="Cancel" @click=${this.#handleClose}></uui-button>
-			<uui-button slot="actions" type="button" label="Import" look="primary" @click=${this.#submitForm}></uui-button>`;
-	}
-	*/
-
-	/// TODO => Tree view needs isolation and single-select option
-	/*#renderImportView() {
-		//TODO: gather this data in some other way, we cannot use the feedback from the server anymore. can we use info about the file directly? or is a change to the end point required?
-		/*
-		if (!this._uploadedDictionary?.dictionaryItems) return;
-
-		return html`
-			<b>Dictionary items</b>
-			<ul>
-				${repeat(
-					this._uploadedDictionary.dictionaryItems,
-					(item) => item.name,
-					(item) => html`<li>${item.name}</li>`
-				)}
-			</ul>
-			<hr />
-			<b>Choose where to import dictionary items (optional)</b>
-			<umb-tree
-				alias="Umb.Tree.Dictionary"
-				@selection-change=${this.#handleSelectionChange}
-				.selection=${this._selection}
-				selectable></umb-tree>
-
-			<uui-button slot="actions" type="button" label="Cancel" @click=${this.#handleClose}></uui-button>
-			<uui-button
-				slot="actions"
-				type="button"
-				label="Import"
-				look="primary"
-				@click=${this.#importDictionary}></uui-button>
-		`;
-	}
-	*/
-
-	// TODO => Determine what to display when dictionary import/upload fails
-	/*#renderErrorView() {
-		return html`Something went wrong`;
-	}*/
-	/*
-	render() {
-		return html` <umb-body-layout headline="Import">
-			${when(this._showUploadView, () => this.#renderUploadView())}
-			${when(this._showImportView, () => this.#renderImportView())}
-			${when(this._showErrorView, () => this.#renderErrorView())}
-		</umb-body-layout>`;
-	}*/
 
 	static styles = [
 		UmbTextStyles,
