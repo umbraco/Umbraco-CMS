ea33b73daa1e6384d10d11941acdd7cd7ecfb00b
Bjarke Berg <mail@bergmania.dk>
2023-11-15 09:03:08 +0100

    Added scope.complete in edge cases. (#15207)
    (cherry picked from commit 28e8ac11f0c37f7f5b8fbd729c33642e2029928e)

--- src/Umbraco.Core/Services/ContentVersionService.cs
+++ src/Umbraco.Core/Services/ContentVersionService.cs
@@ -77,6 +77,7 @@ internal class ContentVersionService : IContentVersionService
 
             if (version is null)
             {
+                scope.Complete();
                 return;
             }
 
@@ -128,6 +129,7 @@ internal class ContentVersionService : IContentVersionService
 
             if (allHistoricVersions is null)
             {
+                scope.Complete();
                 return Array.Empty<ContentVersionMeta>();
             }
 
