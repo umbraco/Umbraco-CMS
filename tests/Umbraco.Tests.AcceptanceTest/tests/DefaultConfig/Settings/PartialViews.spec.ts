23e27dc4acf02202e79102a92e56b4b85ba7a580
Nhu Dinh <hnd@umbraco.dk>
2023-11-16 13:27:20 +0700

    fixed comments

--- tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Settings/PartialViews.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Settings/PartialViews.spec.ts
@@ -2,6 +2,8 @@
 import {expect} from "@playwright/test";
 
 test.describe('Partial Views tests', () => {
+  const partialViewName = 'TestPartialView';
+  const partialViewFileName = partialViewName + ".cshtml";
   
   test.beforeEach(async ({page, umbracoApi, umbracoUi}) => {
     await page.goto(umbracoApi.baseUrl + '/umbraco');
@@ -11,24 +13,21 @@ test.describe('Partial Views tests', () => {
   async function gotoPartialViewDetail(page, partialViewFileName) {
     await page.locator('umb-tree-item').filter({ hasText: 'Partial Views' }).locator('#caret-button').click();
     await page.locator('umb-tree-item').getByLabel(partialViewFileName).click();
-  }
-  
+  } 
 
   test('can create an empty partial view', async ({page, umbracoApi}) => {
-    const partialViewName = 'TestPartialView';
-    const partialViewFileName = partialViewName + ".cshtml";
-    // Act
+    //Arrange
     await umbracoApi.partialView.ensureNameNotExistsAtRoot(partialViewName);
 
+    // Act
     await page.locator('umb-tree-item', {hasText: 'Partial Views'}).getByLabel('Open actions menu').click({force: true});
     await page.getByLabel('New empty partial view').click();
     await page.getByLabel('template name').fill(partialViewName);
-    // TODO: Remove this timeout when frontend validation is implemented
-    await page.waitForTimeout(1000);
+    await page.waitForLoadState('networkidle');
     await page.getByLabel('Save').click();
 
     // Assert
-    expect(await umbracoApi.partialView.nameExistsAtRoot(partialViewFileName)).toBeTruthy;
+    expect(await umbracoApi.partialView.nameExistsAtRoot(partialViewFileName)).toBeTruthy();
     // TODO: when frontend is ready, uncomment this to verify the new partial view is displayed under the Partial Views section 
     //await expect(page.locator('umb-tree-item', {hasText: 'Partial Views'}).getByLabel(partialViewFileName)).toBeVisible();
     // TODO: when frontend is ready, verify the notification displays
@@ -38,22 +37,23 @@ test.describe('Partial Views tests', () => {
   });
 
   test('can create a partial view from snippet', async ({page, umbracoApi}) => {
-    const partialViewName = 'TestPartialViewFromSnippet';
-    const partialViewFileName = partialViewName + ".cshtml";
-    // Act
+    // Arrange
     await umbracoApi.partialView.ensureNameNotExistsAtRoot(partialViewFileName);
+    const expectedTemplateContent = '@inherits Umbraco.Cms.Web.Common.Views.UmbracoViewPage\r\n@using Umbraco.Cms.Core.Routing\r\n@using Umbraco.Extensions\r\n\n@inject IPublishedUrlProvider PublishedUrlProvider\r\n@*\r\n    This snippet makes a breadcrumb of parents using an unordered HTML list.\r\n\r\n    How it works:\r\n    - It uses the Ancestors() method to get all parents and then generates links so the visitor can go back\r\n    - Finally it outputs the name of the current page (without a link)\r\n*@\r\n\r\n@{ var selection = Model.Ancestors().ToArray(); }\r\n\r\n@if (selection?.Length > 0)\r\n{\r\n    <ul class=\"breadcrumb\">\r\n        @* For each page in the ancestors collection which have been ordered by Level (so we start with the highest top node first) *@\r\n        @foreach (var item in selection.OrderBy(x => x.Level))\r\n        {\r\n            <li><a href=\"@item.Url(PublishedUrlProvider)\">@item.Name</a> <span class=\"divider\">/</span></li>\r\n        }\r\n\r\n        @* Display the current page as the last item in the list *@\r\n        <li class=\"active\">@Model.Name</li>\r\n    </ul>\r\n}';
 
+    // Act
     await page.locator('umb-tree-item', {hasText: 'Partial Views'}).getByLabel('Open actions menu').click({force: true});
     await page.getByLabel('New partial view from snippet...').click();
     await page.getByLabel('Breadcrumb').click();
     await page.getByLabel('template name').fill(partialViewName);
-    // TODO: Remove this timeout when frontend validation is implemented
-    await page.waitForTimeout(1000);
+    await page.waitForLoadState('networkidle');
     await page.getByLabel('Save').click();
-    
 
     // Assert
-    expect(await umbracoApi.partialView.nameExistsAtRoot(partialViewFileName)).toBeTruthy;
+    expect(await umbracoApi.partialView.nameExistsAtRoot(partialViewFileName)).toBeTruthy();
+    const partialViewData = await umbracoApi.partialView.getByNameAtRoot(partialViewFileName);
+    await expect(partialViewData.content).toBe(expectedTemplateContent);
+
     // TODO: when frontend is ready, uncomment this to verify the new partial view is displayed under the Partial Views section
     //await expect(page.locator('umb-tree-item', {hasText: 'Partial Views'}).getByLabel(partialViewFileName)).toBeVisible();
     // TODO: when frontend is ready, verify the notification displays
@@ -64,24 +64,24 @@ test.describe('Partial Views tests', () => {
 
   
 test('can update a partial view name', async ({page, umbracoApi}) => {
-    const partialViewName = 'TestPartialView';
-    const partialViewFileName = partialViewName + ".cshtml";
+    // Arrange
     const updatedPartialViewName = 'TestPartialViewUpdated';
     const updatedPartialViewFileName = updatedPartialViewName + ".cshtml";
-    // Arrange
+
     await umbracoApi.partialView.ensureNameNotExistsAtRoot(partialViewFileName);
     await umbracoApi.partialView.create(partialViewFileName, "@inherits Umbraco.Cms.Web.Common.Views.UmbracoViewPage\r\n", "/");
 
     //Act
-    gotoPartialViewDetail(page, partialViewFileName);
+    await gotoPartialViewDetail(page, partialViewFileName);
     await page.getByLabel('template name').fill(updatedPartialViewName);
+    await page.waitForLoadState('networkidle');
     await page.getByLabel('Save').click();
 
     // Assert
-    expect(await umbracoApi.partialView.nameExistsAtRoot(updatedPartialViewFileName)).toBeTruthy;
+    expect(await umbracoApi.partialView.nameExistsAtRoot(updatedPartialViewFileName)).toBeTruthy();
     // TODO: when frontend is ready, uncomment this to verify the updated partial view name is displayed under the Partial Views section
     //await expect(page.locator('umb-tree-item', {hasText: 'Partial Views'}).getByLabel(updatedPartialViewFileName)).toBeVisible();
-    expect(await umbracoApi.partialView.nameExistsAtRoot(partialViewFileName)).toBeFalsy;
+    expect(await umbracoApi.partialView.nameExistsAtRoot(partialViewFileName)).toBeFalsy();
     // TODO: when frontend is ready, uncomment this to verify the old partial view name is NOT displayed under the Partial Views section
     //await expect(page.locator('umb-tree-item', {hasText: 'Partial Views'}).getByLabel(partialViewFileName)).toHaveCount(0);
     
@@ -90,15 +90,13 @@ test('can update a partial view name', async ({page, umbracoApi}) => {
   });
 
   test('can update a partial view content', async ({page, umbracoApi}) => {
-    const partialViewName = 'TestPartialView';
-    const partialViewFileName = partialViewName + ".cshtml";
+    // Arrange
     const updatedPartialViewContent = '@inherits Umbraco.Cms.Web.Common.Views.UmbracoViewPage\r\n' +
       '@{\r\n' +
       '\tLayout = null;\r\n' +
       '}\r\n' +
       '<p>AcceptanceTests</p>';
 
-    // Arrange
     await umbracoApi.partialView.ensureNameNotExistsAtRoot(partialViewFileName);
     await umbracoApi.partialView.create(partialViewFileName, "@inherits Umbraco.Cms.Web.Common.Views.UmbracoViewPage\r\n", "/");
 
@@ -106,19 +104,19 @@ test('can update a partial view name', async ({page, umbracoApi}) => {
     gotoPartialViewDetail(page, partialViewFileName);
     await page.locator('textarea.inputarea').clear();
     await page.locator('textarea.inputarea').fill(updatedPartialViewContent);
+    await page.waitForLoadState('networkidle');
     await page.getByLabel('Save').click();
 
     // Assert
     const updatedPartialView = await umbracoApi.partialView.getByNameAtRoot(partialViewFileName);
-    expect(updatedPartialView.content).toBe(updatedPartialViewContent);
+    await expect(updatedPartialView.content).toBe(updatedPartialViewContent);
     
     // Clean
     await umbracoApi.partialView.ensureNameNotExistsAtRoot(partialViewFileName);
   });
 
   test('can use query builder for a partial view', async ({page, umbracoApi, umbracoUi}) => {
-    const partialViewName = 'TestPartialView';
-    const partialViewFileName = partialViewName + ".cshtml";
+    //Arrange
     const expectedTemplateContent = '\r\n' +
     '@{\r\n' +
     '\tvar selection = Umbraco.ContentAtRoot().FirstOrDefault()\r\n' +
@@ -136,18 +134,17 @@ test('can update a partial view name', async ({page, umbracoApi}) => {
     '\r\n' +
     '@inherits Umbraco.Cms.Web.Common.Views.UmbracoViewPage\r\n';
 
-
-    // Arrange
     await umbracoApi.partialView.ensureNameNotExistsAtRoot(partialViewFileName);
     await umbracoApi.partialView.create(partialViewFileName, "@inherits Umbraco.Cms.Web.Common.Views.UmbracoViewPage\r\n", "/");
 
     // Act
     gotoPartialViewDetail(page, partialViewFileName);
-    await page.locator('#query-builder-button').getByLabel('Query builder').click();
-    await page.waitForTimeout(1000);
-    await page.locator('#property-alias-dropdown').getByLabel('Property alias').click({force: true});
-    await page.waitForTimeout(1000);
-    await page.locator('#property-alias-dropdown').getByText('CreateDate').click({force:true});
+    await page.locator('#query-builder-button').getByLabel('Query builder').click({force:true});
+    await page.waitForLoadState('networkidle');
+    await expect(page.locator('uui-modal-container[backdrop]')).toBeTruthy();
+    await page.locator('#property-alias-dropdown').getByLabel('Property alias').click({force:true});
+    await expect(page.locator('uui-popover[open]')).toBeTruthy();
+    await page.locator('#property-alias-dropdown').getByText('CreateDate').click();
     await page.getByLabel('Submit').click();
     await page.getByLabel('Save').click();
 
@@ -161,10 +158,9 @@ test('can update a partial view name', async ({page, umbracoApi}) => {
 
   // Skip this test now as the function delete dictionary is missing
   test.skip('can insert dictionaryItem into a partial view', async ({page, umbracoApi}) => {
-    const partialViewName = 'TestPartialView';
-    const partialViewFileName = partialViewName + ".cshtml";
-    const dictionaryName = 'TestDictionary';
     // Arrange
+    const dictionaryName = 'TestDictionary';
+    
     await umbracoApi.partialView.ensureNameNotExistsAtRoot(partialViewFileName);
     await umbracoApi.partialView.create(partialViewFileName, "@inherits Umbraco.Cms.Web.Common.Views.UmbracoViewPage\r\n", "/");
 
@@ -177,8 +173,7 @@ test('can update a partial view name', async ({page, umbracoApi}) => {
     gotoPartialViewDetail(page, partialViewFileName);
     await page.getByLabel('Choose value to insert').click();
     await page.getByLabel('Insert Dictionary item').click({force: true});
-    // We need to wait for the modal to load before clicking the button
-    await page.waitForTimeout(1000);
+    await page.waitForLoadState('networkidle');
     await page.locator('umb-tree-picker-modal').locator('#caret-button').click({force: true});
     await page.getByLabel(dictionaryName).click();
     await page.getByLabel('Submit').click();
@@ -194,9 +189,7 @@ test('can update a partial view name', async ({page, umbracoApi}) => {
   });
 
   test('can delete a partial view', async ({page, umbracoApi, umbracoUi}) => {
-    const partialViewName = 'TestPartialView';
-    const partialViewFileName = partialViewName + ".cshtml";
-    // Arrange
+    //Arrange
     await umbracoApi.partialView.ensureNameNotExistsAtRoot(partialViewFileName);
     await umbracoApi.partialView.create(partialViewFileName, "@inherits Umbraco.Cms.Web.Common.Views.UmbracoViewPage\r\n", "/");
 
@@ -215,9 +208,11 @@ test('can update a partial view name', async ({page, umbracoApi}) => {
   });
 
   test('can create a folder', async ({page, umbracoApi}) => {
+    //Arrange
     const folderName = 'TestFolder';
-    // Act
     await umbracoApi.partialView.ensureNameNotExistsAtRoot(folderName);
+
+    // Act
     await page.locator('umb-tree-item', {hasText: 'Partial Views'}).getByLabel('Open actions menu').click({force: true});
     await page.getByLabel('Create folder').click();
     await page.getByRole('textbox', { name: 'Enter folder name...' }).fill('TestFolder');
@@ -233,7 +228,7 @@ test('can update a partial view name', async ({page, umbracoApi}) => {
     await umbracoApi.partialView.ensureNameNotExistsAtRoot(folderName);
   });
 
-  test('can place a partial view into folder', async ({page, umbracoApi}) => {
+  test.skip('can place a partial view into folder', async ({page, umbracoApi}) => {
     // TODO: implement this later as the frontend is missing now
   });
   
