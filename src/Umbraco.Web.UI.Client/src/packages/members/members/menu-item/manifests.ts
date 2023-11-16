8db2a4af7af35ddef8a490e2695b68caf017670e
Mads Rasmussen <madsr@hey.com>
2023-11-16 20:05:56 +0100

    add member tree repository

--- src/Umbraco.Web.UI.Client/src/packages/members/members/menu-item/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/members/members/menu-item/manifests.ts
@@ -1,16 +1,16 @@
+import { UMB_MEMBER_TREE_ALIAS } from '../tree/index.js';
 import type { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
 
 const menuItem: ManifestTypes = {
 	type: 'menuItem',
 	kind: 'tree',
-	alias: 'Umb.MenuItem.Members',
+	alias: 'Umb.MenuItem.Member',
 	name: 'Members Menu Item',
 	weight: 400,
 	meta: {
 		label: 'Members',
 		icon: 'icon-folder',
-		entityType: 'member',
-		treeAlias: 'Umb.Tree.Members',
+		treeAlias: UMB_MEMBER_TREE_ALIAS,
 		menus: ['Umb.Menu.Members'],
 	},
 };
