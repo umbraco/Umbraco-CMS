76611877fb9b5689321aaa29492ee9f4b8f6f93f
Jesper Møller Jensen <26099018+JesmoDev@users.noreply.github.com>
2023-11-16 16:41:49 +1300

    cleanup

--- src/Umbraco.Web.UI.Client/src/packages/media/media-types/workspace/media-type-workspace.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media-types/workspace/media-type-workspace.element.ts
@@ -1,7 +1,7 @@
 import { UmbMediaTypeWorkspaceContext } from './media-type-workspace.context.js';
 import { UmbMediaTypeWorkspaceEditorElement } from './media-type-workspace-editor.element.js';
-import { UmbTextStyles } from "@umbraco-cms/backoffice/style";
-import { css, html, customElement, state } from '@umbraco-cms/backoffice/external/lit';
+import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
+import { html, customElement, state } from '@umbraco-cms/backoffice/external/lit';
 import { UmbLitElement } from '@umbraco-cms/internal/lit-element';
 import type { UmbRoute } from '@umbraco-cms/backoffice/router';
 
@@ -15,7 +15,7 @@ export class UmbMediaTypeWorkspaceElement extends UmbLitElement {
 		{
 			path: 'edit/:id',
 			component: () => this.#element,
-			setup: (component, info) => {
+			setup: (_component, info) => {
 				const id = info.match.params.id;
 				this.#workspaceContext.load(id);
 			},
@@ -26,20 +26,7 @@ export class UmbMediaTypeWorkspaceElement extends UmbLitElement {
 		return html`<umb-router-slot .routes=${this._routes}></umb-router-slot>`;
 	}
 
-	static styles = [
-		UmbTextStyles,
-		css`
-			#header {
-				display: flex;
-				padding: 0 var(--uui-size-layout-1);
-				gap: var(--uui-size-space-4);
-				width: 100%;
-			}
-			uui-input {
-				width: 100%;
-			}
-		`,
-	];
+	static styles = [UmbTextStyles];
 }
 
 export default UmbMediaTypeWorkspaceElement;
