108416fb2b9c7b32535f802c9d148f210c61ce29
Niels Lyngsø <niels.lyngso@gmail.com>
2023-11-17 08:51:20 +0100

    add inline to monaco editor

--- src/Umbraco.Web.UI.Client/src/external/monaco-editor/index.ts
+++ src/Umbraco.Web.UI.Client/src/external/monaco-editor/index.ts
@@ -1,5 +1,5 @@
 /* eslint local-rules/enforce-umbraco-external-imports: 0 */
-import styles from 'monaco-editor/min/vs/editor/editor.main.css';
+import styles from 'monaco-editor/min/vs/editor/editor.main.css?inline';
 //eslint-disable-next-line
 import editorWorker from 'monaco-editor/esm/vs/editor/editor.worker.js?worker';
 //eslint-disable-next-line
