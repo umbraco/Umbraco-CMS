f418c99fa1ebe750bc52acfc0be8475f3cfe5d7c
Mads Rasmussen <madsr@hey.com>
2023-11-20 17:05:34 +0100

    add collection alias condition

--- src/Umbraco.Web.UI.Client/src/packages/core/collection/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/collection/index.ts
@@ -7,3 +7,5 @@ export * from './components/index.js';
 export * from './collection.context.js';
 export * from './collection-filter-model.interface.js';
 export { type CollectionEntityTypeConditionConfig } from './collection-entity-type.condition.js';
+
+export { UMB_COLLECTION_ALIAS_CONDITION } from './collection-alias.condition.js';
