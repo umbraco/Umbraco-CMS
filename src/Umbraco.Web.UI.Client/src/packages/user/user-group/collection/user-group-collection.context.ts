9f7d511a8e4375f5e9e7f42bf380a4e836438557
Mads Rasmussen <madsr@hey.com>
2023-11-20 17:03:25 +0100

    remove arguments

--- src/Umbraco.Web.UI.Client/src/packages/user/user-group/collection/user-group-collection.context.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user-group/collection/user-group-collection.context.ts
@@ -1,5 +1,4 @@
 import type { UmbUserGroupCollectionFilterModel } from '../types.js';
-import { UMB_USER_GROUP_COLLECTION_REPOSITORY_ALIAS } from './repository/manifests.js';
 import { UmbCollectionContext } from '@umbraco-cms/backoffice/collection';
 import type { UserGroupResponseModel } from '@umbraco-cms/backoffice/backend-api';
 import type { UmbControllerHostElement } from '@umbraco-cms/backoffice/controller-api';
@@ -9,6 +8,6 @@ export class UmbUserGroupCollectionContext extends UmbCollectionContext<
 	UmbUserGroupCollectionFilterModel
 > {
 	constructor(host: UmbControllerHostElement) {
-		super(host, 'user-group', UMB_USER_GROUP_COLLECTION_REPOSITORY_ALIAS);
+		super(host);
 	}
 }
