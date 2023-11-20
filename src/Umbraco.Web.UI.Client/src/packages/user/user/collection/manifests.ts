f458b5cf825010813e1e02a9e86e193d763fbcc1
Mads Rasmussen <madsr@hey.com>
2023-11-20 15:57:44 +0100

    register user collection

--- src/Umbraco.Web.UI.Client/src/packages/user/user/collection/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/collection/manifests.ts
@@ -1,4 +1,16 @@
+import { UMB_USER_COLLECTION_REPOSITORY_ALIAS } from './repository/index.js';
 import { manifests as collectionRepositoryManifests } from './repository/manifests.js';
 import { manifests as collectionViewManifests } from './views/manifests.js';
+import { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
 
-export const manifests = [...collectionRepositoryManifests, ...collectionViewManifests];
+export const UMB_USER_COLLECTION_MANIFEST_ALIAS = 'Umb.Collection.User';
+const collectionManifest: ManifestTypes = {
+	type: 'collection',
+	alias: UMB_USER_COLLECTION_MANIFEST_ALIAS,
+	name: 'User Collection',
+	meta: {
+		repositoryAlias: UMB_USER_COLLECTION_REPOSITORY_ALIAS,
+	},
+};
+
+export const manifests = [collectionManifest, ...collectionRepositoryManifests, ...collectionViewManifests];
