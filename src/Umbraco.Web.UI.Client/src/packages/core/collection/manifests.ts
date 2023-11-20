b47933f6086851ff390d32d0f10fe429de7501b0
Mads Rasmussen <madsr@hey.com>
2023-11-20 20:35:12 +0100

    register condition

--- src/Umbraco.Web.UI.Client/src/packages/core/collection/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/collection/manifests.ts
@@ -1,3 +1,4 @@
 import { manifest as collectionEntityTypeCondition } from './collection-entity-type.condition.js';
+import { manifest as collectionAliasCondition } from './collection-alias.condition.js';
 
-export const manifests = [collectionEntityTypeCondition];
+export const manifests = [collectionAliasCondition, collectionEntityTypeCondition];
