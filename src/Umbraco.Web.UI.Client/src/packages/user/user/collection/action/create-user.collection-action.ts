e0709051ddc6bae4654d4997eadf5a86b0ea2806
Mads Rasmussen <madsr@hey.com>
2023-11-21 13:30:29 +0100

    add create user implementation to collection action

--- src/Umbraco.Web.UI.Client/src/packages/user/user/collection/action/create-user.collection-action.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/collection/action/create-user.collection-action.ts
@@ -1,17 +1,28 @@
 import { UmbUserCollectionContext } from '../user-collection.context.js';
 import { UmbCollectionActionBase } from '@umbraco-cms/backoffice/collection';
+import { UmbContextConsumerController } from '@umbraco-cms/backoffice/context-api';
 import { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
+import {
+	UMB_CREATE_USER_MODAL,
+	UMB_MODAL_MANAGER_CONTEXT_TOKEN,
+	UmbModalManagerContext,
+} from '@umbraco-cms/backoffice/modal';
 
 export class UmbCreateUserCollectionAction extends UmbCollectionActionBase<UmbUserCollectionContext> {
+	#modalManagerContext: UmbModalManagerContext | undefined;
+
 	constructor(host: UmbControllerHost) {
 		super(host);
+
+		new UmbContextConsumerController(this.host, UMB_MODAL_MANAGER_CONTEXT_TOKEN, (instance) => {
+			this.#modalManagerContext = instance;
+		});
 	}
 
-	execute(): Promise<void> {
-		throw new Error('Method not implemented.');
+	async execute() {
+		const modalContext = this.#modalManagerContext?.open(UMB_CREATE_USER_MODAL);
+		await modalContext?.onSubmit();
 	}
 
-	destroy(): void {
-		throw new Error('Method not implemented.');
-	}
+	destroy(): void {}
 }
