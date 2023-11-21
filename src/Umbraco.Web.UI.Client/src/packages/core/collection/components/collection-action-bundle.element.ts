620d8782161e26fec0d19aae50e10a9a2ac6838a
Mads Rasmussen <madsr@hey.com>
2023-11-21 13:04:42 +0100

    add element to render collection actions + remove unused code from toolbar

---
+++ src/Umbraco.Web.UI.Client/src/packages/core/collection/components/collection-action-bundle.element.ts
@@ -0,0 +1,36 @@
+import { UMB_COLLECTION_CONTEXT, UmbCollectionContext } from '../collection.context.js';
+import { html, customElement, state, nothing } from '@umbraco-cms/backoffice/external/lit';
+import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
+import { UmbLitElement } from '@umbraco-cms/internal/lit-element';
+
+@customElement('umb-collection-action-bundle')
+export class UmbCollectionActionBundleElement extends UmbLitElement {
+	#collectionContext?: UmbCollectionContext<any, any>;
+
+	@state()
+	_collectionAlias? = '';
+
+	constructor() {
+		super();
+
+		this.consumeContext(UMB_COLLECTION_CONTEXT, (context) => {
+			this.#collectionContext = context;
+			if (!this.#collectionContext) return;
+			this._collectionAlias = this.#collectionContext.getAlias();
+		});
+	}
+
+	render() {
+		return html`
+			${this._collectionAlias ? html`<umb-extension-slot type="collectionAction"></umb-extension-slot>` : nothing}
+		`;
+	}
+
+	static styles = [UmbTextStyles];
+}
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'umb-collection-action-bundle': UmbCollectionActionBundleElement;
+	}
+}
--- src/Umbraco.Web.UI.Client/src/packages/core/collection/components/collection-toolbar.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/collection/components/collection-toolbar.element.ts
@@ -1,86 +1,12 @@
-import type { TooltipMenuItem } from '../../components/tooltip-menu/index.js';
 import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
-import { css, html, nothing, customElement, property, state } from '@umbraco-cms/backoffice/external/lit';
-import { ManifestCollectionView } from '@umbraco-cms/backoffice/extension-registry';
+import { css, html, customElement } from '@umbraco-cms/backoffice/external/lit';
 import { UmbLitElement } from '@umbraco-cms/internal/lit-element';
 
 @customElement('umb-collection-toolbar')
 export class UmbCollectionToolbarElement extends UmbLitElement {
-	@property()
-	public actions: Array<TooltipMenuItem> = [
-		{
-			label: 'File',
-			icon: 'icon-document',
-			action: () => {
-				console.log('Create file');
-			},
-		},
-		{
-			label: 'Folder',
-			icon: 'icon-folder',
-			action: () => {
-				console.log('create folder');
-			},
-		},
-	];
-
-	@property()
-	public useSearch = true;
-
-	@state()
-	private _layouts: Array<ManifestCollectionView> = [];
-
-	@state()
-	private _currentLayout?: ManifestCollectionView;
-
-	@state()
-	private _search = '';
-	@state()
-	private _actionsOpen = false;
-	@state()
-	private _viewTypesOpen = false;
-
-	constructor() {
-		super();
-	}
-
-	private _changeLayout(path: string) {
-		history.pushState(null, '', 'section/media/dashboard/media-management/' + path);
-	}
-
-	private _updateSearch(e: InputEvent) {
-		this._search = (e.target as HTMLInputElement).value;
-
-		this.dispatchEvent(
-			new CustomEvent('search', {
-				detail: this._search,
-			}),
-		);
-	}
-
-	private _renderCreateButton() {
-		if (this.actions.length === 0) return nothing;
-
-		if (this.actions.length === 1) {
-			return html`<uui-button @click=${() => this.actions[0].action()} look="outline">Create</uui-button>`;
-		}
-		if (this.actions.length > 1) {
-			return html`<uui-popover margin="8" .open=${this._actionsOpen} @close=${() => (this._actionsOpen = false)}>
-				<uui-button @click=${() => (this._actionsOpen = !this._actionsOpen)} slot="trigger" look="outline">
-					Create
-				</uui-button>
-				<umb-tooltip-menu slot="popover" .items=${this.actions}></umb-tooltip-menu>
-			</uui-popover>`;
-		}
-
-		return nothing;
-	}
-
 	render() {
 		return html`
-			${this._renderCreateButton()}
-			<umb-extension-slot type="collectionAction"></umb-extension-slot>
-			<uui-input id="search" @input=${this._updateSearch}></uui-input>
+			<umb-collection-action-bundle></umb-collection-action-bundle>
 			<umb-collection-view-bundle></umb-collection-view-bundle>
 		`;
 	}
@@ -91,13 +17,6 @@ export class UmbCollectionToolbarElement extends UmbLitElement {
 			:host {
 				display: flex;
 				gap: var(--uui-size-space-5);
-				width: 100%;
-			}
-
-			uui-popover {
-				width: min-content;
-			}
-			#search {
 				width: 100%;
 			}
 		`,
