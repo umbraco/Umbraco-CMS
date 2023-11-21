72f6175bbe83161fed83ce1f6f68307a3f95f81e
Mads Rasmussen <madsr@hey.com>
2023-11-21 21:22:01 +0100

    extend elementAndApi

--- src/Umbraco.Web.UI.Client/src/packages/core/extension-registry/models/collection.models.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/extension-registry/models/collection.models.ts
@@ -1,7 +1,7 @@
 import type { ConditionTypes } from '../conditions/types.js';
-import type { ManifestElement, ManifestWithDynamicConditions } from '@umbraco-cms/backoffice/extension-api';
+import type { ManifestElementAndApi, ManifestWithDynamicConditions } from '@umbraco-cms/backoffice/extension-api';
 
-export interface ManifestCollection extends ManifestElement, ManifestWithDynamicConditions<ConditionTypes> {
+export interface ManifestCollection extends ManifestElementAndApi, ManifestWithDynamicConditions<ConditionTypes> {
 	type: 'collection';
 	meta: MetaCollection;
 }
