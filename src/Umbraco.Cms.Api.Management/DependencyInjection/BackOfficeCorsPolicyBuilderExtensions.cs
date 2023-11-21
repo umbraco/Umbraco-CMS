f3b652fc6e1ab2a924d27249c9fa2986dca03a96
Elitsa <elm@umbraco.dk>
2023-11-21 13:04:02 +0100

    Adding missing allow

--- src/Umbraco.Cms.Api.Management/DependencyInjection/BackOfficeCorsPolicyBuilderExtensions.cs
+++ src/Umbraco.Cms.Api.Management/DependencyInjection/BackOfficeCorsPolicyBuilderExtensions.cs
@@ -35,7 +35,8 @@ internal static class BackOfficeCorsPolicyBuilderExtensions
                     policy
                         .WithOrigins(customOrigin)
                         .AllowAnyHeader()
-                        .AllowAnyMethod();
+                        .AllowAnyMethod()
+                        .AllowCredentials();
                 });
         });
 
