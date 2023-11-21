26dfbecb6c190ecdae3c1f5625aefa45b9d9cafb
Mads Rasmussen <madsr@hey.com>
2023-11-21 21:18:42 +0100

    add name

--- src/Umbraco.Web.UI.Client/src/packages/user/user/collection/action/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/collection/action/manifests.ts
@@ -5,6 +5,7 @@ import { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
 export const createManifest: ManifestTypes = {
 	type: 'collectionAction',
 	kind: 'button',
+	name: 'Create User Collection Action',
 	alias: 'Umb.CollectionAction.User.Create',
 	api: UmbCreateUserCollectionAction,
 	weight: 200,
@@ -16,6 +17,7 @@ export const createManifest: ManifestTypes = {
 export const inviteManifest: ManifestTypes = {
 	type: 'collectionAction',
 	kind: 'button',
+	name: 'Invite User Collection Action',
 	alias: 'Umb.CollectionAction.User.Invite',
 	api: UmbInviteUserCollectionAction,
 	weight: 100,
