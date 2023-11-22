eebde385a2b6ac1dfcfcb9778aaaacc0c018c261
Niels Lyngsø <niels.lyngso@gmail.com>
2023-11-22 20:09:09 +0100

    regenerate models

---
+++ src/Umbraco.Web.UI.Client/src/external/backend-api/src/models/PublishDocumentWithDescendantsRequestModel.ts
@@ -0,0 +1,11 @@
+/* generated using openapi-typescript-codegen -- do no edit */
+/* istanbul ignore file */
+/* tslint:disable */
+/* eslint-disable */
+
+import type { PublishDocumentRequestModel } from './PublishDocumentRequestModel';
+
+export type PublishDocumentWithDescendantsRequestModel = (PublishDocumentRequestModel & {
+    includeUnpublishedDescendants?: boolean;
+});
+
