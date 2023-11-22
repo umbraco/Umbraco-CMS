b8023067e336afdc595fe80ad7c0a215d4b0558c
Mads Rasmussen <madsr@hey.com>
2023-11-22 13:34:05 +0100

    import data from the correct source

--- src/Umbraco.Web.UI.Client/src/mocks/data/user/user.db.ts
+++ src/Umbraco.Web.UI.Client/src/mocks/data/user/user.db.ts
@@ -1,7 +1,7 @@
 import { UmbEntityData } from '../entity.data.js';
 import { umbUserGroupData } from '../user-group/user-group.db.js';
 import { arrayFilter, stringFilter, queryFilter } from '../utils.js';
-import { data } from '../data-type.data.js';
+import { data } from './user.data.js';
 import { UmbId } from '@umbraco-cms/backoffice/id';
 import { UmbCurrentUser } from '@umbraco-cms/backoffice/current-user';
 import {
