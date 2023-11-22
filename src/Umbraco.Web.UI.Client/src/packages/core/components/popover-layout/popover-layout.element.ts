f9023ec5212e2a353870f1dd76d78cbe5acd9c0a
Jesper Møller Jensen <26099018+JesmoDev@users.noreply.github.com>
2023-11-22 18:46:55 +1300

    add css vars to popover layout

--- src/Umbraco.Web.UI.Client/src/packages/core/components/popover-layout/popover-layout.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/components/popover-layout/popover-layout.element.ts
@@ -4,6 +4,15 @@ import { UmbLitElement } from '@umbraco-cms/internal/lit-element';
 
 // TODO: maybe move this to UI Library.
 // TODO: add overwrites for customization.
+
+/**
+ * Popover layout element.
+ *
+ * @element umb-popover-layout
+ *
+ * @cssprop {String} --umb-popover-layout-background-color - Override background color.
+ * @cssprop {String} --umb-popover-layout-padding - Override padding.
+ */
 @customElement('umb-popover-layout')
 export class UmbPopoverLayoutElement extends UmbLitElement {
 	render() {
@@ -14,12 +23,12 @@ export class UmbPopoverLayoutElement extends UmbLitElement {
 		UmbTextStyles,
 		css`
 			:host {
-				background-color: var(--uui-color-surface);
+				background-color: var(--umb-popover-layout-background-color, var(--uui-color-surface));
 				display: block;
 				border: 1px solid var(--uui-color-border);
 				border-radius: var(--uui-border-radius);
 				box-shadow: var(--uui-shadow-depth-3);
-				padding: var(--uui-size-space-3);
+				padding: var(--umb-popover-layout-padding, var(--uui-size-space-3));
 				overflow: clip;
 			}
 		`,
