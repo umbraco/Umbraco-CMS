7e3326a4d9aa4e5f62421a171af43a1022d39704
Anna Gevel <anngevel@gmail.com>
2023-11-22 12:16:55 +0100

    Fixed StackOverflow exception when using NoopPropertyIndexValueFactory
    Removed recursive call from the GetIndexValues method of NoopPropertyIndexValueFactory for backward compatibility and alignment with other property index value factory classes

--- src/Umbraco.Core/PropertyEditors/NoopPropertyIndexValueFactory.cs
+++ src/Umbraco.Core/PropertyEditors/NoopPropertyIndexValueFactory.cs
@@ -12,5 +12,5 @@ public class NoopPropertyIndexValueFactory : IPropertyIndexValueFactory
 
     [Obsolete("Use the overload with the availableCultures parameter instead, scheduled for removal in v14")]
     public IEnumerable<KeyValuePair<string, IEnumerable<object?>>> GetIndexValues(IProperty property, string? culture, string? segment, bool published)
-        => GetIndexValues(property, culture, segment, published);
+        => GetIndexValues(property, culture, segment, published, Enumerable.Empty<string>());
 }
