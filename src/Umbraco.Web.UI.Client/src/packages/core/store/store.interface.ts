4fb0a1ae634abb8ffa967b9720416728432dcab8
Mads Rasmussen <madsr@hey.com>
2023-11-23 08:51:45 +0100

    update store interface

--- src/Umbraco.Web.UI.Client/src/packages/core/store/store.interface.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/store/store.interface.ts
@@ -1,5 +1,10 @@
-export interface UmbStore<T> {
+import { Observable } from '@umbraco-cms/backoffice/external/rxjs';
+
+export interface UmbStore<T> extends EventTarget {
+	append: (item: T) => void;
 	appendItems: (items: Array<T>) => void;
 	updateItem: (unique: string, item: Partial<T>) => void;
 	removeItem: (unique: string) => void;
+	removeItems: (uniques: Array<string>) => void;
+	all: () => Observable<Array<T>>;
 }
