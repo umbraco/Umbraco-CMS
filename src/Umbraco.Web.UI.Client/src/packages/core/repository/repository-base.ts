f993718e48d08c79894a60846f791ea6ef8b6249
Mads Rasmussen <madsr@hey.com>
2023-11-24 14:59:34 +0100

    add abstract to base class

--- src/Umbraco.Web.UI.Client/src/packages/core/repository/repository-base.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/repository/repository-base.ts
@@ -1,6 +1,6 @@
 import { UmbBaseController, UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 
-export class UmbRepositoryBase extends UmbBaseController {
+export abstract class UmbRepositoryBase extends UmbBaseController {
 	constructor(host: UmbControllerHost) {
 		super(host);
 	}
