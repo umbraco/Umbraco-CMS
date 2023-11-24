19f563a76f5d1365727be213dfda1e9934e76a7a
Mads Rasmussen <madsr@hey.com>
2023-11-24 11:14:41 +0100

    remove data type repo base class

--- src/Umbraco.Web.UI.Client/src/packages/core/data-type/repository/copy/data-type-copy.repository.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/data-type/repository/copy/data-type-copy.repository.ts
@@ -1,21 +1,35 @@
-import { UmbDataTypeRepositoryBase } from '../data-type-repository-base.js';
+import { UMB_DATA_TYPE_TREE_STORE_CONTEXT, UmbDataTypeTreeStore } from '../../tree/data-type.tree.store.js';
 import { UmbDataTypeDetailRepository } from '../detail/data-type-detail.repository.js';
 import { UmbDataTypeCopyServerDataSource } from './data-type-copy.server.data-source.js';
+import { UMB_NOTIFICATION_CONTEXT_TOKEN, UmbNotificationContext } from '@umbraco-cms/backoffice/notification';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
-import { UmbCopyDataSource, UmbCopyRepository } from '@umbraco-cms/backoffice/repository';
+import { UmbCopyDataSource, UmbCopyRepository, UmbRepositoryBase } from '@umbraco-cms/backoffice/repository';
 
-export class UmbCopyDataTypeRepository extends UmbDataTypeRepositoryBase implements UmbCopyRepository {
+export class UmbCopyDataTypeRepository extends UmbRepositoryBase implements UmbCopyRepository {
+	#init: Promise<unknown>;
 	#copySource: UmbCopyDataSource;
 	#detailRepository: UmbDataTypeDetailRepository;
+	#treeStore?: UmbDataTypeTreeStore;
+	#notificationContext?: UmbNotificationContext;
 
 	constructor(host: UmbControllerHost) {
 		super(host);
 		this.#copySource = new UmbDataTypeCopyServerDataSource(this);
 		this.#detailRepository = new UmbDataTypeDetailRepository(this);
+
+		this.#init = Promise.all([
+			this.consumeContext(UMB_DATA_TYPE_TREE_STORE_CONTEXT, (instance) => {
+				this.#treeStore = instance;
+			}).asPromise(),
+
+			this.consumeContext(UMB_NOTIFICATION_CONTEXT_TOKEN, (instance) => {
+				this.#notificationContext = instance;
+			}).asPromise(),
+		]);
 	}
 
 	async copy(unique: string, targetUnique: string | null) {
-		await this._init;
+		await this.#init;
 		const { data: dataTypeCopyUnique, error } = await this.#copySource.copy(unique, targetUnique);
 		if (error) return { error };
 
@@ -24,10 +38,10 @@ export class UmbCopyDataTypeRepository extends UmbDataTypeRepositoryBase impleme
 			if (!dataTypeCopy) throw new Error('Could not find copied data type');
 
 			// TODO: Be aware about this responsibility.
-			this._treeStore!.append(dataTypeCopy);
+			this.#treeStore!.append(dataTypeCopy);
 
 			const notification = { data: { message: `Data type copied` } };
-			this._notificationContext!.peek('positive', notification);
+			this.#notificationContext!.peek('positive', notification);
 		}
 
 		return { data: dataTypeCopyUnique };
--- src/Umbraco.Web.UI.Client/src/packages/core/data-type/repository/move/data-type-move.repository.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/data-type/repository/move/data-type-move.repository.ts
@@ -1,26 +1,40 @@
-import { UmbDataTypeRepositoryBase } from '../data-type-repository-base.js';
+import { UMB_DATA_TYPE_TREE_STORE_CONTEXT, UmbDataTypeTreeStore } from '../../tree/data-type.tree.store.js';
 import { UmbDataTypeMoveServerDataSource } from './data-type-move.server.data-source.js';
+import { UMB_NOTIFICATION_CONTEXT_TOKEN, UmbNotificationContext } from '@umbraco-cms/backoffice/notification';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
-import { UmbMoveDataSource, UmbMoveRepository } from '@umbraco-cms/backoffice/repository';
+import { UmbMoveDataSource, UmbMoveRepository, UmbRepositoryBase } from '@umbraco-cms/backoffice/repository';
 
-export class UmbMoveDataTypeRepository extends UmbDataTypeRepositoryBase implements UmbMoveRepository {
+export class UmbMoveDataTypeRepository extends UmbRepositoryBase implements UmbMoveRepository {
+	#init: Promise<unknown>;
 	#moveSource: UmbMoveDataSource;
+	#treeStore?: UmbDataTypeTreeStore;
+	#notificationContext?: UmbNotificationContext;
 
 	constructor(host: UmbControllerHost) {
 		super(host);
 		this.#moveSource = new UmbDataTypeMoveServerDataSource(this);
+
+		this.#init = Promise.all([
+			this.consumeContext(UMB_DATA_TYPE_TREE_STORE_CONTEXT, (instance) => {
+				this.#treeStore = instance;
+			}).asPromise(),
+
+			this.consumeContext(UMB_NOTIFICATION_CONTEXT_TOKEN, (instance) => {
+				this.#notificationContext = instance;
+			}).asPromise(),
+		]);
 	}
 
 	async move(unique: string, targetUnique: string | null) {
-		await this._init;
+		await this.#init;
 		const { error } = await this.#moveSource.move(unique, targetUnique);
 
 		if (!error) {
 			// TODO: Be aware about this responsibility.
-			this._treeStore!.updateItem(unique, { parentUnique: targetUnique });
+			this.#treeStore!.updateItem(unique, { parentUnique: targetUnique });
 
 			const notification = { data: { message: `Data type moved` } };
-			this._notificationContext!.peek('positive', notification);
+			this.#notificationContext!.peek('positive', notification);
 		}
 
 		return { error };
