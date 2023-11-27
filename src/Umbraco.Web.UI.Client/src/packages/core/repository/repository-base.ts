05829277a73425677def78a2fdc5c535c9b22c53
Niels Lyngsø <niels.lyngso@gmail.com>
2023-11-27 11:53:06 +0100

    Update repository-base.ts

--- src/Umbraco.Web.UI.Client/src/packages/core/repository/repository-base.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/repository/repository-base.ts
@@ -1,5 +1,6 @@
 import { UmbBaseController, UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 
+// TODO: Revisit if constructor method should be omitted
 export abstract class UmbRepositoryBase extends UmbBaseController {
 	constructor(host: UmbControllerHost) {
 		super(host);
