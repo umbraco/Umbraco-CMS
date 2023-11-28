51ec8b5b0ddcae14a1ba7676b19cf375fd698a2e
Mads Rasmussen <madsr@hey.com>
2023-11-28 12:52:07 +0100

    update interface to return detail model

--- src/Umbraco.Web.UI.Client/src/packages/user/user/invite/repository/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/invite/repository/types.ts
@@ -1,7 +1,7 @@
 import { InviteUserRequestModel } from '@umbraco-cms/backoffice/backend-api';
-import { UmbDataSourceErrorResponse } from '@umbraco-cms/backoffice/repository';
+import { UmbDataSourceResponse } from '@umbraco-cms/backoffice/repository';
 
 export interface UmbInviteUserDataSource {
-	invite(requestModel: InviteUserRequestModel): Promise<UmbDataSourceErrorResponse>;
-	resendInvite(userId: string, requestModel: any): Promise<UmbDataSourceErrorResponse>;
+	invite(requestModel: InviteUserRequestModel): Promise<UmbDataSourceResponse<UmbUserDetail>>;
+	resendInvite(userId: string, requestModel: any): Promise<UmbDataSourceResponse<UmbUserDetail>>;
 }
