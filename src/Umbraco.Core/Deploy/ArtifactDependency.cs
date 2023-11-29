3a3d65325d77849eb96c8cc554d68b50c20c022c
Ronald Barendse <ronald@barend.se>
2023-11-29 14:48:07 +0100

    Update all deploy related XML docs/comments

--- src/Umbraco.Core/Deploy/ArtifactDependency.cs
+++ src/Umbraco.Core/Deploy/ArtifactDependency.cs
@@ -1,27 +1,17 @@
-using System.Text.Json.Serialization;
-
 namespace Umbraco.Cms.Core.Deploy;
 
 /// <summary>
 /// Represents an artifact dependency.
 /// </summary>
-/// <remarks>
-/// <para>
-/// Dependencies have an order property which indicates whether it must be respected when ordering artifacts.
-/// </para>
-/// <para>
-/// Dependencies have a mode which can be <see cref="ArtifactDependencyMode.Match" /> or <see cref="ArtifactDependencyMode.Exist" /> depending on whether the checksum should match.
-/// </para>
-/// </remarks>
 public class ArtifactDependency
 {
     /// <summary>
     /// Initializes a new instance of the <see cref="ArtifactDependency" /> class.
     /// </summary>
     /// <param name="udi">The entity identifier of the artifact dependency.</param>
-    /// <param name="ordering">A value indicating whether the dependency is ordering.</param>
-    /// <param name="mode">The dependency mode.</param>
-    /// <param name="checksum">The checksum.</param>
+    /// <param name="ordering">A value indicating whether the dependency must be included when building a dependency tree and ensure the artifact gets deployed in the correct order.</param>
+    /// <param name="mode">A value indicating whether the checksum must match or the artifact just needs to exist.</param>
+    /// <param name="checksum">The checksum of the dependency.</param>
     public ArtifactDependency(Udi udi, bool ordering, ArtifactDependencyMode mode, string? checksum = null)
     {
         Udi = udi;
@@ -39,10 +29,10 @@ public class ArtifactDependency
     public Udi Udi { get; }
 
     /// <summary>
-    /// Gets a value indicating whether the dependency is ordering.
+    /// Gets a value indicating whether the dependency is included when building a dependency tree and gets deployed in the correct order.
     /// </summary>
     /// <value>
-    ///   <c>true</c> if the dependency is ordering; otherwise, <c>false</c>.
+    ///   <c>true</c> if the dependency is included when building a dependency tree and gets deployed in the correct order; otherwise, <c>false</c>.
     /// </value>
     public bool Ordering { get; }
 
