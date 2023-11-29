7600e39daede3722195c627e48577cd57af8999e
kjac <kja@umbraco.dk>
2023-11-29 13:54:01 +0100

    Review changes

--- src/Umbraco.Infrastructure/BackgroundJobs/RecurringBackgroundJobHostedServiceRunner.cs
+++ src/Umbraco.Infrastructure/BackgroundJobs/RecurringBackgroundJobHostedServiceRunner.cs
@@ -11,7 +11,7 @@ public class RecurringBackgroundJobHostedServiceRunner : IHostedService
     private readonly ILogger<RecurringBackgroundJobHostedServiceRunner> _logger;
     private readonly List<IRecurringBackgroundJob> _jobs;
     private readonly Func<IRecurringBackgroundJob, IHostedService> _jobFactory;
-    private readonly List<(IHostedService HostedService, string JobName)> _hostedServices = new();
+    private readonly List<NamedServiceJob> _hostedServices = new();
 
 
     public RecurringBackgroundJobHostedServiceRunner(
@@ -34,13 +34,13 @@ public class RecurringBackgroundJobHostedServiceRunner : IHostedService
             try
             {
 
-                _logger.LogInformation("Creating background hosted service for {job}", jobName);
+                _logger.LogDebug("Creating background hosted service for {job}", jobName);
                 IHostedService hostedService = _jobFactory(job);
 
                 _logger.LogInformation("Starting background hosted service for {job}", jobName);
                 await hostedService.StartAsync(cancellationToken).ConfigureAwait(false);
 
-                _hostedServices.Add((hostedService, jobName));
+                _hostedServices.Add(new NamedServiceJob(jobName, hostedService));
             }
             catch (Exception exception)
             {
@@ -55,20 +55,32 @@ public class RecurringBackgroundJobHostedServiceRunner : IHostedService
     {
         _logger.LogInformation("Stopping recurring background jobs hosted services");
 
-        foreach ((IHostedService HostedService, string JobName) pair in _hostedServices)
+        foreach (NamedServiceJob namedServiceJob in _hostedServices)
         {
             try
             {
-                _logger.LogInformation("Stopping background hosted service for {job}", pair.JobName);
-                await pair.HostedService.StopAsync(stoppingToken).ConfigureAwait(false);
+                _logger.LogInformation("Stopping background hosted service for {job}", namedServiceJob.Name);
+                await namedServiceJob.HostedService.StopAsync(stoppingToken).ConfigureAwait(false);
             }
             catch (Exception exception)
             {
-                _logger.LogError(exception, "Failed to stop background hosted service for {job}", pair.JobName);
+                _logger.LogError(exception, "Failed to stop background hosted service for {job}", namedServiceJob.Name);
             }
         }
 
         _logger.LogInformation("Completed stopping recurring background jobs hosted services");
+    }
 
+    private class NamedServiceJob
+    {
+        public NamedServiceJob(string name, IHostedService hostedService)
+        {
+            Name = name;
+            HostedService = hostedService;
+        }
+
+        public string Name { get; }
+
+        public IHostedService HostedService { get; }
     }
 }
