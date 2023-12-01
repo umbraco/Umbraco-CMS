d21564482a586e96da14d625d4e5afe4bf934485
Niels Lyngsø <niels.lyngso@gmail.com>
2023-12-01 10:56:42 +0100

    viteStaticCopy

--- src/Umbraco.Web.UI.Client/src/packages/core/themes/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/themes/manifests.ts
@@ -17,14 +17,14 @@ export const themes: Array<ManifestTypes> = [
 		type: 'theme',
 		alias: 'umb-dark-theme',
 		name: 'Dark',
-		css: 'src/css/themes/dark.theme.css',
+		css: '/umbraco/backoffice/css/dark.theme.css',
 		weight: 200,
 	},
 	{
 		type: 'theme',
 		alias: 'umb-high-contrast-theme',
 		name: 'High contrast',
-		css: 'src/css/themes/high-contrast.theme.css',
+		css: '/umbraco/backoffice/css/high-contrast.theme.css',
 		weight: 100,
 	},
 ];
