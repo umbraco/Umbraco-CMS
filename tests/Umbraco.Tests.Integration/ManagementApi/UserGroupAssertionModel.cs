5e89cf2ef72cc237572ee75b511fe1b75fb2a7c1
Andreas Zerbst <andr317c@live.dk>
2023-12-01 16:34:04 +0100

    Added a model for UserGroupAssertions

---
+++ tests/Umbraco.Tests.Integration/ManagementApi/UserGroupAssertionModel.cs
@@ -0,0 +1,10 @@
+﻿using System.Net;
+
+namespace Umbraco.Cms.Tests.Integration.ManagementApi;
+
+public class UserGroupAssertionModel
+{
+    public bool Allowed { get; set; }
+
+    public HttpStatusCode ExpectedStatusCode { get; set; }
+}
