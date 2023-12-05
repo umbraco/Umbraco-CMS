4bad9520143d2fd4e490468667a0828992c03745
Andreas Zerbst <73799582+andr317c@users.noreply.github.com>
2023-12-05 04:55:06 +0100

    V14 QA integration test authorization for languages for ManagementApi (#15355)
    * Updated getParam, if we do not do this we would not get the params for the custom attribute Query
    
    * Added integration test for language authorization

---
+++ tests/Umbraco.Tests.Integration/ManagementApi/Language/DeleteLanguageControllerTests.cs
@@ -0,0 +1,55 @@
+﻿using System.Linq.Expressions;
+using System.Net;
+using NUnit.Framework;
+using Umbraco.Cms.Api.Management.Controllers.Language;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Services;
+
+namespace Umbraco.Cms.Tests.Integration.ManagementApi.Language;
+
+public class DeleteLanguageControllerTests : ManagementApiUserGroupTestBase<DeleteLanguageController>
+{
+    private ILanguageService LanguageService => GetRequiredService<ILanguageService>();
+
+    private const string IsoCode = "da";
+
+    private readonly ILanguage _languageResponseModel = new Core.Models.Language(IsoCode, "Danish");
+
+    protected override Expression<Func<DeleteLanguageController, object>> MethodSelector => x => x.Delete(IsoCode);
+
+    [SetUp]
+    public void Setup() => LanguageService.CreateAsync(_languageResponseModel, Constants.Security.SuperUserKey);
+
+    protected override UserGroupAssertionModel AdminUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel EditorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel SensitiveDataUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel TranslatorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel WriterUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel UnauthorizedUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Unauthorized
+    };
+
+    protected override async Task<HttpResponseMessage> ClientRequest() => await Client.DeleteAsync(Url);
+}
---
+++ tests/Umbraco.Tests.Integration/ManagementApi/Language/UpdateLanguageControllerTests.cs
@@ -0,0 +1,62 @@
+﻿using System.Linq.Expressions;
+using System.Net;
+using System.Net.Http.Json;
+using NUnit.Framework;
+using Umbraco.Cms.Api.Management.Controllers.Language;
+using Umbraco.Cms.Api.Management.ViewModels.Language;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Services;
+
+namespace Umbraco.Cms.Tests.Integration.ManagementApi.Language;
+
+public class UpdateLanguageControllerTests : ManagementApiUserGroupTestBase<UpdateLanguageController>
+{
+    private ILanguageService LanguageService => GetRequiredService<ILanguageService>();
+
+    private const string IsoCode = "da";
+
+    private readonly ILanguage _languageResponseModel = new Core.Models.Language(IsoCode, "Danish");
+
+    protected override Expression<Func<UpdateLanguageController, object>> MethodSelector => x => x.Update(IsoCode, null);
+
+    [SetUp]
+    public void Setup() => LanguageService.CreateAsync(_languageResponseModel, Constants.Security.SuperUserKey);
+
+    protected override UserGroupAssertionModel AdminUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel EditorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel SensitiveDataUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel TranslatorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel WriterUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel UnauthorizedUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Unauthorized
+    };
+
+    protected override async Task<HttpResponseMessage> ClientRequest()
+    {
+        UpdateLanguageRequestModel updateLanguageModel = new() { IsMandatory = true };
+
+        return await Client.PutAsync(Url, JsonContent.Create(updateLanguageModel));
+    }
+}
