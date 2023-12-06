141442174b850dbb6e13d10d2e65843d7972138d
Nhu Dinh <150406148+nhudinh0309@users.noreply.github.com>
2023-12-06 16:56:42 +0700

    Add integration tests about authorization handlers for Help (#15369)

---
+++ tests/Umbraco.Tests.Integration/ManagementApi/Help/GetHelpControllerTests.cs
@@ -0,0 +1,44 @@
+using System.Linq.Expressions;
+using System.Net;
+using NUnit.Framework;
+using Umbraco.Cms.Api.Management.Controllers.Help;
+using Umbraco.Cms.Core;
+
+namespace Umbraco.Cms.Tests.Integration.ManagementApi.Help;
+
+[TestFixture]
+public class GetHelpControllerTests : ManagementApiUserGroupTestBase<GetHelpController>
+{
+    protected override Expression<Func<GetHelpController, object>> MethodSelector =>
+        x => x.Get("TestSection", "TestTree", 0, 100, "https://our.umbraco.com");
+
+    protected override UserGroupAssertionModel AdminUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel EditorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel SensitiveDataUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel TranslatorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel WriterUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel UnauthorizedUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Unauthorized
+    };
+}
