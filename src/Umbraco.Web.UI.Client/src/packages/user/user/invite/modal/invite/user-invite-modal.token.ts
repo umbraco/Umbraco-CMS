2046b8f4e8e21e1529c3f7f9a1091148a3b08bde
Niels Lyngsø <niels.lyngso@gmail.com>
2023-12-06 12:06:13 +0100

    invite modal

--- src/Umbraco.Web.UI.Client/src/packages/user/user/invite/modal/invite/user-invite-modal.token.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/invite/modal/invite/user-invite-modal.token.ts
@@ -1,6 +1,6 @@
 import { UmbModalToken } from '@umbraco-cms/backoffice/modal';
 
-export const UMB_INVITE_USER_MODAL = new UmbModalToken('Umb.Modal.User.Invite', {
+export const UMB_INVITE_USER_MODAL = new UmbModalToken<never, never>('Umb.Modal.User.Invite', {
 	type: 'dialog',
 	size: 'small',
 });
