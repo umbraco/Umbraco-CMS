eaaddc8feac029995dbfe0ad59037279295bf89b
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2023-12-07 09:02:32 +0100

    Update src/Umbraco.Infrastructure/BackgroundJobs/Jobs/WebhookFiring.cs
    Co-authored-by: Ronald Barendse <ronald@barend.se>

--- src/Umbraco.Infrastructure/BackgroundJobs/Jobs/WebhookFiring.cs
+++ src/Umbraco.Infrastructure/BackgroundJobs/Jobs/WebhookFiring.cs
@@ -102,6 +102,7 @@ public class WebhookFiring : IRecurringBackgroundJob
             // Add headers
             request.Headers.Add("Umb-Webhook-Event", eventName);
             request.Headers.Add("Umb-Webhook-RetryCount", retryCount.ToString());
+            request.Headers.Add("Umb-Webhook-Date", date.ToString("R"));
 
             foreach (KeyValuePair<string, string> header in webhook.Headers)
             {
