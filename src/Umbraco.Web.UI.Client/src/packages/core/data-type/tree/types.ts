6bcf985defecc15f6c034991a6b78b02bba6c29f
Mads Rasmussen <madsr@hey.com>
2023-12-07 15:31:47 +0100

    extend base type

--- src/Umbraco.Web.UI.Client/src/packages/core/data-type/tree/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/data-type/tree/types.ts
@@ -1,14 +1,9 @@
-import type { UmbEntityTreeRootModel } from '@umbraco-cms/backoffice/tree';
+import type { UmbEntityTreeRootModel, UmbUniqueTreeItemModel } from '@umbraco-cms/backoffice/tree';
 
-export type UmbDataTypeTreeItemModel = {
-	unique: string;
-	parentUnique: string | null;
+export interface UmbDataTypeTreeItemModel extends UmbUniqueTreeItemModel {
 	isFolder: boolean;
 	isContainer: boolean;
-	name: string;
-	type: string;
-	hasChildren: boolean;
-};
+}
 
 // TODO: TREE STORE TYPE PROBLEM:
 export interface UmbDataTypeTreeRootModel extends UmbEntityTreeRootModel {}
