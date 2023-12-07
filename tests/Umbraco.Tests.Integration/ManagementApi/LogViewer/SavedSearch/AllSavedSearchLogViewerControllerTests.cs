d309ef288bc71381e56f8a59636306903561a048
Andreas Zerbst <andr317c@live.dk>
2023-12-07 09:21:55 +0100

    Added test for the logViewer

---
+++ tests/Umbraco.Tests.Integration/ManagementApi/LogViewer/SavedSearch/AllSavedSearchLogViewerControllerTests.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq.Expressions;
+using System.Net;
+using Umbraco.Cms.Api.Management.Controllers.LogViewer.SavedSearch;
+
+namespace Umbraco.Cms.Tests.Integration.ManagementApi.LogViewer.SavedSearch;
+
+public class AllSavedSearchLogViewerControllerTests : ManagementApiUserGroupTestBase<AllSavedSearchLogViewerController>
+{
+    protected override Expression<Func<AllSavedSearchLogViewerController, object>> MethodSelector => x => x.AllSavedSearches(0, 100);
+
+    protected override UserGroupAssertionModel AdminUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel EditorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel SensitiveDataUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel TranslatorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel WriterUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+}
---
+++ tests/Umbraco.Tests.Integration/ManagementApi/LogViewer/SavedSearch/DeleteSavedSearchLogViewerControllerTests.cs
@@ -0,0 +1,38 @@
+﻿using System.Linq.Expressions;
+using System.Net;
+using Umbraco.Cms.Api.Management.Controllers.LogViewer.SavedSearch;
+
+namespace Umbraco.Cms.Tests.Integration.ManagementApi.LogViewer.SavedSearch;
+
+public class
+    DeleteSavedSearchLogViewerControllerTests : ManagementApiUserGroupTestBase<DeleteSavedSearchLogViewerController>
+{
+    protected override Expression<Func<DeleteSavedSearchLogViewerController, object>> MethodSelector => x => x.Delete("Test");
+
+    protected override UserGroupAssertionModel AdminUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.NotFound
+    };
+
+    protected override UserGroupAssertionModel EditorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel SensitiveDataUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel TranslatorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel WriterUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override async Task<HttpResponseMessage> ClientRequest() => await Client.DeleteAsync(Url);
+}
