6fba4296fc5b55f6c678e35bcba0faade0891b29
Nhu Dinh <hnd@umbraco.dk>
2023-12-07 13:27:59 +0700

    Added tests for Member endpoints

---
+++ tests/Umbraco.Tests.Integration/ManagementApi/Member/Item/ItemMemberItemControllerTests.cs
@@ -0,0 +1,43 @@
+using System.Linq.Expressions;
+using System.Net;
+using NUnit.Framework;
+using Umbraco.Cms.Api.Management.Controllers.Member.Item;
+
+namespace Umbraco.Cms.Tests.Integration.ManagementApi.Member.Item;
+
+[TestFixture]
+public class ItemMemberItemControllerTests : ManagementApiUserGroupTestBase<ItemMemberItemController>
+{
+    protected override Expression<Func<ItemMemberItemController, object>> MethodSelector =>
+        x => x.Item(new HashSet<Guid> { Guid.NewGuid() });
+
+    protected override UserGroupAssertionModel AdminUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel EditorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel SensitiveDataUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel TranslatorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel WriterUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel UnauthorizedUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Unauthorized
+    };
+}
