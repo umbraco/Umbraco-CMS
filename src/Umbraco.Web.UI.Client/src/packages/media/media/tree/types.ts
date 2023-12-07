7bb4739c24306030a6ece74341839a9fffc55194
Mads Rasmussen <madsr@hey.com>
2023-12-07 21:22:50 +0100

    use base

--- src/Umbraco.Web.UI.Client/src/packages/media/media/tree/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/tree/types.ts
@@ -1,5 +1,9 @@
 import type { UmbEntityTreeItemModel, UmbEntityTreeRootModel } from '@umbraco-cms/backoffice/tree';
 
-export interface UmbMediaTreeItemModel extends UmbEntityTreeItemModel {}
+export interface UmbMediaTreeItemModel extends UmbEntityTreeItemModel {
+	noAccess: boolean;
+	isTrashed: boolean;
+}
+
 // TODO: TREE STORE TYPE PROBLEM:
 export interface UmbMediaTreeRootModel extends UmbEntityTreeRootModel {}
