c5ba18374420804139ceaec38407f4f74e706dbe
Mads Rasmussen <madsr@hey.com>
2023-12-07 15:41:03 +0100

    align file names

--- src/Umbraco.Web.UI.Client/src/packages/core/data-type/tree/data-type-tree.repository.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/data-type/tree/data-type-tree.repository.ts
@@ -1,6 +1,6 @@
 import { UmbTreeRepositoryBase } from '../../tree/tree-repository-base.js';
 import { UMB_DATA_TYPE_ROOT_ENTITY_TYPE } from '../entity.js';
-import { UmbDataTypeTreeServerDataSource } from './data-type.tree.server.data.js';
+import { UmbDataTypeTreeServerDataSource } from './data-type-tree.server.data-source.js';
 import { UMB_DATA_TYPE_TREE_STORE_CONTEXT } from './data-type.tree.store.js';
 import { UmbDataTypeTreeItemModel, UmbDataTypeTreeRootModel } from './types.js';
 import { type UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
