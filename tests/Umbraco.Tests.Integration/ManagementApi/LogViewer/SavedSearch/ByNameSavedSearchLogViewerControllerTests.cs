ebf2fae54cb2527a5edf247ad83718aa65a83102
Andreas Zerbst <73799582+andr317c@users.noreply.github.com>
2023-12-08 10:51:15 +0100

    V14 QA Added integration test for the logViewer (#15389)
    * Added test for the logViewer
    
    * Fixed tests after review

---
+++ tests/Umbraco.Tests.Integration/ManagementApi/LogViewer/SavedSearch/ByNameSavedSearchLogViewerControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq.Expressions;
+using System.Net;
+using Umbraco.Cms.Api.Management.Controllers.LogViewer.SavedSearch;
+
+namespace Umbraco.Cms.Tests.Integration.ManagementApi.LogViewer.SavedSearch;
+
+public class ByNameSavedSearchLogViewerControllerTests : ManagementApiUserGroupTestBase<ByNameSavedSearchLogViewerController>
+{
+    protected override Expression<Func<ByNameSavedSearchLogViewerController, object>> MethodSelector => x => x.ByName("Test");
+
+    protected override UserGroupAssertionModel AdminUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.NotFound
+    };
+
+    protected override UserGroupAssertionModel EditorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel SensitiveDataUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel TranslatorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel WriterUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel UnauthorizedUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Unauthorized
+    };
+}
