95f3e677853b14a5b9fee0912758a526b58443eb
Mads Rasmussen <madsr@hey.com>
2023-12-08 11:13:25 +0100

    add temp searcher name

--- src/Umbraco.Web.UI.Client/src/mocks/data/examine.data.ts
+++ src/Umbraco.Web.UI.Client/src/mocks/data/examine.data.ts
@@ -23,6 +23,7 @@ export const Indexers: IndexResponseModel[] = [
 		healthStatus: HealthStatusModel.HEALTHY,
 		documentCount: 0,
 		fieldCount: 0,
+		searcherName: '',
 		providerProperties: {
 			CommitCount: 0,
 			DefaultAnalyzer: 'StandardAnalyzer',
@@ -42,6 +43,7 @@ export const Indexers: IndexResponseModel[] = [
 		healthStatus: HealthStatusModel.HEALTHY,
 		documentCount: 0,
 		fieldCount: 0,
+		searcherName: '',
 		providerProperties: {
 			CommitCount: 0,
 			DefaultAnalyzer: 'CultureInvariantWhitespaceAnalyzer',
@@ -61,6 +63,7 @@ export const Indexers: IndexResponseModel[] = [
 		healthStatus: HealthStatusModel.HEALTHY,
 		fieldCount: 0,
 		documentCount: 0,
+		searcherName: '',
 		providerProperties: {
 			CommitCount: 0,
 			DefaultAnalyzer: 'CultureInvariantWhitespaceAnalyzer',
