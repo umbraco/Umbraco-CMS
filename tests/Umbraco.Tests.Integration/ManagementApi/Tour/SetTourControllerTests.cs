58b194b376288752f56d0af21d6f63149c520e14
Andreas Zerbst <andr317c@live.dk>
2023-12-11 15:08:02 +0100

    Added integration tests for the tour endpoints

---
+++ tests/Umbraco.Tests.Integration/ManagementApi/Tour/SetTourControllerTests.cs
@@ -0,0 +1,49 @@
+﻿using System.Linq.Expressions;
+using System.Net;
+using System.Net.Http.Json;
+using Umbraco.Cms.Api.Management.Controllers.Tour;
+using Umbraco.Cms.Api.Management.ViewModels.Tour;
+
+namespace Umbraco.Cms.Tests.Integration.ManagementApi.Tour;
+
+public class SetTourControllerTests : ManagementApiUserGroupTestBase<SetTourController>
+{
+    protected override Expression<Func<SetTourController, object>> MethodSelector => x => x.SetTour(null);
+
+    protected override UserGroupAssertionModel AdminUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel EditorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel SensitiveDataUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel TranslatorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel WriterUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel UnauthorizedUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Unauthorized
+    };
+
+    protected override async Task<HttpResponseMessage> ClientRequest()
+    {
+        SetTourStatusRequestModel setTourStatusRequest = new() { Alias = "testTour", Disabled = false };
+
+        return await Client.PostAsync(Url, JsonContent.Create(setTourStatusRequest));
+    }
+}
