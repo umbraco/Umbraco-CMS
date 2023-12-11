0b62c47c61c91ff7077f618ae0ddddc716703b85
Andreas Zerbst <andr317c@live.dk>
2023-12-11 14:54:40 +0100

    Added integration tests for the tracked reference controllers

---
+++ tests/Umbraco.Tests.Integration/ManagementApi/TrackedReference/ByIdTrackedReferenceControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq.Expressions;
+using System.Net;
+using Umbraco.Cms.Api.Management.Controllers.TrackedReference;
+
+namespace Umbraco.Cms.Tests.Integration.ManagementApi.TrackedReference;
+
+public class ByIdTrackedReferenceControllerTests : ManagementApiUserGroupTestBase<ByIdTrackedReferenceController>
+{
+    protected override Expression<Func<ByIdTrackedReferenceController, object>> MethodSelector =>
+        x => x.Get(Guid.Empty, 0, 100, false);
+
+    protected override UserGroupAssertionModel AdminUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel EditorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel SensitiveDataUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel TranslatorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel WriterUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel UnauthorizedUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Unauthorized
+    };
+}
---
+++ tests/Umbraco.Tests.Integration/ManagementApi/TrackedReference/DescendantsTrackedReferenceControllerTests.cs
@@ -0,0 +1,42 @@
+﻿using System.Linq.Expressions;
+using System.Net;
+using Umbraco.Cms.Api.Management.Controllers.TrackedReference;
+
+namespace Umbraco.Cms.Tests.Integration.ManagementApi.TrackedReference;
+
+public class
+    DescendantsTrackedReferenceControllerTests : ManagementApiUserGroupTestBase<DescendantsTrackedReferenceController>
+{
+    protected override Expression<Func<DescendantsTrackedReferenceController, object>> MethodSelector =>
+        x => x.Descendants(Guid.Empty, 0, 100, false);
+
+    protected override UserGroupAssertionModel AdminUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel EditorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel SensitiveDataUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel TranslatorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel WriterUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel UnauthorizedUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Unauthorized
+    };
+}
