34287d71f14370999b745fdad15cda13f4e36d6e
Andreas Zerbst <andr317c@live.dk>
2023-12-12 21:19:59 +0100

    Added integration test for Document blueprint endpoints

---
+++ tests/Umbraco.Tests.Integration/ManagementApi/DocumentBlueprint/Item/ItemDocumentBlueprintControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq.Expressions;
+using System.Net;
+using Umbraco.Cms.Api.Management.Controllers.DocumentBlueprint.Item;
+
+namespace Umbraco.Cms.Tests.Integration.ManagementApi.DocumentBlueprint.Item;
+
+public class ItemDocumentBlueprintControllerTests : ManagementApiUserGroupTestBase<ItemDocumentBlueprintController>
+{
+    protected override Expression<Func<ItemDocumentBlueprintController, object>> MethodSelector =>
+        x => x.Item(new HashSet<Guid> { Guid.NewGuid() });
+
+    protected override UserGroupAssertionModel AdminUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel EditorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel SensitiveDataUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel TranslatorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel WriterUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel UnauthorizedUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Unauthorized
+    };
+}
---
+++ tests/Umbraco.Tests.Integration/ManagementApi/DocumentBlueprint/Tree/RootDocumentBlueprintTreeControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq.Expressions;
+using System.Net;
+using Umbraco.Cms.Api.Management.Controllers.DocumentBlueprint.Tree;
+
+namespace Umbraco.Cms.Tests.Integration.ManagementApi.DocumentBlueprint.Tree;
+
+public class RootDocumentBlueprintTreeControllerTests : ManagementApiUserGroupTestBase<RootDocumentBlueprintTreeController>
+{
+    protected override Expression<Func<RootDocumentBlueprintTreeController, object>> MethodSelector =>
+        x => x.Root(0, 100);
+
+    protected override UserGroupAssertionModel AdminUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel EditorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel SensitiveDataUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel TranslatorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel WriterUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel UnauthorizedUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Unauthorized
+    };
+}
