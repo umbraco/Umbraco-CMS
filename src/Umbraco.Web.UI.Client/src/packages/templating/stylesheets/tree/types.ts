fd129609524c28cde920c95b9254a3df4ae4f97d
Mads Rasmussen <madsr@hey.com>
2023-12-12 19:25:47 +0100

    set types for repository

--- src/Umbraco.Web.UI.Client/src/packages/templating/stylesheets/tree/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/templating/stylesheets/tree/types.ts
@@ -1,5 +1,5 @@
-import type { UmbFileSystemTreeItemModel } from '@umbraco-cms/backoffice/tree';
+import type { UmbFileSystemTreeItemModel, UmbFileSystemTreeRootModel } from '@umbraco-cms/backoffice/tree';
 
 export interface UmbStylesheetTreeItemModel extends UmbFileSystemTreeItemModel {}
 // TODO: TREE STORE TYPE PROBLEM:
-export interface UmbStylesheetTreeRootModel extends UmbFileSystemTreeItemModel {}
+export interface UmbStylesheetTreeRootModel extends UmbFileSystemTreeRootModel {}
