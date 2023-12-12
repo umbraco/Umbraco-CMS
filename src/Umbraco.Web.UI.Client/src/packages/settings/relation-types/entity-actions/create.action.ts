e93ea1fb44f739acc6dd91f870a9e48222db381f
Jesper Møller Jensen <26099018+JesmoDev@users.noreply.github.com>
2023-12-13 12:46:24 +1300

    add create action

---
+++ src/Umbraco.Web.UI.Client/src/packages/settings/relation-types/entity-actions/create.action.ts
@@ -0,0 +1,15 @@
+import { UmbRelationTypeRepository } from '../repository/relation-type.repository.js';
+import { UmbEntityActionBase } from '@umbraco-cms/backoffice/entity-action';
+import { UmbControllerHostElement } from '@umbraco-cms/backoffice/controller-api';
+
+export class UmbCreateRelationTypeEntityAction extends UmbEntityActionBase<UmbRelationTypeRepository> {
+	// TODO: Could EntityActions take the manifest instead, for more flexibility?
+	constructor(host: UmbControllerHostElement, repositoryAlias: string, unique: string) {
+		super(host, repositoryAlias, unique);
+	}
+
+	async execute() {
+		// TODO: Generate the href or retrieve it from something?
+		history.pushState(null, '', `section/settings/workspace/relation-type/create`);
+	}
+}
