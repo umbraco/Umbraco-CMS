08dacbeeee6e54f65306e516019dc6a87dfde521
Andreas Zerbst <andr317c@live.dk>
2023-12-12 18:10:15 +0100

    Added tests for the health checks

---
+++ tests/Umbraco.Tests.Integration/ManagementApi/HealthCheck/Group/ByNameHealthCheckGroupControllerTests.cs
@@ -0,0 +1,41 @@
+﻿using System.Linq.Expressions;
+using System.Net;
+using Umbraco.Cms.Api.Management.Controllers.HealthCheck.Group;
+
+namespace Umbraco.Cms.Tests.Integration.ManagementApi.HealthCheck.Group;
+
+public class ByNameHealthCheckGroupControllerTests : ManagementApiUserGroupTestBase<ByNameHealthCheckGroupController>
+{
+    protected override Expression<Func<ByNameHealthCheckGroupController, object>> MethodSelector =>
+        x => x.ByName("Configuration");
+
+    protected override UserGroupAssertionModel AdminUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel EditorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel SensitiveDataUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel TranslatorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel WriterUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel UnauthorizedUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Unauthorized
+    };
+}
