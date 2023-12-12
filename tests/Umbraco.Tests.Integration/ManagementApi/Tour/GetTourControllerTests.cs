3507733e7de7422ac314cdf1a09f2009a2315b31
Andreas Zerbst <73799582+andr317c@users.noreply.github.com>
2023-12-12 08:22:50 +0100

    Added integration tests for the tour endpoints (#15423)

---
+++ tests/Umbraco.Tests.Integration/ManagementApi/Tour/GetTourControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq.Expressions;
+using System.Net;
+using Umbraco.Cms.Api.Management.Controllers.Tour;
+
+namespace Umbraco.Cms.Tests.Integration.ManagementApi.Tour;
+
+public class GetTourControllerTests : ManagementApiUserGroupTestBase<GetTourController>
+{
+    protected override Expression<Func<GetTourController, object>> MethodSelector => x => x.GetTours();
+
+    protected override UserGroupAssertionModel AdminUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel EditorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel SensitiveDataUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel TranslatorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel WriterUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel UnauthorizedUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Unauthorized
+    };
+}
