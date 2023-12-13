89979e17ed5a9e51e08b22dec21de4cfbd5d75b3
Mads Rasmussen <madsr@hey.com>
2023-12-13 19:04:04 +0100

    remove wrong import

--- src/Umbraco.Web.UI.Client/src/packages/templating/scripts/repository/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/templating/scripts/repository/index.ts
@@ -1,2 +1,2 @@
-export { UMB_SCRIPT_REPOSITORY_ALIAS } from './manifests.js';
+export { UMB_SCRIPT_DETAIL_REPOSITORY_ALIAS as UMB_SCRIPT_REPOSITORY_ALIAS } from './manifests.js';
 export { UmbScriptDetailRepository } from './script-detail.repository.js';
--- src/Umbraco.Web.UI.Client/src/packages/templating/scripts/repository/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/templating/scripts/repository/manifests.ts
@@ -1,23 +1,22 @@
 import { UmbScriptDetailRepository } from './script-detail.repository.js';
 import { UmbScriptDetailStore } from './script-detail.store.js';
-import { manifests as folderManifests } from '../tree/folder/manifests.js';
 import { ManifestRepository, ManifestStore } from '@umbraco-cms/backoffice/extension-registry';
 
-export const UMB_SCRIPT_REPOSITORY_ALIAS = 'Umb.Repository.Script';
-export const UMB_SCRIPT_STORE_ALIAS = 'Umb.Store.Script';
+export const UMB_SCRIPT_DETAIL_REPOSITORY_ALIAS = 'Umb.Repository.Script.Detail';
+export const UMB_SCRIPT_DETAIL_STORE_ALIAS = 'Umb.Store.Script.Detail';
 
 const repository: ManifestRepository = {
 	type: 'repository',
-	alias: UMB_SCRIPT_REPOSITORY_ALIAS,
-	name: 'Script Repository',
+	alias: UMB_SCRIPT_DETAIL_REPOSITORY_ALIAS,
+	name: 'Script Detail Repository',
 	api: UmbScriptDetailRepository,
 };
 
 const store: ManifestStore = {
 	type: 'store',
-	alias: UMB_SCRIPT_STORE_ALIAS,
-	name: 'Script Store',
+	alias: UMB_SCRIPT_DETAIL_STORE_ALIAS,
+	name: 'Script Detail Store',
 	api: UmbScriptDetailStore,
 };
 
-export const manifests = [repository, store, ...folderManifests];
+export const manifests = [repository, store];
