fe389ac37a7c9a5ced57a65e4fd44494a2002e73
Andreas Zerbst <73799582+andr317c@users.noreply.github.com>
2023-12-13 03:37:20 +0100

    Added tests for the user (#15403)
    * Added tests for the user
    
    * Added a constant for the SuperUserKey so we can avoid the status code 404(as much as possible)
    
    The status code 404 can be quite misleading. And we are already logging in a a super admin in the setup.
    
    * Added missing tests controller
    
    * Fixed wrong status code

---
+++ tests/Umbraco.Tests.Integration/ManagementApi/User/Current/GetPermissionsCurrentUserControllerTests.cs
@@ -0,0 +1,40 @@
+﻿using System.Linq.Expressions;
+using System.Net;
+using Umbraco.Cms.Api.Management.Controllers.User.Current;
+
+namespace Umbraco.Cms.Tests.Integration.ManagementApi.User.Current;
+
+public class GetPermissionsCurrentUserControllerTests : ManagementApiUserGroupTestBase<GetPermissionsCurrentUserController>
+{
+    protected override Expression<Func<GetPermissionsCurrentUserController, object>> MethodSelector => x => x.GetPermissions(new HashSet<Guid> { Guid.Empty });
+
+    protected override UserGroupAssertionModel AdminUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.OK
+    };
+
+    protected override UserGroupAssertionModel EditorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel SensitiveDataUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel TranslatorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel WriterUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Forbidden
+    };
+
+    protected override UserGroupAssertionModel UnauthorizedUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.Unauthorized
+    };
+}
