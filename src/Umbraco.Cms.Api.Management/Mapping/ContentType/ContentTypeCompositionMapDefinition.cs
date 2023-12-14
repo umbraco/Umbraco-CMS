710f594efb2361322e39d000b6670e6825f528e7
Elitsa <elm@umbraco.dk>
2023-12-14 14:05:15 +0100

    Renaming + TODO

--- src/Umbraco.Cms.Api.Management/Mapping/ContentType/ContentTypeCompositionMapDefinition.cs
+++ src/Umbraco.Cms.Api.Management/Mapping/ContentType/ContentTypeCompositionMapDefinition.cs
@@ -7,11 +7,12 @@ namespace Umbraco.Cms.Api.Management.Mapping.ContentType;
 public class ContentTypeCompositionMapDefinition : IMapDefinition
 {
     public void DefineMaps(IUmbracoMapper mapper)
-        => mapper.Define<IContentType, ContentTypeCompositionResponseModel>(
-            (_, _) => new ContentTypeCompositionResponseModel(), Map);
+        => mapper.Define<IContentType, DocumentTypeCompositionResponseModel>(
+            (_, _) => new DocumentTypeCompositionResponseModel(), Map);
 
+    // TODO: Consider abstracting this mapping
     // Umbraco.Code.MapAll
-    private static void Map(IContentType source, ContentTypeCompositionResponseModel target, MapperContext context)
+    private static void Map(IContentType source, DocumentTypeCompositionResponseModel target, MapperContext context)
     {
         target.Id = source.Key;
         target.Name = source.Name ?? string.Empty;
