44c8bd12ce5aa87a0a33842355b79239606dc01e
Mads Rasmussen <madsr@hey.com>
2023-12-18 13:19:24 +0100

    update export names

--- src/Umbraco.Web.UI.Client/src/packages/templating/stylesheets/tree/folder/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/templating/stylesheets/tree/folder/index.ts
@@ -1,6 +1,6 @@
-export { UmbScriptFolderRepository } from './stylesheet-folder.repository.js';
+export { UmbStylesheetFolderRepository } from './stylesheet-folder.repository.js';
 export {
-	UMB_SCRIPT_FOLDER_REPOSITORY_ALIAS,
-	UMB_DELETE_SCRIPT_FOLDER_ENTITY_ACTION_ALIAS,
-	UMB_CREATE_SCRIPT_FOLDER_ENTITY_ACTION_ALIAS,
+	UMB_STYLESHEET_FOLDER_REPOSITORY_ALIAS,
+	UMB_DELETE_STYLESHEET_FOLDER_ENTITY_ACTION_ALIAS,
+	UMB_CREATE_STYLESHEET_FOLDER_ENTITY_ACTION_ALIAS,
 } from './manifests.js';
