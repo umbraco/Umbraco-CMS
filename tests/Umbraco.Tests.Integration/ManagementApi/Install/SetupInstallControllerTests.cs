70efd59c7c91a29c213b59924b5e7369ce52bf21
Andreas Zerbst <73799582+andr317c@users.noreply.github.com>
2023-12-20 11:06:15 +0100

    Added integration test for the install endpoints (#15483)

---
+++ tests/Umbraco.Tests.Integration/ManagementApi/Install/SetupInstallControllerTests.cs
@@ -0,0 +1,58 @@
+﻿using System.Linq.Expressions;
+using System.Net;
+using System.Net.Http.Json;
+using Umbraco.Cms.Api.Management.Controllers.Install;
+using Umbraco.Cms.Api.Management.ViewModels.Installer;
+using Umbraco.Cms.Core.Models;
+
+namespace Umbraco.Cms.Tests.Integration.ManagementApi.Install;
+
+public class SetupInstallControllerTests : ManagementApiUserGroupTestBase<SetupInstallController>
+{
+    protected override Expression<Func<SetupInstallController, object>> MethodSelector =>
+        x => x.Setup(null);
+
+    protected override UserGroupAssertionModel AdminUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.PreconditionRequired
+    };
+
+    protected override UserGroupAssertionModel EditorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.PreconditionRequired
+    };
+
+    protected override UserGroupAssertionModel SensitiveDataUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.PreconditionRequired
+    };
+
+    protected override UserGroupAssertionModel TranslatorUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.PreconditionRequired
+    };
+
+    protected override UserGroupAssertionModel WriterUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.PreconditionRequired
+    };
+
+    protected override UserGroupAssertionModel UnauthorizedUserGroupAssertionModel => new()
+    {
+        ExpectedStatusCode = HttpStatusCode.PreconditionRequired
+    };
+
+    protected override async Task<HttpResponseMessage> ClientRequest()
+    {
+        InstallVResponseModel installResponseModel = new()
+        {
+            User = new UserInstallResponseModel
+            {
+                Name = "Tester", Email = "Test@emails.test", Password = "123457890"
+            },
+            Database = new DatabaseInstallResponseModel { Id = Guid.NewGuid(), ProviderName = "TestProviderName" },
+            TelemetryLevel = TelemetryLevel.Basic
+        };
+        return await Client.PostAsync(Url, JsonContent.Create(installResponseModel));
+    }
+}
