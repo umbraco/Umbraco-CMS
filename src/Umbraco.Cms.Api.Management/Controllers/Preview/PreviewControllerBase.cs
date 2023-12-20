da680100e1568e08cc269d965773cd3a82ee79f1
Zeegaan <skrivdetud@gmail.com>
2023-12-20 10:48:22 +0100

    Add Authorization to previews

--- src/Umbraco.Cms.Api.Management/Controllers/Preview/PreviewControllerBase.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/Preview/PreviewControllerBase.cs
@@ -1,11 +1,14 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Api.Management.Routing;
+using Umbraco.Cms.Web.Common.Authorization;
 
 namespace Umbraco.Cms.Api.Management.Controllers.Preview;
 
 [ApiController]
 [VersionedApiBackOfficeRoute("preview")]
 [ApiExplorerSettings(GroupName = "Preview")]
+[Authorize(Policy = "New" + AuthorizationPolicies.BackOfficeAccess)]
 public class PreviewControllerBase : ManagementApiControllerBase
 {
 }
