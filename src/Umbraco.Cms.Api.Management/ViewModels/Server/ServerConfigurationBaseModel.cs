c9c307c54c63136e2f80d243860f229b4f90d5e3
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2023-12-21 11:01:07 +0100

    Add ServerConfigurationBaseModel

---
+++ src/Umbraco.Cms.Api.Management/ViewModels/Server/ServerConfigurationBaseModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Umbraco.Cms.Api.Management.ViewModels.Server;
+
+public class ServerConfigurationBaseModel
+{
+    public IDictionary<string, object> Items { get; set; } = new Dictionary<string, object>();
+}
