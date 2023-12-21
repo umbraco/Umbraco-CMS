2dc00c6a7cfa89d9cc78f53cc8df7a64c4ede4ca
kjac <kja@umbraco.dk>
2023-12-21 19:11:21 +0100

    Do not prefix system paths with directory separator char

--- src/Umbraco.Cms.Api.Management/Extensions/StringPathExtensions.cs
+++ src/Umbraco.Cms.Api.Management/Extensions/StringPathExtensions.cs
@@ -7,7 +7,7 @@ internal static class StringPathExtensions
 {
     public static string SystemPathToVirtualPath(this string systemPath) => systemPath.Replace('\\', '/').TrimStart('~').EnsureStartsWith('/');
 
-    public static string VirtualPathToSystemPath(this string virtualPath) => virtualPath.Replace('/', Path.DirectorySeparatorChar).EnsureStartsWith(Path.DirectorySeparatorChar);
+    public static string VirtualPathToSystemPath(this string virtualPath) => virtualPath.TrimStart('/').Replace('/', Path.DirectorySeparatorChar);
 
     public static string? ParentPath(this string path) => Path.GetDirectoryName(path);
 }
