564c1c1a0c6a0a6073ba282f055a641a8c3873eb
Markus Johansson <markus@enkelmedia.se>
2023-12-28 15:19:27 +0100

    Fixes #15512 to allow curly braces in anchors

--- src/Umbraco.Web.UI.Client/src/common/services/tinymce.service.js
+++ src/Umbraco.Web.UI.Client/src/common/services/tinymce.service.js
@@ -1184,7 +1184,7 @@ function tinyMceService($rootScope, $q, imageHelper, $locale, $http, $timeout, s
           if (currentTarget.url.indexOf("localLink:") > 0) {
             // if the current link has an anchor, it needs to be considered when getting the udi/id
             // if an anchor exists, reduce the substring max by its length plus two to offset the removed prefix and trailing curly brace
-            var linkId = currentTarget.url.substring(currentTarget.url.indexOf(":") + 1, currentTarget.url.lastIndexOf("}"));
+            var linkId = currentTarget.url.substring(currentTarget.url.indexOf(":") + 1, currentTarget.url.indexOf("}"));
 
             //we need to check if this is an INT or a UDI
             var parsedIntId = parseInt(linkId, 10);
