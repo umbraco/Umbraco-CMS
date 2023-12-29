e8e79ef8ee8af35325acca76cec714714bde7032
Nhu Dinh <hnd@umbraco.dk>
2023-12-29 14:20:09 +0700

    Added ui tests for Relation Type

---
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Settings/RelationTypes.spec.ts
@@ -0,0 +1,29 @@
+﻿import {ConstantHelper, test} from '@umbraco/playwright-testhelpers';
+import {expect} from "@playwright/test";
+
+test.describe('Relation types tests', () => {
+  const relationTypeName = 'Test Relation Type';
+
+  test.beforeEach(async ({umbracoUi}) => {
+    await umbracoUi.goToBackOffice();
+    await umbracoUi.relationType.goToSettingsTreeItem('Relation Types');
+  });
+
+  test.afterEach(async ({umbracoApi}) => {
+    await umbracoApi.relationType.ensureNameNotExists(relationTypeName);
+  });
+
+  test('can create a relation type', async ({umbracoApi, umbracoUi}) => {
+
+  });
+
+  test('can edit a relation type', async ({umbracoApi, umbracoUi}) => {
+
+  });
+
+  test('can show relation', async ({umbracoApi, umbracoUi}) => {
+
+  });
+
+
+});
