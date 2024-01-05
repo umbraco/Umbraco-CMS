0c0d6fcc4c21b26fc4cf9b087c753d7ab6876f34
Niels Lyngsø <niels.lyngso@gmail.com>
2024-01-05 12:51:22 +0100

    declare clone method in interface

--- src/Umbraco.Web.UI.Client/src/libs/context-api/consume/context-request.event.ts
+++ src/Umbraco.Web.UI.Client/src/libs/context-api/consume/context-request.event.ts
@@ -12,6 +12,7 @@ export interface UmbContextRequestEvent<ResultType = unknown> extends Event {
 	readonly apiAlias: string;
 	readonly callback: (context: ResultType) => boolean;
 	readonly stopAtContextMatch: boolean;
+	clone(): UmbContextRequestEvent<ResultType>;
 }
 
 /**
