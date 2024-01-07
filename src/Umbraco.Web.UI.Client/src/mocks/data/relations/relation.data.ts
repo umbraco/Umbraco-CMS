3959890d6ca0b27a6a97f36b4f1d0c08a4a9c2de
Jesper Møller Jensen <26099018+JesmoDev@users.noreply.github.com>
2024-01-08 12:35:04 +1300

    relation mocks

--- src/Umbraco.Web.UI.Client/src/mocks/data/relations/relation.data.ts
+++ src/Umbraco.Web.UI.Client/src/mocks/data/relations/relation.data.ts
@@ -1,65 +1,66 @@
+import { UmbEntityData } from '../entity.data.js';
 import type { RelationResponseModel } from '@umbraco-cms/backoffice/backend-api';
 
 export const data: Array<RelationResponseModel> = [
 	{
-		parentId: '1',
-		parentName: 'TEST Parent 1',
+		parentId: 'e0d39ff5-71d8-453f-b682-9d8d31ee5e06',
+		parentName: 'Relate Document On Copy',
 		childId: '2',
 		childName: 'Child 1',
 		createDate: '2021-01-01',
 		comment: 'Comment 1',
 	},
 	{
-		parentId: '1',
-		parentName: 'Parent 1',
+		parentId: 'e0d39ff5-71d8-453f-b682-9d8d31ee5e06',
+		parentName: 'Relate Document On Copy',
 		childId: '3',
 		childName: 'Child 2',
 		createDate: '2021-01-01',
 		comment: 'Comment 2',
 	},
 	{
-		parentId: '1',
-		parentName: 'Parent 1',
+		parentId: 'e0d39ff5-71d8-453f-b682-9d8d31ee5e06',
+		parentName: 'Relate Document On Copy',
 		childId: '4',
 		childName: 'Child 3',
 		createDate: '2021-01-01',
 		comment: 'Comment 3',
 	},
 	{
-		parentId: '1',
-		parentName: 'Parent 1',
+		parentId: 'e0d39ff5-71d8-453f-b682-9d8d31ee5e06',
+		parentName: 'Relate Document On Copy',
 		childId: '5',
 		childName: 'Child 4',
 		createDate: '2021-01-01',
 		comment: 'Comment 4',
 	},
 	{
-		parentId: '1',
-		parentName: 'Parent 1',
+		parentId: 'e0d39ff5-71d8-453f-b682-9d8d31ee5e06',
+		parentName: 'Relate Document On Copy',
 		childId: '6',
 		childName: 'Child 5',
 		createDate: '2021-01-01',
 		comment: 'Comment 5',
 	},
 	{
-		parentId: '1',
-		parentName: 'Parent 1',
+		parentId: 'e0d39ff5-71d8-453f-b682-9d8d31ee5e06',
+		parentName: 'Relate Document On Copy',
 		childId: '7',
 		childName: 'Child 6',
 		createDate: '2021-01-01',
 		comment: 'Comment 6',
 	},
 	{
-		parentId: '1',
-		parentName: 'Parent 1',
+		parentId: 'e0d39ff5-71d8-453f-b682-9d8d31ee5e06',
+		parentName: 'Relate Document On Copy',
 		childId: '8',
 		childName: 'Child 7',
 		createDate: '2021-01-01',
 		comment: 'Comment 7',
 	},
 	{
-		parentId: '1',
-		parentName: 'Parent 1',
+		parentId: 'e0d39ff5-71d8-453f-b682-9d8d31ee5e06',
+		parentName: 'Relate Document On Copy',
 		childId: '9',
 		childName: 'Child 8',
 		createDate: '2021-01-01',
@@ -73,6 +74,11 @@ export const data: Array<RelationResponseModel> = [
 class UmbRelationData extends UmbEntityData<RelationResponseModel> {
 	constructor() {
 		super(data);
+	}
+
+	getRelationsByParentId(id: string) {
+		const test = this.data.filter((relation) => relation.parentId === id);
+		return { items: test, total: test.length };
 	}
 }
 
