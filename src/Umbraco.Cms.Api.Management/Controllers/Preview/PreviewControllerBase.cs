3acda751b95fe3c5d1bcc7a0da96ff4b8570ec78
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2024-01-09 15:51:59 +0100

    v14: Add Preview controller (#15481)
    * Add Preview controller
    
    * Implement PreviewService
    
    * Add tests for endpoints.
    
    * Add Authorization to previews
    
    * Apply suggestions from code review
    
    Co-authored-by: Elitsa Marinovska <21998037+elit0451@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Elitsa <elm@umbraco.dk>
    Co-authored-by: Elitsa Marinovska <21998037+elit0451@users.noreply.github.com>

---
+++ src/Umbraco.Cms.Api.Management/Controllers/Preview/PreviewControllerBase.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Management.Routing;
+using Umbraco.Cms.Web.Common.Authorization;
+
+namespace Umbraco.Cms.Api.Management.Controllers.Preview;
+
+[ApiController]
+[VersionedApiBackOfficeRoute("preview")]
+[ApiExplorerSettings(GroupName = "Preview")]
+public class PreviewControllerBase : ManagementApiControllerBase
+{
+}
