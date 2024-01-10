2fbfb72fd4ecef65970d04e5181ffd91061bf298
Mads Rasmussen <madsr@hey.com>
2024-01-10 09:40:13 +0100

    add detail to repository alias

--- src/Umbraco.Web.UI.Client/src/packages/templating/partial-views/repository/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/templating/partial-views/repository/manifests.ts
@@ -2,12 +2,12 @@ import { UmbPartialViewDetailRepository } from './partial-view-detail.repository
 import { UmbPartialViewDetailStore } from './partial-view-detail.store.js';
 import { ManifestRepository, ManifestStore } from '@umbraco-cms/backoffice/extension-registry';
 
-export const UMB_PARTIAL_VIEW_REPOSITORY_ALIAS = 'Umb.Repository.PartialView.Detail';
+export const UMB_PARTIAL_VIEW_DETAIL_REPOSITORY_ALIAS = 'Umb.Repository.PartialView.Detail';
 export const UMB_PARTIAL_VIEW_DETAIL_STORE_ALIAS = 'Umb.Store.PartialView.Detail';
 
 const repository: ManifestRepository = {
 	type: 'repository',
-	alias: UMB_PARTIAL_VIEW_REPOSITORY_ALIAS,
+	alias: UMB_PARTIAL_VIEW_DETAIL_REPOSITORY_ALIAS,
 	name: 'Partial View Detail Repository',
 	api: UmbPartialViewDetailRepository,
 };
