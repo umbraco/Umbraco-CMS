648c2ac4ee1b7462179bddc1e61f0975ee17d928
Zeegaan <skrivdetud@gmail.com>
2024-01-10 14:13:23 +0100

    Implement configuration security controller

---
+++ src/Umbraco.Cms.Api.Management/ViewModels/Security/SecurityConfigurationResponseModel.cs
@@ -0,0 +1,6 @@
+﻿namespace Umbraco.Cms.Api.Management.ViewModels.Security;
+
+public class SecurityConfigurationResponseModel
+{
+    public PasswordConfigurationResponseModel PasswordConfiguration { get; set; } = null!;
+}
