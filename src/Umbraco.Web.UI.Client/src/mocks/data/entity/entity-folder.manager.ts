3926a941e19eaeba3cd6b449b9b93e5046cf74ec
Mads Rasmussen <madsr@hey.com>
2024-01-11 11:40:17 +0100

    add mock entity managers

---
+++ src/Umbraco.Web.UI.Client/src/mocks/data/entity/entity-folder.manager.ts
@@ -0,0 +1,61 @@
+import { UmbEntityMockDbBase } from './entity-base.js';
+import {
+	CreateFolderRequestModel,
+	FolderResponseModel,
+	FolderTreeItemResponseModel,
+	UpdateFolderResponseModel,
+} from '@umbraco-cms/backoffice/backend-api';
+
+export class UmbMockEntityFolderManager<MockItemType extends Omit<FolderTreeItemResponseModel, 'type'>> {
+	#db: UmbEntityMockDbBase<MockItemType>;
+	#createMockFolderMapper: (request: CreateFolderRequestModel) => MockItemType;
+
+	constructor(
+		db: UmbEntityMockDbBase<MockItemType>,
+		createMockFolderMapper: (request: CreateFolderRequestModel) => MockItemType,
+	) {
+		this.#db = db;
+		this.#createMockFolderMapper = createMockFolderMapper;
+	}
+
+	create(request: CreateFolderRequestModel) {
+		const newFolder = this.#createMockFolderMapper(request);
+		this.#db.create(newFolder);
+		return newFolder.id;
+	}
+
+	read(id: string) {
+		const mockItem = this.#db.read(id);
+		if (mockItem?.isFolder) {
+			return this.#defaultReadMapper(mockItem);
+		} else {
+			return undefined;
+		}
+	}
+
+	update(id: string, request: UpdateFolderResponseModel) {
+		const mockItem = this.#db.read(id);
+
+		const updatedMockItem = {
+			...mockItem,
+			name: request.name,
+		} as unknown as MockItemType;
+
+		this.#db.update(id, updatedMockItem);
+	}
+
+	delete(id: string) {
+		const dbItem = this.#db.read(id);
+		if (dbItem?.isFolder) {
+			this.#db.delete(id);
+		}
+	}
+
+	#defaultReadMapper = (item: MockItemType): FolderResponseModel => {
+		return {
+			name: item.name,
+			id: item.id,
+			parentId: item.parentId,
+		};
+	};
+}
