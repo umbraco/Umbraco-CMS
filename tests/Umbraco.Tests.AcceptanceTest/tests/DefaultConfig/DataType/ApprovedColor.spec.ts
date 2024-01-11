ef1ef00f1dff867eb04f0c35f862bc1e1acb674c
Nhu Dinh <150406148+nhudinh0309@users.noreply.github.com>
2024-01-11 14:07:10 +0700

    V14 Added acceptance test for basic functions of Data Type, Approved Color and Label (#15551)
    * Updated api tests for DataType
    
    * Added ui tests for basic functions of DataType
    
    * Added ui tests for Approved color
    
    * Added ui tests for Label
    
    * Fixed comments

---
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/DataType/ApprovedColor.spec.ts
@@ -0,0 +1,96 @@
+﻿import {test} from '@umbraco/playwright-testhelpers';
+import {expect} from "@playwright/test";
+
+test.describe('Approved Color tests', () => {
+  let dataTypeDefaultData = null;
+  let dataTypeData = null;
+  const dataTypeName = 'Approved Color';
+  const colorValue = '#ffffff';
+  const colorLabel = 'TestColor';
+
+  test.beforeEach(async ({umbracoUi, umbracoApi}) => {
+    await umbracoUi.goToBackOffice();
+    await umbracoUi.dataType.goToSettingsTreeItem('Data Types');
+    await umbracoUi.dataType.goToDataType(dataTypeName);
+    dataTypeDefaultData = await umbracoApi.dataType.getByName(dataTypeName);
+    dataTypeDefaultData.values = [];
+    await umbracoApi.dataType.update(dataTypeDefaultData.id, dataTypeDefaultData);  
+  });
+
+  test.afterEach(async ({umbracoApi}) => {
+    if (dataTypeDefaultData !== null) {
+      await umbracoApi.dataType.update(dataTypeDefaultData.id, dataTypeDefaultData);   
+    }   
+  });
+
+  test('can include label', async ({umbracoApi, umbracoUi}) => {  
+    // Arrange
+    const expectedDataTypeValues = [
+      {
+        "alias": "useLabel",
+        "value": true
+      }
+    ];
+
+    // Act
+    await umbracoUi.dataType.clickIncludeLabelsSlider();
+    await umbracoUi.dataType.clickSaveButton();
+
+    // Assert
+    await umbracoUi.dataType.isSuccessNotificationVisible();
+    dataTypeData = await umbracoApi.dataType.getByName(dataTypeName);
+    expect(dataTypeData.values).toEqual(expectedDataTypeValues);
+  });
+
+  test('can add color', async ({umbracoApi, umbracoUi}) => {  
+    // Arrange
+    const expectedDataTypeValues = [
+      {
+        "alias": "items",
+        "value": [
+          {
+            "value": colorValue,
+            "label": colorLabel
+          }
+        ]
+      }
+    ];
+
+    // Act
+    await umbracoUi.dataType.addColor(colorValue, colorLabel);
+    await umbracoUi.dataType.clickSaveButton();
+
+    // Assert
+    await umbracoUi.dataType.isSuccessNotificationVisible();
+    dataTypeData = await umbracoApi.dataType.getByName(dataTypeName);
+    expect(dataTypeData.values).toEqual(expectedDataTypeValues);
+  });
+
+  test('can remove color', async ({umbracoApi, umbracoUi}) => {  
+    // Arrange
+    const dataTypeValues = [
+      {
+        "alias": "items",
+        "value": [
+          {
+            "value": colorValue,
+            "label": colorLabel
+          }
+        ]
+      }
+    ];
+    dataTypeData = await umbracoApi.dataType.getByName(dataTypeName);
+    dataTypeData.values = dataTypeValues;
+    await umbracoApi.dataType.update(dataTypeData.id, dataTypeData);  
+    await umbracoUi.reloadPage();
+
+    // Act
+    await umbracoUi.dataType.removeColorByValue(colorValue);
+    await umbracoUi.dataType.clickSaveButton();
+
+    // Assert
+    await umbracoUi.dataType.isSuccessNotificationVisible();
+    dataTypeData = await umbracoApi.dataType.getByName(dataTypeName);
+    expect(dataTypeData.values).toEqual([]);
+  });
+});
---
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/DataType/Label.spec.ts
@@ -0,0 +1,44 @@
+﻿import {test} from '@umbraco/playwright-testhelpers';
+import {expect} from "@playwright/test";
+
+test.describe('Label tests', () => {
+  let dataTypeDefaultData = null;
+  let dataTypeData = null;
+
+
+  test.beforeEach(async ({umbracoUi}) => {
+    await umbracoUi.goToBackOffice();
+    await umbracoUi.dataType.goToSettingsTreeItem('Data Types');
+  });
+
+  test.afterEach(async ({umbracoApi}) => {
+    if (dataTypeDefaultData !== null) {
+      await umbracoApi.dataType.update(dataTypeDefaultData.id, dataTypeDefaultData); 
+    }    
+  });
+
+  const labelTypes = ['Label (bigint)', 'Label (datetime)', 'Label (decimal)', 'Label (integer)', 'Label (string)', 'Label (time)'];
+  for (const labelType of labelTypes) {
+    test(`can change value type in ${labelType}`, async ({umbracoApi, umbracoUi}) => {  
+      // Arrange
+      const expectedDataTypeValues = [
+        {
+          "alias": "umbracoDataValueType",
+          "value": "TEXT"
+        }
+      ];
+      
+      dataTypeDefaultData = await umbracoApi.dataType.getByName(labelType);
+      await umbracoUi.dataType.goToDataType(labelType);
+
+      // Act
+      await umbracoUi.dataType.changeValueType('Long String');
+      await umbracoUi.dataType.clickSaveButton();
+
+      // Assert
+      await umbracoUi.dataType.isSuccessNotificationVisible();
+      dataTypeData = await umbracoApi.dataType.getByName(labelType);
+      expect(dataTypeData.values).toEqual(expectedDataTypeValues);
+    });
+  }
+});
