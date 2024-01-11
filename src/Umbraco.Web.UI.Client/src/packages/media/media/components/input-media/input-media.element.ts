a991ac8d8016165a87aa356aa15a7dadd5941504
leekelleher <leekelleher@gmail.com>
2024-01-11 15:30:37 +0000

    MediaPicker: wired up attributes
    for `filter`, `ignoreUserStartNodes` and `showOpenButton`.
    
    Added method for showing if trashed.

--- src/Umbraco.Web.UI.Client/src/packages/media/media/components/input-media/input-media.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/components/input-media/input-media.element.ts
@@ -60,6 +60,15 @@ export class UmbInputMediaElement extends FormControlMixin(UmbLitElement) {
 		this.#pickerContext.setSelection(ids);
 	}
 
+	@property({ type: String })
+	filter?: string;
+
+	@property({ type: Boolean })
+	showOpenButton?: boolean;
+
+	@property({ type: Boolean })
+	ignoreUserStartNodes?: boolean;
+
 	@property()
 	public set value(idsString: string) {
 		// Its with full purpose we don't call super.value, as thats being handled by the observation of the context selection.
@@ -73,6 +82,10 @@ export class UmbInputMediaElement extends FormControlMixin(UmbLitElement) {
 
 	constructor() {
 		super();
+	}
+
+	connectedCallback() {
+		super.connectedCallback();
 
 		this.addValidator(
 			'rangeUnderflow',
@@ -91,9 +104,16 @@ export class UmbInputMediaElement extends FormControlMixin(UmbLitElement) {
 	}
 
 	protected _openPicker() {
+		// TODO: Configure the media picker, with `filter` and `ignoreUserStartNodes` [LK]
+		console.log('_openPicker', [this.filter, this.ignoreUserStartNodes]);
 		this.#pickerContext.openPicker({
 			hideTreeRoot: true,
 		});
+	}
+
+	protected _openItem(item: MediaItemResponseModel) {
+		// TODO: Implement the Content editing infinity editor. [LK]
+		console.log('TODO: _openItem', item);
 	}
 
 	protected getFormElement() {
@@ -105,6 +125,7 @@ export class UmbInputMediaElement extends FormControlMixin(UmbLitElement) {
 	}
 
 	#renderItems() {
+		if (!this._items) return;
 		// TODO: Add sorting. [LK]
 		return html` ${this._items?.map((item) => this.#renderItem(item))} `;
 	}
@@ -124,12 +145,14 @@ export class UmbInputMediaElement extends FormControlMixin(UmbLitElement) {
 	}
 
 	#renderItem(item: MediaItemResponseModel) {
+		// TODO: `file-ext` value has been hardcoded here. Find out if API model has value for it. [LK]
+		// TODO: How to handle the `showOpenButton` option? [LK]
 		return html`
 			<uui-card-media
 				name=${ifDefined(item.name === null ? undefined : item.name)}
 				detail=${ifDefined(item.id)}
 				file-ext="jpg">
-				<!-- <uui-tag size="s" slot="tag" color="danger">Trashed</uui-tag> -->
+				${this._renderIsTrashed(item)}
 				<uui-action-bar slot="actions">
 					<uui-button label="Copy media">
 						<uui-icon name="icon-documents"></uui-icon>
@@ -140,6 +163,11 @@ export class UmbInputMediaElement extends FormControlMixin(UmbLitElement) {
 				</uui-action-bar>
 			</uui-card-media>
 		`;
+	}
+
+	private _renderIsTrashed(item: MediaItemResponseModel) {
+		if (!item.isTrashed) return;
+		return html`<uui-tag size="s" slot="tag" color="danger">Trashed</uui-tag>`;
 	}
 
 	static styles = [
