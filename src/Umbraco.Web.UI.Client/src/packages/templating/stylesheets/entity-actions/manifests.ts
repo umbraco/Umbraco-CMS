a72f7ade9bd0845806c39b733cc7d3c021f85c27
Mads Rasmussen <madsr@hey.com>
2024-01-11 15:20:36 +0100

    don't rename export

--- src/Umbraco.Web.UI.Client/src/packages/templating/stylesheets/entity-actions/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/templating/stylesheets/entity-actions/manifests.ts
@@ -1,5 +1,5 @@
 import { UMB_STYLESHEET_ENTITY_TYPE } from '../entity.js';
-import { UMB_STYLESHEET_REPOSITORY_ALIAS } from '../repository/index.js';
+import { UMB_STYLESHEET_DETAIL_REPOSITORY_ALIAS } from '../repository/index.js';
 import { manifests as createManifests } from './create/manifests.js';
 import { UmbDeleteEntityAction } from '@umbraco-cms/backoffice/entity-action';
 import { ManifestEntityAction } from '@umbraco-cms/backoffice/extension-registry';
@@ -13,7 +13,7 @@ const stylesheetActions: Array<ManifestEntityAction> = [
 		meta: {
 			icon: 'icon-trash',
 			label: 'Delete',
-			repositoryAlias: UMB_STYLESHEET_REPOSITORY_ALIAS,
+			repositoryAlias: UMB_STYLESHEET_DETAIL_REPOSITORY_ALIAS,
 			entityTypes: [UMB_STYLESHEET_ENTITY_TYPE],
 		},
 	},
--- src/Umbraco.Web.UI.Client/src/packages/templating/stylesheets/repository/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/templating/stylesheets/repository/index.ts
@@ -1,5 +1,5 @@
 export * from './item/index.js';
 
 export * from './stylesheet-detail.repository.js';
-export { UMB_STYLESHEET_DETAIL_REPOSITORY_ALIAS as UMB_STYLESHEET_REPOSITORY_ALIAS } from './manifests.js';
+export { UMB_STYLESHEET_DETAIL_REPOSITORY_ALIAS } from './manifests.js';
 export { type UmbStylesheetDetailStore, UMB_STYLESHEET_DETAIL_STORE_CONTEXT } from './stylesheet-detail.store.js';
