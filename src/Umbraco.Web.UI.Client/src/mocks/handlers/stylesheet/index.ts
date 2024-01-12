6bf17e23a53cbae318d4bd0e704884a556acd867
Mads Rasmussen <madsr@hey.com>
2024-01-12 14:02:03 +0100

    add stylesheet rename request handler

--- src/Umbraco.Web.UI.Client/src/mocks/handlers/stylesheet/index.ts
+++ src/Umbraco.Web.UI.Client/src/mocks/handlers/stylesheet/index.ts
@@ -3,5 +3,13 @@ import { treeHandlers } from './tree.handlers.js';
 import { detailHandlers } from './detail.handlers.js';
 import { itemHandlers } from './item.handlers.js';
 import { overviewHandlers } from './overview.handlers.js';
+import { renameHandlers } from './rename.handlers.js';
 
-export const handlers = [...treeHandlers, ...itemHandlers, ...folderHandlers, ...overviewHandlers, ...detailHandlers];
+export const handlers = [
+	...treeHandlers,
+	...itemHandlers,
+	...folderHandlers,
+	...overviewHandlers,
+	...renameHandlers,
+	...detailHandlers,
+];
