0fad1d0f4671b2ff5fe03a0de9470d18ac7a7db3
Mads Rasmussen <madsr@hey.com>
2024-01-12 21:22:49 +0100

    align static file tree naming + folder structure

--- src/Umbraco.Web.UI.Client/src/packages/static-file/entity.ts
+++ src/Umbraco.Web.UI.Client/src/packages/static-file/entity.ts
@@ -1,2 +1,5 @@
-export const UMB_STATIC_FILE_ROOT_ENTITY_TYPE = 'static-file-root';
 export const UMB_STATIC_FILE_ENTITY_TYPE = 'static-file';
+export const UMB_STATIC_FILE_ROOT_ENTITY_TYPE = 'static-file-root';
+
+export type UmbStaticFileEntityType = typeof UMB_STATIC_FILE_ENTITY_TYPE;
+export type UmbStaticFileRootEntityType = typeof UMB_STATIC_FILE_ROOT_ENTITY_TYPE;
