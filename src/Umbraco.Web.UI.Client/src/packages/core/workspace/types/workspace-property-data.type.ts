f2c29c48cecdd1f5b6b334e27af641007ec3e600
Niels Lyngsø <niels.lyngso@gmail.com>
2024-01-15 11:24:00 +0100

    clean up property Context

--- src/Umbraco.Web.UI.Client/src/packages/core/workspace/types/workspace-property-data.type.ts
+++
@@ -1,9 +0,0 @@
-import { type UmbPropertyEditorConfig } from '../../property-editor/index.js';
-
-export type WorkspacePropertyData<ValueType> = {
-	alias?: string;
-	label?: string;
-	description?: string;
-	value?: ValueType | null;
-	config?: UmbPropertyEditorConfig; // This could potentially then come from hardcoded JS object and not the DataType store.
-};
