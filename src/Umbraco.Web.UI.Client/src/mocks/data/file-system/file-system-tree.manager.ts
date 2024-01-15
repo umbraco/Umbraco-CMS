90a21748ccd03931240827f36361548ad105d26a
Mads Rasmussen <madsr@hey.com>
2024-01-15 16:32:18 +0100

    fix type error

--- src/Umbraco.Web.UI.Client/src/mocks/data/file-system/file-system-tree.manager.ts
+++ src/Umbraco.Web.UI.Client/src/mocks/data/file-system/file-system-tree.manager.ts
@@ -5,21 +5,24 @@ import {
 	PagedFileSystemTreeItemPresentationModel,
 } from '@umbraco-cms/backoffice/backend-api';
 
-export class UmbMockFileSystemTreeManager<T extends Omit<FileSystemTreeItemPresentationModel, 'type' | 'icon'>> {
+export class UmbMockFileSystemTreeManager<T extends Omit<FileSystemTreeItemPresentationModel, 'type'>> {
 	#db: UmbData<T>;
 
 	constructor(mockDb: UmbData<T>) {
 		this.#db = mockDb;
 	}
 
-	getRoot(): PagedFileSystemTreeItemPresentationModel {
+	getRoot(): { items: Array<Omit<FileSystemTreeItemPresentationModel, 'type'>>; total: number } {
 		const items = this.#db.getData().filter((item) => item.parent === null);
 		const treeItems = items.map((item) => createFileSystemTreeItem(item));
 		const total = items.length;
 		return { items: treeItems, total };
 	}
 
-	getChildrenOf(parentPath: string): PagedFileSystemTreeItemPresentationModel {
+	getChildrenOf(parentPath: string): {
+		items: Array<Omit<FileSystemTreeItemPresentationModel, 'type'>>;
+		total: number;
+	} {
 		const items = this.#db.getData().filter((item) => item.parent?.path === parentPath);
 		const treeItems = items.map((item) => createFileSystemTreeItem(item));
 		const total = items.length;
