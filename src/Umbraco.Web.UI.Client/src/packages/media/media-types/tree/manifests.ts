bf99e20dc4272c65d09a2390faa2c5e6d25a7004
Mads Rasmussen <madsr@hey.com>
2024-01-15 18:51:31 +0100

    use unique tree item kind

--- src/Umbraco.Web.UI.Client/src/packages/media/media-types/tree/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media-types/tree/manifests.ts
@@ -38,7 +38,7 @@ const tree: ManifestTree = {
 
 const treeItem: ManifestTreeItem = {
 	type: 'treeItem',
-	kind: 'entity',
+	kind: 'unique',
 	alias: 'Umb.TreeItem.MediaType',
 	name: 'Media Type Tree Item',
 	meta: {
