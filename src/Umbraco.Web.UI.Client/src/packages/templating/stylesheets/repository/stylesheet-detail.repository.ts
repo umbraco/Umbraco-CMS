636553131a5dff9d22e885afd18a17c20c586568
Mads Rasmussen <madsr@hey.com>
2024-01-16 15:36:41 +0100

    use correct type

--- src/Umbraco.Web.UI.Client/src/packages/templating/stylesheets/repository/stylesheet-detail.repository.ts
+++ src/Umbraco.Web.UI.Client/src/packages/templating/stylesheets/repository/stylesheet-detail.repository.ts
@@ -1,11 +1,11 @@
 import { UmbStylesheetDetailModel } from '../types.js';
 import { UmbStylesheetDetailServerDataSource } from './stylesheet-detail.server.data-source.js';
 import { UMB_STYLESHEET_DETAIL_STORE_CONTEXT } from './stylesheet-detail.store.js';
-import { type UmbControllerHostElement } from '@umbraco-cms/backoffice/controller-api';
+import { type UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import { UmbDetailRepositoryBase } from '@umbraco-cms/backoffice/repository';
 
 export class UmbStylesheetDetailRepository extends UmbDetailRepositoryBase<UmbStylesheetDetailModel> {
-	constructor(host: UmbControllerHostElement) {
+	constructor(host: UmbControllerHost) {
 		super(host, UmbStylesheetDetailServerDataSource, UMB_STYLESHEET_DETAIL_STORE_CONTEXT);
 	}
 }
