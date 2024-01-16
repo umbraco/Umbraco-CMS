1a94b0d9adbd11f826116211e567ef50fe92c5af
Mads Rasmussen <madsr@hey.com>
2024-01-16 16:26:39 +0100

    sync document type tree

--- src/Umbraco.Web.UI.Client/src/packages/documents/document-types/tree/document-type.tree.store.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/document-types/tree/document-type.tree.store.ts
@@ -1,6 +1,10 @@
+import { UmbDocumentTypeDetailModel } from '../types.js';
+import { UMB_DOCUMENT_TYPE_DETAIL_STORE_CONTEXT } from '../repository/index.js';
+import { UmbDocumentTypeTreeItemModel } from './types.js';
 import { UmbContextToken } from '@umbraco-cms/backoffice/context-api';
 import { UmbControllerHostElement } from '@umbraco-cms/backoffice/controller-api';
 import { UmbUniqueTreeStore } from '@umbraco-cms/backoffice/tree';
+import { UmbStoreConnector } from '@umbraco-cms/backoffice/store';
 
 /**
  * @export
@@ -16,7 +20,38 @@ export class UmbDocumentTypeTreeStore extends UmbUniqueTreeStore {
 	 */
 	constructor(host: UmbControllerHostElement) {
 		super(host, UMB_DOCUMENT_TYPE_TREE_STORE_CONTEXT.toString());
+
+		new UmbStoreConnector<UmbDocumentTypeTreeItemModel, UmbDocumentTypeDetailModel>(
+			host,
+			this,
+			UMB_DOCUMENT_TYPE_DETAIL_STORE_CONTEXT,
+			(item) => this.#createTreeItemMapper(item),
+			(item) => this.#updateTreeItemMapper(item),
+		);
 	}
+
+	// TODO: revisit this when we have decided on detail model sizes
+	#createTreeItemMapper = (item: UmbDocumentTypeDetailModel) => {
+		const treeItem: UmbDocumentTypeTreeItemModel = {
+			unique: item.unique,
+			parentUnique: item.parentUnique,
+			name: item.name,
+			entityType: item.entityType,
+			isElement: item.isElement,
+			isFolder: false,
+			isContainer: false,
+			hasChildren: false,
+		};
+
+		return treeItem;
+	};
+
+	// TODO: revisit this when we have decided on detail model sizes
+	#updateTreeItemMapper = (item: UmbDocumentTypeDetailModel) => {
+		return {
+			name: item.name,
+		};
+	};
 }
 
 export const UMB_DOCUMENT_TYPE_TREE_STORE_CONTEXT = new UmbContextToken<UmbDocumentTypeTreeStore>(
