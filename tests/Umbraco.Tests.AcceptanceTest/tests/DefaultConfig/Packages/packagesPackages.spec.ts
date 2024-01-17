e698eaedf406ed21684f65e368e34322a59e99d8
Andreas Zerbst <andr317c@live.dk>
2024-01-17 19:27:25 +0100

    Added test for checking if we are able to see the marketplace

---
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Packages/packagesPackages.spec.ts
@@ -0,0 +1,18 @@
+import {ConstantHelper, test} from "@umbraco/playwright-testhelpers";
+import {expect} from "@playwright/test";
+
+test.describe('Packages tests', () => {
+
+  // The MarketPlace is a iFrame we are using from the DXP team, so it is not something we should test. This test is just checking if we have the IFrame
+  test('can see the marketplace', async ({umbracoUi}) => {
+    // Arrange
+    await umbracoUi.goToBackOffice();
+    await umbracoUi.package.goToSection(ConstantHelper.sections.packages);
+
+    // Act
+    await umbracoUi.package.clickPackagesTab();
+
+    // Assert
+    expect(await umbracoUi.package.doesMarketPlaceIFrameExist()).toBeDefined();
+  });
+});
