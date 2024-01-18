57f4e9a08752fcfce7b8c7d9d9bb7aba03f520ef
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2024-01-18 12:24:29 +0100

    Implement document configuration endpoint

---
+++ src/Umbraco.Cms.Api.Management/ViewModels/Document/DocumentConfigurationResponseModel.cs
@@ -0,0 +1,12 @@
+﻿namespace Umbraco.Cms.Api.Management.ViewModels.Document;
+
+public class DocumentConfigurationResponseModel
+{
+    public bool SanitizeTinyMce { get; set; }
+
+    public bool DisableDeleteWhenReferenced { get; set; }
+
+    public bool DisableUnpublishWhenReferenced { get; set; }
+
+    public bool AllowEditInvariantFromNonDefault { get; set; }
+}
