29f3ba02c906e4a853e96f84f75a770e963e8e3d
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-01-18 10:11:35 +0100

    fix storybook build

--- src/Umbraco.Web.UI.Client/src/packages/block/block-type/property-editors/block-type-group-configuration/property-editor-ui-block-type-group-configuration.stories.ts
+++ src/Umbraco.Web.UI.Client/src/packages/block/block-type/property-editors/block-type-group-configuration/property-editor-ui-block-type-group-configuration.stories.ts
@@ -1,15 +1,19 @@
-import { Meta, Story } from '@storybook/web-components';
+import { Meta, StoryObj } from '@storybook/web-components';
 import type { UmbPropertyEditorUIBlockGridGroupConfigurationElement } from './property-editor-ui-block-type-group-configuration.element.js';
 import { html } from '@umbraco-cms/backoffice/external/lit';
 
-import './property-editor-ui-block-grid-group-configuration.element.js';
+import './property-editor-ui-block-type-group-configuration.element.js';
 
-export default {
+const meta: Meta<UmbPropertyEditorUIBlockGridGroupConfigurationElement> = {
 	title: 'Property Editor UIs/Block Grid Group Configuration',
 	component: 'umb-property-editor-ui-block-type-group-configuration',
 	id: 'umb-property-editor-ui-block-type-group-configuration',
-} as Meta;
+};
 
-export const AAAOverview: Story<UmbPropertyEditorUIBlockGridGroupConfigurationElement> = () =>
-	html`<umb-property-editor-ui-block-type-group-configuration></umb-property-editor-ui-block-type-group-configuration>`;
-AAAOverview.storyName = 'Overview';
+export default meta;
+type Story = StoryObj<typeof UmbPropertyEditorUIBlockGridGroupConfigurationElement>;
+
+export const Overview: Story = {
+	render: () =>
+		html`<umb-property-editor-ui-block-type-group-configuration></umb-property-editor-ui-block-type-group-configuration>`,
+};
