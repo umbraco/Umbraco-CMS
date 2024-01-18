e2e36a04b5b9e874d42ed44116f1dccdcef9e540
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2024-01-18 11:42:45 +0100

    Implement template endpoint

---
+++ src/Umbraco.Cms.Api.Management/ViewModels/DataType/DatatypeConfigurationResponseModel.cs
@@ -0,0 +1,8 @@
+﻿using Umbraco.Cms.Core.Configuration.Models;
+
+namespace Umbraco.Cms.Api.Management.ViewModels.DataType;
+
+public class DatatypeConfigurationResponseModel
+{
+    public DataTypeChangeMode CanBeChanged { get; set; }
+}
