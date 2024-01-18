2bdc620b59f34a2dc75356580a98e24adfac495d
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2024-01-18 12:56:50 +0100

    implement document type configuratio endpoint

---
+++ src/Umbraco.Cms.Api.Management/ViewModels/DocumentType/DocumentTypeConfigurationResponseModel.cs
@@ -0,0 +1,10 @@
+﻿using Umbraco.Cms.Core.Configuration.Models;
+
+namespace Umbraco.Cms.Api.Management.ViewModels.DocumentType;
+
+public class DocumentTypeConfigurationResponseModel
+{
+    public DataTypeChangeMode DataTypesCanBeChanged { get; set; }
+
+    public bool DisableTemplates { get; set; }
+}
