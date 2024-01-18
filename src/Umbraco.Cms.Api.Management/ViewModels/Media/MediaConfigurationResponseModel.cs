2afa2fe97768f796b001e31b5974c75c5b1004b8
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2024-01-18 12:16:09 +0100

    Add media configuration endpoint

---
+++ src/Umbraco.Cms.Api.Management/ViewModels/Media/MediaConfigurationResponseModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Umbraco.Cms.Api.Management.ViewModels.Media;
+
+public class MediaConfigurationResponseModel
+{
+    public bool DisableDeleteWhenReferenced { get; set; }
+
+    public bool DisableUnpublishWhenReferenced { get; set; }
+
+    public bool SanitizeTinyMce { get; set; }
+}
