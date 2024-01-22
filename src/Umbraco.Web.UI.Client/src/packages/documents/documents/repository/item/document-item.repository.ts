4ad005fb5f7440448396e7ac35dbeb26d7128987
Mads Rasmussen <madsr@hey.com>
2024-01-22 15:09:43 +0100

    use client side model for document item repo

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/repository/item/document-item.repository.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/repository/item/document-item.repository.ts
@@ -1,10 +1,10 @@
 import { UmbDocumentItemServerDataSource } from './document-item.server.data.js';
 import { UMB_DOCUMENT_ITEM_STORE_CONTEXT } from './document-item.store.js';
+import { UmbDocumentItemModel } from './types.js';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import { UmbItemRepositoryBase } from '@umbraco-cms/backoffice/repository';
-import { DocumentItemResponseModel } from '@umbraco-cms/backoffice/backend-api';
 
-export class UmbDocumentItemRepository extends UmbItemRepositoryBase<DocumentItemResponseModel> {
+export class UmbDocumentItemRepository extends UmbItemRepositoryBase<UmbDocumentItemModel> {
 	constructor(host: UmbControllerHost) {
 		super(host, UmbDocumentItemServerDataSource, UMB_DOCUMENT_ITEM_STORE_CONTEXT);
 	}
