f8dea988a519d287ed827e93e3a33eebf03ca535
Andreas Zerbst <andr317c@live.dk>
2024-01-22 08:01:21 +0100

    Fixed Acceptance Test to match patterns

--- tests/Umbraco.Tests.AcceptanceTest/tests/ApiTesting/Package/Package.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/ApiTesting/Package/Package.spec.ts
@@ -2,18 +2,19 @@ import {test} from '@umbraco/playwright-testhelpers';
 import {expect} from "@playwright/test";
 
 test.describe('Package tests', () => {
-  let packageId = "";
+  let packageId = '';
   const packageName = 'TestName';
 
-  test.beforeEach(async ({page, umbracoApi}) => {
+  test.beforeEach(async ({umbracoApi}) => {
     await umbracoApi.package.ensureNameNotExists(packageName);
   });
 
-  test.afterEach(async ({page, umbracoApi}) => {
+  test.afterEach(async ({umbracoApi}) => {
     await umbracoApi.package.delete(packageId);
   });
 
-  test('can create a package', async ({page, umbracoApi, umbracoUi}) => {
+  test('can create a package', async ({umbracoApi}) => {
+    // Act
     packageId = await umbracoApi.package.create(packageName);
 
     // Assert
@@ -21,7 +22,7 @@ test.describe('Package tests', () => {
   });
 
 
-  test('can update a package', async ({page, umbracoApi, umbracoUi}) => {
+  test('can update a package', async ({ umbracoApi}) => {
     // Arrange
     const wrongPackageName = 'WrongName';
     await umbracoApi.package.ensureNameNotExists(wrongPackageName);
@@ -38,11 +39,14 @@ test.describe('Package tests', () => {
   });
 
 
-  test('can delete a package', async ({page, umbracoApi, umbracoUi}) => {
+  test('can delete a package', async ({umbracoApi}) => {
+    // Arrange
     packageId = await umbracoApi.package.create(packageName);
 
+    await umbracoApi.package.delete(packageId);
+
     // Assert
-    expect(await umbracoApi.package.doesExist(packageId)).toBeTruthy();
+    expect(await umbracoApi.package.doesExist(packageId)).toBeFalsy();
   });
 
 });
