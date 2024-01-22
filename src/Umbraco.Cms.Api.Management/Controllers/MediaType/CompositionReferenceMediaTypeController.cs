3059c0f8b6ece1752628a6688977f3dce8e9e09c
Elitsa <elm@umbraco.dk>
2024-01-22 08:39:21 +0100

    Fix namespace in media type related classes

--- src/Umbraco.Cms.Api.Management/Controllers/MediaType/CompositionReferenceMediaTypeController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/MediaType/CompositionReferenceMediaTypeController.cs
@@ -1,21 +1,21 @@
 using Asp.Versioning;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Umbraco.Cms.Api.Management.ViewModels.MediaType.Composition;
+using Umbraco.Cms.Api.Management.ViewModels.MediaType;
 using Umbraco.Cms.Core.Mapping;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Core.Services.OperationStatus;
 
-namespace Umbraco.Cms.Api.Management.Controllers.MediaType.Composition;
+namespace Umbraco.Cms.Api.Management.Controllers.MediaType;
 
 [ApiVersion("1.0")]
-public class ReferencesCompositionController : MediaTypeControllerBase
+public class CompositionReferenceMediaTypeController : MediaTypeControllerBase
 {
     private readonly IMediaTypeService _mediaTypeService;
     private readonly IUmbracoMapper _umbracoMapper;
 
-    public ReferencesCompositionController(IMediaTypeService mediaTypeService, IUmbracoMapper umbracoMapper)
+    public CompositionReferenceMediaTypeController(IMediaTypeService mediaTypeService, IUmbracoMapper umbracoMapper)
     {
         _mediaTypeService = mediaTypeService;
         _umbracoMapper = umbracoMapper;
--- src/Umbraco.Cms.Api.Management/ViewModels/MediaType/MediaTypeCompositionRequestModel.cs
+++ src/Umbraco.Cms.Api.Management/ViewModels/MediaType/MediaTypeCompositionRequestModel.cs
@@ -1,6 +1,6 @@
 using Umbraco.Cms.Api.Management.ViewModels.ContentType;
 
-namespace Umbraco.Cms.Api.Management.ViewModels.MediaType.Composition;
+namespace Umbraco.Cms.Api.Management.ViewModels.MediaType;
 
 public class MediaTypeCompositionRequestModel : ContentTypeCompositionRequestModelBase
 {
