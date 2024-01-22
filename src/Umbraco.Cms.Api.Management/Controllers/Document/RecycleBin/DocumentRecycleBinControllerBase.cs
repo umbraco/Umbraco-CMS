d088c83c93cf25dafa76f1052d248a51e640c930
Sven Geusens <sge@umbraco.dk>
2024-01-22 11:08:53 +0100

    Refactor OperationStatusResult mapping trough inheritance

--- src/Umbraco.Cms.Api.Management/Controllers/Document/RecycleBin/DocumentRecycleBinControllerBase.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/Document/RecycleBin/DocumentRecycleBinControllerBase.cs
@@ -42,30 +42,4 @@ public class DocumentRecycleBinControllerBase : RecycleBinControllerBase<Recycle
 
         return responseModel;
     }
-
-    protected IActionResult ContentEditingOperationStatusResult(ContentEditingOperationStatus status) =>
-        status switch
-        {
-            ContentEditingOperationStatus.CancelledByNotification => BadRequest(new ProblemDetailsBuilder()
-                .WithTitle("Cancelled by notification")
-                .WithDetail("A notification handler prevented the content operation.")
-                .Build()),
-            ContentEditingOperationStatus.NotFound => NotFound(new ProblemDetailsBuilder()
-                .WithTitle("The content could not be found")
-                .Build()),
-            ContentEditingOperationStatus.NotAllowed => BadRequest(new ProblemDetailsBuilder()
-                .WithTitle("Operation not permitted")
-                .WithDetail("The attempted operation was not permitted, likely due to a permission/configuration mismatch with the operation.")
-                .Build()),
-            ContentEditingOperationStatus.NotInTrash => BadRequest(new ProblemDetailsBuilder()
-                .WithTitle("Content is not in the recycle bin")
-                .WithDetail("The attempted operation requires the targeted content to be in the recycle bin.")
-                .Build()),
-            ContentEditingOperationStatus.Unknown => StatusCode(StatusCodes.Status500InternalServerError, new ProblemDetailsBuilder()
-                .WithTitle("Unknown error. Please see the log for more details.")
-                .Build()),
-            _ => StatusCode(StatusCodes.Status500InternalServerError, new ProblemDetailsBuilder()
-                .WithTitle("Unknown content operation status.")
-                .Build()),
-        };
 }
