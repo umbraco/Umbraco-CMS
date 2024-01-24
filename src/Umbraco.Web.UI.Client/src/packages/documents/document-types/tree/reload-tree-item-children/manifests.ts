d117e130c0875277e9518d7116d92aae3c507702
Mads Rasmussen <madsr@hey.com>
2024-01-24 20:50:33 +0100

    allow reload children for document type folders

--- src/Umbraco.Web.UI.Client/src/packages/documents/document-types/tree/reload-tree-item-children/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/document-types/tree/reload-tree-item-children/manifests.ts
@@ -1,4 +1,8 @@
-import { UMB_DOCUMENT_TYPE_ROOT_ENTITY_TYPE, UMB_DOCUMENT_TYPE_ENTITY_TYPE } from '../../entity.js';
+import {
+	UMB_DOCUMENT_TYPE_ROOT_ENTITY_TYPE,
+	UMB_DOCUMENT_TYPE_ENTITY_TYPE,
+	UMB_DOCUMENT_TYPE_FOLDER_ENTITY_TYPE,
+} from '../../entity.js';
 import { UMB_DOCUMENT_TYPE_DETAIL_REPOSITORY_ALIAS } from '../../repository/detail/manifests.js';
 import { UmbReloadTreeItemChildrenEntityAction } from '@umbraco-cms/backoffice/tree';
 import { type ManifestEntityAction } from '@umbraco-cms/backoffice/extension-registry';
@@ -14,7 +18,11 @@ export const manifests: Array<ManifestEntityAction> = [
 			icon: 'icon-refresh',
 			label: 'Reload children...',
 			repositoryAlias: UMB_DOCUMENT_TYPE_DETAIL_REPOSITORY_ALIAS,
-			entityTypes: [UMB_DOCUMENT_TYPE_ROOT_ENTITY_TYPE, UMB_DOCUMENT_TYPE_ENTITY_TYPE],
+			entityTypes: [
+				UMB_DOCUMENT_TYPE_ROOT_ENTITY_TYPE,
+				UMB_DOCUMENT_TYPE_ENTITY_TYPE,
+				UMB_DOCUMENT_TYPE_FOLDER_ENTITY_TYPE,
+			],
 		},
 	},
 ];
