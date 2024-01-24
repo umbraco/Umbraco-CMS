d3e086ca700fc5b308d05b0583101095d421812c
Niels Lyngsø <niels.lyngso@gmail.com>
2024-01-24 19:28:51 +0100

    append type to import

--- src/Umbraco.Web.UI.Client/src/packages/core/collection/components/collection-view-bundle.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/collection/components/collection-view-bundle.element.ts
@@ -1,5 +1,5 @@
 import { UMB_DEFAULT_COLLECTION_CONTEXT, UmbDefaultCollectionContext } from '../default/collection-default.context.js';
-import { ManifestCollectionView } from '../../extension-registry/models/collection-view.model.js';
+import type { ManifestCollectionView } from '../../extension-registry/models/collection-view.model.js';
 import { css, html, customElement, state, nothing } from '@umbraco-cms/backoffice/external/lit';
 import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
 import { UmbLitElement } from '@umbraco-cms/internal/lit-element';
