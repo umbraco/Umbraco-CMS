4bf35a91ba39ec3108d526f07ec8e84263bcf896
Mads Rasmussen <madsr@hey.com>
2024-01-24 13:29:44 +0100

    add reload entity action that dispatches an event on the action event context

---
+++ src/Umbraco.Web.UI.Client/src/packages/core/data-type/tree/reload-tree-item/reload-tree-item-children.action.ts
@@ -0,0 +1,27 @@
+import { type UmbCopyDataTypeRepository } from '../../repository/copy/data-type-copy.repository.js';
+import { UmbEntityActionBase } from '@umbraco-cms/backoffice/entity-action';
+import { UmbControllerHostElement } from '@umbraco-cms/backoffice/controller-api';
+import { UMB_ACTION_EVENT_CONTEXT, UmbActionEventContext } from '@umbraco-cms/backoffice/action';
+import { UmbReloadTreeItemRequestEvent } from '@umbraco-cms/backoffice/tree';
+
+export class UmbReloadTreeItemChildrenEntityAction extends UmbEntityActionBase<UmbCopyDataTypeRepository> {
+	#actionEventContext?: UmbActionEventContext;
+
+	constructor(host: UmbControllerHostElement, repositoryAlias: string, unique: string, entityType: string) {
+		super(host, repositoryAlias, unique, entityType);
+
+		this.consumeContext(UMB_ACTION_EVENT_CONTEXT, (instance) => {
+			this.#actionEventContext = instance;
+		});
+	}
+
+	async execute() {
+		if (!this.#actionEventContext) throw new Error('Action Event context is not available');
+		this.#actionEventContext.dispatchEvent(
+			new UmbReloadTreeItemRequestEvent({
+				unique: this.unique,
+				entityType: this.entityType,
+			}),
+		);
+	}
+}
