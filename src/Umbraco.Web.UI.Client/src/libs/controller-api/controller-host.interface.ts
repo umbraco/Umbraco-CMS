8b79460299885ef86b469445d251df6a45552261
Niels Lyngsø <niels.lyngso@gmail.com>
2024-01-24 20:22:58 +0100

    interface

--- src/Umbraco.Web.UI.Client/src/libs/controller-api/controller-host.interface.ts
+++ src/Umbraco.Web.UI.Client/src/libs/controller-api/controller-host.interface.ts
@@ -1,6 +1,6 @@
 import type { UmbController } from './controller.interface.js';
 
-export declare class UmbControllerHost {
+export interface UmbControllerHost {
 	hasController(controller: UmbController): boolean;
 	getControllers(filterMethod: (ctrl: UmbController) => boolean): UmbController[];
 	addController(controller: UmbController): void;
