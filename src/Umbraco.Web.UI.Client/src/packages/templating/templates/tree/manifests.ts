0d13010474e28570b60d0de8b914c10dd54485a6
Mads Rasmussen <madsr@hey.com>
2024-01-25 10:55:41 +0100

    add reload for templating trees

--- src/Umbraco.Web.UI.Client/src/packages/templating/templates/tree/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/templating/templates/tree/manifests.ts
@@ -1,6 +1,7 @@
 import { UMB_TEMPLATE_ENTITY_TYPE, UMB_TEMPLATE_ROOT_ENTITY_TYPE } from '../entity.js';
 import { UmbTemplateTreeRepository } from './template-tree.repository.js';
 import { UmbTemplateTreeStore } from './template-tree.store.js';
+import { manifests as reloadTreeItemChildrenManifest } from './reload-tree-item-children/manifests.js';
 import type {
 	ManifestRepository,
 	ManifestTree,
@@ -45,4 +46,4 @@ const treeItem: ManifestTreeItem = {
 	},
 };
 
-export const manifests = [treeRepository, treeStore, tree, treeItem];
+export const manifests = [treeRepository, treeStore, tree, treeItem, ...reloadTreeItemChildrenManifest];
