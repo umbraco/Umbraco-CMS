865ebc47835bd0564cc6ba71e7ece4fa0df3c2b0
Mads Rasmussen <madsr@hey.com>
2024-01-29 12:47:40 +0100

    temp add name

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/repository/item/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/repository/item/types.ts
@@ -1,6 +1,7 @@
 import type { ContentStateModel } from '@umbraco-cms/backoffice/backend-api';
 
 export interface UmbDocumentItemModel {
+	name: string; // TODO: this is not correct. We need to get it from the variants. This is a temp solution.
 	unique: string;
 	isTrashed: boolean;
 	isProtected: boolean;
