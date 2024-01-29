4a18b172e4e1d4ae3a1505667ed3f68199559dc3
Mads Rasmussen <madsr@hey.com>
2024-01-29 11:44:27 +0100

    add client models

--- src/Umbraco.Web.UI.Client/src/packages/media/media/tree/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/tree/types.ts
@@ -1,8 +1,26 @@
-import type { UmbEntityTreeItemModel, UmbEntityTreeRootModel } from '@umbraco-cms/backoffice/tree';
+import type { UmbMediaEntityType, UmbMediaRootEntityType } from '../entity.js';
+import type { ContentStateModel } from '@umbraco-cms/backoffice/backend-api';
+import type { UmbUniqueTreeItemModel, UmbUniqueTreeRootModel } from '@umbraco-cms/backoffice/tree';
 
-export interface UmbMediaTreeItemModel extends Omit<UmbEntityTreeItemModel, 'name'> {
+export interface UmbMediaTreeItemModel extends UmbUniqueTreeItemModel {
+	entityType: UmbMediaEntityType;
 	noAccess: boolean;
 	isTrashed: boolean;
+	isProtected: boolean;
+	mediaType: {
+		id: string;
+		icon: string;
+		hasListView: boolean;
+	};
+	variants: Array<UmbMediaTreeItemVariantModel>;
 }
 
-export interface UmbMediaTreeRootModel extends UmbEntityTreeRootModel {}
+export interface UmbMediaTreeRootModel extends UmbUniqueTreeRootModel {
+	entityType: UmbMediaRootEntityType;
+}
+
+export interface UmbMediaTreeItemVariantModel {
+	name: string;
+	culture: string | null;
+	state: ContentStateModel; // TODO: make our own enum for this. We might have states for "unsaved changes" etc.
+}
