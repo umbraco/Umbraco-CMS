51e7c3dc5572475a214f76e1758870b2ae467e6d
Zeegaan <skrivdetud@gmail.com>
2024-01-30 14:09:42 +0100

    Add controller

---
+++ src/Umbraco.Cms.Api.Management/Controllers/MemberGroup/CreateMemberGroupController.cs
@@ -0,0 +1,40 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Management.ViewModels.MemberGroup;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Mapping;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Security;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Services.OperationStatus;
+
+namespace Umbraco.Cms.Api.Management.Controllers.MemberGroup;
+
+[ApiVersion("1.0")]
+public class CreateMemberGroupController : MemberGroupControllerBase
+{
+    private readonly IMemberGroupService _memberGroupService;
+    private readonly IUmbracoMapper _mapper;
+    private readonly IBackOfficeSecurityAccessor _backOfficeSecurityAccessor;
+
+    public CreateMemberGroupController(IMemberGroupService memberGroupService, IUmbracoMapper mapper, IBackOfficeSecurityAccessor backOfficeSecurityAccessor)
+    {
+        _memberGroupService = memberGroupService;
+        _mapper = mapper;
+        _backOfficeSecurityAccessor = backOfficeSecurityAccessor;
+    }
+
+    [HttpPost]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(typeof(CreateMemberGroupResponseModel), StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
+    public async Task<IActionResult> Create(CreateMemberGroupRequestModel model)
+    {
+        IMemberGroup? memberGroup = _mapper.Map<IMemberGroup>(model);
+        Attempt<IMemberGroup, MemberGroupOperationStatus> result = await _memberGroupService.CreateAsync(memberGroup!, CurrentUserKey(_backOfficeSecurityAccessor));
+        return result.Success
+            ? Ok(_mapper.Map<CreateMemberGroupResponseModel>(result.Result))
+            : MemberGroupOperationStatusResult(result.Status);
+    }
+}
---
+++ src/Umbraco.Cms.Api.Management/Controllers/MemberGroup/MemberGroupControllerBase.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Common.Builders;
+using Umbraco.Cms.Api.Management.Routing;
+using Umbraco.Cms.Core.Services.OperationStatus;
+using Umbraco.Cms.Web.Common.Authorization;
+
+namespace Umbraco.Cms.Api.Management.Controllers.MemberGroup;
+
+[VersionedApiBackOfficeRoute("membergroup")]
+[Authorize(Policy = "New" + AuthorizationPolicies.SectionAccessMembers)]
+public class MemberGroupControllerBase : ManagementApiControllerBase
+{
+    protected IActionResult MemberGroupOperationStatusResult(MemberGroupOperationStatus status) =>
+        status switch
+        {
+            MemberGroupOperationStatus.Success => Ok(),
+            MemberGroupOperationStatus.DuplicateName => NotFound(new ProblemDetailsBuilder()
+                .WithTitle("Duplicate name")
+                .WithDetail("Another group with the same name already exists.")
+                .Build()),
+            _ => StatusCode(StatusCodes.Status500InternalServerError, new ProblemDetailsBuilder()
+                .WithTitle("Unknown member group operation status.")
+                .Build()),
+        };
+}
