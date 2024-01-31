a2f4462c3d13f75baf391212c4fc1a3d216d8e4d
Nikolaj <nikolajlauridsen@protonmail.ch>
2024-01-31 12:48:52 +0100

    Create system text serializer

---
+++ src/Umbraco.PublishedCache.NuCache/DataSource/SystemTextJsonNestedDataSerializer.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.Json;
+using Umbraco.Cms.Core.Models;
+
+namespace Umbraco.Cms.Infrastructure.PublishedCache.DataSource;
+
+public class SystemTextJsonNestedDataSerializer : IContentCacheDataSerializer
+{
+    /// <inheritdoc />
+    public ContentCacheDataModel? Deserialize(
+        IReadOnlyContentBase content,
+        string? stringData,
+        byte[]? byteData,
+        bool published)
+    {
+        if (stringData == null && byteData != null)
+        {
+            throw new NotSupportedException(
+                $"{typeof(JsonContentNestedDataSerializer)} does not support byte[] serialization");
+        }
+
+        return JsonSerializer.Deserialize<ContentCacheDataModel>(stringData!);
+    }
+
+    /// <inheritdoc />
+    public ContentCacheDataSerializationResult Serialize(
+        IReadOnlyContentBase content,
+        ContentCacheDataModel model,
+        bool published)
+    {
+        var json = JsonSerializer.Serialize(model);
+        return new ContentCacheDataSerializationResult(json, null);
+    }
+}
