d2fafbbb615c9e9844f71390359ac144891a6e38
Mads Rasmussen <madsr@hey.com>
2024-02-01 08:38:45 +0100

    update package.json + test runner config

--- src/Umbraco.Web.UI.Client/web-test-runner.config.mjs
+++ src/Umbraco.Web.UI.Client/web-test-runner.config.mjs
@@ -106,8 +106,9 @@ export default {
 						'@umbraco-cms/backoffice/member-type': './src/packages/members/member-types/index.ts',
 						'@umbraco-cms/backoffice/package': './src/packages/packages/package/index.ts',
 
+						'@umbraco-cms/backoffice/language': './src/packages/language/index.ts',
+
 						'@umbraco-cms/backoffice/data-type': './src/packages/core/data-type/index.ts',
-						'@umbraco-cms/backoffice/language': './src/packages/settings/languages/index.ts',
 						'@umbraco-cms/backoffice/logviewer': './src/packages/settings/logviewer/index.ts',
 						'@umbraco-cms/backoffice/relation-type': './src/packages/relations/relation-types/index.ts',
 						'@umbraco-cms/backoffice/relation': './src/packages/relations/relations/index.ts',
