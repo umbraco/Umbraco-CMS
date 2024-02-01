96f4c8515a65511029096b08075aaf0d69db68d5
Mads Rasmussen <madsr@hey.com>
2024-02-01 12:45:21 +0100

    move user group collection handlers into details

--- src/Umbraco.Web.UI.Client/src/mocks/handlers/user-group/index.ts
+++ src/Umbraco.Web.UI.Client/src/mocks/handlers/user-group/index.ts
@@ -1,5 +1,4 @@
-import { collectionHandlers } from './collection.handlers.js';
 import { detailHandlers } from './detail.handlers.js';
 import { itemHandlers } from './item.handlers.js';
 
-export const handlers = [...itemHandlers, ...collectionHandlers, ...detailHandlers];
+export const handlers = [...itemHandlers, ...detailHandlers];
