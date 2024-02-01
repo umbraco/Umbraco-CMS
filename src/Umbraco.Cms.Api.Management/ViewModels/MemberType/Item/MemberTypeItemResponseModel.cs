a5621985209dcb9e424b621bbeccdd64916ab0ca
Elitsa Marinovska <21998037+elit0451@users.noreply.github.com>
2024-02-01 09:23:10 +0100

    Rename items to item to be consistent (#15660)

--- src/Umbraco.Cms.Api.Management/ViewModels/MemberType/Item/MemberTypeItemResponseModel.cs
+++ src/Umbraco.Cms.Api.Management/ViewModels/MemberType/Item/MemberTypeItemResponseModel.cs
@@ -1,6 +1,6 @@
 ﻿using Umbraco.Cms.Api.Management.ViewModels.Item;
 
-namespace Umbraco.Cms.Api.Management.ViewModels.MemberType.Items;
+namespace Umbraco.Cms.Api.Management.ViewModels.MemberType.Item;
 
 public class MemberTypeItemResponseModel : NamedItemResponseModelBase
 {
