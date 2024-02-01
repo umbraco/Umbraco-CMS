1366eb11bf489ca88d1bfd68c6bda432f5174cd8
Mads Rasmussen <madsr@hey.com>
2024-02-01 17:04:36 +0100

    remove double layout

--- src/Umbraco.Web.UI.Client/src/packages/language/workspace/language-root/language-root-workspace.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/language/workspace/language-root/language-root-workspace.element.ts
@@ -1,10 +1,13 @@
+import { UMB_LANGUAGE_COLLECTION_ALIAS } from '../../collection/index.js';
 import { html, customElement } from '@umbraco-cms/backoffice/external/lit';
 import { UmbLitElement } from '@umbraco-cms/internal/lit-element';
 
 @customElement('umb-language-root-workspace')
 export class UmbLanguageRootWorkspaceElement extends UmbLitElement {
 	render() {
-		return html`<umb-collection alias="Umb.Collection.Language"></umb-collection>`;
+		return html` <umb-body-layout main-no-padding headline="Languages">
+			<umb-collection alias=${UMB_LANGUAGE_COLLECTION_ALIAS}></umb-collection>;
+		</umb-body-layout>`;
 	}
 }
 
