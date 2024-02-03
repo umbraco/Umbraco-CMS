825f7e579404db944b55ee623e9b66c3a446e7ae
Mads Rasmussen <madsr@hey.com>
2024-02-03 20:54:46 +0100

    sync store

--- src/Umbraco.Web.UI.Client/src/packages/dictionary/tree/dictionary-tree.store.ts
+++ src/Umbraco.Web.UI.Client/src/packages/dictionary/tree/dictionary-tree.store.ts
@@ -1,5 +1,9 @@
+import type { UmbDictionaryDetailModel } from '../types.js';
+import { UMB_DICTIONARY_DETAIL_STORE_CONTEXT } from '../repository/detail/dictionary-detail.store.js';
+import type { UmbDictionaryTreeItemModel } from './types.js';
 import { UmbContextToken } from '@umbraco-cms/backoffice/context-api';
 import type { UmbControllerHostElement } from '@umbraco-cms/backoffice/controller-api';
+import { UmbStoreConnector } from '@umbraco-cms/backoffice/store';
 import { UmbUniqueTreeStore } from '@umbraco-cms/backoffice/tree';
 
 /**
@@ -16,7 +20,36 @@ export class UmbDictionaryTreeStore extends UmbUniqueTreeStore {
 	 */
 	constructor(host: UmbControllerHostElement) {
 		super(host, UMB_DICTIONARY_TREE_STORE_CONTEXT.toString());
+
+		new UmbStoreConnector<UmbDictionaryTreeItemModel, UmbDictionaryDetailModel>(
+			host,
+			this,
+			UMB_DICTIONARY_DETAIL_STORE_CONTEXT,
+			(item) => this.#createTreeItemMapper(item),
+			(item) => this.#updateTreeItemMapper(item),
+		);
 	}
+
+	// TODO: revisit this when we have decided on detail model sizes
+	#createTreeItemMapper = (item: UmbDictionaryDetailModel) => {
+		const treeItem: UmbDictionaryTreeItemModel = {
+			unique: item.unique,
+			parentUnique: null,
+			name: item.name,
+			entityType: item.entityType,
+			isFolder: false,
+			hasChildren: false,
+		};
+
+		return treeItem;
+	};
+
+	// TODO: revisit this when we have decided on detail model sizes
+	#updateTreeItemMapper = (model: UmbDictionaryDetailModel) => {
+		return {
+			name: model.name,
+		};
+	};
 }
 
 export const UMB_DICTIONARY_TREE_STORE_CONTEXT = new UmbContextToken<UmbDictionaryTreeStore>('UmbDictionaryTreeStore');
