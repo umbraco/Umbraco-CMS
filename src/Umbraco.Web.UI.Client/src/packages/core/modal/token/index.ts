f89862c4c608087261a3cf745869ffc86231cede
Mads Rasmussen <madsr@hey.com>
2024-02-04 14:00:21 +0100

    move search modal token

--- src/Umbraco.Web.UI.Client/src/packages/core/modal/token/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/modal/token/index.ts
@@ -23,7 +23,6 @@ export * from './modal-token.js';
 export * from './permissions-modal.token.js';
 export * from './property-editor-ui-picker-modal.token.js';
 export * from './property-settings-modal.token.js';
-export * from './search-modal.token.js';
 export * from './section-picker-modal.token.js';
 export * from './template-picker-modal.token.js';
 export * from './user-group-picker-modal.token.js';
