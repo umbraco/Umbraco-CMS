1cc97c21ede81d59b10d1877303cf3cf28f8657b
Nikolaj <nikolajlauridsen@protonmail.ch>
2024-02-05 12:21:22 +0100

    Remove duplicate implementation

--- src/Umbraco.PublishedCache.NuCache/DataSource/JsonContentNestedDataSerializerFactory.cs
+++ src/Umbraco.PublishedCache.NuCache/DataSource/JsonContentNestedDataSerializerFactory.cs
@@ -2,7 +2,7 @@ namespace Umbraco.Cms.Infrastructure.PublishedCache.DataSource;
 
 internal class JsonContentNestedDataSerializerFactory : IContentCacheDataSerializerFactory
 {
-    private readonly Lazy<SystemTextJsonNestedDataSerializer> _serializer = new();
+    private readonly Lazy<JsonContentNestedDataSerializer> _serializer = new();
 
     public IContentCacheDataSerializer Create(ContentCacheDataSerializerEntityType types) => _serializer.Value;
 }
