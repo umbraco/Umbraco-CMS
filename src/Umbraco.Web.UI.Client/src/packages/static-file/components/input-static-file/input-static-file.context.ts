82ebacaa60d3123049a98fbe00c1385aab78b44c
Mads Rasmussen <madsr@hey.com>
2024-02-05 14:55:36 +0100

    use client model for static files

--- src/Umbraco.Web.UI.Client/src/packages/static-file/components/input-static-file/input-static-file.context.ts
+++ src/Umbraco.Web.UI.Client/src/packages/static-file/components/input-static-file/input-static-file.context.ts
@@ -1,11 +1,13 @@
 import { UMB_STATIC_FILE_TREE_REPOSITORY_ALIAS } from '../../tree/index.js';
 import { UMB_STATIC_FILE_PICKER_MODAL } from '../../modals/index.js';
+import type { UmbStaticFileItemModel } from '../../repository/item/types.js';
 import { UmbPickerInputContext } from '@umbraco-cms/backoffice/picker-input';
 import type { UmbControllerHostElement } from '@umbraco-cms/backoffice/controller-api';
-import type { StaticFileItemResponseModel } from '@umbraco-cms/backoffice/backend-api';
 
-export class UmbStaticFilePickerContext extends UmbPickerInputContext<StaticFileItemResponseModel> {
+export class UmbStaticFilePickerContext extends UmbPickerInputContext<UmbStaticFileItemModel> {
 	constructor(host: UmbControllerHostElement) {
+		// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+		// @ts-ignore
 		super(host, UMB_STATIC_FILE_TREE_REPOSITORY_ALIAS, UMB_STATIC_FILE_PICKER_MODAL);
 	}
 }
---
+++ src/Umbraco.Web.UI.Client/src/packages/static-file/repository/manifests.ts
@@ -0,0 +1,4 @@
+import { manifests as itemManifests } from './manifests.js';
+import type { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
+
+export const manifests: Array<ManifestTypes> = [...itemManifests];
