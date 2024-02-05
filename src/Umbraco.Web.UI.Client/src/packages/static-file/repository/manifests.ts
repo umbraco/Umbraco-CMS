8a798eb7f161ada1b9027bbfd0c7209b01705f4f
Mads Rasmussen <madsr@hey.com>
2024-02-05 15:21:50 +0100

    fix import

--- src/Umbraco.Web.UI.Client/src/packages/static-file/repository/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/static-file/repository/manifests.ts
@@ -1,4 +1,4 @@
-import { manifests as itemManifests } from './manifests.js';
+import { manifests as itemManifests } from './item/manifests.js';
 import type { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
 
 export const manifests: Array<ManifestTypes> = [...itemManifests];
