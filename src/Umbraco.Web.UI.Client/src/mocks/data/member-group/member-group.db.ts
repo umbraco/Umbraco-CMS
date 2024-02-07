d4ba833c102592a83b45c71d665742c16e540956
Mads Rasmussen <madsr@hey.com>
2024-02-07 12:49:23 +0100

    add filter group to member groups mock db

--- src/Umbraco.Web.UI.Client/src/mocks/data/member-group/member-group.db.ts
+++ src/Umbraco.Web.UI.Client/src/mocks/data/member-group/member-group.db.ts
@@ -1,10 +1,15 @@
 import { UmbEntityMockDbBase } from '../utils/entity/entity-base.js';
 import { UmbMockEntityItemManager } from '../utils/entity/entity-item.manager.js';
 import { UmbMockEntityDetailManager } from '../utils/entity/entity-detail.manager.js';
+import { queryFilter } from '../utils.js';
+import { pagedResult } from '../utils/paged-result.js';
 import type { UmbMockMemberGroupModel } from './member-group.data.js';
 import { data } from './member-group.data.js';
 import { UmbId } from '@umbraco-cms/backoffice/id';
 import type { MemberGroupItemResponseModel } from '@umbraco-cms/backoffice/backend-api';
+
+const memberGroupQueryFilter = (filterOptions: any, item: UmbMockMemberGroupModel) =>
+	queryFilter(filterOptions.filter, item.name);
 
 class UmbMemberGroupMockDB extends UmbEntityMockDbBase<UmbMockMemberGroupModel> {
 	item = new UmbMockEntityItemManager<UmbMockMemberGroupModel>(this, itemResponseMapper);
@@ -12,6 +17,21 @@ class UmbMemberGroupMockDB extends UmbEntityMockDbBase<UmbMockMemberGroupModel> 
 
 	constructor(data: Array<UmbMockMemberGroupModel>) {
 		super(data);
+	}
+
+	filter(options: any): any {
+		const allItems = this.getAll();
+
+		const filterOptions = {
+			skip: options.skip || 0,
+			take: options.take || 25,
+			filter: options.filter,
+		};
+
+		const filteredItems = allItems.filter((item) => memberGroupQueryFilter(filterOptions, item));
+		const paginatedResult = pagedResult(filteredItems, filterOptions.skip, filterOptions.take);
+
+		return { items: paginatedResult.items, total: paginatedResult.total };
 	}
 }
 
