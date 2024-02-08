791d72cce53a04567f3607e1b224f11ca3cacaf6
Mads Rasmussen <madsr@hey.com>
2024-02-08 13:42:57 +0100

    add store sync

--- src/Umbraco.Web.UI.Client/src/packages/members/member-type/tree/member-type-tree.store.ts
+++ src/Umbraco.Web.UI.Client/src/packages/members/member-type/tree/member-type-tree.store.ts
@@ -1,5 +1,9 @@
+import type { UmbMemberTypeDetailModel } from '../types.js';
+import { UMB_MEMBER_TYPE_DETAIL_STORE_CONTEXT } from '../repository/detail/member-type-detail.store.js';
+import type { UmbMemberTypeTreeItemModel } from './types.js';
 import { UmbContextToken } from '@umbraco-cms/backoffice/context-api';
 import type { UmbControllerHostElement } from '@umbraco-cms/backoffice/controller-api';
+import { UmbStoreConnector } from '@umbraco-cms/backoffice/store';
 import { UmbUniqueTreeStore } from '@umbraco-cms/backoffice/tree';
 
 /**
@@ -16,7 +20,36 @@ export class UmbMemberTypeTreeStore extends UmbUniqueTreeStore {
 	 */
 	constructor(host: UmbControllerHostElement) {
 		super(host, UMB_MEMBER_TYPE_TREE_STORE_CONTEXT.toString());
+
+		new UmbStoreConnector<UmbMemberTypeTreeItemModel, UmbMemberTypeDetailModel>(
+			host,
+			this,
+			UMB_MEMBER_TYPE_DETAIL_STORE_CONTEXT,
+			(item) => this.#createTreeItemMapper(item),
+			(item) => this.#updateTreeItemMapper(item),
+		);
 	}
+
+	// TODO: revisit this when we have decided on detail model sizes
+	#createTreeItemMapper = (item: UmbMemberTypeDetailModel) => {
+		const treeItem: UmbMemberTypeTreeItemModel = {
+			unique: item.unique,
+			parentUnique: null,
+			name: item.name,
+			entityType: item.entityType,
+			isFolder: false,
+			hasChildren: false,
+		};
+
+		return treeItem;
+	};
+
+	// TODO: revisit this when we have decided on detail model sizes
+	#updateTreeItemMapper = (item: UmbMemberTypeDetailModel) => {
+		return {
+			name: item.name,
+		};
+	};
 }
 
 export const UMB_MEMBER_TYPE_TREE_STORE_CONTEXT = new UmbContextToken<UmbMemberTypeTreeStore>('UmbMemberTypeTreeStore');
