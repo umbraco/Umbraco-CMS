77de2b107f6380329dcab1faae3a53f8734bc522
Elitsa <elm@umbraco.dk>
2024-02-08 13:18:15 +0100

    Adding mapping

--- src/Umbraco.Cms.Api.Management/Mapping/Document/DocumentMapDefinition.cs
+++ src/Umbraco.Cms.Api.Management/Mapping/Document/DocumentMapDefinition.cs
@@ -1,9 +1,11 @@
 ﻿using Umbraco.Cms.Api.Management.Mapping.Content;
 using Umbraco.Cms.Api.Management.ViewModels.Document;
+using Umbraco.Cms.Api.Management.ViewModels.Document.Collection;
 using Umbraco.Cms.Api.Management.ViewModels.DocumentType;
 using Umbraco.Cms.Core.Mapping;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.PropertyEditors;
+using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Api.Management.Mapping.Document;
 
@@ -15,7 +17,10 @@ public class DocumentMapDefinition : ContentMapDefinition<IContent, DocumentValu
     }
 
     public void DefineMaps(IUmbracoMapper mapper)
-        => mapper.Define<IContent, DocumentResponseModel>((_, _) => new DocumentResponseModel(), Map);
+    {
+        mapper.Define<IContent, DocumentResponseModel>((_, _) => new DocumentResponseModel(), Map);
+        mapper.Define<IContent, DocumentCollectionResponseModel>((_, _) => new DocumentCollectionResponseModel(), Map);
+    }
 
     // Umbraco.Code.MapAll -Urls -Template
     private void Map(IContent source, DocumentResponseModel target, MapperContext context)
@@ -33,5 +38,35 @@ public class DocumentMapDefinition : ContentMapDefinition<IContent, DocumentValu
                     : source.GetPublishDate(culture);
             });
         target.IsTrashed = source.Trashed;
+    }
+
+    // Umbraco.Code.MapAll
+    private void Map(IContent source, DocumentCollectionResponseModel target, MapperContext context)
+    {
+        target.Id = source.Key;
+        target.DocumentType = context.Map<DocumentTypeCollectionReferenceResponseModel>(source.ContentType)!;
+        target.SortOrder = source.SortOrder;
+
+        // If there's a set of property aliases specified in the collection configuration, we will check if the current property's
+        // value should be mapped. If it isn't one of the ones specified in 'includeProperties', we will just return the result
+        // without mapping the value.
+        var includedProperties = context.GetIncludedProperties();
+
+        IEnumerable<IProperty> properties = source.Properties;
+        if (includedProperties is not null)
+        {
+            properties = properties.Where(property => includedProperties.Contains(property.Alias));
+        }
+
+        target.Values = MapValueViewModels(properties);
+        target.Variants = MapVariantViewModels(
+            source,
+            (culture, _, documentVariantViewModel) =>
+            {
+                documentVariantViewModel.State = DocumentVariantStateHelper.GetState(source, culture);
+                documentVariantViewModel.PublishDate = culture == null
+                    ? source.PublishDate
+                    : source.GetPublishDate(culture);
+            });
     }
 }
