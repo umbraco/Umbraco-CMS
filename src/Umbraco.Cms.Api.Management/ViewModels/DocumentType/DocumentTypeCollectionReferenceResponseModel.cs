a6113f6cb3ea8ef666a4971841acc1f2f180b885
Elitsa <elm@umbraco.dk>
2024-02-08 13:15:54 +0100

    Adding content and media type collection response models

---
+++ src/Umbraco.Cms.Api.Management/ViewModels/DocumentType/DocumentTypeCollectionReferenceResponseModel.cs
@@ -0,0 +1,10 @@
+namespace Umbraco.Cms.Api.Management.ViewModels.DocumentType;
+
+public class DocumentTypeCollectionReferenceResponseModel
+{
+    public Guid Id { get; set; }
+
+    public string Alias { get; set; } = string.Empty;
+
+    public string Icon { get; set; } = string.Empty;
+}
---
+++ src/Umbraco.Cms.Api.Management/ViewModels/MediaType/MediaTypeCollectionReferenceResponseModel.cs
@@ -0,0 +1,10 @@
+namespace Umbraco.Cms.Api.Management.ViewModels.MediaType;
+
+public class MediaTypeCollectionReferenceResponseModel
+{
+    public Guid Id { get; set; }
+
+    public string Alias { get; set; } = string.Empty;
+
+    public string Icon { get; set; } = string.Empty;
+}
