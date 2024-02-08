a9377c23704aa73fa96298925b3ded633d1d4402
Mads Rasmussen <madsr@hey.com>
2024-02-08 13:00:02 +0100

    add create entity action

--- src/Umbraco.Web.UI.Client/src/packages/members/member-type/entity-actions/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/members/member-type/entity-actions/manifests.ts
@@ -1,9 +1,23 @@
-import { UMB_MEMBER_TYPE_ENTITY_TYPE } from '../entity.js';
+import { UMB_MEMBER_TYPE_ENTITY_TYPE, UMB_MEMBER_TYPE_ROOT_ENTITY_TYPE } from '../entity.js';
 import { UMB_MEMBER_TYPE_DETAIL_REPOSITORY_ALIAS } from '../repository/detail/index.js';
+import { UmbCreateMemberTypeEntityAction } from './create.action.js';
 import { UmbDeleteEntityAction } from '@umbraco-cms/backoffice/entity-action';
 import type { ManifestEntityAction } from '@umbraco-cms/backoffice/extension-registry';
 
 const entityActions: Array<ManifestEntityAction> = [
+	{
+		type: 'entityAction',
+		alias: 'Umb.EntityAction.MemberType.Create',
+		name: 'Create Member Type Entity Action',
+		weight: 1000,
+		api: UmbCreateMemberTypeEntityAction,
+		meta: {
+			icon: 'icon-add',
+			label: 'Create...',
+			repositoryAlias: UMB_MEMBER_TYPE_DETAIL_REPOSITORY_ALIAS,
+			entityTypes: [UMB_MEMBER_TYPE_ROOT_ENTITY_TYPE],
+		},
+	},
 	{
 		type: 'entityAction',
 		alias: 'Umb.EntityAction.MemberType.Delete',
