de5be2f2807bd7195786ea23b8ad7ea91435d13b
Elitsa <elm@umbraco.dk>
2024-02-08 13:07:53 +0100

    Adding another silo for list views

--- src/Umbraco.Core/Constants-Web.cs
+++ src/Umbraco.Core/Constants-Web.cs
@@ -69,6 +69,7 @@ public static partial class Constants
             public const string Tree = "tree";
             public const string RecycleBin = "recycle-bin";
             public const string Item = "item";
+            public const string Collection = "collection";
         }
 
         public static class AttributeRouting
