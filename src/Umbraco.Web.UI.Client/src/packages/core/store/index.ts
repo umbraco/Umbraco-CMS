608e11e11d46e736bb13bf22de552fa3cd961e53
Mads Rasmussen <madsr@hey.com>
2024-02-08 15:35:57 +0100

    remove unused entity store

--- src/Umbraco.Web.UI.Client/src/packages/core/store/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/store/index.ts
@@ -1,7 +1,6 @@
 export * from './store-base.js';
 export * from './store.interface.js';
 export * from './store.js';
-export * from './entity-item.store.js';
 
 export { UmbDetailStoreBase } from './detail-store-base.js';
 export type { UmbDetailStore } from './detail-store.interface.js';
