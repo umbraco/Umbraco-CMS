25f8925db8970fee954ec8d62307d5bee88851d3
Mads Rasmussen <madsr@hey.com>
2024-02-08 15:31:58 +0100

    remove file-system item store

--- src/Umbraco.Web.UI.Client/src/packages/core/store/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/store/index.ts
@@ -2,7 +2,6 @@ export * from './store-base.js';
 export * from './store.interface.js';
 export * from './store.js';
 export * from './entity-item.store.js';
-export * from './file-system-item.store.js';
 
 export { UmbDetailStoreBase } from './detail-store-base.js';
 export type { UmbDetailStore } from './detail-store.interface.js';
