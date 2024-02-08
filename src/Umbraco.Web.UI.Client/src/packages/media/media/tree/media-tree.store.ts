93db05da3fc3e983c669afd0cec88f5b0f59c50b
JesmoDev <jjensenmoeller@gmail.com>
2024-02-08 14:19:16 +0100

    tree listen for store changes

--- src/Umbraco.Web.UI.Client/src/packages/media/media/tree/media-tree.store.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/tree/media-tree.store.ts
@@ -1,5 +1,9 @@
+import type { UmbMediaDetailModel } from '../types.js';
+import { UMB_MEDIA_DETAIL_STORE_CONTEXT } from '../repository/detail/media-detail.store.js';
+import type { UmbMediaTreeItemModel } from './types.js';
 import { UmbContextToken } from '@umbraco-cms/backoffice/context-api';
 import type { UmbControllerHostElement } from '@umbraco-cms/backoffice/controller-api';
+import { UmbStoreConnector } from '@umbraco-cms/backoffice/store';
 import { UmbUniqueTreeStore } from '@umbraco-cms/backoffice/tree';
 
 /**
@@ -16,7 +20,39 @@ export class UmbMediaTreeStore extends UmbUniqueTreeStore {
 	 */
 	constructor(host: UmbControllerHostElement) {
 		super(host, UMB_MEDIA_TREE_STORE_CONTEXT.toString());
+
+		new UmbStoreConnector<UmbMediaTreeItemModel, UmbMediaDetailModel>(
+			host,
+			this,
+			UMB_MEDIA_DETAIL_STORE_CONTEXT,
+			(item) => this.#createTreeItemMapper(item),
+			(item) => this.#updateTreeItemMapper(item),
+		);
 	}
+
+	// TODO: revisit this when we have decided on detail model sizes
+	// TODO: Fix tree model
+	#createTreeItemMapper = (item: UmbMediaDetailModel) => {
+		const treeItem: UmbMediaTreeItemModel = {
+			unique: item.unique,
+			parentUnique: item.parentUnique,
+			name: item.variants[0].name,
+			entityType: item.entityType,
+			isFolder: false,
+			hasChildren: false,
+			variants: item.variants,
+			isTrashed: item.isTrashed,
+		};
+
+		return treeItem;
+	};
+
+	// TODO: revisit this when we have decided on detail model sizes
+	#updateTreeItemMapper = (item: UmbMediaDetailModel) => {
+		return {
+			name: item.variants[0].name,
+		};
+	};
 }
 
 export const UMB_MEDIA_TREE_STORE_CONTEXT = new UmbContextToken<UmbMediaTreeStore>('UmbMediaTreeStore');
