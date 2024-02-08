a4db82e216de06861962e349fdd05ecec6399c15
Mads Rasmussen <madsr@hey.com>
2024-02-08 11:54:32 +0100

    remove unused code

--- src/Umbraco.Web.UI.Client/src/packages/members/member-type/repository/detail/member-type-detail.repository.ts
+++ src/Umbraco.Web.UI.Client/src/packages/members/member-type/repository/detail/member-type-detail.repository.ts
@@ -1,26 +1,22 @@
 import type { UmbMemberTypeDetailModel } from '../../types.js';
 import { UmbMemberTypeServerDataSource } from './member-type-detail.server.data-source.js';
-import type { UmbMemberTypeDetailStore } from './member-type-detail.store.js';
 import { UMB_MEMBER_TYPE_DETAIL_STORE_CONTEXT } from './member-type-detail.store.js';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import { UmbDetailRepositoryBase } from '@umbraco-cms/backoffice/repository';
-export class UmbMemberTypeDetailRepository extends UmbDetailRepositoryBase<UmbMemberTypeDetailModel> {
-	#init: Promise<unknown>;
-	#detailStore?: UmbMemberTypeDetailStore;
 
+/**
+ * A repository for the Member Type detail
+ * @export
+ * @class UmbMemberTypeDetailRepository
+ * @extends {UmbDetailRepositoryBase<UmbMemberTypeDetailModel>}
+ */
+export class UmbMemberTypeDetailRepository extends UmbDetailRepositoryBase<UmbMemberTypeDetailModel> {
+	/**
+	 * Creates an instance of UmbMemberTypeDetailRepository.
+	 * @param {UmbControllerHost} host
+	 * @memberof UmbMemberTypeDetailRepository
+	 */
 	constructor(host: UmbControllerHost) {
 		super(host, UmbMemberTypeServerDataSource, UMB_MEMBER_TYPE_DETAIL_STORE_CONTEXT);
-
-		this.#init = Promise.all([
-			this.consumeContext(UMB_MEMBER_TYPE_DETAIL_STORE_CONTEXT, (instance) => {
-				this.#detailStore = instance;
-			}).asPromise(),
-		]);
-	}
-
-	async byPropertyEditorUiAlias(propertyEditorUiAlias: string) {
-		if (!propertyEditorUiAlias) throw new Error('propertyEditorUiAlias is missing');
-		await this.#init;
-		return this.#detailStore!.withPropertyEditorUiAlias(propertyEditorUiAlias);
 	}
 }
