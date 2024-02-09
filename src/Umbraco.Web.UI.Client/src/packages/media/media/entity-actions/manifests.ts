8011f45b66ca80bb11800f37201155eb2ae5e71b
JesmoDev <jjensenmoeller@gmail.com>
2024-02-09 13:42:05 +0100

    delete instead of trash

--- src/Umbraco.Web.UI.Client/src/packages/media/media/entity-actions/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/entity-actions/manifests.ts
@@ -1,6 +1,6 @@
 import { UMB_MEDIA_DETAIL_REPOSITORY_ALIAS } from '../repository/index.js';
 import { manifests as createManifests } from './create/manifests.js';
-import { UmbTrashEntityAction } from '@umbraco-cms/backoffice/entity-action';
+import { UmbDeleteEntityAction } from '@umbraco-cms/backoffice/entity-action';
 
 import type { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
 
@@ -8,12 +8,12 @@ const entityActions: Array<ManifestTypes> = [
 	...createManifests,
 	{
 		type: 'entityAction',
-		alias: 'Umb.EntityAction.Media.Trash',
-		name: 'Trash Media Entity Action ',
-		api: UmbTrashEntityAction,
+		alias: 'Umb.EntityAction.Media.Delete',
+		name: 'Delete Media Entity Action ',
+		api: UmbDeleteEntityAction,
 		meta: {
-			icon: 'icon-trash',
-			label: 'Trash',
+			icon: 'icon-delete',
+			label: 'Delete',
 			repositoryAlias: UMB_MEDIA_DETAIL_REPOSITORY_ALIAS,
 			entityTypes: ['media'],
 		},
