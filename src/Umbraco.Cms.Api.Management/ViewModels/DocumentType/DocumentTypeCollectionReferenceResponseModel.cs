62f323eabf2625852fc3541a223841df3efff99c
Elitsa <elm@umbraco.dk>
2024-02-09 13:16:04 +0100

    Adding a base class for content type collection reference model

--- src/Umbraco.Cms.Api.Management/ViewModels/DocumentType/DocumentTypeCollectionReferenceResponseModel.cs
+++ src/Umbraco.Cms.Api.Management/ViewModels/DocumentType/DocumentTypeCollectionReferenceResponseModel.cs
@@ -1,10 +1,7 @@
+using Umbraco.Cms.Api.Management.ViewModels.ContentType;
+
 namespace Umbraco.Cms.Api.Management.ViewModels.DocumentType;
 
-public class DocumentTypeCollectionReferenceResponseModel
+public class DocumentTypeCollectionReferenceResponseModel : ContentTypeCollectionReferenceResponseModelBase
 {
-    public Guid Id { get; set; }
-
-    public string Alias { get; set; } = string.Empty;
-
-    public string Icon { get; set; } = string.Empty;
 }
--- src/Umbraco.Cms.Api.Management/ViewModels/MediaType/MediaTypeCollectionReferenceResponseModel.cs
+++ src/Umbraco.Cms.Api.Management/ViewModels/MediaType/MediaTypeCollectionReferenceResponseModel.cs
@@ -1,10 +1,7 @@
+using Umbraco.Cms.Api.Management.ViewModels.ContentType;
+
 namespace Umbraco.Cms.Api.Management.ViewModels.MediaType;
 
-public class MediaTypeCollectionReferenceResponseModel
+public class MediaTypeCollectionReferenceResponseModel : ContentTypeCollectionReferenceResponseModelBase
 {
-    public Guid Id { get; set; }
-
-    public string Alias { get; set; } = string.Empty;
-
-    public string Icon { get; set; } = string.Empty;
 }
