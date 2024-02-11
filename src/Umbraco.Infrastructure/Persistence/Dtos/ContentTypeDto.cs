d8e9b696fadbf382c1c6ad78d0addf79bbe3d559
nzdev <nzdev@github.com>
2024-02-11 18:46:17 +1300

    another set of for columns

--- src/Umbraco.Infrastructure/Persistence/Dtos/ContentTypeDto.cs
+++ src/Umbraco.Infrastructure/Persistence/Dtos/ContentTypeDto.cs
@@ -19,7 +19,7 @@ internal class ContentTypeDto
     [Column("nodeId")]
     [ForeignKey(typeof(NodeDto))]
     [Index(IndexTypes.UniqueNonClustered, Name = "IX_cmsContentType")]
-    [Index(IndexTypes.NonClustered, Name = "IX_cmsContentType_nodeId", IncludeColumns = "nodeId", ForColumns = "alias,icon,thumbnail,isContainer,variations")]
+    [Index(IndexTypes.NonClustered, Name = "IX_cmsContentType_nodeId", IncludeColumns = "nodeId", ForColumns = "alias,icon,thumbnail,isContainer,variations,allowAtRoot,isElement")]
     public int NodeId { get; set; }
 
     [Column("alias")]
