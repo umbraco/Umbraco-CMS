c5c5fdf40fc0e911cb3a703076e260b42e448bed
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-02-13 11:57:17 +0100

    update the definitions for the json schema

--- src/Umbraco.Web.UI.Client/src/packages/core/extension-registry/umbraco-package.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/extension-registry/umbraco-package.ts
@@ -32,4 +32,25 @@ export interface UmbracoPackage {
 	 * @required
 	 */
 	extensions: ManifestTypes[];
+
+	/**
+	 * @title The importmap for the package
+	 * @description This is used to define the imports and the scopes for the package to be used in the browser. It will be combined with the global importmap.
+	 * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/script/type/importmap
+	 */
+	importmap?: {
+		/**
+		 * @title The imports for the package
+		 * @required
+		 * @minProperties 1
+		 * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/script/type/importmap#imports
+		 */
+		imports: Record<string, string>;
+
+		/**
+		 * @title The scopes for the package
+		 * @see https://developer.mozilla.org/en-US/docs/Web/HTML/Element/script/type/importmap#scopes
+		 */
+		scopes?: Record<string, object>;
+	}
 }
