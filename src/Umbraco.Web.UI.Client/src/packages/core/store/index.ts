d2873df00cacc314b91deedbb91d2f0965223a59
Mads Rasmussen <madsr@hey.com>
2024-02-14 22:23:06 +0100

    remove unused manager + store

--- src/Umbraco.Web.UI.Client/src/packages/core/store/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/store/index.ts
@@ -1,6 +1,5 @@
 export * from './store-base.js';
 export * from './store.interface.js';
-export * from './store.js';
 
 export { UmbDetailStoreBase } from './detail-store-base.js';
 export type { UmbDetailStore } from './detail-store.interface.js';
