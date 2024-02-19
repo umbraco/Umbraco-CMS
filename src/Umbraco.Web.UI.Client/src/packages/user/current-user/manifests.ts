233e16111553d61e0de6ffa9e9ca61569c157904
Mads Rasmussen <madsr@hey.com>
2024-02-19 11:59:34 +0100

    register and exports

--- src/Umbraco.Web.UI.Client/src/packages/user/current-user/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/current-user/manifests.ts
@@ -1,14 +1,15 @@
 import { UmbCurrentUserContext } from './current-user.context.js';
 import { manifests as modalManifests } from './modals/manifests.js';
 import { manifests as userProfileAppsManifests } from './user-profile-apps/manifests.js';
+import { manifests as repositoryManifests } from './repository/manifests.js';
 import type { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
 
 export const headerApps: Array<ManifestTypes> = [
 	{
 		type: 'store',
-		alias: 'Umb.Store.CurrentUser',
-		name: 'Current User Store',
-		js: () => import('./current-user-history.store.js'),
+		alias: 'Umb.Store.CurrentUser.History',
+		name: 'Current User History Store',
+		api: () => import('./current-user-history.store.js'),
 	},
 	{
 		type: 'globalContext',
@@ -20,7 +21,7 @@ export const headerApps: Array<ManifestTypes> = [
 		type: 'headerApp',
 		alias: 'Umb.HeaderApp.CurrentUser',
 		name: 'Current User',
-		js: () => import('./current-user-header-app.element.js'),
+		element: () => import('./current-user-header-app.element.js'),
 		weight: 0,
 		meta: {
 			label: 'TODO: how should we enable this to not be set.',
@@ -30,4 +31,4 @@ export const headerApps: Array<ManifestTypes> = [
 	},
 ];
 
-export const manifests = [...headerApps, ...modalManifests, ...userProfileAppsManifests];
+export const manifests = [...headerApps, ...modalManifests, ...repositoryManifests, ...userProfileAppsManifests];
--- src/Umbraco.Web.UI.Client/src/packages/user/current-user/repository/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/current-user/repository/index.ts
@@ -1,2 +1,3 @@
 export { UmbCurrentUserRepository } from './current-user.repository.js';
 export { UMB_CURRENT_USER_REPOSITORY_ALIAS } from './manifests.js';
+export { UmbCurrentUserStore, UMB_CURRENT_USER_STORE_CONTEXT } from './current-user.store.js';
