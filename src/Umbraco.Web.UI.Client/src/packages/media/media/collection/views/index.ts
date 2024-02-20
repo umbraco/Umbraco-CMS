e740c366879c9d68c3769dd0e1c0918e387c831c
leekelleher <leekelleher@gmail.com>
2024-02-20 17:25:21 +0000

    Adds Media Grid Collection View

---
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/collection/views/index.ts
@@ -0,0 +1,1 @@
+export const UMB_MEDIA_GRID_COLLECTION_VIEW_ALIAS = 'Umb.CollectionView.Media.Grid';
---
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/collection/views/manifests.ts
@@ -0,0 +1,24 @@
+import { UMB_MEDIA_GRID_COLLECTION_VIEW_ALIAS } from './index.js';
+import { UMB_COLLECTION_ALIAS_CONDITION } from '@umbraco-cms/backoffice/collection';
+import type { ManifestCollectionView } from '@umbraco-cms/backoffice/extension-registry';
+
+const gridViewManifest: ManifestCollectionView = {
+	type: 'collectionView',
+	alias: UMB_MEDIA_GRID_COLLECTION_VIEW_ALIAS,
+	name: 'Media Grid Collection View',
+	element: () => import('./grid/media-grid-collection-view.element.js'),
+	weight: 300,
+	meta: {
+		label: 'Grid',
+		icon: 'icon-grid',
+		pathName: 'grid',
+	},
+	conditions: [
+		{
+			alias: UMB_COLLECTION_ALIAS_CONDITION,
+			match: 'Umb.Collection.Media',
+		},
+	],
+};
+
+export const manifests = [gridViewManifest];
