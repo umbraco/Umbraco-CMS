d03839cf8a66df584f124adeaf947aa161e3ed25
JesmoDev <jjensenmoeller@gmail.com>
2024-02-20 10:13:19 +0100

    align exports with document types

--- src/Umbraco.Web.UI.Client/src/packages/media/media-types/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media-types/index.ts
@@ -1,18 +1,8 @@
 import './components/index.js';
 
-export type { UmbAllowedMediaTypeModel } from './repository/index.js';
-export {
-	UmbMediaTypeItemRepository,
-	UMB_MEDIA_TYPE_ITEM_STORE_ALIAS,
-	UMB_MEDIA_TYPE_DETAIL_STORE_ALIAS,
-	UMB_MEDIA_TYPE_DETAIL_STORE_CONTEXT,
-	UmbMediaTypeStructureRepository,
-} from './repository/index.js';
-
-export {
-	UMB_MEDIA_TYPE_ROOT_ENTITY_TYPE,
-	UMB_MEDIA_TYPE_ENTITY_TYPE,
-	UMB_MEDIA_TYPE_FOLDER_ENTITY_TYPE,
-} from './entity.js';
-
 export * from './components/index.js';
+export * from './workspace/index.js';
+
+export * from './repository/index.js';
+export * from './tree/types.js';
+export * from './types.js';
