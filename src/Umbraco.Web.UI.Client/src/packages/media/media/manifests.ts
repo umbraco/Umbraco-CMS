dfe3bdc04fffb168610df748b8c78e6c1102fd04
Mads Rasmussen <madsr@hey.com>
2024-02-20 16:06:56 +0100

    remove demo media permissions

--- src/Umbraco.Web.UI.Client/src/packages/media/media/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/manifests.ts
@@ -5,7 +5,6 @@ import { manifests as menuItemManifests } from './menu-item/manifests.js';
 import { manifests as propertyEditorsManifests } from './property-editors/manifests.js';
 import { manifests as repositoryManifests } from './repository/manifests.js';
 import { manifests as treeManifests } from './tree/manifests.js';
-import { manifests as userPermissionManifests } from './user-permissions/manifests.js';
 import { manifests as workspaceManifests } from './workspace/manifests.js';
 
 export const manifests = [
@@ -16,6 +15,5 @@ export const manifests = [
 	...propertyEditorsManifests,
 	...repositoryManifests,
 	...treeManifests,
-	...userPermissionManifests,
 	...workspaceManifests,
 ];
