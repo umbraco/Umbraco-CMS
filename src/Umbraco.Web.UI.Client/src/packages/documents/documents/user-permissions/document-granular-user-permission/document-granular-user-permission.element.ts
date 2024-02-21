fd986d6455717808a41cbeae3360ffe7d51dded5
Mads Rasmussen <madsr@hey.com>
2024-02-21 15:47:10 +0100

    wip document granular permission element

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/user-permissions/document-granular-user-permission/document-granular-user-permission.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/user-permissions/document-granular-user-permission/document-granular-user-permission.element.ts
@@ -1,19 +1,105 @@
 import type { UmbDocumentUserPermissionModel } from '../types.js';
-import { css, customElement, html, property } from '@umbraco-cms/backoffice/external/lit';
+import { UmbDocumentItemRepository, type UmbDocumentItemModel } from '../../repository/index.js';
+import { css, customElement, html, ifDefined, repeat, state } from '@umbraco-cms/backoffice/external/lit';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
+import type { UmbModalManagerContext } from '@umbraco-cms/backoffice/modal';
+import { UMB_DOCUMENT_PICKER_MODAL, UMB_MODAL_MANAGER_CONTEXT } from '@umbraco-cms/backoffice/modal';
+import { UmbSelectedEvent } from '@umbraco-cms/backoffice/event';
 
 @customElement('umb-document-granular-user-permission')
 export class UmbDocumentGranularUserPermissionElement extends UmbLitElement {
-	@property({ type: Array, attribute: false })
-	value: Array<UmbDocumentUserPermissionModel> = [];
+	/*
+	public get value(): Array<UmbDocumentUserPermissionModel> {
+		return this.#pickerContext.getSelection();
+	}
+	*/
+
+	public set value(value: Array<UmbDocumentUserPermissionModel>) {
+		const uniques = value.map((item) => item.document.id);
+		this.#observePickedDocuments(uniques);
+	}
+
+	@state()
+	private _items?: Array<UmbDocumentItemModel>;
+
+	#documentItemRepository = new UmbDocumentItemRepository(this);
+	#modalManagerContext?: UmbModalManagerContext;
+
+	constructor() {
+		super();
+
+		this.consumeContext(UMB_MODAL_MANAGER_CONTEXT, (instance) => (this.#modalManagerContext = instance));
+	}
+
+	async #observePickedDocuments(uniques: Array<string>) {
+		const { asObservable } = await this.#documentItemRepository.requestItems(uniques);
+		this.observe(asObservable(), (items) => (this._items = items));
+	}
+
+	#openPicker() {
+		const modalContext = this.#modalManagerContext?.open(UMB_DOCUMENT_PICKER_MODAL, {
+			data: {
+				hideTreeRoot: true,
+			},
+		});
+
+		modalContext?.addEventListener(UmbSelectedEvent.TYPE, (event) => {
+			const selectedEvent = event as UmbSelectedEvent;
+			const unique = selectedEvent.unique;
+			if (!unique) return;
+			console.log(unique);
+		});
+
+		modalContext?.onSubmit().then((value) => {
+			//this.#setSelection(selection);
+		});
+	}
 
 	render() {
-		return html`${this.value.map(
-			(permission) => html`
-				<div>${permission.$type}</div>
-				<div>${permission.document.id}</div>
-			`,
-		)}`;
+		return html`${this.#renderItems()} ${this.#renderAddButton()}`;
+	}
+
+	#renderItems() {
+		if (!this._items) return;
+		return html`<uui-ref-list>
+			${repeat(
+				this._items,
+				(item) => item.unique,
+				(item) => this.#renderRef(item),
+			)}
+		</uui-ref-list>`;
+	}
+
+	#renderAddButton() {
+		return html`<uui-button
+			id="add-button"
+			look="placeholder"
+			@click=${this.#openPicker}
+			label=${this.localize.term('general_choose')}></uui-button>`;
+	}
+
+	// TODO: make umb-document-ref element
+	#renderRef(item: UmbDocumentItemModel) {
+		if (!item.unique) return;
+		// TODO: get correct variant name
+		const name = item.variants[0]?.name;
+
+		return html`
+			<uui-ref-node name=${name} detail=${ifDefined(item.unique)}>
+				${this.#renderIcon(item)} ${this.#renderIsTrashed(item)}
+				<uui-action-bar slot="actions"> </uui-action-bar>
+			</uui-ref-node>
+		`;
+	}
+
+	#renderIcon(item: UmbDocumentItemModel) {
+		if (!item.documentType.icon) return;
+		return html`<uui-icon slot="icon" name=${item.documentType.icon}></uui-icon>`;
+	}
+
+	#renderIsTrashed(item: UmbDocumentItemModel) {
+		if (!item.isTrashed) return;
+		return html`<uui-tag size="s" slot="tag" color="danger">Trashed</uui-tag>`;
 	}
 
 	static styles = [css``];
