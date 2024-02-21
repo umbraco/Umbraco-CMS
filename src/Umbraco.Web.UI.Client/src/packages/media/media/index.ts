f2cfb4aca19526ff5c6f8c07422a2d3272f92ccb
Mads Rasmussen <madsr@hey.com>
2024-02-21 19:33:56 +0100

    remove export

--- src/Umbraco.Web.UI.Client/src/packages/media/media/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/index.ts
@@ -3,7 +3,6 @@ import './components/index.js';
 export * from './repository/index.js';
 export * from './workspace/index.js';
 export * from './tracked-reference/index.js';
-export * from './user-permissions/index.js';
 export * from './components/index.js';
 export * from './entity.js';
 
