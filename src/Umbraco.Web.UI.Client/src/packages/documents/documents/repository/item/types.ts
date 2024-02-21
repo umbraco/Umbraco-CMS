1090ed79c188c0c5855cefc75d1133c3af5553ec
Mads Rasmussen <madsr@hey.com>
2024-02-21 21:00:05 +0100

    add entity type to document item model

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/repository/item/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/repository/item/types.ts
@@ -1,6 +1,8 @@
+import type { UmbDocumentEntityType } from '../../entity.js';
 import type { DocumentVariantStateModel } from '@umbraco-cms/backoffice/external/backend-api';
 
 export interface UmbDocumentItemModel {
+	entityType: UmbDocumentEntityType;
 	name: string; // TODO: this is not correct. We need to get it from the variants. This is a temp solution.
 	unique: string;
 	isTrashed: boolean;
