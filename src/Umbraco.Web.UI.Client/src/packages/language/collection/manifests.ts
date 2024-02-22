b73a54139a79466b51a1e55048944dd4799fe224
Niels Lyngsø <niels.lyngso@gmail.com>
2024-02-23 00:46:35 +0100

    set more types

--- src/Umbraco.Web.UI.Client/src/packages/language/collection/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/language/collection/manifests.ts
@@ -2,11 +2,11 @@ import { UMB_LANGUAGE_COLLECTION_REPOSITORY_ALIAS } from './repository/index.js'
 import { manifests as collectionRepositoryManifests } from './repository/manifests.js';
 import { manifests as collectionViewManifests } from './views/manifests.js';
 import { manifests as collectionActionManifests } from './action/manifests.js';
-import type { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
+import type { ManifestCollection } from '@umbraco-cms/backoffice/extension-registry';
 
 export const UMB_LANGUAGE_COLLECTION_ALIAS = 'Umb.Collection.Language';
 
-const collectionManifest: ManifestTypes = {
+const collectionManifest: ManifestCollection = {
 	type: 'collection',
 	kind: 'default',
 	alias: UMB_LANGUAGE_COLLECTION_ALIAS,
