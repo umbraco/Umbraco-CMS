7a2910ca9104ed39e63f9b23a83830db951476e7
Niels Lyngsø <niels.lyngso@gmail.com>
2024-02-22 11:35:53 +0100

    move maintance of workspacePath

--- src/Umbraco.Web.UI.Client/src/packages/core/menu/menu-item/menu-item-default.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/menu/menu-item/menu-item-default.element.ts
@@ -1,18 +1,52 @@
-import { html, customElement, property } from '@umbraco-cms/backoffice/external/lit';
+import { html, customElement, property, state } from '@umbraco-cms/backoffice/external/lit';
 import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
 import type { ManifestMenuItem, UmbMenuItemElement } from '@umbraco-cms/backoffice/extension-registry';
+import { UMB_SECTION_CONTEXT } from '@umbraco-cms/backoffice/section';
 
 @customElement('umb-menu-item-default')
 export class UmbMenuItemDefaultElement extends UmbLitElement implements UmbMenuItemElement {
+	//
+	#pathname?: string;
+
 	@property({ type: Object, attribute: false })
-	manifest!: ManifestMenuItem;
+	private _manifest!: ManifestMenuItem;
+	public get manifest(): ManifestMenuItem {
+		return this._manifest;
+	}
+	public set manifest(value: ManifestMenuItem) {
+		this._manifest = value;
+		this.#constructHref();
+	}
+
+	@state()
+	private _href?: string;
+
+	constructor() {
+		super();
+
+		this.consumeContext(UMB_SECTION_CONTEXT, (sectionContext) => {
+			this.observe(
+				sectionContext?.pathname,
+				(pathname) => {
+					this.#pathname = pathname;
+					this.#constructHref();
+				},
+				'observePathname',
+			);
+		});
+	}
+
+	#constructHref() {
+		if (!this.#pathname || !this.manifest) return;
+		this._href = `section/${this.#pathname}/workspace/${this.manifest.meta.entityType}`;
+	}
 
 	render() {
 		return html`<umb-menu-item-layout
 			.label=${this.manifest.meta.label ?? this.manifest.name}
 			.iconName=${this.manifest.meta.icon ?? ''}
-			.entityType=${this.manifest.meta.entityType}></umb-menu-item-layout>`;
+			.href=${this._href}></umb-menu-item-layout>`;
 	}
 
 	static styles = [UmbTextStyles];
