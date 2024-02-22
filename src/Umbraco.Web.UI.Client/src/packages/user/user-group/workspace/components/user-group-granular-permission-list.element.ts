dc4940f0c1fca64871a40f1ecf504cca505cb3d7
Mads Rasmussen <madsr@hey.com>
2024-02-22 13:59:08 +0100

    remove unused

--- src/Umbraco.Web.UI.Client/src/packages/user/user-group/workspace/components/user-group-granular-permission-list.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user-group/workspace/components/user-group-granular-permission-list.element.ts
@@ -1,4 +1,3 @@
-import { UmbChangeEvent } from '@umbraco-cms/backoffice/event';
 import { UMB_USER_GROUP_WORKSPACE_CONTEXT } from '../user-group-workspace.context.js';
 import { createExtensionElement } from '@umbraco-cms/backoffice/extension-api';
 import type { ManifestGranularUserPermission } from '@umbraco-cms/backoffice/extension-registry';
