eb28636edba985db6fa4b4ccb381b82d40d5991e
Mads Rasmussen <madsr@hey.com>
2024-02-23 19:59:24 +0100

    remove folder

--- src/Umbraco.Web.UI.Client/src/packages/core/repository/data-source/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './data-source-response.interface.js';
--- src/Umbraco.Web.UI.Client/src/packages/core/repository/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/repository/index.ts
@@ -1,9 +1,9 @@
-export * from './data-source/index.js';
 export * from './repository-items.manager.js';
 export * from './repository-base.js';
 
 export * from './item/index.js';
 export * from './detail/index.js';
 
+export type { DataSourceResponse, UmbDataSourceErrorResponse } from './data-source-response.interface.js';
 export type { UmbMoveDataSource, UmbMoveRepository } from './move/index.js';
 export type { UmbCopyDataSource, UmbCopyRepository } from './copy/index.js';
