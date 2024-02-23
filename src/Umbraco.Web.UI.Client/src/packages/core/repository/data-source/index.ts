7f999f523359d2dc4389d250c2f9ceb573e1a937
Mads Rasmussen <madsr@hey.com>
2024-02-23 19:33:46 +0100

    remove export

--- src/Umbraco.Web.UI.Client/src/packages/core/repository/data-source/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/repository/data-source/index.ts
@@ -2,7 +2,5 @@ export * from './collection-data-source.interface.js';
 export * from './copy-data-source.interface.js';
 export * from './data-source-response.interface.js';
 export * from './data-source.interface.js';
-export * from './extend-data-source-paged-response-data.function.js';
-export * from './extend-data-source-response-data.function.js';
 export * from './move-data-source.interface.js';
 export * from './types.js';
