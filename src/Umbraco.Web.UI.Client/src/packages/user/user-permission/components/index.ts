f976c687ac035b327a75e9c6c4cfbb85b9539435
Mads Rasmussen <madsr@hey.com>
2024-02-23 14:03:37 +0100

    move component

--- src/Umbraco.Web.UI.Client/src/packages/user/user-permission/components/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user-permission/components/index.ts
@@ -1,3 +1,5 @@
 import './entity-user-permission-settings-list/entity-user-permission-settings-list.element.js';
+import './user-permission-setting/user-permission-setting.element.js';
 
 export * from './entity-user-permission-settings-list/entity-user-permission-settings-list.element.js';
+export * from './user-permission-setting/user-permission-setting.element.js';
--- src/Umbraco.Web.UI.Client/src/packages/user/user/components/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/components/index.ts
@@ -1,7 +1,6 @@
 import './user-input/user-input.element.js';
-import './user-permission-setting/user-permission-setting.element.js';
 import './user-document-start-node/user-document-start-node.element.js';
 import './user-media-start-node/user-media-start-node.element.js';
 
 export * from './user-input/user-input.element.js';
-export * from './user-permission-setting/user-permission-setting.element.js';
+export * from '../../user-permission/components/user-permission-setting/user-permission-setting.element.js';
