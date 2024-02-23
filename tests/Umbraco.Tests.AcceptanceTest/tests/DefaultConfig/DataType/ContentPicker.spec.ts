96dbd57014b0c90afe010226a1f0d2bebd88cb73
Nhu Dinh <hnd@umbraco.dk>
2024-02-23 13:35:49 +0700

    Added tests for Content Picker

---
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/DataType/ContentPicker.spec.ts
@@ -0,0 +1,42 @@
+﻿import {test} from '@umbraco/playwright-testhelpers';
+import {expect} from "@playwright/test";
+
+const dataTypeName = 'Content Picker';
+test.describe(`${dataTypeName} tests`, () => {
+  let dataTypeDefaultData = null;
+  let dataTypeData = null;
+
+  test.beforeEach(async ({umbracoUi, umbracoApi}) => {
+    await umbracoUi.goToBackOffice();
+    await umbracoUi.dataType.goToSettingsTreeItem('Data Types');
+    await umbracoUi.dataType.goToDataType(dataTypeName);
+    dataTypeDefaultData = await umbracoApi.dataType.getByName(dataTypeName);
+  });
+
+  test.afterEach(async ({umbracoApi}) => {
+    if (dataTypeDefaultData !== null) {
+      await umbracoApi.dataType.update(dataTypeDefaultData.id, dataTypeDefaultData);   
+    }   
+  });
+
+  test('can show open button', async ({umbracoApi, umbracoUi}) => {
+    // Arrange
+    const expectedDataTypeValues = {
+      alias: "showOpenButton",
+      value: true,
+    };
+
+    // Act
+    await umbracoUi.dataType.clickShowOpenButtonSlider();
+    await umbracoUi.dataType.clickSaveButton();
+
+    // Assert
+    dataTypeData = await umbracoApi.dataType.getByName(dataTypeName);
+    expect(dataTypeData.values).toContainEqual(expectedDataTypeValues);
+  });
+
+  // TODO: implement this test when the frontend is ready.
+  test.skip('can add start node', async ({umbracoApi, umbracoUi}) => {
+
+  });
+});
