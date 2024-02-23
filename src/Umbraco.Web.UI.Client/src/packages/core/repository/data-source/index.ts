86a476d12b8e03726ddf17ae5574c2102646a6e7
Mads Rasmussen <madsr@hey.com>
2024-02-23 19:41:03 +0100

    move to folders

--- src/Umbraco.Web.UI.Client/src/packages/core/repository/data-source/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/repository/data-source/index.ts
@@ -1,5 +1,3 @@
 export * from './collection-data-source.interface.js';
-export * from './copy-data-source.interface.js';
 export * from './data-source-response.interface.js';
-export * from './move-data-source.interface.js';
-export * from './types.js';
+export * from '../types.js';
--- src/Umbraco.Web.UI.Client/src/packages/core/repository/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/repository/index.ts
@@ -1,10 +1,12 @@
 export * from './data-source/index.js';
 export * from './detail/detail-repository.interface.js';
 export * from './collection-repository.interface.js';
-export * from './move-repository.interface.js';
-export * from './copy-repository.interface.js';
+export * from './move/move-repository.interface.js';
 export * from './repository-items.manager.js';
 export * from './repository-base.js';
 
 export * from './item/index.js';
 export * from './detail/index.js';
+
+export type { UmbMoveDataSource, UmbMoveRepository } from './move/index.js';
+export type { UmbCopyDataSource, UmbCopyRepository } from './copy/index.js';
