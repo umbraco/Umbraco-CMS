e61157b21c498cd3a9ab43867d21670c6f697570
Mads Rasmussen <madsr@hey.com>
2024-02-23 20:01:49 +0100

    export of paged data model

--- src/Umbraco.Web.UI.Client/src/packages/core/repository/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/repository/index.ts
@@ -7,3 +7,4 @@ export * from './detail/index.js';
 export type { DataSourceResponse, UmbDataSourceErrorResponse } from './data-source-response.interface.js';
 export type { UmbMoveDataSource, UmbMoveRepository } from './move/index.js';
 export type { UmbCopyDataSource, UmbCopyRepository } from './copy/index.js';
+export type { UmbPagedModel } from './types.js';
