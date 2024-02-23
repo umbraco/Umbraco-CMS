625b4ef8a7afd85bddab87a4db9c4c0b6d31279d
Mads Rasmussen <madsr@hey.com>
2024-02-23 19:34:23 +0100

    remove deprecated interface

--- src/Umbraco.Web.UI.Client/src/packages/core/repository/data-source/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/repository/data-source/index.ts
@@ -1,6 +1,5 @@
 export * from './collection-data-source.interface.js';
 export * from './copy-data-source.interface.js';
 export * from './data-source-response.interface.js';
-export * from './data-source.interface.js';
 export * from './move-data-source.interface.js';
 export * from './types.js';
