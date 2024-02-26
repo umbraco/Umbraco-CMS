574559cb8304d9fa2f4a058dfee26fb3b958d8e9
leekelleher <leekelleher@gmail.com>
2024-02-26 11:54:09 +0000

    Entity Action: Adds the `execute` abstract method
    Also ensures the `execute` method is on the base interface
    for both `UmbEntityAction` and `UmbEntityBulkAction`.
    
    Resolves #1288

--- src/Umbraco.Web.UI.Client/src/packages/core/entity-action/entity-action.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/entity-action/entity-action.ts
@@ -1,12 +1,16 @@
-import type { UmbAction} from '../action/index.js';
+import type { UmbAction } from '../action/index.js';
 import { UmbActionBase } from '../action/index.js';
 import type { UmbControllerHostElement } from '@umbraco-cms/backoffice/controller-api';
 
 export interface UmbEntityAction<RepositoryType> extends UmbAction<RepositoryType> {
 	unique: string;
+	execute(): Promise<void>;
 }
 
-export class UmbEntityActionBase<RepositoryType> extends UmbActionBase<RepositoryType> {
+export abstract class UmbEntityActionBase<RepositoryType>
+	extends UmbActionBase<RepositoryType>
+	implements UmbEntityAction<RepositoryType>
+{
 	entityType: string;
 	unique: string;
 	repositoryAlias: string;
@@ -17,4 +21,6 @@ export class UmbEntityActionBase<RepositoryType> extends UmbActionBase<Repositor
 		this.unique = unique;
 		this.repositoryAlias = repositoryAlias;
 	}
+
+	abstract execute(): Promise<void>;
 }
