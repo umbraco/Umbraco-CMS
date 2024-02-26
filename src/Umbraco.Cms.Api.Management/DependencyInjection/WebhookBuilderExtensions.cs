e2461b67f15e7a4db00264d105366e1563b19369
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2024-02-26 13:07:28 +0100

    Move webhook logic from backoffice to management api

---
+++ src/Umbraco.Cms.Api.Management/DependencyInjection/WebhookBuilderExtensions.cs
@@ -0,0 +1,15 @@
+﻿using Umbraco.Cms.Api.Management.Factories;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Extensions;
+
+namespace Umbraco.Cms.Api.Management.DependencyInjection;
+
+internal static class WebhookBuilderExtensions
+{
+    internal static IUmbracoBuilder AddWebhooks(this IUmbracoBuilder builder)
+    {
+        builder.Services.AddUnique<IWebhookPresentationFactory, WebhookPresentationFactory>();
+
+        return builder;
+    }
+}
