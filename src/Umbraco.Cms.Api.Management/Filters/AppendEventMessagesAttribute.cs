f3f9f5c101ad1d6da87fb550eaf70bdec69278dd
kjac <kja@umbraco.dk>
2024-02-28 08:04:37 +0100

    Output notification handler messages as a header

---
+++ src/Umbraco.Cms.Api.Management/Filters/AppendEventMessagesAttribute.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.Serialization;
+
+namespace Umbraco.Cms.Api.Management.Filters;
+
+/// <summary>
+/// When applied to a controller, this ensures that any event messages created during a request (e.g. by notification
+/// handlers) are appended to the response in a custom header.
+/// </summary>
+/// <remarks>
+/// GET operations are explicitly ignored by this, as they are not expected to generate event messages.
+/// </remarks>
+public sealed class AppendEventMessagesAttribute : TypeFilterAttribute
+{
+    public AppendEventMessagesAttribute()
+        : base(typeof(AppendEventMessagesFilter))
+    {
+    }
+
+    private class AppendEventMessagesFilter : IActionFilter
+    {
+        private readonly IEventMessagesFactory _eventMessagesFactory;
+        private readonly IJsonSerializer _jsonSerializer;
+
+        public AppendEventMessagesFilter(IEventMessagesFactory eventMessagesFactory, IJsonSerializer jsonSerializer)
+        {
+            _eventMessagesFactory = eventMessagesFactory;
+            _jsonSerializer = jsonSerializer;
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+            if (context.HttpContext.Response is null)
+            {
+                return;
+            }
+
+            if (context.HttpContext.Request.Method == HttpMethod.Get.Method)
+            {
+                return;
+            }
+
+            EventMessage[]? eventMessages = _eventMessagesFactory.GetOrDefault()?.GetAll()?.ToArray();
+            if (eventMessages is null || eventMessages.Any() is false)
+            {
+                return;
+            }
+
+            var headerContent = _jsonSerializer.Serialize(eventMessages.Select(message =>
+                new Notification
+                {
+                    Message = message.Message, Category = message.Category, Type = message.MessageType
+                }));
+
+            context.HttpContext.Response.Headers[Constants.Headers.Notifications] = headerContent;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+        }
+
+        private class Notification
+        {
+            public required string Message { get; init; }
+
+            public required string Category { get; init; }
+
+            public required EventMessageType Type { get; init; }
+        }
+    }
+}
---
+++ src/Umbraco.Cms.Api.Management/OpenApi/NotificationHeaderFilter.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using Umbraco.Cms.Core;
+
+namespace Umbraco.Cms.Api.Management.OpenApi;
+
+internal class NotificationHeaderFilter : IOperationFilter
+{
+    public void Apply(OpenApiOperation operation, OperationFilterContext context)
+    {
+        if (context.ApiDescription.HttpMethod == HttpMethod.Get.Method)
+        {
+            return;
+        }
+
+        // filter out irrelevant responses (401 will never produce notifications)
+        IEnumerable<OpenApiResponse> relevantResponses = operation
+            .Responses
+            .Where(pair => pair.Key != StatusCodes.Status401Unauthorized.ToString())
+            .Select(pair => pair.Value);
+        foreach (OpenApiResponse response in relevantResponses)
+        {
+            response.Headers.TryAdd(Constants.Headers.Notifications, new OpenApiHeader
+            {
+                Description = "The list of notifications produced during the request.",
+                Schema = new OpenApiSchema { Type = "array" }
+            });
+        }
+    }
+}
