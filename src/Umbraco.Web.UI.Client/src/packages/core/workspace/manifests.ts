16305c4ed2f57392cf97618f7a0f5d364635d97d
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-02-28 12:00:47 +0100

    register the unpublish button

--- src/Umbraco.Web.UI.Client/src/packages/core/workspace/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/workspace/manifests.ts
@@ -1,5 +1,11 @@
 import { manifests as workspaceModals } from './workspace-modal/manifests.js';
 import { manifest as workspaceAliasCondition } from './workspace-alias.condition.js';
 import { manifest as workspaceEntityTypeCondition } from './workspace-entity-type.condition.js';
+import { manifests as workspaceActionMenuItemManifests } from './components/workspace-action/manifests.js';
 
-export const manifests = [...workspaceModals, workspaceAliasCondition, workspaceEntityTypeCondition];
+export const manifests = [
+	...workspaceModals,
+	workspaceAliasCondition,
+	workspaceEntityTypeCondition,
+	...workspaceActionMenuItemManifests,
+];
