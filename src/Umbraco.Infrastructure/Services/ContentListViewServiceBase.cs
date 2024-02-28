umbraco/Umbraco-CMS

commit 4eb1ea62058bc6292371b25bebb53d811e67232c

V14: List view reference in ContentType (#15687)
* Replace all IsContainer mentions with new ListView property

* Map the list view when creating and updating

* Implement list view migration

* Refactor listview to collection

* Do not assign listview if IsContainer is false

* Fix up HasContainerInPath query

* Update OpenApi.json

* fix up according to review

* Refactor duplicate code to use extension method instead

* Dont make new guid, check for null

* Make extension internal

* Fix up after merge

* Use ReferenceById static method instead of extension

src/Umbraco.Infrastructure/Services/ContentListViewServiceBase.cs
@@ -189,7 +189,7 @@ internal abstract class ContentListViewServiceBase<TContent, TContentType, TCont
 
     private async Task<Attempt<ListViewConfiguration?, ContentCollectionOperationStatus>> GetListViewConfigurationFromContentTypeAsync(TContentType? contentType)
     {
-        if (contentType?.IsContainer == false)
+        if (contentType?.ListView is null)
         {
             return Attempt.FailWithStatus<ListViewConfiguration?, ContentCollectionOperationStatus>(ContentCollectionOperationStatus.ContentNotCollection, null);
         }

src/Umbraco.Cms.Api.Management/Factories/DocumentTypeEditingPresentationFactory.cs
@@ -28,6 +28,7 @@ internal sealed class DocumentTypeEditingPresentationFactory : ContentTypeEditin
         createModel.ContainerKey = requestModel.Folder?.Id;
         createModel.AllowedTemplateKeys = requestModel.AllowedTemplates.Select(reference => reference.Id).ToArray();
         createModel.DefaultTemplateKey = requestModel.DefaultTemplate?.Id;
+        createModel.ListView = requestModel.Collection?.Id;
         createModel.AllowedContentTypes = MapAllowedContentTypes(requestModel.AllowedDocumentTypes);
         createModel.Compositions = MapCompositions(requestModel.Compositions);
 
@@ -48,6 +49,7 @@ internal sealed class DocumentTypeEditingPresentationFactory : ContentTypeEditin
 
         updateModel.AllowedTemplateKeys = requestModel.AllowedTemplates.Select(reference => reference.Id).ToArray();
         updateModel.DefaultTemplateKey = requestModel.DefaultTemplate?.Id;
+        updateModel.ListView = requestModel.Collection?.Id;
         updateModel.AllowedContentTypes = MapAllowedContentTypes(requestModel.AllowedDocumentTypes);
         updateModel.Compositions = MapCompositions(requestModel.Compositions);
 

src/Umbraco.Cms.Api.Management/Factories/MediaTypeEditingPresentationFactory.cs
@@ -27,6 +27,7 @@ internal sealed class MediaTypeEditingPresentationFactory : ContentTypeEditingPr
         createModel.ContainerKey = requestModel.Folder?.Id;
         createModel.AllowedContentTypes = MapAllowedContentTypes(requestModel.AllowedMediaTypes);
         createModel.Compositions = MapCompositions(requestModel.Compositions);
+        createModel.ListView = requestModel.Collection?.Id;
 
         return createModel;
     }
@@ -43,6 +44,7 @@ internal sealed class MediaTypeEditingPresentationFactory : ContentTypeEditingPr
 
         updateModel.AllowedContentTypes = MapAllowedContentTypes(requestModel.AllowedMediaTypes);
         updateModel.Compositions = MapCompositions(requestModel.Compositions);
+        updateModel.ListView = requestModel.Collection?.Id;
 
         return updateModel;
     }

src/Umbraco.Cms.Api.Management/Mapping/DocumentType/DocumentTypeMapDefinition.cs
@@ -1,4 +1,5 @@
-﻿using Umbraco.Cms.Api.Management.Mapping.ContentType;
+﻿using Umbraco.Cms.Api.Management.Extensions;
+using Umbraco.Cms.Api.Management.Mapping.ContentType;
 using Umbraco.Cms.Api.Management.ViewModels;
 using Umbraco.Cms.Api.Management.ViewModels.DocumentType;
 using Umbraco.Cms.Core.Mapping;
@@ -30,6 +31,7 @@ public class DocumentTypeMapDefinition : ContentTypeMapDefinition<IContentType, 
         target.VariesByCulture = source.VariesByCulture();
         target.VariesBySegment = source.VariesBySegment();
         target.IsElement = source.IsElement;
+        target.Collection = ReferenceByIdModel.ReferenceOrNull(source.ListView);
         target.Containers = MapPropertyTypeContainers(source);
         target.Properties = MapPropertyTypes(source);
         target.AllowedDocumentTypes = source.AllowedContentTypes?.Select(ct =>
@@ -64,7 +66,7 @@ public class DocumentTypeMapDefinition : ContentTypeMapDefinition<IContentType, 
     {
         target.Id = source.Key;
         target.Icon = source.Icon ?? string.Empty;
-        target.HasListView = source.IsContainer;
+        target.Collection = ReferenceByIdModel.ReferenceOrNull(source.ListView);
     }
 
     // Umbraco.Code.MapAll
@@ -72,7 +74,7 @@ public class DocumentTypeMapDefinition : ContentTypeMapDefinition<IContentType, 
     {
         target.Id = source.Key;
         target.Icon = source.Icon ?? string.Empty;
-        target.HasListView = source.IsContainer;
+        target.Collection = ReferenceByIdModel.ReferenceOrNull(source.ListView);
     }
 
     // Umbraco.Code.MapAll

src/Umbraco.Cms.Api.Management/Mapping/MediaType/MediaTypeMapDefinition.cs
@@ -1,4 +1,5 @@
-﻿using Umbraco.Cms.Api.Management.Mapping.ContentType;
+﻿using Umbraco.Cms.Api.Management.Extensions;
+using Umbraco.Cms.Api.Management.Mapping.ContentType;
 using Umbraco.Cms.Api.Management.ViewModels;
 using Umbraco.Cms.Api.Management.ViewModels.MediaType;
 using Umbraco.Cms.Core.Mapping;
@@ -30,6 +31,8 @@ public class MediaTypeMapDefinition : ContentTypeMapDefinition<IMediaType, Media
         target.VariesByCulture = source.VariesByCulture();
         target.VariesBySegment = source.VariesBySegment();
         target.IsElement = source.IsElement;
+        target.Collection = ReferenceByIdModel.ReferenceOrNull(source.ListView);
+
         target.Containers = MapPropertyTypeContainers(source);
         target.Properties = MapPropertyTypes(source);
         target.AllowedMediaTypes = source.AllowedContentTypes?.Select(ct =>
@@ -47,7 +50,7 @@ public class MediaTypeMapDefinition : ContentTypeMapDefinition<IMediaType, Media
     {
         target.Id = source.Key;
         target.Icon = source.Icon ?? string.Empty;
-        target.HasListView = source.IsContainer;
+        target.Collection = ReferenceByIdModel.ReferenceOrNull(source.ListView);
     }
 
     // Umbraco.Code.MapAll
@@ -55,7 +58,7 @@ public class MediaTypeMapDefinition : ContentTypeMapDefinition<IMediaType, Media
     {
         target.Id = source.Key;
         target.Icon = source.Icon ?? string.Empty;
-        target.HasListView = source.IsContainer;
+        target.Collection = ReferenceByIdModel.ReferenceOrNull(source.ListView);
     }
 
     // Umbraco.Code.MapAll

src/Umbraco.Cms.Api.Management/Mapping/MemberType/MemberTypeMapDefinition.cs
@@ -16,7 +16,7 @@ public class MemberTypeMapDefinition : ContentTypeMapDefinition<IMemberType, Mem
         mapper.Define<ISimpleContentType, MemberTypeReferenceResponseModel>((_, _) => new MemberTypeReferenceResponseModel(), Map);
     }
 
-    // Umbraco.Code.MapAll
+    // Umbraco.Code.MapAll -Collection
     private void Map(IMemberType source, MemberTypeResponseModel target, MapperContext context)
     {
         target.Id = source.Key;
@@ -30,6 +30,7 @@ public class MemberTypeMapDefinition : ContentTypeMapDefinition<IMemberType, Mem
         target.IsElement = source.IsElement;
         target.Containers = MapPropertyTypeContainers(source);
         target.Properties = MapPropertyTypes(source);
+
         target.Compositions = source.ContentTypeComposition.Select(contentTypeComposition => new MemberTypeComposition
         {
             MemberType = new ReferenceByIdModel(contentTypeComposition.Key),
@@ -37,19 +38,17 @@ public class MemberTypeMapDefinition : ContentTypeMapDefinition<IMemberType, Mem
         }).ToArray();
     }
 
-    // Umbraco.Code.MapAll
+    // Umbraco.Code.MapAll -Collection
     private void Map(IMemberType source, MemberTypeReferenceResponseModel target, MapperContext context)
     {
         target.Id = source.Key;
         target.Icon = source.Icon ?? string.Empty;
-        target.HasListView = source.IsContainer;
     }
 
-    // Umbraco.Code.MapAll
+    // Umbraco.Code.MapAll -Collection
     private void Map(ISimpleContentType source, MemberTypeReferenceResponseModel target, MapperContext context)
     {
         target.Id = source.Key;
         target.Icon = source.Icon ?? string.Empty;
-        target.HasListView = source.IsContainer;
     }
 }

src/Umbraco.Cms.Api.Management/ViewModels/ContentType/ContentTypeModelBase.cs
@@ -23,6 +23,8 @@ public abstract class ContentTypeModelBase<TPropertyType, TPropertyTypeContainer
 
     public bool VariesBySegment { get; set; }
 
+    public ReferenceByIdModel? Collection { get; set; }
+
     public bool IsElement { get; set; }
 
     [Required]

src/Umbraco.Cms.Api.Management/ViewModels/ContentType/ContentTypeReferenceResponseModelBase.cs
@@ -6,6 +6,5 @@ public abstract class ContentTypeReferenceResponseModelBase
 
     public string Icon { get; set; } = string.Empty;
 
-    // FIXME: ensure that this naming is consistent with the "full" content type model representation for listviews (pending listview implementation)
-    public bool HasListView { get; set; }
+    public ReferenceByIdModel? Collection { get; set; }
 }

commit caa25d3fb0e80b1484ef8301f8084870acede7cc

Checks that `contentType` isn't `null` before checking for `.ListView`
For a collection use at the Media root, the `contentType` will be
`null`, and not have a `.ListView` configured, so would need to
fallback to the default collection configuration (data-type).

This was a small regression in PR #15687.
https://github.com/umbraco/Umbraco-CMS/pull/15687/files#diff-2ad7a39ca3a7e9fd7c4cbc72a2a28b28bf0d24c12449beff76ff8eb3b0a3ce99L192

src/Umbraco.Infrastructure/Services/ContentListViewServiceBase.cs
@@ -189,7 +189,7 @@ internal abstract class ContentListViewServiceBase<TContent, TContentType, TCont
 
     private async Task<Attempt<ListViewConfiguration?, ContentCollectionOperationStatus>> GetListViewConfigurationFromContentTypeAsync(TContentType? contentType)
     {
-        if (contentType?.ListView is null)
+        if (contentType is not null && contentType.ListView is null)
         {
             return Attempt.FailWithStatus<ListViewConfiguration?, ContentCollectionOperationStatus>(ContentCollectionOperationStatus.ContentNotCollection, null);
         }