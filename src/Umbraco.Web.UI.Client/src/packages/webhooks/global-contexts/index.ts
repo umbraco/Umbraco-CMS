ce6203eba9edbe594186314ed53742ea7c31afca
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-02-29 09:47:18 +0100

    remove global context

--- src/Umbraco.Web.UI.Client/src/packages/webhooks/global-contexts/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './app-webhook.context.js';
--- src/Umbraco.Web.UI.Client/src/packages/webhooks/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/webhooks/manifests.ts
@@ -3,7 +3,6 @@ import { manifests as treeManifests } from './menu-item/manifests.js';
 //import { manifests as entityActions } from './entity-actions/manifests.js';
 import { manifests as workspaceManifests } from './workspace/manifests.js';
 import { manifests as collectionManifests } from './collection/manifests.js';
-import { manifests as globalContextManifests } from './global-contexts/manifests.js';
 
 export const manifests = [
 	...repositoryManifests,
@@ -11,5 +10,4 @@ export const manifests = [
 	...treeManifests,
 	...workspaceManifests,
 	...collectionManifests,
-	...globalContextManifests,
 ];
