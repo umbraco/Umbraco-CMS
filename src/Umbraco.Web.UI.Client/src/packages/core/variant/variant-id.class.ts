5776406d5bff4ba7a975a7460261871df66e2116
Niels Lyngsø <niels.lyngso@gmail.com>
2024-02-29 15:31:55 +0100

    fix variant id

--- src/Umbraco.Web.UI.Client/src/packages/core/variant/variant-id.class.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/variant/variant-id.class.ts
@@ -27,7 +27,7 @@ export class UmbVariantId {
 		const split = str.split('_');
 		const culture = split[0] === UMB_INVARIANT_CULTURE ? null : split[0];
 		const segment = split[1] ?? null;
-		return Object.freeze(new UmbVariantId(segment, culture));
+		return Object.freeze(new UmbVariantId(culture, segment));
 	}
 
 	public readonly culture: string | null = null;
