a7d1167ba93ef8259dd108d57d61b1be245ec344
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2024-02-29 08:36:07 +0100

    Implement endpoints

---
+++ src/Umbraco.Cms.Api.Management/Controllers/DataType/Filter/DataTypeFilterControllerBase.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Management.Routing;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Web.Common.Authorization;
+
+namespace Umbraco.Cms.Api.Management.Controllers.DataType.Filter;
+
+[ApiController]
+[VersionedApiBackOfficeRoute($"{Constants.Web.RoutePath.Filter}/{Constants.UdiEntityType.DataType}")]
+[Authorize(Policy = "New" + AuthorizationPolicies.TreeAccessDocumentsOrDocumentTypes)]
+public abstract class DataTypeFilterControllerBase : ManagementApiControllerBase
+{
+}
---
+++ src/Umbraco.Cms.Api.Management/Controllers/DataType/Filter/FilterDataTypeFilterController.cs
@@ -0,0 +1,42 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Management.ViewModels.DataType.Item;
+using Umbraco.Cms.Core.Mapping;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Services;
+
+namespace Umbraco.Cms.Api.Management.Controllers.DataType.Filter;
+
+[ApiVersion("1.0")]
+public class FilterDataTypeFilterController : DataTypeFilterControllerBase
+{
+    private readonly IDataTypeService _dataTypeService;
+    private readonly IUmbracoMapper _mapper;
+
+    public FilterDataTypeFilterController(IDataTypeService dataTypeService, IUmbracoMapper mapper)
+    {
+        _dataTypeService = dataTypeService;
+        _mapper = mapper;
+    }
+
+    [HttpGet("{*alias}")]
+    [MapToApiVersion("1.0")]
+    [ProducesResponseType(typeof(DataTypeItemResponseModel), StatusCodes.Status200OK)]
+    public async Task<IActionResult> Filter(
+        int skip = 0,
+        int take = 100,
+        [FromQuery] string? editorUiAlias = null,
+        [FromQuery] string filter = "")
+    {
+        IEnumerable<IDataType> dataTypes = Enumerable.Empty<IDataType>();
+
+        if (editorUiAlias is not null)
+        {
+            dataTypes = await _dataTypeService.GetByEditorUiAlias(editorUiAlias);
+        }
+
+        List<DataTypeItemResponseModel> responseModels = _mapper.MapEnumerable<IDataType, DataTypeItemResponseModel>(dataTypes);
+        return Ok(responseModels);
+    }
+}
