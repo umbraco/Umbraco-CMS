4c5eeb2a3cb4a91cfc9fd6c6670599218fc84773
Mads Rasmussen <madsr@hey.com>
2024-02-29 14:19:02 +0100

    use root element in story

--- src/Umbraco.Web.UI.Client/src/packages/core/tree/tree.stories.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/tree/tree.stories.ts
@@ -1,14 +1,14 @@
 import type { Meta, StoryObj } from '@storybook/web-components';
 import './default/default-tree.element.js';
-import type { UmbDefaultTreeElement } from './default/default-tree.element.js';
+import type { UmbTreeElement } from './tree.element.js';
 
-const meta: Meta<UmbDefaultTreeElement> = {
+const meta: Meta<UmbTreeElement> = {
 	title: 'Components/Tree/Tree',
 	component: 'umb-tree',
 };
 
 export default meta;
-type Story = StoryObj<UmbDefaultTreeElement>;
+type Story = StoryObj<UmbTreeElement>;
 
 // TODO: This does not display anything - need help
 export const Overview: Story = {
