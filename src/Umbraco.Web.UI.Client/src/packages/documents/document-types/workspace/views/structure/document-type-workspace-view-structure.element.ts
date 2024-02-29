469c7850bfd10a6b70b5fe12e2f12f62cb81d0da
leekelleher <leekelleher@gmail.com>
2024-02-29 08:30:34 +0000

    Added localization keys for the Collections option
    Danish localization courtesy of Google Translate! :scream:

--- src/Umbraco.Web.UI.Client/src/packages/documents/document-types/workspace/views/structure/document-type-workspace-view-structure.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/document-types/workspace/views/structure/document-type-workspace-view-structure.element.ts
@@ -99,10 +99,8 @@ export class UmbDocumentTypeWorkspaceViewStructureElement extends UmbLitElement 
 				</umb-property-layout>
 			</uui-box>
 			<uui-box headline="Presentation">
-				<umb-property-layout alias="collection" label="Collection">
-					<div slot="description">
-						Configures the content item to show list of its children, the children will not be shown in the tree.
-					</div>
+				<umb-property-layout alias="collection" label="${this.localize.term('contentTypeEditor_collections')}">
+					<div slot="description">${this.localize.term('contentTypeEditor_collectionsDescription')}</div>
 					<div slot="editor">
 						<umb-input-collection-configuration
 							default-value="c0808dd3-8133-4e4b-8ce8-e2bea84a96a4"
