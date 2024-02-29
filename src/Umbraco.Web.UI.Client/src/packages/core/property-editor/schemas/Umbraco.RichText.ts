b6ccdab218c56bc52918ad233ad4bb5b5f4ac3a3
leekelleher <leekelleher@gmail.com>
2024-02-29 18:45:38 +0000

    Changes `mediaParentId` to use the MediaPicker UI
    and sets the config to `max=1`.

--- src/Umbraco.Web.UI.Client/src/packages/core/property-editor/schemas/Umbraco.RichText.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property-editor/schemas/Umbraco.RichText.ts
@@ -12,7 +12,8 @@ export const manifest: ManifestPropertyEditorSchema = {
 					alias: 'mediaParentId',
 					label: 'Image Upload Folder',
 					description: 'Choose the upload location of pasted images',
-					propertyEditorUiAlias: 'Umb.PropertyEditorUi.TreePicker',
+					propertyEditorUiAlias: 'Umb.PropertyEditorUi.MediaPicker',
+					config: [{ alias: 'validationLimit', value: { min: 0, max: 1 } }],
 				},
 				{
 					alias: 'ignoreUserStartNodes',
