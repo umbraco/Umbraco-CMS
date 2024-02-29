1be3fab430784aab250604cad1f4e27fd3b48be1
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-02-29 09:59:37 +0100

    remove outcommented lines

--- src/Umbraco.Web.UI.Client/src/packages/webhooks/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/webhooks/index.ts
@@ -2,6 +2,5 @@ import './components/index.js';
 
 export * from './repository/index.js';
 export * from './collection/index.js';
-export * from './global-contexts/index.js';
 
 export type { UmbWebhookDetailModel } from './types.js';
--- src/Umbraco.Web.UI.Client/src/packages/webhooks/repository/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/webhooks/repository/index.ts
@@ -1,2 +1,1 @@
 export * from './webhooks.repository.js';
-//export * from './sources/index.js';
