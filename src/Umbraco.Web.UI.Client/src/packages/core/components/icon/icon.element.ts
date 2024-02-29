10dcf0364768780e3e5fc0970c2426955f94f122
Lone Iversen <108085781+loivsen@users.noreply.github.com>
2024-02-29 15:14:52 +0100

    jsdoc

--- src/Umbraco.Web.UI.Client/src/packages/core/components/icon/icon.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/components/icon/icon.element.ts
@@ -3,6 +3,11 @@ import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
 import { html, customElement, property, state, ifDefined } from '@umbraco-cms/backoffice/external/lit';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
 
+/**
+ * @element umb-icon
+ * @description A wrapper for the uui-icon component with color alias support
+ * @extends {UmbLitElement}
+ */
 @customElement('umb-icon')
 export class UmbIconElement extends UmbLitElement {
 	@state()
