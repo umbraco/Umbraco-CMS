63a280904e2996796670289ca10d0e34ae512d20
Elitsa <elm@umbraco.dk>
2024-03-01 10:03:49 +0100

    Adding additional configuration for creation of non-existing segments

--- src/Umbraco.Core/Configuration/Models/SegmentSettings.cs
+++ src/Umbraco.Core/Configuration/Models/SegmentSettings.cs
@@ -8,10 +8,16 @@ namespace Umbraco.Cms.Core.Configuration.Models;
 public class SegmentSettings
 {
     private const bool StaticEnabled = false;
+    private const bool StaticAllowCreation = true;
 
     /// <summary>
     ///     Gets or sets a value indicating whether the usage of segments is enabled.
     /// </summary>
     [DefaultValue(StaticEnabled)]
     public bool Enabled { get; set; } = StaticEnabled;
+
+    /// <summary>
+    ///     Gets or sets a value indicating whether the creation of non-existing segments is allowed.
+    /// </summary>
+    public bool AllowCreation { get; set; } = StaticAllowCreation;
 }
