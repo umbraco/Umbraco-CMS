8fa0dc3318ac1e7b3c7c329ede6ec94947726d6c
Zeegaan <skrivdetud@gmail.com>
2024-03-01 08:43:57 +0100

    Add clarifying comments

--- src/Umbraco.Cms.Api.Management/Controllers/DataType/Filter/DataTypeFilterControllerBase.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/DataType/Filter/DataTypeFilterControllerBase.cs
@@ -9,6 +9,7 @@ namespace Umbraco.Cms.Api.Management.Controllers.DataType.Filter;
 [ApiController]
 [ApiExplorerSettings(GroupName = "Data Type")]
 [VersionedApiBackOfficeRoute($"{Constants.Web.RoutePath.Filter}/{Constants.UdiEntityType.DataType}")]
+// This auth policy might become problematic, as when getting DataTypes on Media types, you don't need access to the document tree.
 [Authorize(Policy = "New" + AuthorizationPolicies.TreeAccessDocumentsOrDocumentTypes)]
 public abstract class DataTypeFilterControllerBase : ManagementApiControllerBase
 {
