c52ff82d5b7c1e117a4c87ac618e9e37b9480cab
Mads Rasmussen <madsr@hey.com>
2024-03-02 23:38:36 +0100

    reimplement common delete entity action

--- src/Umbraco.Web.UI.Client/src/packages/core/entity-action/common/delete/delete.action.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/entity-action/common/delete/delete.action.ts
@@ -3,18 +3,58 @@ import type { UmbEntityActionArgs } from '../../types.js';
 import type { MetaEntityActionDeleteKind } from '@umbraco-cms/backoffice/extension-registry';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import { umbConfirmModal } from '@umbraco-cms/backoffice/modal';
+import { UmbExtensionApiInitializer } from '@umbraco-cms/backoffice/extension-api';
+import { umbExtensionsRegistry } from '@umbraco-cms/backoffice/extension-registry';
+import type { UmbDetailRepository, UmbItemRepository } from '@umbraco-cms/backoffice/repository';
 
 export class UmbDeleteEntityAction extends UmbEntityActionBase<MetaEntityActionDeleteKind> {
+	// TODO: make base type for item and detail models
+	#itemRepository?: UmbItemRepository<any>;
+	#detailRepository?: UmbDetailRepository<any>;
+	#init: Promise<unknown>;
+
 	constructor(host: UmbControllerHost, args: UmbEntityActionArgs<MetaEntityActionDeleteKind>) {
 		super(host, args);
+
+		this.#init = Promise.all([
+			new UmbExtensionApiInitializer(
+				this._host,
+				umbExtensionsRegistry,
+				this.args.meta.itemRepositoryAlias,
+				[this._host],
+				(permitted, ctrl) => {
+					this.#itemRepository = permitted ? (ctrl.api as UmbItemRepository<any>) : undefined;
+				},
+			).asPromise(),
+
+			new UmbExtensionApiInitializer(
+				this._host,
+				umbExtensionsRegistry,
+				this.args.meta.detailRepositoryAlias,
+				[this._host],
+				(permitted, ctrl) => {
+					this.#detailRepository = permitted ? (ctrl.api as UmbDetailRepository<any>) : undefined;
+				},
+			).asPromise(),
+		]);
 	}
 
 	async execute() {
+		if (!this.args.unique) throw new Error('Cannot delete an item without a unique identifier.');
+		await this.#init;
+
+		const { data } = await this.#itemRepository!.requestItems([this.args.unique]);
+		const item = data?.[0];
+		if (!item) throw new Error('Item not found.');
+
+		// TODO: handle items with variants
 		await umbConfirmModal(this._host, {
 			headline: `Delete`,
-			content: 'Are you sure you want to delete this item?',
+			content: `Are you sure you want to delete ${item.name}?`,
 			color: 'danger',
 			confirmLabel: 'Delete',
 		});
+
+		await this.#detailRepository!.delete(this.args.unique);
 	}
 }
