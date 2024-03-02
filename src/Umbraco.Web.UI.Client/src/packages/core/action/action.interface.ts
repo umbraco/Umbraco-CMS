7c3fb4a5b52947b684ded2323e2b8d2931c02859
Mads Rasmussen <madsr@hey.com>
2024-03-02 22:44:21 +0100

    Update action.interface.ts

--- src/Umbraco.Web.UI.Client/src/packages/core/action/action.interface.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/action/action.interface.ts
@@ -1,7 +1,8 @@
+import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import type { UmbApi } from '@umbraco-cms/backoffice/extension-api';
 
 export interface UmbAction<ArgsType> extends UmbApi {
-	public args: ArgsType;
-	public execute(): Promise<void>;
-	protected _host: UmbControllerHost;
+	args: ArgsType;
+	execute(): Promise<void>;
+	_host: UmbControllerHost;
 }
