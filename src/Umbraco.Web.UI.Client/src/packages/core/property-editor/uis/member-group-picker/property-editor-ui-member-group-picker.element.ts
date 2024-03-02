5aca7ed65c2fcc646dd5392857d4cd26ab0b8ffc
JesmoDev <jjensenmoeller@gmail.com>
2024-03-02 16:53:11 +0100

    member group picker

--- src/Umbraco.Web.UI.Client/src/packages/core/property-editor/uis/member-group-picker/property-editor-ui-member-group-picker.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property-editor/uis/member-group-picker/property-editor-ui-member-group-picker.element.ts
@@ -1,21 +1,69 @@
-import { html, customElement, property } from '@umbraco-cms/backoffice/external/lit';
+import type { PropertyValueMap } from '@umbraco-cms/backoffice/external/lit';
+import { html, customElement, property, state } from '@umbraco-cms/backoffice/external/lit';
 import type { UmbPropertyEditorUiElement } from '@umbraco-cms/backoffice/extension-registry';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
 import type { UmbPropertyEditorConfigCollection } from '@umbraco-cms/backoffice/property-editor';
+import type { UmbInputMemberGroupElement } from '@umbraco-cms/backoffice/member-group';
 
 /**
- * @element umb-property-editor-ui-member-group-picker
+ * @element umb-property-editor-ui-member-group-group-picker
  */
-@customElement('umb-property-editor-ui-member-group-picker')
+@customElement('umb-property-editor-ui-member-group-group-picker')
 export class UmbPropertyEditorUIMemberGroupPickerElement extends UmbLitElement implements UmbPropertyEditorUiElement {
-	@property()
-	value = '';
+	// private _value: Array<string> = [];
+
+	// @property({ type: Array })
+	// public get value(): Array<string> {
+	// 	return this._value;
+	// }
+	// public set value(value: Array<string>) {
+	// 	this._value = Array.isArray(value) ? value : value ? [value] : [];
+	// }
+
+	@property({ type: String })
+	public value: string = '';
 
 	@property({ attribute: false })
-	public config?: UmbPropertyEditorConfigCollection;
+	public set config(config: UmbPropertyEditorConfigCollection | undefined) {
+		const validationLimit = config?.find((x) => x.alias === 'validationLimit');
+
+		this._limitMin = (validationLimit?.value as any)?.min;
+		this._limitMax = (validationLimit?.value as any)?.max;
+	}
+
+	@state()
+	_items: Array<string> = [];
+
+	@state()
+	private _limitMin?: number;
+	@state()
+	private _limitMax?: number;
+
+	protected updated(_changedProperties: PropertyValueMap<any> | Map<PropertyKey, unknown>): void {
+		super.updated(_changedProperties);
+		if (_changedProperties.has('value')) {
+			this._items = this.value ? this.value.split(',') : [];
+		}
+	}
+
+	private _onChange(event: CustomEvent) {
+		//TODO: This is a hack, something changed so now we need to convert the array to a comma separated string to make it work with the server.
+		const toCommaSeparatedString = (event.target as UmbInputMemberGroupElement).selectedIds.join(',');
+		// this.value = (event.target as UmbInputMemberGroupElement).selectedIds;
+		this.value = toCommaSeparatedString;
+		this.dispatchEvent(new CustomEvent('property-value-change'));
+	}
 
 	render() {
-		return html`<div>umb-property-editor-ui-member-group-picker</div>`;
+		return html`
+			<umb-input-member-group
+				@change=${this._onChange}
+				.selectedIds=${this._items}
+				.min=${this._limitMin ?? 0}
+				.max=${this._limitMax ?? Infinity}
+				>Add</umb-input-member-group
+			>
+		`;
 	}
 }
 
@@ -23,6 +71,6 @@ export default UmbPropertyEditorUIMemberGroupPickerElement;
 
 declare global {
 	interface HTMLElementTagNameMap {
-		'umb-property-editor-ui-member-group-picker': UmbPropertyEditorUIMemberGroupPickerElement;
+		'umb-property-editor-ui-member-group-group-picker': UmbPropertyEditorUIMemberGroupPickerElement;
 	}
 }
