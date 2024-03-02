94ef16dc94c20a1518e661552ed445691fd9f1f9
Mads Rasmussen <madsr@hey.com>
2024-03-02 22:43:07 +0100

    rename

--- src/Umbraco.Web.UI.Client/src/packages/core/entity-action/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/entity-action/index.ts
@@ -1,6 +1,6 @@
 export * from './entity-action-list.element.js';
 export * from './entity-action.element.js';
-export * from './entity-action.js';
+export * from './entity-action-base.js';
 export * from './common/index.js';
 export * from './entity-action.event.js';
 export * from './types.js';
