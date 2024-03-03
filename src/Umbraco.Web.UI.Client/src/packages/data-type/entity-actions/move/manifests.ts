99c983980cb0cf201ad46de99ea14910a91ddc7b
Mads Rasmussen <madsr@hey.com>
2024-03-03 20:16:59 +0100

    use manifest types

--- src/Umbraco.Web.UI.Client/src/packages/data-type/entity-actions/move/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/data-type/entity-actions/move/manifests.ts
@@ -1,10 +1,10 @@
-import { UMB_DATA_TYPE_PICKER_MODAL } from '@umbraco-cms/backoffice/modal';
 import { UMB_DATA_TYPE_ENTITY_TYPE } from '../../entity.js';
 import { UMB_DATA_TYPE_ITEM_REPOSITORY_ALIAS } from '../../repository/index.js';
 import { UMB_MOVE_DATA_TYPE_REPOSITORY_ALIAS } from '../../repository/move/manifests.js';
-import type { ManifestEntityAction } from '@umbraco-cms/backoffice/extension-registry';
+import { UMB_DATA_TYPE_PICKER_MODAL } from '@umbraco-cms/backoffice/modal';
+import type { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
 
-const entityActions: Array<ManifestEntityAction> = [
+const entityActions: Array<ManifestTypes> = [
 	{
 		type: 'entityAction',
 		alias: 'Umb.EntityAction.DataType.Move',
