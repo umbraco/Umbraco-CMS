ae8d3aa8a958f35641ca1fe9f510c8e675c5e51f
Mads Rasmussen <madsr@hey.com>
2024-03-03 15:59:40 +0100

    update create actions

--- src/Umbraco.Web.UI.Client/src/packages/data-type/entity-actions/create/create.action.ts
+++ src/Umbraco.Web.UI.Client/src/packages/data-type/entity-actions/create/create.action.ts
@@ -1,22 +1,22 @@
-import type { UmbDataTypeDetailRepository } from '../../repository/detail/data-type-detail.repository.js';
 import { UMB_DATA_TYPE_CREATE_OPTIONS_MODAL } from './modal/index.js';
+import type { UmbEntityActionArgs } from '@umbraco-cms/backoffice/entity-action';
 import { UmbEntityActionBase } from '@umbraco-cms/backoffice/entity-action';
 import { UMB_MODAL_MANAGER_CONTEXT } from '@umbraco-cms/backoffice/modal';
 
-export class UmbCreateDataTypeEntityAction extends UmbEntityActionBase<UmbDataTypeDetailRepository> {
+export class UmbCreateDataTypeEntityAction extends UmbEntityActionBase<UmbEntityActionArgs<never>> {
 	async execute() {
-		if (!this.repository) throw new Error('Repository is not available');
-
 		const modalManager = await this.getContext(UMB_MODAL_MANAGER_CONTEXT);
 		const modalContext = modalManager.open(this, UMB_DATA_TYPE_CREATE_OPTIONS_MODAL, {
 			data: {
 				parent: {
-					entityType: this.entityType,
-					unique: this.unique,
+					entityType: this.args.entityType,
+					unique: this.args.unique,
 				},
 			},
 		});
 
 		await modalContext.onSubmit();
 	}
+
+	destroy(): void {}
 }
