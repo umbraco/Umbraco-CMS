5a1838326efa11d6cab50d8941e9ba835eb224d3
Niels Lyngsø <niels.lyngso@gmail.com>
2024-03-04 00:10:18 +0100

    model generics

--- src/Umbraco.Web.UI.Client/src/packages/core/extension-registry/models/workspace.model.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/extension-registry/models/workspace.model.ts
@@ -1,7 +1,8 @@
+import type { UmbControllerHostElement } from '@umbraco-cms/backoffice/controller-api';
 import type { ManifestElementAndApi, UmbApi } from '@umbraco-cms/backoffice/extension-api';
 
 // TODO: Missing Extension API Interface:
-export interface ManifestWorkspace extends ManifestElementAndApi<HTMLElement, UmbApi> {
+export interface ManifestWorkspace extends ManifestElementAndApi<UmbControllerHostElement, UmbApi> {
 	type: 'workspace';
 	meta: MetaWorkspace;
 }
