ec5dc1a1b52c16ba05d6627eebc259895267419e
Yinzy00 <yari.marien@telenet.be>
2024-03-03 11:58:02 +0100

    Added getPagedUserGroups to usergroup resource

--- src/Umbraco.Web.UI.Client/src/common/resources/usergroups.resource.js
+++ src/Umbraco.Web.UI.Client/src/common/resources/usergroups.resource.js
@@ -7,90 +7,112 @@
  * Used by the users section to get users and send requests to create, invite, delete, etc. users.
  */
 (function () {
-    'use strict';
+  'use strict';
 
-    function userGroupsResource($http, umbRequestHelper, $q, umbDataFormatter) {
+  function userGroupsResource($http, umbRequestHelper, $q, umbDataFormatter) {
 
-        function getUserGroupScaffold() {
+    function getUserGroupScaffold() {
 
-            return umbRequestHelper.resourcePromise(
-                $http.get(
-                    umbRequestHelper.getApiUrl(
-                        "userGroupsApiBaseUrl",
-                        "GetEmptyUserGroup")),
-                'Failed to get the user group scaffold');
-        }
+      return umbRequestHelper.resourcePromise(
+        $http.get(
+          umbRequestHelper.getApiUrl(
+            "userGroupsApiBaseUrl",
+            "GetEmptyUserGroup")),
+        'Failed to get the user group scaffold');
+    }
 
-        function saveUserGroup(userGroup, isNew) {
-            if (!userGroup) {
-                throw "userGroup not specified";
-            }
+    function saveUserGroup(userGroup, isNew) {
+      if (!userGroup) {
+        throw "userGroup not specified";
+      }
 
-            //need to convert the user data into the correctly formatted save data - it is *not* the same and we don't want to over-post
-            var formattedSaveData = umbDataFormatter.formatUserGroupPostData(userGroup, "save" + (isNew ? "New" : ""));
+      //need to convert the user data into the correctly formatted save data - it is *not* the same and we don't want to over-post
+      var formattedSaveData = umbDataFormatter.formatUserGroupPostData(userGroup, "save" + (isNew ? "New" : ""));
 
-            return umbRequestHelper.resourcePromise(
-                $http.post(
-                    umbRequestHelper.getApiUrl(
-                        "userGroupsApiBaseUrl",
-                        "PostSaveUserGroup"),
-                    formattedSaveData),
-                "Failed to save user group");
-        }
+      return umbRequestHelper.resourcePromise(
+        $http.post(
+          umbRequestHelper.getApiUrl(
+            "userGroupsApiBaseUrl",
+            "PostSaveUserGroup"),
+          formattedSaveData),
+        "Failed to save user group");
+    }
 
-        function getUserGroup(id) {
+    function getUserGroup(id) {
 
-            return umbRequestHelper.resourcePromise(
-                $http.get(
-                    umbRequestHelper.getApiUrl(
-                        "userGroupsApiBaseUrl",
-                        "GetUserGroup",
-                        { id: id })),
-                "Failed to retrieve data for user group " + id);
-
-        }
-
-        function getUserGroups(args) {
-
-            if (!args) {
-                args = { onlyCurrentUserGroups: true };
-            }
-            if (args.onlyCurrentUserGroups === undefined || args.onlyCurrentUserGroups === null) {
-                args.onlyCurrentUserGroups = true;
-            }
-
-            return umbRequestHelper.resourcePromise(
-                $http.get(
-                    umbRequestHelper.getApiUrl(
-                        "userGroupsApiBaseUrl",
-                        "GetUserGroups",
-                        args)),
-                "Failed to retrieve user groups");
-        }
-
-        function deleteUserGroups(userGroupIds) {
-            var query = "userGroupIds=" + userGroupIds.join("&userGroupIds=");
-            return umbRequestHelper.resourcePromise(
-                $http.post(
-                    umbRequestHelper.getApiUrl(
-                        "userGroupsApiBaseUrl",
-                        "PostDeleteUserGroups",
-                        query)),
-                'Failed to delete user groups');
-        }
-
-        var resource = {
-            saveUserGroup: saveUserGroup,
-            getUserGroup: getUserGroup,
-            getUserGroups: getUserGroups,
-            getUserGroupScaffold: getUserGroupScaffold,
-            deleteUserGroups: deleteUserGroups
-        };
-
-        return resource;
+      return umbRequestHelper.resourcePromise(
+        $http.get(
+          umbRequestHelper.getApiUrl(
+            "userGroupsApiBaseUrl",
+            "GetUserGroup",
+            { id: id })),
+        "Failed to retrieve data for user group " + id);
 
     }
 
-    angular.module('umbraco.resources').factory('userGroupsResource', userGroupsResource);
+    function getUserGroups(args) {
+
+      if (!args) {
+        args = { onlyCurrentUserGroups: true };
+      }
+      if (args.onlyCurrentUserGroups === undefined || args.onlyCurrentUserGroups === null) {
+        args.onlyCurrentUserGroups = true;
+      }
+
+      return umbRequestHelper.resourcePromise(
+        $http.get(
+          umbRequestHelper.getApiUrl(
+            "userGroupsApiBaseUrl",
+            "GetUserGroups",
+            args)),
+        "Failed to retrieve user groups");
+    }
+
+    function getPagedUserGroups(args) {
+
+      if (!args) {
+        args = { onlyCurrentUserGroups: true, page: 1 };
+      }
+      if (args.onlyCurrentUserGroups === undefined || args.onlyCurrentUserGroups === null) {
+        args.onlyCurrentUserGroups = true;
+      }
+
+      if (args.page === undefined || args.page === null)
+        args.page = 1;
+
+      return umbRequestHelper.resourcePromise(
+        $http.get(
+          umbRequestHelper.getApiUrl(
+            "userGroupsApiBaseUrl",
+            "GetPagedUserGroups",
+            args)),
+        "Failed to retrieve user groups");
+    }
+
+    function deleteUserGroups(userGroupIds) {
+      var query = "userGroupIds=" + userGroupIds.join("&userGroupIds=");
+      return umbRequestHelper.resourcePromise(
+        $http.post(
+          umbRequestHelper.getApiUrl(
+            "userGroupsApiBaseUrl",
+            "PostDeleteUserGroups",
+            query)),
+        'Failed to delete user groups');
+    }
+
+    var resource = {
+      saveUserGroup: saveUserGroup,
+      getUserGroup: getUserGroup,
+      getUserGroups: getUserGroups,
+      getUserGroupScaffold: getUserGroupScaffold,
+      deleteUserGroups: deleteUserGroups,
+      getPagedUserGroups: getPagedUserGroups
+    };
+
+    return resource;
+
+  }
+
+  angular.module('umbraco.resources').factory('userGroupsResource', userGroupsResource);
 
 })();
