229d1a21fc1e92769acea9d4b47fda38696c6af8
Mads Rasmussen <madsr@hey.com>
2024-03-03 17:05:22 +0100

    Update import.action.ts

--- src/Umbraco.Web.UI.Client/src/packages/dictionary/entity-action/import/import.action.ts
+++ src/Umbraco.Web.UI.Client/src/packages/dictionary/entity-action/import/import.action.ts
@@ -1,12 +1,13 @@
-import type { UmbDictionaryImportRepository } from '../../repository/index.js';
 import { UMB_IMPORT_DICTIONARY_MODAL } from './import-dictionary-modal.token.js';
 import { UmbEntityActionBase } from '@umbraco-cms/backoffice/entity-action';
 import { UMB_MODAL_MANAGER_CONTEXT } from '@umbraco-cms/backoffice/modal';
 
-export default class UmbImportDictionaryEntityAction extends UmbEntityActionBase<UmbDictionaryImportRepository> {
+export default class UmbImportDictionaryEntityAction extends UmbEntityActionBase<never> {
 	async execute() {
 		const modalManager = await this.getContext(UMB_MODAL_MANAGER_CONTEXT);
-		const modalContext = modalManager.open(this, UMB_IMPORT_DICTIONARY_MODAL, { data: { unique: this.unique } });
+		const modalContext = modalManager.open(this, UMB_IMPORT_DICTIONARY_MODAL, { data: { unique: this.args.unique } });
 		await modalContext.onSubmit();
 	}
+
+	destroy(): void {}
 }
