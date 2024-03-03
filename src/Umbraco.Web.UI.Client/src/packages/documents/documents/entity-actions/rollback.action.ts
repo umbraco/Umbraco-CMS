03e501ebe5faeae5df0a4113d2fbf4f8669b3995
Mads Rasmussen <madsr@hey.com>
2024-03-03 21:16:43 +0100

    Update rollback.action.ts

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/entity-actions/rollback.action.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/entity-actions/rollback.action.ts
@@ -1,14 +1,16 @@
-import type { UmbDocumentDetailRepository } from '../repository/index.js';
+import type { UmbEntityActionArgs } from '@umbraco-cms/backoffice/entity-action';
 import { UmbEntityActionBase } from '@umbraco-cms/backoffice/entity-action';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 
-export class UmbRollbackDocumentEntityAction extends UmbEntityActionBase<UmbDocumentDetailRepository> {
-	constructor(host: UmbControllerHost, repositoryAlias: string, unique: string, entityType: string) {
-		super(host, repositoryAlias, unique, entityType);
+export class UmbRollbackDocumentEntityAction extends UmbEntityActionBase<never> {
+	constructor(host: UmbControllerHost, args: UmbEntityActionArgs<never>) {
+		super(host, args);
 	}
 
 	async execute() {
-		console.log(`execute for: ${this.unique}`);
+		console.log(`execute for: ${this.args.unique}`);
 		//await this.repository?.rollback();
 	}
+
+	destroy(): void {}
 }
