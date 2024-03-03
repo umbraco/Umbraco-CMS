f003fd3977ffde3be41b0b537515ea36c3f2aced
Niels Lyngsø <niels.lyngso@gmail.com>
2024-03-03 20:34:11 +0100

    generic type extends for UmbEntityBulkActionArgs

--- src/Umbraco.Web.UI.Client/src/packages/core/entity-bulk-action/entity-bulk-action.interface.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/entity-bulk-action/entity-bulk-action.interface.ts
@@ -1,7 +1,9 @@
 import type { UmbEntityBulkActionArgs } from './types.js';
+import type { MetaEntityBulkAction } from '@umbraco-cms/backoffice/extension-registry';
 import type { UmbAction } from '@umbraco-cms/backoffice/action';
 
-export interface UmbEntityBulkAction<ArgsMetaType> extends UmbAction<UmbEntityBulkActionArgs<ArgsMetaType>> {
+export interface UmbEntityBulkAction<ArgsMetaType extends MetaEntityBulkAction>
+	extends UmbAction<UmbEntityBulkActionArgs<ArgsMetaType>> {
 	selection: Array<string>;
 
 	// I don't think we need this one, now that we have the above one? [NL]
--- src/Umbraco.Web.UI.Client/src/packages/core/entity-bulk-action/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/entity-bulk-action/types.ts
@@ -1,4 +1,6 @@
-export interface UmbEntityBulkActionArgs<MetaArgsType> {
+import type { MetaEntityBulkAction } from '../extension-registry/models/entity-bulk-action.model.js';
+
+export interface UmbEntityBulkActionArgs<MetaArgsType extends MetaEntityBulkAction> {
 	entityType: string;
 	meta: MetaArgsType;
 }
