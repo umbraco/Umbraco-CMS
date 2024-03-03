55d8954b0ccca785a7696a3e0b3b587693fbd7a4
Kenn Jacobsen <kja@umbraco.dk>
2024-03-03 15:53:50 +0100

    Align parent contexts (#15819)
    * Remove explicit parent context in API outputs
    
    * Rename "folder" to "parent" for consistency
    
    * Regenerate OpenApi.json

--- src/Umbraco.Cms.Api.Management/Mapping/DataType/DataTypeViewModelMapDefinition.cs
+++ src/Umbraco.Cms.Api.Management/Mapping/DataType/DataTypeViewModelMapDefinition.cs
@@ -1,20 +1,13 @@
-﻿using Umbraco.Cms.Api.Management.ViewModels;
-using Umbraco.Cms.Api.Management.ViewModels.DataType;
+﻿using Umbraco.Cms.Api.Management.ViewModels.DataType;
 using Umbraco.Cms.Core.Mapping;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.PropertyEditors;
-using Umbraco.Cms.Core.Services;
 using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Api.Management.Mapping.DataType;
 
 public class DataTypeViewModelMapDefinition : IMapDefinition
 {
-    private readonly IDataTypeService _dataTypeService;
-
-    public DataTypeViewModelMapDefinition(IDataTypeService dataTypeService)
-        => _dataTypeService = dataTypeService;
-
     public void DefineMaps(IUmbracoMapper mapper)
         => mapper.Define<IDataType, DataTypeResponseModel>((_, _) => new DataTypeResponseModel(), Map);
 
@@ -22,8 +15,6 @@ public class DataTypeViewModelMapDefinition : IMapDefinition
     private void Map(IDataType source, DataTypeResponseModel target, MapperContext context)
     {
         target.Id = source.Key;
-        Guid? parentId = _dataTypeService.GetContainer(source.ParentId)?.Key;
-        target.Parent = ReferenceByIdModel.ReferenceOrNull(parentId);
         target.Name = source.Name ?? string.Empty;
         target.EditorAlias = source.EditorAlias;
         target.EditorUiAlias = source.EditorUiAlias;
