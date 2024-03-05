361bf80a4e97581fdf3635901705576792496690
Nikolaj <nikolajlauridsen@protonmail.ch>
2024-03-05 09:51:06 +0100

    Don't re-implement operation results

--- src/Umbraco.Cms.Api.Management/Controllers/User/UpdateUserGroupsUserController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/User/UpdateUserGroupsUserController.cs
@@ -2,7 +2,8 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Umbraco.Cms.Api.Common.Builders;
+using Umbraco.Cms.Api.Management.Controllers.UserGroup;
+using Umbraco.Cms.Api.Management.Routing;
 using Umbraco.Cms.Api.Management.ViewModels.User;
 using Umbraco.Cms.Core.Security.Authorization;
 using Umbraco.Cms.Core.Services;
@@ -12,8 +13,14 @@ using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Api.Management.Controllers.User;
 
+// This controller is a bit of a weird case, for all intents and purposes this should be a UserGroupController
+// It uses the UserGroupService to manipulate the members of a user group, however, from the frontend perspective it is a user(s) operation
+// In order to not have to re-implement all the UserGroupOperationStatusResults this controller inherits from UserGroupControllerBase
+// But manually specifies its route and APIExplorerSettings to be under users.
 [ApiVersion("1.0")]
-public class UpdateUserGroupsUserController : UserControllerBase
+[VersionedApiBackOfficeRoute("user")]
+[ApiExplorerSettings(GroupName = "User")]
+public class UpdateUserGroupsUserController : UserGroupControllerBase
 {
     private readonly IAuthorizationService _authorizationService;
     private readonly IUserGroupService _userGroupService;
@@ -41,17 +48,6 @@ public class UpdateUserGroupsUserController : UserControllerBase
 
         UserGroupOperationStatus status = await _userGroupService.UpdateUserGroupsOnUsersAsync(requestModel.UserGroupIds, requestModel.UserIds);
 
-        // This is a bit of weird case, since it's really more of a user group operation, so our base method does not really work.
-        return status switch
-        {
-            UserGroupOperationStatus.Success => Ok(),
-            UserGroupOperationStatus.AdminGroupCannotBeEmpty => BadRequest(new ProblemDetailsBuilder()
-                .WithTitle("Admin group cannot be empty")
-                .WithDetail("The admin group cannot be empty.")
-                .Build()),
-            _ => StatusCode(StatusCodes.Status500InternalServerError, new ProblemDetailsBuilder()
-                .WithTitle("Unknown user group operation status.")
-                .Build()),
-        };
+        return UserGroupOperationStatusResult(status);
     }
 }
