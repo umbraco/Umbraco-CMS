4c2e6b6d84a97a226277928efa73544b3b907a70
Mads Rasmussen <madsr@hey.com>
2024-03-05 08:37:53 +0100

    move template picker token to template module

--- src/Umbraco.Web.UI.Client/src/packages/core/modal/token/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/modal/token/index.ts
@@ -21,5 +21,4 @@ export * from './permissions-modal.token.js';
 export * from './property-editor-ui-picker-modal.token.js';
 export * from './property-settings-modal.token.js';
 export * from './section-picker-modal.token.js';
-export * from './template-picker-modal.token.js';
 export * from './workspace-modal.token.js';
