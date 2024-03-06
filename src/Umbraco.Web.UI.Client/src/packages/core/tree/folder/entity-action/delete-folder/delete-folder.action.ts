3c85eb65e8cb712cf26d2cf4501863c3f20983e8
Mads Rasmussen <madsr@hey.com>
2024-03-06 08:12:28 +0100

    request reload when deleting a folder

--- src/Umbraco.Web.UI.Client/src/packages/core/tree/folder/entity-action/delete-folder/delete-folder.action.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/tree/folder/entity-action/delete-folder/delete-folder.action.ts
@@ -1,6 +1,8 @@
+import { UMB_ACTION_EVENT_CONTEXT } from '@umbraco-cms/backoffice/action';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import type { UmbEntityActionArgs } from '@umbraco-cms/backoffice/entity-action';
 import { UmbEntityActionBase } from '@umbraco-cms/backoffice/entity-action';
+import { UmbRequestReloadStructureForEntityEvent } from '@umbraco-cms/backoffice/event';
 import { UmbExtensionApiInitializer } from '@umbraco-cms/backoffice/extension-api';
 import type { MetaEntityActionFolderKind } from '@umbraco-cms/backoffice/extension-registry';
 import { umbExtensionsRegistry } from '@umbraco-cms/backoffice/extension-registry';
@@ -46,6 +48,14 @@ export class UmbDeleteFolderEntityAction extends UmbEntityActionBase<MetaEntityA
 			});
 
 			await this.#folderRepository?.delete(this.args.unique);
+
+			const actionEventContext = await this.getContext(UMB_ACTION_EVENT_CONTEXT);
+			const event = new UmbRequestReloadStructureForEntityEvent({
+				unique: this.args.unique,
+				entityType: this.args.entityType,
+			});
+
+			actionEventContext.dispatchEvent(event);
 		}
 	}
 }
