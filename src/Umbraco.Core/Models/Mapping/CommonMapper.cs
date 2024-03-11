69a691f315daba382545ffd677886b940bb00acb
Kenn Jacobsen <kja@umbraco.dk>
2024-03-11 13:15:25 +0100

    Removed unused content editing models (#15855)

--- src/Umbraco.Core/Models/Mapping/CommonMapper.cs
+++ src/Umbraco.Core/Models/Mapping/CommonMapper.cs
@@ -1,44 +1,22 @@
 using Umbraco.Cms.Core.Mapping;
-using Umbraco.Cms.Core.Models.ContentEditing;
 using Umbraco.Cms.Core.Models.Membership;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Extensions;
-using UserProfile = Umbraco.Cms.Core.Models.ContentEditing.UserProfile;
 
 namespace Umbraco.Cms.Core.Models.Mapping;
 
 public class CommonMapper
 {
-    private readonly IContentTypeBaseServiceProvider _contentTypeBaseServiceProvider;
-    private readonly ILocalizedTextService _localizedTextService;
     private readonly IUserService _userService;
 
-    public CommonMapper(
-        IUserService userService,
-        IContentTypeBaseServiceProvider contentTypeBaseServiceProvider,
-        ILocalizedTextService localizedTextService)
+    public CommonMapper(IUserService userService)
     {
         _userService = userService;
-        _contentTypeBaseServiceProvider = contentTypeBaseServiceProvider;
-        _localizedTextService = localizedTextService;
     }
 
-    public UserProfile? GetOwner(IContentBase source, MapperContext context)
-    {
-        IProfile? profile = source.GetCreatorProfile(_userService);
-        return profile == null ? null : context.Map<IProfile, UserProfile>(profile);
-    }
+    public string? GetOwnerName(IContentBase source, MapperContext context)
+        => source.GetCreatorProfile(_userService)?.Name;
 
-    public UserProfile? GetCreator(IContent source, MapperContext context)
-    {
-        IProfile? profile = source.GetWriterProfile(_userService);
-        return profile == null ? null : context.Map<IProfile, UserProfile>(profile);
-    }
-
-    public ContentTypeBasic? GetContentType(IContentBase source, MapperContext context)
-    {
-        IContentTypeComposition? contentType = _contentTypeBaseServiceProvider.GetContentTypeOf(source);
-        ContentTypeBasic? contentTypeBasic = context.Map<IContentTypeComposition, ContentTypeBasic>(contentType);
-        return contentTypeBasic;
-    }
+    public string? GetCreatorName(IContent source, MapperContext context)
+        => source.GetWriterProfile(_userService)?.Name;
 }
