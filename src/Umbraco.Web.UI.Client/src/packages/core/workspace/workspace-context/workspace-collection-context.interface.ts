0adec22aaacb6a58267dfc5e861d791e5cd6b510
leekelleher <leekelleher@gmail.com>
2024-03-11 18:43:33 +0000

    Adds Workspace Collection Context interface and token

---
+++ src/Umbraco.Web.UI.Client/src/packages/core/workspace/workspace-context/workspace-collection-context.interface.ts
@@ -0,0 +1,10 @@
+import type { UmbWorkspaceContextInterface } from './workspace-context.interface.js';
+import type { Observable } from '@umbraco-cms/backoffice/external/rxjs';
+import type { UmbContentTypeModel, UmbContentTypePropertyStructureManager } from '@umbraco-cms/backoffice/content-type';
+
+export interface UmbWorkspaceCollectionContextInterface<T extends UmbContentTypeModel>
+	extends UmbWorkspaceContextInterface {
+	contentTypeHasCollection: Observable<boolean>;
+	getCollectionAlias(): string;
+	structure: UmbContentTypePropertyStructureManager<T>;
+}
