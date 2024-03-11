01a1a6b945d04adc2b160476975008f11c4d9e30
Mads Rasmussen <madsr@hey.com>
2024-03-11 21:59:58 +0100

    remove tree store from data type move repo

--- src/Umbraco.Web.UI.Client/src/packages/data-type/repository/move/data-type-move.repository.ts
+++ src/Umbraco.Web.UI.Client/src/packages/data-type/repository/move/data-type-move.repository.ts
@@ -1,16 +1,13 @@
-import type { UmbDataTypeTreeStore } from '../../tree/data-type-tree.store.js';
-import { UMB_DATA_TYPE_TREE_STORE_CONTEXT } from '../../tree/data-type-tree.store.js';
 import { UmbDataTypeMoveServerDataSource } from './data-type-move.server.data-source.js';
 import type { UmbNotificationContext } from '@umbraco-cms/backoffice/notification';
 import { UMB_NOTIFICATION_CONTEXT } from '@umbraco-cms/backoffice/notification';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
-import type { UmbMoveDataSource, UmbMoveRepository} from '@umbraco-cms/backoffice/repository';
+import type { UmbMoveDataSource, UmbMoveRepository } from '@umbraco-cms/backoffice/repository';
 import { UmbRepositoryBase } from '@umbraco-cms/backoffice/repository';
 
 export class UmbMoveDataTypeRepository extends UmbRepositoryBase implements UmbMoveRepository {
 	#init: Promise<unknown>;
 	#moveSource: UmbMoveDataSource;
-	#treeStore?: UmbDataTypeTreeStore;
 	#notificationContext?: UmbNotificationContext;
 
 	constructor(host: UmbControllerHost) {
@@ -18,10 +15,6 @@ export class UmbMoveDataTypeRepository extends UmbRepositoryBase implements UmbM
 		this.#moveSource = new UmbDataTypeMoveServerDataSource(this);
 
 		this.#init = Promise.all([
-			this.consumeContext(UMB_DATA_TYPE_TREE_STORE_CONTEXT, (instance) => {
-				this.#treeStore = instance;
-			}).asPromise(),
-
 			this.consumeContext(UMB_NOTIFICATION_CONTEXT, (instance) => {
 				this.#notificationContext = instance;
 			}).asPromise(),
@@ -33,9 +26,6 @@ export class UmbMoveDataTypeRepository extends UmbRepositoryBase implements UmbM
 		const { error } = await this.#moveSource.move(unique, targetUnique);
 
 		if (!error) {
-			// TODO: Be aware about this responsibility.
-			this.#treeStore!.updateItem(unique, { parentUnique: targetUnique });
-
 			const notification = { data: { message: `Data type moved` } };
 			this.#notificationContext!.peek('positive', notification);
 		}
