c488e984c0da383bd59d4d8591ee0c39b67650d8
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-03-12 14:16:45 +0100

    remove controller

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/modals/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/modals/manifests.ts
@@ -1,20 +1,13 @@
 import type { ManifestModal } from '@umbraco-cms/backoffice/extension-registry';
 
-export const UMB_DOCUMENT_VARIANT_PICKER_MODAL_ALIAS = 'Umb.Modal.DocumentVariantPicker';
 export const UMB_DOCUMENT_PUBLISH_MODAL_ALIAS = 'Umb.Modal.DocumentPublish';
 
 const modals: Array<ManifestModal> = [
 	{
 		type: 'modal',
-		alias: UMB_DOCUMENT_VARIANT_PICKER_MODAL_ALIAS,
-		name: 'Document Variant Picker Modal',
-		js: () => import('./variant-picker/document-variant-picker-modal.element.js'),
-	},
-	{
-		type: 'modal',
 		alias: UMB_DOCUMENT_PUBLISH_MODAL_ALIAS,
 		name: 'Document Publish Modal',
-		js: () => import('./variant-picker/document-variant-picker-modal.element.js'),
+		js: () => import('./publish-modal/document-publish-modal.element.js'),
 	},
 ];
 
