3c921ae0cad5542723913ed3a06452616ddf54e8
Elitsa <elm@umbraco.dk>
2024-03-13 15:07:24 +0100

    Bring AuthorizationPolicies.DenyLocalLoginIfConfigured authz policy to an action level as it doesn't apply to all

--- src/Umbraco.Cms.Api.Management/Controllers/Security/BackOfficeController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/Security/BackOfficeController.cs
@@ -2,6 +2,7 @@
 using Asp.Versioning;
 using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
@@ -15,6 +16,7 @@ using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Configuration.Models;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Security;
+using Umbraco.Cms.Web.Common.Authorization;
 using Umbraco.Cms.Web.Common.Security;
 using Umbraco.Extensions;
 using IdentitySignInResult = Microsoft.AspNetCore.Identity.SignInResult;
@@ -51,6 +53,7 @@ public class BackOfficeController : SecurityControllerBase
     //        once the old backoffice auth is no longer necessary, clean this up and merge with 2FA handling etc.
     [HttpPost("login")]
     [MapToApiVersion("1.0")]
+    [Authorize(Policy = AuthorizationPolicies.DenyLocalLoginIfConfigured)]
     public async Task<IActionResult> Login(LoginRequestModel model)
     {
         var validated = await _backOfficeUserManager.ValidateCredentialsAsync(model.Username, model.Password);
@@ -86,6 +89,7 @@ public class BackOfficeController : SecurityControllerBase
         return Ok();
     }
 
+    [AllowAnonymous]
     [HttpPost("verify-2fa")]
     [MapToApiVersion("1.0")]
     public async Task<IActionResult> Verify2FACode(Verify2FACodeModel model)
@@ -136,6 +140,7 @@ public class BackOfficeController : SecurityControllerBase
         public required string Password { get; init; }
     }
 
+    [AllowAnonymous]
     [HttpGet("authorize")]
     [MapToApiVersion("1.0")]
     public async Task<IActionResult> Authorize()
@@ -158,6 +163,7 @@ public class BackOfficeController : SecurityControllerBase
             : await AuthorizeExternal(request);
     }
 
+    [AllowAnonymous]
     [HttpGet("signout")]
     [MapToApiVersion("1.0")]
     public async Task<IActionResult> Signout()
