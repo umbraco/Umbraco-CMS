99bad60f811e8dbb47f9b960ead8f36b9420640c
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-03-14 11:26:15 +0100

    remove exports

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/workspace/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/workspace/index.ts
@@ -1,6 +1,2 @@
 export * from './document-workspace.context-token.js';
 export * from './document-workspace.context.js';
-export * from './actions/save-and-preview.action.js';
-export * from './actions/save-and-publish.action.js';
-export * from './actions/save-and-schedule.action.js';
-export * from './actions/unpublish.action.js';
