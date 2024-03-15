14fdf07e67bf431a54b2e1625f5da2ae8a6438c8
Zeegaan <skrivdetud@gmail.com>
2024-03-15 12:19:23 +0100

    Use await in async method

--- src/Umbraco.Cms.Api.Management/Install/CreateUnattendedUserNotificationHandler.cs
+++ src/Umbraco.Cms.Api.Management/Install/CreateUnattendedUserNotificationHandler.cs
@@ -52,7 +52,7 @@ public class CreateUnattendedUserNotificationHandler : INotificationAsyncHandler
             return;
         }
 
-        IUser? admin = _userService.GetAsync(Constants.Security.SuperUserKey).GetAwaiter().GetResult();
+        IUser? admin = await _userService.GetAsync(Constants.Security.SuperUserKey);
         if (admin == null)
         {
             throw new InvalidOperationException("Could not find the super user!");
