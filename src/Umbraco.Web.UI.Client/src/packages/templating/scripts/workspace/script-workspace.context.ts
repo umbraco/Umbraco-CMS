214672865406b056f832861676567e46c618076e
Mads Rasmussen <madsr@hey.com>
2024-03-18 19:37:45 +0100

    make parent observable

--- src/Umbraco.Web.UI.Client/src/packages/templating/scripts/workspace/script-workspace.context.ts
+++ src/Umbraco.Web.UI.Client/src/packages/templating/scripts/workspace/script-workspace.context.ts
@@ -13,7 +13,8 @@ import { UmbRequestReloadStructureForEntityEvent } from '@umbraco-cms/backoffice
 export class UmbScriptWorkspaceContext extends UmbEditableWorkspaceContextBase<UmbScriptDetailModel> {
 	public readonly repository = new UmbScriptDetailRepository(this);
 
-	#parent?: { entityType: string; unique: string | null };
+	#parent = new UmbObjectState<{ entityType: string; unique: string | null } | undefined>(undefined);
+	readonly parentUnique = this.#parent.asObservablePart((parent) => (parent ? parent.unique : undefined));
 
 	#data = new UmbObjectState<UmbScriptDetailModel | undefined>(undefined);
 
@@ -78,7 +79,7 @@ export class UmbScriptWorkspaceContext extends UmbEditableWorkspaceContextBase<U
 
 	async create(parent: { entityType: string; unique: string | null }) {
 		this.resetState();
-		this.#parent = parent;
+		this.#parent.setValue(parent);
 		const { data } = await this.repository.createScaffold();
 
 		if (data) {
@@ -93,15 +94,16 @@ export class UmbScriptWorkspaceContext extends UmbEditableWorkspaceContextBase<U
 		let newData = undefined;
 
 		if (this.getIsNew()) {
-			if (!this.#parent) throw new Error('Parent is not set');
-			const { data } = await this.repository.create(this.#data.value, this.#parent.unique);
+			const parent = this.#parent.getValue();
+			if (!parent) throw new Error('Parent is not set');
+			const { data } = await this.repository.create(this.#data.value, parent.unique);
 			newData = data;
 
 			// TODO: this might not be the right place to alert the tree, but it works for now
 			const eventContext = await this.getContext(UMB_ACTION_EVENT_CONTEXT);
 			const event = new UmbReloadTreeItemChildrenRequestEntityActionEvent({
-				entityType: this.#parent.entityType,
-				unique: this.#parent.unique,
+				entityType: parent.entityType,
+				unique: parent.unique,
 			});
 			eventContext.dispatchEvent(event);
 		} else {
