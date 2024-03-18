306c20173ce3ba620927e39957d6a4932e5babaf
Mads Rasmussen <madsr@hey.com>
2024-03-18 20:57:30 +0100

    implement breadcrumb for document types

--- src/Umbraco.Web.UI.Client/src/packages/documents/document-types/tree/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/document-types/tree/index.ts
@@ -1,2 +1,3 @@
 export { UMB_DOCUMENT_TYPE_TREE_STORE_CONTEXT } from './document-type.tree.store.js';
+export { UMB_DOCUMENT_TYPE_TREE_REPOSITORY_ALIAS } from './manifests.js';
 export * from './folder/index.js';
--- src/Umbraco.Web.UI.Client/src/packages/documents/document-types/workspace/document-type-workspace.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/document-types/workspace/document-type-workspace.element.ts
@@ -4,6 +4,8 @@ import { html, customElement, state } from '@umbraco-cms/backoffice/external/lit
 import type { UmbRoute } from '@umbraco-cms/backoffice/router';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
 import { UmbWorkspaceIsNewRedirectController } from '@umbraco-cms/backoffice/workspace';
+import { UmbExtensionsApiInitializer } from '@umbraco-cms/backoffice/extension-api';
+import { umbExtensionsRegistry } from '@umbraco-cms/backoffice/extension-registry';
 
 @customElement('umb-document-type-workspace')
 export class UmbDocumentTypeWorkspaceElement extends UmbLitElement {
@@ -37,6 +39,12 @@ export class UmbDocumentTypeWorkspaceElement extends UmbLitElement {
 			},
 		},
 	];
+
+	constructor() {
+		super();
+		// TODO: We need to recreate when ID changed?
+		new UmbExtensionsApiInitializer(this, umbExtensionsRegistry, 'workspaceContext', [this, this.#workspaceContext]);
+	}
 
 	render() {
 		return html` <umb-router-slot .routes=${this._routes}></umb-router-slot> `;
