1397ea2aa728f3f03bdb5f183de495d6f659d2e8
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-03-19 11:36:51 +0100

    fix fallback text

--- src/Umbraco.Web.UI.Login/src/components/pages/mfa.page.element.ts
+++ src/Umbraco.Web.UI.Login/src/components/pages/mfa.page.element.ts
@@ -154,14 +154,12 @@ export default class UmbMfaPageElement extends LitElement {
               autocomplete="one-time-code"
               placeholder=${until(
                 umbLocalizationContext.localize('login_2faCodeInputHelp'),
-                undefined,
                 'Please enter the verification code'
               )}
               aria-required="true"
               required
               required-message=${until(
                 umbLocalizationContext.localize('login_2faCodeInputHelp'),
-                undefined,
                 'Please enter the verification code'
               )}
               style="width:100%;">
