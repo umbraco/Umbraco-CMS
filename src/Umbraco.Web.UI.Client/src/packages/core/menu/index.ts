7c57fefaeb457bb28c6fa52c6e583b0c827fcb01
Mads Rasmussen <madsr@hey.com>
2024-03-19 15:33:13 +0100

    add interface

--- src/Umbraco.Web.UI.Client/src/packages/core/menu/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/menu/index.ts
@@ -5,3 +5,5 @@ export * from './menu.context.js';
 export * from './menu-tree-structure-workspace-context-base.js';
 export * from './menu-variant-tree-structure-workspace-context-base.js';
 export * from './types.js';
+
+export type { UmbMenuStructureWorkspaceContext } from './menu-structure-workspace-context.interface.js';
