95b9e5ad19f5b6b3e63210ad9b3d80ca51b74740
Bjarke Berg <mail@bergmania.dk>
2024-03-19 10:37:17 +0100

    Added password configuration to the verify responses, so the client knows, and have confirmed the user is allwed to see it

--- src/Umbraco.Cms.Api.Management/Controllers/User/VerifyInviteUserController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/User/VerifyInviteUserController.cs
@@ -2,6 +2,7 @@
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Management.Factories;
 using Umbraco.Cms.Api.Management.ViewModels.User;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Services;
@@ -13,13 +14,18 @@ namespace Umbraco.Cms.Api.Management.Controllers.User;
 public class VerifyInviteUserController : UserControllerBase
 {
     private readonly IUserService _userService;
+    private readonly IPasswordConfigurationPresentationFactory _passwordConfigurationPresentationFactory;
 
-    public VerifyInviteUserController(IUserService userService) => _userService = userService;
+    public VerifyInviteUserController(IUserService userService, IPasswordConfigurationPresentationFactory passwordConfigurationPresentationFactory)
+    {
+        _userService = userService;
+        _passwordConfigurationPresentationFactory = passwordConfigurationPresentationFactory;
+    }
 
     [AllowAnonymous]
     [HttpPost("invite/verify")]
     [MapToApiVersion("1.0")]
-    [ProducesResponseType(StatusCodes.Status200OK)]
+    [ProducesResponseType(typeof(VerifyInviteUserResponseModel), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status400BadRequest)]
     public async Task<IActionResult> Invite(VerifyInviteUserRequestModel model)
@@ -27,7 +33,10 @@ public class VerifyInviteUserController : UserControllerBase
         Attempt<UserOperationStatus> result = await _userService.VerifyInviteAsync(model.User.Id, model.Token);
 
         return result.Success
-            ? Ok()
+            ? Ok(new VerifyInviteUserResponseModel()
+            {
+                PasswordConfiguration = _passwordConfigurationPresentationFactory.CreatePasswordConfigurationResponseModel(),
+            })
             : UserOperationStatusResult(result.Result);
     }
 }
