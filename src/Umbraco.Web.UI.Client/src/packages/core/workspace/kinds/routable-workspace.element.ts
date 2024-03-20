3f125a4b8259cd6065f507cd7cf6f5f17016d3ac
Niels Lyngsø <niels.lyngso@gmail.com>
2024-03-20 10:04:06 +0100

    init workspace context as part of the kind

--- src/Umbraco.Web.UI.Client/src/packages/core/workspace/kinds/routable-workspace.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/workspace/kinds/routable-workspace.element.ts
@@ -1,7 +1,9 @@
 import type { UmbRoutableWorkspaceContext } from '../contexts/routable-workspace-context.interface.js';
+import { umbExtensionsRegistry } from '@umbraco-cms/backoffice/extension-registry';
 import { html, customElement, state } from '@umbraco-cms/backoffice/external/lit';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
 import type { UmbRoute } from '@umbraco-cms/backoffice/router';
+import { UmbExtensionsApiInitializer } from '@umbraco-cms/backoffice/extension-api';
 
 @customElement('umb-routable-workspace')
 export class UmbRoutableWorkspaceElement extends UmbLitElement {
@@ -10,6 +12,8 @@ export class UmbRoutableWorkspaceElement extends UmbLitElement {
 
 	public set api(api: UmbRoutableWorkspaceContext) {
 		this.observe(api.routes.routes, (routes) => (this._routes = routes));
+
+		new UmbExtensionsApiInitializer(this, umbExtensionsRegistry, 'workspaceContext', [api]);
 	}
 
 	render() {
