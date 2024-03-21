788d738c552a0e94719a5e0fb6bf15e5bb57a301
Mads Rasmussen <madsr@hey.com>
2024-03-21 12:20:49 +0100

    remove deleted import

--- src/Umbraco.Web.UI.Client/src/packages/relations/relation-types/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/relations/relation-types/manifests.ts
@@ -1,11 +1,5 @@
 import { manifests as repositoryManifests } from './repository/detail/manifests.js';
 import { manifests as menuItemManifests } from './menu-item/manifests.js';
 import { manifests as workspaceManifests } from './workspace/manifests.js';
-import { manifests as entityActionManifests } from './entity-actions/manifests.js';
 
-export const manifests = [
-	...repositoryManifests,
-	...menuItemManifests,
-	...workspaceManifests,
-	...entityActionManifests,
-];
+export const manifests = [...repositoryManifests, ...menuItemManifests, ...workspaceManifests];
