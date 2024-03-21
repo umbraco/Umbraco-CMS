34be6e4e9dab07d62e01019be139c4c9c3d822d8
Mads Rasmussen <madsr@hey.com>
2024-03-21 10:16:33 +0100

    setup detail model

---
+++ src/Umbraco.Web.UI.Client/src/packages/relations/relation-types/types.ts
@@ -0,0 +1,23 @@
+import type { UmbRelationTypeEntityType } from './entity.js';
+
+export interface UmbRelationTypeDetailModel {
+	alias: string;
+	child: {
+		objectType: {
+			unique: string;
+			name: string;
+		};
+	} | null;
+	entityType: UmbRelationTypeEntityType;
+	isBidirectional: boolean;
+	isDeletable: boolean;
+	isDependency: boolean;
+	name: string;
+	parent: {
+		objectType: {
+			unique: string;
+			name: string;
+		};
+	} | null;
+	unique: string;
+}
