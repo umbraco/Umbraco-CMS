ea946accc155b11488bacc3b4350af9fc1af8323
nikolajlauridsen <nikolajlauridsen@protonmail.ch>
2024-03-21 09:53:32 +0100

    Align umbracoContentVersion table

--- src/Umbraco.Infrastructure/Migrations/Upgrade/V_13_3_0/AlignUpgradedDatabase.cs
+++ src/Umbraco.Infrastructure/Migrations/Upgrade/V_13_3_0/AlignUpgradedDatabase.cs
@@ -1,4 +1,6 @@
 ﻿using NPoco;
+using Umbraco.Cms.Infrastructure.Migrations.Expressions.Alter.Expressions;
+using Umbraco.Cms.Infrastructure.Persistence;
 using ColumnInfo = Umbraco.Cms.Infrastructure.Persistence.SqlSyntax.ColumnInfo;
 
 namespace Umbraco.Cms.Infrastructure.Migrations.Upgrade.V_13_3_0;
@@ -22,8 +24,15 @@ public class AlignUpgradedDatabase : MigrationBase
             return;
         }
 
-        IEnumerable<ColumnInfo> columns = SqlSyntax.GetColumnsInSchema(Context.Database);
+        ColumnInfo[] columns = SqlSyntax.GetColumnsInSchema(Context.Database).ToArray();
 
+        DropCacheInstructionDefaultConstraint(columns);
+        RenameVersionDateColumn(columns);
+
+    }
+
+    private void DropCacheInstructionDefaultConstraint(IEnumerable<ColumnInfo> columns)
+    {
         const string cacheTableName = "umbracoCacheInstruction";
         const string cacheColumnName = "jsonInstruction";
         ColumnInfo? jsonInstructionColumn = columns
@@ -40,5 +49,32 @@ public class AlignUpgradedDatabase : MigrationBase
                 .OnTable(cacheTableName)
                 .OnColumn(cacheColumnName).Do();
         }
+    }
+
+    private void RenameVersionDateColumn(IEnumerable<ColumnInfo> columns)
+    {
+        const string tableName = "umbracoContentVersion";
+        const string columnName = "VersionDate";
+        ColumnInfo? versionDateColumn = columns
+            .FirstOrDefault(x => x is { TableName: tableName, ColumnName: columnName });
+
+        if (versionDateColumn is null)
+        {
+            // The column was not found I.E. the column is correctly named
+            return;
+        }
+
+        Rename.Column(columnName)
+            .OnTable(tableName)
+            .To("versionDate")
+            .Do();
+
+        // Renames the default constraint for the column,
+        // apparently the content version table used to be prefixed with cms and not umbraco
+        // We don't have a fluid way to rename the default constraint so we have to use raw SQL
+        // This should be okay though since we are only running this migration on SQL Server
+        Sql<ISqlContext> renameConstraintQuery = Database.SqlContext.Sql(
+            "EXEC sp_rename N'DF_cmsContentVersion_VersionDate', N'DF_umbracoContentVersion_versionDate', N'OBJECT'");
+        Database.Execute(renameConstraintQuery);
     }
 }
