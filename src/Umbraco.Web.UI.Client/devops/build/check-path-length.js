759d79361743f18ad80278251022e07423f23909
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-03-22 14:03:02 +0100

    optimise azure pipelines

--- src/Umbraco.Web.UI.Client/devops/build/check-path-length.js
+++ src/Umbraco.Web.UI.Client/devops/build/check-path-length.js
@@ -4,7 +4,7 @@ import { join } from 'path';
 const PROJECT_DIR = process.argv[2] ?? '.';
 const MAX_PATH_LENGTH = process.argv[3] ?? 140;
 const IS_CI = process.env.CI === 'true';
-const IS_AZURE_PIPELINES = process.env.SYSTEM_TEAMFOUNDATIONCOLLECTIONURI !== undefined;
+const IS_AZURE_PIPELINES = process.env.TF_BUILD === 'true';
 const IS_GITHUB_ACTIONS = process.env.GITHUB_ACTIONS === 'true';
 const FILE_PATH_COLOR = '\x1b[36m%s\x1b[0m';
 
