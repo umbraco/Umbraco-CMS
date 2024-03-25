dc52ba0faab7787f2761029289e5052c3c5ca571
Sven Geusens <sge@umbraco.dk>
2024-03-25 10:54:12 +0100

    Tests against fixed values instead of calculated ones.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/Extensions/TreeEntityExtensionsTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Extensions/TreeEntityExtensionsTests.cs
@@ -9,23 +9,17 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Extensions;
 [TestFixture]
 public class TreeEntityExtensionsTests
 {
-    [TestCase("-1,1234", 0)]
-    [TestCase("-1,1234,5678", 1)]
-    [TestCase("-1,1234,5678,9012", 2)]
-    [TestCase("-1,1234,5678,9012,2345", 3)]
-    public void Parse_Ancestor_Ids_Excludes_Root_And_Self(string path, int expectedLength)
+    [TestCase("-1,1234", new int[] { })]
+    [TestCase("-1,1234,5678", new int[] { 1234 })]
+    [TestCase("-1,1234,5678,9012", new int[] { 5678, 1234 })]
+    [TestCase("-1,1234,5678,9012,2345", new int[] { 9012, 5678, 1234 })]
+    public void Parse_Ancestor_Ids_Excludes_Root_And_Self(string path, int[] expectedIds)
     {
         var entityMock = new Mock<ITreeEntity>();
         entityMock.SetupGet(m => m.Path).Returns(path);
 
-        var expectedIds = path
-            .Split(',')
-            .Skip(1)
-            .Take(Range.EndAt(Index.FromEnd(1)))
-            .Select(int.Parse)
-            .ToArray();
         var result = entityMock.Object.AncestorIds();
-        Assert.AreEqual(expectedLength, result.Length);
+        Assert.AreEqual(expectedIds.Length, result.Length);
         Assert.That(expectedIds, Is.EquivalentTo(result));
     }
 }
