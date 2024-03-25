b03de5381847f3e8b3ba985c96d66cccfec4b0b2
Mads Rasmussen <madsr@hey.com>
2024-03-25 10:45:44 +0100

    Update relation-type-workspace.context-token.ts

--- src/Umbraco.Web.UI.Client/src/packages/relations/relation-types/workspace/relation-type/relation-type-workspace.context-token.ts
+++ src/Umbraco.Web.UI.Client/src/packages/relations/relation-types/workspace/relation-type/relation-type-workspace.context-token.ts
@@ -1,11 +1,8 @@
 import type { UmbRelationTypeWorkspaceContext } from './relation-type-workspace.context.js';
 import { UmbContextToken } from '@umbraco-cms/backoffice/context-api';
-import type { UmbSaveableWorkspaceContext } from '@umbraco-cms/backoffice/workspace';
 
-export const UMB_RELATION_TYPE_WORKSPACE_CONTEXT = new UmbContextToken<
-	UmbSaveableWorkspaceContext,
-	UmbRelationTypeWorkspaceContext
->(
+// TODO: Make readonly workspace context type
+export const UMB_RELATION_TYPE_WORKSPACE_CONTEXT = new UmbContextToken<any, UmbRelationTypeWorkspaceContext>(
 	'UmbWorkspaceContext',
 	undefined,
 	(context): context is UmbRelationTypeWorkspaceContext => context.getEntityType?.() === 'relation-type',
