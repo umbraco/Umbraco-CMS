cc3e55fb8e3fa9e2854ae5cbb190f79cd2ff116f
Niels Lyngsø <niels.lyngso@gmail.com>
2024-03-27 10:15:04 +0100

    revert appending

--- src/Umbraco.Web.UI.Client/src/packages/core/workspace/contexts/saveable-workspace-context-base.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/workspace/contexts/saveable-workspace-context-base.ts
@@ -29,8 +29,6 @@ export abstract class UmbSaveableWorkspaceContextBase<WorkspaceDataModelType>
 	#isNew = new UmbBooleanState(undefined);
 	isNew = this.#isNew.asObservable();
 
-	abstract readonly unique: Observable<string | null | undefined>;
-
 	readonly routes = new UmbWorkspaceRouteManager(this);
 
 	/*
