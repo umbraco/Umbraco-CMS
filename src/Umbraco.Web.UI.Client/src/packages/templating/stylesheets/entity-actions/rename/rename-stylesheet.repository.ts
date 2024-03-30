bb0cc7ca9280b8c163568ffac09a9730c363d812
Mads Rasmussen <madsr@hey.com>
2024-03-30 19:30:23 +0100

    pass detail store

--- src/Umbraco.Web.UI.Client/src/packages/templating/stylesheets/entity-actions/rename/rename-stylesheet.repository.ts
+++ src/Umbraco.Web.UI.Client/src/packages/templating/stylesheets/entity-actions/rename/rename-stylesheet.repository.ts
@@ -1,11 +1,12 @@
+import { UMB_STYLESHEET_DETAIL_STORE_CONTEXT } from '../../repository/index.js';
 import type { UmbStylesheetDetailModel } from '../../types.js';
 import { UmbRenameStylesheetServerDataSource } from './rename-stylesheet.server.data-source.js';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
-import { UmbRenameRepositoryBase } from '@umbraco-cms/backoffice/entity-action';
+import { UmbRenameServerFileRepositoryBase } from '@umbraco-cms/backoffice/server-file-system';
 
-export class UmbRenameStylesheetRepository extends UmbRenameRepositoryBase<UmbStylesheetDetailModel> {
+export class UmbRenameStylesheetRepository extends UmbRenameServerFileRepositoryBase<UmbStylesheetDetailModel> {
 	constructor(host: UmbControllerHost) {
-		super(host, UmbRenameStylesheetServerDataSource);
+		super(host, UmbRenameStylesheetServerDataSource, UMB_STYLESHEET_DETAIL_STORE_CONTEXT);
 	}
 }
 
