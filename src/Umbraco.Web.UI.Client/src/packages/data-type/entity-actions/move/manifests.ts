50cf342a6749f57851f91d82dfb6ba8bfd7a3c11
Mads Rasmussen <madsr@hey.com>
2024-03-31 20:52:49 +0200

    pass tree repo

--- src/Umbraco.Web.UI.Client/src/packages/data-type/entity-actions/move/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/data-type/entity-actions/move/manifests.ts
@@ -1,7 +1,7 @@
 import { UMB_DATA_TYPE_ENTITY_TYPE } from '../../entity.js';
 import { UMB_DATA_TYPE_PICKER_MODAL } from '../../modals/index.js';
-import { UMB_DATA_TYPE_ITEM_REPOSITORY_ALIAS } from '../../repository/index.js';
-import { UMB_MOVE_DATA_TYPE_REPOSITORY_ALIAS } from '../../repository/move/manifests.js';
+import { UMB_MOVE_DATA_TYPE_REPOSITORY_ALIAS } from '../../repository/index.js';
+import { UMB_DATA_TYPE_TREE_REPOSITORY_ALIAS } from '../../tree/index.js';
 import type { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
 
 const entityActions: Array<ManifestTypes> = [
@@ -12,7 +12,7 @@ const entityActions: Array<ManifestTypes> = [
 		name: 'Move Data Type Entity Action',
 		forEntityTypes: [UMB_DATA_TYPE_ENTITY_TYPE],
 		meta: {
-			itemRepositoryAlias: UMB_DATA_TYPE_ITEM_REPOSITORY_ALIAS,
+			treeRepositoryAlias: UMB_DATA_TYPE_TREE_REPOSITORY_ALIAS,
 			moveRepositoryAlias: UMB_MOVE_DATA_TYPE_REPOSITORY_ALIAS,
 			pickerModal: UMB_DATA_TYPE_PICKER_MODAL,
 		},
