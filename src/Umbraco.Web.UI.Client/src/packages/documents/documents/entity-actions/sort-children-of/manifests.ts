e3c1563cc0f25f333ccc267817ebca8056043448
Mads Rasmussen <madsr@hey.com>
2024-04-01 10:20:15 +0200

    register sort children for document root

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/entity-actions/sort-children-of/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/entity-actions/sort-children-of/manifests.ts
@@ -1,4 +1,4 @@
-import { UMB_DOCUMENT_ENTITY_TYPE } from '../../entity.js';
+import { UMB_DOCUMENT_ENTITY_TYPE, UMB_DOCUMENT_ROOT_ENTITY_TYPE } from '../../entity.js';
 import { UMB_DOCUMENT_ITEM_REPOSITORY_ALIAS } from '../../repository/index.js';
 import { UMB_SORT_CHILDREN_OF_DOCUMENT_REPOSITORY_ALIAS } from './repository/constants.js';
 
@@ -8,7 +8,7 @@ export const manifests = [
 		kind: 'sortChildrenOf',
 		alias: 'Umb.EntityAction.Document.SortChildrenOf',
 		name: 'Sort Children Of Document Entity Action',
-		forEntityTypes: [UMB_DOCUMENT_ENTITY_TYPE],
+		forEntityTypes: [UMB_DOCUMENT_ROOT_ENTITY_TYPE, UMB_DOCUMENT_ENTITY_TYPE],
 		meta: {
 			itemRepositoryAlias: UMB_DOCUMENT_ITEM_REPOSITORY_ALIAS,
 			sortRepositoryAlias: UMB_SORT_CHILDREN_OF_DOCUMENT_REPOSITORY_ALIAS,
