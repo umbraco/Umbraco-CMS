ad843fb1b2045b562f8d691ff3e51e5187b0e946
Mads Rasmussen <madsr@hey.com>
2024-04-02 19:11:54 +0200

    move registration to recycle bin

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/recycle-bin/entity-action/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/recycle-bin/entity-action/manifests.ts
@@ -1,6 +1,7 @@
 import { UMB_DOCUMENT_RECYCLE_BIN_REPOSITORY_ALIAS } from '../repository/index.js';
 import { UMB_DOCUMENT_ENTITY_TYPE } from '../../entity.js';
-import { UMB_DOCUMENT_ITEM_REPOSITORY_ALIAS } from '../../repository/index.js';
+import { UMB_DOCUMENT_DETAIL_REPOSITORY_ALIAS, UMB_DOCUMENT_ITEM_REPOSITORY_ALIAS } from '../../repository/index.js';
+import { UMB_DOCUMENT_RECYCLE_BIN_ENTITY_TYPE } from '../entity.js';
 
 export const manifests = [
 	{
@@ -12,6 +13,17 @@ export const manifests = [
 		meta: {
 			itemRepositoryAlias: UMB_DOCUMENT_ITEM_REPOSITORY_ALIAS,
 			recycleBinRepositoryAlias: UMB_DOCUMENT_RECYCLE_BIN_REPOSITORY_ALIAS,
+		},
+	},
+	{
+		type: 'entityAction',
+		kind: 'delete',
+		alias: 'Umb.EntityAction.Document.Delete',
+		name: 'Delete Document Entity Action',
+		forEntityTypes: [UMB_DOCUMENT_RECYCLE_BIN_ENTITY_TYPE],
+		meta: {
+			itemRepositoryAlias: UMB_DOCUMENT_ITEM_REPOSITORY_ALIAS,
+			detailRepositoryAlias: UMB_DOCUMENT_DETAIL_REPOSITORY_ALIAS,
 		},
 	},
 ];
