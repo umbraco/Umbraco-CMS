95849c265b5bdfa297615db2abe9585e2581bc89
Sven Geusens <geusens@gmail.com>
2024-04-02 12:12:36 +0200

    Document version endpoints (#15946)
    * Rename/Move/duplicate PaginationService to facilitate conversion closer to the data layer
    
    Duplication is because of internal modifier as we don't want to expose these temporary classes
    
    * Move Guid to Int Extensions into core + add unittests
    
    * Added Document version endpoints
    
    Updated used services to use async methods
    
    * Moved PaginationConverter into core so it can be used by the service layer
    
    * Endpoint structure improvements
    
    * Updating OpenApi.json
    
    * Add greedy constructors for contentService tests
    
    * Namespace changes and naming cleanup
    
    * Update openapispec again...
    
    * Refactor injected services
    
    * PR suggestion updates
    
    - Move endpoints into their own structural section as they are also in a different swagger section
    - Naming improvements
    - Allign PresentationFactories with similar classes
    - Cleanup unused assignments
    - Cleanup refactoring comments
    - Improve obsoletion remarks
    
    * Cleanup
    
    * ResponseModel improvements
    
    * OpenApi spec update
    
    ---------
    
    Co-authored-by: Sven Geusens <sge@umbraco.dk>
    Co-authored-by: Elitsa <elm@umbraco.dk>

--- src/Umbraco.Core/Services/IContentVersionService.cs
+++ src/Umbraco.Core/Services/IContentVersionService.cs
@@ -1,4 +1,5 @@
 using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Services.OperationStatus;
 
 namespace Umbraco.Cms.Core.Services;
 
@@ -13,10 +14,19 @@ public interface IContentVersionService
     ///     Gets paginated content versions for given content id paginated.
     /// </summary>
     /// <exception cref="ArgumentException">Thrown when <paramref name="culture" /> is invalid.</exception>
+    [Obsolete("Use the async version instead. Scheduled to be removed in v15")]
     IEnumerable<ContentVersionMeta>? GetPagedContentVersions(int contentId, long pageIndex, int pageSize, out long totalRecords, string? culture = null);
 
     /// <summary>
     ///     Updates preventCleanup value for given content version.
     /// </summary>
+    [Obsolete("Use the async version instead. Scheduled to be removed in v15")]
     void SetPreventCleanup(int versionId, bool preventCleanup, int userId = -1);
+
+    ContentVersionMeta? Get(int versionId);
+    Task<Attempt<PagedModel<ContentVersionMeta>?, ContentVersionOperationStatus>> GetPagedContentVersionsAsync(Guid contentId, string? culture, int skip, int take);
+    Task<Attempt<IContent?, ContentVersionOperationStatus>> GetAsync(Guid versionId);
+
+    Task<Attempt<ContentVersionOperationStatus>> SetPreventCleanupAsync(Guid versionId, bool preventCleanup, Guid userKey);
+    Task<Attempt<ContentVersionOperationStatus>> RollBackAsync(Guid versionId, string? culture, Guid userKey);
 }
