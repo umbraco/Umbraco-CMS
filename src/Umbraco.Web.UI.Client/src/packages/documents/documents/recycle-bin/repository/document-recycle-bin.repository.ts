d8e10b055b38b9156e8a53e66b5aaf4f68ecb101
Mads Rasmussen <madsr@hey.com>
2024-04-03 08:55:21 +0200

    extend recycle bin repo base

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/recycle-bin/repository/document-recycle-bin.repository.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/recycle-bin/repository/document-recycle-bin.repository.ts
@@ -1,29 +1,10 @@
+import { UmbRecycleBinRepositoryBase } from '@umbraco-cms/backoffice/recycle-bin';
 import { UmbDocumentRecycleBinServerDataSource } from './document-recycle-bin.server.data-source.js';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
-import { UmbRepositoryBase } from '@umbraco-cms/backoffice/repository';
 
-export class UmbDocumentRecycleBinRepository extends UmbRepositoryBase {
-	#recycleBinSource: UmbDocumentRecycleBinServerDataSource;
-
+export class UmbDocumentRecycleBinRepository extends UmbRecycleBinRepositoryBase {
 	constructor(host: UmbControllerHost) {
-		super(host);
-		this.#recycleBinSource = new UmbDocumentRecycleBinServerDataSource(this);
-	}
-
-	async requestTrash(args: any) {
-		return this.#recycleBinSource.trash(args);
-	}
-
-	async requestRestore(args: any) {
-		return this.#recycleBinSource.restore(args);
-	}
-
-	async requestEmpty() {
-		return this.#recycleBinSource.empty();
-	}
-
-	async requestOriginalParent(args: any) {
-		return this.#recycleBinSource.getOriginalParent(args);
+		super(host, UmbDocumentRecycleBinServerDataSource);
 	}
 }
 
