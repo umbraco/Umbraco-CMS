623019fe6a42ba458ea8172c0905cdc68d74f986
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-04-03 17:15:25 +0200

    improve mfa action condition so it only spins up the observer in the constructor once (it doesn't need to know about the selected user)

--- src/Umbraco.Web.UI.Client/src/packages/user/user/conditions/user-allow-mfa-action.condition.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/conditions/user-allow-mfa-action.condition.ts
@@ -1,10 +1,14 @@
-import { UmbUserActionConditionBase } from './user-allow-action-base.condition.js';
+import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import type { ManifestCondition } from '@umbraco-cms/backoffice/extension-api';
-import { umbExtensionsRegistry } from '@umbraco-cms/backoffice/extension-registry';
+import { UmbConditionBase, umbExtensionsRegistry } from '@umbraco-cms/backoffice/extension-registry';
 
-export class UmbUserAllowMfaActionCondition extends UmbUserActionConditionBase {
-	async _onUserDataChange() {
+export class UmbUserAllowMfaActionCondition extends UmbConditionBase<never> {
+	// eslint-disable-next-line @typescript-eslint/no-explicit-any
+	constructor(host: UmbControllerHost, args: any) {
+		super(host, args);
+
 		// Check if there are any MFA providers available
+		this.permitted = false;
 		this.observe(
 			umbExtensionsRegistry.byType('mfaLoginProvider'),
 			(exts) => (this.permitted = exts.length > 0),
