cedeede9cd6036895562a2c4da416488597fbdcb
Mads Rasmussen <madsr@hey.com>
2024-04-03 21:04:49 +0200

    Update document-recycle-bin.repository.ts

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/recycle-bin/repository/document-recycle-bin.repository.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/recycle-bin/repository/document-recycle-bin.repository.ts
@@ -1,5 +1,5 @@
-import { UmbRecycleBinRepositoryBase } from '@umbraco-cms/backoffice/recycle-bin';
 import { UmbDocumentRecycleBinServerDataSource } from './document-recycle-bin.server.data-source.js';
+import { UmbRecycleBinRepositoryBase } from '@umbraco-cms/backoffice/recycle-bin';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 
 export class UmbDocumentRecycleBinRepository extends UmbRecycleBinRepositoryBase {
