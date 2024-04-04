1db28d07daf9feb0be8748a3cb2c28328ff7038e
Mads Rasmussen <madsr@hey.com>
2024-04-04 13:28:04 +0200

    add todo

--- src/Umbraco.Web.UI.Client/src/packages/core/modal/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/modal/types.ts
@@ -1,8 +1,8 @@
-export interface UmbPickerModalData<TreeItemType> {
+export interface UmbPickerModalData<ItemType> {
 	multiple?: boolean;
-	hideTreeRoot?: boolean;
-	filter?: (item: TreeItemType) => boolean;
-	pickableFilter?: (item: TreeItemType) => boolean;
+	hideTreeRoot?: boolean; // TODO: this should be moved to a tree picker modal data interface
+	filter?: (item: ItemType) => boolean;
+	pickableFilter?: (item: ItemType) => boolean;
 }
 export interface UmbPickerModalValue {
 	selection: Array<string | null>;
