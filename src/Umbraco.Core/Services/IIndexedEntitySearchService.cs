3a308a1d03076d30187558d587a76ba9e8e2cbb7
kjac <kja@umbraco.dk>
2024-04-04 20:54:34 +0200

    Rename Examine based entity search service

--- src/Umbraco.Core/Services/IIndexedEntitySearchService.cs
+++ src/Umbraco.Core/Services/IIndexedEntitySearchService.cs
@@ -3,7 +3,7 @@ using Umbraco.Cms.Core.Models.Entities;
 
 namespace Umbraco.Cms.Core.Services;
 
-public interface IExamineEntitySearchService
+public interface IIndexedEntitySearchService
 {
     PagedModel<IEntitySlim> Search(UmbracoObjectTypes objectType, string query, int skip = 0, int take = 100, bool ignoreUserStartNodes = false);
 }
