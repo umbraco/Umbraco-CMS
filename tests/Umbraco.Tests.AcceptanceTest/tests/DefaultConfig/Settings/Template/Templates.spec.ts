014b2c2972deaca382787f61547af3db84853dea
Nhu Dinh <hnd@umbraco.dk>
2024-04-04 16:56:40 +0700

    Added timeout

--- tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Settings/Template/Templates.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Settings/Template/Templates.spec.ts
@@ -125,6 +125,7 @@ test.describe('Template tests', () => {
     // Act
     await umbracoUi.template.goToTemplate(templateName);
     await umbracoUi.template.clickSectionsButton();
+    await umbracoUi.waitForTimeout(1000);
     await umbracoUi.template.clickSubmitButton();
     await umbracoUi.template.clickSaveButton();
 
