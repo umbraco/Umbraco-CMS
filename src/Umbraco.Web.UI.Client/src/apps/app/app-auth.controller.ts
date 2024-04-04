94c3d5fca8983074087382adaff9da2143dbcb84
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-04-04 16:50:38 +0200

    add check to see if any auth providers are registered

--- src/Umbraco.Web.UI.Client/src/apps/app/app-auth.controller.ts
+++ src/Umbraco.Web.UI.Client/src/apps/app/app-auth.controller.ts
@@ -1,8 +1,10 @@
-import { UMB_AUTH_CONTEXT, UMB_STORAGE_REDIRECT_URL } from '@umbraco-cms/backoffice/auth';
+import { UMB_AUTH_CONTEXT, UMB_MODAL_APP_AUTH, UMB_STORAGE_REDIRECT_URL } from '@umbraco-cms/backoffice/auth';
 import { UmbControllerBase } from '@umbraco-cms/backoffice/class-api';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import { UmbServerExtensionRegistrator } from '@umbraco-cms/backoffice/extension-api';
 import { umbExtensionsRegistry } from '@umbraco-cms/backoffice/extension-registry';
+import { firstValueFrom } from '@umbraco-cms/backoffice/external/rxjs';
+import { UMB_MODAL_MANAGER_CONTEXT } from '@umbraco-cms/backoffice/modal';
 
 export class UmbAppAuthController extends UmbControllerBase {
 	#authContext?: typeof UMB_AUTH_CONTEXT.TYPE;
@@ -52,11 +54,32 @@ export class UmbAppAuthController extends UmbControllerBase {
 
 		await this.#init;
 
-		this.#authContext.makeAuthorizationRequest();
+		// Figure out which providers are available
+		const availableProviders = await firstValueFrom(umbExtensionsRegistry.byType('authProvider'));
+		if (availableProviders.length === 0) {
+			// No providers available, initiate the authorization request to the default provider
+			this.#authContext.makeAuthorizationRequest();
+		} else {
+			// Check if any provider is redirecting directly to the provider, and if so, redirect to that provider
+			const redirectProvider = availableProviders.find((provider) => provider.meta?.autoRedirect);
+			if (redirectProvider) {
+				this.#authContext.makeAuthorizationRequest(redirectProvider.forProviderName);
+				return true;
+			}
+
+			// Show the provider selection screen
+			console.log('show modal for', availableProviders);
+			const modalContext = await this.getContext(UMB_MODAL_MANAGER_CONTEXT);
+			await modalContext
+				.open(this._host, UMB_MODAL_APP_AUTH)
+				.onSubmit()
+				.catch(() => undefined);
+		}
 
 		// Reinitialize the auth flow (load the state from local storage)
 		this.#authContext.setInitialState();
 
-		return true;
+		// The authorization flow is finished, so let the caller know if the user is authorized
+		return this.#authContext.getIsAuthorized();
 	}
 }
