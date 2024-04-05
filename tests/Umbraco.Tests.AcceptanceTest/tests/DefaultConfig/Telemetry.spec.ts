8f0ee11754d51b12c1a2646c708aec1e7bac4a07
Nhu Dinh <hnd@umbraco.dk>
2024-04-05 20:16:17 +0700

    Fix merge conflict

--- tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Telemetry.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Telemetry.spec.ts
@@ -4,41 +4,21 @@ import {expect} from "@playwright/test";
 test.describe('Telemetry tests', () => {
 
   test.beforeEach(async ({umbracoApi, umbracoUi}) => {
-<<<<<<< HEAD
     await umbracoApi.telemetry.setLevel("Basic");
     await umbracoUi.goToBackOffice();
     await umbracoUi.telemetryData.goToSection(ConstantHelper.sections.settings);
   });
 
-  test.afterEach(async ({umbracoApi}) => {
-=======
->>>>>>> v14/dev
-    await umbracoApi.telemetry.setLevel("Basic");
-    await umbracoUi.goToBackOffice();
-    await umbracoUi.telemetryData.goToSection(ConstantHelper.sections.settings);
-  });
-
-<<<<<<< HEAD
-  test('can change telemetry level', async ({umbracoApi, umbracoUi}) => {
-=======
   test.afterEach(async ({umbracoApi}) => {
     await umbracoApi.telemetry.setLevel("Basic");
   });
 
   test('can change telemetry level', async ({page, umbracoApi, umbracoUi}) => {
->>>>>>> v14/dev
     // Arrange
     const expectedLevel = "Minimal";
     const levelValue = "1";
     await umbracoUi.telemetryData.clickTelemetryDataTab();
     await umbracoUi.telemetryData.changeTelemetryDataLevelValue(levelValue);
-<<<<<<< HEAD
-    await umbracoUi.telemetryData.clickSaveButton();
-    // Assert
-    // UI
-    await umbracoUi.reloadPage();
-    await umbracoUi.telemetryData.doesTelemetryDataLevelHasValue(levelValue)
-=======
 
     // We wait until we are sure that the Telemetry level has been saved before we continue.
     await Promise.all([
@@ -52,7 +32,6 @@ test.describe('Telemetry tests', () => {
     await expect(page.locator('[name="telemetryLevel"] >> input[id=input]')).toHaveValue(levelValue, {timeout: 20000});
     // await umbracoUi.telemetryData.doesTelemetryDataLevelHaveValue(levelValue);
 
->>>>>>> v14/dev
     // API
     expect(await umbracoApi.telemetry.getLevel() == expectedLevel).toBeTruthy();
   });
