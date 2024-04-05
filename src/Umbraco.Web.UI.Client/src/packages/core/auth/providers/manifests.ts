307c9b37e6b94b41cb5192ebfe6fd6fb1813113e
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-04-05 09:50:50 +0200

    register Umbraco as a provider

---
+++ src/Umbraco.Web.UI.Client/src/packages/core/auth/providers/manifests.ts
@@ -0,0 +1,14 @@
+import type { ManifestAuthProvider } from '@umbraco-cms/backoffice/extension-registry';
+
+export const manifests: Array<ManifestAuthProvider> = [
+	{
+		type: 'authProvider',
+		alias: 'Umb.AuthProviders.Umbraco',
+		name: 'Umbraco login provider',
+		forProviderName: 'Umbraco',
+		weight: 1000,
+		meta: {
+			label: 'Umbraco',
+		},
+	},
+];
