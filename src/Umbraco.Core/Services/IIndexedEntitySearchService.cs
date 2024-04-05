29de48fa9ce61ea028945dc2af0e1e5341e0c75b
kjac <kja@umbraco.dk>
2024-04-05 12:54:54 +0200

    Add a few comments to make it easier to tell the "indexed" search service from the "regular" one.

--- src/Umbraco.Core/Services/IIndexedEntitySearchService.cs
+++ src/Umbraco.Core/Services/IIndexedEntitySearchService.cs
@@ -3,6 +3,13 @@ using Umbraco.Cms.Core.Models.Entities;
 
 namespace Umbraco.Cms.Core.Services;
 
+/// <summary>
+/// Performs entity search against search indexes.
+/// </summary>
+/// <remarks>
+/// Note that this service only supports entity types that are included in search indexes.
+/// By default this means documents, media and members.
+/// </remarks>
 public interface IIndexedEntitySearchService
 {
     PagedModel<IEntitySlim> Search(UmbracoObjectTypes objectType, string query, int skip = 0, int take = 100, bool ignoreUserStartNodes = false);
