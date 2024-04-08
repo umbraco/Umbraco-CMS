2e98f9fc149d7c59efcac37325dc6912444b8543
Elitsa <elm@umbraco.dk>
2024-04-08 15:06:14 +0200

    Forgotten Item and Tree controllers

--- src/Umbraco.Cms.Api.Management/Controllers/Webhook/ByKeyWebhookController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/Webhook/ByKeyWebhookController.cs
@@ -3,7 +3,6 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Api.Management.Factories;
 using Umbraco.Cms.Api.Management.ViewModels.Webhook;
-using Umbraco.Cms.Core.Mapping;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Core.Services.OperationStatus;
@@ -26,7 +25,7 @@ public class ByKeyWebhookController : WebhookControllerBase
     [MapToApiVersion("1.0")]
     [ProducesResponseType(typeof(WebhookResponseModel), StatusCodes.Status200OK)]
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
-    public async Task<IActionResult> ByKey(CancellationToken cancellationToken,  Guid id)
+    public async Task<IActionResult> ByKey(CancellationToken cancellationToken, Guid id)
     {
         IWebhook? webhook = await _webhookService.GetAsync(id);
         if (webhook is null)
