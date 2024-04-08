1dba67ac4331fedef3b8fccef264779c5a3ec4eb
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-04-08 10:16:24 +0200

    export types from the appauth module to fix tests

--- src/Umbraco.Web.UI.Client/src/external/openid/index.ts
+++ src/Umbraco.Web.UI.Client/src/external/openid/index.ts
@@ -5,14 +5,12 @@ export {
 	LocalStorageBackend,
 	RedirectRequestHandler,
 	RevokeTokenRequest,
-} from '@openid/appauth';
-export { AuthorizationRequest } from '@openid/appauth/built/authorization_request';
-export { AuthorizationNotifier } from '@openid/appauth/built/authorization_request_handler';
-export { AuthorizationServiceConfiguration } from '@openid/appauth/built/authorization_service_configuration';
-export {
+	AuthorizationNotifier,
+	AuthorizationRequest,
+	AuthorizationServiceConfiguration,
 	GRANT_TYPE_AUTHORIZATION_CODE,
 	GRANT_TYPE_REFRESH_TOKEN,
 	TokenRequest,
-} from '@openid/appauth/built/token_request';
-export { TokenResponse } from '@openid/appauth/built/token_response';
-export type { LocationLike, StringMap } from '@openid/appauth/built/types';
+	TokenResponse,
+} from '@openid/appauth';
+export type { LocationLike, StringMap } from '@openid/appauth';
