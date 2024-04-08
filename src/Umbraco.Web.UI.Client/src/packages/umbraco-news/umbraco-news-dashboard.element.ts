1881d37c7433a9ad41baef9cc9f1ea1d9e588d2f
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-04-08 10:16:36 +0200

    Revert "export types from the appauth module to fix tests"
    This reverts commit 1dba67ac4331fedef3b8fccef264779c5a3ec4eb.

--- src/Umbraco.Web.UI.Client/src/packages/umbraco-news/umbraco-news-dashboard.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/umbraco-news/umbraco-news-dashboard.element.ts
@@ -1,39 +1,61 @@
-import { UMB_AUTH_CONTEXT } from '../core/auth/auth.context.token.js';
-import { tryExecuteAndNotify } from '../core/resources/tryExecuteAndNotify.function.js';
-import { html, customElement, state } from '@umbraco-cms/backoffice/external/lit';
+import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
+import { UMB_CURRENT_USER_CONTEXT } from '@umbraco-cms/backoffice/current-user';
+import { css, html, customElement, state } from '@umbraco-cms/backoffice/external/lit';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
-import { UserGroupResource, type UserGroupResponseModel } from '@umbraco-cms/backoffice/external/backend-api';
 
 @customElement('umb-umbraco-news-dashboard')
 export class UmbUmbracoNewsDashboardElement extends UmbLitElement {
-	@state()
-	_groups: UserGroupResponseModel[] = [];
+	#currentUserContext?: typeof UMB_CURRENT_USER_CONTEXT.TYPE;
 
-	async clearToken() {
-		const authContext = await this.getContext(UMB_AUTH_CONTEXT);
-		authContext.clearTokenStorage();
+	@state()
+	private _name = '';
+
+	constructor() {
+		super();
+		this.consumeContext(UMB_CURRENT_USER_CONTEXT, (instance) => {
+			this.#currentUserContext = instance;
+			this.#observeCurrentUser();
+		});
 	}
 
-	async makeAuthorizedRequest() {
-		const { data } = await tryExecuteAndNotify(this, UserGroupResource.getUserGroup({ skip: 0, take: 10 }));
-
-		if (data) {
-			this._groups = data.items;
-		}
+	#observeCurrentUser(): void {
+		if (!this.#currentUserContext) return;
+		this.observe(this.#currentUserContext.currentUser, (user) => {
+			this._name = user?.name ?? '';
+		});
 	}
 
 	render() {
 		return html`
 			<uui-box class="uui-text">
+				<h1 class="uui-h2" style="margin-top: var(--uui-size-layout-1);">
+					<umb-localize key="dashboard_welcome">Welcome</umb-localize>, ${this._name}
+				</h1>
+				<p class="uui-lead">
+					This is the beta version of Umbraco 14, where you can have a first-hand look at the new Backoffice.
+				</p>
 				<p>
-					<uui-button look="primary" @click=${this.clearToken}>Clear all tokens</uui-button>
-					<uui-button look="primary" @click=${this.makeAuthorizedRequest}>Make authorized request</uui-button>
-
-					${this._groups.map((group) => html` <p>${group.name}</p> `)}
+					Please refer to the
+					<a target="_blank" href="https://docs.umbraco.com/umbraco-cms/v/14.latest-beta/">documentation</a> to learn
+					more about what is possible. Here you will find excellent tutorials, guides, and references to help you get
+					started extending the Backoffice.
 				</p>
 			</uui-box>
 		`;
 	}
+
+	static styles = [
+		UmbTextStyles,
+		css`
+			:host {
+				display: block;
+				padding: var(--uui-size-layout-1);
+			}
+			p {
+				position: relative;
+			}
+		`,
+	];
 }
 
 export default UmbUmbracoNewsDashboardElement;
