ca339dc9ba0839c156b8c3e0833a891f0cce549a
Nhu Dinh <hnd@umbraco.dk>
2024-04-09 15:21:29 +0700

    Fixed locator

--- tests/Umbraco.Tests.AcceptanceTest/tests/auth.setup.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/auth.setup.ts
@@ -1,4 +1,4 @@
-﻿import {test as setup} from '@playwright/test';
+﻿import {expect, test as setup} from '@playwright/test';
 import {STORAGE_STATE} from '../playwright.config';
 import {ConstantHelper, UiHelpers} from "@umbraco/playwright-testhelpers";
 
@@ -7,9 +7,12 @@ setup('authenticate', async ({page}) => {
 
   await umbracoUi.goToBackOffice();
   // This wait is necessary to avoid the flaky test in Window
-  await umbracoUi.waitForTimeout(1000);
-  await umbracoUi.login.enterEmail(process.env.UMBRACO_USER_LOGIN);
-  await umbracoUi.login.enterPassword(process.env.UMBRACO_USER_PASSWORD);
+  //await umbracoUi.waitForTimeout(1000);
+  //await umbracoUi.login.enterEmail(process.env.UMBRACO_USER_LOGIN);
+  //await umbracoUi.login.enterPassword(process.env.UMBRACO_USER_PASSWORD);
+  await expect(umbracoUi.page.locator('name="username"')).toBeVisible({timeout: 20000});
+  await umbracoUi.page.locator('name="username"').fill(process.env.UMBRACO_USER_LOGIN);
+  await umbracoUi.page.locator('name="password"').fill(process.env.UMBRACO_USER_LOGIN);
   await umbracoUi.login.clickLoginButton();
   await umbracoUi.login.goToSection(ConstantHelper.sections.settings);
   await page.context().storageState({path: STORAGE_STATE});
