88ebf5a26f4ca0b649c1f8b61dc8ab930a16aa22
leekelleher <leekelleher@gmail.com>
2024-04-09 16:47:39 +0100

    Changed workspace to be a "routable" kind
    Removed "document-blueprint-workspace.element.ts"
    as the routes are defined in the Workspace Context.
    
    Removed the `#getDataPromise` field for the moment.
    (Whilst we work out any data loading issues)

--- src/Umbraco.Web.UI.Client/src/packages/documents/document-blueprints/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/document-blueprints/manifests.ts
@@ -1,6 +1,5 @@
 import { manifests as entityActionManifests } from './entity-actions/manifests.js';
 import { manifests as menuItemManifests } from './menu-item/manifests.js';
-
 import { manifests as repositoryManifests } from './repository/manifests.js';
 import { manifests as treeManifests } from './tree/manifests.js';
 import { manifests as workspaceManifests } from './workspace/manifests.js';
