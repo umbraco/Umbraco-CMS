de1fc65393ce966331942c759e78684cdbf6cd8d
Kenn Jacobsen <kja@umbraco.dk>
2024-04-09 10:20:20 +0200

    Swagger: Don't use allOf for inheritance + fix polymorphic models (#15985)
    * Swagger: Don't use allOf for inheritance + fix polymorphic models
    
    * Cannot mix $ref and nullable in OpenAPI
    
    * Update OpenApi.json
    
    * Regenerate OpenApi.json

--- src/Umbraco.Cms.Api.Management/ViewModels/Document/DocumentResponseModelBase.cs
+++ src/Umbraco.Cms.Api.Management/ViewModels/Document/DocumentResponseModelBase.cs
@@ -1,11 +1,9 @@
-using Umbraco.Cms.Api.Common.Attributes;
 using Umbraco.Cms.Api.Management.ViewModels.Content;
 using Umbraco.Cms.Api.Management.ViewModels.DocumentType;
 using Umbraco.Cms.Core.Models.ContentEditing;
 
 namespace Umbraco.Cms.Api.Management.ViewModels.Document;
 
-[ShortGenericSchemaName<DocumentValueModel, DocumentVariantResponseModel>("ContentForDocumentResponseModel")]
 public abstract class DocumentResponseModelBase<TValueResponseModelBase, TVariantResponseModel>
     : ContentResponseModelBase<TValueResponseModelBase, TVariantResponseModel>
     where TValueResponseModelBase : ValueModelBase
