7fe1fa772d31368b06bd367c15797d502715d22e
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-04-09 10:19:59 +0200

    set background with logo for backdrop when first signing in

--- src/Umbraco.Web.UI.Client/src/apps/app/app-auth.controller.ts
+++ src/Umbraco.Web.UI.Client/src/apps/app/app-auth.controller.ts
@@ -122,8 +122,8 @@ export class UmbAppAuthController extends UmbControllerBase {
 				},
 				modal: {
 					backdropBackground: this.#firstTimeLoggingIn
-						? 'var(--umb-auth-backdrop, rgba(0, 0, 0, 0.75))'
-						: 'var(--umb-auth-backdrop-timedout, #000)',
+						? 'var(--umb-auth-backdrop, url("/umbraco/backoffice/assets/umbraco_logo_white.svg") 20px 20px / 200px no-repeat, radial-gradient(circle, rgba(2,0,36,1) 0%, rgba(40,58,151,.9) 50%, rgba(0,212,255,1) 100%))'
+						: 'var(--umb-auth-backdrop-timedout, rgba(0,0,0,0.75))',
 				},
 			})
 			.onSubmit()
