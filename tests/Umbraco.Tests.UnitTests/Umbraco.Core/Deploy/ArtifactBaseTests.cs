4df47554eb0e2224aae0d07764acb2d0a97d0796
Bjarke Berg <mail@bergmania.dk>
2024-04-10 19:53:01 +0200

    Post fix after merge.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/Deploy/ArtifactBaseTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Deploy/ArtifactBaseTests.cs
@@ -23,7 +23,6 @@ public class ArtifactBaseTests
             Converters =
             {
                 new JsonUdiConverter(),
-                new JsonGuidUdiConverter()
             }
         });
 
