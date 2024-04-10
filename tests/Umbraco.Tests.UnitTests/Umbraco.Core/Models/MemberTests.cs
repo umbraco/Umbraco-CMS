fd25fe045900f43c3e7f7365bd1af2c6d8fa2bc8
kjac <kja@umbraco.dk>
2024-04-10 12:45:00 +0200

    Remove "additional data" from entities

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/Models/MemberTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Models/MemberTests.cs
@@ -2,7 +2,6 @@
 // See LICENSE for more details.
 
 using System.Diagnostics;
-using System.Linq;
 using Newtonsoft.Json;
 using NUnit.Framework;
 using Umbraco.Cms.Core.Models;
@@ -33,7 +32,6 @@ public class MemberTests
         Assert.AreEqual(clone, member);
         Assert.AreEqual(clone.Id, member.Id);
         Assert.AreEqual(clone.VersionId, member.VersionId);
-        Assert.AreEqual(clone.AdditionalData, member.AdditionalData);
         Assert.AreEqual(clone.ContentType, member.ContentType);
         Assert.AreEqual(clone.ContentTypeId, member.ContentTypeId);
         Assert.AreEqual(clone.CreateDate, member.CreateDate);
@@ -123,10 +121,6 @@ public class MemberTests
             .AddMemberGroups()
             .WithValue("Group 1")
             .WithValue("Group 2")
-            .Done()
-            .AddAdditionalData()
-            .WithKeyValue("test1", 123)
-            .WithKeyValue("test2", "hello")
             .Done()
             .WithPropertyIdsIncrementingFrom(200)
             .AddPropertyData()
