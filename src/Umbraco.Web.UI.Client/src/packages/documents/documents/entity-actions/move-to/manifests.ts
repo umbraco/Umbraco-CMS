caeb5dce2c4f68a7dd10dbe2e9b27b533a2fea6d
Mads Rasmussen <madsr@hey.com>
2024-04-11 13:48:18 +0200

    add move document

---
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/entity-actions/move-to/manifests.ts
@@ -0,0 +1,23 @@
+import { UMB_DOCUMENT_ENTITY_TYPE } from '../../entity.js';
+import { UMB_DOCUMENT_PICKER_MODAL } from '../../modals/document-picker-modal.token.js';
+import { UMB_DOCUMENT_TREE_REPOSITORY_ALIAS } from '../../tree/index.js';
+import { UMB_MOVE_DOCUMENT_REPOSITORY_ALIAS } from './repository/index.js';
+import { manifests as repositoryManifests } from './repository/manifests.js';
+import type { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
+
+const entityActions: Array<ManifestTypes> = [
+	{
+		type: 'entityAction',
+		kind: 'moveTo',
+		alias: 'Umb.EntityAction.Document.Move',
+		name: 'Move Document Entity Action',
+		forEntityTypes: [UMB_DOCUMENT_ENTITY_TYPE],
+		meta: {
+			treeRepositoryAlias: UMB_DOCUMENT_TREE_REPOSITORY_ALIAS,
+			moveToRepositoryAlias: UMB_MOVE_DOCUMENT_REPOSITORY_ALIAS,
+			treePickerModal: UMB_DOCUMENT_PICKER_MODAL,
+		},
+	},
+];
+
+export const manifests = [...entityActions, ...repositoryManifests];
---
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/entity-actions/move-to/repository/document-move.repository.ts
@@ -0,0 +1,20 @@
+import { UmbMoveDocumentServerDataSource } from './document-move.server.data-source.js';
+import { UMB_NOTIFICATION_CONTEXT } from '@umbraco-cms/backoffice/notification';
+import type { UmbMoveToRepository, UmbMoveToRequestArgs } from '@umbraco-cms/backoffice/repository';
+import { UmbRepositoryBase } from '@umbraco-cms/backoffice/repository';
+
+export class UmbMoveDocumentRepository extends UmbRepositoryBase implements UmbMoveToRepository {
+	#moveSource = new UmbMoveDocumentServerDataSource(this);
+
+	async requestMove(args: UmbMoveToRequestArgs) {
+		const { error } = await this.#moveSource.move(args);
+
+		if (!error) {
+			const notificationContext = await this.getContext(UMB_NOTIFICATION_CONTEXT);
+			const notification = { data: { message: `Moved` } };
+			notificationContext.peek('positive', notification);
+		}
+
+		return { error };
+	}
+}
