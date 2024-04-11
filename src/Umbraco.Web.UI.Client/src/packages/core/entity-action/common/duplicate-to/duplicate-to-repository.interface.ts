91017c19ecca783d8885b793bd2341fa3f33f64d
Mads Rasmussen <madsr@hey.com>
2024-04-11 20:43:58 +0200

    collect all in one folder

--- src/Umbraco.Web.UI.Client/src/packages/core/entity-action/common/duplicate-to/duplicate-to-repository.interface.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/entity-action/common/duplicate-to/duplicate-to-repository.interface.ts
@@ -1,4 +1,4 @@
-import type { UmbRepositoryErrorResponse } from '../types.js';
+import type { UmbRepositoryErrorResponse } from '../../../repository/types.js';
 import type { UmbDuplicateToRequestArgs } from './types.js';
 import type { UmbApi } from '@umbraco-cms/backoffice/extension-api';
 
--- src/Umbraco.Web.UI.Client/src/packages/core/entity-action/common/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/entity-action/common/index.ts
@@ -1,4 +1,4 @@
-export * from './duplicate/duplicate.action.js';
+export * from './duplicate-to/index.js';
 export * from './delete/delete.action.js';
 export * from './move-to/move-to.action.js';
 export * from './sort-children-of/sort-children-of.action.js';
