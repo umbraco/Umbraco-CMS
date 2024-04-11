85747593ccc943eab5bb09a93b1ddead3eef3bc0
Alina-Magdalena Tincas <83591955+alina-tincas@users.noreply.github.com>
2024-04-11 11:19:23 +0200

    Update v14 api docs with info from umbraco docs (#15977)
    * update api docs
    
    * small update

--- src/Umbraco.Core/Notifications/MediaSavedNotification.cs
+++ src/Umbraco.Core/Notifications/MediaSavedNotification.cs
@@ -5,14 +5,24 @@ using Umbraco.Cms.Core.Events;
 using Umbraco.Cms.Core.Models;
 
 namespace Umbraco.Cms.Core.Notifications;
-
+/// <summary>
+///  A notification that is used to trigger the IMediaService when the Save method is called in the API and after the data has been persisted.
+/// </summary>
 public sealed class MediaSavedNotification : SavedNotification<IMedia>
 {
     public MediaSavedNotification(IMedia target, EventMessages messages)
         : base(target, messages)
     {
     }
-
+    /// <summary>
+    /// Initializes a new instance of the <see cref="MediaSavedNotification"/>.
+    /// </summary>
+    /// <param name="target">
+    ///  Gets the saved collection of <see cref="IMedia"/> objects.
+    /// </param>
+    /// <param name="messages">
+    /// Initializes a new instance of the <see cref="EventMessages"/>.
+    /// </param>
     public MediaSavedNotification(IEnumerable<IMedia> target, EventMessages messages)
         : base(target, messages)
     {
