df67dfbb37dac6f7c53bb465ff7e7cb268318831
Sven Geusens <sge@umbraco.dk>
2024-04-12 13:13:58 +0200

    Improved faulty query fix by calling a cached path.

--- src/Umbraco.Core/Services/TemplateService.cs
+++ src/Umbraco.Core/Services/TemplateService.cs
@@ -139,11 +139,12 @@ public class TemplateService : RepositoryService, ITemplateService
     {
         using ICoreScope scope = ScopeProvider.CreateCoreScope(autoComplete: true);
 
-        IQuery<ITemplate> query = Query<ITemplate>();
-        if (keys.Any())
+        if (keys.Any() == false)
         {
-            query = query.Where(x => keys.Contains(x.Key));
+            return Task.FromResult((IEnumerable<ITemplate>)_templateRepository.GetAll().OrderBy(x => x.Name));
         }
+
+        IQuery<ITemplate> query = Query<ITemplate>().Where(x => keys.Contains(x.Key));
         IEnumerable<ITemplate> templates = _templateRepository.Get(query).OrderBy(x => x.Name);
 
         return Task.FromResult(templates);
