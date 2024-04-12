fe693ac2c5decea4a9ea8e25fe7b44617ec84334
kjac <kja@umbraco.dk>
2024-04-12 07:03:04 +0200

    Clean up FIXMEs in security controllers

--- src/Umbraco.Cms.Api.Management/Controllers/Security/ResetPasswordController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/Security/ResetPasswordController.cs
@@ -12,7 +12,6 @@ using Umbraco.Cms.Web.Common.Authorization;
 namespace Umbraco.Cms.Api.Management.Controllers.Security;
 
 [ApiVersion("1.0")]
-// FIXME: Add requiring password reset token policy when its implemented
 [Authorize(Policy = AuthorizationPolicies.DenyLocalLoginIfConfigured)]
 public class ResetPasswordController : SecurityControllerBase
 {
