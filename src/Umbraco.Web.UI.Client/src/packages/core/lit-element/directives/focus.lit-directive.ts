7d827bf595b387713dc1698e06ca939007d3926e
Niels Lyngsø <niels.lyngso@gmail.com>
2024-04-12 15:11:23 +0200

    fix directive

--- src/Umbraco.Web.UI.Client/src/packages/core/lit-element/directives/focus.lit-directive.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/lit-element/directives/focus.lit-directive.ts
@@ -1,5 +1,8 @@
 import { AsyncDirective, directive, nothing, type ElementPart } from '@umbraco-cms/backoffice/external/lit';
 
+/**
+ * The `focus` directive sets focus on the given element once its connected to the DOM.
+ */
 class UmbFocusDirective extends AsyncDirective {
 	private _el?: HTMLElement;
 
@@ -9,7 +12,13 @@ class UmbFocusDirective extends AsyncDirective {
 
 	override update(part: ElementPart) {
 		if (this._el !== part.element) {
-			(this._el = part.element as HTMLElement).focus();
+			// This does feel wrong that we need to wait one render. [NL]
+			// Because even if our elements focus method is implemented so it can be called initially, my research shows that calling the focus method at this point is too early, thought the element is connected to the DOM and the focus method is available. [NL]
+			// This smells a bit like the DOMPart of which the directive is in is not connected to the main DOM yet, and therefor cant receive focus. [NL]
+			// Which is why we need to await one render: [NL]
+			requestAnimationFrame(() => {
+				(this._el = part.element as HTMLElement).focus();
+			});
 		}
 		return nothing;
 	}
@@ -22,12 +31,14 @@ class UmbFocusDirective extends AsyncDirective {
 }
 
 /**
- * Sets focus on the given element once its connected to the DOM.
+ * @description
+ * A Lit directive, which sets focus on the element of scope once its connected to the DOM.
  *
+ * @example:
  * ```js
- * render(html`<input ${focus}>`, container);
+ * html`<input ${umbFocus()}>`;
  * ```
  */
-export const focus = directive(UmbFocusDirective);
+export const umbFocus = directive(UmbFocusDirective);
 
-export type { UmbFocusDirective };
+//export type { UmbFocusDirective };
