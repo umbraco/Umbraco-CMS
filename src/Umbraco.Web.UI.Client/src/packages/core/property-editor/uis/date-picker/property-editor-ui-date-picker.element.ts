361cde89fcad33640942534a0249a482b382fe51
leekelleher <leekelleher@gmail.com>
2024-04-15 11:14:46 +0100

    DatePicker: refactor
    Removes the server time offset functionality.
    Wires up the `value` and handles UTC timezone correction.

--- src/Umbraco.Web.UI.Client/src/packages/core/property-editor/uis/date-picker/property-editor-ui-date-picker.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property-editor/uis/date-picker/property-editor-ui-date-picker.element.ts
@@ -1,43 +1,15 @@
-import type { UmbPropertyEditorConfigCollection } from '../../index.js';
 import { UmbPropertyValueChangeEvent } from '../../index.js';
-import { html, customElement, property, state } from '@umbraco-cms/backoffice/external/lit';
-import type { UmbPropertyEditorUiElement } from '@umbraco-cms/backoffice/extension-registry';
+import type { UmbPropertyEditorConfigCollection } from '../../index.js';
+import { html, customElement, property, state, ifDefined } from '@umbraco-cms/backoffice/external/lit';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
 import type { UmbInputDateElement } from '@umbraco-cms/backoffice/components';
+import type { UmbPropertyEditorUiElement } from '@umbraco-cms/backoffice/extension-registry';
 
 /**
  * @element umb-property-editor-ui-date-picker
  */
 @customElement('umb-property-editor-ui-date-picker')
 export class UmbPropertyEditorUIDatePickerElement extends UmbLitElement implements UmbPropertyEditorUiElement {
-	private _value?: Date;
-	private _valueString?: string;
-
-	@property()
-	set value(value: string | undefined) {
-		if (value) {
-			const d = new Date(value);
-			this._value = d;
-			this._valueString = `${d.getFullYear()}-${
-				d.getMonth() + 1
-			}-${d.getDate()}T${d.getHours()}:${d.getMinutes()}:${d.getSeconds()}`;
-		} else {
-			this._value = undefined;
-			this._valueString = undefined;
-		}
-	}
-	get value() {
-		return this._valueString;
-	}
-
-	private _onInput(e: InputEvent) {
-		const dateField = e.target as HTMLInputElement;
-		this.value = dateField.value;
-		this.dispatchEvent(new UmbPropertyValueChangeEvent());
-	}
-
-	private _format?: string;
-
 	@state()
 	private _inputType: UmbInputDateElement['type'] = 'datetime-local';
 
@@ -50,29 +22,34 @@ export class UmbPropertyEditorUIDatePickerElement extends UmbLitElement implemen
 	@state()
 	private _step?: number;
 
-	private _offsetTime?: boolean;
+	@property()
+	set value(value: string | undefined) {
+		if (value) {
+			// NOTE: If the `value` contains a space, then it doesn't contain the timezone, so may not be parsed as UTC. [LK]
+			const datetime = !value.includes(' ') ? value : value + ' +00';
+			this.#value = new Date(datetime).toJSON();
+		}
+	}
+	get value() {
+		return this.#value;
+	}
+	#value?: string;
 
 	public set config(config: UmbPropertyEditorConfigCollection | undefined) {
 		if (!config) return;
 		const oldVal = this._inputType;
 
 		// Format string prevalue/config
-		this._format = config.getValueByAlias('format');
-		const pickTime = this._format?.includes('H') || this._format?.includes('m');
-		if (pickTime) {
-			this._inputType = 'datetime-local';
-		} else {
-			this._inputType = 'date';
-		}
+		const format = config.getValueByAlias<string>('format');
+		const hasTime = format?.includes('H') || format?.includes('m');
+		this._inputType = hasTime ? 'datetime-local' : 'date';
 
 		// Based on the type of format string change the UUI-input type
 		const timeFormatPattern = /^h{1,2}:m{1,2}(:s{1,2})?\s?a?$/gim;
-		if (this._format?.toLowerCase().match(timeFormatPattern)) {
+		if (format?.toLowerCase().match(timeFormatPattern)) {
 			this._inputType = 'time';
 		}
 
-		//TODO:
-		this._offsetTime = config.getValueByAlias('offsetTime');
 		this._min = config.getValueByAlias('min');
 		this._max = config.getValueByAlias('max');
 		this._step = config.getValueByAlias('step');
@@ -80,16 +57,22 @@ export class UmbPropertyEditorUIDatePickerElement extends UmbLitElement implemen
 		this.requestUpdate('_inputType', oldVal);
 	}
 
+	#onChange(event: CustomEvent & { target: HTMLInputElement }) {
+		this.value = event.target.value;
+		this.dispatchEvent(new UmbPropertyValueChangeEvent());
+	}
+
 	render() {
-		return html`<umb-input-date
-			.type=${this._inputType}
-			@input=${this._onInput}
-			.datetime=${this._valueString}
-			.min=${this._min}
-			.max=${this._max}
-			.step=${this._step}
-			.offsetTime=${this._offsetTime || false}
-			label="Pick a date or time"></umb-input-date>`;
+		return html`
+			<umb-input-date
+				value="${ifDefined(this.value)}"
+				.min=${this._min}
+				.max=${this._max}
+				.step=${this._step}
+				.type=${this._inputType}
+				@change=${this.#onChange}>
+			</umb-input-date>
+		`;
 	}
 }
 
