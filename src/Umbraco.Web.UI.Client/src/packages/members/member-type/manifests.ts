c6bdeaf5bfe9a90545dc4961eccb393397763f36
JesmoDev <26099018+JesmoDev@users.noreply.github.com>
2024-04-15 12:57:09 +0200

    member types

--- src/Umbraco.Web.UI.Client/src/packages/members/member-type/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/members/member-type/manifests.ts
@@ -1,6 +1,7 @@
 import { manifests as entityActionsManifests } from './entity-actions/manifests.js';
 import { manifests as menuManifests } from './menu/manifests.js';
 import { manifests as repositoryManifests } from './repository/manifests.js';
+import { manifests as searchManifests } from './search/manifests.js';
 import { manifests as treeManifests } from './tree/manifests.js';
 import { manifests as workspaceManifests } from './workspace/manifests.js';
 
@@ -10,6 +11,7 @@ export const manifests = [
 	...entityActionsManifests,
 	...menuManifests,
 	...repositoryManifests,
+	...searchManifests,
 	...treeManifests,
 	...workspaceManifests,
 ];
