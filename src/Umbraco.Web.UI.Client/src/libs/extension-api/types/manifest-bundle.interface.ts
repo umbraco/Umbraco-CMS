68e51e505362c26499360ffeee6aed58ed53b3d6
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-04-16 16:16:40 +0200

    revert bundles

--- src/Umbraco.Web.UI.Client/src/libs/extension-api/types/manifest-bundle.interface.ts
+++ src/Umbraco.Web.UI.Client/src/libs/extension-api/types/manifest-bundle.interface.ts
@@ -7,12 +7,4 @@ import type { ManifestBase } from './manifest-base.interface.js';
 export interface ManifestBundle<UmbManifestTypes extends ManifestBase = ManifestBase>
 	extends ManifestPlainJs<{ [key: string]: Array<UmbManifestTypes> }> {
 	type: 'bundle';
-
-	/**
-	 * The scope of the bundle. If global, the bundle will be loaded on the root host and never be destroyed.
-	 * If local, the bundle will be loaded on the BackofficeElement and will be destroyed when the host is removed for example when signing out.
-	 * @default 'local'
-	 * @enum ['global', 'local']
-	 */
-	scope?: 'global' | 'local';
 }
