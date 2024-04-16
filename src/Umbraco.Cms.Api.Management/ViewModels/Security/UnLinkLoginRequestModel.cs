97846a28d171929762c5e489a4e0b0db1fef3316
Sven Geusens <sge@umbraco.dk>
2024-04-16 14:58:45 +0200

    More cleanup

--- src/Umbraco.Cms.Api.Management/ViewModels/Security/UnLinkLoginRequestModel.cs
+++ src/Umbraco.Cms.Api.Management/ViewModels/Security/UnLinkLoginRequestModel.cs
@@ -6,10 +6,8 @@ namespace Umbraco.Cms.Api.Management.ViewModels.Security;
 public class UnLinkLoginRequestModel
 {
     [Required]
-    [DataMember(Name = "loginProvider", IsRequired = true)]
     public required string LoginProvider { get; set; }
 
     [Required]
-    [DataMember(Name = "providerKey", IsRequired = true)]
     public required string ProviderKey { get; set; }
 }
