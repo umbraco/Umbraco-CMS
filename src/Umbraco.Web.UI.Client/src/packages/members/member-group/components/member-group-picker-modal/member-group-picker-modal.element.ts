7f257b608f5877ead09d909dee652c538f4460ec
JesmoDev <26099018+JesmoDev@users.noreply.github.com>
2024-04-18 13:00:59 +0200

    fix icons

--- src/Umbraco.Web.UI.Client/src/packages/members/member-group/components/member-group-picker-modal/member-group-picker-modal.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/members/member-group/components/member-group-picker-modal/member-group-picker-modal.element.ts
@@ -61,7 +61,7 @@ export class UmbMemberGroupPickerModalElement extends UmbModalBaseElement<
 							@selected=${() => this.#selectionManager.select(item.unique)}
 							@deselected=${() => this.#selectionManager.deselect(item.unique)}
 							?selected=${this.#selectionManager.isSelected(item.unique)}>
-							<uui-icon slot="icon" name="icon-globe"></uui-icon>
+							<uui-icon slot="icon" name="icon-users"></uui-icon>
 						</uui-menu-item>
 					`,
 				)}
