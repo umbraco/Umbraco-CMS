0307fe09fd2a8902b23b5d49b813cc36bbcaa437
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-04-18 14:50:06 +0200

    add jsdoc

--- src/Umbraco.Web.UI.Client/src/libs/extension-api/types/manifest-app-entrypoint.interface.ts
+++ src/Umbraco.Web.UI.Client/src/libs/extension-api/types/manifest-app-entrypoint.interface.ts
@@ -2,8 +2,11 @@ import type { UmbEntryPointModule } from '../models/index.js';
 import type { ManifestPlainJs } from './base.types.js';
 
 /**
- * This type of extension gives full control and will simply load the specified JS file
- * You could have custom logic to decide which extensions to load/register by using extensionRegistry
+ * Manifest for an `appEntryPoint`, which is loaded up front when the app starts.
+ *
+ * This type of extension gives full control and will simply load the specified JS file.
+ * You could have custom logic to decide which extensions to load/register by using extensionRegistry.
+ * This is useful for extensions that need to be loaded up front, like an `authProvider`.
  */
 export interface ManifestAppEntryPoint extends ManifestPlainJs<UmbEntryPointModule> {
 	type: 'appEntryPoint';
--- src/Umbraco.Web.UI.Client/src/libs/extension-api/types/manifest-entrypoint.interface.ts
+++ src/Umbraco.Web.UI.Client/src/libs/extension-api/types/manifest-entrypoint.interface.ts
@@ -2,8 +2,10 @@ import type { UmbEntryPointModule } from '../models/index.js';
 import type { ManifestPlainJs } from './base.types.js';
 
 /**
- * This type of extension gives full control and will simply load the specified JS file
- * You could have custom logic to decide which extensions to load/register by using extensionRegistry
+ * Manifest for an `entryPoint`, which is loaded after the Backoffice has been loaded and authentication has been done.
+ *
+ * This type of extension gives full control and will simply load the specified JS file.
+ * You could have custom logic to decide which extensions to load/register by using extensionRegistry.
  */
 export interface ManifestEntryPoint extends ManifestPlainJs<UmbEntryPointModule> {
 	type: 'entryPoint';
