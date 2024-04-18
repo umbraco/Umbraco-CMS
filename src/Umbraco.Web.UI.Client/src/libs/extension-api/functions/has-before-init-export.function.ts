fc515ecd687640ff2fe79cb34f0dba70404c495e
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-04-18 09:18:14 +0200

    chore: correct typo in jsdoc

--- src/Umbraco.Web.UI.Client/src/libs/extension-api/functions/has-before-init-export.function.ts
+++ src/Umbraco.Web.UI.Client/src/libs/extension-api/functions/has-before-init-export.function.ts
@@ -1,7 +1,7 @@
 import type { UmbEntryPointModule } from '../models/entry-point.interface.js';
 
 /**
- * Validate if an ESModule export has a function called 'befireInit'
+ * Validate if an ESModule has exported a function called `beforeInit`
  */
 export function hasBeforeInitExport(obj: unknown): obj is Pick<UmbEntryPointModule, 'beforeInit'> {
 	return obj !== null && typeof obj === 'object' && 'beforeInit' in obj;
