448a03b12a57dbb089862b558b85557e80eef4d8
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-04-18 14:32:58 +0200

    remove the beforeInit function

--- src/Umbraco.Web.UI.Client/src/libs/extension-api/models/entry-point.interface.ts
+++ src/Umbraco.Web.UI.Client/src/libs/extension-api/models/entry-point.interface.ts
@@ -2,7 +2,6 @@ import type { UmbExtensionRegistry } from '../registry/extension.registry.js';
 import type { ManifestBase } from '../types/index.js';
 import type { UmbElement } from '@umbraco-cms/backoffice/element-api';
 
-export type UmbEntryPointBeforeInit = (host: UmbElement, extensionRegistry: UmbExtensionRegistry<ManifestBase>) => void;
 export type UmbEntryPointOnInit = (host: UmbElement, extensionRegistry: UmbExtensionRegistry<ManifestBase>) => void;
 
 /**
@@ -10,15 +9,7 @@ export type UmbEntryPointOnInit = (host: UmbElement, extensionRegistry: UmbExten
  */
 export interface UmbEntryPointModule {
 	/**
-	 * Function that will be called when the main element is loaded as the first thing
-	 * before any app logic is executed such as authentication.
-	 * @remark The 'host' argument is the umb-app element.
-	 */
-	beforeInit: UmbEntryPointBeforeInit;
-
-	/**
 	 * Function that will be called when the backoffice element is initialized after authentication.
-	 * @remark The 'host' argument is the umb-backoffice element.
 	 */
 	onInit: UmbEntryPointOnInit;
 }
