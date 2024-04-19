7112b35fca47a76ac5cdba49f0db6e686fdf6a9c
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-04-19 14:53:19 +0200

    sort imports

--- src/Umbraco.Web.UI.Client/src/mocks/handlers/user/current.handlers.ts
+++ src/Umbraco.Web.UI.Client/src/mocks/handlers/user/current.handlers.ts
@@ -1,7 +1,7 @@
 const { rest } = window.MockServiceWorker;
 import { umbUserMockDb } from '../../data/user/user.db.js';
-import type { LinkedLoginsRequestModel } from '@umbraco-cms/backoffice/external/backend-api';
 import { UMB_SLUG } from './slug.js';
+import type { LinkedLoginsRequestModel } from '@umbraco-cms/backoffice/external/backend-api';
 import { umbracoPath } from '@umbraco-cms/backoffice/utils';
 
 export const handlers = [
