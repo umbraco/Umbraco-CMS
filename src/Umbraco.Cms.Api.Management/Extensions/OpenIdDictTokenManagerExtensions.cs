037f81ad5ad16f79d25e6e597d249847602db69d
Zeegaan <skrivdetud@gmail.com>
2024-04-19 08:56:46 +0200

    skip length check

--- src/Umbraco.Cms.Api.Management/Extensions/OpenIdDictTokenManagerExtensions.cs
+++ src/Umbraco.Cms.Api.Management/Extensions/OpenIdDictTokenManagerExtensions.cs
@@ -7,10 +7,6 @@ public static class OpenIdDictTokenManagerExtensions
     public static async Task RevokeUmbracoUserTokens(this IOpenIddictTokenManager openIddictTokenManager, Guid userKey)
     {
         var tokens = await openIddictTokenManager.FindBySubjectAsync(userKey.ToString()).ToArrayAsync();
-        if (tokens.Length == 0)
-        {
-            return;
-        }
 
         foreach (var token in tokens)
         {
