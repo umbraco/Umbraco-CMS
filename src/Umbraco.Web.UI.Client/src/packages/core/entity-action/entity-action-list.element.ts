740d388c8bd0f99bab811ecfbaff0667c9544f50
Niels Lyngsø <niels.lyngso@gmail.com>
2024-04-22 15:05:14 +0200

    entity action list first focus

--- src/Umbraco.Web.UI.Client/src/packages/core/entity-action/entity-action-list.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/entity-action/entity-action-list.element.ts
@@ -30,6 +30,7 @@ export class UmbEntityActionListElement extends UmbLitElement {
 		return this._props.unique;
 	}
 	public set unique(value: string | null | undefined) {
+		if (value === this._props.unique) return;
 		this._props.unique = value;
 		this.#generateApiArgs();
 		this.requestUpdate('_props');
@@ -51,12 +52,14 @@ export class UmbEntityActionListElement extends UmbLitElement {
 
 		this.#entityContext.setEntityType(this._props.entityType);
 		this.#entityContext.setUnique(this._props.unique);
+		this.#hasRenderedOnce = false;
 
 		this._apiArgs = (manifest: ManifestEntityAction<MetaEntityAction>) => {
 			return [{ entityType: this._props.entityType!, unique: this._props.unique!, meta: manifest.meta }];
 		};
 	}
 
+	#hasRenderedOnce?: boolean;
 	render() {
 		return this._filter
 			? html`
@@ -64,7 +67,22 @@ export class UmbEntityActionListElement extends UmbLitElement {
 						type="entityAction"
 						.filter=${this._filter}
 						.elementProps=${this._props}
-						.apiArgs=${this._apiArgs}></umb-extension-with-api-slot>
+						.apiArgs=${this._apiArgs}
+						.renderMethod=${(ext: any, i: number) => {
+							if (!this.#hasRenderedOnce && i === 0) {
+								// TODO: Replace this block:
+								ext.component?.updateComplete.then(async () => {
+									const menuitem = ext.component?.shadowRoot?.querySelector('uui-menu-item');
+									menuitem?.updateComplete.then(async () => {
+										menuitem?.shadowRoot?.querySelector('#label-button')?.focus?.();
+									});
+								});
+								// end of block, with this, when this PR is part of UI Lib: https://github.com/umbraco/Umbraco.UI/pull/789
+								// ext.component?.focus();
+								this.#hasRenderedOnce = true;
+							}
+							return ext.component;
+						}}></umb-extension-with-api-slot>
 				`
 			: '';
 	}
