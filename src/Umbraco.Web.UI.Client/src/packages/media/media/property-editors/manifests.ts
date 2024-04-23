013fdaeaff25fd5ec47c2c2673f9c0e7d3db52f5
Mads Rasmussen <madsr@hey.com>
2024-04-23 21:53:30 +0200

    update

--- src/Umbraco.Web.UI.Client/src/packages/media/media/property-editors/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/property-editors/manifests.ts
@@ -1,6 +1,6 @@
-import { manifest as imageCropper } from './image-cropper/manifests.js';
+import { manifests as imageCropperManifests } from './image-cropper/manifests.js';
 import { manifest as imageCropsConfiguration } from './image-crops-configuration/manifests.js';
 import { manifest as mediaEntityPicker } from './media-entity-picker/manifests.js';
-import { manifest as mediaPicker } from './media-picker/manifests.js';
+import { manifests as mediaPickerManifests } from './media-picker/manifests.js';
 
-export const manifests = [imageCropper, imageCropsConfiguration, mediaEntityPicker, mediaPicker];
+export const manifests = [imageCropperManifests, imageCropsConfiguration, mediaEntityPicker, mediaPickerManifests];
--- src/Umbraco.Web.UI.Client/src/packages/property-editors/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/property-editors/manifests.ts
@@ -1,11 +1,8 @@
 import { manifest as colorEditor } from './color-swatches-editor/manifests.js';
-import { manifest as iconPicker } from './icon-picker/manifests.js';
-import { manifest as multiUrlPicker } from './multi-url-picker/manifests.js';
 import { manifest as numberRange } from './number-range/manifests.js';
 import { manifest as orderDirection } from './order-direction/manifests.js';
 import { manifest as overlaySize } from './overlay-size/manifests.js';
 import { manifest as select } from './select/manifests.js';
-import { manifest as uploadField } from './upload-field/manifests.js';
 import { manifest as valueType } from './value-type/manifests.js';
 import { manifests as checkboxListManifests } from './checkbox-list/manifests.js';
 import { manifests as collectionView } from './collection-view/manifests.js';
@@ -13,15 +10,18 @@ import { manifests as colorPickerManifests } from './color-picker/manifests.js';
 import { manifests as datePickerManifests } from './date-picker/manifests.js';
 import { manifests as dropdownManifests } from './dropdown/manifests.js';
 import { manifests as eyeDropperManifests } from './eye-dropper/manifests.js';
+import { manifests as iconPickerManifests } from './icon-picker/manifests.js';
 import { manifests as labelManifests } from './label/manifests.js';
 import { manifests as multipleTextStringManifests } from './multiple-text-string/manifests.js';
-import { manifests as numbers } from './number/manifests.js';
+import { manifests as multiUrlPickerManifests } from './multi-url-picker/manifests.js';
+import { manifests as numberManifests } from './number/manifests.js';
 import { manifests as radioButtonListManifests } from './radio-button-list/manifests.js';
 import { manifests as sliderManifests } from './slider/manifests.js';
 import { manifests as textareaManifests } from './textarea/manifests.js';
 import { manifests as textBoxManifests } from './text-box/manifests.js';
 import { manifests as toggleManifests } from './toggle/manifests.js';
-import { manifests as treePicker } from './tree-picker/manifests.js';
+import { manifests as treePickerManifests } from './tree-picker/manifests.js';
+import { manifests as uploadFieldManifests } from './upload-field/manifests.js';
 import type { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
 
 export const manifests: Array<ManifestTypes> = [
@@ -31,22 +31,22 @@ export const manifests: Array<ManifestTypes> = [
 	...datePickerManifests,
 	...dropdownManifests,
 	...eyeDropperManifests,
+	...iconPickerManifests,
 	...labelManifests,
 	...multipleTextStringManifests,
-	...numbers,
+	...multiUrlPickerManifests,
+	...numberManifests,
 	...radioButtonListManifests,
 	...sliderManifests,
 	...textareaManifests,
 	...textBoxManifests,
 	...toggleManifests,
-	...treePicker,
+	...treePickerManifests,
+	...uploadFieldManifests,
 	colorEditor,
-	iconPicker,
-	multiUrlPicker,
 	numberRange,
 	orderDirection,
 	overlaySize,
 	select,
-	uploadField,
 	valueType,
 ];
