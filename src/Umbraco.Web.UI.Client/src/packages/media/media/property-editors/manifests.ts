4297a791fde9cea057f85db24cd0ecee19bc6c6a
Mads Rasmussen <madsr@hey.com>
2024-04-23 22:02:46 +0200

    expand array

--- src/Umbraco.Web.UI.Client/src/packages/media/media/property-editors/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/property-editors/manifests.ts
@@ -2,5 +2,11 @@ import { manifests as imageCropperManifests } from './image-cropper/manifests.js
 import { manifest as imageCropsConfiguration } from './image-crops-configuration/manifests.js';
 import { manifest as mediaEntityPicker } from './media-entity-picker/manifests.js';
 import { manifests as mediaPickerManifests } from './media-picker/manifests.js';
+import type { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
 
-export const manifests = [imageCropperManifests, imageCropsConfiguration, mediaEntityPicker, mediaPickerManifests];
+export const manifests: Array<ManifestTypes> = [
+	...imageCropperManifests,
+	imageCropsConfiguration,
+	mediaEntityPicker,
+	...mediaPickerManifests,
+];
