4021f7c170f2253119103a7dc9858ca4da4766d2
Mads Rasmussen <madsr@hey.com>
2024-04-23 13:14:20 +0200

    move slider

--- src/Umbraco.Web.UI.Client/src/packages/core/property-editor/uis/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property-editor/uis/manifests.ts
@@ -13,7 +13,7 @@ import { manifest as orderDirection } from '../../../property-editors/order-dire
 import { manifest as overlaySize } from '../../../property-editors/overlay-size/manifests.js';
 import { manifest as radioButtonList } from '../../../property-editors/radio-button-list/manifests.js';
 import { manifest as select } from '../../../property-editors/select/manifests.js';
-import { manifest as slider } from './slider/manifests.js';
+import { manifest as slider } from '../../../property-editors/slider/manifests.js';
 import { manifest as textArea } from './textarea/manifests.js';
 import { manifest as toggle } from './toggle/manifests.js';
 import { manifest as uploadField } from './upload-field/manifests.js';
