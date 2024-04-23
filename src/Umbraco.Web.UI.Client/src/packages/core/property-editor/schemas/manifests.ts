0ae31dd176946738d6c72efea1c66dd1f8c75614
Mads Rasmussen <madsr@hey.com>
2024-04-23 20:41:22 +0200

    move schemas

--- src/Umbraco.Web.UI.Client/src/packages/core/property-editor/schemas/manifests.ts
+++
@@ -1,8 +0,0 @@
-import { manifest as imageCropper } from './Umbraco.ImageCropper.js';
-import { manifest as markdownEditor } from './Umbraco.MarkdownEditor.js';
-import { manifest as mediaPicker } from './Umbraco.MediaPicker.js';
-import { manifest as richText } from './Umbraco.RichText.js';
-
-import type { ManifestPropertyEditorSchema } from '@umbraco-cms/backoffice/extension-registry';
-
-export const manifests: Array<ManifestPropertyEditorSchema> = [imageCropper, markdownEditor, mediaPicker, richText];
