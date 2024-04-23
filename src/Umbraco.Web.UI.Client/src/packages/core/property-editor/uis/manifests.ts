ab444897e4cc17451ca12cd15877f32f869db850
Mads Rasmussen <madsr@hey.com>
2024-04-23 13:12:27 +0200

    Update manifests.ts

--- src/Umbraco.Web.UI.Client/src/packages/core/property-editor/uis/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property-editor/uis/manifests.ts
@@ -9,7 +9,7 @@ import { manifest as label } from '../../../property-editors/label/manifests.js'
 import { manifest as multipleTextString } from '../../../property-editors/multiple-text-string/manifests.js';
 import { manifest as multiUrlPicker } from '../../../property-editors/multi-url-picker/manifests.js';
 import { manifest as numberRange } from '../../../property-editors/number-range/manifests.js';
-import { manifest as orderDirection } from './order-direction/manifests.js';
+import { manifest as orderDirection } from '../../../property-editors/order-direction/manifests.js';
 import { manifest as overlaySize } from './overlay-size/manifests.js';
 import { manifest as radioButtonList } from './radio-button-list/manifests.js';
 import { manifest as select } from './select/manifests.js';
