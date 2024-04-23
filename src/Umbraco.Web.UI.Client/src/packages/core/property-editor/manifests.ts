ccbaff5e549be39a83f9502adae8c65eecee4c64
Mads Rasmussen <madsr@hey.com>
2024-04-23 13:23:04 +0200

    add manifest file to package

--- src/Umbraco.Web.UI.Client/src/packages/core/property-editor/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property-editor/manifests.ts
@@ -1,4 +1,3 @@
 import { manifests as propertyEditorSchemaManifests } from './schemas/manifests.js';
-import { manifests as propertyEditorUIManifests } from './uis/manifests.js';
 
-export const manifests = [...propertyEditorSchemaManifests, ...propertyEditorUIManifests];
+export const manifests = [...propertyEditorSchemaManifests];
