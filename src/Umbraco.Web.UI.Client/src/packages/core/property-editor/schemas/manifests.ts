ec43c6e5adfdd2fdfa71312f723a0fdff99ea950
Mads Rasmussen <madsr@hey.com>
2024-04-23 19:18:10 +0200

    move textarea and text box schemas

--- src/Umbraco.Web.UI.Client/src/packages/core/property-editor/schemas/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property-editor/schemas/manifests.ts
@@ -1,6 +1,5 @@
 import { manifest as blockGrid } from './Umbraco.BlockGrid.js';
 import { manifest as blockList } from './Umbraco.BlockList.js';
-import { manifest as emailAddress } from './Umbraco.EmailAddress.js';
 import { manifest as iconPicker } from './Umbraco.IconPicker.js';
 import { manifest as imageCropper } from './Umbraco.ImageCropper.js';
 import { manifest as label } from './Umbraco.Label.js';
@@ -14,8 +13,6 @@ import { manifest as multiUrlPicker } from './Umbraco.MultiUrlPicker.js';
 import { manifest as radioButtonList } from './Umbraco.RadioButtonList.js';
 import { manifest as slider } from './Umbraco.Slider.js';
 import { manifest as tags } from './Umbraco.Tags.js';
-import { manifest as textArea } from './Umbraco.TextArea.js';
-import { manifest as textBox } from './Umbraco.TextBox.js';
 import { manifest as richText } from './Umbraco.RichText.js';
 import { manifest as trueFalse } from './Umbraco.TrueFalse.js';
 import { manifest as uploadField } from './Umbraco.UploadField.js';
@@ -26,7 +23,6 @@ import type { ManifestPropertyEditorSchema } from '@umbraco-cms/backoffice/exten
 export const manifests: Array<ManifestPropertyEditorSchema> = [
 	blockGrid,
 	blockList,
-	emailAddress,
 	iconPicker,
 	imageCropper,
 	label,
@@ -40,8 +36,6 @@ export const manifests: Array<ManifestPropertyEditorSchema> = [
 	radioButtonList,
 	slider,
 	tags,
-	textArea,
-	textBox,
 	richText,
 	trueFalse,
 	uploadField,
--- src/Umbraco.Web.UI.Client/src/packages/property-editors/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/property-editors/manifests.ts
@@ -1,12 +1,6 @@
-import { manifests as checkboxListManifests } from './checkbox-list/manifests.js';
 import { manifest as colorEditor } from './color-swatches-editor/manifests.js';
-import { manifests as colorPickerManifests } from './color-picker/manifests.js';
-import { manifests as datePickerManifests } from './date-picker/manifests.js';
-import { manifests as dropdownManifests } from './dropdown/manifests.js';
-import { manifests as eyeDropperManifests } from './eye-dropper/manifests.js';
 import { manifest as iconPicker } from './icon-picker/manifests.js';
 import { manifest as label } from './label/manifests.js';
-import { manifests as multipleTextStringManifests } from './multiple-text-string/manifests.js';
 import { manifest as multiUrlPicker } from './multi-url-picker/manifests.js';
 import { manifest as numberRange } from './number-range/manifests.js';
 import { manifest as orderDirection } from './order-direction/manifests.js';
@@ -14,13 +8,19 @@ import { manifest as overlaySize } from './overlay-size/manifests.js';
 import { manifest as radioButtonList } from './radio-button-list/manifests.js';
 import { manifest as select } from './select/manifests.js';
 import { manifest as slider } from './slider/manifests.js';
-import { manifest as textArea } from './textarea/manifests.js';
 import { manifest as toggle } from './toggle/manifests.js';
 import { manifest as uploadField } from './upload-field/manifests.js';
 import { manifest as valueType } from './value-type/manifests.js';
+import { manifests as checkboxListManifests } from './checkbox-list/manifests.js';
 import { manifests as collectionView } from './collection-view/manifests.js';
+import { manifests as colorPickerManifests } from './color-picker/manifests.js';
+import { manifests as datePickerManifests } from './date-picker/manifests.js';
+import { manifests as dropdownManifests } from './dropdown/manifests.js';
+import { manifests as eyeDropperManifests } from './eye-dropper/manifests.js';
+import { manifests as multipleTextStringManifests } from './multiple-text-string/manifests.js';
 import { manifests as numbers } from './number/manifests.js';
-import { manifests as textBoxes } from './text-box/manifests.js';
+import { manifests as textareaManifests } from './textarea/manifests.js';
+import { manifests as textBoxManifests } from './text-box/manifests.js';
 import { manifests as treePicker } from './tree-picker/manifests.js';
 import type { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
 
@@ -33,7 +33,8 @@ export const manifests: Array<ManifestTypes> = [
 	...eyeDropperManifests,
 	...multipleTextStringManifests,
 	...numbers,
-	...textBoxes,
+	...textareaManifests,
+	...textBoxManifests,
 	...treePicker,
 	colorEditor,
 	iconPicker,
@@ -45,7 +46,6 @@ export const manifests: Array<ManifestTypes> = [
 	radioButtonList,
 	select,
 	slider,
-	textArea,
 	toggle,
 	uploadField,
 	valueType,
