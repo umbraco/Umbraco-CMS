f979c0358552dc31a79d1d8402ad5cfb0195af83
Mads Rasmussen <madsr@hey.com>
2024-04-25 10:07:59 +0200

    move files to the modal they are used. Only load them locally

--- src/Umbraco.Web.UI.Client/src/packages/templating/modals/templating-page-field-builder/templating-page-field-builder-modal.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/templating/modals/templating-page-field-builder/templating-page-field-builder-modal.element.ts
@@ -3,10 +3,13 @@ import type {
 	UmbTemplatingPageFieldBuilderModalData,
 	UmbTemplatingPageFieldBuilderModalValue,
 } from './templating-page-field-builder-modal.token.js';
+import type { UmbFieldDropdownListElement } from './components/field-dropdown-list/index.js';
 import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
 import { css, html, customElement, state } from '@umbraco-cms/backoffice/external/lit';
 import { UmbModalBaseElement } from '@umbraco-cms/backoffice/modal';
-import type { UmbFieldDropdownListElement } from '@umbraco-cms/backoffice/components';
+
+// import of local components
+import './components/field-dropdown-list/index.js';
 
 @customElement('umb-templating-page-field-builder-modal')
 export class UmbTemplatingPageFieldBuilderModalElement extends UmbModalBaseElement<
