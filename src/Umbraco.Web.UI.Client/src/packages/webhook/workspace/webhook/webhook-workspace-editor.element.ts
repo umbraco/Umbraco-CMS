9b6b741c42b0f2810285ffbc12059e711c68b9d8
JesmoDev <26099018+JesmoDev@users.noreply.github.com>
2024-04-30 11:40:18 +0200

    cleanup

--- src/Umbraco.Web.UI.Client/src/packages/webhook/workspace/webhook/webhook-workspace-editor.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/webhook/workspace/webhook/webhook-workspace-editor.element.ts
@@ -7,67 +7,13 @@ import { UmbLitElement, umbFocus } from '@umbraco-cms/backoffice/lit-element';
 import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
 @customElement('umb-webhook-workspace-editor')
 export class UmbWebhookWorkspaceEditorElement extends UmbLitElement {
-	#workspaceContext?: typeof UMB_WEBHOOK_WORKSPACE_CONTEXT.TYPE;
-
-	@state()
-	_webhook?: UmbWebhookDetailModel;
-
-	@state()
-	_isNew?: boolean;
-
-	constructor() {
-		super();
-
-		this.consumeContext(UMB_WEBHOOK_WORKSPACE_CONTEXT, (context) => {
-			this.#workspaceContext = context;
-			this.#observeData();
-		});
-	}
-
-	#observeData() {
-		if (!this.#workspaceContext) return;
-		this.observe(this.#workspaceContext.data, (data) => {
-			this._webhook = data;
-		});
-		this.observe(this.#workspaceContext.isNew, (isNew) => {
-			this._isNew = isNew;
-		});
-	}
-
 	render() {
 		return html`<umb-workspace-editor
 			alias="Umb.Workspace.Webhook"
 			back-path="section/settings/workspace/webhook-root"></umb-workspace-editor>`;
 	}
 
-	static styles = [
-		UmbTextStyles,
-		css`
-			#header {
-				display: flex;
-				padding: 0 var(--uui-size-space-6);
-				gap: var(--uui-size-space-4);
-				width: 100%;
-			}
-
-			uui-input {
-				width: 100%;
-			}
-
-			strong {
-				display: flex;
-				align-items: center;
-			}
-
-			#footer-into {
-				padding: 0 var(--uui-size-layout-1);
-			}
-
-			uui-input:not(:focus) {
-				border: 1px solid transparent;
-			}
-		`,
-	];
+	static styles = [UmbTextStyles, css``];
 }
 
 export default UmbWebhookWorkspaceEditorElement;
