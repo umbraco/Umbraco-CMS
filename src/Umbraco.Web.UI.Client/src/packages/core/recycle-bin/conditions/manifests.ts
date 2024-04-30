3a25491234565ae985bef01e6c9bd61ca2f83e19
Mads Rasmussen <madsr@hey.com>
2024-04-30 13:25:56 +0200

    register conditions

--- src/Umbraco.Web.UI.Client/src/packages/core/recycle-bin/conditions/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/recycle-bin/conditions/manifests.ts
@@ -1,3 +1,4 @@
+import { manifest as isTrashedManifest } from './is-trashed/is-trashed.condition.manifest.js';
 import { manifest as isNotTrashedManifest } from './is-not-trashed/is-not-trashed.condition.manifest.js';
 
-export const manifests = [isNotTrashedManifest];
+export const manifests = [isTrashedManifest, isNotTrashedManifest];
