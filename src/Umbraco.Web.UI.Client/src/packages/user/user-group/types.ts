ee3ae225518f2e91e7ec8ed15216a912000704b4
Mads Rasmussen <madsr@hey.com>
2024-05-01 13:37:26 +0200

    add alias to user group detail model

--- src/Umbraco.Web.UI.Client/src/packages/user/user-group/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user-group/types.ts
@@ -1,19 +1,20 @@
 import type { UmbUserGroupEntityType } from './entity.js';
 
 export interface UmbUserGroupDetailModel {
-	unique: string;
-	entityType: UmbUserGroupEntityType;
-	isSystemGroup: boolean;
-	name: string;
-	icon: string | null;
-	sections: Array<string>;
-	languages: Array<string>;
-	hasAccessToAllLanguages: boolean;
-	documentStartNode: { unique: string } | null;
+	alias: string;
 	documentRootAccess: boolean;
-	mediaStartNode: { unique: string } | null;
-	mediaRootAccess: boolean;
+	documentStartNode: { unique: string } | null;
+	entityType: UmbUserGroupEntityType;
 	fallbackPermissions: Array<string>;
+	hasAccessToAllLanguages: boolean;
+	icon: string | null;
+	isSystemGroup: boolean;
+	languages: Array<string>;
+	mediaRootAccess: boolean;
+	mediaStartNode: { unique: string } | null;
+	name: string;
 	// TODO: add type
 	permissions: Array<any>;
+	sections: Array<string>;
+	unique: string;
 }
