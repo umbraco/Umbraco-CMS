361ea1ad8ae5d56e9f12416bbdd50b4cdc76e66d
Niels Lyngsø <niels.lyngso@gmail.com>
2024-05-02 12:46:58 +0200

    change token to fit iwth entity context context alias

--- src/Umbraco.Web.UI.Client/src/packages/core/recycle-bin/contexts/is-trashed/entity-is-trashed.context-token.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/recycle-bin/contexts/is-trashed/entity-is-trashed.context-token.ts
@@ -2,5 +2,6 @@ import type { UmbEntityIsTrashedContext } from './entity-is-trashed.context.js';
 import { UmbContextToken } from '@umbraco-cms/backoffice/context-api';
 
 export const UMB_ENTITY_IS_TRASHED_CONTEXT = new UmbContextToken<UmbEntityIsTrashedContext>(
-	'UmbEntityIsTrashedContext',
+	'UmbEntityContext',
+	'isTrashed',
 );
