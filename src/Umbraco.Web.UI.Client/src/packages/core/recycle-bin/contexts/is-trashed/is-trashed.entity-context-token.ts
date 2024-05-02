38812980edc832cf90c4df1412df2a9977d65555
Niels Lyngsø <niels.lyngso@gmail.com>
2024-05-02 15:16:59 +0200

    revert token

--- src/Umbraco.Web.UI.Client/src/packages/core/recycle-bin/contexts/is-trashed/is-trashed.entity-context-token.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/recycle-bin/contexts/is-trashed/is-trashed.entity-context-token.ts
@@ -2,6 +2,5 @@ import type { UmbIsTrashedEntityContext } from './is-trashed.entity-context.js';
 import { UmbContextToken } from '@umbraco-cms/backoffice/context-api';
 
 export const UMB_IS_TRASHED_ENTITY_CONTEXT = new UmbContextToken<UmbIsTrashedEntityContext>(
-	'UmbEntityContext',
-	'isTrashed',
+	'UmbEntityIsTrashedContext',
 );
