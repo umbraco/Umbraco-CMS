1382e40180e86bf7ab24d0d888492de494cbbe62
Niels Lyngsø <niels.lyngso@gmail.com>
2024-05-03 22:03:08 +0200

    enforce existence

--- src/Umbraco.Web.UI.Client/src/packages/core/extension-registry/conditions/section-alias.condition.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/extension-registry/conditions/section-alias.condition.ts
@@ -25,7 +25,7 @@ export class UmbSectionAliasCondition
 		if (permissionCheck !== undefined) {
 			this.consumeContext(UMB_SECTION_CONTEXT, (context) => {
 				this.observe(context.alias, (sectionAlias) => {
-					this.permitted = sectionAlias ? permissionCheck(sectionAlias) : false;
+					this.permitted = sectionAlias ? permissionCheck!(sectionAlias) : false;
 				});
 			});
 		}
