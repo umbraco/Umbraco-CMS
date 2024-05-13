87c73479000453a8ea9368f7403576f81d85d962
Bjarke Berg <mail@bergmania.dk>
2024-05-13 08:28:42 +0200

    V14: Reintroduce umbraco api controller as obsolete (#16263)
    * Revert "v14: Remove mentions of UmbracoApiController (#15863)"
    
    This reverts commit 30e2dea57a1034d79ee418377c2f5b5a86746d66.
    
    * Obsolete UmbracoApiController
    
    * Added a few more obsoletion messages
    
    * Removed some of the reintroduced stuff again
    
    * Add obsoletion to FrontEndRoutes controller
    
    ---------
    
    Co-authored-by: kjac <kja@umbraco.dk>

--- src/Umbraco.Web.Common/Extensions/LinkGeneratorExtensions.cs
+++ src/Umbraco.Web.Common/Extensions/LinkGeneratorExtensions.cs
@@ -12,6 +12,36 @@ namespace Umbraco.Extensions;
 
 public static class LinkGeneratorExtensions
 {
+    /// <summary>
+    ///     Return the Url for a Web Api service
+    /// </summary>
+    /// <typeparam name="T">The <see cref="UmbracoApiControllerBase" /></typeparam>
+    [Obsolete("This will be removed in Umbraco 15.")]
+    public static string? GetUmbracoApiService<T>(this LinkGenerator linkGenerator, string actionName, object? id = null)
+        where T : UmbracoApiControllerBase => linkGenerator.GetUmbracoControllerUrl(
+        actionName,
+        typeof(T),
+        new Dictionary<string, object?> { ["id"] = id });
+
+    [Obsolete("This will be removed in Umbraco 15.")]
+    public static string? GetUmbracoApiService<T>(this LinkGenerator linkGenerator, string actionName, IDictionary<string, object?>? values)
+        where T : UmbracoApiControllerBase => linkGenerator.GetUmbracoControllerUrl(actionName, typeof(T), values);
+
+    [Obsolete("This will be removed in Umbraco 15.")]
+    public static string? GetUmbracoApiServiceBaseUrl<T>(
+        this LinkGenerator linkGenerator,
+        Expression<Func<T, object?>> methodSelector)
+        where T : UmbracoApiControllerBase
+    {
+        MethodInfo? method = ExpressionHelper.GetMethodInfo(methodSelector);
+        if (method == null)
+        {
+            throw new MissingMethodException("Could not find the method " + methodSelector + " on type " + typeof(T) +
+                                             " or the result ");
+        }
+
+        return linkGenerator.GetUmbracoApiService<T>(method.Name)?.TrimEnd(method.Name);
+    }
 
     /// <summary>
     ///     Return the Url for an Umbraco controller
@@ -100,5 +130,27 @@ public static class LinkGeneratorExtensions
         }
 
         return linkGenerator.GetUmbracoControllerUrl(actionName, ControllerExtensions.GetControllerName(controllerType), area, values);
+    }
+
+    [Obsolete("This will be removed in Umbraco 15.")]
+    public static string? GetUmbracoApiService<T>(
+        this LinkGenerator linkGenerator,
+        Expression<Func<T, object>> methodSelector)
+        where T : UmbracoApiController
+    {
+        MethodInfo? method = ExpressionHelper.GetMethodInfo(methodSelector);
+        IDictionary<string, object?>? methodParams = ExpressionHelper.GetMethodParams(methodSelector);
+        if (method == null)
+        {
+            throw new MissingMethodException(
+                $"Could not find the method {methodSelector} on type {typeof(T)} or the result ");
+        }
+
+        if (methodParams?.Any() == false)
+        {
+            return linkGenerator.GetUmbracoApiService<T>(method.Name);
+        }
+
+        return linkGenerator.GetUmbracoApiService<T>(method.Name, methodParams);
     }
 }
