c9007c0f7d5b4bcff02efcd126b0edc2423a1ed1
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2024-05-13 14:03:19 +0200

    Add image resizing endpoint

---
+++ src/Umbraco.Cms.Api.Management/Factories/IThumbnailUrlFactory.cs
@@ -0,0 +1,9 @@
+﻿using Umbraco.Cms.Api.Management.ViewModels.Media;
+using Umbraco.Cms.Core.Models;
+
+namespace Umbraco.Cms.Api.Management.Factories;
+
+public interface IThumbnailUrlFactory
+{
+    IEnumerable<MediaUrlInfoResponseModel> CreateUrlSets(IEnumerable<IMedia> mediaItems, int height, int width, ImageCropMode? mode);
+}
