1e95280772848e75d5a96e466159675d37c7e061
Mads Rasmussen <madsr@hey.com>
2024-05-15 14:52:37 +0200

    Update current-user-theme-user-profile-app.element.ts

--- src/Umbraco.Web.UI.Client/src/packages/user/current-user/theme/current-user-theme-user-profile-app.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/current-user/theme/current-user-theme-user-profile-app.element.ts
@@ -48,11 +48,7 @@ export class UmbCurrentUserThemeUserProfileAppElement extends UmbLitElement {
 	render() {
 		if (!this._themes.length) return nothing;
 		return html`
-			<uui-box>
-				<div slot="header">
-					<b>Select Theme</b>
-					<p>Experimental. Only light theme is fully supported</p>
-				</div>
+			<uui-box headline="Theme">
 				<uui-select label="Select theme" .options=${this._themes} @change=${this.#onThemeChange}></uui-select>
 			</uui-box>
 		`;
