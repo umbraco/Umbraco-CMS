bd17020e1a15eef6cf1f64f957b86077456d6884
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-05-24 11:35:51 +0200

    use the `hasOwnOpener` function to test if the request came from within the backoffice, which lets us know that we should stay on the page

--- src/Umbraco.Web.UI.Client/src/apps/app/app.element.ts
+++ src/Umbraco.Web.UI.Client/src/apps/app/app.element.ts
@@ -18,7 +18,7 @@ import {
 	umbExtensionsRegistry,
 } from '@umbraco-cms/backoffice/extension-registry';
 import { filter, first, firstValueFrom } from '@umbraco-cms/backoffice/external/rxjs';
-import { retrieveStoredPath } from '@umbraco-cms/backoffice/utils';
+import { hasOwnOpener, retrieveStoredPath } from '@umbraco-cms/backoffice/utils';
 
 @customElement('umb-app')
 export class UmbAppElement extends UmbLitElement {
@@ -77,12 +77,7 @@ export class UmbAppElement extends UmbLitElement {
 				// The authorization request will be completed in the active window (main or popup) and the authorization signal will be sent.
 				// If we are in a popup window, the storage event in UmbAuthContext will catch the signal and close the window.
 				// If we are in the main window, the signal will be caught right here and the user will be redirected to the root.
-				if (
-					window.opener &&
-					window.opener !== window &&
-					window.opener.location.origin === window.location.origin &&
-					window.opener.location.pathname.startsWith(this.backofficePath)
-				) {
+				if (hasOwnOpener(this.backofficePath)) {
 					(component as UmbAppErrorElement).errorMessage = hasCode
 						? this.localize.term('errors_externalLoginSuccess')
 						: this.localize.term('errors_externalLoginFailed');
