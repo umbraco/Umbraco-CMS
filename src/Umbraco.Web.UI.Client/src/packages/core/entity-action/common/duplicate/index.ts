7eca570aa1ab07fa1b87f1ba19f7267bd5ff21d4
Mads Rasmussen <madsr@hey.com>
2024-05-24 10:50:09 +0200

    move "duplicate to"-entity-action to tree module

--- src/Umbraco.Web.UI.Client/src/packages/core/entity-action/common/duplicate/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/entity-action/common/duplicate/index.ts
@@ -1,5 +1,2 @@
 export { UmbDuplicateEntityAction } from './duplicate/index.js';
-export { UmbDuplicateToEntityAction } from './duplicate-to/index.js';
-
 export * from './duplicate/types.js';
-export * from './duplicate-to/types.js';
--- src/Umbraco.Web.UI.Client/src/packages/core/tree/entity-actions/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/tree/entity-actions/manifests.ts
@@ -1,5 +1,11 @@
-import { manifests as reloadTreeItemChildrenManifests } from './reload-tree-item-children/manifests.js';
+import { manifests as duplicateToManifests } from './duplicate-to/manifests.js';
 import { manifests as moveManifests } from './move/manifests.js';
+import { manifests as reloadTreeItemChildrenManifests } from './reload-tree-item-children/manifests.js';
 import { manifests as sortChildrenOfManifests } from './sort-children-of/manifests.js';
 
-export const manifests = [...reloadTreeItemChildrenManifests, ...moveManifests, ...sortChildrenOfManifests];
+export const manifests = [
+	...duplicateToManifests,
+	...moveManifests,
+	...reloadTreeItemChildrenManifests,
+	...sortChildrenOfManifests,
+];
