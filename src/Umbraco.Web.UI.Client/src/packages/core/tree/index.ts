e622362df96e8dcc5228104e305bbb55e68b2a9e
Mads Rasmussen <madsr@hey.com>
2024-05-24 08:10:07 +0200

    export from tree module

--- src/Umbraco.Web.UI.Client/src/packages/core/tree/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/tree/index.ts
@@ -9,6 +9,7 @@ export {
 	UmbReloadTreeItemChildrenEntityAction,
 	UmbRequestReloadTreeItemChildrenEvent,
 } from './entity-actions/reload-tree-item-children/index.js';
+export type { UmbMoveDataSource, UmbMoveRepository, UmbMoveToRequestArgs } from './entity-actions/move/index.js';
 
 export type { UmbTreePickerModalData, UmbTreePickerModalValue } from './tree-picker/index.js';
 export { UMB_TREE_PICKER_MODAL, UMB_TREE_PICKER_MODAL_ALIAS } from './tree-picker/index.js';
