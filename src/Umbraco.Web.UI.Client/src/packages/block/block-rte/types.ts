1beaf7139b59ea04ef76fa7849a90a55e839ac6a
Niels Lyngsø <niels.lyngso@gmail.com>
2024-05-26 09:50:52 +0200

    exports

---
+++ src/Umbraco.Web.UI.Client/src/packages/block/block-rte/types.ts
@@ -0,0 +1,5 @@
+import type { UmbBlockTypeBaseModel } from '../block-type/index.js';
+
+export const UMB_BLOCK_RTE_TYPE = 'block-rte-type';
+
+export interface UmbBlockRteTypeModel extends UmbBlockTypeBaseModel {}
