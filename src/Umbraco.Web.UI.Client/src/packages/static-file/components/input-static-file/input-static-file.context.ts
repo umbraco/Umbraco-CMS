3bf964c8071b6c3fb71ae9a249d3b051dee2cbd7
leekelleher <leekelleher@gmail.com>
2024-05-27 14:21:34 +0100

    Corrected type imports

--- src/Umbraco.Web.UI.Client/src/packages/static-file/components/input-static-file/input-static-file.context.ts
+++ src/Umbraco.Web.UI.Client/src/packages/static-file/components/input-static-file/input-static-file.context.ts
@@ -1,10 +1,7 @@
 import { UMB_STATIC_FILE_ITEM_REPOSITORY_ALIAS } from '../../repository/item/manifests.js';
-import {
-	UMB_STATIC_FILE_PICKER_MODAL,
-	type UmbStaticFilePickerModalData,
-	type UmbStaticFilePickerModalValue,
-} from '../../modals/index.js';
+import { UMB_STATIC_FILE_PICKER_MODAL } from '../../modals/index.js';
 import type { UmbStaticFileItemModel } from '../../repository/item/types.js';
+import type { UmbStaticFilePickerModalData, UmbStaticFilePickerModalValue } from '../../modals/index.js';
 import { UmbPickerInputContext } from '@umbraco-cms/backoffice/picker-input';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 
