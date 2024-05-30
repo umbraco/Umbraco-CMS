63afd60b44d5006088b7afe31deea33e8e29f7b5
Mads Rasmussen <madsr@hey.com>
2024-05-30 14:57:42 +0200

    only render folders for move document-type

--- src/Umbraco.Web.UI.Client/src/packages/documents/document-types/entity-actions/move-to/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/document-types/entity-actions/move-to/manifests.ts
@@ -15,6 +15,7 @@ const entityActions: Array<ManifestTypes> = [
 			treeRepositoryAlias: UMB_DOCUMENT_TYPE_TREE_REPOSITORY_ALIAS,
 			moveRepositoryAlias: UMB_MOVE_DOCUMENT_TYPE_REPOSITORY_ALIAS,
 			treeAlias: UMB_DOCUMENT_TYPE_TREE_ALIAS,
+			foldersOnly: true,
 		},
 	},
 ];
