8dc991069fe9e676937623f17de8bb87d66efe85
Nhu Dinh <hnd@umbraco.dk>
2024-06-03 08:20:17 +0200

    Updated tests for Document Blueprints

--- tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Settings/DocumentBlueprint/DocumentBlueprints.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Settings/DocumentBlueprint/DocumentBlueprints.spec.ts
@@ -1,4 +1,4 @@
-﻿import {test} from '@umbraco/playwright-testhelpers';
+﻿import {ConstantHelper, test} from '@umbraco/playwright-testhelpers';
 import {expect} from "@playwright/test";
 
 test.describe('Document Blueprints tests', () => {
@@ -11,7 +11,6 @@ test.describe('Document Blueprints tests', () => {
     await umbracoApi.documentType.ensureNameNotExists(documentTypeName);
     documentTypeId = await umbracoApi.documentType.createDefaultDocumentType(documentTypeName);
     await umbracoUi.goToBackOffice();
-    await umbracoUi.documentBlueprints.goToSettingsTreeItem('Document Blueprints');
   });
 
   test.afterEach(async ({umbracoApi}) => {
@@ -20,8 +19,10 @@ test.describe('Document Blueprints tests', () => {
   });
 
   test('can create a document blueprint from the Settings menu @smoke', async ({umbracoApi, umbracoUi}) => {
-    // Act
+    // Arrange
     await umbracoUi.documentBlueprints.goToSettingsTreeItem('Document Blueprints');
+
+    // Act
     await umbracoUi.documentBlueprints.clickActionsMenuAtRoot();
     await umbracoUi.documentBlueprints.clickCreateDocumentBlueprintButton();
     await umbracoUi.documentBlueprints.clickTextButtonWithName(documentTypeName);
@@ -40,9 +41,9 @@ test.describe('Document Blueprints tests', () => {
     await umbracoApi.documentBlueprints.ensureNameNotExists(wrongDocumentBlueprintName);
     await umbracoApi.documentBlueprints.createDefaultDocumentBlueprints(wrongDocumentBlueprintName, documentTypeId);
     expect(await umbracoApi.documentBlueprints.doesNameExist(wrongDocumentBlueprintName)).toBeTruthy();
+    await umbracoUi.documentBlueprints.goToSettingsTreeItem('Document Blueprints');
 
     // Act
-    await umbracoUi.documentBlueprints.goToSettingsTreeItem('Document Blueprints');
     await umbracoUi.documentBlueprints.goToDocumentBlueprints(wrongDocumentBlueprintName);
     await umbracoUi.documentBlueprints.enterDocumentBlueprintName(documentBlueprintName);
     await umbracoUi.documentBlueprints.clickSaveButton();
@@ -50,20 +51,21 @@ test.describe('Document Blueprints tests', () => {
     // Assert
     expect(await umbracoApi.documentBlueprints.doesNameExist(documentBlueprintName)).toBeTruthy();
     expect(await umbracoApi.documentBlueprints.doesNameExist(wrongDocumentBlueprintName)).toBeFalsy();
-    await umbracoUi.documentBlueprints.isDocumentBlueprintRootTreeItemVisible(documentBlueprintName, true);
-    await umbracoUi.documentBlueprints.isDocumentBlueprintRootTreeItemVisible(wrongDocumentBlueprintName, false);
+    await umbracoUi.documentBlueprints.isDocumentBlueprintRootTreeItemVisible(documentBlueprintName, true, false);
+    await umbracoUi.documentBlueprints.isDocumentBlueprintRootTreeItemVisible(wrongDocumentBlueprintName, false, false);
   });
 
   test('can delete a document blueprint @smoke', async ({umbracoApi, umbracoUi}) => {
     // Arrange
     await umbracoApi.documentBlueprints.createDefaultDocumentBlueprints(documentBlueprintName, documentTypeId);
     expect(await umbracoApi.documentBlueprints.doesNameExist(documentBlueprintName)).toBeTruthy();
+    await umbracoUi.documentBlueprints.goToSettingsTreeItem('Document Blueprints');
 
     // Act
-    await umbracoUi.documentBlueprints.goToSettingsTreeItem('Document Blueprints');
-    await umbracoUi.documentBlueprints.clickRootFolderCaretButton();
+    await umbracoUi.documentBlueprints.reloadDocumentBlueprintsTree();
     await umbracoUi.documentBlueprints.clickActionsMenuForDocumentBlueprints(documentBlueprintName);
-    await umbracoUi.documentBlueprints.clickDeleteAndConfirmButton();
+    await umbracoUi.documentBlueprints.clickDeleteMenuButton();
+    await umbracoUi.documentBlueprints.clickConfirmToDeleteButton();
 
     // Assert
     await umbracoUi.documentBlueprints.isSuccessNotificationVisible();
@@ -71,21 +73,23 @@ test.describe('Document Blueprints tests', () => {
     await umbracoUi.documentBlueprints.isDocumentBlueprintRootTreeItemVisible(documentBlueprintName, false);
   });
 
-  // TODO: update this test after merging Content branch
-  test.skip('can create a document blueprint from the Content menu @smoke', async ({umbracoApi, umbracoUi}) => {
+  test('can create a document blueprint from the Content menu @smoke', async ({umbracoApi, umbracoUi}) => {
     // Arrange
+    const documentTypeName = 'TestDocumentTypeForContent';
+    const documentTypeId = await umbracoApi.documentType.createDefaultDocumentTypeWithAllowAsRoot(documentTypeName);
+    await umbracoApi.document.createDefaultDocument(documentBlueprintName, documentTypeId);
+    await umbracoUi.goToBackOffice();
+    await umbracoUi.content.goToSection(ConstantHelper.sections.content);
 
     // Act
-    await umbracoUi.documentBlueprints.goToSettingsTreeItem('Document Blueprints');
-    await umbracoUi.documentBlueprints.clickActionsMenuAtRoot();
-    await umbracoUi.documentBlueprints.clickCreateDocumentBlueprintButton();
-    await umbracoUi.documentBlueprints.clickTextButtonWithName(documentTypeName);
-    await umbracoUi.documentBlueprints.enterDocumentBlueprintName(documentBlueprintName);
-    await umbracoUi.documentBlueprints.clickSaveButton();
+    await umbracoUi.content.clickActionsMenuForContent(documentBlueprintName);
+    await umbracoUi.content.clickCreateDocumentBlueprintButton();
+    await umbracoUi.content.clickSaveModalButton();
 
     // Assert
-    await umbracoUi.documentBlueprints.isSuccessNotificationVisible();
+    await umbracoUi.content.isSuccessNotificationVisible();
     expect(await umbracoApi.documentBlueprints.doesNameExist(documentBlueprintName)).toBeTruthy();
+    await umbracoUi.documentBlueprints.goToSettingsTreeItem('Document Blueprints');
     await umbracoUi.documentBlueprints.isDocumentBlueprintRootTreeItemVisible(documentBlueprintName, true);
   });
 });
