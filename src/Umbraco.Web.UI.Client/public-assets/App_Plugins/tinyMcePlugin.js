a029f2bd2ea8e3487705b953cc86684918586509
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-06-03 14:13:00 +0200

    chore: mock data

--- src/Umbraco.Web.UI.Client/public-assets/App_Plugins/tinyMcePlugin.js
+++ src/Umbraco.Web.UI.Client/public-assets/App_Plugins/tinyMcePlugin.js
@@ -2,7 +2,7 @@ export default class UmbTinyMceMockPlugin {
 	/**
 	 * @param {TinyMcePluginArguments} args
 	 */
-	constructor(host, args) {
+	constructor(args) {
 		// Add your plugin code here
 		console.log('editor initialized', args)
 	}
