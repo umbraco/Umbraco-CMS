69521d73e2743f762d57a827659c2136bdcc0e97
Mads Rasmussen <madsr@hey.com>
2024-06-03 11:57:07 +0200

    add package json + vite config

---
+++ src/Umbraco.Web.UI.Client/src/packages/dictionary/vite.config.ts
@@ -0,0 +1,21 @@
+import { defineConfig } from 'vite';
+import { rmSync } from 'fs';
+
+const dist = '../../../dist-cms/packages/dictionary';
+
+// delete the unbundled dist folder
+rmSync(dist, { recursive: true, force: true });
+
+export default defineConfig({
+	build: {
+		lib: {
+			entry: ['index.ts', 'manifests.ts', 'umbraco-package.ts'],
+			formats: ['es'],
+		},
+		outDir: dist,
+		//sourcemap: true,
+		rollupOptions: {
+			external: [/^@umbraco/],
+		},
+	},
+});
