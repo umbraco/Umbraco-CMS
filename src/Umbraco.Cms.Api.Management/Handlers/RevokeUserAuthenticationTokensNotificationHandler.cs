fade190e138afaaccec36f05ba742f1e1be4734f
kjac <kja@umbraco.dk>
2024-06-04 11:41:10 +0200

    Do not rely on claims to figure out user access + prepare for claims removal

--- src/Umbraco.Cms.Api.Management/Handlers/RevokeUserAuthenticationTokensNotificationHandler.cs
+++ src/Umbraco.Cms.Api.Management/Handlers/RevokeUserAuthenticationTokensNotificationHandler.cs
@@ -3,8 +3,10 @@ using System.Globalization;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using OpenIddict.Abstractions;
+using Umbraco.Cms.Core.Cache;
 using Umbraco.Cms.Core.Configuration.Models;
 using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Models.Membership;
 using Umbraco.Cms.Core.Notifications;
 using Umbraco.Cms.Core.Services;
@@ -13,18 +15,15 @@ using Umbraco.Extensions;
 namespace Umbraco.Cms.Api.Management.Handlers;
 
 internal sealed class RevokeUserAuthenticationTokensNotificationHandler :
-        INotificationAsyncHandler<UserSavingNotification>,
         INotificationAsyncHandler<UserSavedNotification>,
         INotificationAsyncHandler<UserDeletedNotification>,
         INotificationAsyncHandler<UserGroupDeletingNotification>,
-        INotificationAsyncHandler<UserGroupDeletedNotification>,
         INotificationAsyncHandler<UserLoginSuccessNotification>
 {
-    private const string NotificationStateKey = "Umbraco.Cms.Api.Management.Handlers.RevokeUserAuthenticationTokensNotificationHandler";
-
     private readonly IUserService _userService;
     private readonly IUserGroupService _userGroupService;
     private readonly IOpenIddictTokenManager _tokenManager;
+    private readonly AppCaches _appCaches;
     private readonly ILogger<RevokeUserAuthenticationTokensNotificationHandler> _logger;
     private readonly SecuritySettings _securitySettings;
 
@@ -32,60 +31,27 @@ internal sealed class RevokeUserAuthenticationTokensNotificationHandler :
         IUserService userService,
         IUserGroupService userGroupService,
         IOpenIddictTokenManager tokenManager,
+        AppCaches appCaches,
         ILogger<RevokeUserAuthenticationTokensNotificationHandler> logger,
         IOptions<SecuritySettings> securitySettingsOptions)
     {
         _userService = userService;
         _userGroupService = userGroupService;
         _tokenManager = tokenManager;
+        _appCaches = appCaches;
         _logger = logger;
         _securitySettings = securitySettingsOptions.Value;
-    }
-
-    // We need to know the pre-saving state of the saved users in order to compare if their access has changed
-    public async Task HandleAsync(UserSavingNotification notification, CancellationToken cancellationToken)
-    {
-        try
-        {
-            var usersAccess = new Dictionary<Guid, UserStartNodesAndGroupAccess>();
-            foreach (IUser user in notification.SavedEntities)
-            {
-                UserStartNodesAndGroupAccess? priorUserAccess = await GetRelevantUserAccessDataByUserKeyAsync(user.Key);
-                if (priorUserAccess == null)
-                {
-                    continue;
-                }
-
-                usersAccess.Add(user.Key, priorUserAccess);
-            }
-
-            notification.State[NotificationStateKey] = usersAccess;
-        }
-        catch (DbException e)
-        {
-            _logger.LogWarning(e, "This is expected when we upgrade from < Umbraco 14. Otherwise it should not happen");
-        }
     }
 
     public async Task HandleAsync(UserSavedNotification notification, CancellationToken cancellationToken)
     {
         try
         {
-            Dictionary<Guid, UserStartNodesAndGroupAccess>? preSavingUsersState = null;
-
-            if (notification.State.TryGetValue(NotificationStateKey, out var value))
-            {
-                preSavingUsersState = value as Dictionary<Guid, UserStartNodesAndGroupAccess>;
-            }
-
-            // If we have a new user, there is no token
-            if (preSavingUsersState is null || preSavingUsersState.Count == 0)
-            {
-                return;
-            }
-
             foreach (IUser user in notification.SavedEntities)
             {
+                // Flush the start node caches when editing a user
+                user.FlushStartNodeCaches(_appCaches);
+
                 if (user.IsSuper())
                 {
                     continue;
@@ -95,23 +61,6 @@ internal sealed class RevokeUserAuthenticationTokensNotificationHandler :
                 if (user.IsLockedOut || user.IsApproved is false)
                 {
                     await RevokeTokensAsync(user);
-                    continue;
-                }
-
-                // Don't revoke admin tokens to prevent log out when accidental changes
-                if (user.IsAdmin())
-                {
-                    continue;
-                }
-
-                // Check if the user access has changed - we also need to revoke all tokens in this case
-                if (preSavingUsersState.TryGetValue(user.Key, out UserStartNodesAndGroupAccess? preSavingState))
-                {
-                    UserStartNodesAndGroupAccess postSavingState = MapToUserStartNodesAndGroupAccess(user);
-                    if (preSavingState.CompareAccess(postSavingState) == false)
-                    {
-                        await RevokeTokensAsync(user);
-                    }
                 }
             }
         }
@@ -131,46 +80,21 @@ internal sealed class RevokeUserAuthenticationTokensNotificationHandler :
         }
     }
 
-    // We need to know the pre-deleting state of the users part of the deleted group to revoke their tokens
     public async Task HandleAsync(UserGroupDeletingNotification notification, CancellationToken cancellationToken)
     {
-        var usersInGroups = new Dictionary<Guid, IEnumerable<IUser>>();
         foreach (IUserGroup userGroup in notification.DeletedEntities)
         {
-            var users = await GetUsersByGroupKeyAsync(userGroup.Key);
-            if (users == null)
+            IEnumerable<IUser>? users = await GetUsersByGroupKeyAsync(userGroup.Key);
+            if (users is null)
             {
                 continue;
             }
 
-            usersInGroups.Add(userGroup.Key, users);
-        }
-
-        notification.State[NotificationStateKey] = usersInGroups;
-    }
-
-    public async Task HandleAsync(UserGroupDeletedNotification notification, CancellationToken cancellationToken)
-    {
-        Dictionary<Guid, IEnumerable<IUser>>? preDeletingUsersInGroups = null;
-
-        if (notification.State.TryGetValue(NotificationStateKey, out var value))
-        {
-            preDeletingUsersInGroups = value as Dictionary<Guid, IEnumerable<IUser>>;
-        }
-
-        if (preDeletingUsersInGroups is null)
-        {
-            return;
-        }
-
-        // since the user group was deleted, we can only use the information we collected before the deletion
-        // this means that we will not be able to detect users in any groups that were eventually deleted (due to implementor/3th party supplier interference)
-        // that were not in the initial to be deleted list
-        foreach (IUser user in preDeletingUsersInGroups
-                     .Where(group => notification.DeletedEntities.Any(entity => group.Key == entity.Key))
-                     .SelectMany(group => group.Value))
-        {
-            await RevokeTokensAsync(user);
+            // Flush the start node caches for all affected users when editing a group
+            foreach (IUser user in users)
+            {
+                user.FlushStartNodeCaches(_appCaches);
+            }
         }
     }
 
@@ -189,19 +113,6 @@ internal sealed class RevokeUserAuthenticationTokensNotificationHandler :
             await RevokeTokensAsync(user);
         }
     }
-
-    // Get data about the user before saving
-    private async Task<UserStartNodesAndGroupAccess?> GetRelevantUserAccessDataByUserKeyAsync(Guid userKey)
-    {
-        IUser? user = await _userService.GetAsync(userKey);
-
-        return user is null
-            ? null
-            : MapToUserStartNodesAndGroupAccess(user);
-    }
-
-    private UserStartNodesAndGroupAccess MapToUserStartNodesAndGroupAccess(IUser user)
-        => new(user.Groups.Select(g => g.Key), user.StartContentIds, user.StartMediaIds);
 
     // Get data about the users part of a group before deleting it
     private async Task<IEnumerable<IUser>?> GetUsersByGroupKeyAsync(Guid userGroupKey)
@@ -235,36 +146,5 @@ internal sealed class RevokeUserAuthenticationTokensNotificationHandler :
         }
 
         return null;
-    }
-
-    private class UserStartNodesAndGroupAccess
-    {
-        public IEnumerable<Guid> GroupKeys { get; }
-
-        public int[]? StartContentIds { get; }
-
-        public int[]? StartMediaIds { get; }
-
-        public UserStartNodesAndGroupAccess(IEnumerable<Guid> groupKeys, int[]? startContentIds, int[]? startMediaIds)
-        {
-            GroupKeys = groupKeys;
-            StartContentIds = startContentIds;
-            StartMediaIds = startMediaIds;
-        }
-
-        public bool CompareAccess(UserStartNodesAndGroupAccess other)
-        {
-            var areContentStartNodesEqual = (StartContentIds == null && other.StartContentIds == null) ||
-                                            (StartContentIds != null && other.StartContentIds != null &&
-                                            StartContentIds.SequenceEqual(other.StartContentIds));
-
-            var areMediaStartNodesEqual = (StartMediaIds == null && other.StartMediaIds == null) ||
-                                          (StartMediaIds != null && other.StartMediaIds != null &&
-                                          StartMediaIds.SequenceEqual(other.StartMediaIds));
-
-            return areContentStartNodesEqual &&
-                   areMediaStartNodesEqual &&
-                   GroupKeys.SequenceEqual(other.GroupKeys);
-        }
     }
 }
