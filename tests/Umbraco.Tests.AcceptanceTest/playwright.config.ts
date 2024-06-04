775f5b52e0c86e77fedac04ef1058347814014a3
Andreas Zerbst <andr317c@live.dk>
2024-06-04 13:24:43 +0200

    Decreased retry count

--- tests/Umbraco.Tests.AcceptanceTest/playwright.config.ts
+++ tests/Umbraco.Tests.AcceptanceTest/playwright.config.ts
@@ -21,7 +21,7 @@ const config: PlaywrightTestConfig = {
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
   /* Retry on CI only */
-  retries: process.env.CI ? 5 : 2,
+  retries: process.env.CI ? 2 : 1,
   // We don't want to run parallel, as tests might differ in state
   workers:  1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
