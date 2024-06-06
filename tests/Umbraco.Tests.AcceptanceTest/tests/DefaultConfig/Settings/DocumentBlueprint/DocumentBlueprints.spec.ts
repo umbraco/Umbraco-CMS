c4bbea7d461abaf1820204fe8ac28d6d3e9facd9
Nhu Dinh <hnd@umbraco.dk>
2024-06-06 10:12:50 +0200

    Fix arrange steps

--- tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Settings/DocumentBlueprint/DocumentBlueprints.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Settings/DocumentBlueprint/DocumentBlueprints.spec.ts
@@ -1,7 +1,8 @@
 ﻿import {ConstantHelper, test} from '@umbraco/playwright-testhelpers';
 import {expect} from "@playwright/test";
 
-test.describe('Document Blueprints tests', () => {
+// TODO: Remove test.describe before merging
+test.describe('Document Blueprints tests', {tag: '@smoke'}, () => {
   const documentBlueprintName = 'TestDocumentBlueprints';
   const documentTypeName = 'DocumentTypeForBlueprint';
   let documentTypeId = '';
@@ -18,9 +19,9 @@ test.describe('Document Blueprints tests', () => {
     await umbracoApi.documentType.delete(documentTypeId);
   });
 
-  test('can create a document blueprint from the Settings menu @smoke', async ({umbracoApi, umbracoUi}) => {
+  test('can create a document blueprint from the settings menu', {tag: '@smoke'}, async ({umbracoApi, umbracoUi}) => {
     // Arrange
-    await umbracoUi.documentBlueprint.goToSettingsTreeItem('Document Blueprints');
+    await umbracoUi.documentBlueprint.goToSection(ConstantHelper.sections.settings);
 
     // Act
     await umbracoUi.documentBlueprint.clickActionsMenuAtRoot();
@@ -35,16 +36,16 @@ test.describe('Document Blueprints tests', () => {
     await umbracoUi.documentBlueprint.isDocumentBlueprintRootTreeItemVisible(documentBlueprintName, true);
   });
 
-  test('can rename a document blueprint name @smoke', async ({umbracoApi, umbracoUi}) => {
+  test('can rename a document blueprint name', async ({umbracoApi, umbracoUi}) => {
     // Arrange
     const wrongDocumentBlueprintName = 'Wrong Document Blueprint';
     await umbracoApi.documentBlueprint.ensureNameNotExists(wrongDocumentBlueprintName);
     await umbracoApi.documentBlueprint.createDefaultDocumentBlueprint(wrongDocumentBlueprintName, documentTypeId);
     expect(await umbracoApi.documentBlueprint.doesNameExist(wrongDocumentBlueprintName)).toBeTruthy();
-    await umbracoUi.documentBlueprint.goToSettingsTreeItem('Document Blueprints');
+    await umbracoUi.documentBlueprint.goToSection(ConstantHelper.sections.settings);
 
     // Act
-    await umbracoUi.documentBlueprint.goToDocumentBlueprints(wrongDocumentBlueprintName);
+    await umbracoUi.documentBlueprint.goToDocumentBlueprint(wrongDocumentBlueprintName);
     await umbracoUi.documentBlueprint.enterDocumentBlueprintName(documentBlueprintName);
     await umbracoUi.documentBlueprint.clickSaveButton();
 
@@ -55,11 +56,11 @@ test.describe('Document Blueprints tests', () => {
     await umbracoUi.documentBlueprint.isDocumentBlueprintRootTreeItemVisible(wrongDocumentBlueprintName, false, false);
   });
 
-  test('can delete a document blueprint @smoke', async ({umbracoApi, umbracoUi}) => {
+  test('can delete a document blueprint', async ({umbracoApi, umbracoUi}) => {
     // Arrange
     await umbracoApi.documentBlueprint.createDefaultDocumentBlueprint(documentBlueprintName, documentTypeId);
     expect(await umbracoApi.documentBlueprint.doesNameExist(documentBlueprintName)).toBeTruthy();
-    await umbracoUi.documentBlueprint.goToSettingsTreeItem('Document Blueprints');
+    await umbracoUi.documentBlueprint.goToSection(ConstantHelper.sections.settings);
 
     // Act
     await umbracoUi.documentBlueprint.reloadDocumentBlueprintsTree();
@@ -73,7 +74,7 @@ test.describe('Document Blueprints tests', () => {
     await umbracoUi.documentBlueprint.isDocumentBlueprintRootTreeItemVisible(documentBlueprintName, false, false);
   });
 
-  test('can create a document blueprint from the Content menu @smoke', async ({umbracoApi, umbracoUi}) => {
+  test('can create a document blueprint from the Content menu', async ({umbracoApi, umbracoUi}) => {
     // Arrange
     const documentTypeName = 'TestDocumentTypeForContent';
     const documentTypeId = await umbracoApi.documentType.createDefaultDocumentTypeWithAllowAsRoot(documentTypeName);
