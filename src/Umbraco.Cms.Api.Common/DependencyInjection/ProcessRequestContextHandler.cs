a64dbe12a110e3ea5196a584c8a0bd81af1731ce
Kenn Jacobsen <kja@umbraco.dk>
2024-06-10 13:31:51 +0200

    OpenIddict should only handle /umbraco/ requests (#16549)

---
+++ src/Umbraco.Cms.Api.Common/DependencyInjection/ProcessRequestContextHandler.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Http;
+using OpenIddict.Server;
+using OpenIddict.Validation;
+using Umbraco.Cms.Core;
+using Umbraco.Extensions;
+
+namespace Umbraco.Cms.Api.Common.DependencyInjection;
+
+public class ProcessRequestContextHandler
+    : IOpenIddictServerHandler<OpenIddictServerEvents.ProcessRequestContext>, IOpenIddictValidationHandler<OpenIddictValidationEvents.ProcessRequestContext>
+{
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly string _backOfficePathSegment;
+
+    public ProcessRequestContextHandler(IHttpContextAccessor httpContextAccessor)
+    {
+        _httpContextAccessor = httpContextAccessor;
+        _backOfficePathSegment = Constants.System.DefaultUmbracoPath.TrimStart(Constants.CharArrays.Tilde)
+            .EnsureStartsWith('/')
+            .EnsureEndsWith('/');
+    }
+
+    public ValueTask HandleAsync(OpenIddictServerEvents.ProcessRequestContext context)
+    {
+        if (SkipOpenIddictHandlingForRequest())
+        {
+            context.SkipRequest();
+        }
+
+        return ValueTask.CompletedTask;
+    }
+
+    public ValueTask HandleAsync(OpenIddictValidationEvents.ProcessRequestContext context)
+    {
+        if (SkipOpenIddictHandlingForRequest())
+        {
+            context.SkipRequest();
+        }
+
+        return ValueTask.CompletedTask;
+    }
+
+    private bool SkipOpenIddictHandlingForRequest()
+    {
+        var requestPath = _httpContextAccessor.HttpContext?.Request.Path.Value;
+        if (requestPath.IsNullOrWhiteSpace())
+        {
+            return false;
+        }
+
+        return requestPath.StartsWith(_backOfficePathSegment) is false;
+    }
+}
