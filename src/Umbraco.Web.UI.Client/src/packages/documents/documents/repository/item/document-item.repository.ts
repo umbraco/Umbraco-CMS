465c47058b12f59bf5d497962e068afb70e25590
Mads Rasmussen <madsr@hey.com>
2024-06-18 18:41:06 +0200

    split context token and implementation

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/repository/item/document-item.repository.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/repository/item/document-item.repository.ts
@@ -1,5 +1,5 @@
 import { UmbDocumentItemServerDataSource } from './document-item.server.data-source.js';
-import { UMB_DOCUMENT_ITEM_STORE_CONTEXT } from './document-item.store.js';
+import { UMB_DOCUMENT_ITEM_STORE_CONTEXT } from './document-item.store.context-token.js';
 import type { UmbDocumentItemModel } from './types.js';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import { UmbItemRepositoryBase } from '@umbraco-cms/backoffice/repository';
--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/repository/item/document-item.store.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/repository/item/document-item.store.ts
@@ -1,7 +1,7 @@
 import type { UmbDocumentDetailModel } from '../../types.js';
-import { UmbContextToken } from '@umbraco-cms/backoffice/context-api';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import { UmbItemStoreBase } from '@umbraco-cms/backoffice/store';
+import { UMB_DOCUMENT_ITEM_STORE_CONTEXT } from './document-item.store.context-token.js';
 
 /**
  * @export
@@ -22,5 +22,3 @@ export class UmbDocumentItemStore extends UmbItemStoreBase<UmbDocumentDetailMode
 }
 
 export { UmbDocumentItemStore as api };
-
-export const UMB_DOCUMENT_ITEM_STORE_CONTEXT = new UmbContextToken<UmbDocumentItemStore>('UmbDocumentItemStore');
