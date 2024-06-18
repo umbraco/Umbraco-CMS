33c3c591d18862dedff2c5cb8a7fe5f0caa084df
nikolajlauridsen <nikolajlauridsen@protonmail.ch>
2024-06-18 13:30:12 +0200

    Add delivery api toggle

--- src/Umbraco.Web.UI/Program.cs
+++ src/Umbraco.Web.UI/Program.cs
@@ -3,7 +3,9 @@ WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 builder.CreateUmbracoBuilder()
     .AddBackOffice()
     .AddWebsite()
+#if UseDeliveryApi
     .AddDeliveryApi()
+#endif
     .AddComposers()
     .Build();
 
