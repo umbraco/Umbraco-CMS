cf97dbcc1769d1dbaf77849f462b12e4054c8fa0
Mads Rasmussen <madsr@hey.com>
2024-06-19 12:56:21 +0200

    bundle individual modules

--- src/Umbraco.Web.UI.Client/src/packages/documents/vite.config.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/vite.config.ts
@@ -8,5 +8,14 @@ const dist = '../../../dist-cms/packages/documents';
 rmSync(dist, { recursive: true, force: true });
 
 export default defineConfig({
-	...getDefaultConfig({ dist, entry: ['umbraco-package.ts', 'manifests.ts'] }),
+	...getDefaultConfig({
+		dist,
+		entry: {
+			'document-blueprints/index': 'document-blueprints/index.ts',
+			'document-types/index': 'document-types/index.ts',
+			'documents/index': 'documents/index.ts',
+			manifests: 'manifests.ts',
+			'umbraco-package': 'umbraco-package.ts',
+		},
+	}),
 });
