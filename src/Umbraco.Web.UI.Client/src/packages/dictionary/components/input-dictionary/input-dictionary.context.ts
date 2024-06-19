cc530c9f41a8174fcd308e5d5229e9d6e6f4c883
Niels Lyngsø <niels.lyngso@gmail.com>
2024-06-19 15:55:43 +0200

    sort imports

--- src/Umbraco.Web.UI.Client/src/packages/dictionary/components/input-dictionary/input-dictionary.context.ts
+++ src/Umbraco.Web.UI.Client/src/packages/dictionary/components/input-dictionary/input-dictionary.context.ts
@@ -1,9 +1,9 @@
+import { UmbPickerInputContext } from '@umbraco-cms/backoffice/picker-input';
+import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import { UMB_DICTIONARY_ITEM_REPOSITORY_ALIAS } from '../../repository/index.js';
 import { UMB_DICTIONARY_PICKER_MODAL } from '../../modals/dictionary-picker-modal.token.js';
 import type { UmbDictionaryItemModel } from '../../repository/index.js';
 import type { UmbDictionaryTreeItemModel } from '../../tree/types.js';
-import { UmbPickerInputContext } from '@umbraco-cms/backoffice/picker-input';
-import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 
 export class UmbDictionaryPickerContext extends UmbPickerInputContext<
 	UmbDictionaryItemModel,
