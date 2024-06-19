764472b7f570b7de56a723f545ac2e0749ca8f04
Niels Lyngsø <niels.lyngso@gmail.com>
2024-06-19 16:06:05 +0200

    picker context corrections

--- src/Umbraco.Web.UI.Client/src/packages/dictionary/components/input-dictionary/input-dictionary.context.ts
+++ src/Umbraco.Web.UI.Client/src/packages/dictionary/components/input-dictionary/input-dictionary.context.ts
@@ -4,10 +4,16 @@ import { UMB_DICTIONARY_ITEM_REPOSITORY_ALIAS } from '../../repository/index.js'
 import { UMB_DICTIONARY_PICKER_MODAL } from '../../modals/dictionary-picker-modal.token.js';
 import type { UmbDictionaryItemModel } from '../../repository/index.js';
 import type { UmbDictionaryTreeItemModel } from '../../tree/types.js';
+import type {
+	UmbDictionaryPickerModalData,
+	UmbDictionaryPickerModalValue,
+} from '../../modals/dictionary-picker-modal.token.js';
 
 export class UmbDictionaryPickerContext extends UmbPickerInputContext<
 	UmbDictionaryItemModel,
-	UmbDictionaryTreeItemModel
+	UmbDictionaryTreeItemModel,
+	UmbDictionaryPickerModalData,
+	UmbDictionaryPickerModalValue
 > {
 	constructor(host: UmbControllerHost) {
 		super(host, UMB_DICTIONARY_ITEM_REPOSITORY_ALIAS, UMB_DICTIONARY_PICKER_MODAL);
--- src/Umbraco.Web.UI.Client/src/packages/dictionary/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/dictionary/index.ts
@@ -1,4 +1,4 @@
 export * from './repository/index.js';
 export * from './tree/index.js';
+export * from './modals/dictionary-picker-modal.token.js';
 export * from './entity.js';
-export { UMB_DICTIONARY_PICKER_MODAL } from './modals/dictionary-picker-modal.token.js';
