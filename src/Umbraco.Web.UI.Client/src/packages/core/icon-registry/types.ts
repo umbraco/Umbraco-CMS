a3abfb63ca2d36716f3d253d28098fbbf980b3d8
Mads Rasmussen <madsr@hey.com>
2024-06-20 22:28:53 +0200

    add interface for IconModule

--- src/Umbraco.Web.UI.Client/src/packages/core/icon-registry/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/icon-registry/types.ts
@@ -1,9 +1,13 @@
 import type { JsLoaderProperty } from '@umbraco-cms/backoffice/extension-api';
 
-export interface UmbIconDefinition<JsType extends object = object> {
+export interface UmbIconDefinition<JsType = any> {
 	name: string;
 	path: JsLoaderProperty<JsType>;
 	legacy?: boolean;
 }
 
 export type UmbIconDictionary = Array<UmbIconDefinition>;
+
+export interface UmbIconModule {
+	default: string;
+}
