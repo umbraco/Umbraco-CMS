5a58daf82f3ff756440182b9768faa938d2dda97
Mads Rasmussen <madsr@hey.com>
2024-06-20 13:13:27 +0200

    copy icons

--- src/Umbraco.Web.UI.Client/src/packages/core/vite.config.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/vite.config.ts
@@ -1,4 +1,5 @@
 import { defineConfig } from 'vite';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 import { rmSync } from 'fs';
 import { getDefaultConfig } from '../../vite-config-base';
 
@@ -57,5 +58,15 @@ export default defineConfig({
 			//'settings/index': './settings/index.ts', // currently no index file so it is left out
 			manifests: 'manifests.ts',
 		},
+		plugins: [
+			viteStaticCopy({
+				targets: [
+					{
+						src: 'icon-registry/icons/*.js',
+						dest: 'icon-registry/icons',
+					},
+				],
+			}),
+		],
 	}),
 });
