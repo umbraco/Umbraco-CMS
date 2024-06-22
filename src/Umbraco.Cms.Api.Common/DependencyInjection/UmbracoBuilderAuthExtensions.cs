cb090353f4f037431c3f712f2175a366f20999c6
Josh Brown <xd.joshuaaz+github@gmail.com>
2024-06-22 10:20:12 +0100

    Fix unguarded calls to ServiceDescriptor.ImplementationType for keyed services (#16604)
    * Update integration test base class to verify that calls to ServiceDescriptor.ImplementationType are guarded for keyed services
    
    * Fix unguarded calls to ServiceDescriptor.ImplementationType for keyed services

--- src/Umbraco.Cms.Api.Common/DependencyInjection/UmbracoBuilderAuthExtensions.cs
+++ src/Umbraco.Cms.Api.Common/DependencyInjection/UmbracoBuilderAuthExtensions.cs
@@ -17,7 +17,7 @@ public static class UmbracoBuilderAuthExtensions
 {
     public static IUmbracoBuilder AddUmbracoOpenIddict(this IUmbracoBuilder builder)
     {
-        if (builder.Services.Any(x=>x.ImplementationType == typeof(OpenIddictCleanupJob)) is false)
+        if (builder.Services.Any(x => !x.IsKeyedService && x.ImplementationType == typeof(OpenIddictCleanupJob)) is false)
         {
             ConfigureOpenIddict(builder);
         }
