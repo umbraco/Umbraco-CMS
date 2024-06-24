44939b321b94e3e7d7ace5f4c435b71cdbc3a75d
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-06-24 11:11:21 +0200

    fix: import icons from new path

--- src/Umbraco.Web.UI.Client/.storybook/preview.js
+++ src/Umbraco.Web.UI.Client/.storybook/preview.js
@@ -20,7 +20,7 @@ import { UmbIconRegistry } from '../src/packages/core/icon-registry/icon.registr
 import { UmbLitElement } from '../src/packages/core/lit-element';
 import { umbLocalizationRegistry } from '../src/packages/core/localization';
 import customElementManifests from '../dist-cms/custom-elements.json';
-import icons from '../src/packages/core/icon-registry/icons/icons';
+import icons from '../src/packages/core/icon-registry/icons';
 
 import '../src/libs/context-api/provide/context-provider.element';
 import '../src/packages/core/components';
