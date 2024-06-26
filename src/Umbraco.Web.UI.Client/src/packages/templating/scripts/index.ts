3a93aab0c8e4954c001abe0d4f175243bc235b5e
Mads Rasmussen <madsr@hey.com>
2024-06-26 13:14:35 +0200

    export everything from global components

--- src/Umbraco.Web.UI.Client/src/packages/templating/scripts/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/templating/scripts/index.ts
@@ -3,5 +3,6 @@ export * from './workspace/script-workspace.context-token.js';
 export * from './tree/index.js';
 export * from './types.js';
 export * from './entity.js';
+export * from './global-components/index.js';
 
 export { UMB_SCRIPT_PICKER_MODAL } from './modals/script-picker-modal.token.js';
