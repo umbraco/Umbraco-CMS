a320d44de43dc8e3dfa5588844500cd39e97dc5d
Mads Rasmussen <madsr@hey.com>
2024-06-26 08:55:22 +0200

    rename to local components

--- src/Umbraco.Web.UI.Client/src/packages/templating/partial-views/workspace/partial-view-workspace-editor.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/templating/partial-views/workspace/partial-view-workspace-editor.element.ts
@@ -1,4 +1,4 @@
-import type { UmbTemplatingInsertMenuElement } from '../../components/index.js';
+import type { UmbTemplatingInsertMenuElement } from '../../local-components/index.js';
 import { getQuerySnippet } from '../../utils/index.js';
 import { UMB_PARTIAL_VIEW_WORKSPACE_CONTEXT } from './partial-view-workspace.context-token.js';
 import { UMB_TEMPLATE_QUERY_BUILDER_MODAL } from '@umbraco-cms/backoffice/template';
