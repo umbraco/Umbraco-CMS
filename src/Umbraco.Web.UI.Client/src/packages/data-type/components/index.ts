0c55f59b6bc1c9ff34d15a3116ecf34bfa58d40e
Mads Rasmussen <madsr@hey.com>
2024-06-26 14:19:05 +0200

    export picker context

--- src/Umbraco.Web.UI.Client/src/packages/data-type/components/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/data-type/components/index.ts
@@ -2,3 +2,5 @@ import './data-type-input/data-type-input.element.js';
 import './data-type-flow-input/data-type-flow-input.element.js';
 import './ref-data-type/ref-data-type.element.js';
 import './property-editor-config/property-editor-config.element.js';
+
+export * from './data-type-input/index.js';
