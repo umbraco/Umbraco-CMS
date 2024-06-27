84c6a7ccbc4ac9f0b662e868494eebc88e1ae832
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2024-06-27 14:42:26 +0200

    Add new Hybridcache project

---
+++ Umbraco.PublishedCache.HybridCache/ContentCache.cs
@@ -0,0 +1,23 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Cms.Core.PublishedCache;
+
+namespace Umbraco.PublishedCache.HybridCache;
+
+internal class ContentCache : IPublishedHybridCache
+{
+    public IPublishedContent? GetById(bool preview, int contentId) => throw new NotImplementedException();
+
+    public IPublishedContent? GetById(bool preview, Guid contentId) => throw new NotImplementedException();
+
+    public IPublishedContent? GetById(int contentId) => throw new NotImplementedException();
+
+    public IPublishedContent? GetById(Guid contentId) => throw new NotImplementedException();
+
+    public bool HasById(bool preview, int contentId) => throw new NotImplementedException();
+
+    public bool HasById(int contentId) => throw new NotImplementedException();
+
+    public bool HasContent(bool preview) => throw new NotImplementedException();
+
+    public bool HasContent() => throw new NotImplementedException();
+}
---
+++ src/Umbraco.Core/PublishedCache/IPublishedHybridCache.cs
@@ -0,0 +1,72 @@
+﻿using Umbraco.Cms.Core.Models.PublishedContent;
+
+namespace Umbraco.Cms.Core.PublishedCache;
+
+public interface IPublishedHybridCache
+{
+    /// <summary>
+    ///     Gets a content identified by its unique identifier.
+    /// </summary>
+    /// <param name="preview">A value indicating whether to consider unpublished content.</param>
+    /// <param name="contentId">The content unique identifier.</param>
+    /// <returns>The content, or null.</returns>
+    /// <remarks>The value of <paramref name="preview" /> overrides defaults.</remarks>
+    IPublishedContent? GetById(bool preview, int contentId);
+
+    /// <summary>
+    ///     Gets a content identified by its unique identifier.
+    /// </summary>
+    /// <param name="preview">A value indicating whether to consider unpublished content.</param>
+    /// <param name="contentId">The content unique identifier.</param>
+    /// <returns>The content, or null.</returns>
+    /// <remarks>The value of <paramref name="preview" /> overrides defaults.</remarks>
+    IPublishedContent? GetById(bool preview, Guid contentId);
+
+    /// <summary>
+    ///     Gets a content identified by its unique identifier.
+    /// </summary>
+    /// <param name="contentId">The content unique identifier.</param>
+    /// <returns>The content, or null.</returns>
+    /// <remarks>Considers published or unpublished content depending on defaults.</remarks>
+    IPublishedContent? GetById(int contentId);
+
+    /// <summary>
+    ///     Gets a content identified by its unique identifier.
+    /// </summary>
+    /// <param name="contentId">The content unique identifier.</param>
+    /// <returns>The content, or null.</returns>
+    /// <remarks>Considers published or unpublished content depending on defaults.</remarks>
+    IPublishedContent? GetById(Guid contentId);
+
+    /// <summary>
+    ///     Gets a value indicating whether the cache contains a specified content.
+    /// </summary>
+    /// <param name="preview">A value indicating whether to consider unpublished content.</param>
+    /// <param name="contentId">The content unique identifier.</param>
+    /// <returns>A value indicating whether to the cache contains the specified content.</returns>
+    /// <remarks>The value of <paramref name="preview" /> overrides defaults.</remarks>
+    bool HasById(bool preview, int contentId);
+
+    /// <summary>
+    ///     Gets a value indicating whether the cache contains a specified content.
+    /// </summary>
+    /// <param name="contentId">The content unique identifier.</param>
+    /// <returns>A value indicating whether to the cache contains the specified content.</returns>
+    /// <remarks>Considers published or unpublished content depending on defaults.</remarks>
+    bool HasById(int contentId);
+
+    /// <summary>
+    ///     Gets a value indicating whether the cache contains published content.
+    /// </summary>
+    /// <param name="preview">A value indicating whether to consider unpublished content.</param>
+    /// <returns>A value indicating whether the cache contains published content.</returns>
+    /// <remarks>The value of <paramref name="preview" /> overrides defaults.</remarks>
+    bool HasContent(bool preview);
+
+    /// <summary>
+    ///     Gets a value indicating whether the cache contains published content.
+    /// </summary>
+    /// <returns>A value indicating whether the cache contains published content.</returns>
+    /// <remarks>Considers published or unpublished content depending on defaults.</remarks>
+    bool HasContent();
+}
