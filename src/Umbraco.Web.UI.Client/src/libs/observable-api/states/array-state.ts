e46da116253804559f708c6f621e8f9033ce1929
Niels Lyngsø <niels.lyngso@gmail.com>
2024-07-01 11:28:52 +0200

    make mutable array before sorting

--- src/Umbraco.Web.UI.Client/src/libs/observable-api/states/array-state.ts
+++ src/Umbraco.Web.UI.Client/src/libs/observable-api/states/array-state.ts
@@ -35,7 +35,7 @@ export class UmbArrayState<T> extends UmbDeepState<T[]> {
 	 */
 	sortBy(sortMethod?: (a: T, b: T) => number) {
 		this.#sortMethod = sortMethod;
-		super.setValue(this.getValue().sort(this.#sortMethod));
+		super.setValue([...this.getValue()].sort(this.#sortMethod));
 		return this;
 	}
 
@@ -51,7 +51,7 @@ export class UmbArrayState<T> extends UmbDeepState<T[]> {
 	 */
 	override setValue(value: T[]) {
 		if (this.#sortMethod) {
-			super.setValue(value.sort(this.#sortMethod));
+			super.setValue([...value].sort(this.#sortMethod));
 		} else {
 			super.setValue(value);
 		}
