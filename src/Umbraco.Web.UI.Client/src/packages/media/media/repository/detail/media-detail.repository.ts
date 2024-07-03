00760174977f1dc1bcd8d953317bcc6292c68448
Mads Rasmussen <madsr@hey.com>
2024-07-03 21:20:45 +0200

    split consts

--- src/Umbraco.Web.UI.Client/src/packages/media/media/repository/detail/media-detail.repository.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/repository/detail/media-detail.repository.ts
@@ -1,6 +1,6 @@
 import type { UmbMediaDetailModel } from '../../types.js';
 import { UmbMediaServerDataSource } from './media-detail.server.data-source.js';
-import { UMB_MEDIA_DETAIL_STORE_CONTEXT } from './media-detail.store.js';
+import { UMB_MEDIA_DETAIL_STORE_CONTEXT } from './media-detail.store.context-token.js';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import { UmbDetailRepositoryBase } from '@umbraco-cms/backoffice/repository';
 
@@ -9,5 +9,7 @@ export class UmbMediaDetailRepository extends UmbDetailRepositoryBase<UmbMediaDe
 		super(host, UmbMediaServerDataSource, UMB_MEDIA_DETAIL_STORE_CONTEXT);
 	}
 }
+
+export { UmbMediaDetailRepository as api };
 
 export default UmbMediaDetailRepository;
