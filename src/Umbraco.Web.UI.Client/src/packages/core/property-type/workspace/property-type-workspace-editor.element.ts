6f7b4b4c7066014e7c631f26a26fca65a1da174d
Niels Lyngsø <niels.lyngso@gmail.com>
2024-07-04 16:00:02 +0200

    correct headline

--- src/Umbraco.Web.UI.Client/src/packages/core/property-type/workspace/property-type-workspace-editor.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property-type/workspace/property-type-workspace-editor.element.ts
@@ -8,6 +8,9 @@ export class UmbPropertyTypeWorkspaceEditorElement extends UmbLitElement {
 	#workspaceContext?: typeof UMB_PROPERTY_TYPE_WORKSPACE_CONTEXT.TYPE;
 
 	@state()
+	_isNew?: boolean;
+
+	@state()
 	_name?: string;
 
 	@property({ type: String, attribute: false })
@@ -18,6 +21,9 @@ export class UmbPropertyTypeWorkspaceEditorElement extends UmbLitElement {
 
 		this.consumeContext(UMB_PROPERTY_TYPE_WORKSPACE_CONTEXT, (context) => {
 			this.#workspaceContext = context;
+			this.observe(context.isNew, (isNew) => {
+				this._isNew = isNew;
+			});
 			this.observe(context.name, (name) => {
 				this._name = name;
 			});
@@ -26,11 +32,14 @@ export class UmbPropertyTypeWorkspaceEditorElement extends UmbLitElement {
 	}
 
 	override render() {
-		return this.workspaceAlias
+		return this.workspaceAlias && this._isNew !== undefined
 			? html`
 					<umb-workspace-editor
 						alias=${this.workspaceAlias}
-						headline=${this.localize.term('blockEditor_blockConfigurationOverlayTitle', [this._name])}>
+						headline=${this.localize.term(
+							this._isNew ? 'contentTypeEditor_addProperty' : 'contentTypeEditor_editProperty',
+							[this._name],
+						)}>
 					</umb-workspace-editor>
 				`
 			: '';
