09b1f36705108c7577667a3110860e3f7b8871e2
Nhu Dinh <hnd@umbraco.dk>
2024-07-04 14:56:32 +0700

    Removed the test for content picker

--- tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Content/ContentWithPropertyEditors.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Content/ContentWithPropertyEditors.spec.ts
@@ -120,34 +120,6 @@ test('can create content with the tags datatype', async ({umbracoApi, umbracoUi}
   expect(contentData.values[0].value).toEqual([tagName]);
 });
 
-test('can create content with the content picker datatype', async ({umbracoApi, umbracoUi}) => {
-  // Arrange
-  const dataTypeName = 'Content Picker';
-  const contentPickerName = 'TestContentPicker';
-  const dataTypeData = await umbracoApi.dataType.getByName(dataTypeName);
-  const documentTypeId = await umbracoApi.documentType.createDocumentTypeWithPropertyEditor(documentTypeName, dataTypeName, dataTypeData.id);
-  const contentPickerId = await umbracoApi.document.createDefaultDocument(contentPickerName, documentTypeId);
-  await umbracoUi.goToBackOffice();
-  await umbracoUi.content.goToSection(ConstantHelper.sections.content);
-
-  // Act
-  await umbracoUi.content.clickActionsMenuAtRoot();
-  await umbracoUi.content.clickCreateButton();
-  await umbracoUi.content.chooseDocumentType(documentTypeName);
-  await umbracoUi.content.enterContentName(contentName);
-  await umbracoUi.content.addContentPicker(contentPickerName);
-  await umbracoUi.content.clickSaveAndPublishButton();
-
-  // Assert
-  await umbracoUi.content.doesSuccessNotificationsHaveCount(2);
-  expect(await umbracoApi.document.doesNameExist(contentName)).toBeTruthy();
-  const contentData = await umbracoApi.document.getByName(contentName);
-  expect(contentData.values[0].value).toEqual(contentPickerId);
-
-  // Clean
-  await umbracoApi.document.ensureNameNotExists(contentPickerName);
-});
-
 // TODO: Remove skip and update the test when the front-end is ready. Currently the list of content is not displayed.
 test.skip('can create content with the list view - content datatype', async ({umbracoApi, umbracoUi}) => {
   // Arrange
