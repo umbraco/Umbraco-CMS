227559fe7a5d44324b529af4f0248fc98a6a81fa
Mads Rasmussen <madsr@hey.com>
2024-07-04 11:59:11 +0200

    catch the error instead of relying on a timeout

--- src/Umbraco.Web.UI.Client/src/packages/core/extension-registry/utils/create-extension-api-by-alias.test.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/extension-registry/utils/create-extension-api-by-alias.test.ts
@@ -46,7 +46,6 @@ describe('Create Extension Api By Alias Method', () => {
 		hostElement = await fixture(html`<umb-test-controller-host></umb-test-controller-host>`);
 	});
 
-	/*
 	it('Returns `undefined` when manifest does not have any correct properties', (done) => {
 		const manifest: ManifestApi = {
 			type: 'my-test-type',
@@ -55,17 +54,17 @@ describe('Create Extension Api By Alias Method', () => {
 		};
 		umbExtensionsRegistry.register(manifest);
 
-		createExtensionApiByAlias<UmbExtensionApiBoolTestClass>(hostElement, manifest.alias, []).then(() => {
-			umbExtensionsRegistry.unregister(manifest.alias);
-			done(new Error('Should not resolve'));
-		});
-
-		setTimeout(() => {
-			umbExtensionsRegistry.unregister(manifest.alias);
-			done();
-		}, 10);
+		createExtensionApiByAlias<UmbExtensionApiBoolTestClass>(hostElement, manifest.alias, []).then(
+			() => {
+				umbExtensionsRegistry.unregister(manifest.alias);
+				done(new Error('Should not resolve'));
+			},
+			() => {
+				umbExtensionsRegistry.unregister(manifest.alias);
+				done();
+			},
+		);
 	});
-	*/
 
 	it('Handles when `api` property contains a class constructor', async () => {
 		const manifest: ManifestApi<UmbExtensionApiTrueTestClass> = {
