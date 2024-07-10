e3c7a7bf73dc5be5da0791d7f3e9ceee91b6a35a
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-07-10 10:55:02 +0200

    feat: use the `<umb-imaging-thumbnail>` element in media collections

--- src/Umbraco.Web.UI.Client/src/packages/media/media/collection/views/grid/media-grid-collection-view.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/collection/views/grid/media-grid-collection-view.element.ts
@@ -52,7 +52,7 @@ export class UmbMediaGridCollectionViewElement extends UmbLitElement {
 
 		this.observe(this.#collectionContext.loading, (loading) => (this._loading = loading), '_observeLoading');
 
-		this.observe(this.#collectionContext.thumbnailItems, (items) => (this._items = items), '_observeItems');
+		this.observe(this.#collectionContext.items, (items) => (this._items = items), '_observeItems');
 
 		this.observe(
 			this.#collectionContext.selection.selection,
@@ -127,13 +127,7 @@ export class UmbMediaGridCollectionViewElement extends UmbLitElement {
 				@selected=${() => this.#onSelect(item)}
 				@deselected=${() => this.#onDeselect(item)}
 				class="media-item">
-				${when(
-					item.url,
-					() => html`<img src=${item.url!} alt=${item.name} draggable="false" />`,
-					() => html`<umb-icon name=${item.icon}></umb-icon>`,
-				)}
-				<!-- TODO: [LK] I'd like to indicate a busy state when bulk actions are triggered. -->
-				<!-- <div class="container"><uui-loader></uui-loader></div> -->
+				<umb-imaging-thumbnail unique=${item.unique} alt=${item.name} icon=${item.icon}></umb-imaging-thumbnail>
 			</uui-card-media>
 		`;
 	}
@@ -157,16 +151,6 @@ export class UmbMediaGridCollectionViewElement extends UmbLitElement {
 				grid-template-columns: repeat(auto-fill, minmax(200px, 1fr));
 				grid-auto-rows: 200px;
 				gap: var(--uui-size-space-5);
-			}
-
-			img {
-				background-image: url('data:image/svg+xml;charset=utf-8,<svg xmlns="http://www.w3.org/2000/svg" width="100" height="100" fill-opacity=".1"><path d="M50 0h50v50H50zM0 50h50v50H0z"/></svg>');
-				background-size: 10px 10px;
-				background-repeat: repeat;
-			}
-
-			umb-icon {
-				font-size: var(--uui-size-8);
 			}
 		`,
 	];
