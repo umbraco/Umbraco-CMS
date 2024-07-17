47e2c41f9a5c4fa582efd89d6c4f288f08bdafba
leekelleher <leekelleher@gmail.com>
2024-07-17 13:56:50 +0100

    Corrects manifest type
    and includes missing property.

--- src/Umbraco.Web.UI.Client/src/packages/media/media/entity-bulk-actions/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/entity-bulk-actions/manifests.ts
@@ -1,19 +1,21 @@
 import { UMB_MEDIA_COLLECTION_ALIAS } from '../collection/index.js';
+import { UMB_MEDIA_ENTITY_TYPE } from '../entity.js';
 import { manifests as moveToManifests } from './move-to/manifests.js';
 import {
 	UMB_COLLECTION_ALIAS_CONDITION,
 	UMB_COLLECTION_BULK_ACTION_PERMISSION_CONDITION,
 } from '@umbraco-cms/backoffice/collection';
-import type { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
+import type { ManifestEntityBulkAction, ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
 import type { UmbCollectionBulkActionPermissions } from '@umbraco-cms/backoffice/collection';
 
-const entityBulkActions: Array<ManifestTypes> = [
+const entityBulkActions: Array<ManifestEntityBulkAction> = [
 	{
 		type: 'entityBulkAction',
 		alias: 'Umb.EntityBulkAction.Media.Duplicate',
 		name: 'Duplicate Media Entity Bulk Action',
 		weight: 30,
 		api: () => import('./duplicate/duplicate.action.js'),
+		forEntityTypes: [UMB_MEDIA_ENTITY_TYPE],
 		meta: {
 			label: 'Duplicate',
 		},
@@ -34,6 +36,7 @@ const entityBulkActions: Array<ManifestTypes> = [
 		name: 'Delete Media Entity Bulk Action',
 		weight: 10,
 		api: () => import('./delete/delete.action.js'),
+		forEntityTypes: [UMB_MEDIA_ENTITY_TYPE],
 		meta: {
 			label: 'Delete',
 		},
