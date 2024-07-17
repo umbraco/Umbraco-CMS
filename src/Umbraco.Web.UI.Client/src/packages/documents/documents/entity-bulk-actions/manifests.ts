5ede94516e7206f3b01a2415980b001a6f063c77
leekelleher <leekelleher@gmail.com>
2024-07-17 13:55:46 +0100

    Feature: Document "move to" entity bulk action

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/entity-bulk-actions/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/entity-bulk-actions/manifests.ts
@@ -1,13 +1,15 @@
 import { UMB_DOCUMENT_COLLECTION_ALIAS } from '../collection/index.js';
 import { UMB_DOCUMENT_ENTITY_TYPE } from '../entity.js';
+//import { UMB_DOCUMENT_TREE_ALIAS } from '../tree/manifests.js';
+import { manifests as moveToManifests } from './move-to/manifests.js';
 import type { UmbCollectionBulkActionPermissions } from '@umbraco-cms/backoffice/collection';
-import type { ManifestEntityBulkAction } from '@umbraco-cms/backoffice/extension-registry';
+import type { ManifestEntityBulkAction, ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
 import {
 	UMB_COLLECTION_ALIAS_CONDITION,
 	UMB_COLLECTION_BULK_ACTION_PERMISSION_CONDITION,
 } from '@umbraco-cms/backoffice/collection';
 
-export const manifests: Array<ManifestEntityBulkAction> = [
+export const entityBulkActions: Array<ManifestEntityBulkAction> = [
 	{
 		type: 'entityBulkAction',
 		kind: 'default',
@@ -76,30 +78,6 @@ export const manifests: Array<ManifestEntityBulkAction> = [
 		],
 	},
 	*/
-	/* TODO: implement bulk move action
-	{
-		type: 'entityBulkAction',
-		kind: 'default',
-		alias: 'Umb.EntityBulkAction.Document.MoveTo',
-		name: 'Move Document Entity Bulk Action',
-		weight: 20,
-		api: UmbMoveDocumentEntityBulkAction,
-		meta: {
-			label: 'Move',
-		},
-		forEntityTypes: [UMB_DOCUMENT_ENTITY_TYPE],
-		conditions: [
-			{
-				alias: UMB_COLLECTION_ALIAS_CONDITION,
-				match: UMB_DOCUMENT_COLLECTION_ALIAS,
-			},
-			{
-				alias: UMB_COLLECTION_BULK_ACTION_PERMISSION_CONDITION,
-				match: (permissions: UmbCollectionBulkActionPermissions) => permissions.allowBulkMove,
-			},
-		],
-	},
-	*/
 	/* TODO: implement bulk trash action
 	{
 		type: 'entityBulkAction',
@@ -125,3 +103,5 @@ export const manifests: Array<ManifestEntityBulkAction> = [
 	},
 	*/
 ];
+
+export const manifests: Array<ManifestTypes> = [...entityBulkActions, ...moveToManifests];
