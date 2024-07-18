03a2368b498cf40d50e758fba8b3aa0ad3144a9c
leekelleher <leekelleher@gmail.com>
2024-07-18 17:54:21 +0100

    Deprecated the `loadCodeEditor` function

--- src/Umbraco.Web.UI.Client/src/packages/templating/code-editor/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/templating/code-editor/index.ts
@@ -1,6 +1,12 @@
-export type { UmbCodeEditorElement } from './code-editor.element.js';
+export { UmbCodeEditorElement } from './code-editor.element.js';
+export { UmbCodeEditorLoadedEvent } from './code-editor-loaded.event.js';
 export type { UmbCodeEditorController } from './code-editor.controller.js';
+export type * from './code-editor.model.js';
 
+/**
+ * @deprecated Use `import from '@umbraco-cms/backoffice/code-editor';` directly.
+ * This function will be removed in Umbraco 15.
+ */
 export function loadCodeEditor() {
-	return import('./code-editor.element.js');
+	return import('@umbraco-cms/backoffice/code-editor');
 }
