26dc492daf9835bde0401b0f07ca7649de834926
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-07-19 12:02:03 +0200

    feat: add bundling to code-editor

--- src/Umbraco.Web.UI.Client/src/packages/code-editor/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/code-editor/index.ts
@@ -1,8 +1,6 @@
 export * from './components/index.js';
 export * from './models/index.js';
-export { UmbCodeEditorLoadedEvent } from './models/code-editor-loaded.event.js';
 export type { UmbCodeEditorController } from './code-editor.controller.js';
-export type * from './models/code-editor.model.js';
 
 /**
  * @deprecated Use `import from '@umbraco-cms/backoffice/code-editor';` directly.
