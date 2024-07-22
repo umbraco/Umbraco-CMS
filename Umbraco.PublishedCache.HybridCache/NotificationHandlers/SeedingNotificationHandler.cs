97c7d08c19ca9ff78d8ed401d992aaa32771bffe
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2024-07-22 08:18:19 +0200

    Only seed when main

--- Umbraco.PublishedCache.HybridCache/NotificationHandlers/SeedingNotificationHandler.cs
+++ Umbraco.PublishedCache.HybridCache/NotificationHandlers/SeedingNotificationHandler.cs
@@ -1,5 +1,6 @@
 ﻿using Umbraco.Cms.Core.Events;
 using Umbraco.Cms.Core.Notifications;
+using Umbraco.Cms.Core.Sync;
 using Umbraco.Cms.Infrastructure.HybridCache.Services;
 
 namespace Umbraco.Cms.Infrastructure.HybridCache.NotificationHandlers;
@@ -7,13 +8,21 @@ namespace Umbraco.Cms.Infrastructure.HybridCache.NotificationHandlers;
 internal class SeedingNotificationHandler : INotificationAsyncHandler<UmbracoApplicationStartedNotification>
 {
     private readonly IContentCacheService _contentCacheService;
+    private readonly IServerRoleAccessor _serverRoleAccessor;
 
-    public SeedingNotificationHandler(IContentCacheService contentCacheService)
+    public SeedingNotificationHandler(IContentCacheService contentCacheService, IServerRoleAccessor serverRoleAccessor)
     {
         _contentCacheService = contentCacheService;
+        _serverRoleAccessor = serverRoleAccessor;
     }
+
     public async Task HandleAsync(UmbracoApplicationStartedNotification notification, CancellationToken cancellationToken)
     {
+        if (_serverRoleAccessor.CurrentServerRole is ServerRole.Subscriber or ServerRole.Unknown)
+        {
+            return;
+        }
+
         await _contentCacheService.SeedAsync(new List<int>());
     }
 }
