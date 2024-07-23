6da6b3ece8958f9099985fca56ab225324de8554
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-07-23 11:43:10 +0200

    fix: add a routable workspace for the blueprint root

--- src/Umbraco.Web.UI.Client/src/packages/documents/document-blueprints/workspace/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/document-blueprints/workspace/manifests.ts
@@ -1,10 +1,9 @@
-import { UMB_DOCUMENT_BLUEPRINT_ENTITY_TYPE } from '../entity.js';
+import { UMB_DOCUMENT_BLUEPRINT_ENTITY_TYPE, UMB_DOCUMENT_BLUEPRINT_ROOT_ENTITY_TYPE } from '../entity.js';
 import { UmbSaveWorkspaceAction } from '@umbraco-cms/backoffice/workspace';
 import type {
 	ManifestTypes,
 	ManifestWorkspace,
 	ManifestWorkspaceActions,
-	ManifestWorkspaceView,
 } from '@umbraco-cms/backoffice/extension-registry';
 
 export const UMB_DOCUMENT_BLUEPRINT_WORKSPACE_ALIAS = 'Umb.Workspace.DocumentBlueprint';
@@ -20,26 +19,16 @@ const workspace: ManifestWorkspace = {
 	},
 };
 
-const workspaceViews: Array<ManifestWorkspaceView> = [
-	{
-		type: 'workspaceView',
-		kind: 'contentEditor',
-		alias: 'Umb.WorkspaceView.DocumentBlueprint.Edit',
-		name: 'Document Blueprint Workspace Edit View',
-		weight: 200,
-		meta: {
-			label: '#general_content',
-			pathname: 'content',
-			icon: 'document',
-		},
-		conditions: [
-			{
-				alias: 'Umb.Condition.WorkspaceAlias',
-				match: workspace.alias,
-			},
-		],
+const rootWorkspace: ManifestWorkspace = {
+	type: 'workspace',
+	kind: 'routable',
+	alias: 'Umb.Workspace.DocumentBlueprint.Root',
+	name: 'Document Blueprint Root Workspace',
+	api: () => import('./document-blueprint-workspace.context.js'),
+	meta: {
+		entityType: UMB_DOCUMENT_BLUEPRINT_ROOT_ENTITY_TYPE,
 	},
-];
+};
 
 const workspaceActions: Array<ManifestWorkspaceActions> = [
 	{
@@ -63,4 +52,4 @@ const workspaceActions: Array<ManifestWorkspaceActions> = [
 	},
 ];
 
-export const manifests: Array<ManifestTypes> = [workspace, ...workspaceViews, ...workspaceActions];
+export const manifests: Array<ManifestTypes> = [rootWorkspace, workspace, ...workspaceActions];
