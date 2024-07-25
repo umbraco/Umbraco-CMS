8d66ea767ec3cb16760cbdc3631e958a350b02ea
Elitsa <elm@umbraco.dk>
2024-07-25 14:31:27 +0200

    Adding INavigationService dependency and Enlist updating navigation structure actions

--- src/Umbraco.Core/Services/ContentService.cs
+++ src/Umbraco.Core/Services/ContentService.cs
@@ -12,6 +12,7 @@ using Umbraco.Cms.Core.Persistence.Querying;
 using Umbraco.Cms.Core.Persistence.Repositories;
 using Umbraco.Cms.Core.Scoping;
 using Umbraco.Cms.Core.Services.Changes;
+using Umbraco.Cms.Core.Services.Navigation;
 using Umbraco.Cms.Core.Strings;
 using Umbraco.Extensions;
 
@@ -33,25 +34,27 @@ public class ContentService : RepositoryService, IContentService
     private readonly IShortStringHelper _shortStringHelper;
     private readonly ICultureImpactFactory _cultureImpactFactory;
     private readonly IUserIdKeyResolver _userIdKeyResolver;
+    private readonly INavigationService _navigationService;
     private IQuery<IContent>? _queryNotTrashed;
 
     #region Constructors
 
     public ContentService(
-    ICoreScopeProvider provider,
-    ILoggerFactory loggerFactory,
-    IEventMessagesFactory eventMessagesFactory,
-    IDocumentRepository documentRepository,
-    IEntityRepository entityRepository,
-    IAuditRepository auditRepository,
-    IContentTypeRepository contentTypeRepository,
-    IDocumentBlueprintRepository documentBlueprintRepository,
-    ILanguageRepository languageRepository,
-    Lazy<IPropertyValidationService> propertyValidationService,
-    IShortStringHelper shortStringHelper,
-    ICultureImpactFactory cultureImpactFactory,
-    IUserIdKeyResolver userIdKeyResolver)
-    : base(provider, loggerFactory, eventMessagesFactory)
+        ICoreScopeProvider provider,
+        ILoggerFactory loggerFactory,
+        IEventMessagesFactory eventMessagesFactory,
+        IDocumentRepository documentRepository,
+        IEntityRepository entityRepository,
+        IAuditRepository auditRepository,
+        IContentTypeRepository contentTypeRepository,
+        IDocumentBlueprintRepository documentBlueprintRepository,
+        ILanguageRepository languageRepository,
+        Lazy<IPropertyValidationService> propertyValidationService,
+        IShortStringHelper shortStringHelper,
+        ICultureImpactFactory cultureImpactFactory,
+        IUserIdKeyResolver userIdKeyResolver,
+        INavigationService navigationService)
+        : base(provider, loggerFactory, eventMessagesFactory)
     {
         _documentRepository = documentRepository;
         _entityRepository = entityRepository;
@@ -63,7 +66,41 @@ public class ContentService : RepositoryService, IContentService
         _shortStringHelper = shortStringHelper;
         _cultureImpactFactory = cultureImpactFactory;
         _userIdKeyResolver = userIdKeyResolver;
+        _navigationService = navigationService;
         _logger = loggerFactory.CreateLogger<ContentService>();
+    }
+
+    [Obsolete("Use constructor that takes INavigationService as a parameter, scheduled for removal in V16")]
+    public ContentService(
+        ICoreScopeProvider provider,
+        ILoggerFactory loggerFactory,
+        IEventMessagesFactory eventMessagesFactory,
+        IDocumentRepository documentRepository,
+        IEntityRepository entityRepository,
+        IAuditRepository auditRepository,
+        IContentTypeRepository contentTypeRepository,
+        IDocumentBlueprintRepository documentBlueprintRepository,
+        ILanguageRepository languageRepository,
+        Lazy<IPropertyValidationService> propertyValidationService,
+        IShortStringHelper shortStringHelper,
+        ICultureImpactFactory cultureImpactFactory,
+        IUserIdKeyResolver userIdKeyResolver)
+        : this(
+            provider,
+            loggerFactory,
+            eventMessagesFactory,
+            documentRepository,
+            entityRepository,
+            auditRepository,
+            contentTypeRepository,
+            documentBlueprintRepository,
+            languageRepository,
+            propertyValidationService,
+            shortStringHelper,
+            cultureImpactFactory,
+            userIdKeyResolver,
+            StaticServiceProvider.Instance.GetRequiredService<INavigationService>())
+    {
     }
 
     [Obsolete("Use constructor that takes IUserIdKeyResolver as a parameter, scheduled for removal in V15")]
@@ -93,7 +130,8 @@ public class ContentService : RepositoryService, IContentService
             propertyValidationService,
             shortStringHelper,
             cultureImpactFactory,
-            StaticServiceProvider.Instance.GetRequiredService<IUserIdKeyResolver>())
+            StaticServiceProvider.Instance.GetRequiredService<IUserIdKeyResolver>(),
+            StaticServiceProvider.Instance.GetRequiredService<INavigationService>())
     {
     }
 
@@ -1034,6 +1072,11 @@ public class ContentService : RepositoryService, IContentService
             // have always changed if it's been saved in the back office but that's not really fail safe.
             _documentRepository.Save(content);
 
+            // Updates in-memory navigation structure - we only handle new items, other updates are not a concern
+            UpdateInMemoryNavigationStructure(
+                "Umbraco.Cms.Core.Services.ContentService.Save-with-contentSchedule",
+                () => _navigationService.Add(content.Key, GetParent(content)?.Key));
+
             if (contentSchedule != null)
             {
                 _documentRepository.PersistContentSchedule(content, contentSchedule);
@@ -1097,6 +1140,11 @@ public class ContentService : RepositoryService, IContentService
                 content.WriterId = userId;
 
                 _documentRepository.Save(content);
+
+                // Updates in-memory navigation structure - we only handle new items, other updates are not a concern
+                UpdateInMemoryNavigationStructure(
+                    "Umbraco.Cms.Core.Services.ContentService.Save",
+                    () => _navigationService.Add(content.Key, GetParent(content)?.Key));
             }
 
             scope.Notifications.Publish(
@@ -2288,6 +2336,18 @@ public class ContentService : RepositoryService, IContentService
         }
 
         DoDelete(content);
+
+        if (content.Trashed)
+        {
+            // TODO: call trashed navigation delete
+        }
+        else
+        {
+            // Updates in-memory navigation structure
+            UpdateInMemoryNavigationStructure(
+                "Umbraco.Cms.Core.Services.ContentService.DeleteLocked",
+                () => _navigationService.Remove(content.Key));
+        }
     }
 
     // TODO: both DeleteVersions methods below have an issue. Sort of. They do NOT take care of files the way
@@ -2560,6 +2620,18 @@ public class ContentService : RepositoryService, IContentService
             }
         }
         while (total > pageSize);
+
+        if (parentId == Constants.System.RecycleBinContent)
+        {
+            // TODO: call trashed navigation move
+        }
+        else
+        {
+            // Updates in-memory navigation structure
+            UpdateInMemoryNavigationStructure(
+                "Umbraco.Cms.Core.Services.ContentService.PerformMoveLocked",
+                () => _navigationService.Move(content.Key, parent?.Key));
+        }
     }
 
     private void PerformMoveContentLocked(IContent content, int userId, bool? trash)
@@ -2663,6 +2735,9 @@ public class ContentService : RepositoryService, IContentService
     {
         EventMessages eventMessages = EventMessagesFactory.Get();
 
+        // keep track of updates (copied item key and parent key) for the in-memory navigation structure
+        var navigationUpdates = new List<Tuple<Guid, Guid?>>();
+
         IContent copy = content.DeepCloneWithResetIdentities();
         copy.ParentId = parentId;
 
@@ -2698,6 +2773,9 @@ public class ContentService : RepositoryService, IContentService
 
             // save and flush because we need the ID for the recursive Copying events
             _documentRepository.Save(copy);
+
+            // store navigation update information for copied item
+            navigationUpdates.Add(Tuple.Create(copy.Key, GetParent(copy)?.Key));
 
             // add permissions
             if (currentPermissions.Count > 0)
@@ -2750,10 +2828,27 @@ public class ContentService : RepositoryService, IContentService
                         // save and flush (see above)
                         _documentRepository.Save(descendantCopy);
 
+                        // store navigation update information for descendants
+                        navigationUpdates.Add(Tuple.Create(descendantCopy.Key, GetParent(descendantCopy)?.Key));
+
                         copies.Add(Tuple.Create(descendant, descendantCopy));
                         idmap[descendant.Id] = descendantCopy.Id;
                     }
                 }
+            }
+
+            if (navigationUpdates.Count > 0)
+            {
+                // Updates in-memory navigation structure
+                UpdateInMemoryNavigationStructure(
+                    "Umbraco.Cms.Core.Services.ContentService.Copy",
+                    () =>
+                    {
+                        foreach (Tuple<Guid, Guid?> update in navigationUpdates)
+                        {
+                            _navigationService.Add(update.Item1, update.Item2);
+                        }
+                    });
             }
 
             // not handling tags here, because
@@ -3697,4 +3792,29 @@ public class ContentService : RepositoryService, IContentService
         DeleteBlueprintsOfTypes(new[] { contentTypeId }, userId);
 
     #endregion
+
+    /// <summary>
+    ///     Enlists an action in the current scope context to update the in-memory navigation structure
+    ///     when the scope completes successfully.
+    /// </summary>
+    /// <param name="enlistingActionKey">The unique key identifying the action to be enlisted.</param>
+    /// <param name="updateNavigation">The action to be performed for updating the in-memory navigation structure.</param>
+    /// <exception cref="NullReferenceException">Thrown when the scope context is null and therefore cannot be used.</exception>
+    private void UpdateInMemoryNavigationStructure(string enlistingActionKey, Action updateNavigation)
+    {
+        IScopeContext? scopeContext = ScopeProvider.Context;
+
+        if (scopeContext is null)
+        {
+            throw new NullReferenceException($"The {nameof(scopeContext)} is null and cannot be used.");
+        }
+
+        scopeContext.Enlist(enlistingActionKey, completed =>
+        {
+            if (completed)
+            {
+                updateNavigation();
+            }
+        });
+    }
 }
