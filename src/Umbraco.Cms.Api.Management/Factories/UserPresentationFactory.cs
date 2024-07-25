a44aef7c1377fbecc0046cbeb2513459159a1f94
kjac <kja@umbraco.dk>
2024-07-25 09:54:24 +0200

    Support user type in the Management API

--- src/Umbraco.Cms.Api.Management/Factories/UserPresentationFactory.cs
+++ src/Umbraco.Cms.Api.Management/Factories/UserPresentationFactory.cs
@@ -80,6 +80,7 @@ public class UserPresentationFactory : IUserPresentationFactory
             LastLockoutDate = user.LastLockoutDate,
             LastPasswordChangeDate = user.LastPasswordChangeDate,
             IsAdmin = user.IsAdmin(),
+            Type = user.Type
         };
 
         return responseModel;
@@ -92,6 +93,7 @@ public class UserPresentationFactory : IUserPresentationFactory
             Name = user.Name ?? user.Username,
             AvatarUrls = user.GetUserAvatarUrls(_appCaches.RuntimeCache, _mediaFileManager, _imageUrlGenerator)
                 .Select(url => _absoluteUrlBuilder.ToAbsoluteUrl(url).ToString()),
+            Type = user.Type
         };
 
     public async Task<UserCreateModel> CreateCreationModelAsync(CreateUserRequestModel requestModel)
@@ -103,6 +105,7 @@ public class UserPresentationFactory : IUserPresentationFactory
             Name = requestModel.Name,
             UserName = requestModel.UserName,
             UserGroupKeys = requestModel.UserGroupIds.Select(x => x.Id).ToHashSet(),
+            Type = requestModel.Type
         };
 
         return await Task.FromResult(createModel);
