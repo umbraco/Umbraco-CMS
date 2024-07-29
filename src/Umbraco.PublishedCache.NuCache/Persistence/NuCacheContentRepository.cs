umbraco/Umbraco-CMS

commit 8ccc2faee075693a05737b2470c9a71da4dbd651

Add UsePagedSqlQuery setting to NucacheSettings (#14806)
* imlement UsePagedSqlQuery

* Remove unneccesary spacing

src/Umbraco.PublishedCache.NuCache/Persistence/NuCacheContentRepository.cs
@@ -221,19 +221,12 @@ AND cmsContentNu.nodeId IS NULL
             .Append(SqlObjectTypeNotTrashed(SqlContext, Constants.ObjectTypes.Document))
             .Append(SqlOrderByLevelIdSortOrder(SqlContext));
 
-        // Use a more efficient COUNT query
-        Sql<ISqlContext>? sqlCountQuery = SqlContentSourcesCount()
-            .Append(SqlObjectTypeNotTrashed(SqlContext, Constants.ObjectTypes.Document));
-
-        Sql<ISqlContext>? sqlCount =
-            SqlContext.Sql("SELECT COUNT(*) FROM (").Append(sqlCountQuery).Append(") npoco_tbl");
-
         IContentCacheDataSerializer serializer =
             _contentCacheDataSerializerFactory.Create(ContentCacheDataSerializerEntityType.Document);
 
-        // We need to page here. We don't want to iterate over every single row in one connection cuz this can cause an SQL Timeout.
-        // We also want to read with a db reader and not load everything into memory, QueryPaged lets us do that.
-        foreach (ContentSourceDto row in Database.QueryPaged<ContentSourceDto>(_nucacheSettings.Value.SqlPageSize, sql, sqlCount))
+        IEnumerable<ContentSourceDto> dtos = GetContentNodeDtos(sql);
+
+        foreach (ContentSourceDto row in dtos)
         {
             yield return CreateContentNodeKit(row, serializer);
         }
@@ -246,19 +239,12 @@ AND cmsContentNu.nodeId IS NULL
             .Append(SqlWhereNodeIdX(SqlContext, id))
             .Append(SqlOrderByLevelIdSortOrder(SqlContext));
 
-        // Use a more efficient COUNT query
-        Sql<ISqlContext>? sqlCountQuery = SqlContentSourcesCount(SqlContentSourcesSelectUmbracoNodeJoin)
-            .Append(SqlObjectTypeNotTrashed(SqlContext, Constants.ObjectTypes.Document))
-            .Append(SqlWhereNodeIdX(SqlContext, id));
-        Sql<ISqlContext>? sqlCount =
-            SqlContext.Sql("SELECT COUNT(*) FROM (").Append(sqlCountQuery).Append(") npoco_tbl");
-
         IContentCacheDataSerializer serializer =
             _contentCacheDataSerializerFactory.Create(ContentCacheDataSerializerEntityType.Document);
 
-        // We need to page here. We don't want to iterate over every single row in one connection cuz this can cause an SQL Timeout.
-        // We also want to read with a db reader and not load everything into memory, QueryPaged lets us do that.
-        foreach (ContentSourceDto row in Database.QueryPaged<ContentSourceDto>(_nucacheSettings.Value.SqlPageSize, sql, sqlCount))
+        IEnumerable<ContentSourceDto> dtos = GetContentNodeDtos(sql);
+
+        foreach (ContentSourceDto row in dtos)
         {
             yield return CreateContentNodeKit(row, serializer);
         }
@@ -276,19 +262,12 @@ AND cmsContentNu.nodeId IS NULL
             .WhereIn<ContentDto>(x => x.ContentTypeId, ids)
             .Append(SqlOrderByLevelIdSortOrder(SqlContext));
 
-        // Use a more efficient COUNT query
-        Sql<ISqlContext> sqlCountQuery = SqlContentSourcesCount()
-            .Append(SqlObjectTypeNotTrashed(SqlContext, Constants.ObjectTypes.Document))
-            .WhereIn<ContentDto>(x => x.ContentTypeId, ids);
-        Sql<ISqlContext>? sqlCount =
-            SqlContext.Sql("SELECT COUNT(*) FROM (").Append(sqlCountQuery).Append(") npoco_tbl");
-
         IContentCacheDataSerializer serializer =
             _contentCacheDataSerializerFactory.Create(ContentCacheDataSerializerEntityType.Document);
 
-        // We need to page here. We don't want to iterate over every single row in one connection cuz this can cause an SQL Timeout.
-        // We also want to read with a db reader and not load everything into memory, QueryPaged lets us do that.
-        foreach (ContentSourceDto row in Database.QueryPaged<ContentSourceDto>(_nucacheSettings.Value.SqlPageSize, sql, sqlCount))
+        IEnumerable<ContentSourceDto> dtos = GetContentNodeDtos(sql);
+
+        foreach (ContentSourceDto row in dtos)
         {
             yield return CreateContentNodeKit(row, serializer);
         }
@@ -319,18 +298,12 @@ AND cmsContentNu.nodeId IS NULL
             .Append(SqlObjectTypeNotTrashed(SqlContext, Constants.ObjectTypes.Media))
             .Append(SqlOrderByLevelIdSortOrder(SqlContext));
 
-        // Use a more efficient COUNT query
-        Sql<ISqlContext>? sqlCountQuery = SqlMediaSourcesCount()
-            .Append(SqlObjectTypeNotTrashed(SqlContext, Constants.ObjectTypes.Media));
-        Sql<ISqlContext>? sqlCount =
-            SqlContext.Sql("SELECT COUNT(*) FROM (").Append(sqlCountQuery).Append(") npoco_tbl");
-
         IContentCacheDataSerializer serializer =
             _contentCacheDataSerializerFactory.Create(ContentCacheDataSerializerEntityType.Media);
 
-        // We need to page here. We don't want to iterate over every single row in one connection cuz this can cause an SQL Timeout.
-        // We also want to read with a db reader and not load everything into memory, QueryPaged lets us do that.
-        foreach (ContentSourceDto row in Database.QueryPaged<ContentSourceDto>(_nucacheSettings.Value.SqlPageSize, sql, sqlCount))
+        IEnumerable<ContentSourceDto> dtos = GetContentNodeDtos(sql);
+
+        foreach (ContentSourceDto row in dtos)
         {
             yield return CreateMediaNodeKit(row, serializer);
         }
@@ -343,19 +316,12 @@ AND cmsContentNu.nodeId IS NULL
             .Append(SqlWhereNodeIdX(SqlContext, id))
             .Append(SqlOrderByLevelIdSortOrder(SqlContext));
 
-        // Use a more efficient COUNT query
-        Sql<ISqlContext>? sqlCountQuery = SqlMediaSourcesCount(SqlContentSourcesSelectUmbracoNodeJoin)
-            .Append(SqlObjectTypeNotTrashed(SqlContext, Constants.ObjectTypes.Media))
-            .Append(SqlWhereNodeIdX(SqlContext, id));
-        Sql<ISqlContext>? sqlCount =
-            SqlContext.Sql("SELECT COUNT(*) FROM (").Append(sqlCountQuery).Append(") npoco_tbl");
-
         IContentCacheDataSerializer serializer =
             _contentCacheDataSerializerFactory.Create(ContentCacheDataSerializerEntityType.Media);
 
-        // We need to page here. We don't want to iterate over every single row in one connection cuz this can cause an SQL Timeout.
-        // We also want to read with a db reader and not load everything into memory, QueryPaged lets us do that.
-        foreach (ContentSourceDto row in Database.QueryPaged<ContentSourceDto>(_nucacheSettings.Value.SqlPageSize, sql, sqlCount))
+        IEnumerable<ContentSourceDto> dtos = GetContentNodeDtos(sql);
+
+        foreach (ContentSourceDto row in dtos)
         {
             yield return CreateMediaNodeKit(row, serializer);
         }
@@ -373,19 +339,12 @@ AND cmsContentNu.nodeId IS NULL
             .WhereIn<ContentDto>(x => x.ContentTypeId, ids)
             .Append(SqlOrderByLevelIdSortOrder(SqlContext));
 
-        // Use a more efficient COUNT query
-        Sql<ISqlContext> sqlCountQuery = SqlMediaSourcesCount()
-            .Append(SqlObjectTypeNotTrashed(SqlContext, Constants.ObjectTypes.Media))
-            .WhereIn<ContentDto>(x => x.ContentTypeId, ids);
-        Sql<ISqlContext>? sqlCount =
-            SqlContext.Sql("SELECT COUNT(*) FROM (").Append(sqlCountQuery).Append(") npoco_tbl");
-
         IContentCacheDataSerializer serializer =
             _contentCacheDataSerializerFactory.Create(ContentCacheDataSerializerEntityType.Media);
 
-        // We need to page here. We don't want to iterate over every single row in one connection cuz this can cause an SQL Timeout.
-        // We also want to read with a db reader and not load everything into memory, QueryPaged lets us do that.
-        foreach (ContentSourceDto row in Database.QueryPaged<ContentSourceDto>(_nucacheSettings.Value.SqlPageSize, sql, sqlCount))
+        IEnumerable<ContentSourceDto> dtos = GetContentNodeDtos(sql);
+
+        foreach (ContentSourceDto row in dtos)
         {
             yield return CreateMediaNodeKit(row, serializer);
         }
@@ -1029,5 +988,30 @@ WHERE cmsContentNu.nodeId IN (
         var s = new ContentNodeKit(n, dto.ContentTypeId, null, p);
 
         return s;
+    }
+
+    private IEnumerable<ContentSourceDto> GetContentNodeDtos(Sql<ISqlContext> sql)
+    {
+        // We need to page here. We don't want to iterate over every single row in one connection cuz this can cause an SQL Timeout.
+        // We also want to read with a db reader and not load everything into memory, QueryPaged lets us do that.
+        // QueryPaged is very slow on large sites however, so use fetch if UsePagedSqlQuery is disabled.
+        IEnumerable<ContentSourceDto> dtos;
+        if (_nucacheSettings.Value.UsePagedSqlQuery)
+        {
+            // Use a more efficient COUNT query
+            Sql<ISqlContext>? sqlCountQuery = SqlContentSourcesCount()
+                .Append(SqlObjectTypeNotTrashed(SqlContext, Constants.ObjectTypes.Document));
+
+            Sql<ISqlContext>? sqlCount =
+                SqlContext.Sql("SELECT COUNT(*) FROM (").Append(sqlCountQuery).Append(") npoco_tbl");
+
+            dtos = Database.QueryPaged<ContentSourceDto>(_nucacheSettings.Value.SqlPageSize, sql, sqlCount);
+        }
+        else
+        {
+            dtos = Database.Fetch<ContentSourceDto>(sql);
+        }
+
+        return dtos;
     }
 }

src/Umbraco.Core/Configuration/Models/NuCacheSettings.cs
@@ -14,6 +14,7 @@ public class NuCacheSettings
     internal const string StaticNuCacheSerializerType = "MessagePack";
     internal const int StaticSqlPageSize = 1000;
     internal const int StaticKitBatchSize = 1;
+    internal const bool StaticUsePagedSqlQuery = true;
 
     /// <summary>
     ///     Gets or sets a value defining the BTree block size.
@@ -40,4 +41,7 @@ public class NuCacheSettings
     public int KitBatchSize { get; set; } = StaticKitBatchSize;
 
     public bool UnPublishedContentCompression { get; set; } = false;
+
+    [DefaultValue(StaticUsePagedSqlQuery)]
+    public bool UsePagedSqlQuery { get; set; } = true;
 }

commit 2f64847930551defeb262ded2e6bf96520289927

Fixing regression bug introduced in #14806 and found during investigation of #16803 - well tested on db with 300k+ nodes

src/Umbraco.PublishedCache.NuCache/Persistence/NuCacheContentRepository.cs
@@ -221,10 +221,17 @@ AND cmsContentNu.nodeId IS NULL
             .Append(SqlObjectTypeNotTrashed(SqlContext, Constants.ObjectTypes.Document))
             .Append(SqlOrderByLevelIdSortOrder(SqlContext));
 
+        // Use a more efficient COUNT query
+        Sql<ISqlContext>? sqlCountQuery = SqlContentSourcesCount()
+            .Append(SqlObjectTypeNotTrashed(SqlContext, Constants.ObjectTypes.Document));
+
+        Sql<ISqlContext>? sqlCount =
+            SqlContext.Sql("SELECT COUNT(*) FROM (").Append(sqlCountQuery).Append(") npoco_tbl");
+
         IContentCacheDataSerializer serializer =
             _contentCacheDataSerializerFactory.Create(ContentCacheDataSerializerEntityType.Document);
 
-        IEnumerable<ContentSourceDto> dtos = GetContentNodeDtos(sql);
+        IEnumerable<ContentSourceDto> dtos = GetContentNodeDtos(sql, sqlCount);
 
         foreach (ContentSourceDto row in dtos)
         {
@@ -239,10 +246,18 @@ AND cmsContentNu.nodeId IS NULL
             .Append(SqlWhereNodeIdX(SqlContext, id))
             .Append(SqlOrderByLevelIdSortOrder(SqlContext));
 
+        // Use a more efficient COUNT query
+        Sql<ISqlContext>? sqlCountQuery = SqlContentSourcesCount(SqlContentSourcesSelectUmbracoNodeJoin)
+            .Append(SqlObjectTypeNotTrashed(SqlContext, Constants.ObjectTypes.Document))
+            .Append(SqlWhereNodeIdX(SqlContext, id));
+
+        Sql<ISqlContext>? sqlCount =
+            SqlContext.Sql("SELECT COUNT(*) FROM (").Append(sqlCountQuery).Append(") npoco_tbl");
+
         IContentCacheDataSerializer serializer =
             _contentCacheDataSerializerFactory.Create(ContentCacheDataSerializerEntityType.Document);
 
-        IEnumerable<ContentSourceDto> dtos = GetContentNodeDtos(sql);
+        IEnumerable<ContentSourceDto> dtos = GetContentNodeDtos(sql, sqlCount);
 
         foreach (ContentSourceDto row in dtos)
         {
@@ -262,10 +277,18 @@ AND cmsContentNu.nodeId IS NULL
             .WhereIn<ContentDto>(x => x.ContentTypeId, ids)
             .Append(SqlOrderByLevelIdSortOrder(SqlContext));
 
+        // Use a more efficient COUNT query
+        Sql<ISqlContext> sqlCountQuery = SqlContentSourcesCount()
+            .Append(SqlObjectTypeNotTrashed(SqlContext, Constants.ObjectTypes.Document))
+            .WhereIn<ContentDto>(x => x.ContentTypeId, ids);
+
+        Sql<ISqlContext>? sqlCount =
+            SqlContext.Sql("SELECT COUNT(*) FROM (").Append(sqlCountQuery).Append(") npoco_tbl");
+
         IContentCacheDataSerializer serializer =
             _contentCacheDataSerializerFactory.Create(ContentCacheDataSerializerEntityType.Document);
 
-        IEnumerable<ContentSourceDto> dtos = GetContentNodeDtos(sql);
+        IEnumerable<ContentSourceDto> dtos = GetContentNodeDtos(sql, sqlCount);
 
         foreach (ContentSourceDto row in dtos)
         {
@@ -1015,27 +1038,14 @@ WHERE cmsContentNu.nodeId IN (
         return dtos;
     }
 
-    private IEnumerable<ContentSourceDto> GetContentNodeDtos(Sql<ISqlContext> sql)
+    private IEnumerable<ContentSourceDto> GetContentNodeDtos(Sql<ISqlContext> sql, Sql<ISqlContext> sqlCount)
     {
         // We need to page here. We don't want to iterate over every single row in one connection cuz this can cause an SQL Timeout.
         // We also want to read with a db reader and not load everything into memory, QueryPaged lets us do that.
         // QueryPaged is very slow on large sites however, so use fetch if UsePagedSqlQuery is disabled.
-        IEnumerable<ContentSourceDto> dtos;
-        if (_nucacheSettings.Value.UsePagedSqlQuery)
-        {
-            // Use a more efficient COUNT query
-            Sql<ISqlContext>? sqlCountQuery = SqlContentSourcesCount()
-                .Append(SqlObjectTypeNotTrashed(SqlContext, Constants.ObjectTypes.Document));
-
-            Sql<ISqlContext>? sqlCount =
-                SqlContext.Sql("SELECT COUNT(*) FROM (").Append(sqlCountQuery).Append(") npoco_tbl");
-
-            dtos = Database.QueryPaged<ContentSourceDto>(_nucacheSettings.Value.SqlPageSize, sql, sqlCount);
-        }
-        else
-        {
-            dtos = Database.Fetch<ContentSourceDto>(sql);
-        }
+        IEnumerable<ContentSourceDto> dtos = _nucacheSettings.Value.UsePagedSqlQuery ?
+            Database.QueryPaged<ContentSourceDto>(_nucacheSettings.Value.SqlPageSize, sql, sqlCount) :
+            Database.Fetch<ContentSourceDto>(sql);
 
         return dtos;
     }