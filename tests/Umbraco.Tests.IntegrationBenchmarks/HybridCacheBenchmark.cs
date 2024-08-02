617ebf0c9934dab174b89266c9fcdd0d2051d1b9
Zeegaan <skrivdetud@gmail.com>
2024-08-02 11:01:07 +0200

    create new benchmark project

---
+++ tests/Umbraco.Tests.IntegrationBenchmarks/HybridCacheBenchmark.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Tests.Integration.Testing;
+using Umbraco.Cms.Tests.Integration.TestServerTest;
+using Umbraco.Cms.Web.Common.Controllers;
+
+namespace Umbraco.Tests.IntegrationBenchmarks;
+
+public class HybridCacheBenchmark : UmbracoTestServerTestBase
+{
+    [GlobalSetup]
+    public void BenchmarkSetup()
+    {
+        Setup();
+    }
+
+    [Benchmark]
+    public void GetCachedCanAssignNoFactory()
+    {
+        var url = PrepareApiControllerUrl<MyTestController>(x => x.GetWithHybridCache());
+        var response = Client.GetAsync(url).GetAwaiter().GetResult();
+    }
+}
+
+public class MyTestController : UmbracoApiController
+{
+    [HttpGet]
+    public string GetWithHybridCache() => "Hello, World!";
+}
