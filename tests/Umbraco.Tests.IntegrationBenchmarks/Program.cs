56dd31cdb8bf2292a3c16588dcdc59d16fd700d9
Sven Geusens <sge@umbraco.dk>
2024-08-02 14:53:00 +0200

    POC Integration benchmarks with custom api controllers

--- tests/Umbraco.Tests.IntegrationBenchmarks/Program.cs
+++ tests/Umbraco.Tests.IntegrationBenchmarks/Program.cs
@@ -1,8 +1,22 @@
+using BenchmarkDotNet.Configs;
 using BenchmarkDotNet.Running;
 
 namespace Umbraco.Tests.IntegrationBenchmarks;
 
 internal static class Program
 {
-    private static void Main(string[] args) => new BenchmarkSwitcher(typeof(Program).Assembly).Run(args);
+    private static void Main(string[] args) => new BenchmarkSwitcher(typeof(Program).Assembly).Run(args, SetupBenchMarkConfig());
+
+    //used to test the setup
+    // private static void Main(string[] args)
+    // {
+    //     var test = new HybridCacheBenchmark();
+    //     test.BenchmarkSetup();
+    //     test.GetCachedCanAssignNoFactory();
+    //     test.BenchmarkCleanup();
+    // }
+
+    private static IConfig SetupBenchMarkConfig() =>
+        ManualConfig.Create(DefaultConfig.Instance)
+            .WithOptions(ConfigOptions.DisableOptimizationsValidator);
 }
