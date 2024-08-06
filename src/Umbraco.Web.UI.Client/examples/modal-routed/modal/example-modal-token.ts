97d7943ba51f4408f2e51f538e8bd0febf5a93be
Niels Lyngsø <niels.lyngso@gmail.com>
2024-08-06 10:25:19 +0200

    prettier

--- src/Umbraco.Web.UI.Client/examples/modal-routed/modal/example-modal-token.ts
+++ src/Umbraco.Web.UI.Client/examples/modal-routed/modal/example-modal-token.ts
@@ -1,15 +1,14 @@
-import { UmbModalToken } from "@umbraco-cms/backoffice/modal";
+import { UmbModalToken } from '@umbraco-cms/backoffice/modal';
 
-export type Data = {}
-export type RetData = {}
+export type Data = object;
+export type RetData = object;
 
-export const EXAMPLE_ROUTED_MODAL = new UmbModalToken<
-Data,
-RetData
->('example.routed.modal', // this needs to match the alias of the modal registered in manifest.ts
-{
-	modal : {
-		type : 'dialog',
-    size : 'full'
-	}
-});
+export const EXAMPLE_ROUTED_MODAL = new UmbModalToken<Data, RetData>(
+	'example.routed.modal', // this needs to match the alias of the modal registered in manifest.ts
+	{
+		modal: {
+			type: 'dialog',
+			size: 'full',
+		},
+	},
+);
