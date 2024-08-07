1ee88661cf3d42c02c4e0f0d3f9c87f71c5aa879
Elitsa <elm@umbraco.dk>
2024-08-07 10:36:15 +0200

    Adding test for rebuilding the structure

--- tests/Umbraco.Tests.Integration/Umbraco.Core/Services/DocumentNavigationServiceTests.cs
+++ tests/Umbraco.Tests.Integration/Umbraco.Core/Services/DocumentNavigationServiceTests.cs
@@ -110,6 +110,50 @@ public class DocumentNavigationServiceTests : UmbracoIntegrationTest
     // }
 
     [Test]
+    public async Task Structure_Can_Rebuild()
+    {
+        // Arrange
+        Guid nodeKey = Root.Key;
+
+        // Capture original built state of DocumentNavigationService
+        DocumentNavigationService.TryGetParentKey(nodeKey, out Guid? originalParentKey);
+        DocumentNavigationService.TryGetChildrenKeys(nodeKey, out IEnumerable<Guid> originalChildrenKeys);
+        DocumentNavigationService.TryGetDescendantsKeys(nodeKey, out IEnumerable<Guid> originalDescendantsKeys);
+        DocumentNavigationService.TryGetAncestorsKeys(nodeKey, out IEnumerable<Guid> originalAncestorsKeys);
+        DocumentNavigationService.TryGetSiblingsKeys(nodeKey, out IEnumerable<Guid> originalSiblingsKeys);
+
+        // Im-memory navigation structure is empty here
+        var newDocumentNavigationService = new DocumentNavigationService(GetRequiredService<ICoreScopeProvider>(), GetRequiredService<INavigationRepository>());
+        var initialNodeExists = newDocumentNavigationService.TryGetParentKey(nodeKey, out _);
+
+        // Act
+        await newDocumentNavigationService.RebuildAsync();
+
+        // Capture rebuilt state
+        var nodeExists = newDocumentNavigationService.TryGetParentKey(nodeKey, out Guid? parentKeyFromRebuild);
+        newDocumentNavigationService.TryGetChildrenKeys(nodeKey, out IEnumerable<Guid> childrenKeysFromRebuild);
+        newDocumentNavigationService.TryGetDescendantsKeys(nodeKey, out IEnumerable<Guid> descendantsKeysFromRebuild);
+        newDocumentNavigationService.TryGetAncestorsKeys(nodeKey, out IEnumerable<Guid> ancestorsKeysFromRebuild);
+        newDocumentNavigationService.TryGetSiblingsKeys(nodeKey, out IEnumerable<Guid> siblingsKeysFromRebuild);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.IsFalse(initialNodeExists);
+
+            // Verify that the item is present in the navigation structure after a rebuild
+            Assert.IsTrue(nodeExists);
+
+            // Verify that we have the same items as in the original built state of DocumentNavigationService
+            Assert.AreEqual(originalParentKey, parentKeyFromRebuild);
+            CollectionAssert.AreEquivalent(originalChildrenKeys, childrenKeysFromRebuild);
+            CollectionAssert.AreEquivalent(originalDescendantsKeys, descendantsKeysFromRebuild);
+            CollectionAssert.AreEquivalent(originalAncestorsKeys, ancestorsKeysFromRebuild);
+            CollectionAssert.AreEquivalent(originalSiblingsKeys, siblingsKeysFromRebuild);
+        });
+    }
+
+    [Test]
     public async Task Structure_Does_Not_Update_When_Scope_Is_Not_Completed()
     {
         // Arrange
