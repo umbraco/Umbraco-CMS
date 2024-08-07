4916a301caa3eaa6ec6483e3b0f8c044657923e4
Elitsa <elm@umbraco.dk>
2024-08-07 10:30:17 +0200

    Initializing all 4 collections

--- src/Umbraco.Core/Services/Navigation/NavigationInitializationService.cs
+++ src/Umbraco.Core/Services/Navigation/NavigationInitializationService.cs
@@ -3,19 +3,34 @@ using Microsoft.Extensions.Hosting;
 namespace Umbraco.Cms.Core.Services.Navigation;
 
 /// <summary>
-///     Responsible for seeding the in-memory navigation structure at application's startup
-///     by rebuild the navigation structure.
+///     Responsible for seeding the in-memory navigation structures at application's startup
+///     by rebuild the navigation structures.
 /// </summary>
 public class NavigationInitializationService : IHostedLifecycleService
 {
-    private readonly INavigationService _navigationService;
+    private readonly IDocumentNavigationService _documentNavigationService;
+    private readonly IDocumentRecycleBinNavigationService _documentRecycleBinNavigationService;
+    private readonly IMediaNavigationService _mediaNavigationService;
+    private readonly IMediaRecycleBinNavigationService _mediaRecycleBinNavigationService;
 
-    public NavigationInitializationService(INavigationService navigationService)
-        => _navigationService = navigationService;
+    public NavigationInitializationService(
+        IDocumentNavigationService documentNavigationService,
+        IDocumentRecycleBinNavigationService documentRecycleBinNavigationService,
+        IMediaNavigationService mediaNavigationService,
+        IMediaRecycleBinNavigationService mediaRecycleBinNavigationService)
+    {
+        _documentNavigationService = documentNavigationService;
+        _documentRecycleBinNavigationService = documentRecycleBinNavigationService;
+        _mediaNavigationService = mediaNavigationService;
+        _mediaRecycleBinNavigationService = mediaRecycleBinNavigationService;
+    }
 
     public async Task StartingAsync(CancellationToken cancellationToken)
     {
-        await _navigationService.RebuildAsync();
+        await _documentNavigationService.RebuildAsync();
+        await _documentRecycleBinNavigationService.RebuildAsync();
+        await _mediaNavigationService.RebuildAsync();
+        await _mediaRecycleBinNavigationService.RebuildAsync();
     }
 
     public Task StartAsync(CancellationToken cancellationToken) => Task.CompletedTask;
