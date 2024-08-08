7344789e64ec80895e9fcc303677a190329135d9
Mads Rasmussen <madsr@hey.com>
2024-08-08 13:23:42 +0200

    use constant

--- src/Umbraco.Web.UI.Client/src/packages/core/property/conditions/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property/conditions/manifests.ts
@@ -1,10 +1,11 @@
+import { UMB_WRITABLE_PROPERTY_CONDITION_ALIAS } from './constants.js';
 import type { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
 
 export const manifests: Array<ManifestTypes> = [
 	{
 		type: 'condition',
 		name: 'Writable Property Condition',
-		alias: 'Umb.Condition.Property.Writable',
+		alias: UMB_WRITABLE_PROPERTY_CONDITION_ALIAS,
 		api: () => import('./writable-property.condition.js'),
 	},
 ];
