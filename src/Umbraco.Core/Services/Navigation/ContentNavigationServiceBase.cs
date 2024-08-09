daea8c4d920f970168db596e446e1960f87f1b24
Elitsa <elm@umbraco.dk>
2024-08-09 13:06:25 +0200

    Adding trashed items implementation in base - currently managing 2 structures

--- src/Umbraco.Core/Services/Navigation/ContentNavigationServiceBase.cs
+++ src/Umbraco.Core/Services/Navigation/ContentNavigationServiceBase.cs
@@ -10,6 +10,7 @@ internal abstract class ContentNavigationServiceBase
     private readonly ICoreScopeProvider _coreScopeProvider;
     private readonly INavigationRepository _navigationRepository;
     private ConcurrentDictionary<Guid, NavigationNode> _navigationStructure = new();
+    private ConcurrentDictionary<Guid, NavigationNode> _recycleBinNavigationStructure = new();
 
     protected ContentNavigationServiceBase(ICoreScopeProvider coreScopeProvider, INavigationRepository navigationRepository)
     {
@@ -20,97 +21,34 @@ internal abstract class ContentNavigationServiceBase
     public abstract Task RebuildAsync();
 
     public bool TryGetParentKey(Guid childKey, out Guid? parentKey)
-    {
-        if (_navigationStructure.TryGetValue(childKey, out NavigationNode? childNode))
-        {
-            parentKey = childNode.Parent?.Key;
-            return true;
-        }
-
-        // Child doesn't exist
-        parentKey = null;
-        return false;
-    }
+        => TryGetParentKeyFromStructure(_navigationStructure, childKey, out parentKey);
 
     public bool TryGetChildrenKeys(Guid parentKey, out IEnumerable<Guid> childrenKeys)
-    {
-        if (_navigationStructure.TryGetValue(parentKey, out NavigationNode? parentNode) is false)
-        {
-            // Parent doesn't exist
-            childrenKeys = [];
-            return false;
-        }
-
-        childrenKeys = parentNode.Children.Select(child => child.Key);
-        return true;
-    }
+        => TryGetChildrenKeysFromStructure(_navigationStructure, parentKey, out childrenKeys);
 
     public bool TryGetDescendantsKeys(Guid parentKey, out IEnumerable<Guid> descendantsKeys)
-    {
-        var descendants = new List<Guid>();
-
-        if (_navigationStructure.TryGetValue(parentKey, out NavigationNode? parentNode) is false)
-        {
-            // Parent doesn't exist
-            descendantsKeys = [];
-            return false;
-        }
-
-        GetDescendantsRecursively(parentNode, descendants);
-
-        descendantsKeys = descendants;
-        return true;
-    }
+        => TryGetDescendantsKeysFromStructure(_navigationStructure, parentKey, out descendantsKeys);
 
     public bool TryGetAncestorsKeys(Guid childKey, out IEnumerable<Guid> ancestorsKeys)
-    {
-        var ancestors = new List<Guid>();
-
-        if (_navigationStructure.TryGetValue(childKey, out NavigationNode? childNode) is false)
-        {
-            // Child doesn't exist
-            ancestorsKeys = [];
-            return false;
-        }
-
-        while (childNode?.Parent is not null)
-        {
-            ancestors.Add(childNode.Parent.Key);
-            childNode = childNode.Parent;
-        }
-
-        ancestorsKeys = ancestors;
-        return true;
-    }
+        => TryGetAncestorsKeysFromStructure(_navigationStructure, childKey, out ancestorsKeys);
 
     public bool TryGetSiblingsKeys(Guid key, out IEnumerable<Guid> siblingsKeys)
-    {
-        siblingsKeys = [];
+        => TryGetSiblingsKeysFromStructure(_navigationStructure, key, out siblingsKeys);
 
-        if (_navigationStructure.TryGetValue(key, out NavigationNode? node) is false)
-        {
-            return false; // Node doesn't exist
-        }
+    public bool TryGetParentKeyInBin(Guid childKey, out Guid? parentKey)
+        => TryGetParentKeyFromStructure(_recycleBinNavigationStructure, childKey, out parentKey);
 
-        if (node.Parent is null)
-        {
-            // To find siblings of a node at root level, we need to iterate over all items and add those with null Parent
-            siblingsKeys = _navigationStructure
-                .Where(kv => kv.Value.Parent is null && kv.Key != key)
-                .Select(kv => kv.Key)
-                .ToList();
-            return true;
-        }
+    public bool TryGetChildrenKeysInBin(Guid parentKey, out IEnumerable<Guid> childrenKeys)
+        => TryGetChildrenKeysFromStructure(_recycleBinNavigationStructure, parentKey, out childrenKeys);
 
-        if (TryGetChildrenKeys(node.Parent.Key, out IEnumerable<Guid> childrenKeys) is false)
-        {
-            return false; // Couldn't retrieve children keys
-        }
+    public bool TryGetDescendantsKeysInBin(Guid parentKey, out IEnumerable<Guid> descendantsKeys)
+        => TryGetDescendantsKeysFromStructure(_recycleBinNavigationStructure, parentKey, out descendantsKeys);
 
-        // Filter out the node itself to get its siblings
-        siblingsKeys = childrenKeys.Where(childKey => childKey != key).ToList();
-        return true;
-    }
+    public bool TryGetAncestorsKeysInBin(Guid childKey, out IEnumerable<Guid> ancestorsKeys)
+        => TryGetAncestorsKeysFromStructure(_recycleBinNavigationStructure, childKey, out ancestorsKeys);
+
+    public bool TryGetSiblingsKeysInBin(Guid key, out IEnumerable<Guid> siblingsKeys)
+        => TryGetSiblingsKeysFromStructure(_recycleBinNavigationStructure, key, out siblingsKeys);
 
     public NavigationNode? GetNavigationNode(Guid key)
         => _navigationStructure.GetValueOrDefault(key);
@@ -144,31 +82,20 @@ internal abstract class ContentNavigationServiceBase
 
     public bool Remove(Guid key)
     {
-        if (_navigationStructure.TryGetValue(key, out NavigationNode? nodeToRemove) is false)
+        if (TryRemoveNodeFromParentInStructure(_navigationStructure, key, out NavigationNode? nodeToRemove) is false || nodeToRemove is null)
         {
             return false; // Node doesn't exist
         }
 
-        // Remove the node from its parent's children list
-        if (nodeToRemove.Parent is not null && _navigationStructure.TryGetValue(nodeToRemove.Parent.Key, out NavigationNode? parentNode))
-        {
-            parentNode.RemoveChild(nodeToRemove);
-        }
+        // Recursively remove all descendants and add them to recycle bin
+        AddDescendantsToRecycleBinRecursively(nodeToRemove);
 
-        // Recursively remove all descendants
-        RemoveDescendantsRecursively(nodeToRemove);
-
-        // Remove the node itself
-        return _navigationStructure.TryRemove(key, out _);
+        return _recycleBinNavigationStructure.TryAdd(nodeToRemove.Key, nodeToRemove) &&
+               _navigationStructure.TryRemove(key, out _);
     }
 
     public bool Add(Guid key, Guid? parentKey = null)
     {
-        if (_navigationStructure.ContainsKey(key))
-        {
-            return false; // Node with this key already exists
-        }
-
         NavigationNode? parentNode = null;
         if (parentKey.HasValue)
         {
@@ -179,21 +106,24 @@ internal abstract class ContentNavigationServiceBase
         }
 
         var newNode = new NavigationNode(key);
-        _navigationStructure[key] = newNode;
+        if (_navigationStructure.TryAdd(key, newNode) is false)
+        {
+            return false; // Node with this key already exists
+        }
 
         parentNode?.AddChild(newNode);
 
         return true;
     }
 
-    public bool Move(Guid nodeKey, Guid? targetParentKey = null)
+    public bool Move(Guid key, Guid? targetParentKey = null)
     {
-        if (_navigationStructure.TryGetValue(nodeKey, out NavigationNode? nodeToMove) is false)
+        if (_navigationStructure.TryGetValue(key, out NavigationNode? nodeToMove) is false)
         {
             return false; // Node doesn't exist
         }
 
-        if (nodeKey == targetParentKey)
+        if (key == targetParentKey)
         {
             return false; // Cannot move a node to itself
         }
@@ -210,18 +140,46 @@ internal abstract class ContentNavigationServiceBase
             currentParentNode.RemoveChild(nodeToMove);
         }
 
-        // Create a new node with the same key, to update the parent
-        var newNode = new NavigationNode(nodeToMove.Key);
-
         // Set the new parent for the node (if parent node is null - the node is moved to root)
-        targetParentNode?.AddChild(newNode);
-
-        // Copy children to the new node
-        CopyChildren(nodeToMove, newNode);
-
-        _navigationStructure[nodeToMove.Key] = newNode;
+        targetParentNode?.AddChild(nodeToMove);
 
         return true;
+    }
+
+    public bool RemoveFromBin(Guid key)
+    {
+        if (TryRemoveNodeFromParentInStructure(_recycleBinNavigationStructure, key, out NavigationNode? nodeToRemove) is false || nodeToRemove is null)
+        {
+            return false; // Node doesn't exist
+        }
+
+        RemoveDescendantsRecursively(nodeToRemove);
+
+        return _recycleBinNavigationStructure.TryRemove(key, out _);
+    }
+
+    public bool RestoreFromBin(Guid key, Guid? targetParentKey = null)
+    {
+        if (_recycleBinNavigationStructure.TryGetValue(key, out NavigationNode? nodeToRestore) is false)
+        {
+            return false; // Node doesn't exist
+        }
+
+        // If a target parent is specified, try to find it in the main structure
+        NavigationNode? targetParentNode = null;
+        if (targetParentKey.HasValue && _navigationStructure.TryGetValue(targetParentKey.Value, out targetParentNode) is false)
+        {
+            return false; // Target parent doesn't exist
+        }
+
+        // Set the new parent for the node (if parent node is null - the node is moved to root)
+        targetParentNode?.AddChild(nodeToRestore);
+
+        // Restore the node and its descendants from the recycle bin to the main structure
+        RestoreNodeAndDescendantsRecursively(nodeToRestore);
+
+        return _navigationStructure.TryAdd(nodeToRestore.Key, nodeToRestore) &&
+               _recycleBinNavigationStructure.TryRemove(key, out _);
     }
 
     /// <summary>
@@ -242,9 +200,107 @@ internal abstract class ContentNavigationServiceBase
         using ICoreScope scope = _coreScopeProvider.CreateCoreScope(autoComplete: true);
         scope.ReadLock(readLock);
 
-        _navigationStructure = trashed
-            ? _navigationRepository.GetTrashedContentNodesByObjectType(objectTypeKey)
-            : _navigationRepository.GetContentNodesByObjectType(objectTypeKey);
+        if (trashed)
+        {
+            _recycleBinNavigationStructure = _navigationRepository.GetTrashedContentNodesByObjectType(objectTypeKey);
+        }
+        else
+        {
+            _navigationStructure = _navigationRepository.GetContentNodesByObjectType(objectTypeKey);
+        }
+    }
+
+    private bool TryGetParentKeyFromStructure(ConcurrentDictionary<Guid, NavigationNode> structure, Guid childKey, out Guid? parentKey)
+    {
+        if (structure.TryGetValue(childKey, out NavigationNode? childNode))
+        {
+            parentKey = childNode.Parent?.Key;
+            return true;
+        }
+
+        // Child doesn't exist
+        parentKey = null;
+        return false;
+    }
+
+    private bool TryGetChildrenKeysFromStructure(ConcurrentDictionary<Guid, NavigationNode> structure, Guid parentKey, out IEnumerable<Guid> childrenKeys)
+    {
+        if (structure.TryGetValue(parentKey, out NavigationNode? parentNode) is false)
+        {
+            // Parent doesn't exist
+            childrenKeys = [];
+            return false;
+        }
+
+        childrenKeys = parentNode.Children.Select(child => child.Key);
+        return true;
+    }
+
+    private bool TryGetDescendantsKeysFromStructure(ConcurrentDictionary<Guid, NavigationNode> structure, Guid parentKey, out IEnumerable<Guid> descendantsKeys)
+    {
+        var descendants = new List<Guid>();
+
+        if (structure.TryGetValue(parentKey, out NavigationNode? parentNode) is false)
+        {
+            // Parent doesn't exist
+            descendantsKeys = [];
+            return false;
+        }
+
+        GetDescendantsRecursively(parentNode, descendants);
+
+        descendantsKeys = descendants;
+        return true;
+    }
+
+    private bool TryGetAncestorsKeysFromStructure(ConcurrentDictionary<Guid, NavigationNode> structure, Guid childKey, out IEnumerable<Guid> ancestorsKeys)
+    {
+        var ancestors = new List<Guid>();
+
+        if (structure.TryGetValue(childKey, out NavigationNode? childNode) is false)
+        {
+            // Child doesn't exist
+            ancestorsKeys = [];
+            return false;
+        }
+
+        while (childNode?.Parent is not null)
+        {
+            ancestors.Add(childNode.Parent.Key);
+            childNode = childNode.Parent;
+        }
+
+        ancestorsKeys = ancestors;
+        return true;
+    }
+
+    private bool TryGetSiblingsKeysFromStructure(ConcurrentDictionary<Guid, NavigationNode> structure, Guid key, out IEnumerable<Guid> siblingsKeys)
+    {
+        siblingsKeys = [];
+
+        if (structure.TryGetValue(key, out NavigationNode? node) is false)
+        {
+            return false; // Node doesn't exist
+        }
+
+        if (node.Parent is null)
+        {
+            // To find siblings of a node at root level, we need to iterate over all items and add those with null Parent
+            siblingsKeys = structure
+                .Where(kv => kv.Value.Parent is null && kv.Key != key)
+                .Select(kv => kv.Key)
+                .ToList();
+            return true;
+        }
+
+        if (TryGetChildrenKeys(node.Parent.Key, out IEnumerable<Guid> childrenKeys) is false)
+        {
+            return false; // Couldn't retrieve children keys
+        }
+
+        // Filter out the node itself to get its siblings
+        siblingsKeys = childrenKeys.Where(childKey => childKey != key).ToList();
+        return true;
     }
 
     private void GetDescendantsRecursively(NavigationNode node, List<Guid> descendants)
@@ -256,12 +312,42 @@ internal abstract class ContentNavigationServiceBase
         }
     }
 
+    private bool TryRemoveNodeFromParentInStructure(ConcurrentDictionary<Guid, NavigationNode> structure, Guid key, out NavigationNode? nodeToRemove)
+    {
+        if (structure.TryGetValue(key, out nodeToRemove) is false)
+        {
+            return false; // Node doesn't exist
+        }
+
+        // Remove the node from its parent's children list
+        if (nodeToRemove.Parent is not null && structure.TryGetValue(nodeToRemove.Parent.Key, out NavigationNode? parentNode))
+        {
+            parentNode.RemoveChild(nodeToRemove);
+        }
+
+        return true;
+    }
+
+    private void AddDescendantsToRecycleBinRecursively(NavigationNode node)
+    {
+        foreach (NavigationNode child in node.Children)
+        {
+            AddDescendantsToRecycleBinRecursively(child);
+
+            // Only remove the child from the main structure if it was successfully added to the recycle bin
+            if (_recycleBinNavigationStructure.TryAdd(child.Key, child))
+            {
+                _navigationStructure.TryRemove(child.Key, out _);
+            }
+        }
+    }
+
     private void RemoveDescendantsRecursively(NavigationNode node)
     {
         foreach (NavigationNode child in node.Children)
         {
             RemoveDescendantsRecursively(child);
-            _navigationStructure.TryRemove(child.Key, out _);
+            _recycleBinNavigationStructure.TryRemove(child.Key, out _);
         }
     }
 
