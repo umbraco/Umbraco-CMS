b2493a6136b372de6c5da476f5972765ceb72580
Elitsa <elm@umbraco.dk>
2024-08-09 14:16:02 +0200

    Fixing initialization with correct interfaces

--- src/Umbraco.Core/Services/Navigation/NavigationInitializationService.cs
+++ src/Umbraco.Core/Services/Navigation/NavigationInitializationService.cs
@@ -9,30 +9,20 @@ namespace Umbraco.Cms.Core.Services.Navigation;
 public class NavigationInitializationService : IHostedLifecycleService
 {
     private readonly IDocumentNavigationService _documentNavigationService;
-    private readonly IDocumentRecycleBinNavigationService _documentRecycleBinNavigationService;
     private readonly IMediaNavigationService _mediaNavigationService;
-    private readonly IMediaRecycleBinNavigationService _mediaRecycleBinNavigationService;
 
-    public NavigationInitializationService(
-        IDocumentNavigationService documentNavigationService,
-        IDocumentRecycleBinNavigationService documentRecycleBinNavigationService,
-        IMediaNavigationService mediaNavigationService,
-        IMediaRecycleBinNavigationService mediaRecycleBinNavigationService)
+    public NavigationInitializationService(IDocumentNavigationService documentNavigationService, IMediaNavigationService mediaNavigationService)
     {
         _documentNavigationService = documentNavigationService;
-        _documentRecycleBinNavigationService = documentRecycleBinNavigationService;
         _mediaNavigationService = mediaNavigationService;
-        _mediaRecycleBinNavigationService = mediaRecycleBinNavigationService;
     }
 
     public async Task StartingAsync(CancellationToken cancellationToken)
     {
         await _documentNavigationService.RebuildAsync();
         await _documentNavigationService.RebuildBinAsync();
-
-        await _documentRecycleBinNavigationService.RebuildAsync();
         await _mediaNavigationService.RebuildAsync();
-        await _mediaRecycleBinNavigationService.RebuildAsync();
+        await _mediaNavigationService.RebuildBinAsync();
     }
 
     public Task StartAsync(CancellationToken cancellationToken) => Task.CompletedTask;
