bc8b1020fc25ca5e279a7a5043c5289d1c10075c
Elitsa <elm@umbraco.dk>
2024-08-12 15:00:52 +0200

    Clean up IMediaNavigationService

--- src/Umbraco.Core/Services/Navigation/IMediaNavigationService.cs
+++ src/Umbraco.Core/Services/Navigation/IMediaNavigationService.cs
@@ -2,5 +2,4 @@ namespace Umbraco.Cms.Core.Services.Navigation;
 
 public interface IMediaNavigationService : INavigationQueryService, INavigationManagementService
 {
-    bool Move(Guid nodeKey, Guid? targetParentKey = null);
 }
