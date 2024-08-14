68e371725e9b0af722c47406a6c3d48d5d0b1c23
Mads Rasmussen <madsr@hey.com>
2024-08-14 21:28:47 +0200

    temp config

--- src/Umbraco.Web.UI.Client/src/packages/core/picker/picker-modal.context.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/picker/picker-modal.context.ts
@@ -1,12 +1,19 @@
 import { UMB_PICKER_MODAL_CONTEXT } from './picker-modal.context.token.js';
+import { UmbPickerModalSearchManager } from './search/manager/picker-modal-search.manager.js';
 import { UmbContextBase } from '@umbraco-cms/backoffice/class-api';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import { UmbSelectionManager } from '@umbraco-cms/backoffice/utils';
 
 export class UmbPickerModalContext extends UmbContextBase<UmbPickerModalContext> {
 	public readonly selection = new UmbSelectionManager(this);
+	public readonly search = new UmbPickerModalSearchManager(this);
 
 	constructor(host: UmbControllerHost) {
 		super(host, UMB_PICKER_MODAL_CONTEXT);
+
+		// TODO: temp hardcoded search provider
+		this.search.setConfig({
+			providerAlias: 'Umb.SearchProvider.Document',
+		});
 	}
 }
