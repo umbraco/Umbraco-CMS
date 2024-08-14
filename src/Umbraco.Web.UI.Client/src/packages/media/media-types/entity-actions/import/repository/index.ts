9493d25634f9cb872d0f0688a018af7509fc16f9
Lone Iversen <108085781+loivsen@users.noreply.github.com>
2024-08-14 14:25:22 +0200

    naming

--- src/Umbraco.Web.UI.Client/src/packages/media/media-types/entity-actions/import/repository/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media-types/entity-actions/import/repository/index.ts
@@ -1,2 +1,2 @@
-export { UmbDocumentTypeImportRepository } from './media-type-import.repository.js';
-export { UMB_DOCUMENT_TYPE_IMPORT_REPOSITORY_ALIAS } from './constants.js';
+export { UmbMediaTypeImportRepository } from './media-type-import.repository.js';
+export { UMB_MEDIA_TYPE_IMPORT_REPOSITORY_ALIAS } from './constants.js';
--- src/Umbraco.Web.UI.Client/src/packages/media/media-types/entity-actions/import/repository/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media-types/entity-actions/import/repository/manifests.ts
@@ -1,10 +1,10 @@
-import { UMB_DOCUMENT_TYPE_IMPORT_REPOSITORY_ALIAS } from './constants.js';
+import { UMB_MEDIA_TYPE_IMPORT_REPOSITORY_ALIAS } from './constants.js';
 import type { ManifestRepository, ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
 
 const importRepository: ManifestRepository = {
 	type: 'repository',
-	alias: UMB_DOCUMENT_TYPE_IMPORT_REPOSITORY_ALIAS,
-	name: 'Import Document Type Repository',
+	alias: UMB_MEDIA_TYPE_IMPORT_REPOSITORY_ALIAS,
+	name: 'Import Media Type Repository',
 	api: () => import('./media-type-import.repository.js'),
 };
 
