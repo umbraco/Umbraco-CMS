532980e2c338a3e77985f6f525dc0d665980a300
Mads Rasmussen <madsr@hey.com>
2024-08-14 21:27:47 +0200

    add picker modal search manager

---
+++ src/Umbraco.Web.UI.Client/src/packages/core/picker/search/manager/picker-modal-search.manager.ts
@@ -0,0 +1,117 @@
+import type { UmbPickerModalSearchManagerConfig } from './types.js';
+import { UmbControllerBase } from '@umbraco-cms/backoffice/class-api';
+import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
+import type { UmbEntityModel } from '@umbraco-cms/backoffice/entity';
+import { createExtensionApiByAlias } from '@umbraco-cms/backoffice/extension-registry';
+import { UmbArrayState, UmbBooleanState, UmbObjectState } from '@umbraco-cms/backoffice/observable-api';
+import type { UmbSearchProvider, UmbSearchRequestArgs } from '@umbraco-cms/backoffice/search';
+import { debounce } from '@umbraco-cms/backoffice/utils';
+
+/**
+ * A manager for searching items in a picker modal.
+ * @exports
+ * @class UmbPickerModalSearchManager
+ * @augments {UmbControllerBase}
+ * @template ResultItemType
+ * @template QueryType
+ */
+export class UmbPickerModalSearchManager<
+	ResultItemType extends UmbEntityModel = UmbEntityModel,
+	QueryType extends UmbSearchRequestArgs = UmbSearchRequestArgs,
+> extends UmbControllerBase {
+	#query = new UmbObjectState<QueryType | undefined>(undefined);
+	public readonly query = this.#query.asObservable();
+
+	#searching = new UmbBooleanState(false);
+	public readonly searching = this.#searching.asObservable();
+
+	#resultItems = new UmbArrayState<ResultItemType>([], (x) => x.unique);
+	public readonly resultItems = this.#resultItems.asObservable();
+
+	#config?: UmbPickerModalSearchManagerConfig;
+	// TODO: lower requirement for search provider item type
+	#searchProvider?: UmbSearchProvider<any, QueryType>;
+
+	/**
+	 * Creates an instance of UmbPickerModalSearchManager.
+	 * @param {UmbControllerHost} host The controller host for the search manager.
+	 * @memberof UmbPickerModalSearchManager
+	 */
+	constructor(host: UmbControllerHost) {
+		super(host);
+	}
+
+	/**
+	 * Set the configuration for the search manager.
+	 * @param {UmbPickerModalSearchManagerConfig} config The configuration for the search manager.
+	 * @memberof UmbPickerModalSearchManager
+	 */
+	public setConfig(config: UmbPickerModalSearchManagerConfig) {
+		this.#config = config;
+		this.#initSearch();
+	}
+
+	/**
+	 * Get the current configuration for the search manager.
+	 * @returns {UmbPickerModalSearchManagerConfig | undefined} The current configuration for the search manager.
+	 * @memberof UmbPickerModalSearchManager
+	 */
+	public getConfig(): UmbPickerModalSearchManagerConfig | undefined {
+		return this.#config;
+	}
+
+	/**
+	 * Search for items based on the current query.
+	 * @memberof UmbPickerModalSearchManager
+	 */
+	public search = debounce(this.#debouncedSearch, 300);
+
+	/**
+	 * Clear the current search query and result items.
+	 * @memberof UmbPickerModalSearchManager
+	 */
+	public clear() {
+		this.#query.setValue(undefined);
+		this.#resultItems.setValue([]);
+		this.#searching.setValue(false);
+	}
+
+	/**
+	 * Set the search query.
+	 * @param {QueryType} query The search query.
+	 * @memberof UmbPickerModalSearchManager
+	 */
+	public setQuery(query: QueryType) {
+		if (!this.query) {
+			this.clear();
+			return;
+		}
+
+		this.#query.setValue(query);
+	}
+
+	/**
+	 * Get the current search query.
+	 * @memberof UmbPickerModalSearchManager
+	 * @returns {QueryType | undefined} The current search query.
+	 */
+	public getQuery(): QueryType | undefined {
+		return this.#query.getValue();
+	}
+
+	async #initSearch() {
+		const providerAlias = this.#config?.providerAlias;
+		if (!providerAlias) throw new Error('No search provider alias provided');
+		this.#searchProvider = await createExtensionApiByAlias<UmbSearchProvider>(this, providerAlias);
+		if (!this.#searchProvider) throw new Error(`Search Provider with alias ${providerAlias} is not available`);
+	}
+
+	async #debouncedSearch() {
+		if (!this.#searchProvider) throw new Error('Search provider is not available');
+		const query = this.#query.getValue();
+		if (!query) throw new Error('No query provided');
+		const { data } = await this.#searchProvider.search(query);
+		this.#resultItems.setValue(data?.items ?? []);
+		this.#searching.setValue(false);
+	}
+}
