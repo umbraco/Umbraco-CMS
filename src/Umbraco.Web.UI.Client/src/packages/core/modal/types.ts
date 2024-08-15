7f4e08c3a95096878d18709258f3ebcf4008f149
Mads Rasmussen <madsr@hey.com>
2024-08-15 13:40:54 +0200

    add optional search setting to picker modal data

--- src/Umbraco.Web.UI.Client/src/packages/core/modal/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/modal/types.ts
@@ -2,7 +2,13 @@ export interface UmbPickerModalData<ItemType> {
 	multiple?: boolean;
 	filter?: (item: ItemType) => boolean;
 	pickableFilter?: (item: ItemType) => boolean;
+	search?: UmbPickerModalSearchData;
 }
+
+export interface UmbPickerModalSearchData {
+	providerAlias: string;
+}
+
 export interface UmbPickerModalValue {
 	selection: Array<string | null>;
 }
