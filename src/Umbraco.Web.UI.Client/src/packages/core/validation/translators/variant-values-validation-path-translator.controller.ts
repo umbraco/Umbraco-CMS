7f0a0133bbfc3714a7f540baacee0e51fc1b690c
Niels Lyngsø <niels.lyngso@gmail.com>
2024-08-16 13:42:14 +0200

    refactor translators

--- src/Umbraco.Web.UI.Client/src/packages/core/validation/translators/variant-values-validation-path-translator.controller.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/validation/translators/variant-values-validation-path-translator.controller.ts
@@ -1,34 +1,14 @@
-import { UmbDataPathPropertyValueFilter } from '../utils/index.js';
-import { UmbValidationPathTranslatorBase } from './validation-path-translator-base.controller.js';
+import { UmbAbstractArrayValidationPathTranslator } from './abstract-array-path-translator.controller.js';
+import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 
-const InitialPathToMatch = '$.values[';
-const InitialPathToMatchLength = InitialPathToMatch.length;
+export class UmbVariantValuesValidationPathTranslator extends UmbAbstractArrayValidationPathTranslator {
+	constructor(host: UmbControllerHost) {
+		super(host, '$.values[');
+	}
 
-export class UmbVariantValuesValidationPathTranslator extends UmbValidationPathTranslatorBase {
-	translate(path: string) {
+	getDataFromIndex(index: number) {
 		if (!this._context) return;
-		if (path.indexOf(InitialPathToMatch) !== 0) {
-			// We do not handle this path.
-			return false;
-		}
-		const pathEnd = path.indexOf(']');
-		if (pathEnd === -1) {
-			// We do not handle this path.
-			return false;
-		}
-		// retrieve the number from the message values index: [NL]
-		const index = parseInt(path.substring(InitialPathToMatchLength, pathEnd));
-
-		if (isNaN(index)) {
-			// index is not a number, this means its not a path we want to translate. [NL]
-			return false;
-		}
-
-		// Get the data from the validation request, the context holds that for us: [NL]
 		const data = this._context.getTranslationData();
-
-		const specificValue = data.values[index];
-		// replace the values[ number ] with JSON-Path filter values[@.(...)], continues by the rest of the path:
-		return InitialPathToMatch + UmbDataPathPropertyValueFilter(specificValue) + path.substring(path.indexOf(']'));
+		return data.values[index];
 	}
 }
