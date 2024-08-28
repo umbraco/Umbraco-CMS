5d8d5a0675195da4dd9628a042e6570887841cca
Andreas Zerbst <andr317c@live.dk>
2024-08-28 13:07:43 +0200

    Added Media tests

--- tests/Umbraco.Tests.Integration/Umbraco.Core/Services/MediaNavigationServiceTests.Create.cs
+++ tests/Umbraco.Tests.Integration/Umbraco.Core/Services/MediaNavigationServiceTests.Create.cs
@@ -5,5 +5,53 @@ namespace Umbraco.Cms.Tests.Integration.Umbraco.Core.Services;
 
 public partial class MediaNavigationServiceTests
 {
+    // TODO: I am unsure if there are issues with the CreatedMediaCreateModel, when I try to create a ImageMediaType at root, I get validation errors.
+    [Test]
+    public async Task Structure_Updates_When_Creating_Media_At_Root()
+    {
+        // Arrange
+        MediaNavigationQueryService.TryGetSiblingsKeys(Album.Key, out IEnumerable<Guid> initialSiblingsKeys);
+        var initialRootNodeSiblingsCount = initialSiblingsKeys.Count();
+        var createModel = CreateMediaCreateModel("Root Image", Guid.NewGuid(), ImageMediaType.Key);
 
+        // Act
+        // UNSURE,the createAttempt has a propertyValidationError, but it is still able to create the media
+        var createAttempt = await MediaEditingService.CreateAsync(createModel, Constants.Security.SuperUserKey);
+        Guid createdItemKey = createAttempt.Result.Content!.Key;
+        // Verify that the structure has updated by checking the siblings list of the Root once again
+        MediaNavigationQueryService.TryGetSiblingsKeys(Album.Key, out IEnumerable<Guid> updatedSiblingsKeys);
+        List<Guid> siblingsList = updatedSiblingsKeys.ToList();
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.IsNotEmpty(siblingsList);
+            Assert.AreEqual(initialRootNodeSiblingsCount + 1, siblingsList.Count);
+            Assert.AreEqual(createdItemKey, siblingsList.First());
+        });
+    }
+
+    [Test]
+    public async Task Structure_Updates_When_Creating_Child_Media()
+    {
+        // Arrange
+        MediaNavigationQueryService.TryGetChildrenKeys(Album.Key, out IEnumerable<Guid> initialChildrenKeys);
+        var initialChild1ChildrenCount = initialChildrenKeys.Count();
+        var createModel = CreateMediaCreateModel("Child Image", Guid.NewGuid(), ImageMediaType.Key, Album.Key);
+
+        // Act
+        var createAttempt = await MediaEditingService.CreateAsync(createModel, Constants.Security.SuperUserKey);
+        Guid createdItemKey = createAttempt.Result.Content!.Key;
+        // Verify that the structure has updated by checking the children of the Child1 node once again
+        MediaNavigationQueryService.TryGetChildrenKeys(Album.Key, out IEnumerable<Guid> updatedChildrenKeys);
+        List<Guid> childrenList = updatedChildrenKeys.ToList();
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.IsNotEmpty(childrenList);
+            Assert.AreEqual(initialChild1ChildrenCount + 1, childrenList.Count);
+            Assert.IsTrue(childrenList.Contains(createdItemKey));
+        });
+    }
 }
--- tests/Umbraco.Tests.Integration/Umbraco.Core/Services/MediaNavigationServiceTests.Rebuild.cs
+++ tests/Umbraco.Tests.Integration/Umbraco.Core/Services/MediaNavigationServiceTests.Rebuild.cs
@@ -1,4 +1,5 @@
 using NUnit.Framework;
+using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Persistence.Repositories;
 using Umbraco.Cms.Core.Scoping;
 using Umbraco.Cms.Core.Services.Navigation;
@@ -12,21 +13,18 @@ public partial class MediaNavigationServiceTests
     {
         // Arrange
         Guid nodeKey = Album.Key;
-
         // Capture original built state of MediaNavigationService
         MediaNavigationQueryService.TryGetParentKey(nodeKey, out Guid? originalParentKey);
         MediaNavigationQueryService.TryGetChildrenKeys(nodeKey, out IEnumerable<Guid> originalChildrenKeys);
         MediaNavigationQueryService.TryGetDescendantsKeys(nodeKey, out IEnumerable<Guid> originalDescendantsKeys);
         MediaNavigationQueryService.TryGetAncestorsKeys(nodeKey, out IEnumerable<Guid> originalAncestorsKeys);
         MediaNavigationQueryService.TryGetSiblingsKeys(nodeKey, out IEnumerable<Guid> originalSiblingsKeys);
-
         // Im-memory navigation structure is empty here
         var newMediaNavigationService = new MediaNavigationService(GetRequiredService<ICoreScopeProvider>(), GetRequiredService<INavigationRepository>());
         var initialNodeExists = newMediaNavigationService.TryGetParentKey(nodeKey, out _);
 
         // Act
         await newMediaNavigationService.RebuildAsync();
-
         // Capture rebuilt state
         var nodeExists = newMediaNavigationService.TryGetParentKey(nodeKey, out Guid? parentKeyFromRebuild);
         newMediaNavigationService.TryGetChildrenKeys(nodeKey, out IEnumerable<Guid> childrenKeysFromRebuild);
@@ -52,8 +50,44 @@ public partial class MediaNavigationServiceTests
     }
 
     [Test]
-    // TODO: Test that you can rebuild bin structure as well
     public async Task Bin_Structure_Can_Rebuild()
     {
+         // Arrange
+        Guid nodeKey = Album.Key;
+        await MediaEditingService.MoveToRecycleBinAsync(nodeKey, Constants.Security.SuperUserKey);
+        // Capture original built state of MediaNavigationService
+        MediaNavigationQueryService.TryGetParentKeyInBin(nodeKey, out Guid? originalParentKey);
+        MediaNavigationQueryService.TryGetChildrenKeysInBin(nodeKey, out IEnumerable<Guid> originalChildrenKeys);
+        MediaNavigationQueryService.TryGetDescendantsKeysInBin(nodeKey, out IEnumerable<Guid> originalDescendantsKeys);
+        MediaNavigationQueryService.TryGetAncestorsKeysInBin(nodeKey, out IEnumerable<Guid> originalAncestorsKeys);
+        MediaNavigationQueryService.TryGetSiblingsKeysInBin(nodeKey, out IEnumerable<Guid> originalSiblingsKeys);
+        // Im-memory navigation structure is empty here
+        var newMediaNavigationService = new MediaNavigationService(GetRequiredService<ICoreScopeProvider>(), GetRequiredService<INavigationRepository>());
+        var initialNodeExists = newMediaNavigationService.TryGetParentKeyInBin(nodeKey, out _);
+
+        // Act
+        await newMediaNavigationService.RebuildBinAsync();
+        // Capture rebuilt state
+        var nodeExists = newMediaNavigationService.TryGetParentKeyInBin(nodeKey, out Guid? parentKeyFromRebuild);
+        newMediaNavigationService.TryGetChildrenKeysInBin(nodeKey, out IEnumerable<Guid> childrenKeysFromRebuild);
+        newMediaNavigationService.TryGetDescendantsKeysInBin(nodeKey, out IEnumerable<Guid> descendantsKeysFromRebuild);
+        newMediaNavigationService.TryGetAncestorsKeysInBin(nodeKey, out IEnumerable<Guid> ancestorsKeysFromRebuild);
+        newMediaNavigationService.TryGetSiblingsKeysInBin(nodeKey, out IEnumerable<Guid> siblingsKeysFromRebuild);
+
+        // Assert
+        Assert.Multiple(() =>
+        {
+            Assert.IsFalse(initialNodeExists);
+
+            // Verify that the item is present in the navigation structure after a rebuild
+            Assert.IsTrue(nodeExists);
+
+            // Verify that we have the same items as in the original built state of MediaNavigationService
+            Assert.AreEqual(originalParentKey, parentKeyFromRebuild);
+            CollectionAssert.AreEquivalent(originalChildrenKeys, childrenKeysFromRebuild);
+            CollectionAssert.AreEquivalent(originalDescendantsKeys, descendantsKeysFromRebuild);
+            CollectionAssert.AreEquivalent(originalAncestorsKeys, ancestorsKeysFromRebuild);
+            CollectionAssert.AreEquivalent(originalSiblingsKeys, siblingsKeysFromRebuild);
+        });
     }
 }
