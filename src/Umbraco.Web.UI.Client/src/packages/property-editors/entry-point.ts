e25641534a8f106205108d12a49ba6191ee4edd9
Warren Buckley <warren@hackmakedo.com>
2024-08-29 16:14:06 +0000

    Adds _whenExtensionAliasIsRegistered as a promise so that we basically wait until the manifest is registered so late extensions will work as well
    Seems to work with my hack in an entrypoint and the console logs the extension with the updated condition but its the tests I am struggling to get to work.
    
    As this code is heavily worked from me trying to Google, read RXJS docs and talking with Copilot I am not fully in confident in this approach

--- src/Umbraco.Web.UI.Client/src/packages/property-editors/entry-point.ts
+++ src/Umbraco.Web.UI.Client/src/packages/property-editors/entry-point.ts
@@ -1,2 +1,33 @@
+import type { ManifestWithDynamicConditions, UmbConditionConfigBase, UmbEntryPointOnInit } from '@umbraco-cms/backoffice/extension-api';
+import type { WorkspaceAliasConditionConfig } from '@umbraco-cms/backoffice/workspace';
+
 import './checkbox-list/components/index.js';
 import './content-picker/components/index.js';
+
+export const onInit: UmbEntryPointOnInit = (_host, _extensionRegistry) => {
+
+	console.log('HELLO AGAIN');
+
+	const condition: UmbConditionConfigBase = {
+		alias: 'Umb.Condition.WorkspaceAlias',
+		match: 'Umb.Workspace.WARRENYO',
+	} as WorkspaceAliasConditionConfig;
+
+	_extensionRegistry.appendCondition('Umb.Dashboard.UmbracoNewsLATE', condition);
+
+	const ext:ManifestWithDynamicConditions = {
+		alias: 'Umb.Dashboard.UmbracoNewsLATE',
+		type: 'dashboard',
+		name: 'WARREN Package',
+		weight: 100,
+		conditions: [
+			{
+				alias: 'Umb.Condition.WorkspaceAlias',
+				match: 'Umb.Workspace.LATE-COMER-EXISTING',
+			} as WorkspaceAliasConditionConfig,
+		],
+	};
+
+	_extensionRegistry.register(ext);
+
+};
