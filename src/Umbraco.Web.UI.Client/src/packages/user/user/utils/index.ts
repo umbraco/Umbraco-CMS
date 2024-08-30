4d64a5de19609d473716ae32229ae375454da6e5
Mads Rasmussen <madsr@hey.com>
2024-08-30 22:26:13 +0200

    add utils for user model kind

--- src/Umbraco.Web.UI.Client/src/packages/user/user/utils/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/utils/index.ts
@@ -1,1 +1,8 @@
 export * from './is-user.function.js';
+
+export type UmbUserModelKindType = 'Default' | 'Api';
+
+export const UmbUserModelKind = Object.freeze({
+	DEFAULT: 'Default',
+	API: 'Api',
+});
