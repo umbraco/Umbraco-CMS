ee6aaee315f2ea26533e08f04f7af7f257d2ad7b
Andreas Zerbst <andr317c@live.dk>
2024-09-03 14:49:33 +0200

    Added tests

---
+++ tests/Umbraco.Tests.Integration/Umbraco.Core/Cache/PublishedContentTypeCacheTests.cs
@@ -0,0 +1,10 @@
+﻿namespace Umbraco.Cms.Tests.Integration.Umbraco.Core.Cache;
+
+public class PublishedContentTypeCacheTests
+{
+
+// Integration tests på om IPublishedCOntentTypeCache bliver opdateret CRUD
+
+// Create a ContentTypeEdtingBuilder
+// Inspiration: https://github.com/umbraco/Umbraco-CMS/pull/16938/commits/e45fa106840d7b88ed5ea095c29571e24275e925
+}
