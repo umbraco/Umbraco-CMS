42e4bdaf94856daec5e08385e123b9f837fef244
Mads Rasmussen <madsr@hey.com>
2024-09-03 10:53:25 +0200

    set modal types

--- src/Umbraco.Web.UI.Client/src/packages/user/user/client-credential/create/modal/create-user-client-credential-modal.token.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/client-credential/create/modal/create-user-client-credential-modal.token.ts
@@ -14,12 +14,12 @@ export interface UmbCreateUserClientCredentialModalValue {
 	};
 }
 
-export const UMB_CREATE_USER_CLIENT_CREDENTIAL_MODAL = new UmbModalToken(
-	UMB_CREATE_USER_CLIENT_CREDENTIAL_MODAL_ALIAS,
-	{
-		modal: {
-			type: 'dialog',
-			size: 'small',
-		},
+export const UMB_CREATE_USER_CLIENT_CREDENTIAL_MODAL = new UmbModalToken<
+	UmbCreateUserClientCredentialModalData,
+	UmbCreateUserClientCredentialModalValue
+>(UMB_CREATE_USER_CLIENT_CREDENTIAL_MODAL_ALIAS, {
+	modal: {
+		type: 'dialog',
+		size: 'small',
 	},
-);
+});
