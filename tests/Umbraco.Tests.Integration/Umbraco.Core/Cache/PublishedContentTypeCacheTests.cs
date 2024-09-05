9d80212790ecb0cda352828b47b4d04a1d9bd079
Andreas Zerbst <andr317c@live.dk>
2024-09-05 08:34:21 +0200

    Commented out to test

--- tests/Umbraco.Tests.Integration/Umbraco.Core/Cache/PublishedContentTypeCacheTests.cs
+++ tests/Umbraco.Tests.Integration/Umbraco.Core/Cache/PublishedContentTypeCacheTests.cs
@@ -1,6 +1,10 @@
 ﻿using NUnit.Framework;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Models.ContentTypeEditing;
 using Umbraco.Cms.Core.Models.PublishedContent;
 using Umbraco.Cms.Core.PublishedCache;
+using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Core.Services.ContentTypeEditing;
 using Umbraco.Cms.Tests.Common.Builders;
 using Umbraco.Cms.Tests.Common.Testing;
@@ -10,11 +14,13 @@ namespace Umbraco.Cms.Tests.Integration.Umbraco.Core.Cache;
 
 [TestFixture]
 [UmbracoTest(Database = UmbracoTestOptions.Database.NewSchemaPerTest)]
-public class PublishedContentTypeCacheTests : UmbracoIntegrationTestWithContentEditing
+public class PublishedContentTypeCacheTests : UmbracoIntegrationTest
 {
     // Integration tests på om IPublishedCOntentTypeCache bliver opdateret CRUD
 
     // Create a ContentTypeEdtingBuilder
+    protected ContentTypeCreateModel ContentType { get; private set; }
+
 
     // Inspiration: https://github.com/umbraco/Umbraco-CMS/pull/16938/commits/e45fa106840d7b88ed5ea095c29571e24275e925
     protected override void CustomTestSetup(IUmbracoBuilder builder) => builder.AddUmbracoHybridCache();
@@ -29,36 +35,53 @@ public class PublishedContentTypeCacheTests : UmbracoIntegrationTestWithContentE
     public async Task Can_Get_Draft_Content_By_Id()
     {
         //Act
-        ContentTypeEditingService
-        var textPage = await PublishedContentTypeCache.Get(TextpageId, true);
+        ContentTypeEditingBuilder contentTypeBuilder = new ContentTypeEditingBuilder();
+        // contentTypeBuilder.createBasicContentType();
+        //ContentTypeEditingService
 
-        var newContentType = ContentTypeBuilder.CreateBasicContentType();
-        newContentType.Key = ContentType.Key;
-        newContentType.Id = ContentType.Id;
-        newContentType.Alias = ContentType.Alias;
-        newContentType.DefaultTemplateId = ContentType.DefaultTemplateId;
-        ContentTypeService.Save(newContentType);
+        //var textPage = await PublishedContentTypeCache.Get(TextpageId, true);
+
+        // var newContentType = ContentTypeBuilder.CreateBasicContentType();
+        // newContentType.Key = ContentType.Key;
+        // newContentType.Id = ContentType.Id;
+        // newContentType.Alias = ContentType.Alias;
+        // newContentType.DefaultTemplateId = ContentType.DefaultTemplateId;
+        // ContentTypeService.Save(newContentType);
 
         // Assert
-        var newTextPage = await PublishedContentHybridCache.GetByIdAsync(TextpageId, true);
-        Assert.IsNull(newTextPage.Value("title"));
+        //var newTextPage = await PublishedContentHybridCache.GetByIdAsync(TextpageId, true);
+       // Assert.IsNull(newTextPage.Value("title"));
     }
+    //
+    // [Test]
+    // public async Task Can_Get_Draft_Content_By_Key()
+    // {
+    //     // Act
+    //     var textPage = await PublishedContentHybridCache.GetByIdAsync(Textpage.Key.Value, true);
+    //
+    //     var newContentType = ContentTypeBuilder.CreateBasicContentType();
+    //     newContentType.Key = ContentType.Key;
+    //     newContentType.Id = ContentType.Id;
+    //     newContentType.Alias = ContentType.Alias;
+    //     newContentType.DefaultTemplateId = ContentType.DefaultTemplateId;
+    //     ContentTypeService.Save(newContentType);
+    //
+    //     //Assert
+    //     var newTextPage = await PublishedContentHybridCache.GetByIdAsync(Textpage.Key.Value, true);
+    //     Assert.IsNull(newTextPage.Value("title"));
+    // }
 
-    [Test]
-    public async Task Can_Get_Draft_Content_By_Key()
+
+     [SetUp]
+    public new void Setup() => CreateTestData();
+
+    protected async void CreateTestData()
     {
-        // Act
-        var textPage = await PublishedContentHybridCache.GetByIdAsync(Textpage.Key.Value, true);
+        ContentType =
+            ContentTypeEditingBuilder.CreateBasicContentType("umbTextpage", "Textpage");
 
-        var newContentType = ContentTypeBuilder.CreateBasicContentType();
-        newContentType.Key = ContentType.Key;
-        newContentType.Id = ContentType.Id;
-        newContentType.Alias = ContentType.Alias;
-        newContentType.DefaultTemplateId = ContentType.DefaultTemplateId;
-        ContentTypeService.Save(newContentType);
+        var contentTypeResult = await ContentTypeEditingService.CreateAsync(ContentType, Constants.Security.SuperUserKey);
+        Assert.IsTrue(contentTypeResult.Success);
 
-        //Assert
-        var newTextPage = await PublishedContentHybridCache.GetByIdAsync(Textpage.Key.Value, true);
-        Assert.IsNull(newTextPage.Value("title"));
     }
 }
