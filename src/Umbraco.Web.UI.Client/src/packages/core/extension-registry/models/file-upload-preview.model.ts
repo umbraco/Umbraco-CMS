d03c7289d7c70270661c21be76d31076dc16eb35
Lone Iversen <108085781+loivsen@users.noreply.github.com>
2024-09-09 12:39:06 +0200

    jdoc example

--- src/Umbraco.Web.UI.Client/src/packages/core/extension-registry/models/file-upload-preview.model.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/extension-registry/models/file-upload-preview.model.ts
@@ -3,5 +3,9 @@ import type { ManifestElement } from '@umbraco-cms/backoffice/extension-api';
 
 export interface ManifestFileUploadPreview extends ManifestElement<UmbFileUploadPreviewElement> {
 	type: 'fileUploadPreview';
+	/**
+	 * Array of the mime types that are supported by this extension.
+	 * @example ["image/png", "image/jpeg", "image/*"]
+	 */
 	forMimeTypes: Array<string>;
 }
