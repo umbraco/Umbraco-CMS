a6236313c96b2d76f364f658d7aee12d3c737643
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-09-09 18:15:11 +0200

    feat: add handler for troubleshooting

--- src/Umbraco.Web.UI.Client/src/mocks/handlers/server.handlers.ts
+++ src/Umbraco.Web.UI.Client/src/mocks/handlers/server.handlers.ts
@@ -3,7 +3,11 @@ import type {
 	ServerStatusResponseModel,
 	ServerInformationResponseModel,
 } from '@umbraco-cms/backoffice/external/backend-api';
-import { RuntimeLevelModel, RuntimeModeModel } from '@umbraco-cms/backoffice/external/backend-api';
+import {
+	RuntimeLevelModel,
+	RuntimeModeModel,
+	ServerTroubleshootingResponseModel,
+} from '@umbraco-cms/backoffice/external/backend-api';
 import { umbracoPath } from '@umbraco-cms/backoffice/utils';
 
 export const serverRunningHandler = rest.get(umbracoPath('/server/status'), (_req, res, ctx) => {
@@ -36,15 +40,30 @@ export const serverMustUpgradeHandler = rest.get(umbracoPath('/server/status'), 
 	);
 });
 
-export const serverInformationHandler = rest.get(umbracoPath('/server/information'), (_req, res, ctx) => {
-	return res(
-		// Respond with a 200 status code
-		ctx.status(200),
-		ctx.json<ServerInformationResponseModel>({
-			version: '14.0.0-preview004',
-			assemblyVersion: '14.0.0-preview004',
-			baseUtcOffset: '01:00:00',
-			runtimeMode: RuntimeModeModel.BACKOFFICE_DEVELOPMENT,
-		}),
-	);
-});
+export const serverInformationHandlers = [
+	rest.get(umbracoPath('/server/information'), (_req, res, ctx) => {
+		return res(
+			// Respond with a 200 status code
+			ctx.status(200),
+			ctx.json<ServerInformationResponseModel>({
+				version: '14.0.0-preview004',
+				assemblyVersion: '14.0.0-preview004',
+				baseUtcOffset: '01:00:00',
+				runtimeMode: RuntimeModeModel.BACKOFFICE_DEVELOPMENT,
+			}),
+		);
+	}),
+	rest.get(umbracoPath('/server/troubleshooting'), (_req, res, ctx) => {
+		return res(
+			// Respond with a 200 status code
+			ctx.status(200),
+			ctx.json<ServerTroubleshootingResponseModel>({
+				items: [
+					{ name: 'Umbraco base url', data: location.origin },
+					{ name: 'Mocked server', data: 'true' },
+					{ name: 'Umbraco version', data: '14.0.0-preview004' },
+				],
+			}),
+		);
+	}),
+];
