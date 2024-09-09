0e613cc545cce1e648808034acb3f53d1c2c380a
Mads Rasmussen <madsr@hey.com>
2024-09-09 16:35:58 +0200

    Update ApiRequestOptions.ts

--- src/Umbraco.Web.UI.Client/src/external/backend-api/src/core/ApiRequestOptions.ts
+++ src/Umbraco.Web.UI.Client/src/external/backend-api/src/core/ApiRequestOptions.ts
@@ -1,21 +1,14 @@
 export type ApiRequestOptions<T = unknown> = {
-	readonly body?: any;
-	readonly cookies?: Record<string, unknown>;
-	readonly errors?: Record<number | string, string>;
-	readonly formData?: Record<string, unknown> | any[] | Blob | File;
-	readonly headers?: Record<string, unknown>;
-	readonly mediaType?: string;
-	readonly method:
-		| 'DELETE'
-		| 'GET'
-		| 'HEAD'
-		| 'OPTIONS'
-		| 'PATCH'
-		| 'POST'
-		| 'PUT';
+	readonly method: 'GET' | 'PUT' | 'POST' | 'DELETE' | 'OPTIONS' | 'HEAD' | 'PATCH';
+	readonly url: string;
 	readonly path?: Record<string, unknown>;
+	readonly cookies?: Record<string, unknown>;
+	readonly headers?: Record<string, unknown>;
 	readonly query?: Record<string, unknown>;
+	readonly formData?: Record<string, unknown>;
+	readonly body?: any;
+	readonly mediaType?: string;
 	readonly responseHeader?: string;
 	readonly responseTransformer?: (data: unknown) => Promise<T>;
-	readonly url: string;
+	readonly errors?: Record<number | string, string>;
 };
