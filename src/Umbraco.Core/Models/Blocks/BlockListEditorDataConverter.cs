e04f855b230c262da32631030fa76d87d93f6f83
kjac <kja@umbraco.dk>
2024-09-09 06:47:32 +0200

    Don't use deprecated constructor

--- src/Umbraco.Core/Models/Blocks/BlockListEditorDataConverter.cs
+++ src/Umbraco.Core/Models/Blocks/BlockListEditorDataConverter.cs
@@ -19,7 +19,7 @@ public class BlockListEditorDataConverter : BlockEditorDataConverter<BlockListVa
     }
 
     public BlockListEditorDataConverter(IJsonSerializer jsonSerializer)
-        : base(Constants.PropertyEditors.Aliases.BlockList, jsonSerializer)
+        : base(jsonSerializer)
     {
     }
 
