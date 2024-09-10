2b20f0f9981b5c47880bf19bfaaa365063243a5e
kjac <kja@umbraco.dk>
2024-09-10 08:33:48 +0200

    Add EditorAlias to content value outputs

--- src/Umbraco.Cms.Api.Management/ViewModels/Content/ContentCollectionResponseModelBase.cs
+++ src/Umbraco.Cms.Api.Management/ViewModels/Content/ContentCollectionResponseModelBase.cs
@@ -4,7 +4,7 @@ namespace Umbraco.Cms.Api.Management.ViewModels.Content;
 
 public abstract class ContentCollectionResponseModelBase<TValueResponseModelBase, TVariantResponseModel>
     : ContentResponseModelBase<TValueResponseModelBase, TVariantResponseModel>
-    where TValueResponseModelBase : ValueModelBase
+    where TValueResponseModelBase : ValueResponseModelBase
     where TVariantResponseModel : VariantResponseModelBase
 {
     public string? Creator { get; set; }
