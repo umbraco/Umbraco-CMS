7e35937d0d0b41665175f20a28bbae8461c5f936
Mads Rasmussen <madsr@hey.com>
2024-09-11 15:40:37 +0200

    fix path

--- src/Umbraco.Web.UI.Client/src/packages/user/user/workspace/user-root/user-root-workspace.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/workspace/user-root/user-root-workspace.element.ts
@@ -1,4 +1,4 @@
-import { UMB_USER_COLLECTION_ALIAS } from '../collection/index.js';
+import { UMB_USER_COLLECTION_ALIAS } from '../../collection/index.js';
 import { html, customElement } from '@umbraco-cms/backoffice/external/lit';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
 
