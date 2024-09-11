1c3161c26dcc92b51313bf465a36954ef92f1d71
Mads Rasmussen <madsr@hey.com>
2024-09-11 15:40:31 +0200

    add missing type

--- src/Umbraco.Web.UI.Client/src/packages/user/user/workspace/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/workspace/manifests.ts
@@ -1,5 +1,5 @@
 import { manifests as userManifests } from './user/manifests.js';
 import { manifests as userRootManifests } from './user-root/manifests.js';
-import type { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
+import type { ManifestTypes, UmbBackofficeManifestKind } from '@umbraco-cms/backoffice/extension-registry';
 
-export const manifests: Array<ManifestTypes> = [...userManifests, ...userRootManifests];
+export const manifests: Array<ManifestTypes | UmbBackofficeManifestKind> = [...userManifests, ...userRootManifests];
