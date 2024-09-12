50ee26b73fe0b4f27608cb1480dd77b38b2ade10
Niels Lyngsø <niels.lyngso@gmail.com>
2024-09-12 19:50:00 +0200

    use UmbManifestTypes in extension registry

--- src/Umbraco.Web.UI.Client/src/packages/core/extension-registry/registry.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/extension-registry/registry.ts
@@ -1,8 +1,7 @@
-import type { ManifestTypes } from './models/index.js';
 import type { ManifestKind } from '@umbraco-cms/backoffice/extension-api';
 import { UmbExtensionRegistry } from '@umbraco-cms/backoffice/extension-api';
 
-export type UmbBackofficeManifestKind = ManifestKind<ManifestTypes>;
-export type UmbBackofficeExtensionRegistry = UmbExtensionRegistry<ManifestTypes>;
+export type UmbBackofficeManifestKind = ManifestKind<UmbManifestTypes>;
+export type UmbBackofficeExtensionRegistry = UmbExtensionRegistry<UmbManifestTypes>;
 
-export const umbExtensionsRegistry = new UmbExtensionRegistry<ManifestTypes>() as UmbBackofficeExtensionRegistry;
+export const umbExtensionsRegistry = new UmbExtensionRegistry<UmbManifestTypes>() as UmbBackofficeExtensionRegistry;
