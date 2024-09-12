11fb2973f3401d08a8fb4f7839747bb794d28060
Nhu Dinh <hnd@umbraco.dk>
2024-09-12 16:15:59 +0700

    Added tests for Allow At Root property

---
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Content/ContentWithDocumentTypeProperties/ContentWithAllowAtRoot.spec.ts
@@ -0,0 +1,28 @@
+﻿import {ConstantHelper, test} from '@umbraco/playwright-testhelpers';
+
+const documentTypeName = 'TestDocumentTypeForContent';
+
+test.beforeEach(async ({umbracoApi, umbracoUi}) => {
+  await umbracoApi.documentType.ensureNameNotExists(documentTypeName);
+  await umbracoUi.goToBackOffice();
+});
+
+test.afterEach(async ({umbracoApi}) => {
+  await umbracoApi.documentType.ensureNameNotExists(documentTypeName);
+});
+
+test('cannot create any content if allow at root is not enabled', async ({umbracoApi, umbracoUi}) => {
+  // Arrange
+  const noAllowedDocumentTypeAvailableMessage = 'There are no allowed Document Types available for creating content here';
+  await umbracoApi.documentType.createDefaultDocumentType(documentTypeName);
+  await umbracoUi.content.goToSection(ConstantHelper.sections.content);
+
+  // Act
+  await umbracoUi.content.clickActionsMenuAtRoot();
+  await umbracoUi.content.clickCreateButton();
+
+  // Assert
+  await umbracoUi.content.isDocumentTypeNameVisible(documentTypeName, false);
+  await umbracoUi.content.doesModalHaveText(noAllowedDocumentTypeAvailableMessage);
+});
+
