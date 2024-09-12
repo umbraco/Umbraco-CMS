51a9e7f5926441cf2fb6c7278201ad0bc3310113
Ronald Barendse <ronald@barend.se>
2024-09-12 10:34:27 +0200

    Remove CollectionExtensions

--- src/Umbraco.Core/Extensions/CollectionExtensions.cs
+++
@@ -1,8 +0,0 @@
-namespace Umbraco.Cms.Core.Extensions;
-
-public static class CollectionExtensions
-{
-    // Easiest way to return a collection with 1 item, probably not the most performant
-    public static ICollection<T> ToSingleItemCollection<T>(this T item) =>
-        new T[] { item };
-}
