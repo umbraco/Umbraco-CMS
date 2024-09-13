a3b619592880a9dc3d1cf5c3ea18bdaae1417e45
Mads Rasmussen <madsr@hey.com>
2024-09-13 07:31:17 +0200

    add manifest files

---
+++ src/Umbraco.Web.UI.Client/src/packages/members/member/workspace/manifests.ts
@@ -0,0 +1,4 @@
+import { manifests as memberManifests } from './member/manifests.js';
+import type { ManifestTypes, UmbBackofficeManifestKind } from '@umbraco-cms/backoffice/extension-registry';
+
+export const manifests: Array<ManifestTypes | UmbBackofficeManifestKind> = [...memberManifests];
