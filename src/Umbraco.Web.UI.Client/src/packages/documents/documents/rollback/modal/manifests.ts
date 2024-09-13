aadf581a144df6ec3edd99e25b2a4b57260459be
Mads Rasmussen <madsr@hey.com>
2024-09-13 17:07:09 +0200

    Update manifests.ts

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/rollback/modal/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/rollback/modal/manifests.ts
@@ -1,6 +1,7 @@
 import { UMB_ROLLBACK_MODAL_ALIAS } from './constants.js';
+import type { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
 
-export const manifests: Array<UmbExtensionManifest> = [
+export const manifests: Array<ManifestTypes> = [
 	{
 		type: 'modal',
 		alias: UMB_ROLLBACK_MODAL_ALIAS,
