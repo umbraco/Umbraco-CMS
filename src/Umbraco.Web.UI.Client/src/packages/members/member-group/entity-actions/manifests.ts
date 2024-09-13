7048a1f5555f2e915531ac0362268fc0cbc0be7a
Mads Rasmussen <madsr@hey.com>
2024-09-13 21:41:46 +0200

    register create member group action

--- src/Umbraco.Web.UI.Client/src/packages/members/member-group/entity-actions/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/members/member-group/entity-actions/manifests.ts
@@ -2,10 +2,23 @@ import {
 	UMB_MEMBER_GROUP_DETAIL_REPOSITORY_ALIAS,
 	UMB_MEMBER_GROUP_ITEM_REPOSITORY_ALIAS,
 } from '../repository/index.js';
-import { UMB_MEMBER_GROUP_ENTITY_TYPE } from '../entity.js';
+import { UMB_MEMBER_GROUP_ENTITY_TYPE, UMB_MEMBER_GROUP_ROOT_ENTITY_TYPE } from '../entity.js';
 import type { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
 
-const entityActions: Array<ManifestTypes> = [
+export const manifests: Array<ManifestTypes> = [
+	{
+		type: 'entityAction',
+		kind: 'default',
+		alias: 'Umb.EntityAction.MemberGroup.Create',
+		name: 'Create Member Group Entity Action',
+		weight: 1200,
+		api: () => import('./create-member-group.action.js'),
+		forEntityTypes: [UMB_MEMBER_GROUP_ROOT_ENTITY_TYPE],
+		meta: {
+			icon: 'icon-add',
+			label: '#actions_create',
+		},
+	},
 	{
 		type: 'entityAction',
 		kind: 'delete',
@@ -18,5 +31,3 @@ const entityActions: Array<ManifestTypes> = [
 		},
 	},
 ];
-
-export const manifests: Array<ManifestTypes> = [...entityActions];
