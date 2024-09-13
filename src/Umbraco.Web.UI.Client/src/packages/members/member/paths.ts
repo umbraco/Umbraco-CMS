f1f12d4ee30cf4d8a5875ed8598d00d0669a116e
Mads Rasmussen <madsr@hey.com>
2024-09-13 12:15:30 +0200

    add paths

---
+++ src/Umbraco.Web.UI.Client/src/packages/members/member/paths.ts
@@ -0,0 +1,13 @@
+import { UMB_MEMBER_MANAGEMENT_SECTION_PATHNAME } from '../section/paths.js';
+import { UMB_MEMBER_ENTITY_TYPE, UMB_MEMBER_ROOT_ENTITY_TYPE } from './entity.js';
+import { UMB_WORKSPACE_PATH_PATTERN } from '@umbraco-cms/backoffice/workspace';
+
+export const UMB_MEMBER_WORKSPACE_PATH = UMB_WORKSPACE_PATH_PATTERN.generateAbsolute({
+	sectionName: UMB_MEMBER_MANAGEMENT_SECTION_PATHNAME,
+	entityType: UMB_MEMBER_ENTITY_TYPE,
+});
+
+export const UMB_MEMBER_ROOT_WORKSPACE_PATH = UMB_WORKSPACE_PATH_PATTERN.generateAbsolute({
+	sectionName: UMB_MEMBER_MANAGEMENT_SECTION_PATHNAME,
+	entityType: UMB_MEMBER_ROOT_ENTITY_TYPE,
+});
