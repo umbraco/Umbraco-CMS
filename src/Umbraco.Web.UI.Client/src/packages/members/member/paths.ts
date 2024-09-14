18bcbe0e9b4068340ed867ea16be83a1073beaa5
Mads Rasmussen <madsr@hey.com>
2024-09-14 21:22:58 +0200

    add create member entity action

--- src/Umbraco.Web.UI.Client/src/packages/members/member/paths.ts
+++ src/Umbraco.Web.UI.Client/src/packages/members/member/paths.ts
@@ -1,3 +1,4 @@
+import { UmbPathPattern } from '@umbraco-cms/backoffice/router';
 import { UMB_MEMBER_MANAGEMENT_SECTION_PATHNAME } from '../section/paths.js';
 import { UMB_MEMBER_ENTITY_TYPE, UMB_MEMBER_ROOT_ENTITY_TYPE } from './entity.js';
 import { UMB_WORKSPACE_PATH_PATTERN } from '@umbraco-cms/backoffice/workspace';
@@ -11,3 +12,7 @@ export const UMB_MEMBER_ROOT_WORKSPACE_PATH = UMB_WORKSPACE_PATH_PATTERN.generat
 	sectionName: UMB_MEMBER_MANAGEMENT_SECTION_PATHNAME,
 	entityType: UMB_MEMBER_ROOT_ENTITY_TYPE,
 });
+
+export const UMB_CREATE_MEMBER_WORKSPACE_PATH_PATTERN = new UmbPathPattern<{
+	memberTypeUnique: string;
+}>('create/:memberTypeUnique', UMB_MEMBER_WORKSPACE_PATH);
