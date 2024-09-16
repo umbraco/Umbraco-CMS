29dce93a5d5b29adb66dbfe5a0cfa3ac86ada5e7
JesmoDev <26099018+JesmoDev@users.noreply.github.com>
2024-09-16 21:42:14 +0200

    custom toolbar config

--- src/Umbraco.Web.UI.Client/src/packages/rte/tiptap/property-editors/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/rte/tiptap/property-editors/manifests.ts
@@ -1,4 +1,17 @@
 import { manifests as tiptapManifests } from './tiptap/manifests.js';
 import type { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
 
-export const manifests: Array<ManifestTypes> = [...tiptapManifests];
+export const manifests: Array<ManifestTypes> = [
+	...tiptapManifests,
+	{
+		type: 'propertyEditorUi',
+		alias: 'Umb.PropertyEditorUi.Tiptap.ToolbarConfiguration',
+		name: 'Tiptap Toolbar Property Editor UI',
+		js: () => import('./property-editor-ui-tiptap-toolbar-configuration.element.js'),
+		meta: {
+			label: 'Tiptap Toolbar Configuration',
+			icon: 'icon-autofill',
+			group: 'common',
+		},
+	},
+];
