1f99de1739c753c3f4104ac0f1a693f06aeb1ea8
Mads Rasmussen <madsr@hey.com>
2024-09-16 10:57:57 +0200

    only select writable cultures

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/workspace/document-workspace.context.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/workspace/document-workspace.context.ts
@@ -536,11 +536,15 @@ export class UmbDocumentWorkspaceContext
 		const selected = activeVariantIds.concat(changedVariantIds);
 		// Selected can contain entries that are not part of the options, therefor the modal filters selection based on options.
 
+		const readOnlyCultures = this.readOnlyState.getStates().map((s) => s.variantId.culture);
+		const selectedCultures = selected.map((x) => x.toString()).filter((v, i, a) => a.indexOf(v) === i);
+		const writable = selectedCultures.filter((x) => readOnlyCultures.includes(x) === false);
+
 		const options = await firstValueFrom(this.variantOptions);
 
 		return {
 			options,
-			selected: selected.map((x) => x.toString()).filter((v, i, a) => a.indexOf(v) === i),
+			selected: writable,
 		};
 	}
 
