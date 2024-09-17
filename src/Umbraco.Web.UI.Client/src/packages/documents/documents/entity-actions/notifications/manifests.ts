d3e85c54cef812476d24265ebc978fa4d4b55a6e
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-09-17 11:14:21 +0200

    feat: add permissions to notifications

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/entity-actions/notifications/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/entity-actions/notifications/manifests.ts
@@ -1,7 +1,9 @@
 import { UMB_DOCUMENT_ENTITY_TYPE } from '../../entity.js';
+import { UMB_USER_PERMISSION_DOCUMENT_NOTIFICATIONS } from '../../user-permissions/constants.js';
 import { manifests as repositoryManifests } from './repository/manifests.js';
 import { manifests as modalManifests } from './modal/manifests.js';
 import type { ManifestEntityAction } from '@umbraco-cms/backoffice/extension-registry';
+import { UMB_ENTITY_IS_NOT_TRASHED_CONDITION_ALIAS } from '@umbraco-cms/backoffice/recycle-bin';
 
 const actionManifests: Array<ManifestEntityAction> = [
 	{
@@ -14,8 +16,17 @@ const actionManifests: Array<ManifestEntityAction> = [
 		forEntityTypes: [UMB_DOCUMENT_ENTITY_TYPE],
 		meta: {
 			icon: 'icon-megaphone',
-			label: '#notifications_notifications',
+			label: '#actions_notify',
 		},
+		conditions: [
+			{
+				alias: 'Umb.Condition.UserPermission.Document',
+				allOf: [UMB_USER_PERMISSION_DOCUMENT_NOTIFICATIONS],
+			},
+			{
+				alias: UMB_ENTITY_IS_NOT_TRASHED_CONDITION_ALIAS,
+			},
+		],
 	},
 ];
 
