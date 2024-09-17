a445354b97a240996a76332f203a4ab916e4ab27
Mads Rasmussen <madsr@hey.com>
2024-09-17 20:24:13 +0200

    register workspace view

--- src/Umbraco.Web.UI.Client/src/packages/user/user/workspace/user-root/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/workspace/user-root/manifests.ts
@@ -1,13 +1,32 @@
 import { UMB_USER_ROOT_ENTITY_TYPE } from '../../entity.js';
+import { UMB_USER_ROOT_WORKSPACE_ALIAS } from './constants.js';
 
 export const manifests: Array<UmbExtensionManifest> = [
 	{
 		type: 'workspace',
-		alias: 'Umb.Workspace.UserRoot',
-		name: 'User Root Workspace View',
-		element: () => import('./user-root-workspace.element.js'),
+		kind: 'default',
+		alias: UMB_USER_ROOT_WORKSPACE_ALIAS,
+		name: 'User Root Workspace',
 		meta: {
 			entityType: UMB_USER_ROOT_ENTITY_TYPE,
+			headline: '#treeHeaders_users',
 		},
+	},
+	{
+		type: 'workspaceView',
+		alias: 'Umb.WorkspaceView.UserRoot.Collection',
+		name: 'User Root Collection Workspace View',
+		element: () => import('./collection-workspace-view.element.js'),
+		meta: {
+			label: 'Collection',
+			pathname: 'collection',
+			icon: 'icon-layers',
+		},
+		conditions: [
+			{
+				alias: 'Umb.Condition.WorkspaceAlias',
+				match: UMB_USER_ROOT_WORKSPACE_ALIAS,
+			},
+		],
 	},
 ];
