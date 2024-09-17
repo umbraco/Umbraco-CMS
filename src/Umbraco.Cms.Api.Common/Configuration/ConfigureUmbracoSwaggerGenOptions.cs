09fb1798620389ed8775892b36f23f16839e34cb
mattbrailsford <me@mattbrailsford.com>
2024-09-17 09:19:31 +0100

    Adds a SupportedDerivedTypes attribute that can be used to decorate API models to control which derived types can be returned by an endpoint

--- src/Umbraco.Cms.Api.Common/Configuration/ConfigureUmbracoSwaggerGenOptions.cs
+++ src/Umbraco.Cms.Api.Common/Configuration/ConfigureUmbracoSwaggerGenOptions.cs
@@ -61,6 +61,7 @@ public class ConfigureUmbracoSwaggerGenOptions : IConfigureOptions<SwaggerGenOpt
         swaggerGenOptions.TagActionsBy(api => new[] { api.GroupName });
         swaggerGenOptions.OrderActionsBy(ActionOrderBy);
         swaggerGenOptions.SchemaFilter<EnumSchemaFilter>();
+        swaggerGenOptions.SchemaFilter<SupportedDerivedTypesSchemaFilter>();
         swaggerGenOptions.CustomSchemaIds(_schemaIdSelector.SchemaId);
         swaggerGenOptions.SupportNonNullableReferenceTypes();
     }
