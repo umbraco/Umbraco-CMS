027fc5406d312f39a5793e4eb8a2579abfd449a1
Mads Rasmussen <madsr@hey.com>
2024-09-17 15:20:57 +0200

    export token

---
+++ src/Umbraco.Web.UI.Client/src/packages/core/modal/common/confirm/index.ts
@@ -0,0 +1,2 @@
+export * from './confirm-modal.controller.js';
+export * from './confirm-modal.element.js';
--- src/Umbraco.Web.UI.Client/src/packages/core/modal/common/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/modal/common/index.ts
@@ -1,1 +1,3 @@
+export * from './confirm/index.js';
+export * from './discard-changes/index.js';
 export * from './embedded-media/index.js';
