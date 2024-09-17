434ae7f186972ee15f6a186ec877f472dcd6fe39
Mads Rasmussen <madsr@hey.com>
2024-09-17 19:01:35 +0200

    render headline

--- src/Umbraco.Web.UI.Client/src/packages/core/workspace/kinds/default/default-workspace.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/workspace/kinds/default/default-workspace.element.ts
@@ -1,14 +1,20 @@
-import { css, customElement, html } from '@umbraco-cms/backoffice/external/lit';
+import type { ManifestWorkspaceDefaultKind } from './types.js';
+import { customElement, html, ifDefined, property } from '@umbraco-cms/backoffice/external/lit';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
 import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
 
 @customElement('umb-default-workspace')
 export class UmbDefaultWorkspaceElement extends UmbLitElement {
+	@property({ type: Object, attribute: false })
+	manifest?: ManifestWorkspaceDefaultKind;
+
 	override render() {
-		return html`This is a default workspace`;
+		const headline = this.manifest?.meta.headline;
+		return html`<umb-body-layout headline=${ifDefined(headline ? this.localize.string(headline) : undefined)}>
+		</umb-body-layout>`;
 	}
 
-	static override styles = [UmbTextStyles, css``];
+	static override styles = [UmbTextStyles];
 }
 
 export { UmbDefaultWorkspaceElement as element };
