92f560dc49686c77a40904baeed286ba04d589d7
Mads Rasmussen <madsr@hey.com>
2024-09-17 13:40:27 +0200

    add buttons to modal

--- src/Umbraco.Web.UI.Client/src/packages/core/modal/common/discard-changes/discard-changes-modal.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/modal/common/discard-changes/discard-changes-modal.element.ts
@@ -5,7 +5,18 @@ import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
 @customElement('umb-discard-changes-modal')
 export class UmbDiscardChangesModalElement extends UmbModalBaseElement {
 	override render() {
-		return html` <uui-dialog-layout class="uui-text" headline="Discard changes?"> Hello Hello </uui-dialog-layout> `;
+		return html`
+			<uui-dialog-layout class="uui-text" headline="Discard changes?">
+				<uui-button slot="actions" id="cancel" label="Stay" @click=${this._rejectModal}></uui-button>
+				<uui-button
+					slot="actions"
+					id="confirm"
+					color="positive"
+					look="primary"
+					label="Discard Changes"
+					@click=${this._submitModal}></uui-button>
+			</uui-dialog-layout>
+		`;
 	}
 
 	static override styles = [UmbTextStyles];
