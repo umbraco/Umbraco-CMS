410551b4b44c3d91aae39530881dde9876c7678e
Mads Rasmussen <madsr@hey.com>
2024-09-18 08:23:57 +0200

    register collection workspace view kind

--- src/Umbraco.Web.UI.Client/src/packages/core/collection/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/collection/manifests.ts
@@ -1,7 +1,10 @@
+import type { UmbExtensionManifestKind } from '../extension-registry/registry.js';
 import { manifest as collectionAliasCondition } from './collection-alias.manifest.js';
 import { manifest as collectionBulkActionPermissionCondition } from './collection-bulk-action-permission.manifest.js';
+import { manifests as workspaceViewManifests } from './workspace-view/manifests.js';
 
-export const manifests: Array<UmbExtensionManifest> = [
+export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> = [
+	...workspaceViewManifests,
 	collectionAliasCondition,
 	collectionBulkActionPermissionCondition,
 ];
