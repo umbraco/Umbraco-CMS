aae0b6f3196afa42fc6db3d752f49360d5851709
Mads Rasmussen <madsr@hey.com>
2024-09-18 12:26:41 +0200

    use collection workspace view for webhook root

--- src/Umbraco.Web.UI.Client/src/packages/webhook/workspace/webhook-root/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/webhook/workspace/webhook-root/manifests.ts
@@ -1,11 +1,34 @@
+import { UMB_WEBHOOK_COLLECTION_ALIAS } from '../../collection/manifests.js';
+import { UMB_WEBHOOK_ROOT_ENTITY_TYPE } from '../../entity.js';
+import { UMB_WEBHOOK_ROOT_WORKSPACE_ALIAS } from './constants.js';
+
 export const manifests: Array<UmbExtensionManifest> = [
 	{
 		type: 'workspace',
-		alias: 'Umb.Workspace.WebhookRoot',
+		kind: 'default',
+		alias: UMB_WEBHOOK_ROOT_WORKSPACE_ALIAS,
 		name: 'Webhook Root Workspace',
-		element: () => import('./webhook-root-workspace.element.js'),
 		meta: {
-			entityType: 'webhook-root',
+			entityType: UMB_WEBHOOK_ROOT_ENTITY_TYPE,
+			headline: '#treeHeaders_webhooks',
 		},
+	},
+	{
+		type: 'workspaceView',
+		kind: 'collection',
+		alias: 'Umb.WorkspaceView.WebhookRoot.Collection',
+		name: 'Webhook Root Collection Workspace View',
+		meta: {
+			label: 'Collection',
+			pathname: 'collection',
+			icon: 'icon-layers',
+			collectionAlias: UMB_WEBHOOK_COLLECTION_ALIAS,
+		},
+		conditions: [
+			{
+				alias: 'Umb.Condition.WorkspaceAlias',
+				match: UMB_WEBHOOK_ROOT_WORKSPACE_ALIAS,
+			},
+		],
 	},
 ];
