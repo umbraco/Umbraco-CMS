b2716377547e429581362b31dcc133c57812b38d
Mads Rasmussen <madsr@hey.com>
2024-09-18 13:55:52 +0200

    revert

--- src/Umbraco.Web.UI.Client/src/packages/core/workspace/kinds/default/default-workspace.context.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/workspace/kinds/default/default-workspace.context.ts
@@ -3,18 +3,13 @@ import type { UmbWorkspaceContext } from '../../workspace-context.interface.js';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import { UmbContextBase } from '@umbraco-cms/backoffice/class-api';
 import type { ManifestWorkspace } from '@umbraco-cms/backoffice/workspace';
-import { UmbEntityContext } from '@umbraco-cms/backoffice/entity';
 
 export abstract class UmbDefaultWorkspaceContext
 	extends UmbContextBase<UmbDefaultWorkspaceContext>
 	implements UmbWorkspaceContext
 {
 	public workspaceAlias!: string;
-
 	#entityType!: string;
-	#unique: string | null = null;
-
-	#entityContext = new UmbEntityContext(this);
 
 	constructor(host: UmbControllerHost) {
 		super(host, UMB_WORKSPACE_CONTEXT.toString());
@@ -23,13 +18,10 @@ export abstract class UmbDefaultWorkspaceContext
 	set manifest(manifest: ManifestWorkspace) {
 		this.workspaceAlias = manifest.alias;
 		this.#entityType = manifest.meta.entityType;
-
-		this.#entityContext.setEntityType(this.#entityType);
-		this.#entityContext.setUnique(this.#unique);
 	}
 
-	getUnique(): string | null {
-		return this.#unique;
+	getUnique(): string | undefined {
+		return undefined;
 	}
 
 	getEntityType(): string {
