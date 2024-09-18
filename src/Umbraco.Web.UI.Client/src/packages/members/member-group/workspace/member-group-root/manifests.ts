4a39839132469f28acd817de956549ef7c27c28d
Mads Rasmussen <madsr@hey.com>
2024-09-18 08:29:06 +0200

    use collection kind for member group root

--- src/Umbraco.Web.UI.Client/src/packages/members/member-group/workspace/member-group-root/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/members/member-group/workspace/member-group-root/manifests.ts
@@ -1,3 +1,4 @@
+import { UMB_MEMBER_GROUP_COLLECTION_ALIAS } from '../../collection/manifests.js';
 import { UMB_MEMBER_GROUP_ROOT_ENTITY_TYPE } from '../../entity.js';
 import { UMB_MEMBER_GROUP_ROOT_WORKSPACE_ALIAS } from './constants.js';
 
@@ -14,13 +15,14 @@ export const manifests: Array<UmbExtensionManifest> = [
 	},
 	{
 		type: 'workspaceView',
+		kind: 'collection',
 		alias: 'Umb.WorkspaceView.MemberGroupRoot.Collection',
 		name: 'Member Group Root Collection Workspace View',
-		element: () => import('./member-group-root-workspace.element.js'),
 		meta: {
 			label: 'Collection',
 			pathname: 'collection',
 			icon: 'icon-layers',
+			collectionAlias: UMB_MEMBER_GROUP_COLLECTION_ALIAS,
 		},
 		conditions: [
 			{
