34810f056389daca937bb28a7b576a43b442f0c7
Andreas Zerbst <andr317c@live.dk>
2024-09-19 09:10:14 +0200

    Removed unused cacheSettings

--- src/Umbraco.PublishedCache.HybridCache/NotificationHandlers/SeedingNotificationHandler.cs
+++ src/Umbraco.PublishedCache.HybridCache/NotificationHandlers/SeedingNotificationHandler.cs
@@ -1,6 +1,4 @@
-﻿using Microsoft.Extensions.Options;
-using Umbraco.Cms.Core.Events;
-using Umbraco.Cms.Core.Models;
+﻿using Umbraco.Cms.Core.Events;
 using Umbraco.Cms.Core.Notifications;
 using Umbraco.Cms.Infrastructure.HybridCache.Services;
 
@@ -9,12 +7,10 @@ namespace Umbraco.Cms.Infrastructure.HybridCache.NotificationHandlers;
 internal class SeedingNotificationHandler : INotificationAsyncHandler<UmbracoApplicationStartedNotification>
 {
     private readonly IDocumentCacheService _documentCacheService;
-    private readonly CacheSettings _cacheSettings;
 
-    public SeedingNotificationHandler(IDocumentCacheService documentCacheService, CacheSettings cacheSettings)
+    public SeedingNotificationHandler(IDocumentCacheService documentCacheService)
     {
         _documentCacheService = documentCacheService;
-        _cacheSettings = cacheSettings;
     }
 
     public async Task HandleAsync(UmbracoApplicationStartedNotification notification, CancellationToken cancellationToken) => await _documentCacheService.SeedAsync();
