4d17c8144a9a1785b88a5d68c75688cd221716d3
Lee Kelleher <leekelleher@users.noreply.github.com>
2024-09-19 15:10:27 +0100

    Icons tidy-up (#2317)
    - Wired up legacy icon paths
    - Upgraded Lucide npm pkg
    - Fixed Windows bug in generate script

--- src/Umbraco.Web.UI.Client/src/packages/core/icon-registry/icons/icon-page-remove.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/icon-registry/icons/icon-page-remove.ts
@@ -1,4 +1,4 @@
-export default `<!-- @license lucide-static v0.424.0 - ISC -->
+export default `<!-- @license lucide-static v0.441.0 - ISC -->
 <svg
   class="lucide lucide-file-minus-2"
   xmlns="http://www.w3.org/2000/svg"
