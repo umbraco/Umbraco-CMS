cf6137db1869020453d410c1307060af4503fc89
Ronald Barendse <ronald@barend.se>
2024-09-23 09:45:46 +0200

    Add `IAsyncComponent` to allow async initialize/terminate (#16536)
    * Add IAsyncComponent
    
    * Rewrite to use IAsyncComposer
    
    * Add AsyncComponentBase and RuntimeAsyncComponentBase
    
    * Remove manual disposing of components on restart

--- src/Umbraco.Core/Composing/IComponent.cs
+++ src/Umbraco.Core/Composing/IComponent.cs
@@ -1,28 +1,32 @@
 namespace Umbraco.Cms.Core.Composing;
 
-/// <summary>
-///     Represents a component.
-/// </summary>
-/// <remarks>
-///     <para>Components are created by DI and therefore must have a public constructor.</para>
-///     <para>
-///         All components are terminated in reverse order when Umbraco terminates, and
-///         disposable components are disposed.
-///     </para>
-///     <para>
-///         The Dispose method may be invoked more than once, and components
-///         should ensure they support this.
-///     </para>
-/// </remarks>
-public interface IComponent
+/// <inheritdoc />
+[Obsolete("Use IAsyncComponent instead. This interface will be removed in a future version.")]
+public interface IComponent : IAsyncComponent
 {
     /// <summary>
-    ///     Initializes the component.
+    /// Initializes the component.
     /// </summary>
     void Initialize();
 
     /// <summary>
-    ///     Terminates the component.
+    /// Terminates the component.
     /// </summary>
     void Terminate();
+
+    /// <inheritdoc />
+    Task IAsyncComponent.InitializeAsync(bool isRestarting, CancellationToken cancellationToken)
+    {
+        Initialize();
+
+        return Task.CompletedTask;
+    }
+
+    /// <inheritdoc />
+    Task IAsyncComponent.TerminateAsync(bool isRestarting, CancellationToken cancellationToken)
+    {
+        Terminate();
+
+        return Task.CompletedTask;
+    }
 }
