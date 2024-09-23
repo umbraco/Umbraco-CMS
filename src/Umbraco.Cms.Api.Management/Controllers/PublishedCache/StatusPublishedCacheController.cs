e51113b602dab33fe15dc4570742c77e584cd322
nikolajlauridsen <nikolajlauridsen@protonmail.ch>
2024-09-23 12:56:25 +0200

    Remove IPublishedSnapshotStatus

--- src/Umbraco.Cms.Api.Management/Controllers/PublishedCache/StatusPublishedCacheController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/PublishedCache/StatusPublishedCacheController.cs
@@ -6,16 +6,12 @@ using Umbraco.Cms.Core.PublishedCache;
 namespace Umbraco.Cms.Api.Management.Controllers.PublishedCache;
 
 [ApiVersion("1.0")]
+[Obsolete("This no longer relevant since snapshots are no longer used")]
 public class StatusPublishedCacheController : PublishedCacheControllerBase
 {
-    private readonly IPublishedSnapshotStatus _publishedSnapshotStatus;
-
-    public StatusPublishedCacheController(IPublishedSnapshotStatus publishedSnapshotStatus)
-        => _publishedSnapshotStatus = publishedSnapshotStatus;
-
     [HttpGet("status")]
     [MapToApiVersion("1.0")]
     [ProducesResponseType(typeof(string), StatusCodes.Status200OK)]
     public async Task<ActionResult<string>> Status(CancellationToken cancellationToken)
-        => await Task.FromResult(Ok(_publishedSnapshotStatus.GetStatus()));
+        => await Task.FromResult(Ok("Obsoleted"));
 }
