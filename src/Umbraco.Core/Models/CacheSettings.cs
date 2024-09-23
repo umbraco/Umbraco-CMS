af718e793aa2a94f36dd3b5728ea849ec5742a3a
Bjarke Berg <mail@bergmania.dk>
2024-09-23 14:09:19 +0200

    Add default value to timespan

--- src/Umbraco.Core/Models/CacheSettings.cs
+++ src/Umbraco.Core/Models/CacheSettings.cs
@@ -9,6 +9,7 @@ public class CacheSettings
     internal const int StaticDocumentBreadthFirstSeedCount = 100;
 
     internal const int StaticMediaBreadthFirstSeedCount = 100;
+    internal const string StaticSeedCacheDuration = "365.00:00:00";
 
     /// <summary>
     ///     Gets or sets a value for the collection of content type ids to always have in the cache.
@@ -23,5 +24,6 @@ public class CacheSettings
     [DefaultValue(StaticMediaBreadthFirstSeedCount)]
     public int MediaBreadthFirstSeedCount { get; set; } = StaticDocumentBreadthFirstSeedCount;
 
-    public TimeSpan SeedCacheDuration { get; set; } = TimeSpan.FromDays(365);
+    [DefaultValue(StaticSeedCacheDuration)]
+    public TimeSpan SeedCacheDuration { get; set; } = TimeSpan.Parse(StaticSeedCacheDuration);
 }
