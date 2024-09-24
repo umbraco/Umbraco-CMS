1766b5e431ef724bf8d314cecbfe34a3b5ff8996
Mads Rasmussen <madsr@hey.com>
2024-09-24 11:48:46 +0200

    add interface

---
+++ src/Umbraco.Web.UI.Client/src/packages/core/workspace/data-manager/workspace-data-manager.interface.ts
@@ -0,0 +1,9 @@
+import type { UmbController } from '@umbraco-cms/backoffice/controller-api';
+import type { UmbEntityModel } from '@umbraco-cms/backoffice/entity';
+
+export interface UmbWorkspaceDataManager<ModelType extends UmbEntityModel> extends UmbController {
+	getPersistedData(): ModelType | undefined;
+	getCurrentData(): ModelType | undefined;
+	setPersistedData(data: ModelType | undefined): void;
+	setCurrentData(data: ModelType | undefined): void;
+}
