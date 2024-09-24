dc5125898e2bb67569383a31b4c4346fa72a79d9
Niels Lyngsø <niels.lyngso@gmail.com>
2024-09-24 14:35:21 +0200

    correct method call

--- src/Umbraco.Web.UI.Client/src/packages/block/block/context/block-entries.context.ts
+++ src/Umbraco.Web.UI.Client/src/packages/block/block/context/block-entries.context.ts
@@ -112,7 +112,7 @@ export abstract class UmbBlockEntriesContext<
 			this._manager!.removeOneSettings(layout.settingsKey);
 		}
 		this._manager!.removeOneContent(contentKey);
-		this._manager!.removeExposes(contentKey);
+		this._manager!.removeExposesOf(contentKey);
 
 		this._layoutEntries.removeOne(contentKey);
 	}
