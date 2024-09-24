41ccbf621cc7de36ab17ca415f0448f4ba354efc
Mads Rasmussen <madsr@hey.com>
2024-09-24 19:03:46 +0200

    localize

--- src/Umbraco.Web.UI.Client/src/packages/core/modal/common/discard-changes/discard-changes-modal.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/modal/common/discard-changes/discard-changes-modal.element.ts
@@ -6,14 +6,19 @@ import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
 export class UmbDiscardChangesModalElement extends UmbModalBaseElement {
 	override render() {
 		return html`
-			<uui-dialog-layout class="uui-text" headline="Discard changes?">
-				<uui-button slot="actions" id="cancel" label="Stay" @click=${this._rejectModal}></uui-button>
+			<uui-dialog-layout class="uui-text" headline=${this.localize.term('prompt_unsavedChanges')}>
+				<umb-localize key="prompt_unsavedChangesWarning"></umb-localize>
+				<uui-button
+					slot="actions"
+					id="cancel"
+					label=${this.localize.term('prompt_stay')}
+					@click=${this._rejectModal}></uui-button>
 				<uui-button
 					slot="actions"
 					id="confirm"
 					color="positive"
 					look="primary"
-					label="Discard Changes"
+					label=${this.localize.term('prompt_discardChanges')}
 					@click=${this._submitModal}></uui-button>
 			</uui-dialog-layout>
 		`;
