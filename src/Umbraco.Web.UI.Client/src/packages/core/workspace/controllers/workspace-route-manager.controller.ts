cde9e247c936b334a2ee14d6158e9ac55a7b9d5f
Mads Rasmussen <madsr@hey.com>
2024-09-24 21:27:27 +0200

    add js docs

--- src/Umbraco.Web.UI.Client/src/packages/core/workspace/controllers/workspace-route-manager.controller.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/workspace/controllers/workspace-route-manager.controller.ts
@@ -2,11 +2,21 @@ import { UmbControllerBase } from '@umbraco-cms/backoffice/class-api';
 import { UmbArrayState } from '@umbraco-cms/backoffice/observable-api';
 import type { UmbRoute } from '@umbraco-cms/backoffice/router';
 
+/**
+ * The workspace route manager.
+ * @class UmbWorkspaceRouteManager
+ * @augments {UmbControllerBase}
+ */
 export class UmbWorkspaceRouteManager extends UmbControllerBase {
 	//
 	#routes = new UmbArrayState<UmbRoute>([], (x) => x.path);
 	public readonly routes = this.#routes.asObservable();
 
+	/**
+	 * Set the routes for the workspace.
+	 * @param {Array<UmbRoute>} routes The routes for the workspace.
+	 * @memberof UmbWorkspaceRouteManager
+	 */
 	setRoutes(routes: Array<UmbRoute>) {
 		this.#routes.setValue([
 			...routes,
@@ -15,5 +25,14 @@ export class UmbWorkspaceRouteManager extends UmbControllerBase {
 				component: async () => (await import('@umbraco-cms/backoffice/router')).UmbRouteNotFoundElement,
 			},
 		]);
+	}
+
+	/**
+	 * Get the routes for the workspace.
+	 * @returns {Array<UmbRoute>} The routes for the workspace.
+	 * @memberof UmbWorkspaceRouteManager
+	 */
+	getRoutes(): Array<UmbRoute> {
+		return this.#routes.getValue();
 	}
 }
