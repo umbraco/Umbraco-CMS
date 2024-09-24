797bd4e001e2485fa800736ea37a2c7d0e81fb65
nikolajlauridsen <nikolajlauridsen@protonmail.ch>
2024-09-24 13:35:40 +0200

    Delete NuCache 🎉

--- src/Umbraco.PublishedCache.NuCache/DataSource/ContentCacheDataSerializerEntityType.cs
+++
@@ -1,9 +0,0 @@
-namespace Umbraco.Cms.Infrastructure.PublishedCache.DataSource;
-
-[Flags]
-public enum ContentCacheDataSerializerEntityType
-{
-    Document = 1,
-    Media = 2,
-    Member = 4,
-}
