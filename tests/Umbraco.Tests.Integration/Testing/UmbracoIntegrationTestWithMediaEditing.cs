d5fd05627d301f74238e93d592a26bfef1aee9f1
Andreas Zerbst <andr317c@live.dk>
2024-09-25 11:20:51 +0200

    Media test

---
+++ tests/Umbraco.Tests.Integration/Testing/UmbracoIntegrationTestWithMediaEditing.cs
@@ -0,0 +1,45 @@
+﻿using NUnit.Framework;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Models.ContentEditing;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Services.ContentTypeEditing;
+using Umbraco.Cms.Tests.Common.Builders;
+
+namespace Umbraco.Cms.Tests.Integration.Testing;
+
+public abstract class UmbracoIntegrationTestWithMediaEditing : UmbracoIntegrationTest
+{
+    protected IMediaTypeEditingService MediaTypeEditingService => GetRequiredService<IMediaTypeEditingService>();
+    protected IMediaTypeService MediaTypeService => GetRequiredService<IMediaTypeService>();
+
+    protected IMediaEditingService MediaEditingService => GetRequiredService<IMediaEditingService>();
+
+    // protected IMediaTypeContainerService MediaTypeContainerService => GetRequiredService<IMediaTypeContainerService>();
+
+
+
+    protected MediaCreateModel RootFolder { get; private set; }
+
+    protected MediaCreateModel RootImage { get; private set; }
+
+    protected MediaCreateModel SubFolder1 { get; private set; }
+
+    protected MediaCreateModel SubFolder2 { get; private set; }
+
+    protected MediaCreateModel SubSubImage { get; private set; }
+
+    protected MediaCreateModel SubSubFile { get; private set; }
+
+
+
+    [SetUp]
+    public new void Setup() => CreateTestData();
+
+    protected async void CreateTestData()
+    {
+        var tester = MediaTypeEditingBuilder.CreateBasicMediaType();
+
+        await MediaTypeEditingService.CreateAsync(tester, Constants.Security.SuperUserKey);
+        // RootFolder =  MediaEditingBuilder.CreateBasicContent()
+    }
+}
---
+++ tests/Umbraco.Tests.Integration/Umbraco.PublishedCache.HybridCache/MediaTest.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Models.ContentEditing;
+using Umbraco.Cms.Core.PublishedCache;
+using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Web;
+using Umbraco.Cms.Tests.Common.Builders;
+using Umbraco.Cms.Tests.Common.Builders.Extensions;
+using Umbraco.Cms.Tests.Common.Testing;
+using Umbraco.Cms.Tests.Integration.Testing;
+
+namespace Umbraco.Cms.Tests.Integration.Umbraco.PublishedCache.HybridCache;
+
+[TestFixture]
+[UmbracoTest(Database = UmbracoTestOptions.Database.NewSchemaPerTest)]
+public class MediaTest : UmbracoIntegrationTestWithMediaEditing
+{
+
+
+    protected override void CustomTestSetup(IUmbracoBuilder builder) => builder.AddUmbracoHybridCache();
+
+
+    [Test]
+    public async Task Can_Get_Media_By_Key()
+    {
+
+    }
+
+}
