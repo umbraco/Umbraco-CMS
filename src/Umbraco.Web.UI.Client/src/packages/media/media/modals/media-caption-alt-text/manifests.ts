902bcbb54340e8389cbde51cdeadd37fc5b9a12d
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-09-25 14:41:37 +0200

    feat: move the media with caption modal from the tinymce to the media module

--- src/Umbraco.Web.UI.Client/src/packages/media/media/modals/media-caption-alt-text/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/modals/media-caption-alt-text/manifests.ts
@@ -3,6 +3,6 @@ export const manifests: Array<UmbExtensionManifest> = [
 		type: 'modal',
 		alias: 'Umb.Modal.MediaCaptionAltText',
 		name: 'Media Caption Alt Text',
-		element: () => import('./media-caption-alt-text/media-caption-alt-text-modal.element.js'),
+		element: () => import('./media-caption-alt-text-modal.element.js'),
 	},
 ];
--- src/Umbraco.Web.UI.Client/src/packages/tiny-mce/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/tiny-mce/manifests.ts
@@ -1,5 +1,4 @@
 import { manifests as propertyEditors } from './property-editors/manifests.js';
 import { manifests as plugins } from './plugins/manifests.js';
-import { manifests as modalManifests } from './modals/manifests.js';
 
-export const manifests: Array<UmbExtensionManifest> = [...propertyEditors, ...plugins, ...modalManifests];
+export const manifests: Array<UmbExtensionManifest> = [...propertyEditors, ...plugins];
