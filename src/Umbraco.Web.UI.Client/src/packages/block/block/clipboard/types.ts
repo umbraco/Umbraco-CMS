50ceb60d73397064b2af44ba44bf402db7011e8d
Niels Lyngsø <niels.lyngso@gmail.com>
2024-09-25 14:18:19 +0200

    omit clipboard

--- src/Umbraco.Web.UI.Client/src/packages/block/block/clipboard/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/block/block/clipboard/types.ts
@@ -1,9 +1,9 @@
-import type { UmbClipboardEntry } from '@umbraco-cms/backoffice/clipboard';
+//import type { UmbClipboardEntry } from '@umbraco-cms/backoffice/clipboard';
 
 /**
  * A Clipboard entry for Blocks.
  */
-export type UmbBlockClipboardEntry = UmbClipboardEntry<'block', UmbBlockClipboardEntryMeta, any>;
+//export type UmbBlockClipboardEntry = UmbClipboardEntry<'block', UmbBlockClipboardEntryMeta, any>;
 
 interface UmbBlockClipboardEntryMeta {
 	/**
