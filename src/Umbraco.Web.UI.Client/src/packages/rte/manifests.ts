ad88d3a08f3d1991e0cd4c258151ae61b8260107
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-09-26 14:52:20 +0200

    chore: eslint

--- src/Umbraco.Web.UI.Client/src/packages/rte/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/rte/manifests.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line local-rules/no-relative-import-to-import-map-module
 import { manifests as tiptapManifests } from './tiptap/manifests.js';
 import type { ManifestTypes, UmbExtensionManifestKind } from '@umbraco-cms/backoffice/extension-registry';
 
--- src/Umbraco.Web.UI.Client/src/packages/rte/tiptap/property-editors/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/rte/tiptap/property-editors/manifests.ts
@@ -1,3 +1,4 @@
+// eslint-disable-next-line local-rules/no-relative-import-to-import-map-module
 import { manifests as tiptapManifests } from './tiptap/manifests.js';
 import type { ManifestTypes } from '@umbraco-cms/backoffice/extension-registry';
 
