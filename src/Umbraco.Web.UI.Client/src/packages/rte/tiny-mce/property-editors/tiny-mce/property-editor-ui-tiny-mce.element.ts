8f7046a27285f5bf43f49c7ccec63e1811e2e454
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-09-27 10:41:10 +0200

    feat: create base element for rte property editor UIs to ensure blocks especially are treated the same way

--- src/Umbraco.Web.UI.Client/src/packages/rte/tiny-mce/property-editors/tiny-mce/property-editor-ui-tiny-mce.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/rte/tiny-mce/property-editors/tiny-mce/property-editor-ui-tiny-mce.element.ts
@@ -1,120 +1,14 @@
+import { UmbRteBaseElement } from '../../../components/rte-base.element.js';
 import type { UmbInputTinyMceElement } from '../../components/input-tiny-mce/input-tiny-mce.element.js';
-import { UMB_BLOCK_RTE_BLOCK_LAYOUT_ALIAS } from '../../../types.js';
-import { customElement, html, property, state } from '@umbraco-cms/backoffice/external/lit';
-import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
-import { UmbPropertyValueChangeEvent } from '@umbraco-cms/backoffice/property-editor';
-import type { UmbPropertyEditorConfigCollection } from '@umbraco-cms/backoffice/property-editor';
-import type { UmbPropertyEditorUiElement } from '@umbraco-cms/backoffice/extension-registry';
-import {
-	UmbBlockRteEntriesContext,
-	type UmbBlockRteLayoutModel,
-	UmbBlockRteManagerContext,
-	type UmbBlockRteTypeModel,
-} from '@umbraco-cms/backoffice/block-rte';
-import type { UmbBlockValueType } from '@umbraco-cms/backoffice/block';
+import { customElement, html } from '@umbraco-cms/backoffice/external/lit';
 
 import '../../components/input-tiny-mce/input-tiny-mce.element.js';
-
-export interface UmbRichTextEditorValueType {
-	markup: string;
-	blocks: UmbBlockValueType<UmbBlockRteLayoutModel>;
-}
 
 /**
  * @element umb-property-editor-ui-tiny-mce
  */
 @customElement('umb-property-editor-ui-tiny-mce')
-export class UmbPropertyEditorUITinyMceElement extends UmbLitElement implements UmbPropertyEditorUiElement {
-	//
-	// No need to registerer as a LIT-property, as we are calling it directly and no need for it to be reactive [NL]
-	public set config(config: UmbPropertyEditorConfigCollection | undefined) {
-		if (!config) return;
-
-		this._config = config;
-
-		const blocks = config.getValueByAlias<Array<UmbBlockRteTypeModel>>('blocks') ?? [];
-		this.#managerContext.setBlockTypes(blocks);
-
-		this.#managerContext.setEditorConfiguration(config);
-	}
-
-	@property({ attribute: false })
-	public set value(value: UmbRichTextEditorValueType | undefined) {
-		const buildUpValue: Partial<UmbRichTextEditorValueType> = value ? { ...value } : {};
-		buildUpValue.markup ??= '';
-		buildUpValue.blocks ??= { layout: {}, contentData: [], settingsData: [] };
-		buildUpValue.blocks.layout ??= {};
-		buildUpValue.blocks.contentData ??= [];
-		buildUpValue.blocks.settingsData ??= [];
-		this._value = buildUpValue as UmbRichTextEditorValueType;
-
-		if (this._latestMarkup !== this._value.markup) {
-			this._markup = this._value.markup;
-		}
-
-		this.#managerContext.setLayouts(buildUpValue.blocks.layout[UMB_BLOCK_RTE_BLOCK_LAYOUT_ALIAS] ?? []);
-		this.#managerContext.setContents(buildUpValue.blocks.contentData);
-		this.#managerContext.setSettings(buildUpValue.blocks.settingsData);
-	}
-	public get value(): UmbRichTextEditorValueType {
-		return this._value;
-	}
-
-	/**
-	 * Sets the input to readonly mode, meaning value cannot be changed but still able to read and select its content.
-	 * @type {boolean}
-	 * @attr
-	 * @default false
-	 */
-	@property({ type: Boolean, reflect: true })
-	readonly = false;
-
-	@state()
-	_config?: UmbPropertyEditorConfigCollection;
-
-	@state()
-	private _value: UmbRichTextEditorValueType = {
-		markup: '',
-		blocks: { layout: {}, contentData: [], settingsData: [] },
-	};
-
-	// Separate state for markup, to avoid re-rendering/re-setting the value of the TinyMCE editor when the value does not really change.
-	@state()
-	private _markup = '';
-	private _latestMarkup = ''; // The latest value gotten from the TinyMCE editor.
-
-	readonly #managerContext = new UmbBlockRteManagerContext(this);
-	readonly #entriesContext = new UmbBlockRteEntriesContext(this);
-
-	constructor() {
-		super();
-
-		this.observe(this.#entriesContext.layoutEntries, (layouts) => {
-			// Update manager:
-			this.#managerContext.setLayouts(layouts);
-		});
-
-		this.observe(this.#managerContext.layouts, (layouts) => {
-			this._value = {
-				...this._value,
-				blocks: { ...this._value.blocks, layout: { [UMB_BLOCK_RTE_BLOCK_LAYOUT_ALIAS]: layouts } },
-			};
-			this.#fireChangeEvent();
-		});
-		this.observe(this.#managerContext.contents, (contents) => {
-			this._value = { ...this._value, blocks: { ...this._value.blocks, contentData: contents } };
-			this.#fireChangeEvent();
-		});
-		this.observe(this.#managerContext.settings, (settings) => {
-			this._value = { ...this._value, blocks: { ...this._value.blocks, settingsData: settings } };
-			this.#fireChangeEvent();
-		});
-	}
-
-	#fireChangeEvent() {
-		this.dispatchEvent(new UmbPropertyValueChangeEvent());
-	}
-
+export class UmbPropertyEditorUITinyMceElement extends UmbRteBaseElement {
 	#onChange(event: CustomEvent & { target: UmbInputTinyMceElement }) {
 		const value = event.target.value;
 
@@ -134,10 +28,8 @@ export class UmbPropertyEditorUITinyMceElement extends UmbLitElement implements 
 		// Remove unused Blocks of Blocks Layout. Leaving only the Blocks that are present in Markup.
 		//const blockElements = editor.dom.select(`umb-rte-block, umb-rte-block-inline`);
 		const usedContentUdis = Array.from(blockEls).map((blockElement) => blockElement.getAttribute('data-content-udi'));
-		const unusedBlocks = this.#managerContext.getLayouts().filter((x) => usedContentUdis.indexOf(x.contentUdi) === -1);
-		unusedBlocks.forEach((blockLayout) => {
-			this.#managerContext.removeOneLayout(blockLayout.contentUdi);
-		});
+
+		this._filterUnusedBlocks(usedContentUdis);
 
 		// Then get the content of the editor and update the value.
 		// maybe in this way doc.body.innerHTML;
@@ -149,7 +41,7 @@ export class UmbPropertyEditorUITinyMceElement extends UmbLitElement implements 
 			markup: markup,
 		};
 
-		this.#fireChangeEvent();
+		this._fireChangeEvent();
 	}
 
 	override render() {
--- src/Umbraco.Web.UI.Client/src/packages/rte/types.ts
+++
@@ -1,1 +0,0 @@
-export const UMB_BLOCK_RTE_BLOCK_LAYOUT_ALIAS = 'Umbraco.TinyMCE';
