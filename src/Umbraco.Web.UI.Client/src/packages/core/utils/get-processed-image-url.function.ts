33820ee33fe39ae1e135a183f2e9b22f365c6eb3
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-09-27 11:21:12 +0200

    fix: make the `getProcessedImageUrl` return a path

--- src/Umbraco.Web.UI.Client/src/packages/core/utils/get-processed-image-url.function.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/utils/get-processed-image-url.function.ts
@@ -1,18 +1,25 @@
-// TODO: This does not feel like a utility, but should instead become a repository/data-source/resource something in that direction [NL]
+import type { GetImagingResizeUrlsData } from '@umbraco-cms/backoffice/external/backend-api';
 
 /**
- * Returns the URL of the processed image
- * @param imagePath
- * @param options
+ * Returns the URL of the processed image.
+ * @param {string} imagePath The path to the image.
+ * @param {GetImagingResizeUrlsData} options The options for resizing the image.
+ * @returns {Promise<string>} The URL of the processed image.
  */
-export async function getProcessedImageUrl(imagePath: string, options: any) {
+export async function getProcessedImageUrl(imagePath: string, options: GetImagingResizeUrlsData): Promise<string> {
 	if (!options) {
 		return imagePath;
 	}
 
-	// TODO => use backend cli when available
-	const result = await fetch('/umbraco/management/api/v1/images/GetProcessedImageUrl');
-	const url = (await result.json()) as string;
+	const searchParams = new URLSearchParams({
+		width: options.width?.toString() ?? '',
+		height: options.height?.toString() ?? '',
+		mode: options.mode ?? '',
+	});
+
+	// This should ideally use the ImagingService.getImagingResizeUrls method, but
+	// that would require the GUID of the media item, which is not available here.
+	const url = `${imagePath}?${searchParams.toString()}`;
 
 	return url;
 }
