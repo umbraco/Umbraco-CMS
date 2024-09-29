d8da18729919c93d451bf5eef225cb30674a5b3c
Andreas Zerbst <andr317c@live.dk>
2024-09-29 21:18:59 +0200

    Cleaned setup

--- tests/Umbraco.Tests.Integration/Testing/UmbracoIntegrationTestWithMediaEditing.cs
+++ tests/Umbraco.Tests.Integration/Testing/UmbracoIntegrationTestWithMediaEditing.cs
@@ -1,8 +1,12 @@
 ﻿using NUnit.Framework;
 using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Models.ContentEditing;
+using Umbraco.Cms.Core.Models.ContentTypeEditing;
 using Umbraco.Cms.Core.Services;
+using Umbraco.Cms.Core.Services.ContentTypeEditing;
 using Umbraco.Cms.Tests.Common.Builders;
+using Umbraco.Cms.Tests.Common.TestHelpers;
 
 namespace Umbraco.Cms.Tests.Integration.Testing;
 
@@ -12,6 +16,7 @@ public abstract class UmbracoIntegrationTestWithMediaEditing : UmbracoIntegratio
 
     protected IMediaEditingService MediaEditingService => GetRequiredService<IMediaEditingService>();
 
+    protected IMediaTypeEditingService MediaTypeEditingService => GetRequiredService<IMediaTypeEditingService>();
 
     protected MediaCreateModel RootFolder { get; private set; }
 
@@ -29,6 +34,14 @@ public abstract class UmbracoIntegrationTestWithMediaEditing : UmbracoIntegratio
 
     protected int SubFolder2Id { get; private set; }
 
+    protected MediaCreateModel SubImage { get; private set; }
+
+    protected int SubImageId { get; private set; }
+
+    protected MediaCreateModel SubTestMedia { get; private set; }
+
+    protected int SubTestMediaId { get; private set; }
+
     protected MediaCreateModel SubSubImage { get; private set; }
 
     protected int SubSubImageId { get; private set; }
@@ -37,22 +50,35 @@ public abstract class UmbracoIntegrationTestWithMediaEditing : UmbracoIntegratio
 
     protected int SubSubFileId { get; private set; }
 
+    protected MediaTypeCreateModel CustomMediaTypeCreateModel { get; private set; }
+
+    protected IMediaType CustomMediaType { get; private set; }
 
     [SetUp]
     public new void Setup() => CreateTestData();
 
     protected async void CreateTestData()
     {
-        // MediaTypeCreateModel = MediaTypeEditingBuilder.CreateBasicFolderMediaType();
-        //
-        // var mediaTypeFolderAttempt = await MediaTypeEditingService.CreateAsync(MediaTypeCreateModel, Constants.Security.SuperUserKey);
-        // Assert.IsTrue(mediaTypeFolderAttempt.Success);
+        CustomMediaTypeCreateModel = MediaTypeEditingBuilder.CreateMediaTypeWithOneProperty();
+        var mediaTypeTestAttempt = await MediaTypeEditingService.CreateAsync(CustomMediaTypeCreateModel, Constants.Security.SuperUserKey);
+        Assert.IsTrue(mediaTypeTestAttempt.Success);
+        CustomMediaType = mediaTypeTestAttempt.Result;
 
         // Gets all media types
+        var folderMediaTypes = await MediaTypeEditingService.GetFolderMediaTypes(0, 10);
+        var folderMediaType = folderMediaTypes.Items.FirstOrDefault(x => x.Alias == "Folder");
         var mediaTypes = MediaTypeService.GetAll();
         var mediaTypesList = mediaTypes.ToList();
-        var folderMediaType = mediaTypesList.FirstOrDefault(x => x.Alias == "Folder");
         var imageMediaType = mediaTypesList.FirstOrDefault(x => x.Alias == "Image");
+
+        // Add CustomMediaType to FolderMediaType AllowedContentTypes
+        var mediaTypeUpdateHelper = new MediaTypeUpdateHelper();
+        var updateModel = mediaTypeUpdateHelper.CreateMediaTypeUpdateModel(folderMediaType);
+        var folderMediaTypeAllowedContentTypes = updateModel.AllowedContentTypes.ToList();
+        folderMediaTypeAllowedContentTypes.Add(new ContentTypeSort(key: mediaTypeTestAttempt.Result.Key, sortOrder: folderMediaTypeAllowedContentTypes.Count, alias: CustomMediaTypeCreateModel.Alias));
+        updateModel.AllowedContentTypes = folderMediaTypeAllowedContentTypes;
+        var updatedFolderMediaTypeAttempt = MediaTypeEditingService.UpdateAsync(folderMediaType, updateModel, Constants.Security.SuperUserKey);
+        Assert.IsTrue(updatedFolderMediaTypeAttempt.Result.Success);
 
         // Create and Save RootFolder
         RootFolder = MediaEditingBuilder.CreateSimpleMedia(folderMediaType.Key, "RootFolder", null);
@@ -88,6 +114,24 @@ public abstract class UmbracoIntegrationTestWithMediaEditing : UmbracoIntegratio
         if (subFolder2Attempt.Result.Content != null)
         {
             SubFolder2Id = subFolder2Attempt.Result.Content.Id;
+        }
+
+        // Create and Save SubImage
+        SubImage = MediaEditingBuilder.CreateSimpleMedia(imageMediaType.Key, "SubImage", RootFolder.Key);
+        var subImageAttempt = await MediaEditingService.CreateAsync(SubImage, Constants.Security.SuperUserKey);
+        Assert.IsTrue(subImageAttempt.Success);
+        if (subImageAttempt.Result.Content != null)
+        {
+            SubImageId = subImageAttempt.Result.Content.Id;
+        }
+
+        // Create and Save SubTestMedia
+        SubTestMedia = MediaEditingBuilder.CreateMediaWithAProperty(mediaTypeTestAttempt.Result.Key, "SubTestMedia", RootFolder.Key, "testProperty", "This is a test");
+        var subTestMediaAttempt = await MediaEditingService.CreateAsync(SubTestMedia, Constants.Security.SuperUserKey);
+        Assert.IsTrue(subTestMediaAttempt.Success);
+        if (subTestMediaAttempt.Result.Content != null)
+        {
+            SubTestMediaId = subTestMediaAttempt.Result.Content.Id;
         }
 
         // Create and Save SubSubImage
