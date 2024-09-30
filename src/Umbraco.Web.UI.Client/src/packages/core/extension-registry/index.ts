c639a776a8ce6143bcc0267d07b609f01ff9d7b7
Mads Rasmussen <madsr@hey.com>
2024-09-30 14:54:31 +0200

    clean up

--- src/Umbraco.Web.UI.Client/src/packages/core/extension-registry/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/extension-registry/index.ts
@@ -2,7 +2,6 @@ export * from './conditions/index.js';
 export * from './initializers/index.js';
 export * from './registry.js';
 export * from './utils/index.js';
-export type * from './interfaces/index.js';
 export type * from './models/index.js';
 export type * from './extensions/index.js';
 
--- src/Umbraco.Web.UI.Client/src/packages/core/extension-registry/interfaces/index.ts
+++
@@ -1,1 +0,0 @@
-export * from './external-login-provider-element.interface.js';
