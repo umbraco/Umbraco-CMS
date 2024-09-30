389b579ba5da8759886b5e7569a836681dc99bf0
Mads Rasmussen <madsr@hey.com>
2024-09-30 17:21:49 +0200

    register dictionary overview dashboard

--- src/Umbraco.Web.UI.Client/src/packages/dictionary/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/dictionary/manifests.ts
@@ -3,7 +3,6 @@ import { manifests as dashboardManifests } from './dashboard/manifests.js';
 import { manifests as entityActionManifests } from './entity-action/manifests.js';
 import { manifests as menuManifests } from './menu/manifests.js';
 import { manifests as repositoryManifests } from './repository/manifests.js';
-import { manifests as sectionManifests } from './section/manifests.js';
 import { manifests as treeManifests } from './tree/manifests.js';
 import { manifests as workspaceManifests } from './workspace/manifests.js';
 
@@ -13,7 +12,6 @@ export const manifests: Array<UmbExtensionManifest> = [
 	...entityActionManifests,
 	...menuManifests,
 	...repositoryManifests,
-	...sectionManifests,
 	...treeManifests,
 	...workspaceManifests,
 ];
