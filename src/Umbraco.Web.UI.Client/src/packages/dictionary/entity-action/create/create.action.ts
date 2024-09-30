51076e889bf5000757a992332408f3105be09829
Mads Rasmussen <madsr@hey.com>
2024-09-30 21:26:33 +0200

    fix imports

--- src/Umbraco.Web.UI.Client/src/packages/dictionary/entity-action/create/create.action.ts
+++ src/Umbraco.Web.UI.Client/src/packages/dictionary/entity-action/create/create.action.ts
@@ -1,7 +1,5 @@
-import type { UmbEntityActionArgs } from '@umbraco-cms/backoffice/entity-action';
 import { UmbEntityActionBase } from '@umbraco-cms/backoffice/entity-action';
-import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
-import { UMB_CREATE_DICTIONARY_WORKSPACE_PATH_PATTERN } from '../../workspace/paths';
+import { UMB_CREATE_DICTIONARY_WORKSPACE_PATH_PATTERN } from '../../workspace/index.js';
 
 export class UmbCreateDictionaryEntityAction extends UmbEntityActionBase<never> {
 	override async execute() {
