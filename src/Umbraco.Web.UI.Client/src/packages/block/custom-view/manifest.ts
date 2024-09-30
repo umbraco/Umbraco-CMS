082eaebdfe79f8b542cfd5ce70fe5c81ba582b7e
Niels Lyngsø <niels.lyngso@gmail.com>
2024-09-30 09:00:11 +0200

    merge

--- src/Umbraco.Web.UI.Client/src/packages/block/custom-view/manifest.ts
+++
@@ -1,8 +0,0 @@
-export const manifest: UmbExtensionManifest = {
-	type: 'blockEditorCustomView',
-	alias: 'Umb.blockEditorCustomView.TestView',
-	name: 'Block Editor Custom View Test',
-	element: () => import('./custom-view.element.js'),
-	forContentTypeAlias: 'elementTypeHeadline',
-	forBlockEditor: 'block-grid',
-};
