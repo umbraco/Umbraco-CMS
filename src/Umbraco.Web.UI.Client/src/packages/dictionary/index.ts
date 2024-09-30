e2eb731db19efaf20ec3a53bb540c8bf62451c5d
Mads Rasmussen <madsr@hey.com>
2024-09-30 19:34:19 +0200

    add paths

--- src/Umbraco.Web.UI.Client/src/packages/dictionary/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/dictionary/index.ts
@@ -3,3 +3,4 @@ export * from './tree/index.js';
 export * from './modals/dictionary-picker-modal.token.js';
 export * from './entity.js';
 export * from './components/index.js';
+export * from './paths.js';
