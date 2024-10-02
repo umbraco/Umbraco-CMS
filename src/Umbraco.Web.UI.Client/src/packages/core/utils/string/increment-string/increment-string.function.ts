77c143cc51b006b5269bb9abe1bb79847287ef0b
Mads Rasmussen <madsr@hey.com>
2024-10-02 20:25:22 +0200

    add docs

--- src/Umbraco.Web.UI.Client/src/packages/core/utils/string/increment-string/increment-string.function.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/utils/string/increment-string/increment-string.function.ts
@@ -1,7 +1,8 @@
 /**
- *
- * @param text
+ * @description Increment string
+ * @param {string} text The text to increment
+ * @returns {string} The incremented string
  */
-export function incrementString(text: string) {
+export function incrementString(text: string): string {
 	return text.replace(/(\d*)$/, (_, t) => (+t + 1).toString().padStart(t.length, '0'));
 }
