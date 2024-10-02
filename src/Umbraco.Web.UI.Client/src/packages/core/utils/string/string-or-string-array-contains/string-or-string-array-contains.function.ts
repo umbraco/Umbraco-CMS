b73a2fe644f91758ebfaf6af52496e70c7c19573
Mads Rasmussen <madsr@hey.com>
2024-10-02 20:32:37 +0200

    add docs

--- src/Umbraco.Web.UI.Client/src/packages/core/utils/string/string-or-string-array-contains/string-or-string-array-contains.function.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/utils/string/string-or-string-array-contains/string-or-string-array-contains.function.ts
@@ -1,8 +1,9 @@
 /**
- *
- * @param value
- * @param search
+ * @description Check if a string or array of strings contains a specific string
+ * @param value The string or array of strings to search in
+ * @param search The string to search for
+ * @returns {boolean} Whether the string or array of strings contains the search string
  */
-export function stringOrStringArrayContains(value: string | Array<string>, search: string) {
+export function stringOrStringArrayContains(value: string | Array<string>, search: string): boolean {
 	return Array.isArray(value) ? value.indexOf(search) !== -1 : value === search;
 }
