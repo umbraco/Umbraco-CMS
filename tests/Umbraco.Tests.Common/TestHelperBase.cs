dc43e6cd24f85fae6c11ee754191d3e98bdbf25f
Jeroen Breuer <jbreuer@gmail.com>
2024-10-02 10:09:15 +0200

    Fix MediaFileManager warning.

--- tests/Umbraco.Tests.Common/TestHelperBase.cs
+++ tests/Umbraco.Tests.Common/TestHelperBase.cs
@@ -96,8 +96,7 @@ public abstract class TestHelperBase
                 Mock.Of<IMediaPathScheme>(),
                 loggerFactory.CreateLogger<MediaFileManager>(),
                 Mock.Of<IShortStringHelper>(),
-                Mock.Of<IServiceProvider>(),
-                Options.Create(new ContentSettings()));
+                Mock.Of<IServiceProvider>());
             var databaseFactory = new Mock<IUmbracoDatabaseFactory>();
             var database = new Mock<IUmbracoDatabase>();
             var sqlContext = new Mock<ISqlContext>();
