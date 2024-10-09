ae0d5416e847330271966c10f5319bc235d5d207
Niels Lyngsø <niels.lyngso@gmail.com>
2024-10-09 12:15:01 +0200

    optimize observable part

--- src/Umbraco.Web.UI.Client/src/packages/core/content-type/structure/content-type-structure-manager.class.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/content-type/structure/content-type-structure-manager.class.ts
@@ -70,9 +70,9 @@ export class UmbContentTypeStructureManager<
 		// Notice this may need to use getValue to avoid resetting it self. [NL]
 		return contentTypes.some((x) => x.properties.length > 0);
 	});
-	readonly contentTypePropertyAliases = this.#contentTypes.asObservablePart((contentTypes) => {
-		return contentTypes.flatMap((x) => x.properties ?? []).map((x) => x.alias);
-	});
+	readonly contentTypePropertyAliases = createObservablePart(this.contentTypeProperties, (properties) =>
+		properties.map((x) => x.alias),
+	);
 	readonly contentTypeUniques = this.#contentTypes.asObservablePart((x) => x.map((y) => y.unique));
 	readonly contentTypeAliases = this.#contentTypes.asObservablePart((x) => x.map((y) => y.alias));
 
