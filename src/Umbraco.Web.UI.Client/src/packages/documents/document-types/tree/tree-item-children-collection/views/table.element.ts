639d57f1cb4635a51179b3da6718efadc46e690b
Mads Rasmussen <madsr@hey.com>
2024-10-10 11:05:42 +0200

    register modal routes for editing

--- src/Umbraco.Web.UI.Client/src/packages/documents/document-types/tree/tree-item-children-collection/views/table.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/document-types/tree/tree-item-children-collection/views/table.element.ts
@@ -1,3 +1,4 @@
+import { UMB_EDIT_DOCUMENT_TYPE_WORKSPACE_PATH_PATTERN } from '../../../paths.js';
 import type { UmbDefaultCollectionContext } from '@umbraco-cms/backoffice/collection';
 import { UMB_COLLECTION_CONTEXT } from '@umbraco-cms/backoffice/collection';
 import type { UmbTableColumn, UmbTableConfig, UmbTableItem } from '@umbraco-cms/backoffice/components';
@@ -5,6 +6,8 @@ import { css, html, customElement, state } from '@umbraco-cms/backoffice/externa
 import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
 import type { UmbTreeItemModel } from '@umbraco-cms/backoffice/tree';
+import { UmbModalRouteRegistrationController, type UmbModalRouteBuilder } from '@umbraco-cms/backoffice/router';
+import { UMB_WORKSPACE_MODAL } from '@umbraco-cms/backoffice/workspace';
 
 const elementName = 'umb-tree-item-children-table-collection-view';
 @customElement(elementName)
@@ -26,14 +29,16 @@ export class UmbTreeItemChildrenTableCollectionViewElement extends UmbLitElement
 	private _tableItems: Array<UmbTableItem> = [];
 
 	#collectionContext?: UmbDefaultCollectionContext<any>;
+	#routeBuilder?: UmbModalRouteBuilder;
 
 	constructor() {
 		super();
 
 		this.consumeContext(UMB_COLLECTION_CONTEXT, (instance) => {
 			this.#collectionContext = instance;
-			this.#observeCollectionItems();
 		});
+
+		this.#registerModalRoute();
 	}
 
 	#observeCollectionItems() {
@@ -41,15 +46,35 @@ export class UmbTreeItemChildrenTableCollectionViewElement extends UmbLitElement
 		this.observe(this.#collectionContext.items, (items) => this.#createTableItems(items), 'umbCollectionItemsObserver');
 	}
 
-	#createTableItems(item: Array<UmbTreeItemModel>) {
-		this._tableItems = item.map((item) => {
+	#registerModalRoute() {
+		new UmbModalRouteRegistrationController(this, UMB_WORKSPACE_MODAL)
+			.addAdditionalPath(':entityType')
+			.onSetup((params) => {
+				return { data: { entityType: params.entityType, preset: {} } };
+			})
+			.observeRouteBuilder((routeBuilder) => {
+				this.#routeBuilder = routeBuilder;
+
+				// NOTE: Configuring the observations AFTER the route builder is ready,
+				// otherwise there is a race condition and `#collectionContext.items` tends to win. [LK]
+				this.#observeCollectionItems();
+			});
+	}
+
+	#createTableItems(items: Array<UmbTreeItemModel>) {
+		this._tableItems = items.map((item) => {
+			const editPath = this.#routeBuilder
+				? this.#routeBuilder({ entityType: item.entityType }) +
+					UMB_EDIT_DOCUMENT_TYPE_WORKSPACE_PATH_PATTERN.generateLocal({ unique: item.unique })
+				: '';
+
 			return {
 				id: item.unique,
 				icon: item.isFolder && !item.icon ? 'icon-folder' : item.icon,
 				data: [
 					{
 						columnAlias: 'name',
-						value: item.name,
+						value: html`<uui-button href=${editPath} label=${item.name}></uui-button>`,
 					},
 				],
 			};
