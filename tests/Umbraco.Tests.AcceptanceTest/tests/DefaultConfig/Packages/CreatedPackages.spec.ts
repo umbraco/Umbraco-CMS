289e0a956c89b953fa5ac064cd9e3edf27489ddd
Andreas Zerbst <andr317c@live.dk>
2024-10-10 12:24:33 +0200

    Cleaned package tests

--- tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Packages/CreatedPackages.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Packages/CreatedPackages.spec.ts
@@ -15,7 +15,7 @@ test.afterEach(async ({umbracoApi}) => {
   await umbracoApi.package.ensureNameNotExists(packageName);
 });
 
-test('can create a empty package', {tag: '@smoke'}, async ({page, umbracoUi}) => {
+test('can create a empty package', {tag: '@smoke'}, async ({umbracoUi}) => {
   // Act
   await umbracoUi.package.clickCreatePackageButton();
   await umbracoUi.package.enterPackageName(packageName);
@@ -27,14 +27,14 @@ test('can create a empty package', {tag: '@smoke'}, async ({page, umbracoUi}) =>
   await umbracoUi.package.isPackageNameVisible(packageName);
 });
 
-test('can update package name', async ({page, umbracoApi, umbracoUi}) => {
+test('can update package name', async ({umbracoApi, umbracoUi}) => {
   // Arrange
   const wrongPackageName = 'WrongPackageName';
   await umbracoApi.package.ensureNameNotExists(wrongPackageName);
   await umbracoApi.package.createEmptyPackage(wrongPackageName);
   await umbracoUi.reloadPage();
+  await umbracoUi.package.goToSection(ConstantHelper.sections.packages);
   await umbracoUi.package.clickCreatedTab();
-
 
   // Act
   await umbracoUi.package.clickExistingPackageName(wrongPackageName);
@@ -48,27 +48,23 @@ test('can update package name', async ({page, umbracoApi, umbracoUi}) => {
   expect(umbracoApi.package.doesNameExist(packageName)).toBeTruthy();
 });
 
-test('can delete a package', async ({page, umbracoApi, umbracoUi}) => {
+test('can delete a package', async ({umbracoApi, umbracoUi}) => {
   // Arrange
   await umbracoApi.package.createEmptyPackage(packageName);
   await umbracoUi.reloadPage();
   await umbracoUi.package.clickCreatedTab();
 
   // Act
-  await page.pause()
   await umbracoUi.package.clickDeleteButtonForPackageName(packageName);
-  await umbracoUi.package.clickDeleteExactLabel();
+  await umbracoUi.package.clickConfirmToDeleteButton();
 
   // Assert
-  await umbracoUi.package.isSuccessNotificationVisible()
   await umbracoUi.package.clickCreatedTab();
   await umbracoUi.package.isPackageNameVisible(packageName, false);
   expect(await umbracoApi.package.doesNameExist(packageName)).toBeFalsy();
 });
 
-// TODO: Update the locators for the choose button. If it is updated or not
-// TODO: Remove .skip when the test is able to run. Currently it is not possible to add content to a package
-test('can create a package with content', async ({page, umbracoApi, umbracoUi}) => {
+test('can create a package with content', async ({umbracoApi, umbracoUi}) => {
   // Arrange
   const documentTypeName = 'TestDocumentType';
   const documentName = 'TestDocument';
@@ -79,50 +75,43 @@ test('can create a package with content', async ({page, umbracoApi, umbracoUi}) 
   // Act
   await umbracoUi.package.clickCreatePackageButton();
   await umbracoUi.package.enterPackageName(packageName);
-  await page.pause();
-  // The frontend has updated the button name to "Choose" of "Add". But they are a bit unsure if they want to change it to select instead.
-  // So for the moment I have used the page instead of our UiHelper. Because it is easier to change the locator.
-  // await umbracoUi.package.clickAddContentToPackageButton();
-  await page.locator('[label="Content"] >> [label="Choose"]').click();
+  await umbracoUi.package.clickAddContentToPackageButton();
   await umbracoUi.package.clickLabelWithName(documentName);
-  await umbracoUi.package.clickChooseBtn();
+  await umbracoUi.package.clickChooseContainerButton();
   await umbracoUi.package.clickCreateButton();
 
   // Assert
-  await umbracoUi.package.isSuccessNotificationVisible()
+  await umbracoUi.package.isSuccessNotificationVisible();
   const packageData = await umbracoApi.package.getByName(packageName);
   expect(packageData.contentNodeId == documentId).toBeTruthy();
-  await umbracoUi.package.clickCreatedTab();
-  await umbracoUi.package.clickExistingPackageName(packageName);
-  expect(umbracoUi.package.isButtonWithNameVisible(documentName + ' ' + documentId)).toBeTruthy();
+  expect(umbracoUi.package.isButtonWithNameVisible(documentName)).toBeTruthy();
 
   // Clean
   await umbracoApi.documentType.ensureNameNotExists(documentTypeName);
 });
 
-// Currently unable to run this test. Because you are not able to save a mediaId
 test('can create a package with media', async ({umbracoApi, umbracoUi}) => {
   // Arrange
-  const mediaTypeName = 'TestMediaType';
   const mediaName = 'TestMedia';
+  await umbracoApi.media.ensureNameNotExists(mediaName);
   const mediaId = await umbracoApi.media.createDefaultMediaFile(mediaName);
 
   // Act
   await umbracoUi.package.clickCreatePackageButton();
   await umbracoUi.package.enterPackageName(packageName);
   await umbracoUi.package.clickAddMediaToPackageButton();
-  await umbracoUi.package.clickCaretButton();
-  await umbracoUi.package.clickLabelWithName(mediaName);
+  await umbracoUi.media.selectMediaByName(mediaName);
   await umbracoUi.package.clickSubmitButton();
   await umbracoUi.package.clickCreateButton();
 
   // Assert
-  await umbracoUi.package.isSuccessNotificationVisible()
-  await umbracoUi.package.clickCreatedTab();
-  await umbracoUi.package.clickExistingPackageName(packageName);
-  expect(umbracoUi.package.isButtonWithNameVisible(mediaTypeName + ' ' + mediaId)).toBeTruthy();
+  await umbracoUi.package.isSuccessNotificationVisible();
+  expect(umbracoUi.package.isTextWithExactNameVisible(mediaName)).toBeTruthy();
   const packageData = await umbracoApi.package.getByName(packageName);
   expect(packageData.mediaIds[0] == mediaId).toBeTruthy();
+
+  // Clean
+  await umbracoApi.media.ensureNameNotExists(mediaName);
 });
 
 test('can create a package with document types', async ({umbracoApi, umbracoUi}) => {
@@ -135,16 +124,13 @@ test('can create a package with document types', async ({umbracoApi, umbracoUi})
   await umbracoUi.package.clickCreatePackageButton();
   await umbracoUi.package.enterPackageName(packageName);
   await umbracoUi.package.clickAddDocumentTypeToPackageButton();
-  await umbracoUi.package.clickCaretButton();
   await umbracoUi.package.clickLabelWithName(documentTypeName);
-  await umbracoUi.package.clickSubmitButton();
+  await umbracoUi.package.clickChooseContainerButton();
   await umbracoUi.package.clickCreateButton();
 
   // Assert
-  await umbracoUi.package.isSuccessNotificationVisible()
-  await umbracoUi.package.clickCreatedTab();
-  await umbracoUi.package.clickExistingPackageName(packageName);
-  expect(umbracoUi.package.isButtonWithNameVisible(documentTypeName + ' ' + documentTypeId)).toBeTruthy();
+  await umbracoUi.package.isSuccessNotificationVisible();
+  expect(umbracoUi.package.isButtonWithNameVisible(documentTypeName)).toBeTruthy();
   const packageData = await umbracoApi.package.getByName(packageName);
   expect(packageData.documentTypes[0] == documentTypeId).toBeTruthy();
 
@@ -152,7 +138,6 @@ test('can create a package with document types', async ({umbracoApi, umbracoUi})
   await umbracoApi.documentType.ensureNameNotExists(documentTypeName);
 });
 
-// TODO: Remove .skip when the test is able to run. Currently waiting for button
 test('can create a package with media types', async ({umbracoApi, umbracoUi}) => {
   // Arrange
   const mediaTypeName = 'TestMediaType';
@@ -163,16 +148,13 @@ test('can create a package with media types', async ({umbracoApi, umbracoUi}) =>
   await umbracoUi.package.clickCreatePackageButton();
   await umbracoUi.package.enterPackageName(packageName);
   await umbracoUi.package.clickAddMediaTypeToPackageButton();
-  await umbracoUi.package.clickCaretButton();
-  await umbracoUi.package.clickLabelWithName(mediaTypeName);
-  await umbracoUi.package.clickSubmitButton()
+  await umbracoUi.package.clickButtonWithName(mediaTypeName, true);
+  await umbracoUi.package.clickChooseContainerButton()
   await umbracoUi.package.clickCreateButton();
 
   // Assert
   await umbracoUi.package.isSuccessNotificationVisible()
-  await umbracoUi.package.clickCreatedTab();
-  await umbracoUi.package.clickExistingPackageName(packageName);
-  expect(umbracoUi.package.isButtonWithNameVisible(mediaTypeName + ' ' + mediaTypeId)).toBeTruthy();
+  expect(umbracoUi.package.isButtonWithNameVisible(mediaTypeName)).toBeTruthy();
   const packageData = await umbracoApi.package.getByName(packageName);
   expect(packageData.mediaTypes[0] == mediaTypeId).toBeTruthy();
 
@@ -180,9 +162,9 @@ test('can create a package with media types', async ({umbracoApi, umbracoUi}) =>
   await umbracoApi.mediaType.ensureNameNotExists(mediaTypeName);
 });
 
-// TODO: Remove .skip when the test is able to run. After adding a language to a package and saving. The language is not saved or anything.
 test('can create a package with languages', async ({umbracoApi, umbracoUi}) => {
   // Arrange
+  await umbracoApi.language.ensureNameNotExists('Danish');
   const languageId = await umbracoApi.language.createDanishLanguage();
   const languageData = await umbracoApi.language.get(languageId);
   const languageName = languageData.name;
@@ -190,17 +172,13 @@ test('can create a package with languages', async ({umbracoApi, umbracoUi}) => {
   // Act
   await umbracoUi.package.clickCreatePackageButton();
   await umbracoUi.package.enterPackageName(packageName);
-  await umbracoUi.package.clickExistingPackageName(packageName);
   await umbracoUi.package.clickAddLanguageToPackageButton();
-  await umbracoUi.package.clickCaretButton();
-  await umbracoUi.package.clickLabelWithName(languageName);
+  await umbracoUi.package.clickButtonWithName(languageName);
   await umbracoUi.package.clickSubmitButton()
   await umbracoUi.package.clickCreateButton();
 
   // Assert
-  await umbracoUi.package.isSuccessNotificationVisible()
-  await umbracoUi.package.clickCreatedTab();
-  await umbracoUi.package.clickExistingPackageName(packageName);
+  await umbracoUi.package.isSuccessNotificationVisible();
   expect(umbracoUi.package.isButtonWithNameVisible(languageName + ' ' + languageId)).toBeTruthy();
   const packageData = await umbracoApi.package.getByName(packageName);
   expect(packageData.languages[0] == languageId).toBeTruthy();
@@ -209,79 +187,69 @@ test('can create a package with languages', async ({umbracoApi, umbracoUi}) => {
   await umbracoApi.language.ensureNameNotExists(languageName);
 });
 
-// TODO: Remove .skip when the test is able to run. Currently waiting for button
 test('can create a package with dictionary', async ({umbracoApi, umbracoUi}) => {
   // Arrange
   const dictionaryName = 'TestDictionary';
-  await umbracoApi.dictionary.createDefaultDictionary(dictionaryName);
+  const dictionaryId = await umbracoApi.dictionary.createDefaultDictionary(dictionaryName);
 
   // Act
   await umbracoUi.package.clickCreatePackageButton();
   await umbracoUi.package.enterPackageName(packageName);
   await umbracoUi.package.clickAddDictionaryToPackageButton();
-  await umbracoUi.package.clickCaretButton();
-  await umbracoUi.package.clickLabelWithName(dictionaryName);
-  await umbracoUi.package.clickSubmitButton()
+  await umbracoUi.package.clickButtonWithName(dictionaryName);
+  await umbracoUi.package.clickChooseContainerButton();
   await umbracoUi.package.clickCreateButton();
 
   // Assert
-  await umbracoUi.package.isSuccessNotificationVisible()
-  await umbracoUi.package.clickCreatedTab();
-  await umbracoUi.package.clickExistingPackageName(packageName);
+  await umbracoUi.package.isSuccessNotificationVisible();
   expect(umbracoUi.package.isButtonWithNameVisible(dictionaryName)).toBeTruthy();
   const packageData = await umbracoApi.package.getByName(packageName);
-  expect(packageData.dictionaryItems[0] == dictionaryName).toBeTruthy();
+  expect(packageData.dictionaryItems[0] == dictionaryId).toBeTruthy();
 
   // Clean
   await umbracoApi.dictionary.ensureNameNotExists(dictionaryName);
 });
 
-// TODO: Remove .skip when the test is able to run. After adding a dataType to a package and saving. The datatype is not saved or anything.
 test('can create a package with data types', async ({umbracoApi, umbracoUi}) => {
   // Arrange
   const dataTypeName = 'TestDataType';
+  await umbracoApi.dataType.ensureNameNotExists(dataTypeName);
   const dataTypeId = await umbracoApi.dataType.createDateTypeDataType(dataTypeName);
 
   // Act
   await umbracoUi.package.clickCreatePackageButton();
   await umbracoUi.package.enterPackageName(packageName);
   await umbracoUi.package.clickAddDataTypesToPackageButton();
-  await umbracoUi.package.clickCaretButton();
   await umbracoUi.package.clickLabelWithName(dataTypeName);
-  await umbracoUi.package.clickSubmitButton()
+  await umbracoUi.package.clickChooseContainerButton();
   await umbracoUi.package.clickCreateButton();
 
   // Assert
-  await umbracoUi.package.isSuccessNotificationVisible()
-  await umbracoUi.package.clickCreatedTab();
-  await umbracoUi.package.clickExistingPackageName(packageName);
+  await umbracoUi.package.isSuccessNotificationVisible();
   expect(umbracoUi.package.isButtonWithNameVisible(dataTypeName)).toBeTruthy();
   const packageData = await umbracoApi.package.getByName(packageName);
   expect(packageData.dataTypes[0] == dataTypeId).toBeTruthy();
 
   // Clean
-  await umbracoApi.dictionary.ensureNameNotExists(dataTypeName);
+  await umbracoApi.dataType.ensureNameNotExists(dataTypeName);
 });
 
-// TODO: Remove .skip when the test is able to run. Currently waiting for button
 test('can create a package with templates', async ({umbracoApi, umbracoUi}) => {
   // Arrange
   const templateName = 'TestTemplate';
+  await umbracoApi.template.ensureNameNotExists(templateName);
   const templateId = await umbracoApi.template.createDefaultTemplate(templateName);
 
   // Act
   await umbracoUi.package.clickCreatePackageButton();
   await umbracoUi.package.enterPackageName(packageName);
   await umbracoUi.package.clickAddTemplatesToPackageButton();
-  await umbracoUi.package.clickCaretButton();
   await umbracoUi.package.clickLabelWithName(templateName);
-  await umbracoUi.package.clickSubmitButton()
+  await umbracoUi.package.clickChooseContainerButton();
   await umbracoUi.package.clickCreateButton();
 
   // Assert
-  await umbracoUi.package.isSuccessNotificationVisible()
-  await umbracoUi.package.clickCreatedTab();
-  await umbracoUi.package.clickExistingPackageName(packageName);
+  await umbracoUi.package.isSuccessNotificationVisible();
   expect(umbracoUi.package.isButtonWithNameVisible(templateName)).toBeTruthy();
   const packageData = await umbracoApi.package.getByName(packageName);
   expect(packageData.templates[0] == templateId).toBeTruthy();
@@ -290,25 +258,22 @@ test('can create a package with templates', async ({umbracoApi, umbracoUi}) => {
   await umbracoApi.template.ensureNameNotExists(templateName);
 });
 
-// TODO: Remove .skip when the test is able to run. Currently waiting for button
 test('can create a package with stylesheets', async ({umbracoApi, umbracoUi}) => {
   // Arrange
-  const stylesheetName = 'TestStylesheet';
+  const stylesheetName = 'TestStylesheet.css';
+  await umbracoApi.stylesheet.ensureNameNotExists(stylesheetName);
   const stylesheetId = await umbracoApi.stylesheet.createDefaultStylesheet(stylesheetName);
 
   // Act
   await umbracoUi.package.clickCreatePackageButton();
   await umbracoUi.package.enterPackageName(packageName);
   await umbracoUi.package.clickAddStylesheetToPackageButton();
-  await umbracoUi.package.clickCaretButton();
   await umbracoUi.package.clickLabelWithName(stylesheetName);
-  await umbracoUi.package.clickSubmitButton()
+  await umbracoUi.package.clickChooseContainerButton();
   await umbracoUi.package.clickCreateButton();
 
   // Assert
-  await umbracoUi.package.isSuccessNotificationVisible()
-  await umbracoUi.package.clickCreatedTab();
-  await umbracoUi.package.clickExistingPackageName(packageName);
+  await umbracoUi.package.isSuccessNotificationVisible();
   expect(umbracoUi.package.isButtonWithNameVisible(stylesheetName)).toBeTruthy();
   const packageData = await umbracoApi.package.getByName(packageName);
   expect(packageData.stylesheets[0] == stylesheetId).toBeTruthy();
@@ -317,25 +282,22 @@ test('can create a package with stylesheets', async ({umbracoApi, umbracoUi}) =>
   await umbracoApi.stylesheet.ensureNameNotExists(stylesheetName);
 });
 
-// TODO: Remove .skip when the test is able to run. Currently waiting for button
 test('can create a package with scripts', async ({umbracoApi, umbracoUi}) => {
   // Arrange
-  const scriptName = 'TestScripts';
+  const scriptName = 'TestScripts.js';
+  await umbracoApi.script.ensureNameNotExists(scriptName);
   const scriptId = await umbracoApi.script.createDefaultScript(scriptName);
 
   // Act
   await umbracoUi.package.clickCreatePackageButton();
   await umbracoUi.package.enterPackageName(packageName);
   await umbracoUi.package.clickAddScriptToPackageButton();
-  await umbracoUi.package.clickCaretButton();
   await umbracoUi.package.clickLabelWithName(scriptName);
-  await umbracoUi.package.clickSubmitButton()
+  await umbracoUi.package.clickChooseContainerButton();
   await umbracoUi.package.clickCreateButton();
 
   // Assert
-  await umbracoUi.package.isSuccessNotificationVisible()
-  await umbracoUi.package.clickCreatedTab();
-  await umbracoUi.package.clickExistingPackageName(packageName);
+  await umbracoUi.package.isSuccessNotificationVisible();
   expect(umbracoUi.package.isButtonWithNameVisible(scriptName)).toBeTruthy();
   const packageData = await umbracoApi.package.getByName(packageName);
   expect(packageData.scripts[0] == scriptId).toBeTruthy();
@@ -344,34 +306,29 @@ test('can create a package with scripts', async ({umbracoApi, umbracoUi}) => {
   await umbracoApi.script.ensureNameNotExists(scriptName);
 });
 
-// TODO: Remove .skip when the test is able to run. Currently waiting for button
 test('can create a package with partial views', async ({umbracoApi, umbracoUi}) => {
   // Arrange
-  const partialViewName = 'TestPartialView';
+  const partialViewName = 'TestPartialView.cshtml';
   const partialViewId = await umbracoApi.partialView.createDefaultPartialView(partialViewName);
 
   // Act
   await umbracoUi.package.clickCreatePackageButton();
   await umbracoUi.package.enterPackageName(packageName);
   await umbracoUi.package.clickAddPartialViewToPackageButton();
-  await umbracoUi.package.clickCaretButton();
   await umbracoUi.package.clickLabelWithName(partialViewName);
-  await umbracoUi.package.clickSubmitButton()
+  await umbracoUi.package.clickChooseContainerButton();
+  await umbracoUi.package.clickCreateButton();
 
   // Assert
-  await umbracoUi.package.isSuccessNotificationVisible()
-  await umbracoUi.package.clickCreatedTab();
-  await umbracoUi.package.clickExistingPackageName(packageName);
+  await umbracoUi.package.isSuccessNotificationVisible();
   expect(umbracoUi.package.isButtonWithNameVisible(partialViewName)).toBeTruthy();
   const packageData = await umbracoApi.package.getByName(packageName);
   expect(packageData.partialViews[0] == partialViewId).toBeTruthy();
 
   // Clean
-  await umbracoApi.package.ensureNameNotExists(packageName);
+  await umbracoApi.partialView.ensureNameNotExists(partialViewName);
 });
 
-// Currently you are not able to download a package
-//TODO: Remove skip when the frontend is ready
 test('can download a package', async ({umbracoApi, umbracoUi}) => {
   // Arrange
   const packageId = await umbracoApi.package.createEmptyPackage(packageName);
