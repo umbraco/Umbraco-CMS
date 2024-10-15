c0cad814e63bb7a9a2dc3c3cadda80206a8e70c5
Elitsa <elm@umbraco.dk>
2024-10-15 13:54:16 +0300

    Updating names

--- tests/Umbraco.Tests.Integration/Umbraco.Core/Services/DocumentNavigationServiceTests.MoveToRecycleBin.cs
+++ tests/Umbraco.Tests.Integration/Umbraco.Core/Services/DocumentNavigationServiceTests.MoveToRecycleBin.cs
@@ -6,7 +6,7 @@ namespace Umbraco.Cms.Tests.Integration.Umbraco.Core.Services;
 public partial class DocumentNavigationServiceTests
 {
     [Test]
-    public async Task Structure_Updates_When_Moving_Content_To_Recycle_Bin()
+    public async Task Parent_And_Descendants_Are_Updated_When_Content_Is_Moved_To_Recycle_Bin()
     {
         // Arrange
         Guid nodeToMoveToRecycleBin = Child3.Key;
@@ -19,7 +19,7 @@ public partial class DocumentNavigationServiceTests
         DocumentNavigationQueryService.TryGetDescendantsKeys(nodeToMoveToRecycleBin, out IEnumerable<Guid> initialDescendantsKeys);
         var beforeMoveDescendants = initialDescendantsKeys.ToList();
         DocumentNavigationQueryService.TryGetChildrenKeys(originalParentKey.Value, out IEnumerable<Guid> initialParentChildrenKeys);
-        var beforeMoveParentSiblingsCount = initialParentChildrenKeys.Count();
+        var beforeMoveParentChildrenCount = initialParentChildrenKeys.Count();
 
         // Act
         await ContentEditingService.MoveToRecycleBinAsync(nodeToMoveToRecycleBin, Constants.Security.SuperUserKey);
@@ -30,7 +30,7 @@ public partial class DocumentNavigationServiceTests
         DocumentNavigationQueryService.TryGetDescendantsKeysInBin(nodeToMoveToRecycleBin, out IEnumerable<Guid> afterMoveDescendantsKeys);
         var afterMoveDescendants = afterMoveDescendantsKeys.ToList();
         DocumentNavigationQueryService.TryGetChildrenKeys((Guid)originalParentKey, out IEnumerable<Guid> afterMoveParentChildrenKeys);
-        var afterMoveParentSiblingsCount = afterMoveParentChildrenKeys.Count();
+        var afterMoveParentChildrenCount = afterMoveParentChildrenKeys.Count();
         DocumentNavigationQueryService.TryGetSiblingsKeysInBin(nodeInRecycleBin, out IEnumerable<Guid> afterMoveRecycleBinSiblingsKeys);
         var afterMoveRecycleBinSiblingsCount = afterMoveRecycleBinSiblingsKeys.Count();
 
@@ -42,7 +42,7 @@ public partial class DocumentNavigationServiceTests
 
             Assert.IsNull(updatedParentKeyInRecycleBin); // Verify the node's parent is now located at the root of the recycle bin (null)
             Assert.AreEqual(beforeMoveDescendants, afterMoveDescendants);
-            Assert.AreEqual(beforeMoveParentSiblingsCount - 1, afterMoveParentSiblingsCount);
+            Assert.AreEqual(beforeMoveParentChildrenCount - 1, afterMoveParentChildrenCount);
             Assert.AreEqual(beforeMoveRecycleBinSiblingsCount + 1, afterMoveRecycleBinSiblingsCount);
         });
     }
