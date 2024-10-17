27d9ebea72d97831a5df09998c4bef52d47685ff
Nhu Dinh <hnd@umbraco.dk>
2024-10-17 10:12:19 +0700

    Added tests for rendering content with truefalse

---
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/RenderingContent/RenderingContentWithTrueFalse.spec.ts
@@ -0,0 +1,40 @@
+﻿import {AliasHelper, test} from '@umbraco/playwright-testhelpers';
+
+const contentName = 'Test Rendering Content';
+const documentTypeName = 'TestDocumentTypeForContent';
+const dataTypeName = 'True/false';
+const templateName = 'TestTemplateForContent';
+const propertyName = 'Test TrueFalse';
+let dataTypeData;
+
+test.beforeEach(async ({umbracoApi}) => {
+  dataTypeData = await umbracoApi.dataType.getByName(dataTypeName); 
+});
+
+test.afterEach(async ({umbracoApi}) => {
+  await umbracoApi.document.ensureNameNotExists(contentName); 
+  await umbracoApi.documentType.ensureNameNotExists(documentTypeName);
+  await umbracoApi.template.ensureNameNotExists(templateName);
+});
+
+const trueFalses = [
+  {type: 'true value ', value: true, expectedValue: 'True'},
+  {type: 'false value', value: false, expectedValue: 'False'},
+];
+
+for (const trueFalse of trueFalses) {
+  test(`can render content with ${trueFalse.type}`, async ({umbracoApi, umbracoUi}) => {
+    // Arrange
+    const templateId = await umbracoApi.template.createTemplateWithDisplayingStringValue(templateName, AliasHelper.toAlias(propertyName));
+    await umbracoApi.document.createPublishedDocumentWithValue(contentName, trueFalse.value, dataTypeData.id, templateId, propertyName, documentTypeName);
+    const contentData = await umbracoApi.document.getByName(contentName);
+    const contentURL = contentData.urls[0].url;
+
+    // Act
+    await umbracoUi.contentRender.navigateToRenderedContentPage(contentURL);
+
+    // Assert
+    await umbracoUi.contentRender.doesContentRenderValueHaveText(trueFalse.expectedValue);
+  });
+}
+
