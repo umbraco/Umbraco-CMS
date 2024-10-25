b24bedb93af93e55f3be31f184c409c37d07c331
Niels Lyngsø <niels.lyngso@gmail.com>
2024-10-25 15:14:32 +0200

    update to use == in JSON Path Queries

--- src/Umbraco.Web.UI.Client/src/packages/block/block/workspace/block-element-manager.ts
+++ src/Umbraco.Web.UI.Client/src/packages/block/block/workspace/block-element-manager.ts
@@ -49,7 +49,7 @@ export class UmbBlockElementManager extends UmbControllerBase {
 		this.observe(this.contentTypeId, (id) => this.structure.loadType(id));
 		this.observe(this.unique, (key) => {
 			if (key) {
-				this.validation.setDataPath('$.' + dataPathPropertyName + `[?(@.key = '${key}')]`);
+				this.validation.setDataPath('$.' + dataPathPropertyName + `[?(@.key == '${key}')]`);
 			}
 		});
 	}
