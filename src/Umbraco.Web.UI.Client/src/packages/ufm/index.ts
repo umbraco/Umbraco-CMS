e718367f15ec40913a7dced13529999d35eaa907
Niels Lyngsø <niels.lyngso@gmail.com>
2024-10-25 10:51:56 +0200

    move abstract classes out of types

--- src/Umbraco.Web.UI.Client/src/packages/ufm/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/ufm/index.ts
@@ -1,6 +1,7 @@
 export * from './components/index.js';
 export * from './contexts/index.js';
 export * from './controllers/ufm-virtual-render.controller.js';
+export * from './filters/base.filter.js';
 export * from './plugins/index.js';
 export * from './types.js';
 export * from './ufm-component.extension.js';
--- src/Umbraco.Web.UI.Client/src/packages/ufm/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/ufm/types.ts
@@ -1,7 +1,1 @@
-import type { UmbUfmFilterApi } from './ufm-filter.extension.js';
-
-// TODO: this is not a type, in TypeScript world, as it is an actual class. So it should be moved elsewhere [NL]
-export abstract class UmbUfmFilterBase implements UmbUfmFilterApi {
-	abstract filter(...args: Array<unknown>): string | undefined | null;
-	destroy() {}
-}
+export type * from './ufm-filter.extension.js';
