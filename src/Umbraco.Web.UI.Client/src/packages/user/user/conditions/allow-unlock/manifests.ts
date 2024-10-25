ef77734a5d09975a9611ef55ca53828f46201c40
Niels Lyngsø <niels.lyngso@gmail.com>
2024-10-25 09:41:33 +0200

    user conditions

--- src/Umbraco.Web.UI.Client/src/packages/user/user/conditions/allow-unlock/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/conditions/allow-unlock/manifests.ts
@@ -1,8 +1,10 @@
+import { UMB_USER_ALLOW_UNLOCK_CONDITION_ALIAS } from './constants.js';
+
 export const manifests: Array<UmbExtensionManifest> = [
 	{
 		type: 'condition',
 		name: 'User Allow Unlock Action Condition',
-		alias: 'Umb.Condition.User.AllowUnlockAction',
+		alias: UMB_USER_ALLOW_UNLOCK_CONDITION_ALIAS,
 		api: () => import('./user-allow-unlock-action.condition.js'),
 	},
 ];
