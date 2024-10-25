002b4baf473e8e505ff26fd777540be53b6a661f
Niels Lyngsø <niels.lyngso@gmail.com>
2024-10-25 10:41:11 +0200

    ufm types

--- src/Umbraco.Web.UI.Client/src/packages/ufm/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/ufm/types.ts
@@ -1,6 +1,6 @@
 import type { UmbUfmFilterApi } from './ufm-filter.extension.js';
 
-// TODO: This is not a type? So it should ideally be move to a different file. [NL]
+// TODO: this is not a type, in TypeScript world, as it is an actual class. So it should be moved elsewhere [NL]
 export abstract class UmbUfmFilterBase implements UmbUfmFilterApi {
 	abstract filter(...args: Array<unknown>): string | undefined | null;
 	destroy() {}
