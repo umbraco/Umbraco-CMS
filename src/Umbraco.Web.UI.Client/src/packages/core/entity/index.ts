4f0606eb8add4f59600026f1b48f7fb79b79dc6e
Mads Rasmussen <madsr@hey.com>
2024-10-28 15:11:43 +0100

    Update index.ts

--- src/Umbraco.Web.UI.Client/src/packages/core/entity/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/entity/index.ts
@@ -1,3 +1,4 @@
 export { UMB_ENTITY_CONTEXT } from './entity.context-token.js';
 export { UmbEntityContext } from './entity.context.js';
+export * from './create-option-action/index.js';
 export * from './types.js';
