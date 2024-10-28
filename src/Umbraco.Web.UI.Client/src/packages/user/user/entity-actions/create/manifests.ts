eb98f2dd65cc5d2cc427b8a2fd673494dac57d90
Mads Rasmussen <madsr@hey.com>
2024-10-28 15:08:23 +0100

    register create options

--- src/Umbraco.Web.UI.Client/src/packages/user/user/entity-actions/create/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/entity-actions/create/manifests.ts
@@ -1,4 +1,4 @@
-import { UMB_USER_ROOT_ENTITY_TYPE } from '../../entity.js';
+import { UMB_USER_ENTITY_TYPE, UMB_USER_ROOT_ENTITY_TYPE } from '../../entity.js';
 
 import { manifests as modalManifests } from './modal/manifests.js';
 
@@ -16,6 +16,32 @@ export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> =
 		meta: {
 			icon: 'icon-add',
 			label: '#actions_create',
+			additionalOptions: true,
+		},
+	},
+	{
+		type: 'entityCreateOptionAction',
+		kind: 'default',
+		alias: 'Umb.EntityCreateOptionAction.User.Default',
+		name: 'Default User Entity Create Option Action',
+		weight: 1200,
+		forEntityTypes: [UMB_USER_ENTITY_TYPE],
+		meta: {
+			icon: 'icon-add',
+			label: 'Default (Extension)',
+			additionalOptions: true,
+		},
+	},
+	{
+		type: 'entityCreateOptionAction',
+		kind: 'default',
+		alias: 'Umb.EntityCreateOptionAction.User.Api',
+		name: 'Api User Entity Create Option Action',
+		weight: 1200,
+		forEntityTypes: [UMB_USER_ENTITY_TYPE],
+		meta: {
+			icon: 'icon-add',
+			label: 'Api (Extension)',
 			additionalOptions: true,
 		},
 	},
