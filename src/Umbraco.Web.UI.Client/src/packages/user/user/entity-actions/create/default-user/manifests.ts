53c514e26f7bff2567c43205db4f532c5f7e83b5
Mads Rasmussen <madsr@hey.com>
2024-10-28 22:18:27 +0100

    register user entity create option actions

---
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/entity-actions/create/default-user/manifests.ts
@@ -0,0 +1,18 @@
+import { UMB_USER_ROOT_ENTITY_TYPE } from '../../../entity.js';
+
+export const manifests: Array<UmbExtensionManifest> = [
+	{
+		type: 'entityCreateOptionAction',
+		kind: 'default',
+		alias: 'Umb.EntityCreateOptionAction.User.Default',
+		name: 'Default User Entity Create Option Action',
+		weight: 1200,
+		api: () => import('./default-user-entity-create-option-action.js'),
+		forEntityTypes: [UMB_USER_ROOT_ENTITY_TYPE],
+		meta: {
+			icon: 'icon-user',
+			label: '#user_userKindDefault',
+			additionalOptions: true,
+		},
+	},
+];
