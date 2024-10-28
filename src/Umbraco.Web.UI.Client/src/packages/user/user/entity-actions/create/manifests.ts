ff73e5e5423edc7b12eb407f4e375f7351796775
Mads Rasmussen <madsr@hey.com>
2024-10-28 22:49:05 +0100

    setup common action and generic modal

--- src/Umbraco.Web.UI.Client/src/packages/user/user/entity-actions/create/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/entity-actions/create/manifests.ts
@@ -1,26 +1,17 @@
 import { UMB_USER_ROOT_ENTITY_TYPE } from '../../entity.js';
 import { manifests as apiUser } from './api-user/manifests.js';
 import { manifests as defaultUser } from './default-user/manifests.js';
-import { manifests as modalManifests } from './modal/manifests.js';
 
 import type { UmbExtensionManifestKind } from '@umbraco-cms/backoffice/extension-registry';
 
 export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> = [
 	{
 		type: 'entityAction',
-		kind: 'default',
+		kind: 'create',
 		alias: 'Umb.EntityAction.User.Create',
 		name: 'Create User Entity Action',
-		weight: 1200,
-		api: () => import('./create-user-entity-action.js'),
 		forEntityTypes: [UMB_USER_ROOT_ENTITY_TYPE],
-		meta: {
-			icon: 'icon-add',
-			label: '#actions_create',
-			additionalOptions: true,
-		},
 	},
 	...apiUser,
 	...defaultUser,
-	...modalManifests,
 ];
