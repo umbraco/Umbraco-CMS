87ea24eb39d9149effea5776ef508239c15c4d58
Mads Rasmussen <madsr@hey.com>
2024-10-28 17:47:41 +0100

    export manifests

--- src/Umbraco.Web.UI.Client/src/packages/core/entity/create-option-action/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/entity/create-option-action/manifests.ts
@@ -1,4 +1,4 @@
-import { manifests as defaultEntityActionManifests } from './default/manifests.js';
+import { manifests as defaultManifests } from './default/manifests.js';
 import type { UmbExtensionManifestKind } from '@umbraco-cms/backoffice/extension-registry';
 
-export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> = [...defaultEntityActionManifests];
+export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> = [...defaultManifests];
---
+++ src/Umbraco.Web.UI.Client/src/packages/core/entity/manifests.ts
@@ -0,0 +1,4 @@
+import { manifests as createOptionActionManifests } from './create-option-action/manifests.js';
+import type { UmbExtensionManifestKind } from '@umbraco-cms/backoffice/extension-registry';
+
+export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> = [...createOptionActionManifests];
