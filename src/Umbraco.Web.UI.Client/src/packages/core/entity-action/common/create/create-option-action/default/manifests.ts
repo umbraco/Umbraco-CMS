f93153a7aaaccbd257fdb522d4119f624a01c6fa
Mads Rasmussen <madsr@hey.com>
2024-10-29 22:02:22 +0100

    add default kind

---
+++ src/Umbraco.Web.UI.Client/src/packages/core/entity-action/common/create/create-option-action/default/manifests.ts
@@ -0,0 +1,18 @@
+export const manifests = [
+	{
+		type: 'kind',
+		alias: 'Umb.Kind.EntityCreateOptionAction.Default',
+		matchKind: 'default',
+		matchType: 'entityCreateOptionAction',
+		manifest: {
+			type: 'entityCreateOptionAction',
+			kind: 'default',
+			weight: 1000,
+			meta: {
+				icon: '',
+				label: 'Fill out label',
+				description: 'Fill out description',
+			},
+		},
+	},
+];
--- src/Umbraco.Web.UI.Client/src/packages/core/entity-action/common/create/create-option-action/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/entity-action/common/create/create-option-action/index.ts
@@ -1,3 +1,4 @@
+export * from './default/index.js';
 export * from './entity-create-option-action-base.js';
 export * from './entity-create-option-action.extension.js';
 export * from './entity-create-option-action.interface.js';
