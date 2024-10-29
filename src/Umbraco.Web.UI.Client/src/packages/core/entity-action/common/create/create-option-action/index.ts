311cdc4d1dbb1edb5f3b62a833f3ac2b4d129185
Mads Rasmussen <madsr@hey.com>
2024-10-29 20:00:45 +0100

    render as ref item

--- src/Umbraco.Web.UI.Client/src/packages/core/entity-action/common/create/create-option-action/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/entity-action/common/create/create-option-action/index.ts
@@ -1,6 +1,3 @@
-import './global-components/index.js';
-
-export * from './global-components/index.js';
 export * from './entity-create-option-action-base.js';
 export * from './entity-create-option-action.extension.js';
 export * from './entity-create-option-action.interface.js';
