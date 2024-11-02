2f8fba9512cae3c537beb0597dde79db71cc6014
Mads Rasmussen <madsr@hey.com>
2024-11-02 21:00:48 +0100

    add create collection action kind

--- src/Umbraco.Web.UI.Client/src/packages/core/collection/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/collection/manifests.ts
@@ -1,9 +1,11 @@
 import type { UmbExtensionManifestKind } from '../extension-registry/registry.js';
 import { manifest as collectionAliasCondition } from './collection-alias.manifest.js';
 import { manifest as collectionBulkActionPermissionCondition } from './collection-bulk-action-permission.manifest.js';
+import { manifests as actionManifests } from './action/manifests.js';
 import { manifests as workspaceViewManifests } from './workspace-view/manifests.js';
 
 export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> = [
+	...actionManifests,
 	...workspaceViewManifests,
 	collectionAliasCondition,
 	collectionBulkActionPermissionCondition,
