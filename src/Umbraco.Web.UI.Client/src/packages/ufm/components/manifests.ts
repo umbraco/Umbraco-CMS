53592b019b8c6dc6b01d0bb687a3d34098a6c04a
Lee Kelleher <leekelleher@users.noreply.github.com>
2024-11-04 13:28:51 +0000

    Hotfix: UFM alias prefixes (#2510)
    * Adds `alias` to UFM component registrations
    
    * Adds support for `alias` prefix in UFM syntax
    
    e.g. `{(alias:|marker)(text)}` would be `{umbContentName: propertyAlias}` or `{~propertyAlias}`
    
    * UFM Content Name: adds warning for using the `~` prefix marker
    
    The `~` prefix will be deprecated in a future major release.

--- src/Umbraco.Web.UI.Client/src/packages/ufm/components/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/ufm/components/manifests.ts
@@ -6,20 +6,20 @@ export const manifests: Array<ManifestUfmComponent> = [
 		alias: 'Umb.Markdown.LabelValue',
 		name: 'Label Value UFM Component',
 		api: () => import('./label-value/label-value.component.js'),
-		meta: { marker: '=' },
+		meta: { alias: 'umbValue', marker: '=' },
 	},
 	{
 		type: 'ufmComponent',
 		alias: 'Umb.Markdown.Localize',
 		name: 'Localize UFM Component',
 		api: () => import('./localize/localize.component.js'),
-		meta: { marker: '#' },
+		meta: { alias: 'umbLocalize', marker: '#' },
 	},
 	{
 		type: 'ufmComponent',
 		alias: 'Umb.Markdown.ContentName',
 		name: 'Content Name UFM Component',
 		api: () => import('./content-name/content-name.component.js'),
-		meta: { marker: '~' },
+		meta: { alias: 'umbContentName', marker: '~' },
 	},
 ];
