e0fca6fa7bac84dd02205d67e839e4e705013302
Niels Lyngsø <niels.lyngso@gmail.com>
2024-11-04 12:39:31 +0100

    type export

--- src/Umbraco.Web.UI.Client/src/packages/core/entity-create-option-action/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/entity-create-option-action/index.ts
@@ -1,4 +1,4 @@
 export * from './entity-create-option-action-base.js';
 export * from './entity-create-option-action.extension.js';
 export * from './entity-create-option-action.interface.js';
-export * from './types.js';
+export type * from './types.js';
