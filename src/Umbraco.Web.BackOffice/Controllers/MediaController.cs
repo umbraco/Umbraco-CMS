umbraco/Umbraco-CMS

commit 311d322129d6db5ed5c3f1be98ee58e4e35ed2d4

Add code infrastructure to validate file content (#14657)
* Implemented modular architecture for filestream security sanitization with an svg-html example

* 31440: Refactoring, applied to more entry points and removed test analyzer

* 31440 Added Unittests for FileStreamSecurityValidator

* PR fixes and better unittest mock names

---------

Co-authored-by: Sven Geusens <sge@umbraco.dk>

src/Umbraco.Web.BackOffice/Controllers/MediaController.cs
@@ -5,6 +5,7 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.Infrastructure;
+using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Microsoft.Extensions.Primitives;
@@ -50,6 +51,7 @@ namespace Umbraco.Cms.Web.BackOffice.Controllers;
 public class MediaController : ContentControllerBase
 {
     private readonly AppCaches _appCaches;
+    private readonly IFileStreamSecurityValidator? _fileStreamSecurityValidator; // make non nullable in v14
     private readonly IAuthorizationService _authorizationService;
     private readonly IBackOfficeSecurityAccessor _backofficeSecurityAccessor;
     private readonly ContentSettings _contentSettings;
@@ -66,6 +68,58 @@ public class MediaController : ContentControllerBase
     private readonly ISqlContext _sqlContext;
     private readonly IUmbracoMapper _umbracoMapper;
 
+    [ActivatorUtilitiesConstructor]
+    public MediaController(
+        ICultureDictionary cultureDictionary,
+        ILoggerFactory loggerFactory,
+        IShortStringHelper shortStringHelper,
+        IEventMessagesFactory eventMessages,
+        ILocalizedTextService localizedTextService,
+        IOptionsSnapshot<ContentSettings> contentSettings,
+        IMediaTypeService mediaTypeService,
+        IMediaService mediaService,
+        IEntityService entityService,
+        IBackOfficeSecurityAccessor backofficeSecurityAccessor,
+        IUmbracoMapper umbracoMapper,
+        IDataTypeService dataTypeService,
+        ISqlContext sqlContext,
+        IContentTypeBaseServiceProvider contentTypeBaseServiceProvider,
+        IRelationService relationService,
+        PropertyEditorCollection propertyEditors,
+        MediaFileManager mediaFileManager,
+        MediaUrlGeneratorCollection mediaUrlGenerators,
+        IHostingEnvironment hostingEnvironment,
+        IImageUrlGenerator imageUrlGenerator,
+        IJsonSerializer serializer,
+        IAuthorizationService authorizationService,
+        AppCaches appCaches,
+        IFileStreamSecurityValidator streamSecurityValidator)
+        : base(cultureDictionary, loggerFactory, shortStringHelper, eventMessages, localizedTextService, serializer)
+    {
+        _shortStringHelper = shortStringHelper;
+        _contentSettings = contentSettings.Value;
+        _mediaTypeService = mediaTypeService;
+        _mediaService = mediaService;
+        _entityService = entityService;
+        _backofficeSecurityAccessor = backofficeSecurityAccessor;
+        _umbracoMapper = umbracoMapper;
+        _dataTypeService = dataTypeService;
+        _localizedTextService = localizedTextService;
+        _sqlContext = sqlContext;
+        _contentTypeBaseServiceProvider = contentTypeBaseServiceProvider;
+        _relationService = relationService;
+        _propertyEditors = propertyEditors;
+        _mediaFileManager = mediaFileManager;
+        _mediaUrlGenerators = mediaUrlGenerators;
+        _hostingEnvironment = hostingEnvironment;
+        _logger = loggerFactory.CreateLogger<MediaController>();
+        _imageUrlGenerator = imageUrlGenerator;
+        _authorizationService = authorizationService;
+        _appCaches = appCaches;
+        _fileStreamSecurityValidator = streamSecurityValidator;
+    }
+
+    [Obsolete("Use constructor overload that has fileStreamSecurityValidator, scheduled for removal in v14")]
     public MediaController(
         ICultureDictionary cultureDictionary,
         ILoggerFactory loggerFactory,
@@ -724,6 +778,17 @@ public class MediaController : ContentControllerBase
                 continue;
             }
 
+            using var stream = new MemoryStream();
+            await formFile.CopyToAsync(stream);
+            if (_fileStreamSecurityValidator != null && _fileStreamSecurityValidator.IsConsideredSafe(stream) == false)
+            {
+                tempFiles.Notifications.Add(new BackOfficeNotification(
+                    _localizedTextService.Localize("speechBubbles", "operationFailedHeader"),
+                    _localizedTextService.Localize("media", "fileSecurityValidationFailure"),
+                    NotificationStyle.Warning));
+                continue;
+            }
+
             if (string.IsNullOrEmpty(mediaTypeAlias))
             {
                 mediaTypeAlias = Constants.Conventions.MediaTypes.File;
@@ -801,11 +866,8 @@ public class MediaController : ContentControllerBase
             IMedia createdMediaItem = _mediaService.CreateMedia(mediaItemName, parentId.Value, mediaTypeAlias,
                 _backofficeSecurityAccessor.BackOfficeSecurity?.CurrentUser?.Id ?? -1);
 
-            await using (Stream stream = formFile.OpenReadStream())
-            {
                 createdMediaItem.SetValue(_mediaFileManager, _mediaUrlGenerators, _shortStringHelper,
                     _contentTypeBaseServiceProvider, Constants.Conventions.Media.File, fileName, stream);
-            }
 
             Attempt<OperationResult?> saveResult = _mediaService.Save(createdMediaItem,
                 _backofficeSecurityAccessor.BackOfficeSecurity?.CurrentUser?.Id ?? -1);

src/Umbraco.Core/DependencyInjection/UmbracoBuilder.cs
@@ -327,6 +327,9 @@ namespace Umbraco.Cms.Core.DependencyInjection
             Services.AddUnique<ICultureImpactFactory>(provider => new CultureImpactFactory(provider.GetRequiredService<IOptionsMonitor<ContentSettings>>()));
             Services.AddUnique<IDictionaryService, DictionaryService>();
             Services.AddUnique<ITemporaryMediaService, TemporaryMediaService>();
+
+            // Register filestream security analyzers
+            Services.AddUnique<IFileStreamSecurityValidator,FileStreamSecurityValidator>();
         }
     }
 }

src/Umbraco.Core/EmbeddedResources/Lang/en.xml
@@ -341,6 +341,7 @@
     <key alias="createFolderFailed">Failed to create a folder under parent id %0%</key>
     <key alias="renameFolderFailed">Failed to rename the folder with id %0%</key>
     <key alias="dragAndDropYourFilesIntoTheArea">Drag and drop your file(s) into the area</key>
+    <key alias="fileSecurityValidationFailure">One or more file security validations have failed</key>
   </area>
   <area alias="member">
     <key alias="createNewMember">Create a new member</key>

src/Umbraco.Core/EmbeddedResources/Lang/en_us.xml
@@ -352,6 +352,7 @@
     <key alias="renameFolderFailed">Failed to rename the folder with id %0%</key>
     <key alias="dragAndDropYourFilesIntoTheArea">Drag and drop your file(s) into the area</key>
     <key alias="uploadNotAllowed">Upload is not allowed in this location.</key>
+    <key alias="fileSecurityValidationFailure">One or more file security validations have failed</key>
   </area>
   <area alias="member">
     <key alias="createNewMember">Create a new member</key>

src/Umbraco.Core/EmbeddedResources/Lang/nl.xml
@@ -340,6 +340,7 @@
     <key alias="renameFolderFailed">Kan de map met id %0% niet hernoemen</key>
     <key alias="dragAndDropYourFilesIntoTheArea">Sleep en zet je bestand(en) neer in dit gebied</key>
     <key alias="uploadNotAllowed">Upload is niet toegelaten in deze locatie.</key>
+    <key alias="fileSecurityValidationFailure">Een of meerdere veiligheid validaties zijn gefaald voor het bestand</key>
   </area>
   <area alias="member">
     <key alias="createNewMember">Maak nieuw lid aan</key>

src/Umbraco.Core/Security/FileStreamSecurityValidator.cs
@@ -1,0 +1,38 @@
+namespace Umbraco.Cms.Core.Security;
+
+public class FileStreamSecurityValidator : IFileStreamSecurityValidator
+{
+    private readonly IEnumerable<IFileStreamSecurityAnalyzer> _fileAnalyzers;
+
+    public FileStreamSecurityValidator(IEnumerable<IFileStreamSecurityAnalyzer> fileAnalyzers)
+    {
+        _fileAnalyzers = fileAnalyzers;
+    }
+
+    /// <summary>
+    /// Analyzes whether the file content is considered safe with registered IFileStreamSecurityAnalyzers
+    /// </summary>
+    /// <param name="fileStream">Needs to be a Read seekable stream</param>
+    /// <returns>Whether the file is considered safe after running the necessary analyzers</returns>
+    public bool IsConsideredSafe(Stream fileStream)
+    {
+        foreach (var fileAnalyzer in _fileAnalyzers)
+        {
+            fileStream.Seek(0, SeekOrigin.Begin);
+            if (!fileAnalyzer.ShouldHandle(fileStream))
+            {
+                continue;
+            }
+
+            fileStream.Seek(0, SeekOrigin.Begin);
+            if (fileAnalyzer.IsConsideredSafe(fileStream) == false)
+            {
+                return false;
+            }
+        }
+        fileStream.Seek(0, SeekOrigin.Begin);
+        // If no analyzer we consider the file to be safe as the implementer has the possibility to add additional analyzers
+        // Or all analyzers deem te file to be safe
+        return true;
+    }
+}

src/Umbraco.Core/Security/IFileStreamSecurityAnalyzer.cs
@@ -1,0 +1,20 @@
+namespace Umbraco.Cms.Core.Security;
+
+public interface IFileStreamSecurityAnalyzer
+{
+
+    /// <summary>
+    /// Indicates whether the analyzer should process the file
+    /// The implementation should be considerably faster than IsConsideredSafe
+    /// </summary>
+    /// <param name="fileStream"></param>
+    /// <returns></returns>
+    bool ShouldHandle(Stream fileStream);
+
+    /// <summary>
+    /// Analyzes whether the file content is considered safe
+    /// </summary>
+    /// <param name="fileStream">Needs to be a Read/Write seekable stream</param>
+    /// <returns>Whether the file is considered safe</returns>
+    bool IsConsideredSafe(Stream fileStream);
+}

src/Umbraco.Core/Security/IFileStreamSecurityValidator.cs
@@ -1,0 +1,11 @@
+namespace Umbraco.Cms.Core.Security;
+
+public interface IFileStreamSecurityValidator
+{
+    /// <summary>
+    /// Analyzes wether the file content is considered safe with registered IFileStreamSecurityAnalyzers
+    /// </summary>
+    /// <param name="fileStream">Needs to be a Read seekable stream</param>
+    /// <returns>Whether the file is considered safe after running the necessary analyzers</returns>
+    bool IsConsideredSafe(Stream fileStream);
+}

commit 2745834a5fcc554e9ee75555677512761cce225c

Fix out of memory of 2gb+ (max 4gb) error introduced by #14657 SVG xss security fix

src/Umbraco.Web.BackOffice/Controllers/MediaController.cs
@@ -788,8 +788,7 @@ public class MediaController : ContentControllerBase
                     continue;
                 }
 
-                using var stream = new MemoryStream();
-                await formFile.CopyToAsync(stream);
+                await using var stream = formFile.OpenReadStream();
                 if (_fileStreamSecurityValidator != null && _fileStreamSecurityValidator.IsConsideredSafe(stream) == false)
                 {
                     tempFiles.Notifications.Add(new BackOfficeNotification(