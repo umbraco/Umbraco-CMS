b7ac34539beed8ceba793a8e78088db48f5651e2
Sven Geusens <sge@umbraco.dk>
2024-11-06 14:42:04 +0100

    Made some membertype endpoints available for member related actions

--- src/Umbraco.Web.Common/Authorization/AuthorizationPolicies.cs
+++ src/Umbraco.Web.Common/Authorization/AuthorizationPolicies.cs
@@ -53,6 +53,7 @@ public static class AuthorizationPolicies
     public const string TreeAccessMediaOrMediaTypes = nameof(TreeAccessMediaOrMediaTypes);
     public const string TreeAccessDictionaryOrTemplates = nameof(TreeAccessDictionaryOrTemplates);
     public const string TreeAccessDocumentOrMediaOrContentTypes = nameof(TreeAccessDocumentOrMediaOrContentTypes);
+    public const string TreeAccessMembersOrMemberTypes = nameof(TreeAccessMembersOrMemberTypes);
 
     // other
     public const string DictionaryPermissionByResource = nameof(DictionaryPermissionByResource);
