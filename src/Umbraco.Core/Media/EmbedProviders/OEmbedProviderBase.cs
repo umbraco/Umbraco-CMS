998bf4facb527c6adf4a671966f59c59786890ce
Zeegaan <skrivdetud@gmail.com>
2024-11-07 10:56:46 +0100

    Remove obsolete methods from OEmbedProviderBase.cs

--- src/Umbraco.Core/Media/EmbedProviders/OEmbedProviderBase.cs
+++ src/Umbraco.Core/Media/EmbedProviders/OEmbedProviderBase.cs
@@ -59,22 +59,6 @@ public abstract class OEmbedProviderBase : IEmbedProvider
         return fullUrl.ToString();
     }
 
-    [Obsolete("Use DownloadResponseAsync instead. This will be removed in Umbraco 15.")]
-    public virtual string DownloadResponse(string url)
-    {
-        if (_httpClient == null)
-        {
-            _httpClient = new HttpClient();
-            _httpClient.DefaultRequestHeaders.UserAgent.TryParseAdd(Constants.HttpClients.Headers.UserAgentProductName);
-        }
-
-        using (var request = new HttpRequestMessage(HttpMethod.Get, url))
-        {
-            using HttpResponseMessage response = _httpClient.SendAsync(request).GetAwaiter().GetResult();
-            return response.Content.ReadAsStringAsync().Result;
-        }
-    }
-
     public virtual async Task<string> DownloadResponseAsync(string url, CancellationToken cancellationToken)
     {
         if (_httpClient == null)
@@ -90,14 +74,6 @@ public abstract class OEmbedProviderBase : IEmbedProvider
         }
     }
 
-    [Obsolete("Use GetJsonResponseAsync instead. This will be removed in Umbraco 15.")]
-    public virtual T? GetJsonResponse<T>(string url)
-        where T : class
-    {
-        var response = DownloadResponse(url);
-        return _jsonSerializer.Deserialize<T>(response);
-    }
-
     public virtual async Task<T?> GetJsonResponseAsync<T>(string url, CancellationToken cancellationToken)
         where T : class
     {
@@ -108,16 +84,6 @@ public abstract class OEmbedProviderBase : IEmbedProvider
     public virtual async Task<XmlDocument> GetXmlResponseAsync(string url, CancellationToken cancellationToken)
     {
         var response =  await DownloadResponseAsync(url, cancellationToken);
-        var doc = new XmlDocument();
-        doc.LoadXml(response);
-
-        return doc;
-    }
-
-    [Obsolete("Use GetXmlResponseAsync instead. This will be removed in Umbraco 15.")]
-    public virtual XmlDocument GetXmlResponse(string url)
-    {
-        var response = DownloadResponse(url);
         var doc = new XmlDocument();
         doc.LoadXml(response);
 
