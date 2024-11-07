d91292267d5fa47ecc1f887fa0dd4cc374572927
Mads Rasmussen <madsr@hey.com>
2024-11-07 11:20:01 +0100

    remove circular dependency + deprecate export

--- src/Umbraco.Web.UI.Client/src/packages/core/content/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/content/index.ts
@@ -1,6 +1,8 @@
 export { UMB_CONTENT_PROPERTY_CONTEXT } from './content-property.context-token.js';
 export { UmbContentPropertyContext } from './content-property.context.js';
+
 export * from './collection/index.js';
+export * from './components/index.js';
 export * from './constants.js';
 export * from './controller/merge-content-variant-data.controller.js';
 export * from './manager/index.js';
