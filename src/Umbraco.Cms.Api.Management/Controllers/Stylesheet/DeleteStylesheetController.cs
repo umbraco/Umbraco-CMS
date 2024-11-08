868beea0dd1f13f5810dd91f3735d0091416e57f
Sven Geusens <sge@umbraco.dk>
2024-11-08 12:18:13 +0100

    Made some stylesheet endpoints available for document/media/member related actions (#17442)

--- src/Umbraco.Cms.Api.Management/Controllers/Stylesheet/DeleteStylesheetController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/Stylesheet/DeleteStylesheetController.cs
@@ -1,14 +1,17 @@
 ﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Api.Management.Extensions;
 using Umbraco.Cms.Core.Security;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Core.Services.OperationStatus;
+using Umbraco.Cms.Web.Common.Authorization;
 
 namespace Umbraco.Cms.Api.Management.Controllers.Stylesheet;
 
 [ApiVersion("1.0")]
+[Authorize(Policy = AuthorizationPolicies.TreeAccessStylesheets)]
 public class DeleteStylesheetController : StylesheetControllerBase
 {
     private readonly IStylesheetService _stylesheetService;
