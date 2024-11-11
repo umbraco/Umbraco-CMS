14ef1e7bd7ce8567b09c503b5f13a25f342fe10c
Mads Rasmussen <madsr@hey.com>
2024-11-11 14:26:55 +0100

    wip entity-detail-not-found

---
+++ src/Umbraco.Web.UI.Client/src/packages/core/workspace/entity-detail/entity-detail-not-found.element.ts
@@ -0,0 +1,47 @@
+import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
+import { css, html, customElement } from '@umbraco-cms/backoffice/external/lit';
+import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
+
+@customElement('umb-entity-detail-not-found')
+export class UmbEntityDetailNotFoundElement extends UmbLitElement {
+	override render() {
+		return html`
+			<div class="uui-text">
+				<h4><umb-localize key="routing_routeNotFoundTitle"></umb-localize></h4>
+				<umb-localize key="routing_routeNotFoundDescription"></umb-localize>
+			</div>
+		`;
+	}
+
+	static override styles = [
+		UmbTextStyles,
+		css`
+			:host {
+				display: block;
+				width: 100%;
+				height: 100%;
+				min-width: 0;
+			}
+
+			:host > div {
+				display: flex;
+				flex-direction: column;
+				justify-content: center;
+				align-items: center;
+				height: 100%;
+			}
+
+			@keyframes fadeIn {
+				100% {
+					opacity: 100%;
+				}
+			}
+		`,
+	];
+}
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'umb-entity-detail-not-found': UmbEntityDetailNotFoundElement;
+	}
+}
