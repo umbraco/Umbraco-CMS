d9c5825114924bb9e4b029072ecc8735157705b2
Kaspar Boel Kjeldsen <kaspar.kjeldsen@knowit.dk>
2024-11-11 12:40:10 +0100

    Documentation

--- src/Umbraco.Infrastructure/Mail/Interfaces/IEmailSenderClient.cs
+++ src/Umbraco.Infrastructure/Mail/Interfaces/IEmailSenderClient.cs
@@ -7,8 +7,16 @@ using MimeKit;
 
 namespace Umbraco.Cms.Infrastructure.Mail.Interfaces
 {
+    /// <summary>
+    /// Client for sending an email from a MimeMessage
+    /// </summary>
     public interface IEmailSenderClient
     {
+        /// <summary>
+        /// Sends the email message
+        /// </summary>
+        /// <param name="message"></param>
+        /// <returns></returns>
         public Task SendAsync(MimeMessage? message);
     }
 }
