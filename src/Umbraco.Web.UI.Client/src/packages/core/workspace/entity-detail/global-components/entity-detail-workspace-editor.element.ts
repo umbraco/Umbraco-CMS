be8ab4e3b20e9d5bf18b6163c89c8624da0d67e5
Mads Rasmussen <madsr@hey.com>
2024-11-11 21:01:05 +0100

    render error element

--- src/Umbraco.Web.UI.Client/src/packages/core/workspace/entity-detail/global-components/entity-detail-workspace-editor.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/workspace/entity-detail/global-components/entity-detail-workspace-editor.element.ts
@@ -7,20 +7,30 @@ export class UmbEntityDetailWorkspaceEditorElement extends UmbLitElement {
 	#context?: typeof UMB_ENTITY_DETAIL_WORKSPACE_CONTEXT.TYPE;
 
 	@state()
+	private _entityType = '';
+
+	@state()
 	private _isLoading = false;
+
+	@state()
+	private _exists = false;
 
 	constructor() {
 		super();
 
 		this.consumeContext(UMB_ENTITY_DETAIL_WORKSPACE_CONTEXT, (context) => {
 			this.#context = context;
-			this.observe(this.#context.loading.isOn, (isLoading) => {
-				this._isLoading = isLoading;
-			});
+			this.observe(this.#context?.entityType, (entityType) => (this._entityType = entityType));
+			this.observe(this.#context?.loading.isOn, (isLoading) => (this._isLoading = isLoading));
+			this.observe(this.#context?.data, (data) => (this._exists = !!data));
 		});
 	}
 
 	protected override render() {
+		if (!this._exists && !this._isLoading) {
+			return html`<umb-entity-detail-not-found entity-type=${this._entityType}></umb-entity-detail-not-found>`;
+		}
+
 		return html`<umb-workspace-editor ?loading=${this._isLoading}>
 			<slot name="header" slot="header"></slot>
 		</umb-workspace-editor>`;
