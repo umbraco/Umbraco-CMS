22bfae8c3b41f2be145ad09b180b9fbde574b730
Mads Rasmussen <madsr@hey.com>
2024-11-12 21:55:42 +0100

    add todo

--- src/Umbraco.Web.UI.Client/src/packages/core/workspace/workspace-context.interface.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/workspace/workspace-context.interface.ts
@@ -1,6 +1,8 @@
 import type { UmbApi } from '@umbraco-cms/backoffice/extension-api';
+import type { Observable } from '@umbraco-cms/backoffice/observable-api';
 
 export interface UmbWorkspaceContext extends UmbApi {
 	readonly workspaceAlias: string;
 	getEntityType(): string;
+	// TODO: add entityType observable
 }
