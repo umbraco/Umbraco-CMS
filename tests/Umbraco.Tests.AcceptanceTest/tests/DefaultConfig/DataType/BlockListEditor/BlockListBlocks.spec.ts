6c3b46a6d8f71595061b42b906cf0c3732b7cc8b
Nhu Dinh <hnd@umbraco.dk>
2024-11-12 13:16:16 +0700

    Fixed failing tests for Content Picker, Block setting due to UI changes

--- tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/DataType/BlockListEditor/BlockListBlocks.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/DataType/BlockListEditor/BlockListBlocks.spec.ts
@@ -104,7 +104,7 @@ test('can open content model in a block', async ({umbracoApi, umbracoUi}) => {
   // Act
   await umbracoUi.dataType.goToDataType(blockListEditorName);
   await umbracoUi.dataType.goToBlockWithName(elementTypeName);
-  await umbracoUi.dataType.openBlockContentModel();
+  await umbracoUi.dataType.clickReferenceNodeLinkWithName(elementTypeName);
 
   // Assert
   await umbracoUi.dataType.isElementWorkspaceOpenInBlock(elementTypeName);
