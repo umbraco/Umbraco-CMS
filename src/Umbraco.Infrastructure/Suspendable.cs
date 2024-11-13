54f2d29abc7fa10c6ae315256833ffe2ca13471a
Ronald Barendse <ronald@barend.se>
2024-11-13 09:56:54 +0100

    Obsolete PageCacheRefresher

--- src/Umbraco.Infrastructure/Suspendable.cs
+++ src/Umbraco.Infrastructure/Suspendable.cs
@@ -7,52 +7,18 @@ namespace Umbraco.Cms.Infrastructure;
 
 public static class Suspendable
 {
+    [Obsolete("The PageCacheRefresher is not used anymore and will be removed in a future version.")]
     public static class PageCacheRefresher
     {
-        private static bool _tried;
-        private static bool _suspended;
+        public static bool CanRefreshDocumentCacheFromDatabase => false;
 
-        public static bool CanRefreshDocumentCacheFromDatabase
-        {
-            get
-            {
-                // trying a full refresh
-                if (_suspended == false)
-                {
-                    return true;
-                }
-
-                _tried = true; // remember we tried
-                return false;
-            }
-        }
-
-        // trying a partial update
-        // ok if not suspended, or if we haven't done a full already
-        public static bool CanUpdateDocumentCache => _suspended == false || _tried == false;
+        public static bool CanUpdateDocumentCache => true;
 
         public static void SuspendDocumentCache()
-        {
-            StaticApplicationLogging.Logger.LogInformation("Suspend document cache.");
-            _suspended = true;
-        }
+        { }
 
         public static void ResumeDocumentCache(CacheRefresherCollection cacheRefresherCollection)
-        {
-            _suspended = false;
-
-            StaticApplicationLogging.Logger.LogInformation("Resume document cache (reload:{Tried}).", _tried);
-
-            if (_tried == false)
-            {
-                return;
-            }
-
-            _tried = false;
-
-            ICacheRefresher? pageRefresher = cacheRefresherCollection[ContentCacheRefresher.UniqueId];
-            pageRefresher?.RefreshAll();
-        }
+        { }
     }
 
     // This is really needed at all since the only place this is used is in ExamineComponent and that already maintains a flag of whether it suspsended or not
