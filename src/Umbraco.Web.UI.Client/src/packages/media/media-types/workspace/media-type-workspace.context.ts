7fde527ad38ce9cc9bc60fd325937e4ed10b29ad
Mads Rasmussen <madsr@hey.com>
2024-11-14 14:47:43 +0100

    clean up

--- src/Umbraco.Web.UI.Client/src/packages/media/media-types/workspace/media-type-workspace.context.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media-types/workspace/media-type-workspace.context.ts
@@ -1,31 +1,17 @@
-import { UmbMediaTypeDetailRepository } from '../repository/detail/media-type-detail.repository.js';
 import { UMB_MEDIA_TYPE_ENTITY_TYPE } from '../entity.js';
 import type { UmbMediaTypeDetailModel } from '../types.js';
 import { UmbMediaTypeWorkspaceEditorElement } from './media-type-workspace-editor.element.js';
 import {
-	UmbSubmittableWorkspaceContextBase,
 	type UmbRoutableWorkspaceContext,
 	UmbWorkspaceIsNewRedirectController,
 } from '@umbraco-cms/backoffice/workspace';
-import {
-	UmbContentTypeStructureManager,
-	UmbContentTypeWorkspaceContextBase,
-} from '@umbraco-cms/backoffice/content-type';
-import { UmbObjectState } from '@umbraco-cms/backoffice/observable-api';
-import type {
-	UmbContentTypeCompositionModel,
-	UmbContentTypeSortModel,
-	UmbContentTypeWorkspaceContext,
-} from '@umbraco-cms/backoffice/content-type';
+import { UmbContentTypeWorkspaceContextBase } from '@umbraco-cms/backoffice/content-type';
+import type { UmbContentTypeSortModel, UmbContentTypeWorkspaceContext } from '@umbraco-cms/backoffice/content-type';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import type { UmbReferenceByUnique } from '@umbraco-cms/backoffice/models';
-import { UMB_ACTION_EVENT_CONTEXT } from '@umbraco-cms/backoffice/action';
-import {
-	UmbRequestReloadChildrenOfEntityEvent,
-	UmbRequestReloadStructureForEntityEvent,
-} from '@umbraco-cms/backoffice/entity-action';
 import { UMB_MEDIA_TYPE_WORKSPACE_ALIAS } from './constants.js';
 import { UMB_MEDIA_TYPE_DETAIL_REPOSITORY_ALIAS } from '../repository/index.js';
+import type { UmbEntityModel } from '@umbraco-cms/backoffice/entity';
 
 type DetailModelType = UmbMediaTypeDetailModel;
 export class UmbMediaTypeWorkspaceContext
@@ -46,7 +32,8 @@ export class UmbMediaTypeWorkspaceContext
 				setup: async (_component, info) => {
 					const parentEntityType = info.match.params.parentEntityType;
 					const parentUnique = info.match.params.parentUnique === 'null' ? null : info.match.params.parentUnique;
-					await this.create({ entityType: parentEntityType, unique: parentUnique });
+					const parent: UmbEntityModel = { entityType: parentEntityType, unique: parentUnique };
+					await this.createScaffold({ parent });
 
 					new UmbWorkspaceIsNewRedirectController(
 						this,
@@ -86,72 +73,17 @@ export class UmbMediaTypeWorkspaceContext
 		this.structure.updateOwnerContentType({ allowedContentTypes });
 	}
 
-	setCompositions(compositions: Array<UmbContentTypeCompositionModel>) {
-		this.structure.updateOwnerContentType({ compositions });
-	}
-
 	setCollection(collection: UmbReferenceByUnique) {
 		this.structure.updateOwnerContentType({ collection });
 	}
 
-	async create(parent: { entityType: string; unique: string | null }) {
-		this.resetState();
-		this.#parent.setValue(parent);
-		const { data } = await this.structure.createScaffold();
-		if (!data) return undefined;
-
-		this.setIsNew(true);
-		this.#persistedData.setValue(data);
-		return data;
-	}
-
-	async load(unique: string) {
-		this.resetState();
-		const { data, asObservable } = await this.structure.loadType(unique);
-
-		if (data) {
-			this.setIsNew(false);
-			this.#persistedData.update(data);
-		}
-
-		if (asObservable) {
-			this.observe(asObservable(), (entity) => this.#onStoreChange(entity), 'umbMediaTypeStoreObserver');
-		}
-	}
-
 	/**
-	 * Save or creates the media type, based on wether its a new one or existing.
+	 * @deprecated Use the createScaffold method instead. Will be removed in 17.
+	 * @param {UmbEntityModel} parent
+	 * @memberof UmbMediaTypeWorkspaceContext
 	 */
-	async submit() {
-		const data = this.getData();
-		if (!data) {
-			throw new Error('Something went wrong, there is no data for media type you want to save...');
-		}
-
-		if (this.getIsNew()) {
-			const parent = this.#parent.getValue();
-			if (!parent) throw new Error('Parent is not set');
-
-			await this.structure.create(parent.unique);
-
-			const eventContext = await this.getContext(UMB_ACTION_EVENT_CONTEXT);
-			const event = new UmbRequestReloadChildrenOfEntityEvent({
-				entityType: parent.entityType,
-				unique: parent.unique,
-			});
-			eventContext.dispatchEvent(event);
-			this.setIsNew(false);
-		} else {
-			await this.structure.save();
-
-			const actionEventContext = await this.getContext(UMB_ACTION_EVENT_CONTEXT);
-			const event = new UmbRequestReloadStructureForEntityEvent({
-				unique: this.getUnique()!,
-				entityType: this.getEntityType(),
-			});
-
-			actionEventContext.dispatchEvent(event);
-		}
+	async create(parent: UmbEntityModel) {
+		this.createScaffold({ parent });
 	}
 }
 
