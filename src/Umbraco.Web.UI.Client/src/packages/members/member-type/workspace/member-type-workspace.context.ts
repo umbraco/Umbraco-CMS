3d8b73a7b377afdbe3b63b511ab70ba71e6dd53a
Mads Rasmussen <madsr@hey.com>
2024-11-14 14:47:34 +0100

    clean up

--- src/Umbraco.Web.UI.Client/src/packages/members/member-type/workspace/member-type-workspace.context.ts
+++ src/Umbraco.Web.UI.Client/src/packages/members/member-type/workspace/member-type-workspace.context.ts
@@ -1,25 +1,16 @@
-import { UMB_MEMBER_TYPE_DETAIL_REPOSITORY_ALIAS, UmbMemberTypeDetailRepository } from '../repository/detail/index.js';
+import { UMB_MEMBER_TYPE_DETAIL_REPOSITORY_ALIAS } from '../repository/detail/index.js';
 import type { UmbMemberTypeDetailModel } from '../types.js';
 import { UMB_MEMBER_TYPE_ENTITY_TYPE } from '../index.js';
 import { UmbMemberTypeWorkspaceEditorElement } from './member-type-workspace-editor.element.js';
 import {
-	UmbSubmittableWorkspaceContextBase,
 	type UmbRoutableWorkspaceContext,
 	UmbWorkspaceIsNewRedirectController,
 } from '@umbraco-cms/backoffice/workspace';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import {
-	type UmbContentTypeCompositionModel,
-	UmbContentTypeStructureManager,
 	type UmbContentTypeWorkspaceContext,
 	UmbContentTypeWorkspaceContextBase,
 } from '@umbraco-cms/backoffice/content-type';
-import { UMB_ACTION_EVENT_CONTEXT } from '@umbraco-cms/backoffice/action';
-import { UmbObjectState } from '@umbraco-cms/backoffice/observable-api';
-import {
-	UmbRequestReloadChildrenOfEntityEvent,
-	UmbRequestReloadStructureForEntityEvent,
-} from '@umbraco-cms/backoffice/entity-action';
 import { UMB_MEMBER_TYPE_WORKSPACE_ALIAS } from './manifests.js';
 import type { UmbEntityModel } from '@umbraco-cms/backoffice/entity';
 
@@ -37,12 +28,13 @@ export class UmbMemberTypeWorkspaceContext
 
 		this.routes.setRoutes([
 			{
-				path: 'create/parent/:entityType/:parentUnique',
+				path: 'create/parent/:parentEntityType/:parentUnique',
 				component: UmbMemberTypeWorkspaceEditorElement,
 				setup: async (_component, info) => {
-					const parentEntityType = info.match.params.entityType;
+					const parentEntityType = info.match.params.parentEntityType;
 					const parentUnique = info.match.params.parentUnique === 'null' ? null : info.match.params.parentUnique;
-					await this.create({ entityType: parentEntityType, unique: parentUnique });
+					const parent: UmbEntityModel = { entityType: parentEntityType, unique: parentUnique };
+					await this.createScaffold({ parent });
 
 					new UmbWorkspaceIsNewRedirectController(
 						this,
@@ -63,7 +55,7 @@ export class UmbMemberTypeWorkspaceContext
 	}
 
 	/**
-	 * @deprecated Use the individual set methods instead
+	 * @deprecated Use the individual set methods instead. Will be removed in 17.
 	 * @template PropertyName
 	 * @param {PropertyName} propertyName
 	 * @param {EntityDetailModel[PropertyName]} value
@@ -76,47 +68,13 @@ export class UmbMemberTypeWorkspaceContext
 		this.structure.updateOwnerContentType({ [propertyName]: value });
 	}
 
-	setCompositions(compositions: Array<UmbContentTypeCompositionModel>) {
-		this.structure.updateOwnerContentType({ compositions });
-	}
-
+	/**
+	 * @deprecated Use the createScaffold method instead. Will be removed in 17.
+	 * @param {UmbEntityModel} parent
+	 * @memberof UmbMemberTypeWorkspaceContext
+	 */
 	async create(parent: UmbEntityModel) {
 		this.createScaffold({ parent });
-	}
-
-	/**
-	 * Save or creates the member type, based on wether its a new one or existing.
-	 */
-	async submit() {
-		const data = this.getData();
-		if (!data) {
-			throw new Error('Something went wrong, there is no data for media type you want to save...');
-		}
-
-		if (this.getIsNew()) {
-			const parent = this.#parent.getValue();
-			if (!parent) throw new Error('Parent is not set');
-
-			await this.structure.create(parent.unique);
-
-			const eventContext = await this.getContext(UMB_ACTION_EVENT_CONTEXT);
-			const event = new UmbRequestReloadChildrenOfEntityEvent({
-				entityType: parent.entityType,
-				unique: parent.unique,
-			});
-			eventContext.dispatchEvent(event);
-			this.setIsNew(false);
-		} else {
-			await this.structure.save();
-
-			const actionEventContext = await this.getContext(UMB_ACTION_EVENT_CONTEXT);
-			const event = new UmbRequestReloadStructureForEntityEvent({
-				unique: this.getUnique()!,
-				entityType: this.getEntityType(),
-			});
-
-			actionEventContext.dispatchEvent(event);
-		}
 	}
 }
 
