342b0589ee359c291b5cf8a4ef5a06229e97c1c5
Mads Rasmussen <madsr@hey.com>
2024-11-14 12:25:40 +0100

    remove duplicate code

--- src/Umbraco.Web.UI.Client/src/packages/members/member-type/workspace/member-type-workspace.context.ts
+++ src/Umbraco.Web.UI.Client/src/packages/members/member-type/workspace/member-type-workspace.context.ts
@@ -1,4 +1,4 @@
-import { UmbMemberTypeDetailRepository } from '../repository/detail/index.js';
+import { UMB_MEMBER_TYPE_DETAIL_REPOSITORY_ALIAS, UmbMemberTypeDetailRepository } from '../repository/detail/index.js';
 import type { UmbMemberTypeDetailModel } from '../types.js';
 import { UMB_MEMBER_TYPE_ENTITY_TYPE } from '../index.js';
 import { UmbMemberTypeWorkspaceEditorElement } from './member-type-workspace-editor.element.js';
@@ -12,6 +12,7 @@ import {
 	type UmbContentTypeCompositionModel,
 	UmbContentTypeStructureManager,
 	type UmbContentTypeWorkspaceContext,
+	UmbContentTypeWorkspaceContextBase,
 } from '@umbraco-cms/backoffice/content-type';
 import { UMB_ACTION_EVENT_CONTEXT } from '@umbraco-cms/backoffice/action';
 import { UmbObjectState } from '@umbraco-cms/backoffice/observable-api';
@@ -19,59 +20,20 @@ import {
 	UmbRequestReloadChildrenOfEntityEvent,
 	UmbRequestReloadStructureForEntityEvent,
 } from '@umbraco-cms/backoffice/entity-action';
+import { UMB_MEMBER_TYPE_WORKSPACE_ALIAS } from './manifests.js';
+import type { UmbEntityModel } from '@umbraco-cms/backoffice/entity';
 
-type EntityType = UmbMemberTypeDetailModel;
+type EntityDetailModel = UmbMemberTypeDetailModel;
 export class UmbMemberTypeWorkspaceContext
-	extends UmbSubmittableWorkspaceContextBase<EntityType>
-	implements UmbContentTypeWorkspaceContext<EntityType>, UmbRoutableWorkspaceContext
+	extends UmbContentTypeWorkspaceContextBase<EntityDetailModel>
+	implements UmbContentTypeWorkspaceContext<EntityDetailModel>, UmbRoutableWorkspaceContext
 {
-	readonly IS_CONTENT_TYPE_WORKSPACE_CONTEXT = true;
-
-	public readonly repository = new UmbMemberTypeDetailRepository(this);
-
-	#parent = new UmbObjectState<{ entityType: string; unique: string | null } | undefined>(undefined);
-	readonly parentUnique = this.#parent.asObservablePart((parent) => (parent ? parent.unique : undefined));
-	readonly parentEntityType = this.#parent.asObservablePart((parent) => (parent ? parent.entityType : undefined));
-
-	#persistedData = new UmbObjectState<EntityType | undefined>(undefined);
-
-	// General for content types:
-	readonly data;
-	readonly unique;
-	readonly name;
-	getName(): string | undefined {
-		return this.structure.getOwnerContentType()?.name;
-	}
-	readonly alias;
-	readonly description;
-	readonly icon;
-
-	readonly allowedAtRoot;
-	readonly variesByCulture;
-	readonly variesBySegment;
-	readonly isElement;
-	readonly allowedContentTypes;
-	readonly compositions;
-
-	readonly structure = new UmbContentTypeStructureManager<EntityType>(this, this.repository);
-
 	constructor(host: UmbControllerHost) {
-		super(host, 'Umb.Workspace.MemberType');
-
-		// General for content types:
-		this.data = this.structure.ownerContentType;
-
-		this.unique = this.structure.ownerContentTypeObservablePart((data) => data?.unique);
-		this.name = this.structure.ownerContentTypeObservablePart((data) => data?.name);
-		this.alias = this.structure.ownerContentTypeObservablePart((data) => data?.alias);
-		this.description = this.structure.ownerContentTypeObservablePart((data) => data?.description);
-		this.icon = this.structure.ownerContentTypeObservablePart((data) => data?.icon);
-		this.allowedAtRoot = this.structure.ownerContentTypeObservablePart((data) => data?.allowedAtRoot);
-		this.variesByCulture = this.structure.ownerContentTypeObservablePart((data) => data?.variesByCulture);
-		this.variesBySegment = this.structure.ownerContentTypeObservablePart((data) => data?.variesBySegment);
-		this.isElement = this.structure.ownerContentTypeObservablePart((data) => data?.isElement);
-		this.allowedContentTypes = this.structure.ownerContentTypeObservablePart((data) => data?.allowedContentTypes);
-		this.compositions = this.structure.ownerContentTypeObservablePart((data) => data?.compositions);
+		super(host, {
+			workspaceAlias: UMB_MEMBER_TYPE_WORKSPACE_ALIAS,
+			entityType: UMB_MEMBER_TYPE_ENTITY_TYPE,
+			detailRepositoryAlias: UMB_MEMBER_TYPE_DETAIL_REPOSITORY_ALIAS,
+		});
 
 		this.routes.setRoutes([
 			{
@@ -100,78 +62,26 @@ export class UmbMemberTypeWorkspaceContext
 		]);
 	}
 
-	set<PropertyName extends keyof EntityType>(propertyName: PropertyName, value: EntityType[PropertyName]) {
+	/**
+	 * @deprecated Use the individual set methods instead
+	 * @template PropertyName
+	 * @param {PropertyName} propertyName
+	 * @param {EntityDetailModel[PropertyName]} value
+	 * @memberof UmbMemberTypeWorkspaceContext
+	 */
+	set<PropertyName extends keyof EntityDetailModel>(
+		propertyName: PropertyName,
+		value: EntityDetailModel[PropertyName],
+	) {
 		this.structure.updateOwnerContentType({ [propertyName]: value });
-	}
-
-	protected override resetState(): void {
-		super.resetState();
-		this.#persistedData.setValue(undefined);
-	}
-
-	getData() {
-		return this.structure.getOwnerContentType();
-	}
-
-	getUnique() {
-		return this.getData()?.unique;
-	}
-
-	getEntityType() {
-		return UMB_MEMBER_TYPE_ENTITY_TYPE;
-	}
-
-	setName(name: string) {
-		this.structure.updateOwnerContentType({ name });
-	}
-
-	setAlias(alias: string) {
-		this.structure.updateOwnerContentType({ alias });
-	}
-
-	setDescription(description: string) {
-		this.structure.updateOwnerContentType({ description });
-	}
-
-	// TODO: manage setting icon color alias?
-	setIcon(icon: string) {
-		this.structure.updateOwnerContentType({ icon });
 	}
 
 	setCompositions(compositions: Array<UmbContentTypeCompositionModel>) {
 		this.structure.updateOwnerContentType({ compositions });
 	}
 
-	async create(parent: { entityType: string; unique: string | null }) {
-		this.resetState();
-		this.#parent.setValue(parent);
-		const { data } = await this.structure.createScaffold();
-		if (!data) return undefined;
-
-		this.setIsNew(true);
-		this.#persistedData.setValue(data);
-		return data;
-	}
-
-	async load(unique: string) {
-		this.resetState();
-		const { data, asObservable } = await this.structure.loadType(unique);
-
-		if (data) {
-			this.setIsNew(false);
-			this.#persistedData.update(data);
-		}
-
-		if (asObservable) {
-			this.observe(asObservable(), (entity) => this.#onStoreChange(entity), 'umbMemberTypeStoreObserver');
-		}
-	}
-
-	#onStoreChange(entity: EntityType | undefined) {
-		if (!entity) {
-			//TODO: This solution is alright for now. But reconsider when we introduce signal-r
-			history.pushState(null, '', 'section/settings/workspace/member-type-root');
-		}
+	async create(parent: UmbEntityModel) {
+		this.createScaffold({ parent });
 	}
 
 	/**
@@ -207,13 +117,6 @@ export class UmbMemberTypeWorkspaceContext
 
 			actionEventContext.dispatchEvent(event);
 		}
-	}
-
-	public override destroy(): void {
-		this.#persistedData.destroy();
-		this.structure.destroy();
-		this.repository.destroy();
-		super.destroy();
 	}
 }
 
