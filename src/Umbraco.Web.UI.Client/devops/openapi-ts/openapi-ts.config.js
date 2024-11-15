2fa7b7031b2a1b4e60881e68f472d735057e1916
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-11-15 15:19:22 +0100

    build(deps-dev): bump @hey-api/openapi-ts from 0.52.11 to 0.55.3
    regenerated all models and migrated the config

--- src/Umbraco.Web.UI.Client/devops/openapi-ts/openapi-ts.config.js
+++ src/Umbraco.Web.UI.Client/devops/openapi-ts/openapi-ts.config.js
@@ -1,18 +1,22 @@
 import { defineConfig } from '@hey-api/openapi-ts';
 
 export default defineConfig({
-	client: 'fetch',
-	input: 'https://raw.githubusercontent.com/umbraco/Umbraco-CMS/v15/dev/src/Umbraco.Cms.Api.Management/OpenApi.json',
+	client: 'legacy/fetch',
+	debug: true,
+	input: '../Umbraco.Cms.Api.Management/OpenApi.json',
 	output: {
 		path: 'src/external/backend-api/src',
 		format: 'prettier',
 		lint: 'eslint',
 	},
-	schemas: false,
-	services: {
-		asClass: true,
-	},
-	types: {
-		enums: 'typescript',
-	},
+	plugins: [
+		{
+			name: '@hey-api/types',
+			enums: 'typescript'
+		},
+		{
+			name: '@hey-api/services',
+			asClass: true
+		}
+	]
 });
