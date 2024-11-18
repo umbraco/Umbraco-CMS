9feccde7e23c532e02b95cc38f1ce135d7ea5b3d
Mads Rasmussen <madsr@hey.com>
2024-11-18 22:20:19 +0100

    import type

--- src/Umbraco.Web.UI.Client/src/packages/core/clipboard/context/clipboard.context-token.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/clipboard/context/clipboard.context-token.ts
@@ -1,3 +1,4 @@
 import { UmbContextToken } from '@umbraco-cms/backoffice/context-api';
+import type { UmbClipboardContext } from './clipboard.context.js';
 
 export const UMB_CLIPBOARD_CONTEXT = new UmbContextToken<UmbClipboardContext>('UmbClipboardContext');
