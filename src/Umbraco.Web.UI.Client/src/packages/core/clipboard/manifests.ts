3a4e138e298a0284d671b998ae544f121c8cb4a3
Mads Rasmussen <madsr@hey.com>
2024-11-19 15:07:56 +0100

    register detail manifests

--- src/Umbraco.Web.UI.Client/src/packages/core/clipboard/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/clipboard/manifests.ts
@@ -1,5 +1,6 @@
 import { manifests as clipboardRootManifests } from './clipboard-root/manifests.js';
 import { manifests as collectionManifests } from './collection/manifests.js';
 import { manifests as contextManifests } from './context/manifests.js';
+import { manifests as detailManifests } from './detail/manifests.js';
 
-export const manifests = [...contextManifests, ...clipboardRootManifests, ...collectionManifests];
+export const manifests = [...contextManifests, ...clipboardRootManifests, ...collectionManifests, ...detailManifests];
