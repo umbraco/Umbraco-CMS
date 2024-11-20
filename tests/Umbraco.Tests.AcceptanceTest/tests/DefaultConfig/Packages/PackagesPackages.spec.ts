e398e2807fa1bbdf7fe0966100b97c3aba75b513
Andreas Zerbst <73799582+andr317c@users.noreply.github.com>
2024-11-20 08:12:44 +0100

    V15 QA cherrypicked tests from v14 (#17540)
    * Cherrypicked tests from 14
    
    * Removed property
    
    * Updates
    
    * Updated tests
    
    * Skip test
    
    * Bumped version
    
    * Bumped version
    
    * Added smoke tag to tests

--- tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Packages/PackagesPackages.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Packages/PackagesPackages.spec.ts
@@ -1,7 +1,8 @@
 import {ConstantHelper, test} from '@umbraco/playwright-testhelpers';
 
+// TODO: There is currently an issue with playwright and the Iframe in the marketplace, look into this.
 // The MarketPlace is a iFrame we are using from the DXP team, so it is not something we should test. This test is just checking if we have the IFrame
-test('can see the marketplace', async ({umbracoUi}) => {
+test.skip('can see the marketplace', async ({umbracoUi}) => {
   // Arrange
   await umbracoUi.goToBackOffice();
   await umbracoUi.package.goToSection(ConstantHelper.sections.packages);
