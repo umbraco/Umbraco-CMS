f5138fa2aebbd220f3c42ae207be42a63d50df3e
Mads Rasmussen <madsr@hey.com>
2024-11-21 15:12:36 +0100

    use picker input context

--- src/Umbraco.Web.UI.Client/src/packages/property-editors/color-picker/property-actions/paste-from-clipboard/paste-from-clipboard.property-action.ts
+++ src/Umbraco.Web.UI.Client/src/packages/property-editors/color-picker/property-actions/paste-from-clipboard/paste-from-clipboard.property-action.ts
@@ -1,15 +1,29 @@
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import { UMB_PROPERTY_CONTEXT } from '@umbraco-cms/backoffice/property';
 import { UmbPropertyActionBase, type UmbPropertyActionArgs } from '@umbraco-cms/backoffice/property-action';
-import { UMB_CLIPBOARD_CONTEXT } from '@umbraco-cms/backoffice/clipboard';
+import {
+	UMB_CLIPBOARD_CONTEXT,
+	UMB_CLIPBOARD_ENTRY_ITEM_REPOSITORY_ALIAS,
+	UMB_CLIPBOARD_ENTRY_PICKER_MODAL,
+	type UmbClipboardEntryDetailModel,
+} from '@umbraco-cms/backoffice/clipboard';
+import { UmbPickerInputContext } from '@umbraco-cms/backoffice/picker-input';
 
 export class UmbColorPickerPasteFromClipboardPropertyAction extends UmbPropertyActionBase {
 	#clipboardContext?: typeof UMB_CLIPBOARD_CONTEXT.TYPE;
 	#propertyContext?: typeof UMB_PROPERTY_CONTEXT.TYPE;
 	#init?: Promise<unknown>;
 
+	#pickerInputContext = new UmbPickerInputContext<UmbClipboardEntryDetailModel>(
+		this,
+		UMB_CLIPBOARD_ENTRY_ITEM_REPOSITORY_ALIAS,
+		UMB_CLIPBOARD_ENTRY_PICKER_MODAL,
+	);
+
 	constructor(host: UmbControllerHost, args: UmbPropertyActionArgs<never>) {
 		super(host, args);
+
+		this.#pickerInputContext.max = 1;
 
 		this.#init = Promise.all([
 			this.consumeContext(UMB_CLIPBOARD_CONTEXT, (context) => {
@@ -26,7 +40,8 @@ export class UmbColorPickerPasteFromClipboardPropertyAction extends UmbPropertyA
 		await this.#init;
 
 		try {
-			const value = await this.#clipboardContext?.pick();
+			await this.#pickerInputContext.openPicker();
+			const value = this.#pickerInputContext.getSelection();
 			debugger;
 		} catch (error) {}
 	}
