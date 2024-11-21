d7ed9636cb550ba9d2f91cc8ff0aa2bf8ba6d762
Mads Rasmussen <madsr@hey.com>
2024-11-21 20:46:59 +0100

    poc paste property action

--- src/Umbraco.Web.UI.Client/src/packages/property-editors/color-picker/property-actions/paste-from-clipboard/paste-from-clipboard.property-action.ts
+++ src/Umbraco.Web.UI.Client/src/packages/property-editors/color-picker/property-actions/paste-from-clipboard/paste-from-clipboard.property-action.ts
@@ -1,32 +1,25 @@
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import { UMB_PROPERTY_CONTEXT } from '@umbraco-cms/backoffice/property';
 import { UmbPropertyActionBase, type UmbPropertyActionArgs } from '@umbraco-cms/backoffice/property-action';
-import {
-	UMB_CLIPBOARD_CONTEXT,
-	UMB_CLIPBOARD_ENTRY_ITEM_REPOSITORY_ALIAS,
-	UMB_CLIPBOARD_ENTRY_PICKER_MODAL,
-	type UmbClipboardEntryDetailModel,
-} from '@umbraco-cms/backoffice/clipboard';
-import { UmbPickerInputContext } from '@umbraco-cms/backoffice/picker-input';
+import { UMB_MODAL_MANAGER_CONTEXT } from '@umbraco-cms/backoffice/modal';
+import { UMB_CLIPBOARD_ENTRY_PICKER_MODAL, UmbClipboardEntryDetailRepository } from '@umbraco-cms/backoffice/clipboard';
 
 export class UmbColorPickerPasteFromClipboardPropertyAction extends UmbPropertyActionBase {
 	#propertyContext?: typeof UMB_PROPERTY_CONTEXT.TYPE;
+	#modalManagerContext?: typeof UMB_MODAL_MANAGER_CONTEXT.TYPE;
+	#clipboardEntryDetailRepository = new UmbClipboardEntryDetailRepository(this);
 	#init?: Promise<unknown>;
-
-	#pickerInputContext = new UmbPickerInputContext<UmbClipboardEntryDetailModel>(
-		this,
-		UMB_CLIPBOARD_ENTRY_ITEM_REPOSITORY_ALIAS,
-		UMB_CLIPBOARD_ENTRY_PICKER_MODAL,
-	);
 
 	constructor(host: UmbControllerHost, args: UmbPropertyActionArgs<never>) {
 		super(host, args);
 
-		this.#pickerInputContext.max = 1;
-
 		this.#init = Promise.all([
 			this.consumeContext(UMB_PROPERTY_CONTEXT, (context) => {
 				this.#propertyContext = context;
+			}).asPromise(),
+
+			this.consumeContext(UMB_MODAL_MANAGER_CONTEXT, (context) => {
+				this.#modalManagerContext = context;
 			}).asPromise(),
 		]);
 	}
@@ -35,9 +28,19 @@ export class UmbColorPickerPasteFromClipboardPropertyAction extends UmbPropertyA
 		await this.#init;
 
 		try {
-			await this.#pickerInputContext.openPicker();
-			const value = this.#pickerInputContext.getSelection();
-			debugger;
+			const modalContext = this.#modalManagerContext?.open(this, UMB_CLIPBOARD_ENTRY_PICKER_MODAL);
+			const value = await modalContext?.onSubmit();
+			const clipboardEntryUnique = value?.selection?.[0];
+			if (clipboardEntryUnique) {
+				const { data: clipboardEntry, error } =
+					await this.#clipboardEntryDetailRepository.requestByUnique(clipboardEntryUnique);
+
+				const clipboardEntryData = clipboardEntry?.data[0];
+
+				if (clipboardEntryData) {
+					this.#propertyContext?.setValue(clipboardEntryData);
+				}
+			}
 		} catch (error) {}
 	}
 }
