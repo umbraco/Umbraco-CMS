d4963d0483b185a9b355525885f90b151a02f326
Mads Rasmussen <madsr@hey.com>
2024-11-25 12:08:58 +0100

    fix import

--- src/Umbraco.Web.UI.Client/src/packages/media/media/search/media.search-provider.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/search/media.search-provider.ts
@@ -1,7 +1,7 @@
-import type { UmbMediaItemModel } from '../index.js';
 import { UmbMediaSearchRepository } from './media-search.repository.js';
 import type { UmbSearchProvider, UmbSearchRequestArgs } from '@umbraco-cms/backoffice/search';
 import { UmbControllerBase } from '@umbraco-cms/backoffice/class-api';
+import type { UmbMediaSearchItemModel } from './types.js';
 
 export class UmbMediaSearchProvider extends UmbControllerBase implements UmbSearchProvider<UmbMediaSearchItemModel> {
 	#repository = new UmbMediaSearchRepository(this);
