32f00a3f1c97f6800f4851f171cfb8474d35a7f6
Mads Rasmussen <madsr@hey.com>
2024-11-26 13:55:48 +0100

    temp use repo instead of context

--- src/Umbraco.Web.UI.Client/src/packages/core/clipboard/property-actions/copy/copy-to-clipboard.property-action.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/clipboard/property-actions/copy/copy-to-clipboard.property-action.ts
@@ -1,24 +1,24 @@
 import { UMB_CLIPBOARD_CONTEXT } from '../../context/index.js';
-import type { UmbClipboardEntryDetailModel } from '../../clipboard-entry/index.js';
-import { UMB_CLIPBOARD_ENTRY_ENTITY_TYPE } from '../../clipboard-entry/entity.js';
+import { UmbClipboardEntryDetailRepository } from '../../clipboard-entry/index.js';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import { UMB_NOTIFICATION_CONTEXT } from '@umbraco-cms/backoffice/notification';
-import { UMB_PROPERTY_CONTEXT } from '@umbraco-cms/backoffice/property';
+import { UMB_PROPERTY_CONTEXT, UMB_PROPERTY_DATASET_CONTEXT } from '@umbraco-cms/backoffice/property';
 import { UmbPropertyActionBase, type UmbPropertyActionArgs } from '@umbraco-cms/backoffice/property-action';
-import { UmbId } from '@umbraco-cms/backoffice/id';
 
 export class UmbColorPickerCopyToClipboardPropertyAction extends UmbPropertyActionBase {
-	#clipboardContext?: typeof UMB_CLIPBOARD_CONTEXT.TYPE;
+	#propertyDatasetContext?: typeof UMB_PROPERTY_DATASET_CONTEXT.TYPE;
 	#propertyContext?: typeof UMB_PROPERTY_CONTEXT.TYPE;
 	#notificationContext?: typeof UMB_NOTIFICATION_CONTEXT.TYPE;
 	#init?: Promise<unknown>;
+
+	#clipboardDetailRepository = new UmbClipboardEntryDetailRepository(this);
 
 	constructor(host: UmbControllerHost, args: UmbPropertyActionArgs<never>) {
 		super(host, args);
 
 		this.#init = Promise.all([
-			this.consumeContext(UMB_CLIPBOARD_CONTEXT, (context) => {
-				this.#clipboardContext = context;
+			this.consumeContext(UMB_PROPERTY_DATASET_CONTEXT, (context) => {
+				this.#propertyDatasetContext = context;
 			}).asPromise(),
 
 			this.consumeContext(UMB_PROPERTY_CONTEXT, (context) => {
@@ -33,27 +33,28 @@ export class UmbColorPickerCopyToClipboardPropertyAction extends UmbPropertyActi
 
 	override async execute() {
 		await this.#init;
+		const workspaceName = this.#propertyDatasetContext?.getName() || 'Unnamed workspace';
+		const propertyLabel = this.#propertyContext?.getLabel() || 'Unnamed property';
 		const propertyValue = this.#propertyContext?.getValue();
-		const propertyLabel = this.#propertyContext?.getLabel() ?? 'Color';
+		const clipboardEntryName = workspaceName ? `${workspaceName} - ${propertyLabel}` : propertyLabel;
 
 		if (!propertyValue) {
 			// TODO: Add correct message + localization
-			this.#notificationContext!.peek('danger', { data: { message: 'No value' } });
+			this.#notificationContext!.peek('danger', { data: { message: 'The property does not have a value to copy' } });
 		}
 
 		// TODO: Add correct meta data
-		// TODO use scaffold to create clipboard entry
-		const clipboardEntryDetail: UmbClipboardEntryDetailModel = {
-			entityType: UMB_CLIPBOARD_ENTRY_ENTITY_TYPE,
-			unique: UmbId.new(),
-			type: 'color',
-			name: propertyLabel,
-			icons: ['icon-color'],
+		const { data } = await this.#clipboardDetailRepository.createScaffold({
+			type: 'color', // TODO: make this value dynamic
+			name: clipboardEntryName,
+			icons: ['icon-color'], // TODO: make this value dynamic
 			meta: {},
 			data: [propertyValue],
-		};
+		});
 
-		await this.#clipboardContext!.create(clipboardEntryDetail);
+		if (data) {
+			await this.#clipboardDetailRepository.create(data);
+		}
 	}
 }
 export { UmbColorPickerCopyToClipboardPropertyAction as api };
