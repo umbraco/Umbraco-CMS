71ae92f60a21e435c9a6491ce77a66385ed63c6f
Mads Rasmussen <madsr@hey.com>
2024-11-26 13:57:35 +0100

    delete unused copy property action

--- src/Umbraco.Web.UI.Client/src/packages/core/property-action/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property-action/manifests.ts
@@ -7,23 +7,6 @@ export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> =
 	{
 		type: 'propertyAction',
 		kind: 'default',
-		alias: 'Umb.PropertyAction.Copy',
-		name: 'Copy Property Action',
-		api: () => import('./common/copy/property-action-copy.controller.js'),
-		forPropertyEditorUis: [],
-		meta: {
-			icon: 'icon-paste-in',
-			label: 'Copy',
-		},
-		conditions: [
-			{
-				alias: UMB_WRITABLE_PROPERTY_CONDITION_ALIAS,
-			},
-		],
-	},
-	{
-		type: 'propertyAction',
-		kind: 'default',
 		alias: 'Umb.PropertyAction.Clear',
 		name: 'Clear Property Action',
 		api: () => import('./common/clear/property-action-clear.controller.js'),
