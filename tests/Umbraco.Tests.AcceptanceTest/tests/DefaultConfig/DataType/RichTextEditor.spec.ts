6b9a23df4d9188520240b8d3166f9877e3c252da
Nhu Dinh <hnd@umbraco.dk>
2024-11-28 13:56:05 +0700

    Updated tests for rich text editor data type

--- tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/DataType/RichTextEditor.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/DataType/RichTextEditor.spec.ts
@@ -3,10 +3,9 @@ import {expect} from "@playwright/test";
 
 const dataTypeName = 'Richtext editor';
 
-// TODO: Remove skip when the front-end is ready. Currently the TinyMCE still be the default plugins in rich text editor
-test.skip('titap is the default plugins in rich text editor', async ({umbracoApi, umbracoUi}) => {
+test('titap is the default plugins in rich text editor', async ({umbracoApi, umbracoUi}) => {
   // Arrange
-  const tiptapPropertyEditorName = 'Rich Text Editor Property Editor UI';
+  const tiptapPropertyEditorName = 'Rich Text Editor [Tiptap] Property Editor UI';
   const tiptapAlias = 'Umbraco.RichText';
   const tiptapUiAlias = 'Umb.PropertyEditorUi.Tiptap';
   await umbracoUi.goToBackOffice();
