093b4f3f330c3889b308dd97891921d8ac62ae64
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2024-11-28 17:41:18 +0100

    Refactor to use IWebhostEnvironment for wwwroot files.

--- src/Umbraco.Web.BackOffice/Trees/StaticFilesTreeController.cs
+++ src/Umbraco.Web.BackOffice/Trees/StaticFilesTreeController.cs
@@ -1,6 +1,8 @@
 using System.Net;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.FileProviders;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Events;
 using Umbraco.Cms.Core.IO;
@@ -17,61 +19,45 @@ public class StaticFilesTreeController : TreeController
     private const string Webroot = "wwwroot";
     private readonly IFileSystem _fileSystem;
     private readonly IMenuItemCollectionFactory _menuItemCollectionFactory;
+    private readonly IWebHostEnvironment _webHostEnvironment;
 
     public StaticFilesTreeController(
         ILocalizedTextService localizedTextService,
         UmbracoApiControllerTypeCollection umbracoApiControllerTypeCollection,
         IEventAggregator eventAggregator,
         IPhysicalFileSystem fileSystem,
-        IMenuItemCollectionFactory menuItemCollectionFactory)
+        IMenuItemCollectionFactory menuItemCollectionFactory,
+        IWebHostEnvironment webHostEnvironment)
         : base(localizedTextService, umbracoApiControllerTypeCollection, eventAggregator)
     {
         _fileSystem = fileSystem;
         _menuItemCollectionFactory = menuItemCollectionFactory;
+        _webHostEnvironment = webHostEnvironment;
     }
 
     protected override ActionResult<TreeNodeCollection> GetTreeNodes(string id, FormCollection queryStrings)
     {
         var path = string.IsNullOrEmpty(id) == false && id != Constants.System.RootString
             ? WebUtility.UrlDecode(id).TrimStart("/")
-            : "";
+            : string.Empty;
 
         var nodes = new TreeNodeCollection();
-        IEnumerable<string> directories = _fileSystem.GetDirectories(path);
 
-        foreach (var directory in directories)
+        // Add App_Plugins && wwwroot folder if path is empty, as we are only returning root folders.
+        if (path == string.Empty)
         {
-            // We don't want any other directories under the root node other than the ones serving static files - App_Plugins and wwwroot
-            if (id == Constants.System.RootString && directory != AppPlugins && directory != Webroot)
-            {
-                continue;
-            }
-
-            var hasChildren = _fileSystem.GetFiles(directory).Any() || _fileSystem.GetDirectories(directory).Any();
-
-            var name = Path.GetFileName(directory);
-            TreeNode? node = CreateTreeNode(WebUtility.UrlEncode(directory), path, queryStrings, name,
-                Constants.Icons.Folder, hasChildren);
-
-            if (node != null)
-            {
-                nodes.Add(node);
-            }
+            AddRootFolder(AppPlugins, queryStrings, nodes);
+            AddRootFolder(Webroot, queryStrings, nodes);
         }
-
-        // Only get the files inside App_Plugins and wwwroot
-        IEnumerable<string> files = _fileSystem.GetFiles(path)
-            .Where(x => x.StartsWith(AppPlugins) || x.StartsWith(Webroot));
-
-        foreach (var file in files)
+        else
         {
-            var name = Path.GetFileName(file);
-            TreeNode? node = CreateTreeNode(WebUtility.UrlEncode(file), path, queryStrings, name,
-                Constants.Icons.DefaultIcon, false);
-
-            if (node != null)
+            if (path.StartsWith(Webroot, StringComparison.OrdinalIgnoreCase))
             {
-                nodes.Add(node);
+                AddWebRootFiles(path, queryStrings, nodes);
+            }
+            else if (path.StartsWith(AppPlugins, StringComparison.OrdinalIgnoreCase))
+            {
+                AddPhysicalFiles(path, queryStrings, nodes);
             }
         }
 
@@ -81,4 +67,59 @@ public class StaticFilesTreeController : TreeController
     // We don't have any menu item options (such as create/delete/reload) & only use the root node to load a custom UI
     protected override ActionResult<MenuItemCollection> GetMenuForNode(string id, FormCollection queryStrings) =>
         _menuItemCollectionFactory.Create();
+
+    private void AddRootFolder(string directory, FormCollection queryStrings, TreeNodeCollection nodes)
+    {
+        if (_fileSystem.DirectoryExists(directory) is false)
+        {
+            return;
+        }
+
+        var hasChildren = _fileSystem.GetFiles(directory).Any() || _fileSystem.GetDirectories(directory).Any();
+
+        var name = Path.GetFileName(directory);
+        TreeNode node = CreateTreeNode(WebUtility.UrlEncode(directory), "", queryStrings, name, Constants.Icons.Folder, hasChildren);
+        nodes.Add(node);
+    }
+
+    private void AddPhysicalFiles(string path, FormCollection queryStrings, TreeNodeCollection nodes)
+    {
+        IEnumerable<string> files = _fileSystem.GetFiles(path)
+            .Where(x => x.StartsWith(AppPlugins) || x.StartsWith(Webroot));
+
+        foreach (var file in files)
+        {
+            var name = Path.GetFileName(file);
+            TreeNode node = CreateTreeNode(WebUtility.UrlEncode(file), path, queryStrings, name, Constants.Icons.DefaultIcon, false);
+            nodes.Add(node);
+        }
+    }
+
+    private void AddWebRootFiles(string path, FormCollection queryStrings, TreeNodeCollection nodes)
+    {
+
+        var calculatedPath = path.TrimStart(Webroot);
+        IDirectoryContents files = _webHostEnvironment.WebRootFileProvider.GetDirectoryContents(calculatedPath);
+        foreach (IFileInfo file in files)
+        {
+            TreeNode? node = null;
+            if (file.IsDirectory)
+            {
+                if (file.PhysicalPath != null)
+                {
+                    var hasChildren = _fileSystem.GetFiles(file.PhysicalPath).Any() || _fileSystem.GetDirectories(file.PhysicalPath).Any();
+                    node = CreateTreeNode(WebUtility.UrlEncode(string.Join("/", path, file.Name)), path, queryStrings, file.Name, Constants.Icons.Folder, hasChildren);
+                }
+            }
+            else
+            {
+                node = CreateTreeNode(WebUtility.UrlEncode(string.Join("/", path, file.Name)), path, queryStrings, file.Name, Constants.Icons.DefaultIcon, false);
+            }
+
+            if (node != null)
+            {
+                nodes.Add(node);
+            }
+        }
+    }
 }
