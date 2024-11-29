5b1d72e8024630e73c74104e85fae74a252a0db7
Mads Rasmussen <madsr@hey.com>
2024-11-29 13:21:00 +0100

    Chore: Enable MSW in web/test-runner + add tests for state manager + isCurrentUser util (#17584)
    * add tests for UmbEntityWorkspaceDataManager
    
    * add tests for umbObjectToPropertyValueArray
    
    * remove unused
    
    * split into individual files
    
    * get from window
    
    * fix import
    
    * setup msw
    
    * temp removal of different mock states
    
    * add service worker
    
    * add tests for isCurrentUser util
    
    * Update package.json
    
    * silence more text logs
    
    * fix race condition
    
    ---------
    
    Co-authored-by: Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
    Co-authored-by: Niels Lyngsø <niels.lyngso@gmail.com>
    Co-authored-by: Niels Lyngsø <nsl@umbraco.dk>

--- src/Umbraco.Web.UI.Client/src/packages/user/current-user/utils/is-current-user-an-admin.function.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/current-user/utils/is-current-user-an-admin.function.ts
@@ -1,16 +1,15 @@
-import { UMB_CURRENT_USER_CONTEXT } from '../current-user.context.token.js';
 import { UmbContextConsumerController } from '@umbraco-cms/backoffice/context-api';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
+import { UMB_CURRENT_USER_CONTEXT } from '../current-user.context.token.js';
 
 /**
- * Check if the current user is the user with the given unique id
+ * Check if the current user is an admin
  * @param host
- * @param userUnique
  */
-export const isCurrentUser = async (host: UmbControllerHost, userUnique: string) => {
+export const isCurrentUserAnAdmin = async (host: UmbControllerHost) => {
 	const ctrl = new UmbContextConsumerController(host, UMB_CURRENT_USER_CONTEXT);
 	const currentUserContext = await ctrl.asPromise();
 	ctrl.destroy();
 
-	return await currentUserContext!.isUserCurrentUser(userUnique);
+	return currentUserContext!.isCurrentUserAdmin();
 };
