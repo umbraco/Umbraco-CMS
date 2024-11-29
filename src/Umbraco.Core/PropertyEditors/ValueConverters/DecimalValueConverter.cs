e0f9636a1a5f5196f57bcaa4ba6a591dc3a40e31
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2024-11-29 12:03:02 +0100

    Add check for integer

--- src/Umbraco.Core/PropertyEditors/ValueConverters/DecimalValueConverter.cs
+++ src/Umbraco.Core/PropertyEditors/ValueConverters/DecimalValueConverter.cs
@@ -37,6 +37,12 @@ public class DecimalValueConverter : PropertyValueConverterBase
             return Convert.ToDecimal(sourceDouble);
         }
 
+        // is it a integer?
+        if (source is int sourceInteger)
+        {
+            return Convert.ToDecimal(sourceInteger);
+        }
+
         // is it a string?
         if (source is string sourceString)
         {
