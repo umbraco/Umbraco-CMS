85a4f6b872f05c9fa58a3e40a21fffa7d84e6fc3
Niels Lyngsø <niels.lyngso@gmail.com>
2024-12-01 20:48:32 +0100

    fix users

--- src/Umbraco.Web.UI.Client/src/packages/user/user/client-credential/create/modal/constants.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/client-credential/create/modal/constants.ts
@@ -1,1 +1,1 @@
-export const UMB_CREATE_USER_CLIENT_CREDENTIAL_MODAL_ALIAS = 'Umb.Modal.User.ClientCredential.Create';
+export { UMB_CREATE_USER_CLIENT_CREDENTIAL_MODAL } from './create-user-client-credential-modal.token.js';
