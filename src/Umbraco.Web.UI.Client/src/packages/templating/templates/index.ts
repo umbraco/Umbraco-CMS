4d87c922c4f5776e5cd0c86ac667770827e35171
Niels Lyngsø <niels.lyngso@gmail.com>
2024-12-02 11:31:19 +0100

    fix templates consts export

--- src/Umbraco.Web.UI.Client/src/packages/templating/templates/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/templating/templates/index.ts
@@ -1,6 +1,7 @@
 import './global-components/index.js';
 
 export * from './global-components/index.js';
+export * from './constants.js';
 export * from './entity.js';
 export * from './modals/index.js';
 export * from './repository/index.js';
