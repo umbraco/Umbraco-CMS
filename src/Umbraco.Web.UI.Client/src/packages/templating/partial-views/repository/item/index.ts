aa12a1216ffaeea6358f7848e2e8272a746e44a1
Niels Lyngsø <niels.lyngso@gmail.com>
2024-12-02 21:50:24 +0100

    partial views

--- src/Umbraco.Web.UI.Client/src/packages/templating/partial-views/repository/item/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/templating/partial-views/repository/item/index.ts
@@ -1,3 +1,1 @@
 export { UmbPartialViewItemRepository } from './partial-view-item.repository.js';
-export { UMB_PARTIAL_VIEW_ITEM_REPOSITORY_ALIAS, UMB_PARTIAL_VIEW_ITEM_STORE_ALIAS } from './manifests.js';
-export { UMB_PARTIAL_VIEW_ITEM_STORE_CONTEXT } from './partial-view-item.store.context-token.js';
