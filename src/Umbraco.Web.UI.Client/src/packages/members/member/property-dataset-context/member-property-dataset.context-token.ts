6ecaf9ab74787f3c5b7e82a22a50c994742c2806
Niels Lyngsø <niels.lyngso@gmail.com>
2024-12-02 22:09:19 +0100

    member

--- src/Umbraco.Web.UI.Client/src/packages/members/member/property-dataset-context/member-property-dataset.context-token.ts
+++ src/Umbraco.Web.UI.Client/src/packages/members/member/property-dataset-context/member-property-dataset.context-token.ts
@@ -1,5 +1,5 @@
 import { UMB_MEMBER_ENTITY_TYPE } from '../entity.js';
-import type { UmbMemberPropertyDatasetContext } from './member-property-dataset-context.js';
+import type { UmbMemberPropertyDatasetContext } from './member-property-dataset.context.js';
 import type { UmbPropertyDatasetContext } from '@umbraco-cms/backoffice/property';
 import { UmbContextToken } from '@umbraco-cms/backoffice/context-api';
 
