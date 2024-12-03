d9d1398cbd041eb9c5b8b81f787abf448b60d8d9
Niels Lyngsø <niels.lyngso@gmail.com>
2024-12-03 13:21:48 +0100

    media-type

--- src/Umbraco.Web.UI.Client/src/packages/media/media-types/tree/folder/constants.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media-types/tree/folder/constants.ts
@@ -1,3 +1,2 @@
-export * from './conditions/constants.js';
 export * from './repository/constants.js';
 export * from './workspace/constants.js';
--- src/Umbraco.Web.UI.Client/src/packages/templating/templates/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/templating/templates/index.ts
@@ -6,6 +6,5 @@ export * from './entity.js';
 export * from './modals/index.js';
 export * from './repository/index.js';
 export * from './tree/index.js';
-export * from './conditions/index.js';
 
 export type { UmbTemplateDetailModel } from './types.js';
