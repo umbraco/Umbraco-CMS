cb6ca57422a4922178f641f4094f7724f9182baf
Mads Rasmussen <madsr@hey.com>
2024-12-03 19:15:23 +0100

    wip render state

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/publishing/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/publishing/index.ts
@@ -1,1 +1,2 @@
 export * from './repository/index.js';
+export * from './document-publishing.workspace-context.token.js';
--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/workspace/document-workspace-split-view-variant-selector.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/workspace/document-workspace-split-view-variant-selector.element.ts
@@ -1,13 +1,19 @@
 import type { UmbDocumentVariantOptionModel } from '../types.js';
 import { sortVariants } from '../utils.js';
-import { customElement, html } from '@umbraco-cms/backoffice/external/lit';
+import { customElement, html, nothing, state } from '@umbraco-cms/backoffice/external/lit';
 import { DocumentVariantStateModel } from '@umbraco-cms/backoffice/external/backend-api';
 import { UmbWorkspaceSplitViewVariantSelectorElement } from '@umbraco-cms/backoffice/workspace';
+import { UMB_DOCUMENT_PUBLISHING_WORKSPACE_CONTEXT } from '../publishing/index.js';
 
 const elementName = 'umb-document-workspace-split-view-variant-selector';
 @customElement(elementName)
 export class UmbDocumentWorkspaceSplitViewVariantSelectorElement extends UmbWorkspaceSplitViewVariantSelectorElement<UmbDocumentVariantOptionModel> {
 	protected override _variantSorter = sortVariants;
+
+	@state()
+	private _variantsWithPendingChanges: Array<any> = [];
+
+	#documentPublishingWorkspaceContext?: typeof UMB_DOCUMENT_PUBLISHING_WORKSPACE_CONTEXT.TYPE;
 
 	#publishStateLocalizationMap = {
 		[DocumentVariantStateModel.DRAFT]: 'content_unpublished',
@@ -16,8 +22,31 @@ export class UmbDocumentWorkspaceSplitViewVariantSelectorElement extends UmbWork
 		[DocumentVariantStateModel.NOT_CREATED]: 'content_notCreated',
 	};
 
+	constructor() {
+		super();
+		this.consumeContext(UMB_DOCUMENT_PUBLISHING_WORKSPACE_CONTEXT, (instance) => {
+			this.#documentPublishingWorkspaceContext = instance;
+			this.#observePendingChanges();
+		});
+	}
+
+	async #observePendingChanges() {
+		this.observe(
+			this.#documentPublishingWorkspaceContext?.publishedPendingChanges.variantsWithChanges,
+			(variants) => {
+				this._variantsWithPendingChanges = variants || [];
+			},
+			'_observePendingChanges',
+		);
+	}
+
+	#hasPendingChanges(variant: UmbDocumentVariantOptionModel) {
+		return this._variantsWithPendingChanges.some((x) => x.variantId.compare(variant));
+	}
+
 	override _renderVariantDetails(variantOption: UmbDocumentVariantOptionModel) {
-		return html` ${this.localize.term(
+		return html`${this.#hasPendingChanges(variantOption) ? html`<uui-tag>Pending changes</uui-tag>` : nothing}
+		${this.localize.term(
 			this.#publishStateLocalizationMap[variantOption.variant?.state || DocumentVariantStateModel.NOT_CREATED],
 		)}`;
 	}
