9693e507613603f26e8c35ed3177dbe0d6cd5b46
Niels Lyngsø <niels.lyngso@gmail.com>
2024-12-03 14:20:33 +0100

    user permissions

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/user-permissions/conditions/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/user-permissions/conditions/manifests.ts
@@ -1,8 +1,10 @@
+import { UMB_DOCUMENT_USER_PERMISSION_CONDITION_ALIAS } from './constants.js';
+
 export const manifests: Array<UmbExtensionManifest> = [
 	{
 		type: 'condition',
 		name: 'Document User Permission Condition',
-		alias: 'Umb.Condition.UserPermission.Document',
+		alias: UMB_DOCUMENT_USER_PERMISSION_CONDITION_ALIAS,
 		api: () => import('./document-user-permission.condition.js'),
 	},
 ];
