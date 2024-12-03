5b332f1a9caab1d1186df8b11ee68dc6d0d70efd
Nhu Dinh <hnd@umbraco.dk>
2024-12-03 15:37:46 +0700

    Updated tests - not done

--- tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Content/ContentWithTiptap.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Content/ContentWithTiptap.spec.ts
@@ -1,12 +1,13 @@
-﻿import {ConstantHelper, test} from '@umbraco/playwright-testhelpers';
+﻿import {ConstantHelper, NotificationConstantHelper, test} from '@umbraco/playwright-testhelpers';
 import {expect} from "@playwright/test";
 
 const contentName = 'TestContent';
 const documentTypeName = 'TestDocumentTypeForContent';
-const dataTypeName = 'Tags';
-const tagsName = ['testTag1', 'testTag2'];
+const customDataTypeName = 'Test RTE Tiptap';
+let customDataTypeData = null;
 
 test.beforeEach(async ({umbracoApi, umbracoUi}) => {
+  customDataTypeData = await umbracoApi.dataType.createDefaultTiptapDataType(customDataTypeName);
   await umbracoApi.documentType.ensureNameNotExists(documentTypeName);
   await umbracoApi.document.ensureNameNotExists(contentName);
   await umbracoUi.goToBackOffice();
@@ -15,13 +16,13 @@ test.beforeEach(async ({umbracoApi, umbracoUi}) => {
 test.afterEach(async ({umbracoApi}) => {
   await umbracoApi.document.ensureNameNotExists(contentName); 
   await umbracoApi.documentType.ensureNameNotExists(documentTypeName);
+  await umbracoApi.dataType.ensureNameNotExists(customDataTypeName);
 });
 
-test('can create content with one tag', async ({umbracoApi, umbracoUi}) => {
+test('can create content with empty RTE Tiptap property editor', async ({umbracoApi, umbracoUi}) => {
   // Arrange
   const expectedState = 'Draft'; 
-  const dataTypeData = await umbracoApi.dataType.getByName(dataTypeName);
-  await umbracoApi.documentType.createDocumentTypeWithPropertyEditor(documentTypeName, dataTypeName, dataTypeData.id);
+  await umbracoApi.documentType.createDocumentTypeWithPropertyEditor(documentTypeName, customDataTypeName, customDataTypeData.id);
   await umbracoUi.content.goToSection(ConstantHelper.sections.content);
 
   // Act
@@ -29,16 +30,14 @@ test('can create content with one tag', async ({umbracoApi, umbracoUi}) => {
   await umbracoUi.content.clickCreateButton();
   await umbracoUi.content.chooseDocumentType(documentTypeName);
   await umbracoUi.content.enterContentName(contentName);
-  await umbracoUi.content.clickPlusIconButton();
-  await umbracoUi.content.enterTag(tagsName[0]);
   await umbracoUi.content.clickSaveButton();
 
   // Assert
-  await umbracoUi.content.isSuccessNotificationVisible();
+  await umbracoUi.content.doesSuccessNotificationHaveText(NotificationConstantHelper.success.created);
   expect(await umbracoApi.document.doesNameExist(contentName)).toBeTruthy();
   const contentData = await umbracoApi.document.getByName(contentName);
   expect(contentData.variants[0].state).toBe(expectedState);
-  expect(contentData.values[0].value).toEqual([tagsName[0]]);
+  expect(contentData.values).toEqual([]);
 });
 
 test('can publish content with multiple tags', async ({umbracoApi, umbracoUi}) => {
