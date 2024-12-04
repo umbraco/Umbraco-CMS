514893bb6810a04d6bdb673a15605886d92f862d
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2024-12-04 13:20:40 +0100

    Add custom serializer

---
+++ src/Umbraco.PublishedCache.HybridCache/Serialization/HybridCacheSerializer.cs
@@ -0,0 +1,26 @@
+﻿using System.Buffers;
+using MessagePack;
+using MessagePack.Resolvers;
+using Microsoft.Extensions.Caching.Hybrid;
+
+namespace Umbraco.Cms.Infrastructure.HybridCache.Serialization;
+
+internal class HybridCacheSerializer : IHybridCacheSerializer<ContentCacheNode>
+{
+    private readonly MessagePackSerializerOptions _options;
+
+    public HybridCacheSerializer()
+    {
+        MessagePackSerializerOptions defaultOptions = ContractlessStandardResolver.Options;
+        IFormatterResolver resolver = CompositeResolver.Create(defaultOptions.Resolver);
+
+        _options = defaultOptions
+            .WithResolver(resolver)
+            .WithCompression(MessagePackCompression.Lz4BlockArray)
+            .WithSecurity(MessagePackSecurity.UntrustedData);
+    }
+
+    public ContentCacheNode Deserialize(ReadOnlySequence<byte> source) => MessagePackSerializer.Deserialize<ContentCacheNode>(source, _options);
+
+    public void Serialize(ContentCacheNode value, IBufferWriter<byte> target) => target.Write(MessagePackSerializer.Serialize(value, _options));
+}
