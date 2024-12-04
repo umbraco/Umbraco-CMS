5e78a0061299736d0c32263e950d891608a8e18e
Andreas Zerbst <andr317c@live.dk>
2024-12-04 13:57:29 +0100

    Start of appsetting

---
+++ tests/Umbraco.Tests.AcceptanceTest/tests/UnattendedInstallConfig/Install/Install.spec.ts
@@ -0,0 +1,1 @@
+// To be able to test different databases, we need to set an additional UnattendenInstallConfig up because we would have to start from scratch, otherwise we would be using the same database.
