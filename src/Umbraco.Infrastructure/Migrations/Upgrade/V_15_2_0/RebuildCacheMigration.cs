23a3127e74e9810564fb1e7ecdcf09379169a6e2
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2024-12-04 13:20:55 +0100

    Add migration to rebuild cache

---
+++ src/Umbraco.Infrastructure/Migrations/Upgrade/V_15_2_0/RebuildCacheMigration.cs
@@ -0,0 +1,15 @@
+﻿using Umbraco.Cms.Core.PublishedCache;
+
+namespace Umbraco.Cms.Infrastructure.Migrations.Upgrade.V_15_2_0;
+
+[Obsolete("Will be removed in V18")]
+public class RebuildCacheMigration : MigrationBase
+{
+    private readonly IDocumentCacheService _documentCacheService;
+
+    public RebuildCacheMigration(IMigrationContext context, IDocumentCacheService documentCacheService) : base(context) =>
+        _documentCacheService = documentCacheService;
+
+    protected override void Migrate() =>
+        _documentCacheService.ClearMemoryCacheAsync(CancellationToken.None).GetAwaiter().GetResult();
+}
