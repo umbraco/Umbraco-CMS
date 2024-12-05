73fe41e0b884b2fb7f932a905c6931ce8c8a2fdc
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2024-12-05 09:52:10 +0100

    feat: add `umb-app-logo` element to display the backoffice logo including server url and appropriate tags

---
+++ src/Umbraco.Web.UI.Client/src/apps/app/app-logo.element.ts
@@ -0,0 +1,46 @@
+import { UMB_APP_CONTEXT } from '@umbraco-cms/backoffice/app';
+import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
+import { customElement, html, nothing, state } from '@umbraco-cms/backoffice/external/lit';
+
+@customElement('umb-app-logo')
+export class UmbAppLogoElement extends UmbLitElement {
+	@state()
+	private _logoUrl?: string;
+
+	constructor() {
+		super();
+
+		this.consumeContext(UMB_APP_CONTEXT, (instance) => {
+			this._logoUrl = `${instance.getServerUrl()}/umbraco/management/api/v1/security/back-office/graphics/logo`;
+		});
+	}
+
+	/**
+	 * Do not use shadow DOM for this element.
+	 * @returns {this} The element instance.
+	 */
+	override createRenderRoot(): this {
+		return this;
+	}
+
+	override render() {
+		if (!this._logoUrl) {
+			return nothing;
+		}
+
+		return html`
+			<img
+				aria-hidden="true"
+				loading="eager"
+				src="/umbraco/management/api/v1/security/back-office/graphics/logo"
+				alt="logo"
+				style="height: 100%" />
+		`;
+	}
+}
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'umb-app-logo': UmbAppLogoElement;
+	}
+}
