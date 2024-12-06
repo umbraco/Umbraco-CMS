da2e1ddca124dfbce258bc8578f37af0f1801cf1
Zeegaan <skrivdetud@gmail.com>
2024-12-06 09:52:47 +0100

    Also clear media cache

--- src/Umbraco.Infrastructure/Migrations/Upgrade/V_15_1_0/RebuildCacheMigration.cs
+++ src/Umbraco.Infrastructure/Migrations/Upgrade/V_15_1_0/RebuildCacheMigration.cs
@@ -6,10 +6,18 @@ namespace Umbraco.Cms.Infrastructure.Migrations.Upgrade.V_15_1_0;
 public class RebuildCacheMigration : MigrationBase
 {
     private readonly IDocumentCacheService _documentCacheService;
+    private readonly IMediaCacheService _mediaCacheService;
 
-    public RebuildCacheMigration(IMigrationContext context, IDocumentCacheService documentCacheService) : base(context) =>
+    public RebuildCacheMigration(IMigrationContext context, IDocumentCacheService documentCacheService, IMediaCacheService mediaCacheService) : base(context)
+    {
         _documentCacheService = documentCacheService;
+        _mediaCacheService = mediaCacheService;
+    }
 
-    protected override void Migrate() =>
+    protected override void Migrate()
+    {
         _documentCacheService.ClearMemoryCacheAsync(CancellationToken.None).GetAwaiter().GetResult();
+        _mediaCacheService.ClearMemoryCacheAsync(CancellationToken.None).GetAwaiter().GetResult();
+    }
+
 }
