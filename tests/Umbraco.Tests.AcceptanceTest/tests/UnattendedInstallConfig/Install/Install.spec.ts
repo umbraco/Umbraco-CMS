8830c221d4249361fad02eaec38571a057a5efb8
Andreas Zerbst <andr317c@live.dk>
2024-12-09 14:59:56 +0100

    Updated setup of playwright

--- tests/Umbraco.Tests.AcceptanceTest/tests/UnattendedInstallConfig/Install/Install.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/UnattendedInstallConfig/Install/Install.spec.ts
@@ -1,1 +1,8 @@
 // To be able to test different databases, we need to set an additional UnattendenInstallConfig up because we would have to start from scratch, otherwise we would be using the same database.
+import {expect} from '@playwright/test';
+import {ConstantHelper, test} from '@umbraco/playwright-testhelpers';
+
+
+test('test', async ({umbracoApi, umbracoUi}) => {
+  // Arrange
+});
