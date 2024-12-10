92d6cb9ca2be715a2c2eb05dceec39aaba4cbc11
Andreas Zerbst <andr317c@live.dk>
2024-12-10 10:23:37 +0100

    Fixed indentation

--- tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/RenderingContent/RenderingContentWithMultiURLPicker.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/RenderingContent/RenderingContentWithMultiURLPicker.spec.ts
@@ -102,6 +102,6 @@ test('can render content with multiple url value', async ({umbracoApi, umbracoUi
   await umbracoUi.contentRender.doesContentRenderValueContainText(linkTitle);
   await umbracoUi.contentRender.doesContentRenderValueContainText(mediaFileName);
 
-    // Assert
-    await umbracoUi.contentRender.doesContentRenderValueContainText(linkTitle);
+  // Assert
+  await umbracoUi.contentRender.doesContentRenderValueContainText(linkTitle);
 });
