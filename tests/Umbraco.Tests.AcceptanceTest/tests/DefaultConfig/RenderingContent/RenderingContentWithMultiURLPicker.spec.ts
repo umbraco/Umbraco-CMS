dc2dc090f02851acdfd06f52428dbcd24b82161a
Nhu Dinh <150406148+nhudinh0309@users.noreply.github.com>
2024-12-10 17:55:27 +0700

    V15 QA Added tests for rendering content with member picker, multi url picker and media picker (#17717)
    * Added test for rendering content with member picker value
    
    * Added tests for rendering content with multiple media picker
    
    * Added tests for rendering content with multi url picker
    
    * Bumped version
    
    * Updated tests for member picker
    
    * Make all rendering content tests run in the pipeline
    
    * Bumped version
    
    * Fixed indentation
    
    * Added clean for media
    
    * Reverted
    
    ---------
    
    Co-authored-by: Andreas Zerbst <73799582+andr317c@users.noreply.github.com>
    Co-authored-by: Andreas Zerbst <andr317c@live.dk>

---
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/RenderingContent/RenderingContentWithMultiURLPicker.spec.ts
@@ -0,0 +1,107 @@
+import {AliasHelper, test} from '@umbraco/playwright-testhelpers';
+
+const contentName = 'Test Rendering Content';
+const documentTypeName = 'TestDocumentTypeForContent';
+const dataTypeName = 'Multi URL Picker';
+const templateName = 'TestTemplateForContent';
+const propertyName = 'Test Member Picker';
+let dataTypeData = null;
+
+test.beforeEach(async ({umbracoApi}) => {
+  dataTypeData = await umbracoApi.dataType.getByName(dataTypeName);
+});
+
+test.afterEach(async ({umbracoApi}) => {
+  await umbracoApi.document.ensureNameNotExists(contentName);
+  await umbracoApi.documentType.ensureNameNotExists(documentTypeName);
+  await umbracoApi.template.ensureNameNotExists(templateName);
+});
+
+test('can render content with document link value', async ({umbracoApi, umbracoUi}) => {
+  // Arrange
+  // Create a document to link
+  const documentTypeForLinkedDocumentName = 'TestDocumentType';
+  const documentTypeForLinkedDocumentId = await umbracoApi.documentType.createDefaultDocumentTypeWithAllowAsRoot(documentTypeForLinkedDocumentName);
+  const linkedDocumentName = 'LinkedDocument';
+  const linkedDocumentId = await umbracoApi.document.createDefaultDocument(linkedDocumentName, documentTypeForLinkedDocumentId);
+  await umbracoApi.document.publish(linkedDocumentId);
+  // Create a published document with document link value
+  const templateId = await umbracoApi.template.createTemplateWithDisplayingMultiURLPickerValue(templateName, AliasHelper.toAlias(propertyName));
+  await umbracoApi.document.createPublishedDocumentWithDocumentLinkURLPicker(contentName, linkedDocumentName, linkedDocumentId, dataTypeData.id, templateId, propertyName, documentTypeName);
+  const contentData = await umbracoApi.document.getByName(contentName);
+  const contentURL = contentData.urls[0].url;
+
+  // Act
+  await umbracoUi.contentRender.navigateToRenderedContentPage(contentURL);
+
+  // Assert
+  await umbracoUi.contentRender.doesContentRenderValueContainText(linkedDocumentName);
+
+  // Clean
+  await umbracoApi.document.ensureNameNotExists(linkedDocumentName);
+  await umbracoApi.documentType.ensureNameNotExists(documentTypeForLinkedDocumentName);
+});
+
+test('can render content with media link value', async ({umbracoApi, umbracoUi}) => {
+  // Arrange
+  // Create a media to pick
+  const mediaFileName = 'TestMediaFileForContent';
+  await umbracoApi.media.ensureNameNotExists(mediaFileName);
+  const mediaFileId = await umbracoApi.media.createDefaultMediaWithImage(mediaFileName);
+  // Create a published document with media link value
+  const templateId = await umbracoApi.template.createTemplateWithDisplayingMultiURLPickerValue(templateName, AliasHelper.toAlias(propertyName));
+  await umbracoApi.document.createPublishedDocumentWithImageLinkURLPicker(contentName, mediaFileName, mediaFileId, dataTypeData.id, templateId, propertyName, documentTypeName);
+  const contentData = await umbracoApi.document.getByName(contentName);
+  const contentURL = contentData.urls[0].url;
+
+  // Act
+  await umbracoUi.contentRender.navigateToRenderedContentPage(contentURL);
+
+  // Assert
+  await umbracoUi.contentRender.doesContentRenderValueContainText(mediaFileName);
+
+  // Clean
+  await umbracoApi.media.ensureNameNotExists(mediaFileName);
+});
+
+test('can render content with external link value', async ({umbracoApi, umbracoUi}) => {
+  // Arrange
+  const linkUrl = 'https://docs.umbraco.com';
+  const linkTitle = 'Umbraco Documentation';
+  // Create a published document with external link value
+  const templateId = await umbracoApi.template.createTemplateWithDisplayingMultiURLPickerValue(templateName, AliasHelper.toAlias(propertyName));
+  await umbracoApi.document.createPublishedDocumentWithExternalLinkURLPicker(contentName, linkTitle, linkUrl, dataTypeData.id, templateId, propertyName, documentTypeName);
+  const contentData = await umbracoApi.document.getByName(contentName);
+  const contentURL = contentData.urls[0].url;
+
+  // Act
+  await umbracoUi.contentRender.navigateToRenderedContentPage(contentURL);
+
+  // Assert
+  await umbracoUi.contentRender.doesContentRenderValueContainText(linkTitle);
+});
+
+test('can render content with multiple url value', async ({umbracoApi, umbracoUi}) => {
+  // Arrange
+  const linkUrl = 'https://docs.umbraco.com';
+  const linkTitle = 'Umbraco Documentation';
+  // Create a media to pick
+  const mediaFileName = 'TestMediaFileForContent';
+  await umbracoApi.media.ensureNameNotExists(mediaFileName);
+  const mediaFileId = await umbracoApi.media.createDefaultMediaWithImage(mediaFileName);
+  // Create a published document with external link value and image url value
+  const templateId = await umbracoApi.template.createTemplateWithDisplayingMultiURLPickerValue(templateName, AliasHelper.toAlias(propertyName));
+  await umbracoApi.document.createPublishedDocumentWithImageLinkAndExternalLink(contentName, mediaFileName, mediaFileId, linkTitle, linkUrl, dataTypeData.id, templateId, propertyName, documentTypeName);
+  const contentData = await umbracoApi.document.getByName(contentName);
+  const contentURL = contentData.urls[0].url;
+
+  // Act
+  await umbracoUi.contentRender.navigateToRenderedContentPage(contentURL);
+
+  // Assert
+  await umbracoUi.contentRender.doesContentRenderValueContainText(linkTitle);
+  await umbracoUi.contentRender.doesContentRenderValueContainText(mediaFileName);
+
+  // Clean
+  await umbracoApi.media.ensureNameNotExists(mediaFileName);
+});
