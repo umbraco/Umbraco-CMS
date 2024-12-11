0f2e4b813b11e4aac06bb0f95007537908533de7
nikolajlauridsen <nikolajlauridsen@protonmail.ch>
2024-12-11 10:40:28 +0100

    Minor cleanup

--- src/Umbraco.Cms.Api.Management/ServerEvents/UserConnectionManager.cs
+++ src/Umbraco.Cms.Api.Management/ServerEvents/UserConnectionManager.cs
@@ -2,12 +2,14 @@
 
 namespace Umbraco.Cms.Api.Management.ServerEvents;
 
+/// <inheritdoc />
 internal sealed class UserConnectionManager : IUserConnectionManager
 {
     // We use a normal dictionary instead of ConcurrentDictionary, since we need to lock the set anyways.
     private readonly Dictionary<Guid, HashSet<string>> _connections = new();
     private readonly object _lock = new();
 
+    /// <inheritdoc/>
     public ISet<string> GetConnections(Guid userKey)
     {
         lock (_lock)
@@ -16,6 +18,7 @@ internal sealed class UserConnectionManager : IUserConnectionManager
         }
     }
 
+    /// <inheritdoc/>
     public void AddConnection(Guid userKey, string connectionId)
     {
         lock (_lock)
@@ -30,6 +33,7 @@ internal sealed class UserConnectionManager : IUserConnectionManager
         }
     }
 
+    /// <inheritdoc/>
     public void RemoveConnection(Guid userKey, string connectionId)
     {
         lock (_lock)
