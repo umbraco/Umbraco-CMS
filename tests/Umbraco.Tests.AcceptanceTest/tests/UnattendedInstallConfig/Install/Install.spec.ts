aaffe15ddc71fe0c6dd4adffb150debd8a4b23fd
Andreas Zerbst <andr317c@live.dk>
2024-12-12 10:12:46 +0100

    Added install test

--- tests/Umbraco.Tests.AcceptanceTest/tests/UnattendedInstallConfig/Install/Install.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/UnattendedInstallConfig/Install/Install.spec.ts
@@ -1,8 +1,28 @@
-// To be able to test different databases, we need to set an additional UnattendenInstallConfig up because we would have to start from scratch, otherwise we would be using the same database.
+// To be able to test different databases, we need to set an additional UnattendedInstallConfig up because we would have to start from scratch, otherwise we would be using the same database.
 import {expect} from '@playwright/test';
 import {ConstantHelper, test} from '@umbraco/playwright-testhelpers';
 
+const name = 'TestName';
+const email = process.env.UMBRACO_USER_LOGIN;
+const password = process.env.UMBRACO_USER_PASSWORD;
 
-test('test', async ({umbracoApi, umbracoUi}) => {
+test('Install Umbraco', async ({page, umbracoUi}) => {
+  test.slow();
   // Arrange
+  await page.goto(process.env.url + '/umbraco/install');
+
+  // Act
+  await page.getByLabel('name').fill(name);
+  await page.getByLabel('email').fill(email);
+  await page.getByLabel('password', {exact: true}).fill(password);
+  await page.getByLabel('Next').click();
+  await page.getByLabel('Next').click();
+  await page.getByLabel('Install').click();
+
+  // Assert
+  await expect(page.getByLabel('E-mail')).toBeVisible({timeout: 30000});
+  await page.getByLabel('E-mail').fill(email);
+  await page.getByLabel('Password').fill(password);
+  await page.getByLabel('Login').click();
+  await umbracoUi.content.goToSection(ConstantHelper.sections.content);
 });
