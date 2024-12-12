2d428cc0fcfc271821bccae4dcdbc4298fc81926
Niels Lyngsø <nsl@umbraco.dk>
2024-12-12 12:36:33 +0100

    Hotfix: ensure we export all extension types (#17787)
    * export all Extension Manifest Types
    
    * clean up
    
    * lint fix

--- src/Umbraco.Web.UI.Client/src/packages/core/auth/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/auth/index.ts
@@ -5,6 +5,5 @@ export * from './auth.context.token.js';
 export * from './modals/index.js';
 export type * from './models/openApiConfiguration.js';
 export * from './components/index.js';
-export type * from './auth-provider.extension.js';
 
 export type * from './types.js';
--- src/Umbraco.Web.UI.Client/src/packages/core/picker/search/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/picker/search/index.ts
@@ -1,4 +1,3 @@
 export * from './manager/index.js';
 export * from './picker-search-field.element.js';
 export * from './picker-search-result.element.js';
-export type * from './result-item/index.js';
