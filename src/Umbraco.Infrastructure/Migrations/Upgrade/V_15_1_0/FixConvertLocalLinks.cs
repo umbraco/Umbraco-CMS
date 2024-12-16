aeaa8cf910cbbe03a14d544f6e36bc80335f65d7
Sven Geusens <sge@umbraco.dk>
2024-12-16 10:42:17 +0100

    RebuildCache

--- src/Umbraco.Infrastructure/Migrations/Upgrade/V_15_1_0/FixConvertLocalLinks.cs
+++ src/Umbraco.Infrastructure/Migrations/Upgrade/V_15_1_0/FixConvertLocalLinks.cs
@@ -108,6 +108,8 @@ public class FixConvertLocalLinks : MigrationBase
                     propertyEditorAlias);
             }
         }
+
+        RebuildCache = true;
     }
 
     private bool ProcessPropertyTypes(IPropertyType[] propertyTypes, IDictionary<int, ILanguage> languagesById)
