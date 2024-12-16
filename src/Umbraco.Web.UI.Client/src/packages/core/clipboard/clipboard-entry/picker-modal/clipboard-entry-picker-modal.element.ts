e040b00e33e1171ed185d5ad133add3eddacf6d6
Mads Rasmussen <madsr@hey.com>
2024-12-16 19:11:10 +0100

    reload picker content

--- src/Umbraco.Web.UI.Client/src/packages/core/clipboard/clipboard-entry/picker-modal/clipboard-entry-picker-modal.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/clipboard/clipboard-entry/picker-modal/clipboard-entry-picker-modal.element.ts
@@ -7,6 +7,12 @@ import type {
 import { html, customElement, state, repeat } from '@umbraco-cms/backoffice/external/lit';
 import { UmbSelectionManager } from '@umbraco-cms/backoffice/utils';
 import { UmbModalBaseElement } from '@umbraco-cms/backoffice/modal';
+import { UmbEntityContext } from '@umbraco-cms/backoffice/entity';
+import { UMB_ACTION_EVENT_CONTEXT } from '@umbraco-cms/backoffice/action';
+import {
+	UmbRequestReloadChildrenOfEntityEvent,
+	UmbRequestReloadStructureForEntityEvent,
+} from '@umbraco-cms/backoffice/entity-action';
 
 @customElement('umb-clipboard-entry-picker-modal')
 export class UmbClipboardEntryPickerModalElement extends UmbModalBaseElement<
@@ -18,6 +24,14 @@ export class UmbClipboardEntryPickerModalElement extends UmbModalBaseElement<
 
 	#collectionRepository = new UmbClipboardCollectionRepository(this);
 	#selectionManager = new UmbSelectionManager(this);
+	#entityContext = new UmbEntityContext(this);
+	#actionEventContext?: typeof UMB_ACTION_EVENT_CONTEXT.TYPE;
+
+	constructor() {
+		super();
+		this.#entityContext.setEntityType('clipboard-entry');
+		this.#entityContext.setUnique(null);
+	}
 
 	override connectedCallback(): void {
 		super.connectedCallback();
@@ -28,9 +42,15 @@ export class UmbClipboardEntryPickerModalElement extends UmbModalBaseElement<
 		this.observe(this.#selectionManager.selection, (selection) => {
 			this.value = { selection };
 		});
+
+		this.#listenToEntityEvents();
 	}
 
 	override async firstUpdated() {
+		this.#requestItems();
+	}
+
+	async #requestItems() {
 		const entryType = this.data?.entry?.type;
 
 		const { data } = await this.#collectionRepository.requestCollection({ entry: { type: entryType } });
@@ -52,6 +72,49 @@ export class UmbClipboardEntryPickerModalElement extends UmbModalBaseElement<
 	#close() {
 		this.modalContext?.reject();
 	}
+
+	async #listenToEntityEvents() {
+		this.consumeContext(UMB_ACTION_EVENT_CONTEXT, (context) => {
+			this.#actionEventContext = context;
+
+			context?.removeEventListener(
+				UmbRequestReloadStructureForEntityEvent.TYPE,
+				this.#onReloadStructureRequest as unknown as EventListener,
+			);
+
+			context?.removeEventListener(
+				UmbRequestReloadChildrenOfEntityEvent.TYPE,
+				this.#onReloadChildrenRequest as unknown as EventListener,
+			);
+
+			context?.addEventListener(
+				UmbRequestReloadStructureForEntityEvent.TYPE,
+				this.#onReloadStructureRequest as unknown as EventListener,
+			);
+
+			context?.addEventListener(
+				UmbRequestReloadChildrenOfEntityEvent.TYPE,
+				this.#onReloadChildrenRequest as unknown as EventListener,
+			);
+		});
+	}
+
+	#onReloadStructureRequest = (event: UmbRequestReloadStructureForEntityEvent) => {
+		const hasItem = this._items.some((item) => item.unique === event.getUnique());
+		if (hasItem) {
+			this.#requestItems();
+		}
+	};
+
+	#onReloadChildrenRequest = async (event: UmbRequestReloadChildrenOfEntityEvent) => {
+		// check if the collection is in the same context as the entity from the event
+		const unique = this.#entityContext.getUnique();
+		const entityType = this.#entityContext.getEntityType();
+
+		if (unique === event.getUnique() && entityType === event.getEntityType()) {
+			this.#requestItems();
+		}
+	};
 
 	override render() {
 		return html`<umb-body-layout headline="Clipboard">
@@ -84,6 +147,20 @@ export class UmbClipboardEntryPickerModalElement extends UmbModalBaseElement<
 				<uui-button label="Submit" look="primary" color="positive" @click=${this.#submit}></uui-button>
 			</div>
 		</umb-body-layout> `;
+	}
+
+	override destroy(): void {
+		this.#actionEventContext?.removeEventListener(
+			UmbRequestReloadStructureForEntityEvent.TYPE,
+			this.#onReloadStructureRequest as unknown as EventListener,
+		);
+
+		this.#actionEventContext?.removeEventListener(
+			UmbRequestReloadChildrenOfEntityEvent.TYPE,
+			this.#onReloadChildrenRequest as unknown as EventListener,
+		);
+
+		super.destroy();
 	}
 }
 
