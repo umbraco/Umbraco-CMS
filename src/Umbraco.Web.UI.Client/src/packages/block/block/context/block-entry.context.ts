de963eb5ad4cfeb88dfe6d63efd07749c3149a8b
Mads Rasmussen <madsr@hey.com>
2024-12-17 14:48:30 +0100

    add get methods

--- src/Umbraco.Web.UI.Client/src/packages/block/block/context/block-entry.context.ts
+++ src/Umbraco.Web.UI.Client/src/packages/block/block/context/block-entry.context.ts
@@ -81,6 +81,30 @@ export abstract class UmbBlockEntryContext<
 	public readonly contentElementTypeAlias = this.#contentElementType.asObservablePart((x) => x?.alias);
 	public readonly contentElementTypeIcon = this.#contentElementType.asObservablePart((x) => x?.icon);
 
+	/**
+	 * Get the name of the content element type.
+	 * @returns {string | undefined} - the name of the content element type.
+	 */
+	public getContentElementTypeName(): string | undefined {
+		return this.#contentElementType.getValue()?.name;
+	}
+
+	/**
+	 * Get the alias of the content element type.
+	 * @returns {string | undefined} - the alias of the content element type.
+	 */
+	public getContentElementTypeAlias(): string | undefined {
+		return this.#contentElementType.getValue()?.alias;
+	}
+
+	/**
+	 * Get the icon of the content element type.
+	 * @returns {string | undefined} - the icon of the content element type.
+	 */
+	public getContentElementTypeIcon(): string | undefined {
+		return this.#contentElementType.getValue()?.icon;
+	}
+
 	_blockType = new UmbObjectState<BlockType | undefined>(undefined);
 	public readonly blockType = this._blockType.asObservable();
 	public readonly contentElementTypeKey = this._blockType.asObservablePart((x) => x?.contentElementTypeKey);
@@ -93,6 +117,14 @@ export abstract class UmbBlockEntryContext<
 	public readonly contentKey = this._layout.asObservablePart((x) => x?.contentKey);
 	public readonly settingsKey = this._layout.asObservablePart((x) => (x ? (x.settingsKey ?? null) : undefined));
 	public readonly unique = this._layout.asObservablePart((x) => x?.contentKey);
+
+	/**
+	 * Get the layout of the block.
+	 * @returns {BlockLayoutType | undefined} - the layout of the block.
+	 */
+	public getLayout(): BlockLayoutType | undefined {
+		return this._layout.getValue();
+	}
 
 	#label = new UmbStringState('');
 	public readonly label = this.#label.asObservable();
@@ -216,6 +248,14 @@ export abstract class UmbBlockEntryContext<
 		return this.#contentValuesObservable;
 	}
 
+	/**
+	 * Get the content of the block.
+	 * @returns {UmbBlockDataModel | undefined} - the content of the block.
+	 */
+	public getContent(): UmbBlockDataModel | undefined {
+		return this.#content.getValue();
+	}
+
 	#settings = new UmbObjectState<UmbBlockDataModel | undefined>(undefined);
 	//public readonly settings = this.#settings.asObservable();
 	protected readonly _settingsValueArray = this.#settings.asObservablePart((x) => x?.values);
@@ -241,6 +281,14 @@ export abstract class UmbBlockEntryContext<
 			);
 		}
 		return this.#settingsValuesObservable;
+	}
+
+	/**
+	 * Get the settings of the block.
+	 * @returns {UmbBlockDataModel | undefined} - the settings of the block.
+	 */
+	public getSettings(): UmbBlockDataModel | undefined {
+		return this.#settings.getValue();
 	}
 
 	abstract readonly showContentEdit: Observable<boolean>;
