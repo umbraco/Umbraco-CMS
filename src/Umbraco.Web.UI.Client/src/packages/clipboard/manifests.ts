f539805d37b16030b65ca2de895a1738f01ffba8
Mads Rasmussen <madsr@hey.com>
2024-12-17 21:19:09 +0100

    add package files

--- src/Umbraco.Web.UI.Client/src/packages/clipboard/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/clipboard/manifests.ts
@@ -1,8 +1,8 @@
-import type { UmbExtensionManifestKind } from '../core/extension-registry/registry.js';
 import { manifests as collectionManifests } from './collection/manifests.js';
 import { manifests as entryManifests } from './clipboard-entry/manifests.js';
 import { manifests as propertyActionManifests } from './property-actions/manifests.js';
 import { manifests as rootManifests } from './clipboard-root/manifests.js';
+import type { UmbExtensionManifestKind } from '@umbraco-cms/backoffice/extension-registry';
 
 export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> = [
 	...collectionManifests,
