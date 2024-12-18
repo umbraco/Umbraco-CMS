0f172ac718e20a94102346cd38d2d393635d43d5
Mads Rasmussen <madsr@hey.com>
2024-12-18 08:53:18 +0100

    remove clipboard from core vite.config

--- src/Umbraco.Web.UI.Client/src/packages/core/vite.config.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/vite.config.ts
@@ -14,7 +14,6 @@ export default defineConfig({
 			'action/index': './action/index.ts',
 			'audit-log/index': './audit-log/index.ts',
 			'auth/index': './auth/index.ts',
-			'clipboard/index': './clipboard/index.ts',
 			'collection/index': './collection/index.ts',
 			'components/index': './components/index.ts',
 			'const/index': './const/index.ts',
