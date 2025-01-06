993cee44f88335ada8737d306588a6e5ef01fee2
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-01-06 16:46:25 +0100

    fix: update "allow change password" and "allow mfa" conditions to take the user configuration into consideration

--- src/Umbraco.Web.UI.Client/src/packages/user/user/conditions/allow-change-password/user-allow-change-password-action.condition.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/conditions/allow-change-password/user-allow-change-password-action.condition.ts
@@ -1,14 +1,23 @@
-import { UmbUserKind } from '../../utils/index.js';
-import { UmbUserActionConditionBase } from '../user-allow-action-base.condition.js';
+import UmbUserConfigRepository from '../../repository/config/user-config.repository.js';
+import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
+import type { UmbConditionConfigBase } from '@umbraco-cms/backoffice/extension-api';
+import { UmbConditionBase } from '@umbraco-cms/backoffice/extension-registry';
 
-export class UmbUserAllowChangePasswordActionCondition extends UmbUserActionConditionBase {
-	async _onUserDataChange() {
-		// don't allow the current user to delete themselves
-		if (this.userKind === UmbUserKind.DEFAULT) {
-			this.permitted = true;
-		} else {
-			this.permitted = false;
-		}
+export class UmbUserAllowChangePasswordActionCondition extends UmbConditionBase<UmbConditionConfigBase> {
+	constructor(host: UmbControllerHost, args: any) {
+		super(host, args);
+
+		const configRepository = new UmbUserConfigRepository(host);
+
+		this.observe(configRepository.initialized, () => {
+			this.observe(
+				configRepository.part('allowChangePassword'),
+				(isAllowed) => {
+					this.permitted = isAllowed;
+				},
+				'_userAllowChangePasswordActionCondition',
+			);
+		});
 	}
 }
 
--- src/Umbraco.Web.UI.Client/src/packages/user/user/conditions/allow-mfa/user-allow-mfa-action.condition.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/conditions/allow-mfa/user-allow-mfa-action.condition.ts
@@ -1,17 +1,24 @@
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import { UmbConditionBase, umbExtensionsRegistry } from '@umbraco-cms/backoffice/extension-registry';
+import { observeMultiple } from '@umbraco-cms/backoffice/observable-api';
+import { UmbUserConfigRepository } from '../../repository/config/index.js';
 
 export class UmbUserAllowMfaActionCondition extends UmbConditionBase<never> {
 	// eslint-disable-next-line @typescript-eslint/no-explicit-any
 	constructor(host: UmbControllerHost, args: any) {
 		super(host, args);
 
-		// Check if there are any MFA providers available
-		this.observe(
-			umbExtensionsRegistry.byType('mfaLoginProvider'),
-			(exts) => (this.permitted = exts.length > 0),
-			'_userAllowMfaActionConditionProviders',
-		);
+		const repository = new UmbUserConfigRepository(host);
+
+		this.observe(repository.initialized, () => {
+			this.observe(
+				observeMultiple([repository.part('allowTwoFactor'), umbExtensionsRegistry.byType('mfaLoginProvider')]),
+				([allowTwoFactor, exts]) => {
+					this.permitted = allowTwoFactor && exts.length > 0;
+				},
+				'_userAllowMfaActionCondition',
+			);
+		});
 	}
 }
 
