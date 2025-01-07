00752544f66762180082944026c0c678c9bfea9a
Nikolaj Geisle <70372949+Zeegaan@users.noreply.github.com>
2025-01-07 11:36:36 +0100

    Remove old comment

--- src/Umbraco.Core/Services/DocumentUrlService.cs
+++ src/Umbraco.Core/Services/DocumentUrlService.cs
@@ -451,9 +451,6 @@ public class DocumentUrlService : IDocumentUrlService
             return "#";
         }
 
-        // We have to bypass the cache here to check if content is published, as
-        // if this is called during a <see cref="ContentPublishedNotification" /> call, the cache
-        // is still not populated with the correct urls
         if(isDraft is false && string.IsNullOrWhiteSpace(culture) is false && _publishStatusQueryService.IsDocumentPublished(documentKey, culture) is false)
         {
             return "#";
