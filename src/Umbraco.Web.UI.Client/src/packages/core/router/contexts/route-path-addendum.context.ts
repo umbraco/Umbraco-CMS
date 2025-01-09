0eef5e5801fa1a675bbb50a6c47037427da41f31
Niels Lyngsø <niels.lyngso@gmail.com>
2025-01-09 11:39:51 +0100

    context

---
+++ src/Umbraco.Web.UI.Client/src/packages/core/router/contexts/route-path-addendum.context.ts
@@ -0,0 +1,39 @@
+import type { UmbRoutePathAddendum } from './route-path-addendum.interface.js';
+import { UMB_ROUTE_PATH_ADDENDUM_CONTEXT } from './route-path-addendum.context-token.js';
+import { UmbContextBase } from '@umbraco-cms/backoffice/class-api';
+import { UmbStringState } from '@umbraco-cms/backoffice/observable-api';
+import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
+
+export class UmbRoutePathAddendumContext
+	extends UmbContextBase<UmbRoutePathAddendum, typeof UMB_ROUTE_PATH_ADDENDUM_CONTEXT>
+	implements UmbRoutePathAddendum
+{
+	#parentAddendum?: string;
+	#currentAddendum?: string;
+
+	#pathAddendum = new UmbStringState(undefined);
+	readonly addendum = this.#pathAddendum.asObservable();
+
+	constructor(host: UmbControllerHost) {
+		super(host, UMB_ROUTE_PATH_ADDENDUM_CONTEXT);
+
+		this.consumeContext(UMB_ROUTE_PATH_ADDENDUM_CONTEXT, (context) => {
+			this.observe(context.addendum, (addendum) => {
+				this.#parentAddendum = addendum;
+				this.#update();
+			});
+		}).skipHost();
+	}
+
+	setAddendum(addendum: string) {
+		this.#currentAddendum = addendum;
+		this.#update();
+	}
+
+	#update() {
+		if (this.#parentAddendum === undefined || this.#currentAddendum === undefined) {
+			return;
+		}
+		this.#pathAddendum.setValue(this.#parentAddendum + this.#currentAddendum);
+	}
+}
