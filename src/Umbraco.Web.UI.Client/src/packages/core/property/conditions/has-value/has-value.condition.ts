95685c2aa5d8cd5391f4232f946014a7ca07a6fe
Mads Rasmussen <madsr@hey.com>
2025-01-09 13:45:43 +0100

    use config type

--- src/Umbraco.Web.UI.Client/src/packages/core/property/conditions/has-value/has-value.condition.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property/conditions/has-value/has-value.condition.ts
@@ -1,17 +1,14 @@
 import { UMB_PROPERTY_CONTEXT } from '../../components/index.js';
+import type { UmbPropertyHasValueConditionConfig } from './types.js';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
-import type {
-	UmbConditionConfigBase,
-	UmbConditionControllerArguments,
-	UmbExtensionCondition,
-} from '@umbraco-cms/backoffice/extension-api';
+import type { UmbConditionControllerArguments, UmbExtensionCondition } from '@umbraco-cms/backoffice/extension-api';
 import { UmbConditionBase } from '@umbraco-cms/backoffice/extension-registry';
 
 export class UmbPropertyHasValueCondition
-	extends UmbConditionBase<UmbConditionConfigBase>
+	extends UmbConditionBase<UmbPropertyHasValueConditionConfig>
 	implements UmbExtensionCondition
 {
-	constructor(host: UmbControllerHost, args: UmbConditionControllerArguments<UmbConditionConfigBase>) {
+	constructor(host: UmbControllerHost, args: UmbConditionControllerArguments<UmbPropertyHasValueConditionConfig>) {
 		super(host, args);
 
 		this.consumeContext(UMB_PROPERTY_CONTEXT, (context) => {
