d253360d01bfb3e805f6fe694a18de766c5b44d0
Mads Rasmussen <madsr@hey.com>
2025-01-09 13:39:01 +0100

    add condition config type

--- src/Umbraco.Web.UI.Client/src/packages/core/property/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property/types.ts
@@ -1,3 +1,4 @@
-export type * from './types/index.js';
-export type * from './property-value-resolver/types.js';
+export type * from './conditions/types.js';
 export type * from './property-value-cloner/types.js';
+export type * from './property-value-resolver/types.js';
+export type * from './types/index.js';
