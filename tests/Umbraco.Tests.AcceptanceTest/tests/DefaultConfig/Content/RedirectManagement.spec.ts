3e054d1b5470f4ced5440d27a4b72a20ee7d71cf
Nhu Dinh <hnd@umbraco.dk>
2025-01-09 14:13:36 +0700

    Updated tests for redirect management

--- tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Content/RedirectManagement.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Content/RedirectManagement.spec.ts
@@ -11,15 +11,11 @@ const updatedContentName = 'UpdatedContentName';
 
 test.beforeEach(async ({umbracoApi, umbracoUi}) => {
   await umbracoApi.redirectManagement.setStatus(enableStatus);
-  await umbracoUi.goToBackOffice();
-  // Create a content
-  await umbracoApi.documentType.ensureNameNotExists(documentTypeName);
+  // Create a published content
   documentTypeId = await umbracoApi.documentType.createDefaultDocumentTypeWithAllowAsRoot(documentTypeName);
   contentId = await umbracoApi.document.createDefaultDocument(contentName, documentTypeId);
-  // Publish the content
-  await umbracoUi.content.goToSection(ConstantHelper.sections.content);
-  await umbracoUi.content.clickActionsMenuForContent(contentName);
-  await umbracoUi.content.clickPublishButton();
+  await umbracoApi.document.publish(contentId);
+  await umbracoUi.goToBackOffice();
 });
 
 test.afterEach(async ({umbracoApi}) => {
@@ -52,8 +48,7 @@ test('can disable URL tracker', async ({umbracoApi, umbracoUi}) => {
   expect(statusData.status).toBe(disableStatus);
 });
 
-// TODO: Remove skip when the frond-end is ready. Currently there is no redirect have been made after renaming a published page
-test.skip('can re-enable URL tracker', async ({umbracoApi, umbracoUi}) => {
+test('can re-enable URL tracker', async ({umbracoApi, umbracoUi}) => {
   // Arrange
   await umbracoApi.redirectManagement.setStatus(disableStatus);
 
@@ -61,42 +56,45 @@ test.skip('can re-enable URL tracker', async ({umbracoApi, umbracoUi}) => {
   await umbracoUi.content.goToSection(ConstantHelper.sections.content);
   await umbracoUi.redirectManagement.clickRedirectManagementTab();
   await umbracoUi.redirectManagement.clickEnableURLTrackerButton();
-
-  // Assert
-  // Verify that if renaming a published page, there are one redirects have been made
   // rename the published content
   await umbracoUi.content.goToSection(ConstantHelper.sections.content);
   await umbracoUi.content.goToContentWithName(contentName);
   await umbracoUi.content.enterContentName(updatedContentName);
   await umbracoUi.content.clickSaveAndPublishButton();
+
+  // Assert
+  // Verify that if renaming a published page, there are one redirects have been made
   // verify that there is one redirects have been made
   const contentData = await umbracoApi.document.get(contentId);
   await umbracoUi.content.goToSection(ConstantHelper.sections.content);
   await umbracoUi.redirectManagement.clickRedirectManagementTab();
-  await umbracoUi.redirectManagement.isTextWithExactNameVisible(contentData.urls[0].url);
+  await umbracoUi.redirectManagement.isTextWithExactNameVisible(contentData.urls[0].url + '/');
   // Verify that the status is Enable
   const statusData = await umbracoApi.redirectManagement.getStatus();
   expect(statusData.status).toBe(enableStatus);
 });
 
-// TODO: Remove skip and update this when the front-end is ready. Currently it always return "No redirects matching this search criteria" when searching.
-test.skip('can search for original URL', async ({umbracoUi}) => {
+test('can search for original URL', async ({umbracoUi, umbracoApi}) => {
   // Arrange
-  const searchKeyword = '/test-content/';
-  // TODO: rename content to add an item in the redirect url management
+  await umbracoUi.content.goToSection(ConstantHelper.sections.content);
+  await umbracoUi.content.goToContentWithName(contentName);
+  await umbracoUi.content.enterContentName(updatedContentName);
+  await umbracoUi.content.clickSaveAndPublishButton();
 
   // Act
+  const contentData = await umbracoApi.document.get(contentId);
+  const searchKeyword = contentData.urls[0].url;
   await umbracoUi.content.goToSection(ConstantHelper.sections.content);
   await umbracoUi.redirectManagement.clickRedirectManagementTab();
   await umbracoUi.redirectManagement.enterOriginalUrl(searchKeyword);
   await umbracoUi.redirectManagement.clickSearchButton();
 
   // Assert
-  // TODO: verify the search result
+  const resultData = await umbracoApi.redirectManagement.filterByText(searchKeyword);
+  await umbracoUi.redirectManagement.doesRedirectManagementRowsHaveCount(resultData.total);
 });
 
-// TODO: Remove skip when the frond-end is ready. Currently there is no redirect have been made after renaming a published page
-test.skip('can delete a redirect', async ({umbracoApi, umbracoUi}) => {
+test('can delete a redirect', async ({umbracoApi, umbracoUi}) => {
   // Arrange
   // Rename the published content
   await umbracoUi.content.goToSection(ConstantHelper.sections.content);
@@ -105,6 +103,7 @@ test.skip('can delete a redirect', async ({umbracoApi, umbracoUi}) => {
   await umbracoUi.content.clickSaveAndPublishButton();
 
   // Act
+  await umbracoUi.waitForTimeout(1000);
   await umbracoUi.content.goToSection(ConstantHelper.sections.content);
   await umbracoUi.redirectManagement.clickRedirectManagementTab();
   await umbracoUi.redirectManagement.deleteFirstRedirectURL();
