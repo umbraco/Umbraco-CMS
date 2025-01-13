58ec6ca5ac382b09fe8c60ebe799c618c7415203
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-01-13 17:35:43 +0100

    feat: make class abstract and add explanatory comment

--- src/Umbraco.Web.UI.Client/src/packages/core/workspace/entity-detail/entity-named-detail-workspace-base.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/workspace/entity-detail/entity-named-detail-workspace-base.ts
@@ -4,7 +4,7 @@ import type { UmbEntityDetailWorkspaceContextCreateArgs } from './types.js';
 import type { UmbNamedEntityModel } from '@umbraco-cms/backoffice/entity';
 import type { UmbDetailRepository } from '@umbraco-cms/backoffice/repository';
 
-export class UmbEntityNamedDetailWorkspaceContextBase<
+export abstract class UmbEntityNamedDetailWorkspaceContextBase<
 		NamedDetailModelType extends UmbNamedEntityModel = UmbNamedEntityModel,
 		NamedDetailRepositoryType extends
 			UmbDetailRepository<NamedDetailModelType> = UmbDetailRepository<NamedDetailModelType>,
@@ -24,6 +24,7 @@ export class UmbEntityNamedDetailWorkspaceContextBase<
 	}
 
 	setName(name: string | undefined) {
+		// We have to cast to Partial because TypeScript doesn't understand that the model has a name property due to generic sub-types
 		this._data.updateCurrent({ name } as Partial<NamedDetailModelType>);
 	}
 }
