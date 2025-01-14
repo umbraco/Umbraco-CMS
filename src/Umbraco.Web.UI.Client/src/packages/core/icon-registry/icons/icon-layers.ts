482af686a0b1711396ca9cff93bc516add710f3e
Mathias Helsengren <mhe@umbraco.dk>
2025-01-14 15:50:33 +0100

    Updated the npm packages (#17975)
    * Updated the npm packages
    
    * generated server api and icons

--- src/Umbraco.Web.UI.Client/src/packages/core/icon-registry/icons/icon-layers.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/icon-registry/icons/icon-layers.ts
@@ -1,16 +1,1 @@
-export default `<!-- @license lucide-static v0.460.0 - ISC -->
-<svg
-  class="lucide lucide-layers-3"
-  xmlns="http://www.w3.org/2000/svg"
-  viewBox="0 0 24 24"
-  fill="none"
-  stroke="currentColor"
-  stroke-width="1.75"
-  stroke-linecap="round"
-  stroke-linejoin="round"
->
-  <path d="m12.83 2.18a2 2 0 0 0-1.66 0L2.6 6.08a1 1 0 0 0 0 1.83l8.58 3.91a2 2 0 0 0 1.66 0l8.58-3.9a1 1 0 0 0 0-1.83Z" />
-  <path d="m6.08 9.5-3.5 1.6a1 1 0 0 0 0 1.81l8.6 3.91a2 2 0 0 0 1.65 0l8.58-3.9a1 1 0 0 0 0-1.83l-3.5-1.59" />
-  <path d="m6.08 14.5-3.5 1.6a1 1 0 0 0 0 1.81l8.6 3.91a2 2 0 0 0 1.65 0l8.58-3.9a1 1 0 0 0 0-1.83l-3.5-1.59" />
-</svg>
-`;
+export default `<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" fill="currentColor"><path d="M194.227 248.271l-43.363-41.089h-80.61l102.62 97.286h268.411l-59.317-56.197H194.227zm9.942-24.934H472.55l-102.648-97.29H101.518l102.651 97.29zm-41.239 106.05l-43.33-41.075H38.99l102.651 97.283h268.383l-59.32-56.208H162.93z"/></svg>`;
