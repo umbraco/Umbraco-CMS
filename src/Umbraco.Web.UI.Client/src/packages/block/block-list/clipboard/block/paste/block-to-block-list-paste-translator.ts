c6e0c39ba593cae361d2bdae9e8ca7a6bce42530
Mads Rasmussen <madsr@hey.com>
2025-01-14 16:40:53 +0100

    add compatability check for block list

--- src/Umbraco.Web.UI.Client/src/packages/block/block-list/clipboard/block/paste/block-to-block-list-paste-translator.ts
+++ src/Umbraco.Web.UI.Client/src/packages/block/block-list/clipboard/block/paste/block-to-block-list-paste-translator.ts
@@ -8,6 +8,16 @@ export class UmbBlockToBlockListClipboardPastePropertyValueTranslator
 	extends UmbControllerBase
 	implements UmbClipboardPastePropertyValueTranslator<UmbBlockClipboardEntryValueModel, UmbBlockListValueModel>
 {
+	async isCompatible(
+		value: UmbBlockClipboardEntryValueModel,
+		config: Array<{ alias: string; value: Array<{ contentElementTypeKey: string }> }>,
+	): Promise<boolean> {
+		const allowedBlockContentTypes =
+			config.find((c) => c.alias === 'blocks')?.value.map((b) => b.contentElementTypeKey) ?? [];
+		const blockContentTypes = value.contentData.map((c) => c.contentTypeKey);
+		return blockContentTypes?.every((b) => allowedBlockContentTypes.includes(b)) ?? false;
+	}
+
 	async translate(value: UmbBlockClipboardEntryValueModel) {
 		if (!value) {
 			throw new Error('Value is missing.');
