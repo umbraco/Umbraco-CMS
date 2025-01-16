c56cefe0368acae6a601ea14a31c2a32854e8c4a
Andreas Zerbst <andr317c@live.dk>
2025-01-16 15:50:24 +0100

    Made adjustments to pipeline

--- tests/Umbraco.Tests.AcceptanceTest/playwright.config.ts
+++ tests/Umbraco.Tests.AcceptanceTest/playwright.config.ts
@@ -23,7 +23,7 @@ export default defineConfig({
   // We don't want to run parallel, as tests might differ in state
   workers: 1,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  reporter: process.env.CI ? 'line' : 'html',
+  reporter: process.env.CI ? 'blob' : 'html',
   outputDir: "./results",
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
