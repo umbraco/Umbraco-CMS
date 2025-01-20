d46ab488ffe3c1da0802c078fcac60bcf25823bf
Jonathan <jpk@umbraco.dk>
2025-01-20 15:05:51 +0100

    Minor change to the modal headline

--- src/Umbraco.Web.UI.Client/src/assets/lang/en-us.ts
+++ src/Umbraco.Web.UI.Client/src/assets/lang/en-us.ts
@@ -568,7 +568,7 @@ export default {
 		linkToMedia: 'Link to media',
 		selectContentStartNode: 'Select content start node',
 		selectMedia: 'Select media',
-		chooseMedia: 'Choose media',
+		chooseMedia: 'Choose Media Start nodes',
 		selectMediaType: 'Select media type',
 		selectIcon: 'Select icon',
 		selectItem: 'Select item',
