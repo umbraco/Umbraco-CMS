076733dcc2f99598b35a53330b2df1c258bfbeed
Mads Rasmussen <madsr@hey.com>
2025-01-20 07:27:07 +0100

    export consts

--- src/Umbraco.Web.UI.Client/src/packages/media/media-types/constants.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media-types/constants.ts
@@ -1,4 +1,6 @@
 export * from './entity-actions/constants.js';
+export * from './media-type-root/constants.js';
+export * from './paths.js';
 export * from './repository/constants.js';
 export * from './tree/constants.js';
 export * from './workspace/constants.js';
