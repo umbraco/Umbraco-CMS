a6b9f201b34fe7a54128509c3ad43d0948ecef17
Mads Rasmussen <madsr@hey.com>
2025-01-20 09:27:57 +0100

    Feature: Data type create options (#18023)
    * add folder create option action kind
    
    * register data type create options
    
    * register folder collection actions
    
    * use kind for document type folder option
    
    * fix types
    
    * fix weight
    
    * update translations
    
    * add icon label, description
    
    * add deprecation warning

--- src/Umbraco.Web.UI.Client/src/packages/documents/document-types/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/document-types/manifests.ts
@@ -5,8 +5,9 @@ import { manifests as repositoryManifests } from './repository/manifests.js';
 import { manifests as searchManifests } from './search/manifests.js';
 import { manifests as treeManifests } from './tree/manifests.js';
 import { manifests as workspaceManifests } from './workspace/manifests.js';
+import type { UmbExtensionManifestKind } from '@umbraco-cms/backoffice/extension-registry';
 
-export const manifests: Array<UmbExtensionManifest> = [
+export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> = [
 	...entityActionsManifests,
 	...menuManifests,
 	...propertyEditorManifests,
