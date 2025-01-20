7eb9f5d2cd67ddd4b1e68649602a00a3a5599b31
Mads Rasmussen <madsr@hey.com>
2025-01-20 14:32:31 +0100

    more type safety

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/search/document-search.repository.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/search/document-search.repository.ts
@@ -1,6 +1,6 @@
 import { UmbDocumentSearchServerDataSource } from './document-search.server.data-source.js';
-import type { UmbDocumentSearchItemModel } from './types.js';
-import type { UmbSearchRepository, UmbSearchRequestArgs } from '@umbraco-cms/backoffice/search';
+import type { UmbDocumentSearchItemModel, UmbDocumentSearchRequestArgs } from './types.js';
+import type { UmbSearchRepository } from '@umbraco-cms/backoffice/search';
 import { UmbControllerBase } from '@umbraco-cms/backoffice/class-api';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import type { UmbApi } from '@umbraco-cms/backoffice/extension-api';
@@ -17,7 +17,7 @@ export class UmbDocumentSearchRepository
 		this.#dataSource = new UmbDocumentSearchServerDataSource(this);
 	}
 
-	search(args: UmbSearchRequestArgs) {
+	search(args: UmbDocumentSearchRequestArgs) {
 		return this.#dataSource.search(args);
 	}
 }
