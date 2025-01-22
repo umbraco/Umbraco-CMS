a19578004f1b8e4bc8ad8ffb129efba7188afccc
Mads Rasmussen <madsr@hey.com>
2025-01-22 10:16:41 +0100

    require permission to update + publish to render "save and publish"

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/publishing/publish/workspace-action/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/publishing/publish/workspace-action/manifests.ts
@@ -1,4 +1,9 @@
 import { UMB_DOCUMENT_WORKSPACE_ALIAS } from '../../../workspace/constants.js';
+import {
+	UMB_DOCUMENT_USER_PERMISSION_CONDITION_ALIAS,
+	UMB_USER_PERMISSION_DOCUMENT_PUBLISH,
+	UMB_USER_PERMISSION_DOCUMENT_UPDATE,
+} from '../../../user-permissions/constants.js';
 import { UMB_WORKSPACE_CONDITION_ALIAS } from '@umbraco-cms/backoffice/workspace';
 import { UMB_ENTITY_IS_NOT_TRASHED_CONDITION_ALIAS } from '@umbraco-cms/backoffice/recycle-bin';
 
@@ -19,6 +24,10 @@ export const manifests: Array<UmbExtensionManifest> = [
 			{
 				alias: UMB_WORKSPACE_CONDITION_ALIAS,
 				match: UMB_DOCUMENT_WORKSPACE_ALIAS,
+			},
+			{
+				alias: UMB_DOCUMENT_USER_PERMISSION_CONDITION_ALIAS,
+				allOf: [UMB_USER_PERMISSION_DOCUMENT_UPDATE, UMB_USER_PERMISSION_DOCUMENT_PUBLISH],
 			},
 			{
 				alias: UMB_ENTITY_IS_NOT_TRASHED_CONDITION_ALIAS,
