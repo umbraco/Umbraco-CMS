8c0b19e55b69ad741523d6eed8d6aaa68c73a6f3
Mads Rasmussen <madsr@hey.com>
2025-01-22 10:15:38 +0100

    require permission to create or update to render save action

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/workspace/actions/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/workspace/actions/manifests.ts
@@ -1,4 +1,9 @@
 import { UMB_DOCUMENT_WORKSPACE_ALIAS } from '../constants.js';
+import {
+	UMB_DOCUMENT_USER_PERMISSION_CONDITION_ALIAS,
+	UMB_USER_PERMISSION_DOCUMENT_CREATE,
+	UMB_USER_PERMISSION_DOCUMENT_UPDATE,
+} from '../../user-permissions/constants.js';
 import { UMB_ENTITY_IS_NOT_TRASHED_CONDITION_ALIAS } from '@umbraco-cms/backoffice/recycle-bin';
 import { UMB_WORKSPACE_CONDITION_ALIAS } from '@umbraco-cms/backoffice/workspace';
 
@@ -19,6 +24,10 @@ export const manifests: Array<UmbExtensionManifest> = [
 			{
 				alias: UMB_WORKSPACE_CONDITION_ALIAS,
 				match: UMB_DOCUMENT_WORKSPACE_ALIAS,
+			},
+			{
+				alias: UMB_DOCUMENT_USER_PERMISSION_CONDITION_ALIAS,
+				oneOf: [UMB_USER_PERMISSION_DOCUMENT_CREATE, UMB_USER_PERMISSION_DOCUMENT_UPDATE],
 			},
 			{
 				alias: UMB_ENTITY_IS_NOT_TRASHED_CONDITION_ALIAS,
