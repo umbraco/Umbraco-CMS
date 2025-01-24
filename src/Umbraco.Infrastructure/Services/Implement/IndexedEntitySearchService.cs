3a721986e69c6660c6d48a3ea34b68be4adfde4f
kjac <kja@umbraco.dk>
2025-01-24 10:33:08 +0100

    Fix indexed search for specific member and media types

--- src/Umbraco.Infrastructure/Services/Implement/IndexedEntitySearchService.cs
+++ src/Umbraco.Infrastructure/Services/Implement/IndexedEntitySearchService.cs
@@ -15,20 +15,31 @@ internal sealed class IndexedEntitySearchService : IIndexedEntitySearchService
     private readonly IBackOfficeExamineSearcher _backOfficeExamineSearcher;
     private readonly IEntityService _entityService;
     private readonly IContentTypeService _contentTypeService;
+    private readonly IMediaTypeService _mediaTypeService;
+    private readonly IMemberTypeService _memberTypeService;
 
     public IndexedEntitySearchService(IBackOfficeExamineSearcher backOfficeExamineSearcher, IEntityService entityService)
-        : this(backOfficeExamineSearcher, entityService, StaticServiceProvider.Instance.GetRequiredService<IContentTypeService>())
+        : this(
+            backOfficeExamineSearcher,
+            entityService,
+            StaticServiceProvider.Instance.GetRequiredService<IContentTypeService>(),
+            StaticServiceProvider.Instance.GetRequiredService<IMediaTypeService>(),
+            StaticServiceProvider.Instance.GetRequiredService<IMemberTypeService>())
     {
     }
 
     public IndexedEntitySearchService(
         IBackOfficeExamineSearcher backOfficeExamineSearcher,
         IEntityService entityService,
-        IContentTypeService contentTypeService)
+        IContentTypeService contentTypeService,
+        IMediaTypeService mediaTypeService,
+        IMemberTypeService memberTypeService)
     {
         _backOfficeExamineSearcher = backOfficeExamineSearcher;
         _entityService = entityService;
         _contentTypeService = contentTypeService;
+        _mediaTypeService = mediaTypeService;
+        _memberTypeService = memberTypeService;
     }
 
     public PagedModel<IEntitySlim> Search(UmbracoObjectTypes objectType, string query, int skip = 0, int take = 100, bool ignoreUserStartNodes = false)
@@ -64,7 +75,13 @@ internal sealed class IndexedEntitySearchService : IIndexedEntitySearchService
 
         Guid[]? contentTypeIdsAsArray = contentTypeIds as Guid[] ?? contentTypeIds?.ToArray();
         var contentTypeAliases = contentTypeIdsAsArray?.Length > 0
-            ? _contentTypeService.GetMany(contentTypeIdsAsArray).Select(x => x.Alias).ToArray()
+            ? (entityType switch
+            {
+                UmbracoEntityTypes.Document => _contentTypeService.GetMany(contentTypeIdsAsArray).Select(x => x.Alias),
+                UmbracoEntityTypes.Media => _mediaTypeService.GetMany(contentTypeIdsAsArray).Select(x => x.Alias),
+                UmbracoEntityTypes.Member => _memberTypeService.GetMany(contentTypeIdsAsArray).Select(x => x.Alias),
+                _ => throw new NotSupportedException("This service only supports searching for documents, media and members")
+            }).ToArray()
             : null;
 
         IEnumerable<ISearchResult> searchResults = _backOfficeExamineSearcher.Search(
