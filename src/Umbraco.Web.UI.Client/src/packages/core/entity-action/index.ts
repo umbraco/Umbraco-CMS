86f034b4f937545b467a8c76ff7a479f4f68495f
Mads Rasmussen <madsr@hey.com>
2025-01-29 13:14:46 +0100

    export

--- src/Umbraco.Web.UI.Client/src/packages/core/entity-action/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/entity-action/index.ts
@@ -5,6 +5,8 @@ export * from './constants.js';
 export * from './entity-action-base.js';
 export * from './entity-action-list.element.js';
 export * from './entity-action.event.js';
+export * from './has-children/index.js';
+
 export type * from './types.js';
 
 export { UmbRequestReloadStructureForEntityEvent } from './request-reload-structure-for-entity.event.js';
