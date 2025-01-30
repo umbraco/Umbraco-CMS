60c344e41b2ae318827564a8dcbb39f742d393e2
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-01-30 08:56:27 +0100

    Update src/Umbraco.Web.UI.Client/src/packages/core/utils/bytes/bytes.function.ts
    Co-authored-by: Lee Kelleher <leekelleher@users.noreply.github.com>

--- src/Umbraco.Web.UI.Client/src/packages/core/utils/bytes/bytes.function.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/utils/bytes/bytes.function.ts
@@ -1,9 +1,8 @@
 /* This Source Code has been derived from Lee Kelleher's Contentment.
  * https://github.com/leekelleher/umbraco-contentment/blob/develop/src/Umbraco.Community.Contentment/DataEditors/Bytes/bytes.js
+ * SPDX-License-Identifier: MPL-2.0
  * Copyright © 2019 Lee Kelleher.
- * This Source Code Form is subject to the terms of the Mozilla Public
- * License, v. 2.0. If a copy of the MPL was not distributed with this
- * file, You can obtain one at https://mozilla.org/MPL/2.0/. */
+ * Modifications are licensed under the MIT License.
 
 export interface IFormatBytesOptions {
 	/**
