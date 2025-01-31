73a69fa250eb7f8ae920435a8f9c1a2f3b6a0ce4
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-01-31 09:46:21 +0100

    use correct styling

--- src/Umbraco.Web.UI.Client/src/packages/media/media/components/input-upload-field/input-upload-field.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/components/input-upload-field/input-upload-field.element.ts
@@ -274,7 +274,7 @@ export class UmbInputUploadFieldElement extends UmbLitElement {
 			#badge {
 				position: absolute;
 				top: 50%;
-				left: 50;
+				left: 50%;
 				transform: translate(-50%, -50%);
 				width: 100px;
 
