dbd6e009f13113ebe8f8e852a922b6af31b6ed77
Mads Rasmussen <madsr@hey.com>
2025-02-01 22:17:38 +0100

    test register permission

--- src/Umbraco.Web.UI.Client/src/packages/user/user-permission/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user-permission/manifests.ts
@@ -1,3 +1,16 @@
 import { manifests as userPermissionModalManifests } from './modals/manifests.js';
 
-export const manifests: Array<UmbExtensionManifest> = [...userPermissionModalManifests];
+export const manifests: Array<UmbExtensionManifest> = [
+	...userPermissionModalManifests,
+	{
+		type: 'userPermission',
+		alias: 'Umb.UserPermission.Developer',
+		name: 'Developer User Permission',
+		weight: 1000,
+		meta: {
+			label: 'Developer User Permission',
+			description: 'Enables access to developer hints and tools.',
+			verbs: ['Umb.Developer'],
+		},
+	},
+];
