13458a961a977a15c2ea7f19aa063f8248a72589
Mads Rasmussen <madsr@hey.com>
2025-02-01 22:17:23 +0100

    export type

--- src/Umbraco.Web.UI.Client/src/packages/user/user-permission/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user-permission/types.ts
@@ -1,5 +1,6 @@
 export type * from './user-granular-permission.extension.js';
 export type * from './entity-user-permission.extension.js';
+export type * from './user-permission/types.js';
 export interface UmbUserPermissionModel {
 	$type: string;
 	verbs: Array<string>;
