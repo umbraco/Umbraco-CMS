c31be7103e13ec68a3f1f1d30c9d01fa5d0e1bbb
Mads Rasmussen <madsr@hey.com>
2025-02-03 09:33:26 +0100

    remove double registration

--- src/Umbraco.Web.UI.Client/src/packages/webhook/collection/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/webhook/collection/manifests.ts
@@ -1,4 +1,3 @@
 import { manifests as webhooksManifests } from './webhooks/manifests.js';
-import { manifests as deliveriesManifests } from '../delivery/collection/manifests.js';
 
-export const manifests: Array<UmbExtensionManifest> = [...webhooksManifests, ...deliveriesManifests];
+export const manifests: Array<UmbExtensionManifest> = [...webhooksManifests];
