81d0ec09162ad109da06609c2437265d45c2bb5e
Mads Rasmussen <madsr@hey.com>
2025-02-03 13:55:15 +0100

    export delivery consts

--- src/Umbraco.Web.UI.Client/src/packages/webhook/constants.ts
+++ src/Umbraco.Web.UI.Client/src/packages/webhook/constants.ts
@@ -1,4 +1,5 @@
-export * from './webhook/constants.js';
 export * from './components/constants.js';
+export * from './delivery/constants.js';
 export * from './repository/constants.js';
 export * from './webhook-root/constants.js';
+export * from './webhook/constants.js';
