588553471408754384afd90339f5382ce7a4f43c
Andy Butland <abutland73@gmail.com>
2025-02-04 10:11:00 +0100

    Update typed client models.

--- src/Umbraco.Web.UI.Client/src/external/backend-api/src/types.gen.ts
+++ src/Umbraco.Web.UI.Client/src/external/backend-api/src/types.gen.ts
@@ -423,6 +423,8 @@ export type CreateUserRequestModel = {
 
 export type CreateWebhookRequestModel = {
     enabled: boolean;
+    name?: (string) | null;
+    description?: (string) | null;
     url: string;
     contentTypeKeys: Array<(string)>;
     headers: {
@@ -2670,6 +2672,8 @@ export type UpdateUserRequestModel = {
 
 export type UpdateWebhookRequestModel = {
     enabled: boolean;
+    name?: (string) | null;
+    description?: (string) | null;
     url: string;
     contentTypeKeys: Array<(string)>;
     headers: {
@@ -2904,6 +2908,8 @@ export type WebhookLogResponseModel = {
 
 export type WebhookResponseModel = {
     enabled: boolean;
+    name?: (string) | null;
+    description?: (string) | null;
     url: string;
     contentTypeKeys: Array<(string)>;
     headers: {
