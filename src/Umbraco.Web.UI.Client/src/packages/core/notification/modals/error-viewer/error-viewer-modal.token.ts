cab07ba6f6e5d5e608941de7321f714e55b32df2
Niels Lyngsø <nsl@umbraco.dk>
2025-02-04 13:44:25 +0100

    Update src/Umbraco.Web.UI.Client/src/packages/core/notification/modals/error-viewer/error-viewer-modal.token.ts
    Co-authored-by: Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>

--- src/Umbraco.Web.UI.Client/src/packages/core/notification/modals/error-viewer/error-viewer-modal.token.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/notification/modals/error-viewer/error-viewer-modal.token.ts
@@ -1,5 +1,5 @@
 import { UmbModalToken } from '@umbraco-cms/backoffice/modal';
-import type { UmbPeekErrorArgs } from '../../types';
+import type { UmbPeekErrorArgs } from '../../types.js';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface UmbErrorViewerModalData extends UmbPeekErrorArgs {}
