17d0a16e88dc63c6c9db0242973b2102cbd28ca0
Nhu Dinh <hnd@umbraco.dk>
2025-02-05 14:41:08 +0700

    Cleaned up

--- tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Content/ContentWithMultiURLPicker.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Content/ContentWithMultiURLPicker.spec.ts
@@ -284,7 +284,7 @@ test('cannot submit the empty link', async ({umbracoApi, umbracoUi}) => {
   await umbracoUi.content.clickAddButton();
 
   // Assert
-  await umbracoUi.content.isTextWithMessageVisible(warningText);
+  await umbracoUi.content.isTextWithMessageVisible(warningEmptyLink);
 });
 
 test('cannot update the URL picker with empty link', async ({umbracoApi, umbracoUi}) => {
