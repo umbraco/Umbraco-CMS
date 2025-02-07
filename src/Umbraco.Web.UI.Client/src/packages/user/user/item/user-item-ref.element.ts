1adf436e5c037390d06310965286b5e339630597
Mads Rasmussen <madsr@hey.com>
2025-02-07 14:06:31 +0100

    implement for user input

---
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/item/user-item-ref.element.ts
@@ -0,0 +1,68 @@
+import { UMB_USER_ENTITY_TYPE } from '../entity.js';
+import type { UmbUserItemModel } from '../repository/index.js';
+import { css, customElement, html, nothing, property } from '@umbraco-cms/backoffice/external/lit';
+import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
+import { UmbModalRouteRegistrationController } from '@umbraco-cms/backoffice/router';
+import { UMB_WORKSPACE_MODAL } from '@umbraco-cms/backoffice/workspace';
+
+@customElement('umb-user-item-ref')
+export class UmbDocumentItemRefElement extends UmbLitElement {
+	@property({ type: Object })
+	item?: UmbUserItemModel;
+
+	@property({ type: Boolean })
+	readonly = false;
+
+	@property({ type: Boolean })
+	standalone = false;
+
+	_editPath = '';
+
+	constructor() {
+		super();
+
+		new UmbModalRouteRegistrationController(this, UMB_WORKSPACE_MODAL)
+			.addAdditionalPath(UMB_USER_ENTITY_TYPE)
+			.onSetup(() => {
+				return { data: { entityType: UMB_USER_ENTITY_TYPE, preset: {} } };
+			})
+			.observeRouteBuilder((routeBuilder) => {
+				this._editPath = routeBuilder({});
+			});
+	}
+
+	#getHref(item: UmbUserItemModel) {
+		return `${this._editPath}/edit/${item.unique}`;
+	}
+
+	override render() {
+		if (!this.item) return nothing;
+
+		return html`
+			<uui-ref-node-user name=${this.item.name} ?readonly=${this.readonly} ?standalone=${this.standalone}>
+				<umb-user-avatar
+					slot="icon"
+					.name=${this.item.name}
+					.kind=${this.item.kind}
+					.imgUrls=${this.item.avatarUrls}></umb-user-avatar>
+				<slot name="actions" slot="actions"></slot>
+			</uui-ref-node-user>
+		`;
+	}
+
+	static override styles = [
+		css`
+			umb-user-avatar {
+				font-size: var(--uui-size-4);
+			}
+		`,
+	];
+}
+
+export { UmbDocumentItemRefElement as element };
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'umb-document-item-ref': UmbDocumentItemRefElement;
+	}
+}
