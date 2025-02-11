13d5feaf3092d2e811027eeafad015ec2ec4c391
Mads Rasmussen <madsr@hey.com>
2025-02-11 10:03:15 +0100

    append unique

--- src/Umbraco.Web.UI.Client/src/packages/user/user/item/user-item-ref.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/item/user-item-ref.element.ts
@@ -1,17 +1,41 @@
 import { UMB_USER_ENTITY_TYPE } from '../entity.js';
 import type { UmbUserItemModel } from '../repository/index.js';
+import { UMB_USER_MANAGEMENT_SECTION_ALIAS } from '../../section/constants.js';
 import { css, customElement, html, nothing, property, state } from '@umbraco-cms/backoffice/external/lit';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
 import { UmbModalRouteRegistrationController } from '@umbraco-cms/backoffice/router';
 import { UMB_SECTION_USER_PERMISSION_CONDITION_ALIAS } from '@umbraco-cms/backoffice/section';
 import { UMB_WORKSPACE_MODAL } from '@umbraco-cms/backoffice/workspace';
-import { UMB_USER_MANAGEMENT_SECTION_ALIAS } from '../../section/constants.js';
 import { createExtensionApiByAlias } from '@umbraco-cms/backoffice/extension-registry';
 
 @customElement('umb-user-item-ref')
 export class UmbUserItemRefElement extends UmbLitElement {
+	#item?: UmbUserItemModel | undefined;
+
 	@property({ type: Object })
-	item?: UmbUserItemModel;
+	public get item(): UmbUserItemModel | undefined {
+		return this.#item;
+	}
+	public set item(value: UmbUserItemModel | undefined) {
+		const oldValue = this.#item;
+		this.#item = value;
+
+		if (!this.#item) {
+			this.#modalRoute?.destroy();
+			return;
+		}
+
+		if (oldValue?.unique !== this.#item.unique) {
+			this.#modalRoute = new UmbModalRouteRegistrationController(this, UMB_WORKSPACE_MODAL)
+				.addAdditionalPath(UMB_USER_ENTITY_TYPE + '/' + this.#item.unique)
+				.onSetup(() => {
+					return { data: { entityType: UMB_USER_ENTITY_TYPE, preset: {} } };
+				})
+				.observeRouteBuilder((routeBuilder) => {
+					this._editPath = routeBuilder({});
+				});
+		}
+	}
 
 	@property({ type: Boolean })
 	readonly = false;
@@ -24,6 +48,8 @@ export class UmbUserItemRefElement extends UmbLitElement {
 
 	@state()
 	_userHasSectionAccess = false;
+
+	#modalRoute?: any;
 
 	constructor() {
 		super();
@@ -38,15 +64,6 @@ export class UmbUserItemRefElement extends UmbLitElement {
 				},
 			},
 		]);
-
-		new UmbModalRouteRegistrationController(this, UMB_WORKSPACE_MODAL)
-			.addAdditionalPath(UMB_USER_ENTITY_TYPE)
-			.onSetup(() => {
-				return { data: { entityType: UMB_USER_ENTITY_TYPE, preset: {} } };
-			})
-			.observeRouteBuilder((routeBuilder) => {
-				this._editPath = routeBuilder({});
-			});
 	}
 
 	#getHref(item: UmbUserItemModel) {
