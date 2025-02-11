2b0ae998e126e4209139e382a386eaded9549a88
Mads Rasmussen <madsr@hey.com>
2025-02-11 19:13:39 +0100

    add observables

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/item/document-item-data-resolver.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/item/document-item-data-resolver.ts
@@ -3,7 +3,9 @@ import type { UmbDocumentItemModel } from './types.js';
 import { UmbControllerBase } from '@umbraco-cms/backoffice/class-api';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import type { DocumentVariantStateModel } from '@umbraco-cms/backoffice/external/backend-api';
+import type { UmbAppLanguageContext } from '@umbraco-cms/backoffice/language';
 import { UMB_APP_LANGUAGE_CONTEXT } from '@umbraco-cms/backoffice/language';
+import { UmbBooleanState, UmbStringState } from '@umbraco-cms/backoffice/observable-api';
 import { UMB_PROPERTY_DATASET_CONTEXT } from '@umbraco-cms/backoffice/property';
 import type { UmbVariantId } from '@umbraco-cms/backoffice/variant';
 
@@ -19,19 +21,48 @@ export class UmbDocumentItemDataResolver extends UmbControllerBase {
 	#propertyDataSetCulture?: UmbVariantId;
 	#item?: UmbDocumentItemModel | undefined;
 
+	#init: Promise<[UmbAppLanguageContext]>;
+
+	#unique = new UmbStringState(undefined);
+	public readonly unique = this.#unique.asObservable();
+
+	#name = new UmbStringState(undefined);
+	public readonly name = this.#name.asObservable();
+
+	#icon = new UmbStringState(undefined);
+	public readonly icon = this.#icon.asObservable();
+
+	#isTrashed = new UmbBooleanState(undefined);
+	public readonly isTrashed = this.#isTrashed.asObservable();
+
+	#isDraft = new UmbBooleanState(undefined);
+	public readonly isDraft = this.#isDraft.asObservable();
+
 	constructor(host: UmbControllerHost) {
 		super(host);
 
-		this.consumeContext(UMB_APP_LANGUAGE_CONTEXT, (context) => {
-			this.observe(context.appLanguageCulture, (culture) => (this.#appCulture = culture));
-			this.observe(context.appDefaultLanguage, (value) => {
-				this.#defaultCulture = value?.unique;
-			});
-		});
-
+		// We do not depend on this context because we know is it only available in some cases
 		this.consumeContext(UMB_PROPERTY_DATASET_CONTEXT, (context) => {
 			this.#propertyDataSetCulture = context.getVariantId();
+			this.#setName();
+			this.#setIsDraft();
 		});
+
+		this.#init = Promise.all([
+			this.consumeContext(UMB_APP_LANGUAGE_CONTEXT, (context) => {
+				this.observe(context.appLanguageCulture, (culture) => {
+					this.#appCulture = culture;
+					this.#setName();
+					this.#setIsDraft();
+				});
+
+				this.observe(context.appDefaultLanguage, (value) => {
+					this.#defaultCulture = value?.unique;
+					this.#setName();
+					this.#setIsDraft();
+				});
+			}).asPromise(),
+		]);
 	}
 
 	/**
@@ -50,63 +81,92 @@ export class UmbDocumentItemDataResolver extends UmbControllerBase {
 	 */
 	setItem(item: UmbDocumentItemModel | undefined) {
 		this.#item = item;
+
+		if (!this.#item) {
+			this.#unique.setValue(undefined);
+			this.#name.setValue(undefined);
+			this.#icon.setValue(undefined);
+			this.#isTrashed.setValue(undefined);
+			this.#isDraft.setValue(undefined);
+			return;
+		}
+
+		this.#unique.setValue(this.#item.unique);
+		this.#icon.setValue(this.#item.documentType.icon);
+		this.#isTrashed.setValue(this.#item.isTrashed);
 	}
 
 	/**
 	 * Get the unique of the item
-	 * @returns {string | undefined} The unique of the item
+	 * @returns {Promise<string | undefined>} The unique of the item
 	 * @memberof UmbDocumentItemDataResolver
 	 */
-	getUnique(): string | undefined {
-		return this.#item?.unique;
+	async getUnique(): Promise<string | undefined> {
+		await this.#init;
+		return this.#unique.getValue();
 	}
 
 	/**
 	 * Get the name of the item
-	 * @returns {string} The name of the item
+	 * @returns {Promise<string>} The name of the item
 	 * @memberof UmbDocumentItemDataResolver
 	 */
-	getName(): string {
-		const variant = this.#getCurrentVariant();
-		const fallbackName = this.#findVariant(this.#defaultCulture)?.name;
-
-		return variant?.name ?? `(${fallbackName})`;
+	async getName(): Promise<string> {
+		await this.#init;
+		return this.#name.getValue() || '';
 	}
 
 	/**
 	 * Get the icon of the item
-	 * @returns {string | undefined} The icon of the item
+	 * @returns {Promise<string | undefined>} The icon of the item
 	 * @memberof UmbDocumentItemDataResolver
 	 */
-	getIcon(): string | undefined {
+	async getIcon(): Promise<string | undefined> {
+		await this.#init;
 		return this.#item?.documentType.icon;
 	}
 
 	/**
 	 * Get the state of the item
-	 * @returns {string | undefined} The state of the item
+	 * @returns {Promise<string | undefined>} The state of the item
 	 * @memberof UmbDocumentItemDataResolver
 	 */
-	getState(): DocumentVariantStateModel | null | undefined {
+	async getState(): Promise<DocumentVariantStateModel | null | undefined> {
+		await this.#init;
 		return this.#getCurrentVariant()?.state;
 	}
 
 	/**
 	 * Get the isDraft of the item
-	 * @returns {boolean} The isDraft of the item
+	 * @returns {Promise<boolean>} The isDraft of the item
 	 * @memberof UmbDocumentItemDataResolver
 	 */
-	getIsDraft(): boolean {
-		return this.getState() === UmbDocumentVariantState.DRAFT || false;
+	async getIsDraft(): Promise<boolean> {
+		await this.#init;
+		return this.#isDraft.getValue() ?? false;
 	}
 
 	/**
 	 * Get the isTrashed of the item
-	 * @returns {boolean | undefined} The isTrashed of the item
+	 * @returns {Promise<boolean | undefined>} The isTrashed of the item
 	 * @memberof UmbDocumentItemDataResolver
 	 */
-	getIsTrashed(): boolean {
+	async getIsTrashed(): Promise<boolean> {
+		await this.#init;
 		return this.#item?.isTrashed ?? false;
+	}
+
+	#setName() {
+		const variant = this.#getCurrentVariant();
+		const fallbackName = this.#findVariant(this.#defaultCulture)?.name;
+		const name = variant?.name ?? `(${fallbackName})`;
+		this.#name.setValue(name);
+	}
+
+	#setIsDraft() {
+		const variant = this.#getCurrentVariant();
+		const isDraft = variant?.state === UmbDocumentVariantState.DRAFT || false;
+		this.#isDraft.setValue(isDraft);
 	}
 
 	#findVariant(culture: string | undefined) {
