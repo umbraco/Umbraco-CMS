5379fec2d339f49418a93da35819ea1594cfde6e
Lee Kelleher <leekelleher@users.noreply.github.com>
2025-02-14 15:37:41 +0000

    Data Type create option description (#18338)
    * Added description for Data Type create option
    
    * Updated "create_folderDescription" in English fallback
    
    as it had previously been updated in English US only.
    
    * US to GB typo correction

--- src/Umbraco.Web.UI.Client/src/assets/lang/en.ts
+++ src/Umbraco.Web.UI.Client/src/assets/lang/en.ts
@@ -438,10 +438,10 @@ export default {
 		compositionDescription:
 			"Defines a re-usable set of properties that can be included in the definition of\n      multiple other Document Types. For example, a set of 'Common Page Settings'.\n    ",
 		folder: 'Folder',
-		folderDescription:
-			'Used to organise the Document Types, Compositions and Element Types created in this\n      Document Type tree.\n    ',
+		folderDescription: 'Used to organise items and other folders. Keep items structured and easy to access.',
 		newFolder: 'New folder',
 		newDataType: 'New Data Type',
+		newDataTypeDescription: 'Used to define a configuration for a Property Type on a Content Type.',
 		newJavascriptFile: 'New JavaScript file',
 		newEmptyPartialView: 'New empty partial view',
 		newPartialViewMacro: 'New partial view macro',
