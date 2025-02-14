718edaa32481b73ad2906ba839e8b21c1c55091b
Andreas Zerbst <andr317c@live.dk>
2025-02-14 13:19:04 +0100

    Fixed tests

--- tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Content/ContentWithListViewContent.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Content/ContentWithListViewContent.spec.ts
@@ -102,6 +102,8 @@ test('can publish content with a child in the list', async ({umbracoApi, umbraco
   await umbracoUi.content.goToContentWithName(contentName);
 
   // Act
+  // Currently necessary
+  await umbracoUi.waitForTimeout(500);
   await umbracoUi.content.clickSaveAndPublishButton();
   await umbracoUi.content.doesSuccessNotificationsHaveCount(2);
   await umbracoUi.content.goToContentInListViewWithName(childContentName);
