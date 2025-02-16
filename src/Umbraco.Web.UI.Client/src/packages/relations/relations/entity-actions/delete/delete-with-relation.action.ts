1e084b4d157f65974c6698a9aad4d49c4cca26ab
Mads Rasmussen <madsr@hey.com>
2025-02-16 19:40:54 +0100

    wip delete with relation

---
+++ src/Umbraco.Web.UI.Client/src/packages/relations/relations/entity-actions/delete/delete-with-relation.action.ts
@@ -0,0 +1,48 @@
+import type { MetaEntityActionDeleteWithRelationKind } from './types.js';
+import { UMB_DELETE_WITH_RELATION_CONFIRM_MODAL } from './modal/constants.js';
+import { UMB_MODAL_MANAGER_CONTEXT } from '@umbraco-cms/backoffice/modal';
+import { UMB_ACTION_EVENT_CONTEXT } from '@umbraco-cms/backoffice/action';
+import { UmbEntityActionBase, UmbRequestReloadStructureForEntityEvent } from '@umbraco-cms/backoffice/entity-action';
+
+/**
+ * Entity action for deleting an item with relations.
+ * @class UmbDeleteWithRelationEntityAction
+ * @augments {UmbEntityActionBase<MetaEntityActionDeleteWithRelationKind>}
+ */
+export class UmbDeleteWithRelationEntityAction extends UmbEntityActionBase<MetaEntityActionDeleteWithRelationKind> {
+	/**
+	 * Executes the action.
+	 * @memberof UmbDeleteWithRelationEntityAction
+	 */
+	override async execute() {
+		if (!this.args.unique) throw new Error('Cannot delete an item without a unique identifier.');
+
+		const modalManager = await this.getContext(UMB_MODAL_MANAGER_CONTEXT);
+
+		const modal = modalManager.open(this, UMB_DELETE_WITH_RELATION_CONFIRM_MODAL, {
+			data: {
+				unique: this.args.unique,
+				entityType: this.args.entityType,
+				itemRepositoryAlias: this.args.meta.itemRepositoryAlias,
+				referenceRepositoryAlias: this.args.meta.referenceRepositoryAlias,
+			},
+		});
+
+		await modal.onSubmit();
+
+		this.#notify();
+	}
+
+	async #notify() {
+		const actionEventContext = await this.getContext(UMB_ACTION_EVENT_CONTEXT);
+
+		const event = new UmbRequestReloadStructureForEntityEvent({
+			unique: this.args.unique,
+			entityType: this.args.entityType,
+		});
+
+		actionEventContext.dispatchEvent(event);
+	}
+}
+
+export { UmbDeleteWithRelationEntityAction as api };
---
+++ src/Umbraco.Web.UI.Client/src/packages/relations/relations/entity-actions/delete/modal/delete-with-relation-modal.element.ts
@@ -0,0 +1,184 @@
+import type { UmbEntityReferenceRepository, UmbReferenceItemModel } from '../../../types.js';
+import type {
+	UmbDeleteWithRelationConfirmModalData,
+	UmbDeleteWithRelationConfirmModalValue,
+} from './delete-with-relation-modal.token.js';
+import { html, customElement, css, state, nothing, type PropertyValues } from '@umbraco-cms/backoffice/external/lit';
+import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
+import { UmbModalBaseElement } from '@umbraco-cms/backoffice/modal';
+import { umbFocus } from '@umbraco-cms/backoffice/lit-element';
+import type { UmbItemRepository } from '@umbraco-cms/backoffice/repository';
+import { createExtensionApiByAlias } from '@umbraco-cms/backoffice/extension-registry';
+
+@customElement('umb-delete-with-relation-confirm-modal')
+export class UmbDeleteWithRelationConfirmModalElement extends UmbModalBaseElement<
+	UmbDeleteWithRelationConfirmModalData,
+	UmbDeleteWithRelationConfirmModalValue
+> {
+	@state()
+	_name?: string;
+
+	@state()
+	_referencedByItems: Array<UmbReferenceItemModel> = [];
+
+	@state()
+	_totalReferencedByItems: number = 0;
+
+	@state()
+	_totalDescendantsWithReferences: number = 0;
+
+	@state()
+	_descendantsWithReferences: Array<any> = [];
+
+	#itemRepository?: UmbItemRepository<any>;
+	#referenceRepository?: UmbEntityReferenceRepository;
+
+	#limitItems = 3;
+
+	protected override firstUpdated(_changedProperties: PropertyValues): void {
+		super.firstUpdated(_changedProperties);
+		this.#initData();
+	}
+
+	async #initData() {
+		if (!this.data) {
+			this.#itemRepository?.destroy();
+			this.#referenceRepository?.destroy();
+			return;
+		}
+
+		this.#itemRepository = await createExtensionApiByAlias<UmbItemRepository<any>>(this, this.data.itemRepositoryAlias);
+
+		const { data } = await this.#itemRepository.requestItems([this.data.unique]);
+		const item = data?.[0];
+		if (!item) throw new Error('Item not found.');
+
+		this._name = item.name;
+
+		if (!this.data?.referenceRepositoryAlias) {
+			throw new Error('Missing referenceRepositoryAlias in data.');
+		}
+
+		this.#referenceRepository = await createExtensionApiByAlias<UmbEntityReferenceRepository>(
+			this,
+			this.data?.referenceRepositoryAlias,
+		);
+
+		this.#loadReferencedBy();
+		this.#loadDescendantsWithReferences();
+	}
+
+	async #loadReferencedBy() {
+		if (!this.#referenceRepository) {
+			throw new Error('Failed to create reference repository.');
+		}
+
+		if (!this.data?.unique) {
+			throw new Error('Missing unique in data.');
+		}
+
+		const { data } = await this.#referenceRepository.requestReferencedBy(this.data.unique, 0, this.#limitItems);
+
+		if (data) {
+			this._referencedByItems = [...data.items];
+			this._totalReferencedByItems = data.total;
+		}
+	}
+
+	async #loadDescendantsWithReferences() {
+		if (!this.#referenceRepository) {
+			throw new Error('Failed to create reference repository.');
+		}
+
+		if (!this.#itemRepository) {
+			throw new Error('Failed to create item repository.');
+		}
+
+		// If the repository does not have the method, we don't need to load the referenced descendants.
+		if (!this.#referenceRepository.requestDescendantsWithReferences) return;
+
+		if (!this.data?.unique) {
+			throw new Error('Missing unique in data.');
+		}
+
+		const { data } = await this.#referenceRepository.requestDescendantsWithReferences(
+			this.data.unique,
+			0,
+			this.#limitItems,
+		);
+
+		if (data) {
+			this._totalDescendantsWithReferences = data.total;
+			const uniques = data.items.map((item) => item.unique).filter((unique) => unique) as Array<string>;
+			const { data: items } = await this.#itemRepository.requestItems(uniques);
+			this._descendantsWithReferences = items ?? [];
+		}
+	}
+
+	override render() {
+		return html`
+			<uui-dialog-layout class="uui-text" headline="Trash">
+				<p>Are you sure you want to move <strong>${this._name}</strong> to the recycle bin?</p>
+				${this.#renderItems('references_labelDependsOnThis', this._referencedByItems, this._totalReferencedByItems)}
+				${this.#renderItems(
+					'references_labelDependentDescendants',
+					this._descendantsWithReferences,
+					this._totalDescendantsWithReferences,
+				)}
+
+				<uui-button slot="actions" id="cancel" label="Cancel" @click=${this._rejectModal}></uui-button>
+
+				<uui-button
+					slot="actions"
+					id="confirm"
+					color="danger"
+					look="primary"
+					label="Trash"
+					@click=${this._submitModal}
+					${umbFocus()}></uui-button>
+			</uui-dialog-layout>
+		`;
+	}
+
+	#renderItems(headline: string, items: Array<UmbReferenceItemModel>, total: number) {
+		if (total === 0) return nothing;
+
+		return html`
+			<h5 id="reference-headline">${this.localize.term(headline)}</h5>
+			<uui-ref-list>
+				${items.map(
+					(item) =>
+						html`<umb-entity-item-ref .item=${item} readonly ?standalone=${total === 1}></umb-entity-item-ref> `,
+				)}
+			</uui-ref-list>
+			${total > this.#limitItems
+				? html`<span>${this.localize.term('references_labelMoreReferences', total - this.#limitItems)}</span>`
+				: nothing}
+		`;
+	}
+
+	static override styles = [
+		UmbTextStyles,
+		css`
+			uui-dialog-layout {
+				max-inline-size: 60ch;
+			}
+
+			#reference-headline {
+				margin-bottom: var(--uui-size-3);
+			}
+
+			uui-ref-list {
+				margin-bottom: var(--uui-size-2);
+			}
+		`,
+	];
+}
+
+export { UmbDeleteWithRelationConfirmModalElement as element };
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'umb-delete-with-relation-confirm-modal': UmbDeleteWithRelationConfirmModalElement;
+	}
+}
