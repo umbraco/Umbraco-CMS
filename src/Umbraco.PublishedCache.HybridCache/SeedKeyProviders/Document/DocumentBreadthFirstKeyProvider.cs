6215708b415ee25568f72219f44e5b4b6a0f5a54
mole <nikolajlauridsen@protonmail.ch>
2025-02-17 10:48:12 +0100

    Add comment on how to refactor breadth first key provider

--- src/Umbraco.PublishedCache.HybridCache/SeedKeyProviders/Document/DocumentBreadthFirstKeyProvider.cs
+++ src/Umbraco.PublishedCache.HybridCache/SeedKeyProviders/Document/DocumentBreadthFirstKeyProvider.cs
@@ -19,6 +19,12 @@ internal sealed class DocumentBreadthFirstKeyProvider : BreadthFirstKeyProvider,
         _seedCount = cacheSettings.Value.DocumentBreadthFirstSeedCount;
     }
 
+
+    // TODO: V16 - Move this method back to the base class
+    // The main need for this is because we now need to filter the keys, based on if they have published ancestor path or not
+    // We should add `FilterKeys` virtual method on the base class that does nothing, and then override it here instead
+    // Note that it's important that we do this filtering as we're doing the search, since we want to make sure we hit the seed count
+    // For instance if you have 500 content nodes, request 100 seeded, we need to return 100 keys, even if we need to filter out 20 of them
     public new ISet<Guid> GetSeedKeys()
     {
         if (_seedCount == 0)
