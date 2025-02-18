ac837751d861bcfbf2a6d5aaf69e2363580cfaf5
Mads Rasmussen <madsr@hey.com>
2025-02-18 12:36:13 +0100

    add methods to get bulk references for documents

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/reference/repository/document-reference.repository.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/reference/repository/document-reference.repository.ts
@@ -16,6 +16,11 @@ export class UmbDocumentReferenceRepository extends UmbControllerBase implements
 		return this.#referenceSource.getReferencedBy(unique, skip, take);
 	}
 
+	async requestAreReferenced(uniques: Array<string>, skip = 0, take = 20) {
+		if (!uniques || uniques.length === 0) throw new Error(`uniques is required`);
+		return this.#referenceSource.getAreReferenced(uniques, skip, take);
+	}
+
 	async requestDescendantsWithReferences(unique: string, skip = 0, take = 20) {
 		if (!unique) throw new Error(`unique is required`);
 		return this.#referenceSource.getReferencedDescendants(unique, skip, take);
--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/reference/repository/document-reference.server.data.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/reference/repository/document-reference.server.data.ts
@@ -56,6 +56,38 @@ export class UmbDocumentReferenceServerDataSource extends UmbControllerBase impl
 	}
 
 	/**
+	 * Fetches items that are referenced by the given uniques from the server
+	 * @param {Array<string>} uniques - The unique identifiers of the items to fetch
+	 * @param {number} skip - The number of items to skip
+	 * @param {number} take - The number of items to take
+	 * @returns {Promise<UmbDataSourceResponse<UmbPagedModel<UmbEntityModel>>>} - Items that are referenced by other items
+	 * @memberof UmbDocumentReferenceServerDataSource
+	 */
+	async getAreReferenced(
+		uniques: Array<string>,
+		skip: number = 0,
+		take: number = 20,
+	): Promise<UmbDataSourceResponse<UmbPagedModel<UmbEntityModel>>> {
+		const { data, error } = await tryExecuteAndNotify(
+			this,
+			DocumentService.getDocumentAreReferenced({ id: uniques, skip, take }),
+		);
+
+		if (data) {
+			const items: Array<UmbEntityModel> = data.items.map((item) => {
+				return {
+					unique: item.id,
+					entityType: UMB_DOCUMENT_ENTITY_TYPE,
+				};
+			});
+
+			return { data: { items, total: data.total } };
+		}
+
+		return { data, error };
+	}
+
+	/**
 	 * Returns any descendants of the given unique that is referenced by other items
 	 * @param {string} unique - The unique identifier of the item to fetch descendants for
 	 * @param {number} skip - The number of items to skip
