0a871522828ff0b70da93997e3525487b3577ce6
Mads Rasmussen <madsr@hey.com>
2025-02-19 17:40:15 +0100

    implement methods for are referenced

--- src/Umbraco.Web.UI.Client/src/packages/media/media/reference/repository/media-reference.server.data.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/reference/repository/media-reference.server.data.ts
@@ -9,7 +9,7 @@ import type { UmbPagedModel, UmbDataSourceResponse } from '@umbraco-cms/backoffi
 
 /**
  * @class UmbMediaReferenceServerDataSource
- * @implements {RepositoryDetailDataSource}
+ * @implements {UmbEntityReferenceDataSource}
  */
 export class UmbMediaReferenceServerDataSource extends UmbControllerBase implements UmbEntityReferenceDataSource {
 	#dataMapper = new UmbManagementApiDataMapper(this);
@@ -48,6 +48,38 @@ export class UmbMediaReferenceServerDataSource extends UmbControllerBase impleme
 			});
 
 			const items = await Promise.all(promises);
+
+			return { data: { items, total: data.total } };
+		}
+
+		return { data, error };
+	}
+
+	/**
+	 * Checks if the items are referenced by other items
+	 * @param {Array<string>} uniques - The unique identifiers of the items to fetch
+	 * @param {number} skip - The number of items to skip
+	 * @param {number} take - The number of items to take
+	 * @returns {Promise<UmbDataSourceResponse<UmbPagedModel<UmbEntityModel>>>} - Items that are referenced by other items
+	 * @memberof UmbMediaReferenceServerDataSource
+	 */
+	async getAreReferenced(
+		uniques: Array<string>,
+		skip: number = 0,
+		take: number = 20,
+	): Promise<UmbDataSourceResponse<UmbPagedModel<UmbEntityModel>>> {
+		const { data, error } = await tryExecuteAndNotify(
+			this,
+			MediaService.getMediaAreReferenced({ id: uniques, skip, take }),
+		);
+
+		if (data) {
+			const items: Array<UmbEntityModel> = data.items.map((item) => {
+				return {
+					unique: item.id,
+					entityType: UMB_MEDIA_ENTITY_TYPE,
+				};
+			});
 
 			return { data: { items, total: data.total } };
 		}
