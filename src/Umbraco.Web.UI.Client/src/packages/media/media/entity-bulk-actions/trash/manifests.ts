d7c0081cf4c438331019675ff6a9d3d0a1eb218f
Mads Rasmussen <madsr@hey.com>
2025-02-19 17:40:38 +0100

    change to use bulk trash with relation

--- src/Umbraco.Web.UI.Client/src/packages/media/media/entity-bulk-actions/trash/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/entity-bulk-actions/trash/manifests.ts
@@ -1,18 +1,22 @@
-import { UMB_MEDIA_COLLECTION_ALIAS } from '../../constants.js';
+import {
+	UMB_MEDIA_COLLECTION_ALIAS,
+	UMB_MEDIA_ITEM_REPOSITORY_ALIAS,
+	UMB_MEDIA_REFERENCE_REPOSITORY_ALIAS,
+} from '../../constants.js';
 import { UMB_MEDIA_ENTITY_TYPE } from '../../entity.js';
-import { UMB_BULK_TRASH_MEDIA_REPOSITORY_ALIAS } from './constants.js';
 import { manifests as repositoryManifests } from './repository/manifests.js';
 import { UMB_COLLECTION_ALIAS_CONDITION } from '@umbraco-cms/backoffice/collection';
 
 const bulkTrashAction: UmbExtensionManifest = {
 	type: 'entityBulkAction',
-	kind: 'trash',
+	kind: 'trashWithRelation',
 	alias: 'Umb.EntityBulkAction.Media.Trash',
 	name: 'Trash Media Entity Bulk Action',
 	weight: 10,
 	forEntityTypes: [UMB_MEDIA_ENTITY_TYPE],
 	meta: {
-		bulkTrashRepositoryAlias: UMB_BULK_TRASH_MEDIA_REPOSITORY_ALIAS,
+		itemRepositoryAlias: UMB_MEDIA_ITEM_REPOSITORY_ALIAS,
+		referenceRepositoryAlias: UMB_MEDIA_REFERENCE_REPOSITORY_ALIAS,
 	},
 	conditions: [
 		{
