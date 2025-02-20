19529c1e02f291a81d7383d92408d25f7f0272bc
Niels Lyngsø <niels.lyngso@gmail.com>
2025-02-20 21:29:29 +0100

    builder

---
+++ src/Umbraco.Web.UI.Client/src/packages/core/property/property-value-preset/property-value-preset-builder.controller.ts
@@ -0,0 +1,79 @@
+import type { UmbPropertyValueData, UmbPropertyValueDataPotentiallyWithEditorAlias } from '../index.js';
+import { UmbControllerBase } from '@umbraco-cms/backoffice/class-api';
+import { createExtensionApi } from '@umbraco-cms/backoffice/extension-api';
+import { umbExtensionsRegistry } from '@umbraco-cms/backoffice/extension-registry';
+import type {
+	UmbPropertyTypePresetModel,
+	UmbPropertyTypePresetWithSchemaAliasModel,
+	UmbPropertyValuePresetApi,
+} from './types.js';
+
+//type PropertyTypesType = UmbPropertyTypePresetModel | UmbPropertyTypePresetWithSchemaAliasModel;
+
+export class UmbPropertyValuePresetBuilderController extends UmbControllerBase {
+	/**
+	 * Clones the property data.
+	 * @param {UmbPropertyValueDataPotentiallyWithEditorAlias} property - The property data.
+	 * @returns {Promise<UmbPropertyValueDataPotentiallyWithEditorAlias>} - A promise that resolves to the cloned property data.
+	 */
+	async create<
+		GivenPropertyTypesType extends UmbPropertyTypePresetModel | UmbPropertyTypePresetWithSchemaAliasModel,
+		ReturnType = GivenPropertyTypesType extends UmbPropertyTypePresetWithSchemaAliasModel
+			? UmbPropertyValueDataPotentiallyWithEditorAlias
+			: UmbPropertyValueData,
+	>(propertyTypes: Array<GivenPropertyTypesType>): Promise<Array<ReturnType>> {
+		const result = await Promise.all(propertyTypes.map(this.#createPropertyPreset<ReturnType>));
+
+		this.destroy();
+
+		return result;
+	}
+
+	#createPropertyPreset = async <ReturnType>(
+		propertyType: UmbPropertyTypePresetModel | UmbPropertyTypePresetWithSchemaAliasModel,
+	): Promise<ReturnType> => {
+		const editorAlias: string | undefined = (propertyType as UmbPropertyTypePresetWithSchemaAliasModel)
+			.propertyEditorSchemaAlias;
+
+		const editorUiAlias = propertyType.propertyEditorUiAlias;
+
+		const alias = propertyType.alias;
+		if (!alias) {
+			throw new Error(`alias not defined in ${propertyType}`);
+		}
+
+		// Find a preset for this editor alias:
+		const manifests = umbExtensionsRegistry.getByTypeAndFilter(
+			'propertyValuePreset',
+			(x) => x.forPropertyEditorSchemaAlias === editorAlias || x.forPropertyEditorUiAlias === editorUiAlias,
+		);
+
+		const apis = (await Promise.all(manifests.map((x) => createExtensionApi(this, x)))).filter(
+			(x) => x !== undefined,
+		) as Array<UmbPropertyValuePresetApi>;
+
+		let value: unknown = undefined;
+		// Important to use a inline for loop, to secure that each entry is processed(asynchronously) in order
+		for (const api of apis) {
+			if (!api.processValue) {
+				throw new Error(
+					`processValue method is not defined in one of the apis of these extensions: ${manifests.map((x) => x.alias).join(', ')}`,
+				);
+			}
+			value = await api.processValue(value, propertyType.config);
+		}
+
+		if (editorAlias) {
+			return {
+				editorAlias,
+				alias,
+				value,
+			} satisfies UmbPropertyValueDataPotentiallyWithEditorAlias as ReturnType;
+		} else {
+			return {
+				alias,
+				value,
+			} satisfies UmbPropertyValueData as ReturnType;
+		}
+	};
+}
