9753c74e891bc7b45ab4e7970f4681174ca02281
Niels Lyngsø <niels.lyngso@gmail.com>
2025-02-24 19:32:08 +0100

    leaving some concept code for another time

--- src/Umbraco.Web.UI.Client/src/libs/observable-api/utils/json-string-comparison.function.ts
+++ src/Umbraco.Web.UI.Client/src/libs/observable-api/utils/json-string-comparison.function.ts
@@ -1,12 +1,12 @@
 /**
  * @function jsonStringComparison
- * @param {unknown} a - The first object to compare.
- * @param {unknown} b - The second object to compare.
+ * @param {object} a - The first object to compare.
+ * @param {object} b - The second object to compare.
  * @returns {boolean} - Returns true if the JSON strings are identical.
  * @description - Compares two objects by converting them to JSON strings.
  * This is a JSON comparison and should only be used for simple objects.
  * Meaning no class instances can take part in this data.
  */
-export function jsonStringComparison(a: unknown, b: unknown): boolean {
+export function jsonStringComparison(a: object, b: object): boolean {
 	return JSON.stringify(a) === JSON.stringify(b);
 }
