3749afde2cc76e6ceb7c9b9c68276ee0e59e918c
Mads Rasmussen <madsr@hey.com>
2025-02-25 12:20:50 +0100

    rename class

--- src/Umbraco.Web.UI.Client/src/packages/property-editors/toggle/property-value-preset.TrueFalse.ts
+++ src/Umbraco.Web.UI.Client/src/packages/property-editors/toggle/property-value-preset.TrueFalse.ts
@@ -2,7 +2,7 @@ import type { UmbPropertyEditorUIToggleValue } from './types.js';
 import type { UmbPropertyValuePreset } from '@umbraco-cms/backoffice/property';
 import type { UmbPropertyEditorConfig } from '@umbraco-cms/backoffice/property-editor';
 
-export class UmbPropertyValuePresetTrueFalseApi
+export class UmbTrueFalsePropertyValuePreset
 	implements UmbPropertyValuePreset<UmbPropertyEditorUIToggleValue, UmbPropertyEditorConfig>
 {
 	async processValue(value: undefined | UmbPropertyEditorUIToggleValue, config: UmbPropertyEditorConfig) {
@@ -13,4 +13,4 @@ export class UmbPropertyValuePresetTrueFalseApi
 	destroy(): void {}
 }
 
-export { UmbPropertyValuePresetTrueFalseApi as api };
+export { UmbTrueFalsePropertyValuePreset as api };
