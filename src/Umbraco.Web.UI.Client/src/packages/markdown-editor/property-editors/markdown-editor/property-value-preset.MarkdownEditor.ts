a4db51e2ed35a41981601f32fad8e1830f8d985f
Mads Rasmussen <madsr@hey.com>
2025-02-25 12:27:49 +0100

    rename

--- src/Umbraco.Web.UI.Client/src/packages/markdown-editor/property-editors/markdown-editor/property-value-preset.MarkdownEditor.ts
+++ src/Umbraco.Web.UI.Client/src/packages/markdown-editor/property-editors/markdown-editor/property-value-preset.MarkdownEditor.ts
@@ -2,7 +2,7 @@ import type { UmbPropertyValuePreset } from '@umbraco-cms/backoffice/property';
 import type { UmbPropertyEditorConfig } from '@umbraco-cms/backoffice/property-editor';
 import type { UmbPropertyEditorUIMarkdownValue } from './types.js';
 
-export class UmbPropertyValuePresetMarkdownApi implements UmbPropertyValuePreset<UmbPropertyEditorUIMarkdownValue> {
+export class UmbMarkdownPropertyValuePreset implements UmbPropertyValuePreset<UmbPropertyEditorUIMarkdownValue> {
 	async processValue(value: undefined | UmbPropertyEditorUIMarkdownValue, config: UmbPropertyEditorConfig) {
 		const defaultValue = config.find((x) => x.alias === 'defaultValue')?.value as string | undefined;
 		return value !== undefined ? value : defaultValue;
@@ -11,4 +11,4 @@ export class UmbPropertyValuePresetMarkdownApi implements UmbPropertyValuePreset
 	destroy(): void {}
 }
 
-export { UmbPropertyValuePresetMarkdownApi as api };
+export { UmbMarkdownPropertyValuePreset as api };
