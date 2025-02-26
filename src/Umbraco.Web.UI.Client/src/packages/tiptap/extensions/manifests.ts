bdf1f58662028abd6d19bfedd0521804459cfcd7
leekelleher <leekelleher@gmail.com>
2025-02-26 22:45:59 +0000

    Implements Font Size as toolbar menu kind

--- src/Umbraco.Web.UI.Client/src/packages/tiptap/extensions/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/tiptap/extensions/manifests.ts
@@ -528,13 +528,25 @@ const toolbarExtensions: Array<ManifestTiptapToolbarExtension> = [
 	},
 	{
 		type: 'tiptapToolbarExtension',
+		kind: 'menu',
 		alias: 'Umb.Tiptap.Toolbar.FontSize',
 		name: 'Font Size Tiptap Extension',
-		element: () => import('./toolbar/font-size-tiptap-toolbar.element.js'),
+		api: () => import('./toolbar/font-size.tiptap-toolbar-api.js'),
 		meta: {
 			alias: 'umbFontSize',
 			icon: 'icon-ruler',
 			label: 'Font size',
+			items: [
+				{ label: '8pt', data: '8pt;' },
+				{ label: '10pt', data: '10pt;' },
+				{ label: '12pt', data: '12pt;' },
+				{ label: '14pt', data: '14pt;' },
+				{ label: '16pt', data: '16pt;' },
+				{ label: '18pt', data: '18pt;' },
+				{ label: '24pt', data: '24pt;' },
+				{ label: '26pt', data: '26pt;' },
+				{ label: '48pt', data: '48pt;' },
+			],
 		},
 	},
 ];
