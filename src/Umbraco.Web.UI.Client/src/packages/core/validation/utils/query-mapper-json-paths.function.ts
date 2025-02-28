f9dfab0946de4e481051d45a6212c475a07d875b
Niels Lyngsø <niels.lyngso@gmail.com>
2025-02-28 22:36:48 +0100

    umbQueryMapperForJsonPaths

---
+++ src/Umbraco.Web.UI.Client/src/packages/core/validation/utils/query-mapper-json-paths.function.ts
@@ -0,0 +1,58 @@
+import { GetValueByJsonPath } from './json-path.function.js';
+import { umbScopeMapperForJsonPaths } from './scope-mapper-json-paths.function.js';
+
+/**
+ * Helper method to map paths matching a certain scope.
+ * @param {Array<string>} paths - the JSON paths to map.
+ * @param {string} scopePath - the JSON path to scope the mapping to.
+ * @param {(Array<string>)} scopedMapper - Map function which receives the paths in the scope and returns the resolved paths.
+ * @returns {string} - the paths, including the once mapped by the scoped mapper. Notice the order is kept.
+ */
+export async function umbQueryMapperForJsonPaths<T>(
+	scopePaths: Array<string>,
+	scopeData: Array<T>,
+	queryConstructor: (entry: T) => string,
+	mapper?: (scopedPaths: Array<string>, propertyData: T) => Promise<Array<string>>,
+): Promise<Array<string>> {
+	const uniquePointers: Array<string> = [];
+
+	// Make sure each entry gets a query:
+	// propertyPaths is like ['$[0].value', '$[0].value.something', '$[1].value', '$[?.(@.prop == 'value')].value'] group them by the first index or query.:
+	let pathsWithQueries = scopePaths.map(
+		(path) => {
+			// This could already be translated, meaning it can both be an index or a JSON-Path query.
+			const pathSplit = path.split(']', 2);
+			// grab left side of ] and get rid of `$[`
+			let pointer = pathSplit[0].substring(2);
+			const numberPointer = Number(pointer);
+
+			// If a number index, then create JSON-Path Query:
+			if (!isNaN(numberPointer)) {
+				pointer = queryConstructor(scopeData[numberPointer]);
+			}
+
+			if (!uniquePointers.includes(pointer)) {
+				uniquePointers.push(pointer);
+			}
+
+			return `$[${pointer}]${pathSplit[1]}`;
+		},
+		{} as Record<string, Array<string>>,
+	);
+
+	if (mapper) {
+		// Translate each property:
+		for (const uniquePath of uniquePointers) {
+			const propertyData = GetValueByJsonPath(scopeData, `$[${uniquePath}]`) as T;
+
+			pathsWithQueries = await umbScopeMapperForJsonPaths(
+				pathsWithQueries,
+				`$[${uniquePath}]`,
+				async (scopedPaths: Array<string>) => {
+					return mapper(scopedPaths, propertyData);
+				},
+			);
+		}
+	}
+	return pathsWithQueries;
+}
---
+++ src/Umbraco.Web.UI.Client/src/packages/core/validation/utils/query-mapper-json-paths.test.ts
@@ -0,0 +1,62 @@
+import { expect } from '@open-wc/testing';
+import { umbQueryMapperForJsonPaths } from './query-mapper-json-paths.function.js';
+import { assert } from 'chai';
+
+describe('umbQueryMapperForJsonPaths', () => {
+	it('provides queries for index based pointers', async () => {
+		const data = [
+			{ key: '0', value: 'one' },
+			{ key: '1', value: 'two' },
+			{ key: '2', value: 'three' },
+		];
+		const paths = [`$[?(@.key == '0')].value`, `$[1].value`];
+
+		const result = await umbQueryMapperForJsonPaths(paths, data, (entry: (typeof data)[0]) => {
+			return `?(@.key == '${entry.key}')`;
+		});
+
+		expect(result.length).to.eq(2);
+		expect(result[0]).to.eq(`$[?(@.key == '0')].value`);
+		expect(result[1]).to.eq(`$[?(@.key == '1')].value`);
+	});
+
+	it('runs the mapper for both index and query based pointers', async () => {
+		const data = [
+			{ key: '0', value: 'one' },
+			{ key: '1', value: 'two' },
+			{ key: '2', value: 'three' },
+		];
+		const paths = [`$[?(@.key == '0')].value.hey`, `$[0].value`, `$[1].value`];
+
+		let testCounter = 0;
+
+		const result = await umbQueryMapperForJsonPaths(
+			paths,
+			data,
+			(entry: (typeof data)[0]) => {
+				return `?(@.key == '${entry.key}')`;
+			},
+			async (paths: Array<string>, data) => {
+				testCounter++;
+				if (testCounter === 1) {
+					expect(paths.length).to.eq(2);
+					expect(paths[0]).to.eq(`$.value.hey`);
+					expect(paths[1]).to.eq(`$.value`);
+					expect(data.value).to.eq(`one`);
+				} else if (testCounter === 2) {
+					expect(paths.length).to.eq(1);
+					expect(paths[0]).to.eq(`$.value`);
+					expect(data.value).to.eq(`two`);
+				} else {
+					assert.fail('The mapper should only run twice');
+				}
+				return paths;
+			},
+		);
+
+		expect(result.length).to.eq(3);
+		expect(result[0]).to.eq(`$[?(@.key == '0')].value.hey`);
+		expect(result[1]).to.eq(`$[?(@.key == '0')].value`);
+		expect(result[2]).to.eq(`$[?(@.key == '1')].value`);
+	});
+});
