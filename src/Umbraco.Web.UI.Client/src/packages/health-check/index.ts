04b9319f0870052d80089dd821018b1defebd242
Mads Rasmussen <madsr@hey.com>
2025-02-28 14:36:41 +0100

    fix linting

--- src/Umbraco.Web.UI.Client/src/packages/health-check/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/health-check/index.ts
@@ -1,4 +1,4 @@
 export type * from './health-check.extension.js';
 export { UMB_HEALTHCHECK_DASHBOARD_CONTEXT } from './health-check-dashboard.context.js';
 
-export * from './constants.js';
+export type * from './constants.js';
