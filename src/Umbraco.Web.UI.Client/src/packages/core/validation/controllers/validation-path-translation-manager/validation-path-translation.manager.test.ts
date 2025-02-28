28b805c8d83fc5feca7fb7f0ebf7e29dcea5cc33
Niels Lyngsø <niels.lyngso@gmail.com>
2025-02-28 23:01:11 +0100

    Validation Path Translation manager

--- src/Umbraco.Web.UI.Client/src/packages/core/validation/controllers/validation-path-translation-manager/validation-path-translation.manager.test.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/validation/controllers/validation-path-translation-manager/validation-path-translation.manager.test.ts
@@ -1,264 +1,127 @@
 import { expect } from '@open-wc/testing';
 import { umbExtensionsRegistry } from '@umbraco-cms/backoffice/extension-registry';
-import type {
-	ManifestPropertyValueResolver,
-	ManifestPropertyValueCloner,
-	UmbPropertyValueData,
-	UmbPropertyValueResolver,
-	UmbPropertyValueCloner,
-} from '../types.js';
-import type { UmbVariantDataModel } from '@umbraco-cms/backoffice/variant';
+import type { UmbValidationPathTranslator } from '../types.js';
 import { customElement } from '@umbraco-cms/backoffice/external/lit';
 import { UmbControllerHostElementMixin } from '@umbraco-cms/backoffice/controller-api';
-import { UmbPropertyValueCloneController } from './property-value-clone.controller.js';
+import { UmbValidationTranslatorController } from './validation-path-translation.manager.js';
+import type { UmbValidationMessage } from '../../context/validation-messages.manager.js';
+import { UmbControllerBase } from '@umbraco-cms/backoffice/class-api';
+import { umbScopeMapperForJsonPaths } from '../../utils/scope-mapper-json-paths.function.js';
+import { umbQueryMapperForJsonPaths } from '../../utils/query-mapper-json-paths.function.js';
 
 @customElement('umb-test-controller-host')
 export class UmbTestControllerHostElement extends UmbControllerHostElementMixin(HTMLElement) {}
 
-type TestPropertyValueWithId = {
-	id: string;
-};
-
-type TestPropertyValueNestedType = TestPropertyValueWithId & {
-	nestedValue?: UmbPropertyValueData<TestPropertyValueWithId>;
-};
-
-export class TestPropertyValueResolver
-	implements
-		UmbPropertyValueResolver<
-			UmbPropertyValueData<TestPropertyValueNestedType>,
-			UmbPropertyValueData,
-			UmbVariantDataModel
-		>
-{
-	async processValues(
-		property: UmbPropertyValueData<TestPropertyValueNestedType>,
-		valuesCallback: (values: Array<UmbPropertyValueData>) => Promise<Array<UmbPropertyValueData> | undefined>,
-	): Promise<UmbPropertyValueData<TestPropertyValueNestedType>> {
-		if (property.value) {
-			const nestedValue = property.value.nestedValue;
-			const processedValues = nestedValue ? await valuesCallback([nestedValue]) : undefined;
-			return {
-				...property,
-				value: {
-					...property.value,
-					nestedValue: processedValues ? processedValues[0] : undefined,
-				},
-			} as UmbPropertyValueData<TestPropertyValueNestedType>;
-		}
-		return property;
-	}
-}
-
-export class TestPropertyValueCloner implements UmbPropertyValueCloner<TestPropertyValueWithId> {
-	async cloneValue(value: TestPropertyValueWithId): Promise<TestPropertyValueWithId> {
-		return { ...value, id: 'updated-id' };
-	}
-
-	destroy(): void {}
-}
-
 describe('UmbValidationTranslatorController', () => {
-	describe('Cloner', () => {
-		beforeEach(async () => {
-			const manifestCloner: ManifestPropertyValueCloner = {
-				type: 'propertyValueCloner',
-				name: 'test-cloner-1',
-				alias: 'Umb.Test.Cloner.1',
-				api: TestPropertyValueCloner,
-				forEditorAlias: 'test-editor',
-			};
+	describe('Without translators', () => {
+		let host: UmbTestControllerHostElement;
+		let ctrl!: UmbValidationTranslatorController;
+		let translationData = {
+			propertyStr: 'value',
+			propertyArray: [0, 1, 2, 3],
+			propertyObj: {
+				propStr: 'value',
+			},
+		};
 
-			umbExtensionsRegistry.register(manifestCloner);
+		beforeEach(async () => {
+			host = new UmbTestControllerHostElement();
+			ctrl = new UmbValidationTranslatorController(host, { translationData, pathTranslators: [] });
 		});
 		afterEach(async () => {
-			umbExtensionsRegistry.unregister('Umb.Test.Cloner.1');
+			host.destroy();
 		});
 
-		it('clones value', async () => {
-			const ctrlHost = new UmbTestControllerHostElement();
-			const ctrl = new UmbPropertyValueCloneController(ctrlHost);
-
-			const value = {
-				editorAlias: 'test-editor',
-				alias: 'test',
-				culture: null,
-				segment: null,
-				value: {
-					id: 'not-updated-id',
+		it('returns value', async () => {
+			const msgs: Array<UmbValidationMessage> = [
+				{
+					key: '1',
+					type: 'client',
+					path: '$.propertyStr',
+					body: 'test',
 				},
-			};
+			];
 
-			const result = await ctrl.clone(value);
+			const result = await ctrl.translate(msgs);
 
-			expect((result.value as TestPropertyValueNestedType | undefined)?.id).to.be.equal('updated-id');
+			expect(result[0].key).to.be.equal('1');
+			expect(result[0].path).to.be.equal('$.propertyStr');
 		});
 	});
 
-	describe('Resolvers and Cloner', () => {
+	describe('Using a translator', () => {
+		let host: UmbTestControllerHostElement;
+		let ctrl!: UmbValidationTranslatorController;
+		let translationData = {
+			propArray: [
+				{
+					key: '1',
+					name: 'array1',
+					inner: [
+						{
+							key: '1a',
+							name: 'inner1a',
+						},
+						{
+							key: '1b',
+							name: 'inner1b',
+						},
+					],
+				},
+				{
+					key: '2',
+					name: 'array2',
+					inner: [
+						{
+							key: '2a',
+							name: 'inner2a',
+						},
+						{
+							key: '2b',
+							name: 'inner2b',
+						},
+					],
+				},
+			],
+		};
+
+		class Translator extends UmbControllerBase implements UmbValidationPathTranslator<typeof translationData> {
+			async translate(paths: Array<string>, data: typeof translationData): Promise<Array<string>> {
+				// Method to filter and cut the initial path, returning an array:
+				return umbScopeMapperForJsonPaths(paths, '$.propArray', async (innerPaths) => {
+					return await umbQueryMapperForJsonPaths(
+						innerPaths,
+						data.propArray,
+						(entry: (typeof translationData.propArray)[0]) => {
+							return `?(@.key == '${entry.key}')`;
+						},
+					);
+				});
+			}
+		}
+
 		beforeEach(async () => {
-			const manifestResolver: ManifestPropertyValueResolver = {
-				type: 'propertyValueResolver',
-				name: 'test-resolver-1',
-				alias: 'Umb.Test.Resolver.1',
-				api: TestPropertyValueResolver,
-				forEditorAlias: 'test-editor',
-			};
-
-			umbExtensionsRegistry.register(manifestResolver);
-
-			const manifestResolverOnly: ManifestPropertyValueResolver = {
-				type: 'propertyValueResolver',
-				name: 'test-resolver-1',
-				alias: 'Umb.Test.Resolver.2',
-				api: TestPropertyValueResolver,
-				forEditorAlias: 'only-resolver-editor',
-			};
-
-			umbExtensionsRegistry.register(manifestResolverOnly);
-
-			const manifestCloner: ManifestPropertyValueCloner = {
-				type: 'propertyValueCloner',
-				name: 'test-cloner-1',
-				alias: 'Umb.Test.Cloner.1',
-				api: TestPropertyValueCloner,
-				forEditorAlias: 'test-editor',
-			};
-
-			umbExtensionsRegistry.register(manifestCloner);
+			host = new UmbTestControllerHostElement();
+			ctrl = new UmbValidationTranslatorController(host, { translationData, pathTranslators: [Translator] });
 		});
 		afterEach(async () => {
-			umbExtensionsRegistry.unregister('Umb.Test.Resolver.1');
-			umbExtensionsRegistry.unregister('Umb.Test.Resolver.2');
-			umbExtensionsRegistry.unregister('Umb.Test.Cloner.1');
+			host.destroy();
 		});
 
-		it('clones value and inner values', async () => {
-			const ctrlHost = new UmbTestControllerHostElement();
-			const ctrl = new UmbPropertyValueCloneController(ctrlHost);
-
-			const value = {
-				editorAlias: 'test-editor',
-				alias: 'test',
-				culture: null,
-				segment: null,
-				value: {
-					id: 'not-updated-id',
+		it('returns value', async () => {
+			const msgs: Array<UmbValidationMessage> = [
+				{
+					key: '1',
+					type: 'client',
+					path: '$.propArray[0].innerValue',
+					body: 'test',
 				},
-			};
+			];
 
-			const result = await ctrl.clone(value);
+			const result = await ctrl.translate(msgs);
 
-			expect((result.value as TestPropertyValueNestedType | undefined)?.id).to.be.equal('updated-id');
-		});
-
-		it('clones only inner values', async () => {
-			const ctrlHost = new UmbTestControllerHostElement();
-			const ctrl = new UmbPropertyValueCloneController(ctrlHost);
-
-			const value = {
-				editorAlias: 'only-resolver-editor',
-				alias: 'not-to-be-handled',
-				culture: null,
-				segment: null,
-				value: {
-					id: 'not-updated-id',
-					nestedValue: {
-						editorAlias: 'test-editor',
-						alias: 'some',
-						culture: null,
-						segment: null,
-						value: {
-							id: 'inner-not-updated-id',
-						},
-					},
-				},
-			};
-
-			const result = await ctrl.clone(value);
-
-			expect((result.value as TestPropertyValueNestedType | undefined)?.id).to.be.equal('not-updated-id');
-			expect((result.value as TestPropertyValueNestedType | undefined)?.nestedValue?.value?.id).to.be.equal(
-				'updated-id',
-			);
-		});
-
-		it('clones value and inner values', async () => {
-			const ctrlHost = new UmbTestControllerHostElement();
-			const ctrl = new UmbPropertyValueCloneController(ctrlHost);
-
-			const value = {
-				editorAlias: 'test-editor',
-				alias: 'test',
-				culture: null,
-				segment: null,
-				value: {
-					id: 'not-updated-id',
-					nestedValue: {
-						editorAlias: 'test-editor',
-						alias: 'some',
-						culture: null,
-						segment: null,
-						value: {
-							id: 'inner-not-updated-id',
-						},
-					},
-				},
-			};
-
-			const result = await ctrl.clone(value);
-
-			expect((result.value as TestPropertyValueNestedType | undefined)?.id).to.be.equal('updated-id');
-			expect((result.value as TestPropertyValueNestedType | undefined)?.nestedValue?.value?.id).to.be.equal(
-				'updated-id',
-			);
-		});
-
-		it('clones value and inner values for two levels', async () => {
-			const ctrlHost = new UmbTestControllerHostElement();
-			const ctrl = new UmbPropertyValueCloneController(ctrlHost);
-
-			const value = {
-				editorAlias: 'test-editor',
-				alias: 'test',
-				culture: null,
-				segment: null,
-				value: {
-					id: 'not-updated-id',
-					nestedValue: {
-						editorAlias: 'test-editor',
-						alias: 'some',
-						culture: null,
-						segment: null,
-						value: {
-							id: 'inner-not-updated-id',
-							nestedValue: {
-								editorAlias: 'test-editor',
-								alias: 'another',
-								culture: null,
-								segment: null,
-								value: {
-									id: 'inner-inner-not-updated-id',
-								},
-							},
-						},
-					},
-				},
-			};
-
-			const result = await ctrl.clone(value);
-
-			expect((result.value as TestPropertyValueNestedType | undefined)?.id).to.be.equal('updated-id');
-			expect((result.value as TestPropertyValueNestedType | undefined)?.nestedValue?.value?.id).to.be.equal(
-				'updated-id',
-			);
-			expect(
-				(
-					(result.value as TestPropertyValueNestedType | undefined)?.nestedValue?.value as
-						| TestPropertyValueNestedType
-						| undefined
-				)?.nestedValue?.value?.id,
-			).to.be.equal('updated-id');
+			expect(result[0].key).to.be.equal('1');
+			expect(result[0].path).to.be.equal(`$.propArray[?(@.key == '1')].innerValue`);
 		});
 	});
 });
