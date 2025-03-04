8887487b9728d5bcb60f7c19f1136811d515a4c0
Niels Lyngsø <niels.lyngso@gmail.com>
2025-03-04 20:41:02 +0100

    content detail path translator

--- src/Umbraco.Web.UI.Client/src/packages/core/validation/controllers/validation-path-translation/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/validation/controllers/validation-path-translation/index.ts
@@ -1,1 +1,2 @@
 export * from './validation-path-translation.controller.js';
+export * from './validation-property-path-translation.controller.js';
--- src/Umbraco.Web.UI.Client/src/packages/core/validation/utils/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/validation/utils/index.ts
@@ -3,3 +3,4 @@ export * from './data-path-variant-query.function.js';
 export * from './extract-json-query-properties.function.js';
 export * from './json-path.function.js';
 export * from './scope-mapper-json-paths.function.js';
+export * from './query-mapper-json-paths.function.js';
