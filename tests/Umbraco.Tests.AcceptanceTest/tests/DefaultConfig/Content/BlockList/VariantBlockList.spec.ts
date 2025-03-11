c7f0e76ad40c0c4585aa776eb56523ca21ecba36
Andreas Zerbst <andr317c@live.dk>
2025-03-11 14:00:31 +0100

    Added tests

--- tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Content/BlockList/VariantBlockList.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Content/BlockList/VariantBlockList.spec.ts
@@ -26,28 +26,32 @@ const contentName = 'ContentName';
 
 test.beforeEach(async ({umbracoApi, umbracoUi}) => {
   await umbracoApi.language.ensureIsoCodeNotExists('da');
-  const textStringDataType = await umbracoApi.dataType.getByName(textStringDataTypeName)
-  textStringDataTypeId = textStringDataType.id
+  const textStringDataType = await umbracoApi.dataType.getByName(textStringDataTypeName);
+  textStringDataTypeId = textStringDataType.id;
   await umbracoApi.language.createDanishLanguage();
 
 });
 
 test.afterEach(async ({umbracoApi, umbracoUi}) => {
   await umbracoApi.language.ensureIsoCodeNotExists('da');
+  await umbracoApi.documentType.ensureNameNotExists(documentTypeName);
+  await umbracoApi.documentType.ensureNameNotExists(blockName);
+  await umbracoApi.dataType.ensureNameNotExists(blockListName);
 });
 
-test('invariant document with invariant block list with invariant block with an invariant textString', async ({umbracoApi, umbracoUi}) => {
+test('invariant document type with invariant block list with invariant block with an invariant textString', async ({umbracoApi, umbracoUi}) => {
   // Arrange
-  elementTypeId = await umbracoApi.documentType.createDefaultElementType(blockName, elementGroupName, textStringDataTypeName, textStringDataTypeId)
-  blockListId = await umbracoApi.dataType.createBlockListDataTypeWithABlock(blockListName, elementTypeId)
-  documentTypeId = await umbracoApi.documentType.createDocumentTypeWithPropertyEditor(documentTypeName, blockListName, blockListId, documentTypeGroupName)
-  await umbracoApi.document.createDefaultDocument(contentName, documentTypeId)
+  elementTypeId = await umbracoApi.documentType.createDefaultElementType(blockName, elementGroupName, textStringName, textStringDataTypeId);
+  blockListId = await umbracoApi.dataType.createBlockListDataTypeWithABlock(blockListName, elementTypeId);
+  documentTypeId = await umbracoApi.documentType.createDocumentTypeWithPropertyEditor(documentTypeName, blockListName, blockListId, documentTypeGroupName);
+  await umbracoApi.document.createDefaultDocument(contentName, documentTypeId);
   await umbracoUi.goToBackOffice();
   await umbracoUi.content.goToSection(ConstantHelper.sections.content);
   await umbracoUi.content.goToContentWithName(contentName);
 
   // Act
-  await umbracoUi.content.clickAddBlockElementButton()
+  await umbracoUi.content.clickAddBlockElementButton();
+  await umbracoUi.content.clickBlockElementWithName(blockName);
   await umbracoUi.content.enterTextstring(textStringText);
   await umbracoUi.content.clickCreateModalButton();
   await umbracoUi.content.clickSaveAndPublishButton();
@@ -61,18 +65,19 @@ test('invariant document with invariant block list with invariant block with an 
   await umbracoUi.content.doesPropertyContainValue(textStringName, textStringText);
 });
 
-test('invariant document with invariant block list with variant block with an invariant textString', async ({umbracoApi, umbracoUi}) => {
+test('invariant document type with invariant block list with variant block with an invariant textString', async ({umbracoApi, umbracoUi}) => {
   // Arrange
-  elementTypeId = await umbracoApi.documentType.createDefaultElementTypeWithVaryByCulture(blockName, elementGroupName, textStringName, textStringDataTypeId, true, false)
-  blockListId = await umbracoApi.dataType.createBlockListDataTypeWithABlock(blockListName, elementTypeId)
-  documentTypeId = await umbracoApi.documentType.createDocumentTypeWithPropertyEditor(documentTypeName, blockListName, blockListId, documentTypeGroupName)
-  await umbracoApi.document.createDefaultDocument(contentName, documentTypeId)
+  elementTypeId = await umbracoApi.documentType.createDefaultElementTypeWithVaryByCulture(blockName, elementGroupName, textStringName, textStringDataTypeId, true, false);
+  blockListId = await umbracoApi.dataType.createBlockListDataTypeWithABlock(blockListName, elementTypeId);
+  documentTypeId = await umbracoApi.documentType.createDocumentTypeWithPropertyEditor(documentTypeName, blockListName, blockListId, documentTypeGroupName);
+  await umbracoApi.document.createDefaultDocument(contentName, documentTypeId);
   await umbracoUi.goToBackOffice();
   await umbracoUi.content.goToSection(ConstantHelper.sections.content);
   await umbracoUi.content.goToContentWithName(contentName);
 
   // Act
-  await umbracoUi.content.clickAddBlockElementButton()
+  await umbracoUi.content.clickAddBlockElementButton();
+  await umbracoUi.content.clickBlockElementWithName(blockName);
   await umbracoUi.content.enterTextstring(textStringText);
   await umbracoUi.content.clickCreateModalButton();
   await umbracoUi.content.clickSaveAndPublishButton();
@@ -86,18 +91,20 @@ test('invariant document with invariant block list with variant block with an in
   await umbracoUi.content.doesPropertyContainValue(textStringName, textStringText);
 });
 
-test('invariant document with invariant block list with variant block with an variant textString', async ({umbracoApi, umbracoUi}) => {
+// Remove fixme when this test works. Currently the textstring value is is not saved when saving / publishing the document
+test.fixme('invariant document type with invariant block list with variant block with an variant textString', async ({umbracoApi, umbracoUi}) => {
   // Arrange
-  elementTypeId = await umbracoApi.documentType.createDefaultElementTypeWithVaryByCulture(blockName, elementGroupName, textStringName, textStringDataTypeId, true, true)
-  blockListId = await umbracoApi.dataType.createBlockListDataTypeWithABlock(blockListName, elementTypeId)
-  documentTypeId = await umbracoApi.documentType.createDocumentTypeWithPropertyEditor(documentTypeName, blockListName, blockListId, documentTypeGroupName)
-  await umbracoApi.document.createDefaultDocument(contentName, documentTypeId)
+  elementTypeId = await umbracoApi.documentType.createDefaultElementTypeWithVaryByCulture(blockName, elementGroupName, textStringName, textStringDataTypeId, true, true);
+  blockListId = await umbracoApi.dataType.createBlockListDataTypeWithABlock(blockListName, elementTypeId);
+  documentTypeId = await umbracoApi.documentType.createDocumentTypeWithPropertyEditor(documentTypeName, blockListName, blockListId, documentTypeGroupName);
+  await umbracoApi.document.createDefaultDocument(contentName, documentTypeId);
   await umbracoUi.goToBackOffice();
   await umbracoUi.content.goToSection(ConstantHelper.sections.content);
   await umbracoUi.content.goToContentWithName(contentName);
 
   // Act
-  await umbracoUi.content.clickAddBlockElementButton()
+  await umbracoUi.content.clickAddBlockElementButton();
+  await umbracoUi.content.clickBlockElementWithName(blockName)
   await umbracoUi.content.enterTextstring(textStringText);
   await umbracoUi.content.clickCreateModalButton();
   await umbracoUi.content.clickSaveAndPublishButton();
@@ -111,21 +118,23 @@ test('invariant document with invariant block list with variant block with an va
   await umbracoUi.content.doesPropertyContainValue(textStringName, textStringText);
 });
 
-test('invariant document with invariant block list with invariant block with an variant textString', async ({umbracoApi, umbracoUi}) => {
+test('variant document type with variant block list with variant block with an variant textString', async ({umbracoApi, umbracoUi}) => {
   // Arrange
-  elementTypeId = await umbracoApi.documentType.createDefaultElementTypeWithVaryByCulture(blockName, elementGroupName, textStringName, textStringDataTypeId, false, true)
-  blockListId = await umbracoApi.dataType.createBlockListDataTypeWithABlock(blockListName, elementTypeId)
-  documentTypeId = await umbracoApi.documentType.createDocumentTypeWithPropertyEditor(documentTypeName, blockListName, blockListId, documentTypeGroupName)
-  await umbracoApi.document.createDefaultDocument(contentName, documentTypeId)
+  elementTypeId = await umbracoApi.documentType.createDefaultElementTypeWithVaryByCulture(blockName, elementGroupName, textStringName, textStringDataTypeId, true, true);
+  blockListId = await umbracoApi.dataType.createBlockListDataTypeWithABlock(blockListName, elementTypeId);
+  documentTypeId = await umbracoApi.documentType.createDocumentTypeWithPropertyEditor(documentTypeName, blockListName, blockListId, documentTypeGroupName, true);
+  await umbracoApi.document.createDefaultDocumentWithEnglishCulture(contentName, documentTypeId);
   await umbracoUi.goToBackOffice();
   await umbracoUi.content.goToSection(ConstantHelper.sections.content);
   await umbracoUi.content.goToContentWithName(contentName);
 
   // Act
-  await umbracoUi.content.clickAddBlockElementButton()
+  await umbracoUi.content.clickAddBlockElementButton();
+  await umbracoUi.content.clickBlockElementWithName(blockName);
   await umbracoUi.content.enterTextstring(textStringText);
   await umbracoUi.content.clickCreateModalButton();
   await umbracoUi.content.clickSaveAndPublishButton();
+  await umbracoUi.content.clickContainerSaveAndPublishButton();
 
   // Assert
   await umbracoUi.content.doesSuccessNotificationHaveText(NotificationConstantHelper.success.saved);
@@ -136,15 +145,57 @@ test('invariant document with invariant block list with invariant block with an 
   await umbracoUi.content.doesPropertyContainValue(textStringName, textStringText);
 });
 
-test('variant document with variant block list with variant block with an variant textString', async ({umbracoApi, umbracoUi}) => {
+test('variant document type with invariant block list with variant block with an invariant textString', async ({umbracoApi, umbracoUi}) => {
+  // Arrange
+  elementTypeId = await umbracoApi.documentType.createDefaultElementTypeWithVaryByCulture(blockName, elementGroupName, textStringName, textStringDataTypeId, true, false);
+  blockListId = await umbracoApi.dataType.createBlockListDataTypeWithABlock(blockListName, elementTypeId);
+  documentTypeId = await umbracoApi.documentType.createDocumentTypeWithPropertyEditor(documentTypeName, blockListName, blockListId, documentTypeGroupName, true, false);
+  await umbracoApi.document.createDefaultDocumentWithEnglishCulture(contentName, documentTypeId);
+  await umbracoUi.goToBackOffice();
+  await umbracoUi.content.goToSection(ConstantHelper.sections.content);
+  await umbracoUi.content.goToContentWithName(contentName);
 
+  // Act
+  await umbracoUi.content.clickAddBlockElementButton();
+  await umbracoUi.content.clickBlockElementWithName(blockName);
+  await umbracoUi.content.enterTextstring(textStringText);
+  await umbracoUi.content.clickCreateModalButton();
+  await umbracoUi.content.clickSaveAndPublishButton();
+  await umbracoUi.content.clickContainerSaveAndPublishButton();
+
+  // Assert
+  await umbracoUi.content.doesSuccessNotificationHaveText(NotificationConstantHelper.success.saved);
+  await umbracoUi.content.doesSuccessNotificationHaveText(NotificationConstantHelper.success.published);
+
+  await umbracoUi.reloadPage();
+  await umbracoUi.content.goToBlockListBlockWithName(documentTypeGroupName, blockListName, blockName);
+  await umbracoUi.content.doesPropertyContainValue(textStringName, textStringText);
 });
 
-test('variant document with invariant block list with variant block with an invariant textString', async ({umbracoApi, umbracoUi}) => {
+test('variant document type with invariant block list with variant block with an variant textString', async ({umbracoApi, umbracoUi}) => {
+  // Arrange
+  elementTypeId = await umbracoApi.documentType.createDefaultElementTypeWithVaryByCulture(blockName, elementGroupName, textStringName, textStringDataTypeId, true, true);
+  blockListId = await umbracoApi.dataType.createBlockListDataTypeWithABlock(blockListName, elementTypeId);
+  documentTypeId = await umbracoApi.documentType.createDocumentTypeWithPropertyEditor(documentTypeName, blockListName, blockListId, documentTypeGroupName, true, false);
+  await umbracoApi.document.createDefaultDocumentWithEnglishCulture(contentName, documentTypeId);
+  await umbracoUi.goToBackOffice();
+  await umbracoUi.content.goToSection(ConstantHelper.sections.content);
+  await umbracoUi.content.goToContentWithName(contentName);
 
-});
+  // Act
+  await umbracoUi.content.clickAddBlockElementButton()
+  await umbracoUi.content.clickBlockElementWithName(blockName);
+  await umbracoUi.content.enterTextstring(textStringText);
+  await umbracoUi.content.clickCreateModalButton();
+  await umbracoUi.content.clickSaveAndPublishButton();
+  await umbracoUi.content.clickContainerSaveAndPublishButton();
 
-test('variant document with invariant block list with variant block with an variant textString', async ({umbracoApi, umbracoUi}) => {
+  // Assert
+  await umbracoUi.content.doesSuccessNotificationHaveText(NotificationConstantHelper.success.saved);
+  await umbracoUi.content.doesSuccessNotificationHaveText(NotificationConstantHelper.success.published);
 
+  await umbracoUi.reloadPage();
+  await umbracoUi.content.goToBlockListBlockWithName(documentTypeGroupName, blockListName, blockName);
+  await umbracoUi.content.doesPropertyContainValue(textStringName, textStringText);
 });
 
