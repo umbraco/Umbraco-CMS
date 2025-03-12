5320f524c26bddc70909adb1ba8f162ace61e682
Andy Butland <abutland73@gmail.com>
2025-03-12 15:35:57 +0100

    Removed obsolete methods on ITwoFactorLoginService. Removed obsolete ITemporaryMediaService.

--- src/Umbraco.Core/DependencyInjection/UmbracoBuilder.cs
+++ src/Umbraco.Core/DependencyInjection/UmbracoBuilder.cs
@@ -370,7 +370,6 @@ namespace Umbraco.Cms.Core.DependencyInjection
 
             Services.AddUnique<ICultureImpactFactory>(provider => new CultureImpactFactory(provider.GetRequiredService<IOptionsMonitor<ContentSettings>>()));
             Services.AddUnique<IDictionaryService, DictionaryService>();
-            Services.AddUnique<ITemporaryMediaService, TemporaryMediaService>();
             Services.AddUnique<IMediaImportService, MediaImportService>();
 
             // Register filestream security analyzers
