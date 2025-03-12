55e893e47475062bb4de0af6c0caea6fbc7da3b0
Mads Rasmussen <madsr@hey.com>
2025-03-12 13:47:04 +0100

    remove temp test

--- src/Umbraco.Web.UI.Client/src/packages/media/media-types/structure/manifests.ts
+++
@@ -1,4 +0,0 @@
-import { manifests as permissionManifests } from './user-permission/manifests.js';
-import type { UmbExtensionManifestKind } from '@umbraco-cms/backoffice/extension-registry';
-
-export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> = [...permissionManifests];
--- src/Umbraco.Web.UI.Client/src/packages/media/media-types/structure/user-permission/constants.ts
+++
@@ -1,1 +0,0 @@
-export const UMB_USER_PERMISSION_MEDIA_TYPE_STRUCTURE_PROPERTY_SEE = 'Umb.MediaType.Structure.Property.See';
