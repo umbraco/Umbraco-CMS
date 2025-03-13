bf1992a4b17802f4eeb18a16d9a7091eb91b8f45
Mads Rasmussen <madsr@hey.com>
2025-03-13 14:16:35 +0100

    rename see permission + add write permission

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/user-permissions/document-value/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/user-permissions/document-value/manifests.ts
@@ -1,17 +1,29 @@
-import { UMB_USER_PERMISSION_DOCUMENT_VALUE_SEE } from './constants.js';
+import { UMB_USER_PERMISSION_DOCUMENT_VALUE_READ, UMB_USER_PERMISSION_DOCUMENT_VALUE_WRITE } from './constants.js';
 import type { UmbExtensionManifestKind } from '@umbraco-cms/backoffice/extension-registry';
 
 export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> = [
 	{
 		type: 'entityUserPermission',
-		alias: 'Umb.EntityUserPermission.DocumentValue.See',
-		name: 'See Document Value User Permission',
+		alias: 'Umb.EntityUserPermission.DocumentValue.Read',
+		name: 'Read Document Value User Permission',
 		forEntityTypes: ['document-value'],
 		weight: 200,
 		meta: {
-			verbs: [UMB_USER_PERMISSION_DOCUMENT_VALUE_SEE],
-			label: 'See',
-			description: 'See Document values',
+			verbs: [UMB_USER_PERMISSION_DOCUMENT_VALUE_READ],
+			label: 'Read',
+			description: 'Read Document values',
+		},
+	},
+	{
+		type: 'entityUserPermission',
+		alias: 'Umb.EntityUserPermission.DocumentValue.Write',
+		name: 'Write Document Value User Permission',
+		forEntityTypes: ['document-value'],
+		weight: 200,
+		meta: {
+			verbs: [UMB_USER_PERMISSION_DOCUMENT_VALUE_WRITE],
+			label: 'Write',
+			description: 'Write Document values',
 		},
 	},
 	{
