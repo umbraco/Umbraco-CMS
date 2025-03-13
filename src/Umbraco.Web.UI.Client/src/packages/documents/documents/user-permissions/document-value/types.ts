66f20827bf549a554a84998246897e5274f88905
Mads Rasmussen <madsr@hey.com>
2025-03-13 14:29:00 +0100

    require property type unique

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/user-permissions/document-value/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/user-permissions/document-value/types.ts
@@ -1,12 +1,10 @@
 import type { UmbUserPermissionModel } from '@umbraco-cms/backoffice/user-permission';
 
-export interface UmbReferencedByAlias {
-	alias: string;
-}
-
 export interface UmbDocumentValueUserPermissionModel extends UmbUserPermissionModel {
 	documentType: {
 		unique: string;
 	};
-	property: UmbReferencedByAlias;
+	property: {
+		unique: string;
+	};
 }
