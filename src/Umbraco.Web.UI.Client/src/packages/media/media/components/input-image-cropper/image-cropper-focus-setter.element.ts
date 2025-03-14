e6078ddbfc62f1aba558754f9c404bf01ed431a5
Lan Nguyen Thuy <lnt@umbraco.dk>
2025-03-14 12:35:17 +0700

    fix focal point drag not working on firefox

--- src/Umbraco.Web.UI.Client/src/packages/media/media/components/input-image-cropper/image-cropper-focus-setter.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/components/input-image-cropper/image-cropper-focus-setter.element.ts
@@ -148,7 +148,7 @@ export class UmbImageCropperFocusSetterElement extends UmbLitElement {
 
 	#handleGridDrag(event: PointerEvent) {
 		if (this.disabled || this.hideFocalPoint) return;
-
+		
 		const grid = this.wrapperElement;
 		const handle = this.focalPointElement;
 
@@ -169,12 +169,33 @@ export class UmbImageCropperFocusSetterElement extends UmbLitElement {
 
 				this.coords.x = x;
 				this.coords.y = y;
-
+				
 				this.#setFocalPoint(x, y, width, height);
 			},
 			onStop: () => (this._isDraggingGridHandle = false),
 			initialEvent: event,
 		});
+	}
+
+	#changeFocalPoint(event: PointerEvent){
+		if (this.disabled || this.hideFocalPoint) return;
+
+		const grid = this.wrapperElement;
+		const handle = this.focalPointElement;
+
+		if (!grid) return;
+
+		handle?.focus();
+
+		const dims = grid.getBoundingClientRect();
+		const defaultView = grid.ownerDocument.defaultView!;
+		const { width, height } = grid.getBoundingClientRect();
+		const offsetX = dims.left + defaultView.scrollX;
+		const offsetY = dims.top + defaultView.scrollY;
+
+		const x = event.pageX - offsetX;
+		const y = event.pageY - offsetY;
+		this.#setFocalPoint(x, y, width, height);
 	}
 
 	#handleGridKeyDown(event: KeyboardEvent) {
@@ -215,7 +236,7 @@ export class UmbImageCropperFocusSetterElement extends UmbLitElement {
 	override render() {
 		if (!this.src) return nothing;
 		return html`
-			<div id="wrapper" @mousedown=${this.#handleGridDrag} @touchstart=${this.#handleGridDrag}>
+			<div id="wrapper" @click=${this.#changeFocalPoint} @mousedown=${this.#handleGridDrag} @touchstart=${this.#handleGridDrag}>
 				<img id="image" @keydown=${() => nothing} src=${this.src} alt="" />
 				<span
 					id="focal-point"
