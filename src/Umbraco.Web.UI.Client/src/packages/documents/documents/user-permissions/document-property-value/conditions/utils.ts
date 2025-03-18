14080c769274deb6bd524037c3ff6516873ac1a8
Mads Rasmussen <madsr@hey.com>
2025-03-18 13:18:18 +0100

    use permission type for guard check

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/user-permissions/document-property-value/conditions/utils.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/user-permissions/document-property-value/conditions/utils.ts
@@ -3,13 +3,10 @@ import type { UmbDocumentPropertyValueUserPermissionModel } from '../types.js';
 /**
  *
  * @param permission
- * @returns {boolean} True if the permission is a DocumentPropertyValuePermissionPresentationModel
+ * @returns {boolean} True if the permission is a document-property-value' permission
  */
 export function isDocumentPropertyValueUserPermission(
 	permission: unknown,
 ): permission is UmbDocumentPropertyValueUserPermissionModel {
-	return (
-		(permission as UmbDocumentPropertyValueUserPermissionModel).$type ===
-		'DocumentPropertyValuePermissionPresentationModel'
-	);
+	return (permission as UmbDocumentPropertyValueUserPermissionModel).permissionType === 'document-property-value';
 }
