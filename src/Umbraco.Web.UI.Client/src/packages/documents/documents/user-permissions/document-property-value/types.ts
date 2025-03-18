7f589477ff1a15caca91e688919f6d40a2eb32b2
Mads Rasmussen <madsr@hey.com>
2025-03-18 13:19:01 +0100

    require specific permission type

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/user-permissions/document-property-value/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/user-permissions/document-property-value/types.ts
@@ -2,6 +2,7 @@ import type { UmbReferenceByUnique } from '@umbraco-cms/backoffice/models';
 import type { UmbUserPermissionModel } from '@umbraco-cms/backoffice/user-permission';
 
 export interface UmbDocumentPropertyValueUserPermissionModel extends UmbUserPermissionModel {
+	permissionType: 'document-property-value';
 	documentType: UmbReferenceByUnique;
 	propertyType: UmbReferenceByUnique;
 }
