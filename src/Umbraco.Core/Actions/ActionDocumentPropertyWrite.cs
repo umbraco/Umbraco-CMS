15e7b9074e59dbd8aa569a58203b15805b70c02c
kjac <kja@umbraco.dk>
2025-03-20 13:34:21 +0100

    Add migration for default document property value permissions

---
+++ src/Umbraco.Core/Actions/ActionDocumentPropertyWrite.cs
@@ -0,0 +1,29 @@
+﻿namespace Umbraco.Cms.Core.Actions;
+
+public class ActionDocumentPropertyWrite : IAction
+{
+    /// <inheritdoc cref="IAction.ActionLetter" />
+    public const string ActionLetter = "Umb.Document.Property.Write";
+
+    /// <inheritdoc cref="IAction.ActionAlias" />
+    public const string ActionAlias = "documentpropertywrite";
+
+    /// <inheritdoc/>
+    public string Letter => ActionLetter;
+
+    /// <inheritdoc/>
+    public string Alias => ActionAlias;
+
+    /// <inheritdoc />
+    public bool ShowInNotifier => false;
+
+    /// <inheritdoc />
+    public bool CanBePermissionAssigned => true;
+
+    /// <inheritdoc />
+    public string Icon => string.Empty;
+
+    /// <inheritdoc />
+    public string Category => Constants.Conventions.PermissionCategories.OtherCategory;
+}
+
