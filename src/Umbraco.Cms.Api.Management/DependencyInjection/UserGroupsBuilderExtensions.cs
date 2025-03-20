c8112b0868cb23d58879588d196fd5eec8642b1d
kjac <kja@umbraco.dk>
2025-03-20 13:18:37 +0100

    Rename models (last time, promise!)

--- src/Umbraco.Cms.Api.Management/DependencyInjection/UserGroupsBuilderExtensions.cs
+++ src/Umbraco.Cms.Api.Management/DependencyInjection/UserGroupsBuilderExtensions.cs
@@ -16,8 +16,8 @@ internal static class UserGroupsBuilderExtensions
         builder.Services.AddSingleton<IPermissionMapper, DocumentPermissionMapper>();
         builder.Services.AddSingleton<IPermissionPresentationMapper, DocumentPermissionMapper>();
 
-        builder.Services.AddSingleton<IPermissionMapper, DocumentTypePropertyPermissionMapper>();
-        builder.Services.AddSingleton<IPermissionPresentationMapper, DocumentTypePropertyPermissionMapper>();
+        builder.Services.AddSingleton<IPermissionMapper, DocumentPropertyValuePermissionMapper>();
+        builder.Services.AddSingleton<IPermissionPresentationMapper, DocumentPropertyValuePermissionMapper>();
 
         return builder;
     }
