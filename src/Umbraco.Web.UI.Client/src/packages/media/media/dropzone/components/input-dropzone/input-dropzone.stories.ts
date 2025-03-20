79bc54de59b292de4cc9bcdc4effe0fb75f5586d
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-03-20 16:30:35 +0100

    move input-dropzone into dropzone package

---
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/dropzone/components/input-dropzone/input-dropzone.stories.ts
@@ -0,0 +1,55 @@
+import type { UmbInputDropzoneElement } from './input-dropzone.element.js';
+import type { Meta, StoryObj } from '@storybook/web-components';
+
+import './input-dropzone.element.js';
+import { html } from '@umbraco-cms/backoffice/external/lit';
+
+const meta: Meta<UmbInputDropzoneElement> = {
+	id: 'umb-input-dropzone',
+	title: 'Components/Inputs/Dropzone',
+	component: 'umb-input-dropzone',
+	args: {
+		disabled: false,
+		accept: '',
+		createAsTemporary: true,
+	},
+	decorators: [(Story) => html`<div style="width: 300px">${Story()}</div>`],
+	parameters: {
+		layout: 'centered',
+		actions: {
+			handles: ['submitted', 'change'],
+		},
+	},
+};
+
+export default meta;
+
+type Story = StoryObj<UmbInputDropzoneElement>;
+
+export const Overview: Story = {};
+
+export const WithDisabled: Story = {
+	args: {
+		disabled: true,
+	},
+};
+
+export const WithAccept: Story = {
+	args: {
+		accept: 'jpg,png',
+	},
+	parameters: {
+		docs: {
+			description: {
+				story: 'This is a dropzone with an accept attribute set to "jpg,png".',
+			},
+		},
+	},
+};
+
+export const WithDefaultSlot: Story = {
+	render: () =>
+		html`<umb-input-dropzone>
+			<div>Custom slot</div>
+		</umb-input-dropzone>`,
+};
