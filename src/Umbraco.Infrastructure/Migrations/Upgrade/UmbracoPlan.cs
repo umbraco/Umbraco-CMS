94d36a1381e20131814b7b6bb2d900ab9086e076
kjac <kja@umbraco.dk>
2025-03-20 13:34:21 +0100

    Add migration for default document property value permissions

--- src/Umbraco.Infrastructure/Migrations/Upgrade/UmbracoPlan.cs
+++ src/Umbraco.Infrastructure/Migrations/Upgrade/UmbracoPlan.cs
@@ -114,5 +114,6 @@ public class UmbracoPlan : MigrationPlan
 
         // To 15.4.0
         To<V_15_4_0.UpdateDocumentUrlToPersistMultipleSegmentsPerDocument>("{A9E72794-4036-4563-B543-1717C73B8879}");
+        To<V_15_4_0.AddDocumentPropertyPermissions>("{D1568C33-A697-455F-8D16-48060CB954A1}");
     }
 }
---
+++ src/Umbraco.Infrastructure/Migrations/Upgrade/V_15_4_0/AddDocumentPropertyPermissions.cs
@@ -0,0 +1,47 @@
+﻿using Umbraco.Cms.Core.Actions;
+using Umbraco.Cms.Infrastructure.Persistence.Dtos;
+
+namespace Umbraco.Cms.Infrastructure.Migrations.Upgrade.V_15_4_0;
+
+[Obsolete("Remove in Umbraco 18.")]
+internal class AddDocumentPropertyPermissions : MigrationBase
+{
+    public AddDocumentPropertyPermissions(IMigrationContext context)
+        : base(context)
+    {
+    }
+
+    protected override void Migrate()
+    {
+        List<UserGroupDto>? userGroups = Database.Fetch<UserGroupDto>();
+
+        foreach (UserGroupDto userGroupDto in userGroups ?? [])
+        {
+            List<UserGroup2PermissionDto>? currentPermissions = Database.Fetch<UserGroup2PermissionDto>(
+                "WHERE userGroupKey = @UserGroupKey",
+                new { UserGroupKey = userGroupDto.Key });
+
+            if (currentPermissions is null || currentPermissions.Count is 0)
+            {
+                continue;
+            }
+
+            if (currentPermissions.Any(p => p.Permission == ActionDocumentPropertyRead.ActionLetter))
+            {
+                return;
+            }
+
+            Database.InsertBulk(
+            [
+                new UserGroup2PermissionDto
+                {
+                    UserGroupKey = userGroupDto.Key, Permission = ActionDocumentPropertyRead.ActionLetter
+                },
+                new UserGroup2PermissionDto
+                {
+                    UserGroupKey = userGroupDto.Key, Permission = ActionDocumentPropertyWrite.ActionLetter
+                }
+            ]);
+        }
+    }
+}
