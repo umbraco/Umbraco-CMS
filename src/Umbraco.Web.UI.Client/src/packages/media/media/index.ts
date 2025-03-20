3cbdc4821aa2ee66f88e8c6e22293cc7e50aceb0
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-03-20 17:14:38 +0100

    reexport everything from dropzone

--- src/Umbraco.Web.UI.Client/src/packages/media/media/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/index.ts
@@ -1,5 +1,3 @@
-import '@umbraco-cms/backoffice/dropzone'; // TODO: Introduced for backward compatibility in V15 because the components were once part of this package. Remove later on.
-
 export * from './components/index.js';
 export * from './constants.js';
 export * from './reference/index.js';
@@ -11,3 +9,9 @@ export * from './utils/index.js';
 export { UmbMediaAuditLogRepository } from './audit-log/index.js';
 
 export type * from './types.js';
+
+// #region Dropzone
+// TODO: The following import and export were introduced for backward compatibility in V15 because the components were once part of this package. Remove later on.
+import '@umbraco-cms/backoffice/dropzone';
+export * from '@umbraco-cms/backoffice/dropzone';
+// #endregion
