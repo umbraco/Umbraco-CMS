45964a4581979400cac259704adc975552fa8fad
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-03-20 17:01:24 +0100

    import for backward compatibility

--- src/Umbraco.Web.UI.Client/src/packages/media/media/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/index.ts
@@ -1,3 +1,5 @@
+import '@umbraco-cms/backoffice/dropzone'; // TODO: Introduced for backward compatibility in V15 because the components were once part of this package. Remove later on.
+
 export * from './components/index.js';
 export * from './constants.js';
 export * from './reference/index.js';
