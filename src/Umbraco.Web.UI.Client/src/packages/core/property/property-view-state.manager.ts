2152d9688e84db0a16d5bfa65cdb054faa788330
Mads Rasmussen <madsr@hey.com>
2025-03-23 19:42:25 +0100

    add helpers

--- src/Umbraco.Web.UI.Client/src/packages/core/property/property-view-state.manager.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property/property-view-state.manager.ts
@@ -1,5 +1,6 @@
 import type { UmbReferenceByUnique } from '@umbraco-cms/backoffice/models';
 import { UmbStateManager, type UmbState } from '@umbraco-cms/backoffice/utils';
+import type { Observable } from '@umbraco-cms/backoffice/observable-api';
 
 export interface UmbPropertyViewState extends UmbState {
 	propertyType: UmbReferenceByUnique;
@@ -7,4 +8,19 @@ export interface UmbPropertyViewState extends UmbState {
 
 export class UmbPropertyViewStateManager<
 	ViewStateType extends UmbPropertyViewState = UmbPropertyViewState,
-> extends UmbStateManager<ViewStateType> {}
+> extends UmbStateManager<ViewStateType> {
+	/**
+	 * Get the viewable state
+	 * @returns {Observable<boolean>} True if the property is viewable
+	 * @memberof UmbPropertyViewStateManager
+	 */
+	public readonly isViewable = this.isOn;
+
+	/**
+	 * Get the property viewable state
+	 * @returns {boolean} True if the property is viewable
+	 */
+	public getIsViewable(): boolean {
+		return this.getIsOn();
+	}
+}
