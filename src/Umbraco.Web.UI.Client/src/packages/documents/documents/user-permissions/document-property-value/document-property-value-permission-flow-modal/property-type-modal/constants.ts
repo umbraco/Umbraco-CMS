fdacecd8557957246771c49717d5f3ae80f359e7
Mads Rasmussen <madsr@hey.com>
2025-03-24 08:29:37 +0100

    export consts

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/user-permissions/document-property-value/document-property-value-permission-flow-modal/property-type-modal/constants.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/user-permissions/document-property-value/document-property-value-permission-flow-modal/property-type-modal/constants.ts
@@ -1,2 +1,4 @@
+export { UMB_DOCUMENT_PROPERTY_VALUE_USER_PERMISSION_FLOW_PROPERTY_TYPE_MODAL } from './property-type-modal.token.js';
+
 export const UMB_DOCUMENT_PROPERTY_VALUE_USER_PERMISSION_FLOW_PROPERTY_TYPE_MODAL_ALIAS =
 	'Umb.Modal.DocumentPropertyValueUserPermissionFlow.PropertyType';
