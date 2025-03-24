34649db5c9a1d48c7a116899c2d7a5b240520e4a
Mads Rasmussen <madsr@hey.com>
2025-03-24 15:46:34 +0100

    Block Grid: Fix circular dependencies (#18782)
    * Update input-rich-media.element.ts
    
    * fix circular
    
    * fixes circular dependencies in the validation module
    
    * Update bind-to-validation.lit-directive.ts
    
    * fix workspace circular
    
    * Update error-viewer-modal.token.ts
    
    * move contexts next to component
    
    * fix import errors
    
    * import local components from one file
    
    * lint
    
    * import in property editor file
    
    * fix import
    
    * remove duplicate import

---
+++ src/Umbraco.Web.UI.Client/src/packages/block/block-grid/local-components.ts
@@ -0,0 +1,8 @@
+import './components/block-grid-areas-container/block-grid-areas-container.element.js';
+import './components/block-grid-block-inline/block-grid-block-inline.element.js';
+import './components/block-grid-block-unsupported/block-grid-block-unsupported.element.js';
+import './components/block-grid-block/block-grid-block.element.js';
+import './components/block-grid-entries/block-grid-entries.element.js';
+import './components/block-grid-entry/block-grid-entry.element.js';
+import './components/block-scale-handler/block-scale-handler.element.js';
+import './components/ref-grid-block/ref-grid-block.element.js';
