84f442bf637563b785b69b50a05e1089176c2521
Mads Rasmussen <madsr@hey.com>
2025-03-24 08:22:46 +0100

    export consts

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/user-permissions/document-property-value/constants.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/user-permissions/document-property-value/constants.ts
@@ -1,2 +1,4 @@
 export const UMB_USER_PERMISSION_DOCUMENT_PROPERTY_VALUE_READ = 'Umb.Document.PropertyValue.Read';
 export const UMB_USER_PERMISSION_DOCUMENT_PROPERTY_VALUE_WRITE = 'Umb.Document.PropertyValue.Write';
+
+export * from './conditions/constants.js';
