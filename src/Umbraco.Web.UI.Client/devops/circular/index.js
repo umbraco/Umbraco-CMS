6996b1d4f48337eaf91f1af08b3d89b773523295
Niels Lyngsø <niels.lyngso@gmail.com>
2025-03-25 08:54:24 +0100

    update code

--- src/Umbraco.Web.UI.Client/devops/circular/index.js
+++ src/Umbraco.Web.UI.Client/devops/circular/index.js
@@ -40,18 +40,23 @@ if (circular.length) {
 	}
 	console.error('\nPlease fix the circular dependencies before proceeding.\n');
 
+	/*
+	// Curently disabled as we don't have Graphviz installed on the CI servers neither do we use this visualization currently.
+	// Ideally its an opt in feature that is triggered by a environment variable.
 	try {
 		const imagePath = join(__dirname, '../../madge');
 		mkdirSync(imagePath, { recursive: true });
 		const image = await madgeSetup.image(join(imagePath, 'circular.svg'), true);
 		console.log('Circular dependencies graph generated:', image);
 	} catch { console.warn('No image generated. Make sure Graphviz is in your $PATH if you want a visualization'); }
+	*/
 
 	// TODO: Remove this check and set an exit with argument 1 when we have fixed all circular dependencies.
-	if (circular.length > 8)
-		process.exit(1)
-	else
-		process.exit(0)
+	if (circular.length > 8) {
+		process.exit(1);
+	} else {
+		process.exit(0);
+	}
 }
 
 console.log('\nNo circular dependencies detected.\n');
