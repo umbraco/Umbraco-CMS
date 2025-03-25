0172302a25d658a7b7b076a8624593d771e3dd8a
Mads Rasmussen <madsr@hey.com>
2025-03-25 11:56:44 +0100

    split logic

---
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/user-permissions/document-property-value/workspace-context/document-block-property-value-user-permission.workspace-context.ts
@@ -0,0 +1,52 @@
+import { UMB_DOCUMENT_WORKSPACE_CONTEXT } from '../../../workspace/constants.js';
+import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
+import { observeMultiple } from '@umbraco-cms/backoffice/observable-api';
+import { UMB_BLOCK_WORKSPACE_CONTEXT } from '@umbraco-cms/backoffice/block';
+import { UMB_PROPERTY_DATASET_CONTEXT } from '@umbraco-cms/backoffice/property';
+import { UmbPropertyValueUserPermissionWorkspaceContextBase } from './property-value-user-permission-workspace-context-base.js';
+
+export class UmbDocumentBlockPropertyValueUserPermissionWorkspaceContext extends UmbPropertyValueUserPermissionWorkspaceContextBase {
+	#blockWorkspaceContext?: typeof UMB_BLOCK_WORKSPACE_CONTEXT.TYPE;
+
+	constructor(host: UmbControllerHost) {
+		super(host);
+
+		this.consumeContext(UMB_BLOCK_WORKSPACE_CONTEXT, async (context) => {
+			this.#blockWorkspaceContext = context;
+
+			// We only want to apply the permission logic if the block is in a document
+			const documentWorkspaceContext = await this.consumeContext(UMB_DOCUMENT_WORKSPACE_CONTEXT, () => {})
+				.passContextAliasMatches()
+				.asPromise();
+
+			if (documentWorkspaceContext) {
+				this.#observeDocumentBlockProperties();
+			}
+		});
+	}
+
+	async #observeDocumentBlockProperties() {
+		if (!this.#blockWorkspaceContext) return;
+		const datasetContext = await this.getContext(UMB_PROPERTY_DATASET_CONTEXT);
+		if (!datasetContext) return;
+
+		const structureManager = this.#blockWorkspaceContext.content.structure;
+
+		this.observe(
+			observeMultiple([structureManager.contentTypeProperties, this.#blockWorkspaceContext.variantId]),
+			([properties, variantId]) => {
+				if (properties.length === 0) return;
+				if (!variantId) return;
+
+				this._setPermissions(
+					properties,
+					[variantId],
+					structureManager.propertyViewState,
+					structureManager.propertyWriteState,
+				);
+			},
+		);
+	}
+}
+
+export { UmbDocumentBlockPropertyValueUserPermissionWorkspaceContext as api };
