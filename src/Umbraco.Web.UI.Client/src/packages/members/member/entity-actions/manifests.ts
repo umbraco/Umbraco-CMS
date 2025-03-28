1c0bd606f347de19607861986072fb592e4caec0
Andy Butland <abutland73@gmail.com>
2025-03-28 10:06:14 +0100

    Removed unused import.

--- src/Umbraco.Web.UI.Client/src/packages/members/member/entity-actions/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/members/member/entity-actions/manifests.ts
@@ -3,7 +3,6 @@ import { UMB_MEMBER_ENTITY_TYPE } from '../entity.js';
 import { UMB_MEMBER_DETAIL_REPOSITORY_ALIAS } from '../repository/detail/manifests.js';
 import { manifests as createManifests } from './create/manifests.js';
 import { UMB_MEMBER_REFERENCE_REPOSITORY_ALIAS } from '../reference/constants.js';
-import { UMB_ENTITY_IS_TRASHED_CONDITION_ALIAS } from '@umbraco-cms/backoffice/recycle-bin';
 
 export const manifests: Array<UmbExtensionManifest> = [
 	{
