cf078f82f7e00b05f6382aba9b0f2b189a5ddbfe
Niels Lyngsø <niels.lyngso@gmail.com>
2025-03-28 12:33:57 +0100

    make the deprecation discoverable from TSC

--- src/Umbraco.Web.UI.Client/src/packages/block/block-rte/context/block-rte-manager.context.ts
+++ src/Umbraco.Web.UI.Client/src/packages/block/block-rte/context/block-rte-manager.context.ts
@@ -32,9 +32,8 @@ export class UmbBlockRteManagerContext<
 		// This property is used by some implementations, but not used in this. Do not remove. [NL]
 		// eslint-disable-next-line @typescript-eslint/no-unused-vars
 		_originData?: UmbBlockRteWorkspaceOriginData,
-	) {
+	): never {
 		throw new Error('Method deparecated use createWithPresets');
-		return {} as UmbBlockDataObjectModel<BlockLayoutType>;
 	}
 	async createWithPresets(
 		contentElementTypeKey: string,
