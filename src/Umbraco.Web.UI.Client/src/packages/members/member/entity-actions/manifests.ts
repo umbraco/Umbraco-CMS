331d9d5b233462febff10ca9097cf80d7a27de73
Andy Butland <abutland73@gmail.com>
2025-03-28 09:59:28 +0100

    Add relation notification to member deletion.

--- src/Umbraco.Web.UI.Client/src/packages/members/member/entity-actions/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/members/member/entity-actions/manifests.ts
@@ -2,17 +2,20 @@ import { UMB_MEMBER_ITEM_REPOSITORY_ALIAS } from '../item/constants.js';
 import { UMB_MEMBER_ENTITY_TYPE } from '../entity.js';
 import { UMB_MEMBER_DETAIL_REPOSITORY_ALIAS } from '../repository/detail/manifests.js';
 import { manifests as createManifests } from './create/manifests.js';
+import { UMB_MEMBER_REFERENCE_REPOSITORY_ALIAS } from '../reference/constants.js';
+import { UMB_ENTITY_IS_TRASHED_CONDITION_ALIAS } from '@umbraco-cms/backoffice/recycle-bin';
 
 export const manifests: Array<UmbExtensionManifest> = [
 	{
 		type: 'entityAction',
-		kind: 'delete',
 		alias: 'Umb.EntityAction.Member.Delete',
 		name: 'Delete Member Entity Action',
+		kind: 'deleteWithRelation',
 		forEntityTypes: [UMB_MEMBER_ENTITY_TYPE],
 		meta: {
-			detailRepositoryAlias: UMB_MEMBER_DETAIL_REPOSITORY_ALIAS,
 			itemRepositoryAlias: UMB_MEMBER_ITEM_REPOSITORY_ALIAS,
+			detailRepositoryAlias: UMB_MEMBER_DETAIL_REPOSITORY_ALIAS,
+			referenceRepositoryAlias: UMB_MEMBER_REFERENCE_REPOSITORY_ALIAS,
 		},
 	},
 	...createManifests,
