ea4455c6f10c099c59409b80e7f6861dacc68138
Mads Rasmussen <madsr@hey.com>
2025-03-28 13:58:13 +0100

    align naming

--- src/Umbraco.Web.UI.Client/src/packages/relations/relations/global-components/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/relations/relations/global-components/index.ts
@@ -1,5 +1,5 @@
-import './confirm-action-entity-references.element.js';
-import './confirm-bulk-action-entity-references.element.js';
+import './confirm-action-modal-entity-references.element.js';
+import './confirm-bulk-action-modal-entity-references.element.js';
 
-export * from './confirm-action-entity-references.element.js';
-export * from './confirm-bulk-action-entity-references.element.js';
+export * from './confirm-action-modal-entity-references.element.js';
+export * from './confirm-bulk-action-modal-entity-references.element.js';
