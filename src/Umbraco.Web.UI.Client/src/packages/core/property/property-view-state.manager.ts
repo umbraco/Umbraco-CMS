d191be7b722bbf3a6326981ea9952b52694982d0
Niels Lyngsø <niels.lyngso@gmail.com>
2025-04-01 23:55:11 +0200

    clean up

--- src/Umbraco.Web.UI.Client/src/packages/core/property/property-view-state.manager.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property/property-view-state.manager.ts
@@ -13,21 +13,6 @@ export class UmbPropertyViewStateManager<
 		super(host);
 		// To avoid breaking changes in rendering this state is stopped by default. This means that properties are viewable by default.
 		// We start this state in workspaces where we want to control the viewability of properties.
-		this.stop();
-	}
-
-	/**
-	 * Get the viewable state
-	 * @returns {Observable<boolean>} True if the property is viewable
-	 * @memberof UmbPropertyViewStateManager
-	 */
-	public readonly isViewable = this.isOn;
-
-	/**
-	 * Get the property viewable state
-	 * @returns {boolean} True if the property is viewable
-	 */
-	public getIsViewable(): boolean {
-		return this.getIsOn();
+		this.fallbackToOn();
 	}
 }
