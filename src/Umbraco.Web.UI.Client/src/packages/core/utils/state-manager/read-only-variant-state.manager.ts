6a80ecca424f30e2cbd75cedb1ad9d1c1746cdd6
Niels Lyngsø <niels.lyngso@gmail.com>
2025-04-01 22:24:14 +0200

    use Entry type

--- src/Umbraco.Web.UI.Client/src/packages/core/utils/state-manager/read-only-variant-state.manager.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/utils/state-manager/read-only-variant-state.manager.ts
@@ -1,9 +1,9 @@
-import type { UmbState } from './state.manager.js';
+import type { UmbStateEntry } from './state.manager.js';
 import { UmbReadOnlyStateManager } from './read-only-state.manager.js';
 import type { UmbVariantId } from '@umbraco-cms/backoffice/variant';
 
-export interface UmbVariantState extends UmbState {
-	variantId: UmbVariantId;
+export interface UmbVariantState extends UmbStateEntry {
+	variantId?: UmbVariantId;
 }
 
 export class UmbReadOnlyVariantStateManager extends UmbReadOnlyStateManager<UmbVariantState> {}
