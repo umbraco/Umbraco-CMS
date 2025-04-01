b2fb403d44f62b33989b59d67e53791ccc6ba3df
Niels Lyngsø <niels.lyngso@gmail.com>
2025-04-01 22:24:14 +0200

    use Entry type

--- src/Umbraco.Web.UI.Client/src/packages/core/utils/state-manager/read-only-state.manager.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/utils/state-manager/read-only-state.manager.ts
@@ -1,7 +1,7 @@
-import type { UmbState } from './state.manager.js';
+import type { UmbStateEntry } from './state.manager.js';
 import { UmbStateManager } from './state.manager.js';
 
-export class UmbReadOnlyStateManager<StateType extends UmbState> extends UmbStateManager<StateType> {
+export class UmbReadOnlyStateManager<StateType extends UmbStateEntry> extends UmbStateManager<StateType> {
 	readonly isReadOnly = this.isOn;
 
 	/**
