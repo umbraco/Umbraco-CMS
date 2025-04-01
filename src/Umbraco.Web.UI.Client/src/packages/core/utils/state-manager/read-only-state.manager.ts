ca4683e3e93ff2e39a4f0150ea6c0785c3b27d79
Niels Lyngsø <niels.lyngso@gmail.com>
2025-04-01 23:54:44 +0200

    get rid of things not yet released

--- src/Umbraco.Web.UI.Client/src/packages/core/utils/state-manager/read-only-state.manager.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/utils/state-manager/read-only-state.manager.ts
@@ -3,12 +3,4 @@ import { UmbStateManager } from './state.manager.js';
 
 export class UmbReadOnlyStateManager<StateType extends UmbStateEntry> extends UmbStateManager<StateType> {
 	readonly isReadOnly = this.isOn;
-
-	/**
-	 * Get the read only state
-	 * @returns {boolean} True if the state is read only
-	 */
-	getIsReadOnly(): boolean {
-		return this.getIsOn();
-	}
 }
