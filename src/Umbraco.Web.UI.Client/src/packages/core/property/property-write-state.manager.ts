c0f2afab8c3f2db46416934490ec3ab09fddaccf
Niels Lyngsø <niels.lyngso@gmail.com>
2025-04-01 23:55:11 +0200

    clean up

--- src/Umbraco.Web.UI.Client/src/packages/core/property/property-write-state.manager.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property/property-write-state.manager.ts
@@ -13,21 +13,6 @@ export class UmbPropertyWriteStateManager<
 		super(host);
 		// To avoid breaking changes in rendering this state is stopped by default. This means that properties are viewable by default.
 		// We start this state in workspaces where we want to control the writability of properties.
-		this.stop();
-	}
-
-	/**
-	 * Get the writable state
-	 * @returns {Observable<boolean>} True if the property is writable
-	 * @memberof UmbPropertyWriteStateManager
-	 */
-	public readonly isWritable = this.isOn;
-
-	/**
-	 * Get the property writable state
-	 * @returns {boolean} True if the property is writable
-	 */
-	public getIsWritable(): boolean {
-		return this.getIsOn();
+		this.fallbackToOn();
 	}
 }
