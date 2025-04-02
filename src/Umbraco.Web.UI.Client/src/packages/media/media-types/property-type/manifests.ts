bd910b86f45db5d45ecfec4819c38316f3e8756c
Mads Rasmussen <madsr@hey.com>
2025-04-02 12:00:02 +0200

    register media type property type ref

--- src/Umbraco.Web.UI.Client/src/packages/media/media-types/property-type/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media-types/property-type/manifests.ts
@@ -1,3 +1,4 @@
+import { UMB_MEDIA_TYPE_PROPERTY_TYPE_ENTITY_TYPE } from './entity.js';
 import { UMB_MANAGEMENT_API_DATA_SOURCE_ALIAS } from '@umbraco-cms/backoffice/repository';
 
 export const manifests: Array<UmbExtensionManifest> = [
@@ -8,5 +9,12 @@ export const manifests: Array<UmbExtensionManifest> = [
 		api: () => import('./media-type-property-type-reference-response.management-api.mapping.js'),
 		forDataSource: UMB_MANAGEMENT_API_DATA_SOURCE_ALIAS,
 		forDataModel: 'MediaTypePropertyReferenceResponseModel',
+	},
+	{
+		type: 'entityItemRef',
+		alias: 'Umb.EntityItemRef.MediaTypePropertyType',
+		name: 'Media Type Property Type Entity Item Reference',
+		element: () => import('./media-type-property-type-item-ref.element.js'),
+		forEntityTypes: [UMB_MEDIA_TYPE_PROPERTY_TYPE_ENTITY_TYPE],
 	},
 ];
