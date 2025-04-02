9743ca40f69bb8e67e9798fa253a2693c9733c71
Mads Rasmussen <madsr@hey.com>
2025-04-02 11:50:59 +0200

    register ref item

--- src/Umbraco.Web.UI.Client/src/packages/documents/document-types/property-type/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/document-types/property-type/manifests.ts
@@ -1,3 +1,4 @@
+import { UMB_DOCUMENT_TYPE_PROPERTY_TYPE_ENTITY_TYPE } from './entity.js';
 import { UMB_MANAGEMENT_API_DATA_SOURCE_ALIAS } from '@umbraco-cms/backoffice/repository';
 
 export const manifests: Array<UmbExtensionManifest> = [
@@ -8,5 +9,12 @@ export const manifests: Array<UmbExtensionManifest> = [
 		api: () => import('./document-type-property-type-reference-response.management-api.mapping.js'),
 		forDataSource: UMB_MANAGEMENT_API_DATA_SOURCE_ALIAS,
 		forDataModel: 'DocumentTypePropertyReferenceResponseModel',
+	},
+	{
+		type: 'entityItemRef',
+		alias: 'Umb.EntityItemRef.DocumentTypePropertyType',
+		name: 'Document Type Property Type Entity Item Reference',
+		element: () => import('./document-type-property-type-item-ref.element.js'),
+		forEntityTypes: [UMB_DOCUMENT_TYPE_PROPERTY_TYPE_ENTITY_TYPE],
 	},
 ];
