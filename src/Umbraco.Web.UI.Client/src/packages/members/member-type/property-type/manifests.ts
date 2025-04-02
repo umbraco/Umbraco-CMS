d6a0ef38333c2af04ad1a9b97c820bfb4e5c0102
Mads Rasmussen <madsr@hey.com>
2025-04-02 11:55:05 +0200

    register member type property type ref

--- src/Umbraco.Web.UI.Client/src/packages/members/member-type/property-type/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/members/member-type/property-type/manifests.ts
@@ -1,3 +1,4 @@
+import { UMB_MEMBER_TYPE_PROPERTY_TYPE_ENTITY_TYPE } from './entity.js';
 import { UMB_MANAGEMENT_API_DATA_SOURCE_ALIAS } from '@umbraco-cms/backoffice/repository';
 
 export const manifests: Array<UmbExtensionManifest> = [
@@ -8,5 +9,12 @@ export const manifests: Array<UmbExtensionManifest> = [
 		api: () => import('./member-type-property-type-reference-response.management-api.mapping.js'),
 		forDataSource: UMB_MANAGEMENT_API_DATA_SOURCE_ALIAS,
 		forDataModel: 'MemberTypePropertyReferenceResponseModel',
+	},
+	{
+		type: 'entityItemRef',
+		alias: 'Umb.EntityItemRef.MemberTypePropertyType',
+		name: 'Member Type Property Type Entity Item Reference',
+		element: () => import('./member-type-property-type-item-ref.element.js'),
+		forEntityTypes: [UMB_MEMBER_TYPE_PROPERTY_TYPE_ENTITY_TYPE],
 	},
 ];
