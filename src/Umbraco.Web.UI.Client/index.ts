14e8c1e9e66da81f699fdcf981d5653156148354
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-04-02 10:00:23 +0200

    feat: optimise load order

--- src/Umbraco.Web.UI.Client/index.ts
+++ src/Umbraco.Web.UI.Client/index.ts
@@ -1,4 +1,34 @@
 import { UmbAppElement } from '@umbraco-cms/backoffice/app';
+import { umbExtensionsRegistry } from '@umbraco-cms/backoffice/extension-registry';
 
 const appElement = new UmbAppElement();
-document.body.appendChild(appElement);
+appElement.backofficePath = '/';
+
+//#region Vite Mock Setup
+if (import.meta.env.VITE_UMBRACO_USE_MSW === 'on') {
+	const { startMockServiceWorker } = await import(/* @vite-ignore */ './src/mocks/index.js');
+	appElement.bypassAuth = true;
+	startMockServiceWorker();
+} else {
+	appElement.serverUrl = import.meta.env.VITE_UMBRACO_API_URL;
+}
+
+// Example injector:
+if (import.meta.env.VITE_EXAMPLE_PATH) {
+	import(/* @vite-ignore */ './' + import.meta.env.VITE_EXAMPLE_PATH + '/index.ts').then((js) => {
+		if (js) {
+			Object.keys(js).forEach((key) => {
+				const value = js[key];
+
+				if (Array.isArray(value)) {
+					umbExtensionsRegistry.registerMany(value);
+				} else if (typeof value === 'object') {
+					umbExtensionsRegistry.register(value);
+				}
+			});
+		}
+	});
+}
+//#endregion
+
+document.body.append(appElement);
