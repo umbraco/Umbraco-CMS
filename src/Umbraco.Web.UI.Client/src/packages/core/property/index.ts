e92ac20512bc54de79bae6bf2a85d9c59e12aeee
Niels Lyngsø <niels.lyngso@gmail.com>
2025-04-03 23:45:59 +0200

    remove property state managers

--- src/Umbraco.Web.UI.Client/src/packages/core/property/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property/index.ts
@@ -1,13 +1,7 @@
 export * from './components/index.js';
 export * from './conditions/index.js';
 export * from './property-dataset/index.js';
-export * from './property-read-only-state.manager.js';
 export * from './property-value-cloner/property-value-clone.controller.js';
 export * from './property-value-preset/index.js';
-export * from './property-view-state.manager.js';
-export * from './property-write-state.manager.js';
-export * from './variant-property-read-only-state.manager.js';
-export * from './variant-property-view-state.manager.js';
-export * from './variant-property-write-state.manager.js';
 
 export type * from './types.js';
