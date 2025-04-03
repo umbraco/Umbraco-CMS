8754caa06f3e276efba9259932cf5338f9f22e19
Lee Kelleher <leekelleher@users.noreply.github.com>
2025-04-03 17:30:21 +0100

    V16: Stylesheet Create Options (#18911)
    * Adds `entityCreateOptionAction` extensions for Stylesheets
    
    * Deprecates `umb-stylesheet-create-options-modal` and token.
    
    * Corrected localization labels
    
    to resolve failing acceptance test.
    
    * Updated @umbraco/playwright-testhelpers to 16.0.0
    
    * Removed redundant RTE Stylesheet acceptance tests

--- src/Umbraco.Web.UI.Client/src/packages/templating/stylesheets/entity-actions/create/create.action.ts
+++ src/Umbraco.Web.UI.Client/src/packages/templating/stylesheets/entity-actions/create/create.action.ts
@@ -2,6 +2,7 @@ import { UMB_STYLESHEET_CREATE_OPTIONS_MODAL } from './options-modal/stylesheet-
 import { UmbEntityActionBase } from '@umbraco-cms/backoffice/entity-action';
 import { umbOpenModal } from '@umbraco-cms/backoffice/modal';
 
+/** @deprecated No longer used internally. This will be removed in Umbraco 18. [LK] */
 export class UmbStylesheetCreateOptionsEntityAction extends UmbEntityActionBase<never> {
 	override async execute() {
 		await umbOpenModal(this, UMB_STYLESHEET_CREATE_OPTIONS_MODAL, {
