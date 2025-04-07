0b995c562a4938ff367a1adb36b3472d7e71f02c
Niels Lyngsø <niels.lyngso@gmail.com>
2025-04-07 11:38:56 +0200

    fix tests

--- src/Umbraco.Web.UI.Client/src/packages/core/property/property-guard-manager/variant-property-guard.manager.test.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property/property-guard-manager/variant-property-guard.manager.test.ts
@@ -15,43 +15,43 @@ describe('UmbVariantPropertyGuardManager', () => {
 	const propA = { unique: 'propA' };
 	const propB = { unique: 'propB' };
 
-	const ruleInv = { unique: '1', message: 'State 1', state: true, variantId: invariantVariant };
-	const ruleEn = { unique: '2', message: 'State 2', state: true, variantId: englishVariant };
-	const rulePlain = { unique: '3', message: 'State 3', state: true };
-	const ruleNoInv = { unique: '01', message: 'State 01', state: false, variantId: invariantVariant };
-	const ruleNoEn = { unique: '02', message: 'State 02', state: false, variantId: englishVariant };
-	const ruleNoPlain = { unique: '03', message: 'State 03', state: false };
+	const ruleInv = { unique: '1', message: 'State 1', permitted: true, variantId: invariantVariant };
+	const ruleEn = { unique: '2', message: 'State 2', permitted: true, variantId: englishVariant };
+	const rulePlain = { unique: '3', message: 'State 3', permitted: true };
+	const ruleNoInv = { unique: '01', message: 'State 01', permitted: false, variantId: invariantVariant };
+	const ruleNoEn = { unique: '02', message: 'State 02', permitted: false, variantId: englishVariant };
+	const ruleNoPlain = { unique: '03', message: 'State 03', permitted: false };
 
 	const statePropAInv = {
 		unique: 'a1',
 		message: 'State 1',
-		state: true,
+		permitted: true,
 		variantId: invariantVariant,
 		propertyType: propA,
 	};
 	const statePropBEn = {
 		unique: 'b2',
 		message: 'State 2',
-		state: true,
+		permitted: true,
 		variantId: englishVariant,
 		propertyType: propB,
 	};
-	const statePropAPlain = { unique: 'a3', message: 'State 3', state: true, propertyType: propA };
+	const statePropAPlain = { unique: 'a3', message: 'State 3', permitted: true, propertyType: propA };
 	const stateNoPropAInv = {
 		unique: 'a01',
 		message: 'State 01',
-		state: false,
+		permitted: false,
 		variantId: invariantVariant,
 		propertyType: propA,
 	};
 	const stateNoPropBEn = {
 		unique: 'b02',
 		message: 'State 02',
-		state: false,
+		permitted: false,
 		variantId: englishVariant,
 		propertyType: propB,
 	};
-	const stateNoPropAPlain = { unique: 'a03', message: 'State 03', state: false, propertyType: propA };
+	const stateNoPropAPlain = { unique: 'a03', message: 'State 03', permitted: false, propertyType: propA };
 
 	beforeEach(() => {
 		const hostElement = new UmbTestControllerHostElement();
