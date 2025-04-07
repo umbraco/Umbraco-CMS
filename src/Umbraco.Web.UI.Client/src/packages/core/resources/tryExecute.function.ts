6862fd0100a8af1cdb3bd2bee12d26476534cadb
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-04-07 10:59:34 +0200

    docs: adds jsdocs

--- src/Umbraco.Web.UI.Client/src/packages/core/resources/tryExecute.function.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/resources/tryExecute.function.ts
@@ -3,6 +3,22 @@ import type { UmbTryExecuteOptions } from './types.js';
 import type { UmbDataSourceResponse } from '@umbraco-cms/backoffice/repository';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 
+/**
+ * Make a request and handle errors.
+ * @param {UmbControllerHost} host The host to use for the request and where notifications will be shown.
+ * @param {Promise<T>} promise The promise to execute.
+ * @param {UmbTryExecuteOptions} opts Options for the request.
+ * @returns {Promise<UmbDataSourceResponse<T>>} A promise that resolves with the response data or rejects with an error.
+ * @template T The type of the response data.
+ * @example
+ * const { data, error } = await tryExecute(this, myPromise, {
+ *   abortSignal: myAbortSignal,
+ *   disableNotifications: false,
+ * });
+ * if (!error) {
+ *	 console.log('Success:', data);
+ * }
+ */
 export async function tryExecute<T>(
 	host: UmbControllerHost,
 	promise: Promise<T>,
