015b443468bc889e65ecb0e9d13e4fbef3624412
Niels Lyngsø <niels.lyngso@gmail.com>
2025-04-07 16:38:53 +0200

    impl view guard property filtering

--- src/Umbraco.Web.UI.Client/src/packages/core/content/workspace/views/edit/content-editor-properties.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/content/workspace/views/edit/content-editor-properties.element.ts
@@ -7,14 +7,17 @@ import type {
 } from '@umbraco-cms/backoffice/content-type';
 import { UmbContentTypePropertyStructureHelper } from '@umbraco-cms/backoffice/content-type';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
-import { UMB_PROPERTY_STRUCTURE_WORKSPACE_CONTEXT } from '@umbraco-cms/backoffice/workspace';
 import { UmbVariantId } from '@umbraco-cms/backoffice/variant';
 import { UMB_PROPERTY_DATASET_CONTEXT } from '@umbraco-cms/backoffice/property';
 import './content-editor-property.element.js';
+import { UMB_CONTENT_WORKSPACE_CONTEXT } from '@umbraco-cms/backoffice/content';
 
 @customElement('umb-content-workspace-view-edit-properties')
 export class UmbContentWorkspaceViewEditPropertiesElement extends UmbLitElement {
+	#workspaceContext?: typeof UMB_CONTENT_WORKSPACE_CONTEXT.TYPE;
 	#propertyStructureHelper = new UmbContentTypePropertyStructureHelper<UmbContentTypeModel>(this);
+	#properties?: Array<UmbPropertyTypeModel>;
+	#visiblePropertiesUniques: Array<string> = [];
 
 	@property({ type: String, attribute: 'container-id', reflect: false })
 	public get containerId(): string | null | undefined {
@@ -28,35 +31,69 @@ export class UmbContentWorkspaceViewEditPropertiesElement extends UmbLitElement 
 	_variantId?: UmbVariantId;
 
 	@state()
-	_propertyStructure?: Array<UmbPropertyTypeModel>;
+	_visibleProperties?: Array<UmbPropertyTypeModel>;
 
 	constructor() {
 		super();
 
 		this.consumeContext(UMB_PROPERTY_DATASET_CONTEXT, (datasetContext) => {
 			this._variantId = datasetContext.getVariantId();
+			this.#processPropertyStructure();
 		});
 
-		this.consumeContext(UMB_PROPERTY_STRUCTURE_WORKSPACE_CONTEXT, (workspaceContext) => {
+		this.consumeContext(UMB_CONTENT_WORKSPACE_CONTEXT, (workspaceContext) => {
+			this.#workspaceContext = workspaceContext;
+
 			this.#propertyStructureHelper.setStructureManager(
 				// Assuming its the same content model type that we are working with here... [NL]
 				workspaceContext.structure as unknown as UmbContentTypeStructureManager<UmbContentTypeModel>,
 			);
-		});
 
-		this.observe(
-			this.#propertyStructureHelper.propertyStructure,
-			(propertyStructure) => {
-				this._propertyStructure = propertyStructure;
-			},
-			null,
+			this.observe(
+				this.#propertyStructureHelper.propertyStructure,
+				(properties) => {
+					this.#properties = properties;
+					this.#processPropertyStructure();
+				},
+				'observePropertyStructure',
+			);
+		});
+	}
+
+	#processPropertyStructure() {
+		if (!this.#workspaceContext || !this.#properties || !this.#propertyStructureHelper) {
+			return;
+		}
+
+		const propertyViewGuard = this.#workspaceContext.propertyViewGuard;
+
+		this.#properties.forEach((property) => {
+			const propertyVariantId = new UmbVariantId(this._variantId?.culture, this._variantId?.segment);
+			this.observe(propertyViewGuard.permittedForVariantAndProperty(propertyVariantId, property), (permitted) => {
+				if (permitted) {
+					this.#visiblePropertiesUniques.push(property.unique);
+					this.#calculateVisaibleProperties();
+				} else {
+					const index = this.#visiblePropertiesUniques.indexOf(property.unique);
+					if (index !== -1) {
+						this.#visiblePropertiesUniques.splice(index, 1);
+						this.#calculateVisaibleProperties();
+					}
+				}
+			});
+		});
+	}
+
+	#calculateVisaibleProperties() {
+		this._visibleProperties = this.#properties!.filter((property) =>
+			this.#visiblePropertiesUniques.includes(property.unique),
 		);
 	}
 
 	override render() {
-		return this._variantId && this._propertyStructure
+		return this._variantId && this._visibleProperties
 			? repeat(
-					this._propertyStructure,
+					this._visibleProperties,
 					(property) => property.alias,
 					(property) =>
 						html`<umb-content-workspace-view-edit-property
