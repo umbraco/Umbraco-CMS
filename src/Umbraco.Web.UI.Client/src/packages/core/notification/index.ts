50a228ab5241f805e4f64fe8efea189139050377
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-04-08 08:43:55 +0200

    fix: resolves an unintended circular reference based on files being moved around
    introduced in #18939

--- src/Umbraco.Web.UI.Client/src/packages/core/notification/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/notification/index.ts
@@ -1,7 +1,5 @@
 import './layouts/default/index.js';
 export * from './controllers/peek-error/index.js';
-export * from '../resources/extractUmbNotificationColor.function.js';
-export * from './isUmbNotifications.function.js';
 export * from './notification-handler.js';
 export * from './notification.context.js';
 
