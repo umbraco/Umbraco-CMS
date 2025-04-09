ae91eee05f1180ae083f2c231cba3f21f2ec9ec8
Mads Rasmussen <madsr@hey.com>
2025-04-09 11:08:28 +0200

    Document permission inheritance in UI (#18935)
    * check the full path for permissions
    
    * fix race condition
    
    * wip update permission when variants change
    
    * Populate ancestor keys on document tree response items.
    
    * Populate ancestor keys on document collection response items.
    
    * Update OpenApi.json
    
    * generate server models
    
    * update types
    
    * map data
    
    * add ancestor context
    
    * set ancestors in context
    
    * use ancestor context in tree
    
    * clean up
    
    * provide ancestor context from a collection item
    
    * provide ancestor context from structure context
    
    * Use array of objects rather than Ids for the ancestor collection.
    
    * Update OpenApi.json.
    
    * add ancestor data to mocks
    
    * set ancestors ids in mocks
    
    * omit ancestors for recycle bin item
    
    * use correct models for document blueprint mock data
    
    * remove constructor
    
    * mock documents for testing
    
    * add user group permission test data
    
    * wip document user permission condition tests
    
    * generate new server models
    
    * update data efter server models update
    
    * clean up
    
    * Update entity-actions-table-column-view.element.ts
    
    * longer time for not found to appear
    
    * use arg
    
    * observe alias
    
    * set new the right place
    
    * remove const
    
    ---------
    
    Co-authored-by: Andy Butland <abutland73@gmail.com>
    Co-authored-by: Niels Lyngsø <nsl@umbraco.dk>
    Co-authored-by: Niels Lyngsø <niels.lyngso@gmail.com>

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/tree/tree-item/document-tree-item.context.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/tree/tree-item/document-tree-item.context.ts
@@ -3,6 +3,7 @@ import { UmbDocumentItemDataResolver } from '../../item/index.js';
 import { UmbDefaultTreeItemContext } from '@umbraco-cms/backoffice/tree';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import { UmbIsTrashedEntityContext } from '@umbraco-cms/backoffice/recycle-bin';
+import { UmbAncestorsEntityContext } from '@umbraco-cms/backoffice/entity';
 
 export class UmbDocumentTreeItemContext extends UmbDefaultTreeItemContext<
 	UmbDocumentTreeItemModel,
@@ -10,12 +11,14 @@ export class UmbDocumentTreeItemContext extends UmbDefaultTreeItemContext<
 > {
 	// TODO: Provide this together with the EntityContext, ideally this takes part via a extension-type [NL]
 	#isTrashedContext = new UmbIsTrashedEntityContext(this);
+	#ancestorsContext = new UmbAncestorsEntityContext(this);
 	#item = new UmbDocumentItemDataResolver(this);
 
 	readonly name = this.#item.name;
 	readonly icon = this.#item.icon;
 	readonly isDraft = this.#item.isDraft;
 
+	readonly ancestors = this._treeItem.asObservablePart((item) => item?.ancestors ?? []);
 	readonly isTrashed = this._treeItem.asObservablePart((item) => item?.isTrashed ?? false);
 
 	constructor(host: UmbControllerHost) {
@@ -23,6 +26,10 @@ export class UmbDocumentTreeItemContext extends UmbDefaultTreeItemContext<
 
 		this.observe(this.isTrashed, (isTrashed) => {
 			this.#isTrashedContext.setIsTrashed(isTrashed);
+		});
+
+		this.observe(this.ancestors, (ancestors) => {
+			this.#ancestorsContext.setAncestors(ancestors);
 		});
 	}
 
