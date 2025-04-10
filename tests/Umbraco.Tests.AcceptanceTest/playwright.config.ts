b96aca8db5d6e117fe8c2ee6af3a1e9f805cb84a
Nhu Dinh <hnd@umbraco.dk>
2025-04-10 16:49:19 +0700

    Increase global timeout as the block tests need more time to run

--- tests/Umbraco.Tests.AcceptanceTest/playwright.config.ts
+++ tests/Umbraco.Tests.AcceptanceTest/playwright.config.ts
@@ -8,13 +8,13 @@ export const STORAGE_STATE = path.join(__dirname, 'playwright/.auth/user.json');
 export default defineConfig({
   testDir: './tests/',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 70 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
      * For example in `await expect(locator).toHaveText();`
      */
-    timeout: 5000
+    timeout: 3000
   },
   /* Fail the build on CI if you accidentally left test.only in the source code. */
   forbidOnly: !!process.env.CI,
