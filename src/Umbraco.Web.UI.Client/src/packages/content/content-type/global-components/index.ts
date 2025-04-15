e6ab1d9a6be5aaf0c2e7979dc2439c044d6540f4
Mads Rasmussen <madsr@hey.com>
2025-04-15 11:27:06 +0200

    Move + Rename input-collection-configuration element to input-content-type-collection-configuration (#19041)
    * move to content-type module
    
    * make name more explicit
    
    * add total to text
    
    * update threshold
    
    * remove completed todo

--- src/Umbraco.Web.UI.Client/src/packages/content/content-type/global-components/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/content/content-type/global-components/index.ts
@@ -1,3 +1,5 @@
 import './content-type-workspace-editor-header.element.js';
+import './input-content-type-collection-configuration/input-content-type-collection-configuration.element.js';
 
 export * from './content-type-workspace-editor-header.element.js';
+export * from './input-content-type-collection-configuration/input-content-type-collection-configuration.element.js';
