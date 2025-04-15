f493ffd360a9417f3ecd6682c9d885298ffc7949
Mads Rasmussen <madsr@hey.com>
2025-04-15 09:30:18 +0200

    update threshold

--- src/Umbraco.Web.UI.Client/devops/module-dependencies/index.js
+++ src/Umbraco.Web.UI.Client/devops/module-dependencies/index.js
@@ -2,7 +2,7 @@ import fs from 'fs';
 import path from 'path';
 import { createImportMap } from '../importmap/index.js';
 
-const ILLEGAL_CORE_IMPORTS_THRESHOLD = 7;
+const ILLEGAL_CORE_IMPORTS_THRESHOLD = 6;
 const SELF_IMPORTS_THRESHOLD = 11;
 
 const clientProjectRoot = path.resolve(import.meta.dirname, '../../');
