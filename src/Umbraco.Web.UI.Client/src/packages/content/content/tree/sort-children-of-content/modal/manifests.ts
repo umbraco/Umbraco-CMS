d49df3f58bea82b77726be7f632531cbd6674e6e
Mads Rasmussen <madsr@hey.com>
2025-04-15 12:38:02 +0200

    Update manifests.ts

--- src/Umbraco.Web.UI.Client/src/packages/content/content/tree/sort-children-of-content/modal/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/content/content/tree/sort-children-of-content/modal/manifests.ts
@@ -1,8 +1,11 @@
-export const manifests: Array<UmbExtensionManifest> = [
+import { UMB_SORT_CHILDREN_OF_CONTENT_MODAL_ALIAS } from './constants.js';
+import type { ManifestModal } from '@umbraco-cms/backoffice/modal';
+
+export const manifests: Array<ManifestModal> = [
 	{
 		type: 'modal',
-		alias: 'Umb.Modal.TrashWithRelation',
-		name: 'Trash With Relation Modal',
-		element: () => import('./trash-with-relation-modal.element.js'),
+		alias: UMB_SORT_CHILDREN_OF_CONTENT_MODAL_ALIAS,
+		name: 'Sort Children Of Content Modal',
+		element: () => import('./sort-children-of-modal.element.js'),
 	},
 ];
