b1795da0eee6a07ccae3267e1687b711e0603b5c
Lee Kelleher <leekelleher@users.noreply.github.com>
2025-04-17 08:57:19 +0100

    Adds `searchResultItem` element for Media items (#19058)
    * Adds `searchResultItem` for Media
    
    Includes "trashed" state tag
    
    * Removes trailing whitespace from manifest name
    
    ---------
    
    Co-authored-by: Andy Butland <abutland73@gmail.com>

--- src/Umbraco.Web.UI.Client/src/packages/media/media/search/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/search/manifests.ts
@@ -16,6 +16,7 @@ export const manifests: Array<UmbExtensionManifest> = [
 		name: 'Media Search Result Item',
 		alias: 'Umb.SearchResultItem.Media',
 		type: 'searchResultItem',
+		element: () => import('./media-search-result-item.element.js'),
 		forEntityTypes: [UMB_MEDIA_ENTITY_TYPE],
 	},
 ];
