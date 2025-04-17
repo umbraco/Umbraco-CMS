68e19866f0ffcb9d53c949fdc75e0b5dd355b044
Andy Butland <abutland73@gmail.com>
2025-04-17 09:35:23 +0200

    Added unit test for LoggingConfiguration.

---
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Logging/LoggingConfigurationTests.cs
@@ -0,0 +1,22 @@
+using NUnit.Framework;
+using Umbraco.Cms.Core.Logging;
+
+namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Logging;
+
+[TestFixture]
+public class LoggingConfigurationTests
+{
+    [Test]
+    public void Can_Get_Supported_Log_File_Name_Format_Arguments()
+    {
+        var config = new LoggingConfiguration("c:\\logs\\", "UmbracoLogFile_{0}_{1}..json", "MachineName,EnvironmentName");
+        var result = config.GetLogFileNameFormatArguments();
+
+        Assert.AreEqual(2, result.Length);
+
+        var expectedMachineName = Environment.MachineName;
+        var expectedEnvironmentName = Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT");
+        Assert.AreEqual(expectedMachineName, result[0]);
+        Assert.AreEqual(expectedEnvironmentName, result[1]);
+    }
+}
