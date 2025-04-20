e99d970f7c237b2dfbad282906d8611c63d5c562
kjac <kja@umbraco.dk>
2025-04-20 21:05:14 +0200

    Segment support for the Delivery API

---
+++ src/Umbraco.Cms.Api.Delivery/Filters/ContextualizeFromAcceptHeadersAttribute.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+using Umbraco.Cms.Core.DeliveryApi;
+using Umbraco.Cms.Core.Models.PublishedContent;
+using Umbraco.Extensions;
+
+namespace Umbraco.Cms.Api.Delivery.Filters;
+
+internal sealed class ContextualizeFromAcceptHeadersAttribute : TypeFilterAttribute
+{
+    public ContextualizeFromAcceptHeadersAttribute()
+        : base(typeof(LocalizeFromAcceptLanguageHeaderAttributeFilter))
+    {
+    }
+
+    private class LocalizeFromAcceptLanguageHeaderAttributeFilter : IActionFilter
+    {
+        private readonly IRequestCultureService _requestCultureService;
+        private readonly IRequestSegmmentService _requestSegmentService;
+        private readonly IVariationContextAccessor _variationContextAccessor;
+
+        public LocalizeFromAcceptLanguageHeaderAttributeFilter(
+            IRequestCultureService requestCultureService,
+            IRequestSegmmentService requestSegmentService,
+            IVariationContextAccessor variationContextAccessor)
+        {
+            _requestCultureService = requestCultureService;
+            _requestSegmentService = requestSegmentService;
+            _variationContextAccessor = variationContextAccessor;
+        }
+
+        public void OnActionExecuting(ActionExecutingContext context)
+        {
+            var requestedCulture = _requestCultureService.GetRequestedCulture();
+            var requestedSegment = _requestSegmentService.GetRequestedSegment();
+            if (requestedCulture.IsNullOrWhiteSpace() && requestedSegment.IsNullOrWhiteSpace())
+            {
+                return;
+            }
+
+            // contextualize the request
+            // NOTE: request culture or segment can be null here (but not both), so make sure to retain any existing
+            //       context by means of fallback to current variation context (if available)
+            _variationContextAccessor.VariationContext = new VariationContext(
+                requestedCulture ?? _variationContextAccessor.VariationContext?.Culture,
+                requestedSegment ?? _variationContextAccessor.VariationContext?.Segment);
+        }
+
+        public void OnActionExecuted(ActionExecutedContext context)
+        {
+        }
+    }
+}
--- src/Umbraco.Cms.Api.Delivery/Services/RequestRoutingService.cs
+++ src/Umbraco.Cms.Api.Delivery/Services/RequestRoutingService.cs
@@ -1,6 +1,5 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Umbraco.Cms.Core.DeliveryApi;
-using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Models.PublishedContent;
 using Umbraco.Cms.Core.PublishedCache;
 using Umbraco.Cms.Core.Routing;
@@ -11,14 +10,19 @@ namespace Umbraco.Cms.Api.Delivery.Services;
 internal sealed class RequestRoutingService : RoutingServiceBase, IRequestRoutingService
 {
     private readonly IRequestCultureService _requestCultureService;
+    private readonly IVariationContextAccessor _variationContextAccessor;
 
     public RequestRoutingService(
         IDomainCache domainCache,
         IHttpContextAccessor httpContextAccessor,
         IRequestStartItemProviderAccessor requestStartItemProviderAccessor,
-        IRequestCultureService requestCultureService)
-        : base(domainCache, httpContextAccessor, requestStartItemProviderAccessor) =>
+        IRequestCultureService requestCultureService,
+        IVariationContextAccessor variationContextAccessor)
+        : base(domainCache, httpContextAccessor, requestStartItemProviderAccessor)
+    {
         _requestCultureService = requestCultureService;
+        _variationContextAccessor = variationContextAccessor;
+    }
 
     /// <inheritdoc />
     public string GetContentRoute(string requestedPath)
@@ -50,9 +54,14 @@ internal sealed class RequestRoutingService : RoutingServiceBase, IRequestRoutin
         }
 
         // the Accept-Language header takes precedence over configured domain culture
-        if (domainAndUri.Culture != null && _requestCultureService.GetRequestedCulture().IsNullOrWhiteSpace())
+        if (domainAndUri.Culture != null
+            && _requestCultureService.GetRequestedCulture().IsNullOrWhiteSpace()
+            && _variationContextAccessor.VariationContext?.Culture != domainAndUri.Culture)
         {
-            _requestCultureService.SetRequestCulture(domainAndUri.Culture);
+            // update the variation context to match the requested domain culture while retaining any contextualized segment
+            _variationContextAccessor.VariationContext = new VariationContext(
+                culture: domainAndUri.Culture,
+                segment: _variationContextAccessor.VariationContext?.Segment);
         }
 
         // when resolving content from a configured domain, the content cache expects the content route
