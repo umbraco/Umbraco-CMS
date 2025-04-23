ac81973e3b6b87b72e4e6ab92d507fe90600e784
Niels Lyngsø <niels.lyngso@gmail.com>
2025-04-23 10:26:36 +0200

    use UMB_ENTITY_WORKSPACE_CONTEXT for right typing

--- src/Umbraco.Web.UI.Client/src/packages/core/menu/menu-tree-structure-workspace-context-base.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/menu/menu-tree-structure-workspace-context-base.ts
@@ -2,7 +2,7 @@ import type { UmbStructureItemModel } from './types.js';
 import type { UmbTreeRepository, UmbTreeItemModel, UmbTreeRootModel } from '@umbraco-cms/backoffice/tree';
 import { createExtensionApiByAlias } from '@umbraco-cms/backoffice/extension-registry';
 import { UmbContextBase } from '@umbraco-cms/backoffice/class-api';
-import { UMB_WORKSPACE_CONTEXT } from '@umbraco-cms/backoffice/workspace';
+import { UMB_ENTITY_WORKSPACE_CONTEXT } from '@umbraco-cms/backoffice/workspace';
 import { UmbArrayState, UmbObjectState } from '@umbraco-cms/backoffice/observable-api';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 
@@ -11,7 +11,7 @@ interface UmbMenuTreeStructureWorkspaceContextBaseArgs {
 }
 
 export abstract class UmbMenuTreeStructureWorkspaceContextBase extends UmbContextBase {
-	#workspaceContext?: any;
+	#workspaceContext?: typeof UMB_ENTITY_WORKSPACE_CONTEXT.TYPE;
 	#args: UmbMenuTreeStructureWorkspaceContextBaseArgs;
 
 	#structure = new UmbArrayState<UmbStructureItemModel>([], (x) => x.unique);
@@ -25,11 +25,9 @@ export abstract class UmbMenuTreeStructureWorkspaceContextBase extends UmbContex
 		super(host, 'UmbMenuStructureWorkspaceContext');
 		this.#args = args;
 
-		this.consumeContext(UMB_WORKSPACE_CONTEXT, (instance) => {
+		this.consumeContext(UMB_ENTITY_WORKSPACE_CONTEXT, (instance) => {
 			this.#workspaceContext = instance;
-			// eslint-disable-next-line @typescript-eslint/ban-ts-comment
-			// @ts-ignore
-			this.#workspaceContext.observe(this.#workspaceContext.unique, (value) => {
+			this.observe(this.#workspaceContext?.unique, (value) => {
 				if (!value) return;
 				this.#requestStructure();
 			});
