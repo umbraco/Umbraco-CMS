5639b2f15f83236d3a0c4c873447d64a3dd6af36
kjac <kja@umbraco.dk>
2025-04-23 14:20:56 +0200

    Reintroduce the tests ignored by #19060

--- tests/Umbraco.Tests.Integration/Umbraco.Infrastructure/Services/ContentEditingServiceTests.Update.cs
+++ tests/Umbraco.Tests.Integration/Umbraco.Infrastructure/Services/ContentEditingServiceTests.Update.cs
@@ -85,7 +85,6 @@ public partial class ContentEditingServiceTests
     }
 
     [Test]
-    [Ignore("Validation for segment-only variants will be fixed in a follow-up PR.")]
     public async Task Can_Update_Segment_Variant()
     {
         var content = await CreateSegmentVariantContent();
@@ -391,7 +390,6 @@ public partial class ContentEditingServiceTests
     }
 
     [Test]
-    [Ignore("Validation for segment-only variants will be fixed in a follow-up PR.")]
     public async Task Cannot_Update_With_Variant_Property_Value_For_Invariant_Content()
     {
         var content = await CreateInvariantContent();
