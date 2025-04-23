a3848077b4fd7d1099fb37944a15cce589d15f26
Niels Lyngsø <niels.lyngso@gmail.com>
2025-04-23 14:37:14 +0200

    fix context consumption

--- src/Umbraco.Web.UI.Client/examples/custom-validation-workspace-context/custom-validation-workspace-context.ts
+++ src/Umbraco.Web.UI.Client/examples/custom-validation-workspace-context/custom-validation-workspace-context.ts
@@ -8,10 +8,7 @@ import { UmbVariantId } from '@umbraco-cms/backoffice/variant';
 const EXAMPLE_CUSTOM_VALIDATION_PROPERTY_ALIAS = 'tiptap';
 
 // The Example Workspace Context Controller:
-export class CustomValidationWorkspaceContext extends UmbContextBase<
-	CustomValidationWorkspaceContext,
-	typeof EXAMPLE_CUSTOM_VALIDATION_CONTEXT
-> {
+export class CustomValidationWorkspaceContext extends UmbContextBase {
 	#validators?: Array<CustomValidationValidator>;
 
 	constructor(host: UmbControllerHost) {
@@ -30,12 +27,12 @@ export class CustomValidationWorkspaceContext extends UmbContextBase<
 					if (propertyType.variesByCulture) {
 						// Because this property exists in multiple cultures we should observe cultures an create a custom validator for each culture value:
 						this.observe(
-							context.variantOptions,
+							context?.variantOptions,
 							(variantOptions) => {
 								// clean up old validators:
 								this.#validators?.forEach((x) => x.destroy());
 
-								this.#validators = variantOptions.map((option) => {
+								this.#validators = variantOptions?.map((option) => {
 									return new CustomValidationValidator(
 										this,
 										EXAMPLE_CUSTOM_VALIDATION_PROPERTY_ALIAS,
