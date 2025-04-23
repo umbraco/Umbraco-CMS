ba33de49e774fac8f4ebc7069e31ccc70d750c84
Niels Lyngsø <niels.lyngso@gmail.com>
2025-04-23 10:29:25 +0200

    correct type to use UMB_SUBMITTABLE_WORKSPACE_CONTEXT

--- src/Umbraco.Web.UI.Client/src/packages/core/menu/menu-tree-structure-workspace-context-base.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/menu/menu-tree-structure-workspace-context-base.ts
@@ -2,7 +2,7 @@ import type { UmbStructureItemModel } from './types.js';
 import type { UmbTreeRepository, UmbTreeItemModel, UmbTreeRootModel } from '@umbraco-cms/backoffice/tree';
 import { createExtensionApiByAlias } from '@umbraco-cms/backoffice/extension-registry';
 import { UmbContextBase } from '@umbraco-cms/backoffice/class-api';
-import { UMB_ENTITY_WORKSPACE_CONTEXT } from '@umbraco-cms/backoffice/workspace';
+import { UMB_SUBMITTABLE_WORKSPACE_CONTEXT } from '@umbraco-cms/backoffice/workspace';
 import { UmbArrayState, UmbObjectState } from '@umbraco-cms/backoffice/observable-api';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 
@@ -11,7 +11,7 @@ interface UmbMenuTreeStructureWorkspaceContextBaseArgs {
 }
 
 export abstract class UmbMenuTreeStructureWorkspaceContextBase extends UmbContextBase {
-	#workspaceContext?: typeof UMB_ENTITY_WORKSPACE_CONTEXT.TYPE;
+	#workspaceContext?: typeof UMB_SUBMITTABLE_WORKSPACE_CONTEXT.TYPE;
 	#args: UmbMenuTreeStructureWorkspaceContextBaseArgs;
 
 	#structure = new UmbArrayState<UmbStructureItemModel>([], (x) => x.unique);
@@ -25,7 +25,8 @@ export abstract class UmbMenuTreeStructureWorkspaceContextBase extends UmbContex
 		super(host, 'UmbMenuStructureWorkspaceContext');
 		this.#args = args;
 
-		this.consumeContext(UMB_ENTITY_WORKSPACE_CONTEXT, (instance) => {
+		// TODO: set up context token that supports parentEntityType, parentUnique, entityType.
+		this.consumeContext(UMB_SUBMITTABLE_WORKSPACE_CONTEXT, (instance) => {
 			this.#workspaceContext = instance;
 			this.observe(this.#workspaceContext?.unique, (value) => {
 				if (!value) return;
@@ -57,13 +58,15 @@ export abstract class UmbMenuTreeStructureWorkspaceContextBase extends UmbContex
 
 		const isNew = this.#workspaceContext?.getIsNew();
 
-		const entityTypeObservable = isNew ? this.#workspaceContext?.parentEntityType : this.#workspaceContext?.entityType;
+		const entityTypeObservable = isNew
+			? (this.#workspaceContext as any)?.parentEntityType
+			: (this.#workspaceContext as any).entityType;
 		const entityType = (await this.observe(entityTypeObservable, () => {})?.asPromise()) as string;
 		if (!entityType) throw new Error('Entity type is not available');
 
 		// If the entity type is different from the root entity type, then we can request the ancestors.
 		if (entityType !== root?.entityType) {
-			const uniqueObservable = isNew ? this.#workspaceContext?.parentUnique : this.#workspaceContext?.unique;
+			const uniqueObservable = isNew ? (this.#workspaceContext as any)?.parentUnique : this.#workspaceContext?.unique;
 			const unique = (await this.observe(uniqueObservable, () => {})?.asPromise()) as string;
 			if (!unique) throw new Error('Unique is not available');
 
