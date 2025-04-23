505f9d55b29c8c325bbd72166bb6ac81318ed605
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-04-23 15:17:24 +0200

    chore(mock): adds mock endpoints for 'publish with descendants'

--- src/Umbraco.Web.UI.Client/src/mocks/handlers/document/publishing.handlers.ts
+++ src/Umbraco.Web.UI.Client/src/mocks/handlers/document/publishing.handlers.ts
@@ -3,9 +3,12 @@ import { createProblemDetails } from '../../data/utils.js';
 import { umbDocumentMockDb } from '../../data/document/document.db.js';
 import { UMB_SLUG } from './slug.js';
 import type {
+	PublishWithDescendantsResultModel,
 	GetDocumentByIdPublishedResponse,
 	PublishDocumentRequestModel,
+	PublishDocumentWithDescendantsRequestModel,
 	UnpublishDocumentRequestModel,
+	GetDocumentByIdPublishWithDescendantsResultByTaskIdResponse,
 } from '@umbraco-cms/backoffice/external/backend-api';
 import { umbracoPath } from '@umbraco-cms/backoffice/utils';
 
@@ -24,6 +27,45 @@ export const publishingHandlers = [
 				return res(ctx.status(400), ctx.json(createProblemDetails({ title: 'Schedule', detail: error.message })));
 			}
 			throw new Error('An error occurred while publishing the document');
+		}
+	}),
+
+	rest.put(umbracoPath(`${UMB_SLUG}/:id/publish-with-descendants`), async (req, res, ctx) => {
+		const id = req.params.id as string;
+		if (!id) return res(ctx.status(400));
+		const requestBody = (await req.json()) as PublishDocumentWithDescendantsRequestModel;
+		if (!requestBody) return res(ctx.status(400, 'no body found'));
+
+		try {
+			const taskId = umbDocumentMockDb.publishing.publishWithDescendants(id, requestBody);
+			return res(ctx.status(200), ctx.json<PublishWithDescendantsResultModel>({ taskId, isComplete: false }));
+		} catch (error) {
+			if (error instanceof Error) {
+				return res(ctx.status(400), ctx.json(createProblemDetails({ title: 'Schedule', detail: error.message })));
+			}
+			throw new Error('An error occurred while publishing the document');
+		}
+	}),
+
+	rest.get(umbracoPath(`${UMB_SLUG}/:id/publish-with-descendants/result/:taskId`), async (req, res, ctx) => {
+		const id = req.params.id as string;
+		if (!id) return res(ctx.status(400));
+		const taskId = req.params.taskId as string;
+		if (!taskId) return res(ctx.status(400));
+
+		try {
+			const isComplete = umbDocumentMockDb.publishing.taskResult(taskId);
+			return res(
+				ctx.status(200),
+				ctx.json<GetDocumentByIdPublishWithDescendantsResultByTaskIdResponse>({ taskId, isComplete }),
+			);
+		} catch (error) {
+			return res(
+				ctx.status(400),
+				ctx.json(
+					createProblemDetails({ title: 'Task', detail: error instanceof Error ? error.message : 'An error occurred' }),
+				),
+			);
 		}
 	}),
 
