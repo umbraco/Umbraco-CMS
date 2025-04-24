6490c53fd8750e94c62a32285bbd86e1ace7a201
kjac <kja@umbraco.dk>
2025-04-24 11:21:40 +0200

    Retrofit ContentEditingBaseBuilder to match new create/update models

--- tests/Umbraco.Tests.Common/Builders/ContentEditingBaseBuilder.cs
+++ tests/Umbraco.Tests.Common/Builders/ContentEditingBaseBuilder.cs
@@ -6,7 +6,6 @@ using Umbraco.Extensions;
 namespace Umbraco.Cms.Tests.Common.Builders;
 
 public abstract class ContentEditingBaseBuilder<TCreateModel> : BuilderBase<TCreateModel>,
-    IWithInvariantNameBuilder,
     IWithKeyBuilder,
     IWithContentTypeKeyBuilder,
     IWithParentKeyBuilder,
@@ -14,23 +13,16 @@ public abstract class ContentEditingBaseBuilder<TCreateModel> : BuilderBase<TCre
     where TCreateModel : ContentCreationModelBase, new()
 {
     protected TCreateModel _model = new();
-    private List<ContentEditingPropertyValueBuilder<ContentEditingBaseBuilder<TCreateModel>>> _invariantProperties = [];
+    private List<ContentEditingPropertyValueBuilder<ContentEditingBaseBuilder<TCreateModel>>> _properties = [];
     private List<ContentEditingVariantBuilder<ContentEditingBaseBuilder<TCreateModel>>> _variants = [];
     private Guid _contentTypeKey;
     private Guid? _parentKey;
     private Guid? _key;
-    private string _invariantName;
 
     Guid? IWithKeyBuilder.Key
     {
         get => _key;
         set => _key = value;
-    }
-
-    string IWithInvariantNameBuilder.InvariantName
-    {
-        get => _invariantName;
-        set => _invariantName = value;
     }
 
     Guid? IWithParentKeyBuilder.ParentKey
@@ -45,13 +37,6 @@ public abstract class ContentEditingBaseBuilder<TCreateModel> : BuilderBase<TCre
         set => _contentTypeKey = value;
     }
 
-    public ContentEditingPropertyValueBuilder<ContentEditingBaseBuilder<TCreateModel>> AddInvariantProperty()
-    {
-        var builder = new ContentEditingPropertyValueBuilder<ContentEditingBaseBuilder<TCreateModel>>(this);
-        _invariantProperties.Add(builder);
-        return builder;
-    }
-
     public ContentEditingVariantBuilder<ContentEditingBaseBuilder<TCreateModel>> AddVariant()
     {
         var builder = new ContentEditingVariantBuilder<ContentEditingBaseBuilder<TCreateModel>>(this);
@@ -59,8 +44,20 @@ public abstract class ContentEditingBaseBuilder<TCreateModel> : BuilderBase<TCre
         return builder;
     }
 
+    public ContentEditingPropertyValueBuilder<ContentEditingBaseBuilder<TCreateModel>> AddProperty()
+    {
+        var builder = new ContentEditingPropertyValueBuilder<ContentEditingBaseBuilder<TCreateModel>>(this);
+        _properties.Add(builder);
+        return builder;
+    }
+
     public override TCreateModel Build()
     {
+        if (_variants.Any() is false)
+        {
+            throw new InvalidOperationException("Expected at least one variant (invariant is also a variant).");
+        }
+
         if (_parentKey is not null)
         {
             _model.ParentKey = _parentKey;
@@ -68,21 +65,7 @@ public abstract class ContentEditingBaseBuilder<TCreateModel> : BuilderBase<TCre
 
         _model.ContentTypeKey = _contentTypeKey;
         _model.Key = _key ?? Guid.NewGuid();
-        _model.Properties = _invariantProperties
-            .Select(p => p.Build())
-            .Union(_variants.SelectMany(variant => variant.GetProperties().Select(p => p.Build())))
-            .ToArray();
-
-        if (_invariantName.IsNullOrWhiteSpace() is false)
-        {
-            if (_variants.Any())
-            {
-                throw new InvalidOperationException("Cannot combine invariant and variant variants.");
-            }
-
-            AddVariant().WithName(_invariantName);
-        }
-
+        _model.Properties = _properties.Select(p => p.Build()).ToArray();
         _model.Variants = _variants.Select(x => x.Build()).ToList();
 
         return _model;
