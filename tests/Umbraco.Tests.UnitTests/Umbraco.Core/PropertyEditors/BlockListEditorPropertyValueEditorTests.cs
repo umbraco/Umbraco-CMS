5586531d059af95417018e1005f840ee2bdfee7c
Andy Butland <abutland73@gmail.com>
2025-04-28 16:36:59 +0200

    Added unit tests to verify behaviour.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/PropertyEditors/BlockListEditorPropertyValueEditorTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/PropertyEditors/BlockListEditorPropertyValueEditorTests.cs
@@ -6,7 +6,9 @@ using NUnit.Framework;
 using Umbraco.Cms.Core.Cache;
 using Umbraco.Cms.Core.Cache.PropertyEditors;
 using Umbraco.Cms.Core.IO;
+using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Models.Blocks;
+using Umbraco.Cms.Core.Models.Editors;
 using Umbraco.Cms.Core.Models.Validation;
 using Umbraco.Cms.Core.PropertyEditors;
 using Umbraco.Cms.Core.PropertyEditors.ValueConverters;
@@ -14,6 +16,9 @@ using Umbraco.Cms.Core.Serialization;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Core.Strings;
 using Umbraco.Cms.Infrastructure.Serialization;
+using Umbraco.Cms.Tests.Common.Builders;
+using Umbraco.Cms.Tests.Common.Builders.Extensions;
+using Umbraco.Extensions;
 using static Umbraco.Cms.Core.PropertyEditors.BlockListPropertyEditorBase;
 
 namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.PropertyEditors;
@@ -21,6 +26,9 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.PropertyEditors;
 [TestFixture]
 public class BlockListEditorPropertyValueEditorTests
 {
+    private static readonly Guid _contentTypeKey = Guid.NewGuid();
+    private static readonly Guid _contentKey = Guid.NewGuid();
+
     [Test]
     public void Validates_Null_As_Below_Configured_Min()
     {
@@ -83,14 +91,56 @@ public class BlockListEditorPropertyValueEditorTests
         Assert.IsNull(result);
     }
 
-    private static JsonObject CreateBlocksJson(int numberOfBlocks)
+    [Test]
+    public void FromEditor_With_Null_Current_Value_Returns_Expected_Json_Value()
+    {
+        var editedValue = CreateBlocksJson(1);
+        var currentValue = CreateBlocksJson(1, "Hello 2");
+        var editor = CreateValueEditor();
+
+        var contentPropertyData = new ContentPropertyData(editedValue, null);
+
+        var result = editor.FromEditor(contentPropertyData, null);
+        var resultAsJson = (JsonObject)JsonNode.Parse(result.ToString());
+        Assert.AreEqual("Hello", resultAsJson["contentData"][0]["values"][0]["value"].ToString());
+    }
+
+    [Test]
+    public void FromEditor_With_Current_Value_Returns_Expected_Json_Value()
+    {
+        var editedValue = CreateBlocksJson(1, "Hello 2");
+        var currentValue = CreateBlocksJson(1);
+        var editor = CreateValueEditor();
+
+        var contentPropertyData = new ContentPropertyData(editedValue, null);
+
+        var result = editor.FromEditor(contentPropertyData, currentValue);
+        var resultAsJson = (JsonObject)JsonNode.Parse(result.ToString());
+        Assert.AreEqual("Hello 2", resultAsJson["contentData"][0]["values"][0]["value"].ToString());
+    }
+
+    [Test]
+    public void FromEditor_With_Block_Item_Editor_That_Uses_Current_Value_Returns_Expected_Json_Value()
+    {
+        var editedValue = CreateBlocksJson(1, "Hello 2");
+        var currentValue = CreateBlocksJson(1);
+        var editor = CreateValueEditor(ValueEditorSetup.ConcatenatingTextValueEditor);
+
+        var contentPropertyData = new ContentPropertyData(editedValue, null);
+
+        var result = editor.FromEditor(contentPropertyData, currentValue);
+        var resultAsJson = (JsonObject)JsonNode.Parse(result.ToString());
+        Assert.AreEqual("Hello, Hello 2", resultAsJson["contentData"][0]["values"][0]["value"].ToString());
+    }
+
+    private static JsonObject CreateBlocksJson(int numberOfBlocks, string blockPropertyValue = "Hello")
     {
         var layoutItems = new JsonArray();
         var contentData = new JsonArray();
         for (int i = 0; i < numberOfBlocks; i++)
         {
             layoutItems.Add(CreateLayoutBlockJson());
-            contentData.Add(CreateContentDataBlockJson());
+            contentData.Add(CreateContentDataBlockJson(blockPropertyValue));
         }
 
         return new JsonObject
@@ -109,14 +159,14 @@ public class BlockListEditorPropertyValueEditorTests
         new()
         {
             { "$type", "BlockListLayoutItem" },
-            { "contentKey", Guid.NewGuid() },
+            { "contentKey", _contentKey },
         };
 
-    private static JsonObject CreateContentDataBlockJson() =>
+    private static JsonObject CreateContentDataBlockJson(string blockPropertyValue) =>
         new()
         {
-            { "contentTypeKey", Guid.Parse("01935a73-c86b-4521-9dcb-ad7cea402215") },
-            { "key", Guid.NewGuid() },
+            { "contentTypeKey", _contentTypeKey },
+            { "key", _contentKey },
             {
                 "values",
                 new JsonArray
@@ -125,32 +175,82 @@ public class BlockListEditorPropertyValueEditorTests
                     {
                         { "editorAlias", "Umbraco.TextBox" },
                         { "alias", "message" },
-                        { "value", "Hello" },
+                        { "value", blockPropertyValue },
                     },
                 }
             }
         };
 
-    private static BlockListEditorPropertyValueEditor CreateValueEditor()
+    private enum ValueEditorSetup
+    {
+        TextOnlyValueEditor,
+        ConcatenatingTextValueEditor,
+    }
+
+    private static BlockListEditorPropertyValueEditor CreateValueEditor(ValueEditorSetup valueEditorSetup = ValueEditorSetup.TextOnlyValueEditor)
     {
         var localizedTextServiceMock = new Mock<ILocalizedTextService>();
-        localizedTextServiceMock.Setup(x => x.Localize(
+        localizedTextServiceMock
+            .Setup(x => x.Localize(
                 It.IsAny<string>(),
                 It.IsAny<string>(),
                 It.IsAny<CultureInfo>(),
-        It.IsAny<IDictionary<string, string>>()))
+                It.IsAny<IDictionary<string, string>>()))
             .Returns((string key, string alias, CultureInfo culture, IDictionary<string, string> args) => $"{key}_{alias}");
 
         var jsonSerializer = new SystemTextJsonSerializer();
         var languageService = Mock.Of<ILanguageService>();
 
+        var dataValueEditorFactoryMock = new Mock<IDataValueEditorFactory>();
+
+        DataEditor textBoxEditor;
+        switch (valueEditorSetup)
+        {
+            case ValueEditorSetup.ConcatenatingTextValueEditor:
+                dataValueEditorFactoryMock
+                    .Setup(x => x.Create<ConcatenatingTextValueEditor>(It.IsAny<object[]>()))
+                    .Returns(new ConcatenatingTextValueEditor(
+                        Mock.Of<IShortStringHelper>(),
+                        new SystemTextJsonSerializer()));
+                textBoxEditor = new ConcatenatingTextboxPropertyEditor(
+                    dataValueEditorFactoryMock.Object);
+
+                break;
+            default:
+                dataValueEditorFactoryMock
+                    .Setup(x => x.Create<TextOnlyValueEditor>(It.IsAny<object[]>()))
+                    .Returns(new TextOnlyValueEditor(
+                        new DataEditorAttribute("a"),
+                        Mock.Of<ILocalizedTextService>(),
+                        Mock.Of<IShortStringHelper>(),
+                        new SystemTextJsonSerializer(),
+                        Mock.Of<IIOHelper>()));
+                textBoxEditor = new TextboxPropertyEditor(
+                    dataValueEditorFactoryMock.Object,
+                    Mock.Of<IIOHelper>());
+                break;
+        }
+
+        var propertyEditors = new PropertyEditorCollection(new DataEditorCollection(() => textBoxEditor.Yield()));
+
+        var elementType = new ContentTypeBuilder()
+            .WithKey(_contentTypeKey)
+            .AddPropertyType()
+                .WithAlias("message")
+                .Done()
+            .Build();
+        var elementTypeCacheMock = new Mock<IBlockEditorElementTypeCache>();
+        elementTypeCacheMock
+            .Setup(x => x.GetMany(It.Is<IEnumerable<Guid>>(y => y.First() == _contentTypeKey)))
+            .Returns([elementType]);
+
         return new BlockListEditorPropertyValueEditor(
             new DataEditorAttribute("alias"),
             new BlockListEditorDataConverter(jsonSerializer),
-            new(new DataEditorCollection(() => [])),
+            propertyEditors,
             new DataValueReferenceFactoryCollection(Enumerable.Empty<IDataValueReferenceFactory>),
             Mock.Of<IDataTypeConfigurationCache>(),
-            Mock.Of<IBlockEditorElementTypeCache>(),
+            elementTypeCacheMock.Object,
             localizedTextServiceMock.Object,
             new NullLogger<BlockListEditorPropertyValueEditor>(),
             Mock.Of<IShortStringHelper>(),
@@ -169,5 +269,47 @@ public class BlockListEditorPropertyValueEditorTests
                 },
             },
         };
+    }
+
+    /// <summary>
+    /// An illustrative property editor that uses the edited and current value when returning a result from the FromEditor calls.
+    /// </summary>
+    /// <remarks>
+    /// This is used to simulate a real-world editor that needs to use this value from within the block editor and verify
+    /// that it receives and processes the value.
+    /// </remarks>
+    [DataEditor(
+        global::Umbraco.Cms.Core.Constants.PropertyEditors.Aliases.TextBox)]
+    private class ConcatenatingTextboxPropertyEditor : DataEditor
+    {
+        public ConcatenatingTextboxPropertyEditor(IDataValueEditorFactory dataValueEditorFactory)
+            : base(dataValueEditorFactory)
+        {
+        }
+
+        protected override IDataValueEditor CreateValueEditor() =>
+            DataValueEditorFactory.Create<ConcatenatingTextValueEditor>(Attribute!);
+    }
+
+    /// <summary>
+    /// An illustrative value editor that uses the edited and current value when returning a result from the FromEditor calls.
+    /// </summary>
+    /// <remarks>
+    /// See notes on <see cref="ConcatenatingTextboxPropertyEditor"/>.
+    /// </remarks>
+    private class ConcatenatingTextValueEditor : DataValueEditor
+    {
+        public ConcatenatingTextValueEditor(IShortStringHelper shortStringHelper, IJsonSerializer? jsonSerializer)
+            : base(shortStringHelper, jsonSerializer)
+        {
+        }
+
+        public override object FromEditor(ContentPropertyData propertyData, object? currentValue)
+        {
+            var editedValue = propertyData.Value;
+            var currentValueAsString = currentValue?.ToString() ?? string.Empty;
+            var editedValueAsString = editedValue?.ToString() ?? string.Empty;
+            return $"{currentValueAsString}, {editedValueAsString}";
+        }
     }
 }
