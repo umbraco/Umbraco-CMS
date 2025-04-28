f927abf65211a28f9f9091147316076bcc92e0c3
Migaroez <geusens@gmail.com>
2025-04-28 18:00:38 +0200

    Added extended webhook content responses

--- src/Umbraco.Core/Webhooks/ExtendedEvents/Content/ExtendedContentPublishedWebhookEvent.cs
+++ src/Umbraco.Core/Webhooks/ExtendedEvents/Content/ExtendedContentPublishedWebhookEvent.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.Options;
 using Umbraco.Cms.Core.Configuration.Models;
 using Umbraco.Cms.Core.DeliveryApi;
 using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Models.DeliveryApi;
 using Umbraco.Cms.Core.Models.PublishedContent;
 using Umbraco.Cms.Core.Notifications;
 using Umbraco.Cms.Core.PublishedCache;
@@ -10,20 +11,23 @@ using Umbraco.Cms.Core.Sync;
 
 namespace Umbraco.Cms.Core.Webhooks.Events;
 
-// todo: convert to proper extended media event when deliveryApi models are fixed
 [WebhookEvent("Content Published", Constants.WebhookEvents.Types.Content)]
 public class ExtendedContentPublishedWebhookEvent : WebhookEventContentBase<ContentPublishedNotification, IContent>
 {
-    private readonly IApiContentBuilder _apiContentBuilder;
+    private readonly IApiContentResponseBuilder _apiContentBuilder;
     private readonly IPublishedContentCache _publishedContentCache;
+    private readonly IOutputExpansionStrategyAccessor _outputExpansionStrategyAccessor;
+    private readonly IVariationContextAccessor _variationContextAccessor;
 
     public ExtendedContentPublishedWebhookEvent(
         IWebhookFiringService webhookFiringService,
         IWebhookService webhookService,
         IOptionsMonitor<WebhookSettings> webhookSettings,
         IServerRoleAccessor serverRoleAccessor,
-        IApiContentBuilder apiContentBuilder,
-        IPublishedContentCache publishedContentCache)
+        IApiContentResponseBuilder apiContentBuilder,
+        IPublishedContentCache publishedContentCache,
+        IOutputExpansionStrategyAccessor outputExpansionStrategyAccessor,
+        IVariationContextAccessor variationContextAccessor)
         : base(
             webhookFiringService,
             webhookService,
@@ -32,15 +36,58 @@ public class ExtendedContentPublishedWebhookEvent : WebhookEventContentBase<Cont
     {
         _apiContentBuilder = apiContentBuilder;
         _publishedContentCache = publishedContentCache;
+        _outputExpansionStrategyAccessor = outputExpansionStrategyAccessor;
+        _variationContextAccessor = variationContextAccessor;
     }
 
     public override string Alias => Constants.WebhookEvents.Aliases.ContentPublish;
 
-    protected override IEnumerable<IContent> GetEntitiesFromNotification(ContentPublishedNotification notification) => notification.PublishedEntities;
+    protected override IEnumerable<IContent> GetEntitiesFromNotification(ContentPublishedNotification notification) =>
+        notification.PublishedEntities;
 
     protected override object? ConvertEntityToRequestPayload(IContent entity)
     {
         IPublishedContent? publishedContent = _publishedContentCache.GetById(entity.Key);
-        return publishedContent is null ? null : _apiContentBuilder.Build(publishedContent);
+        IApiContentResponse? deliveryContent =
+            publishedContent is null ? null : _apiContentBuilder.Build(publishedContent);
+
+        if (deliveryContent == null)
+        {
+            return null;
+        }
+
+        var cultures = new Dictionary<string, object>();
+
+        // just to be safe that messing with the variationContext doesn't screw things up
+        VariationContext? originalVariationContext = _variationContextAccessor.VariationContext;
+
+        foreach (KeyValuePair<string, IApiContentRoute> culture in deliveryContent.Cultures)
+        {
+            _variationContextAccessor.VariationContext = new VariationContext(culture.Key);
+
+            IDictionary<string, object?> properties =
+                _outputExpansionStrategyAccessor.TryGetValue(out IOutputExpansionStrategy? outputExpansionStrategy)
+                    ? outputExpansionStrategy.MapContentProperties(publishedContent!)
+                    : new Dictionary<string, object?>();
+
+            cultures.Add(culture.Key, new
+            {
+                culture.Value.Path,
+                culture.Value.StartItem,
+                properties,
+            });
+        }
+
+        _variationContextAccessor.VariationContext = originalVariationContext;
+
+        return new
+        {
+            deliveryContent.Id,
+            deliveryContent.ContentType,
+            deliveryContent.Name,
+            deliveryContent.CreateDate,
+            deliveryContent.UpdateDate,
+            cultures,
+        };
     }
 }
--- src/Umbraco.Core/Webhooks/ExtendedEvents/Content/ExtendedContentSavedWebhookEvent.cs
+++ src/Umbraco.Core/Webhooks/ExtendedEvents/Content/ExtendedContentSavedWebhookEvent.cs
@@ -2,6 +2,7 @@ using Microsoft.Extensions.Options;
 using Umbraco.Cms.Core.Configuration.Models;
 using Umbraco.Cms.Core.DeliveryApi;
 using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Models.DeliveryApi;
 using Umbraco.Cms.Core.Models.PublishedContent;
 using Umbraco.Cms.Core.Notifications;
 using Umbraco.Cms.Core.PublishedCache;
@@ -10,20 +11,23 @@ using Umbraco.Cms.Core.Sync;
 
 namespace Umbraco.Cms.Core.Webhooks.Events;
 
-// todo: convert to proper extended media event when deliveryApi models are fixed
 [WebhookEvent("Content Saved", Constants.WebhookEvents.Types.Content)]
 public class ExtendedContentSavedWebhookEvent : WebhookEventContentBase<ContentSavedNotification, IContent>
 {
-    private readonly IApiContentBuilder _apiContentBuilder;
+    private readonly IApiContentResponseBuilder _apiContentBuilder;
     private readonly IPublishedContentCache _contentCache;
+    private readonly IVariationContextAccessor _variationContextAccessor;
+    private readonly IOutputExpansionStrategyAccessor _outputExpansionStrategyAccessor;
 
     public ExtendedContentSavedWebhookEvent(
         IWebhookFiringService webhookFiringService,
         IWebhookService webhookService,
         IOptionsMonitor<WebhookSettings> webhookSettings,
         IServerRoleAccessor serverRoleAccessor,
-        IApiContentBuilder apiContentBuilder,
-        IPublishedContentCache contentCache)
+        IApiContentResponseBuilder apiContentBuilder,
+        IPublishedContentCache contentCache,
+        IVariationContextAccessor variationContextAccessor,
+        IOutputExpansionStrategyAccessor outputExpansionStrategyAccessor)
         : base(
             webhookFiringService,
             webhookService,
@@ -32,6 +36,8 @@ public class ExtendedContentSavedWebhookEvent : WebhookEventContentBase<ContentS
     {
         _apiContentBuilder = apiContentBuilder;
         _contentCache = contentCache;
+        _variationContextAccessor = variationContextAccessor;
+        _outputExpansionStrategyAccessor = outputExpansionStrategyAccessor;
     }
 
     public override string Alias => Constants.WebhookEvents.Aliases.ContentSaved;
@@ -43,6 +49,45 @@ public class ExtendedContentSavedWebhookEvent : WebhookEventContentBase<ContentS
     {
         // Get preview/saved version of content
         IPublishedContent? publishedContent = _contentCache.GetById(true, entity.Key);
-        return publishedContent is null ? null : _apiContentBuilder.Build(publishedContent);
+        IApiContentResponse? deliveryContent = publishedContent is null ? null : _apiContentBuilder.Build(publishedContent);
+
+        if (deliveryContent == null)
+        {
+            return null;
+        }
+
+        var cultures = new Dictionary<string, object>();
+
+        // just to be safe that messing with the variationContext doesn't screw things up
+        VariationContext? originalVariationContext = _variationContextAccessor.VariationContext;
+
+        foreach (KeyValuePair<string, IApiContentRoute> culture in deliveryContent.Cultures)
+        {
+            _variationContextAccessor.VariationContext = new VariationContext(culture.Key);
+
+            IDictionary<string, object?> properties =
+                _outputExpansionStrategyAccessor.TryGetValue(out IOutputExpansionStrategy? outputExpansionStrategy)
+                    ? outputExpansionStrategy.MapContentProperties(publishedContent!)
+                    : new Dictionary<string, object?>();
+
+            cultures.Add(culture.Key, new
+            {
+                culture.Value.Path,
+                culture.Value.StartItem,
+                properties,
+            });
+        }
+
+        _variationContextAccessor.VariationContext = originalVariationContext;
+
+        return new
+        {
+            deliveryContent.Id,
+            deliveryContent.ContentType,
+            deliveryContent.Name,
+            deliveryContent.CreateDate,
+            deliveryContent.UpdateDate,
+            cultures,
+        };
     }
 }
