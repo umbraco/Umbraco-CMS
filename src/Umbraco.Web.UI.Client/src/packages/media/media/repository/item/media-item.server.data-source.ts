0b4b24ae5eeff00a4a049bd9aff03efc0e4bdb7f
Mads Rasmussen <madsr@hey.com>
2025-05-02 13:38:16 +0200

    wip testing implementations

--- src/Umbraco.Web.UI.Client/src/packages/media/media/repository/item/media-item.server.data-source.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/repository/item/media-item.server.data-source.ts
@@ -4,8 +4,10 @@ import type { MediaItemResponseModel } from '@umbraco-cms/backoffice/external/ba
 import { MediaService } from '@umbraco-cms/backoffice/external/backend-api';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import { UmbItemServerDataSourceBase } from '@umbraco-cms/backoffice/repository';
-import { tryExecute, UmbApiError, UmbCancelError, UmbError } from '@umbraco-cms/backoffice/resources';
+import type { UmbApiError, UmbCancelError, UmbError } from '@umbraco-cms/backoffice/resources';
+import { tryExecute } from '@umbraco-cms/backoffice/resources';
 import { batchArray } from '@umbraco-cms/backoffice/utils';
+import { UmbControllerBase } from '@umbraco-cms/backoffice/class-api';
 
 /**
  * A data source for Media items that fetches data from the server
@@ -49,32 +51,13 @@ export class UmbMediaItemServerDataSource extends UmbItemServerDataSourceBase<
 	override async getItems(uniques: Array<string>) {
 		if (!uniques) throw new Error('Uniques are missing');
 
-		let data: Array<MediaItemResponseModel> | undefined;
-		let error: UmbError | UmbApiError | UmbCancelError | Error | undefined;
+		const itemRequestManager = new UmbManagementApiItemGetRequestManager(this.#host, {
+			// eslint-disable-next-line local-rules/no-direct-api-import
+			api: (args) => MediaService.getItemMedia({ query: { id: args.uniques } }),
+			uniques,
+		});
 
-		const batchSize = 40;
-		if (uniques.length > batchSize) {
-			const chunks = batchArray<string>(uniques, batchSize);
-			const results = await batchTryExecute(this.#host, chunks, (chunk) =>
-				// eslint-disable-next-line local-rules/no-direct-api-import
-				MediaService.getItemMedia({ query: { id: chunk } }),
-			);
-
-			const errors = results.filter((promiseResult) => promiseResult.status === 'rejected');
-
-			if (errors.length > 0) {
-				// TODO: investigate if its ok to only return the first error
-				error = errors[0].reason;
-			}
-
-			data = results
-				.filter((promiseResult) => promiseResult.status === 'fulfilled')
-				.flatMap((promiseResult) => promiseResult.value.data);
-		} else {
-			const result = await tryExecute(this.#host, MediaService.getItemMedia({ query: { id: uniques } }));
-			data = result.data;
-			error = result.error;
-		}
+		const { data, error } = await itemRequestManager.request();
 
 		if (data) {
 			const items = data.map((item) => mapper(item));
@@ -106,6 +89,51 @@ const mapper = (item: MediaItemResponseModel): UmbMediaItemModel => {
 		}),
 	};
 };
+
+interface UmbManagementApiItemGetRequestManagerArgs<ResponseModelType> {
+	api: (args: { uniques: Array<string> }) => Promise<ResponseModelType>;
+	uniques: Array<string>;
+}
+class UmbManagementApiItemGetRequestManager<ResponseModelType> extends UmbControllerBase {
+	#apiCallback: (args: { uniques: Array<string> }) => Promise<ResponseModelType>;
+	#uniques: Array<string>;
+	#batchSize: number = 1;
+
+	constructor(host: UmbControllerHost, args: UmbManagementApiItemGetRequestManagerArgs<ResponseModelType>) {
+		super(host);
+		this.#apiCallback = args.api;
+		this.#uniques = args.uniques;
+	}
+
+	async request() {
+		if (!this.#uniques) throw new Error('Uniques are missing');
+
+		let data: Array<ResponseModelType> | undefined;
+		let error: UmbError | UmbApiError | UmbCancelError | Error | undefined;
+
+		if (this.#uniques.length > this.#batchSize) {
+			const chunks = batchArray<string>(this.#uniques, this.#batchSize);
+			const results = await batchTryExecute(this, chunks, (chunk) => this.#apiCallback({ uniques: chunk }));
+
+			const errors = results.filter((promiseResult) => promiseResult.status === 'rejected');
+
+			if (errors.length > 0) {
+				// TODO: investigate if its ok to only return the first error
+				error = errors[0].reason;
+			}
+
+			data = results
+				.filter((promiseResult) => promiseResult.status === 'fulfilled')
+				.flatMap((promiseResult) => promiseResult.value.data);
+		} else {
+			const result = await tryExecute(this, this.#apiCallback({ uniques: this.#uniques }));
+			data = result.data;
+			error = result.error;
+		}
+
+		return { data, error };
+	}
+}
 
 /**
  * Batches promises and returns a promise that resolves to an array of results
