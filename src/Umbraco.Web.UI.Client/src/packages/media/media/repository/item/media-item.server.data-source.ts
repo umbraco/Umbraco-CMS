45cc5fc967294c581229056ab60b2684d90d5358
Mads Rasmussen <madsr@hey.com>
2025-05-02 12:45:53 +0200

    split function

--- src/Umbraco.Web.UI.Client/src/packages/media/media/repository/item/media-item.server.data-source.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/repository/item/media-item.server.data-source.ts
@@ -109,6 +109,19 @@ const mapper = (item: MediaItemResponseModel): UmbMediaItemModel => {
 
 /**
  * Batches promises and returns a promise that resolves to an array of results
+ * @param {Array<Array<BatchEntryType>>} chunks - The array of chunks to process
+ * @param {(chunk: Array<BatchEntryType>) => Promise<PromiseResult>} callback - The function to call for each chunk
+ * @returns {Promise<PromiseSettledResult<PromiseResult>[]>} - A promise that resolves to an array of results
+ */
+function batchPromises<BatchEntryType, PromiseResult>(
+	chunks: Array<Array<BatchEntryType>>,
+	callback: (chunk: Array<BatchEntryType>) => Promise<PromiseResult>,
+): Promise<PromiseSettledResult<PromiseResult>[]> {
+	return Promise.allSettled(chunks.map((chunk) => callback(chunk)));
+}
+
+/**
+ * Batches promises and returns a promise that resolves to an array of results
  * @param {UmbControllerHost} host - The host to use for the request and where notifications will be shown
  * @param {Array<Array<BatchEntryType>>} chunks - The array of chunks to process
  * @param {(chunk: Array<BatchEntryType>) => Promise<PromiseResult>} callback - The function to call for each chunk
@@ -119,5 +132,5 @@ function batchTryExecute<BatchEntryType, PromiseResult>(
 	chunks: Array<Array<BatchEntryType>>,
 	callback: (chunk: Array<BatchEntryType>) => Promise<PromiseResult>,
 ): Promise<PromiseSettledResult<PromiseResult>[]> {
-	return Promise.allSettled(chunks.map((chunk) => tryExecute(host, callback(chunk))));
+	return batchPromises(chunks, (chunk) => tryExecute(host, callback(chunk)));
 }
