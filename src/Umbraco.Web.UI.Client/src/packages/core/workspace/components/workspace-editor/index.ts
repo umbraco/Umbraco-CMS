4642e0e32b09b5b1d6cfd8fe29121c283d5029a6
Niels Lyngsø <niels.lyngso@gmail.com>
2025-05-06 19:52:36 +0200

    export UMB_WORKSPACE_VIEW_CONTEXT

--- src/Umbraco.Web.UI.Client/src/packages/core/workspace/components/workspace-editor/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/workspace/components/workspace-editor/index.ts
@@ -1,3 +1,4 @@
 export * from './workspace-editor.element.js';
 export * from './workspace-view-navigation.context-token.js';
 export * from './workspace-view-navigation.context.js';
+export * from './workspace-view.context-token.js';
