84699de3b01f637980b1317cbeb79ec8aefc350b
leekelleher <leekelleher@gmail.com>
2025-05-07 12:02:18 +0100

    Added validation message for invalid cultures/locales

--- src/Umbraco.Web.UI.Client/src/packages/core/localization/components/ui-culture-input/ui-culture-input.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/localization/components/ui-culture-input/ui-culture-input.element.ts
@@ -1,10 +1,10 @@
-import type { ManifestLocalization } from '../../extensions/localization.extension.js';
+import { customElement, html, property, state } from '@umbraco-cms/backoffice/external/lit';
 import { UmbChangeEvent } from '@umbraco-cms/backoffice/event';
-import { css, html, customElement, query, state, property } from '@umbraco-cms/backoffice/external/lit';
-import type { UUISelectEvent } from '@umbraco-cms/backoffice/external/uui';
-import { UUIFormControlMixin } from '@umbraco-cms/backoffice/external/uui';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
 import { umbExtensionsRegistry } from '@umbraco-cms/backoffice/extension-registry';
+import { UmbFormControlMixin } from '@umbraco-cms/backoffice/validation';
+import type { PropertyValues } from '@umbraco-cms/backoffice/external/lit';
+import type { UUISelectEvent } from '@umbraco-cms/backoffice/external/uui';
 
 interface UmbCultureInputOption {
 	name: string;
@@ -12,61 +12,95 @@ interface UmbCultureInputOption {
 }
 
 @customElement('umb-ui-culture-input')
-export class UmbUiCultureInputElement extends UUIFormControlMixin(UmbLitElement, '') {
+export class UmbUiCultureInputElement extends UmbFormControlMixin<string, typeof UmbLitElement, undefined>(
+	UmbLitElement,
+) {
+	#invalidCulture?: string;
+	#invalidBaseCulture?: string;
+
 	@state()
 	private _options: Array<UmbCultureInputOption> = [];
 
-	@query('uui-combobox')
-	private _selectElement!: HTMLInputElement;
-
 	@property({ type: String })
-	override get value() {
-		return super.value;
-	}
-	override set value(value: FormDataEntryValue | FormData) {
-		if (typeof value === 'string') {
+	override set value(value: string | undefined) {
+		if (value && typeof value === 'string') {
 			const oldValue = super.value;
 			super.value = value.toLowerCase();
 			this.requestUpdate('value', oldValue);
 		}
 	}
+	override get value(): string | undefined {
+		return super.value;
+	}
 
 	constructor() {
 		super();
-		this.#observeTranslations();
-	}
 
-	#observeTranslations() {
 		this.observe(
 			umbExtensionsRegistry.byType('localization'),
-			(localizationManifests) => {
-				this.#mapToOptions(localizationManifests);
+			(manifests) => {
+				const options = manifests
+					.filter((manifest) => !!manifest.meta.culture)
+					.map((manifest) => {
+						const culture = manifest.meta.culture.toLowerCase();
+						return {
+							name: this.localize.term(`uiCulture_${culture}`),
+							value: culture,
+						};
+					});
+
+				const distinct = [...new Map(options.map((item) => [item.value, item])).values()];
+
+				this._options = distinct.sort((a, b) => a.value.localeCompare(b.value));
 			},
 			'umbObserveLocalizationManifests',
 		);
+
+		this.addValidator(
+			'customError',
+			() => this.localize.term('user_languageNotFound', this.#invalidCulture, this.value),
+			() => !!this.#invalidCulture && !this.#invalidBaseCulture,
+		);
+
+		this.addValidator(
+			'customError',
+			() => this.localize.term('user_languageNotFoundFallback', this.#invalidCulture, this.#invalidBaseCulture),
+			() => !!this.#invalidCulture && !!this.#invalidBaseCulture,
+		);
 	}
 
-	#mapToOptions(manifests: Array<ManifestLocalization>) {
-		const options = manifests
-			.filter((manifest) => !!manifest.meta.culture)
-			.map((manifest) => {
-				const culture = manifest.meta.culture.toLowerCase();
-				return {
-					name: this.localize.term(`uiCulture_${culture}`),
-					value: culture,
-				};
-			});
+	protected override firstUpdated(_changedProperties: PropertyValues): void {
+		super.firstUpdated(_changedProperties);
 
-		const distinct = [...new Map(options.map((item) => [item.value, item])).values()];
+		// Check if the culture can be found.
+		const found = this._options.find((option) => option.value === this.value);
+		if (!found) {
+			this.#invalidCulture = this.value;
 
-		this._options = distinct.sort((a, b) => a.value.localeCompare(b.value));
-	}
+			// if not found, check for the base culture
+			if (this.value?.includes('-')) {
+				const baseCulture = this.value.split('-')[0];
+				const foundBase = this._options.find((option) => option.value === baseCulture);
+				if (foundBase) {
+					this.value = foundBase.value;
+				} else {
+					// if the base culture is not found, set the value to "en"
+					this.#invalidBaseCulture = baseCulture;
+					this.value = 'en';
+				}
+			} else {
+				// if the base culture is not found, set the value to "en"
+				this.value = 'en';
+			}
+		}
 
-	protected override getFormElement() {
-		return this._selectElement;
+		this.addFormControlElement(this.shadowRoot!.querySelector('uui-select')!);
+		this.checkValidity();
 	}
 
 	#onCustomValidationChange(event: UUISelectEvent) {
+		this.#invalidCulture = undefined;
+		this.#invalidBaseCulture = undefined;
 		this.value = event.target.value.toString();
 		this.dispatchEvent(new UmbChangeEvent());
 	}
@@ -74,23 +108,11 @@ export class UmbUiCultureInputElement extends UUIFormControlMixin(UmbLitElement,
 	override render() {
 		return html`
 			<uui-select
-				style="margin-top: var(--uui-size-space-1)"
-				@change=${this.#onCustomValidationChange}
-				.options=${this._options.map((e) => ({
-					name: e.name,
-					value: e.value,
-					selected: e.value == this.value,
-				}))}></uui-select>
+				.options=${this._options.map((e) => ({ ...e, selected: e.value == this.value }))}
+				@change=${this.#onCustomValidationChange}>
+			</uui-select>
 		`;
 	}
-
-	static override styles = [
-		css`
-			:host {
-				display: block;
-			}
-		`,
-	];
 }
 
 export default UmbUiCultureInputElement;
