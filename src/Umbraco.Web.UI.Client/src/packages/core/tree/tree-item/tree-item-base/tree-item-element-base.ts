a53e2a379ca0ff66c723b3176b4beb34f813b965
Nhu Dinh <hnd@umbraco.dk>
2025-05-08 16:40:13 +0700

    Added label attribute to uui-menu-item

--- src/Umbraco.Web.UI.Client/src/packages/core/tree/tree-item/tree-item-base/tree-item-element-base.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/tree/tree-item/tree-item-base/tree-item-element-base.ts
@@ -137,7 +137,7 @@ export abstract class UmbTreeItemElementBase<
 				.hasChildren=${this._hasChildren}
 				.showChildren=${this._isOpen}
 				.caretLabel=${this.localize.term('visuallyHiddenTexts_expandChildItems') + ' ' + this._label}
-				.label=${this._label}
+				label=${this._label}
 				href="${ifDefined(this._isSelectableContext ? undefined : this._href)}">
 				${this.renderIconContainer()} ${this.renderLabel()} ${this.#renderActions()} ${this.#renderChildItems()}
 				<slot></slot>
