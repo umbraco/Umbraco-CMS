713d5eff5e41da366217aa58afde5d3d19c38798
Niels Lyngsø <nsl@umbraco.dk>
2025-05-09 09:25:39 +0200

    setup context tokens to support menu structure workspace (#19172)

--- src/Umbraco.Web.UI.Client/src/packages/language/menu/language-menu-structure.context.ts
+++ src/Umbraco.Web.UI.Client/src/packages/language/menu/language-menu-structure.context.ts
@@ -4,7 +4,7 @@ import { UmbContextBase } from '@umbraco-cms/backoffice/class-api';
 import { UMB_WORKSPACE_CONTEXT } from '@umbraco-cms/backoffice/workspace';
 import { UmbArrayState } from '@umbraco-cms/backoffice/observable-api';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
-import type { UmbStructureItemModel } from '@umbraco-cms/backoffice/menu';
+import { UMB_MENU_STRUCTURE_WORKSPACE_CONTEXT, type UmbStructureItemModel } from '@umbraco-cms/backoffice/menu';
 
 export class UmbLanguageNavigationStructureWorkspaceContext extends UmbContextBase {
 	// TODO: figure out the correct type where we have "data" available
@@ -14,8 +14,9 @@ export class UmbLanguageNavigationStructureWorkspaceContext extends UmbContextBa
 	public readonly structure = this.#structure.asObservable();
 
 	constructor(host: UmbControllerHost) {
-		// TODO: set up context token
-		super(host, 'UmbMenuStructureWorkspaceContext');
+		super(host, UMB_MENU_STRUCTURE_WORKSPACE_CONTEXT);
+		// 'UmbMenuStructureWorkspaceContext' is Obsolete, will be removed in v.18
+		this.provideContext('UmbMenuStructureWorkspaceContext', this);
 
 		this.consumeContext(UMB_WORKSPACE_CONTEXT, (instance) => {
 			this.#workspaceContext = instance;
