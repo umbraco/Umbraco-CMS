74e087b166f9284a5ec8244227bd1f761ca8d70e
Andy Butland <abutland73@gmail.com>
2025-05-09 10:33:46 +0200

    Updated unit tests.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/Extensions/IntExtensionsTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Extensions/IntExtensionsTests.cs
@@ -18,4 +18,24 @@ public class IntExtensionsTests
         var result = input.ToGuid();
         Assert.AreEqual(expected, result.ToString());
     }
+
+    [TestCase("00000014-0000-0000-0000-000000000000", 20)]
+    [TestCase("0000006a-0000-0000-0000-000000000000", 106)]
+    [TestCase("000f423f-0000-0000-0000-000000000000", 999999)]
+    [TestCase("211d1ae3-0000-0000-0000-000000000000", 555555555)]
+    [TestCase("0d93047e-558d-4311-8a9d-b89e6fca0337", null)]
+    public void TryParseFromGuid_Parses_Expected_Integer(string input, int? expected)
+    {
+        var result = IntExtensions.TryParseFromGuid(Guid.Parse(input), out int? intValue);
+        if (expected is null)
+        {
+            Assert.IsFalse(result);
+            Assert.IsFalse(intValue.HasValue);
+        }
+        else
+        {
+            Assert.IsTrue(result);
+            Assert.AreEqual(expected, intValue.Value);
+        }
+    }
 }
