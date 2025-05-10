0a4133667e05741706993f836c1441e5a66d6fd8
Mads Rasmussen <madsr@hey.com>
2025-05-10 21:29:34 +0200

    render workspace icon for document type folders

--- src/Umbraco.Web.UI.Client/src/packages/documents/document-types/tree/folder/workspace/document-type-folder-editor.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/document-types/tree/folder/workspace/document-type-folder-editor.element.ts
@@ -1,4 +1,4 @@
-import { html, customElement } from '@umbraco-cms/backoffice/external/lit';
+import { html, customElement, css } from '@umbraco-cms/backoffice/external/lit';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
 import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
 
@@ -7,11 +7,21 @@ const elementName = 'umb-document-type-folder-workspace-editor';
 export class UmbDocumentTypeFolderWorkspaceEditorElement extends UmbLitElement {
 	override render() {
 		return html`<umb-workspace-editor>
+			<umb-icon id="icon" slot="header" name="icon-folder"></umb-icon>
 			<umb-workspace-header-name-editable slot="header"></umb-workspace-header-name-editable>
 		</umb-workspace-editor>`;
 	}
 
-	static override styles = [UmbTextStyles];
+	static override styles = [
+		UmbTextStyles,
+		css`
+			#icon {
+				display: inline-block;
+				font-size: var(--uui-size-6);
+				margin-right: var(--uui-size-space-4);
+			}
+		`,
+	];
 }
 
 export { UmbDocumentTypeFolderWorkspaceEditorElement as element };
