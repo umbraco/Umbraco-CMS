88bb3da46ba97898150a0a54c6e5e971e9154aa3
Mads Rasmussen <madsr@hey.com>
2025-05-11 21:25:32 +0200

    use element

--- src/Umbraco.Web.UI.Client/src/packages/documents/document-types/tree/folder/workspace/document-type-folder-editor.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/document-types/tree/folder/workspace/document-type-folder-editor.element.ts
@@ -1,33 +1,17 @@
-import { html, customElement, css } from '@umbraco-cms/backoffice/external/lit';
+import { html, customElement } from '@umbraco-cms/backoffice/external/lit';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
-import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
 
-const elementName = 'umb-document-type-folder-workspace-editor';
-@customElement(elementName)
+@customElement('umb-document-type-folder-workspace-editor')
 export class UmbDocumentTypeFolderWorkspaceEditorElement extends UmbLitElement {
 	override render() {
-		return html`<umb-workspace-editor>
-			<umb-icon id="icon" slot="header" name="icon-folder"></umb-icon>
-			<umb-workspace-header-name-editable slot="header"></umb-workspace-header-name-editable>
-		</umb-workspace-editor>`;
+		return html`<umb-folder-workspace-editor></umb-folder-workspace-editor>`;
 	}
-
-	static override styles = [
-		UmbTextStyles,
-		css`
-			#icon {
-				display: inline-block;
-				font-size: var(--uui-size-6);
-				margin-right: var(--uui-size-space-4);
-			}
-		`,
-	];
 }
 
 export { UmbDocumentTypeFolderWorkspaceEditorElement as element };
 
 declare global {
 	interface HTMLElementTagNameMap {
-		[elementName]: UmbDocumentTypeFolderWorkspaceEditorElement;
+		['umb-document-type-folder-workspace-editor']: UmbDocumentTypeFolderWorkspaceEditorElement;
 	}
 }
