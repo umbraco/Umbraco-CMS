9f6879d5688210eadcfa9eeacccad2e1c1f605e0
kjac <kja@umbraco.dk>
2025-05-13 11:57:27 +0200

    Cleaned up usings

--- src/Umbraco.Cms.Api.Management/Controllers/Segment/SegmentControllerBase.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/Segment/SegmentControllerBase.cs
@@ -1,9 +1,7 @@
-using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Umbraco.Cms.Api.Management.Routing;
 using Umbraco.Cms.Core.Services.OperationStatus;
-using Umbraco.Cms.Web.Common.Authorization;
 
 namespace Umbraco.Cms.Api.Management.Controllers.Segment;
 
