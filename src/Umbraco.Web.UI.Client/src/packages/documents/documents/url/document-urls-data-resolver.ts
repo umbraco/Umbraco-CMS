02d82da79e4e534f60415eef2b565763b7c037a7
Mads Rasmussen <madsr@hey.com>
2025-05-13 14:15:31 +0200

    add document urls data resolver

---
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/url/document-urls-data-resolver.ts
@@ -0,0 +1,105 @@
+import type { UmbDocumentUrlModel } from './repository/types.js';
+import { UmbControllerBase } from '@umbraco-cms/backoffice/class-api';
+import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
+import { UMB_APP_LANGUAGE_CONTEXT } from '@umbraco-cms/backoffice/language';
+import { UmbArrayState } from '@umbraco-cms/backoffice/observable-api';
+import { UMB_PROPERTY_DATASET_CONTEXT } from '@umbraco-cms/backoffice/property';
+import type { UmbVariantId } from '@umbraco-cms/backoffice/variant';
+
+/**
+ * A controller for resolving data for document urls
+ * @exports
+ * @class UmbDocumentUrlsDataResolver
+ * @augments {UmbControllerBase}
+ */
+export class UmbDocumentUrlsDataResolver extends UmbControllerBase {
+	#defaultCulture?: string;
+	#appCulture?: string;
+	#propertyDataSetCulture?: UmbVariantId;
+	#data?: Array<UmbDocumentUrlModel> | undefined;
+
+	#init: Promise<unknown>;
+
+	#urls = new UmbArrayState<UmbDocumentUrlModel>([], (url) => url.url);
+	/**
+	 * The urls for the current culture
+	 * @returns {ObservableArray<UmbDocumentUrlModel>} The urls for the current culture
+	 * @memberof UmbDocumentUrlsDataResolver
+	 */
+	public readonly urls = this.#urls.asObservable();
+
+	constructor(host: UmbControllerHost) {
+		super(host);
+
+		this.consumeContext(UMB_PROPERTY_DATASET_CONTEXT, (context) => {
+			this.#propertyDataSetCulture = context?.getVariantId();
+			this.#setCultureAwareValues();
+		});
+
+		// We do not depend on this context because we know is it only available in some cases
+		this.#init = this.consumeContext(UMB_APP_LANGUAGE_CONTEXT, (context) => {
+			this.observe(context?.appLanguageCulture, (culture) => {
+				this.#appCulture = culture;
+				this.#setCultureAwareValues();
+			});
+
+			this.observe(context?.appDefaultLanguage, (value) => {
+				this.#defaultCulture = value?.unique;
+				this.#setCultureAwareValues();
+			});
+		}).asPromise();
+	}
+
+	/**
+	 * Get the current data
+	 * @returns {Array<UmbDocumentUrlModel> | undefined} The current data
+	 * @memberof UmbDocumentUrlsDataResolver
+	 */
+	getData(): Array<UmbDocumentUrlModel> | undefined {
+		return this.#data;
+	}
+
+	/**
+	 * Set the current data
+	 * @param {Array<UmbDocumentUrlModel> | undefined} data The current data
+	 * @memberof UmbDocumentUrlsDataResolver
+	 */
+	setData(data: Array<UmbDocumentUrlModel> | undefined) {
+		this.#data = data;
+
+		if (!this.#data) {
+			this.#urls.setValue([]);
+			return;
+		}
+
+		this.#setCultureAwareValues();
+	}
+
+	/**
+	 * Get the urls for the current culture
+	 * @returns {(Promise<Array<UmbDocumentUrlModel> | []>)} The urls for the current culture
+	 * @memberof UmbDocumentUrlsDataResolver
+	 */
+	async getUrls(): Promise<Array<UmbDocumentUrlModel> | []> {
+		await this.#init;
+		return this.#urls.getValue();
+	}
+
+	#setCultureAwareValues() {
+		this.#setUrls();
+	}
+
+	#setUrls() {
+		const data = this.#getDataForCurrentCulture();
+		this.#urls.setValue(data ?? []);
+	}
+
+	#getCurrentCulture(): string | undefined {
+		return this.#propertyDataSetCulture?.culture || this.#appCulture || this.#defaultCulture;
+	}
+
+	#getDataForCurrentCulture(): Array<UmbDocumentUrlModel> | undefined {
+		const culture = this.#getCurrentCulture();
+		return this.#data?.filter((x) => x.culture === culture);
+	}
+}
