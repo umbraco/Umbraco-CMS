7dac2a5041e6cf1e432f80465714349b3d23f86e
Mads Rasmussen <madsr@hey.com>
2025-05-16 21:12:18 +0200

    add media global search

---
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/search/global-search/manifests.ts
@@ -0,0 +1,15 @@
+import { UMB_MEDIA_SEARCH_PROVIDER_ALIAS } from '../constants.js';
+import { UMB_MEDIA_GLOBAL_SEARCH_ALIAS } from './constants.js';
+
+export const manifests: Array<UmbExtensionManifest> = [
+	{
+		name: 'Media Global Search',
+		alias: UMB_MEDIA_GLOBAL_SEARCH_ALIAS,
+		type: 'globalSearch',
+		weight: 700,
+		meta: {
+			label: 'Media',
+			searchProvider: UMB_MEDIA_SEARCH_PROVIDER_ALIAS,
+		},
+	},
+];
