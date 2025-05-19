1fdeb4f251b795c1bc4e2813efcbcff0f259fd68
Mads Rasmussen <madsr@hey.com>
2025-05-19 10:50:12 +0200

    Add explicit Global Search extension point (#19348)
    * add global search extension
    
    * render global search extension in search modal
    
    * register document global search
    
    * add media global search
    
    * add data type global search
    
    * add dictionary global search
    
    * import manifests
    
    * register document type global search
    
    * add media type global search
    
    * register member global search
    
    * register member type global search
    
    * register template global search
    
    * export missing consts
    
    * export missing consts
    
    * export missing consts
    
    * add conditions

---
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/search/global-search/constants.ts
@@ -0,0 +1,1 @@
+export const UMB_DOCUMENT_GLOBAL_SEARCH_ALIAS = 'Umb.GlobalSearch.Document';
---
+++ src/Umbraco.Web.UI.Client/src/packages/templating/templates/search/constants.ts
@@ -0,0 +1,2 @@
+export * from './global-search/constants.js';
+export const UMB_TEMPLATE_SEARCH_PROVIDER_ALIAS = 'Umb.SearchProvider.Template';
