204b4800fa16d443498e262435fd1db9de5f84e9
Mads Rasmussen <madsr@hey.com>
2025-05-20 10:07:53 +0200

    use folder-workspace-editor for templating folders

--- src/Umbraco.Web.UI.Client/src/packages/templating/scripts/tree/folder/workspace/script-folder-workspace-editor.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/templating/scripts/tree/folder/workspace/script-folder-workspace-editor.element.ts
@@ -1,42 +1,11 @@
-import { UMB_SCRIPT_FOLDER_WORKSPACE_CONTEXT } from './script-folder-workspace.context-token.js';
-import { css, html, customElement, state } from '@umbraco-cms/backoffice/external/lit';
+import { html, customElement } from '@umbraco-cms/backoffice/external/lit';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
-import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
 
 @customElement('umb-script-folder-workspace-editor')
 export class UmbScriptFolderWorkspaceEditorElement extends UmbLitElement {
-	@state()
-	private _name = '';
-
-	#workspaceContext?: typeof UMB_SCRIPT_FOLDER_WORKSPACE_CONTEXT.TYPE;
-
-	constructor() {
-		super();
-
-		this.consumeContext(UMB_SCRIPT_FOLDER_WORKSPACE_CONTEXT, (workspaceContext) => {
-			this.#workspaceContext = workspaceContext;
-			this.#observeName();
-		});
-	}
-
-	#observeName() {
-		if (!this.#workspaceContext) return;
-		this.observe(
-			this.#workspaceContext.name,
-			(name) => {
-				if (name !== this._name) {
-					this._name = name ?? '';
-				}
-			},
-			'observeName',
-		);
-	}
-
 	override render() {
-		return html`<umb-workspace-editor headline=${this._name}> </umb-workspace-editor>`;
+		return html`<umb-folder-workspace-editor></umb-folder-workspace-editor>`;
 	}
-
-	static override styles = [UmbTextStyles, css``];
 }
 
 export { UmbScriptFolderWorkspaceEditorElement as element };
