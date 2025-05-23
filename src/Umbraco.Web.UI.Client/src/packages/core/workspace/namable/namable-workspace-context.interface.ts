1b40aef53f032b51e7f17dff6800c4da1db70d2e
Mads Rasmussen <madsr@hey.com>
2025-05-23 15:38:38 +0200

    introduce name write guard manager

--- src/Umbraco.Web.UI.Client/src/packages/core/workspace/namable/namable-workspace-context.interface.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/workspace/namable/namable-workspace-context.interface.ts
@@ -1,8 +1,10 @@
 import type { UmbWorkspaceContext } from '../workspace-context.interface.js';
+import type { UmbNameWriteGuardManager } from './name-write-guard.manager.js';
 import type { Observable } from '@umbraco-cms/backoffice/external/rxjs';
 
 export interface UmbNamableWorkspaceContext extends UmbWorkspaceContext {
 	name: Observable<string | undefined>;
 	getName(): string | undefined;
 	setName(name: string): void;
+	nameWriteGuard: UmbNameWriteGuardManager;
 }
