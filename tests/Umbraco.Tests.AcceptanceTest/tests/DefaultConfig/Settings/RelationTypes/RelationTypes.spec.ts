9cc3ff60e185a616864b68292bb1581f22fd5353
Andreas Zerbst <andr317c@live.dk>
2025-06-03 14:45:13 +0200

    Updated relation type tests

---
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Settings/RelationTypes/RelationTypes.spec.ts
@@ -0,0 +1,125 @@
+﻿import {test} from '@umbraco/playwright-testhelpers';
+
+test.beforeEach(async ({umbracoUi}) => {
+  await umbracoUi.goToBackOffice();
+  await umbracoUi.relationType.goToSettingsTreeItem('Relations');
+});
+
+const relationTypes = [
+  {
+    name: 'Relate Document On Copy',
+    parentType: 'Document',
+    childType: 'Document',
+    biDirectional: 'true',
+    dependency: 'false'
+  },
+  {
+    name: 'Relate Parent Document On Delete',
+    parentType: 'Document',
+    childType: 'Document',
+    biDirectional: 'false',
+    dependency: 'false'
+  },
+  {
+    name: 'Relate Parent Media Folder On Delete',
+    parentType: 'Media',
+    childType: 'Media',
+    biDirectional: 'false',
+    dependency: 'false'
+  },
+  {name: 'Related Document', parentType: '', childType: '', biDirectional: 'false', dependency: 'true'},
+  {name: 'Related Media', parentType: '', childType: '', biDirectional: 'false', dependency: 'true'},
+  {name: 'Related Member', parentType: '', childType: '', biDirectional: 'false', dependency: 'true'}
+];
+
+for (const relationType of relationTypes) {
+  test(`can see relation type ${relationType.name}`, async ({umbracoUi}) => {
+    // Act
+    await umbracoUi.relationType.goToRelationTypeWithName(relationType.name);
+
+    // Assert
+    await umbracoUi.relationType.doesParentTypeContainValue(relationType.parentType)
+    await umbracoUi.relationType.doesChildTypeContainValue(relationType.childType)
+    await umbracoUi.relationType.doesBidirectionalContainValue(relationType.biDirectional)
+    await umbracoUi.relationType.doesDependencyContainValue(relationType.dependency)
+  });
+}
+
+
+test('can see related document in relation type', async ({umbracoApi, umbracoUi}) => {
+  // Arrange
+  // Content Picker
+  const contentPickerName = 'Content Picker'
+  const contentPickerData = await umbracoApi.dataType.getByName(contentPickerName)
+  // Document Type
+  const documentTypeName = 'TestDocumentType'
+  const documentTypeId = await umbracoApi.documentType.createDocumentTypeWithPropertyEditor(documentTypeName, contentPickerName, contentPickerData.id)
+  // Content
+  const contentName = 'TestContent'
+  const contentToBePickedName = 'ContentToBePicker'
+  const contentToBePickedId = await umbracoApi.document.createDefaultDocument(contentToBePickedName, documentTypeId)
+  await umbracoApi.document.createDocumentWithContentPicker(contentName, documentTypeId, contentToBePickedId)
+
+  // Act
+  await umbracoUi.relationType.goToRelationTypeWithName('Related Document');
+
+  // Assert
+  await umbracoUi.relationType.isRelationWithParentAndChildVisible(contentName, contentToBePickedName);
+
+  // Clean
+  await umbracoApi.documentType.ensureNameNotExists(documentTypeName);
+});
+
+test('can see related media in relation type', async ({umbracoApi, umbracoUi}) => {
+  // Arrange
+  // Media Picker
+  const mediaPickerName = 'Media Picker';
+  const mediaPickerData = await umbracoApi.dataType.getByName(mediaPickerName);
+  // Media
+  const mediaName = 'TestMedia';
+  const mediaFileId = await umbracoApi.media.createDefaultMediaFile(mediaName);
+  // Document Type
+  const documentTypeName = 'TestDocumentType';
+  const documentTypeId = await umbracoApi.documentType.createDocumentTypeWithPropertyEditor(documentTypeName, mediaPickerName, mediaPickerData.id);
+  // Content
+  const contentName = 'TestContent';
+  await umbracoApi.document.createDocumentWithOneMediaPicker(contentName, documentTypeId, mediaFileId);
+
+  // Act
+  await umbracoUi.relationType.goToRelationTypeWithName('Related Media');
+
+  // Assert
+  await umbracoUi.relationType.isRelationWithParentAndChildVisible(contentName, mediaName);
+
+  // Clean
+  await umbracoApi.media.ensureNameNotExists(mediaName);
+  await umbracoApi.documentType.ensureNameNotExists(documentTypeName);
+});
+
+test('can see related member in relation type', async ({umbracoApi, umbracoUi}) => {
+  // Arrange
+  // MemberPicker
+  const memberPickerName = 'Member Picker';
+  const memberPickerData = await umbracoApi.dataType.getByName(memberPickerName);
+  // Member
+  const memberTypeData = await umbracoApi.memberType.getByName('Member');
+  const memberName = 'TestMember';
+  const memberEmail = 'TestMemberEmail@test.com';
+  const memberId = await umbracoApi.member.createDefaultMember(memberName, memberTypeData.id, memberEmail, memberEmail, memberEmail);
+  // DocumentType
+  const documentTypeName = 'TestDocumentType';
+  const documentTypeId = await umbracoApi.documentType.createDocumentTypeWithPropertyEditor(documentTypeName, memberPickerName, memberPickerData.id);
+  // Content
+  const contentName = 'TestContent';
+  await umbracoApi.document.createDocumentWithMemberPicker(contentName, documentTypeId, memberId);
+
+  // Act
+  await umbracoUi.relationType.goToRelationTypeWithName('Related Member');
+
+  // Assert
+  await umbracoUi.relationType.isRelationWithParentAndChildVisible(contentName, memberName);
+
+  // Clean
+  await umbracoApi.member.ensureNameNotExists(memberName);
+  await umbracoApi.documentType.ensureNameNotExists(documentTypeName);
+});
