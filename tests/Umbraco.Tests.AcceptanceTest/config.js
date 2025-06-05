c7dcbfec603ed95ca2511e570fce3c1d974e5924
Andreas Zerbst <73799582+andr317c@users.noreply.github.com>
2025-06-05 13:43:00 +0200

    V16 QA Publish Console Error Logs (#19417)
    * Updated configuration
    
    * Updated pipeline
    
    * Updated nightly pipeline
    
    * Bumped version
    
    * updated if statement

--- tests/Umbraco.Tests.AcceptanceTest/config.js
+++ tests/Umbraco.Tests.AcceptanceTest/config.js
@@ -2,45 +2,48 @@ const prompt = require('prompt');
 const fs = require('fs');
 
 const properties = [
-    {
-        description: 'Enter your Umbraco superadmin username/email',
-        name: 'username',
-        required: true
-    },
-    {
-        description: 'Enter your Umbraco superadmin password',
-        name: 'password',
-        hidden: true,
-        required: true
-    },
-    {
-        description: 'Enter CMS URL, or leave empty for default(https://localhost:44339)',
-        name: 'baseUrl'
-    }
+  {
+    description: 'Enter your Umbraco superadmin username/email',
+    name: 'username',
+    required: true
+  },
+  {
+    description: 'Enter your Umbraco superadmin password',
+    name: 'password',
+    hidden: true,
+    required: true
+  },
+  {
+    description: 'Enter CMS URL, or leave empty for default(https://localhost:44339)',
+    name: 'baseUrl'
+  }
 ];
 
+const configPath = './.env';
 
-const configPath = './.env'
-
-console.log("Configure your Umbraco test environment")
+console.log("Configure your Umbraco test environment");
 
 prompt.start();
 
 prompt.get(properties, function (error, result) {
-    if (error) { return onError(error); }
+  if (error) {
+    return onError(error);
+  }
 
-var fileContent = `UMBRACO_USER_LOGIN=${result.username}
+  const fileContent = `UMBRACO_USER_LOGIN=${result.username}
 UMBRACO_USER_PASSWORD=${result.password}
 URL=${result.baseUrl || "https://localhost:44339"}
-STORAGE_STAGE_PATH=${__dirname.replace(/\\/g,'/')}/playwright/.auth/user.json`;
+STORAGE_STAGE_PATH=${__dirname.replace(/\\/g, '/')}/playwright/.auth/user.json
+CONSOLE_ERRORS_PATH=${__dirname.replace(/\\/g, '/')}/console-errors.json`;
 
-    fs.writeFile(configPath, fileContent, function (error) {
-        if (error) return console.error(error);
-        console.log('Configuration saved');
-    });
+
+  fs.writeFile(configPath, fileContent, function (error) {
+    if (error) return console.error(error);
+    console.log('Configuration saved');
+  });
 });
 
 function onError(error) {
-    console.error(error);
-    return true;
+  console.error(error);
+  return true;
 }
