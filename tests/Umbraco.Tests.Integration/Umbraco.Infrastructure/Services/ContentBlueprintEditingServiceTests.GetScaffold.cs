94659db533e01947ca11786e27353635289ceaa4
Laura Neto <12862535+lauraneto@users.noreply.github.com>
2025-06-11 10:15:35 +0200

    Added integration tests to verify that new block ids are generated when creating content from a blueprint

--- tests/Umbraco.Tests.Integration/Umbraco.Infrastructure/Services/ContentBlueprintEditingServiceTests.GetScaffold.cs
+++ tests/Umbraco.Tests.Integration/Umbraco.Infrastructure/Services/ContentBlueprintEditingServiceTests.GetScaffold.cs
@@ -1,6 +1,9 @@
 using NUnit.Framework;
+using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Events;
+using Umbraco.Cms.Core.Models.Blocks;
 using Umbraco.Cms.Core.Notifications;
+using Umbraco.Cms.Core.PropertyEditors;
 using Umbraco.Cms.Tests.Integration.Attributes;
 
 namespace Umbraco.Cms.Tests.Integration.Umbraco.Infrastructure.Services;
@@ -8,7 +11,10 @@ namespace Umbraco.Cms.Tests.Integration.Umbraco.Infrastructure.Services;
 public partial class ContentBlueprintEditingServiceTests
 {
     public static void AddScaffoldedNotificationHandler(IUmbracoBuilder builder)
-        => builder.AddNotificationHandler<ContentScaffoldedNotification, ContentScaffoldedNotificationHandler>();
+        => builder
+            .AddNotificationHandler<ContentScaffoldedNotification, ContentScaffoldedNotificationHandler>()
+            .AddNotificationHandler<ContentScaffoldedNotification, BlockListPropertyNotificationHandler>()
+            .AddNotificationHandler<ContentScaffoldedNotification, BlockGridPropertyNotificationHandler>();
 
     [TestCase(true)]
     [TestCase(false)]
@@ -49,6 +55,58 @@ public partial class ContentBlueprintEditingServiceTests
     {
         var result = await ContentBlueprintEditingService.GetScaffoldedAsync(Guid.NewGuid());
         Assert.IsNull(result);
+    }
+
+    [TestCase(false, Constants.PropertyEditors.Aliases.BlockList)]
+    [TestCase(false, Constants.PropertyEditors.Aliases.BlockGrid)]
+    [TestCase(false, Constants.PropertyEditors.Aliases.RichText)]
+    [TestCase(true, Constants.PropertyEditors.Aliases.BlockList)]
+    [TestCase(true, Constants.PropertyEditors.Aliases.BlockGrid)]
+    [TestCase(true, Constants.PropertyEditors.Aliases.RichText)]
+    [ConfigureBuilder(ActionName = nameof(AddScaffoldedNotificationHandler))]
+    public async Task Get_Scaffold_With_Blocks_Generates_New_Block_Ids(bool variant, string editorAlias)
+    {
+        var blueprint = await CreateBlueprintWithBlocksEditor(variant, editorAlias);
+        var result = await ContentBlueprintEditingService.GetScaffoldedAsync(blueprint.Content.Key);
+        Assert.IsNotNull(result);
+        Assert.AreNotEqual(blueprint.Content.Key, result.Key);
+
+        List<Guid> newKeys = [];
+        var newInvariantBlocklist = GetBlockValue("invariantBlocks");
+        newKeys.AddRange(
+            newInvariantBlocklist.Layout
+            .SelectMany(x => x.Value)
+            .SelectMany(v => new List<Guid> { v.ContentKey, v.SettingsKey!.Value }));
+
+        if (variant)
+        {
+            foreach (var culture in result.AvailableCultures)
+            {
+                var newVariantBlocklist = GetBlockValue("blocks", culture);
+                newKeys.AddRange(
+                    newVariantBlocklist.Layout
+                        .SelectMany(x => x.Value)
+                        .SelectMany(v => new List<Guid> { v.ContentKey, v.SettingsKey!.Value }));
+            }
+        }
+
+        foreach (var newKey in newKeys)
+        {
+            Assert.IsTrue(!blueprint.BlockKeys.Contains(newKey), "The blocks in a content item generated from a template should have new keys.");
+        }
+
+        return;
+
+        BlockValue GetBlockValue(string propertyAlias, string? culture = null)
+        {
+            return editorAlias switch
+            {
+                Constants.PropertyEditors.Aliases.BlockList => JsonSerializer.Deserialize<BlockListValue>(result.GetValue<string>(propertyAlias, culture)),
+                Constants.PropertyEditors.Aliases.BlockGrid => JsonSerializer.Deserialize<BlockGridValue>(result.GetValue<string>(propertyAlias, culture)),
+                Constants.PropertyEditors.Aliases.RichText => JsonSerializer.Deserialize<RichTextEditorValue>(result.GetValue<string>(propertyAlias, culture)).Blocks!,
+                _ => throw new NotSupportedException($"Editor alias '{editorAlias}' is not supported for block blueprints."),
+            };
+        }
     }
 
     public class ContentScaffoldedNotificationHandler : INotificationHandler<ContentScaffoldedNotification>
