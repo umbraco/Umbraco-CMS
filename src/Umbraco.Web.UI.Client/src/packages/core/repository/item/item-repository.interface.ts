e89e18f5bae5903b55faafc6389e650c0c0e81db
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-06-12 12:59:11 +0200

    V16: Item and Detail Base Repository should use correct typings for return types (#19447)
    * fix: add a catcher to most `asPromise` for stores to prevent cascading errors
    
    * fix: remove conditional instances - they should be able to be undefined
    
    * fix: check for missing store and extract UmbProblemDetails
    
    * fix: only append data if no error
    
    * fix: adds error handling to missing stores and to extract the ProblemDetails object
    
    * revert commit
    
    * fix: ignore errors completely instead of unsetting stores
    
    * revert commit
    
    * chore: cleanup imports
    
    * fix: do not unset store
    
    * stop observation in a proper way
    
    * stop observation of for document-user-permissions
    
    * check for manager twice
    
    * save action
    
    * save action optional
    
    * fix: ensure the right types are used for base stores
    
    ---------
    
    Co-authored-by: Niels Lyngsø <niels.lyngso@gmail.com>

--- src/Umbraco.Web.UI.Client/src/packages/core/repository/item/item-repository.interface.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/repository/item/item-repository.interface.ts
@@ -1,12 +1,8 @@
+import type { UmbRepositoryResponseWithAsObservable } from '../types.js';
 import type { Observable } from '@umbraco-cms/backoffice/external/rxjs';
 import type { UmbApi } from '@umbraco-cms/backoffice/extension-api';
-import type { UmbProblemDetails } from '@umbraco-cms/backoffice/resources';
 
 export interface UmbItemRepository<ItemType> extends UmbApi {
-	requestItems: (uniques: string[]) => Promise<{
-		data?: Array<ItemType> | undefined;
-		error?: UmbProblemDetails | undefined;
-		asObservable?: () => Observable<Array<ItemType>>;
-	}>;
+	requestItems: (uniques: string[]) => Promise<UmbRepositoryResponseWithAsObservable<ItemType[] | undefined>>;
 	items: (uniques: string[]) => Promise<Observable<Array<ItemType>> | undefined>;
 }
