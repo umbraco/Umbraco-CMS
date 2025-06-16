dd381577bf4daba621651b7d58f4b085c7c72957
Mads Rasmussen <madsr@hey.com>
2025-06-16 21:26:55 +0200

    add manifest meta types

--- src/Umbraco.Web.UI.Client/src/packages/core/dashboard/dashboard-app.extension.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/dashboard/dashboard-app.extension.ts
@@ -4,6 +4,12 @@ export interface ManifestDashboardApp
 	extends ManifestElement,
 		ManifestWithDynamicConditions<UmbExtensionConditionConfig> {
 	type: 'dashboardApp';
+	meta: MetaDashboardApp;
+}
+
+export interface MetaDashboardApp {
+	headline: string;
+	size: 'small' | 'medium' | 'large';
 }
 
 declare global {
