0308156d788bb73286847fe92f19141778e91028
Laura Neto <12862535+lauraneto@users.noreply.github.com>
2025-06-24 11:22:36 +0200

    Added missing scope

--- src/Umbraco.Infrastructure/BackgroundJobs/Jobs/CacheInstructionsPruningJob.cs
+++ src/Umbraco.Infrastructure/BackgroundJobs/Jobs/CacheInstructionsPruningJob.cs
@@ -1,6 +1,8 @@
 using Microsoft.Extensions.Options;
 using Umbraco.Cms.Core.Configuration.Models;
 using Umbraco.Cms.Core.Persistence.Repositories;
+using Umbraco.Cms.Core.Scoping;
+using Umbraco.Cms.Infrastructure.Scoping;
 
 namespace Umbraco.Cms.Infrastructure.BackgroundJobs.Jobs;
 
@@ -11,21 +13,25 @@ public class CacheInstructionsPruningJob : IRecurringBackgroundJob
 {
     private readonly IOptions<GlobalSettings> _globalSettings;
     private readonly ICacheInstructionRepository _cacheInstructionRepository;
+    private readonly ICoreScopeProvider _scopeProvider;
     private readonly TimeProvider _timeProvider;
 
     /// <summary>
     /// Initializes a new instance of the <see cref="CacheInstructionsPruningJob"/> class.
     /// </summary>
+    /// <param name="scopeProvider">Provides scopes for database operations.</param>
     /// <param name="globalSettings">The global settings configuration.</param>
     /// <param name="cacheInstructionRepository">The repository for cache instructions.</param>
     /// <param name="timeProvider">The time provider.</param>
     public CacheInstructionsPruningJob(
         IOptions<GlobalSettings> globalSettings,
         ICacheInstructionRepository cacheInstructionRepository,
+        ICoreScopeProvider scopeProvider,
         TimeProvider timeProvider)
     {
         _globalSettings = globalSettings;
         _cacheInstructionRepository = cacheInstructionRepository;
+        _scopeProvider = scopeProvider;
         _timeProvider = timeProvider;
         Period = globalSettings.Value.DatabaseServerMessenger.TimeBetweenPruneOperations;
     }
@@ -44,7 +50,12 @@ public class CacheInstructionsPruningJob : IRecurringBackgroundJob
     public Task RunJobAsync()
     {
         DateTimeOffset pruneDate = _timeProvider.GetUtcNow() - _globalSettings.Value.DatabaseServerMessenger.TimeToRetainInstructions;
-        _cacheInstructionRepository.DeleteInstructionsOlderThan(pruneDate.DateTime);
+        using (ICoreScope scope = _scopeProvider.CreateCoreScope())
+        {
+            _cacheInstructionRepository.DeleteInstructionsOlderThan(pruneDate.DateTime);
+            scope.Complete();
+        }
+
         return Task.CompletedTask;
     }
 }
