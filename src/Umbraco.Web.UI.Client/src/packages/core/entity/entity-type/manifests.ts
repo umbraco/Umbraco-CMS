035a8b74536ce1b873452692e28141631b1bfd1f
Mads Rasmussen <madsr@hey.com>
2025-06-26 15:40:00 +0200

    Update manifests.ts

--- src/Umbraco.Web.UI.Client/src/packages/core/entity/entity-type/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/entity/entity-type/manifests.ts
@@ -3,7 +3,7 @@ import { UMB_ENTITY_TYPE_CONDITION_ALIAS } from './constants.js';
 export const manifests: Array<UmbExtensionManifest> = [
 	{
 		type: 'condition',
-		name: 'Umbraco Entity Unique Condition',
+		name: 'Umbraco Entity Type Condition',
 		alias: UMB_ENTITY_TYPE_CONDITION_ALIAS,
 		api: () => import('./entity-type.condition.js'),
 	},
