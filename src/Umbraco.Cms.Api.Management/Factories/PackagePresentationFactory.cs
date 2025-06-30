2cb114ffafefdc7b1a082bb36a1b6a0cc2875da7
Andy Butland <abutland73@gmail.com>
2025-06-30 12:20:11 +0200

    Fix check for pending package migration to use the package not the plan name (#19509)
    * Fix check for pending package migration to use the package not plan name.
    
    * Cover all package name/identifier permutations and fix the API output for multiple plans
    
    * Adjusted log message to not refer to unattended migrations as migrations may be being run attended.
    
    ---------
    
    Co-authored-by: Kenn Jacobsen <kja@umbraco.dk>

--- src/Umbraco.Cms.Api.Management/Factories/PackagePresentationFactory.cs
+++ src/Umbraco.Cms.Api.Management/Factories/PackagePresentationFactory.cs
@@ -1,10 +1,12 @@
 using System.Collections.Specialized;
 using System.Web;
 using Microsoft.Extensions.Options;
+using Umbraco.Cms.Api.Common.ViewModels.Pagination;
 using Umbraco.Cms.Api.Management.ViewModels.Package;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Configuration.Models;
 using Umbraco.Cms.Core.Mapping;
+using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Packaging;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Extensions;
@@ -41,6 +43,25 @@ internal class PackagePresentationFactory : IPackagePresentationFactory
         {
             MarketplaceUrl = GetMarketplaceUrl(),
         };
+
+    public PagedViewModel<PackageMigrationStatusResponseModel> CreatePackageMigrationStatusResponseModel(PagedModel<InstalledPackage> installedPackages)
+    {
+        InstalledPackage[] installedPackagesAsArray = installedPackages.Items as InstalledPackage[] ?? installedPackages.Items.ToArray();
+
+        return new PagedViewModel<PackageMigrationStatusResponseModel>
+        {
+            Total = installedPackages.Total,
+            Items = installedPackagesAsArray
+                .GroupBy(package => package.PackageName)
+                .Select(packages => packages.OrderByDescending(package => package.HasPendingMigrations).First())
+                .Select(package => new PackageMigrationStatusResponseModel
+                {
+                    PackageName = package.PackageName ?? string.Empty,
+                    HasPendingMigrations = package.HasPendingMigrations,
+                })
+                .ToArray(),
+        };
+    }
 
     private string GetMarketplaceUrl()
     {
