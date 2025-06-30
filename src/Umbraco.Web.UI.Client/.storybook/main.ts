998f09a320b33d0c558e7d16746048f1ecc5ccc0
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-06-30 15:27:27 +0200

    chore: run storybook v9 migrations

--- src/Umbraco.Web.UI.Client/.storybook/main.ts
+++ src/Umbraco.Web.UI.Client/.storybook/main.ts
@@ -4,12 +4,12 @@ import remarkGfm from 'remark-gfm';
 
 const config: StorybookConfig = {
 	stories: ['../@(src|libs|apps|storybook)/**/*.mdx', '../@(src|libs|apps|storybook)/**/*.stories.@(js|jsx|ts|tsx)'],
+
 	addons: [
 		getAbsolutePath('@storybook/addon-links'),
-		getAbsolutePath('@storybook/addon-essentials'),
 		getAbsolutePath('@storybook/addon-a11y'),
 		{
-			name: '@storybook/addon-docs',
+			name: getAbsolutePath('@storybook/addon-docs'),
 			options: {
 				mdxPluginOptions: {
 					mdxCompileOptions: {
@@ -19,10 +19,12 @@ const config: StorybookConfig = {
 			},
 		},
 	],
+
 	framework: {
 		name: getAbsolutePath('@storybook/web-components-vite'),
 		options: {},
 	},
+
 	staticDirs: [
 		'../public-assets',
 		'../public',
@@ -32,12 +34,11 @@ const config: StorybookConfig = {
 			to: 'assets/icons',
 		},
 	],
+
 	typescript: {
 		check: true,
 	},
-	docs: {
-        autodocs: true
-    },
+
 	managerHead(head, { configType }) {
 		const base = process.env.VITE_BASE_PATH || '/';
 		const injections = [
@@ -45,6 +46,7 @@ const config: StorybookConfig = {
 		];
 		return configType === 'PRODUCTION' ? `${injections.join('')}${head}` : head;
 	},
+
 	refs: {
 		uui: {
 			title: 'Umbraco UI Library',
