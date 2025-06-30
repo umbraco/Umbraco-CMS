927a53423a8e6e5be8fc22db6c5435a529da1c04
Mads Rasmussen <madsr@hey.com>
2025-06-30 09:26:32 +0200

    remove unused

--- src/Umbraco.Web.UI.Client/src/packages/core/entity/entity-type/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/entity/entity-type/types.ts
@@ -2,7 +2,6 @@ import type { UMB_ENTITY_TYPE_CONDITION_ALIAS } from './constants.js';
 import type { UmbConditionConfigBase } from '@umbraco-cms/backoffice/extension-api';
 
 export type UmbEntityTypeConditionConfig = UmbConditionConfigBase<typeof UMB_ENTITY_TYPE_CONDITION_ALIAS> & {
-	allOf?: Array<string>;
 	match: string;
 	oneOf?: Array<string>;
 };
