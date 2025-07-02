8a146e82f5f210e723b18d2ad812059cf9788931
Mads Rasmussen <madsr@hey.com>
2025-07-02 12:23:07 +0200

    add collapsed and expanded events

--- src/Umbraco.Web.UI.Client/src/packages/core/utils/expansion/events/entity-collapsed.event.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/utils/expansion/events/entity-collapsed.event.ts
@@ -1,12 +1,12 @@
 import type { UmbEntityModel } from '@umbraco-cms/backoffice/entity';
 
-export class UmbEntityExpandedEvent extends Event {
-	public static readonly TYPE = 'entity-expanded';
+export class UmbEntityCollapsedEvent extends Event {
+	public static readonly TYPE = 'entity-collapsed';
 	entity: UmbEntityModel | undefined;
 
 	public constructor(entity?: UmbEntityModel) {
 		// mimics the native change event
-		super(UmbEntityExpandedEvent.TYPE, { bubbles: true, composed: false, cancelable: false });
+		super(UmbEntityCollapsedEvent.TYPE, { bubbles: true, composed: false, cancelable: false });
 		this.entity = entity;
 	}
 }
