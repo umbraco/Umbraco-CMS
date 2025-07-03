e148141b970c48737c7afed6470c179269a88334
Mads Rasmussen <madsr@hey.com>
2025-07-03 22:15:54 +0200

    add stack example

--- src/Umbraco.Web.UI.Client/src/packages/core/components/stack/stack.stories.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/components/stack/stack.stories.ts
@@ -1,19 +1,22 @@
-import type { Meta, StoryObj } from '@storybook/web-components-vite';
-import './stack.element.js';
 import type { UmbStackElement } from './stack.element.js';
+import type { Meta, StoryObj } from '@storybook/web-components-vite';
+import { html } from '@umbraco-cms/backoffice/external/lit';
+
+import './stack.element.js';
 
 const meta: Meta<UmbStackElement> = {
 	title: 'Generic Components/Stack',
 	component: 'umb-stack',
+	render: (args) =>
+		html`<umb-stack .divide=${args.divide} .look=${args.look}>
+			<div>Element 1</div>
+			<div>Element 2</div>
+			<div>Element 3</div>
+		</umb-stack>`,
 };
 
 export default meta;
-
 type Story = StoryObj<UmbStackElement>;
-
-export const Default: Story = {
-	args: {},
-};
 
 export const Divide: Story = {
 	args: {
