0627c69225f9303453de32f377df285866335a38
Mads Rasmussen <madsr@hey.com>
2025-07-04 09:15:50 +0200

    Update icon.stories.ts

--- src/Umbraco.Web.UI.Client/src/packages/core/components/icon/icon.stories.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/components/icon/icon.stories.ts
@@ -1,6 +1,7 @@
-import type { Meta, StoryObj } from '@storybook/web-components-vite';
-import './icon.element.js';
 import type { UmbIconElement } from './icon.element.js';
+import type { Meta, StoryObj } from '@storybook/web-components-vite';
+
+import './icon.element.js';
 
 const meta: Meta<UmbIconElement> = {
 	title: 'Generic Components/Icon/Icon',
