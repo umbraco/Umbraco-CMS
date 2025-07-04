b6c28c4b39e31428abc30ab8f305be6c5da282cd
Mads Rasmussen <madsr@hey.com>
2025-07-04 08:18:49 +0200

    Create data-type-input.stories.ts

---
+++ src/Umbraco.Web.UI.Client/src/packages/data-type/components/data-type-input/data-type-input.stories.ts
@@ -0,0 +1,16 @@
+import type { UmbDataTypeInputElement } from './data-type-input.element.js';
+import type { Meta, StoryObj } from '@storybook/web-components-vite';
+import './data-type-input.element.js';
+import { html } from '@umbraco-cms/backoffice/external/lit';
+
+const meta: Meta<UmbDataTypeInputElement> = {
+	title: 'Entity/Data Type/Components/Input Data Type',
+	component: 'umb-data-type-input',
+	render: () => html`<umb-data-type-input></umb-data-type-input>`,
+};
+
+export default meta;
+type Story = StoryObj<UmbDataTypeInputElement>;
+export const Overview: Story = {
+	args: {},
+};
