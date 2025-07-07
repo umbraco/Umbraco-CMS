fe0820e00974bdf9b8dd6b214771fbbbd4e3e88a
Migaroez <geusens@gmail.com>
2025-07-07 12:36:03 +0200

    Cover positive case and make test case output more readable

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/Services/EntityServiceTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Services/EntityServiceTests.cs
@@ -13,14 +13,18 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Core.Services;
 [TestFixture]
 public class EntityServiceTests
 {
-    [TestCase(-1, 1)]
-    [TestCase(1, -1)]
-    [TestCase(-1, -1)]
-    public void Siblings_Invalid_Index(int before, int after)
+    [TestCase(1, 1, false, TestName = "Siblings_Index_Validation_Valid")]
+    [TestCase(-1, 1, true, TestName = "Siblings_Index_Validation_InvalidBefore")]
+    [TestCase(1, -1, true, TestName = "Siblings_Index_Validation_InvalidAfter")]
+    [TestCase(-1, -1, true, TestName = "Siblings_Index_Validation_InvalidBeforeAndAfter")]
+    public void Siblings_Index_Validation(int before, int after, bool shouldThrow)
     {
         var sut = CreateEntityService();
 
-        Assert.Throws<ArgumentOutOfRangeException>(() => sut.GetSiblings(Guid.NewGuid(), UmbracoObjectTypes.Document, before, after));
+        if (shouldThrow)
+        {
+            Assert.Throws<ArgumentOutOfRangeException>(() => sut.GetSiblings(Guid.NewGuid(), UmbracoObjectTypes.Document, before, after));
+        }
     }
 
 
