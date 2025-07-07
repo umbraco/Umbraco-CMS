9fb65c2c4922270c1a4bd3db33d322e08b58727b
Migaroez <geusens@gmail.com>
2025-07-07 12:35:30 +0200

    Add test covered requirement to description

--- src/Umbraco.Core/Services/IEntityService.cs
+++ src/Umbraco.Core/Services/IEntityService.cs
@@ -175,8 +175,8 @@ public interface IEntityService
     /// </summary>
     /// <param name="key">The key of the target entity whose siblings are to be retrieved.</param>
     /// <param name="objectType">The object type key of the entities.</param>
-    /// <param name="before">The number of siblings to retrieve before the target entity.</param>
-    /// <param name="after">The number of siblings to retrieve after the target entity.</param>
+    /// <param name="before">The number of siblings to retrieve before the target entity. Needs to be greater or equal to 0.</param>
+    /// <param name="after">The number of siblings to retrieve after the target entity. Needs to be greater or equal to 0.</param>
     /// <param name="ordering">The ordering to apply to the siblings.</param>
     /// <returns>Enumerable of sibling entities.</returns>
     IEnumerable<IEntitySlim> GetSiblings(
