3d13e4ff23eb0a4338d4e1835c070d0073237c8b
leekelleher <leekelleher@gmail.com>
2025-07-08 14:04:21 +0100

    Capitalizing the JS in `UmbUfmJsMarkedExtensionApi` class name
    for consistency and improved readability.

--- src/Umbraco.Web.UI.Client/src/packages/ufm/extensions/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/ufm/extensions/types.ts
@@ -3,4 +3,4 @@ export type * from './ufm-filter.extension.js';
 export type * from './ufm-component.extension.js';
 
 export type { UmbUfmMarkedExtensionApi } from './ufm-marked-extension.api.js';
-export type { UmbUfmjsMarkedExtensionApi } from './ufmjs-marked-extension.api.js';
+export type { UmbUfmJsMarkedExtensionApi } from './ufmjs-marked-extension.api.js';
--- src/Umbraco.Web.UI.Client/src/packages/ufm/extensions/ufmjs-marked-extension.api.ts
+++ src/Umbraco.Web.UI.Client/src/packages/ufm/extensions/ufmjs-marked-extension.api.ts
@@ -2,7 +2,7 @@ import type { UmbMarkedExtensionApi } from './marked-extension.extension.js';
 import type { Marked, Tokens } from '@umbraco-cms/backoffice/external/marked';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 
-export class UmbUfmjsMarkedExtensionApi implements UmbMarkedExtensionApi {
+export class UmbUfmJsMarkedExtensionApi implements UmbMarkedExtensionApi {
 	constructor(_host: UmbControllerHost, marked: Marked) {
 		marked.use({
 			extensions: [
@@ -38,4 +38,4 @@ export class UmbUfmjsMarkedExtensionApi implements UmbMarkedExtensionApi {
 	destroy() {}
 }
 
-export default UmbUfmjsMarkedExtensionApi;
+export default UmbUfmJsMarkedExtensionApi;
