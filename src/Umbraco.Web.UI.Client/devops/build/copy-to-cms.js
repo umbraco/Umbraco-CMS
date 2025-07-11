4c8e4706cd75671c6a47a2a4dec00a1c4dd2e5e4
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-07-11 14:40:59 +0200

    feat: copies the backoffice static assets

--- src/Umbraco.Web.UI.Client/devops/build/copy-to-cms.js
+++ src/Umbraco.Web.UI.Client/devops/build/copy-to-cms.js
@@ -6,4 +6,14 @@ const outputDir = '../Umbraco.Cms.StaticAssets/wwwroot/umbraco/backoffice';
 rmSync(outputDir, { recursive: true, force: true });
 cpSync(srcDir, outputDir, { recursive: true });
 
+// Copy assets
+console.log('--- Copying assets ---');
+cpSync('./src/assets', `${outputDir}/assets`, { recursive: true });
+console.log('--- Copying assets done ---');
+
+// Copy SRC CSS
+console.log('--- Copying src CSS ---');
+cpSync('./src/css', `${outputDir}/css`, { recursive: true });
+console.log('--- Copying src CSS done ---');
+
 console.log('--- Copied build output to CMS successfully. ---');
