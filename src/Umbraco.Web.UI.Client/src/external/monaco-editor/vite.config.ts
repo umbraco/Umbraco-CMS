5e6214bc585e05ac8170eafd6eb1c7a65c5e880a
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-07-11 14:43:12 +0200

    feat: copies the correct monaco-editor assets

--- src/Umbraco.Web.UI.Client/src/external/monaco-editor/vite.config.ts
+++ src/Umbraco.Web.UI.Client/src/external/monaco-editor/vite.config.ts
@@ -1,11 +1,17 @@
 import { defineConfig } from 'vite';
-import { rmSync } from 'fs';
+import { cpSync, rmSync } from 'fs';
 import { getDefaultConfig } from '../../vite-config-base';
 
 const dist = '../../../dist-cms/external/monaco-editor';
+const distAssets = '../../../dist-cms';
 
 // delete the unbundled dist folder
 rmSync(dist, { recursive: true, force: true });
+
+// copy fonts
+cpSync('../../../node_modules/monaco-editor/min/vs/base/browser/ui/codicons', `${distAssets}/assets/fonts`, {
+	recursive: true,
+});
 
 export default defineConfig({
 	...getDefaultConfig({
