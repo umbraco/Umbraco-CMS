013a4e5fafa8bad6106c6a2deb1172c2661e8cda
Andy Butland <abutland73@gmail.com>
2025-07-11 15:12:34 +0200

    Return 404 on delivery API requests for segments that are invalid or not created.

--- src/Umbraco.Core/DeliveryApi/ApiContentBuilderBase.cs
+++ src/Umbraco.Core/DeliveryApi/ApiContentBuilderBase.cs
@@ -1,4 +1,6 @@
-﻿using Umbraco.Cms.Core.Models.DeliveryApi;
+using Microsoft.Extensions.DependencyInjection;
+using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.Models.DeliveryApi;
 using Umbraco.Cms.Core.Models.PublishedContent;
 
 namespace Umbraco.Cms.Core.DeliveryApi;
@@ -7,21 +9,42 @@ public abstract class ApiContentBuilderBase<T>
     where T : IApiContent
 {
     private readonly IApiContentNameProvider _apiContentNameProvider;
-    private readonly IApiContentRouteBuilder _apiContentRouteBuilder;
     private readonly IOutputExpansionStrategyAccessor _outputExpansionStrategyAccessor;
 
-    protected ApiContentBuilderBase(IApiContentNameProvider apiContentNameProvider, IApiContentRouteBuilder apiContentRouteBuilder, IOutputExpansionStrategyAccessor outputExpansionStrategyAccessor)
+    [Obsolete("Please use the constructor that takes all parameters. Scheduled for removal in Umbraco 17.")]
+    protected ApiContentBuilderBase(
+        IApiContentNameProvider apiContentNameProvider,
+        IApiContentRouteBuilder apiContentRouteBuilder,
+        IOutputExpansionStrategyAccessor outputExpansionStrategyAccessor)
+        : this(
+              apiContentNameProvider,
+              apiContentRouteBuilder,
+              outputExpansionStrategyAccessor,
+              StaticServiceProvider.Instance.GetRequiredService<IVariationContextAccessor>())
+    {
+    }
+
+    protected ApiContentBuilderBase(
+        IApiContentNameProvider apiContentNameProvider,
+        IApiContentRouteBuilder apiContentRouteBuilder,
+        IOutputExpansionStrategyAccessor outputExpansionStrategyAccessor,
+        IVariationContextAccessor variationContextAccessor)
     {
         _apiContentNameProvider = apiContentNameProvider;
-        _apiContentRouteBuilder = apiContentRouteBuilder;
+        ApiContentRouteBuilder = apiContentRouteBuilder;
         _outputExpansionStrategyAccessor = outputExpansionStrategyAccessor;
+        VariationContextAccessor = variationContextAccessor;
     }
+
+    protected IApiContentRouteBuilder ApiContentRouteBuilder { get; }
+
+    protected IVariationContextAccessor VariationContextAccessor { get; }
 
     protected abstract T Create(IPublishedContent content, string name, IApiContentRoute route, IDictionary<string, object?> properties);
 
     public virtual T? Build(IPublishedContent content)
     {
-        IApiContentRoute? route = _apiContentRouteBuilder.Build(content);
+        IApiContentRoute? route = ApiContentRouteBuilder.Build(content);
         if (route is null)
         {
             return default;
@@ -32,10 +55,32 @@ public abstract class ApiContentBuilderBase<T>
                 ? outputExpansionStrategy.MapContentProperties(content)
                 : new Dictionary<string, object?>();
 
+        // If a segment is requested and all segmented properties are null, we consider the segment as not created or non-existing and return null.
+        // This aligns the behaviour of the API when it comes to "Accept-Segment" and "Accept-Language" requests, so 404 is returned for both when
+        // the segment or language is not created or does not exist.
+        // It also aligns with what we show in the backoffice for whether a segment is "Published" or "Not created".
+        // Requested languages that aren't created or don't exist will already have exited early in the route builder.
+        if (IsSegmentRequested() && AreAllSegmentedPropertiesNull(content, properties))
+        {
+            return default;
+        }
+
         return Create(
             content,
             _apiContentNameProvider.GetName(content),
             route,
             properties);
+    }
+
+    private bool IsSegmentRequested() => string.IsNullOrWhiteSpace(VariationContextAccessor.VariationContext?.Segment) is false;
+
+    private static bool AreAllSegmentedPropertiesNull(
+        IPublishedContent content,
+        IDictionary<string, object?> properties)
+    {
+        IEnumerable<string> segmentedProperties = content.Properties
+            .Where(x => x.PropertyType.Variations.HasFlag(Models.ContentVariation.Segment))
+            .Select(x => x.Alias);
+        return segmentedProperties.All(x => properties.ContainsKey(x) is false || properties[x] is null);
     }
 }
