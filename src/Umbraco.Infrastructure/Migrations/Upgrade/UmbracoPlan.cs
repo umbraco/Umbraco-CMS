1c2361f29aa44268bdb4a7304048c6c870c47d9e
Andy Butland <abutland73@gmail.com>
2025-07-23 13:04:27 +0200

    Added migration to plan.

--- src/Umbraco.Infrastructure/Migrations/Upgrade/UmbracoPlan.cs
+++ src/Umbraco.Infrastructure/Migrations/Upgrade/UmbracoPlan.cs
@@ -122,5 +122,6 @@ public class UmbracoPlan : MigrationPlan
 
         // To 16.2.0
         To<V_16_2_0.AddLongRunningOperations>("{741C22CF-5FB8-4343-BF79-B97A58C2CCBA}");
+        To<V_16_2_0.AddDocumentUrlLock>("{BE11D4D3-3A1F-4598-90D4-B548BD188C48}");
     }
 }
