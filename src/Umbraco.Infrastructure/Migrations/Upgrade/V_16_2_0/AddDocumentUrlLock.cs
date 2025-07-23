be8e0f319d372a1f7198f14e50843b58220fa6ac
Sven Geusens <sge@umbraco.dk>
2025-07-23 13:52:08 +0200

    Fix navigationUrlService and underlying models not being thread safe (#19689)
    * Fix navigationUrlService and underlying models not being thread safe
    
    * Added migration to plan.
    
    ---------
    
    Co-authored-by: Andy Butland <abutland73@gmail.com>

--- src/Umbraco.Infrastructure/Migrations/Upgrade/V_16_2_0/AddDocumentUrlLock.cs
+++ src/Umbraco.Infrastructure/Migrations/Upgrade/V_16_2_0/AddDocumentUrlLock.cs
@@ -7,29 +7,24 @@ using Umbraco.Extensions;
 namespace Umbraco.Cms.Infrastructure.Migrations.Upgrade.V_16_2_0;
 
 [Obsolete("Remove in Umbraco 18.")]
-public class AddLongRunningOperations : MigrationBase
+internal class AddDocumentUrlLock : MigrationBase
 {
-    public AddLongRunningOperations(IMigrationContext context)
+    public AddDocumentUrlLock(IMigrationContext context)
         : base(context)
     {
     }
 
     protected override void Migrate()
     {
-        if (!TableExists(Constants.DatabaseSchema.Tables.LongRunningOperation))
-        {
-            Create.Table<LongRunningOperationDto>().Do();
-        }
-
         Sql<ISqlContext> sql = Database.SqlContext.Sql()
             .Select<LockDto>()
             .From<LockDto>()
-            .Where<LockDto>(x => x.Id == Constants.Locks.LongRunningOperations);
+            .Where<LockDto>(x => x.Id == Constants.Locks.DocumentUrls);
 
         LockDto? existingLockDto = Database.FirstOrDefault<LockDto>(sql);
         if (existingLockDto is null)
         {
-            Database.Insert(Constants.DatabaseSchema.Tables.Lock, "id", false, new LockDto { Id = Constants.Locks.LongRunningOperations, Name = "LongRunningOperations" });
+            Database.Insert(Constants.DatabaseSchema.Tables.Lock, "id", false, new LockDto { Id = Constants.Locks.DocumentUrls, Name = "DocumentUrls" });
         }
     }
 }
