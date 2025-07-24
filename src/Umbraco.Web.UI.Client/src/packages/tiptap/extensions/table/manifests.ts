44c51fb83296dc6b9a4c9b5842f7d1eef8e8d8a8
leekelleher <leekelleher@gmail.com>
2025-07-24 15:34:57 +0100

    Replaces table toolbar menu with the new `menu` extensions

--- src/Umbraco.Web.UI.Client/src/packages/tiptap/extensions/table/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/tiptap/extensions/table/manifests.ts
@@ -38,43 +38,27 @@ const toolbarExtensions: Array<UmbExtensionManifest> = [
 		api: () => import('./table.tiptap-toolbar-api.js'),
 		element: () => import('./components/table-toolbar-menu.element.js'),
 		forExtensions: ['Umb.Tiptap.Table'],
+		items: [
+			{
+				label: 'Cell',
+				menu: UMB_MENU_TIPTAP_TABLE_CELL_ALIAS,
+			},
+			{
+				label: 'Row',
+				menu: UMB_MENU_TIPTAP_TABLE_ROW_ALIAS,
+			},
+			{
+				label: 'Column',
+				menu: UMB_MENU_TIPTAP_TABLE_COLUMN_ALIAS,
+				separatorAfter: true,
+			},
+		],
+		menu: UMB_MENU_TIPTAP_TABLE_ALIAS,
 		meta: {
 			alias: 'table',
 			icon: 'icon-table',
 			label: 'Table',
 			look: 'icon',
-			items: [
-				{
-					label: 'Cell',
-					items: [
-						{ label: 'Merge cells', data: 'mergeCells' },
-						{ label: 'Split cell', data: 'splitCell' },
-						{ label: 'Merge or split', data: 'mergeOrSplit' },
-						{ label: 'Toggle header cell', data: 'toggleHeaderCell' },
-					],
-				},
-				{
-					label: 'Row',
-					items: [
-						{ label: 'Add row before', icon: 'icon-page-up', data: 'addRowBefore' },
-						{ label: 'Add row after', icon: 'icon-page-down', data: 'addRowAfter' },
-						{ label: 'Delete row', icon: 'icon-trash', data: 'deleteRow' },
-						{ label: 'Toggle header row', data: 'toggleHeaderRow' },
-					],
-				},
-				{
-					label: 'Column',
-					items: [
-						{ label: 'Add column before', icon: 'icon-navigation-first', data: 'addColumnBefore' },
-						{ label: 'Add column after', icon: 'icon-tab-key', data: 'addColumnAfter' },
-						{ label: 'Delete column', icon: 'icon-trash', data: 'deleteColumn' },
-						{ label: 'Toggle header column', data: 'toggleHeaderColumn' },
-					],
-					separatorAfter: true,
-				},
-				{ label: 'Table properties', data: 'tableProperties' },
-				{ label: 'Delete table', icon: 'icon-trash', data: 'deleteTable' },
-			],
 		},
 	},
 ];
