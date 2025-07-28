d621c8091bbcc38bfc68ebc0cb0d3d91a38bb76c
Mads Rasmussen <madsr@hey.com>
2025-07-28 13:50:59 +0200

    Add config to radio button list Storybook story
    Introduces a sample UmbPropertyEditorConfigCollection to the radio button list

--- src/Umbraco.Web.UI.Client/src/packages/property-editors/radio-button-list/property-editor-ui-radio-button-list.stories.ts
+++ src/Umbraco.Web.UI.Client/src/packages/property-editors/radio-button-list/property-editor-ui-radio-button-list.stories.ts
@@ -1,6 +1,7 @@
 import type { UmbPropertyEditorUIRadioButtonListElement } from './property-editor-ui-radio-button-list.element.js';
 import type { Meta, StoryFn } from '@storybook/web-components-vite';
 import { html } from '@umbraco-cms/backoffice/external/lit';
+import { UmbPropertyEditorConfigCollection } from '@umbraco-cms/backoffice/property-editor';
 
 import './property-editor-ui-radio-button-list.element.js';
 
@@ -10,5 +11,16 @@ export default {
 	id: 'umb-property-editor-ui-radio-button-list',
 } as Meta;
 
+const config = new UmbPropertyEditorConfigCollection([
+	{
+		alias: 'items',
+		value: [
+			{ name: 'Option 1', value: 'option1' },
+			{ name: 'Option 2', value: 'option2' },
+			{ name: 'Option 3', value: 'option3' },
+		],
+	},
+]);
+
 export const Docs: StoryFn<UmbPropertyEditorUIRadioButtonListElement> = () =>
-	html`<umb-property-editor-ui-radio-button-list></umb-property-editor-ui-radio-button-list>`;
+	html`<umb-property-editor-ui-radio-button-list .config=${config}></umb-property-editor-ui-radio-button-list>`;
