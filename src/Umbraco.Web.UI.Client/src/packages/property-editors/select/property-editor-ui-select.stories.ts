2eef74044c89d437fc7ad5ad51276aa2dc8aff6b
Mads Rasmussen <madsr@hey.com>
2025-07-28 13:49:49 +0200

    Add config to select property editor story
    Introduces a sample UmbPropertyEditorConfigCollection to the select property editor Storybook story, providing predefined options for demonstration and testing purposes.

--- src/Umbraco.Web.UI.Client/src/packages/property-editors/select/property-editor-ui-select.stories.ts
+++ src/Umbraco.Web.UI.Client/src/packages/property-editors/select/property-editor-ui-select.stories.ts
@@ -1,6 +1,7 @@
 import type { UmbPropertyEditorUISelectElement } from './property-editor-ui-select.element.js';
 import type { Meta, StoryFn } from '@storybook/web-components-vite';
 import { html } from '@umbraco-cms/backoffice/external/lit';
+import { UmbPropertyEditorConfigCollection } from '@umbraco-cms/backoffice/property-editor';
 
 import './property-editor-ui-select.element.js';
 
@@ -10,5 +11,16 @@ export default {
 	id: 'umb-property-editor-ui-select',
 } as Meta;
 
+const config = new UmbPropertyEditorConfigCollection([
+	{
+		alias: 'items',
+		value: [
+			{ name: 'Option 1', value: 'option1' },
+			{ name: 'Option 2', value: 'option2' },
+			{ name: 'Option 3', value: 'option3' },
+		],
+	},
+]);
+
 export const Docs: StoryFn<UmbPropertyEditorUISelectElement> = () =>
-	html`<umb-property-editor-ui-select></umb-property-editor-ui-select>`;
+	html`<umb-property-editor-ui-select .config=${config}></umb-property-editor-ui-select>`;
