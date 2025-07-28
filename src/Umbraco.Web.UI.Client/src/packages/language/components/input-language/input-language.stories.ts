69ca3c1e25288bfafd85f4804624ce3fd79c2778
Mads Rasmussen <madsr@hey.com>
2025-07-28 12:59:53 +0200

    fix input-language component

--- src/Umbraco.Web.UI.Client/src/packages/language/components/input-language/input-language.stories.ts
+++ src/Umbraco.Web.UI.Client/src/packages/language/components/input-language/input-language.stories.ts
@@ -1,10 +1,13 @@
-import type { Meta, StoryObj } from '@storybook/web-components-vite';
-import './input-language.element.js';
 import type { UmbInputLanguageElement } from './input-language.element.js';
+import type { Meta, StoryObj } from '@storybook/web-components-vite';
+import { html } from '@umbraco-cms/backoffice/external/lit';
+
+import './input-language.element.js';
 
 const meta: Meta<UmbInputLanguageElement> = {
-	title: 'Entity/Language/Components/Input Language Picker',
-	component: 'umb-input-language-picker',
+	title: 'Entity/Language/Components/Input Language',
+	component: 'umb-input-language',
+	render: () => html`<umb-input-language></umb-input-language>`,
 };
 
 export default meta;
