41f10494378a04eb18de9db4cb57926e7665879d
Mads Rasmussen <madsr@hey.com>
2025-07-29 09:38:17 +0200

    fix lint error

--- src/Umbraco.Web.UI.Client/src/packages/core/dashboard/app/picker/picker.context.token.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/dashboard/app/picker/picker.context.token.ts
@@ -1,5 +1,5 @@
+import type { UmbDashboardAppPickerContext } from './picker.context.js';
 import { UmbContextToken } from '@umbraco-cms/backoffice/context-api';
-import { UmbDashboardAppPickerContext } from './picker.context';
 
 export const UMB_DASHBOARD_APP_PICKER_CONTEXT = new UmbContextToken<UmbDashboardAppPickerContext>(
 	'Umb.Modal.DashboardAppPicker',
