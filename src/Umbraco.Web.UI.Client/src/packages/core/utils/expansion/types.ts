4cf39f02485b5349bc0a2b5e006b2e482f755269
Mads Rasmussen <madsr@hey.com>
2025-07-30 11:04:21 +0200

    add expansion model with target

--- src/Umbraco.Web.UI.Client/src/packages/core/utils/expansion/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/utils/expansion/types.ts
@@ -1,3 +1,7 @@
 import type { UmbEntityModel } from '@umbraco-cms/backoffice/entity';
 
 export type UmbEntityExpansionModel = Array<UmbEntityModel>;
+
+export interface UmbEntityExpansionEntryModel extends UmbEntityModel {
+	target?: UmbEntityModel;
+}
