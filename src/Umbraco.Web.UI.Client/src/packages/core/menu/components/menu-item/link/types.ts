d7c1d04ae9106b8f07ae90bd979897ca0b1bb976
Mads Rasmussen <madsr@hey.com>
2025-07-30 11:23:33 +0200

    fix import

--- src/Umbraco.Web.UI.Client/src/packages/core/menu/components/menu-item/link/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/menu/components/menu-item/link/types.ts
@@ -1,4 +1,4 @@
-import type { ManifestMenuItem, MetaMenuItem } from '../../../menu-item.extension.js';
+import type { ManifestMenuItem, MetaMenuItem } from '../../../menu-item/types.js';
 
 export interface ManifestMenuItemLinkKind extends ManifestMenuItem {
 	type: 'menuItem';
