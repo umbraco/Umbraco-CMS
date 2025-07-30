a2f63f357f0513cd352318ce750f9ab0dddf930a
Corné Hoskam <choskam@hotmail.com>
2025-07-30 15:48:02 +0200

    Re-implement original variation context for backwards compatibility

--- src/Umbraco.Core/Models/PublishedContent/VariationContext.cs
+++ src/Umbraco.Core/Models/PublishedContent/VariationContext.cs
@@ -28,6 +28,13 @@ public class VariationContext
     ///     Gets the segment for the content item
     /// </summary>
     /// <param name="contentId"></param>
+    /// <returns</returns>
+    public virtual string GetSegment(int contentId) => Segment;
+
+    /// <summary>
+    ///     Gets the segment for the content item
+    /// </summary>
+    /// <param name="contentId"></param>
     /// <param name="propertyAlias"></param>
     /// <returns></returns>
     public virtual string GetSegment(int contentId, string? propertyAlias = null) => Segment;
