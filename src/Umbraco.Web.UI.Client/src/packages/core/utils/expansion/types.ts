9204675a45a04a330f65e30b389d9beb506ee208
Mads Rasmussen <madsr@hey.com>
2025-07-31 13:10:02 +0200

    use entry model

--- src/Umbraco.Web.UI.Client/src/packages/core/utils/expansion/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/utils/expansion/types.ts
@@ -1,6 +1,6 @@
 import type { UmbEntityModel } from '@umbraco-cms/backoffice/entity';
 
-export type UmbEntityExpansionModel = Array<UmbEntityModel>;
+export type UmbEntityExpansionModel = Array<UmbEntityExpansionEntryModel>;
 
 export interface UmbEntityExpansionEntryModel extends UmbEntityModel {
 	target?: UmbEntityModel;
