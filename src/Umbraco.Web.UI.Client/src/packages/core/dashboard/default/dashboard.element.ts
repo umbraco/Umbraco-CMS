7b8c46e51251f2d97731a2afde43e7f9ade73bb3
Mads Rasmussen <madsr@hey.com>
2025-07-31 21:53:51 +0200

    make linter happy

--- src/Umbraco.Web.UI.Client/src/packages/core/dashboard/default/dashboard.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/dashboard/default/dashboard.element.ts
@@ -1,7 +1,8 @@
-import type { ManifestDashboardApp } from '../dashboard-app.extension.js';
-import { UMB_DASHBOARD_APP_PICKER_MODAL } from '../app/picker/picker-modal.token.js';
+import { UMB_DASHBOARD_APP_PICKER_MODAL } from '../app/constants.js';
+import type { ManifestDashboardApp } from '../app/types.js';
+import type { DashboardAppInstance, UserDashboardAppConfiguration } from './types.js';
 import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
-import { css, html, customElement, nothing, ifDefined, state, repeat, styleMap, when } from '@umbraco-cms/backoffice/external/lit';
+import { css, html, customElement, state, repeat, styleMap, when } from '@umbraco-cms/backoffice/external/lit';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
 import {
 	loadManifestElement,
@@ -11,11 +12,11 @@ import {
 } from '@umbraco-cms/backoffice/extension-api';
 import { umbOpenModal } from '@umbraco-cms/backoffice/modal';
 import { umbExtensionsRegistry } from '@umbraco-cms/backoffice/extension-registry';
-import { UUIBlinkAnimationValue, UUIBlinkKeyframes } from '@umbraco-cms/backoffice/external/uui';
-import type { DashboardAppInstance, UserDashboardAppConfiguration } from './types.js';
+import { UUIBlinkAnimationValue } from '@umbraco-cms/backoffice/external/uui';
 import { UmbSorterController } from '@umbraco-cms/backoffice/sorter';
 import { UmbId } from '@umbraco-cms/backoffice/id';
 import { UserDataService } from '@umbraco-cms/backoffice/external/backend-api';
+import { tryExecute } from '@umbraco-cms/backoffice/resources';
 
 @customElement('umb-dashboard')
 export class UmbDashboardElement extends UmbLitElement {
@@ -28,12 +29,12 @@ export class UmbDashboardElement extends UmbLitElement {
 	]);
 
 	#gridSizeMap = new Map([
-		['small',{columns:1, rows: 1}],
-		['medium',{columns:2, rows: 2}],
-		['large',{columns:2, rows: 2}],
+		['small', { columns: 1, rows: 1 }],
+		['medium', { columns: 2, rows: 2 }],
+		['large', { columns: 2, rows: 2 }],
 	]);
 
-	#sorter? : UmbSorterController<DashboardAppInstance>;
+	#sorter?: UmbSorterController<DashboardAppInstance>;
 
 	#extensionsController?: UmbExtensionsElementInitializer<UmbExtensionManifest, 'dashboardApp', ManifestDashboardApp>;
 
@@ -41,15 +42,17 @@ export class UmbDashboardElement extends UmbLitElement {
 	_editMode = false;
 
 	@state()
-	_apps : Array<DashboardAppInstance> = [];
+	_apps: Array<DashboardAppInstance> = [];
 
 	@state()
 	_appUniques: Array<string> = [];
 
 	@state()
-	_availableApps : PermittedControllerType<UmbExtensionElementInitializer<ManifestDashboardApp, any, ManifestDashboardApp, HTMLElement | undefined>>[] = [];
+	_availableApps: PermittedControllerType<
+		UmbExtensionElementInitializer<ManifestDashboardApp, any, ManifestDashboardApp, HTMLElement | undefined>
+	>[] = [];
 
-	_userDataKey : string | undefined;
+	_userDataKey: string | undefined;
 
 	constructor() {
 		super();
@@ -70,11 +73,9 @@ export class UmbDashboardElement extends UmbLitElement {
 		this.#sorter.disable();
 
 		this.#observeDashboardApps();
-
 	}
 
 	#observeDashboardApps(): void {
-
 		this.#extensionsController?.destroy();
 		this.#extensionsController = new UmbExtensionsElementInitializer<
 			UmbExtensionManifest,
@@ -84,165 +85,152 @@ export class UmbDashboardElement extends UmbLitElement {
 			this,
 			umbExtensionsRegistry,
 			'dashboardApp',
-			(manifest) => true,
+			undefined,
 			(extensionControllers) => {
-
 				this._availableApps = extensionControllers;
 
 				this.#load();
-
 			},
 			undefined, // We can leave the alias to undefined, as we destroy this our selfs.
 		);
 	}
 
-	async #drawDashboardApps(config : UserDashboardAppConfiguration) {
+	async #drawDashboardApps(config: UserDashboardAppConfiguration) {
+		const apps = [...this._apps];
 
-		let apps = [...this._apps];
-
-		if(config.apps.length == 0) {
+		if (config.apps.length == 0) {
 			//TODO: Draw all?
-		}
-		else
-		{
-
-			for(const configuredApp of config.apps){
-
-				var controller = this._availableApps.find(x=>x.alias == configuredApp.alias);
-				if(!controller)
-					return undefined;
+		} else {
+			for (const configuredApp of config.apps) {
+				const controller = this._availableApps.find((x) => x.alias == configuredApp.alias);
+				if (!controller) return undefined;
 
 				const size = this.#sizeMap.get(controller.manifest.meta?.size) ?? this.#defaultSize;
 				const headline = controller.manifest?.meta?.headline
 					? this.localize.string(controller.manifest?.meta?.headline)
 					: undefined;
 
-				let gridSize = this.#gridSizeMap.get(size)!;
+				const gridSize = this.#gridSizeMap.get(size)!;
 
 				const elementPropsValue = controller.manifest.element ?? controller.manifest.js;
-				var appElementCtor = await loadManifestElement(elementPropsValue!)!; //TODO: Validate
+				const appElementCtor = await loadManifestElement(elementPropsValue!)!; //TODO: Validate
 
-				if(!appElementCtor)
-					return;
+				if (!appElementCtor) return;
 
 				apps.push({
-					key : UmbId.new(),
-					alias : controller.alias,
-					columns : gridSize.columns,
-					rows : gridSize?.rows,
-					headline : headline,
-					component : new appElementCtor(),
+					key: UmbId.new(),
+					alias: controller.alias,
+					columns: gridSize.columns,
+					rows: gridSize?.rows,
+					headline: headline,
+					component: new appElementCtor(),
 				});
-
 			}
 		}
 
 		this._apps = apps;
-
 	}
 
 	async #openAppPicker() {
-
 		const value = await umbOpenModal(this, UMB_DASHBOARD_APP_PICKER_MODAL, {
 			data: {
 				multiple: true,
 			},
-			value : {
-				selection : []
-			}
+			value: {
+				selection: [],
+			},
 		}).catch(() => undefined);
 
 		if (value) {
+			const apps = [...this._apps];
 
-			let apps = [...this._apps];
-
-			for(const alias of value.selection) {
-
-				var controller = this._availableApps.find(x=>x.alias == alias);
-				if(!controller)
-					return undefined;
+			for (const alias of value.selection) {
+				const controller = this._availableApps.find((x) => x.alias == alias);
+				if (!controller) return undefined;
 
 				const size = this.#sizeMap.get(controller.manifest.meta?.size) ?? this.#defaultSize;
-						const headline = controller.manifest?.meta?.headline
-							? this.localize.string(controller.manifest?.meta?.headline)
-							: undefined;
+				const headline = controller.manifest?.meta?.headline
+					? this.localize.string(controller.manifest?.meta?.headline)
+					: undefined;
 
-				let gridSize = this.#gridSizeMap.get(size)!;
+				const gridSize = this.#gridSizeMap.get(size)!;
 
 				const elementPropsValue = controller.manifest.element ?? controller.manifest.js;
-				var appElementCtor = await loadManifestElement(elementPropsValue!)!; //TODO: Validate
+				const appElementCtor = await loadManifestElement(elementPropsValue!)!; //TODO: Validate
 
-				if(!appElementCtor)
-					return;
+				if (!appElementCtor) return;
 
 				apps.push({
-					key : UmbId.new(),
-					alias : controller.alias,
-					columns : gridSize.columns,
-					rows : gridSize?.rows,
-					headline : headline,
-					component : new appElementCtor(),
+					key: UmbId.new(),
+					alias: controller.alias,
+					columns: gridSize.columns,
+					rows: gridSize?.rows,
+					headline: headline,
+					component: new appElementCtor(),
 				});
-
 			}
 
 			this._apps = apps;
 
 			this.#save();
-
 		}
 	}
 
-	async #save(){
-
-		let config = {
-			apps : this._apps.map((x)=> {return {alias : x.alias }})
+	async #save() {
+		const config = {
+			apps: this._apps.map((x) => {
+				return { alias: x.alias };
+			}),
 		} as UserDashboardAppConfiguration;
 
-		if(this._userDataKey){
-			var res = await UserDataService.putUserData({
-				body : {
-					key : this._userDataKey,
-					group : 'test',
-					identifier : '',
-					value : JSON.stringify(config),
-				}
-			});
+		if (this._userDataKey) {
+			await tryExecute(
+				this,
+				UserDataService.putUserData({
+					body: {
+						key: this._userDataKey,
+						group: 'test',
+						identifier: '',
+						value: JSON.stringify(config),
+					},
+				}),
+			);
+		} else {
+			await tryExecute(
+				this,
+				UserDataService.postUserData({
+					body: {
+						group: 'test',
+						identifier: '',
+						value: JSON.stringify(config),
+					},
+				}),
+			);
 		}
-		else {
-			var res = await UserDataService.postUserData({
-				body : {
-					group : 'test',
-					identifier : '',
-					value : JSON.stringify(config),
-				}
-			});
-		}
-
 	}
 
-	async #load(){
+	async #load() {
+		const res = await tryExecute(
+			this,
+			UserDataService.getUserData({
+				query: {
+					groups: ['test'],
+				},
+			}),
+		);
 
-		var res = await UserDataService.getUserData({
-			query : {
-				groups : ['test']
-			}
-		});
-
-		if(res.data.items.length == 0)
-			return;
+		if (res.data.items.length == 0) return;
 
 		this._userDataKey = res.data.items[0].key;
 
 		const userConfig = JSON.parse(res.data.items[0].value);
 
 		this.#drawDashboardApps(userConfig);
-
 	}
 
 	async #enterEditMode() {
 		this.#sorter?.setModel(this._apps);
-		this.#sorter?.enable()
+		this.#sorter?.enable();
 		this._editMode = true;
 	}
 
@@ -252,60 +240,58 @@ export class UmbDashboardElement extends UmbLitElement {
 		this.#save();
 	}
 
-	async #remove(elementKey : string) {
-
-		this._apps = this._apps.filter(x=>x.key != elementKey);
-
+	async #remove(elementKey: string) {
+		this._apps = this._apps.filter((x) => x.key != elementKey);
 	}
-
-	#onPopoverToggle = false;
 
 	override render() {
 		return html`
 			<section id="content">
 				<div class="main-actions">
-
-					${when(this._editMode,
-						()=>html`<uui-button @click=${this.#leaveEditMode}>Done <umb-icon name="icon-done"></umb-icon></uui-button>`,
-						()=>html`<uui-button
-						id="action-button"
-						data-mark="workspace:action-menu-button"
-						popovertarget="workspace-entity-action-menu-popover"
-						label=${this.localize.term('general_actions')}>
-						<uui-symbol-more></uui-symbol-more>
-					</uui-button>
-					<uui-popover-container
-						id="workspace-entity-action-menu-popover"
-						placement="bottom-end">
-						<umb-popover-layout>
-							<uui-scroll-container>
-								<uui-menu-item @click=${this.#openAppPicker} label="Add">
-									<umb-icon slot="icon" name="icon-add"></umb-icon>
-								</uui-menu-item>
-								<uui-menu-item @click=${this.#enterEditMode} label="Edit">
-									<umb-icon slot="icon" name="icon-edit"></umb-icon>
-								</uui-menu-item>
-							</uui-scroll-container>
-						</umb-popover-layout>
-					</uui-popover-container>`)
-					}
+					${when(
+						this._editMode,
+						() =>
+							html`<uui-button @click=${this.#leaveEditMode}>Done <umb-icon name="icon-done"></umb-icon></uui-button>`,
+						() =>
+							html`<uui-button
+									id="action-button"
+									data-mark="workspace:action-menu-button"
+									popovertarget="workspace-entity-action-menu-popover"
+									label=${this.localize.term('general_actions')}>
+									<uui-symbol-more></uui-symbol-more>
+								</uui-button>
+								<uui-popover-container id="workspace-entity-action-menu-popover" placement="bottom-end">
+									<umb-popover-layout>
+										<uui-scroll-container>
+											<uui-menu-item @click=${this.#openAppPicker} label="Add">
+												<umb-icon slot="icon" name="icon-add"></umb-icon>
+											</uui-menu-item>
+											<uui-menu-item @click=${this.#enterEditMode} label="Edit">
+												<umb-icon slot="icon" name="icon-edit"></umb-icon>
+											</uui-menu-item>
+										</uui-scroll-container>
+									</umb-popover-layout>
+								</uui-popover-container>`,
+					)}
 				</div>
 				<div class="grid-container">
 					${repeat(
 						this._apps,
 						(element) => element.key,
 						(element) =>
-							html`
-								<div
-									style=${styleMap({gridColumn:`span ${element.columns}`,gridRow:`span ${element.rows}`})}
-									class="dashboard-app"
-									data-sorter-id=${element.key}>
-											${when(this._editMode,
-												()=>html`<uui-button color="danger" compact @click=${()=> this.#remove(element.key)}><umb-icon name="icon-trash"></umb-icon></uui-button>`
-											)}
-										${element.component}
-
-								</div>`,
+							html` <div
+								style=${styleMap({ gridColumn: `span ${element.columns}`, gridRow: `span ${element.rows}` })}
+								class="dashboard-app"
+								data-sorter-id=${element.key}>
+								${when(
+									this._editMode,
+									() =>
+										html`<uui-button color="danger" compact @click=${() => this.#remove(element.key)}
+											><umb-icon name="icon-trash"></umb-icon
+										></uui-button>`,
+								)}
+								${element.component}
+							</div>`,
 					)}
 				</div>
 			</section>
@@ -328,21 +314,21 @@ export class UmbDashboardElement extends UmbLitElement {
 
 			.main-actions {
 				display: flex;
-    		justify-content: flex-end;
+				justify-content: flex-end;
 			}
 
 			uui-box div[slot='header-actions'] uui-button {
-				font-size:12px;
+				font-size: 12px;
 				--uui-button-height: auto;
 			}
 
 			uui-box {
-				height:100%;
-				position:relative;
+				height: 100%;
+				position: relative;
 			}
 
 			.grid-container {
-				margin-top:var(--uui-size-space-3);
+				margin-top: var(--uui-size-space-3);
 				display: grid;
 				grid-template-columns: repeat(4, 1fr);
 				grid-auto-rows: 225px;
@@ -365,9 +351,9 @@ export class UmbDashboardElement extends UmbLitElement {
 			}
 
 			.dashboard-app {
-				position:relative;
-				display:block;
-				height:100%;
+				position: relative;
+				display: block;
+				height: 100%;
 
 				&::after {
 					content: '';
@@ -381,10 +367,10 @@ export class UmbDashboardElement extends UmbLitElement {
 				}
 
 				& > uui-button {
-					position:absolute;
-					top:0;
-					right:0;
-					z-index:1;
+					position: absolute;
+					top: 0;
+					right: 0;
+					z-index: 1;
 				}
 
 				&[drag-placeholder] {
@@ -413,11 +399,8 @@ export class UmbDashboardElement extends UmbLitElement {
 						transition: opacity 50ms 16ms;
 						opacity: 0;
 					}
-
 				}
-
 			}
-
 		`,
 	];
 }
@@ -429,4 +412,3 @@ declare global {
 		['umb-dashboard']: UmbDashboardElement;
 	}
 }
-
