18f1318f44e90e56749aed8665ca84fd572a4398
Andy Butland <abutland73@gmail.com>
2025-08-04 08:32:55 +0200

    Adds abstraction around boot time checks for database availability.

---
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/Persistence/DefaultDatabaseAvailabilityCheckTests.cs
@@ -0,0 +1,66 @@
+using Microsoft.Extensions.Logging.Abstractions;
+using Moq;
+using NUnit.Framework;
+using Umbraco.Cms.Core.Install.Models;
+using Umbraco.Cms.Infrastructure.Persistence;
+using Umbraco.Cms.Persistence.SqlServer.Services;
+
+namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Infrastructure.Persistence;
+
+[TestFixture]
+public class DefaultDatabaseAvailabilityCheckTests
+{
+    [Test]
+    public void IsDatabaseAvailable_WithDatabaseUnavailable_ReturnsFalse()
+    {
+        var mockDatabaseFactory = new Mock<IUmbracoDatabaseFactory>();
+        mockDatabaseFactory
+            .Setup(x => x.CanConnect)
+            .Returns(false);
+
+        var sut = CreateDefaultDatabaseAvailabilityCheck();
+        var result = sut.IsDatabaseAvailable(mockDatabaseFactory.Object);
+        Assert.IsFalse(result);
+    }
+
+    [Test]
+    public void IsDatabaseAvailable_WithDatabaseImmediatelyAvailable_ReturnsTrue()
+    {
+        var mockDatabaseFactory = new Mock<IUmbracoDatabaseFactory>();
+        mockDatabaseFactory
+            .Setup(x => x.CanConnect)
+            .Returns(true);
+
+        var sut = CreateDefaultDatabaseAvailabilityCheck();
+        var result = sut.IsDatabaseAvailable(mockDatabaseFactory.Object);
+        Assert.IsTrue(result);
+    }
+
+    [TestCase(5, true)]
+    [TestCase(6, false)]
+    public void IsDatabaseAvailable_WithDatabaseImmediatelyAvailableAfterMultipleAttempts_ReturnsExpectedResult(int attemptsUntilConnection, bool expectedResult)
+    {
+        var attemptResults = new Queue<bool>();
+        for (var i = 0; i < attemptsUntilConnection - 1; i++)
+        {
+            attemptResults.Enqueue(false);
+        }
+
+        attemptResults.Enqueue(true);
+
+        var mockDatabaseFactory = new Mock<IUmbracoDatabaseFactory>();
+        mockDatabaseFactory
+            .Setup(x => x.CanConnect)
+            .Returns(attemptResults.Dequeue);
+
+        var sut = CreateDefaultDatabaseAvailabilityCheck();
+        var result = sut.IsDatabaseAvailable(mockDatabaseFactory.Object);
+        Assert.AreEqual(expectedResult, result);
+    }
+
+    private static DefaultDatabaseAvailabilityCheck CreateDefaultDatabaseAvailabilityCheck()
+        => new(new NullLogger<DefaultDatabaseAvailabilityCheck>())
+        {
+            AttemptDelayMilliseconds = 1 // Set to 1 ms for faster tests.
+        };
+}
