381cf0da56091ee1aa3a77d4db0790b639bd6ad8
Mads Rasmussen <madsr@hey.com>
2025-08-07 11:06:55 +0200

    add example dashboard and entity action

---
+++ src/Umbraco.Web.UI.Client/examples/section-sidebar-menu-expansion/playground-dashboard/section-sidebar-menu-playground.element.ts
@@ -0,0 +1,111 @@
+import { html, customElement, LitElement, css, state, repeat } from '@umbraco-cms/backoffice/external/lit';
+import { UmbElementMixin } from '@umbraco-cms/backoffice/element-api';
+import {
+	UMB_SECTION_SIDEBAR_MENU_GLOBAL_CONTEXT,
+	UMB_SECTION_SIDEBAR_MENU_SECTION_CONTEXT,
+} from '@umbraco-cms/backoffice/menu';
+
+@customElement('example-section-sidebar-menu-playground-dashboard')
+export class ExampleSectionSidebarMenuPlaygroundDashboard extends UmbElementMixin(LitElement) {
+	#globalContext?: typeof UMB_SECTION_SIDEBAR_MENU_GLOBAL_CONTEXT.TYPE;
+	#sectionContext?: typeof UMB_SECTION_SIDEBAR_MENU_SECTION_CONTEXT.TYPE;
+
+	@state()
+	_globalExpansion: Array<any> = [];
+
+	@state()
+	_sectionExpansion: Array<any> = [];
+
+	constructor() {
+		super();
+
+		this.consumeContext(UMB_SECTION_SIDEBAR_MENU_GLOBAL_CONTEXT, (section) => {
+			this.#globalContext = section;
+			this.#observeGlobalExpansion();
+		});
+
+		this.consumeContext(UMB_SECTION_SIDEBAR_MENU_SECTION_CONTEXT, (section) => {
+			this.#sectionContext = section;
+			this.#observeSectionExpansion();
+		});
+	}
+
+	#observeGlobalExpansion() {
+		this.observe(this.#globalContext?.expansion.expansion, (items) => {
+			this._globalExpansion = items || [];
+		});
+	}
+
+	#observeSectionExpansion() {
+		this.observe(this.#sectionContext?.expansion.expansion, (items) => {
+			this._sectionExpansion = items || [];
+		});
+	}
+
+	override render() {
+		return html` <umb-stack>
+			<uui-box headline="Open Items for this section">
+				${repeat(
+					this._sectionExpansion,
+					(item) => item.entityType + item.unique,
+					(item) => this.#renderItem(item),
+				)}
+			</uui-box>
+			<uui-box headline="Open Items for all sections">
+				${repeat(
+					this._globalExpansion,
+					(item) => item.entityType + item.unique,
+					(item) => this.#renderItem(item),
+				)}
+			</uui-box>
+		</umb-stack>`;
+	}
+
+	#renderItem(item: any) {
+		return html`<div class="expansion-entry">
+			<div>
+				<span class="label">Entity Type:</span>
+				${item.entityType}
+			</div>
+			<div>
+				<span class="label">Unique:</span>
+				${item.unique}
+			</div>
+			<div>
+				<span class="label">Menu Item Alias:</span>
+				${item.menuItemAlias}
+			</div>
+			<div>
+				<span class="label">Section Alias:</span>
+				${item.sectionAlias}
+			</div>
+		</div>`;
+	}
+
+	static override styles = [
+		css`
+			:host {
+				display: block;
+				padding: var(--uui-size-layout-1);
+			}
+
+			.expansion-entry {
+				border-bottom: 1px solid var(--uui-color-border);
+				padding: var(--uui-size-space-2);
+				margin-bottom: var(--uui-size-space-2);
+			}
+
+			.label {
+				font-weight: bold;
+			}
+		`,
+	];
+}
+
+export { ExampleSectionSidebarMenuPlaygroundDashboard as element };
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'example-section-sidebar-menu-playground-dashboard': ExampleSectionSidebarMenuPlaygroundDashboard;
+	}
+}
