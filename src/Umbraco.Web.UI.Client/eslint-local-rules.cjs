cf47838efd242ca85a281dcda11bb6c501967bfa
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-08-07 11:56:05 +0200

    revert relative js extension imports

--- src/Umbraco.Web.UI.Client/eslint-local-rules.cjs
+++ src/Umbraco.Web.UI.Client/eslint-local-rules.cjs
@@ -2,6 +2,7 @@
 
 const enforceElementSuffixOnElementClassNameRule = require('./devops/eslint/rules/enforce-element-suffix-on-element-class-name.cjs');
 const enforceUmbracoExternalImportsRule = require('./devops/eslint/rules/enforce-umbraco-external-imports.cjs');
+const ensureRelativeImportUseJsExtensionRule = require('./devops/eslint/rules/ensure-relative-import-use-js-extension.cjs');
 const noDirectApiImportRule = require('./devops/eslint/rules/no-direct-api-import.cjs');
 const preferImportAliasesRule = require('./devops/eslint/rules/prefer-import-aliases.cjs');
 const preferStaticStylesLastRule = require('./devops/eslint/rules/prefer-static-styles-last.cjs');
@@ -10,6 +11,7 @@ const noRelativeImportToImportMapModule = require('./devops/eslint/rules/no-rela
 module.exports = {
 	'enforce-element-suffix-on-element-class-name': enforceElementSuffixOnElementClassNameRule,
 	'enforce-umbraco-external-imports': enforceUmbracoExternalImportsRule,
+	'ensure-relative-import-use-js-extension': ensureRelativeImportUseJsExtensionRule,
 	'no-direct-api-import': noDirectApiImportRule,
 	'prefer-import-aliases': preferImportAliasesRule,
 	'prefer-static-styles-last': preferStaticStylesLastRule,
