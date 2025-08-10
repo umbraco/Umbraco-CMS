01535c1da43296ac52320f5be9a37b9ad2f465e9
Mads Rasmussen <madsr@hey.com>
2025-08-10 21:56:00 +0200

    wip

--- src/Umbraco.Web.UI.Client/src/apps/backoffice/components/backoffice-header-sections.element.ts
+++ src/Umbraco.Web.UI.Client/src/apps/backoffice/components/backoffice-header-sections.element.ts
@@ -16,6 +16,10 @@ export class UmbBackofficeHeaderSectionsElement extends UmbLitElement {
 
 	private _backofficeContext?: UmbBackofficeContext;
 
+	private _pathFromAlias = new Map<string, string>();
+	private _aliasFromPath = new Map<string, string>();
+	private _lastPathInSection = new Map<string, string>();
+
 	constructor() {
 		super();
 
@@ -24,6 +28,8 @@ export class UmbBackofficeHeaderSectionsElement extends UmbLitElement {
 			this._observeSections();
 			this._observeCurrentSection();
 		});
+
+		//window.addEventListener('willchangestate', this.#onWillNavigate);
 	}
 
 	private _observeSections() {
@@ -34,22 +40,107 @@ export class UmbBackofficeHeaderSectionsElement extends UmbLitElement {
 			(allowedSections) => {
 				const oldValue = this._sections;
 				this._sections = allowedSections;
+				this._sections.forEach((section) => {
+					if (section.manifest?.meta?.pathname) {
+						this._pathFromAlias.set(section.alias, this.#getSectionRelativePath(section));
+						this._aliasFromPath.set(this.#getSectionRelativePath(section), section.alias);
+					}
+				});
 				this.requestUpdate('_sections', oldValue);
 			},
 			'observeSections',
 		);
 	}
 
+	#prevPath?: string;
+	#userClickedSection = false;
+
+	#onWillNavigate = (e: CustomEvent) => {
+		if (this.#userClickedSection === false) {
+			return;
+		}
+
+		this.#prevPath = window.location.pathname;
+		const nextPath = e.detail.url;
+
+		console.log('Previous path:', this.#prevPath);
+		console.log('Next path:', nextPath);
+
+		// Check if the next path is a section root path and therefore a section change.
+		const isSectionChange = this._aliasFromPath.has(nextPath);
+
+		if (isSectionChange) {
+			// Check if we have a previous path in the section.
+			const newSectionAlias = this._aliasFromPath.get(nextPath);
+
+			if (newSectionAlias && this._lastPathInSection.has(newSectionAlias)) {
+				const lastPathInNewSection = this._lastPathInSection.get(newSectionAlias);
+				if (lastPathInNewSection) {
+					e.preventDefault();
+					history.pushState(null, '', lastPathInNewSection);
+					console.log('Navigating to last path in section:', lastPathInNewSection);
+				}
+			}
+		}
+
+		this.#userClickedSection = false;
+
+		/*
+		// Check if the next path is the root of the current section. This means that the section is being "reloaded".
+		const isSectionReload = this._pathFromAlias.get(this._currentSectionAlias) === nextPath;
+
+		/* If its a section reload, we let the navigation continue as normal. 
+		 Navigating to the section root and doing a reload. */
+		/*
+		if (isSectionReload) {
+			return;
+		} else {
+			const nextSectionAlias = this._aliasFromPath.get(nextPath);
+			if (!nextSectionAlias) return;
+			const lastPathInNewSection = this._lastPathInSection.get(nextSectionAlias);
+			// If we have a last path in the section, we navigate to that instead.
+			if (lastPathInNewSection) {
+				e.preventDefault();
+				history.pushState(null, '', lastPathInNewSection);
+				debugger;
+				return;
+			} else {
+				return;
+			}
+		}
+		*/
+	};
+
 	private _observeCurrentSection() {
 		if (!this._backofficeContext) return;
 
 		this.observe(
 			this._backofficeContext.activeSectionAlias,
-			(currentSectionAlias) => {
-				this._currentSectionAlias = currentSectionAlias || '';
+			(newSectionAlias) => {
+				// When the section changes, we store the last path that was navigated to in that section.
+				// This is used to navigate back to the last path in the section when navigating to the section root.
+				// Only store the last path if it is different from section root path.
+				const currentSectionRootPath = this._pathFromAlias.get(this._currentSectionAlias || '');
+
+				if (this._currentSectionAlias && this.#prevPath && this.#prevPath !== currentSectionRootPath) {
+					// ensure that we only store a last path if it includes the section root path.
+					if (this.#prevPath.startsWith(currentSectionRootPath || '')) {
+						this._lastPathInSection.set(this._currentSectionAlias, this.#prevPath);
+					}
+				}
+
+				this._currentSectionAlias = newSectionAlias || '';
 			},
 			'observeCurrentSection',
 		);
+	}
+
+	#getSectionRelativePath(section: UmbExtensionManifestInitializer<ManifestSection>) {
+		return `/section/${section.manifest?.meta.pathname}`;
+	}
+
+	#onSectionClick() {
+		this.#userClickedSection = true;
 	}
 
 	override render() {
@@ -61,13 +152,14 @@ export class UmbBackofficeHeaderSectionsElement extends UmbLitElement {
 					(section) => html`
 						<uui-tab
 							?active="${this._currentSectionAlias === section.alias}"
-							href="${`section/${section.manifest?.meta.pathname}`}"
+							href="${ifDefined(this.#getSectionRelativePath(section))}"
 							label="${ifDefined(
 								section.manifest?.meta.label
 									? this.localize.string(section.manifest?.meta.label)
 									: section.manifest?.name,
 							)}"
-							data-mark="section-link:${section.alias}"></uui-tab>
+							data-mark="section-link:${section.alias}"
+							@click=${this.#onSectionClick}></uui-tab>
 					`,
 				)}
 			</uui-tab-group>
