c139362457a8ec29f36de096c6d62648f09a9101
Mads Rasmussen <madsr@hey.com>
2025-08-11 20:59:10 +0200

    correct alias

--- src/Umbraco.Web.UI.Client/src/packages/management-api/server-event/global-context/server-event.context-token.ts
+++ src/Umbraco.Web.UI.Client/src/packages/management-api/server-event/global-context/server-event.context-token.ts
@@ -1,7 +1,6 @@
-import type { UmbClipboardContext } from './clipboard.context.js';
-import { UmbContextToken } from '@umbraco-cms/backoffice/context-api';
 import type { UmbManagementApiServerEventContext } from './server-event.context.js';
+import { UmbContextToken } from '@umbraco-cms/backoffice/context-api';
 
 export const UMB_MANAGEMENT_API_SERVER_EVENT_CONTEXT = new UmbContextToken<UmbManagementApiServerEventContext>(
-	'UmbClipboardContext',
+	'UmbManagementApiServerEventContext',
 );
