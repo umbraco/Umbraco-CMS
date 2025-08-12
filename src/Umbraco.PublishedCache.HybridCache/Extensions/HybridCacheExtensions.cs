82ff6790bc5b3654a594c6b773945ab402d668b0
Andy Butland <abutland73@gmail.com>
2025-08-12 07:34:50 +0100

    Tidied up and populated XML header comments.

--- src/Umbraco.PublishedCache.HybridCache/Extensions/HybridCacheExtensions.cs
+++ src/Umbraco.PublishedCache.HybridCache/Extensions/HybridCacheExtensions.cs
@@ -27,10 +27,13 @@ internal static class HybridCacheExtensions
     /// Returns true if the cache contains an item with a matching key, along with the value of the matching cache entry.
     /// </summary>
     /// <typeparam name="T">The type of the value of the item in the cache.</typeparam>
-    /// <param name="cache">An instance of <see cref="HybridCache"/></param>
+    /// <param name="cache">An instance of <see cref="Microsoft.Extensions.Caching.Hybrid.HybridCache"/></param>
     /// <param name="key">The name (key) of the item to search for in the cache.</param>
     /// <returns>A tuple of <see cref="bool"/> and the object (if found) retrieved from the cache.</returns>
-    /// <remarks>Will never add or alter the state of any items in the cache.</remarks>
+    /// <remarks>
+    /// Hat-tip: https://github.com/dotnet/aspnetcore/discussions/57191
+    /// Will never add or alter the state of any items in the cache.
+    /// </remarks>
     public static async Task<(bool Exists, T? Value)> TryGetValueAsync<T>(this Microsoft.Extensions.Caching.Hybrid.HybridCache cache, string key)
     {
         var exists = true;
