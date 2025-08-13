44af43e0910a798cb1eec6365413f8baa0e63055
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-08-13 17:54:10 +0200

    chore: run eslint:fix

--- src/Umbraco.Web.UI.Client/src/libs/class-api/context.interface.ts
+++ src/Umbraco.Web.UI.Client/src/libs/class-api/context.interface.ts
@@ -1,6 +1,5 @@
 import type { UmbController } from '@umbraco-cms/backoffice/controller-api';
 
-// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface UmbContext extends UmbController {
 	getHostElement(): Element;
 }
