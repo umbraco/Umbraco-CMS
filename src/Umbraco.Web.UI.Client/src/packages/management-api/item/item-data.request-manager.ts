107e6d6c9fb5672f8984b2c4a3e3c3b6c99d85d1
Mads Rasmussen <madsr@hey.com>
2025-08-14 22:33:56 +0200

    introduce item cache

---
+++ src/Umbraco.Web.UI.Client/src/packages/management-api/item/item-data.request-manager.ts
@@ -0,0 +1,95 @@
+import { UMB_MANAGEMENT_API_SERVER_EVENT_CONTEXT } from '../server-event/constants.js';
+import type { UmbManagementApiItemDataCache } from './cache.js';
+import { tryExecute, UmbApiError, UmbCancelError, type UmbApiResponse } from '@umbraco-cms/backoffice/resources';
+import { UmbControllerBase } from '@umbraco-cms/backoffice/class-api';
+import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
+
+export interface UmbManagementApiItemDataRequestManagerArgs<ItemResponseModelType> {
+	getItems: (unique: Array<string>) => Promise<UmbApiResponse<{ data: Array<ItemResponseModelType> }>>;
+	dataCache: UmbManagementApiItemDataCache<ItemResponseModelType>;
+	serverEventSource: string;
+}
+
+export class UmbManagementApiItemDataRequestManager<ItemResponseModelType> extends UmbControllerBase {
+	#dataCache: UmbManagementApiItemDataCache<ItemResponseModelType>;
+	#serverEventSource: string;
+	#serverEventContext?: typeof UMB_MANAGEMENT_API_SERVER_EVENT_CONTEXT.TYPE;
+
+	#getItems;
+	#isConnectedToServerEvents = false;
+
+	constructor(host: UmbControllerHost, args: UmbManagementApiItemDataRequestManagerArgs<ItemResponseModelType>) {
+		super(host);
+
+		this.#getItems = args.getItems;
+		this.#dataCache = args.dataCache;
+		this.#serverEventSource = args.serverEventSource;
+
+		this.consumeContext(UMB_MANAGEMENT_API_SERVER_EVENT_CONTEXT, (context) => {
+			this.#serverEventContext = context;
+			this.#observeServerEvents();
+		});
+	}
+
+	async getItems(ids: Array<string>): Promise<UmbApiResponse<{ data?: Array<ItemResponseModelType> }>> {
+		let data: Array<ItemResponseModelType> | undefined;
+		let error: UmbApiError | UmbCancelError | undefined;
+		let idsToRequest: Array<string> = [...ids];
+		let cacheItems: Array<ItemResponseModelType> = [];
+		let serverItems: Array<ItemResponseModelType> | undefined;
+
+		// Only read from the cache when we are connected to the server events
+		if (this.#isConnectedToServerEvents) {
+			console.log(this.#dataCache);
+			const cachedIds = ids.filter((id) => this.#dataCache.has(id));
+			cacheItems = cachedIds
+				.map((id) => this.#dataCache.get(id))
+				.filter((x) => x !== undefined) as Array<ItemResponseModelType>;
+			idsToRequest = ids.filter((id) => !this.#dataCache.has(id));
+		}
+
+		if (idsToRequest.length > 0) {
+			const { data: serverData, error: serverError } = await tryExecute(this, this.#getItems(idsToRequest));
+
+			serverItems = serverData ?? [];
+			error = serverError;
+
+			if (this.#isConnectedToServerEvents) {
+				// If we are connected to server events, we can cache the server data
+				serverItems?.forEach((item) => this.#dataCache.set(item.id, item));
+			}
+		}
+
+		data = [...cacheItems, ...(serverItems ?? [])];
+
+		return { data, error };
+	}
+
+	#observeServerEvents() {
+		this.observe(
+			this.#serverEventContext?.isConnected,
+			(isConnected) => {
+				/* We purposefully ignore the initial value of isConnected.
+				We only care about whether the connection is established or not (true/false) */
+				if (isConnected === undefined) return;
+				this.#isConnectedToServerEvents = isConnected;
+
+				// Clear the cache if we lose connection to the server events
+				if (this.#isConnectedToServerEvents === false) {
+					this.#dataCache.clear();
+				}
+			},
+			'umbObserveServerEventsConnection',
+		);
+
+		// Invalidate cache entries when entities are updated or deleted
+		this.observe(
+			this.#serverEventContext?.byEventSourceAndTypes(this.#serverEventSource, ['Updated', 'Deleted']),
+			(event) => {
+				if (!event) return;
+				this.#dataCache.delete(event.key);
+			},
+			'umbObserveServerEvents',
+		);
+	}
+}
