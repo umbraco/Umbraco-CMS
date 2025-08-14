5639712f726495970b90578db9d821d04f01c6f0
Andreas Zerbst <andr317c@live.dk>
2025-08-14 23:32:49 +0200

    Get env

--- tests/Umbraco.Tests.AcceptanceTest/tests/ExternalLogin/AzureADB2C/AzureB2CSettings.cs
+++ tests/Umbraco.Tests.AcceptanceTest/tests/ExternalLogin/AzureADB2C/AzureB2CSettings.cs
@@ -2,10 +2,10 @@
 {
     public class AzureB2CSettings
     {
-        public string Domain { get; set; } = string.Empty;
-        public string Tenant { get; set; } = string.Empty;
-        public string Policy { get; set; } = string.Empty;
-        public string ClientId { get; set; } = string.Empty;
-        public string ClientSecret { get; set; } = string.Empty;
+        public string Domain { get; set; } = Environment.GetEnvironmentVariable("AZUREB2CDOMAIN") ?? string.Empty;
+        public string Tenant { get; set; } = Environment.GetEnvironmentVariable("AZUREB2CTENANT") ?? string.Empty;
+        public string Policy { get; set; } = Environment.GetEnvironmentVariable("AZUREB2CPOLICY") ?? string.Empty;
+        public string ClientId { get; set; } = Environment.GetEnvironmentVariable("AZUREB2CCLIENTID") ?? string.Empty;
+        public string ClientSecret { get; set; } = Environment.GetEnvironmentVariable("AZUREB2CCLIENTSECRET") ?? string.Empty;
     }
 }
