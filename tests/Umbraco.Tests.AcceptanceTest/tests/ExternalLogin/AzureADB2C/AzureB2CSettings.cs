d7045a2c9c4bebce15edb485c790c569d1eb9f0c
Andreas Zerbst <andr317c@live.dk>
2025-08-15 09:28:39 +0200

    Updated typo

--- tests/Umbraco.Tests.AcceptanceTest/tests/ExternalLogin/AzureADB2C/AzureB2CSettings.cs
+++ tests/Umbraco.Tests.AcceptanceTest/tests/ExternalLogin/AzureADB2C/AzureB2CSettings.cs
@@ -5,7 +5,7 @@
         public string Domain { get; set; } = Environment.GetEnvironmentVariable("AZUREADB2CDOMAIN") ?? string.Empty;
         public string Tenant { get; set; } = Environment.GetEnvironmentVariable("AZUREADB2CTENANT") ?? string.Empty;
         public string Policy { get; set; } = Environment.GetEnvironmentVariable("AZUREADB2CPOLICY") ?? string.Empty;
-        public string ClientId { get; set; } = Environment.GetEnvironmentVariable("AZURESDB2CCLIENTID") ?? string.Empty;
-        public string ClientSecret { get; set; } = Environment.GetEnvironmentVariable("AZURESDB2CCLIENTSECRET") ?? string.Empty;
+        public string ClientId { get; set; } = Environment.GetEnvironmentVariable("AZUREADB2CCLIENTID") ?? string.Empty;
+        public string ClientSecret { get; set; } = Environment.GetEnvironmentVariable("AZUREADB2CCLIENTSECRET") ?? string.Empty;
     }
 }
