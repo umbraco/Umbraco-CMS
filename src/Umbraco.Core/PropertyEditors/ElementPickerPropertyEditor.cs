3a53a9b6d0028e932657be484e6298323dab0449
kjac <kja@umbraco.dk>
2025-08-15 13:14:44 +0200

    Element picker property editor

---
+++ src/Umbraco.Core/PropertyEditors/ElementPickerPropertyEditor.cs
@@ -0,0 +1,39 @@
+﻿using Umbraco.Cms.Core.IO;
+using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Models.Editors;
+using Umbraco.Cms.Core.Serialization;
+using Umbraco.Cms.Core.Strings;
+
+namespace Umbraco.Cms.Core.PropertyEditors;
+
+/// <summary>
+///     Element picker property editor that stores element keys
+/// </summary>
+[DataEditor(
+    Constants.PropertyEditors.Aliases.ElementPicker,
+    ValueType = ValueTypes.Json,
+    ValueEditorIsReusable = true)]
+public class ElementPickerPropertyEditor : DataEditor
+{
+    public ElementPickerPropertyEditor(IDataValueEditorFactory dataValueEditorFactory)
+        : base(dataValueEditorFactory)
+        => SupportsReadOnly = true;
+
+    protected override IDataValueEditor CreateValueEditor() =>
+        DataValueEditorFactory.Create<ElementPickerPropertyValueEditor>(Attribute!);
+
+    internal sealed class ElementPickerPropertyValueEditor : DataValueEditor, IDataValueReference
+    {
+        public ElementPickerPropertyValueEditor(
+            IShortStringHelper shortStringHelper,
+            IJsonSerializer jsonSerializer,
+            IIOHelper ioHelper,
+            DataEditorAttribute attribute)
+            : base(shortStringHelper, jsonSerializer, ioHelper, attribute)
+        {
+        }
+
+        // TODO ELEMENTS: implement reference tracking from element picker
+        public IEnumerable<UmbracoEntityReference> GetReferences(object? value) => [];
+    }
+}
---
+++ src/Umbraco.Web.UI.Client/src/packages/property-editors/element-picker/Umbraco.ElementPicker.ts
@@ -0,0 +1,10 @@
+﻿import type { ManifestPropertyEditorSchema } from '@umbraco-cms/backoffice/property-editor';
+
+export const manifest: ManifestPropertyEditorSchema = {
+	type: 'propertyEditorSchema',
+	name: 'Element Picker',
+	alias: 'Umbraco.ElementPicker',
+	meta: {
+		defaultPropertyEditorUiAlias: 'Umb.PropertyEditorUi.ElementPicker',
+	},
+};
