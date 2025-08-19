0c0cc0253c175ae8910e0cd26fc6df238374e6b2
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-08-19 10:53:34 +0200

    fix: always redirect to stored path even on failure
    the user may have landed up on the page by mistake

--- src/Umbraco.Web.UI.Client/src/apps/app/app.element.ts
+++ src/Umbraco.Web.UI.Client/src/apps/app/app.element.ts
@@ -19,7 +19,7 @@ import {
 	umbExtensionsRegistry,
 } from '@umbraco-cms/backoffice/extension-registry';
 import { filter, first, firstValueFrom } from '@umbraco-cms/backoffice/external/rxjs';
-import { hasOwnOpener, retrieveStoredPath } from '@umbraco-cms/backoffice/utils';
+import { hasOwnOpener, redirectToStoredPath } from '@umbraco-cms/backoffice/utils';
 import { UmbApiInterceptorController } from '@umbraco-cms/backoffice/resources';
 import { umbHttpClient } from '@umbraco-cms/backoffice/http-client';
 
@@ -65,6 +65,7 @@ export class UmbAppElement extends UmbLitElement {
 				if (!this.#authContext) {
 					(component as UmbAppOauthElement).failure = true;
 					console.error('[Fatal] Auth context is not available');
+					redirectToStoredPath(this.backofficePath);
 					return;
 				}
 
@@ -73,6 +74,7 @@ export class UmbAppElement extends UmbLitElement {
 				if (!hasCode) {
 					(component as UmbAppOauthElement).failure = true;
 					console.error('[Fatal] No code in query parameters');
+					redirectToStoredPath(this.backofficePath);
 					return;
 				}
 
@@ -86,17 +88,7 @@ export class UmbAppElement extends UmbLitElement {
 							filter((x) => !!x),
 							first(),
 						),
-						() => {
-							// Redirect to the saved state or root
-							const url = retrieveStoredPath();
-							const isBackofficePath = url?.pathname.startsWith(this.backofficePath) ?? true;
-
-							if (isBackofficePath) {
-								history.replaceState(null, '', url?.toString() ?? '');
-							} else {
-								window.location.href = url?.toString() ?? this.backofficePath;
-							}
-						},
+						() => redirectToStoredPath(this.backofficePath),
 					);
 				}
 
