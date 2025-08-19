03404efb21ce13ebd4d6cfa82ff8c4114035371f
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-08-19 11:38:05 +0200

    feat: allows function to force a window redirect

--- src/Umbraco.Web.UI.Client/src/packages/core/utils/path/stored-path.function.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/utils/path/stored-path.function.ts
@@ -35,12 +35,13 @@ export function setStoredPath(path: string): void {
  * Redirect the user to the stored path or the base path if not available.
  * If the basePath matches the start of the stored path, the browser will replace the state instead of redirecting.
  * @param {string} basePath - The base path to redirect to if no stored path is available.
+ * @param {boolean} [force=false] - If true, will redirect using Location
  */
-export function redirectToStoredPath(basePath: string): void {
+export function redirectToStoredPath(basePath: string, force = false): void {
 	const url = retrieveStoredPath();
 	const isBackofficePath = url?.pathname.startsWith(basePath) ?? true;
 
-	if (isBackofficePath) {
+	if (isBackofficePath && !force) {
 		history.replaceState(null, '', url?.toString() ?? '');
 	} else {
 		window.location.href = url?.toString() ?? basePath;
