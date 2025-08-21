a729442cf93109553a3100b1fa0b955382f93e8b
NillasKA <kramernicklas@gmail.com>
2025-08-21 12:56:05 +0200

    Removing obsoleted code from RequestCultureService.cs

--- src/Umbraco.Core/DeliveryApi/IRequestCultureService.cs
+++ src/Umbraco.Core/DeliveryApi/IRequestCultureService.cs
@@ -6,7 +6,4 @@ public interface IRequestCultureService
     ///     Gets the requested culture from the "Accept-Language" header, if present.
     /// </summary>
     string? GetRequestedCulture();
-
-    [Obsolete("Use IVariationContextAccessor to manipulate the variation context. Scheduled for removal in V17.")]
-    void SetRequestCulture(string culture);
 }
