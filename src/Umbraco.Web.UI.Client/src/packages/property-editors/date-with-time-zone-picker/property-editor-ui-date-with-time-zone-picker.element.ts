00bc2f26e443bd3eaa77b449b625dac6f821c9b3
Laura Neto <12862535+lauraneto@users.noreply.github.com>
2025-08-21 12:23:38 +0200

    Fixed searches with regex special characters throwing errors

--- src/Umbraco.Web.UI.Client/src/packages/property-editors/date-with-time-zone-picker/property-editor-ui-date-with-time-zone-picker.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/property-editors/date-with-time-zone-picker/property-editor-ui-date-with-time-zone-picker.element.ts
@@ -287,7 +287,7 @@ export class UmbPropertyEditorUIDateWithTimeZonePickerElement
 	#onTimeZoneSearch(event: UUIComboboxEvent) {
 		const searchTerm = (event.target as UUIComboboxElement)?.search;
 		this._filteredTimeZoneOptions = this._timeZoneOptions.filter(
-			(option) => new RegExp(searchTerm, 'i').test(option.name) || option.offset === searchTerm,
+			(option) => option.name.toLowerCase().includes(searchTerm.toLowerCase()) || option.offset === searchTerm,
 		);
 	}
 
