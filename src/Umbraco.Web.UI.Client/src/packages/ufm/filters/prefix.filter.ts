af4bb4076667f7b85307f90465147d682ca8b759
Erik-Jan Westendorp <erikjanwestendorp@outlook.com>
2025-08-26 16:43:50 +0200

    Remove console.log

--- src/Umbraco.Web.UI.Client/src/packages/ufm/filters/prefix.filter.ts
+++ src/Umbraco.Web.UI.Client/src/packages/ufm/filters/prefix.filter.ts
@@ -3,7 +3,6 @@ import { UmbUfmFilterBase } from './base.filter.js';
 class UmbUfmPrefixFilterApi extends UmbUfmFilterBase {
 
   	filter(str?: string, prefix: string = '') {
-        console.log('Prefix filter called with:', { str, prefix });
         if (!str) return '';
 		return `${prefix} ${str}`;
 	}
