ac410f0883c49207e2cb512219cca73435251c36
Niels Lyngsø <niels.lyngso@gmail.com>
2025-08-27 10:13:03 +0200

    clean up

--- src/Umbraco.Web.UI.Client/src/packages/core/workspace/components/workspace-footer/workspace-footer.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/workspace/components/workspace-footer/workspace-footer.element.ts
@@ -29,9 +29,6 @@ function ExtensionApiArgsMethod(
 @customElement('umb-workspace-footer')
 export class UmbWorkspaceFooterLayoutElement extends UmbLitElement {
 	@state()
-	private _withinModal = false;
-
-	@state()
 	private _modalContext?: UmbModalContext;
 
 	@state()
@@ -63,13 +60,12 @@ export class UmbWorkspaceFooterLayoutElement extends UmbLitElement {
 							label=${this._isNew ? 'Cancel' : 'Close'}
 							@click=${this.#rejectModal}></uui-button>`
 					: ''}
+
 				<slot name="actions" slot="actions"></slot>
 				<umb-extension-with-api-slot
 					slot="actions"
 					type="workspaceAction"
 					.apiArgs=${ExtensionApiArgsMethod}></umb-extension-with-api-slot>
-
-				<slot name="actions" slot="actions"></slot>
 			</umb-footer-layout>
 		`;
 	}
