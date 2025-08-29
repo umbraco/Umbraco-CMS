1013a04972b2bff2a8e62fedbdfae135166840d5
Nhu Dinh <hnd@umbraco.dk>
2025-08-29 11:47:09 +0700

    Added tests for content delivery api

---
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DeliveryApi/ContentDeliveryApi.spec.ts
@@ -0,0 +1,27 @@
+import {expect} from '@playwright/test';
+import {AliasHelper, test} from '@umbraco/playwright-testhelpers';
+
+test('can get content from delivery api', async ({umbracoApi}) => {
+  // Arrange
+  const documentTypeName = 'TestDocumentType';
+  const contentName = 'TestContent';
+  const dataTypeName = 'Textstring';
+  const textStringValue = 'This is a test text string value';
+  await umbracoApi.documentType.ensureNameNotExists(documentTypeName);
+  const dataType = await umbracoApi.dataType.getByName(dataTypeName);
+  const documentTypeId = await umbracoApi.documentType.createDocumentTypeWithPropertyEditor(documentTypeName, dataTypeName, dataType.id, 'TestGroup');
+  const documentId = await umbracoApi.document.createDocumentWithTextContent(contentName, documentTypeId, textStringValue, dataTypeName);
+  const propertyValue = {
+    dataTypeName: AliasHelper.toAlias(dataTypeName),
+    dataTypeValue: textStringValue
+  }
+
+  // Act
+  await umbracoApi.document.publish(documentId);
+
+  // Assert
+  expect(await umbracoApi.contentDeliveryApi.doesContentItemWithIdContainValues(documentId, contentName, AliasHelper.toAlias(documentTypeName), [propertyValue])).toBeTruthy();
+
+  // Clean
+  await umbracoApi.documentType.ensureNameNotExists(documentTypeName);
+});
