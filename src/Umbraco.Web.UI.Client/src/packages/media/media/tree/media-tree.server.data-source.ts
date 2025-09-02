7a74f0a0a19f315bed1b11840441e0616c651d7c
Mads Rasmussen <madsr@hey.com>
2025-09-02 12:47:22 +0200

    use request manager for media tree

--- src/Umbraco.Web.UI.Client/src/packages/media/media/tree/media-tree.server.data-source.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/tree/media-tree.server.data-source.ts
@@ -4,82 +4,80 @@ import type {
 	UmbMediaTreeItemModel,
 	UmbMediaTreeRootItemsRequestArgs,
 } from './types.js';
-import type { UmbTreeAncestorsOfRequestArgs } from '@umbraco-cms/backoffice/tree';
-import { UmbTreeServerDataSourceBase } from '@umbraco-cms/backoffice/tree';
-import { MediaService, type MediaTreeItemResponseModel } from '@umbraco-cms/backoffice/external/backend-api';
-import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
+import { UmbManagementApiMediaTreeDataRequestManager } from './server-data-source/media-tree.server.request-manager.js';
+import type { UmbTreeAncestorsOfRequestArgs, UmbTreeDataSource } from '@umbraco-cms/backoffice/tree';
+import { UmbControllerBase } from '@umbraco-cms/backoffice/class-api';
+import type { MediaTreeItemResponseModel } from '@umbraco-cms/backoffice/external/backend-api';
 
 /**
  * A data source for the Media tree that fetches data from the server
  * @class UmbMediaTreeServerDataSource
- * @augments {UmbTreeServerDataSourceBase}
  */
-export class UmbMediaTreeServerDataSource extends UmbTreeServerDataSourceBase<
-	MediaTreeItemResponseModel,
-	UmbMediaTreeItemModel,
-	UmbMediaTreeRootItemsRequestArgs,
-	UmbMediaTreeChildrenOfRequestArgs
-> {
-	/**
-	 * Creates an instance of UmbMediaTreeServerDataSource.
-	 * @param {UmbControllerHost} host - The controller host for this controller to be appended to
-	 * @memberof UmbMediaTreeServerDataSource
-	 */
-	constructor(host: UmbControllerHost) {
-		super(host, {
-			getRootItems,
-			getChildrenOf,
-			getAncestorsOf,
-			mapper,
-		});
+export class UmbMediaTreeServerDataSource
+	extends UmbControllerBase
+	implements UmbTreeDataSource<UmbMediaTreeItemModel>
+{
+	#treeRequestManager = new UmbManagementApiMediaTreeDataRequestManager(this);
+
+	async getRootItems(args: UmbMediaTreeRootItemsRequestArgs) {
+		const { data, error } = await this.#treeRequestManager.getRootItems(args);
+
+		const mappedData = data
+			? {
+					...data,
+					items: data?.items.map((item) => this.#mapItem(item)),
+				}
+			: undefined;
+
+		return { data: mappedData, error };
+	}
+
+	async getChildrenOf(args: UmbMediaTreeChildrenOfRequestArgs) {
+		const { data, error } = await this.#treeRequestManager.getChildrenOf(args);
+
+		const mappedData = data
+			? {
+					...data,
+					items: data?.items.map((item) => this.#mapItem(item)),
+				}
+			: undefined;
+
+		return { data: mappedData, error };
+	}
+
+	async getAncestorsOf(args: UmbTreeAncestorsOfRequestArgs) {
+		const { data, error } = await this.#treeRequestManager.getAncestorsOf(args);
+
+		const mappedData = data?.map((item) => this.#mapItem(item));
+
+		return { data: mappedData, error };
+	}
+
+	#mapItem(item: MediaTreeItemResponseModel): UmbMediaTreeItemModel {
+		return {
+			unique: item.id,
+			parent: {
+				unique: item.parent ? item.parent.id : null,
+				entityType: item.parent ? UMB_MEDIA_ENTITY_TYPE : UMB_MEDIA_ROOT_ENTITY_TYPE,
+			},
+			entityType: UMB_MEDIA_ENTITY_TYPE,
+			hasChildren: item.hasChildren,
+			noAccess: item.noAccess,
+			isTrashed: item.isTrashed,
+			isFolder: false,
+			mediaType: {
+				unique: item.mediaType.id,
+				icon: item.mediaType.icon,
+				collection: item.mediaType.collection ? { unique: item.mediaType.collection.id } : null,
+			},
+			name: item.variants[0]?.name, // TODO: this is not correct. We need to get it from the variants. This is a temp solution.
+			variants: item.variants.map((variant) => {
+				return {
+					name: variant.name,
+					culture: variant.culture || null,
+				};
+			}),
+			createDate: item.createDate,
+		};
 	}
 }
-
-const getRootItems = (args: UmbMediaTreeRootItemsRequestArgs) =>
-	// eslint-disable-next-line local-rules/no-direct-api-import
-	MediaService.getTreeMediaRoot({ query: { dataTypeId: args.dataType?.unique, skip: args.skip, take: args.take } });
-
-const getChildrenOf = (args: UmbMediaTreeChildrenOfRequestArgs) => {
-	if (args.parent.unique === null) {
-		return getRootItems(args);
-	} else {
-		// eslint-disable-next-line local-rules/no-direct-api-import
-		return MediaService.getTreeMediaChildren({
-			query: { parentId: args.parent.unique, dataTypeId: args.dataType?.unique, skip: args.skip, take: args.take },
-		});
-	}
-};
-
-const getAncestorsOf = (args: UmbTreeAncestorsOfRequestArgs) =>
-	// eslint-disable-next-line local-rules/no-direct-api-import
-	MediaService.getTreeMediaAncestors({
-		query: { descendantId: args.treeItem.unique },
-	});
-
-const mapper = (item: MediaTreeItemResponseModel): UmbMediaTreeItemModel => {
-	return {
-		unique: item.id,
-		parent: {
-			unique: item.parent ? item.parent.id : null,
-			entityType: item.parent ? UMB_MEDIA_ENTITY_TYPE : UMB_MEDIA_ROOT_ENTITY_TYPE,
-		},
-		entityType: UMB_MEDIA_ENTITY_TYPE,
-		hasChildren: item.hasChildren,
-		noAccess: item.noAccess,
-		isTrashed: item.isTrashed,
-		isFolder: false,
-		mediaType: {
-			unique: item.mediaType.id,
-			icon: item.mediaType.icon,
-			collection: item.mediaType.collection ? { unique: item.mediaType.collection.id } : null,
-		},
-		name: item.variants[0]?.name, // TODO: this is not correct. We need to get it from the variants. This is a temp solution.
-		variants: item.variants.map((variant) => {
-			return {
-				name: variant.name,
-				culture: variant.culture || null,
-			};
-		}),
-		createDate: item.createDate,
-	};
-};
