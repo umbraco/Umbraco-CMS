aed121845ed9d908a508fc58313984aff5e265e9
Mads Rasmussen <madsr@hey.com>
2025-09-02 13:16:43 +0200

    add request manager for data type tree

--- src/Umbraco.Web.UI.Client/src/packages/data-type/tree/server-data-source/data-type-tree.server.request-manager.ts
+++ src/Umbraco.Web.UI.Client/src/packages/data-type/tree/server-data-source/data-type-tree.server.request-manager.ts
@@ -1,36 +1,65 @@
 /* eslint-disable local-rules/no-direct-api-import */
-
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import {
 	DataTypeService,
 	type DataTypeTreeItemResponseModel,
-	type GetTreeDataTypeAncestorsData,
-	type GetTreeDataTypeChildrenData,
-	type GetTreeDataTypeRootData,
 	type PagedDataTypeTreeItemResponseModel,
+	type SubsetDataTypeTreeItemResponseModel,
 } from '@umbraco-cms/backoffice/external/backend-api';
-import { UmbManagementApiTreeDataRequestManager } from '@umbraco-cms/backoffice/management-api';
+import {
+	UmbManagementApiTreeDataRequestManager,
+	type UmbManagementApiTreeAncestorsOfRequestArgs,
+	type UmbManagementApiTreeChildrenOfRequestArgs,
+	type UmbManagementApiTreeRootItemsRequestArgs,
+	type UmbManagementApiTreeSiblingsFromRequestArgs,
+} from '@umbraco-cms/backoffice/management-api';
 
 export class UmbManagementApiDataTypeTreeDataRequestManager extends UmbManagementApiTreeDataRequestManager<
+	DataTypeTreeItemResponseModel,
+	UmbManagementApiTreeRootItemsRequestArgs,
 	PagedDataTypeTreeItemResponseModel,
+	UmbManagementApiTreeChildrenOfRequestArgs,
 	PagedDataTypeTreeItemResponseModel,
-	DataTypeTreeItemResponseModel
+	UmbManagementApiTreeAncestorsOfRequestArgs,
+	Array<DataTypeTreeItemResponseModel>,
+	UmbManagementApiTreeSiblingsFromRequestArgs,
+	SubsetDataTypeTreeItemResponseModel
 > {
 	constructor(host: UmbControllerHost) {
 		super(host, {
-			getRootItems: (args: GetTreeDataTypeRootData['query']) =>
+			getRootItems: (args) =>
 				DataTypeService.getTreeDataTypeRoot({
-					query: { foldersOnly: args.foldersOnly, skip: args.skip, take: args.take },
+					query: {
+						foldersOnly: args.foldersOnly,
+						skip: args.paging.skip,
+						take: args.paging.take,
+					},
 				}),
 
-			getChildrenOf: (args: GetTreeDataTypeChildrenData['query']) =>
+			getChildrenOf: (args) =>
 				DataTypeService.getTreeDataTypeChildren({
-					query: { parentId: args.parentId, foldersOnly: args.foldersOnly, skip: args.skip, take: args.take },
+					query: {
+						parentId: args.parent.unique,
+						foldersOnly: args.foldersOnly,
+						skip: args.paging.skip,
+						take: args.paging.take,
+					},
 				}),
 
-			getAncestorsOf: (args: GetTreeDataTypeAncestorsData['query']) =>
+			getAncestorsOf: (args) =>
 				DataTypeService.getTreeDataTypeAncestors({
-					query: { descendantId: args.descendantId },
+					query: {
+						descendantId: args.treeItem.unique,
+					},
+				}),
+
+			getSiblingsFrom: (args) =>
+				DataTypeService.getTreeDataTypeSiblings({
+					query: {
+						target: args.paging.target.unique,
+						before: args.paging.takeBefore,
+						after: args.paging.takeAfter,
+					},
 				}),
 		});
 	}
