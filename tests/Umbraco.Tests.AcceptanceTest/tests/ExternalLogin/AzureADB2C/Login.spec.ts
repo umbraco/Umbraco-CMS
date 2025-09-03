b95f7d40efea2e56073f516bae1c072e0d056f67
Andreas Zerbst <andr317c@live.dk>
2025-09-03 11:57:12 +0200

    Updated locator

--- tests/Umbraco.Tests.AcceptanceTest/tests/ExternalLogin/AzureADB2C/Login.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/ExternalLogin/AzureADB2C/Login.spec.ts
@@ -1,21 +1,19 @@
-// To be able to test different databases, we need to set an additional UnattendedInstallConfig up because we would have to start from scratch, otherwise we would be using the same database.
 import {ConstantHelper, test} from '@umbraco/playwright-testhelpers';
 
 const azureEmail = process.env.AZUREADB2CTESTUSEREMAIL;
 const azurePassword = process.env.AZUREADB2CTESTUSERPASSWORD;
 
 // Really simple test to check if we can log in using Azure AD B2C
-test('Log in to Umbraco using Azure AD B2C', async ({page, umbracoUi}) => {
+test('Log in to Umbraco using Azure AD B2C', async ({umbracoUi}) => {
   test.slow();
   // Arrange
   await umbracoUi.goToBackOffice();
 
-  await page.locator('umb-auth-provider-default').getByText('Sign in with Sign in with Azure AD B2C').click();
-
-
-  await page.locator('#email').fill(azureEmail);
-  await page.locator('#password').fill(azurePassword);
-  await page.getByRole('button', { name: 'Sign in' }).click();
+  // Act
+  await umbracoUi.login.clickSignInWithAzureAdB2CButton();
+  await umbracoUi.login.enterAzureAdB2CEmail(azureEmail);
+  await umbracoUi.login.enterAzureAdB2CPassword(azurePassword);
+  await umbracoUi.login.clickSignInButton();
 
   // Assert
   await umbracoUi.content.goToSection(ConstantHelper.sections.content);
