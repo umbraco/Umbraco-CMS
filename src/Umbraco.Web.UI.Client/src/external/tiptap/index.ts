umbraco/Umbraco-CMS

commit d988cb93758e8cbf601a1a8877fc7d90aa8f4169

Workspace View Manifest Overrides (#20033)
* POC of manifest overrides

* clean up

* Collection Manager for Content Workspaces, refactor of existing solution

* lint fix

src/Umbraco.Web.UI.Client/src/packages/content/content/collection/content-collection-workspace.context-token.ts
@@ -10,5 +10,5 @@ export const UMB_CONTENT_COLLECTION_WORKSPACE_CONTEXT = new UmbContextToken<
 	'UmbWorkspaceContext',
 	undefined,
 	(context): context is UmbContentCollectionWorkspaceContext<UmbContentTypeModel> =>
-		(context as UmbContentCollectionWorkspaceContext<UmbContentTypeModel>).contentTypeHasCollection !== undefined,
+		(context as UmbContentCollectionWorkspaceContext<UmbContentTypeModel>).collection.hasCollection !== undefined,
 );

src/Umbraco.Web.UI.Client/src/packages/content/content/collection/index.ts
@@ -1,2 +1,3 @@
 export type * from './content-collection-workspace-context.interface.js';
 export * from './content-collection-workspace.context-token.js';
+export * from './manager/content-collection-manager.controller.js';

src/Umbraco.Web.UI.Client/src/packages/content/content/collection/manifests.ts
@@ -1,4 +1,6 @@
+import { UMB_WORKSPACE_HAS_CONTENT_COLLECTION_CONDITION_ALIAS } from './workspace-has-content-collection/constants.js';
 import { manifests as workspaceHasContentCollectionManifests } from './workspace-has-content-collection/manifests.js';
+import { UMB_WORKSPACE_ENTITY_IS_NEW_CONDITION_ALIAS } from '@umbraco-cms/backoffice/workspace';
 import type { UmbExtensionManifestKind } from '@umbraco-cms/backoffice/extension-registry';
 
 export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> = [
@@ -18,6 +20,27 @@ export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> =
 				icon: 'icon-grid',
 			},
 		},
+	},
+	{
+		type: 'workspaceView',
+		kind: 'contentCollection',
+		alias: 'Umb.WorkspaceView.Content.Collection',
+		name: 'Content Workspace Collection View',
+		weight: 1000,
+		meta: {
+			label: 'Collection',
+			pathname: 'collection',
+			icon: 'icon-grid',
+		},
+		conditions: [
+			{
+				alias: UMB_WORKSPACE_HAS_CONTENT_COLLECTION_CONDITION_ALIAS,
+			},
+			{
+				alias: UMB_WORKSPACE_ENTITY_IS_NEW_CONDITION_ALIAS,
+				match: false,
+			},
+		],
 	},
 	...workspaceHasContentCollectionManifests,
 ];

src/Umbraco.Web.UI.Client/src/packages/core/collection/types.ts
@@ -1,5 +1,6 @@
 import type { ManifestCollection } from './extensions/types.js';
 import type { Observable } from '@umbraco-cms/backoffice/external/rxjs';
+import type { UmbEntityUnique } from '@umbraco-cms/backoffice/entity';
 import type { UmbPaginationManager } from '@umbraco-cms/backoffice/utils';
 
 export type * from './action/create/types.js';
@@ -17,7 +18,7 @@ export interface UmbCollectionBulkActionPermissions {
 }
 
 export interface UmbCollectionConfiguration {
-	unique?: string;
+	unique?: UmbEntityUnique;
 	dataTypeId?: string;
 	/** @deprecated No longer used internally. This will be removed in Umbraco 17. [LK] */
 	allowedEntityBulkActions?: UmbCollectionBulkActionPermissions;

src/Umbraco.Web.UI.Client/src/packages/documents/documents/workspace/manifests.ts
@@ -1,14 +1,8 @@
 import { UMB_DOCUMENT_ENTITY_TYPE } from '../entity.js';
 import { UMB_DOCUMENT_WORKSPACE_ALIAS } from './constants.js';
 import { manifests as actionManifests } from './actions/manifests.js';
-import {
-	UMB_CONTENT_HAS_PROPERTIES_WORKSPACE_CONDITION,
-	UMB_WORKSPACE_HAS_CONTENT_COLLECTION_CONDITION_ALIAS,
-} from '@umbraco-cms/backoffice/content';
-import {
-	UMB_WORKSPACE_CONDITION_ALIAS,
-	UMB_WORKSPACE_ENTITY_IS_NEW_CONDITION_ALIAS,
-} from '@umbraco-cms/backoffice/workspace';
+import { UMB_CONTENT_HAS_PROPERTIES_WORKSPACE_CONDITION } from '@umbraco-cms/backoffice/content';
+import { UMB_WORKSPACE_CONDITION_ALIAS } from '@umbraco-cms/backoffice/workspace';
 
 export const manifests: Array<UmbExtensionManifest> = [
 	{
@@ -20,30 +14,6 @@ export const manifests: Array<UmbExtensionManifest> = [
 		meta: {
 			entityType: UMB_DOCUMENT_ENTITY_TYPE,
 		},
-	},
-	{
-		type: 'workspaceView',
-		kind: 'contentCollection',
-		alias: 'Umb.WorkspaceView.Document.Collection',
-		name: 'Document Workspace Collection View',
-		meta: {
-			label: 'Collection',
-			pathname: 'collection',
-			icon: 'icon-grid',
-		},
-		conditions: [
-			{
-				alias: UMB_WORKSPACE_CONDITION_ALIAS,
-				match: UMB_DOCUMENT_WORKSPACE_ALIAS,
-			},
-			{
-				alias: UMB_WORKSPACE_HAS_CONTENT_COLLECTION_CONDITION_ALIAS,
-			},
-			{
-				alias: UMB_WORKSPACE_ENTITY_IS_NEW_CONDITION_ALIAS,
-				match: false,
-			},
-		],
 	},
 	{
 		type: 'workspaceView',

src/Umbraco.Web.UI.Client/src/packages/media/media/workspace/manifests.ts
@@ -1,14 +1,7 @@
 import { UMB_MEDIA_WORKSPACE_ALIAS } from './constants.js';
-import {
-	UmbSubmitWorkspaceAction,
-	UMB_WORKSPACE_CONDITION_ALIAS,
-	UMB_WORKSPACE_ENTITY_IS_NEW_CONDITION_ALIAS,
-} from '@umbraco-cms/backoffice/workspace';
+import { UmbSubmitWorkspaceAction, UMB_WORKSPACE_CONDITION_ALIAS } from '@umbraco-cms/backoffice/workspace';
+import { UMB_CONTENT_HAS_PROPERTIES_WORKSPACE_CONDITION } from '@umbraco-cms/backoffice/content';
 import { UMB_ENTITY_IS_NOT_TRASHED_CONDITION_ALIAS } from '@umbraco-cms/backoffice/recycle-bin';
-import {
-	UMB_CONTENT_HAS_PROPERTIES_WORKSPACE_CONDITION,
-	UMB_WORKSPACE_HAS_CONTENT_COLLECTION_CONDITION_ALIAS,
-} from '@umbraco-cms/backoffice/content';
 
 export const manifests: Array<UmbExtensionManifest> = [
 	{
@@ -20,30 +13,6 @@ export const manifests: Array<UmbExtensionManifest> = [
 		meta: {
 			entityType: 'media',
 		},
-	},
-	{
-		type: 'workspaceView',
-		kind: 'contentCollection',
-		alias: 'Umb.WorkspaceView.Media.Collection',
-		name: 'Media Workspace Collection View',
-		meta: {
-			label: 'Collection',
-			pathname: 'collection',
-			icon: 'icon-grid',
-		},
-		conditions: [
-			{
-				alias: UMB_WORKSPACE_CONDITION_ALIAS,
-				match: UMB_MEDIA_WORKSPACE_ALIAS,
-			},
-			{
-				alias: UMB_WORKSPACE_HAS_CONTENT_COLLECTION_CONDITION_ALIAS,
-			},
-			{
-				alias: UMB_WORKSPACE_ENTITY_IS_NEW_CONDITION_ALIAS,
-				match: false,
-			},
-		],
 	},
 	{
 		type: 'workspaceView',

src/Umbraco.Web.UI.Client/src/packages/content/content/collection/content-collection-workspace-context.interface.ts
@@ -1,9 +1,8 @@
-import type { Observable } from '@umbraco-cms/backoffice/external/rxjs';
+import type { UmbContentCollectionManager } from './manager/content-collection-manager.controller.js';
 import type { UmbContentTypeModel, UmbContentTypeStructureManager } from '@umbraco-cms/backoffice/content-type';
 import type { UmbEntityWorkspaceContext } from '@umbraco-cms/backoffice/workspace';
 
 export interface UmbContentCollectionWorkspaceContext<T extends UmbContentTypeModel> extends UmbEntityWorkspaceContext {
-	contentTypeHasCollection: Observable<boolean>;
-	getCollectionAlias(): string;
+	collection: UmbContentCollectionManager;
 	structure: UmbContentTypeStructureManager<T>;
 }

src/Umbraco.Web.UI.Client/src/packages/content/content/collection/content-collection-workspace-view.element.ts
@@ -1,13 +1,9 @@
 import { UMB_CONTENT_COLLECTION_WORKSPACE_CONTEXT } from './content-collection-workspace.context-token.js';
 import { customElement, html, nothing, state } from '@umbraco-cms/backoffice/external/lit';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
-import { UmbDataTypeDetailRepository } from '@umbraco-cms/backoffice/data-type';
-import { UmbPropertyEditorConfigCollection } from '@umbraco-cms/backoffice/property-editor';
-import type { UmbDataTypeDetailModel } from '@umbraco-cms/backoffice/data-type';
-import type { UmbWorkspaceViewElement } from '@umbraco-cms/backoffice/workspace';
 import type { UmbCollectionConfiguration } from '@umbraco-cms/backoffice/collection';
+import type { UmbWorkspaceViewElement } from '@umbraco-cms/backoffice/workspace';
 
-const elementName = 'umb-content-collection-workspace-view';
 @customElement('umb-content-collection-workspace-view')
 export class UmbContentCollectionWorkspaceViewElement extends UmbLitElement implements UmbWorkspaceViewElement {
 	@state()
@@ -19,57 +15,23 @@ export class UmbContentCollectionWorkspaceViewElement extends UmbLitElement impl
 	@state()
 	private _collectionAlias?: string;
 
-	@state()
-	private _documentUnique?: string;
-
-	#dataTypeDetailRepository = new UmbDataTypeDetailRepository(this);
-
 	constructor() {
 		super();
-		this.#observeConfig();
-	}
 
-	async #observeConfig() {
 		this.consumeContext(UMB_CONTENT_COLLECTION_WORKSPACE_CONTEXT, (workspaceContext) => {
-			this._collectionAlias = workspaceContext?.getCollectionAlias();
-			this._documentUnique = workspaceContext?.getUnique() ?? '';
+			this._collectionAlias = workspaceContext?.collection.getCollectionAlias();
 
 			this.observe(
-				workspaceContext?.structure.ownerContentType,
-				async (contentType) => {
-					if (!contentType || !contentType.collection) return;
-
-					const dataTypeUnique = contentType.collection.unique;
-
-					if (dataTypeUnique) {
-						await this.#dataTypeDetailRepository.requestByUnique(dataTypeUnique);
-						this.observe(
-							await this.#dataTypeDetailRepository.byUnique(dataTypeUnique),
-							(dataType) => {
-								if (!dataType) return;
-								this._config = this.#mapDataTypeConfigToCollectionConfig(dataType);
-								this._loading = false;
-							},
-							'_observeConfigDataType',
-						);
+				workspaceContext?.collection.collectionConfig,
+				(config) => {
+					if (config) {
+						this._config = config;
+						this._loading = false;
 					}
 				},
 				'_observeConfigContentType',
 			);
 		});
-	}
-
-	#mapDataTypeConfigToCollectionConfig(dataType: UmbDataTypeDetailModel): UmbCollectionConfiguration {
-		const config = new UmbPropertyEditorConfigCollection(dataType.values);
-		const pageSize = Number(config.getValueByAlias('pageSize'));
-		return {
-			unique: this._documentUnique,
-			layouts: config?.getValueByAlias('layouts'),
-			orderBy: config?.getValueByAlias('orderBy') ?? 'updateDate',
-			orderDirection: config?.getValueByAlias('orderDirection') ?? 'asc',
-			pageSize: isNaN(pageSize) ? 50 : pageSize,
-			userDefinedProperties: config?.getValueByAlias('includeProperties'),
-		};
 	}
 
 	override render() {
@@ -82,6 +44,6 @@ export { UmbContentCollectionWorkspaceViewElement as element };
 
 declare global {
 	interface HTMLElementTagNameMap {
-		[elementName]: UmbContentCollectionWorkspaceViewElement;
+		'umb-content-collection-workspace-view': UmbContentCollectionWorkspaceViewElement;
 	}
 }

commit 1b5734d1c8fc795aadfdb5e7ac7232e57498c771

Tiptap RTE: Capabilities Tidy-up (#20078)
* Updates RTE mock data

* UFM: Adds fallback for "monospace" font-family

* Removes the Font Family/Sizes Menu extension

This feature is not ready yet.

* Tighten up Tiptap config buttons style

* Fixes bug with Collections context-token

Unrelated to Tiptap, but causes data-types to throw an error.
Bug introduced in PR #20033

* Deprecations for v17

src/Umbraco.Web.UI.Client/src/external/tiptap/index.ts
@@ -127,7 +127,7 @@ export {
 	Underline,
 } from '@tiptap/extension-underline';
 
-/** @deprecated No longer used internally. This will be removed in Umbraco 18. [LK] */
+/** @deprecated No longer used internally. This will be removed in Umbraco 17. [LK] */
 export { StarterKit } from '@tiptap/starter-kit';
 
 // CUSTOM EXTENSIONS

src/Umbraco.Web.UI.Client/src/mocks/data/data-type/data-type.data.ts
@@ -1070,17 +1070,35 @@ export const data: Array<UmbMockDataTypeModel> = [
 				alias: 'extensions',
 				value: [
 					'Umb.Tiptap.RichTextEssentials',
+					'Umb.Tiptap.Anchor',
+					'Umb.Tiptap.Block',
+					'Umb.Tiptap.Blockquote',
+					'Umb.Tiptap.Bold',
+					'Umb.Tiptap.BulletList',
+					'Umb.Tiptap.CodeBlock',
 					'Umb.Tiptap.Embed',
 					'Umb.Tiptap.Figure',
+					'Umb.Tiptap.Heading',
+					'Umb.Tiptap.HorizontalRule',
+					'Umb.Tiptap.HtmlAttributeClass',
+					'Umb.Tiptap.HtmlAttributeDataset',
+					'Umb.Tiptap.HtmlAttributeId',
+					'Umb.Tiptap.HtmlAttributeStyle',
+					'Umb.Tiptap.HtmlTagDiv',
+					'Umb.Tiptap.HtmlTagSpan',
 					'Umb.Tiptap.Image',
+					'Umb.Tiptap.Italic',
 					'Umb.Tiptap.Link',
 					'Umb.Tiptap.MediaUpload',
+					'Umb.Tiptap.OrderedList',
+					'Umb.Tiptap.Strike',
 					'Umb.Tiptap.Subscript',
 					'Umb.Tiptap.Superscript',
 					'Umb.Tiptap.Table',
 					'Umb.Tiptap.TextAlign',
 					'Umb.Tiptap.TextDirection',
 					'Umb.Tiptap.TextIndent',
+					'Umb.Tiptap.TrailingNode',
 					'Umb.Tiptap.Underline',
 					'Umb.Tiptap.WordCount',
 				],

src/Umbraco.Web.UI.Client/src/mocks/data/document/document.data.ts
@@ -968,7 +968,7 @@ export const data: Array<UmbMockDocumentModel> = [
 <p>Some value for the RTE with an <a href="https://google.com">external link</a> and an <a type="document" href="/{localLink:c05da24d-7740-447b-9cdc-bd8ce2172e38}">internal link</a> foo foo</p>
 
 <p>The following tests the embed plugin:</p>
-<p><div class="umb-embed-holder" data-embed-height="240" data-embed-width="360" data-embed-constrain="false" data-embed-url="https://www.youtube.com/watch?v=QRIWz9SotY4"><iframe width="360" height="240" src="https://www.youtube.com/embed/QRIWz9SotY4?feature=oembed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen="allowfullscreen" title="Deep dive into Rich Text Editor"></iframe></div></p>
+<p><span class="umb-embed-holder" data-embed-height="240" data-embed-width="360" data-embed-constrain="false" data-embed-url="https://www.youtube.com/watch?v=QRIWz9SotY4"><iframe width="360" height="240" src="https://www.youtube.com/embed/QRIWz9SotY4?feature=oembed" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen="allowfullscreen" title="Deep dive into Rich Text Editor"></iframe></span></p>
 
 <p><span id="foo">Some</span> value for the RTE with an <a target="" data-router-slot="disabled" href="https://google.com" type="external">external link</a> and an <a target="" data-router-slot="disabled" href="/{localLink:c05da24d-7740-447b-9cdc-bd8ce2172e38}" type="document">internal link</a>.</p>
 

src/Umbraco.Web.UI.Client/src/packages/content/content/collection/content-collection-workspace.context-token.ts
@@ -10,5 +10,5 @@ export const UMB_CONTENT_COLLECTION_WORKSPACE_CONTEXT = new UmbContextToken<
 	'UmbWorkspaceContext',
 	undefined,
 	(context): context is UmbContentCollectionWorkspaceContext<UmbContentTypeModel> =>
-		(context as UmbContentCollectionWorkspaceContext<UmbContentTypeModel>).collection.hasCollection !== undefined,
+		(context as UmbContentCollectionWorkspaceContext<UmbContentTypeModel>).collection?.hasCollection !== undefined,
 );

src/Umbraco.Web.UI.Client/src/packages/tiptap/extensions/font-family/manifests.ts
@@ -1,14 +1,4 @@
-const UMB_MENU_TIPTAP_FONT_FAMILY_ALIAS = 'Umb.Menu.Tiptap.FontFamily';
-
-const menu: Array<UmbExtensionManifest> = [
-	{
-		type: 'menu',
-		alias: UMB_MENU_TIPTAP_FONT_FAMILY_ALIAS,
-		name: 'Tiptap Font Family Menu',
-	},
-];
-
-const toolbarExtensions: Array<UmbExtensionManifest> = [
+export const manifests: Array<UmbExtensionManifest> = [
 	{
 		type: 'tiptapToolbarExtension',
 		kind: 'menu',
@@ -23,7 +13,6 @@ const toolbarExtensions: Array<UmbExtensionManifest> = [
 			{ label: 'Cursive', appearance: { style: 'font-family: cursive;' }, data: 'cursive' },
 			{ label: 'Fantasy', appearance: { style: 'font-family: fantasy;' }, data: 'fantasy' },
 		],
-		menu: UMB_MENU_TIPTAP_FONT_FAMILY_ALIAS,
 		meta: {
 			alias: 'umbFontFamily',
 			icon: 'icon-ruler-alt',
@@ -31,5 +20,3 @@ const toolbarExtensions: Array<UmbExtensionManifest> = [
 		},
 	},
 ];
-
-export const manifests: Array<UmbExtensionManifest> = [...menu, ...toolbarExtensions];

src/Umbraco.Web.UI.Client/src/packages/tiptap/extensions/font-size/manifests.ts
@@ -1,14 +1,4 @@
-const UMB_MENU_TIPTAP_FONT_SIZE_ALIAS = 'Umb.Menu.Tiptap.FontSize';
-
-const menu: Array<UmbExtensionManifest> = [
-	{
-		type: 'menu',
-		alias: UMB_MENU_TIPTAP_FONT_SIZE_ALIAS,
-		name: 'Tiptap Font Size Menu',
-	},
-];
-
-const toolbarExtensions: Array<UmbExtensionManifest> = [
+export const manifests: Array<UmbExtensionManifest> = [
 	{
 		type: 'tiptapToolbarExtension',
 		kind: 'menu',
@@ -27,7 +17,6 @@ const toolbarExtensions: Array<UmbExtensionManifest> = [
 			{ label: '26pt', data: '26pt' },
 			{ label: '48pt', data: '48pt' },
 		],
-		menu: UMB_MENU_TIPTAP_FONT_SIZE_ALIAS,
 		meta: {
 			alias: 'umbFontSize',
 			icon: 'icon-ruler',
@@ -35,5 +24,3 @@ const toolbarExtensions: Array<UmbExtensionManifest> = [
 		},
 	},
 ];
-
-export const manifests: Array<UmbExtensionManifest> = [...menu, ...toolbarExtensions];

src/Umbraco.Web.UI.Client/src/packages/tiptap/property-editors/statusbar-configuration/property-editor-ui-tiptap-statusbar-configuration.element.ts
@@ -310,6 +310,7 @@ export class UmbPropertyEditorUiTiptapStatusbarConfigurationElement
 					div {
 						display: flex;
 						gap: var(--uui-size-1);
+						align-items: flex-end;
 					}
 				}
 			}
@@ -369,6 +370,7 @@ export class UmbPropertyEditorUiTiptapStatusbarConfigurationElement
 							div {
 								display: flex;
 								gap: var(--uui-size-1);
+								align-items: flex-end;
 							}
 						}
 					}

src/Umbraco.Web.UI.Client/src/packages/tiptap/property-editors/toolbar-configuration/property-editor-ui-tiptap-toolbar-configuration.element.ts
@@ -364,6 +364,7 @@ export class UmbPropertyEditorUiTiptapToolbarConfigurationElement
 					div {
 						display: flex;
 						gap: var(--uui-size-1);
+						align-items: flex-end;
 					}
 				}
 			}

src/Umbraco.Web.UI.Client/src/packages/tiptap/property-editors/toolbar-configuration/tiptap-toolbar-group-configuration.element.ts
@@ -153,6 +153,7 @@ export class UmbTiptapToolbarGroupConfigurationElement<
 					div {
 						display: flex;
 						gap: var(--uui-size-1);
+						align-items: flex-end;
 					}
 
 					uui-symbol-expand {

src/Umbraco.Web.UI.Client/src/packages/tiptap/types.ts
@@ -3,7 +3,7 @@ import type { UmbBlockRteLayoutModel } from '@umbraco-cms/backoffice/block-rte';
 
 export type * from './extensions/types.js';
 
-// TODO: Rename this type:
+/** @deprecated No longer used internally. This will be removed in Umbraco 17. [LK] */
 export interface UmbPropertyEditorUiValueType {
 	markup: string;
 	blocks: UmbBlockValueType<UmbBlockRteLayoutModel>;

src/Umbraco.Web.UI.Client/src/packages/ufm/components/ufm-render/ufm-render.element.ts
@@ -64,7 +64,7 @@ export class UmbUfmRenderElement extends UmbLitElement {
 			}
 
 			code {
-				font-family: var(--uui-font-monospace);
+				font-family: var(--uui-font-monospace, monospace);
 				white-space: pre-wrap;
 				background-color: var(--uui-color-background);
 				border-radius: var(--uui-border-radius);