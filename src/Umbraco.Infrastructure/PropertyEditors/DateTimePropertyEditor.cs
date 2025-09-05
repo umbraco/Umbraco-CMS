db17327d9326c685e895ba48a53b6aa173d51dcf
NillasKA <kramernicklas@gmail.com>
2025-09-05 13:46:12 +0200

    Moving date converter logic to DateTimePropertyEditor.cs

--- src/Umbraco.Infrastructure/PropertyEditors/DateTimePropertyEditor.cs
+++ src/Umbraco.Infrastructure/PropertyEditors/DateTimePropertyEditor.cs
@@ -1,8 +1,15 @@
 // Copyright (c) Umbraco.
 // See LICENSE for more details.
 
+using System.Data.SqlTypes;
+using System.Globalization;
+using Umbraco.Cms.Core.IO;
 using Umbraco.Cms.Core.Models;
+using Umbraco.Cms.Core.Models.Editors;
 using Umbraco.Cms.Core.PropertyEditors.Validators;
+using Umbraco.Cms.Core.Serialization;
+using Umbraco.Cms.Core.Strings;
+using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Core.PropertyEditors;
 
@@ -25,8 +32,45 @@ public class DateTimePropertyEditor : DataEditor
     /// <inheritdoc />
     protected override IDataValueEditor CreateValueEditor()
     {
-        IDataValueEditor editor = base.CreateValueEditor();
-        editor.Validators.Add(new DateTimeValidator());
-        return editor;
+        return DataValueEditorFactory.Create<DateTimePropertyValueEditor>(Attribute!);
+
+        // IDataValueEditor editor = base.CreateValueEditor();
+        // editor.Validators.Add(new DateTimeValidator());
+        // return editor;
+    }
+
+    internal sealed class DateTimePropertyValueEditor : DataValueEditor
+    {
+        public DateTimePropertyValueEditor(
+            IShortStringHelper shortStringHelper,
+            IJsonSerializer jsonSerializer,
+            IIOHelper ioHelper,
+            DataEditorAttribute attribute)
+            : base(shortStringHelper, jsonSerializer, ioHelper, attribute)
+        {
+        }
+
+        /// <inheritdoc/>
+        public override object? FromEditor(ContentPropertyData editorValue, object? currentValue)
+        {
+            if (editorValue.DataTypeConfiguration is Dictionary<string, object> dictionary)
+            {
+                string[] array = ["hh:mm", "hh:mm:ss"];
+
+                // Ensures we don't care about the customers formatting, weather the wish to write
+                // hh:mm or HH:MM etc...
+                if (!dictionary.Values.OfType<string>().Any(value => array.Contains(value, StringComparer.InvariantCultureIgnoreCase)))
+                {
+                    return editorValue.Value;
+                }
+
+                if (DateTime.TryParse(editorValue.Value as string, out DateTime myDate) || editorValue.Value is DateTime)
+                {
+                    editorValue.Value = new DateTime(SqlDateTime.MinValue.Value.Year, myDate.Month, myDate.Day, myDate.Hour, myDate.Minute, myDate.Second).ToString("yyyy-MM-dd HH:mm:ss");
+                }
+            }
+
+            return editorValue.Value;
+        }
     }
 }
