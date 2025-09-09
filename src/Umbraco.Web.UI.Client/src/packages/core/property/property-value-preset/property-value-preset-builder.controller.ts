cd7a6128e653b3f138b3ba8b454b52d2d1b51e2d
Niels Lyngsø <niels.lyngso@gmail.com>
2025-09-09 20:28:55 +0200

    implement callArgs type updates

--- src/Umbraco.Web.UI.Client/src/packages/core/property/property-value-preset/property-value-preset-builder.controller.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property/property-value-preset/property-value-preset-builder.controller.ts
@@ -1,3 +1,4 @@
+import { UMB_BLOCK_ENTRY_CONTEXT } from '@umbraco-cms/backoffice/block';
 import type { UmbPropertyValueData, UmbPropertyValueDataPotentiallyWithEditorAlias } from '../index.js';
 import type {
 	ManifestPropertyValuePreset,
@@ -5,6 +6,7 @@ import type {
 	UmbPropertyTypePresetWithSchemaAliasModel,
 	UmbPropertyValuePreset,
 	UmbPropertyValuePresetApiCallArgs,
+	UmbPropertyValuePresetApiCallArgsEntityBase,
 } from './types.js';
 import { UmbControllerBase } from '@umbraco-cms/backoffice/class-api';
 import { createExtensionApi } from '@umbraco-cms/backoffice/extension-api';
@@ -15,6 +17,8 @@ const EMPTY_CALL_ARGS = Object.freeze({});
 export class UmbPropertyValuePresetBuilderController<
 	ReturnType = UmbPropertyValueData | UmbPropertyValueDataPotentiallyWithEditorAlias,
 > extends UmbControllerBase {
+	#baseCreateArgs?: UmbPropertyValuePresetApiCallArgsEntityBase;
+
 	/**
 	 * Clones the property data.
 	 * @param {UmbPropertyValueDataPotentiallyWithEditorAlias} propertyTypes - Data about the properties to make a preset for.
@@ -22,7 +26,25 @@ export class UmbPropertyValuePresetBuilderController<
 	 */
 	async create<GivenPropertyTypesType extends UmbPropertyTypePresetModel>(
 		propertyTypes: Array<GivenPropertyTypesType>,
+		// TODO: Remove Option argument and Partial<> in v.17.0 [NL]
+		createArgs?: Partial<UmbPropertyValuePresetApiCallArgsEntityBase>,
 	): Promise<Array<ReturnType>> {
+		//
+		// TODO: Clean up warnings in v.17.0 [NL]
+		this.#baseCreateArgs = {
+			entityType: createArgs?.entityType ?? 'document',
+			entityUnique:
+				createArgs?.entityUnique ??
+				'needs to be parsed from UmbPropertyValuePresetBuilderController, this is not present because of a custom legacy implementation',
+			entityTypeUnique: createArgs?.entityTypeUnique,
+		};
+
+		if (!createArgs?.entityUnique) {
+			console.log(
+				`[UmbPropertyValuePresetBuilderController] - entityUnique was not provided. This will be required in v.17.0 and must be provided when calling create().`,
+			);
+		}
+
 		const result = await Promise.all(propertyTypes.map(this.#createPropertyPreset));
 
 		//Merge all the values into a single array:
@@ -87,9 +109,17 @@ export class UmbPropertyValuePresetBuilderController<
 	protected async _generatePropertyValue(
 		apis: Array<UmbPropertyValuePreset>,
 		propertyType: UmbPropertyTypePresetModel | UmbPropertyTypePresetWithSchemaAliasModel,
-		callArgs: UmbPropertyValuePresetApiCallArgs,
+		incomingCallArgs: Partial<UmbPropertyValuePresetApiCallArgs>,
 	): Promise<ReturnType | undefined> {
 		let value: unknown = undefined;
+
+		const callArgs: UmbPropertyValuePresetApiCallArgs = {
+			...this.#baseCreateArgs!,
+			alias: propertyType.alias,
+			propertyEditorUiAlias: propertyType.propertyEditorUiAlias,
+			propertyEditorSchemaAlias: (propertyType as UmbPropertyTypePresetWithSchemaAliasModel).propertyEditorSchemaAlias,
+			...incomingCallArgs,
+		};
 		// Important to use a inline for loop, to secure that each entry is processed(asynchronously) in order
 		for (const api of apis) {
 			if (!api.processValue) {
