b9b4014d9fe3da86e9e210ea6c5348784a87f7de
Niels Lyngsø <niels.lyngso@gmail.com>
2025-09-10 12:19:22 +0200

    fix nullable checks

--- src/Umbraco.Web.UI.Client/src/packages/media/media/components/input-upload-field/input-upload-field.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/components/input-upload-field/input-upload-field.element.ts
@@ -93,7 +93,7 @@ export class UmbInputUploadFieldElement extends UmbFormControlMixin<UmbMediaValu
 	}
 
 	async #getPreviewElementAlias() {
-		if (!this.value.src) return;
+		if (!this.value?.src) return;
 		const manifests = await this.#getManifests();
 		const fallbackAlias = manifests.find((manifest) =>
 			stringOrStringArrayContains(manifest.forMimeTypes, '*/*'),
@@ -165,7 +165,7 @@ export class UmbInputUploadFieldElement extends UmbFormControlMixin<UmbMediaValu
 	}
 
 	override render() {
-		if (!this.temporaryFile && !this.value.src) {
+		if (!this.temporaryFile && !this.value?.src) {
 			return this.#renderDropzone();
 		}
 
