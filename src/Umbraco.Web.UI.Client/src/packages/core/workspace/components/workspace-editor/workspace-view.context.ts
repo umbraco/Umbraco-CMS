cf7027da94b3c1d4046448fe2beb84b859247723
Niels Lyngsø <niels.lyngso@gmail.com>
2025-09-15 22:19:38 +0200

    update type

--- src/Umbraco.Web.UI.Client/src/packages/core/workspace/components/workspace-editor/workspace-view.context.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/workspace/components/workspace-editor/workspace-view.context.ts
@@ -1,5 +1,5 @@
+import type { UmbClassInterface } from '@umbraco-cms/backoffice/class-api';
 import type { ManifestWorkspaceView } from '../../types.js';
-import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import { UmbViewContext } from '@umbraco-cms/backoffice/view';
 export class UmbWorkspaceViewContext extends UmbViewContext {
 	public readonly IS_WORKSPACE_VIEW_CONTEXT = true as const;
@@ -7,7 +7,7 @@ export class UmbWorkspaceViewContext extends UmbViewContext {
 	// Note: manifest can change later, but because we currently only use the alias from it, it's not something we need to handle. [NL]
 	public manifest: ManifestWorkspaceView;
 
-	constructor(host: UmbControllerHost, manifest: ManifestWorkspaceView) {
+	constructor(host: UmbClassInterface, manifest: ManifestWorkspaceView) {
 		super(host, manifest.alias);
 		this.manifest = manifest;
 	}
