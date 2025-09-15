a3ec05e3e3931903cde93ce77d611ff7c04814e6
Marcus Wilhelmson <marcus.wilhelmson@consid.se>
2025-09-15 23:04:13 +0200

    fix api breaking change

---
+++ src/Umbraco.Cms.Api.Common/Extensions/ActionDescriptorApiCommonExtensions.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Mvc.Abstractions;
+using Umbraco.Cms.Api.Common.Attributes;
+using Umbraco.Cms.Api.Common.Configuration;
+
+namespace Umbraco.Extensions;
+
+public static class ActionDescriptorApiCommonExtensions
+{
+
+    private static string? GetMapToApiAttributeValue(this ActionDescriptor actionDescriptor)
+    {
+        IEnumerable<MapToApiAttribute> mapToApiAttributes = actionDescriptor?.EndpointMetadata?.OfType<MapToApiAttribute>() ?? [];
+
+        return mapToApiAttributes.SingleOrDefault()?.ApiName;
+    }
+
+    public static bool HasMapToApiAttribute(this ActionDescriptor actionDescriptor, string apiName)
+    {
+        var value = actionDescriptor.GetMapToApiAttributeValue();
+
+        return value == apiName
+               || (value is null && apiName == DefaultApiConfiguration.ApiName);
+    }
+}
--- src/Umbraco.Cms.Api.Common/Extensions/MethodInfoApiCommonExtensions.cs
+++ src/Umbraco.Cms.Api.Common/Extensions/MethodInfoApiCommonExtensions.cs
@@ -1,12 +1,11 @@
 using System.Reflection;
 using Asp.Versioning;
-using Microsoft.AspNetCore.Mvc.Abstractions;
 using Umbraco.Cms.Api.Common.Attributes;
 using Umbraco.Cms.Api.Common.Configuration;
 
 namespace Umbraco.Extensions;
 
-public static class MapToApiAttributeExtensions
+public static class MethodInfoApiCommonExtensions
 {
 
     public static string? GetMapToApiVersionAttributeValue(this MethodInfo methodInfo)
@@ -18,29 +17,14 @@ public static class MapToApiAttributeExtensions
 
     public static string? GetMapToApiAttributeValue(this MethodInfo methodInfo)
     {
-        MapToApiAttribute[] mapToApis = [.. (methodInfo.DeclaringType?.GetCustomAttributes(typeof(MapToApiAttribute), inherit: true) ?? []).Cast<MapToApiAttribute>()];
+        MapToApiAttribute[] mapToApis = (methodInfo.DeclaringType?.GetCustomAttributes(typeof(MapToApiAttribute), inherit: true) ?? Array.Empty<object>()).Cast<MapToApiAttribute>().ToArray();
 
         return mapToApis.SingleOrDefault()?.ApiName;
-    }
-
-    private static string? GetMapToApiAttributeValue(this ActionDescriptor actionDescriptor)
-    {
-        IEnumerable<MapToApiAttribute> mapToApiAttributes = actionDescriptor?.EndpointMetadata?.OfType<MapToApiAttribute>() ?? [];
-
-        return mapToApiAttributes.SingleOrDefault()?.ApiName;
     }
 
     public static bool HasMapToApiAttribute(this MethodInfo methodInfo, string apiName)
     {
         var value = methodInfo.GetMapToApiAttributeValue();
-
-        return value == apiName
-               || (value is null && apiName == DefaultApiConfiguration.ApiName);
-    }
-
-    public static bool HasMapToApiAttribute(this ActionDescriptor actionDescriptor, string apiName)
-    {
-        var value = actionDescriptor.GetMapToApiAttributeValue();
 
         return value == apiName
                || (value is null && apiName == DefaultApiConfiguration.ApiName);
