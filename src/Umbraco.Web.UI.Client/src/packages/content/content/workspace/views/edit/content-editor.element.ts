umbraco/Umbraco-CMS

commit 7f2d515763ea4810ac4d99ecb2080ff9a8f90d4d

Workspace view navigation context (#19255)
* todos

* navigation context

* replace raw manifests with view context

* Array State has method

* rename to hint and much more

* Notes for later

* correcting one word

* more notes

* update JS Docs

* update tests for getHasOne

* fix context api usage

* update code for v.16

* correct test

* export UMB_WORKSPACE_VIEW_CONTEXT

* minor corrections

* rename to _hintMap

* refactor part 1

* update version number in comment

* clear method for array states

* declare hint import map

* mega refactor

* final corrections for working POC

* clean up path logic

* implement scaffold

* propagation and inheritance from view to workspace

* separate types from classes

* refactor to view context

* rename editor navigation context to editor context

* propagate removals

* clean up notes

* Hints for Content Tabs

* use const path

* handle gone parent

* added comments on something to be looked at

* hints context types

* contentTypeMergedContainers

* lint fixes

* public contentTypeMergedContainers

* refactor property structure helper class

* a few notes for Presets

* set variant ID instead of parsing it to the constructor

* do not inject root to the path

* adjust structure manager logic

* UmbPropertyTypeContainerMergedModel type update

* correct mergedContainersOfParentIdAndType

* fix lint errors

* fix missing import

* Update src/Umbraco.Web.UI.Client/src/packages/core/hint/context/hints.controller.ts

Co-authored-by: Copilot <175728472+Copilot@users.noreply.github.com>

* Update src/Umbraco.Web.UI.Client/src/packages/content/content/workspace/content-validation-to-hints.manager.ts

Co-authored-by: Copilot <175728472+Copilot@users.noreply.github.com>

* Update src/Umbraco.Web.UI.Client/src/packages/content/content/workspace/content-validation-to-hints.manager.ts

Co-authored-by: Copilot <175728472+Copilot@users.noreply.github.com>

---------

src/Umbraco.Web.UI.Client/src/packages/content/content/workspace/views/edit/content-editor.element.ts
@@ -1,5 +1,5 @@
 import type { UmbContentWorkspaceViewEditTabElement } from './content-editor-tab.element.js';
-import { css, html, customElement, state, repeat } from '@umbraco-cms/backoffice/external/lit';
+import { css, html, customElement, state, repeat, nothing } from '@umbraco-cms/backoffice/external/lit';
 import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
 import type {
 	UmbContentTypeModel,
@@ -15,6 +15,8 @@ import { encodeFolderName } from '@umbraco-cms/backoffice/router';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
 import type { UmbWorkspaceViewElement } from '@umbraco-cms/backoffice/workspace';
 import './content-editor-tab.element.js';
+import type { UmbVariantHint } from '@umbraco-cms/backoffice/hint';
+import { UMB_VIEW_CONTEXT, UmbViewContext } from '@umbraco-cms/backoffice/view';
 
 @customElement('umb-content-workspace-view-edit')
 export class UmbContentWorkspaceViewEditElement extends UmbLitElement implements UmbWorkspaceViewElement {
@@ -23,6 +25,7 @@ export class UmbContentWorkspaceViewEditElement extends UmbLitElement implements
 	@state()
 	private _hasRootProperties = false;
   */
+	#viewContext?: UmbViewContext;
 
 	@state()
 	private _hasRootGroups = false;
@@ -39,6 +42,11 @@ export class UmbContentWorkspaceViewEditElement extends UmbLitElement implements
 	@state()
 	private _activePath = '';
 
+	@state()
+	private _hintMap: Map<string, UmbVariantHint> = new Map();
+
+	#tabViewContexts: Array<UmbViewContext> = [];
+
 	#structureManager?: UmbContentTypeStructureManager<UmbContentTypeModel>;
 
 	private _tabsStructureHelper = new UmbContentTypeContainerStructureHelper<UmbContentTypeModel>(this);
@@ -46,13 +54,20 @@ export class UmbContentWorkspaceViewEditElement extends UmbLitElement implements
 	constructor() {
 		super();
 
+		this.consumeContext(UMB_VIEW_CONTEXT, (context) => {
+			this.#viewContext = context;
+			this.#tabViewContexts.forEach((view) => {
+				view.inheritFrom(this.#viewContext);
+			});
+		});
+
 		this._tabsStructureHelper.setIsRoot(true);
 		this._tabsStructureHelper.setContainerChildType('Tab');
 		this.observe(
 			this._tabsStructureHelper.mergedContainers,
 			(tabs) => {
 				this._tabs = tabs;
-				this._createRoutes();
+				this.#createRoutes();
 			},
 			null,
 		);
@@ -73,13 +88,13 @@ export class UmbContentWorkspaceViewEditElement extends UmbLitElement implements
 			await this.#structureManager.hasRootContainers('Group'),
 			(hasRootGroups) => {
 				this._hasRootGroups = hasRootGroups;
-				this._createRoutes();
+				this.#createRoutes();
 			},
 			'_observeGroups',
 		);
 	}
 
-	private _createRoutes() {
+	#createRoutes() {
 		if (!this._tabs || !this.#structureManager) return;
 		const routes: UmbRoute[] = [];
 
@@ -91,18 +106,21 @@ export class UmbContentWorkspaceViewEditElement extends UmbLitElement implements
 					(component as UmbContentWorkspaceViewEditTabElement).containerId = null;
 				},
 			});
+			this.#createViewContext('root');
 		}
 
 		if (this._tabs.length > 0) {
 			this._tabs?.forEach((tab) => {
 				const tabName = tab.name ?? '';
+				const path = `tab/${encodeFolderName(tabName)}`;
 				routes.push({
-					path: `tab/${encodeFolderName(tabName)}`,
+					path,
 					component: () => import('./content-editor-tab.element.js'),
 					setup: (component) => {
 						(component as UmbContentWorkspaceViewEditTabElement).containerId = tab.id;
 					},
 				});
+				this.#createViewContext(path);
 			});
 		}
 
@@ -122,35 +140,45 @@ export class UmbContentWorkspaceViewEditElement extends UmbLitElement implements
 		this._routes = routes;
 	}
 
+	#createViewContext(viewAlias: string) {
+		if (!this.#tabViewContexts.find((context) => context.viewAlias === viewAlias)) {
+			const view = new UmbViewContext(this, viewAlias);
+			this.#tabViewContexts.push(view);
+
+			view.inheritFrom(this.#viewContext);
+
+			this.observe(
+				view.firstHintOfVariant,
+				(hint) => {
+					if (hint) {
+						this._hintMap.set(viewAlias, hint);
+					} else {
+						this._hintMap.delete(viewAlias);
+					}
+					this.requestUpdate('_hintMap');
+				},
+				'umbObserveState_' + viewAlias,
+			);
+		}
+	}
+
 	override render() {
 		if (!this._routes || !this._tabs) return;
 		return html`
 			<umb-body-layout header-fit-height>
 				${this._routerPath && (this._tabs.length > 1 || (this._tabs.length === 1 && this._hasRootGroups))
 					? html` <uui-tab-group slot="header">
-							${this._hasRootGroups && this._tabs.length > 0
-								? html`
-										<uui-tab
-											label=${this.localize.term('general_generic')}
-											.active=${this._routerPath + '/root' === this._activePath ||
-											this._routerPath + '/' === this._activePath}
-											href=${this._routerPath + '/root'}></uui-tab>
-									`
-								: ''}
+							${this._hasRootGroups && this._tabs.length > 0 ? this.#renderTab('root', '#general_generic') : nothing}
 							${repeat(
 								this._tabs,
 								(tab) => tab.name,
 								(tab, index) => {
-									const path = this._routerPath + '/tab/' + encodeFolderName(tab.name || '');
-									return html`<uui-tab
-										label=${this.localize.string(tab.name ?? '#general_unnamed')}
-										.active=${path === this._activePath ||
-										(!this._hasRootGroups && index === 0 && this._routerPath + '/' === this._activePath)}
-										href=${path}></uui-tab>`;
+									const path = 'tab/' + encodeFolderName(tab.name || '');
+									return this.#renderTab(path, tab.name, index);
 								},
 							)}
 						</uui-tab-group>`
-					: ''}
+					: nothing}
 
 				<umb-router-slot
 					inherit-addendum
@@ -164,6 +192,25 @@ export class UmbContentWorkspaceViewEditElement extends UmbLitElement implements
 				</umb-router-slot>
 			</umb-body-layout>
 		`;
+	}
+
+	#renderTab(path: string, name: string, index = 0) {
+		const hint = this._hintMap.get(path);
+		const fullPath = this._routerPath + '/' + path;
+		const active =
+			fullPath === this._activePath ||
+			(!this._hasRootGroups && index === 0 && this._routerPath + '/' === this._activePath);
+		return html`<uui-tab
+			.label=${this.localize.string(name ?? '#general_unnamed')}
+			.active=${active}
+			href=${fullPath}
+			data-mark="content-tab:${path}"
+			>${hint && !active
+				? html`<uui-badge slot="extra" .color=${hint.color ?? 'default'} ?attention=${hint.color === 'invalid'}
+						>${hint.text}</uui-badge
+					>`
+				: nothing}</uui-tab
+		>`;
 	}
 
 	static override styles = [

src/Umbraco.Web.UI.Client/examples/workspace-view-hint/hint-workspace-view.ts
@@ -1,0 +1,84 @@
+import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
+import { css, html, customElement, LitElement } from '@umbraco-cms/backoffice/external/lit';
+import { UmbElementMixin } from '@umbraco-cms/backoffice/element-api';
+import { UMB_WORKSPACE_VIEW_CONTEXT } from '@umbraco-cms/backoffice/workspace';
+import { UmbVariantId } from '@umbraco-cms/backoffice/variant';
+import { UMB_DOCUMENT_WORKSPACE_CONTEXT } from '@umbraco-cms/backoffice/document';
+
+@customElement('example-hint-workspace-view')
+export class ExampleHintWorkspaceView extends UmbElementMixin(LitElement) {
+	//
+
+	async onClick() {
+		/*
+		const context = await this.getContext(UMB_WORKSPACE_VIEW_NAVIGATION_CONTEXT);
+		if (!context) {
+			throw new Error('Could not find the context');
+		}
+		const view = await context.getViewContext('example.workspaceView.hint');
+		*/
+
+		/*
+		const view = await this.getContext(UMB_WORKSPACE_VIEW_CONTEXT);
+		if (!view) {
+			throw new Error('Could not find the view');
+		}
+
+		if (view.hints.has('exampleHintFromToggleAction')) {
+			view.hints.removeOne('exampleHintFromToggleAction');
+		} else {
+			view.hints.addOne({
+				unique: 'exampleHintFromToggleAction',
+				text: 'Hi',
+				color: 'invalid',
+				weight: 100,
+			});
+		}
+			*/
+
+		const workspace = await this.getContext(UMB_DOCUMENT_WORKSPACE_CONTEXT);
+		if (!workspace) {
+			throw new Error('Could not find the workspace');
+		}
+
+		if (workspace.hints.has('exampleHintFromToggleAction')) {
+			workspace.hints.removeOne('exampleHintFromToggleAction');
+		} else {
+			workspace.hints.addOne({
+				unique: 'exampleHintFromToggleAction',
+				path: ['Umb.WorkspaceView.Document.Edit', 'root'],
+				text: 'Hi',
+				color: 'invalid',
+				weight: 100,
+			});
+		}
+	}
+
+	override render() {
+		return html`
+			<uui-box class="uui-text">
+				<h1 class="uui-h2" style="margin-top: var(--uui-size-layout-1);">See the hint on this views tab</h1>
+				<p>This is toggle on/off via this button:</p>
+				<uui-button type="button" @click=${this.onClick} look="primary" color="positive">Toggle hint</uui-button>
+			</uui-box>
+		`;
+	}
+
+	static override styles = [
+		UmbTextStyles,
+		css`
+			:host {
+				display: block;
+				padding: var(--uui-size-layout-1);
+			}
+		`,
+	];
+}
+
+export { ExampleHintWorkspaceView as element };
+
+declare global {
+	interface HTMLElementTagNameMap {
+		'example-hint-workspace-view': ExampleHintWorkspaceView;
+	}
+}

src/Umbraco.Web.UI.Client/examples/workspace-view-hint/index.ts
@@ -1,0 +1,22 @@
+import { UMB_WORKSPACE_CONDITION_ALIAS } from '@umbraco-cms/backoffice/workspace';
+
+export const manifests: Array<UmbExtensionManifest> = [
+	{
+		type: 'workspaceView',
+		name: 'Example Badge Workspace View',
+		alias: 'example.workspaceView.hint',
+		element: () => import('./hint-workspace-view.js'),
+		weight: 900,
+		meta: {
+			label: 'View with badge',
+			pathname: 'badge',
+			icon: 'icon-lab',
+		},
+		conditions: [
+			{
+				alias: UMB_WORKSPACE_CONDITION_ALIAS,
+				match: 'Umb.Workspace.Document',
+			},
+		],
+	},
+];

src/Umbraco.Web.UI.Client/src/libs/observable-api/states/array-state.test.ts
@@ -44,6 +44,13 @@ describe('ArrayState', () => {
 		});
 	});
 
+	it('getHasOne method, return true when key exists', () => {
+		expect(subject.getHasOne('2')).to.be.true;
+	});
+	it('getHasOne method, return false when key does not exists', () => {
+		expect(subject.getHasOne('1337')).to.be.false;
+	});
+
 	it('filter method, removes anything that is not true of the given predicate method', (done) => {
 		const expectedData = [initialData[0], initialData[2]];
 

src/Umbraco.Web.UI.Client/src/libs/observable-api/states/array-state.ts
@@ -48,10 +48,10 @@ export class UmbArrayState<T, U = unknown> extends UmbDeepState<T[]> {
 	 * @param {T} data - The next data for this state to hold.
 	 * @description - Set the data of this state, if sortBy has been defined for this state the data will be sorted before set. If data is different than current this will trigger observations to update.
 	 * @example <caption>Example change the data of a state</caption>
-	 * const myState = new UmbArrayState('Good morning');
-	 * // myState.value is equal 'Good morning'.
-	 * myState.setValue('Goodnight')
-	 * // myState.value is equal 'Goodnight'.
+	 * const myState = new UmbArrayState(['Good morning']);
+	 * // myState.value is equal ['Good morning'].
+	 * myState.setValue(['Goodnight'])
+	 * // myState.value is equal ['Goodnight'].
 	 */
 	override setValue(value: T[]) {
 		if (value && this.#sortMethod) {
@@ -59,6 +59,19 @@ export class UmbArrayState<T, U = unknown> extends UmbDeepState<T[]> {
 		} else {
 			super.setValue(value);
 		}
+	}
+
+	/**
+	 * @function clear
+	 * @description - Set the data of this state to an empty array.
+	 * @example <caption>Example clearing the data of a state</caption>
+	 * const myState = new UmbArrayState(['Good morning']);
+	 * // myState.value is equal ['Good morning'].
+	 * myState.clear()
+	 * // myState.value is equal [].
+	 */
+	clear() {
+		super.setValue([]);
 	}
 
 	/**

src/Umbraco.Web.UI.Client/src/packages/block/block-grid/block-grid-manager/block-grid-manager.context-token.ts
@@ -1,7 +1,7 @@
 import type { UmbBlockGridManagerContext } from './block-grid-manager.context.js';
 import { UmbContextToken } from '@umbraco-cms/backoffice/context-api';
 
-// TODO: Make discriminator method for this:
+// TODO: Make discriminator method for this: (Aim to do this for v.17) [NL]
 export const UMB_BLOCK_GRID_MANAGER_CONTEXT = new UmbContextToken<
 	UmbBlockGridManagerContext,
 	UmbBlockGridManagerContext

src/Umbraco.Web.UI.Client/src/packages/block/block-grid/components/block-grid-entries/block-grid-entries.context-token.ts
@@ -1,5 +1,5 @@
 import type { UmbBlockGridEntriesContext } from './block-grid-entries.context.js';
 import { UmbContextToken } from '@umbraco-cms/backoffice/context-api';
 
-// TODO: Make discriminator method for this:
+// TODO: Make discriminator method for this: (Aim to do this for v.17) [NL]
 export const UMB_BLOCK_GRID_ENTRIES_CONTEXT = new UmbContextToken<UmbBlockGridEntriesContext>('UmbBlockEntriesContext');

src/Umbraco.Web.UI.Client/src/packages/block/block-grid/components/block-grid-entry/block-grid-entry.context-token.ts
@@ -1,5 +1,5 @@
 import type { UmbBlockGridEntryContext } from './block-grid-entry.context.js';
 import { UmbContextToken } from '@umbraco-cms/backoffice/context-api';
 
-// TODO: Make discriminator method for this:
+// TODO: Make discriminator method for this: (Aim to do this for v.17) [NL]
 export const UMB_BLOCK_GRID_ENTRY_CONTEXT = new UmbContextToken<UmbBlockGridEntryContext>('UmbBlockEntryContext');

commit 3299b402268562bcaa2984e6a71c35d7309cb005

Fixes regression with hidden tabs in the Content Editor
Regression occurred in #19255, originally fixed in #19370.

src/Umbraco.Web.UI.Client/src/packages/content/content/workspace/views/edit/content-editor.element.ts
@@ -201,7 +201,7 @@ export class UmbContentWorkspaceViewEditElement extends UmbLitElement implements
 			fullPath === this._activePath ||
 			(!this._hasRootGroups && index === 0 && this._routerPath + '/' === this._activePath);
 		return html`<uui-tab
-			.label=${this.localize.string(name ?? '#general_unnamed')}
+			label=${this.localize.string(name ?? '#general_unnamed')}
 			.active=${active}
 			href=${fullPath}
 			data-mark="content-tab:${path}"