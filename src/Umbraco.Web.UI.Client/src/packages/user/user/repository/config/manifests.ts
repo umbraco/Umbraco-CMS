3d7ac5776c55c7e25e55b249395e279edba95bea
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-09-15 16:14:48 +0200

    V17: Moves current-user related entities from 'user' to 'current-user' package (#20125)
    * fix: moves current user config repository and related dependencies to the 'current-user' package
    
    previously, it was not exported, so is not a breaking change
    
    * chore: moves current-user-allow-mfa condition to the 'current-user' package to avoid circular dependencies (and because it naturally belongs there)
    
    * feat: exports all current-user config-related items
    
    * chore: move to 'current-user'
    
    * chore: make sure to export all constants

--- src/Umbraco.Web.UI.Client/src/packages/user/user/repository/config/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/repository/config/manifests.ts
@@ -1,9 +1,4 @@
-import {
-	UMB_CURRENT_USER_CONFIG_REPOSITORY_ALIAS,
-	UMB_CURRENT_USER_CONFIG_STORE_ALIAS,
-	UMB_USER_CONFIG_REPOSITORY_ALIAS,
-	UMB_USER_CONFIG_STORE_ALIAS,
-} from './constants.js';
+import { UMB_USER_CONFIG_REPOSITORY_ALIAS, UMB_USER_CONFIG_STORE_ALIAS } from './constants.js';
 
 export const manifests: Array<UmbExtensionManifest> = [
 	{
@@ -17,17 +12,5 @@ export const manifests: Array<UmbExtensionManifest> = [
 		alias: UMB_USER_CONFIG_REPOSITORY_ALIAS,
 		name: 'User Config Repository',
 		api: () => import('./user-config.repository.js'),
-	},
-	{
-		type: 'store',
-		alias: UMB_CURRENT_USER_CONFIG_STORE_ALIAS,
-		name: 'Current User Config Store',
-		api: () => import('./current-user-config.store.js'),
-	},
-	{
-		type: 'repository',
-		alias: UMB_CURRENT_USER_CONFIG_REPOSITORY_ALIAS,
-		name: 'Current User Config Repository',
-		api: () => import('./current-user-config.repository.js'),
 	},
 ];
