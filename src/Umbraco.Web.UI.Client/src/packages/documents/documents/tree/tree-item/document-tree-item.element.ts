e1204ac2331846253aed7056c3db96b1e0934496
Mads Rasmussen <madsr@hey.com>
2025-09-17 13:41:43 +0200

    remove unused styles

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/tree/tree-item/document-tree-item.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/tree/tree-item/document-tree-item.element.ts
@@ -1,6 +1,6 @@
 import type { UmbDocumentTreeItemModel } from '../types.js';
 import type { UmbDocumentTreeItemContext } from './document-tree-item.context.js';
-import { css, html, nothing, customElement, classMap, state, property } from '@umbraco-cms/backoffice/external/lit';
+import { css, html, customElement, classMap, state, property } from '@umbraco-cms/backoffice/external/lit';
 import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
 import { UmbTreeItemElementBase } from '@umbraco-cms/backoffice/tree';
 
@@ -37,58 +37,10 @@ export class UmbDocumentTreeItemElement extends UmbTreeItemElementBase<
 	static override styles = [
 		UmbTextStyles,
 		css`
-			#icon-container {
-				position: relative;
-			}
-
-			#icon {
-				vertical-align: middle;
-				font-size: 16px;
-			}
-
 			#label {
 				white-space: nowrap;
 				overflow: hidden;
 				text-overflow: ellipsis;
-			}
-
-			#state-icon {
-				position: absolute;
-				bottom: -5px;
-				right: -5px;
-				font-size: 10px;
-				background: var(--uui-color-surface);
-				width: 14px;
-				height: 14px;
-				border-radius: 100%;
-				line-height: 14px;
-			}
-
-			:hover #state-icon {
-				background: var(--uui-color-surface-emphasis);
-			}
-
-			/** Active */
-			[active] #state-icon {
-				background: var(--uui-color-current);
-			}
-
-			[active]:hover #state-icon {
-				background: var(--uui-color-current-emphasis);
-			}
-
-			/** Selected */
-			[selected] #state-icon {
-				background-color: var(--uui-color-selected);
-			}
-
-			[selected]:hover #state-icon {
-				background-color: var(--uui-color-selected-emphasis);
-			}
-
-			/** Disabled */
-			[disabled] #state-icon {
-				background-color: var(--uui-color-disabled);
 			}
 
 			.draft {
