044cbbc614ac2b90f291ea5ccfbf5b46fb049054
NillasKA <kramernicklas@gmail.com>
2025-09-17 13:48:50 +0200

    Obsoleting old constructors related to PartialView

--- src/Umbraco.Cms.Api.Management/Controllers/PartialView/Tree/ChildrenPartialViewTreeController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/PartialView/Tree/ChildrenPartialViewTreeController.cs
@@ -1,17 +1,33 @@
 ﻿using Asp.Versioning;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.DependencyInjection;
 using Umbraco.Cms.Core.IO;
 using Umbraco.Cms.Api.Common.ViewModels.Pagination;
+using Umbraco.Cms.Api.Management.Services.FileSystem;
 using Umbraco.Cms.Api.Management.ViewModels.Tree;
+using Umbraco.Cms.Core.DependencyInjection;
 
 namespace Umbraco.Cms.Api.Management.Controllers.PartialView.Tree;
 
 [ApiVersion("1.0")]
 public class ChildrenPartialViewTreeController : PartialViewTreeControllerBase
 {
+    private readonly IPartialViewTreeService _partialViewTreeService;
+
+    public ChildrenPartialViewTreeController(IPartialViewTreeService partialViewTreeService)
+        : this(partialViewTreeService, StaticServiceProvider.Instance.GetRequiredService<FileSystems>())
+        => _partialViewTreeService = partialViewTreeService;
+
+    [ActivatorUtilitiesConstructor]
+    [Obsolete("Please use the other constructor. Scheduled to be removed in Umbraco 19")]
+    public ChildrenPartialViewTreeController(IPartialViewTreeService partialViewTreeService, FileSystems fileSystems)
+        : base(partialViewTreeService, fileSystems) =>
+        _partialViewTreeService = partialViewTreeService;
+
+    [Obsolete("Please use the other constructor. Scheduled to be removed in Umbraco 19")]
     public ChildrenPartialViewTreeController(FileSystems fileSystems)
-        : base(fileSystems)
+        : this(StaticServiceProvider.Instance.GetRequiredService<IPartialViewTreeService>(), fileSystems)
     {
     }
 
--- src/Umbraco.Cms.Api.Management/Controllers/PartialView/Tree/SiblingsPartialViewTreeController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/PartialView/Tree/SiblingsPartialViewTreeController.cs
@@ -1,42 +1,40 @@
-﻿using Asp.Versioning;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
-using Umbraco.Cms.Core.IO;
 using Umbraco.Cms.Api.Common.ViewModels.Pagination;
 using Umbraco.Cms.Api.Management.Services.FileSystem;
 using Umbraco.Cms.Api.Management.ViewModels.Tree;
 using Umbraco.Cms.Core.DependencyInjection;
+using Umbraco.Cms.Core.IO;
 
 namespace Umbraco.Cms.Api.Management.Controllers.PartialView.Tree;
 
-[ApiVersion("1.0")]
-public class RootPartialViewTreeController : PartialViewTreeControllerBase
+public class SiblingsPartialViewTreeController : PartialViewTreeControllerBase
 {
     private readonly IPartialViewTreeService _partialViewTreeService;
 
-    public RootPartialViewTreeController(IPartialViewTreeService partialViewTreeService)
+    public SiblingsPartialViewTreeController(IPartialViewTreeService partialViewTreeService)
         : this(partialViewTreeService, StaticServiceProvider.Instance.GetRequiredService<FileSystems>())
         => _partialViewTreeService = partialViewTreeService;
 
     [ActivatorUtilitiesConstructor]
     [Obsolete("Please use the other constructor. Scheduled to be removed in Umbraco 19")]
-    public RootPartialViewTreeController(IPartialViewTreeService partialViewTreeService, FileSystems fileSystems)
+    public SiblingsPartialViewTreeController(IPartialViewTreeService partialViewTreeService, FileSystems fileSystems)
         : base(partialViewTreeService, fileSystems) =>
         _partialViewTreeService = partialViewTreeService;
 
     [Obsolete("Please use the other constructor. Scheduled to be removed in Umbraco 19")]
-    public RootPartialViewTreeController(FileSystems fileSystems)
+    public SiblingsPartialViewTreeController(FileSystems fileSystems)
         : this(StaticServiceProvider.Instance.GetRequiredService<IPartialViewTreeService>(), fileSystems)
     {
     }
 
-    [HttpGet("root")]
-    [MapToApiVersion("1.0")]
-    [ProducesResponseType(typeof(PagedViewModel<FileSystemTreeItemPresentationModel>), StatusCodes.Status200OK)]
-    public async Task<ActionResult<PagedViewModel<FileSystemTreeItemPresentationModel>>> Root(
+    [HttpGet("siblings")]
+    [ProducesResponseType(typeof(SubsetViewModel<FileSystemTreeItemPresentationModel>), StatusCodes.Status200OK)]
+    public async Task<ActionResult<SubsetViewModel<FileSystemTreeItemPresentationModel>>> Siblings(
         CancellationToken cancellationToken,
-        int skip = 0,
-        int take = 100)
-        => await GetRoot(skip, take);
+        string path,
+        int before,
+        int after)
+        => await GetSiblings(path, before, after);
 }
