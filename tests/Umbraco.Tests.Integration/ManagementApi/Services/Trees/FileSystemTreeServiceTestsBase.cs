e452a155a0cd830ef63847aab54e7284f8beadae
NillasKA <kramernicklas@gmail.com>
2025-09-22 10:36:35 +0200

    Rewritten tests for less bloat and less duplicate code

---
+++ tests/Umbraco.Tests.Integration/ManagementApi/Services/Trees/FileSystemTreeServiceTestsBase.cs
@@ -0,0 +1,88 @@
+﻿using System.Text;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using NUnit.Framework;
+using Umbraco.Cms.Core.Configuration.Models;
+using Umbraco.Cms.Core.Hosting;
+using Umbraco.Cms.Core.IO;
+using Umbraco.Cms.Tests.Common.TestHelpers;
+using Umbraco.Cms.Tests.Integration.Testing;
+
+namespace Umbraco.Cms.Tests.Integration.ManagementApi.Services.Trees;
+
+public abstract class FileSystemTreeServiceTestsBase : UmbracoIntegrationTest
+{
+    public FileSystems _fileSystems;
+    public IFileSystem _fileSystem;
+
+    protected abstract string path { get; }
+
+    public IHostingEnvironment HostingEnvironment => GetRequiredService<IHostingEnvironment>();
+
+    protected virtual T GetRequiredService<T>() => Services.GetRequiredService<T>();
+
+    [SetUp]
+    public void SetUpFileSystem()
+    {
+        _fileSystem = new PhysicalFileSystem(IOHelper, HostingEnvironment, LoggerFactory.CreateLogger<PhysicalFileSystem>(), HostingEnvironment.MapPathWebRoot(path), HostingEnvironment.ToAbsolute(path));
+
+        _fileSystems = FileSystemsCreator.CreateTestFileSystems(
+            LoggerFactory,
+            IOHelper,
+            GetRequiredService<IOptions<GlobalSettings>>(),
+            HostingEnvironment,
+            _fileSystem,
+            _fileSystem,
+            _fileSystem,
+            _fileSystem);
+        for (int i = 0; i < 10; i++)
+        {
+            using (var stream = CreateStream("/tests/"))
+            {
+                _fileSystem.AddFile($"file{i}", stream);
+            }
+        }
+    }
+
+    [TearDown]
+    public void TearDownFileSystem()
+    {
+        // Delete all files
+        Purge(_fileSystems.StylesheetsFileSystem, string.Empty);
+        Purge(_fileSystems.ScriptsFileSystem, string.Empty);
+        Purge(_fileSystems.PartialViewsFileSystem, string.Empty);
+        Purge(_fileSystems.MvcViewsFileSystem, string.Empty);
+
+        _fileSystems = null;
+    }
+
+    private void Purge(IFileSystem fs, string path)
+    {
+        var files = fs.GetFiles(path, "*");
+        foreach (var file in files)
+        {
+            fs.DeleteFile(file);
+        }
+
+        var dirs = fs.GetDirectories(path);
+        foreach (var dir in dirs)
+        {
+            Purge(fs, dir);
+            fs.DeleteDirectory(dir);
+        }
+    }
+
+    protected Stream CreateStream(string contents = null)
+    {
+        if (string.IsNullOrEmpty(contents))
+        {
+            contents = "/* test */";
+        }
+
+        var bytes = Encoding.UTF8.GetBytes(contents);
+        var stream = new MemoryStream(bytes);
+
+        return stream;
+    }
+}
--- tests/Umbraco.Tests.Integration/ManagementApi/Services/Trees/PartialViewTreeServiceTests.cs
+++ tests/Umbraco.Tests.Integration/ManagementApi/Services/Trees/PartialViewTreeServiceTests.cs
@@ -14,90 +14,23 @@ using Umbraco.Cms.Tests.Integration.Testing;
 
 namespace Umbraco.Cms.Tests.Integration.ManagementApi.Services.Trees;
 
-public class PartialViewTreeServiceTests : UmbracoIntegrationTest
+public class PartialViewTreeServiceTests : FileSystemTreeServiceTestsBase
 {
-    private FileSystems _fileSystems;
-    private IFileSystem _fileSystem;
-
-    protected virtual T GetRequiredService<T>() => Services.GetRequiredService<T>();
-
-    private IHostingEnvironment HostingEnvironment => GetRequiredService<IHostingEnvironment>();
-
-    [SetUp]
-    public void SetUpFileSystem()
-    {
-        var path = Constants.SystemDirectories.PartialViews;
-        _fileSystem = new PhysicalFileSystem(IOHelper, HostingEnvironment, LoggerFactory.CreateLogger<PhysicalFileSystem>(), HostingEnvironment.MapPathWebRoot(path), HostingEnvironment.ToAbsolute(path));
-
-        _fileSystems = FileSystemsCreator.CreateTestFileSystems(
-            LoggerFactory,
-            IOHelper,
-            GetRequiredService<IOptions<GlobalSettings>>(),
-            HostingEnvironment,
-            _fileSystem,
-            null,
-            null,
-            null);
-        for (int i = 0; i < 10; i++)
-        {
-            using (var stream = CreateStream("/partialViewTreeServiceTests/"))
-            {
-                _fileSystem.AddFile($"file{i}.cshtml", stream);
-            }
-        }
-    }
-
-    [TearDown]
-    public void TearDownFileSystem()
-    {
-        // Delete all files
-        Purge(_fileSystems.PartialViewsFileSystem, string.Empty);
-
-        _fileSystems = null;
-    }
+    protected override string path => Constants.SystemDirectories.PartialViews;
 
     [Test]
     public void Can_Get_Siblings_From_PartialView_Tree_Service()
     {
         var service = new PartialViewTreeService(_fileSystems);
 
-        FileSystemTreeItemPresentationModel[] treeModel = service.GetSiblingsViewModels("file5.cshtml", 1, 1, out long before, out var after);
-        int index = Array.FindIndex(treeModel, item => item.Name == "file5.cshtml");
+        FileSystemTreeItemPresentationModel[] treeModel = service.GetSiblingsViewModels("file5", 1, 1, out long before, out var after);
+        int index = Array.FindIndex(treeModel, item => item.Name == "file5");
 
-        Assert.AreEqual(treeModel[index].Name, "file5.cshtml");
-        Assert.AreEqual(treeModel[index - 1].Name, "file4.cshtml");
-        Assert.AreEqual(treeModel[index + 1].Name, "file6.cshtml");
+        Assert.AreEqual(treeModel[index].Name, "file5");
+        Assert.AreEqual(treeModel[index - 1].Name, "file4");
+        Assert.AreEqual(treeModel[index + 1].Name, "file6");
         Assert.That(treeModel.Length == 3);
         Assert.AreEqual(after, 3);
         Assert.AreEqual(before, 4);
-    }
-
-    private void Purge(IFileSystem fs, string path)
-    {
-        var files = fs.GetFiles(path, "*.cshtml");
-        foreach (var file in files)
-        {
-            fs.DeleteFile(file);
-        }
-
-        var dirs = fs.GetDirectories(path);
-        foreach (var dir in dirs)
-        {
-            Purge(fs, dir);
-            fs.DeleteDirectory(dir);
-        }
-    }
-
-    protected Stream CreateStream(string contents = null)
-    {
-        if (string.IsNullOrEmpty(contents))
-        {
-            contents = "/* test */";
-        }
-
-        var bytes = Encoding.UTF8.GetBytes(contents);
-        var stream = new MemoryStream(bytes);
-
-        return stream;
     }
 }
