92c116abbe9925865d5b2491569c244b01764e59
Laura Neto <12862535+lauraneto@users.noreply.github.com>
2025-09-22 13:26:18 +0200

    Place the date time property index value factories in their own files

---
+++ src/Umbraco.Core/PropertyEditors/IDateTimeUnspecifiedPropertyIndexValueFactory.cs
@@ -0,0 +1,6 @@
+namespace Umbraco.Cms.Core.PropertyEditors;
+
+/// <summary>
+/// Factory for creating index values for Date Time Unspecified properties.
+/// </summary>
+public interface IDateTimeUnspecifiedPropertyIndexValueFactory : IPropertyIndexValueFactory;
