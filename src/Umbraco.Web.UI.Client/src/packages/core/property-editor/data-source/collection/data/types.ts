194f4023dbaa020e9a9c42bea57c54badcc6537b
Mads Rasmussen <madsr@hey.com>
2025-09-22 09:34:24 +0200

    extend base type

--- src/Umbraco.Web.UI.Client/src/packages/core/property-editor/data-source/collection/data/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property-editor/data-source/collection/data/types.ts
@@ -1,14 +1,17 @@
-import type { UmbCollectionDataSource, UmbCollectionFilterModel } from '@umbraco-cms/backoffice/collection';
+import type {
+	UmbCollectionDataSource,
+	UmbCollectionFilterModel,
+	UmbCollectionItemModel,
+} from '@umbraco-cms/backoffice/collection';
 
 export type UmbLanguageCollectionDataSource = UmbCollectionDataSource<
 	UmbPropertyEditorDataSourceCollectionItemModel,
 	UmbPropertyEditorDataSourceCollectionFilterModel
 >;
 
-export interface UmbPropertyEditorDataSourceCollectionItemModel {
-	entityType: string;
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
+export interface UmbPropertyEditorDataSourceCollectionItemModel extends UmbCollectionItemModel {
 	unique: string;
-	icon: string;
 }
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
