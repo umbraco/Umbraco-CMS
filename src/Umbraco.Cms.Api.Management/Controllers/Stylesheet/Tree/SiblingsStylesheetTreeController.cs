8213da1b7781c1936b3e25e7dd709846839c1878
Nicklas Kramer <kramernicklas@gmail.com>
2025-09-23 11:17:25 +0200

    Trees: Expanding sibling endpoints to include all entities with trees (#20150)
    * Adding member types sibling endpoints
    
    * Introducing sibling endpoint for Partial Views and logic.
    
    * Introducing sibling endpoint for stylesheets
    
    * Introducing sibling endpoint for scripts
    
    * Introducing FileSystemTreeServiceBase.cs
    
    * Introducing interfaces for implementation specific services
    
    * Introducing services for specific trees
    
    * Modifying controller bases to fit new interface and logic.
    
    * Obsoleting old constructors related to PartialView
    
    * Obsoleting ctors related to Stylesheets
    
    * Obsoleting ctors related to scripts
    
    * Adding tests for scriptsTreeService
    
    * Adding tests for siblings
    
    * Removing unused dependencies
    
    * Removing signs and replacing it with flags
    
    * Fixing breaking changes by obsoletion
    
    * Fixing more breaking changes
    
    * Registering missing service
    
    * Fixing breaking changes again
    
    * Changing name of method GetSiblingsViewModels
    
    * Rewritten tests for less bloat and less duplicate code
    
    * Expanding tests to include other methods from service
    
    * Test refactoring: avoided populating file systems that weren't under test, updated encapsulation, renaming, further re-use.
    
    * Management API: Expanding the existing sibling endpoints to support trashed entities (#20154)
    
    * Refactoring existing logic to include trashed items
    
    * Including tests for trashed entities
    
    * Groundwork for trashed siblings
    
    * Documents trashed siblings endpoint
    
    * Controller for Media trashed items
    
    * Expanding tests to include a test for trashed siblings
    
    * Code review corrections
    
    * Resolving code review
    
    ---------
    
    Co-authored-by: Andy Butland <abutland73@gmail.com>

--- src/Umbraco.Cms.Api.Management/Controllers/Stylesheet/Tree/SiblingsStylesheetTreeController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/Stylesheet/Tree/SiblingsStylesheetTreeController.cs
@@ -1,7 +1,7 @@
-﻿using Asp.Versioning;
-using Microsoft.AspNetCore.Http;
+﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.DependencyInjection;
+using Umbraco.Cms.Api.Common.ViewModels.Pagination;
 using Umbraco.Cms.Api.Management.Services.FileSystem;
 using Umbraco.Cms.Api.Management.ViewModels.Tree;
 using Umbraco.Cms.Core.DependencyInjection;
@@ -9,33 +9,33 @@ using Umbraco.Cms.Core.IO;
 
 namespace Umbraco.Cms.Api.Management.Controllers.Stylesheet.Tree;
 
-[ApiVersion("1.0")]
-public class AncestorsStylesheetTreeController : StylesheetTreeControllerBase
+public class SiblingsStylesheetTreeController : StylesheetTreeControllerBase
 {
     private readonly IStyleSheetTreeService _styleSheetTreeService;
 
     // TODO Remove the static service provider, and replace with base when the other constructors are obsoleted.
-    public AncestorsStylesheetTreeController(IStyleSheetTreeService styleSheetTreeService)
+    public SiblingsStylesheetTreeController(IStyleSheetTreeService styleSheetTreeService)
         : this(styleSheetTreeService, StaticServiceProvider.Instance.GetRequiredService<FileSystems>())
         => _styleSheetTreeService = styleSheetTreeService;
 
     [ActivatorUtilitiesConstructor]
     [Obsolete("Please use the other constructor. Scheduled to be removed in Umbraco 19")]
-    public AncestorsStylesheetTreeController(IStyleSheetTreeService styleSheetTreeService, FileSystems fileSystems)
+    public SiblingsStylesheetTreeController(IStyleSheetTreeService styleSheetTreeService, FileSystems fileSystems)
         : base(styleSheetTreeService, fileSystems) =>
         _styleSheetTreeService = styleSheetTreeService;
 
     [Obsolete("Please use the other constructor. Scheduled to be removed in Umbraco 19")]
-    public AncestorsStylesheetTreeController(FileSystems fileSystems)
+    public SiblingsStylesheetTreeController(FileSystems fileSystems)
         : this(StaticServiceProvider.Instance.GetRequiredService<IStyleSheetTreeService>(), fileSystems)
     {
     }
 
-    [HttpGet("ancestors")]
-    [MapToApiVersion("1.0")]
-    [ProducesResponseType(typeof(IEnumerable<FileSystemTreeItemPresentationModel>), StatusCodes.Status200OK)]
-    public async Task<ActionResult<IEnumerable<FileSystemTreeItemPresentationModel>>> Ancestors(
+    [HttpGet("siblings")]
+    [ProducesResponseType(typeof(SubsetViewModel<FileSystemTreeItemPresentationModel>), StatusCodes.Status200OK)]
+    public async Task<ActionResult<SubsetViewModel<FileSystemTreeItemPresentationModel>>> Siblings(
         CancellationToken cancellationToken,
-        string descendantPath)
-        => await GetAncestors(descendantPath);
+        string path,
+        int before,
+        int after)
+        => await GetSiblings(path, before, after);
 }
