4aff99b443a352e44604e646ea6aafe28122e828
Mads Rasmussen <madsr@hey.com>
2025-09-24 17:05:28 +0200

    export types

--- src/Umbraco.Web.UI.Client/src/packages/data-type/property-editor-data-source/extension/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/data-type/property-editor-data-source/extension/types.ts
@@ -1,5 +1,8 @@
 import type { UmbApi } from '@umbraco-cms/backoffice/extension-api';
 
+export type * from './picker-property-editor-collection-data-source.extension.js';
+export type * from './picker-property-editor-tree-data-source.extension.js';
+
 export interface UmbPickerPropertyEditorTreeDataSource extends UmbApi {
 	execute(): Promise<void>;
 }
