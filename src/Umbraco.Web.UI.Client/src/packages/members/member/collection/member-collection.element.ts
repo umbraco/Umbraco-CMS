7d27dcb5d8ee6bc346ef1fe03d72fdc3001d963f
leekelleher <leekelleher@gmail.com>
2025-09-25 15:13:30 +0100

    Members: Removal of client-side deprecations for v17

--- src/Umbraco.Web.UI.Client/src/packages/members/member/collection/member-collection.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/members/member/collection/member-collection.element.ts
@@ -10,7 +10,6 @@ export class UmbMemberCollectionElement extends UmbCollectionDefaultElement {
 	}
 }
 
-/** @deprecated Should be exported as `element` only; to be removed in Umbraco 17. */
 export default UmbMemberCollectionElement;
 
 export { UmbMemberCollectionElement as element };
