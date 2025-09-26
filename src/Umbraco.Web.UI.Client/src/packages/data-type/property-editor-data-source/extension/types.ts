8d482cd3aebf3b5f9019bdc7c704bd104855fcf0
Mads Rasmussen <madsr@hey.com>
2025-09-26 18:51:34 +0200

    update interfaces

--- src/Umbraco.Web.UI.Client/src/packages/data-type/property-editor-data-source/extension/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/data-type/property-editor-data-source/extension/types.ts
@@ -1,12 +1,24 @@
+import type { UmbCollectionRepository } from '@umbraco-cms/backoffice/collection';
 import type { UmbApi } from '@umbraco-cms/backoffice/extension-api';
+import type { UmbItemRepository } from '@umbraco-cms/backoffice/repository';
+import type { UmbSearchRepository } from '@umbraco-cms/backoffice/search';
+import type { UmbTreeRepository } from '@umbraco-cms/backoffice/tree';
 
 export type * from './picker-property-editor-collection-data-source.extension.js';
 export type * from './picker-property-editor-tree-data-source.extension.js';
 
 export interface UmbPickerPropertyEditorTreeDataSource extends UmbApi {
-	execute(): Promise<void>;
+	tree: UmbTreeRepository;
+	// TODO: Change 'any' to a more specific type
+	item: UmbItemRepository<any>;
+	// TODO: Change 'any' to a more specific type
+	search?: UmbSearchRepository<any>;
 }
 
 export interface UmbPickerPropertyEditorCollectionDataSource extends UmbApi {
-	execute(): Promise<void>;
+	collection: UmbCollectionRepository;
+	// TODO: Change 'any' to a more specific type
+	item: UmbItemRepository<any>;
+	// TODO: Change 'any' to a more specific type
+	search?: UmbSearchRepository<any>;
 }
