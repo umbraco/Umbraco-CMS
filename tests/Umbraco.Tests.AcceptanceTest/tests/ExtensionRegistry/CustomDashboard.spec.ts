0e2720da88882f22e193ebe1e5a0094038686b0a
Lan Nguyen Thuy <lnt@umbraco.dk>
2025-09-29 16:24:39 +0700

    update test dashboard using helper

--- tests/Umbraco.Tests.AcceptanceTest/tests/ExtensionRegistry/CustomDashboard.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/ExtensionRegistry/CustomDashboard.spec.ts
@@ -1,30 +1,22 @@
 import {ConstantHelper, test} from '@umbraco/playwright-testhelpers';
-import { expect } from '@playwright/test';
 
 //Dashboard
 const dashboardName = 'Welcome Dashboard';
 
 test('custom dashboard is visible in content section', async ({page, umbracoUi}) => {
-    // Arrange 
+    // Act
     await umbracoUi.goToBackOffice();
     await umbracoUi.content.goToSection(ConstantHelper.sections.content);
 
-    // Act
-    const dashboardTab = page.locator(`uui-tab[label="${dashboardName}"]`);
-
     // Assert
-    await expect(dashboardTab).toBeVisible();
+    await umbracoUi.content.isDashboardTabVisible(dashboardName, true);
 });
 
-
 test('custom dashboard is not visible in media section', async ({page, umbracoUi}) => {
-    // Arrange 
+    // Act
     await umbracoUi.goToBackOffice();
     await umbracoUi.content.goToSection(ConstantHelper.sections.media);
 
-    // Act
-    const dashboardTab = page.locator(`uui-tab[label="${dashboardName}"]`);
-
     // Assert
-    await expect(dashboardTab).not.toBeVisible();
+    await umbracoUi.content.isDashboardTabVisible(dashboardName, false);
 });
