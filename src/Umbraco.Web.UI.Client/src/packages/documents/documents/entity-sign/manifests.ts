cc9cb63371d404e836d574cc7512e532a76d255d
Niels Lyngsø <niels.lyngso@gmail.com>
2025-09-29 22:05:37 +0200

    fix import extension mess

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/entity-sign/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/entity-sign/manifests.ts
@@ -1,4 +1,4 @@
-import { manifests as collectionManifests } from './has-collection/manifests.ts.js';
-import { manifests as draftManifests } from './is-draft/manifests.ts.js';
+import { manifests as collectionManifests } from './has-collection/manifests.js';
+import { manifests as draftManifests } from './is-draft/manifests.js';
 
 export const manifests: Array<UmbExtensionManifest> = [...collectionManifests, ...draftManifests];
