235d80796abd3dbf0d4ffa8e4b115e81679136bb
Nhu Dinh <hnd@umbraco.dk>
2025-09-29 13:08:44 +0700

    Format code

--- tests/Umbraco.Tests.AcceptanceTest/tests/ExtensionRegistry/ReadOnlyGuardRules.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/ExtensionRegistry/ReadOnlyGuardRules.spec.ts
@@ -8,10 +8,10 @@ const documentTypeName = 'TestDocumentTypeForContent';
 // DataType
 const dataTypeName = 'Textstring';
 
-test.afterEach(async ({umbracoApi}) => {
-	await umbracoApi.document.ensureNameNotExists(contentName);
-	await umbracoApi.documentType.ensureNameNotExists(documentTypeName);
-	await umbracoApi.language.ensureIsoCodeNotExists('da');
+test.afterEach(async ({ umbracoApi }) => {
+  await umbracoApi.document.ensureNameNotExists(contentName);
+  await umbracoApi.documentType.ensureNameNotExists(documentTypeName);
+  await umbracoApi.language.ensureIsoCodeNotExists("da");
 });
 
 test('can lock an invariant content', {tag: '@release'}, async ({umbracoApi, umbracoUi}) => {
@@ -24,33 +24,33 @@ test('can lock an invariant content', {tag: '@release'}, async ({umbracoApi, umb
 
   // Act
   await umbracoUi.content.goToContentWithName(contentName);
-	await umbracoUi.content.isDocumentReadOnly(false);
+  await umbracoUi.content.isDocumentReadOnly(false);
   await umbracoUi.content.clickWorkspaceActionMenuButton();
   await umbracoUi.content.clickLockActionMenuOption();
 
   // Assert
-	await umbracoUi.content.isContentNameReadOnly();
-	await umbracoUi.content.isDocumentReadOnly(true);
-	await umbracoUi.content.isPropertyEditorUiWithNameReadOnly('text-box');
+  await umbracoUi.content.isContentNameReadOnly();
+  await umbracoUi.content.isDocumentReadOnly(true);
+  await umbracoUi.content.isPropertyEditorUiWithNameReadOnly("text-box");
 });
 
 test('can lock a variant content', async ({umbracoApi, umbracoUi}) => {
-	// Arrange
-	await umbracoApi.language.createDanishLanguage();
-	const dataTypeData = await umbracoApi.dataType.getByName(dataTypeName);
-	const documentTypeId = await umbracoApi.documentType.createVariantDocumentTypeWithInvariantPropertyEditor(documentTypeName, dataTypeName, dataTypeData.id);
-	await umbracoApi.document.createDocumentWithEnglishCultureAndTextContent(contentName, documentTypeId, contentText, dataTypeName);
-	await umbracoUi.goToBackOffice();
-	await umbracoUi.content.goToSection(ConstantHelper.sections.content);
+  // Arrange
+  await umbracoApi.language.createDanishLanguage();
+  const dataTypeData = await umbracoApi.dataType.getByName(dataTypeName);
+  const documentTypeId = await umbracoApi.documentType.createVariantDocumentTypeWithInvariantPropertyEditor(documentTypeName, dataTypeName, dataTypeData.id);
+  await umbracoApi.document.createDocumentWithEnglishCultureAndTextContent(contentName, documentTypeId, contentText, dataTypeName);
+  await umbracoUi.goToBackOffice();
+  await umbracoUi.content.goToSection(ConstantHelper.sections.content);
 
   // Act
   await umbracoUi.content.goToContentWithName(contentName);
-	await umbracoUi.content.isDocumentReadOnly(false);
+  await umbracoUi.content.isDocumentReadOnly(false);
   await umbracoUi.content.clickWorkspaceActionMenuButton();
   await umbracoUi.content.clickLockActionMenuOption();
 
   // Assert
-	await umbracoUi.content.isContentNameReadOnly();
-	await umbracoUi.content.isDocumentReadOnly(true);
-	await umbracoUi.content.isPropertyEditorUiWithNameReadOnly('text-box');
+  await umbracoUi.content.isContentNameReadOnly();
+  await umbracoUi.content.isDocumentReadOnly(true);
+  await umbracoUi.content.isPropertyEditorUiWithNameReadOnly("text-box");
 });
