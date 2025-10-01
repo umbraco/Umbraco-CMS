81009115e988d6ea05c4b3a6d2af8d9edb31497d
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-10-01 09:09:42 +0200

    feat: adds new repository for document by id segment options

---
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/repository/segment/document-segment.repository.ts
@@ -0,0 +1,42 @@
+import type {
+	UmbDocumentSegmentFilterModel,
+	UmbDocumentSegmentModel,
+	UmbDocumentSegmentResponseModel,
+} from './types.js';
+import { UmbRepositoryBase, type UmbRepositoryResponse } from '@umbraco-cms/backoffice/repository';
+import { DocumentService } from '@umbraco-cms/backoffice/external/backend-api';
+import { tryExecute } from '@umbraco-cms/backoffice/resources';
+
+export class UmbDocumentSegmentRepository extends UmbRepositoryBase {
+	/**
+	 * Get available segment options for a document by its ID.
+	 * @param unique The unique identifier of the document.
+	 * @param filter The filter options to apply.
+	 * @returns A promise that resolves with the available segment options.
+	 */
+	async getDocumentByIdSegmentOptions(
+		unique: string,
+		filter: UmbDocumentSegmentFilterModel,
+	): Promise<UmbRepositoryResponse<UmbDocumentSegmentResponseModel>> {
+		const { data, error } = await tryExecute(
+			this,
+			DocumentService.getDocumentByIdAvailableSegmentOptions({ path: { id: unique }, query: filter }),
+		);
+
+		if (data) {
+			const items = data.items.map((item) => {
+				const model: UmbDocumentSegmentModel = {
+					alias: item.alias,
+					name: item.alias,
+					cultures: item.cultures,
+				};
+
+				return model;
+			});
+
+			return { data: { items, total: data.total } };
+		}
+
+		return { error };
+	}
+}
---
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/repository/segment/index.ts
@@ -0,0 +1,1 @@
+export * from './document-segment.repository.js';
