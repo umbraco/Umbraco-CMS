b92aa0954ff4e905c31a99a1300569fd2622ba36
leekelleher <leekelleher@gmail.com>
2025-10-02 09:55:12 +0100

    Re-generate TypeScript client
    from Management API

--- src/Umbraco.Web.UI.Client/src/packages/core/backend-api/types.gen.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/backend-api/types.gen.ts
@@ -840,7 +840,9 @@ export type DocumentTypeTreeItemResponseModel = {
 
 export type DocumentUrlInfoModel = {
     culture: string | null;
-    url: string;
+    url: string | null;
+    message: string | null;
+    provider: string;
 };
 
 export type DocumentUrlInfoResponseModel = {
@@ -1366,7 +1368,7 @@ export type MediaTypeTreeItemResponseModel = {
 
 export type MediaUrlInfoModel = {
     culture: string | null;
-    url: string;
+    url: string | null;
 };
 
 export type MediaUrlInfoResponseModel = {
@@ -6187,6 +6189,49 @@ export type PutDocumentByIdNotificationsResponses = {
      */
     200: unknown;
 };
+
+export type GetDocumentByIdPreviewUrlData = {
+    body?: never;
+    path: {
+        id: string;
+    };
+    query?: {
+        providerAlias?: string;
+        culture?: string;
+        segment?: string;
+    };
+    url: '/umbraco/management/api/v1/document/{id}/preview-url';
+};
+
+export type GetDocumentByIdPreviewUrlErrors = {
+    /**
+     * Bad Request
+     */
+    400: ProblemDetails;
+    /**
+     * The resource is protected and requires an authentication token
+     */
+    401: unknown;
+    /**
+     * The authenticated user does not have access to this resource
+     */
+    403: unknown;
+    /**
+     * Not Found
+     */
+    404: ProblemDetails;
+};
+
+export type GetDocumentByIdPreviewUrlError = GetDocumentByIdPreviewUrlErrors[keyof GetDocumentByIdPreviewUrlErrors];
+
+export type GetDocumentByIdPreviewUrlResponses = {
+    /**
+     * OK
+     */
+    200: DocumentUrlInfoModel;
+};
+
+export type GetDocumentByIdPreviewUrlResponse = GetDocumentByIdPreviewUrlResponses[keyof GetDocumentByIdPreviewUrlResponses];
 
 export type DeleteDocumentByIdPublicAccessData = {
     body?: never;
