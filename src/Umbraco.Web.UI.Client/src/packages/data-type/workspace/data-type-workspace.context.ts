63637b861338d459bc3bc93056e6f0dbf821fbc7
Laura Neto <12862535+lauraneto@users.noreply.github.com>
2025-10-02 15:56:52 +0200

    Fix editor alias not updating when selecting a different property editor UI

--- src/Umbraco.Web.UI.Client/src/packages/data-type/workspace/data-type-workspace.context.ts
+++ src/Umbraco.Web.UI.Client/src/packages/data-type/workspace/data-type-workspace.context.ts
@@ -196,7 +196,7 @@ export class UmbDataTypeWorkspaceContext
 				}));
 				this.#propertyEditorUISettingsDefaultData = manifest?.meta.settings?.defaultData || [];
 				const manifestPropertyEditorSchemaAlias = manifest?.meta.propertyEditorSchemaAlias;
-				if (manifestPropertyEditorSchemaAlias && !this.getPropertyEditorSchemaAlias()) {
+				if (manifestPropertyEditorSchemaAlias) {
 					// Fallback to the default property editor schema for this property editor UI.
 					this.setPropertyEditorSchemaAlias(manifestPropertyEditorSchemaAlias);
 				}
