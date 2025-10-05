9bed313c7c1858e4b4db261c9a54d1cbd1b8b997
Mads Rasmussen <madsr@hey.com>
2025-10-05 15:20:29 +0200

    Update types.ts

--- src/Umbraco.Web.UI.Client/src/packages/core/collection/item/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/collection/item/types.ts
@@ -1,7 +1,4 @@
 import type { UmbEntityModel } from '@umbraco-cms/backoffice/entity';
 
 // eslint-disable-next-line @typescript-eslint/no-empty-object-type
-export interface UmbCollectionItemModel extends UmbEntityModel {
-	name: string;
-	icon: string;
-}
+export interface UmbCollectionItemModel extends UmbEntityModel {}
