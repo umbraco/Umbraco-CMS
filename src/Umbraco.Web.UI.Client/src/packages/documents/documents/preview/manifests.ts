b4ec2448eb778aa41e968ce87a804151559db244
leekelleher <leekelleher@gmail.com>
2025-10-06 16:00:56 +0100

    Relocated "Save and Preview" workspace action
    reworked using the "default" `previewOption` kind.

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/preview/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/preview/manifests.ts
@@ -1,5 +1,8 @@
 import { manifests as previewOptionManifests } from './preview-option/manifests.js';
+import { manifests as workspaceActionManifests } from './workspace-action/manifests.js';
 import type { UmbExtensionManifestKind } from '@umbraco-cms/backoffice/extension-registry';
+
 export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> = [
 	...previewOptionManifests,
+	...workspaceActionManifests,
 ];
