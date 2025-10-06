c67478874e11dbc55959e053daa08dc1179e51b2
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-10-06 09:09:17 +0200

    Segmentation: The Backoffice should use the enhanced endpoint to fetch segment options for documents based on GUID (#20340)
    * feat: adds new repository for document by id segment options
    
    * chore: mocks up the new endpoint
    
    * feat: all 'null' segments should appear on all languages
    
    * feat: uses new endpoint in content detail workspace base
    
    * feat: maps up the name of the segment
    
    * chore: mock segment data
    
    * feat: adds filter on available segments
    
    * feat: do not alter behavior depending on "undefined" and "null"
    
    * chore: updates mock handler
    
    * feat: ensures that the segments are loaded based on an override method (because they only work for documents) and that they use a generic type (to avoid circular imports)
    
    * feat: refines the segment filter
    
    * chore: updates deprecated model
    
    * feat: treats all culture-less segments as applying to everything
    
    * docs: updates console warn for developers

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/workspace/document-workspace.context.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/workspace/document-workspace.context.ts
@@ -1,7 +1,7 @@
 import { UmbDocumentTypeDetailRepository } from '../../document-types/repository/detail/document-type-detail.repository.js';
 import { UmbDocumentPropertyDatasetContext } from '../property-dataset-context/document-property-dataset.context.js';
 import type { UmbDocumentDetailRepository } from '../repository/index.js';
-import { UMB_DOCUMENT_DETAIL_REPOSITORY_ALIAS } from '../repository/index.js';
+import { UMB_DOCUMENT_DETAIL_REPOSITORY_ALIAS, UmbDocumentSegmentRepository } from '../repository/index.js';
 import type { UmbDocumentDetailModel, UmbDocumentVariantModel } from '../types.js';
 import {
 	UMB_CREATE_DOCUMENT_WORKSPACE_PATH_PATTERN,
@@ -63,6 +63,7 @@ export class UmbDocumentWorkspaceContext
 	readonly templateId = this._data.createObservablePartOfCurrent((data) => data?.template?.unique || null);
 
 	#isTrashedContext = new UmbIsTrashedEntityContext(this);
+	#documentSegmentRepository = new UmbDocumentSegmentRepository(this);
 
 	constructor(host: UmbControllerHost) {
 		super(host, {
@@ -205,6 +206,24 @@ export class UmbDocumentWorkspaceContext
 		}
 
 		return response;
+	}
+
+	protected override async loadSegments(): Promise<void> {
+		this.observe(
+			this.unique,
+			async (unique) => {
+				if (!unique) {
+					this._segments.setValue([]);
+					return;
+				}
+				const { data } = await this.#documentSegmentRepository.getDocumentByIdSegmentOptions(unique, {
+					skip: 0,
+					take: 9999,
+				});
+				this._segments.setValue(data?.items ?? []);
+			},
+			'_loadSegmentsUnique',
+		);
 	}
 
 	async create(parent: UmbEntityModel, documentTypeUnique: string, blueprintUnique?: string) {
