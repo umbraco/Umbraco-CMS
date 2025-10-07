8a7ac51d0e36d4b78977ee5432f7afd478073cd3
Andy Butland <abutland73@gmail.com>
2025-10-07 08:32:03 +0200

    Moved update of path to media file in recycle bin with deleted suffix to the server.

--- src/Umbraco.Cms.Api.Management/Mapping/Media/MediaMapDefinition.cs
+++ src/Umbraco.Cms.Api.Management/Mapping/Media/MediaMapDefinition.cs
@@ -1,13 +1,16 @@
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Options;
 using Umbraco.Cms.Api.Management.Mapping.Content;
 using Umbraco.Cms.Api.Management.ViewModels.Media;
 using Umbraco.Cms.Api.Management.ViewModels.Media.Collection;
 using Umbraco.Cms.Api.Management.ViewModels.MediaType;
+using Umbraco.Cms.Core.Configuration.Models;
 using Umbraco.Cms.Core.DependencyInjection;
 using Umbraco.Cms.Core.Mapping;
 using Umbraco.Cms.Core.Models;
 using Umbraco.Cms.Core.Models.Mapping;
 using Umbraco.Cms.Core.PropertyEditors;
+using Umbraco.Cms.Core.PropertyEditors.ValueConverters;
 using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Api.Management.Mapping.Media;
@@ -15,13 +18,32 @@ namespace Umbraco.Cms.Api.Management.Mapping.Media;
 public class MediaMapDefinition : ContentMapDefinition<IMedia, MediaValueResponseModel, MediaVariantResponseModel>, IMapDefinition
 {
     private readonly CommonMapper _commonMapper;
+    private ImagingSettings _imagingSettings;
 
     public MediaMapDefinition(
         PropertyEditorCollection propertyEditorCollection,
         CommonMapper commonMapper,
-        IDataValueEditorFactory dataValueEditorFactory)
+        IDataValueEditorFactory dataValueEditorFactory,
+        IOptionsMonitor<ImagingSettings> imagingSettings)
         : base(propertyEditorCollection, dataValueEditorFactory)
-        => _commonMapper = commonMapper;
+    {
+        _commonMapper = commonMapper;
+        _imagingSettings = imagingSettings.CurrentValue;
+        imagingSettings.OnChange(x => _imagingSettings = x);
+    }
+
+    [Obsolete("Please use the non-obsolete constructor. Scheduled for removal in Umbraco 18.")]
+    public MediaMapDefinition(
+        PropertyEditorCollection propertyEditorCollection,
+        CommonMapper commonMapper,
+        IDataValueEditorFactory dataValueEditorFactory)
+        : this(
+              propertyEditorCollection,
+              commonMapper,
+              dataValueEditorFactory,
+              StaticServiceProvider.Instance.GetRequiredService<IOptionsMonitor<ImagingSettings>>())
+    {
+    }
 
     [Obsolete("Please use the non-obsolete constructor. Scheduled for removal in Umbraco 18.")]
     public MediaMapDefinition(
@@ -48,6 +70,39 @@ public class MediaMapDefinition : ContentMapDefinition<IMedia, MediaValueRespons
         target.Values = MapValueViewModels(source.Properties);
         target.Variants = MapVariantViewModels(source);
         target.IsTrashed = source.Trashed;
+
+        // If protection for media files in the recycle bin is enabled, and the media item is trashed, amend the value of the file path
+        // to have the `.deleted` suffix that will have been added to the persisted file.
+        if (target.IsTrashed && _imagingSettings.EnableMediaRecycleBinProtection)
+        {
+            foreach (MediaValueResponseModel value in target.Values
+                .Where(x => x.EditorAlias.Equals(Core.Constants.PropertyEditors.Aliases.ImageCropper)))
+            {
+                if (value.Value is not null &&
+                    value.Value is ImageCropperValue imageCropperValue &&
+                    string.IsNullOrWhiteSpace(imageCropperValue.Src) is false)
+                {
+                    value.Value = new ImageCropperValue
+                    {
+                        Crops = imageCropperValue.Crops,
+                        FocalPoint = imageCropperValue.FocalPoint,
+                        TemporaryFileId = imageCropperValue.TemporaryFileId,
+                        Src = SuffixMediaPath(imageCropperValue.Src, Core.Constants.Conventions.Media.TrashedMediaSuffix),
+                    };
+                }
+            }
+        }
+    }
+
+    private static string SuffixMediaPath(string filePath, string suffix)
+    {
+        int lastDotIndex = filePath.LastIndexOf('.');
+        if (lastDotIndex == -1)
+        {
+            return filePath + suffix;
+        }
+
+        return filePath[..lastDotIndex] + suffix + filePath[lastDotIndex..];
     }
 
     // Umbraco.Code.MapAll -Flags
