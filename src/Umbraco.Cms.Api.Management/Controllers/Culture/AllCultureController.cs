7799febac02da557a83ec1a8300045091cd634d9
Owain Williams <owaingdwilliams@gmail.com>
2025-10-08 19:06:03 +0300

    [ADD] Cultures Summary and Description

--- src/Umbraco.Cms.Api.Management/Controllers/Culture/AllCultureController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/Culture/AllCultureController.cs
@@ -22,12 +22,14 @@ public class AllCultureController : CultureControllerBase
     }
 
     /// <summary>
-    ///     Returns all cultures available for creating languages.
+    ///     Retrieve a paginated list of supported cultures for creating languages
     /// </summary>
     /// <returns></returns>
     [HttpGet]
     [MapToApiVersion("1.0")]
     [ProducesResponseType(typeof(PagedViewModel<CultureReponseModel>), StatusCodes.Status200OK)]
+    [EndpointSummary("Retrieve a paginated list of supported cultures for creating languages")]
+    [EndpointDescription("Returns a paginated collection of supported culture information available in Umbraco. Use the `skip` and `take` parameters to control pagination. Each item includes culture-specific data such as name, display name, and locale identifiers.")]
     public async Task<PagedViewModel<CultureReponseModel>> GetAll(CancellationToken cancellationToken, int skip = 0, int take = 100)
     {
         CultureInfo[] all = _cultureService.GetValidCultureInfos();
