deda3ffb8e368da72af20e8658e009f72369bd29
Rick Butterfield <rcb@umbraco.dk>
2025-10-08 15:16:01 +0100

    Obsolete interfaces without `expires` parameter, delegate to an existing method.

--- src/Umbraco.Core/Mail/IEmailSender.cs
+++ src/Umbraco.Core/Mail/IEmailSender.cs
@@ -7,13 +7,21 @@ namespace Umbraco.Cms.Core.Mail;
 /// </summary>
 public interface IEmailSender
 {
+    [Obsolete("Use the overload with expires parameter.")]
     Task SendAsync(EmailMessage message, string emailType);
 
-    Task SendAsync(EmailMessage message, string emailType, TimeSpan? expires);
+    Task SendAsync(EmailMessage message, string emailType, TimeSpan? expires)
+#pragma warning disable CS0618 // Type or member is obsolete
+        => SendAsync(message, emailType);
+#pragma warning restore CS0618 // Type or member is obsolete
 
+    [Obsolete("Use the overload with expires parameter.")]
     Task SendAsync(EmailMessage message, string emailType, bool enableNotification);
 
-    Task SendAsync(EmailMessage message, string emailType, bool enableNotification, TimeSpan? expires);
+    Task SendAsync(EmailMessage message, string emailType, bool enableNotification, TimeSpan? expires)
+#pragma warning disable CS0618 // Type or member is obsolete
+        => SendAsync(message, emailType, enableNotification);
+#pragma warning restore CS0618 // Type or member is obsolete
 
     bool CanSendRequiredEmail();
 }
