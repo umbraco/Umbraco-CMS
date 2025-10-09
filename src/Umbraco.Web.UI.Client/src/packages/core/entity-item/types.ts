5eea225bf29bf86889b15c7e52f19108fcb9513b
Mads Rasmussen <madsr@hey.com>
2025-10-09 10:58:15 +0200

    add todo

--- src/Umbraco.Web.UI.Client/src/packages/core/entity-item/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/entity-item/types.ts
@@ -1,6 +1,10 @@
 import type { UmbEntityModel, UmbNamedEntityModel } from '@umbraco-cms/backoffice/entity';
 export type * from './item-data-api-get-request-controller/types.js';
 
+// TODO: v19 - remove
+/**
+ * @deprecated - Deprecated since v17. Will be removed in v19. Use UmbItemModel instead.
+ */
 export interface UmbDefaultItemModel extends UmbNamedEntityModel {
 	icon?: string;
 }
