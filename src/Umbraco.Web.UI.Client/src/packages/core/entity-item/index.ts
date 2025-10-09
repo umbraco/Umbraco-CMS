7dfb9a1db9c849a6ab890158271d19615332ba3f
Mads Rasmussen <madsr@hey.com>
2025-10-09 10:57:57 +0200

    add utils for fallback name and icon

--- src/Umbraco.Web.UI.Client/src/packages/core/entity-item/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/entity-item/index.ts
@@ -1,4 +1,5 @@
 export * from './item-data-api-get-request-controller/index.js';
 export * from './entity-item-ref/index.js';
+export * from './utils.js';
 
 export type { UmbItemModel } from './types.js';
