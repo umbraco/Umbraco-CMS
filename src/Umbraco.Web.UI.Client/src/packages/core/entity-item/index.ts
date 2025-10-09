447c27c2e37da376294ae1483e7fbd0e1a2c27d9
Mads Rasmussen <madsr@hey.com>
2025-10-09 08:51:06 +0200

    add interface for item model

--- src/Umbraco.Web.UI.Client/src/packages/core/entity-item/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/entity-item/index.ts
@@ -1,2 +1,4 @@
 export * from './item-data-api-get-request-controller/index.js';
 export * from './entity-item-ref/index.js';
+
+export type { UmbItemModel } from './types.js';
--- src/Umbraco.Web.UI.Client/src/packages/core/entity-item/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/entity-item/types.ts
@@ -1,6 +1,12 @@
-import type { UmbNamedEntityModel } from '@umbraco-cms/backoffice/entity';
+import type { UmbEntityModel, UmbNamedEntityModel } from '@umbraco-cms/backoffice/entity';
 export type * from './item-data-api-get-request-controller/types.js';
 
 export interface UmbDefaultItemModel extends UmbNamedEntityModel {
+	icon?: string;
+}
+
+export interface UmbItemModel extends UmbEntityModel {
+	unique: string;
+	name?: string;
 	icon?: string;
 }
