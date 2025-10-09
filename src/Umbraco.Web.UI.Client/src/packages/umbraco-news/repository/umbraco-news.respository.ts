1ea18139ec27a51ae1100a98353e9a1a097da952
leekelleher <leekelleher@gmail.com>
2025-10-09 16:05:38 +0100

    Updates News repository to use `NewsDashboardService`
    This automatically wires up with the mock data when not using the server.

--- src/Umbraco.Web.UI.Client/src/packages/umbraco-news/repository/umbraco-news.respository.ts
+++ src/Umbraco.Web.UI.Client/src/packages/umbraco-news/repository/umbraco-news.respository.ts
@@ -1,24 +1,10 @@
-import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
-import type { UmbDataSourceResponse } from '@umbraco-cms/backoffice/repository';
+import { tryExecute } from '@umbraco-cms/backoffice/resources';
+import { NewsDashboardService } from '@umbraco-cms/backoffice/external/backend-api';
+import { UmbRepositoryBase } from '@umbraco-cms/backoffice/repository';
 
-// Mock data server for now (backend not ready)
-import { UmbNewsStoriesMockDataSource } from './sources/umbraco-news.mock.data.js';
-
-//In the log-viewer they use the Models from the backend-api
-//to define the types, but here I am not sure where to use it
-import type { ServerNewsStory } from './sources/index.js';
-
-export class UmbNewsStoriesRepository {
-	#host: UmbControllerHost;
-	#mockDS: UmbNewsStoriesMockDataSource;
-
-	constructor(host: UmbControllerHost) {
-		this.#host = host;
-		this.#mockDS = new UmbNewsStoriesMockDataSource(this.#host);
-	}
-
-	/** Fetch all stories. */
-	async getAll() {
-		return this.#mockDS.getAllNewsStories();
+export class UmbNewsDashboardRepository extends UmbRepositoryBase {
+	async getNewsDashboard() {
+		const { data } = await tryExecute(this, NewsDashboardService.getNewsDashboard());
+		return data;
 	}
 }
