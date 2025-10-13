6f95757b664971de66fb00968c237448f7bb6694
leekelleher <leekelleher@gmail.com>
2025-10-13 16:13:42 +0100

    Simplified the news repo/data-source classes
    by extending `UmbControllerBase`, the host constructor is handled for us.

--- src/Umbraco.Web.UI.Client/src/packages/umbraco-news/repository/umbraco-news.respository.ts
+++ src/Umbraco.Web.UI.Client/src/packages/umbraco-news/repository/umbraco-news.respository.ts
@@ -1,18 +1,10 @@
-import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import { UmbNewsServerDataSource } from './umbraco-news.server.data-source.js';
 import { UmbRepositoryBase } from '@umbraco-cms/backoffice/repository';
 import type { NewsDashboardResponseModel } from '@umbraco-cms/backoffice/external/backend-api';
 import type { UmbDataSourceResponse } from '@umbraco-cms/backoffice/repository';
 
 export class UmbNewsDashboardRepository extends UmbRepositoryBase {
-	#host: UmbControllerHost;
-	#dataSource: UmbNewsServerDataSource;
-
-	constructor(host: UmbControllerHost) {
-		super(host);
-		this.#host = host;
-		this.#dataSource = new UmbNewsServerDataSource(this.#host);
-	}
+	#dataSource = new UmbNewsServerDataSource(this);
 
 	async getNewsDashboard(): Promise<UmbDataSourceResponse<NewsDashboardResponseModel>> {
 		return this.#dataSource.getNewsItems();
--- src/Umbraco.Web.UI.Client/src/packages/umbraco-news/repository/umbraco-news.server.data-source.ts
+++ src/Umbraco.Web.UI.Client/src/packages/umbraco-news/repository/umbraco-news.server.data-source.ts
@@ -1,31 +1,25 @@
-import type { UmbNewsDataSource } from './index.js';
-import { NewsDashboardService } from '@umbraco-cms/backoffice/external/backend-api';
-import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import { tryExecute } from '@umbraco-cms/backoffice/resources';
+import { NewsDashboardService } from '@umbraco-cms/backoffice/external/backend-api';
+import { UmbControllerBase } from '@umbraco-cms/backoffice/class-api';
+import type { NewsDashboardResponseModel } from '@umbraco-cms/backoffice/external/backend-api';
+import type { UmbDataSourceResponse } from '@umbraco-cms/backoffice/repository';
+
+export interface UmbNewsDataSource {
+	getNewsItems(): Promise<UmbDataSourceResponse<NewsDashboardResponseModel>>;
+}
 
 /**
  * A data source for the news items
  * @class UmbNewsServerDataSource
  * @implements {UmbNewsDataSource}
  */
-export class UmbNewsServerDataSource implements UmbNewsDataSource {
-	#host: UmbControllerHost;
-
-	/**
-	 * Creates an instance of UmbNewsServerDataSource.
-	 * @param {UmbControllerHost} host - The controller host for this controller to be appended to
-	 * @memberof UmbNewsServerDataSource
-	 */
-	constructor(host: UmbControllerHost) {
-		this.#host = host;
-	}
-
+export class UmbNewsServerDataSource extends UmbControllerBase implements UmbNewsDataSource {
 	/**
 	 * Get all news items from the server
 	 * @returns {*}
 	 * @memberof UmbNewsServerDataSource
 	 */
 	async getNewsItems() {
-		return await tryExecute(this.#host, NewsDashboardService.getNewsDashboard());
+		return await tryExecute(this._host, NewsDashboardService.getNewsDashboard());
 	}
 }
