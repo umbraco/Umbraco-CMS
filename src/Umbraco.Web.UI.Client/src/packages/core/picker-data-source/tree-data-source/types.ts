97d1d0171f8b6ebb9665be82ca3ed7a32cba0e17
Mads Rasmussen <madsr@hey.com>
2025-10-13 22:28:39 +0200

    Update types.ts

--- src/Umbraco.Web.UI.Client/src/packages/core/picker-data-source/tree-data-source/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/picker-data-source/tree-data-source/types.ts
@@ -1,12 +1,11 @@
 import type { UmbPickerDataSource } from '../data-source/types.js';
-import type { UmbItemModel } from '@umbraco-cms/backoffice/entity-item';
 import type { UmbApi } from '@umbraco-cms/backoffice/extension-api';
 import type { UmbTreeItemModel, UmbTreeRepository, UmbTreeRootModel } from '@umbraco-cms/backoffice/tree';
 
 export interface UmbPickerTreeDataSource<
 	TreeItemType extends UmbTreeItemModel = UmbTreeItemModel,
 	TreeRootType extends UmbTreeRootModel = UmbTreeRootModel,
-> extends UmbPickerDataSource<UmbItemModel, TreeItemType>,
+> extends UmbPickerDataSource,
 		UmbTreeRepository<TreeItemType, TreeRootType>,
 		UmbApi {
 	treePickableFilter?: (item: TreeItemType) => boolean;
