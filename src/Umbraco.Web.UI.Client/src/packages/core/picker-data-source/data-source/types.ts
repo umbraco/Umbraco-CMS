6f0880a4615c529eb73d5cf7b1556725d2dc53fe
Mads Rasmussen <madsr@hey.com>
2025-10-13 21:48:00 +0200

    set up types for pickable filters in data sources

--- src/Umbraco.Web.UI.Client/src/packages/core/picker-data-source/data-source/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/picker-data-source/data-source/types.ts
@@ -1,8 +1,13 @@
+import type { UmbItemModel } from '@umbraco-cms/backoffice/entity-item';
 import type { UmbApi } from '@umbraco-cms/backoffice/extension-api';
 import type { UmbItemRepository } from '@umbraco-cms/backoffice/repository';
 import type { UmbConfigCollectionModel } from '@umbraco-cms/backoffice/utils';
 
-export interface UmbPickerDataSource extends UmbItemRepository<any>, UmbApi {
+export interface UmbPickerDataSource<
+	PickedItemType extends UmbItemModel = UmbItemModel,
+	PickerItemType extends UmbItemModel = UmbItemModel,
+> extends UmbItemRepository<PickedItemType>,
+		UmbApi {
 	setConfig?(config: UmbConfigCollectionModel | undefined): void;
 	getConfig?(): UmbConfigCollectionModel | undefined;
 }
--- src/Umbraco.Web.UI.Client/src/packages/core/picker-data-source/tree-data-source/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/picker-data-source/tree-data-source/types.ts
@@ -1,5 +1,13 @@
 import type { UmbPickerDataSource } from '../data-source/types.js';
+import type { UmbItemModel } from '@umbraco-cms/backoffice/entity-item';
 import type { UmbApi } from '@umbraco-cms/backoffice/extension-api';
-import type { UmbTreeRepository } from '@umbraco-cms/backoffice/tree';
+import type { UmbTreeItemModel, UmbTreeRepository, UmbTreeRootModel } from '@umbraco-cms/backoffice/tree';
 
-export interface UmbPickerTreeDataSource extends UmbPickerDataSource, UmbTreeRepository, UmbApi {}
+export interface UmbPickerTreeDataSource<
+	TreeItemType extends UmbTreeItemModel = UmbTreeItemModel,
+	TreeRootType extends UmbTreeRootModel = UmbTreeRootModel,
+> extends UmbPickerDataSource<UmbItemModel, TreeItemType>,
+		UmbTreeRepository<TreeItemType, TreeRootType>,
+		UmbApi {
+	treePickableFilter?: (item: TreeItemType) => boolean;
+}
