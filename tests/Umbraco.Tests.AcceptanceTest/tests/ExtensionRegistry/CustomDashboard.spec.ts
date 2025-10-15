72314706c627bb47153e19ec72d5c7682c45e878
Lan Nguyen Thuy <lnt@umbraco.dk>
2025-10-15 09:40:24 +0700

    fix format code

--- tests/Umbraco.Tests.AcceptanceTest/tests/ExtensionRegistry/CustomDashboard.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/ExtensionRegistry/CustomDashboard.spec.ts
@@ -1,22 +1,22 @@
 import {ConstantHelper, test} from '@umbraco/playwright-testhelpers';
 
-//Dashboard
+// Dashboard
 const dashboardName = 'Welcome Dashboard';
 
 test('can see the custom dashboard in content section', async ({umbracoUi}) => {
-    // Act
-    await umbracoUi.goToBackOffice();
-    await umbracoUi.content.goToSection(ConstantHelper.sections.content);
+  // Act
+  await umbracoUi.goToBackOffice();
+  await umbracoUi.content.goToSection(ConstantHelper.sections.content);
 
-    // Assert
-    await umbracoUi.content.isDashboardTabWithNameVisible(dashboardName, true);
+  // Assert
+  await umbracoUi.content.isDashboardTabWithNameVisible(dashboardName, true);
 });
 
 test('can not see the custom dashboard in media section', async ({umbracoUi}) => {
-    // Act
-    await umbracoUi.goToBackOffice();
-    await umbracoUi.content.goToSection(ConstantHelper.sections.media);
+  // Act
+  await umbracoUi.goToBackOffice();
+  await umbracoUi.content.goToSection(ConstantHelper.sections.media);
 
-    // Assert
-    await umbracoUi.content.isDashboardTabWithNameVisible(dashboardName, false);
+  // Assert
+  await umbracoUi.content.isDashboardTabWithNameVisible(dashboardName, false);
 });
