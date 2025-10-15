db8d4865ea5977f80373b72493eab79432c54d5d
Mads Rasmussen <madsr@hey.com>
2025-10-15 13:18:59 +0200

    register user collection menu

--- src/Umbraco.Web.UI.Client/src/packages/user/user/collection/menu/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/collection/menu/manifests.ts
@@ -1,6 +1,17 @@
 import { UMB_USER_ENTITY_TYPE } from '../../entity.js';
+import { UMB_USER_COLLECTION_REPOSITORY_ALIAS } from '../repository/constants.js';
+import { UMB_USER_COLLECTION_MENU_ALIAS } from './constants.js';
 
 export const manifests: Array<UmbExtensionManifest> = [
+	{
+		type: 'collectionMenu',
+		kind: 'default',
+		alias: UMB_USER_COLLECTION_MENU_ALIAS,
+		name: 'User Collection Menu',
+		meta: {
+			collectionRepositoryAlias: UMB_USER_COLLECTION_REPOSITORY_ALIAS,
+		},
+	},
 	{
 		type: 'collectionMenuItem',
 		kind: 'default',
