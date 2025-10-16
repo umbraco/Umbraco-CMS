23ff105539102eaf8f3ca265bc8f91ff265cec85
Mads Rasmussen <madsr@hey.com>
2025-10-16 14:55:58 +0200

    remove duplicate request to reload

--- src/Umbraco.Web.UI.Client/src/packages/media/media/collection/media-collection.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/collection/media-collection.element.ts
@@ -62,7 +62,6 @@ export class UmbMediaCollectionElement extends UmbCollectionDefaultElement {
 	async #onComplete(event: Event) {
 		event.preventDefault();
 		this._progress = -1;
-		this.#collectionContext?.loadCollection();
 
 		const eventContext = await this.getContext(UMB_ACTION_EVENT_CONTEXT);
 		if (!eventContext) {
