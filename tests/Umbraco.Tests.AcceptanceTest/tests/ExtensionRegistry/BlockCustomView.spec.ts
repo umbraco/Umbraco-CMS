720ad663130c7ffd413aded82089a977da5b1575
Lan Nguyen Thuy <lnt@umbraco.dk>
2025-10-17 09:55:12 +0700

    add new test for block custom view

--- tests/Umbraco.Tests.AcceptanceTest/tests/ExtensionRegistry/BlockCustomView.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/ExtensionRegistry/BlockCustomView.spec.ts
@@ -14,7 +14,7 @@ const blockListName = 'TestBlockListForContent';
 // ElementType
 const elementGroupName = 'TestElementGroupForContent';
 const firstElementTypeName = 'TestElementTypeForContent';
-const secondElementTypeName = 'Hero Banner';
+const secondElementTypeName = 'Element Type For Custom Block View';
 // Setting Model
 const settingModelName = 'Test Setting Model';
 const groupName = 'Test Group';
@@ -125,9 +125,44 @@ test('block custom view can display values from the content and settings parts',
 	await umbracoUi.content.clickAddBlockSettingsTabButton();
 	await umbracoUi.content.enterTextArea(settingValue);
 	await umbracoUi.content.clickCreateModalButton();
-	await umbracoUi.waitForTimeout(5000);
 
 	// Assert
 	await umbracoUi.content.isBlockCustomViewVisible(blockCustomViewLocator);
 	await umbracoUi.content.doesBlockCustomViewHaveValue(blockCustomViewLocator, valueText);
+});
+
+test('block custom view can display values from the content and settings parts after update', async ({umbracoApi, umbracoUi}) => {
+	// Arrange
+	const contentValue = 'This is block test';
+	const settingValue = 'This is setting test';
+	const updatedContentValue = 'This is updated block test';
+	const updatedSettingValue = 'This is updated setting test';
+	const updatedValueText = `Heading and Theme: ${updatedContentValue} - ${updatedSettingValue}`;
+	const textStringDataType = await umbracoApi.dataType.getByName(dataTypeName);
+	const textAreaDataType = await umbracoApi.dataType.getByName(textAreaDataTypeName);
+	const elementTypeId = await umbracoApi.documentType.createDefaultElementType(secondElementTypeName, elementGroupName, propertyEditorName, textStringDataType.id);
+	const settingsElementTypeId = await umbracoApi.documentType.createDefaultElementType(settingModelName, groupName, propertyEditorSettingName, textAreaDataType.id);
+	const blockListId = await umbracoApi.dataType.createBlockListDataTypeWithContentAndSettingsElementType(blockListName, elementTypeId, settingsElementTypeId);
+	const documentTypeId = await umbracoApi.documentType.createDocumentTypeWithPropertyEditor(documentTypeName, blockListName, blockListId, contentGroupName);
+	await umbracoApi.document.createDefaultDocument(contentName, documentTypeId);
+
+	// Act
+	await umbracoUi.goToBackOffice();
+	await umbracoUi.content.goToSection(ConstantHelper.sections.content);
+	await umbracoUi.content.goToContentWithName(contentName);
+	await umbracoUi.content.clickAddBlockElementButton();
+	await umbracoUi.content.clickBlockCardWithName(secondElementTypeName);
+	await umbracoUi.content.enterTextstring(contentValue);
+	await umbracoUi.content.clickAddBlockSettingsTabButton();
+	await umbracoUi.content.enterTextArea(settingValue);
+	await umbracoUi.content.clickCreateModalButton();
+	await umbracoUi.content.clickEditBlockListBlockButton();
+	await umbracoUi.content.enterTextstring(updatedContentValue);
+	await umbracoUi.content.clickAddBlockSettingsTabButton();
+	await umbracoUi.content.enterTextArea(updatedSettingValue);
+	await umbracoUi.content.clickUpdateButton();
+
+	// Assert
+	await umbracoUi.content.isBlockCustomViewVisible(blockCustomViewLocator);
+	await umbracoUi.content.doesBlockCustomViewHaveValue(blockCustomViewLocator, updatedValueText);
 });
