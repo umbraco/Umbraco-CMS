0e919783b904c82c8894ce1194c88dcc6fe48b1d
kjac <kja@umbraco.dk>
2025-10-22 09:13:13 +0200

    Remove IsExternal from the API again

--- src/Umbraco.Cms.Api.Management/Factories/DocumentUrlFactory.cs
+++ src/Umbraco.Cms.Api.Management/Factories/DocumentUrlFactory.cs
@@ -98,7 +98,6 @@ public class DocumentUrlFactory : IDocumentUrlFactory
             Url = url,
             Message = urlInfo.Message,
             Provider = urlInfo.Provider,
-            IsExternal = urlInfo.IsExternal,
         };
     }
 }
--- src/Umbraco.Cms.Api.Management/ViewModels/Document/DocumentUrlInfo.cs
+++ src/Umbraco.Cms.Api.Management/ViewModels/Document/DocumentUrlInfo.cs
@@ -7,6 +7,4 @@ public class DocumentUrlInfo : ContentUrlInfoBase
     public required string? Message { get; init; }
 
     public required string Provider { get; init; }
-
-    public required bool IsExternal { get; init; }
 }
