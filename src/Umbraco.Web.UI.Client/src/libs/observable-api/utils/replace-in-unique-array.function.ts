524bee92cd004adb23672be81a16343864f2d8ae
Niels Lyngsø <niels.lyngso@gmail.com>
2025-10-22 11:08:17 +0200

    fix js docs

--- src/Umbraco.Web.UI.Client/src/libs/observable-api/utils/replace-in-unique-array.function.ts
+++ src/Umbraco.Web.UI.Client/src/libs/observable-api/utils/replace-in-unique-array.function.ts
@@ -7,7 +7,7 @@
  * @example <caption>Example replace an entry of an Array. Where the key is unique and the item will only be replaced if matched with existing.</caption>
  * const data = [{key: 'myKey', value:'initialValue'}];
  * const entry = {key: 'myKey', value: 'replacedValue'};
- * const newDataSet = replaceUniqueArray(data, entry, x => x.key === key);
+ * const newDataSet = replaceInUniqueArray(data, entry, x => x.key === key);
  */
 export function replaceInUniqueArray<T>(data: T[], entry: T, getUniqueMethod: (entry: T) => unknown): T[] {
 	const unique = getUniqueMethod(entry);
