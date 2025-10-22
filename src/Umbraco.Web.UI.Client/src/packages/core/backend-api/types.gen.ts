a06b7cc6c5525042956418ffcd9b9eb0fd99d2b0
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-10-22 07:56:19 +0200

    chore: generates api models

--- src/Umbraco.Web.UI.Client/src/packages/core/backend-api/types.gen.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/backend-api/types.gen.ts
@@ -856,6 +856,7 @@ export type DocumentUrlInfoModel = {
     url: string | null;
     message: string | null;
     provider: string;
+    isExternal: boolean;
 };
 
 export type DocumentUrlInfoResponseModel = {
