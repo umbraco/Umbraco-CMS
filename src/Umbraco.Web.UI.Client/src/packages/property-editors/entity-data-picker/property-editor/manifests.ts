8f67c4b7e552f3f84111916582351d560299f5fc
Mads Rasmussen <madsr@hey.com>
2025-10-27 13:16:30 +0100

    register schema for property editor + move settings ui

--- src/Umbraco.Web.UI.Client/src/packages/property-editors/entity-data-picker/property-editor/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/property-editors/entity-data-picker/property-editor/manifests.ts
@@ -1,34 +1,23 @@
+import { manifests as schemaManifests } from './Umbraco.EntityDataPicker.js';
 import { UMB_PICKER_DATA_SOURCE_TYPE } from '@umbraco-cms/backoffice/picker-data-source';
-import type { ManifestPropertyEditorUi } from '@umbraco-cms/backoffice/property-editor';
 
-const manifest: ManifestPropertyEditorUi = {
-	type: 'propertyEditorUi',
-	alias: 'Umb.PropertyEditorUi.EntityDataPicker',
-	name: 'Entity Data Picker Property Editor UI',
-	element: () => import('./entity-data-picker-property-editor-ui.element.js'),
-	meta: {
-		label: 'Entity Data Picker',
-		icon: 'icon-page-add',
-		group: 'pickers',
-		propertyEditorSchemaAlias: 'Umbraco.EntityDataPicker',
-		supportsReadOnly: true,
-		supportsDataSource: {
-			enabled: true,
-			forDataSourceTypes: [UMB_PICKER_DATA_SOURCE_TYPE],
-		},
-		settings: {
-			properties: [
-				// TODO: Move this to schema manifest when server can validate it
-				{
-					alias: 'validationLimit',
-					label: 'Amount',
-					propertyEditorUiAlias: 'Umb.PropertyEditorUi.NumberRange',
-					config: [{ alias: 'validationRange', value: { min: 0, max: Infinity } }],
-					weight: 100,
-				},
-			],
+export const manifests: Array<UmbExtensionManifest> = [
+	{
+		type: 'propertyEditorUi',
+		alias: 'Umb.PropertyEditorUi.EntityDataPicker',
+		name: 'Entity Data Picker Property Editor UI',
+		element: () => import('./entity-data-picker-property-editor-ui.element.js'),
+		meta: {
+			label: 'Entity Data Picker',
+			icon: 'icon-page-add',
+			group: 'pickers',
+			propertyEditorSchemaAlias: 'Umbraco.EntityDataPicker',
+			supportsReadOnly: true,
+			supportsDataSource: {
+				enabled: true,
+				forDataSourceTypes: [UMB_PICKER_DATA_SOURCE_TYPE],
+			},
 		},
 	},
-};
-
-export const manifests: Array<UmbExtensionManifest> = [manifest];
+	...schemaManifests,
+];
