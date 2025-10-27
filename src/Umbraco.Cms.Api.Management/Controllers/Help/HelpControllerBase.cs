842a6579d9071d183fcd8533b28c08a5f61d761d
Sebastiaan Janssen <sebastiaan@umbraco.com>
2025-10-27 10:41:37 +0100

    Obsolete the HelpController instead

---
+++ src/Umbraco.Cms.Api.Management/Controllers/Help/HelpControllerBase.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Management.Routing;
+
+namespace Umbraco.Cms.Api.Management.Controllers.Help;
+
+[Obsolete("This is no longer used and will be removed in v19")]
+[VersionedApiBackOfficeRoute("help")]
+[ApiExplorerSettings(GroupName = "Help")]
+public abstract class HelpControllerBase : ManagementApiControllerBase
+{
+}
