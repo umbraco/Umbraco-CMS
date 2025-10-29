bba555f371b9af29fe7bb0da920d6e2b8cbae342
Niels Lyngsø <niels.lyngso@gmail.com>
2025-10-29 14:44:42 +0100

    move logic and update comment

--- src/Umbraco.Web.UI.Client/src/apps/backoffice/components/backoffice-header-sections.element.ts
+++ src/Umbraco.Web.UI.Client/src/apps/backoffice/components/backoffice-header-sections.element.ts
@@ -71,13 +71,6 @@ export class UmbBackofficeHeaderSectionsElement extends UmbLitElement {
 		event.stopPropagation();
 		event.preventDefault();
 
-		// useNavigatingState is set false, then the navigation store state is disabled for the current sections
-		if (manifest?.meta.preventUrlRetention !== true) {
-			const sectionPath = this.#getSectionPath(manifest);
-			history.pushState(null, '', sectionPath);
-			return;
-		}
-
 		// Store the current path for the section so we can redirect to it next time the section is visited
 		if (this._currentSectionAlias) {
 			const currentPath = window.location.pathname;
@@ -90,8 +83,9 @@ export class UmbBackofficeHeaderSectionsElement extends UmbLitElement {
 
 		const clickedSectionAlias = manifest.alias;
 
-		// If the clicked section is the same as the current section, we just load the original section path to load the section root
-		if (this._currentSectionAlias === clickedSectionAlias) {
+		// If preventUrlRetention is set to true then go to the section root.
+		// Or if the clicked section is the current active one, then navigate to the section root
+		if (manifest?.meta.preventUrlRetention === true || this._currentSectionAlias === clickedSectionAlias) {
 			const sectionPath = this.#getSectionPath(manifest);
 			history.pushState(null, '', sectionPath);
 			return;
