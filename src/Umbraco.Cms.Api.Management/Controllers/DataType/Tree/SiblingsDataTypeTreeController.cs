773617f09c73b5df5d251d46efc7424760944c5a
Owain Williams <owaingdwilliams@gmail.com>
2025-10-30 07:31:58 +0000

    Update src/Umbraco.Cms.Api.Management/Controllers/DataType/Tree/SiblingsDataTypeTreeController.cs
    Co-authored-by: Andy Butland <abutland73@gmail.com>

--- src/Umbraco.Cms.Api.Management/Controllers/DataType/Tree/SiblingsDataTypeTreeController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/DataType/Tree/SiblingsDataTypeTreeController.cs
@@ -25,7 +25,7 @@ public class SiblingsDataTypeTreeController : DataTypeTreeControllerBase
     [HttpGet("siblings")]
     [ProducesResponseType(typeof(SubsetViewModel<DataTypeTreeItemResponseModel>), StatusCodes.Status200OK)]
     [EndpointSummary("Gets a collection of data type tree sibling items.")]
-    [EndpointDescription("Get sibling data types of the target data type. The result returns a paged list of data types that are siblings to the target data type identified by the provided Id.")]
+    [EndpointDescription("Gets a paged collection of data type tree items that are siblings of the provided parent Id. The collection can be optionally filtered to return only folder, or folders and data types.  The result returns the data type tree items' properties.")]
     public async Task<ActionResult<SubsetViewModel<DataTypeTreeItemResponseModel>>> Siblings(CancellationToken cancellationToken, Guid target, int before, int after, bool foldersOnly = false)
     {
         RenderFoldersOnly(foldersOnly);
