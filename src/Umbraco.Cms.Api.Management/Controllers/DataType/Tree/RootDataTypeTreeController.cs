4010422d9450096c0005e33bc3bb6baacd12700a
Owain Williams <owaingdwilliams@gmail.com>
2025-10-30 07:31:23 +0000

    Update src/Umbraco.Cms.Api.Management/Controllers/DataType/Tree/RootDataTypeTreeController.cs
    Co-authored-by: Andy Butland <abutland73@gmail.com>

--- src/Umbraco.Cms.Api.Management/Controllers/DataType/Tree/RootDataTypeTreeController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/DataType/Tree/RootDataTypeTreeController.cs
@@ -28,7 +28,7 @@ public class RootDataTypeTreeController : DataTypeTreeControllerBase
     [MapToApiVersion("1.0")]
     [ProducesResponseType(typeof(PagedViewModel<DataTypeTreeItemResponseModel>), StatusCodes.Status200OK)]
     [EndpointSummary("Get root data type tree items.")]
-    [EndpointDescription("Get root data type tree items with optional pagination and folder filtering.")]
+    [EndpointDescription("Gets a paged collection of data type items from the root of the data type tree. The collection can be optionally filtered to return only folder, or folders and data types. The result returns the data type tree items' properties.")]
     public async Task<ActionResult<PagedViewModel<DataTypeTreeItemResponseModel>>> Root(CancellationToken cancellationToken, int skip = 0, int take = 100, bool foldersOnly = false)
     {
         RenderFoldersOnly(foldersOnly);
