bf1f5bb64a7cd1b41849ddcbff23d75cdd947209
Mads Rasmussen <madsr@hey.com>
2025-10-30 19:00:05 +0100

    add menu context and breadcrumbs for document type folders

--- src/Umbraco.Web.UI.Client/src/packages/documents/document-types/menu/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/document-types/menu/manifests.ts
@@ -1,4 +1,5 @@
-import { UMB_DOCUMENT_TYPE_TREE_ALIAS } from '../tree/index.js';
+import { UMB_DOCUMENT_TYPE_WORKSPACE_ALIAS } from '../constants.js';
+import { UMB_DOCUMENT_TYPE_FOLDER_WORKSPACE_ALIAS, UMB_DOCUMENT_TYPE_TREE_ALIAS } from '../tree/index.js';
 import { UMB_DOCUMENT_TYPE_MENU_ITEM_ALIAS } from './constants.js';
 import { UMB_WORKSPACE_CONDITION_ALIAS } from '@umbraco-cms/backoffice/workspace';
 
@@ -27,7 +28,7 @@ export const manifests: Array<UmbExtensionManifest> = [
 		conditions: [
 			{
 				alias: UMB_WORKSPACE_CONDITION_ALIAS,
-				match: 'Umb.Workspace.DocumentType',
+				match: UMB_DOCUMENT_TYPE_WORKSPACE_ALIAS,
 			},
 		],
 	},
@@ -40,6 +41,34 @@ export const manifests: Array<UmbExtensionManifest> = [
 			{
 				alias: UMB_WORKSPACE_CONDITION_ALIAS,
 				match: 'Umb.Workspace.DocumentType',
+			},
+		],
+	},
+	{
+		type: 'workspaceContext',
+		kind: 'menuStructure',
+		name: 'Document Type Folder Menu Structure Workspace Context',
+		alias: 'Umb.Context.DocumentTypeFolder.Menu.Structure',
+		api: () => import('./document-type-menu-structure.context.js'),
+		meta: {
+			menuItemAlias: UMB_DOCUMENT_TYPE_MENU_ITEM_ALIAS,
+		},
+		conditions: [
+			{
+				alias: UMB_WORKSPACE_CONDITION_ALIAS,
+				match: UMB_DOCUMENT_TYPE_FOLDER_WORKSPACE_ALIAS,
+			},
+		],
+	},
+	{
+		type: 'workspaceFooterApp',
+		kind: 'menuBreadcrumb',
+		alias: 'Umb.WorkspaceFooterApp.DocumentTypeFolder.Breadcrumb',
+		name: 'Document Type Folder Breadcrumb Workspace Footer App',
+		conditions: [
+			{
+				alias: UMB_WORKSPACE_CONDITION_ALIAS,
+				match: UMB_DOCUMENT_TYPE_FOLDER_WORKSPACE_ALIAS,
 			},
 		],
 	},
