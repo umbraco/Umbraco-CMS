89b37f659833310d36675d70d1ec09fd650680d5
Andy Butland <abutland73@gmail.com>
2025-11-07 09:20:55 +0100

    Linting.

--- src/Umbraco.Web.UI.Client/src/packages/core/property-value-presentation/property-value-presentation-base.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property-value-presentation/property-value-presentation-base.ts
@@ -1,6 +1,6 @@
 import { property } from "@umbraco-cms/backoffice/external/lit";
 import { UmbLitElement } from "@umbraco-cms/backoffice/lit-element";
-import type { UmbPropertyValuePresentationDisplayOption } from "src/packages/core/property-value-presentation/property-value-presentation.extension";
+import type { UmbPropertyValuePresentationDisplayOption } from "../../core/property-value-presentation/property-value-presentation.extension";
 
 export abstract class UmbPropertyValuePresentationBaseElement extends UmbLitElement{
   @property()
