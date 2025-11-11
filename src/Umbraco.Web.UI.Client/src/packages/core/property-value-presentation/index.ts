55a807d8fb8c3b0fd8f538e1cbb9634d970000f0
leekelleher <leekelleher@gmail.com>
2025-11-11 16:50:41 +0000

    Added importmap for "@umbraco-cms/backoffice/property-value-presentation"

--- src/Umbraco.Web.UI.Client/src/packages/core/property-value-presentation/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property-value-presentation/index.ts
@@ -1,3 +1,3 @@
 export { UmbPropertyValuePresentationBaseElement } from './property-value-presentation-base.js';
 export { UmbPropertyValuePresentationDisplayOption } from './property-value-presentation.extension.js';
-export type { ManifestPropertyValuePresentation } from './property-value-presentation.extension.js';
+export type * from './types.js';
