72d4550a4c0b645ead10af66cd82a711ba4aa7f9
leekelleher <leekelleher@gmail.com>
2025-11-20 10:34:16 +0000

    Refactored...
    - Adds JSDocs
    - changed to use `Directive`
    - constructor checks if the directive is being used on an element
    - return `noChange` from update

--- src/Umbraco.Web.UI.Client/src/packages/core/lit-element/directives/ignore-password-managers.lit-directive.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/lit-element/directives/ignore-password-managers.lit-directive.ts
@@ -1,13 +1,23 @@
-import { directive, AsyncDirective, nothing } from '@umbraco-cms/backoffice/external/lit';
-import type { ElementPart } from '@umbraco-cms/backoffice/external/lit';
+import { directive, Directive, noChange, nothing, PartType } from '@umbraco-cms/backoffice/external/lit';
+import type { ElementPart, PartInfo } from '@umbraco-cms/backoffice/external/lit';
 
-class UmbIgnorePasswordManagersDirective extends AsyncDirective {
+/**
+ * @description The directive applies attributes to ignore password managers on the given element.
+ */
+class UmbIgnorePasswordManagersDirective extends Directive {
 	#attributes = [
 		{ name: 'data-1p-ignore', value: '' }, // 1Password
 		{ name: 'data-bwignore', value: '' }, // Bitwarden
 		{ name: 'data-form-type', value: 'other' }, // Dashlane
 		{ name: 'data-lpignore', value: 'true' }, // LastPass
 	];
+
+	constructor(partInfo: PartInfo) {
+		super(partInfo);
+		if (partInfo.type != PartType.ELEMENT) {
+			throw new Error('The `umbIgnorePasswordManagers` directive can only be used in element parts');
+		}
+	}
 
 	override render() {
 		return nothing;
@@ -17,7 +27,12 @@ class UmbIgnorePasswordManagersDirective extends AsyncDirective {
 		this.#attributes.forEach((attr) => {
 			part.element.setAttribute(attr.name, attr.value);
 		});
+		return noChange;
 	}
 }
 
+/**
+ * @description A Lit directive which applies password manager ignore attributes to an element.
+ * @example html`<div ${umbIgnorePasswordManagers()}></div>`
+ */
 export const umbIgnorePasswordManagers = directive(UmbIgnorePasswordManagersDirective);
