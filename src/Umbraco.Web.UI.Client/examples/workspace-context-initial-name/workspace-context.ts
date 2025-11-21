7752d85f3ef2847052e99c518f6237be8baf69ec
Niels Lyngsø <niels.lyngso@gmail.com>
2025-11-21 16:23:04 +0100

    update example

--- src/Umbraco.Web.UI.Client/examples/workspace-context-initial-name/workspace-context.ts
+++ src/Umbraco.Web.UI.Client/examples/workspace-context-initial-name/workspace-context.ts
@@ -1,30 +1,27 @@
-import { UmbContextToken } from '@umbraco-cms/backoffice/context-api';
-import { UmbContextBase } from '@umbraco-cms/backoffice/class-api';
+import { UmbControllerBase } from '@umbraco-cms/backoffice/class-api';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import { UMB_DOCUMENT_WORKSPACE_CONTEXT } from '@umbraco-cms/backoffice/document';
 import { UmbVariantId } from '@umbraco-cms/backoffice/variant';
 
 // The Example Workspace Context Controller:
-export class WorkspaceContextNameManipulation extends UmbContextBase {
+export class ExampleWorkspaceContextNameManipulation extends UmbControllerBase {
 	constructor(host: UmbControllerHost) {
-		super(host, MANIPULATE_NAME_WORKSPACE_CONTEXT);
+		super(host);
+
+		console.log('Hello');
 
 		this.consumeContext(UMB_DOCUMENT_WORKSPACE_CONTEXT, (workspace) => {
 			this.observe(workspace?.loading.isOn, (isLoading) => {
 				// Only manipulate the name when we are not loading:
 				if (isLoading) return;
-				// Get if new:
-				const isNew = workspace?.getIsNew() ?? false;
-				if (isNew) {
-					// Set the name if it's already empty (We do not want to overwrite if it's a Blueprint)
-					// Notice we can put a ! on the workspace, if the Document is new, then we also know we have a workspace.
-					// Notice we need to provide a Variant-ID to getName, as Document names are variant specific. Here we get the Invariant name — this will need to be extended if you are looking to support multiple variants.
-					const variantId = UmbVariantId.CreateInvariant();
-					const name = workspace!.getName(variantId);
-					if (name === undefined) {
-						const manipulatedName = `New Document - ${new Date().toLocaleDateString('en-GB')}`;
-						workspace!.setName(manipulatedName, variantId);
-					}
+				// Set the name if it's already empty (We do not want to overwrite if it's a Blueprint)
+				// Notice we can put a ! on the workspace, if the Document is new, then we also know we have a workspace.
+				// Notice we need to provide a Variant-ID to getName, as Document names are variant specific. Here we get the Invariant name — this will need to be extended if you are looking to support multiple variants.
+				const variantId = UmbVariantId.CreateInvariant();
+				const name = workspace!.getName(variantId);
+				if (name === undefined) {
+					const manipulatedName = `New Document - ${new Date().toLocaleDateString('en-GB')}`;
+					workspace!.setName(manipulatedName, variantId);
 				}
 			});
 		});
@@ -32,10 +29,4 @@ export class WorkspaceContextNameManipulation extends UmbContextBase {
 }
 
 // Declare a api export, so Extension Registry can initialize this class:
-export const api = WorkspaceContextNameManipulation;
-
-// Declare a Context Token that other elements can use to request the WorkspaceContextNameManipulation:
-export const MANIPULATE_NAME_WORKSPACE_CONTEXT = new UmbContextToken<WorkspaceContextNameManipulation>(
-	'UmbWorkspaceContext',
-	'example.workspaceContext.initialName',
-);
+export { ExampleWorkspaceContextNameManipulation as api };
