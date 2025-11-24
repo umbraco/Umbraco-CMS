ae4a216d91055480fc742340bf45e26f99a11db9
engjlr <enl@umbraco.dk>
2025-11-24 10:45:39 +0100

    Fixed import.

--- src/Umbraco.Web.UI.Client/src/packages/core/workspace/conditions/workspace-content-type-unique/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/workspace/conditions/workspace-content-type-unique/manifests.ts
@@ -1,5 +1,5 @@
-import { UMB_WORKSPACE_CONTENT_TYPE_UNIQUE_CONDITION } from './constants';
-import { UmbWorkspaceContentTypeUniqueCondition } from './workspace-content-type-unique.condition';
+import { UMB_WORKSPACE_CONTENT_TYPE_UNIQUE_CONDITION } from './constants.js';
+import { UmbWorkspaceContentTypeUniqueCondition } from './workspace-content-type-unique.condition.js';
 
 export const manifests: Array<UmbExtensionManifest> = [
 	{
