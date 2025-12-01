d4097ae4cf8eec338d82da913e2647a8a5734327
Mads Rasmussen <madsr@hey.com>
2025-12-01 12:40:44 +0100

    utilise ref and card kind for picker views

--- src/Umbraco.Web.UI.Client/src/packages/property-editors/entity-data-picker/picker-collection/views/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/property-editors/entity-data-picker/picker-collection/views/manifests.ts
@@ -6,15 +6,9 @@ import { UMB_COLLECTION_ALIAS_CONDITION } from '@umbraco-cms/backoffice/collecti
 export const manifests: Array<UmbExtensionManifest> = [
 	{
 		type: 'collectionView',
+		kind: 'ref',
 		alias: UMB_ENTITY_DATA_PICKER_REF_COLLECTION_VIEW,
 		name: 'Entity Data Picker Ref Collection View',
-		element: () => import('./ref/entity-data-picker-ref-collection-view.element.js'),
-		weight: 400,
-		meta: {
-			label: 'Refs',
-			icon: 'icon-rows-4',
-			pathName: 'refs',
-		},
 		conditions: [
 			{
 				alias: UMB_COLLECTION_ALIAS_CONDITION,
@@ -24,15 +18,9 @@ export const manifests: Array<UmbExtensionManifest> = [
 	},
 	{
 		type: 'collectionView',
+		kind: 'card',
 		alias: UMB_ENTITY_DATA_PICKER_CARD_COLLECTION_VIEW,
 		name: 'Entity Data Picker Card Collection View',
-		element: () => import('./card/entity-data-picker-card-collection-view.element.js'),
-		weight: 200,
-		meta: {
-			label: 'Cards',
-			icon: 'icon-grid',
-			pathName: 'cards',
-		},
 		conditions: [
 			{
 				alias: UMB_COLLECTION_ALIAS_CONDITION,
