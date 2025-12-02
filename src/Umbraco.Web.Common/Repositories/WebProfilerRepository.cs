06b58b35add30d65e65e6fdbbd2e28acccdfc4dc
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2025-12-02 17:06:43 +0100

    fix: sets profiling cookie to httpOnly and strict in order to run non-secure

--- src/Umbraco.Web.Common/Repositories/WebProfilerRepository.cs
+++ src/Umbraco.Web.Common/Repositories/WebProfilerRepository.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Http;
 using Umbraco.Cms.Core.Persistence.Repositories;
+using Umbraco.Cms.Core.Web;
 using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Web.Common.Repositories;
@@ -11,21 +12,34 @@ internal sealed class WebProfilerRepository : IWebProfilerRepository
     private const string QueryName = "umbDebug";
 
     private readonly IHttpContextAccessor _httpContextAccessor;
+    private readonly ICookieManager _cookieManager;
 
-    public WebProfilerRepository(IHttpContextAccessor httpContextAccessor)
+    public WebProfilerRepository(IHttpContextAccessor httpContextAccessor, ICookieManager cookieManager)
     {
         _httpContextAccessor = httpContextAccessor;
+        _cookieManager = cookieManager;
     }
 
     public void SetStatus(int userId, bool status)
     {
         if (status)
         {
-            _httpContextAccessor.GetRequiredHttpContext().Response.Cookies.Append(CookieName, "1", new CookieOptions { Expires = DateTime.Now.AddYears(1) });
+            // This cookie does not need to be secure as it is only used for local debugging purposes.
+            // It is also marked as SameSite=Strict to avoid being sent in cross-site requests, which means
+            // it will not be sent when the BackOffice is hosted on a different domain/port than the frontend.
+            // The intention is to force debug profiling on without any code snippets, but as it can only be activated from
+            // the local BackOffice, it will unfortunately only work in same-site scenarios.
+            // If the user wants to debug profile cross-site, they can use the query string or header options.
+            _cookieManager.SetCookieValue(
+                CookieName,
+                "1",
+                httpOnly: true,
+                secure: false,
+                sameSiteMode: "Strict");
         }
         else
         {
-            _httpContextAccessor.GetRequiredHttpContext().Response.Cookies.Delete(CookieName);
+            _cookieManager.ExpireCookie(CookieName);
         }
     }
 
@@ -43,6 +57,6 @@ internal sealed class WebProfilerRepository : IWebProfilerRepository
             return xUmbDebug;
         }
 
-        return request.Cookies.ContainsKey(CookieName);
+        return _cookieManager.HasCookie(CookieName);
     }
 }
