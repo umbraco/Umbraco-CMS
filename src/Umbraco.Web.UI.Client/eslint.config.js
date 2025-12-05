6d919e00b33b47f6d128af76d4601f1f5d422a5d
Mads Rasmussen <madsr@hey.com>
2025-12-05 15:10:17 +0100

    Removed duplicate and redundant '@typescript-eslint/no-unused-vars' rule definitions, consolidating the configuration to use only 'argsIgnorePattern'.

--- src/Umbraco.Web.UI.Client/eslint.config.js
+++ src/Umbraco.Web.UI.Client/eslint.config.js
@@ -86,15 +86,9 @@ export default [
 		...importPlugin.flatConfigs.typescript,
 		rules: {
 			'no-unused-vars': 'off', //Let '@typescript-eslint/no-unused-vars' catch the errors to allow unused function parameters (ex: in interfaces)
-			'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_', varsIgnorePattern: '^_' }],
+			'@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
 			'@typescript-eslint/no-non-null-assertion': 'off',
 			'@typescript-eslint/no-explicit-any': 'warn',
-			'@typescript-eslint/no-unused-vars': [
-				'error',
-				{
-					argsIgnorePattern: '^_',
-				},
-			],
 			'@typescript-eslint/consistent-type-exports': 'error',
 			'@typescript-eslint/consistent-type-imports': 'error',
 			'@typescript-eslint/no-import-type-side-effects': 'warn',
