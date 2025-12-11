013e1a225ecd6479891b1a5f8bd726a914cde795
leekelleher <leekelleher@gmail.com>
2025-12-11 10:26:34 +0000

    Register "Clipboard Property Context" for Tiptap RTE
    we can't make this generic for all RTEs,
    since it is bound to the property-editor UI alias.

---
+++ src/Umbraco.Web.UI.Client/src/packages/tiptap/clipboard/manifest.ts
@@ -0,0 +1,7 @@
+export const manifest: UmbExtensionManifest = {
+	type: 'propertyContext',
+	kind: 'clipboard',
+	alias: 'Umb.PropertyContext.Tiptap.Clipboard',
+	name: 'Rich Text Editor [Tiptap] Clipboard Property Context',
+	forPropertyEditorUis: ['Umb.PropertyEditorUi.Tiptap'], // TODO: [LK] Move to a constants file.
+};
