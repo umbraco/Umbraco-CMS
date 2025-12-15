08ee542085bfcafe26fd23c7f05a4a34d036aad7
Nhu Dinh <hnd@umbraco.dk>
2025-12-15 11:37:03 +0700

    Updated variables name

--- tests/Umbraco.Tests.AcceptanceTest/tests/ContentSettingConfig/AllowEditInvariantFromNonDefaultIsTrue.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/ContentSettingConfig/AllowEditInvariantFromNonDefaultIsTrue.spec.ts
@@ -18,14 +18,14 @@ const cultures = [
   {isoCode: secondCulture, name: danishContentName},
 ];
 // Block List
-const blockList1Name = 'BlockList1';
-const blockList2Name = 'BlockList2';
+const firstBlockListName = 'FirstBlockList';
+const secondBlockListName = 'SecondBlockList';
 // Element Type
-const block1ElementTypeName = 'Block1ElementType';
-const block2ElementTypeName = 'Block2ElementType';
+const firstBlockElementTypeName = 'FirstBlockElementType';
+const secondBlockElementTypeName = 'SecondBlockElementType';
 // Block Properties
-const text1Name = 'Text1';
-const text2Name = 'Text2';
+const firstTextName = 'FirstText';
+const secondTextName = 'SecondText';
 
 test.beforeEach(async ({umbracoApi}) => {
   await umbracoApi.documentType.ensureNameNotExists(documentTypeName);
@@ -36,10 +36,10 @@ test.beforeEach(async ({umbracoApi}) => {
 test.afterEach(async ({umbracoApi}) => {
   await umbracoApi.document.ensureNameNotExists(englishContentName);
   await umbracoApi.documentType.ensureNameNotExists(documentTypeName);
-  await umbracoApi.documentType.ensureNameNotExists(block1ElementTypeName);
-  await umbracoApi.documentType.ensureNameNotExists(block2ElementTypeName);
-  await umbracoApi.dataType.ensureNameNotExists(blockList1Name);
-  await umbracoApi.dataType.ensureNameNotExists(blockList2Name);
+  await umbracoApi.documentType.ensureNameNotExists(firstBlockElementTypeName);
+  await umbracoApi.documentType.ensureNameNotExists(secondBlockElementTypeName);
+  await umbracoApi.dataType.ensureNameNotExists(firstBlockListName);
+  await umbracoApi.dataType.ensureNameNotExists(secondBlockListName);
   await umbracoApi.language.ensureIsoCodeNotExists(secondCulture);
 });
 
@@ -47,16 +47,16 @@ test('can edit variant text property in non-default language when AllowEditInvar
   // Arrange
   const documentTypeId = await umbracoApi.documentType.createDocumentTypeWithVariantAndInvariantBlockLists(
     documentTypeName,
-    text1Name,
+    firstTextName,
     textStringDataType.id,
-    text2Name,
+    secondTextName,
     textStringDataType.id,
-    blockList1Name,
-    blockList2Name,
-    block1ElementTypeName,
-    block2ElementTypeName
+    firstBlockListName,
+    secondBlockListName,
+    firstBlockElementTypeName,
+    secondBlockElementTypeName
   );
-  await umbracoApi.document.createDocumentWithMultipleVariantsWithSharedProperty(englishContentName, documentTypeId, AliasHelper.toAlias(text2Name), textStringEditorAlias, cultures, '');
+  await umbracoApi.document.createDocumentWithMultipleVariantsWithSharedProperty(englishContentName, documentTypeId, AliasHelper.toAlias(secondTextName), textStringEditorAlias, cultures, '');
   await umbracoUi.goToBackOffice();
   await umbracoUi.content.goToSection(ConstantHelper.sections.content);
 
@@ -68,23 +68,23 @@ test('can edit variant text property in non-default language when AllowEditInvar
   await umbracoUi.content.switchLanguage(secondCulture);
 
   // Assert
-  await umbracoUi.content.isDocumentPropertyEditable(text1Name, true);
+  await umbracoUi.content.isDocumentPropertyEditable(firstTextName, true);
 });
 
 test('can edit invariant text property in non-default language when AllowEditInvariantFromNonDefault is true', async ({umbracoApi, umbracoUi}) => {
   // Arrange
   const documentTypeId = await umbracoApi.documentType.createDocumentTypeWithVariantAndInvariantBlockLists(
     documentTypeName,
-    text1Name,
+    firstTextName,
     textStringDataType.id,
-    text2Name,
+    secondTextName,
     textStringDataType.id,
-    blockList1Name,
-    blockList2Name,
-    block1ElementTypeName,
-    block2ElementTypeName
+    firstBlockListName,
+    secondBlockListName,
+    firstBlockElementTypeName,
+    secondBlockElementTypeName
   );
-  await umbracoApi.document.createDocumentWithMultipleVariantsWithSharedProperty(englishContentName, documentTypeId, AliasHelper.toAlias(text2Name), textStringEditorAlias, cultures, '');
+  await umbracoApi.document.createDocumentWithMultipleVariantsWithSharedProperty(englishContentName, documentTypeId, AliasHelper.toAlias(secondTextName), textStringEditorAlias, cultures, '');
   await umbracoUi.goToBackOffice();
   await umbracoUi.content.goToSection(ConstantHelper.sections.content);
 
@@ -96,23 +96,23 @@ test('can edit invariant text property in non-default language when AllowEditInv
   await umbracoUi.content.switchLanguage(secondCulture);
 
   // Assert
-  await umbracoUi.content.isDocumentPropertyEditable(text2Name, true);
+  await umbracoUi.content.isDocumentPropertyEditable(secondTextName, true);
 });
 
 test('can edit variant block list in non-default language when AllowEditInvariantFromNonDefault is true', async ({umbracoApi, umbracoUi}) => {
   // Arrange
   const documentTypeId = await umbracoApi.documentType.createDocumentTypeWithVariantAndInvariantBlockLists(
     documentTypeName,
-    text1Name,
+    firstTextName,
     textStringDataType.id,
-    text2Name,
+    secondTextName,
     textStringDataType.id,
-    blockList1Name,
-    blockList2Name,
-    block1ElementTypeName,
-    block2ElementTypeName
+    firstBlockListName,
+    secondBlockListName,
+    firstBlockElementTypeName,
+    secondBlockElementTypeName
   );
-  await umbracoApi.document.createDocumentWithMultipleVariantsWithSharedProperty(englishContentName, documentTypeId, AliasHelper.toAlias(text2Name), textStringEditorAlias, cultures, '');
+  await umbracoApi.document.createDocumentWithMultipleVariantsWithSharedProperty(englishContentName, documentTypeId, AliasHelper.toAlias(secondTextName), textStringEditorAlias, cultures, '');
   await umbracoUi.goToBackOffice();
   await umbracoUi.content.goToSection(ConstantHelper.sections.content);
 
@@ -124,23 +124,23 @@ test('can edit variant block list in non-default language when AllowEditInvarian
   await umbracoUi.content.switchLanguage(secondCulture);
 
   // Assert
-  await umbracoUi.content.isAddBlockListElementWithNameVisible(blockList1Name);
+  await umbracoUi.content.isAddBlockListElementWithNameVisible(firstBlockListName);
 });
 
 test('can edit invariant block list in non-default language when AllowEditInvariantFromNonDefault is true', async ({umbracoApi, umbracoUi}) => {
   // Arrange
   const documentTypeId = await umbracoApi.documentType.createDocumentTypeWithVariantAndInvariantBlockLists(
     documentTypeName,
-    text1Name,
+    firstTextName,
     textStringDataType.id,
-    text2Name,
+    secondTextName,
     textStringDataType.id,
-    blockList1Name,
-    blockList2Name,
-    block1ElementTypeName,
-    block2ElementTypeName
+    firstBlockListName,
+    secondBlockListName,
+    firstBlockElementTypeName,
+    secondBlockElementTypeName
   );
-  await umbracoApi.document.createDocumentWithMultipleVariantsWithSharedProperty(englishContentName, documentTypeId, AliasHelper.toAlias(text2Name), textStringEditorAlias, cultures, '');
+  await umbracoApi.document.createDocumentWithMultipleVariantsWithSharedProperty(englishContentName, documentTypeId, AliasHelper.toAlias(secondTextName), textStringEditorAlias, cultures, '');
   await umbracoUi.goToBackOffice();
   await umbracoUi.content.goToSection(ConstantHelper.sections.content);
 
@@ -152,145 +152,145 @@ test('can edit invariant block list in non-default language when AllowEditInvari
   await umbracoUi.content.switchLanguage(secondCulture);
 
   // Assert
-  await umbracoUi.content.isAddBlockListElementWithNameVisible(blockList2Name);
+  await umbracoUi.content.isAddBlockListElementWithNameVisible(secondBlockListName);
 });
 
 test('can edit variant text property inside a variant block in non-default language when AllowEditInvariantFromNonDefault is true', async ({umbracoApi, umbracoUi}) => {
   // Arrange
   const documentTypeId = await umbracoApi.documentType.createDocumentTypeWithVariantAndInvariantBlockLists(
     documentTypeName,
-    text1Name,
+    firstTextName,
     textStringDataType.id,
-    text2Name,
+    secondTextName,
     textStringDataType.id,
-    blockList1Name,
-    blockList2Name,
-    block1ElementTypeName,
-    block2ElementTypeName
+    firstBlockListName,
+    secondBlockListName,
+    firstBlockElementTypeName,
+    secondBlockElementTypeName
   );
-  await umbracoApi.document.createDocumentWithMultipleVariantsWithSharedProperty(englishContentName, documentTypeId, AliasHelper.toAlias(text2Name), textStringEditorAlias, cultures, '');
+  await umbracoApi.document.createDocumentWithMultipleVariantsWithSharedProperty(englishContentName, documentTypeId, AliasHelper.toAlias(secondTextName), textStringEditorAlias, cultures, '');
   await umbracoUi.goToBackOffice();
   await umbracoUi.content.goToSection(ConstantHelper.sections.content);
 
   // Act
   await umbracoUi.content.goToContentWithName(englishContentName);
-  await umbracoUi.content.clickAddBlockListElementWithName(blockList1Name);
-  await umbracoUi.content.clickBlockElementWithName(block1ElementTypeName);
-  await umbracoUi.content.enterBlockPropertyValue(text1Name, 'Variant text in block');
-  await umbracoUi.content.enterBlockPropertyValue(text2Name, 'Invariant text in block');
+  await umbracoUi.content.clickAddBlockListElementWithName(firstBlockListName);
+  await umbracoUi.content.clickBlockElementWithName(firstBlockElementTypeName);
+  await umbracoUi.content.enterBlockPropertyValue(firstTextName, 'Variant text in block');
+  await umbracoUi.content.enterBlockPropertyValue(secondTextName, 'Invariant text in block');
   await umbracoUi.content.clickCreateModalButton();
   await umbracoUi.content.clickSaveAndPublishButton();
   await umbracoUi.content.clickContainerSaveAndPublishButton();
   await umbracoUi.content.isSuccessNotificationVisible();
   await umbracoUi.content.switchLanguage(secondCulture);
-  await umbracoUi.content.clickAddBlockListElementWithName(blockList1Name);
-  await umbracoUi.content.clickBlockElementWithName(block1ElementTypeName);
+  await umbracoUi.content.clickAddBlockListElementWithName(firstBlockListName);
+  await umbracoUi.content.clickBlockElementWithName(firstBlockElementTypeName);
 
   // Assert
-  await umbracoUi.content.isBlockPropertyEditable(text1Name, true);
+  await umbracoUi.content.isBlockPropertyEditable(firstTextName, true);
 });
 
 test('can edit invariant text property inside a variant block in non-default language when AllowEditInvariantFromNonDefault is true', async ({umbracoApi, umbracoUi}) => {
   // Arrange
   const documentTypeId = await umbracoApi.documentType.createDocumentTypeWithVariantAndInvariantBlockLists(
     documentTypeName,
-    text1Name,
+    firstTextName,
     textStringDataType.id,
-    text2Name,
+    secondTextName,
     textStringDataType.id,
-    blockList1Name,
-    blockList2Name,
-    block1ElementTypeName,
-    block2ElementTypeName
+    firstBlockListName,
+    secondBlockListName,
+    firstBlockElementTypeName,
+    secondBlockElementTypeName
   );
-  await umbracoApi.document.createDocumentWithMultipleVariantsWithSharedProperty(englishContentName, documentTypeId, AliasHelper.toAlias(text2Name), textStringEditorAlias, cultures, '');
+  await umbracoApi.document.createDocumentWithMultipleVariantsWithSharedProperty(englishContentName, documentTypeId, AliasHelper.toAlias(secondTextName), textStringEditorAlias, cultures, '');
   await umbracoUi.goToBackOffice();
   await umbracoUi.content.goToSection(ConstantHelper.sections.content);
 
   // Act
   await umbracoUi.content.goToContentWithName(englishContentName);
-  await umbracoUi.content.clickAddBlockListElementWithName(blockList1Name);
-  await umbracoUi.content.clickBlockElementWithName(block1ElementTypeName);
-  await umbracoUi.content.enterBlockPropertyValue(text1Name, 'Variant text in block');
-  await umbracoUi.content.enterBlockPropertyValue(text2Name, 'Invariant text in block');
+  await umbracoUi.content.clickAddBlockListElementWithName(firstBlockListName);
+  await umbracoUi.content.clickBlockElementWithName(firstBlockElementTypeName);
+  await umbracoUi.content.enterBlockPropertyValue(firstTextName, 'Variant text in block');
+  await umbracoUi.content.enterBlockPropertyValue(secondTextName, 'Invariant text in block');
   await umbracoUi.content.clickCreateModalButton();
   await umbracoUi.content.clickSaveAndPublishButton();
   await umbracoUi.content.clickContainerSaveAndPublishButton();
   await umbracoUi.content.isSuccessNotificationVisible();
   await umbracoUi.content.switchLanguage(secondCulture);
-  await umbracoUi.content.clickAddBlockListElementWithName(blockList1Name);
-  await umbracoUi.content.clickBlockElementWithName(block1ElementTypeName);
+  await umbracoUi.content.clickAddBlockListElementWithName(firstBlockListName);
+  await umbracoUi.content.clickBlockElementWithName(firstBlockElementTypeName);
 
   // Assert
-  await umbracoUi.content.isBlockPropertyEditable(text2Name, true);
+  await umbracoUi.content.isBlockPropertyEditable(secondTextName, true);
 });
 
 test('can edit variant text property inside invariant block in non-default language when AllowEditInvariantFromNonDefault is true', async ({umbracoApi, umbracoUi}) => {
   // Arrange
   const documentTypeId = await umbracoApi.documentType.createDocumentTypeWithVariantAndInvariantBlockLists(
     documentTypeName,
-    text1Name,
+    firstTextName,
     textStringDataType.id,
-    text2Name,
+    secondTextName,
     textStringDataType.id,
-    blockList1Name,
-    blockList2Name,
-    block1ElementTypeName,
-    block2ElementTypeName
+    firstBlockListName,
+    secondBlockListName,
+    firstBlockElementTypeName,
+    secondBlockElementTypeName
   );
-  await umbracoApi.document.createDocumentWithMultipleVariantsWithSharedProperty(englishContentName, documentTypeId, AliasHelper.toAlias(text2Name), textStringEditorAlias, cultures, '');
+  await umbracoApi.document.createDocumentWithMultipleVariantsWithSharedProperty(englishContentName, documentTypeId, AliasHelper.toAlias(secondTextName), textStringEditorAlias, cultures, '');
   await umbracoUi.goToBackOffice();
   await umbracoUi.content.goToSection(ConstantHelper.sections.content);
 
   // Act
   await umbracoUi.content.goToContentWithName(englishContentName);
-  await umbracoUi.content.clickAddBlockListElementWithName(blockList1Name);
-  await umbracoUi.content.clickBlockElementWithName(block2ElementTypeName);
-  await umbracoUi.content.enterBlockPropertyValue(text1Name, 'Text1 in invariant block');
-  await umbracoUi.content.enterBlockPropertyValue(text2Name, 'Text2 in invariant block');
+  await umbracoUi.content.clickAddBlockListElementWithName(firstBlockListName);
+  await umbracoUi.content.clickBlockElementWithName(secondBlockElementTypeName);
+  await umbracoUi.content.enterBlockPropertyValue(firstTextName, 'Text1 in invariant block');
+  await umbracoUi.content.enterBlockPropertyValue(secondTextName, 'Text2 in invariant block');
   await umbracoUi.content.clickCreateModalButton();
   await umbracoUi.content.clickSaveAndPublishButton();
   await umbracoUi.content.clickContainerSaveAndPublishButton();
   await umbracoUi.content.isSuccessNotificationVisible();
   await umbracoUi.content.switchLanguage(secondCulture);
-  await umbracoUi.content.clickAddBlockListElementWithName(blockList1Name);
-  await umbracoUi.content.clickBlockElementWithName(block2ElementTypeName);
+  await umbracoUi.content.clickAddBlockListElementWithName(firstBlockListName);
+  await umbracoUi.content.clickBlockElementWithName(secondBlockElementTypeName);
 
   // Assert
-  await umbracoUi.content.isBlockPropertyEditable(text1Name, true);
+  await umbracoUi.content.isBlockPropertyEditable(firstTextName, true);
 });
 
 test('can edit invariant text property inside an invariant block in non-default language when AllowEditInvariantFromNonDefault is true', async ({umbracoApi, umbracoUi}) => {
   // Arrange
   const documentTypeId = await umbracoApi.documentType.createDocumentTypeWithVariantAndInvariantBlockLists(
     documentTypeName,
-    text1Name,
+    firstTextName,
     textStringDataType.id,
-    text2Name,
+    secondTextName,
     textStringDataType.id,
-    blockList1Name,
-    blockList2Name,
-    block1ElementTypeName,
-    block2ElementTypeName
+    firstBlockListName,
+    secondBlockListName,
+    firstBlockElementTypeName,
+    secondBlockElementTypeName
   );
-  await umbracoApi.document.createDocumentWithMultipleVariantsWithSharedProperty(englishContentName, documentTypeId, AliasHelper.toAlias(text2Name), textStringEditorAlias, cultures, '');
+  await umbracoApi.document.createDocumentWithMultipleVariantsWithSharedProperty(englishContentName, documentTypeId, AliasHelper.toAlias(secondTextName), textStringEditorAlias, cultures, '');
   await umbracoUi.goToBackOffice();
   await umbracoUi.content.goToSection(ConstantHelper.sections.content);
 
   // Act
   await umbracoUi.content.goToContentWithName(englishContentName);
-  await umbracoUi.content.clickAddBlockListElementWithName(blockList1Name);
-  await umbracoUi.content.clickBlockElementWithName(block2ElementTypeName);
-  await umbracoUi.content.enterBlockPropertyValue(text1Name, 'Text1 in invariant block');
-  await umbracoUi.content.enterBlockPropertyValue(text2Name, 'Text2 in invariant block');
+  await umbracoUi.content.clickAddBlockListElementWithName(firstBlockListName);
+  await umbracoUi.content.clickBlockElementWithName(secondBlockElementTypeName);
+  await umbracoUi.content.enterBlockPropertyValue(firstTextName, 'Text1 in invariant block');
+  await umbracoUi.content.enterBlockPropertyValue(secondTextName, 'Text2 in invariant block');
   await umbracoUi.content.clickCreateModalButton();
   await umbracoUi.content.clickSaveAndPublishButton();
   await umbracoUi.content.clickContainerSaveAndPublishButton();
   await umbracoUi.content.isSuccessNotificationVisible();
   await umbracoUi.content.switchLanguage(secondCulture);
-  await umbracoUi.content.clickAddBlockListElementWithName(blockList1Name);
-  await umbracoUi.content.clickBlockElementWithName(block2ElementTypeName);
+  await umbracoUi.content.clickAddBlockListElementWithName(firstBlockListName);
+  await umbracoUi.content.clickBlockElementWithName(secondBlockElementTypeName);
 
   // Assert
-  await umbracoUi.content.isBlockPropertyEditable(text2Name, true);
+  await umbracoUi.content.isBlockPropertyEditable(secondTextName, true);
 });
