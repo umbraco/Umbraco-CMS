0644587c9ed95cad5cf93fdcd4e0eb6fef0236cc
Lee Kelleher <leekelleher@users.noreply.github.com>
2025-12-15 10:22:11 +0000

    Block List: Sort mode (#21060)
    * Block List: added sort-mode
    
    * Fix missing closing bracket
    
    * register sort mode toolbar element on start up
    
    ---------
    
    Co-authored-by: Mads Rasmussen <madsr@hey.com>
    # Conflicts:
    #	src/Umbraco.Web.UI.Client/src/packages/block/block-list/property-editors/block-list-editor/property-editor-ui-block-list.element.ts

--- src/Umbraco.Web.UI.Client/src/packages/block/block-list/property-editors/block-list-editor/property-editor-ui-block-list.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/block/block-list/property-editors/block-list-editor/property-editor-ui-block-list.element.ts
@@ -3,32 +3,31 @@ import { UmbBlockListEntriesContext } from '../../context/block-list-entries.con
 import type { UmbBlockListLayoutModel, UmbBlockListValueModel } from '../../types.js';
 import type { UmbBlockListEntryElement } from '../../components/block-list-entry/index.js';
 import { UMB_BLOCK_LIST_PROPERTY_EDITOR_SCHEMA_ALIAS } from './constants.js';
-import { UmbLitElement, umbDestroyOnDisconnect } from '@umbraco-cms/backoffice/lit-element';
-import { html, customElement, property, state, repeat, css, nothing } from '@umbraco-cms/backoffice/external/lit';
-import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
+import { css, customElement, html, nothing, property, repeat, state } from '@umbraco-cms/backoffice/external/lit';
+import { debounceTime } from '@umbraco-cms/backoffice/external/rxjs';
+import {
+	extractJsonQueryProps,
+	UmbFormControlMixin,
+	UmbValidationContext,
+	UMB_VALIDATION_EMPTY_LOCALIZATION_KEY,
+} from '@umbraco-cms/backoffice/validation';
+import { jsonStringComparison, observeMultiple } from '@umbraco-cms/backoffice/observable-api';
+import { umbDestroyOnDisconnect, UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
+import { UmbSorterController } from '@umbraco-cms/backoffice/sorter';
+import { UMB_PROPERTY_CONTEXT } from '@umbraco-cms/backoffice/property';
+import { UMB_CONTENT_WORKSPACE_CONTEXT } from '@umbraco-cms/backoffice/content';
+import type { UmbBlockLayoutBaseModel } from '@umbraco-cms/backoffice/block';
+import type { UmbBlockTypeBaseModel } from '@umbraco-cms/backoffice/block-type';
+import type { UmbModalRouteBuilder } from '@umbraco-cms/backoffice/router';
+import type { UmbNumberRangeValueType } from '@umbraco-cms/backoffice/models';
 import type {
 	UmbPropertyEditorConfigCollection,
 	UmbPropertyEditorUiElement,
 } from '@umbraco-cms/backoffice/property-editor';
-import type { UmbNumberRangeValueType } from '@umbraco-cms/backoffice/models';
-import type { UmbModalRouteBuilder } from '@umbraco-cms/backoffice/router';
 import type { UmbSorterConfig } from '@umbraco-cms/backoffice/sorter';
-import { UmbSorterController } from '@umbraco-cms/backoffice/sorter';
-import type { UmbBlockLayoutBaseModel } from '@umbraco-cms/backoffice/block';
-import type { UmbBlockTypeBaseModel } from '@umbraco-cms/backoffice/block-type';
+import { UMB_VARIANT_CONTEXT } from '@umbraco-cms/backoffice/variant';
 
 import '../../components/block-list-entry/index.js';
-import { UMB_PROPERTY_CONTEXT } from '@umbraco-cms/backoffice/property';
-import {
-	extractJsonQueryProps,
-	UMB_VALIDATION_EMPTY_LOCALIZATION_KEY,
-	UmbFormControlMixin,
-	UmbValidationContext,
-} from '@umbraco-cms/backoffice/validation';
-import { jsonStringComparison, observeMultiple } from '@umbraco-cms/backoffice/observable-api';
-import { debounceTime } from '@umbraco-cms/backoffice/external/rxjs';
-import { UMB_CONTENT_WORKSPACE_CONTEXT } from '@umbraco-cms/backoffice/content';
-import { UMB_VARIANT_CONTEXT } from '@umbraco-cms/backoffice/variant';
 
 const SORTER_CONFIG: UmbSorterConfig<UmbBlockListLayoutModel, UmbBlockListEntryElement> = {
 	getUniqueOfElement: (element) => {
@@ -163,6 +162,9 @@ export class UmbPropertyEditorUIBlockListElement
 	@state()
 	private _notSupportedVariantSetting?: boolean;
 
+	@state()
+	private _isSortMode = false;
+
 	constructor() {
 		super();
 
@@ -256,6 +258,8 @@ export class UmbPropertyEditorUIBlockListElement
 				'observeContextualVariantId',
 			);
 		});
+
+		this.observe(this.#managerContext.isSortMode, (isSortMode) => (this._isSortMode = isSortMode ?? false));
 
 		this.addValidator(
 			'rangeUnderflow',
@@ -379,10 +383,9 @@ export class UmbPropertyEditorUIBlockListElement
 	}
 
 	override render() {
-		if (this._notSupportedVariantSetting) {
-			return nothing;
-		}
+		if (this._notSupportedVariantSetting) return nothing;
 		return html`
+			${this.#renderSortModeToolbar()}
 			${repeat(
 				this._layouts,
 				(layout, index) => `${index}_${layout.contentKey}`,
@@ -407,9 +410,12 @@ export class UmbPropertyEditorUIBlockListElement
 
 	#renderInlineCreateButton(index: number) {
 		if (this.readonly) return nothing;
-		return html`<uui-button-inline-create
-			label=${this._createButtonLabel}
-			href=${this._catalogueRouteBuilder?.({ view: 'create', index: index }) ?? ''}></uui-button-inline-create>`;
+		return html`
+			<uui-button-inline-create
+				label=${this._createButtonLabel}
+				href=${this._catalogueRouteBuilder?.({ view: 'create', index: index }) ?? ''}>
+			</uui-button-inline-create>
+		`;
 	}
 
 	#renderCreateButton() {
@@ -442,9 +448,12 @@ export class UmbPropertyEditorUIBlockListElement
 		`;
 	}
 
-	static override readonly styles = [
-		UmbTextStyles,
+	#renderSortModeToolbar() {
+		if (!this._isSortMode) return nothing;
+		return html`<umb-property-sort-mode-toolbar></umb-property-sort-mode-toolbar>`;
+	}
 
+	static override readonly styles = [
 		css`
 			:host {
 				display: grid;
