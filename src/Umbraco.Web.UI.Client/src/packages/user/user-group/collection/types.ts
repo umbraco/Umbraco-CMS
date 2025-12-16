d60459e1a75a2a65f6a98efd4f41ed1ddfc2a0d7
Mads Rasmussen <madsr@hey.com>
2025-12-16 18:02:26 +0100

    Update types.ts

--- src/Umbraco.Web.UI.Client/src/packages/user/user-group/collection/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user-group/collection/types.ts
@@ -1,6 +1,6 @@
 import type { UmbCollectionFilterModel } from '@umbraco-cms/backoffice/collection';
 
 export interface UmbUserGroupCollectionFilterModel extends UmbCollectionFilterModel {
-	// @deprecated Use 'filter' property instead. Will be removed in version 19.0.0
+	/** @deprecated Use 'filter' property instead. Will be removed in version 19.0.0 */
 	query?: string;
 }
