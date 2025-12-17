294b231e9f581d1677feeffefa53df6b61d9f579
engjlr <enl@umbraco.dk>
2025-12-17 14:37:32 +0100

    Export condition.

--- src/Umbraco.Web.UI.Client/src/packages/data-type/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/data-type/index.ts
@@ -5,6 +5,7 @@ export * from './components/index.js';
 export * from './constants.js';
 export * from './repository/index.js';
 export * from './modals/constants.js';
+export * from './entity-actions/conditions/allow-delete/constants.js';
 
 export { UmbMoveDataTypeRepository, UMB_MOVE_DATA_TYPE_REPOSITORY_ALIAS } from './entity-actions/move-to/index.js';
 
