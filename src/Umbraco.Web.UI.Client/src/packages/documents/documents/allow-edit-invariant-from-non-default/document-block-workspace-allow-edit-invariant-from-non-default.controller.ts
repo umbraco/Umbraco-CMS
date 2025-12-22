b7b04e97699fd0dd93ff6a66df279d7e08d3b577
Niels Lyngsø <niels.lyngso@gmail.com>
2025-12-22 11:47:27 +0100

    clean up and comments

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/allow-edit-invariant-from-non-default/document-block-workspace-allow-edit-invariant-from-non-default.controller.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/allow-edit-invariant-from-non-default/document-block-workspace-allow-edit-invariant-from-non-default.controller.ts
@@ -9,11 +9,11 @@ import { observeMultiple, type Observable } from '@umbraco-cms/backoffice/observ
 
 export class UmbDocumentBlockWorkspaceAllowEditInvariantFromNonDefaultController extends UmbDocumentAllowEditInvariantFromNonDefaultControllerBase {
 	protected async _preventEditInvariantFromNonDefault() {
-		const varyingPropertyContext = await this.getContext(UMB_PROPERTY_CONTEXT_FOR_CULTURE_VARIANT, {
+		//
+		const varyingProperty = await this.getContext(UMB_PROPERTY_CONTEXT_FOR_CULTURE_VARIANT, {
 			passContextAliasMatches: true,
 		}).catch(() => undefined);
-
-		if (varyingPropertyContext) {
+		if (varyingProperty) {
 			// If we have a varying property context, we can assume we are in a culture variant branch and therefore we can assume the property is allowed to be edited.
 			return;
 		}
@@ -26,59 +26,65 @@ export class UmbDocumentBlockWorkspaceAllowEditInvariantFromNonDefaultController
 			throw new Error('Missing Document Workspace Context');
 		}
 
-		const blockWorkspaceContext = await this.getContext(UMB_BLOCK_WORKSPACE_CONTEXT);
-
-		// Note, what if its two levels away?
-		if (!blockWorkspaceContext) {
+		const blockWorkspace = await this.getContext(UMB_BLOCK_WORKSPACE_CONTEXT);
+		if (!blockWorkspace) {
 			throw new Error('Missing Block Workspace Context');
 		}
 
-		const blockManager = await this.getContext(UMB_BLOCK_MANAGER_CONTEXT).catch(() => undefined);
+		// --- Rule for invariant fields in variant blocks edited via a non-default variant ---
 
 		// Existing rules for variant blocks (where datasetVariantId matches the viewing language)
 		this._observeAndApplyRule({
-			propertiesObservable: blockWorkspaceContext.content.structure.contentTypeProperties,
+			propertiesObservable: blockWorkspace.content.structure.contentTypeProperties,
 			variantOptionsObservable: documentWorkspaceContext.variantOptions,
-			propertyWriteGuard: blockWorkspaceContext.content.propertyWriteGuard,
+			propertyWriteGuard: blockWorkspace.content.propertyWriteGuard,
 		});
 		this._observeAndApplyRule({
-			propertiesObservable: blockWorkspaceContext.settings.structure.contentTypeProperties,
+			propertiesObservable: blockWorkspace.settings.structure.contentTypeProperties,
 			variantOptionsObservable: documentWorkspaceContext.variantOptions,
-			propertyWriteGuard: blockWorkspaceContext.settings.propertyWriteGuard,
+			propertyWriteGuard: blockWorkspace.settings.propertyWriteGuard,
 		});
+
+		// --- Rule for everything inside invariant blocks viewed from a non-default language ---
+
+		/** Note because this rule only applies in invariant contexts, then we can use the nearest Block Manager to see what the active variant is. In a data-branch of a mix between varying and not-varying data this would not work [NL] */
+		const blockManager = await this.getContext(UMB_BLOCK_MANAGER_CONTEXT);
+
+		if (!blockManager) {
+			throw new Error('Missing Block Manager Context');
+		}
 
 		// Additional rules for invariant blocks (where datasetVariantId is invariant)
 		// These blocks don't vary by culture, so their datasetVariantId is always invariant,
 		// but we still need to prevent editing when viewing from a non-default language.
-		if (blockManager) {
-			this.#applyRuleForInvariantBlocks(
-				blockManager.variantId,
-				blockWorkspaceContext.content.structure.variesByCulture,
-				documentWorkspaceContext.variantOptions,
-				blockWorkspaceContext.content.propertyWriteGuard,
-			);
-			this.#applyRuleForInvariantBlocks(
-				blockManager.variantId,
-				blockWorkspaceContext.settings.structure.variesByCulture,
-				documentWorkspaceContext.variantOptions,
-				blockWorkspaceContext.settings.propertyWriteGuard,
-			);
-		}
+		this.#applyRuleForInvariantBlocks(
+			blockManager.variantId,
+			blockWorkspace.content.structure.variesByCulture,
+			documentWorkspaceContext.variantOptions,
+			blockWorkspace.content.propertyWriteGuard,
+		);
+		this.#applyRuleForInvariantBlocks(
+			blockManager.variantId,
+			blockWorkspace.settings.structure.variesByCulture,
+			documentWorkspaceContext.variantOptions,
+			blockWorkspace.settings.propertyWriteGuard,
+		);
 	}
 
+	// eslint-disable-next-line jsdoc/require-param
 	/**
 	 * For invariant element types, the block's datasetVariantId is always invariant (null culture).
 	 * The existing rules target non-default language datasetVariantIds (e.g., da-DK), which don't match.
 	 * This method adds a rule with invariant datasetVariantId when viewing from a non-default language.
 	 */
 	#applyRuleForInvariantBlocks(
-		managerVariantId$: Observable<UmbVariantId | undefined>,
-		variesByCulture$: Observable<boolean | undefined>,
-		variantOptions$: Observable<UmbEntityVariantOptionModel<UmbDocumentVariantModel>[]>,
+		managerVariantId: Observable<UmbVariantId | undefined>,
+		variesByCulture: Observable<boolean | undefined>,
+		variantOptions: Observable<UmbEntityVariantOptionModel<UmbDocumentVariantModel>[]>,
 		propertyWriteGuard: UmbVariantPropertyGuardManager,
 	) {
 		this.observe(
-			observeMultiple([managerVariantId$, variesByCulture$, variantOptions$]),
+			observeMultiple([managerVariantId, variesByCulture, variantOptions]),
 			([managerVariantId, variesByCulture, variantOptions]) => {
 				// Only apply for invariant element types (blocks that don't vary by culture)
 				if (variesByCulture !== false) return;
