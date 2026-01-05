2144cc20167fef1e08fdcfa289f935b7596229ad
engjlr <enl@umbraco.dk>
2026-01-05 10:01:18 +0100

    Fixed linting error.

--- src/Umbraco.Web.UI.Client/src/packages/core/workspace/conditions/workspace-is-loaded/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/workspace/conditions/workspace-is-loaded/types.ts
@@ -1,6 +1,7 @@
 import type { UMB_CONTENT_WORKSPACE_IS_LOADED_CONDITION_ALIAS } from './constants.js';
 import type { UmbConditionConfigBase } from '@umbraco-cms/backoffice/extension-api';
 
+// eslint-disable-next-line @typescript-eslint/no-empty-object-type
 export interface UmbContentWorkspaceIsLoadedConditionConfig
 	extends UmbConditionConfigBase<typeof UMB_CONTENT_WORKSPACE_IS_LOADED_CONDITION_ALIAS> {}
 
