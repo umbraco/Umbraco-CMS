c74c38d6e093e36a750140a18c1a9cde4ade66b2
Andy Butland <abutland73@gmail.com>
2026-01-05 15:59:52 +0100

    Apply suggestion from code review.

--- tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/HealthChecks/MarkdownToHtmlConverterTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/HealthChecks/MarkdownToHtmlConverterTests.cs
@@ -11,57 +11,6 @@ namespace Umbraco.Cms.Tests.UnitTests.Umbraco.Infrastructure.HealthChecks;
 [TestFixture]
 public class MarkdownToHtmlConverterTests
 {
-    [HealthCheck("CFD6FC34-59C9-4402-B55F-C8BC96B628A1", "Stub check")]
-    public abstract class StubHealthCheck : HealthCheck
-    {
-        private readonly string _message;
-        private readonly StatusResultType _resultType;
-
-        protected StubHealthCheck(StatusResultType resultType, string message)
-        {
-            _resultType = resultType;
-            _message = message;
-        }
-
-        public override HealthCheckStatus ExecuteAction(HealthCheckAction action) =>
-            throw new NotImplementedException();
-
-        public override Task<IEnumerable<HealthCheckStatus>> GetStatusAsync() =>
-            Task.FromResult<IEnumerable<HealthCheckStatus>>([new(_message) { ResultType = _resultType }]);
-    }
-
-    [HealthCheck("CFD6FC34-59C9-4402-B55F-C8BC96B628A1", "Success Check")]
-    public class SuccessHealthCheck : StubHealthCheck
-    {
-        public SuccessHealthCheck()
-            : base(StatusResultType.Success, "Check passed")
-        {
-        }
-    }
-
-    [HealthCheck("CFD6FC34-59C9-4402-B55F-C8BC96B628A2", "Warning Check")]
-    public class WarningHealthCheck : StubHealthCheck
-    {
-        public WarningHealthCheck()
-            : base(StatusResultType.Warning, "Check has warnings")
-        {
-        }
-    }
-
-    [HealthCheck("CFD6FC34-59C9-4402-B55F-C8BC96B628A3", "Error Check")]
-    public class ErrorHealthCheck : StubHealthCheck
-    {
-        public ErrorHealthCheck()
-            : base(StatusResultType.Error, "Check failed")
-        {
-        }
-    }
-
-#pragma warning disable CS0618 // Type or member is obsolete
-    private static MarkdownToHtmlConverter CreateConverter() =>
-        new(new HeyRedMarkdownToHtmlConverter());
-#pragma warning restore CS0618 // Type or member is obsolete
-
     [Test]
     public async Task ToHtml_WithSuccessResult_AppliesGreenHighlighting()
     {
@@ -143,4 +92,55 @@ public class MarkdownToHtmlConverterTests
 
         Assert.That(html, Does.Not.Contain("Check passed"));
     }
+
+    [HealthCheck("CFD6FC34-59C9-4402-B55F-C8BC96B628A1", "Stub check")]
+    public abstract class StubHealthCheck : HealthCheck
+    {
+        private readonly string _message;
+        private readonly StatusResultType _resultType;
+
+        protected StubHealthCheck(StatusResultType resultType, string message)
+        {
+            _resultType = resultType;
+            _message = message;
+        }
+
+        public override HealthCheckStatus ExecuteAction(HealthCheckAction action) =>
+            throw new NotImplementedException();
+
+        public override Task<IEnumerable<HealthCheckStatus>> GetStatusAsync() =>
+            Task.FromResult<IEnumerable<HealthCheckStatus>>([new(_message) { ResultType = _resultType }]);
+    }
+
+    [HealthCheck("CFD6FC34-59C9-4402-B55F-C8BC96B628A1", "Success Check")]
+    public class SuccessHealthCheck : StubHealthCheck
+    {
+        public SuccessHealthCheck()
+            : base(StatusResultType.Success, "Check passed")
+        {
+        }
+    }
+
+    [HealthCheck("CFD6FC34-59C9-4402-B55F-C8BC96B628A2", "Warning Check")]
+    public class WarningHealthCheck : StubHealthCheck
+    {
+        public WarningHealthCheck()
+            : base(StatusResultType.Warning, "Check has warnings")
+        {
+        }
+    }
+
+    [HealthCheck("CFD6FC34-59C9-4402-B55F-C8BC96B628A3", "Error Check")]
+    public class ErrorHealthCheck : StubHealthCheck
+    {
+        public ErrorHealthCheck()
+            : base(StatusResultType.Error, "Check failed")
+        {
+        }
+    }
+
+#pragma warning disable CS0618 // Type or member is obsolete
+    private static MarkdownToHtmlConverter CreateConverter() =>
+        new(new HeyRedMarkdownToHtmlConverter());
+#pragma warning restore CS0618 // Type or member is obsolete
 }
