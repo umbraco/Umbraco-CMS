89cb3646eda279c464375be08016e002097671d7
mole <nikolajlauridsen@protonmail.ch>
2026-01-06 10:03:39 +0100

    Fix bind mounts permission issues

---
+++ templates/UmbracoProject/entrypoint.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+set -e
+
+# Fix ownership of bind-mounted directories so the app can write to them
+chown -R $APP_UID:$APP_UID /app/wwwroot/media /app/wwwroot/scripts /app/wwwroot/css /app/Views /app/umbraco
+
+# Drop privileges and run the application as the app user
+exec gosu $APP_UID dotnet UmbracoProject.dll
