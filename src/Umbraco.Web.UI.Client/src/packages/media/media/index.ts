6221619f4123adfb2264fa08f5b1e72ec2832f38
Niels Lyngsø <niels.lyngso@gmail.com>
2026-01-07 21:14:02 +0100

    fix media modal exports

--- src/Umbraco.Web.UI.Client/src/packages/media/media/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/index.ts
@@ -2,7 +2,7 @@ export * from './audit-log/index.js';
 export * from './components/index.js';
 export * from './constants.js';
 export * from './dropzone/index.js';
-export { UMB_IMAGE_CROPPER_EDITOR_MODAL, UMB_MEDIA_PICKER_MODAL } from './modals/index.js';
+export * from './modals/index.js';
 export * from './recycle-bin/index.js';
 export * from './reference/index.js';
 export * from './repository/index.js';
