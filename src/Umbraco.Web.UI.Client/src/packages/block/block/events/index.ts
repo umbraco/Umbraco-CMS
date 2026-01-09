5854bd6cb511979012f4e2d0f6ae3b339bce1e4e
Niels Lyngsø <niels.lyngso@gmail.com>
2026-01-09 08:50:40 +0100

    add Block created event

---
+++ src/Umbraco.Web.UI.Client/src/packages/block/block/events/index.ts
@@ -0,0 +1,1 @@
+export * from './block-created.event.js';
--- src/Umbraco.Web.UI.Client/src/packages/block/block/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/block/block/index.ts
@@ -1,6 +1,7 @@
 export * from './clipboard/index.js';
 export * from './components/index.js';
 export * from './context/index.js';
+export * from './events/index.js';
 export * from './modals/index.js';
 export * from './property-value-cloner/index.js';
 export * from './property-value-resolver/index.js';
