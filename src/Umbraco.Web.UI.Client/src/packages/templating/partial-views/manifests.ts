8b91a46cd9949a4c8ee59ffd8d48a51799343c85
Mads Rasmussen <madsr@hey.com>
2026-01-13 11:20:04 +0100

    Replaces dynamic imports of entry-point.js with static imports across all manifests

--- src/Umbraco.Web.UI.Client/src/packages/templating/partial-views/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/templating/partial-views/manifests.ts
@@ -3,6 +3,7 @@ import { manifests as menuManifests } from './menu/manifests.js';
 import { manifests as treeManifests } from './tree/manifests.js';
 import { manifests as entityActionsManifests } from './entity-actions/manifests.js';
 import { manifests as workspaceManifests } from './workspace/manifests.js';
+import * as entryPointModule from './entry-point.js';
 
 export const manifests: Array<UmbExtensionManifest> = [
 	...repositoryManifests,
@@ -14,6 +15,6 @@ export const manifests: Array<UmbExtensionManifest> = [
 		name: 'Partial View Backoffice Entry Point',
 		alias: 'Umb.EntryPoint.Partial View',
 		type: 'backofficeEntryPoint',
-		js: () => import('./entry-point.js'),
+		js: entryPointModule,
 	},
 ];
