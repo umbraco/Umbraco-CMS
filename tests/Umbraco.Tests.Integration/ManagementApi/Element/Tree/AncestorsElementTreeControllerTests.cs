897abee4659d8a7fda7174a4fac10340fa792398
Laura Neto <12862535+lauraneto@users.noreply.github.com>
2026-01-14 15:08:26 +0100

    Elements: Add integration tests for element tree start node permissions
    Add tests to verify that users with element start node restrictions can only see
    and access elements within their permitted hierarchy.

--- tests/Umbraco.Tests.Integration/ManagementApi/Element/Tree/AncestorsElementTreeControllerTests.cs
+++ tests/Umbraco.Tests.Integration/ManagementApi/Element/Tree/AncestorsElementTreeControllerTests.cs
@@ -2,9 +2,7 @@ using System.Linq.Expressions;
 using System.Net;
 using System.Net.Http.Json;
 using NUnit.Framework;
-using Umbraco.Cms.Api.Common.ViewModels.Pagination;
 using Umbraco.Cms.Api.Management.Controllers.Element.Tree;
-using Umbraco.Cms.Api.Management.ViewModels;
 using Umbraco.Cms.Api.Management.ViewModels.Tree;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Services;
@@ -13,14 +11,33 @@ using Umbraco.Cms.Tests.Common.Builders.Extensions;
 
 namespace Umbraco.Cms.Tests.Integration.ManagementApi.Element.Tree;
 
-public class RootElementTreeControllerTests : ManagementApiUserGroupTestBase<RootElementTreeController>
+public class AncestorsElementTreeControllerTests : ManagementApiUserGroupTestBase<AncestorsElementTreeController>
 {
     private IElementContainerService ElementContainerService => GetRequiredService<IElementContainerService>();
 
     private IUserGroupService UserGroupService => GetRequiredService<IUserGroupService>();
 
-    protected override Expression<Func<RootElementTreeController, object>> MethodSelector =>
-        x => x.Root(CancellationToken.None, 0, 100, false);
+    private Guid _grandparentKey;
+    private Guid _parentKey;
+    private int _parentId;
+
+    [SetUp]
+    public async Task Setup()
+    {
+        // Create grandparent container
+        var grandparentResult = await ElementContainerService.CreateAsync(null, $"GrandparentContainer {Guid.NewGuid()}", null, Constants.Security.SuperUserKey);
+        Assert.IsTrue(grandparentResult.Success, $"Failed to create grandparent: {grandparentResult.Status}");
+        _grandparentKey = grandparentResult.Result!.Key;
+
+        // Create parent container
+        var parentResult = await ElementContainerService.CreateAsync(null, $"ParentContainer {Guid.NewGuid()}", _grandparentKey, Constants.Security.SuperUserKey);
+        Assert.IsTrue(parentResult.Success, $"Failed to create parent: {parentResult.Status}");
+        _parentKey = parentResult.Result!.Key;
+        _parentId = parentResult.Result!.Id;
+    }
+
+    protected override Expression<Func<AncestorsElementTreeController, object>> MethodSelector =>
+        x => x.Ancestors(CancellationToken.None, _parentKey);
 
     protected override UserGroupAssertionModel AdminUserGroupAssertionModel
         => new() { ExpectedStatusCode = HttpStatusCode.OK };
@@ -41,47 +58,33 @@ public class RootElementTreeControllerTests : ManagementApiUserGroupTestBase<Roo
         => new() { ExpectedStatusCode = HttpStatusCode.Unauthorized };
 
     [Test]
-    public async Task User_With_Start_Node_Only_Sees_Permitted_Roots()
+    public async Task User_With_Start_Node_Can_Access_Ancestors()
     {
-        // Create two folders at root level
-        var folder1Result = await ElementContainerService.CreateAsync(
-            null,
-            $"Folder 1 {Guid.NewGuid()}",
-            null,
-            Constants.Security.SuperUserKey);
-        Assert.IsTrue(folder1Result.Success);
-        var folder1 = folder1Result.Result!;
-
-        var folder2Result = await ElementContainerService.CreateAsync(
-            null,
-            $"Folder 2 {Guid.NewGuid()}",
-            null,
-            Constants.Security.SuperUserKey);
-        Assert.IsTrue(folder2Result.Success);
-
-        // Create a user group with start node = folder1 only
+        // Create a user group with Library section access and start node = parent folder
         var userGroup = new UserGroupBuilder()
             .WithAlias(Guid.NewGuid().ToString("N"))
             .WithName("Test Group With Element Start Node")
             .WithAllowedSections(["library"])
-            .WithStartElementId(folder1.Id)
+            .WithStartElementId(_parentId)
             .Build();
 
         await UserGroupService.CreateAsync(userGroup, Constants.Security.SuperUserKey);
 
-        // Authenticate as a user in that group
+        // Authenticate as a user in the group with the restricted start node
         await AuthenticateClientAsync(Client, $"startnodetest{Guid.NewGuid():N}@umbraco.com", "1234567890", userGroup.Key);
 
-        // Get root tree items
+        // Get ancestors of the parent folder (user's start node)
         var response = await ClientRequest();
 
-        // Should succeed
         Assert.AreEqual(HttpStatusCode.OK, response.StatusCode, await response.Content.ReadAsStringAsync());
 
-        // Parse response and verify only folder1 is returned
-        var result = await response.Content.ReadFromJsonAsync<PagedViewModel<ElementTreeItemResponseModel>>(JsonSerializerOptions);
+        var result = await response.Content.ReadFromJsonAsync<IEnumerable<ElementTreeItemResponseModel>>(JsonSerializerOptions);
         Assert.IsNotNull(result);
-        Assert.AreEqual(1, result.Total);
-        Assert.AreEqual(folder1.Key, result.Items.First().Id);
+        var ancestors = result.ToList();
+
+        // Ancestors should include grandparent and parent (the target itself)
+        Assert.AreEqual(2, ancestors.Count, "Should return grandparent and parent");
+        Assert.AreEqual(_grandparentKey, ancestors[0].Id, "First ancestor should be grandparent");
+        Assert.AreEqual(_parentKey, ancestors[1].Id, "Second ancestor should be parent (target)");
     }
 }
--- tests/Umbraco.Tests.Integration/ManagementApi/Element/Tree/ChildrenElementTreeControllerTests.cs
+++ tests/Umbraco.Tests.Integration/ManagementApi/Element/Tree/ChildrenElementTreeControllerTests.cs
@@ -1,7 +1,10 @@
 using System.Linq.Expressions;
 using System.Net;
+using System.Net.Http.Json;
 using NUnit.Framework;
+using Umbraco.Cms.Api.Common.ViewModels.Pagination;
 using Umbraco.Cms.Api.Management.Controllers.Element.Tree;
+using Umbraco.Cms.Api.Management.ViewModels.Tree;
 using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Models.ContentEditing;
 using Umbraco.Cms.Core.Services;
@@ -18,6 +21,8 @@ public class ChildrenElementTreeControllerTests : ManagementApiUserGroupTestBase
 
     private IContentTypeService ContentTypeService => GetRequiredService<IContentTypeService>();
 
+    private IUserGroupService UserGroupService => GetRequiredService<IUserGroupService>();
+
     private Guid _parentKey;
 
     [SetUp]
@@ -26,24 +31,26 @@ public class ChildrenElementTreeControllerTests : ManagementApiUserGroupTestBase
         // Create element type
         var elementType = new ContentTypeBuilder()
             .WithAlias(Guid.NewGuid().ToString())
-            .WithName("Test Element")
+            .WithName($"Test Element {Guid.NewGuid()}")
             .WithIsElement(true)
             .Build();
         await ContentTypeService.CreateAsync(elementType, Constants.Security.SuperUserKey);
 
         // Create parent container
-        var parentResult = await ElementContainerService.CreateAsync(null, "ParentContainer", null, Constants.Security.SuperUserKey);
+        var parentResult = await ElementContainerService.CreateAsync(null, $"ParentContainer {Guid.NewGuid()}", null, Constants.Security.SuperUserKey);
+        Assert.IsTrue(parentResult.Success, $"Failed to create parent: {parentResult.Status}");
         _parentKey = parentResult.Result!.Key;
 
         // Create child container
-        await ElementContainerService.CreateAsync(null, "ChildContainer", _parentKey, Constants.Security.SuperUserKey);
+        var childContainerResult = await ElementContainerService.CreateAsync(null, $"ChildContainer {Guid.NewGuid()}", _parentKey, Constants.Security.SuperUserKey);
+        Assert.IsTrue(childContainerResult.Success, $"Failed to create child container: {childContainerResult.Status}");
 
         // Create child element
         var createModel = new ElementCreateModel
         {
             ContentTypeKey = elementType.Key,
             ParentKey = _parentKey,
-            Variants = [new VariantModel { Name = "ChildElement" }],
+            Variants = [new VariantModel { Name = $"ChildElement {Guid.NewGuid()}" }],
         };
         await ElementEditingService.CreateAsync(createModel, Constants.Security.SuperUserKey);
     }
@@ -68,4 +75,42 @@ public class ChildrenElementTreeControllerTests : ManagementApiUserGroupTestBase
 
     protected override UserGroupAssertionModel UnauthorizedUserGroupAssertionModel
         => new() { ExpectedStatusCode = HttpStatusCode.Unauthorized };
+
+    [Test]
+    public async Task User_With_Start_Node_Cannot_Access_Children_Outside_Start_Node()
+    {
+        // Create another folder to be the user's start node (different from the one in Setup)
+        var startNodeResult = await ElementContainerService.CreateAsync(
+            null,
+            $"Start Node Folder {Guid.NewGuid()}",
+            _parentKey,
+            Constants.Security.SuperUserKey);
+        Assert.IsTrue(startNodeResult.Success, $"Failed to create start node folder: {startNodeResult.Status}");
+        var startNodeFolder = startNodeResult.Result!;
+
+        // Create a user group with Library section access but with a non-root element start node
+        var userGroup = new UserGroupBuilder()
+            .WithAlias(Guid.NewGuid().ToString("N"))
+            .WithName("Test Group With Element Start Node")
+            .WithAllowedSections(["library"])
+            .WithStartElementId(startNodeFolder.Id)
+            .Build();
+
+        await UserGroupService.CreateAsync(userGroup, Constants.Security.SuperUserKey);
+
+        // Authenticate as a user in the group with the restricted start node
+        await AuthenticateClientAsync(Client, $"startnodetest{Guid.NewGuid():N}@umbraco.com", "1234567890", userGroup.Key);
+
+        // Try to access the children of the parent folder created in Setup (which is outside the start node)
+        var response = await ClientRequest();
+
+        // Should succeed
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode, await response.Content.ReadAsStringAsync());
+
+        // Parse response and verify only folder1 is returned
+        var result = await response.Content.ReadFromJsonAsync<PagedViewModel<ElementTreeItemResponseModel>>(JsonSerializerOptions);
+        Assert.IsNotNull(result);
+        Assert.AreEqual(1, result.Total);
+        Assert.AreEqual(startNodeFolder.Key, result.Items.First().Id);
+    }
 }
