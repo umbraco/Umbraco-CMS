fc48a72233fdee3c10b54d1c749cb933ca2791bc
Jacob Overgaard <752371+iOvergaard@users.noreply.github.com>
2026-01-14 15:26:13 +0100

    test: removes .only

--- tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Users/Permissions/User/ContentStartNodes.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Users/Permissions/User/ContentStartNodes.spec.ts
@@ -155,7 +155,7 @@ test("can see parent of start node but not access it", async ({
   );
 });
 
-test.only("see no-access view when deep-linking to restricted document", async ({
+test("see no-access view when deep-linking to restricted document", async ({
   umbracoApi,
   umbracoUi,
 }) => {
