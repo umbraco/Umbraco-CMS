f8e0f96cf981a2b534840d5167606fb8ab1f3f2d
kjac <kja@umbraco.dk>
2026-01-14 11:52:09 +0100

    Endpoints for Elements versioning

---
+++ src/Umbraco.Cms.Api.Management/Controllers/ElementVersion/ElementVersionControllerBase.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Management.Controllers.DocumentVersion;
+using Umbraco.Cms.Api.Management.Routing;
+using Umbraco.Cms.Core;
+using Umbraco.Cms.Core.Services.OperationStatus;
+
+namespace Umbraco.Cms.Api.Management.Controllers.ElementVersion;
+
+[VersionedApiBackOfficeRoute($"{Constants.UdiEntityType.Element}-version")]
+[ApiExplorerSettings(GroupName = $"{nameof(Constants.UdiEntityType.Element)} Version")]
+public abstract class ElementVersionControllerBase : ManagementApiControllerBase
+{
+    protected IActionResult MapFailure(ContentVersionOperationStatus status)
+        => DocumentVersionControllerBase.MapFailure(status);
+}
