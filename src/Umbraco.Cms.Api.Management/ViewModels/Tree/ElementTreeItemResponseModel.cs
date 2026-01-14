b533601e852bc864ac24977d60094750907fdee6
Laura Neto <12862535+lauraneto@users.noreply.github.com>
2026-01-14 10:21:57 +0100

    Elements: Move NoAccess property to FolderTreeItemResponseModel base class
    This allows both elements and folders to indicate access status in the tree.

--- src/Umbraco.Cms.Api.Management/ViewModels/Tree/ElementTreeItemResponseModel.cs
+++ src/Umbraco.Cms.Api.Management/ViewModels/Tree/ElementTreeItemResponseModel.cs
@@ -5,8 +5,6 @@ namespace Umbraco.Cms.Api.Management.ViewModels.Tree;
 
 public class ElementTreeItemResponseModel : FolderTreeItemResponseModel
 {
-    public bool NoAccess { get; set; }
-
     public DateTimeOffset CreateDate { get; set; }
 
     public DocumentTypeReferenceResponseModel? DocumentType { get; set; }
