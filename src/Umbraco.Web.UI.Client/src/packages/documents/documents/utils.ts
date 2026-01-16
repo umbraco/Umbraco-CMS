2c41adb1ce42b569e02a0ac07aceeb1aed175cca
Niels Lyngsø <niels.lyngso@gmail.com>
2026-01-16 17:24:47 +0100

    refactor to avoid inline methods

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/utils.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/utils.ts
@@ -21,31 +21,38 @@ const getVariantStateOrderValue = (variant?: UmbDocumentVariantOptionModel['vari
 	return variantStatesOrder[variant.state] || fallbackOrder;
 };
 
-export const sortVariants = (a: VariantType, b: VariantType) => {
-	const compareDefault = (a: VariantType, b: VariantType) =>
-		(a.language?.isDefault ? -1 : 1) - (b.language?.isDefault ? -1 : 1);
-
-	// Make sure mandatory variants goes on top, unless they are published, cause then they already goes to the top and then we want to mix them with other published variants.
-	const compareMandatory = (a: VariantType, b: VariantType) =>
-		a.variant?.state === DocumentVariantStateModel.PUBLISHED_PENDING_CHANGES ||
+// eslint-disable-next-line jsdoc/require-jsdoc
+function compareDefault(a: VariantType, b: VariantType) {
+	return (a.language?.isDefault ? -1 : 1) - (b.language?.isDefault ? -1 : 1);
+}
+// Make sure mandatory variants goes on top, unless they are published, cause then they already goes to the top and then we want to mix them with other published variants.
+// eslint-disable-next-line jsdoc/require-jsdoc
+function compareMandatory(a: VariantType, b: VariantType) {
+	return a.variant?.state === DocumentVariantStateModel.PUBLISHED_PENDING_CHANGES ||
 		a.variant?.state === DocumentVariantStateModel.PUBLISHED
-			? 0
-			: (a.language?.isMandatory ? -1 : 1) - (b.language?.isMandatory ? -1 : 1);
-	const compareState = (a: VariantType, b: VariantType) =>
-		getVariantStateOrderValue(a.variant) - getVariantStateOrderValue(b.variant);
+		? 0
+		: (a.language?.isMandatory ? -1 : 1) - (b.language?.isMandatory ? -1 : 1);
+}
 
-	const compareName = (a: VariantType, b: VariantType) => {
-		const nameA = a.language?.name;
-		const nameB = b.language?.name;
+// eslint-disable-next-line jsdoc/require-jsdoc
+function compareState(a: VariantType, b: VariantType) {
+	return getVariantStateOrderValue(a.variant) - getVariantStateOrderValue(b.variant);
+}
 
-		// If both names are missing, consider them equal.
-		if (!nameA && !nameB) return 0;
-		// If only one name is missing, sort the defined name first.
-		if (!nameA) return 1;
-		if (!nameB) return -1;
+// eslint-disable-next-line jsdoc/require-jsdoc
+function compareName(a: VariantType, b: VariantType) {
+	const nameA = a.language?.name;
+	const nameB = b.language?.name;
 
-		return nameA.localeCompare(nameB);
-	};
+	// If both names are missing, consider them equal.
+	if (!nameA && !nameB) return 0;
+	// If only one name is missing, sort the defined name first.
+	if (!nameA) return 1;
+	if (!nameB) return -1;
+
+	return nameA.localeCompare(nameB);
+}
+export const sortVariants = (a: VariantType, b: VariantType) => {
 	return compareDefault(a, b) || compareMandatory(a, b) || compareState(a, b) || compareName(a, b);
 };
 
