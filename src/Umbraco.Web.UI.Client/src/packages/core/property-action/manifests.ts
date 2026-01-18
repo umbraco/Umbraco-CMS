07360f54df9e81091d83690c68442166b8811221
engjlr <enl@umbraco.dk>
2026-01-18 13:45:46 +0100

    Enable Clear action for BlockList and BlockGrid editors.

--- src/Umbraco.Web.UI.Client/src/packages/core/property-action/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property-action/manifests.ts
@@ -2,7 +2,6 @@ import { manifests as kindManifests } from './kinds/manifests.js';
 import type { UmbExtensionManifestKind } from '@umbraco-cms/backoffice/extension-registry';
 
 import { UMB_WRITABLE_PROPERTY_CONDITION_ALIAS } from '@umbraco-cms/backoffice/property';
-
 export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> = [
 	{
 		type: 'propertyAction',
@@ -10,7 +9,7 @@ export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> =
 		alias: 'Umb.PropertyAction.Clear',
 		name: 'Clear Property Action',
 		api: () => import('./common/clear/property-action-clear.controller.js'),
-		forPropertyEditorUis: [],
+		forPropertyEditorUis: ['Umb.PropertyEditorUi.BlockList', 'Umb.PropertyEditorUi.BlockGrid'],
 		meta: {
 			icon: 'icon-trash',
 			label: 'Clear',
