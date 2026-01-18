4a17a78fe9b037d98899c92b2b4398fedcf20f60
engjlr <enl@umbraco.dk>
2026-01-18 13:51:45 +0100

    Add has-value condition to property action manifests.

--- src/Umbraco.Web.UI.Client/src/packages/core/property-action/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property-action/manifests.ts
@@ -1,7 +1,10 @@
 import { manifests as kindManifests } from './kinds/manifests.js';
 import type { UmbExtensionManifestKind } from '@umbraco-cms/backoffice/extension-registry';
 
-import { UMB_WRITABLE_PROPERTY_CONDITION_ALIAS } from '@umbraco-cms/backoffice/property';
+import {
+	UMB_PROPERTY_HAS_VALUE_CONDITION_ALIAS,
+	UMB_WRITABLE_PROPERTY_CONDITION_ALIAS,
+} from '@umbraco-cms/backoffice/property';
 export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> = [
 	{
 		type: 'propertyAction',
@@ -17,6 +20,9 @@ export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> =
 		conditions: [
 			{
 				alias: UMB_WRITABLE_PROPERTY_CONDITION_ALIAS,
+			},
+			{
+				alias: UMB_PROPERTY_HAS_VALUE_CONDITION_ALIAS,
 			},
 		],
 	},
