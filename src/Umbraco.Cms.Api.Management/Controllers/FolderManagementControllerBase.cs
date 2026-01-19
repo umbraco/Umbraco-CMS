79e39653cfee386e242e712983680f20136baa87
kjac <kja@umbraco.dk>
2026-01-19 17:21:08 +0100

    Include "isTrashed" in folder response models

--- src/Umbraco.Cms.Api.Management/Controllers/FolderManagementControllerBase.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/FolderManagementControllerBase.cs
@@ -39,7 +39,8 @@ public abstract class FolderManagementControllerBase<TTreeEntity> : ManagementAp
         return Ok(new FolderResponseModel
         {
             Name = container.Name!,
-            Id = container.Key
+            Id = container.Key,
+            IsTrashed = container.Trashed
         });
     }
 
