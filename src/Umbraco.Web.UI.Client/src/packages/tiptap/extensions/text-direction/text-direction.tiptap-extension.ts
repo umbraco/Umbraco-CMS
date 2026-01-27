791a99caf71d9d15dcc4208be2b341b2752eca84
leekelleher <leekelleher@gmail.com>
2026-01-27 09:29:28 +0000

    Resolved the export issue

--- src/Umbraco.Web.UI.Client/src/packages/tiptap/extensions/text-direction/text-direction.tiptap-extension.ts
+++ src/Umbraco.Web.UI.Client/src/packages/tiptap/extensions/text-direction/text-direction.tiptap-extension.ts
@@ -1,7 +1,8 @@
 // NOTE: `TextDirection` is now bundled with Tiptap since v3.11.0. [LK]
 // https://github.com/ueberdosis/tiptap/pull/7207
 
-import { extensions as TiptapExtensions } from '../../externals.js';
+// eslint-disable-next-line local-rules/enforce-umbraco-external-imports
+import { extensions as TiptapExtensions } from '@tiptap/core';
 
 /** @deprecated No longer used internally. This will be removed in Umbraco 19. [LK] */
 export interface UmbTiptapTextDirectionOptions {
@@ -10,4 +11,4 @@ export interface UmbTiptapTextDirectionOptions {
 }
 
 /** @deprecated No longer required, (since it comes default with Tiptap). This will be removed in Umbraco 19. [LK] */
-export const TextDirection = TiptapExtensions.TextDirection;
+export const TextDirection = TiptapExtensions.TextDirection.extend<UmbTiptapTextDirectionOptions>();
