9b8640e30d81ad7450fb4e028e27414535742bc8
engjlr <enl@umbraco.dk>
2026-02-02 13:06:13 +0100

    Remove unused import.

--- src/Umbraco.Web.UI.Client/src/packages/core/property-action/kinds/clear/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property-action/kinds/clear/manifests.ts
@@ -1,8 +1,5 @@
 import type { UmbExtensionManifestKind } from '@umbraco-cms/backoffice/extension-registry';
-import {
-	UMB_PROPERTY_HAS_VALUE_CONDITION_ALIAS,
-	UMB_WRITABLE_PROPERTY_CONDITION_ALIAS,
-} from '@umbraco-cms/backoffice/property';
+import { UMB_WRITABLE_PROPERTY_CONDITION_ALIAS } from '@umbraco-cms/backoffice/property';
 import { UMB_PROPERTY_ACTION_DEFAULT_KIND_MANIFEST } from '../default/index.js';
 
 export const UMB_PROPERTY_ACTION_CLEAR_KIND_MANIFEST: UmbExtensionManifestKind = {
