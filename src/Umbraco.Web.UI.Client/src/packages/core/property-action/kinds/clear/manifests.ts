dcc38b0ba2b57ad37e0b9530102e428ffc388f7a
engjlr <enl@umbraco.dk>
2026-02-02 11:30:40 +0100

    Add clear kind property action.

---
+++ src/Umbraco.Web.UI.Client/src/packages/core/property-action/kinds/clear/manifests.ts
@@ -0,0 +1,22 @@
+import type { UmbExtensionManifestKind } from '@umbraco-cms/backoffice/extension-registry';
+import {
+	UMB_PROPERTY_HAS_VALUE_CONDITION_ALIAS,
+	UMB_WRITABLE_PROPERTY_CONDITION_ALIAS,
+} from '@umbraco-cms/backoffice/property';
+
+export const UMB_PROPERTY_ACTION_CLEAR_KIND_MANIFEST: UmbExtensionManifestKind = {
+	type: 'kind',
+	alias: 'Umb.Kind.PropertyAction.Clear',
+	matchType: 'propertyAction',
+	matchKind: 'clear',
+	manifest: {
+		api: () => import('../../common/clear/property-action-clear.controller.js'),
+		meta: {
+			icon: 'icon-trash',
+			label: '#actions_clear',
+		},
+		conditions: [{ alias: UMB_WRITABLE_PROPERTY_CONDITION_ALIAS }, { alias: UMB_PROPERTY_HAS_VALUE_CONDITION_ALIAS }],
+	},
+};
+
+export const manifest = UMB_PROPERTY_ACTION_CLEAR_KIND_MANIFEST;
--- src/Umbraco.Web.UI.Client/src/packages/core/property-action/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property-action/manifests.ts
@@ -1,30 +1,4 @@
 import { manifests as kindManifests } from './kinds/manifests.js';
 import type { UmbExtensionManifestKind } from '@umbraco-cms/backoffice/extension-registry';
 
-import {
-	UMB_PROPERTY_HAS_VALUE_CONDITION_ALIAS,
-	UMB_WRITABLE_PROPERTY_CONDITION_ALIAS,
-} from '@umbraco-cms/backoffice/property';
-export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> = [
-	{
-		type: 'propertyAction',
-		kind: 'default',
-		alias: 'Umb.PropertyAction.Clear',
-		name: 'Clear Property Action',
-		api: () => import('./common/clear/property-action-clear.controller.js'),
-		forPropertyEditorUis: ['Umb.PropertyEditorUi.BlockList', 'Umb.PropertyEditorUi.BlockGrid'],
-		meta: {
-			icon: 'icon-trash',
-			label: 'Clear',
-		},
-		conditions: [
-			{
-				alias: UMB_WRITABLE_PROPERTY_CONDITION_ALIAS,
-			},
-			{
-				alias: UMB_PROPERTY_HAS_VALUE_CONDITION_ALIAS,
-			},
-		],
-	},
-	...kindManifests,
-];
+export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> = [...kindManifests];
