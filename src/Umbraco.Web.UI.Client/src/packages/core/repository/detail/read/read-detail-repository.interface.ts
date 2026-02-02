e0ca3191dec5ecdd6a8052ca571e16dbd4195ed2
Mads Rasmussen <madsr@hey.com>
2026-02-02 14:19:54 +0100

    Add batch read methods to detail interfaces

--- src/Umbraco.Web.UI.Client/src/packages/core/repository/detail/read/read-detail-repository.interface.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/repository/detail/read/read-detail-repository.interface.ts
@@ -1,8 +1,9 @@
-import type { UmbRepositoryResponseWithAsObservable } from '../../types.js';
+import type { UmbRepositoryResponse, UmbRepositoryResponseWithAsObservable } from '../../types.js';
 import type { UmbApi } from '@umbraco-cms/backoffice/extension-api';
 import type { Observable } from '@umbraco-cms/backoffice/external/rxjs';
 
 export interface UmbReadDetailRepository<DetailModelType> extends UmbApi {
 	requestByUnique(unique: string): Promise<UmbRepositoryResponseWithAsObservable<DetailModelType | undefined>>;
+	requestByUniques?(uniques: Array<string>): Promise<UmbRepositoryResponse<Array<DetailModelType> | undefined>>;
 	byUnique?(unique: string): Promise<Observable<DetailModelType | undefined>>;
 }
