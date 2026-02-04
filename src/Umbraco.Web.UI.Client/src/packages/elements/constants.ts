b086560eecb5a010ebdd597f57c0707bed7fd5e9
leekelleher <leekelleher@gmail.com>
2026-02-04 17:58:01 +0000

    Element constants tidy-up

---
+++ src/Umbraco.Web.UI.Client/src/packages/elements/constants.ts
@@ -0,0 +1,13 @@
+export { UMB_ELEMENT_ENTITY_TYPE, UMB_ELEMENT_ROOT_ENTITY_TYPE, UMB_ELEMENT_FOLDER_ENTITY_TYPE } from './entity.js';
+
+export * from './collection/constants.js';
+export * from './item/constants.js';
+export * from './menu/constants.js';
+export * from './paths.js';
+export * from './publishing/constants.js';
+export * from './recycle-bin/constants.js';
+export * from './reference/constants.js';
+export * from './repository/constants.js';
+export * from './rollback/constants.js';
+export * from './user-permissions/constants.js';
+export * from './workspace/constants.js';
