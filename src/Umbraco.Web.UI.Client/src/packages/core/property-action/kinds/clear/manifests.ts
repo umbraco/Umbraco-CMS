4cb74a35b74efc20bf5d987380e0a6290585fca6
Mads Rasmussen <madsr@hey.com>
2026-02-04 09:46:18 +0100

    move clear controller into kinds/clear folder

--- src/Umbraco.Web.UI.Client/src/packages/core/property-action/kinds/clear/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property-action/kinds/clear/manifests.ts
@@ -1,6 +1,6 @@
+import { UMB_PROPERTY_ACTION_DEFAULT_KIND_MANIFEST } from '../default/index.js';
 import type { UmbExtensionManifestKind } from '@umbraco-cms/backoffice/extension-registry';
 import { UMB_WRITABLE_PROPERTY_CONDITION_ALIAS } from '@umbraco-cms/backoffice/property';
-import { UMB_PROPERTY_ACTION_DEFAULT_KIND_MANIFEST } from '../default/index.js';
 
 export const UMB_PROPERTY_ACTION_CLEAR_KIND_MANIFEST: UmbExtensionManifestKind = {
 	type: 'kind',
@@ -9,7 +9,7 @@ export const UMB_PROPERTY_ACTION_CLEAR_KIND_MANIFEST: UmbExtensionManifestKind =
 	matchKind: 'clear',
 	manifest: {
 		...UMB_PROPERTY_ACTION_DEFAULT_KIND_MANIFEST.manifest,
-		api: () => import('../../common/clear/property-action-clear.controller.js'),
+		api: () => import('./property-action-clear.controller.js'),
 		meta: {
 			icon: 'icon-trash',
 			label: '#actions_clear',
