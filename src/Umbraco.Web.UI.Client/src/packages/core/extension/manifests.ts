76401f965f4d7398e945efbb000ae5862c3afcb7
Mads Rasmussen <madsr@hey.com>
2026-02-06 15:52:25 +0100

    clean up

--- src/Umbraco.Web.UI.Client/src/packages/core/extension/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/extension/manifests.ts
@@ -1,11 +1,11 @@
 import { manifests as collectionManifests } from './collection/manifests.js';
-import { manifests as entityActionManifests } from './entity-actions/manifests.js';
+import { manifests as unregisterManifests } from './unregister/manifests.js';
 import { manifests as itemManifests } from './item/manifests.js';
 import { manifests as pickerDataSourceManifests } from './picker-data-source/manifests.js';
 
 export const manifests: Array<UmbExtensionManifest> = [
 	...collectionManifests,
-	...entityActionManifests,
+	...unregisterManifests,
 	...itemManifests,
 	...pickerDataSourceManifests,
 ];
