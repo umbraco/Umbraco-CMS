f5dae82fbbedcc5a6b196dc461c1f5d82c679f12
Mads Rasmussen <madsr@hey.com>
2026-02-09 19:04:01 +0100

    proxy value and selection

--- src/Umbraco.Web.UI.Client/src/packages/core/extension/global-components/input-extension.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/extension/global-components/input-extension.element.ts
@@ -3,13 +3,12 @@ import { html, customElement, property } from '@umbraco-cms/backoffice/external/
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
 import { UmbFormControlMixin } from '@umbraco-cms/backoffice/validation';
 import { UmbChangeEvent } from '@umbraco-cms/backoffice/event';
-import { splitStringToArray } from '@umbraco-cms/backoffice/utils';
 
 @customElement('umb-input-extension')
 export class UmbInputExtensionElement extends UmbFormControlMixin<string | undefined, typeof UmbLitElement>(
 	UmbLitElement,
 ) {
-	#pickerDataSource = new UmbExtensionPickerDataSource(this);
+	#dataSource = new UmbExtensionPickerDataSource(this);
 
 	@property({ type: Number })
 	min = 0;
@@ -18,23 +17,7 @@ export class UmbInputExtensionElement extends UmbFormControlMixin<string | undef
 	max = Infinity;
 
 	@property({ type: Array })
-	public set selection(uniques: Array<string>) {
-		this.#selection = uniques;
-		super.value = uniques.length > 0 ? uniques.join(',') : undefined;
-	}
-	public get selection(): Array<string> {
-		return this.#selection;
-	}
-	#selection: Array<string> = [];
-
-	@property({ type: String })
-	public override set value(selectionString: string | undefined) {
-		this.#selection = splitStringToArray(selectionString);
-		super.value = selectionString;
-	}
-	public override get value(): string | undefined {
-		return this.#selection.length > 0 ? this.#selection.join(',') : undefined;
-	}
+	selection: Array<string> = [];
 
 	@property({ type: Boolean, reflect: true })
 	readonly = false;
@@ -46,15 +29,16 @@ export class UmbInputExtensionElement extends UmbFormControlMixin<string | undef
 	#onChange(event: Event) {
 		event.stopPropagation();
 		const target = event.target as HTMLElement & { selection: Array<string>; value: string | undefined };
-		this.#selection = target.selection;
-		super.value = target.value;
+		this.selection = target.selection;
+		this.value = target.value;
 		this.dispatchEvent(new UmbChangeEvent());
 	}
 
 	override render() {
 		return html`<umb-input-entity-data
-			.selection=${this.#selection}
-			.dataSourceApi=${this.#pickerDataSource}
+			.dataSourceApi=${this.#dataSource}
+			.value=${this.value}
+			.selection=${this.selection}
 			.min=${this.min}
 			.max=${this.max}
 			?readonly=${this.readonly}
