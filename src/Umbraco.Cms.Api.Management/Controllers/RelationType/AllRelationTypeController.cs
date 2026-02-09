1d1edac337aeb30c4ea6987cad85784aa0de8c81
Andy Butland <abutland73@gmail.com>
2026-02-09 10:36:23 +0100

    Add descriptions to controllers missing them.

--- src/Umbraco.Cms.Api.Management/Controllers/RelationType/AllRelationTypeController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/RelationType/AllRelationTypeController.cs
@@ -27,7 +27,7 @@ public class AllRelationTypeController : RelationTypeControllerBase
     [MapToApiVersion("1.0")]
     [ProducesResponseType(typeof(PagedViewModel<RelationTypeResponseModel>), StatusCodes.Status200OK)]
     [EndpointSummary("Gets a paginated collection of relation types.")]
-    [EndpointDescription("Gets a paginated collection of relation types.")]
+    [EndpointDescription("Gets a paginated collection of all relation types configured in the system.")]
     public async Task<ActionResult<PagedViewModel<RelationTypeResponseModel>>> Get(
         CancellationToken cancellationToken,
         int skip = 0,
