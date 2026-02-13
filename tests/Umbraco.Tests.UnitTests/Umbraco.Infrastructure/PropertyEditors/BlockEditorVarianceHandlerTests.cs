8fa89bc840b3b336f1f654048880f4583c5976ff
root <root@dragon.second>
2026-02-13 17:13:14 +0200

    fix: correct Assert.ThrowsAsync usage - await the task when accessing exception

--- tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/PropertyEditors/BlockEditorVarianceHandlerTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Infrastructure/PropertyEditors/BlockEditorVarianceHandlerTests.cs
@@ -87,9 +87,9 @@ public class BlockEditorVarianceHandlerTests
     public async Task AlignPropertyVarianceAsync_Throws_When_PropertyType_Is_Null()
     {
         var propertyValues = new List<BlockPropertyValue> { new() { Culture = null, PropertyType = null! } };
-        var ex = await Assert.ThrowsAsync<ArgumentException>(async () => 
+        var ex = Assert.ThrowsAsync<ArgumentException>(async () => 
             await ExecuteAlignPropertyVarianceAsync(ContentVariation.Culture, propertyValues, null));
-        Assert.IsTrue(ex!.Message.Contains("property type"));
+        Assert.IsTrue((await ex)!.Message.Contains("property type"));
     }
 
     [Test]
