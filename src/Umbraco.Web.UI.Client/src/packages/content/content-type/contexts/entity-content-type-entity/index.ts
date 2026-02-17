e7c4a9ededc4ba601ee0b36fc9df75da3053755c
Mads Rasmussen <madsr@hey.com>
2026-02-17 10:37:39 +0100

    introduce entity content type entity context

---
+++ src/Umbraco.Web.UI.Client/src/packages/content/content-type/contexts/entity-content-type-entity/index.ts
@@ -0,0 +1,2 @@
+export { UMB_ENTITY_CONTENT_TYPE_ENTITY_CONTEXT as UMB_ENTITY_CONTENT_TYPE_CONTEXT } from './entity-content-type-entity.context-token.js';
+export { UmbEntityContentTypeEntityContext } from './entity-content-type-entity.context.js';
--- src/Umbraco.Web.UI.Client/src/packages/content/content-type/contexts/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/content/content-type/contexts/index.ts
@@ -1,2 +1,3 @@
+export * from './entity-content-type-entity/index.js';
 export * from './property-structure-workspace.context-token.js';
 export type * from './property-structure-workspace-context.interface.js';
