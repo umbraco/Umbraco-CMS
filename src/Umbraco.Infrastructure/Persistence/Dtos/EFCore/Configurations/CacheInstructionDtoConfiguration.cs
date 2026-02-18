4addc043c044a5d8bf7edfc1fdf353a2612221c4
NillasKA <kramernicklas@gmail.com>
2026-02-18 15:01:00 +0100

    DTO Setup

---
+++ src/Umbraco.Infrastructure/Persistence/Dtos/EFCore/Configurations/CacheInstructionDtoConfiguration.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Umbraco.Cms.Infrastructure.Persistence.Dtos.EFCore.Configurations;
+
+public class CacheInstructionDtoConfiguration : IEntityTypeConfiguration<CacheInstructionDto>
+{
+    public void Configure(EntityTypeBuilder<CacheInstructionDto> builder)
+    {
+        builder.ToTable(CacheInstructionDto.TableName);
+
+        builder.HasKey(x => x.Id);
+
+        builder.Property(x => x.Id)
+            .HasColumnName("id")
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.UtcStamp)
+            .HasColumnName("utcStamp");
+
+        builder.Property(x => x.Instructions)
+            .HasColumnName("jsonInstruction")
+            .HasMaxLength(int.MaxValue);
+
+        builder.Property(x => x.OriginIdentity)
+            .HasColumnName("originated")
+            .HasMaxLength(500);
+
+        builder.Property(x => x.InstructionCount)
+            .HasColumnName("instructionCount")
+            .HasDefaultValue(1);
+    }
+}
---
+++ src/Umbraco.Infrastructure/Persistence/Dtos/EFCore/Configurations/LastSyncedDtoConfiguration.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Umbraco.Cms.Infrastructure.Persistence.Dtos.EFCore.Configurations;
+
+public class LastSyncedDtoConfiguration : IEntityTypeConfiguration<LastSyncedDto>
+{
+    public void Configure(EntityTypeBuilder<LastSyncedDto> builder)
+    {
+        builder.ToTable(LastSyncedDto.TableName);
+
+        builder.HasKey(x => x.MachineId);
+
+        builder.Property(x => x.MachineId)
+            .HasColumnName("machineId");
+
+        builder.Property(x => x.LastSyncedInternalId)
+            .HasColumnName("lastSyncedInternalId");
+
+        builder.Property(x => x.LastSyncedInternalId)
+            .HasColumnName("lastSyncedInternalId");
+
+        builder.Property(x => x.LastSyncedDate)
+            .HasColumnName("lastSyncedDate");
+    }
+}
