928afbc1143bea5e3ef319a2867b134ce393b74a
Andy Butland <abutland73@gmail.com>
2026-02-19 14:24:03 +0100

    Add pagination to the member group picker.

--- src/Umbraco.Web.UI.Client/src/packages/members/member-group/components/member-group-picker-modal/member-group-picker-modal.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/members/member-group/components/member-group-picker-modal/member-group-picker-modal.element.ts
@@ -4,31 +4,65 @@ import type {
 	UmbMemberGroupPickerModalValue,
 	UmbMemberGroupPickerModalData,
 } from './member-group-picker-modal.token.js';
-import { html, customElement, state, repeat } from '@umbraco-cms/backoffice/external/lit';
-import { UmbSelectionManager } from '@umbraco-cms/backoffice/utils';
+import { css, html, customElement, nothing, state, repeat } from '@umbraco-cms/backoffice/external/lit';
+import { UmbPaginationManager, UmbSelectionManager } from '@umbraco-cms/backoffice/utils';
 import { UmbModalBaseElement } from '@umbraco-cms/backoffice/modal';
+import type { UUIPaginationEvent } from '@umbraco-cms/backoffice/external/uui';
+
+const PAGE_SIZE = 50;
 
 @customElement('umb-member-group-picker-modal')
 export class UmbMemberGroupPickerModalElement extends UmbModalBaseElement<
 	UmbMemberGroupPickerModalData,
 	UmbMemberGroupPickerModalValue
 > {
+	static override styles = [
+		css`
+			uui-pagination {
+				display: block;
+				margin-top: var(--uui-size-layout-1);
+			}
+		`,
+	];
+
 	@state()
 	private _memberGroups: Array<UmbMemberGroupDetailModel> = [];
 
+	@state()
+	private _currentPage: number = 1;
+
+	@state()
+	private _totalPages: number = 1;
+
 	#collectionRepository = new UmbMemberGroupCollectionRepository(this);
 	#selectionManager = new UmbSelectionManager(this);
+	#pagination = new UmbPaginationManager();
 
 	override connectedCallback(): void {
 		super.connectedCallback();
+		this.#pagination.setPageSize(PAGE_SIZE);
 		this.#selectionManager.setSelectable(true);
 		this.#selectionManager.setMultiple(this.data?.multiple ?? false);
 		this.#selectionManager.setSelection(this.value?.selection ?? []);
 	}
 
-	override async firstUpdated() {
-		const { data } = await this.#collectionRepository.requestCollection({});
+	override firstUpdated() {
+		this.#loadMemberGroups();
+	}
+
+	async #loadMemberGroups() {
+		const skip = this.#pagination.getSkip();
+		const { data } = await this.#collectionRepository.requestCollection({ skip, take: PAGE_SIZE });
 		this._memberGroups = data?.items ?? [];
+		this.#pagination.setTotalItems(data?.total ?? 0);
+		this._currentPage = this.#pagination.getCurrentPageNumber();
+		this._totalPages = this.#pagination.getTotalPages();
+	}
+
+	#onPageChange(event: UUIPaginationEvent) {
+		event.stopPropagation();
+		this.#pagination.setCurrentPageNumber(event.target.current);
+		this.#loadMemberGroups();
 	}
 
 	get #filteredMemberGroups() {
@@ -66,6 +100,16 @@ export class UmbMemberGroupPickerModalElement extends UmbModalBaseElement<
 					`,
 				)}
 			</uui-box>
+			${this._totalPages > 1
+				? html`<uui-pagination
+						.current=${this._currentPage}
+						.total=${this._totalPages}
+						firstlabel=${this.localize.term('general_first')}
+						previouslabel=${this.localize.term('general_previous')}
+						nextlabel=${this.localize.term('general_next')}
+						lastlabel=${this.localize.term('general_last')}
+						@change=${this.#onPageChange}></uui-pagination>`
+				: nothing}
 			<div slot="actions">
 				<uui-button label=${this.localize.term('general_close')} @click=${this.#close}></uui-button>
 				<uui-button
