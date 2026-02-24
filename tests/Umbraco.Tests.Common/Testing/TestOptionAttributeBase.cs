708c5536e5c1a9ececef201f42db1d3803bf2fe6
Sven Geusens <geusens@gmail.com>
2026-02-24 11:12:53 +0100

    Dont blow up GetTestOptions when inside testfixtures

--- tests/Umbraco.Tests.Common/Testing/TestOptionAttributeBase.cs
+++ tests/Umbraco.Tests.Common/Testing/TestOptionAttributeBase.cs
@@ -8,6 +8,7 @@ using System.Reflection;
 using NUnit.Framework;
 using NUnit.Framework.Internal;
 using Umbraco.Cms.Core.Exceptions;
+using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Tests.Common.Testing;
 
@@ -28,14 +29,19 @@ public abstract class TestOptionAttributeBase : Attribute
         where TOptions : TestOptionAttributeBase, new()
     {
         var test = TestContext.CurrentContext.Test;
-        var methodName = test.MethodName;
         var type = TestExecutionContext.CurrentContext.TestObject.GetType();
-        var methodInfo = type.GetMethod(methodName); // what about overloads?
-        var options = GetTestOptions<TOptions>(methodInfo);
-        return options;
+
+        var methodInfo = type.GetMethod(test.MethodName ?? string.Empty); // what about overloads?
+        if (methodInfo is not null)
+        {
+            var options = GetTestOptions<TOptions>(methodInfo);
+            return options;
+        }
+
+        return Get<TOptions>(type, null);
     }
 
-    private static TOptions Get<TOptions>(Type type, TOptions attr)
+    private static TOptions Get<TOptions>(Type type, TOptions? attr)
         where TOptions : TestOptionAttributeBase, new()
     {
         while (type != null && type != typeof(object))
