fa19c962112731e123a818d5186cd7f873649db8
NillasKA <kramernicklas@gmail.com>
2026-02-25 15:13:48 +0100

    Creating dto

---
+++ src/Umbraco.Infrastructure/Persistence/Dtos/EFCore/Configurations/KeyValueDtoConfiguration.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Umbraco.Cms.Infrastructure.Persistence.Dtos.EFCore.Configurations;
+
+public class KeyValueDtoConfiguration : IEntityTypeConfiguration<KeyValueDto>
+{
+    public void Configure(EntityTypeBuilder<KeyValueDto> builder)
+    {
+        builder.ToTable(KeyValueDto.TableName);
+
+        builder.HasKey(x => x.Key);
+
+        builder.Property(x => x.Key)
+            .HasColumnName(KeyValueDto.PrimaryKeyColumnName)
+            .ValueGeneratedOnAdd();
+
+        builder.Property(x => x.Value)
+            .HasColumnName("value");
+
+        builder.Property(x => x.UpdateDate)
+            .HasColumnName("updated");
+    }
+}
