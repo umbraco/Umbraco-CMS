9e881c2dcaebbbf2c16464435d03003243edf0fb
Andreas Zerbst <andr317c@live.dk>
2026-02-25 21:21:22 +0100

    Fixed test

--- tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Content/ContentInfoTab.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/DefaultConfig/Content/ContentInfoTab.spec.ts
@@ -17,7 +17,7 @@ test.afterEach(async ({umbracoApi}) => {
 
 test('can see correct information when published', async ({umbracoApi, umbracoUi}) => {
   // Arrange
-  const notPublishContentLink = 'Not created';
+  const notPublishContentLink = 'This item is not published';
   const dataTypeName = 'Textstring';
   const contentText = 'This is test content text';
   const dataTypeData = await umbracoApi.dataType.getByName(dataTypeName);
