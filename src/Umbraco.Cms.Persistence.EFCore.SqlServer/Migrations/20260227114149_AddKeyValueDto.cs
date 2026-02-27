782a8de847944d9b6e57d8b1a18ec70c01aa33bf
NillasKA <kramernicklas@gmail.com>
2026-02-27 13:09:13 +0100

    Making migration no-op

--- src/Umbraco.Cms.Persistence.EFCore.SqlServer/Migrations/20260227114149_AddKeyValueDto.cs
+++ src/Umbraco.Cms.Persistence.EFCore.SqlServer/Migrations/20260227114149_AddKeyValueDto.cs
@@ -11,25 +11,12 @@ namespace Umbraco.Cms.Persistence.EFCore.SqlServer.Migrations
         /// <inheritdoc />
         protected override void Up(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.CreateTable(
-                name: "umbracoKeyValue",
-                columns: table => new
-                {
-                    key = table.Column<string>(type: "nvarchar(256)", maxLength: 256, nullable: false),
-                    value = table.Column<string>(type: "nvarchar(max)", nullable: true),
-                    updated = table.Column<DateTime>(type: "datetime2", nullable: false)
-                },
-                constraints: table =>
-                {
-                    table.PrimaryKey("PK_umbracoKeyValue", x => x.key);
-                });
+            // NO-OP
         }
 
         /// <inheritdoc />
         protected override void Down(MigrationBuilder migrationBuilder)
         {
-            migrationBuilder.DropTable(
-                name: "umbracoKeyValue");
         }
     }
 }
