384cf9362eea524b2964aeceee60cc71bea73716
Andreas Zerbst <andr317c@live.dk>
2026-02-27 10:51:15 +0100

    Updated imports

--- tests/Umbraco.Tests.AcceptanceTest/tests/ExtensionRegistry/Segments/CultureSpecificSegmentValidation.spec.ts
+++ tests/Umbraco.Tests.AcceptanceTest/tests/ExtensionRegistry/Segments/CultureSpecificSegmentValidation.spec.ts
@@ -1,4 +1,4 @@
-import {test} from '@umbraco/playwright-testhelpers';
+import {test} from '@umbraco/acceptance-test-helpers';
 import {expect} from "@playwright/test";
 
 // Regression tests for https://github.com/umbraco/Umbraco-CMS/issues/21029
