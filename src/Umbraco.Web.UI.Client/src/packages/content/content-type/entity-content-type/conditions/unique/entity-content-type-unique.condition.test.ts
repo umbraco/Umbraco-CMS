8b554102074563754005de4e892342fd443edf7d
Mads Rasmussen <madsr@hey.com>
2026-03-02 11:05:16 +0100

    Update entity-content-type-unique.condition.test.ts

--- src/Umbraco.Web.UI.Client/src/packages/content/content-type/entity-content-type/conditions/unique/entity-content-type-unique.condition.test.ts
+++ src/Umbraco.Web.UI.Client/src/packages/content/content-type/entity-content-type/conditions/unique/entity-content-type-unique.condition.test.ts
@@ -31,7 +31,7 @@ describe('UmbEntityContentTypeUniqueCondition', () => {
 	});
 
 	afterEach(() => {
-		condition.hostDisconnected();
+		condition?.hostDisconnected();
 		document.body.innerHTML = '';
 	});
 
