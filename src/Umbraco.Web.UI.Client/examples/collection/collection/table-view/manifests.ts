60326a7c7b663cd2cd30cac0f46fcc7b86936521
Mads Rasmussen <madsr@hey.com>
2026-03-02 14:59:26 +0100

    Use table kind in collection example

--- src/Umbraco.Web.UI.Client/examples/collection/collection/table-view/manifests.ts
+++ src/Umbraco.Web.UI.Client/examples/collection/collection/table-view/manifests.ts
@@ -4,15 +4,9 @@ import { UMB_COLLECTION_ALIAS_CONDITION } from '@umbraco-cms/backoffice/collecti
 export const manifests: Array<UmbExtensionManifest> = [
 	{
 		type: 'collectionView',
+		kind: 'table',
 		alias: 'Example.CollectionView.Table',
 		name: 'Example Table Collection View',
-		js: () => import('./collection-view.element.js'),
-		weight: 100,
-		meta: {
-			label: 'Table',
-			icon: 'icon-table',
-			pathName: 'table',
-		},
 		conditions: [
 			{
 				alias: UMB_COLLECTION_ALIAS_CONDITION,
