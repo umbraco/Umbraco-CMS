128af128e4c629fd34897697d9962daa4ac04c99
Mads Rasmussen <madsr@hey.com>
2026-03-03 13:44:32 +0100

    introduce info modal

--- src/Umbraco.Web.UI.Client/src/packages/templating/local-components/production-mode-workspace-action/production-mode-workspace-action.api.ts
+++ src/Umbraco.Web.UI.Client/src/packages/templating/local-components/production-mode-workspace-action/production-mode-workspace-action.api.ts
@@ -1,15 +1,14 @@
 import { UmbLocalizationController } from '@umbraco-cms/backoffice/localization-api';
-import { umbConfirmModal } from '@umbraco-cms/backoffice/modal';
+import { umbInfoModal } from '@umbraco-cms/backoffice/modal';
 import { UmbWorkspaceActionBase } from '@umbraco-cms/backoffice/workspace';
 
 export class UmbTemplatingProductionModeWorkspaceActionApi extends UmbWorkspaceActionBase {
 	#localize = new UmbLocalizationController(this);
 
 	public override async execute(): Promise<void> {
-		await umbConfirmModal(this, {
+		await umbInfoModal(this, {
 			headline: this.#localize.term('template_productionMode'),
 			content: this.#localize.term('template_runtimeModeProduction'),
-			confirmLabel: this.#localize.term('general_close'),
 		}).catch(() => undefined);
 	}
 }
