f0f70920ff95daf9a146e740fe11e5b691afcfa4
Mads Rasmussen <madsr@hey.com>
2026-03-05 12:20:19 +0100

    fix path

--- src/Umbraco.Web.UI.Client/vite.config.ts
+++ src/Umbraco.Web.UI.Client/vite.config.ts
@@ -30,7 +30,7 @@ export const plugins: PluginOption[] = [
 				dest: 'umbraco/backoffice/assets',
 			},
 			{
-				src: 'mocks/handlers/backoffice/assets/*',
+				src: 'mocks/msw-handlers/backoffice/assets/*',
 				dest: 'umbraco/backoffice/assets',
 			},
 			{
