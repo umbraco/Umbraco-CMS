19c66ae19524cc8f70e5cefd2e85bf0989ca7278
Mads Rasmussen <madsr@hey.com>
2026-03-05 11:24:59 +0100

    rename folder

--- src/Umbraco.Web.UI.Client/mocks/e2e-handlers.ts
+++ src/Umbraco.Web.UI.Client/mocks/e2e-handlers.ts
@@ -1,20 +1,20 @@
-import { handlers as dataTypeHandlers } from './handlers/data-type/index.js';
-import { handlers as documentTypeHandlers } from './handlers/document-type/index.js';
-import { handlers as installHandlers } from './handlers/install.handlers.js';
-import * as manifestsHandlers from './handlers/manifests.handlers.js';
-import { handlers as publishedStatusHandlers } from './handlers/published-status.handlers.js';
-import * as serverHandlers from './handlers/server.handlers.js';
-import { handlers as upgradeHandlers } from './handlers/upgrade.handlers.js';
-import { handlers as userHandlers } from './handlers/user/index.js';
-import { handlers as telemetryHandlers } from './handlers/telemetry.handlers.js';
-import { handlers as examineManagementHandlers } from './handlers/examine-management.handlers.js';
-import { handlers as modelsBuilderHandlers } from './handlers/modelsbuilder.handlers.js';
-import { handlers as profileHandlers } from './handlers/performance-profiling.handlers.js';
-import { handlers as healthCheckHandlers } from './handlers/health-check.handlers.js';
-import { handlers as languageHandlers } from './handlers/language/index.js';
-import { handlers as redirectManagementHandlers } from './handlers/redirect-management.handlers.js';
-import { handlers as packageHandlers } from './handlers/package.handlers.js';
-import { handlers as configHandlers } from './handlers/config.handlers.js';
+import { handlers as dataTypeHandlers } from './msw-handlers/data-type/index.js';
+import { handlers as documentTypeHandlers } from './msw-handlers/document-type/index.js';
+import { handlers as installHandlers } from './msw-handlers/install.handlers.js';
+import * as manifestsHandlers from './msw-handlers/manifests.handlers.js';
+import { handlers as publishedStatusHandlers } from './msw-handlers/published-status.handlers.js';
+import * as serverHandlers from './msw-handlers/server.handlers.js';
+import { handlers as upgradeHandlers } from './msw-handlers/upgrade.handlers.js';
+import { handlers as userHandlers } from './msw-handlers/user/index.js';
+import { handlers as telemetryHandlers } from './msw-handlers/telemetry.handlers.js';
+import { handlers as examineManagementHandlers } from './msw-handlers/examine-management.handlers.js';
+import { handlers as modelsBuilderHandlers } from './msw-handlers/modelsbuilder.handlers.js';
+import { handlers as profileHandlers } from './msw-handlers/performance-profiling.handlers.js';
+import { handlers as healthCheckHandlers } from './msw-handlers/health-check.handlers.js';
+import { handlers as languageHandlers } from './msw-handlers/language/index.js';
+import { handlers as redirectManagementHandlers } from './msw-handlers/redirect-management.handlers.js';
+import { handlers as packageHandlers } from './msw-handlers/package.handlers.js';
+import { handlers as configHandlers } from './msw-handlers/config.handlers.js';
 
 export const handlers = [
 	serverHandlers.serverRunningHandler,
