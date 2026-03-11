7319430e70aa909b837bb08917019e83ac1b59fa
engjlr <enl@umbraco.dk>
2026-03-11 10:51:44 +0100

    Add options observable for generic kinds api.

--- src/Umbraco.Web.UI.Client/src/packages/core/collection/filter/multi-select/default-multi-select-collection-filter.api.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/collection/filter/multi-select/default-multi-select-collection-filter.api.ts
@@ -1,10 +1,13 @@
-import type { UmbCollectionFilterApi } from '../collection-filter-api.interface.js';
+import type { UmbCollectionFilterApi, UmbSelectOption } from '../collection-filter-api.interface.js';
 import { UmbControllerBase } from '@umbraco-cms/backoffice/class-api';
 import { UmbArrayState } from '@umbraco-cms/backoffice/observable-api';
 
 export class UmbDefaultMultiSelectCollectionFilterApi extends UmbControllerBase implements UmbCollectionFilterApi {
 	#value = new UmbArrayState<string>([], (x) => x);
 	public readonly value = this.#value.asObservable();
+
+	#options = new UmbArrayState<UmbSelectOption>([], (x) => x.value);
+	public readonly options = this.#options.asObservable();
 
 	public setValue(values: Array<string>) {
 		this.#value.setValue(values);
--- src/Umbraco.Web.UI.Client/src/packages/core/collection/filter/select/default-select-collection-filter.api.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/collection/filter/select/default-select-collection-filter.api.ts
@@ -1,4 +1,4 @@
-import type { UmbCollectionFilterApi } from '../collection-filter-api.interface.js';
+import type { UmbCollectionFilterApi, UmbSelectOption } from '../collection-filter-api.interface.js';
 import { UmbControllerBase } from '@umbraco-cms/backoffice/class-api';
 import { UmbArrayState } from '@umbraco-cms/backoffice/observable-api';
 import { UMB_COLLECTION_CONTEXT } from '../../default/index.js';
@@ -6,8 +6,11 @@ import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
 import type { ManifestCollectionFilter } from '../collection-filter.extension.js';
 
 export class UmbDefaultSelectCollectionFilterApi extends UmbControllerBase implements UmbCollectionFilterApi {
-	#selection = new UmbArrayState<string>([], (x) => x);
-	public readonly value = this.#selection.asObservable();
+	#value = new UmbArrayState<string>([], (x) => x);
+	public readonly value = this.#value.asObservable();
+
+	#options = new UmbArrayState<UmbSelectOption>([], (x) => x.value);
+	public readonly options = this.#options.asObservable();
 
 	#collectionContext?: typeof UMB_COLLECTION_CONTEXT.TYPE;
 
@@ -26,13 +29,13 @@ export class UmbDefaultSelectCollectionFilterApi extends UmbControllerBase imple
 		this.observe(this.#collectionContext?.filter, (value: any) => {
 			const filterKey = this.manifest?.meta?.filterKey;
 			if (filterKey && value?.[filterKey]) {
-				this.#selection.setValue([value[filterKey]]);
+				this.#value.setValue([value[filterKey]]);
 			}
 		});
 	}
 
 	public setValue(values: Array<string>) {
-		this.#selection.setValue(values);
+		this.#value.setValue(values);
 		const filterKey = this.manifest?.meta?.filterKey;
 		if (filterKey) {
 			this.#collectionContext?.setFilter({ [filterKey]: values[0] });
