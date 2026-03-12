9ac73c7addb5ebc0d1c52f2b93d5c7b9b9432c72
Andy Butland <abutland73@gmail.com>
2026-03-12 16:43:08 +0100

    Added missing description.

--- src/Umbraco.Cms.Api.Management/Factories/IDataTypePresentationFactory.cs
+++ src/Umbraco.Cms.Api.Management/Factories/IDataTypePresentationFactory.cs
@@ -17,5 +17,12 @@ public interface IDataTypePresentationFactory
     /// <param name="requestModel">The model containing the details required to create the data type.</param>
     /// <returns>A task representing the asynchronous operation, containing an <see cref="Attempt{IDataType, DataTypeOperationStatus}"/> that indicates the result of the creation attempt and its status.</returns>
     Task<Attempt<IDataType, DataTypeOperationStatus>> CreateAsync(CreateDataTypeRequestModel requestModel);
+
+    /// <summary>
+    /// Asynchronously updates an existing data type based on the provided request model.
+    /// </summary>
+    /// <param name="requestModel">The model containing the updated data type details.</param>
+    /// <param name="current">The existing <see cref="IDataType"/> instance to update.</param>
+    /// <returns>A task representing the asynchronous operation, containing an <see cref="Attempt{IDataType, DataTypeOperationStatus}"/> that indicates the result of the update attempt and its status.</returns>
     Task<Attempt<IDataType, DataTypeOperationStatus>> CreateAsync(UpdateDataTypeRequestModel requestModel, IDataType current);
 }
