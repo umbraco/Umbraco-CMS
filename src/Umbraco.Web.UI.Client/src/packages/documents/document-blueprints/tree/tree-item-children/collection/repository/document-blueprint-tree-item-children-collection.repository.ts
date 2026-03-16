6f44fd59fb92b9b7d2b1b0fd05639a68302dab22
Mads Rasmussen <madsr@hey.com>
2026-03-16 11:18:00 +0100

    init implementation

---
+++ src/Umbraco.Web.UI.Client/src/packages/documents/document-blueprints/tree/tree-item-children/collection/repository/document-blueprint-tree-item-children-collection.repository.ts
@@ -0,0 +1,32 @@
+import { UmbDocumentBlueprintTreeRepository } from '../../../document-blueprint-tree.repository.js';
+import type { UmbCollectionFilterModel, UmbCollectionRepository } from '@umbraco-cms/backoffice/collection';
+import { UmbRepositoryBase } from '@umbraco-cms/backoffice/repository';
+import { UMB_ENTITY_CONTEXT, type UmbEntityModel } from '@umbraco-cms/backoffice/entity';
+
+export class UmbDocumentBlueprintTreeItemChildrenCollectionRepository
+	extends UmbRepositoryBase
+	implements UmbCollectionRepository
+{
+	#treeRepository = new UmbDocumentBlueprintTreeRepository(this);
+
+	async requestCollection(filter: UmbCollectionFilterModel) {
+		const entityContext = await this.getContext(UMB_ENTITY_CONTEXT);
+		if (!entityContext) throw new Error('Entity context not found');
+
+		const entityType = entityContext.getEntityType();
+		const unique = entityContext.getUnique();
+
+		if (!entityType) throw new Error('Entity type not found');
+		if (unique === undefined) throw new Error('Unique not found');
+
+		const parent: UmbEntityModel = { entityType, unique };
+
+		if (parent.unique === null) {
+			return this.#treeRepository.requestTreeRootItems({ skip: filter.skip, take: filter.take });
+		} else {
+			return this.#treeRepository.requestTreeItemsOf({ parent, skip: filter.skip, take: filter.take });
+		}
+	}
+}
+
+export { UmbDocumentBlueprintTreeItemChildrenCollectionRepository as api };
---
+++ src/Umbraco.Web.UI.Client/src/packages/templating/stylesheets/tree/tree-item-children/collection/repository/stylesheet-tree-item-children-collection.repository.ts
@@ -0,0 +1,32 @@
+import { UmbStylesheetTreeRepository } from '../../../stylesheet-tree.repository.js';
+import type { UmbCollectionFilterModel, UmbCollectionRepository } from '@umbraco-cms/backoffice/collection';
+import { UmbRepositoryBase } from '@umbraco-cms/backoffice/repository';
+import { UMB_ENTITY_CONTEXT, type UmbEntityModel } from '@umbraco-cms/backoffice/entity';
+
+export class UmbStylesheetTreeItemChildrenCollectionRepository
+	extends UmbRepositoryBase
+	implements UmbCollectionRepository
+{
+	#treeRepository = new UmbStylesheetTreeRepository(this);
+
+	async requestCollection(filter: UmbCollectionFilterModel) {
+		const entityContext = await this.getContext(UMB_ENTITY_CONTEXT);
+		if (!entityContext) throw new Error('Entity context not found');
+
+		const entityType = entityContext.getEntityType();
+		const unique = entityContext.getUnique();
+
+		if (!entityType) throw new Error('Entity type not found');
+		if (unique === undefined) throw new Error('Unique not found');
+
+		const parent: UmbEntityModel = { entityType, unique };
+
+		if (parent.unique === null) {
+			return this.#treeRepository.requestTreeRootItems({ skip: filter.skip, take: filter.take });
+		} else {
+			return this.#treeRepository.requestTreeItemsOf({ parent, skip: filter.skip, take: filter.take });
+		}
+	}
+}
+
+export { UmbStylesheetTreeItemChildrenCollectionRepository as api };
