bd5b3a833c305f1c3367f83cdcbac794032ec483
leekelleher <leekelleher@gmail.com>
2026-03-25 10:05:42 +0000

    Removed `StartNodeId` from the server config

--- src/Umbraco.Core/PropertyEditors/ElementPickerConfiguration.cs
+++ src/Umbraco.Core/PropertyEditors/ElementPickerConfiguration.cs
@@ -5,12 +5,6 @@ namespace Umbraco.Cms.Core.PropertyEditors;
 /// </summary>
 public class ElementPickerConfiguration : IIgnoreUserStartNodesConfig
 {
-    /// <summary>
-    /// Gets or sets the start node ID for the element picker.
-    /// </summary>
-    [ConfigurationField("startNodeId")]
-    public Guid? StartNodeId { get; set; }
-
     /// <inheritdoc />
     [ConfigurationField(Constants.DataTypes.ReservedPreValueKeys.IgnoreUserStartNodes)]
     public bool IgnoreUserStartNodes { get; set; }
