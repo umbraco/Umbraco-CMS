39c266c27c9be309031c3901f3523a0ad97406a1
Sven Geusens <geusens@gmail.com>
2026-04-05 11:43:04 +0200

    Removed affected variance tracking that is nog longer being used

--- src/Umbraco.Cms.Api.Management/ViewModels/Patching/ContentPatchModel.cs
+++ src/Umbraco.Cms.Api.Management/ViewModels/Patching/ContentPatchModel.cs
@@ -8,17 +8,5 @@ public class ContentPatchModel
     /// <summary>
     /// Gets or sets collection of PATCH operations to apply.
     /// </summary>
-    public PatchOperationModel[] Operations { get; set; } = Array.Empty<PatchOperationModel>();
-
-    /// <summary>
-    /// Gets or sets cultures explicitly affected by this patch (extracted from operation paths).
-    /// Used for authorization checks.
-    /// </summary>
-    public IEnumerable<string> AffectedCultures { get; set; } = Array.Empty<string>();
-
-    /// <summary>
-    /// Gets or sets segments explicitly affected by this patch (extracted from operation paths).
-    /// </summary>
-    public IEnumerable<string> AffectedSegments { get; set; } = Array.Empty<string>();
-
+    public PatchOperationModel[] Operations { get; set; } = [];
 }
