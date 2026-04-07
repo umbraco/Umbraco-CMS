858fc5b87f032138bf35505e44561fc821d1670d
Mads Rasmussen <madsr@hey.com>
2026-04-07 19:17:01 +0200

    Use UmbLitElement instead of LitElement

--- src/Umbraco.Web.UI.Client/src/packages/core/collection/view/table/entity-name-table-column-layout.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/collection/view/table/entity-name-table-column-layout.element.ts
@@ -1,8 +1,9 @@
 import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
-import { html, LitElement, nothing, customElement, property } from '@umbraco-cms/backoffice/external/lit';
+import { html, nothing, customElement, property } from '@umbraco-cms/backoffice/external/lit';
+import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
 
 @customElement('umb-entity-name-table-column-layout')
-export class UmbEntityNameTableColumnLayoutElement extends LitElement {
+export class UmbEntityNameTableColumnLayoutElement extends UmbLitElement {
 	@property({ attribute: false })
 	value!: { name: string; href?: string };
 
