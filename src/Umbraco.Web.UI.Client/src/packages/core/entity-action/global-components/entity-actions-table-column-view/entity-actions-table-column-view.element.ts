aeb54c7b37a5eae74b9d8cf28c9e4fbffdd13a82
Mads Rasmussen <madsr@hey.com>
2026-04-08 11:01:07 +0200

    fix breaking change and introduce a deprecation warning

--- src/Umbraco.Web.UI.Client/src/packages/core/entity-action/global-components/entity-actions-table-column-view/entity-actions-table-column-view.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/entity-action/global-components/entity-actions-table-column-view/entity-actions-table-column-view.element.ts
@@ -1,6 +1,16 @@
+import type { UmbEntityModel, UmbNamedEntityModel } from '@umbraco-cms/backoffice/entity';
 import { html, customElement, property } from '@umbraco-cms/backoffice/external/lit';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
+import { UmbDeprecation } from '@umbraco-cms/backoffice/utils';
 import type { UmbTableColumn, UmbTableColumnLayoutElement, UmbTableItem } from '@umbraco-cms/backoffice/components';
+
+type UmbEntityActionsTableColumnValue = UmbEntityModel | UmbNamedEntityModel | { name?: string };
+
+const deprecation = new UmbDeprecation({
+	deprecated: 'Passing `entityType` and `unique` via the `value` property on `<umb-entity-actions-table-column-view>`.',
+	removeInVersion: '19',
+	solution: 'Provide the entity type and unique via the UMB_ENTITY_CONTEXT context instead.',
+});
 
 @customElement('umb-entity-actions-table-column-view')
 export class UmbEntityActionsTableColumnViewElement extends UmbLitElement implements UmbTableColumnLayoutElement {
@@ -11,12 +21,27 @@ export class UmbEntityActionsTableColumnViewElement extends UmbLitElement implem
 	item!: UmbTableItem;
 
 	@property({ attribute: false })
-	value: { name?: string } = {};
+	get value(): UmbEntityActionsTableColumnValue {
+		return this.#value;
+	}
+	set value(newValue: UmbEntityActionsTableColumnValue) {
+		this.#value = newValue ?? {};
+		if ('entityType' in this.#value || 'unique' in this.#value) {
+			deprecation.warn();
+		}
+	}
+	#value: UmbEntityActionsTableColumnValue = {};
 
 	override render() {
+		// TODO (v19): Remove deprecated property forwarding when entityType/unique on value is removed.
+		const entityType = 'entityType' in this.value ? this.value.entityType : undefined;
+		const unique = 'unique' in this.value ? this.value.unique : undefined;
+
 		return html`
 			<umb-entity-actions-bundle
-				.label=${this.localize.string(this.value?.name)}>
+				.entityType=${entityType}
+				.unique=${unique}
+				.label=${this.localize.string((this.value as UmbNamedEntityModel)?.name)}>
 			</umb-entity-actions-bundle>
 		`;
 	}
