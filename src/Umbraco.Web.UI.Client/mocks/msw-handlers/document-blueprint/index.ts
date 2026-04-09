67f43bc22e5d55a17d2b290212acfc704afa2189
Mads Rasmussen <madsr@hey.com>
2026-04-09 14:45:11 +0200

    manual merge

--- src/Umbraco.Web.UI.Client/mocks/msw-handlers/document-blueprint/index.ts
+++ src/Umbraco.Web.UI.Client/mocks/msw-handlers/document-blueprint/index.ts
@@ -1,6 +1,5 @@
 import { treeHandlers } from './tree.handlers.js';
 import { itemHandlers } from './item.handlers.js';
 import { detailHandlers } from './detail.handlers.js';
-import { structureHandlers } from './structure.handlers.js';
 
-export const handlers = [...treeHandlers, ...itemHandlers, ...structureHandlers, ...detailHandlers];
+export const handlers = [...treeHandlers, ...itemHandlers, ...detailHandlers];
