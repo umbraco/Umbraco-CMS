2cbe69d6eaae1af08e9f508fb10311d57728ee02
leekelleher <leekelleher@gmail.com>
2026-04-09 10:31:19 +0100

    Updated permission prefixes
    from `Umb.ElementFolder.` to match the server `Umb.ElementContainer.`

--- src/Umbraco.Web.UI.Client/src/packages/elements/folder/user-permissions/constants.ts
+++ src/Umbraco.Web.UI.Client/src/packages/elements/folder/user-permissions/constants.ts
@@ -1,7 +1,7 @@
-export const UMB_USER_PERMISSION_ELEMENT_FOLDER_CREATE = 'Umb.ElementFolder.Create';
-export const UMB_USER_PERMISSION_ELEMENT_FOLDER_DELETE = 'Umb.ElementFolder.Delete';
-export const UMB_USER_PERMISSION_ELEMENT_FOLDER_MOVE = 'Umb.ElementFolder.Move';
-export const UMB_USER_PERMISSION_ELEMENT_FOLDER_READ = 'Umb.ElementFolder.Read';
-export const UMB_USER_PERMISSION_ELEMENT_FOLDER_UPDATE = 'Umb.ElementFolder.Update';
+export const UMB_USER_PERMISSION_ELEMENT_FOLDER_CREATE = 'Umb.ElementContainer.Create';
+export const UMB_USER_PERMISSION_ELEMENT_FOLDER_DELETE = 'Umb.ElementContainer.Delete';
+export const UMB_USER_PERMISSION_ELEMENT_FOLDER_MOVE = 'Umb.ElementContainer.Move';
+export const UMB_USER_PERMISSION_ELEMENT_FOLDER_READ = 'Umb.ElementContainer.Read';
+export const UMB_USER_PERMISSION_ELEMENT_FOLDER_UPDATE = 'Umb.ElementContainer.Update';
 
 export * from './conditions/constants.js';
