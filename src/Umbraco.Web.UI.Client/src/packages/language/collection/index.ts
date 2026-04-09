fd544b4bc27ec0e0cd487db1442aeff3d3c56a59
Mads Rasmussen <madsr@hey.com>
2026-04-09 09:50:57 +0200

    Add language collection context

--- src/Umbraco.Web.UI.Client/src/packages/language/collection/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/language/collection/index.ts
@@ -1,2 +1,3 @@
+export { UmbLanguageCollectionContext } from './language-collection.context.js';
 export { UmbLanguageCollectionRepository } from './repository/index.js';
 export { UMB_LANGUAGE_COLLECTION_ALIAS } from './constants.js';
