d3a7e6b3c6270e4b67d047bfa9a9597cc904388d
Mads Rasmussen <madsr@hey.com>
2026-04-14 20:54:59 +0200

    format date the same way as the property editor

--- src/Umbraco.Web.UI.Client/src/packages/property-editors/date-time/date-time-with-time-zone-picker/value-summary/value-summary.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/property-editors/date-time/date-time-with-time-zone-picker/value-summary/value-summary.element.ts
@@ -1,11 +1,15 @@
 import type { UmbDateTimeWithTimeZonePropertyEditorValue } from '../types.js';
-import { customElement, html } from '@umbraco-cms/backoffice/external/lit';
+import { customElement, html, nothing } from '@umbraco-cms/backoffice/external/lit';
+import { DateTime } from '@umbraco-cms/backoffice/external/luxon';
 import { UmbValueSummaryElementBase } from '@umbraco-cms/backoffice/value-summary';
 
 @customElement('umb-date-time-with-time-zone-property-editor-value-summary')
 export class UmbDateTimeWithTimeZonePropertyEditorValueSummaryElement extends UmbValueSummaryElementBase<UmbDateTimeWithTimeZonePropertyEditorValue> {
 	override render() {
-		return html`<span>${this._value?.date} ${this._value?.timeZone}</span>`;
+		if (!this._value?.date || !this._value?.timeZone) return nothing;
+		// TODO: Replace Luxon with `Temporal.PlainDateTime.from(date).toZonedDateTime(timeZone).toLocaleString()` once Temporal is available in all browsers.
+		const localDate = DateTime.fromISO(this._value.date, { zone: this._value.timeZone }).toLocal();
+		return html`<span>${localDate.toFormat('ff')}</span>`;
 	}
 }
 
