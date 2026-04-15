1ec3708519aa1971604a4b1c243dcb6bc89dd94b
NillasKA <kramernicklas@gmail.com>
2026-04-15 11:08:30 +0200

    Migration added

--- src/Umbraco.Cms.Persistence.EFCore.Sqlite/SqliteMigrationProvider.cs
+++ src/Umbraco.Cms.Persistence.EFCore.Sqlite/SqliteMigrationProvider.cs
@@ -56,6 +56,7 @@ public class SqliteMigrationProvider : IMigrationProvider
             EFCoreMigration.AddKeyValueDto => typeof(Migrations.AddKeyValueDto),
             EFCoreMigration.SqliteCollation => typeof(Migrations.SqliteCollation),
             EFCoreMigration.AddLanguageDto => typeof(Migrations.AddLanguageDto),
+            EFCoreMigration.AddRedirectUrlDto => typeof(Migrations.AddRedirectUrlDto),
             _ => throw new ArgumentOutOfRangeException(nameof(migration), $@"Not expected migration value: {migration}")
         };
 }
