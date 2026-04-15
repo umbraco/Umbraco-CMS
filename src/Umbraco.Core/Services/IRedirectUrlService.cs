95cc67a549499bc7b3c3c7b4bec28956125ced5f
NillasKA <kramernicklas@gmail.com>
2026-04-15 09:29:43 +0200

    Making service async

--- src/Umbraco.Core/Services/IRedirectUrlService.cs
+++ src/Umbraco.Core/Services/IRedirectUrlService.cs
@@ -1,4 +1,3 @@
-using System.Threading.Tasks;
 using Umbraco.Cms.Core.Models;
 
 namespace Umbraco.Cms.Core.Services;
@@ -15,37 +14,37 @@ public interface IRedirectUrlService : IService
     /// <param name="contentKey">The content unique key.</param>
     /// <param name="culture">The culture.</param>
     /// <remarks>Is a proper Umbraco route eg /path/to/foo or 123/path/tofoo.</remarks>
-    void Register(string url, Guid contentKey, string? culture = null);
+    Task RegisterAsync(string url, Guid contentKey, string? culture = null);
 
     /// <summary>
     ///     Deletes all redirect URLs for a given content.
     /// </summary>
     /// <param name="contentKey">The content unique key.</param>
-    void DeleteContentRedirectUrls(Guid contentKey);
+    Task DeleteContentRedirectUrlsAsync(Guid contentKey);
 
     /// <summary>
     ///     Deletes a redirect URL.
     /// </summary>
     /// <param name="redirectUrl">The redirect URL to delete.</param>
-    void Delete(IRedirectUrl redirectUrl);
+    Task DeleteAsync(IRedirectUrl redirectUrl);
 
     /// <summary>
     ///     Deletes a redirect URL.
     /// </summary>
     /// <param name="id">The redirect URL identifier.</param>
-    void Delete(Guid id);
+    Task DeleteAsync(Guid id);
 
     /// <summary>
     ///     Deletes all redirect URLs.
     /// </summary>
-    void DeleteAll();
+    Task DeleteAllAsync();
 
     /// <summary>
     ///     Gets the most recent redirect URLs corresponding to an Umbraco redirect URL route.
     /// </summary>
     /// <param name="url">The Umbraco redirect URL route.</param>
     /// <returns>The most recent redirect URLs corresponding to the route.</returns>
-    IRedirectUrl? GetMostRecentRedirectUrl(string url);
+    Task<IRedirectUrl?> GetMostRecentRedirectUrlAsync(string url);
 
     /// <summary>
     ///     Gets the most recent redirect URLs corresponding to an Umbraco redirect URL route.
@@ -53,65 +52,53 @@ public interface IRedirectUrlService : IService
     /// <param name="url">The Umbraco redirect URL route.</param>
     /// <param name="culture">The culture of the request.</param>
     /// <returns>The most recent redirect URLs corresponding to the route.</returns>
-    IRedirectUrl? GetMostRecentRedirectUrl(string url, string? culture);
-
-    /// <summary>
-    /// Gets the most recent redirect URLs corresponding to an Umbraco redirect URL route.
-    /// </summary>
-    /// <param name="url">The Umbraco redirect URL route.</param>
-    /// <param name="culture">The culture of the request.</param>
-    /// <returns>The most recent redirect URLs corresponding to the route.</returns>
-    Task<IRedirectUrl?> GetMostRecentRedirectUrlAsync(string url, string? culture) => Task.FromResult(GetMostRecentRedirectUrl(url, culture));
+    Task<IRedirectUrl?> GetMostRecentRedirectUrlAsync(string url, string? culture);
 
         /// <summary>
     ///     Gets all redirect URLs for a content item.
     /// </summary>
     /// <param name="contentKey">The content unique key.</param>
     /// <returns>All redirect URLs for the content item.</returns>
-    IEnumerable<IRedirectUrl> GetContentRedirectUrls(Guid contentKey);
+    Task<IEnumerable<IRedirectUrl>> GetContentRedirectUrlsAsync(Guid contentKey);
 
     /// <summary>
     ///     Gets all redirect URLs.
     /// </summary>
-    /// <param name="pageIndex">The page index.</param>
+    /// <param name="pageIndex">The zero-based page index.</param>
     /// <param name="pageSize">The page size.</param>
-    /// <param name="total">The total count of redirect URLs.</param>
-    /// <returns>The redirect URLs.</returns>
-    IEnumerable<IRedirectUrl> GetAllRedirectUrls(long pageIndex, int pageSize, out long total);
+    /// <returns>A paged model containing the redirect URLs and the total count.</returns>
+    Task<PagedModel<IRedirectUrl>> GetAllRedirectUrlsAsync(long pageIndex, int pageSize);
 
     /// <summary>
     ///     Gets all redirect URLs.
     /// </summary>
     /// <param name="skip">Amount to skip.</param>
     /// <param name="take">Amount to take.</param>
-    /// <param name="total">The total count of redirect URLs.</param>
-    /// <returns>The redirect URLs.</returns>
-    IEnumerable<IRedirectUrl> GetAllRedirectUrls(int skip, int take, out long total)
+    /// <returns>A paged model containing the redirect URLs and the total count.</returns>
+    async Task<PagedModel<IRedirectUrl>> GetAllRedirectUrlsAsync(int skip, int take)
     {
         PaginationHelper.ConvertSkipTakeToPaging(skip, take, out var pageNumber, out var pageSize);
 
-        return GetAllRedirectUrls(pageNumber, pageSize, out total);
+        return await GetAllRedirectUrlsAsync(pageNumber, pageSize);
     }
 
     /// <summary>
     ///     Gets all redirect URLs below a given content item.
     /// </summary>
     /// <param name="rootContentId">The content unique identifier.</param>
-    /// <param name="pageIndex">The page index.</param>
+    /// <param name="pageIndex">The zero-based page index.</param>
     /// <param name="pageSize">The page size.</param>
-    /// <param name="total">The total count of redirect URLs.</param>
-    /// <returns>The redirect URLs.</returns>
-    IEnumerable<IRedirectUrl> GetAllRedirectUrls(int rootContentId, long pageIndex, int pageSize, out long total);
+    /// <returns>A paged model containing the redirect URLs and the total count.</returns>
+    Task<PagedModel<IRedirectUrl>> GetAllRedirectUrlsAsync(int rootContentId, long pageIndex, int pageSize);
 
     /// <summary>
     ///     Searches for all redirect URLs that contain a given search term in their URL property.
     /// </summary>
     /// <param name="searchTerm">The term to search for.</param>
-    /// <param name="pageIndex">The page index.</param>
+    /// <param name="pageIndex">The zero-based page index.</param>
     /// <param name="pageSize">The page size.</param>
-    /// <param name="total">The total count of redirect URLs.</param>
-    /// <returns>The redirect URLs.</returns>
-    IEnumerable<IRedirectUrl> SearchRedirectUrls(string searchTerm, long pageIndex, int pageSize, out long total);
+    /// <returns>A paged model containing the matching redirect URLs and the total count.</returns>
+    Task<PagedModel<IRedirectUrl>> SearchRedirectUrlsAsync(string searchTerm, long pageIndex, int pageSize);
 
     /// <summary>
     ///     Searches for all redirect URLs that contain a given search term in their URL property.
@@ -119,12 +106,11 @@ public interface IRedirectUrlService : IService
     /// <param name="searchTerm">The term to search for.</param>
     /// <param name="skip">Amount to skip.</param>
     /// <param name="take">Amount to take.</param>
-    /// <param name="total">The total count of redirect URLs.</param>
-    /// <returns>The redirect URLs.</returns>
-    IEnumerable<IRedirectUrl> SearchRedirectUrls(string searchTerm, int skip, int take, out long total)
+    /// <returns>A paged model containing the matching redirect URLs and the total count.</returns>
+    async Task<PagedModel<IRedirectUrl>> SearchRedirectUrlsAsync(string searchTerm, int skip, int take)
     {
         PaginationHelper.ConvertSkipTakeToPaging(skip, take, out var pageNumber, out var pageSize);
 
-        return SearchRedirectUrls(searchTerm, pageNumber, pageSize, out total);
+        return await SearchRedirectUrlsAsync(searchTerm, pageNumber, pageSize);
     }
 }
