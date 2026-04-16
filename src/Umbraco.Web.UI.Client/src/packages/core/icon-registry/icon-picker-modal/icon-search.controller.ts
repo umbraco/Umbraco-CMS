253cc0ee403c8962902648fce3cff0fe678f8859
Niels Lyngsø <niels.lyngso@gmail.com>
2026-04-16 10:48:14 +0200

    full icon name search if query contains a -

--- src/Umbraco.Web.UI.Client/src/packages/core/icon-registry/icon-picker-modal/icon-search.controller.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/icon-registry/icon-picker-modal/icon-search.controller.ts
@@ -124,7 +124,7 @@ export class UmbIconSearchController extends UmbControllerBase {
 
 		// 5+ letters match on the full icon name.
 		// e.g. "icon-bu" matches "icon-bug"
-		if (query.length > 5 && icon.name.startsWith(query)) {
+		if (query.includes('-') && query.length > query.indexOf('-') + 1 && icon.name.startsWith(query)) {
 			return 400;
 		}
 
