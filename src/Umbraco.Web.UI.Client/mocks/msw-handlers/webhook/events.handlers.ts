4d63e98dda8c12261bb812e791ac9ca590f3eb29
Mads Rasmussen <madsr@hey.com>
2026-04-16 21:30:40 +0200

    Support pagination in webhook events handler

--- src/Umbraco.Web.UI.Client/mocks/msw-handlers/webhook/events.handlers.ts
+++ src/Umbraco.Web.UI.Client/mocks/msw-handlers/webhook/events.handlers.ts
@@ -4,7 +4,10 @@ import { UMB_SLUG } from './slug.js';
 import { umbracoPath } from '@umbraco-cms/backoffice/utils';
 
 export const eventsHandlers = [
-	http.get(umbracoPath(`${UMB_SLUG}/events`), () => {
-		return HttpResponse.json(umbWebhookEventMockDb.getAll());
+	http.get(umbracoPath(`${UMB_SLUG}/events`), ({ request }) => {
+		const url = new URL(request.url);
+		const skip = Number(url.searchParams.get('skip')) || undefined;
+		const take = Number(url.searchParams.get('take')) || undefined;
+		return HttpResponse.json(umbWebhookEventMockDb.get({ skip, take }));
 	}),
 ];
