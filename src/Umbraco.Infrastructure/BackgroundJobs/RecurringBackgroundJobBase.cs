4182e858a873dc6b2917b8f8e8acc0752d5230fe
Ronald Barendse <ronald@barend.se>
2026-04-17 00:21:05 +0200

    Tidy RecurringBackgroundJobBase docs and runner error handling

--- src/Umbraco.Infrastructure/BackgroundJobs/RecurringBackgroundJobBase.cs
+++ src/Umbraco.Infrastructure/BackgroundJobs/RecurringBackgroundJobBase.cs
@@ -10,7 +10,20 @@ namespace Umbraco.Cms.Infrastructure.BackgroundJobs;
 /// </remarks>
 public abstract class RecurringBackgroundJobBase : IRecurringBackgroundJob
 {
+    /// <summary>
+    /// The default delay to use for recurring tasks for the first run after application start-up if no alternative is configured.
+    /// </summary>
+    /// <remarks>
+    /// The default of 3 minutes is chosen to allow the application to finish starting up and stabilize before the first execution of recurring tasks.
+    /// </remarks>
     protected internal static readonly TimeSpan DefaultDelay = TimeSpan.FromMinutes(3);
+
+    /// <summary>
+    /// The default server roles that recurring background jobs run on.
+    /// </summary>
+    /// <remarks>
+    /// The default of running on both <see cref="ServerRole.Single" /> and <see cref="ServerRole.SchedulingPublisher" /> is chosen to ensure recurring background jobs do not run on every server (in a load-balanced environment).
+    /// </remarks>
     protected internal static readonly ServerRole[] DefaultServerRoles = [ServerRole.Single, ServerRole.SchedulingPublisher];
 
     /// <inheritdoc />
