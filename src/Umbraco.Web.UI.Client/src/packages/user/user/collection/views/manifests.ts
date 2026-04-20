36bba6606f4b47716343e42ab4fcc312f7b16bbc
Mads Rasmussen <madsr@hey.com>
2026-04-20 11:22:06 +0200

    remove test registration

--- src/Umbraco.Web.UI.Client/src/packages/user/user/collection/views/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user/collection/views/manifests.ts
@@ -1,49 +1,18 @@
 import { UMB_USER_COLLECTION_ALIAS } from '../constants.js';
-import { UMB_COLLECTION_VIEW_USER_GRID, UMB_COLLECTION_VIEW_USER_TABLE } from './constants.js';
 import { UMB_USER_STATE_VALUE_TYPE } from '../../value-summary/constants.js';
-import { UMB_DATE_TIME_VALUE_TYPE } from '@umbraco-cms/backoffice/value-type';
 import { UMB_USER_GROUP_REFERENCES_VALUE_TYPE } from '../../../user-group/value-summary/constants.js';
+import { UMB_COLLECTION_VIEW_USER_GRID, UMB_COLLECTION_VIEW_USER_TABLE } from './constants.js';
+import { UMB_DATE_TIME_VALUE_TYPE } from '@umbraco-cms/backoffice/value-type';
 import { UMB_COLLECTION_ALIAS_CONDITION } from '@umbraco-cms/backoffice/collection';
 
 export const manifests: Array<UmbExtensionManifest> = [
 	{
 		type: 'collectionView',
+		kind: 'table',
 		alias: UMB_COLLECTION_VIEW_USER_TABLE,
 		name: 'User Table Collection View',
-		element: () => import('./table/user-table-collection-view.element.js'),
+		weight: 700,
 		meta: {
-			label: 'Table',
-			icon: 'icon-table',
-			pathName: 'table',
-		},
-		conditions: [
-			{
-				alias: UMB_COLLECTION_ALIAS_CONDITION,
-				match: UMB_USER_COLLECTION_ALIAS,
-			},
-		],
-	},
-	{
-		type: 'collectionView',
-		kind: 'card',
-		alias: UMB_COLLECTION_VIEW_USER_GRID,
-		name: 'User Grid Collection View',
-		conditions: [
-			{
-				alias: UMB_COLLECTION_ALIAS_CONDITION,
-				match: UMB_USER_COLLECTION_ALIAS,
-			},
-		],
-	},
-	{
-		type: 'collectionView',
-		kind: 'table',
-		alias: 'Umb.CollectionView.User.Table.Kind',
-		name: 'User Table Kind Collection View',
-		meta: {
-			label: 'Table',
-			icon: 'icon-table',
-			pathName: 'table2',
 			columns: [
 				{
 					field: 'userGroupUniques',
@@ -62,6 +31,18 @@ export const manifests: Array<UmbExtensionManifest> = [
 				},
 			],
 		},
+		conditions: [
+			{
+				alias: UMB_COLLECTION_ALIAS_CONDITION,
+				match: UMB_USER_COLLECTION_ALIAS,
+			},
+		],
+	},
+	{
+		type: 'collectionView',
+		kind: 'card',
+		alias: UMB_COLLECTION_VIEW_USER_GRID,
+		name: 'User Grid Collection View',
 		conditions: [
 			{
 				alias: UMB_COLLECTION_ALIAS_CONDITION,
