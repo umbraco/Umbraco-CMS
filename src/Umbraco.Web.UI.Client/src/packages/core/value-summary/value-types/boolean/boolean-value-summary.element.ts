5549cd7e58eb3ac9df560b2f7ae8bec9d1876db1
Mads Rasmussen <madsr@hey.com>
2026-04-21 20:01:06 +0200

    Skip render when boolean value is undefined

--- src/Umbraco.Web.UI.Client/src/packages/core/value-summary/value-types/boolean/boolean-value-summary.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/value-summary/value-types/boolean/boolean-value-summary.element.ts
@@ -1,9 +1,10 @@
 import { UmbValueSummaryElementBase } from '../../base/value-summary-element.base.js';
-import { customElement, html } from '@umbraco-cms/backoffice/external/lit';
+import { customElement, html, nothing } from '@umbraco-cms/backoffice/external/lit';
 
 @customElement('umb-boolean-value-summary')
 export class UmbBooleanValueSummaryElement extends UmbValueSummaryElementBase<boolean> {
 	override render() {
+		if (this._value === undefined) return nothing;
 		return html`${this._value === true
 			? html`<uui-icon name="icon-true"></uui-icon>`
 			: html`<uui-icon name="icon-false"></uui-icon>`}`;
