831593c7406fd7c2ff23b25d97a9fa726c9065dc
Niels Lyngsø <niels.lyngso@gmail.com>
2026-04-24 19:08:38 +0200

    remove `as const`

--- src/Umbraco.Web.UI.Client/src/packages/core/variant/variant-id.class.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/variant/variant-id.class.ts
@@ -7,7 +7,7 @@ export const UMB_INVARIANT_CULTURE = 'invariant';
  * The identifier is not specific for ContentType Variants, but is used for many type of identification of a culture and a segment. One case is any property of a ContentType can be resolved into a VariantId depending on their structural settings such as Vary by Culture and Vary by Segmentation.
  */
 export class UmbVariantId {
-	public static readonly INVARIANT = Object.freeze(new UmbVariantId(null, null)) as const;
+	public static readonly INVARIANT = Object.freeze(new UmbVariantId(null, null));
 
 	public static Create(variantData: UmbObjectWithVariantProperties): UmbVariantId {
 		return Object.freeze(new UmbVariantId(variantData.culture, variantData.segment));
