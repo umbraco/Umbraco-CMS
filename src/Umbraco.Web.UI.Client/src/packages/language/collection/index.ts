c8893cf423cc4169c2b37389a8c643f8a18c8744
Mads Rasmussen <madsr@hey.com>
2026-04-28 13:29:33 +0200

    Update index.ts

--- src/Umbraco.Web.UI.Client/src/packages/language/collection/index.ts
+++ src/Umbraco.Web.UI.Client/src/packages/language/collection/index.ts
@@ -1,3 +1,2 @@
-export { UmbLanguageCollectionContext } from './language-collection.context.js';
 export { UmbLanguageCollectionRepository } from './repository/index.js';
 export { UMB_LANGUAGE_COLLECTION_ALIAS } from './constants.js';
