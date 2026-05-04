69a21da7ae9fc5a85eb2eb1761a33e7b0d8c0446
Niels Lyngsø <niels.lyngso@gmail.com>
2026-05-04 15:13:32 +0200

    simpler comment

--- src/Umbraco.Web.UI.Client/src/packages/core/value-summary/extensions/value-summary-api.interface.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/value-summary/extensions/value-summary-api.interface.ts
@@ -2,9 +2,7 @@ import type { UmbApi } from '@umbraco-cms/backoffice/extension-api';
 import type { Observable } from '@umbraco-cms/backoffice/external/rxjs';
 
 /**
- * Per-element API for value summary extensions.
- * Set on `element.api` by `umb-extension-with-api-slot`.
- * Elements observe `value` to get the display value.
+ * Element API for value summary extensions.
  */
 export interface UmbValueSummaryApi extends UmbApi {
 	readonly value: Observable<unknown>;
