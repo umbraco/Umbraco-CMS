4cd83a3571ee6896d96d308395d56a2a9f076c55
Laura Neto <12862535+lauraneto@users.noreply.github.com>
2026-05-06 12:50:09 +0200

    Update tests/Umbraco.Tests.UnitTests/Umbraco.Core/Services/ElementPermissionServiceTests.cs

--- tests/Umbraco.Tests.UnitTests/Umbraco.Core/Services/ElementPermissionServiceTests.cs
+++ tests/Umbraco.Tests.UnitTests/Umbraco.Core/Services/ElementPermissionServiceTests.cs
@@ -90,7 +90,7 @@ public class ElementPermissionServiceTests
             [
                 new EntitySlim
                 {
-                    NodeObjectType = Constants.ObjectTypes.ElementContainer,
+                    NodeObjectType = Constants.ObjectTypes.Element,
                     Id = ElementNodeId,
                     Path = ElementNodePath,
                 }
