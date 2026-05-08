36274a203004e0768220c24451842dacff9d331f
Laura Neto <12862535+lauraneto@users.noreply.github.com>
2026-05-08 09:01:39 +0200

    Add helper for registering custom backoffice OpenAPI documents
    Bundles AddOpenApi, the [MapToApi]-aware ShouldInclude predicate, the
    Umbraco schema reference ID convention, and AddOpenApiDocumentToUi
    behind a single IUmbracoBuilder.AddBackOfficeOpenApiDocument call.
    Authors pass documentName, an optional title (used both as Info.Title
    and the UI dropdown label), and an optional configure callback that
    runs last so it can override anything the helper sets. An optional
    jsonOptionsName is forwarded to ReplaceOpenApiSchemaService for
    documents that need schema-time JSON serialization aligned to a named
    JsonOptions.
    
    Schema reference ID logic moves out of ConfigureUmbracoOpenApiOptionsBase
    into UmbracoSchemaIdGenerator.CreateSchemaReferenceId so both the new
    helper and the base class share one source of truth. The extension
    template's composer collapses to a single AddBackOfficeOpenApiDocument
    call, with document Info.Version, backoffice security, and the operation
    ID transformer staying in the configure callback.

--- src/Umbraco.Cms.Api.Common/OpenApi/UmbracoSchemaIdGenerator.cs
+++ src/Umbraco.Cms.Api.Common/OpenApi/UmbracoSchemaIdGenerator.cs
@@ -1,4 +1,6 @@
+using System.Text.Json.Serialization.Metadata;
 using System.Text.RegularExpressions;
+using Microsoft.AspNetCore.OpenApi;
 using Umbraco.Extensions;
 
 namespace Umbraco.Cms.Api.Common.OpenApi;
@@ -8,6 +10,32 @@ namespace Umbraco.Cms.Api.Common.OpenApi;
 /// </summary>
 public static class UmbracoSchemaIdGenerator
 {
+    /// <summary>
+    /// Creates a schema reference ID for the given JSON type info, applying Umbraco's naming conventions to
+    /// types in the <c>Umbraco.Cms</c> namespace and falling back to the framework default for other types.
+    /// </summary>
+    /// <param name="jsonTypeInfo">The JSON type info to create a schema reference ID for.</param>
+    /// <returns>The schema reference ID, or <c>null</c> if the type should be inlined.</returns>
+    internal static string? CreateSchemaReferenceId(JsonTypeInfo jsonTypeInfo)
+    {
+        // Ensure that only types that would normally be included in the schema generation are given a schema reference ID.
+        // Otherwise, we should return null to inline them.
+        var defaultSchemaReferenceId = OpenApiOptions.CreateDefaultSchemaReferenceId(jsonTypeInfo);
+        if (defaultSchemaReferenceId is null)
+        {
+            return null;
+        }
+
+        Type targetType = Nullable.GetUnderlyingType(jsonTypeInfo.Type) ?? jsonTypeInfo.Type;
+
+        if (targetType.Namespace?.StartsWith("Umbraco.Cms") is not true)
+        {
+            return defaultSchemaReferenceId;
+        }
+
+        return Generate(targetType);
+    }
+
     /// <summary>
     /// Generates a schema ID for the specified type following Umbraco's naming conventions.
     /// </summary>
--- templates/UmbracoExtension/Composers/UmbracoExtensionApiComposer.cs
+++ templates/UmbracoExtension/Composers/UmbracoExtensionApiComposer.cs
@@ -1,6 +1,3 @@
-using Microsoft.Extensions.DependencyInjection;
-using Microsoft.OpenApi;
-using Umbraco.Cms.Api.Common.Attributes;
 using Umbraco.Cms.Api.Common.DependencyInjection;
 using Umbraco.Cms.Api.Management.OpenApi;
 using Umbraco.Cms.Core.Composing;
@@ -11,46 +8,32 @@ namespace Umbraco.Extension.Composers;
 public class UmbracoExtensionApiComposer : IComposer
 {
     public void Compose(IUmbracoBuilder builder)
-    {
-        builder.Services.AddOpenApi(
+        => builder.AddBackOfficeOpenApiDocument(
             Constants.ApiName,
-            options =>
+            documentTitle: "Umbraco Extension Backoffice API",
+            configure: options =>
             {
                 // Related documentation:
                 // https://docs.umbraco.com/umbraco-cms/tutorials/creating-a-backoffice-api
                 // https://docs.umbraco.com/umbraco-cms/tutorials/creating-a-backoffice-api/adding-a-custom-openapi-document
                 // https://docs.umbraco.com/umbraco-cms/tutorials/creating-a-backoffice-api/versioning-your-api
                 // https://docs.umbraco.com/umbraco-cms/tutorials/creating-a-backoffice-api/access-policies
-
-                // Configure the OpenAPI generation options
-                // Add in a new OpenAPI document solely for our own package that can be browsed via the OpenAPI UI
-                // Along with having a generated OpenAPI JSON file that we can use to auto generate a TypeScript client
                 options.AddDocumentTransformer((document, _, _) =>
                 {
-                    document.Info = new OpenApiInfo
-                    {
-                        Title = "Umbraco ExtensionBackoffice API",
-                        Version = "1.0",
-                        // Contact = new OpenApiContact
-                        // {
-                        //     Name = "Some Developer",
-                        //     Email = "you@company.com",
-                        //     Url = new Uri("https://company.com")
-                        // }
-                    };
+                    document.Info.Version = "1.0";
+                    // document.Info.Contact = new OpenApiContact
+                    // {
+                    //     Name = "Some Developer",
+                    //     Email = "you@company.com",
+                    //     Url = new Uri("https://company.com")
+                    // };
                     return Task.CompletedTask;
                 });
 
-                // Only include endpoints decorated with the `MapToApi` attribute for our own API.
-                options.ShouldInclude = apiDescription =>
-                    apiDescription.ActionDescriptor.EndpointMetadata
-                        .OfType<MapToApiAttribute>()
-                        .Any(attribute => attribute.ApiName == Constants.ApiName);
-
-                // Enable Umbraco authentication for the "Example" OpenAPI document
+                // Enable Umbraco authentication for this OpenAPI document.
                 options.AddBackofficeSecurityRequirements();
 
-                // This is used to generate operation IDs in our OpenAPI JSON file so that the generated 
+                // This is used to generate operation IDs in our OpenAPI JSON file so that the generated
                 // TypeScript client has nice method names which are not too verbose.
                 // https://docs.umbraco.com/umbraco-cms/tutorials/creating-a-backoffice-api/umbraco-schema-and-operation-ids#operation-ids
                 options.AddOperationTransformer((operation, context, _) =>
@@ -59,7 +42,4 @@ public class UmbracoExtensionApiComposer : IComposer
                     return Task.CompletedTask;
                 });
             });
-
-        builder.Services.AddOpenApiDocumentToUi(Constants.ApiName, "Umbraco Extension Backoffice API");
-    }
 }
