bdddc40e83802f7b2e9b9b84a6a94d5e7845f30e
Laura Neto <12862535+lauraneto@users.noreply.github.com>
2026-05-11 18:35:37 +0200

    Remove redundant operation-id override from extension template
    UmbracoOperationIdTransformer is now part of the AddBackOfficeOpenApiDocument
    defaults, so the template's custom action-name transformer would only overwrite
    the work the default just did. Drop it, and consolidate the documentation
    pointer to a single link.

--- templates/UmbracoExtension/Composers/UmbracoExtensionApiComposer.cs
+++ templates/UmbracoExtension/Composers/UmbracoExtensionApiComposer.cs
@@ -7,19 +7,16 @@ namespace Umbraco.Extension.Composers;
 
 public class UmbracoExtensionApiComposer : IComposer
 {
-    public void Compose(IUmbracoBuilder builder)
-        => builder.AddBackOfficeOpenApiDocument(
+    public void Compose(IUmbracoBuilder builder) =>
+
+        // See https://docs.umbraco.com/umbraco-cms/tutorials/creating-a-backoffice-api (and its sub-pages) for
+        // guidance on customizing this document.
+        builder.AddBackOfficeOpenApiDocument(
             Constants.ApiName,
             document => document
                 .WithTitle("Umbraco Extension Backoffice API")
                 .WithBackOfficeAuthentication()
                 .ConfigureOpenApiOptions(options =>
-                {
-                    // Related documentation:
-                    // https://docs.umbraco.com/umbraco-cms/tutorials/creating-a-backoffice-api
-                    // https://docs.umbraco.com/umbraco-cms/tutorials/creating-a-backoffice-api/adding-a-custom-openapi-document
-                    // https://docs.umbraco.com/umbraco-cms/tutorials/creating-a-backoffice-api/versioning-your-api
-                    // https://docs.umbraco.com/umbraco-cms/tutorials/creating-a-backoffice-api/access-policies
                     options.AddDocumentTransformer((doc, _, _) =>
                     {
                         doc.Info.Version = "1.0";
@@ -30,15 +27,5 @@ public class UmbracoExtensionApiComposer : IComposer
                         //     Url = new Uri("https://company.com")
                         // };
                         return Task.CompletedTask;
-                    });
-
-                    // This is used to generate operation IDs in our OpenAPI JSON file so that the generated
-                    // TypeScript client has nice method names which are not too verbose.
-                    // https://docs.umbraco.com/umbraco-cms/tutorials/creating-a-backoffice-api/umbraco-schema-and-operation-ids#operation-ids
-                    options.AddOperationTransformer((operation, context, _) =>
-                    {
-                        operation.OperationId = $"{context.Description.ActionDescriptor.RouteValues["action"]}";
-                        return Task.CompletedTask;
-                    });
-                }));
+                    })));
 }
