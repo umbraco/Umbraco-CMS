cf68ed32cd70c44f5a49b539872f03b5b442f530
Mads Rasmussen <madsr@hey.com>
2026-05-13 14:06:09 +0200

    fix types

--- src/Umbraco.Web.UI.Client/src/packages/relations/relation-types/collection/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/relations/relation-types/collection/types.ts
@@ -1,7 +1,10 @@
 import type { UmbRelationTypeDetailModel } from '../types.js';
+import type { UmbRelationTypeEntityType } from '../entity.js';
 import type { UmbCollectionItemModel } from '@umbraco-cms/backoffice/collection';
 
-export interface UmbRelationTypeCollectionItemModel extends UmbRelationTypeDetailModel, UmbCollectionItemModel {}
+export interface UmbRelationTypeCollectionItemModel extends UmbRelationTypeDetailModel, UmbCollectionItemModel {
+	entityType: UmbRelationTypeEntityType;
+}
 
 export interface UmbRelationTypeCollectionFilterModel {
 	skip?: number;
