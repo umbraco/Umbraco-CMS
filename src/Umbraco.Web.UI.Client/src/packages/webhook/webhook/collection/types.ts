7bba5fc3ccf369af06ea4c3fc24feeb7d2fd20d9
Mads Rasmussen <madsr@hey.com>
2026-05-15 10:22:26 +0200

    Update types.ts

--- src/Umbraco.Web.UI.Client/src/packages/webhook/webhook/collection/types.ts
+++ src/Umbraco.Web.UI.Client/src/packages/webhook/webhook/collection/types.ts
@@ -1,7 +1,17 @@
-import type { UmbWebhookDetailModel } from '../types.js';
 import type { UmbCollectionItemModel } from '@umbraco-cms/backoffice/collection';
+import type { UmbWebhookEventModel } from '../../webhook-event/types.js';
+import type { UmbWebhookEntityType } from '../../entity.js';
 
-export interface UmbWebhookCollectionItemModel extends UmbWebhookDetailModel, UmbCollectionItemModel {}
+export interface UmbWebhookCollectionItemModel extends UmbCollectionItemModel {
+	entityType: UmbWebhookEntityType;
+	name: string;
+	enabled: boolean;
+	url: string;
+	description: string | null | undefined;
+	headers: Record<string, string>;
+	events: Array<UmbWebhookEventModel>;
+	contentTypes: Array<string>;
+}
 
 export interface UmbWebhookCollectionFilterModel {
 	skip?: number;
