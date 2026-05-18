b108f3ce3a95aabcb68002599771d925d6c99942
Zeegaan <skrivdetud@gmail.com>
2026-05-18 15:55:37 +0900

    Fix to key

--- src/Umbraco.Cms.Api.Management/Controllers/RelationType/ByKeyRelationTypeController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/RelationType/ByKeyRelationTypeController.cs
@@ -34,9 +34,9 @@ public class ByKeyRelationTypeController : RelationTypeControllerBase
     [ProducesResponseType(typeof(ProblemDetails), StatusCodes.Status404NotFound)]
     [EndpointSummary("Gets a relation type.")]
     [EndpointDescription("Gets a relation type identified by the provided Id.")]
-    public async Task<IActionResult> ByKey(CancellationToken cancellationToken, Guid id)
+    public async Task<IActionResult> ByKey(CancellationToken cancellationToken, Guid key)
     {
-        IRelationType? relationType = await _relationService.GetRelationTypeByKeyAsync(id, cancellationToken);
+        IRelationType? relationType = await _relationService.GetRelationTypeByKeyAsync(key, cancellationToken);
         if (relationType is null)
         {
             return RelationTypeNotFound();
