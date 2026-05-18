ca057fee84c0343e78ac97e10187a3552f653fbb
Ronald Barendse <ronald@barend.se>
2026-05-19 00:30:00 +0200

    Register IRecurringBackgroundJobTrigger as open generic and drop AddTriggerableRecurringBackgroundJob

--- src/Umbraco.Infrastructure/DependencyInjection/UmbracoBuilder.BackgroundJobs.cs
+++ src/Umbraco.Infrastructure/DependencyInjection/UmbracoBuilder.BackgroundJobs.cs
@@ -40,6 +40,7 @@ public static partial class UmbracoBuilderExtensions
         builder.Services.AddSingleton(RecurringBackgroundJobHostedService.CreateHostedServiceFactory);
         builder.Services.AddSingleton<RecurringBackgroundJobHostedServiceRunner>();
         builder.Services.AddHostedService(sp => sp.GetRequiredService<RecurringBackgroundJobHostedServiceRunner>());
+        builder.Services.AddSingleton(typeof(IRecurringBackgroundJobTrigger<>), typeof(RecurringBackgroundJobTrigger<>));
         builder.Services.AddHostedService<QueuedHostedService>();
         builder.AddNotificationAsyncHandler<PostRuntimePremigrationsUpgradeNotification, NavigationInitializationNotificationHandler>();
         builder.AddNotificationAsyncHandler<PostRuntimePremigrationsUpgradeNotification, PublishStatusInitializationNotificationHandler>();
