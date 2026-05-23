e06a583f1a22dc41a87371d3631d41927014059f
Mads Rasmussen <madsr@hey.com>
2026-05-23 10:09:03 +0200

    Backoffice: Embed package root manifests into umbraco-package.ts to reduce startup requests (#22957)
    * Consolidate block package into index export
    
    * keep umbraco-package.ts and embed manifests instead
    
    * Inline package manifests into umbraco-package
    
    * update docs

--- src/Umbraco.Web.UI.Client/src/packages/media/umbraco-package.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/umbraco-package.ts
@@ -1,4 +1,18 @@
+import { manifests as mediaManifests } from './media/manifests.js';
+import { manifests as mediaSectionManifests } from './media-section/manifests.js';
+import { manifests as mediaTypesManifests } from './media-types/manifests.js';
+import { manifests as imagingManifests } from './imaging/manifests.js';
+import { manifests as dropzoneManifests } from './dropzone/manifests.js';
+import type { UmbExtensionManifestKind } from '@umbraco-cms/backoffice/extension-registry';
 import * as entryPointModule from './entry-point.js';
+
+export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> = [
+	...mediaSectionManifests,
+	...mediaManifests,
+	...mediaTypesManifests,
+	...imagingManifests,
+	...dropzoneManifests,
+];
 
 export const name = 'Umbraco.Core.MediaManagement';
 export const extensions = [
@@ -6,7 +20,9 @@ export const extensions = [
 		name: 'Media Management Bundle',
 		alias: 'Umb.Bundle.MediaManagement',
 		type: 'bundle',
-		js: () => import('./manifests.js'),
+		js: {
+			manifests,
+		},
 	},
 	{
 		name: 'Media Management Entry Point',
