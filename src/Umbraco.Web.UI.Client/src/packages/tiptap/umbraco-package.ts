893bb61d0d0c5cae9c344613b9384e6c4c2c4c02
Mads Rasmussen <madsr@hey.com>
2026-05-23 10:09:03 +0200

    Backoffice: Embed package root manifests into umbraco-package.ts to reduce startup requests (#22957)
    * Consolidate block package into index export
    
    * keep umbraco-package.ts and embed manifests instead
    
    * Inline package manifests into umbraco-package
    
    * update docs

--- src/Umbraco.Web.UI.Client/src/packages/tiptap/umbraco-package.ts
+++ src/Umbraco.Web.UI.Client/src/packages/tiptap/umbraco-package.ts
@@ -1,9 +1,22 @@
+import { manifests as clipboardManifests } from './clipboard/manifests.js';
+import { manifests as tiptapExtensionManifests } from './extensions/manifests.js';
+import { manifests as propertyEditors } from './property-editors/manifests.js';
+import type { UmbExtensionManifestKind } from '@umbraco-cms/backoffice/extension-registry';
+
+export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> = [
+	...clipboardManifests,
+	...tiptapExtensionManifests,
+	...propertyEditors,
+];
+
 export const name = 'Umbraco.Core.Tiptap';
 export const extensions = [
 	{
 		name: 'Tiptap Bundle',
 		alias: 'Umb.Bundle.Tiptap',
 		type: 'bundle',
-		js: () => import('./manifests.js'),
+		js: {
+			manifests,
+		},
 	},
 ];
