f0013330e6c6280449a2d832775b31a9d98e2038
Mads Rasmussen <madsr@hey.com>
2026-05-23 10:09:03 +0200

    Backoffice: Embed package root manifests into umbraco-package.ts to reduce startup requests (#22957)
    * Consolidate block package into index export
    
    * keep umbraco-package.ts and embed manifests instead
    
    * Inline package manifests into umbraco-package
    
    * update docs

--- src/Umbraco.Web.UI.Client/src/packages/code-editor/umbraco-package.ts
+++ src/Umbraco.Web.UI.Client/src/packages/code-editor/umbraco-package.ts
@@ -1,9 +1,16 @@
+import { manifest as propertyEditorManifest } from './property-editor/manifests.js';
+import { manifests as codeEditorModalManifests } from './code-editor-modal/manifests.js';
+
+export const manifests: Array<UmbExtensionManifest> = [propertyEditorManifest, ...codeEditorModalManifests];
+
 export const name = 'Umbraco.CodeEditor';
 export const extensions = [
 	{
 		name: 'Umbraco Code Editor Bundle',
 		alias: 'Umb.Bundle.UmbracoCodeEditor',
 		type: 'bundle',
-		js: () => import('./manifests.js'),
+		js: {
+			manifests,
+		},
 	},
 ];
