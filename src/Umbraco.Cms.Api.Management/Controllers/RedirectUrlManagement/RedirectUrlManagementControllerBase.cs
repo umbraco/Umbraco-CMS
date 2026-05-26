554646562c4b83dfb887129f007dd05624bd34ca
NillasKA <kramernicklas@gmail.com>
2026-05-26 12:55:33 +0200

    Adding Operation Status Results to base controller.

--- src/Umbraco.Cms.Api.Management/Controllers/RedirectUrlManagement/RedirectUrlManagementControllerBase.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/RedirectUrlManagement/RedirectUrlManagementControllerBase.cs
@@ -1,6 +1,9 @@
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Umbraco.Cms.Api.Common.Builders;
 using Umbraco.Cms.Api.Management.Routing;
+using Umbraco.Cms.Core.Services.OperationStatus;
 using Umbraco.Cms.Web.Common.Authorization;
 
 namespace Umbraco.Cms.Api.Management.Controllers.RedirectUrlManagement;
@@ -14,4 +17,24 @@ namespace Umbraco.Cms.Api.Management.Controllers.RedirectUrlManagement;
 [Authorize(Policy = AuthorizationPolicies.SectionAccessContent)]
 public class RedirectUrlManagementControllerBase : ManagementApiControllerBase
 {
+    /// <summary>
+    /// Maps a <see cref="RedirectUrlOperationStatus"/> to an appropriate <see cref="IActionResult"/>.
+    /// </summary>
+    /// <param name="status">The operation status to map.</param>
+    /// <returns>An <see cref="IActionResult"/> describing the outcome of the operation.</returns>
+    protected IActionResult RedirectUrlOperationStatusResult(RedirectUrlOperationStatus status) =>
+        status switch
+        {
+            RedirectUrlOperationStatus.Success => Ok(),
+            RedirectUrlOperationStatus.NotFound => NotFound(new ProblemDetailsBuilder()
+                .WithTitle("The redirect URL could not be found")
+                .Build()),
+            RedirectUrlOperationStatus.CancelledByNotification => BadRequest(new ProblemDetailsBuilder()
+                .WithTitle("Cancelled by notification")
+                .WithDetail("A notification handler prevented the redirect URL operation.")
+                .Build()),
+            _ => StatusCode(StatusCodes.Status500InternalServerError, new ProblemDetailsBuilder()
+                .WithTitle("Unknown redirect URL operation status.")
+                .Build()),
+        };
 }
