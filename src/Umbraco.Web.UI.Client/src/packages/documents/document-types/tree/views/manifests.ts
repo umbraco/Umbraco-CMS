3a1c6940cb7241666940bcd4dac7906953434b68
Mads Rasmussen <madsr@hey.com>
2026-06-02 13:16:55 +0200

    Add Document Type table tree view manifest

--- src/Umbraco.Web.UI.Client/src/packages/documents/document-types/tree/views/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/document-types/tree/views/manifests.ts
@@ -1,4 +1,5 @@
 import { UMB_DOCUMENT_TYPE_TREE_ALIAS } from '../constants.js';
+import { UMB_BOOLEAN_VALUE_TYPE } from '@umbraco-cms/backoffice/value-type';
 import type { UmbExtensionManifestKind } from '@umbraco-cms/backoffice/extension-registry';
 
 export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> = [
@@ -15,5 +16,21 @@ export const manifests: Array<UmbExtensionManifest | UmbExtensionManifestKind> =
 		alias: 'Umb.TreeView.DocumentType.Card',
 		name: 'Document Type Card Tree View',
 		forTrees: [UMB_DOCUMENT_TYPE_TREE_ALIAS],
+	},
+	{
+		type: 'treeView',
+		kind: 'table',
+		alias: 'Umb.TreeView.DocumentType.Table',
+		name: 'Document Type Table Tree View',
+		forTrees: [UMB_DOCUMENT_TYPE_TREE_ALIAS],
+		meta: {
+			columns: [
+				{
+					field: 'isElement',
+					label: '#contentTypeEditor_elementType',
+					valueType: UMB_BOOLEAN_VALUE_TYPE,
+				},
+			],
+		},
 	},
 ];
