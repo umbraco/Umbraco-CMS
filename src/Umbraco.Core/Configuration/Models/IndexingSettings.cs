0400328ec178735c8150b74a26fbf0a0208340df
Laura Neto <12862535+lauraneto@users.noreply.github.com>
2026-06-17 16:33:01 +0200

    Add IndexSharedElements indexing setting

--- src/Umbraco.Core/Configuration/Models/IndexingSettings.cs
+++ src/Umbraco.Core/Configuration/Models/IndexingSettings.cs
@@ -9,6 +9,7 @@ namespace Umbraco.Cms.Core.Configuration.Models;
 public class IndexingSettings
 {
     private const bool StaticExplicitlyIndexEachNestedProperty = false;
+    private const bool StaticIndexSharedElements = false;
     private const int StaticBatchSize = 10000;
 
     /// <summary>
@@ -16,6 +17,12 @@ public class IndexingSettings
     /// </summary>
     [DefaultValue(StaticExplicitlyIndexEachNestedProperty)]
     public bool ExplicitlyIndexEachNestedProperty { get; set; } = StaticExplicitlyIndexEachNestedProperty;
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the content of shared elements referenced by block editors is flattened into the index entry of referencing documents. Requires a rebuild of indexes when changed.
+    /// </summary>
+    [DefaultValue(StaticIndexSharedElements)]
+    public bool IndexSharedElements { get; set; } = StaticIndexSharedElements;
 
     /// <summary>
     /// Gets or sets a value for how many items to index at a time.
