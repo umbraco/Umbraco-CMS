7c1b907410335ff079bfc9550c9c56a022b7db84
Niels Lyngsø <nsl@umbraco.dk>
2026-06-19 13:26:18 +0200

    Block Workspace: Support variant properties and make '$settings' a key-value-object (Closes #23095) (#23123)
    * map settings to become a key-value-object
    
    * implement type safety for block label ufm values
    
    * added TODOs
    
    * support variant value in Block Workspace Label

--- src/Umbraco.Web.UI.Client/src/packages/block/block-list/components/inline-list-block/inline-list-block.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/block/block-list/components/inline-list-block/inline-list-block.element.ts
@@ -12,7 +12,11 @@ import { UmbLanguageItemRepository } from '@umbraco-cms/backoffice/language';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
 import { UmbTextStyles } from '@umbraco-cms/backoffice/style';
 import type { UmbApiConstructorArgumentsMethodType } from '@umbraco-cms/backoffice/extension-api';
-import type { UmbBlockDataType, UMB_BLOCK_WORKSPACE_CONTEXT } from '@umbraco-cms/backoffice/block';
+import type {
+	UmbBlockDataType,
+	UMB_BLOCK_WORKSPACE_CONTEXT,
+	UmbBlockLabelUfmValueType,
+} from '@umbraco-cms/backoffice/block';
 
 import '../../../block/workspace/views/edit/block-workspace-view-edit-content-no-router.element.js';
 import { UmbContextBoundary } from '@umbraco-cms/backoffice/context-api';
@@ -186,7 +190,7 @@ export class UmbInlineListBlockElement extends UmbLitElement {
 	}
 
 	#renderBlockInfo() {
-		const blockValue = { ...this.content, $settings: this.settings, $index: this.index };
+		const blockValue: UmbBlockLabelUfmValueType = { ...this.content, $settings: this.settings, $index: this.index };
 		return html`
 			<span id="content">
 				<span id="icon">
