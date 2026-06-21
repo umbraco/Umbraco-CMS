e5ee2fdb8c158955d2ab1dd5b419c6668f78e918
Bjarne Fyrstenborg <bjarne_fyrstenborg@hotmail.com>
2026-06-22 00:38:52 +0200

    Change max validation message type from boolean to string

--- src/Umbraco.Web.UI.Client/src/packages/multi-url-picker/components/input-multi-url/input-multi-url.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/multi-url-picker/components/input-multi-url/input-multi-url.element.ts
@@ -87,7 +87,7 @@ export class UmbInputMultiUrlElement extends UmbFormControlMixin<string, typeof 
 
 	/**
 	 * Max validation message.
-	 * @type {boolean}
+	 * @type {string}
 	 * @attr
 	 * @default
 	 */
