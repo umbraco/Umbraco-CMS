bb8877e60f982026266ae59a771b188da2d47bc3
leekelleher <leekelleher@gmail.com>
2026-06-29 18:49:54 +0100

    deprecate(variant): add deprecation wrappers for sortVariants in member and document-blueprint utils

--- src/Umbraco.Web.UI.Client/src/packages/documents/document-blueprints/utils.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/document-blueprints/utils.ts
@@ -1,16 +1,22 @@
+import { sortVariants as _sortVariants } from '@umbraco-cms/backoffice/variant';
+import { UmbDeprecation } from '@umbraco-cms/backoffice/utils';
 import type { UmbDocumentBlueprintVariantOptionModel } from './types.js';
 
 type VariantType = UmbDocumentBlueprintVariantOptionModel;
 
-export const sortVariants = (a: VariantType, b: VariantType) => {
-	const compareDefault = (a: VariantType, b: VariantType) =>
-		(a.language?.isDefault ? -1 : 1) - (b.language?.isDefault ? -1 : 1);
+/**
+ * Sorts document blueprint variants based on multiple criteria:
+ * @param {VariantType} a - First variant to compare
+ * @param {VariantType} b - Second variant to compare
+ * @returns {number} - Sorting value
+ * @deprecated Deprecated since v17.6. Import `sortVariants` from `@umbraco-cms/backoffice/variant` instead. Scheduled for removal in Umbraco 19.
+ */
+export function sortVariants(a: VariantType, b: VariantType): number {
+	new UmbDeprecation({
+		deprecated: 'sortVariants from @umbraco-cms/backoffice/document-blueprint',
+		removeInVersion: '19.0.0',
+		solution: 'Import `sortVariants` from `@umbraco-cms/backoffice/variant` instead.',
+	}).warn();
 
-	// Make sure mandatory variants goes on top.
-	const compareMandatory = (a: VariantType, b: VariantType) =>
-		(a.language?.isMandatory ? -1 : 1) - (b.language?.isMandatory ? -1 : 1);
-
-	const compareName = (a: VariantType, b: VariantType) => a.variant?.name.localeCompare(b.variant?.name || '') || 99;
-
-	return compareDefault(a, b) || compareMandatory(a, b) || compareName(a, b);
-};
+	return _sortVariants(a, b);
+}
