ee8dc278d34571c262bbc8e53b16af8263bc5ba3
leekelleher <leekelleher@gmail.com>
2026-06-29 18:28:29 +0100

    deprecate(variant): add deprecation wrapper for sortVariants in documents/utils.ts

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/utils.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/utils.ts
@@ -1,1 +1,22 @@
-export { sortVariants } from '@umbraco-cms/backoffice/variant';
+import { sortVariants as _sortVariants } from '@umbraco-cms/backoffice/variant';
+import { UmbDeprecation } from '@umbraco-cms/backoffice/utils';
+import type { UmbDocumentVariantOptionModel } from './types.js';
+
+type VariantType = UmbDocumentVariantOptionModel;
+
+/**
+ * Sorts document variants based on multiple criteria:
+ * @param {VariantType} a - First variant to compare
+ * @param {VariantType} b - Second variant to compare
+ * @returns {number} - Sorting value
+ * @deprecated Deprecated since v17.6. Import `sortVariants` from `@umbraco-cms/backoffice/variant` instead. Scheduled for removal in Umbraco 19. [LK]
+ */
+export function sortVariants(a: VariantType, b: VariantType): number {
+	new UmbDeprecation({
+		deprecated: 'sortVariants from @umbraco-cms/backoffice/documents',
+		removeInVersion: '19.0.0',
+		solution: 'Import `sortVariants` from `@umbraco-cms/backoffice/variant` instead.',
+	}).warn();
+
+	return _sortVariants(a, b);
+}
