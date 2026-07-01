64877e8471a8b87f26dbd33eb6fe07ed608a6acb
Niels Lyngsø <niels.lyngso@gmail.com>
2026-07-01 14:11:31 +0200

    remove null check

--- src/Umbraco.Web.UI.Client/src/packages/core/property/property-value-preset/property-value-preset-builder.controller.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/property/property-value-preset/property-value-preset-builder.controller.ts
@@ -124,7 +124,8 @@ export class UmbPropertyValuePresetBuilderController<
 		// Only process value if it is `undefined`, if a property has a value we do not want to process it. [NL]
 		// TODO: For v.19, we should run presets for all value always. Make it up to the preset to decide if it should process the value or not. [NL]
 		// Consider how we should communicate such change, as some might have build their own presets that rely on the current behavior. [NL]
-		if (value === undefined || value === null) {
+		// notice the important difference between `undefined` and `null` here, as `null` is a valid value for a property, while `undefined` means that the property has no value. [NL]
+		if (value === undefined) {
 			const callArgs: UmbPropertyValuePresetApiCallArgs = {
 				...this.#baseCreateArgs!,
 				alias: propertyType.alias,
