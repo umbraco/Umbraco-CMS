40a590aa99a5840ab4cbf8eea5ba77b72b3b0d75
Niels Lyngsø <niels.lyngso@gmail.com>
2026-07-07 15:31:05 +0200

    refactor for shadowdom compatible solution

--- src/Umbraco.Web.UI.Client/src/apps/app/outline-style.controller.ts
+++ src/Umbraco.Web.UI.Client/src/apps/app/outline-style.controller.ts
@@ -1,39 +1,38 @@
 import { UmbControllerBase } from '@umbraco-cms/backoffice/class-api';
 
 export class UmbOutlineStyleController extends UmbControllerBase {
-	#hadMouseDown = false;
+	#hideFocusOutline = false;
 
 	override hostConnected(): void {
 		super.hostConnected();
 
 		const host = this.getHostElement();
-		host.addEventListener('focusout', this.#onFocusOut);
-		host.addEventListener('mousedown', this.#onMouseDown);
-		host.addEventListener('mouseup', this.#onMouseUp);
+		host.addEventListener('pointerdown', this.#onPointerDown);
+		host.addEventListener('keydown', this.#onKeyDown as EventListener, { capture: true });
 	}
 
-	#onFocusOut = () => {
-		if (this.#hadMouseDown === false) {
-			document.body.style.removeProperty('--uui-show-focus-outline');
+	#onPointerDown = () => {
+		if (!this.#hideFocusOutline) {
+			document.body.style.setProperty('--uui-show-focus-outline', '0');
+			this.#hideFocusOutline = true;
 		}
-		this.#hadMouseDown = false;
 	};
+	#onKeyDown = (e: KeyboardEvent) => {
+		// If tab or shift tab is pressed, we want to show the focus outline, but only if the last interaction was not a pointer event.
+		if (e.key !== 'Tab') return;
 
-	#onMouseDown = () => {
-		document.body.style.setProperty('--uui-show-focus-outline', '0');
-		this.#hadMouseDown = true;
-	};
-
-	#onMouseUp = () => {
-		this.#hadMouseDown = false;
+		// If the last interaction was a pointer event, we don't want to show the focus outline.
+		if (this.#hideFocusOutline) {
+			document.body.style.removeProperty('--uui-show-focus-outline');
+			this.#hideFocusOutline = false;
+		}
 	};
 
 	override hostDisconnected(): void {
 		super.hostDisconnected();
 
 		const host = this.getHostElement();
-		host.removeEventListener('focusout', this.#onFocusOut);
-		host.removeEventListener('mousedown', this.#onMouseDown);
-		host.removeEventListener('mouseup', this.#onMouseUp);
+		host.removeEventListener('pointerdown', this.#onPointerDown);
+		host.removeEventListener('keydown', this.#onKeyDown as EventListener, { capture: true });
 	}
 }
