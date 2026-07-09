0f2aab7eb89ed0f27148699e3088f6ddeff70c6c
Dirk Seefeld <dirk.seefeld@idseefeld.de>
2026-07-09 19:47:52 +0200

    improve comments

--- src/Umbraco.Cms.Api.Management/Controllers/User/Current/GetElementPermissionsCurrentUserController.cs
+++ src/Umbraco.Cms.Api.Management/Controllers/User/Current/GetElementPermissionsCurrentUserController.cs
@@ -27,6 +27,7 @@ public class GetElementPermissionsCurrentUserController : CurrentUserControllerB
     /// Initializes a new instance of the <see cref="GetElementPermissionsCurrentUserController"/> class, which handles requests related to retrieving element permissions for the current user.
     /// </summary>
     /// <param name="backOfficeSecurityAccessor">Provides access to back office security information for the current user.</param>
+    /// <param name="userService">Service for managing and retrieving user information. Unused and only for backward compatibility.</param>
     /// <param name="mapper">The Umbraco object mapper used for mapping between models.</param>
     /// <param name="elementPermissionService">Service for managing element permissions.</param>
     [ActivatorUtilitiesConstructor]
@@ -47,7 +48,7 @@ public class GetElementPermissionsCurrentUserController : CurrentUserControllerB
     /// <param name="backOfficeSecurityAccessor">Provides access to back office security information for the current user.</param>
     /// <param name="userService">Service for managing and retrieving user information.</param>
     /// <param name="mapper">The Umbraco object mapper used for mapping between models.</param>
-    [Obsolete("Please use the constructor without IUserService. Scheduled for removal in Umbraco 19.")]
+    [Obsolete("Please use the constructor with all parameters. Scheduled for removal in Umbraco 19.")]
     public GetElementPermissionsCurrentUserController(
         IBackOfficeSecurityAccessor backOfficeSecurityAccessor,
         IUserService userService,
