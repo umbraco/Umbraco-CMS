7023227dfe2dc38ed1812622c637a3de76d2bdc8
Mads Rasmussen <madsr@hey.com>
2026-08-04 14:13:50 +0200

    Sync collection filter field value

--- src/Umbraco.Web.UI.Client/src/packages/core/collection/components/collection-filter-field.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/collection/components/collection-filter-field.element.ts
@@ -1,23 +1,38 @@
 import { UMB_COLLECTION_CONTEXT } from '../default/index.js';
-import { css, customElement, html } from '@umbraco-cms/backoffice/external/lit';
+import { css, customElement, html, state } from '@umbraco-cms/backoffice/external/lit';
 import { debounce } from '@umbraco-cms/backoffice/utils';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
 
 @customElement('umb-collection-filter-field')
 export class UmbCollectionFilterFieldElement extends UmbLitElement {
+	@state()
+	private _value = '';
+
 	#collectionContext?: typeof UMB_COLLECTION_CONTEXT.TYPE;
+	#hasUserInput = false;
 
 	constructor() {
 		super();
 
 		this.consumeContext(UMB_COLLECTION_CONTEXT, (context) => {
 			this.#collectionContext = context;
+
+			// Sync filter if the value is updated directly in the context, ex. when the filter is restored from memory.
+			this.observe(
+				context?.filter,
+				(filter) => {
+					if (this.#hasUserInput) return;
+					this._value = (filter as { filter?: string } | undefined)?.filter ?? '';
+				},
+				'umbCollectionFilterFieldValueObserver',
+			);
 		});
 	}
 
 	#debouncedFilter = debounce((filter: string) => this.#collectionContext?.setFilter({ filter }), 500);
 
 	#onInput(event: InputEvent & { target: HTMLInputElement }) {
+		this.#hasUserInput = true;
 		const filter = event.target.value ?? '';
 		this.#debouncedFilter(filter);
 	}
@@ -28,6 +43,7 @@ export class UmbCollectionFilterFieldElement extends UmbLitElement {
 				label=${this.localize.term('general_filter')}
 				placeholder=${this.localize.term('placeholders_filter')}
 				data-mark="input:filter"
+				.value=${this._value}
 				@input=${this.#onInput}></uui-input>
 		`;
 	}
