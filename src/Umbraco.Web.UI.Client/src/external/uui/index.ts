e778fbd12f7a0125c7378284695f50f8fafff91c
Niels Lyngsø <nsl@umbraco.dk>
2026-08-11 10:51:59 +0200

    Hotfix: export a Backoffice version of demandCustomElement (#23624)
    * export a Backoffice version of demandCustomElement for backwards compatibility with UUI v.1
    
    * rename func JS docs
    
    * add @deprecated

--- src/Umbraco.Web.UI.Client/src/external/uui/index.ts
+++ src/Umbraco.Web.UI.Client/src/external/uui/index.ts
@@ -14,3 +14,28 @@ declare global {
 		disabled?: boolean;
 	}
 }
+
+/**
+ * @deprecated
+ * For v.17 we need to keep this function around for backwards compatibility with the legacy UUI 1.x custom elements.
+ * TODO: To be removed in v.18.
+ * Fire a warning if the custom element with the provided name isn't available.
+ * @func demandCustomElement
+ * @param {HTMLElement} requester - Reference to the element requiring this custom element..
+ * @param {string} elementName - Tag name of the required custom element.
+ * @param {string} message - Optional message describing the consequences of it not begin available.
+ */
+export const demandCustomElement = (
+	requester: HTMLElement,
+	elementName: string,
+	message: string = `This element has to be present for ${requester.nodeName} to work appropriate.`,
+) => {
+	if (!customElements.get(elementName)) {
+		console.warn(
+			`%c ${requester.nodeName} requires ${elementName} element to be registered!`,
+			'font-weight: bold;',
+			message,
+			requester,
+		);
+	}
+};
