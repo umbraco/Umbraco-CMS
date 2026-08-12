3db8101c633f15f25af7aac71b9ffe9a73b70077
kjac <kja@umbraco.dk>
2026-08-12 10:09:53 +0200

    Reorder the migrations

--- src/Umbraco.Infrastructure/Migrations/Upgrade/UmbracoPlan.cs
+++ src/Umbraco.Infrastructure/Migrations/Upgrade/UmbracoPlan.cs
@@ -118,8 +118,8 @@ public partial class UmbracoPlan : MigrationPlan
         To<V_18_1_0.AddContentTypeIdIndexForContent>("{AE533AF6-4611-4E25-AA4D-89AEFA468E79}");
 
         // To 19.0.0
-        To<V_19_0_0.AddIndexDocumentTable>("{8A5C1B2E-4F6D-4E1A-9C3B-D2E7F0A16C54}");
         To<V_19_0_0.AddExternalBlockElementRelationType>("{2D8F1B6E-4C3A-4E7D-9A1B-5F0C7E2D8A93}");
+        To<V_19_0_0.AddIndexDocumentTable>("{8A5C1B2E-4F6D-4E1A-9C3B-D2E7F0A16C54}");
         To<V_19_0_0.RemoveLegacyExamineIndexFiles>("{6F1A9C3D-2B4E-4F7A-8C1D-9E3F5A7B2C60}");
     }
 
