49339034ce235709880b97d140c45c55143a0546
kjac <kja@umbraco.dk>
2026-08-12 10:09:37 +0200

    Add (and obsolete) index alias constants to match the search package

--- src/Umbraco.Cms.Search.Core/Constants.cs
+++ src/Umbraco.Cms.Search.Core/Constants.cs
@@ -1,7 +1,33 @@
 ﻿namespace Umbraco.Cms.Search.Core;
 
+using CoreConstants = Umbraco.Cms.Core.Constants;
+
 public static class Constants
 {
+    [Obsolete("Please use CoreConstants.IndexAliases instead. Scheduled for removal in Umbraco 21.")]
+    public static class IndexAliases
+    {
+        /// <summary>
+        ///     The alias of the published content index.
+        /// </summary>
+        public const string PublishedContent = CoreConstants.IndexAliases.PublishedContent;
+
+        /// <summary>
+        ///     The alias of the draft content index.
+        /// </summary>
+        public const string DraftContent = CoreConstants.IndexAliases.DraftContent;
+
+        /// <summary>
+        ///     The alias of the draft media index.
+        /// </summary>
+        public const string DraftMedia = CoreConstants.IndexAliases.DraftMedia;
+
+        /// <summary>
+        ///     The alias of the draft members index.
+        /// </summary>
+        public const string DraftMembers = CoreConstants.IndexAliases.DraftMembers;
+    }
+
     public static class FieldNames
     {
         private const string FieldPrefix = "Umb_";
