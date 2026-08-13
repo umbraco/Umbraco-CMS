426f428992f738c34f2dab26fa17077d01d3c089
Niels Lyngsø <niels.lyngso@gmail.com>
2026-08-13 16:54:34 +0200

    ESLint fix

--- src/Umbraco.Web.UI.Client/src/packages/content/content-type/structure/content-type-structure-manager.class.ts
+++ src/Umbraco.Web.UI.Client/src/packages/content/content-type/structure/content-type-structure-manager.class.ts
@@ -6,8 +6,9 @@ import type {
 	UmbPropertyTypeModel,
 } from '../types.js';
 
+// eslint-disable-next-line @typescript-eslint/no-unused-vars -- referenced only via {@link} in JSDoc below
 import type { UmbContentTypePropertyStructureHelper } from './content-type-property-structure-helper.class.js';
-
+// eslint-disable-next-line @typescript-eslint/no-unused-vars -- referenced only via {@link} in JSDoc below
 import type { UmbContentTypeContainerStructureHelper } from './content-type-container-structure-helper.class.js';
 import {
 	UmbRepositoryDetailsManager,
