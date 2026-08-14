ee51b5c37f71cc74f0f09850abd736c7ebcb137c
Andy Butland <abutland73@gmail.com>
2026-08-14 12:19:51 +0100

    Correct filtering of rollback version for segmented content.

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/rollback/modal/rollback-modal.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/rollback/modal/rollback-modal.element.ts
@@ -104,7 +104,9 @@ export class UmbRollbackModalElement extends UmbModalBaseElement<UmbRollbackModa
 			this.#currentDocument = data;
 			const itemVariants = this.#currentDocument?.variants ?? [];
 
-			this._isInvariant = itemVariants.length === 1 && new UmbVariantId(itemVariants[0].culture).isInvariant();
+			// A document is culture invariant when none of its variants carry a culture. It may still hold
+			// several variants, as any segment adds one.
+			this._isInvariant = itemVariants.every((variant) => UmbVariantId.Create(variant).isCultureInvariant());
 			this.#selectCulture();
 
 			const cultures = itemVariants.map((x) => x.culture).filter((x) => x !== null) as string[];
