8c895c8de31ff2e9d871ec1f9130b723cde06bdc
leekelleher <leekelleher@gmail.com>
2026-08-18 17:52:58 +0100

    Linting

--- src/Umbraco.Web.UI.Client/src/packages/relations/relations/reference/modal/constants.ts
+++ src/Umbraco.Web.UI.Client/src/packages/relations/relations/reference/modal/constants.ts
@@ -3,11 +3,11 @@ import { UmbModalToken } from '@umbraco-cms/backoffice/modal';
 
 export const UMB_ENTITY_REFERENCES_MODAL_ALIAS = 'Umb.Modal.EntityReferences';
 
-export const UMB_ENTITY_REFERENCES_MODAL = new UmbModalToken<UmbEntityReferencesModalData, UmbEntityReferencesModalValue>(
-	UMB_ENTITY_REFERENCES_MODAL_ALIAS,
-	{
-		modal: {
-			type: 'dialog',
-		},
+export const UMB_ENTITY_REFERENCES_MODAL = new UmbModalToken<
+	UmbEntityReferencesModalData,
+	UmbEntityReferencesModalValue
+>(UMB_ENTITY_REFERENCES_MODAL_ALIAS, {
+	modal: {
+		type: 'dialog',
 	},
-);
+});
