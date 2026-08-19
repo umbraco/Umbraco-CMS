d196a8ef61b8668b75fd2554216e523077f8aaee
Niels Lyngsø <niels.lyngso@gmail.com>
2026-08-19 12:53:21 +0200

    help rendering, avoid temprorary showing the wrong variant selector

--- src/Umbraco.Web.UI.Client/src/packages/core/workspace/components/workspace-split-view/workspace-split-view.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/workspace/components/workspace-split-view/workspace-split-view.element.ts
@@ -38,6 +38,10 @@ export class UmbWorkspaceSplitViewElement extends UmbLitElement {
 	@property({ attribute: false })
 	public overrides?: Array<UmbDeepPartialObject<ManifestWorkspaceView>>;
 
+	// TODO: Verify that this is needed for this fix. [NL]
+	@property({ attribute: false })
+	public hasCustomVariantSelector?: boolean;
+
 	@property({ type: Number })
 	public set splitViewIndex(index: number) {
 		this.splitViewContext.setSplitViewIndex(index);
@@ -105,7 +109,7 @@ export class UmbWorkspaceSplitViewElement extends UmbLitElement {
 				<slot id="icon" name="icon" slot="header"></slot>
 				<slot id="header" name="variant-selector" slot="header" @slotchange=${this.#onVariantSelectorSlotChanged}>
 					${when(
-						!this._variantSelectorSlotHasContent,
+						!this.hasCustomVariantSelector && !this._variantSelectorSlotHasContent,
 						() => html`<umb-workspace-split-view-variant-selector></umb-workspace-split-view-variant-selector>`,
 					)}
 				</slot>
