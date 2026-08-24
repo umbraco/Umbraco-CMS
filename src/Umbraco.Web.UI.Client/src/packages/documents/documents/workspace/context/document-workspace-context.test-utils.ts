b81df4cc586d7f6d4249ebbdbb42a322b83718a4
Andy Butland <abutland73@gmail.com>
2026-08-24 17:27:17 +0100

    Attempt to fix failing client-side tests running on CI.

--- src/Umbraco.Web.UI.Client/src/packages/documents/documents/workspace/context/document-workspace-context.test-utils.ts
+++ src/Umbraco.Web.UI.Client/src/packages/documents/documents/workspace/context/document-workspace-context.test-utils.ts
@@ -6,6 +6,11 @@ import { UmbDataTypeItemStore } from '../../../../data-type/repository/item/data
 import { manifests as userPermissionConditionManifests } from '../../user-permissions/document/conditions/manifests.js';
 import { manifests as dataTypeItemManifests } from '../../../../data-type/repository/item/manifests.js';
 import { UmbCurrentUserContext, UmbCurrentUserStore } from '@umbraco-cms/backoffice/current-user';
+import { UMB_APP_LANGUAGE_CONTEXT, UMB_LANGUAGE_ENTITY_TYPE } from '@umbraco-cms/backoffice/language';
+import type { UmbLanguageDetailModel } from '@umbraco-cms/backoffice/language';
+import { UmbContextProviderController } from '@umbraco-cms/backoffice/context-api';
+import { UmbControllerBase } from '@umbraco-cms/backoffice/class-api';
+import { UmbArrayState, UmbObjectState } from '@umbraco-cms/backoffice/observable-api';
 import type { UmbModalContextClassArgs, UmbModalToken } from '@umbraco-cms/backoffice/modal';
 import { UmbModalManagerContext } from '@umbraco-cms/backoffice/modal';
 import type { UmbControllerHost } from '@umbraco-cms/backoffice/controller-api';
@@ -13,6 +18,45 @@ import { UmbControllerHostElementMixin } from '@umbraco-cms/backoffice/controlle
 import { UmbActionEventContext } from '@umbraco-cms/backoffice/action';
 import { UmbNotificationContext } from '@umbraco-cms/backoffice/notification';
 import { customElement } from '@umbraco-cms/backoffice/external/lit';
+
+// Mirrors the languages of the mock data sets these tests run against.
+const TEST_LANGUAGES: Array<UmbLanguageDetailModel> = [
+	{
+		entityType: UMB_LANGUAGE_ENTITY_TYPE,
+		unique: 'en-US',
+		name: 'English',
+		isDefault: true,
+		isMandatory: true,
+		fallbackIsoCode: null,
+	},
+	{
+		entityType: UMB_LANGUAGE_ENTITY_TYPE,
+		unique: 'da',
+		name: 'Danish',
+		isDefault: false,
+		isMandatory: false,
+		fallbackIsoCode: 'en-US',
+	},
+];
+
+// The workspace derives its variant options from the app languages, so a culture-varying document has no
+// cultures to expand to unless they are provided. Serving them from the host keeps that independent of
+// whichever mock data set happens to be loaded when the test runs.
+class UmbTestAppLanguageContext extends UmbControllerBase {
+	#languages = new UmbArrayState<UmbLanguageDetailModel>(TEST_LANGUAGES, (x) => x.unique);
+	readonly languages = this.#languages.asObservable();
+	readonly cultures = this.#languages.asObservablePart((x) => x.map((y) => y.unique));
+	readonly appDefaultLanguage = this.#languages.asObservablePart((x) => x.find((y) => y.isDefault));
+	readonly moreThanOneLanguage = this.#languages.asObservablePart((x) => x.length > 1);
+
+	#appLanguage = new UmbObjectState<UmbLanguageDetailModel | undefined>(TEST_LANGUAGES[0]);
+	readonly appLanguage = this.#appLanguage.asObservable();
+	readonly appLanguageCulture = this.#appLanguage.asObservablePart((x) => x?.unique);
+
+	getAppCulture() {
+		return this.#appLanguage.getValue()?.unique;
+	}
+}
 
 export const TEST_MANIFESTS = [
 	...documentDetailRepositoryManifests,
@@ -51,6 +95,11 @@ export class UmbTestDocumentWorkspaceHostElement extends UmbControllerHostElemen
 		new UmbMockModalManagerContext(this);
 		new UmbNotificationContext(this);
 		new UmbCurrentUserStore(this);
+		new UmbContextProviderController(
+			this,
+			UMB_APP_LANGUAGE_CONTEXT,
+			new UmbTestAppLanguageContext(this) as unknown as typeof UMB_APP_LANGUAGE_CONTEXT.TYPE,
+		);
 	}
 
 	public async init() {
