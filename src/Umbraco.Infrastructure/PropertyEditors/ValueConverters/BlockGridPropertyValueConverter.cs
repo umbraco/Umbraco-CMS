6958d63bcc9e97490a31224a9db894ae579355c0
kjac <kja@umbraco.dk>
2026-09-08 13:23:17 +0200

    Update Delivery API cache level for block editors

--- src/Umbraco.Infrastructure/PropertyEditors/ValueConverters/BlockGridPropertyValueConverter.cs
+++ src/Umbraco.Infrastructure/PropertyEditors/ValueConverters/BlockGridPropertyValueConverter.cs
@@ -87,7 +87,8 @@ namespace Umbraco.Cms.Core.PropertyEditors.ValueConverters
             => ConvertIntermediateToBlockGridModel(owner, propertyType, referenceCacheLevel, inter, preview);
 
         /// <inheritdoc />
-        public PropertyCacheLevel GetDeliveryApiPropertyCacheLevel(IPublishedPropertyType propertyType) => GetPropertyCacheLevel(propertyType);
+        public PropertyCacheLevel GetDeliveryApiPropertyCacheLevel(IPublishedPropertyType propertyType)
+            => PropertyCacheLevel.Elements;
 
         /// <inheritdoc />
         public PropertyCacheLevel GetDeliveryApiPropertyCacheLevelForExpansion(IPublishedPropertyType propertyType) => PropertyCacheLevel.Snapshot;
