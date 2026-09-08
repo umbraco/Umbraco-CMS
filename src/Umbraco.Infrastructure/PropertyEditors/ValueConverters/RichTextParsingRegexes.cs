77212f0263316a17f3fede941f6677a790581cbb
kjac <kja@umbraco.dk>
2026-09-08 08:10:50 +0200

    Simplify regex and make it a bit more lenient

--- src/Umbraco.Infrastructure/PropertyEditors/ValueConverters/RichTextParsingRegexes.cs
+++ src/Umbraco.Infrastructure/PropertyEditors/ValueConverters/RichTextParsingRegexes.cs
@@ -8,6 +8,6 @@ internal static partial class RichTextParsingRegexes
     /// Returns a regular expression that matches Umbraco RTE block elements, including optional inline variants and any other attributes (e.g. class, data-key) around data-content-key.
     /// </summary>
     /// <returns>A <see cref="Regex"/> for matching Umbraco RTE block elements.</returns>
-    [GeneratedRegex("<umb-rte-block(?:-inline)?(?:\\s+[\\w-]+=\"[^\"]*\")*?\\s+data-content-key=\"(?<key>[^\"]+)\"(?:\\s+[\\w-]+=\"[^\"]*\")*\\s*>(?:<!--Umbraco-Block-->)?<\\/umb-rte-block(?:-inline)?>")]
+    [GeneratedRegex("<umb-rte-block(?:-inline)?(?:.*=\"[^\"]*\")*?\\s+data-content-key=\"(?<key>[^\"]+)\"(?:.*=\"[^\"]*\")*\\s*>(?:<!--Umbraco-Block-->)?<\\/umb-rte-block(?:-inline)?>")]
     public static partial Regex BlockRegex();
 }
