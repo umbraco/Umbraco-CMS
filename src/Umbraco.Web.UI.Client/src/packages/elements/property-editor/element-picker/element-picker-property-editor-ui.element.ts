b2f080a3abcb703160c596fa4589228d6fb11648
Andy Butland <abutland73@gmail.com>
2026-09-14 13:39:43 +0100

    Remove dynamic root from media and element pickers.

--- src/Umbraco.Web.UI.Client/src/packages/elements/property-editor/element-picker/element-picker-property-editor-ui.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/elements/property-editor/element-picker/element-picker-property-editor-ui.element.ts
@@ -1,6 +1,4 @@
 ﻿import { UMB_ELEMENT_FOLDER_ENTITY_TYPE } from '../../entity.js';
-import { UmbDynamicRootResolver } from '@umbraco-cms/backoffice/dynamic-root';
-import type { UmbContentPickerDynamicRoot } from '@umbraco-cms/backoffice/dynamic-root';
 import { customElement, html, property, state } from '@umbraco-cms/backoffice/external/lit';
 import { UmbChangeEvent } from '@umbraco-cms/backoffice/event';
 import { UmbFormControlMixin, UMB_VALIDATION_EMPTY_LOCALIZATION_KEY } from '@umbraco-cms/backoffice/validation';
@@ -43,8 +41,6 @@ export class UmbElementPickerPropertyEditorUIElement
 			? { unique: startNodeId[0], entityType: UMB_ELEMENT_FOLDER_ENTITY_TYPE }
 			: undefined;
 
-		this.#dynamicRoot = config.getValueByAlias<UmbContentPickerDynamicRoot>('dynamicRoot');
-
 		const allowedContentTypes = config.getValueByAlias<string>('allowedContentTypes');
 		this._allowedContentTypes = allowedContentTypes ? allowedContentTypes.split(',').filter(Boolean) : undefined;
 	}
@@ -67,10 +63,6 @@ export class UmbElementPickerPropertyEditorUIElement
 	@state()
 	private _startNode?: UmbTreeStartNode;
 
-	#dynamicRoot?: UmbContentPickerDynamicRoot;
-
-	#dynamicRootResolver = new UmbDynamicRootResolver(this);
-
 	@state()
 	private _allowedContentTypes?: string[];
 
@@ -78,18 +70,10 @@ export class UmbElementPickerPropertyEditorUIElement
 		return this.shadowRoot?.querySelector('umb-input-element')?.focus();
 	}
 
-	override async firstUpdated(changedProperties: Map<string | number | symbol, unknown>) {
+	override firstUpdated(changedProperties: Map<string | number | symbol, unknown>) {
 		super.firstUpdated(changedProperties);
 
 		this.addFormControlElement(this.shadowRoot!.querySelector('umb-input-element')!);
-
-		// A fixed start node wins; the dynamic root is only resolved when there is none.
-		if (!this._startNode) {
-			const unique = await this.#dynamicRootResolver.resolveStartNodeUnique(this.#dynamicRoot);
-			if (unique) {
-				this._startNode = { unique, entityType: UMB_ELEMENT_FOLDER_ENTITY_TYPE };
-			}
-		}
 
 		if (this._min && this._max && this._min > this._max) {
 			console.warn(
--- src/Umbraco.Web.UI.Client/src/packages/elements/property-editor/element-picker/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/elements/property-editor/element-picker/manifests.ts
@@ -42,13 +42,6 @@ const propertyEditorUi: ManifestPropertyEditorUi = {
 					],
 					weight: 30,
 				},
-				{
-					alias: 'dynamicRoot',
-					label: 'Dynamic root',
-					description: 'Resolve the start node from the content being edited, when no start node is set',
-					propertyEditorUiAlias: 'Umb.PropertyEditorUi.DynamicRoot',
-					weight: 40,
-				},
 			],
 		},
 	},
