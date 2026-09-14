bc936687334738ad0cd4a463df6bbdafd4933d36
Niels Lyngsø <niels.lyngso@gmail.com>
2026-09-14 14:04:55 +0200

    only observe local message when autoReport is on

--- src/Umbraco.Web.UI.Client/src/packages/core/validation/controllers/validation.controller.ts
+++ src/Umbraco.Web.UI.Client/src/packages/core/validation/controllers/validation.controller.ts
@@ -221,7 +221,6 @@ export class UmbValidationController extends UmbControllerBase implements UmbVal
 	#readyToSync() {
 		if (this.#sync && this.#parent) {
 			this.#parent.addValidator(this);
-			this.observe(this.messages.messages, this.#transferMessages, 'observeLocalMessages');
 		}
 	}
 
@@ -235,6 +234,7 @@ export class UmbValidationController extends UmbControllerBase implements UmbVal
 	autoReport() {
 		this.#sync = true;
 		this.#readyToSync();
+		this.observe(this.messages.messages, this.#transferMessages, 'observeLocalMessages');
 	}
 
 	/**
