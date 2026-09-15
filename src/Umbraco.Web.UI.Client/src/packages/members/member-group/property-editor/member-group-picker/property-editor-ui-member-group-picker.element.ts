223f2b21f181e93fdcbae2458525a38146b4f4b7
Andy Butland <abutland73@gmail.com>
2026-09-15 14:02:43 +0100

    Revert removal of validation limit on member group picker.

--- src/Umbraco.Web.UI.Client/src/packages/members/member-group/property-editor/member-group-picker/property-editor-ui-member-group-picker.element.ts
+++ src/Umbraco.Web.UI.Client/src/packages/members/member-group/property-editor/member-group-picker/property-editor-ui-member-group-picker.element.ts
@@ -1,7 +1,11 @@
 import type { UmbInputMemberGroupElement } from '../../components/index.js';
-import { html, customElement, property } from '@umbraco-cms/backoffice/external/lit';
+import { html, customElement, property, state } from '@umbraco-cms/backoffice/external/lit';
 import { UmbLitElement } from '@umbraco-cms/backoffice/lit-element';
-import type { UmbPropertyEditorUiElement } from '@umbraco-cms/backoffice/property-editor';
+import type { UmbNumberRangeValueType } from '@umbraco-cms/backoffice/models';
+import type {
+	UmbPropertyEditorConfigCollection,
+	UmbPropertyEditorUiElement,
+} from '@umbraco-cms/backoffice/property-editor';
 import { UmbChangeEvent } from '@umbraco-cms/backoffice/event';
 import { UMB_VALIDATION_EMPTY_LOCALIZATION_KEY, UmbFormControlMixin } from '@umbraco-cms/backoffice/validation';
 
@@ -13,6 +17,14 @@ export class UmbPropertyEditorUIMemberGroupPickerElement
 	extends UmbFormControlMixin<string, typeof UmbLitElement, undefined>(UmbLitElement, undefined)
 	implements UmbPropertyEditorUiElement
 {
+	public set config(config: UmbPropertyEditorConfigCollection | undefined) {
+		if (!config) return;
+
+		const minMax = config?.getValueByAlias<UmbNumberRangeValueType>('validationLimit');
+		this._min = minMax?.min ?? 0;
+		this._max = minMax?.max ?? Infinity;
+	}
+
 	/**
 	 * Sets the input to readonly mode, meaning value cannot be changed but still able to read and select its content.
 	 * @type {boolean}
@@ -30,6 +42,12 @@ export class UmbPropertyEditorUIMemberGroupPickerElement
 		this.addFormControlElement(this.shadowRoot!.querySelector('umb-input-member-group')!);
 	}
 
+	@state()
+	private _min = 0;
+
+	@state()
+	private _max = Infinity;
+
 	#onChange(event: CustomEvent & { target: UmbInputMemberGroupElement }) {
 		this.value = event.target.value;
 		this.dispatchEvent(new UmbChangeEvent());
@@ -38,6 +56,8 @@ export class UmbPropertyEditorUIMemberGroupPickerElement
 	override render() {
 		return html`
 			<umb-input-member-group
+				.min=${this._min}
+				.max=${this._max}
 				.value=${this.value}
 				@change=${this.#onChange}
 				?required=${this.mandatory}
