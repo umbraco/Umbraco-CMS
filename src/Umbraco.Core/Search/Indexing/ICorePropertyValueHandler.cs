0b03105396c4b29e37729bd3f10b85c0b3e726b4
kjac <kja@umbraco.dk>
2026-09-15 14:03:23 +0200

    Clarify comment for ICorePropertyValueHandler

--- src/Umbraco.Core/Search/Indexing/ICorePropertyValueHandler.cs
+++ src/Umbraco.Core/Search/Indexing/ICorePropertyValueHandler.cs
@@ -1,8 +1,7 @@
 namespace Umbraco.Cms.Core.Search.Indexing;
 
 /// <summary>
-/// This is an internal marker interface for built-in property value handlers. It allows us to prioritize custom
-/// property value handlers over the built-in ones.
+/// This is a marker interface for built-in property value handlers, allowing custom handlers to be prioritized over them.
 /// </summary>
 public interface ICorePropertyValueHandler
 {
