8d56d365013dadaf9bb45527063abcccd76be48d
Mads Rasmussen <madsr@hey.com>
2026-09-18 12:38:59 +0200

    Member Types: Add missing breadcrumb to member type folder workspaces (#23952)
    * Add member type folder workspace menu context manifests
    
    * Move folder menu manifests into tree packages

--- src/Umbraco.Web.UI.Client/src/packages/media/media-types/menu/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media-types/menu/manifests.ts
@@ -1,4 +1,4 @@
-import { UMB_MEDIA_TYPE_TREE_ALIAS, UMB_MEDIA_TYPE_FOLDER_WORKSPACE_ALIAS } from '../tree/constants.js';
+import { UMB_MEDIA_TYPE_TREE_ALIAS } from '../tree/constants.js';
 import { UMB_MEDIA_TYPE_WORKSPACE_ALIAS } from '../workspace/constants.js';
 import { UMB_MEDIA_TYPE_MENU_ITEM_ALIAS } from './constants.js';
 import { UMB_WORKSPACE_CONDITION_ALIAS } from '@umbraco-cms/backoffice/workspace';
@@ -41,34 +41,6 @@ export const manifests: Array<UmbExtensionManifest> = [
 			{
 				alias: UMB_WORKSPACE_CONDITION_ALIAS,
 				match: UMB_MEDIA_TYPE_WORKSPACE_ALIAS,
-			},
-		],
-	},
-	{
-		type: 'workspaceContext',
-		kind: 'menuStructure',
-		name: 'Media Type Folder Menu Structure Workspace Context',
-		alias: 'Umb.Context.MediaTypeFolder.Menu.Structure',
-		api: () => import('./media-type-menu-structure.context.js'),
-		meta: {
-			menuItemAlias: UMB_MEDIA_TYPE_MENU_ITEM_ALIAS,
-		},
-		conditions: [
-			{
-				alias: UMB_WORKSPACE_CONDITION_ALIAS,
-				match: UMB_MEDIA_TYPE_FOLDER_WORKSPACE_ALIAS,
-			},
-		],
-	},
-	{
-		type: 'workspaceFooterApp',
-		kind: 'menuBreadcrumb',
-		alias: 'Umb.WorkspaceFooterApp.MediaTypeFolder.Breadcrumb',
-		name: 'Media Type Folder Breadcrumb Workspace Footer App',
-		conditions: [
-			{
-				alias: UMB_WORKSPACE_CONDITION_ALIAS,
-				match: UMB_MEDIA_TYPE_FOLDER_WORKSPACE_ALIAS,
 			},
 		],
 	},
