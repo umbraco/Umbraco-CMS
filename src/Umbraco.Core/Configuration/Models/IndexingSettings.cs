a02ac3aac07e3adf951fb60dca90845cf26e6193
Zeegaan <skrivdetud@gmail.com>
2026-09-21 10:40:44 +0900

    Remove obsolete setting (breaking)

--- src/Umbraco.Core/Configuration/Models/IndexingSettings.cs
+++ src/Umbraco.Core/Configuration/Models/IndexingSettings.cs
@@ -8,15 +8,8 @@ namespace Umbraco.Cms.Core.Configuration.Models;
 [UmbracoOptions(Constants.Configuration.ConfigIndexing)]
 public class IndexingSettings
 {
-    private const bool StaticExplicitlyIndexEachNestedProperty = false;
     private const bool StaticIndexExternalBlockElements = false;
     private const int StaticBatchSize = 10000;
-
-    /// <summary>
-    /// Gets or sets a value for whether each nested property should have it's own indexed value. Requires a rebuild of indexes when changed.
-    /// </summary>
-    [DefaultValue(StaticExplicitlyIndexEachNestedProperty)]
-    public bool ExplicitlyIndexEachNestedProperty { get; set; } = StaticExplicitlyIndexEachNestedProperty;
 
     /// <summary>
     /// Gets or sets a value indicating whether the content of external elements referenced by block editors is flattened into the index entry of referencing documents. Requires a rebuild of indexes when changed.
