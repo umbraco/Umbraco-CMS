cf422a7fb0ecebefeec6451e48cc09917645eb22
Mads Rasmussen <madsr@hey.com>
2026-09-25 09:40:56 +0200

    light manifest file clean up

--- src/Umbraco.Web.UI.Client/src/packages/media/media/property-editors/media-start-node-access/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/media/media/property-editors/media-start-node-access/manifests.ts
@@ -1,16 +1,14 @@
-import type { ManifestPropertyEditorUi } from '@umbraco-cms/backoffice/property-editor';
-
-const manifest: ManifestPropertyEditorUi = {
-	type: 'propertyEditorUi',
-	alias: 'Umb.PropertyEditorUi.MediaStartNodeAccess',
-	name: 'Media Start Node Access Property Editor UI',
-	element: () => import('./media-start-node-access-property-editor-ui.element.js'),
-	meta: {
-		label: 'Media Start Node Access',
-		icon: 'icon-picture',
-		group: '#propertyEditorUIGroups_pickers',
-		supportsReadOnly: true,
+export const manifests: Array<UmbExtensionManifest> = [
+	{
+		type: 'propertyEditorUi',
+		alias: 'Umb.PropertyEditorUi.MediaStartNodeAccess',
+		name: 'Media Start Node Access Property Editor UI',
+		element: () => import('./media-start-node-access-property-editor-ui.element.js'),
+		meta: {
+			label: 'Media Start Node Access',
+			icon: 'icon-picture',
+			group: '#propertyEditorUIGroups_pickers',
+			supportsReadOnly: true,
+		},
 	},
-};
-
-export const manifests: Array<UmbExtensionManifest> = [manifest];
+];
--- src/Umbraco.Web.UI.Client/src/packages/user/user-group/property-editor/user-group-picker/manifests.ts
+++ src/Umbraco.Web.UI.Client/src/packages/user/user-group/property-editor/user-group-picker/manifests.ts
@@ -1,15 +1,13 @@
-import type { ManifestPropertyEditorUi } from '@umbraco-cms/backoffice/property-editor';
-
-const manifest: ManifestPropertyEditorUi = {
-	type: 'propertyEditorUi',
-	alias: 'Umb.PropertyEditorUi.UserGroupPicker',
-	name: 'User Group Picker Property Editor UI',
-	element: () => import('./property-editor-ui-user-group-picker.element.js'),
-	meta: {
-		label: 'User Group Picker',
-		icon: 'icon-users',
-		group: '#propertyEditorUIGroups_people',
+export const manifests: Array<UmbExtensionManifest> = [
+	{
+		type: 'propertyEditorUi',
+		alias: 'Umb.PropertyEditorUi.UserGroupPicker',
+		name: 'User Group Picker Property Editor UI',
+		element: () => import('./property-editor-ui-user-group-picker.element.js'),
+		meta: {
+			label: 'User Group Picker',
+			icon: 'icon-users',
+			group: '#propertyEditorUIGroups_people',
+		},
 	},
-};
-
-export const manifests: Array<UmbExtensionManifest> = [manifest];
+];
